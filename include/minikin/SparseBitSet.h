5adafc0d84d238948b5d257ec5311030ca04271c
Kenny Root <kroot@google.com>
2013-09-11 23:24:24 -0700

    Use canonical UniquePtr.h file
    Change-Id: I00953971034a7d00ca165accdab528d2b8ff27a7

--- include/minikin/SparseBitSet.h
+++ include/minikin/SparseBitSet.h
@@ -19,7 +19,7 @@
 
 #include <stdint.h>
 #include <sys/types.h>
-#include "utils/UniquePtr.h"
+#include <UniquePtr.h>
 
 // ---------------------------------------------------------------------------
 
