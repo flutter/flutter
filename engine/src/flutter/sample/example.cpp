ded72d1e4a2bd01048319e44293138a87f95bf28
Behdad Esfahbod <behdad@google.com>
2014-07-24 19:18:14 -0400

    Remove deprecated API
    It has been unused outside minikin.
    
    Change-Id: Iaa2237767d81c77f90d0264e633375e601dd72f1

--- engine/src/flutter/sample/example.cpp
+++ engine/src/flutter/sample/example.cpp
@@ -83,10 +83,10 @@ int runMinikinTest() {
     FontCollection *collection = makeFontCollection();
     Layout layout;
     layout.setFontCollection(collection);
-    layout.setProperties("font-size: 32;");
     const char *text = "fine world \xe0\xa4\xa8\xe0\xa4\xae\xe0\xa4\xb8\xe0\xa5\x8d\xe0\xa4\xa4\xe0\xa5\x87";
+    const char *style = "font-size: 32;";
     icu::UnicodeString icuText = icu::UnicodeString::fromUTF8(text);
-    layout.doLayout(icuText.getBuffer(), icuText.length());
+    layout.doLayout(icuText.getBuffer(), 0, icuText.length(), icuText.length(), style);
     layout.dump();
     Bitmap bitmap(250, 50);
     layout.draw(&bitmap, 10, 40, 32);
