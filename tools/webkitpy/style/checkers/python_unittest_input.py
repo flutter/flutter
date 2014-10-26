7d0b4fb726848ca4cdc13e6339dbc51ea4ebc071
Ojan Vafai <ojan@chromium.org>
2014-10-26 10:36:25 -0700

    Prune a bunch of webkitpy.
    Remove a bunch of unused bits. Some of these bits we might
    want to add back (e.g. the python style checker), but we'd
    want it as part of the mojo style checker.
    
    R=abarth@chromium.org
    
    Review URL: https://codereview.chromium.org/675343003

--- tools/webkitpy/style/checkers/python_unittest_input.py
+++
@@ -1,4 +0,0 @@
-# This file is sample input for python_unittest.py and includes two
-# problems, one that will generate a PEP-8 warning for trailing whitespace
-# and one that will generate a pylint error for an undefined variable.
-print error() 
--- tools/webkitpy/thirdparty/irc/__init__.py
+++
@@ -1,1 +0,0 @@
-# This file is required for Python to search this directory for modules.
