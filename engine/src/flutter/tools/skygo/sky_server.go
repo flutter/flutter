0cbe4deb25c3e8c0b22d8a30f7ad4b26f9d28be0
Eric Seidel <eseidel@chromium.org>
2014-12-16 11:20:40 -0800

    Add POST support to XHR as well as .status and statusText support
    Also fixed all the XHR tests to actually run and work
    I learned from elliot that the it function has to
    take a "done" argument to be treated asynchronously.
    
    My utf8 conversion is a hack, but we deleted
    window.TextEncoder (it was a module) when we forked.
    We could resurrect TextEncoder (and probably should)
    but I've left that for a separate change.
    
    I also augmented sky_server to have a special /echo_post
    handler so we can test POST requests.
    
    R=esprehn@chromium.org
    BUG=
    
    Committed: https://chromium.googlesource.com/external/mojo/+/5ef81d249b841f44c9593ffb0158d64c7e0febfc
    
    Review URL: https://codereview.chromium.org/810523002

--- engine/src/flutter/tools/skygo/sky_server.go
+++ engine/src/flutter/tools/skygo/sky_server.go
@@ -6,6 +6,7 @@ package main
 
 import (
     "flag"
+    "io/ioutil"
     "net/http"
     "path"
     "strings"
@@ -38,6 +39,11 @@ func main() {
     genRoot := path.Join(root, "out", *configuration, "gen")
 
     http.Handle("/", skyHandler(root))
+    http.HandleFunc("/echo_post", func(w http.ResponseWriter, r *http.Request) {
+        defer r.Body.Close()
+        body, _ := ioutil.ReadAll(r.Body)
+        w.Write(body)
+    })
     http.Handle("/mojo/public/", http.StripPrefix("/mojo/public/", skyHandler(path.Join(genRoot, "mojo", "public"))))
     http.Handle("/mojo/services/", http.StripPrefix("/mojo/services/", skyHandler(path.Join(genRoot, "mojo", "services"))))
     http.Handle("/sky/services/", http.StripPrefix("/sky/services/", skyHandler(path.Join(genRoot, "sky", "services"))))
