656c44bafb1b44dc8aaeddb0501637a60d858f7c
Adam Barth <abarth@chromium.org>
2015-01-15 16:32:16 -0800

    Make tracing work on Android
    Previously, the trace coordinator would try to write a trace to the file
    system, but it is awkward to write to the file system on Android. Instead, we
    now stream the trace data to a data pipe. The Sky debugger consumes that data
    pipe and sends it to the host via the command HTTP server.
    
    R=eseidel@chromium.org, jamesr@chromium.org
    
    Review URL: https://codereview.chromium.org/853943005

---
+++ engine/src/flutter/tools/debugger/prompt/trace_collector.h
@@ -0,0 +1,40 @@
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef SKY_TOOLS_DEBUGGER_PROMPT_TRACE_COLLECTOR_H_
+#define SKY_TOOLS_DEBUGGER_PROMPT_TRACE_COLLECTOR_H_
+
+#include <vector>
+
+#include "base/callback.h"
+#include "mojo/common/data_pipe_drainer.h"
+
+namespace sky {
+namespace debugger {
+
+class TraceCollector : public mojo::common::DataPipeDrainer::Client {
+ public:
+  typedef base::Callback<void(std::string)> TraceCallback;
+
+  explicit TraceCollector(mojo::ScopedDataPipeConsumerHandle source);
+  ~TraceCollector();
+
+  void GetTrace(TraceCallback callback);
+
+ private:
+  void OnDataAvailable(const void* data, size_t num_bytes) override;
+  void OnDataComplete() override;
+
+  std::string GetTraceAsString();
+
+  mojo::common::DataPipeDrainer drainer_;
+  std::vector<char> trace_;
+  bool is_complete_;
+  TraceCallback callback_;
+};
+
+}  // namespace debugger
+}  // namespace sky
+
+#endif  // SKY_TOOLS_DEBUGGER_PROMPT_TRACE_COLLECTOR_H_
