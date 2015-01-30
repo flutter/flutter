0f23bf9144fbfc2c993c0ba9a6be085b9fbea504
Adam Barth <abarth@chromium.org>
2015-01-30 14:05:57 -0800

    Add a standalone SkyShell.apk for testing
    This CL adds a SkyShell.apk, which will become a direct embedding of Sky on
    Android that will let us run performance tests on the Sky engine in a
    self-contained environment on a real device.
    
    R=eseidel@chromium.org
    
    Review URL: https://codereview.chromium.org/893643002

---
+++ shell/library_loader.cc
@@ -0,0 +1,40 @@
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/android/base_jni_registrar.h"
+#include "base/android/jni_android.h"
+#include "base/android/jni_registrar.h"
+#include "base/android/library_loader/library_loader_hooks.h"
+#include "base/logging.h"
+#include "sky/shell/sky_main.h"
+
+namespace {
+
+base::android::RegistrationMethod kSkyRegisteredMethods[] = {
+    {"SkyMain", sky::shell::RegisterSkyMain},
+};
+
+bool RegisterMojoJni(JNIEnv* env) {
+  return RegisterNativeMethods(env, kSkyRegisteredMethods,
+                               arraysize(kSkyRegisteredMethods));
+}
+
+}  // namespace
+
+// This is called by the VM when the shared library is first loaded.
+JNI_EXPORT jint JNI_OnLoad(JavaVM* vm, void* reserved) {
+  base::android::InitVM(vm);
+  JNIEnv* env = base::android::AttachCurrentThread();
+
+  if (!base::android::RegisterLibraryLoaderEntryHook(env))
+    return -1;
+
+  if (!base::android::RegisterJni(env))
+    return -1;
+
+  if (!RegisterMojoJni(env))
+    return -1;
+
+  return JNI_VERSION_1_4;
+}
