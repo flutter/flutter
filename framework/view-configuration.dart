4b1def112bed643211fe0c2708779be086e79b01
Eric Seidel <eseidel@chromium.org>
2015-02-24 14:23:54 -0800

    Add ViewConfiguration and make our scrollbar size match Android
    Turns out scrollbars are very inconsistent throughout Android apps.
    
    R=abarth@chromium.org
    
    Review URL: https://codereview.chromium.org/955653005

---
+++ framework/view-configuration.dart
@@ -0,0 +1,10 @@
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Modeled after Android's ViewConfiguration:
+// https://github.com/android/platform_frameworks_base/blob/master/core/java/android/view/ViewConfiguration.java
+
+const int kScrollbarSize = 10;
+const int kScrollbarFadeDelay = 300;
+const int kScrollbarFadeDuration = 250;
