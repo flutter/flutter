999fc7afa629ca47c0489294276be6d1be399df2
Eric Seidel <eseidel@chromium.org>
2015-02-27 16:46:19 -0800

    Add support for scroll and fling gestures to SkyShell.
    This is not complete gesture support by far, but it's a start.
    MojoShell and Chrome use a C++ GestureDetector, this code attempts
    to use the Android (Java) GestureDetector instead.
    
    We probably should not be sending gesturetap until we've decided
    it's not a scroll, etc, but this implementation does not go that far.
    
    I had to fix a bug whereby we were assuming the InputEvent.time_stamp
    was in TimeDelta's internal format, which was wrong.  When we get
    time_stamp from Android its in ms since boot.
    
    R=abarth@chromium.org
    
    Review URL: https://codereview.chromium.org/969493002

--- engine/src/flutter/shell/ui/input_event_converter.cc
+++ engine/src/flutter/shell/ui/input_event_converter.cc
@@ -15,8 +15,7 @@ scoped_ptr<blink::WebInputEvent> BuildWebPointerEvent(
     const InputEventPtr& event, float device_pixel_ratio) {
   scoped_ptr<blink::WebPointerEvent> web_event(new blink::WebPointerEvent);
 
-  web_event->timeStampMS =
-      base::TimeDelta::FromInternalValue(event->time_stamp).InMillisecondsF();
+  web_event->timeStampMS = event->time_stamp;
 
   switch (event->type) {
     case EVENT_TYPE_POINTER_DOWN:
@@ -47,15 +46,66 @@ scoped_ptr<blink::WebInputEvent> BuildWebPointerEvent(
   return web_event.Pass();
 }
 
+scoped_ptr<blink::WebInputEvent> BuildWebGestureEvent(
+    const InputEventPtr& event, float device_pixel_ratio) {
+  scoped_ptr<blink::WebGestureEvent> web_event(new blink::WebGestureEvent);
+
+  web_event->timeStampMS = event->time_stamp;
+
+  switch (event->type) {
+    case EVENT_TYPE_GESTURE_SCROLL_BEGIN:
+      web_event->type = blink::WebInputEvent::GestureScrollBegin;
+      break;
+    case EVENT_TYPE_GESTURE_SCROLL_END:
+      web_event->type = blink::WebInputEvent::GestureScrollEnd;
+      break;
+    case EVENT_TYPE_GESTURE_SCROLL_UPDATE:
+      web_event->type = blink::WebInputEvent::GestureScrollUpdate;
+      web_event->data.scrollUpdate.deltaX =
+          event->gesture_data->dx / device_pixel_ratio;
+      web_event->data.scrollUpdate.deltaY =
+          event->gesture_data->dy / device_pixel_ratio;
+      break;
+    case EVENT_TYPE_GESTURE_FLING_START:
+      web_event->type = blink::WebInputEvent::GestureFlingStart;
+      web_event->data.flingStart.velocityX =
+          event->gesture_data->velocityX / device_pixel_ratio;
+      web_event->data.flingStart.velocityY =
+          event->gesture_data->velocityY / device_pixel_ratio;
+      break;
+    case EVENT_TYPE_GESTURE_FLING_CANCEL:
+      web_event->type = blink::WebInputEvent::GestureFlingCancel;
+      break;
+    default:
+      break;
+  }
+
+  if (event->gesture_data) {
+    web_event->x = event->gesture_data->x / device_pixel_ratio;
+    web_event->y = event->gesture_data->y / device_pixel_ratio;
+  }
+
+  return web_event.Pass();
+}
+
 }  // namespace
 
 scoped_ptr<blink::WebInputEvent> ConvertEvent(const InputEventPtr& event,
                                               float device_pixel_ratio) {
-  if (event->type == EVENT_TYPE_POINTER_DOWN ||
-      event->type == EVENT_TYPE_POINTER_UP ||
-      event->type == EVENT_TYPE_POINTER_MOVE ||
-      event->type == EVENT_TYPE_POINTER_CANCEL) {
-    return BuildWebPointerEvent(event, device_pixel_ratio);
+  switch (event->type) {
+    case EVENT_TYPE_POINTER_DOWN:
+    case EVENT_TYPE_POINTER_UP:
+    case EVENT_TYPE_POINTER_MOVE:
+    case EVENT_TYPE_POINTER_CANCEL:
+      return BuildWebPointerEvent(event, device_pixel_ratio);
+    case EVENT_TYPE_GESTURE_SCROLL_BEGIN:
+    case EVENT_TYPE_GESTURE_SCROLL_UPDATE:
+    case EVENT_TYPE_GESTURE_SCROLL_END:
+    case EVENT_TYPE_GESTURE_FLING_START:
+    case EVENT_TYPE_GESTURE_FLING_CANCEL:
+      return BuildWebGestureEvent(event, device_pixel_ratio);
+    case EVENT_TYPE_UNKNOWN:
+      NOTIMPLEMENTED() << "ConvertEvent received unexpected EVENT_TYPE_UNKNOWN";
   }
 
   return scoped_ptr<blink::WebInputEvent>();
