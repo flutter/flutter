8ea48be24b7dd7b7faa9621183e8cfc42efeb2ee
Eric Seidel <eseidel@chromium.org>
2015-03-03 11:17:10 -0800

    Change fn-stocks to use a different color toolbar.
    R=abarth@chromium.org
    
    Review URL: https://codereview.chromium.org/973133002

--- examples/fn/widgets/toolbar.dart
+++ examples/fn/widgets/toolbar.dart
@@ -9,7 +9,7 @@ class Toolbar extends Component {
     align-items: center;
     height: 84px;
     z-index: 1;
-    background-color: #3F51B5;
+    background-color: #9C27B0; // Purple 500
     color: white;
     box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);'''
   );
