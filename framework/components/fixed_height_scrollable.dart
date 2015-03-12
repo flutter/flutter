111a25a569b307dfad5f2f35984dcecbd1dfffd3
Adam Barth <abarth@chromium.org>
2015-03-12 09:40:43 -0700

    Rename fling-curve.dart to fling_curve.dart
    We're using underscores as word separators per Dart style.
    
    TBR=eseidel@chromium.org
    
    Review URL: https://codereview.chromium.org/998373002

--- framework/components/fixed_height_scrollable.dart
+++ framework/components/fixed_height_scrollable.dart
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import '../animation/fling-curve.dart';
+import '../animation/fling_curve.dart';
 import '../fn.dart';
 import 'dart:sky' as sky;
 
