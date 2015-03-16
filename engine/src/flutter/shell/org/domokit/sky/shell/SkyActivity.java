8e39231ad3286c62adb1625532194423578b642a
Adam Barth <abarth@chromium.org>
2015-03-16 11:14:04 -0700

    Make the status bar translucent
    This CL lets Sky apps draw behind the Android status bar, better matching the
    material design spec.
    
    R=eseidel@chromium.org
    
    Review URL: https://codereview.chromium.org/1010493007

--- engine/src/flutter/shell/org/domokit/sky/shell/SkyActivity.java
+++ engine/src/flutter/shell/org/domokit/sky/shell/SkyActivity.java
@@ -6,6 +6,8 @@ package org.domokit.sky.shell;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
 
 /**
  * Base class for activities that use Sky.
@@ -19,6 +21,14 @@ public class SkyActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        getWindow().addFlags(
+                WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        getWindow().setStatusBarColor(0x40000000);
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+
         SkyMain.ensureInitialized(getApplicationContext());
         mView = new PlatformView(this);
         setContentView(mView);
