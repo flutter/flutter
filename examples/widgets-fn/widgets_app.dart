4b6783ceee6a6d42a169914e602e25d00b56d6e3
Adam Barth <abarth@chromium.org>
2015-03-17 19:21:36 -0700

    Add a menu to the stocks app
    We should probably move this menu into a view for an individual stock, but for
    now add it to the main stock screen.
    
    R=ojan@chromium.org, eseidel@chromium.org
    
    Review URL: https://codereview.chromium.org/1008003007

--- examples/widgets-fn/widgets_app.dart
+++ examples/widgets-fn/widgets_app.dart
@@ -5,7 +5,6 @@
 import '../../framework/fn.dart';
 import '../../framework/components/button.dart';
 import '../../framework/components/popup_menu.dart';
-import '../../framework/components/popup_menu_item.dart';
 
 class WidgetsApp extends App {
   static final Style _menuStyle = new Style('''
@@ -22,14 +21,14 @@ class WidgetsApp extends App {
           style: _menuStyle,
           children: [
             new PopupMenu(
-              children: [
-                  new PopupMenuItem(key: '1', children: [new Text('People & options')]),
-                  new PopupMenuItem(key: '2', children: [new Text('New group conversation')]),
-                  new PopupMenuItem(key: '3', children: [new Text('Turn history off')]),
-                  new PopupMenuItem(key: '4', children: [new Text('Archive')]),
-                  new PopupMenuItem(key: '5', children: [new Text('Delete')]),
-                  new PopupMenuItem(key: '6', children: [new Text('Un-merge SMS')]),
-                  new PopupMenuItem(key: '7', children: [new Text('Help & feeback')]),
+              items: [
+                [new Text('People & options')],
+                [new Text('New group conversation')],
+                [new Text('Turn history off')],
+                [new Text('Archive')],
+                [new Text('Delete')],
+                [new Text('Un-merge SMS')],
+                [new Text('Help & feeback')],
               ],
               level: 4),
             ]
--- framework/components/popup_menu.dart
+++ framework/components/popup_menu.dart
@@ -5,19 +5,27 @@
 import '../fn.dart';
 import '../theme/colors.dart';
 import 'material.dart';
+import 'popup_menu_item.dart';
 
 class PopupMenu extends Component {
   static final Style _style = new Style('''
     border-radius: 2px;
+    padding: 8px 0;
     background-color: ${Grey[50]};'''
   );
 
-  List<Node> children;
+  List<List<Node>> items;
   int level;
 
-  PopupMenu({ Object key, this.children, this.level }) : super(key: key);
+  PopupMenu({ Object key, this.items, this.level }) : super(key: key);
 
   Node build() {
+    List<Node> children = [];
+    int i = 0;
+    items.forEach((List<Node> item) {
+      children.add(new PopupMenuItem(key: i++, children: item));
+    });
+
     return new Material(
       style: _style,
       children: children,
