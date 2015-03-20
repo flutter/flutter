b14165ea843bfc5487c8b7dde6910b03156fd481
Adam Barth <abarth@chromium.org>
2015-03-20 14:16:43 -0700

    Enable tracing in SkyShell
    This CL teaches shelldb how to trace SkyShell.
    
    R=eseidel@chromium.org
    
    Review URL: https://codereview.chromium.org/1027903002

---
+++ engine/src/flutter/shell/org/domokit/sky/shell/TracingController.java
@@ -0,0 +1,75 @@
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.domokit.sky.shell;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Environment;
+import android.util.Log;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
+
+import org.chromium.base.JNINamespace;
+
+/**
+ * A controller for the tracing system.
+ */
+@JNINamespace("sky::shell")
+class TracingController {
+    private static final String TAG = "TracingController";
+    private static final String TRACING_START = ".TRACING_START";
+    private static final String TRACING_STOP = ".TRACING_STOP";
+
+    private final Context mContext;
+    private final TracingBroadcastReceiver mBroadcastReceiver;
+    private final TracingIntentFilter mIntentFilter;
+
+    public TracingController(Context context) {
+        mContext = context;
+        mBroadcastReceiver = new TracingBroadcastReceiver();
+        mIntentFilter = new TracingIntentFilter(context);
+
+        mContext.registerReceiver(mBroadcastReceiver, mIntentFilter);
+    }
+
+    private String generateTracingFilePath() {
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd-HHmmss", Locale.US);
+        formatter.setTimeZone(TimeZone.getTimeZone("UTC"));
+        File dir = mContext.getCacheDir();
+        String date = formatter.format(new Date());
+        File file = new File(dir, "sky-trace-" + date + ".json");
+        return file.getPath();
+    }
+
+    class TracingIntentFilter extends IntentFilter {
+        TracingIntentFilter(Context context) {
+            Log.e(TAG, context.getPackageName() + TRACING_START);
+            addAction(context.getPackageName() + TRACING_START);
+            addAction(context.getPackageName() + TRACING_STOP);
+        }
+    }
+
+    class TracingBroadcastReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction().endsWith(TRACING_START)) {
+                nativeStartTracing();
+            } else if (intent.getAction().endsWith(TRACING_STOP)) {
+                nativeStopTracing(generateTracingFilePath());
+            } else {
+                Log.e(TAG, "Unexpected intent: " + intent);
+            }
+        }
+    }
+
+    private static native void nativeStartTracing();
+    private static native void nativeStopTracing(String path);
+}
---
+++ engine/src/flutter/shell/tracing_controller.cc
@@ -0,0 +1,70 @@
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "sky/shell/tracing_controller.h"
+
+#include "base/android/jni_android.h"
+#include "base/android/jni_string.h"
+#include "base/files/file_util.h"
+#include "base/macros.h"
+#include "base/trace_event/trace_event.h"
+#include "jni/TracingController_jni.h"
+
+namespace sky {
+namespace shell {
+namespace {
+
+const char kStart[] = "{\"traceEvents\":[";
+const char kEnd[] = "]}";
+
+static FILE* g_file = NULL;
+
+void Write(const std::string& data) {
+  ignore_result(fwrite(data.data(), data.length(), 1, g_file));
+}
+
+void HandleChunk(const scoped_refptr<base::RefCountedString>& chunk,
+                 bool has_more_events) {
+  Write(chunk->data());
+  if (has_more_events)
+    Write(",");
+
+  if (!has_more_events) {
+    Write(kEnd);
+    base::CloseFile(g_file);
+    g_file = NULL;
+
+    LOG(INFO) << "Trace complete";
+  }
+}
+
+}  // namespace
+
+static void StartTracing(JNIEnv* env, jclass clazz) {
+  LOG(INFO) << "Starting trace";
+
+  base::trace_event::TraceLog::GetInstance()->SetEnabled(
+      base::trace_event::CategoryFilter("*"),
+      base::trace_event::TraceLog::RECORDING_MODE,
+      base::trace_event::TraceOptions(base::trace_event::RECORD_UNTIL_FULL));
+}
+
+static void StopTracing(JNIEnv* env, jclass clazz, jstring path) {
+  base::trace_event::TraceLog::GetInstance()->SetDisabled();
+
+  base::FilePath file_path(base::android::ConvertJavaStringToUTF8(env, path));
+  g_file = base::OpenFile(file_path, "w");
+  CHECK(g_file) << "Failed to open file " << file_path.LossyDisplayName();
+
+  LOG(INFO) << "Saving trace to " << file_path.LossyDisplayName();
+  Write(kStart);
+  base::trace_event::TraceLog::GetInstance()->Flush(base::Bind(&HandleChunk));
+}
+
+bool RegisterTracingController(JNIEnv* env) {
+  return RegisterNativesImpl(env);
+}
+
+}  // namespace shell
+}  // namespace sky
