65f13ec5e7f8c591a17c1c5267632a17ad1484d5
Eric Seidel <eseidel@chromium.org>
2015-03-20 12:45:25 -0700

    Attempt to make us not crash on KitKat.
    https://github.com/domokit/mojo/issues/68
    
    R=abarth@chromium.org
    
    Review URL: https://codereview.chromium.org/1026873002

--- shell/org/domokit/sky/shell/SkyActivity.java
+++ shell/org/domokit/sky/shell/SkyActivity.java
@@ -5,6 +5,7 @@
 package org.domokit.sky.shell;
 
 import android.app.Activity;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
@@ -24,7 +25,9 @@ public class SkyActivity extends Activity {
 
         getWindow().addFlags(
                 WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-        getWindow().setStatusBarColor(0x40000000);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setStatusBarColor(0x40000000);
+        }
         getWindow().getDecorView().setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                 | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
