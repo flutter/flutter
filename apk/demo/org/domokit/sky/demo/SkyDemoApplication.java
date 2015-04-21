1d74d5f631533e8e70a66f7b4c5d09c9fd86816f
Dave Moore <davemoore@chromium.org>
2015-04-21 09:56:05 -0700

    Add sensors application to mojo
    R=qsr@chromium.org
    
    Review URL: https://codereview.chromium.org/1093033002

--- apk/demo/org/domokit/sky/demo/SkyDemoApplication.java
+++ apk/demo/org/domokit/sky/demo/SkyDemoApplication.java
@@ -7,11 +7,11 @@ package org.domokit.sky.demo;
 import android.content.Context;
 
 import org.chromium.mojo.keyboard.KeyboardServiceImpl;
+import org.chromium.mojo.sensors.SensorServiceImpl;
 import org.chromium.mojo.system.Core;
 import org.chromium.mojo.system.MessagePipeHandle;
 import org.chromium.mojom.keyboard.KeyboardService;
 import org.chromium.mojom.sensors.SensorService;
-import org.domokit.sensors.SensorServiceImpl;
 import org.domokit.sky.shell.ServiceFactory;
 import org.domokit.sky.shell.ServiceRegistry;
 import org.domokit.sky.shell.SkyApplication;
@@ -27,7 +27,7 @@ public class SkyDemoApplication extends SkyApplication {
         ServiceRegistry.SHARED.register(SensorService.MANAGER.getName(), new ServiceFactory() {
             @Override
             public void connectToService(Context context, Core core, MessagePipeHandle pipe) {
-                new SensorServiceImpl(context, core, pipe);
+                new SensorServiceImpl(context, pipe);
             }
         });
 
