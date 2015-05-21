045620e02716821e97ef136e2baf9ec2d0a37463
Adam Barth <abarth@chromium.org>
2015-05-21 12:34:38 -0700

    Port touch-demo.sky to SkyView universe
    R=eseidel@chromium.org
    
    Review URL: https://codereview.chromium.org/1151613005

---
+++ examples/raw/touch_demo.dart
@@ -0,0 +1,91 @@
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'dart:math';
+import 'dart:sky';
+import 'package:sky/framework/layout2.dart';
+
+// Material design colors. :p
+List<int> colors = [
+  0xFF009688,
+  0xFFFFC107,
+  0xFF9C27B0,
+  0xFF03A9F4,
+  0xFF673AB7,
+  0xFFCDDC39,
+];
+
+class Dot {
+  final Paint _paint;
+  double x = 0.0;
+  double y = 0.0;
+  double radius = 0.0;
+
+  Dot({int color}) : _paint = new Paint()..color = color;
+
+  void update(PointerEvent event) {
+    x = event.x;
+    y = event.y;
+    radius = 5 + (95 * event.pressure);
+  }
+
+  void paint(RenderNodeDisplayList canvas) {
+    canvas.drawCircle(x, y, radius, _paint);
+  }
+}
+
+class RenderTouchDemo extends RenderBox {
+  Map<int, Dot> dots = new Map();
+
+  RenderTouchDemo();
+
+  bool handlePointer(PointerEvent event, { double x: 0.0, double y: 0.0 }) {
+    switch (event.type) {
+      case 'pointerdown':
+        int color = colors[event.pointer.remainder(colors.length)];
+        dots[event.pointer] = new Dot(color: color)..update(event);
+        break;
+      case 'pointerup':
+        dots.remove(event.pointer);
+        break;
+      case 'pointercancel':
+        dots = new Map();
+        break;
+      case 'pointermove':
+        dots[event.pointer].update(event);
+        break;
+    }
+    markNeedsPaint();
+    return true;
+  }
+
+  void paint(RenderNodeDisplayList canvas) {
+    dots.forEach((_, Dot dot) {
+      dot.paint(canvas);
+    });
+  }
+}
+
+RenderView renderView;
+
+void beginFrame(double timeStamp) {
+  RenderNode.flushLayout();
+  renderView.paintFrame();
+}
+
+bool handleEvent(Event event) {
+  if (event is! PointerEvent)
+    return false;
+  return renderView.handlePointer(event, x: event.x, y: event.y);
+}
+
+void main() {
+  view.setEventCallback(handleEvent);
+  view.setBeginFrameCallback(beginFrame);
+
+  renderView = new RenderView(root: new RenderTouchDemo());
+  renderView.layout(newWidth: view.width, newHeight: view.height);
+
+  view.scheduleFrame();
+}
