db50ce1659c1d89e81051ca5dfa76cbe11d6bbec
Eric Seidel <eseidel@chromium.org>
2015-05-28 13:56:36 -0700

    Add hello_fn2.dart and make Text("Hello fn2") actually render something.
    R=ianh@google.com
    
    Review URL: https://codereview.chromium.org/1160843005

---
+++ examples/fn2/hello_fn2.dart
@@ -0,0 +1,15 @@
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:sky/framework/fn2.dart';
+
+class HelloWorldApp extends App {
+  UINode build() {
+    return new Text('Hello, fn2!');
+  }
+}
+
+void main() {
+  new HelloWorldApp();
+}
