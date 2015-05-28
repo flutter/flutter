57d269086c724a2c1908b50bb98827d8d316e00d
Hixie <github.com@spam.hixie.ch>
2015-05-28 15:19:36 -0700

    Make RenderParagraph mutable, and make it fit the new RenderNode protocols
    R=abarth@chromium.org, eseidel@chromium.org
    
    Review URL: https://codereview.chromium.org/1165463002

--- engine/src/flutter/tests/raw/render_flex.dart
+++ engine/src/flutter/tests/raw/render_flex.dart
@@ -14,7 +14,7 @@ class RenderSolidColor extends RenderDecoratedBox {
 
   RenderSolidColor(int backgroundColor, { this.desiredSize: const sky.Size.infinite() })
       : backgroundColor = backgroundColor,
-        super(new BoxDecoration(backgroundColor: backgroundColor)) {
+        super(decoration: new BoxDecoration(backgroundColor: backgroundColor)) {
   }
 
   sky.Size getIntrinsicDimensions(BoxConstraints constraints) {
@@ -39,45 +39,57 @@ void main() {
   initUnit();
 
   test("should flex", () {
-    var root = new RenderFlex(
-        direction: FlexDirection.Vertical,
-        decoration: new BoxDecoration(backgroundColor: 0xFF000000));
+    RenderFlex flexRoot = new RenderFlex(direction: FlexDirection.Vertical);
 
-    void addFlexChild(RenderFlex parent, int backgroundColor, { int flex: 0 }) {
+    RenderNode root = new RenderDecoratedBox(
+      decoration: new BoxDecoration(backgroundColor: 0xFF000000),
+      child: flexRoot
+    );
+
+    void addFlexChildSolidColor(RenderFlex parent, int backgroundColor, { int flex: 0 }) {
       RenderNode child = new RenderSolidColor(backgroundColor);
       parent.add(child);
       child.parentData.flex = flex;
     }
 
     // Yellow bar at top
-    addFlexChild(root, 0xFFFFFF00, flex: 1);
+    addFlexChildSolidColor(flexRoot, 0xFFFFFF00, flex: 1);
 
     // Turquoise box
-    root.add(new RenderSolidColor(0x7700FFFF, desiredSize: new sky.Size(100.0, 100.0)));
+    flexRoot.add(new RenderSolidColor(0x7700FFFF, desiredSize: new sky.Size(100.0, 100.0)));
 
     // Green and cyan render block with padding
-    var renderBlock = new RenderBlock(decoration: new BoxDecoration(backgroundColor: 0xFFFFFFFF));
+    var renderBlock = new RenderBlock();
 
     renderBlock.add(new RenderSolidColor(0xFF00FF00, desiredSize: new sky.Size(100.0, 50.0)));
     renderBlock.add(new RenderSolidColor(0x7700FFFF, desiredSize: new sky.Size(50.0, 100.0)));
 
-    root.add(new RenderPadding(const EdgeDims(10.0, 10.0, 10.0, 10.0), renderBlock));
+    var renderDecoratedBlock = new RenderDecoratedBox(
+      decoration: new BoxDecoration(backgroundColor: 0xFFFFFFFF),
+      child: renderBlock
+    );
 
-    var row = new RenderFlex(
-      direction: FlexDirection.Horizontal,
-      decoration: new BoxDecoration(backgroundColor: 0xFF333333));
+    flexRoot.add(new RenderPadding(const EdgeDims(10.0, 10.0, 10.0, 10.0), renderDecoratedBlock));
+
+    var row = new RenderFlex(direction: FlexDirection.Horizontal);
 
     // Purple and blue cells
-    addFlexChild(row, 0x77FF00FF, flex: 1);
-    addFlexChild(row, 0xFF0000FF, flex: 2);
+    addFlexChildSolidColor(row, 0x77FF00FF, flex: 1);
+    addFlexChildSolidColor(row, 0xFF0000FF, flex: 2);
 
-    root.add(row);
-    row.parentData.flex = 3;
+    var decoratedRow = new RenderDecoratedBox(
+      decoration: new BoxDecoration(backgroundColor: 0xFF333333),
+      child: row
+    );
+
+    flexRoot.add(decoratedRow);
+    decoratedRow.parentData.flex = 3;
 
     app = new AppView(root);
 
     expect(root.size.width, equals(sky.view.width));
     expect(root.size.height, equals(sky.view.height));
     expect(renderBlock.size.width, equals(sky.view.width - 20.0));
+
   });
 }
