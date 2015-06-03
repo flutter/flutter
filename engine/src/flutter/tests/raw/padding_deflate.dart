8d6e3bef6e10680357404689da9f62b9ab6053c8
Eric Seidel <eseidel@chromium.org>
2015-06-03 11:12:17 -0700

    Fix min/max typo in BoxConstraints.deflate
    R=ianh@google.com
    
    Review URL: https://codereview.chromium.org/1153413003

---
+++ engine/src/flutter/tests/raw/padding_deflate.dart
@@ -0,0 +1,26 @@
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'dart:sky' as sky;
+import '../resources/third_party/unittest/unittest.dart';
+import '../resources/unit.dart';
+import '../resources/display_list.dart';
+import 'package:sky/framework/rendering/box.dart';
+
+void main() {
+  initUnit();
+
+  test("should not have a 0 sized colored Box", () {
+    var coloredBox = new RenderDecoratedBox();
+    var paddingBox = new RenderPadding(padding: const EdgeDims.all(10.0),
+        child: coloredBox);
+    RenderBox root = new RenderDecoratedBox(
+      child: paddingBox
+    );
+    TestView renderView = new TestView(child: root);
+    renderView.layout(new ViewConstraints(width: sky.view.width, height: sky.view.height));
+    expect(coloredBox.size.width, equals(sky.view.width - 20));
+    expect(coloredBox.size.height, equals(sky.view.height - 20));
+  });
+}
