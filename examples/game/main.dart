ef070b2cfb1467583749b39c39da485ed42c9345
Viktor Lidholt <viktorl@google.com>
2015-06-03 15:05:55 -0700

    Sky example game enhancements, adds preloading of images and adds transform modes to SpriteBox
    R=eseidel@chromium.org
    
    Review URL: https://codereview.chromium.org/1149183004

--- examples/game/main.dart
+++ examples/game/main.dart
@@ -6,6 +6,15 @@ import 'package:sky/framework/app.dart';
 AppView app;
 
 void main() {
+  // Load images
+  new ImageMap([
+      "https://raw.githubusercontent.com/slembcke/GalacticGuardian.spritebuilder/GDC/Packages/SpriteBuilder%20Resources.sbpack/resources-auto/BurnTexture.png",
+      "https://raw.githubusercontent.com/slembcke/GalacticGuardian.spritebuilder/GDC/Packages/SpriteBuilder%20Resources.sbpack/Sprites/resources-auto/asteroid_big_002.png",
+    ],
+    allLoaded);
+}
+
+void allLoaded(ImageMap loader) {
   // Create a new app with the sprite box that contains our game world
-  app = new AppView(new SpriteBox(new GameWorld(1024.0, 1024.0),SpriteBoxTransformMode.letterbox));
+  app = new AppView(new SpriteBox(new GameWorld(loader),SpriteBoxTransformMode.letterbox));
 }
