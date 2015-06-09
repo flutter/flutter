aa80a91279d46755be1f89537491ee52e31de5ec
Matt Perry <mpcomplete@chromium.org>
2015-06-09 14:55:52 -0400

    Change the ColorFilter constructor to ColorFilter.Mode, in case we want to add more ColorFilter types.
    R=eseidel@chromium.org
    
    Review URL: https://codereview.chromium.org/1163373007.

---
+++ engine/core/painting/ColorFilter.dart
@@ -0,0 +1,13 @@
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+part of dart.sky;
+
+/// Extends the generated _ColorFilter interface via the PrivateDart attribute.
+class ColorFilter extends _ColorFilter {
+  // This is the only ColorFilter type we need, but use a named constructor so
+  // we can add more in the future.
+  ColorFilter.Mode(Color color, TransferMode transferMode)
+      : super(color, transferMode) {}
+}
