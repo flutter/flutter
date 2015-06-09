44a0af042c5de9f9a304ca384973c35d47e0a6bb
Adam Barth <abarth@chromium.org>
2015-06-09 10:31:39 -0700

    Add a proper material shadow the popup menu
    R=eseidel@chromium.org
    
    Review URL: https://codereview.chromium.org/1172723002.

--- examples/raw/shadowed_box.dart
+++ examples/raw/shadowed_box.dart
@@ -6,17 +6,23 @@ import 'dart:sky';
 import 'package:sky/framework/app.dart';
 import 'package:sky/framework/rendering/box.dart';
 import 'package:sky/framework/rendering/flex.dart';
+import 'package:sky/framework/theme2/shadows.dart';
 
 AppView app;
 
 void main() {
   var coloredBox = new RenderDecoratedBox(
-    decoration: new BoxDecoration(backgroundColor: const Color(0xFFFFFF00))
+    decoration: new BoxDecoration(
+      backgroundColor: const Color(0xFFFAFAFA),
+      boxShadow: Shadow[3])
   );
-  var shadow = const BoxShadow(
-      color: const Color(0xFFEEEEEE), offset: const Size(5.0, 5.0), blur: 5.0);
-  var shadowBox = new RenderShadowedBox(shadow: shadow, child: coloredBox);
-  var paddedBox = new RenderPadding(padding: const EdgeDims.all(30.0),
-      child: shadowBox);
-  app = new AppView(paddedBox);
+  var paddedBox = new RenderPadding(
+    padding: const EdgeDims.all(50.0),
+    child: coloredBox);
+  app = new AppView(new RenderDecoratedBox(
+    decoration: const BoxDecoration(
+      backgroundColor: const Color(0xFFFFFFFF)
+    ),
+    child: paddedBox
+  ));
 }
