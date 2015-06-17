bc68bc57299a687c282a4cbf8291e0868ba09a54
Collin Jackson <jackson@google.com>
2015-06-16 23:37:29 -0700

    Enable render view override for stocks app to enable testing
    This fixes the test
    
    TBR=abarth
    
    Review URL: https://codereview.chromium.org/1192533002.

--- examples/stocks2/lib/stock_app.dart
+++ examples/stocks2/lib/stock_app.dart
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'package:sky/rendering/box.dart';
 import 'package:sky/widgets/basic.dart';
 import 'package:sky/widgets/navigator.dart';
 import 'package:sky/widgets/widget.dart';
@@ -10,6 +11,9 @@ import 'stock_home.dart';
 import 'stock_settings.dart';
 
 class StocksApp extends App {
+
+  StocksApp({ RenderView renderViewOverride }) : super(renderViewOverride: renderViewOverride);
+
   Widget build() {
     return new Navigator(
       routes: [
