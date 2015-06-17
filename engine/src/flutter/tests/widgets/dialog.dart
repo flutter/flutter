f1193d37bd8dde89c0efd97c8af6d77cbd099b86
Adam Barth <abarth@chromium.org>
2015-06-16 17:11:23 -0700

    Add a basic Dialog
    The dialog will need many more features to be complete, but this class is a
    basic skeleton of what we'll need.
    
    R=jackson@google.com, jackson@chromium.org
    
    Review URL: https://codereview.chromium.org/1189893002.

---
+++ engine/src/flutter/tests/widgets/dialog.dart
@@ -0,0 +1,21 @@
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:sky/widgets/basic.dart';
+import 'package:sky/widgets/dialog.dart';
+
+import '../resources/display_list.dart';
+
+main() async {
+  WidgetTester tester = new WidgetTester();
+
+  await tester.test(() {
+    return new Dialog(
+      title: new Text("I am a title"),
+      content: new Text("I am some content")
+    );
+  });
+
+  await tester.endTest();
+}
