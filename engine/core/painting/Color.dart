381c19e075795bdb8920874ebe26140228d00978
Adam Barth <abarth@chromium.org>
2015-06-18 10:32:26 -0700

    If Color constructor is passed a value > 0xFFFFFFFF, Sky crashes
    We now ensure the color is <= 0xFFFFFFFF.
    
    Fixes #245
    
    R=ianh@google.com
    
    Review URL: https://codereview.chromium.org/1179413008.

--- engine/core/painting/Color.dart
+++ engine/core/painting/Color.dart
@@ -5,12 +5,12 @@
 part of dart.sky;
 
 class Color {
-  const Color(this._value);
+  const Color(int value) : _value = (value & 0xFFFFFFFF);
   const Color.fromARGB(int a, int r, int g, int b) :
-    _value = (((a & 0xff) << 24) |
-              ((r & 0xff) << 16) |
-              ((g & 0xff) << 8) |
-              ((b & 0xff) << 0));
+    _value = ((((a & 0xff) << 24) |
+                ((r & 0xff) << 16) |
+                ((g & 0xff) << 8) |
+                ((b & 0xff) << 0)) & 0xFFFFFFFF);
 
   final int _value;
   int get value => _value;
