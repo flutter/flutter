e66ecb97ca053678c22658a49fd021036583d37f
Hixie <github.com@spam.hixie.ch>
2015-06-18 13:40:14 -0700

    More files I accidentally deleted instead of moving. TBR=abarth
    Review URL: https://codereview.chromium.org/1192183002.

---
+++ sdk/lib/theme/shadows.dart
@@ -0,0 +1,60 @@
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'dart:sky' show Color, Size;
+
+import '../painting/box_painter.dart';
+
+const Map<int, List<BoxShadow>> shadows = const {
+  1: const [
+    const BoxShadow(
+      color: const Color(0x1F000000),
+      offset: const Size(0.0, 1.0),
+      blur: 3.0),
+    const BoxShadow(
+      color: const Color(0x3D000000),
+      offset: const Size(0.0, 1.0),
+      blur: 2.0),
+    ],
+  2: const [
+    const BoxShadow(
+      color: const Color(0x29000000),
+      offset: const Size(0.0, 3.0),
+      blur: 6.0),
+    const BoxShadow(
+      color: const Color(0x3B000000),
+      offset: const Size(0.0, 3.0),
+      blur: 6.0),
+  ],
+  3: const [
+    const BoxShadow(
+      color: const Color(0x30000000),
+      offset: const Size(0.0, 10.0),
+      blur: 20.0),
+    const BoxShadow(
+      color: const Color(0x3B000000),
+      offset: const Size(0.0, 6.0),
+      blur: 6.0),
+  ],
+  4: const [
+    const BoxShadow(
+      color: const Color(0x40000000),
+      offset: const Size(0.0, 14.0),
+      blur: 28.0),
+    const BoxShadow(
+      color: const Color(0x38000000),
+      offset: const Size(0.0, 10.0),
+      blur: 10.0),
+  ],
+  5: const [
+    const BoxShadow(
+      color: const Color(0x4E000000),
+      offset: const Size(0.0, 19.0),
+      blur: 28.0),
+    const BoxShadow(
+      color: const Color(0x38000000),
+      offset: const Size(0.0, 15.0),
+      blur: 12.0),
+  ],
+};
---
+++ sdk/lib/theme/typography.dart
@@ -0,0 +1,53 @@
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// See http://www.google.com/design/spec/style/typography.html
+
+import 'dart:sky';
+
+import '../painting/text_style.dart';
+
+// TODO(eseidel): Font weights are supposed to be language relative!
+// These values are for English-like text.
+class _TextTheme {
+  _TextTheme(Color color54, Color color87)
+    : display4 = new TextStyle(fontSize: 112.0, fontWeight: FontWeight.w100, color: color54),
+      display3 = new TextStyle(fontSize:  56.0, fontWeight: FontWeight.w400, color: color54),
+      display2 = new TextStyle(fontSize:  45.0, fontWeight: FontWeight.w400, color: color54),
+      display1 = new TextStyle(fontSize:  34.0, fontWeight: FontWeight.w400, color: color54),
+      headline = new TextStyle(fontSize:  24.0, fontWeight: FontWeight.w400, color: color87),
+      title    = new TextStyle(fontSize:  20.0, fontWeight: FontWeight.w500, color: color87),
+      subhead  = new TextStyle(fontSize:  16.0, fontWeight: FontWeight.w400, color: color87),
+      body2    = new TextStyle(fontSize:  14.0, fontWeight: FontWeight.w500, color: color87),
+      body1    = new TextStyle(fontSize:  14.0, fontWeight: FontWeight.w400, color: color87),
+      caption  = new TextStyle(fontSize:  12.0, fontWeight: FontWeight.w400, color: color54),
+      button   = new TextStyle(fontSize:  14.0, fontWeight: FontWeight.w500, color: color87);
+
+  final TextStyle display4;
+  final TextStyle display3;
+  final TextStyle display2;
+  final TextStyle display1;
+  final TextStyle headline;
+  final TextStyle title;
+  final TextStyle subhead;
+  final TextStyle body2;
+  final TextStyle body1;
+  final TextStyle caption;
+  final TextStyle button;
+}
+
+
+final _TextTheme black = new _TextTheme(
+  const Color(0xFF757575),
+  const Color(0xFF212121)
+);
+
+
+final _TextTheme white = new _TextTheme(
+  const Color(0xFF8A8A8A),
+  const Color(0xFFDEDEDE)
+);
+
+// TODO(abarth): Maybe this should be hard-coded in Scaffold?
+const String typeface = 'font-family: sans-serif';
