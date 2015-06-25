3df24275408739e433d301139c1d98c13f056373
Hixie <github.com@spam.hixie.ch>
2015-06-24 17:01:14 -0700

    Use the baseline information exposed by C++ to pipe baseline data through RenderBox.
    This also fixes the C++ side to give the right baseline information.
    Previously it was giving the baseline distance for the font, but not
    for the actual laid-out text.
    
    I considered also providing a "defaultBaseline" accessor that returns
    the distance for the actual dominant baseline, but it turns out right
    now we never decide the baseline is ideographic. We always use the
    alphabetic baseline. We should probably fix that...
    
    R=eseidel@chromium.org
    
    Review URL: https://codereview.chromium.org/1200233002.

---
+++ examples/rendering/baseline.dart
@@ -0,0 +1,78 @@
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'dart:sky' as sky;
+
+import 'package:sky/painting/text_style.dart';
+import 'package:sky/rendering/block.dart';
+import 'package:sky/rendering/box.dart';
+import 'package:sky/rendering/object.dart';
+import 'package:sky/rendering/paragraph.dart';
+import 'package:sky/rendering/sky_binding.dart';
+
+RenderBox getBox(double lh) {
+  RenderParagraph paragraph = new RenderParagraph(
+    new InlineStyle(
+      new TextStyle(),
+      [
+        new InlineText('test'),
+        new InlineStyle(
+          new TextStyle(
+            color: const Color(0xFF0000A0),
+            fontFamily: 'serif',
+            fontSize: 50.0,
+            height: lh
+          ),
+          [new InlineText('مرحبا Hello')]
+        )
+      ]
+    )
+  );
+  return new RenderPadding(
+    padding: new EdgeDims.all(10.0),
+    child: new RenderConstrainedBox(
+      additionalConstraints: new BoxConstraints.tightFor(height: 200.0),
+      child: new RenderDecoratedBox(
+        decoration: new BoxDecoration(
+          backgroundColor: const Color(0xFFFFFFFF)
+        ),
+        child: new RenderPadding(
+          padding: new EdgeDims.all(10.0),
+          child: new RenderCustomPaint(
+            child: paragraph,
+            callback: (canvas, size) {
+              double baseline = paragraph.getDistanceToBaseline(TextBaseline.alphabetic);
+              double w = paragraph.getMaxIntrinsicWidth(new BoxConstraints.loose(size));
+              double h = paragraph.getMaxIntrinsicHeight(new BoxConstraints.loose(size));
+              Path path = new Path();
+              path.moveTo(0.0, 0.0);
+              path.lineTo(w, 0.0);
+              path.moveTo(0.0, baseline);
+              path.lineTo(w, baseline);
+              path.moveTo(0.0, h);
+              path.lineTo(w, h);
+              Paint paint = new Paint();
+              paint.color = const Color(0xFFFF9000);
+              paint.setStyle(sky.PaintingStyle.stroke);
+              paint.strokeWidth = 3.0;
+              canvas.drawPath(path, paint);  
+            }
+          )
+        )
+      )
+    )
+  );
+}
+
+void main() {
+  RenderBox root = new RenderBlock(children: [
+    new RenderConstrainedBox(
+      additionalConstraints: new BoxConstraints.tightFor(height: 50.0)
+    ),
+    getBox(1.0),
+    getBox(null),
+  ]);
+  var b = new SkyBinding(root: root);
+  // b.onFrame = b.debugDumpRenderTree;
+}
