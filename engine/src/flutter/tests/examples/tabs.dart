c2fc0a2c93758605ef23a0390efb5cd082065ad2
Adam Barth <abarth@chromium.org>
2015-06-26 22:52:40 -0700

    Move examples to //sky/sdk/example
    We're trying to follow the pattern described in https://www.dartlang.org/tools/pub/package-layout.html
    
    TBR=iansf@google.com
    
    Review URL: https://codereview.chromium.org/1210173004.

--- engine/src/flutter/tests/examples/tabs.dart
+++ engine/src/flutter/tests/examples/tabs.dart
@@ -6,7 +6,7 @@ import 'dart:async';
 
 import 'package:sky/widgets/widget.dart';
 
-import '../../sdk/lib/example/widgets/tabs.dart';
+import '../../sdk/example/widgets/tabs.dart';
 import '../resources/display_list.dart';
 
 main() async {
