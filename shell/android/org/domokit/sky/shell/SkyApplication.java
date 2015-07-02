ae892ea9c88e911fe927efaddb47c5467c8c8af4
Adam Barth <abarth@chromium.org>
2015-07-02 11:24:57 -0700

    Switch SkyShell to its own ResourceExtractor
    This CL prepares us to have more interesting resources than base's
    ResourceExtractor understands.
    
    TBR=eseidel@google.com
    
    Review URL: https://codereview.chromium.org/1221153002.

--- shell/android/org/domokit/sky/shell/SkyApplication.java
+++ shell/android/org/domokit/sky/shell/SkyApplication.java
@@ -9,7 +9,6 @@ import android.util.Log;
 
 import org.chromium.base.BaseChromiumApplication;
 import org.chromium.base.PathUtils;
-import org.chromium.base.ResourceExtractor;
 import org.chromium.base.library_loader.LibraryLoader;
 import org.chromium.base.library_loader.LibraryProcessType;
 import org.chromium.base.library_loader.ProcessInitException;
@@ -27,34 +26,46 @@ public class SkyApplication extends BaseChromiumApplication {
 
     private static final String TAG = "SkyApplication";
     private static final String PRIVATE_DATA_DIRECTORY_SUFFIX = "sky_shell";
-    private static final String[] SKY_MANDATORY_PAKS = {
+    private static final String[] SKY_RESOURCES = {
         "icudtl.dat", SNAPSHOT
     };
+
+    private ResourceExtractor mResourceExtractor;
+
+    public ResourceExtractor getResourceExtractor() {
+        return mResourceExtractor;
+    }
 
     @Override
     public void onCreate() {
         super.onCreate();
-        initializeJavaUtils();
-        initializeNative();
-        initializeServiceRegistry();
-        ResourceExtractor.setMandatoryPaksToExtract(SKY_MANDATORY_PAKS);
+        initJavaUtils();
+        initResources();
+        initNative();
+        initServiceRegistry();
     }
 
-    private void initializeJavaUtils() {
+    private void initJavaUtils() {
         PathUtils.setPrivateDataDirectorySuffix(PRIVATE_DATA_DIRECTORY_SUFFIX,
                                                 getApplicationContext());
     }
 
-    private void initializeNative() {
+    private void initResources() {
+        mResourceExtractor = new ResourceExtractor(getApplicationContext());
+        mResourceExtractor.addResources(SKY_RESOURCES);
+        mResourceExtractor.start();
+    }
+
+    private void initNative() {
         try {
             LibraryLoader.get(LibraryProcessType.PROCESS_BROWSER).ensureInitialized();
         } catch (ProcessInitException e) {
-            Log.e(TAG, "sky_shell initialization failed.", e);
+            Log.e(TAG, "Unable to load Sky Engine binary.", e);
             throw new RuntimeException(e);
         }
     }
 
-    private void initializeServiceRegistry() {
+    private void initServiceRegistry() {
         ServiceRegistry.SHARED.register(NetworkService.MANAGER.getName(), new ServiceFactory() {
             public void connectToService(Context context, Core core, MessagePipeHandle pipe) {
                 new NetworkServiceImpl(context, core, pipe);
