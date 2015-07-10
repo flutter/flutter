87f477eb930ae57c59cb407e0f32dae5d2b31c9f
Hixie <github.com@spam.hixie.ch>
2015-07-10 13:39:33 -0700

    Minor style fixes to TaskDescription.
    R=eseidel@chromium.org
    
    Review URL: https://codereview.chromium.org/1233513003 .

--- sdk/lib/widgets/task_description.dart
+++ sdk/lib/widgets/task_description.dart
@@ -2,18 +2,20 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:sky/widgets/widget.dart';
-import 'package:sky/widgets/theme.dart';
-import 'package:sky/mojo/activity.dart' as activity;
+import '../mojo/activity.dart' as activity;
+import 'theme.dart';
+import 'widget.dart';
 
 class TaskDescription extends Component {
-  Widget child;
-  String label;
 
-  TaskDescription({this.label, this.child});
+  TaskDescription({ this.label, this.child });
+
+  final Widget child;
+  final String label;
 
   Widget build() {
     activity.updateTaskDescription(label, Theme.of(this).primaryColor);
     return child;
   }
+
 }
