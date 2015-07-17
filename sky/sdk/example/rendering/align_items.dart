d7d625b2bfa63db12b5e8a78040cac4d518c769b
Collin Jackson <jackson@google.com>
2015-07-17 14:29:05 -0700

    Update example

--- sky/sdk/example/rendering/align_items.dart
+++ sky/sdk/example/rendering/align_items.dart
@@ -16,38 +16,33 @@ import 'solid_color_box.dart';
 void main() {
   var table = new RenderFlex(direction: FlexDirection.vertical);
 
-  void addRow(FlexAlignItems align) {
+  for(FlexAlignItems alignItems in FlexAlignItems.values) {
     TextStyle style = const TextStyle(color: const Color(0xFF000000));
-    RenderParagraph paragraph = new RenderParagraph(new InlineStyle(style, [new InlineText("${align}")]));
+    RenderParagraph paragraph = new RenderParagraph(new InlineStyle(style, [new InlineText("${alignItems}")]));
     table.add(new RenderPadding(child: paragraph, padding: new EdgeDims.only(top: 20.0)));
-    var row = new RenderFlex(direction: FlexDirection.horizontal);
+    var row = new RenderFlex(alignItems: alignItems);
 
     style = new TextStyle(fontSize: 15.0, color: const Color(0xFF000000));
     row.add(new RenderDecoratedBox(
-      decoration: new BoxDecoration(backgroundColor: const Color(0xFFFFCCCC)),
+      decoration: new BoxDecoration(backgroundColor: const Color(0x7FFFCCCC)),
       child: new RenderParagraph(new InlineStyle(style, [new InlineText('foo foo foo')]))
     ));
     style = new TextStyle(fontSize: 10.0, color: const Color(0xFF000000));
     row.add(new RenderDecoratedBox(
-      decoration: new BoxDecoration(backgroundColor: const Color(0xFFCCFFCC)),
+      decoration: new BoxDecoration(backgroundColor: const Color(0x7FCCFFCC)),
       child: new RenderParagraph(new InlineStyle(style, [new InlineText('foo foo foo')]))
     ));
+    var subrow = new RenderFlex(alignItems: alignItems);
     style = new TextStyle(fontSize: 25.0, color: const Color(0xFF000000));
-    row.add(new RenderDecoratedBox(
-      decoration: new BoxDecoration(backgroundColor: const Color(0xFFCCCCFF)),
+    subrow.add(new RenderDecoratedBox(
+      decoration: new BoxDecoration(backgroundColor: const Color(0x7FCCCCFF)),
       child: new RenderParagraph(new InlineStyle(style, [new InlineText('foo foo foo foo')]))
     ));
-    row.add(new RenderSolidColorBox(const Color(0xFFCCFFFF), desiredSize: new Size(30.0, 40.0)));
-    row.alignItems = align;
+    subrow.add(new RenderSolidColorBox(const Color(0x7FCCFFFF), desiredSize: new Size(30.0, 40.0)));
+    row.add(subrow);
     table.add(row);
     row.parentData.flex = 1;
   }
-
-  addRow(FlexAlignItems.start);
-  addRow(FlexAlignItems.end);
-  addRow(FlexAlignItems.center);
-  addRow(FlexAlignItems.stretch);
-  addRow(FlexAlignItems.baseline);
 
   RenderDecoratedBox root = new RenderDecoratedBox(
     decoration: new BoxDecoration(backgroundColor: const Color(0xFFFFFFFF)),
