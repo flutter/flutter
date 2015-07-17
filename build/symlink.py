d96729c38a7abe7dd96284ef4c9692aa8acd6e69
Hans Muller <hansmuller@chromium.org>
2015-07-17 15:27:29 -0700

    updated

--- build/symlink.py
+++ build/symlink.py
@@ -2,26 +2,19 @@
 # Copyright (c) 2013 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
-
 """Make a symlink and optionally touch a file (to handle dependencies)."""
-
-
 import errno
 import optparse
 import os.path
 import shutil
 import sys
-
-
 def Main(argv):
   parser = optparse.OptionParser()
   parser.add_option('-f', '--force', action='store_true')
   parser.add_option('--touch')
-
   options, args = parser.parse_args(argv[1:])
   if len(args) < 2:
     parser.error('at least two arguments required.')
-
   target = args[-1]
   sources = args[:-1]
   for s in sources:
@@ -39,12 +32,8 @@ def Main(argv):
         os.symlink(s, t)
       else:
         raise
-
-
   if options.touch:
     with open(options.touch, 'w') as f:
       pass
-
-
 if __name__ == '__main__':
   sys.exit(Main(sys.argv))
