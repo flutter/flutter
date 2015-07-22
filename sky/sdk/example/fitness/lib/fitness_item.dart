ea28a63d9a88fa66c981ce07958320fafc5caad1
Collin Jackson <jackson@google.com>
2015-07-21 17:11:39 -0700

    First pass at adding meal tracking to fitness app

---
+++ sky/sdk/example/fitness/lib/fitness_item.dart
@@ -0,0 +1,46 @@
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:sky/widgets/basic.dart';
+import 'package:sky/widgets/card.dart';
+import 'package:sky/widgets/dismissable.dart';
+
+typedef void FitnessItemHandler(FitnessItem item);
+
+const double kFitnessItemHeight = 79.0;
+
+class FitnessItem {
+  FitnessItem({ this.when }) {
+    assert(when != null);
+  }
+  final DateTime when;
+
+  // TODO(jackson): Internationalize
+  String get displayDate => "${when.year.toString()}-${when.month.toString().padLeft(2,'0')}-${when.day.toString().padLeft(2,'0')}";
+}
+
+abstract class FitnessItemRow extends Component {
+
+  FitnessItemRow({ FitnessItem item, this.onDismissed })
+   : this.item = item,
+     super(key: item.when.toString());
+
+  final FitnessItem item;
+  final FitnessItemHandler onDismissed;
+
+  Widget buildContent();
+
+  Widget build() {
+    return new Dismissable(
+      onDismissed: () => onDismissed(item),
+      child: new Card(
+        child: new Container(
+          height: kFitnessItemHeight,
+          padding: const EdgeDims.all(8.0),
+          child: buildContent()
+        )
+      )
+    );
+  }
+}
--- sky/sdk/example/fitness/lib/fitness_types.dart
+++ sky/sdk/example/fitness/lib/fitness_types.dart
@@ -2,5 +2,5 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-enum FitnessMode { measure, run }
+enum FitnessMode { feed, chart }
 enum BackupMode { enabled, disabled }
