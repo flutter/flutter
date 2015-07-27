be52cb8690798d8a66728e506fc318b499dcf76e
Adam Barth <abarth@chromium.org>
2015-07-27 16:56:18 -0700

    Improve init.dart not to assume `dart` is in your PATH
    Instead, use Platform.executable, which is the path to the current `dart`
    executable.

--- packages/flutter/bin/init.dart
+++ packages/flutter/bin/init.dart
@@ -5,13 +5,12 @@
 /*
 * This script should be invoked via 'pub run' after 'pub get':
 * $ pub run sky:init
-* NOTE: The 'dart' executable must be on your $PATH for this script to work.
 */
 
 import 'dart:io';
 
 main(List<String> arguments) {
-  ProcessResult result = Process.runSync('dart', ['-p', 'packages', 'packages/mojom/generate.dart']);
+  ProcessResult result = Process.runSync(Platform.executable, ['-p', 'packages', 'packages/mojom/generate.dart']);
   stdout.write(result.stdout);
   stderr.write(result.stderr);
 }
