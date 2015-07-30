de6aa23a9318656d97c770e968dcb1cf044454b1
John McCutchan <johnmccutchan@google.com>
2015-07-30 10:57:47 -0700

    Fix patch application

--- engine/src/flutter/sky/tools/roll/patch.py
+++ engine/src/flutter/sky/tools/roll/patch.py
@@ -7,14 +7,14 @@ import os
 import subprocess
 import utils
 
-def patch_and_filter(dest_dir):
+def patch_and_filter(dest_dir, relative_patches_dir):
   os.chdir(dest_dir)
 
   utils.filter_file("build/landmines.py",
       lambda line: not "gyp_environment" in line)
   utils.commit("filter gyp_environment out of build/landmines.py")
 
-  patch(dest_dir)
+  patch(dest_dir, relative_patches_dir)
 
 
 def patch(dest_dir, relative_patches_dir=os.curdir):
