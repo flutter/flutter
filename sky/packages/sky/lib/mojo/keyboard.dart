3346808587df1314308f0e190aebdea78600a7ef
John McCutchan <johnmccutchan@google.com>
2015-07-30 13:58:27 -0700

    Fix imports

--- sky/packages/sky/lib/mojo/keyboard.dart
+++ sky/packages/sky/lib/mojo/keyboard.dart
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:mojom/keyboard/keyboard.mojom.dart';
+import 'package:mojo_services/keyboard/keyboard.mojom.dart';
 import 'package:sky/mojo/shell.dart' as shell;
 
 class _KeyboardConnection {
