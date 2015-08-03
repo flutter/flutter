78379cf4aa4a9018cb16d780781145f6138c1ee6
Adam Barth <abarth@chromium.org>
2015-08-03 15:09:49 -0700

    Add a widgets.dart uber library
    Now you can import 'package:sky/widgets.dart' to get all the widgets.

--- sky/packages/sky/example/hello_world/lib/main.dart
+++ sky/packages/sky/example/hello_world/lib/main.dart
@@ -2,9 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-library hello_world;
-
-import 'package:sky/widgets/basic.dart';
+import 'package:sky/widgets.dart';
 
 class HelloWorldApp extends App {
   Widget build() {
