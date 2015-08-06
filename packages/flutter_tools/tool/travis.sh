0257cbd12ba2f8a2a028899e45c889707e3b391c
Devon Carew <devoncarew@gmail.com>
2015-08-06 14:25:55 -0700

    build sky_tools using travis

---
+++ packages/flutter_tools/tool/travis.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Copyright 2015 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# Fast fail the script on failures.
+set -e
+
+# Verify that the libraries are error free.
+dartanalyzer --fatal-warnings \
+  bin/build_sky_apk.dart \
+  lib/sky_server.dart
