1cc0d732118a9db748c77c7dba2ad64421fe87c4
Adam Barth <abarth@chromium.org>
2015-08-11 10:13:34 -0700

    Add support for immersive mode on Android
    Fixes #512

--- sky/services/activity/src/org/domokit/activity/ActivityImpl.java
+++ sky/services/activity/src/org/domokit/activity/ActivityImpl.java
@@ -6,14 +6,16 @@ package org.domokit.activity;
 
 import android.content.ActivityNotFoundException;
 import android.net.Uri;
-import android.util.Log;
 import android.os.Build;
+import android.util.Log;
+import android.view.View;
 
 import org.chromium.mojo.system.MojoException;
 import org.chromium.mojom.activity.Activity;
 import org.chromium.mojom.activity.ComponentName;
 import org.chromium.mojom.activity.Intent;
 import org.chromium.mojom.activity.StringExtra;
+import org.chromium.mojom.activity.SystemUiVisibility;
 import org.chromium.mojom.activity.TaskDescription;
 
 /**
@@ -94,6 +96,28 @@ public class ActivityImpl implements Activity {
                     description.primaryColor
                 )
         );
+    }
+
+    @Override
+    public void setSystemUiVisibility(int visibility) {
+      if (sCurrentActivity == null) {
+          return;
+      }
+      int flags = View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+                  View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN;
+
+      if (visibility >= SystemUiVisibility.FULLSCREEN) {
+          flags |= View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+                   View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+                   View.SYSTEM_UI_FLAG_FULLSCREEN;
+      }
+
+      if (visibility >= SystemUiVisibility.IMMERSIVE) {
+          flags |= View.SYSTEM_UI_FLAG_IMMERSIVE |
+                   View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+      }
+
+      sCurrentActivity.getWindow().getDecorView().setSystemUiVisibility(flags);
     }
 
     @Override
