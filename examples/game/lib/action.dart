9cb2cdacba17a23aef6eb503927acf86e4820d2b
Viktor Lidholt <viktorl@google.com>
2015-08-17 17:07:55 -0700

    Updates demo game to use curves from the animation package

--- examples/game/lib/action.dart
+++ examples/game/lib/action.dart
@@ -553,28 +553,3 @@ class _ColorDiff {
 
   _ColorDiff(this.alpha, this.red, this.green, this.blue);
 }
-
-double _bounce(double t)
-{
-  if (t < 1.0 / 2.75) {
-    return 7.5625 * t * t;
-  } else if (t < 2 / 2.75) {
-    t -= 1.5 / 2.75;
-    return 7.5625 * t * t + 0.75;
-  } else if (t < 2.5 / 2.75) {
-    t -= 2.25 / 2.75;
-    return 7.5625 * t * t + 0.9375;
-  }
-  t -= 2.625 / 2.75;
-  return 7.5625 * t * t + 0.984375;
-}
-
-class BounceOutCurve implements Curve {
-  const BounceOutCurve();
-
-  double transform(double t) {
-    return _bounce(t);
-  }
-}
-
-const BounceOutCurve bounceOut = const BounceOutCurve();
