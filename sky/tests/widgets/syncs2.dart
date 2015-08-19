b0a1649e48f357a9dad80b4bfae2cae9b1dff52f
Hixie <github.com@spam.hixie.ch>
2015-08-19 12:16:41 -0700

    Rewrite the MultiChildRenderObjectWrapper syncing algorithm.
    This also changes the way we insert nodes into a
    MultiChildRenderObjectWrapper's renderObject, which fixes issue #626.
    Now, instead of the slot being a renderObject, it's the Widget that
    currently uses that renderObject. That way when the Widget changes
    which renderObject to use, the siblings of that Widget in the same
    child list don't have to be notified of the change.
    
    I tested performance of the new algorithm vs the old algorithm using
    the stocks demo at idle and the stocks demo scrolling steadily. The
    data suggests the algorithms are roughly equivalent in performance.

---
+++ sky/tests/widgets/syncs2.dart
@@ -0,0 +1,57 @@
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:sky/widgets.dart';
+
+import '../resources/display_list.dart';
+
+// see issue 626
+
+class ProblemComponent extends StatefulComponent {
+
+  void syncFields(ProblemComponent source) { }
+
+  bool _flag = false;
+
+  void flip() {
+    setState(() {
+      _flag = true;
+    });
+  }
+
+  Widget build() {
+    if (_flag)
+      return new Padding(padding: const EdgeDims.all(10.0));
+    return new SizedBox(width: 100.0, height: 100.0);
+  }
+}
+
+ProblemComponent a;
+ProblemComponent b;
+
+class TestApp extends App {
+  Widget build() {
+    return new Flex([
+        a = new ProblemComponent(),
+        b = new ProblemComponent()
+      ],
+    direction: FlexDirection.vertical);
+  }
+}
+
+main() async {
+  try {
+    TestRenderView renderViewOverride = new TestRenderView();
+    TestApp app = new TestApp();
+    runApp(app, renderViewOverride: renderViewOverride);
+    await renderViewOverride.checkFrame();
+    b.flip();
+    await renderViewOverride.checkFrame();
+    a.flip();
+    await renderViewOverride.checkFrame();
+    renderViewOverride.endTest();
+  } catch (e, s) {
+    print("Exception: $e\nStack:\n$s\n");
+  }
+}
