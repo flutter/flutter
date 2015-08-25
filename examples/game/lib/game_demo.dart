cca7006d95299c1ea548edb87c275367812d0ee9
Viktor Lidholt <viktorl@google.com>
2015-08-25 09:28:33 -0700

    Removes custom joystick code and adds a new moving enemy to demo game

--- examples/game/lib/game_demo.dart
+++ examples/game/lib/game_demo.dart
@@ -6,6 +6,5 @@ import 'sprites.dart';
 
 import 'package:sky/rendering/object.dart';
 import 'package:sky/widgets/framework.dart';
-import 'package:sky/animation/curves.dart';
 
 part 'game_demo_node.dart';
