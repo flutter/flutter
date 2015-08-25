f16249adcd72b635ed7de35a7bd30b199525767c
Adam Barth <abarth@chromium.org>
2015-08-25 16:13:33 -0700

    Add a test for #833
    This test just exercises Align to make sure it can sync.

---
+++ sky/unit/test/widget/align_test.dart
@@ -0,0 +1,27 @@
+import 'package:sky/widgets.dart';
+import 'package:test/test.dart';
+
+import 'build_utils.dart';
+
+void main() {
+  test('Global key mutate during iteration', () {
+    WidgetTester tester = new WidgetTester();
+
+    tester.pumpFrame(() {
+      return new Align(
+        child: new Container(),
+        horizontal: 0.75,
+        vertical: 0.75
+      );
+    });
+
+    tester.pumpFrame(() {
+      return new Align(
+        child: new Container(),
+        horizontal: 0.5,
+        vertical: 0.5
+      );
+    });
+
+  });
+}
