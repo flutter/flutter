38be8f6e8d8c9f19360dec1c6812dee4ca62933d
Andrew Wilson <anwilson@chromium.org>
2015-08-31 14:21:22 -0700

    Persist immersiveness.

--- sky/services/activity/src/org/domokit/activity/ActivityImpl.java
+++ sky/services/activity/src/org/domokit/activity/ActivityImpl.java
@@ -28,12 +28,39 @@ import org.chromium.mojom.activity.UserFeedback;
 public class ActivityImpl implements Activity {
     private static final String TAG = "ActivityImpl";
     private static android.app.Activity sCurrentActivity;
+    private static int sVisibility;
 
     public ActivityImpl() {
     }
 
     public static void setCurrentActivity(android.app.Activity activity) {
         sCurrentActivity = activity;
+    }
+
+    public static void onResumeActivity(android.app.Activity activity) {
+        updateSystemUiVisibility(activity);
+    }
+
+    private static void updateSystemUiVisibility(android.app.Activity activity) {
+        if (activity == null) {
+            return;
+        }
+
+        int flags = View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN;
+
+        if (sVisibility >= SystemUiVisibility.FULLSCREEN) {
+            flags |= View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+                     View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+                     View.SYSTEM_UI_FLAG_FULLSCREEN;
+        }
+
+        if (sVisibility >= SystemUiVisibility.IMMERSIVE) {
+            flags |= View.SYSTEM_UI_FLAG_IMMERSIVE |
+                     View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+        }
+
+        sCurrentActivity.getWindow().getDecorView().setSystemUiVisibility(flags);
     }
 
     @Override
@@ -110,24 +137,8 @@ public class ActivityImpl implements Activity {
 
     @Override
     public void setSystemUiVisibility(int visibility) {
-        if (sCurrentActivity == null) {
-            return;
-        }
-        int flags = View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
-                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN;
-
-        if (visibility >= SystemUiVisibility.FULLSCREEN) {
-            flags |= View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
-                     View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
-                     View.SYSTEM_UI_FLAG_FULLSCREEN;
-        }
-
-        if (visibility >= SystemUiVisibility.IMMERSIVE) {
-            flags |= View.SYSTEM_UI_FLAG_IMMERSIVE |
-                     View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
-        }
-
-        sCurrentActivity.getWindow().getDecorView().setSystemUiVisibility(flags);
+        sVisibility = visibility;
+        updateSystemUiVisibility(sCurrentActivity);
     }
 
     @Override
