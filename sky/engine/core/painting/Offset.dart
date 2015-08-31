96e203329c6b2a32c5e0195b37a2057ffb4500de
Adam Barth <abarth@chromium.org>
2015-08-30 18:36:06 -0700

    Move lerp for the engine types into dart:sky
    This patch just adds them to dart:sky. A future patch will actually use them in
    the framework once we've published an updated sky_engine package. Also, add
    dartdoc to the classes touched in this patch.

--- sky/engine/core/painting/Offset.dart
+++ sky/engine/core/painting/Offset.dart
@@ -4,21 +4,31 @@
 
 part of dart.sky;
 
-/// Holds a 2D floating-point offset.
-/// Think of this as a vector from an unspecified point
+/// An immutable 2D floating-point offset
+///
+/// An Offset represents a vector from an unspecified point
 class Offset extends OffsetBase {
   const Offset(dx, dy) : super(dx, dy);
-  Offset.copy(Offset source) : super(source.dx, source.dy);
 
+  /// The x component of the offset
   double get dx => _dx;
+
+  /// The y component of the offset
   double get dy => _dy;
 
+  /// The magnitude of the offset
   double get distance => math.sqrt(_dx * _dx + _dy * _dy);
 
+  /// An offset with zero magnitude
   static const Offset zero = const Offset(0.0, 0.0);
+
+  /// An offset with infinite x and y components
   static const Offset infinite = const Offset(double.INFINITY, double.INFINITY);
 
+  /// Returns a new offset with the x component scaled by scaleX and the y component scaled by scaleY
   Offset scale(double scaleX, double scaleY) => new Offset(dx * scaleX, dy * scaleY);
+
+  /// Returns a new offset with translateX added to the x component and translateY added to the y component
   Offset translate(double translateX, double translateY) => new Offset(dx + translateX, dy + translateY);
 
   Offset operator -() => new Offset(-dx, -dy);
@@ -28,10 +38,25 @@ class Offset extends OffsetBase {
   Offset operator /(double operand) => new Offset(dx / operand, dy / operand);
   Offset operator ~/(double operand) => new Offset((dx ~/ operand).toDouble(), (dy ~/ operand).toDouble());
   Offset operator %(double operand) => new Offset(dx % operand, dy % operand);
+
+  /// Returns a rect of the given size that starts at (0, 0) plus this offset
   Rect operator &(Size other) => new Rect.fromLTWH(dx, dy, other.width, other.height);
 
-  // does the equivalent of "return new Point(0,0) + this"
+  /// Returns the point at (0, 0) plus this offset.
   Point toPoint() => new Point(this.dx, this.dy);
+
+  /// Linearly interpolate between two offsets
+  ///
+  /// If either offset is null, this function interpolates from [Offset.zero].
+  static Offset lerp(Offset a, Offset b, double t) {
+    if (a == null && b == null)
+      return null;
+    if (a == null)
+      return b * t;
+    if (b == null)
+      return a * (1.0 - t);
+    return new Offset(lerpNum(a.dx, b.dx, t), lerpNum(a.dy, b.dy, t));
+  }
 
   String toString() => "Offset($dx, $dy)";
 }
--- sky/engine/core/painting/Rect.dart
+++ sky/engine/core/painting/Rect.dart
@@ -4,10 +4,11 @@
 
 part of dart.sky;
 
-/// Holds 4 floating-point coordinates for a rectangle.
+/// An immutable 2D, axis-aligned, floating-point rectangle
 class Rect {
   Rect();
 
+  /// Construct a rectangle from left, top, right, and bottom edges
   Rect.fromLTRB(double left, double top, double right, double bottom) {
     _value
       ..[0] = left
@@ -16,6 +17,7 @@ class Rect {
       ..[3] = bottom;
   }
 
+  /// Construct a rectangle from left, top edges and a width and height
   Rect.fromLTWH(double left, double top, double width, double height) {
     _value
       ..[0] = left
@@ -25,20 +27,38 @@ class Rect {
   }
 
   final Float32List _value = new Float32List(4);
+
+  /// The offset of the left edge of this rectangle from the x axis
   double get left => _value[0];
+
+  /// The offset of the top edge of this rectangle from the y axis
   double get top => _value[1];
+
+  /// The offset of the right edge of this rectangle from the x axis
   double get right => _value[2];
+
+  /// The offset of the bottom edge of this rectangle from the y axis
   double get bottom => _value[3];
 
+  /// A rectangle with left, top, right, and bottom edges all at zero
+  static final Rect zero = new Rect();
+
+  /// Returns a new rectangle translated by the given offset
   Rect shift(Offset offset) {
     return new Rect.fromLTRB(left + offset.dx, top + offset.dy, right + offset.dx, bottom + offset.dy);
   }
+
+  /// Returns a new rectangle with edges moved outwards by the given delta
   Rect inflate(double delta) {
     return new Rect.fromLTRB(left - delta, top - delta, right + delta, bottom + delta);
   }
+
+  /// Returns a new rectangle with edges moved inwards by the given delta
   Rect deflate(double delta) {
     return inflate(-delta);
   }
+
+  /// Returns a new rectangle that is the intersection of the given rectangle and this rectangle
   Rect intersect(Rect other) {
     return new Rect.fromLTRB(
       math.max(left, other.left),
@@ -47,27 +67,62 @@ class Rect {
       math.max(bottom, other.bottom));
   }
 
+  /// The distance between the left and right edges of this rectangle
   double get width => right - left;
+
+  /// The distance between the top and bottom edges of this rectangle
   double get height => bottom - top;
 
+  /// The distance between upper-left corner and the lower-right corner of this rectangle
   Size get size => new Size(width, height);
 
+  /// The lesser of the width and the height of this rectangle
   double get shortestSide {
     double w = width.abs();
     double h = height.abs();
     return w < h ? w : h;
   }
 
+  /// The point halfway between the left and right and the top and bottom edges of this rectangle
   Point get center => new Point(left + width / 2.0, top + height / 2.0);
+
+  /// The point at the intersection of the top and left edges of this rectangle
   Point get topLeft => new Point(left, top);
+
+  /// The point at the intersection of the top and right edges of this rectangle
   Point get topRight => new Point(right, top);
+
+  /// The point at the intersection of the bottom and left edges of this rectangle
   Point get bottomLeft => new Point(left, bottom);
+
+  /// The point at the intersection of the bottom and right edges of this rectangle
   Point get bottomRight => new Point(right, bottom);
 
-  // Rects are inclusive of the top and left edges but exclusive of the bottom
-  // right edges.
+  /// Whether the given point lies between the left and right and the top and bottom edges of this rectangle
+  ///
+  /// Rectangles include their top and left edges but exclude their bottom and right edges.
   bool contains(Point point) {
     return point.x >= left && point.x < right && point.y >= top && point.y < bottom;
+  }
+
+  /// Linearly interpolate between two rectangles
+  ///
+  /// If either rect is null, this function interpolates from [Rect.zero].
+  static Rect lerp(Rect a, Rect b, double t) {
+    if (a == null && b == null)
+      return null;
+    if (a == null)
+      return new Rect.fromLTRB(b.left * t, b.top * t, b.right * t, b.bottom * t);
+    if (b == null) {
+      double k = 1.0 - t;
+      return new Rect.fromLTRB(b.left * k, b.top * k, b.right * k, b.bottom * k);
+    }
+    return new Rect.fromLTRB(
+      lerpNum(a.left, b.left, t),
+      lerpNum(a.top, b.top, t),
+      lerpNum(a.right, b.right, t),
+      lerpNum(a.bottom, b.bottom, t)
+    );
   }
 
   bool operator ==(other) {
