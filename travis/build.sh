7be75752820236961b6e5bacf5e0f348fcb937e6
Adam Barth <abarth@chromium.org>
2015-09-01 21:24:56 -0700

    Make travis green

--- travis/build.sh
+++ travis/build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -ex
 
+# Smoke test sky_shell
+./out/sky_shell --help
+
 pushd sky/unit
 SKY_SHELL=../../out/sky_shell ../../third_party/dart-sdk/dart-sdk/bin/pub run sky_tools:sky_test -j 1
 popd
