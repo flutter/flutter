8507e8ab4016e0c67ddbe13c808d68bed9a61971
Adam Barth <abarth@chromium.org>
2015-09-01 20:39:51 -0700

    Run unit test on Travis with a prebuilt sky_shell

--- travis/build.sh
+++ travis/build.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -ex
 
-./sky/tools/gn --release
-ninja -j 4 -C out/Release
-./sky/tools/run_tests --release -j 1
+pushd sky/unit
+SKY_SHELL=../../out/sky_shell ../../third_party/dart-sdk/dart-sdk/bin/pub run sky_tools:sky_test -j 1
+popd
+
+pushd sky/packages/sky
+../../../third_party/dart-sdk/dart-sdk/bin/dartanalyzer lib/widgets.dart
+popd
--- travis/setup.sh
+++ travis/setup.sh
@@ -1,29 +1,14 @@
 #!/bin/bash
 set -ex
 
-# Create an src/ directory to work with.
-# TODO(alhaad): This is a temporary hack. Find a better way to do this.
-mkdir ../src
-mv * ../src
-mv .??* ../src
-mv ../src .
+./tools/dart/update.py
 
-# Get depot_tools.
-git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
-export PATH="$(pwd)/depot_tools:${PATH}"
+pushd sky/unit
+../../third_party/dart-sdk/dart-sdk/bin/pub get
+popd
 
-# Get dependencies.
-sudo apt-get install libdbus-1-dev libgconf2-dev bison gperf wdiff python-openssl libxtst-dev
-sudo easy_install pip
-sudo pip install requests
+pushd sky/packages/sky
+../../third_party/dart-sdk/dart-sdk/bin/pub get
+popd
 
-# Get gsutil
-rm -f gsutil.tar.gz
-wget https://storage.googleapis.com/pub/gsutil.tar.gz
-tar xzf gsutil.tar.gz
-
-# Setup .gclient file.
-cp src/travis/gclient .gclient
-
-cd src
-gclient sync
+./sky/tools/download_sky_shell.py sky/unit/packages/sky_engine/REVISION out
