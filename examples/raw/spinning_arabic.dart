d341bedd64da9f3834f093d0e5a7611cceb648ee
Adam Barth <abarth@chromium.org>
2015-09-03 09:39:45 -0700

    Actually make the raw examples work again
    In my previous patch, I forgot to fill in the other diagonal entries in the
    device transform matrix.

--- examples/raw/spinning_arabic.dart
+++ examples/raw/spinning_arabic.dart
@@ -33,7 +33,9 @@ sky.Scene composite(sky.Picture picture, sky.Rect paintBounds) {
   sky.Rect sceneBounds = new sky.Rect.fromLTWH(0.0, 0.0, sky.view.width * devicePixelRatio, sky.view.height * devicePixelRatio);
   Float32List deviceTransform = new Float32List(16)
     ..[0] = devicePixelRatio
-    ..[5] = devicePixelRatio;
+    ..[5] = devicePixelRatio
+    ..[10] = 1.0
+    ..[15] = 1.0;
   sky.SceneBuilder sceneBuilder = new sky.SceneBuilder(sceneBounds)
     ..pushTransform(deviceTransform)
     ..addPicture(sky.Offset.zero, picture, paintBounds)
@@ -49,6 +51,7 @@ void beginFrame(double timeStamp) {
   sky.Picture picture = paint(paintBounds, delta);
   sky.Scene scene = composite(picture, paintBounds);
   sky.view.scene = scene;
+  sky.view.scheduleFrame();
 }
 
 void main() {
@@ -59,6 +62,7 @@ void main() {
   block.style['display'] = 'paragraph';
   block.style['direction'] = 'rtl';
   block.style['unicode-bidi'] = 'plaintext';
+  block.style['color'] = 'black';
   block.appendChild(arabic);
   block.appendChild(more);
 
--- examples/raw/spinning_square.dart
+++ examples/raw/spinning_square.dart
@@ -29,7 +29,9 @@ void beginFrame(double timeStamp) {
   sky.Rect sceneBounds = new sky.Rect.fromLTWH(0.0, 0.0, sky.view.width * devicePixelRatio, sky.view.height * devicePixelRatio);
   Float32List deviceTransform = new Float32List(16)
     ..[0] = devicePixelRatio
-    ..[5] = devicePixelRatio;
+    ..[5] = devicePixelRatio
+    ..[10] = 1.0
+    ..[15] = 1.0;
   sky.SceneBuilder sceneBuilder = new sky.SceneBuilder(sceneBounds)
     ..pushTransform(deviceTransform)
     ..addPicture(sky.Offset.zero, picture, paintBounds)
@@ -37,6 +39,7 @@ void beginFrame(double timeStamp) {
   sky.view.scene = sceneBuilder.build();
 
   sky.tracing.end('beginFrame');
+  sky.view.scheduleFrame();
 }
 
 void main() {
