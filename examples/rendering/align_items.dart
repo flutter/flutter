c78b7d05afaf0813cfed5d441a12061c7e44d3c3
Adam Barth <abarth@chromium.org>
2015-09-09 16:49:36 -0700

    Remove redundant enum declarations from text_style.dart
    These are now declared in dart:sky as part of ParagraphBuilder.

--- examples/rendering/align_items.dart
+++ examples/rendering/align_items.dart
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'dart:sky';
-
 import 'package:sky/rendering.dart';
 
 import 'solid_color_box.dart';
