da2ee9129a41eb61531500302e9c1f8e470e9172
Adam Barth <abarth@chromium.org>
2015-09-23 11:26:04 -0700

    Port some widgets to fn3

---
+++ packages/flutter/lib/src/fn3/button_state.dart
@@ -0,0 +1,41 @@
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:sky/src/fn3/basic.dart';
+import 'package:sky/src/fn3/framework.dart';
+
+abstract class ButtonState<T extends StatefulComponent> extends ComponentState<T> {
+  ButtonState(T config) : super(config);
+
+  bool highlight;
+
+  void _handlePointerDown(_) {
+    setState(() {
+      highlight = true;
+    });
+  }
+
+  void _handlePointerUp(_) {
+    setState(() {
+      highlight = false;
+    });
+  }
+
+  void _handlePointerCancel(_) {
+    setState(() {
+      highlight = false;
+    });
+  }
+
+  Widget build(BuildContext context) {
+    return new Listener(
+      onPointerDown: _handlePointerDown,
+      onPointerUp: _handlePointerUp,
+      onPointerCancel: _handlePointerCancel,
+      child: buildContent()
+    );
+  }
+
+  Widget buildContent();
+}
