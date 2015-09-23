638c62d5f418fb08e4cf35260ba55afaeca9e0ac
Matt Perry <mpcomplete@chromium.org>
2015-09-23 14:09:38 -0400

    Replace C++ UpdateTask with an empty dart version, loaded as a snapshot.
    This adds a new placeholder dart package updater in sky/packages. This is built
    into a snapshot and compiled into the Sky engine binary using the same
    mechanism as the Dart isolate.
    
    I also added a SkyHeadless class, similar to SkyView, used for running Dart
    code without a view.

---
+++ engine/src/flutter/sky/packages/updater/lib/main.dart
@@ -0,0 +1,14 @@
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+class UpdateTask {
+  UpdateTask() {}
+
+  String toString() => "UpdateTask()";
+}
+
+void main() {
+  var x = new UpdateTask();
+  print("Success: $x");
+}
