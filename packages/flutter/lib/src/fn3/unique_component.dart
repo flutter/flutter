e1b8f0b65925e705f3bbebdd7e2d091aa6958c6b
Adam Barth <abarth@chromium.org>
2015-09-25 15:27:15 -0700

    Add UniqueComponent helper for creating components with unique states

---
+++ packages/flutter/lib/src/fn3/unique_component.dart
@@ -0,0 +1,16 @@
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:sky/src/fn3.dart';
+
+abstract class UniqueComponent<T extends State> extends StatefulComponent {
+ UniqueComponent({ GlobalKey key }) : super(key: key ?? new GlobalKey());
+
+ T createState();
+
+ T get currentState {
+   GlobalKey globalKey = key;
+   return globalKey.currentState;
+ }
+}
