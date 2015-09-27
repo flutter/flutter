e7f726c240544bee00d53d9287a818a285652f28
Devon Carew <devoncarew@gmail.com>
2015-09-26 23:19:25 -0700

    customize the generated dartdoc like flutter.io

--- packages/flutter/lib/services.dart
+++ packages/flutter/lib/services.dart
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-/// System services exposed to Flutter apps
+/// System services exposed to Flutter apps.
 ///
 /// For example, this library includes [fetch], which fetches data from the
 /// network.
