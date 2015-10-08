8547676089a9be511a816e7e13ff60a90f1bc92d
Adam Barth <abarth@chromium.org>
2015-10-08 10:13:29 -0700

    SnackBar throws exception on creation
    It was trying to grab the route's performance before it was created. Now we
    create the performance eagerly again.

---
+++ sky/unit/test/widget/snack_bar_test.dart
@@ -0,0 +1,40 @@
+import 'package:sky/widgets.dart';
+import 'package:test/test.dart';
+
+import 'widget_tester.dart';
+
+void main() {
+  test('SnackBar control test', () {
+    testWidgets((WidgetTester tester) {
+      String helloSnackBar = 'Hello SnackBar';
+      GlobalKey<PlaceholderState> placeholderKey = new GlobalKey<PlaceholderState>();
+      Key tapTarget = new Key('tap-target');
+
+      tester.pumpWidget(new App(
+        routes: {
+          '/': (RouteArguments args) {
+            return new GestureDetector(
+              onTap: () {
+                showSnackBar(
+                  navigator: args.navigator,
+                  placeholderKey: placeholderKey,
+                  content: new Text(helloSnackBar)
+                );
+              },
+              child: new Center(
+                key: tapTarget,
+                child: new Placeholder(key: placeholderKey)
+              )
+            );
+          }
+        }
+      ));
+
+      tester.tap(tester.findElementByKey(tapTarget));
+
+      expect(tester.findText(helloSnackBar), isNull);
+      tester.pump();
+      expect(tester.findText(helloSnackBar), isNotNull);
+    });
+  });
+}
