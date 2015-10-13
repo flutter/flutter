06a962390614e2f21d57f714e82f3784a6d0d992
Adam Barth <abarth@chromium.org>
2015-10-13 16:34:01 -0700

    Rename skysprites to flutter_sprites

--- examples/game/test_drawatlas.dart
+++ examples/game/test_drawatlas.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
-import 'package:flutter_sprites/skysprites.dart';
+import 'package:flutter_sprites/flutter_sprites.dart';
 
 AssetBundle _initBundle() {
   if (rootBundle != null)
--- skysprites/lib/src/sound.dart
+++ skysprites/lib/src/sound.dart
@@ -1,4 +1,4 @@
-part of skysprites;
+part of flutter_sprites;
 
 // TODO: The sound effects should probably use Android's SoundPool instead of
 // MediaPlayer as it is more efficient and flexible for playing back sound effects
