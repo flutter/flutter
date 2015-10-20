2ffd0ffb954ffa563ab9fdf94f7f76a57f5b7d49
Hixie <github.com@spam.hixie.ch>
2015-10-20 09:50:10 -0700

    RenderOverflowBox baseline logic
    I accidentally lost this when extracting RenderOverflowBox from
    RenderProxyBox.

--- examples/rendering/baseline.dart
+++ examples/rendering/baseline.dart
@@ -37,7 +37,7 @@ RenderBox getBox(double lh) {
           padding: new EdgeDims.all(10.0),
           child: new RenderCustomPaint(
             child: paragraph,
-            onPaint: (canvas, size) {
+            onPaint: (PaintingCanvas canvas, Size size) {
               double baseline = paragraph.getDistanceToBaseline(TextBaseline.alphabetic);
               double w = paragraph.getMaxIntrinsicWidth(new BoxConstraints.loose(size));
               double h = paragraph.getMaxIntrinsicHeight(new BoxConstraints.loose(size));
