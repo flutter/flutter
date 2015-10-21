ab9d70505c861970371ae7d4396d106fd4ae84c2
Hixie <github.com@spam.hixie.ch>
2015-10-21 11:57:26 -0700

    Use a card for the stock symbol viewer page.

--- examples/stocks/lib/stock_row.dart
+++ examples/stocks/lib/stock_row.dart
@@ -4,24 +4,24 @@
 
 part of stocks;
 
-enum StockRowPartKind { arrow, symbol, price }
+enum StockRowPartKind { arrow }
 
-class StockRowPartGlobalKey extends GlobalKey {
-  const StockRowPartGlobalKey(this.stock, this.part) : super.constructor();
+class StockRowPartKey extends GlobalKey {
+  const StockRowPartKey(this.stock, this.part) : super.constructor();
   final Stock stock;
   final StockRowPartKind part;
   bool operator ==(dynamic other) {
-    if (other is! StockRowPartGlobalKey)
+    if (other is! StockRowPartKey)
       return false;
-    final StockRowPartGlobalKey typedOther = other;
+    final StockRowPartKey typedOther = other;
     return stock == typedOther.stock &&
            part == typedOther.part;
   }
   int get hashCode => 37 * (37 * (373) + identityHashCode(stock)) + identityHashCode(part);
-  String toString() => '[StockRowPartGlobalKey ${stock.symbol}:${part.toString().split(".")[1]})]';
+  String toString() => '[StockRowPartKey ${stock.symbol}:${part.toString().split(".")[1]})]';
 }
 
-typedef void StockRowActionCallback(Stock stock, GlobalKey row, GlobalKey arrowKey, GlobalKey symbolKey, GlobalKey priceKey);
+typedef void StockRowActionCallback(Stock stock, GlobalKey arrowKey);
 
 class StockRow extends StatelessComponent {
   StockRow({
@@ -29,38 +29,34 @@ class StockRow extends StatelessComponent {
     this.onPressed,
     this.onLongPressed
   }) : this.stock = stock,
-       arrowKey = new StockRowPartGlobalKey(stock, StockRowPartKind.arrow),
-       symbolKey = new StockRowPartGlobalKey(stock, StockRowPartKind.symbol),
-       priceKey = new StockRowPartGlobalKey(stock, StockRowPartKind.price),
+       _arrowKey = new StockRowPartKey(stock, StockRowPartKind.arrow),
        super(key: new GlobalObjectKey(stock));
 
   final Stock stock;
   final StockRowActionCallback onPressed;
   final StockRowActionCallback onLongPressed;
-  final GlobalKey arrowKey;
-  final GlobalKey symbolKey;
-  final GlobalKey priceKey;
+
+  final Key _arrowKey;
 
   static const double kHeight = 79.0;
 
   GestureTapCallback _getTapHandler(StockRowActionCallback callback) {
     if (callback == null)
       return null;
-    return () => callback(stock, key, arrowKey, symbolKey, priceKey);
+    return () => callback(stock, _arrowKey);
   }
 
   GestureLongPressCallback _getLongPressHandler(StockRowActionCallback callback) {
     if (callback == null)
       return null;
-    return () => callback(stock, key, arrowKey, symbolKey, priceKey);
+    return () => callback(stock, _arrowKey);
   }
 
   Widget build(BuildContext context) {
     String lastSale = "\$${stock.lastSale.toStringAsFixed(2)}";
-
     String changeInPrice = "${stock.percentChange.toStringAsFixed(2)}%";
-    if (stock.percentChange > 0) changeInPrice = "+" + changeInPrice;
-
+    if (stock.percentChange > 0)
+      changeInPrice = "+" + changeInPrice;
     return new InkWell(
       onTap: _getTapHandler(onPressed),
       onLongPress: _getLongPressHandler(onLongPressed),
@@ -72,39 +68,38 @@ class StockRow extends StatelessComponent {
           )
         ),
         child: new Row(<Widget>[
-          new Container(
-            key: arrowKey,
-            child: new StockArrow(percentChange: stock.percentChange),
-            margin: const EdgeDims.only(right: 5.0)
-          ),
-          new Flexible(
-            child: new Row(<Widget>[
-                new Flexible(
-                  flex: 2,
-                  child: new Text(
-                    stock.symbol,
-                    key: symbolKey
-                  )
-                ),
-                new Flexible(
-                  child: new Text(
-                    lastSale,
-                    style: const TextStyle(textAlign: TextAlign.right),
-                    key: priceKey
-                  )
-                ),
-                new Flexible(
-                  child: new Text(
-                    changeInPrice,
-                    style: Theme.of(context).text.caption.copyWith(textAlign: TextAlign.right)
-                  )
-                ),
-              ],
-              alignItems: FlexAlignItems.baseline,
-              textBaseline: DefaultTextStyle.of(context).textBaseline
-            )
-          )
-        ])
+            new Container(
+              key: _arrowKey,
+              child: new StockArrow(percentChange: stock.percentChange),
+              margin: const EdgeDims.only(right: 5.0)
+            ),
+            new Flexible(
+              child: new Row(<Widget>[
+                  new Flexible(
+                    flex: 2,
+                    child: new Text(
+                      stock.symbol
+                    )
+                  ),
+                  new Flexible(
+                    child: new Text(
+                      lastSale,
+                      style: const TextStyle(textAlign: TextAlign.right)
+                    )
+                  ),
+                  new Flexible(
+                    child: new Text(
+                      changeInPrice,
+                      style: const TextStyle(textAlign: TextAlign.right)
+                    )
+                  ),
+                ],
+                alignItems: FlexAlignItems.baseline,
+                textBaseline: DefaultTextStyle.of(context).textBaseline
+              )
+            ),
+          ]
+        )
       )
     );
   }
