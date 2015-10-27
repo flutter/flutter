fbfbd57f6578e93a6faa0ff241d4885ea4ccc863
Adam Barth <abarth@chromium.org>
2015-10-26 20:57:04 -0700

    Add CustomOneChildLayout
    A CustomOneChildLayout is a widget that lets you perform a custom layout in the
    simplified setting of sizing and positioning a single child.

---
+++ sky/unit/test/widget/custom_one_child_layout_test.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/widgets.dart';
+import 'package:test/test.dart';
+
+import 'widget_tester.dart';
+
+class TestOneChildLayoutDelegate extends OneChildLayoutDelegate {
+  BoxConstraints constraintsFromGetSize;
+  BoxConstraints constraintsFromGetConstraintsForChild;
+  Size sizeFromGetPositionForChild;
+  Size childSizeFromGetPositionForChild;
+
+  Size getSize(BoxConstraints constraints) {
+    constraintsFromGetSize = constraints;
+    return new Size(200.0, 300.0);
+  }
+
+  BoxConstraints getConstraintsForChild(BoxConstraints constraints) {
+    constraintsFromGetConstraintsForChild = constraints;
+    return new BoxConstraints(
+      minWidth: 100.0,
+      maxWidth: 150.0,
+      minHeight: 200.0,
+      maxHeight: 400.0
+    );
+  }
+
+  Point getPositionForChild(Size size, Size childSize) {
+    sizeFromGetPositionForChild = size;
+    childSizeFromGetPositionForChild = childSize;
+    return Point.origin;
+  }
+}
+
+void main() {
+  test('Control test for CustomOneChildLayout', () {
+    testWidgets((WidgetTester tester) {
+      TestOneChildLayoutDelegate delegate = new TestOneChildLayoutDelegate();
+      tester.pumpWidget(new Center(
+        child: new CustomOneChildLayout(delegate: delegate, child: new Container())
+      ));
+
+      expect(delegate.constraintsFromGetSize.minWidth, 0.0);
+      expect(delegate.constraintsFromGetSize.maxWidth, 800.0);
+      expect(delegate.constraintsFromGetSize.minHeight, 0.0);
+      expect(delegate.constraintsFromGetSize.maxHeight, 600.0);
+
+      expect(delegate.constraintsFromGetConstraintsForChild.minWidth, 0.0);
+      expect(delegate.constraintsFromGetConstraintsForChild.maxWidth, 800.0);
+      expect(delegate.constraintsFromGetConstraintsForChild.minHeight, 0.0);
+      expect(delegate.constraintsFromGetConstraintsForChild.maxHeight, 600.0);
+
+      expect(delegate.sizeFromGetPositionForChild.width, 200.0);
+      expect(delegate.sizeFromGetPositionForChild.height, 300.0);
+
+      expect(delegate.childSizeFromGetPositionForChild.width, 150.0);
+      expect(delegate.childSizeFromGetPositionForChild.height, 400.0);
+    });
+  });
+}
