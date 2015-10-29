e904ab5773a36fff7434d364a361af20b297cf10
Adam Barth <abarth@chromium.org>
2015-10-29 13:42:57 -0700

    Add missing import

--- packages/flutter/lib/services.dart
+++ packages/flutter/lib/services.dart
@@ -18,4 +18,5 @@ export 'src/services/image_cache.dart';
 export 'src/services/image_decoder.dart';
 export 'src/services/image_resource.dart';
 export 'src/services/keyboard.dart';
+export 'src/services/service_registry.dart';
 export 'src/services/shell.dart';
