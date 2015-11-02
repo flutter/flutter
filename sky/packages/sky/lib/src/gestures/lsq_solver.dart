b486bfdb6cb55c3af2a5f0ed7216edc52574ad52
Kris Giesing <kgiesing@google.com>
2015-11-02 13:30:36 -0800

    Style pass on velocity tracker and event recorder

--- sky/packages/sky/lib/src/gestures/lsq_solver.dart
+++ sky/packages/sky/lib/src/gestures/lsq_solver.dart
@@ -1,22 +1,26 @@
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import "dart:math" as math;
 import "dart:typed_data";
 
-class Vector {
-  Vector(int size)
-  : _offset = 0, _length = size, _elem = new Float64List(size);
+class _Vector {
+  _Vector(int size)
+  : _offset = 0, _length = size, _elements = new Float64List(size);
 
-  Vector.fromValues(List<double> values)
-  : _offset = 0, _length = values.length, _elem = values;
+  _Vector.fromValues(List<double> values)
+  : _offset = 0, _length = values.length, _elements = values;
 
-  Vector.fromVOL(List<double> values, int offset, int length)
-  : _offset = offset, _length = length, _elem = values;
+  _Vector.fromVOL(List<double> values, int offset, int length)
+  : _offset = offset, _length = length, _elements = values;
 
   int get length => _length;
 
-  operator [](int i) => _elem[i + _offset];
-  operator []=(int i, double value) => _elem[i + _offset] = value;
+  operator [](int i) => _elements[i + _offset];
+  operator []=(int i, double value) => _elements[i + _offset] = value;
 
-  operator *(Vector a) {
+  operator *(_Vector a) {
     double result = 0.0;
     for (int i = 0; i < _length; i++) {
       result += this[i] * a[i];
@@ -38,28 +42,32 @@ class Vector {
 
   final int _offset;
   final int _length;
-  final List<double> _elem;
+  final List<double> _elements;
 }
 
-class Matrix {
-  Matrix(int rows, int cols)
+class _Matrix {
+  _Matrix(int rows, int cols)
   : _rows = rows,
-    _cols = cols,
-    _elem = new Float64List(rows * cols);
+    _columns = cols,
+    _elements = new Float64List(rows * cols);
 
-  double get(int row, int col) => _elem[row * _cols + col];
+  double get(int row, int col) => _elements[row * _columns + col];
   void set(int row, int col, double value) {
-    _elem[row * _cols + col] = value;
+    _elements[row * _columns + col] = value;
   }
 
-  Vector getRow(int row) => new Vector.fromVOL(_elem, row * _cols, _cols);
+  _Vector getRow(int row) => new _Vector.fromVOL(
+    _elements,
+    row * _columns,
+    _columns
+  );
 
   String toString() {
     String result = "";
     for (int i = 0; i < _rows; i++) {
       if (i > 0)
         result += "; ";
-      for (int j = 0; j < _cols; j++) {
+      for (int j = 0; j < _columns; j++) {
         if (j > 0)
           result += ", ";
         result += get(i, j).toString();
@@ -69,8 +77,8 @@ class Matrix {
   }
 
   final int _rows;
-  final int _cols;
-  final List<double> _elem;
+  final int _columns;
+  final List<double> _elements;
 }
 
 class PolynomialFit {
@@ -99,10 +107,9 @@ class LeastSquaresSolver {
     // Shorthands for the purpose of notation equivalence to original C++ code
     final int m = x.length;
     final int n = degree + 1;
-    final List<double> out_b = result.coefficients;
 
     // Expand the X vector to a matrix A, pre-multiplied by the weights.
-    Matrix a = new Matrix(n, m);
+    _Matrix a = new _Matrix(n, m);
     for (int h = 0; h < m; h++) {
       a.set(0, h, w[h]);
       for (int i = 1; i < n; i++) {
@@ -113,15 +120,15 @@ class LeastSquaresSolver {
     // Apply the Gram-Schmidt process to A to obtain its QR decomposition.
 
     // Orthonormal basis, column-major ordVectorer.
-    Matrix q = new Matrix(n, m);
+    _Matrix q = new _Matrix(n, m);
     // Upper triangular matrix, row-major order.
-    Matrix r = new Matrix(n, n);
+    _Matrix r = new _Matrix(n, n);
     for (int j = 0; j < n; j++) {
       for (int h = 0; h < m; h++) {
         q.set(j, h, a.get(j, h));
       }
       for (int i = 0; i < j; i++) {
-        double dot = q.getRow(j)*q.getRow(i);
+        double dot = q.getRow(j) * q.getRow(i);
         for (int h = 0; h < m; h++) {
           q.set(j, h, q.get(j, h) - dot * q.get(i, h));
         }
@@ -133,56 +140,57 @@ class LeastSquaresSolver {
         return null;
       }
 
-      double invNorm = 1.0 / norm;
+      double inverseNorm = 1.0 / norm;
       for (int h = 0; h < m; h++) {
-        q.set(j, h, q.get(j, h) * invNorm);
+        q.set(j, h, q.get(j, h) * inverseNorm);
       }
       for (int i = 0; i < n; i++) {
-        r.set(j, i, i < j ? 0.0 : q.getRow(j)*a.getRow(i));
+        r.set(j, i, i < j ? 0.0 : q.getRow(j) * a.getRow(i));
       }
     }
 
     // Solve R B = Qt W Y to find B.  This is easy because R is upper triangular.
     // We just work from bottom-right to top-left calculating B's coefficients.
-    Vector wy = new Vector(m);
+    _Vector wy = new _Vector(m);
     for (int h = 0; h < m; h++) {
       wy[h] = y[h] * w[h];
     }
     for (int i = n; i-- != 0;) {
-      out_b[i] = q.getRow(i) * wy;
+      result.coefficients[i] = q.getRow(i) * wy;
       for (int j = n - 1; j > i; j--) {
-        out_b[i] -= r.get(i, j) * out_b[j];
+        result.coefficients[i] -= r.get(i, j) * result.coefficients[j];
       }
-      out_b[i] /= r.get(i, i);
+      result.coefficients[i] /= r.get(i, i);
     }
 
-    // Calculate the coefficient of determination as 1 - (SSerr / SStot) where
-    // SSerr is the residual sum of squares (variance of the error),
-    // and SStot is the total sum of squares (variance of the data) where each
-    // has been weighted.
-    double ymean = 0.0;
+    // Calculate the coefficient of determination (confidence) as:
+    // 1 - (sumSquaredError / sumSquaredTotal)
+    // where sumSquaredError is the residual sum of squares (variance of the
+    // error), and sumSquaredTotal is the total sum of squares (variance of the
+    // data) where each has been weighted.
+    double yMean = 0.0;
     for (int h = 0; h < m; h++) {
-      ymean += y[h];
+      yMean += y[h];
     }
-    ymean /= m;
+    yMean /= m;
 
-    double sserr = 0.0;
-    double sstot = 0.0;
+    double sumSquaredError = 0.0;
+    double sumSquaredTotal = 0.0;
     for (int h = 0; h < m; h++) {
-      double err = y[h] - out_b[0];
+      double err = y[h] - result.coefficients[0];
       double term = 1.0;
       for (int i = 1; i < n; i++) {
         term *= x[h];
-        err -= term * out_b[i];
+        err -= term * result.coefficients[i];
       }
-      sserr += w[h] * w[h] * err * err;
-      double v = y[h] - ymean;
-      sstot += w[h] * w[h] * v * v;
+      sumSquaredError += w[h] * w[h] * err * err;
+      double v = y[h] - yMean;
+      sumSquaredTotal += w[h] * w[h] * v * v;
     }
 
-    double det = sstot > 0.000001 ? 1.0 - (sserr / sstot) : 1.0;
-
-    result.confidence = det;
+    result.confidence = sumSquaredTotal > 0.000001 ?
+      1.0 - (sumSquaredError / sumSquaredTotal) :
+      1.0;
 
     return result;
   }
