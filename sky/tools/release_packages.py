573ce5881b40b5716209ebf27924892d5155aedf
Adam Barth <abarth@chromium.org>
2015-11-03 15:47:50 -0800

    Split big_red_button into two scripts:
     - sky/tools/release_engine.py, which releases sky_engine and sky_services
     - sky/tools/release_packages.py, which releases flutter and flutter_sprites

---
+++ sky/tools/release_packages.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# Copyright 2015 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# See https://github.com/domokit/sky_engine/wiki/Release-process
+
+import os
+import subprocess
+import sys
+
+
+def main():
+    engine_root = os.path.abspath('.')
+    if not os.path.exists(os.path.join(engine_root, 'sky')):
+        print "Cannot find //sky. Is %s the Flutter engine repository?" % engine_root
+        return 1
+
+    pub_path = os.path.join(engine_root, 'third_party/dart-sdk/dart-sdk/bin/pub')
+
+    if args.publish:
+        subprocess.check_call([pub_path, 'publish', '--force'], cwd=os.path.join(engine_root, 'sky/packages/sky'))
+        subprocess.check_call([pub_path, 'publish', '--force'], cwd=os.path.join(engine_root, 'skysprites'))
+
+
+if __name__ == '__main__':
+    sys.exit(main())
