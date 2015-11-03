c9e621cd6cd77b67432f9b75032425f17d9cacf0
Collin Jackson <jackson@google.com>
2015-11-03 15:53:23 -0800

    Bare bones Mojo reimplementation of Dart’s http package

---
+++ sky/packages/sky/lib/src/http/response.dart
@@ -0,0 +1,12 @@
+// Copyright (c) 2012, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+library response;
+
+/// An HTTP response where the entire response body is known in advance.
+class Response {
+  const Response(this.body, this.statusCode);
+  final String body;
+  final int statusCode;
+}
