8fd1ce77386693a6eda1f92bcfad04d0b2272361
Chinmay Garde <chinmaygarde@google.com>
2015-11-05 12:23:55 -0800

    Disable Dart profiling on iOS and MacOS

--- sky/engine/core/script/dart_init.cc
+++ sky/engine/core/script/dart_init.cc
@@ -80,10 +80,9 @@ static const char* kDartArgs[] = {
     // default profile period to 100Hz. This number is suitable for older
     // Raspberry Pi devices but quite low for current smartphones.
     "--profile_period=1000",
-#if (WTF_OS_IOS || WTF_OS_MACOSX) && !defined(NDEBUG)
+#if (WTF_OS_IOS || WTF_OS_MACOSX)
     // On platforms where LLDB is the primary debugger, SIGPROF signals
-    // overwhelm LLDB. Since in debug builds, CPU profiling information is
-    // less useful anyway, vm profiling is disabled to enable a usable debugger.
+    // overwhelm LLDB.
     "--no-profile",
 #endif
 };
