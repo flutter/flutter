68eebaa5dbe9cf4917572bca6bde88a9cad273e0
Adam Barth <abarth@chromium.org>
2015-11-10 09:29:37 -0800

    Add some basic benchmarks of the Stocks app
     - animation_bench.dart. This benchmark measures the full main-thread pipeline
       for ticking the drawer entrance and exit animation.
     - build_bench.dart. This benchmark measures a full app rebuild when there's no
       state change.
     - layout_bench.dart. This benchmark measures a full relayout.

---
+++ packages/unit/benchmark/stocks/layout_bench.dart
@@ -0,0 +1,40 @@
+import 'dart:io';
+
+import 'package:flutter/rendering.dart';
+import 'package:flutter/material.dart';
+import 'package:stocks/main.dart' as stocks;
+import 'package:stocks/stock_data.dart' as stock_data;
+import '../../test/widget/widget_tester.dart';
+
+const int _kNumberOfIterations = 100000;
+const bool _kRunForever = false;
+
+void main() {
+  assert(false); // Don't run in checked mode
+  stock_data.StockDataFetcher.actuallyFetchData = false;
+
+  testWidgets((WidgetTester tester) {
+    stocks.main();
+    tester.pump(); // Start startup animation
+    tester.pump(const Duration(seconds: 1)); // Complete startup animation
+    tester.tapAt(new Point(20.0, 20.0)); // Open drawer
+    tester.pump(); // Start drawer animation
+    tester.pump(const Duration(seconds: 1)); // Complete drawer animation
+  });
+
+  ViewConstraints big = const ViewConstraints(size: const Size(360.0, 640.0));
+  ViewConstraints small = const ViewConstraints(size: const Size(355.0, 635.0));
+  RenderView renderView = WidgetFlutterBinding.instance.renderView;
+
+  Stopwatch watch = new Stopwatch()
+    ..start();
+
+  for (int i = 0; i < _kNumberOfIterations || _kRunForever; ++i) {
+    renderView.rootConstraints = (i % 2 == 0) ? big : small;
+    RenderObject.flushLayout();
+  }
+
+  watch.stop();
+  print("Stock layout: " + watch.elapsed.toString());
+  exit(0);
+}
