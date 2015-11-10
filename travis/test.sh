2644e8592295149be34ddebb8729dd0ae9c18bbe
Devon Carew <devoncarew@gmail.com>
2015-11-10 08:49:11 -0800

    test more packages on travis

--- travis/test.sh
+++ travis/test.sh
@@ -2,8 +2,10 @@
 set -ex
 
 (cd packages/cassowary; pub global run tuneup check; pub run test -j1)
+(cd packages/flutter_sprites; pub global run tuneup check)
 (cd packages/flutter_tools; pub global run tuneup check; pub run test -j1)
 (cd packages/flx; pub global run tuneup check; pub run test -j1)
 (cd packages/newton; pub global run tuneup check; pub run test -j1)
+(cd packages/updater; pub global run tuneup check)
 
 ./bin/flutter test --engine-src-path bin/cache/travis
