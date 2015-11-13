d041f3ead58c5d543c4519f50781aeaa57b89b28
Hixie <github.com@spam.hixie.ch>
2015-11-13 11:13:33 -0800

    More resilient Widget tests
    - force the time dilation to 1.0 for the Widget tests, so that a local
      change doesn't break all the tests during development.
    - add missing license block to all the files.
    - set ui.window.onBeginFrame to null when you use WidgetTester, so that
      the engine doesn't trigger any confusing frames after our fake frames.

--- packages/unit/test/widget/dismissable_test.dart
+++ packages/unit/test/widget/dismissable_test.dart
@@ -1,3 +1,7 @@
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
 import 'package:test/test.dart';
@@ -280,3 +284,4 @@ void main() {
     });
   });
 }
+
--- packages/unit/test/widget/input_test.dart
+++ packages/unit/test/widget/input_test.dart
@@ -1,3 +1,7 @@
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import 'package:mojo_services/keyboard/keyboard.mojom.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
