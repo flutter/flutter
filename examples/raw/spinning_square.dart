c964a1f1f8814acd86a61e354e5189a601c6eddd
Adam Barth <abarth@chromium.org>
2015-11-18 12:55:07 -0800

    Switch from ui.tracing to dart:Developer Timeline
    This change will let us remove the tracing interface from dart:ui and also
    makes these timeline events show up in Observatory.

--- examples/raw/spinning_square.dart
+++ examples/raw/spinning_square.dart
@@ -2,43 +2,44 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'dart:developer';
 import 'dart:math' as math;
-import 'dart:ui' as ui;
 import 'dart:typed_data';
+import 'dart:ui' as ui;
 
 Duration timeBase = null;
 
 void beginFrame(Duration timeStamp) {
-  ui.tracing.begin('beginFrame');
-  if (timeBase == null)
-    timeBase = timeStamp;
-  double delta = (timeStamp - timeBase).inMicroseconds / Duration.MICROSECONDS_PER_MILLISECOND;
+  Timeline.timeSync('beginFrame', () {
+    if (timeBase == null)
+      timeBase = timeStamp;
+    double delta = (timeStamp - timeBase).inMicroseconds / Duration.MICROSECONDS_PER_MILLISECOND;
 
-  // paint
-  ui.Rect paintBounds = ui.Point.origin & ui.window.size;
-  ui.PictureRecorder recorder = new ui.PictureRecorder();
-  ui.Canvas canvas = new ui.Canvas(recorder, paintBounds);
-  canvas.translate(paintBounds.width / 2.0, paintBounds.height / 2.0);
-  canvas.rotate(math.PI * delta / 1800);
-  canvas.drawRect(new ui.Rect.fromLTRB(-100.0, -100.0, 100.0, 100.0),
-                  new ui.Paint()..color = const ui.Color.fromARGB(255, 0, 255, 0));
-  ui.Picture picture = recorder.endRecording();
+    // paint
+    ui.Rect paintBounds = ui.Point.origin & ui.window.size;
+    ui.PictureRecorder recorder = new ui.PictureRecorder();
+    ui.Canvas canvas = new ui.Canvas(recorder, paintBounds);
+    canvas.translate(paintBounds.width / 2.0, paintBounds.height / 2.0);
+    canvas.rotate(math.PI * delta / 1800);
+    canvas.drawRect(new ui.Rect.fromLTRB(-100.0, -100.0, 100.0, 100.0),
+                    new ui.Paint()..color = const ui.Color.fromARGB(255, 0, 255, 0));
+    ui.Picture picture = recorder.endRecording();
 
-  // composite
-  final double devicePixelRatio = ui.window.devicePixelRatio;
-  ui.Rect sceneBounds = new ui.Rect.fromLTWH(0.0, 0.0, ui.window.size.width * devicePixelRatio, ui.window.size.height * devicePixelRatio);
-  Float64List deviceTransform = new Float64List(16)
-    ..[0] = devicePixelRatio
-    ..[5] = devicePixelRatio
-    ..[10] = 1.0
-    ..[15] = 1.0;
-  ui.SceneBuilder sceneBuilder = new ui.SceneBuilder(sceneBounds)
-    ..pushTransform(deviceTransform)
-    ..addPicture(ui.Offset.zero, picture, paintBounds)
-    ..pop();
-  ui.window.render(sceneBuilder.build());
+    // composite
+    final double devicePixelRatio = ui.window.devicePixelRatio;
+    ui.Rect sceneBounds = new ui.Rect.fromLTWH(0.0, 0.0, ui.window.size.width * devicePixelRatio, ui.window.size.height * devicePixelRatio);
+    Float64List deviceTransform = new Float64List(16)
+      ..[0] = devicePixelRatio
+      ..[5] = devicePixelRatio
+      ..[10] = 1.0
+      ..[15] = 1.0;
+    ui.SceneBuilder sceneBuilder = new ui.SceneBuilder(sceneBounds)
+      ..pushTransform(deviceTransform)
+      ..addPicture(ui.Offset.zero, picture, paintBounds)
+      ..pop();
+    ui.window.render(sceneBuilder.build());
+  });
 
-  ui.tracing.end('beginFrame');
   ui.window.scheduleFrame();
 }
 
