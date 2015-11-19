3b247b435148de13ad54d421d8c0cb304c6b8dca
Kris Giesing <kgiesing@google.com>
2015-11-18 17:24:38 -0800

    Use embedder to fetch keyboard service

--- packages/flutter/lib/src/services/keyboard.dart
+++ packages/flutter/lib/src/services/keyboard.dart
@@ -14,7 +14,7 @@ class _KeyboardConnection {
 
   _KeyboardConnection() {
     proxy = new KeyboardServiceProxy.unbound();
-    shell.connectToService("mojo:keyboard", proxy);
+    shell.connectToService(null, proxy);
   }
 
   KeyboardServiceProxy proxy;
