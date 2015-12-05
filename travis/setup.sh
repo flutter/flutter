6ca057e508452e9d6c5630ce580e3fad6973580d
Adam Barth <abarth@chromium.org>
2015-12-05 14:09:28 -0800

    Update Flutter API docs continuously
    The patch teaches Travis how to update our API docs continuously as we land
    patches in master.

--- travis/setup.sh
+++ travis/setup.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
-set -ex
+set -e
+
+if [ $TRAVIS_PULL_REQUEST = "false" ]; then
+  echo $KEY_FILE | base64 --decode > gcloud_key_file.json
+fi
+
+set -x
 
 dart dev/update_packages.dart
 (cd packages/unit; ../../bin/flutter cache populate)
+
+if [ $TRAVIS_PULL_REQUEST = "false" ]; then
+  export CLOUDSDK_CORE_DISABLE_PROMPTS=1
+  curl https://sdk.cloud.google.com | bash
+fi
