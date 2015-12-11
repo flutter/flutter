fb374a9f5a507a6b403fbef42baf110bf78a02d1
Adam Barth <abarth@chromium.org>
2015-12-11 10:18:50 -0800

    Remove unused code
    We don't use yasm, libjpeg_turbo, or mojo's devtools.

--- third_party/yasm/source/config/mac/libyasm-stdint.h
+++
@@ -1,9 +0,0 @@
-#ifndef _YASM_LIBYASM_STDINT_H
-#define _YASM_LIBYASM_STDINT_H 1
-#ifndef _GENERATED_STDINT_H
-#define _GENERATED_STDINT_H "yasm 0.8.0"
-/* generated using gcc -std=gnu99 */
-#define _STDINT_HAVE_STDINT_H 1
-#include <stdint.h>
-#endif
-#endif
