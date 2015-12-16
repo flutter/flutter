88ac7eeba9a293a4ec092e3997460bda88dee941
Seth Ladd <sethladd@google.com>
2015-12-16 13:55:14 -0800

    analytics and a homepage for docs.flutter.io

--- travis/test.sh
+++ travis/test.sh
@@ -21,12 +21,15 @@ flutter analyze --flutter-repo --no-current-directory --no-current-package --con
 if [ $TRAVIS_PULL_REQUEST = "false" ]; then
   if [ $TRAVIS_BRANCH = "master" ]; then
     pub global activate dartdoc 0.8.4
-    (cd packages/flutter; ~/.pub-cache/bin/dartdoc --header=doc/styles.html)
+    cat packages/flutter/doc/styles.html doc/_analytics.html > /tmp/_header.html
+    (cd packages/flutter; ~/.pub-cache/bin/dartdoc --header=/tmp/_header.html)
 
     GSUTIL=$HOME/google-cloud-sdk/bin/gsutil
     GCLOUD=$HOME/google-cloud-sdk/bin/gcloud
     $GCLOUD auth activate-service-account --key-file gcloud_key_file.json
     $GSUTIL -m -q rsync -r -d packages/flutter/doc/api gs://docs.flutter.io/flutter
     $GSUTIL -m -q rsync -r -d packages/flutter/doc/api gs://docs.domokit.org/flutter
+    $GSUTIL -m -q cp doc/index.html gs://docs.flutter.io/flutter/index.html
+    $GSUTIL -m -q cp doc/index.html gs://docs.domokit.org/flutter/index.html
   fi
 fi
