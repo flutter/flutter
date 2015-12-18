1bde1e7939b8102dc39402b620b7e8e86df905e9
Eric Seidel <eseidel@google.com>
2015-12-18 08:29:47 -0800

    Generate docs for more of our packages
    I also stopped pushing docs to domokit.org I presume we
    were doing that to not break old links.  @abarth
    
    @sethladd

--- packages/newton/lib/newton.dart
+++ packages/newton/lib/newton.dart
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+/// Simple Physics Simulations for Dart. Springs, friction, gravity, etc.
 library newton;
 
 import 'dart:math' as math;
--- travis/test.sh
+++ travis/test.sh
@@ -22,14 +22,24 @@ if [ $TRAVIS_PULL_REQUEST = "false" ]; then
   if [ $TRAVIS_BRANCH = "master" ]; then
     pub global activate dartdoc 0.8.4
     cat packages/flutter/doc/styles.html doc/_analytics.html > /tmp/_header.html
+
+    # TODO(eseidel): This should just call a helper script.
+    # If you add a package to this list, update doc/index.html to point to it.
     (cd packages/flutter; ~/.pub-cache/bin/dartdoc --header=/tmp/_header.html)
+    (cd packages/playfair; ~/.pub-cache/bin/dartdoc --header=/tmp/_header.html)
+    (cd packages/newton; ~/.pub-cache/bin/dartdoc --header=/tmp/_header.html)
+    (cd packages/cassowary; ~/.pub-cache/bin/dartdoc --header=/tmp/_header.html)
+    (cd packages/flutter_test; ~/.pub-cache/bin/dartdoc --header=/tmp/_header.html)
 
     GSUTIL=$HOME/google-cloud-sdk/bin/gsutil
     GCLOUD=$HOME/google-cloud-sdk/bin/gcloud
     $GCLOUD auth activate-service-account --key-file gcloud_key_file.json
-    $GSUTIL -m -q rsync -r -d packages/flutter/doc/api gs://docs.flutter.io/flutter
-    $GSUTIL -m -q rsync -r -d packages/flutter/doc/api gs://docs.domokit.org/flutter
     $GSUTIL -m -q cp doc/index.html gs://docs.flutter.io/index.html
-    $GSUTIL -m -q cp doc/index.html gs://docs.domokit.org/index.html
+
+    $GSUTIL -m -q rsync -r -d packages/flutter/doc/api gs://docs.flutter.io/flutter
+    $GSUTIL -m -q rsync -r -d packages/playfair/doc/api gs://docs.flutter.io/playfair
+    $GSUTIL -m -q rsync -r -d packages/newton/doc/api gs://docs.flutter.io/newton
+    $GSUTIL -m -q rsync -r -d packages/cassowary/doc/api gs://docs.flutter.io/cassowary
+    $GSUTIL -m -q rsync -r -d packages/flutter_test/doc/api gs://docs.flutter.io/flutter_test
   fi
 fi
