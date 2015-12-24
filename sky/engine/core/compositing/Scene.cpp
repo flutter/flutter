ca934cf9563d68953cb11fa5da08809f8529a9fc
Adam Barth <abarth@chromium.org>
2015-12-24 01:26:59 -0800

    Reduce the boilerplate when creating bindings
    Instead, use C++ template to generate the callback trampolines and the
    registration block.

--- sky/engine/core/compositing/Scene.cpp
+++ sky/engine/core/compositing/Scene.cpp
@@ -7,25 +7,18 @@
 #include "third_party/skia/include/core/SkCanvas.h"
 #include "third_party/skia/include/core/SkPictureRecorder.h"
 #include "sky/engine/tonic/dart_args.h"
+#include "sky/engine/tonic/dart_binding_macros.h"
 #include "sky/engine/tonic/dart_converter.h"
 #include "sky/engine/tonic/dart_library_natives.h"
 
 namespace blink {
-namespace {
-
-void DisposeCallback(Dart_NativeArguments args) {
-  DartCall(&Scene::dispose, args);
-}
-
-}  // namespace
 
 IMPLEMENT_WRAPPERTYPEINFO(Scene);
 
-void Scene::RegisterNatives(DartLibraryNatives* natives) {
-  natives->Register({
-    { "Scene_dispose", DisposeCallback, 1, true },
-  });
-}
+#define FOR_EACH_BINDING(V) \
+  V(Scene, dispose)
+
+DART_BIND_ALL(Scene, FOR_EACH_BINDING)
 
 PassRefPtr<Scene> Scene::create(
     std::unique_ptr<sky::compositor::Layer> rootLayer,
