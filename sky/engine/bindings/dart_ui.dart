0f2321348638151c1a7c3ce421495ca6b99d107f
Adam Barth <abarth@chromium.org>
2015-12-29 19:52:27 -0800

    Fully detach the IDL compiler from the build
    Prior to this patch, we were still using it to generate dart_ui.dart.
    We'll eventually want to reorganize the dart files in the engine. See
    https://github.com/flutter/flutter/issues/1040 for some discussion.

---
+++ sky/engine/bindings/dart_ui.dart
@@ -0,0 +1,31 @@
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+library dart_ui;
+
+import 'dart:async';
+import 'dart:nativewrappers';
+import 'dart:typed_data';
+import 'dart:math' as math;
+
+part 'Color.dart';
+part 'FilterQuality.dart';
+part 'Offset.dart';
+part 'OffsetBase.dart';
+part 'Paint.dart';
+part 'PaintingStyle.dart';
+part 'Point.dart';
+part 'RRect.dart';
+part 'RSTransform.dart';
+part 'Rect.dart';
+part 'Size.dart';
+part 'TransferMode.dart';
+part 'compositing.dart';
+part 'hash_codes.dart';
+part 'hooks.dart';
+part 'lerp.dart';
+part 'natives.dart';
+part 'painting.dart';
+part 'text.dart';
+part 'window.dart';
