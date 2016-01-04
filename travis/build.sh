bde18f1fa8618b5e778781d0bdff1aa822e1c9ca
Chinmay Garde <chinmaygarde@google.com>
2016-01-04 13:25:46 -0800

    Make travis pick up DEPS updates

--- travis/build.sh
+++ travis/build.sh
@@ -3,7 +3,6 @@ set -ex
 
 PATH="$HOME/depot_tools:$PATH"
 
-(cd ..; gclient sync)
 sky/tools/gn --debug
 ninja -C out/Debug generate_dart_ui
 travis/analyze.sh
--- travis/setup.sh
+++ travis/setup.sh
@@ -3,6 +3,8 @@ set -ex
 
 (cd; git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git)
 
+PATH="$HOME/depot_tools:$PATH"
+
 cd ..
 mv engine src
 mkdir engine
@@ -10,3 +12,4 @@ mv src engine
 cd engine/src
 
 mv travis/gclient ../.gclient
+gclient sync
