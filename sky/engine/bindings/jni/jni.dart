a5104c4a6126e987ed80a803a497adae647102e6
Jason Simmons <jsimmons@chromium.org>
2016-01-08 17:53:23 -0800

    Implement simple method calls in the Dart/JNI bridge
    Also adopt a standard pattern for handling exceptions in JNI APIs

--- sky/engine/bindings/jni/jni.dart
+++ sky/engine/bindings/jni/jni.dart
@@ -8,13 +8,17 @@ import 'dart:nativewrappers';
 
 /// Wrapper for a Java class accessed via JNI.
 class JniClass extends NativeFieldWrapperClass2 {
-  static JniClass fromName(String name) native 'JniClass_fromName';
+  static JniClass fromName(String name) native 'JniClass_FromName';
 
-  int getFieldId(String name, String sig) native 'JniClass_getFieldId';
-  int getStaticFieldId(String name, String sig) native 'JniClass_getStaticFieldId';
+  int getFieldId(String name, String sig) native 'JniClass_GetFieldId';
+  int getStaticFieldId(String name, String sig) native 'JniClass_GetStaticFieldId';
+  int getMethodId(String name, String sig) native 'JniClass_GetMethodId';
+  int getStaticMethodId(String name, String sig) native 'JniClass_GetStaticMethodId';
 
-  int getStaticIntField(int fieldId) native 'JniClass_getStaticIntField';
-  JniObject getStaticObjectField(int fieldId) native 'JniClass_getStaticObjectField';
+  int getStaticIntField(int fieldId) native 'JniClass_GetStaticIntField';
+  JniObject getStaticObjectField(int fieldId) native 'JniClass_GetStaticObjectField';
+
+  int callStaticLongMethod(int methodId, List args) native 'JniClass_CallStaticLongMethod';
 }
 
 /// Wrapper for a Java object accessed via JNI.
