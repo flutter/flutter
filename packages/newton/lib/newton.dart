87ab798dafa1099623b8286c07a3bc24f112c392
Adam Barth <abarth@chromium.org>
2016-01-20 15:55:47 -0800

    Move ClampedSimulation into newton
    ClampedSimulation makes more sense at the newton layer because it's dealing
    entirely with newton concepts.

--- packages/newton/lib/newton.dart
+++ packages/newton/lib/newton.dart
@@ -1,4 +1,4 @@
-// Copyright (c) 2015 The Chromium Authors. All rights reserved.
+// Copyright 2016 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -12,6 +12,7 @@ part 'src/simulation_group.dart';
 part 'src/tolerance.dart';
 part 'src/utils.dart';
 
+part 'src/clamped_simulation.dart';
 part 'src/friction_simulation.dart';
 part 'src/gravity_simulation.dart';
 part 'src/scroll_simulation.dart';
