58c7fdef36402f9d0e8c1a4d44d502657197ab29
Adam Barth <abarth@chromium.org>
2016-01-25 17:02:37 -0800

    Add RawKeyboardListener
    RawKeyboardListener makes it easier to listen for raw keyboard events.
    
    Fixes #787

--- examples/widgets/raw_keyboard.dart
+++ examples/widgets/raw_keyboard.dart
@@ -1,81 +1,79 @@
-// Copyright (c) 2015, the Flutter project authors.  Please see the AUTHORS file
-// for details. All rights reserved. Use of this source code is governed by a
-// BSD-style license that can be found in the LICENSE file.
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
-import 'package:sky_services/raw_keyboard/raw_keyboard.mojom.dart' as mojo;
-import 'package:sky_services/sky/input_event.mojom.dart' as mojo;
+import 'package:sky_services/sky/input_event.mojom.dart' as mojom;
+
+GlobalKey _key = new GlobalKey();
 
 void main() {
   runApp(
     new MaterialApp(
       title: "Hardware Key Demo",
-      routes: {
-        '/': (RouteArguments args) => const HardwareKeyDemo()
+      routes: <String, RouteBuilder>{
+        '/': (RouteArguments args) {
+          return new Scaffold(
+            toolBar: new ToolBar(
+              center: new Text("Hardware Key Demo")
+            ),
+            body: new Material(
+              child: new RawKeyboardDemo(
+                key: _key
+              )
+            )
+          );
+        }
       }
     )
   );
 }
 
-class HardwareKeyDemo extends StatefulComponent {
-  const HardwareKeyDemo();
-  HardwareKeyDemoState createState() => new HardwareKeyDemoState();
+class RawKeyboardDemo extends StatefulComponent {
+  RawKeyboardDemo({ GlobalKey key }) : super(key: key);
+
+  _HardwareKeyDemoState createState() => new _HardwareKeyDemoState();
 }
 
-class HardwareKeyDemoState extends State<HardwareKeyDemo> implements mojo.RawKeyboardListener {
+class _HardwareKeyDemoState extends State<RawKeyboardDemo> {
+  mojom.InputEvent _event = null;
 
-  mojo.InputEvent _event = null;
-
-  void initState() {
-    mojo.RawKeyboardServiceProxy rawKeyboardService = new mojo.RawKeyboardServiceProxy.unbound();
-    try {
-      shell.connectToService(null, rawKeyboardService);
-      mojo.RawKeyboardListenerStub listener = new mojo.RawKeyboardListenerStub.unbound()
-        ..impl = this;
-      rawKeyboardService.ptr.addListener(listener);
-    } finally {
-      rawKeyboardService.close();
-    }
-    super.initState();
-  }
-
-  void onKey(mojo.InputEvent event) {
+  void _handleKey(mojom.InputEvent event) {
     setState(() {
       _event = event;
     });
   }
 
-  Widget _buildBody() {
-    if (_event == null) {
-      return new Center(
-        child: new Text("Press a key", style: Typography.black.display1)
+  Widget build(BuildContext context)  {
+    bool focused = Focus.at(context);
+    Widget child;
+    if (!focused) {
+      child = new GestureDetector(
+        onTap: () {
+          Focus.moveTo(config.key);
+        },
+        child: new Center(
+          child: new Text('Tap to focus', style: Typography.black.display1)
+        )
+      );
+    } else if (_event == null) {
+      child = new Center(
+        child: new Text('Press a key', style: Typography.black.display1)
+      );
+    } else {
+      child = new Column(
+        children: <Widget>[
+          new Text('${_event.type}', style: Typography.black.body2),
+          new Text('${_event.keyData.keyCode}', style: Typography.black.display4)
+        ],
+        justifyContent: FlexJustifyContent.center
       );
     }
-    return new Column(
-      children: <Widget>[
-        new Text(
-          '${_event.type}',
-          style: Typography.black.body2
-        ),
-        new Text(
-          '${_event.keyData.keyCode}',
-          style: Typography.black.display4
-        )
-      ],
-      justifyContent: FlexJustifyContent.center
-    );
-  }
-
-  Widget build(BuildContext context)  {
-    return new Scaffold(
-      toolBar: new ToolBar(
-        center: new Text("Hardware Key Demo")
-      ),
-      body: new Material(
-        child: _buildBody()
-      )
+    return new RawKeyboardListener(
+      focused: focused,
+      onKey: _handleKey,
+      child: child
     );
   }
 }
