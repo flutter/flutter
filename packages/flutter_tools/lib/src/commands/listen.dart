09cca3b34c6f9acf91e41d7f77d8e7ca8782f855
Devon Carew <devoncarew@gmail.com>
2016-01-29 14:32:12 -0800

    massage the help text

--- packages/flutter_tools/lib/src/commands/listen.dart
+++ packages/flutter_tools/lib/src/commands/listen.dart
@@ -12,10 +12,11 @@ import 'start.dart';
 class ListenCommand extends StartCommandBase {
   final String name = 'listen';
   final String description =
-      'Listen for changes to files and reload the running app on all\n'
-      'connected devices (Android only). By default, only listens to\n'
-      '"./" and "./lib/". To listen to additional directories, list them on\n'
-      'the command line.';
+    'Listen for changes to files and reload the running app on all connected devices '
+    '(Android only).';
+  final String usageFooter =
+    'By default, only listens to "./" and "./lib/". To listen to additional directories, list them on\n'
+    'the command line.';
 
   /// Only run once.  Used for testing.
   final bool singleRun;
