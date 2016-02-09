9cea6c50f53b43a69c4fc849c2e9359081b07278
Hixie <github.com@spam.hixie.ch>
2016-02-08 18:01:24 -0800

    Fix drawer in accessibility mode
    The "leaf merge" feature was getting confused when we reset the
    SemanticsNode. We now separately track whether the node itself is marked
    as being merged vs whether we inherited that state, and we don't reset
    the inherited state until you're reattached or reserialised. In the
    latter case, we do a "just in time" clearing of the flag just like we
    previously did a "just in time" setting of the flag, except now the flag
    we're setting or clearing is the inherited flag not the actual flag.

---
+++ packages/flutter/test/widget/semantics_8_test.dart
@@ -0,0 +1,96 @@
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:test/test.dart';
+
+import 'test_semantics.dart';
+
+void main() {
+  test('Semantics 8 - Merging with reset', () {
+    testWidgets((WidgetTester tester) {
+      TestSemanticsListener client = new TestSemanticsListener();
+
+      tester.pumpWidget(
+        new MergeSemantics(
+          child: new Semantics(
+            container: true,
+            child: new Semantics(
+              container: true,
+              child: new Stack(
+                children: <Widget>[
+                  new Semantics(
+                    checked: true
+                  ),
+                  new Semantics(
+                    label: 'label'
+                  )
+                ]
+              )
+            )
+          )
+        )
+      );
+      expect(client.updates.length, equals(2));
+      expect(client.updates[0].id, equals(0));
+      expect(client.updates[0].flags.canBeTapped, isFalse);
+      expect(client.updates[0].flags.canBeLongPressed, isFalse);
+      expect(client.updates[0].flags.canBeScrolledHorizontally, isFalse);
+      expect(client.updates[0].flags.canBeScrolledVertically, isFalse);
+      expect(client.updates[0].flags.hasCheckedState, isTrue);
+      expect(client.updates[0].flags.isChecked, isTrue);
+      expect(client.updates[0].strings.label, equals('label'));
+      expect(client.updates[0].geometry.transform, isNull);
+      expect(client.updates[0].geometry.left, equals(0.0));
+      expect(client.updates[0].geometry.top, equals(0.0));
+      expect(client.updates[0].geometry.width, equals(800.0));
+      expect(client.updates[0].geometry.height, equals(600.0));
+      expect(client.updates[0].children.length, equals(0));
+      expect(client.updates[1], isNull);
+      client.updates.clear();
+
+      // switch the order of the inner Semantics node to trigger a reset
+      tester.pumpWidget(
+        new MergeSemantics(
+          child: new Semantics(
+            container: true,
+            child: new Semantics(
+              container: true,
+              child: new Stack(
+                children: <Widget>[
+                  new Semantics(
+                    label: 'label'
+                  ),
+                  new Semantics(
+                    checked: true
+                  )
+                ]
+              )
+            )
+          )
+        )
+      );
+      expect(client.updates.length, equals(2));
+      expect(client.updates[0].id, equals(0));
+      expect(client.updates[0].flags.canBeTapped, isFalse);
+      expect(client.updates[0].flags.canBeLongPressed, isFalse);
+      expect(client.updates[0].flags.canBeScrolledHorizontally, isFalse);
+      expect(client.updates[0].flags.canBeScrolledVertically, isFalse);
+      expect(client.updates[0].flags.hasCheckedState, isTrue);
+      expect(client.updates[0].flags.isChecked, isTrue);
+      expect(client.updates[0].strings.label, equals('label'));
+      expect(client.updates[0].geometry.transform, isNull);
+      expect(client.updates[0].geometry.left, equals(0.0));
+      expect(client.updates[0].geometry.top, equals(0.0));
+      expect(client.updates[0].geometry.width, equals(800.0));
+      expect(client.updates[0].geometry.height, equals(600.0));
+      expect(client.updates[0].children.length, equals(0));
+      expect(client.updates[1], isNull);
+      client.updates.clear();
+
+    });
+  });
+}
