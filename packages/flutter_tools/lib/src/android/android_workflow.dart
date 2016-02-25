6e0c4f0fcf156ba154d068be3f03cfb2f510b346
Devon Carew <devoncarew@google.com>
2016-02-25 09:25:06 -0800

    Update android_workflow.dart
    fix https://github.com/flutter/flutter/issues/2157

--- packages/flutter_tools/lib/src/android/android_workflow.dart
+++ packages/flutter_tools/lib/src/android/android_workflow.dart
@@ -28,7 +28,8 @@ class AndroidWorkflow extends Workflow {
     androidValidator.addValidator(new Validator(
       'Android SDK',
       description: 'enable development for Android devices',
-      resolution: 'Download at https://developer.android.com/sdk/',
+      resolution: 'Download from https://developer.android.com/sdk/ (or visit '
+        'https://flutter.io/setup/#android-setup for detailed instructions)',
       validatorFunction: sdkExists
     ));
 
