a021befd1921e0d55c84a3022dd89db95cb15977
Chinmay Garde <chinmaygarde@google.com>
2016-03-08 16:42:07 -0800

    Prepare a framework providing an embeddable Flutter view controller

---
+++ sky/shell/platform/ios/public/Flutter.h
@@ -0,0 +1,11 @@
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef FLUTTER_FLUTTER_H_
+#define FLUTTER_FLUTTER_H_
+
+#include "FlutterMacros.h"
+#include "FlutterViewController.h"
+
+#endif  // FLUTTER_FLUTTER_H_
