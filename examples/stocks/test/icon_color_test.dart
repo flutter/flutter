f7e6100b08fd114f44f5d7e2a76bf880f2b2ca1f
Devon Carew <devoncarew@google.com>
2016-04-01 11:32:02 -0700

    sort imports
    * sort imports

--- examples/stocks/test/icon_color_test.dart
+++ examples/stocks/test/icon_color_test.dart
@@ -4,12 +4,12 @@
 
 import 'dart:ui' as ui show window;
 
-import 'package:test/test.dart';
-import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:flutter_test/flutter_test.dart';
 import 'package:stocks/main.dart' as stocks;
 import 'package:stocks/stock_data.dart' as stock_data;
+import 'package:test/test.dart';
 
 Element findElementOfExactWidgetTypeGoingDown(Element node, Type targetType) {
   void walker(Element child) {
--- examples/stocks/test_driver/scroll_perf_test.dart
+++ examples/stocks/test_driver/scroll_perf_test.dart
@@ -3,6 +3,7 @@
 // found in the LICENSE file.
 
 import 'dart:async';
+
 import 'package:flutter_driver/flutter_driver.dart';
 import 'package:test/test.dart';
 
