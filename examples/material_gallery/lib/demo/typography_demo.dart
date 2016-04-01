78d5e42d92a85f7e06450b99c5aedff3728694d1
Hixie <github.com@spam.hixie.ch>
2016-04-01 14:10:58 -0700

    Tweak the typography demo to match the spec more
    - use capitalisation from the spec for the labels
    
    - use caption font instead of body1 font

--- examples/material_gallery/lib/demo/typography_demo.dart
+++ examples/material_gallery/lib/demo/typography_demo.dart
@@ -19,7 +19,7 @@ class TextStyleItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final ThemeData theme = Theme.of(context);
-    final TextStyle nameStyle = theme.textTheme.body1.copyWith(color: theme.textTheme.caption.color);
+    final TextStyle nameStyle = theme.textTheme.caption.copyWith(color: theme.textTheme.caption.color);
     return new Padding(
       padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 16.0),
       child: new Row(
@@ -30,7 +30,7 @@ class TextStyleItem extends StatelessWidget {
             child: new Text(name, style: nameStyle)
           ),
           new Flexible(
-            child: new Text(text, style: style)
+            child: new Text(text, style: style.copyWith(height: 1.0))
           )
         ]
       )
@@ -43,21 +43,21 @@ class TypographyDemo extends StatelessWidget {
   Widget build(BuildContext context) {
     final TextTheme textTheme = Theme.of(context).textTheme;
     final List<Widget> styleItems = <Widget>[
-      new TextStyleItem(name: 'display3', style: textTheme.display3, text: 'Regular 56sp'),
-      new TextStyleItem(name: 'display2', style: textTheme.display2, text: 'Regular 45sp'),
-      new TextStyleItem(name: 'display1', style: textTheme.display1, text: 'Regular 34sp'),
-      new TextStyleItem(name: 'headline', style: textTheme.headline, text: 'Regular 24sp'),
-      new TextStyleItem(name: 'title', style: textTheme.title, text: 'Medium 20sp'),
-      new TextStyleItem(name: 'subhead', style: textTheme.subhead, text: 'Regular 16sp'),
-      new TextStyleItem(name: 'body2', style: textTheme.body2, text: 'Medium 14sp'),
-      new TextStyleItem(name: 'body1', style: textTheme.body1, text: 'Reguluar 14sp'),
-      new TextStyleItem(name: 'caption', style: textTheme.caption, text: 'Regular 12sp'),
-      new TextStyleItem(name: 'button', style: textTheme.button, text: 'MEDIUM (ALL CAPS) 14sp')
+      new TextStyleItem(name: 'Display 3', style: textTheme.display3, text: 'Regular 56sp'),
+      new TextStyleItem(name: 'Display 2', style: textTheme.display2, text: 'Regular 45sp'),
+      new TextStyleItem(name: 'Display 1', style: textTheme.display1, text: 'Regular 34sp'),
+      new TextStyleItem(name: 'Headline', style: textTheme.headline, text: 'Regular 24sp'),
+      new TextStyleItem(name: 'Title', style: textTheme.title, text: 'Medium 20sp'),
+      new TextStyleItem(name: 'Subheading', style: textTheme.subhead, text: 'Regular 16sp'),
+      new TextStyleItem(name: 'Body 2', style: textTheme.body2, text: 'Medium 14sp'),
+      new TextStyleItem(name: 'Body 1', style: textTheme.body1, text: 'Reguluar 14sp'),
+      new TextStyleItem(name: 'Caption', style: textTheme.caption, text: 'Regular 12sp'),
+      new TextStyleItem(name: 'Button', style: textTheme.button, text: 'MEDIUM (ALL CAPS) 14sp')
     ];
 
     if (MediaQuery.of(context).size.width > 500.0) {
       styleItems.insert(0, new TextStyleItem(
-        name: 'display4',
+        name: 'Display 4',
         style: textTheme.display4,
         text: 'Light 112sp'
       ));
