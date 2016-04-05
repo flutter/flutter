4f39fd2169088ce5edb89719ddd4cb102a1b6d62
Devon Carew <devoncarew@google.com>
2016-04-05 10:09:52 -0700

    Merge pull request #2575 from flutter/devoncarew-patch-1
    Update jni.dart

--- sky/engine/bindings/jni/jni.dart
+++ sky/engine/bindings/jni/jni.dart
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+/// Invoke Java Native Interface APIs from Dart.
 library dart_jni;
 
 import 'dart:collection';
