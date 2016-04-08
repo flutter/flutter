f269f0102073eb8236c9180a46224a2348367806
Adam Barth <abarth@chromium.org>
2016-04-08 09:37:14 -0700

    Convert some Columns into Blocks
    These columns were secretly re-creating Block in a more complex way. Now we
    just use Block directly.

--- examples/material_gallery/lib/demo/list_demo.dart
+++ examples/material_gallery/lib/demo/list_demo.dart
@@ -38,9 +38,7 @@ class ListDemoState extends State<ListDemo> {
         decoration: new BoxDecoration(
           border: new Border(top: new BorderSide(color: Colors.black26))
         ),
-        child: new Column(
-          mainAxisAlignment: MainAxisAlignment.collapse,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
+        child: new Block(
           children: <Widget>[
             new ListItem(
               dense: true,
