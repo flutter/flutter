3605ef7b7048c0c21f7a22c0310c92efd672ec87
Devon Carew <devoncarew@google.com>
2016-04-08 08:57:24 -0700

    update library docs

--- packages/flutter_test/lib/flutter_test.dart
+++ packages/flutter_test/lib/flutter_test.dart
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-/// Testing library for flutter, built on top of package:test.
+/// Testing library for flutter, built on top of `package:test`.
 library flutter_test;
 
 export 'src/instrumentation.dart';
