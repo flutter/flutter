2204ef625d31401a3063c8fc6e2babdfa46a177a
Hixie <github.com@spam.hixie.ch>
2016-04-13 14:30:15 -0700

    Stop importing identifiers we don't use.

--- packages/flutter/lib/src/widgets/app.dart
+++ packages/flutter/lib/src/widgets/app.dart
@@ -3,7 +3,7 @@
 // found in the LICENSE file.
 
 import 'dart:async';
-import 'dart:ui' as ui show Locale, WindowPadding, window;
+import 'dart:ui' as ui show window;
 
 import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
--- packages/flutter/test/widget/asset_vendor_test.dart
+++ packages/flutter/test/widget/asset_vendor_test.dart
@@ -3,7 +3,7 @@
 // found in the LICENSE file.
 
 import 'dart:async';
-import 'dart:ui' as ui show Image, hashValues;
+import 'dart:ui' as ui show Image;
 
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter/rendering.dart';
