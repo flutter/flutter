29e0bf8d5c6f724f2619eed370426497df6de2b6
Hixie <github.com@spam.hixie.ch>
2016-04-15 11:38:24 -0700

    Use the same logic in flushPaint as flushLayout
    If two repaint boundaries mark themselves dirty, but the second one is a
    child of the first, then the second one will get repainted by the first
    and then when we come to paint it directly, we get confused because it
    isn't dirty any more.
    
    We ran into this in layout before. Apply the same fix. Also, apply the
    same fix to composition while we're at it.

---
+++ packages/flutter/test/rendering/repaint_boundary_test.dart
@@ -0,0 +1,37 @@
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/rendering.dart';
+import 'package:test/test.dart';
+
+import 'rendering_tester.dart';
+
+void main() {
+  test("nested repaint boundaries - smoke test", () {
+    RenderOpacity a, b, c;
+    a = new RenderOpacity(
+      opacity: 1.0,
+      child: new RenderRepaintBoundary(
+        child: b = new RenderOpacity(
+          opacity: 1.0,
+          child: new RenderRepaintBoundary(
+            child: c = new RenderOpacity(
+              opacity: 1.0
+            )
+          )
+        )
+      )
+    );
+    layout(a, phase: EnginePhase.flushSemantics);
+    c.opacity = 0.9;
+    layout(a, phase: EnginePhase.flushSemantics);
+    a.opacity = 0.8;
+    c.opacity = 0.8;
+    layout(a, phase: EnginePhase.flushSemantics);
+    a.opacity = 0.7;
+    b.opacity = 0.7;
+    c.opacity = 0.7;
+    layout(a, phase: EnginePhase.flushSemantics);
+  });
+}
