845c6aa0754d9932e63ee0203e22b0f535a7f4d5
Adam Barth <abarth@chromium.org>
2016-04-25 16:43:13 -0700

    Block should work inside LazyBlock
    Previously we were locking down the state even when calling layout in
    LazyBlock. Now we lock only when building children. Making this work well
    involved moving the catch out of lockState and into the few callers who
    actually wanted it.
    
    Fixes #3534

---
+++ packages/flutter/test/widget/lazy_block_test.dart
@@ -0,0 +1,34 @@
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter/material.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('Block inside LazyBlock', () {
+    testWidgets((WidgetTester tester) {
+      tester.pumpWidget(new LazyBlock(
+        delegate: new LazyBlockChildren(
+          children: <Widget>[
+            new Block(
+              children: <Widget>[
+                new Text('1'),
+                new Text('2'),
+                new Text('3'),
+              ]
+            ),
+            new Block(
+              children: <Widget>[
+                new Text('4'),
+                new Text('5'),
+                new Text('6'),
+              ]
+            ),
+          ]
+        )
+      ));
+    });
+  });
+}
