0b09d3f7fccd445ab9d9ce59d6f21d2b28619fed
Devon Carew <devoncarew@google.com>
2016-05-01 15:36:31 -0700

    fix the dev/dartdoc.dart script

--- dev/dartdoc.dart
+++ dev/dartdoc.dart
@@ -4,7 +4,22 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'tools/dartdoc.dart' as dartdoc;
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
 
 // TODO (devoncarew): Remove this once the chromium bot is updated.
-main(List<String> args) => dartdoc.main(args);
+Future<Null> main(List<String> args) async {
+  Process.runSync('pub', <String>['get'], workingDirectory: 'dev/tools');
+  Process process = await Process.start('dart', <String>['dartdoc.dart'],
+    workingDirectory: 'dev/tools');
+  process.stdout
+    .transform(UTF8.decoder)
+    .transform(const LineSplitter())
+    .listen(print);
+  process.stderr
+    .transform(UTF8.decoder)
+    .transform(const LineSplitter())
+    .listen(print);
+  exit(await process.exitCode);
+}
