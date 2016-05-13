7e4a91f3406a6f1182371d2445b33d6882143006
Seth Ladd <sethladd@gmail.com>
2016-05-13 12:27:06 -0700

    add a webmaster tools verification file (#3902)
    * add a webmaster tools verification file
    
    * also put the verification file at the root of our bucket
    
    * add comment for when we can simplify this script

--- infra/docs.sh
+++ infra/docs.sh
@@ -8,7 +8,12 @@ pub global activate dartdoc
 (cd dev/tools; pub get)
 dart dev/tools/dartdoc.dart
 
+cp dev/docs/google2ed1af765c529f57.html dev/docs/doc/api
+
 # Upload the docs.
 if [ "$1" = "--upload" ]; then
+  # TODO: delete this line when we publish our API docs into the
+  # root of the bucket
+  gsutil cp dev/docs/google2ed1af765c529f57.html gs://docs.flutter.io
   gsutil -m rsync -d -r dev/docs/doc/api gs://docs.flutter.io/flutter
 fi
