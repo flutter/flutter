850edd883df9192cd9b4e5c3c589bbc0f90e6608
Seth Ladd <sethladd@google.com>
2016-05-13 11:30:36 -0700

    also put the verification file at the root of our bucket

--- infra/docs.sh
+++ infra/docs.sh
@@ -12,5 +12,6 @@ cp dev/docs/google2ed1af765c529f57.html dev/docs/doc/api
 
 # Upload the docs.
 if [ "$1" = "--upload" ]; then
+  gsutil cp dev/docs/google2ed1af765c529f57.html gs://docs.flutter.io
   gsutil -m rsync -d -r dev/docs/doc/api gs://docs.flutter.io/flutter
 fi
