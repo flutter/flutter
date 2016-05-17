4e82be767bdd11b3919816f0f1551ed758c2ced8
Seth Ladd <sethladd@google.com>
2016-05-16 19:00:41 -0700

    recurse on the cp

--- infra/docs.sh
+++ infra/docs.sh
@@ -19,10 +19,13 @@ if [ "$1" = "--upload" ]; then
   # This isn't great, because we're uploading our files twice. But,
   # we're ensuring we're not leaving any deleted files on the server.
   # And we're ensuring we're compressing text files.
+  # Unfortunately, rsync can't set Content-Encoding for a subset of files,
+  # and rsync can't run in a "only delete files on server that are no
+  # longer in the local source dir".
 
   # Ensure files on server are deleted when no longer in local generated source.
   gsutil -m rsync -d -r dev/docs/doc/ gs://docs.flutter.io/
 
   # Ensure compressable files are gzipped and then stored.
-  gsutil -m cp -z "js,json,html,css" dev/docs/doc/ gs://docs.flutter.io/
+  gsutil -m cp -r -z "js,json,html,css" dev/docs/doc/ gs://docs.flutter.io/
 fi
