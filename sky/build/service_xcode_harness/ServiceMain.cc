1db1d1507d5ff56701d0432df1e4bf95d5d1e721
Chinmay Garde <chinmaygarde@gmail.com>
2016-05-18 11:26:33 -0700

    Create an Xcode project template for dynamic service creation. (#2695)
    * Create an Xcode project template for dynamic service creation.
    
    The `create_native_service_sdk.py` will be run on the buildbot similar
    to `create_ios_sdk.py`. This will create an Xcode project with both arm
    and x64 assets that the user can use to fill in the service
    implementation.
    
    * Add the complete_static_lib property to sdk_lib_archive in
    dynamic/BUILD.gn

---
+++ sky/build/service_xcode_harness/ServiceMain.cc
@@ -0,0 +1,11 @@
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "mojo/public/cpp/application/interface_factory.h"
+#include "mojo/public/cpp/bindings/strong_binding.h"
+
+void FlutterServicePerform(mojo::ScopedMessagePipeHandle client_handle,
+                           const mojo::String& service_name) {
+  // Your service implementation goes here.
+}
