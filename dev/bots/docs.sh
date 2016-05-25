604c75f1ed90291cb79589193d6580bb0b8206d9
Seth Ladd <sethladd@gmail.com>
2016-05-25 15:19:06 -0700

    compress text assets for docs, when uploading (#3926)
    * compress text assets for docs, when uploading
    
    * reorder args
    
    * ensure old files are deleted
    
    * recurse on the cp
    
    * set source path correctly

--- dev/bots/docs.sh
+++ dev/bots/docs.sh
@@ -16,5 +16,16 @@ cp dev/docs/google2ed1af765c529f57.html dev/docs/doc
 
 # Upload the docs.
 if [ "$1" = "--upload" ]; then
+  # This isn't great, because we're uploading our files twice. But,
+  # we're ensuring we're not leaving any deleted files on the server.
+  # And we're ensuring we're compressing text files.
+  # Unfortunately, rsync can't set Content-Encoding for a subset of files,
+  # and rsync can't run in a "only delete files on server that are no
+  # longer in the local source dir".
+
+  # Ensure files on server are deleted when no longer in local generated source.
   gsutil -m rsync -d -r dev/docs/doc/ gs://docs.flutter.io/
+
+  # Ensure compressable files are gzipped and then stored.
+  gsutil -m cp -r -z "js,json,html,css" dev/docs/doc/* gs://docs.flutter.io/
 fi
