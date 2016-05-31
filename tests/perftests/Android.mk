08bfef04afed092b6a6a8a5fc5d1e1165c5304ae
Seigo Nonaka <nona@google.com>
2016-05-31 15:25:21 +0900

    Introduce initial minikin perftest
    Bug: 28992567
    Change-Id: Id14cc86761d6c5b408262fe8684e2b293a420a4f

---
+++ tests/perftests/Android.mk
@@ -0,0 +1,31 @@
+#
+# Copyright (C) 2016 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+LOCAL_PATH := $(call my-dir)
+
+perftest_src_files := \
+  FontLanguage.cpp \
+  main.cpp
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := minikin_perftests
+LOCAL_CPPFLAGS := -Werror -Wall -Wextra
+LOCAL_SRC_FILES := $(perftest_src_files)
+LOCAL_STATIC_LIBRARIES := libminikin
+LOCAL_C_INCLUDES := \
+  $(LOCAL_PATH)/../../libs/minikin \
+  external/harfbuzz_ng/src
+include $(BUILD_NATIVE_BENCHMARK)
