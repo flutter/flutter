bc826d3268d6e3d65332787bc4de5e8b1dcd93e1
Todd Volkert <tvolkert@users.noreply.github.com>
2016-05-31 17:51:45 -0700

    Fix system ui overlays on Android. (#2728)
    The logic was reversed -- we should only go into fullscreen if the
    user didn't specify a TOP overlay, and we should only hide the
    system ui navigation if the user didn't specify a BOTTOM overlay.

--- sky/services/platform/src/org/domokit/platform/SystemChromeImpl.java
+++ sky/services/platform/src/org/domokit/platform/SystemChromeImpl.java
@@ -60,10 +60,10 @@ public class SystemChromeImpl implements SystemChrome {
         int flags = View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
                     View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN;
 
-        if ((overlays & SystemUiOverlay.TOP) != 0) {
+        if ((overlays & SystemUiOverlay.TOP) == 0) {
             flags |= View.SYSTEM_UI_FLAG_FULLSCREEN;
         }
-        if ((overlays & SystemUiOverlay.BOTTOM) != 0) {
+        if ((overlays & SystemUiOverlay.BOTTOM) == 0) {
             flags |= View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
                      View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
         }
