d3e6f94394ecc44780578f21ad8ec76059fb291e
Todd Volkert <tvolkert@google.com>
2016-06-07 14:02:17 -0700

    Missed a refactor point

--- packages/flutter/lib/src/material/app.dart
+++ packages/flutter/lib/src/material/app.dart
@@ -177,8 +177,7 @@ class _MaterialAppState extends State<MaterialApp> {
       child: new WidgetsApp(
         title: config.title,
         textStyle: _errorTextStyle,
-        brightness: theme.brightness == Brightness.light
-          ? Brightness.light : Brightness.dark,
+        brightness: theme.brightness,
         color: theme?.primaryColor ?? Colors.blue[500], // blue[500] is the primary color of the default theme
         navigatorObserver: _heroController,
         onGenerateRoute: _onGenerateRoute,
