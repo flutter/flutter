a89c08ae496e402b46a71e2f86941c1f3968b2a5
Seigo Nonaka <nona@google.com>
2016-06-16 16:58:14 +0900

    Introduce FontCollection perftest
    This CL introduces performance tests for FontCollection.
    
    To support TTC file in /system/fonts, this CL also extends FontTestUtils
    
    Bug:29142734
    Change-Id: I9d8ad24ca55f61031b85623ab7c26234239e4f41

--- engine/src/flutter/tests/perftests/Android.mk
+++ engine/src/flutter/tests/perftests/Android.mk
@@ -18,7 +18,10 @@ LOCAL_PATH := $(call my-dir)
 
 perftest_src_files := \
   ../util/FileUtils.cpp \
+  ../util/FontTestUtils.cpp \
+  ../util/MinikinFontForTest.cpp \
   ../util/UnicodeUtils.cpp \
+  FontCollection.cpp \
   FontLanguage.cpp \
   GraphemeBreak.cpp \
   Hyphenator.cpp \
@@ -29,12 +32,20 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := minikin_perftests
 LOCAL_CPPFLAGS := -Werror -Wall -Wextra
 LOCAL_SRC_FILES := $(perftest_src_files)
-LOCAL_STATIC_LIBRARIES := libminikin
+LOCAL_STATIC_LIBRARIES := \
+  libminikin \
+  libxml2
+
 LOCAL_SHARED_LIBRARIES := \
+  libharfbuzz_ng \
   libicuuc \
-  liblog
+  liblog \
+  libskia
+
 LOCAL_C_INCLUDES := \
   $(LOCAL_PATH)/../ \
   $(LOCAL_PATH)/../../libs/minikin \
-  external/harfbuzz_ng/src
+  external/harfbuzz_ng/src \
+  external/libxml2/include
+
 include $(BUILD_NATIVE_BENCHMARK)
