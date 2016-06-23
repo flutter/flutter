4be730ac9a914c9b91e27a0ab6c4439fd67185cd
Adam Barth <abarth@chromium.org>
2016-06-23 11:56:20 -0700

    Only download cloud SDK if we need it on Travis

--- dev/bots/setup.sh
+++ dev/bots/setup.sh
@@ -5,8 +5,10 @@ echo $KEY_FILE | base64 --decode > ../gcloud_key_file.json
 
 set -x
 
-export CLOUDSDK_CORE_DISABLE_PROMPTS=1
-curl https://sdk.cloud.google.com | bash
+if [ -n "$TRAVIS" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+  export CLOUDSDK_CORE_DISABLE_PROMPTS=1
+  curl https://sdk.cloud.google.com | bash
+fi
 
 # disable analytics on the bots and download Flutter dependencies
 ./bin/flutter config --no-analytics
