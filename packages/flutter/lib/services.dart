490622b4da85c59e3d64bde33d622c40387e2dc6
Chinmay Garde <chinmaygarde@gmail.com>
2016-06-24 14:51:03 -0700

    Add a wrapper for accessing the URL launcher service. (#4688)

--- packages/flutter/lib/services.dart
+++ packages/flutter/lib/services.dart
@@ -30,3 +30,4 @@ export 'src/services/path_provider.dart';
 export 'src/services/shell.dart';
 export 'src/services/system_chrome.dart';
 export 'src/services/system_sound.dart';
+export 'src/services/url_launcher.dart';
