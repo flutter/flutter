9c0c0225a69fa8fa4344d6c32180694a764b3006
Adam Barth <github@adambarth.com>
2016-06-24 20:46:17 -0700

    Add a test for GridPaper (#4754)
    Previously this widget was completely untested.

---
+++ packages/flutter/test/widget/grid_paper_test.dart
@@ -0,0 +1,17 @@
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
+
+void main() {
+  testWidgets('GridPaper control test', (WidgetTester tester) async {
+    await tester.pumpWidget(new GridPaper());
+    List<Layer> layers1 = tester.layers;
+    await tester.pumpWidget(new GridPaper());
+    List<Layer> layers2 = tester.layers;
+    expect(layers1, equals(layers2));
+  });
+}
