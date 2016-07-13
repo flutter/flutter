ca72520d93e6dc7dade0980e564f1b2b8169ff01
Jason Simmons <jason-simmons@users.noreply.github.com>
2016-07-13 14:39:53 -0700

    Provide a PlatformView implementation for Linux (#2796)

--- sky/shell/platform/linux/platform_view_linux.cc
+++ sky/shell/platform/linux/platform_view_linux.cc
@@ -2,13 +2,36 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "sky/shell/platform_view.h"
+#include "sky/shell/platform/linux/platform_view_linux.h"
 
 namespace sky {
 namespace shell {
 
-PlatformView* PlatformView::Create(const Config& config) {
-  return new PlatformView(config);
+PlatformView* PlatformView::Create(const Config& config,
+                                   SurfaceConfig surface_config) {
+  return new PlatformViewLinux(config, surface_config);
+}
+
+PlatformViewLinux::PlatformViewLinux(const Config& config,
+                                    SurfaceConfig surface_config)
+    : PlatformView(config, surface_config), weak_factory_(this) {}
+
+PlatformViewLinux::~PlatformViewLinux() {}
+
+base::WeakPtr<sky::shell::PlatformView> PlatformViewLinux::GetWeakViewPtr() {
+  return weak_factory_.GetWeakPtr();
+}
+
+uint64_t PlatformViewLinux::DefaultFramebuffer() const {
+  return 0;
+}
+
+bool PlatformViewLinux::ContextMakeCurrent() {
+  return false;
+}
+
+bool PlatformViewLinux::SwapBuffers() {
+  return false;
 }
 
 }  // namespace shell
