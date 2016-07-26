2b18a40f0bc07ddbd5824fc26fe2c53bba9159d2
Chinmay Garde <chinmaygarde@gmail.com>
2016-07-26 16:26:04 -0700

    Utilities for creating texture backed SkImages. (#2829)

---
+++ flow/open_gl.h
@@ -0,0 +1,28 @@
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef FLOW_OPEN_GL_H_
+#define FLOW_OPEN_GL_H_
+
+#include "build/build_config.h"
+
+#if OS_IOS
+
+#include <OpenGLES/ES2/gl.h>
+
+#elif OS_MACOSX
+
+#include <OpenGL/gl.h>
+
+#elif OS_ANDROID
+
+#include <GLES2/gl2.h>
+
+#else
+
+#error OpenGL headers not found for this platform.
+
+#endif
+
+#endif  // FLOW_OPEN_GL_H_
