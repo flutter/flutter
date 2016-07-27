8219ab61abfc85f996344253597f385bf4ab4a21
Adam Barth <github@adambarth.com>
2016-07-27 13:49:30 -0700

    Don't rebuild routes on the second animation frame (#5097)
    Previously we would rebuild every route on the second animation frame to
    rejigger the offstage bit and the animations. Now we build the page once and
    update the offstage bit in place and update the animations using
    ProxyAnimations.

--- packages/flutter/test/material/app_test.dart
+++ packages/flutter/test/material/app_test.dart
@@ -72,4 +72,49 @@ void main() {
     expect(state1, equals(state2));
     expect(state2.marker, equals('original'));
   });
+
+  testWidgets('Do not rebuild page on the second frame of the route transition', (WidgetTester tester) async {
+    int buildCounter = 0;
+    await tester.pumpWidget(
+      new MaterialApp(
+        home: new Builder(
+          builder: (BuildContext context) {
+            return new Material(
+              child: new RaisedButton(
+                child: new Text('X'),
+                onPressed: () { Navigator.of(context).pushNamed('/next'); }
+              )
+            );
+          }
+        ),
+        routes: <String, WidgetBuilder>{
+          '/next': (BuildContext context) {
+            return new Builder(
+              builder: (BuildContext context) {
+                ++buildCounter;
+                return new Container();
+              }
+            );
+          }
+        }
+      )
+    );
+
+    expect(buildCounter, 0);
+    await tester.tap(find.text('X'));
+    expect(buildCounter, 0);
+    await tester.pump();
+    expect(buildCounter, 1);
+    await tester.pump(const Duration(milliseconds: 10));
+    expect(buildCounter, 1);
+    await tester.pump(const Duration(milliseconds: 10));
+    expect(buildCounter, 1);
+    await tester.pump(const Duration(milliseconds: 10));
+    expect(buildCounter, 1);
+    await tester.pump(const Duration(milliseconds: 10));
+    expect(buildCounter, 1);
+    await tester.pump(const Duration(seconds: 1));
+    expect(buildCounter, 2);
+  });
+
 }
