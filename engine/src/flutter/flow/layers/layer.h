3fbc245f7edda005b5404833c37e56c40b1175b2
Adam Barth <github@adambarth.com>
2016-08-01 22:21:37 -0700

    Remove skia::RefPtr (flutter/engine#2851)
    Also, remove the hacks that integrate Skia ref counting with WTF. Now we
    just use sk_sp everywhere for managing Skia ref counts.

--- engine/src/flutter/flow/layers/layer.h
+++ engine/src/flutter/flow/layers/layer.h
@@ -13,7 +13,6 @@
 #include "glue/trace_event.h"
 #include "lib/ftl/logging.h"
 #include "lib/ftl/macros.h"
-#include "skia/ext/refptr.h"
 #include "third_party/skia/include/core/SkCanvas.h"
 #include "third_party/skia/include/core/SkColor.h"
 #include "third_party/skia/include/core/SkColorFilter.h"
