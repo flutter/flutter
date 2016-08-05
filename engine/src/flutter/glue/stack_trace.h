2652ee26547d69804270f18775ef7f95431ec37c
Adam Barth <github@adambarth.com>
2016-08-04 23:31:21 -0700

    Add glue::PrintStackTrace() (flutter/engine#2869)
    We don't have a way to print stack traces on Fuchsia yet. This patch
    isolates this base dependency in //glue.

---
+++ engine/src/flutter/glue/stack_trace.h
@@ -0,0 +1,14 @@
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef GLUE_STACK_TRACE_H_
+#define GLUE_STACK_TRACE_H_
+
+namespace glue {
+
+void PrintStackTrace();
+
+}  // namespace glue
+
+#endif  // GLUE_STACK_TRACE_H_
