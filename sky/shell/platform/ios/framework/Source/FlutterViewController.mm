78d70176a3f3f5c99785589929468189b0f44651
Adam Barth <abarth@chromium.org>
2016-08-09 16:38:59 -0700

    Fix iOS build

--- sky/shell/platform/ios/framework/Source/FlutterViewController.mm
+++ sky/shell/platform/ios/framework/Source/FlutterViewController.mm
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import "sky/shell/platform/ios/framework/Headers/FlutterViewController.h"
+#import "flutter/sky/shell/platform/ios/framework/Headers/FlutterViewController.h"
 
 #include "base/bind.h"
 #include "base/mac/scoped_block.h"
