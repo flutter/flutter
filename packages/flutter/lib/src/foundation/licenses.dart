a194e59390838dafd659a0a1a45f546d1a997b0f
Ian Hickson <ian@hixie.ch>
2016-08-09 20:03:14 -0700

    Support multilicense LICENSE files generically. (#5310)
    Also, add a "flutter build flx --report-licensed-packages" option for
    when you need to get the list of the packages affected by licenses.

--- packages/flutter/lib/src/foundation/licenses.dart
+++ packages/flutter/lib/src/foundation/licenses.dart
@@ -243,8 +243,14 @@ class LicenseEntryWithLineBreaks extends LicenseEntry {
 /// The flutter tool will automatically collect the contents of all the LICENSE
 /// files found at the root of each package into a single LICENSE file in the
 /// default asset bundle. Each license in that file is separated from the next
-/// by a line of eighty hyphens (`-`). The `services` package registers a
-/// license collector that splits that file and adds each entry to the registry.
+/// by a line of eighty hyphens (`-`), and begins with a list of package names
+/// that the license applies to, one to a line, separated from the next by a
+/// blank line. The `services` package registers a license collector that splits
+/// that file and adds each entry to the registry.
+///
+/// The LICENSE files in each package can either consist of a single license, or
+/// can be in the format described above. In the latter case, each component
+/// license and list of package names is merged independently.
 class LicenseRegistry {
   LicenseRegistry._();
 
