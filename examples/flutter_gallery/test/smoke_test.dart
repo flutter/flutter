9c15407ba9a86c2c25885aea2932bb6be3d71098
Ian Hickson <ian@hixie.ch>
2016-08-11 13:40:26 -0700

    When a list is scrolling, children can't be tapped (#5222) (#5348)

--- examples/flutter_gallery/test/smoke_test.dart
+++ examples/flutter_gallery/test/smoke_test.dart
@@ -9,6 +9,8 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_gallery/gallery/item.dart' show GalleryItem, kAllGalleryItems;
 import 'package:flutter_gallery/main.dart' as flutter_gallery_main;
 
+const String kCaption = 'Flutter Gallery';
+
 final List<String> demoCategories = new LinkedHashSet<String>.from(
   kAllGalleryItems.map((GalleryItem item) => item.category)
 ).toList();
@@ -43,8 +45,9 @@ Future<Null> smokeDemo(WidgetTester tester, String routeName) async {
 
   await tester.tap(menuItem);
   await tester.pump(); // Launch the demo.
-  await tester
-      .pump(const Duration(seconds: 1)); // Wait until the demo has opened.
+  await tester.pump(const Duration(seconds: 1)); // Wait until the demo has opened.
+
+  expect(find.text(kCaption), findsNothing);
 
   // Go back
   if (routeName == '/pesto') {
@@ -64,15 +67,13 @@ Future<Null> smokeDemo(WidgetTester tester, String routeName) async {
 }
 
 void main() {
-  TestWidgetsFlutterBinding binding =
-      TestWidgetsFlutterBinding.ensureInitialized();
-  if (binding is LiveTestWidgetsFlutterBinding) binding.allowAllFrames = true;
-
   testWidgets('Flutter Gallery app smoke test', (WidgetTester tester) async {
     flutter_gallery_main
         .main(); // builds the app and schedules a frame but doesn't trigger one
     await tester.pump(); // see https://github.com/flutter/flutter/issues/1865
     await tester.pump(); // triggers a frame
+
+    expect(find.text(kCaption), findsOneWidget);
 
     final List<double> scrollDeltas = new List<double>();
     double previousY = tester.getTopRight(find.text(demoCategories[0])).y;
@@ -87,15 +88,17 @@ void main() {
       final String routeName = routeNames[i];
       await smokeDemo(tester, routeName);
       await tester.scroll(findGalleryItemByRouteName(tester, routeName), new Offset(0.0, scrollDeltas[i]));
-      await tester.pump();
+      await tester.pump(); // start the scroll
+      await tester.pump(const Duration(milliseconds: 500)); // wait for overscroll to timeout, if necessary
+      await tester.pump(const Duration(milliseconds: 2000)); // wait for overscroll to fade away, if necessary
+      tester.binding.debugAssertNoTransientCallbacks('A transient callback was still active after leaving route $routeName');
     }
 
     Finder navigationMenuButton = findNavigationMenuButton(tester);
     expect(navigationMenuButton, findsOneWidget);
     await tester.tap(navigationMenuButton);
     await tester.pump(); // Start opening drawer.
-    await tester
-        .pump(const Duration(seconds: 1)); // Wait until it's really opened.
+    await tester.pump(const Duration(seconds: 1)); // Wait until it's really opened.
 
     // switch theme
     await tester.tap(find.text('Dark'));
