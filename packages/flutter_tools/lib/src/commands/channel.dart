406ed245259526ab1efe24b4e86fd5062430c2df
Dan Rubel <danrubel@google.com>
2016-08-23 16:56:02 -0400

    add flutter channel command fixes https://github.com/flutter/flutter/issues/2513

---
+++ packages/flutter_tools/lib/src/commands/channel.dart
@@ -0,0 +1,72 @@
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'dart:async';
+
+import '../base/process.dart';
+import '../cache.dart';
+import '../globals.dart';
+import '../runner/flutter_command.dart';
+
+class ChannelCommand extends FlutterCommand {
+  @override
+  final String name = 'channel';
+
+  @override
+  final String description = 'List or switch flutter channels.';
+
+  @override
+  String get invocation => '${runner.executableName} $name [<channel-name>]';
+
+  @override
+  bool get requiresProjectRoot => false;
+
+  @override
+  Future<int> runInProject() async {
+    switch (argResults.rest.length) {
+      case 0:
+        return await _listChannels();
+      case 1:
+        return await _switchChannel(argResults.rest[0]);
+      default:
+        printStatus('Too many arguments.');
+        printStatus(usage);
+        return 2;
+    }
+  }
+
+  Future<int> _listChannels() async {
+    String currentBranch;
+    await runCommandAndStreamOutput(
+      <String>['git', 'rev-parse', '--abbrev-ref', 'HEAD'],
+      workingDirectory: Cache.flutterRoot,
+      mapFunction: (String line) {
+        currentBranch = line;
+        return null;
+      },
+    );
+
+    printStatus('Flutter channels:');
+    return runCommandAndStreamOutput(
+      <String>['git', 'branch', '-r'],
+      workingDirectory: Cache.flutterRoot,
+      mapFunction: (String line) {
+        List<String> split = line.split('/');
+        if (split.length < 2) return null;
+        String branchName = split[1];
+        if (branchName.startsWith('HEAD')) return null;
+        if (branchName == currentBranch) return '* $branchName';
+        return '  $branchName';
+      },
+    );
+  }
+
+  Future<int> _switchChannel(String branchName) {
+    printStatus('Switching to flutter channel named $branchName');
+    return runCommandAndStreamOutput(
+      <String>['git', 'checkout', branchName],
+      workingDirectory: Cache.flutterRoot,
+    );
+  }
+}
---
+++ packages/flutter_tools/test/channel_test.dart
@@ -0,0 +1,44 @@
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:args/command_runner.dart';
+import 'package:flutter_tools/src/base/context.dart';
+import 'package:flutter_tools/src/base/logger.dart';
+import 'package:flutter_tools/src/cache.dart';
+import 'package:flutter_tools/src/commands/channel.dart';
+import 'package:flutter_tools/src/device.dart';
+import 'package:test/test.dart';
+
+import 'src/common.dart';
+import 'src/context.dart';
+
+void main() {
+  group('channel', () {
+    Logger oldLogger;
+    BufferLogger logger;
+
+    setUp(() {
+      Cache.disableLocking();
+      logger = new BufferLogger();
+      Cache.flutterRoot = '../..';
+      context[DeviceManager] = new MockDeviceManager();
+      oldLogger = context[Logger];
+      context[Logger] = logger;
+    });
+
+    tearDown(() {
+      context[Logger] = oldLogger;
+    });
+
+    test('list', () async {
+      ChannelCommand command = new ChannelCommand();
+      CommandRunner runner = createTestCommandRunner(command);
+      expect(await runner.run(<String>['channel']), 0);
+      expect(logger.errorText, hasLength(0));
+      expect(logger.statusText, contains('channels'));
+      expect(logger.statusText, contains('master'));
+      expect(logger.statusText, contains('* ')); // current channel mark
+    });
+  });
+}
