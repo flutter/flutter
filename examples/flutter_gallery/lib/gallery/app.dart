0c2708b3414117ce470531db04544d5dcbdd6cf9
Collin Jackson <jackson@google.com>
2016-08-24 08:43:06 -0700

    Add update dialog to gallery, open source part of #4626

--- examples/flutter_gallery/lib/gallery/app.dart
+++ examples/flutter_gallery/lib/gallery/app.dart
@@ -7,11 +7,12 @@ import 'package:flutter/scheduler.dart' show timeDilation;
 
 import 'item.dart';
 import 'home.dart';
+import 'updates.dart';
 
 final Map<String, WidgetBuilder> _kRoutes = new Map<String, WidgetBuilder>.fromIterable(
   kAllGalleryItems,
   key: (GalleryItem item) => item.routeName,
-  value: (GalleryItem item) => item.buildRoute
+  value: (GalleryItem item) => item.buildRoute,
 );
 
 final ThemeData _kGalleryLightTheme = new ThemeData(
@@ -21,11 +22,13 @@ final ThemeData _kGalleryLightTheme = new ThemeData(
 
 final ThemeData _kGalleryDarkTheme = new ThemeData(
   brightness: Brightness.dark,
-  primarySwatch: Colors.lightBlue
+  primarySwatch: Colors.lightBlue,
 );
 
 class GalleryApp extends StatefulWidget {
-  GalleryApp({ Key key }) : super(key: key);
+  GalleryApp({this.updateUrlFetcher, Key key}) : super(key: key);
+
+  final UpdateUrlFetcher updateUrlFetcher;
 
   @override
   GalleryAppState createState() => new GalleryAppState();
@@ -47,27 +50,44 @@ class GalleryAppState extends State<GalleryApp> {
 
     Widget home = new GalleryHome(
       useLightTheme: _useLightTheme,
-      onThemeChanged: (bool value) { setState(() { _useLightTheme = value; }); },
+      onThemeChanged: (bool value) {
+        setState(() {
+          _useLightTheme = value;
+        });
+      },
       showPerformanceOverlay: _showPerformanceOverlay,
-      onShowPerformanceOverlayChanged: (bool value) { setState(() { _showPerformanceOverlay = value; }); },
+      onShowPerformanceOverlayChanged: (bool value) {
+        setState(() {
+          _showPerformanceOverlay = value;
+        });
+      },
       timeDilation: timeDilation,
-      onTimeDilationChanged: (double value) { setState(() { timeDilation = value; }); }
+      onTimeDilationChanged: (double value) {
+        setState(() {
+          timeDilation = value;
+        });
+      },
     );
 
-    if (showPreviewBanner) {
+    if (showPreviewBanner)
       home = new Banner(
         message: 'PREVIEW',
         location: BannerLocation.topRight,
-        child: home
+        child: home,
       );
-    }
+
+    if (config.updateUrlFetcher != null)
+      home = new Updater(
+        updateUrlFetcher: config.updateUrlFetcher,
+        child: home,
+      );
 
     return new MaterialApp(
       title: 'Flutter Gallery',
       theme: _useLightTheme ? _kGalleryLightTheme : _kGalleryDarkTheme,
       showPerformanceOverlay: _showPerformanceOverlay,
       routes: _kRoutes,
-      home: home
+      home: home,
     );
   }
 }
---
+++ examples/flutter_gallery/test/update_test.dart
@@ -0,0 +1,57 @@
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_gallery/gallery/app.dart';
+
+// Example of an updateUrlFetcher that only prompts once a day
+DateTime lastUpdateCheck;
+Future<String> mockUpdateUrlFetcher() {
+  if (lastUpdateCheck != null &&
+      new DateTime.now().difference(lastUpdateCheck) < new Duration(days: 1)) {
+    return null; // We already checked for updates recently
+  }
+  lastUpdateCheck = new DateTime.now();
+
+  // A real implementation would connect to the network to retrieve this value
+  return new Future<String>.value('http://www.example.com/');
+}
+
+Finder byTooltip(WidgetTester tester, String message) {
+  return find.byWidgetPredicate((Widget widget) {
+    return widget is Tooltip && widget.message == message;
+  });
+}
+
+Finder findBackButton(WidgetTester tester) => byTooltip(tester, 'Back');
+
+void main() {
+  TestWidgetsFlutterBinding binding = TestWidgetsFlutterBinding.ensureInitialized();
+  if (binding is LiveTestWidgetsFlutterBinding) binding.allowAllFrames = true;
+
+  // Regression test for https://github.com/flutter/flutter/pull/5168
+  testWidgets('update dialog', (WidgetTester tester) async {
+    await tester.pumpWidget(new GalleryApp(updateUrlFetcher: mockUpdateUrlFetcher));
+    await tester.pump(); // see https://github.com/flutter/flutter/issues/1865
+    await tester.pump(); // triggers a frame
+
+    expect(find.text('UPDATE'), findsOneWidget);
+
+    await tester.tap(find.text('NO THANKS'));
+    await tester.pump();
+
+    await tester.tap(find.text('Shrine'));
+    await tester.pump(); // Launch shrine
+    await tester.pump(const Duration(seconds: 1)); // transition is complete
+
+    Finder backButton = findBackButton(tester);
+    expect(backButton, findsOneWidget);
+    await tester.tap(backButton);
+    await tester.pump(); // Start the pop "back" operation.
+    await tester.pump(const Duration(seconds: 1)); // transition is complete
+
+    expect(find.text('UPDATE'), findsNothing);
+  });
+}
