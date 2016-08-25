ae5c78d0e5cdb63c2067e4a31e976cf20a348f79
Dan Rubel <danrubel@google.com>
2016-08-25 12:11:11 -0400

    restore original formatting

--- packages/flutter_tools/test/upgrade_test.dart
+++ packages/flutter_tools/test/upgrade_test.dart
@@ -19,31 +19,19 @@ void main() {
     bool _match(String line) => UpgradeCommand.matchesGitLine(line);
 
     test('regex match', () {
-      expect(
-          _match(' .../flutter_gallery/lib/demo/buttons_demo.dart    | 10 +--'),
-          true);
-      expect(
-          _match(
-              ' dev/benchmarks/complex_layout/lib/main.dart        |  24 +-'),
-          true);
+      expect(_match(' .../flutter_gallery/lib/demo/buttons_demo.dart    | 10 +--'), true);
+      expect(_match(' dev/benchmarks/complex_layout/lib/main.dart        |  24 +-'), true);
 
-      expect(
-          _match(
-              ' rename {packages/flutter/doc => dev/docs}/styles.html (92%)'),
-          true);
+      expect(_match(' rename {packages/flutter/doc => dev/docs}/styles.html (92%)'), true);
       expect(_match(' delete mode 100644 doc/index.html'), true);
-      expect(
-          _match(
-              ' create mode 100644 examples/flutter_gallery/lib/gallery/demo.dart'),
-          true);
+      expect(_match(' create mode 100644 examples/flutter_gallery/lib/gallery/demo.dart'), true);
 
       expect(_match('Fast-forward'), true);
     });
 
     test('regex doesn\'t match', () {
       expect(_match('Updating 79cfe1e..5046107'), false);
-      expect(_match('229 files changed, 6179 insertions(+), 3065 deletions(-)'),
-          false);
+      expect(_match('229 files changed, 6179 insertions(+), 3065 deletions(-)'), false);
     });
 
     group('findProjectRoot', () {
