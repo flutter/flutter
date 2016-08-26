b8980aeb35ecaafc7081177594408dca682df48a
Dragoș Tiselice <dragostiselice@gmail.com>
2016-08-26 14:27:18 -0700

    Added expansion panels. (#5523)
    Added ExpansionPanel and ExpansionPanelList. The implementation
    is based on the Material design spec, without enforcing every
    facet of it, leaving the developer using this the liberty to
    enforced it lower down the widget tree. This decision was made
    based on the argument that implementing more of the spec would
    reduce flexibility to a point where some design would not be
    implementable with this widget anymore.

---
+++ packages/flutter/test/material/expansion_panels_test.dart
@@ -0,0 +1,69 @@
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('ExpansionPanelList test', (WidgetTester tester) async {
+    int index;
+    bool isExpanded;
+
+    await tester.pumpWidget(
+      new ScrollableViewport(
+        child: new ExpansionPanelList(
+          expansionCallback: (int _index, bool _isExpanded) {
+            index = _index;
+            isExpanded = _isExpanded;
+          },
+          children: <ExpansionPanel>[
+            new ExpansionPanel(
+              headerBuilder: (BuildContext context, bool isExpanded) {
+                return new Text(isExpanded ? 'B' : 'A');
+              },
+              body: new SizedBox(height: 100.0)
+            )
+          ]
+        )
+      )
+    );
+
+    expect(find.text('A'), findsOneWidget);
+    expect(find.text('B'), findsNothing);
+    RenderBox box = tester.renderObject(find.byType(ExpansionPanelList));
+    double oldHeight = box.size.height;
+    expect(find.byType(ExpandIcon), findsOneWidget);
+    await tester.tap(find.byType(ExpandIcon));
+    expect(index, 0);
+    expect(isExpanded, isFalse);
+    box = tester.renderObject(find.byType(ExpansionPanelList));
+    expect(box.size.height, equals(oldHeight));
+
+    await tester.pumpWidget(
+      new ScrollableViewport(
+        child: new ExpansionPanelList(
+          expansionCallback: (int _index, bool _isExpanded) {
+            index = _index;
+            isExpanded = _isExpanded;
+          },
+          children: <ExpansionPanel>[
+            new ExpansionPanel(
+              headerBuilder: (BuildContext context, bool isExpanded) {
+                return new Text(isExpanded ? 'B' : 'A');
+              },
+              body: new SizedBox(height: 100.0),
+              isExpanded: true
+            )
+          ]
+        )
+      )
+    );
+    await tester.pump(const Duration(milliseconds: 200));
+
+    expect(find.text('A'), findsNothing);
+    expect(find.text('B'), findsOneWidget);
+    box = tester.renderObject(find.byType(ExpansionPanelList));
+    expect(box.size.height - oldHeight, greaterThanOrEqualTo(100.0)); // 100 + some margin
+  });
+}
