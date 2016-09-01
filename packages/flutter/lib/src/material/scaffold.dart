3732d754cadbde57b8c9ef8979438389300e4ec8
Matt Perry <mpcomplete+github@gmail.com>
2016-09-01 13:22:22 -0400

    Re-enable iOS back gesture. (#5680)
    The iOS transition is the more buggy part of this feature, and it wasn't
    disabled. Let's just leave them enabled while I work on improvements.

--- packages/flutter/lib/src/material/scaffold.dart
+++ packages/flutter/lib/src/material/scaffold.dart
@@ -22,9 +22,6 @@ const double _kFloatingActionButtonMargin = 16.0; // TODO(hmuller): should be de
 const Duration _kFloatingActionButtonSegue = const Duration(milliseconds: 200);
 final Tween<double> _kFloatingActionButtonTurnTween = new Tween<double>(begin: -0.125, end: 0.0);
 
-// iOS back gesture is in development. This flag will go away when it's ready
-// to ship.
-const bool _kBackGestureEnabled = false;
 const double _kBackGestureWidth = 20.0;
 
 /// The Scaffold's appbar is the toolbar, bottom, and the "flexible space"
@@ -700,9 +697,7 @@ class ScaffoldState extends State<Scaffold> {
   NavigationGestureController _backGestureController;
 
   bool _shouldHandleBackGesture() {
-    return _kBackGestureEnabled &&
-      Theme.of(context).platform == TargetPlatform.iOS &&
-      Navigator.canPop(context);
+    return Theme.of(context).platform == TargetPlatform.iOS && Navigator.canPop(context);
   }
 
   void _handleDragStart(DragStartDetails details) {
