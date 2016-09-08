bb6d32b498b133f666d4fd6eeb37b9022093c541
Adam Barth <github@adambarth.com>
2016-09-07 22:19:50 -0700

    Factor runtime initialization into InitRuntime (flutter/engine#3009)
    This makes the code easier to share between Fuchsia and stand-alone
    Flutter.

---
+++ engine/src/flutter/runtime/platform_impl.h
@@ -0,0 +1,27 @@
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef FLUTTER_RUNTIME_PLATFORM_IMPL_H_
+#define FLUTTER_RUNTIME_PLATFORM_IMPL_H_
+
+#include "lib/ftl/macros.h"
+#include "flutter/sky/engine/public/platform/Platform.h"
+
+namespace blink {
+
+class PlatformImpl : public Platform {
+ public:
+  explicit PlatformImpl();
+  ~PlatformImpl() override;
+
+  // blink::Platform methods:
+  std::string defaultLocale() override;
+
+ private:
+  FTL_DISALLOW_COPY_AND_ASSIGN(PlatformImpl);
+};
+
+}  // namespace blink
+
+#endif  // FLUTTER_RUNTIME_PLATFORM_IMPL_H_
---
+++ engine/src/flutter/runtime/runtime_init.cc
@@ -0,0 +1,29 @@
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "flutter/runtime/runtime_init.h"
+
+#include "flutter/glue/trace_event.h"
+#include "flutter/runtime/dart_init.h"
+#include "flutter/runtime/platform_impl.h"
+#include "flutter/sky/engine/public/web/Sky.h"
+#include "lib/ftl/logging.h"
+
+namespace blink {
+namespace {
+
+PlatformImpl* g_platform_impl = nullptr;
+
+}  // namespace
+
+void InitRuntime() {
+  TRACE_EVENT0("flutter", "InitRuntime");
+
+  FTL_CHECK(!g_platform_impl);
+  g_platform_impl = new PlatformImpl();
+  InitEngine(g_platform_impl);
+  InitDartVM();
+}
+
+}  // namespace blink
