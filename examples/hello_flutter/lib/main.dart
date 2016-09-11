8d102cbe401ce378bf6c480510e1bf5a61a97203
Adam Barth <github@adambarth.com>
2016-09-11 15:53:48 -0700

    Add hello_flutter and make it work on Fuchsia (#3016)

---
+++ examples/hello_flutter/lib/main.dart
@@ -0,0 +1,50 @@
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// This example shows how to show the text 'Hello, world.' using using the raw
+// interface to the engine.
+
+import 'dart:ui' as ui;
+
+void beginFrame(Duration timeStamp) {
+  final double devicePixelRatio = ui.window.devicePixelRatio;
+  // TODO(abarth): ui.window.size should be in physical units.
+  final ui.Size logicalSize = ui.window.size;
+
+  final ui.ParagraphBuilder paragraphBuilder = new ui.ParagraphBuilder()
+    ..addText('Hello, world.');
+  final ui.Paragraph paragraph = paragraphBuilder.build(new ui.ParagraphStyle())
+    ..layout(new ui.ParagraphConstraints(width: logicalSize.width));
+
+  final ui.Rect physicalBounds = ui.Point.origin & (logicalSize * devicePixelRatio);
+  final ui.PictureRecorder recorder = new ui.PictureRecorder();
+  final ui.Canvas canvas = new ui.Canvas(recorder, physicalBounds);
+  canvas.scale(devicePixelRatio, devicePixelRatio);
+  canvas.drawRect(ui.Point.origin & logicalSize, new ui.Paint()..color = const ui.Color(0xFF0000FF));
+  canvas.drawParagraph(paragraph, new ui.Offset(
+    (logicalSize.width - paragraph.maxIntrinsicWidth) / 2.0,
+    (logicalSize.height - paragraph.height) / 2.0
+  ));
+  final ui.Picture picture = recorder.endRecording();
+
+  final ui.SceneBuilder sceneBuilder = new ui.SceneBuilder()
+    // TODO(abarth): We should be able to add a picture without pushing a
+    // container layer first.
+    ..pushClipRect(physicalBounds)
+    ..addPicture(ui.Offset.zero, picture)
+    ..pop();
+
+  ui.window.render(sceneBuilder.build());
+}
+
+// This function is the primary entry point to your application. The engine
+// calls main() as soon as it has loaded your code.
+void main() {
+  // The engine calls onBeginFrame whenever it wants us to produce a frame.
+  ui.window.onBeginFrame = beginFrame;
+  // Here we kick off the whole process by asking the engine to schedule a new
+  // frame. The engine will eventually call onBeginFrame when it is time for us
+  // to actually produce the frame.
+  ui.window.scheduleFrame();
+}
