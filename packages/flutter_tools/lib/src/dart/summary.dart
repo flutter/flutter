0774a6748e0d1829512d551e375eb46940d3c35f
Konstantin Scheglov <scheglov@google.com>
2016-09-12 13:58:32 -0700

    Build unlinked summaries for packages on upgrade. (#5816)
    * Build unlinked summaries for packages on upgrade.
    
    * Iterate through the packages in folders.
    
    * Rollback the debug change.

--- packages/flutter_tools/lib/src/dart/summary.dart
+++ packages/flutter_tools/lib/src/dart/summary.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:io' as io;
 
 import 'package:analyzer/file_system/file_system.dart';
@@ -8,6 +9,9 @@ import 'package:analyzer/src/dart/sdk/sdk.dart'; // ignore: implementation_impor
 import 'package:analyzer/src/generated/engine.dart'; // ignore: implementation_imports
 import 'package:analyzer/src/generated/source.dart'; // ignore: implementation_imports
 import 'package:analyzer/src/summary/summary_file_builder.dart'; // ignore: implementation_imports
+import 'package:analyzer/src/summary/pub_summary.dart'; // ignore: implementation_imports
+import 'package:flutter_tools/src/base/logger.dart';
+import 'package:flutter_tools/src/dart/pub.dart';
 import 'package:flutter_tools/src/globals.dart';
 import 'package:package_config/packages.dart';
 import 'package:path/path.dart' as pathos;
@@ -73,4 +77,55 @@ void buildSkyEngineSdkSummary(
       new SummaryBuilder(sources, sdk.context, config).build();
   String outputPath = pathos.join(skyEnginePath, outBundleName);
   new io.File(outputPath).writeAsBytesSync(output.sum);
+}
+
+Future<Null> buildUnlinkedForPackages(String flutterPath) async {
+  PhysicalResourceProvider provider = PhysicalResourceProvider.INSTANCE;
+  PubSummaryManager manager =
+      new PubSummaryManager(provider, '__unlinked_${io.pid}.ds');
+
+  Folder flutterFolder = provider.getFolder(flutterPath);
+
+  //
+  // Build in packages/.
+  //
+  Folder packagesFolder = flutterFolder.getChildAssumingFolder('packages');
+  await _buildUnlinkedForDirectChildren(manager, packagesFolder);
+
+  //
+  // Build in bin/cache/pkg/.
+  //
+  Folder pkgFolder = flutterFolder
+      .getChildAssumingFolder('bin')
+      .getChildAssumingFolder('cache')
+      .getChildAssumingFolder('pkg');
+  await _buildUnlinkedForDirectChildren(manager, pkgFolder);
+}
+
+Future<Null> _buildUnlinkedForDirectChildren(
+    PubSummaryManager manager, Folder packagesFolder) async {
+  for (Resource child in packagesFolder.getChildren()) {
+    if (child is Folder) {
+      await _buildUnlinkedForPackage(manager, child);
+    }
+  }
+}
+
+Future<Null> _buildUnlinkedForPackage(
+    PubSummaryManager manager, Folder packageFolder) async {
+  if (packageFolder.exists) {
+    String name = packageFolder.shortName;
+    File pubspecFile = packageFolder.getChildAssumingFile('pubspec.yaml');
+    Folder libFolder = packageFolder.getChildAssumingFolder('lib');
+    if (pubspecFile.exists && libFolder.exists) {
+      await pubGet(directory: packageFolder.path);
+      Status status =
+          logger.startProgress('Building unlinked bundles for $name...');
+      try {
+        await manager.computeUnlinkedForFolder(name, libFolder);
+      } finally {
+        status.stop(showElapsedTime: true);
+      }
+    }
+  }
 }
