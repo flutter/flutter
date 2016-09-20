c5930684496523019d7694b2096aa762df1cb8dc
James Robinson <jamesr@users.noreply.github.com>
2016-09-20 14:57:56 -0700

    Add flutter_services package to uniformly expose services to Flutter library (flutter/engine#3046)
    This adds a flutter_services package responsible for exposing the Dart
    interfaces exported by the engine in a uniform fashion, regardless of
    how and where the generated code for the interfaces are organized.

---
+++ engine/src/flutter/sky/packages/flutter_services/lib/mojo/gfx/composition/scene_token.dart
@@ -0,0 +1,6 @@
+/// Copyright 2014 The Chromium Authors. All rights reserved.
+/// Use of this source code is governed by a BSD-style license that can be
+/// found in the LICENSE file.
+
+import 'package:mojo_services/mojo/gfx/composition/scene_token.mojom.dart';
+export 'package:mojo_services/mojo/gfx/composition/scene_token.mojom.dart';
