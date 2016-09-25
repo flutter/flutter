2e5bbddab9fb18793bf3faf9f4b2df2619503d30
Devon Carew <devoncarew@google.com>
2016-09-25 00:59:11 -0700

    fix devices test; print on test failure

--- packages/flutter_tools/test/src/context.dart
+++ packages/flutter_tools/test/src/context.dart
@@ -26,7 +26,7 @@ void testUsingContext(String description, dynamic testMethod(), {
   Timeout timeout,
   Map<Type, dynamic> overrides: const <Type, dynamic>{}
 }) {
-  test(description, () {
+  test(description, () async {
     AppContext testContext = new AppContext();
 
     overrides.forEach((Type type, dynamic value) {
@@ -65,7 +65,17 @@ void testUsingContext(String description, dynamic testMethod(), {
         testContext[XCode] = new XCode();
     }
 
-    return testContext.runInZone(testMethod);
+    try {
+      return await testContext.runInZone(testMethod);
+    } catch (error) {
+      if (testContext[Logger] is BufferLogger) {
+        BufferLogger bufferLogger = testContext[Logger];
+        if (bufferLogger.errorText.isNotEmpty)
+          print(bufferLogger.errorText);
+      }
+      throw error;
+    }
+
   }, timeout: timeout);
 }
 
