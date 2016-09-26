78c74116a90f2ca52386f14ddf183f815437482b
Ian Hickson <ian@hixie.ch>
2016-09-26 10:27:53 -0700

    LayoutBuilder + GlobalKey + setState assert (#6068)
    This silences an assertion that fired when reparenting a widget with a
    global key inside a LayoutBuilder callback when that callback also
    happened to call setState (directly or indirectly) on that widget.
    
    Normally such setStates are considered ok since we know we haven't
    cleaned that subtree yet, but we were not correctly handling the case
    where the list needed resorting in that situation.

---
+++ packages/flutter/test/widget/layout_builder_and_global_keys_test.dart
@@ -0,0 +1,59 @@
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter_test/flutter_test.dart' hide TypeMatcher;
+import 'package:flutter/widgets.dart';
+
+class Wrapper extends StatelessWidget {
+  Wrapper({
+    Key key,
+    this.child,
+  }) : super(key: key);
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) => child;
+}
+
+class StatefulWrapper extends StatefulWidget {
+  StatefulWrapper({
+    Key key,
+    this.child,
+  }) : super(key: key);
+
+  final Widget child;
+
+  @override
+  StatefulWrapperState createState() => new StatefulWrapperState();
+}
+
+class StatefulWrapperState extends State<StatefulWrapper> {
+
+  void trigger() {
+    setState(() { /* for test purposes */ });
+  }
+
+  @override
+  Widget build(BuildContext context) => config.child;
+}
+
+void main() {
+  testWidgets('Moving global key inside a LayoutBuilder', (WidgetTester tester) async {
+    GlobalKey<StatefulWrapperState> key = new GlobalKey<StatefulWrapperState>();
+    await tester.pumpWidget(
+      new LayoutBuilder(builder: (BuildContext context, BoxConstraints constraints) {
+        return new Wrapper(
+          child: new StatefulWrapper(key: key, child: new Container(height: 100.0)),
+        );
+      }),
+    );
+    await tester.pumpWidget(
+      new LayoutBuilder(builder: (BuildContext context, BoxConstraints constraints) {
+        key.currentState.trigger();
+        return new StatefulWrapper(key: key, child: new Container(height: 100.0));
+      }),
+    );
+  });
+}
