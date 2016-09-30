e46d62218f5fbf80149d4f125965487af5eb45df
P.Y. Laligand <py.laligand@gmail.com>
2016-09-30 10:31:02 -0700

    Use the flutter tool to build flx files with gn. (flutter/engine#3067)

--- engine/src/flutter/build/package.py
+++ engine/src/flutter/build/package.py
@@ -6,16 +6,26 @@
 import argparse
 import subprocess
 import os
-import StringIO
 import sys
-import zipfile
 
 
 def main():
   parser = argparse.ArgumentParser(description='Package a Flutter application')
 
-  parser.add_argument('--snapshotter', type=str, required=True,
-                      help='The snapshot binary to use')
+  parser.add_argument('--root', type=str, required=True,
+                      help='The root of the output directory')
+  parser.add_argument('--dart', type=str, required=True,
+                      help='The Dart binary to use')
+  parser.add_argument('--flutter-tools-packages', type=str, required=True,
+                      help='The package map for the Flutter tool')
+  parser.add_argument('--flutter-tools-main', type=str, required=True,
+                      help='The main.drt file for the Flutter tool')
+  parser.add_argument('--snapshotter-path', type=str, required=True,
+                      help='The Flutter snapshotter')
+  parser.add_argument('--working-dir', type=str, required=True,
+                      help='The directory where to put intermediate files')
+  parser.add_argument('--app-dir', type=str, required=True,
+                      help='The root of the app')
   parser.add_argument('--main-dart', type=str, required=True,
                       help='The main.dart file to use')
   parser.add_argument('--packages', type=str, required=True,
@@ -24,36 +34,30 @@ def main():
                       help='Path to application snapshot')
   parser.add_argument('--depfile', type=str, required=True,
                       help='Where to output dependency information')
-  parser.add_argument('--build-output', type=str, required=True,
-                      help='The final target to use in the dependency information')
-  parser.add_argument('--bundle', type=str, required=True,
+  parser.add_argument('--output-file', type=str, required=True,
                       help='Where to output application bundle')
-  parser.add_argument('--bundle-header', type=str, required=True,
-                      help='String to prepend to bundle')
 
   args = parser.parse_args()
 
+  env = os.environ.copy()
+  env['LD_LIBRARY_PATH'] = args.root
+
   result = subprocess.call([
-    args.snapshotter,
+    args.dart,
+    '--packages=%s' % args.flutter_tools_packages,
+    args.flutter_tools_main,
+    'build',
+    'mojo',
+    '--snapshotter-path=%s' % args.snapshotter_path,
+    '--working-dir=%s' % args.working_dir,
+    '--target=%s' % args.main_dart,
     '--packages=%s' % args.packages,
     '--snapshot=%s' % args.snapshot,
     '--depfile=%s' % args.depfile,
-    '--build-output=%s' % args.build_output,
-    args.main_dart,
-  ])
+    '--output-file=%s' % args.output_file,
+  ], env=env, cwd=args.app_dir)
 
-  if result != 0:
-    return result
-
-  archive = StringIO.StringIO()
-  with zipfile.ZipFile(archive, 'w') as z:
-    z.write(args.snapshot, 'snapshot_blob.bin', zipfile.ZIP_DEFLATED)
-
-  with open(args.bundle, 'w') as f:
-    if args.bundle_header:
-        f.write(args.bundle_header)
-        f.write('\n')
-    f.write(archive.getvalue())
+  return result
 
 
 if __name__ == '__main__':
