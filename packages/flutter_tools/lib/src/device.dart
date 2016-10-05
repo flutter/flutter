b00e6cda6605b2bd25a6d184454fe3ed933b754c
Devon Carew <devoncarew@google.com>
2016-10-04 20:29:10 -0700

    tweak the device description for emulators (#6214)

--- packages/flutter_tools/lib/src/device.dart
+++ packages/flutter_tools/lib/src/device.dart
@@ -249,8 +249,10 @@ abstract class Device {
 
     return devices.map((Device device) {
       String supportIndicator = device.isSupported() ? '' : ' (unsupported)';
-      if (device.isLocalEmulator)
-        supportIndicator += ' • simulator';
+      if (device.isLocalEmulator) {
+        String type = device.platform == TargetPlatform.ios ? 'simulator' : 'emulator';
+        supportIndicator += ' ($type)';
+      }
       return '${device.name.padRight(nameWidth)} • '
              '${device.id.padRight(idWidth)} • '
              '${getNameForTargetPlatform(device.platform)}$supportIndicator';
