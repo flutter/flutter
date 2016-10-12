6be2d61c935b09c3e2058f9566e8761505f55c5e
Adam Barth <github@adambarth.com>
2016-10-12 10:25:35 -0700

    Enable tracing on Fuchsia (flutter/engine#3116)

--- engine/src/flutter/glue/trace_event.h
+++ engine/src/flutter/glue/trace_event.h
@@ -3,13 +3,7 @@
 // found in the LICENSE file.
 
 #if defined(__Fuchsia__)
-#define TRACE_EVENT0(a, b)
-#define TRACE_EVENT1(a, b, c, d)
-#define TRACE_EVENT2(a, b, c, d, e, f)
-#define TRACE_EVENT_ASYNC_BEGIN0(a, b, c)
-#define TRACE_EVENT_ASYNC_END0(a, b, c)
-#define TRACE_EVENT_ASYNC_BEGIN1(a, b, c, d, e)
-#define TRACE_EVENT_ASYNC_END1(a, b, c, d, e)
+#include "lib/trace_event/macros.h"
 #else
 #include "base/trace_event/trace_event.h"
 #endif  // defined(__Fuchsia__)
