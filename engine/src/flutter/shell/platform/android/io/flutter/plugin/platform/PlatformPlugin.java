04a38139ada85117ca27a5e8499c3bfbfc303afe
Adam Barth <github@adambarth.com>
2016-10-13 11:10:02 -0700

    Don't start in fullscreen mode on Android (flutter/engine#3129)
    Fixes https://github.com/flutter/flutter/issues/6303

--- engine/src/flutter/shell/platform/android/io/flutter/plugin/platform/PlatformPlugin.java
+++ engine/src/flutter/shell/platform/android/io/flutter/plugin/platform/PlatformPlugin.java
@@ -25,15 +25,12 @@ import org.json.JSONObject;
  */
 public class PlatformPlugin extends JSONMessageListener implements ActivityLifecycleListener {
     private final Activity mActivity;
-    private static final int DEFAULT_OVERLAYS = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-            | View.SYSTEM_UI_FLAG_FULLSCREEN
-            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+    public static final int DEFAULT_SYSTEM_UI = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN;
 
     public PlatformPlugin(Activity activity) {
         mActivity = activity;
-        mEnabledOverlays = DEFAULT_OVERLAYS;
+        mEnabledOverlays = DEFAULT_SYSTEM_UI;
     }
 
     @Override
@@ -130,7 +127,10 @@ public class PlatformPlugin extends JSONMessageListener implements ActivityLifec
     private int mEnabledOverlays;
 
     private void setSystemChromeEnabledSystemUIOverlays(JSONArray overlays) throws JSONException {
-        int enabledOverlays = DEFAULT_OVERLAYS;
+        int enabledOverlays = DEFAULT_SYSTEM_UI
+            | View.SYSTEM_UI_FLAG_FULLSCREEN
+            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
 
          if (overlays.length() == 0) {
              enabledOverlays |= View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
