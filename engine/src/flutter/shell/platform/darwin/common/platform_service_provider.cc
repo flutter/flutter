96ba6d57c1d5d7ef4f73654d71c17a9b20edc05a
Adam Barth <github@adambarth.com>
2016-10-18 10:04:36 -0700

    Remove clipboard mojo service (flutter/engine#3143)
    All the clients have migrated to platform messages.

--- engine/src/flutter/shell/platform/darwin/common/platform_service_provider.cc
+++ engine/src/flutter/shell/platform/darwin/common/platform_service_provider.cc
@@ -5,7 +5,6 @@
 #include "flutter/shell/platform/darwin/common/platform_service_provider.h"
 
 #if TARGET_OS_IPHONE
-#include "flutter/services/editing/ios/clipboard_impl.h"
 #include "flutter/services/vsync/ios/vsync_provider_ios_impl.h"
 #else
 #include "flutter/services/vsync/mac/vsync_provider_mac_impl.h"
@@ -23,11 +22,6 @@ void PlatformServiceProvider::ConnectToService(
     const mojo::String& service_name,
     mojo::ScopedMessagePipeHandle client_handle) {
 #if TARGET_OS_IPHONE
-  if (service_name == ::editing::Clipboard::Name_) {
-    new sky::services::editing::ClipboardImpl(
-        mojo::InterfaceRequest<::editing::Clipboard>(client_handle.Pass()));
-    return;
-  }
   if (service_name == ::vsync::VSyncProvider::Name_) {
     new sky::services::vsync::VsyncProviderIOSImpl(
         mojo::InterfaceRequest<::vsync::VSyncProvider>(client_handle.Pass()));
