b0015c4e06e4ae8e70032f5cce77ebad1ad56192
Petr Hosek <petrhosek@users.noreply.github.com>
2016-11-02 14:38:09 -0700

    Split the snapshot generation into a separate action (flutter/engine#3168)
    This is to ensure that the depfile generated by the snapshotter has
    the correct target.

--- engine/src/flutter/build/package.py
+++ engine/src/flutter/build/package.py
@@ -22,20 +22,14 @@ def main():
                       help='The package map for the Flutter tool')
   parser.add_argument('--flutter-tools-main', type=str, required=True,
                       help='The main.dart file for the Flutter tool')
-  parser.add_argument('--snapshotter-path', type=str, required=True,
-                      help='The Flutter snapshotter')
   parser.add_argument('--working-dir', type=str, required=True,
                       help='The directory where to put intermediate files')
   parser.add_argument('--app-dir', type=str, required=True,
                       help='The root of the app')
-  parser.add_argument('--main-dart', type=str, required=True,
-                      help='The main.dart file to use')
   parser.add_argument('--packages', type=str, required=True,
                       help='The package map to use')
   parser.add_argument('--snapshot', type=str, required=True,
                       help='Path to application snapshot')
-  parser.add_argument('--depfile', type=str, required=True,
-                      help='Where to output dependency information')
   parser.add_argument('--output-file', type=str, required=True,
                       help='Where to output application bundle')
 
@@ -49,12 +43,9 @@ def main():
     args.dart,
     '--packages=%s' % args.flutter_tools_packages,
     args.flutter_tools_main,
-    '--snapshotter-path=%s' % args.snapshotter_path,
     '--working-dir=%s' % args.working_dir,
-    '--target=%s' % args.main_dart,
     '--packages=%s' % args.packages,
     '--snapshot=%s' % args.snapshot,
-    '--depfile=%s' % args.depfile,
     '--output-file=%s' % args.output_file,
     '--header=#!mojo mojo:flutter_content_handler',
   ], env=env, cwd=args.app_dir)
