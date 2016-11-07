1d7337b4d83eaf2c913711b2579e4f67dbc812e8
Adam Barth <github@adambarth.com>
2016-11-07 15:51:42 -0800

    Switch Flutter to //lib/fidl (flutter/engine#3206)

--- engine/src/flutter/glue/trace_event.h
+++ engine/src/flutter/glue/trace_event.h
@@ -3,7 +3,13 @@
 // found in the LICENSE file.
 
 #if defined(__Fuchsia__)
-#include "lib/trace_event/macros.h"
+#define TRACE_EVENT0(a, b)
+#define TRACE_EVENT1(a, b, c, d)
+#define TRACE_EVENT2(a, b, c, d, e, f)
+#define TRACE_EVENT_ASYNC_BEGIN0(a, b, c)
+#define TRACE_EVENT_ASYNC_END0(a, b, c)
+#define TRACE_EVENT_ASYNC_BEGIN1(a, b, c, d, e)
+#define TRACE_EVENT_ASYNC_END1(a, b, c, d, e)
 #else
 #include "base/trace_event/trace_event.h"
 #endif  // defined(__Fuchsia__)
