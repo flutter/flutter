fbe3fb2e6c8b5c8041f0f48bb2b63abcffb27ae9
Adam Barth <abarth@chromium.org>
2016-11-07 22:43:13 -0800

    Add support for raw key events on Fuchsia
    See #6631

--- packages/flutter/test/widgets/raw_keyboard_listener_test.dart
+++ packages/flutter/test/widgets/raw_keyboard_listener_test.dart
@@ -2,12 +2,51 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:flutter_test/flutter_test.dart';
+import 'dart:convert';
+import 'dart:typed_data';
+
+import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void sendFakeKeyEvent(Map<String, dynamic> data) {
+  String message = JSON.encode(data);
+  Uint8List encoded = UTF8.encoder.convert(message);
+  PlatformMessages.handlePlatformMessage(
+      'flutter/keyevent', encoded.buffer.asByteData(), (_) {});
+}
 
 void main() {
   testWidgets('Can dispose without keyboard', (WidgetTester tester) async {
     await tester.pumpWidget(new RawKeyboardListener(child: new Container()));
     await tester.pumpWidget(new Container());
+  });
+
+  testWidgets('Fuchsia key event', (WidgetTester tester) async {
+    List<RawKeyEvent> events = <RawKeyEvent>[];
+
+    await tester.pumpWidget(new RawKeyboardListener(
+      focused: true,
+      onKey: (RawKeyEvent event) {
+        events.add(event);
+      },
+      child: new Container(),
+    ));
+
+    sendFakeKeyEvent(<String, dynamic>{
+      'type': 'keydown',
+      'keymap': 'fuchsia',
+      'hidUsage': 0x04,
+      'codePoint': 0x64,
+    });
+
+    await tester.idle();
+
+    expect(events.length, 1);
+    expect(events[0].runtimeType, equals(RawKeyDownEvent));
+    expect(events[0].data.runtimeType, equals(RawKeyEventDataFuchsia));
+    RawKeyEventDataFuchsia typedData = events[0].data;
+    expect(typedData.hidUsage, 0x04);
+    expect(typedData.codePoint, 0x64);
   });
 }
