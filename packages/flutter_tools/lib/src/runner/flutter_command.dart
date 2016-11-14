4bb1cc81143e0ba0620932016013547b808cf6d2
danrubel <danrubel@google.com>
2016-11-14 11:04:06 -0500

    add braces around multi-line statement

--- packages/flutter_tools/lib/src/runner/flutter_command.dart
+++ packages/flutter_tools/lib/src/runner/flutter_command.dart
@@ -206,10 +206,11 @@ abstract class FlutterCommand extends Command<Null> {
   void commonCommandValidator() {
     if (!PackageMap.isUsingCustomPackagesPath) {
       // Don't expect a pubspec.yaml file if the user passed in an explicit .packages file path.
-      if (!FileSystemEntity.isFileSync('pubspec.yaml'))
+      if (!FileSystemEntity.isFileSync('pubspec.yaml')) {
         throw new ToolExit('Error: No pubspec.yaml file found.\n'
           'This command should be run from the root of your Flutter project.\n'
           'Do not run this command from the root of your git clone of Flutter.');
+      }
     }
 
     if (_usesTargetOption) {
