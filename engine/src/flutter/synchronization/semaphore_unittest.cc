bc255c65dc9d20c01feac81e71af6f3be4242755
Chinmay Garde <chinmaygarde@gmail.com>
2016-11-23 15:57:52 -0800

    On host targets, add dependencies on host unit test executables. (flutter/engine#3269)

---
+++ engine/src/flutter/synchronization/semaphore_unittest.cc
@@ -0,0 +1,27 @@
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <thread>
+
+#include "flutter/synchronization/semaphore.h"
+#include "gtest/gtest.h"
+
+TEST(SemaphoreTest, SimpleValidity) {
+  flutter::Semaphore sem(100);
+  ASSERT_TRUE(sem.IsValid());
+}
+
+TEST(SemaphoreTest, WaitOnZero) {
+  flutter::Semaphore sem(0);
+  ASSERT_FALSE(sem.TryWait());
+}
+
+TEST(SemaphoreTest, WaitOnZeroSignalThenWait) {
+  flutter::Semaphore sem(0);
+  ASSERT_FALSE(sem.TryWait());
+  std::thread thread([&sem]() { sem.Signal(); });
+  thread.join();
+  ASSERT_TRUE(sem.TryWait());
+  ASSERT_FALSE(sem.TryWait());
+}
