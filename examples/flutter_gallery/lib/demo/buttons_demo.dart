5bc7ba66f69d720243449b3e34a7796ac3603a28
Hans Muller <hansmuller@chromium.org>
2016-11-30 16:37:59 -0800

    DropdownButton hint

--- examples/flutter_gallery/lib/demo/buttons_demo.dart
+++ examples/flutter_gallery/lib/demo/buttons_demo.dart
@@ -133,6 +133,7 @@ class _ButtonsDemoState extends State<ButtonsDemo> {
   // https://en.wikipedia.org/wiki/Free_Four
   String dropdown1Value = 'Free';
   String dropdown2Value = 'Four';
+  String dropdown3Value;
 
   Widget buildDropdownButton() {
     return new Padding(
@@ -140,6 +141,47 @@ class _ButtonsDemoState extends State<ButtonsDemo> {
       child: new Column(
         mainAxisAlignment: MainAxisAlignment.start,
         children: <Widget>[
+          new ListItem(
+            title: new Text('Simple dropdown:'),
+            trailing: new DropdownButton<String>(
+              value: dropdown2Value,
+              onChanged: (String newValue) {
+                setState(() {
+                  dropdown2Value = newValue;
+                });
+              },
+              items: <String>['One', 'Two', 'Free', 'Four'].map((String value) {
+                return new DropdownMenuItem<String>(
+                  value: value,
+                  child: new Text(value),
+                );
+              }).toList(),
+            ),
+          ),
+          new SizedBox(
+            height: 24.0,
+          ),
+          new ListItem(
+            title: new Text('Dropdown with a hint:'),
+            trailing: new DropdownButton<String>(
+              value: dropdown3Value,
+              hint: new Text('Choose'),
+              onChanged: (String newValue) {
+                setState(() {
+                  dropdown3Value = newValue;
+                });
+              },
+              items: <String>['One', 'Two', 'Free', 'Four'].map((String value) {
+                return new DropdownMenuItem<String>(
+                  value: value,
+                  child: new Text(value),
+                );
+              }).toList(),
+            ),
+          ),
+          new SizedBox(
+            height: 24.0,
+          ),
           new ListItem(
             title: new Text('Scrollable dropdown:'),
             trailing: new DropdownButton<String>(
@@ -162,26 +204,6 @@ class _ButtonsDemoState extends State<ButtonsDemo> {
                 .toList(),
              ),
           ),
-          new SizedBox(
-            height: 24.0,
-          ),
-          new ListItem(
-            title: new Text('Simple dropdown:'),
-            trailing: new DropdownButton<String>(
-              value: dropdown2Value,
-              onChanged: (String newValue) {
-                setState(() {
-                  dropdown2Value = newValue;
-                });
-              },
-              items: <String>['One', 'Two', 'Free', 'Four'].map((String value) {
-                return new DropdownMenuItem<String>(
-                  value: value,
-                  child: new Text(value),
-                );
-              }).toList(),
-            ),
-          )
         ],
       ),
     );
