a56d9c1666c6f41e6822d5c1048b7529701a737b
Benjamin Lerman <qsr@chromium.org>
2016-12-14 16:08:42 +0100

    Remove obsolete usage of the fuchsia tracing macros. (flutter/engine#3311)

--- engine/src/flutter/glue/trace_event.h
+++ engine/src/flutter/glue/trace_event.h
@@ -5,14 +5,14 @@
 #if defined(__Fuchsia__)
 #include "apps/tracing/lib/trace/event.h"
 
-#define TRACE_EVENT0(a, b) TRACE_DURATION0(a, b)
-#define TRACE_EVENT1(a, b, c, d) TRACE_DURATION1(a, b, c, d)
-#define TRACE_EVENT2(a, b, c, d, e, f) TRACE_DURATION2(a, b, c, d, e, f)
-#define TRACE_EVENT_ASYNC_BEGIN0(a, b, c) TRACE_ASYNC_BEGIN0(a, b, c)
-#define TRACE_EVENT_ASYNC_END0(a, b, c) TRACE_ASYNC_END0(a, b, c)
+#define TRACE_EVENT0(a, b) TRACE_DURATION(a, b)
+#define TRACE_EVENT1(a, b, c, d) TRACE_DURATION(a, b, c, d)
+#define TRACE_EVENT2(a, b, c, d, e, f) TRACE_DURATION(a, b, c, d, e, f)
+#define TRACE_EVENT_ASYNC_BEGIN0(a, b, c) TRACE_ASYNC_BEGIN(a, b, c)
+#define TRACE_EVENT_ASYNC_END0(a, b, c) TRACE_ASYNC_END(a, b, c)
 #define TRACE_EVENT_ASYNC_BEGIN1(a, b, c, d, e) \
-  TRACE_ASYNC_BEGIN1(a, b, c, d, e)
-#define TRACE_EVENT_ASYNC_END1(a, b, c, d, e) TRACE_ASYNC_END1(a, b, c, d, e)
+  TRACE_ASYNC_BEGIN(a, b, c, d, e)
+#define TRACE_EVENT_ASYNC_END1(a, b, c, d, e) TRACE_ASYNC_END(a, b, c, d, e)
 
 #else
 #include "base/trace_event/trace_event.h"
