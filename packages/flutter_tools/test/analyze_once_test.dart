1061b58a19c4fb6362d3f126c0b4624c92945ccd
Dan Rubel <danrubel@google.com>
2017-01-11 13:41:42 -0500

    remove unused import

--- packages/flutter_tools/test/analyze_once_test.dart
+++ packages/flutter_tools/test/analyze_once_test.dart
@@ -4,7 +4,6 @@
 
 import 'dart:async';
 
-import 'package:flutter_tools/src/base/common.dart';
 import 'package:flutter_tools/src/base/file_system.dart';
 import 'package:flutter_tools/src/base/os.dart';
 import 'package:flutter_tools/src/commands/analyze.dart';
