f8d40ab58dfb2977c1e7466e4f602cb7bc9ce70b
Ian Hickson <ian@hixie.ch>
2017-01-25 23:21:19 -0800

    proxy_box tests

---
+++ packages/flutter/test/rendering/semantics_and_children_test.dart
@@ -0,0 +1,34 @@
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:test/test.dart';
+
+int countSemanticsChildren(RenderObject object) {
+  int count = 0;
+  object.visitChildrenForSemantics((RenderObject child) {
+    count += 1;
+  });
+  return count;
+}
+    
+void main() {
+  test('RenderOpacity and children and semantics', () {
+    RenderOpacity box = new RenderOpacity(child: new RenderParagraph(new TextSpan()));
+    expect(countSemanticsChildren(box), 1);
+    box.opacity = 0.5;
+    expect(countSemanticsChildren(box), 1);
+    box.opacity = 0.25;
+    expect(countSemanticsChildren(box), 1);
+    box.opacity = 0.125;
+    expect(countSemanticsChildren(box), 1);
+    box.opacity = 0.0;
+    expect(countSemanticsChildren(box), 0);
+    box.opacity = 0.125;
+    expect(countSemanticsChildren(box), 1);
+    box.opacity = 0.0;
+    expect(countSemanticsChildren(box), 0);
+  });
+}
