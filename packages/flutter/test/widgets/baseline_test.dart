1e4f17f8ae493b82b5323db69bd4c66e9c74ae0e
Ian Hickson <ian@hixie.ch>
2017-01-27 01:51:47 -0800

    Enable Ahem fonts for tests.
    ...and add a baseline test.

--- packages/flutter/test/widgets/baseline_test.dart
+++ packages/flutter/test/widgets/baseline_test.dart
@@ -6,7 +6,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter/widgets.dart';
 
 void main() {
-  testWidgets('Baseline', (WidgetTester tester) async {
+  testWidgets('Baseline - control test', (WidgetTester tester) async {
     await tester.pumpWidget(
       new Center(
         child: new DefaultTextStyle(
@@ -18,6 +18,27 @@ void main() {
         ),
       ),
     );
+    debugDumpRenderTree();
     expect(tester.renderObject<RenderBox>(find.text('X')).size, const Size(100.0, 100.0));
+  });
+
+  testWidgets('Baseline - position test', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      new Center(
+        child: new Baseline(
+          baseline: 180.0,
+          baselineType: TextBaseline.alphabetic,
+          child: new DefaultTextStyle(
+            style: new TextStyle(
+              fontFamily: 'Ahem',
+              fontSize: 100.0,
+            ),
+            child: new Text('X'),
+          ),
+        ),
+      ),
+    );
+    expect(tester.renderObject<RenderBox>(find.text('X')).size, const Size(100.0, 100.0));
+    expect(tester.renderObject<RenderBox>(find.byType(Baseline)).size, const Size(100.0, 200.0));
   });
 }
