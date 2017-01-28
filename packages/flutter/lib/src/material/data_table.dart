571834ea3ce225d452859e9cbc781968dd8cf3c6
Ian Hickson <ian@hixie.ch>
2017-01-28 01:48:18 -0800

    Indicate required fields in flutter_gallery demo. (#7717)

--- packages/flutter/lib/src/material/data_table.dart
+++ packages/flutter/lib/src/material/data_table.dart
@@ -596,12 +596,12 @@ class DataTable extends StatelessWidget {
 /// Must have an ancestor [Material] widget in which to cause ink
 /// reactions and an ancestor [Table] widget to establish a row.
 ///
-/// The TableRowInkWell must be in the same coordinate space (modulo
+/// The [TableRowInkWell] must be in the same coordinate space (modulo
 /// translations) as the [Table]. If it's rotated or scaled or
 /// otherwise transformed, it will not be able to describe the
 /// rectangle of the row in its own coordinate system as a [Rect], and
 /// thus the splash will not occur. (In general, this is easy to
-/// achieve: just put the TableRowInkWell as the direct child of the
+/// achieve: just put the [TableRowInkWell] as the direct child of the
 /// [Table], and put the other contents of the cell inside it.)
 class TableRowInkWell extends InkResponse {
   /// Creates an ink well for a table row.
