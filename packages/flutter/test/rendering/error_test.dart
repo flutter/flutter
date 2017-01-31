df6ea8b7314d8cbbdce35222b62358c8c5e109a0
Xiao Yu <xster@google.com>
2017-01-31 01:35:59 -0800

    Add paragraph to mock_canvas

---
+++ packages/flutter/test/rendering/error_test.dart
@@ -0,0 +1,22 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
+
+import '../rendering/mock_canvas.dart';
+
+/// Unit tests error.dart's usage via ErrorWidget.
+void main() {
+  final String errorMessage = 'Some error message';
+
+  testWidgets('test draw error paragraph', (WidgetTester tester) async {
+    await tester.pumpWidget(new ErrorWidget(new Exception(errorMessage)));
+
+    expect(find.byType(ErrorWidget), paints
+        ..rect(rect: new Rect.fromLTWH(0.0, 0.0, 800.0, 600.0))
+        ..paragraph(offset: Offset.zero));
+  });
+}
