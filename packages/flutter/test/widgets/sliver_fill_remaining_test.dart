6a07268397b7b2277c876dca8f0f081462f84d93
Adam Barth <abarth@chromium.org>
2017-01-31 23:41:02 -0800

    Add SliverFill
    SliverFill fills the remaining space in the viewport with each box
    child. We'll use this sliver as a building block for pageable lists.

---
+++ packages/flutter/test/widgets/sliver_fill_remaining_test.dart
@@ -0,0 +1,48 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter/widgets.dart';
+
+void main() {
+  testWidgets('SliverFillRemaining control test', (WidgetTester tester) async {
+    List<Widget> children = new List<Widget>.generate(20, (int i) {
+      return new Container(child: new Text('$i'));
+    });
+
+    await tester.pumpWidget(
+      new ScrollableViewport2(
+        slivers: <Widget>[
+          new SliverFill(
+            delegate: new SliverChildListDelegate(children),
+          ),
+        ],
+      ),
+    );
+
+    RenderBox box = tester.renderObject<RenderBox>(find.byType(Container).first);
+    expect(box.size.height, equals(600.0));
+
+    expect(find.text('0'), findsOneWidget);
+    expect(find.text('1'), findsNothing);
+    expect(find.text('2'), findsNothing);
+
+    await tester.scroll(find.byType(ScrollableViewport2), const Offset(0.0, -700.0));
+    await tester.pump();
+
+    expect(find.text('0'), findsNothing);
+    expect(find.text('1'), findsOneWidget);
+    expect(find.text('2'), findsOneWidget);
+    expect(find.text('3'), findsNothing);
+    expect(find.text('4'), findsNothing);
+
+    await tester.scroll(find.byType(ScrollableViewport2), const Offset(0.0, 200.0));
+    await tester.pump();
+
+    expect(find.text('0'), findsOneWidget);
+    expect(find.text('1'), findsOneWidget);
+    expect(find.text('2'), findsNothing);
+    expect(find.text('3'), findsNothing);
+  });
+}
