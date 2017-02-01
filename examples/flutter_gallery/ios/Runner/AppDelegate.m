4c0912bb726722bf3f848842aa2eb95818c32ea0
Chris Bracken <chris@bracken.jp>
2017-01-31 17:24:26 -0800

    Enable iOS scroll to top on status bar tap in Gallery

--- examples/flutter_gallery/ios/Runner/AppDelegate.m
+++ examples/flutter_gallery/ios/Runner/AppDelegate.m
@@ -3,4 +3,15 @@
 
 @implementation AppDelegate
 
+- (void) touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+  [super touchesBegan:touches withEvent:event];
+
+  // Support scroll to top on status bar tap.
+  UIViewController *viewController =
+      [UIApplication sharedApplication].keyWindow.rootViewController;
+  if ([viewController isKindOfClass:[FlutterViewController class]]) {
+    [(FlutterViewController*)viewController handleStatusBarTouches:event];
+  }
+}
+
 @end
