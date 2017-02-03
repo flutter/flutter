45df428f8ef55705817eb389f4ff47630afba588
Chris Bracken <chris@bracken.jp>
2017-02-02 16:29:22 -0800

    Support status bar taps in FlutterAppDelegate (flutter/engine#3387)
    Provide a default status bar tap handler for FlutterAppDelegate.
    By default, taps are passed to the key window's rootViewController if
    it's a FlutterViewController. Apps with custom app delegates can
    customize this behaviour to send the tap to the FlutterViewController(s)
    they prefer by overriding touchesBegan:withEvent.

--- engine/src/flutter/shell/platform/darwin/ios/framework/Source/FlutterAppDelegate.mm
+++ engine/src/flutter/shell/platform/darwin/ios/framework/Source/FlutterAppDelegate.mm
@@ -3,7 +3,28 @@
 // found in the LICENSE file.
 
 #include "flutter/shell/platform/darwin/ios/framework/Headers/FlutterAppDelegate.h"
+#include "flutter/shell/platform/darwin/ios/framework/Headers/FlutterViewController.h"
 
 @implementation FlutterAppDelegate
+
+// Returns the key window's rootViewController, if it's a FlutterViewController.
+// Otherwise, returns nil.
+- (FlutterViewController*)rootFlutterViewController {
+  UIViewController *viewController =
+      [UIApplication sharedApplication].keyWindow.rootViewController;
+  if ([viewController isKindOfClass:[FlutterViewController class]]) {
+    return (FlutterViewController*)viewController;
+  }
+  return nil;
+}
+
+- (void) touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+  [super touchesBegan:touches withEvent:event];
+
+  // Pass status bar taps to key window Flutter rootViewController.
+  if (self.rootFlutterViewController != nil) {
+    [self.rootFlutterViewController handleStatusBarTouches:event];
+  }
+}
 
 @end
