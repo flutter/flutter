ea21d0c542d05a7738a8357db48fe9490c25c621
Ian Hickson <ian@hixie.ch>
2017-02-08 10:44:54 -0800

    Make the text shorter so that it doesn't overflow (#7970)
    The Ahem font has a much wider average text width than most fonts.

--- packages/flutter/test/material/data_table_test_utils.dart
+++ packages/flutter/test/material/data_table_test_utils.dart
@@ -16,14 +16,14 @@ class Dessert {
 }
 
 final List<Dessert> kDesserts = <Dessert>[
-  new Dessert('Frozen yogurt',                        159,  6.0,  24,  4.0,  87, 14,  1),
-  new Dessert('Ice cream sandwich',                   237,  9.0,  37,  4.3, 129,  8,  1),
-  new Dessert('Eclair',                               262, 16.0,  24,  6.0, 337,  6,  7),
-  new Dessert('Cupcake',                              305,  3.7,  67,  4.3, 413,  3,  8),
-  new Dessert('Gingerbread',                          356, 16.0,  49,  3.9, 327,  7, 16),
-  new Dessert('Jelly bean',                           375,  0.0,  94,  0.0,  50,  0,  0),
-  new Dessert('Lollipop',                             392,  0.2,  98,  0.0,  38,  0,  2),
-  new Dessert('Honeycomb',                            408,  3.2,  87,  6.5, 562,  0, 45),
-  new Dessert('Donut',                                452, 25.0,  51,  4.9, 326,  2, 22),
-  new Dessert('KitKat',                               518, 26.0,  65,  7.0,  54, 12,  6),
+  new Dessert('FY',  159,  6.0,  24,  4.0,  87, 14,  1),
+  new Dessert('ICS', 237,  9.0,  37,  4.3, 129,  8,  1),
+  new Dessert('E',   262, 16.0,  24,  6.0, 337,  6,  7),
+  new Dessert('C',   305,  3.7,  67,  4.3, 413,  3,  8),
+  new Dessert('G',   356, 16.0,  49,  3.9, 327,  7, 16),
+  new Dessert('JB',  375,  0.0,  94,  0.0,  50,  0,  0),
+  new Dessert('L',   392,  0.2,  98,  0.0,  38,  0,  2),
+  new Dessert('H',   408,  3.2,  87,  6.5, 562,  0, 45),
+  new Dessert('D',   452, 25.0,  51,  4.9, 326,  2, 22),
+  new Dessert('KK',  518, 26.0,  65,  7.0,  54, 12,  6),
 ];
