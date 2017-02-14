23b766b76dbaca6ff1cd3e8d6cd0f183054c56f9
Ryan Macnak <rmacnak@google.com>
2017-02-14 09:28:55 -0800

    Adjust/weaken stack_trace_test.dart to pass in the face of async causal stack traces (Issue #8128).

--- packages/flutter/test/foundation/stack_trace_test.dart
+++ packages/flutter/test/foundation/stack_trace_test.dart
@@ -6,19 +6,22 @@ import 'package:flutter/foundation.dart';
 import 'package:test/test.dart';
 
 void main() {
+  // TODO(8128): These tests and the filtering mechanism should be revisited to account for causal async stack traces.
+
   test('FlutterError.defaultStackFilter', () {
     List<String> filtered = FlutterError.defaultStackFilter(StackTrace.current.toString().trimRight().split('\n')).toList();
-    expect(filtered.length, 4);
+    expect(filtered.length, greaterThanOrEqualTo(4));
     expect(filtered[0], matches(r'^#0 +main\.<anonymous closure> \(.*stack_trace_test\.dart:[0-9]+:[0-9]+\)$'));
     expect(filtered[1], matches(r'^#1 +Declarer\.test\.<anonymous closure>\.<<anonymous closure>_async_body>\.<anonymous closure>\.<<anonymous closure>_async_body> \(package:test/.+:[0-9]+:[0-9]+\)$'));
-    expect(filtered[2], matches(r'^#[1-9][0-9]+ +Declarer\._runSetUps\.<_runSetUps_async_body> \(package:test/.+:[0-9]+:[0-9]+\)$'));
-    expect(filtered[3], matches(r'^\(elided [1-9][0-9]+ frames from package dart:async, package dart:async-patch, and package stack_trace\)$'));
+    expect(filtered[2], equals('<asynchronous suspension>'));
+    expect(filtered.last, matches(r'^\(elided [1-9][0-9]+ frames from package dart:async, package dart:async-patch, and package stack_trace\)$'));
   });
 
   test('FlutterError.defaultStackFilter (async test body)', () async {
     List<String> filtered = FlutterError.defaultStackFilter(StackTrace.current.toString().trimRight().split('\n')).toList();
-    expect(filtered.length, 2);
+    expect(filtered.length, greaterThanOrEqualTo(3));
     expect(filtered[0], matches(r'^#0 +main\.<anonymous closure>\.<<anonymous closure>_async_body> \(.*stack_trace_test\.dart:[0-9]+:[0-9]+\)$'));
-    expect(filtered[1], matches(r'^\(elided [1-9][0-9]+ frames from package dart:async and package stack_trace\)$'));
+    expect(filtered[1], equals('<asynchronous suspension>'));
+    expect(filtered.last, matches(r'^\(elided [1-9][0-9]+ frames from package dart:async, package dart:async-patch, and package stack_trace\)$'));
   });
 }
