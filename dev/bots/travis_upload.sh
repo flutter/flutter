7f0b908ddccfe05baaa2a6abbff433457602b772
Adam Barth <github@adambarth.com>
2017-02-14 10:25:02 -0800

    Don't upload lcov to coveralls for pull requests (#8143)

--- dev/bots/travis_upload.sh
+++ dev/bots/travis_upload.sh
@@ -6,16 +6,16 @@ export PATH="$PWD/bin:$PWD/bin/cache/dart-sdk/bin:$PATH"
 
 LCOV_FILE=./packages/flutter/coverage/lcov.info
 
-if [ -n "$TRAVIS" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ -a "$LCOV_FILE" ]; then
+if [ "$TRAVIS_PULL_REQUEST" = "false" ] && [ "$TRAVIS_BRANCH" = "master" ] && [ -f "$LCOV_FILE" ]; then
   GSUTIL=$HOME/google-cloud-sdk/bin/gsutil
   GCLOUD=$HOME/google-cloud-sdk/bin/gcloud
 
   $GCLOUD auth activate-service-account --key-file ../gcloud_key_file.json
   STORAGE_URL=gs://flutter_infra/flutter/coverage/lcov.info
   $GSUTIL cp $LCOV_FILE $STORAGE_URL
+
+  (cd packages/flutter && coveralls-lcov coverage/lcov.info)
 fi
 
 # generate the API docs, upload them
 ./dev/bots/docs.sh
-
-(cd packages/flutter && coveralls-lcov coverage/lcov.info)
