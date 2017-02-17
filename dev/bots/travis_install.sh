cc808cda5c06c6eef6417c049f09ce2e4e39053a
Adam Barth <abarth@chromium.org>
2017-02-16 20:49:25 -0800

    Fix travis_install.sh

--- dev/bots/travis_install.sh
+++ dev/bots/travis_install.sh
@@ -2,6 +2,6 @@
 set -ex
 
 if [ -n "$TRAVIS" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-  - gem install coveralls-lcov
-  - npm install -g firebase-tools@">=3.0.4 <3.1.0"
+  gem install coveralls-lcov
+  npm install -g firebase-tools@">=3.0.4 <3.1.0"
 fi
