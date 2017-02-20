59fa628b87c69a1eec2bd2b551450e5d676197fc
Faisal Abid <faisal.abid@gmail.com>
2017-02-20 02:37:40 -0500

    heroku deployment of branches in pr

--- dev/bots/travis_install.sh
+++ dev/bots/travis_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex
 
-if [ -n "$TRAVIS" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+if [ -n "$TRAVIS" ]; then
   gem install coveralls-lcov
   npm install -g firebase-tools@">=3.0.4 <3.1.0"
 fi
