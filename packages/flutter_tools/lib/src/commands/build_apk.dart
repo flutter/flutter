99002c91dee924fb1e0155c0cd134471eb003fca
Michael Goderbauer <goderbauer@google.com>
2017-02-21 15:57:35 -0800

    Revert "Revert "Replace jarsigner with apksigner" (#8164)"
    This reverts commit 70d7fe3a0dede9f3556fda8fe018dc4ed4a08490.
    
    The Java version missmatch on the bots has been resolved.

--- packages/flutter_tools/lib/src/commands/build_apk.dart
+++ packages/flutter_tools/lib/src/commands/build_apk.dart
@@ -11,7 +11,6 @@ import '../artifacts.dart';
 import '../base/common.dart';
 import '../base/file_system.dart';
 import '../base/logger.dart';
-import '../base/os.dart';
 import '../base/process.dart';
 import '../base/process_manager.dart';
 import '../base/utils.dart';
@@ -70,14 +69,14 @@ class _ApkBuilder {
   File _aapt;
   File _dx;
   File _zipalign;
-  File _jarsigner;
+  File _apksigner;
 
   _ApkBuilder(this.sdk) {
     _androidJar = fs.file(sdk.androidJarPath);
     _aapt = fs.file(sdk.aaptPath);
     _dx = fs.file(sdk.dxPath);
     _zipalign = fs.file(sdk.zipalignPath);
-    _jarsigner = os.which('jarsigner');
+    _apksigner = fs.file(sdk.apksignerPath);
   }
 
   String checkDependencies() {
@@ -89,10 +88,8 @@ class _ApkBuilder {
       return 'Cannot find dx at ${_dx.path}';
     if (!processManager.canRun(_zipalign.path))
       return 'Cannot find zipalign at ${_zipalign.path}';
-    if (_jarsigner == null)
-      return 'Cannot find jarsigner in PATH.';
-    if (!_jarsigner.existsSync())
-      return 'Cannot find jarsigner at ${_jarsigner.path}';
+    if (!processManager.canRun(_apksigner.path))
+      return 'Cannot find apksigner at ${_apksigner.path}';
     return null;
   }
 
@@ -125,15 +122,13 @@ class _ApkBuilder {
   }
 
   void sign(File keystore, String keystorePassword, String keyAlias, String keyPassword, File outputApk) {
-    assert(_jarsigner != null);
-    runCheckedSync(<String>[_jarsigner.path,
-      '-keystore', keystore.path,
-      '-storepass', keystorePassword,
-      '-keypass', keyPassword,
-      '-digestalg', 'SHA1',
-      '-sigalg', 'MD5withRSA',
+    assert(_apksigner != null);
+    runCheckedSync(<String>[_apksigner.path, 'sign',
+      '--ks', keystore.path,
+      '--ks-key-alias', keyAlias,
+      '--ks-pass', 'pass:$keystorePassword',
+      '--key-pass', 'pass:$keyPassword',
       outputApk.path,
-      keyAlias,
     ]);
   }
 
@@ -357,14 +352,13 @@ int _buildApk(
       unalignedApk, components.manifest, assetBuilder.directory,
       artifactBuilder.directory, components.resources, buildMode
     );
-
-    int signResult = _signApk(builder, components, unalignedApk, keystore, buildMode);
-    if (signResult != 0)
-      return signResult;
-
     File finalApk = fs.file(outputFile);
     ensureDirectoryExists(finalApk.path);
     builder.align(unalignedApk, finalApk);
+
+    int signResult = _signApk(builder, components, finalApk, keystore, buildMode);
+    if (signResult != 0)
+      return signResult;
 
     printTrace('calculateSha: $outputFile');
     File apkShaFile = fs.file('$outputFile.sha1');
