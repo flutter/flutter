bffafa4cada2d9e36ac8daac744d227ec8f82317
Adam Barth <abarth@chromium.org>
2017-03-01 22:16:07 -0800

    Improve docs for Center
    Fixes #8478

--- packages/flutter/lib/src/widgets/basic.dart
+++ packages/flutter/lib/src/widgets/basic.dart
@@ -710,6 +710,14 @@ class Align extends SingleChildRenderObjectWidget {
 
 /// A widget that centers its child within itself.
 ///
+/// This widget will be as big as possible if its dimensions are constrained and
+/// [widthFactor] and [heightFactor] are null. If a dimension is unconstrained
+/// and the corresponding size factor is null then the widget will match its
+/// child's size in that dimension. If a size factor is non-null then the
+/// corresponding dimension of this widget will be the product of the child's
+/// dimension and the size factor. For example if widthFactor is 2.0 then
+/// the width of this widget will always be twice its child's width.
+///
 /// See also:
 ///
 ///  * [Align], which lets you arbitrarily position a child within itself,
