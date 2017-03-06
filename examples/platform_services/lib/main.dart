22455887dab8d4fd866bc84373b41541f9f05d0d
Sarah Zakarias <zarah@google.com>
2017-03-06 11:28:08 +0100

    make variable final

--- examples/platform_services/lib/main.dart
+++ examples/platform_services/lib/main.dart
@@ -23,7 +23,7 @@ class _PlatformServicesState extends State<PlatformServices> {
       batteryLevel = "iOS is not supported yet.";
     } else {
       try {
-        int result = await platform.invokeMethod('getBatteryLevel');
+        final int result = await platform.invokeMethod('getBatteryLevel');
         batteryLevel = 'Battery level at $result. %';
       } on PlatformException catch (e) {
         batteryLevel = "Failed to get battery level: '${e.message}'.";
