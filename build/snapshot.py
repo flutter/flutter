cd417b0519153a3c8b232ab321683341a0efdb3a
Michael Goderbauer <goderbauer@google.com>
2017-03-14 10:24:17 -0700

    Remove sky_snapshot sources (#3471)
    * Remove sky_snapshot sources
    
    * update licenses

--- build/snapshot.py
+++ build/snapshot.py
@@ -14,29 +14,33 @@ def main():
 
   parser.add_argument('--snapshotter-path', type=str, required=True,
                       help='The Flutter snapshotter')
-  parser.add_argument('--app-dir', type=str, required=True,
-                      help='The root of the app')
+  parser.add_argument('--vm-snapshot-data', type=str, required=True,
+                      help='Path to vm_isolate_snapshot.bin')
+  parser.add_argument('--isolate-snapshot-data', type=str, required=True,
+                      help='Path to isolate_snapshot.bin')
   parser.add_argument('--main-dart', type=str, required=True,
                       help='The main.dart file to use')
   parser.add_argument('--packages', type=str, required=True,
                       help='The package map to use')
   parser.add_argument('--snapshot', type=str, required=True,
-                      help='Path to application snapshot')
+                      help='Where to output application snapshot')
   parser.add_argument('--depfile', type=str, required=True,
                       help='Where to output dependency information')
-  parser.add_argument('--build-output', type=str, required=True,
-                      help='Target name used in the depfile')
+  parser.add_argument('--root-build-dir', type=str, required=True,
+                      help='The root build dir for --depfile and --snapshot')
 
   args = parser.parse_args()
 
   result = subprocess.call([
     args.snapshotter_path,
+    '--snapshot_kind=script',
+    '--vm_snapshot_data=%s' % args.vm_snapshot_data,
+    '--isolate_snapshot_data=%s' % args.isolate_snapshot_data,
     '--packages=%s' % args.packages,
-    '--snapshot=%s' % args.snapshot,
-    '--depfile=%s' % args.depfile,
-    '--build-output=%s' % args.build_output,
+    '--script_snapshot=%s' % args.snapshot,
+    '--dependencies=%s' % args.depfile,
     args.main_dart,
-  ], cwd=args.app_dir)
+  ], cwd=args.root_build_dir)
 
   return result
 
