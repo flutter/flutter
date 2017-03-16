79002c74538dcb82b866d4413d93b7f8e47b4974
Michael Goderbauer <goderbauer@google.com>
2017-03-16 13:47:14 -0700

    add tasks for Windows bots in device lab (#8827)

---
+++ dev/devicelab/bin/tasks/hot_mode_dev_cycle_win__benchmark.dart
@@ -0,0 +1,9 @@
+// Copyright (c) 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'hot_mode_dev_cycle__benchmark.dart' as hot_mode_dev_cycle_benchmark;
+
+void main() {
+  hot_mode_dev_cycle_benchmark.main();
+}
