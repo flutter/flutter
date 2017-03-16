7b1bcd5157647a63e1535057a9e8c2a14431e437
Michael Goderbauer <goderbauer@google.com>
2017-03-16 16:59:50 -0700

    Remove nasty ANSI workaround for Windows
    The problem has been fixed upstream in the Dart VM.
    
    This simplifies our setup instructions on Windows (will update the wiki).
    Furthermore, this also means that going forward there is no diffrence between PowerShell and Cmd for the Flutter experience on Windows.

--- packages/flutter_tools/lib/src/base/logger.dart
+++ packages/flutter_tools/lib/src/base/logger.dart
@@ -249,10 +249,12 @@ enum _LogType {
 
 class AnsiTerminal {
   AnsiTerminal() {
-    final String term = platform.environment['TERM'];
-    // FLUTTER_ANSI_TERMINAL is a work-around for https://github.com/dart-lang/sdk/issues/28614
-    final bool flutterAnsiTerm = platform.environment.containsKey('FLUTTER_ANSI_TERMINAL');
-    supportsColor = (term != null && term != 'dumb') || flutterAnsiTerm;
+    if (platform.isWindows) {
+      supportsColor = platform.ansiSupported;
+    } else {
+      final String term = platform.environment['TERM'];
+      supportsColor = (term != null && term != 'dumb');
+    }
   }
 
   static const String KEY_F1  = '\u001BOP';
