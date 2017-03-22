43d007f3b39b9f094aabb64e3baf51953039cc89
Sarah Zakarias <zarah@google.com>
2017-03-22 13:01:57 +0100

    removed exception

--- examples/platform_services/android/app/src/main/java/com/example/flutter/MainActivity.java
+++ examples/platform_services/android/app/src/main/java/com/example/flutter/MainActivity.java
@@ -37,8 +37,6 @@ public class MainActivity extends FlutterActivity {
               } else {
                 response.error("UNAVAILABLE", "Battery level not available.", null);
               }
-            } else {
-              throw new IllegalArgumentException("Unknown method " + call.method);
             }
           }
     });
