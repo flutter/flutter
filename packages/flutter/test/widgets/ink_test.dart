0959725a47848133fa7b98704dd1f8f5a2511bd0
Luke <freeman.luke@gmail.com>
2017-03-27 12:52:45 -0700

    remove unused import

--- packages/flutter/test/widgets/ink_test.dart
+++ packages/flutter/test/widgets/ink_test.dart
@@ -1,4 +1,3 @@
-import '../rendering/mock_canvas.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_test/src/widget_tester.dart';
