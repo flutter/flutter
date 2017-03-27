657a4c3fc64fcd0d9f3767e6f87db6ce811a56a1
Sarah Zakarias <zarah@google.com>
2017-03-27 14:33:15 +0200

    update platform_channel package name (#9021)
    * update platform_channel package name
    
    * lowercase name

--- examples/platform_channel/android/app/src/androidTest/java/com/example/platformchannel/ExampleInstrumentedTest.java
+++ examples/platform_channel/android/app/src/androidTest/java/com/example/platformchannel/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.flutter;
+package com.example.platformchannel;
 
 import android.graphics.Bitmap;
 import android.support.test.InstrumentationRegistry;
--- examples/platform_channel/android/app/src/main/java/com/example/platformchannel/MainActivity.java
+++ examples/platform_channel/android/app/src/main/java/com/example/platformchannel/MainActivity.java
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package com.example.flutter;
+package com.example.platformchannel;
 
 import android.content.ContextWrapper;
 import android.content.Intent;
