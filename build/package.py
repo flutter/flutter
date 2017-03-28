2768d2f31ce806e7e388c3fffa05c6c479fa85bd
P.Y. Laligand <py.laligand@gmail.com>
2017-03-28 13:14:02 -0700

    Use the new flutter_tools app. (#3533)

--- build/package.py
+++ build/package.py
@@ -12,16 +12,10 @@ import sys
 def main():
   parser = argparse.ArgumentParser(description='Package a Flutter application')
 
-  parser.add_argument('--root', type=str, required=True,
-                      help='The root of the output directory')
   parser.add_argument('--flutter-root', type=str, required=True,
                       help='The root of the Flutter SDK')
-  parser.add_argument('--dart', type=str, required=True,
-                      help='The Dart binary to use')
-  parser.add_argument('--flutter-tools-packages', type=str, required=True,
-                      help='The package map for the Flutter tool')
-  parser.add_argument('--flutter-tools-main', type=str, required=True,
-                      help='The main.dart file for the Flutter tool')
+  parser.add_argument('--flutter-tools', type=str, required=True,
+                      help='The executable for the Flutter tool')
   parser.add_argument('--working-dir', type=str, required=True,
                       help='The directory where to put intermediate files')
   parser.add_argument('--app-dir', type=str, required=True,
@@ -41,13 +35,10 @@ def main():
   args = parser.parse_args()
 
   env = os.environ.copy()
-  env['LD_LIBRARY_PATH'] = args.root
   env['FLUTTER_ROOT'] = args.flutter_root
 
   call_args = [
-    args.dart,
-    '--packages=%s' % args.flutter_tools_packages,
-    args.flutter_tools_main,
+    args.flutter_tools,
     '--working-dir=%s' % args.working_dir,
     '--packages=%s' % args.packages,
     '--snapshot=%s' % args.snapshot,
