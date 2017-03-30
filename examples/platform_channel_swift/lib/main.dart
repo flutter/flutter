a94e995f3e838e343b3b496b2f51e368c14a6e1f
Sarah Zakarias <zarah@google.com>
2017-03-30 15:11:19 +0200

    Add swift version of Platform Channel example (#9098)
    * wip
    
    * wip
    
    * delete main.m
    
    * readd main.m
    
    * update to use new channel api
    
    * update android
    
    * removed android/
    
    * remove debug print
    
    * remomved main.m
    
    * Update year in copyright
    
    * break long line and update name
    
    * mit comments
    
    * update examples/README
    
    * break line
    
    * update README
    
    * update test

---
+++ examples/platform_channel_swift/lib/main.dart
@@ -0,0 +1,53 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class PlatformChannel extends StatefulWidget {
+  @override
+  _PlatformChannelState createState() => new _PlatformChannelState();
+}
+
+class _PlatformChannelState extends State<PlatformChannel> {
+  static const PlatformMethodChannel platform = const PlatformMethodChannel('samples.flutter.io/battery');
+  String _batteryLevel = '';
+
+  Future<Null> _getBatteryLevel() async {
+    String batteryLevel;
+    try {
+      final int result = await platform.invokeMethod('getBatteryLevel');
+      batteryLevel = 'Battery level at $result % .';
+    } on PlatformException catch (e) {
+      batteryLevel = "Failed to get battery level: '${e.message}'.";
+    } 
+    setState(() {
+      _batteryLevel = batteryLevel;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Material(
+      child: new Center(
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            new RaisedButton(
+              child: new Text('Get Battery Level'),
+              onPressed: _getBatteryLevel,
+            ),
+            new Text(_batteryLevel, key: new Key('Battery level label')),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(new PlatformChannel());
+}
