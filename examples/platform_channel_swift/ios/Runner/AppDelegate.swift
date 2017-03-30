7caad2d6526d66ab06e7f20aa069ccc4d375b5f1
Sarah Winther Zakarias <zarah@google.com>
2017-03-30 11:44:45 +0200

    remomved main.m

--- examples/platform_channel_swift/ios/Runner/AppDelegate.swift
+++ examples/platform_channel_swift/ios/Runner/AppDelegate.swift
@@ -5,6 +5,7 @@
 import UIKit
 import Flutter
 
+@UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
 
   override func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
