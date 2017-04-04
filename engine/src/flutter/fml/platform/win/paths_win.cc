4de155b1e374cc98df27e06924e33b0bd1ab412f
Michael Goderbauer <goderbauer@google.com>
2017-04-04 14:00:51 -0700

    add paths_win.cc to FML (flutter/engine#3563)
    * add paths_win.cc to FML
    
    * update licenses

---
+++ engine/src/flutter/fml/platform/win/paths_win.cc
@@ -0,0 +1,28 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "flutter/fml/paths.h"
+
+#include <windows.h>
+
+#include "lib/ftl/files/path.h"
+
+namespace fml {
+namespace paths {
+
+std::pair<bool, std::string> GetExecutableDirectoryPath() {
+  HMODULE module = GetModuleHandle(NULL);
+  if (module == NULL) {
+    return {false, ""};
+  }
+  char path[MAX_PATH];
+  DWORD read_size = GetModuleFileNameA(module, path, MAX_PATH);
+  if (read_size == 0 || read_size == MAX_PATH) {
+    return {false, ""};
+  }
+  return {true, files::GetDirectoryName(std::string{path, read_size})};
+}
+
+}  // namespace paths
+}  // namespace fml
