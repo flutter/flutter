a86b9ffb85fe5aece7e40c94806f9d00287cf47f
Xiao Yu <xster@google.com>
2017-04-12 17:18:09 -0700

    Add comments

--- packages/flutter_tools/templates/create/ios.tmpl/Runner/AppDelegate.m
+++ packages/flutter_tools/templates/create/ios.tmpl/Runner/AppDelegate.m
@@ -21,6 +21,10 @@
 - (void)applicationDidEnterBackground:(UIApplication *)application {
   // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
   // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+
+  // The following keeps the Flutter session alive when the device screen locks
+  // in debug mode. It allows continued use of features like hot reload and 
+  // taking screenshots once the device unlocks again.
   if ([@"debug" isEqualToString:
       [[NSBundle mainBundle] objectForInfoDictionaryKey:@"FLTBuildMode"]]) {
     [application beginBackgroundTaskWithName:@"Flutter debug task"
