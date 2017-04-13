0ccef297016c3deb2fb74a85ba7b84ddd7d49100
Xiao Yu <xster@google.com>
2017-04-12 22:52:18 -0700

    Create DecoratedBoxTransition

--- packages/flutter/lib/src/widgets/transitions.dart
+++ packages/flutter/lib/src/widgets/transitions.dart
@@ -8,6 +8,7 @@ import 'package:flutter/foundation.dart';
 import 'package:vector_math/vector_math_64.dart' show Matrix4;
 
 import 'basic.dart';
+import 'container.dart';
 import 'framework.dart';
 
 export 'package:flutter/rendering.dart' show RelativeRect;
@@ -383,6 +384,43 @@ class RelativePositionedTransition extends AnimatedWidget {
       right: offsets.right,
       bottom: offsets.bottom,
       left: offsets.left,
+      child: child,
+    );
+  }
+}
+
+/// Animated version of a [DecoratedBox] that animates the different properties
+/// of its [Decoration].
+class DecoratedBoxTransition extends AnimatedWidget {
+  /// Creates an animated [DecorationBox] whose [Decoration] animation updates
+  /// the widget. 
+  /// 
+  /// `decoration` and `position` cannot be null.
+  /// 
+  /// Also see [DecoratedBox].
+  DecoratedBoxTransition({
+    Key key,
+    @required this.decoration,
+    this.position: DecorationPosition.background,
+    @required this.child,
+  }) : super(key: key, listenable: decoration);
+
+  /// Animation of the decoration to paint.
+  ///
+  /// Can be created using a [DecorationTween].
+  final Animation<Decoration> decoration;
+
+  /// Whether to paint the box decoration behind or in front of the child.
+  final DecorationPosition position;
+
+  /// The widget below this widget in the tree.
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return new DecoratedBox(
+      decoration: decoration.value,
+      position: position,
       child: child,
     );
   }
