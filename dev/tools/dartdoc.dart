2f95288e77c1a932eb14afd993c609793e4dfa0e
Adam Barth <abarth@chromium.org>
2017-04-19 15:23:02 -0700

    Update engine

--- dev/tools/dartdoc.dart
+++ dev/tools/dartdoc.dart
@@ -113,8 +113,8 @@ void createFooter(String footerPath) {
 void sanityCheckDocs() {
   final List<String> canaries = <String>[
     '$kDocRoot/api/dart.io/File-class.html',
-    '$kDocRoot/api/dart_ui/Canvas-class.html',
-    '$kDocRoot/api/dart_ui/Canvas/drawRect.html',
+    '$kDocRoot/api/dart.ui/Canvas-class.html',
+    '$kDocRoot/api/dart.ui/Canvas/drawRect.html',
     '$kDocRoot/api/flutter_test/WidgetTester/pumpWidget.html',
     '$kDocRoot/api/material/Material-class.html',
     '$kDocRoot/api/material/Tooltip-class.html',
