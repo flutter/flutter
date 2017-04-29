52d0dac458e72e81dd0bcf2779db31fa6e0f7356
Hans Muller <hansmuller@chromium.org>
2017-04-28 17:07:29 -0700

    AnimatedList sample app

---
+++ examples/samples/animated_list/ios/Runner/main.m
@@ -0,0 +1,10 @@
+#import <UIKit/UIKit.h>
+#import <Flutter/Flutter.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil,
+                                 NSStringFromClass([AppDelegate class]));
+    }
+}
