534faaa31ecdc25400fd774e8517d19b5706cbca
Chris Bracken <chris@bracken.jp>
2017-05-04 18:22:02 -0700

    Update Hello World plugin integration
    Enable plugin infra for both iOS and Android post-#9791.

---
+++ examples/hello_world/ios/Runner/PluginRegistry.m
@@ -0,0 +1,15 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "PluginRegistry.h"
+
+@implementation PluginRegistry
+
+- (instancetype)initWithController:(FlutterViewController *)controller {
+  if (self = [super init]) {
+  }
+  return self;
+}
+
+@end
