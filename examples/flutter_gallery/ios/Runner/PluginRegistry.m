0fadcd0f32a8f5c661e71fb752ddadb6395e1840
Ian Hickson <ian@hixie.ch>
2017-05-08 09:34:52 -0700

    Revert "Revert "Revert plugin use in gallery (#9838)" (#9877)"
    This reverts commit ede72a804305e1d8495dfa1e463115ec6d5ad3c6.

--- examples/flutter_gallery/ios/Runner/PluginRegistry.m
+++ examples/flutter_gallery/ios/Runner/PluginRegistry.m
@@ -8,7 +8,6 @@
 
 - (instancetype)initWithController:(FlutterViewController *)controller {
   if (self = [super init]) {
-    _url_launcher = [[UrlLauncherPlugin alloc] initWithController:controller];
   }
   return self;
 }
