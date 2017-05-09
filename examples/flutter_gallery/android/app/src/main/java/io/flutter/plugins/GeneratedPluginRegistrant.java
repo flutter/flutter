a7fd74d60309f51ccc9ef41bb8031028edcff329
Sarah Zakarias <zarah@google.com>
2017-05-09 13:48:08 +0200

    add GeneratedPluginRegistrant.java

---
+++ examples/flutter_gallery/android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
@@ -0,0 +1,13 @@
+package io.flutter.plugins;
+
+import io.flutter.plugin.common.PluginRegistry;
+import io.flutter.plugins.url_launcher.UrlLauncherPlugin;
+
+/**
+ * Generated file. Do not edit.
+ */
+public final class GeneratedPluginRegistrant {
+  public static void registerWith(PluginRegistry registry) {
+    UrlLauncherPlugin.registerWith(registry.registrarFor("io.flutter.plugins.url_launcher.UrlLauncherPlugin"));
+  }
+}
