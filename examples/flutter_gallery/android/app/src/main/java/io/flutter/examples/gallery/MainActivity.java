4e3a4ce8303a137d74d562d05f6500a097d425f0
Sarah Zakarias <zarah@google.com>
2017-05-09 14:00:06 +0200

    Update Flutter Gallery to new plugin registry (#9914)

--- examples/flutter_gallery/android/app/src/main/java/io/flutter/examples/gallery/MainActivity.java
+++ examples/flutter_gallery/android/app/src/main/java/io/flutter/examples/gallery/MainActivity.java
@@ -3,15 +3,13 @@ package io.flutter.examples.gallery;
 import android.os.Bundle;
 
 import io.flutter.app.FlutterActivity;
-import io.flutter.plugins.PluginRegistry;
+import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
-    PluginRegistry pluginRegistry;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        pluginRegistry = new PluginRegistry();
-        pluginRegistry.registerAll(this);
+        GeneratedPluginRegistrant.registerWith(this);
     }
 }
