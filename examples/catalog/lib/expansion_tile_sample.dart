dfe79e01496d3402b29ba58d5b722fe240573505
Hans Muller <hansmuller@chromium.org>
2017-05-10 11:05:26 -0700

    Added sample, still need a test, doc

---
+++ examples/catalog/lib/expansion_tile_sample.dart
@@ -0,0 +1,85 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+
+class Entry {
+  Entry(this.title, [this.children]);
+  final String title;
+  final List<Entry> children;
+}
+
+final List<Entry> data = <Entry>[
+  new Entry('Chapter A',
+    <Entry>[
+      new Entry('Section A0',
+        <Entry>[
+          new Entry('Item A0.1'),
+          new Entry('Item A0.2'),
+          new Entry('Item A0.3'),
+        ],
+      ),
+      new Entry('Section A1'),
+      new Entry('Section A2'),
+    ],
+  ),
+  new Entry('Chapter B',
+    <Entry>[
+      new Entry('Section B0'),
+      new Entry('Section B1'),
+    ],
+  ),
+  new Entry('Chapter C',
+    <Entry>[
+      new Entry('Section C0'),
+      new Entry('Section C1'),
+      new Entry('Section C2',
+        <Entry>[
+          new Entry('Item C2.0'),
+          new Entry('Item C2.1'),
+          new Entry('Item C2.2'),
+          new Entry('Item C2.3'),
+        ],
+      ),
+    ],
+  ),
+];
+
+class EntryItem extends StatelessWidget {
+  EntryItem(this.entry);
+
+  final Entry entry;
+
+  Widget _buildTiles(Entry root) {
+    if (root.children == null)
+      return new ListTile(title: new Text(root.title));
+    return new ExpansionTile(
+      title: new Text(root.title),
+      children: root.children.map(_buildTiles).toList(),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _buildTiles(entry);
+  }
+}
+
+class ExpansionTileSample extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: const Text('ExpansionTile'),
+      ),
+      body: new ListView(
+        children: data.map((Entry entry) => new EntryItem(entry)).toList(),
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(new MaterialApp(home: new ExpansionTileSample()));
+}
