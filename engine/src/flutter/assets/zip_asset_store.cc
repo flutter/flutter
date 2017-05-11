4626789ca24f75d8d916e5731ec81f334bc9d802
Chinmay Garde <chinmaygarde@gmail.com>
2017-05-10 17:11:08 -0700

    Create a cache of unz_file_pos entries in ZipAssetStore for faster lookup. (flutter/engine#3682)

--- engine/src/flutter/assets/zip_asset_store.cc
+++ engine/src/flutter/assets/zip_asset_store.cc
@@ -7,34 +7,41 @@
 #include <fcntl.h>
 #include <unistd.h>
 
+#include <string>
 #include <utility>
 
+#include "flutter/fml/trace_event.h"
 #include "lib/ftl/files/eintr_wrapper.h"
 #include "lib/ftl/files/unique_fd.h"
 #include "lib/zip/unique_unzipper.h"
-#include "third_party/zlib/contrib/minizip/unzip.h"
 
 namespace blink {
 
 ZipAssetStore::ZipAssetStore(UnzipperProvider unzipper_provider)
-    : unzipper_provider_(std::move(unzipper_provider)) {}
+    : unzipper_provider_(std::move(unzipper_provider)) {
+  BuildStatCache();
+}
 
-ZipAssetStore::~ZipAssetStore() {}
+ZipAssetStore::~ZipAssetStore() = default;
 
 bool ZipAssetStore::GetAsBuffer(const std::string& asset_name,
                                 std::vector<uint8_t>* data) {
-  zip::UniqueUnzipper unzipper = unzipper_provider_();
-  if (!unzipper.is_valid())
-    return false;
+  TRACE_EVENT0("flutter", "ZipAssetStore::GetAsBuffer");
+  auto found = stat_cache_.find(asset_name);
 
-  int result = unzLocateFile(unzipper.get(), asset_name.c_str(), 0);
-  if (result != UNZ_OK) {
+  if (found == stat_cache_.end()) {
     return false;
   }
 
-  unz_file_info file_info;
-  result = unzGetCurrentFileInfo(unzipper.get(), &file_info, nullptr, 0,
-                                 nullptr, 0, nullptr, 0);
+  auto unzipper = unzipper_provider_();
+
+  if (!unzipper.is_valid()) {
+    return false;
+  }
+
+  int result = UNZ_OK;
+
+  result = unzGoToFilePos(unzipper.get(), &(found->second.file_pos));
   if (result != UNZ_OK) {
     FTL_LOG(WARNING) << "unzGetCurrentFileInfo failed, error=" << result;
     return false;
@@ -46,17 +53,60 @@ bool ZipAssetStore::GetAsBuffer(const std::string& asset_name,
     return false;
   }
 
-  data->resize(file_info.uncompressed_size);
+  data->resize(found->second.uncompressed_size);
   int total_read = 0;
   while (total_read < static_cast<int>(data->size())) {
     int bytes_read = unzReadCurrentFile(
         unzipper.get(), data->data() + total_read, data->size() - total_read);
-    if (bytes_read <= 0)
+    if (bytes_read <= 0) {
       return false;
+    }
     total_read += bytes_read;
   }
 
   return true;
+}
+
+void ZipAssetStore::BuildStatCache() {
+  TRACE_EVENT0("flutter", "ZipAssetStore::BuildStatCache");
+  auto unzipper = unzipper_provider_();
+
+  if (!unzipper.is_valid()) {
+    return;
+  }
+
+  if (unzGoToFirstFile(unzipper.get()) != UNZ_OK) {
+    return;
+  }
+
+  do {
+    int result = UNZ_OK;
+
+    // Get the current file name.
+    unz_file_info file_info = {};
+    char file_name[255];
+    result = unzGetCurrentFileInfo(unzipper.get(), &file_info, file_name,
+                                   sizeof(file_name), nullptr, 0, nullptr, 0);
+    if (result != UNZ_OK) {
+      continue;
+    }
+
+    if (file_info.uncompressed_size == 0) {
+      continue;
+    }
+
+    // Get the current file position.
+    unz_file_pos file_pos = {};
+    result = unzGetFilePos(unzipper.get(), &file_pos);
+    if (result != UNZ_OK) {
+      continue;
+    }
+
+    std::string file_name_key(file_name, file_info.size_filename);
+    CacheEntry entry(file_pos, file_info.uncompressed_size);
+    stat_cache_.emplace(std::move(file_name_key), std::move(entry));
+
+  } while (unzGoToNextFile(unzipper.get()) == UNZ_OK);
 }
 
 }  // namespace blink
