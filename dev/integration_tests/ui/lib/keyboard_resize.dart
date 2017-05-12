303d4dcbc0204691d1e5c72ffd90459b5432fe74
Jason Simmons <jason-simmons@users.noreply.github.com>
2017-05-11 17:09:29 -0700

    fix an analyzer warning (#10018)

--- dev/integration_tests/ui/lib/keyboard_resize.dart
+++ dev/integration_tests/ui/lib/keyboard_resize.dart
@@ -55,7 +55,7 @@ class _MyHomePageState extends State<MyHomePage> {
         key: const Key(keys.kUnfocusButton),
         onPressed: () { textField.focusNode.unfocus(); },
         tooltip: 'Unfocus',
-        child: new Icon(Icons.done),
+        child: const Icon(Icons.done),
       ),
     );
   }
