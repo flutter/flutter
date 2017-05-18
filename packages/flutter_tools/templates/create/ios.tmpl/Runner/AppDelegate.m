ce75cbcbc2bb1ee7e94903eaf2cbbb7815bbc02f
Mikkel Ravn <mravn@google.com>
2017-05-18 08:56:24 +0200

    Re-added comment on where to add code

--- packages/flutter_tools/templates/create/ios.tmpl/Runner/AppDelegate.m
+++ packages/flutter_tools/templates/create/ios.tmpl/Runner/AppDelegate.m
@@ -5,6 +5,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
   [GeneratedPluginRegistrant registerWithRegistry:self];
+  // Override point for customization after application launch.
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
