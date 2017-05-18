8e8d1e81875f06d5be2feadaad28ada872d46bd2
Sarah Zakarias <zarah@google.com>
2017-05-18 14:47:34 +0200

    wip

--- examples/full_platform_view/android/app/src/main/java/com/yourcompany/full_platform_view/MainActivity.java
+++ examples/full_platform_view/android/app/src/main/java/com/yourcompany/full_platform_view/MainActivity.java
@@ -13,7 +13,7 @@ import io.flutter.plugins.GeneratedPluginRegistrant;
 import io.flutter.view.FlutterMain;
 import io.flutter.view.FlutterView;
 
-public class MainActivity extends AppCompatActivity  {
+public class MainActivity extends FlutterActivity  {
   private FlutterView flutterView;
 
   private static final String CHANNEL = "samples.flutter.io/full_screen";
@@ -21,38 +21,13 @@ public class MainActivity extends AppCompatActivity  {
   static final int COUNT_REQUEST = 42;
 
   private BasicMessageChannel messageChannel;
-  private String[] getArgsFromIntent(Intent intent) {
-    // Before adding more entries to this list, consider that arbitrary
-    // Android applications can generate intents with extra data and that
-    // there are many security-sensitive args in the binary.
-    ArrayList<String> args = new ArrayList<String>();
-    if (intent.getBooleanExtra("trace-startup", false)) {
-      args.add("--trace-startup");
-    }
-    if (intent.getBooleanExtra("start-paused", false)) {
-      args.add("--start-paused");
-    }
-    if (intent.getBooleanExtra("enable-dart-profiling", false)) {
-      args.add("--enable-dart-profiling");
-    }
-    if (!args.isEmpty()) {
-      String[] argsArray = new String[args.size()];
-      return args.toArray(argsArray);
-    }
-    return null;
-  }
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-   // GeneratedPluginRegistrant.registerWith(this);
+    GeneratedPluginRegistrant.registerWith(this);
 
-    String[] args = getArgsFromIntent(getIntent());
-    FlutterMain.ensureInitializationComplete(getApplicationContext(), args);
-    setContentView(R.layout.flutter_view_layout);
-
-    flutterView = (FlutterView) findViewById(R.id.flutter_view);
-    flutterView.runFromBundle(FlutterMain.findAppBundlePath(getApplicationContext()), null);
+    flutterView = getFlutterView();
     messageChannel = new BasicMessageChannel<>(flutterView, CHANNEL, StringCodec.INSTANCE);
     messageChannel.
             setMessageHandler(new BasicMessageChannel.MessageHandler<String>() {
@@ -72,33 +47,10 @@ public class MainActivity extends AppCompatActivity  {
 
   @Override
   protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-
-    System.out.println("JUHUUUU result" + resultCode + "request code " + requestCode + "data " + data.getIntExtra("count", 0));
-    System.out.println("Flutter view : " + flutterView);
     if (requestCode == COUNT_REQUEST) {
       if (resultCode == RESULT_OK) {
         messageChannel.send("7");
       }
     }
-   // flutterView.onPostResume();
-  }
-  @Override
-  protected void onDestroy() {
-    if (flutterView != null) {
-      flutterView.destroy();
-    }
-    super.onDestroy();
-  }
-
-  @Override
-  protected void onPause() {
-    super.onPause();
-    flutterView.onPause();
-  }
-
-  @Override
-  protected void onPostResume() {
-    super.onPostResume();
-    flutterView.onPostResume();
   }
 }
