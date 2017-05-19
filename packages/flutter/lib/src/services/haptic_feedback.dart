48237d54c1c3546854ec44242cac05e082869854
Chris Bracken <chris@bracken.jp>
2017-05-19 14:50:11 -0700

    Small doc clarification to HapticFeedback.vibrate (#10217)
    Eliminate the implementation detail of which function is called to
    trigger vibration on iOS, instead report the vibration constant.
    
    Also note that not all iOS devices support haptic feedback (e.g., older
    iPod touch devices).

--- packages/flutter/lib/src/services/haptic_feedback.dart
+++ packages/flutter/lib/src/services/haptic_feedback.dart
@@ -15,8 +15,8 @@ class HapticFeedback {
 
   /// Provides haptic feedback to the user for a short duration.
   ///
-  /// On iOS, this uses the platform "sound" for vibration (via
-  /// `AudioServicesPlaySystemSound`).
+  /// On iOS devices that support haptic feedback, this uses the default system
+  /// vibration value (`kSystemSoundID_Vibrate`).
   ///
   /// On Android, this uses the platform haptic feedback API to simulates a
   /// short tap on a virtual keyboard.
