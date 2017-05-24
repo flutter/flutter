71160fbaced26ac2d4edbd7ba66fa3e0ce139e22
Mikkel Ravn <mravn@google.com>
2017-05-24 11:13:48 +0200

    Changed template indentation to 2 spaces

--- packages/flutter_tools/templates/create/ios-swift.tmpl/Runner/AppDelegate.swift
+++ packages/flutter_tools/templates/create/ios-swift.tmpl/Runner/AppDelegate.swift
@@ -3,10 +3,11 @@ import Flutter
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-    override func application(
-        _ application: UIApplication,
-        didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        GeneratedPluginRegistrant.register(with: self);
-        return super.application(application, didFinishLaunchingWithOptions: launchOptions);
-    }
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?
+  ) -> Bool {
+    GeneratedPluginRegistrant.register(with: self);
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions);
+  }
 }
