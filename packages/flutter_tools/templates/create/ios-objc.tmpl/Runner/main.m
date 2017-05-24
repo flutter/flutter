6762e869536338d89a6ec8441a3a3b2a269d7b78
Mikkel Nygaard Ravn <mravn@google.com>
2017-05-24 12:37:23 +0200

    Change template indentation to 2 spaces (#10289)

--- packages/flutter_tools/templates/create/ios-objc.tmpl/Runner/main.m
+++ packages/flutter_tools/templates/create/ios-objc.tmpl/Runner/main.m
@@ -3,8 +3,7 @@
 #import "AppDelegate.h"
 
 int main(int argc, char * argv[]) {
-    @autoreleasepool {
-        return UIApplicationMain(argc, argv, nil,
-                                 NSStringFromClass([AppDelegate class]));
-    }
+  @autoreleasepool {
+    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
 }
