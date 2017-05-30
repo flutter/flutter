d14bb2cdfcf4c49bbe9e7a7d89bee450a886622f
Sarah Zakarias <zarah@google.com>
2017-05-30 13:28:30 +0200

    Add example showing full-screen iOS/Android view from Flutter (#10386)

---
+++ examples/platform_view/ios/Runner/AppDelegate.h
@@ -0,0 +1,10 @@
+// Copyright 2017, the Flutter project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+#import <UIKit/UIKit.h>
+#import <Flutter/Flutter.h>
+#import "PlatformViewController.h"
+
+@interface AppDelegate : FlutterAppDelegate <PlatformViewControllerDelegate>
+@end
