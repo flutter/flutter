09de6f79da75d1c37dd46349a3217193fc070ee8
Gary Qian <GaryQian@users.noreply.github.com>
2017-06-01 17:50:03 -0700

    Add software as fallback if Vulkan and OpenGL fails (#3725)

--- shell/platform/android/platform_view_android.cc
+++ shell/platform/android/platform_view_android.cc
@@ -112,7 +112,12 @@ static std::unique_ptr<AndroidSurface> InitializePlatformSurface() {
     return surface;
   }
 
-  FTL_CHECK(false) << "Could not initialize either the Vulkan or OpenGL "
+  if (auto surface = InitializePlatformSurfaceSoftware()) {
+      FTL_DLOG(INFO) << "Software surface initialized.";
+      return surface;
+  }
+
+  FTL_CHECK(false) << "Could not initialize either the Vulkan, OpenGL, or Software"
                       "surface backends. Flutter requires a GPU to render.";
   return nullptr;
 }
