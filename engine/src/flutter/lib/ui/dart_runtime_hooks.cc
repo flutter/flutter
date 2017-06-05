72b41db25f9ce62a306b635c04cb684fbd6a4242
Chris Bracken <chris@bracken.jp>
2017-06-05 14:27:41 -0700

    Log to stdout on all platforms except Android (flutter/engine#3739)
    Fixes a Fuchsia breakage introduced in
    710df81249922f6270a9ef078348d2492480794c.

--- engine/src/flutter/lib/ui/dart_runtime_hooks.cc
+++ engine/src/flutter/lib/ui/dart_runtime_hooks.cc
@@ -143,11 +143,11 @@ void Logger_PrintString(Dart_NativeArguments args) {
     // Write to the logcat on Android.
     const char* tag = Settings::Get().log_tag.c_str();
     __android_log_print(ANDROID_LOG_INFO, tag, "%.*s", (int)length, chars);
-#elif defined(__APPLE__) || defined(OS_LINUX)
-    // On iOS and in flutter_tester (on both macOS and Linux, write directly to
-    // stdout. On iOS, this is redirected to ASL via
+#else
+    // On Fuchsia, iOS and in flutter_tester (on both macOS and Linux), write
+    // directly to stdout. On iOS, this is redirected to ASL via
     // RedirectIOConnectionsToSyslog in platform_mac.mm.
-    // TODO(cbracken) replace with dedicated (non-stdout) logging on iOS.
+    // TODO(cbracken): replace with dedicated (non-stdout) logging on iOS.
     fwrite(chars, 1, length, stdout);
     fputs("\n", stdout);
     fflush(stdout);
