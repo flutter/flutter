a84877222c8c349ce9c4bdbb4929cea47b1ffb7b
Ian Hickson <ian@hixie.ch>
2017-06-12 14:50:04 -0700

    Run flutter_tools tests serially (#10643)
    We suspect maybe recent failures are caused by race conditions from
    running flutter_tools tests in parallel.

--- dev/bots/test.dart
+++ dev/bots/test.dart
@@ -118,7 +118,7 @@ Future<Null> _pubRunTest(
   String workingDirectory, {
   String testPath,
 }) {
-  final List<String> args = <String>['run', 'test', '-rexpanded'];
+  final List<String> args = <String>['run', 'test', '-j1', '-rexpanded'];
   if (testPath != null)
     args.add(testPath);
   return _runCommand(pub, args, workingDirectory: workingDirectory);
