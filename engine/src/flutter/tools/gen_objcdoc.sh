d981e6764e9684a79ca41572c4fed00f2a841e7c
Sarah Zakarias <zarah@google.com>
2017-06-21 09:51:47 +0200

    Add script for generating ojbc doc for Flutter iOS libraries (flutter/engine#3788)

---
+++ engine/src/flutter/tools/gen_objcdoc.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+# Copyright 2017 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# Generates objc docs for Flutter iOS libraries.
+
+if [ ! -d "shell/platform/darwin/ios" ]
+  then
+      echo "Error: This script must be run at the root of the Flutter source tree."
+      exit 1
+fi
+
+if [ $# -eq 0 ]
+  then
+      echo "Error: Argument specifying output directory required."
+      exit 1
+fi
+
+jazzy \
+  --objc\
+  --clean\
+  --author Flutter Team\
+  --author_url 'https://flutter.io'\
+  --github_url 'https://github.com/flutter'\
+  --github-file-prefix 'http://github.com/flutter/engine/blob/master'\
+  --module-version 1.0.0\
+  --xcodebuild-arguments --objc,shell/platform/darwin/ios/framework/Headers/Flutter.h,--,-x,objective-c,-isysroot,$(xcrun --show-sdk-path),-I,$(pwd)\
+  --module Flutter\
+  --root-url https://docs.flutter.io/objc/\
+  --output $1\
+  --no-download-badge
