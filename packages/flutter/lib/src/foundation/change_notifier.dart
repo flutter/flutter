02cd6ca4fb9ae57c2497c0e3d13f59c5c5de9400
Jacob Richman <jacobr@google.com>
2017-06-20 18:13:28 -0700

    Change all ocurrences of '$runtimeType#$hashCode' to use the idAndType method. (#10871)
    * Change all instances of '$runtimeType#$hashCode' to use the describeIdentity method.
    
    The describeIdentity method generates a shorter description with a consistent length
    consisting of the runtime type and the a 5 hex character long truncated
    version of the hash code.

--- packages/flutter/lib/src/foundation/change_notifier.dart
+++ packages/flutter/lib/src/foundation/change_notifier.dart
@@ -7,6 +7,7 @@ import 'package:meta/meta.dart';
 import 'assertions.dart';
 import 'basic_types.dart';
 import 'observer_list.dart';
+import 'print.dart';
 
 /// An object that maintains a list of listeners.
 abstract class Listenable {
@@ -185,5 +186,5 @@ class ValueNotifier<T> extends ChangeNotifier {
   }
 
   @override
-  String toString() => '$runtimeType#$hashCode($value)';
+  String toString() => '${idAndType(this)}($value)';
 }
