2726f2852b2a27b6996a5dda8155d2721d6b3535
Mikkel Ravn <mravn@google.com>
2017-06-27 14:56:22 +0200

    Lint fix

--- packages/flutter_tools/lib/src/android/gradle.dart
+++ packages/flutter_tools/lib/src/android/gradle.dart
@@ -138,7 +138,7 @@ distributionBase=GRADLE_USER_HOME
 distributionPath=wrapper/dists
 zipStoreBase=GRADLE_USER_HOME
 zipStorePath=wrapper/dists
-distributionUrl=https\\://services.gradle.org/distributions/gradle-${gradleVersion}-all.zip
+distributionUrl=https\\://services.gradle.org/distributions/gradle-$gradleVersion-all.zip
 ''', flush: true,
     );
   }
