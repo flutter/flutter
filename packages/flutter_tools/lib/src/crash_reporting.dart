03792a9b3ace819ded17aa067f26b67e8ae5b340
Todd Volkert <tvolkert@google.com>
2017-06-27 15:49:24 -0700

    Minor fixes in tools:
    * Include the process' `stdout` and `stderr` when it returns a
      non-zero exit code in `runCheckedAsync()`
    * Defensively catch errors in `AndroidDevice.isAppInstalled()`
      and return false
    * Prepare for including the error message in crash reports - to
      be uncommented when https://github.com/flutter/flutter/wiki/Flutter-CLI-crash-reporting
      has been live for sufficient time for users to see it and have
      a chance to respond.

--- packages/flutter_tools/lib/src/crash_reporting.dart
+++ packages/flutter_tools/lib/src/crash_reporting.dart
@@ -90,6 +90,8 @@ class CrashReportSender {
       req.fields['osVersion'] = os.name;  // this actually includes version
       req.fields['type'] = _kDartTypeId;
       req.fields['error_runtime_type'] = '${error.runtimeType}';
+      // TODO(tvolkert): Uncomment this (flutter/flutter/11008)
+      //req.fields['error_message'] = '$error';
 
       final String stackTraceWithRelativePaths = new Chain.parse(stackTrace.toString()).terse.toString();
       req.files.add(new http.MultipartFile.fromString(
