92750833d5e74510feb60b0e96b65757edae8e74
xster <xiao@xster.net>
2017-07-12 19:15:54 -0700

    Stop sending cocoapods stats (#11120)
    * Disable cocoapods stats
    
    * Add a comment for code readers
    
    * fix new test

--- packages/flutter_tools/lib/src/ios/cocoapods.dart
+++ packages/flutter_tools/lib/src/ios/cocoapods.dart
@@ -117,7 +117,12 @@ class CocoaPods {
     final ProcessResult result = await processManager.run(
       <String>['pod', 'install', '--verbose'],
       workingDirectory: bundle.path,
-      environment: <String, String>{'FLUTTER_FRAMEWORK_DIR': engineDirectory},
+      environment: <String, String>{
+        'FLUTTER_FRAMEWORK_DIR': engineDirectory,
+        // See https://github.com/flutter/flutter/issues/10873.
+        // CocoaPods analytics adds a lot of latency.
+        'COCOAPODS_DISABLE_STATS': 'true',
+      },
     );
     status.stop();
     if (logger.isVerbose || result.exitCode != 0) {
