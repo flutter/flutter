dd2fb3c04bb090915b698266ace8680d2747c9db
Jason Simmons <jason-simmons@users.noreply.github.com>
2017-07-18 11:45:34 -0700

    Accumulate a batch of Skia objects that will be destructed later on the IO thread (#3888)
    See https://github.com/dart-lang/sdk/issues/29971

--- lib/ui/painting/utils.h
+++ lib/ui/painting/utils.h
@@ -2,15 +2,35 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "flutter/common/threads.h"
+#include "lib/ftl/synchronization/mutex.h"
 #include "third_party/skia/include/core/SkRefCnt.h"
 
+#include <queue>
+
 namespace blink {
+
+// A queue that holds Skia objects that must be destructed on the IO thread.
+class SkiaUnrefQueue {
+ public:
+  static SkiaUnrefQueue& Get();
+
+  void Unref(SkRefCnt* object);
+
+ private:
+  SkiaUnrefQueue();
+  void Drain();
+
+  static SkiaUnrefQueue instance_;
+
+  ftl::Mutex mutex_;
+  std::deque<SkRefCnt*> objects_ FTL_GUARDED_BY(mutex_);
+  bool drain_pending_ FTL_GUARDED_BY(mutex_);
+};
 
 template <typename T> void SkiaUnrefOnIOThread(sk_sp<T>* sp) {
   T* object = sp->release();
   if (object) {
-    Threads::IO()->PostTask([object]() { object->unref(); });
+    SkiaUnrefQueue::Get().Unref(object);
   }
 }
 
