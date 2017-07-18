29bacddb699c6f996bf9f6a241d35bd4cf650df4
Jason Simmons <jason-simmons@users.noreply.github.com>
2017-07-18 11:45:34 -0700

    Accumulate a batch of Skia objects that will be destructed later on the IO thread (flutter/engine#3888)
    See https://github.com/dart-lang/sdk/issues/29971

---
+++ engine/src/flutter/lib/ui/painting/utils.cc
@@ -0,0 +1,48 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "flutter/common/threads.h"
+#include "flutter/lib/ui/painting/utils.h"
+
+namespace blink {
+
+namespace {
+
+constexpr ftl::TimeDelta kDrainDelay = ftl::TimeDelta::FromMilliseconds(250);
+
+}  // anonymous namespace
+
+SkiaUnrefQueue::SkiaUnrefQueue()
+    : drain_pending_(false) {}
+
+SkiaUnrefQueue SkiaUnrefQueue::instance_;
+
+SkiaUnrefQueue& SkiaUnrefQueue::Get() {
+  return instance_;
+}
+
+void SkiaUnrefQueue::Unref(SkRefCnt* object) {
+  ftl::MutexLocker lock(&mutex_);
+  objects_.push_back(object);
+  if (!drain_pending_) {
+    drain_pending_ = true;
+    Threads::IO()->PostDelayedTask([this] { Drain(); },
+                                   kDrainDelay);
+  }
+}
+
+void SkiaUnrefQueue::Drain() {
+  std::deque<SkRefCnt*> skia_objects;
+  {
+    ftl::MutexLocker lock(&mutex_);
+    objects_.swap(skia_objects);
+    drain_pending_ = false;
+  }
+
+  for (SkRefCnt* skia_object : skia_objects) {
+    skia_object->unref();
+  }
+}
+
+}  // namespace blink
