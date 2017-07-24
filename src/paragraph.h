056d8b718715b4ca3db8f242ede020495f91ded3
Gary Qian <garyq@google.com>
2017-07-24 15:06:37 -0700

    Add unittests for kerning.
    Change-Id: I494bbd3993cde76cabc9dfc042dcee4b0fb6af4d

--- src/paragraph.h
+++ src/paragraph.h
@@ -118,6 +118,7 @@ class Paragraph {
   FRIEND_TEST(RenderTest, DISABLED_ArabicParagraph);
   FRIEND_TEST(RenderTest, SpacingParagraph);
   FRIEND_TEST(RenderTest, LongWordParagraph);
+  FRIEND_TEST(RenderTest, KernParagraph);
 
   std::vector<uint16_t> text_;
   StyledRuns runs_;
