b0caefa339171dfa48b72b1064652d7c11cfe857
Chinmay Garde <chinmaygarde@gmail.com>
2017-08-04 15:39:18 -0700

    Be explicit about the iOS SDK version and deployment targets. (#87)
    Our minimum supported iOS version was 8.0. But the SDK we used was the first one returned by “xcodebuild —version —sdk”. Now we are explicit about that version too. This way, we can use newer features guarded by the appropriate version checks. If the Xcode installed on the host is too old or does not include the appropriate SDK, the GN step itself will terminate with a error.

--- build/config/ios/ios_sdk.py
+++ build/config/ios/ios_sdk.py
@@ -7,13 +7,17 @@ import sys
 
 # This script returns the path to the SDK of the given type. Pass the type of
 # SDK you want, which is typically "iphone" or "iphonesimulator".
-#
-# In the GYP build, this is done inside GYP itself based on the SDKROOT
-# variable.
 
-if len(sys.argv) != 2:
-  print "Takes one arg (SDK to find)"
+if len(sys.argv) != 3:
+  print "Takes two args (SDK to find and the version)"
   sys.exit(1)
 
-print subprocess.check_output(['xcodebuild', '-version', '-sdk',
-                               sys.argv[1], 'Path']).strip()
+command =  [
+  'xcodebuild',
+  '-version',
+  '-sdk',
+  ''.join([sys.argv[1], sys.argv[2]]),
+  'Path'
+]
+
+print subprocess.check_output(command).strip()
