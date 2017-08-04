db752f2d3a0aaa23337fae19ba5432676a0ae00f
Michael Goderbauer <goderbauer@google.com>
2017-08-04 15:57:45 -0700

    Allow tapping on TabBar indicator to switch tabs

--- packages/flutter/test/material/tabs_test.dart
+++ packages/flutter/test/material/tabs_test.dart
@@ -7,6 +7,7 @@ import 'dart:ui' show SemanticsFlags, SemanticsAction;
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter/physics.dart';
 
 import '../rendering/mock_canvas.dart';
@@ -1120,4 +1121,40 @@ void main() {
     expect(find.text('PAGE'), findsOneWidget);
   });
 
+  testWidgets('can tap on indicator at very bottom of TabBar to switch tabs', (WidgetTester tester) async {
+    final TabController controller = new TabController(
+      vsync: const TestVSync(),
+      length: 2,
+      initialIndex: 0,
+    );
+
+    await tester.pumpWidget(
+      new Material(
+        child: new Column(
+          children: <Widget>[
+            new TabBar(
+              controller: controller,
+              indicatorWeight: 30.0,
+              tabs: const <Widget>[const Tab(text: 'TAB1'), const Tab(text: 'TAB2')],
+            ),
+            new Flexible(
+              child: new TabBarView(
+                controller: controller,
+                children: const <Widget>[const Text('PAGE1'), const Text('PAGE2')],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+
+    expect(controller.index, 0);
+
+    final Offset bottomRight = tester.getBottomRight(find.byType(TabBar)) - const Offset(1.0, 1.0);
+    final TestGesture gesture = await tester.startGesture(bottomRight);
+    await gesture.up();
+    await tester.pumpAndSettle();
+
+    expect(controller.index, 1);
+  });
 }
