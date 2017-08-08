74835db563b7c31b4c883eeea15e689a1bdbd303
Chris Bracken <chris@bracken.jp>
2017-08-08 10:52:24 -0700

    Avoid rebuilding snapshots if no change to source (#11551)
    This change re-introduces skipping snapshot builds if input sources (and
    outputs) have not changed since the last snapshot build, with a bugfix
    to include the entry-point source in the checksum used to check whether
    rebuild can be skipped. This ensures that the following sequence
    invalidates the cached build, resulting in two snapshot builds:
    
      flutter build ios lib/foo.dart
      flutter build ios lib/bar.dart
    
    This reverts commit 3d5afb5a81052b7d55661549320d6d43f893f448.

--- packages/flutter_tools/lib/src/flx.dart
+++ packages/flutter_tools/lib/src/flx.dart
@@ -65,8 +65,9 @@ Future<int> _createSnapshot({
     try {
         final String json = await checksumFile.readAsString();
         final Checksum oldChecksum = new Checksum.fromJson(json);
-        final Set<String> inputPaths = await _readDepfile(depfilePath);
+        final Set<String> inputPaths = await readDepfile(depfilePath);
         inputPaths.add(snapshotPath);
+        inputPaths.add(mainPath);
         final Checksum newChecksum = new Checksum.fromFiles(inputPaths);
         if (oldChecksum == newChecksum) {
           printTrace('Skipping snapshot build. Checksums match.');
@@ -85,8 +86,9 @@ Future<int> _createSnapshot({
 
   // Compute and record input file checksums.
   try {
-    final Set<String> inputPaths = await _readDepfile(depfilePath);
+    final Set<String> inputPaths = await readDepfile(depfilePath);
     inputPaths.add(snapshotPath);
+    inputPaths.add(mainPath);
     final Checksum checksum = new Checksum.fromFiles(inputPaths);
     await checksumFile.writeAsString(checksum.toJson());
   } catch (e, s) {
@@ -94,24 +96,6 @@ Future<int> _createSnapshot({
     printTrace('Error during snapshot checksum output: $e\n$s');
   }
   return 0;
-}
-
-/// Parses a VM snapshot dependency file.
-///
-/// Snapshot dependency files are a single line mapping the output snapshot to a
-/// space-separated list of input files used to generate that output. e.g,
-///
-/// outfile : file1.dart file2.dart file3.dart
-Future<Set<String>> _readDepfile(String depfilePath) async {
-  // Depfile format:
-  // outfile : file1.dart file2.dart file3.dart
-  final String contents = await fs.file(depfilePath).readAsString();
-  final String dependencies = contents.split(': ')[1];
-  return dependencies
-      .split(' ')
-      .map((String path) => path.trim())
-      .where((String path) => path.isNotEmpty)
-      .toSet();
 }
 
 Future<Null> build({
