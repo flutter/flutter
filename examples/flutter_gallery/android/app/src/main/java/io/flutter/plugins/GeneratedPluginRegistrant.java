04209dac0e265c8152ed6872e3392e2b8be04248
Alexander Aprelev <aam@google.com>
2017-08-22 16:31:35 -0700

    Revert unintended changes to GeneratedPluginRestrant

--- examples/flutter_gallery/android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
+++ examples/flutter_gallery/android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
@@ -1,13 +1,13 @@
 package io.flutter.plugins;
 
 import io.flutter.plugin.common.PluginRegistry;
-import io.flutter.plugins.urllauncher.UrlLauncherPlugin;
+import io.flutter.plugins.url_launcher.UrlLauncherPlugin;
 
 /**
  * Generated file. Do not edit.
  */
 public final class GeneratedPluginRegistrant {
   public static void registerWith(PluginRegistry registry) {
-    UrlLauncherPlugin.registerWith(registry.registrarFor("io.flutter.plugins.urllauncher.UrlLauncherPlugin"));
+    UrlLauncherPlugin.registerWith(registry.registrarFor("io.flutter.plugins.url_launcher.UrlLauncherPlugin"));
   }
 }
