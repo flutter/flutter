e73818547bcef4d7d0f3ff7d14b40c89cb0cc542
Mikkel Ravn <mravn@google.com>
2017-08-23 15:14:37 +0200

    Updated gradle plugin test

--- dev/devicelab/bin/tasks/gradle_plugin_test.dart
+++ dev/devicelab/bin/tasks/gradle_plugin_test.dart
@@ -15,29 +15,45 @@ void main() {
     final Directory tmp = await Directory.systemTemp.createTemp('gradle');
     final FlutterProject project = await FlutterProject.create(tmp, 'hello');
 
-    section('gradlew assembleDebug');
-    await project.runGradleTask('assembleDebug');
+    try {
+      section('gradlew assembleDebug');
+      await project.runGradleTask('assembleDebug');
 
-    section('gradlew assembleProfile');
-    await project.runGradleTask('assembleProfile');
+      section('gradlew assembleProfile');
+      await project.runGradleTask('assembleProfile');
 
-    section('gradlew assembleRelease');
-    await project.runGradleTask('assembleRelease');
+      section('gradlew assembleRelease');
+      await project.runGradleTask('assembleRelease');
 
-    section('gradlew assembleLocal (custom debug build)');
-    await project.addCustomBuildType('local', initWith: 'debug');
-    await project.runGradleTask('assembleLocal');
+      section('gradlew assembleLocal (custom debug build)');
+      await project.addCustomBuildType('local', initWith: 'debug');
+      await project.runGradleTask('assembleLocal');
 
-    section('gradlew assembleBeta (custom release build)');
-    await project.addCustomBuildType('beta', initWith: 'release');
-    await project.runGradleTask('assembleBeta');
+      section('gradlew assembleBeta (custom release build)');
+      await project.addCustomBuildType('beta', initWith: 'release');
+      await project.runGradleTask('assembleBeta');
 
-    section('gradlew assembleFreeDebug (product flavor)');
-    await project.addProductFlavor('free');
-    await project.runGradleTask('assembleFreeDebug');
+      section('gradlew assembleFreeDebug (product flavor)');
+      await project.addProductFlavor('free');
+      await project.runGradleTask('assembleFreeDebug');
 
-    await project.parent.delete(recursive: true);
-    return new TaskResult.success(null);
+      section('gradlew on script with error');
+      await project.introduceError();
+      final ProcessResult result = await project.resultOfGradleTask('assembleRelease');
+      if (result.exitCode == 0)
+        return new TaskResult.failure('Gradle did not exit with error as expected');
+      final String output = result.stdout + '\n' + result.stderr;
+      if (output.contains('GradleException') || output.contains('Failed to notify') || output.contains('at org.gradle'))
+        return new TaskResult.failure('Gradle output should not contain stacktrace');
+      if (!output.contains('Build failed') || !output.contains('builTypes'))
+        return new TaskResult.failure('Gradle output should contain a readable error message');
+
+      return new TaskResult.success(null);
+    } catch(e) {
+      return new TaskResult.failure(e.toString());
+    } finally {
+      project.parent.deleteSync(recursive: true);
+    }
   });
 }
 
@@ -90,18 +106,30 @@ android {
     ''');
   }
 
-  Future<Null> runGradleTask(String task) async {
-    final ProcessResult result = await Process.run(
-      './gradlew',
-      <String>['-q', 'app:$task'],
-      workingDirectory: androidPath,
+  Future<Null> introduceError() async {
+    final File buildScript = new File(
+      path.join(androidPath, 'app', 'build.gradle'),
     );
+    await buildScript.writeAsString((await buildScript.readAsString()).replaceAll('buildTypes', 'builTypes'));
+  }
+
+  Future<Null> runGradleTask(String task) async {
+    final ProcessResult result = await resultOfGradleTask(task);
     if (result.exitCode != 0) {
       print('stdout:');
       print(result.stdout);
       print('stderr:');
       print(result.stderr);
     }
-    assert(result.exitCode == 0);
+    if (result.exitCode != 0)
+      throw 'Gradle exited with error';
+  }
+
+  Future<ProcessResult> resultOfGradleTask(String task) async {
+    return await Process.run(
+      './gradlew',
+      <String>['app:$task'],
+      workingDirectory: androidPath,
+    );
   }
 }
