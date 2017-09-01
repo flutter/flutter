c2e8a2e3ceb64c9f81d18c8ef6e430ce96078b85
Jason Simmons <jason-simmons@users.noreply.github.com>
2017-09-01 16:09:47 -0700

    Use a generic sans-serif font family on Android that will be recognized by Skia's font manager (#4055)

---
+++ third_party/txt/src/txt/platform_android.cc
@@ -0,0 +1,13 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "txt/platform.h"
+
+namespace txt {
+
+std::string GetDefaultFontFamily() {
+  return "sans-serif";
+}
+
+}  // namespace txt
