d73d959c5031cb2e4aa1950b16e3aff12952127b
Adam Barth <abarth@chromium.org>
2017-09-04 14:40:31 -0700

    Add RTL support to Switch and CupertinoSwitch
    Fixes #11868

--- packages/flutter/lib/src/cupertino/switch.dart
+++ packages/flutter/lib/src/cupertino/switch.dart
@@ -127,6 +127,7 @@ class _CupertinoSwitchRenderObjectWidget extends LeafRenderObjectWidget {
       value: value,
       activeColor: activeColor,
       onChanged: onChanged,
+      textDirection: Directionality.of(context),
       vsync: vsync,
     );
   }
@@ -137,6 +138,7 @@ class _CupertinoSwitchRenderObjectWidget extends LeafRenderObjectWidget {
       ..value = value
       ..activeColor = activeColor
       ..onChanged = onChanged
+      ..textDirection = Directionality.of(context)
       ..vsync = vsync;
   }
 }
@@ -159,6 +161,7 @@ class _RenderCupertinoSwitch extends RenderConstrainedBox implements SemanticsAc
     @required bool value,
     @required Color activeColor,
     ValueChanged<bool> onChanged,
+    @required TextDirection textDirection,
     @required TickerProvider vsync,
   }) : assert(value != null),
        assert(activeColor != null),
@@ -166,6 +169,7 @@ class _RenderCupertinoSwitch extends RenderConstrainedBox implements SemanticsAc
        _value = value,
        _activeColor = activeColor,
        _onChanged = onChanged,
+       _textDirection = textDirection,
        _vsync = vsync,
        super(additionalConstraints: const BoxConstraints.tightFor(width: _kSwitchWidth, height: _kSwitchHeight)) {
     _tap = new TapGestureRecognizer()
@@ -254,6 +258,16 @@ class _RenderCupertinoSwitch extends RenderConstrainedBox implements SemanticsAc
     }
   }
 
+  TextDirection get textDirection => _textDirection;
+  TextDirection _textDirection;
+  set textDirection(TextDirection value) {
+    assert(value != null);
+    if (_textDirection == value)
+      return;
+    _textDirection = value;
+    markNeedsPaint();
+  }
+
   bool get isInteractive => onChanged != null;
 
   TapGestureRecognizer _tap;
@@ -328,7 +342,15 @@ class _RenderCupertinoSwitch extends RenderConstrainedBox implements SemanticsAc
       _position
         ..curve = null
         ..reverseCurve = null;
-      _positionController.value += details.primaryDelta / _kTrackInnerLength;
+      final double delta = details.primaryDelta / _kTrackInnerLength;
+      switch (textDirection) {
+        case TextDirection.rtl:
+          _positionController.value -= delta;
+          break;
+        case TextDirection.ltr:
+          _positionController.value += delta;
+          break;
+      }
     }
   }
 
@@ -378,11 +400,21 @@ class _RenderCupertinoSwitch extends RenderConstrainedBox implements SemanticsAc
   void paint(PaintingContext context, Offset offset) {
     final Canvas canvas = context.canvas;
 
-    final double currentPosition = _position.value;
+    final double currentValue = _position.value;
     final double currentReactionValue = _reaction.value;
 
+    double visualPosition;
+    switch (textDirection) {
+      case TextDirection.rtl:
+        visualPosition = 1.0 - currentValue;
+        break;
+      case TextDirection.ltr:
+        visualPosition = currentValue;
+        break;
+    }
+
     final Color trackColor = _value ? activeColor : _kTrackColor;
-    final double borderThickness = 1.5 + (_kTrackRadius - 1.5) * math.max(currentReactionValue, currentPosition);
+    final double borderThickness = 1.5 + (_kTrackRadius - 1.5) * math.max(currentReactionValue, currentValue);
 
     final Paint paint = new Paint()
       ..color = trackColor;
@@ -401,12 +433,12 @@ class _RenderCupertinoSwitch extends RenderConstrainedBox implements SemanticsAc
     final double thumbLeft = lerpDouble(
       trackRect.left + _kTrackInnerStart - CupertinoThumbPainter.radius,
       trackRect.left + _kTrackInnerEnd - CupertinoThumbPainter.radius - currentThumbExtension,
-      currentPosition,
+      visualPosition,
     );
     final double thumbRight = lerpDouble(
       trackRect.left + _kTrackInnerStart + CupertinoThumbPainter.radius + currentThumbExtension,
       trackRect.left + _kTrackInnerEnd + CupertinoThumbPainter.radius,
-      currentPosition,
+      visualPosition,
     );
     final double thumbCenterY = offset.dy + size.height / 2.0;
 
