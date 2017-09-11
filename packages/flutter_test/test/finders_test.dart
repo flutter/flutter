848b4bf98582d4cfe9bf6b853baee3ae76bb7574
Yegor Jbanov <yjbanov@google.com>
2017-09-11 09:06:37 -0700

    add Directionality in test

--- packages/flutter_test/test/finders_test.dart
+++ packages/flutter_test/test/finders_test.dart
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -9,7 +10,7 @@ void main() {
   group('hitTestable', () {
     testWidgets('excludes non-hit-testable widgets', (WidgetTester tester) async {
       await tester.pumpWidget(
-        new IndexedStack(
+        _boilerplate(new IndexedStack(
           sizing: StackFit.expand,
           children: <Widget>[
             new GestureDetector(
@@ -25,7 +26,7 @@ void main() {
               child: const SizedBox.expand(),
             ),
           ],
-        )
+        )),
       );
       expect(find.byType(GestureDetector), findsNWidgets(2));
       final Finder hitTestable = find.byType(GestureDetector).hitTestable(at: const FractionalOffset(0.5, 0.5));
@@ -33,4 +34,11 @@ void main() {
       expect(tester.widget(hitTestable).key, const ValueKey<int>(0));
     });
   });
+}
+
+Widget _boilerplate(Widget child) {
+  return new Directionality(
+    textDirection: TextDirection.ltr,
+    child: child,
+  );
 }
