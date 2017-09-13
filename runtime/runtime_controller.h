05751f7be993316057d772e8334a783537e10e26
Ian McKellar <ian@mckellar.org>
2017-09-13 10:29:21 -0700

    Support cleaner Dart isolate shutdown handling. (#4096)
    If an isolate shuts down (for example if an app calls
    Isolate.current.kill()), the UIDartState* on DartController will refer
    to a freed object. This wires through notification that the is shutting
    down through to the DartController so it can clean up appropriately.

--- runtime/runtime_controller.h
+++ runtime/runtime_controller.h
@@ -61,6 +61,7 @@ class RuntimeController : public WindowClient, public IsolateClient {
   void HandlePlatformMessage(fxl::RefPtr<PlatformMessage> message) override;
 
   void DidCreateSecondaryIsolate(Dart_Isolate isolate) override;
+  void WillShutDownIsolate(Dart_Isolate isolate) override;
 
   RuntimeDelegate* client_;
   std::string language_code_;
