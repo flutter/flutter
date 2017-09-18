e8a9d1adb4db2ccf512863b0ead6f7110ebeed13
Greg Spencer <gspencer@google.com>
2017-09-18 11:25:03 -0700

    Removing unneeded textHeight parameter

--- packages/flutter/lib/src/material/text_selection.dart
+++ packages/flutter/lib/src/material/text_selection.dart
@@ -164,7 +164,7 @@ class _MaterialTextSelectionControls extends TextSelectionControls {
 
   /// Builder for material-style text selection handles.
   @override
-  Widget buildHandle(BuildContext context, TextSelectionHandleType type, double textHeight) {
+  Widget buildHandle(BuildContext context, TextSelectionHandleType type) {
     final Widget handle = new SizedBox(
       width: _kHandleSize,
       height: _kHandleSize,
