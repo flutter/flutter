a7e14ea474aff4e8258befea8d22ef404562e6eb
Jason Simmons <jason-simmons@users.noreply.github.com>
2017-09-22 15:15:59 -0700

    libtxt: refactor glyph position calculation (#4134)
    * Remove padding values in line_heights and glyph_position_x.  Each value
      in glyph_position_x now represents an actual glyph in the layout.
    
    * Remove code intended to handle extra characters beyond the end of the
      last line.  The LineBreaker should ensure that the end of the last run
      matches the end of the last line.
    
    * Return the upstream/downstream affinity of the cursor position in
      GetGlyphPositionAtCoordinate.
    
    * Account for the space at the end of a word wrapped line in
      GetGlyphPositionAtCoordinate / GetCoordinatesForGlyphPosition

--- lib/ui/text/paragraph_impl_txt.cc
+++ lib/ui/text/paragraph_impl_txt.cc
@@ -75,10 +75,10 @@ std::vector<TextBox> ParagraphImplTxt::getRectsForRange(unsigned start,
 
 Dart_Handle ParagraphImplTxt::getPositionForOffset(double dx, double dy) {
   Dart_Handle result = Dart_NewList(2);
-  Dart_ListSetAt(
-      result, 0,
-      ToDart(m_paragraph->GetGlyphPositionAtCoordinate(dx, dy, true)));
-  Dart_ListSetAt(result, 1, ToDart(static_cast<int>(EAffinity::DOWNSTREAM)));
+  txt::Paragraph::PositionWithAffinity pos =
+      m_paragraph->GetGlyphPositionAtCoordinate(dx, dy, true);
+  Dart_ListSetAt(result, 0, ToDart(pos.position));
+  Dart_ListSetAt(result, 1, ToDart(static_cast<int>(pos.affinity)));
   return result;
 }
 
