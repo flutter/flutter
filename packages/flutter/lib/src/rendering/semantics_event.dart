97cf7f95db014801d2eda81c1fd21bf494ab09c5
Michael Goderbauer <goderbauer@google.com>
2017-09-26 11:13:47 -0700

    fix analyze warning

--- packages/flutter/lib/src/rendering/semantics_event.dart
+++ packages/flutter/lib/src/rendering/semantics_event.dart
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/painting.dart';
-import 'package:meta/meta.dart';
 
 /// An event that can be send by the application to notify interested listeners
 /// that something happened to the user interface (e.g. a view scrolled).
