8a441ae72b3b66e0515b59ba256e192b2ff5a831
jiamingc <jiamingc@google.com>
2017-09-26 10:05:51 -0700

    Make switch widget accept 3 other colors. (#12118)
    * Make switch widget accept 3 other colors: inactiveThumbColor, inactiveTrackColor, activeTrackColor.
    
    * Make switch widget accept 3 other colors.
    
    * Make switch widget accept 3 other colors.

--- packages/flutter/test/material/switch_test.dart
+++ packages/flutter/test/material/switch_test.dart
@@ -6,6 +6,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+import '../rendering/mock_canvas.dart';
+
 void main() {
   testWidgets('Switch can toggle on tap', (WidgetTester tester) async {
     final Key switchKey = new UniqueKey();
@@ -110,8 +112,6 @@ void main() {
       ),
     );
 
-    expect(value, isFalse);
-
     await tester.drag(find.byType(Switch), const Offset(30.0, 0.0));
 
     expect(value, isFalse);
@@ -129,5 +129,60 @@ void main() {
     await tester.drag(find.byType(Switch), const Offset(30.0, 0.0));
 
     expect(value, isFalse);
+  });
+
+  testWidgets('Switch can be set color', (WidgetTester tester) async {
+    bool value = false;
+    await tester.pumpWidget(
+      new Directionality(
+        textDirection: TextDirection.rtl,
+        child: new StatefulBuilder(
+          builder: (BuildContext context, StateSetter setState) {
+            return new Material(
+              child: new Center(
+                child: new Switch(
+                  value: value,
+                  onChanged: (bool newValue) {
+                    setState(() {
+                      value = newValue;
+                    });
+                  },
+                  activeColor: Colors.red[500],
+                  activeTrackColor: Colors.green[500],
+                  inactiveThumbColor: Colors.yellow[500],
+                  inactiveTrackColor: Colors.blue[500],
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+
+    expect(
+        Material.of(tester.element(find.byType(Switch))),
+        paints
+          ..rrect(
+              color: Colors.blue[500],
+              rrect: new RRect.fromLTRBR(
+                  383.5, 293.0, 416.5, 307.0, const Radius.circular(7.0)))
+          ..circle(color: const Color(0x33000000))
+          ..circle(color: const Color(0x24000000))
+          ..circle(color: const Color(0x1f000000))
+          ..circle(color: Colors.yellow[500]));
+    await tester.drag(find.byType(Switch), const Offset(-30.0, 0.0));
+    await tester.pump();
+
+    expect(
+        Material.of(tester.element(find.byType(Switch))),
+        paints
+          ..rrect(
+              color: Colors.green[500],
+              rrect: new RRect.fromLTRBR(
+                  383.5, 293.0, 416.5, 307.0, const Radius.circular(7.0)))
+          ..circle(color: const Color(0x33000000))
+          ..circle(color: const Color(0x24000000))
+          ..circle(color: const Color(0x1f000000))
+          ..circle(color: Colors.red[500]));
   });
 }
