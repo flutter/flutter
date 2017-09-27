44f13442ce8740fed05945e480b08ef4e1b73488
Chris Bracken <chris@bracken.jp>
2017-09-26 17:12:20 -0700

    Eliminate MethodCall hashCode and equals
    Since MethodCall equality checks are limited to test scenarios, this
    patch replaces them with an equivalent test matcher. At present
    MethodCalls are always used in scenarios where indentity-based
    equality/hashing is appropriate.
    
    This change avoids an assertion failure when MethodCall args are
    Iterable (possible since args are of type dyanmic), and hashValue() from
    dart:ui asserts that its input is not an Iterable.
    
    The alternative of implementing support for deep equality in dart:ui was
    rejected on the basis that if we're to encourage performant code,
    expensive checks should be obviously-expensive to the author.

--- packages/flutter/test/services/system_chrome_test.dart
+++ packages/flutter/test/services/system_chrome_test.dart
@@ -7,6 +7,8 @@ import 'dart:typed_data';
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+import 'message_codecs_utils.dart';
+
 void main() {
   testWidgets('SystemChrome overlay style test', (WidgetTester tester) async {
     // The first call is a cache miss and will queue a microtask
@@ -33,10 +35,11 @@ void main() {
       DeviceOrientation.portraitUp,
     ]);
 
-    expect(log, equals(<MethodCall>[new MethodCall(
+    expect(log, hasLength(1));
+    expect(log.single, isMethodCall(
       'SystemChrome.setPreferredOrientations',
-      <String>['DeviceOrientation.portraitUp'],
-    )]));
+      arguments: <String>['DeviceOrientation.portraitUp'],
+    ));
   });
 
   test('setApplicationSwitcherDescription control test', () async {
@@ -50,10 +53,11 @@ void main() {
       const ApplicationSwitcherDescription(label: 'Example label', primaryColor: 0xFF00FF00)
     );
 
-    expect(log, equals(<MethodCall>[new MethodCall(
+    expect(log, hasLength(1));
+    expect(log.single, isMethodCall(
       'SystemChrome.setApplicationSwitcherDescription',
-      <String, dynamic>{'label':'Example label','primaryColor':4278255360}
-    )]));
+      arguments: <String, dynamic>{'label': 'Example label', 'primaryColor': 4278255360},
+    ));
   });
 
   test('setApplicationSwitcherDescription missing plugin', () async {
@@ -80,9 +84,10 @@ void main() {
 
     await SystemChrome.setEnabledSystemUIOverlays(<SystemUiOverlay>[SystemUiOverlay.top]);
 
-    expect(log, equals(<MethodCall>[new MethodCall(
+    expect(log, hasLength(1));
+    expect(log.single, isMethodCall(
       'SystemChrome.setEnabledSystemUIOverlays',
-      <String>['SystemUiOverlay.top'],
-    )]));
+      arguments: <String>['SystemUiOverlay.top'],
+    ));
   });
 }
