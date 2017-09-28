1a3097025dbc13969c6ada32cc559023984fdda1
Alexander Aprelev <aam@google.com>
2017-09-28 12:45:54 -0700

    Clone hot reload benchmark for --preview-dart-2 option. (#12251)
    * Clone hot reload benchmark for --preview-dart-2 option.
    
    * Get rid of linux and win preview_dart_2 (only android would be sufficient for now). Refactor code into lib/tasks
    
    * Revert 2016 to 2017
    
    * Mark new test as flaky

--- dev/devicelab/bin/tasks/hot_mode_dev_cycle__preview_dart_2_benchmark.dart
+++ dev/devicelab/bin/tasks/hot_mode_dev_cycle__preview_dart_2_benchmark.dart
@@ -1,4 +1,4 @@
-// Copyright (c) 2016 The Chromium Authors. All rights reserved.
+// Copyright (c) 2017 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -8,5 +8,5 @@ import 'package:flutter_devicelab/tasks/hot_mode_tests.dart';
 import 'package:flutter_devicelab/framework/framework.dart';
 
 Future<Null> main() async {
-  await task(createHotModeTest());
+  await task(createHotModeTest(isPreviewDart2: true));
 }
