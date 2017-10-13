305420c8e73674d182381eb7b252f641ff559f0b
Amir Hardon <amirha@google.com>
2017-10-13 12:51:08 -0700

    Review comments followup.

--- packages/flutter/lib/src/widgets/gesture_detector.dart
+++ packages/flutter/lib/src/widgets/gesture_detector.dart
@@ -95,7 +95,7 @@ class GestureRecognizerFactoryWithHandlers<T extends GestureRecognizer> extends 
 /// If this widget has a child, it defers to that child for its sizing behavior.
 /// If it does not have a child, it grows to fit the parent instead.
 ///
-/// By default a GestureDetector with an invisible child ignores touches,
+/// By default a GestureDetector with an invisible child ignores touches;
 /// this behavior can be controlled with [behavior].
 ///
 /// GestureDetector also listens for accessibility events and maps
