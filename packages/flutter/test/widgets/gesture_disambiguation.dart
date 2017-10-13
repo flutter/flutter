f5c6c5cc58dd54cca9ba0f3a83fb88f79731988a
Amir Hardon <amirha@google.com>
2017-10-12 19:09:33 -0700

    Fix analyzer warnings

--- packages/flutter/test/widgets/gesture_disambiguation.dart
+++ packages/flutter/test/widgets/gesture_disambiguation.dart
@@ -12,19 +12,18 @@ void main() {
     int detector2TapCount = 0;
 
     final Widget widget = new GestureDetector(
-        onVerticalDragStart: (d) => didStartDrag = true,
         child: new Column(
             mainAxisAlignment: MainAxisAlignment.start,
             children: <Widget>[
               new GestureDetector(
                   onTap: () => detector1TapCount++,
                   behavior: HitTestBehavior.opaque,
-                  child: new SizedBox(width: 200.0, height: 200.0),
+                  child: const SizedBox(width: 200.0, height: 200.0),
               ),
               new GestureDetector(
                 onTap: () => detector2TapCount++,
                 behavior: HitTestBehavior.opaque,
-                child: new SizedBox(width: 200.0, height: 200.0)
+                child: const SizedBox(width: 200.0, height: 200.0)
               )
             ],
           ));
@@ -33,11 +32,11 @@ void main() {
 
     // The following pointer event sequence was causing the issue described
     // in issue #12470 by triggering 2 tap events on the second detector.
-    TestGesture gesture1 = await tester.startGesture(new Offset(400.0, 10.0), pointer: 1);
-    TestGesture gesture2 = await tester.startGesture(new Offset(400.0, 210.0), pointer: 2);
+    final TestGesture gesture1 = await tester.startGesture(const Offset(400.0, 10.0), pointer: 1);
+    final TestGesture gesture2 = await tester.startGesture(const Offset(400.0, 210.0), pointer: 2);
     await gesture1.up();
     await gesture2.cancel();
-    TestGesture gesture3 = await tester.startGesture(new Offset(400.0, 250.0), pointer: 3);
+    final TestGesture gesture3 = await tester.startGesture(const Offset(400.0, 250.0), pointer: 3);
     await gesture3.up();
 
     expect(detector1TapCount, 1);
