f1f3c793344e59aa4e119678eeeb22f9efe01d96
Amir Hardon <amirha@google.com>
2017-10-18 09:22:48 -0700

    Review comments followup

--- packages/flutter/lib/src/services/semantics_service.dart
+++ packages/flutter/lib/src/services/semantics_service.dart
@@ -6,13 +6,20 @@ import 'dart:async';
 
 import 'system_channels.dart';
 
-/// Allows access to the semantic feedback interface on the device.
+/// Allows access to the platform's accessibility services.
+///
+/// Events sent by this service are handled by the platform-specific
+/// accessibility bridge in Flutter's engine.
 class SemanticsService {
   SemanticsService._();
 
   /// Sends a semantic announcement.
   ///
-  /// Can be used for on-demand accessibility announcements.
+  /// This should be used for announcement that are not seamlessly announced by
+  /// the system as a result of a UI state change.
+  ///
+  /// For example a camera application can use this method to make accessibility
+  /// announcements regarding objects in the viewfinder.
   static Future<Null> announce(String message) async {
     final Map<String, dynamic> event = <String, dynamic>{
       'type': 'announce',
