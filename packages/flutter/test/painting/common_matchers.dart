acd523f3efb83b052c5ca5319bb1e0628d0bc461
Todd Volkert <tvolkert@google.com>
2017-10-18 20:11:39 -0700

    Fix analyzer errors

--- packages/flutter/test/painting/common_matchers.dart
+++ packages/flutter/test/painting/common_matchers.dart
@@ -5,6 +5,8 @@
 import 'package:flutter/painting.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+import '../rendering/mock_canvas.dart';
+
 final Matcher isUnitCircle = isPathThat(
   includes: <Offset>[
     const Offset(-0.6035617555492896, 0.2230970398703236),
