95d7ce44b4222f1e858f03f2b6fb6cf67cfea6d6
Amir Hardon <amirha@google.com>
2017-10-23 09:54:03 -0700

    Update the material card test with the new button flag.

--- packages/flutter/test/material/card_test.dart
+++ packages/flutter/test/material/card_test.dart
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'dart:ui' show SemanticsFlags;
+
 import 'package:flutter/rendering.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
@@ -47,6 +49,7 @@ void main() {
                 label: 'Button',
                 textDirection: TextDirection.ltr,
                 actions: SemanticsAction.tap.index,
+                flags: SemanticsFlags.isButton.index,
               ),
             ],
           ),
