f781f928d146fd83f7f70b852e559991863a3c83
Chinmay Garde <chinmaygarde@gmail.com>
2017-10-30 14:15:32 -0700

    Test that the embedder can be launched and shut down correctly. (#4281)

--- shell/platform/embedder/tests/embedder_unittests.cc
+++ shell/platform/embedder/tests/embedder_unittests.cc
@@ -2,14 +2,42 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <string>
 #include "embedder.h"
-#include "gtest/gtest.h"
+#include "flutter/testing/testing.h"
 
 TEST(EmbedderTest, MustNotRunWithInvalidArgs) {
   FlutterEngine engine = nullptr;
   FlutterRendererConfig config = {};
   FlutterProjectArgs args = {};
-  auto result = FlutterEngineRun(FLUTTER_ENGINE_VERSION + 1, &config, &args,
-                                 NULL, &engine);
-  ASSERT_NE(result, kSuccess);
+  FlutterResult result = FlutterEngineRun(FLUTTER_ENGINE_VERSION + 1, &config,
+                                          &args, NULL, &engine);
+  ASSERT_NE(result, FlutterResult::kSuccess);
+}
+
+TEST(EmbedderTest, CanLaunchAndShutdownWithValidProjectArgs) {
+  FlutterOpenGLRendererConfig renderer = {
+      .struct_size = sizeof(FlutterOpenGLRendererConfig),
+      .make_current = [](void*) { return false; },
+      .clear_current = [](void*) { return false; },
+      .present = [](void*) { return false; },
+      .fbo_callback = [](void*) -> uint32_t { return 0; },
+  };
+
+  std::string main =
+      std::string(testing::GetFixturesPath()) + "/simple_main.dart";
+
+  FlutterRendererConfig config = {.type = FlutterRendererType::kOpenGL,
+                                  .open_gl = renderer};
+  FlutterProjectArgs args = {.struct_size = sizeof(FlutterProjectArgs),
+                             .assets_path = "",
+                             .main_path = main.c_str(),
+                             .packages_path = ""};
+  FlutterEngine engine = nullptr;
+  FlutterResult result = FlutterEngineRun(FLUTTER_ENGINE_VERSION, &config,
+                                          &args, nullptr, &engine);
+  ASSERT_EQ(result, FlutterResult::kSuccess);
+
+  result = FlutterEngineShutdown(engine);
+  ASSERT_EQ(result, FlutterResult::kSuccess);
 }
