99f4ee26a9b32b395bf310936afccfb6d2976ee2
Kezhu Wang <kezhuw@gmail.com>
2017-11-02 12:57:54 +0800

    Remove build phase restriction on Element.didChangeDependencies and InheritedElement.dispatchDidChangeDependencies
    The restriction was introduced in commit 8c3d5838dd539fec52731bdce3dc73bcc11c4f7c(#12213).

--- packages/flutter/lib/src/widgets/framework.dart
+++ packages/flutter/lib/src/widgets/framework.dart
@@ -3298,29 +3298,8 @@ abstract class Element extends DiagnosticableTree implements BuildContext {
   /// this function to notify this element of the change.
   @mustCallSuper
   void didChangeDependencies() {
-    assert(_active); // otherwise markNeedsBuild is a no-op
-    assert(_debugCheckOwnerBuildTargetExists('didChangeDependencies'));
+    assert(_active); // otherwise dependencies should be unlinked in deactivate().
     markNeedsBuild();
-  }
-
-  bool _debugCheckOwnerBuildTargetExists(String methodName) {
-    assert(() {
-      if (owner._debugCurrentBuildTarget == null) {
-        throw new FlutterError(
-            '$methodName for ${widget.runtimeType} was called at an '
-                'inappropriate time.\n'
-                'It may only be called while the widgets are being built. A possible '
-                'cause of this error is when $methodName is called during '
-                'one of:\n'
-                ' * network I/O event\n'
-                ' * file I/O event\n'
-                ' * timer\n'
-                ' * microtask (caused by Future.then, async/await, scheduleMicrotask)'
-        );
-      }
-      return true;
-    }());
-    return true;
   }
 
   /// Returns a description of what caused this element to be created.
@@ -3968,7 +3947,6 @@ class InheritedElement extends ProxyElement {
   /// by first obtaining their [InheritedElement] using
   /// [BuildContext.ancestorInheritedElementForWidgetOfExactType].
   void dispatchDidChangeDependencies() {
-    assert(_debugCheckOwnerBuildTargetExists('dispatchDidChangeDependencies'));
     for (Element dependent in _dependents) {
       assert(() {
         // check that it really is our descendant
