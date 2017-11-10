130af88fde419a2c67629548e5eefbc0b50d73b0
Greg Spencer <gspencergoog@users.noreply.github.com>
2017-11-09 16:37:21 -0800

    Adding Rect.expandToInclude and Rect.longestSide (#4309)
    Adding Rect.expandToInclude and Rect.longestSide to make the Rect API more symmetric.

--- testing/dart/rect_test.dart
+++ testing/dart/rect_test.dart
@@ -8,7 +8,7 @@ import 'package:test/test.dart';
 
 void main() {
   test("rect accessors", () {
-    Rect r = new Rect.fromLTRB(1.0, 3.0, 5.0, 7.0);
+    final Rect r = new Rect.fromLTRB(1.0, 3.0, 5.0, 7.0);
     expect(r.left, equals(1.0));
     expect(r.top, equals(3.0));
     expect(r.right, equals(5.0));
@@ -16,22 +16,67 @@ void main() {
   });
 
   test("rect created by width and height", () {
-    Rect r = new Rect.fromLTWH(1.0, 3.0, 5.0, 7.0);
+    final Rect r = new Rect.fromLTWH(1.0, 3.0, 5.0, 7.0);
     expect(r.left, equals(1.0));
     expect(r.top, equals(3.0));
     expect(r.right, equals(6.0));
     expect(r.bottom, equals(10.0));
+    expect(r.shortestSide, equals(5.0));
+    expect(r.longestSide, equals(7.0));
   });
 
   test("rect intersection", () {
-    Rect r1 = new Rect.fromLTRB(0.0, 0.0, 100.0, 100.0);
-    Rect r2 = new Rect.fromLTRB(50.0, 50.0, 200.0, 200.0);
-    Rect r3 = r1.intersect(r2);
+    final Rect r1 = new Rect.fromLTRB(0.0, 0.0, 100.0, 100.0);
+    final Rect r2 = new Rect.fromLTRB(50.0, 50.0, 200.0, 200.0);
+    final Rect r3 = r1.intersect(r2);
     expect(r3.left, equals(50.0));
     expect(r3.top, equals(50.0));
     expect(r3.right, equals(100.0));
     expect(r3.bottom, equals(100.0));
-    Rect r4 = r2.intersect(r1);
+    final Rect r4 = r2.intersect(r1);
     expect(r4, equals(r3));
+  });
+
+  test("rect expandToInclude overlapping rects", () {
+    final Rect r1 = new Rect.fromLTRB(0.0, 0.0, 100.0, 100.0);
+    final Rect r2 = new Rect.fromLTRB(50.0, 50.0, 200.0, 200.0);
+    final Rect r3 = r1.expandToInclude(r2);
+    expect(r3.left, equals(0.0));
+    expect(r3.top, equals(0.0));
+    expect(r3.right, equals(200.0));
+    expect(r3.bottom, equals(200.0));
+    final Rect r4 = r2.expandToInclude(r1);
+    expect(r4, equals(r3));
+  });
+
+  test("rect expandToInclude crossing rects", () {
+    final Rect r1 = new Rect.fromLTRB(50.0, 0.0, 50.0, 200.0);
+    final Rect r2 = new Rect.fromLTRB(0.0, 50.0, 200.0, 50.0);
+    final Rect r3 = r1.expandToInclude(r2);
+    expect(r3.left, equals(0.0));
+    expect(r3.top, equals(0.0));
+    expect(r3.right, equals(200.0));
+    expect(r3.bottom, equals(200.0));
+    final Rect r4 = r2.expandToInclude(r1);
+    expect(r4, equals(r3));
+  });
+
+  test("size created from doubles", () {
+    final Size size = new Size(5.0, 7.0);
+    expect(size.width, equals(5.0));
+    expect(size.height, equals(7.0));
+    expect(size.shortestSide, equals(5.0));
+    expect(size.longestSide, equals(7.0));
+  });
+
+  test("rounded rect created from rect and radii", () {
+    final Rect baseRect = new Rect.fromLTWH(1.0, 3.0, 5.0, 7.0);
+    final RRect r = new RRect.fromRectXY(baseRect, 1.0, 1.0);
+    expect(r.left, equals(1.0));
+    expect(r.top, equals(3.0));
+    expect(r.right, equals(6.0));
+    expect(r.bottom, equals(10.0));
+    expect(r.shortestSide, equals(5.0));
+    expect(r.longestSide, equals(7.0));
   });
 }
