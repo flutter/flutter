271d588afdf69ab6e331878f6ea79b82f32b038b
Greg Spencer <gspencer@google.com>
2017-11-10 08:36:54 -0800

    Review changes

--- packages/flutter_tools/test/commands/ide_config_test.dart
+++ packages/flutter_tools/test/commands/ide_config_test.dart
@@ -1,4 +1,4 @@
-// Copyright 2015 The Chromium Authors. All rights reserved.
+// Copyright 2017 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -38,7 +38,7 @@ void main() {
           contents[path] = 'dir';
         } else if (fs.isFileSync(absPath)) {
           contents[path] = fs.file(absPath).readAsStringSync();
-        } else {}
+        }
       }
       return contents;
     }
