807d1e8d60ddac666f45f09123b02ad7f22be595
Mikkel Ravn <mravn@google.com>
2017-11-21 10:22:56 +0100

    Add integration test of textures for Android

---
+++ dev/integration_tests/external_ui/lib/main.dart
@@ -0,0 +1,108 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_driver/driver_extension.dart';
+
+void main() {
+  enableFlutterDriverExtension();
+  runApp(new MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State createState() => new MyAppState();
+}
+
+const MethodChannel channel = const MethodChannel('texture');
+
+enum FrameState { initial, slow, afterSlow, fast, afterFast }
+
+class MyAppState extends State<MyApp> {
+  int _widgetBuilds = 0;
+  FrameState _state = FrameState.initial;
+  String _summary = 'Press play to start producing frames';
+  IconData _icon = Icons.play_arrow;
+
+  void _summarizeStats() async {
+    final double framesProduced = await channel.invokeMethod('getProducedFrameRate');
+    final double framesConsumed = await channel.invokeMethod('getConsumedFrameRate');
+    _summary = '''
+Produced: ${framesProduced.toStringAsFixed(1)}fps
+Consumed: ${framesConsumed.toStringAsFixed(1)}fps
+Widget builds: $_widgetBuilds''';
+  }
+
+  void _nextState() async {
+    switch (_state) {
+    case FrameState.initial:
+      _widgetBuilds = 0;
+      _summary = 'Producing frames slowly, collecting stats...';
+      _state = FrameState.slow;
+      _icon = Icons.stop;
+      channel.invokeMethod('start', 10);
+      break;
+    case FrameState.slow:
+      await channel.invokeMethod('stop');
+      await _summarizeStats();
+      _icon = Icons.fast_forward;
+      _state = FrameState.afterSlow;
+      break;
+    case FrameState.afterSlow:
+      _widgetBuilds = 0;
+      _summary = 'Producing frames quickly, collecting stats...';
+      _state = FrameState.fast;
+      _icon = Icons.stop;
+      channel.invokeMethod('start', 100);
+      break;
+    case FrameState.fast:
+      await channel.invokeMethod('stop');
+      await _summarizeStats();
+      _state = FrameState.afterFast;
+      _icon = Icons.replay;
+      break;
+    case FrameState.afterFast:
+      _summary = 'Press play to start producing frames';
+      _state = FrameState.initial;
+      _icon = Icons.play_arrow;
+      break;
+    }
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    _widgetBuilds++;
+    return new MaterialApp(
+      home: new Scaffold(
+        body: new Center(
+          child: new Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              new Container(
+                width: 300.0,
+                height: 200.0,
+                child: new Texture(textureId: 0),
+              ),
+              new Container(
+                width: 300.0,
+                height: 60.0,
+                color: Colors.grey,
+                child: new Center(
+                  child: new Text(
+                    _summary,
+                    key: const ValueKey<String>('summary'),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        floatingActionButton: new FloatingActionButton(
+          key: const ValueKey<String>('fab'),
+          child: new Icon(_icon),
+          onPressed: _nextState,
+        ),
+      ),
+    );
+  }
+}
---
+++ dev/integration_tests/external_ui/test_driver/main_test.dart
@@ -0,0 +1,51 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'dart:async';
+import 'dart:io';
+import 'package:flutter_driver/flutter_driver.dart';
+import 'package:test/test.dart';
+
+final RegExp statsRegExp = new RegExp('Produced: (.*)fps\nConsumed: (.*)fps\nWidget builds: (.*)');
+
+void main() {
+  group('texture suite', () {
+    FlutterDriver driver;
+
+    setUpAll(() async {
+      driver = await FlutterDriver.connect();
+    });
+
+    test('texture rendering', () async {
+      final SerializableFinder fab = find.byValueKey('fab');
+      final SerializableFinder summary = find.byValueKey('summary');
+
+      await driver.tap(fab);
+      await new Future.delayed(const Duration(seconds: 3));
+      await driver.tap(fab);
+
+      final String statsSlow = await driver.getText(summary);
+      final Match matchSlow = statsRegExp.matchAsPrefix(statsSlow);
+      expect(matchSlow, isNotNull);
+      expect(double.parse(matchSlow.group(1)), closeTo(10.0, 2.0));
+      expect(double.parse(matchSlow.group(2)), closeTo(10.0, 2.0));
+      expect(int.parse(matchSlow.group(3)), 1);
+
+      await driver.tap(fab);
+      await new Future.delayed(const Duration(seconds: 3));
+      await driver.tap(fab);
+
+      final String statsFast = await driver.getText(summary);
+      final Match matchFast = statsRegExp.matchAsPrefix(statsFast);
+      expect(matchFast, isNotNull);
+      expect(double.parse(matchFast.group(1)), closeTo(100.0, 2.0));
+      expect(double.parse(matchFast.group(2)), inInclusiveRange(52.0, 61.0));
+      expect(int.parse(matchFast.group(3)), 1);
+    });
+
+    tearDownAll(() async {
+      driver?.close();
+    });
+  });
+}
