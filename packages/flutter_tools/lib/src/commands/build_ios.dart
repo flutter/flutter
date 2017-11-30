c9af26e77fd78f2d4b309e494414819e32afb7a1
Alexander Aprelev <aam@google.com>
2017-11-30 09:42:37 -0800

    Hide preview-dart-2 flag. Show only in verbose

--- packages/flutter_tools/lib/src/commands/build_ios.dart
+++ packages/flutter_tools/lib/src/commands/build_ios.dart
@@ -13,7 +13,7 @@ import '../ios/mac.dart';
 import 'build.dart';
 
 class BuildIOSCommand extends BuildSubCommand {
-  BuildIOSCommand() {
+  BuildIOSCommand({bool verboseHelp: false}) {
     usesTargetOption();
     usesFlavorOption();
     usesPubOption();
@@ -29,7 +29,8 @@ class BuildIOSCommand extends BuildSubCommand {
     argParser.addFlag('simulator', help: 'Build for the iOS simulator instead of the device.');
     argParser.addFlag('codesign', negatable: true, defaultsTo: true,
         help: 'Codesign the application bundle (only available on device builds).');
-    argParser.addFlag('preview-dart-2', negatable: false);
+    argParser.addFlag('preview-dart-2', negatable: false,
+        hide: !verboseHelp);
   }
 
   @override
