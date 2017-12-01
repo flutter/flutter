31f0a6055c65df5985a6c65b947a6031d88ec59d
Ben Konyi <bkonyi@google.com>
2017-11-30 19:47:20 -0500

    Changes to assets/ build/ flow/ runtime/ and shell/ to allow for compilation on Windows (flutter/engine#4407)
    Made changes to assets/ build/ flow/ runtime/ and shell/ to allow for
    compilation on Windows.

--- engine/src/flutter/assets/directory_asset_bundle.cc
+++ engine/src/flutter/assets/directory_asset_bundle.cc
@@ -3,9 +3,13 @@
 // found in the LICENSE file.
 
 #include "flutter/assets/directory_asset_bundle.h"
+#include "lib/fxl/build_config.h"
 
 #include <fcntl.h>
+
+#if !defined(OS_WIN)
 #include <unistd.h>
+#endif
 
 #include <utility>
 
