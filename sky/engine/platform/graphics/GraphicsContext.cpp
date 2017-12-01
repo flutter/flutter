3afab47010894fe580caead3014740b2f2f70d6a
Brian Salomon <bsalomon@google.com>
2017-12-01 09:48:57 -0500

    Remove use of SkPath::setConvexity in GraphicsContext. (#4398)

--- sky/engine/platform/graphics/GraphicsContext.cpp
+++ sky/engine/platform/graphics/GraphicsContext.cpp
@@ -1421,17 +1421,6 @@ void GraphicsContext::setPathFromConvexPoints(SkPath* path,
     path->lineTo(WebCoreFloatToSkScalar(points[i].x()),
                  WebCoreFloatToSkScalar(points[i].y()));
   }
-
-  /*  The code used to just blindly call this
-          path->setIsConvex(true);
-      But webkit can sometimes send us non-convex 4-point values, so we mark the
-     path's convexity as unknown, so it will get computed by skia at draw time.
-      See crbug.com 108605
-  */
-  SkPath::Convexity convexity = SkPath::kConvex_Convexity;
-  if (numPoints == 4)
-    convexity = SkPath::kUnknown_Convexity;
-  path->setConvexity(convexity);
 }
 
 void GraphicsContext::setRadii(SkVector* radii,
