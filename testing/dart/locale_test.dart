7e4df308a7f39bf1027ca6bc1f37d0a3b2e79e76
Ian Hickson <ian@hixie.ch>
2017-12-06 12:17:30 -0800

    Make Locale know about the deprecated Hebrew language code. (#4411)
    ...by making it know about ALL the deprecated language and region codes.

---
+++ testing/dart/locale_test.dart
@@ -0,0 +1,22 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'dart:ui';
+
+import 'package:test/test.dart';
+
+void main() {
+  test('Locale', () {
+    final Null $null = null;
+    expect(const Locale('en').toString(), 'en');
+    expect(const Locale('en'), new Locale('en', $null));
+    expect(const Locale('en').hashCode, new Locale('en', $null).hashCode);
+    expect(const Locale('en'), isNot(new Locale('en', ''))));
+    expect(const Locale('en').hashCode, isNot(new Locale('en', '').hashCode));
+    expect(const Locale('en', 'US').toString(), 'en_US');
+    expect(const Locale('iw').toString(), 'he');
+    expect(const Locale('iw', 'DD').toString(), 'he_DE');
+    expect(const Locale('iw', 'DD'), const Locale('he', 'DE'));
+  });
+}
