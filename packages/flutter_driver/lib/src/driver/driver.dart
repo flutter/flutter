b6ca52796c5e1e3996b2539fde44cd5d33b76516
Yegor Jbanov <yjbanov@google.com>
2017-12-06 15:08:16 -0800

    document enterText behavior

--- packages/flutter_driver/lib/src/driver/driver.dart
+++ packages/flutter_driver/lib/src/driver/driver.dart
@@ -423,7 +423,13 @@ class FlutterDriver {
   ///
   /// This method does not use the operating system keyboard to enter text.
   /// Instead it emulates text entry by sending events identical to those sent
-  /// by the operating system keyboard.
+  /// by the operating system keyboard (the "TextInputClient.updateEditingState"
+  /// method channel call).
+  ///
+  /// Generally the behavior is dependent on the implementation of the widget
+  /// receiving the input. Usually, editable widgets, such as [EditableText] and
+  /// those built on top of it would replace the currently entered text with the
+  /// provided `text`.
   ///
   /// It is assumed that the widget receiving text input is focused prior to
   /// calling this method. Typically, a test would activate a widget, e.g. using
