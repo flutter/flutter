ab936a3a22b6ec40c7e1a7e42305a67f9c74f888
Ian Hickson <ian@hixie.ch>
2017-12-06 12:17:46 -0800

    Update gallery for https://github.com/flutter/flutter/pull/13195 (#13361)

--- examples/flutter_gallery/android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
+++ examples/flutter_gallery/android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
@@ -1,13 +1,19 @@
 package io.flutter.plugins;
 
 import io.flutter.plugin.common.PluginRegistry;
+import io.flutter.plugins.connectivity.ConnectivityPlugin;
+import io.flutter.plugins.deviceinfo.DeviceInfoPlugin;
 import io.flutter.plugins.urllauncher.UrlLauncherPlugin;
+import io.flutter.videoplayer.VideoPlayerPlugin;
 
 /**
  * Generated file. Do not edit.
  */
 public final class GeneratedPluginRegistrant {
   public static void registerWith(PluginRegistry registry) {
+    ConnectivityPlugin.registerWith(registry.registrarFor("io.flutter.plugins.connectivity.ConnectivityPlugin"));
+    DeviceInfoPlugin.registerWith(registry.registrarFor("io.flutter.plugins.deviceinfo.DeviceInfoPlugin"));
     UrlLauncherPlugin.registerWith(registry.registrarFor("io.flutter.plugins.urllauncher.UrlLauncherPlugin"));
+    VideoPlayerPlugin.registerWith(registry.registrarFor("io.flutter.videoplayer.VideoPlayerPlugin"));
   }
 }
--- examples/flutter_gallery/ios/Runner/GeneratedPluginRegistrant.m
+++ examples/flutter_gallery/ios/Runner/GeneratedPluginRegistrant.m
@@ -3,12 +3,18 @@
 //
 
 #import "GeneratedPluginRegistrant.h"
+#import <connectivity/ConnectivityPlugin.h>
+#import <device_info/DeviceInfoPlugin.h>
 #import <url_launcher/UrlLauncherPlugin.h>
+#import <video_player/VideoPlayerPlugin.h>
 
 @implementation GeneratedPluginRegistrant
 
 + (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [ConnectivityPlugin registerWithRegistrar:[registry registrarForPlugin:@"ConnectivityPlugin"]];
+  [FLTDeviceInfoPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTDeviceInfoPlugin"]];
   [UrlLauncherPlugin registerWithRegistrar:[registry registrarForPlugin:@"UrlLauncherPlugin"]];
+  [FLTVideoPlayerPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTVideoPlayerPlugin"]];
 }
 
 @end
