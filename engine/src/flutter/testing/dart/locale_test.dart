c3a99d4fbbecd97cd7e55deecdf7b88e99510a66
Chris Bracken <chris@bracken.jp>
2017-12-06 15:38:40 -0800

    Fix an extra paren in locale_test.dart (flutter/engine#4423)

--- engine/src/flutter/testing/dart/locale_test.dart
+++ engine/src/flutter/testing/dart/locale_test.dart
@@ -12,7 +12,7 @@ void main() {
     expect(const Locale('en').toString(), 'en');
     expect(const Locale('en'), new Locale('en', $null));
     expect(const Locale('en').hashCode, new Locale('en', $null).hashCode);
-    expect(const Locale('en'), isNot(new Locale('en', ''))));
+    expect(const Locale('en'), isNot(new Locale('en', '')));
     expect(const Locale('en').hashCode, isNot(new Locale('en', '').hashCode));
     expect(const Locale('en', 'US').toString(), 'en_US');
     expect(const Locale('iw').toString(), 'he');
