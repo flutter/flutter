1000b1b48def89dc0df3b45f7411dde111cfc956
Amir Hardon <amirha@google.com>
2017-12-07 09:37:04 -0800

    use PathCubic in placeholder icon to silence unused warning

--- packages/flutter/lib/src/material_animated_icons/data/menu_arrow.g.dart
+++ packages/flutter/lib/src/material_animated_icons/data/menu_arrow.g.dart
@@ -11,36 +11,37 @@ const _AnimatedIconData _$menu_arrow = const _AnimatedIconData(
   const Size(48.0, 48.0),
   const <_PathFrames> [
     const _PathFrames(
-      opacities: const <double> [1.0, 0.2],
+      opacities: const <double> [1.0, 1.0],
       commands: const <_PathCommand> [
         const _PathMoveTo(
           const <Offset> [
-            const Offset(0.0, 0.0),
-            const Offset(0.0, 38.0),
+            const Offset(0.0, 24.0),
+            const Offset(0.0, 24.0),
+            const Offset(0.0, 24.0),
+          ],
+        ),
+        const _PathCubicTo(
+          const <Offset> [
+            const Offset(16.0, 24.0),
+            const Offset(16.0, 10.0),
+            const Offset(16.0, 48.0),
+          ],
+          const <Offset> [
+            const Offset(32.0, 24.0),
+            const Offset(32.0, 10.0),
+            const Offset(32.0, 48.0),
+          ],
+          const <Offset> [
+            const Offset(48.0, 24.0),
+            const Offset(48.0, 24.0),
+            const Offset(48.0, 24.0),
           ],
         ),
         const _PathLineTo(
           const <Offset> [
-            const Offset(48.0, 0.0),
-            const Offset(48.0, 38.0),
-          ],
-        ),
-        const _PathLineTo(
-          const <Offset> [
-            const Offset(48.0, 10.0),
-            const Offset(48.0, 48.0),
-          ],
-        ),
-        const _PathLineTo(
-          const <Offset> [
-            const Offset(0.0, 10.0),
-            const Offset(0.0, 48.0),
-          ],
-        ),
-        const _PathLineTo(
-          const <Offset> [
-            const Offset(0.0, 00.0),
-            const Offset(0.0, 38.0),
+            const Offset(0.0, 24.0),
+            const Offset(0.0, 24.0),
+            const Offset(0.0, 24.0),
           ],
         ),
         const _PathClose(),
