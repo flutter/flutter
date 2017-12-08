927a143d7973aa7ee1241b8e64542b5f2afd958b
Zachary Anderson <zanderso@users.noreply.github.com>
2017-12-08 09:38:02 -0800

    [flutter_tools][plugins] generated registerWith guards against re-registration (#13384)

--- packages/flutter_tools/lib/src/plugins.dart
+++ packages/flutter_tools/lib/src/plugins.dart
@@ -102,9 +102,21 @@ import {{package}}.{{class}};
  */
 public final class GeneratedPluginRegistrant {
   public static void registerWith(PluginRegistry registry) {
+    if (alreadyRegisteredWith(registry)) {
+      return;
+    }
 {{#plugins}}
     {{class}}.registerWith(registry.registrarFor("{{package}}.{{class}}"));
 {{/plugins}}
+  }
+
+  private static boolean alreadyRegisteredWith(PluginRegistry registry) {
+    final String key = GeneratedPluginRegistrant.class.getCanonicalName();
+    if (registry.hasPlugin(key)) {
+      return true;
+    }
+    registry.registrarFor(key);
+    return false;
   }
 }
 ''';
