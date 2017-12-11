e2e5a8b4bdfb5b4e6bf60b9d5f43575efdf0a90c
Fredrik Simón <fredrik@fsimon.net>
2017-12-11 22:29:22 +0100

    Expose textAlign on TextFormField (#13414)
    * Expose textAlign on TextFormField
    
    Fixes #11404
    
    * Added name to AUTHORS
    
    * Added a test for TextFormWidget's textAlign

---
+++ packages/flutter/test/material/text_form_field_test.dart
@@ -0,0 +1,30 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('Passes textAlign to underlying TextField', (WidgetTester tester) async {
+    const TextAlign alignment = TextAlign.center;
+
+    await tester.pumpWidget(
+      new MaterialApp(
+        home: new Material(
+          child: new Center(
+            child: new TextFormField(
+              textAlign: alignment,
+            ),
+          ),
+        ),
+      ),
+    );
+
+    final Finder textFieldFinder = find.byType(TextField);
+    expect(textFieldFinder, findsOneWidget);
+
+    final TextField textFieldWidget = tester.widget(textFieldFinder);
+    expect(textFieldWidget.textAlign, alignment);
+  });
+}
