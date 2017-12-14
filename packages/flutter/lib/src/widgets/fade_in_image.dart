3a42ebef9a545b67532378e127a574347755687e
Xiao Yu <xster@google.com>
2017-12-14 12:48:01 -0800

    Document various 'alignment' members so it's clear how to concretely specify it.

--- packages/flutter/lib/src/widgets/fade_in_image.dart
+++ packages/flutter/lib/src/widgets/fade_in_image.dart
@@ -251,6 +251,13 @@ class FadeInImage extends StatefulWidget {
   /// must be in scope.
   ///
   /// Defaults to [Alignment.center].
+  ///
+  /// See also:
+  ///
+  ///  * [Alignment], a class with convenient constants typically used to
+  ///    specify an [AlignmentGeometry].
+  ///  * [AlignmentDirectional], like [Alignment] for specifying alignments
+  ///    relative to text direction.
   final AlignmentGeometry alignment;
 
   /// How to paint any portions of the layout bounds not covered by the image.
