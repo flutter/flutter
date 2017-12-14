fa96568e1545410b9baf7aaa10c9ce3f49d99ff4
Craig Stout <cstout@google.com>
2017-12-14 15:00:24 -0800

    Transition render target images before submit.
    Required by the spec; see '6.7.4. Queue Family Ownership Transfer'
    of Vulkan 1.0.66.
    
    Transitioning the image layout also fixes an image resolve failure
    issue introduced by optimizations present in the intel mesa vulkan
    driver v17.2.

--- engine/src/flutter/vulkan/vulkan_provider.h
+++ engine/src/flutter/vulkan/vulkan_provider.h
@@ -13,6 +13,22 @@ class VulkanProvider {
  public:
   virtual const vulkan::VulkanProcTable& vk() = 0;
   virtual const vulkan::VulkanHandle<VkDevice>& vk_device() = 0;
+
+  vulkan::VulkanHandle<VkFence> CreateFence() {
+    const VkFenceCreateInfo create_info = {
+        .sType = VK_STRUCTURE_TYPE_FENCE_CREATE_INFO,
+        .pNext = nullptr,
+        .flags = 0,
+    };
+    VkFence fence;
+    if (VK_CALL_LOG_ERROR(vk().CreateFence(vk_device(), &create_info, nullptr,
+                                           &fence)) != VK_SUCCESS)
+      return vulkan::VulkanHandle<VkFence>();
+
+    return {fence, [this](VkFence fence) {
+              vk().DestroyFence(vk_device(), fence, nullptr);
+            }};
+  }
 };
 
 }  // namespace vulkan
