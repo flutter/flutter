37c132e2ac8b6a54686fd8dd021816f343b582e6
Jason Simmons <jason-simmons@users.noreply.github.com>
2017-12-13 17:17:05 -0800

    Provide the skia_vulkan_header variable now required by Skia's Vulkan build scripts (#4459)

---
+++ vulkan/skia_vulkan_header.h
@@ -0,0 +1,11 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifdef __ANDROID__
+#ifndef VK_USE_PLATFORM_ANDROID_KHR
+#define VK_USE_PLATFORM_ANDROID_KHR 1
+#endif  // VK_USE_PLATFORM_ANDROID_KHR
+#endif  // __ANDROID__
+
+#include "third_party/vulkan/src/vulkan/vulkan.h"
