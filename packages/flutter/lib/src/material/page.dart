flutter/flutter

commit b1c870b31295ec2ea508d80ab5e3771fb3240b10

Reenable fade transition for Material page transition (#13048) (#13680)
Fixes #12877 by reverting #9472

packages/flutter/lib/src/material/page.dart
@@ -24,9 +24,14 @@ class _MountainViewPageTransition extends StatelessWidget {
          parent: routeAnimation, // The route's linear 0.0 - 1.0 animation.
          curve: Curves.fastOutSlowIn,
        )),
+       _opacityAnimation = new CurvedAnimation(
+         parent: routeAnimation,
+         curve: Curves.easeIn, // Eyeballed from other Material apps.
+       ),
        super(key: key);
 
   final Animation<Offset> _positionAnimation;
+  final Animation<double> _opacityAnimation;
   final Widget child;
 
   @override
@@ -34,7 +39,10 @@ class _MountainViewPageTransition extends StatelessWidget {
     // TODO(ianh): tell the transform to be un-transformed for hit testing
     return new SlideTransition(
       position: _positionAnimation,
-      child: child,
+      child: new FadeTransition(
+        opacity: _opacityAnimation,
+        child: child,
+      ),
     );
   }
 }

packages/flutter/test/material/page_test.dart
@@ -28,6 +28,8 @@ void main() {
     await tester.pump();
     await tester.pump(const Duration(milliseconds: 1));
 
+    Opacity widget2Opacity =
+        tester.element(find.text('Page 2')).ancestorWidgetOfExactType(Opacity);
     Offset widget2TopLeft = tester.getTopLeft(find.text('Page 2'));
     final Size widget2Size = tester.getSize(find.text('Page 2'));
 
@@ -35,8 +37,10 @@ void main() {
     expect(widget1TopLeft.dx == widget2TopLeft.dx, true);
     // Page 1 is above page 2 mid-transition.
     expect(widget1TopLeft.dy < widget2TopLeft.dy, true);
-    // Animation begins from the top of the page.
-    expect(widget2TopLeft.dy < widget2Size.height, true);
+    // Animation begins 3/4 of the way up the page.
+    expect(widget2TopLeft.dy < widget2Size.height / 4.0, true);
+    // Animation starts with page 2 being near transparent.
+    expect(widget2Opacity.opacity < 0.01, true);
 
     await tester.pump(const Duration(milliseconds: 300));
 
@@ -48,10 +52,14 @@ void main() {
     await tester.pump();
     await tester.pump(const Duration(milliseconds: 1));
 
+    widget2Opacity =
+        tester.element(find.text('Page 2')).ancestorWidgetOfExactType(Opacity);
     widget2TopLeft = tester.getTopLeft(find.text('Page 2'));
 
     // Page 2 starts to move down.
     expect(widget1TopLeft.dy < widget2TopLeft.dy, true);
+    // Page 2 starts to lose opacity.
+    expect(widget2Opacity.opacity < 1.0, true);
 
     await tester.pump(const Duration(milliseconds: 300));
 

commit 54db5546ab1681a04067297e2a256026f0a1bc08

Attempt a less invasive way to disable fading.
Hopefully this will fix the performance regression in https://github.com/flutter/flutter/pull/13680 but with an easier way to flip the switch.

packages/flutter/lib/src/material/page.dart
@@ -18,16 +18,17 @@ final Tween<Offset> _kBottomUpTween = new Tween<Offset>(
 class _MountainViewPageTransition extends StatelessWidget {
   _MountainViewPageTransition({
     Key key,
+    @required bool fade,
     @required Animation<double> routeAnimation,
     @required this.child,
   }) : _positionAnimation = _kBottomUpTween.animate(new CurvedAnimation(
          parent: routeAnimation, // The route's linear 0.0 - 1.0 animation.
          curve: Curves.fastOutSlowIn,
        )),
-       _opacityAnimation = new CurvedAnimation(
+       _opacityAnimation = fade ? new CurvedAnimation(
          parent: routeAnimation,
          curve: Curves.easeIn, // Eyeballed from other Material apps.
-       ),
+       ) : const AlwaysStoppedAnimation<double>(1.0),
        super(key: key);
 
   final Animation<Offset> _positionAnimation;
@@ -82,6 +83,14 @@ class MaterialPageRoute<T> extends PageRoute<T> {
     // ignore: prefer_asserts_in_initializer_lists , https://github.com/dart-lang/sdk/issues/31223
     assert(opaque);
   }
+
+  /// Turns on the fading of routes during page transitions.
+  ///
+  /// This is currently disabled by default because of performance issues on
+  /// low-end phones. Eventually these issues will be resolved and this flag
+  /// will be removed.
+  @Deprecated('This flag will eventually be removed once the performance issues are resolved. See: https://github.com/flutter/flutter/issues/13736')
+  static bool debugEnableFadingRoutes = false;
 
   /// Builds the primary contents of the route.
   final WidgetBuilder builder;
@@ -159,6 +168,7 @@ class MaterialPageRoute<T> extends PageRoute<T> {
       return new _MountainViewPageTransition(
         routeAnimation: animation,
         child: child,
+        fade: debugEnableFadingRoutes, // ignore: deprecated_member_use
       );
     }
   }

packages/flutter/test/material/page_test.dart
@@ -40,7 +40,7 @@ void main() {
     // Animation begins 3/4 of the way up the page.
     expect(widget2TopLeft.dy < widget2Size.height / 4.0, true);
     // Animation starts with page 2 being near transparent.
-    expect(widget2Opacity.opacity < 0.01, true);
+    expect(widget2Opacity.opacity < 0.01, MaterialPageRoute.debugEnableFadingRoutes); // ignore: deprecated_member_use
 
     await tester.pump(const Duration(milliseconds: 300));
 
@@ -59,7 +59,7 @@ void main() {
     // Page 2 starts to move down.
     expect(widget1TopLeft.dy < widget2TopLeft.dy, true);
     // Page 2 starts to lose opacity.
-    expect(widget2Opacity.opacity < 1.0, true);
+    expect(widget2Opacity.opacity < 1.0, MaterialPageRoute.debugEnableFadingRoutes); // ignore: deprecated_member_use
 
     await tester.pump(const Duration(milliseconds: 300));
 