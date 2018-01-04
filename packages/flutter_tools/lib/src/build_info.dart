8cb858f6b29782a53f39d182c40f02647237a968
Siva <a-siva@users.noreply.github.com>
2018-01-04 10:39:02 -0800

    support for --strong option (#13859)
    * Plumb a --strong option through to the front end server and the engine
    so that we can run flutter apps in preview-dart-2 and strong mode
    
    * - Address analyzer lint issues
    *- correctly set up strong mode option in the case of AOT builds

--- packages/flutter_tools/lib/src/build_info.dart
+++ packages/flutter_tools/lib/src/build_info.dart
@@ -12,6 +12,7 @@ import 'globals.dart';
 class BuildInfo {
   const BuildInfo(this.mode, this.flavor,
       {this.previewDart2,
+      this.strongMode,
       this.extraFrontEndOptions,
       this.extraGenSnapshotOptions,
       this.preferSharedLibrary});
@@ -27,6 +28,9 @@ class BuildInfo {
 
   // Whether build should be done using Dart2 Frontend parser.
   final bool previewDart2;
+  
+  // Whether build should use strong mode semantics.
+  final bool strongMode;
 
   /// Extra command-line options for front-end.
   final String extraFrontEndOptions;
