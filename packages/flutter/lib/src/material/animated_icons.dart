885e96914df6e4386fbdd53cb950a53089010682
amirh <amirh@users.noreply.github.com>
2018-01-04 13:01:45 -0800

    Move animated_icons into the material package (#13886)

--- packages/flutter/lib/src/material/animated_icons.dart
+++ packages/flutter/lib/src/material/animated_icons.dart
@@ -3,26 +3,24 @@
 // found in the LICENSE file.
 
 /// Flutter widgets implementing Material Design animated icons.
-///
-/// To use, import `package:flutter/material_animated_icons.dart`.
 library material_animated_icons;
 
 import 'dart:math' as math show pi;
 import 'dart:ui' as ui show Paint, Path, Canvas;
 import 'dart:ui' show lerpDouble;
 
-import 'package:flutter/material.dart';
-import 'package:meta/meta.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/widgets.dart';
 
 // This package is split into multiple parts to enable a private API that is
 // testable.
 
 // Public API.
-part 'src/material_animated_icons/animated_icons.dart';
+part 'animated_icons/animated_icons.dart';
 // Provides a public interface for referring to the private icon
 // implementations.
-part 'src/material_animated_icons/animated_icons_data.dart';
+part 'animated_icons/animated_icons_data.dart';
 
 // Animated icons data files.
-part 'src/material_animated_icons/data/arrow_menu.g.dart';
-part 'src/material_animated_icons/data/menu_arrow.g.dart';
+part 'animated_icons/data/arrow_menu.g.dart';
+part 'animated_icons/data/menu_arrow.g.dart';
