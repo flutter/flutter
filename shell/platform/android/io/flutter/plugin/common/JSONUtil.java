9b98d194f292ebf9b9c322bc0a603999d5848f5b
Mehmet Fidanboylu <mehmetf@users.noreply.github.com>
2018-01-07 07:51:46 -0800

    Fix build break caused by https://github.com/flutter/engine/pull/4491 (#4524)

--- shell/platform/android/io/flutter/plugin/common/JSONUtil.java
+++ shell/platform/android/io/flutter/plugin/common/JSONUtil.java
@@ -3,6 +3,7 @@ package io.flutter.plugin.common;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
 import java.util.HashMap;
 import java.util.Map;
