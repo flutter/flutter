c3366a65e1117fd5a548aba4184b38f026a54541
Alexander Aprelev <aam@google.com>
2018-01-08 10:05:44 -0800

    Set asset directory path only on first hot reload. (#13934)

--- packages/flutter_tools/lib/src/run_hot.dart
+++ packages/flutter_tools/lib/src/run_hot.dart
@@ -515,9 +515,13 @@ class HotRunner extends ResidentRunner {
 
       int countExpectedReports = 0;
       for (FlutterDevice device in flutterDevices) {
-        // List has one report per Flutter view.
-        await device.resetAssetDirectory();
+        if (_runningFromSnapshot) {
+          // Asset directory has to be set only once when we switch from
+          // running from snapshot to running from uploaded files.
+          await device.resetAssetDirectory();
+        }
 
+        // List has one report per Flutter view.
         final List<Future<Map<String, dynamic>>> reports = device.reloadSources(
           entryPath,
           pause: pause
