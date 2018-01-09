8e777342f545c198e9bdd4b327ba8fcaaf61771d
Xiao Yu <xster@google.com>
2018-01-09 15:41:03 -0800

    repatch lost https://github.com/flutter/flutter/commit/977701cf695ec106a52dde9f3659a7b132826748

--- packages/flutter/test/widgets/list_wheel_scroll_view_test.dart
+++ packages/flutter/test/widgets/list_wheel_scroll_view_test.dart
@@ -3,6 +3,7 @@
 // found in the LICENSE file.
 
 import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
 
 import '../rendering/mock_canvas.dart';
