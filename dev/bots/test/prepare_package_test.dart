6fda8ee821dd2e63e7949e57b10efde84bb0a3cc
Greg Spencer <gspencergoog@users.noreply.github.com>
2018-01-09 19:47:19 -0800

    Reverting package changes until I can figure out how to fix Windows. (#14007)
    * Revert "Fixed output validation. (#14005)"
    
    This reverts commit d84398db72807f8910833d5fe44a097be152c74a.
    
    * Revert "Update package prep script to do async process execution and emit output as it happens. (#13918)"
    
    This reverts commit b7169c1d951aa188c1cdc3b980e257e8a92f8abe.

--- dev/bots/test/prepare_package_test.dart
+++ dev/bots/test/prepare_package_test.dart
@@ -2,13 +2,12 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'dart:async';
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 import 'package:path/path.dart' as path;
-import 'package:process/process.dart';
 
 import '../prepare_package.dart';
 
@@ -18,86 +17,87 @@ void main() {
     Directory tmpDir;
     Directory flutterDir;
     File outputFile;
-    MockProcessManager processManager;
-    List<MockProcess> results = <MockProcess>[];
+    MockProcessRunner runner;
+    List<MockProcessResult> results;
     final List<List<String>> args = <List<String>>[];
     final List<Map<Symbol, dynamic>> namedArgs = <Map<Symbol, dynamic>>[];
+    final String zipExe = Platform.isWindows ? '7za.exe' : 'zip';
+    final String tarExe = Platform.isWindows ? 'tar.exe' : 'tar';
+    final String gitExe = Platform.isWindows ? 'git.bat' : 'git';
     String flutterExe;
 
     void _verifyCommand(List<dynamic> args, String expected) {
       final List<String> expectedList = expected.split(' ');
-      expect(args[0], orderedEquals(expectedList));
+      final String executable = expectedList.removeAt(0);
+      expect(args[0], executable);
+      expect(args[1], orderedEquals(expectedList));
     }
 
-    Future<Process> _nextResult(Invocation invocation) async {
+    ProcessResult _nextResult(Invocation invocation) {
       args.add(invocation.positionalArguments);
       namedArgs.add(invocation.namedArguments);
-      final Process result = results.isEmpty ? new MockProcess('', '', 0) : results.removeAt(0);
-      return new Future<Process>.value(result);
+      return results.isEmpty ? new MockProcessResult('', '', 0) : results.removeAt(0);
     }
 
     void _answerWithResults() {
       when(
-        processManager.start(
+        runner.call(
+          typed(captureAny),
           typed(captureAny),
           environment: typed(captureAny, named: 'environment'),
           workingDirectory: typed(captureAny, named: 'workingDirectory'),
+          includeParentEnvironment: typed(captureAny, named: 'includeParentEnvironment'),
         ),
       ).thenAnswer(_nextResult);
     }
 
     setUp(() async {
-      processManager = new MockProcessManager();
+      runner = new MockProcessRunner();
       args.clear();
       namedArgs.clear();
       tmpDir = await Directory.systemTemp.createTemp('flutter_');
-      outputFile =
-          new File(path.join(tmpDir.absolute.path, ArchiveCreator.defaultArchiveName('master')));
       flutterDir = new Directory(path.join(tmpDir.path, 'flutter'));
-      flutterDir.createSync(recursive: true);
       flutterExe =
-          path.join(flutterDir.path, 'bin', 'flutter');
+          path.join(flutterDir.path, 'bin', Platform.isWindows ? 'flutter.bat' : 'flutter');
     });
 
     tearDown(() async {
-      // On Windows, the directory is locked and not able to be deleted, because it is a
-      // temporary directory. So we just leave some (very small, because we're not actually
-      // building archives here) trash around to be deleted at the next reboot.
-      if (!Platform.isWindows) {
-        await tmpDir.delete(recursive: true);
-      }
+      await tmpDir.delete(recursive: true);
     });
 
     test('sets PUB_CACHE properly', () async {
-      preparer =
-          new ArchiveCreator(tmpDir, processManager: processManager, subprocessOutput: false);
+      outputFile = new File(path.join(tmpDir.absolute.path, 'flutter_master.tar.xz'));
+      preparer = new ArchiveCreator(tmpDir, outputFile, runner: runner);
       _answerWithResults();
-      await preparer.createArchive('master', outputFile);
+      results = <MockProcessResult>[new MockProcessResult('deadbeef\n', '', 0)];
+      preparer.checkoutFlutter('master');
+      preparer.prepareArchive();
+      preparer.createArchive();
       expect(
-        verify(processManager.start(
+        verify(runner.call(
+          captureAny,
           captureAny,
           workingDirectory: captureAny,
           environment: captureAny,
-        )).captured[1]['PUB_CACHE'],
+          includeParentEnvironment: typed(captureAny, named: 'includeParentEnvironment'),
+        )).captured[2]['PUB_CACHE'],
         endsWith(path.join('flutter', '.pub-cache')),
       );
     });
 
-    test('calls the right commands for archive output', () async {
-      preparer =
-          new ArchiveCreator(tmpDir, processManager: processManager, subprocessOutput: false);
+    test('calls the right commands for tar output', () async {
+      outputFile = new File(path.join(tmpDir.absolute.path, 'flutter_master.tar.xz'));
+      preparer = new ArchiveCreator(tmpDir, outputFile, runner: runner);
       _answerWithResults();
-      await preparer.createArchive('master', outputFile);
+      results = <MockProcessResult>[new MockProcessResult('deadbeef\n', '', 0)];
+      preparer.checkoutFlutter('master');
+      preparer.prepareArchive();
+      preparer.createArchive();
       final List<String> commands = <String>[
-        'git clone -b master https://chromium.googlesource.com/external/github.com/flutter/flutter',
-        'git reset --hard master',
-        'git remote remove origin',
-        'git remote add origin https://github.com/flutter/flutter.git',
-      ];
-      if (Platform.isWindows) {
-        commands.add('7za x ${path.join(tmpDir.path, 'mingit.zip')}');
-      }
-      commands.addAll(<String>[
+        '$gitExe clone -b master https://chromium.googlesource.com/external/github.com/flutter/flutter',
+        '$gitExe reset --hard master',
+        '$gitExe remote remove origin',
+        '$gitExe remote add origin https://github.com/flutter/flutter.git',
         '$flutterExe doctor',
         '$flutterExe update-packages',
         '$flutterExe precache',
@@ -105,13 +105,43 @@ void main() {
         '$flutterExe create --template=app ${path.join(tmpDir.path, 'create_app')}',
         '$flutterExe create --template=package ${path.join(tmpDir.path, 'create_package')}',
         '$flutterExe create --template=plugin ${path.join(tmpDir.path, 'create_plugin')}',
-        'git clean -f -X **/.packages',
-      ]);
-      if (Platform.isWindows) {
-        commands.add('7za a -tzip -mx=9 ${outputFile.absolute.path} flutter');
-      } else {
-        commands.add('tar cJf ${outputFile.absolute.path} flutter');
+        '$gitExe clean -f -X **/.packages',
+        '$tarExe cJf ${path.join(tmpDir.path, 'flutter_master.tar.xz')} flutter',
+      ];
+      int step = 0;
+      for (String command in commands) {
+        _verifyCommand(args[step++], command);
       }
+    });
+
+    test('calls the right commands for zip output', () async {
+      outputFile = new File(path.join(tmpDir.absolute.path, 'flutter_master.zip'));
+      preparer = new ArchiveCreator(tmpDir, outputFile, runner: runner);
+      _answerWithResults();
+      results = <MockProcessResult>[new MockProcessResult('deadbeef\n', '', 0)];
+      preparer.checkoutFlutter('master');
+      preparer.prepareArchive();
+      preparer.createArchive();
+      final List<String> commands = <String>[
+        '$gitExe clone -b master https://chromium.googlesource.com/external/github.com/flutter/flutter',
+        '$gitExe reset --hard master',
+        '$gitExe remote remove origin',
+        '$gitExe remote add origin https://github.com/flutter/flutter.git',
+        '$flutterExe doctor',
+        '$flutterExe update-packages',
+        '$flutterExe precache',
+        '$flutterExe ide-config',
+        '$flutterExe create --template=app ${path.join(tmpDir.path, 'create_app')}',
+        '$flutterExe create --template=package ${path.join(tmpDir.path, 'create_package')}',
+        '$flutterExe create --template=plugin ${path.join(tmpDir.path, 'create_plugin')}',
+        '$gitExe clean -f -X **/.packages',
+      ];
+      if (Platform.isWindows) {
+        commands.add('$zipExe a -tzip -mx=9 ${path.join(tmpDir.path, 'flutter_master.zip')} flutter');
+      } else {
+        commands.add('$zipExe -r -9 -q ${path.join(tmpDir.path, 'flutter_master.zip')} flutter');
+      }
+
       int step = 0;
       for (String command in commands) {
         _verifyCommand(args[step++], command);
@@ -119,37 +149,50 @@ void main() {
     });
 
     test('throws when a command errors out', () async {
-      preparer =
-          new ArchiveCreator(tmpDir, processManager: processManager, subprocessOutput: false);
+      outputFile = new File(path.join(tmpDir.absolute.path, 'flutter.tar.xz'));
+      preparer = new ArchiveCreator(
+        tmpDir,
+        outputFile,
+        runner: runner,
+      );
 
-      results = <MockProcess>[
-        new MockProcess('', '', 0),
-        new MockProcess('', "Don't panic.\n", -1)
+      results = <MockProcessResult>[
+        new MockProcessResult('', '', 0),
+        new MockProcessResult('OMG! OMG! an ERROR!\n', '', -1)
       ];
       _answerWithResults();
-      expect(expectAsync2<Null, String, File>(preparer.createArchive)('master', new File('foo')),
+      expect(() => preparer.checkoutFlutter('master'),
           throwsA(const isInstanceOf<ProcessFailedException>()));
+      expect(args.length, 2);
+      _verifyCommand(args[0],
+          '$gitExe clone -b master https://chromium.googlesource.com/external/github.com/flutter/flutter');
+      _verifyCommand(args[1], '$gitExe reset --hard master');
     });
   });
 }
 
-class MockProcessManager extends Mock implements ProcessManager {}
+class MockProcessRunner extends Mock implements Function {
+  ProcessResult call(
+    String executable,
+    List<String> arguments, {
+    String workingDirectory,
+    Map<String, String> environment,
+    bool includeParentEnvironment,
+    bool runInShell,
+    Encoding stdoutEncoding,
+    Encoding stderrEncoding,
+  });
+}
 
-class MockProcess extends Mock implements Process {
-  MockProcess(this._stdout, [this._stderr, this._exitCode]);
-
-  String _stdout;
-  String _stderr;
-  int _exitCode;
+class MockProcessResult extends Mock implements ProcessResult {
+  MockProcessResult(this.stdout, [this.stderr = '', this.exitCode = 0]);
 
   @override
-  Stream<List<int>> get stdout =>
-      new Stream<List<int>>.fromIterable(<List<int>>[_stdout.codeUnits]);
+  dynamic stdout = '';
 
   @override
-  Stream<List<int>> get stderr =>
-      new Stream<List<int>>.fromIterable(<List<int>>[_stderr.codeUnits]);
+  dynamic stderr;
 
   @override
-  Future<int> get exitCode => new Future<int>.value(_exitCode);
+  int exitCode;
 }
