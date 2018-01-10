9536b807810fcd3517790048c438bc7c487b59ff
Ian Hickson <ian@hixie.ch>
2018-01-09 22:17:57 -0800

    Offset.direction (#4530)
    Since we've got a vector class, and it has a getter for the magnitude,
    why not also a getter for the angle.

---
+++ testing/dart/geometry_test.dart
@@ -0,0 +1,22 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'dart:ui';
+import 'dart:math' show pi;
+
+import 'package:test/test.dart';
+
+void main() {
+  test('Offset.direction', () {
+    expect(const Offset(0.0, 0.0).direction, 0.0);
+    expect(const Offset(0.0, 1.0).direction, pi / 2.0);
+    expect(const Offset(0.0, -1.0).direction, -pi / 2.0);
+    expect(const Offset(1.0, 0.0).direction, 0.0);
+    expect(const Offset(1.0, 1.0).direction, pi / 4.0);
+    expect(const Offset(1.0, -1.0).direction, -pi / 4.0);
+    expect(const Offset(-1.0, 0.0).direction, pi);
+    expect(const Offset(-1.0, 1.0).direction, pi * 3.0 / 4.0);
+    expect(const Offset(-1.0, -1.0).direction, -pi * 3.0 / 4.0);
+  });
+}
