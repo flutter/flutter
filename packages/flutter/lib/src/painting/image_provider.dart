80d6095d98f362cd1a117dd093336ae52c05785b
Ian Hickson <ian@hixie.ch>
2018-01-12 15:03:51 -0800

    Fix ShapeDecoration (#14009)
    And add a test that catches these problems.

--- packages/flutter/lib/src/painting/image_provider.dart
+++ packages/flutter/lib/src/painting/image_provider.dart
@@ -118,7 +118,7 @@ class ImageConfiguration {
     if (devicePixelRatio != null) {
       if (hasArguments)
         result.write(', ');
-      result.write('devicePixelRatio: $devicePixelRatio');
+      result.write('devicePixelRatio: ${devicePixelRatio.toStringAsFixed(1)}');
       hasArguments = true;
     }
     if (locale != null) {
