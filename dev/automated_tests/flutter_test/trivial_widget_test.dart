bd780cc2b34f6434a23594b4791f505763219863
Janice Collins <jcollins@google.com>
2018-01-18 12:52:46 -0800

    Fix analysis error

--- dev/automated_tests/flutter_test/trivial_widget_test.dart
+++ dev/automated_tests/flutter_test/trivial_widget_test.dart
@@ -2,10 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:flutter/foundation.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  testWidgets('A trivial widget test', (WidgetTester tester) async {
-  });
+  testWidgets('A trivial widget test', (WidgetTester tester) async {});
 }
