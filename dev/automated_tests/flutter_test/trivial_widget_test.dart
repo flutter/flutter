e6c191614bd251a45595e2578ce572828c36fcc2
Janice Collins <jcollins@google.com>
2018-01-18 12:28:59 -0800

    Ignore extraneous error messages outside of one skipped test

---
+++ dev/automated_tests/flutter_test/trivial_widget_test.dart
@@ -0,0 +1,11 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('A trivial widget test', (WidgetTester tester) async {
+  });
+}
