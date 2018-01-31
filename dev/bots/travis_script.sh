0a05fcc08774dd40d4abc67007d07d5e38b929e5
Xiao Yu <xster@google.com>
2018-01-30 19:22:20 -0800

    iOS works

---
+++ dev/bots/travis_script.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -ex
+
+export PATH="$PWD/bin:$PWD/bin/cache/dart-sdk/bin:$PATH"
+
+if [ "$SHARD" -ne "build_and_deploy_gallery" ]
+  dart ./dev/bots/test.dart
+else
+  if [ "$TRAVIS_OS_NAME" = "linux" ]
+    (cd examples/flutter_gallery; flutter build apk --release)
+    if [ "$TRAVIS_BRANCH" = "dev" ] && [ "$TRAVIS_PULL_REQUEST" = false ]
+      (cd examples/flutter_gallery/android; bundle exec fastlane deploy_play_store_alpha)
+    fi
+  elif [ "$TRAVIS_OS_NAME" = "osx" ]
+    (cd examples/flutter_gallery; flutter build ios --release --no-codesign)
+    if [ "$TRAVIS_BRANCH" = "dev" ] && [ "$TRAVIS_PULL_REQUEST" = false ]
+      (cd examples/flutter_gallery/ios; bundle exec fastlane build_and_deploy_testflight)
+    fi
+  fi
+fi
+
+if [ "$TRAVIS_OS_NAME" = "linux" ] && \
+   [ "$SHARD" = "docs" ]; then
+  # generate the API docs, upload them
+  ./dev/bots/docs.sh
+fi
