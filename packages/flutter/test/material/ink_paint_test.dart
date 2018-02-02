590b766b9bb95dfcae274aec394b0a77509279c0
Hans Muller <hansmuller@google.com>
2018-02-02 12:39:58 -0800

    Fix InkRipple cancel animation race

--- packages/flutter/test/material/ink_paint_test.dart
+++ packages/flutter/test/material/ink_paint_test.dart
@@ -248,4 +248,34 @@ void main() {
 
     await gesture.up();
   });
+
+  testWidgets('Cancel an InkRipple that was disposed when its animation ended', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      new Material(
+        child: new Center(
+          child: new Container(
+            width: 100.0,
+            height: 100.0,
+            child: new InkWell(
+              onTap: () { },
+              radius: 100.0,
+              splashFactory: InkRipple.splashFactory,
+            ),
+          ),
+        ),
+      ),
+    );
+
+    final Offset tapDownOffset = tester.getTopLeft(find.byType(InkWell));
+    final Offset inkWellCenter = tester.getCenter(find.byType(InkWell));
+    await tester.tapAt(tapDownOffset);
+    await tester.pump(); // start gesture
+    await tester.pump(const Duration(milliseconds: 375)); // _kFadeOutDuration, in_ripple.dart
+
+    final TestGesture gesture = await tester.startGesture(tapDownOffset);
+    await tester.pump(); // start gesture
+    await gesture.moveTo(const Offset(0.0, 0.0));
+    await gesture.up();
+    await tester.pumpAndSettle();
+  });
 }
