026a171e736c9fb00bb5603399d57d25a022acca
Vyacheslav Egorov <vegorov@google.com>
2018-02-12 18:12:05 +0100

    Update expectations in stack_trace_test: there is no async-patch in Dart 2 mode.
    Dart 2 applies core library patches during the build, so there is only a single
     dart:async library.

--- packages/flutter/test/foundation/stack_trace_test.dart
+++ packages/flutter/test/foundation/stack_trace_test.dart
@@ -18,7 +18,7 @@ void main() {
     expect(filtered[0], matches(r'^#0 +main\.<anonymous closure> \(.*stack_trace_test\.dart:[0-9]+:[0-9]+\)$'));
     expect(filtered[1], matches(r'^#1 +Declarer\.test\.<anonymous closure>.<anonymous closure>.<anonymous closure> \(package:test' + dividerRegExp + r'.+:[0-9]+:[0-9]+\)$'));
     expect(filtered[2], equals('<asynchronous suspension>'));
-    expect(filtered.last, matches(r'^\(elided [1-9][0-9]+ frames from package dart:async, package dart:async-patch, and package stack_trace\)$'));
+    expect(filtered.last, matches(r'^\(elided [1-9][0-9]+ frames from package dart:async(, package dart:async-patch,)? and package stack_trace\)$'));
   });
 
   test('FlutterError.defaultStackFilter (async test body)', () async {
@@ -26,6 +26,6 @@ void main() {
     expect(filtered.length, greaterThanOrEqualTo(3));
     expect(filtered[0], matches(r'^#0 +main\.<anonymous closure> \(.*stack_trace_test\.dart:[0-9]+:[0-9]+\)$'));
     expect(filtered[1], equals('<asynchronous suspension>'));
-    expect(filtered.last, matches(r'^\(elided [1-9][0-9]+ frames from package dart:async, package dart:async-patch, and package stack_trace\)$'));
+    expect(filtered.last, matches(r'^\(elided [1-9][0-9]+ frames from package dart:async(, package dart:async-patch,)? and package stack_trace\)$'));
   });
 }
