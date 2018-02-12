7b61378b7ebf472bae440ebeabb8978f79ed0ba3
Ryan Macnak <rmacnak@google.com>
2018-02-12 14:55:11 -0800

    [fuchsia] Decouple common packages from the core snapshot. (flutter/engine#4660)

--- engine/src/flutter/lib/snapshot/snapshot_fuchsia.dart
+++ engine/src/flutter/lib/snapshot/snapshot_fuchsia.dart
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// These libraries are required. Only the core snapshot knows how to resolve
-// them.
 import 'dart:async';
 import 'dart:collection';
 import 'dart:convert';
@@ -17,18 +15,3 @@ import 'dart:mozart.internal';
 import 'dart:typed_data';
 import 'dart:ui';
 import 'dart:zircon';
-
-// These libraries are optional. They are included in the core snapshot and
-// partially compiled to avoid repeating them in each application and improve
-// startup time.
-import 'package:lib.app.dart/app.dart';
-import 'package:flutter/animation.dart';
-import 'package:flutter/foundation.dart';
-import 'package:flutter/gestures.dart';
-import 'package:flutter/painting.dart';
-import 'package:flutter/physics.dart';
-import 'package:flutter/rendering.dart';
-import 'package:flutter/scheduler.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter/widgets.dart';
-import 'package:lib.fidl.dart/bindings.dart';
