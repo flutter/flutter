f38e9d4c3154b6da0f0bbee65d72181eaaff7ea7
xster <xiao@xster.net>
2018-02-15 14:00:32 -0800

    Small glow overscroll dartdoc addition (#14579)
    * Small glow overscroll dartdoc addition
    
    * Directly specify [MaterialApp]
    
    * review

--- packages/flutter/lib/src/widgets/scroll_physics.dart
+++ packages/flutter/lib/src/widgets/scroll_physics.dart
@@ -362,7 +362,8 @@ class BouncingScrollPhysics extends ScrollPhysics {
 ///    behavior.
 ///  * [GlowingOverscrollIndicator], which is used by [ScrollConfiguration] to
 ///    provide the glowing effect that is usually found with this clamping effect
-///    on Android.
+///    on Android. When using a [MaterialApp], the [GlowingOverscrollIndicator]'s
+///    glow color is specified to use [ThemeData.accentColor].
 class ClampingScrollPhysics extends ScrollPhysics {
   /// Creates scroll physics that prevent the scroll offset from exceeding the
   /// bounds of the content..
