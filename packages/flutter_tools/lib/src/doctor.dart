9a7ab6d8fa28bb183672b96dd132c6438b86a6bf
Victor Choueiri <victor@ctrlanddev.com>
2018-02-20 17:45:44 +0200

    Add flutter version to non-verbose doctor output

--- packages/flutter_tools/lib/src/doctor.dart
+++ packages/flutter_tools/lib/src/doctor.dart
@@ -250,7 +250,7 @@ class _FlutterValidator extends DoctorValidator {
     }
 
     return new ValidationResult(valid, messages,
-      statusInfo: 'on ${os.name}, locale ${platform.localeName}, channel ${version.channel}'
+      statusInfo: 'on ${os.name}, locale ${platform.localeName}, channel ${version.channel}, v${version.frameworkVersion}'
     );
   }
 }
