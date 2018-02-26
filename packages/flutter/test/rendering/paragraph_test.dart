aa04a056f39cc9db23878509dc61e0dae7cd7b05
Jason Simmons <jason-simmons@users.noreply.github.com>
2018-02-26 14:45:00 -0800

    Disable a text field test that fails on some Macs with libtxt (#14895)

--- packages/flutter/test/rendering/paragraph_test.dart
+++ packages/flutter/test/rendering/paragraph_test.dart
@@ -70,8 +70,8 @@ void main() {
     expect(boxes.any((ui.TextBox box) => box.left == 250 && box.top == 0), isTrue);
     expect(boxes.any((ui.TextBox box) => box.right == 100 && box.top == 10), isTrue);
   },
-  // Ahem-based tests don't yet quite work on Windows or the MacOS environment in Travis
-  skip: Platform.isWindows || (runningOnTravis && Platform.isMacOS));
+  // Ahem-based tests don't yet quite work on Windows or some MacOS environments
+  skip: Platform.isWindows || Platform.isMacOS);
 
   test('getWordBoundary control test', () {
     final RenderParagraph paragraph = new RenderParagraph(
