49385973cb88fb031753feb47a27048dde92abad
Xiao Yu <xster@google.com>
2018-02-28 14:59:35 -0800

    tests

--- packages/flutter/test/services/haptic_feedback_test.dart
+++ packages/flutter/test/services/haptic_feedback_test.dart
@@ -18,4 +18,32 @@ void main() {
     expect(log, hasLength(1));
     expect(log.single, isMethodCall('HapticFeedback.vibrate', arguments: null));
   });
+
+  test('Haptic feedback variation tests', () async {
+    final List<MethodCall> log = <MethodCall>[];
+
+    SystemChannels.platform.setMockMethodCallHandler((MethodCall methodCall) async {
+      log.add(methodCall);
+    });
+
+    const List<Function> hapticFunctions = const <Function>[
+      HapticFeedback.lightImpact,
+      HapticFeedback.mediumImpact,
+      HapticFeedback.heavyImpact,
+      HapticFeedback.selectionClick,
+    ];
+    final RegExp functionName = new RegExp(r".*\'(\w+)\'");
+
+    for (int i = 0; i < hapticFunctions.length; i++) {
+      await Function.apply(hapticFunctions[i], null);
+      expect(log, hasLength(i + 1));
+      expect(
+        log.last,
+        isMethodCall(
+          'HapticFeedback.vibrate',
+          arguments: 'HapticFeedbackType.${functionName.firstMatch(hapticFunctions[i].toString()).group(1)}',
+        ),
+      );
+    }
+  });
 }
