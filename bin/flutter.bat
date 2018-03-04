1bba44569b1da9b3122f3df67c1951fe8b269542
Todd Volkert <tvolkert@users.noreply.github.com>
2018-03-04 12:15:22 -0800

    Fix download Dart DK step to work for paths with apostrophes (#15137)
    Fixes https://github.com/flutter/flutter/issues/15136

--- bin/flutter.bat
+++ bin/flutter.bat
@@ -88,7 +88,10 @@ GOTO :after_subroutine
 
   :do_sdk_update_and_snapshot
     ECHO Checking Dart SDK version...
-    CALL PowerShell.exe -ExecutionPolicy Bypass -Command "& '%FLUTTER_ROOT%/bin/internal/update_dart_sdk.ps1'"
+    SET update_dart_bin=%FLUTTER_ROOT%/bin/internal/update_dart_sdk.ps1
+    REM Escape apostrophes from the executable path
+    SET "update_dart_bin=!update_dart_bin:'=''!"
+    CALL PowerShell.exe -ExecutionPolicy Bypass -Command "& '%update_dart_bin%'"
     IF "%ERRORLEVEL%" NEQ "0" (
       ECHO Error: Unable to update Dart SDK. Retrying...
       timeout /t 5 /nobreak
