285375cea10ef940c32243d9061cd35ce7f597eb
asiva <asiva@google.com>
2018-03-05 09:01:16 -0800

    Fix some of the generic types specified to avoid runtime errors in --preview-dart-2
      type 'YamlMap' is not a subtype of type 'Map<String, dynamic>' where
        YamlMap is from package:yaml/src/yaml_node.dart
        Map is from dart:core
        String is from dart:core
    
      package:flutter_devicelab/framework/manifest.dart 17:36       loadTaskManifest
      test/manifest_test.dart 14:33                                 main.<fn>.<fn>
      package:test/src/backend/declarer.dart 161:27                 Declarer.test.<fn>.<fn>.<fn>

--- dev/devicelab/lib/framework/manifest.dart
+++ dev/devicelab/lib/framework/manifest.dart
@@ -52,7 +52,7 @@ class ManifestTask {
   final String stage;
 
   /// Capabilities required of the build agent to be able to perform this task.
-  final List<String> requiredAgentCapabilities;
+  final List requiredAgentCapabilities;
 
   /// Whether this test is flaky.
   ///
@@ -75,14 +75,14 @@ class ManifestError extends Error {
 
 // There's no good YAML validator, at least not for Dart, so we validate
 // manually. It's not too much code and produces good error messages.
-Manifest _validateAndParseManifest(Map<String, dynamic> manifestYaml) {
+Manifest _validateAndParseManifest(Map manifestYaml) {
   _checkKeys(manifestYaml, 'manifest', const <String>['tasks']);
   return new Manifest._(_validateAndParseTasks(manifestYaml['tasks']));
 }
 
 List<ManifestTask> _validateAndParseTasks(dynamic tasksYaml) {
   _checkType(tasksYaml is Map, tasksYaml, 'Value of "tasks"', 'dictionary');
-  final List<String> sortedKeys = tasksYaml.keys.toList()..sort();
+  final List sortedKeys = tasksYaml.keys.toList()..sort();
   return sortedKeys.map((dynamic taskName) => _validateAndParseTask(taskName, tasksYaml[taskName])).toList();
 }
 
@@ -107,7 +107,7 @@ ManifestTask _validateAndParseTask(dynamic taskName, dynamic taskYaml) {
     _checkType(timeoutInMinutes is int, timeoutInMinutes, 'timeout_in_minutes', 'integer');
   }
 
-  final List<String> capabilities = _validateAndParseCapabilities(taskName, taskYaml['required_agent_capabilities']);
+  final List capabilities = _validateAndParseCapabilities(taskName, taskYaml['required_agent_capabilities']);
   return new ManifestTask._(
     name: taskName,
     description: taskYaml['description'],
@@ -118,7 +118,7 @@ ManifestTask _validateAndParseTask(dynamic taskName, dynamic taskYaml) {
   );
 }
 
-List<String> _validateAndParseCapabilities(String taskName, dynamic capabilitiesYaml) {
+List _validateAndParseCapabilities(String taskName, dynamic capabilitiesYaml) {
   _checkType(capabilitiesYaml is List, capabilitiesYaml, 'required_agent_capabilities', 'list');
   for (int i = 0; i < capabilitiesYaml.length; i++) {
     final dynamic capability = capabilitiesYaml[i];
@@ -141,7 +141,7 @@ void _checkIsNotBlank(dynamic value, String variableName, String ownerName) {
   }
 }
 
-void _checkKeys(Map<String, dynamic> map, String variableName, List<String> allowedKeys) {
+void _checkKeys(Map map, String variableName, List<String> allowedKeys) {
   for (String key in map.keys) {
     if (!allowedKeys.contains(key)) {
       throw new ManifestError(
