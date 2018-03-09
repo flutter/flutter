657a2cd2681f153c075ede8c9939a47eb5eb99f6
Jonah Williams <jonahwilliams@users.noreply.github.com>
2018-03-09 09:30:13 -0800

    fix spacing issue and add test for supportsHardwareRendering (#15305)
    * fix spacing issue and add test for supportsHardwareRendering
    
    * remove extra async tag
    
    * move spacing to prev line

--- packages/flutter_tools/lib/src/commands/run.dart
+++ packages/flutter_tools/lib/src/commands/run.dart
@@ -289,12 +289,12 @@ class RunCommand extends RunCommandBase {
           final bool enableSoftwareRendering = argResults['enable-software-rendering'] == true;
           if (enableSoftwareRendering) {
             printStatus(
-              'Using software rendering with device ${device.name}. You may get better performance'
+              'Using software rendering with device ${device.name}. You may get better performance '
               'with hardware mode by configuring hardware rendering for your device.'
             );
           } else {
             printStatus(
-              'Using hardware rendering with device ${device.name}. If you get graphics artifacts,'
+              'Using hardware rendering with device ${device.name}. If you get graphics artifacts, '
               'consider enabling software rendering with "--enable-software-rendering".'
             );
           }
