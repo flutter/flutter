8df63c0a2acdf040e30876157640475bd0fa78b4
Dave Shuckerow <djshuckerow@google.com>
2018-03-13 13:14:07 -0700

    Explain the animation progress in the fab animation

--- packages/flutter/lib/src/material/scaffold.dart
+++ packages/flutter/lib/src/material/scaffold.dart
@@ -132,13 +132,18 @@ abstract class FloatingActionButtonAnimator {
   /// This is the default [FloatingActionButton] motion animation.
   static const FloatingActionButtonAnimator scaling = const _ScalingFabMotionAnimator();
 
-  /// Gets the [FloatingActionButton]'s [Offset] based on a [progress].
+  /// Gets the [FloatingActionButton]'s position relative to the origin of the
+  /// [Scaffold] based on [progress].
   /// 
-  /// [begin] is the [Offset] provided by the previous [FloatingActionButtonPositioner].
+  /// [begin] is the [Offset] provided by the previous 
+  /// [FloatingActionButtonPositioner].
   /// 
-  /// [end] is the [Offset] provided by the new [FloatingActionButtonPositioner].
+  /// [end] is the [Offset] provided by the new 
+  /// [FloatingActionButtonPositioner].
   /// 
   /// [progress] is the current progress of the transition animation.
+  /// When [progress] is 0.0, the returned [Offset] should be equal to [begin].
+  /// when [progress] is 1.0, the returned [Offset] should be equal to [end].
   Offset getOffset({@required Offset begin, @required Offset end, @required double progress});
 
   /// Animates the scale of the [FloatingActionButton].
