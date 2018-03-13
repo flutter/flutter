d042b3e830f50bf180f9d0d6e3f152fe23a6107c
Michael Goderbauer <goderbauer@google.com>
2018-03-13 12:49:30 -0700

    Send TYPE_VIEW_SELECTED event for changes to SemanticsFlag.isSelected (#4780)

--- shell/platform/android/io/flutter/view/AccessibilityBridge.java
+++ shell/platform/android/io/flutter/view/AccessibilityBridge.java
@@ -538,6 +538,13 @@ class AccessibilityBridge extends AccessibilityNodeProvider implements BasicMess
                 // Simulate a click so TalkBack announces the change in checked state.
                 sendAccessibilityEvent(object.id, AccessibilityEvent.TYPE_VIEW_CLICKED);
             }
+            if (mA11yFocusedObject != null && mA11yFocusedObject.id == object.id
+                    && !object.hadFlag(Flag.IS_SELECTED) && object.hasFlag(Flag.IS_SELECTED)) {
+                AccessibilityEvent event =
+                        obtainAccessibilityEvent(object.id, AccessibilityEvent.TYPE_VIEW_SELECTED);
+                event.getText().add(object.label);
+                sendAccessibilityEvent(event);
+            }
             if (mInputFocusedObject != null && mInputFocusedObject.id == object.id
                     && object.hadFlag(Flag.IS_TEXT_FIELD)
                     && object.hasFlag(Flag.IS_TEXT_FIELD)) {
