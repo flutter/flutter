a2592786d30d4513a6cc52e698c3fdb9d82e02d7
Dave Shuckerow <djshuckerow@google.com>
2018-03-15 13:09:35 -0700

    2018 copyright

--- examples/flutter_gallery/lib/demo/material/fab_motion_demo.dart
+++ examples/flutter_gallery/lib/demo/material/fab_motion_demo.dart
@@ -1,4 +1,4 @@
-// Copyright 2015 The Chromium Authors. All rights reserved.
+// Copyright 2018 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
