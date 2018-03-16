77ac590fc0e98016997e2496a14de6f21f564993
asiva <asiva@google.com>
2018-03-15 17:39:32 -0700

    Fix lint in the experimental line.

--- packages/flutter_tools/test/commands/create_test.dart
+++ packages/flutter_tools/test/commands/create_test.dart
@@ -464,7 +464,7 @@ Future<Null> _analyzeProject(String workingDir, {String target}) async {
 }
 
 Future<Null> _runFlutterTest(Directory workingDir, {String target}) async {
-  await null;
+  await new Future.value(null);
   final String flutterToolsPath = fs.path.absolute(fs.path.join(
     'bin',
     'flutter_tools.dart',
