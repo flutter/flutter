4c29cf72255be467bc59e25e4c7f87a6c1ee077d
Unknown <scottstoll2017@gmail.com>
2018-03-21 15:26:56 -0400

    Added spacer.dart to flutter>lib>src>widgets
    Added a spacer widget that has been well received in Gitter. It is rediculously simple, fast and intuitive. I was encouraged to push it by several people.

---
+++ packages/flutter/lib/src/widgets/spacer.dart
@@ -0,0 +1,37 @@
+// All components of the Spacer Widget are Copyright 2016 The Chromium Authors.
+// All rights reserved. The Spacer Widget itself was created by Scott Stoll.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+
+/// Spacer:
+/// A widget that creates an adjustable, empty
+/// spacer that can be used to tune the alignment of
+/// the UI. It was created to allow developers to 
+/// accomplish this in ten characters, rather than
+/// four lines of code.
+/// 
+/// It uses an empty Container, wrapped
+/// with an Expanded. The Spacer takes an optional
+/// parameter that is used as the flex value of the
+/// Expanded. It defaults to 1.
+///
+/// Basic usage:
+/// Spacer(); (Expanded's flex = 1)
+/// Spacer(5); (Expanded's flex = 5)
+
+class Spacer extends StatelessWidget {
+
+  final int flexFactor;
+  Spacer([int this.flexFactor = 1]);
+
+  @override
+  Widget build(BuildContext context) {
+    return new Expanded(
+      child: new Container(),
+      flex: flexFactor,
+    );
+  }
+}
+
