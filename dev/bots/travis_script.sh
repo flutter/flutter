104858cba3300c2dcc30b1bab1150f49347f6cc1
xster <xiao@xster.net>
2018-03-23 10:45:59 -0700

    Do not push -pre versions to stores (#15845)

--- dev/bots/travis_script.sh
+++ dev/bots/travis_script.sh
@@ -5,27 +5,28 @@ set -ex
 export PATH="$PWD/bin:$PWD/bin/cache/dart-sdk/bin:$PATH"
 
 if [ "$SHARD" = "build_and_deploy_gallery" ]; then
-  echo "Building and deploying Flutter Gallery"
+  version=$(<version)
+  echo "Building and deploying Flutter Gallery $version"
   if [ "$TRAVIS_OS_NAME" = "linux" ]; then
     echo "Building Flutter Gallery for Android..."
     export ANDROID_HOME=`pwd`/android-sdk
     (cd examples/flutter_gallery; flutter build apk --release)
     echo "Android Flutter Gallery built"
-    if [[ "$TRAVIS_PULL_REQUEST" == "false" && "$TRAVIS_BRANCH" == "dev" ]]; then
+    if [[ "$TRAVIS_PULL_REQUEST" == "false" && "$TRAVIS_BRANCH" == "dev" && $version != *"pre"* ]]; then
       echo "Deploying to Play Store..."
       (cd examples/flutter_gallery/android; bundle install && bundle exec fastlane deploy_play_store)
     else
-      echo "Flutter Gallery is only deployed to the Play Store on merged dev branch commits"
+      echo "Flutter Gallery is only deployed to the Play Store on merged and tagged dev branch commits"
     fi
   elif [ "$TRAVIS_OS_NAME" = "osx" ]; then
     echo "Building Flutter Gallery for iOS..."
     (cd examples/flutter_gallery; flutter build ios --release --no-codesign)
     echo "iOS Flutter Gallery built"
-    if [[ "$TRAVIS_PULL_REQUEST" == "false" && "$TRAVIS_BRANCH" == "dev" ]]; then
+    if [[ "$TRAVIS_PULL_REQUEST" == "false" && "$TRAVIS_BRANCH" == "dev" && $version != *"pre"* ]]; then
       echo "Re-building with distribution profile and deploying to TestFlight..."
       (cd examples/flutter_gallery/ios; bundle install && bundle exec fastlane build_and_deploy_testflight)
     else
-      echo "Flutter Gallery is only deployed to the TestFlight on merged dev branch commits"
+      echo "Flutter Gallery is only deployed to the TestFlight on merged and tagged dev branch commits"
     fi
   fi
 elif [ "$SHARD" = "docs" ]; then
