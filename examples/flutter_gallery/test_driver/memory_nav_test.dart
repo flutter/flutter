a0099a9016c34914e55f4f133f0281799775f230
Hans Muller <hansmuller@chromium.org>
2018-03-27 13:39:30 -0700

    Make gallery tests more robust (#15957)

--- examples/flutter_gallery/test_driver/memory_nav_test.dart
+++ examples/flutter_gallery/test_driver/memory_nav_test.dart
@@ -23,18 +23,14 @@ void main() {
       final SerializableFinder menuItem = find.text('Text fields');
       driver.waitFor(menuItem).then<Null>((Null value) async {
         scroll = false;
-        await new Future<Null>.delayed(kWaitBetweenActions);
         for (int i = 0; i < 15; i++) {
           await driver.tap(menuItem);
-          await new Future<Null>.delayed(kWaitBetweenActions);
           await driver.tap(find.byTooltip('Back'));
-          await new Future<Null>.delayed(kWaitBetweenActions);
         }
         completer.complete();
       });
       while (scroll) {
         await driver.scroll(find.text('Flutter Gallery'), 0.0, -500.0, const Duration(milliseconds: 80));
-        await new Future<Null>.delayed(kWaitBetweenActions);
       }
       await completer.future;
     }, timeout: const Timeout(const Duration(minutes: 1)));
