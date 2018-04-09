eea6197a24256fe588e18127f56c733bc42b6a8d
Alexander Aprelev <aam@google.com>
2018-04-09 14:34:46 -0700

    Perform exit from server, not starter as awaiting from starter causes hanging in incremental compilation scenario. (#4955)

--- frontend_server/bin/starter.dart
+++ frontend_server/bin/starter.dart
@@ -1,10 +1,7 @@
 library frontend_server;
 
-import 'dart:async';
-import 'dart:io';
-
 import 'package:frontend_server/server.dart';
 
-Future<Null> main(List<String> args) async {
-  exit(await starter(args));
+void main(List<String> args){
+  starter(args);
 }
