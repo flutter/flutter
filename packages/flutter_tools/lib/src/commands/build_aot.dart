a7218d8cb612b728d0b7511bf25df6c931d008da
Konstantin Shcheglov <scheglov@google.com>
2018-04-09 21:15:04 -0700

    Add a new Device backed by flutter_tester.

--- packages/flutter_tools/lib/src/commands/build_aot.dart
+++ packages/flutter_tools/lib/src/commands/build_aot.dart
@@ -247,6 +247,7 @@ Future<String> _buildAotSnapshot(
     case TargetPlatform.linux_x64:
     case TargetPlatform.windows_x64:
     case TargetPlatform.fuchsia:
+    case TargetPlatform.tester:
       assert(false);
   }
 
@@ -338,6 +339,7 @@ Future<String> _buildAotSnapshot(
     case TargetPlatform.linux_x64:
     case TargetPlatform.windows_x64:
     case TargetPlatform.fuchsia:
+    case TargetPlatform.tester:
       assert(false);
   }
 
