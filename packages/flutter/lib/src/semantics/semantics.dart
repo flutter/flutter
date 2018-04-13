29d5059b5a729295ab43db751164c1aeddb30bdc
Jonah Williams <jonahwilliams@google.com>
2018-04-12 17:07:18 -0700

    Update semantics.dart

--- packages/flutter/lib/src/semantics/semantics.dart
+++ packages/flutter/lib/src/semantics/semantics.dart
@@ -70,7 +70,7 @@ class SemanticsTag {
 
 /// Summary information about a [SemanticsNode] object.
 ///
-/// A semantics node might [SemanticsNode.fAllDescendantsIntoThisNode],
+/// A semantics node might [SemanticsNode.mergeAllDescendantsIntoThisNode],
 /// which means the individual fields on the semantics node don't fully describe
 /// the semantics at that node. This data structure contains the full semantics
 /// for the node.
