3a1094c321a772acf787a40ba9ade776d93a005c
Hans Muller <hansmuller@google.com>
2018-04-13 08:29:23 -0700

    Remove the rest of the uses of Number.tryParse()

--- packages/fuchsia_remote_debug_protocol/lib/src/fuchsia_remote_connection.dart
+++ packages/fuchsia_remote_debug_protocol/lib/src/fuchsia_remote_connection.dart
@@ -197,7 +197,8 @@ class FuchsiaRemoteConnection {
       final int lastSpace = trimmed.lastIndexOf(' ');
       final String lastWord = trimmed.substring(lastSpace + 1);
       if ((lastWord != '.') && (lastWord != '..')) {
-        final int value = int.tryParse(lastWord);
+        // ignore: deprecated_member_use
+        final int value = int.parse(lastWord, onError: (_) => null);
         if (value != null) {
           ports.add(value);
         }
