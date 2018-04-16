044f186cbad455ae068580cfd22c30f387da4f55
Michael Goderbauer <goderbauer@google.com>
2018-04-16 13:01:31 -0700

    Update AppBar docs to analyze sample code

--- packages/flutter/lib/src/material/app_bar.dart
+++ packages/flutter/lib/src/material/app_bar.dart
@@ -188,7 +188,7 @@ class AppBar extends StatefulWidget implements PreferredSizeWidget {
   /// For less common operations, consider using a [PopupMenuButton] as the
   /// last action.
   ///
-  /// For example:
+  /// ## Sample code
   ///
   /// ```dart
   /// return new Scaffold(
@@ -767,11 +767,11 @@ class SliverAppBar extends StatefulWidget {
   /// For less common operations, consider using a [PopupMenuButton] as the
   /// last action.
   ///
-  /// For example:
+  /// ## Sample code
   ///
   /// ```dart
   /// return new Scaffold(
-  ///   body: new CustomView(
+  ///   body: new CustomScrollView(
   ///     primary: true,
   ///     slivers: <Widget>[
   ///       new SliverAppBar(
