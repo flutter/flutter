b1ecf1ea9a10d3c8429c088274de335f05d4e25b
Jonah Williams <jonahwilliams@google.com>
2018-04-16 14:09:20 -0700

    Add localizations for navigation menu, popup menu, dialog, and alert (#16633)
    * add localizations for navigation menu, popup menu, dialog, and alert

--- packages/flutter/test/material/localizations_test.dart
+++ packages/flutter/test/material/localizations_test.dart
@@ -30,6 +30,10 @@ void main() {
     expect(localizations.selectAllButtonLabel, isNotNull);
     expect(localizations.viewLicensesButtonLabel, isNotNull);
     expect(localizations.modalBarrierDismissLabel, isNotNull);
+    expect(localizations.drawerLabel, isNotNull);
+    expect(localizations.popupMenuLabel, isNotNull);
+    expect(localizations.dialogLabel, isNotNull);
+    expect(localizations.alertDialogLabel, isNotNull);
 
     expect(localizations.aboutListTileTitle('FOO'), isNotNull);
     expect(localizations.aboutListTileTitle('FOO'), contains('FOO'));
