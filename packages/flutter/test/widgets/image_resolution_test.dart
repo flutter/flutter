dd32d1b1799640ed90a14ba18300027a3e5ded38
Todd Volkert <tvolkert@users.noreply.github.com>
2018-04-17 02:52:00 -0700

    Roll engine to 1bc0e1bb9a8db882a21086096cc2b1e0854312b7 (#16643)
    * Roll engine to d6d4eec24c38bb06a9b1aef4e70c321d447ba762
    
    Contains the following engine commits:
    
    flutter/engine@1bc0e1b On windows, refer to Dart snapshot directly executable.
    flutter/engine@cf6ca32 Revert "Roll Dart to fe606f890b0a311da802c78b0af414a3c2087a79"
    flutter/engine@4b45a53 Only make ERROR and FATAL log levels visible by default.
    flutter/engine@98f6c2d Fix shell launcher test (dart2 compliant and stop polling)
    flutter/engine@75851f0 Roll Dart to fe606f890b0a311da802c78b0af414a3c2087a79
    flutter/engine@9495a52 On iOS, try to use ES3, then fall back to ES2.
    flutter/engine@d6d4eec Fix broken Linux build
    flutter/engine@d15dc76 Fix broken Chromebot build
    flutter/engine@c17a629 Breadcrumbs to the wiki
    flutter/engine@4eaf2c2 Return raw (unencoded) bytes in Image.toByteData()
    flutter/engine@d812a61 Roll src/third_party/skia/ e669bdfad..276886160 (9 commits)
    flutter/engine@62aeab7 Call SkSurface::flush instead of getting a backend handle in vulkan_swapchain.
    flutter/engine@f4ebb18 Roll src/third_party/skia/ 3462eb034..e669bdfad (54 commits)
    flutter/engine@0b7d6be Re-land "do not pause rendering when android view loses focus"
    flutter/engine@91dee59 If the rasterizer has a valid surface context, use that to create a snapshotting render target.
    flutter/engine@570231b Specify the packages file path when running engine dart tests.
    flutter/engine@0b9cef5 Minor update to README
    flutter/engine@c386418 Promote line height in text style created from paragraph style
    flutter/engine@4e0fbb6 If the test specifies a .dill file, dont make the engine interpret is as source.
    flutter/engine@58e84c8 Re-land "Support multiple shells in a single process.
    flutter/engine@37e5df0 Remove only use of SkPaint::kGenA8FromLCD
    flutter/engine@b7358b3 libtxt: cache font families that are remapped to the default font family
    flutter/engine@a530035 libtxt: fix leaks in Skia object reference counting
    flutter/engine@0c74fc9 [fuchsia] Fix use-after-free
    flutter/engine@b6d2dde [fuchsia] Fix build
    flutter/engine@1dd1264 [async] Adapt to libasync API changes.
    
    * Run test platform finalizers in reverse order to ensure that shell is shutdown before trying to delete any temporary files or directories. This fixes "Access Denied" error on Windows (see #16657).

--- packages/flutter/test/widgets/image_resolution_test.dart
+++ packages/flutter/test/widgets/image_resolution_test.dart
@@ -4,7 +4,7 @@
 
 import 'dart:async';
 import 'dart:typed_data';
-import 'dart:ui' as ui show EncodingFormat, Image;
+import 'dart:ui' as ui show Image;
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/rendering.dart';
@@ -26,7 +26,7 @@ class TestImage implements ui.Image {
   void dispose() { }
 
   @override
-  Future<ByteData> toByteData({ui.EncodingFormat format}) async {
+  Future<ByteData> toByteData() async {
     throw new UnsupportedError('Cannot encode test image');
   }
 }
