243bb77a5d0b7c9ac021b4b6dab2b6dfe840ef62
Danny Tuppeny <dantup@google.com>
2018-04-18 14:50:16 +0100

    Add support for launching emulators

--- packages/flutter_tools/lib/src/commands/emulators.dart
+++ packages/flutter_tools/lib/src/commands/emulators.dart
@@ -12,6 +12,11 @@ import '../globals.dart';
 import '../runner/flutter_command.dart';
 
 class EmulatorsCommand extends FlutterCommand {
+  EmulatorsCommand() {
+    argParser.addOption('start',
+        help: 'The full or partial ID of the emulator to start.');
+  }
+
   @override
   final String name = 'emulators';
 
@@ -20,23 +25,46 @@ class EmulatorsCommand extends FlutterCommand {
 
   @override
   Future<Null> runCommand() async {
-    if (!doctor.canListAnything) {
+    if (doctor.workflows.every((Workflow w) => !w.canListEmulators)) {
       throwToolExit(
-        "Unable to locate emulators; please run 'flutter doctor' for "
-        'information about installing additional components.',
-        exitCode: 1);
+          "Unable to query emulators; please run 'flutter doctor' for "
+          'information about installing additional components.',
+          exitCode: 1);
     }
 
-    final List<Emulator> emulators = await emulatorManager.getAllAvailableEmulators().toList();
+    if (argResults.wasParsed('start')) {
+      await _startEmulator(argResults['start']);
+    } else {
+      await _listEmulators();
+    }
+  }
+
+  Future<Null> _startEmulator(String id) async {
+    final List<Emulator> emulators =
+        await emulatorManager.getEmulatorsById(id).toList();
 
     if (emulators.isEmpty) {
-      printStatus(
-        'No emulators available.\n\n'
-        // TODO: Change these when we support creation
-        // 'You may need to create images using "flutter emulators --create"\n'
-        'You may need to create one using Android Studio\n'
-        'or visit https://flutter.io/setup/ for troubleshooting tips.');
-      final List<String> diagnostics = await emulatorManager.getEmulatorDiagnostics();
+      printStatus("No emulator found that matches the ID '$id'.");
+    } else if (emulators.length > 1) {
+      printStatus("More than one emulator matches the ID '$id':\n");
+      Emulator.printEmulators(emulators);
+    } else {
+      emulators.first.launch();
+    }
+  }
+
+  Future<Null> _listEmulators() async {
+    final List<Emulator> emulators =
+        await emulatorManager.getAllAvailableEmulators().toList();
+
+    if (emulators.isEmpty) {
+      printStatus('No emulators available.\n\n'
+          // TODO: Change these when we support creation
+          // 'You may need to create images using "flutter emulators --create"\n'
+          'You may need to create one using Android Studio\n'
+          'or visit https://flutter.io/setup/ for troubleshooting tips.');
+      final List<String> diagnostics =
+          await emulatorManager.getEmulatorDiagnostics();
       if (diagnostics.isNotEmpty) {
         printStatus('');
         for (String diagnostic in diagnostics) {
@@ -44,7 +72,8 @@ class EmulatorsCommand extends FlutterCommand {
         }
       }
     } else {
-      printStatus('${emulators.length} available ${pluralize('emulators', emulators.length)}:\n');
+      printStatus(
+          '${emulators.length} available ${pluralize('emulators', emulators.length)}:\n');
       await Emulator.printEmulators(emulators);
     }
   }
