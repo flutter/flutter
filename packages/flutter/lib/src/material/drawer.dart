b74ed16b3ceb38e3d419634574acdd7287546680
jonahwilliams <campfish91@gmail.com>
2018-04-19 13:16:00 -0700

    remove recursive import of material

--- packages/flutter/lib/src/material/drawer.dart
+++ packages/flutter/lib/src/material/drawer.dart
@@ -3,7 +3,7 @@
 // found in the LICENSE file.
 
 import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter/src/material/material_localizations.dart';
 import 'package:flutter/widgets.dart';
 
 import 'colors.dart';
