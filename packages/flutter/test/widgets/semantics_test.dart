05f200d8ae898d6ec10f5077714bb51eb631047b
Jonah Williams <jonahwilliams@google.com>
2018-04-19 12:35:24 -0700

    Roll engine to 3405e23a32af4ea3655e5d1a78955066b2573331 (#16755)
    * update engine version, ignore scopesRoute and namesRoute flags until framework support is added

--- packages/flutter/test/widgets/semantics_test.dart
+++ packages/flutter/test/widgets/semantics_test.dart
@@ -473,12 +473,15 @@ void main() {
           obscured: true,
         )
     );
-
+    // TODO(jonahwilliams): remove when adding engine support for edge semantics
+    final List<SemanticsFlag> flags = SemanticsFlag.values.values
+     .where((SemanticsFlag flag) => flag != SemanticsFlag.scopesRoute && flag != SemanticsFlag.namesRoute)
+     .toList();
     final TestSemantics expectedSemantics = new TestSemantics.root(
       children: <TestSemantics>[
         new TestSemantics.rootChild(
           rect: TestSemantics.fullScreen,
-          flags: SemanticsFlag.values.values.toList(),
+          flags: flags,
         ),
       ],
     );
