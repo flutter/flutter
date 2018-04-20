f5b42177021312c4afae666b3b1912f3909e5c29
Yegor <yjbanov@google.com>
2018-04-19 18:29:49 -0700

    retry VMService connection; expect missing PRODUCT_BUNDLE_IDENTIFIER (#16770)
    Fixes #13655

--- packages/flutter_tools/test/vmservice_test.dart
+++ packages/flutter_tools/test/vmservice_test.dart
@@ -2,9 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:web_socket_channel/web_socket_channel.dart';
 import 'package:test/test.dart';
 
+import 'package:flutter_tools/src/base/io.dart';
 import 'package:flutter_tools/src/base/port_scanner.dart';
 import 'package:flutter_tools/src/vmservice.dart';
 
@@ -14,7 +14,7 @@ void main() {
       final int port = await const HostPortScanner().findAvailablePort();
       expect(
         VMService.connect(Uri.parse('http://localhost:$port')),
-        throwsA(const isInstanceOf<WebSocketChannelException>()),
+        throwsA(const isInstanceOf<SocketException>()),
       );
     });
   });
