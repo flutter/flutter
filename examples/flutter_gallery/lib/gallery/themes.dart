8d82592fdd9e7f9945d471bc7da477381ebcfc95
Hans Muller <hansmuller@google.com>
2018-04-20 10:17:47 -0700

    checkpoint

--- examples/flutter_gallery/lib/gallery/themes.dart
+++ examples/flutter_gallery/lib/gallery/themes.dart
@@ -5,47 +5,61 @@
 import 'package:flutter/material.dart';
 
 class GalleryTheme {
-  const GalleryTheme({ this.name, this.data });
+  const GalleryTheme._(this.name, this.data);
 
   final String name;
   final ThemeData data;
-
-  @override
-  bool operator ==(dynamic other) {
-    if (runtimeType != other.runtimeType)
-      return false;
-    final GalleryTheme typedOther = other;
-    return name == typedOther.name && data == typedOther.data;
-  }
-
-  @override
-  int get hashCode => hashValues(name, data);
-
-  @override
-  String toString() {
-    return '$runtimeType($name)';
-  }
 }
 
-final GalleryTheme kDarkGalleryTheme = new GalleryTheme(
-  name: 'Dark theme',
-  data: new ThemeData.dark(),
-);
+final GalleryTheme kDarkGalleryTheme = new GalleryTheme._('Dark', _buildDarkTheme());
+final GalleryTheme kLightGalleryTheme = new GalleryTheme._('Light', _buildLightTheme());
 
-final GalleryTheme kLightGalleryTheme = new GalleryTheme(
-  name: 'Light theme',
-  data: new ThemeData(
-    primarySwatch: Colors.blue,
-    scaffoldBackgroundColor: Colors.white,
-    backgroundColor: Colors.white,
-    //dividerColor: const Color(0xFFAAF7FE),
-    buttonColor: Colors.blue[500],
+TextTheme _buildTextTheme(TextTheme base) {
+  return base.copyWith(
+    title: base.title.copyWith(
+      fontFamily: 'GoogleSans',
+    ),
+  );
+}
+
+ThemeData _buildDarkTheme() {
+  const primaryColor = const Color(0xFF0175c2);
+  final ThemeData base = new ThemeData.dark();
+  return base.copyWith(
+    primaryColor: primaryColor,
+    buttonColor: primaryColor,
+    indicatorColor: Colors.white,
+    accentColor: const Color(0xFF13B9FD),
+    canvasColor: const Color(0xFF202124),
+    scaffoldBackgroundColor: const Color(0xFF202124),
+    backgroundColor: const Color(0xFF202124),
     buttonTheme: const ButtonThemeData(
       textTheme: ButtonTextTheme.primary,
     ),
-    errorColor: const Color(0xFFFF1744),
-    highlightColor: Colors.transparent,
+    textTheme: _buildTextTheme(base.textTheme),
+    primaryTextTheme: _buildTextTheme(base.primaryTextTheme),
+    accentTextTheme: _buildTextTheme(base.accentTextTheme),
+  );
+}
+
+ThemeData _buildLightTheme() {
+  const Color primaryColor = const Color(0xFF0175c2);
+  final ThemeData base = new ThemeData.light();
+  return base.copyWith(
+    primaryColor: primaryColor,
+    buttonColor: primaryColor,
+    indicatorColor: Colors.white,
     splashColor: Colors.white24,
     splashFactory: InkRipple.splashFactory,
-  ),
-);
+    accentColor: const Color(0xFF13B9FD),
+    canvasColor: Colors.white,
+    scaffoldBackgroundColor: Colors.white,
+    backgroundColor: Colors.white,
+    buttonTheme: const ButtonThemeData(
+      textTheme: ButtonTextTheme.primary,
+    ),
+    textTheme: _buildTextTheme(base.textTheme),
+    primaryTextTheme: _buildTextTheme(base.primaryTextTheme),
+    accentTextTheme: _buildTextTheme(base.accentTextTheme),
+  );
+}
