286cd359513b853a6bf13a93d1fda25420e90f46
血心 <taiyangniaoming@foxmail.com>
2018-04-24 04:53:00 +0800

    Fix bug for calling isEmpty on null object. (#16782)

--- packages/flutter_localizations/lib/src/material_localizations.dart
+++ packages/flutter_localizations/lib/src/material_localizations.dart
@@ -133,8 +133,7 @@ class GlobalMaterialLocalizations implements MaterialLocalizations {
   intl.DateFormat _yearMonthFormat;
 
   static String _computeLocaleName(Locale locale) {
-    final String localeName = locale.countryCode.isEmpty ? locale.languageCode : locale.toString();
-    return intl.Intl.canonicalizedLocale(localeName);
+    return intl.Intl.canonicalizedLocale(locale.toString());
   }
 
   @override
