8b99d1d83ccc7027692e9f316d4ae9085ef6e355
Todd Volkert <tvolkert@users.noreply.github.com>
2018-04-28 13:54:08 -0700

    Add `expectLater` to flutter_test (#17063)
    * Wrap `expectLater()` in flutter_tester so that callers can
      wait for the matcher to complete in their tests
    
    https://github.com/flutter/flutter/issues/16859

--- packages/flutter_test/test/widget_tester_test.dart
+++ packages/flutter_test/test/widget_tester_test.dart
@@ -2,9 +2,13 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'dart:async';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:test/test.dart' as test_package;
+import 'package:test/src/frontend/async_matcher.dart' show AsyncMatcher;
 
 const List<Widget> fooBarTexts = const <Text>[
   const Text('foo', textDirection: TextDirection.ltr),
@@ -12,6 +16,36 @@ const List<Widget> fooBarTexts = const <Text>[
 ];
 
 void main() {
+  group('expectLater', () {
+    testWidgets('completes when matcher completes', (WidgetTester tester) async {
+      final Completer<void> completer = new Completer<void>();
+      final Future<Null> future = expectLater(null, new FakeMatcher(completer));
+      String value;
+      future.then((void _) {
+        value = '123';
+      });
+      test_package.expect(value, isNull);
+      completer.complete();
+      test_package.expect(value, isNull);
+      await future;
+      await tester.pump();
+      test_package.expect(value, '123');
+    });
+
+    testWidgets('Prevents re-entrant calls', (WidgetTester tester) async {
+      final Completer<void> completer = new Completer<void>();
+      final Future<Null> future = expectLater(null, new FakeMatcher(completer));
+      try {
+        expectLater(null, new FakeMatcher(completer));
+        fail('FlutterError expected but not thrown');
+      } on FlutterError catch (error) {
+        completer.complete();
+        await future;
+        expect(error.message, contains('Guarded function conflict'));
+      }
+    });
+  });
+
   group('findsOneWidget', () {
     testWidgets('finds exactly one widget', (WidgetTester tester) async {
       await tester.pumpWidget(const Text('foo', textDirection: TextDirection.ltr));
@@ -417,4 +451,20 @@ void main() {
     controller.forward();
     expect(await tester.pumpAndSettle(const Duration(milliseconds: 300)), 5); // 0, 300, 600, 900, 1200ms
   });
+}
+
+class FakeMatcher extends AsyncMatcher {
+  FakeMatcher(this.completer);
+
+  final Completer<void> completer;
+
+  @override
+  Future<String> matchAsync(dynamic object) {
+    return completer.future.then<String>((void _) {
+      return object?.toString();
+    });
+  }
+
+  @override
+  Description describe(Description description) => description.add('--fake--');
 }
