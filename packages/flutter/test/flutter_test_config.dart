05ca5f0924acf53881705596c3e8bda09d864b5b
Todd Volkert <tvolkert@google.com>
2018-05-02 08:51:27 -0700

    Simplify test configs by sharing common main

--- packages/flutter/test/flutter_test_config.dart
+++ packages/flutter/test/flutter_test_config.dart
@@ -2,12 +2,4 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'dart:async';
-
-import 'package:flutter_goldens/flutter_goldens.dart';
-import 'package:flutter_test/flutter_test.dart';
-
-Future<void> main(FutureOr<void> testMain()) async {
-  goldenFileComparator = await FlutterGoldenFileComparator.fromDefaultComparator();
-  await testMain();
-}
+export 'package:flutter_goldens/flutter_goldens.dart' show main;
