f541c25fe26791bf4a9314fbb4be7ef25cdcb40a
Todd Volkert <tvolkert@users.noreply.github.com>
2018-05-03 07:39:41 -0700

    Add Flutter-repo-specific golden comparator (#17205)
    In order to avoid checking binaries into flutter/flutter,
    this comparator can be used to retrieve golden files from
    a sibling flutter/goldens repository.
    
    https://github.com/flutter/flutter/issues/16859

--- packages/flutter_tools/lib/src/test/flutter_platform.dart
+++ packages/flutter_tools/lib/src/test/flutter_platform.dart
@@ -685,9 +685,7 @@ void main() {
     );
     if (testConfigFile != null) {
       buffer.write('''
-    return () {
-      test_config.main(test.main);
-    };
+    return () => test_config.main(test.main);
 ''');
     } else {
       buffer.write('''
