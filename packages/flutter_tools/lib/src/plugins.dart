693b19e59f001aef663b3731e79975b47efbdd20
Mikkel Nygaard Ravn <mravn@google.com>
2018-05-04 20:49:39 +0200

    Revert "Recommend upgrading to Cocoapods 1.5.0 (#17210)"
    This reverts commit c64ace84d56b42561d8846785b026887b832d637.

--- packages/flutter_tools/lib/src/plugins.dart
+++ packages/flutter_tools/lib/src/plugins.dart
@@ -238,7 +238,7 @@ void injectPlugins({String directory}) {
     _writeAndroidPluginRegistrant(directory, plugins);
   if (fs.isDirectorySync(fs.path.join(directory, 'ios'))) {
     _writeIOSPluginRegistrant(directory, plugins);
-    final CocoaPods cocoaPods = new CocoaPods();
+    const CocoaPods cocoaPods = const CocoaPods();
     if (plugins.isNotEmpty)
       cocoaPods.setupPodfile(directory);
     if (changed)
