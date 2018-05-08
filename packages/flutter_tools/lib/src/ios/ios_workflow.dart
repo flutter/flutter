61c71718720a581f8a246eb31f28b45cf657853a
Mikkel Nygaard Ravn <mravn@google.com>
2018-05-08 08:02:39 +0200

    Recommend using Cocoapods 1.5.0 (#17308)

--- packages/flutter_tools/lib/src/ios/ios_workflow.dart
+++ packages/flutter_tools/lib/src/ios/ios_workflow.dart
@@ -171,7 +171,9 @@ class IOSWorkflow extends DoctorValidator implements Workflow {
         }
       }
 
-      if (await cocoaPods.isCocoaPodsInstalledAndMeetsVersionCheck) {
+      final CocoaPodsStatus cocoaPodsStatus = await cocoaPods.evaluateCocoaPodsInstallation;
+
+      if (cocoaPodsStatus == CocoaPodsStatus.recommended) {
         if (await cocoaPods.isCocoaPodsInitialized) {
           messages.add(new ValidationMessage('CocoaPods version ${await cocoaPods.cocoaPodsVersionText}'));
         } else {
@@ -186,7 +188,7 @@ class IOSWorkflow extends DoctorValidator implements Workflow {
         }
       } else {
         brewStatus = ValidationType.partial;
-        if (!await cocoaPods.hasCocoaPods) {
+        if (cocoaPodsStatus == CocoaPodsStatus.notInstalled) {
           messages.add(new ValidationMessage.error(
             'CocoaPods not installed.\n'
             '$noCocoaPodsConsequence\n'
@@ -194,8 +196,8 @@ class IOSWorkflow extends DoctorValidator implements Workflow {
             '$cocoaPodsInstallInstructions'
           ));
         } else {
-          messages.add(new ValidationMessage.error(
-            'CocoaPods out of date ($cocoaPods.cocoaPodsMinimumVersion is required).\n'
+          messages.add(new ValidationMessage.hint(
+            'CocoaPods out of date (${cocoaPods.cocoaPodsRecommendedVersion} is recommended).\n'
             '$noCocoaPodsConsequence\n'
             'To upgrade:\n'
             '$cocoaPodsUpgradeInstructions'
