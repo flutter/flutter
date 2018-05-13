45b9a7dea1a2f1d92115e7d52311df1d9f0862b2
nuv631 <Samuel.Edwards@capitalone.com>
2018-05-12 21:28:54 -0400

    Updated to latest stable Gradle Version 4.7

--- dev/bots/travis_setup.sh
+++ dev/bots/travis_setup.sh
@@ -40,7 +40,7 @@ if [ -n "$TRAVIS" ]; then
     echo y | sdkmanager "extras;google;m2repository" >/dev/null
     echo y | sdkmanager "patcher;v4" >/dev/null
     sdkmanager --list
-    wget http://services.gradle.org/distributions/gradle-4.1-bin.zip
+    wget https://services.gradle.org/distributions/gradle-4.7-bin.zip
     unzip -qq gradle-4.1-bin.zip
     export GRADLE_HOME=$PWD/gradle-4.1
     export PATH=$GRADLE_HOME/bin:$PATH
