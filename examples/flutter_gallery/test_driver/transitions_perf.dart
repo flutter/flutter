c666549d91c6d3a01579c779dab05523a50b0cad
Chris Bracken <chris@bracken.jp>
2018-05-15 11:23:31 -0700

    Adjust scroll increment in transition perf test (#17593)
    When attempting to locate an element in the Flutter Gallery transition
    performance driver test, attempt to scroll downward in increments of 25%
    of the screen rather than by an absolute pixel amount.
    
    This helps get the test passing on devices with relatively small screen
    heights.

--- examples/flutter_gallery/test_driver/transitions_perf.dart
+++ examples/flutter_gallery/test_driver/transitions_perf.dart
@@ -4,16 +4,30 @@
 
 import 'dart:async';
 import 'dart:convert' show JsonEncoder;
+import 'dart:ui' show window, Size;
 
 import 'package:flutter_driver/driver_extension.dart';
 import 'package:flutter_gallery/gallery/demos.dart';
 import 'package:flutter_gallery/main.dart' as app;
 
+
 Future<String> _handleMessages(String message) async {
-  assert(message == 'demoNames');
-  return const JsonEncoder.withIndent('  ').convert(
-    kAllGalleryDemos.map((GalleryDemo demo) => '${demo.title}@${demo.category.name}').toList(),
-  );
+  assert(message == 'demoNames' || message == 'mediaSize');
+  const JsonEncoder jsonEncoder = const JsonEncoder.withIndent('  ');
+  switch(message) {
+    case 'demoNames':
+      return jsonEncoder.convert(
+        kAllGalleryDemos.map((GalleryDemo demo) => '${demo.title}@${demo.category.name}').toList(),
+      );
+    case 'mediaSize':
+      final Size size = window.physicalSize / window.devicePixelRatio;
+      return jsonEncoder.convert(<String, double>{
+        'width': size.width,
+        'height': size.height,
+      });
+  }
+  assert(false, 'Not reachable');
+  return '';
 }
 
 void main() {
