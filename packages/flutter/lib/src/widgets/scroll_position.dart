1d55ced045d6273ba191ea7252f4c462b32e644d
Xiao Yu <xster@google.com>
2018-05-17 16:32:21 -0700

    merge in a super class dartdoc

--- packages/flutter/lib/src/widgets/scroll_position.dart
+++ packages/flutter/lib/src/widgets/scroll_position.dart
@@ -264,11 +264,18 @@ abstract class ScrollPosition extends ViewportOffset with ScrollMetrics {
     _pixels = value;
   }
 
-  /// Offset correction callable by render objects that holds a [ViewportOffset]
-  /// during layout to adjust offset and recompute the layout again.
+  /// Apply a layout-time correction to the scroll offset.
+  ///
+  /// This method should change the [pixels] value by `correction`, but without
+  /// calling [notifyListeners]. It is called during layout by the
+  /// [RenderViewport], before [applyContentDimensions]. After this method is
+  /// called, the layout will be recomputed and that may result in this method
+  /// being called again, though this should be very rare.
   ///
   /// See also:
   ///
+  ///  * [jumpTo], for also changing the scroll position when not in layout.
+  ///    [jumpTo] applies the change immediately and notifies its listeners.
   ///  * [correctPixels], which is used by the [ScrollPosition] itself to
   ///    set the offset initially during construction or after
   ///    [applyViewportDimension] or [applyContentDimensions] is called.
