f83a4d5a2311d407d8467d942fe649202c8fc932
Noam Drong <noam@drong.net>
2018-05-17 20:49:07 +0200

    Fix missing semicolon...

--- packages/flutter/lib/src/cupertino/icons.dart
+++ packages/flutter/lib/src/cupertino/icons.dart
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:flutter/widgets.dart'
+import 'package:flutter/widgets.dart';
 
 /// Identifiers for the supported Cupertino icons.
 ///
