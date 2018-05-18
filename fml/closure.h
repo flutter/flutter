2e6aad39a29df4767132ab1ed760aa8ebe1d78ec
Chinmay Garde <chinmaygarde@gmail.com>
2018-05-18 16:25:29 -0700

    Import synchronization utilities into FML. (#5312)

---
+++ fml/closure.h
@@ -0,0 +1,16 @@
+// Copyright 2016 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef FLUTTER_FML_CLOSURE_H_
+#define FLUTTER_FML_CLOSURE_H_
+
+#include <functional>
+
+namespace fml {
+
+using closure = std::function<void()>;
+
+}  // namespace fml
+
+#endif  // FLUTTER_FML_CLOSURE_H_
---
+++ fml/synchronization/thread_checker_unittest.cc
@@ -0,0 +1,37 @@
+// Copyright 2016 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "flutter/fml/synchronization/thread_checker.h"
+
+#include <thread>
+
+#include "gtest/gtest.h"
+
+namespace fml {
+namespace {
+
+TEST(ThreadCheckerTest, SameThread) {
+  ThreadChecker checker;
+  EXPECT_TRUE(checker.IsCreationThreadCurrent());
+}
+
+// Note: This test depends on |std::thread| being compatible with
+// |pthread_self()|.
+TEST(ThreadCheckerTest, DifferentThreads) {
+  ThreadChecker checker1;
+  EXPECT_TRUE(checker1.IsCreationThreadCurrent());
+
+  std::thread thread([&checker1]() {
+    ThreadChecker checker2;
+    EXPECT_TRUE(checker2.IsCreationThreadCurrent());
+    EXPECT_FALSE(checker1.IsCreationThreadCurrent());
+  });
+  thread.join();
+
+  // Note: Without synchronization, we can't look at |checker2| from the main
+  // thread.
+}
+
+}  // namespace
+}  // namespace fml
