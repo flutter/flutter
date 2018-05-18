5d4c2bb32d949df564440791668e2bdbae044c1f
Christopher Araüjo <dcaraujo39@gmail.com>
2018-05-18 23:52:46 +0200

    Added onChangeStart and onChangeEnd to CupertinoSlider (#17535)
    This is a follow up on issue #17169 and the pull request #17298
    
    This pull request adds the onChangeStart and onChangeEnd callbacks for CupertinoSlider. These are called when a user starts and ends a change respectively.
    
    Pushing for @dcaraujo0872, the PR author.

--- packages/flutter/test/cupertino/slider_test.dart
+++ packages/flutter/test/cupertino/slider_test.dart
@@ -11,6 +11,14 @@ import 'package:flutter_test/flutter_test.dart';
 import '../widgets/semantics_tester.dart';
 
 void main() {
+
+  Future<Null> _dragSlider(WidgetTester tester, Key sliderKey) {
+    final Offset topLeft = tester.getTopLeft(find.byKey(sliderKey));
+    const double unit = CupertinoThumbPainter.radius;
+    const double delta = 3.0 * unit;
+    return tester.dragFrom(topLeft + const Offset(unit, unit), const Offset(delta, 0.0));
+  }
+
   testWidgets('Slider does not move when tapped (LTR)', (WidgetTester tester) async {
     final Key sliderKey = new UniqueKey();
     double value = 0.0;
@@ -79,10 +87,10 @@ void main() {
     expect(SchedulerBinding.instance.transientCallbackCount, equals(0));
   });
 
-
-  testWidgets('Slider moves when dragged (LTR)', (WidgetTester tester) async {
+  testWidgets('Slider calls onChangeStart once when interaction begins', (WidgetTester tester) async {
     final Key sliderKey = new UniqueKey();
     double value = 0.0;
+    int numberOfTimesOnChangeStartIsCalled = 0;
 
     await tester.pumpWidget(new Directionality(
       textDirection: TextDirection.ltr,
@@ -98,6 +106,91 @@ void main() {
                     value = newValue;
                   });
                 },
+                onChangeStart: (double value) {
+                  numberOfTimesOnChangeStartIsCalled++;
+                }
+              ),
+            ),
+          );
+        },
+      ),
+    ));
+
+    await _dragSlider(tester, sliderKey);
+    
+    expect(numberOfTimesOnChangeStartIsCalled, equals(1));
+
+    await tester.pump(); // No animation should start.
+    // Check the transientCallbackCount before tearing down the widget to ensure
+    // that no animation is running.
+    expect(SchedulerBinding.instance.transientCallbackCount, equals(0));
+  });
+
+  testWidgets('Slider calls onChangeEnd once after interaction has ended', (WidgetTester tester) async {
+    final Key sliderKey = new UniqueKey();
+    double value = 0.0;
+    int numberOfTimesOnChangeEndIsCalled = 0;
+
+    await tester.pumpWidget(new Directionality(
+      textDirection: TextDirection.ltr,
+      child: new StatefulBuilder(
+        builder: (BuildContext context, StateSetter setState) {
+          return new Material(
+            child: new Center(
+              child: new CupertinoSlider(
+                key: sliderKey,
+                value: value,
+                onChanged: (double newValue) {
+                  setState(() {
+                    value = newValue;
+                  });
+                },
+                onChangeEnd: (double value) {
+                  numberOfTimesOnChangeEndIsCalled++;
+                }
+              ),
+            ),
+          );
+        },
+      ),
+    ));
+    
+    await _dragSlider(tester, sliderKey);
+    
+    expect(numberOfTimesOnChangeEndIsCalled, equals(1));
+
+    await tester.pump(); // No animation should start.
+    // Check the transientCallbackCount before tearing down the widget to ensure
+    // that no animation is running.
+    expect(SchedulerBinding.instance.transientCallbackCount, equals(0));
+  });
+
+  testWidgets('Slider moves when dragged (LTR)', (WidgetTester tester) async {
+    final Key sliderKey = new UniqueKey();
+    double value = 0.0;
+    double startValue;
+    double endValue;
+
+    await tester.pumpWidget(new Directionality(
+      textDirection: TextDirection.ltr,
+      child: new StatefulBuilder(
+        builder: (BuildContext context, StateSetter setState) {
+          return new Material(
+            child: new Center(
+              child: new CupertinoSlider(
+                key: sliderKey,
+                value: value,
+                onChanged: (double newValue) {
+                  setState(() {
+                    value = newValue;
+                  });
+                },
+                onChangeStart: (double value) {
+                  startValue = value;
+                },
+                onChangeEnd: (double value) {
+                  endValue = value;
+                }
               ),
             ),
           );
@@ -106,12 +199,18 @@ void main() {
     ));
 
     expect(value, equals(0.0));
+    
     final Offset topLeft = tester.getTopLeft(find.byKey(sliderKey));
     const double unit = CupertinoThumbPainter.radius;
     const double delta = 3.0 * unit;
     await tester.dragFrom(topLeft + const Offset(unit, unit), const Offset(delta, 0.0));
+
     final Size size = tester.getSize(find.byKey(sliderKey));
-    expect(value, equals(delta / (size.width - 2.0 * (8.0 + CupertinoThumbPainter.radius))));
+    final double finalValue = delta / (size.width - 2.0 * (8.0 + CupertinoThumbPainter.radius));
+    expect(startValue, equals(0.0));
+    expect(value, equals(finalValue));
+    expect(endValue, equals(finalValue));
+    
     await tester.pump(); // No animation should start.
     // Check the transientCallbackCount before tearing down the widget to ensure
     // that no animation is running.
@@ -121,7 +220,9 @@ void main() {
   testWidgets('Slider moves when dragged (RTL)', (WidgetTester tester) async {
     final Key sliderKey = new UniqueKey();
     double value = 0.0;
-
+    double startValue;
+    double endValue;
+    
     await tester.pumpWidget(new Directionality(
       textDirection: TextDirection.rtl,
       child: new StatefulBuilder(
@@ -136,6 +237,16 @@ void main() {
                     value = newValue;
                   });
                 },
+                onChangeStart: (double value) {
+                  setState(() {
+                    startValue = value;
+                  });
+                },
+                onChangeEnd: (double value) {
+                  setState(() {
+                    endValue = value;
+                  });
+                }
               ),
             ),
           );
@@ -144,12 +255,18 @@ void main() {
     ));
 
     expect(value, equals(0.0));
+    
     final Offset bottomRight = tester.getBottomRight(find.byKey(sliderKey));
     const double unit = CupertinoThumbPainter.radius;
     const double delta = 3.0 * unit;
     await tester.dragFrom(bottomRight - const Offset(unit, unit), const Offset(-delta, 0.0));
+    
     final Size size = tester.getSize(find.byKey(sliderKey));
-    expect(value, equals(delta / (size.width - 2.0 * (8.0 + CupertinoThumbPainter.radius))));
+    final double finalValue = delta / (size.width - 2.0 * (8.0 + CupertinoThumbPainter.radius));
+    expect(startValue, equals(0.0));
+    expect(value, equals(finalValue));
+    expect(endValue, equals(finalValue));
+    
     await tester.pump(); // No animation should start.
     // Check the transientCallbackCount before tearing down the widget to ensure
     // that no animation is running.
