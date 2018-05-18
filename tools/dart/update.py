1e4fc3e8333b715dd091c1853578704471372cee
Siva <a-siva@users.noreply.github.com>
2018-05-18 16:58:38 -0700

    Update dart version to 2.0.0-dev.55.0 (#142)

--- tools/dart/update.py
+++ tools/dart/update.py
@@ -19,7 +19,7 @@ import zipfile
 # How to roll the dart sdk: Just change this url! We write this to the stamp
 # file after we download, and then check the stamp file for differences.
 SDK_URL_BASE = ('http://gsdview.appspot.com/dart-archive/channels/dev/raw/'
-                '2.0.0-dev.47.0/sdk/')
+                '2.0.0-dev.55.0/sdk/')
 
 LINUX_64_SDK = 'dartsdk-linux-x64-release.zip'
 MACOS_64_SDK = 'dartsdk-macos-x64-release.zip'
