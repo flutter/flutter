99a1dde524d318b44fadc269491476f0f1693c98
Chinmay Garde <chinmaygarde@gmail.com>
2018-05-18 16:02:01 -0700

    Import FXL timing utilities into FML. (#5311)

---
+++ fml/time/time_point.cc
@@ -0,0 +1,76 @@
+// Copyright 2016 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "flutter/fml/time/time_point.h"
+
+#include "flutter/fml/build_config.h"
+
+#if defined(OS_MACOSX) || defined(OS_IOS)
+#include <mach/kern_return.h>
+#include <mach/mach_time.h>
+#elif defined(OS_FUCHSIA)
+#include <zircon/syscalls.h>
+#elif defined(OS_WIN)
+#include <windows.h>
+#else
+#include <time.h>
+#endif  // defined(OS_MACOSX) || defined(OS_IOS)
+
+#include "flutter/fml/logging.h"
+
+namespace fml {
+
+// Mac OS X/iOS don't have a (useful) |clock_gettime()|.
+// Note: Chromium's |base::TimeTicks::Now()| uses boot time (obtained via
+// |sysctl()| with |CTL_KERN|/|KERN_BOOTTIME|). For our current purposes,
+// monotonic time (which pauses during sleeps) is sufficient. TODO(vtl): If/when
+// we use this for other purposes, maybe we should use boot time (maybe also on
+// POSIX).
+#if defined(OS_MACOSX) || defined(OS_IOS)
+
+mach_timebase_info_data_t GetMachTimebaseInfo() {
+  mach_timebase_info_data_t timebase_info = {};
+  kern_return_t error = mach_timebase_info(&timebase_info);
+  FML_DCHECK(error == KERN_SUCCESS);
+  return timebase_info;
+}
+
+// static
+TimePoint TimePoint::Now() {
+  static mach_timebase_info_data_t timebase_info = GetMachTimebaseInfo();
+  return TimePoint(mach_absolute_time() * timebase_info.numer /
+                   timebase_info.denom);
+}
+
+#elif defined(OS_FUCHSIA)
+
+// static
+TimePoint TimePoint::Now() {
+  return TimePoint(zx_clock_get(ZX_CLOCK_MONOTONIC));
+}
+
+#elif defined(OS_WIN)
+
+TimePoint TimePoint::Now() {
+  uint64_t freq = 0;
+  uint64_t count = 0;
+  QueryPerformanceFrequency((LARGE_INTEGER*)&freq);
+  QueryPerformanceCounter((LARGE_INTEGER*)&count);
+  return TimePoint((count * 1000000000) / freq);
+}
+
+#else
+
+// static
+TimePoint TimePoint::Now() {
+  struct timespec ts;
+  int res = clock_gettime(CLOCK_MONOTONIC, &ts);
+  FML_DCHECK(res == 0);
+  (void)res;
+  return TimePoint::FromEpochDelta(TimeDelta::FromTimespec(ts));
+}
+
+#endif  // defined(OS_MACOSX) || defined(OS_IOS)
+
+}  // namespace fml
---
+++ fml/time/time_point.h
@@ -0,0 +1,70 @@
+// Copyright 2016 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef FLUTTER_FML_TIME_TIME_POINT_H_
+#define FLUTTER_FML_TIME_TIME_POINT_H_
+
+#include <stdint.h>
+
+#include <iosfwd>
+
+#include "flutter/fml/time/time_delta.h"
+
+namespace fml {
+
+// A TimePoint represents a point in time represented as an integer number of
+// nanoseconds elapsed since an arbitrary point in the past.
+//
+// WARNING: This class should not be serialized across reboots, or across
+// devices: the reference point is only stable for a given device between
+// reboots.
+class TimePoint {
+ public:
+  // Default TimePoint with internal value 0 (epoch).
+  constexpr TimePoint() = default;
+
+  static TimePoint Now();
+
+  static constexpr TimePoint Min() {
+    return TimePoint(std::numeric_limits<int64_t>::min());
+  }
+
+  static constexpr TimePoint Max() {
+    return TimePoint(std::numeric_limits<int64_t>::max());
+  }
+
+  static constexpr TimePoint FromEpochDelta(TimeDelta ticks) {
+    return TimePoint(ticks.ToNanoseconds());
+  }
+
+  TimeDelta ToEpochDelta() const { return TimeDelta::FromNanoseconds(ticks_); }
+
+  // Compute the difference between two time points.
+  TimeDelta operator-(TimePoint other) const {
+    return TimeDelta::FromNanoseconds(ticks_ - other.ticks_);
+  }
+
+  TimePoint operator+(TimeDelta duration) const {
+    return TimePoint(ticks_ + duration.ToNanoseconds());
+  }
+  TimePoint operator-(TimeDelta duration) const {
+    return TimePoint(ticks_ - duration.ToNanoseconds());
+  }
+
+  bool operator==(TimePoint other) const { return ticks_ == other.ticks_; }
+  bool operator!=(TimePoint other) const { return ticks_ != other.ticks_; }
+  bool operator<(TimePoint other) const { return ticks_ < other.ticks_; }
+  bool operator<=(TimePoint other) const { return ticks_ <= other.ticks_; }
+  bool operator>(TimePoint other) const { return ticks_ > other.ticks_; }
+  bool operator>=(TimePoint other) const { return ticks_ >= other.ticks_; }
+
+ private:
+  explicit constexpr TimePoint(int64_t ticks) : ticks_(ticks) {}
+
+  int64_t ticks_ = 0;
+};
+
+}  // namespace fml
+
+#endif  // FLUTTER_FML_TIME_TIME_POINT_H_
