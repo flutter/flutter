93b02cf2f970db573239651388f70afc0804aa88
Chinmay Garde <chinmaygarde@gmail.com>
2018-05-18 16:02:01 -0700

    Import FXL timing utilities into FML. (flutter/engine#5311)

---
+++ engine/src/flutter/fml/time/time_point_unittest.cc
@@ -0,0 +1,18 @@
+// Copyright 2017 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "flutter/fml/time/time_point.h"
+
+#include "gtest/gtest.h"
+
+namespace fml {
+namespace {
+
+TEST(TimePoint, Control) {
+  EXPECT_LT(TimePoint::Min(), TimePoint::Now());
+  EXPECT_GT(TimePoint::Max(), TimePoint::Now());
+}
+
+}  // namespace
+}  // namespace fml
