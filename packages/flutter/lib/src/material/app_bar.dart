14cce3866801eb7ad1bcf779530f15e0367b3c8a
Hans Muller <hansmuller@chromium.org>
2018-05-23 13:14:12 -0700

    Remove built-in right-edge appbar padding (#17852)

--- packages/flutter/lib/src/material/app_bar.dart
+++ packages/flutter/lib/src/material/app_bar.dart
@@ -425,15 +425,12 @@ class _AppBarState extends State<AppBar> {
       );
     }
 
-    final Widget toolbar = new Padding(
-      padding: const EdgeInsetsDirectional.only(end: 4.0),
-      child: new NavigationToolbar(
-        leading: leading,
-        middle: title,
-        trailing: actions,
-        centerMiddle: widget._getEffectiveCenterTitle(themeData),
-        middleSpacing: widget.titleSpacing,
-      ),
+    final Widget toolbar = new NavigationToolbar(
+      leading: leading,
+      middle: title,
+      trailing: actions,
+      centerMiddle: widget._getEffectiveCenterTitle(themeData),
+      middleSpacing: widget.titleSpacing,
     );
 
     // If the toolbar is allocated less than kToolbarHeight make it
