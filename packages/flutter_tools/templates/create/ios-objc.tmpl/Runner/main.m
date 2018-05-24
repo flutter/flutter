ca300100b8bb5ed7b08b1dc633fd115db719ad14
Sarah Zakarias <zarah@google.com>
2018-05-24 08:49:17 +0200

    Update plugin template to comply with new type and style rules (#17840)

--- packages/flutter_tools/templates/create/ios-objc.tmpl/Runner/main.m
+++ packages/flutter_tools/templates/create/ios-objc.tmpl/Runner/main.m
@@ -1,8 +1,8 @@
-#import <UIKit/UIKit.h>
 #import <Flutter/Flutter.h>
+#import <UIKit/UIKit.h>
 #import "AppDelegate.h"
 
-int main(int argc, char * argv[]) {
+int main(int argc, char* argv[]) {
   @autoreleasepool {
     return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
   }
