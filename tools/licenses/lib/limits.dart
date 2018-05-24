b60738297e40f52a25045768d938d9153c7d2cad
Adam Barth <github@adambarth.com>
2018-05-24 10:00:16 -0700

    Improve licenses script (#5355)

---
+++ tools/licenses/lib/limits.dart
@@ -0,0 +1,6 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// TODO(ianh): vastly increase this before checkin
+const int kMaxSize = 5 * 1024; // only look for copyrights and licenses at the top of the file
