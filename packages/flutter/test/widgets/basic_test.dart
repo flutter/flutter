d85fc0db96e0725a6f76909c2ba8172a2f184bdb
Emmanuel Garcia <blasten@gmail.com>
2018-05-23 22:13:52 -0700

    Add hit test for FractionalTranslation

--- packages/flutter/test/widgets/basic_test.dart
+++ packages/flutter/test/widgets/basic_test.dart
@@ -55,7 +55,37 @@ void main() {
   });
 
   group('FractionalTranslation', () {
-    testWidgets('hit test', (WidgetTester tester) async {
+    testWidgets('hit test - inside bounding box', (WidgetTester tester) async {
+      final GlobalKey key1 = new GlobalKey();
+      bool _pointerDown = false;
+
+      await tester.pumpWidget(
+        new Center(
+          child: new FractionalTranslation(
+            translation: Offset.zero,
+            transformHitTests: true,
+            child: new Listener(
+              onPointerDown: (PointerDownEvent event) {
+                _pointerDown = true;
+              },
+              child: new SizedBox(
+                key: key1,
+                width: 100.0,
+                height: 100.0,
+                child: new Container(
+                  color: const Color(0xFF0000FF)
+                ),
+              ),
+            )
+          )
+        )
+      );
+      expect(_pointerDown, isFalse);
+      await tester.tap(find.byKey(key1));
+      expect(_pointerDown, isTrue);
+    });
+
+    testWidgets('hit test - outside bounding box', (WidgetTester tester) async {
       final GlobalKey key1 = new GlobalKey();
       bool _pointerDown = false;
 
