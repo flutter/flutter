d2fb8b9b2783f0900d2096a632b14c755113ed1c
Todd Volkert <tvolkert@google.com>
2018-05-26 10:17:45 -0700

    fix

--- dev/bots/travis_setup.sh
+++ dev/bots/travis_setup.sh
@@ -52,7 +52,7 @@ if [ -n "$TRAVIS" ]; then
     android list targets
     target_id="$(android list targets | grep -E '^id: ' | head -n1 | sed -E 's/id: ([^ ]+).*$/\1/')"
     echo "Found android target with id: $target_id"
-    android create avd --force --name test --target $target_id --abi armeabi-v7a
+    android create avd --force --name test --target $target_id --abi google_apis/armeabi-v7a
     emulator -avd test -no-skin -no-audio -no-window &
     android-wait-for-emulator
   fi
