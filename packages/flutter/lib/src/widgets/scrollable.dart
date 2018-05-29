719f76318ff46775e58b08ec6d83cc6172889736
maxchehab <maxchehab@gmail.com>
2018-05-29 13:37:33 -0700

    Placed 'dart:' and 'package:' imports before relative imports

--- packages/flutter/lib/src/widgets/scrollable.dart
+++ packages/flutter/lib/src/widgets/scrollable.dart
@@ -1,14 +1,13 @@
 // Copyright 2015 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-import '../material/theme.dart';
-
 import 'dart:async';
 
 import 'package:flutter/gestures.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/scheduler.dart';
 
+import '../material/theme.dart';
 import 'basic.dart';
 import 'framework.dart';
 import 'gesture_detector.dart';
