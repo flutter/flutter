6e1766500f9a4aef82b32a0ec5f11062f834b78f
Chris Bracken <chris@bracken.jp>
2018-06-07 13:29:52 -0700

    Always use Dart 2 in gradle build

--- packages/flutter_tools/lib/src/android/gradle.dart
+++ packages/flutter_tools/lib/src/android/gradle.dart
@@ -327,18 +327,16 @@ Future<Null> _buildGradleProjectV2(String gradle, BuildInfo buildInfo, String ta
   if (target != null) {
     command.add('-Ptarget=$target');
   }
-  if (buildInfo.previewDart2) {
-    if (buildInfo.trackWidgetCreation)
-      command.add('-Ptrack-widget-creation=true');
-    if (buildInfo.extraFrontEndOptions != null)
-      command.add('-Pextra-front-end-options=${buildInfo.extraFrontEndOptions}');
-    if (buildInfo.extraGenSnapshotOptions != null)
-      command.add('-Pextra-gen-snapshot-options=${buildInfo.extraGenSnapshotOptions}');
-    if (buildInfo.fileSystemRoots != null && buildInfo.fileSystemRoots.isNotEmpty)
-      command.add('-Pfilesystem-roots=${buildInfo.fileSystemRoots.join('|')}');
-    if (buildInfo.fileSystemScheme != null)
-      command.add('-Pfilesystem-scheme=${buildInfo.fileSystemScheme}');
-  }
+  if (buildInfo.trackWidgetCreation)
+    command.add('-Ptrack-widget-creation=true');
+  if (buildInfo.extraFrontEndOptions != null)
+    command.add('-Pextra-front-end-options=${buildInfo.extraFrontEndOptions}');
+  if (buildInfo.extraGenSnapshotOptions != null)
+    command.add('-Pextra-gen-snapshot-options=${buildInfo.extraGenSnapshotOptions}');
+  if (buildInfo.fileSystemRoots != null && buildInfo.fileSystemRoots.isNotEmpty)
+    command.add('-Pfilesystem-roots=${buildInfo.fileSystemRoots.join('|')}');
+  if (buildInfo.fileSystemScheme != null)
+    command.add('-Pfilesystem-scheme=${buildInfo.fileSystemScheme}');
   if (buildInfo.buildSharedLibrary && androidSdk.ndk != null) {
     command.add('-Pbuild-shared-library=true');
   }
