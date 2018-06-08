6c3ef1e838978c18d8aa294ffd5f6c091ef8fb30
Jonah Williams <jonahwilliams@google.com>
2018-06-08 15:23:14 -0700

    Update app_bar.dart

--- packages/flutter/lib/src/material/app_bar.dart
+++ packages/flutter/lib/src/material/app_bar.dart
@@ -7,7 +7,6 @@ import 'dart:math' as math;
 import 'package:flutter/foundation.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter/src/widgets/annotated_region.dart';
 import 'package:flutter/widgets.dart';
 
 import 'back_button.dart';
