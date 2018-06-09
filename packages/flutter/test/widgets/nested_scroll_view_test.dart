aed378bf88fe2c4cf6320ade01bda6a5d87171f2
Jonah Williams <jonahwilliams@google.com>
2018-06-08 17:27:12 -0700

    undo test change

--- packages/flutter/test/widgets/nested_scroll_view_test.dart
+++ packages/flutter/test/widgets/nested_scroll_view_test.dart
@@ -558,7 +558,7 @@ void main() {
     expect(buildCount, expectedBuildCount);
     expect(find.text('Item 18'), findsNothing);
     expect(find.text('Item 2'), findsNWidgets(2));
-    expect(find.byType(NestedScrollView), paints..shadow());
+    expect(find.byType(NestedScrollView), isNot(paints..shadow()));
     await tester.pump(const Duration(seconds: 1)); // shadow would be finished coming back
     expect(find.byType(NestedScrollView), isNot(paints..shadow()));
     await gesture5.up();
