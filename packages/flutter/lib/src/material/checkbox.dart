21c2941503a6d537198936e61cf59768cc9fcc73
jonahwilliams <campfish91@gmail.com>
2018-06-09 21:59:11 -0700

    increase size of checkbox, switch, and radio button to 48 by 48 to conform to android a11y scanner

--- packages/flutter/lib/src/material/checkbox.dart
+++ packages/flutter/lib/src/material/checkbox.dart
@@ -200,7 +200,7 @@ class _RenderCheckbox extends RenderToggleable {
         activeColor: activeColor,
         inactiveColor: inactiveColor,
         onChanged: onChanged,
-        size: const Size(2 * kRadialReactionRadius, 2 * kRadialReactionRadius),
+        size: const Size(2 * kRadialReactionRadius + 8.0, 2 * kRadialReactionRadius + 8.0),
         vsync: vsync,
       );
 
