be02d0cf09f13003ee59f5a9a571c9878d52d3ac
Chris Bracken <chris@bracken.jp>
2018-06-11 18:12:10 -0700

    Revert "Eliminate support for Dart 1 (#5504)" (#5505)
    Broke runtime_unittests, which are still running directly from source.
    
    This reverts commit 0ea93c3a3e40426fd222ba22c2f60756c8a32097.

--- shell/platform/darwin/ios/framework/Headers/Flutter.h
+++ shell/platform/darwin/ios/framework/Headers/Flutter.h
@@ -8,30 +8,30 @@
 /**
  BREAKING CHANGES:
 
- February 28, 2018: Removed "initWithFLXArchive" and "initWithFLXArchiveWithScriptSnapshot".
+ February 28, 2018: Removed "initWithFLXArchive" and
+ "initWithFLXArchiveWithScriptSnapshot".
 
- January 15, 2018: Marked "initWithFLXArchive" and "initWithFLXArchiveWithScriptSnapshot" as
- unavailable following the deprecation from December 11, 2017. Scheduled to be removed on February
+ January 15, 2018: Marked "initWithFLXArchive" and
+ "initWithFLXArchiveWithScriptSnapshot" as unavailable following the
+ deprecation from December 11, 2017. Scheduled to be removed on February
  19, 2018.
 
  January 09, 2018: Deprecated "FlutterStandardBigInteger" and its use in
- "FlutterStandardMessageCodec" and "FlutterStandardMethodCodec". Scheduled to be marked as
- unavailable once the deprecation has been available on the flutter/flutter alpha branch for four
- weeks. "FlutterStandardBigInteger" was needed because the Dart 1.0 int type had no size limit. With
- Dart 2.0, the int type is a fixed-size, 64-bit signed integer. If you need to communicate larger
- integers, use NSString encoding instead.
+ "FlutterStandardMessageCodec" and "FlutterStandardMethodCodec". Scheduled to
+ be marked as unavailable once the deprecation has been available on the
+ flutter/flutter alpha branch for four weeks. "FlutterStandardBigInteger" was
+ needed because the Dart 1.0 int type had no size limit. With Dart 2.0, the
+ int type is a fixed-size, 64-bit signed integer. If you need to communicate
+ larger integers, use NSString encoding instead.
 
- December 11, 2017: Deprecated "initWithFLXArchive" and "initWithFLXArchiveWithScriptSnapshot" and
- scheculed the same to be marked as unavailable on January 15, 2018. Instead,
- "initWithFlutterAssets" and "initWithFlutterAssetsWithScriptSnapshot" should be used. The reason
- for this change is that the FLX archive will be deprecated and replaced with a flutter assets
- directory containing the same files as the FLX did.
+ December 11, 2017: Deprecated "initWithFLXArchive" and
+ "initWithFLXArchiveWithScriptSnapshot" and scheculed the same to be marked as
+ unavailable on January 15, 2018. Instead, "initWithFlutterAssets" and
+ "initWithFlutterAssetsWithScriptSnapshot" should be used. The reason for this
+ change is that the FLX archive will be deprecated and replaced with a flutter
+ assets directory containing the same files as the FLX did.
 
  November 29, 2017: Added a BREAKING CHANGES section.
-
- June 11, 2018: Eliminated "initWithFlutterAssetsWithScriptSnapshot" which was only used for Dart 1
- code. Flutter is now Dart 2 only. In Dart 2, application code is loaded from a kernel snapshot in
- debug mode, or AOT-compiled and linked in profile and release modes.
  */
 
 #include "FlutterAppDelegate.h"
