8fcfb30c18348204dc7ae92887b36f886fc05c9b
Almarhoon Ibraheem <ibeem@outlook.com>
2018-06-15 11:32:30 +0300

    Add ability to set collapse mode in flexible space bar

--- packages/flutter/lib/src/material/flexible_space_bar.dart
+++ packages/flutter/lib/src/material/flexible_space_bar.dart
@@ -10,6 +10,19 @@ import 'package:flutter/widgets.dart';
 import 'constants.dart';
 import 'theme.dart';
 
+/// The collapsing effect while the space bar expands or collapses.
+///
+enum CollapseMode {
+  /// The background widget will scroll in a parallax fashion.
+  parallax,
+
+  /// The background widget pin in place until it reaches the min extent.
+  pin,
+
+  /// The background widget will act as normal with no collapsing effect.
+  none,
+}
+
 /// The part of a material design [AppBar] that expands and collapses.
 ///
 /// Most commonly used in in the [SliverAppBar.flexibleSpace] field, a flexible
@@ -34,7 +47,8 @@ class FlexibleSpaceBar extends StatefulWidget {
     Key key,
     this.title,
     this.background,
-    this.centerTitle
+    this.centerTitle,
+    this.collapseMode = CollapseMode.parallax
   }) : super(key: key);
 
   /// The primary contents of the flexible space bar when expanded.
@@ -51,6 +65,11 @@ class FlexibleSpaceBar extends StatefulWidget {
   ///
   /// Defaults to being adapted to the current [TargetPlatform].
   final bool centerTitle;
+
+  /// Collapse effect while scrolling.
+  ///
+  /// Defaults to Parallax mode.
+  final CollapseMode collapseMode;
 
   /// Wraps a widget that contains an [AppBar] to convey sizing information down
   /// to the [FlexibleSpaceBar].
@@ -106,6 +125,20 @@ class _FlexibleSpaceBarState extends State<FlexibleSpaceBar> {
     return null;
   }
 
+  double _getCollapsePadding(double t, _FlexibleSpaceBarSettings settings){
+    final double deltaExtent = settings.maxExtent - settings.minExtent;
+
+    switch(widget.collapseMode){
+      case CollapseMode.pin:
+        return -(settings.maxExtent - settings.currentExtent);
+      case CollapseMode.none:
+        return 0.0;
+      case CollapseMode.parallax:
+      default:
+        return -new Tween<double>(begin: 0.0, end: deltaExtent / 4.0).lerp(t);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final _FlexibleSpaceBarSettings settings = context.inheritFromWidgetOfExactType(_FlexibleSpaceBarSettings);
@@ -125,10 +158,9 @@ class _FlexibleSpaceBarState extends State<FlexibleSpaceBar> {
       const double fadeEnd = 1.0;
       assert(fadeStart <= fadeEnd);
       final double opacity = 1.0 - new Interval(fadeStart, fadeEnd).transform(t);
-      final double parallax = new Tween<double>(begin: 0.0, end: deltaExtent / 4.0).lerp(t);
       if (opacity > 0.0) {
         children.add(new Positioned(
-          top: -parallax,
+          top: _getCollapsePadding(t, settings),
           left: 0.0,
           right: 0.0,
           height: settings.maxExtent,
