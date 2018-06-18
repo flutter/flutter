e05b4e94ca138ad71d807cfc46f893afa102ea1e
Danny Tuppeny <dantup@google.com>
2018-06-18 08:38:30 +0100

    Extract waiting for response + tidy up

--- packages/flutter_tools/test/integration/flutter_tester_hot_reload.dart
+++ packages/flutter_tools/test/integration/flutter_tester_hot_reload.dart
@@ -15,8 +15,11 @@ import '../src/common.dart';
 import '../src/context.dart';
 import 'util.dart';
 
+// Set this to true for debugging to get JSON written to stdout.
+const bool printJson = false;
+Directory tempDir;
+
 void main() {
-  Directory tempDir;
 
   setUp(() async {
     tempDir = await fs.systemTempDirectory.createTemp('test_app');
@@ -27,125 +30,132 @@ void main() {
       tempDir?.deleteSync(recursive: true);
       tempDir = null;
     } catch (e) {
-      // Ignored.
+      // Don't fail tests if we failed to clean up temp folder.
     }
   });
 
   group('FlutterTesterDevice', () {
     testUsingContext('can hot reload', () async {
-      writePubspec(tempDir.path);
-      writePackages(tempDir.path);
-      await getPackages(tempDir.path);
+      await _setupSampleProject();
 
-      final String mainPath = fs.path.join(tempDir.path, 'lib', 'main.dart');
-      writeFile(mainPath, r'''
-import 'package:flutter/material.dart';
+      // TODO(dantup): Is there a better way than spawning a proc? This breaks debugging..
+      // However, there's a lot of logic inside RunCommand that wouldn't be good
+      // to duplicate here.
+      final Process proc = await _runFlutter(tempDir);
 
-void main() => runApp(new MyApp());
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Flutter Demo',
-      home: new Container(),
-    );
-  }
-}
-''');
-
-      const ProcessManager _processManager = const LocalProcessManager();
-      final Process proc = await _processManager.start(
-        <String>[
-          fs.path.join(getFlutterRoot(), 'bin', 'flutter'),
-          'run',
-          '--machine',
-          '-d',
-          'flutter-tester',
-          mainPath
-        ],
-        workingDirectory: tempDir.path,
-      );
-
-      String appId;
-      bool ok;
-      final Completer<Null> ready = new Completer<Null>();
       // Make a broadcast stream for stdout so we can have many listeners
       final StreamController<String> stdout = new StreamController<String>.broadcast();
       _transformToLines(proc.stdout).listen((String line) => stdout.add(line));
+
       // Capture stderr to a buffer for better error messages
       final StringBuffer stderr = new StringBuffer();
       _transformToLines(proc.stderr).listen(stderr.writeln);
-      stdout.stream.listen(print);
-      // Handle startup and log any non-JSON output to the error buffer (since
-      // we're running in machine, we get errors in stdout).
-      stdout.stream.listen((String line) {
-        final dynamic json = _parseFlutterResponse(line);
-        if (json != null && json['event'] == 'app.started') {
-          appId = json['params']['appId'];
-          ready.complete();
-          ok = true;
-        } else if (json == null) {
-          stderr.writeln(line);
-        }
-      });
-      // Set a flag on exit so we know we quit unexpectedly.
-      proc.exitCode.then((int exitCode) => ok = false);
-      await Future.any(<Future<void>>[ready.future, proc.exitCode]);
-      if (!ok) {
-        throw 'Failed to run test app.\n\n$stderr';
+
+      if (printJson) {
+        stdout.stream.listen(print);
       }
 
-      // Send a hot reload.
-      final Future<Map<String, dynamic>> hotReloadRequest = _sendFlutterRequest(
+      final Map<String, dynamic> started =await _waitFor(stdout.stream, event: 'app.started');
+      final String appId = started['params']['appId'];
+
+      final Map<String, dynamic> hotReloadResp = await _send(
           stdout.stream,
           proc.stdin,
           'app.restart',
           <String, dynamic>{'appId': appId, 'fullRestart': false});
 
-      // Wait for a response or the app to quit
-      await Future.any(<Future<void>>[proc.exitCode, hotReloadRequest]);
+      if (hotReloadResp['error'] != null || hotReloadResp['result'] == false)
+        throw 'Unexpected error response: ${hotReloadResp['error']}\n\n${stderr.toString()}';
 
-      if (!ok) {
-        throw 'App crashed during hot reloads.\n\n$stderr';
-      }
-
-      final Map<String, dynamic> hotReloadResp = await hotReloadRequest;
-
-      if (hotReloadResp['error'] != null)
-        throw 'Unexpected error response ${hotReloadResp['error']}';
-
-      // Send a stop request and wait for successfull exit.
-      _sendFlutterRequest(stdout.stream, proc.stdin,
-          'app.stop', <String, dynamic>{'appId': appId});
-      final int result = await proc.exitCode;
-      if (result != 0)
-        throw 'Received unexpected exit code $result from run process.\n\n$stderr';
+      // Send a stop request and wait for exit.
+      await _send(stdout.stream, proc.stdin, 'app.stop', <String, dynamic>{'appId': appId});
+      await proc.exitCode;
     });
   });
 }
 
+Future<void> _setupSampleProject() async {
+  writePubspec(tempDir.path);
+  writePackages(tempDir.path);
+  await getPackages(tempDir.path);
+  
+  final String mainPath = fs.path.join(tempDir.path, 'lib', 'main.dart');
+  writeFile(mainPath, r'''
+  import 'package:flutter/material.dart';
+  
+  void main() => runApp(new MyApp());
+  
+  class MyApp extends StatelessWidget {
+    @override
+    Widget build(BuildContext context) {
+      return new MaterialApp(
+  title: 'Flutter Demo',
+  home: new Container(),
+      );
+    }
+  }
+  ''');
+}
+
+Future<Process> _runFlutter(Directory projectDir) async {
+  final String flutterBin = fs.path.join(getFlutterRoot(), 'bin', 'flutter');
+  final List<String> command = <String>[
+    flutterBin,
+    'run',
+    '--machine',
+    '-d',
+    'flutter-tester'
+  ];
+  if (printJson) {
+    print('Spawning $command in ${projectDir.path}');
+  }
+  const ProcessManager _processManager = const LocalProcessManager();
+  final Process proc = await _processManager.start(
+    command,
+    workingDirectory: projectDir.path,
+  );
+  return proc;
+}
+
 int id = 1;
-Future<Map<String, dynamic>> _sendFlutterRequest(
+Future<Map<String, dynamic>> _send(
     Stream<String> stdout, IOSink stdin, String method, dynamic params) async {
   final int requestId = id++;
-  final Completer<Map<String, dynamic>> response = new Completer<Map<String, dynamic>>();
-  final StreamSubscription<String> responseSubscription = stdout.listen((String line) {
-    final Map<String, dynamic> json = _parseFlutterResponse(line);
-    if (json != null && json['id'] == requestId) {
-      response.complete(json);
-    }
-  });
   final Map<String, dynamic> req = <String, dynamic>{
     'id': requestId,
     'method': method,
     'params': params
   };
   final String jsonEncoded = json.encode(<Map<String, dynamic>>[req]);
+  if (printJson) {
+    print(jsonEncoded);
+  }
   stdin.writeln(jsonEncoded);
-  final Map<String, dynamic> result = await response.future;
-  responseSubscription.cancel();
-  return result;
+  return _waitFor(stdout, id: requestId);
+}
+
+Future<Map<String, dynamic>> _waitFor(Stream<String> stdout,
+    {String event, int id}) async {
+  final Completer<Map<String, dynamic>> response =
+      new Completer<Map<String, dynamic>>();
+  final StreamSubscription<String> sub = stdout.listen((String line) {
+    final dynamic json = _parseFlutterResponse(line);
+    if (json == null) {
+      return;
+    }
+    else if ((event != null && json['event'] == event)
+        || (id != null && json['id'] == id)) {
+      response.complete(json);
+    }
+  });
+  // TODO(dantup): Why can't I remove this Null/Null/Object?
+  final Future<Null> timeout = Future<Null>.delayed(const Duration(seconds: 10))
+      .then((Object a) => throw new Exception('Did not receive expected event/response within 10s'));
+  try {
+    return await Future.any(<Future<Map<String, dynamic>>>[response.future, timeout]);
+  } finally {
+    sub.cancel();
+  }
 }
 
 Map<String, dynamic> _parseFlutterResponse(String line) {
