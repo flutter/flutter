df92deb2a54939784e596e6cbdc08b6d56c57680
jonahwilliams <campfish91@gmail.com>
2018-06-21 21:44:46 -0700

    Add ink tests for raw material button

--- packages/flutter/test/material/raw_material_button_test.dart
+++ packages/flutter/test/material/raw_material_button_test.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+import '../rendering/mock_canvas.dart';
 import '../widgets/semantics_tester.dart';
 
 void main() {
@@ -57,5 +58,58 @@ void main() {
     ), ignoreTransform: true));
 
     semantics.dispose();
+  });
+
+  testWidgets('Ink splash from center tap originates in correct location', (WidgetTester tester) async {
+    const Color highlightColor = const Color(0xAAFF0000);
+    const Color splashColor = const Color(0xAA0000FF);
+    const Color fillColor = const Color(0xFFEF5350);
+
+    await tester.pumpWidget(
+      new RawMaterialButton(
+        materialTapTargetSize: MaterialTapTargetSize.padded,
+        onPressed: () {},
+        fillColor: fillColor,
+        highlightColor: highlightColor,
+        splashColor: splashColor,
+        child: const SizedBox(),
+      )
+    );
+
+    final Offset center = tester.getCenter(find.byType(InkWell));
+    final TestGesture gesture = await tester.startGesture(center);
+    await tester.pump(); // start gesture
+    await tester.pump(const Duration(milliseconds: 200)); // wait for splash to be well under way
+
+    final RenderBox box = Material.of(tester.element(find.byType(InkWell))) as dynamic;
+    // centered in material button.
+    expect(box, paints..circle(x: 44.0, y: 18.0, color: splashColor));
+    await gesture.up();
+  });
+
+  testWidgets('Ink splash from tap above material originates in correct location', (WidgetTester tester) async {
+    const Color highlightColor = const Color(0xAAFF0000);
+    const Color splashColor = const Color(0xAA0000FF);
+    const Color fillColor = const Color(0xFFEF5350);
+
+    await tester.pumpWidget(
+      new RawMaterialButton(
+        materialTapTargetSize: MaterialTapTargetSize.padded,
+        onPressed: () {},
+        fillColor: fillColor,
+        highlightColor: highlightColor,
+        splashColor: splashColor,
+        child: const SizedBox(),
+      )
+    );
+
+    final Offset top = tester.getRect(find.byType(InkWell)).topCenter;
+    final TestGesture gesture = await tester.startGesture(top);
+    await tester.pump(); // start gesture
+    await tester.pump(const Duration(milliseconds: 200)); // wait for splash to be well under way
+    final RenderBox box = Material.of(tester.element(find.byType(InkWell))) as dynamic;
+    // paints above above material
+    expect(box, paints..circle(x: 44.0, y: 0.0, color: splashColor));
+    await gesture.up();
   });
 }
