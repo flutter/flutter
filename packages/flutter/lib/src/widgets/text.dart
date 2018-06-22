aa04311de3e68a76382f875a4a183167737c2ec9
Hans Muller <hansmuller@chromium.org>
2018-06-22 15:28:32 -0700

    can the can can

--- packages/flutter/lib/src/widgets/text.dart
+++ packages/flutter/lib/src/widgets/text.dart
@@ -175,7 +175,7 @@ class DefaultTextStyle extends InheritedWidget {
 /// )
 /// ```
 ///
-/// Using the [Text.rich] constructor, the [Text] widget can can
+/// Using the [Text.rich] constructor, the [Text] widget can
 /// display a paragraph with differently styled [TextSpan]s. The sample
 /// that follows displays "Hello beautiful world" with different styles
 /// for each word.
