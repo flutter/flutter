7d2f3b70694ef2fe477d56a6d919f68bf4740ec4
Danny Tuppeny <dantup@google.com>
2018-06-28 09:18:50 +0100

    Only skip these tests on Windows as they work elsewhere

--- packages/flutter_tools/test/integration/hot_reload_test.dart
+++ packages/flutter_tools/test/integration/hot_reload_test.dart
@@ -43,7 +43,7 @@ void main() {
           _project.breakpointLine);
 
       expect(isolate.pauseEvent, const isInstanceOf<VMPauseBreakpointEvent>());
-    }, skip: true); // https://github.com/flutter/flutter/issues/17833
+    }, skip: platform.isWindows); // https://github.com/flutter/flutter/issues/17833
 
     test('hits breakpoints with file:// prefixes after reload', () async {
       await _flutter.run(withDebugger: true);
