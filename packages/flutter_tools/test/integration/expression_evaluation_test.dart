ab3fb3d0ffc00a637abceab39433aeeccb9366d1
Danny Tuppeny <dantup@google.com>
2018-07-02 15:37:03 +0100

    Skip expression evaluation tests on Windows

--- packages/flutter_tools/test/integration/expression_evaluation_test.dart
+++ packages/flutter_tools/test/integration/expression_evaluation_test.dart
@@ -6,6 +6,7 @@ import 'dart:async';
 
 import 'package:file/file.dart';
 import 'package:flutter_tools/src/base/file_system.dart';
+import 'package:flutter_tools/src/base/platform.dart';
 import 'package:test/test.dart';
 import 'package:vm_service_client/vm_service_client.dart';
 
@@ -132,6 +133,6 @@ void main() {
       await _flutter.run(withDebugger: true);
       await breakInBuildMethod(_flutter);
       await evaluateComplexReturningExpressions();
-    });
+    }, skip: platform.isWindows); // https://github.com/flutter/flutter/issues/17833
   }, timeout: const Timeout.factor(3));
 }
