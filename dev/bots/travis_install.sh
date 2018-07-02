74db9366b4b1b094369388ef50fb26f3b7fd3104
Danny Tuppeny <dantup@google.com>
2018-07-02 13:42:06 +0100

    Temporarily comment out this condition so the install runs on Travis

--- dev/bots/travis_install.sh
+++ dev/bots/travis_install.sh
@@ -18,8 +18,8 @@ function retry {
   return 0
 }
 
-if [ -n "$TRAVIS" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-  gem install coveralls-lcov
-  gem install bundler
+#if [ -n "$TRAVIS" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+#  gem install coveralls-lcov
+#  gem install bundler
   retry 5 npm install -g firebase-tools@">=3.6.1 <3.7.0"
-fi
+#fi
