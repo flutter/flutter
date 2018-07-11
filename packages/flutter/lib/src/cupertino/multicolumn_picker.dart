7a71c9cef785cbc7c8ef26ca36a9bef391ccf33e
Viet Do <doxuanviet@doxuanviet.mtv.corp.google.com>
2018-07-11 14:37:24 -0700

    fix errors in analyze

--- packages/flutter/lib/src/cupertino/multicolumn_picker.dart
+++ packages/flutter/lib/src/cupertino/multicolumn_picker.dart
@@ -2,7 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/widgets.dart';
+
+import 'picker.dart';
 
 /// An iOS picker with multiple columns.
 /// Consists of a list of CupertinoPickers, and this class will display them
