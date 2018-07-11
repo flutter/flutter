8a60f2cc4c247e76d5caef506d4ab55c1ba245d1
Greg Spencer <gspencer@google.com>
2018-07-10 19:36:58 -0700

    Add Bash and Zsh command completion to Flutter command

---
+++ packages/flutter_tools/lib/src/commands/shell_completion.dart
@@ -0,0 +1,40 @@
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'dart:async';
+
+import 'package:completion/completion.dart';
+import '../runner/flutter_command.dart';
+
+class ShellCompletionCommand extends FlutterCommand {
+  ShellCompletionCommand({ bool verboseHelp = false });
+
+  @override
+  final String name = 'bash-completion';
+
+  @override
+  final String description =
+    'Outputs command line shell completion setup scripts.\n\n'
+    'This command prints the flutter command line completion script for Bash and Zsh. To use it,\n'
+    'redirect the output into a file and follow the instructions in the output to install it in\n'
+    'your shell environment. Once added, your shell will be able to complete flutter commands and\n'
+    'options.\n\n'
+    'The Flutter tool anonymously reports feature usage statistics and basic crash reports to help improve\n'
+    'Flutter tools over time. See Google\'s privacy policy: https://www.google.com/intl/en/policies/privacy/';
+
+  @override
+  final List<String> aliases = <String>['zsh-completion'];
+
+  @override
+  bool get shouldUpdateCache => false;
+
+  /// Return null to disable tracking of the `bash-completion` command.
+  @override
+  Future<String> get usagePath => null;
+
+  @override
+  Future<Null> runCommand() async {
+    print(generateCompletionScript(<String>['flutter']));
+  }
+}
--- packages/flutter_tools/lib/src/runner/flutter_command_runner.dart
+++ packages/flutter_tools/lib/src/runner/flutter_command_runner.dart
@@ -7,6 +7,7 @@ import 'dart:convert';
 
 import 'package:args/args.dart';
 import 'package:args/command_runner.dart';
+import 'package:completion/completion.dart';
 import 'package:file/file.dart';
 import 'package:platform/platform.dart';
 import 'package:process/process.dart';
@@ -175,6 +176,25 @@ class FlutterCommandRunner extends CommandRunner<Null> {
       print('Unable to locate flutter root: $error');
     }
     return '.';
+  }
+
+  @override
+  ArgResults parse(Iterable<String> args) {
+    try {
+      return tryArgsCompletion(args, argParser);
+    } on ArgParserException catch (error) {
+      if (error.commands.isEmpty) {
+        usageException(error.message);
+      }
+
+      Command<Null> command = commands[error.commands.first];
+      for (String commandName in error.commands.skip(1)) {
+        command = command.subcommands[commandName];
+      }
+
+      command.usageException(error.message);
+      return null;
+    }
   }
 
   @override
