048ce32a7452f69671bcc572504fed95220a935f
Chris Bracken <chris@bracken.jp>
2018-07-11 15:06:33 -0700

    Eliminate checks for package six
    Neither Flutter nor any of its direct dependencies/tooling relies on
    package six. ios-deploy depends on LLDB.framework (included with Xcode),
    which relies on a Python script that imports this package but uses
    whichever Python is at the front of the path. Flutter now invokes
    ios-deploy with a PATH with /usr/bin forced to the front in order to
    avoid this problem.
    
    We could have retained the check out of paranoia, but this seems
    unnecessary since it's entirely possible LLDB.framework may one day drop
    this dependency, in which case, I'd expect the base system install of
    Python would likely drop it as well.

--- packages/flutter_tools/lib/src/ios/ios_workflow.dart
+++ packages/flutter_tools/lib/src/ios/ios_workflow.dart
@@ -48,8 +48,6 @@ class IOSWorkflow extends DoctorValidator implements Workflow {
 
   bool get hasHomebrew => os.which('brew') != null;
 
-  bool get hasPythonSixModule => kPythonSix.isInstalled;
-
   Future<String> get macDevMode async => (await runAsync(<String>['DevToolsSecurity', '-status'])).processResult.stdout;
 
   Future<bool> get _iosDeployIsInstalledAndMeetsVersionCheck async {
@@ -67,7 +65,6 @@ class IOSWorkflow extends DoctorValidator implements Workflow {
   Future<ValidationResult> validate() async {
     final List<ValidationMessage> messages = <ValidationMessage>[];
     ValidationType xcodeStatus = ValidationType.missing;
-    ValidationType pythonStatus = ValidationType.missing;
     ValidationType brewStatus = ValidationType.missing;
     String xcodeVersionInfo;
 
@@ -119,14 +116,6 @@ class IOSWorkflow extends DoctorValidator implements Workflow {
             '  sudo xcode-select --switch /Applications/Xcode.app/Contents/Developer'
         ));
       }
-    }
-
-    // Python dependencies installed
-    if (hasPythonSixModule) {
-      pythonStatus = ValidationType.installed;
-    } else {
-      pythonStatus = ValidationType.missing;
-      messages.add(new ValidationMessage.error(kPythonSix.errorMessage));
     }
 
     // brew installed
@@ -221,7 +210,7 @@ class IOSWorkflow extends DoctorValidator implements Workflow {
     }
 
     return new ValidationResult(
-      <ValidationType>[xcodeStatus, pythonStatus, brewStatus].reduce(_mergeValidationTypes),
+      <ValidationType>[xcodeStatus, brewStatus].reduce(_mergeValidationTypes),
       messages,
       statusInfo: xcodeVersionInfo
     );
