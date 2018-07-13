3628fa0f6b6381ea34a69c048f2b4a1220a178c4
amirh <amirh@users.noreply.github.com>
2018-07-13 14:08:24 -0700

    Implement PlatformViewsController. (flutter/engine#5722)
    Each platform view created (by a plugin supplied factory) is attached to
    a virtual display.
    The virtual displays are controlled by VirtualDisplayController objects.
    The PlatformViewsController maintains a mapping from a platform view's
    id to its VirtualDisplayController, which allows it to operate on the
    virtual display for a given platform view ID when asked so over the
    method channel.
    
    This is using API level 20 APIs, on lower API levels all platform views
    method channel calls are noops.
    We can make this work on API 19 with some refactoring to the
    TextureRegistry (allow the engine Java code to recycle a texture entry
    id).
    
    This CL also adds a platform view id parameter to the
    PlatformViewFactory#create() method. This allows plugins to route
    platform channel messages to specific instances of a platform view.
    
    TBD in future CLs:
      * Forward touch events to the platform views.
      * Support accessibility for platform views.
    
    flutter/flutter#19030

---
+++ engine/src/flutter/shell/platform/android/io/flutter/plugin/platform/SingleViewPresentation.java
@@ -0,0 +1,64 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package io.flutter.plugin.platform;
+
+import android.annotation.TargetApi;
+import android.app.Presentation;
+import android.content.Context;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.Display;
+import android.widget.FrameLayout;
+
+@TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
+class SingleViewPresentation extends Presentation {
+    private final PlatformViewFactory mViewFactory;
+
+    private PlatformView mView;
+    private int mViewId;
+
+    // As the root view of a display cannot be detached, we use this mContainer
+    // as the root, and attach mView to it. This allows us to detach mView.
+    private FrameLayout mContainer;
+
+    /**
+     * Creates a presentation that will use the view factory to create a new
+     * platform view in the presentation's onCreate, and attach it.
+     */
+    public SingleViewPresentation(Context outerContext, Display display, PlatformViewFactory viewFactory, int viewId) {
+        super(outerContext, display);
+        mViewFactory = viewFactory;
+        mViewId = viewId;
+    }
+
+    /**
+     * Creates a presentation that will attach an already existing view as
+     * its root view.
+     *
+     * <p>The display's density must match the density of the context used
+     * when the view was created.
+     */
+    public SingleViewPresentation(Context outerContext, Display display, PlatformView view) {
+        super(outerContext, display);
+        mViewFactory = null;
+        mView = view;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (mView == null) {
+            mView = mViewFactory.create(getContext(), mViewId);
+        }
+        mContainer = new FrameLayout(getContext());
+        mContainer.addView(mView.getView());
+        setContentView(mContainer);
+    }
+
+    public PlatformView detachView() {
+        mContainer.removeView(mView.getView());
+        return mView;
+    }
+}
