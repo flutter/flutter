b981ad556623f21754f5aba0d21a5625614ffaef
Natalie Sampsell <nsampsell@google.com>
2018-07-16 13:34:42 -0700

    Changed title

--- examples/flutter_gallery/lib/demo/cupertino/cupertino_segmented_control_demo.dart
+++ examples/flutter_gallery/lib/demo/cupertino/cupertino_segmented_control_demo.dart
@@ -56,7 +56,7 @@ class _CupertinoSegmentedControlDemoState extends State<CupertinoSegmentedContro
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        title: const Text('Cupertino Segmented Control'),
+        title: const Text('Segmented Control'),
       ),
       body: new Column(
         children: <Widget>[
