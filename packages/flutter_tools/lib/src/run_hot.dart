59a3acc554c445a342b7523c967aff94ae63fe5c
Danny Tuppeny <dantup@google.com>
2018-07-16 08:33:12 +0100

    Change some Completers to void to fix Dart 2 issues

--- packages/flutter_tools/lib/src/run_hot.dart
+++ packages/flutter_tools/lib/src/run_hot.dart
@@ -235,7 +235,7 @@ class HotRunner extends ResidentRunner {
   @override
   Future<int> run({
     Completer<DebugConnectionInfo> connectionInfoCompleter,
-    Completer<Null> appStartedCompleter,
+    Completer<void> appStartedCompleter,
     String route,
     bool shouldBuild = true
   }) async {
