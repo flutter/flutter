535430d2c40d07cd1bcbff0bd142545ab97eb528
Amir Hardon <amirha@google.com>
2018-07-18 15:24:05 -0700

    Skip flaky test (expression_evaluation_test)

--- packages/flutter_tools/test/integration/expression_evaluation_test.dart
+++ packages/flutter_tools/test/integration/expression_evaluation_test.dart
@@ -110,6 +110,8 @@ void main() {
       await breakInBuildMethod(_flutter);
       await evaluateComplexReturningExpressions();
     });
-  // https://github.com/flutter/flutter/issues/17833
-  }, timeout: const Timeout.factor(3), skip: platform.isWindows);
+    // https://github.com/flutter/flutter/issues/17833
+    // The test appears to be flaky and time out some times, skipping while
+    // investigation is ongoing: https://github.com/flutter/flutter/issues/19542
+  }, timeout: const Timeout.factor(3), skip: true);
 }
