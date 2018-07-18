98fc707abcaaab5664e0c891e4d3d9966ef3b4fd
Ryan Macnak <rmacnak@google.com>
2018-07-18 13:18:05 -0700

    Revert "Fix Dart 2 reload when running from a snapshot instead of platform.dill. (#5792)" (flutter/engine#5795)
    This reverts commit fd1656945ba40e7389f23998446e9ec8e9a4e10c.

--- engine/src/flutter/runtime/dart_isolate.cc
+++ engine/src/flutter/runtime/dart_isolate.cc
@@ -720,16 +720,6 @@ DartIsolate::CreateDartVMAndEmbedderObjectPair(
     );
   }
 
-  // TODO(rmacnak): This flag setting business preserves a side effect of using
-  // Dart_CreateIsolateFromKernel. It should be removed when some of the
-  // internal logic in reload no longer uses this flag.
-  Dart_IsolateFlags nonnull_flags;
-  if (flags == nullptr) {
-    Dart_IsolateFlagsInitialize(&nonnull_flags);
-    flags = &nonnull_flags;
-  }
-  flags->use_dart_frontend = true;
-
   // Create the Dart VM isolate and give it the embedder object as the baton.
   Dart_Isolate isolate =
       (vm->GetPlatformKernel().GetSize() > 0)
