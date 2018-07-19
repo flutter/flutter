ea47c395eb9f35d070220bf6f6be2b15bfb2ef49
Dave Shuckerow <djshuckerow@google.com>
2018-07-19 13:39:16 -0700

    Reduce the haptic feedback on the draggable widget.

--- packages/flutter/lib/src/widgets/drag_target.dart
+++ packages/flutter/lib/src/widgets/drag_target.dart
@@ -290,7 +290,7 @@ class LongPressDraggable<T> extends Draggable<T> {
       ..onStart = (Offset position) {
         final Drag result = onStart(position);
         if (result != null)
-          HapticFeedback.vibrate();
+          HapticFeedback.selectionClick();
         return result;
       };
   }
