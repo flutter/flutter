44ef32d658979876f49c6640c0dc77091a95e2eb
Tian Lun Lee <tianlun@google.com>
2018-07-19 16:35:59 -0400

    Add build test for Flutter codelabs

--- dev/bots/travis_script.sh
+++ dev/bots/travis_script.sh
@@ -59,5 +59,9 @@ elif [ "$SHARD" = "docs" ]; then
     ./dev/bots/docs.sh
   fi
 else
-  TEST_COMMIT_RANGE="${TRAVIS_COMMIT_RANGE}" dart ./dev/bots/test.dart
+  if [ "$SHARD" = "flutter_build_test" ]; then
+    ./dev/bots/flutter_build_test.sh
+  else
+    TEST_COMMIT_RANGE="${TRAVIS_COMMIT_RANGE}" dart ./dev/bots/test.dart
+  fi
 fi
