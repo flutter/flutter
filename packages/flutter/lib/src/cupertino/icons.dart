e5d3a6121d4a01ad398eb88be5d80efd9e0ffe68
creativecreatorormaybenot <19204050+creativecreatorormaybenot@users.noreply.github.com>
2018-07-21 16:23:22 +0200

    Added 16 icons
    Names inspired by existing names from the Icons class for Material design.

--- packages/flutter/lib/src/cupertino/icons.dart
+++ packages/flutter/lib/src/cupertino/icons.dart
@@ -143,4 +143,52 @@ class CupertinoIcons {
 
   /// Two right-facing intertwined arrows.
   static const IconData shuffle_thick = const IconData(0xf221, fontFamily: iconFont, fontPackage: iconFontPackage);
+
+  /// Symbolizes a photo camera
+  static const IconData photo_camera = const IconData(0xf3f5, fontFamily: iconFont, fontPackage: iconFontPackage);
+
+  /// Solid [photo_camera]
+  static const IconData photo_camera_solid = const IconData(0xf3f6, fontFamily: iconFont, fontPackage: iconFontPackage);
+
+  /// Symbolizes a video camera
+  static const IconData video_camera = const IconData(0xf4cc, fontFamily: iconFont, fontPackage: iconFontPackage);
+
+  /// Solid [video_camera]
+  static const IconData video_camera_solid = const IconData(0xf4cd, fontFamily: iconFont, fontPackage: iconFontPackage);
+
+  /// Camera filled with two circular arrows, which indicate switching
+  static const IconData switch_camera = const IconData(0xf49e, fontFamily: iconFont, fontPackage: iconFontPackage);
+
+  /// Solid [switch_camera]
+  static const IconData switch_camera_solid = const IconData(0xf49f, fontFamily: iconFont, fontPackage: iconFontPackage);
+
+  /// Envelopes stacked facing forwards
+  static const IconData collections = const IconData(0xf3c9, fontFamily: iconFont, fontPackage: iconFontPackage);
+
+  /// Solid [collections]
+  static const IconData collections_solid = const IconData(0xf3ca, fontFamily: iconFont, fontPackage: iconFontPackage);
+
+  /// Single envelope, i.e. a folder
+  static const IconData folder = const IconData(0xf434, fontFamily: iconFont, fontPackage: iconFontPackage);
+
+  /// Solid [folder]
+  static const IconData folder_solid = const IconData(0xf435, fontFamily: iconFont, fontPackage: iconFontPackage);
+
+  /// Old version of [folder]
+  static const IconData folder_open = const IconData(0xf38a, fontFamily: iconFont, fontPackage: iconFontPackage);
+
+  /// Trash can
+  static const IconData delete = const IconData(0xf4c4, fontFamily: iconFont, fontPackage: iconFontPackage);
+
+  /// Solid [delete]
+  static const IconData delete_solid = const IconData(0xf4c5, fontFamily: iconFont, fontPackage: iconFontPackage);
+
+  /// Old version of [delete]
+  static const IconData delete_old = const IconData(0xf37f, fontFamily: iconFont, fontPackage: iconFontPackage);
+
+  /// A pen (pencil [create_simple])
+  static const IconData create = const IconData(0xf2bf, fontFamily: iconFont, fontPackage: iconFontPackage);
+
+  /// A pencil (pen [create]), simple because the pencil has less detail, i.e. a more minimal design
+  static const IconData create_simple = const IconData(0xf37e, fontFamily: iconFont, fontPackage: iconFontPackage);
 }
