8c9eac22a9c5a55639b9b7f9cbc832f8292f774a
Danny Tuppeny <danny@tuppeny.com>
2018-07-25 15:31:57 +0100

    Look up breakpoints rather than relying on magic numbers (#19708)
    * Look up breakpoints rather than relying on magic numbers
    
    * Make breakpoint comment more obvious

--- packages/flutter_tools/test/integration/test_data/test_project.dart
+++ packages/flutter_tools/test/integration/test_data/test_project.dart
@@ -16,8 +16,8 @@ abstract class TestProject {
   String get main;
 
   // Valid locations for a breakpoint for tests that just need to break somewhere.
-  String get breakpointFile;
-  int get breakpointLine;
+  String get breakpointFile => fs.path.join(dir.path, 'lib', 'main.dart');
+  int get breakpointLine => lineContaining(main, '// BREAKPOINT');
 
   Future<void> setUpIn(Directory dir) async {
     this.dir = dir;
@@ -28,5 +28,12 @@ abstract class TestProject {
 
   void cleanup() {
     dir?.deleteSync(recursive: true);
+  }
+
+  int lineContaining(String contents, String search) {
+    final int index = contents.split('\n').indexWhere((String l) => l.contains(search));
+    if (index == -1)
+      throw new Exception("Did not find '$search' inside the file");
+    return index;
   }
 }
