70791c308734e92383585d884eabaa8be6a2e63a
Leaf Petersen <leafpetersen@users.noreply.github.com>
2018-07-26 12:23:24 -0700

    Roll pubspec forward to new json_schema, matcher and vector_math (#19802)
    Pull in new versions of packages with fixes for SCREAMING CAPS CONSTANTS.

--- packages/flutter_tools/lib/src/commands/update_packages.dart
+++ packages/flutter_tools/lib/src/commands/update_packages.dart
@@ -28,7 +28,7 @@ import '../runner/flutter_command.dart';
 const Map<String, String> _kManuallyPinnedDependencies = <String, String>{
   // Add pinned packages here.
   'mockito': '3.0.0-beta', // TODO(aam): https://github.com/dart-lang/mockito/issues/110
-  'matcher': '0.12.2', // TODO(ianh): https://github.com/flutter/flutter/issues/18608, https://github.com/dart-lang/matcher/pull/88
+  'matcher': '0.12.2+1', // TODO(ianh): https://github.com/flutter/flutter/issues/18608, https://github.com/dart-lang/matcher/pull/88
   'flutter_gallery_assets': '0.1.4', // See //examples/flutter_gallery/pubspec.yaml
 };
 
