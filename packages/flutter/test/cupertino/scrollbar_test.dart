13e36e8a4cf5cf4f2f5645d64bfb1173c2efd106
Natalie Sampsell <nsampsell@google.com>
2018-08-02 13:38:15 -0700

    Working on scrollbar tests

--- packages/flutter/test/cupertino/scrollbar_test.dart
+++ packages/flutter/test/cupertino/scrollbar_test.dart
@@ -42,4 +42,22 @@ void main() {
       color: const Color(0x15777777),
     ));
   });
+
+  testWidgets('Scrollbar is not smaller than minLength with large scroll views', (WidgetTester tester) async {
+    await tester.pumpWidget(new Directionality(
+      textDirection: TextDirection.ltr,
+      child: new CupertinoScrollbar(
+        child: new SingleChildScrollView(
+          child: const SizedBox(width: 4000.0, height: 20000.0),
+        ),
+      ),
+    ));
+    final TestGesture gesture = await tester.startGesture(tester.getCenter(find.byType(SingleChildScrollView)));
+    await gesture.moveBy(const Offset(0.0, -10.0));
+    await tester.pump();
+    await tester.pump(const Duration(milliseconds: 500));
+    expect(find.byType(CupertinoScrollbar), paints..rrect(
+      rrect: RRect.fromLTRBR(795.0, 4.3, 797.5, 40.3, Radius.circular(1.3)),
+    ));
+  });
 }
