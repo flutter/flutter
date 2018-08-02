6ebaadcb02ed1b4e7a1d79548a1be47b20f83f71
Yuqian Li <liyuqian@google.com>
2018-08-02 15:08:05 -0700

    Fix import

--- packages/flutter/lib/src/painting/clip.dart
+++ packages/flutter/lib/src/painting/clip.dart
@@ -2,9 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'dart:ui' show Clip;
-
-import 'package:flutter/painting.dart';
+import 'dart:ui' show Canvas, Clip, Path, Paint, Rect, RRect;
 
 /// Clip utilities used by [PaintingContext] and [TestRecordingPaintingContext].
 abstract class ClipContext {
