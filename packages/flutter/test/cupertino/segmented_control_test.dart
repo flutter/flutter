9196398ec1d49555c524081f041f1241927d0885
Natalie Sampsell <nsampsell@google.com>
2018-08-06 12:49:33 -0700

    Golden tests run on MacOS

--- packages/flutter/test/cupertino/segmented_control_test.dart
+++ packages/flutter/test/cupertino/segmented_control_test.dart
@@ -1311,7 +1311,7 @@ void main() {
       find.byType(RepaintBoundary),
       matchesGoldenFile('segmented_control_test.0.0.png'),
     );
-  }, skip: !Platform.isLinux);
+  }, skip: !Platform.isMacOS);
 
   testWidgets('Golden Test Pressed State', (WidgetTester tester) async {
     final Map<int, Widget> children = <int, Widget>{};
@@ -1349,5 +1349,5 @@ void main() {
       find.byType(RepaintBoundary),
       matchesGoldenFile('segmented_control_test.1.0.png'),
     );
-  }, skip: !Platform.isLinux);
+  }, skip: !Platform.isMacOS);
 }
