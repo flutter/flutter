acadc28e73853d2f541b88b4bb69d5c4ca51b797
Mikkel Nygaard Ravn <mravn@google.com>
2018-08-10 09:56:13 +0200

    Fix typos (#20417)

--- packages/flutter/lib/src/material/material_localizations.dart
+++ packages/flutter/lib/src/material/material_localizations.dart
@@ -138,7 +138,7 @@ abstract class MaterialLocalizations {
   /// Label read out by accessibility tools (TalkBack or VoiceOver) for a modal
   /// barrier to indicate that a tap dismisses the barrier.
   ///
-  /// A modal barrier can for example be found behind a alert or popup to block
+  /// A modal barrier can for example be found behind an alert or popup to block
   /// user interaction with elements behind it.
   String get modalBarrierDismissLabel;
 
