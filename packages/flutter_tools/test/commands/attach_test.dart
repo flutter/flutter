0e9859932de037d669f1192559e1d7089d49c5a2
Alexander Aprelev <aam@google.com>
2018-08-15 12:36:13 -0700

    Fix tests so they use memory file system and can find lib/main.dart

--- packages/flutter_tools/test/commands/attach_test.dart
+++ packages/flutter_tools/test/commands/attach_test.dart
@@ -4,8 +4,10 @@
 
 import 'dart:async';
 
+import 'package:file/memory.dart';
 import 'package:flutter_tools/src/base/common.dart';
 import 'package:flutter_tools/src/base/file_system.dart';
+import 'package:flutter_tools/src/base/platform.dart';
 import 'package:flutter_tools/src/cache.dart';
 import 'package:flutter_tools/src/commands/attach.dart';
 import 'package:flutter_tools/src/device.dart';
@@ -19,8 +21,15 @@ import '../src/mocks.dart';
 
 void main() {
   group('attach', () {
+    final FileSystem testFileSystem = new MemoryFileSystem(
+      style: platform.isWindows ? FileSystemStyle.windows : FileSystemStyle
+          .posix,
+    );
+
     setUpAll(() {
       Cache.disableLocking();
+      testFileSystem.directory('lib').createSync();
+      testFileSystem.file('lib/main.dart').createSync();
     });
 
     testUsingContext('finds observatory port and forwards', () async {
@@ -33,14 +42,17 @@ void main() {
         // Now that the reader is used, start writing messages to it.
         Timer.run(() {
           mockLogReader.addLine('Foo');
-          mockLogReader.addLine('Observatory listening on http://127.0.0.1:$devicePort');
+          mockLogReader.addLine(
+              'Observatory listening on http://127.0.0.1:$devicePort');
         });
 
         return mockLogReader;
       });
       when(device.portForwarder).thenReturn(portForwarder);
-      when(portForwarder.forward(devicePort, hostPort: anyNamed('hostPort'))).thenAnswer((_) async => hostPort);
-      when(portForwarder.forwardedPorts).thenReturn(<ForwardedPort>[new ForwardedPort(hostPort, devicePort)]);
+      when(portForwarder.forward(devicePort, hostPort: anyNamed('hostPort')))
+          .thenAnswer((_) async => hostPort);
+      when(portForwarder.forwardedPorts).thenReturn(
+          <ForwardedPort>[new ForwardedPort(hostPort, devicePort)]);
       when(portForwarder.unforward(any)).thenAnswer((_) async => null);
       testDeviceManager.addDevice(device);
 
@@ -48,10 +60,14 @@ void main() {
 
       await createTestCommandRunner(command).run(<String>['attach']);
 
-      verify(portForwarder.forward(devicePort, hostPort: anyNamed('hostPort'))).called(1);
+      verify(portForwarder.forward(devicePort, hostPort: anyNamed('hostPort')))
+          .called(1);
 
       mockLogReader.dispose();
-    });
+    }, overrides: <Type, Generator>{
+      FileSystem: () => testFileSystem,
+    },
+    );
 
     testUsingContext('selects specified target', () async {
       const int devicePort = 499;
@@ -61,40 +77,45 @@ void main() {
       final MockAndroidDevice device = new MockAndroidDevice();
       final MockHotRunnerFactory mockHotRunnerFactory = new MockHotRunnerFactory();
       when(device.portForwarder).thenReturn(portForwarder);
-      when(portForwarder.forward(devicePort, hostPort: anyNamed('hostPort'))).thenAnswer((_) async => hostPort);
-      when(portForwarder.forwardedPorts).thenReturn(<ForwardedPort>[new ForwardedPort(hostPort, devicePort)]);
+      when(portForwarder.forward(devicePort, hostPort: anyNamed('hostPort')))
+          .thenAnswer((_) async => hostPort);
+      when(portForwarder.forwardedPorts).thenReturn(
+          <ForwardedPort>[new ForwardedPort(hostPort, devicePort)]);
       when(portForwarder.unforward(any)).thenAnswer((_) async => null);
       when(mockHotRunnerFactory.build(any,
           target: anyNamed('target'),
           debuggingOptions: anyNamed('debuggingOptions'),
           packagesFilePath: anyNamed('packagesFilePath'),
-          usesTerminalUI: anyNamed('usesTerminalUI'))).thenReturn(new MockHotRunner());
+          usesTerminalUI: anyNamed('usesTerminalUI'))).thenReturn(
+          new MockHotRunner());
 
       testDeviceManager.addDevice(device);
       when(device.getLogReader()).thenAnswer((_) {
         // Now that the reader is used, start writing messages to it.
         Timer.run(() {
           mockLogReader.addLine('Foo');
-          mockLogReader.addLine('Observatory listening on http://127.0.0.1:$devicePort');
+          mockLogReader.addLine(
+              'Observatory listening on http://127.0.0.1:$devicePort');
         });
 
         return mockLogReader;
       });
-      final Directory temp = fs.systemTempDirectory.createTempSync('flutter_tools');
-      final File foo = fs.file(temp.childFile('foo'));
-      foo.createSync();
+      final File foo = fs.file('lib/foo.dart')
+        ..createSync();
 
-      final AttachCommand command = new AttachCommand(hotRunnerFactory: mockHotRunnerFactory);
-      await createTestCommandRunner(command).run(<String>['attach', '-t', foo.path, '-v']);
+      final AttachCommand command = new AttachCommand(
+          hotRunnerFactory: mockHotRunnerFactory);
+      await createTestCommandRunner(command).run(
+          <String>['attach', '-t', foo.path, '-v']);
 
       verify(mockHotRunnerFactory.build(any,
           target: foo.path,
           debuggingOptions: anyNamed('debuggingOptions'),
           packagesFilePath: anyNamed('packagesFilePath'),
           usesTerminalUI: anyNamed('usesTerminalUI'))).called(1);
-
-      temp.deleteSync(recursive: true);
-    });
+    }, overrides: <Type, Generator>{
+      FileSystem: () => testFileSystem,
+    },);
 
     testUsingContext('forwards to given port', () async {
       const int devicePort = 499;
@@ -104,16 +125,20 @@ void main() {
 
       when(device.portForwarder).thenReturn(portForwarder);
       when(portForwarder.forward(devicePort)).thenAnswer((_) async => hostPort);
-      when(portForwarder.forwardedPorts).thenReturn(<ForwardedPort>[new ForwardedPort(hostPort, devicePort)]);
+      when(portForwarder.forwardedPorts).thenReturn(
+          <ForwardedPort>[new ForwardedPort(hostPort, devicePort)]);
       when(portForwarder.unforward(any)).thenAnswer((_) async => null);
       testDeviceManager.addDevice(device);
 
       final AttachCommand command = new AttachCommand();
 
-      await createTestCommandRunner(command).run(<String>['attach', '--debug-port', '$devicePort']);
+      await createTestCommandRunner(command).run(
+          <String>['attach', '--debug-port', '$devicePort']);
 
       verify(portForwarder.forward(devicePort)).called(1);
-    });
+    }, overrides: <Type, Generator>{
+      FileSystem: () => testFileSystem,
+    },);
 
     testUsingContext('exits when no device connected', () async {
       final AttachCommand command = new AttachCommand();
@@ -122,7 +147,9 @@ void main() {
         throwsA(const isInstanceOf<ToolExit>()),
       );
       expect(testLogger.statusText, contains('No connected devices'));
-    });
+    }, overrides: <Type, Generator>{
+      FileSystem: () => testFileSystem,
+    },);
 
     testUsingContext('exits when multiple devices connected', () async {
       Device aDeviceWithId(String id) {
@@ -144,7 +171,9 @@ void main() {
       expect(testLogger.statusText, contains('More than one device'));
       expect(testLogger.statusText, contains('xx1'));
       expect(testLogger.statusText, contains('yy2'));
-    });
+    }, overrides: <Type, Generator>{
+      FileSystem: () => testFileSystem,
+    },);
   });
 }
 
