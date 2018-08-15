698e0aeb4d0dcb5046b8faab6f419e926bd9ccee
Michael Goderbauer <goderbauer@google.com>
2018-08-14 17:27:26 -0700

    typo

--- bin/flutter.bat
+++ bin/flutter.bat
@@ -4,10 +4,12 @@ REM Use of this source code is governed by a BSD-style license that can be
 REM found in the LICENSE file.
 
 REM This wrapper script copies the actual launch script from internal/ to cache/
-REM and then executes the copy in cache/. Copying is neccessary to prevent
+REM and then executes the copy in cache/. Copying is necessary to prevent
 REM Flutter from modifying the script while it is executing during a
 REM "flutter upgrade" or "flutter channel" command. Modifying the script
 REM mid-execution would cause Windows to throw errors.
+
+SETLOCAL ENABLEDELAYEDEXPANSION
 
 FOR %%i IN ("%~dp0..") DO SET FLUTTER_ROOT=%%~fi
 SET cache_dir=%FLUTTER_ROOT%\bin\cache
