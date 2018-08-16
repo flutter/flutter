0e192c974a936c99b787b52581ff8f173b6de5e2
Mikkel Ravn <mravn@google.com>
2018-08-16 10:49:05 +0200

    Remove flutter_tools test outcome dependency on packages get

--- packages/flutter_tools/test/commands/test_test.dart
+++ packages/flutter_tools/test/commands/test_test.dart
@@ -104,8 +104,10 @@ Future<Null> _testFile(String testName, String workingDirectory, String testDire
 
   expect(exec.exitCode, exitCode);
   final List<String> output = exec.stdout.split('\n');
-  if (output.first == 'Waiting for another flutter command to release the startup lock...')
+  while (output.first == 'Waiting for another flutter command to release the startup lock...'
+      || output.first.startsWith('Running "flutter packages get" in')) {
     output.removeAt(0);
+  }
   output.add('<<stderr>>');
   output.addAll(exec.stderr.split('\n'));
   final List<String> expectations = fs.file(fullTestExpectation).readAsLinesSync();
