c5452480f94751453ea4794de289a763dc890a16
Will Larche <larche@google.com>
2018-08-20 16:47:30 -0400

    [Gallery] More elevations for the elevation demo.

--- examples/flutter_gallery/lib/demo/material/elevation_demo.dart
+++ examples/flutter_gallery/lib/demo/material/elevation_demo.dart
@@ -5,12 +5,23 @@ class ElevationDemo extends StatelessWidget {
 
   List<Widget> buildCards() {
     final List<Widget> cards = <Widget>[];
+    const List<double> elevations = <double>[
+      0.0,
+      1.0,
+      2.0,
+      3.0,
+      4.0,
+      5.0,
+      8.0,
+      16.0,
+      24.0,
+    ];
 
-    for (double i = 0.0; i <= 5.0; ++i) {
+    for (double i in elevations) {
       cards.add(Center(
         child: Card(
           margin: const EdgeInsets.all(20.0),
-          elevation: i,
+          elevation: _shouldShowElevation ? i : 0.0,
           child: SizedBox(
             height: 100.0,
             width: 100.0,
