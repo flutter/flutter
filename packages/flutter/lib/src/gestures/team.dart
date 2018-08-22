80c9740616102cc919c539cadd78b8072383d757
Amir Hardon <amirha@google.com>
2018-08-21 18:10:28 -0700

    review comment followup

--- packages/flutter/lib/src/gestures/team.dart
+++ packages/flutter/lib/src/gestures/team.dart
@@ -113,8 +113,9 @@ class _CombiningGestureArenaMember extends GestureArenaMember {
 /// [AndroidView] uses a team with a captain to decide which gestures are
 /// forwarded to the native view. For example if we want to forward taps and
 /// vertical scrolls to a native Android view, [TapGestureRecognizers] and
-/// [VerticalDragGestureRecognizer] are added to a team with a captain, the
-/// captain allows [AndroidView] to know when any gestures in the team has been
+/// [VerticalDragGestureRecognizer] are added to a team with a captain(the captain is set to be a
+/// gesture recognizer that never explicitly claims the gesture).
+/// The captain allows [AndroidView] to know when any gestures in the team has been
 /// recognized (or all other arena members are out), once the captain wins the
 /// gesture is forwarded to the Android view.
 ///
