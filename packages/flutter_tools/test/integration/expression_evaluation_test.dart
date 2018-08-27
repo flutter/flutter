4466c9db4ccd5d8530f8edbdb5eb4fa02e193c74
Danny Tuppeny <danny@tuppeny.com>
2018-08-27 09:29:07 +0100

    Re-enable integration tests (#20947)
    * Unskip expression evaluation integration tests for non-Windows
    
    * Add additional into to skip comments
    
    * Add missing import

--- packages/flutter_tools/test/integration/expression_evaluation_test.dart
+++ packages/flutter_tools/test/integration/expression_evaluation_test.dart
@@ -6,6 +6,7 @@ import 'dart:async';
 
 import 'package:file/file.dart';
 import 'package:flutter_tools/src/base/file_system.dart';
+import 'package:flutter_tools/src/base/platform.dart';
 
 import 'package:vm_service_client/vm_service_client.dart';
 
@@ -107,8 +108,7 @@ void main() {
       await breakInBuildMethod(_flutter);
       await evaluateComplexReturningExpressions();
     });
-    // https://github.com/flutter/flutter/issues/17833
-    // The test appears to be flaky and time out some times, skipping while
-    // investigation is ongoing: https://github.com/flutter/flutter/issues/19542
-  }, timeout: const Timeout.factor(6), skip: true);
+    // TODO(dantup): Unskip after flutter-tester is fixed on Windows:
+    // https://github.com/flutter/flutter/issues/17833.
+  }, timeout: const Timeout.factor(6), skip: platform.isWindows);
 }
