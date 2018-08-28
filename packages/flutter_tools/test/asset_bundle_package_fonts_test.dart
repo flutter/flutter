7a92b0b67f110925b66530c9090cf03bfd9c959a
Danny Tuppeny <dantup@google.com>
2018-08-28 13:46:59 +0100

    Change assert_bundle_package_font_test to memory file system
    This is to work towards being able to run the tests without `-j1` (#21113). These tests were using the real filesystem and setting/relying on fs.currentDirectory. There was a comment about this being because the memory provider didnt' support POSIX and Windows, however that seems to have changed since (and many other asset tests already do something similar to this).

--- packages/flutter_tools/test/asset_bundle_package_fonts_test.dart
+++ packages/flutter_tools/test/asset_bundle_package_fonts_test.dart
@@ -6,15 +6,18 @@ import 'dart:async';
 import 'dart:convert';
 
 import 'package:file/file.dart';
+import 'package:file/memory.dart';
 
 import 'package:flutter_tools/src/asset.dart';
 import 'package:flutter_tools/src/base/file_system.dart';
+import 'package:flutter_tools/src/base/platform.dart';
 import 'package:flutter_tools/src/cache.dart';
+import 'package:flutter_tools/src/flutter_manifest.dart';
 
 import 'src/common.dart';
 import 'src/context.dart';
 
-void main() {
+void main() {  
   void writePubspecFile(String path, String name, {String fontsSection}) {
     if (fontsSection == null) {
       fontsSection = '';
@@ -89,25 +92,38 @@ $fontsSection
       ..writeAsStringSync(font);
   }
 
-  // These tests do not use a memory file system because we want to ensure that
-  // asset bundles work correctly on Windows and Posix systems.
-  Directory tempDir;
-  Directory oldCurrentDir;
+  // TODO(dantup): This function exists in many tests, is there somewhere appropriate
+  // to put a single copy and call from tests that need it?
+  void writeSchemaFile(FileSystem filesystem, String schemaData) {
+    final String schemaPath = buildSchemaPath(filesystem);
+    final File schemaFile = filesystem.file(schemaPath);
 
-  setUp(() async {
-    tempDir = fs.systemTempDirectory.createTempSync('flutter_asset_bundle_test.');
-    oldCurrentDir = fs.currentDirectory;
-    fs.currentDirectory = tempDir;
-  });
+    final String schemaDir = buildSchemaDir(filesystem);
 
-  tearDown(() {
-    fs.currentDirectory = oldCurrentDir;
-    tryToDelete(tempDir);
-  });
+    filesystem.directory(schemaDir).createSync(recursive: true);
+    filesystem.file(schemaFile).writeAsStringSync(schemaData);
+  }
+
+  void writeBasicSchemaFile() {
+    // Write schemaData otherwise pubspec.yaml file can't be loaded.
+      writeSchemaFile(fs, '{}');
+  }
 
   group('AssetBundle fonts from packages', () {
+    FileSystem testFileSystem;
+
+    setUp(() async {
+      testFileSystem = new MemoryFileSystem(
+        style: platform.isWindows
+          ? FileSystemStyle.windows
+          : FileSystemStyle.posix,
+      );
+      testFileSystem.currentDirectory = testFileSystem.systemTempDirectory.createTempSync('flutter_asset_bundle_test.');
+    });
+
     testUsingContext('App includes neither font manifest nor fonts when no defines fonts', () async {
       establishFlutterRoot();
+      writeBasicSchemaFile();
 
       writePubspecFile('pubspec.yaml', 'test');
       writePackagesFile('test_package:p/p/lib/');
@@ -117,10 +133,13 @@ $fontsSection
       await bundle.build(manifestPath: 'pubspec.yaml');
       expect(bundle.entries.length, 3); // LICENSE, AssetManifest, FontManifest
       expect(bundle.entries.containsKey('FontManifest.json'), isTrue);
+    }, overrides: <Type, Generator>{
+      FileSystem: () => testFileSystem,
     });
 
     testUsingContext('App font uses font file from package', () async {
       establishFlutterRoot();
+      writeBasicSchemaFile();
 
       const String fontsSection = '''
        - family: foo
@@ -142,10 +161,13 @@ $fontsSection
         <String>['test_package'],
         expectedFontManifest,
       );
+    }, overrides: <Type, Generator>{
+      FileSystem: () => testFileSystem,
     });
 
     testUsingContext('App font uses local font file and package font file', () async {
       establishFlutterRoot();
+      writeBasicSchemaFile();
 
       const String fontsSection = '''
        - family: foo
@@ -171,10 +193,13 @@ $fontsSection
         <String>['test_package'],
         expectedFontManifest,
       );
+    }, overrides: <Type, Generator>{
+      FileSystem: () => testFileSystem,
     });
 
     testUsingContext('App uses package font with own font file', () async {
       establishFlutterRoot();
+      writeBasicSchemaFile();
 
       writePubspecFile('pubspec.yaml', 'test');
       writePackagesFile('test_package:p/p/lib/');
@@ -201,10 +226,13 @@ $fontsSection
         <String>['test_package'],
         expectedFontManifest,
       );
+    }, overrides: <Type, Generator>{
+      FileSystem: () => testFileSystem,
     });
 
     testUsingContext('App uses package font with font file from another package', () async {
       establishFlutterRoot();
+      writeBasicSchemaFile();
 
       writePubspecFile('pubspec.yaml', 'test');
       writePackagesFile('test_package:p/p/lib/\ntest_package2:p2/p/lib/');
@@ -232,10 +260,13 @@ $fontsSection
         <String>['test_package2'],
         expectedFontManifest,
       );
+    }, overrides: <Type, Generator>{
+      FileSystem: () => testFileSystem,
     });
 
     testUsingContext('App uses package font with properties and own font file', () async {
       establishFlutterRoot();
+      writeBasicSchemaFile();
 
       writePubspecFile('pubspec.yaml', 'test');
       writePackagesFile('test_package:p/p/lib/');
@@ -264,10 +295,13 @@ $fontsSection
         <String>['test_package'],
         expectedFontManifest,
       );
+    }, overrides: <Type, Generator>{
+      FileSystem: () => testFileSystem,
     });
 
     testUsingContext('App uses local font and package font with own font file.', () async {
       establishFlutterRoot();
+      writeBasicSchemaFile();
 
       const String fontsSection = '''
        - family: foo
@@ -300,6 +334,8 @@ $fontsSection
         <String>['test_package'],
         expectedFontManifest,
       );
+    }, overrides: <Type, Generator>{
+      FileSystem: () => testFileSystem,
     });
   });
 }
