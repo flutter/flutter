37b01e1f43ca1bd237fed0d8203c63c8afc1b534
johnsonmh <michaelhenry.johnson@gmail.com>
2018-08-28 11:29:34 -0400

    put "new" keywords back in

--- packages/flutter/lib/src/material/floating_action_button.dart
+++ packages/flutter/lib/src/material/floating_action_button.dart
@@ -259,15 +259,15 @@ class _FloatingActionButtonState extends State<FloatingActionButton> {
     );
 
     if (widget.tooltip != null) {
-      final Widget tooltip = MergeSemantics(
-        child: Tooltip(
+      final Widget tooltip = new MergeSemantics(
+        child: new Tooltip(
           message: widget.tooltip,
           child: result,
         ),
       );
       // The long-pressable area for the tooltip should always be as big as
       // the tooltip even if there is no child.
-      result = widget.child != null ? tooltip : SizedBox.expand(child: tooltip);
+      result = widget.child != null ? tooltip : new SizedBox.expand(child: tooltip);
     }
 
     if (widget.heroTag != null) {
