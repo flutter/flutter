4ffd916961d9e78739cb9050311e33b9a30975d5
Alexander Aprelev <aam@google.com>
2018-08-28 20:05:01 -0700

    Add const

--- packages/flutter/lib/src/widgets/single_child_scroll_view.dart
+++ packages/flutter/lib/src/widgets/single_child_scroll_view.dart
@@ -183,7 +183,7 @@ import 'scrollable.dart';
 /// * [Scrollable], which handles arbitrary scrolling effects.
 class SingleChildScrollView extends StatelessWidget {
   /// Creates a box in which a single widget can be scrolled.
-  SingleChildScrollView({
+  const SingleChildScrollView({
     Key key,
     this.scrollDirection = Axis.vertical,
     this.reverse = false,
