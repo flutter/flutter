605c146d029d4344468e714bfedf3c3f841adc45
Michael Klimushyn <michael@mklim.xyz>
2018-09-05 10:44:02 -0700

    Enable transparent FlutterViews (flutter/engine#6163)
    - Changes rasterizer to clear the canvas with `SkColor_TRANSPARENT`
    - Removes the unused `backgroundColor` param from `FlutterView.java`

--- engine/src/flutter/shell/platform/android/platform_view_android_jni.cc
+++ engine/src/flutter/shell/platform/android/platform_view_android_jni.cc
@@ -176,8 +176,7 @@ static jstring GetObservatoryUri(JNIEnv* env, jclass clazz) {
 static void SurfaceCreated(JNIEnv* env,
                            jobject jcaller,
                            jlong shell_holder,
-                           jobject jsurface,
-                           jint backgroundColor) {
+                           jobject jsurface) {
   // Note: This frame ensures that any local references used by
   // ANativeWindow_fromSurface are released immediately. This is needed as a
   // workaround for https://code.google.com/p/android/issues/detail?id=68174
@@ -638,7 +637,7 @@ bool PlatformViewAndroid::Register(JNIEnv* env) {
   static const JNINativeMethod view_methods[] = {
       {
           .name = "nativeSurfaceCreated",
-          .signature = "(JLandroid/view/Surface;I)V",
+          .signature = "(JLandroid/view/Surface;)V",
           .fnPtr = reinterpret_cast<void*>(&shell::SurfaceCreated),
       },
       {
