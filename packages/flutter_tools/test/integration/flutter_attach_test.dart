2e199656eb2e9a1e4ef92608b488f85c62cd533d
Jonah Williams <jonahwilliams@google.com>
2018-09-05 18:03:43 -0700

    Revert "Add a detach command to detach without terminating" (#21464)

--- packages/flutter_tools/test/integration/flutter_attach_test.dart
+++ packages/flutter_tools/test/integration/flutter_attach_test.dart
@@ -6,6 +6,7 @@ import 'package:file/file.dart';
 import 'package:flutter_tools/src/base/file_system.dart';
 
 import '../src/common.dart';
+import '../src/context.dart';
 import 'test_data/basic_project.dart';
 import 'test_driver.dart';
 
@@ -17,34 +18,22 @@ void main() {
   setUp(() async {
     tempDir = fs.systemTempDirectory.createTempSync('flutter_attach_test.');
     await _project.setUpIn(tempDir);
-    _flutterRun = new FlutterTestDriver(tempDir, logPrefix: 'RUN');
-    _flutterAttach = new FlutterTestDriver(tempDir, logPrefix: 'ATTACH');
+    _flutterRun = new FlutterTestDriver(tempDir);
+    _flutterAttach = new FlutterTestDriver(tempDir);
   });
 
   tearDown(() async {
-    await _flutterAttach.detach();
+    // We can't call stop() on both of these because they'll both try to stop the
+    // same app. Just quit the attach process and then send a stop to the original
+    // process.
     await _flutterRun.stop();
+    await _flutterAttach.quit();
     tryToDelete(tempDir);
   });
 
   group('attached process', () {
-    test('can hot reload', () async {
+    testUsingContext('can hot reload', () async {
       await _flutterRun.run(withDebugger: true);
-      await _flutterAttach.attach(_flutterRun.vmServicePort);
-      await _flutterAttach.hotReload();
-    });
-    test('can detach, reattach, hot reload', () async {
-      await _flutterRun.run(withDebugger: true);
-      await _flutterAttach.attach(_flutterRun.vmServicePort);
-      await _flutterAttach.detach();
-      await _flutterAttach.attach(_flutterRun.vmServicePort);
-      await _flutterAttach.hotReload();
-    });
-    test('killing process behaves the same as detach ', () async {
-      await _flutterRun.run(withDebugger: true);
-      await _flutterAttach.attach(_flutterRun.vmServicePort);
-      await _flutterAttach.quit();
-      _flutterAttach = new FlutterTestDriver(tempDir, logPrefix: 'ATTACH-2');
       await _flutterAttach.attach(_flutterRun.vmServicePort);
       await _flutterAttach.hotReload();
     });
