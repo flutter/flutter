11943ce7e76cb6212221c2c56752d42beaf1e9ed
Jonah Williams <jonahwilliams@google.com>
2018-09-07 10:36:52 -0700

    Revert "Shortcuts (#21083)" (#21556)
    This reverts commit 6e7f3e6f5a11341edd5c2519cbe1c7bc178cb8e9.

--- packages/flutter/lib/src/services/text_input.dart
+++ packages/flutter/lib/src/services/text_input.dart
@@ -534,23 +534,6 @@ class TextEditingValue {
   );
 }
 
-/// An interface for manipulating the selection, to be used by the implementor
-/// of the toolbar widget.
-abstract class TextSelectionDelegate {
-  /// Gets the current text input.
-  TextEditingValue get textEditingValue;
-
-  /// Sets the current text input (replaces the whole line).
-  set textEditingValue(TextEditingValue value);
-
-  /// Hides the text selection toolbar.
-  void hideToolbar();
-
-  /// Brings the provided [TextPosition] into the visible area of the text
-  /// input.
-  void bringIntoView(TextPosition position);
-}
-
 /// An interface to receive information from [TextInput].
 ///
 /// See also:
