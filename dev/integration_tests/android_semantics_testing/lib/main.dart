92b7ab50af69cf57a478fa3a00506677ecb44c30
Jonah Williams <jonahwilliams@google.com>
2018-09-11 09:39:41 -0700

    Add text field to semantics integration test (#21665)

--- dev/integration_tests/android_semantics_testing/lib/main.dart
+++ dev/integration_tests/android_semantics_testing/lib/main.dart
@@ -6,12 +6,15 @@ import 'dart:async';
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_driver/driver_extension.dart';
 
 import 'src/tests/controls_page.dart';
+import 'src/tests/text_field_page.dart';
 
 void main() {
+  timeDilation = 0.05; // remove animations.
   enableFlutterDriverExtension(handler: dataHandler);
   runApp(const TestApp());
 }
@@ -20,17 +23,26 @@ const MethodChannel kSemanticsChannel = MethodChannel('semantics');
 
 Future<String> dataHandler(String message) async {
   if (message.contains('getSemanticsNode')) {
+    final Completer<String> completer = new Completer<String>();
     final int id = int.tryParse(message.split('#')[1]) ?? 0;
-    final dynamic result = await kSemanticsChannel.invokeMethod('getSemanticsNode', <String, dynamic>{
-      'id': id,
-    });
-    return json.encode(result);
+    Future<void> completeSemantics([Object _]) async {
+      final dynamic result = await kSemanticsChannel.invokeMethod('getSemanticsNode', <String, dynamic>{
+        'id': id,
+      });
+      completer.complete(json.encode(result));
+    }
+    if (SchedulerBinding.instance.hasScheduledFrame)
+      SchedulerBinding.instance.addPostFrameCallback(completeSemantics);
+    else
+      completeSemantics();
+    return completer.future;
   }
   throw new UnimplementedError();
 }
 
 const List<String> routes = <String>[
   selectionControlsRoute,
+  textFieldRoute,
 ];
 
 class TestApp extends StatelessWidget {
@@ -41,11 +53,12 @@ class TestApp extends StatelessWidget {
     return new MaterialApp(
       routes: <String, WidgetBuilder>{
         selectionControlsRoute: (BuildContext context) => new SelectionControlsPage(),
+        textFieldRoute: (BuildContext context) => new TextFieldPage(),
       },
       home: new Builder(
         builder: (BuildContext context) {
           return new Scaffold(
-            body: new Column(
+            body: new ListView(
               children: routes.map((String value) {
                 return new MaterialButton(
                   child: new Text(value),
