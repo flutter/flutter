a527219a1c73259e93743288fcee208db47a1c52
Alexander Aprelev <aam@google.com>
2018-09-12 16:29:54 -0700

    Parallelize compilation in flutter_platform test setup.

--- packages/flutter_tools/lib/src/test/flutter_platform.dart
+++ packages/flutter_tools/lib/src/test/flutter_platform.dart
@@ -4,6 +4,8 @@
 
 import 'dart:async';
 import 'dart:convert';
+import 'dart:io' show Platform;
+import 'dart:math' show max;
 
 import 'package:meta/meta.dart';
 import 'package:stream_channel/stream_channel.dart';
@@ -207,9 +209,6 @@ class _Compiler {
     // for independent execution.
     final Directory outputDillDirectory = fs.systemTempDirectory
         .createTempSync('flutter_test_compiler.');
-    final File outputDill = outputDillDirectory.childFile('output.dill');
-
-    printTrace('Compiler will use the following file as its incremental dill file: ${outputDill.path}');
 
     bool suppressOutput = false;
     void reportCompilerMessage(String message) {
@@ -245,52 +244,69 @@ class _Compiler {
       // Only trigger processing if queue was empty - i.e. no other requests
       // are currently being processed. This effectively enforces "one
       // compilation request at a time".
-      if (isEmpty) {
-        while (compilationQueue.isNotEmpty) {
-          final _CompilationRequest request = compilationQueue.first;
-          printTrace('Compiling ${request.path}');
-          final Stopwatch compilerTime = new Stopwatch()..start();
-          bool firstCompile = false;
-          if (compiler == null) {
-            compiler = createCompiler();
-            firstCompile = true;
-          }
-          suppressOutput = false;
-          final CompilerOutput compilerOutput = await handleTimeout<CompilerOutput>(
-              compiler.recompile(
-                  request.path,
-                  <String>[request.path],
-                  outputPath: outputDill.path),
-              request.path);
-          final String outputPath = compilerOutput?.outputFilename;
-
-          // In case compiler didn't produce output or reported compilation
-          // errors, pass [null] upwards to the consumer and shutdown the
-          // compiler to avoid reusing compiler that might have gotten into
-          // a weird state.
-          if (outputPath == null || compilerOutput.errorCount > 0) {
-            request.result.complete(null);
-            await _shutdown();
-          } else {
-            final File outputFile = fs.file(outputPath);
-            final File kernelReadyToRun = await outputFile.copy('${request.path}.dill');
-            final File testCache = fs.file(testFilePath);
-            if (firstCompile || !testCache.existsSync() || (testCache.lengthSync() < outputFile.lengthSync())) {
-              // The idea is to keep the cache file up-to-date and include as
-              // much as possible in an effort to re-use as many packages as
-              // possible.
-              ensureDirectoryExists(testFilePath);
-              await outputFile.copy(testFilePath);
-            }
-            request.result.complete(kernelReadyToRun.path);
-            compiler.accept();
-            compiler.reset();
-          }
-          printTrace('Compiling ${request.path} took ${compilerTime.elapsedMilliseconds}ms');
-          // Only remove now when we finished processing the element
-          compilationQueue.removeAt(0);
-        }
+      ResidentCompiler compiler;
+      String outputDill;
+      if (idleCompilers.isEmpty && compilers < max(1, Platform.numberOfProcessors - 2) /* concurrency */) {
+        // Create compiler
+        compiler = new ResidentCompiler(
+            artifacts.getArtifactPath(Artifact.flutterPatchedSdkPath),
+            packagesPath: PackageMap.globalPackagesPath,
+            trackWidgetCreation: trackWidgetCreation);
+        compilers++;
+        outputDill = outputDillDirectory.childFile('output_$compilers.dill').path;
+        printTrace('Compiler will use the following file as its incremental dill file: ${outputDill}');
+      } else if (idleCompilers.isEmpty) {
+        // The request was added to the queue, but we can't process it now.
+        return;
+      } else {
+        compiler = idleCompilers.removeLast();
       }
+      while (compilationQueue.isNotEmpty) {
+        final _CompilationRequest request = compilationQueue.first;
+        // Only remove now when we finished processing the element
+        compilationQueue.removeAt(0);
+        printTrace('Compiling ${request.path}');
+        final Stopwatch compilerTime = new Stopwatch()..start();
+        bool firstCompile = false;
+        if (compiler == null) {
+          compiler = createCompiler();
+          firstCompile = true;
+        }
+        suppressOutput = false;
+        final CompilerOutput compilerOutput = await handleTimeout<CompilerOutput>(
+            compiler.recompile(
+                request.path,
+                <String>[request.path],
+                outputPath: outputDill),
+            request.path);
+        final String outputPath = compilerOutput?.outputFilename;
+
+        // In case compiler didn't produce output or reported compilation
+        // errors, pass [null] upwards to the consumer and shutdown the
+        // compiler to avoid reusing compiler that might have gotten into
+        // a weird state.
+        if (outputPath == null || compilerOutput.errorCount > 0) {
+          request.result.complete(null);
+          await _shutdown(compiler);
+        } else {
+          final File outputFile = fs.file(outputPath);
+          final File kernelReadyToRun = await outputFile.copy('${request.path}.dill');
+          final File testCache = fs.file(testFilePath);
+          if (firstCompile || !testCache.existsSync() || (testCache.lengthSync() < outputFile.lengthSync())) {
+            // The idea is to keep the cache file up-to-date and include as
+            // much as possible in an effort to re-use as many packages as
+            // possible.
+            ensureDirectoryExists(testFilePath);
+            await outputFile.copy(testFilePath);
+          }
+          request.result.complete(kernelReadyToRun.path);
+          compiler.accept();
+          compiler.reset();
+        }
+        printTrace('Compiling ${request.path} took ${compilerTime.elapsedMilliseconds}ms');
+      }
+
+      idleCompilers.add(compiler);
     }, onDone: () {
       printTrace('Deleting ${outputDillDirectory.path}...');
       outputDillDirectory.deleteSync(recursive: true);
@@ -300,7 +316,8 @@ class _Compiler {
   final StreamController<_CompilationRequest> compilerController =
       new StreamController<_CompilationRequest>();
   final List<_CompilationRequest> compilationQueue = <_CompilationRequest>[];
-  ResidentCompiler compiler;
+  List<ResidentCompiler> idleCompilers = <ResidentCompiler>[];
+  int compilers = 0;
 
   Future<String> compile(String mainDart) {
     final Completer<String> completer = new Completer<String>();
@@ -308,17 +325,21 @@ class _Compiler {
     return handleTimeout<String>(completer.future, mainDart);
   }
 
-  Future<void> _shutdown() async {
+  Future<void> _shutdown(ResidentCompiler compiler) async {
     // Check for null in case this instance is shut down before the
     // lazily-created compiler has been created.
     if (compiler != null) {
       await compiler.shutdown();
+      idleCompilers.remove(compiler);
+      compilers--;
       compiler = null;
     }
   }
 
   Future<void> dispose() async {
-    await _shutdown();
+    for (ResidentCompiler compiler in idleCompilers.toList()) {
+      await _shutdown(compiler);
+    }
     await compilerController.close();
   }
 
