e2d0b062d9c436f63bae68c5799118c0d82db24d
Alexandre Ardhuin <alexandre.ardhuin@gmail.com>
2018-09-18 05:54:03 +0200

    remove unnecessary ignore prefer_const_constructors (#21934)

--- packages/flutter/test/material/reorderable_list_test.dart
+++ packages/flutter/test/material/reorderable_list_test.dart
@@ -914,7 +914,6 @@ Future<void> longPressDrag(WidgetTester tester, Offset start, Offset end) async 
 
 class _Stateful extends StatefulWidget {
   // Ignoring the preference for const constructors because we want to test with regular non-const instances.
-  // ignore:prefer_const_constructors
   // ignore:prefer_const_constructors_in_immutables
   _Stateful({Key key}) : super(key: key);
 
