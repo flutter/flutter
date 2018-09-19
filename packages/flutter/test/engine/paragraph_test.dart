6b5ae37e2021a8c00fd4d3224c291100f527e537
Yegor Jbanov <yjbanov@google.com>
2018-09-19 13:42:27 -0700

    use flutter_test

--- packages/flutter/test/engine/paragraph_test.dart
+++ packages/flutter/test/engine/paragraph_test.dart
@@ -4,7 +4,7 @@
 
 import 'dart:ui';
 
-import 'package:test/test.dart';
+import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   // Ahem font uses a constant ideographic/alphabetic baseline ratio.
