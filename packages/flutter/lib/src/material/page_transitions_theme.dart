f02ea20fcd910bdeec86831776b84dd6bf5a7015
Hans Muller <hansmuller@google.com>
2018-09-24 10:45:37 -0700

    update

--- packages/flutter/lib/src/material/page_transitions_theme.dart
+++ packages/flutter/lib/src/material/page_transitions_theme.dart
@@ -9,12 +9,6 @@ import 'package:flutter/widgets.dart';
 import 'colors.dart';
 import 'theme.dart';
 
-// Fractional offset from 1/4 screen below the top to fully on screen.
-final Tween<Offset> _kBottomUpTween = Tween<Offset>(
-  begin: const Offset(0.0, 0.25),
-  end: Offset.zero,
-);
-
 // Slides the page upwards and fades it in, starting from 1/4 screen
 // below the top.
 class _FadeUpwardsPageTransition extends StatelessWidget {
@@ -22,10 +16,15 @@ class _FadeUpwardsPageTransition extends StatelessWidget {
     Key key,
     @required Animation<double> routeAnimation, // The route's linear 0.0 - 1.0 animation.
     @required this.child,
-  }) : _positionAnimation = routeAnimation.drive(_kBottomUpTween.chain(_fastOutSlowInTween)),
+  }) : _positionAnimation = routeAnimation.drive(_bottomUpTween.chain(_fastOutSlowInTween)),
        _opacityAnimation = routeAnimation.drive(_easeInTween),
        super(key: key);
 
+  // Fractional offset from 1/4 screen below the top to fully on screen.
+  static final Tween<Offset> _bottomUpTween = Tween<Offset>(
+    begin: const Offset(0.0, 0.25),
+    end: Offset.zero,
+  );
   static final Animatable<double> _fastOutSlowInTween = CurveTween(curve: Curves.fastOutSlowIn);
   static final Animatable<double> _easeInTween = CurveTween(curve: Curves.easeIn);
 
