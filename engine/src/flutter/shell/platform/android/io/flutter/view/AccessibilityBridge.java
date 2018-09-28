12981679ba08b258c5a6cb08829357ca84622ad5
Jonah Williams <jonahwilliams@google.com>
2018-09-27 23:24:54 -0700

    make sure we dont set live regionin api level < 18 (flutter/engine#6375)

--- engine/src/flutter/shell/platform/android/io/flutter/view/AccessibilityBridge.java
+++ engine/src/flutter/shell/platform/android/io/flutter/view/AccessibilityBridge.java
@@ -169,7 +169,9 @@ class AccessibilityBridge
                 // Text fields will always be created as a live region, so that updates to
                 // the label trigger polite announcements. This makes it easy to follow a11y
                 // guidelines for text fields on Android.
-                result.setLiveRegion(View.ACCESSIBILITY_LIVE_REGION_POLITE);
+                if (Build.VERSION.SDK_INT > Build.VERSION_CODES.JELLY_BEAN_MR2) {
+                    result.setLiveRegion(View.ACCESSIBILITY_LIVE_REGION_POLITE);
+                }
             }
 
             // Cursor movements
@@ -294,7 +296,7 @@ class AccessibilityBridge
                 result.addAction(AccessibilityNodeInfo.ACTION_SCROLL_BACKWARD);
             }
         }
-        if (object.hasFlag(Flag.IS_LIVE_REGION)) {
+        if (object.hasFlag(Flag.IS_LIVE_REGION) && Build.VERSION.SDK_INT > Build.VERSION_CODES.JELLY_BEAN_MR2) {
             result.setLiveRegion(View.ACCESSIBILITY_LIVE_REGION_POLITE);
         }
 
