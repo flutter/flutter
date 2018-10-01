48fb726b018cbc471cfba55a15dba76dbae09a11
Alexandre Ardhuin <alexandre.ardhuin@gmail.com>
2018-10-01 21:22:51 +0200

    compare to null with == (#21944)
    * compare to null with ==
    
    * address review comments

--- packages/flutter/lib/src/painting/box_decoration.dart
+++ packages/flutter/lib/src/painting/box_decoration.dart
@@ -83,11 +83,10 @@ class BoxDecoration extends Decoration {
     this.backgroundBlendMode,
     this.shape = BoxShape.rectangle,
   }) : assert(shape != null),
-       // TODO(mattcarroll): Use "backgroundBlendMode == null" when https://github.com/dart-lang/sdk/issues/34180 is in.
        assert(
-         identical(backgroundBlendMode, null) || color != null || gradient != null,
+         backgroundBlendMode == null || color != null || gradient != null,
          'backgroundBlendMode applies to BoxDecoration\'s background color or '
-         'gradient, but no color or gradient were provided.'
+         'gradient, but no color or gradient was provided.'
        );
 
   @override
