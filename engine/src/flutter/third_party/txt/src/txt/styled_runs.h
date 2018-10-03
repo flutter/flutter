479147ef118c1e2cc485633829406974cae4b24c
Gary Qian <garyq@google.com>
2018-10-03 16:28:21 -0700

    Disable line_height tests on Windows (flutter/engine#6423)

--- engine/src/flutter/third_party/txt/src/txt/styled_runs.h
+++ engine/src/flutter/third_party/txt/src/txt/styled_runs.h
@@ -79,7 +79,7 @@ class StyledRuns {
   FRIEND_TEST(ParagraphTest, HyphenBreakParagraph);
   FRIEND_TEST(ParagraphTest, RepeatLayoutParagraph);
   FRIEND_TEST(ParagraphTest, Ellipsize);
-  FRIEND_TEST(ParagraphTest, LineHeightsParagraph);
+  FRIEND_TEST_WINDOWS_DISABLED(ParagraphTest, LineHeightsParagraph);
 
   struct IndexedRun {
     size_t style_index = 0;
