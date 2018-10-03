63420eb4b0c2a88361f08f4ea41db4f434278092
Michael Goderbauer <goderbauer@google.com>
2018-10-02 18:28:22 -0700

    Hide App.framework.dSYM from Spotlight (#22540)

--- packages/flutter_tools/bin/xcode_backend.sh
+++ packages/flutter_tools/bin/xcode_backend.sh
@@ -146,7 +146,13 @@ BuildApp() {
     RunCommand cp -r -- "${app_framework}" "${derived_dir}"
 
     StreamOutput " ├─Generating dSYM file..."
-    RunCommand xcrun dsymutil -o "${build_dir}/aot/App.dSYM" "${app_framework}/App"
+    # Xcode calls `symbols` during app store upload, which uses Spotlight to
+    # find dSYM files for embedded frameworks. When it finds the dSYM file for
+    # `App.framework` it throws an error, which aborts the app store upload.
+    # To avoid this, we place the dSYM files in a folder ending with ".noindex",
+    # which hides it from Spotlight, https://github.com/flutter/flutter/issues/22560.
+    mkdir "${build_dir}/dSYMs.noindex"
+    RunCommand xcrun dsymutil -o "${build_dir}/dSYMs.noindex/App.framework.dSYM" "${app_framework}/App"
     if [[ $? -ne 0 ]]; then
       EchoError "Failed to generate debug symbols (dSYM) file for ${app_framework}/App."
       exit -1
