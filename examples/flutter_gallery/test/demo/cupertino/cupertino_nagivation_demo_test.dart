2e286e129b8e38e81511d6b3172fcf80d0c1ecef
jslavitz <jslavitz@google.com>
2018-10-04 16:53:34 -0700

    work on test

--- examples/flutter_gallery/test/demo/cupertino/cupertino_nagivation_demo_test.dart
+++ examples/flutter_gallery/test/demo/cupertino/cupertino_nagivation_demo_test.dart
@@ -41,9 +41,76 @@ void main() {
     await gesture.moveBy(const Offset(300.0, 0.0));
     await tester.pump();
 
-    // Page 1 is now visible.
+    // Page 1 is not visible.
     expect(find.text('Page 1'), findsNothing);
 
+    debugDefaultTargetPlatformOverride = null;
+  });
+
+  testWidgets('test only edge swipes work (LTR)', (WidgetTester tester) async {
+      debugDefaultTargetPlatformOverride = TargetPlatform.iOS;
+
+      await tester.pumpWidget(
+      CupertinoApp(
+        onGenerateRoute: (RouteSettings settings) {
+          return CupertinoPageRoute<void>(
+              settings: settings,
+              builder: (BuildContext context) {
+                final bool firstPage = settings.name == '/';
+                if (firstPage)
+                  return const Text('Page 1');
+                else
+                  return CupertinoNavigationDemo();
+              }
+          );
+        },
+      ),
+    );
+
+    tester.state<NavigatorState>(find.byType(Navigator)).pushNamed('/next');
+
+    await tester.pump();
+    await tester.pump(const Duration(milliseconds: 400));
+
+    // Page 2 covers page 1.
+    expect(find.text('Page 1'), findsNothing);
+    expect(find.text('Home'), isOnstage);
+
+    // Drag from the middle to the right.
+    TestGesture gesture = await tester.startGesture(const Offset(200.0, 200.0));
+    await gesture.moveBy(const Offset(300.0, 0.0));
+    await tester.pump();
+
+    // Nothing should happen.
+    expect(find.text('Page 1'), findsNothing);
+    expect(find.text('Home'), isOnstage);
+
+    // Drag from the right to the left.
+    gesture = await tester.startGesture(const Offset(795.0, 200.0));
+    await gesture.moveBy(const Offset(-300.0, 0.0));
+    await tester.pump();
+
+    // Nothing should happen.
+    expect(find.text('Page 1'), findsNothing);
+    expect(find.text('Home'), isOnstage);
+
+    // Drag from the right to the further right.
+    gesture = await tester.startGesture(const Offset(795.0, 200.0));
+    await gesture.moveBy(const Offset(300.0, 0.0));
+    await tester.pump();
+
+    // Nothing should happen.
+    expect(find.text('Page 1'), findsNothing);
+    expect(find.text('Home'), isOnstage);
+
+    // Now drag from the left edge.
+    gesture = await tester.startGesture(const Offset(5.0, 200.0));
+    await gesture.moveBy(const Offset(300.0, 0.0));
+    await tester.pump();
+
+    // Page 1 is now visible.
+    expect(find.text('Page 1'), isOnstage);
+    expect(find.text('Home'), isOnstage);
     debugDefaultTargetPlatformOverride = null;
   });
 }
