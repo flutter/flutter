4f8d355d85980034c3ca389fcb3e7aa12b53f912
jslavitz <jslavitz@google.com>
2018-10-04 14:57:18 -0700

    working on tests

--- examples/flutter_gallery/test/demo/cupertino/cupertino_nagivation_demo_test.dart
+++ examples/flutter_gallery/test/demo/cupertino/cupertino_nagivation_demo_test.dart
@@ -8,15 +8,83 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 
 void main() {
-  debugDefaultTargetPlatformOverride = TargetPlatform.android;
   testWidgets('Back button doesn\'t work on Android', (WidgetTester tester) async {
+    debugDefaultTargetPlatformOverride = TargetPlatform.android;
+
     await tester.pumpWidget(
       CupertinoApp(
-        home: CupertinoNavigationDemo()
+        home: CupertinoButton(
+          child: const Text('test'),
+          onPressed: (){},
+        ),
+        routes: <String, WidgetBuilder>{
+          'nav demo', CupertinoNavigationDemo()
+        }
       ),
     );
 
     await tester.pageBack();
+    debugDefaultTargetPlatformOverride = null;
   });
-  debugDefaultTargetPlatformOverride = null;
+
+  testWidgets('test only edge swipes work (LTR)', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      CupertinoApp(
+        onGenerateRoute: (RouteSettings settings) {
+          return CupertinoPageRoute<void>(
+              settings: settings,
+              builder: (BuildContext context) {
+                final String pageNumber = settings.name == '/' ? '1' : '2';
+                return Center(child: Text('Page $pageNumber'));
+              }
+          );
+        },
+      ),
+    );
+
+    tester.state<NavigatorState>(find.byType(Navigator)).pushNamed('/next');
+
+    await tester.pump();
+    await tester.pump(const Duration(milliseconds: 400));
+
+    // Page 2 covers page 1.
+    expect(find.text('Page 1'), findsNothing);
+    expect(find.text('Page 2'), isOnstage);
+
+    // Drag from the middle to the right.
+    TestGesture gesture = await tester.startGesture(const Offset(200.0, 200.0));
+    await gesture.moveBy(const Offset(300.0, 0.0));
+    await tester.pump();
+
+    // Nothing should happen.
+    expect(find.text('Page 1'), findsNothing);
+    expect(find.text('Page 2'), isOnstage);
+
+    // Drag from the right to the left.
+    gesture = await tester.startGesture(const Offset(795.0, 200.0));
+    await gesture.moveBy(const Offset(-300.0, 0.0));
+    await tester.pump();
+
+    // Nothing should happen.
+    expect(find.text('Page 1'), findsNothing);
+    expect(find.text('Page 2'), isOnstage);
+
+    // Drag from the right to the further right.
+    gesture = await tester.startGesture(const Offset(795.0, 200.0));
+    await gesture.moveBy(const Offset(300.0, 0.0));
+    await tester.pump();
+
+    // Nothing should happen.
+    expect(find.text('Page 1'), findsNothing);
+    expect(find.text('Page 2'), isOnstage);
+
+    // Now drag from the left edge.
+    gesture = await tester.startGesture(const Offset(5.0, 200.0));
+    await gesture.moveBy(const Offset(300.0, 0.0));
+    await tester.pump();
+
+    // Page 1 is now visible.
+    expect(find.text('Page 1'), isOnstage);
+    expect(find.text('Page 2'), isOnstage);
+  });
 }
