605b33519ea068d0ce178f92b8894ed3bf11ffa0
Yegor Jbanov <yjbanov@google.com>
2018-10-05 10:45:57 -0700

    fix pub global activate syntax

--- dev/bots/docs.sh
+++ dev/bots/docs.sh
@@ -64,7 +64,7 @@ fi
 
 # Install and activate dartdoc.
 #"$PUB" global activate dartdoc 0.22.0
-"$PUB" global activate dartdoc -sgit https://github.com/dart-lang/dartdoc.git
+"$PUB" global activate -sgit https://github.com/dart-lang/dartdoc.git
 
 # This script generates a unified doc set, and creates
 # a custom index.html, placing everything into dev/docs/doc.
