bc0c274553a624c270081c61493b660b9fe8e434
jslavitz <jslavitz@google.com>
2018-10-05 14:44:03 -0700

    fix

--- examples/hello_world/lib/main.dart
+++ examples/hello_world/lib/main.dart
@@ -3,29 +3,5 @@
 // found in the LICENSE file.
 
 import 'package:flutter/widgets.dart';
-import 'package:flutter/material.dart';
 
-//void main() => runApp(const Center(child: Text('Hello, world!', textDirection: TextDirection.ltr)));
-
-void main() {
-  final List<DropdownMenuItem<int>> items =
-      List<DropdownMenuItem<int>>.generate(100, (int i) =>
-  DropdownMenuItem<int>(value: i, child: Text('$i')));
-
-  final DropdownButton<int> button = DropdownButton<int>(
-    value: 87,
-    onChanged: (int newValue){},
-    items: items,
-  );
-
-  runApp(
-    MaterialApp(
-      home: Material(
-      child: Align(
-        alignment: Alignment.bottomCenter,
-        child: button,
-        ),
-      ),
-    ),
-  );
-}
+void main() => runApp(const Center(child: Text('Hello, world!', textDirection: TextDirection.ltr)));
