7cde96f3158f56164938906ab73b00c800975e1f
Michael Klimushyn <mklim@google.com>
2018-10-05 11:56:13 -0700

    Add `run_tests.sh` to cirrus (flutter/engine#6441)
    This replaces `ci/test.sh` with `run_tests.sh`. `run_tests.sh` includes
    `ci/test.sh` and multiple other tests.
    
    Partially addresses flutter/flutter#22682. Temporarily skipped tests
    should be fixed and re-enabled in a follow-up commit.

--- engine/src/flutter/ci/build.sh
+++ engine/src/flutter/ci/build.sh
@@ -5,7 +5,10 @@ PATH="$HOME/depot_tools:$PATH"
 
 cd ..
 
+# Build the dart UI files
 flutter/tools/gn --unoptimized
 ninja -C out/host_debug_unopt generate_dart_ui
+
+# Analyze the dart UI
 flutter/ci/analyze.sh
 flutter/ci/licenses.sh
