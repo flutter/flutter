1aa5af91641258cd83dc75e84eed8aa501ea0f1e
Stanislav Baranov <stanislav@gmail.com>
2018-10-09 13:04:06 -0700

    Remove unused deps and flags from flutter tool. (#22571)

--- packages/flutter_tools/bin/xcode_backend.sh
+++ packages/flutter_tools/bin/xcode_backend.sh
@@ -204,7 +204,6 @@ BuildApp() {
     build bundle                                                            \
     --target-platform=ios                                                   \
     --target="${target_path}"                                               \
-    --snapshot="${build_dir}/snapshot_blob.bin"                             \
     --${build_mode}                                                         \
     --depfile="${build_dir}/snapshot_blob.bin.d"                            \
     --asset-dir="${derived_dir}/flutter_assets"                             \
