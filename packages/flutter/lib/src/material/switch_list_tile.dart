8d545e9301cc71517cf8ecff13823449c263f0d3
jslavitz <jslavitz@gmail.com>
2018-10-09 13:43:15 -0700

    Add disabledThumbColor and other properties from Switch to SwitchListTile final (#22823)
    * added missing properties

--- packages/flutter/lib/src/material/switch_list_tile.dart
+++ packages/flutter/lib/src/material/switch_list_tile.dart
@@ -74,6 +74,9 @@ class SwitchListTile extends StatelessWidget {
     @required this.value,
     @required this.onChanged,
     this.activeColor,
+    this.activeTrackColor,
+    this.inactiveThumbColor,
+    this.inactiveTrackColor,
     this.activeThumbImage,
     this.inactiveThumbImage,
     this.title,
@@ -122,6 +125,21 @@ class SwitchListTile extends StatelessWidget {
   ///
   /// Defaults to accent color of the current [Theme].
   final Color activeColor;
+
+  /// The color to use on the track when this switch is on.
+  ///
+  /// Defaults to [ThemeData.toggleableActiveColor] with the opacity set at 50%.
+  final Color activeTrackColor;
+
+  /// The color to use on the thumb when this switch is off.
+  ///
+  /// Defaults to the colors described in the Material design specification.
+  final Color inactiveThumbColor;
+
+  /// The color to use on the track when this switch is off.
+  ///
+  /// Defaults to the colors described in the Material design specification.
+  final Color inactiveTrackColor;
 
   /// An image to use on the thumb of this switch when the switch is on.
   final ImageProvider activeThumbImage;
@@ -173,6 +191,9 @@ class SwitchListTile extends StatelessWidget {
       activeThumbImage: activeThumbImage,
       inactiveThumbImage: inactiveThumbImage,
       materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+      activeTrackColor: activeTrackColor,
+      inactiveTrackColor: inactiveTrackColor,
+      inactiveThumbColor: inactiveThumbColor,
     );
     return MergeSemantics(
       child: ListTileTheme.merge(
