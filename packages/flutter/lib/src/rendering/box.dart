02c32fc2a7aebf4b9c1bab19043c7bb47fad0350
Stanislav Baranov <sbaranov@google.com>
2018-10-17 14:36:50 -0700

    tweak/simplify

--- packages/flutter/lib/src/rendering/box.dart
+++ packages/flutter/lib/src/rendering/box.dart
@@ -2014,12 +2014,11 @@ abstract class RenderBox extends RenderObject {
     final double det = transform.invert();
     if (det == 0.0)
       return Offset.zero;
+    final Vector3 n = Vector3(0.0, 0.0, 1.0);
     final Vector3 i = transform.perspectiveTransform(Vector3(0.0, 0.0, 0.0));
     final Vector3 d = transform.perspectiveTransform(Vector3(0.0, 0.0, 1.0)) - i;
     final Vector3 s = transform.perspectiveTransform(Vector3(point.dx, point.dy, 0.0));
-    final Vector3 o = Vector3(0.0, 0.0, 0.0);
-    final Vector3 n = Vector3(0.0, 0.0, 1.0);
-    final Vector3 p = s + d * (n.dot(o - s) / n.dot(d));
+    final Vector3 p = s - d * n.dot(s) / n.dot(d);
     return Offset(p.x, p.y);
   }
 
