ab7a9b7f3011fbc43e95acf86aa7bf155349157e
Devon Carew <devoncarew@google.com>
2018-10-18 15:43:06 -0700

    don't disable TODO issues in IDEs

--- packages/flutter_tools/lib/src/commands/analyze_once.dart
+++ packages/flutter_tools/lib/src/commands/analyze_once.dart
@@ -87,7 +87,8 @@ class AnalyzeOnce extends AnalyzeBase {
       }
     });
     server.onErrors.listen((FileAnalysisErrors fileErrors) {
-      errors.addAll(fileErrors.errors);
+      // Record the issues found (but filter out to do comments).
+      errors.addAll(fileErrors.errors.where((AnalysisError error) => error.type != 'TODO'));
     });
 
     await server.start();
@@ -148,9 +149,9 @@ class AnalyzeOnce extends AnalyzeBase {
       final int errorCount = errors.length;
       printStatus('');
       if (undocumentedMembers > 0) {
-        throwToolExit('$errorCount ${pluralize('issue', errorCount)} found. (ran in ${seconds}s; $dartdocMessage)');
+        throwToolExit('$errorCount ${pluralize('issue', errorCount)} found (ran in ${seconds}s; $dartdocMessage).');
       } else {
-        throwToolExit('$errorCount ${pluralize('issue', errorCount)} found. (ran in ${seconds}s)');
+        throwToolExit('$errorCount ${pluralize('issue', errorCount)} found (ran in ${seconds}s).');
       }
     }
 
