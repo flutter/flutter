979de076b3350c3c69fe45fb0cce61bdef51076a
Chinmay Garde <chinmaygarde@gmail.com>
2018-10-17 17:20:44 -0700

    Add a CountDownLatch to fml with tests. (#6574)

---
+++ fml/synchronization/count_down_latch.h
@@ -0,0 +1,29 @@
+// Copyright 2018 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <atomic>
+
+#include "flutter/fml/macros.h"
+#include "flutter/fml/synchronization/waitable_event.h"
+
+namespace fml {
+
+class CountDownLatch {
+ public:
+  CountDownLatch(size_t count);
+
+  ~CountDownLatch();
+
+  void Wait();
+
+  void CountDown();
+
+ private:
+  std::atomic_size_t count_;
+  ManualResetWaitableEvent waitable_event_;
+
+  FML_DISALLOW_COPY_AND_ASSIGN(CountDownLatch);
+};
+
+}  // namespace fml
