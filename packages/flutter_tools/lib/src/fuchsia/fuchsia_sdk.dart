00b8fef8fa7437234e10792300fb2b78bcc92166
jonahwilliams <jonahwilliams@google.com>
2018-10-19 12:35:24 -0700

    git status

--- packages/flutter_tools/lib/src/fuchsia/fuchsia_sdk.dart
+++ packages/flutter_tools/lib/src/fuchsia/fuchsia_sdk.dart
@@ -2,8 +2,11 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'dart:typed_data';
+
 import '../base/common.dart';
 import '../base/context.dart';
+import '../base/io.dart';
 import '../base/process.dart';
 
 /// The [FuchsiaSdk] instance.
@@ -16,31 +19,35 @@ FuchsiaSdk get fuchsiaSdk => context[FuchsiaSdk];
 class FuchsiaSdk {
   /// Invokes the `netaddr` command.
   ///
-  /// This returns the network address of an attached fuchsia device.
-  String netaddr() {
-    String text;
+  /// This returns the network address of an attached fuchsia device. Does
+  /// not currently support multiple attached devices.
+  ///
+  /// Example output:
+  ///     $ fx netaddr --fuchsia
+  ///     > fe80::9aaa:fcff:fe60:d3af%eth1
+  Stream<Uint8List> netaddr() async * {
     try {
-      text = runSync(<String>['fx', 'netaddr', '--fuchsia']);
+      final Process process = await runDetached(<String>['fx', 'netaddr', '--fuchsia']);
+      yield* process.stdout;
     } on ArgumentError catch (exception) {
-      throwToolExit('Unable to run "netaddr": ${exception.message}');
+      throwToolExit('$exception');
     }
-    return text;
   }
 
   /// Invokes the `netls` command.
   ///
-  /// This lists attached fuchsia devices with their name and address.
+  /// This lists attached fuchsia devices with their name and address. Does
+  /// not currently support multiple attached devices.
   ///
   /// Example output:
   ///     $ fx netls
   ///     > device liliac-shore-only-last (fe80::82e4:da4d:fe81:227d/3)
-  String netls() {
-    String text;
+  Stream<Uint8List> netls() async * {
     try {
-      text = runSync(<String>['fx', 'netls']);
+      final Process process = await runDetached(<String>['fx', 'netls']);
+      yield* process.stdout;
     } on ArgumentError catch (exception) {
-      throwToolExit('Unable to run "netls": ${exception.message}');
+      throwToolExit('$exception');
     }
-    return text;
   }
 }
