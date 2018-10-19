bb14fa0301798d281a3ef1da61758d4994b6a3d9
Danny Tuppeny <danny@tuppeny.com>
2018-10-19 08:07:54 +0100

    Use (processors-1) for test runs (test defaults to processors/2) (#22803)
    * Use (processors-1) for test runs (test defaults to processors/2)
    
    * Add missing import 🙄

--- dev/bots/test.dart
+++ dev/bots/test.dart
@@ -4,6 +4,7 @@
 
 import 'dart:async';
 import 'dart:io';
+import 'dart:math' as math;
 
 import 'package:path/path.dart' as path;
 
@@ -223,6 +224,8 @@ Future<void> _pubRunTest(
   bool enableFlutterToolAsserts = false
 }) {
   final List<String> args = <String>['run', 'test', '-rcompact'];
+  final int concurrency = math.max(1, Platform.numberOfProcessors - 1);
+  args.add('-j$concurrency');
   if (!hasColor)
     args.add('--no-color');
   if (testPath != null)
