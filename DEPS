flutter/engine

commit c524b4ba37a5738ba70b7ebe2eb04fe70dc7702d

Revert "Roll buildtools to 759274dd801b2f78ae0c4066101d0beca27ffc9a" (#6621)

DEPS
@@ -104,7 +104,7 @@ vars = {
   # Build bot tooling for iOS
   'ios_tools_revision': '69b7c1b160e7107a6a98d948363772dc9caea46f',
 
-  'buildtools_revision': '759274dd801b2f78ae0c4066101d0beca27ffc9a',
+  'buildtools_revision': 'c1408453246f0475547b6fe634c2f3dad71c6457',
 }
 
 # Only these hosts are allowed for dependencies in this DEPS file.

flutter/flutter

commit 36df8bec97091d149f9079faa269c361d1b39629

Roll buildtools to 5a9e1b3a0b84a2871f20f85fde665e54a894ba72 (flutter/engine#6633)
This one includes the fix for the bug that caused the rollback in https://github.com/flutter/engine/pull/6621.

DEPS
@@ -104,7 +104,7 @@ vars = {
   # Build bot tooling for iOS
   'ios_tools_revision': '69b7c1b160e7107a6a98d948363772dc9caea46f',
 
-  'buildtools_revision': 'c1408453246f0475547b6fe634c2f3dad71c6457',
+  'buildtools_revision': '5a9e1b3a0b84a2871f20f85fde665e54a894ba72',
 }
 
 # Only these hosts are allowed for dependencies in this DEPS file.