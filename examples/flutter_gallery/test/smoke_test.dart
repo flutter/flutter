32fbe1af3d2cb0ddc64da1cdc62f7e72076d7e38
xster <xiao@xster.net>
2018-10-23 13:03:35 -0700

    Platform agnostic gallery smoke test (#23423)

--- examples/flutter_gallery/test/smoke_test.dart
+++ examples/flutter_gallery/test/smoke_test.dart
@@ -18,6 +18,14 @@ const String kGalleryTitle = 'Flutter gallery';
 // values approved by verityToStringOutput().
 int toStringErrors = 0;
 
+// There are 3 places where the Gallery demos are traversed.
+// 1- In widget tests such as examples/flutter_gallery/test/smoke_test.dart
+// 2- In driver tests such as examples/flutter_gallery/test_driver/transitions_perf_test.dart
+// 3- In on-device instrumentation tests such as examples/flutter_gallery/test/live_smoketest.dart
+//
+// If you change navigation behavior in the Gallery or in the framework, make
+// sure all 3 are covered.
+
 void reportToStringError(String name, String route, int lineNumber, List<String> lines, String message) {
   // If you're on line 12, then it has index 11.
   // If you want 1 line before and 1 line after, then you want lines with index 10, 11, and 12.
