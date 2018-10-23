411ad917fb3d9c375b0aa4da33259a9c27f4f632
xster <xiao@xster.net>
2018-10-23 13:03:35 -0700

    Platform agnostic gallery smoke test (#23423)

--- examples/flutter_gallery/test_driver/transitions_perf_test.dart
+++ examples/flutter_gallery/test_driver/transitions_perf_test.dart
@@ -35,6 +35,14 @@ const List<String> kProfiledDemos = <String>[
   'Pickers@Material',
 ];
 
+// There are 3 places where the Gallery demos are traversed.
+// 1- In widget tests such as examples/flutter_gallery/test/smoke_test.dart
+// 2- In driver tests such as examples/flutter_gallery/test_driver/transitions_perf_test.dart
+// 3- In on-device instrumentation tests such as examples/flutter_gallery/test/live_smoketest.dart
+//
+// If you change navigation behavior in the Gallery or in the framework, make
+// sure all 3 are covered.
+
 // Demos that will be backed out of within FlutterDriver.runUnsynchronized();
 //
 // These names must match GalleryItem titles from kAllGalleryDemos
