0b68068d6a33f35cec568ae6a9b394ff2a9fdb76
Alexander Aprelev <aam@google.com>
2018-10-26 18:56:49 -0700

    Revert "Run reload asynchronously so that multiple devices can reload in parallel. (#22693)" (#23598)
    This reverts commit 709f54f4bbd776cb256a0edf885a64848121a894 as it seems to have broken two tests: flutter_gallery__back_button_memory, named_isolates_test.

--- packages/flutter_tools/lib/src/resident_runner.dart
+++ packages/flutter_tools/lib/src/resident_runner.dart
@@ -79,15 +79,11 @@ class FlutterDevice {
     vmServices = localVmServices;
   }
 
-  Future<void> refreshViews() {
+  Future<void> refreshViews() async {
     if (vmServices == null || vmServices.isEmpty)
-      return Future<void>.value(null);
-    final List<Future<void>> futures = <Future<void>>[];
+      return;
     for (VMService service in vmServices)
-      futures.add(service.vm.refreshViews());
-    final Completer<void> completer = Completer<void>();
-    Future.wait(futures).whenComplete(() => completer.complete(null)); // ignore: unawaited_futures
-    return completer.future;
+      await service.vm.refreshViews();
   }
 
   List<FlutterView> get views {
