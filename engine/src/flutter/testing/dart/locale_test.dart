20bd079a611fbb8ac739c3a9a7468aa0261a94d8
hugovdm <hugovdm@gmail.com>
2018-10-29 22:48:59 +0100

    Add Locale.fromSubtags and support for scriptCode. (flutter/engine#6518)
    * Add Locale.fromComponents.
    
    * Change toString from underscores to dashes. Expand the unit tests.
    
    * Rename 'fromComponents' to 'create'. Change variants from String to List<String>.
    
    * Use default for language parameter. Use hashCode/hashList.
    
    * Have toString() stick with old (underscore) behaviour.
    
    * Demonstrate empty-list bug in assert code.
    
    * Fix empty-list assert bug.
    
    * Add ignores for lint issues. Unsure about 71340 though.
    
    * Fix operator== via _listEquals.
    
    * Remove length-checking asserts: we're anyway not checking characters in fields.
    
    * Documentation update.
    
    * Change reasoning for ignore:prefer_initializing_formals.
    
    * Try 'fromSubtags' as new constructor name.
    
    * Documentation improvements based on Pull Request review.
    
    * Assert-fail for invalid-length subtags and drop bad subtags in production code.
    
    * Revert "Assert-fail for invalid-length subtags and drop bad subtags in production code."
    
    This reverts commit d6f06f5e7b3537d60000c47641580475ef16abbe.
    
    * Re-fix Locale.toString() for variants=[].
    
    * Tear out variants, in case we want to have one fewer pointer in the future.
    
    * Make named parameters' names consistent with member names.
    
    * Also remove _listEquals: no longer in use.
    
    * Lint fix.
    
    * Fix code review nits.
    
    * Lint fix for assert, and a couple more not-zero-length-string asserts.
    
    * Code Review: two of three nits addressed...
    
    * Review fix: change 'should' to 'must' for subtag prescriptions.
    
    * Assert-check that countryCode is never ''.

--- engine/src/flutter/testing/dart/locale_test.dart
+++ engine/src/flutter/testing/dart/locale_test.dart
@@ -12,11 +12,35 @@ void main() {
     expect(const Locale('en').toString(), 'en');
     expect(const Locale('en'), new Locale('en', $null));
     expect(const Locale('en').hashCode, new Locale('en', $null).hashCode);
-    expect(const Locale('en'), isNot(new Locale('en', '')));
-    expect(const Locale('en').hashCode, isNot(new Locale('en', '').hashCode));
     expect(const Locale('en', 'US').toString(), 'en_US');
     expect(const Locale('iw').toString(), 'he');
     expect(const Locale('iw', 'DD').toString(), 'he_DE');
     expect(const Locale('iw', 'DD'), const Locale('he', 'DE'));
+  });
+
+  test('Locale.fromSubtags', () {
+    expect(const Locale.fromSubtags().languageCode, 'und');
+    expect(const Locale.fromSubtags().scriptCode, null);
+    expect(const Locale.fromSubtags().countryCode, null);
+
+    expect(const Locale.fromSubtags(languageCode: 'en').toString(), 'en');
+    expect(const Locale.fromSubtags(languageCode: 'en').languageCode, 'en');
+    expect(const Locale.fromSubtags(scriptCode: 'Latn').toString(), 'und_Latn');
+    expect(const Locale.fromSubtags(scriptCode: 'Latn').scriptCode, 'Latn');
+    expect(const Locale.fromSubtags(countryCode: 'US').toString(), 'und_US');
+    expect(const Locale.fromSubtags(countryCode: 'US').countryCode, 'US');
+
+    expect(Locale.fromSubtags(languageCode: 'es', countryCode: '419').toString(), 'es_419');
+    expect(Locale.fromSubtags(languageCode: 'es', countryCode: '419').languageCode, 'es');
+    expect(Locale.fromSubtags(languageCode: 'es', countryCode: '419').countryCode, '419');
+
+    expect(Locale.fromSubtags(languageCode: 'zh', scriptCode: 'Hans', countryCode: 'CN').toString(), 'zh_Hans_CN');
+  });
+
+  test('Locale equality', () {
+    expect(Locale.fromSubtags(languageCode: 'en'),
+           isNot(Locale.fromSubtags(languageCode: 'en', scriptCode: 'Latn')));
+    expect(Locale.fromSubtags(languageCode: 'en').hashCode,
+           isNot(Locale.fromSubtags(languageCode: 'en', scriptCode: 'Latn').hashCode));
   });
 }
