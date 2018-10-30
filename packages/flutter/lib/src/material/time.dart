75c3dfcd3d75df7308210cba608442a92422f4e0
Hans Muller <hansmuller@google.com>
2018-10-30 09:33:39 -0700

    More debugCheckHasMaterialLocalizations() asserts

--- packages/flutter/lib/src/material/time.dart
+++ packages/flutter/lib/src/material/time.dart
@@ -3,9 +3,13 @@
 // found in the LICENSE file.
 
 import 'dart:ui' show hashValues;
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
 
 import 'material_localizations.dart';
+
+import 'debug.dart';
 
 /// Whether the [TimeOfDay] is before or after noon.
 enum DayPeriod {
@@ -96,7 +100,8 @@ class TimeOfDay {
   ///
   /// This is a shortcut for [MaterialLocalizations.formatTimeOfDay].
   String format(BuildContext context) {
-    debugCheckHasMediaQuery(context);
+    assert(debugCheckHasMediaQuery(context));
+    assert(debugCheckHasMaterialLocalizations(context));
     final MaterialLocalizations localizations = MaterialLocalizations.of(context);
     return localizations.formatTimeOfDay(
       this,
