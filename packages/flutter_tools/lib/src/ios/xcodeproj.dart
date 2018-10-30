35df987424bd37251741b895aab6bb327d155b54
Dan Field <dfield@gmail.com>
2018-10-30 08:31:13 -0700

    Revert "Reland Xcode backend refactor (#23574)"
    This reverts commit 7270f2845d9a975387191ab6adcada4c492273b5.

--- packages/flutter_tools/lib/src/ios/xcodeproj.dart
+++ packages/flutter_tools/lib/src/ios/xcodeproj.dart
@@ -49,10 +49,20 @@ Future<void> updateGeneratedXcodeProperties({
   if (targetOverride != null)
     localsBuffer.writeln('FLUTTER_TARGET=$targetOverride');
 
+  // The runtime mode for the current build.
+  localsBuffer.writeln('FLUTTER_BUILD_MODE=${buildInfo.modeName}');
+
   // The build outputs directory, relative to FLUTTER_APPLICATION_PATH.
   localsBuffer.writeln('FLUTTER_BUILD_DIR=${getBuildDirectory()}');
 
   localsBuffer.writeln('SYMROOT=\${SOURCE_ROOT}/../${getIosBuildDirectory()}');
+
+  if (!project.isModule) {
+    // For module projects we do not want to write the FLUTTER_FRAMEWORK_DIR
+    // explicitly. Rather we rely on the xcode backend script and the Podfile
+    // logic to derive it from FLUTTER_ROOT and FLUTTER_BUILD_MODE.
+    localsBuffer.writeln('FLUTTER_FRAMEWORK_DIR=${flutterFrameworkDir(buildInfo.mode)}');
+  }
 
   final String buildName = buildInfo?.buildName ?? project.manifest.buildName;
   if (buildName != null) {
@@ -237,17 +247,6 @@ class XcodeProjectInfo {
       return baseConfiguration + '-$scheme';
   }
 
-  /// Checks whether the [buildConfigurations] contains the specified string, without
-  /// regard to case.
-  bool hasBuildConfiguratinForBuildMode(String buildMode) {
-    buildMode = buildMode.toLowerCase();
-    for (String name in buildConfigurations) {
-      if (name.toLowerCase() == buildMode) {
-        return true;
-      }
-    }
-    return false;
-  }
   /// Returns unique scheme matching [buildInfo], or null, if there is no unique
   /// best match.
   String schemeFor(BuildInfo buildInfo) {
@@ -263,7 +262,7 @@ class XcodeProjectInfo {
   /// null, if there is no unique best match.
   String buildConfigurationFor(BuildInfo buildInfo, String scheme) {
     final String expectedConfiguration = expectedBuildConfigurationFor(buildInfo, scheme);
-    if (hasBuildConfiguratinForBuildMode(expectedConfiguration))
+    if (buildConfigurations.contains(expectedConfiguration))
       return expectedConfiguration;
     final String baseConfiguration = _baseConfigurationFor(buildInfo);
     return _uniqueMatch(buildConfigurations, (String candidate) {
@@ -275,13 +274,7 @@ class XcodeProjectInfo {
     });
   }
 
-  static String _baseConfigurationFor(BuildInfo buildInfo) {
-    if (buildInfo.isDebug)
-      return 'Debug';
-    if (buildInfo.isProfile)
-      return 'Profile';
-    return 'Release';
-  }
+  static String _baseConfigurationFor(BuildInfo buildInfo) => buildInfo.isDebug ? 'Debug' : 'Release';
 
   static String _uniqueMatch(Iterable<String> strings, bool matches(String s)) {
     final List<String> options = strings.where(matches).toList();
