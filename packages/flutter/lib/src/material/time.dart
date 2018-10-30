202089d2178f9878b2195fe4d2709b85d21669dc
Hans Muller <hansmuller@google.com>
2018-10-30 09:37:30 -0700

    updated

--- packages/flutter/lib/src/material/time.dart
+++ packages/flutter/lib/src/material/time.dart
@@ -4,12 +4,11 @@
 
 import 'dart:ui' show hashValues;
 
-import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
 
+import 'debug.dart';
 import 'material_localizations.dart';
 
-import 'debug.dart';
 
 /// Whether the [TimeOfDay] is before or after noon.
 enum DayPeriod {
