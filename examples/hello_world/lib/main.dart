8fb360ce0084f22b26f9a513e1ee52d4d03f89af
jslavitz <jslavitz@google.com>
2018-10-31 17:35:48 -0700

    fix

--- examples/hello_world/lib/main.dart
+++ examples/hello_world/lib/main.dart
@@ -3,23 +3,5 @@
 // found in the LICENSE file.
 
 import 'package:flutter/widgets.dart';
-import 'package:flutter/material.dart';
 
-//void main() => runApp(const Center(child: Text('Hello, world!', textDirection: TextDirection.ltr)));
-
-void main() {
-  runApp(
-    MaterialApp(
-      theme: ThemeData(platform: TargetPlatform.android),
-      home: Material(
-        child: FlexibleSpaceBar.createSettings(
-          currentExtent: 4.0,
-          maxExtent: 8.0,
-          minExtent: 2.0,
-          toolbarOpacity: 0.2,
-          child: AppBar()
-        ),
-      ),
-    ),
-  );
-}
+void main() => runApp(const Center(child: Text('Hello, world!', textDirection: TextDirection.ltr)));
