07d1ba87e0144c161d21f889e1b578a4cfc13677
Todd Volkert <tvolkert@users.noreply.github.com>
2018-11-05 15:10:13 -0800

    Update doctor instructions for libimobiledevice installation (#23946)
    https://github.com/flutter/flutter/issues/22595

--- packages/flutter_tools/lib/src/ios/ios_workflow.dart
+++ packages/flutter_tools/lib/src/ios/ios_workflow.dart
@@ -133,6 +133,9 @@ class IOSValidator extends DoctorValidator {
         brewStatus = ValidationType.partial;
         messages.add(ValidationMessage.error(
             'libimobiledevice and ideviceinstaller are not installed. To install, run:\n'
+            '  brew update\n'
+            '  brew install --HEAD usbmuxd\n'
+            '  brew link usbmuxd\n'
             '  brew install --HEAD libimobiledevice\n'
             '  brew install ideviceinstaller'
         ));
@@ -142,7 +145,12 @@ class IOSValidator extends DoctorValidator {
             'Verify that all connected devices have been paired with this computer in Xcode.\n'
             'If all devices have been paired, libimobiledevice and ideviceinstaller may require updating.\n'
             'To update, run:\n'
+            '  brew update\n'
             '  brew uninstall --ignore-dependencies libimobiledevice\n'
+            '  brew uninstall --ignore-dependencies usbmuxd\n'
+            '  brew install --HEAD usbmuxd\n'
+            '  brew unlink usbmuxd\n'
+            '  brew link usbmuxd\n'
             '  brew install --HEAD libimobiledevice\n'
             '  brew install ideviceinstaller'
         ));
@@ -151,6 +159,8 @@ class IOSValidator extends DoctorValidator {
         messages.add(ValidationMessage.error(
           'ideviceinstaller is not installed; this is used to discover connected iOS devices.\n'
           'To install, run:\n'
+          '  brew install --HEAD usbmuxd\n'
+          '  brew link usbmuxd\n'
           '  brew install --HEAD libimobiledevice\n'
           '  brew install ideviceinstaller'
         ));
