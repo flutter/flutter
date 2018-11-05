ab432bd0aa89d07edd6a88ca1d5361471e8cf420
Danny Tuppeny <dantup@google.com>
2018-11-05 10:33:39 +0000

    Move integration tests to use vm_service_lib

--- packages/flutter_tools/test/integration/expression_evaluation_test.dart
+++ packages/flutter_tools/test/integration/expression_evaluation_test.dart
@@ -7,7 +7,7 @@ import 'dart:async';
 import 'package:file/file.dart';
 import 'package:flutter_tools/src/base/file_system.dart';
 
-import 'package:vm_service_client/vm_service_client.dart';
+import 'package:vm_service_lib/vm_service_lib.dart';
 
 import '../src/common.dart';
 import 'test_data/basic_project.dart';
@@ -31,45 +31,45 @@ void main() {
       tryToDelete(tempDir);
     });
 
-    Future<VMIsolate> breakInBuildMethod(FlutterTestDriver flutter) async {
+    Future<Isolate> breakInBuildMethod(FlutterTestDriver flutter) async {
       return _flutter.breakAt(
           _project.buildMethodBreakpointUri,
           _project.buildMethodBreakpointLine);
     }
 
-    Future<VMIsolate> breakInTopLevelFunction(FlutterTestDriver flutter) async {
+    Future<Isolate> breakInTopLevelFunction(FlutterTestDriver flutter) async {
       return _flutter.breakAt(
           _project.topLevelFunctionBreakpointUri,
           _project.topLevelFunctionBreakpointLine);
     }
 
     Future<void> evaluateTrivialExpressions() async {
-      VMInstanceRef res;
+      InstanceRef res;
 
-      res = await _flutter.evaluateExpression('"test"');
-      expect(res is VMStringInstanceRef && res.value == 'test', isTrue);
+      res = await _flutter.evaluateInFrame('"test"');
+      expect(res.kind == InstanceKind.kString && res.valueAsString == 'test', isTrue);
 
-      res = await _flutter.evaluateExpression('1');
-      expect(res is VMIntInstanceRef && res.value == 1, isTrue);
+      res = await _flutter.evaluateInFrame('1');
+      expect(res.kind == InstanceKind.kInt && res.valueAsString == 1.toString(), isTrue);
 
-      res = await _flutter.evaluateExpression('true');
-      expect(res is VMBoolInstanceRef && res.value == true, isTrue);
+      res = await _flutter.evaluateInFrame('true');
+      expect(res.kind == InstanceKind.kBool && res.valueAsString == true.toString(), isTrue);
     }
 
     Future<void> evaluateComplexExpressions() async {
-      final VMInstanceRef res = await _flutter.evaluateExpression('new DateTime.now().year');
-      expect(res is VMIntInstanceRef && res.value == DateTime.now().year, isTrue);
+      final InstanceRef res = await _flutter.evaluateInFrame('new DateTime.now().year');
+      expect(res.kind == InstanceKind.kInt && res.valueAsString == DateTime.now().year.toString(), isTrue);
     }
 
     Future<void> evaluateComplexReturningExpressions() async {
       final DateTime now = DateTime.now();
-      final VMInstanceRef resp = await _flutter.evaluateExpression('new DateTime.now()');
-      expect(resp.klass.name, equals('DateTime'));
+      final InstanceRef resp = await _flutter.evaluateInFrame('new DateTime.now()');
+      expect(resp.classRef.name, equals('DateTime'));
       // Ensure we got a reasonable approximation. The more accurate we try to
       // make this, the more likely it'll fail due to differences in the time
       // in the remote VM and the local VM at the time the code runs.
-      final VMStringInstanceRef res = await resp.evaluate(r'"$year-$month-$day"');
-      expect(res.value,
+      final InstanceRef res = await _flutter.evaluate(resp.id, r'"$year-$month-$day"');
+      expect(res.valueAsString,
           equals('${now.year}-${now.month}-${now.day}'));
     }
 
