cd65cfd9fecf9d4316c99e58a1831ae996406091
jslavitz <jslavitz@google.com>
2018-11-06 15:23:15 -0800

    adds comment

--- packages/flutter/lib/src/rendering/sliver.dart
+++ packages/flutter/lib/src/rendering/sliver.dart
@@ -688,6 +688,8 @@ class SliverGeometry extends Diagnosticable {
         );
       }
       verify(maxPaintExtent != null, 'The "maxPaintExtent" is null.');
+      // If the maxPaintExtent is slightly more than paintExtent, but the difference is still less
+      // than epsilon, we will not throw the assert below.
       if (paintExtent - maxPaintExtent > _epsilon) {
         verify(false,
           'The "maxPaintExtent" is less than the "paintExtent".\n' +
