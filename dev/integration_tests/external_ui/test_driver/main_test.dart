1f16f375a06c8b804c70ded9df441622ad877c37
Jonah Williams <jonahwilliams@google.com>
2018-11-06 09:12:19 -0800

    Revert "Remove package:test dependency from flutter_test (#23755)"
    This reverts commit 71e9bd6f25125112473d5a094ee77fadaf7c259e.

--- dev/integration_tests/external_ui/test_driver/main_test.dart
+++ dev/integration_tests/external_ui/test_driver/main_test.dart
@@ -5,7 +5,7 @@
 import 'dart:async';
 
 import 'package:flutter_driver/flutter_driver.dart';
-import 'package:test_api/test_api.dart' hide TypeMatcher, isInstanceOf;
+import 'package:test/test.dart' hide TypeMatcher, isInstanceOf;
 
 final RegExp calibrationRegExp = RegExp('Flutter frame rate is (.*)fps');
 final RegExp statsRegExp = RegExp('Produced: (.*)fps\nConsumed: (.*)fps\nWidget builds: (.*)');
