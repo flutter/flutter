0def82ddb0e2062bda371b01b3fbe4de4a891787
Michael Goderbauer <goderbauer@google.com>
2018-11-07 12:24:35 -0800

    Unify copyright lines (flutter/engine#6757)

--- engine/src/flutter/fml/paths.cc
+++ engine/src/flutter/fml/paths.cc
@@ -1,4 +1,4 @@
-// Copyright 2018 The Flutter Authors. All rights reserved.
+// Copyright 2013 The Flutter Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
--- engine/src/flutter/shell/platform/android/io/flutter/app/FlutterActivity.java
+++ engine/src/flutter/shell/platform/android/io/flutter/app/FlutterActivity.java
@@ -1,4 +1,4 @@
-// Copyright 2015 The Chromium Authors. All rights reserved.
+// Copyright 2013 The Flutter Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -109,7 +109,7 @@ public class FlutterActivity extends Activity implements FlutterView.Provider, P
             super.onBackPressed();
         }
     }
-    
+
     @Override
     protected void onStop() {
         eventDelegate.onStop();
