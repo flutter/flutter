3b2d6e1b118ee26ba523967553b56be212c7b46b
Jonah Williams <jonahwilliams@google.com>
2018-11-08 21:50:36 -0800

    Update time_test.dart
    Remove package:test import

--- packages/flutter_tools/test/time_test.dart
+++ packages/flutter_tools/test/time_test.dart
@@ -2,9 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:test/test.dart';
-
 import 'package:flutter_tools/src/time.dart';
+
+import 'src/common.dart'
 
 void main() {
   group(SystemClock, () {
