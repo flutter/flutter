8cbd3d736b1288af48e38009bc2f21d20be8363d
Amir Hardon <amirh@users.noreply.github.com>
2018-11-09 20:36:17 -0800

    Escape objective c method brackets in dartdoc (#24199)

--- packages/flutter/lib/src/widgets/platform_view.dart
+++ packages/flutter/lib/src/widgets/platform_view.dart
@@ -228,13 +228,13 @@ class UiKitView extends StatefulWidget {
   /// {@macro flutter.widgets.platformViews.directionParam}
   final TextDirection layoutDirection;
 
-  /// Passed as the `arguments` argument of [-[FlutterPlatformViewFactory createWithFrame:viewIdentifier:arguments:]](/objcdoc/Protocols/FlutterPlatformViewFactory.html#/c:objc(pl)FlutterPlatformViewFactory(im)createWithFrame:viewIdentifier:arguments:)
+  /// Passed as the `arguments` argument of [-\[FlutterPlatformViewFactory createWithFrame:viewIdentifier:arguments:\]](/objcdoc/Protocols/FlutterPlatformViewFactory.html#/c:objc(pl)FlutterPlatformViewFactory(im)createWithFrame:viewIdentifier:arguments:)
   ///
   /// This can be used by plugins to pass constructor parameters to the embedded Android view.
   final dynamic creationParams;
 
   /// The codec used to encode `creationParams` before sending it to the
-  /// platform side. It should match the codec returned by [-[FlutterPlatformViewFactory createArgsCodec:]](/objcdoc/Protocols/FlutterPlatformViewFactory.html#/c:objc(pl)FlutterPlatformViewFactory(im)createArgsCodec)
+  /// platform side. It should match the codec returned by [-\[FlutterPlatformViewFactory createArgsCodec:\]](/objcdoc/Protocols/FlutterPlatformViewFactory.html#/c:objc(pl)FlutterPlatformViewFactory(im)createArgsCodec)
   ///
   /// This is typically one of: [StandardMessageCodec], [JSONMessageCodec], [StringCodec], or [BinaryCodec].
   ///
