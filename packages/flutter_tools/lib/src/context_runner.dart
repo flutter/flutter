a871d5916407366ce659331b4189af364f0c5e75
Jonah Williams <jonahwilliams@google.com>
2018-11-10 17:02:32 -0800

    Apply basic log filtering and formatting to fuchsia logs. (#24143)

--- packages/flutter_tools/lib/src/context_runner.dart
+++ packages/flutter_tools/lib/src/context_runner.dart
@@ -4,8 +4,6 @@
 
 import 'dart:async';
 
-import 'package:quiver/time.dart';
-
 import 'android/android_sdk.dart';
 import 'android/android_studio.dart';
 import 'android/android_workflow.dart';
@@ -19,6 +17,7 @@ import 'base/io.dart';
 import 'base/logger.dart';
 import 'base/os.dart';
 import 'base/platform.dart';
+import 'base/time.dart';
 import 'base/utils.dart';
 import 'cache.dart';
 import 'compile.dart';
@@ -55,7 +54,6 @@ Future<T> runInContext<T>(
       AssetBundleFactory: () => AssetBundleFactory.defaultInstance,
       BotDetector: () => const BotDetector(),
       Cache: () => Cache(),
-      Clock: () => const Clock(),
       CocoaPods: () => CocoaPods(),
       CocoaPodsValidator: () => const CocoaPodsValidator(),
       Config: () => Config(),
@@ -68,7 +66,7 @@ Future<T> runInContext<T>(
       FuchsiaArtifacts: () => FuchsiaArtifacts(),
       FuchsiaWorkflow: () => FuchsiaWorkflow(),
       Flags: () => const EmptyFlags(),
-      FlutterVersion: () => FlutterVersion(const Clock()),
+      FlutterVersion: () => FlutterVersion(const SystemClock()),
       GenSnapshot: () => const GenSnapshot(),
       HotRunnerConfig: () => HotRunnerConfig(),
       IMobileDevice: () => const IMobileDevice(),
@@ -80,6 +78,7 @@ Future<T> runInContext<T>(
       OperatingSystemUtils: () => OperatingSystemUtils(),
       PlistBuddy: () => const PlistBuddy(),
       SimControl: () => SimControl(),
+      SystemClock: () => const SystemClock(),
       Stdio: () => const Stdio(),
       Usage: () => Usage(),
       Xcode: () => Xcode(),
