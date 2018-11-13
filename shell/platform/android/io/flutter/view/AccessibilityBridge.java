ea4bbbd440617a139737ba4aed103650dde7d8fb
Dan Field <dfield@gmail.com>
2018-11-12 16:08:59 -0800

    Avoid announcing text field when it lacks a11y focus (#6830)
    * Avoid announcing text field when it lacks a11y focus

--- shell/platform/android/io/flutter/view/AccessibilityBridge.java
+++ shell/platform/android/io/flutter/view/AccessibilityBridge.java
@@ -763,7 +763,12 @@ class AccessibilityBridge
                 sendAccessibilityEvent(event);
             }
             if (mInputFocusedObject != null && mInputFocusedObject.id == object.id
-                    && object.hadFlag(Flag.IS_TEXT_FIELD) && object.hasFlag(Flag.IS_TEXT_FIELD)) {
+                    && object.hadFlag(Flag.IS_TEXT_FIELD) && object.hasFlag(Flag.IS_TEXT_FIELD)
+                    // If we have a TextField that has InputFocus, we should avoid announcing it if something
+                    // else we track has a11y focus. This needs to still work when, e.g., IME has a11y focus
+                    // or the "PASTE" popup is used though.
+                    // See more discussion at https://github.com/flutter/flutter/issues/23180
+                    && (mA11yFocusedObject == null || (mA11yFocusedObject.id == mInputFocusedObject.id))) {
                 String oldValue = object.previousValue != null ? object.previousValue : "";
                 String newValue = object.value != null ? object.value : "";
                 AccessibilityEvent event = createTextChangedEvent(object.id, oldValue, newValue);
