762dba9c5517ecc49c14577e881f5b92a401bc15
haog <haoguo@unity3d.com>
2018-11-14 17:14:08 +0800

    fix ISSUE-22212

--- packages/flutter/lib/src/widgets/editable_text.dart
+++ packages/flutter/lib/src/widgets/editable_text.dart
@@ -665,6 +665,7 @@ class EditableTextState extends State<EditableText> with AutomaticKeepAliveClien
                   : TextInputAction.done
               ),
               textCapitalization: widget.textCapitalization,
+              keyboardAppearance: widget.keyboardAppearance,
           )
       )..setEditingState(localValue);
     }
