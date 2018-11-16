3b8ba460c71983a4966201fdf3798e34bd89779b
jslavitz <jslavitz@gmail.com>
2018-11-16 12:47:34 -0800

    Scaffold Drawer pull out area fix for notched devices (#24367)
    * Adds fix and test

--- packages/flutter/lib/src/material/drawer.dart
+++ packages/flutter/lib/src/material/drawer.dart
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'dart:math';
+
 import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
 
@@ -375,6 +377,14 @@ class DrawerControllerState extends State<DrawerController> with SingleTickerPro
   }
 
   Widget _buildDrawer(BuildContext context) {
+    final bool drawerIsStart = widget.alignment == DrawerAlignment.start;
+    final EdgeInsets padding = MediaQuery.of(context).padding;
+    double dragAreaWidth = drawerIsStart ? padding.left : padding.right;
+
+    if (Directionality.of(context) == TextDirection.rtl)
+      dragAreaWidth = drawerIsStart ? padding.right : padding.left;
+
+    dragAreaWidth = max(dragAreaWidth, _kEdgeDragWidth);
     if (_controller.status == AnimationStatus.dismissed) {
       return Align(
         alignment: _drawerOuterAlignment,
@@ -384,7 +394,7 @@ class DrawerControllerState extends State<DrawerController> with SingleTickerPro
           onHorizontalDragEnd: _settle,
           behavior: HitTestBehavior.translucent,
           excludeFromSemantics: true,
-          child: Container(width: _kEdgeDragWidth)
+          child: Container(width: dragAreaWidth),
         ),
       );
     } else {
@@ -420,7 +430,7 @@ class DrawerControllerState extends State<DrawerController> with SingleTickerPro
                     child: FocusScope(
                       key: _drawerKey,
                       node: _focusScopeNode,
-                      child: widget.child
+                      child: widget.child,
                     ),
                   ),
                 ),
