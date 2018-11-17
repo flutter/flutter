7449dc427e09e5243a0ce75846567efe84ba2fd9
Jason Simmons <jason-simmons@users.noreply.github.com>
2018-11-17 12:27:06 -0800

    Revert "Guard the service protocol's global handlers list with a reader/writer lock (#6888)" (flutter/engine#6893)
    This reverts commit e740d7f1dbdb69dc4fd38e39c887dd3cccae7c3d.
    
    (shared_timed_mutex is unavailable in the iOS build)

--- engine/src/flutter/runtime/service_protocol.cc
+++ engine/src/flutter/runtime/service_protocol.cc
@@ -53,21 +53,21 @@ ServiceProtocol::~ServiceProtocol() {
 
 void ServiceProtocol::AddHandler(Handler* handler,
                                  Handler::Description description) {
-  std::unique_lock<std::shared_timed_mutex> lock(handlers_mutex_);
+  std::lock_guard<std::mutex> lock(handlers_mutex_);
   handlers_.emplace(handler, description);
 }
 
 void ServiceProtocol::RemoveHandler(Handler* handler) {
-  std::unique_lock<std::shared_timed_mutex> lock(handlers_mutex_);
+  std::lock_guard<std::mutex> lock(handlers_mutex_);
   handlers_.erase(handler);
 }
 
 void ServiceProtocol::SetHandlerDescription(Handler* handler,
                                             Handler::Description description) {
-  std::shared_lock<std::shared_timed_mutex> lock(handlers_mutex_);
+  std::lock_guard<std::mutex> lock(handlers_mutex_);
   auto it = handlers_.find(handler);
   if (it != handlers_.end())
-    it->second.Store(description);
+    it->second = description;
 }
 
 void ServiceProtocol::ToggleHooks(bool set) {
@@ -175,7 +175,7 @@ bool ServiceProtocol::HandleMessage(fml::StringView method,
     return HandleListViewsMethod(response);
   }
 
-  std::shared_lock<std::shared_timed_mutex> lock(handlers_mutex_);
+  std::lock_guard<std::mutex> lock(handlers_mutex_);
 
   if (handlers_.size() == 0) {
     WriteServerErrorResponse(response,
@@ -246,11 +246,12 @@ void ServiceProtocol::Handler::Description::Write(
 
 bool ServiceProtocol::HandleListViewsMethod(
     rapidjson::Document& response) const {
-  std::shared_lock<std::shared_timed_mutex> lock(handlers_mutex_);
+  // Collect handler descriptions on their respective task runners.
+  std::lock_guard<std::mutex> lock(handlers_mutex_);
   std::vector<std::pair<intptr_t, Handler::Description>> descriptions;
   for (const auto& handler : handlers_) {
     descriptions.emplace_back(reinterpret_cast<intptr_t>(handler.first),
-                              handler.second.Load());
+                              handler.second);
   }
 
   auto& allocator = response.GetAllocator();
