1c328db01890a1ea2ad36db45e397419cb0c4380
Remi Rousselet <darky12s@gmail.com>
2018-11-17 19:59:51 +0100

    hooks

--- packages/flutter/lib/src/widgets/framework.dart
+++ packages/flutter/lib/src/widgets/framework.dart
@@ -8,6 +8,7 @@ import 'dart:developer';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/rendering.dart';
+import 'package:flutter/src/widgets/async.dart';
 
 import 'debug.dart';
 import 'focus_manager.dart';
@@ -1744,6 +1745,78 @@ class _InactiveElements {
   }
 }
 
+@immutable
+abstract class Hook {
+  HookState createHookState();
+}
+
+class HookState<T extends Hook> {
+  Element _element;
+  BuildContext get context => _element;
+  T _hook;
+  T get hook => _hook;
+
+
+  void initHook() {}
+  void dispose() {}
+  void didUpdateWidget(covariant Widget widget) {}
+  void didUpdateHook(covariant Hook hook) {}
+
+  void setState(VoidCallback callback) {
+    callback();
+    _element.markNeedsBuild();
+  }
+}
+
+class _StreamHook<T> extends Hook {
+  final Stream<T> stream;
+  final T initialData;
+
+  _StreamHook({this.stream, this.initialData});
+
+  @override
+  HookState<Hook> createHookState() => _StreamHookState<T>();
+}
+
+class _StreamHookState<T> extends HookState<_StreamHook<T>> {
+  StreamSubscription<T> subscription;
+  AsyncSnapshot<T> snapshot;
+
+  @override
+  void initHook() {
+    super.initHook();
+    snapshot = hook.stream == null
+     ? AsyncSnapshot<T>.nothing()
+     : AsyncSnapshot<T>.withData(ConnectionState.waiting, hook.initialData);
+    subscription = hook.stream.listen(onData, onDone: onDone, onError: onError);
+  }
+            
+  void onData(T event) {
+    print('on data $event');
+    setState(() {
+      snapshot = AsyncSnapshot<T>.withData(ConnectionState.active, event);
+    });
+  }
+        
+  void onDone() {
+    setState(() {
+      snapshot = AsyncSnapshot<T>.withData(ConnectionState.done, snapshot.data);
+    });
+  }
+
+  void onError(Object error) {
+    setState(() {
+      snapshot = AsyncSnapshot<T>.withError(ConnectionState.active, error);
+    });
+  }
+
+  @override
+  void dispose() {
+    subscription?.cancel();
+    super.dispose();
+  }
+}
+
 /// Signature for the callback to [BuildContext.visitChildElements].
 ///
 /// The argument is the child being visited.
@@ -1847,6 +1920,10 @@ abstract class BuildContext {
   /// usually has many render objects and therefore the distance to the nearest
   /// render object is usually short.
   RenderObject findRenderObject();
+
+  HookState<T> useHook<T extends Hook>(Hook hook);
+  
+  AsyncSnapshot<T> useStream<T>(Stream<T> stream, {T initialData});
 
   /// The size of the [RenderBox] returned by [findRenderObject].
   ///
@@ -2580,6 +2657,40 @@ abstract class Element extends DiagnosticableTree implements BuildContext {
   BuildOwner _owner;
 
   bool _active = false;
+
+  int _hooksIndex;
+  List<HookState<dynamic>> _hooks;
+
+  HookState<T> useHook<T extends Hook>(Hook hook) {
+    final int hooksIndex = _hooksIndex;
+    _hooksIndex++;
+
+
+    _hooks ??= <HookState<dynamic>>[];
+  
+    HookState<T> state;
+    if (hooksIndex >= _hooks.length) {
+      state = hook.createHookState()
+        .._element = this
+        .._hook = hook
+        ..initHook();
+      _hooks.add(state);
+    }
+    else {
+      state = _hooks[hooksIndex];
+      if (!identical(state._hook,hook)) {
+        final Hook previousHook = state._hook;
+        state._hook = hook;
+        state.didUpdateHook(previousHook);
+      }
+    }
+    return state;
+  }
+
+  AsyncSnapshot<T> useStream<T>(Stream<T> stream, { T initialData }) {
+    final _StreamHookState<T> state = useHook(_StreamHook<T>(stream: stream, initialData: initialData));
+    return state.snapshot;
+  }
 
   @mustCallSuper
   void _reassemble() {
@@ -3527,6 +3638,7 @@ abstract class Element extends DiagnosticableTree implements BuildContext {
       owner._debugCurrentBuildTarget = this;
       return true;
     }());
+    _hooksIndex = 0;
     performRebuild();
     assert(() {
       assert(owner._debugCurrentBuildTarget == this);
