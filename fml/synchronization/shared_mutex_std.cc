fd0911cc0fb66a17a1486a27af175109879a8042
Jason Simmons <jason-simmons@users.noreply.github.com>
2018-11-17 22:04:37 -0800

    Guard the service protocol's global handlers list with a reader/writer lock (#6888) (#6895)
    The service protocol holds the lock while waiting for completion of service
    RPC tasks.  These tasks (specifically hot restart/RunInView) may need to
    modify a handler's description data.
    
    Task execution and ServiceProtocol::SetHandlerDescription will obtain a shared
    lock to make this possible.  AddHandler and RemoveHandler will obtain an
    exclusive lock in order to guard against a handler being deleted while a
    service task is running.

---
+++ fml/synchronization/shared_mutex_std.cc
@@ -0,0 +1,25 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "flutter/fml/synchronization/shared_mutex_std.h"
+
+namespace fml {
+
+SharedMutex* SharedMutex::Create() {
+  return new SharedMutexStd();
+}
+
+void SharedMutexStd::Lock() {
+  mutex_.lock();
+}
+
+void SharedMutexStd::LockShared() {
+  mutex_.lock_shared();
+}
+
+void SharedMutexStd::Unlock() {
+  mutex_.unlock();
+}
+
+}  // namespace fml
