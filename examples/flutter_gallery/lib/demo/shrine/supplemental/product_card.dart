f91ec816c7b8f1794c37e0139ef537b1baab2ec8
Hans Muller <hansmuller@google.com>
2018-11-19 10:33:51 -0800

    added property TODO

--- examples/flutter_gallery/lib/demo/shrine/supplemental/product_card.dart
+++ examples/flutter_gallery/lib/demo/shrine/supplemental/product_card.dart
@@ -48,7 +48,7 @@ class ProductCard extends StatelessWidget {
         return GestureDetector(
           onTap: () {
             model.addProductToCart(product.id);
-            // TODO: Add Snackbar
+            // TODO(redbrogdon): Add Snackbar
           },
           child: child,
         );
