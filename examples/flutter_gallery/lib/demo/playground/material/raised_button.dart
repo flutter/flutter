e9760162de5b362197a6a681ef27a661dfb43929
btobin <brianjtobin@gmail.com>
2018-11-19 20:28:33 -0800

    raised button properties

--- examples/flutter_gallery/lib/demo/playground/material/raised_button.dart
+++ examples/flutter_gallery/lib/demo/playground/material/raised_button.dart
@@ -10,14 +10,14 @@ class MaterialRaisedButtonDemo extends StatefulWidget {
 
 class _MaterialRaisedButtonDemoState extends State<MaterialRaisedButtonDemo> {
   double elevation = 8.0;
-  double borderShape = 8.0;
+  String borderShape = 'rounded';
   Color color = Colors.blue;
   Color splashColor = Colors.lightBlue;
 
   static const double labelFontSize = 16.0;
-  static const double controlHeight = 65.0;
+  static const double controlHeight = 40.0;
+  static const double selectedElevation = 3.0;
   static const Color labelColor = Colors.blue;
-  
 
   String _code() {
     return """
@@ -37,25 +37,7 @@ FlatButton(
       padding: const EdgeInsets.all(16.0).copyWith(top: 30.0),
       child: Text(
         _code(),
-        style: TextStyle(
-          color: Colors.grey[800],
-          fontFamily: 'Monospace'
-        ),
-      ),
-    );
-  }
-
-  Widget _primaryWidget() {
-    return Padding(
-      padding: const EdgeInsets.only(top: 19.0).copyWith(bottom: 20.0),
-      child: Center(
-        child: RaisedButton(
-          child: const Text('Button'),
-          elevation: elevation,
-          splashColor: splashColor,
-          color: color,
-          onPressed: () {},
-        ),
+        style: TextStyle(color: Colors.grey[800], fontFamily: 'Monospace'),
       ),
     );
   }
@@ -65,71 +47,82 @@ FlatButton(
       padding: const EdgeInsets.only(bottom: 5.0),
       height: controlHeight,
       child: ListView(
-        padding: const EdgeInsets.all(10.0),
-        scrollDirection: Axis.horizontal,
-        children: children
-      ),
-    );
-  }
-
-  Widget _rawButton() {
-    return RawMaterialButton(
-      onPressed: () {
-      },
-      constraints: const BoxConstraints.tightFor(
-        width: 32.0,
-        height: 32.0,
-      ),
-      fillColor: Colors.pink,
-      shape: const CircleBorder(
-        side: BorderSide(
-          color: Colors.grey,
-          width: 2.0,
-        ),
-      ),
-      child: Semantics(
-        value: '',
-        selected: false,
-      ),
+          padding: const EdgeInsets.all(10.0),
+          scrollDirection: Axis.horizontal,
+          children: children),
     );
   }
 
   Widget _propertyColumn(String label, Widget widget) {
     return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Padding(
-          padding: const EdgeInsets.only(left: 15.0).copyWith(top: 15.0).copyWith(bottom: 20.0),
-          child: Text(
-            label,
-            style: const TextStyle(
-              color: labelColor,
-              fontSize: labelFontSize,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(left: 15.0)
+                .copyWith(top: 15.0)
+                .copyWith(bottom: 20.0),
+            child: Text(
+              label,
+              style: const TextStyle(
+                color: labelColor,
+                fontSize: labelFontSize,
+              ),
             ),
           ),
-        ),
-        widget,
-      ]
-    );
+          Padding(
+            padding: const EdgeInsets.only(bottom: 10.0),
+            child: widget,
+          ),
+        ]);
   }
 
-  Widget _shapeButton(Color value) {
-    return RaisedButton(
-      shape: const CircleBorder(
-        side: BorderSide(
-          color: Colors.grey,
-          width: 2.0,
-        ),
+  ShapeBorder _borderShape(String type, [bool side = true]) {
+    ShapeBorder shape;
+
+    BorderSide borderSide = const BorderSide(
+      color: Colors.grey,
+      width: 2.0,
+    );
+
+    borderSide = side ? borderSide : BorderSide.none;
+
+    switch (type) {
+      case 'box':
+        shape = RoundedRectangleBorder(
+            borderRadius: BorderRadius.zero, side: borderSide);
+        break;
+      case 'rounded':
+        shape = RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10.0), side: borderSide);
+        break;
+      case 'beveled':
+        shape = BeveledRectangleBorder(
+            borderRadius: BorderRadius.circular(10.0),
+            side: borderSide.copyWith(width: 1.0));
+        break;
+      case 'circle':
+        shape = CircleBorder(side: borderSide);
+        break;
+    }
+    return shape;
+  }
+
+  Widget _shapeButton(String shape) {
+    return ButtonTheme(
+      minWidth: controlHeight,
+      height: controlHeight,
+      child: RaisedButton(
+        shape: _borderShape(shape),
+        color: borderShape == shape ? Colors.blue : Colors.white,
+        elevation: borderShape == shape ? selectedElevation : 0.0,
+        child: const Text(''),
+        onPressed: () {
+          setState(() {
+            borderShape = shape;
+          });
+        },
       ),
-      color: value,
-      child: const Text(''),
-      elevation: 0.0,
-      onPressed: () {
-        setState(() {
-          color = value;
-        });
-      },
     );
   }
 
@@ -138,14 +131,14 @@ FlatButton(
       minWidth: controlHeight,
       height: controlHeight,
       child: RaisedButton(
-        shape: CircleBorder(
+        shape: StadiumBorder(
           side: BorderSide(
-            color: Colors.grey[350],
+            color: color == value ? Colors.white : Colors.grey[350],
             width: 2.0,
           ),
         ),
         color: value,
-        elevation: color == value ? 3.0 : 0.0,
+        elevation: color == value ? selectedElevation : 0.0,
         child: const Text(''),
         onPressed: () {
           setState(() {
@@ -165,7 +158,7 @@ FlatButton(
           value: elevation,
           min: 0.0,
           max: 24.0,
-          divisions: 3,
+          divisions: 6,
           onChanged: (double value) {
             setState(() {
               elevation = value;
@@ -179,26 +172,73 @@ FlatButton(
   Widget _shapeControl() {
     return _propertyColumn(
       'Shape',
-      _buttonListContainer(<Widget>[
-        _colorButton(Colors.white),
-        _colorButton(Colors.orange),
-        _colorButton(Colors.cyan[200]),
-        _colorButton(Colors.lightBlue[300]),
-      ])
+      Container(
+        padding: const EdgeInsets.only(left: controlHeight)
+            .copyWith(right: controlHeight),
+        height: controlHeight,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            _shapeButton('box'),
+            _shapeButton('beveled'),
+            _shapeButton('rounded'),
+            _shapeButton('circle'),
+          ],
+        ),
+      ),
     );
   }
 
   Widget _colorControl() {
     return _propertyColumn(
       'Color',
-      _buttonListContainer(<Widget>[
-        _colorButton(Colors.white),
-        _colorButton(Colors.orange),
-        _colorButton(Colors.cyan[200]),
-        _colorButton(Colors.lightBlue[300]),
-        _colorButton(Colors.blue),
-        _colorButton(Colors.blue[800]),
-      ])
+      Container(
+        padding: const EdgeInsets.only(left: 20.0).copyWith(right: 20.0),
+        height: controlHeight,
+        child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              _colorButton(Colors.white),
+              _colorButton(Colors.orange),
+              _colorButton(Colors.cyan[200]),
+              _colorButton(Colors.lightBlue[300]),
+              _colorButton(Colors.blue),
+              _colorButton(Colors.blue[800]),
+            ]),
+      ),
+    );
+  }
+
+  Widget _primaryWidget() {
+    return Padding(
+      padding: const EdgeInsets.only(top: controlHeight)
+          .copyWith(bottom: controlHeight),
+      child: Center(
+        child: ButtonTheme(
+          minWidth: 160.0,
+          height: 50.0,
+          child: RaisedButton(
+            padding: const EdgeInsets.all(5.0),
+            color: color,
+            child: Text(
+              'BUTTON',
+              style: TextStyle(
+                fontSize: 16.0,
+                color: color == Colors.white ? Colors.grey[900] : Colors.white,
+              ),
+            ),
+            shape: borderShape == 'circle'
+                ? RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30.0))
+                : _borderShape(borderShape, false),
+            elevation: elevation,
+            splashColor: splashColor,
+            onPressed: () {},
+          ),
+        ),
+      ),
     );
   }
 
