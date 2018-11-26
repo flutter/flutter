0fc87277c8e16fa747e14c1b476c74c7ce57784a
Jonah Williams <jonahwilliams@google.com>
2018-11-26 08:57:49 -0800

    Update focus_test.dart
    Remove unrelated `hide group` clause on import

--- packages/flutter/test/widgets/focus_test.dart
+++ packages/flutter/test/widgets/focus_test.dart
@@ -4,7 +4,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_test/flutter_test.dart' hide group;
+import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter/widgets.dart';
 
 class TestFocusable extends StatefulWidget {
