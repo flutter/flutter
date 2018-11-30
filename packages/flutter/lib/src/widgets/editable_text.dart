dce0644cd03e006acada201925bcfaa7c1e79d69
jslavitz <jslavitz@google.com>
2018-11-30 11:52:13 -0800

    fixed import

--- packages/flutter/lib/src/widgets/editable_text.dart
+++ packages/flutter/lib/src/widgets/editable_text.dart
@@ -8,7 +8,7 @@ import 'dart:ui' as ui;
 import 'package:flutter/rendering.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flutter/widgets.dart' show TickerProviderStateMixin;
 
 import 'automatic_keep_alive.dart';
 import 'basic.dart';
