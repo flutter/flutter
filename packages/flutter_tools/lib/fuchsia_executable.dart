bc3012320346d8508cc97184905f8c8bfd05948a
jslavitz <jslavitz@google.com>
2018-12-03 17:11:00 -0800

    fix

--- packages/flutter_tools/lib/fuchsia_executable.dart
+++ packages/flutter_tools/lib/fuchsia_executable.dart
@@ -1,9 +1,13 @@
 // Copyright 2018 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+
 import 'dart:async';
+
 import 'package:args/args.dart';
+
 import 'runner.dart' as runner;
+
 import 'src/artifacts.dart';
 import 'src/base/common.dart';
 import 'src/base/context.dart';
@@ -14,7 +18,8 @@ import 'src/commands/shell_completion.dart';
 import 'src/fuchsia/fuchsia_sdk.dart';
 import 'src/run_hot.dart';
 import 'src/runner/flutter_command.dart';
-final ArgParser parser = ArgParser.allowAnything()
+
+ final ArgParser parser = ArgParser.allowAnything()
   ..addOption('verbose', abbr: 'v')
   ..addOption('help', abbr: 'h')
   ..addOption(
@@ -29,6 +34,7 @@ final ArgParser parser = ArgParser.allowAnything()
     'ssh-config',
     help: 'The path to the ssh configuration file.',
   );
+
 /// Main entry point for fuchsia commands.
 ///
 /// This function is intended to be used within the fuchsia source tree.
@@ -40,6 +46,7 @@ Future<void> main(List<String> args) async {
   final File dartSdk = fs.file(results['dart-sdk']);
   final File frontendServer = fs.file(results['frontend-server']);
   final File sshConfig = fs.file(results['ssh-config']);
+
   if (!dartSdk.existsSync()) {
     throwToolExit('--dart-sdk is required: ${dartSdk.path} does not exist.');
   }
@@ -49,21 +56,22 @@ Future<void> main(List<String> args) async {
   if (!sshConfig.existsSync()) {
     throwToolExit('--ssh-config is required: ${sshConfig.path} does not exist.');
   }
+
   await runner.run(args, <FlutterCommand>[
     AttachCommand(verboseHelp: verboseHelp),
     DevicesCommand(),
     ShellCompletionCommand(),
   ], verbose: verbose,
-      muteCommandLogging: help,
-      verboseHelp: verboseHelp,
-      overrides: <Type, Generator>{
-        FuchsiaArtifacts: () => FuchsiaArtifacts(sshConfig: sshConfig),
-        Artifacts: () => OverrideArtifacts(
-          parent: CachedArtifacts(),
-          frontendServer: frontendServer,
-          engineDartBinary: dartSdk,
-        ),
-        HotRunnerConfig: () => HotRunnerConfig()
-          ..computeDartDependencies = false,
-      });
+     muteCommandLogging: help,
+     verboseHelp: verboseHelp,
+     overrides: <Type, Generator>{
+       FuchsiaArtifacts: () => FuchsiaArtifacts(sshConfig: sshConfig),
+       Artifacts: () => OverrideArtifacts(
+         parent: CachedArtifacts(),
+         frontendServer: frontendServer,
+         engineDartBinary: dartSdk,
+       ),
+       HotRunnerConfig: () => HotRunnerConfig()
+         ..computeDartDependencies = false,
+     });
 }
