19620dfa7e58e7e2a94c697dd35dde178be76466
GaryQian <garyq@google.com>
2018-12-10 11:18:23 -0800

    Indentation

--- packages/flutter_tools/lib/src/commands/build.dart
+++ packages/flutter_tools/lib/src/commands/build.dart
@@ -45,7 +45,7 @@ abstract class BuildSubCommand extends FlutterCommand {
   @override
   @mustCallSuper
   Future<FlutterCommandResult> runCommand() async {
-        final String channel = FlutterVersion.instance.getBranchName();
+    final String channel = FlutterVersion.instance.getBranchName();
     if (channel == 'master') {
       printStatus('🐉', newline: false, color: TerminalColor.red);
       printStatus(' This is the $channel channel. Shipping apps from this channel is not recommended as it has not been as heavily tested as the stable channel. To build using the stable channel, consider using:\n    "flutter channel stable".');
