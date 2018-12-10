e77f785bfa627813696a3c70f0bb60ee73ce617d
Jonah Williams <jonahwilliams@google.com>
2018-12-10 12:22:10 -0800

    Update fuchsia_attach.dart

--- packages/flutter_tools/bin/fuchsia_attach.dart
+++ packages/flutter_tools/bin/fuchsia_attach.dart
@@ -22,7 +22,7 @@ final ArgParser parser = ArgParser()
   ..addOption('build-dir', help: 'The fuchsia build directory')
   ..addOption('dart-sdk', help: 'The prebuilt dart SDK')
   ..addOption('target', help: 'The GN target to attach to')
-  ..addOption('entrypoint', help: 'The filename of the main method. Defaults to main.dart')
+  ..addOption('entrypoint', defaultsTo: 'main.dart', help: 'The filename of the main method. Defaults to main.dart')
   ..addOption('device', help: 'The device id to attach to')
   ..addFlag('verbose', negatable: true);
 
@@ -52,7 +52,7 @@ Future<void> main(List<String> args) async {
   fs.currentDirectory = path;
 
   // Check for a package with a lib directory.
-  final String entrypoint = argResults['entrypoint'] ?? 'main.dart';
+  final String entrypoint = argResults['entrypoint'];
   String targetFile = 'lib/$entrypoint';
   if (!fs.file(targetFile).existsSync()) {
     // Otherwise assume the package is flat.
