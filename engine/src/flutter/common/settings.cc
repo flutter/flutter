f604da64090ea6f8047289c6589248c2025a917d
Chris Bracken <chris@bracken.jp>
2018-12-10 12:36:09 -0800

    Rename dart-non-checked-mode: disable-dart-asserts (flutter/engine#6977)
    In Dart 2, runtime checked mode has been eliminated. Many of these type
    checks have been moved to static compile-time checks, the remainder are
    enforced at runtime, and are no longer optional.

--- engine/src/flutter/common/settings.cc
+++ engine/src/flutter/common/settings.cc
@@ -35,7 +35,7 @@ std::string Settings::ToString() const {
   stream << "trace_startup: " << trace_startup << std::endl;
   stream << "endless_trace_buffer: " << endless_trace_buffer << std::endl;
   stream << "enable_dart_profiling: " << enable_dart_profiling << std::endl;
-  stream << "dart_non_checked_mode: " << dart_non_checked_mode << std::endl;
+  stream << "disable_dart_asserts: " << disable_dart_asserts << std::endl;
   stream << "enable_observatory: " << enable_observatory << std::endl;
   stream << "observatory_port: " << observatory_port << std::endl;
   stream << "ipv6: " << ipv6 << std::endl;
