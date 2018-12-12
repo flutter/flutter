59abf37e251f28c9d85ae819c2bd3445015e13a9
Greg Spencer <gspencergoog@users.noreply.github.com>
2018-12-12 07:29:36 +0000

    Update dev/devicelab/lib/framework/adb.dart
    Co-Authored-By: DanTup <danny@tuppeny.com>

--- dev/devicelab/lib/framework/adb.dart
+++ dev/devicelab/lib/framework/adb.dart
@@ -467,7 +467,7 @@ String get adbPath {
           : Platform.environment['ANDROID_SDK_ROOT'];
 
   if (androidHome == null)
-    throw 'ANDROID_SDK_ROOT environment variable missing. This variable must '
+    throw 'The ANDROID_SDK_ROOT and ANDROID_HOME environment variables are missing. At least one of these variables must '
         'point to the Android SDK directory containing platform-tools.';
 
   final String adbPath = path.join(androidHome, 'platform-tools/adb');
