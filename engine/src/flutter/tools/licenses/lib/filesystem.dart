41ac8b88ec2dca7950e266f9e1cca753ca933951
Chris Bracken <chris@bracken.jp>
2018-12-20 12:12:51 -0800

    [License] Enable directives_ordering lint

--- engine/src/flutter/tools/licenses/lib/filesystem.dart
+++ engine/src/flutter/tools/licenses/lib/filesystem.dart
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'dart:convert';
 import 'dart:collection';
+import 'dart:convert';
 import 'dart:io' as io;
 
 import 'package:path/path.dart' as path;
