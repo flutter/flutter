4d353de4938ec5598ed9b785a6fa75c6290028da
Rusino <Rusino@users.noreply.github.com>
2019-01-02 16:23:35 -0500

    Preparing to remove SK_SUPPORT_LEGACY_PAINT_TEXTMEASURE (#7354)
    * Preparing to remove SK_SUPPORT_LEGACY_PAINT_TEXTMEASURE
    
    * Fixing a bug
    
    * Formatting problem

--- third_party/txt/src/txt/font_skia.cc
+++ third_party/txt/src/txt/font_skia.cc
@@ -15,6 +15,7 @@
  */
 
 #include "font_skia.h"
+#include "SkFont.h"
 
 #include <minikin/MinikinFont.h>
 
@@ -47,33 +48,32 @@ FontSkia::FontSkia(sk_sp<SkTypeface> typeface)
 
 FontSkia::~FontSkia() = default;
 
-static void FontSkia_SetSkiaPaint(sk_sp<SkTypeface> typeface,
-                                  SkPaint* skPaint,
-                                  const minikin::MinikinPaint& paint) {
-  skPaint->setTypeface(std::move(typeface));
-  skPaint->setTextEncoding(kGlyphID_SkTextEncoding);
+static void FontSkia_SetSkiaFont(sk_sp<SkTypeface> typeface,
+                                 SkFont* skFont,
+                                 const minikin::MinikinPaint& paint) {
+  skFont->setTypeface(std::move(typeface));
   // TODO: set more paint parameters from Minikin
-  skPaint->setTextSize(paint.size);
+  skFont->setSize(paint.size);
 }
 
 float FontSkia::GetHorizontalAdvance(uint32_t glyph_id,
                                      const minikin::MinikinPaint& paint) const {
-  SkPaint skPaint;
+  SkFont skFont;
   uint16_t glyph16 = glyph_id;
   SkScalar skWidth;
-  FontSkia_SetSkiaPaint(typeface_, &skPaint, paint);
-  skPaint.getTextWidths(&glyph16, sizeof(glyph16), &skWidth, NULL);
+  FontSkia_SetSkiaFont(typeface_, &skFont, paint);
+  skFont.getWidths(&glyph16, 1, &skWidth);
   return skWidth;
 }
 
 void FontSkia::GetBounds(minikin::MinikinRect* bounds,
                          uint32_t glyph_id,
                          const minikin::MinikinPaint& paint) const {
-  SkPaint skPaint;
+  SkFont skFont;
   uint16_t glyph16 = glyph_id;
   SkRect skBounds;
-  FontSkia_SetSkiaPaint(typeface_, &skPaint, paint);
-  skPaint.getTextWidths(&glyph16, sizeof(glyph16), NULL, &skBounds);
+  FontSkia_SetSkiaFont(typeface_, &skFont, paint);
+  skFont.getWidths(&glyph16, 1, NULL, &skBounds);
   bounds->mLeft = skBounds.fLeft;
   bounds->mTop = skBounds.fTop;
   bounds->mRight = skBounds.fRight;
