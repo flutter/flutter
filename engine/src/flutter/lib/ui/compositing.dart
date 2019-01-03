d1d5d70f3b363f31546f90e44a3ac6cc63c2381e
liyuqian <liyuqian@google.com>
2019-01-03 10:06:17 -0800

    Fix typo clas -> class (flutter/engine#7360)

--- engine/src/flutter/lib/ui/compositing.dart
+++ engine/src/flutter/lib/ui/compositing.dart
@@ -200,7 +200,7 @@ class SceneBuilder extends NativeFieldWrapperClass2 {
   /// All the engine layers that are in the subtree of the retained layer will
   /// be automatically appended to the current engine layer tree.
   ///
-  /// Therefore, when implementing a subclas of the [Layer] concept defined in
+  /// Therefore, when implementing a subclass of the [Layer] concept defined in
   /// the rendering layer of Flutter's framework, once this is called, there's
   /// no need to call [addToScene] for its children layers.
   EngineLayer addRetained(EngineLayer retainedLayer) native 'SceneBuilder_addRetained';
