f1930481955c6e67215fa5072faff9daf8d189b0
jslavitz <jslavitz@google.com>
2019-01-08 13:29:16 -0800

    removed whitespace

--- packages/flutter/lib/src/widgets/gesture_detector.dart
+++ packages/flutter/lib/src/widgets/gesture_detector.dart
@@ -383,7 +383,7 @@ class GestureDetector extends StatelessWidget {
   /// drag behavior feel slightly more reactive.
   ///
   /// By default, the drag start behavior is [DragStartBehavior.start].
-  /// 
+  ///
   /// Only the [onStart] callbacks for the [VerticalDragGestureRecognizer],
   /// [HorizontalDragGestureRecognizer] and [PanGestureRecognizer] are affected
   /// by this setting.
