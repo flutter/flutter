58d391a54cc955aa9a0e9ed90fd2a870f03b750c
Mehmet Fidanboylu <mehmetf@users.noreply.github.com>
2019-01-08 11:06:43 -0800

    Extract TestBorder into a utility file (#25888)

--- packages/flutter/test/widgets/shape_decoration_test.dart
+++ packages/flutter/test/widgets/shape_decoration_test.dart
@@ -11,6 +11,7 @@ import 'package:flutter_test/flutter_test.dart';
 import '../painting/image_data.dart';
 import '../painting/mocks_for_image_cache.dart';
 import '../rendering/mock_canvas.dart';
+import 'test_border.dart' show TestBorder;
 
 Future<void> main() async {
   final ui.Image rawImage = await decodeImageFromList(Uint8List.fromList(kTransparentImage));
@@ -103,35 +104,4 @@ Future<void> main() async {
       ],
     );
   });
-}
-
-typedef Logger = void Function(String caller);
-
-class TestBorder extends ShapeBorder {
-  const TestBorder(this.onLog) : assert(onLog != null);
-
-  final Logger onLog;
-
-  @override
-  EdgeInsetsGeometry get dimensions => const EdgeInsetsDirectional.only(start: 1.0);
-
-  @override
-  ShapeBorder scale(double t) => TestBorder(onLog);
-
-  @override
-  Path getInnerPath(Rect rect, { TextDirection textDirection }) {
-    onLog('getInnerPath $rect $textDirection');
-    return Path();
-  }
-
-  @override
-  Path getOuterPath(Rect rect, { TextDirection textDirection }) {
-    onLog('getOuterPath $rect $textDirection');
-    return Path();
-  }
-
-  @override
-  void paint(Canvas canvas, Rect rect, { TextDirection textDirection }) {
-    onLog('paint $rect $textDirection');
-  }
 }
