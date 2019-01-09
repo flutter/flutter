14b564ee9c9e54535f5247eafcf2b06d5f64be7e
Dan Field <dfield@gmail.com>
2019-01-09 14:44:42 -0800

    Update docs for editable_text (TextField) (#26269)
    * update docs for editable_text (TextField)
    
    * typo, clarify behavior when lines > 1

--- packages/flutter/lib/src/widgets/editable_text.dart
+++ packages/flutter/lib/src/widgets/editable_text.dart
@@ -346,8 +346,13 @@ class EditableText extends StatefulWidget {
   /// If this is 1 (the default), the text will not wrap, but will scroll
   /// horizontally instead.
   ///
-  /// If this is null, there is no limit to the number of lines. If it is not
-  /// null, the value must be greater than zero.
+  /// If this is null, there is no limit to the number of lines, and the text
+  /// container will start with enough vertical space for one line and
+  /// automatically grow to accomodate additional lines as they are entered.
+  ///
+  /// If it is not null, the value must be greater than zero. If it is greater
+  /// than 1, it will take up enough horizontal space to accomodate that number
+  /// of lines.
   /// {@endtemplate}
   final int maxLines;
 
