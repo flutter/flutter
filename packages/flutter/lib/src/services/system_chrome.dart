944be359a468c25954719a8ce366ee3b90cfd9c4
Jonah Williams <jonahwilliams@google.com>
2019-01-09 11:18:50 -0800

    Update system_chrome.dart

--- packages/flutter/lib/src/services/system_chrome.dart
+++ packages/flutter/lib/src/services/system_chrome.dart
@@ -347,6 +347,7 @@ class SystemChrome {
   /// to a random value on Android.
   ///
   /// ```dart imports
+  ///    import 'package:flutter/services';
   ///    import 'dart:math' as math;
   /// ```
   ///
