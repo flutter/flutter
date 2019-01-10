83af6f48d6d180cf76bf7f6715cd015ac0bf2674
Jonah Williams <jonahwilliams@google.com>
2019-01-10 13:21:03 -0800

    Adds a type parameter to invokeMethod (and additional utility methods) (#26303)

--- packages/flutter/lib/src/services/system_navigator.dart
+++ packages/flutter/lib/src/services/system_navigator.dart
@@ -20,6 +20,6 @@ class SystemNavigator {
   /// the latter may cause the underlying platform to act as if the application
   /// had crashed.
   static Future<void> pop() async {
-    await SystemChannels.platform.invokeMethod('SystemNavigator.pop');
+    await SystemChannels.platform.invokeMethod<void>('SystemNavigator.pop');
   }
 }
