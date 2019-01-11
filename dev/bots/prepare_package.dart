5a434c61afeae59cf6aebc42fb3920d1a807effd
xster <xiao@xster.net>
2019-01-10 19:08:03 -0800

    Let the packaging recipe use gsutil.py (#26402)

--- dev/bots/prepare_package.dart
+++ dev/bots/prepare_package.dart
@@ -534,7 +534,7 @@ class ArchivePublisher {
     bool failOk = false,
   }) async {
     return _processRunner.runProcess(
-      <String>['gsutil']..addAll(args),
+      <String>['gsutil.py', '--']..addAll(args),
       workingDirectory: workingDirectory,
       failOk: failOk,
     );
@@ -561,7 +561,7 @@ class ArchivePublisher {
       args.addAll(<String>['-h', 'Content-Type:$mimeType']);
     }
     args.addAll(<String>['cp', src, dest]);
-    return _runGsUtil(args);
+    return await _runGsUtil(args);
   }
 }
 
