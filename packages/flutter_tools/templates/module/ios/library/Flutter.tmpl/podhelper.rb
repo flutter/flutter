ea7b4db0ff69758a2f6e88792b5c1538811bfe5c
mcplectrum <torben_sch@hotmail.com>
2019-01-15 20:26:00 +0100

    Disable bitcode in every case

--- packages/flutter_tools/templates/module/ios/library/Flutter.tmpl/podhelper.rb
+++ packages/flutter_tools/templates/module/ios/library/Flutter.tmpl/podhelper.rb
@@ -65,8 +65,8 @@ plugin_pods.map { |r|
 post_install do |installer|
     installer.pods_project.targets.each do |target|
         target.build_configurations.each do |config|
-            next if  config.base_configuration_reference == nil
             config.build_settings['ENABLE_BITCODE'] = 'NO'
+            next if  config.base_configuration_reference == nil
             xcconfig_path = config.base_configuration_reference.real_path
             File.open(xcconfig_path, 'a+') do |file|
                 file.puts "#include \"#{File.realpath(File.join(framework_dir, 'Generated.xcconfig'))}\""
