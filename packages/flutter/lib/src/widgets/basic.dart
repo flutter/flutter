f808e838923e436ff8d36ee79ec5392f4bb4f3c1
Kartik Sharma <kartik.sharma1607@outlook.com>
2019-01-15 20:42:36 +0530

    Added Sample code for Stack widget

--- packages/flutter/lib/src/widgets/basic.dart
+++ packages/flutter/lib/src/widgets/basic.dart
@@ -2851,6 +2851,44 @@ class ListBody extends MultiChildRenderObjectWidget {
 /// [CustomMultiChildLayout] instead. In particular, when using a [Stack] you
 /// can't position children relative to their size or the stack's own size.
 ///
+/// {@tool sample}
+/// 
+/// This example shows how Stack can be used to enhance text visibility
+/// by adding gradient backdrops.
+/// 
+/// SizedBox(
+///   height: 250,
+///   child: Stack(
+///     fit: StackFit.expand,
+///     children: <Widget>[
+///       Image.asset(
+///         "assets/pizza.jpg",
+///         fit: BoxFit.cover,
+///       ),
+///       Container(
+///         padding: EdgeInsets.all(5.0),
+///         alignment: Alignment.bottomCenter,
+///         decoration: BoxDecoration(
+///           gradient: LinearGradient(
+///             begin: Alignment.topCenter,
+///             end: Alignment.bottomCenter,
+///             colors: <Color>[
+///               Colors.black.withAlpha(0),
+///               Colors.black12,
+///               Colors.black45
+///             ],
+///           ),
+///         ),
+///         child: Text(
+///           "Yummy Pizza",
+///           style: TextStyle(color: Colors.white, fontSize: 20.0),
+///         ),
+///       ),
+///     ],
+///   ),
+/// )
+/// {@end-tool}
+///
 /// See also:
 ///
 ///  * [Align], which sizes itself based on its child's size and positions
