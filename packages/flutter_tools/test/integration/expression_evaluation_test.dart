d93a294c9b2aafdc40e6b22966eb34bbb2b97ae4
Danny Tuppeny <dantup@google.com>
2019-01-16 16:44:10 +0000

    Swap the last two prefixes

--- packages/flutter_tools/test/integration/expression_evaluation_test.dart
+++ packages/flutter_tools/test/integration/expression_evaluation_test.dart
@@ -22,7 +22,7 @@ void main() {
     FlutterRunTestDriver _flutter;
 
     setUp(() async {
-      tempDir = createResolvedTempDirectorySync('run_expression_eval_test.');
+      tempDir = createResolvedTempDirectorySync('expression_test.');
       await _project.setUpIn(tempDir);
       _flutter = FlutterRunTestDriver(tempDir);
     });
@@ -87,7 +87,7 @@ void main() {
     FlutterTestTestDriver _flutter;
 
     setUp(() async {
-      tempDir = createResolvedTempDirectorySync('expression_test.');
+      tempDir = createResolvedTempDirectorySync('test_expression_eval_test.');
       await _project.setUpIn(tempDir);
       _flutter = FlutterTestTestDriver(tempDir);
     });
