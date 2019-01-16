4b8cfb84e57905b89288e29721f125e8f0784c6f
Michael Goderbauer <goderbauer@google.com>
2019-01-15 18:56:21 -0800

    Revert "Revert "Add elevation and thickness to SemanticsNode (#7282)" (#7483)" (flutter/engine#7484)
    This reverts commit ba3b5d9c9e5d275b1f1be4a77d961750a2857dc6.

--- engine/src/flutter/lib/ui/semantics.dart
+++ engine/src/flutter/lib/ui/semantics.dart
@@ -607,6 +607,14 @@ class SemanticsUpdateBuilder extends NativeFieldWrapperClass2 {
   ///
   /// The `transform` is a matrix that maps this node's coordinate system into
   /// its parent's coordinate system.
+  ///
+  /// The `elevation` describes the distance in z-direction between this node
+  /// and the `elevation` of the parent.
+  ///
+  /// The `thickness` describes how much space this node occupies in the
+  /// z-direction starting at `elevation`. Basically, in the z-direction the
+  /// node starts at `elevation` above the parent and ends at `elevation` +
+  /// `thickness` above the parent.
   void updateNode({
     int id,
     int flags,
@@ -618,6 +626,8 @@ class SemanticsUpdateBuilder extends NativeFieldWrapperClass2 {
     double scrollPosition,
     double scrollExtentMax,
     double scrollExtentMin,
+    double elevation,
+    double thickness,
     Rect rect,
     String label,
     String hint,
@@ -647,6 +657,8 @@ class SemanticsUpdateBuilder extends NativeFieldWrapperClass2 {
       rect.top,
       rect.right,
       rect.bottom,
+      elevation,
+      thickness,
       label,
       hint,
       value,
@@ -674,6 +686,8 @@ class SemanticsUpdateBuilder extends NativeFieldWrapperClass2 {
     double top,
     double right,
     double bottom,
+    double elevation,
+    double thickness,
     String label,
     String hint,
     String value,
