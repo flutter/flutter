244b077bee02ce86d325610480541df7a165f12f
Danny Tuppeny <danny@tuppeny.com>
2019-01-17 11:29:18 +0000

    Give integration tests unique temp folder names (#26227)
    This will help track down any that aren't cleaning up and also may help track down leaked flutter_tester processes (https://github.com/dart-lang/sdk/issues/35549).

--- packages/flutter_tools/test/integration/daemon_mode_test.dart
+++ packages/flutter_tools/test/integration/daemon_mode_test.dart
@@ -22,7 +22,7 @@ void main() {
     Process process;
 
     setUp(() async {
-      tempDir = createResolvedTempDirectorySync();
+      tempDir = createResolvedTempDirectorySync('daemon_mode_test.');
       await _project.setUpIn(tempDir);
     });
 
