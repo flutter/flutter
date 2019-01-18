b66f23c027288b8364f7f08517adb2a2491ac675
Alexander Aprelev <aam@google.com>
2019-01-18 10:15:04 -0800

    Report early error if appropriate host local engine is not found.

--- packages/flutter_tools/lib/src/runner/flutter_command_runner.dart
+++ packages/flutter_tools/lib/src/runner/flutter_command_runner.dart
@@ -457,6 +457,9 @@ class FlutterCommandRunner extends CommandRunner<void> {
     final String basename = fs.path.basename(engineBuildPath);
     final String hostBasename = 'host_' + basename.replaceFirst('_sim_', '_').substring(basename.indexOf('_') + 1);
     final String engineHostBuildPath = fs.path.normalize(fs.path.join(fs.path.dirname(engineBuildPath), hostBasename));
+    if (!fs.isDirectorySync(engineHostBuildPath)) {
+      throwToolExit(userMessages.runnerNoEngineBuild(engineHostBuildPath), exitCode: 2);
+    }
 
     return EngineBuildPaths(targetEngine: engineBuildPath, hostEngine: engineHostBuildPath);
   }
