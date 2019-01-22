c57e66fdae38d135ec2435b8eebb98cc23bcc43a
jslavitz <jslavitz@google.com>
2019-01-22 15:09:02 -0800

    fixes android pressure range (#7516)

--- shell/platform/android/io/flutter/view/FlutterView.java
+++ shell/platform/android/io/flutter/view/FlutterView.java
@@ -474,11 +474,10 @@ public class FlutterView extends SurfaceView
 
         packet.putLong(0); // obscured
 
-        // TODO(eseidel): Could get the calibrated range if necessary:
-        // event.getDevice().getMotionRange(MotionEvent.AXIS_PRESSURE)
+        InputDevice.MotionRange pressureRange = event.getDevice().getMotionRange(MotionEvent.AXIS_PRESSURE);
         packet.putDouble(event.getPressure(pointerIndex)); // pressure
-        packet.putDouble(0.0); // pressure_min
-        packet.putDouble(1.0); // pressure_max
+        packet.putDouble(pressureRange.getMin()); // pressure_min
+        packet.putDouble(pressureRange.getMax()); // pressure_max
 
         if (pointerKind == kPointerDeviceKindStylus) {
             packet.putDouble(event.getAxisValue(MotionEvent.AXIS_DISTANCE, pointerIndex)); // distance
