e16edbf518a0f36c7d493522dd640cb741e6aa7e
Jonah Williams <jonahwilliams@google.com>
2019-01-22 18:40:13 -0800

    make FDE opt-in via the environment variable (#26898)

--- packages/flutter_tools/test/macos/macos_workflow_test.dart
+++ packages/flutter_tools/test/macos/macos_workflow_test.dart
@@ -4,9 +4,7 @@
 
 import 'package:mockito/mockito.dart';
 
-import 'package:flutter_tools/src/base/file_system.dart';
 import 'package:flutter_tools/src/base/platform.dart';
-import 'package:flutter_tools/src/cache.dart';
 import 'package:flutter_tools/src/macos/macos_workflow.dart';
 
 import '../src/common.dart';
@@ -15,8 +13,11 @@ import '../src/context.dart';
 void main() {
   group(MacOSWorkflow, () {
     final MockPlatform mac = MockPlatform();
+    final MockPlatform macWithFde = MockPlatform()
+      ..environment['FLUTTER_DESKTOP_EMBEDDING'] = 'true';
     final MockPlatform notMac = MockPlatform();
     when(mac.isMacOS).thenReturn(true);
+    when(macWithFde.isMacOS).thenReturn(true);
     when(notMac.isMacOS).thenReturn(false);
 
     testUsingContext('Applies to mac platform', () {
@@ -30,30 +31,17 @@ void main() {
       Platform: () => notMac,
     });
 
-    final MockFileSystem fileSystem = MockFileSystem();
-    final MockDirectory directory = MockDirectory();
-    Cache.flutterRoot = '';
-    when(fileSystem.directory(Cache.flutterRoot)).thenReturn(directory);
-    when(directory.parent).thenReturn(directory);
-    when(directory.childDirectory('flutter-desktop-embedding')).thenReturn(directory);
-    when(directory.existsSync()).thenReturn(true);
-
     testUsingContext('defaults', () {
       expect(macOSWorkflow.canListEmulators, false);
       expect(macOSWorkflow.canLaunchDevices, true);
       expect(macOSWorkflow.canListDevices, true);
     }, overrides: <Type, Generator>{
-      Platform: () => mac,
-      FileSystem: () => fileSystem,
+      Platform: () => macWithFde,
     });
   });
 }
 
-class MockFileSystem extends Mock implements FileSystem {}
-
-class MockDirectory extends Mock implements Directory {}
-
 class MockPlatform extends Mock implements Platform {
   @override
-  Map<String, String> get environment => const <String, String>{};
+  Map<String, String> environment = <String, String>{};
 }
