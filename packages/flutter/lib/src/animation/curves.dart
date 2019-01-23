32375f1ca1197fdcd33bf784dc897740d9a189c2
Marco Scannadinari <m@scannadinari.co.uk>
2019-01-23 16:25:42 +0000

    Expand SuspendedCurve class docs

--- packages/flutter/lib/src/animation/curves.dart
+++ packages/flutter/lib/src/animation/curves.dart
@@ -472,6 +472,19 @@ class ElasticInOutCurve extends Curve {
 /// A curve that progresses linearly until a specified [startingPoint], at which
 /// point [curve] will begin. Unlike [Interval], [curve] will not start at zero,
 /// but will use [startingPoint] as the Y position.
+///
+/// For example, if [startingPoint] is set to `0.5`, and [curve] is set to
+/// [Curves.easeOut], then the bottom-left quarter of the curve will be a
+/// straight line, and the top-right quarter will contain the entire contents of
+/// [Curves.easeOut].
+///
+/// This is useful in situations where a widget must track the user's finger
+/// (which requires a linear animation), and afterwards can be flung using a
+/// curve specified with the [curve] argument, after the finger is released. In
+/// such a case, the value of [startingPoint] would be the progress of the
+/// animation at the time when the finger was released.
+///
+/// The [startingPoint] and [curve] arguments must not be null.
 class SuspendedCurve extends Curve {
   /// Creates a suspended curve.
   const SuspendedCurve(this.startingPoint, { this.curve = Curves.easeOutCubic })
