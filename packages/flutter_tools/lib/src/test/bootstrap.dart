411ae2ee15353608b48442bd829311504bc8fc92
Anthony Bullard <arbullard@google.com>
2019-01-25 10:39:19 -0800

    Address review feedback
    Removed references to 'hot reload', and cleaned style nits.
    Also, updated packages to pass analyze.

--- packages/flutter_tools/lib/src/test/bootstrap.dart
+++ packages/flutter_tools/lib/src/test/bootstrap.dart
@@ -1,3 +1,7 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import 'package:meta/meta.dart';
 import '../base/file_system.dart';
 import '../base/io.dart';
