d27b340205770f00bcf0babaf1b2a3accd912813
Gary Qian <garyq@google.com>
2019-01-24 21:37:56 -0500

    Revert "Experimental flags for hot reloads, fixed" (#27059)
    * Revert "Make UriMapper and StdoutHandler public and add test cases (#26932)"
    
    This reverts commit 6cf554b0c414097ce473a6f37bb1a4d5104df3c0.
    
    * Revert "Update packages (#27046)"
    
    This reverts commit 496c57386bb2e51d0153358d6c29ebd6a5a25ecd.
    
    * Revert "roll engine for TODAY bug (#27049)"
    
    This reverts commit 5ae6346f1d46af0f7cc30e9fcbd985e3242c9c76.
    
    * Revert "Warn when building on master channel (#25007)"
    
    This reverts commit dd65a54628159af18c0ab04d4868456526fa91e5.
    
    * Revert "Add experimentalBuildEnabled flag and initial shim for build_runner (#26989)"
    
    This reverts commit 1237ee8f63995e1bdddd651e6d28daf6ab145f3d.
    
    * Revert "Experimental flags for hot reloads, fixed (#27043)"
    
    This reverts commit 2c05d08f0ca1301616220b656dce70839419493c.

--- packages/flutter_tools/lib/src/compile.dart
+++ packages/flutter_tools/lib/src/compile.dart
@@ -331,8 +331,7 @@ class ResidentCompiler {
     CompilerMessageConsumer compilerMessageConsumer = printError,
     String initializeFromDill,
     TargetModel targetModel = TargetModel.flutter,
-    bool unsafePackageSerialization,
-    List<String> experimentalFlags,
+    bool unsafePackageSerialization
   }) : assert(_sdkRoot != null),
        _trackWidgetCreation = trackWidgetCreation,
        _packagesPath = packagesPath,
@@ -342,8 +341,7 @@ class ResidentCompiler {
        _stdoutHandler = StdoutHandler(consumer: compilerMessageConsumer),
        _controller = StreamController<_CompilationRequest>(),
        _initializeFromDill = initializeFromDill,
-       _unsafePackageSerialization = unsafePackageSerialization,
-       _experimentalFlags = experimentalFlags {
+       _unsafePackageSerialization = unsafePackageSerialization {
     // This is a URI, not a file path, so the forward slash is correct even on Windows.
     if (!_sdkRoot.endsWith('/'))
       _sdkRoot = '$_sdkRoot/';
@@ -359,7 +357,6 @@ class ResidentCompiler {
   final StdoutHandler _stdoutHandler;
   String _initializeFromDill;
   bool _unsafePackageSerialization;
-  final List<String> _experimentalFlags;
 
   final StreamController<_CompilationRequest> _controller;
 
@@ -471,10 +468,6 @@ class ResidentCompiler {
     }
     if (_unsafePackageSerialization == true) {
       command.add('--unsafe-package-serialization');
-    }
-    if (_experimentalFlags != null) {
-      final String expFlags = _experimentalFlags.join(',');
-      command.add('--enable-experiment=$expFlags');
     }
     printTrace(command.join(' '));
     _server = await processManager.start(command);
