f0d1a5e89f65768da225b599f6106d9fe9d46f64
Konstantin Scheglov <scheglov@google.com>
2019-01-29 11:49:57 -0800

    Add default values for optional parameters. (#27197)

--- packages/flutter/test/painting/shape_decoration_test.dart
+++ packages/flutter/test/painting/shape_decoration_test.dart
@@ -125,7 +125,7 @@ class TestImage implements ui.Image {
   void dispose() { }
 
   @override
-  Future<ByteData> toByteData({ui.ImageByteFormat format}) async {
+  Future<ByteData> toByteData({ui.ImageByteFormat format = ui.ImageByteFormat.rawRgba}) async {
     throw UnsupportedError('Cannot encode test image');
   }
 }
