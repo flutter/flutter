d2fa082d594a6bfcbffe1eced3b368c1e8390b3e
Matt Carroll <mattcarroll@nestlabs.com>
2019-02-01 00:54:05 -0800

    Added public comments for analyzer.

--- packages/flutter_test/lib/src/window.dart
+++ packages/flutter_test/lib/src/window.dart
@@ -18,9 +18,11 @@ class TestWindow implements Window {
   @override
   double get devicePixelRatio => _devicePixelRatio ?? _window.devicePixelRatio;
   double _devicePixelRatio;
+  /// Hides the real device pixel ratio and reports the given [devicePixelRatio] instead.
   set devicePixelRatioTestValue(double devicePixelRatio) {
     _devicePixelRatio = devicePixelRatio;
   }
+  /// Deletes any existing test device pixel ratio and returns to using the real device pixel ratio.
   void clearDevicePixelRatioTestValue() {
     _devicePixelRatio = null;
   }
@@ -28,9 +30,11 @@ class TestWindow implements Window {
   @override
   Size get physicalSize => _physicalSizeTestValue ?? _window.physicalSize;
   double _physicalSizeTestValue;
+  /// Hides the real physical size and reports the given [physicalSizeTestValue] instead.
   set physicalSizeTestValue (double physicalSizeTestValue) {
     _physicalSizeTestValue = physicalSizeTestValue;
   }
+  /// Deletes any existing test physical size and returns to using the real physical size.
   void clearPhysicalSizeTestValue() {
     _physicalSizeTestValue = null;
   }
@@ -38,9 +42,11 @@ class TestWindow implements Window {
   @override
   WindowPadding get viewInsets => _viewInsetsTestValue ??  _window.viewInsets;
   WindowPadding _viewInsetsTestValue;
+  /// Hides the real view insets and reports the given [viewInsetsTestValue] instead.
   set viewInsetsTestValue(WindowPadding viewInsetsTestValue) {
     _viewInsetsTestValue = viewInsetsTestValue;
   }
+  /// Deletes any existing test view insets and returns to using the real view insets.
   void clearViewInsetsTestValue() {
     _viewInsetsTestValue = null;
   }
@@ -48,9 +54,11 @@ class TestWindow implements Window {
   @override
   WindowPadding get padding => _paddingTestValue ?? _window.padding;
   WindowPadding _paddingTestValue;
+  /// Hides the real padding and reports the given [paddingTestValue] instead.
   set paddingTestValue(WindowPadding paddingTestValue) {
     _paddingTestValue = paddingTestValue;
   }
+  /// Deletes any existing test padding and returns to using the real padding.
   void clearPaddingTestValue() {
     _paddingTestValue = null;
   }
@@ -65,9 +73,11 @@ class TestWindow implements Window {
   @override
   Locale get locale => _localeTestValue ?? _window.locale;
   Locale _localeTestValue;
+  /// Hides the real locale and reports the given [localeTestValue] instead.
   set localeTestValue(Locale localeTestValue) {
     _localeTestValue = localeTestValue;
   }
+  /// Deletes any existing test locale and returns to using the real locale.
   void clearLocaleTestValue() {
     _localeTestValue = null;
   }
@@ -75,9 +85,11 @@ class TestWindow implements Window {
   @override
   List<Locale> get locales => _localesTestValue ?? _window.locales;
   List<Locale> _localesTestValue;
+  /// Hides the real locales and reports the given [localesTestValue] instead.
   set localesTestValue(List<Locale> localesTestValue) {
     _localesTestValue = localesTestValue;
   }
+  /// Deletes any existing test locales and returns to using the real locales.
   void clearLocalesTestValue() {
     _localesTestValue = null;
   }
@@ -92,9 +104,11 @@ class TestWindow implements Window {
   @override
   double get textScaleFactor => _textScaleFactorTestValue ?? _window.textScaleFactor;
   double _textScaleFactorTestValue;
+  /// Hides the real text scale factor and reports the given [textScaleFactorTestValue] instead.
   set textScaleFactorTestValue(double textScaleFactorTestValue) {
     _textScaleFactorTestValue = textScaleFactorTestValue;
   }
+  /// Deletes any existing test text scale factor and returns to using the real text scale factor.
   void clearTextScaleFactorTestValue() {
     _textScaleFactorTestValue = null;
   }
@@ -102,9 +116,11 @@ class TestWindow implements Window {
   @override
   bool get alwaysUse24HourFormat => _alwaysUse24HourFormatTestValue ?? _window.alwaysUse24HourFormat;
   bool _alwaysUse24HourFormatTestValue;
+  /// Hides the real clock format and reports the given [alwaysUse24HourFormatTestValue] instead.
   set alwaysUse24HourFormatTestValue(bool alwaysUse24HourFormatTestValue) {
     _alwaysUse24HourFormatTestValue = alwaysUse24HourFormatTestValue;
   }
+  /// Deletes any existing test clock format and returns to using the real clock format.
   void clearAlwaysUse24HourTestValue() {
     _alwaysUse24HourFormatTestValue = null;
   }
@@ -140,9 +156,11 @@ class TestWindow implements Window {
   @override
   String get defaultRouteName => _defaultRouteNameTestValue ?? _window.defaultRouteName;
   String _defaultRouteNameTestValue;
+  /// Hides the real default route name and reports the given [defaultRouteNameTestValue] instead.
   set defaultRouteNameTestValue(String defaultRouteNameTestValue) {
     _defaultRouteNameTestValue = defaultRouteNameTestValue;
   }
+  /// Deletes any existing test default route name and returns to using the real default route name.
   void clearDefaultRouteNameTestValue() {
     _defaultRouteNameTestValue = null;
   }
@@ -160,9 +178,11 @@ class TestWindow implements Window {
   @override
   bool get semanticsEnabled => _semanticsEnabledTestValue ?? _window.semanticsEnabled;
   bool _semanticsEnabledTestValue;
+  /// Hides the real semantics enabled and reports the given [semanticsEnabledTestValue] instead.
   set semanticsEnabledTestValue(bool semanticsEnabledTestValue) {
     _semanticsEnabledTestValue = semanticsEnabledTestValue;
   }
+  /// Deletes any existing test semantics enabled and returns to using the real semantics enabled.
   void clearSemanticsEnabledTestValue() {
     _semanticsEnabledTestValue = null;
   }
@@ -184,9 +204,11 @@ class TestWindow implements Window {
   @override
   AccessibilityFeatures get accessibilityFeatures => _accessibilityFeaturesTestValue ?? _window.accessibilityFeatures;
   AccessibilityFeatures _accessibilityFeaturesTestValue;
+  /// Hides the real accessibility features and reports the given [accessibilityFeaturesTestValue] instead.
   set accessibilityFeaturesTestValue(AccessibilityFeatures accessibilityFeaturesTestValue) {
     _accessibilityFeaturesTestValue = accessibilityFeaturesTestValue;
   }
+  /// Deletes any existing test accessibility features and returns to using the real accessibility features.
   void clearAccessibilityFeaturesTestValue() {
     _accessibilityFeaturesTestValue = null;
   }
