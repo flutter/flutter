0bb3a943046a0ba2a378f5a11a3dae85455aac0f
Alexander Aprelev <aam@google.com>
2019-01-31 17:43:32 -0800

    Don't send accept/reject if compilation never started. (#27319)
    * Don't send accept/reject if compilation never started.
    
    Ensure that we wait for reject's response since that is async operation.
    
    Fixes https://github.com/flutter/flutter/issues/27120.
    
    * Fix analysis errors
    
    * Rename flag. Ensure we raise it on first compilation too.

--- packages/flutter_tools/test/compile_test.dart
+++ packages/flutter_tools/test/compile_test.dart
@@ -274,13 +274,27 @@ example:org-dartlang-app:///lib/
       );
       expect(mockFrontendServerStdIn.getAndClear(), 'compile /path/to/main.dart\n');
 
+      // No accept or reject commands should be issued until we
+      // send recompile request.
+      await _accept(streamController, generator, mockFrontendServerStdIn, '');
+      await _reject(streamController, generator, mockFrontendServerStdIn, '', '');
+
       await _recompile(streamController, generator, mockFrontendServerStdIn,
         'result abc\nline1\nline2\nabc /path/to/main.dart.dill 0\n');
+
+      await _accept(streamController, generator, mockFrontendServerStdIn, '^accept\\n\$');
+
+      await _recompile(streamController, generator, mockFrontendServerStdIn,
+          'result abc\nline1\nline2\nabc /path/to/main.dart.dill 0\n');
+
+      await _reject(streamController, generator, mockFrontendServerStdIn, 'result abc\nabc\n',
+          '^reject\\n\$');
 
       verifyNoMoreInteractions(mockFrontendServerStdIn);
       expect(mockFrontendServerStdIn.getAndClear(), isEmpty);
       expect(logger.errorText, equals(
         '\nCompiler message:\nline0\nline1\n'
+        '\nCompiler message:\nline1\nline2\n'
         '\nCompiler message:\nline1\nline2\n'
       ));
     }, overrides: <Type, Generator>{
@@ -501,6 +515,34 @@ Future<void> _recompile(StreamController<List<int>> streamController,
   expect(commands, matches(re));
   final Match match = re.firstMatch(commands);
   expect(match[1] == match[2], isTrue);
+  mockFrontendServerStdIn._stdInWrites.clear();
+}
+
+Future<void> _accept(StreamController<List<int>> streamController,
+    ResidentCompiler generator, MockStdIn mockFrontendServerStdIn,
+    String expected) async {
+  // Put content into the output stream after generator.recompile gets
+  // going few lines below, resets completer.
+  generator.accept();
+  final String commands = mockFrontendServerStdIn.getAndClear();
+  final RegExp re = RegExp(expected);
+  expect(commands, matches(re));
+  mockFrontendServerStdIn._stdInWrites.clear();
+}
+
+Future<void> _reject(StreamController<List<int>> streamController,
+    ResidentCompiler generator, MockStdIn mockFrontendServerStdIn,
+    String mockCompilerOutput, String expected) async {
+  // Put content into the output stream after generator.recompile gets
+  // going few lines below, resets completer.
+  scheduleMicrotask(() {
+    streamController.add(utf8.encode(mockCompilerOutput));
+  });
+  final CompilerOutput output = await generator.reject();
+  expect(output, isNull);
+  final String commands = mockFrontendServerStdIn.getAndClear();
+  final RegExp re = RegExp(expected);
+  expect(commands, matches(re));
   mockFrontendServerStdIn._stdInWrites.clear();
 }
 
