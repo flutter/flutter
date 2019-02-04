c0181b94e4b321f775ac6d1019f2de7342b1a912
Veljko Vuković <42891350+wolfcro1984@users.noreply.github.com>
2019-02-04 21:59:49 +0100

    Added tests for optional header

--- packages/flutter/test/material/paginated_data_table_test.dart
+++ packages/flutter/test/material/paginated_data_table_test.dart
@@ -282,4 +282,77 @@ void main() {
     expect(find.text('Rows per page:'), findsOneWidget);
     expect(tester.getTopLeft(find.text('Rows per page:')).dx, 18.0); // 14 padding in the footer row, 4 padding from the card
   });
+
+  testWidgets('PaginatedDataTable header is null without actions', (WidgetTester tester) async {
+    final TestDataSource source = TestDataSource();
+
+    final PaginatedDataTable table = PaginatedDataTable(
+      header: null,
+      source: source,
+      rowsPerPage: 5,
+      // dragStartBehavior: DragStartBehavior.down,
+      availableRowsPerPage: const <int>[ 5 ],
+      onRowsPerPageChanged: (int rowsPerPage) { },
+      columns: const <DataColumn>[
+        DataColumn(label: Text('COL1')),
+        DataColumn(label: Text('COL2')),
+        DataColumn(label: Text('COL3')),
+      ],
+    );
+
+    expect(table.header, null);
+
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Align(
+          alignment: Alignment.topLeft,
+          child: SizedBox(
+            width: 100.0,
+            child: table,
+          ),
+        ),
+      ),
+    );
+
+    Card dataTableCard = find.byType(Card).evaluate().first.widget as Card;
+    // if the header row isn't visible the column contains 2 elements
+    expect((dataTableCard.child as Column).children.length, 2);
+  });
+
+  testWidgets('PaginatedDataTable header is null with actions', (WidgetTester tester) async {
+    final TestDataSource source = TestDataSource();
+
+    final PaginatedDataTable table = PaginatedDataTable(
+      header: null,
+      source: source,
+      rowsPerPage: 5,
+      actions: <Widget>[Icon(Icons.add)],
+      // dragStartBehavior: DragStartBehavior.down,
+      availableRowsPerPage: const <int>[ 5 ],
+      onRowsPerPageChanged: (int rowsPerPage) { },
+      columns: const <DataColumn>[
+        DataColumn(label: Text('COL1')),
+        DataColumn(label: Text('COL2')),
+        DataColumn(label: Text('COL3')),
+      ],
+    );
+
+    expect(table.header, null);
+
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Align(
+          alignment: Alignment.topLeft,
+          child: SizedBox(
+            width: 100.0,
+            child: table,
+          ),
+        ),
+      ),
+    );
+
+    Card dataTableCard = find.byType(Card).evaluate().first.widget as Card;
+    // if the header row is visible the column contains 3 elements
+    expect((dataTableCard.child as Column).children.length, 3);
+  });
 }
