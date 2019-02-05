476e50f622f74a39e1a458b2bb870f0a05858002
Veljko Vuković <42891350+wolfcro1984@users.noreply.github.com>
2019-02-05 11:02:23 +0100

    Clean code warnings

--- packages/flutter/lib/src/material/paginated_data_table.dart
+++ packages/flutter/lib/src/material/paginated_data_table.dart
@@ -387,7 +387,7 @@ class PaginatedDataTableState extends State<PaginatedDataTable> {
       Container(width: 14.0),
     ]);
 
-    List<Widget> cardWidgets = new List<Widget>();
+    final List<Widget> cardWidgets = <Widget>[];
 
     if (headerWidgets.isNotEmpty) {
       // The header will be visible only if the headerWidgets collection is not empty
