a0be637b610a50c71254dc608a9b165f10d48561
Xiao Yu <xster@google.com>
2019-02-05 00:07:32 -0800

    Print 50000$ monopoly money

--- packages/flutter_tools/lib/src/base/process.dart
+++ packages/flutter_tools/lib/src/base/process.dart
@@ -6,6 +6,7 @@ import 'dart:async';
 
 import '../convert.dart';
 import '../globals.dart';
+import 'common.dart';
 import 'file_system.dart';
 import 'io.dart';
 import 'process_manager.dart';
@@ -193,7 +194,7 @@ Future<int> runInteractively(List<String> command, {
   );
   // The real stdin will never finish streaming. Pipe until the child process
   // finishes.
-  process.stdin.addStream(stdin); // ignore: unawaited_futures
+  unawaited(process.stdin.addStream(stdin));
   // Wait for stdout and stderr to be fully processed, because process.exitCode
   // may complete first.
   await Future.wait<dynamic>(<Future<dynamic>>[
