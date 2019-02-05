b548f8612bf46f69372006fc01201906d56c4d54
Xiao Yu <xster@google.com>
2019-02-04 23:36:56 -0800

    left issue #27529 to handle separately

--- packages/flutter/lib/src/rendering/proxy_box.dart
+++ packages/flutter/lib/src/rendering/proxy_box.dart
@@ -2635,11 +2635,6 @@ class RenderPointerListener extends RenderProxyBoxWithHitTestBehavior {
   }
 
   @override
-  bool hitTest(HitTestResult result, {Offset position}) {
-    return super.hitTest(result, position: position);
-  }
-
-  @override
   void handleEvent(PointerEvent event, HitTestEntry entry) {
     assert(debugHandleEvent(event, entry));
     // The onPointerEnter, onPointerHover, and onPointerExit events are are
