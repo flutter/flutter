b06a709305eabc4765aef674f6cc705cd17cf3a2
Jonah Williams <jonahwilliams@google.com>
2019-02-05 18:35:20 -0800

    Use flutter_tools to generate build_script (#27277)

--- packages/flutter_tools/lib/src/context_runner.dart
+++ packages/flutter_tools/lib/src/context_runner.dart
@@ -22,6 +22,7 @@ import 'base/time.dart';
 import 'base/user_messages.dart';
 import 'base/utils.dart';
 import 'build_runner/build_runner.dart';
+import 'build_runner/build_script_generator.dart';
 import 'cache.dart';
 import 'compile.dart';
 import 'devfs.dart';
@@ -60,6 +61,7 @@ Future<T> runInContext<T>(
       Artifacts: () => CachedArtifacts(),
       AssetBundleFactory: () => AssetBundleFactory.defaultInstance,
       BotDetector: () => const BotDetector(),
+      BuildScriptGeneratorFactory: () => const BuildScriptGeneratorFactory(),
       BuildRunnerFactory: () => const BuildRunnerFactory(),
       Cache: () => Cache(),
       CocoaPods: () => CocoaPods(),
