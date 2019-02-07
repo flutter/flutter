d4f7be189039d8c36d06317f89a140f8fddaf725
Alexander Ryzhov <aryzhov07@gmail.com>
2019-02-07 13:20:10 -0800

    Fixed #27621: CupertinoTimerPicker breaks if minuteInterval > 1 (#27647)

--- packages/flutter/lib/src/cupertino/date_picker.dart
+++ packages/flutter/lib/src/cupertino/date_picker.dart
@@ -1150,7 +1150,7 @@ class _CupertinoTimerPickerState extends State<CupertinoTimerPicker> {
       backgroundColor: _kBackgroundColor,
       onSelectedItemChanged: (int index) {
         setState(() {
-          selectedMinute = index;
+          selectedMinute = index * widget.minuteInterval;
           widget.onTimerDurationChanged(
             Duration(
               hours: selectedHour ?? 0,
@@ -1262,7 +1262,7 @@ class _CupertinoTimerPickerState extends State<CupertinoTimerPicker> {
       backgroundColor: _kBackgroundColor,
       onSelectedItemChanged: (int index) {
         setState(() {
-          selectedSecond = index;
+          selectedSecond = index * widget.secondInterval;
           widget.onTimerDurationChanged(
             Duration(
               hours: selectedHour ?? 0,
