f149d742a7706ae27669235066264687f3738c0e
Anthony Bullard <arbullard@google.com>
2019-02-07 12:53:58 -0800

    More review feedback
    * Addressed all style nits
    * Refactored functions inside of runTests into top-level functions
    * High-level flow of running tests should now be clearer
      * Set up variables
      * Compile tests
      * Run tests
      * If --watch:
        * Compile and run on 'r'
        * Quit on 'q'
      * Clean up and exit

--- packages/flutter_tools/lib/src/test/runner.dart
+++ packages/flutter_tools/lib/src/test/runner.dart
@@ -41,29 +41,13 @@ Future<int> runTests(
   @required int concurrency,
 }) async {
   // Compute the command-line arguments for package:test.
-  final List<String> testArgs = <String>[];
-  if (!terminal.supportsColor) {
-    testArgs.addAll(<String>['--no-color']);
-  }
-
-  if (machine) {
-    testArgs.addAll(<String>['-r', 'json']);
-  } else {
-    testArgs.addAll(<String>['-r', 'expanded']);
-  }
-
-  testArgs.add('--concurrency=$concurrency');
-
-  for (String name in names) {
-    testArgs..add('--name')..add(name);
-  }
-
-  for (String plainName in plainNames) {
-    testArgs..add('--plain-name')..add(plainName);
-  }
-
-  testArgs.add('--');
-  testArgs.addAll(testFiles);
+  final List<String> testArgs = buildTestArgs(
+    machine: machine,
+    concurrency: concurrency,
+    testFiles: testFiles,
+    names: names,
+    plainNames: plainNames,
+  );
 
   // Configure package:test to use the Flutter engine for child processes.
   final String shellPath = artifacts.getArtifactPath(Artifact.flutterTester);
@@ -72,34 +56,18 @@ Future<int> runTests(
 
   final Uri projectRootDirectory = fs.currentDirectory.uri;
   final TestCompiler compiler = TestCompiler(trackWidgetCreation, projectRootDirectory);
-  final List<String> invalidatedFiles = <String>[];
   final InternetAddressType serverType =
       ipv6 ? InternetAddressType.IPv6 : InternetAddressType.IPv4;
+  // Make the global packages path absolute.
+  // (Makes sure it still works after we change the current directory.)
+  PackageMap.globalPackagesPath =
+      fs.path.normalize(fs.path.absolute(PackageMap.globalPackagesPath));
+  print(PackageMap.globalPackagesPath);
+  // Call package:test's main method in the appropriate directory.
+  final Directory saved = fs.currentDirectory;
 
-  Future<void> compileTestFiles() async {
-    int index = 0;
-    final Map<String, String> precompiledDillFiles = <String, String>{};
-    final Map<String, List<Finalizer>> finalizers = <String, List<Finalizer>>{};
-    for (String file in testFiles) {
-      final List<Finalizer> fileFinalizers = <Finalizer>[];
-      final String mainDart = createListenerDart(
-        fileFinalizers,
-        index,
-        file,
-        loader.kHosts[serverType],
-        updateGoldens,
-      );
 
-      final String dillPath = await compiler.compile(mainDart, invalidatedFiles: invalidatedFiles);
-      // We only need to invalidate the changed file once, so clear the list
-      if (invalidatedFiles.isNotEmpty) {
-        invalidatedFiles.clear();
-      }
-      precompiledDillFiles[file] = dillPath;
-      finalizers[file] = fileFinalizers;
-      index++;
-    }
-
+  void installHook(Map<String, String> precompiledDillFiles, Map<String, List<Finalizer>> finalizers) {
     loader.installHook(
       shellPath: shellPath,
       watcher: watcher,
@@ -115,22 +83,11 @@ Future<int> runTests(
     );
   }
 
-  await compileTestFiles();
-
-  // Make the global packages path absolute.
-  // (Makes sure it still works after we change the current directory.)
-  PackageMap.globalPackagesPath =
-      fs.path.normalize(fs.path.absolute(PackageMap.globalPackagesPath));
-
-  // Call package:test's main method in the appropriate directory.
-  final Directory saved = fs.currentDirectory;
-
-  void printReloadMessage() {
-    printStatus(
-      "Press 'r' to rerun your tests, 'q' to quit",
-      color: TerminalColor.red
-    );
-  }
+  await _compileTestFiles(compiler, testFiles,
+    installHook: installHook,
+    host: loader.kHosts[serverType],
+    updateGoldens: updateGoldens
+  );
 
   try {
     if (workDir != null) {
@@ -139,50 +96,145 @@ Future<int> runTests(
     }
 
     printTrace('running test package with arguments: $testArgs');
+    print(PackageMap.globalPackagesPath);
     await test.runTests(testArgs);
 
     if (watchTests) {
-      final DirectoryWatcher directoryWatcher = DirectoryWatcher(saved.path);
-
-      directoryWatcher.events.listen((WatchEvent event) {
-        if (event.path.endsWith('.dart')) {
-          invalidatedFiles.add(event.path);
-        }
-      });
-
-      printReloadMessage();
-
-      terminal.singleCharMode = true;
-      await for (String char in terminal.keystrokes.asBroadcastStream()){
-        if (char == 'r') {
-          final Status status = logger.startProgress('Recompiling test files...\n', timeout: null);
-          await compileTestFiles();
-          status.stop();
-
-          await test.runTests(testArgs);
-
-          printReloadMessage();
-        } else if (char == 'q') {
-          break;
-        }
-      }
-
-      await directoryWatcher.ready;
+      await _testWatch(
+        testDir: saved,
+        testFiles: testFiles,
+        testArgs: testArgs,
+        host: loader.kHosts[serverType],
+        updateGoldens: updateGoldens,
+        installHook: installHook,
+        compiler: compiler,
+      );
     }
 
-    try {
-      test.completeShutdown();
-    } on StateError catch (e) {
-      printTrace(e.toString());
-    } finally {
-      await compiler.dispose();
-    }
+    test.completeShutdown();
 
     // test.main() sets dart:io's exitCode global.
     printTrace('test package returned with exit code $exitCode');
 
-    return exitCode;
+  } on StateError catch (e) {
+    printTrace(e.toString());
   } finally {
+    await compiler.dispose();
     fs.currentDirectory = saved;
+  }
+  return exitCode;
+}
+
+List<String> buildTestArgs({
+  bool machine,
+  int concurrency,
+  List<String> names,
+  List<String> plainNames,
+  List<String> testFiles,
+}) {
+  final List<String> testArgs = <String>[];
+  if (!terminal.supportsColor) {
+    testArgs.addAll(<String>['--no-color']);
+  }
+
+  if (machine) {
+    testArgs.addAll(<String>['-r', 'json']);
+  } else {
+    testArgs.addAll(<String>['-r', 'compact']);
+  }
+
+  testArgs.add('--concurrency=$concurrency');
+
+  for (String name in names) {
+    testArgs..add('--name')..add(name);
+  }
+
+  for (String plainName in plainNames) {
+    testArgs..add('--plain-name')..add(plainName);
+  }
+
+  testArgs.add('--');
+  testArgs.addAll(testFiles);
+
+  return testArgs;
+}
+
+typedef _HookInstaller = void Function(Map<String, String> precompiledDillFiles, Map<String, List<Finalizer>> finalizers);
+
+Future<void> _compileTestFiles(
+  TestCompiler compiler,
+  List<String> testFiles, {
+  _HookInstaller installHook,
+  InternetAddress host,
+  bool updateGoldens,
+  List<String> invalidatedFiles = const <String>[]
+}) async {
+  int index = 0;
+  final Map<String, String> precompiledDillFiles = <String, String>{};
+  final Map<String, List<Finalizer>> finalizers = <String, List<Finalizer>>{};
+  for (String file in testFiles) {
+    final List<Finalizer> fileFinalizers = <Finalizer>[];
+    final String mainDart = createListenerDart(
+      fileFinalizers,
+      index,
+      file,
+      host,
+      updateGoldens,
+    );
+
+    final String dillPath = await compiler.compile(mainDart, invalidatedFiles: invalidatedFiles);
+    // We only need to invalidate the changed file once, so clear the list
+    if (invalidatedFiles.isNotEmpty) {
+      invalidatedFiles.clear();
+    }
+    precompiledDillFiles[file] = dillPath;
+    finalizers[file] = fileFinalizers;
+    index++;
+  }
+
+  installHook(precompiledDillFiles, finalizers);
+}
+
+void _printReloadMessage() {
+  printStatus(
+    "Press 'r' to rerun your tests, 'q' to quit",
+    color: TerminalColor.red
+  );
+}
+
+Future<void> _testWatch({
+  Directory testDir,
+  List<String> testArgs,
+  List<String> testFiles,
+  TestCompiler compiler,
+  InternetAddress host,
+  bool updateGoldens,
+  _HookInstaller installHook
+}) async {
+  final List<String> invalidatedFiles = <String>[];
+  final DirectoryWatcher directoryWatcher = DirectoryWatcher(testDir.path);
+
+  directoryWatcher.events.listen((WatchEvent event) {
+    if (event.path.endsWith('.dart')) {
+      invalidatedFiles.add(event.path);
+    }
+  });
+  _printReloadMessage();
+
+  await directoryWatcher.ready;
+  terminal.singleCharMode = true;
+  await for (String char in terminal.keystrokes.asBroadcastStream()){
+    if (char == 'r') {
+      final Status status = logger.startProgress('Recompiling test files...\n', timeout: null);
+      await _compileTestFiles(compiler, testFiles,
+        installHook: installHook,
+        host: host,
+        updateGoldens: updateGoldens);
+      status.stop();
+      await test.runTests(testArgs);
+      _printReloadMessage();
+    } else if (char == 'q') {
+      break;
+    }
   }
 }
