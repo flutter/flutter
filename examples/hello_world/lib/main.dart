b2cd0c2e03b0e214ff8f4e883daa33555e384cbe
jslavitz <jslavitz@google.com>
2019-02-06 18:25:52 -0800

    removed

--- examples/hello_world/lib/main.dart
+++ examples/hello_world/lib/main.dart
@@ -3,5 +3,23 @@
 // found in the LICENSE file.
 
 import 'package:flutter/widgets.dart';
+import 'package:flutter/cupertino.dart';
+//void main() => runApp(const Center(child: Text('Hello, world!', textDirection: TextDirection.ltr)));
 
-void main() => runApp(const Center(child: Text('Hello, world!', textDirection: TextDirection.ltr)));
+void main() {
+  DateTime date;
+  runApp(CupertinoApp(
+    home: SizedBox(
+      height: 400.0,
+      width: 400.0,
+      child: CupertinoDatePicker(
+        mode: CupertinoDatePickerMode.date,
+        onDateTimeChanged: (DateTime newDate) {
+          date = newDate;
+          print(date);
+        },
+        initialDateTime: DateTime(2018, 3, 30),
+      ),
+    ),
+  ));
+}
