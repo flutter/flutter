2b8565e806c29783d1520567631ce6dd141857d9
Yuqian Li <liyuqian@google.com>
2019-02-07 16:39:41 -0800

    Fix analyze

--- dev/benchmarks/macrobenchmarks/test_driver/cubic_bezier_perf_test.dart
+++ dev/benchmarks/macrobenchmarks/test_driver/cubic_bezier_perf_test.dart
@@ -4,6 +4,8 @@
 
 import 'package:macrobenchmarks/common.dart';
 
+import 'util.dart';
+
 void main() {
   macroPerfTest('cubic_bezier_perf', kCubicBezierRouteName);
 }
--- dev/benchmarks/macrobenchmarks/test_driver/cull_opacity_perf_test.dart
+++ dev/benchmarks/macrobenchmarks/test_driver/cull_opacity_perf_test.dart
@@ -2,12 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'dart:async';
-
-import 'package:flutter_driver/flutter_driver.dart';
-import 'package:test/test.dart' hide TypeMatcher, isInstanceOf;
-
 import 'package:macrobenchmarks/common.dart';
+
+import 'util.dart';
 
 void main() {
   macroPerfTest('cull_opacity_perf', kCullOpacityRouteName, pageDelay: Duration(seconds: 1), duration: Duration(seconds: 10));
