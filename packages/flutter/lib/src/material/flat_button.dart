9f1818bb652fc0b281c298105f6eb6ec9b2d1c5f
Shivam Bhasin <32370728+shivamvk@users.noreply.github.com>
2019-02-11 12:58:16 +0530

    Update flat_button.dart

--- packages/flutter/lib/src/material/flat_button.dart
+++ packages/flutter/lib/src/material/flat_button.dart
@@ -45,18 +45,23 @@ import 'theme_data.dart';
 /// in your flutter code.
 ///
 /// ```dart
-/// FlatButton(
-///              onPressed: () {
-///                _doSomething();
-///              },
-///              child: Text(
-///                "Flat Button",
-///                style: TextStyle(
-///                  color: Colors.white,
-///                  fontSize: 20.0
-///                ),
-///              ),
-///            )
+///          FlatButton(
+///           color: Colors.blue,
+///           textColor: Colors.white,
+///           disabledColor: Colors.grey,
+///           disabledTextColor: Colors.black,
+///           padding: EdgeInsets.all(8.0),
+///           splashColor: Colors.blueAccent,
+///           onPressed: () {
+///             /*_doSomething();*/
+///           },
+///           child: Text(
+///             "Flat Button",
+///             style: TextStyle(
+///               fontSize: 20.0
+///             ),
+///           ),
+///         )
 /// ```
 /// {@end-tool}
 /// See also:
