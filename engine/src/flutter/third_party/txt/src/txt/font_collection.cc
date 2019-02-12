ff5f45ad8e0ed78784dfe9a84659bb29f8d3e065
Gary Qian <garyq@google.com>
2019-02-12 13:11:16 -0800

    Revert "Use all font managers to discover fonts for strut. (#7734)" (flutter/engine#7801)
    This reverts commit b5b92f94ecf1d75f80c23b5b842e40e33b305acb.

--- engine/src/flutter/third_party/txt/src/txt/font_collection.cc
+++ engine/src/flutter/third_party/txt/src/txt/font_collection.cc
@@ -178,6 +178,26 @@ FontCollection::GetMinikinFontCollectionForFamilies(
   return font_collection;
 }
 
+minikin::MinikinFont* FontCollection::GetMinikinFontForFamilies(
+    const std::vector<std::string>& font_families,
+    minikin::FontStyle style) {
+  std::shared_ptr<minikin::FontFamily> font_family = nullptr;
+  for (std::string family_name : font_families) {
+    font_family = FindFontFamilyInManagers(family_name);
+    if (font_family != nullptr) {
+      break;
+    }
+  }
+  if (font_family == nullptr) {
+    const auto default_font_family = GetDefaultFontFamily();
+    font_family = FindFontFamilyInManagers(default_font_family);
+  }
+  if (font_family == nullptr) {
+    return nullptr;
+  }
+  return font_family.get()->getClosestMatch(style).font;
+}
+
 std::shared_ptr<minikin::FontFamily> FontCollection::FindFontFamilyInManagers(
     const std::string& family_name) {
   // Search for the font family in each font manager.
