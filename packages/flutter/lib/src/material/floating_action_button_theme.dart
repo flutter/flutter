64f47a17a0505c39201014c8e592f18e6d2abfd0
Anthony Robledo <anthonyrobledo@gmail.com>
2019-02-12 15:43:18 -0500

    floating action button theme based on 2 props

--- packages/flutter/lib/src/material/floating_action_button_theme.dart
+++ packages/flutter/lib/src/material/floating_action_button_theme.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 import 'theme.dart';
 
-/// TODO(clockismith): this
+/// TODO(clocksmith): dartdoc
 class FloatingActionButtonTheme extends InheritedWidget {
   /// Applies the given theme [data] to [child].
   ///
@@ -57,6 +58,92 @@ class FloatingActionButtonTheme extends InheritedWidget {
   bool updateShouldNotify(FloatingActionButtonTheme oldWidget) => data != oldWidget.data;
 }
 
+/// TODO(clocksmith): dartdoc
 class FloatingActionButtonThemeData extends Diagnosticable {
+  /// TODO(clocksmith): dartdoc
+  const FloatingActionButtonThemeData({
+    @required this.backgroundColor,
+    @required this.foregroundColor,
+  })  : assert(backgroundColor != null),
+        assert(foregroundColor != null);
 
+  factory FloatingActionButtonThemeData.fromDefaults({
+    Brightness brightness,
+    Color primaryColor,
+    Color onPrimaryColor,
+    @required TextStyle textStyle,
+  }) {
+    return FloatingActionButtonThemeData(
+      backgroundColor: primaryColor,
+      foregroundColor: onPrimaryColor,
+    );
+}
+
+  /// Color to be used for the unselected, enabled floating action buttons's
+  /// background.
+  ///
+  /// The default is the primary color.
+  final Color backgroundColor;
+
+  /// Color to be used for the unselected, enabled floating action buttons's
+  /// foreground.
+  ///
+  /// The default is the onPrimary color.
+  final Color foregroundColor;
+
+  /// Creates a copy of this object but with the given fields replaced with the
+  /// new values.
+  FloatingActionButtonThemeData copyWith({
+    Color backgroundColor,
+    Color foregroundColor,
+  }) {
+    return FloatingActionButtonThemeData(
+      backgroundColor: backgroundColor ?? this.backgroundColor,
+      foregroundColor: foregroundColor ?? this.foregroundColor,
+    );
+  }
+
+  static FloatingActionButtonThemeData lerp(FloatingActionButtonThemeData a, FloatingActionButtonThemeData b, double t) {
+    assert(t != null);
+    if (a == null && b == null)
+      return null;
+    return FloatingActionButtonThemeData(
+      backgroundColor: Color.lerp(a?.backgroundColor, b?.backgroundColor, t),
+      foregroundColor: Color.lerp(a?.foregroundColor, b?.foregroundColor, t),
+    );
+  }
+  @override
+  int get hashCode {
+    return hashValues(
+      backgroundColor,
+      foregroundColor,
+    );
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) {
+      return true;
+    }
+    if (other.runtimeType != runtimeType) {
+      return false;
+    }
+    final FloatingActionButtonThemeData otherData = other;
+    return otherData.backgroundColor == backgroundColor
+        && otherData.foregroundColor == foregroundColor;
+  }
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    final ThemeData defaultTheme = ThemeData.fallback();
+    final FloatingActionButtonThemeData defaultData = FloatingActionButtonThemeData.fromDefaults(
+      primaryColor: defaultTheme.primaryColor,
+      onPrimaryColor: Colors.white, // This is wrong
+      brightness: defaultTheme.brightness,
+      textStyle: defaultTheme.textTheme.body2,
+    );
+    properties.add(DiagnosticsProperty<Color>('backgroundColor', backgroundColor, defaultValue: defaultData.backgroundColor));
+    properties.add(DiagnosticsProperty<Color>('foregroundColor', foregroundColor, defaultValue: defaultData.foregroundColor));
+  }
 }
