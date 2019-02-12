a724cc591733ae9e7ef770fedec1727106abf9f1
Jonah Williams <jonahwilliams@google.com>
2019-02-12 15:57:27 -0800

    Update inherited_dependencies.dart

--- packages/flutter/test/widgets/inherited_dependencies.dart
+++ packages/flutter/test/widgets/inherited_dependencies.dart
@@ -1,3 +1,7 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import 'package:flutter/src/widgets/basic.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_test/flutter_test.dart';
