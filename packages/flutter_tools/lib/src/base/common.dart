4f5d96eff9b1174131227425b7e82f2e47fb5ebf
KyleWong <kang.wang1988@gmail.com>
2019-02-13 14:33:14 +0800

    Don't cache result for homeDirPath.

--- packages/flutter_tools/lib/src/base/common.dart
+++ packages/flutter_tools/lib/src/base/common.dart
@@ -7,16 +7,14 @@ import 'platform.dart';
 
 /// Return the absolute path of the user's home directory
 String get homeDirPath {
-  if (_homeDirPath == null) {
-    _homeDirPath = platform.isWindows
-        ? platform.environment['USERPROFILE']
-        : platform.environment['HOME'];
-    if (_homeDirPath != null)
-      _homeDirPath = fs.path.absolute(_homeDirPath);
+  String _homeDirPath = platform.isWindows
+      ? platform.environment['USERPROFILE']
+      : platform.environment['HOME'];
+  if (_homeDirPath != null) {
+    _homeDirPath = fs.path.absolute(_homeDirPath);
   }
   return _homeDirPath;
 }
-String _homeDirPath;
 
 /// Throw a specialized exception for expected situations
 /// where the tool should exit with a clear message to the user
