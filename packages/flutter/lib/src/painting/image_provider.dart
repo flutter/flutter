4a9b33e0d26495575a3a5baf7f091703b8159cd1
Kaushik Iska <kaushikiska@google.com>
2019-02-14 12:46:16 -0800

    change to // from ///, and do specific error handling

--- packages/flutter/lib/src/painting/image_provider.dart
+++ packages/flutter/lib/src/painting/image_provider.dart
@@ -281,22 +281,26 @@ abstract class ImageProvider<T> {
       );
     }
 
-    /// `obtainKey` can throw both sync and async errors.
-    /// `catchError` handles cases where async errors are thrown and the outer try block is for sync errors.
-    ///
-    /// `onError` callback on [ImageCache] handles the cases where `obtainKey` is a sync future and `load` throws.
+    // `obtainKey` can throw both sync and async errors.
+    // `catchError` handles cases where async errors are thrown and the try block is for sync errors.
+    //
+    // `onError` callback on [ImageCache] handles the cases where `obtainKey` is a sync future and `load` throws.
+    Future<T> key;
     try {
-      obtainKey(configuration).then<void>((T key) {
-        obtainedKey = key;
-        final ImageStreamCompleter completer = PaintingBinding.instance
-            .imageCache.putIfAbsent(key, () => load(key), onError: handleError);
-        if (completer != null) {
-          stream.setCompleter(completer);
-        }
-      }).catchError(handleError);
+      key = obtainKey(configuration);
     } catch (error, stackTrace) {
       handleError(error, stackTrace);
     }
+
+    key.then<void>((T key) {
+      obtainedKey = key;
+      final ImageStreamCompleter completer = PaintingBinding.instance
+          .imageCache.putIfAbsent(key, () => load(key), onError: handleError);
+      if (completer != null) {
+        stream.setCompleter(completer);
+      }
+    }).catchError(handleError);
+
     return stream;
   }
 
