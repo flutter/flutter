0ca1d1f318e6a4964a9c2b957524d3fb8d38b3ac
Jason Simmons <jason-simmons@users.noreply.github.com>
2019-02-15 09:43:23 -0800

    Remove the Dart JIT snapshot data from AOT builds of the embedder library (#7806)
    Also extend the embedder unit tests to cover AOT execution

--- shell/platform/embedder/tests/embedder_unittests.cc
+++ shell/platform/embedder/tests/embedder_unittests.cc
@@ -4,7 +4,37 @@
 
 #include <string>
 #include "embedder.h"
+#include "flutter/fml/file.h"
+#include "flutter/fml/mapping.h"
 #include "flutter/testing/testing.h"
+
+namespace {
+
+void MapAOTAsset(std::vector<std::unique_ptr<fml::FileMapping>>& aot_mappings,
+                 const fml::UniqueFD& fixtures_dir,
+                 const char* path,
+                 bool executable,
+                 const uint8_t** data,
+                 size_t* size) {
+  fml::UniqueFD file =
+      fml::OpenFile(fixtures_dir, path, false, fml::FilePermission::kRead);
+  std::unique_ptr<fml::FileMapping> mapping;
+  if (executable) {
+    mapping = std::make_unique<fml::FileMapping>(
+        file, std::initializer_list<fml::FileMapping::Protection>{
+                  fml::FileMapping::Protection::kRead,
+                  fml::FileMapping::Protection::kExecute});
+  } else {
+    mapping = std::make_unique<fml::FileMapping>(
+        file, std::initializer_list<fml::FileMapping::Protection>{
+                  fml::FileMapping::Protection::kRead});
+  }
+  *data = mapping->GetMapping();
+  *size = mapping->GetSize();
+  aot_mappings.emplace_back(std::move(mapping));
+}
+
+}  // anonymous namespace
 
 TEST(EmbedderTest, MustNotRunWithInvalidArgs) {
   FlutterEngine engine = nullptr;
@@ -33,6 +63,22 @@ TEST(EmbedderTest, CanLaunchAndShutdownWithValidProjectArgs) {
     std::string str_data = reinterpret_cast<char*>(data);
     ASSERT_EQ(str_data, "Data");
   };
+
+  fml::UniqueFD fixtures_dir = fml::OpenDirectory(
+      testing::GetFixturesPath(), false, fml::FilePermission::kRead);
+  std::vector<std::unique_ptr<fml::FileMapping>> aot_mappings;
+  if (fml::FileExists(fixtures_dir, "vm_snapshot_data")) {
+    MapAOTAsset(aot_mappings, fixtures_dir, "vm_snapshot_data", false,
+                &args.vm_snapshot_data, &args.vm_snapshot_data_size);
+    MapAOTAsset(aot_mappings, fixtures_dir, "vm_snapshot_instr", true,
+                &args.vm_snapshot_instructions,
+                &args.vm_snapshot_instructions_size);
+    MapAOTAsset(aot_mappings, fixtures_dir, "isolate_snapshot_data", false,
+                &args.isolate_snapshot_data, &args.isolate_snapshot_data_size);
+    MapAOTAsset(aot_mappings, fixtures_dir, "isolate_snapshot_instr", true,
+                &args.isolate_snapshot_instructions,
+                &args.isolate_snapshot_instructions_size);
+  }
 
   std::string str_data = "Data";
   void* user_data = const_cast<char*>(str_data.c_str());
