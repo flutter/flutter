0ff918a63408f22548fd1a1b9ebd5147ea87dd86
jonahwilliams <jonahwilliams@google.com>
2019-02-15 10:00:53 -0800

    remove accidentally commited libs

--- packages/flutter/lib/src/foundation/unsupported_web.dart
+++
@@ -1,7 +0,0 @@
-/// The largest SMI value.
-///
-/// See <https://www.dartlang.org/articles/numeric-computation/#smis-and-mints>
-///
-/// When compiling to JavaScript, this value is not supported since it is
-/// larger than the maximum safe 32bit integer.
-const int kMaxUnsignedSMI = 0;
