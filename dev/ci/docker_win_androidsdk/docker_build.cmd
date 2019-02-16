d60dfd2653274048774267838f741acaad529098
Dan Field <dnfield@google.com>
2019-02-15 22:31:52 -0800

    Remove unnecessary files

--- dev/ci/docker_win_androidsdk/docker_build.cmd
+++
@@ -1,7 +0,0 @@
-SET CIRRUS_TAG 2>Nul | Findstr/I "."
-IF ERRORLEVEL 1 SET TAG=latest ELSE TAG=%CIRRUS_TAG%
-
-# pull to make sure we are not rebuilding for nothing
-docker pull "gcr.io/flutter-cirrus/win-android-image:%TAG%"
-
-docker build --tag "gcr.io/flutter-cirrus/win-android-image:%TAG%" .
