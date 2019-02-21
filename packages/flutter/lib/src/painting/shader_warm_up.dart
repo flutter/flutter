eeb56c4c38a93df3805fa499e271851f78d73941
Yuqian Li <liyuqian@google.com>
2019-02-21 14:58:42 -0800

    Improve docs and replace w/h with size

--- packages/flutter/lib/src/painting/shader_warm_up.dart
+++ packages/flutter/lib/src/painting/shader_warm_up.dart
@@ -1,14 +1,20 @@
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import 'dart:developer';
-import 'dart:ui';
+import 'dart:ui' as ui;
+
+import 'package:meta/meta.dart';
 
 /// Interface for drawing an image to warm up Skia shader compilations.
 ///
 /// When Skia first sees a certain type of draw operations on GPU, it needs to
 /// compile the corresponding shader. The compilation can be slow (20ms-200ms).
-/// Having that time as a startup latency is much better than having a jank in
+/// Having that time as a startup latency is often better than having a jank in
 /// the middle of an animation.
 ///
-/// Therefore we use this during the [runApp] call to move common shader
+/// Therefore, we use this during the [runApp] call to move common shader
 /// compilations from animation time to startup time. By default, a
 /// [DefaultShaderWarmUp] is used. Create a custom [ShaderWarmUp] subclass to
 /// replace it if needed.
@@ -17,32 +23,47 @@ import 'dart:ui';
 /// compilation depends on the specific GPU hardware and driver a device has. It
 /// can't be pre-computed during the Flutter engine compilation as the engine is
 /// device agnostic.
+///
+/// If no warm up is desired (e.g., when the startup latency is crucial), send a
+/// custom ShaderWarmUp with an empty [warmUpOnCanvas] to [runApp]
 abstract class ShaderWarmUp {
   /// Allow const constructors for subclasses.
   const ShaderWarmUp();
 
-  /// Width of the image to be drawn.
-  int get width;
-
-  /// Height of the image to be drawn.
-  int get height;
+  /// The size of the warm up image.
+  ///
+  /// The exact size shouldn't matter much as long as it's not too far away from
+  /// the target device's screen. 1024x1024 is a good choice as it is within an
+  /// order of magnitude of most devices.
+  ///
+  /// A custom shader warm up can override this based on targeted devices.
+  ui.Size get size => const ui.Size(1024, 1024);
 
   /// Trigger draw operations on a given canvas to warm up GPU shader
   /// compilation cache.
-  void warmUpOnCanvas(Canvas canvas);
+  ///
+  /// To decide which draw operations to be added to your custom warm up
+  /// process, try capture an skp using `flutter screenshot --observatory-
+  /// port=<port> --type=skia` and analyze it with https://debugger.skia.org.
+  /// Alternatively, one may run the app with `flutter run --trace-skia` and
+  /// then examine the GPU thread in the observatory timeline to see which
+  /// Skia draw operations are commonly used, and which shader compilations
+  /// are causing janks.
+  @protected
+  void warmUpOnCanvas(ui.Canvas canvas);
 
-  /// Construct an offscreen image of size [width]x[height], and execute
-  /// [warmUpOnCanvas] on a canvas associated with that image.
+  /// Construct an offscreen image of [size], and execute [warmUpOnCanvas] on a
+  /// canvas associated with that image.
   void execute() {
-    final PictureRecorder recorder = PictureRecorder();
-    final Canvas canvas = Canvas(recorder);
+    final ui.PictureRecorder recorder = ui.PictureRecorder();
+    final ui.Canvas canvas = ui.Canvas(recorder);
 
     warmUpOnCanvas(canvas);
 
-    final Picture picture = recorder.endRecording();
+    final ui.Picture picture = recorder.endRecording();
     final TimelineTask shaderWarmUpTask = TimelineTask();
     shaderWarmUpTask.start('Warm-up shader');
-    picture.toImage(width, height).then((Image image) {
+    picture.toImage(size.width.ceil(), size.height.ceil()).then((ui.Image image) {
       shaderWarmUpTask.finish();
     });
   }
@@ -53,92 +74,72 @@ abstract class ShaderWarmUp {
 /// The draw operations being warmed up here are decided according to Flutter
 /// engineers' observation and experience based on the apps and the performance
 /// issues seen so far.
-///
-/// To decide which draw operations to be added to your custom warm up process,
-/// try capture an skp using `flutter screenshot --observatory- port=<port>
-/// --type=skia` and analyze it with https://debugger.skia.org. Alternatively,
-/// one may run the app with `flutter run --trace-skia` and then examine the GPU
-/// thread in the observatory timeline to see which Skia draw operations are
-/// commonly used, and which shader compilations are causing janks.
-///
-/// The size of [width]=[height]=1000 here are picked arbitrarily. The only goal
-/// is to have them not too far away from the actual resolution (very few mobile
-/// devices seem to have a resolution that is 10x larger or 10x smaller than
-/// 1000x1000). The size should have little effect on the shader warm-up. A
-/// custom shader warm up can pick another arbitrary number that's close to the
-/// targeted devices.
 class DefaultShaderWarmUp extends ShaderWarmUp {
   /// Allow [DefaultShaderWarmUp] to be used as the default value of parameters.
   const DefaultShaderWarmUp();
 
-  @override
-  int get width => 1000;
-
-  @override
-  int get height => 1000;
-
   /// Trigger common draw operations on a canvas to warm up GPU shader
   /// compilation cache.
   @override
-  void warmUpOnCanvas(Canvas canvas) {
-    final RRect rrect = RRect.fromLTRBXY(20, 20, 60, 60, 10, 10);
-    final Path rrectPath = Path()..addRRect(rrect);
+  void warmUpOnCanvas(ui.Canvas canvas) {
+    final ui.RRect rrect = ui.RRect.fromLTRBXY(20.0, 20.0, 60.0, 60.0, 10.0, 10.0);
+    final ui.Path rrectPath = ui.Path()..addRRect(rrect);
 
-    final Path circlePath = Path()..addOval(
-        Rect.fromCircle(center: const Offset(40, 40), radius: 20)
+    final ui.Path circlePath = ui.Path()..addOval(
+        ui.Rect.fromCircle(center: const ui.Offset(40.0, 40.0), radius: 20.0)
     );
 
-    // The following path is copied from
+    // The following path is based on
     // https://skia.org/user/api/SkCanvas_Reference#SkCanvas_drawPath
-    final Path path = Path();
-    path.moveTo(20, 60);
-    path.quadraticBezierTo(60, 20, 60, 60);
+    final ui.Path path = ui.Path();
+    path.moveTo(20.0, 60.0);
+    path.quadraticBezierTo(60.0, 20.0, 60.0, 60.0);
     path.close();
-    path.moveTo(60, 20);
-    path.quadraticBezierTo(60, 60, 20, 60);
+    path.moveTo(60.0, 20.0);
+    path.quadraticBezierTo(60.0, 60.0, 20.0, 60.0);
 
-    final List<Path> paths = <Path>[rrectPath, circlePath, path];
+    final List<ui.Path> paths = <ui.Path>[rrectPath, circlePath, path];
 
-    final List<Paint> paints = <Paint>[
-      Paint()
+    final List<ui.Paint> paints = <ui.Paint>[
+      ui.Paint()
         ..isAntiAlias = true
-        ..style = PaintingStyle.fill,
-      Paint()
+        ..style = ui.PaintingStyle.fill,
+      ui.Paint()
         ..isAntiAlias = true
-        ..style = PaintingStyle.stroke
+        ..style = ui.PaintingStyle.stroke
         ..strokeWidth = 10,
-      Paint()
+      ui.Paint()
         ..isAntiAlias = true
-        ..style = PaintingStyle.stroke
+        ..style = ui.PaintingStyle.stroke
         ..strokeWidth = 0.1  // hairline
     ];
 
     // Warm up path stroke and fill shaders.
-    for (int i = 0; i < paths.length; i++) {
+    for (int i = 0; i < paths.length; i += 1) {
       canvas.save();
-      for (Paint paint in paints) {
+      for (ui.Paint paint in paints) {
         canvas.drawPath(paths[i], paint);
-        canvas.translate(80, 0);
+        canvas.translate(80.0, 0.0);
       }
       canvas.restore();
-      canvas.translate(0, 80);
+      canvas.translate(0.0, 80.0);
     }
 
     // Warm up shadow shaders.
-    const Color black = Color(0xFF000000);  // foundation can't access material/colors
+    const ui.Color black = ui.Color(0xFF000000);
     canvas.save();
     canvas.drawShadow(rrectPath, black, 10.0, true);
-    canvas.translate(80, 0);
+    canvas.translate(80.0, 0.0);
     canvas.drawShadow(rrectPath, black, 10.0, false);
     canvas.restore();
 
     // Warm up text shaders.
-    canvas.translate(0, 80);
-    final ParagraphBuilder paragraphBuilder = ParagraphBuilder(
-      ParagraphStyle(textDirection: TextDirection.ltr),
-    )..pushStyle(TextStyle(color: black))..addText('Hello, world.');
-    final Paragraph paragraph = paragraphBuilder.build()
-      ..layout(const ParagraphConstraints(width: 60));
-    canvas.drawParagraph(paragraph, const Offset(20, 20));
+    canvas.translate(0.0, 80.0);
+    final ui.ParagraphBuilder paragraphBuilder = ui.ParagraphBuilder(
+      ui.ParagraphStyle(textDirection: ui.TextDirection.ltr),
+    )..pushStyle(ui.TextStyle(color: black))..addText('_');
+    final ui.Paragraph paragraph = paragraphBuilder.build()
+      ..layout(const ui.ParagraphConstraints(width: 60.0));
+    canvas.drawParagraph(paragraph, const ui.Offset(20.0, 20.0));
   }
 }
