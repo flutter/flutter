3f0ce769e296b977cd0bcd8f4f7390f207ef50a8
liyuqian <liyuqian@google.com>
2019-02-21 15:27:37 -0800

    Revert "Revert "Reland PerformanceOverlayLayer golden test (#7863)" (#7895)" (#7911)
    This reverts commit 6d8bd99afb16f7a1ebb59a2657ec76802606a581.

---
+++ flow/flow_run_all_unittests.cc
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2017 Google, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "flutter/fml/command_line.h"
+#include "flutter/fml/logging.h"
+#include "gtest/gtest.h"
+
+#include "flow_test_utils.h"
+
+int main(int argc, char** argv) {
+  testing::InitGoogleTest(&argc, argv);
+  fml::CommandLine cmd = fml::CommandLineFromArgcArgv(argc, argv);
+  flow::SetGoldenDir(
+      cmd.GetOptionValueWithDefault("golden-dir", "flutter/testing/resources"));
+  flow::SetFontFile(cmd.GetOptionValueWithDefault(
+      "font-file",
+      "flutter/third_party/txt/third_party/fonts/Roboto-Regular.ttf"));
+  return RUN_ALL_TESTS();
+}
