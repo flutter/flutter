f0c0a32f30fb89c34acfaa700a6cffa830e4a871
jonahwilliams <jonahwilliams@google.com>
2019-02-22 10:58:30 -0800

    conditionally add desktop devices to devicemanager

--- packages/flutter_tools/lib/src/device.dart
+++ packages/flutter_tools/lib/src/device.dart
@@ -11,6 +11,7 @@ import 'base/context.dart';
 import 'base/file_system.dart';
 import 'base/utils.dart';
 import 'build_info.dart';
+import 'desktop.dart';
 import 'fuchsia/fuchsia_device.dart';
 
 import 'globals.dart';
@@ -35,10 +36,16 @@ class DeviceManager {
     IOSSimulators(),
     FuchsiaDevices(),
     FlutterTesterDevices(),
-    MacOSDevices(),
-    LinuxDevices(),
-    WindowsDevices(),
-  ]);
+  ] + _maybeAddDesktop);
+
+  /// Only add desktop devices if the flag is enabled.
+  static List<DeviceDiscovery> get _maybeAddDesktop {
+    return flutterDesktopEnabled ? <DeviceDiscovery>[
+      MacOSDevices(),
+      LinuxDevices(),
+      WindowsDevices(),
+    ] : <DeviceDiscovery>[];
+  }
 
   String _specifiedDeviceId;
 
