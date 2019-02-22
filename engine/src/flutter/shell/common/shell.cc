191884bbc1e5a8b6e65db5bc8fed93f68e5ccf60
Mehmet Fidanboylu <mehmetf@users.noreply.github.com>
2019-02-22 14:49:15 -0800

    New setting to decide whether we want the engine to load ICU mapping. (flutter/engine#7928)

--- engine/src/flutter/shell/common/shell.cc
+++ engine/src/flutter/shell/common/shell.cc
@@ -187,12 +187,14 @@ static void PerformInitializationTasks(const blink::Settings& settings) {
       FML_DLOG(INFO) << "Skia deterministic rendering is enabled.";
     }
 
-    if (settings.icu_data_path.size() != 0) {
-      fml::icu::InitializeICU(settings.icu_data_path);
-    } else if (settings.icu_mapper) {
-      fml::icu::InitializeICUFromMapping(settings.icu_mapper());
-    } else {
-      FML_DLOG(WARNING) << "Skipping ICU initialization in the shell.";
+    if (settings.icu_initialization_required) {
+      if (settings.icu_data_path.size() != 0) {
+        fml::icu::InitializeICU(settings.icu_data_path);
+      } else if (settings.icu_mapper) {
+        fml::icu::InitializeICUFromMapping(settings.icu_mapper());
+      } else {
+        FML_DLOG(WARNING) << "Skipping ICU initialization in the shell.";
+      }
     }
   });
 }
