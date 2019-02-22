a8d5e8e815e349c768542753b54d75b4b3814672
Scarlet Slavitz <jslavitz@google.com>
2019-02-22 13:42:08 -0800

    tests

--- packages/flutter/test/painting/continuous_stadium_border_test.dart
+++ packages/flutter/test/painting/continuous_stadium_border_test.dart
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-//import 'dart:io' show Platform;
+import 'dart:io' show Platform;
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -20,116 +20,133 @@ void main() {
     expect(ShapeBorder.lerp(c10, c20, 1.0), c20);
   });
 
-// TODO(jslavitz): Run golden tests.
-//  testWidgets('Golden test medium sized stadium, medium radius', (WidgetTester tester) async {
-//    await tester.pumpWidget(
-//      RepaintBoundary(
-//        child: Container(
-//          alignment: Alignment.center,
-//          child: Material(
-//            color: Colors.blueAccent[400],
-//            shape: const ContinuousStadiumBorder(
-//              borderRadius: 28.0,
-//            ),
-//            child: const SizedBox(
-//              height: 100,
-//              width: 200,
-//            ),
-//          ),
-//        ),
-//      ),
-//    );
-//
-//    await tester.pumpAndSettle();
-//
-//    await expectLater(
-//      find.byType(RepaintBoundary),
-//      matchesGoldenFile('continuous_stadium_border.golden_test_medium_medium.png'),
-//      skip: !Platform.isLinux,
-//    );
-//  });
-//
-//  testWidgets('Golden test small sized small, medium radius', (WidgetTester tester) async {
-//    await tester.pumpWidget(
-//      RepaintBoundary(
-//        child: Container(
-//          alignment: Alignment.center,
-//          child: Material(
-//            color: Colors.blueAccent[400],
-//            shape: const ContinuousStadiumBorder(
-//              borderRadius: 28.0,
-//            ),
-//            child: const SizedBox(
-//              height: 10,
-//              width: 100,
-//            ),
-//          ),
-//        ),
-//      ),
-//    );
-//
-//    await tester.pumpAndSettle();
-//
-//    await expectLater(
-//      find.byType(RepaintBoundary),
-//      matchesGoldenFile('continuous_stadium_border.golden_test_small_medium.png'),
-//      skip: !Platform.isLinux,
-//    );
-//  });
-//
-//  testWidgets('Golden test very small stadium, medium radius', (WidgetTester tester) async {
-//    await tester.pumpWidget(
-//      RepaintBoundary(
-//        child: Container(
-//          alignment: Alignment.center,
-//          child: Material(
-//            color: Colors.blueAccent[400],
-//            shape: const ContinuousStadiumBorder(
-//              borderRadius: 28.0,
-//            ),
-//            child: const SizedBox(
-//              height: 5,
-//              width: 100,
-//            ),
-//          ),
-//        ),
-//      ),
-//    );
-//
-//    await tester.pumpAndSettle();
-//
-//    await expectLater(
-//      find.byType(RepaintBoundary),
-//      matchesGoldenFile('continuous_stadium_border.golden_test_vsmall_medium.png'),
-//      skip: !Platform.isLinux,
-//    );
-//  });
-//
-//  testWidgets('Golden test large radii', (WidgetTester tester) async {
-//    await tester.pumpWidget(
-//      RepaintBoundary(
-//        child: Container(
-//          alignment: Alignment.center,
-//          child: Material(
-//            color: Colors.blueAccent[400],
-//            shape: const ContinuousStadiumBorder(
-//              borderRadius: 50.0,
-//            ),
-//            child: const SizedBox(
-//              height: 200,
-//              width: 400,
-//            ),
-//          ),
-//        ),
-//      ),
-//    );
-//
-//    await tester.pumpAndSettle();
-//
-//    await expectLater(
-//      find.byType(RepaintBoundary),
-//      matchesGoldenFile('continuous_stadium_border.golden_test_large_large.png'),
-//      skip: !Platform.isLinux,
-//    );
-//  });
+  testWidgets('Golden test medium sized stadium, medium radius', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      RepaintBoundary(
+        child: Container(
+          alignment: Alignment.center,
+          child: Material(
+            color: Colors.blueAccent[400],
+            shape: const ContinuousStadiumBorder(),
+            child: const SizedBox(
+              height: 100,
+              width: 200,
+            ),
+          ),
+        ),
+      ),
+    );
+
+    await tester.pumpAndSettle();
+
+    await expectLater(
+      find.byType(RepaintBoundary),
+      matchesGoldenFile('continuous_stadium_border.golden_test_medium_medium.png'),
+      skip: !Platform.isLinux,
+    );
+  });
+
+  testWidgets('Golden test small sized small, medium radius', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      RepaintBoundary(
+        child: Container(
+          alignment: Alignment.center,
+          child: Material(
+            color: Colors.blueAccent[400],
+            shape: const ContinuousStadiumBorder(),
+            child: const SizedBox(
+              height: 10,
+              width: 100,
+            ),
+          ),
+        ),
+      ),
+    );
+
+    await tester.pumpAndSettle();
+
+    await expectLater(
+      find.byType(RepaintBoundary),
+      matchesGoldenFile('continuous_stadium_border.golden_test_small_medium.png'),
+      skip: !Platform.isLinux,
+    );
+  });
+
+  testWidgets('Golden test very small stadium, medium radius', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      RepaintBoundary(
+        child: Container(
+          alignment: Alignment.center,
+          child: Material(
+            color: Colors.blueAccent[400],
+            shape: const ContinuousStadiumBorder(),
+            child: const SizedBox(
+              height: 5,
+              width: 100,
+            ),
+          ),
+        ),
+      ),
+    );
+
+    await tester.pumpAndSettle();
+
+    await expectLater(
+      find.byType(RepaintBoundary),
+      matchesGoldenFile('continuous_stadium_border.golden_test_vsmall_medium.png'),
+      skip: !Platform.isLinux,
+    );
+  });
+
+  testWidgets('Golden test large radii', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      RepaintBoundary(
+        child: Container(
+          alignment: Alignment.center,
+          child: Material(
+            color: Colors.blueAccent[400],
+            shape: const ContinuousStadiumBorder(),
+            child: const SizedBox(
+              height: 200,
+              width: 400,
+            ),
+          ),
+        ),
+      ),
+    );
+
+    await tester.pumpAndSettle();
+
+    await expectLater(
+      find.byType(RepaintBoundary),
+      matchesGoldenFile('continuous_stadium_border.golden_test_large_large.png'),
+      skip: !Platform.isLinux,
+    );
+  });
+
+  testWidgets('Golden test large radii vertical alignment', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      RepaintBoundary(
+        child: Container(
+          alignment: Alignment.center,
+          child: Material(
+            color: Colors.blueAccent[400],
+            shape: const ContinuousStadiumBorder(),
+            child: const SizedBox(
+              height: 400,
+              width: 200,
+            ),
+          ),
+        ),
+      ),
+    );
+
+    await tester.pumpAndSettle();
+
+    await expectLater(
+      find.byType(RepaintBoundary),
+      matchesGoldenFile('continuous_stadium_border.golden_test_large_large_vertical.png'),
+      skip: !Platform.isLinux,
+    );
+  });
 }
