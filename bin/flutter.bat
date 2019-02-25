4319d4e3f4ed33461533653743de1a63dc63da1f
Ben Konyi <bkonyi@google.com>
2019-02-25 13:06:10 -0800

    Update flutter.bat

--- bin/flutter.bat
+++ bin/flutter.bat
@@ -1,4 +1,4 @@
-@ECHO off
+@ECHO on
 REM Copyright 2017 The Chromium Authors. All rights reserved.
 REM Use of this source code is governed by a BSD-style license that can be
 REM found in the LICENSE file.
