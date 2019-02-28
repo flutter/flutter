d7a40caeeff263043e9b670da7b8b1c06d687fe4
liyuqian <liyuqian@google.com>
2019-02-28 15:14:35 -0800

    Add convex path and non-AA paint to shader warm-up (#28614)
    One of our important client's SKP shows that this could improve one of
    their janky frame by 20ms. This also improves our
    flutter_gallery__transition_perf's worst frame time by ~20ms.
    
    On the other hand, 15ms has been added to the start-up latency. I guess
    it's a little faster to compile the shader on the start-up because we're
    compiling a lot of shaders there and the CPU cache must be hot.
    
    ## Related Issues
    
    https://github.com/flutter/flutter/issues/813

--- packages/flutter/lib/src/painting/binding.dart
+++ packages/flutter/lib/src/painting/binding.dart
@@ -38,8 +38,8 @@ mixin PaintingBinding on BindingBase, ServicesBinding {
   /// shaders that are not covered by [DefaultShaderWarmUp], it may cause jank
   /// in the middle of an animation or interaction. In that case, set
   /// [shaderWarmUp] to a custom [ShaderWarmUp] before calling [initInstances]
-  /// (usually before [runApp] for normal flutter apps, and before
-  /// [enableFlutterDriverExtension] for flutter drive tests). Paint the scene
+  /// (usually before [runApp] for normal Flutter apps, and before
+  /// [enableFlutterDriverExtension] for Flutter drive tests). Paint the scene
   /// in the custom [ShaderWarmUp] so Flutter can pre-compile and cache the
   /// shaders during startup. The warm up is only costly (100ms-200ms,
   /// depending on the shaders to compile) during the first run after the
@@ -49,6 +49,10 @@ mixin PaintingBinding on BindingBase, ServicesBinding {
   /// Currently the warm-up happens synchronously on the GPU thread which means
   /// the rendering of the first frame on the GPU thread will be postponed until
   /// the warm-up is finished.
+  ///
+  /// See also:
+  ///
+  ///  * [ShaderWarmUp], the interface of how this warm up works.
   static ShaderWarmUp shaderWarmUp = const DefaultShaderWarmUp();
 
   /// The singleton that implements the Flutter framework's image cache.
