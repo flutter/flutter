4891f4871e038f1436c996e5ee9a86614491ee64
Amir Hardon <amirha@google.com>
2019-02-28 12:22:38 -0800

    UiKitView reject absorbed touch events

--- packages/flutter/lib/src/rendering/platform_view.dart
+++ packages/flutter/lib/src/rendering/platform_view.dart
@@ -323,6 +323,8 @@ class RenderUiKitView extends RenderBox {
 
   _UiKitViewGestureRecognizer _gestureRecognizer;
 
+  PointerEvent _lastPointerDownEvent;
+
   @override
   void performResize() {
     size = constraints.biggest;
@@ -349,13 +351,41 @@ class RenderUiKitView extends RenderBox {
 
   @override
   void handleEvent(PointerEvent event, HitTestEntry entry) {
-    if (event is PointerDownEvent) {
-      _gestureRecognizer.addPointer(event);
+    if (!(event is PointerDownEvent)) {
+      return;
     }
+    _gestureRecognizer.addPointer(event);
+    _lastPointerDownEvent = event;
+  }
+
+  // This is registered as a global PointerRoute while the render object is attached.
+  void _handleGlobalPointerEvent(PointerEvent event) {
+    if (!(event is PointerDownEvent)) {
+      return;
+    }
+    final Offset localOffset = globalToLocal(event.position);
+    if(!(Offset.zero & size).contains(localOffset)) {
+      return;
+    }
+    if (event != _lastPointerDownEvent) {
+      // The pointer event is in the bounds of this render box, but we didn't get it in handleEvent.
+      // This means that the pointer event was absorbed by a different render object.
+      // Since on the platform side the FlutterTouchIntercepting view is seeing all events that are
+      // within its bounds we need to tell it to reject the current touch sequence.
+      _viewController.rejectGesture();
+    }
+    _lastPointerDownEvent = null;
+  }
+
+  @override
+  void attach(PipelineOwner owner) {
+    super.attach(owner);
+    GestureBinding.instance.pointerRouter.addGlobalRoute(_handleGlobalPointerEvent);
   }
 
   @override
   void detach() {
+    GestureBinding.instance.pointerRouter.removeGlobalRoute(_handleGlobalPointerEvent);
     _gestureRecognizer.reset();
     super.detach();
   }
