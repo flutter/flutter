03bec0d1b68ff57b6bf31d7cb8586837443b05fd
Gary Qian <garyq@google.com>
2019-03-01 10:43:09 -0800

    Buffer lifecycle in WindowData (flutter/engine#7999)

--- engine/src/flutter/shell/common/engine.cc
+++ engine/src/flutter/shell/common/engine.cc
@@ -291,6 +291,8 @@ bool Engine::HandleLifecyclePlatformMessage(blink::PlatformMessage* message) {
   if (state == "AppLifecycleState.resumed" && have_surface_) {
     ScheduleFrame();
   }
+  runtime_controller_->SetLifecycleState(state);
+  // We always want pass the message forwards directly into the framework.
   return false;
 }
 
