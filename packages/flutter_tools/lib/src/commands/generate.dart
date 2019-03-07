f5c3579f303b95971ac48843c06bcb463ed96416
Jonah Williams <jonahwilliams@google.com>
2019-03-07 14:52:29 -0800

    Remove extra build_runner modes, remove flutter_build (#28873)

--- packages/flutter_tools/lib/src/commands/generate.dart
+++ packages/flutter_tools/lib/src/commands/generate.dart
@@ -3,6 +3,7 @@
 // found in the LICENSE file.
 
 import '../base/common.dart';
+import '../cache.dart';
 import '../codegen.dart';
 import '../project.dart';
 import '../runner/flutter_command.dart';
@@ -22,11 +23,21 @@ class GenerateCommand extends FlutterCommand {
 
   @override
   Future<FlutterCommandResult> runCommand() async {
+    Cache.releaseLockEarly();
     if (!experimentalBuildEnabled) {
       throwToolExit('FLUTTER_EXPERIMENTAL_BUILD is not enabled, codegen is unsupported.');
     }
     final FlutterProject flutterProject = await FlutterProject.current();
-    await codeGenerator.generate(flutterProject, mainPath: argResults['target']);
+    final CodegenDaemon codegenDaemon = await codeGenerator.daemon(flutterProject);
+    codegenDaemon.startBuild();
+    await for (CodegenStatus codegenStatus in codegenDaemon.buildResults) {
+      if (codegenStatus == CodegenStatus.Failed) {
+        throwToolExit('Code generation failed');
+      }
+      if (codegenStatus ==CodegenStatus.Succeeded) {
+        break;
+      }
+    }
     return null;
   }
 }
