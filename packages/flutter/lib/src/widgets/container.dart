8a51d9e9a8e0e4cc1a3c8acdbbd9abd5c193d27c
Kate Lovett <katelovett@google.com>
2019-03-08 10:46:51 -0800

    Analyzer fix.

--- packages/flutter/lib/src/widgets/container.dart
+++ packages/flutter/lib/src/widgets/container.dart
@@ -5,7 +5,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/painting.dart';
 import 'package:flutter/rendering.dart';
-import 'package:flutter/material.dart';
 
 import 'basic.dart';
 import 'framework.dart';
