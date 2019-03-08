79898d3b266323c5e43e163d68c7b40fde4a3836
Kate Lovett <katelovett@google.com>
2019-03-08 15:59:06 -0800

    Fixing wrap and indentation.

--- packages/flutter/lib/src/widgets/container.dart
+++ packages/flutter/lib/src/widgets/container.dart
@@ -213,8 +213,7 @@ class DecoratedBox extends SingleChildRenderObjectWidget {
 /// ```dart
 /// Container(
 ///   constraints: BoxConstraints.expand(
-///   height:
-///     Theme.of(context).textTheme.display1.fontSize * 1.1 + 200.0,
+///     height: Theme.of(context).textTheme.display1.fontSize * 1.1 + 200.0,
 ///   ),
 ///   padding: const EdgeInsets.all(8.0),
 ///   color: Colors.blue[600],
