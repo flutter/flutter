62f7eed8348343371d5f124697ad0f7446decd82
Veljko Vuković <42891350+wolfcro1984@users.noreply.github.com>
2019-03-09 22:41:00 +0100

    Code corrections

--- packages/flutter/test/material/paginated_data_table_test.dart
+++ packages/flutter/test/material/paginated_data_table_test.dart
@@ -2,8 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:flutter/material.dart'
-import 'package:flutter/gestures.dart' show DragStartBehavior;
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'data_table_test_utils.dart';
