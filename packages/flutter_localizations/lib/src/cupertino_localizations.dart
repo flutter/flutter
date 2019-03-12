88ba71c06cec3efc8a5250537dae5700004d8844
Xiao Yu <xster@google.com>
2019-03-11 20:16:29 -0700

    re-combed the arb file and categorized the global source in the interface

---
+++ packages/flutter_localizations/lib/src/cupertino_localizations.dart
@@ -0,0 +1,13 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/cupertino.dart';
+import 'l10n/generated_date_localizations.dart' as date_localizations;
+
+import 'widgets_localizations.dart';
+
+abstract class GlobalCupertinoLocalizations implements CupertinoLocalizations {
+
+}
