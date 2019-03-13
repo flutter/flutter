906d684a7765cfe37b0edcdd167d3e319c7a22d4
Chinmay Garde <chinmaygarde@gmail.com>
2019-03-13 13:53:22 -0700

    Reland ""Add support for trace counters with variable arguments and instrument the raster cache." (#8145)
    This reverts commit bc901324faf5a24f9220cc7ecbcf5b97b96ae09f and fixes the
    discovered on Windows builds.

--- flow/raster_cache.h
+++ flow/raster_cache.h
@@ -33,6 +33,10 @@ class RasterCacheResult {
 
   void draw(SkCanvas& canvas, const SkPaint* paint = nullptr) const;
 
+  SkISize image_dimensions() const {
+    return image_ ? image_->dimensions() : SkISize::Make(0, 0);
+  };
+
  private:
   sk_sp<SkImage> image_;
   SkRect logical_rect_;
@@ -87,6 +91,7 @@ class RasterCache {
   void Prepare(PrerollContext* context, Layer* layer, const SkMatrix& ctm);
 
   RasterCacheResult Get(const SkPicture& picture, const SkMatrix& ctm) const;
+
   RasterCacheResult Get(Layer* layer, const SkMatrix& ctm) const;
 
   void SweepAfterFrame();
@@ -126,6 +131,8 @@ class RasterCache {
   LayerRasterCacheKey::Map<Entry> layer_cache_;
   bool checkerboard_images_;
   fml::WeakPtrFactory<RasterCache> weak_factory_;
+
+  void TraceStatsToTimeline() const;
 
   FML_DISALLOW_COPY_AND_ASSIGN(RasterCache);
 };
