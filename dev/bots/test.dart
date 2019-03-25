355d2084a6cab3a8b57ca12987b64b3ee4680b68
jonahwilliams <jonahwilliams@google.com>
2019-03-25 14:14:52 -0700

    pass FLUTTER_TOOLS_ARGS to snapshot command

--- dev/bots/test.dart
+++ dev/bots/test.dart
@@ -28,12 +28,12 @@ final bool useFlutterTestFormatter = Platform.environment['FLUTTER_TEST_FORMATTE
 final bool noUseBuildRunner = Platform.environment['FLUTTER_TEST_NO_BUILD_RUNNER'] == 'true';
 
 const Map<String, ShardRunner> _kShards = <String, ShardRunner>{
-  'tests': _runTests,
+  //'tests': _runTests,
   'tool_tests': _runToolTests,
-  'build_tests': _runBuildTests,
-  'coverage': _runCoverage,
-  'integration_tests': _runIntegrationTests,
-  'add2app_test': _runAdd2AppTest,
+  // 'build_tests': _runBuildTests,
+  // 'coverage': _runCoverage,
+  // 'integration_tests': _runIntegrationTests,
+  // 'add2app_test': _runAdd2AppTest,
 };
 
 const Duration _kLongTimeout = Duration(minutes: 45);
@@ -190,6 +190,7 @@ Future<void> _runToolTests() async {
       path.join(flutterRoot, 'packages', 'flutter_tools'),
       flutterRoot,
       tableData: bigqueryApi?.tabledata,
+      enableFlutterToolAsserts: true,
     );
   }
 
