d71f324e48bf299b6170c884550e7375f2a5c473
Jonah Williams <jonahwilliams@google.com>
2019-03-26 08:45:29 -0700

    Lazy cache 4 (#29785)

--- packages/flutter_tools/lib/src/commands/precache.dart
+++ packages/flutter_tools/lib/src/commands/precache.dart
@@ -4,6 +4,7 @@
 
 import 'dart:async';
 
+import '../cache.dart';
 import '../globals.dart';
 import '../runner/flutter_command.dart';
 
@@ -11,6 +12,12 @@ class PrecacheCommand extends FlutterCommand {
   PrecacheCommand() {
     argParser.addFlag('all-platforms', abbr: 'a', negatable: false,
         help: 'Precache artifacts for all platforms.');
+    argParser.addFlag('android', negatable: true, defaultsTo: true,
+        help: 'Precache artifacts for Android development');
+    argParser.addFlag('ios', negatable: true, defaultsTo: true,
+        help: 'Precache artifacts for iOS developemnt');
+    argParser.addFlag('web', negatable: true, defaultsTo: false,
+        help: 'Precache artifacts for web development');
   }
 
   @override
@@ -24,14 +31,25 @@ class PrecacheCommand extends FlutterCommand {
 
   @override
   Future<FlutterCommandResult> runCommand() async {
-    if (argResults['all-platforms'])
+    if (argResults['all-platforms']) {
       cache.includeAllPlatforms = true;
+    }
+    final Set<DevelopmentArtifact> requiredArtifacts = <DevelopmentArtifact>{ DevelopmentArtifact.universal };
+    if (argResults['android']) {
+      requiredArtifacts.add(DevelopmentArtifact.android);
+    }
+    if (argResults['ios']) {
+      requiredArtifacts.add(DevelopmentArtifact.iOS);
+    }
+    if (argResults['web']) {
+      requiredArtifacts.add(DevelopmentArtifact.web);
+    }
 
-    if (cache.isUpToDate())
+    if (cache.isUpToDate(requiredArtifacts)) {
       printStatus('Already up-to-date.');
-    else
-      await cache.updateAll();
-
+    } else {
+      await cache.updateAll(requiredArtifacts);
+    }
     return null;
   }
 }
