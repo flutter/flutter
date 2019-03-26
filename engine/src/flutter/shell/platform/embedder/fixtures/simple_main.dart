90c1cc90cf3a36a2ca2d68719c9c8e266a65aace
Chinmay Garde <chinmaygarde@gmail.com>
2019-03-26 13:49:41 -0700

    Revert "Allow specification of std::functions as native entrypoints from Dart code. (#8309)" (flutter/engine#8312)
    This reverts commit 07f506f752b4f1220cf51768fe2dcf595d03a039.

--- engine/src/flutter/shell/platform/embedder/fixtures/simple_main.dart
+++ engine/src/flutter/shell/platform/embedder/fixtures/simple_main.dart
@@ -6,15 +6,3 @@ void customEntrypoint() {
 }
 
 void sayHiFromCustomEntrypoint() native "SayHiFromCustomEntrypoint";
-
-
-@pragma('vm:entry-point')
-void customEntrypoint1() {
-  sayHiFromCustomEntrypoint1();
-  sayHiFromCustomEntrypoint2();
-  sayHiFromCustomEntrypoint3();
-}
-
-void sayHiFromCustomEntrypoint1() native "SayHiFromCustomEntrypoint1";
-void sayHiFromCustomEntrypoint2() native "SayHiFromCustomEntrypoint2";
-void sayHiFromCustomEntrypoint3() native "SayHiFromCustomEntrypoint3";
