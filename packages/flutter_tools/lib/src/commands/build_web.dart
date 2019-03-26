659303f81f9155076d8cb392aadf197580edcb05
Jonah Williams <jonahwilliams@google.com>
2019-03-25 20:54:11 -0700

    Update build_web.dart

--- packages/flutter_tools/lib/src/commands/build_web.dart
+++ packages/flutter_tools/lib/src/commands/build_web.dart
@@ -8,7 +8,7 @@ import '../base/common.dart';
 import '../base/logger.dart';
 import '../build_info.dart';
 import '../globals.dart';
-import '../runner/flutter_command.dart' show DevelopmentArtifact, ExitStatus, FlutterCommandResult;
+import '../runner/flutter_command.dart' show DevelopmentArtifact, FlutterCommandResult;
 import '../web/compile.dart';
 import 'build.dart';
 
