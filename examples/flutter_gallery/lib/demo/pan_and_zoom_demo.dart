0194ebb5f7267014e9c9da3fbe4297506902958d
Justin McCandless <jmccandless@google.com>
2019-03-26 11:10:11 -0700

    Code review cleanup in main demo file

--- examples/flutter_gallery/lib/demo/pan_and_zoom_demo.dart
+++ examples/flutter_gallery/lib/demo/pan_and_zoom_demo.dart
@@ -5,7 +5,7 @@ import 'pan_and_zoom_demo_edit_board_point.dart';
 import 'pan_and_zoom_demo_transform_interaction.dart';
 
 class PanAndZoomDemo extends StatefulWidget {
-  const PanAndZoomDemo({Key key}) : super(key: key);
+  const PanAndZoomDemo({ Key key }) : super(key: key);
 
   static const String routeName = '/pan_and_zoom';
 
@@ -28,68 +28,73 @@ class _PanAndZoomDemoState extends State<PanAndZoomDemo> {
     final BoardPainter painter = BoardPainter(
       board: _board,
     );
-    final Size screenSize = MediaQuery.of(context).size;
-    final Size visibleSize = Size(screenSize.width * 3, screenSize.height * 2);
-
     final FloatingActionButton floatingActionButton = _board.selected == null
       ? FloatingActionButton(
-        onPressed: () => setState(() {
-          _reset = true;
-        }),
-        tooltip: 'Reset Transform',
-        backgroundColor: Theme.of(context).primaryColor,
-        child: const Icon(Icons.home),
-      )
+          onPressed: () {
+            setState(() {
+              _reset = true;
+            });
+          },
+          tooltip: 'Reset Transform',
+          backgroundColor: Theme.of(context).primaryColor,
+          child: const Icon(Icons.home),
+        )
       : FloatingActionButton(
-        onPressed: () {
-          if (_board.selected == null) {
-            return;
-          }
-          showModalBottomSheet<Widget>(context: context, builder: (BuildContext context) {
-            return EditBoardPoint(
-              boardPoint: _board.selected,
-              onSetColor: (Color color) {
-                setState(() {
-                  _board = _board.setBoardPointColor(_board.selected, color);
-                  Navigator.pop(context);
-                });
-              },
-            );
-          });
-        },
-        tooltip: 'Edit Tile',
-        child: const Icon(Icons.edit),
-      );
+          onPressed: () {
+            if (_board.selected == null) {
+              return;
+            }
+            showModalBottomSheet<Widget>(context: context, builder: (BuildContext context) {
+              return EditBoardPoint(
+                boardPoint: _board.selected,
+                onSetColor: (Color color) {
+                  setState(() {
+                    _board = _board.setBoardPointColor(_board.selected, color);
+                    Navigator.pop(context);
+                  });
+                },
+              );
+            });
+          },
+          tooltip: 'Edit Tile',
+          child: const Icon(Icons.edit),
+        );
 
     // The scene is drawn by a CustomPaint, but user interaction is handled by
     // the TransformInteraction parent widget.
     return Scaffold(
       appBar: AppBar(),
-      body: TransformInteraction(
-        // TODO(justinmc): I'm not sure that I like this pattern that I came up
-        // with for resetting TransformInteraction from the parent level. Would
-        // a controller pattern be better, or is there something else?
-        reset: _reset,
-        onResetEnd: () {
-          setState(() {
-            _reset = false;
-          });
+      body: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+          final Size size = Size(constraints.maxWidth, constraints.maxHeight);
+          final Size visibleSize = Size(size.width * 3, size.height * 2);
+          return TransformInteraction(
+            // TODO(justinmc): I'm not sure that I like this pattern that I came up
+            // with for resetting TransformInteraction from the parent level. Would
+            // a controller pattern be better, or is there something else?
+            reset: _reset,
+            onResetEnd: () {
+              setState(() {
+                _reset = false;
+              });
+            },
+            child: CustomPaint(
+              size: Size.infinite,
+              painter: painter,
+            ),
+            visibleRect: Rect.fromLTWH(
+              -visibleSize.width / 2,
+              -visibleSize.height / 2,
+              visibleSize.width,
+              visibleSize.height,
+            ),
+            // Center the board in the middle of the screen. It's drawn centered at
+            // the origin, which is the top left corner of the TransformInteraction.
+            initialTranslation: Offset(size.width / 2, size.height / 2),
+            onTapUp: _onTapUp,
+            size: size,
+          );
         },
-        child: CustomPaint(
-          size: Size.infinite,
-          painter: painter,
-        ),
-        visibleRect: Rect.fromLTWH(
-          -visibleSize.width / 2,
-          -visibleSize.height / 2,
-          visibleSize.width,
-          visibleSize.height,
-        ),
-        // Center the board in the middle of the screen. It's drawn centered at
-        // the origin, which is the top left corner of the TransformInteraction.
-        initialTranslation: Offset(screenSize.width / 2, screenSize.height / 2),
-        onTapUp: _onTapUp,
-        size: screenSize,
       ),
       floatingActionButton: floatingActionButton,
     );
@@ -106,17 +111,18 @@ class _PanAndZoomDemoState extends State<PanAndZoomDemo> {
 // CustomPainter is what is passed to CustomPaint and actually draws the scene
 // when its `paint` method is called.
 class BoardPainter extends CustomPainter {
-  BoardPainter({
+  const BoardPainter({
     this.board,
   });
 
-  Board board;
+  final Board board;
 
   @override
   void paint(Canvas canvas, Size size) {
     void drawBoardPoint(BoardPoint boardPoint) {
-      final Color color = board.selected == boardPoint
-        ? boardPoint.color.withOpacity(0.2) : boardPoint.color;
+      final Color color = boardPoint.color.withOpacity(
+        board.selected == boardPoint ? 0.2 : 1.0,
+      );
       final Vertices vertices = board.getVerticesForBoardPoint(boardPoint, color);
       canvas.drawVertices(vertices, BlendMode.color, Paint());
     }
