a521d5667f9011ac190bcdf03503caf975a959d8
Jonah Williams <jonahwilliams@google.com>
2019-03-27 10:37:31 -0700

    Update text_field_test.dart

--- packages/flutter/test/material/text_field_test.dart
+++ packages/flutter/test/material/text_field_test.dart
@@ -1443,8 +1443,8 @@ void main() {
     await tester.pumpWidget(builder(maxLines, multipleLineText));
     final Text hintTextWidget = tester.widget(find.text(multipleLineText));
     expect(hintTextWidget.maxLines, equals(maxLines));
-    expect(findHintText(multipleLineText).size.dx, greaterThanOrEqualTo(oneLineHintSize.dx));
-    expect(findHintText(multipleLineText).size.dy, greaterThanOrEqualTo(oneLineHintSize.dy));
+    expect(findHintText(multipleLineText).size.width, greaterThanOrEqualTo(oneLineHintSize.width));
+    expect(findHintText(multipleLineText).size.height, greaterThanOrEqualTo(oneLineHintSize.height));
   });
 
   testWidgets('Can drag handles to change selection in multiline', (WidgetTester tester) async {
