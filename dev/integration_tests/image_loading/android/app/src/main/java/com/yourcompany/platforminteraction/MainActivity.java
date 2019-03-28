2b85fa6fdfe449c9c2b1e83d3eb30b872686b50f
jonahwilliams <jonahwilliams@google.com>
2019-03-28 16:13:36 -0700

    update versions

--- dev/integration_tests/image_loading/android/app/src/main/java/com/yourcompany/platforminteraction/MainActivity.java
+++ dev/integration_tests/image_loading/android/app/src/main/java/com/yourcompany/platforminteraction/MainActivity.java
@@ -4,6 +4,7 @@
 
 package com.yourcompany.platforminteraction;
 
+import android.os.Bundle;
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 import io.flutter.view.FlutterView;
