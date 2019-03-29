aa3eef03af631d854dff9ad90434437fa5c8d540
Dan Field <dnfield@google.com>
2019-03-29 14:52:54 -0700

    whitespace fix

--- packages/flutter/lib/src/widgets/draggable_scrollable_sheet.dart
+++ packages/flutter/lib/src/widgets/draggable_scrollable_sheet.dart
@@ -52,30 +52,30 @@ typedef ScrollableWidgetBuilder = Widget Function(
 ///
 /// ```dart
 /// class HomePage extends StatelessWidget {
-///  @override
-///  Widget build(BuildContext context) {
-///    return Scaffold(
-///      appBar: AppBar(
-///        title: const Text('DraggableScrollableSheet'),
-///      ),
-///      body: SizedBox.expand(
-///        child: DraggableScrollableSheet(
-///          builder: (BuildContext context, ScrollController scrollController) {
-///            return Container(
-///              color: Colors.blue[100],
-///              child: ListView.builder(
-///                controller: scrollController,
-///                itemCount: 25,
-///                itemBuilder: (BuildContext context, int index) {
-///                  return ListTile(title: Text('Item $index'));
-///                },
-///              ),
-///            );
-///          },
-///        ),
-///      ),
-///    );
-///  }
+///   @override
+///   Widget build(BuildContext context) {
+///     return Scaffold(
+///       appBar: AppBar(
+///         title: const Text('DraggableScrollableSheet'),
+///       ),
+///       body: SizedBox.expand(
+///         child: DraggableScrollableSheet(
+///           builder: (BuildContext context, ScrollController scrollController) {
+///             return Container(
+///               color: Colors.blue[100],
+///               child: ListView.builder(
+///                 controller: scrollController,
+///                 itemCount: 25,
+///                 itemBuilder: (BuildContext context, int index) {
+///                   return ListTile(title: Text('Item $index'));
+///                 },
+///               ),
+///             );
+///           },
+///         ),
+///       ),
+///     );
+///   }
 /// }
 /// ```
 /// {@end-tool}
