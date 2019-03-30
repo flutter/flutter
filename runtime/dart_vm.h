8dabd60a30d048bf90b4bc0b29914ea00bda182c
Chinmay Garde <chinmaygarde@gmail.com>
2019-03-29 20:53:40 -0700

    Remove unused DartVM::IsKernelMapping (#8381)

--- runtime/dart_vm.h
+++ runtime/dart_vm.h
@@ -38,8 +38,6 @@ class DartVM : public fml::RefCountedThreadSafe<DartVM> {
 
   static bool IsRunningPrecompiledCode();
 
-  static bool IsKernelMapping(const fml::FileMapping* mapping);
-
   const Settings& GetSettings() const;
 
   const DartSnapshot& GetVMSnapshot() const;
