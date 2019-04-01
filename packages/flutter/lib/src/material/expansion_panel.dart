9685a0b15d3504ae60cacaeb3bf001e70bce224c
Shi Hao Hong <shihaohong94@gmail.com>
2019-04-01 16:17:50 -0700

    improve ExpansionPanelRadio description

--- packages/flutter/lib/src/material/expansion_panel.dart
+++ packages/flutter/lib/src/material/expansion_panel.dart
@@ -91,10 +91,12 @@ class ExpansionPanel {
 }
 
 /// An expansion panel that allows for radio-like functionality.
+/// This means that at any given time, only one [ExpansionPanelRadio]
+/// can remain expanded at any given time.
 ///
 /// A unique identifier [value] must be assigned to each panel.
 /// This identifier allows the [ExpansionPanelList] to determine
-/// which [ExpansionPanelRadio] instance should stay open.
+/// which [ExpansionPanelRadio] instance should be expanded.
 ///
 /// See [ExpansionPanelList.radio] for a sample implementation.
 class ExpansionPanelRadio extends ExpansionPanel {
