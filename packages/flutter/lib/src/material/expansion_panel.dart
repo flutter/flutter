6da96d764c3acaef240da655727f531c970db52a
Shi Hao Hong <shihaohong94@gmail.com>
2019-04-02 13:03:06 -0700

    fix mismatching curly bracket in dartdoc example

--- packages/flutter/lib/src/material/expansion_panel.dart
+++ packages/flutter/lib/src/material/expansion_panel.dart
@@ -149,46 +149,45 @@ class ExpansionPanelRadio extends ExpansionPanel {
 /// ```
 ///
 /// ```dart
-///   List<Item> _data = generateItems(8);
+/// List<Item> _data = generateItems(8);
 ///
-///   @override
-///   Widget build(BuildContext context) {
-///     return SingleChildScrollView(
-///       child: Container(
-///         child: _buildPanel(),
-///       ),
-///     );
-///   }
+/// @override
+/// Widget build(BuildContext context) {
+///   return SingleChildScrollView(
+///     child: Container(
+///       child: _buildPanel(),
+///     ),
+///   );
+/// }
 ///
-///   Widget _buildPanel() {
-///     return ExpansionPanelList(
-///       expansionCallback: (int index, bool isExpanded) {
-///         setState(() {
-///           _data[index].isExpanded = !isExpanded;
-///         });
-///       },
-///       children: _data.map<ExpansionPanel>((Item item) {
-///         return ExpansionPanel(
-///           headerBuilder: (BuildContext context, bool isExpanded) {
-///             return ListTile(
-///               title: Text(item.headerValue),
-///             );
-///           },
-///           body: ListTile(
-///             title: Text(item.expandedValue),
-///             subtitle: Text('To delete this panel, tap the trash can icon'),
-///             trailing: Icon(Icons.delete),
-///             onTap: () {
-///               setState(() {
-///                 _data.removeWhere((currentItem) => item == currentItem);
-///               });
-///             }
-///           ),
-///           isExpanded: item.isExpanded,
-///         );
-///       }).toList(),
-///     );
-///   }
+/// Widget _buildPanel() {
+///   return ExpansionPanelList(
+///     expansionCallback: (int index, bool isExpanded) {
+///       setState(() {
+///         _data[index].isExpanded = !isExpanded;
+///       });
+///     },
+///     children: _data.map<ExpansionPanel>((Item item) {
+///       return ExpansionPanel(
+///         headerBuilder: (BuildContext context, bool isExpanded) {
+///           return ListTile(
+///             title: Text(item.headerValue),
+///           );
+///         },
+///         body: ListTile(
+///           title: Text(item.expandedValue),
+///           subtitle: Text('To delete this panel, tap the trash can icon'),
+///           trailing: Icon(Icons.delete),
+///           onTap: () {
+///             setState(() {
+///               _data.removeWhere((currentItem) => item == currentItem);
+///             });
+///           }
+///         ),
+///         isExpanded: item.isExpanded,
+///       );
+///     }).toList(),
+///   );
 /// }
 /// ```
 /// {@end-tool}
@@ -252,42 +251,41 @@ class ExpansionPanelList extends StatefulWidget {
   /// ```
   ///
   /// ```dart
-  ///   List<Item> _data = generateItems(8);
+  /// List<Item> _data = generateItems(8);
   ///
-  ///   @override
-  ///   Widget build(BuildContext context) {
-  ///     return SingleChildScrollView(
-  ///       child: Container(
-  ///         child: _buildPanel(),
-  ///       ),
-  ///     );
-  ///   }
+  /// @override
+  /// Widget build(BuildContext context) {
+  ///   return SingleChildScrollView(
+  ///     child: Container(
+  ///       child: _buildPanel(),
+  ///     ),
+  ///   );
+  /// }
   ///
-  ///   Widget _buildPanel() {
-  ///     return ExpansionPanelList.radio(
-  ///       initialOpenPanelValue: 2,
-  ///       children: _data.map<ExpansionPanelRadio>((Item item) {
-  ///         return ExpansionPanelRadio(
-  ///           value: item.id,
-  ///           headerBuilder: (BuildContext context, bool isExpanded) {
-  ///             return ListTile(
-  ///               title: Text(item.headerValue),
-  ///             );
-  ///           },
-  ///           body: ListTile(
-  ///             title: Text(item.expandedValue),
-  ///             subtitle: Text('To delete this panel, tap the trash can icon'),
-  ///             trailing: Icon(Icons.delete),
-  ///             onTap: () {
-  ///               setState(() {
-  ///                 _data.removeWhere((currentItem) => item == currentItem);
-  ///               });
-  ///             }
-  ///           )
-  ///         );
-  ///       }).toList(),
-  ///     );
-  ///   }
+  /// Widget _buildPanel() {
+  ///   return ExpansionPanelList.radio(
+  ///     initialOpenPanelValue: 2,
+  ///     children: _data.map<ExpansionPanelRadio>((Item item) {
+  ///       return ExpansionPanelRadio(
+  ///         value: item.id,
+  ///         headerBuilder: (BuildContext context, bool isExpanded) {
+  ///           return ListTile(
+  ///             title: Text(item.headerValue),
+  ///           );
+  ///         },
+  ///         body: ListTile(
+  ///           title: Text(item.expandedValue),
+  ///           subtitle: Text('To delete this panel, tap the trash can icon'),
+  ///           trailing: Icon(Icons.delete),
+  ///           onTap: () {
+  ///             setState(() {
+  ///               _data.removeWhere((currentItem) => item == currentItem);
+  ///             });
+  ///           }
+  ///         )
+  ///       );
+  ///     }).toList(),
+  ///   );
   /// }
   ///
   /// ```
