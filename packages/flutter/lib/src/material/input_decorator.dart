32f252e410aff8a6c5aa4dfe420a3418458c140f
GaryQian <garyq@google.com>
2019-04-02 13:23:31 -0700

    Implement InputDecorator baseline

--- packages/flutter/lib/src/material/input_decorator.dart
+++ packages/flutter/lib/src/material/input_decorator.dart
@@ -1058,9 +1058,7 @@ class _RenderDecoration extends RenderBox {
 
   @override
   double computeDistanceToActualBaseline(TextBaseline baseline) {
-    return computeMaxIntrinsicHeight(0);
-    assert(false, 'not implemented');
-    return 0.0;
+    return _boxParentData(input).offset.dy + input.computeDistanceToActualBaseline(baseline);
   }
 
   // Records where the label was painted.
