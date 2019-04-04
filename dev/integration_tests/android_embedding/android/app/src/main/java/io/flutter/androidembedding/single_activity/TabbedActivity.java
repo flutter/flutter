9552e8bd1e8c9655321c833ea09244d3b354651c
Matt Carroll <mattcarroll@nestlabs.com>
2019-04-04 13:51:14 -0700

    Commit to repro issue

--- dev/integration_tests/android_embedding/android/app/src/main/java/io/flutter/androidembedding/single_activity/TabbedActivity.java
+++ dev/integration_tests/android_embedding/android/app/src/main/java/io/flutter/androidembedding/single_activity/TabbedActivity.java
@@ -1,21 +1,19 @@
 package io.flutter.androidembedding.single_activity;
 
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-
+import android.graphics.PixelFormat;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
-import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-
 import android.widget.TextView;
 
 import io.flutter.androidembedding.R;
@@ -23,59 +21,72 @@ import io.flutter.embedding.android.FlutterFragment;
 import io.flutter.embedding.android.FlutterView;
 
 public class TabbedActivity extends AppCompatActivity {
-
-  /**
-   * The {@link android.support.v4.view.PagerAdapter} that will provide
-   * fragments for each of the sections. We use a
-   * {@link FragmentPagerAdapter} derivative, which will keep every
-   * loaded fragment in memory. If this becomes too memory intensive, it
-   * may be best to switch to a
-   * {@link android.support.v4.app.FragmentStatePagerAdapter}.
-   */
-  private SectionsPagerAdapter mSectionsPagerAdapter;
-
-  /**
-   * The {@link ViewPager} that will host the section contents.
-   */
-  private ViewPager mViewPager;
+  private static final String TAG = "TabbedActivity";
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_tabbed);
 
+    getWindow().setFormat(PixelFormat.TRANSPARENT);
+
     Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
     setSupportActionBar(toolbar);
-    // Create the adapter that will return a fragment for each of the three
-    // primary sections of the activity.
-    mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
 
-    // Set up the ViewPager with the sections adapter.
-    mViewPager = (ViewPager) findViewById(R.id.container);
-    mViewPager.setAdapter(mSectionsPagerAdapter);
+    BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+    bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+      @Override
+      public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+          case R.id.action_settings:
+            showAndroidPage();
+            return true;
+          case R.id.action_flutter:
+            showFlutterPage();
+            return true;
+        }
+        return false;
+      }
+    });
+
+    if (savedInstanceState == null) {
+      getSupportFragmentManager()
+          .beginTransaction()
+          .add(R.id.container, PlaceholderFragment.newInstance(0))
+          .commit();
+    }
   }
 
-
-  @Override
-  public boolean onCreateOptionsMenu(Menu menu) {
-    // Inflate the menu; this adds items to the action bar if it is present.
-    getMenuInflater().inflate(R.menu.menu_tabbed, menu);
-    return true;
-  }
-
-  @Override
-  public boolean onOptionsItemSelected(MenuItem item) {
-    // Handle action bar item clicks here. The action bar will
-    // automatically handle clicks on the Home/Up button, so long
-    // as you specify a parent activity in AndroidManifest.xml.
-    int id = item.getItemId();
-
-    //noinspection SimplifiableIfStatement
-    if (id == R.id.action_settings) {
-      return true;
+  private void showAndroidPage() {
+    Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.container);
+    if (fragment instanceof FlutterFragment) {
+      FlutterFragment flutterFragment = (FlutterFragment) fragment;
+//      flutterFragment.prepareForNavigation();
     }
 
-    return super.onOptionsItemSelected(item);
+    getSupportFragmentManager()
+        .beginTransaction()
+        .replace(R.id.container, PlaceholderFragment.newInstance(0))
+        .commit();
+  }
+
+  private void showFlutterPage() {
+    Log.d(TAG, "showFlutterPage()");
+    Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.container);
+    if (fragment instanceof FlutterFragment) {
+      FlutterFragment flutterFragment = (FlutterFragment) fragment;
+//      flutterFragment.prepareForNavigation();
+
+      getSupportFragmentManager()
+          .beginTransaction()
+          .remove(flutterFragment)
+          .commit();
+    } else {
+      getSupportFragmentManager()
+          .beginTransaction()
+          .replace(R.id.container, new FlutterFragment.Builder().dartEntrypoint("fragmentFlutter").build())
+          .commit();
+    }
   }
 
   /**
@@ -110,36 +121,6 @@ public class TabbedActivity extends AppCompatActivity {
       TextView textView = (TextView) rootView.findViewById(R.id.section_label);
       textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
       return rootView;
-    }
-  }
-
-  /**
-   * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-   * one of the sections/tabs/pages.
-   */
-  public class SectionsPagerAdapter extends FragmentPagerAdapter {
-
-    public SectionsPagerAdapter(FragmentManager fm) {
-      super(fm);
-    }
-
-    @Override
-    public Fragment getItem(int position) {
-      if (position == 0) {
-        // getItem is called to instantiate the fragment for the given page.
-        // Return a PlaceholderFragment (defined as a static inner class below).
-        return PlaceholderFragment.newInstance(position + 1);
-      } else {
-        return new FlutterFragment.Builder()
-            .dartEntrypoint("fragmentFlutter")
-            .renderMode(FlutterView.RenderMode.surface)
-            .build();
-      }
-    }
-
-    @Override
-    public int getCount() {
-      return 2;
     }
   }
 }
