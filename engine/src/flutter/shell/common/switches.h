e1622af4b56d248dd475552d1c1d34746d0c665b
Chinmay Garde <chinmaygarde@gmail.com>
2019-04-09 13:26:48 -0700

    Remove the unused EnableBlink flag. (flutter/engine#8518)
    This used to be when we had two text shaping engines.

--- engine/src/flutter/shell/common/switches.h
+++ engine/src/flutter/shell/common/switches.h
@@ -90,9 +90,6 @@ DEF_SWITCH(SkiaDeterministicRendering,
            "Skips the call to SkGraphics::Init(), thus avoiding swapping out"
            "some Skia function pointers based on available CPU features. This"
            "is used to obtain 100% deterministic behavior in Skia rendering.")
-DEF_SWITCH(EnableBlink,
-           "enable-blink",
-           "Enable Blink as the text shaping library instead of libtxt.")
 DEF_SWITCH(FLX, "flx", "Specify the FLX path.")
 DEF_SWITCH(FlutterAssetsDir,
            "flutter-assets-dir",
