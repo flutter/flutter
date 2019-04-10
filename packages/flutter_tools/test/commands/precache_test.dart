94bdabc2953bc62088a6638e0ae406ef555b2cb4
Jonah Williams <jonahwilliams@google.com>
2019-04-10 15:36:54 -0700

    Update precache_test.dart

--- packages/flutter_tools/test/commands/precache_test.dart
+++ packages/flutter_tools/test/commands/precache_test.dart
@@ -9,6 +9,7 @@ import 'package:flutter_tools/src/runner/flutter_command.dart';
 import 'package:flutter_tools/src/version.dart';
 import 'package:mockito/mockito.dart';
 
+import '../src/common.dart';
 import '../src/context.dart';
 import '../src/mocks.dart';
 
