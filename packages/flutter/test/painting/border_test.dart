fb020020b00ebbfadf99adb0bb441c68e861e7bc
Todd Volkert <tvolkert@google.com>
2019-04-10 14:51:02 -0700

    Review comments

--- packages/flutter/test/painting/border_test.dart
+++ packages/flutter/test/painting/border_test.dart
@@ -14,9 +14,9 @@ void main() {
   });
 
   test('Border.uniform constructor', () {
-    expect(() => Border.fromSide(null), throwsAssertionError);
+    expect(() => Border.fromBorderSide(null), throwsAssertionError);
     const BorderSide side = BorderSide();
-    const Border border = Border.fromSide(side);
+    const Border border = Border.fromBorderSide(side);
     expect(border.left, same(side));
     expect(border.top, same(side));
     expect(border.right, same(side));
