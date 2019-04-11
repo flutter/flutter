438a951b763d5e71c9a9306bb51d18bc9924d9cc
Christopher Fujino <christopherfujino@gmail.com>
2019-04-11 15:46:20 -0700

    remove comment

--- packages/flutter_tools/lib/src/commands/drive.dart
+++ packages/flutter_tools/lib/src/commands/drive.dart
@@ -82,21 +82,6 @@ class DriveCommand extends RunCommandBase {
 
   Device _device;
   Device get device => _device;
-  /*
-  bool get shouldBuild {
-    dynamic returnValue;
-    if (argResults == null)
-      returnValue = true;
-    else if (argResults['build'] == null)
-      returnValue = true;
-    else
-      returnValue = argResults['build'];
-    print('in our getter...');
-    print(argResults['build']);
-    print(returnValue);
-    return returnValue;
-  }
-  */
   bool get shouldBuild => argResults['build'] ?? true;
 
   /// Subscription to log messages printed on the device or simulator.
