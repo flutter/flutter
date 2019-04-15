985c93f4168f89effd924a63a8bf926b85cff10b
Matt Carroll <mattcarroll@nestlabs.com>
2019-04-15 15:37:27 -0700

    WIP

--- dev/integration_tests/android_embedding/android/app/src/main/java/io/flutter/androidembedding/single_activity/FragTransitionActivity.java
+++ dev/integration_tests/android_embedding/android/app/src/main/java/io/flutter/androidembedding/single_activity/FragTransitionActivity.java
@@ -2,13 +2,14 @@ package io.flutter.androidembedding.single_activity;
 
 import android.content.Context;
 import android.graphics.PixelFormat;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -23,8 +24,8 @@ import io.flutter.embedding.android.FlutterFragment;
 import io.flutter.embedding.android.FlutterView;
 import io.flutter.embedding.engine.FlutterEngine;
 
-public class TabbedActivity extends AppCompatActivity implements FlutterFragment.FlutterEngineProvider {
-  private static final String TAG = "TabbedActivity";
+public class FragTransitionActivity extends AppCompatActivity implements FlutterFragment.FlutterEngineProvider {
+  private static final String TAG = "FragTransitionActivity";
 
   private static FlutterEngine staticEngine;
 
@@ -44,10 +45,12 @@ public class TabbedActivity extends AppCompatActivity implements FlutterFragment
       public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
         switch (menuItem.getItemId()) {
           case R.id.action_settings:
-            showAndroidPage();
+            switchFragment();
+//            showAndroidPage();
             return true;
           case R.id.action_flutter:
-            showFlutterPage();
+            switchFragment();
+//            showFlutterPage();
             return true;
         }
         return false;
@@ -62,37 +65,64 @@ public class TabbedActivity extends AppCompatActivity implements FlutterFragment
     }
   }
 
-  private void showAndroidPage() {
-    Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.container);
-    if (fragment instanceof FlutterFragment) {
-      FlutterFragment flutterFragment = (FlutterFragment) fragment;
-//      flutterFragment.prepareForNavigation();
+  boolean showingFlutter = false;
+
+  public void switchFragment() {
+    FragmentManager fm = getSupportFragmentManager();
+    if (fm.getBackStackEntryCount() > 0) {
+      FragmentManager.BackStackEntry firstEntry = fm.getBackStackEntryAt(0);
+      fm.popBackStack(firstEntry.getId(), FragmentManager.POP_BACK_STACK_INCLUSIVE);
     }
-
-    getSupportFragmentManager()
-        .beginTransaction()
-        .replace(R.id.container, PlaceholderFragment.newInstance(0))
-        .commit();
-  }
-
-  private void showFlutterPage() {
-    Log.d(TAG, "showFlutterPage()");
-    Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.container);
-    if (fragment instanceof FlutterFragment) {
-      FlutterFragment flutterFragment = (FlutterFragment) fragment;
-//      flutterFragment.prepareForNavigation();
-
-      getSupportFragmentManager()
-          .beginTransaction()
-          .remove(flutterFragment)
-          .commit();
+    FragmentTransaction ft = fm.beginTransaction();
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+      ft.setCustomAnimations(
+          R.anim.navigation_fragment_enter,
+          android.R.anim.fade_out,
+          R.anim.navigation_fragment_enter,
+          android.R.anim.fade_out);
+    }
+    if (showingFlutter) {
+      showingFlutter = false;
+      ft.replace(R.id.container, new PlaceholderFragment());
     } else {
-      getSupportFragmentManager()
-          .beginTransaction()
-          .replace(R.id.container, new FlutterFragment.Builder().dartEntrypoint("fragmentFlutter").build())
-          .commit();
+      showingFlutter = true;
+      ft.replace(R.id.container, new FlutterFragment.Builder().dartEntrypoint("fragmentFlutter").renderMode(FlutterView.RenderMode.texture).build());
     }
+    ft.commit();
+    fm.executePendingTransactions();
   }
+
+//  private void showAndroidPage() {
+//    Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.container);
+//    if (fragment instanceof FlutterFragment) {
+//      FlutterFragment flutterFragment = (FlutterFragment) fragment;
+////      flutterFragment.prepareForNavigation();
+//    }
+//
+//    getSupportFragmentManager()
+//        .beginTransaction()
+//        .replace(R.id.container, PlaceholderFragment.newInstance(0))
+//        .commit();
+//  }
+//
+//  private void showFlutterPage() {
+//    Log.d(TAG, "showFlutterPage()");
+//    Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.container);
+//    if (fragment instanceof FlutterFragment) {
+//      FlutterFragment flutterFragment = (FlutterFragment) fragment;
+////      flutterFragment.prepareForNavigation();
+//
+//      getSupportFragmentManager()
+//          .beginTransaction()
+//          .remove(flutterFragment)
+//          .commit();
+//    } else {
+//      getSupportFragmentManager()
+//          .beginTransaction()
+//          .replace(R.id.container, new FlutterFragment.Builder().dartEntrypoint("fragmentFlutter").build())
+//          .commit();
+//    }
+//  }
 
   @Nullable
   @Override
@@ -133,7 +163,8 @@ public class TabbedActivity extends AppCompatActivity implements FlutterFragment
                              Bundle savedInstanceState) {
       View rootView = inflater.inflate(R.layout.fragment_tabbed, container, false);
       TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-      textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
+      textView.setText("Android Fragment!");
+//      textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
       return rootView;
     }
   }
