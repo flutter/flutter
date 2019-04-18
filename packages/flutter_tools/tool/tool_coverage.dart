e7abbbf4914a86789e7fd49da47b96fb46bf47c9
jonahwilliams <jonahwilliams@google.com>
2019-04-17 23:07:57 -0700

    simplify script logic

--- packages/flutter_tools/tool/tool_coverage.dart
+++ packages/flutter_tools/tool/tool_coverage.dart
@@ -11,47 +11,21 @@ import 'package:flutter_tools/src/project.dart';
 import 'package:flutter_tools/src/test/coverage_collector.dart';
 import 'package:pool/pool.dart';
 import 'package:path/path.dart' as path;
-import 'package:pedantic/pedantic.dart';
 
 /// A temp directory to create synthetic test files in.
 final Directory tempDirectory = Directory.systemTemp.createTempSync('_flutter_coverage')..createSync();
 
-/// A coverage map that can be re-initialized to speed up subsequent runs.
-final File coverageMap = File(path.join(Directory.current.path, '.coverage_report', 'coverage_map.json'));
-
 /// Generates an html coverage report for the flutter_tool.
-///
-/// By default, this will run all tests under the test directory. For a faster
-/// workflow, specific test files to run can also be passed as arguments. In
-/// these cases, the existing coverage information is persisted and can be
-/// reused.
 ///
 /// Must be run from the flutter_tools directory.
 ///
 /// Requires lcov and genhtml to be on PATH.
 /// See: https://github.com/linux-test-project/lcov.git.
-Future<void> main(List<String> arguments) async {
+Future<void> main() async {
   await runInContext(() async {
     final CoverageCollector coverageCollector = CoverageCollector(
       flutterProject: await FlutterProject.current(),
     );
-    // initialize from existing coverage map if it exists.
-    if (coverageMap.existsSync()) {
-      try {
-        final Map<String, dynamic> pending = json.decode(coverageMap.readAsStringSync());
-        final Map<String, Map<int, int>> result = <String, Map<int, int>>{};
-        for (String key in pending.keys) {
-          final Map<dynamic, dynamic> subMap = pending[key];
-          result[key] = Map<dynamic, dynamic>.fromIterables(
-            subMap.keys.map<int>((dynamic key) => int.parse(key)),
-            subMap.values,
-          );
-        }
-        coverageCollector.globalHitmap = result;
-      } catch (_) {
-        coverageMap.deleteSync();
-      }
-    }
     final String flutterRoot = Directory.current.parent.parent.path;
     final String dartPath = path.join(flutterRoot, 'bin', 'cache', 'dart-sdk', 'bin', 'dart');
     final Pool pool = Pool(Platform.numberOfProcessors);
@@ -73,30 +47,27 @@ void main() {
       final Uri coverageUri = Uri.parse('http://127.0.0.1:$port');
       final Completer<void> completer = Completer<void>();
       final Process testProcess = await Process.start(
-          dartPath,
-          <String>[
-            '--packages=${File('.packages').absolute.path}',
-            '--pause-isolates-on-exit',
-            '--enable-asserts',
-            '--enable-vm-service=${coverageUri.port}',
-            fakeTest.path,
-          ],
-          runInShell: true,
-          environment: <String, String>{
-            'FLUTTER_ROOT': Directory.current.parent.parent.path,
-          }).timeout(const Duration(seconds: 30));
+        dartPath,
+        <String>[
+          '--packages=${File('.packages').absolute.path}',
+          '--pause-isolates-on-exit',
+          '--enable-asserts',
+          '--enable-vm-service=${coverageUri.port}',
+          fakeTest.path,
+        ],
+        runInShell: true,
+        environment: <String, String>{
+          'FLUTTER_ROOT': Directory.current.parent.parent.path,
+        }).timeout(const Duration(seconds: 30));
       testProcess.stdout
-          .transform(utf8.decoder)
-          .transform(const LineSplitter())
-          .listen((String line) {
-            print(line);
-            if (line.contains('All tests passed') || line.contains('Some tests failed')) {
-              completer.complete(null);
-            }
-          });
-      unawaited(testProcess.exitCode.then((int code) {
-        print('test process exited with $code');
-      }));
+        .transform(utf8.decoder)
+        .transform(const LineSplitter())
+        .listen((String line) {
+          print(line);
+          if (line.contains('All tests passed') || line.contains('Some tests failed')) {
+            completer.complete(null);
+          }
+        });
       try {
         await completer.future;
         await coverageCollector.collectCoverage(testProcess, coverageUri).timeout(const Duration(seconds: 30));
@@ -114,21 +85,7 @@ void main() {
       }
       pending.add(runTest(fileSystemEntity));
     }
-   await Future.wait(pending);
-
-    if (!coverageMap.existsSync()) {
-      coverageMap.createSync(recursive: true);
-    }
-    print('saving coverage...');
-    coverageMap.writeAsStringSync(json.encode(coverageCollector.globalHitmap, toEncodable: (dynamic value) {
-      if (value is Map) {
-        return Map<dynamic, dynamic>.fromIterables(
-          value.keys.map<String>((Object key) => key.toString()),
-          value.values,
-        );
-      }
-      return value;
-    }));
+    await Future.wait(pending);
     final String lcovData = await coverageCollector.finalizeCoverage();
     final String lcovPath = path.join('.coverage_report', 'tools.lcov');
     final String htmlPath = path.join('.coverage_report', 'report.html');
