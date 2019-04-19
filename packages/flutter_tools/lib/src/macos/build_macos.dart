324a08e32d1c5550e0b86474c138e858d5ef3a4c
jonahwilliams <jonahwilliams@google.com>
2019-04-19 15:07:13 -0700

    Remove package import

--- packages/flutter_tools/lib/src/macos/build_macos.dart
+++ packages/flutter_tools/lib/src/macos/build_macos.dart
@@ -2,9 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:flutter_tools/src/base/file_system.dart';
-
 import '../base/common.dart';
+import '../base/file_system.dart';
 import '../base/io.dart';
 import '../base/logger.dart';
 import '../base/process_manager.dart';
