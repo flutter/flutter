9b886b0329f34f3accdaee354c1bac88175b6df1
Shi Hao Hong <shihaohong94@gmail.com>
2019-04-23 14:15:29 -0700

    Prevent ListTile onChanged invocation when radio is checked

--- packages/flutter/lib/src/material/radio_list_tile.dart
+++ packages/flutter/lib/src/material/radio_list_tile.dart
@@ -228,7 +228,13 @@ class RadioListTile<T> extends StatelessWidget {
           isThreeLine: isThreeLine,
           dense: dense,
           enabled: onChanged != null,
-          onTap: onChanged != null ? () { onChanged(value); } : null,
+          onTap: onChanged != null ?
+            () {
+              if (!checked) {
+                onChanged(value);
+              }
+            } :
+            null,
           selected: selected,
         ),
       ),
