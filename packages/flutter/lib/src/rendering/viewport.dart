316efe094283d3201f09d205c727dd9add70ab5d
Shi Hao Hong <shihaohong94@gmail.com>
2019-04-24 17:01:24 -0700

    Remove accidentally added newline

--- packages/flutter/lib/src/rendering/viewport.dart
+++ packages/flutter/lib/src/rendering/viewport.dart
@@ -1584,7 +1584,6 @@ class RenderShrinkWrappingViewport extends RenderViewportBase<SliverLogicalConta
           size = Size(constraints.minWidth, constraints.maxHeight);
           break;
       }
-
       offset.applyViewportDimension(0.0);
       _maxScrollExtent = 0.0;
       _shrinkWrapExtent = 0.0;
