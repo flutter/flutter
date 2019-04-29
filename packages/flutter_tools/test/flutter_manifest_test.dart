2dd64a0e037c14d46b4d14b0308b58ec844fafcc
Emmanuel Garcia <blasten@gmail.com>
2019-04-29 15:48:55 -0700

    Fix #31764: Show appropriate error message when fonts isn't iterable

--- packages/flutter_tools/test/flutter_manifest_test.dart
+++ packages/flutter_tools/test/flutter_manifest_test.dart
@@ -6,7 +6,9 @@ import 'dart:async';
 
 import 'package:file/file.dart';
 import 'package:file/memory.dart';
+import 'package:flutter_tools/src/base/context.dart';
 import 'package:flutter_tools/src/base/file_system.dart';
+import 'package:flutter_tools/src/base/logger.dart';
 import 'package:flutter_tools/src/cache.dart';
 import 'package:flutter_tools/src/flutter_manifest.dart';
 
@@ -463,6 +465,7 @@ flutter:
         expectedBuildNumber: null,
       );
     });
+
     test('parses no version clause', () async {
       const String manifest = '''
 name: test
@@ -477,6 +480,28 @@ flutter:
         expectedBuildName: null,
         expectedBuildNumber: null,
       );
+    });
+
+    testUsingContext('returns proper error when font detail is malformed', () async {
+      final BufferLogger logger = context.get<Logger>();
+      const String manifest = '''
+name: test
+dependencies:
+  flutter:
+    sdk: flutter
+flutter:
+  fonts:
+    - family: foo
+      fonts:
+        -asset: a/bar
+''';
+      final dynamic flutterManifest = FlutterManifest.createFromString(manifest);
+
+      expect(flutterManifest, isNull);
+      expect(logger.errorText, contains('Expected "fonts" to either be null or a list.'));
+
+    }, overrides: <Type, Generator>{
+      Logger: () => BufferLogger(),
     });
   });
 
