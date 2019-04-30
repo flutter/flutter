1219e5c7d2cc384b2426a3fda18e53a4c4592cd4
Kaushik Iska <iska.kaushik@gmail.com>
2019-04-30 15:25:20 -0700

    Re-create texture from pixel buffer onGrContextCreate (#8792)
    * Re-create texture from pixel buffer onGrContextCreate
    
    OnGrContextDestroy we destroy the texture, this is because
    we can not access it from the potentially new context that
    we get on bringing the app back to foreground.
    
    To show a valid texture on fg, we need to preserve the pixel
    buffer, using which we will create the new texture.
    
    * ensure texture cache exists
    
    * only reset pixel buffer if not null.

--- shell/platform/darwin/ios/ios_external_texture_gl.h
+++ shell/platform/darwin/ios/ios_external_texture_gl.h
@@ -27,9 +27,14 @@ class IOSExternalTextureGL : public flutter::Texture {
   void MarkNewFrameAvailable() override;
 
  private:
+  void CreateTextureFromPixelBuffer();
+
+  void EnsureTextureCacheExists();
+
   NSObject<FlutterTexture>* external_texture_;
   fml::CFRef<CVOpenGLESTextureCacheRef> cache_ref_;
   fml::CFRef<CVOpenGLESTextureRef> texture_ref_;
+  fml::CFRef<CVPixelBufferRef> buffer_ref_;
   FML_DISALLOW_COPY_AND_ASSIGN(IOSExternalTextureGL);
 };
 
