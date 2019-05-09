6d04d3f824b5a4cb7b7d8c6f280ff0c8343eabc6
Michael Goderbauer <goderbauer@google.com>
2019-05-09 09:50:26 +0200

    everything const

--- packages/flutter_driver/lib/src/common/gesture.dart
+++ packages/flutter_driver/lib/src/common/gesture.dart
@@ -14,14 +14,17 @@ class Tap extends CommandWithTarget {
   Tap.deserialize(Map<String, String> json) : super.deserialize(json);
 
   @override
-  final String kind = 'tap';
+  String get kind => 'tap';
 }
 
 /// The result of a [Tap] command.
 class TapResult extends Result {
+  /// Creates a [TapResult].
+  const TapResult();
+
   /// Deserializes this result from JSON.
   static TapResult fromJson(Map<String, dynamic> json) {
-    return TapResult();
+    return const TapResult();
   }
 
   @override
@@ -63,7 +66,7 @@ class Scroll extends CommandWithTarget {
   final int frequency;
 
   @override
-  final String kind = 'scroll';
+  String get kind => 'scroll';
 
   @override
   Map<String, String> serialize() => super.serialize()..addAll(<String, String>{
@@ -76,9 +79,12 @@ class Scroll extends CommandWithTarget {
 
 /// The result of a [Scroll] command.
 class ScrollResult extends Result {
+  /// Creates a [ScrollResult].
+  const ScrollResult();
+
   /// Deserializes this result from JSON.
   static ScrollResult fromJson(Map<String, dynamic> json) {
-    return ScrollResult();
+    return const ScrollResult();
   }
 
   @override
@@ -106,7 +112,7 @@ class ScrollIntoView extends CommandWithTarget {
   final double alignment;
 
   @override
-  final String kind = 'scrollIntoView';
+  String get kind => 'scrollIntoView';
 
   @override
   Map<String, String> serialize() => super.serialize()..addAll(<String, String>{
