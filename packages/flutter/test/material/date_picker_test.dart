c8dbd001993905e52971a6720b7a303ce71433a9
Greg Spencer <gspencergoog@users.noreply.github.com>
2019-05-16 09:31:05 -0700

    Skip flaky date picker tests on Windows (#32817)
    Date picker tests have been flaky on Windows for a long time, and repeatedly require restarting of bots. In the interest of productivity, disabling them on Windows.
    
    Related to #19696

--- packages/flutter/test/material/date_picker_test.dart
+++ packages/flutter/test/material/date_picker_test.dart
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'dart:io';
 import 'dart:ui';
 
 import 'package:flutter/rendering.dart';
@@ -16,7 +17,11 @@ import 'feedback_tester.dart';
 void main() {
   group('showDatePicker', () {
     _tests();
-  });
+  },
+    // Skip on Windows because this test is quite flaky when run on Windows,
+    // until https://github.com/flutter/flutter/issues/19696 is fixed.
+    skip: Platform.isWindows,
+  );
 }
 
 void _tests() {
