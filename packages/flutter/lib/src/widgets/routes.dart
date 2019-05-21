4b641ea91261dc5a7dafe63b87e01ec23433d244
Tong Mu <dkwingsmt@gmail.com>
2019-05-21 11:19:41 -0700

    Resume focus on didPopNext

--- packages/flutter/lib/src/widgets/routes.dart
+++ packages/flutter/lib/src/widgets/routes.dart
@@ -1250,6 +1250,15 @@ abstract class ModalRoute<T> extends TransitionRoute<T> with LocalHistoryRoute<T
     );
   }
 
+  @override
+  void didPopNext(Route<dynamic> nextRoute) {
+    super.didPopNext(nextRoute);
+    // When the route on top of this route is popped, resume focus onto the
+    // widget that received the last focus.
+    FocusScope.of(subtreeContext).focusedChild?.requestFocus();
+  }
+
+
   // We cache the part of the modal scope that doesn't change from frame to
   // frame so that we minimize the amount of building that happens.
   Widget _modalScopeCache;
