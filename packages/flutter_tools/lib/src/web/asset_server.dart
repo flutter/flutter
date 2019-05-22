d1ca8e637d23d6160616cc39bf27c79130caf5de
jonahwilliams <jonahwilliams@google.com>
2019-05-22 13:31:41 -0700

    update with upstreamed changes

--- packages/flutter_tools/lib/src/web/asset_server.dart
+++ packages/flutter_tools/lib/src/web/asset_server.dart
@@ -78,6 +78,14 @@ class WebAssetServer {
           .childDirectory('web')
           .childFile('index.html');
       await _completeRequest(request, file, 'text/html');
+    } else if (uri.path.contains('require.js')) {
+      final HttpClient client = HttpClient();
+      final HttpClientRequest lookupRequest = await client.getUrl(Uri.parse('https://cdnjs.cloudflare.com/ajax/libs/require.js/2.3.6/require.min.js'));
+      final HttpClientResponse lookupResponse = await lookupRequest.close();
+      request.response.statusCode = lookupResponse.statusCode;
+      request.response.headers.add(HttpHeaders.contentTypeHeader, 'text/javascript');
+      await request.response.addStream(lookupResponse);
+      await request.response.close();
     } else if (uri.path.endsWith('main.dart.js')) {
       final File file = fs.file(fs.path.join(flutterProject.generated.path,
           'lib', '${fs.path.basename(target)}.js'));
