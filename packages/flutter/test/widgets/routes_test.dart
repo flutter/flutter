e6a45f032949ab741ac9e7043c60dc17ad968c04
Tong Mu <dkwingsmt@gmail.com>
2019-05-21 17:58:06 -0700

    Add TODO

--- packages/flutter/test/widgets/routes_test.dart
+++ packages/flutter/test/widgets/routes_test.dart
@@ -507,6 +507,11 @@ void main() {
 
   group('ModalRoute', () {
     testWidgets('should resume focus when the next route is popped', (WidgetTester tester) async {
+      // TODO(tongmu): It's currently using PageRoute (from MaterialApp)
+      // instead of ModalRoute, which might leave a risk but avoids building
+      // everything from scratch by using the existing setup by MaterialApp and
+      // TextField as a focusable widget. We might want to write a better test.
+
       final FocusNode node = FocusNode();
       BuildContext pageContext;
       final Widget widget = MaterialApp(
