901f82376eaf6e9b889ebf3a4118044c494415b5
Ian Hickson <ian@hixie.ch>
2019-05-21 22:29:08 -0700

    Fixes for new dependencies.

--- packages/flutter_tools/test/devfs_test.dart
+++ packages/flutter_tools/test/devfs_test.dart
@@ -7,8 +7,8 @@ import 'dart:convert';
 
 import 'package:file/file.dart';
 import 'package:file/memory.dart';
-import 'package:flutter_tools/src/base/io.dart';
 import 'package:flutter_tools/src/base/file_system.dart';
+import 'package:flutter_tools/src/base/io.dart';
 import 'package:flutter_tools/src/devfs.dart';
 import 'package:flutter_tools/src/vmservice.dart';
 import 'package:json_rpc_2/json_rpc_2.dart' as rpc;
