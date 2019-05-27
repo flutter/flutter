d0b8eba0afb94fd62c10ca4dde8bd18091593ed7
Mehmet Fidanboylu <mehmetf@users.noreply.github.com>
2019-05-26 20:07:13 -0700

    Fix internal break since listing contents can return null (flutter/engine#9078)

--- engine/src/flutter/shell/platform/android/io/flutter/view/FlutterMain.java
+++ engine/src/flutter/shell/platform/android/io/flutter/view/FlutterMain.java
@@ -375,6 +375,9 @@ public class FlutterMain {
     private static Set<String> listLibs(@NonNull Context applicationContext) {
         ApplicationInfo applicationInfo = getApplicationInfo(applicationContext);
         File[] files = new File(applicationInfo.nativeLibraryDir).listFiles();
+        if (files == null) {
+            files = new File[0];
+        }
 
         ImmutableSetBuilder<String> builder = ImmutableSetBuilder.newInstance();
         for (File file : files) {
