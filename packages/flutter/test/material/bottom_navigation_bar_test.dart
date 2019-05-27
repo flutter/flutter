6fccf23e5b01949826a5b8eea233bcf1e90672ee
jonahwilliams <jonahwilliams@google.com>
2019-05-27 12:07:28 -0700

    add web safe indirection to Platform.isPlatform getters

--- packages/flutter/test/material/bottom_navigation_bar_test.dart
+++ packages/flutter/test/material/bottom_navigation_bar_test.dart
@@ -2,7 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'dart:io';
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
@@ -1436,7 +1435,7 @@ void main() {
       await expectLater(
         find.byType(BottomNavigationBar),
         matchesGoldenFile('bottom_navigation_bar.shifting_transition.2.$pump.png'),
-        skip: !Platform.isLinux,
+        skip: !isLinux,
       );
     }
   });
--- packages/flutter/test/material/tab_bar_theme_test.dart
+++ packages/flutter/test/material/tab_bar_theme_test.dart
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'dart:io' show Platform;
-
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -270,7 +268,7 @@ void main() {
     await expectLater(
       find.byKey(_painterKey),
       matchesGoldenFile('tab_bar_theme.tab_indicator_size_tab.png'),
-      skip: !Platform.isLinux,
+      skip: !isLinux,
     );
   });
 
@@ -282,7 +280,7 @@ void main() {
     await expectLater(
       find.byKey(_painterKey),
       matchesGoldenFile('tab_bar_theme.tab_indicator_size_label.png'),
-      skip: !Platform.isLinux,
+      skip: !isLinux,
     );
   });
 
@@ -299,7 +297,7 @@ void main() {
     await expectLater(
       find.byKey(_painterKey),
       matchesGoldenFile('tab_bar_theme.custom_tab_indicator.png'),
-      skip: !Platform.isLinux,
+      skip: !isLinux,
     );
   });
 
@@ -316,7 +314,7 @@ void main() {
     await expectLater(
       find.byKey(_painterKey),
       matchesGoldenFile('tab_bar_theme.beveled_rect_indicator.png'),
-      skip: !Platform.isLinux,
+      skip: !isLinux,
     );
   });
 }
