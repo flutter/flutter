f0de8cc15e3566a56cdc9ea92a317816ec987075
Gary Qian <garyq@google.com>
2019-05-28 17:06:57 -0700

    Remove outdated TODOs (flutter/engine#9114)

--- engine/src/flutter/third_party/txt/src/txt/paragraph.h
+++ engine/src/flutter/third_party/txt/src/txt/paragraph.h
@@ -56,8 +56,6 @@ class Paragraph {
 
   enum Affinity { UPSTREAM, DOWNSTREAM };
 
-  // TODO(garyq): Implement kIncludeLineSpacing and kExtendEndOfLine
-
   // Options for various types of bounding boxes provided by
   // GetRectsForRange(...).
   enum class RectHeightStyle {
