0624bd6cb0459bd56db1e50dbc3bd55ee5ddc36d
jonahwilliams <jonahwilliams@google.com>
2019-05-29 12:00:13 -0700

    fix license headers

--- packages/flutter_tools/lib/src/test/flutter_web_platform.dart
+++ packages/flutter_tools/lib/src/test/flutter_web_platform.dart
@@ -1,6 +1,6 @@
-// Copyright (c) 2016, the Dart project authors.  Please see the AUTHORS file
-// for details. All rights reserved. Use of this source code is governed by a
-// BSD-style license that can be found in the LICENSE file.
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
 // ignore_for_file: implementation_imports
 
