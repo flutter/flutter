5469af4778b5128288ec3e4ac1060d303ee73eb8
YourFavouriteOreo <adiwani98@gmail.com>
2019-05-30 05:14:32 +0300

    Utilizing IconThemeColor

--- examples/hello_world/lib/main.dart
+++ examples/hello_world/lib/main.dart
@@ -2,7 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 void main() => runApp(const Center(child: Text('Hello, world!', textDirection: TextDirection.ltr)));
