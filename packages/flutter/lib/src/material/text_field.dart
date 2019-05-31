66f3c1db2d0a14630b6b12363fd324c22814a306
Rubens de Melo <rubensdemelo@gmail.com>
2019-05-31 13:02:38 -0300

    Include remember to dispose texteditingcontroller
    Almost the same note here https://flutter.dev/docs/cookbook/forms/text-field-changes#2-use-a-texteditingcontroller .

--- packages/flutter/lib/src/material/text_field.dart
+++ packages/flutter/lib/src/material/text_field.dart
@@ -62,6 +62,10 @@ typedef InputCounterWidgetBuilder = Widget Function(
 /// tapped an ink splash that paints on the material is triggered, see
 /// [ThemeData.splashFactory].
 ///
+/// Remember to [dispose] the TextEditingController when it is no longer needed. 
+/// This will ensure we discard any resources used by the object. 
+/// 
+///
 /// To integrate the [TextField] into a [Form] with other [FormField] widgets,
 /// consider using [TextFormField].
 ///
