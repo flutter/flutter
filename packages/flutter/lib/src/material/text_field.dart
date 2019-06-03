6898c187fa3409991197a9e578f5d8c4f2e526f5
Rubens de Melo <rubensdemelo@gmail.com>
2019-06-03 14:18:20 -0300

    include of, remove extra white space and improve description link to cookbook.
    include of, remove extra white space and improve description link to cookbook.

--- packages/flutter/lib/src/material/text_field.dart
+++ packages/flutter/lib/src/material/text_field.dart
@@ -65,7 +65,7 @@ typedef InputCounterWidgetBuilder = Widget Function(
 /// To integrate the [TextField] into a [Form] with other [FormField] widgets,
 /// consider using [TextFormField].
 /// 
-/// Remember to [dispose] the [TextEditingController] when it is no longer needed. 
+/// Remember to [dispose] of the [TextEditingController] when it is no longer needed.
 /// This will ensure we discard any resources used by the object.
 ///
 /// {@tool sample}
@@ -94,7 +94,7 @@ typedef InputCounterWidgetBuilder = Widget Function(
 ///    [TextField]. The [EditableText] widget is rarely used directly unless
 ///    you are implementing an entirely different design language, such as
 ///    Cupertino.
-///  * <https://flutter.dev/docs/cookbook/forms/text-field-changes#2-use-a-texteditingcontroller>
+///  * Learn how to use a [TextEditingController] in one of our [cookbook recipe]s.(https://flutter.dev/docs/cookbook/forms/text-field-changes#2-use-a-texteditingcontroller)
 class TextField extends StatefulWidget {
   /// Creates a Material Design text field.
   ///
