730025fa1735fc0260865f1d4e0a9fbaa0d1c91c
chunhtai <47866232+chunhtai@users.noreply.github.com>
2019-06-03 14:00:43 -0700

    fix issue 14014 read only text field (#32059)

--- packages/flutter/lib/src/cupertino/text_field.dart
+++ packages/flutter/lib/src/cupertino/text_field.dart
@@ -139,6 +139,9 @@ class CupertinoTextField extends StatefulWidget {
   /// this is a single-line text field and will scroll horizontally when
   /// overflown. [maxLines] must not be zero.
   ///
+  /// The text cursor is not shown if [showCursor] is false or if [showCursor]
+  /// is null (the default) and [readOnly] is true.
+  ///
   /// See also:
   ///
   ///  * [minLines]
@@ -167,6 +170,8 @@ class CupertinoTextField extends StatefulWidget {
     this.style,
     this.strutStyle,
     this.textAlign = TextAlign.start,
+    this.readOnly = false,
+    this.showCursor,
     this.autofocus = false,
     this.obscureText = false,
     this.autocorrect = true,
@@ -190,6 +195,7 @@ class CupertinoTextField extends StatefulWidget {
     this.scrollController,
     this.scrollPhysics,
   }) : assert(textAlign != null),
+       assert(readOnly != null),
        assert(autofocus != null),
        assert(obscureText != null),
        assert(autocorrect != null),
@@ -312,6 +318,12 @@ class CupertinoTextField extends StatefulWidget {
 
   /// {@macro flutter.widgets.editableText.textAlign}
   final TextAlign textAlign;
+
+  /// {@macro flutter.widgets.editableText.readOnly}
+  final bool readOnly;
+
+  /// {@macro flutter.widgets.editableText.showCursor}
+  final bool showCursor;
 
   /// {@macro flutter.widgets.editableText.autofocus}
   final bool autofocus;
@@ -489,6 +501,8 @@ class _CupertinoTextFieldState extends State<CupertinoTextField> with AutomaticK
   // For backwards-compatibility, we treat a null kind the same as touch.
   bool _shouldShowSelectionToolbar = true;
 
+  bool _showSelectionHandles = false;
+
   @override
   void initState() {
     super.initState();
@@ -644,8 +658,11 @@ class _CupertinoTextFieldState extends State<CupertinoTextField> with AutomaticK
     if (cause == SelectionChangedCause.longPress) {
       _editableText?.bringIntoView(selection.base);
     }
-    if (_shouldShowSelectionHandles(cause)) {
-      _editableText?.showHandles();
+    final bool willShowSelectionHandles = _shouldShowSelectionHandles(cause);
+    if (willShowSelectionHandles != _showSelectionHandles) {
+      setState(() {
+        _showSelectionHandles = willShowSelectionHandles;
+      });
     }
   }
 
@@ -812,6 +829,9 @@ class _CupertinoTextFieldState extends State<CupertinoTextField> with AutomaticK
           child: EditableText(
             key: _editableTextKey,
             controller: controller,
+            readOnly: widget.readOnly,
+            showCursor: widget.showCursor,
+            showSelectionHandles: _showSelectionHandles,
             focusNode: _effectiveFocusNode,
             keyboardType: widget.keyboardType,
             textInputAction: widget.textInputAction,
