2823f69b5d6c6cf9f3f694fcb6ac85e772cfbc43
GaryQian <garyq@google.com>
2019-06-03 18:05:20 -0700

    Analyzer

--- packages/flutter/test/widgets/backdrop_filter_test.dart
+++ packages/flutter/test/widgets/backdrop_filter_test.dart
@@ -10,7 +10,7 @@ import 'package:flutter/material.dart';
 
 void main() {
   testWidgets('BackdropFilter\'s cull rect does not shrink', (WidgetTester tester) async {
-    tester.binding.addTime(Duration(seconds: 15));
+    tester.binding.addTime(const Duration(seconds: 15));
     await tester.pumpWidget(
       MaterialApp(
         home: Scaffold(
