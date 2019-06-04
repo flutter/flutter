9094f39a94695cabbd449c9862cee629865c3bf5
Jonah Williams <jonahwilliams@google.com>
2019-06-03 23:19:42 -0700

    Add a real-er web restart, doctor, workflow (#33786)

--- packages/flutter_tools/lib/src/doctor.dart
+++ packages/flutter_tools/lib/src/doctor.dart
@@ -33,6 +33,8 @@ import 'proxy_validator.dart';
 import 'tester/flutter_tester.dart';
 import 'version.dart';
 import 'vscode/vscode_validator.dart';
+import 'web/web_validator.dart';
+import 'web/workflow.dart';
 import 'windows/windows_workflow.dart';
 
 Doctor get doctor => context.get<Doctor>();
@@ -65,6 +67,9 @@ class _DefaultDoctorValidatorsProvider implements DoctorValidatorsProvider {
 
       if (iosWorkflow.appliesToHostPlatform)
         _validators.add(iosValidator);
+
+      if (webWorkflow.appliesToHostPlatform)
+        _validators.add(const WebValidator());
 
       final List<DoctorValidator> ideValidators = <DoctorValidator>[];
       ideValidators.addAll(AndroidStudioValidator.allValidators);
@@ -278,6 +283,8 @@ class Doctor {
 
 /// A series of tools and required install steps for a target platform (iOS or Android).
 abstract class Workflow {
+  const Workflow();
+
   /// Whether the workflow applies to this platform (as in, should we ever try and use it).
   bool get appliesToHostPlatform;
 
