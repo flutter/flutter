3371da1e09511cb047c746e8209ec91550714b05
Gary Qian <garyq@google.com>
2019-06-05 16:01:58 -0700

    Add platform_fuchsia.cc for default font on fuchsia (#9204)

---
+++ third_party/txt/src/txt/platform_fuchsia.cc
@@ -0,0 +1,17 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "txt/platform.h"
+
+namespace txt {
+
+std::string GetDefaultFontFamily() {
+  return "Roboto";
+}
+
+sk_sp<SkFontMgr> GetDefaultFontManager() {
+  return SkFontMgr::RefDefault();
+}
+
+}  // namespace txt
