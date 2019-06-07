18f4119b09b313721c7f3e08392813b338996275
GaryQian <garyq@google.com>
2019-06-07 16:54:14 -0700

    Typo

--- packages/flutter/lib/src/painting/inline_span.dart
+++ packages/flutter/lib/src/painting/inline_span.dart
@@ -105,7 +105,7 @@ abstract class InlineSpan extends DiagnosticableTree {
   /// Returns the [GestureRecognizer] associated with this span if this is an
   /// instance of [TextSpan], otherwise returns null.
   @Deprecated('InlineSpan does not innately have a recognizer. Use TextSpan.recognizer instead.')
-  GestureRecognizer get recognizer; => null;
+  GestureRecognizer get recognizer => null;
 
   /// Apply the properties of this object to the given [ParagraphBuilder], from
   /// which a [Paragraph] can be obtained.
