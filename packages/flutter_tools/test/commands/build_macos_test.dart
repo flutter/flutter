71bfe3be9f66adea08a21bd533f3f826cfc21592
Jonah Williams <jonahwilliams@google.com>
2019-06-10 12:01:14 -0700

    Revert "More verification on flutter build web, add tests and cleanup (#34090)" (#34166)

--- packages/flutter_tools/test/commands/build_macos_test.dart
+++ packages/flutter_tools/test/commands/build_macos_test.dart
@@ -19,34 +19,24 @@ import '../src/context.dart';
 import '../src/mocks.dart';
 
 void main() {
-  MockProcessManager mockProcessManager;
-  MemoryFileSystem memoryFilesystem;
-  MockProcess mockProcess;
-  MockPlatform macosPlatform;
-  MockPlatform notMacosPlatform;
+  Cache.disableLocking();
+  final MockProcessManager mockProcessManager = MockProcessManager();
+  final MemoryFileSystem memoryFilesystem = MemoryFileSystem();
+  final MockProcess mockProcess = MockProcess();
+  final MockPlatform macosPlatform = MockPlatform();
+  final MockPlatform notMacosPlatform = MockPlatform();
 
-  setUpAll(() {
-    Cache.disableLocking();
-  });
-
-  setUp(() {
-    mockProcessManager = MockProcessManager();
-    memoryFilesystem = MemoryFileSystem();
-    mockProcess = MockProcess();
-    macosPlatform = MockPlatform();
-    notMacosPlatform = MockPlatform();
-    when(mockProcess.exitCode).thenAnswer((Invocation invocation) async {
+  when(mockProcess.exitCode).thenAnswer((Invocation invocation) async {
     return 0;
-    });
-    when(mockProcess.stderr).thenAnswer((Invocation invocation) {
-      return const Stream<List<int>>.empty();
-    });
-    when(mockProcess.stdout).thenAnswer((Invocation invocation) {
-      return const Stream<List<int>>.empty();
-    });
-    when(macosPlatform.isMacOS).thenReturn(true);
-    when(notMacosPlatform.isMacOS).thenReturn(false);
   });
+  when(mockProcess.stderr).thenAnswer((Invocation invocation) {
+    return const Stream<List<int>>.empty();
+  });
+  when(mockProcess.stdout).thenAnswer((Invocation invocation) {
+    return const Stream<List<int>>.empty();
+  });
+  when(macosPlatform.isMacOS).thenReturn(true);
+  when(notMacosPlatform.isMacOS).thenReturn(false);
 
   testUsingContext('macOS build fails when there is no macos project', () async {
     final BuildCommand command = BuildCommand();
