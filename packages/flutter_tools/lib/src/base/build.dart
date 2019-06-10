8627ff433b4658195e66b9c0034902116f53d580
Emmanuel Garcia <egarciad@google.com>
2019-06-10 13:16:09 -0700

    Allow multi-abi shared libraries in APKs and App bundles (#34123)
    * Gradle generates ELF shared libraries instead of AOT snapshots.
    * `flutter build apk/appbundle` supports multiple `--target-platform` and defaults to `android-arm` and `android-arm64`.
    * `flutter build apk` now has a flag called `--split-per-abi`.

--- packages/flutter_tools/lib/src/base/build.dart
+++ packages/flutter_tools/lib/src/base/build.dart
@@ -137,7 +137,7 @@ class AOTSnapshotter {
       final String aotSharedLibrary = fs.path.join(outputDir.path, 'app.so');
       outputPaths.add(aotSharedLibrary);
       genSnapshotArgs.add('--snapshot_kind=app-aot-elf');
-      genSnapshotArgs.add('--assembly=$aotSharedLibrary');
+      genSnapshotArgs.add('--elf=$aotSharedLibrary');
     } else {
       // Blob AOT snapshot.
       final String vmSnapshotData = fs.path.join(outputDir.path, 'vm_snapshot_data');
