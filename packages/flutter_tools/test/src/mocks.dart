e59d9a815d6a3a49e4058e63c107f8d051af1042
Ben Konyi <bkonyi@google.com>
2019-06-11 11:37:47 -0700

    Reland "Added --dart-flags option to flutter run (#33924)" (#34181)
    Reland "Added --dart-flags option to flutter run (#33924)"
    
    This reverts commit 587687eedf81aa83521cca96b8fb65a4adc8dd90.

--- packages/flutter_tools/test/src/mocks.dart
+++ packages/flutter_tools/test/src/mocks.dart
@@ -37,6 +37,18 @@ class MockApplicationPackageStore extends ApplicationPackageStore {
   );
 }
 
+class MockApplicationPackageFactory extends Mock implements ApplicationPackageFactory {
+  final MockApplicationPackageStore _store = MockApplicationPackageStore();
+
+  @override
+  Future<ApplicationPackage> getPackageForPlatform(
+    TargetPlatform platform, {
+    File applicationBinary,
+  }) async {
+    return _store.getPackageForPlatform(platform);
+  }
+}
+
 /// An SDK installation with several SDK levels (19, 22, 23).
 class MockAndroidSdk extends Mock implements AndroidSdk {
   static Directory createSdkDirectory({
