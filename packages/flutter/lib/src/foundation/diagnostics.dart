5c7477d0031d94b9260a63bf4eef9682b86194b3
Michael Goderbauer <goderbauer@google.com>
2019-06-13 09:46:35 -0700

    Include raw value in Diagnostics json for basic types

--- packages/flutter/lib/src/foundation/diagnostics.dart
+++ packages/flutter/lib/src/foundation/diagnostics.dart
@@ -2489,6 +2489,8 @@ class DiagnosticsProperty<T> extends DiagnosticsNode {
     json['defaultLevel'] = describeEnum(_defaultLevel);
     if (value is Diagnosticable || value is DiagnosticsNode)
       json['isDiagnosticableValue'] = true;
+    if (value is num || value is String || value is bool)
+      json['value'] = value;
     return json;
   }
 
