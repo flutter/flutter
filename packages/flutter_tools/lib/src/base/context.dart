264de26572d2539cec7028aae6591ed1ddeb299c
Michael Thomsen <mit-mit@users.noreply.github.com>
2019-06-14 14:19:36 +0200

    Change API doc link to api.dart.dev (#34388)

--- packages/flutter_tools/lib/src/base/context.dart
+++ packages/flutter_tools/lib/src/base/context.dart
@@ -43,7 +43,7 @@ AppContext get context => Zone.current[_Key.key] as AppContext ?? AppContext._ro
 /// scope) is created.
 ///
 /// Child contexts are created and run using zones. To read more about how
-/// zones work, see https://api.dartlang.org/stable/dart-async/Zone-class.html.
+/// zones work, see https://api.dart.dev/stable/dart-async/Zone-class.html.
 class AppContext {
   AppContext._(
     this._parent,
