0ac81aa39f2d7288c3f42258135f95e656ce6fc1
Dan Field <dnfield@google.com>
2019-06-14 22:50:15 -0700

    start

--- packages/flutter_test/lib/src/widget_tester.dart
+++ packages/flutter_test/lib/src/widget_tester.dart
@@ -68,10 +68,10 @@ typedef WidgetTesterCallback = Future<void> Function(WidgetTester widgetTester);
 /// In general, timeouts are race conditions and cause flakes, so best practice
 /// is to avoid the use of timeouts in tests.
 ///
-/// If the `enableSemantics` parameter is set to `true`,
+/// If the `semanticsEnabled` parameter is set to `true`,
 /// [WidgetTester.ensureSemantics] will have been called before the tester is
 /// passed to the `callback`, and that handle will automatically be disposed
-/// after the callback is finished.
+/// after the callback is finished. It defaults to true.
 ///
 /// This function uses the [test] function in the test package to
 /// register the given callback as a test. The callback, when run,
@@ -100,13 +100,14 @@ void testWidgets(
   bool skip = false,
   test_package.Timeout timeout,
   Duration initialTimeout,
-  bool semanticsEnabled = false,
+  bool semanticsEnabled = true,
 }) {
   final TestWidgetsFlutterBinding binding = TestWidgetsFlutterBinding.ensureInitialized();
   final WidgetTester tester = WidgetTester._(binding);
   test(
     description,
     () {
+      debugResetSemanticsIdCounter();
       SemanticsHandle semanticsHandle;
       if (semanticsEnabled == true) {
         semanticsHandle = tester.ensureSemantics();
