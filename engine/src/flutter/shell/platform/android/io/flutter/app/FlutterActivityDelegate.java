4d5f19b50bb36a43f82ed939b423990c77d5827f
Dan Field <dnfield@google.com>
2019-06-19 13:13:13 -0700

    Wire intent args for observatory port (flutter/engine#9378)

--- engine/src/flutter/shell/platform/android/io/flutter/app/FlutterActivityDelegate.java
+++ engine/src/flutter/shell/platform/android/io/flutter/app/FlutterActivityDelegate.java
@@ -312,6 +312,13 @@ public final class FlutterActivityDelegate
         if (intent.getBooleanExtra("verbose-logging", false)) {
             args.add("--verbose-logging");
         }
+        final int observatoryPort = intent.getIntExtra("observatory-port", 0);
+        if (observatoryPort > 0) {
+            args.add("--observatory-port=" + Integer.toString(observatoryPort));
+        }
+        if (intent.getBooleanExtra("disable-service-auth-codes", false)) {
+            args.add("--disable-service-auth-codes");
+        }
         // NOTE: all flags provided with this argument are subject to filtering
         // based on a whitelist in shell/common/switches.cc. If any flag provided
         // is not present in the whitelist, the process will immediately
