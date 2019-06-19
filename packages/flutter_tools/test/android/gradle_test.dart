674b0df5462c11ab9345ffa804b47999fd44efbe
Jonah Williams <jonahwilliams@google.com>
2019-06-19 10:09:42 -0700

    Update gradle_test.dart

--- packages/flutter_tools/test/android/gradle_test.dart
+++ packages/flutter_tools/test/android/gradle_test.dart
@@ -46,6 +46,7 @@ void main() {
       final GradleProject gradleProject = MockGradleProject();
       const AndroidBuildInfo buildInfo = AndroidBuildInfo(BuildInfo.debug);
       when(gradleProject.apkFilesFor(buildInfo)).thenReturn(<String>['not_real']);
+      when(gradleProhect.apkDirectory).thenReturn(fs.currentDirectory);
 
       expect(findApkFiles(gradleProject, buildInfo), <File>[]);
     }, overrides: <Type, Generator>{
