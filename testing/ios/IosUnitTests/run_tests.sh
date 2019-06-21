f6389583f4c8029c1910355aa116ccd885a7db32
gaaclarke <30870216+gaaclarke@users.noreply.github.com>
2019-06-21 16:43:35 -0700

    Ios unit tests choose engine (#9432)
    * Split out the run_tests script to a build_and_run_tests script to make
    it easier to run the tests on luci.
    
    * Made build and run pass in its argument to run.

--- testing/ios/IosUnitTests/run_tests.sh
+++ testing/ios/IosUnitTests/run_tests.sh
@@ -1,9 +1,16 @@
-pushd $PWD
-cd ../../../..
-./flutter/tools/gn --ios --simulator --unoptimized
-ninja -j 100 -C out/ios_debug_sim_unopt/
-popd
+FLUTTER_ENGINE=ios_debug_sim_unopt
+
+if [ $# -eq 1 ]; then
+  FLUTTER_ENGINE=$1
+fi
+
+PRETTY="cat"
+if which xcpretty; then
+  PRETTY="xcpretty"
+fi
+
 xcodebuild -sdk iphonesimulator \
   -scheme IosUnitTests \
   -destination 'platform=iOS Simulator,name=iPhone SE,OS=12.2' \
-  test
+  test \
+  FLUTTER_ENGINE=$FLUTTER_ENGINE | $PRETTY
