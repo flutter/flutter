c06aedb1d36877828abbaab86ce402d578a54f96
Dan Field <dnfield@google.com>
2019-06-24 14:15:58 -0700

    Call oper

--- dev/bots/flutter_tool_build_cache.ps1
+++ dev/bots/flutter_tool_build_cache.ps1
@@ -1,3 +1,3 @@
 cd "$Env:CIRRUS_WORKING_DIR\packages\flutter_tools"
 
-"$Env:CIRRUS_WORKING_DIR\bin\cache\dart-sdk\bin\pub.exe" run build_runner build
+& "$Env:CIRRUS_WORKING_DIR\bin\cache\dart-sdk\bin\pub.exe" run build_runner build
