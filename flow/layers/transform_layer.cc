aa9b3a180a114b3cc917694a72f6896f9dc013c3
Chris Yang <ychris@google.com>
2019-06-25 15:29:47 -0700

    Reland "IOS Platform view transform/clipping (#9075)" and fix the breakage. (#9483)
    * Revert "Revert "IOS Platform view transform/clipping (#9075)" (#9480)"
    
    This reverts commit 00d929f7f6088375b006746718a65b84678d01c0.
    
    * fix fuschia buid

--- flow/layers/transform_layer.cc
+++ flow/layers/transform_layer.cc
@@ -66,7 +66,10 @@ void TransformLayer::Paint(PaintContext& context) const {
 
   SkAutoCanvasRestore save(context.internal_nodes_canvas, true);
   context.internal_nodes_canvas->concat(transform_);
+  context.mutators_stack.pushTransform(transform_);
+
   PaintChildren(context);
+  context.mutators_stack.pop();
 }
 
 }  // namespace flutter
