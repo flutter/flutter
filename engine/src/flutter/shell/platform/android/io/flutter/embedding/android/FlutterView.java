59bd2aecc04f7f24743bfd0ed4a19d0e146377e7
Matt Carroll <matthew-carroll@users.noreply.github.com>
2019-06-25 01:59:33 -0700

    Removed unused imports in new embedding. (flutter/engine#9463)

--- engine/src/flutter/shell/platform/android/io/flutter/embedding/android/FlutterView.java
+++ engine/src/flutter/shell/platform/android/io/flutter/embedding/android/FlutterView.java
@@ -27,7 +27,6 @@ import android.widget.FrameLayout;
 
 import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
 import java.util.Set;
