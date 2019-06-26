2f1d4f03b5e55facec54b379256f5d0123c8a6ae
Chris Yang <cyang8954@gmail.com>
2019-06-25 18:26:31 -0700

    fix build breakage on PlatformViews.mm (#9495)

--- shell/platform/darwin/ios/framework/Source/FlutterPlatformViews.mm
+++ shell/platform/darwin/ios/framework/Source/FlutterPlatformViews.mm
@@ -230,7 +230,7 @@ UIView* FlutterPlatformViewsController::ReconstructClipViewsChain(int number_of_
 
 void FlutterPlatformViewsController::ApplyMutators(const MutatorsStack& mutators_stack,
                                                    UIView* embedded_view) {
-  FML_DCHECK(CATransform3DEqualToTransform(head.layer.transform, CATransform3DIdentity));
+  FML_DCHECK(CATransform3DEqualToTransform(embedded_view.layer.transform, CATransform3DIdentity));
 
   UIView* head = embedded_view;
   head.clipsToBounds = YES;
