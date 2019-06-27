cfa99d3dfb5290bb7f8d39b6cbcf5e029792c05b
Jonah Williams <jonahwilliams@google.com>
2019-06-27 11:10:19 -0700

    Update coverage_collector.dart

--- packages/flutter_tools/lib/src/test/coverage_collector.dart
+++ packages/flutter_tools/lib/src/test/coverage_collector.dart
@@ -191,8 +191,9 @@ Future<Map<String, dynamic>> collect(Uri serviceUri, bool Function(String) libra
   if (!waitPaused) {
     return _getAllCoverage(vmService, libraryPredicate);
   }
+  const int kPollAttempts = 20;
   int i = 0;
-  while (i < 20) {
+  while (i < kPollAttempts) {
     await isolate.load();
     if (isolate.pauseEvent?.kind == ServiceEvent.kPauseStart) {
       break;
@@ -200,7 +201,7 @@ Future<Map<String, dynamic>> collect(Uri serviceUri, bool Function(String) libra
     await Future<void>.delayed(const Duration(milliseconds: 50));
     i += 1;
   }
-  if (i == 20) {
+  if (i == kPollAttempts) {
     print('Isolate $debugName was never paused, refusing to collect coverage');
     return const <String, dynamic>{
       'type': 'CodeCoverage',
