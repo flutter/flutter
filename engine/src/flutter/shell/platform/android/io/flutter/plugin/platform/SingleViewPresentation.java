77a2f1c89bc8e7d582caec085fed707d3c1fd9d1
Amir Hardon <amirh@users.noreply.github.com>
2019-06-28 13:43:23 -0700

    Revert "Avoid a full screen overlay within virtual displays (#9343)" (flutter/engine#9581)
    This reverts commit 2b66f64511df7de1404802aab5ccbdbb8587244e.

--- engine/src/flutter/shell/platform/android/io/flutter/plugin/platform/SingleViewPresentation.java
+++ engine/src/flutter/shell/platform/android/io/flutter/plugin/platform/SingleViewPresentation.java
@@ -29,13 +29,13 @@ import static android.view.View.OnFocusChangeListener;
  *
  * The view hierarchy for the presentation is as following:
  *
- *                rootView
- *                  |
- *       state.fakeWindowViewGroup
- *             /            \
- *   state.container   [other popup views]
- *         |
- *    EmbeddedView
+ *          rootView
+ *         /         \
+ *        /           \
+ *       /             \
+ *   container       state.fakeWindowViewGroup
+ *      |
+ *   EmbeddedView
  */
 @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
 class SingleViewPresentation extends Presentation {
@@ -55,9 +55,6 @@ class SingleViewPresentation extends Presentation {
 
         // Contains views that were added directly to the window manager (e.g android.widget.PopupWindow).
         private FakeWindowViewGroup fakeWindowViewGroup;
-
-        // Contains the embedded platform view (platformView.getView()) when it is attached to the presentation.
-        private FrameLayout container;
     }
 
     private final PlatformViewFactory viewFactory;
@@ -75,12 +72,12 @@ class SingleViewPresentation extends Presentation {
     // so when we create the platform view we can tell it its view id.
     private Object createParams;
 
-    // The root view for the presentation, it has a single child called fakeWindowViewGroup which contains
-    // views that were added directly to the presentation's window manager. fakeWindowViewGroup's first
-    // child is the state.container which contains the embedded view. So all other views are drawn on-top but
-    // the embedded view itself is not obscured directly by the fakeWindowViewGroup.
-    //
+    // The root view for the presentation, it has 2 childs: container which contains the embedded view, and
+    // fakeWindowViewGroup which contains views that were added directly to the presentation's window manager.
     private AccessibilityDelegatingFrameLayout rootView;
+
+    // Contains the embedded platform view (platformView.getView()) when it is attached to the presentation.
+    private FrameLayout container;
 
     private PresentationState state;
 
@@ -145,17 +142,12 @@ class SingleViewPresentation extends Presentation {
         if (state.fakeWindowViewGroup == null) {
             state.fakeWindowViewGroup = new FakeWindowViewGroup(getContext());
         }
-        if (state.container == null) {
-            state.container = new FrameLayout(getContext());
-            final WindowManager.LayoutParams params = new WindowManager.LayoutParams();
-            params.gravity = Gravity.FILL;
-            state.fakeWindowViewGroup.addView(state.container, params);
-        }
         if (state.windowManagerHandler == null) {
             WindowManager windowManagerDelegate = (WindowManager) getContext().getSystemService(WINDOW_SERVICE);
             state.windowManagerHandler = new WindowManagerHandler(windowManagerDelegate, state.fakeWindowViewGroup);
         }
 
+        container = new FrameLayout(getContext());
         PresentationContext context = new PresentationContext(getContext(), state.windowManagerHandler);
 
         if (state.platformView == null) {
@@ -163,8 +155,9 @@ class SingleViewPresentation extends Presentation {
         }
 
         View embeddedView = state.platformView.getView();
-        state.container.addView(embeddedView);
+        container.addView(embeddedView);
         rootView = new AccessibilityDelegatingFrameLayout(getContext(), accessibilityEventsDelegate, embeddedView);
+        rootView.addView(container);
         rootView.addView(state.fakeWindowViewGroup);
 
         embeddedView.setOnFocusChangeListener(focusChangeListener);
@@ -178,7 +171,7 @@ class SingleViewPresentation extends Presentation {
     }
 
     public PresentationState detachState() {
-        state.container.removeAllViews();
+        container.removeAllViews();
         rootView.removeAllViews();
         return state;
     }
