c64c182e9ec2e907e8cffd1fbb0f6b54a8ac733d
Tong Mu <dkwingsmt@users.noreply.github.com>
2019-06-28 00:37:01 -0700

    New benchmark: Gesture semantics (#35232)
    * Add semanticsEnabled to widgetBenchmark
    * Add button_matrix_app and gesture benchmark

--- dev/devicelab/lib/tasks/microbenchmarks.dart
+++ dev/devicelab/lib/tasks/microbenchmarks.dart
@@ -53,6 +53,7 @@ TaskFunction createMicrobenchmarkTask() {
     allResults.addAll(await _runMicrobench('lib/stocks/build_bench.dart'));
     allResults.addAll(await _runMicrobench('lib/geometry/rrect_contains_bench.dart'));
     allResults.addAll(await _runMicrobench('lib/gestures/velocity_tracker_bench.dart'));
+    allResults.addAll(await _runMicrobench('lib/gestures/gesture_detector_bench.dart'));
     allResults.addAll(await _runMicrobench('lib/stocks/animation_bench.dart'));
 
     return TaskResult.success(allResults, benchmarkScoreKeys: allResults.keys.toList());
