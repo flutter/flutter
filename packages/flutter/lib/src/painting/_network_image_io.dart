dfa0872d558ba4b4bb1462cffe7dd0596309396f
Matan Shukry <matanshukry@gmail.com>
2019-06-29 19:13:50 +0300

    Using custom exception class for network loading error

--- packages/flutter/lib/src/painting/_network_image_io.dart
+++ packages/flutter/lib/src/painting/_network_image_io.dart
@@ -15,13 +15,15 @@ import 'image_provider.dart' as image_provider;
 import 'image_stream.dart';
 
 /// The dart:io implemenation of [image_provider.NetworkImage].
-class NetworkImage extends image_provider.ImageProvider<image_provider.NetworkImage> implements image_provider.NetworkImage {
+class NetworkImage
+    extends image_provider.ImageProvider<image_provider.NetworkImage>
+    implements image_provider.NetworkImage {
   /// Creates an object that fetches the image at the given URL.
   ///
   /// The arguments [url] and [scale] must not be null.
   const NetworkImage(this.url, { this.scale = 1.0, this.headers })
-    : assert(url != null),
-      assert(scale != null);
+      : assert(url != null),
+        assert(scale != null);
 
   @override
   final String url;
@@ -33,7 +35,8 @@ class NetworkImage extends image_provider.ImageProvider<image_provider.NetworkIm
   final Map<String, String> headers;
 
   @override
-  Future<NetworkImage> obtainKey(image_provider.ImageConfiguration configuration) {
+  Future<NetworkImage> obtainKey(
+      image_provider.ImageConfiguration configuration) {
     return SynchronousFuture<NetworkImage>(this);
   }
 
@@ -42,7 +45,8 @@ class NetworkImage extends image_provider.ImageProvider<image_provider.NetworkIm
     // Ownership of this controller is handed off to [_loadAsync]; it is that
     // method's responsibility to close the controller's stream when the image
     // has been loaded or an error is thrown.
-    final StreamController<ImageChunkEvent> chunkEvents = StreamController<ImageChunkEvent>();
+    final StreamController<ImageChunkEvent> chunkEvents = StreamController<
+        ImageChunkEvent>();
 
     return MultiFrameImageStreamCompleter(
       codec: _loadAsync(key, chunkEvents),
@@ -50,7 +54,8 @@ class NetworkImage extends image_provider.ImageProvider<image_provider.NetworkIm
       scale: key.scale,
       informationCollector: () {
         return <DiagnosticsNode>[
-          DiagnosticsProperty<image_provider.ImageProvider>('Image provider', this),
+          DiagnosticsProperty<image_provider.ImageProvider>(
+              'Image provider', this),
           DiagnosticsProperty<image_provider.NetworkImage>('Image key', key),
         ];
       },
@@ -61,7 +66,8 @@ class NetworkImage extends image_provider.ImageProvider<image_provider.NetworkIm
   // We set `autoUncompress` to false to ensure that we can trust the value of
   // the `Content-Length` HTTP header. We automatically uncompress the content
   // in our call to [consolidateHttpClientResponseBytes].
-  static final HttpClient _sharedHttpClient = HttpClient()..autoUncompress = false;
+  static final HttpClient _sharedHttpClient = HttpClient()
+    ..autoUncompress = false;
 
   static HttpClient get _httpClient {
     HttpClient client = _sharedHttpClient;
@@ -73,10 +79,8 @@ class NetworkImage extends image_provider.ImageProvider<image_provider.NetworkIm
     return client;
   }
 
-  Future<ui.Codec> _loadAsync(
-    NetworkImage key,
-    StreamController<ImageChunkEvent> chunkEvents,
-  ) async {
+  Future<ui.Codec> _loadAsync(NetworkImage key,
+      StreamController<ImageChunkEvent> chunkEvents,) async {
     try {
       assert(key == this);
 
@@ -87,7 +91,8 @@ class NetworkImage extends image_provider.ImageProvider<image_provider.NetworkIm
       });
       final HttpClientResponse response = await request.close();
       if (response.statusCode != HttpStatus.ok)
-        throw Exception('HTTP request failed, statusCode: ${response?.statusCode}, $resolved');
+        throw NetworkImageLoadException(
+            code: response?.statusCode, uri: resolved);
 
       final Uint8List bytes = await consolidateHttpClientResponseBytes(
         response,
@@ -121,4 +126,29 @@ class NetworkImage extends image_provider.ImageProvider<image_provider.NetworkIm
 
   @override
   String toString() => '$runtimeType("$url", scale: $scale)';
+}
+
+/// Thrown to indicate that loading a network image failed
+///
+class NetworkImageLoadException implements Exception {
+  /// Creates a [NetworkImageLoadException] with the specified http status
+  /// [code] and the [uri]
+  NetworkImageLoadException({
+    @required this.code,
+    @required this.uri,
+  })
+      : assert(uri != null),
+        message = 'HTTP request failed, statusCode: $code, $uri';
+
+  /// An http status code.
+  final int code;
+
+  /// A human-readable error message.
+  final String message;
+
+  /// Resolved URI of the requested image.
+  final Uri uri;
+
+  @override
+  String toString() => message;
 }
