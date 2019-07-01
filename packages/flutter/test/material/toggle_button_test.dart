e76ef3d6e30468c3d679cbdddc51e2dc298a1395
Shi Hao Hong <shihaohong94@gmail.com>
2019-07-01 14:44:42 -0700

    Stubbed out toggle button tests

---
+++ packages/flutter/test/material/toggle_button_test.dart
@@ -0,0 +1,51 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  // taps toggle state
+
+  // null onPressed disables the buttons
+
+  // children cannot be null
+
+  // isSelected cannot be null
+
+  // children and isSelected have to be same length
+
+  // test all default colors
+    // color
+    // selectedcolor
+    // disabledColor
+    // fillColor
+    // focusColor
+    // highlightColor
+    // hoverColor
+    // splashColor
+    // borderColor
+    // selectedBorderColor
+    // disabledBorderColor
+
+  // test custom colors
+    // color
+    // selectedcolor
+    // disabledColor
+    // fillColor
+    // focusColor
+    // highlightColor
+    // hoverColor
+    // splashColor
+    // borderColor
+    // selectedBorderColor
+    // disabledBorderColor
+
+  // test default border radius
+  // test custom border radius
+  // test default border width
+  // test custom border width
+
+  // test proper paints based on state
+}
