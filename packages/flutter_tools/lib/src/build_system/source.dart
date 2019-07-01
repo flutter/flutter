751b993831429b8c5bb8927e352b47929a337d8e
Jonah Williams <jonahwilliams@google.com>
2019-07-01 13:17:25 -0700

    Update source.dart

--- packages/flutter_tools/lib/src/build_system/source.dart
+++ packages/flutter_tools/lib/src/build_system/source.dart
@@ -2,10 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:flutter_tools/src/artifacts.dart';
-import 'package:flutter_tools/src/build_info.dart';
-
+import '../artifacts.dart';
 import '../base/file_system.dart';
+import '../build_info.dart';
 import '../globals.dart';
 import 'build_system.dart';
 import 'exceptions.dart';
