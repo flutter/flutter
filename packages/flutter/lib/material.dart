8f36d4d5326addcc6e1dff7716eae9991fd4b47f
Shi Hao Hong <shihaohong94@gmail.com>
2019-07-02 11:15:26 -0700

    Fix - add import for toggle_buttons instead of toggle_button to material.dart

--- packages/flutter/lib/material.dart
+++ packages/flutter/lib/material.dart
@@ -113,7 +113,7 @@ export 'src/material/theme.dart';
 export 'src/material/theme_data.dart';
 export 'src/material/time.dart';
 export 'src/material/time_picker.dart';
-export 'src/material/toggle_button.dart';
+export 'src/material/toggle_buttons.dart';
 export 'src/material/toggleable.dart';
 export 'src/material/tooltip.dart';
 export 'src/material/typography.dart';
