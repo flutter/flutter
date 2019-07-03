fff63b3a459e590a21c3e04f8a078fa565396ae4
Chris Yang <ychris@google.com>
2019-07-03 14:47:49 -0700

    make the cirrus happy

--- packages/flutter/lib/src/widgets/platform_view.dart
+++ packages/flutter/lib/src/widgets/platform_view.dart
@@ -200,8 +200,9 @@ class AndroidView extends StatefulWidget {
 ///
 /// Construction of UIViews is done asynchronously, before the UIView is ready this widget paints
 /// nothing while maintaining the same layout constraints.
-/// 
-/// If a conic path clipping is applied to a UIKitView, a quad path is used to approximate the clip due to limitation of Quartz.
+///
+/// If a conic path clipping is applied to a UIKitView,
+/// a quad path is used to approximate the clip due to limitation of Quartz.
 class UiKitView extends StatefulWidget {
   /// Creates a widget that embeds an iOS view.
   ///
