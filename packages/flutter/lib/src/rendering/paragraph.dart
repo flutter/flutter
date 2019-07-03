3a3d119008f14ad7accdd10072b2334b34953ae7
GaryQian <garyq@google.com>
2019-07-03 12:36:17 -0700

    Update comments

--- packages/flutter/lib/src/rendering/paragraph.dart
+++ packages/flutter/lib/src/rendering/paragraph.dart
@@ -321,7 +321,8 @@ class RenderParagraph extends RenderBox
     // TODO(garyq): Since our metric for ideographic baseline is currently inacurrate
     // and the non-alphabetic baselines are based off of the alphabetic baseline, we
     // use the alphabetic for now to produce correct layouts. We should eventually change
-    // this back to pass the `baseline` property in.
+    // this back to pass the `baseline` property when the ideographic baseline is properly
+    // implemented (https://github.com/flutter/flutter/issues/22625).
     return _textPainter.computeDistanceToActualBaseline(TextBaseline.alphabetic);
   }
 
