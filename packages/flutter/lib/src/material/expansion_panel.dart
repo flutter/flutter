3ac97e8b115661ca517f2436be072da8147800b3
Shi Hao Hong <shihaohong94@gmail.com>
2019-07-02 17:28:57 -0700

    Ignore semantics on expansionIndicator

--- packages/flutter/lib/src/material/expansion_panel.dart
+++ packages/flutter/lib/src/material/expansion_panel.dart
@@ -497,6 +497,7 @@ class _ExpansionPanelListState extends State<ExpansionPanelList> {
               _isChildExpanded(index),
             )
           : IgnorePointer(
+              ignoringSemantics: true,
               child: child.expandIconBuilder(
                 context,
                 _isChildExpanded(index),
