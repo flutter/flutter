567492173aebe4237c0dbbe0f2bd36d2b774c261
Shi Hao Hong <shihaohong94@gmail.com>
2019-07-10 15:58:46 -0700

    Added TODO comments

---
+++ packages/flutter/lib/src/material/tooltip_theme.dart
@@ -0,0 +1,11 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// TODO: Create TooltipThemeData
+
+// TODO: Create TooltipTheme
+
+// TODO: Wire TooltipTheme through ThemeData
+
+// TODO: Wire TooltipTheme through Tooltip widget
