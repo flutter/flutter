b647c3be921d3792c5385615cd40d646fc9d0b4d
Matt Carroll <matthew-carroll@users.noreply.github.com>
2019-07-10 00:34:35 -0700

    Making analyzer happy.

--- dev/integration_tests/android_splash_screens/splash_screen_kitchen_sink/splash_screen_kitchen_sink/android/app/src/main/res/drawable/normal_background.xml
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="utf-8"?>
-<layer-list xmlns:android="http://schemas.android.com/apk/res/android">
-    <item android:drawable="@android:color/white" />
-</layer-list>
--- dev/integration_tests/android_splash_screens/splash_screen_load_rotate/lib/main.dart
+++ dev/integration_tests/android_splash_screens/splash_screen_load_rotate/lib/main.dart
@@ -20,13 +20,13 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+  const MyHomePage({Key key, this.title}) : super(key: key);
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
