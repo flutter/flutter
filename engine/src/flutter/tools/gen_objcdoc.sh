9e6381e31911937a9f95dceb8622cb4aa347868e
Dan Field <dnfield@google.com>
2019-07-10 23:06:40 -0700

    fix objcdoc generation (flutter/engine#9772)

--- engine/src/flutter/tools/gen_objcdoc.sh
+++ engine/src/flutter/tools/gen_objcdoc.sh
@@ -5,9 +5,10 @@
 
 # Generates objc docs for Flutter iOS libraries.
 
-if [[ ! -d "shell/platform/darwin/ios" ]]
+FLUTTER_UMBRELLA_HEADER=$(find ../out -maxdepth 4 -type f -name Flutter.h | grep 'ios_' | head -n 1)
+if [[ ! -f "$FLUTTER_UMBRELLA_HEADER" ]]
   then
-      echo "Error: This script must be run at the root of the Flutter source tree."
+      echo "Error: This script must be run at the root of the Flutter source tree with at least one built Flutter.framework in ../out/ios*/Flutter.framework."
       exit 1
 fi
 
@@ -35,7 +36,36 @@ jazzy \
   --github_url 'https://github.com/flutter'\
   --github-file-prefix 'http://github.com/flutter/engine/blob/master'\
   --module-version 1.0.0\
-  --xcodebuild-arguments --objc,shell/platform/darwin/ios/framework/Headers/Flutter.h,--,-x,objective-c,-isysroot,"$(xcrun --show-sdk-path --sdk iphonesimulator)",-I,"$(pwd)"\
+  --xcodebuild-arguments --objc,"$FLUTTER_UMBRELLA_HEADER",--,-x,objective-c,-isysroot,"$(xcrun --show-sdk-path --sdk iphonesimulator)",-I,"$(pwd)"\
   --module Flutter\
   --root-url https://docs.flutter.io/objc/\
   --output "$1"
+
+EXPECTED_CLASSES="FlutterAppDelegate.html
+FlutterBasicMessageChannel.html
+FlutterCallbackCache.html
+FlutterCallbackInformation.html
+FlutterDartProject.html
+FlutterEngine.html
+FlutterError.html
+FlutterEventChannel.html
+FlutterHeadlessDartRunner.html
+FlutterMethodCall.html
+FlutterMethodChannel.html
+FlutterPluginAppLifeCycleDelegate.html
+FlutterStandardMessageCodec.html
+FlutterStandardMethodCodec.html
+FlutterStandardReader.html
+FlutterStandardReaderWriter.html
+FlutterStandardTypedData.html
+FlutterStandardWriter.html
+FlutterViewController.html"
+
+ACTUAL_CLASSES=$(ls "$1/Classes" | sort)
+
+if [[ $EXPECTED_CLASSES != $ACTUAL_CLASSES ]]; then
+  echo "Expected classes did not match actual classes"
+  echo
+  diff <(echo "$EXPECTED_CLASSES") <(echo "$ACTUAL_CLASSES")
+  exit -1
+fi
