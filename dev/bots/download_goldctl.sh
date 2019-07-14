3813dccd08fcc233ff84e9877afa5089616d8dd9
Kate Lovett <katelovett@google.com>
2019-07-14 13:14:17 -0700

    Cirrus checks

--- dev/bots/download_goldctl.sh
+++ dev/bots/download_goldctl.sh
@@ -6,4 +6,8 @@ git clone --depth 1 https://chromium.googlesource.com/chromium/tools/depot_tools
 cd tmp/depot_tools
 echo -e '# Ensure File\n$ServiceURL https://chrome-infra-packages.appspot.com\n\n# Skia Gold Client goldctl\nskia/tools/goldctl/${platform} latest' > ensure.txt
 ./cipd ensure -ensure-file ensure.txt -root .
-ls
+ls -l
+pwd
+cd ../..
+pwd
+ls -l
