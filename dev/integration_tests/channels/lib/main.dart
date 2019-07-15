30e8c4b4b98ce51952aaf31269723ae51ee97298
Todd Volkert <tvolkert@google.com>
2019-07-15 14:48:53 -0700

    Fixed device lab test

--- dev/integration_tests/channels/lib/main.dart
+++ dev/integration_tests/channels/lib/main.dart
@@ -182,7 +182,7 @@ class _TestAppState extends State<TestApp> {
         ),
         body: Padding(
           padding: const EdgeInsets.all(20.0),
-          child: FutureBuilder<TestStepResult>(
+          child: FutureBuilder<TestStepResult>.withoutInitialData(
             future: _result,
             builder: _buildTestResultWidget,
           ),
