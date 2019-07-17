579a417e057d61ace9e425970aa4192278e12259
Yegor Jbanov <yjbanov@google.com>
2019-07-17 15:48:08 -0700

    add deprecation date

--- packages/flutter/lib/src/rendering/object.dart
+++ packages/flutter/lib/src/rendering/object.dart
@@ -1931,6 +1931,7 @@ abstract class RenderObject extends AbstractNode with DiagnosticableTreeMixin im
   /// is dirty.
   ///
   /// For production code, consider [layer].
+  // Deprecated as of July 17, 2019.
   @Deprecated('use the "layer" getter instead.')
   ContainerLayer get debugLayer {
     ContainerLayer result;
