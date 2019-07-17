11fad1ac9d1df8dafe17c2f39cfc2c1b26470889
Dan Field <dfield@gmail.com>
2019-07-17 15:53:33 -0700

    Update slivers_appbar_floating_pinned_test.dart

--- packages/flutter/test/widgets/slivers_appbar_floating_pinned_test.dart
+++ packages/flutter/test/widgets/slivers_appbar_floating_pinned_test.dart
@@ -6,7 +6,6 @@ import 'package:flutter/rendering.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter/material.dart';
 
-
 import 'semantics_tester.dart';
 
 void main() {
