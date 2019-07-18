a23e079f9d202de94b66d59599425fe44a086f40
Liam Appelbe <liama@google.com>
2019-07-18 09:28:41 -0700

    simarm_x64 in flutter

--- packages/flutter_tools/lib/src/base/build.dart
+++ packages/flutter_tools/lib/src/base/build.dart
@@ -60,10 +60,10 @@ class GenSnapshot {
     // generate armv7 code. Running as an x86_64 binary will generate arm64
     // code. /usr/bin/arch can be used to run binaries with the specified
     // architecture.
-    if (snapshotType.platform == TargetPlatform.ios) {
-      final String hostArch = iosArch == IOSArch.armv7 ? '-i386' : '-x86_64';
-      return runCommandAndStreamOutput(<String>['/usr/bin/arch', hostArch, snapshotterPath, ...args]);
-    }
+    // if (snapshotType.platform == TargetPlatform.ios) {
+    //   final String hostArch = iosArch == IOSArch.armv7 ? '-i386' : '-x86_64';
+    //   return runCommandAndStreamOutput(<String>['/usr/bin/arch', hostArch, snapshotterPath, ...args]);
+    // }
 
     StringConverter outputFilter;
     if (additionalArgs.contains('--strip')) {
