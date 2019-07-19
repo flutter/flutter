80e73ce22cbdb026c79fc664ae47e81f0219c0c6
Aaron Clarke <aaclarke@google.com>
2019-07-19 15:23:53 -0700

    Tweaked whitespace again

--- packages/flutter/lib/src/painting/shader_warm_up.dart
+++ packages/flutter/lib/src/painting/shader_warm_up.dart
@@ -57,7 +57,7 @@ abstract class ShaderWarmUp {
   /// The size of the warm up image.
   ///
   /// The exact size shouldn't matter much as long as all draws are onscreen.
-  /// 100x100 is an arbitrary small size that's easy to fit significant draw 
+  /// 100x100 is an arbitrary small size that's easy to fit significant draw
   /// calls onto.
   ///
   /// A custom shader warm up can override this based on targeted devices.
