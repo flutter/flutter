fc5324fe9e1a3e75c2ec551fb9aa1079f8055a25
Collin Jackson <jackson@google.com>
2019-07-18 17:16:51 -0700

    only one exit 0

--- dev/bots/firebase_testlab.sh
+++ dev/bots/firebase_testlab.sh
@@ -37,6 +37,6 @@ gcloud firebase test android run --type robo \
 # Check logcat for "E/flutter" - if it's there, something's wrong.
 gsutil cp gs://flutter_firebase_testlab/release_smoke_test/$GIT_REVISION/$CIRRUS_BUILD_ID/walleye-26-en-portrait/logcat /tmp/logcat
 ! grep "E/flutter" /tmp/logcat || false
-grep "I/flutter" /tmp/logcat || exit 0
+grep "I/flutter" /tmp/logcat
 
 popd
