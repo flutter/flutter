8010f4a06a008e8970a7e5ec6ba32eef9293c4fc
Chris Yang <ychris@google.com>
2019-07-18 18:22:06 -0700

    add a test for nest clipping, scrolling and platform view

--- dev/integration_tests/android_views/lib/main.dart
+++ dev/integration_tests/android_views/lib/main.dart
@@ -2,12 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_driver/driver_extension.dart';
 import 'motion_events_page.dart';
 import 'mutation_page.dart';
-
 import 'page.dart';
+import 'scroll_vew_nested_platform_view.dart';
 
 final List<Page> _allPages = <Page>[
   const MotionEventsPage(),
   const MutationCompositionPage(),
+  const ScrollViewNestedPlatformView(),
 ];
 
 void main() {
---
+++ dev/integration_tests/android_views/lib/scroll_vew_nested_platform_view.dart
@@ -0,0 +1,45 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+import 'page.dart';
+import 'simple_platform_view.dart';
+
+class ScrollViewNestedPlatformView extends Page {
+  const ScrollViewNestedPlatformView()
+      : super('ScrollViewNestedPlatformView Tests',
+            const ValueKey<String>('ScrollViewNestedPlatformViewListTile'));
+
+  @override
+  Widget build(BuildContext context) {
+    return ScrollViewNestedPlatformViewBody();
+  }
+}
+
+
+class ScrollViewNestedPlatformViewBody extends StatefulWidget {
+
+  const ScrollViewNestedPlatformViewBody():super(key: const ValueKey<String>('ScrollViewNestedPlatformView'));
+
+  @override
+  State createState() => ScrollViewNestedPlatformViewBodyState();
+}
+
+class ScrollViewNestedPlatformViewBodyState extends State<ScrollViewNestedPlatformViewBody> {
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+          borderRadius: const BorderRadius.only(
+            topLeft: Radius.circular(10.0),
+            topRight: Radius.circular(10.0),
+          ),
+          child: SingleChildScrollView(
+            child: ConstrainedBox(
+              constraints: const BoxConstraints(maxHeight: 100.0),
+              child: const SimplePlatformView(),
+            ),
+          ),
+      );
+  }
+}
