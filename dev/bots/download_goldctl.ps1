c23396964b08ee3f4dd075e7cf1adc431797adb4
Kate Lovett <katelovett@google.com>
2019-07-19 14:53:05 -0700

    Updating windows script

--- dev/bots/download_goldctl.ps1
+++ dev/bots/download_goldctl.ps1
@@ -1,5 +1,7 @@
-$url= "https://chrome-infra-packages.appspot.com/p/skia/tools/goldctl/windows-amd64/+/"
-$path = "c:\Windows\Temp\flutter sdk\goldctl.zip"
+$url= "https://storage.googleapis.com/chrome-infra/depot_tools.zip"
+#"https://chrome-infra-packages.appspot.com/p/skia/tools/goldctl/windows-amd64/+/"
+$path = "c:\Windows\Temp\flutter sdk\depot_tools.zip"
+#"c:\Windows\Temp\flutter sdk\goldctl.zip"
 
 (New-Object System.Net.WebClient).DownloadFile($url, $path)
-Expand-Archive -LiteralPath $path -DestinationPath "C:\Windows\Temp\goldctl_tool"
+Expand-Archive -LiteralPath $path -DestinationPath "C:\Windows\Temp\depot_tools"
