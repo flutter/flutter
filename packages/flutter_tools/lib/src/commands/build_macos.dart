c0f9f7851e8d15dfaaef23e8c0df561639ee638c
Jonah Williams <jonahwilliams@google.com>
2019-07-21 18:21:15 -0700

    Use FlutterFeatures to configure web and desktop devices (#36465)

--- packages/flutter_tools/lib/src/commands/build_macos.dart
+++ packages/flutter_tools/lib/src/commands/build_macos.dart
@@ -8,6 +8,7 @@ import '../base/common.dart';
 import '../base/platform.dart';
 import '../build_info.dart';
 import '../cache.dart';
+import '../features.dart';
 import '../macos/build_macos.dart';
 import '../project.dart';
 import '../runner/flutter_command.dart' show FlutterCommandResult;
@@ -35,9 +36,6 @@ class BuildMacosCommand extends BuildSubCommand {
   final String name = 'macos';
 
   @override
-  bool isExperimental = true;
-
-  @override
   bool hidden = true;
 
   @override
@@ -47,13 +45,16 @@ class BuildMacosCommand extends BuildSubCommand {
   };
 
   @override
-  String get description => 'build the macOS desktop target (Experimental).';
+  String get description => 'build the macOS desktop target.';
 
   @override
   Future<FlutterCommandResult> runCommand() async {
     Cache.releaseLockEarly();
     final BuildInfo buildInfo = getBuildInfo();
     final FlutterProject flutterProject = FlutterProject.current();
+    if (!featureFlags.isMacOSEnabled) {
+      throwToolExit('"build macos" is not currently supported.');
+    }
     if (!platform.isMacOS) {
       throwToolExit('"build macos" only supported on macOS hosts.');
     }
