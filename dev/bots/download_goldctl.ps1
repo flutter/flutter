bfdb88ecdb2c2bd940d31f9e1f46993d6703abba
Kate Lovett <katelovett@google.com>
2019-07-23 09:49:03 -0700

    Adding shared ensure file

--- dev/bots/download_goldctl.ps1
+++ dev/bots/download_goldctl.ps1
@@ -4,7 +4,9 @@ $path = "C:\Windows\Temp\flutter sdk\depot_tools.zip"
 #"c:\Windows\Temp\flutter sdk\goldctl.zip"
 
 (New-Object System.Net.WebClient).DownloadFile($url, $path)
-Expand-Archive -LiteralPath $path -DestinationPath "C:\Windows\Temp\depot_tools"
+Expand-Archive -LiteralPath $path -DestinationPath "C:\Windows\Temp\flutter sdk\depot_tools"
 Get-ChildItem
-cd C:\Windows\Temp\depot_tools
+cd C:\Windows\Temp\flutter sdk\depot_tools
+Get-ChildItem
+cmd.exe /C "C:\Windows\Temp\flutter sdk\depot_tools\cipd.bat ensure -ensure-file C:\Windows\Temp\flutter sdk\dev\bots\ensure_goldctl.txt -root ."
 Get-ChildItem
