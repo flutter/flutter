a350d60677b4b4f830bb4a1ba3e96cc4415c0ea3
Kate Lovett <katelovett@google.com>
2019-07-23 14:02:08 -0700

    ++

--- dev/bots/download_goldctl.ps1
+++ dev/bots/download_goldctl.ps1
@@ -1,8 +1,8 @@
 $url= "https://storage.googleapis.com/chrome-infra/depot_tools.zip"
 $zipPath = "C:\Windows\Temp\flutter sdk\depot_tools.zip"
 $path = "C:\Windows\Temp\flutter sdk\depot_tools"
-$cipd = "C:\Windows\Temp\flutter sdk\depot_tools\cipd.bat"
-$ensureFile = "C:\Windows\Temp\flutter sdk\dev\bots\ensure_goldctl.txt"
+$cipd = "C:\Windows\Temp\flutter^ sdk\depot_tools\cipd.bat"
+$ensureFile = "C:\Windows\Temp\flutter^ sdk\dev\bots\ensure_goldctl.txt"
 
 
 (New-Object System.Net.WebClient).DownloadFile($url, $zipPath)
