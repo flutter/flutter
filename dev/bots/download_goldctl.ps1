2ef3b8c257a6aa3438a77983807df52be9c73087
Kate Lovett <katelovett@google.com>
2019-07-23 08:59:44 -0700

    windows script

--- dev/bots/download_goldctl.ps1
+++ dev/bots/download_goldctl.ps1
@@ -1,7 +1,10 @@
 $url= "https://storage.googleapis.com/chrome-infra/depot_tools.zip"
 #"https://chrome-infra-packages.appspot.com/p/skia/tools/goldctl/windows-amd64/+/"
-$path = "c:\Windows\Temp\flutter sdk\depot_tools.zip"
+$path = "C:\Windows\Temp\flutter sdk\depot_tools.zip"
 #"c:\Windows\Temp\flutter sdk\goldctl.zip"
 
 (New-Object System.Net.WebClient).DownloadFile($url, $path)
 Expand-Archive -LiteralPath $path -DestinationPath "C:\Windows\Temp\depot_tools"
+Get-ChildItem
+cd C:\Windows\Temp\depot_tools
+Get-ChildItem
