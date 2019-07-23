4d4c7a509274b5a9027bf6eb64019473645c9f40
Jia Hao Goh <jiahaog@users.noreply.github.com>
2019-07-23 10:40:25 -0700

    Parameterize CoverageCollector with a library name predicate
    An optimization to the coverage collection speed was added in #30811. This commit further expands on it to parameterize the CoverageCollector with a custom predicate, allowing internal use cases to filter the RPC calls to the Dart VM based on scripts of interest to coverage collection.

--- packages/flutter_tools/lib/src/commands/test.dart
+++ packages/flutter_tools/lib/src/commands/test.dart
@@ -190,8 +190,9 @@ class TestCommand extends FastFlutterCommand {
 
     CoverageCollector collector;
     if (argResults['coverage'] || argResults['merge-coverage']) {
+      final String projectName = FlutterProject.current().manifest.appName;
       collector = CoverageCollector(
-        flutterProject: FlutterProject.current(),
+        libraryPredicate: (String libraryName) => libraryName.contains(projectName),
       );
     }
 
