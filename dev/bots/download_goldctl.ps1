0d2f58af444e13927396fa235d3e864b56d8c931
Kate Lovett <katelovett@google.com>
2019-07-24 10:46:03 -0700

    ++

--- dev/bots/download_goldctl.ps1
+++ dev/bots/download_goldctl.ps1
@@ -1,15 +1,15 @@
 $url= "https://storage.googleapis.com/chrome-infra/depot_tools.zip"
 $zipPath = "C:\Windows\Temp\depot_tools.zip"
 $path = "C:\Windows\Temp\depot_tools"
-$gclient = "C:\Windows\Temp\depot_tools\gclient.bat"
-$cipd = "C:\Windows\Temp\depot_tools\cipd.bat"
-$ensureFile = "C:\Windows\Temp\depot_tools\ensure.txt"
-$text = "# Ensure File\n$ServiceURL https://chrome-infra-packages.appspot.com\n\n# Skia Gold Client goldctl\nskia/tools/goldctl/${platform} latest"
+$gclient = "$path\gclient.bat"
+$cipd = "$path\cipd.bat"
+$ensureFile = "$path\ensure.txt"
+$text = "# Ensure File\n`$ServiceURL https://chrome-infra-packages.appspot.com\n\n# Skia Gold Client goldctl\nskia/tools/goldctl/`${platform} latest"
 
 # Check version for changing the the default encoding
 Write-Output "PS Version"
 $PSVersionTable.PSVersion
-$PSDefaultParameterValues['Out-File:Encoding'] = 'utf8'
+$PSDefaultParameterValues['Out-File:Encoding'] = 'ascii'
 
 (New-Object System.Net.WebClient).DownloadFile($url, $zipPath)
 Expand-Archive -LiteralPath $zipPath -DestinationPath $path
