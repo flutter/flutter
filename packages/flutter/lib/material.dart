0a2fece0f89270c60378b354874de1fb135877bc
Shi-Hao Hong <shihaohong@google.com>
2019-07-24 08:24:09 -0700

    [Material] Implement TooltipTheme and Tooltip.textStyle, fix Tooltip debugLabel, update Tooltip defaults (#36030)
    * Implement TooltipThemeData and TooltipTheme
    
    * Add tooltip text style property
    
    * Update default tooltip text style and decoration to match Material specification
    
    * Fix debugLabel incorrectly labeling "white" as "black", and vice versa by default

--- packages/flutter/lib/material.dart
+++ packages/flutter/lib/material.dart
@@ -116,6 +116,7 @@ export 'src/material/time.dart';
 export 'src/material/time_picker.dart';
 export 'src/material/toggleable.dart';
 export 'src/material/tooltip.dart';
+export 'src/material/tooltip_theme.dart';
 export 'src/material/typography.dart';
 export 'src/material/user_accounts_drawer_header.dart';
 export 'widgets.dart';
