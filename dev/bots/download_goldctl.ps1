6e84de3c35baa45e261275f34c3744df6e09e3ad
Kate Lovett <katelovett@google.com>
2019-07-24 09:50:00 -0700

    CIPD failing on windows.

--- dev/bots/download_goldctl.ps1
+++ dev/bots/download_goldctl.ps1
@@ -1,17 +1,21 @@
 $url= "https://storage.googleapis.com/chrome-infra/depot_tools.zip"
 $zipPath = "C:\Windows\Temp\depot_tools.zip"
 $path = "C:\Windows\Temp\depot_tools"
+$gclient = "C:\Windows\Temp\depot_tools\gclient.bat"
 $cipd = "C:\Windows\Temp\depot_tools\cipd.bat"
 $ensureFile = "C:\Windows\Temp\depot_tools\ensure.txt"
 $text = "# Ensure File\n$ServiceURL https://chrome-infra-packages.appspot.com\n\n# Skia Gold Client goldctl\nskia/tools/goldctl/${platform} latest"
 
 (New-Object System.Net.WebClient).DownloadFile($url, $zipPath)
 Expand-Archive -LiteralPath $zipPath -DestinationPath $path
-Get-ChildItem
+#Get-ChildItem
 cd $path
-Get-ChildItem
+Write-Output "Running gclient"
+cmd.exe /C "$gclient"
+#Get-ChildItem
 #Out-File $text -FilePath $ensureFile
 $text > $ensureFile
-Get-ChildItem
+#Get-ChildItem
+Write-Output "Running cipd"
 cmd.exe /C "$cipd ensure -ensure-file $ensureFile -root $path"
 Get-ChildItem
