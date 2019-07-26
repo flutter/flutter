addbc5d4cbe0cefec3226a22cadaf4d999b6ccbd
Jonah Williams <jonahwilliams@google.com>
2019-07-26 12:56:08 -0700

    add suggested changes from flutter roll (flutter/engine#10176)

--- engine/src/flutter/shell/platform/android/io/flutter/embedding/android/DrawableSplashScreen.java
+++ engine/src/flutter/shell/platform/android/io/flutter/embedding/android/DrawableSplashScreen.java
@@ -12,7 +12,6 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
-import android.widget.FrameLayout;
 import android.widget.ImageView;
 
 /**
