40fa6fb0faaa285e3f80231423ab3a8dcfcc2175
Emmanuel Garcia <blasten@gmail.com>
2019-07-29 16:52:58 -0700

    Test that modules built as AAR contain the right assets and artifacts

--- dev/devicelab/bin/tasks/build_aar_module_test.dart
+++ dev/devicelab/bin/tasks/build_aar_module_test.dart
@@ -5,6 +5,7 @@
 import 'dart:async';
 import 'dart:io';
 
+import 'package:flutter_devicelab/framework/apk_utils.dart';
 import 'package:flutter_devicelab/framework/framework.dart';
 import 'package:flutter_devicelab/framework/utils.dart';
 import 'package:path/path.dart' as path;
@@ -134,6 +135,33 @@ Future<void> main() async {
         'package_info_release-1.0.pom',
       ));
 
+      section('Check assets in release AAR');
+
+      final Iterable<String> releaseAar = await getFilesInAar(path.join(
+        repoPath,
+        'io',
+        'flutter',
+        'devicelab',
+        'hello',
+        'flutter_release',
+        '1.0',
+        'flutter_release-1.0.aar',
+      ));
+
+      checkItContains<String>(<String>[
+        'assets/flutter_assets/FontManifest.json',
+        'assets/flutter_assets/packages/cupertino_icons/assets/CupertinoIcons.ttf',
+      ], releaseAar);
+
+      section('Check AOT blobs in release AAR');
+
+      checkItContains<String>(<String>[
+        'jni/arm64-v8a/libapp.so',
+        'jni/arm64-v8a/libflutter.so',
+        'jni/armeabi-v7a/libapp.so',
+        'jni/armeabi-v7a/libflutter.so',
+      ], releaseAar);
+
       section('Build debug AAR');
 
       await inDirectory(projectDir, () async {
@@ -149,9 +177,9 @@ Future<void> main() async {
         'flutter',
         'devicelab',
         'hello',
-        'flutter_release',
+        'flutter_debug',
         '1.0',
-        'flutter_release-1.0.aar',
+        'flutter_debug-1.0.aar',
       ));
 
       checkFileExists(path.join(
@@ -209,7 +237,41 @@ Future<void> main() async {
         'package_info_debug-1.0.pom',
       ));
 
+
+      section('Check assets in debug AAR');
+
+      final Iterable<String> debugAar = await getFilesInAar(path.join(
+        repoPath,
+        'io',
+        'flutter',
+        'devicelab',
+        'hello',
+        'flutter_debug',
+        '1.0',
+        'flutter_debug-1.0.aar',
+      ));
+
+      checkItContains<String>(<String>[
+        'assets/flutter_assets/FontManifest.json',
+        'assets/flutter_assets/packages/cupertino_icons/assets/CupertinoIcons.ttf',
+        // JIT snapshots.
+        'assets/flutter_assets/isolate_snapshot_data',
+        'assets/flutter_assets/kernel_blob.bin',
+        'assets/flutter_assets/vm_snapshot_data',
+      ], debugAar);
+
+      section('Check AOT blobs in debug AAR');
+
+      checkItContains<String>(<String>[
+        'jni/arm64-v8a/libflutter.so',
+        'jni/armeabi-v7a/libflutter.so',
+        'jni/x86/libflutter.so',
+        'jni/x86_64/libflutter.so',
+      ], debugAar);
+
       return TaskResult.success(null);
+    } on TaskResult catch (taskResult) {
+      return taskResult;
     } catch (e) {
       return TaskResult.failure(e.toString());
     } finally {
