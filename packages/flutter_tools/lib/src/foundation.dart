28f05191bc7a766efbc5cadc63dadb21e32afd0b
jonahwilliams <jonahwilliams@google.com>
2019-08-07 10:58:14 -0700

    re-export foundation through tool

---
+++ packages/flutter_tools/lib/src/foundation.dart
@@ -0,0 +1,6 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Re-export diagnostics node for formatting.
+export '../../../flutter/lib/src/foundation/diagnostics.dart'; // ignore: uri_does_not_exist
