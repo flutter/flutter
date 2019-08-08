51904fc4bda988cb0719fcf19efada85d844fc86
Matt Carroll <matthew-carroll@users.noreply.github.com>
2019-08-08 16:22:45 -0700

    Forwards iOS dark mode trait to the Flutter framework (#34441). (flutter/engine#9722)

--- engine/src/flutter/shell/platform/darwin/ios/framework/Source/FlutterViewControllerTest.m
+++ engine/src/flutter/shell/platform/darwin/ios/framework/Source/FlutterViewControllerTest.m
@@ -6,6 +6,8 @@
 #import <XCTest/XCTest.h>
 #import "flutter/shell/platform/darwin/ios/framework/Headers/FlutterViewController.h"
 
+#include "FlutterBinaryMessenger.h"
+
 @interface FlutterViewControllerTest : XCTestCase
 @end
 
@@ -21,6 +23,87 @@
   OCMStub([engine binaryMessenger]).andReturn(messenger);
   XCTAssertEqual(vc.binaryMessenger, messenger);
   OCMVerify([engine binaryMessenger]);
+}
+
+- (void)testItReportsLightPlatformBrightnessByDefault {
+  // Setup test.
+  id engine = OCMClassMock([FlutterEngine class]);
+
+  id settingsChannel = OCMClassMock([FlutterBasicMessageChannel class]);
+  OCMStub([engine settingsChannel]).andReturn(settingsChannel);
+
+  FlutterViewController* vc = [[FlutterViewController alloc] initWithEngine:engine
+                                                                    nibName:nil
+                                                                     bundle:nil];
+
+  // Exercise behavior under test.
+  [vc traitCollectionDidChange:nil];
+
+  // Verify behavior.
+  OCMVerify([settingsChannel sendMessage:[OCMArg checkWithBlock:^BOOL(id message) {
+                               return [message[@"platformBrightness"] isEqualToString:@"light"];
+                             }]]);
+}
+
+- (void)testItReportsDarkPlatformBrightnessWhenTraitCollectionRequestsIt {
+  // Setup test.
+  id engine = OCMClassMock([FlutterEngine class]);
+
+  id settingsChannel = OCMClassMock([FlutterBasicMessageChannel class]);
+  OCMStub([engine settingsChannel]).andReturn(settingsChannel);
+
+  FlutterViewController* vc = [[FlutterViewController alloc] initWithEngine:engine
+                                                                    nibName:nil
+                                                                     bundle:nil];
+  id mockTraitCollection = [self setupFakeUserInterfaceStyle:UIUserInterfaceStyleDark];
+
+  // Exercise behavior under test.
+  [vc traitCollectionDidChange:nil];
+
+  // Verify behavior.
+  OCMVerify([settingsChannel sendMessage:[OCMArg checkWithBlock:^BOOL(id message) {
+                               return [message[@"platformBrightness"] isEqualToString:@"light"];
+                             }]]);
+
+  // Restore UIUserInterfaceStyle
+  [mockTraitCollection stopMocking];
+}
+
+- (void)testItReportsPlatformBrightnessWhenExpected {
+  // Setup test.
+  id engine = OCMClassMock([FlutterEngine class]);
+
+  id settingsChannel = OCMClassMock([FlutterBasicMessageChannel class]);
+  OCMStub([engine settingsChannel]).andReturn(settingsChannel);
+
+  FlutterViewController* vc = [[FlutterViewController alloc] initWithEngine:engine
+                                                                    nibName:nil
+                                                                     bundle:nil];
+  id mockTraitCollection = [self setupFakeUserInterfaceStyle:UIUserInterfaceStyleDark];
+
+  __block int messageCount = 0;
+  OCMStub([settingsChannel sendMessage:[OCMArg any]]).andDo(^(NSInvocation* invocation) {
+    messageCount = messageCount + 1;
+  });
+
+  // Exercise behavior under test.
+  [vc updateViewConstraints];
+  [vc viewWillLayoutSubviews];
+  [vc traitCollectionDidChange:nil];
+  [vc onUserSettingsChanged:nil];
+
+  // Verify behavior.
+  XCTAssertEqual(messageCount, 4);
+
+  // Restore UIUserInterfaceStyle
+  [mockTraitCollection stopMocking];
+}
+
+- (UITraitCollection*)setupFakeUserInterfaceStyle:(UIUserInterfaceStyle)style {
+  id mockTraitCollection = OCMClassMock([UITraitCollection class]);
+  OCMStub([mockTraitCollection userInterfaceStyle]).andReturn(UIUserInterfaceStyleDark);
+  OCMStub([mockTraitCollection currentTraitCollection]).andReturn(mockTraitCollection);
+  return mockTraitCollection;
 }
 
 @end
