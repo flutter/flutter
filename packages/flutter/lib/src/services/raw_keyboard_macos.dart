8674d5e2f61dde485962f24791c2c611c534bdf0
Francisco Magdaleno <franciscojma86@gmail.com>
2019-08-09 09:07:18 -0700

    Update packages/flutter/lib/src/services/raw_keyboard_macos.dart
    Co-Authored-By: Greg Spencer <gspencergoog@users.noreply.github.com>

--- packages/flutter/lib/src/services/raw_keyboard_macos.dart
+++ packages/flutter/lib/src/services/raw_keyboard_macos.dart
@@ -100,7 +100,7 @@ class RawKeyEventDataMacOs extends RawKeyEventData {
     }
 
     // Control keys like "backspace" and movement keys like arrow keys won't have a character,
-    // but it's known by the physical keyboard. Since there is no logical keycode map for macOS
+    // but are present on the physical keyboard. Since there is no logical keycode map for macOS
     // (macOS uses the keycode to reference physical keys), a LogicalKeyboardKey is created with
     // the physical key's HID usage and debugName. This avoids the need for duplicating the physical
     // key map.
