9ae65461a0d3b403a0457f4db7804a8412981efc
stuartmorgan <stuartmorgan@google.com>
2019-08-12 15:42:32 -0700

    Increase the license block scan from 5k to 6k (flutter/engine#10956)
    Split out of #10782 since it affects the detection of the license blocks of some files in third_party/boringssl.

--- engine/src/flutter/tools/licenses/lib/limits.dart
+++ engine/src/flutter/tools/licenses/lib/limits.dart
@@ -2,5 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// TODO(ianh): vastly increase this before checkin
-const int kMaxSize = 5 * 1024; // only look for copyrights and licenses at the top of the file
+// Only look for copyrights and licenses at the top of the file.
+// This can be increased as necessary, but the larger the value, the slower the
+// script will be, so be judicious.
+const int kMaxSize = 6 * 1024;
