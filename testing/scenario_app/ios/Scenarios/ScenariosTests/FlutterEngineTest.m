e22893966a24c2a3facf133c158437b40533297d
Dan Field <dnfield@google.com>
2019-08-12 21:20:59 -0700

    Expose isolateId for engine (#10823)

---
+++ testing/scenario_app/ios/Scenarios/ScenariosTests/FlutterEngineTest.m
@@ -0,0 +1,31 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import <Flutter/Flutter.h>
+#import <XCTest/XCTest.h>
+#import "AppDelegate.h"
+
+@interface FlutterEngineTest : XCTestCase
+@end
+
+@implementation FlutterEngineTest
+
+- (void)testIsolateId {
+  FlutterEngine* engine = [[FlutterEngine alloc] initWithName:@"test" project:nil];
+  XCTAssertNil(engine.isolateId);
+  [self keyValueObservingExpectationForObject:engine keyPath:@"isolateId" handler:nil];
+
+  XCTAssertTrue([engine runWithEntrypoint:nil]);
+
+  [self waitForExpectationsWithTimeout:30.0 handler:nil];
+
+  XCTAssertNotNil(engine.isolateId);
+  XCTAssertTrue([engine.isolateId hasPrefix:@"isolates/"]);
+
+  [engine destroyContext];
+
+  XCTAssertNil(engine.isolateId);
+}
+
+@end
