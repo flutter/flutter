52c4e01e50ade28368440ec239b0a37706d52534
jonahwilliams <jonahwilliams@google.com>
2019-08-13 14:36:42 -0700

    add without test cases

--- packages/flutter_tools/test/general.shard/base/zone_check_test.dart
+++ packages/flutter_tools/test/general.shard/base/zone_check_test.dart
@@ -9,6 +9,67 @@ import 'package:flutter_tools/src/base/zone_check.dart';
 import '../../src/common.dart';
 
 void main() {
+  test('sync Completer.completeError does throw error into zone', () async {
+    bool caughtByZone = false;
+    bool caughtByHandler = false;
+    final Zone zone = Zone.current.fork(specification: ZoneSpecification(
+      handleUncaughtError: (
+          Zone self,
+          ZoneDelegate parent,
+          Zone zone,
+          Object error,
+          StackTrace stackTrace,
+          ) {
+        caughtByZone = true;
+      },
+    ));
+
+    await zone.run(() async {
+      try {
+        await () async {
+          final Completer<void> completer = Completer<void>.sync();
+          completer.completeError(Exception());
+          return completer.future;
+        }();
+      } on Exception {
+        caughtByHandler = true;
+      }
+    });
+    // after hitting all zone handlers, backtracks through regular exception
+    // handling.
+    expect(caughtByZone, true);
+    expect(caughtByHandler, true);
+  });
+
+  test('async method does not throw exception into zone', () async {
+    bool caughtByZone = false;
+    bool caughtByHandler = false;
+    final Zone zone = Zone.current.fork(specification: ZoneSpecification(
+      handleUncaughtError: (
+          Zone self,
+          ZoneDelegate parent,
+          Zone zone,
+          Object error,
+          StackTrace stackTrace,
+          ) {
+        caughtByZone = true;
+      },
+    ));
+
+    await zone.run(() async {
+      try {
+        await () async {
+          await null;
+          throw Exception();
+        }();
+      } on Exception {
+        caughtByHandler = true;
+      }
+    });
+    expect(caughtByZone, false);
+    expect(caughtByHandler, true);
+  });
+
   test('sync Completer.completeError does not throw error into zone runZoneChecked and try/catch', () async {
     bool caughtByZone = false;
     bool caughtByHandler = false;
@@ -58,6 +119,7 @@ void main() {
     await zone.run(() async {
       try {
         await runZoneChecked(() async {
+          await null;
           throw Exception();
         });
       } on Exception {
