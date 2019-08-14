4f00cb745c2895feb47985725f8ada94df72ce36
Dan Field <dnfield@google.com>
2019-08-14 12:43:33 -0700

    Add script for running ios Tests on simulator (#10999)

---
+++ testing/scenario_app/run_ios_tests.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+set -e
+
+FLUTTER_ENGINE=ios_debug_sim_unopt
+
+if [ $# -eq 1 ]; then
+  FLUTTER_ENGINE=$1
+fi
+
+PRETTY="cat"
+if which xcpretty; then
+  PRETTY="xcpretty"
+fi
+
+cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd
+
+./compile_ios_jit.sh ../../../out/host_debug_unopt ../../../out/$FLUTTER_ENGINE/clang_x64
+
+pushd ios/Scenarios
+
+set -o pipefail && xcodebuild -sdk iphonesimulator \
+  -scheme ScenariosTests \
+  -destination 'platform=iOS Simulator,name=iPhone SE,OS=12.2' \
+  test \
+  FLUTTER_ENGINE=$FLUTTER_ENGINE | $PRETTY
+
+popd
