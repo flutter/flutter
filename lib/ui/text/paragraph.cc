33d168f99fd0e9c3b2cb777e11294ac8049b73a9
Jason Simmons <jason-simmons@users.noreply.github.com>
2019-08-15 15:23:00 -0700

    Remove the ParagraphImpl class from the text API (#11012)
    ParagraphImpl was used to switch between libtxt and Blink implementations
    of text rendering and is now obsolete.

--- lib/ui/text/paragraph.cc
+++ lib/ui/text/paragraph.cc
@@ -38,8 +38,7 @@ IMPLEMENT_WRAPPERTYPEINFO(ui, Paragraph);
 DART_BIND_ALL(Paragraph, FOR_EACH_BINDING)
 
 Paragraph::Paragraph(std::unique_ptr<txt::Paragraph> paragraph)
-    : m_paragraphImpl(
-          std::make_unique<ParagraphImplTxt>(std::move(paragraph))) {}
+    : m_paragraph(std::move(paragraph)) {}
 
 Paragraph::~Paragraph() = default;
 
@@ -51,64 +50,87 @@ size_t Paragraph::GetAllocationSize() {
 }
 
 double Paragraph::width() {
-  return m_paragraphImpl->width();
+  return m_paragraph->GetMaxWidth();
 }
 
 double Paragraph::height() {
-  return m_paragraphImpl->height();
+  return m_paragraph->GetHeight();
 }
 
 double Paragraph::longestLine() {
-  return m_paragraphImpl->longestLine();
+  return m_paragraph->GetLongestLine();
 }
 
 double Paragraph::minIntrinsicWidth() {
-  return m_paragraphImpl->minIntrinsicWidth();
+  return m_paragraph->GetMinIntrinsicWidth();
 }
 
 double Paragraph::maxIntrinsicWidth() {
-  return m_paragraphImpl->maxIntrinsicWidth();
+  return m_paragraph->GetMaxIntrinsicWidth();
 }
 
 double Paragraph::alphabeticBaseline() {
-  return m_paragraphImpl->alphabeticBaseline();
+  return m_paragraph->GetAlphabeticBaseline();
 }
 
 double Paragraph::ideographicBaseline() {
-  return m_paragraphImpl->ideographicBaseline();
+  return m_paragraph->GetIdeographicBaseline();
 }
 
 bool Paragraph::didExceedMaxLines() {
-  return m_paragraphImpl->didExceedMaxLines();
+  return m_paragraph->DidExceedMaxLines();
 }
 
 void Paragraph::layout(double width) {
-  m_paragraphImpl->layout(width);
+  m_paragraph->Layout(width);
 }
 
 void Paragraph::paint(Canvas* canvas, double x, double y) {
-  m_paragraphImpl->paint(canvas, x, y);
+  SkCanvas* sk_canvas = canvas->canvas();
+  if (!sk_canvas)
+    return;
+  m_paragraph->Paint(sk_canvas, x, y);
 }
 
 std::vector<TextBox> Paragraph::getRectsForRange(unsigned start,
                                                  unsigned end,
                                                  unsigned boxHeightStyle,
                                                  unsigned boxWidthStyle) {
-  return m_paragraphImpl->getRectsForRange(
+  std::vector<TextBox> result;
+  std::vector<txt::Paragraph::TextBox> boxes = m_paragraph->GetRectsForRange(
       start, end, static_cast<txt::Paragraph::RectHeightStyle>(boxHeightStyle),
       static_cast<txt::Paragraph::RectWidthStyle>(boxWidthStyle));
+  for (const txt::Paragraph::TextBox& box : boxes) {
+    result.emplace_back(box.rect, static_cast<TextDirection>(box.direction));
+  }
+  return result;
 }
 
 std::vector<TextBox> Paragraph::getRectsForPlaceholders() {
-  return m_paragraphImpl->getRectsForPlaceholders();
+  std::vector<TextBox> result;
+  std::vector<txt::Paragraph::TextBox> boxes =
+      m_paragraph->GetRectsForPlaceholders();
+  for (const txt::Paragraph::TextBox& box : boxes) {
+    result.emplace_back(box.rect, static_cast<TextDirection>(box.direction));
+  }
+  return result;
 }
 
 Dart_Handle Paragraph::getPositionForOffset(double dx, double dy) {
-  return m_paragraphImpl->getPositionForOffset(dx, dy);
+  Dart_Handle result = Dart_NewListOf(Dart_CoreType_Int, 2);
+  txt::Paragraph::PositionWithAffinity pos =
+      m_paragraph->GetGlyphPositionAtCoordinate(dx, dy);
+  Dart_ListSetAt(result, 0, ToDart(pos.position));
+  Dart_ListSetAt(result, 1, ToDart(static_cast<int>(pos.affinity)));
+  return result;
 }
 
 Dart_Handle Paragraph::getWordBoundary(unsigned offset) {
-  return m_paragraphImpl->getWordBoundary(offset);
+  txt::Paragraph::Range<size_t> point = m_paragraph->GetWordBoundary(offset);
+  Dart_Handle result = Dart_NewListOf(Dart_CoreType_Int, 2);
+  Dart_ListSetAt(result, 0, ToDart(point.start));
+  Dart_ListSetAt(result, 1, ToDart(point.end));
+  return result;
 }
 
 }  // namespace flutter
