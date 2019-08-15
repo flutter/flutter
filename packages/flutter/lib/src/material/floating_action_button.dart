6de720338d8a3d365e64a6dabc334c9d95766909
MH Johnson <johnsonmh@users.noreply.github.com>
2019-08-15 13:12:21 -0400

    [Material] Add splashColor to FAB and FAB ThemeData (#38467)
    * [Material] Add splashColor param to FAB and FAB ThemeData

--- packages/flutter/lib/src/material/floating_action_button.dart
+++ packages/flutter/lib/src/material/floating_action_button.dart
@@ -133,6 +133,7 @@ class FloatingActionButton extends StatelessWidget {
     this.backgroundColor,
     this.focusColor,
     this.hoverColor,
+    this.splashColor,
     this.heroTag = const _DefaultHeroTag(),
     this.elevation,
     this.focusElevation,
@@ -173,6 +174,7 @@ class FloatingActionButton extends StatelessWidget {
     this.elevation,
     this.focusElevation,
     this.hoverElevation,
+    this.splashColor,
     this.highlightElevation,
     this.disabledElevation,
     @required this.onPressed,
@@ -242,6 +244,12 @@ class FloatingActionButton extends StatelessWidget {
   ///
   /// Defaults to [ThemeData.hoverColor] for the current theme.
   final Color hoverColor;
+
+  /// The splash color for this [FloatingActionButton]'s [InkWell].
+  ///
+  /// If null, [FloatingActionButtonThemeData.splashColor] is used, if that is
+  /// null, [ThemeData.splashColor] is used.
+  final Color splashColor;
 
   /// The tag to apply to the button's [Hero] widget.
   ///
@@ -410,6 +418,9 @@ class FloatingActionButton extends StatelessWidget {
     final Color hoverColor = this.hoverColor
       ?? floatingActionButtonTheme.hoverColor
       ?? theme.hoverColor;
+    final Color splashColor = this.splashColor
+      ?? floatingActionButtonTheme.splashColor
+      ?? theme.splashColor;
     final double elevation = this.elevation
       ?? floatingActionButtonTheme.elevation
       ?? _defaultElevation;
@@ -447,6 +458,7 @@ class FloatingActionButton extends StatelessWidget {
       fillColor: backgroundColor,
       focusColor: focusColor,
       hoverColor: hoverColor,
+      splashColor: splashColor,
       textStyle: textStyle,
       shape: shape,
       clipBehavior: clipBehavior ?? Clip.none,
@@ -480,6 +492,7 @@ class FloatingActionButton extends StatelessWidget {
     properties.add(ColorProperty('backgroundColor', backgroundColor, defaultValue: null));
     properties.add(ColorProperty('focusColor', focusColor, defaultValue: null));
     properties.add(ColorProperty('hoverColor', hoverColor, defaultValue: null));
+    properties.add(ColorProperty('splashColor', splashColor, defaultValue: null));
     properties.add(ObjectFlagProperty<Object>('heroTag', heroTag, ifPresent: 'hero'));
     properties.add(DoubleProperty('elevation', elevation, defaultValue: null));
     properties.add(DoubleProperty('focusElevation', focusElevation, defaultValue: null));
