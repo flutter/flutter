c93765bc93e4fe7724391a0e92031eec9906651b
andrijasinski <andri.jasinski@gmail.com>
2019-08-16 10:05:15 +0300

    disable indexing for all flutter builds

--- packages/flutter_tools/lib/src/ios/mac.dart
+++ packages/flutter_tools/lib/src/ios/mac.dart
@@ -378,10 +378,7 @@ Future<XcodeBuildResult> buildXcodeProject({
   // Don't log analytics for downstream Flutter commands.
   // e.g. `flutter build bundle`.
   buildCommands.add('FLUTTER_SUPPRESS_ANALYTICS=true');
-
-  if (io.Platform.environment.containsKey('CI') && io.Platform.environment['CI'] == 'true') {
-    buildCommands.add('COMPILER_INDEX_STORE_ENABLE=NO');
-  }
+  buildCommands.add('COMPILER_INDEX_STORE_ENABLE=NO');
 
   final Stopwatch sw = Stopwatch()..start();
   initialBuildStatus = logger.startProgress('Running Xcode build...', timeout: timeoutConfiguration.fastOperation);
