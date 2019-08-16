33489f6c0da78629ff0f5a56868bc7881b4f64d5
GaryQian <garyq@google.com>
2019-08-16 14:11:50 -0700

    Add tests

--- packages/flutter/lib/src/rendering/editable.dart
+++ packages/flutter/lib/src/rendering/editable.dart
@@ -1020,7 +1020,7 @@ class RenderEditable extends RenderBox {
   /// The maximum amount the text is allowed to scroll.
   ///
   /// This value is only valid after layout and can change as additional
-  /// text is entered or removed in order to accommodate expanding
+  /// text is entered or removed in order to accommodate expanding when
   /// [expands] is set to true.
   double get maxScrollExtent => _maxScrollExtent;
   double _maxScrollExtent = 0;
