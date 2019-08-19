afb3cb3f89f543282f495406b2abc8fb6a223fea
Matt Carroll <matthew-carroll@users.noreply.github.com>
2019-08-19 14:12:45 -0700

    Notify framework to clear input connection when app is backgrounded (#35054) (flutter/engine#9498)

--- engine/src/flutter/shell/platform/android/io/flutter/plugin/editing/TextInputPlugin.java
+++ engine/src/flutter/shell/platform/android/io/flutter/plugin/editing/TextInputPlugin.java
@@ -46,6 +46,13 @@ public class TextInputPlugin {
     // target is a platform view. See the comments on lockPlatformViewInputConnection for more details.
     private boolean isInputConnectionLocked;
 
+    private final Runnable onInputConnectionClosed = new Runnable() {
+        @Override
+        public void run() {
+            clearTextInputClient();
+        }
+    };
+
     public TextInputPlugin(View view, @NonNull DartExecutor dartExecutor, @NonNull PlatformViewsController platformViewsController) {
         mView = view;
         mImm = (InputMethodManager) view.getContext().getSystemService(
@@ -219,7 +226,8 @@ public class TextInputPlugin {
             view,
             inputTarget.id,
             textInputChannel,
-            mEditable
+            mEditable,
+            onInputConnectionClosed
         );
         outAttrs.initialSelStart = Selection.getSelectionStart(mEditable);
         outAttrs.initialSelEnd = Selection.getSelectionEnd(mEditable);
