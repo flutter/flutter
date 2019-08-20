b2f8d3a6689816e6f03f0f7ae05f463e9920ed97
James D. Lin <jamesderlin@users.noreply.github.com>
2019-08-20 10:28:49 -0700

    Instrument pending timers in tests (#37646)
    Flutter widget tests assert if a test completes with timers still
    pending.  However, it can be hard to diagnose where a pending timer
    came from.  For example, a widget might consume a third-party library
    that internally uses a timer.
    
    I added a FakeAsync.pendingTimersDebugInfo getter to quiver
    (https://github.com/google/quiver-dart/pull/500).  Make flutter_test
    use it.
    
    Additionally modify Flutter's debugPrintStack to take an optional
    StackTrace argument instead of always printing StackTrace.current.
    
    Fixes #4237.

--- packages/flutter/lib/src/foundation/assertions.dart
+++ packages/flutter/lib/src/foundation/assertions.dart
@@ -737,20 +737,25 @@ class FlutterError extends Error with DiagnosticableTreeMixin implements Asserti
   }
 }
 
-/// Dump the current stack to the console using [debugPrint] and
+/// Dump the stack to the console using [debugPrint] and
 /// [FlutterError.defaultStackFilter].
 ///
-/// The current stack is obtained using [StackTrace.current].
+/// If the `stackTrace` parameter is null, the [StackTrace.current] is used to
+/// obtain the stack.
 ///
 /// The `maxFrames` argument can be given to limit the stack to the given number
-/// of lines. By default, all non-filtered stack lines are shown.
+/// of lines before filtering is applied. By default, all stack lines are
+/// included.
 ///
 /// The `label` argument, if present, will be printed before the stack.
-void debugPrintStack({ String label, int maxFrames }) {
+void debugPrintStack({StackTrace stackTrace, String label, int maxFrames}) {
   if (label != null)
     debugPrint(label);
-  Iterable<String> lines = StackTrace.current.toString().trimRight().split('\n');
-  if (kIsWeb) {
+  stackTrace ??= StackTrace.current;
+  Iterable<String> lines = stackTrace.toString().trimRight().split('\n');
+  if (   kIsWeb
+      && lines.isNotEmpty
+      && lines.first.contains('StackTrace.current')) {
     // Remove extra call to StackTrace.current for web platform.
     // TODO(ferhat): remove when https://github.com/flutter/flutter/issues/37635
     // is addressed.
