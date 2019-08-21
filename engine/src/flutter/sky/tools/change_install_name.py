59964aa29c56e3a497a727587ddf4c57cb43a5ac
stuartmorgan <stuartmorgan@google.com>
2019-08-20 21:15:42 -0700

    Fix change_install_name.py to be GN-friendly (flutter/engine#11309)
    change_install_name.py was operating on framework library files
    in-place, which breaks GN's timestamp analysis handling since a file
    can't be both an input and output of an action. As a result no-op builds
    on macOS were not actually no-ops.
    
    This changes the script to operate on an output copy, both fixing the
    no-op build issue, and simplifying the GN framework construction scripts
    by combining the copy step and the install-name step.
    
    Fixes https://github.com/flutter/flutter/issues/33465

--- engine/src/flutter/sky/tools/change_install_name.py
+++ engine/src/flutter/sky/tools/change_install_name.py
@@ -9,25 +9,25 @@ import sys
 import os
 
 
-def MakeStamp(stamp_path):
-  dir_name = os.path.dirname(stamp_path)
-
-  if not os.path.isdir(dir_name):
-    os.makedirs()
-
-  with open(stamp_path, 'a'):
-    os.utime(stamp_path, None)
-
-
 def main():
   parser = argparse.ArgumentParser(
       description='Changes the install name of a dylib')
 
   parser.add_argument('--dylib', type=str)
   parser.add_argument('--install_name', type=str)
-  parser.add_argument('--stamp', type=str)
+  # install_name_tool operates in place, which can't be expressed in GN, so
+  # this tool copies to a new location first, then operates on the copy.
+  parser.add_argument('--output', type=str)
 
   args = parser.parse_args()
+
+  subprocess.check_call([
+    '/usr/bin/env',
+    'cp',
+    '-fp',
+    args.dylib,
+    args.output,
+  ])
 
   subprocess.check_call([
     '/usr/bin/env',
@@ -35,10 +35,8 @@ def main():
     'install_name_tool',
     '-id',
     args.install_name,
-    args.dylib,
+    args.output,
   ])
-
-  MakeStamp(args.stamp)
 
 if __name__ == '__main__':
   sys.exit(main())
