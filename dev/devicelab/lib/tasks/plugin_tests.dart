4607df9f44453e63576625dbf217c9bbec9a346b
Collin Jackson <jackson@google.com>
2019-08-22 13:33:59 -0700

    Add test of plugin template working

--- dev/devicelab/lib/tasks/plugin_tests.dart
+++ dev/devicelab/lib/tasks/plugin_tests.dart
@@ -23,8 +23,8 @@ TaskFunction combine(List<TaskFunction> tasks) {
   };
 }
 
-/// Defines task that creates new Flutter project, adds a plugin, and then
-/// builds the specified [buildTarget].
+/// Defines task that creates new Flutter project, adds a local and remote
+/// plugin, and then builds the specified [buildTarget].
 class PluginTest {
   PluginTest(this.buildTarget, this.options);
 
@@ -32,19 +32,25 @@ class PluginTest {
   final List<String> options;
 
   Future<TaskResult> call() async {
-    section('Create Flutter project');
     final Directory tempDir = Directory.systemTemp.createTempSync('flutter_devicelab_plugin_test.');
     try {
-      final FlutterProject project = await FlutterProject.create(tempDir, options);
+      section('Create plugin');
+      final _FlutterApp plugin = await _FlutterPlugin.create(tempDir, options);
+      section('Test plugin');
+      await plugin.test();
+      section('Create Flutter app');
+      final _FlutterApp app = await _FlutterApp.create(tempDir, options);
       try {
         if (buildTarget == 'ios')
           await prepareProvisioningCertificates(project.rootPath);
-        section('Add plugin');
-        await project.addPlugin('path_provider');
+        section('Add plugins');
+        await app.addPlugin('plugintest', :path => '../plugintest')
+        await app.addPlugin('path_provider');
         section('Build');
-        await project.build(buildTarget);
+        await app.build(buildTarget);
       } finally {
-        await project.delete();
+        await plugin.delete();
+        await app.delete();
       }
       return TaskResult.success(null);
     } catch (e) {
@@ -55,17 +61,17 @@ class PluginTest {
   }
 }
 
-class FlutterProject {
-  FlutterProject(this.parent, this.name);
+class _FlutterPlugin {
+  _FlutterPlugin(this.parent, this.name);
 
   final Directory parent;
   final String name;
 
-  static Future<FlutterProject> create(Directory directory, List<String> options) async {
+  static Future<_FlutterPlugin> create(Directory directory, List<String> options) async {
     await inDirectory(directory, () async {
       await flutter(
         'create',
-        options: <String>['--template=app', '--org', 'io.flutter.devicelab', ...options, 'plugintest'],
+        options: <String>['--template=plugin', '--org', 'io.flutter.devicelab', ...options, 'plugintest'],
       );
     });
     return FlutterProject(directory, 'plugintest');
@@ -73,12 +79,53 @@ class FlutterProject {
 
   String get rootPath => path.join(parent.path, name);
 
-  Future<void> addPlugin(String plugin) async {
+  Future<void> test() async {
+    await inDirectory(Directory(rootPath), () async {
+      await flutter('test');
+    });
+  }
+
+  Future<void> delete() async {
+    if (Platform.isWindows) {
+      // A running Gradle daemon might prevent us from deleting the project
+      // folder on Windows.
+      await exec(
+        path.absolute(path.join(rootPath, 'android', 'gradlew.bat')),
+        <String>['--stop'],
+        canFail: true,
+      );
+      // TODO(ianh): Investigating if flakiness is timing dependent.
+      await Future<void>.delayed(const Duration(seconds: 10));
+    }
+    rmTree(parent);
+  }
+}
+
+class _FlutterApp {
+  _FlutterApp(this.parent, this.name);
+
+  final Directory parent;
+  final String name;
+
+  static Future<_FlutterApp> create(Directory directory, List<String> options) async {
+    await inDirectory(directory, () async {
+      await flutter(
+        'create',
+        options: <String>['--template=app', '--org', 'io.flutter.devicelab', ...options, 'plugintestapp'],
+      );
+    });
+    return _FlutterApp(directory, 'plugintestapp');
+  }
+
+  String get rootPath => path.join(parent.path, name);
+
+  Future<void> addPlugin(String plugin, { String path }) async {
     final File pubspec = File(path.join(rootPath, 'pubspec.yaml'));
     String content = await pubspec.readAsString();
+    String dependency = path != null ? '$plugin\n    path: $path' : plugin;
     content = content.replaceFirst(
       '\ndependencies:\n',
-      '\ndependencies:\n  $plugin:\n',
+      '\ndependencies:\n  $dependency\n',
     );
     await pubspec.writeAsString(content, flush: true);
   }
