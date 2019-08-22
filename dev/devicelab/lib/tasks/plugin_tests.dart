547da21c7ddc616fe02ba0329c09c06d162329a7
Collin Jackson <jackson@google.com>
2019-08-22 13:50:40 -0700

    Refactor for increased code reuse

--- dev/devicelab/lib/tasks/plugin_tests.dart
+++ dev/devicelab/lib/tasks/plugin_tests.dart
@@ -63,11 +63,33 @@ class PluginTest {
   }
 }
 
-class _FlutterPlugin {
-  _FlutterPlugin(this.parent, this.name);
+abstract class _FlutterProject {
+  _FlutterProject(this.parent, this.name);
 
   final Directory parent;
   final String name;
+
+  String get rootPath => path.join(parent.path, name);
+
+  Future<void> delete() async {
+    if (Platform.isWindows) {
+      // A running Gradle daemon might prevent us from deleting the project
+      // folder on Windows.
+      await exec(
+        path.absolute(path.join(rootPath, 'android', 'gradlew.bat')),
+        <String>['--stop'],
+        canFail: true,
+      );
+      // TODO(ianh): Investigating if flakiness is timing dependent.
+      await Future<void>.delayed(const Duration(seconds: 10));
+    }
+    rmTree(parent);
+  }
+
+}
+
+class _FlutterPlugin extends _FlutterProject {
+  _FlutterPlugin(this.parent, this.name) : _FlutterProject(parent, name);
 
   static Future<_FlutterPlugin> create(
       Directory directory, List<String> options) async {
@@ -86,35 +108,15 @@ class _FlutterPlugin {
     return _FlutterPlugin(directory, 'plugintest');
   }
 
-  String get rootPath => path.join(parent.path, name);
-
   Future<void> test() async {
     await inDirectory(Directory(rootPath), () async {
       await flutter('test');
     });
   }
-
-  Future<void> delete() async {
-    if (Platform.isWindows) {
-      // A running Gradle daemon might prevent us from deleting the project
-      // folder on Windows.
-      await exec(
-        path.absolute(path.join(rootPath, 'android', 'gradlew.bat')),
-        <String>['--stop'],
-        canFail: true,
-      );
-      // TODO(ianh): Investigating if flakiness is timing dependent.
-      await Future<void>.delayed(const Duration(seconds: 10));
-    }
-    rmTree(parent);
-  }
 }
 
-class _FlutterApp {
-  _FlutterApp(this.parent, this.name);
-
-  final Directory parent;
-  final String name;
+class _FlutterApp extends _FlutterProject {
+  _FlutterApp(this.parent, this.name) : _FlutterProject(parent, name);
 
   static Future<_FlutterApp> create(
       Directory directory, List<String> options) async {
@@ -133,8 +135,6 @@ class _FlutterApp {
     return _FlutterApp(directory, 'plugintestapp');
   }
 
-  String get rootPath => path.join(parent.path, name);
-
   Future<void> addPlugin(String plugin, {String pluginPath}) async {
     final File pubspec = File(path.join(rootPath, 'pubspec.yaml'));
     String content = await pubspec.readAsString();
@@ -151,20 +151,5 @@ class _FlutterApp {
     await inDirectory(Directory(rootPath), () async {
       await flutter('build', options: <String>[target]);
     });
-  }
-
-  Future<void> delete() async {
-    if (Platform.isWindows) {
-      // A running Gradle daemon might prevent us from deleting the project
-      // folder on Windows.
-      await exec(
-        path.absolute(path.join(rootPath, 'android', 'gradlew.bat')),
-        <String>['--stop'],
-        canFail: true,
-      );
-      // TODO(ianh): Investigating if flakiness is timing dependent.
-      await Future<void>.delayed(const Duration(seconds: 10));
-    }
-    rmTree(parent);
   }
 }
