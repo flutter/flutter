9c47f99c7c9c564c62242fbcb2f6686710900a2d
Kate Lovett <katelovett@google.com>
2019-08-22 13:56:59 -0700

    Doc updates, waiting on images to land.

--- packages/flutter_test/lib/src/goldens.dart
+++ packages/flutter_test/lib/src/goldens.dart
@@ -23,6 +23,24 @@ import 'package:test_api/test_api.dart' as test_package show TestFailure;
 /// fake async constraints that are normally imposed on widget tests (i.e. the
 /// need or the ability to call [WidgetTester.pump] to advance the microtask
 /// queue).
+///
+/// ### What is Golden File Testing?
+///
+/// The term __golden file__ refers to a master image that is considered the true
+/// rendering of a given widget, state, application, or other visual
+/// representation you have chosen to capture.
+///
+/// By keeping a master reference of visual aspects of your application, you can
+/// prevent unintended changes as you develop by testing against them.
+///
+/// TODO(Piinks): images etc.
+///
+/// See also:
+///
+///  * [LocalFileComparator] for the default [GoldenFileComparator]
+///    implementation for `flutter test`.
+///  * [matchesGoldenFile], the function from [flutter_test] that invokes the
+///    comparator.
 abstract class GoldenFileComparator {
   /// Compares the pixels of decoded png [imageBytes] against the golden file
   /// identified by [golden].
@@ -258,14 +276,39 @@ class TrivialComparator implements GoldenFileComparator {
 
 /// The default [GoldenFileComparator] implementation for `flutter test`.
 ///
-/// This comparator loads golden files from the local file system, treating the
-/// golden key as a relative path from the test file's directory.
+/// The term __golden file__ refers to a master image that is considered the true
+/// rendering of a given widget, state, application, or other visual
+/// representation you have chosen to capture. This comparator loads golden
+/// files from the local file system, treating the golden key as a relative
+/// path from the test file's directory.
 ///
 /// This comparator performs a pixel-for-pixel comparison of the decoded PNGs,
-/// returning true only if there's an exact match.
+/// returning true only if there's an exact match. In cases where the captured
+/// test image does not match the golden file, this comparator will provide
+/// output to illustrate the difference, described in further detail below.
 ///
 /// When using `flutter test --update-goldens`, [LocalFileComparator]
-/// updates the files on disk to match the rendering.
+/// updates the golden files on disk to match the rendering.
+///
+/// ### Local Output from Golden File Testing
+///
+/// The [LocalFileComparator] will output test feedback when a golden file test
+/// fails. This output takes the form of differential images contained within a
+/// `failures` directory that will be generated in the same location specified
+/// by the golden key. The differential images include the master and test
+/// images that were compared, as well as an isolated diff of detected pixels,
+/// and a masked diff that overlays these detected pixels over the master image.
+///
+/// The following images are examples of a test failure output:
+///
+/// TODO(Piinks) - images etc
+///
+/// See also:
+///
+///   * [GoldenFileComparator], the abstract class that [LocalFileComparator]
+///   implements.
+///   * [matchesGoldenFile], the function from [flutter_test] that invokes the
+///    comparator.
 class LocalFileComparator extends GoldenFileComparator {
   /// Creates a new [LocalFileComparator] for the specified [testFile].
   ///
