6ef71c29ea1435741e57a8593373093a9259c717
Lisa Liao <lisaliao@google.com>
2019-08-23 08:24:50 -0700

    Addressing PR Feedback

--- examples/flutter_gallery/lib/demo/shrine/login.dart
+++ examples/flutter_gallery/lib/demo/shrine/login.dart
@@ -24,7 +24,7 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   final TextEditingController _usernameController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
-  static final ShapeDecoration _decoration = const ShapeDecoration(
+  static const ShapeDecoration _decoration =  ShapeDecoration(
     shape: BeveledRectangleBorder(
       side: BorderSide(color: kShrineBrown900, width: 0.5),
       borderRadius: BorderRadius.all(Radius.circular(7.0)),
