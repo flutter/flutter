d753316b895cbb7f16cd25e349e4471f3efef370
Venkataramana Neelapala <vramana98@gmail.com>
2019-08-23 15:27:35 +0530

    removed unwanted imports

--- packages/flutter/test/cupertino/date_picker_test.dart
+++ packages/flutter/test/cupertino/date_picker_test.dart
@@ -5,7 +5,6 @@
 import 'dart:ui';
 
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter/semantics.dart';
 import 'package:flutter_test/flutter_test.dart';
 
