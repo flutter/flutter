ffe8c0b33dfbde0f729e355f6873db4fc71e5289
Yegor <yjbanov@google.com>
2019-08-22 17:35:07 -0700

    preventDefault on touchend to show iOS keyboard (#11390)

--- lib/web_ui/lib/src/engine/pointer_binding.dart
+++ lib/web_ui/lib/src/engine/pointer_binding.dart
@@ -318,6 +318,9 @@ class TouchAdapter extends BaseAdapter {
     });
 
     _addEventListener('touchend', (html.Event event) {
+      // On Safari Mobile, the keyboard does not show unless this line is
+      // added.
+      event.preventDefault();
       _updateButtonDownState(_kPrimaryMouseButton, false);
       _callback(_convertEventToPointerData(ui.PointerChange.up, event));
     });
