08ab67bf00ae19b789f0a97541c8d316658077b2
robinsontom <robinsontom@google.com>
2019-08-26 11:51:55 -0700

    Add test for RenderSliverFloatingPersistentHeader maxScrollObstructionExtent

---
+++ packages/flutter/test/rendering/sliver_persistent_header_test.dart
@@ -0,0 +1,38 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/rendering.dart';
+import '../flutter_test_alternative.dart';
+
+import 'rendering_tester.dart';
+
+void main() {
+  test('RenderSliverFloatingPersistentHeader maxScrollObstructionExtent', () {
+    final TestRenderSliverFloatingPersistentHeader header = TestRenderSliverFloatingPersistentHeader(child: RenderSizedBox(const Size(400.0, 100.0)));
+    final RenderViewport root = RenderViewport(
+      axisDirection: AxisDirection.down,
+      crossAxisDirection: AxisDirection.right,
+      offset: ViewportOffset.zero(),
+      cacheExtent: 0,
+      children: <RenderSliver>[
+        header,
+      ],
+    );
+    layout(root);
+
+    expect(header.geometry.maxScrollObstructionExtent, 0);
+  });
+}
+
+class TestRenderSliverFloatingPersistentHeader extends RenderSliverFloatingPersistentHeader {
+  TestRenderSliverFloatingPersistentHeader({
+    RenderBox child,
+  }) : super(child: child);
+
+  @override
+  double get maxExtent => 200;
+
+  @override
+  double get minExtent => 100;
+}
