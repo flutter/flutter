0f8b8c91a3d8fa3b4182ab71144b88672c9f9a1b
Jonah Williams <jonahwilliams@google.com>
2019-08-26 15:06:36 -0700

    Kill resident runner on browser disconnect. (#39066)

--- packages/flutter_tools/lib/src/build_runner/resident_web_runner.dart
+++ packages/flutter_tools/lib/src/build_runner/resident_web_runner.dart
@@ -160,6 +160,7 @@ class ResidentWebRunner extends ResidentRunner {
       );
       if (supportsServiceProtocol) {
         _debugConnection = await _webFs.runAndDebug();
+        unawaited(_debugConnection.onDone.whenComplete(exit));
       }
     } catch (err, stackTrace) {
       printError(err.toString());
