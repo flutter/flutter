15f119a4429877691cad869386a0ef09ea5a41d2
LongCat is Looong <31859944+LongCatIsLooong@users.noreply.github.com>
2019-08-26 12:29:24 -0700

    activity indicator

--- packages/flutter/lib/src/cupertino/activity_indicator.dart
+++ packages/flutter/lib/src/cupertino/activity_indicator.dart
@@ -9,21 +9,27 @@ import 'package:flutter/widgets.dart';
 import 'colors.dart';
 
 const double _kDefaultIndicatorRadius = 10.0;
+final Color _kActiveTickColor = CupertinoDynamicColor.withBrightness(
+  color: const Color(0x99606067),
+  darkColor: const Color(0x99EBEBF5),
+);
 
-/// An iOS-style activity indicator.
+/// An iOS-style activity indicator that spins clockwise.
 ///
 /// See also:
 ///
 ///  * <https://developer.apple.com/ios/human-interface-guidelines/controls/progress-indicators/#activity-indicators>
 class CupertinoActivityIndicator extends StatefulWidget {
-  /// Creates an iOS-style activity indicator.
-  const CupertinoActivityIndicator({
+  /// Creates an iOS-style activity indicator that spins clockwise.
+  CupertinoActivityIndicator({
     Key key,
     this.animating = true,
     this.radius = _kDefaultIndicatorRadius,
+    Color activeTickColor,
   }) : assert(animating != null),
        assert(radius != null),
        assert(radius > 0),
+       activeTickColor = activeTickColor ?? _kActiveTickColor,
        super(key: key);
 
   /// Whether the activity indicator is running its animation.
@@ -35,6 +41,12 @@ class CupertinoActivityIndicator extends StatefulWidget {
   ///
   /// Defaults to 10px. Must be positive and cannot be null.
   final double radius;
+
+  /// The color used to paint the most prominent tick (the tick with the highest opacity).
+  ///
+  /// Defaults to a CupertinoDynamicColor extracted from the large indeterminate
+  /// spinners in https://developer.apple.com/design/resources/.
+  final Color activeTickColor;
 
   @override
   _CupertinoActivityIndicatorState createState() => _CupertinoActivityIndicatorState();
@@ -81,6 +93,7 @@ class _CupertinoActivityIndicatorState extends State<CupertinoActivityIndicator>
       child: CustomPaint(
         painter: _CupertinoActivityIndicatorPainter(
           position: _controller,
+          activeColor: CupertinoDynamicColor.resolve(widget.activeTickColor, context),
           radius: widget.radius,
         ),
       ),
@@ -91,12 +104,11 @@ class _CupertinoActivityIndicatorState extends State<CupertinoActivityIndicator>
 const double _kTwoPI = math.pi * 2.0;
 const int _kTickCount = 12;
 const int _kHalfTickCount = _kTickCount ~/ 2;
-const Color _kTickColor = CupertinoColors.lightBackgroundGray;
-const Color _kActiveTickColor = Color(0xFF9D9D9D);
 
 class _CupertinoActivityIndicatorPainter extends CustomPainter {
   _CupertinoActivityIndicatorPainter({
-    this.position,
+    @required this.position,
+    @required this.activeColor,
     double radius,
   }) : tickFundamentalRRect = RRect.fromLTRBXY(
            -radius,
@@ -110,6 +122,7 @@ class _CupertinoActivityIndicatorPainter extends CustomPainter {
 
   final Animation<double> position;
   final RRect tickFundamentalRRect;
+  final Color activeColor;
 
   @override
   void paint(Canvas canvas, Size size) {
@@ -122,7 +135,7 @@ class _CupertinoActivityIndicatorPainter extends CustomPainter {
 
     for (int i = 0; i < _kTickCount; ++ i) {
       final double t = (((i + activeTick) % _kTickCount) / _kHalfTickCount).clamp(0.0, 1.0);
-      paint.color = Color.lerp(_kActiveTickColor, _kTickColor, t);
+      paint.color = activeColor.withAlpha((t * activeColor.alpha).clamp(0, 255));
       canvas.drawRRect(tickFundamentalRRect, paint);
       canvas.rotate(-_kTwoPI / _kTickCount);
     }
@@ -132,6 +145,6 @@ class _CupertinoActivityIndicatorPainter extends CustomPainter {
 
   @override
   bool shouldRepaint(_CupertinoActivityIndicatorPainter oldPainter) {
-    return oldPainter.position != position;
+    return oldPainter.position != position || oldPainter.activeColor != activeColor;
   }
 }
