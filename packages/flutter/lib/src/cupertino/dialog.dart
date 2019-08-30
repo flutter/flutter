e9ef70bc0e36e81edd94b55d8c5ca2f4cab5302f
LongCat is Looong <31859944+LongCatIsLooong@users.noreply.github.com>
2019-08-30 10:07:53 -0700

    add tests

--- packages/flutter/lib/src/cupertino/dialog.dart
+++ packages/flutter/lib/src/cupertino/dialog.dart
@@ -5,6 +5,7 @@
 import 'dart:math' as math;
 import 'dart:ui' show ImageFilter;
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
@@ -224,35 +225,38 @@ class CupertinoAlertDialog extends StatelessWidget {
     final CupertinoLocalizations localizations = CupertinoLocalizations.of(context);
     final bool isInAccessibilityMode = _isInAccessibilityMode(context);
     final double textScaleFactor = MediaQuery.of(context).textScaleFactor;
-    return MediaQuery(
-      data: MediaQuery.of(context).copyWith(
-        // iOS does not shrink dialog content below a 1.0 scale factor
-        textScaleFactor: math.max(textScaleFactor, 1.0),
-      ),
-      child: LayoutBuilder(
-        builder: (BuildContext context, BoxConstraints constraints) {
-          return Center(
-            child: Container(
-              margin: const EdgeInsets.symmetric(vertical: _kEdgePadding),
-              width: isInAccessibilityMode
-                  ? _kAccessibilityCupertinoDialogWidth
-                  : _kCupertinoDialogWidth,
-              child: CupertinoPopupSurface(
-                isSurfacePainted: false,
-                child: Semantics(
-                  namesRoute: true,
-                  scopesRoute: true,
-                  explicitChildNodes: true,
-                  label: localizations.alertDialogLabel,
-                  child: _CupertinoDialogRenderWidget(
-                    contentSection: _buildContent(context),
-                    actionsSection: _buildActions(),
+    return CupertinoUserInterfaceLevel(
+      data: CupertinoUserInterfaceLevelData.elevated,
+      child: MediaQuery(
+        data: MediaQuery.of(context).copyWith(
+          // iOS does not shrink dialog content below a 1.0 scale factor
+          textScaleFactor: math.max(textScaleFactor, 1.0),
+        ),
+        child: LayoutBuilder(
+          builder: (BuildContext context, BoxConstraints constraints) {
+            return Center(
+              child: Container(
+                margin: const EdgeInsets.symmetric(vertical: _kEdgePadding),
+                width: isInAccessibilityMode
+                ? _kAccessibilityCupertinoDialogWidth
+                : _kCupertinoDialogWidth,
+                child: CupertinoPopupSurface(
+                  isSurfacePainted: false,
+                  child: Semantics(
+                    namesRoute: true,
+                    scopesRoute: true,
+                    explicitChildNodes: true,
+                    label: localizations.alertDialogLabel,
+                    child: _CupertinoDialogRenderWidget(
+                      contentSection: _buildContent(context),
+                      actionsSection: _buildActions(),
+                    ),
                   ),
                 ),
               ),
-            ),
-          );
-        },
+            );
+          },
+        ),
       ),
     );
   }
