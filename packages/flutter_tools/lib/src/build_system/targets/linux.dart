ae166150ee5a7e94a5d0ac0e4bc8d7f6a9d8ed0b
Jonah Williams <jonahwilliams@google.com>
2019-09-03 12:46:08 -0700

    remove input files argument to target (#39701)

--- packages/flutter_tools/lib/src/build_system/targets/linux.dart
+++ packages/flutter_tools/lib/src/build_system/targets/linux.dart
@@ -36,12 +36,11 @@ class UnpackLinux extends Target {
   List<Target> get dependencies => <Target>[];
 
   @override
-  Future<void> build(List<File> inputFiles, Environment environment) async {
+  Future<void> build(Environment environment) async {
     final String basePath = artifacts.getArtifactPath(Artifact.linuxDesktopPath);
-    for (File input in inputFiles) {
-      if (fs.path.basename(input.path) == 'linux.dart') {
-        continue;
-      }
+    for (File input in fs.directory(basePath)
+        .listSync(recursive: true)
+        .whereType<File>()) {
       final String outputPath = fs.path.join(
         environment.projectDir.path,
         'linux',
