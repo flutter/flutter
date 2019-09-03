96c417043bef9f9bc4dbec63dcb66f0dcef4bc4b
Greg Spencer <gspencer@google.com>
2019-09-03 16:44:17 -0700

    Review Changes

--- packages/flutter/lib/src/foundation/observer_list.dart
+++ packages/flutter/lib/src/foundation/observer_list.dart
@@ -10,7 +10,7 @@ import 'dart:collection';
 /// Consider using an [ObserverList] instead of a [List] when the number of
 /// [contains] calls dominates the number of [add] and [remove] calls.
 ///
-/// This class will include int the [iterator] each added item in the order it
+/// This class will include in the [iterator] each added item in the order it
 /// was added, as many times as it was added.
 ///
 /// If there will be a large number of observers, consider using
@@ -57,7 +57,6 @@ class ObserverList<T> extends Iterable<T> {
       if (_set == null) {
         _set = HashSet<T>.from(_list);
       } else {
-        _set.clear();
         _set.addAll(_list);
       }
       _isDirty = false;
@@ -81,7 +80,7 @@ class ObserverList<T> extends Iterable<T> {
 /// For small numbers of observers (e.g. less than 10), use [ObserverList] instead.
 ///
 /// The iteration semantics of the this class are slightly different from
-/// [ObserverList].  This class will only return an item once in the [iterator],
+/// [ObserverList]. This class will only return an item once in the [iterator],
 /// no matter how many times it was added, although it does require that an item
 /// be removed as many times as it was added for it to stop appearing in the
 /// [iterator]. It will return them in the order the first instance of an item
