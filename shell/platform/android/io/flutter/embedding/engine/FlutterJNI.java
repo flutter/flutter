edee1fea585002935a6203d33a278bfec18aeb0a
Emmanuel Garcia <egarciad@google.com>
2019-09-05 13:23:04 -0700

    Add @Keep annotation (#11893)

--- shell/platform/android/io/flutter/embedding/engine/FlutterJNI.java
+++ shell/platform/android/io/flutter/embedding/engine/FlutterJNI.java
@@ -9,6 +9,7 @@ import android.content.res.AssetManager;
 import android.graphics.Bitmap;
 import android.graphics.SurfaceTexture;
 import android.os.Looper;
+import android.support.annotation.Keep;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.annotation.UiThread;
@@ -92,6 +93,7 @@ import io.flutter.view.FlutterCallbackInformation;
  *    bool enabled = FlutterJNI.nativeGetIsSoftwareRenderingEnabled();
  * }
  */
+@Keep
 public class FlutterJNI {
   private static final String TAG = "FlutterJNI";
 
