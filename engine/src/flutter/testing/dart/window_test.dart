df4c16f94791c7e0d58b8f1e13c9b2eab67d0112
liyuqian <liyuqian@google.com>
2019-09-05 10:41:55 -0700

    Revert "Add a BroadcastStream to FrameTiming (#11041)" (flutter/engine#11841)
    This reverts commit 226c44ba3c24cd89b4e47d0ce59b21ac7af6946e.
    
    Reason for revert: Stream is considered a bad API: https://github.com/flutter/engine/pull/11041#issuecomment-526892247

--- engine/src/flutter/testing/dart/window_test.dart
+++ engine/src/flutter/testing/dart/window_test.dart
@@ -24,42 +24,4 @@ void main() {
     FrameTiming timing = FrameTiming(<int>[1000, 8000, 9000, 19500]);
     expect(timing.toString(), 'FrameTiming(buildDuration: 7.0ms, rasterDuration: 10.5ms, totalSpan: 18.5ms)');
   });
-
-  test('window.frameTimings works', () async {
-    // Test a single subscription. Check that debugNeedsReportTimings is
-    // properly reset after the subscription is cancelled.
-    expect(window.debugNeedsReportTimings, false);
-    final FrameTiming mockTiming = FrameTiming(<int>[1000, 8000, 9000, 19500]);
-    final Future<FrameTiming> frameTiming = window.frameTimings.first;
-    expect(window.debugNeedsReportTimings, true);
-    window.debugReportTimings(<FrameTiming>[mockTiming]);
-    expect(await frameTiming, equals(mockTiming));
-    expect(window.debugNeedsReportTimings, false);
-
-    // Test multiple (two) subscriptions after that debugNeedsReportTimings has
-    // been reset to false by the single subscription test above.
-    //
-    // Subscription 1
-    final Future<FrameTiming> timingFuture = window.frameTimings.first;
-    //
-    // Subscription 2
-    final List<FrameTiming> timings = <FrameTiming>[];
-    final Completer<void> completer = Completer<void>();
-    int frameCount = 0;
-    window.frameTimings.listen((FrameTiming t) {
-      timings.add(t);
-      frameCount += 1;
-      if (frameCount == 2) {
-        completer.complete();
-      }
-    });
-
-    final FrameTiming secondMock = FrameTiming(<int>[1, 2, 3, 4]);
-    window.debugReportTimings(<FrameTiming>[secondMock, secondMock]);
-    final FrameTiming timing = await timingFuture;
-    expect(timing != mockTiming, isTrue);
-    expect(timing, equals(secondMock));
-    await completer.future;
-    expect(timings, hasLength(2));
-  });
 }
