e2325600eaa6ef59b043694059de6d9ca317fbb0
Michael Goderbauer <goderbauer@google.com>
2019-09-05 17:08:50 -0700

    Do not render any frames when just initializing Bindings (#39535)

--- packages/flutter/test/foundation/service_extensions_test.dart
+++ packages/flutter/test/foundation/service_extensions_test.dart
@@ -70,6 +70,7 @@ class TestServiceExtensionsBinding extends BindingBase
   bool frameScheduled = false;
   @override
   void scheduleFrame() {
+    ensureFrameCallbacksRegistered();
     frameScheduled = true;
   }
   Future<void> doFrame() async {
@@ -126,7 +127,7 @@ void main() {
   final List<String> console = <String>[];
 
   setUpAll(() async {
-    binding = TestServiceExtensionsBinding();
+    binding = TestServiceExtensionsBinding()..scheduleFrame();
     expect(binding.frameScheduled, isTrue);
 
     // We need to test this service extension here because the result is true
@@ -140,7 +141,7 @@ void main() {
     firstFrameResult = await binding.testExtension('didSendFirstFrameRasterizedEvent', <String, String>{});
     expect(firstFrameResult, <String, String>{'enabled': 'false'});
 
-    await binding.doFrame(); // initial frame scheduled by creating the binding
+    await binding.doFrame();
 
     expect(binding.debugDidSendFirstFrameEvent, isTrue);
     firstFrameResult = await binding.testExtension('didSendFirstFrameEvent', <String, String>{});
