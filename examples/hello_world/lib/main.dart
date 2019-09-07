1052f2a6881a6a61af297dbea26e45e1b3df67d3
MH Johnson <johnsonmh@johnsonmh-macbookpro2.roam.corp.google.com>
2019-09-07 01:05:50 -0400

    Adding MaterialStateBuilder demo

--- examples/hello_world/lib/main.dart
+++ examples/hello_world/lib/main.dart
@@ -1,7 +1,43 @@
-// Copyright 2015 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-void main() => runApp(const Center(child: Text('Hello, world!', textDirection: TextDirection.ltr)));
+void main() => runApp(_Home());
+
+class _Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: MaterialStateBuilder(
+            builder: (Set<MaterialState> states) {
+              final bool pressed = states.contains(MaterialState.pressed);
+              final bool hovered = states.contains(MaterialState.hovered);
+
+              return RaisedButton(
+                onPressed: () {},
+                elevation: pressed ? 10 : 2,
+                color: Colors.blue,
+                shape: RoundedRectangleBorder(
+                  side: BorderSide(
+                    color: pressed ? Colors.yellow : Colors.white,
+                    width: hovered ? 2 : 1,
+                  ),
+                ),
+                child: Text(
+                  'Button',
+                  style: TextStyle(
+                    color: pressed ? Colors.yellow : Colors.white,
+                    decoration: hovered ? TextDecoration.underline : null,
+                    fontSize: 20.0,
+                  ),
+                ),
+              );
+            },
+            disabled: false,
+          ),
+        ),
+      ),
+    );
+  }
+}
