9d679192b513952afb95b0668f3536376230b21e
Jonah Williams <jonahwilliams@google.com>
2019-09-10 11:40:24 -0700

    Update _binding_io.dart

--- packages/flutter_test/lib/src/_binding_io.dart
+++ packages/flutter_test/lib/src/_binding_io.dart
@@ -7,10 +7,10 @@ import 'dart:convert';
 import 'dart:io';
 import 'dart:typed_data';
 
-import 'package:path/path.dart' as path;
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:meta/meta.dart';
+import 'package:path/path.dart' as path;
 
 import 'binding.dart';
 
