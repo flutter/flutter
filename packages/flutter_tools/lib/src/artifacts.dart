b31206c228ee9fc203a5d6536e90eccdb4c2f272
Jonah Williams <jonahwilliams@google.com>
2019-09-10 20:03:35 -0700

    Revert "build bundle with assemble (#37508)"
    This reverts commit a7aff567417c361574ac0343599256b2d9727268.

--- packages/flutter_tools/lib/src/artifacts.dart
+++ packages/flutter_tools/lib/src/artifacts.dart
@@ -92,9 +92,6 @@ String _artifactToFileName(Artifact artifact, [ TargetPlatform platform, BuildMo
     case Artifact.frontendServerSnapshotForEngineDartSdk:
       return 'frontend_server.dart.snapshot';
     case Artifact.engineDartBinary:
-      if (platform == TargetPlatform.windows_x64) {
-        return 'dart.exe';
-      }
       return 'dart';
     case Artifact.dart2jsSnapshot:
       return 'dart2js.dart.snapshot';
@@ -268,7 +265,7 @@ class CachedArtifacts extends Artifacts {
       case Artifact.engineDartSdkPath:
         return dartSdkPath;
       case Artifact.engineDartBinary:
-        return fs.path.join(dartSdkPath, 'bin', _artifactToFileName(artifact, platform));
+        return fs.path.join(dartSdkPath, 'bin', _artifactToFileName(artifact));
       case Artifact.platformKernelDill:
         return fs.path.join(_getFlutterPatchedSdkPath(mode), _artifactToFileName(artifact));
       case Artifact.platformLibrariesJson:
--- packages/flutter_tools/test/general.shard/build_system/targets/dart_test.dart
+++ packages/flutter_tools/test/general.shard/build_system/targets/dart_test.dart
@@ -81,7 +81,6 @@ flutter_tools:lib/''');
         fs.path.join('bin', 'cache', 'pkg', 'sky_engine', 'sdk_ext',
             'vmservice_io.dart'),
         fs.path.join('bin', 'cache', 'dart-sdk', 'bin', 'dart'),
-        fs.path.join('bin', 'cache', 'dart-sdk', 'bin', 'dart.exe'),
         fs.path.join(engineArtifacts, getNameForHostPlatform(hostPlatform),
             'frontend_server.dart.snapshot'),
         fs.path.join(engineArtifacts, 'android-arm-profile',
