a7aff567417c361574ac0343599256b2d9727268
Jonah Williams <jonahwilliams@google.com>
2019-09-10 19:28:38 -0700

    build bundle with assemble (#37508)

--- packages/flutter_tools/lib/src/commands/assemble.dart
+++ packages/flutter_tools/lib/src/commands/assemble.dart
@@ -5,7 +5,6 @@
 import 'package:meta/meta.dart';
 
 import '../base/common.dart';
-import '../base/context.dart';
 import '../base/file_system.dart';
 import '../build_system/build_system.dart';
 import '../build_system/targets/assets.dart';
@@ -17,9 +16,6 @@ import '../build_system/targets/windows.dart';
 import '../globals.dart';
 import '../project.dart';
 import '../runner/flutter_command.dart';
-
-/// The [BuildSystem] instance.
-BuildSystem get buildSystem => context.get<BuildSystem>();
 
 /// All currently implemented targets.
 const List<Target> _kDefaultTargets = <Target>[
