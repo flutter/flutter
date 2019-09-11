0c3fcd1bb5a283463dbb810919bb72f8c0894b59
MH Johnson <johnsonmh@johnsonmh-macbookpro2.roam.corp.google.com>
2019-09-11 10:09:18 -0400

    revert example

--- examples/hello_world/lib/main.dart
+++ examples/hello_world/lib/main.dart
@@ -1,43 +1,7 @@
-import 'package:flutter/material.dart';
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import 'package:flutter/widgets.dart';
 
-void main() => runApp(_Home());
-
-class _Home extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    final VoidCallback onPressed = () {};
-
-    return MaterialApp(
-      home: Scaffold(
-        body: MaterialStateBuilder(
-          disabled: onPressed == null,
-          builder: (BuildContext context, Set<MaterialState> states) {
-            final bool pressed = states.contains(MaterialState.pressed);
-            final bool hovered = states.contains(MaterialState.hovered);
-
-            return RaisedButton(
-              onPressed: onPressed,
-              elevation: pressed ? 10 : 2,
-              color: Colors.blue,
-              shape: RoundedRectangleBorder(
-                side: BorderSide(
-                  color: pressed ? Colors.yellow : Colors.white,
-                  width: hovered ? 2 : 1,
-                ),
-              ),
-              child: Text(
-                'Button',
-                style: TextStyle(
-                  color: pressed ? Colors.yellow : Colors.white,
-                  decoration: hovered ? TextDecoration.underline : null,
-                  fontSize: 20.0,
-                ),
-              ),
-            );
-          },
-        ),
-      ),
-    );
-  }
-}
+void main() => runApp(const Center(child: Text('Hello, world!', textDirection: TextDirection.ltr)));
