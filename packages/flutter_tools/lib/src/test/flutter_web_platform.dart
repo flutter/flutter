266c8f29b1462026d5810e83af5e2ca43aae9b4c
jonahwilliams <jonahwilliams@google.com>
2019-09-10 20:19:29 -0700

    remove timeout from test

--- packages/flutter_tools/lib/src/test/flutter_web_platform.dart
+++ packages/flutter_tools/lib/src/test/flutter_web_platform.dart
@@ -528,12 +528,7 @@ class BrowserManager {
       }
       completer.completeError(error, stackTrace);
     }));
-
-    return completer.future.timeout(const Duration(seconds: 30), onTimeout: () {
-      chrome.close();
-      throwToolExit('Timed out waiting for ${runtime.name} to connect.');
-      return;
-    });
+    return completer.future;
   }
 
   /// Loads [_BrowserEnvironment].
