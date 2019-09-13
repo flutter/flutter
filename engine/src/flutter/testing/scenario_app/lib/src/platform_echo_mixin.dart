6e2b87462f89872473647cbf17a88303f0070589
xster <xster@google.com>
2019-09-13 10:34:36 -0700

    Reland add some AppLifecycleTests

---
+++ engine/src/flutter/testing/scenario_app/lib/src/platform_echo_mixin.dart
@@ -0,0 +1,19 @@
+// Copyright 2019 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'dart:typed_data';
+import 'dart:ui';
+
+import 'scenario.dart';
+
+mixin PlatformEchoMixin on Scenario {
+  @override
+  void onPlatformMessage(
+    String name,
+    ByteData data,
+    PlatformMessageResponseCallback callback,
+  ) {
+    window.sendPlatformMessage(name, data, null);
+  }
+}
