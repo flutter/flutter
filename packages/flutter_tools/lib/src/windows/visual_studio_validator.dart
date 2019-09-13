0a0d60fca1f7aeae7d20aeaf487e74b79fb14f99
Francisco Magdaleno <franciscojma86@gmail.com>
2019-09-12 18:44:36 -0700

    Adds list required components when VS is not installed (#40397)

--- packages/flutter_tools/lib/src/windows/visual_studio_validator.dart
+++ packages/flutter_tools/lib/src/windows/visual_studio_validator.dart
@@ -12,6 +12,8 @@ VisualStudioValidator get visualStudioValidator => context.get<VisualStudioValid
 class VisualStudioValidator extends DoctorValidator {
   const VisualStudioValidator() : super('Visual Studio - develop for Windows');
 
+  int get majorVersion => int.tryParse(visualStudio.fullVersion.split('.')[0]);
+
   @override
   Future<ValidationResult> validate() async {
     final List<ValidationMessage> messages = <ValidationMessage>[];
@@ -46,7 +48,6 @@ class VisualStudioValidator extends DoctorValidator {
         messages.add(ValidationMessage.error(userMessages.visualStudioNotLaunchable));
       } else  if (!visualStudio.hasNecessaryComponents) {
         status = ValidationType.partial;
-        final int majorVersion = int.tryParse(visualStudio.fullVersion.split('.')[0]);
         messages.add(ValidationMessage.error(
             userMessages.visualStudioMissingComponents(
                 visualStudio.workloadDescription,
@@ -57,7 +58,12 @@ class VisualStudioValidator extends DoctorValidator {
       versionInfo = '${visualStudio.displayName} ${visualStudio.displayVersion}';
     } else {
       status = ValidationType.missing;
-      messages.add(ValidationMessage.error(userMessages.visualStudioMissing));
+      messages.add(ValidationMessage.error(
+        userMessages.visualStudioMissing(
+          visualStudio.workloadDescription,
+          visualStudio.necessaryComponentDescriptions(majorVersion)
+        )
+      ));
     }
 
     return ValidationResult(status, messages, statusInfo: versionInfo);
