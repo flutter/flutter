743db255683b4cb972467ce03011a19a9080b498
Jonah Williams <jonahwilliams@google.com>
2019-09-12 18:30:24 -0700

    place conditions on same line (#40391)

--- packages/flutter_test/lib/flutter_test.dart
+++ packages/flutter_test/lib/flutter_test.dart
@@ -46,8 +46,7 @@ library flutter_test;
 
 export 'dart:async' show Future;
 
-export 'src/_goldens_io.dart'
-  if (dart.library.html) 'src/_goldens_web.dart';
+export 'src/_goldens_io.dart' if (dart.library.html) 'src/_goldens_web.dart';
 export 'src/accessibility.dart';
 export 'src/all_elements.dart';
 export 'src/binding.dart';
