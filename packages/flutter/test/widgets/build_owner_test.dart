442e599abada51ca9da315fde4c756a1a7b998bd
Remi Rousselet <darky12s@gmail.com>
2019-09-14 13:22:00 +0200

    didChangeDependencies test

--- packages/flutter/test/widgets/build_owner_test.dart
+++ packages/flutter/test/widgets/build_owner_test.dart
@@ -6,16 +6,46 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter/widgets.dart';
 
 void main() {
-  testWidgets('BuildOwner.isBuilding is true when the widget tree builds, and false otherwise',
+  testWidgets('BuildOwner.isBuilding is true inside didChangeDependencies/build and false otherwise',
     (WidgetTester tester) async {
       expect(tester.binding.buildOwner.isBuilding, isFalse);
 
-      await tester.pumpWidget(Builder(builder: (BuildContext _) {
-        expect(tester.binding.buildOwner.isBuilding, isTrue);
-        return Container();
-      }));
+      await tester.pumpWidget(
+        _Stateful(
+          didChangeDependencies: () {
+            expect(tester.binding.buildOwner.isBuilding, isTrue);
+          },
+          builder: (BuildContext _) {
+            expect(tester.binding.buildOwner.isBuilding, isTrue);
+            return Container();
+          },
+        ),
+      );
 
       expect(tester.binding.buildOwner.isBuilding, isFalse);
     },
   );
+}
+
+class _Stateful extends StatefulWidget {
+  const _Stateful({Key key, this.didChangeDependencies, this.builder}) : super(key: key);
+
+  final VoidCallback didChangeDependencies;
+  final WidgetBuilder builder;
+
+  @override
+  _StatefulState createState() => _StatefulState();
+}
+
+class _StatefulState extends State<_Stateful> {
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    widget.didChangeDependencies?.call();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return widget.builder(context);
+  }
 }
