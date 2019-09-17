9f2bdc921c721e0f0beff66198c2a7a84467a964
xster <xster@google.com>
2019-09-17 11:09:18 -0700

    Let Material BackButton have a custom onPressed handler (#39600)

--- packages/flutter/lib/src/cupertino/nav_bar.dart
+++ packages/flutter/lib/src/cupertino/nav_bar.dart
@@ -1259,7 +1259,8 @@ class CupertinoNavigationBarBackButton extends StatelessWidget {
   /// to pop the [Navigator].
   ///
   /// It can, for instance, be used to pop the platform's navigation stack
-  /// instead of Flutter's [Navigator].
+  /// via [SystemNavigator] instead of Flutter's [Navigator] in add-to-app
+  /// situations.
   ///
   /// Defaults to null.
   final VoidCallback onPressed;
