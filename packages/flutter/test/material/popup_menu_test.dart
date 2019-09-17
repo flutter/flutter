6d18c20a9575648a1de20358ecbb4ed5b8cfc6c2
Hans Muller <hansmuller@google.com>
2019-09-17 08:48:27 -0700

    Update PopupMenu layout (#40179)

--- packages/flutter/test/material/popup_menu_test.dart
+++ packages/flutter/test/material/popup_menu_test.dart
@@ -690,6 +690,160 @@ void main() {
     await tester.tap(find.text('Menu Button'));
   });
 
+  testWidgets('PopupMenuItem child height is a minimum, child is vertically centered', (WidgetTester tester) async {
+    final Key popupMenuButtonKey = UniqueKey();
+    final Type menuItemType = const PopupMenuItem<String>(child: Text('item')).runtimeType;
+
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Scaffold(
+          body: Center(
+            child: PopupMenuButton<String>(
+              key: popupMenuButtonKey,
+              child: const Text('button'),
+              onSelected: (String result) { },
+              itemBuilder: (BuildContext context) {
+                return <PopupMenuEntry<String>>[
+                  // This menu item's height will be 48 because the the default minimum height
+                  // is 48 and the height of the text is less than 48.
+                  const PopupMenuItem<String>(
+                    value: '0',
+                    child: Text('Item 0'),
+                  ),
+                  // This menu item's height parameter specifies its minium height. The
+                  // overall height of the menu item will be 50 because the child's
+                  // height 40, is less than 50.
+                  const PopupMenuItem<String>(
+                    height: 50,
+                    value: '1',
+                    child: SizedBox(
+                      height: 40,
+                      child: Text('Item 1'),
+                    ),
+                  ),
+                  // This menu item's height parameter specifies its minium height, so the
+                  // overall height of the menu item will be 75.
+                  const PopupMenuItem<String>(
+                    height: 75,
+                    value: '2',
+                    child: SizedBox(
+                      child: Text('Item 2'),
+                    ),
+                  ),
+                  // This menu item's height will be 100.
+                  const PopupMenuItem<String>(
+                    value: '3',
+                    child: SizedBox(
+                      height: 100,
+                      child: Text('Item 3'),
+                    ),
+                  ),
+                ];
+              },
+            ),
+          ),
+        ),
+      ),
+    );
+
+    // Show the menu
+    await tester.tap(find.byKey(popupMenuButtonKey));
+    await tester.pumpAndSettle();
+
+    // The menu items and their InkWells should have the expected vertical size
+    expect(tester.getSize(find.widgetWithText(menuItemType, 'Item 0')).height, 48);
+    expect(tester.getSize(find.widgetWithText(menuItemType, 'Item 1')).height, 50);
+    expect(tester.getSize(find.widgetWithText(menuItemType, 'Item 2')).height, 75);
+    expect(tester.getSize(find.widgetWithText(menuItemType, 'Item 3')).height, 100);
+    expect(tester.getSize(find.widgetWithText(InkWell, 'Item 0')).height, 48);
+    expect(tester.getSize(find.widgetWithText(InkWell, 'Item 1')).height, 50);
+    expect(tester.getSize(find.widgetWithText(InkWell, 'Item 2')).height, 75);
+    expect(tester.getSize(find.widgetWithText(InkWell, 'Item 3')).height, 100);
+
+    // Menu item children which whose height is less than the PopupMenuItem
+    // are vertically centered.
+    expect(
+      tester.getRect(find.widgetWithText(menuItemType, 'Item 0')).center.dy,
+      tester.getRect(find.text('Item 0')).center.dy,
+    );
+    expect(
+      tester.getRect(find.widgetWithText(menuItemType, 'Item 2')).center.dy,
+      tester.getRect(find.text('Item 2')).center.dy,
+    );
+  });
+
+  testWidgets('Update PopupMenuItem layout while the menu is visible', (WidgetTester tester) async {
+    final Key popupMenuButtonKey = UniqueKey();
+    final Type menuItemType = const PopupMenuItem<String>(child: Text('item')).runtimeType;
+
+    Widget buildFrame({
+      TextDirection textDirection = TextDirection.ltr,
+      double fontSize = 24,
+    }) {
+      return MaterialApp(
+        builder: (BuildContext context, Widget child) {
+          return Directionality(
+            textDirection: textDirection,
+            child: PopupMenuTheme(
+              data: PopupMenuTheme.of(context).copyWith(
+                textStyle: Theme.of(context).textTheme.subhead.copyWith(fontSize: fontSize),
+              ),
+              child: child,
+            ),
+          );
+        },
+        home: Scaffold(
+          body: PopupMenuButton<String>(
+            key: popupMenuButtonKey,
+            child: const Text('button'),
+            onSelected: (String result) { },
+            itemBuilder: (BuildContext context) {
+              return <PopupMenuEntry<String>>[
+                const PopupMenuItem<String>(
+                  value: '0',
+                  child: Text('Item 0'),
+                ),
+                const PopupMenuItem<String>(
+                  value: '1',
+                  child: Text('Item 1'),
+                ),
+              ];
+            },
+          ),
+        ),
+      );
+    }
+
+    // Show the menu
+    await tester.pumpWidget(buildFrame());
+    await tester.tap(find.byKey(popupMenuButtonKey));
+    await tester.pumpAndSettle();
+
+    // The menu items should have their default heights and horizontal alignment.
+    expect(tester.getSize(find.widgetWithText(menuItemType, 'Item 0')).height, 48);
+    expect(tester.getSize(find.widgetWithText(menuItemType, 'Item 1')).height, 48);
+    expect(tester.getTopLeft(find.text('Item 0')).dx, 24);
+    expect(tester.getTopLeft(find.text('Item 1')).dx, 24);
+
+    // While the menu is up, change its font size to 64 (default is 16).
+    await tester.pumpWidget(buildFrame(fontSize: 64));
+    await tester.pumpAndSettle(); // Theme changes are animated.
+    expect(tester.getSize(find.widgetWithText(menuItemType, 'Item 0')).height, 128);
+    expect(tester.getSize(find.widgetWithText(menuItemType, 'Item 1')).height, 128);
+    expect(tester.getSize(find.text('Item 0')).height, 128);
+    expect(tester.getSize(find.text('Item 1')).height, 128);
+    expect(tester.getTopLeft(find.text('Item 0')).dx, 24);
+    expect(tester.getTopLeft(find.text('Item 1')).dx, 24);
+
+    // While the menu is up, change the textDirection to rtl. Now menu items
+    // will be aligned right.
+    await tester.pumpWidget(buildFrame(textDirection: TextDirection.rtl));
+    await tester.pumpAndSettle(); // Theme changes are animated.
+    expect(tester.getSize(find.widgetWithText(menuItemType, 'Item 0')).height, 48);
+    expect(tester.getSize(find.widgetWithText(menuItemType, 'Item 1')).height, 48);
+    expect(tester.getTopLeft(find.text('Item 0')).dx, 72);
+    expect(tester.getTopLeft(find.text('Item 1')).dx, 72);
+  });
 }
 
 class TestApp extends StatefulWidget {
