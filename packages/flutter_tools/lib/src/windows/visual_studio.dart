5d07e402b99b1a8507966daff42db11e5dcd7649
Francisco Madgaleno <franciscojma86@gmail.com>
2019-09-18 15:29:25 -0700

    Adds disclaimer unconditionally

--- packages/flutter_tools/lib/src/windows/visual_studio.dart
+++ packages/flutter_tools/lib/src/windows/visual_studio.dart
@@ -127,12 +127,11 @@ class VisualStudio {
     // component name is significantly different in different versions.
     // Default to the latest known description, but use a specific string
     // if a known older version is requested.
-    String cppToolchainDescription = 'MSVC v142 - VS 2019 C++ x64/x86 build tools\n'
-      ' - If there are multiple versions, install the latest one';
+    String cppToolchainDescription = 'MSVC v142 - VS 2019 C++ x64/x86 build tools';
     if (visualStudioMajorVersion == 15) {
       cppToolchainDescription = 'VC++ 2017 version 15.9 v14.16 latest v141 tools';
     }
-
+    cppToolchainDescription += '\n - If there are multiple versions, install the latest one';
     return <String, String>{
       // The MSBuild tool and related command-line toolchain.
       'Microsoft.Component.MSBuild': 'MSBuild',
