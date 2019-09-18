8814f6865c6ed93790726863cf8addb7fea18fe1
David Iglesias <ditman@gmail.com>
2019-09-18 10:45:43 -0700

    Ensure DRRects without corners also draw. (#12330)
    Uncovered this bug running some additional engine tests. Backported the
    test here, and fixed the recording_canvas code.

--- lib/web_ui/test/engine/recording_canvas_test.dart
+++ lib/web_ui/test/engine/recording_canvas_test.dart
@@ -25,15 +25,12 @@ void main() {
     test('Happy case', () {
       underTest.drawDRRect(rrect, rrect.deflate(1), somePaint);
       underTest.apply(mockCanvas);
-      // Expect drawDRRect to be called
-      expect(mockCanvas.methodCallLog.length, equals(1));
-      MockCanvasCall mockCall = mockCanvas.methodCallLog[0];
-      expect(mockCall.methodName, equals('drawDRRect'));
-      expect(mockCall.arguments, equals({
+
+      _expectDrawCall(mockCanvas, {
           'outer': rrect,
           'inner': rrect.deflate(1),
           'paint': somePaint.webOnlyPaintData,
-        }));
+        });
     });
 
     test('Inner RRect > Outer RRect', () {
@@ -56,5 +53,27 @@ void main() {
       // Expect nothing to be called
       expect(mockCanvas.methodCallLog.length, equals(0));
     });
+
+    test('preserve old scuba test behavior', () {
+      final RRect outer = RRect.fromRectAndCorners(const Rect.fromLTRB(10, 20, 30, 40));
+      final RRect inner = RRect.fromRectAndCorners(const Rect.fromLTRB(12, 22, 28, 38));
+
+      underTest.drawDRRect(outer, inner, somePaint);
+      underTest.apply(mockCanvas);
+
+      _expectDrawCall(mockCanvas, {
+          'outer': outer,
+          'inner': inner,
+          'paint': somePaint.webOnlyPaintData,
+        });
+    });
   });
+}
+
+// Expect a drawDRRect call to be registered in the mock call log, with the expectedArguments
+void _expectDrawCall(MockEngineCanvas mock, Map<String, dynamic> expectedArguments) {
+  expect(mock.methodCallLog.length, equals(1));
+  MockCanvasCall mockCall = mock.methodCallLog[0];
+  expect(mockCall.methodName, equals('drawDRRect'));
+  expect(mockCall.arguments, equals(expectedArguments));
 }
