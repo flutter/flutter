4dc5f9eed668eebcbb14bc37925aaf5f6f8ad164
LongCat is Looong <31859944+LongCatIsLooong@users.noreply.github.com>
2019-09-18 13:48:14 -0700

    remove unnecessary import

--- packages/flutter/lib/src/widgets/scrollbar.dart
+++ packages/flutter/lib/src/widgets/scrollbar.dart
@@ -7,7 +7,6 @@ import 'dart:math' as math;
 import 'package:flutter/animation.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/rendering.dart';
-import 'package:flutter/widgets.dart';
 
 import 'scroll_metrics.dart';
 
