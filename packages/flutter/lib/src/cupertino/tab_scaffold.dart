2c6147f4af0794912da90f86af11a7cb5a542d54
LongCat is Looong <31859944+LongCatIsLooong@users.noreply.github.com>
2019-09-18 14:46:40 -0700

    tab scaffold

--- packages/flutter/lib/src/cupertino/tab_scaffold.dart
+++ packages/flutter/lib/src/cupertino/tab_scaffold.dart
@@ -4,6 +4,7 @@
 
 import 'package:flutter/widgets.dart';
 import 'bottom_tab_bar.dart';
+import 'colors.dart';
 import 'theme.dart';
 
 /// Coordinates tab selection between a [CupertinoTabBar] and a [CupertinoTabScaffold].
@@ -423,7 +424,8 @@ class _CupertinoTabScaffoldState extends State<CupertinoTabScaffold> {
 
     return DecoratedBox(
       decoration: BoxDecoration(
-        color: widget.backgroundColor ?? CupertinoTheme.of(context).scaffoldBackgroundColor,
+        color: CupertinoDynamicColor.resolve(widget.backgroundColor, context)
+            ?? CupertinoTheme.of(context).scaffoldBackgroundColor,
       ),
       child: Stack(
         children: stacked,
--- packages/flutter/test/cupertino/material/tab_scaffold_test.dart
+++ packages/flutter/test/cupertino/material/tab_scaffold_test.dart
@@ -174,6 +174,53 @@ void main() {
     expect(tab2.text.style.color, CupertinoColors.destructiveRed);
   });
 
+  testWidgets('dark mode background color', (WidgetTester tester) async {
+    const CupertinoDynamicColor backgroundColor = CupertinoDynamicColor.withBrightness(
+      color: Color(0xFF123456),
+      darkColor: Color(0xFF654321),
+    );
+    await tester.pumpWidget(
+      CupertinoApp(
+        theme: const CupertinoThemeData(brightness: Brightness.light),
+        home: CupertinoTabScaffold(
+          backgroundColor: backgroundColor,
+          tabBar: _buildTabBar(),
+          tabBuilder: (BuildContext context, int index) {
+            return const Placeholder();
+          },
+        ),
+      ),
+    );
+
+    BoxDecoration tabDecoration = tester.widget<DecoratedBox>(find.descendant(
+      of: find.byType(CupertinoTabBar),
+      matching: find.byType(DecoratedBox),
+    )).decoration;
+
+    expect(tabDecoration.color.value, backgroundColor.color.value);
+
+    // Dark mode
+    await tester.pumpWidget(
+      CupertinoApp(
+        theme: const CupertinoThemeData(brightness: Brightness.dark),
+        home: CupertinoTabScaffold(
+          backgroundColor: backgroundColor,
+          tabBar: _buildTabBar(),
+          tabBuilder: (BuildContext context, int index) {
+            return const Placeholder();
+          },
+        ),
+      ),
+    );
+
+    tabDecoration = tester.widget<DecoratedBox>(find.descendant(
+        of: find.byType(CupertinoTabBar),
+        matching: find.byType(DecoratedBox),
+    )).decoration;
+
+    expect(tabDecoration.color.value, backgroundColor.darkColor.value);
+  });
+
   testWidgets('Does not lose state when focusing on text input', (WidgetTester tester) async {
     // Regression testing for https://github.com/flutter/flutter/issues/28457.
 
