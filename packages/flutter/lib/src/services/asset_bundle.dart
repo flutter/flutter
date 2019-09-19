89f39ed8256d7aeb212e7511d50fbe989d2d2794
Alexander Aprelev <aam@google.com>
2019-09-18 20:38:54 -0700

    Use separate isolate for image loading. (#34188)
    * Use separate isolate for image loading. Use TransferableTypedData to const-cost receive bytes from that isolate.

--- packages/flutter/lib/src/services/asset_bundle.dart
+++ packages/flutter/lib/src/services/asset_bundle.dart
@@ -5,6 +5,7 @@
 import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
+import 'dart:isolate';
 import 'dart:typed_data';
 
 import 'package:flutter/foundation.dart';
@@ -120,8 +121,8 @@ class NetworkAssetBundle extends AssetBundle {
         'Unable to load asset: $key\n'
         'HTTP status code: ${response.statusCode}'
       );
-    final Uint8List bytes = await consolidateHttpClientResponseBytes(response);
-    return bytes.buffer.asByteData();
+    final TransferableTypedData transferable = await getHttpClientResponseBytes(response);
+    return transferable.materialize().asByteData();
   }
 
   /// Retrieve a string from the asset bundle, parse it with the given function,
