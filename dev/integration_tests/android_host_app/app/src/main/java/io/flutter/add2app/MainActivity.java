4b3bd766f630506eab87fa3b6c3ab1c60c44b699
Emmanuel Garcia <egarciad@google.com>
2019-09-20 08:29:21 -0700

    Use AndroidX in new projects by default (#40925)

--- dev/integration_tests/android_host_app/app/src/main/java/io/flutter/add2app/MainActivity.java
+++ dev/integration_tests/android_host_app/app/src/main/java/io/flutter/add2app/MainActivity.java
@@ -1,7 +1,7 @@
 package io.flutter.add2app;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
 import io.flutter.facade.Flutter;
 
 public class MainActivity extends AppCompatActivity {
