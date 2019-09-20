42483fe71a31e92426ff012c7074d278efb82424
Michael Goderbauer <goderbauer@google.com>
2019-09-20 10:10:00 -0700

    Update packages/flutter/lib/src/widgets/transitions.dart
    Co-Authored-By: Kate Lovett <katelovett@google.com>

--- packages/flutter/lib/src/widgets/transitions.dart
+++ packages/flutter/lib/src/widgets/transitions.dart
@@ -926,7 +926,7 @@ class DefaultTextStyleTransition extends AnimatedWidget {
 /// ```
 /// {@end-tool}
 ///
-/// {@animation 300 300 https://flutter.github.io/assets-for-api-docs/assets/widgets/aniamted_builder.mp4}
+/// {@animation 300 300 https://flutter.github.io/assets-for-api-docs/assets/widgets/animated_builder.mp4}
 ///
 /// See also:
 ///
