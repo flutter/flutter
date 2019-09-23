a5007c4de2fc61654c1f770f6188d04552f1b1d5
Emmanuel Garcia <egarciad@google.com>
2019-09-23 13:43:26 -0700

    Add embedding as API dependency instead of compile only dependency

---
+++ dev/integration_tests/abstract_method_smoke_test/lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_inappbrowser/flutter_inappbrowser.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePage createState() => _HomePage();
+}
+
+class _HomePage extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+
+    // Trigger the second route.
+    // https://github.com/flutter/flutter/issues/40126
+    WidgetsBinding.instance.addPostFrameCallback((_) async {
+      Navigator.of(context).push(
+          MaterialPageRoute<void>(builder: (_) => SecondPage()));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  } 
+}
+
+class SecondPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          TextFormField(),
+          const Expanded(
+            child: InAppWebView(
+              initialUrl: 'https://google.com',
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
