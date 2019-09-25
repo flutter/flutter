8c9fa6e6399376b79ae9ef5d85df9eb8795575bf
Kate Lovett <katelovett@google.com>
2019-09-25 13:39:46 -0700

    ++

--- packages/flutter/lib/src/rendering/paragraph.dart
+++ packages/flutter/lib/src/rendering/paragraph.dart
@@ -861,6 +861,7 @@ class RenderParagraph extends RenderBox
           if (info.recognizer is TapGestureRecognizer) {
             final TapGestureRecognizer recognizer = info.recognizer;
             configuration.onTap = recognizer.onTap;
+            configuration.isLink = true;
           } else if (info.recognizer is LongPressGestureRecognizer) {
             final LongPressGestureRecognizer recognizer = info.recognizer;
             configuration.onLongPress = recognizer.onLongPress;
