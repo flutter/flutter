d8c94fcdfc769dd40961701cdb7b56f546c8875b
Jonah Williams <jonahwilliams@google.com>
2019-09-25 15:12:58 -0700

    Support deferred imports on profile/release builds of Flutter Web (#41222)

--- packages/flutter_tools/lib/src/build_runner/web_fs.dart
+++ packages/flutter_tools/lib/src/build_runner/web_fs.dart
@@ -4,10 +4,9 @@
 
 import 'dart:async';
 
+import 'package:archive/archive.dart';
 import 'package:build_daemon/client.dart';
-import 'package:build_daemon/constants.dart';
-import 'package:build_daemon/constants.dart' hide BuildMode;
-import 'package:build_daemon/constants.dart' as daemon show BuildMode;
+import 'package:build_daemon/constants.dart' as daemon;
 import 'package:build_daemon/data/build_status.dart';
 import 'package:build_daemon/data/build_target.dart';
 import 'package:build_daemon/data/server_log.dart';
@@ -89,6 +88,7 @@ class WebFs {
     this._server,
     this._dwds,
     this.uri,
+    this._assetServer,
   );
 
   /// The server uri.
@@ -97,6 +97,7 @@ class WebFs {
   final HttpServer _server;
   final Dwds _dwds;
   final BuildDaemonClient _client;
+  final AssetServer _assetServer;
   StreamSubscription<void> _connectedApps;
 
   static const String _kHostName = 'localhost';
@@ -106,6 +107,7 @@ class WebFs {
     await _dwds?.stop();
     await _server.close(force: true);
     await _connectedApps?.cancel();
+    _assetServer?.dispose();
   }
 
   Future<DebugConnection> _cachedExtensionFuture;
@@ -180,21 +182,22 @@ class WebFs {
     await assetBundle.build();
     await writeBundle(fs.directory(getAssetBuildDirectory()), assetBundle.entries);
 
-    // Initialize the dwds server.
-    final int hostPort = port == null ? await os.findFreePort() : int.tryParse(port);
-    // Map the bootstrap files to the correct package directory.
     final String targetBaseName = fs.path
-      .withoutExtension(target).replaceFirst('lib${fs.path.separator}', '');
+        .withoutExtension(target).replaceFirst('lib${fs.path.separator}', '');
     final Map<String, String> mappedUrls = <String, String>{
       'main.dart.js': 'packages/${flutterProject.manifest.appName}/'
-        '${targetBaseName}_web_entrypoint.dart.js',
+          '${targetBaseName}_web_entrypoint.dart.js',
       '${targetBaseName}_web_entrypoint.dart.js.map': 'packages/${flutterProject.manifest.appName}/'
-        '${targetBaseName}_web_entrypoint.dart.js.map',
+          '${targetBaseName}_web_entrypoint.dart.js.map',
       '${targetBaseName}_web_entrypoint.dart.bootstrap.js': 'packages/${flutterProject.manifest.appName}/'
-        '${targetBaseName}_web_entrypoint.dart.bootstrap.js',
+          '${targetBaseName}_web_entrypoint.dart.bootstrap.js',
       '${targetBaseName}_web_entrypoint.digests': 'packages/${flutterProject.manifest.appName}/'
-        '${targetBaseName}_web_entrypoint.digests',
+          '${targetBaseName}_web_entrypoint.digests',
     };
+
+    // Initialize the dwds server.
+    final int hostPort = port == null ? await os.findFreePort() : int.tryParse(port);
+
     final Pipeline pipeline = const Pipeline().addMiddleware((Handler innerHandler) {
       return (Request request) async {
         // Redirect the main.dart.js to the target file we decided to serve.
@@ -237,9 +240,10 @@ class WebFs {
     } else {
       handler = pipeline.addHandler(proxyHandler('http://localhost:$daemonAssetPort/web/'));
     }
+    final AssetServer assetServer = AssetServer(flutterProject, targetBaseName);
     Cascade cascade = Cascade();
     cascade = cascade.add(handler);
-    cascade = cascade.add(_assetHandler(flutterProject));
+    cascade = cascade.add(assetServer.handle);
     final HttpServer server = await httpMultiServerFactory(hostname ?? _kHostName, hostPort);
     shelf_io.serveRequests(server, cascade.handler);
     return WebFs(
@@ -247,98 +251,136 @@ class WebFs {
       server,
       dwds,
       'http://$_kHostName:$hostPort/',
+      assetServer,
     );
   }
+}
 
-  static Future<Response> Function(Request request) _assetHandler(FlutterProject flutterProject) {
-    final PackageMap packageMap = PackageMap(PackageMap.globalPackagesPath);
-    return (Request request) async {
-      if (request.url.path.contains('stack_trace_mapper')) {
-        final File file = fs.file(fs.path.join(
-          artifacts.getArtifactPath(Artifact.engineDartSdkPath),
+class AssetServer {
+  AssetServer(this.flutterProject, this.targetBaseName);
+
+  final FlutterProject flutterProject;
+  final String targetBaseName;
+  final PackageMap packageMap = PackageMap(PackageMap.globalPackagesPath);
+  Directory partFiles;
+
+  Future<Response> handle(Request request) async {
+    if (request.url.path.contains('stack_trace_mapper')) {
+      final File file = fs.file(fs.path.join(
+        artifacts.getArtifactPath(Artifact.engineDartSdkPath),
+        'lib',
+        'dev_compiler',
+        'web',
+        'dart_stack_trace_mapper.js',
+      ));
+      return Response.ok(file.readAsBytesSync(), headers: <String, String>{
+        'Content-Type': 'text/javascript',
+      });
+    } else if (request.url.path.endsWith('part.js')) {
+      // Lazily unpack any deferred imports in release/profile mode. These are
+      // placed into an archive by build_runner, and are named based on the main
+      // entrypoint + a "part" suffix (Though the actual names are arbitrary).
+      // To make this easier to deal with they are copied into a temp directory.
+      if (partFiles == null) {
+        final File dart2jsArchive = fs.file(fs.path.join(
+          flutterProject.dartTool.path,
+          'build',
+          'flutter_web',
+          '${flutterProject.manifest.appName}',
           'lib',
-          'dev_compiler',
-          'web',
-          'dart_stack_trace_mapper.js',
+          '${targetBaseName}_web_entrypoint.dart.js.tar.gz',
         ));
-        return Response.ok(file.readAsBytesSync(), headers: <String, String>{
-          'Content-Type': 'text/javascript',
-        });
-      } else if (request.url.path.contains('require.js')) {
-        final File file = fs.file(fs.path.join(
-          artifacts.getArtifactPath(Artifact.engineDartSdkPath),
-          'lib',
-          'dev_compiler',
-          'kernel',
-          'amd',
-          'require.js',
-        ));
-        return Response.ok(file.readAsBytesSync(), headers: <String, String>{
-          'Content-Type': 'text/javascript',
-        });
-      } else if (request.url.path.endsWith('dart_sdk.js')) {
-        final File file = fs.file(fs.path.join(
-          artifacts.getArtifactPath(Artifact.flutterWebSdk),
-          'kernel',
-          'amd',
-          'dart_sdk.js',
-        ));
-        return Response.ok(file.readAsBytesSync(), headers: <String, String>{
-          'Content-Type': 'text/javascript',
-        });
-      } else if (request.url.path.endsWith('dart_sdk.js.map')) {
-        final File file = fs.file(fs.path.join(
-          artifacts.getArtifactPath(Artifact.flutterWebSdk),
-          'kernel',
-          'amd',
-          'dart_sdk.js.map',
-        ));
-        return Response.ok(file.readAsBytesSync());
-      } else if (request.url.path.endsWith('.dart')) {
-        // This is likely a sourcemap request. The first segment is the
-        // package name, and the rest is the path to the file relative to
-        // the package uri. For example, `foo/bar.dart` would represent a
-        // file at a path like `foo/lib/bar.dart`. If there is no leading
-        // segment, then we assume it is from the current package.
-
-        // Handle sdk requests that have mangled urls from engine build.
-        if (request.url.path.contains('flutter_web_sdk')) {
-          // Note: the request is a uri and not a file path, so they always use `/`.
-          final String sdkPath = fs.path.joinAll(request.url.path.split('flutter_web_sdk/').last.split('/'));
-          final String webSdkPath = artifacts.getArtifactPath(Artifact.flutterWebSdk);
-          return Response.ok(fs.file(fs.path.join(webSdkPath, sdkPath)).readAsBytesSync());
-        }
-
-        final String packageName = request.url.pathSegments.length == 1
-          ? flutterProject.manifest.appName
-          : request.url.pathSegments.first;
-        String filePath = fs.path.joinAll(request.url.pathSegments.length == 1
-          ? request.url.pathSegments
-          : request.url.pathSegments.skip(1));
-        String packagePath = packageMap.map[packageName]?.toFilePath(windows: platform.isWindows);
-        // If the package isn't found, then we have an issue with relative
-        // paths within the main project.
-        if (packagePath == null) {
-          packagePath = packageMap.map[flutterProject.manifest.appName]
-            .toFilePath(windows: platform.isWindows);
-          filePath = request.url.path;
-        }
-        final File file = fs.file(fs.path.join(packagePath, filePath));
-        if (file.existsSync()) {
-          return Response.ok(file.readAsBytesSync());
-        }
-        return Response.notFound('');
-      } else if (request.url.path.contains('assets')) {
-        final String assetPath = request.url.path.replaceFirst('assets/', '');
-        final File file = fs.file(fs.path.join(getAssetBuildDirectory(), assetPath));
-        if (file.existsSync()) {
-          return Response.ok(file.readAsBytesSync());
-        } else {
-          return Response.notFound('');
+        if (dart2jsArchive.existsSync()) {
+          final Archive archive = TarDecoder().decodeBytes(dart2jsArchive.readAsBytesSync());
+          partFiles = fs.systemTempDirectory.createTempSync('_flutter_tool')
+            ..createSync();
+          for (ArchiveFile file in archive) {
+            partFiles.childFile(file.name).writeAsBytesSync(file.content);
+          }
         }
       }
+      final String fileName = fs.path.basename(request.url.path);
+      return Response.ok(partFiles.childFile(fileName).readAsBytesSync(), headers: <String, String>{
+        'Content-Type': 'text/javascript',
+      });
+    } else if (request.url.path.contains('require.js')) {
+      final File file = fs.file(fs.path.join(
+        artifacts.getArtifactPath(Artifact.engineDartSdkPath),
+        'lib',
+        'dev_compiler',
+        'kernel',
+        'amd',
+        'require.js',
+      ));
+      return Response.ok(file.readAsBytesSync(), headers: <String, String>{
+        'Content-Type': 'text/javascript',
+      });
+    } else if (request.url.path.endsWith('dart_sdk.js')) {
+      final File file = fs.file(fs.path.join(
+        artifacts.getArtifactPath(Artifact.flutterWebSdk),
+        'kernel',
+        'amd',
+        'dart_sdk.js',
+      ));
+      return Response.ok(file.readAsBytesSync(), headers: <String, String>{
+        'Content-Type': 'text/javascript',
+      });
+    } else if (request.url.path.endsWith('dart_sdk.js.map')) {
+      final File file = fs.file(fs.path.join(
+        artifacts.getArtifactPath(Artifact.flutterWebSdk),
+        'kernel',
+        'amd',
+        'dart_sdk.js.map',
+      ));
+      return Response.ok(file.readAsBytesSync());
+    } else if (request.url.path.endsWith('.dart')) {
+      // This is likely a sourcemap request. The first segment is the
+      // package name, and the rest is the path to the file relative to
+      // the package uri. For example, `foo/bar.dart` would represent a
+      // file at a path like `foo/lib/bar.dart`. If there is no leading
+      // segment, then we assume it is from the current package.
+
+      // Handle sdk requests that have mangled urls from engine build.
+      if (request.url.path.contains('flutter_web_sdk')) {
+        // Note: the request is a uri and not a file path, so they always use `/`.
+        final String sdkPath = fs.path.joinAll(request.url.path.split('flutter_web_sdk/').last.split('/'));
+        final String webSdkPath = artifacts.getArtifactPath(Artifact.flutterWebSdk);
+        return Response.ok(fs.file(fs.path.join(webSdkPath, sdkPath)).readAsBytesSync());
+      }
+
+      final String packageName = request.url.pathSegments.length == 1
+          ? flutterProject.manifest.appName
+          : request.url.pathSegments.first;
+      String filePath = fs.path.joinAll(request.url.pathSegments.length == 1
+          ? request.url.pathSegments
+          : request.url.pathSegments.skip(1));
+      String packagePath = packageMap.map[packageName]?.toFilePath(windows: platform.isWindows);
+      // If the package isn't found, then we have an issue with relative
+      // paths within the main project.
+      if (packagePath == null) {
+        packagePath = packageMap.map[flutterProject.manifest.appName]
+            .toFilePath(windows: platform.isWindows);
+        filePath = request.url.path;
+      }
+      final File file = fs.file(fs.path.join(packagePath, filePath));
+      if (file.existsSync()) {
+        return Response.ok(file.readAsBytesSync());
+      }
       return Response.notFound('');
-    };
+    } else if (request.url.path.contains('assets')) {
+      final String assetPath = request.url.path.replaceFirst('assets/', '');
+      final File file = fs.file(fs.path.join(getAssetBuildDirectory(), assetPath));
+      if (file.existsSync()) {
+        return Response.ok(file.readAsBytesSync());
+      } else {
+        return Response.notFound('');
+      }
+    }
+    return Response.notFound('');
+  }
+
+  void dispose() {
+    partFiles?.deleteSync(recursive: true);
   }
 }
 
@@ -459,7 +501,7 @@ class BuildDaemonCreator {
 
   /// Retrieve the asset server port for the current daemon.
   int assetServerPort(Directory workingDirectory) {
-    final String portFilePath = fs.path.join(daemonWorkspace(workingDirectory.path), '.asset_server_port');
+    final String portFilePath = fs.path.join(daemon.daemonWorkspace(workingDirectory.path), '.asset_server_port');
     return int.tryParse(fs.file(portFilePath).readAsStringSync());
   }
 }
