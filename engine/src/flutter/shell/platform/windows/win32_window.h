9c8f5d8074efb150765a53aa203e4ffa8058a0d1
Francisco Magdaleno <franciscojma86@gmail.com>
2019-09-26 15:23:58 -0700

    Adds support for 5 mouse buttons (flutter/engine#12450)

--- engine/src/flutter/shell/platform/windows/win32_window.h
+++ engine/src/flutter/shell/platform/windows/win32_window.h
@@ -15,6 +15,22 @@
 
 namespace flutter {
 
+// Struct holding the mouse state. The engine doesn't keep track of which mouse
+// buttons have been pressed, so it's the embedding's responsibility.
+struct MouseState {
+  // True if the last event sent to Flutter had at least one mouse button
+  // pressed.
+  bool flutter_state_is_down = false;
+
+  // True if kAdd has been sent to Flutter. Used to determine whether
+  // to send a kAdd event before sending an incoming mouse event, since Flutter
+  // expects pointers to be added before events are sent for them.
+  bool flutter_state_is_added = false;
+
+  // The currently pressed buttons, as represented in FlutterPointerEvent.
+  uint64_t buttons = 0;
+};
+
 // A class abstraction for a high DPI aware Win32 Window.  Intended to be
 // inherited from by classes that wish to specialize with custom
 // rendering and input handling.
@@ -81,12 +97,12 @@ class Win32Window {
   // window bounds.
   virtual void OnPointerMove(double x, double y) = 0;
 
-  // Called when the left mouse button goes down
-  virtual void OnPointerDown(double x, double y) = 0;
+  // Called when the a mouse button, determined by |button|, goes down.
+  virtual void OnPointerDown(double x, double y, UINT button) = 0;
 
-  // Called when the left mouse button goes from
+  // Called when the a mouse button, determined by |button|, goes from
   // down to up
-  virtual void OnPointerUp(double x, double y) = 0;
+  virtual void OnPointerUp(double x, double y, UINT button) = 0;
 
   // Called when the mouse leaves the window.
   virtual void OnPointerLeave() = 0;
@@ -111,6 +127,27 @@ class Win32Window {
   UINT GetCurrentWidth();
 
   UINT GetCurrentHeight();
+
+  // Gets the current mouse state.
+  MouseState GetMouseState() { return mouse_state_; }
+
+  // Resets the mouse state to its default values.
+  void ResetMouseState() { mouse_state_ = MouseState(); }
+
+  // Updates the mouse state to whether the last event to Flutter had at least
+  // one mouse button pressed.
+  void SetMouseFlutterStateDown(bool is_down) {
+    mouse_state_.flutter_state_is_down = is_down;
+  }
+
+  // Updates the mouse state to whether the last event to Flutter was a kAdd
+  // event.
+  void SetMouseFlutterStateAdded(bool is_added) {
+    mouse_state_.flutter_state_is_added = is_added;
+  }
+
+  // Updates the currently pressed buttons.
+  void SetMouseButtons(uint64_t buttons) { mouse_state_.buttons = buttons; }
 
  private:
   // Activates tracking for a "mouse leave" event.
@@ -142,6 +179,9 @@ class Win32Window {
 
   // Set to true to be notified when the mouse leaves the window.
   bool tracking_mouse_leave_ = false;
+
+  // Keeps track of mouse state in relation to the window.
+  MouseState mouse_state_;
 };
 
 }  // namespace flutter
