2362ec5b236365fc1d242b6bf7ff3112950f07a6
Jonah Williams <jonahwilliams@google.com>
2019-09-26 15:57:57 -0700

    Flutter build bundle without --precompiled should always perform a debug build. (#41401)

--- packages/flutter_tools/lib/src/commands/build_bundle.dart
+++ packages/flutter_tools/lib/src/commands/build_bundle.dart
@@ -21,7 +21,10 @@ class BuildBundleCommand extends BuildSubCommand {
     usesBuildNumberOption();
     addBuildModeFlags(verboseHelp: verboseHelp);
     argParser
-      ..addFlag('precompiled', negatable: false)
+      ..addFlag('precompiled', negatable: false, help: 'If not provided, then '
+        'a debug build is always provided, regardless of build mode. If provided '
+        'then release is the default mode.'
+      )
       // This option is still referenced by the iOS build scripts. We should
       // remove it once we've updated those build scripts.
       ..addOption('asset-base', help: 'Ignored. Will be removed.', hide: !verboseHelp)
