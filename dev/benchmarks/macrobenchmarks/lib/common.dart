868382ac5fe8a5e63e4871e15eceb19c35c544da
liyuqian <liyuqian@google.com>
2019-09-29 10:57:19 -0700

    Revert "Reland "Measure iOS CPU/GPU percentage (#41234)" (#41426)"
    This reverts commit 67b5bdf99b19e5f04bae5fb82af91aa695fbfbb9.

--- dev/benchmarks/macrobenchmarks/lib/common.dart
+++ dev/benchmarks/macrobenchmarks/lib/common.dart
@@ -5,4 +5,3 @@
 const String kCullOpacityRouteName = '/cull_opacity';
 const String kCubicBezierRouteName = '/cubic_bezier';
 const String kBackdropFilterRouteName = '/backdrop_filter';
-const String kSimpleAnimationRouteName = '/simple_animation';
