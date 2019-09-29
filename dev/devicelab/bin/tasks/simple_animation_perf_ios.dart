9cc29c61df7dd4600b6bacabb10cc441c8429f9b
liyuqian <liyuqian@google.com>
2019-09-29 11:33:43 -0700

    Reland "Measure iOS CPU/GPU percentage #41426" (#41578)
    This reverts commit baea9bf7cc7447b5ec6e6d377a68b17e79cb9ccf.
    
    Additionally,   we let the test run on mac8 with iphonexs because the test won't run on Xcode 10.1 (mac3-7). Hence we force it to run on mac8 which currently has Xcode 10.2.

--- dev/devicelab/bin/tasks/simple_animation_perf_ios.dart
+++ dev/devicelab/bin/tasks/simple_animation_perf_ios.dart
@@ -2,13 +2,12 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'dart:async';
 
-import 'package:flutter_devicelab/tasks/perf_tests.dart';
 import 'package:flutter_devicelab/framework/adb.dart';
 import 'package:flutter_devicelab/framework/framework.dart';
+import 'package:flutter_devicelab/tasks/perf_tests.dart';
 
 Future<void> main() async {
   deviceOperatingSystem = DeviceOperatingSystem.ios;
-  await task(createBackdropFilterPerfTest(needsMeasureCpuGpu: true));
+  await task(createSimpleAnimationPerfTest(needsMeasureCpuGpu: true));
 }
