0a4b5afcc5024235a632872624f5ead9a1ce4b8b
Shi Hao Hong <shihaohong94@gmail.com>
2019-09-30 15:26:22 -0700

    Code review feedback

--- packages/flutter/lib/src/material/dropdown.dart
+++ packages/flutter/lib/src/material/dropdown.dart
@@ -710,13 +710,13 @@ class DropdownButton<T> extends StatefulWidget {
   /// The text style to use for text in the dropdown button and the dropdown
   /// menu that appears when you tap the button.
   ///
-  /// To use a separate text style for the dropdown button, consider using
-  /// [selectedItemBuilder].
+  /// To use a separate text style for selected item when it's displayed within
+  /// the dropdown button,, consider using [selectedItemBuilder].
   ///
   /// {@tool snippet --template=stateful_widget_scaffold}
   ///
   /// This sample shows a `DropdownButton` with a dropdown button text style
-  /// that is unique from its menu items.
+  /// that is different than its menu items.
   ///
   /// ```dart
   /// List<String> options = <String>['One', 'Two', 'Free', 'Four'];
@@ -725,33 +725,32 @@ class DropdownButton<T> extends StatefulWidget {
   /// @override
   /// Widget build(BuildContext context) {
   ///   return Container(
+  ///     alignment: Alignment.center,
   ///     color: Colors.blue,
-  ///     child: Center(
-  ///       child: DropdownButton<String>(
-  ///         value: dropdownValue,
-  ///         onChanged: (String newValue) {
-  ///           setState(() {
-  ///             dropdownValue = newValue;
-  ///           });
-  ///         },
-  ///         style: TextStyle(color: Colors.blue),
-  ///         selectedItemBuilder: (BuildContext context) {
-  ///           return options.map((String value) {
-  ///             return Text(
-  ///               dropdownValue,
-  ///               style: TextStyle(color: Colors.white),
-  ///             );
-  ///           }).toList();
-  ///         },
-  ///         items: options.map<DropdownMenuItem<String>>((String value) {
-  ///           return DropdownMenuItem<String>(
-  ///             value: value,
-  ///             child: Text(value),
+  ///     child: DropdownButton<String>(
+  ///       value: dropdownValue,
+  ///       onChanged: (String newValue) {
+  ///         setState(() {
+  ///           dropdownValue = newValue;
+  ///         });
+  ///       },
+  ///       style: TextStyle(color: Colors.blue),
+  ///       selectedItemBuilder: (BuildContext context) {
+  ///         return options.map((String value) {
+  ///           return Text(
+  ///             dropdownValue,
+  ///             style: TextStyle(color: Colors.white),
   ///           );
-  ///         }).toList(),
-  ///       ),
+  ///         }).toList();
+  ///       },
+  ///       items: options.map<DropdownMenuItem<String>>((String value) {
+  ///         return DropdownMenuItem<String>(
+  ///           value: value,
+  ///           child: Text(value),
+  ///         );
+  ///       }).toList(),
   ///     ),
-  ///   ),
+  ///   );
   /// }
   /// ```
   /// {@end-tool}
