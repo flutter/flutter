a3d56a210425acee5648d58aa637d491551e1c75
Shi Hao Hong <shihaohong94@gmail.com>
2019-09-30 11:19:25 -0700

    Update DefaultTabController to allow for zero tabs

--- packages/flutter/test/material/tabs_test.dart
+++ packages/flutter/test/material/tabs_test.dart
@@ -2350,4 +2350,76 @@ void main() {
     expect(find.text('Tab1'), findsOneWidget);
     expect(find.text('Tab2'), findsOneWidget);
   });
+
+  testWidgets('DefaultTabController should allow for a length of zero', (WidgetTester tester) async {
+    List<String> tabTextContent = <String>[];
+
+    await tester.pumpWidget(
+      MaterialApp(
+        home: StatefulBuilder(
+          builder: (BuildContext context, StateSetter setState) {
+            return DefaultTabController(
+              length: tabTextContent.length,
+              child: Scaffold(
+                appBar: AppBar(
+                  title: const Text('Default TabBar Preview'),
+                  bottom: tabTextContent.isNotEmpty
+                    ? TabBar(
+                      isScrollable: true,
+                      tabs: tabTextContent.map((String textContent) {
+                        return Tab(text: textContent);
+                      }).toList(),
+                    )
+                    : null,
+                ),
+                body: tabTextContent.isNotEmpty
+                  ? TabBarView(
+                    children: tabTextContent.map((String textContent) {
+                      return Tab(text: '$textContent\'s view');
+                    }).toList())
+                  : const Center(child: Text('No tabs')),
+                bottomNavigationBar: BottomAppBar(
+                  child: Row(
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      IconButton(
+                        key: const Key('Add tab'),
+                        icon: const Icon(Icons.add),
+                        onPressed: () {
+                          setState(() {
+                            tabTextContent = List<String>.from(tabTextContent)
+                              ..add('Tab ${tabTextContent.length + 1}');
+                          });
+                        },
+                      ),
+                      IconButton(
+                        key: const Key('Del tab'),
+                        icon: const Icon(Icons.delete),
+                        onPressed: () {
+                          setState(() {
+                            tabTextContent = List<String>.from(tabTextContent)
+                              ..removeLast();
+                          });
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+
+    expect(find.text('No tabs'), findsOneWidget);
+    await tester.tap(find.byKey(const Key('Add tab')));
+    await tester.pumpAndSettle();
+    expect(find.text('Tab 1'), findsOneWidget);
+    expect(find.text('Tab 1\'s view'), findsOneWidget);
+    await tester.tap(find.byKey(const Key('Del tab')));
+    await tester.pumpAndSettle();
+    expect(find.text('No tabs'), findsOneWidget);
+  });
 }
