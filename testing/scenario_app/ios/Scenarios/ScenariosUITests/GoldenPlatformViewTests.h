bf620dc56860d696b387c09e80f824d8ba480e15
Chris Yang <ychris@google.com>
2019-10-02 01:51:31 +0800

    Reland "Add iOS platform view mutation XCUITests to the scenario app(#11652)" (#12707)

---
+++ testing/scenario_app/ios/Scenarios/ScenariosUITests/GoldenPlatformViewTests.h
@@ -0,0 +1,30 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import <XCTest/XCTest.h>
+#import "PlatformViewGoldenTestManager.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+// The base class of all the PlatformView golden tests.
+//
+// A new PlatformView golden tests can subclass this and override the `-initiWithInvocation:`
+// method, which then retun the `-initWithManager:invocation:`
+//
+// Then in any test method, call `checkGolden` to perform the golden test.
+//
+// This base class doesn't run any test case on its own.
+@interface GoldenPlatformViewTests : XCTestCase
+
+// Initialize with a `PlatformViewGoldenTestManager`.
+- (instancetype)initWithManager:(PlatformViewGoldenTestManager*)manager
+                     invocation:(NSInvocation*)invocation;
+
+// Take a sceenshot of the test app and check it has the same pixels with goldenImage inside the
+// `PlatformViewGoldenTestManager`.
+- (void)checkGolden;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ testing/scenario_app/ios/Scenarios/ScenariosUITests/PlatformViewGoldenTestManager.h
@@ -0,0 +1,29 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import <Foundation/Foundation.h>
+#import "GoldenImage.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+extern NSDictionary* launchArgsMap;
+
+// Manages a `GoldenPlatformViewTests`.
+//
+// It creates the correct `identifer` based on the `launchArg`.
+// It also generates the correct GoldenImage based on the `identifier`.
+@interface PlatformViewGoldenTestManager : NSObject
+
+@property(readonly, strong, nonatomic) GoldenImage* goldenImage;
+@property(readonly, copy, nonatomic) NSString* identifier;
+@property(readonly, copy, nonatomic) NSString* launchArg;
+
+// Initilize with launchArg.
+//
+// Crahes if the launchArg is not mapped in `Appdelegate.launchArgsMap`.
+- (instancetype)initWithLaunchArg:(NSString*)launchArg;
+
+@end
+
+NS_ASSUME_NONNULL_END
