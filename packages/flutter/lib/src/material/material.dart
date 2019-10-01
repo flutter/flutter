f0bc78163ffe948980c54d3777aaebf83ab7974f
JoseAlba <jaguado@google.com>
2019-10-01 11:21:51 +0200

    Updated the dark mode overlay from Colors.white to colorScheme.onSurface

--- packages/flutter/lib/src/material/material.dart
+++ packages/flutter/lib/src/material/material.dart
@@ -330,7 +330,7 @@ Color _elevationOverlayColor(BuildContext context, Color background, double elev
     // This formula matches the values in the spec:
     // https://material.io/design/color/dark-theme.html#properties
     final double opacity = (4.5 * math.log(elevation + 1) + 2) / 100.0;
-    final Color overlay = Colors.white.withOpacity(opacity);
+    final Color overlay = theme.colorScheme.onSurface.withOpacity(opacity);
     return Color.alphaBlend(overlay, background);
   }
   return background;
