e9edb8e14f6d3bdc8d63b202f59fa35f8a7f7a32
Mouad Debbar <mouad.debbar@gmail.com>
2019-10-02 15:09:08 -0700

    Support correct keymap for web (#12712)

--- lib/web_ui/test/keyboard_test.dart
+++ lib/web_ui/test/keyboard_test.dart
@@ -3,6 +3,7 @@
 // found in the LICENSE file.
 
 import 'dart:html' as html;
+import 'dart:js_util' as js_util;
 import 'dart:typed_data';
 
 import 'package:ui/src/engine.dart';
@@ -15,7 +16,7 @@ void main() {
     test('initializes', () {
       expect(Keyboard.instance, isNull);
       Keyboard.initialize();
-      expect(Keyboard.instance, isNotNull);
+      expect(Keyboard.instance, isA<Keyboard>());
     });
 
     test('dispatches keyup to flutter/keyevent channel', () {
@@ -27,17 +28,16 @@ void main() {
         dataReceived = const JSONMessageCodec().decodeMessage(data);
       };
 
-      html.window.dispatchEvent(html.KeyboardEvent('keyup'));
+      dispatchKeyboardEvent('keyup', key: 'SomeKey', code: 'SomeCode');
 
       expect(channelReceived, 'flutter/keyevent');
-      expect(dataReceived['type'], 'keyup');
-      expect(dataReceived['keymap'], 'android');
-
-      // Unfortunately there's no way to fake `keyCode`.
-      expect(dataReceived['keyCode'], 0);
-
-      // Unfortunately there's no way to fake `key`.
-      expect(dataReceived, isNot(contains('codePoint')));
+      expect(dataReceived, <String, dynamic>{
+        'type': 'keyup',
+        'keymap': 'web',
+        'code': 'SomeCode',
+        'key': 'SomeKey',
+        'metaState': 0x0,
+      });
     });
 
     test('dispatches keydown to flutter/keyevent channel', () {
@@ -49,17 +49,96 @@ void main() {
         dataReceived = const JSONMessageCodec().decodeMessage(data);
       };
 
-      html.window.dispatchEvent(html.KeyboardEvent('keydown'));
+      dispatchKeyboardEvent('keydown', key: 'SomeKey', code: 'SomeCode');
 
       expect(channelReceived, 'flutter/keyevent');
-      expect(dataReceived['type'], 'keydown');
-      expect(dataReceived['keymap'], 'android');
+      expect(dataReceived, <String, dynamic>{
+        'type': 'keydown',
+        'keymap': 'web',
+        'code': 'SomeCode',
+        'key': 'SomeKey',
+        'metaState': 0x0,
+      });
+    });
 
-      // Unfortunately there's no way to fake `keyCode`.
-      expect(dataReceived['keyCode'], 0);
+    test('dispatches correct meta state', () {
+      Map<String, dynamic> dataReceived;
+      ui.window.onPlatformMessage = (String channel, ByteData data,
+          ui.PlatformMessageResponseCallback callback) {
+        dataReceived = const JSONMessageCodec().decodeMessage(data);
+      };
 
-      // Unfortunately there's no way to fake `key`.
-      expect(dataReceived, isNot(contains('codePoint')));
+      dispatchKeyboardEvent(
+        'keydown',
+        key: 'SomeKey',
+        code: 'SomeCode',
+        isControlPressed: true,
+      );
+      expect(dataReceived, <String, dynamic>{
+        'type': 'keydown',
+        'keymap': 'web',
+        'code': 'SomeCode',
+        'key': 'SomeKey',
+        //          ctrl
+        'metaState': 0x4,
+      });
+
+      dispatchKeyboardEvent(
+        'keydown',
+        key: 'SomeKey',
+        code: 'SomeCode',
+        isShiftPressed: true,
+        isAltPressed: true,
+        isMetaPressed: true,
+      );
+      expect(dataReceived, <String, dynamic>{
+        'type': 'keydown',
+        'keymap': 'web',
+        'code': 'SomeCode',
+        'key': 'SomeKey',
+        //          shift  alt   meta
+        'metaState': 0x1 | 0x2 | 0x8,
+      });
+    });
+
+    test('dispatches repeat events', () {
+      List<Map<String, dynamic>> messages = <Map<String, dynamic>>[];
+      ui.window.onPlatformMessage = (String channel, ByteData data,
+          ui.PlatformMessageResponseCallback callback) {
+        messages.add(const JSONMessageCodec().decodeMessage(data));
+      };
+
+      dispatchKeyboardEvent(
+        'keydown',
+        key: 'SomeKey',
+        code: 'SomeCode',
+        repeat: true,
+      );
+      dispatchKeyboardEvent(
+        'keydown',
+        key: 'SomeKey',
+        code: 'SomeCode',
+        repeat: true,
+      );
+      dispatchKeyboardEvent(
+        'keydown',
+        key: 'SomeKey',
+        code: 'SomeCode',
+        repeat: true,
+      );
+
+      final Map<String, dynamic> expectedMessage = <String, dynamic>{
+        'type': 'keydown',
+        'keymap': 'web',
+        'code': 'SomeCode',
+        'key': 'SomeKey',
+        'metaState': 0,
+      };
+      expect(messages, <Map<String, dynamic>>[
+        expectedMessage,
+        expectedMessage,
+        expectedMessage,
+      ]);
     });
 
     test('stops dispatching events after dispose', () {
@@ -69,19 +148,48 @@ void main() {
         count += 1;
       };
 
-      html.window.dispatchEvent(html.KeyboardEvent('keydown'));
+      dispatchKeyboardEvent('keydown');
       expect(count, 1);
-      html.window.dispatchEvent(html.KeyboardEvent('keyup'));
+      dispatchKeyboardEvent('keyup');
       expect(count, 2);
 
       Keyboard.instance.dispose();
       expect(Keyboard.instance, isNull);
 
       // No more event dispatching.
-      html.window.dispatchEvent(html.KeyboardEvent('keydown'));
+      dispatchKeyboardEvent('keydown');
       expect(count, 2);
-      html.window.dispatchEvent(html.KeyboardEvent('keyup'));
+      dispatchKeyboardEvent('keyup');
       expect(count, 2);
     });
   });
+}
+
+void dispatchKeyboardEvent(
+  String type, {
+  String key,
+  String code,
+  bool repeat = false,
+  bool isShiftPressed = false,
+  bool isAltPressed = false,
+  bool isControlPressed = false,
+  bool isMetaPressed = false,
+}) {
+  final Function jsKeyboardEvent =
+      js_util.getProperty(html.window, 'KeyboardEvent');
+  final List<dynamic> eventArgs = <dynamic>[
+    type,
+    <String, dynamic>{
+      'key': key,
+      'code': code,
+      'repeat': repeat,
+      'shiftKey': isShiftPressed,
+      'altKey': isAltPressed,
+      'ctrlKey': isControlPressed,
+      'metaKey': isMetaPressed,
+    }
+  ];
+  final event =
+      js_util.callConstructor(jsKeyboardEvent, js_util.jsify(eventArgs));
+  html.window.dispatchEvent(event);
 }
