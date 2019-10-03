fadbcc62f640e6d60ad132f87789a69ecfb3da21
Yegor Jbanov <yjbanov@google.com>
2019-10-03 09:46:25 -0700

    add more web tests;use blacklist instead of whitelist

--- dev/bots/test.dart
+++ dev/bots/test.dart
@@ -435,18 +435,34 @@ Future<void> _runTests() async {
 }
 
 Future<void> _runWebTests() async {
-  await _runFlutterWebTest(path.join(flutterRoot, 'packages', 'flutter'), tests: <String>[
-    'test/foundation/',
-    'test/physics/',
-    'test/services/',
-    // TODO(yjbanov): re-enable when flakiness is resolved
-    // 'test/rendering/',
-    // 'test/painting/',
-    // 'test/scheduler/',
-    // 'test/semantics/',
-    // 'test/widgets/',
-    // 'test/material/',
-  ]);
+  final Directory flutterPackageDir = Directory(path.join(flutterRoot, 'packages', 'flutter'));
+  final Directory testDir = Directory(path.join(flutterPackageDir.path, 'test'));
+
+  final List<String> directories = <String>[];
+
+  // TODO(yjbanov): we're getting rid of this blacklist as part of https://github.com/flutter/flutter/projects/60
+  const List<String> kBlacklist = <String>[
+    'test/cupertino',
+    'test/examples',
+    'test/material',
+    'test/painting',
+    'test/rendering',
+    'test/semantics',
+    'test/widgets',
+  ];
+
+  for (Directory subDirectory in testDir.listSync().whereType<Directory>()) {
+    final String relativePath = path.relative(subDirectory.path, from: flutterPackageDir.path);
+
+    if (kBlacklist.contains(relativePath)) {
+      // The test is blacklisted from running on the Web.
+      continue;
+    }
+
+    directories.add(relativePath);
+  }
+
+  await _runFlutterWebTest(flutterPackageDir.path, tests: directories);
   await _runFlutterWebTest(path.join(flutterRoot, 'packages', 'flutter_web_plugins'), tests: <String>['test']);
 }
 
