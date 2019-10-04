bba0065cf62cde3ec9b49cfd446f5b3de94ac5b7
Yegor <yjbanov@google.com>
2019-10-04 15:07:05 -0700

    add option for bulk-updating screenshots; update screenshots (#12797)
    * add option for bulk-updating screenshots; update screenshots

--- lib/web_ui/test/golden_tests/golden_success_smoke_test.dart
+++ lib/web_ui/test/golden_tests/golden_success_smoke_test.dart
@@ -6,10 +6,15 @@ import 'dart:html' as html;
 
 import 'package:test/test.dart';
 import 'package:ui/ui.dart';
+import 'package:ui/src/engine.dart';
 import 'package:web_engine_tester/golden_tester.dart';
 
-void main() {
+void main() async {
+  debugEmulateFlutterTesterEnvironment = true;
+  await webOnlyInitializePlatform(assetManager: WebOnlyMockAssetManager());
+
   test('screenshot test reports success', () async {
+    html.document.body.style.fontFamily = 'Roboto';
     html.document.body.innerHtml = 'Hello world!';
     await matchGoldenFile('__local__/smoke_test.png', region: Rect.fromLTWH(0, 0, 320, 200));
   });
