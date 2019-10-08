eac710b56becf699265f33f63736ce6679de015c
Christopher Fujino <christopherfujino@gmail.com>
2019-10-08 11:11:19 -0700

    fix analyzer

--- packages/flutter_tools/test/general.shard/ios/devices_test.dart
+++ packages/flutter_tools/test/general.shard/ios/devices_test.dart
@@ -80,7 +80,7 @@ void main() {
 
     group('dispose()', () {
       testUsingContext('Calling dispose() kills all log readers & port forwarders', () async {
-        IOSDevice device = IOSDevice('123');
+        final IOSDevice device = IOSDevice('123');
         final MockApplicationPackage applicationPackage1 = MockApplicationPackage();
         final MockApplicationPackage applicationPackage2 = MockApplicationPackage();
         final MockDeviceLogReader mockDeviceLogReader1 = MockDeviceLogReader();
