f48f0fa01b06704c5730065871771830ff7ce588
David Iglesias <ditman@gmail.com>
2019-10-08 09:40:09 -0700

    Open source canvas tests from flutter_web_ui (#12819)
    * Open source canvas tests from flutter_web_ui
    
    Reimplement some painter tests as screenshot tests on canvas.
    
    Needs https://github.com/flutter/goldens/pull/53
    Fixes https://github.com/flutter/flutter/issues/42027
    
    * Remove whitespace
    
    * Address PR feedback
    
    * Add latest hash to dev/goldens_lock.yaml
    * Rename (with git mv) *scuba_test.dart -> *golden_test.dart

---
+++ lib/web_ui/test/golden_tests/engine/canvas_lines_golden_test.dart
@@ -0,0 +1,66 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'dart:html' as html;
+
+import 'package:ui/src/engine.dart';
+import 'package:ui/ui.dart';
+import 'package:test/test.dart';
+
+import 'package:web_engine_tester/golden_tester.dart';
+
+void main() async {
+  final Rect region = Rect.fromLTWH(0, 0, 300, 300);
+
+  BitmapCanvas canvas;
+
+  setUp(() {
+    canvas = BitmapCanvas(region);
+  });
+
+  tearDown(() {
+    canvas.rootElement.remove();
+  });
+
+  test('draws lines with varying strokeWidth', () async {
+
+    paintLines(canvas);
+
+    html.document.body.append(canvas.rootElement);
+    await matchGoldenFile('canvas_lines_thickness.png', region: region);
+  }, timeout: const Timeout(Duration(seconds: 10)));
+
+}
+
+void paintLines(BitmapCanvas canvas) {
+    final PaintData paint1 = PaintData()
+      ..color = Color(0xFF9E9E9E) // Colors.grey
+      ..strokeWidth = 1.0
+      ..style = PaintingStyle.stroke;
+    final PaintData paint2 = PaintData()
+      ..color = Color(0x7fff0000)
+      ..strokeWidth = 1.0
+      ..style = PaintingStyle.stroke;
+    final PaintData paint3 = PaintData()
+      ..color = Color(0xFF4CAF50) //Colors.green
+      ..strokeWidth = 1.0
+      ..style = PaintingStyle.stroke;
+    // Draw markers around 100x100 box
+    canvas.drawLine(Offset(50, 50), Offset(52, 50), paint1);
+    canvas.drawLine(Offset(150, 50), Offset(148, 50), paint1);
+    canvas.drawLine(Offset(50, 150), Offset(52, 150), paint1);
+    canvas.drawLine(Offset(150, 150), Offset(148, 150), paint1);
+    // Draw diagonal
+    canvas.drawLine(Offset(50, 50), Offset(150, 150), paint2);
+    // Draw horizontal
+    paint3.strokeWidth = 1.0;
+    paint3.color = Color(0xFFFF0000);
+    canvas.drawLine(Offset(50, 55), Offset(150, 55), paint3);
+    paint3.strokeWidth = 2.0;
+    paint3.color = Color(0xFF2196F3); // Colors.blue;
+    canvas.drawLine(Offset(50, 60), Offset(150, 60), paint3);
+    paint3.strokeWidth = 4.0;
+    paint3.color = Color(0xFFFF9800); // Colors.orange;
+    canvas.drawLine(Offset(50, 70), Offset(150, 70), paint3);
+}
