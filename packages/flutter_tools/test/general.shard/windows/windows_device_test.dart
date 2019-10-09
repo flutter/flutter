09411713b61ffc432c4c9327779ea2bb6331d301
stuartmorgan <stuartmorgan@google.com>
2019-10-09 16:28:10 -0700

    Make desktop stopApp only apply to processes Flutter started (#41519)

--- packages/flutter_tools/test/general.shard/windows/windows_device_test.dart
+++ packages/flutter_tools/test/general.shard/windows/windows_device_test.dart
@@ -4,7 +4,6 @@
 
 import 'package:file/memory.dart';
 import 'package:flutter_tools/src/base/file_system.dart';
-import 'package:flutter_tools/src/base/io.dart';
 import 'package:flutter_tools/src/base/platform.dart';
 import 'package:flutter_tools/src/build_info.dart';
 import 'package:flutter_tools/src/project.dart';
@@ -12,7 +11,6 @@ import 'package:flutter_tools/src/windows/application_package.dart';
 import 'package:flutter_tools/src/windows/windows_device.dart';
 import 'package:flutter_tools/src/device.dart';
 import 'package:mockito/mockito.dart';
-import 'package:process/process.dart';
 
 import '../../src/common.dart';
 import '../../src/context.dart';
@@ -21,28 +19,9 @@ void main() {
   group(WindowsDevice, () {
     final WindowsDevice device = WindowsDevice();
     final MockPlatform notWindows = MockPlatform();
-    final MockProcessManager mockProcessManager = MockProcessManager();
-    const String flutterToolBinary = 'flutter.exe';
 
     when(notWindows.isWindows).thenReturn(false);
     when(notWindows.environment).thenReturn(const <String, String>{});
-    when(mockProcessManager.runSync(
-      <String>['powershell', '-script="Get-CimInstance Win32_Process"'],
-      workingDirectory: anyNamed('workingDirectory'),
-      environment: anyNamed('environment'),
-    )).thenAnswer((Invocation invocation) {
-      // The flutter tool process is returned as output to the powershell script
-      final MockProcessResult result = MockProcessResult();
-      when(result.exitCode).thenReturn(0);
-      when<String>(result.stdout).thenReturn('$pid  $flutterToolBinary');
-      when<String>(result.stderr).thenReturn('');
-      return result;
-    });
-    when(mockProcessManager.run(
-      <String>['Taskkill', '/PID', '$pid', '/F'],
-      workingDirectory: anyNamed('workingDirectory'),
-      environment: anyNamed('environment'),
-    )).thenThrow(Exception('Flutter tool process has been killed'));
 
     testUsingContext('defaults', () async {
       final PrebuiltWindowsApp windowsApp = PrebuiltWindowsApp(executable: 'foo');
@@ -52,18 +31,7 @@ void main() {
       expect(await device.uninstallApp(windowsApp), true);
       expect(await device.isLatestBuildInstalled(windowsApp), true);
       expect(await device.isAppInstalled(windowsApp), true);
-      expect(await device.stopApp(windowsApp), false);
       expect(device.category, Category.desktop);
-    }, overrides: <Type, Generator>{
-      ProcessManager: () => mockProcessManager,
-    });
-
-    test('noop port forwarding', () async {
-      final WindowsDevice device = WindowsDevice();
-      final DevicePortForwarder portForwarder = device.portForwarder;
-      final int result = await portForwarder.forward(2);
-      expect(result, 2);
-      expect(portForwarder.forwardedPorts.isEmpty, true);
     });
 
     testUsingContext('No devices listed if platform unsupported', () async {
@@ -93,24 +61,24 @@ void main() {
       FileSystem: () => MemoryFileSystem(),
     });
 
-    testUsingContext('The current running process is not killed when stopping the app', () async {
-      // The name of the executable is the same as the flutter tool one
-      final PrebuiltWindowsApp windowsApp = PrebuiltWindowsApp(executable: flutterToolBinary);
-      expect(await device.stopApp(windowsApp), false);
+    testUsingContext('executablePathForDevice uses the correct package executable', () async {
+      final MockWindowsApp mockApp = MockWindowsApp();
+      const String debugPath = 'debug/executable';
+      const String profilePath = 'profile/executable';
+      const String releasePath = 'release/executable';
+      when(mockApp.executable(BuildMode.debug)).thenReturn(debugPath);
+      when(mockApp.executable(BuildMode.profile)).thenReturn(profilePath);
+      when(mockApp.executable(BuildMode.release)).thenReturn(releasePath);
+
+      expect(WindowsDevice().executablePathForDevice(mockApp, BuildMode.debug), debugPath);
+      expect(WindowsDevice().executablePathForDevice(mockApp, BuildMode.profile), profilePath);
+      expect(WindowsDevice().executablePathForDevice(mockApp, BuildMode.release), releasePath);
     }, overrides: <Type, Generator>{
-      ProcessManager: () => mockProcessManager,
+      FileSystem: () => MemoryFileSystem(),
     });
   });
 }
 
 class MockPlatform extends Mock implements Platform {}
 
-class MockFileSystem extends Mock implements FileSystem {}
-
-class MockFile extends Mock implements File {}
-
-class MockProcessManager extends Mock implements ProcessManager {}
-
-class MockProcess extends Mock implements Process {}
-
-class MockProcessResult extends Mock implements ProcessResult {}
+class MockWindowsApp extends Mock implements WindowsApp {}
