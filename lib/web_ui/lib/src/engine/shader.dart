d9c3afae26d55dab653f09e1bb969ed000948766
Ferhat <ferhat@gmail.com>
2019-10-09 15:51:31 -0700

    [web] Implement basic radial gradient (TileMode.clamp, no transform) (#12811)
    * Add GradientRadial paintStyle implementation

--- lib/web_ui/lib/src/engine/shader.dart
+++ lib/web_ui/lib/src/engine/shader.dart
@@ -162,7 +162,8 @@ class GradientLinear extends EngineGradient {
   }
 }
 
-// TODO(flutter_web): Add screenshot tests when infra is ready.
+// TODO(flutter_web): For transforms and tile modes implement as webgl
+// shader instead. See https://github.com/flutter/flutter/issues/32819
 class GradientRadial extends EngineGradient {
   GradientRadial(this.center, this.radius, this.colors, this.colorStops,
       this.tileMode, this.matrix4)
@@ -177,20 +178,25 @@ class GradientRadial extends EngineGradient {
 
   @override
   Object createPaintStyle(html.CanvasRenderingContext2D ctx) {
-    // TODO(flutter_web): see https://github.com/flutter/flutter/issues/32819
     if (matrix4 != null && !Matrix4.fromFloat64List(matrix4).isIdentity()) {
-        throw UnimplementedError('matrix4 not supported in GradientRadial shader');
+      throw UnimplementedError(
+          'matrix4 not supported in GradientRadial shader');
     }
-    final html.CanvasGradient gradient =
-        ctx.createRadialGradient(center.dx, center.dy, 0, center.dx, center.dy, radius);
+    if (tileMode != ui.TileMode.clamp) {
+      throw UnimplementedError(
+          'TileMode not supported in GradientRadial shader');
+    }
+    final html.CanvasGradient gradient = ctx.createRadialGradient(
+        center.dx, center.dy, 0, center.dx, center.dy, radius);
     if (colorStops == null) {
       assert(colors.length == 2);
       gradient.addColorStop(0, colors[0].toCssString());
       gradient.addColorStop(1, colors[1].toCssString());
       return gradient;
-    }
-    for (int i = 0; i < colors.length; i++) {
-      gradient.addColorStop(colorStops[i], colors[i].toCssString());
+    } else {
+      for (int i = 0; i < colors.length; i++) {
+        gradient.addColorStop(colorStops[i], colors[i].toCssString());
+      }
     }
     return gradient;
   }
