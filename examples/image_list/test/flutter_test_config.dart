8a9508301bfe37a7282b3e0569abe534746f5aae
Alexander Aprelev <aam@google.com>
2019-10-09 13:07:04 -0700

    Add note regarding flutter_goldens use

--- examples/image_list/test/flutter_test_config.dart
+++ examples/image_list/test/flutter_test_config.dart
@@ -1,1 +1,10 @@
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Note that presence of this file enables use of https://github.com/flutter/goldens
+// repository for golden file storage. To store golden files in this project's
+// [test] folder, simply remove this file and remove `flutter_goldens` dev_dependency
+// from pubspec.yaml.
+
 export 'package:flutter_goldens/flutter_goldens.dart' show main;
