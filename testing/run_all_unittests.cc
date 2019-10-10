0622b051b63b399838eb7956bed97d76fc8bdf77
Dan Field <dnfield@google.com>
2019-10-09 21:16:11 -0700

    Minimal test harness for iOS (#13029)

--- testing/run_all_unittests.cc
+++ testing/run_all_unittests.cc
@@ -2,9 +2,21 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "flutter/fml/build_config.h"
 #include "gtest/gtest.h"
 
+#ifdef OS_IOS
+#include <asl.h>
+#endif  // OS_IOS
+
 int main(int argc, char** argv) {
+#ifdef OS_IOS
+  asl_log_descriptor(NULL, NULL, ASL_LEVEL_NOTICE, STDOUT_FILENO,
+                     ASL_LOG_DESCRIPTOR_WRITE);
+  asl_log_descriptor(NULL, NULL, ASL_LEVEL_ERR, STDERR_FILENO,
+                     ASL_LOG_DESCRIPTOR_WRITE);
+#endif  // OS_IOS
+
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
