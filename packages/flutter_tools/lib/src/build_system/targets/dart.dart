4d6d74cff07dc7a31e07b4951d935e62fb41b9fa
jonahwilliams <jonahwilliams@google.com>
2019-10-10 15:15:41 -0700

    add test case for jit_release

--- packages/flutter_tools/lib/src/build_system/targets/dart.dart
+++ packages/flutter_tools/lib/src/build_system/targets/dart.dart
@@ -211,7 +211,7 @@ class KernelSnapshot extends Target {
 
     final CompilerOutput output = await compiler.compile(
       sdkRoot: artifacts.getArtifactPath(Artifact.flutterPatchedSdkPath, mode: buildMode),
-      aot: buildMode != BuildMode.debug,
+      aot: buildMode == BuildMode.profile || buildMode == BuildMode.release,
       enableAsserts: buildMode == BuildMode.debug,
       trackWidgetCreation: buildMode == BuildMode.debug,
       targetModel: TargetModel.flutter,
