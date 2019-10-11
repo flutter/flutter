48ab41ac6337691826f31cc24ee37ed819e8e495
gaaclarke <30870216+gaaclarke@users.noreply.github.com>
2019-10-11 11:34:11 -0700

    Removed retain cycle from notification center. (flutter/engine#13073)

--- engine/src/flutter/shell/platform/darwin/ios/framework/Source/FlutterEngine.mm
+++ engine/src/flutter/shell/platform/darwin/ios/framework/Source/FlutterEngine.mm
@@ -118,7 +118,10 @@ NSString* const FlutterDefaultDartEntrypoint = nil;
 
   NSNotificationCenter* center = [NSNotificationCenter defaultCenter];
   [center removeObserver:self name:UIApplicationDidReceiveMemoryWarningNotification object:nil];
-  [_flutterViewControllerWillDeallocObserver release];
+  if (_flutterViewControllerWillDeallocObserver) {
+    [center removeObserver:_flutterViewControllerWillDeallocObserver];
+    [_flutterViewControllerWillDeallocObserver release];
+  }
 
   [super dealloc];
 }
@@ -176,13 +179,25 @@ NSString* const FlutterDefaultDartEntrypoint = nil;
   self.iosPlatformView->SetOwnerViewController(_viewController);
   [self maybeSetupPlatformViewChannels];
 
+  __block FlutterEngine* blockSelf = self;
   self.flutterViewControllerWillDeallocObserver =
       [[NSNotificationCenter defaultCenter] addObserverForName:FlutterViewControllerWillDealloc
                                                         object:viewController
                                                          queue:[NSOperationQueue mainQueue]
                                                     usingBlock:^(NSNotification* note) {
-                                                      [self notifyViewControllerDeallocated];
+                                                      [blockSelf notifyViewControllerDeallocated];
                                                     }];
+}
+
+- (void)setFlutterViewControllerWillDeallocObserver:(id<NSObject>)observer {
+  if (observer != _flutterViewControllerWillDeallocObserver) {
+    if (_flutterViewControllerWillDeallocObserver) {
+      [[NSNotificationCenter defaultCenter]
+          removeObserver:_flutterViewControllerWillDeallocObserver];
+      [_flutterViewControllerWillDeallocObserver release];
+    }
+    _flutterViewControllerWillDeallocObserver = [observer retain];
+  }
 }
 
 - (void)notifyViewControllerDeallocated {
