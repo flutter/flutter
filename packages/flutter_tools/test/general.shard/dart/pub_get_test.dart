53be552ae115544d0278ac1783e153665c6afdc6
Ian Hickson <ian@hixie.ch>
2019-10-10 19:37:01 -0700

    Be more verbose when pub fails (#42187)

--- packages/flutter_tools/test/general.shard/dart/pub_get_test.dart
+++ packages/flutter_tools/test/general.shard/dart/pub_get_test.dart
@@ -44,52 +44,79 @@ void main() {
       time.elapse(const Duration(milliseconds: 500));
       expect(testLogger.statusText,
         'Running "flutter pub get" in /...\n'
-        'pub get failed (69) -- attempting retry 1 in 1 second...\n',
+        'pub get failed (server unavailable) -- attempting retry 1 in 1 second...\n',
       );
       expect(processMock.lastPubEnvironment, contains('flutter_cli:flutter_tests'));
       expect(processMock.lastPubCache, isNull);
       time.elapse(const Duration(milliseconds: 500));
       expect(testLogger.statusText,
         'Running "flutter pub get" in /...\n'
-        'pub get failed (69) -- attempting retry 1 in 1 second...\n'
-        'pub get failed (69) -- attempting retry 2 in 2 seconds...\n',
+        'pub get failed (server unavailable) -- attempting retry 1 in 1 second...\n'
+        'pub get failed (server unavailable) -- attempting retry 2 in 2 seconds...\n',
       );
       time.elapse(const Duration(seconds: 1));
       expect(testLogger.statusText,
         'Running "flutter pub get" in /...\n'
-        'pub get failed (69) -- attempting retry 1 in 1 second...\n'
-        'pub get failed (69) -- attempting retry 2 in 2 seconds...\n',
+        'pub get failed (server unavailable) -- attempting retry 1 in 1 second...\n'
+        'pub get failed (server unavailable) -- attempting retry 2 in 2 seconds...\n',
       );
       time.elapse(const Duration(seconds: 100)); // from t=0 to t=100
       expect(testLogger.statusText,
         'Running "flutter pub get" in /...\n'
-        'pub get failed (69) -- attempting retry 1 in 1 second...\n'
-        'pub get failed (69) -- attempting retry 2 in 2 seconds...\n'
-        'pub get failed (69) -- attempting retry 3 in 4 seconds...\n' // at t=1
-        'pub get failed (69) -- attempting retry 4 in 8 seconds...\n' // at t=5
-        'pub get failed (69) -- attempting retry 5 in 16 seconds...\n' // at t=13
-        'pub get failed (69) -- attempting retry 6 in 32 seconds...\n' // at t=29
-        'pub get failed (69) -- attempting retry 7 in 64 seconds...\n', // at t=61
+        'pub get failed (server unavailable) -- attempting retry 1 in 1 second...\n'
+        'pub get failed (server unavailable) -- attempting retry 2 in 2 seconds...\n'
+        'pub get failed (server unavailable) -- attempting retry 3 in 4 seconds...\n' // at t=1
+        'pub get failed (server unavailable) -- attempting retry 4 in 8 seconds...\n' // at t=5
+        'pub get failed (server unavailable) -- attempting retry 5 in 16 seconds...\n' // at t=13
+        'pub get failed (server unavailable) -- attempting retry 6 in 32 seconds...\n' // at t=29
+        'pub get failed (server unavailable) -- attempting retry 7 in 64 seconds...\n', // at t=61
       );
       time.elapse(const Duration(seconds: 200)); // from t=0 to t=200
       expect(testLogger.statusText,
         'Running "flutter pub get" in /...\n'
-        'pub get failed (69) -- attempting retry 1 in 1 second...\n'
-        'pub get failed (69) -- attempting retry 2 in 2 seconds...\n'
-        'pub get failed (69) -- attempting retry 3 in 4 seconds...\n'
-        'pub get failed (69) -- attempting retry 4 in 8 seconds...\n'
-        'pub get failed (69) -- attempting retry 5 in 16 seconds...\n'
-        'pub get failed (69) -- attempting retry 6 in 32 seconds...\n'
-        'pub get failed (69) -- attempting retry 7 in 64 seconds...\n'
-        'pub get failed (69) -- attempting retry 8 in 64 seconds...\n' // at t=39
-        'pub get failed (69) -- attempting retry 9 in 64 seconds...\n' // at t=103
-        'pub get failed (69) -- attempting retry 10 in 64 seconds...\n', // at t=167
+        'pub get failed (server unavailable) -- attempting retry 1 in 1 second...\n'
+        'pub get failed (server unavailable) -- attempting retry 2 in 2 seconds...\n'
+        'pub get failed (server unavailable) -- attempting retry 3 in 4 seconds...\n'
+        'pub get failed (server unavailable) -- attempting retry 4 in 8 seconds...\n'
+        'pub get failed (server unavailable) -- attempting retry 5 in 16 seconds...\n'
+        'pub get failed (server unavailable) -- attempting retry 6 in 32 seconds...\n'
+        'pub get failed (server unavailable) -- attempting retry 7 in 64 seconds...\n'
+        'pub get failed (server unavailable) -- attempting retry 8 in 64 seconds...\n' // at t=39
+        'pub get failed (server unavailable) -- attempting retry 9 in 64 seconds...\n' // at t=103
+        'pub get failed (server unavailable) -- attempting retry 10 in 64 seconds...\n', // at t=167
       );
     });
     expect(testLogger.errorText, isEmpty);
     expect(error, isNull);
   }, overrides: <Type, Generator>{
     ProcessManager: () => MockProcessManager(69),
+    FileSystem: () => MockFileSystem(),
+    Platform: () => FakePlatform(
+      environment: UnmodifiableMapView<String, String>(<String, String>{}),
+    ),
+    Pub: () => const Pub(),
+  });
+
+  testUsingContext('pub get 66 shows message from pub', () async {
+    try {
+      await pub.get(context: PubContext.flutterTests, checkLastModified: false);
+      throw AssertionError('pubGet did not fail');
+    } on ToolExit catch (error) {
+      expect(error.message, 'pub get failed (66; err3)');
+    }
+    expect(testLogger.statusText,
+      'Running "flutter pub get" in /...\n'
+      'out1\n'
+      'out2\n'
+      'out3\n'
+    );
+    expect(testLogger.errorText,
+      'err1\n'
+      'err2\n'
+      'err3\n'
+    );
+  }, overrides: <Type, Generator>{
+    ProcessManager: () => MockProcessManager(66, stderr: 'err1\nerr2\nerr3\n', stdout: 'out1\nout2\nout3\n'),
     FileSystem: () => MockFileSystem(),
     Platform: () => FakePlatform(
       environment: UnmodifiableMapView<String, String>(<String, String>{}),
@@ -218,10 +245,12 @@ typedef StartCallback = void Function(List<dynamic> command);
 
 class MockProcessManager implements ProcessManager {
   MockProcessManager(this.fakeExitCode, {
+    this.stdout = '',
     this.stderr = '',
   });
 
   final int fakeExitCode;
+  final String stdout;
   final String stderr;
 
   String lastPubEnvironment;
@@ -240,6 +269,7 @@ class MockProcessManager implements ProcessManager {
     lastPubCache = environment['PUB_CACHE'];
     return Future<Process>.value(mocks.createMockProcess(
       exitCode: fakeExitCode,
+      stdout: stdout,
       stderr: stderr,
     ));
   }
