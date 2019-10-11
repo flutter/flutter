7662328349465fdabb8f91b97fccfffa8496a203
Dan Field <dnfield@google.com>
2019-10-10 20:44:56 -0700

    unused import

--- packages/flutter/test/painting/gradient_test.dart
+++ packages/flutter/test/painting/gradient_test.dart
@@ -6,7 +6,6 @@ import 'dart:math' as math;
 import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter/painting.dart';
-import 'package:vector_math/vector_math_64.dart';
 
 void main() {
   test('LinearGradient scale test', () {
