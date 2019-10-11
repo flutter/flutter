914990883067051becd83ff68752f97679fc423e
Michael Klimushyn <mklim@google.com>
2019-10-10 18:23:23 -0700

    Fix abstract_method_smoke_test flakiness (#42454)
    The flake appears to be coming from the Google Maps plugin. This test
    just needs a platform view plugin without the interface method
    implemented in general, not Maps specifically. Update it here to avoid
    the issue in Maps until that's fixed.
    
    Create a very simple platform view implementation that exercises this
    potential bug in order to create a minimal test case with less risk of
    causing second order issues like depending on any kind of full package
    would cause.

--- dev/integration_tests/abstract_method_smoke_test/android/app/src/main/kotlin/com/example/abstract_method_smoke_test/MainActivity.kt
+++ dev/integration_tests/abstract_method_smoke_test/android/app/src/main/kotlin/com/example/abstract_method_smoke_test/MainActivity.kt
@@ -1,35 +1,61 @@
 package com.example.abstract_method_smoke_test
 
 import android.content.Context
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
 import android.view.inputmethod.InputMethodManager
 import io.flutter.app.FlutterActivity
-import io.flutter.plugin.common.MethodChannel;
-import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugin.common.StandardMessageCodec
+import io.flutter.plugin.platform.PlatformView
+import io.flutter.plugin.platform.PlatformViewFactory
 import io.flutter.plugins.GeneratedPluginRegistrant
 
-class MainActivity: FlutterActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    GeneratedPluginRegistrant.registerWith(this)
+class MainActivity : FlutterActivity() {
+    class SimplePlatformView(context: Context) : PlatformView {
+        private val view: View = View(context)
 
-    // Triggers the Android keyboard, which causes the resize of the Flutter view.
-    // We need to wait for the app to complete.
-    MethodChannel(getFlutterView(), "com.example.abstract_method_smoke_test")
-        .setMethodCallHandler({ call, result ->
-          toggleInput()
-          result.success(null)
-        })
-  }
+        init {
+            view.setBackgroundColor(Color.CYAN)
+        }
 
-  override fun onPause() {
-    // Hide the input when the app is closed.
-    toggleInput()
-    super.onPause()
-  }
+        override fun dispose() {}
 
-  fun toggleInput() {
-    val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-    imm.toggleSoftInput(InputMethodManager.SHOW_IMPLICIT, 0)
-  }
+        override fun getView(): View {
+            return view
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        GeneratedPluginRegistrant.registerWith(this)
+
+        registrarFor("com.example.abstract_method_smoke_test")
+                .platformViewRegistry()
+                .registerViewFactory("simple", object : PlatformViewFactory(StandardMessageCodec.INSTANCE) {
+                    override fun create(context: Context?, viewId: Int, args: Any?): PlatformView {
+                        return SimplePlatformView(this@MainActivity)
+                    }
+                })
+
+        // Triggers the Android keyboard, which causes the resize of the Flutter view.
+        // We need to wait for the app to complete.
+        MethodChannel(flutterView, "com.example.abstract_method_smoke_test")
+                .setMethodCallHandler { _, result ->
+                    toggleInput()
+                    result.success(null)
+                }
+    }
+
+    override fun onPause() {
+        // Hide the input when the app is closed.
+        toggleInput()
+        super.onPause()
+    }
+
+    private fun toggleInput() {
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.toggleSoftInput(InputMethodManager.SHOW_IMPLICIT, 0)
+    }
 }
