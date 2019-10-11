7a3d0619c58f25ab5cd8a8d4b4fad3d3be4e5f11
Jonah Williams <jonahwilliams@google.com>
2019-10-10 18:19:58 -0700

    Update depfile.dart

--- packages/flutter_tools/lib/src/build_system/depfile.dart
+++ packages/flutter_tools/lib/src/build_system/depfile.dart
@@ -2,9 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:flutter_tools/src/base/platform.dart';
-
 import '../base/file_system.dart';
+import '../base/platform.dart';
 import '../globals.dart';
 
 /// A class for representing depfile formats.
