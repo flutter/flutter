711b050531b962a6facb5b86e8e71163f184c515
Albertus Angga Raharja <albertusangga@google.com>
2019-10-14 02:55:25 -0700

    Add structured errors in Debug

--- packages/flutter/test/material/debug_test.dart
+++ packages/flutter/test/material/debug_test.dart
@@ -10,7 +10,75 @@ void main() {
     await tester.pumpWidget(const ListTile());
     final dynamic exception = tester.takeException();
     expect(exception, isFlutterError);
-    expect(exception.toString(), startsWith('No Material widget found.'));
-    expect(exception.toString(), endsWith(':\n  ListTile\nThe ancestors of this widget were:\n  [root]'));
+    final FlutterError error = exception;
+    expect(error.toStringDeep(),
+        'FlutterError\n'
+            '   No Material widget found.\n'
+            '   ListTile widgets require a Material widget ancestor.\n'
+            '   In material design, most widgets are conceptually "printed" on a\n'
+            '   sheet of material. In Flutter\'s material library, that material\n'
+            '   is represented by the Material widget. It is the Material widget\n'
+            '   that renders ink splashes, for instance. Because of this, many\n'
+            '   material library widgets require that there be a Material widget\n'
+            '   in the tree above them.\n'
+            '   To introduce a Material widget, you can either directly include\n'
+            '   one, or use a widget that contains Material itself, such as a\n'
+            '   Card, Dialog, Drawer, or Scaffold.\n'
+            '   The specific widget that could not find a Material ancestor was:\n'
+            '     ListTile\n'
+            '   The ancestors of this widget were:\n'
+            '     [root]\n'
+    );
+  });
+
+  testWidgets('debugCheckHasMaterialLocalizations control test', (
+      WidgetTester tester) async {
+    await tester.pumpWidget(const BackButton());
+    final dynamic exception = tester.takeException();
+    expect(exception, isFlutterError);
+    final FlutterError error = exception;
+    expect(error.toStringDeep(),
+        'FlutterError\n'
+            '   No MaterialLocalizations found.\n'
+            '   BackButton widgets require MaterialLocalizations to be provided\n'
+            '   by a Localizations widget ancestor.\n'
+            '   Localizations are used to generate many different messages,\n'
+            '   labels,and abbreviations which are used by the material library.\n'
+            '   To introduce a MaterialLocalizations, either use a  MaterialApp\n'
+            '   at the root of your application to include them automatically, or\n'
+            '   add a Localization widget with a MaterialLocalizations delegate.\n'
+            '   The specific widget that could not find a MaterialLocalizations\n'
+            '   ancestor was:\n'
+            '     BackButton\n'
+            '   The ancestors of this widget were:\n'
+            '     [root]\n'
+    );
+  });
+
+  testWidgets(
+      'debugCheckHasScaffold control test', (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(
+        home: Builder(
+            builder: (BuildContext context) {
+              showBottomSheet<void>(context: context,
+                  builder: (BuildContext context) => Container());
+              return Container();
+            }
+        )
+    ));
+    final dynamic exception = tester.takeException();
+    expect(exception, isFlutterError);
+    final FlutterError error = exception;
+    expect(error.toStringDeep(), startsWith(
+        'FlutterError\n'
+            '   No Scaffold widget found.\n'
+            '   Builder widgets require a Scaffold widget ancestor.\n'
+            '   The specific widget that could not find a Scaffold ancestor was:\n'
+            '     Builder\n'
+    ));
+    expect(error.toStringDeep(), endsWith(
+        '   Typically, the Scaffold widget is introduced by the MaterialApp\n'
+            '   or WidgetsApp widget at the top of your application widget tree.\n'
+    ));
   });
 }
