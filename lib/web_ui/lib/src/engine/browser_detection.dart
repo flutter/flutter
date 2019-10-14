c643366c19e39c99463d6718db3e25939c789db1
Mouad Debbar <mouad.debbar@gmail.com>
2019-10-14 13:10:01 -0700

    Support keyboard types on mobile browsers (#13044)

--- lib/web_ui/lib/src/engine/browser_detection.dart
+++ lib/web_ui/lib/src/engine/browser_detection.dart
@@ -77,11 +77,24 @@ OperatingSystem _operatingSystem;
 ///
 /// This is used to implement operating system specific behavior such as
 /// soft keyboards.
-OperatingSystem get operatingSystem =>
-    _operatingSystem ??= _detectOperatingSystem();
+OperatingSystem get operatingSystem {
+  if (debugOperatingSystemOverride != null) {
+    return debugOperatingSystemOverride;
+  }
+  return _operatingSystem ??= _detectOperatingSystem();
+}
+
+/// Override the value of [operatingSystem].
+///
+/// Setting this to `null` lets [operatingSystem] detect the real OS that the
+/// app is running on.
+///
+/// This is intended to be used for testing and debugging only.
+OperatingSystem debugOperatingSystemOverride;
 
 OperatingSystem _detectOperatingSystem() {
   final String platform = html.window.navigator.platform;
+  final String userAgent = html.window.navigator.userAgent;
 
   if (platform.startsWith('Mac')) {
     return OperatingSystem.macOs;
@@ -89,7 +102,10 @@ OperatingSystem _detectOperatingSystem() {
       platform.toLowerCase().contains('ipad') ||
       platform.toLowerCase().contains('ipod')) {
     return OperatingSystem.iOs;
-  } else if (platform.toLowerCase().contains('android')) {
+  } else if (userAgent.contains('Android')) {
+    // The Android OS reports itself as "Linux armv8l" in
+    // [html.window.navigator.platform]. So we have to check the user-agent to
+    // determine if the OS is Android or not.
     return OperatingSystem.android;
   } else if (platform.startsWith('Linux')) {
     return OperatingSystem.linux;
---
+++ lib/web_ui/lib/src/engine/text_editing/input_type.dart
@@ -0,0 +1,129 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+part of engine;
+
+/// Various types of inputs used in text fields.
+///
+/// These types are coming from Flutter's [TextInputType]. Currently, we don't
+/// support all the types. We fallback to [EngineInputType.text] when Flutter
+/// sends a type that isn't supported.
+// TODO(flutter_web): Support more types.
+abstract class EngineInputType {
+  const EngineInputType();
+
+  static EngineInputType fromName(String name) {
+    switch (name) {
+      case 'TextInputType.number':
+        return number;
+      case 'TextInputType.phone':
+        return phone;
+      case 'TextInputType.emailAddress':
+        return emailAddress;
+      case 'TextInputType.url':
+        return url;
+      case 'TextInputType.multiline':
+        return multiline;
+
+      case 'TextInputType.text':
+      default:
+        return text;
+    }
+  }
+
+  /// Single-line text input type.
+  static const TextInputType text = TextInputType();
+
+  /// Numeric input type.
+  static const NumberInputType number = NumberInputType();
+
+  /// Phone number input type.
+  static const PhoneInputType phone = PhoneInputType();
+
+  /// Email address input type.
+  static const EmailInputType emailAddress = EmailInputType();
+
+  /// URL input type.
+  static const UrlInputType url = UrlInputType();
+
+  /// Multi-line text input type.
+  static const MultilineInputType multiline = MultilineInputType();
+
+  /// The HTML `inputmode` attribute to be set on the DOM element.
+  ///
+  /// This HTML attribute helps the browser decide what kind of keyboard works
+  /// best for this text field.
+  ///
+  /// For various `inputmode` values supported by browsers, see:
+  /// <https://developer.mozilla.org/en-US/docs/Web/HTML/Global_attributes/inputmode>.
+  String get inputmodeAttribute;
+
+  /// Create the appropriate DOM element for this input type.
+  html.HtmlElement createDomElement() => html.InputElement();
+
+  /// Given a [domElement], set attributes that are specific to this input type.
+  void configureDomElement(html.HtmlElement domElement) {
+    if (inputmodeAttribute == null) {
+      return;
+    }
+
+    // Only apply `inputmode` in mobile browsers so that the right virtual
+    // keyboard shows up.
+    if (operatingSystem == OperatingSystem.iOs ||
+        operatingSystem == OperatingSystem.android) {
+      domElement.setAttribute('inputmode', inputmodeAttribute);
+    }
+  }
+}
+
+/// Single-line text input type.
+class TextInputType extends EngineInputType {
+  const TextInputType();
+
+  @override
+  final String inputmodeAttribute = 'text';
+}
+
+/// Numeric input type.
+class NumberInputType extends EngineInputType {
+  const NumberInputType();
+
+  @override
+  final String inputmodeAttribute = 'numeric';
+}
+
+/// Phone number input type.
+class PhoneInputType extends EngineInputType {
+  const PhoneInputType();
+
+  @override
+  final String inputmodeAttribute = 'tel';
+}
+
+/// Email address input type.
+class EmailInputType extends EngineInputType {
+  const EmailInputType();
+
+  @override
+  final String inputmodeAttribute = 'email';
+}
+
+/// URL input type.
+class UrlInputType extends EngineInputType {
+  const UrlInputType();
+
+  @override
+  final String inputmodeAttribute = 'url';
+}
+
+/// Multi-line text input type.
+class MultilineInputType extends EngineInputType {
+  const MultilineInputType();
+
+  @override
+  final String inputmodeAttribute = null;
+
+  @override
+  html.HtmlElement createDomElement() => html.TextAreaElement();
+}
