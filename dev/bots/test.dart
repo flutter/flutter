8a0c0be5d38f89d5e03ca0a547d1118e7739aa76
Danny Tuppeny <dantup@google.com>
2019-10-14 10:22:09 +0100

    Enable asserts
    See https://github.com/flutter/flutter/issues/36476

--- dev/bots/test.dart
+++ dev/bots/test.dart
@@ -268,7 +268,7 @@ Future<void> _runToolTests() async {
         tableData: bigqueryApi?.tabledata,
         // TODO(ianh): The integration tests fail to start on Windows if asserts are enabled.
         // See https://github.com/flutter/flutter/issues/36476
-        enableFlutterToolAsserts: !(subshard == 'integration' && Platform.isWindows),
+        enableFlutterToolAsserts: true,
       );
     },
   );
