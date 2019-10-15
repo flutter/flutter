478a5447a61d6abccd5b7b92940d52da340668cd
Tong Mu <dkwingsmt@gmail.com>
2019-10-15 14:36:20 -0700

    Remove redundant files

--- examples/flutter_gallery/linux/flutter/ephemeral/generated_config.mk
+++
@@ -1,7 +0,0 @@
-# Generated code do not commit.
-export FLUTTER_ROOT=/usr/local/google/home/tongmu/dev/flutter
-export TRACK_WIDGET_CREATION=false
-export FLUTTER_TARGET=/usr/local/google/home/tongmu/dev/flutter/examples/flutter_gallery/lib/main.dart
-export PROJECT_DIR=/usr/local/google/home/tongmu/dev/flutter/examples/flutter_gallery
-export FLUTTER_ENGINE=/usr/local/google/home/tongmu/dev/engine/src
-export LOCAL_ENGINE=host_debug_unopt
