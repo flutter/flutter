7d1430ac29852b27b89b1b75312417a0871d0b43
jaumard <jimmy.aumard@gmail.com>
2019-10-15 21:47:53 +0200

    add test on attach detach editable

--- packages/flutter/test/rendering/editable_test.dart
+++ packages/flutter/test/rendering/editable_test.dart
@@ -2,11 +2,11 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:flutter/rendering.dart';
-import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_test/flutter_test.dart';
 
 import '../rendering/mock_canvas.dart';
 import '../rendering/recording_canvas.dart';
