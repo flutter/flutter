1ba5742de29ef12a86bb152f748cdc634dd86a23
jonahwilliams <jonahwilliams@google.com>
2019-10-15 19:17:35 -0700

    Fix NPE in Chrome Device

--- packages/flutter_tools/lib/src/web/web_device.dart
+++ packages/flutter_tools/lib/src/web/web_device.dart
@@ -1,3 +1,4 @@
+
 // Copyright 2019 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
@@ -61,7 +62,7 @@ class ChromeDevice extends Device {
 
   @override
   DeviceLogReader getLogReader({ApplicationPackage app}) {
-    return NoOpDeviceLogReader(app.name);
+    return NoOpDeviceLogReader(app?.name);
   }
 
   @override
