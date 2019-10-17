cce4b37edeceeca4901cc6354b9080d0c3fbc5ae
Kate Lovett <katelovett@google.com>
2019-10-17 15:43:53 -0700

    Adding sample snippet

--- packages/flutter/lib/src/material/flexible_space_bar.dart
+++ packages/flutter/lib/src/material/flexible_space_bar.dart
@@ -50,6 +50,88 @@ enum StretchMode {
 /// [FlexibleSpaceBar.createSettings], to convey sizing information down to the
 /// [FlexibleSpaceBar].
 ///
+/// {@tool snippet --template=freeform}
+/// This sample application demonstrates the different features of the
+/// [FlexibleSpaceBar] when used in a [SliverAppBar]. This app bar is configured
+/// to stretch into the overscroll space, and uses the
+/// [FlexibleSpaceBar.stretchModes] to apply `fadeTitle`, `blurBackground` and
+/// `zoomBackground`. The app bar also makes use of [CollapseMode.parallax] by
+/// default.
+///
+/// ```dart imports
+/// import 'package:flutter/material.dart';
+/// ```
+/// ```dart
+/// void main() => runApp(MaterialApp(home: MyApp()));
+///
+/// class MyApp extends StatelessWidget {
+///   @override
+///   Widget build(BuildContext context) {
+///     return Scaffold(
+///       body: CustomScrollView(
+///         physics: const BouncingScrollPhysics(),
+///         slivers: <Widget>[
+///           SliverAppBar(
+///             stretch: true,
+///             onStretchTrigger: () {
+///               print('Stretched!');
+///             },
+///             expandedHeight: 300.0,
+///             flexibleSpace: FlexibleSpaceBar(
+///               stretchModes: <StretchMode>[
+///                 StretchMode.zoomBackground,
+///                 StretchMode.blurBackground,
+///                 StretchMode.fadeTitle,
+///               ],
+///               centerTitle: true,
+///               title: const Text('FlexibleSpaceBar'),
+///               background: Stack(
+///                 fit: StackFit.expand,
+///                 children: [
+///                   Image.network(
+///                     'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg',
+///                     fit: BoxFit.cover,
+///                   ),
+///                   const DecoratedBox(
+///                     decoration: BoxDecoration(
+///                       gradient: LinearGradient(
+///                         begin: Alignment(0.0, 0.5),
+///                         end: Alignment(0.0, 0.0),
+///                         colors: <Color>[
+///                           Color(0x60000000),
+///                           Color(0x00000000),
+///                         ],
+///                       ),
+///                     ),
+///                   ),
+///                 ],
+///               ),
+///             ),
+///           ),
+///           SliverList(
+///             delegate: SliverChildListDelegate([
+///               ListTile(
+///                 leading: Icon(Icons.wb_sunny),
+///                 title: Text('Sunday'),
+///                 subtitle: Text('sunny, h: 80, l: 65'),
+///               ),
+///               ListTile(
+///                 leading: Icon(Icons.wb_sunny),
+///                 title: Text('Monday'),
+///                 subtitle: Text('sunny, h: 80, l: 65'),
+///               ),
+///               // ListTiles++
+///             ]),
+///           ),
+///         ],
+///       ),
+///     );
+///   }
+/// }
+///
+/// ```
+/// {@end-tool}
+///
 /// See also:
 ///
 ///  * [SliverAppBar], which implements the expanding and contracting.
