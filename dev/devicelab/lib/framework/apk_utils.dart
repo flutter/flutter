f240348ba48d78a5ebcf85017d691b826ee62757
jonahwilliams <jonahwilliams@google.com>
2019-10-18 15:12:07 -0700

    add more debugging

--- dev/devicelab/lib/framework/apk_utils.dart
+++ dev/devicelab/lib/framework/apk_utils.dart
@@ -379,7 +379,7 @@ Future<ProcessResult> _resultOfGradleTask({String workingDirectory, String task,
 
   final List<String> args = <String>[
     'app:$task',
-    ...?options,
+    ...<String>['--debug', ...?options],
   ];
   final String gradle = path.join(workingDirectory, Platform.isWindows ? 'gradlew.bat' : './gradlew');
   print('┌── $gradle');
