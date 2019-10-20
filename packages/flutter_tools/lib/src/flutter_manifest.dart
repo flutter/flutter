8e62aeef0e6783b1622d8517747d7f53709c3486
Jonah Williams <jonahwilliams@google.com>
2019-10-20 14:51:45 -0700

    Update flutter_manifest.dart

--- packages/flutter_tools/lib/src/flutter_manifest.dart
+++ packages/flutter_tools/lib/src/flutter_manifest.dart
@@ -401,7 +401,7 @@ void _validateFlutter(YamlMap yaml, List<String> errors) {
         }
         break;
       case 'plugin':
-        if (kvp.value is! YamlMap || kvp == null) {
+        if (kvp.value is! YamlMap || kvp.value == null) {
           errors.add('Expected "${kvp.key}" to be an object, but got ${kvp.value} (${kvp.value.runtimeType}).');
           break;
         }
