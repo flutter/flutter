212da7b793a68dabdcc5daf72e0ef1bfbc0a2734
jonahwilliams <jonahwilliams@google.com>
2019-10-22 15:25:52 -0700

    re-arrange test and ResidentCompiler interface

--- packages/flutter_tools/test/general.shard/resident_runner_test.dart
+++ packages/flutter_tools/test/general.shard/resident_runner_test.dart
@@ -10,9 +10,8 @@ import 'package:flutter_tools/src/base/common.dart';
 import 'package:flutter_tools/src/base/context.dart';
 import 'package:flutter_tools/src/base/file_system.dart';
 import 'package:flutter_tools/src/base/logger.dart';
-import 'package:flutter_tools/src/base/process_manager.dart';
 import 'package:flutter_tools/src/build_info.dart';
-import 'package:flutter_tools/src/convert.dart';
+import 'package:flutter_tools/src/compile.dart';
 import 'package:flutter_tools/src/devfs.dart';
 import 'package:flutter_tools/src/device.dart';
 import 'package:flutter_tools/src/features.dart';
@@ -28,7 +27,6 @@ import 'package:mockito/mockito.dart';
 
 import '../src/common.dart';
 import '../src/context.dart';
-import '../src/mocks.dart';
 import '../src/testbed.dart';
 
 void main() {
@@ -608,39 +606,24 @@ void main() {
 
   test('FlutterDevice uses dartdevc configuration when targeting web', () => testbed.run(() async {
     final MockDevice mockDevice = MockDevice();
-    final StreamController<List<int>> stdout = StreamController<List<int>>();
     when(mockDevice.targetPlatform).thenAnswer((Invocation invocation) async {
       return TargetPlatform.web_javascript;
     });
-    when(processManager.start(any)).thenAnswer((Invocation invocation) async {
-      final List<String> arguments = invocation.positionalArguments.first;
-      // Contains configuration for dartdevc target.
-      expect(arguments,
-        contains(artifacts.getArtifactPath(Artifact.flutterWebSdk, mode: BuildMode.debug) + '/'));
-      expect(arguments,
-        contains(artifacts.getArtifactPath(Artifact.webPlatformKernelDill, mode: BuildMode.debug)));
-      expect(arguments,
-        contains('--target=dartdevc'));
-      return FakeProcess(
-        stdout: stdout.stream,
-      );
-    });
 
-    final FlutterDevice flutterDevice = await FlutterDevice.create(
+    final DefaultResidentCompiler residentCompiler = (await FlutterDevice.create(
       mockDevice,
       buildMode: BuildMode.debug,
       flutterProject: FlutterProject.current(),
       target: null,
       trackWidgetCreation: true,
-    );
-    final Future<void> onDone = flutterDevice.generator.recompile('main.dart', <Uri>[], outputPath: 'app.dill');
-    stdout.add(utf8.encode('result 578145e3-fe1c-4094-9dda-c208d5a60168\n'));
-    stdout.add(utf8.encode('578145e3-fe1c-4094-9dda-c208d5a60168\n'));
-    stdout.add(utf8.encode('+file:///foo/flutter/packages/flutter/lib/src/physics/utils.dart\n'));
-    stdout.add(utf8.encode('578145e3-fe1c-4094-9dda-c208d5a60168 app.dill 0\n'));
-    await onDone;
+    )).generator;
+
+    expect(residentCompiler.targetModel, TargetModel.dartdevc);
+    expect(residentCompiler.sdkRoot,
+      artifacts.getArtifactPath(Artifact.flutterWebSdk, mode: BuildMode.debug) + '/');
+    expect(residentCompiler.platformDill,
+      artifacts.getArtifactPath(Artifact.webPlatformKernelDill, mode: BuildMode.debug));
   }, overrides: <Type, Generator>{
-    ProcessManager: () => MockProcessManager(),
     FeatureFlags: () => TestFeatureFlags(isIncrementalCompilerEnabled: true),
   }));
 }
