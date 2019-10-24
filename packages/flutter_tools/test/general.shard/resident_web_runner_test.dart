896f54ad7d6038e43eefdfebfa7fe31d32189108
Jonah Williams <jonahwilliams@google.com>
2019-10-24 10:55:42 -0700

    catch ChromeDebugException from dwds (#43390)

--- packages/flutter_tools/test/general.shard/resident_web_runner_test.dart
+++ packages/flutter_tools/test/general.shard/resident_web_runner_test.dart
@@ -584,6 +584,25 @@ void main() {
     await expectation;
   }));
 
+  test('Successfully turns ChromeDebugError into ToolExit', () => testbed.run(() async {
+     _setupMocks();
+    final Completer<DebugConnectionInfo> connectionInfoCompleter = Completer<DebugConnectionInfo>();
+    final Completer<void> unhandledErrorCompleter = Completer<void>();
+    when(mockWebFs.connect(any)).thenAnswer((Invocation _) async {
+      unawaited(unhandledErrorCompleter.future.then((void value) {
+        throw ChromeDebugException(<String, dynamic>{});
+      }));
+      return ConnectionResult(mockAppConnection, mockDebugConnection);
+    });
+
+    final Future<void> expectation = expectLater(() => residentWebRunner.run(
+      connectionInfoCompleter: connectionInfoCompleter,
+    ), throwsA(isInstanceOf<ToolExit>()));
+
+    unhandledErrorCompleter.complete();
+    await expectation;
+  }));
+
   test('Successfully turns OptionsSkew error into ToolExit', () => testbed.run(() async {
     _setupMocks();
     final Completer<DebugConnectionInfo> connectionInfoCompleter = Completer<DebugConnectionInfo>();
