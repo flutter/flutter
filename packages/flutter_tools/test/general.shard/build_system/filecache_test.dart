8e78d3c47ebd4ed8ecefa2d1ea81f992afcaa232
Jonah Williams <jonahwilliams@google.com>
2019-10-27 12:56:23 -0700

    Update filecache_test.dart

--- packages/flutter_tools/test/general.shard/build_system/filecache_test.dart
+++ packages/flutter_tools/test/general.shard/build_system/filecache_test.dart
@@ -2,7 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:file/memory.dart';
 import 'package:flutter_tools/src/base/file_system.dart';
 import 'package:flutter_tools/src/base/logger.dart';
 import 'package:flutter_tools/src/build_system/build_system.dart';
