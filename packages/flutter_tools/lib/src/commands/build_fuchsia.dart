87bce1a4e9a7e4358c65a98be1c4ed4366fdad62
Zachary Anderson <zanderso@users.noreply.github.com>
2019-10-28 09:38:08 -0700

    [flutter_tool] Use engine flutter_runner prebuilts (#43381)
    * [flutter_tool] Use engine flutter_runner prebuilts
    
    * Update packages/flutter_tools/lib/src/fuchsia/fuchsia_build.dart
    
    Co-Authored-By: Jonah Williams <jonahwilliams@google.com>

--- packages/flutter_tools/lib/src/commands/build_fuchsia.dart
+++ packages/flutter_tools/lib/src/commands/build_fuchsia.dart
@@ -10,6 +10,7 @@ import '../base/platform.dart';
 import '../build_info.dart';
 import '../cache.dart';
 import '../fuchsia/fuchsia_build.dart';
+import '../fuchsia/fuchsia_pm.dart';
 import '../project.dart';
 import '../runner/flutter_command.dart' show FlutterCommandResult;
 import 'build.dart';
@@ -19,6 +20,17 @@ class BuildFuchsiaCommand extends BuildSubCommand {
   BuildFuchsiaCommand({bool verboseHelp = false}) {
     usesTargetOption();
     addBuildModeFlags(verboseHelp: verboseHelp);
+    argParser.addOption(
+      'runner-source',
+      help: 'The package source to use for the flutter_runner. '
+            '"${FuchsiaPackageServer.deviceHost}" implies using a runner already on the device. '
+            '"${FuchsiaPackageServer.toolHost}" implies using a runner distributed with Flutter.',
+      allowed: <String>[
+        FuchsiaPackageServer.deviceHost,
+        FuchsiaPackageServer.toolHost,
+      ],
+      defaultsTo: FuchsiaPackageServer.toolHost,
+    );
   }
 
   @override
@@ -34,7 +46,7 @@ class BuildFuchsiaCommand extends BuildSubCommand {
   };
 
   @override
-  String get description => 'build the Fuchsia target (Experimental).';
+  String get description => 'Build the Fuchsia target (Experimental).';
 
   @override
   Future<FlutterCommandResult> runCommand() async {
@@ -42,22 +54,24 @@ class BuildFuchsiaCommand extends BuildSubCommand {
     final BuildInfo buildInfo = getBuildInfo();
     final FlutterProject flutterProject = FlutterProject.current();
     if (!platform.isLinux && !platform.isMacOS) {
-      throwToolExit('"build Fuchsia" only supported on Linux and MacOS hosts.');
+      throwToolExit('"build fuchsia" is only supported on Linux and MacOS hosts.');
     }
     if (!flutterProject.fuchsia.existsSync()) {
-      throwToolExit('No Fuchsia project configured.');
+      throwToolExit('No Fuchsia project is configured.');
     }
     final String appName = flutterProject.fuchsia.project.manifest.appName;
     final String cmxPath = fs.path.join(
         flutterProject.fuchsia.meta.path, '$appName.cmx');
     final File cmxFile = fs.file(cmxPath);
     if (!cmxFile.existsSync()) {
-      throwToolExit('Fuchsia build requires a .cmx file at $cmxPath for the app');
+      throwToolExit('The Fuchsia build requires a .cmx file at $cmxPath for the app.');
     }
     await buildFuchsia(
-        fuchsiaProject: flutterProject.fuchsia,
-        target: targetFile,
-        buildInfo: buildInfo);
+      fuchsiaProject: flutterProject.fuchsia,
+      target: targetFile,
+      buildInfo: buildInfo,
+      runnerPackageSource: argResults['runner-source'],
+    );
     return null;
   }
 }
