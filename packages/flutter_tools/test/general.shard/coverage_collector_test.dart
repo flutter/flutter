900201454757c43bdd9ba4ebe4f40b40af7fbb72
Larry McKenzie <lmckenzie@users.noreply.github.com>
2019-10-30 10:35:45 -0700

    Added a null check for ranges in the sourceReport map. (#43667)

--- packages/flutter_tools/test/general.shard/coverage_collector_test.dart
+++ packages/flutter_tools/test/general.shard/coverage_collector_test.dart
@@ -20,14 +20,427 @@ void main() {
 
   test('Coverage collector Can handle coverage sentinenl data', () async {
     when(mockVMService.vm.isolates.first.invokeRpcRaw('getScripts', params: anyNamed('params')))
-      .thenAnswer((Invocation invocation) async {
-        return <String, Object>{'type': 'Sentinel', 'kind': 'Collected', 'valueAsString': '<collected>'};
-      });
+        .thenAnswer((Invocation invocation) async {
+      return <String, Object>{'type': 'Sentinel', 'kind': 'Collected', 'valueAsString': '<collected>'};
+    });
     final Map<String, Object> result = await collect(null, (String predicate) => true, connector: (Uri uri) async {
       return mockVMService;
     });
 
     expect(result, <String, Object>{'type': 'CodeCoverage', 'coverage': <Object>[]});
+  });
+
+  test('Coverage collector can handle null scripts value', () async {
+    when(mockVMService.vm.isolates.first.invokeRpcRaw('getScripts', params: anyNamed('params')))
+        .thenAnswer((Invocation invocation) async {
+      return <String, Object>{
+        'scripts': <Map<String, Object>>[
+          <String, Object>{'uri': 'some_uri', 'id': 'some_id'}
+        ]
+      };
+    });
+    when(mockVMService.vm.isolates.first.invokeRpcRaw('getSourceReport', params: anyNamed('params')))
+        .thenAnswer((Invocation invocation) async {
+      return <String, Object>{
+        'ranges': <Map<String, Object>>[<String, Object>{}]
+      };
+    });
+    when(mockVMService.vm.isolates.first.invokeRpcRaw('getObject', params: anyNamed('params')))
+        .thenAnswer((Invocation invocation) async {
+      return <String, Object>{};
+    });
+    final Map<String, Object> result = await collect(null, (String predicate) => true, connector: (Uri uri) async {
+      return mockVMService;
+    });
+
+    expect(result, <String, Object>{'type': 'CodeCoverage', 'coverage': <Object>[]});
+  });
+
+  test('Coverage collector can handle null ranges value', () async {
+    when(mockVMService.vm.isolates.first.invokeRpcRaw('getScripts', params: anyNamed('params')))
+        .thenAnswer((Invocation invocation) async {
+      return <String, Object>{
+        'scripts': <Map<String, Object>>[
+          <String, Object>{'uri': 'some_uri', 'id': 'some_id'}
+        ]
+      };
+    });
+    when(mockVMService.vm.isolates.first.invokeRpcRaw('getSourceReport', params: anyNamed('params')))
+        .thenAnswer((Invocation invocation) async {
+      return <String, Object>{'scripts': <String, Object>{}};
+    });
+    when(mockVMService.vm.isolates.first.invokeRpcRaw('getObject', params: anyNamed('params')))
+        .thenAnswer((Invocation invocation) async {
+      return <String, Object>{};
+    });
+    final Map<String, Object> result = await collect(null, (String predicate) => true, connector: (Uri uri) async {
+      return mockVMService;
+    });
+
+    expect(result, <String, Object>{'type': 'CodeCoverage', 'coverage': <Object>[]});
+  });
+
+  test('Coverage collector can handle null scriptIndex values', () async {
+    when(mockVMService.vm.isolates.first.invokeRpcRaw('getScripts', params: anyNamed('params')))
+        .thenAnswer((Invocation invocation) async {
+      return <String, Object>{
+        'scripts': <Map<String, Object>>[
+          <String, Object>{'uri': 'some_uri', 'id': 'some_id'}
+        ]
+      };
+    });
+    when(mockVMService.vm.isolates.first.invokeRpcRaw('getSourceReport', params: anyNamed('params')))
+        .thenAnswer((Invocation invocation) async {
+      return <String, Object>{
+        'scripts': <String, Object>{'uri': 'some_uri', 'id': 'some_id'},
+        'ranges': <Map<String, Object>>[
+          <String, Object>{'coverage': <String, Object>{}}
+        ]
+      };
+    });
+    when(mockVMService.vm.isolates.first.invokeRpcRaw('getObject', params: anyNamed('params')))
+        .thenAnswer((Invocation invocation) async {
+      return <String, Object>{};
+    });
+    final Map<String, Object> result = await collect(null, (String predicate) => true, connector: (Uri uri) async {
+      return mockVMService;
+    });
+
+    expect(result, <String, Object>{'type': 'CodeCoverage', 'coverage': <Object>[]});
+  });
+
+  test('Coverage collector can handle null scriptRef values', () async {
+    when(mockVMService.vm.isolates.first.invokeRpcRaw('getScripts', params: anyNamed('params')))
+        .thenAnswer((Invocation invocation) async {
+      return <String, Object>{
+        'scripts': <Map<String, Object>>[
+          <String, Object>{'uri': 'some_uri', 'id': 'some_id'}
+        ]
+      };
+    });
+    when(mockVMService.vm.isolates.first.invokeRpcRaw('getSourceReport', params: anyNamed('params')))
+        .thenAnswer((Invocation invocation) async {
+      return <String, Object>{
+        'scripts': <String, Object>{'uri': 'some_uri', 'id': 'some_id'},
+        'ranges': <Map<String, Object>>[
+          <String, Object>{'coverage': <String, Object>{}, 'scriptIndex': 'some_value'}
+        ]
+      };
+    });
+    when(mockVMService.vm.isolates.first.invokeRpcRaw('getObject', params: anyNamed('params')))
+        .thenAnswer((Invocation invocation) async {
+      return <String, Object>{};
+    });
+    final Map<String, Object> result = await collect(null, (String predicate) => true, connector: (Uri uri) async {
+      return mockVMService;
+    });
+
+    expect(result, <String, Object>{'type': 'CodeCoverage', 'coverage': <Object>[]});
+  });
+
+  test('Coverage collector can handle null scriptRef uri values', () async {
+    when(mockVMService.vm.isolates.first.invokeRpcRaw('getScripts', params: anyNamed('params')))
+        .thenAnswer((Invocation invocation) async {
+      return <String, Object>{
+        'scripts': <Map<String, Object>>[
+          <String, Object>{'uri': 'some_uri', 'id': 'some_id'}
+        ]
+      };
+    });
+    when(mockVMService.vm.isolates.first.invokeRpcRaw('getSourceReport', params: anyNamed('params')))
+        .thenAnswer((Invocation invocation) async {
+      return <String, Object>{
+        'scripts': <String, Object>{'uri': 'some_uri', 'id': 'some_id', 'index_0': <String, Object>{}},
+        'ranges': <Map<String, Object>>[
+          <String, Object>{'coverage': <String, Object>{}, 'scriptIndex': 'index_0'}
+        ]
+      };
+    });
+    when(mockVMService.vm.isolates.first.invokeRpcRaw('getObject', params: anyNamed('params')))
+        .thenAnswer((Invocation invocation) async {
+      return <String, Object>{};
+    });
+    final Map<String, Object> result = await collect(null, (String predicate) => true, connector: (Uri uri) async {
+      return mockVMService;
+    });
+
+    expect(result, <String, Object>{'type': 'CodeCoverage', 'coverage': <Object>[]});
+  });
+
+  test('Coverage collector can handle null scriptRef id values', () async {
+    when(mockVMService.vm.isolates.first.invokeRpcRaw('getScripts', params: anyNamed('params')))
+        .thenAnswer((Invocation invocation) async {
+      return <String, Object>{
+        'scripts': <Map<String, Object>>[
+          <String, Object>{'uri': 'some_uri', 'id': 'some_id'}
+        ]
+      };
+    });
+    when(mockVMService.vm.isolates.first.invokeRpcRaw('getSourceReport', params: anyNamed('params')))
+        .thenAnswer((Invocation invocation) async {
+      return <String, Object>{
+        'scripts': <String, Object>{
+          'uri': 'some_uri',
+          'id': 'some_id',
+          'index_0': <String, Object>{'uri': 'some_uri'}
+        },
+        'ranges': <Map<String, Object>>[
+          <String, Object>{'coverage': <String, Object>{}, 'scriptIndex': 'index_0'}
+        ]
+      };
+    });
+    when(mockVMService.vm.isolates.first.invokeRpcRaw('getObject', params: anyNamed('params')))
+        .thenAnswer((Invocation invocation) async {
+      return <String, Object>{};
+    });
+    final Map<String, Object> result = await collect(null, (String predicate) => true, connector: (Uri uri) async {
+      return mockVMService;
+    });
+
+    expect(result, <String, Object>{'type': 'CodeCoverage', 'coverage': <Object>[]});
+  });
+
+  test('Coverage collector can handle null scriptById values', () async {
+    when(mockVMService.vm.isolates.first.invokeRpcRaw('getScripts', params: anyNamed('params')))
+        .thenAnswer((Invocation invocation) async {
+      return <String, Object>{
+        'scripts': <Map<String, Object>>[
+          <String, Object>{'uri': 'some_uri', 'id': 'some_id'}
+        ]
+      };
+    });
+    when(mockVMService.vm.isolates.first.invokeRpcRaw('getSourceReport', params: anyNamed('params')))
+        .thenAnswer((Invocation invocation) async {
+      return <String, Object>{
+        'scripts': <String, Object>{
+          'uri': 'some_uri',
+          'id': 'some_id',
+          'index_0': <String, Object>{'uri': 'some_uri', 'id': '01'},
+        },
+        'ranges': <Map<String, Object>>[
+          <String, Object>{'coverage': <String, Object>{}, 'scriptIndex': 'index_0'}
+        ]
+      };
+    });
+    when(mockVMService.vm.isolates.first.invokeRpcRaw('getObject', params: anyNamed('params')))
+        .thenAnswer((Invocation invocation) async {
+      return <String, Object>{};
+    });
+    final Map<String, Object> result = await collect(null, (String predicate) => true, connector: (Uri uri) async {
+      return mockVMService;
+    });
+
+    expect(result, <String, Object>{'type': 'CodeCoverage', 'coverage': <Object>[]});
+  });
+
+  test('Coverage collector can handle null tokenPosTable values', () async {
+    when(mockVMService.vm.isolates.first.invokeRpcRaw('getScripts', params: anyNamed('params')))
+        .thenAnswer((Invocation invocation) async {
+      return <String, Object>{
+        'scripts': <Map<String, Object>>[
+          <String, Object>{'uri': 'some_uri', 'id': 'some_id'}
+        ]
+      };
+    });
+    when(mockVMService.vm.isolates.first.invokeRpcRaw('getSourceReport', params: anyNamed('params')))
+        .thenAnswer((Invocation invocation) async {
+      return <String, Object>{
+        'scripts': <String, Object>{
+          'uri': 'some_uri',
+          'id': 'some_id',
+          'index_0': <String, Object>{'uri': 'some_uri', 'id': '01'},
+          '01': <String, Object>{},
+        },
+        'ranges': <Map<String, Object>>[
+          <String, Object>{'coverage': <String, Object>{}, 'scriptIndex': 'index_0'}
+        ]
+      };
+    });
+    when(mockVMService.vm.isolates.first.invokeRpcRaw('getObject', params: anyNamed('params')))
+        .thenAnswer((Invocation invocation) async {
+      return <String, Object>{};
+    });
+    final Map<String, Object> result = await collect(null, (String predicate) => true, connector: (Uri uri) async {
+      return mockVMService;
+    });
+
+    expect(result, <String, Object>{
+      'type': 'CodeCoverage',
+      'coverage': <Map<String, Object>>[
+        <String, Object>{
+          'source': 'some_uri',
+          'script': <String, Object>{
+            'type': '@Script',
+            'fixedId': true,
+            'id': 'libraries/1/scripts/some_uri',
+            'uri': 'some_uri',
+            '_kind': 'library'
+          },
+          'hits': <Map<String, Object>>[]
+        }
+      ]
+    });
+  });
+
+  test('Coverage collector can handle null hits values', () async {
+    when(mockVMService.vm.isolates.first.invokeRpcRaw('getScripts', params: anyNamed('params')))
+        .thenAnswer((Invocation invocation) async {
+      return <String, Object>{
+        'scripts': <Map<String, Object>>[
+          <String, Object>{'uri': 'some_uri', 'id': 'some_id'}
+        ]
+      };
+    });
+    when(mockVMService.vm.isolates.first.invokeRpcRaw('getSourceReport', params: anyNamed('params')))
+        .thenAnswer((Invocation invocation) async {
+      return <String, Object>{
+        'scripts': <String, Object>{
+          'uri': 'some_uri',
+          'id': 'some_id',
+          'index_0': <String, Object>{'uri': 'some_uri', 'id': '01'},
+          '01': <String, Object>{'tokenPosTable': <dynamic>[]},
+        },
+        'ranges': <Map<String, Object>>[
+          <String, Object>{'coverage': <String, Object>{}, 'scriptIndex': 'index_0'}
+        ]
+      };
+    });
+    when(mockVMService.vm.isolates.first.invokeRpcRaw('getObject', params: anyNamed('params')))
+        .thenAnswer((Invocation invocation) async {
+      return <String, Object>{};
+    });
+    final Map<String, Object> result = await collect(null, (String predicate) => true, connector: (Uri uri) async {
+      return mockVMService;
+    });
+
+    expect(result, <String, Object>{
+      'type': 'CodeCoverage',
+      'coverage': <Map<String, Object>>[
+        <String, Object>{
+          'source': 'some_uri',
+          'script': <String, Object>{
+            'type': '@Script',
+            'fixedId': true,
+            'id': 'libraries/1/scripts/some_uri',
+            'uri': 'some_uri',
+            '_kind': 'library'
+          },
+          'hits': <Map<String, Object>>[]
+        }
+      ]
+    });
+  });
+
+  test('Coverage collector can handle null misses values', () async {
+    when(mockVMService.vm.isolates.first.invokeRpcRaw('getScripts', params: anyNamed('params')))
+        .thenAnswer((Invocation invocation) async {
+      return <String, Object>{
+        'scripts': <Map<String, Object>>[
+          <String, Object>{'uri': 'some_uri', 'id': 'some_id'}
+        ]
+      };
+    });
+    when(mockVMService.vm.isolates.first.invokeRpcRaw('getSourceReport', params: anyNamed('params')))
+        .thenAnswer((Invocation invocation) async {
+      return <String, Object>{
+        'scripts': <String, Object>{
+          'uri': 'some_uri',
+          'id': 'some_id',
+          'index_0': <String, Object>{'uri': 'some_uri', 'id': '01'},
+          '01': <String, Object>{'tokenPosTable': <dynamic>[]},
+        },
+        'ranges': <Map<String, Object>>[
+          <String, Object>{
+            'coverage': <String, Object>{'hits': <dynamic>[]},
+            'scriptIndex': 'index_0'
+          }
+        ]
+      };
+    });
+    when(mockVMService.vm.isolates.first.invokeRpcRaw('getObject', params: anyNamed('params')))
+        .thenAnswer((Invocation invocation) async {
+      return <String, Object>{};
+    });
+    final Map<String, Object> result = await collect(null, (String predicate) => true, connector: (Uri uri) async {
+      return mockVMService;
+    });
+
+    expect(result, <String, Object>{
+      'type': 'CodeCoverage',
+      'coverage': <Map<String, Object>>[
+        <String, Object>{
+          'source': 'some_uri',
+          'script': <String, Object>{
+            'type': '@Script',
+            'fixedId': true,
+            'id': 'libraries/1/scripts/some_uri',
+            'uri': 'some_uri',
+            '_kind': 'library'
+          },
+          'hits': <Map<String, Object>>[]
+        }
+      ]
+    });
+  });
+
+  test('Coverage collector should process hits and misses', () async {
+    when(mockVMService.vm.isolates.first.invokeRpcRaw('getScripts', params: anyNamed('params')))
+        .thenAnswer((Invocation invocation) async {
+      return <String, Object>{
+        'scripts': <Map<String, Object>>[
+          <String, Object>{'uri': 'some_uri', 'id': 'some_id'}
+        ]
+      };
+    });
+    when(mockVMService.vm.isolates.first.invokeRpcRaw('getSourceReport', params: anyNamed('params')))
+        .thenAnswer((Invocation invocation) async {
+      return <String, Object>{
+        'scripts': <String, Object>{
+          'uri': 'some_uri',
+          'id': 'some_id',
+          'index_0': <String, Object>{'uri': 'some_uri', 'id': '01'},
+          '01': <String, Object>{
+            'tokenPosTable': <dynamic>[
+              <int>[1, 100, 5, 101, 8],
+              <int>[2, 102, 7],
+            ]
+          },
+        },
+        'ranges': <Map<String, Object>>[
+          <String, Object>{
+            'coverage': <String, Object>{
+              'hits': <dynamic>[100, 101],
+              'misses': <dynamic>[102],
+            },
+            'scriptIndex': 'index_0'
+          }
+        ]
+      };
+    });
+    when(mockVMService.vm.isolates.first.invokeRpcRaw('getObject', params: anyNamed('params')))
+        .thenAnswer((Invocation invocation) async {
+      return <String, Object>{};
+    });
+    final Map<String, Object> result = await collect(null, (String predicate) => true, connector: (Uri uri) async {
+      return mockVMService;
+    });
+
+    expect(result, <String, Object>{
+      'type': 'CodeCoverage',
+      'coverage': <Map<String, Object>>[
+        <String, Object>{
+          'source': 'some_uri',
+          'script': <String, Object>{
+            'type': '@Script',
+            'fixedId': true,
+            'id': 'libraries/1/scripts/some_uri',
+            'uri': 'some_uri',
+            '_kind': 'library'
+          },
+          'hits': <int>[1, 2, 2, 0]
+        }
+      ]
+    });
   });
 }
 
@@ -38,13 +451,13 @@ class MockVMService extends Mock implements VMService {
 
 class MockVM extends Mock implements VM {
   @override
-  final List<MockIsolate> isolates = <MockIsolate>[ MockIsolate() ];
+  final List<MockIsolate> isolates = <MockIsolate>[MockIsolate()];
 }
 
 class MockIsolate extends Mock implements Isolate {}
 
 class MockProcess extends Mock implements Process {
-  final Completer<int>completer = Completer<int>();
+  final Completer<int> completer = Completer<int>();
 
   @override
   Future<int> get exitCode => completer.future;
