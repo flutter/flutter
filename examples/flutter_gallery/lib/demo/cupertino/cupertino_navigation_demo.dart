ebe34a399fb85f793d61be3c671a7d337bda3ead
LongCat is Looong <31859944+LongCatIsLooong@users.noreply.github.com>
2019-10-29 18:53:48 -0700

    update colors

--- examples/flutter_gallery/lib/demo/cupertino/cupertino_navigation_demo.dart
+++ examples/flutter_gallery/lib/demo/cupertino/cupertino_navigation_demo.dart
@@ -228,7 +228,7 @@ class Tab1RowItem extends StatelessWidget {
                         Text(
                           'Buy this cool color',
                           style: TextStyle(
-                            color: CupertinoDynamicColor.resolve(CupertinoColors.tertiaryLabel, context),
+                            color: CupertinoDynamicColor.resolve(CupertinoColors.secondaryLabel, context),
                             fontSize: 13.0,
                             fontWeight: FontWeight.w300,
                           ),
@@ -583,21 +583,31 @@ class Tab2ConversationBubble extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Color backgroundColor;
+    Color foregroundColor;
+
+    switch (color) {
+      case Tab2ConversationBubbleColor.gray:
+        backgroundColor = CupertinoDynamicColor.resolve(CupertinoColors.systemFill, context);
+        foregroundColor = CupertinoDynamicColor.resolve(CupertinoColors.label, context);
+        break;
+      case Tab2ConversationBubbleColor.blue:
+        backgroundColor = CupertinoTheme.of(context).primaryColor;
+        foregroundColor = CupertinoColors.white;
+        break;
+    }
+
     return Container(
       decoration: BoxDecoration(
         borderRadius: const BorderRadius.all(Radius.circular(18.0)),
-        color: color == Tab2ConversationBubbleColor.blue
-            ? CupertinoColors.activeBlue
-            : CupertinoColors.lightBackgroundGray,
+        color: backgroundColor,
       ),
       margin: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 8.0),
       padding: const EdgeInsets.symmetric(horizontal: 14.0, vertical: 10.0),
       child: Text(
         text,
         style: TextStyle(
-          color: color == Tab2ConversationBubbleColor.blue
-              ? CupertinoColors.white
-              : CupertinoColors.black,
+          color: foregroundColor,
           letterSpacing: -0.4,
           fontSize: 15.0,
           fontWeight: FontWeight.w400,
@@ -662,11 +672,14 @@ class Tab2ConversationRow extends StatelessWidget {
         crossAxisAlignment: isSelf ? CrossAxisAlignment.center : CrossAxisAlignment.end,
         children: <Widget>[
           if (avatar != null) avatar,
-          Tab2ConversationBubble(
-            text: text,
-            color: isSelf
+          CupertinoUserInterfaceLevel(
+            data: CupertinoUserInterfaceLevelData.elevated,
+            child: Tab2ConversationBubble(
+              text: text,
+              color: isSelf
               ? Tab2ConversationBubbleColor.blue
               : Tab2ConversationBubbleColor.gray,
+            ),
           ),
         ],
       ),
@@ -716,55 +729,47 @@ class CupertinoDemoTab3 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
-      navigationBar: CupertinoNavigationBar(
-        trailing: trailingButtons,
-      ),
-      child: DecoratedBox(
-        decoration: BoxDecoration(
-          color: CupertinoTheme.of(context).brightness == Brightness.light
-              ? CupertinoColors.extraLightBackgroundGray
-              : CupertinoColors.darkBackgroundGray,
-        ),
-        child: ListView(
-          children: <Widget>[
-            const Padding(padding: EdgeInsets.only(top: 32.0)),
-            GestureDetector(
-              onTap: () {
-                Navigator.of(context, rootNavigator: true).push(
-                  CupertinoPageRoute<bool>(
-                    fullscreenDialog: true,
-                    builder: (BuildContext context) => Tab3Dialog(),
-                  ),
-                );
-              },
-              child: Container(
-                decoration: BoxDecoration(
-                  color: CupertinoTheme.of(context).scaffoldBackgroundColor,
-                  border: const Border(
-                    top: BorderSide(color: Color(0xFFBCBBC1), width: 0.0),
-                    bottom: BorderSide(color: Color(0xFFBCBBC1), width: 0.0),
-                  ),
+    navigationBar: CupertinoNavigationBar(trailing: trailingButtons),
+    backgroundColor: CupertinoColors.systemBackground,
+      child: ListView(
+        children: <Widget>[
+          const Padding(padding: EdgeInsets.only(top: 32.0)),
+          GestureDetector(
+            onTap: () {
+              Navigator.of(context, rootNavigator: true).push(
+                CupertinoPageRoute<bool>(
+                  fullscreenDialog: true,
+                  builder: (BuildContext context) => Tab3Dialog(),
                 ),
-                height: 44.0,
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-                  child: SafeArea(
-                    top: false,
-                    bottom: false,
-                    child: Row(
-                      children: <Widget>[
-                        Text(
-                          'Sign in',
-                          style: TextStyle(color: CupertinoTheme.of(context).primaryColor),
-                        ),
-                      ],
-                    ),
+              );
+            },
+            child: Container(
+              decoration: BoxDecoration(
+                color: CupertinoTheme.of(context).scaffoldBackgroundColor,
+                border: const Border(
+                  top: BorderSide(color: Color(0xFFBCBBC1), width: 0.0),
+                  bottom: BorderSide(color: Color(0xFFBCBBC1), width: 0.0),
+                ),
+              ),
+              height: 44.0,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                child: SafeArea(
+                  top: false,
+                  bottom: false,
+                  child: Row(
+                    children: <Widget>[
+                      Text(
+                        'Sign in',
+                        style: TextStyle(color: CupertinoTheme.of(context).primaryColor),
+                      ),
+                    ],
                   ),
                 ),
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
