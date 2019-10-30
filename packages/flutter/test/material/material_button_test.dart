5aed7b87c53fc9bafd08ea71361751a757079287
Bernardo Ferrari <bernaferrari2@gmail.com>
2019-10-29 22:41:44 -0300

    Add onLongPress to Buttons (#40641)
    * Add onLongPress to Buttons.
    
    * Button enabled status will now respond to onLongPress

--- packages/flutter/test/material/material_button_test.dart
+++ packages/flutter/test/material/material_button_test.dart
@@ -298,6 +298,81 @@ void main() {
     expect(focusNode.hasPrimaryFocus, isTrue);
   });
 
+  testWidgets('MaterialButton onPressed and onLongPress callbacks are correctly called when non-null', (WidgetTester tester) async {
+
+    bool wasPressed;
+    Finder materialButton;
+
+    Widget buildFrame({ VoidCallback onPressed, VoidCallback onLongPress }) {
+      return Directionality(
+        textDirection: TextDirection.ltr,
+        child: MaterialButton(
+          child: const Text('button'),
+          onPressed: onPressed,
+          onLongPress: onLongPress,
+        ),
+      );
+    }
+
+    // onPressed not null, onLongPress null.
+    wasPressed = false;
+    await tester.pumpWidget(
+      buildFrame(onPressed: () { wasPressed = true; }, onLongPress: null),
+    );
+    materialButton = find.byType(MaterialButton);
+    expect(tester.widget<MaterialButton>(materialButton).enabled, true);
+    await tester.tap(materialButton);
+    expect(wasPressed, true);
+
+    // onPressed null, onLongPress not null.
+    wasPressed = false;
+    await tester.pumpWidget(
+      buildFrame(onPressed: null, onLongPress: () { wasPressed = true; }),
+    );
+    materialButton = find.byType(MaterialButton);
+    expect(tester.widget<MaterialButton>(materialButton).enabled, true);
+    await tester.longPress(materialButton);
+    expect(wasPressed, true);
+
+    // onPressed null, onLongPress null.
+    await tester.pumpWidget(
+      buildFrame(onPressed: null, onLongPress: null),
+    );
+    materialButton = find.byType(MaterialButton);
+    expect(tester.widget<MaterialButton>(materialButton).enabled, false);
+  });
+
+  testWidgets('MaterialButton onPressed and onLongPress callbacks are distinctly recognized', (WidgetTester tester) async {
+    bool didPressButton = false;
+    bool didLongPressButton = false;
+
+    await tester.pumpWidget(
+      Directionality(
+        textDirection: TextDirection.ltr,
+        child: MaterialButton(
+          onPressed: () {
+            didPressButton = true;
+          },
+          onLongPress: () {
+            didLongPressButton = true;
+          },
+          child: const Text('button'),
+        ),
+      ),
+    );
+
+    final Finder materialButton = find.byType(MaterialButton);
+    expect(tester.widget<MaterialButton>(materialButton).enabled, true);
+
+    expect(didPressButton, isFalse);
+    await tester.tap(materialButton);
+    expect(didPressButton, isTrue);
+
+    expect(didLongPressButton, isFalse);
+    await tester.longPress(materialButton);
+    expect(didLongPressButton, isTrue);
+  });
+
   // This test is very similar to the '...explicit splashColor and highlightColor' test
   // in icon_button_test.dart. If you change this one, you may want to also change that one.
   testWidgets('MaterialButton with explicit splashColor and highlightColor', (WidgetTester tester) async {
