fe540c854438e08243f002b7c42ec278644d9d50
Emmanuel Garcia <egarciad@google.com>
2019-10-31 14:29:21 -0700

    Fix stdout test (#43927)

--- dev/devicelab/bin/tasks/run_release_test.dart
+++ dev/devicelab/bin/tasks/run_release_test.dart
@@ -73,7 +73,7 @@ void main() {
         throw 'flutter run --release had unexpected second line: ${stdout.first}';
       }
       stdout.removeAt(0);
-      if (!(stdout.first.startsWith('Built build/app/outputs/apk/release/app-release.apk (') && stdout.first.endsWith('MB).'))) {
+      if (!(stdout.first.contains('Built build/app/outputs/apk/release/app-release.apk (') && stdout.first.contains('MB).'))) {
         throw 'flutter run --release had unexpected third line: ${stdout.first}';
       }
       stdout.removeAt(0);
