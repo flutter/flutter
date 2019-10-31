74db5dfbf55e20de36b73f9c96e3817ee57814e0
Dan Field <dnfield@google.com>
2019-10-30 23:33:38 -0700

    Revert "Reland: Migrate examples new embedding (#43868)" (#43885)
    This reverts commit 46edc80237f9f95b661c09dd1c194f47f28f1efc.

--- examples/layers/android/app/src/main/java/io/flutter/examples/Layers/MainActivity.java
+++ examples/layers/android/app/src/main/java/io/flutter/examples/Layers/MainActivity.java
@@ -4,14 +4,15 @@
 
 package io.flutter.examples.Layers;
 
-import androidx.annotation.NonNull;
-import dev.flutter.plugins.GeneratedPluginRegistrant;
-import io.flutter.embedding.android.FlutterActivity;
-import io.flutter.embedding.engine.FlutterEngine;
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
   @Override
-  public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
-    GeneratedPluginRegistrant.registerWith(flutterEngine);
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
   }
 }
+
