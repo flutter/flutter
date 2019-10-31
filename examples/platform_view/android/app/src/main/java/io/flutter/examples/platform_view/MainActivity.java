962010992694e73d3310c17af461fde130b8856a
Emmanuel Garcia <egarciad@google.com>
2019-10-31 16:17:31 -0700

    Revert "Revert "Reland: Migrate examples new embedding (#43868)" (#43885)" (#43939)

--- examples/platform_view/android/app/src/main/java/io/flutter/examples/platform_view/MainActivity.java
+++ examples/platform_view/android/app/src/main/java/io/flutter/examples/platform_view/MainActivity.java
@@ -5,13 +5,12 @@
 package io.flutter.examples.platform_view;
 
 import android.content.Intent;
-import android.os.Bundle;
-
-import io.flutter.plugins.GeneratedPluginRegistrant;
-
-import io.flutter.app.FlutterActivity;
+import androidx.annotation.NonNull;
+import dev.flutter.plugins.GeneratedPluginRegistrant;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
 
 public class MainActivity extends FlutterActivity {
   private static final String CHANNEL = "samples.flutter.io/platform_view";
@@ -21,23 +20,22 @@ public class MainActivity extends FlutterActivity {
   private MethodChannel.Result result;
 
   @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    GeneratedPluginRegistrant.registerWith(this);
+  public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+    GeneratedPluginRegistrant.registerWith(flutterEngine);
 
-    new MethodChannel(getFlutterView(), CHANNEL).setMethodCallHandler(
-            new MethodChannel.MethodCallHandler() {
-              @Override
-              public void onMethodCall(MethodCall methodCall, MethodChannel.Result result) {
-                MainActivity.this.result = result;
-                int count = methodCall.arguments();
-                if (methodCall.method.equals(METHOD_SWITCH_VIEW)) {
-                  onLaunchFullScreen(count);
-                } else {
-                  result.notImplemented();
-                }
-              }
-            }
+    new MethodChannel(flutterEngine.getDartExecutor(), CHANNEL).setMethodCallHandler(
+      new MethodChannel.MethodCallHandler() {
+        @Override
+        public void onMethodCall(MethodCall methodCall, MethodChannel.Result result) {
+          MainActivity.this.result = result;
+          int count = methodCall.arguments();
+          if (methodCall.method.equals(METHOD_SWITCH_VIEW)) {
+            onLaunchFullScreen(count);
+          } else {
+            result.notImplemented();
+          }
+        }
+      }
     );
   }
 
