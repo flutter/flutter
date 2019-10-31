de67282cddc0998652347af8727c35a8f3988dd0
Kate Lovett <katelovett@google.com>
2019-10-31 16:28:31 -0700

    Updated with external dev

--- packages/flutter/lib/src/rendering/sliver_group.dart
+++ packages/flutter/lib/src/rendering/sliver_group.dart
@@ -7,19 +7,61 @@ import 'dart:math' as math;
 import 'package:flutter/foundation.dart';
 import 'package:flutter/rendering.dart';
 
-/// A sliver that lays out its sliver children along the main axis of the view port.
-///
-/// See also:
-///
-/// * [RenderFlex], boxes to place multiple box children in a linear array.
-/// * [RenderSliverList], a sliver to place multiple box children in a linear array along the main axis.
-/// * [RenderViewport], a box to place multiple sliver children in a linear array.
-/// * [RenderSliverToBoxAdapter], to place a single box child where a sliver is expected.
-class RenderSliverGroup extends RenderSliver with ContainerRenderObjectMixin<RenderSliver, SliverPhysicalContainerParentData> {
-  /// Creates a render object that lays out it children along the main axis of the viewport
-  RenderSliverGroup({List<RenderSliver> children}) {
+/// A sliver that lays out its sliver children in the main axis, taking into
+/// account any flex factors.
+// TODO(Piinks): More Docs!
+class RenderSliverFlex extends RenderSliver with ContainerRenderObjectMixin<RenderSliver, SliverPhysicalContainerParentData> {
+  ///
+  RenderSliverFlex({
+    this.key,
+    bool pushPinnedHeaders = true,
+    double overrideGroupMaxExtent = 0.0,
+    List<RenderSliver> children,
+  }) : _pushPinnedHeaders = pushPinnedHeaders,
+      _overrideGroupMaxExtent = overrideGroupMaxExtent {
     addAll(children);
   }
+
+  /// Temp - used for debugging
+  final Key key;
+
+  /// Whether or not pinned [SliverPersistentHeaders] should be pushed off
+  /// screen.
+  bool get pushPinnedHeaders => _pushPinnedHeaders;
+  bool _pushPinnedHeaders;
+  set pushPinnedHeaders(bool newValue) {
+    assert(newValue != null);
+    if (newValue == _pushPinnedHeaders)
+      return;
+    _pushPinnedHeaders = newValue;
+    markNeedsLayout();
+  }
+
+  /// Set a custom max extent for the sliver.
+  ///
+  /// This allows for more control over rthe flex factor at different offsets
+  /// within a scrollable.
+  double get overrideGroupMaxExtent => _overrideGroupMaxExtent;
+  double _overrideGroupMaxExtent;
+  set overrideGroupMaxExtent(double newValue) {
+    assert(newValue >= 0.0);
+    if (newValue == _overrideGroupMaxExtent)
+      return;
+    _overrideGroupMaxExtent = newValue;
+    markNeedsLayout();
+  }
+
+  Iterable<RenderSliver> get _childrenInPaintOrder sync* {
+    RenderSliver child = lastChild;
+    while (child != null) {
+      yield child;
+      child = childBefore(child);
+    }
+  }
+
+  /// Doc all of these
+  double _spacePerFlex;
+  double _originalScrollExtent;
 
   @override
   void setupParentData(RenderObject child) {
@@ -27,72 +69,14 @@ class RenderSliverGroup extends RenderSliver with ContainerRenderObjectMixin<Ren
       child.parentData = SliverPhysicalContainerParentData();
   }
 
-  @override
-  void performLayout() {
-    RenderSliver child = firstChild;
-
-    double layoutOffset = 0;
-    double scrollOffset = constraints.scrollOffset;
-    double precedingScrollExtent = constraints.precedingScrollExtent;
-    double maxPaintOffset = layoutOffset + constraints.overlap;
-
-    geometry = SliverGeometry.zero;
-
-    while (child != null) {
-      final double childScrollOffset = math.max(0, scrollOffset - layoutOffset);
-
-      child.layout(
-        constraints.copyWith(
-          scrollOffset: childScrollOffset,
-          precedingScrollExtent: precedingScrollExtent,
-          overlap: maxPaintOffset - layoutOffset,
-          remainingPaintExtent: math.max(0, constraints.remainingPaintExtent - layoutOffset),
-          remainingCacheExtent: math.max(0, constraints.remainingCacheExtent - layoutOffset),
-          cacheOrigin: math.max(-childScrollOffset, constraints.cacheOrigin),
-        ),
-        parentUsesSize: true
-      );
-
-      final SliverPhysicalContainerParentData childParentData = child.parentData;
-      final SliverGeometry childGeometry = child.geometry;
-
-      geometry = SliverGeometry(
-        scrollExtent: geometry.scrollExtent + childGeometry.scrollExtent,
-        paintExtent: math.max(geometry.paintExtent, layoutOffset + childGeometry.paintOrigin + childGeometry.paintExtent),
-        layoutExtent: geometry.layoutExtent + childGeometry.layoutExtent,
-        maxPaintExtent: geometry.maxPaintExtent + childGeometry.maxPaintExtent,
-        maxScrollObstructionExtent: geometry.maxScrollObstructionExtent + childGeometry.maxScrollObstructionExtent,
-        hitTestExtent: geometry.hitTestExtent + childGeometry.hitTestExtent,
-        visible: geometry.visible || childGeometry.visible,
-        hasVisualOverflow: geometry.hasVisualOverflow || childGeometry.hasVisualOverflow,
-        scrollOffsetCorrection: childGeometry.scrollOffsetCorrection,
-        cacheExtent: geometry.cacheExtent + childGeometry.cacheExtent,
-      );
-
-      // No need to layout other children as parent has to correct the scrollOffset
-      // and layout us again.
-      if (geometry.scrollOffsetCorrection != null) {
-        return;
-      }
-
-      final double effectiveLayoutOffset = layoutOffset + childGeometry.paintOrigin;
-
-      childParentData.paintOffset = _computeAbsolutePaintOffset(child, effectiveLayoutOffset);
-
-      maxPaintOffset = math.max(effectiveLayoutOffset + childGeometry.paintExtent, maxPaintOffset);
-
-      layoutOffset = layoutOffset + math.min(constraints.remainingPaintExtent, childGeometry.layoutExtent);
-      precedingScrollExtent += childGeometry.scrollExtent;
-      scrollOffset -= childGeometry.scrollExtent;
-
-      child = childParentData.nextSibling;
-    }
-  }
-
   Offset _computeAbsolutePaintOffset(RenderSliver child, double layoutOffset) {
     assert(child != null);
     assert(child.geometry != null);
-    switch (applyGrowthDirectionToAxisDirection(constraints.axisDirection, constraints.growthDirection)) {
+    final AxisDirection axisDirection = applyGrowthDirectionToAxisDirection(
+      constraints.axisDirection,
+      constraints.growthDirection,
+    );
+    switch (axisDirection) {
       case AxisDirection.up:
       case AxisDirection.down:
         return Offset(0.0, layoutOffset);
@@ -103,44 +87,143 @@ class RenderSliverGroup extends RenderSliver with ContainerRenderObjectMixin<Ren
     return null;
   }
 
-  @override
-  double childMainAxisPosition(RenderObject child) {
-    assert(child.parent == this);
+  int _getFlex(RenderSliver child) {
     final SliverPhysicalContainerParentData childParentData = child.parentData;
-    // TODO: implement childMainAxisPosition
+    return childParentData.flex ?? 0;
   }
 
   @override
-  double childScrollOffset(RenderObject child) {
-    assert(child.parent == this);
-    // TODO
-  }
+  void performLayout() {
+    // TODO(Piinks): remaining edge cases
+    //    - exception from paintExtent > maxPaintExtent from precision error
+    //    - exception from expanding pinned headers
+    assert(constraints != null);
+    geometry = SliverGeometry.zero;
+    double groupLayoutExtent = 0;
+    double groupMaxPaintOffset = constraints.overlap;
+    double groupPrecedingScrollExtent = constraints.precedingScrollExtent;
+    double groupScrollOffset = constraints.scrollOffset;
+    int groupTotalFlex = 0;
 
-  @override
-  void paint(PaintingContext context, Offset offset) {
-    for (final RenderSliver child in _childrenInPaintOrder) {
+    RenderSliver child = firstChild;
+    while (child != null) {
+      final double childScrollOffset = math.max(0, groupScrollOffset - groupLayoutExtent);
+      final int childFlexFactor = _getFlex(child);
+      groupTotalFlex += childFlexFactor;
+
+      child.layout(
+        constraints.copyWith(
+          scrollOffset: childScrollOffset,
+          precedingScrollExtent: groupPrecedingScrollExtent,
+          overlap: groupMaxPaintOffset - groupLayoutExtent,
+          remainingPaintExtent: math.max(
+            0,
+            constraints.remainingPaintExtent - groupLayoutExtent,
+          ),
+          remainingCacheExtent: math.max(
+            0,
+            constraints.remainingCacheExtent - groupLayoutExtent,
+          ),
+          cacheOrigin: math.max(
+            -childScrollOffset,
+            constraints.cacheOrigin,
+          ),
+          flexExtent: childFlexFactor > 0 && _spacePerFlex != null ?
+            childFlexFactor * _spacePerFlex :
+            0.0,
+        ),
+        parentUsesSize: true
+      );
+
       final SliverPhysicalContainerParentData childParentData = child.parentData;
-      if (child.geometry.visible) {
-        context.paintChild(child, offset + childParentData.paintOffset);
-      }
-    }
-  }
+      final SliverGeometry childGeometry = child.geometry;
 
-  @override
-  bool hitTestChildren(HitTestResult result, {@required double mainAxisPosition, @required double crossAxisPosition}) {
-    assert(mainAxisPosition != null);
-    assert(crossAxisPosition != null);
-    for (final RenderSliver child in _childrenInPaintOrder) {
-      if (child.geometry.visible &&
-          child.hitTest(
-            result,
-            mainAxisPosition: _computeChildMainAxisPosition(child, mainAxisPosition),
-            crossAxisPosition: crossAxisPosition,
-          )) {
-        return true;
+      geometry = SliverGeometry(
+        scrollExtent: geometry.scrollExtent + childGeometry.scrollExtent,
+        paintExtent: math.max(
+          geometry.paintExtent,
+          groupLayoutExtent + childGeometry.paintOrigin + childGeometry.paintExtent,
+        ),
+        layoutExtent: geometry.layoutExtent + childGeometry.layoutExtent,
+        maxPaintExtent: geometry.maxPaintExtent + childGeometry.maxPaintExtent,
+        maxScrollObstructionExtent: geometry.maxScrollObstructionExtent + childGeometry.maxScrollObstructionExtent,
+        hitTestExtent: geometry.hitTestExtent + childGeometry.hitTestExtent,
+        visible: geometry.visible || childGeometry.visible,
+        hasVisualOverflow: geometry.hasVisualOverflow || childGeometry.hasVisualOverflow,
+        scrollOffsetCorrection: childGeometry.scrollOffsetCorrection,
+        cacheExtent: geometry.cacheExtent + childGeometry.cacheExtent,
+      );
+
+      // Scroll offset will be adjusted, and layout rerun.
+      if (geometry.scrollOffsetCorrection != null)
+        return;
+
+      final double effectiveLayoutOffset = groupLayoutExtent + childGeometry.paintOrigin;
+
+      childParentData.paintOffset = _computeAbsolutePaintOffset(
+        child,
+        effectiveLayoutOffset,
+      );
+
+      groupMaxPaintOffset = math.max(
+        effectiveLayoutOffset + childGeometry.paintExtent,
+        groupMaxPaintOffset,
+      );
+
+      groupLayoutExtent = groupLayoutExtent + math.min(
+        constraints.remainingPaintExtent,
+        childGeometry.layoutExtent
+      );
+      groupPrecedingScrollExtent += childGeometry.scrollExtent;
+      groupScrollOffset -= childGeometry.scrollExtent;
+
+      child = childParentData.nextSibling;
+    }
+
+    // Flex Factoring
+    double scrollOffsetForFlex = constraints.scrollOffset;
+    if (_originalScrollExtent == null) {
+      _originalScrollExtent = geometry.layoutExtent + constraints.precedingScrollExtent;
+      if (_originalScrollExtent < constraints.scrollOffset) {
+        _originalScrollExtent += geometry.scrollExtent;
+        scrollOffsetForFlex = 0.0;
       }
     }
-    return false;
+
+    if (_originalScrollExtent < constraints.viewportMainAxisExtent &&
+        groupTotalFlex > 0 && _spacePerFlex == null) {
+
+      _spacePerFlex = (constraints.viewportMainAxisExtent - (_originalScrollExtent + scrollOffsetForFlex) )/groupTotalFlex;
+      performLayout();
+    }
+
+    // Push Pinned Headers
+    final double headerPushExtent = -(geometry.scrollExtent - constraints.scrollOffset) + constraints.overlap;
+    double groupOffset = 0.0;
+
+    if (headerPushExtent < 0.0 &&
+        headerPushExtent.abs() <= firstChild.geometry.scrollExtent) {
+
+      groupOffset = -(firstChild.geometry.scrollExtent + headerPushExtent);
+    } else if (headerPushExtent > 0.0) {
+      groupOffset = -firstChild.geometry.scrollExtent;
+    }
+
+    if (pushPinnedHeaders && groupOffset != 0.0 &&
+      (firstChild is RenderSliverPinnedPersistentHeader || firstChild is RenderSliverFloatingPinnedPersistentHeader)) {
+
+      firstChild.layout(
+        firstChild.constraints.copyWith(flexExtent: groupOffset),
+        parentUsesSize: true,
+      );
+      geometry = geometry.copyWith(
+        paintExtent: geometry.paintExtent + groupOffset,
+        layoutExtent: math.min(
+          geometry.layoutExtent,
+          geometry.paintExtent + groupOffset,
+        ),
+      );
+    }
   }
 
   double _computeChildMainAxisPosition(RenderSliver child, double parentMainAxisPosition) {
@@ -161,17 +244,28 @@ class RenderSliverGroup extends RenderSliver with ContainerRenderObjectMixin<Ren
   }
 
   @override
-  void applyPaintTransform(RenderObject child, Matrix4 transform) {
-    assert(child != null);
-    final SliverPhysicalParentData childParentData = child.parentData;
-    childParentData.applyPaintTransform(transform);
+  bool hitTestChildren(HitTestResult result, {@required double mainAxisPosition, @required double crossAxisPosition}) {
+    assert(mainAxisPosition != null);
+    assert(crossAxisPosition != null);
+    for (final RenderSliver child in _childrenInPaintOrder) {
+      final bool childHitTest = child.hitTest(
+        result,
+        mainAxisPosition: _computeChildMainAxisPosition(child, mainAxisPosition),
+        crossAxisPosition: crossAxisPosition,
+      );
+      if (child.geometry.visible && childHitTest)
+        return true;
+    }
+    return false;
   }
 
-  Iterable<RenderSliver> get _childrenInPaintOrder sync* {
-    RenderSliver child = lastChild;
-    while (child != null) {
-      yield child;
-      child = childBefore(child);
+  @override
+  void paint(PaintingContext context, Offset offset) {
+    for (final RenderSliver child in _childrenInPaintOrder) {
+      final SliverPhysicalContainerParentData childParentData = child.parentData;
+      if (child.geometry.visible) {
+        context.paintChild(child, offset + childParentData.paintOffset);
+      }
     }
   }
 }
--- packages/flutter/lib/src/widgets/sliver.dart
+++ packages/flutter/lib/src/widgets/sliver.dart
@@ -1650,52 +1650,115 @@ class SliverFillRemaining extends SingleChildRenderObjectWidget {
   }
 }
 
-/// A sliver that lays out multiple sliver children along the main axis of the viewport.
-///
-/// {@tool sample}
-///
-/// This example uses a [SliverGroup] to apply the same padding to several slivers
-///
-/// ```dart
-/// CustomScrollView(
-///   slivers: <Widget>[
-///     SliverAppBar(
-///       title: const Text('Example'),
-///       pinned: true,
-///     ),
-///     SliverPadding(
-///       padding: const EdgeInsets.symmetric(horizontal: 16),
-///       sliver: SliverGroup(
-///         slivers: <Widget>[
-///           const SliverToBoxAdapter(child: Text('Favorites')),
-///           SliverList(
-///             delegate: SliverChildListDelegate(favorites),
-///           ),
-///           const SliverToBoxAdapter(child: Text('All items')),
-///           SliverList(
-///             delegate: SliverChildListDelegate(items),
-///           ),
-///         ],
-///       ),
-///     ),
-///   ],
-/// );
-/// ```
-/// {@end-tool}
-///
-/// See also:
-///
-/// * [Column], [Row], boxes to place multiple box children in a linear array.
-/// * [SliverList], a sliver to place multiple box children in a linear array along the main axis.
-/// * [CustomScrollView], a box to place multiple sliver children in a linear array.
-/// * [SliverToBoxAdapter], to place a single box child where a sliver is expected.
-class SliverGroup extends MultiChildRenderObjectWidget {
-  /// Creates a sliver that lays out it children along the main axis of the viewport
-  SliverGroup({@required List<Widget> slivers, Key key})
-      : super(key: key, children: slivers);
+/// A sliver that lays out its sliver children in the main axis, taking into
+/// account any flex factors.
+// TODO(Piinks): More Docs!
+class SliverFlex extends MultiChildRenderObjectWidget {
+  ///
+  SliverFlex({
+    Key key,
+    @required List<Widget> slivers,
+    bool pushPinnedHeaders = true,
+    double overrideGroupMaxExtent = 0.0,
+  }) : _pushPinnedHeaders = pushPinnedHeaders,
+      _overrideGroupMaxExtent = overrideGroupMaxExtent,
+      super(children: slivers, key: key);
+
+  final bool _pushPinnedHeaders;
+  final double _overrideGroupMaxExtent;
 
   @override
-  RenderObject createRenderObject(BuildContext context) => RenderSliverGroup();
+  RenderSliverFlex createRenderObject(BuildContext context) {
+    return RenderSliverFlex(
+      key: key,
+      pushPinnedHeaders: _pushPinnedHeaders,
+      overrideGroupMaxExtent: _overrideGroupMaxExtent,
+    );
+  }
+
+  @override
+  void updateRenderObject(BuildContext context, RenderSliverFlex renderObject) {
+    renderObject.pushPinnedHeaders = _pushPinnedHeaders;
+    renderObject.overrideGroupMaxExtent = _overrideGroupMaxExtent;
+  }
+}
+
+/// A sliver that lays out multiple sliver children along the main axis of the
+/// viewport.
+// TODO(Piinks): More Docs!
+class SliverGroup extends SliverFlex {
+  ///
+  SliverGroup({
+    Key key,
+    List<Widget> slivers = const <Widget>[],
+    bool pushPinnedHeaders = true,
+    double overrideGroupMaxExtent = 0.0,
+  }) : super(
+    key: key,
+    slivers: slivers,
+    pushPinnedHeaders: pushPinnedHeaders,
+    overrideGroupMaxExtent: overrideGroupMaxExtent,
+  );
+}
+
+/// The sliver adaptation of the [Expanded] widget.
+// TODO(Piinks): More Docs!
+class SliverExpanded extends ParentDataWidget<SliverFlex> {
+  ///
+  const SliverExpanded({
+    this.flex = 1,
+    Widget child,
+  }) : super(child: child);
+
+  /// The flex factor to use for this child
+  final int flex;
+
+  @override
+  void applyParentData(RenderObject renderObject) {
+    assert(renderObject.parentData is SliverPhysicalParentData);
+    final SliverPhysicalContainerParentData parentData = renderObject.parentData;
+    bool needsLayout = false;
+
+    if (parentData.flex != flex) {
+      parentData.flex = flex;
+      needsLayout = true;
+    }
+
+    if (needsLayout) {
+      final AbstractNode targetParent = renderObject.parent;
+      if (targetParent is RenderObject)
+        targetParent.markNeedsLayout();
+    }
+  }
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties.add(IntProperty('flex', flex));
+  }
+}
+
+/// The sliver adaptation of the [Spacer] widget.
+// TODO(Piinks): More Docs!
+class SliverSpacer extends StatelessWidget {
+  ///
+  const SliverSpacer({
+    Key key,
+    this.flex = 1
+  }) : assert(flex != null),
+      assert(flex > 0),
+      super(key: key);
+
+  ///
+  final int flex;
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverExpanded(
+      flex: flex,
+      child: const SliverToBoxAdapter(child:SizedBox.shrink()),
+    );
+  }
 }
 
 /// Mark a child as needing to stay alive even when it's in a lazy list that
