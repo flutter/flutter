267c681b1909347062e35e2aab5ee772b68e380f
Dan Field <dnfield@google.com>
2019-11-01 14:45:17 -0700

    Print the output (flutter/engine#13479)

--- engine/src/flutter/testing/run_tests.py
+++ engine/src/flutter/testing/run_tests.py
@@ -23,6 +23,9 @@ dart_tests_dir = os.path.join(buildroot_dir, 'flutter', 'testing', 'dart',)
 
 time_sensitve_test_flag = '--gtest_filter=-*TimeSensitiveTest*'
 
+def RunCmd(cmd, **kwargs):
+  print(subprocess.check_output(cmd, **kwargs))
+
 def IsMac():
   return sys.platform == 'darwin'
 
@@ -56,19 +59,19 @@ def FindExecutablePath(path):
 
 def RunEngineExecutable(build_dir, executable_name, filter, flags=[], cwd=buildroot_dir):
   if filter is not None and executable_name not in filter:
-    print 'Skipping %s due to filter.' % executable_name
+    print('Skipping %s due to filter.' % executable_name)
     return
 
   executable = FindExecutablePath(os.path.join(build_dir, executable_name))
 
-  print 'Running %s in %s' % (executable_name, cwd)
+  print('Running %s in %s' % (executable_name, cwd))
   test_command = [ executable ] + flags
-  print ' '.join(test_command)
-  subprocess.check_output(test_command, cwd=cwd)
+  print(' '.join(test_command))
+  RunCmd(test_command, cwd=cwd)
 
 
 def RunCCTests(build_dir, filter):
-  print "Running Engine Unit-tests."
+  print("Running Engine Unit-tests.")
 
   shuffle_flags = [
     "--gtest_shuffle",
@@ -115,7 +118,7 @@ def RunCCTests(build_dir, filter):
 
 
 def RunEngineBenchmarks(build_dir, filter):
-  print "Running Engine Benchmarks."
+  print("Running Engine Benchmarks.")
 
   RunEngineExecutable(build_dir, 'shell_benchmarks', filter)
 
@@ -127,7 +130,7 @@ def RunEngineBenchmarks(build_dir, filter):
 
 
 def SnapshotTest(build_dir, dart_file, kernel_file_output, verbose_dart_snapshot):
-  print "Generating snapshot for test %s" % dart_file
+  print("Generating snapshot for test %s" % dart_file)
 
   dart = os.path.join(build_dir, 'dart-sdk', 'bin', 'dart')
   frontend_server = os.path.join(build_dir, 'gen', 'frontend_server.dart.snapshot')
@@ -155,10 +158,9 @@ def SnapshotTest(build_dir, dart_file, kernel_file_output, verbose_dart_snapshot
   ]
 
   if verbose_dart_snapshot:
-    subprocess.check_output(snapshot_command, cwd=buildroot_dir)
+    RunCmd(snapshot_command, cwd=buildroot_dir)
   else:
-    with open(os.devnull,"w") as out_file:
-      subprocess.check_output(snapshot_command, cwd=buildroot_dir, stdout=out_file)
+    subprocess.check_output(snapshot_command, cwd=buildroot_dir)
   assert os.path.exists(kernel_file_output)
 
 
@@ -180,17 +182,17 @@ def RunDartTest(build_dir, dart_file, verbose_dart_snapshot, multithreaded):
   else:
     threading = 'single-threaded'
 
-  print "Running test '%s' using 'flutter_tester' (%s)" % (kernel_file_name, threading)
+  print("Running test '%s' using 'flutter_tester' (%s)" % (kernel_file_name, threading))
   RunEngineExecutable(build_dir, 'flutter_tester', None, command_args)
 
 def RunPubGet(build_dir, directory):
-  print "Running 'pub get' in the tests directory %s" % dart_tests_dir
+  print("Running 'pub get' in the tests directory %s" % dart_tests_dir)
 
   pub_get_command = [
     os.path.join(build_dir, 'dart-sdk', 'bin', 'pub'),
     'get'
   ]
-  subprocess.check_output(pub_get_command, cwd=directory)
+  RunCmd(pub_get_command, cwd=directory)
 
 
 def EnsureDebugUnoptSkyPackagesAreBuilt():
@@ -206,7 +208,7 @@ def EnsureDebugUnoptSkyPackagesAreBuilt():
   # Attempt running Ninja if the out directory exists.
   # We don't want to blow away any custom GN args the caller may have already set.
   if os.path.exists(variant_out_dir):
-    subprocess.check_output(ninja_command, cwd=buildroot_dir)
+    RunCmd(ninja_command, cwd=buildroot_dir)
     return
 
   gn_command = [
@@ -217,8 +219,8 @@ def EnsureDebugUnoptSkyPackagesAreBuilt():
     '--no-lto',
   ]
 
-  subprocess.check_output(gn_command, cwd=buildroot_dir)
-  subprocess.check_output(ninja_command, cwd=buildroot_dir)
+  RunCmd(gn_command, cwd=buildroot_dir)
+  RunCmd(ninja_command, cwd=buildroot_dir)
 
 def EnsureJavaTestsAreBuilt(android_out_dir):
   ninja_command = [
@@ -231,7 +233,7 @@ def EnsureJavaTestsAreBuilt(android_out_dir):
   # Attempt running Ninja if the out directory exists.
   # We don't want to blow away any custom GN args the caller may have already set.
   if os.path.exists(android_out_dir):
-    subprocess.check_output(ninja_command, cwd=buildroot_dir)
+    RunCmd(ninja_command, cwd=buildroot_dir)
     return
 
   # Otherwise prepare the directory first, then build the test.
@@ -242,8 +244,8 @@ def EnsureJavaTestsAreBuilt(android_out_dir):
     '--runtime-mode=debug',
     '--no-lto',
   ]
-  subprocess.check_output(gn_command, cwd=buildroot_dir)
-  subprocess.check_output(ninja_command, cwd=buildroot_dir)
+  RunCmd(gn_command, cwd=buildroot_dir)
+  RunCmd(ninja_command, cwd=buildroot_dir)
 
 def AssertExpectedJavaVersion():
   EXPECTED_VERSION = '1.8'
@@ -277,7 +279,7 @@ def RunJavaTests(filter, android_variant='android_debug_unopt'):
     test_class
   ]
 
-  return subprocess.check_output(command)
+  RunCmd(command)
 
 def RunDartTests(build_dir, filter, verbose_dart_snapshot):
   # This one is a bit messy. The pubspec.yaml at flutter/testing/dart/pubspec.yaml
@@ -292,9 +294,9 @@ def RunDartTests(build_dir, filter, verbose_dart_snapshot):
 
   for dart_test_file in dart_tests:
     if filter is not None and os.path.basename(dart_test_file) not in filter:
-      print "Skipping %s due to filter." % dart_test_file
+      print("Skipping %s due to filter." % dart_test_file)
     else:
-      print "Testing dart file %s" % dart_test_file
+      print("Testing dart file %s" % dart_test_file)
       RunDartTest(build_dir, dart_test_file, verbose_dart_snapshot, True)
       RunDartTest(build_dir, dart_test_file, verbose_dart_snapshot, False)
 
