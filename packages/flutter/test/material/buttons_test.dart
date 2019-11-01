36daffffd786daa48d777f01bd3391070dbc3d36
Nikita Zhelonkin <niks.mbox@gmail.com>
2019-11-01 19:58:22 +0300

    Remove unused import

--- packages/flutter/test/material/buttons_test.dart
+++ packages/flutter/test/material/buttons_test.dart
@@ -10,7 +10,6 @@ import 'package:flutter_test/flutter_test.dart';
 
 import '../rendering/mock_canvas.dart';
 import '../widgets/semantics_tester.dart';
-import 'feedback_tester.dart';
 
 void main() {
   setUp(() {
