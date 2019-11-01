a6b6da69c5b1a5e48b4a7e8208b8f7ebfef789f5
Justin McCandless <jmccandless@google.com>
2019-11-01 17:46:08 -0400

    Remove GridPhotoViewer code that's been replaced by InteractiveViewer

--- examples/flutter_gallery/lib/demo/material/grid_list_demo.dart
+++ examples/flutter_gallery/lib/demo/material/grid_list_demo.dart
@@ -14,7 +14,6 @@ enum GridDemoTileStyle {
 
 typedef BannerTapCallback = void Function(Photo photo);
 
-const double _kMinFlingVelocity = 800.0;
 const String _kGalleryAssetsPackage = 'flutter_gallery_assets';
 
 class Photo {
@@ -37,15 +36,6 @@ class Photo {
   bool get isValid => assetName != null && title != null && caption != null && isFavorite != null;
 }
 
-class GridPhotoViewer extends StatefulWidget {
-  const GridPhotoViewer({ Key key, this.photo }) : super(key: key);
-
-  final Photo photo;
-
-  @override
-  _GridPhotoViewerState createState() => _GridPhotoViewerState();
-}
-
 class _GridTitleText extends StatelessWidget {
   const _GridTitleText(this.text);
 
@@ -57,95 +47,6 @@ class _GridTitleText extends StatelessWidget {
       fit: BoxFit.scaleDown,
       alignment: Alignment.centerLeft,
       child: Text(text),
-    );
-  }
-}
-
-class _GridPhotoViewerState extends State<GridPhotoViewer> with SingleTickerProviderStateMixin {
-  AnimationController _controller;
-  Animation<Offset> _flingAnimation;
-  Offset _offset = Offset.zero;
-  double _scale = 1.0;
-  Offset _normalizedOffset;
-  double _previousScale;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = AnimationController(vsync: this)
-      ..addListener(_handleFlingAnimation);
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  // The maximum offset value is 0,0. If the size of this renderer's box is w,h
-  // then the minimum offset value is w - _scale * w, h - _scale * h.
-  Offset _clampOffset(Offset offset) {
-    final Size size = context.size;
-    final Offset minOffset = Offset(size.width, size.height) * (1.0 - _scale);
-    return Offset(offset.dx.clamp(minOffset.dx, 0.0), offset.dy.clamp(minOffset.dy, 0.0));
-  }
-
-  void _handleFlingAnimation() {
-    setState(() {
-      _offset = _flingAnimation.value;
-    });
-  }
-
-  void _handleOnScaleStart(ScaleStartDetails details) {
-    setState(() {
-      _previousScale = _scale;
-      _normalizedOffset = (details.focalPoint - _offset) / _scale;
-      // The fling animation stops if an input gesture starts.
-      _controller.stop();
-    });
-  }
-
-  void _handleOnScaleUpdate(ScaleUpdateDetails details) {
-    setState(() {
-      _scale = (_previousScale * details.scale).clamp(1.0, 4.0);
-      // Ensure that image location under the focal point stays in the same place despite scaling.
-      _offset = _clampOffset(details.focalPoint - _normalizedOffset * _scale);
-    });
-  }
-
-  void _handleOnScaleEnd(ScaleEndDetails details) {
-    final double magnitude = details.velocity.pixelsPerSecond.distance;
-    if (magnitude < _kMinFlingVelocity)
-      return;
-    final Offset direction = details.velocity.pixelsPerSecond / magnitude;
-    final double distance = (Offset.zero & context.size).shortestSide;
-    _flingAnimation = _controller.drive(Tween<Offset>(
-      begin: _offset,
-      end: _clampOffset(_offset + direction * distance),
-    ));
-    _controller
-      ..value = 0.0
-      ..fling(velocity: magnitude / 1000.0);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onScaleStart: _handleOnScaleStart,
-      onScaleUpdate: _handleOnScaleUpdate,
-      onScaleEnd: _handleOnScaleEnd,
-      child: ClipRect(
-        child: Transform(
-          transform: Matrix4.identity()
-            ..translate(_offset.dx, _offset.dy)
-            ..scale(_scale),
-          child: Image.asset(
-            widget.photo.assetName,
-            package: widget.photo.assetPackage,
-            fit: BoxFit.cover,
-          ),
-        ),
-      ),
     );
   }
 }
@@ -175,8 +76,6 @@ class GridDemoPhotoItem extends StatelessWidget {
           body: SizedBox.expand(
             child: Hero(
               tag: photo.tag,
-              // TODO(justinmc): Remove this and its code.
-              //child: GridPhotoViewer(photo: photo),
               child: InteractiveViewer(
                 disableRotation: true,
                 child: Image.asset(
