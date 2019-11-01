b94c50ad9edad2fb383740971f836d2337ee77d6
Emmanuel Garcia <egarciad@google.com>
2019-11-01 11:13:39 -0700

    Revert: Migrate examples to Android embedding v2 (#43997)
    This reverts commit 99cd4403848185c530f6eadd43d1511376c45c15.

--- examples/hello_world/android/app/src/main/java/io/flutter/examples/hello_world/MainActivity.java
+++ examples/hello_world/android/app/src/main/java/io/flutter/examples/hello_world/MainActivity.java
@@ -1,13 +1,13 @@
 package io.flutter.examples.hello_world;
 
-import androidx.annotation.NonNull;
-import dev.flutter.plugins.GeneratedPluginRegistrant;
-import io.flutter.embedding.android.FlutterActivity;
-import io.flutter.embedding.engine.FlutterEngine;
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
   @Override
-  public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
-    GeneratedPluginRegistrant.registerWith(flutterEngine);
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
   }
 }
