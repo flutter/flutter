7e3e60d9027f6645c220e34c9d72c17f308d7e98
xster <xster@google.com>
2019-11-01 17:53:15 -0700

    Release shim bindings when detaching (flutter/engine#13432)

--- engine/src/flutter/shell/platform/android/test/io/flutter/FlutterTestSuite.java
+++ engine/src/flutter/shell/platform/android/test/io/flutter/FlutterTestSuite.java
@@ -15,6 +15,7 @@ import io.flutter.embedding.android.FlutterViewTest;
 import io.flutter.embedding.engine.FlutterEngineCacheTest;
 import io.flutter.embedding.engine.FlutterJNITest;
 import io.flutter.embedding.engine.RenderingComponentTest;
+import io.flutter.embedding.engine.plugins.shim.ShimPluginRegistryTest;
 import io.flutter.embedding.engine.renderer.FlutterRendererTest;
 import io.flutter.embedding.engine.systemchannels.PlatformChannelTest;
 import io.flutter.plugin.common.StandardMessageCodecTest;
@@ -40,6 +41,7 @@ import test.io.flutter.embedding.engine.dart.DartExecutorTest;
     PreconditionsTest.class,
     RenderingComponentTest.class,
     StandardMessageCodecTest.class,
+    ShimPluginRegistryTest.class,
     SingleViewPresentationTest.class,
     SmokeTest.class,
     TextInputPluginTest.class,
