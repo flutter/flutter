69d4ffb36da60db917090344800b82b963152555
xster <xster@google.com>
2019-11-05 12:38:42 -0800

    Add Android embedding version analytics (#44043)

--- packages/flutter_tools/lib/src/reporting/usage.dart
+++ packages/flutter_tools/lib/src/reporting/usage.dart
@@ -55,6 +55,8 @@ enum CustomDimensions {
   commandBuildAppBundleBuildMode, // cd42
   buildEventError,  // cd43
   commandResultEventMaxRss,  // cd44
+  commandRunAndroidEmbeddingVersion, // cd45
+  commandPackagesAndroidEmbeddingVersion, // cd46
 }
 
 String cdKey(CustomDimensions cd) => 'cd${cd.index + 1}';
