e8a081f48ab38e36526b932a5da2097d94b57117
Kate Lovett <katelovett@google.com>
2019-11-06 13:21:08 -0800

    Analyzer therapy

--- packages/flutter/lib/src/widgets/nested_scroll_view.dart
+++ packages/flutter/lib/src/widgets/nested_scroll_view.dart
@@ -91,7 +91,7 @@ typedef NestedScrollViewHeaderSliversBuilder = List<Widget> Function(BuildContex
 ///           // This is not necessary if the "headerSliverBuilder" only builds
 ///           // widgets that do not overlap the next sliver.
 ///           handle: NestedScrollView.sliverOverlapAbsorberHandleFor(context),
-///           child: SliverAppBar(
+///           sliver: SliverAppBar(
 ///             title: const Text('Books'), // This is the title in the app bar.
 ///             pinned: true,
 ///             expandedHeight: 150.0,
