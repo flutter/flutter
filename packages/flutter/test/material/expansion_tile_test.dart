baf7fb75b4323322c8546cbdd1cb53f641628348
Luiz França <francaldr@gmail.com>
2019-11-07 00:34:49 -0300

    Removing unnecessary configs

--- packages/flutter/test/material/expansion_tile_test.dart
+++ packages/flutter/test/material/expansion_tile_test.dart
@@ -104,23 +104,27 @@ void main() {
 
     double getHeight(Key key) => tester.getSize(find.byKey(key)).height;
     Container getContainer(Key key) => tester.firstWidget(find.descendant(
-      of: find.byKey(key),
-      matching: find.byType(Container),
-    ));
+          of: find.byKey(key),
+          matching: find.byType(Container),
+        ));
 
-    expect(getHeight(topKey), getHeight(expandedKey) - getHeight(tileKey) - 2.0);
+    expect(
+        getHeight(topKey), getHeight(expandedKey) - getHeight(tileKey) - 2.0);
     expect(getHeight(topKey), getHeight(collapsedKey) - 2.0);
     expect(getHeight(topKey), getHeight(defaultKey) - 2.0);
 
-    BoxDecoration expandedContainerDecoration = getContainer(expandedKey).decoration;
+    BoxDecoration expandedContainerDecoration =
+        getContainer(expandedKey).decoration;
     expect(expandedContainerDecoration.color, Colors.red);
     expect(expandedContainerDecoration.border.top.color, _dividerColor);
     expect(expandedContainerDecoration.border.bottom.color, _dividerColor);
 
-    BoxDecoration collapsedContainerDecoration = getContainer(collapsedKey).decoration;
+    BoxDecoration collapsedContainerDecoration =
+        getContainer(collapsedKey).decoration;
     expect(collapsedContainerDecoration.color, Colors.transparent);
     expect(collapsedContainerDecoration.border.top.color, Colors.transparent);
-    expect(collapsedContainerDecoration.border.bottom.color, Colors.transparent);
+    expect(
+        collapsedContainerDecoration.border.bottom.color, Colors.transparent);
 
     await tester.tap(find.text('Expanded'));
     await tester.tap(find.text('Collapsed'));
@@ -130,17 +134,21 @@ void main() {
 
     // Pump to the middle of the animation for expansion.
     await tester.pump(const Duration(milliseconds: 100));
-    final BoxDecoration collapsingContainerDecoration = getContainer(collapsedKey).decoration;
+    final BoxDecoration collapsingContainerDecoration =
+        getContainer(collapsedKey).decoration;
     expect(collapsingContainerDecoration.color, Colors.transparent);
     // Opacity should change but color component should remain the same.
-    expect(collapsingContainerDecoration.border.top.color, const Color(0x15333333));
-    expect(collapsingContainerDecoration.border.bottom.color, const Color(0x15333333));
+    expect(collapsingContainerDecoration.border.top.color,
+        const Color(0x15333333));
+    expect(collapsingContainerDecoration.border.bottom.color,
+        const Color(0x15333333));
 
     // Pump all the way to the end now.
     await tester.pump(const Duration(seconds: 1));
 
     expect(getHeight(topKey), getHeight(expandedKey) - 2.0);
-    expect(getHeight(topKey), getHeight(collapsedKey) - getHeight(tileKey) - 2.0);
+    expect(
+        getHeight(topKey), getHeight(collapsedKey) - getHeight(tileKey) - 2.0);
     expect(getHeight(topKey), getHeight(defaultKey) - getHeight(tileKey) - 2.0);
 
     // Expanded should be collapsed now.
@@ -195,8 +203,10 @@ void main() {
       ),
     );
 
-    Color iconColor(Key key) => tester.state<TestIconState>(find.byKey(key)).iconTheme.color;
-    Color textColor(Key key) => tester.state<TestTextState>(find.byKey(key)).textStyle.color;
+    Color iconColor(Key key) =>
+        tester.state<TestIconState>(find.byKey(key)).iconTheme.color;
+    Color textColor(Key key) =>
+        tester.state<TestTextState>(find.byKey(key)).textStyle.color;
 
     expect(textColor(expandedTitleKey), _accentColor);
     expect(textColor(collapsedTitleKey), _headerColor);
@@ -219,26 +229,11 @@ void main() {
   testWidgets('ExpansionTile subtitle', (WidgetTester tester) async {
     await tester.pumpWidget(
       MaterialApp(
-        theme: ThemeData(
-          platform: TargetPlatform.iOS,
-          accentColor: _accentColor,
-          unselectedWidgetColor: _unselectedWidgetColor,
-          textTheme: const TextTheme(subhead: TextStyle(color: _headerColor)),
-        ),
-        home: Material(
-          child: SingleChildScrollView(
-            child: Column(
-              children: <Widget>[
-                const ListTile(title: Text('Top')),
-                ExpansionTile(
-                  initiallyExpanded: true,
-                  title: const Text('Title'),
-                  subtitle: const Text('Subtitle'),
-                  backgroundColor: Colors.red,
-                  children: const <Widget>[ListTile(title: Text('0'))],
-                ),
-              ],
-            ),
+        home: Scaffold(
+          body: const ExpansionTile(
+            title: Text('Title'),
+            subtitle: Text('Subtitle'),
+            children: <Widget>[ListTile(title: Text('0'))],
           ),
         ),
       ),
