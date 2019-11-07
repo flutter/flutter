0afc1731610c9a69027cbdd5032d0d2722d533c6
LongCat is Looong <31859944+LongCatIsLooong@users.noreply.github.com>
2019-11-06 22:08:06 -0800

    fix import

--- packages/flutter/lib/src/cupertino/icon_theme_data.dart
+++ packages/flutter/lib/src/cupertino/icon_theme_data.dart
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:flutter/src/foundation/diagnostics.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
 import 'colors.dart';
 
