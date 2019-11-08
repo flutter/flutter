1e836050f81cca74de1fa8b251cbc7a1c0ee7afd
Tong Mu <dkwingsmt@gmail.com>
2019-11-07 17:55:58 -0800

    More tests

--- packages/flutter/test/widgets/mouse_region_my_test.dart
+++ packages/flutter/test/widgets/mouse_region_my_test.dart
@@ -4,9 +4,41 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter/gestures.dart';
 
 void main() {
-  testWidgets('detects pointer enter', (WidgetTester tester) async {
+
+  Widget scaffold({List<Widget> children}) {
+    return Directionality(
+      textDirection: TextDirection.ltr,
+      child: Container(
+        width: 100,
+        height: 100,
+        alignment: Alignment.topLeft,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: children,
+        ),
+      ),
+    );
+  }
+
+  Widget mouseRegionInner({bool hovered, void Function(bool) setHovered}) {
+    return Container(
+      width: 10,
+      height: 10,
+      child: MouseRegion(
+        onEnter: (_) { setHovered(true); },
+        onExit: (_) { setHovered(false); },
+        child: hovered ? const Text('hover inner') : const Text('unhover inner'),
+      ),
+    );
+  }
+
+  Widget outerText({bool hovered}) {
+    return hovered ? const Text('hover outer') : const Text('unhover outer');
+  }
+
+  testWidgets('startup', (WidgetTester tester) async {
     bool hovered = false;
-    StateSetter mySetState;
 
     final TestGesture gesture = await tester.createGesture(kind: PointerDeviceKind.mouse);
     await gesture.addPointer(location: const Offset(5, 5));
@@ -14,31 +46,13 @@ void main() {
 
     await tester.pumpWidget(
       StatefulBuilder(builder: (BuildContext context, StateSetter setState) {
-        mySetState = setState;
-        return Directionality(
-          textDirection: TextDirection.ltr,
-          child: Container(
-            width: 100,
-            height: 100,
-            alignment: Alignment.topLeft,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Container(
-                  width: 10,
-                  height: 10,
-                  child: MouseRegion(
-                    onEnter: (_) { setState(() { hovered = true; }); },
-                    onExit: (_) { setState(() { hovered = false; }); },
-                    child: hovered ? const Text('hover outer') : const Text('unhover outer'),
-                  ),
-                ),
-                if (hovered) const Text('hover inner') else const Text('unhover inner'),
-              ],
-            ),
+        return scaffold(children: <Widget>[
+          mouseRegionInner(
+            hovered: hovered,
+            setHovered: (bool value) { setState(() { hovered = value; }); }
           ),
-        );
+          outerText(hovered: hovered),
+        ]);
       }),
     );
 
@@ -49,5 +63,81 @@ void main() {
 
     expect(find.text('hover outer'), findsOneWidget);
     expect(find.text('hover inner'), findsOneWidget);
+  });
+
+  testWidgets('attach', (WidgetTester tester) async {
+    bool hovered = false;
+
+    final TestGesture gesture = await tester.createGesture(kind: PointerDeviceKind.mouse);
+    await gesture.addPointer(location: const Offset(5, 5));
+    addTearDown(gesture.removePointer);
+
+    await tester.pumpWidget(
+      StatefulBuilder(builder: (BuildContext context, StateSetter setState) {
+        return scaffold(children: <Widget>[
+          outerText(hovered: hovered),
+        ]);
+      }),
+    );
+
+    expect(find.text('unhover outer'), findsOneWidget);
+
+    await tester.pumpWidget(
+      StatefulBuilder(builder: (BuildContext context, StateSetter setState) {
+        return scaffold(children: <Widget>[
+          mouseRegionInner(
+            hovered: hovered,
+            setHovered: (bool value) { setState(() { hovered = value; }); }
+          ),
+          outerText(hovered: hovered),
+        ]);
+      }),
+    );
+
+    expect(find.text('unhover outer'), findsOneWidget);
+    expect(find.text('unhover inner'), findsOneWidget);
+
+    await tester.pump();
+
+    expect(find.text('hover outer'), findsOneWidget);
+    expect(find.text('hover inner'), findsOneWidget);
+  });
+
+  testWidgets('detach', (WidgetTester tester) async {
+    bool hovered = true;
+
+    final TestGesture gesture = await tester.createGesture(kind: PointerDeviceKind.mouse);
+    await gesture.addPointer(location: const Offset(5, 5));
+    addTearDown(gesture.removePointer);
+
+    await tester.pumpWidget(
+      StatefulBuilder(builder: (BuildContext context, StateSetter setState) {
+        return scaffold(children: <Widget>[
+          mouseRegionInner(
+            hovered: hovered,
+            setHovered: (bool value) { print(value); setState(() { hovered = value; }); }
+          ),
+          outerText(hovered: hovered),
+        ]);
+      }),
+    );
+
+    expect(find.text('hover outer'), findsOneWidget);
+    expect(find.text('hover inner'), findsOneWidget);
+
+    // Throws
+    await tester.pumpWidget(
+      StatefulBuilder(builder: (BuildContext context, StateSetter setState) {
+        return scaffold(children: <Widget>[
+          outerText(hovered: hovered),
+        ]);
+      }),
+    );
+
+    // expect(find.text('hover outer'), findsOneWidget);
+
+    // await tester.pump();
+
+    // expect(find.text('unhover outer'), findsOneWidget);
   });
 }
