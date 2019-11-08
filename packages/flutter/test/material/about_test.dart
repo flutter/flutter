55c89bf01aa06260732199821a5d4ccd1a184b1e
Fourman Alex <fourman.alex@gmail.com>
2019-11-08 18:54:25 +0200

    about_test.dart: formatting

--- packages/flutter/test/material/about_test.dart
+++ packages/flutter/test/material/about_test.dart
@@ -450,24 +450,25 @@ void main() {
     expect(nestedObserver.dialogCount, 1);
   });
 
-  testWidgets(
-      "AboutListTile's child should not be offset when the icon is not specified.",
-          (WidgetTester tester) async {
-        await tester.pumpWidget(
-          const MaterialApp(
-            home: Scaffold(
-              body: AboutListTile(
-                child: Text('About'),
-              ),
-            ),
+  testWidgets("AboutListTile's child should not be offset when the icon is not specified.", (WidgetTester tester) async {
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: Scaffold(
+          body: AboutListTile(
+            child: Text('About'),
           ),
-        );
+        ),
+      ),
+    );
 
-        expect(
-            find.descendant(
-                of: find.byType(AboutListTile), matching: find.byType(Icon)),
-            findsNothing);
-      });
+    expect(
+      find.descendant(
+        of: find.byType(AboutListTile),
+        matching: find.byType(Icon),
+      ),
+      findsNothing,
+    );
+  });
 }
 
 class FakeLicenseEntry extends LicenseEntry {
