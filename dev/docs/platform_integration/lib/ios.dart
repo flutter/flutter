d1e66f96bb8555084585e6ab2b482d5511c244c4
Ian Hickson <ian@hixie.ch>
2019-11-08 16:53:21 -0800

    Make sure all our .dart files have license headers (#44467)

--- dev/docs/platform_integration/lib/ios.dart
+++ dev/docs/platform_integration/lib/ios.dart
@@ -1,2 +1,6 @@
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 /// [Flutter platform integration APIs for iOS.](https://docs.flutter.io/objcdoc/)
 library iOS;
