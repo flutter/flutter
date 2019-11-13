9a3042cb50a7cc2a354e2ffe3c3ef26a2f79893a
Todd Volkert <tvolkert@users.noreply.github.com>
2019-11-13 14:12:46 -0800

    Remove unused import (#13832)

--- shell/platform/android/io/flutter/embedding/engine/FlutterEngine.java
+++ shell/platform/android/io/flutter/embedding/engine/FlutterEngine.java
@@ -8,7 +8,6 @@ import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.HashSet;
 import java.util.Set;
