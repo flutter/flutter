01327cfff152463139cd028af6fc61c4a1685f78
Kaushik Iska <iska.kaushik@gmail.com>
2019-11-13 14:33:21 -0800

    Disable LTO on Fuchsia (flutter/engine#13842)

--- engine/src/flutter/tools/fuchsia/build_fuchsia_artifacts.py
+++ engine/src/flutter/tools/fuchsia/build_fuchsia_artifacts.py
@@ -207,8 +207,10 @@ def BuildTarget(runtime_mode, arch, product, enable_lto):
       runtime_mode,
   ]
 
-  if not enable_lto:
-    flags.append('--no-lto')
+  # Always disable lto until https://github.com/flutter/flutter/issues/44841
+  # gets fixed.
+  # if not enable_lto:
+  flags.append('--no-lto')
 
   RunGN(out_dir, flags)
   BuildNinjaTargets(out_dir, GetTargetsToBuild(product))
