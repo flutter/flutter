2bbb85d8f58864c94fa3bf25e7c64fbf7f555d7f
Jonah Williams <jonahwilliams@google.com>
2019-11-13 13:01:41 -0800

    Allow specifying device-vmservice-port and host-vmservice-port (#44027)

--- packages/flutter_tools/test/commands.shard/hermetic/attach_test.dart
+++ packages/flutter_tools/test/commands.shard/hermetic/attach_test.dart
@@ -400,7 +400,7 @@ void main() {
           ],
         );
         await completer.future;
-        verifyNever(portForwarder.forward(devicePort));
+        verifyNever(portForwarder.forward(devicePort, hostPort: anyNamed('hostPort')));
 
         await expectLoggerInterruptEndsTask(task, logger);
         await loggerSubscription.cancel();
@@ -432,7 +432,7 @@ void main() {
           ],
         );
         await completer.future;
-        verifyNever(portForwarder.forward(devicePort));
+        verifyNever(portForwarder.forward(devicePort, hostPort: anyNamed('hostPort')));
 
         await expectLoggerInterruptEndsTask(task, logger);
         await loggerSubscription.cancel();
