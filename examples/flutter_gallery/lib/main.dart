ff86b5ddb14187a088965dff4f1a9abed5a46ad9
Greg Spencer <gspencer@google.com>
2019-11-12 18:59:57 -0800

    ensure traversed nodes are visible

--- examples/flutter_gallery/lib/main.dart
+++ examples/flutter_gallery/lib/main.dart
@@ -4,11 +4,19 @@
 
 // Thanks for checking out Flutter!
 // Like what you see? Tweet us @FlutterDev
+import 'dart:io';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 import 'gallery/app.dart';
 
 void main() {
+  if (!kIsWeb && Platform.isMacOS) {
+    // TODO(gspencergoog): Update this when TargetPlatform includes macOS. https://github.com/flutter/flutter/issues/31366
+    // See https://github.com/flutter/flutter/wiki/Desktop-shells#target-platform-override
+    debugDefaultTargetPlatformOverride = TargetPlatform.fuchsia;
+  }
+
   runApp(const GalleryApp());
 }
