f821bd3bd5cd6d2f1547f10567ddf3a0c96c6987
Greg Spencer <gspencer@google.com>
2019-11-13 10:05:44 -0800

    Update manual_tests to be able to run on macos/web

--- dev/manual_tests/lib/text.dart
+++ dev/manual_tests/lib/text.dart
@@ -13,7 +13,7 @@ import 'package:flutter/scheduler.dart';
 int seed = 0;
 
 void main() {
-  if (Platform.isMacOS) {
+  if (!kIsWeb && Platform.isMacOS) {
     // TODO(gspencergoog): Update this when TargetPlatform includes macOS. https://github.com/flutter/flutter/issues/31366
     // See https://github.com/flutter/flutter/wiki/Desktop-shells#target-platform-override
     debugDefaultTargetPlatformOverride = TargetPlatform.fuchsia;
