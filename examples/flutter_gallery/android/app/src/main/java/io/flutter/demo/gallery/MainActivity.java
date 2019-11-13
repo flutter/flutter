efdf5c76f8107079bdaa414c3d5cd9e1e7abf7b4
Matt Carroll <matthew-carroll@users.noreply.github.com>
2019-11-12 21:42:42 -0800

    PR Updates.

--- examples/flutter_gallery/android/app/src/main/java/io/flutter/demo/gallery/MainActivity.java
+++ examples/flutter_gallery/android/app/src/main/java/io/flutter/demo/gallery/MainActivity.java
@@ -6,6 +6,7 @@ package io.flutter.demo.gallery;
 
 import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
 
 public class MainActivity extends FlutterActivity {
     private FlutterGalleryInstrumentation instrumentation;
