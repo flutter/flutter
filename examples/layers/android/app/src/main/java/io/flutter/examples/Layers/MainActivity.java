f7b9ff9e20635e5d3f4e7c37a8a2d87c9ac56f05
Sahand Akbarzadeh <sahandevs@gmail.com>
2019-11-15 09:44:13 +0330

    Revert "Revert: Migrate examples to the new embedding (#43689)"
    This reverts commit 55cd3875265966ddd7161d7521d58d95d29ed2bb.

--- examples/layers/android/app/src/main/java/io/flutter/examples/Layers/MainActivity.java
+++ examples/layers/android/app/src/main/java/io/flutter/examples/Layers/MainActivity.java
@@ -4,15 +4,14 @@
 
 package io.flutter.examples.Layers;
 
-import android.os.Bundle;
-import io.flutter.app.FlutterActivity;
-import io.flutter.plugins.GeneratedPluginRegistrant;
+import androidx.annotation.NonNull;
+import dev.flutter.plugins.GeneratedPluginRegistrant;
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
 
 public class MainActivity extends FlutterActivity {
   @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    GeneratedPluginRegistrant.registerWith(this);
+  public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+    GeneratedPluginRegistrant.registerWith(flutterEngine);
   }
 }
-
