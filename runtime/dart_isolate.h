096ba6892a435a9149b0406592eab7ba866bceb8
Alexander Aprelev <aam@google.com>
2019-11-15 09:22:26 -0800

    Reland children isolates sharing isolate group change. (#13758)
    * Revert "Revert "Provide dart vm initalize isolate callback so that children isolates belong to parent's isolate group. (#9888)" (#12327)"
    
    * Ensure that when isolate shuts down it calls isolate_data, rather than isolage_group_data callback.

--- runtime/dart_isolate.h
+++ runtime/dart_isolate.h
@@ -401,6 +401,13 @@ class DartIsolate : public UIDartState {
   ///
   fml::RefPtr<fml::TaskRunner> GetMessageHandlingTaskRunner() const;
 
+  // Root isolate of the VM application
+  bool IsRootIsolate() const { return is_root_isolate_; }
+  // Isolate that owns IsolateGroup it lives in.
+  // When --no-enable-isolate-groups dart vm flag is set,
+  // all child isolates will have their own IsolateGroups.
+  bool IsGroupRootIsolate() const { return is_group_root_isolate_; }
+
  private:
   using ChildIsolatePreparer = std::function<bool(DartIsolate*)>;
 
@@ -425,6 +432,8 @@ class DartIsolate : public UIDartState {
   fml::RefPtr<fml::TaskRunner> message_handling_task_runner_;
   const fml::closure isolate_create_callback_;
   const fml::closure isolate_shutdown_callback_;
+  const bool is_root_isolate_;
+  const bool is_group_root_isolate_;
 
   DartIsolate(const Settings& settings,
               fml::RefPtr<const DartSnapshot> isolate_snapshot,
@@ -437,18 +446,17 @@ class DartIsolate : public UIDartState {
               std::string advisory_script_entrypoint,
               ChildIsolatePreparer child_isolate_preparer,
               fml::closure isolate_create_callback,
-              fml::closure isolate_shutdown_callback);
+              fml::closure isolate_shutdown_callback,
+              bool is_root_isolate,
+              bool is_group_root_isolate);
+  FML_WARN_UNUSED_RESULT bool Initialize(Dart_Isolate isolate);
 
-  FML_WARN_UNUSED_RESULT bool Initialize(Dart_Isolate isolate,
-                                         bool is_root_isolate);
-
-  void SetMessageHandlingTaskRunner(fml::RefPtr<fml::TaskRunner> runner,
-                                    bool is_root_isolate);
+  void SetMessageHandlingTaskRunner(fml::RefPtr<fml::TaskRunner> runner);
 
   bool LoadKernel(std::shared_ptr<const fml::Mapping> mapping, bool last_piece);
 
   FML_WARN_UNUSED_RESULT
-  bool LoadLibraries(bool is_root_isolate);
+  bool LoadLibraries();
 
   bool UpdateThreadPoolNames() const;
 
@@ -466,6 +474,10 @@ class DartIsolate : public UIDartState {
       Dart_IsolateFlags* flags,
       std::shared_ptr<DartIsolate>* embedder_isolate,
       char** error);
+
+  // |Dart_IsolateInitializeCallback|
+  static bool DartIsolateInitializeCallback(void** child_callback_data,
+                                            char** error);
 
   static Dart_Isolate DartCreateAndStartServiceIsolate(
       const char* package_root,
@@ -485,8 +497,17 @@ class DartIsolate : public UIDartState {
       bool is_root_isolate,
       char** error);
 
+  static bool InitializeIsolate(std::shared_ptr<DartIsolate> embedder_isolate,
+                                Dart_Isolate isolate,
+                                char** error);
+
   // |Dart_IsolateShutdownCallback|
   static void DartIsolateShutdownCallback(
+      std::shared_ptr<DartIsolate>* isolate_group_data,
+      std::shared_ptr<DartIsolate>* isolate_data);
+
+  // |Dart_IsolateCleanupCallback|
+  static void DartIsolateCleanupCallback(
       std::shared_ptr<DartIsolate>* isolate_group_data,
       std::shared_ptr<DartIsolate>* isolate_data);
 
