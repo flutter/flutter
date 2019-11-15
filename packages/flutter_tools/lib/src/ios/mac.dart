6bfd8e6beb8b2272b4da46e118c06723fe05c0dc
Sahand Akbarzadeh <sahandevs@gmail.com>
2019-11-15 09:44:13 +0330

    Revert "Use PRODUCT_BUNDLE_IDENTIFIER from buildSettings to find correct bundle id on iOS when using flavors (#42964)"
    This reverts commit b19e77db9cdf8387ab411dbe7aed8d1a7a38c9d1.

--- packages/flutter_tools/lib/src/ios/mac.dart
+++ packages/flutter_tools/lib/src/ios/mac.dart
@@ -562,16 +562,7 @@ Future<XcodeBuildResult> buildXcodeProject({
     } else {
       printError('Build succeeded but the expected app at $expectedOutputDirectory not found');
     }
-    return XcodeBuildResult(
-        success: true,
-        output: outputDir,
-        xcodeBuildExecution: XcodeBuildExecution(
-          buildCommands: buildCommands,
-          appDirectory: app.project.hostAppRoot.path,
-          buildForPhysicalDevice: buildForDevice,
-          buildSettings: buildSettings,
-      ),
-    );
+    return XcodeBuildResult(success: true, output: outputDir);
   }
 }
 
