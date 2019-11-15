36dd8e0f3d487ec6ca76e079f4576f50316f54e3
Sahand Akbarzadeh <sahandevs@gmail.com>
2019-11-15 09:44:13 +0330

    Revert "Make module_test_ios pass (#44147)"
    This reverts commit 654b71a9813c2dd0e24a79a39883eed6fc5ae6d4.

--- dev/integration_tests/ios_host_app_swift/Host/AppDelegate.swift
+++ dev/integration_tests/ios_host_app_swift/Host/AppDelegate.swift
@@ -1,5 +1,9 @@
 import UIKit
 
+// Prove plugins can be module-imported from the host app.
+import device_info
+import google_maps_flutter
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
   func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
