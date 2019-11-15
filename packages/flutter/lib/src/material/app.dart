c510f5cfd87259be5c09b9c22d40b5ef7f7cd222
Sahand Akbarzadeh <sahandevs@gmail.com>
2019-11-15 09:44:13 +0330

    Revert "Remove print and fix code formatting (#43843)"
    This reverts commit e706a99f4735d3dfd212acbada02d0595d3d4ff8.

--- packages/flutter/lib/src/material/app.dart
+++ packages/flutter/lib/src/material/app.dart
@@ -553,9 +553,8 @@ class _MaterialAppState extends State<MaterialApp> {
       key: GlobalObjectKey(this),
       navigatorKey: widget.navigatorKey,
       navigatorObservers: _navigatorObservers,
-      pageRouteBuilder: <T>(RouteSettings settings, WidgetBuilder builder) {
-        return MaterialPageRoute<T>(settings: settings, builder: builder);
-      },
+        pageRouteBuilder: <T>(RouteSettings settings, WidgetBuilder builder) =>
+            MaterialPageRoute<T>(settings: settings, builder: builder),
       home: widget.home,
       routes: widget.routes,
       initialRoute: widget.initialRoute,
