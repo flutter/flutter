54e86799234c9d4bef7fe279bc8058c58f458cdc
Jonah Williams <jonahwilliams@google.com>
2019-11-18 14:09:40 -0800

    Provide specific field to accept depfiles in target class (#44481)
    * add depfile slot to Target class
    
    * Update source.dart
    
    * Update source.dart

--- packages/flutter_tools/lib/src/build_system/targets/web.dart
+++ packages/flutter_tools/lib/src/build_system/targets/web.dart
@@ -113,12 +113,14 @@ class Dart2JSTarget extends Target {
     Source.artifact(Artifact.engineDartBinary),
     Source.pattern('{BUILD_DIR}/main.dart'),
     Source.pattern('{PROJECT_DIR}/.packages'),
-    Source.depfile('dart2js.d'),
   ];
 
   @override
-  List<Source> get outputs => const <Source>[
-    Source.depfile('dart2js.d'),
+  List<Source> get outputs => const <Source>[];
+
+  @override
+  List<String> get depfiles => const <String>[
+    'dart2js.d',
   ];
 
   @override
@@ -187,14 +189,17 @@ class WebReleaseBundle extends Target {
     Source.pattern('{BUILD_DIR}/main.dart.js'),
     Source.pattern('{PROJECT_DIR}/pubspec.yaml'),
     Source.pattern('{PROJECT_DIR}/web/index.html'),
-    Source.depfile('flutter_assets.d'),
   ];
 
   @override
   List<Source> get outputs => const <Source>[
     Source.pattern('{OUTPUT_DIR}/main.dart.js'),
     Source.pattern('{OUTPUT_DIR}/index.html'),
-    Source.depfile('flutter_assets.d'),
+  ];
+
+  @override
+  List<String> get depfiles => const <String>[
+    'dart2js.d',
   ];
 
   @override
