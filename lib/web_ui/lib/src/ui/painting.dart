f4fba66c2fad1c1d5705ea0734ee4250211f6757
Kate Lovett <katelovett@google.com>
2019-11-19 09:48:04 -0800

    Adding opacity -> alpha method to Color class (#13902)

--- lib/web_ui/lib/src/ui/painting.dart
+++ lib/web_ui/lib/src/ui/painting.dart
@@ -226,6 +226,14 @@ class Color {
     }
   }
 
+  /// Returns an alpha value representative of the provided [opacity] value.
+  ///
+  /// The [opacity] value may not be null.
+  static int getAlphaFromOpacity(double opacity) {
+    assert(opacity != null);
+    return (opacity.clamp(0.0, 1.0) * 255).round();
+  }
+
   @override
   bool operator ==(dynamic other) {
     if (identical(this, other)) {
