1596624fb8ee2938df094ab005f0b508e799c899
Amir Hardon <amirha@google.com>
2019-11-20 15:56:28 -0800

    add link to plugin pubspec doc

--- packages/flutter_tools/lib/src/plugins.dart
+++ packages/flutter_tools/lib/src/plugins.dart
@@ -163,7 +163,8 @@ class Plugin {
     if (usesOldPluginFormat && usesNewPluginFormat) {
       const String errorMessage =
           'The flutter.plugin.platforms key cannot be used in combination with the old'
-          'flutter.plugin.{androidPackage,iosPrefix,pluginClass} keys.';
+          'flutter.plugin.{androidPackage,iosPrefix,pluginClass} keys.'
+          'See: https://flutter.dev/docs/development/packages-and-plugins/developing-packages#plugin';
       return <String>[errorMessage];
     }
 
