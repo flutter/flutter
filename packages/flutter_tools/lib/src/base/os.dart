9bcf5c384cea4168945778398f5606373bae65d4
Jonah Williams <jonahwilliams@google.com>
2019-11-20 14:45:32 -0800

    Revert "Reduce some direct package:archive usage (#44608)" (#45291)
    This reverts commit 6b5634fed35cb0d818e99de9f8c66586ed1b27a7.

--- packages/flutter_tools/lib/src/base/os.dart
+++ packages/flutter_tools/lib/src/base/os.dart
@@ -249,7 +249,6 @@ class _WindowsUtils extends OperatingSystemUtils {
 
   @override
   void zip(Directory data, File zipFile) {
-    // TODO(jonahwilliams): investigate whether we can remove this dependency with https://github.com/flutter/flutter/issues/45278
     final Archive archive = Archive();
     for (FileSystemEntity entity in data.listSync(recursive: true)) {
       if (entity is! File) {
