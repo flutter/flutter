c11d3f15b7787089909c091ab5db4f3962b2d8ae
Kate Lovett <katelovett@google.com>
2019-11-20 12:49:34 -0800

    Added docs

--- packages/flutter/lib/src/rendering/proxy_sliver.dart
+++ packages/flutter/lib/src/rendering/proxy_sliver.dart
@@ -4,9 +4,24 @@
 
 import 'package:flutter/rendering.dart';
 
-/// Doc
+/// A base class for sliver render objects that resemble their children.
+///
+/// A proxy sliver has a single child and simply mimics all the properties of
+/// that child by calling through to the child for each function in the render
+/// sliver protocol. For example, a proxy sliver determines its geometry by
+/// asking its sliver child to layout with the same constraints and then
+/// matching the geometry.
+///
+/// A proxy sliver isn't useful on its own because you might as well just
+/// replace the proxy sliver with its child. However, RenderProxySliver is a
+/// useful base class for render objects that wish to mimic most, but not all,
+/// of the properties of their sliver child.
 abstract class RenderProxySliver extends RenderSliver with RenderObjectWithChildMixin<RenderSliver> {
-  /// Doc
+  /// Creates a proxy render sliver.
+  ///
+  /// Proxy render slivers aren't created directly because they simply proxy
+  /// the render sliver protocol to their sliver [child]. Instead, use one of
+  /// the subclasses.
   RenderProxySliver([RenderSliver child]) {
     this.child = child;
   }
