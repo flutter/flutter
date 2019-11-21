34f8a219bb0e44587b51abe7cf3044b53616b50d
Amir Hardon <amirh@users.noreply.github.com>
2019-11-20 17:00:46 -0800

    Allow unknown fields in pubspec plugin section (#45303)

--- packages/flutter_tools/test/general.shard/plugin_parsing_test.dart
+++ packages/flutter_tools/test/general.shard/plugin_parsing_test.dart
@@ -78,5 +78,52 @@ void main() {
       expect(webPlugin.fileName, 'web_plugin.dart');
       expect(windowsPlugin.pluginClass, 'WinSamplePlugin');
     });
+
+    test('Unknown fields are allowed (allows some future compatibility)', () {
+      const String pluginYamlRaw = 'implements: same_plugin\n' // this should be ignored by the tool
+          'platforms:\n'
+          ' android:\n'
+          '  package: com.flutter.dev\n'
+          '  pluginClass: ASamplePlugin\n'
+          '  anUnknownField: ASamplePlugin\n' // this should be ignored by the tool
+          ' ios:\n'
+          '  pluginClass: ISamplePlugin\n'
+          ' linux:\n'
+          '  pluginClass: LSamplePlugin\n'
+          ' macos:\n'
+          '  pluginClass: MSamplePlugin\n'
+          ' web:\n'
+          '  pluginClass: WebSamplePlugin\n'
+          '  fileName: web_plugin.dart\n'
+          ' windows:\n'
+          '  pluginClass: WinSamplePlugin\n';
+
+      final dynamic pluginYaml = loadYaml(pluginYamlRaw);
+      final Plugin plugin =
+      Plugin.fromYaml(_kTestPluginName, _kTestPluginPath, pluginYaml);
+
+      final AndroidPlugin androidPlugin =
+      plugin.platforms[AndroidPlugin.kConfigKey];
+      final IOSPlugin iosPlugin = plugin.platforms[IOSPlugin.kConfigKey];
+      final LinuxPlugin linuxPlugin =
+      plugin.platforms[LinuxPlugin.kConfigKey];
+      final MacOSPlugin macOSPlugin =
+      plugin.platforms[MacOSPlugin.kConfigKey];
+      final WebPlugin webPlugin = plugin.platforms[WebPlugin.kConfigKey];
+      final WindowsPlugin windowsPlugin =
+      plugin.platforms[WindowsPlugin.kConfigKey];
+      final String androidPluginClass = androidPlugin.pluginClass;
+      final String iosPluginClass = iosPlugin.pluginClass;
+
+      expect(iosPluginClass, 'ISamplePlugin');
+      expect(androidPluginClass, 'ASamplePlugin');
+      expect(iosPlugin.classPrefix, '');
+      expect(androidPlugin.package, 'com.flutter.dev');
+      expect(linuxPlugin.pluginClass, 'LSamplePlugin');
+      expect(macOSPlugin.pluginClass, 'MSamplePlugin');
+      expect(webPlugin.pluginClass, 'WebSamplePlugin');
+      expect(webPlugin.fileName, 'web_plugin.dart');
+      expect(windowsPlugin.pluginClass, 'WinSamplePlugin');
+    });
   });
 }
