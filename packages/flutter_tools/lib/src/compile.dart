d361b3e46912f2f6d9087c3a7256a464bb9b42dd
Jonah Williams <jonahwilliams@google.com>
2019-11-22 13:36:12 -0800

    Revert "Improve time to development by initializing frontend_server concurrently with platform build (#45236)" (#45422)

--- packages/flutter_tools/lib/src/compile.dart
+++ packages/flutter_tools/lib/src/compile.dart
@@ -468,6 +468,7 @@ abstract class ResidentCompiler {
     List<String> dartDefines,
   }) = DefaultResidentCompiler;
 
+
   /// If invoked for the first time, it compiles Dart script identified by
   /// [mainPath], [invalidatedFiles] list is ignored.
   /// On successive runs [invalidatedFiles] indicates which files need to be
