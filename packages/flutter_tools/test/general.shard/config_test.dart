f6c88e43f441ad7a2cda46e71671d1095190de2e
xster <xiao@xster.net>
2019-11-22 10:49:46 -0800

    Revert "catch parse error from corrupt config (#45319)"
    This reverts commit 6640c8b9b4381d032b25b6035b82b2a17c6f00e3.

--- packages/flutter_tools/test/general.shard/config_test.dart
+++ packages/flutter_tools/test/general.shard/config_test.dart
@@ -2,59 +2,54 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:file/memory.dart';
 import 'package:flutter_tools/src/base/config.dart';
 import 'package:flutter_tools/src/base/file_system.dart';
-import 'package:flutter_tools/src/base/logger.dart';
 
 import '../src/common.dart';
 
 void main() {
   Config config;
-  MemoryFileSystem memoryFileSystem;
+  Directory tempDir;
 
   setUp(() {
-    memoryFileSystem = MemoryFileSystem();
-    final File file = memoryFileSystem.file('example');
+    tempDir = fs.systemTempDirectory.createTempSync('flutter_config_test.');
+    final File file = fs.file(fs.path.join(tempDir.path, '.settings'));
     config = Config(file);
   });
-  test('Config get set value', () async {
-    expect(config.getValue('foo'), null);
-    config.setValue('foo', 'bar');
-    expect(config.getValue('foo'), 'bar');
-    expect(config.keys, contains('foo'));
+
+  tearDown(() {
+    tryToDelete(tempDir);
   });
 
-  test('Config get set bool value', () async {
-    expect(config.getValue('foo'), null);
-    config.setValue('foo', true);
-    expect(config.getValue('foo'), true);
-    expect(config.keys, contains('foo'));
-  });
+  group('config', () {
+    test('get set value', () async {
+      expect(config.getValue('foo'), null);
+      config.setValue('foo', 'bar');
+      expect(config.getValue('foo'), 'bar');
+      expect(config.keys, contains('foo'));
+    });
 
-  test('Config containsKey', () async {
-    expect(config.containsKey('foo'), false);
-    config.setValue('foo', 'bar');
-    expect(config.containsKey('foo'), true);
-  });
+    test('get set bool value', () async {
+      expect(config.getValue('foo'), null);
+      config.setValue('foo', true);
+      expect(config.getValue('foo'), true);
+      expect(config.keys, contains('foo'));
+    });
 
-  test('Config removeValue', () async {
-    expect(config.getValue('foo'), null);
-    config.setValue('foo', 'bar');
-    expect(config.getValue('foo'), 'bar');
-    expect(config.keys, contains('foo'));
-    config.removeValue('foo');
-    expect(config.getValue('foo'), null);
-    expect(config.keys, isNot(contains('foo')));
-  });
+    test('containsKey', () async {
+      expect(config.containsKey('foo'), false);
+      config.setValue('foo', 'bar');
+      expect(config.containsKey('foo'), true);
+    });
 
-  test('Config parse error', () {
-    final BufferLogger bufferLogger =BufferLogger();
-    final File file = memoryFileSystem.file('example')
-      ..writeAsStringSync('{"hello":"bar');
-    config = Config(file, bufferLogger);
-
-    expect(file.existsSync(), false);
-    expect(bufferLogger.errorText, contains('Failed to decode preferences'));
+    test('removeValue', () async {
+      expect(config.getValue('foo'), null);
+      config.setValue('foo', 'bar');
+      expect(config.getValue('foo'), 'bar');
+      expect(config.keys, contains('foo'));
+      config.removeValue('foo');
+      expect(config.getValue('foo'), null);
+      expect(config.keys, isNot(contains('foo')));
+    });
   });
 }
