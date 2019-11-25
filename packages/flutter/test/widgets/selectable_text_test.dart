60b47c4def851c21e3acce2511da309c170d7272
KagurazakaHanabi <i@yaerin.com>
2019-11-25 13:43:09 +0800

    add test

--- packages/flutter/test/widgets/selectable_text_test.dart
+++ packages/flutter/test/widgets/selectable_text_test.dart
@@ -177,10 +177,7 @@ void main() {
     debugResetSemanticsIdCounter();
   });
 
-  Widget selectableTextBuilder({
-    String text = '',
-    int maxLines = 1,
-  }) {
+  Widget selectableTextBuilder({String text = '', int maxLines = 1}) {
     return boilerplate(
       child: SelectableText(
         text,
@@ -192,22 +189,32 @@ void main() {
 
   testWidgets('has expected defaults', (WidgetTester tester) async {
     await tester.pumpWidget(
-      const MediaQuery(
-        data: MediaQueryData(devicePixelRatio: 1.0),
-        child: Directionality(
-          textDirection: TextDirection.ltr,
-          child: SelectableText('selectable text'),
-        ),
+      boilerplate(
+          child: const SelectableText('selectable text'),
       ),
     );
 
-    final SelectableText selectableText =
-    tester.firstWidget(find.byType(SelectableText));
+    final SelectableText selectableText = tester.firstWidget(find.byType(SelectableText));
     expect(selectableText.showCursor, false);
     expect(selectableText.autofocus, false);
     expect(selectableText.dragStartBehavior, DragStartBehavior.start);
     expect(selectableText.cursorWidth, 2.0);
     expect(selectableText.enableInteractiveSelection, true);
+
+    RenderBox renderBox = tester.renderObject(find.byType(SelectableText));
+    expect(renderBox.size.height, 14.0);
+
+    await tester.pumpWidget(
+      boilerplate(
+        child: const SelectableText(
+          'selectable text',
+          textScaleFactor: 1.9,
+        ),
+      ),
+    );
+
+    renderBox = tester.renderObject(find.byType(SelectableText));
+    expect(renderBox.size.height, 27.0);
   });
 
   testWidgets('Rich selectable text has expected defaults', (WidgetTester tester) async {
