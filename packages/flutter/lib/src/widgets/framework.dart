8995d711f30f5184fdc2f742d17242a12d3811b0
Chun-Heng Tai <chtai@google.com>
2019-11-27 14:59:54 -0800

    fix comment

--- packages/flutter/lib/src/widgets/framework.dart
+++ packages/flutter/lib/src/widgets/framework.dart
@@ -184,11 +184,11 @@ abstract class GlobalKey<T extends State<StatefulWidget>> extends Key {
           ),
         ]);
       }
-      // The parent might have multiple global key reservation. This can happens
+      // The parent might have multiple global key reservations. This can happen
       // when a dirty node under LayoutBuilder is rebuilt before the LayoutBuilder
-      // updates its widget subtree. At the end, the result of dirty node rebuild will
-      // be overwritten by the LayoutBuilder rebuild. It should be safe to remove
-      // all the other reservations of GlobalKeys.
+      // updates its widget subtree in rendering layer. At the end, the result
+      // of dirty node rebuild will be overwritten by the LayoutBuilder rebuild.
+      // It should be safe to remove all the other reservations of GlobalKeys.
       // See https://github.com/flutter/flutter/issues/43780
       _debugReservations.removeWhere((_, Element e) => e == parent);
       _debugReservations[this] = parent;
