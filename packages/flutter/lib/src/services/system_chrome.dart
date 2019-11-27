b52cc0819ca4071d66470d0d7098233b3fb38352
Alexandre Ardhuin <alexandre.ardhuin@gmail.com>
2019-11-27 21:37:46 +0100

    implicit-casts:false in flutter/lib/src/services

--- packages/flutter/lib/src/services/system_chrome.dart
+++ packages/flutter/lib/src/services/system_chrome.dart
@@ -196,13 +196,13 @@ class SystemUiOverlayStyle {
   bool operator ==(dynamic other) {
     if (other.runtimeType != runtimeType)
       return false;
-    final SystemUiOverlayStyle typedOther = other;
-    return typedOther.systemNavigationBarColor == systemNavigationBarColor
-      && typedOther.systemNavigationBarDividerColor == systemNavigationBarDividerColor
-      && typedOther.statusBarColor == statusBarColor
-      && typedOther.statusBarIconBrightness == statusBarIconBrightness
-      && typedOther.statusBarBrightness == statusBarBrightness
-      && typedOther.systemNavigationBarIconBrightness == systemNavigationBarIconBrightness;
+    return other is SystemUiOverlayStyle
+        && other.systemNavigationBarColor == systemNavigationBarColor
+        && other.systemNavigationBarDividerColor == systemNavigationBarDividerColor
+        && other.statusBarColor == statusBarColor
+        && other.statusBarIconBrightness == statusBarIconBrightness
+        && other.statusBarBrightness == statusBarBrightness
+        && other.systemNavigationBarIconBrightness == systemNavigationBarIconBrightness;
   }
 }
 
