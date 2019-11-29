552b06f99a5c5c59071038cf51a0841ac15915a8
Alexandre Ardhuin <alexandre.ardhuin@gmail.com>
2019-11-29 10:01:27 +0100

    implicit-casts:false in examples

--- examples/flutter_gallery/lib/gallery/demos.dart
+++ examples/flutter_gallery/lib/gallery/demos.dart
@@ -22,8 +22,8 @@ class GalleryDemoCategory {
       return true;
     if (runtimeType != other.runtimeType)
       return false;
-    final GalleryDemoCategory typedOther = other;
-    return typedOther.name == name && typedOther.icon == icon;
+    return other is GalleryDemoCategory
+        && other.name == name && other.icon == icon;
   }
 
   @override
@@ -582,6 +582,6 @@ final Map<GalleryDemoCategory, List<GalleryDemo>> kGalleryCategoryToDemos =
 final Map<String, String> kDemoDocumentationUrl =
     Map<String, String>.fromIterable(
       kAllGalleryDemos.where((GalleryDemo demo) => demo.documentationUrl != null),
-      key: (dynamic demo) => demo.routeName,
-      value: (dynamic demo) => demo.documentationUrl,
+      key: (dynamic demo) => (demo as GalleryDemo).routeName,
+      value: (dynamic demo) => (demo as GalleryDemo).documentationUrl,
     );
