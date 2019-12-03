a76c5c1c83366b6fb87d8b850f85ac224eff3ccb
Sahand Akbarzadeh <sahandevs@gmail.com>
2019-12-03 18:13:01 +0000

    Add clip behaviour to Container (#44971)

--- packages/flutter/test/painting/box_decoration_test.dart
+++ packages/flutter/test/painting/box_decoration_test.dart
@@ -68,4 +68,18 @@ void main() {
       paints..rect(rect: Offset.zero & size),
     );
   });
+
+  test('BoxDecoration.getClipPath', () {
+    const double radius = 10;
+    final BoxDecoration decoration = BoxDecoration(
+      borderRadius: BorderRadius.circular(radius),
+    );
+    const Rect rect = Rect.fromLTWH(0.0, 0.0, 100.0, 20.0);
+    final Path clipPath = decoration.getClipPath(rect, TextDirection.ltr);
+    final Matcher isLookLikeExpectedPath = isPathThat(
+      includes: const <Offset>[ Offset(30.0, 10.0), Offset(50.0, 10.0), ],
+      excludes: const <Offset>[ Offset(1.0, 1.0), Offset(99.0, 19.0), ],
+    );
+    expect(clipPath, isLookLikeExpectedPath);
+  });
 }
