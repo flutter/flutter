77417c5777100d0f031eff54fa0c9657390ffe3c
Yuqian Li <liyuqian@google.com>
2019-12-03 15:23:18 -0800

    Memory test on scrolling large images quickly
    For https://github.com/flutter/flutter/issues/19558
    
    This test reveals the issue that Flutter currently retains as much as
    200MB memory after scrolling. (200MB can further grow if we scroll more
    times). There should be plenty of time for the GC to clean such memory.

---
+++ dev/benchmarks/macrobenchmarks/lib/src/large_images.dart
@@ -0,0 +1,25 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+
+class LargeImagesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final ImageCache imageCache = PaintingBinding.instance.imageCache;
+    imageCache.maximumSize = 30;
+    imageCache.maximumSizeBytes = 50 << 20;
+    return GridView.builder(
+      itemCount: 1000,
+      gridDelegate:
+      const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+      itemBuilder: (BuildContext context, int index) {
+        return Image.network(
+            'http://via.placeholder.com/'
+                '${500 + index}x1000',
+            key: ValueKey<int>(index));
+      },
+    ).build(context);
+  }
+}
---
+++ dev/devicelab/bin/tasks/fast_scroll_large_images__memory.dart
@@ -0,0 +1,47 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/// Measure application memory usage after pausing and resuming the app
+/// with the Android back button.
+
+import 'dart:async';
+
+import 'package:flutter_devicelab/framework/adb.dart';
+import 'package:flutter_devicelab/framework/framework.dart';
+import 'package:flutter_devicelab/framework/utils.dart';
+import 'package:flutter_devicelab/tasks/perf_tests.dart';
+
+const String kPackageName = 'com.example.macrobenchmarks';
+const String kActivityName = 'com.example.macrobenchmarks.MainActivity';
+
+class FastScrollLargeImagesMemoryTest extends MemoryTest {
+  FastScrollLargeImagesMemoryTest()
+      : super(
+          '${flutterDirectory.path}/dev/benchmarks/macrobenchmarks',
+          'test_memory/large_images.dart', kPackageName,
+        );
+
+  @override
+  AndroidDevice get device => super.device;
+
+  @override
+  int get iterationCount => 5;
+
+  /// Perform a series of back button suspend and resume cycles.
+  @override
+  Future<void> useMemory() async {
+    await launchApp();
+    await recordStart();
+    for (int iteration = 0; iteration < 4; iteration += 1) {
+      await device.shellExec('input', <String>['swipe', '0 1000 0 0 100']);
+      await Future<void>.delayed(const Duration(milliseconds: 2000));
+    }
+    await recordEnd();
+  }
+}
+
+Future<void> main() async {
+  deviceOperatingSystem = DeviceOperatingSystem.android;
+  await task(FastScrollLargeImagesMemoryTest().run);
+}
