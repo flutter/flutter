a77a9a6c5aa3a23cc02b487c1348bd662ba0c101
Aaron Clarke <aaclarke@google.com>
2019-12-04 14:22:56 -0800

    Added source annoations

--- packages/flutter/lib/src/painting/image_cache.dart
+++ packages/flutter/lib/src/painting/image_cache.dart
@@ -99,13 +99,17 @@ class ImageCache {
   /// behavior, which is to increase the [maximumByteSize] to accomodate the
   /// large image.
   ///
+  /// {@tool sample}
+  ///
   /// Here is an example implementation that increases the cache size in
   /// response to a large image:
+  /// ```dart
   /// static void handler(ImageCache imageCache, int imageSize) {
   ///   final int newSize = imageSize + 1000;
   ///   imageCache.maximumSizeBytes = newSize;
   ///   Analytics.log("Increase image cache size", newSize);
   /// }
+  /// ```
   set largeImageHandler(LargeImageHandler handler) {
     _largeImageHandler = handler;
   }
