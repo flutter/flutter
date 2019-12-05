9f8e44722a88f4574869ae71e548f1fd30cb1687
gaaclarke <30870216+gaaclarke@users.noreply.github.com>
2019-12-04 16:17:43 -0800

    Started specifying the OS version for running the tests. (#14094)

--- testing/scenario_app/run_ios_tests.sh
+++ testing/scenario_app/run_ios_tests.sh
@@ -21,7 +21,7 @@ pushd ios/Scenarios
 
 set -o pipefail && xcodebuild -sdk iphonesimulator \
   -scheme Scenarios \
-  -destination 'platform=iOS Simulator,name=iPhone SE' \
+  -destination 'platform=iOS Simulator,OS=12.2,name=iPhone SE' \
   test \
   FLUTTER_ENGINE=$FLUTTER_ENGINE | $PRETTY
 
