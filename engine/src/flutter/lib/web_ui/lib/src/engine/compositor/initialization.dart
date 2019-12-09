9d5a3842a38432a8f81429b45f4ec53745af6dc4
Harry Terkelsen <hterkelsen@users.noreply.github.com>
2019-12-09 14:28:13 -0800

    Use the versioned canvaskit from unpkg. (flutter/engine#14264)
    The one from particles.skia.org is not intended for production use.

--- engine/src/flutter/lib/web_ui/lib/src/engine/compositor/initialization.dart
+++ engine/src/flutter/lib/web_ui/lib/src/engine/compositor/initialization.dart
@@ -9,7 +9,7 @@ const bool experimentalUseSkia =
     bool.fromEnvironment('FLUTTER_WEB_USE_SKIA', defaultValue: false);
 
 /// The URL to use when downloading the CanvasKit script and associated wasm.
-const String canvasKitBaseUrl = 'https://particles.skia.org/static/';
+const String canvasKitBaseUrl = 'https://unpkg.com/canvaskit-wasm@0.10.0/bin/';
 
 /// Initialize the Skia backend.
 ///
