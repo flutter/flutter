c60b22a6bf3c0f1ff229d4a7e0291f07f9dfa76d
My MacPro <gasperg@umich.edu>
2019-12-09 17:40:53 -0500

    Added initial changes

--- examples/hello_world/lib/main.dart
+++ examples/hello_world/lib/main.dart
@@ -1,7 +1,61 @@
-// Copyright 2015 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 
-import 'package:flutter/widgets.dart';
+void main() {
+  runApp(MyApp());
+}
 
-void main() => runApp(const Center(child: Text('Hello, world!', textDirection: TextDirection.ltr)));
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final appName = 'Custom Themes';
+
+    return MaterialApp(
+      title: appName,
+      theme: ThemeData.dark().copyWith(
+        // Define the default brightness and colors.
+        brightness: Brightness.light,
+        accentColor: Colors.green[600],
+        primarySwatch: Colors.purple,
+        // fontFamily: 'Nunito',
+      ),
+      home: MyHomePage(
+        title: appName,
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final String title;
+
+  MyHomePage({Key key, @required this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Center(
+        child: Container(
+          color: Theme.of(context).accentColor,
+          child: Text(
+            'Text with a background color',
+            style: Theme.of(context).textTheme.title,
+          ),
+        ),
+      ),
+      floatingActionButton: Theme(
+        data: Theme.of(context).copyWith(
+          colorScheme:
+              Theme.of(context).colorScheme.copyWith(secondary: Colors.yellow),
+        ),
+        child: FloatingActionButton(
+          onPressed: null,
+          child: Icon(Icons.add),
+        ),
+      ),
+    );
+  }
+}
