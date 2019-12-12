0ed7061cd7c3ad734c81a859c177337d0a71d6fe
Sean Freiburg <seanfreiburg@gmail.com>
2019-12-12 10:28:01 -0800

    Add buildNumber param to "flutter build aar" command (#46291)

--- packages/flutter_tools/test/general.shard/android/gradle_test.dart
+++ packages/flutter_tools/test/general.shard/android/gradle_test.dart
@@ -936,6 +936,7 @@ plugin2=${plugin2.path}
           '-Pflutter-root=$flutterRoot',
           '-Poutput-dir=${buildDirectory.path}',
           '-Pis-plugin=true',
+          '-PbuildNumber=1.0',
           '-Ptarget-platform=android-arm,android-arm64,android-x64',
           'assembleAarRelease',
         ],
@@ -950,6 +951,7 @@ plugin2=${plugin2.path}
           '-Pflutter-root=$flutterRoot',
           '-Poutput-dir=${buildDirectory.path}',
           '-Pis-plugin=true',
+          '-PbuildNumber=1.0',
           '-Ptarget-platform=android-arm,android-arm64,android-x64',
           'assembleAarRelease',
         ],
@@ -1548,6 +1550,7 @@ plugin2=${plugin2.path}
         project: FlutterProject.current(),
         outputDirectory: fs.directory('build/'),
         target: '',
+        buildNumber: '1.0',
       );
 
       expect(
@@ -1710,6 +1713,7 @@ plugin2=${plugin2.path}
         project: FlutterProject.current(),
         outputDirectory: fs.directory('build/'),
         target: '',
+        buildNumber: '2.0',
       );
 
       final List<String> actualGradlewCall = verify(
@@ -1724,6 +1728,7 @@ plugin2=${plugin2.path}
       expect(actualGradlewCall, contains('-Plocal-engine-out=out/android_arm'));
       expect(actualGradlewCall, contains('-Plocal-engine-repo=/.tmp_rand0/flutter_tool_local_engine_repo.rand0'));
       expect(actualGradlewCall, contains('-Plocal-engine-build-mode=release'));
+      expect(actualGradlewCall, contains('-PbuildNumber=2.0'));
 
     }, overrides: <Type, Generator>{
       AndroidSdk: () => mockAndroidSdk,
@@ -1742,6 +1747,7 @@ plugin2=${plugin2.path}
         buildModes: const <String>{'release', 'debug', 'profile'},
         androidPackage: 'com.mycompany',
         repoDirectory: fs.directory('build/'),
+        buildNumber: '2.2',
       );
 
       expect(
@@ -1764,9 +1770,9 @@ plugin2=${plugin2.path}
           '  3. Make the host app depend on the Flutter module:\n'
           '\n'
           '    dependencies {\n'
-          '      releaseImplementation \'com.mycompany:flutter_release:1.0\n'
-          '      debugImplementation \'com.mycompany:flutter_debug:1.0\n'
-          '      profileImplementation \'com.mycompany:flutter_profile:1.0\n'
+          '      releaseImplementation \'com.mycompany:flutter_release:2.2\n'
+          '      debugImplementation \'com.mycompany:flutter_debug:2.2\n'
+          '      profileImplementation \'com.mycompany:flutter_profile:2.2\n'
           '    }\n'
           '\n'
           '\n'
@@ -1872,6 +1878,7 @@ plugin2=${plugin2.path}
         buildModes: const <String>{'profile'},
         androidPackage: 'com.mycompany',
         repoDirectory: fs.directory('build/'),
+        buildNumber: '1.0',
       );
 
       expect(
