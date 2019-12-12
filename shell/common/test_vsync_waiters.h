ffbe2a40e72239b8518c2de8fee41367bb150319
Kaushik Iska <iska.kaushik@gmail.com>
2019-12-12 12:27:56 -0800

    [testing] Move test vsync waiters to their own TUs (#14456)
    This move makes it easier to add more vsync waiters.

---
+++ shell/common/test_vsync_waiters.h
@@ -0,0 +1,44 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#define FML_USED_ON_EMBEDDER
+
+#ifndef FLUTTER_SHELL_COMMON_TEST_VSYNC_WAITERS_H_
+#define FLUTTER_SHELL_COMMON_TEST_VSYNC_WAITERS_H_
+
+#include "flutter/shell/common/shell.h"
+
+namespace flutter {
+namespace testing {
+
+class ShellTestVsyncClock {
+ public:
+  /// Simulate that a vsync signal is triggered.
+  void SimulateVSync();
+
+  /// A future that will return the index the next vsync signal.
+  std::future<int> NextVSync();
+
+ private:
+  std::mutex mutex_;
+  std::vector<std::promise<int>> vsync_promised_;
+  size_t vsync_issued_ = 0;
+};
+
+class ShellTestVsyncWaiter : public VsyncWaiter {
+ public:
+  ShellTestVsyncWaiter(TaskRunners task_runners, ShellTestVsyncClock& clock)
+      : VsyncWaiter(std::move(task_runners)), clock_(clock) {}
+
+ protected:
+  void AwaitVSync() override;
+
+ private:
+  ShellTestVsyncClock& clock_;
+};
+
+}  // namespace testing
+}  // namespace flutter
+
+#endif  // FLUTTER_SHELL_COMMON_TEST_VSYNC_WAITERS_H_
