332e77ca147a5909daf7fdc76f4a5fc43e03cad9
Jonah Williams <jonahwilliams@google.com>
2019-12-16 14:19:19 -0800

    Update common_test.dart

--- packages/flutter_tools/test/general.shard/base/common_test.dart
+++ packages/flutter_tools/test/general.shard/base/common_test.dart
@@ -3,7 +3,6 @@
 // found in the LICENSE file.
 
 import 'package:flutter_tools/src/base/common.dart';
-import 'package:flutter_tools/src/base/file_system.dart';
 import 'package:flutter_tools/src/base/context.dart';
 
 import '../../src/common.dart';
