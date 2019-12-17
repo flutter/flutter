a15a81be218f8b2fa2f63d7cd0f8cb0b3a6c2e08
Emmanuel Garcia <egarciad@google.com>
2019-12-17 14:10:36 -0800

    Fix androidSdk NPE (#47187)

--- packages/flutter_tools/test/src/android_common.dart
+++ packages/flutter_tools/test/src/android_common.dart
@@ -5,6 +5,7 @@
 import 'package:meta/meta.dart';
 
 import 'package:flutter_tools/src/android/android_builder.dart';
+import 'package:flutter_tools/src/base/file_system.dart';
 import 'package:flutter_tools/src/build_info.dart';
 import 'package:flutter_tools/src/project.dart';
 
@@ -32,4 +33,19 @@ class FakeAndroidBuilder implements AndroidBuilder {
     @required AndroidBuildInfo androidBuildInfo,
     @required String target,
   }) async {}
+}
+
+/// Creates a [FlutterProject] in a directory named [flutter_project]
+/// within [directoryOverride].
+class FakeFlutterProjectFactory extends FlutterProjectFactory {
+  FakeFlutterProjectFactory(this.directoryOverride) :
+    assert(directoryOverride != null);
+
+  final Directory directoryOverride;
+
+  @override
+  FlutterProject fromDirectory(Directory _) {
+    projects.clear();
+    return super.fromDirectory(directoryOverride.childDirectory('flutter_project'));
+  }
 }
