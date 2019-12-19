f1eaa10e8c097085ca0e741f73ca23149249c9ab
LongCatIsLooong <31859944+LongCatIsLooong@users.noreply.github.com>
2019-12-19 10:17:05 -0800

    Revert "Revert "Fix sliver geometry assert (#47027)" (#47390)"
    This reverts commit f32dee6e30e325174a6e2faf8c2c613607da97ec.

--- packages/flutter/lib/src/rendering/sliver.dart
+++ packages/flutter/lib/src/rendering/sliver.dart
@@ -1211,17 +1211,17 @@ abstract class RenderSliver extends RenderObject {
       }
     ));
     assert(() {
-      if (geometry.paintExtent > constraints.remainingPaintExtent) {
+      if (geometry.paintOrigin + geometry.paintExtent > constraints.remainingPaintExtent) {
         throw FlutterError.fromParts(<DiagnosticsNode>[
           ErrorSummary('SliverGeometry has a paintOffset that exceeds the remainingPaintExtent from the constraints.'),
           describeForError('The render object whose geometry violates the constraints is the following'),
           ..._debugCompareFloats(
             'remainingPaintExtent', constraints.remainingPaintExtent,
-            'paintExtent', geometry.paintExtent,
+            'paintOrigin + paintExtent', geometry.paintOrigin + geometry.paintExtent,
           ),
           ErrorDescription(
-            'The paintExtent must cause the child sliver to paint within the viewport, and so '
-            'cannot exceed the remainingPaintExtent.',
+            'The paintOrigin and paintExtent must cause the child sliver to paint '
+            'within the viewport, and so cannot exceed the remainingPaintExtent.',
           ),
         ]);
       }
