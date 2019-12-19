b44f4ae7679134493acbe39c40d3770305378157
George Wright <gw280@google.com>
2019-12-19 17:25:06 -0500

    Fix message_loop_fuchsia and thus enable fml_tests and flow_tests for Fuchsia (flutter/engine#14583)

--- engine/src/flutter/fml/platform/fuchsia/message_loop_fuchsia.cc
+++ engine/src/flutter/fml/platform/fuchsia/message_loop_fuchsia.cc
@@ -30,9 +30,9 @@ void MessageLoopFuchsia::WakeUp(fml::TimePoint time_point) {
     due_time = zx::nsec((time_point - now).ToNanoseconds());
   }
 
-  FML_DCHECK(async::PostDelayedTask(
-                 loop_.dispatcher(), [this]() { RunExpiredTasksNow(); },
-                 due_time) == ZX_OK);
+  auto status = async::PostDelayedTask(
+      loop_.dispatcher(), [this]() { RunExpiredTasksNow(); }, due_time);
+  FML_DCHECK(status == ZX_OK);
 }
 
 }  // namespace fml
