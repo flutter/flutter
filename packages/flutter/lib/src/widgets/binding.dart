eb968dd116f3211bd97b9e172b4afcb0858ef6c5
Alexandre Ardhuin <alexandre.ardhuin@gmail.com>
2019-12-19 11:23:01 +0100

    Reland implicit-casts: false (#47431)

--- packages/flutter/lib/src/widgets/binding.dart
+++ packages/flutter/lib/src/widgets/binding.dart
@@ -332,7 +332,7 @@ mixin WidgetsBinding on BindingBase, ServicesBinding, SchedulerBinding, GestureB
       registerServiceExtension(
         name: 'fastReassemble',
         callback: (Map<String, Object> params) async {
-          final String className = params['class'];
+          final String className = params['class'] as String;
           void markElementsDirty(Element element) {
             if (element == null) {
               return;
