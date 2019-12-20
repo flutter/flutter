dcb72bb4ab1669d607844229178b525e8f82b7ae
Tianguang Zhang <penn.zht@gmail.com>
2019-12-20 12:56:54 +0100

    Add effectiveConstraints to theme_data.dart.

--- packages/flutter/lib/src/material/theme_data.dart
+++ packages/flutter/lib/src/material/theme_data.dart
@@ -1815,6 +1815,16 @@ class VisualDensity extends Diagnosticable {
     );
   }
 
+  /// Calculate effective constraints for a widget with constraints [constraints].
+  ///
+  /// Used in the widgets [RawMaterialButton] and [IconButton].
+  BoxConstraints effectiveConstraints(BoxConstraints constraints){
+    return constraints.copyWith(
+      minWidth: (constraints.minWidth + baseSizeAdjustment.dx).clamp(0.0, double.infinity).toDouble(),
+      minHeight: (constraints.minHeight + baseSizeAdjustment.dy).clamp(0.0, double.infinity).toDouble(),
+    );
+  }
+
   @override
   bool operator ==(dynamic other) {
     if (other.runtimeType != runtimeType) {
