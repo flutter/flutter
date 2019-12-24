9be381a891b6d578c1903834f8b62fa158f0a6d6
liyuqian <liyuqian@google.com>
2019-12-23 17:22:01 -0800

    Run and collect benchmarks (flutter/engine#14556)
    This will start to collect engine benchmarks in the flutter-cirrus
    Datastore for all post-submit commits.
    
    We're using this to test how https://github.com/liyuqian/metrics_center
    works. Once it's stablized, we should move metrics_center into
    flutter/packages, and migrate data to flutter-infra Datastore.
    
    Related issue: https://github.com/flutter/flutter/issues/37434

---
+++ engine/src/flutter/testing/benchmark/bin/parse_and_send.dart
@@ -0,0 +1,54 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:git/git.dart';
+import 'package:metrics_center/flutter.dart';
+import 'package:metrics_center/google_benchmark.dart';
+
+Future<String> _getGitRevision() async {
+  final GitDir gitDir = await GitDir.fromExisting('../../');
+  // Somehow gitDir.currentBranch() doesn't work in Cirrus with "fatal: 'HEAD' -
+  // not a valid ref". Therefore, we use "git log" to get the revision manually.
+  final ProcessResult logResult =
+      await gitDir.runCommand(<String>['log', '--pretty=format:%H', '-n', '1']);
+  if (logResult.exitCode != 0) {
+    throw 'Unexpected exit code ${logResult.exitCode}';
+  }
+  return logResult.stdout.toString();
+}
+
+Future<List<FlutterEngineMetricPoint>> _parse(String jsonFileName) async {
+  final String gitRevision = await _getGitRevision();
+  final List<MetricPoint> rawPoints =
+      await GoogleBenchmarkParser.parse(jsonFileName);
+  final List<FlutterEngineMetricPoint> points = <FlutterEngineMetricPoint>[];
+  for (MetricPoint rawPoint in rawPoints) {
+    points.add(FlutterEngineMetricPoint(
+      rawPoint.tags[kNameKey],
+      rawPoint.value,
+      gitRevision,
+      moreTags: rawPoint.tags,
+    ));
+  }
+  return points;
+}
+
+Future<void> main(List<String> args) async {
+  if (args.length != 1) {
+    throw 'Must have one argument: <benchmark_json_file>';
+  }
+  final List<FlutterEngineMetricPoint> points = await _parse(args[0]);
+  // The data will be sent to the Datastore of the GCP project specified through
+  // environment variable BENCHMARK_GCP_CREDENTIALS. The engine Cirrus job has
+  // currently configured the GCP project to flutter-cirrus for test. We'll
+  // eventually migrate to flutter-infra project once the test is done.
+  final FlutterDestination destination =
+      await FlutterDestination.makeFromCredentialsJson(
+    jsonDecode(Platform.environment['BENCHMARK_GCP_CREDENTIALS']),
+  );
+  await destination.update(points);
+}
---
+++ engine/src/flutter/testing/benchmark/test/parse_and_send_test.dart
@@ -0,0 +1,24 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'dart:io';
+
+import 'package:test/test.dart';
+
+void main() {
+  // In order to run this test, one should download a service account
+  // credentials json from a test GCP project, and put that json as
+  // `secret/test_gcp_credentials.json`. There's a `flutter-test` project for
+  // Flutter team members.
+  test('parse_and_send with example json does not crash.', () async {
+    final String testCred =
+        File('secret/test_gcp_credentials.json').readAsStringSync();
+    Process.runSync('dart', <String>[
+      'bin/parse_and_send.dart',
+      'example/txt_benchmarks.json',
+    ], environment: <String, String>{
+      'BENCHMARK_GCP_CREDENTIALS': testCred,
+    });
+  });
+}
