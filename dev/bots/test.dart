41e498d55b691ae41e75358aa88d2a255e24fe4d
Dan Field <dnfield@google.com>
2019-12-29 14:46:39 -0800

    randomize tool tests

--- dev/bots/test.dart
+++ dev/bots/test.dart
@@ -604,6 +604,7 @@ Future<void> _pubRunTest(String workingDirectory, {
     args.add('--no-color');
   if (testPath != null)
     args.add(testPath);
+  args.add('--test-randomize-ordering-seed=random');
   final Map<String, String> pubEnvironment = <String, String>{
     'FLUTTER_ROOT': flutterRoot,
   };
