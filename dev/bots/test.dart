a95dd95e6662d90b47d0aae83f62ad2363bbbbec
Dan Field <dnfield@google.com>
2019-12-30 19:40:57 -0800

    Fix analyze_once tests

--- dev/bots/test.dart
+++ dev/bots/test.dart
@@ -604,7 +604,6 @@ Future<void> _pubRunTest(String workingDirectory, {
     args.add('--no-color');
   if (testPath != null)
     args.add(testPath);
-  args.add('--test-randomize-ordering-seed=random');
   final Map<String, String> pubEnvironment = <String, String>{
     'FLUTTER_ROOT': flutterRoot,
   };
