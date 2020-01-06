c2fbd99a8d9f096b9e5c2d48c7e5c7044e605c63
Sam Rawlins <srawlins@google.com>
2020-01-05 20:51:09 -0800

    ignore

--- packages/flutter/test/foundation/bit_field_test.dart
+++ packages/flutter/test/foundation/bit_field_test.dart
@@ -7,7 +7,8 @@
 import 'package:flutter/foundation.dart';
 import '../flutter_test_alternative.dart';
 
-enum _TestEnum { c, d, e, }
+// ignore: unused_field
+enum _TestEnum { a, b, c, d, e, f, g, h, }
 
 void main() {
   test('BitField control test', () {
