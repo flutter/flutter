541b5477381d81fa703907b2916968eefe692ce8
Kate Lovett <katelovett@google.com>
2020-01-07 09:25:35 -0800

    Analyzer fixes for sample code

--- packages/flutter/lib/src/widgets/transitions.dart
+++ packages/flutter/lib/src/widgets/transitions.dart
@@ -583,53 +583,53 @@ class FadeTransition extends SingleChildRenderObjectWidget {
 
 /// Animates the opacity of a sliver widget.
 ///
-/// {@tool snippet --template=stateful_widget_scaffold}
+/// {@tool snippet --template=stateful_widget_scaffold_center_freeform_state}
 /// Creates a [CustomScrollView] with a [SliverFixedExtentList] that uses a
 /// [SliverFadeTransition] to fade the list in and out.
 ///
-/// ```dart preamble
-/// AnimationController controller;
-/// Animation<double> animation;
-///
-/// initState() {
-///   super.initState();
-///   controller = AnimationController(
-///       duration: const Duration(milliseconds: 1000), vsync: this);
-///   animation = CurvedAnimation(parent: controller, curve: Curves.easeIn);
-///
-///   animation.addStatusListener((status) {
-///     if (status == AnimationStatus.completed) {
-///       controller.reverse();
-///     } else if (status == AnimationStatus.dismissed) {
-///       controller.forward();
-///     }
-///   });
-///
-///   controller.forward();
-/// }
-/// ```
 /// ```dart
-/// Widget build(BuildContext context) {
-///   return CustomScrollView(
-///     slivers: <Widget>[
-///       SliverFadeTransition(
-///         opacity: animation,
-///         sliver: SliverFixedExtentList(
-///           itemExtent: 100.0,
-///           delegate: SliverChildBuilderDelegate(
-///             (BuildContext context, int index) {
-///               return Container(
-///                 color: index % 2 == 0
-///                   ? Colors.indigo[200]
-///                   : Colors.orange[200],
-///               );
-///             },
-///             childCount: 5,
+/// class _MyStatefulWidgetState extends State<MyStatefulWidget> with SingleTickerProviderStateMixin {
+///   AnimationController controller;
+///   Animation<double> animation;
+///
+///   initState() {
+///     super.initState();
+///     controller = AnimationController(
+///         duration: const Duration(milliseconds: 1000), vsync: this);
+///     animation = CurvedAnimation(parent: controller, curve: Curves.easeIn);
+///
+///     animation.addStatusListener((status) {
+///       if (status == AnimationStatus.completed) {
+///         controller.reverse();
+///       } else if (status == AnimationStatus.dismissed) {
+///         controller.forward();
+///       }
+///     });
+///     controller.forward();
+///   }
+///
+///   Widget build(BuildContext context) {
+///     return CustomScrollView(
+///       slivers: <Widget>[
+///         SliverFadeTransition(
+///           opacity: animation,
+///           sliver: SliverFixedExtentList(
+///             itemExtent: 100.0,
+///             delegate: SliverChildBuilderDelegate(
+///               (BuildContext context, int index) {
+///                 return Container(
+///                   color: index % 2 == 0
+///                     ? Colors.indigo[200]
+///                     : Colors.orange[200],
+///                 );
+///               },
+///               childCount: 5,
+///             ),
 ///           ),
-///         ),
-///       )
-///     ]
-///   );
+///         )
+///       ]
+///     );
+///   }
 /// }
 /// ```
 /// {@end-tool}
