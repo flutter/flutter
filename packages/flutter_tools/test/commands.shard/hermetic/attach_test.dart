137226b8af5afbbedc5568b87c6abbd91f195166
jonahwilliams <jonahwilliams@google.com>
2020-01-08 13:50:38 -0800

    [flutter_tools] Remove context usage from status and clean up test cases

--- packages/flutter_tools/test/commands.shard/hermetic/attach_test.dart
+++ packages/flutter_tools/test/commands.shard/hermetic/attach_test.dart
@@ -699,7 +699,11 @@ class StreamLogger extends Logger {
     int progressIndicatorPadding = kDefaultStatusPadding,
   }) {
     _log('[progress] $message');
-    return SilentStatus(timeout: timeout, timeoutConfiguration: timeoutConfiguration)..start();
+    return SilentStatus(
+      timeout: timeout,
+      timeoutConfiguration: timeoutConfiguration,
+      stopwatch: Stopwatch(),
+    )..start();
   }
 
   bool _interrupt = false;
