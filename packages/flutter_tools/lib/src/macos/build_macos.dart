6fb0dcc82c5fe7eb929338214dc595207df3b1f9
stuartmorgan <stuartmorgan@google.com>
2020-01-08 12:39:49 -0800

    Check for desktop project files before building (#48350)
    Moves the checks that projects have been configured for desktop to a lower level, where they will cover more codepaths (e.g., 'run'), and improves them to check for native build projects, rather than just directories, to catch cases where the directory exists (e.g., due to accidental creation of generated files).
    
    Also adds links to the error messages pointing to instructions on adding desktop support to a project.
    
    Fixes #47145

--- packages/flutter_tools/lib/src/macos/build_macos.dart
+++ packages/flutter_tools/lib/src/macos/build_macos.dart
@@ -20,6 +20,12 @@ Future<void> buildMacOS({
   BuildInfo buildInfo,
   String targetOverride,
 }) async {
+  if (!flutterProject.macos.xcodeWorkspace.existsSync()) {
+    throwToolExit('No macOS desktop project configured. '
+      'See https://flutter.dev/desktop#add-desktop-support-to-an-existing-flutter-project '
+      'to learn about adding macOS support to a project.');
+  }
+
   final Directory flutterBuildDir = globals.fs.directory(getMacOSBuildDirectory());
   if (!flutterBuildDir.existsSync()) {
     flutterBuildDir.createSync(recursive: true);
