a7c550ad614104ed1ae3a8adcb0b881103b5d749
Tianguang Zhang <penn.zht@gmail.com>
2020-01-09 15:16:58 +0100

    Remove abstract class.

--- packages/flutter/lib/src/material/floating_action_button_location.dart
+++ packages/flutter/lib/src/material/floating_action_button_location.dart
@@ -160,13 +160,17 @@ double _startOffset(ScaffoldPrelayoutGeometry scaffoldGeometry, { double offset 
   return null;
 }
 
+double _centerOffset(ScaffoldPrelayoutGeometry scaffoldGeometry) {
+  return (scaffoldGeometry.scaffoldSize.width - scaffoldGeometry.floatingActionButtonSize.width) / 2.0;
+}
+
 class _CenterFloatFloatingActionButtonLocation extends FloatingActionButtonLocation {
   const _CenterFloatFloatingActionButtonLocation();
 
   @override
   Offset getOffset(ScaffoldPrelayoutGeometry scaffoldGeometry) {
     // Compute the x-axis offset.
-    final double fabX = (scaffoldGeometry.scaffoldSize.width - scaffoldGeometry.floatingActionButtonSize.width) / 2.0;
+    final double fabX = _centerOffset(scaffoldGeometry);
 
     // Compute the y-axis offset.
     final double contentBottom = scaffoldGeometry.contentBottom;
@@ -213,53 +217,44 @@ class _EndFloatFloatingActionButtonLocation extends FloatingActionButtonLocation
   String toString() => 'FloatingActionButtonLocation.endFloat';
 }
 
-// Provider of common logic for [FloatingActionButtonLocation]s that
-// dock to the [BottomAppBar].
-abstract class _DockedFloatingActionButtonLocation extends FloatingActionButtonLocation {
-  const _DockedFloatingActionButtonLocation();
+double _getDockedY(ScaffoldPrelayoutGeometry scaffoldGeometry) {
+  final double contentBottom = scaffoldGeometry.contentBottom;
+  final double bottomSheetHeight = scaffoldGeometry.bottomSheetSize.height;
+  final double fabHeight = scaffoldGeometry.floatingActionButtonSize.height;
+  final double snackBarHeight = scaffoldGeometry.snackBarSize.height;
 
-  // Positions the Y coordinate of the [FloatingActionButton] at a height
-  // where it docks to the [BottomAppBar].
-  @protected
-  double getDockedY(ScaffoldPrelayoutGeometry scaffoldGeometry) {
-    final double contentBottom = scaffoldGeometry.contentBottom;
-    final double bottomSheetHeight = scaffoldGeometry.bottomSheetSize.height;
-    final double fabHeight = scaffoldGeometry.floatingActionButtonSize.height;
-    final double snackBarHeight = scaffoldGeometry.snackBarSize.height;
+  double fabY = contentBottom - fabHeight / 2.0;
+  // The FAB should sit with a margin between it and the snack bar.
+  if (snackBarHeight > 0.0)
+    fabY = math.min(fabY, contentBottom - snackBarHeight - fabHeight - kFloatingActionButtonMargin);
+  // The FAB should sit with its center in front of the top of the bottom sheet.
+  if (bottomSheetHeight > 0.0)
+    fabY = math.min(fabY, contentBottom - bottomSheetHeight - fabHeight / 2.0);
 
-    double fabY = contentBottom - fabHeight / 2.0;
-    // The FAB should sit with a margin between it and the snack bar.
-    if (snackBarHeight > 0.0)
-      fabY = math.min(fabY, contentBottom - snackBarHeight - fabHeight - kFloatingActionButtonMargin);
-    // The FAB should sit with its center in front of the top of the bottom sheet.
-    if (bottomSheetHeight > 0.0)
-      fabY = math.min(fabY, contentBottom - bottomSheetHeight - fabHeight / 2.0);
-
-    final double maxFabY = scaffoldGeometry.scaffoldSize.height - fabHeight;
-    return math.min(maxFabY, fabY);
-  }
+  final double maxFabY = scaffoldGeometry.scaffoldSize.height - fabHeight;
+  return math.min(maxFabY, fabY);
 }
 
-class _EndDockedFloatingActionButtonLocation extends _DockedFloatingActionButtonLocation {
+class _EndDockedFloatingActionButtonLocation extends FloatingActionButtonLocation {
   const _EndDockedFloatingActionButtonLocation();
 
   @override
   Offset getOffset(ScaffoldPrelayoutGeometry scaffoldGeometry) {
     final double fabX = _endOffset(scaffoldGeometry);
-    return Offset(fabX, getDockedY(scaffoldGeometry));
+    return Offset(fabX, _getDockedY(scaffoldGeometry));
   }
 
   @override
   String toString() => 'FloatingActionButtonLocation.endDocked';
 }
 
-class _CenterDockedFloatingActionButtonLocation extends _DockedFloatingActionButtonLocation {
+class _CenterDockedFloatingActionButtonLocation extends FloatingActionButtonLocation {
   const _CenterDockedFloatingActionButtonLocation();
 
   @override
   Offset getOffset(ScaffoldPrelayoutGeometry scaffoldGeometry) {
-    final double fabX = (scaffoldGeometry.scaffoldSize.width - scaffoldGeometry.floatingActionButtonSize.width) / 2.0;
-    return Offset(fabX, getDockedY(scaffoldGeometry));
+    final double fabX = _centerOffset(scaffoldGeometry);
+    return Offset(fabX, _getDockedY(scaffoldGeometry));
   }
 
   @override
