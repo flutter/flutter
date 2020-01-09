b7b35fc1f9e203ce9d665a282bb4120b36cfb3d1
Chun-Heng Tai <chtai@google.com>
2020-01-08 16:48:43 -0800

    Revert "Revert "reland add lifecycle enum and fix the scheduleforcedframe (#45133)" (#45260)"
    This reverts commit f20471d081c1e627bc9faebea8eb58c98bce9506.

--- packages/flutter/lib/src/scheduler/binding.dart
+++ packages/flutter/lib/src/scheduler/binding.dart
@@ -335,9 +335,8 @@ mixin SchedulerBinding on BindingBase, ServicesBinding {
         _setFramesEnabledState(true);
         break;
       case AppLifecycleState.paused:
+      case AppLifecycleState.detached:
         _setFramesEnabledState(false);
-        break;
-      default:
         break;
     }
   }
@@ -355,6 +354,8 @@ mixin SchedulerBinding on BindingBase, ServicesBinding {
         return AppLifecycleState.resumed;
       case 'AppLifecycleState.inactive':
         return AppLifecycleState.inactive;
+      case 'AppLifecycleState.detached':
+        return AppLifecycleState.detached;
     }
     return null;
   }
@@ -806,6 +807,11 @@ mixin SchedulerBinding on BindingBase, ServicesBinding {
   /// Consider using [scheduleWarmUpFrame] instead if the goal is to update the
   /// rendering as soon as possible (e.g. at application startup).
   void scheduleForcedFrame() {
+    // TODO(chunhtai): Removes the if case once the issue is fixed
+    // https://github.com/flutter/flutter/issues/45131
+    if (!_framesEnabled)
+      return;
+
     if (_hasScheduledFrame)
       return;
     assert(() {
