4fa39f86cb7de95e5a58152f4957c3476469eb9f
brandondiamond <1036976+brandondiamond@users.noreply.github.com>
2020-01-08 21:26:44 -0500

    Add support for on/off switch labels when built on iOS 13. (flutter/engine#12467)

--- engine/src/flutter/lib/ui/window.dart
+++ engine/src/flutter/lib/ui/window.dart
@@ -1213,6 +1213,7 @@ class AccessibilityFeatures {
   static const int _kDisableAnimationsIndex = 1 << 2;
   static const int _kBoldTextIndex = 1 << 3;
   static const int _kReduceMotionIndex = 1 << 4;
+  static const int _kOnOffSwitchLabelsIndex = 1 << 5;
 
   // A bitfield which represents each enabled feature.
   final int _index;
@@ -1240,6 +1241,11 @@ class AccessibilityFeatures {
   /// Only supported on iOS.
   bool get reduceMotion => _kReduceMotionIndex & _index != 0;
 
+  /// The platform is requesting that on/off labels be added to switches.
+  ///
+  /// Only supported on iOS.
+  bool get onOffSwitchLabels => _kOnOffSwitchLabelsIndex & _index != 0;
+
   @override
   String toString() {
     final List<String> features = <String>[];
@@ -1253,6 +1259,8 @@ class AccessibilityFeatures {
       features.add('boldText');
     if (reduceMotion)
       features.add('reduceMotion');
+    if (onOffSwitchLabels)
+      features.add('onOffSwitchLabels');
     return 'AccessibilityFeatures$features';
   }
 
--- engine/src/flutter/shell/platform/darwin/ios/framework/Source/FlutterViewController.mm
+++ engine/src/flutter/shell/platform/darwin/ios/framework/Source/FlutterViewController.mm
@@ -238,6 +238,13 @@ typedef enum UIAccessibilityContrast : NSInteger {
              selector:@selector(onUserSettingsChanged:)
                  name:UIContentSizeCategoryDidChangeNotification
                object:nil];
+
+  if (@available(iOS 13, *)) {
+    [center addObserver:self
+               selector:@selector(onAccessibilityStatusChanged:)
+                   name:UIAccessibilityOnOffSwitchLabelsDidChangeNotification
+                 object:nil];
+  }
 }
 
 - (void)setInitialRoute:(NSString*)route {
@@ -889,6 +896,10 @@ static flutter::PointerData::DeviceKind DeviceKindFromTouchType(UITouch* touch) 
     flags |= static_cast<int32_t>(flutter::AccessibilityFeatureFlag::kReduceMotion);
   if (UIAccessibilityIsBoldTextEnabled())
     flags |= static_cast<int32_t>(flutter::AccessibilityFeatureFlag::kBoldText);
+  if (@available(iOS 13, *)) {
+    if (UIAccessibilityIsOnOffSwitchLabelsEnabled())
+      flags |= static_cast<int32_t>(flutter::AccessibilityFeatureFlag::kOnOffSwitchLabels);
+  }
 #if TARGET_OS_SIMULATOR
   // There doesn't appear to be any way to determine whether the accessibility
   // inspector is enabled on the simulator. We conservatively always turn on the
