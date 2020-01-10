5f4e4a3f8acba51fb771a1ddc64849c905afb250
Jonah Williams <jonahwilliams@google.com>
2020-01-10 09:11:42 -0800

    Revert "Migrate some more integration tests to Android embedding v2 (#48532)" (#48582)
    This reverts commit 1613a62e85269433682ffc97653d197c50c06240.

--- dev/integration_tests/image_loading/android/app/src/main/java/com/yourcompany/platforminteraction/MainActivity.java
+++ dev/integration_tests/image_loading/android/app/src/main/java/com/yourcompany/platforminteraction/MainActivity.java
@@ -4,14 +4,15 @@
 
 package com.yourcompany.platforminteraction;
 
-import androidx.annotation.NonNull;
-import io.flutter.embedding.android.FlutterActivity;
-import io.flutter.embedding.engine.FlutterEngine;
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
+import io.flutter.view.FlutterView;
 
 public class MainActivity extends FlutterActivity {
   @Override
-  public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
-    GeneratedPluginRegistrant.registerWith(flutterEngine);
+  protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      GeneratedPluginRegistrant.registerWith(this);
   }
 }
