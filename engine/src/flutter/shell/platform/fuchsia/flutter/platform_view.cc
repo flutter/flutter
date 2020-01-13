9d4e65e8a53481d0cff2df5434cdfaa3bc78be89
Gary Qian <garyq@google.com>
2020-01-13 13:37:49 -0800

    Trivial typo fix to kick tests. (flutter/engine#15557)

--- engine/src/flutter/shell/platform/fuchsia/flutter/platform_view.cc
+++ engine/src/flutter/shell/platform/fuchsia/flutter/platform_view.cc
@@ -567,7 +567,7 @@ std::unique_ptr<flutter::VsyncWaiter> PlatformView::CreateVSyncWaiter() {
 // |flutter::PlatformView|
 std::unique_ptr<flutter::Surface> PlatformView::CreateRenderingSurface() {
   // This platform does not repeatly lose and gain a surface connection. So the
-  // surface is setup once during platform view setup and and returned to the
+  // surface is setup once during platform view setup and returned to the
   // shell on the initial (and only) |NotifyCreated| call.
   return std::move(surface_);
 }
