b854406ef2f2fb1205296587d582b1f14f2f4b24
Anthony Robledo <anthonyrobledo@gmail.com>
2020-01-13 15:32:08 -0500

    Remove accidental files

--- examples/flutter_gallery/macos/Flutter/GeneratedPluginRegistrant.swift
+++ examples/flutter_gallery/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,8 @@
 import FlutterMacOS
 import Foundation
 
+import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
