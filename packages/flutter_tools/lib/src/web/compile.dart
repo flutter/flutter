916d5fff3d9626772b0310812939a0f7959b7518
Jonah Williams <jonahwilliams@google.com>
2020-01-13 17:13:02 -0800

    [flutter_tools][web] Add basic service worker generation support to web applications (#48344)

--- packages/flutter_tools/lib/src/web/compile.dart
+++ packages/flutter_tools/lib/src/web/compile.dart
@@ -39,7 +39,7 @@ Future<void> buildWeb(
   final Status status = globals.logger.startProgress('Compiling $target for the Web...', timeout: null);
   final Stopwatch sw = Stopwatch()..start();
   try {
-    final BuildResult result = await buildSystem.build(const WebReleaseBundle(), Environment(
+    final BuildResult result = await buildSystem.build(const WebServiceWorker(), Environment(
       outputDir: globals.fs.directory(getWebBuildDirectory()),
       projectDir: globals.fs.currentDirectory,
       buildDir: flutterProject.directory
