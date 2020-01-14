c6066c0a5d8ba232aaa34693cafbe5d24445088c
Jenn Magder <magder@google.com>
2020-01-13 16:38:19 -0800

    Generate bitcode for plugin frameworks for flutter build ios-framework

--- dev/devicelab/bin/tasks/build_ios_framework_module_test.dart
+++ dev/devicelab/bin/tasks/build_ios_framework_module_test.dart
@@ -123,6 +123,7 @@ Future<void> main() async {
         );
 
         await _checkFrameworkArchs(appFrameworkPath, mode);
+        await _checkBitcode(appFrameworkPath, mode);
 
         final String aotSymbols = await dylibSymbols(appFrameworkPath);
 
@@ -168,6 +169,7 @@ Future<void> main() async {
         );
 
         await _checkFrameworkArchs(engineFrameworkPath, mode);
+        await _checkBitcode(engineFrameworkPath, mode);
 
         checkFileExists(path.join(
           outputPath,
@@ -211,6 +213,7 @@ Future<void> main() async {
           'device_info',
         );
         await _checkFrameworkArchs(pluginFrameworkPath, mode);
+        await _checkBitcode(pluginFrameworkPath, mode);
 
         checkFileExists(path.join(
           outputPath,
@@ -235,7 +238,7 @@ Future<void> main() async {
         }
       }
 
-      section("Check all modes' have generated plugin registrant");
+      section('Check all modes have generated plugin registrant');
 
       for (final String mode in <String>['Debug', 'Profile', 'Release']) {
         final String registrantFrameworkPath = path.join(
@@ -246,6 +249,7 @@ Future<void> main() async {
         );
 
         await _checkFrameworkArchs(registrantFrameworkPath, mode);
+        await _checkBitcode(registrantFrameworkPath, mode);
 
         checkFileExists(path.join(
           outputPath,
@@ -308,5 +312,14 @@ Future<void> _checkFrameworkArchs(String frameworkPath, String mode) async {
   // Release and Profile should not.
   if (containsSimulator != isDebug) {
     throw TaskResult.failure('$mode $frameworkPath x86_64 architecture ${isDebug ? 'missing' : 'present'}');
+  }
+}
+
+Future<void> _checkBitcode(String frameworkPath, String mode) async {
+  checkFileExists(frameworkPath);
+
+  // Bitcode only needed in Release mode for archiving.
+  if (mode == 'Release' && !await containsBitcode(frameworkPath)) {
+    throw TaskResult.failure('$frameworkPath does not contain bitcode');
   }
 }
