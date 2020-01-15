bc41ab513932694fccaed8c2d40e2e66630fdc88
Nurhan Turgut <nurhan@google.com>
2020-01-14 16:44:44 -0800

    [web] Add Edge support for Flutter Engine unit-tests. (#15641)
    * Add Edge support for Flutter Engine unit-tests. This does not include the launcher
    
    * Add mapping from browser name to platform.
    
    * remove ie from supported browser maps

---
+++ lib/web_ui/dev/edge.dart
@@ -0,0 +1,53 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'dart:async';
+import 'dart:io';
+
+import 'browser.dart';
+import 'common.dart';
+import 'edge_installation.dart';
+
+/// A class for running an instance of Edge.
+///
+/// Most of the communication with the browser is expected to happen via HTTP,
+/// so this exposes a bare-bones API. The browser starts as soon as the class is
+/// constructed, and is killed when [close] is called.
+///
+/// Any errors starting or running the process are reported through [onExit].
+class Edge extends Browser {
+  @override
+  final name = 'Edge';
+
+  static String version;
+
+  /// Starts a new instance of Safari open to the given [url], which may be a
+  /// [Uri] or a [String].
+  factory Edge(Uri url, {bool debug = false}) {
+    version = EdgeArgParser.instance.version;
+
+    assert(version != null);
+    return Edge._(() async {
+      // TODO(nurhan): Configure info log for LUCI.
+      final BrowserInstallation installation = await getEdgeInstallation(
+        version,
+        infoLog: DevNull(),
+      );
+
+      // Debug is not a valid option for Edge. Remove it.
+      String pathToOpen = url.toString();
+      if(pathToOpen.contains('debug')) {
+        int index = pathToOpen.indexOf('debug');
+        pathToOpen = pathToOpen.substring(0, index-1);
+      }
+
+      var process =
+          await Process.start(installation.executable, ['$pathToOpen','-k']);
+
+      return process;
+    });
+  }
+
+  Edge._(Future<Process> startBrowser()) : super(startBrowser);
+}
--- lib/web_ui/dev/supported_browsers.dart
+++ lib/web_ui/dev/supported_browsers.dart
@@ -8,7 +8,9 @@ import 'browser.dart';
 import 'chrome.dart';
 import 'chrome_installer.dart';
 import 'common.dart';
+import 'edge.dart';
 import 'environment.dart';
+import 'edge_installation.dart';
 import 'firefox.dart';
 import 'firefox_installer.dart';
 import 'safari.dart';
@@ -23,21 +25,43 @@ import 'safari_installation.dart';
 ///
 /// One should also implement [BrowserArgParser] and add it to the [argParsers].
 class SupportedBrowsers {
-  final List<BrowserArgParser> argParsers =
-      List.of([ChromeArgParser.instance, FirefoxArgParser.instance, SafariArgParser.instance]);
+  final List<BrowserArgParser> argParsers = List.of([
+    ChromeArgParser.instance,
+    EdgeArgParser.instance,
+    FirefoxArgParser.instance,
+    SafariArgParser.instance
+  ]);
 
-  final List<String> supportedBrowserNames = ['chrome', 'firefox', 'safari'];
+  final List<String> supportedBrowserNames = [
+    'chrome',
+    'edge',
+    'firefox',
+    'safari'
+  ];
 
   final Map<String, Runtime> supportedBrowsersToRuntimes = {
     'chrome': Runtime.chrome,
+    'edge': Runtime.internetExplorer,
     'firefox': Runtime.firefox,
     'safari': Runtime.safari,
   };
 
+  final Map<String, String> supportedBrowserToPlatform = {
+    'chrome': 'chrome',
+    'edge': 'ie',
+    'firefox': 'firefox',
+    'safari': 'safari',
+  };
+
   final Map<String, String> browserToConfiguration = {
-    'chrome': '--configuration=${environment.webUiRootDir.path}/dart_test_chrome.yaml',
-    'firefox': '--configuration=${environment.webUiRootDir.path}/dart_test_firefox.yaml',
-    'safari': '--configuration=${environment.webUiRootDir.path}/dart_test_safari.yaml',
+    'chrome':
+        '--configuration=${environment.webUiRootDir.path}/dart_test_chrome.yaml',
+    'edge':
+        '--configuration=${environment.webUiRootDir.path}/dart_test_edge.yaml',
+    'firefox':
+        '--configuration=${environment.webUiRootDir.path}/dart_test_firefox.yaml',
+    'safari':
+        '--configuration=${environment.webUiRootDir.path}/dart_test_safari.yaml',
   };
 
   static final SupportedBrowsers _singletonInstance = SupportedBrowsers._();
@@ -48,8 +72,10 @@ class SupportedBrowsers {
   SupportedBrowsers._();
 
   Browser getBrowser(Runtime runtime, Uri url, {bool debug = false}) {
-    if (runtime ==  Runtime.chrome) {
+    if (runtime == Runtime.chrome) {
       return Chrome(url, debug: debug);
+    } else if (runtime == Runtime.internetExplorer) {
+      return Edge(url, debug: debug);
     } else if (runtime == Runtime.firefox) {
       return Firefox(url, debug: debug);
     } else if (runtime == Runtime.safari) {
