b891456ebf062f7db073b6531737742dce24d912
Justin McCandless <jmccandless@google.com>
2020-01-17 12:53:02 -0800

    Cupertino(Sliding)SegmentedControl docs reference (#48925)

--- packages/flutter/lib/src/cupertino/segmented_control.dart
+++ packages/flutter/lib/src/cupertino/segmented_control.dart
@@ -61,6 +61,8 @@ const Duration _kFadeDuration = Duration(milliseconds: 165);
 ///
 /// See also:
 ///
+///  * [CupertinoSegmentedControl], a segmented control widget in the style used
+///    up until iOS 13.
 ///  * <https://developer.apple.com/design/human-interface-guidelines/ios/controls/segmented-controls/>
 class CupertinoSegmentedControl<T> extends StatefulWidget {
   /// Creates an iOS-style segmented control bar.
