d584054d0432f2a28ffd92b6fa475791ca87655e
Tianguang Zhang <penn.zht@gmail.com>
2020-01-17 17:25:25 -0500

    Minor changes.

--- examples/flutter_gallery/test/demo/material/menu_demo_test.dart
+++ examples/flutter_gallery/test/demo/material/menu_demo_test.dart
@@ -89,10 +89,10 @@ class CustomContrastGuideline extends AccessibilityGuideline {
       } else {
         return Evaluation.fail(
             '$element:\nExpected contrast ratio of at least '
-                '$kMinimumRatio but found ${contrastRatio.toStringAsFixed(2)} '
-                'The computed light color was: ${report.lightColor}, '
-                'The computed dark color was: ${report.darkColor}\n'
-                'See also: https://www.w3.org/TR/UNDERSTANDING-WCAG20/visual-audio-contrast-contrast.html'
+            '$kMinimumRatio but found ${contrastRatio.toStringAsFixed(2)} \n'
+            'The computed light color was: ${report.lightColor}, '
+            'The computed dark color was: ${report.darkColor}\n'
+            'See also: https://www.w3.org/TR/UNDERSTANDING-WCAG20/visual-audio-contrast-contrast.html\n'
         );
       }
     }
