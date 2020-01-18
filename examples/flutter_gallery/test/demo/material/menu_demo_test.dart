8e399b861355d84b92160b8cfb529be8bf5cc235
Tianguang Zhang <penn.zht@gmail.com>
2020-01-17 20:34:12 -0500

    Rename.

--- examples/flutter_gallery/test/demo/material/menu_demo_test.dart
+++ examples/flutter_gallery/test/demo/material/menu_demo_test.dart
@@ -34,9 +34,9 @@ void main() {
 
     await expectLater(tester, meetsGuideline(textContrastGuideline));
 
-    final List<Element> elements = find.byWidgetPredicate((Widget widget) => widget is Icon).evaluate().toList();
+    final List<Element> icons = find.byWidgetPredicate((Widget widget) => widget is Icon).evaluate().toList();
 
-    await expectLater(tester, meetsGuideline(CustomContrastGuideline(elements: elements)));
+    await expectLater(tester, meetsGuideline(CustomContrastGuideline(elements: icons)));
   });
 }
 
