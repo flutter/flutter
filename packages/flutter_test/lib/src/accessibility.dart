994a4d1e1feef3830bd4e4d848954094bc81f032
Tianguang Zhang <penn.zht@gmail.com>
2020-01-20 16:55:12 +0100

    Use common code for subsetFromRect.

--- packages/flutter_test/lib/src/accessibility.dart
+++ packages/flutter_test/lib/src/accessibility.dart
@@ -263,7 +263,7 @@ class MinimumTextContrastGuideline extends AccessibilityGuideline {
       if (_isNodeOffScreen(paintBounds, tester.binding.window)) {
         return result;
       }
-      final List<int> subset = _subsetToRect(byteData, paintBounds, image.width, image.height);
+      final List<int> subset = _subsetFromRect(byteData, paintBounds, image.width, image.height);
       // Node was too far off screen.
       if (subset.isEmpty) {
         return result;
@@ -310,35 +310,6 @@ class MinimumTextContrastGuideline extends AccessibilityGuideline {
       || paintBounds.left <  -50.0
       || paintBounds.bottom > (window.physicalSize.height * window.devicePixelRatio) + 50.0
       || paintBounds.right > (window.physicalSize.width * window.devicePixelRatio)  + 50.0;
-  }
-
-  List<int> _subsetToRect(ByteData data, Rect paintBounds, int width, int height) {
-    final int newWidth = paintBounds.size.width.ceil();
-    final int newHeight = paintBounds.size.height.ceil();
-    final int leftX = paintBounds.topLeft.dx.ceil();
-    final int rightX = leftX + newWidth;
-    final int topY = paintBounds.topLeft.dy.ceil();
-    final int bottomY = topY + newHeight;
-    final List<int> buffer = <int>[];
-
-    // Data is stored in row major order.
-    for (int i = 0; i < data.lengthInBytes; i+=4) {
-      final int index = i ~/ 4;
-      final int dx = index % width;
-      final int dy = index ~/ width;
-      if (dx >= leftX && dx <= rightX && dy >= topY && dy <= bottomY) {
-        final int r = data.getUint8(i);
-        final int g = data.getUint8(i + 1);
-        final int b = data.getUint8(i + 2);
-        final int a = data.getUint8(i + 3);
-        final int color = (((a & 0xff) << 24) |
-          ((r & 0xff) << 16) |
-          ((g & 0xff) << 8)  |
-          ((b & 0xff) << 0)) & 0xFFFFFFFF;
-        buffer.add(color);
-      }
-    }
-    return buffer;
   }
 
   @override
@@ -419,40 +390,6 @@ class CustomContrastGuideline extends AccessibilityGuideline {
     }
 
     return result;
-  }
-
-  List<int> _subsetFromRect(ByteData data, Rect paintBounds, int width, int height) {
-    final Rect truePaintBounds = paintBounds.intersect(
-      Rect.fromLTWH(0.0, 0.0, width.toDouble(), height.toDouble()),
-    );
-
-    final int leftX   = truePaintBounds.left.floor();
-    final int rightX  = truePaintBounds.right.ceil();
-    final int topY    = truePaintBounds.top.floor();
-    final int bottomY = truePaintBounds.bottom.ceil();
-
-    final List<int> buffer = <int>[];
-
-    int _getPixel(ByteData data, int x, int y) {
-      final int offset = (y * width + x) * 4;
-      final int r = data.getUint8(offset);
-      final int g = data.getUint8(offset + 1);
-      final int b = data.getUint8(offset + 2);
-      final int a = data.getUint8(offset + 3);
-      final int color = (((a & 0xff) << 24) |
-      ((r & 0xff) << 16) |
-      ((g & 0xff) << 8)  |
-      ((b & 0xff) << 0)) & 0xFFFFFFFF;
-      return color;
-    }
-
-    for (int x = leftX; x < rightX; x ++) {
-      for (int y = topY; y < bottomY; y ++) {
-        buffer.add(_getPixel(data, x, y));
-      }
-    }
-
-    return buffer;
   }
 
   @override
@@ -548,6 +485,40 @@ class ContrastReport {
       b = math.pow((b + 0.055)/ 1.055, 2.4).toDouble();
     return 0.2126 * r + 0.7152 * g + 0.0722 * b;
   }
+}
+
+List<int> _subsetFromRect(ByteData data, Rect paintBounds, int width, int height) {
+  final Rect truePaintBounds = paintBounds.intersect(
+    Rect.fromLTWH(0.0, 0.0, width.toDouble(), height.toDouble()),
+  );
+
+  final int leftX   = truePaintBounds.left.floor();
+  final int rightX  = truePaintBounds.right.ceil();
+  final int topY    = truePaintBounds.top.floor();
+  final int bottomY = truePaintBounds.bottom.ceil();
+
+  final List<int> buffer = <int>[];
+
+  int _getPixel(ByteData data, int x, int y) {
+    final int offset = (y * width + x) * 4;
+    final int r = data.getUint8(offset);
+    final int g = data.getUint8(offset + 1);
+    final int b = data.getUint8(offset + 2);
+    final int a = data.getUint8(offset + 3);
+    final int color = (((a & 0xff) << 24) |
+    ((r & 0xff) << 16) |
+    ((g & 0xff) << 8)  |
+    ((b & 0xff) << 0)) & 0xFFFFFFFF;
+    return color;
+  }
+
+  for (int x = leftX; x < rightX; x ++) {
+    for (int y = topY; y < bottomY; y ++) {
+      buffer.add(_getPixel(data, x, y));
+    }
+  }
+
+  return buffer;
 }
 
 /// A guideline which requires tappable semantic nodes a minimum size of 48 by 48.
