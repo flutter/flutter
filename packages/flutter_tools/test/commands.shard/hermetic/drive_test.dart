0b98f676e143b2fa505df6776d31ed26ed18fa0d
Angjie Li <56002538+angjieli@users.noreply.github.com>
2020-01-21 16:48:02 -0800

    Use flutter_tools to start WebDriver browser. (#49228)

--- packages/flutter_tools/test/commands.shard/hermetic/drive_test.dart
+++ packages/flutter_tools/test/commands.shard/hermetic/drive_test.dart
@@ -17,6 +17,7 @@ import 'package:flutter_tools/src/device.dart';
 import 'package:flutter_tools/src/build_info.dart';
 import 'package:mockito/mockito.dart';
 import 'package:flutter_tools/src/globals.dart' as globals;
+import 'package:webdriver/sync_io.dart' as sync_io;
 
 import '../../src/common.dart';
 import '../../src/context.dart';
@@ -177,10 +178,6 @@ void main() {
         // VM_SERVICE_URL is not set by drive command arguments
         expect(environment, <String, String>{
           'VM_SERVICE_URL': 'null',
-          'SELENIUM_PORT': '4567',
-          'BROWSER_NAME': 'firefox',
-          'BROWSER_DIMENSION': '1024,768',
-          'HEADLESS': 'false',
         });
         return null;
       });
@@ -196,10 +193,6 @@ void main() {
         'drive',
         '--target=$testApp',
         '--no-pub',
-        '--no-headless',
-        '--driver-port=4567',
-        '--browser-name=firefox',
-        '--browser-dimension=1024,768',
       ];
       await createTestCommandRunner(command).run(args);
       expect(testLogger.errorText, isEmpty);
@@ -582,6 +575,147 @@ void main() {
         '--cache-sksl',
         () => debuggingOptions.cacheSkSL,
       );
+    });
+  });
+
+  group('getDesiredCapabilities', () {
+    test('Chrome with headless on', () {
+      final Map<String, dynamic> expected = <String, dynamic>{
+        'acceptInsecureCerts': true,
+        'browserName': 'chrome',
+        'goog:loggingPrefs': <String, String>{ sync_io.LogType.performance: 'ALL'},
+        'chromeOptions': <String, dynamic>{
+          'w3c': false,
+          'args': <String>[
+            '--bwsi',
+            '--disable-background-timer-throttling',
+            '--disable-default-apps',
+            '--disable-extensions',
+            '--disable-popup-blocking',
+            '--disable-translate',
+            '--no-default-browser-check',
+            '--no-sandbox',
+            '--no-first-run',
+            '--headless'
+          ],
+          'perfLoggingPrefs': <String, String>{
+            'traceCategories':
+            'devtools.timeline,'
+                'v8,blink.console,benchmark,blink,'
+                'blink.user_timing'
+          }
+        }
+      };
+
+      expect(getDesiredCapabilities(Browser.chrome, true), expected);
+    });
+
+    test('Chrome with headless off', () {
+      final Map<String, dynamic> expected = <String, dynamic>{
+        'acceptInsecureCerts': true,
+        'browserName': 'chrome',
+        'goog:loggingPrefs': <String, String>{ sync_io.LogType.performance: 'ALL'},
+        'chromeOptions': <String, dynamic>{
+          'w3c': false,
+          'args': <String>[
+            '--bwsi',
+            '--disable-background-timer-throttling',
+            '--disable-default-apps',
+            '--disable-extensions',
+            '--disable-popup-blocking',
+            '--disable-translate',
+            '--no-default-browser-check',
+            '--no-sandbox',
+            '--no-first-run',
+          ],
+          'perfLoggingPrefs': <String, String>{
+            'traceCategories':
+            'devtools.timeline,'
+                'v8,blink.console,benchmark,blink,'
+                'blink.user_timing'
+          }
+        }
+      };
+
+      expect(getDesiredCapabilities(Browser.chrome, false), expected);
+
+    });
+
+    test('Firefox with headless on', () {
+      final Map<String, dynamic> expected = <String, dynamic>{
+        'acceptInsecureCerts': true,
+        'browserName': 'firefox',
+        'moz:firefoxOptions' : <String, dynamic>{
+          'args': <String>['-headless'],
+          'prefs': <String, dynamic>{
+            'dom.file.createInChild': true,
+            'dom.timeout.background_throttling_max_budget': -1,
+            'media.autoplay.default': 0,
+            'media.gmp-manager.url': '',
+            'media.gmp-provider.enabled': false,
+            'network.captive-portal-service.enabled': false,
+            'security.insecure_field_warning.contextual.enabled': false,
+            'test.currentTimeOffsetSeconds': 11491200
+          },
+          'log': <String, String>{'level': 'trace'}
+        }
+      };
+
+      expect(getDesiredCapabilities(Browser.firefox, true), expected);
+    });
+
+    test('Firefox with headless off', () {
+      final Map<String, dynamic> expected = <String, dynamic>{
+        'acceptInsecureCerts': true,
+        'browserName': 'firefox',
+        'moz:firefoxOptions' : <String, dynamic>{
+          'args': <String>[],
+          'prefs': <String, dynamic>{
+            'dom.file.createInChild': true,
+            'dom.timeout.background_throttling_max_budget': -1,
+            'media.autoplay.default': 0,
+            'media.gmp-manager.url': '',
+            'media.gmp-provider.enabled': false,
+            'network.captive-portal-service.enabled': false,
+            'security.insecure_field_warning.contextual.enabled': false,
+            'test.currentTimeOffsetSeconds': 11491200
+          },
+          'log': <String, String>{'level': 'trace'}
+        }
+      };
+
+      expect(getDesiredCapabilities(Browser.firefox, false), expected);
+    });
+
+    test('Edge', () {
+      final Map<String, dynamic> expected = <String, dynamic>{
+        'acceptInsecureCerts': true,
+        'browserName': 'edge',
+      };
+
+      expect(getDesiredCapabilities(Browser.edge, false), expected);
+    });
+
+    test('macOS Safari', () {
+      final Map<String, dynamic> expected = <String, dynamic>{
+        'browserName': 'safari',
+        'safari.options': <String, dynamic>{
+          'skipExtensionInstallation': true,
+          'cleanSession': true
+        }
+      };
+
+      expect(getDesiredCapabilities(Browser.safari, false), expected);
+    });
+
+    test('iOS Safari', () {
+      final Map<String, dynamic> expected = <String, dynamic>{
+        'platformName': 'ios',
+        'browserName': 'safari',
+        'safari:useSimulator': true
+      };
+
+      expect(getDesiredCapabilities(Browser.iosSafari, false), expected);
     });
   });
 }
