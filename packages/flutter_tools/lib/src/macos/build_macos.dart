7bd73b5614ec32b9923672c9c507b9fd7b917f29
Jonah Williams <jonahwilliams@google.com>
2020-01-23 15:03:04 -0800

    [flutter_tools] Remove context from Xcode and most of Xcodeproj (#48661)

--- packages/flutter_tools/lib/src/macos/build_macos.dart
+++ packages/flutter_tools/lib/src/macos/build_macos.dart
@@ -85,7 +85,7 @@ Future<void> buildMacOS({
       'OBJROOT=${globals.fs.path.join(flutterBuildDir.absolute.path, 'Build', 'Intermediates.noindex')}',
       'SYMROOT=${globals.fs.path.join(flutterBuildDir.absolute.path, 'Build', 'Products')}',
       'COMPILER_INDEX_STORE_ENABLE=NO',
-      ...environmentVariablesAsXcodeBuildSettings()
+      ...environmentVariablesAsXcodeBuildSettings(globals.platform)
     ], trace: true);
   } finally {
     status.cancel();
