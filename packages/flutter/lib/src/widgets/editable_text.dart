742f6615ba86c096099b83e799b1d6e6a981aa38
GaryQian <garyq@google.com>
2020-01-24 14:40:13 -0800

    Adjustments

--- packages/flutter/lib/src/widgets/editable_text.dart
+++ packages/flutter/lib/src/widgets/editable_text.dart
@@ -1626,13 +1626,14 @@ class EditableTextState extends State<EditableText> with AutomaticKeepAliveClien
   }
 
   void _formatAndSetValue(TextEditingValue value) {
+    // Check if the new value is the same as the current local value, or is the same
+    // as the post-formatting value of the previous pass.
     final bool textChanged = _value?.text != value?.text && value?.text != _lastKnownRemoteTextEditingValue?.text;
     if (textChanged && widget.inputFormatters != null && widget.inputFormatters.isNotEmpty) {
       for (final TextInputFormatter formatter in widget.inputFormatters)
         value = formatter.formatEditUpdate(_value, value);
       _value = value;
-      // _updateRemoteEditingValueIfNeeded();
-      _updateRemoteEditingValueIfNeeded()
+      _updateRemoteEditingValueIfNeeded();
     } else {
       _value = value;
     }
