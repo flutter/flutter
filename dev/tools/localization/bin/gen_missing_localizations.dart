c62efcef26eb57f353b035baf6dcb96235dc70e7
Darren Austin <darrenaustin@google.com>
2020-01-23 16:35:41 -0800

    Style changes based on code review.

--- dev/tools/localization/bin/gen_missing_localizations.dart
+++ dev/tools/localization/bin/gen_missing_localizations.dart
@@ -1,11 +1,11 @@
-// Copyright 2020 The Flutter Authors. All rights reserved.
+// Copyright 2014 The Flutter Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 // This program updates the language locale arb files with any missing resource
-// entries that are included in the english arb files. This is useful when
+// entries that are included in the English arb files. This is useful when
 // adding new resources for localization. You can just add the appropriate
-// entries to the english arb file and then run this script. It will then check
+// entries to the English arb file and then run this script. It will then check
 // all of the other language locale arb files and update them with new 'TBD'
 // entries for any missing resources. These will be picked up by the localization
 // team and then translated.
@@ -29,9 +29,9 @@ import '../localizations_validator.dart';
 Future<void> main(List<String> rawArgs) async {
   checkCwdIsRepoRoot('gen_missing_localizations');
 
-  final String l10nPath = path.join('packages', 'flutter_localizations', 'lib', 'src', 'l10n');
-  updateMissingResources(l10nPath, 'material');
-  updateMissingResources(l10nPath, 'cupertino');
+  final String localizationPath = path.join('packages', 'flutter_localizations', 'lib', 'src', 'l10n');
+  updateMissingResources(localizationPath, 'material');
+  updateMissingResources(localizationPath, 'cupertino');
 }
 
 Map<String, dynamic> loadBundle(File file) {
@@ -73,13 +73,13 @@ bool isPluralVariation(String key, Map<String, dynamic> bundle) {
   return bundle.containsKey('${prefix}Other');
 }
 
-void updateMissingResources(String l10nPath, String groupPrefix) {
-  final Directory l10nDir = Directory(l10nPath);
+void updateMissingResources(String localizationPath, String groupPrefix) {
+  final Directory localizationDir = Directory(localizationPath);
   final RegExp filenamePattern = RegExp('${groupPrefix}_(\\w+)\.arb');
 
-  final Set<String> requiredKeys = resourceKeys(loadBundle(File(path.join(l10nPath, '${groupPrefix}_en.arb'))));
+  final Set<String> requiredKeys = resourceKeys(loadBundle(File(path.join(localizationPath, '${groupPrefix}_en.arb'))));
 
-  for (final FileSystemEntity entity in l10nDir.listSync().toList()..sort(sortFilesByPath)) {
+  for (final FileSystemEntity entity in localizationDir.listSync().toList()..sort(sortFilesByPath)) {
     final String entityPath = entity.path;
     if (FileSystemEntity.isFileSync(entityPath) && filenamePattern.hasMatch(entityPath)) {
       final String localeString = filenamePattern.firstMatch(entityPath)[1];
