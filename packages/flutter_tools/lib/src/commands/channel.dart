38f1259655244cf206df032017895b1368285a9e
Prerak Mann <31154435+mannprerak2@users.noreply.github.com>
2020-01-26 00:33:03 +0530

    sort channels by stability upon running flutter channel (#48427)

--- packages/flutter_tools/lib/src/commands/channel.dart
+++ packages/flutter_tools/lib/src/commands/channel.dart
@@ -55,7 +55,7 @@ class ChannelCommand extends FlutterCommand {
     // Beware: currentBranch could contain PII. See getBranchName().
     final String currentChannel = FlutterVersion.instance.channel;
     final String currentBranch = FlutterVersion.instance.getBranchName();
-    final Set<String> seenChannels = <String>{};
+    final Set<String> seenUnofficialChannels = <String>{};
     final List<String> rawOutput = <String>[];
 
     showAll = showAll || currentChannel != currentBranch;
@@ -65,31 +65,54 @@ class ChannelCommand extends FlutterCommand {
       <String>['git', 'branch', '-r'],
       workingDirectory: Cache.flutterRoot,
       mapFunction: (String line) {
-        if (verbose) {
-          rawOutput.add(line);
-        }
-        final List<String> split = line.split('/');
-        if (split.length < 2) {
-          return null;
-        }
-        final String branchName = split[1];
-        if (seenChannels.contains(branchName)) {
-          return null;
-        }
-        seenChannels.add(branchName);
-        if (branchName == currentBranch) {
-          return '* $branchName';
-        }
-        if (!branchName.startsWith('HEAD ') &&
-            (showAll || FlutterVersion.officialChannels.contains(branchName))) {
-          return '  $branchName';
-        }
+        rawOutput.add(line);
         return null;
       },
     );
     if (result != 0) {
       final String details = verbose ? '\n${rawOutput.join('\n')}' : '';
       throwToolExit('List channels failed: $result$details', exitCode: result);
+    }
+
+    final List<String> officialChannels = FlutterVersion.officialChannels.toList();
+    final List<bool> availableChannels = List<bool>.filled(officialChannels.length, false);
+
+    for (final String line in rawOutput) {
+      final List<String> split = line.split('/');
+      final String branch = split[1];
+      if (split.length > 1) {
+        final int index = officialChannels.indexOf(branch);
+
+        if (index != -1) { // Mark all available channels official channels from output
+          availableChannels[index] = true;
+        } else if (showAll && !seenUnofficialChannels.contains(branch)) {
+        // add other branches to seenUnofficialChannels if --all flag is given (to print later)
+          seenUnofficialChannels.add(branch);
+        }
+      }
+    }
+
+    // print all available official channels in sorted manner
+    for (int i = 0; i < officialChannels.length; i++) {
+      // only print non-missing channels
+      if (availableChannels[i]) {
+        String currentIndicator = ' ';
+        if (officialChannels[i] == currentChannel){
+          currentIndicator = '*';
+        }
+        globals.printStatus('$currentIndicator ${officialChannels[i]}');
+      }
+    }
+
+    // print all remaining channels if showAll is true
+    if (showAll) {
+      for (final String branch in seenUnofficialChannels) {
+        if (currentBranch == branch) {
+          globals.printStatus('* $branch');
+        } else if (!branch.startsWith('HEAD ')) {
+          globals.printStatus('  $branch');
+        }
+      }
     }
   }
 
