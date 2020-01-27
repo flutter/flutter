8aa8151ada28e575d9047267827227b12de977c4
Jenn Magder <magder@google.com>
2020-01-27 10:32:50 -0800

    Inject platform into build ios-framework command (#49463)

--- packages/flutter_tools/test/commands.shard/hermetic/build_ios_framework_test.dart
+++ packages/flutter_tools/test/commands.shard/hermetic/build_ios_framework_test.dart
@@ -5,41 +5,54 @@
 import 'dart:io';
 
 import 'package:file/memory.dart';
+import 'package:flutter_tools/src/aot.dart';
 import 'package:flutter_tools/src/build_info.dart';
+import 'package:flutter_tools/src/bundle.dart';
 import 'package:flutter_tools/src/cache.dart';
 import 'package:flutter_tools/src/commands/build_ios_framework.dart';
 import 'package:flutter_tools/src/base/file_system.dart';
 import 'package:flutter_tools/src/version.dart';
 import 'package:flutter_tools/src/globals.dart' as globals;
 import 'package:mockito/mockito.dart';
+import 'package:platform/platform.dart';
 
 import '../../src/common.dart';
 import '../../src/context.dart';
 
 void main() {
   group('build ios-framework', () {
+    MemoryFileSystem memoryFileSystem;
+    MockFlutterVersion mockFlutterVersion;
+    MockGitTagVersion mockGitTagVersion;
+    MockCache mockCache;
+    Directory outputDirectory;
+    FakePlatform fakePlatform;
+
+    setUpAll(() {
+      Cache.disableLocking();
+    });
+
+    setUp(() {
+      memoryFileSystem = MemoryFileSystem();
+      mockFlutterVersion = MockFlutterVersion();
+      mockGitTagVersion = MockGitTagVersion();
+      mockCache = MockCache();
+      fakePlatform = FakePlatform()..operatingSystem = 'macos';
+
+      when(mockFlutterVersion.gitTagVersion).thenReturn(mockGitTagVersion);
+      outputDirectory = globals.fs.systemTempDirectory
+          .createTempSync('flutter_build_ios_framework_test_output.')
+          .childDirectory('Debug')
+        ..createSync();
+    });
+
     group('podspec', () {
-      MemoryFileSystem memoryFileSystem;
-      MockFlutterVersion mockFlutterVersion;
-      MockGitTagVersion mockGitTagVersion;
-      MockCache mockCache;
-      Directory outputDirectory;
       const String storageBaseUrl = 'https://fake.googleapis.com';
       const String engineRevision = '0123456789abcdef';
       File licenseFile;
 
       setUp(() {
-        memoryFileSystem = MemoryFileSystem();
-        mockFlutterVersion = MockFlutterVersion();
-        mockGitTagVersion = MockGitTagVersion();
-        mockCache = MockCache();
-
         when(mockFlutterVersion.gitTagVersion).thenReturn(mockGitTagVersion);
-        outputDirectory = globals.fs.systemTempDirectory
-            .createTempSync('flutter_build_ios_framework_test_output.')
-            .childDirectory('Debug')
-          ..createSync();
-
         when(mockCache.storageBaseUrl).thenReturn(storageBaseUrl);
         when(mockCache.engineRevision).thenReturn(engineRevision);
         licenseFile = memoryFileSystem.file('LICENSE');
@@ -51,6 +64,9 @@ void main() {
         when(mockFlutterVersion.frameworkVersion).thenReturn(frameworkVersion);
 
         final BuildIOSFrameworkCommand command = BuildIOSFrameworkCommand(
+          aotBuilder: MockAotBuilder(),
+          bundleBuilder: MockBundleBuilder(),
+          platform: fakePlatform,
           flutterVersion: mockFlutterVersion,
           cache: mockCache
         );
@@ -73,6 +89,9 @@ void main() {
         when(mockGitTagVersion.commits).thenReturn(2);
 
         final BuildIOSFrameworkCommand command = BuildIOSFrameworkCommand(
+          aotBuilder: MockAotBuilder(),
+          bundleBuilder: MockBundleBuilder(),
+          platform: fakePlatform,
           flutterVersion: mockFlutterVersion,
           cache: mockCache
         );
@@ -92,6 +111,9 @@ void main() {
         when(mockGitTagVersion.commits).thenReturn(0);
 
         final BuildIOSFrameworkCommand command = BuildIOSFrameworkCommand(
+          aotBuilder: MockAotBuilder(),
+          bundleBuilder: MockBundleBuilder(),
+          platform: fakePlatform,
           flutterVersion: mockFlutterVersion,
           cache: mockCache
         );
@@ -123,8 +145,11 @@ void main() {
 
         testUsingContext('contains license and version', () async {
           final BuildIOSFrameworkCommand command = BuildIOSFrameworkCommand(
-              flutterVersion: mockFlutterVersion,
-              cache: mockCache
+            aotBuilder: MockAotBuilder(),
+            bundleBuilder: MockBundleBuilder(),
+            platform: fakePlatform,
+            flutterVersion: mockFlutterVersion,
+            cache: mockCache
           );
           command.produceFlutterPodspec(BuildMode.debug, outputDirectory);
 
@@ -140,8 +165,11 @@ void main() {
 
         testUsingContext('debug URL', () async {
           final BuildIOSFrameworkCommand command = BuildIOSFrameworkCommand(
-              flutterVersion: mockFlutterVersion,
-              cache: mockCache
+            aotBuilder: MockAotBuilder(),
+            bundleBuilder: MockBundleBuilder(),
+            platform: fakePlatform,
+            flutterVersion: mockFlutterVersion,
+            cache: mockCache
           );
           command.produceFlutterPodspec(BuildMode.debug, outputDirectory);
 
@@ -155,8 +183,11 @@ void main() {
 
         testUsingContext('profile URL', () async {
           final BuildIOSFrameworkCommand command = BuildIOSFrameworkCommand(
-              flutterVersion: mockFlutterVersion,
-              cache: mockCache
+            aotBuilder: MockAotBuilder(),
+            bundleBuilder: MockBundleBuilder(),
+            platform: fakePlatform,
+            flutterVersion: mockFlutterVersion,
+            cache: mockCache
           );
           command.produceFlutterPodspec(BuildMode.profile, outputDirectory);
 
@@ -170,8 +201,11 @@ void main() {
 
         testUsingContext('release URL', () async {
           final BuildIOSFrameworkCommand command = BuildIOSFrameworkCommand(
-              flutterVersion: mockFlutterVersion,
-              cache: mockCache
+            aotBuilder: MockAotBuilder(),
+            bundleBuilder: MockBundleBuilder(),
+            platform: fakePlatform,
+            flutterVersion: mockFlutterVersion,
+            cache: mockCache
           );
           command.produceFlutterPodspec(BuildMode.release, outputDirectory);
 
@@ -190,3 +224,5 @@ void main() {
 class MockFlutterVersion extends Mock implements FlutterVersion {}
 class MockGitTagVersion extends Mock implements GitTagVersion {}
 class MockCache extends Mock implements Cache {}
+class MockAotBuilder extends Mock implements AotBuilder {}
+class MockBundleBuilder extends Mock implements BundleBuilder {}
