d1e25bf233e9d19a7f5fece0cf9c1fd263c0c0e4
Jonah Williams <jonahwilliams@google.com>
2020-01-28 13:08:04 -0800

    [flutter_tools] remove alternative build aot code path (#49577)

--- dev/bots/test.dart
+++ dev/bots/test.dart
@@ -328,8 +328,17 @@ Future<void> _runBuildTests() async {
 
 Future<void> _flutterBuildAot(String relativePathToApplication) async {
   print('${green}Testing AOT build$reset for $cyan$relativePathToApplication$reset...');
+  final String absoluteTarget = path.join(path.absolute(relativePathToApplication), 'lib', 'main.dart');
   await runCommand(flutter,
-    <String>['build', 'aot', '-v'],
+    <String>[
+      'assemble',
+      '-dTargetFile=$absoluteTarget',
+      '-dTargetPlatform=android',
+      '-dBuildMode=release',
+      '--output=build/aot',
+      'android_aot_bundle_release_android-arm64',
+      'android_aot_bundle_release_android-arm',
+    ],
     workingDirectory: path.join(flutterRoot, relativePathToApplication),
   );
 }
