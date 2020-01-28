5f218ab381bdde8a78808a7fbcf83a2fd8bda346
Jonah Williams <jonahwilliams@google.com>
2020-01-27 17:41:29 -0800

    [flutter_tools] Generate correct entrypoint module name for experimental web compiler (#49486)

--- packages/flutter_tools/test/general.shard/web/devfs_web_test.dart
+++ packages/flutter_tools/test/general.shard/web/devfs_web_test.dart
@@ -35,21 +35,23 @@ void main() {
   MockHttpHeaders headers;
   Completer<void> closeCompleter;
   WebAssetServer webAssetServer;
-  MockPlatform windows;
   MockPlatform linux;
   Packages packages;
+  MockPlatform windows;
 
   setUpAll(() async {
-    packages = await loadPackagesFile(Uri.base.resolve('.packages'));
+    packages = await loadPackagesFile(Uri.base.resolve('.packages'), loader: (Uri uri) async {
+      return utf8.encode('\n');
+    });
   });
 
   setUp(() {
-    windows = MockPlatform();
     linux = MockPlatform();
-    when(windows.environment).thenReturn(const <String, String>{});
-    when(windows.isWindows).thenReturn(true);
+    windows = MockPlatform();
     when(linux.isWindows).thenReturn(false);
     when(linux.environment).thenReturn(const <String, String>{});
+    when(windows.environment).thenReturn(const <String, String>{});
+    when(windows.isWindows).thenReturn(true);
     testbed = Testbed(setup: () {
       mockHttpServer = MockHttpServer();
       requestController = StreamController<HttpRequest>.broadcast();
@@ -139,29 +141,6 @@ void main() {
     Platform: () => linux,
   }));
 
-  test('serves JavaScript files from in memory cache on Windows', () => testbed.run(() async {
-    final File source = globals.fs.file('source')
-      ..writeAsStringSync('main() {}');
-    final File sourcemap = globals.fs.file('sourcemap')
-      ..writeAsStringSync('{}');
-    final File manifest = globals.fs.file('manifest')
-      ..writeAsStringSync(json.encode(<String, Object>{'/C:/foo.js': <String, Object>{
-        'code': <int>[0, source.lengthSync()],
-        'sourcemap': <int>[0, 2],
-      }}));
-    webAssetServer.write(source, manifest, sourcemap);
-
-    when(request.uri).thenReturn(Uri.parse('http://foobar/C:/foo.js'));
-    requestController.add(request);
-    await closeCompleter.future;
-
-    verify(headers.add('Content-Length', source.lengthSync())).called(1);
-    verify(headers.add('Content-Type', 'application/javascript')).called(1);
-    verify(response.add(source.readAsBytesSync())).called(1);
-  }, overrides: <Type, Generator>{
-    Platform: () => windows,
-  }));
-
   test('serves JavaScript files from in memory cache not from manifest', () => testbed.run(() async {
     webAssetServer.writeFile('/foo.js', 'main() {}');
 
@@ -193,7 +172,30 @@ void main() {
     verify(response.statusCode = 404).called(1);
   }));
 
-  test('serves Dart files from in filesystem on Windows', () => testbed.run(() async {
+  test('serves JavaScript files from in memory cache on Windows', () => testbed.run(() async {
+    final File source = globals.fs.file('source')
+      ..writeAsStringSync('main() {}');
+    final File sourcemap = globals.fs.file('sourcemap')
+      ..writeAsStringSync('{}');
+    final File manifest = globals.fs.file('manifest')
+      ..writeAsStringSync(json.encode(<String, Object>{'/C:/foo.js': <String, Object>{
+        'code': <int>[0, source.lengthSync()],
+        'sourcemap': <int>[0, 2],
+      }}));
+    webAssetServer.write(source, manifest, sourcemap);
+
+    when(request.uri).thenReturn(Uri.parse('http://foobar/C:/foo.js'));
+    requestController.add(request);
+    await closeCompleter.future;
+
+    verify(headers.add('Content-Length', source.lengthSync())).called(1);
+    verify(headers.add('Content-Type', 'application/javascript')).called(1);
+    verify(response.add(source.readAsBytesSync())).called(1);
+  }, overrides: <Type, Generator>{
+    Platform: () => windows,
+  }));
+
+   test('serves Dart files from in filesystem on Windows', () => testbed.run(() async {
     final File source = globals.fs.file('foo.dart').absolute
       ..createSync(recursive: true)
       ..writeAsStringSync('void main() {}');
@@ -203,6 +205,22 @@ void main() {
     await closeCompleter.future;
 
     verify(headers.add('Content-Length', source.lengthSync())).called(1);
+    verify(response.addStream(any)).called(1);
+  }, overrides: <Type,  Generator>{
+    Platform: () => windows,
+  }));
+
+   test('serves asset files from in filesystem with known mime type on Windows', () => testbed.run(() async {
+    final File source = globals.fs.file(globals.fs.path.join('build', 'flutter_assets', 'foo.png'))
+      ..createSync(recursive: true)
+      ..writeAsBytesSync(kTransparentImage);
+
+    when(request.uri).thenReturn(Uri.parse('http://foobar/assets/foo.png'));
+    requestController.add(request);
+    await closeCompleter.future;
+
+    verify(headers.add('Content-Length', source.lengthSync())).called(1);
+    verify(headers.add('Content-Type', 'image/png')).called(1);
     verify(response.addStream(any)).called(1);
   }, overrides: <Type,  Generator>{
     Platform: () => windows,
@@ -244,23 +262,6 @@ void main() {
     verify(headers.add('Content-Type', 'image/png')).called(1);
     verify(response.addStream(any)).called(1);
   }));
-
-  test('serves asset files from in filesystem with known mime type on Windows', () => testbed.run(() async {
-    final File source = globals.fs.file(globals.fs.path.join('build', 'flutter_assets', 'foo.png'))
-      ..createSync(recursive: true)
-      ..writeAsBytesSync(kTransparentImage);
-
-    when(request.uri).thenReturn(Uri.parse('http://foobar/assets/foo.png'));
-    requestController.add(request);
-    await closeCompleter.future;
-
-    verify(headers.add('Content-Length', source.lengthSync())).called(1);
-    verify(headers.add('Content-Type', 'image/png')).called(1);
-    verify(response.addStream(any)).called(1);
-  }, overrides: <Type,  Generator>{
-    Platform: () => windows,
-  }));
-
 
   test('serves asset files files from in filesystem with unknown mime type and length > 12', () => testbed.run(() async {
     final File source = globals.fs.file(globals.fs.path.join('build', 'flutter_assets', 'foo'))
