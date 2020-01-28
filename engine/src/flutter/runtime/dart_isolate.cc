a801e2264fc2f48646d3a3bc2f15a3416e6dc87b
Chinmay Garde <chinmaygarde@google.com>
2020-01-27 18:31:00 -0800

    Disable setting a library tag handler. (flutter/engine#16086)
    This is only used in the command line VM. The only conceivable use of this would
    be with IsolateMirror.loadURI which is not supported in Flutter or used in known
    embedder API implementations (even ones that use mirrors).

--- engine/src/flutter/runtime/dart_isolate.cc
+++ engine/src/flutter/runtime/dart_isolate.cc
@@ -184,11 +184,6 @@ bool DartIsolate::Initialize(Dart_Isolate dart_isolate) {
 
   SetMessageHandlingTaskRunner(GetTaskRunners().GetUITaskRunner());
 
-  if (tonic::LogIfError(
-          Dart_SetLibraryTagHandler(tonic::DartState::HandleLibraryTag))) {
-    return false;
-  }
-
   if (!UpdateThreadPoolNames()) {
     return false;
   }
@@ -623,10 +618,9 @@ Dart_Isolate DartIsolate::DartCreateAndStartServiceIsolate(
 
   tonic::DartState::Scope scope(service_isolate);
   if (!DartServiceIsolate::Startup(
-          settings.observatory_host,           // server IP address
-          settings.observatory_port,           // server observatory port
-          tonic::DartState::HandleLibraryTag,  // embedder library tag handler
-          false,  //  disable websocket origin check
+          settings.observatory_host,  // server IP address
+          settings.observatory_port,  // server observatory port
+          false,                      //  disable websocket origin check
           settings.disable_service_auth_codes,  // disable VM service auth codes
           error                                 // error (out)
           )) {
