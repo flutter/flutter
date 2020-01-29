1a5992f15831e33b8efc24a55fc25f331dc51a90
Tianguang Zhang <penn.zht@gmail.com>
2020-01-29 17:45:50 +0100

    Removed unnecessary TODO. No need to unify these 2 functions.

--- packages/flutter/lib/src/material/floating_action_button_location.dart
+++ packages/flutter/lib/src/material/floating_action_button_location.dart
@@ -291,7 +291,6 @@ double _topOffsetY(ScaffoldPrelayoutGeometry scaffoldGeometry, { double offset =
   return scaffoldGeometry.contentTop - fabHalfHeight;
 }
 
-// TODO: Unify _floatingOffsetY and _dockedOffsetY.
 // TODO: Use same documentation.
 
 double _floatingOffsetY(ScaffoldPrelayoutGeometry scaffoldGeometry, { double offset = 0.0 }) {
