e717f377aad9bc8837a3ef06ee9df5cb60e438e2
Justin McCandless <jmccandless@google.com>
2020-01-29 14:23:18 -0800

    Correct imports in text test utils

--- packages/flutter/test/material/text.dart
+++ packages/flutter/test/material/text.dart
@@ -2,6 +2,11 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
+
 // Returns the first RenderEditable.
 RenderEditable findRenderEditable(WidgetTester tester) {
   final RenderObject root = tester.renderObject(find.byType(EditableText));
