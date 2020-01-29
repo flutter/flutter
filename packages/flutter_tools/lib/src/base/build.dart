cb3b7ab48743fbf37cb09d7bca5f09b2c17bf806
Jonah Williams <jonahwilliams@google.com>
2020-01-29 13:43:23 -0800

    [flutter_tools] simplify symbol logic for iOS (#49628)

--- packages/flutter_tools/lib/src/base/build.dart
+++ packages/flutter_tools/lib/src/base/build.dart
@@ -138,11 +138,7 @@ class AOTSnapshotter {
       outputPaths.add(assembly);
       genSnapshotArgs.add('--snapshot_kind=app-aot-assembly');
       genSnapshotArgs.add('--assembly=$assembly');
-      // TODO(jonahwilliams): determine the correct time to use strip
-      // since this is required for the future dwarf strack traces option.
-      if (bitcode && buildMode == BuildMode.release) {
-        genSnapshotArgs.add('--strip');
-      }
+      genSnapshotArgs.add('--strip');
     } else {
       final String aotSharedLibrary = globals.fs.path.join(outputDir.path, 'app.so');
       outputPaths.add(aotSharedLibrary);
