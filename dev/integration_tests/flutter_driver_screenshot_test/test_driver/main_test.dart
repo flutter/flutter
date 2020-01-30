4223e89c572c6859e1ba16dbc8818fada333a099
Emmanuel Garcia <egarciad@google.com>
2020-01-29 17:50:42 -0800

    Refactor code and put types back in WebGoldenComparator

--- dev/integration_tests/flutter_driver_screenshot_test/test_driver/main_test.dart
+++ dev/integration_tests/flutter_driver_screenshot_test/test_driver/main_test.dart
@@ -5,7 +5,7 @@
 import 'dart:async';
 import 'package:flutter_driver/flutter_driver.dart';
 import 'package:test/test.dart';
-import 'package:flutter_test/src/buffer_golden_matcher.dart';
+import 'package:flutter_test/src/buffer_matcher.dart';
 
 Future<void> main() async {
   FlutterDriver driver;
