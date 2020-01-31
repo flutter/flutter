47b74faac65ed37cbcbd631b83e755c9fa17c62e
Tianguang Zhang <penn.zht@gmail.com>
2020-01-31 21:23:54 +0100

    Finish refactoring.

--- packages/flutter/lib/src/material/floating_action_button_location.dart
+++ packages/flutter/lib/src/material/floating_action_button_location.dart
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// TODO: This file needs refactoring.
-
 import 'dart:math' as math;
 
 import 'package:flutter/foundation.dart';
