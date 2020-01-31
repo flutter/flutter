1eb5bb653c64611777902a26bf62d9672ac84349
Emmanuel Garcia <egarciad@google.com>
2020-01-31 12:14:08 -0800

    Revert "Use skia golden files in driver test (#49750)" (#49900)

--- dev/devicelab/lib/tasks/integration_tests.dart
+++ dev/devicelab/lib/tasks/integration_tests.dart
@@ -107,22 +107,15 @@ TaskFunction createAndroidSplashScreenKitchenSinkTest() {
   );
 }
 
-/// Executes a driver test that takes a screenshot and compares it against a golden image.
-/// If [useFlutterGold] is true, the golden image is served by Flutter Gold
-/// (https://flutter-gold.skia.org/), otherwise the golden image is read from the disk.
-TaskFunction createFlutterDriverScreenshotTest({
-  bool useFlutterGold = false,
-}) {
+TaskFunction createFlutterDriverScreenshotTest() {
   return DriverTest(
     '${flutterDirectory.path}/dev/integration_tests/flutter_driver_screenshot_test',
     'lib/main.dart',
-    extraOptions: useFlutterGold ? const <String>[
-      '--driver', 'test_driver/flutter_gold_main_test.dart'
-    ] : null
   );
 }
 
 class DriverTest {
+
   DriverTest(
     this.testDirectory,
     this.testTarget, {
