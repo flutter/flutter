f2ade05de2c24592de84c1520a068b93ecba2507
Dan Field <dnfield@google.com>
2020-01-31 23:31:27 -0800

    Revert "Modifies accessibility bridge to populate new node fields in semantic… (#15116)" (#16323)
    This reverts commit 4a55b4fd2500bf96de93c4feb4172d88b93d52c3.

--- shell/platform/fuchsia/flutter/accessibility_bridge.cc
+++ shell/platform/fuchsia/flutter/accessibility_bridge.cc
@@ -80,38 +80,11 @@ AccessibilityBridge::GetNodeAttributes(const flutter::SemanticsNode& node,
 }
 
 fuchsia::accessibility::semantics::States AccessibilityBridge::GetNodeStates(
-    const flutter::SemanticsNode& node,
-    size_t* additional_size) const {
+    const flutter::SemanticsNode& node) const {
   fuchsia::accessibility::semantics::States states;
-  (*additional_size) += sizeof(fuchsia::accessibility::semantics::States);
-
-  // Set checked state.
-  if (!node.HasFlag(flutter::SemanticsFlags::kHasCheckedState)) {
-    states.set_checked_state(
-        fuchsia::accessibility::semantics::CheckedState::NONE);
-  } else {
-    states.set_checked_state(
-        node.HasFlag(flutter::SemanticsFlags::kIsChecked)
-            ? fuchsia::accessibility::semantics::CheckedState::CHECKED
-            : fuchsia::accessibility::semantics::CheckedState::UNCHECKED);
+  if (node.HasFlag(flutter::SemanticsFlags::kHasCheckedState)) {
+    states.set_checked(node.HasFlag(flutter::SemanticsFlags::kIsChecked));
   }
-
-  // Set selected state.
-  states.set_selected(node.HasFlag(flutter::SemanticsFlags::kIsSelected));
-
-  // Set hidden state.
-  states.set_hidden(node.HasFlag(flutter::SemanticsFlags::kIsHidden));
-
-  // Set value.
-  if (node.value.size() > fuchsia::accessibility::semantics::MAX_VALUE_SIZE) {
-    states.set_value(node.value.substr(
-        0, fuchsia::accessibility::semantics::MAX_VALUE_SIZE));
-    (*additional_size) += fuchsia::accessibility::semantics::MAX_VALUE_SIZE;
-  } else {
-    states.set_value(node.value);
-    (*additional_size) += node.value.size();
-  }
-
   return states;
 }
 
@@ -226,7 +199,7 @@ void AccessibilityBridge::AddSemanticsNodeUpdate(
         .set_location(GetNodeLocation(flutter_node))
         .set_transform(GetNodeTransform(flutter_node))
         .set_attributes(GetNodeAttributes(flutter_node, &this_node_size))
-        .set_states(GetNodeStates(flutter_node, &this_node_size))
+        .set_states(GetNodeStates(flutter_node))
         .set_child_ids(child_ids);
     this_node_size +=
         kNodeIdSize * flutter_node.childrenInTraversalOrder.size();
