ab6af0148e9f07e0394df565b58d92d6a2defc4f
Jonah Williams <jonahwilliams@google.com>
2020-02-04 15:02:55 -0800

    Revert "[flutter_tools] Migrate xcode_backend.sh to flutter assemble (#49842)" (#50151)
    This reverts commit c2353b2911c3a39b1cba45790da26b3680ff52cd.

--- packages/flutter_tools/lib/src/aot.dart
+++ packages/flutter_tools/lib/src/aot.dart
@@ -12,9 +12,14 @@ import 'base/io.dart';
 import 'base/logger.dart';
 import 'base/process.dart';
 import 'build_info.dart';
+import 'build_system/build_system.dart';
+import 'build_system/targets/dart.dart';
+import 'build_system/targets/ios.dart';
+import 'cache.dart';
 import 'dart/package_map.dart';
 import 'globals.dart' as globals;
 import 'ios/bitcode.dart';
+import 'project.dart';
 
 /// Builds AOT snapshots given a platform, build mode and a path to a Dart
 /// library.
@@ -34,6 +39,21 @@ class AotBuilder {
   }) async {
     if (platform == null) {
       throwToolExit('No AOT build platform specified');
+    }
+
+    if (_canUseAssemble(platform)
+        && extraGenSnapshotOptions?.isEmpty != false
+        && extraFrontEndOptions?.isEmpty != false) {
+      await _buildWithAssemble(
+        targetFile: mainDartFile,
+        outputDir: outputPath,
+        targetPlatform: platform,
+        buildMode: buildMode,
+        quiet: quiet,
+        iosArchs: iosBuildArchs ?? defaultIOSArchs,
+        bitcode: bitcode ?? kBitcodeEnabledDefault,
+      );
+      return;
     }
 
     if (bitcode) {
@@ -152,5 +172,82 @@ class AotBuilder {
       globals.printStatus(builtMessage);
     }
     return;
+  }
+
+  bool _canUseAssemble(TargetPlatform targetPlatform) {
+    switch (targetPlatform) {
+      case TargetPlatform.ios:
+        return true;
+      case TargetPlatform.android_arm:
+      case TargetPlatform.android_arm64:
+      case TargetPlatform.android_x86:
+      case TargetPlatform.darwin_x64:
+      case TargetPlatform.android_x64:
+      case TargetPlatform.linux_x64:
+      case TargetPlatform.windows_x64:
+      case TargetPlatform.fuchsia_arm64:
+      case TargetPlatform.fuchsia_x64:
+      case TargetPlatform.tester:
+      case TargetPlatform.web_javascript:
+      default:
+        return false;
+    }
+  }
+
+  Future<void> _buildWithAssemble({
+    TargetPlatform targetPlatform,
+    BuildMode buildMode,
+    String targetFile,
+    String outputDir,
+    bool quiet,
+    Iterable<DarwinArch> iosArchs,
+    bool bitcode,
+  }) async {
+    Status status;
+    if (!quiet) {
+      final String typeName = globals.artifacts.getEngineType(targetPlatform, buildMode);
+      status = globals.logger.startProgress(
+        'Building AOT snapshot in ${getFriendlyModeName(buildMode)} mode ($typeName)...',
+        timeout: timeoutConfiguration.slowOperation,
+      );
+    }
+    final FlutterProject flutterProject = FlutterProject.current();
+    final Target target = buildMode == BuildMode.profile
+      ? const AotAssemblyProfile()
+      : const AotAssemblyRelease();
+
+    final BuildResult result = await buildSystem.build(target, Environment(
+      projectDir: flutterProject.directory,
+      cacheDir: globals.cache.getRoot(),
+      flutterRootDir: globals.fs.directory(Cache.flutterRoot),
+      outputDir: globals.fs.directory(outputDir),
+      buildDir: flutterProject.directory
+        .childDirectory('.dart_tool')
+        .childDirectory('flutter_build'),
+      defines: <String, String>{
+        kBuildMode: getNameForBuildMode(buildMode),
+        kTargetPlatform: getNameForTargetPlatform(targetPlatform),
+        kTargetFile: targetFile,
+        kIosArchs: iosArchs.map(getNameForDarwinArch).join(','),
+        kBitcodeFlag: bitcode.toString()
+      }
+    ));
+    status?.stop();
+    if (!result.success) {
+      for (final ExceptionMeasurement measurement in result.exceptions.values) {
+        globals.printError('Target ${measurement.target} failed: ${measurement.exception}',
+          stackTrace: measurement.fatal
+            ? measurement.stackTrace
+            : null,
+        );
+      }
+      throwToolExit('Failed to build aot.');
+    }
+    final String builtMessage = 'Built to $outputDir${globals.fs.path.separator}.';
+    if (quiet) {
+      globals.printTrace(builtMessage);
+    } else {
+      globals.printStatus(builtMessage);
+    }
   }
 }
