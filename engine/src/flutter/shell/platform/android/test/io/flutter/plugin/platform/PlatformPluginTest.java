672995657d8b0c10802a621e727f5aa6c6f85df9
Matt Carroll <matthew-carroll@users.noreply.github.com>
2020-02-03 16:25:27 -0800

    Added API guards to flags in PlatformPlugin that were introduced after Flutter's min SDK version. (flutter/engine#13965)

---
+++ engine/src/flutter/shell/platform/android/test/io/flutter/plugin/platform/PlatformPluginTest.java
@@ -0,0 +1,37 @@
+package io.flutter.plugin.platform;
+
+import android.app.Activity;
+import android.view.View;
+import android.view.Window;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+import io.flutter.embedding.engine.systemchannels.PlatformChannel;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@Config(manifest=Config.NONE)
+@RunWith(RobolectricTestRunner.class)
+public class PlatformPluginTest {
+  @Config(sdk = 16)
+  @Test
+  public void itIgnoresNewHapticEventsOnOldAndroidPlatforms() {
+    View fakeDecorView = mock(View.class);
+    Window fakeWindow = mock(Window.class);
+    when(fakeWindow.getDecorView()).thenReturn(fakeDecorView);
+    Activity fakeActivity = mock(Activity.class);
+    when(fakeActivity.getWindow()).thenReturn(fakeWindow);
+    PlatformChannel fakePlatformChannel = mock(PlatformChannel.class);
+    PlatformPlugin platformPlugin = new PlatformPlugin(fakeActivity, fakePlatformChannel);
+
+    // HEAVY_IMPACT haptic response is only available on "M" (23) and later.
+    platformPlugin.vibrateHapticFeedback(PlatformChannel.HapticFeedbackType.HEAVY_IMPACT);
+
+    // SELECTION_CLICK haptic response is only available on "LOLLIPOP" (21) and later.
+    platformPlugin.vibrateHapticFeedback(PlatformChannel.HapticFeedbackType.SELECTION_CLICK);
+  }
+}
