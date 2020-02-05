aebc0094bb1923898d0f32b857794403d191a0ce
Yegor Jbanov <yjbanov@google.com>
2020-02-04 20:19:40 -0800

    Web benchmarks: make headless mode opt-out

--- dev/devicelab/lib/tasks/web_benchmarks.dart
+++ dev/devicelab/lib/tasks/web_benchmarks.dart
@@ -99,8 +99,13 @@ Future<TaskResult> runWebBenchmark({ @required bool useCanvasKit }) async {
         '--disable-translate',
       ];
 
-      final bool isUncalibratedSmokeTest =
-          io.Platform.environment['UNCALIBRATED_SMOKE_TEST'] == 'true';
+      // TODO(yjbanov): temporarily disables headful Chrome until we get
+      //                devicelab hardware that is able to run it. Our current
+      //                GCE VMs can only run in headless mode.
+      //                See: https://github.com/flutter/flutter/issues/50164
+      final bool isUncalibratedSmokeTest = io.Platform.environment['CALIBRATED'] != 'true';
+      // final bool isUncalibratedSmokeTest =
+      //     io.Platform.environment['UNCALIBRATED_SMOKE_TEST'] == 'true';
       if (isUncalibratedSmokeTest) {
         print('Running in headless mode because running on uncalibrated hardware.');
         args.add('--headless');
