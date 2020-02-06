76b291a65973685bb9d92a99810e27b3de9afc6c
gaaclarke <30870216+gaaclarke@users.noreply.github.com>
2020-02-05 16:12:16 -0800

    Added a plugin method that gets called when the engine is about to be deleted (#16336)

--- shell/platform/darwin/ios/framework/Source/FlutterEngine.mm
+++ shell/platform/darwin/ios/framework/Source/FlutterEngine.mm
@@ -29,17 +29,19 @@
 
 NSString* const FlutterDefaultDartEntrypoint = nil;
 
+@interface FlutterEngineRegistrar : NSObject <FlutterPluginRegistrar>
+@property(nonatomic, assign) FlutterEngine* flutterEngine;
+- (instancetype)initWithPlugin:(NSString*)pluginKey flutterEngine:(FlutterEngine*)flutterEngine;
+@end
+
 @interface FlutterEngine () <FlutterTextInputDelegate, FlutterBinaryMessenger>
 // Maintains a dictionary of plugin names that have registered with the engine.  Used by
 // FlutterEngineRegistrar to implement a FlutterPluginRegistrar.
 @property(nonatomic, readonly) NSMutableDictionary* pluginPublications;
+@property(nonatomic, readonly) NSMutableDictionary<NSString*, FlutterEngineRegistrar*>* registrars;
 
 @property(nonatomic, readwrite, copy) NSString* isolateId;
 @property(nonatomic, retain) id<NSObject> flutterViewControllerWillDeallocObserver;
-@end
-
-@interface FlutterEngineRegistrar : NSObject <FlutterPluginRegistrar>
-- (instancetype)initWithPlugin:(NSString*)pluginKey flutterEngine:(FlutterEngine*)flutterEngine;
 @end
 
 @implementation FlutterEngine {
@@ -98,6 +100,7 @@ NSString* const FlutterDefaultDartEntrypoint = nil;
     _dartProject.reset([project retain]);
 
   _pluginPublications = [NSMutableDictionary new];
+  _registrars = [[NSMutableDictionary alloc] init];
   _platformViewsController.reset(new flutter::FlutterPlatformViewsController());
 
   _binaryMessenger = [[FlutterBinaryMessengerRelay alloc] initWithParent:self];
@@ -122,8 +125,24 @@ NSString* const FlutterDefaultDartEntrypoint = nil;
 }
 
 - (void)dealloc {
+  /// Notify plugins of dealloc.  This should happen first in dealloc since the
+  /// plugins may be talking to things like the binaryMessenger.
+  [_pluginPublications enumerateKeysAndObjectsUsingBlock:^(id key, id object, BOOL* stop) {
+    if ([object respondsToSelector:@selector(detachFromEngineForRegistrar:)]) {
+      NSObject<FlutterPluginRegistrar>* registrar = self.registrars[key];
+      [object detachFromEngineForRegistrar:registrar];
+    }
+  }];
+
+  /// nil out weak references.
+  [_registrars
+      enumerateKeysAndObjectsUsingBlock:^(id key, FlutterEngineRegistrar* registrar, BOOL* stop) {
+        registrar.flutterEngine = nil;
+      }];
+
   [_labelPrefix release];
   [_pluginPublications release];
+  [_registrars release];
   _binaryMessenger.parent = nil;
   [_binaryMessenger release];
 
@@ -647,7 +666,10 @@ NSString* const FlutterDefaultDartEntrypoint = nil;
 - (NSObject<FlutterPluginRegistrar>*)registrarForPlugin:(NSString*)pluginKey {
   NSAssert(self.pluginPublications[pluginKey] == nil, @"Duplicate plugin key: %@", pluginKey);
   self.pluginPublications[pluginKey] = [NSNull null];
-  return [[[FlutterEngineRegistrar alloc] initWithPlugin:pluginKey flutterEngine:self] autorelease];
+  FlutterEngineRegistrar* result = [[FlutterEngineRegistrar alloc] initWithPlugin:pluginKey
+                                                                    flutterEngine:self];
+  self.registrars[pluginKey] = result;
+  return [result autorelease];
 }
 
 - (BOOL)hasPlugin:(NSString*)pluginKey {
@@ -686,20 +708,18 @@ NSString* const FlutterDefaultDartEntrypoint = nil;
 
 @implementation FlutterEngineRegistrar {
   NSString* _pluginKey;
-  FlutterEngine* _flutterEngine;
 }
 
 - (instancetype)initWithPlugin:(NSString*)pluginKey flutterEngine:(FlutterEngine*)flutterEngine {
   self = [super init];
   NSAssert(self, @"Super init cannot be nil");
-  _pluginKey = [pluginKey retain];
-  _flutterEngine = [flutterEngine retain];
+  _pluginKey = [pluginKey copy];
+  _flutterEngine = flutterEngine;
   return self;
 }
 
 - (void)dealloc {
   [_pluginKey release];
-  [_flutterEngine release];
   [super dealloc];
 }
 
