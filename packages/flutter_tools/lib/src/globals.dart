487d557aa6a110b0090f32f78a498534c57f9173
Jenn Magder <magder@google.com>
2020-02-06 11:38:47 -0800

    Reland Replace ideviceinfo and idevice_id with xcdevice (#50252)

--- packages/flutter_tools/lib/src/globals.dart
+++ packages/flutter_tools/lib/src/globals.dart
@@ -57,6 +57,8 @@ Xcode get xcode => context.get<Xcode>();
 FlutterVersion get flutterVersion => context.get<FlutterVersion>();
 IMobileDevice get iMobileDevice => context.get<IMobileDevice>();
 
+XCDevice get xcdevice => context.get<XCDevice>();
+
 /// Display an error level message to the user. Commands should use this if they
 /// fail in some way.
 ///
