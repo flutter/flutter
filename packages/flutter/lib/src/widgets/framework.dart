e300c1a45aa042387165de175bc407561ec2bc75
creativecreatorormaybenot <19204050+creativecreatorormaybenot@users.noreply.github.com>
2020-02-06 01:43:02 +0000

    Docs: provide Guidance for SingleChildRenderObjectWidget implementations (#49993)

--- packages/flutter/lib/src/widgets/framework.dart
+++ packages/flutter/lib/src/widgets/framework.dart
@@ -1782,9 +1782,14 @@ abstract class LeafRenderObjectWidget extends RenderObjectWidget {
   LeafRenderObjectElement createElement() => LeafRenderObjectElement(this);
 }
 
-/// A superclass for RenderObjectWidgets that configure RenderObject subclasses
+/// A superclass for [RenderObjectWidget]s that configure [RenderObject] subclasses
 /// that have a single child slot. (This superclass only provides the storage
 /// for that child, it doesn't actually provide the updating logic.)
+///
+/// Typically, the render object assigned to this widget will make use of
+/// [RenderObjectWithChildMixin] to implement a single-child model. The mixin
+/// exposes a [RenderObjectWithChildMixin.child] property that allows
+/// retrieving the render object belonging to the [child] widget.
 abstract class SingleChildRenderObjectWidget extends RenderObjectWidget {
   /// Abstract const constructor. This constructor enables subclasses to provide
   /// const constructors so that they can be used in const expressions.
