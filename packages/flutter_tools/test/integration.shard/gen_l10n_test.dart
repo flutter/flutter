4402569343d2797d8a9325e3e396cdedf427e1d8
Shi Hao Hong <shihaohong@google.com>
2020-02-06 15:42:37 -0800

    Implement locale resolution integration test

--- packages/flutter_tools/test/integration.shard/gen_l10n_test.dart
+++ packages/flutter_tools/test/integration.shard/gen_l10n_test.dart
@@ -23,13 +23,10 @@ import 'test_utils.dart';
 // loaded workstation, so the test could time out on a heavily loaded bot.
 void main() {
   Directory tempDir;
-  final GenL10nProject _project = GenL10nProject();
   FlutterRunTestDriver _flutter;
 
   setUp(() async {
     tempDir = createResolvedTempDirectorySync('gen_l10n_test.');
-    await _project.setUpIn(tempDir);
-    _flutter = FlutterRunTestDriver(tempDir);
   });
 
   tearDown(() async {
@@ -48,7 +45,69 @@ void main() {
     }
   }
 
-  test('generated l10n classes produce expected localized strings', () async {
+  // test('generated l10n classes produce expected localized strings', () async {
+  //   final GenL10nProject _project = GenL10nProject();
+  //   await _project.setUpIn(tempDir);
+  //   _flutter = FlutterRunTestDriver(tempDir);
+
+  //   // Get the intl packages before running gen_l10n.
+  //   final String flutterBin = globals.platform.isWindows ? 'flutter.bat' : 'flutter';
+  //   final String flutterPath = globals.fs.path.join(getFlutterRoot(), 'bin', flutterBin);
+  //   runCommand(<String>[flutterPath, 'pub', 'get']);
+
+  //   // Generate lib/l10n/app_localizations.dart
+  //   final String genL10nPath = globals.fs.path.join(getFlutterRoot(), 'dev', 'tools', 'localization', 'bin', 'gen_l10n.dart');
+  //   final String dartBin = globals.platform.isWindows ? 'dart.exe' : 'dart';
+  //   final String dartPath = globals.fs.path.join(getFlutterRoot(), 'bin', 'cache', 'dart-sdk', 'bin', dartBin);
+  //   runCommand(<String>[dartPath, genL10nPath]);
+
+  //   // Run the app defined in GenL10nProject.main and wait for it to
+  //   // send '#l10n END' to its stdout.
+  //   final Completer<void> l10nEnd = Completer<void>();
+  //   final StringBuffer stdout = StringBuffer();
+  //   final StreamSubscription<String> subscription = _flutter.stdout.listen((String line) {
+  //     if (line.contains('#l10n')) {
+  //       stdout.writeln(line.substring(line.indexOf('#l10n')));
+  //     }
+  //     if (line.contains('#l10n END')) {
+  //       l10nEnd.complete();
+  //     }
+  //   });
+  //   await _flutter.run();
+  //   await l10nEnd.future;
+  //   await subscription.cancel();
+  //   expect(stdout.toString(),
+  //     '#l10n 0 (Hello World)\n'
+  //     '#l10n 1 (Hello World)\n'
+  //     '#l10n 2 (Hello World)\n'
+  //     '#l10n 3 (Hello World on Friday, January 1, 1960)\n'
+  //     '#l10n 4 (Hello world argument on 1/1/1960 at 00:00)\n'
+  //     '#l10n 5 (Hello World from 1960 to 2020)\n'
+  //     '#l10n 6 (Hello for 123)\n'
+  //     '#l10n 7 (Hello for price USD123.00)\n'
+  //     '#l10n 8 (Hello)\n'
+  //     '#l10n 9 (Hello World)\n'
+  //     '#l10n 10 (Hello two worlds)\n'
+  //     '#l10n 11 (Hello on Friday, January 1, 1960)\n'
+  //     '#l10n 12 (Hello World, on Friday, January 1, 1960)\n'
+  //     '#l10n 13 (Hello two worlds, on Friday, January 1, 1960)\n'
+  //     '#l10n 14 (Hello)\n'
+  //     '#l10n 15 (Hello new World)\n'
+  //     '#l10n 16 (Hello two new worlds)\n'
+  //     '#l10n 17 (Hello other 0 worlds, with a total of 100 citizens)\n'
+  //     '#l10n 18 (Hello World of 101 citizens)\n'
+  //     '#l10n 19 (Hello two worlds with 102 total citizens)\n'
+  //     '#l10n 20 ([Hello] #World#)\n'
+  //     '#l10n 21 ([Hello] -World- #123#)\n'
+  //     '#l10n END\n'
+  //   );
+  // });
+
+  test('locales with country code specified resolves to general locale if message is undefined', () async {
+    final GenL10nLocaleResolutionProject _project = GenL10nLocaleResolutionProject();
+    await _project.setUpIn(tempDir);
+    _flutter = FlutterRunTestDriver(tempDir);
+
     // Get the intl packages before running gen_l10n.
     final String flutterBin = globals.platform.isWindows ? 'flutter.bat' : 'flutter';
     final String flutterPath = globals.fs.path.join(getFlutterRoot(), 'bin', flutterBin);
@@ -76,28 +135,19 @@ void main() {
     await l10nEnd.future;
     await subscription.cancel();
     expect(stdout.toString(),
-      '#l10n 0 (Hello World)\n'
-      '#l10n 1 (Hello World)\n'
+      // Uses messages in app_en_CA.arb
+      '#l10n 0 (Hello, Canadian World)\n'
+      '#l10n 1 (Hello for price CAD123.00)\n'
+      // Uses messages in app_en.arb, since the app_en_CA.arb
+      // equivalents are untranslated
       '#l10n 2 (Hello World)\n'
-      '#l10n 3 (Hello World on Friday, January 1, 1960)\n'
-      '#l10n 4 (Hello world argument on 1/1/1960 at 00:00)\n'
-      '#l10n 5 (Hello World from 1960 to 2020)\n'
-      '#l10n 6 (Hello for 123)\n'
-      '#l10n 7 (Hello for price USD123.00)\n'
-      '#l10n 8 (Hello)\n'
-      '#l10n 9 (Hello World)\n'
-      '#l10n 10 (Hello two worlds)\n'
-      '#l10n 11 (Hello on Friday, January 1, 1960)\n'
-      '#l10n 12 (Hello World, on Friday, January 1, 1960)\n'
-      '#l10n 13 (Hello two worlds, on Friday, January 1, 1960)\n'
-      '#l10n 14 (Hello)\n'
-      '#l10n 15 (Hello new World)\n'
-      '#l10n 16 (Hello two new worlds)\n'
-      '#l10n 17 (Hello other 0 worlds, with a total of 100 citizens)\n'
-      '#l10n 18 (Hello World of 101 citizens)\n'
-      '#l10n 19 (Hello two worlds with 102 total citizens)\n'
-      '#l10n 20 ([Hello] #World#)\n'
-      '#l10n 21 ([Hello] -World- #123#)\n'
+      '#l10n 3 (Hello World)\n'
+      '#l10n 4 (Hello World on Friday, January 1, 1960)\n'
+      // Date is automatically represented as "1960-01-01" instead of
+      // "01-01-1960" since locale is passed into DateFormat.
+      '#l10n 5 (Hello world argument on 1960-01-01 at 00:00)\n'
+      '#l10n 6 (Hello World from 1960 to 2020)\n'
+      '#l10n 7 (Hello for 123)\n'
       '#l10n END\n'
     );
   });
--- packages/flutter_tools/test/integration.shard/test_data/gen_l10n_project.dart
+++ packages/flutter_tools/test/integration.shard/test_data/gen_l10n_project.dart
@@ -35,7 +35,8 @@ dependencies:
 ''';
 
   @override
-  final String main = r'''
+  String get main => _main;
+  static const String _main = r'''
 import 'package:flutter/material.dart';
 
 import 'l10n/app_localizations.dart';
@@ -242,6 +243,90 @@ void main() {
       "count": {},
       "hello": {},
       "world": {}
+    }
+  }
+}
+''';
+
+
+}
+
+class GenL10nLocaleResolutionProject extends GenL10nProject {
+  @override
+  Future<void> setUpIn(Directory dir) {
+    this.dir = dir;
+    writeFile(globals.fs.path.join(dir.path, 'lib', 'l10n', 'app_en_CA.arb'), appEnCa);
+    return super.setUpIn(dir);
+  }
+
+  @override
+  String get main => _main;
+  static const String _main = r'''
+import 'package:flutter/material.dart';
+
+import 'l10n/app_localizations.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    try {
+      print('get localizations');
+      final AppLocalizations localizations = AppLocalizations.of(context);
+      print('localizations: $localizations');
+      print('localizations.helloWorld');
+      final List<String> results = <String>[
+        '${localizations.helloWorld}',
+        '${localizations.helloCost("price", 123)}',
+        '${localizations.hello("World")}',
+        '${localizations.greeting("Hello", "World")}',
+        '${localizations.helloWorldOn(DateTime(1960))}',
+        '${localizations.helloOn("world argument", DateTime(1960), DateTime(1960))}',
+        '${localizations.helloWorldDuring(DateTime(1960), DateTime(2020))}',
+        '${localizations.helloFor(123)}',
+      ];
+      int n = 0;
+      for (final String result in results) {
+        print('#l10n $n ($result)\n');
+        n += 1;
+      }
+    } finally {
+      print('#l10n END\n');
+    }
+    return Container();
+  }
+}
+
+void main() {
+  runApp(
+    MaterialApp(
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: <Locale>[
+        Locale('en', 'CA'),
+      ],
+      home: Home(),
+    ),
+  );
+}
+''';
+
+  final String appEnCa = r'''
+{
+  "@@locale": "en_CA",
+
+  "helloWorld": "Hello, Canadian World",
+  "@helloWorld": {
+    "description": "The conventional newborn programmer greeting"
+  },
+
+  "helloCost": "Hello for {price} {value}",
+  "@helloCost": {
+    "description": "A message with string and int (currency) parameters",
+    "placeholders": {
+      "price": {},
+      "value": {
+        "type": "int",
+        "format": "currency"
+      }
     }
   }
 }
