181cdec079fef2605f2cb144a0bd871ff2a3da16
Jonah Williams <jonahwilliams@google.com>
2020-02-06 17:05:41 -0800

    [flutter_tools] remove automatic multiroot scheme (#50284)

--- packages/flutter_tools/lib/src/build_system/targets/web.dart
+++ packages/flutter_tools/lib/src/build_system/targets/web.dart
@@ -11,7 +11,6 @@ import '../../build_info.dart';
 import '../../compile.dart';
 import '../../dart/package_map.dart';
 import '../../globals.dart' as globals;
-import '../../project.dart';
 import '../build_system.dart';
 import '../depfile.dart';
 import 'assets.dart';
@@ -156,9 +155,7 @@ class Dart2JSTarget extends Target {
     final bool csp = environment.defines[kCspMode] == 'true';
     final BuildMode buildMode = getBuildModeForName(environment.defines[kBuildMode]);
     final String specPath = globals.fs.path.join(globals.artifacts.getArtifactPath(Artifact.flutterWebSdk), 'libraries.json');
-    final String packageFile = FlutterProject.fromDirectory(environment.projectDir).hasBuilders
-      ? PackageMap.globalGeneratedPackagesPath
-      : PackageMap.globalPackagesPath;
+    final String packageFile = PackageMap.globalPackagesPath;
     final File outputFile = environment.buildDir.childFile('main.dart.js');
 
     final ProcessResult result = await globals.processManager.run(<String>[
