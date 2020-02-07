9d7fbc90971f8a63a8e964ef72df6f599d999213
Jenn Magder <magder@google.com>
2020-02-07 13:57:24 -0800

    Update minimum version to Xcode 11.0 (#50315)

--- packages/flutter_tools/test/src/context.dart
+++ packages/flutter_tools/test/src/context.dart
@@ -366,13 +366,13 @@ class FakeXcodeProjectInterpreter implements XcodeProjectInterpreter {
   bool get isInstalled => true;
 
   @override
-  String get versionText => 'Xcode 10.2';
+  String get versionText => 'Xcode 11.0';
 
   @override
-  int get majorVersion => 10;
+  int get majorVersion => 11;
 
   @override
-  int get minorVersion => 2;
+  int get minorVersion => 0;
 
   @override
   Future<Map<String, String>> getBuildSettings(
