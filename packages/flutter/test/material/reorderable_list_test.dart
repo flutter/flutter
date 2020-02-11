2c3a41c0e3d37af4593c3d71927d8f723c28374f
Lamonte <exts@users.noreply.github.com>
2020-02-11 12:58:02 -0600

    Exposed optional scrollController property in ReorderableListView (#49148)

--- packages/flutter/test/material/reorderable_list_test.dart
+++ packages/flutter/test/material/reorderable_list_test.dart
@@ -263,6 +263,65 @@ void main() {
         expect(scrollView.controller, primary2);
       });
 
+      testWidgets('Test custom ScrollController behavior when set', (WidgetTester tester) async {
+        const Key firstBox = Key('C');
+        const Key secondBox = Key('B');
+        const Key thirdBox = Key('A');
+        final ScrollController customController = ScrollController();
+        await tester.pumpWidget(
+          MaterialApp(
+            home: Scaffold(
+              body: SizedBox(
+                height: 200,
+                child: ReorderableListView(
+                  scrollController: customController,
+                  onReorder: (int oldIndex, int newIndex) { },
+                  children: const <Widget>[
+                    SizedBox(width: 100.0, height: 100.0, child: Text('C'), key: firstBox),
+                    SizedBox(width: 100.0, height: 100.0, child: Text('B'), key: secondBox),
+                    SizedBox(width: 100.0, height: 100.0, child: Text('A'), key: thirdBox),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        );
+
+        // Check initial scroll offset of first list item relative to
+        // the offset of the list view.
+        customController.animateTo(
+          40.0,
+          duration: const Duration(milliseconds: 200),
+          curve: Curves.linear
+        );
+        await tester.pumpAndSettle();
+        Offset listViewTopLeft = tester.getTopLeft(
+          find.byType(ReorderableListView),
+        );
+        Offset firstBoxTopLeft = tester.getTopLeft(
+          find.byKey(firstBox)
+        );
+        expect(firstBoxTopLeft.dy, listViewTopLeft.dy - 40.0);
+
+        // Drag the UI to see if the scroll controller updates accordingly
+        await tester.drag(
+          find.text('B'),
+          const Offset(0.0, -100.0),
+        );
+        listViewTopLeft = tester.getTopLeft(
+          find.byType(ReorderableListView),
+        );
+        firstBoxTopLeft = tester.getTopLeft(
+          find.byKey(firstBox),
+        );
+        // Initial scroll controller offset: 40.0
+        // Drag UI by 100.0 upwards vertically
+        // First 20.0 px always ignored, so scroll offset is only
+        // shifted by 80.0.
+        // Final offset: 40.0 + 80.0 = 120.0
+        expect(customController.offset, 120.0);
+      });
+
       testWidgets('Still builds when no PrimaryScrollController is available', (WidgetTester tester) async {
         final Widget reorderableList = ReorderableListView(
           children: const <Widget>[
