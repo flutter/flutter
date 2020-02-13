2efd7df4ef85c469f256a6b9681b8416b1d99f3c
Stuart Morgan <stuartmorgan@google.com>
2020-02-12 22:37:12 -0800

    Generate Windows plugin link settings

--- packages/flutter_tools/lib/src/plugins.dart
+++ packages/flutter_tools/lib/src/plugins.dart
@@ -19,6 +19,7 @@ import 'globals.dart' as globals;
 import 'macos/cocoapods.dart';
 import 'platform_plugins.dart';
 import 'project.dart';
+import 'windows/property_sheet.dart';
 
 void _renderTemplateToFile(String template, dynamic context, String filePath) {
   final String renderedTemplate =
@@ -852,12 +853,13 @@ Future<void> _writeMacOSPluginRegistrant(FlutterProject project, List<Plugin> pl
   );
 }
 
-Future<void> _writeWindowsPluginRegistrant(FlutterProject project, List<Plugin> plugins) async {
+Future<void> _writeWindowsPluginFiles(FlutterProject project, List<Plugin> plugins) async {
   final List<Map<String, dynamic>> windowsPlugins = _extractPlatformMaps(plugins, WindowsPlugin.kConfigKey);
   final Map<String, dynamic> context = <String, dynamic>{
     'plugins': windowsPlugins,
   };
   await _writeCppPluginRegistrant(project.windows.managedDirectory, context);
+  await _writeWindowsPluginProperties(project.windows.generatedPluginPropertySheetFile, windowsPlugins);
 }
 
 Future<void> _writeCppPluginRegistrant(Directory destination, Map<String, dynamic> templateContext) async {
@@ -872,6 +874,12 @@ Future<void> _writeCppPluginRegistrant(Directory destination, Map<String, dynami
     templateContext,
     globals.fs.path.join(registryDirectory, 'generated_plugin_registrant.cc'),
   );
+}
+
+Future<void> _writeWindowsPluginProperties(File propertyFile, List<Map<String, dynamic>> windowsPlugins) async {
+  final List<String> pluginLibraryFilenames = windowsPlugins.map(
+    (Map<String, dynamic> plugin) => '${plugin['name']}_plugin.lib').toList();
+  propertyFile.writeAsStringSync(PropertySheet(libraryDependencies: pluginLibraryFilenames).toString());
 }
 
 Future<void> _writeWebPluginRegistrant(FlutterProject project, List<Plugin> plugins) async {
@@ -1013,7 +1021,7 @@ Future<void> injectPlugins(FlutterProject project, {bool checkProjects = false})
     await _writeMacOSPluginRegistrant(project, plugins);
   }
   if (featureFlags.isWindowsEnabled && project.windows.existsSync()) {
-    await _writeWindowsPluginRegistrant(project, plugins);
+    await _writeWindowsPluginFiles(project, plugins);
   }
   for (final XcodeBasedProject subproject in <XcodeBasedProject>[project.ios, project.macos]) {
     if (!project.isModule && (!checkProjects || subproject.existsSync())) {
---
+++ packages/flutter_tools/lib/src/windows/property_sheet.dart
@@ -0,0 +1,99 @@
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:xml/xml.dart' as xml;
+
+/// A utility class for building property sheet (.props) files for use
+/// with MSBuild/Visual Studio projects.
+class PropertySheet {
+  /// Creates a PropertySheet with the given properties.
+  const PropertySheet({this.libraryDependencies, this.environmentVariables});
+
+  /// Libraries to link against.
+  final List<String> libraryDependencies;
+
+  /// Variables to make available both as build macros and as environment
+  /// variables for script steps.
+  final Map<String, String> environmentVariables;
+
+  @override
+  String toString() {
+    // See https://docs.microsoft.com/en-us/cpp/build/reference/vcxproj-file-structure#property-sheet-layout
+
+    final xml.XmlBuilder builder = xml.XmlBuilder();
+    builder.processing('xml', 'version="1.0" encoding="utf-8"');
+    builder.element('Project', nest: () {
+      builder.attribute('ToolsVersion', '4.0');
+      builder.attribute(
+          'xmlns', 'http://schemas.microsoft.com/developer/msbuild/2003');
+
+      builder.element('ImportGroup', nest: () {
+        builder.attribute('Label', 'PropertySheets');
+      });
+      builder.element('PropertyGroup', nest: () {
+        builder.attribute('Label', 'UserMacros');
+
+        _addEnviromentVariableUserMacros(builder);
+      });
+      builder.element('PropertyGroup');
+      builder.element('ItemDefinitionGroup', nest: () {
+        _addLibraryDependencies(builder);
+      });
+      builder.element('ItemGroup', nest: () {
+        _addEnvironmentVariableBuildMacros(builder);
+      });
+    });
+
+    return builder.build().toXmlString(pretty: true, indent: '  ');
+  }
+
+  /// Writes key/value pairs for any environment variables as user macros.
+  ///
+  /// Must be called within the context of the ItemDefinitionGroup.
+  void _addLibraryDependencies(xml.XmlBuilder builder) {
+    if (libraryDependencies == null || libraryDependencies.isEmpty) {
+      return;
+    }
+    builder.element('Link', nest: () {
+      builder.element('AdditionalDependencies', nest: () {
+        builder.text('${libraryDependencies.join(';')};%(AdditionalDependencies)');
+      });
+    });
+  }
+
+  /// Writes key/value pairs for any environment variables as user macros.
+  ///
+  /// Must be called within the context of the UserMacros PropertyGroup.
+  void _addEnviromentVariableUserMacros(xml.XmlBuilder builder) {
+    if (environmentVariables == null) {
+      return;
+    }
+    for (final MapEntry<String, String> variable in environmentVariables.entries) {
+      builder.element(variable.key, nest: () {
+        builder.text(variable.value);
+      });
+    }
+  }
+
+  /// Writes the BuildMacros to expose environment variable UserMacros to the
+  /// environment.
+  ///
+  /// Must be called within the context of the ItemGroup.
+  void _addEnvironmentVariableBuildMacros(xml.XmlBuilder builder) {
+    if (environmentVariables == null) {
+      return;
+    }
+    for (final String name in environmentVariables.keys) {
+      builder.element('BuildMacro', nest: () {
+        builder.attribute('Include', name);
+        builder.element('Value', nest: () {
+          builder.text('\$($name)');
+        });
+        builder.element('EnvironmentVariable', nest: () {
+          builder.text('true');
+        });
+      });
+    }
+  }
+}
