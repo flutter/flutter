e1b4456a34b38844bbbc91a17c2f83506f9889c6
Inconnu08 <taufiqrx8@gmail.com>
2020-02-14 02:35:52 +0600

    Feature: allow minLines in SelectableText

--- packages/flutter/test/widgets/selectable_text_test.dart
+++ packages/flutter/test/widgets/selectable_text_test.dart
@@ -177,12 +177,13 @@ void main() {
     debugResetSemanticsIdCounter();
   });
 
-  Widget selectableTextBuilder({String text = '', int maxLines = 1}) {
+  Widget selectableTextBuilder({String text = '', int maxLines = 1, int minLines}) {
     return boilerplate(
       child: SelectableText(
         text,
         style: const TextStyle(color: Colors.black, fontSize: 34.0),
         maxLines: maxLines,
+        minLines: minLines
       ),
     );
   }
@@ -1153,6 +1154,39 @@ void main() {
     expect(newFirstPos.dy, firstPos.dy);
     expect(inputBox.hitTest(BoxHitTestResult(), position: inputBox.globalToLocal(newFirstPos)), isTrue);
     expect(inputBox.hitTest(BoxHitTestResult(), position: inputBox.globalToLocal(newFourthPos)), isFalse);
+  });
+
+  testWidgets('minLines cannot be greater than maxLines', (WidgetTester tester) async {
+    try {
+      await tester.pumpWidget(
+        overlay(
+          child: Container(
+            width: 300.0,
+            child: SelectableText(
+              'abcd',
+              minLines: 4,
+              maxLines: 3,
+            ),
+          ),
+        ),
+      );
+    } on AssertionError catch (e) {
+      expect(e.toString(), contains("minLines can't be greater than maxLines"));
+    }
+  });
+
+  testWidgets('Selectable height with minLine', (WidgetTester tester) async {
+    await tester.pumpWidget(selectableTextBuilder());
+
+    RenderBox findTextBox() => tester.renderObject(find.byType(SelectableText));
+
+    final RenderBox textBox = findTextBox();
+    final Size emptyInputSize = textBox.size;
+
+    // Even if the text is a one liner, minimum height of SelectableText will determined by minLines
+    await tester.pumpWidget(selectableTextBuilder(text: 'No wrapping here.', minLines: 2, maxLines: 3));
+    expect(findTextBox(), equals(textBox));
+    expect(textBox.size.height, emptyInputSize.height * 2);
   });
 
   testWidgets('Can align to center', (WidgetTester tester) async {
