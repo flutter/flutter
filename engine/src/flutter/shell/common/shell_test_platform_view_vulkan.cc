f99b991c2b15104fad7b195edd8f05174078407d
George Wright <gw280@google.com>
2020-02-14 13:02:38 -0800

    Revert "[shell tests] Integrate Vulkan with Shell Tests"
    This reverts commit 57a8e04df9bd24b313ca4c309e7c8ba904559ea1.

--- engine/src/flutter/shell/common/shell_test_platform_view_vulkan.cc
+++ engine/src/flutter/shell/common/shell_test_platform_view_vulkan.cc
@@ -3,6 +3,7 @@
 // found in the LICENSE file.
 
 #include "flutter/shell/common/shell_test_platform_view_vulkan.h"
+#include "flutter/shell/gpu/gpu_surface_vulkan.h"
 
 namespace flutter {
 namespace testing {
@@ -29,7 +30,7 @@ void ShellTestPlatformViewVulkan::SimulateVSync() {
 
 // |PlatformView|
 std::unique_ptr<Surface> ShellTestPlatformViewVulkan::CreateRenderingSurface() {
-  return std::make_unique<OffScreenSurface>(proc_table_);
+  return std::make_unique<GPUSurfaceVulkan>(this, nullptr, true);
 }
 
 // |PlatformView|
@@ -39,128 +40,9 @@ PointerDataDispatcherMaker ShellTestPlatformViewVulkan::GetDispatcherMaker() {
   };
 }
 
-ShellTestPlatformViewVulkan::OffScreenSurface::OffScreenSurface(
-    fml::RefPtr<vulkan::VulkanProcTable> vk)
-    : valid_(false), vk_(std::move(vk)) {
-  if (!vk_ || !vk_->HasAcquiredMandatoryProcAddresses()) {
-    FML_DLOG(ERROR) << "Proc table has not acquired mandatory proc addresses.";
-    return;
-  }
-
-  // Create the application instance.
-  std::vector<std::string> extensions = {};
-
-  application_ = std::make_unique<vulkan::VulkanApplication>(
-      *vk_, "FlutterTest", std::move(extensions));
-
-  if (!application_->IsValid() || !vk_->AreInstanceProcsSetup()) {
-    // Make certain the application instance was created and it setup the
-    // instance proc table entries.
-    FML_DLOG(ERROR) << "Instance proc addresses have not been setup.";
-    return;
-  }
-
-  // Create the device.
-
-  logical_device_ = application_->AcquireFirstCompatibleLogicalDevice();
-
-  if (logical_device_ == nullptr || !logical_device_->IsValid() ||
-      !vk_->AreDeviceProcsSetup()) {
-    // Make certain the device was created and it setup the device proc table
-    // entries.
-    FML_DLOG(ERROR) << "Device proc addresses have not been setup.";
-    return;
-  }
-
-  // Create the Skia GrContext.
-  if (!CreateSkiaGrContext()) {
-    FML_DLOG(ERROR) << "Could not create Skia context.";
-    return;
-  }
-
-  valid_ = true;
-}
-
-bool ShellTestPlatformViewVulkan::OffScreenSurface::CreateSkiaGrContext() {
-  GrVkBackendContext backend_context;
-
-  if (!CreateSkiaBackendContext(&backend_context)) {
-    FML_DLOG(ERROR) << "Could not create Skia backend context.";
-    return false;
-  }
-
-  sk_sp<GrContext> context = GrContext::MakeVulkan(backend_context);
-
-  if (context == nullptr) {
-    FML_DLOG(ERROR) << "Failed to create GrContext";
-    return false;
-  }
-
-  context->setResourceCacheLimits(vulkan::kGrCacheMaxCount,
-                                  vulkan::kGrCacheMaxByteSize);
-
-  context_ = context;
-
-  return true;
-}
-
-bool ShellTestPlatformViewVulkan::OffScreenSurface::CreateSkiaBackendContext(
-    GrVkBackendContext* context) {
-  auto getProc = vk_->CreateSkiaGetProc();
-
-  if (getProc == nullptr) {
-    FML_DLOG(ERROR) << "GetProcAddress is null";
-    return false;
-  }
-
-  uint32_t skia_features = 0;
-  if (!logical_device_->GetPhysicalDeviceFeaturesSkia(&skia_features)) {
-    FML_DLOG(ERROR) << "Failed to get Physical Device features";
-    return false;
-  }
-
-  context->fInstance = application_->GetInstance();
-  context->fPhysicalDevice = logical_device_->GetPhysicalDeviceHandle();
-  context->fDevice = logical_device_->GetHandle();
-  context->fQueue = logical_device_->GetQueueHandle();
-  context->fGraphicsQueueIndex = logical_device_->GetGraphicsQueueIndex();
-  context->fMinAPIVersion = application_->GetAPIVersion();
-  context->fMaxAPIVersion = application_->GetAPIVersion();
-  context->fFeatures = skia_features;
-  context->fGetProc = std::move(getProc);
-  context->fOwnsInstanceAndDevice = false;
-  return true;
-}
-
-ShellTestPlatformViewVulkan::OffScreenSurface::~OffScreenSurface() {}
-
-bool ShellTestPlatformViewVulkan::OffScreenSurface::IsValid() {
-  return valid_;
-}
-
-std::unique_ptr<SurfaceFrame>
-ShellTestPlatformViewVulkan::OffScreenSurface::AcquireFrame(
-    const SkISize& size) {
-  auto image_info = SkImageInfo::Make(size, SkColorType::kRGBA_8888_SkColorType,
-                                      SkAlphaType::kOpaque_SkAlphaType);
-  auto surface = SkSurface::MakeRenderTarget(context_.get(), SkBudgeted::kNo,
-                                             image_info, 0, nullptr);
-  SurfaceFrame::SubmitCallback callback =
-      [](const SurfaceFrame&, SkCanvas* canvas) -> bool { return true; };
-
-  return std::make_unique<SurfaceFrame>(std::move(surface), true,
-                                        std::move(callback));
-}
-
-GrContext* ShellTestPlatformViewVulkan::OffScreenSurface::GetContext() {
-  return context_.get();
-}
-
-SkMatrix ShellTestPlatformViewVulkan::OffScreenSurface::GetRootTransformation()
-    const {
-  SkMatrix matrix;
-  matrix.reset();
-  return matrix;
+// |GPUSurfaceVulkanDelegate|
+fml::RefPtr<vulkan::VulkanProcTable> ShellTestPlatformViewVulkan::vk() {
+  return proc_table_;
 }
 
 }  // namespace testing
