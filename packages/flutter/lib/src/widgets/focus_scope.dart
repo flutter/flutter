9ec22f2fee505fa8ffbfdf07ae8dd2eef5b81059
Greg Spencer <gspencer@google.com>
2020-02-13 16:12:35 -0800

    Add a sample for restricting focus using FocusScope

--- packages/flutter/lib/src/widgets/focus_scope.dart
+++ packages/flutter/lib/src/widgets/focus_scope.dart
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 
 import 'basic.dart';
@@ -488,7 +489,8 @@ class _FocusState extends State<Focus> {
 }
 
 /// A [FocusScope] is similar to a [Focus], but also serves as a scope for other
-/// [Focus]s and [FocusScope]s, grouping them together.
+/// [Focus]s and [FocusScope]s, grouping them together, and restricting focus
+/// traversal to the scoped controls.
 ///
 /// Like [Focus], [FocusScope] provides an [onFocusChange] as a way to be
 /// notified when the focus is given to or removed from this widget.
@@ -505,7 +507,7 @@ class _FocusState extends State<Focus> {
 /// more information about the details of what node management entails if not
 /// using a [FocusScope] widget.
 ///
-/// A [DefaultTraversalPolicy] widget provides the [FocusTraversalPolicy] for
+/// A [FocusTraversalGroup] widget provides the [FocusTraversalPolicy] for
 /// the [FocusScopeNode]s owned by its descendant widgets. Each [FocusScopeNode]
 /// has [FocusNode] descendants. The traversal policy defines what "previous
 /// focus", "next focus", and "move focus in this direction" means for them.
@@ -519,6 +521,149 @@ class _FocusState extends State<Focus> {
 /// To move the focus, use methods on [FocusScopeNode]. For instance, to move
 /// the focus to the next node, call `Focus.of(context).nextFocus()`.
 ///
+/// {@tool dartpad --template=stateful_widget_material}
+/// This example demonstrates using a [FocusScope] to restrict focus to a particular
+/// portion of the app. In this case, restricting focus to the visible part of a
+/// Stack.
+///
+/// ```dart preamble
+/// /// A demonstration pane.
+/// ///
+/// /// This is just a separate widget to simplify the example.
+/// class Pane extends StatelessWidget {
+///   const Pane({
+///     Key key,
+///     this.focusNode,
+///     this.onPressed,
+///     this.child,
+///     this.backgroundColor,
+///     this.icon,
+///   }) : super(key: key);
+///
+///   final FocusNode focusNode;
+///   final VoidCallback onPressed;
+///   final Widget child;
+///   final Color backgroundColor;
+///   final Widget icon;
+///
+///   @override
+///   Widget build(BuildContext context) {
+///     return Material(
+///       color: backgroundColor,
+///       child: Stack(
+///         fit: StackFit.expand,
+///         children: <Widget>[
+///           Center(
+///             child: child,
+///           ),
+///           Align(
+///             alignment: Alignment.topLeft,
+///             child: IconButton(
+///               autofocus: true,
+///               focusNode: focusNode,
+///               onPressed: onPressed,
+///               icon: icon,
+///             ),
+///           ),
+///         ],
+///       ),
+///     );
+///   }
+/// }
+/// ```
+///
+/// ```dart
+///   bool backdropIsVisible = false;
+///   FocusNode backdropNode = FocusNode(debugLabel: 'Close Backdrop Button');
+///   FocusNode foregroundNode = FocusNode(debugLabel: 'Option Button');
+///
+///   @override
+///   void dispose() {
+///     super.dispose();
+///     backdropNode.dispose();
+///     foregroundNode.dispose();
+///   }
+///
+///   Widget _buildStack(BuildContext context, BoxConstraints constraints) {
+///     Size stackSize = constraints.biggest;
+///     return Stack(
+///       fit: StackFit.expand,
+///       // The backdrop is behind the front widget in the Stack, but the widgets
+///       // would still be active and traversable without the FocusScope.
+///       children: <Widget>[
+///         // TRY THIS: Try removing this FocusScope entirely to see how it affects
+///         // the behavior. Without this FocusScope, the "ANOTHER BUTTON TO FOCUS"
+///         // button, and the IconButton in the backdrop Pane would be focusable
+///         // even when the backdrop wasn't visible.
+///         FocusScope(
+///           // TRY THIS: Try commenting out this line. Notice that the focus
+///           // starts on the backdrop and is stuck there? It seems like the app is
+///           // non-responsive, but it actually isn't. This line makes sure that
+///           // this focus scope and its children can't be focused when they're not
+///           // visible. It might help to make the background color of the
+///           // foreground pane semi-transparent to see it clearly.
+///           canRequestFocus: backdropIsVisible,
+///           child: Pane(
+///             icon: Icon(Icons.close),
+///             focusNode: backdropNode,
+///             backgroundColor: Colors.lightBlue,
+///             onPressed: () => setState(() => backdropIsVisible = false),
+///             child: Column(
+///               mainAxisAlignment: MainAxisAlignment.center,
+///               children: <Widget>[
+///                 // This button would be not visible, but still focusable from
+///                 // the foreground pane without the FocusScope.
+///                 RaisedButton(
+///                   onPressed: () => print('You pressed the other button!'),
+///                   child: Text('ANOTHER BUTTON TO FOCUS'),
+///                 ),
+///                 DefaultTextStyle(
+///                     style: Theme.of(context).textTheme.headline2,
+///                     child: Text('BACKDROP')),
+///               ],
+///             ),
+///           ),
+///         ),
+///         AnimatedPositioned(
+///           curve: Curves.easeInOut,
+///           duration: const Duration(milliseconds: 300),
+///           top: backdropIsVisible ? stackSize.height * 0.9 : 0.0,
+///           width: stackSize.width,
+///           height: stackSize.height,
+///           onEnd: () {
+///             if (backdropIsVisible) {
+///               backdropNode.requestFocus();
+///             } else {
+///               foregroundNode.requestFocus();
+///             }
+///           },
+///           child: Pane(
+///             icon: Icon(Icons.menu),
+///             focusNode: foregroundNode,
+///             // TRY THIS: Try changing this to Colors.green.withOpacity(0.8) to see for
+///             // yourself that the hidden components do/don't get focus.
+///             backgroundColor: Colors.green,
+///             onPressed: backdropIsVisible
+///                 ? null
+///                 : () => setState(() => backdropIsVisible = true),
+///             child: DefaultTextStyle(
+///                 style: Theme.of(context).textTheme.headline2,
+///                 child: Text('FOREGROUND')),
+///           ),
+///         ),
+///       ],
+///     );
+///   }
+///
+///   @override
+///   Widget build(BuildContext context) {
+///     // Use a LayoutBuilder so that we can base the size of the stack on the size
+///     // of its parent.
+///     return LayoutBuilder(builder: _buildStack);
+///   }
+/// ```
+/// {@end-tool}
+///
 /// See also:
 ///
 ///  * [FocusScopeNode], which represents a scope node in the focus hierarchy.
@@ -530,8 +675,8 @@ class _FocusState extends State<Focus> {
 ///    events to focused nodes.
 ///  * [FocusTraversalPolicy], an object used to determine how to move the focus
 ///    to other nodes.
-///  * [DefaultFocusTraversal], a widget used to configure the default focus
-///    traversal policy for a widget subtree.
+///  * [FocusTraversalGroup], a widget used to configure the focus traversal
+///    policy for a widget subtree.
 class FocusScope extends Focus {
   /// Creates a widget that manages a [FocusScopeNode].
   ///
