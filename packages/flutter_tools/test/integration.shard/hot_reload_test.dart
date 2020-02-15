1371a8abaeaff82c2560b2a917c8658358cc2dd5
jonahwilliams <jonahwilliams@google.com>
2020-02-15 14:15:27 -0800

    fix test sync

--- packages/flutter_tools/test/integration.shard/hot_reload_test.dart
+++ packages/flutter_tools/test/integration.shard/hot_reload_test.dart
@@ -36,10 +36,10 @@ void main() {
   });
 
   test('newly added code executes during hot reload', () async {
-    await _flutter.run();
-    _project.uncommentHotReloadPrint();
     final StringBuffer stdout = StringBuffer();
     final StreamSubscription<String> subscription = _flutter.stdout.listen(stdout.writeln);
+    await _flutter.run();
+    _project.uncommentHotReloadPrint();
     try {
       await _flutter.hotReload();
       expect(stdout.toString(), contains('(((((RELOAD WORKED)))))'));
@@ -49,10 +49,10 @@ void main() {
   });
 
   test('reloadMethod triggers hot reload behavior', () async {
-    await _flutter.run();
-    _project.uncommentHotReloadPrint();
     final StringBuffer stdout = StringBuffer();
     final StreamSubscription<String> subscription = _flutter.stdout.listen(stdout.writeln);
+    await _flutter.run();
+    _project.uncommentHotReloadPrint();
     try {
       final String libraryId = _project.buildBreakpointUri.toString();
       await _flutter.reloadMethod(libraryId: libraryId, classId: 'MyApp');
@@ -72,7 +72,6 @@ void main() {
 
   test('breakpoints are hit after hot reload', () async {
     Isolate isolate;
-    await _flutter.run(withDebugger: true, startPaused: true);
     final Completer<void> sawTick1 = Completer<void>();
     final Completer<void> sawTick3 = Completer<void>();
     final Completer<void> sawDebuggerPausedMessage = Completer<void>();
@@ -92,6 +91,7 @@ void main() {
         }
       },
     );
+    await _flutter.run(withDebugger: true, startPaused: true);
     await _flutter.resume(); // we start paused so we can set up our TICK 1 listener before the app starts
     unawaited(sawTick1.future.timeout(
       const Duration(seconds: 5),
@@ -125,13 +125,18 @@ void main() {
   });
 
   test("hot reload doesn't reassemble if paused", () async {
-    await _flutter.run(withDebugger: true);
+    final Completer<void> sawTick1 = Completer<void>();
     final Completer<void> sawTick2 = Completer<void>();
     final Completer<void> sawTick3 = Completer<void>();
     final Completer<void> sawDebuggerPausedMessage1 = Completer<void>();
     final Completer<void> sawDebuggerPausedMessage2 = Completer<void>();
     final StreamSubscription<String> subscription = _flutter.stdout.listen(
       (String line) {
+        print(line);
+        if (line.contains('(((TICK 1)))')) {
+          expect(sawTick1.isCompleted, isFalse);
+          sawTick1.complete();
+        }
         if (line.contains('((((TICK 2))))')) {
           expect(sawTick2.isCompleted, isFalse);
           sawTick2.complete();
@@ -146,6 +151,8 @@ void main() {
         }
       },
     );
+    await _flutter.run(withDebugger: true);
+    await sawTick1.future;
     await _flutter.addBreakpoint(
       _project.buildBreakpointUri,
       _project.buildBreakpointLine,
