b0584d744134b3f4e695fc5d92cefb505a76ba99
Jonah Williams <jonahwilliams@google.com>
2020-02-15 14:10:53 -0800

    Update stack_trace.dart

--- dev/integration_tests/web/lib/stack_trace.dart
+++ dev/integration_tests/web/lib/stack_trace.dart
@@ -58,7 +58,7 @@ const List<StackFrame> expectedDebugStackFrames = <StackFrame>[
     packagePath: 'stack_trace.dart.lib.js',
     line: 132,
     column: 17,
-    className: '<foo>',
+    className: '<unknown>',
     method: 'foo',
     source: '',
   ),
