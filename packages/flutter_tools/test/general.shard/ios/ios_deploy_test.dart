18c6c3daf8a2571ac013b873784d495e0b662775
Christopher Fujino <christopherfujino@gmail.com>
2020-02-18 16:33:39 -0800

    remove unused show import

--- packages/flutter_tools/test/general.shard/ios/ios_deploy_test.dart
+++ packages/flutter_tools/test/general.shard/ios/ios_deploy_test.dart
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'dart:io' show Process, ProcessException;
+import 'dart:io' show Process;
 
 import 'package:flutter_tools/src/artifacts.dart';
 import 'package:flutter_tools/src/base/logger.dart';
