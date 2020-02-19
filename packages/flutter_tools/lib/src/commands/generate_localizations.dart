ac7b60aa165b44f33829291618f965f539e317c4
Shi Hao Hong <shihaohong94@gmail.com>
2020-02-19 11:00:05 -0800

    WIP - working flutter_tool, cannot end process

--- packages/flutter_tools/lib/src/commands/generate_localizations.dart
+++ packages/flutter_tools/lib/src/commands/generate_localizations.dart
@@ -5,35 +5,85 @@
 import 'dart:async';
 import 'dart:io';
 
+import 'package:path/path.dart' as path;
+
 import '../cache.dart';
 import '../globals.dart' as globals;
-
 import '../runner/flutter_command.dart';
 
 class GenerateLocalizationsCommand extends FlutterCommand {
+  GenerateLocalizationsCommand() {
+    argParser.addOption(
+      'arb-dir',
+      defaultsTo: path.join('lib', 'l10n'),
+      help: 'The directory where all localization files should reside. For '
+        'example, the template and translated arb files should be located here. '
+        'Also, the generated output messages Dart files for each locale and the '
+        'generated localizations classes will be created here.',
+    );
+    argParser.addOption(
+      'template-arb-file',
+      defaultsTo: 'app_en.arb',
+      help: 'The template arb file that will be used as the basis for '
+        'generating the Dart localization and messages files.',
+    );
+    argParser.addOption(
+      'output-localization-file',
+      defaultsTo: 'app_localizations.dart',
+      help: 'The filename for the output localization and localizations '
+        'delegate classes.',
+    );
+    argParser.addOption(
+      'output-class',
+      defaultsTo: 'AppLocalizations',
+      help: 'The Dart class name to use for the output localization and '
+        'localizations delegate classes.',
+    );
+    argParser.addOption(
+      'preferred-supported-locales',
+      help: 'The list of preferred supported locales for the application. '
+        'By default, the tool will generate the supported locales list in '
+        'alphabetical order. Use this flag if you would like to default to '
+        'a different locale. \n\n'
+        "For example, pass in ['en_US'] if you would like your app to "
+        'default to American English if a device supports it.',
+    );
+  }
+
   @override
   String get name => 'generate-localizations';
 
   @override
   String get description => 'Generate Dart files from ARB files to localize a Flutter application.';
 
-  String get dartSdkPath {
-    return globals.fs.path.join(Cache.flutterRoot, 'bin', 'cache', 'dart-sdk');
-  }
-
   @override
   Future<FlutterCommandResult> runCommand() async {
+    final String dartSdkPath = globals.fs.path.join(Cache.flutterRoot, 'bin', 'cache', 'dart-sdk');
     final String root = globals.fs.path.absolute(Cache.flutterRoot);
+
+    final List<String> options = <String>[
+      '--arb-dir=${argResults['arb-dir']}',
+      '--template-arb-file=${argResults['template-arb-file']}',
+      '--output-localization-file=${argResults['output-localization-file']}',
+      '--output-class=${argResults['output-class']}',
+      if (argResults['preferred-supported-locales'] != null)
+        '--preferred-supported-locales=${argResults['preferred-supported-locales']}'
+    ];
 
     final List<String> command = <String>[
       globals.fs.path.join(dartSdkPath, 'bin', 'dart'),
       globals.fs.path.join(root, 'dev', 'tools', 'localization', 'bin', 'gen_l10n.dart'),
+      ...options,
     ];
+
+    print('starting process');
     final ProcessResult result = await globals.processManager.run(command);
+    print('end process');
 
-    print(result.stdout);
-    print(result.stderr);
-
+    if (result.exitCode != 0) {
+      stderr.write(result.stderr);
+      return FlutterCommandResult.fail();
+    }
     return FlutterCommandResult.success();
   }
 }
