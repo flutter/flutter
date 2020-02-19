b0c5be347f17414740d760cac1d629df4390e9da
Nurhan Turgut <nurhan@google.com>
2020-02-19 10:58:07 -0800

    addressing reviewer comments

--- examples/hello_world/test_driver/smoke_web_engine_test.dart
+++ examples/hello_world/test_driver/smoke_web_engine_test.dart
@@ -1,4 +1,7 @@
-// Imports the Flutter Driver API.
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import 'package:flutter_driver/flutter_driver.dart';
 import 'package:test/test.dart';
 
