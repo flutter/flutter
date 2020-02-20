1793108ba1ce31c8e06bc8676ddf457613eafcb6
Greg Spencer <gspencergoog@users.noreply.github.com>
2020-02-19 18:41:03 -0800

    Add additional focus samples. (#50846)

--- packages/flutter/lib/src/widgets/actions.dart
+++ packages/flutter/lib/src/widgets/actions.dart
@@ -374,12 +374,14 @@ class Actions extends InheritedWidget {
 /// {@tool dartpad --template=stateful_widget_material}
 /// This example shows how keyboard interaction can be added to a custom control
 /// that changes color when hovered and focused, and can toggle a light when
-/// activated, either by touch or by hitting the `X` key on the keyboard.
+/// activated, either by touch or by hitting the `X` key on the keyboard when
+/// the "And Me" button has the keyboard focus (be sure to use TAB to move the
+/// focus to the "And Me" button before trying it out).
 ///
 /// This example defines its own key binding for the `X` key, but in this case,
 /// there is also a default key binding for [ActivateAction] in the default key
 /// bindings created by [WidgetsApp] (the parent for [MaterialApp], and
-/// [CupertinoApp]), so the `ENTER` key will also activate the control.
+/// [CupertinoApp]), so the `ENTER` key will also activate the buttons.
 ///
 /// ```dart imports
 /// import 'package:flutter/services.dart';
