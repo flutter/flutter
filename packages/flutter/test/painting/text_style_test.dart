645f423e3b3b9910684e0c11d63e110dc8a8eb08
Yegor Jbanov <yjbanov@google.com>
2020-02-20 09:59:17 -0800

    fix unused import

--- packages/flutter/test/painting/text_style_test.dart
+++ packages/flutter/test/painting/text_style_test.dart
@@ -4,7 +4,6 @@
 
 import 'dart:ui' as ui show TextStyle, ParagraphStyle, FontFeature, Shadow;
 
-import 'package:flutter/foundation.dart';
 import 'package:flutter/painting.dart';
 import '../flutter_test_alternative.dart';
 
