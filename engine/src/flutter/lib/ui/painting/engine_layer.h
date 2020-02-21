b1f692e9cb246c5e946eee64a9e1729186589a67
Dan Field <dnfield@google.com>
2020-02-20 17:31:03 -0800

    Refactor of ClaimDartHandle -> AssociateWithDartWrapper (flutter/engine#16720)

--- engine/src/flutter/lib/ui/painting/engine_layer.h
+++ engine/src/flutter/lib/ui/painting/engine_layer.h
@@ -33,7 +33,7 @@ class EngineLayer : public RefCountedDartWrappable<EngineLayer> {
   static void MakeRetained(Dart_Handle dart_handle,
                            std::shared_ptr<flutter::ContainerLayer> layer) {
     auto engine_layer = fml::MakeRefCounted<EngineLayer>(layer);
-    engine_layer->ClaimDartHandle(dart_handle);
+    engine_layer->AssociateWithDartWrapper(dart_handle);
   }
 
   static void RegisterNatives(tonic::DartLibraryNatives* natives);
