5cfb57a556fecd4087a8537b15650a433be78e89
stuartmorgan <stuartmorgan@google.com>
2020-02-23 22:37:39 -0800

    Fix auto-added import

--- packages/flutter_tools/lib/src/windows/visual_studio_solution_utils.dart
+++ packages/flutter_tools/lib/src/windows/visual_studio_solution_utils.dart
@@ -2,8 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:flutter_tools/src/base/common.dart';
-
+import '../base/common.dart';
 import '../base/file_system.dart';
 import '../convert.dart';
 import '../globals.dart' as globals;
