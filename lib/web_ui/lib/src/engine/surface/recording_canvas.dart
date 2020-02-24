b87bb0a161b5cc5d079ff937f97080d8d7882159
Ferhat <ferhat@gmail.com>
2020-02-24 11:19:47 -0800

    [web] Fix canvas leak when dpi changes. Evict from BitmapCanvas cache under… (#16721)
    * Fix canvas leak when dpi changes. Evict from BitmapCanvas cache under memory pressure.
    
    * optimized _reduceCanvasMemoryUsage
    
    * Addressed review comments

--- lib/web_ui/lib/src/engine/surface/recording_canvas.dart
+++ lib/web_ui/lib/src/engine/surface/recording_canvas.dart
@@ -183,8 +183,6 @@ class RecordingCanvas {
   }
 
   void drawColor(ui.Color color, ui.BlendMode blendMode) {
-    _hasArbitraryPaint = true;
-    _didDraw = true;
     _paintBounds.grow(_paintBounds.maxPaintBounds);
     _commands.add(PaintDrawColor(color, blendMode));
   }
@@ -315,6 +313,21 @@ class RecordingCanvas {
   }
 
   void drawPath(ui.Path path, SurfacePaint paint) {
+    if (paint.shader == null) {
+      // For Rect/RoundedRect paths use drawRect/drawRRect code paths for
+      // DomCanvas optimization.
+      SurfacePath sPath = path;
+      final ui.Rect rect = sPath.webOnlyPathAsRect;
+      if (rect != null) {
+        drawRect(rect, paint);
+        return;
+      }
+      final ui.RRect rrect = sPath.webOnlyPathAsRoundedRect;
+      if (rrect != null) {
+        drawRRect(rrect, paint);
+        return;
+      }
+    }
     _hasArbitraryPaint = true;
     _didDraw = true;
     ui.Rect pathBounds = path.getBounds();
