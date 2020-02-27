9bebd2c5e2c76f6c0519d3e9b72098d6d6e4966b
jonahwilliams <jonahwilliams@google.com>
2020-02-27 09:22:49 -0800

    [flutter_tools] swap mustache dep for mustache_template

--- packages/flutter_tools/lib/src/build_runner/mustache_template.dart
+++ packages/flutter_tools/lib/src/build_runner/mustache_template.dart
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:mustache/mustache.dart';
+import 'package:mustache_template/mustache_template.dart';
 
 import '../base/template.dart';
 
