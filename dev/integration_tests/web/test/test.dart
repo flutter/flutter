e77529bbb361a3dc2381468fd116f9645f476c01
Jonah Williams <jonahwilliams@google.com>
2020-02-28 13:20:04 -0800

    Update test.dart

--- dev/integration_tests/web/test/test.dart
+++ dev/integration_tests/web/test/test.dart
@@ -2,24 +2,15 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'dart:html' as html;
-
 import 'package:web_integration/a.dart'
   if (dart.library.io) 'package:web_integration/b.dart' as message1;
 import 'package:web_integration/c.dart'
   if (dart.library.html) 'package:web_integration/d.dart' as message2;
 
 void main() {
-  String message;
   if (message1.message == 'a' && message2.message == 'd')  {
-    message = '--- TEST SUCCEEDED ---';
+    print('--- TEST SUCCEEDED ---');
   } else {
-    message = '--- TEST FAILED ---';
+    print('--- TEST FAILED ---');
   }
-  print(message);
-  html.HttpRequest.request(
-    '/test-result',
-    method: 'POST',
-    sendData: message,
-  );
 }
