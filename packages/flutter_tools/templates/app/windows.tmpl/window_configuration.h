28f5a65453c5d7260897030cea13d998b507e68f
Stuart Morgan <stuartmorgan@google.com>
2020-03-03 23:44:07 +0100

    Minor fixes

--- packages/flutter_tools/templates/app/windows.tmpl/window_configuration.h
+++ packages/flutter_tools/templates/app/windows.tmpl/window_configuration.h
@@ -2,10 +2,9 @@
 #define WINDOW_CONFIGURATION_
 
 // This is a temporary approach to isolate changes that people are likely to
-// make to the example project from main.cpp, where the APIs are still in flux.
-// This will avoid people needing to resolve conflicts or re-create changes
-// slightly different every time the Windows Flutter API surface changes just
-// because of, e.g., a local change to the window title.
+// make to main.cpp, where the APIs are still in flux. This will reduce the
+// need to resolve conflicts or re-create changes slightly differently every
+// time the Windows Flutter API surface changes.
 //
 // Longer term there should be simpler configuration options for common
 // customizations like this, without requiring native code changes.
