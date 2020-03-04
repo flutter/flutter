f8f5d96153e73cbdf4dca3881a8ad8f32a9c7753
Stuart Morgan <stuartmorgan@google.com>
2020-03-04 09:09:14 +0100

    Template typo fixes

--- packages/flutter_tools/templates/app/windows.tmpl/resource.h
+++ packages/flutter_tools/templates/app/windows.tmpl/resource.h
@@ -5,7 +5,7 @@
 #define IDI_APP_ICON                    101
 
 // Next default values for new objects
-// 
+//
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        102
