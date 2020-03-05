bb44e388f3c114ae2ae0e2a74dd25ed33ef06627
jonahwilliams <jonahwilliams@google.com>
2020-03-04 20:46:19 -0800

    [flutter_tools] resolve requests to web directory in debug mode

---
+++ dev/integration_tests/web/lib/web_directory_loading.dart
@@ -0,0 +1,23 @@
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'dart:html' as html;
+
+Future<void> main() async {
+  try {
+    final html.HttpRequest request = await html.HttpRequest.request(
+      '/example',
+      method: 'GET',
+    );
+    final String body = request.responseText;
+    if (body == 'This is an Example') {
+      print('--- TEST SUCCEEDED ---');
+    } else {
+      print('--- TEST FAILED ---');
+    }
+  } catch (err) {
+    print(err);
+    print('--- TEST FAILED ---');
+  }
+}
