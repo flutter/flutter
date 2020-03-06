85a2c1f44f11759e6eb3619fa3b802b69b94a85b
Aaron Clarke <aaclarke@google.com>
2020-03-06 11:35:09 -0800

    formatted code

--- packages/flutter/lib/src/widgets/scroll_view.dart
+++ packages/flutter/lib/src/widgets/scroll_view.dart
@@ -330,8 +330,9 @@ abstract class ScrollView extends StatelessWidget {
         child: scrollableResult,
         onNotification: (ScrollUpdateNotification notification) {
           final FocusScopeNode focusScope = FocusScope.of(context);
-          if (notification.dragDetails != null
-              && (focusScope.focusedChild != null && focusScope.focusedChild.hasFocus)) {
+          if (notification.dragDetails != null &&
+              (focusScope.focusedChild != null &&
+                  focusScope.focusedChild.hasFocus)) {
             focusScope.unfocus();
           }
           return false;
