bf4369f304e4e1458218000a961e111bad04b65e
Dan Field <dnfield@google.com>
2020-03-06 21:38:35 -0800

    Reland fix `--version` (#52141)

--- packages/flutter_tools/test/commands.shard/hermetic/doctor_test.dart
+++ packages/flutter_tools/test/commands.shard/hermetic/doctor_test.dart
@@ -4,12 +4,17 @@
 
 import 'dart:async';
 
+import 'package:args/command_runner.dart';
+import 'package:file/memory.dart';
 import 'package:flutter_tools/src/artifacts.dart';
 import 'package:flutter_tools/src/base/common.dart';
+import 'package:flutter_tools/src/base/file_system.dart';
 import 'package:flutter_tools/src/base/io.dart';
 
 import 'package:flutter_tools/src/base/terminal.dart';
 import 'package:flutter_tools/src/base/user_messages.dart';
+import 'package:flutter_tools/src/cache.dart';
+import 'package:flutter_tools/src/commands/doctor.dart';
 import 'package:flutter_tools/src/doctor.dart';
 import 'package:flutter_tools/src/features.dart';
 import 'package:flutter_tools/src/globals.dart' as globals;
@@ -722,6 +727,50 @@ void main() {
     FeatureFlags: () => TestFeatureFlags(isWebEnabled: true),
     ProcessManager: () => MockProcessManager(),
   });
+
+  testUsingContext('Fetches tags to get the right version', () async {
+    Cache.disableLocking();
+
+    final DoctorCommand doctorCommand = DoctorCommand();
+    final CommandRunner<void> commandRunner = createTestCommandRunner(doctorCommand);
+
+    await commandRunner.run(<String>['doctor']);
+
+    verify(mockFlutterVersion.fetchTagsAndUpdate()).called(1);
+
+    Cache.enableLocking();
+  }, overrides: <Type, Generator>{
+    ProcessManager: () => FakeProcessManager.any(),
+    FileSystem: () => MemoryFileSystem.test(),
+    FlutterVersion: () => mockFlutterVersion,
+    Doctor: () => NoOpDoctor(),
+  }, initializeFlutterRoot: false);
+}
+
+class NoOpDoctor implements Doctor {
+  @override
+  bool get canLaunchAnything => true;
+
+  @override
+  bool get canListAnything => true;
+
+  @override
+  Future<bool> checkRemoteArtifacts(String engineRevision) async => true;
+
+  @override
+  Future<bool> diagnose({ bool androidLicenses = false, bool verbose = true, bool showColor = true }) async => true;
+
+  @override
+  List<ValidatorTask> startValidatorTasks() => <ValidatorTask>[];
+
+  @override
+  Future<void> summary() => null;
+
+  @override
+  List<DoctorValidator> get validators => <DoctorValidator>[];
+
+  @override
+  List<Workflow> get workflows => <Workflow>[];
 }
 
 class MockUsage extends Mock implements Usage {}
@@ -1062,4 +1111,3 @@ class VsCodeValidatorTestTargets extends VsCodeValidator {
 }
 
 class MockProcessManager extends Mock implements ProcessManager {}
-class MockFlutterVersion extends Mock implements FlutterVersion {}
