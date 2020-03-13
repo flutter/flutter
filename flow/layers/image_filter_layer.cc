e48761e5bb05531fc2313351be5610b0c1452ed0
Jim Graham <flar@google.com>
2020-03-13 14:44:27 -0700

    Reland ImageFiltered bounds fix (#17077)
    Fix for https://github.com/flutter/flutter/issues/51978 to use the bounds of the filtered result as the layer's paint bounds.

--- flow/layers/image_filter_layer.cc
+++ flow/layers/image_filter_layer.cc
@@ -11,9 +11,22 @@ ImageFilterLayer::ImageFilterLayer(sk_sp<SkImageFilter> filter)
 
 void ImageFilterLayer::Preroll(PrerollContext* context,
                                const SkMatrix& matrix) {
+  TRACE_EVENT0("flutter", "ImageFilterLayer::Preroll");
+
   Layer::AutoPrerollSaveLayerState save =
       Layer::AutoPrerollSaveLayerState::Create(context);
-  ContainerLayer::Preroll(context, matrix);
+
+  child_paint_bounds_ = SkRect::MakeEmpty();
+  PrerollChildren(context, matrix, &child_paint_bounds_);
+  if (filter_) {
+    const SkIRect filter_input_bounds = child_paint_bounds_.roundOut();
+    SkIRect filter_output_bounds =
+        filter_->filterBounds(filter_input_bounds, SkMatrix::I(),
+                              SkImageFilter::kForward_MapDirection);
+    set_paint_bounds(SkRect::Make(filter_output_bounds));
+  } else {
+    set_paint_bounds(child_paint_bounds_);
+  }
 
   if (!context->has_platform_view && context->raster_cache &&
       SkRect::Intersects(context->cull_rect, paint_bounds())) {
@@ -48,8 +61,12 @@ void ImageFilterLayer::Paint(PaintContext& context) const {
   SkPaint paint;
   paint.setImageFilter(filter_);
 
+  // Normally a save_layer is sized to the current layer bounds, but in this
+  // case the bounds of the child may not be the same as the filtered version
+  // so we use the child_paint_bounds_ which were snapshotted from the
+  // Preroll on the children before we adjusted them based on the filter.
   Layer::AutoSaveLayer save_layer =
-      Layer::AutoSaveLayer::Create(context, paint_bounds(), &paint);
+      Layer::AutoSaveLayer::Create(context, child_paint_bounds_, &paint);
   PaintChildren(context);
 }
 
