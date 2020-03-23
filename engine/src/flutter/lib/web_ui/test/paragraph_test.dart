0201dfd46b229ab5982756d9a4d626c8055ae79c
Mouad Debbar <mouad.debbar@gmail.com>
2020-03-23 13:22:10 -0700

    [web] Introduce js interop to enable experimental flags on web (flutter/engine#17099)

--- engine/src/flutter/lib/web_ui/test/paragraph_test.dart
+++ engine/src/flutter/lib/web_ui/test/paragraph_test.dart
@@ -4,7 +4,7 @@
 
 // @dart = 2.6
 import 'package:ui/src/engine.dart';
-import 'package:ui/ui.dart';
+import 'package:ui/ui.dart' hide window;
 
 import 'package:test/test.dart';
 
@@ -12,18 +12,20 @@ void testEachMeasurement(String description, VoidCallback body, {bool skip}) {
   test('$description (dom measurement)', () async {
     try {
       TextMeasurementService.initialize(rulerCacheCapacity: 2);
+      WebExperiments.instance.useCanvasText = false;
       return body();
     } finally {
+      WebExperiments.instance.useCanvasText = null;
       TextMeasurementService.clearCache();
     }
   }, skip: skip);
   test('$description (canvas measurement)', () async {
     try {
       TextMeasurementService.initialize(rulerCacheCapacity: 2);
-      TextMeasurementService.enableExperimentalCanvasImplementation = true;
+      WebExperiments.instance.useCanvasText = true;
       return body();
     } finally {
-      TextMeasurementService.enableExperimentalCanvasImplementation = false;
+      WebExperiments.instance.useCanvasText = null;
       TextMeasurementService.clearCache();
     }
   }, skip: skip);
@@ -184,7 +186,7 @@ void main() async {
   test('getPositionForOffset multi-line', () {
     // [Paragraph.getPositionForOffset] for multi-line text doesn't work well
     // with dom-based measurement.
-    TextMeasurementService.enableExperimentalCanvasImplementation = true;
+    WebExperiments.instance.useCanvasText = true;
     TextMeasurementService.initialize(rulerCacheCapacity: 2);
 
     final ParagraphBuilder builder = ParagraphBuilder(ParagraphStyle(
@@ -280,11 +282,11 @@ void main() async {
     );
 
     TextMeasurementService.clearCache();
-    TextMeasurementService.enableExperimentalCanvasImplementation = false;
+    WebExperiments.instance.useCanvasText = null;
   });
 
   test('getPositionForOffset multi-line centered', () {
-    TextMeasurementService.enableExperimentalCanvasImplementation = true;
+    WebExperiments.instance.useCanvasText = true;
     TextMeasurementService.initialize(rulerCacheCapacity: 2);
 
     final ParagraphBuilder builder = ParagraphBuilder(ParagraphStyle(
@@ -387,7 +389,7 @@ void main() async {
     );
 
     TextMeasurementService.clearCache();
-    TextMeasurementService.enableExperimentalCanvasImplementation = false;
+    WebExperiments.instance.useCanvasText = null;
   });
 
   testEachMeasurement('getBoxesForRange returns a box', () {
@@ -782,7 +784,7 @@ void main() async {
 
   test('longestLine', () {
     // [Paragraph.longestLine] is only supported by canvas-based measurement.
-    TextMeasurementService.enableExperimentalCanvasImplementation = true;
+    WebExperiments.instance.useCanvasText = true;
     TextMeasurementService.initialize(rulerCacheCapacity: 2);
 
     final ParagraphBuilder builder = ParagraphBuilder(ParagraphStyle(
@@ -797,7 +799,7 @@ void main() async {
     expect(paragraph.longestLine, 50.0);
 
     TextMeasurementService.clearCache();
-    TextMeasurementService.enableExperimentalCanvasImplementation = false;
+    WebExperiments.instance.useCanvasText = null;
   });
 
   testEachMeasurement('getLineBoundary (single-line)', () {
@@ -824,7 +826,7 @@ void main() async {
   test('getLineBoundary (multi-line)', () {
     // [Paragraph.getLineBoundary] for multi-line paragraphs is only supported
     // by canvas-based measurement.
-    TextMeasurementService.enableExperimentalCanvasImplementation = true;
+    WebExperiments.instance.useCanvasText = true;
     TextMeasurementService.initialize(rulerCacheCapacity: 2);
 
     final ParagraphBuilder builder = ParagraphBuilder(ParagraphStyle(
@@ -867,7 +869,7 @@ void main() async {
     }
 
     TextMeasurementService.clearCache();
-    TextMeasurementService.enableExperimentalCanvasImplementation = false;
+    WebExperiments.instance.useCanvasText = null;
   });
 
   testEachMeasurement('width should be a whole integer', () {
