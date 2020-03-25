926cd6cf912d9a605c89c97d3e96f9d682dc1cc7
Aaron Clarke <aaclarke@google.com>
2020-03-25 12:03:40 -0700

    added license

--- dev/tools/localization/gen_subtag_registry.dart
+++ dev/tools/localization/gen_subtag_registry.dart
@@ -1,7 +1,6 @@
-/// A script to generate a Dart cache of https://www.iana.org.  This should be
-/// run occasionally.  It was created since iana.org was found to be flakey.
-///
-/// To execute: dart gen_subtag_registry.dart > language_subtag_registry.dart
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
 import 'dart:async';
 import 'dart:convert';
@@ -9,12 +8,20 @@ import 'dart:io';
 
 const String registry = 'https://www.iana.org/assignments/language-subtag-registry/language-subtag-registry';
 
+/// A script to generate a Dart cache of https://www.iana.org.  This should be
+/// run occasionally.  It was created since iana.org was found to be flakey.
+///
+/// To execute: dart gen_subtag_registry.dart > language_subtag_registry.dart
 Future<void> main() async {
   final HttpClient client = HttpClient();
   final HttpClientRequest request = await client.getUrl(Uri.parse(registry));
   final HttpClientResponse response = await request.close();
   final String body = (await response.cast<List<int>>().transform<String>(utf8.decoder).toList()).join('');
-  print('''/// Cache of $registry.
+  print('''// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/// Cache of $registry.
 const String languageSubtagRegistry = \'\'\'$body\'\'\';''');
   client.close(force: true);
 }
--- dev/tools/localization/language_subtag_registry.dart
+++ dev/tools/localization/language_subtag_registry.dart
@@ -1,3 +1,7 @@
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 /// Cache of https://www.iana.org/assignments/language-subtag-registry/language-subtag-registry.
 const String languageSubtagRegistry = '''File-Date: 2020-03-16
 %%
