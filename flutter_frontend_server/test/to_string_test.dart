cda57c14cb4e3039a5eae78e805a634859b1c2d9
Dan Field <dnfield@google.com>
2020-03-26 13:11:54 -0700

    Do not drop toString for enums (#17352)

--- flutter_frontend_server/test/to_string_test.dart
+++ flutter_frontend_server/test/to_string_test.dart
@@ -121,6 +121,25 @@ void main(List<String> args) async {
     verifyNever(statement.replaceWith(any));
   });
 
+  test('ToStringVisitor ignores enum toString', () {
+    final ToStringVisitor visitor = ToStringVisitor(uiAndFlutter);
+    final MockProcedure procedure = MockProcedure();
+    final MockFunctionNode function = MockFunctionNode();
+    final MockStatement statement = MockStatement();
+    final Library library = Library(Uri.parse('package:some_package/src/blah.dart'));
+    when(procedure.function).thenReturn(function);
+    when(procedure.name).thenReturn(Name('toString'));
+    when(procedure.annotations).thenReturn(const <Expression>[]);
+    when(procedure.enclosingLibrary).thenReturn(library);
+    when(procedure.enclosingClass).thenReturn(Class()..isEnum = true);
+    when(procedure.isAbstract).thenReturn(false);
+    when(procedure.isStatic).thenReturn(false);
+    when(function.body).thenReturn(statement);
+
+    visitor.visitProcedure(procedure);
+    verifyNever(statement.replaceWith(any));
+  });
+
   test('ToStringVisitor ignores non-specified libraries', () {
     final ToStringVisitor visitor = ToStringVisitor(uiAndFlutter);
     final MockProcedure procedure = MockProcedure();
@@ -254,6 +273,7 @@ void main(List<String> args) async {
       expect(
         runResult.stdout.trim(),
         '{"Paint.toString":"Paint(Color(0xffffffff))",'
+         '"Brightness.toString":"Brightness.dark",'
          '"Foo.toString":"I am a Foo",'
          '"Keep.toString":"I am a Keep"}',
       );
@@ -275,6 +295,7 @@ void main(List<String> args) async {
       expect(
         runResult.stdout.trim(),
         '{"Paint.toString":"Instance of \'Paint\'",'
+         '"Brightness.toString":"Brightness.dark",'
          '"Foo.toString":"Instance of \'Foo\'",'
          '"Keep.toString":"I am a Keep"}',
       );
