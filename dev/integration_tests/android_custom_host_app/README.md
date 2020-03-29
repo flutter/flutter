304395ae078b85e0d25d6455b16714065fbaa629
Anisov Aleksey <aav@tomsksoft.com>
2020-03-30 00:47:27 +0700

    add project for devicelab test

---
+++ dev/integration_tests/android_custom_host_app/README.md
@@ -0,0 +1,3 @@
+# Android custom host app
+
+Used by the `module_custom_host_app_name_test.dart` device lab test.
---
+++ dev/integration_tests/android_custom_host_app/SampleApp/src/main/java/io/flutter/add2app/MainActivity.java
@@ -0,0 +1,18 @@
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package io.flutter.add2app;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import io.flutter.facade.Flutter;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(Flutter.createView(this, getLifecycle(), "route1"));
+  }
+}
