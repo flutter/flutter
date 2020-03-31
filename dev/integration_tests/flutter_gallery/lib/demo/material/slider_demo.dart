2aba5c84f40c63a8d24ead6af7d02fa8867f88f4
Jose Alba <jaguado@google.com>
2020-03-31 13:56:01 -0400

    [Slider] Rebase. (#52663)

--- dev/integration_tests/flutter_gallery/lib/demo/material/slider_demo.dart
+++ dev/integration_tests/flutter_gallery/lib/demo/material/slider_demo.dart
@@ -69,6 +69,7 @@ class _CustomRangeThumbShape extends RangeSliderThumbShape {
     @required SliderThemeData sliderTheme,
     TextDirection textDirection,
     Thumb thumb,
+    bool isPressed,
   }) {
     final Canvas canvas = context.canvas;
     final ColorTween colorTween = ColorTween(
@@ -130,6 +131,8 @@ class _CustomThumbShape extends SliderComponentShape {
     SliderThemeData sliderTheme,
     TextDirection textDirection,
     double value,
+    double textScaleFactor,
+    Size sizeWithOverflow,
   }) {
     final Canvas canvas = context.canvas;
     final ColorTween colorTween = ColorTween(
@@ -169,6 +172,8 @@ class _CustomValueIndicatorShape extends SliderComponentShape {
     SliderThemeData sliderTheme,
     TextDirection textDirection,
     double value,
+    double textScaleFactor,
+    Size sizeWithOverflow,
   }) {
     final Canvas canvas = context.canvas;
     final ColorTween enableColor = ColorTween(
@@ -268,15 +273,21 @@ class _SlidersState extends State<_Sliders> {
                   ),
                 ),
               ),
-              Slider.adaptive(
-                value: _continuousValue,
-                min: 0.0,
-                max: 100.0,
-                onChanged: (double value) {
-                  setState(() {
-                    _continuousValue = value;
-                  });
-                },
+              SliderTheme(
+                data: const SliderThemeData(
+                  showValueIndicator: ShowValueIndicator.always,
+                ),
+                child: Slider.adaptive(
+                  label: _continuousValue.toStringAsFixed(6).toString(),
+                  value: _continuousValue,
+                  min: 0.0,
+                  max: 100.0,
+                  onChanged: (double value) {
+                    setState(() {
+                      _continuousValue = value;
+                    });
+                  },
+                ),
               ),
               const Text('Continuous with Editable Numerical Value'),
             ],
@@ -314,7 +325,7 @@ class _SlidersState extends State<_Sliders> {
                   activeTrackColor: Colors.deepPurple,
                   inactiveTrackColor: theme.colorScheme.onSurface.withOpacity(0.5),
                   activeTickMarkColor: theme.colorScheme.onSurface.withOpacity(0.7),
-                  inactiveTickMarkColor:  theme.colorScheme.surface.withOpacity(0.7),
+                  inactiveTickMarkColor: theme.colorScheme.surface.withOpacity(0.7),
                   overlayColor: theme.colorScheme.onSurface.withOpacity(0.12),
                   thumbColor: Colors.deepPurple,
                   valueIndicatorColor: Colors.deepPurpleAccent,
