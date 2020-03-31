e84b874a5ee4a514cff7916d97ff1aa81e6fedb8
nujz <37606102+nujz@users.noreply.github.com>
2020-03-31 10:11:39 +0800

    Update packages/flutter/lib/src/widgets/scroll_position_with_single_context.dart
    Co-Authored-By: Kate Lovett <katelovett@google.com>

--- packages/flutter/lib/src/widgets/scroll_position_with_single_context.dart
+++ packages/flutter/lib/src/widgets/scroll_position_with_single_context.dart
@@ -197,8 +197,7 @@ class ScrollPositionWithSingleContext extends ScrollPosition implements ScrollAc
     goIdle();
     if (pixels != value) {
       final double oldPixels = pixels;
-      correctPixels(value);
-      notifyListeners();
+      forcePixels(value)
       didStartScroll();
       didUpdateScrollPositionBy(pixels - oldPixels);
       didEndScroll();
