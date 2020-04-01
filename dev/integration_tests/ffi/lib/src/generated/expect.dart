69f8d74a4b77c3cd14129a45a771ab2ad1cc3e64
Martin Kustermann <kustermann@google.com>
2020-04-01 11:42:27 +0200

    add flutter license header

--- dev/integration_tests/ffi/lib/src/generated/expect.dart
+++ dev/integration_tests/ffi/lib/src/generated/expect.dart
@@ -1,3 +1,7 @@
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 // Copyright (c) 2012, the Dart project authors.  Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
