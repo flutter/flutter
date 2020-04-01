6ed6d9bdc87b7ac84de61f42338cb5183668440c
Brian Osman <brianosman@google.com>
2020-04-01 10:27:50 -0400

    Finish removing deprecated SkCanvas virtuals (flutter/engine#17445)
    API staging is complete - these defines have been removed.

--- engine/src/flutter/testing/mock_canvas.h
+++ engine/src/flutter/testing/mock_canvas.h
@@ -195,13 +195,6 @@ class MockCanvas : public SkCanvasVirtualEnforcer<SkCanvas> {
                  bool,
                  const SkPaint&) override;
   void onDrawRRect(const SkRRect&, const SkPaint&) override;
-#if defined(SK_SUPPORT_LEGACY_ONDRAWBITMAP_VIRTUALS)
-  void onDrawBitmapRect(const SkBitmap&,
-                        const SkRect*,
-                        const SkRect&,
-                        const SkPaint*,
-                        SrcRectConstraint) override;
-#endif
   void onDrawImage(const SkImage* image,
                    SkScalar x,
                    SkScalar y,
@@ -215,23 +208,10 @@ class MockCanvas : public SkCanvasVirtualEnforcer<SkCanvas> {
                        const SkIRect&,
                        const SkRect&,
                        const SkPaint*) override;
-#if defined(SK_SUPPORT_LEGACY_ONDRAWBITMAP_VIRTUALS)
-  void onDrawBitmap(const SkBitmap& bitmap,
-                    SkScalar x,
-                    SkScalar y,
-                    const SkPaint* paint) override;
-#endif
   void onDrawImageLattice(const SkImage*,
                           const Lattice&,
                           const SkRect&,
                           const SkPaint*) override;
-#ifdef SK_SUPPORT_LEGACY_DRAWVERTS_VIRTUAL
-  void onDrawVerticesObject(const SkVertices*,
-                            const SkVertices::Bone[],
-                            int,
-                            SkBlendMode,
-                            const SkPaint&) override {}
-#endif
   void onDrawVerticesObject(const SkVertices*,
                             SkBlendMode,
                             const SkPaint&) override;
