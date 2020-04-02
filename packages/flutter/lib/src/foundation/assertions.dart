4e1a57d9abfb7691855fd74dae649a99c479cab1
ferhatb <ferhat@gmail.com>
2020-04-01 18:25:24 -0700

    skip both build_runner based and frontend_server based calls

--- packages/flutter/lib/src/foundation/assertions.dart
+++ packages/flutter/lib/src/foundation/assertions.dart
@@ -926,7 +926,8 @@ void debugPrintStack({StackTrace stackTrace, String label, int maxFrames}) {
     // is addressed.
     lines = lines.skipWhile((String line) {
       return line.contains('StackTrace.current') ||
-             line.contains('dart-sdk/lib/_internal');
+             line.contains('dart-sdk/lib/_internal') ||
+             line.contains('dart:sdk_internal');
     });
   }
   if (maxFrames != null)
