8abc2970a2e76769c817660cd36ef90bf532b1ad
LongCat is Looong <31859944+LongCatIsLooong@users.noreply.github.com>
2020-04-03 11:25:56 -0700

    update test

--- packages/flutter/test/services/autofill_test.dart
+++ packages/flutter/test/services/autofill_test.dart
@@ -5,8 +5,6 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'text_input_test.dart' show FakeTextChannel, FakeTextInputClient;
-
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
 
@@ -74,8 +72,8 @@ void main() {
   });
 }
 
-class FakeAutofillClient extends FakeTextInputClient with AutofillClientMixin implements AutofillTrigger {
-  FakeAutofillClient(TextEditingValue currentTextEditingValue): super(currentTextEditingValue);
+class FakeAutofillClient with AutofillClientMixin implements AutofillTrigger {
+  FakeAutofillClient(this.currentTextEditingValue);
 
   @override
   TextInputConfiguration textInputConfiguration;
@@ -83,6 +81,7 @@ class FakeAutofillClient extends FakeTextInputClient with AutofillClientMixin im
   @override
   void updateEditingValue(TextEditingValue newEditingValue) {
     currentTextEditingValue = newEditingValue;
+    latestMethodCall = 'updateEditingValue';
   }
 
   @override
@@ -90,9 +89,90 @@ class FakeAutofillClient extends FakeTextInputClient with AutofillClientMixin im
 
   @override
   TextInputConfiguration get configuration => textInputConfiguration;
+
+  String latestMethodCall = '';
+
+  @override
+  TextEditingValue currentTextEditingValue;
+
+  @override
+  void performAction(TextInputAction action) {
+    latestMethodCall = 'performAction';
+  }
+
+  @override
+  void updateFloatingCursor(RawFloatingCursorPoint point) {
+    latestMethodCall = 'updateFloatingCursor';
+  }
+
+  @override
+  void connectionClosed() {
+    latestMethodCall = 'connectionClosed';
+  }
 }
 
 class FakeAutofillScope with AutofillScopeMixin implements AutofillScope {
   @override
   List<AutofillClient> autofillClients;
+}
+
+class FakeTextChannel implements MethodChannel {
+  FakeTextChannel(this.outgoing) : assert(outgoing != null);
+
+  Future<dynamic> Function(MethodCall) outgoing;
+  Future<void> Function(MethodCall) incoming;
+
+  List<MethodCall> outgoingCalls = <MethodCall>[];
+
+  @override
+  BinaryMessenger get binaryMessenger => throw UnimplementedError();
+
+  @override
+  MethodCodec get codec => const JSONMethodCodec();
+
+  @override
+  Future<List<T>> invokeListMethod<T>(String method, [dynamic arguments]) => throw UnimplementedError();
+
+  @override
+  Future<Map<K, V>> invokeMapMethod<K, V>(String method, [dynamic arguments]) => throw UnimplementedError();
+
+  @override
+  Future<T> invokeMethod<T>(String method, [dynamic arguments]) async {
+    final MethodCall call = MethodCall(method, arguments);
+    outgoingCalls.add(call);
+    return await outgoing(call) as T;
+  }
+
+  @override
+  String get name => 'flutter/textinput';
+
+  @override
+  void setMethodCallHandler(Future<void> Function(MethodCall call) handler) {
+    incoming = handler;
+  }
+
+  @override
+  void setMockMethodCallHandler(Future<void> Function(MethodCall call) handler)  => throw UnimplementedError();
+
+  void validateOutgoingMethodCalls(List<MethodCall> calls) {
+    expect(outgoingCalls.length, calls.length);
+    bool hasError = false;
+    for (int i = 0; i < calls.length; i++) {
+      final ByteData outgoingData = codec.encodeMethodCall(outgoingCalls[i]);
+      final ByteData expectedData = codec.encodeMethodCall(calls[i]);
+      final String outgoingString = utf8.decode(outgoingData.buffer.asUint8List());
+      final String expectedString = utf8.decode(expectedData.buffer.asUint8List());
+
+      if (outgoingString != expectedString) {
+        print(
+          'Index $i did not match:\n'
+          '  actual:   ${outgoingCalls[i]}\n'
+          '  expected: ${calls[i]}');
+        hasError = true;
+      }
+    }
+    if (hasError) {
+      fail('Calls did not match.');
+    }
+  }
 }
