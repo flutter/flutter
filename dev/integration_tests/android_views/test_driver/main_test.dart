e46646bedfc1f7bbd3fd608e66acd8603e0cfb1f
Amir Hardon <amirh@users.noreply.github.com>
2020-04-04 16:41:01 -0700

    revive the android_views test (#53963)

--- dev/integration_tests/android_views/test_driver/main_test.dart
+++ dev/integration_tests/android_views/test_driver/main_test.dart
@@ -3,7 +3,6 @@
 // found in the LICENSE file.
 
 import 'dart:async';
-import 'dart:io' show Platform;
 import 'package:flutter_driver/flutter_driver.dart';
 import 'package:test/test.dart' hide TypeMatcher, isInstanceOf;
 
@@ -20,14 +19,12 @@ Future<void> main() async {
 
   group('MotionEvents tests ', () {
     test('recomposition', () async {
-      if (Platform.isAndroid) {
-        final SerializableFinder motionEventsListTile =
-        find.byValueKey('MotionEventsListTile');
-        await driver.tap(motionEventsListTile);
-        await driver.waitFor(find.byValueKey('PlatformView'));
-        final String errorMessage = await driver.requestData('run test');
-        expect(errorMessage, '');
-      }
+      final SerializableFinder motionEventsListTile =
+      find.byValueKey('MotionEventsListTile');
+      await driver.tap(motionEventsListTile);
+      await driver.waitFor(find.byValueKey('PlatformView'));
+      final String errorMessage = await driver.requestData('run test');
+      expect(errorMessage, '');
     });
   });
 }
