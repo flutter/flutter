9ecdb10779e12957ee776f4ec82caaee93d24c9c
Amir Hardon <amirh@users.noreply.github.com>
2020-04-04 17:15:56 -0700

    Revert "revive the android_views test (#53963)"
    This reverts commit f0aea666d7d911f7d9c741f95ee94b6b53ebc6e4.

--- dev/integration_tests/android_views/test_driver/main_test.dart
+++ dev/integration_tests/android_views/test_driver/main_test.dart
@@ -3,6 +3,7 @@
 // found in the LICENSE file.
 
 import 'dart:async';
+import 'dart:io' show Platform;
 import 'package:flutter_driver/flutter_driver.dart';
 import 'package:test/test.dart' hide TypeMatcher, isInstanceOf;
 
@@ -19,12 +20,14 @@ Future<void> main() async {
 
   group('MotionEvents tests ', () {
     test('recomposition', () async {
-      final SerializableFinder motionEventsListTile =
-      find.byValueKey('MotionEventsListTile');
-      await driver.tap(motionEventsListTile);
-      await driver.waitFor(find.byValueKey('PlatformView'));
-      final String errorMessage = await driver.requestData('run test');
-      expect(errorMessage, '');
+      if (Platform.isAndroid) {
+        final SerializableFinder motionEventsListTile =
+        find.byValueKey('MotionEventsListTile');
+        await driver.tap(motionEventsListTile);
+        await driver.waitFor(find.byValueKey('PlatformView'));
+        final String errorMessage = await driver.requestData('run test');
+        expect(errorMessage, '');
+      }
     });
   });
 }
