ce1f799b7e3a4cdac303ee714aa8f3730a85ee5d
Jonah Williams <campfish91@gmail.com>
2020-04-06 10:26:23 -0700

    fix more spacing

--- packages/flutter_tools/test/general.shard/license_collector_test.dart
+++ packages/flutter_tools/test/general.shard/license_collector_test.dart
@@ -46,6 +46,7 @@ const String _kApacheLicense = r'''
 
       "License" shall mean the terms and conditions for use, reproduction,
       and distribution as defined by Sections 1 through 9 of this document.
+
       "Licensor" shall mean the copyright owner or entity authorized by
       the copyright owner that is granting the License.
 
@@ -59,6 +60,7 @@ const String _kApacheLicense = r'''
 
       "You" (or "Your") shall mean an individual or Legal Entity
       exercising permissions granted by this License.
+
       "Source" form shall mean the preferred form for making modifications,
       including but not limited to software source code, documentation
       source, and configuration files.
@@ -94,6 +96,7 @@ const String _kApacheLicense = r'''
       Licensor for the purpose of discussing and improving the Work, but
       excluding communication that is conspicuously marked or otherwise
       designated in writing by the copyright owner as "Not a Contribution."
+
       "Contributor" shall mean Licensor and any individual or Legal Entity
       on behalf of whom a Contribution has been received by Licensor and
       subsequently incorporated within the Work.
