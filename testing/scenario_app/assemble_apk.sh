1b48463bf31a73c1b1b7d0ab36a718649a921af2
Emmanuel Garcia <egarciad@google.com>
2020-04-08 08:40:04 -0700

    Change the directory to the scenario app (#17583)

--- testing/scenario_app/assemble_apk.sh
+++ testing/scenario_app/assemble_apk.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-"${BASH_SOURCE%/*}/compile_android_aot.sh" $1 $2
+pushd "${BASH_SOURCE%/*}"
+  ./compile_android_aot.sh "$1" "$2"
+popd
 
 pushd "${BASH_SOURCE%/*}/android"
 ./gradlew assembleDebug --no-daemon
