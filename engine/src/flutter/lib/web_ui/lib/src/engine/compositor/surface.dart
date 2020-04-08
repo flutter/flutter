fc74a96f37717ddf440aba6db6bc231e0443b33a
Harry Terkelsen <hterkelsen@users.noreply.github.com>
2020-04-08 11:55:23 -0700

    Do not enable antialiasing by default in CanvasKit mode (flutter/engine#17534)
    * Do not enable antialiasing by default in CanvasKit mode
    
    * Add comment

--- engine/src/flutter/lib/web_ui/lib/src/engine/compositor/surface.dart
+++ engine/src/flutter/lib/web_ui/lib/src/engine/compositor/surface.dart
@@ -114,8 +114,12 @@ class Surface {
       ..position = 'absolute'
       ..width = '${logicalSize.width.ceil()}px'
       ..height = '${logicalSize.height.ceil()}px';
-    final int glContext = canvasKit
-        .callMethod('GetWebGLContext', <html.CanvasElement>[htmlCanvas]);
+    final int glContext = canvasKit.callMethod('GetWebGLContext', <dynamic>[
+      htmlCanvas,
+      // Default to no anti-aliasing. Paint commands can be explicitly
+      // anti-aliased by setting their `Paint` object's `antialias` property.
+      js.JsObject.jsify({'antialias': 0}),
+    ]);
     final js.JsObject grContext =
         canvasKit.callMethod('MakeGrContext', <dynamic>[glContext]);
     final js.JsObject skSurface =
