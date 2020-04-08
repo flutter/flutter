74cedd3373e8d0b70a29b79a9c2a4d783618fbc2
Gary Qian <garyq@google.com>
2020-04-08 15:42:02 -0700

    Add missing properties to TextStyle.apply (#54305)

--- packages/flutter/test/painting/text_style_test.dart
+++ packages/flutter/test/painting/text_style_test.dart
@@ -233,6 +233,11 @@ void main() {
 
     final ui.TextStyle uis1 = s2.getTextStyle();
     expect(uis1.toString(), 'TextStyle(color: unspecified, decoration: unspecified, decorationColor: unspecified, decorationStyle: unspecified, decorationThickness: unspecified, fontWeight: unspecified, fontStyle: unspecified, textBaseline: unspecified, fontFamily: foo, fontFamilyFallback: [Roboto, test], fontSize: unspecified, letterSpacing: unspecified, wordSpacing: unspecified, height: unspecified, locale: unspecified, background: unspecified, foreground: unspecified, shadows: unspecified, fontFeatures: unspecified)');
+
+    expect(s2.apply().fontFamily, 'foo');
+    expect(s2.apply().fontFamilyFallback, const <String>['Roboto', 'test']);
+    expect(s2.apply(fontFamily: 'bar').fontFamily, 'bar');
+    expect(s2.apply(fontFamilyFallback: const <String>['Banana']).fontFamilyFallback, const <String>['Banana']);
   }, skip: isBrowser);
 
   test('TextStyle.debugLabel', () {
@@ -366,5 +371,17 @@ void main() {
     final ui.ParagraphStyle paragraphStyle1 = style1.getParagraphStyle(textScaleFactor: 1);
 
     expect(paragraphStyle0 == paragraphStyle1, true);
+  });
+
+  test('TextStyle apply', () {
+    const TextStyle style = TextStyle(fontSize: 10);
+    expect(style.apply().shadows, isNull);
+    expect(style.apply(shadows: const <ui.Shadow>[ui.Shadow(blurRadius: 2.0)]).shadows, const <ui.Shadow>[ui.Shadow(blurRadius: 2.0)]);
+    expect(style.apply().fontStyle, isNull);
+    expect(style.apply(fontStyle: FontStyle.italic).fontStyle, FontStyle.italic);
+    expect(style.apply().locale, isNull);
+    expect(style.apply(locale: const Locale.fromSubtags(languageCode: 'es')).locale, const Locale.fromSubtags(languageCode: 'es'));
+    expect(style.apply().fontFeatures, isNull);
+    expect(style.apply(fontFeatures: const <ui.FontFeature>[ui.FontFeature.enable('test')]).fontFeatures, const <ui.FontFeature>[ui.FontFeature.enable('test')]);
   });
 }
