b12892d0c35a1487e3727090667dc1c2e795f05b
Jacob MacDonald <jakemac@google.com>
2020-04-08 12:46:57 -0700

    some additional fixes

--- packages/flutter_test/lib/src/_goldens_io.dart
+++ packages/flutter_test/lib/src/_goldens_io.dart
@@ -146,7 +146,7 @@ class LocalComparisonOutput {
         output.parent.createSync(recursive: true);
         final ByteData pngBytes =
             await entry.value.toByteData(format: ImageByteFormat.png);
-        output.writeAsBytesSync(Uint8List.sublistView(pngBytes));
+        output.writeAsBytesSync(pngBytes.buffer.asUint8List());
       }
     }
     throw test_package.TestFailure(
@@ -228,7 +228,11 @@ Future<ComparisonResult> compareLists(List<int> test, List<int> master) async {
       if (diffPixel != 0 ) {
         final int invertedMasterPixel = invertedMasterRgba.getUint32(byteOffset);
         final int invertedTestPixel = invertedTestRgba.getUint32(byteOffset);
-        final int maskPixel = math.max(invertedMasterPixel, invertedTestPixel);
+        // We grab the max of the 0xAABBGGRR encoded bytes, and then convert
+        // back to 0xRRGGBBAA for the actual pixel value, since this is how it
+        // was historically done.
+        final int maskPixel = _toRGBA(
+            math.max(_toABGR(invertedMasterPixel), _toABGR(invertedTestPixel)));
         maskedDiffRgba.setUint32(byteOffset, maskPixel);
         isolatedDiffRgba.setUint32(byteOffset, maskPixel);
         pixelDiffCount++;
@@ -261,6 +265,7 @@ ByteData _invert(ByteData imageBytes) {
     bytes.setUint8(i, 255 - imageBytes.getUint8(i));
     bytes.setUint8(i + 1, 255 - imageBytes.getUint8(i + 1));
     bytes.setUint8(i + 2, 255 - imageBytes.getUint8(i + 2));
+    bytes.setUint8(i + 3, imageBytes.getUint8(i + 3));
   }
   return bytes;
 }
@@ -279,13 +284,13 @@ class DefaultWebGoldenComparator extends WebGoldenComparator {
 }
 
 /// Reads the red value out of a 32 bit rgba pixel.
-int _readRed(int pixel) => pixel >> 24 & 0xff;
+int _readRed(int pixel) => (pixel >> 24) & 0xff;
 
 /// Reads the green value out of a 32 bit rgba pixel.
-int _readGreen(int pixel) => pixel >> 16 & 0xff;
+int _readGreen(int pixel) => (pixel >> 16) & 0xff;
 
 /// Reads the blue value out of a 32 bit rgba pixel.
-int _readBlue(int pixel) => pixel >> 8 & 0xff;
+int _readBlue(int pixel) => (pixel >> 8) & 0xff;
 
 /// Reads the alpha value out of a 32 bit rgba pixel.
 int _readAlpha(int pixel) => pixel & 0xff;
@@ -294,7 +299,19 @@ int _readAlpha(int pixel) => pixel & 0xff;
 Future<Image> _createImage(ByteData bytes, int width, int height) {
   final Completer<Image> completer = Completer<Image>();
   decodeImageFromPixels(
-      Uint8List.sublistView(bytes), width, height, PixelFormat.rgba8888,
+      bytes.buffer.asUint8List(), width, height, PixelFormat.rgba8888,
       completer.complete);
   return completer.future;
 }
+
+// Converts a 32 bit rgba pixel to a 32 bit abgr pixel
+int _toABGR(int rgba) =>
+    (_readAlpha(rgba) << 24) |
+    (_readBlue(rgba) << 16) |
+    (_readGreen(rgba) << 8) |
+    _readRed(rgba);
+
+// Converts a 32 bit abgr pixel to a 32 bit rgba pixel
+int _toRGBA(int abgr) =>
+  // This is just a mirror of the other conversion.
+  _toABGR(abgr);
