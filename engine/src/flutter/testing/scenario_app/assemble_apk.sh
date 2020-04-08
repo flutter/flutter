227202102330e5864676d5527d4aa25b004ea32c
Dan Field <dnfield@google.com>
2020-04-08 14:52:14 -0700

    set -e on assemble_apk.sh (flutter/engine#17588)
    * set -e on assemble_apk.sh
    
    * build the necessary artifacts

--- engine/src/flutter/testing/scenario_app/assemble_apk.sh
+++ engine/src/flutter/testing/scenario_app/assemble_apk.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+set -e
+
+pushd "${BASH_SOURCE%/*}/../../.."
+  ./flutter/tools/gn --unopt
+  ninja -C out/host_debug_unopt sky_engine sky_services
+popd
+
 pushd "${BASH_SOURCE%/*}"
   ./compile_android_aot.sh "$1" "$2"
 popd
