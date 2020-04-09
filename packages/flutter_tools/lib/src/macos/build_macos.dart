f5d82b556f64736d48fe4446c0759a04a6dd9393
Jonah Williams <jonahwilliams@google.com>
2020-04-09 09:00:44 -0700

    [flutter_tools] make verbose macOS builds actually verbose (#54320)

--- packages/flutter_tools/lib/src/macos/build_macos.dart
+++ packages/flutter_tools/lib/src/macos/build_macos.dart
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'package:meta/meta.dart';
+
 import '../base/common.dart';
 import '../base/file_system.dart';
 import '../base/logger.dart';
@@ -18,6 +20,7 @@ Future<void> buildMacOS({
   FlutterProject flutterProject,
   BuildInfo buildInfo,
   String targetOverride,
+  @required bool verboseLogging,
 }) async {
   if (!flutterProject.macos.xcodeWorkspace.existsSync()) {
     throwToolExit('No macOS desktop project configured. '
@@ -83,6 +86,8 @@ Future<void> buildMacOS({
       '-derivedDataPath', flutterBuildDir.absolute.path,
       'OBJROOT=${globals.fs.path.join(flutterBuildDir.absolute.path, 'Build', 'Intermediates.noindex')}',
       'SYMROOT=${globals.fs.path.join(flutterBuildDir.absolute.path, 'Build', 'Products')}',
+      if (verboseLogging)
+        'VERBOSE_SCRIPT_LOGGING=YES',
       'COMPILER_INDEX_STORE_ENABLE=NO',
       ...environmentVariablesAsXcodeBuildSettings(globals.platform)
     ], trace: true);
