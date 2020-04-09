7968a927909330cd02227112fa77ea5554e893dd
Georg Wechslberger <wechslbe@ma.tum.de>
2020-04-09 19:31:12 +0200

    added test to build ios app with flutter companion for simulator

--- dev/devicelab/bin/tasks/ios_app_with_watch_companion.dart
+++ dev/devicelab/bin/tasks/ios_app_with_watch_companion.dart
@@ -9,6 +9,8 @@ Future<void> main() async {
   await task(() async {
     section('Copy test Flutter App with WatchOS Companion');
 
+    String watchDeviceID = '';
+    String phoneDeviceID = '';
     final Directory tempDir = Directory.systemTemp
         .createTempSync('ios_app_with_watch_companion_test');
     final Directory projectDir =
@@ -40,13 +42,114 @@ Future<void> main() async {
 
       if (!appReleaseBuilt) {
         return TaskResult.failure(
-            'Failed to build flutter iOS app with WatchOS companion.');
+            'Failed to build flutter iOS app with WatchOS companion in release mode.');
       }
+
+      section('Create debug build');
+
+      await inDirectory(projectDir, () async {
+        await flutter(
+          'build',
+          options: <String>['ios', '--debug', '--no-codesign'],
+        );
+      });
+
+      final bool appDebugBuilt = exists(Directory(path.join(
+        projectDir.path,
+        'build',
+        'ios',
+        'iphoneos',
+        'Runner.app',
+      )));
+
+      if (!appDebugBuilt) {
+        return TaskResult.failure(
+            'Failed to build flutter iOS app with WatchOS companion in debug mode.');
+      }
+
+      section('Create build for a simulator device');
+
+      // Create iOS simulator devices.
+      phoneDeviceID = await eval(
+        'xcrun',
+        <String>[
+          'simctl',
+          'create',
+          'TestFlutteriPhoneWithWatch',
+          'com.apple.CoreSimulator.SimDeviceType.iPhone-11',
+          'com.apple.CoreSimulator.SimRuntime.iOS-13-2'
+        ],
+        canFail: false,
+        workingDirectory: flutterDirectory.path,
+      );
+
+      watchDeviceID = await eval(
+        'xcrun',
+        <String>[
+          'simctl',
+          'create',
+          'TestFlutterWatch',
+          'com.apple.CoreSimulator.SimDeviceType.Apple-Watch-Series-5-44mm',
+          'com.apple.CoreSimulator.SimRuntime.watchOS-6-1'
+        ],
+        canFail: false,
+        workingDirectory: flutterDirectory.path,
+      );
+
+      // Pair watch with phone.
+      await eval(
+        'xcrun',
+        <String>['simctl', 'pair', watchDeviceID, phoneDeviceID],
+        canFail: false,
+        workingDirectory: flutterDirectory.path,
+      );
+
+      await inDirectory(projectDir, () async {
+        await flutter(
+          'build',
+          options: <String>[
+            'ios',
+            '--debug',
+            '--no-codesign',
+            '-d',
+            phoneDeviceID
+          ],
+        );
+      });
+
+      final bool appSimulatorBuilt = exists(Directory(path.join(
+        projectDir.path,
+        'build',
+        'ios',
+        'iphoneos',
+        'Runner.app',
+      )));
+
+      if (!appSimulatorBuilt) {
+        return TaskResult.failure(
+            'Failed to build flutter iOS app with WatchOS companion in debug mode for simulated device.');
+      }
+
       return TaskResult.success(null);
     } catch (e) {
       return TaskResult.failure(e.toString());
     } finally {
       rmTree(tempDir);
+      // Delete simulator devices
+      if (watchDeviceID != '')
+        await eval(
+          'xcrun',
+          <String>['simctl', 'delete', phoneDeviceID],
+          canFail: true,
+          workingDirectory: flutterDirectory.path,
+        );
+      if (phoneDeviceID != '')
+        await eval(
+          'xcrun',
+          <String>['simctl', 'delete', watchDeviceID],
+          canFail: true,
+          workingDirectory: flutterDirectory.path,
+        );
     }
   });
 }
