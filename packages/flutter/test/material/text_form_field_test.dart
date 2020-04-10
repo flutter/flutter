27f7e8e7701e901b06de200d88392a84d25cd35b
Christian Mürtz <christian.muertz@t-online.de>
2020-04-10 21:38:58 +0200

    Make TextFormFieldState.didChange change the text fields value

--- packages/flutter/test/material/text_form_field_test.dart
+++ packages/flutter/test/material/text_form_field_test.dart
@@ -331,4 +331,26 @@ void main() {
     await tester.pump(const Duration(milliseconds: 300));
     expect(tapCount, 3);
   });
+
+  testWidgets('didChange changes text fields value', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Material(
+          child: Center(
+            child: TextFormField(
+              initialValue: 'initialValue',
+            ),
+          ),
+        ),
+      )
+    );
+
+    expect(find.text('initialValue'), findsOneWidget);
+
+    final FormFieldState<String> state = tester.state<FormFieldState<String>>(find.byType(TextFormField));
+    state.didChange('changedValue');
+
+    expect(find.text('initialValue'), findsNothing);
+    expect(find.text('changedValue'), findsOneWidget);
+  });
 }
