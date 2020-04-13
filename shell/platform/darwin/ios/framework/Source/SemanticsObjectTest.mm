61ec840edd01e0c7e2be505f4f5d7fefb3c448c3
gaaclarke <30870216+gaaclarke@users.noreply.github.com>
2020-04-13 12:34:54 -0700

    Made it so unit tests can be written against all ios engine code. (#17624)

---
+++ shell/platform/darwin/ios/framework/Source/SemanticsObjectTest.mm
@@ -0,0 +1,41 @@
+#import <OCMock/OCMock.h>
+#import <XCTest/XCTest.h>
+
+#include "flutter/shell/platform/darwin/common/framework/Headers/FlutterMacros.h"
+#import "flutter/shell/platform/darwin/ios/framework/Source/SemanticsObject.h"
+
+FLUTTER_ASSERT_ARC
+
+namespace flutter {
+namespace {
+class MockAccessibilityBridge : public AccessibilityBridgeIos {
+ public:
+  MockAccessibilityBridge() { view_ = [[UIView alloc] init]; }
+  UIView* view() const override { return view_; }
+  UIView<UITextInput>* textInputView() override { return nil; }
+  void DispatchSemanticsAction(int32_t id, SemanticsAction action) override {}
+  void DispatchSemanticsAction(int32_t id,
+                               SemanticsAction action,
+                               std::vector<uint8_t> args) override {}
+  FlutterPlatformViewsController* GetPlatformViewsController() const override { return nil; }
+
+ private:
+  UIView* view_;
+};
+}  // namespace
+}  // namespace flutter
+
+@interface SemanticsObjectTest : XCTestCase
+@end
+
+@implementation SemanticsObjectTest
+
+- (void)testCreate {
+  fml::WeakPtrFactory<flutter::AccessibilityBridgeIos> factory(
+      new flutter::MockAccessibilityBridge());
+  fml::WeakPtr<flutter::AccessibilityBridgeIos> bridge = factory.GetWeakPtr();
+  SemanticsObject* object = [[SemanticsObject alloc] initWithBridge:bridge uid:0];
+  XCTAssertNotNil(object);
+}
+
+@end
