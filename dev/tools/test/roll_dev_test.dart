38886c7dd8e2ab4a329bf26ad3d0dd06a2fb1984
Christopher Fujino <christopherfujino@gmail.com>
2020-04-14 14:35:04 -0700

    wip

--- dev/tools/test/roll_dev_test.dart
+++ dev/tools/test/roll_dev_test.dart
@@ -9,16 +9,18 @@ void main() {
   group('parseFullTag', () {
     test('returns match on valid version input', () {
       final List<String> validTags = <String>[
-        '1.2.3-dev.1.2-3-gabc123',
-        'v1.2.3',
+        '1.2.3-1.2.pre-3-gabc123',
+        '10.2.30-12.22.pre-45-gabc123',
       ];
       for (final String validTag in validTags) {
         final Match match = parseFullTag(validTag);
         expect(match, isNotNull);
       }
     });
+
     test('returns null on invalid version input', () {
       final List<String> validTags = <String>[
+        '1.2.3-dev.1.2-3-gabc123',
         '1.2.3-1.2-3-gabc123',
         'v1.2.3',
         '1.2.3',
@@ -27,6 +29,16 @@ void main() {
         final Match match = parseFullTag(validTag);
         expect(match, null);
       }
+    });
+  });
+
+  group('getVersionFromParts', () {
+    test('returns correct string from valid parts', () {
+      List<int> parts = <int>[1, 2, 3, 4, 5];
+      expect(getVersionFromParts(parts), '1.2.3-4.5.pre');
+
+      parts = <int>[11, 2, 33, 1, 0];
+      expect(getVersionFromParts(parts), '11.2.33-1.0.pre');
     });
   });
 }
