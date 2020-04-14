9cc69d47a5a97fc5b7aea8dd08b84d3d322714c5
Jonah Williams <jonahwilliams@google.com>
2020-04-13 19:00:02 -0700

    [flutter_tools] support machine and coverage together but for real (#54692)

--- packages/flutter_tools/lib/src/test/watcher.dart
+++ packages/flutter_tools/lib/src/test/watcher.dart
@@ -17,14 +17,14 @@ abstract class TestWatcher {
   ///
   /// The child process won't exit until this method completes.
   /// Not called if the process died.
-  Future<void> handleFinishedTest(ProcessEvent event) async { }
+  Future<void> handleFinishedTest(ProcessEvent event);
 
   /// Called when the test process crashed before connecting to test harness.
-  Future<void> handleTestCrashed(ProcessEvent event) async { }
+  Future<void> handleTestCrashed(ProcessEvent event);
 
   /// Called if we timed out waiting for the test process to connect to test
   /// harness.
-  Future<void> handleTestTimedOut(ProcessEvent event) async { }
+  Future<void> handleTestTimedOut(ProcessEvent event);
 }
 
 /// Describes a child process started during testing.
