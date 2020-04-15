492917d0b684264b5af6ebd451321e71c8b5a6ed
Christopher Fujino <christopherfujino@gmail.com>
2020-04-15 15:15:12 -0700

    add additional test cases

--- dev/tools/test/roll_dev_test.dart
+++ dev/tools/test/roll_dev_test.dart
@@ -15,6 +15,7 @@ void main() {
         '2.0.0-1.99.pre-45-gf0adb240a',
         '12.34.56-78.90.pre-12-g9db2703a2',
         '0.0.1-0.0.pre-1-g07601eb95ff82f01e870566586340ed2e87b9cbb',
+        '958.80.144-6.224.pre-7803-g06e90',
       ];
       for (final String validTag in validTags) {
         final Match match = parseFullTag(validTag);
@@ -32,6 +33,7 @@ void main() {
         '10.0.1-0.0.pre-gf0adb240a',
         '10.0.1-0.0.pre-3-gggggggggg',
         '1.2.3-1.2.pre-3-abc123',
+        '1.2.3-1.2.pre-3-gabc123_',
       ];
       for (final String invalidTag in invalidTags) {
         final Match match = parseFullTag(invalidTag);
