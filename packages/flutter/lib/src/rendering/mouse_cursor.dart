30271c7c550417e96d09f17099f0276b6fa77dfc
Tong Mu <dkwingsmt@gmail.com>
2020-04-15 19:52:56 -0700

    More doc changes

--- packages/flutter/lib/src/rendering/mouse_cursor.dart
+++ packages/flutter/lib/src/rendering/mouse_cursor.dart
@@ -19,22 +19,22 @@ import 'mouse_tracking.dart';
 /// specified than the one that's currently active. If no cursors are associated
 /// with a position, it defaults to [SystemMouseCursors.basic].
 ///
-/// A [MouseCursor] object may contain the full resources that are ready to be
-/// consumed by the system (in which case it should subclass
-/// [PreparedMouseCursor]), or might contain a full specification and need to
-/// collect resources before being converted to a [PreparedMouseCursor].
+/// A [MouseCursor] object may contain the full resources, ready to be consumed
+/// by the system (in which case it should subclass [PreparedMouseCursor]), or it
+/// might contain a full specification and need to collect resources before being
+/// converted to a [PreparedMouseCursor].
 ///
 /// ## Cursor classes
 ///
-/// [SystemMouseCursor] are prepared cursors that are natively supported by the
+/// A [SystemMouseCursor] is a prepared cursor that is natively supported by the
 /// platform that the program is running on, and is the most common kind of
 /// cursor. All supported system mouse cursors are enumerated in
 /// [SystemMouseCursors].
 ///
-/// [NoopMouseCursor] are prepared cursors that do nothing when switched onto. It
-/// is useful in special cases such as a platform view where the mouse cursor is
-/// managed by other means. Its singleton instance is available at
-/// [SystemMouseCursors.uncontrolled].
+/// A [NoopMouseCursor] ia a a prepared cursor that keeps the current cursor when
+/// activated. It is useful in special cases such as a platform view where the
+/// mouse cursor is managed by other means. Its singleton instance is available
+/// at [SystemMouseCursors.uncontrolled].
 ///
 /// ## Using cursors
 ///
@@ -45,7 +45,7 @@ import 'mouse_tracking.dart';
 /// {@tool snippet --template=stateless_widget_material}
 /// This sample creates a rectangular region that is wrapped by a [MouseRegion]
 /// with a system mouse cursor. The mouse pointer becomes an I-beam when
-/// hovering the region.
+/// hovering over the region.
 ///
 /// ```dart imports
 /// import 'package:flutter/widgets.dart';
@@ -72,7 +72,7 @@ import 'mouse_tracking.dart';
 /// {@end-tool}
 ///
 /// Assigning regions with mouse cursors on platforms that do not support mouse
-/// cursors, or when there are no mouse connected, will result in no-op.
+/// cursors, or when there are no mice connected, will have no effect.
 ///
 /// ## Related classes
 ///
@@ -87,26 +87,24 @@ import 'mouse_tracking.dart';
 ///
 /// See also:
 ///
-///  * [MouseRegion], which is a common way of assigning a region with a
-///    [MouseCursor].
-///  * [MouseTracker], which determines the cursor that each device should show,
-///    and dispatches the changing callbacks.
-///  * [SystemMouseCursors], which enumerates supported system cursors.
+///  * [MouseRegion], a widget used to associate a [MouseCursor] with a region.
+///  * [MouseTracker], a class that determines the cursor that each device should
+///    show, and dispatches the changing callbacks.
+///  * [SystemMouseCursors], a class that enumerates supported system cursors.
 @immutable
 abstract class MouseCursor with Diagnosticable {
   /// Abstract const constructor. This constructor enables subclasses to provide
   /// const constructors so that they can be used in const expressions.
   const MouseCursor();
 
-  /// A very short pretty description of the mouse cursor.
+  /// A very short description of the mouse cursor.
   ///
   /// The [debugDescription] shoule be a few words that can differentiate
   /// instances of a class to make debug information more readable. For example,
   /// a [SystemMouseCursor] class with description "drag" will be printed as
   /// "SystemMouseCursor(drag)".
   ///
-  /// The [debugDescription] must not be null, but can be an empty string,
-  /// which means the class is not configurable.
+  /// The [debugDescription] must not be null, but can be an empty string.
   String get debugDescription;
 
   @override
@@ -119,11 +117,11 @@ abstract class MouseCursor with Diagnosticable {
 }
 
 /// An interface for mouse cursors that have all resources prepared and ready to
-/// be sent to the system.
+/// be used by the operating system.
 ///
 /// Although [PreparedMouseCursor] adds no changes on top of [MouseCursor], this
-/// class is designed to prevent unprepared cursor types from methods that
-/// directly talk to the system.
+/// class is designed to prevent unprepared cursor types from accessing methods
+/// that directly talk to the system.
 abstract class PreparedMouseCursor extends MouseCursor {
   /// Abstract const constructor. This constructor enables subclasses to provide
   /// const constructors so that they can be used in const expressions.
@@ -134,8 +132,7 @@ abstract class PreparedMouseCursor extends MouseCursor {
 ///
 /// The singleton instance of this class is available at
 /// [SystemMouseCursors.uncontrolled], which also introduces its usage.
-/// Directly instantiating this class is unnecessary and thus not allowed, since
-/// it's not configurable.
+/// Directly instantiating this class is not allowed.
 class NoopMouseCursor extends PreparedMouseCursor {
   // Application code shouldn't directly instantiate this class, since its only
   // instance is accessible at [SystemMouseCursors.releaseControl].
@@ -145,21 +142,23 @@ class NoopMouseCursor extends PreparedMouseCursor {
   String get debugDescription => '';
 }
 
-/// A mouse cursor that comes with the system that the program is running on.
+/// A mouse cursor that is standard on the platform that the application is
+/// running on.
 ///
 /// System cursors are the most commonly used cursors, since they are available
-/// without external resources, and matches the experience of native apps.
-/// Examples of system cursors are a pointing arrow, a pointing hand, a double
-/// arrow for resizing, or a text I-beam, etc.
+/// without external resources and match the experience of native apps. Examples
+/// of system cursors are a pointing arrow, a pointing hand, a double arrow for
+/// resizing, or a text I-beam, etc.
 /// 
 /// An instance of [SystemMouseCursor] refers to one cursor from each platform
-/// that represents the same concept, such as text, being clickable, or forbidden
-/// operation. Since the set of system cursors supported by each platform varies,
-/// multiple instances can correspond to the same system cursor.
+/// that represents the same concept, such as being text text, being clickable,
+/// or being a forbidden operation. Since the set of system cursors supported by
+/// each platform varies, multiple instances can correspond to the same system
+/// cursor.
 ///
 /// [SystemMouseCursors] enumerates the complete set of system cursors supported
-/// by Flutter, which are hard-coded in the platform engine. Therefore, manually
-/// instantiating this class is neither useful nor supported.
+/// by Flutter, which are hard-coded in the engine. Therefore, manually
+/// instantiating this class is not supported.
 class SystemMouseCursor extends PreparedMouseCursor {
   // Application code shouldn't directly instantiate system mouse cursors, since
   // the supported system cursors are enumerated in [SystemMouseCursors].
@@ -171,9 +170,7 @@ class SystemMouseCursor extends PreparedMouseCursor {
 
   /// A globally unique number that identifies the shape of the cursor.
   ///
-  /// The exact value of a [shapeCode] is intentionally random, and its
-  /// interpretation (i.e. corresponding system cursor) is hard-coded in the
-  /// platform engine.
+  /// A [shapeCode] is an opaque, platform-dependent value.
   ///
   /// See the documentation of [SystemMouseCursor] for introduction.
   final int shapeCode;
@@ -201,31 +198,30 @@ class SystemMouseCursor extends PreparedMouseCursor {
 
 /// A collection of system [MouseCursor]s.
 ///
-/// System cursors are mouse cursors provided by the current platform, available
-/// without external resources, and are therefore the most common cursors.
+/// System cursors are standard mouse cursors that are provided by the current
+/// platform. They don't require external resources.
 ///
 /// [SystemMouseCursors] is a superset of the system cursors of every platform
 /// that Flutter supports, therefore some of these objects might map to the same
-/// result, or fallback to the basic arrow. This mapping is implemented by the
-/// Flutter engine that the program is running on.
+/// result, or fallback to the basic arrow. This mapping is defined by the
+/// Flutter engine.
 /// 
 /// The cursor names are chosen to reflect the cursors' use cases instead of
-/// their shapes, because different platforms might (although not common) use
+/// their shapes, because different platforms might (although not commonly) use
 /// different shapes for the same use case.
 class SystemMouseCursors {
-  // This class only contains static members, and should not be initiated or
+  // This class only contains static members, and should not be instantiated or
   // extended.
   factory SystemMouseCursors._() => null;
 
   /// A special value that tells Flutter to release the control of cursors.
   ///
-  /// A region with this value will absorb the search for mouse cursor
-  /// configuration, but the pointer's cursor will not be changed when it enters
-  /// or is hovering this region. In other words, Flutter has released its
-  /// control over the mouse cursor.
+  /// A region with this value will absorb the mouse cursor hit test without
+  /// changing the pointer's cursor when it enters or is hovering over this
+  /// region.
   ///
-  /// This value is typically used on a platform view or another layer that
-  /// manages the cursor by itself.
+  /// This value is typically used on a platform view, which detects pointer
+  /// movement and changes cursors in a non-Flutter way.
   static const NoopMouseCursor uncontrolled = NoopMouseCursor._();
 
   // The `shapeCode` values are chosen as the first 6 bytes of the MD5 hash of the
@@ -234,11 +230,7 @@ class SystemMouseCursors {
   //
   // The `shapeCode` values must be kept in sync with the engine implementations.
 
-  /// Displays no cursor at the pointer. In other words, hide the cursor.
-  /// 
-  /// Setting cursor to [none] means Flutter is still taking the control.
-  /// If there is something else controlling the cursor and Flutter should not
-  /// overwrite, such as a platform view, use [uncontrolled],
+  /// Hide the cursor.
   static const SystemMouseCursor none = SystemMouseCursor._(shapeCode: 0x334c4a, debugDescription: 'none');
 
   /// The platform-dependent basic cursor.
