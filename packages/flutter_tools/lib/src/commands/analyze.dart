9d38fb184dc8eb9e07557c08142404b308ae75a4
Jonah Williams <jonahwilliams@google.com>
2020-04-18 21:08:55 -0700

    Update analyze.dart

--- packages/flutter_tools/lib/src/commands/analyze.dart
+++ packages/flutter_tools/lib/src/commands/analyze.dart
@@ -60,10 +60,10 @@ class AnalyzeCommand extends FlutterCommand {
         negatable: false,
         hide: !verboseHelp,
         help: 'Also output the analysis time.');
-    // Hidden option to allow mix non-nullable support.
+    // Hidden option to allow mixed non-nullable support.
     argParser.addFlag('permit-non-nullable',
         hide: true,
-        help: 'Do not report experiment not enabled errors'
+        help: 'Do not report experiment_not_enabled errors.'
     );
 
     usesPubOption();
