22d548a9a0ab4ccba74584150097456c8749ca66
Jim Graham <flar@google.com>
2020-04-20 14:12:44 -0700

    ignore .idea directory in dart analyzer and analyzer benchmark output

--- dev/bots/analyze.dart
+++ dev/bots/analyze.dart
@@ -1147,6 +1147,8 @@ Iterable<File> _allFiles(String workingDirectory, String extension, { @required 
         continue;
       if (path.basename(entity.path) == '.git')
         continue;
+      if (path.basename(entity.path) == '.idea')
+        continue;
       if (path.basename(entity.path) == '.gradle')
         continue;
       if (path.basename(entity.path) == '.dart_tool')
