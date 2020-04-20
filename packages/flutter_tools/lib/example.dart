10d218a63d0fcffc017db34818c0eb834e7e4488
Jonah Williams <jonahwilliams@google.com>
2020-04-20 11:10:47 -0700

    Delete example.dart

--- packages/flutter_tools/lib/example.dart
+++
@@ -1,8 +0,0 @@
-
-
-import 'dart:io';
-
-void main() {
-  print(File(r'C:\Users\Jonah\flutter\bin\cache\artifacts\material_fonts\MaterialIcons-Regular.ttf')
-    .readAsBytesSync().sublist(0, 12));
-}
