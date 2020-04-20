4f9d480cbbe064a1dde9e3e97f093b40f0883d4a
Jonah Williams <jonahwilliams@google.com>
2020-04-20 00:51:46 -0700

    Update vmservice.dart

--- packages/flutter_tools/lib/src/vmservice.dart
+++ packages/flutter_tools/lib/src/vmservice.dart
@@ -480,7 +480,7 @@ class VMService implements vm_service.VmService {
 
   @override
   Future<vm_service.Response> callServiceExtension(String method,
-      {String isolateId, Map<Sring, Object> args}) {
+      {String isolateId, Map<String, Object> args}) {
     return _delegateService.callServiceExtension(method, isolateId: isolateId, args: args);
   }
 
