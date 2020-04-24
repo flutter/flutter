b8da4709a76bdfdf2d0bf579325f6c8ac2e61aec
maksimka101 <zemlyanikin.max@yandex.ru>
2020-04-24 13:42:53 +0300

    Add license header to the host_validator.dart file.

--- packages/flutter_tools/lib/src/host_validator.dart
+++ packages/flutter_tools/lib/src/host_validator.dart
@@ -1,3 +1,7 @@
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import 'package:flutter_tools/src/doctor.dart';
 import 'package:flutter_tools/src/globals.dart';
 import 'package:http/http.dart' as http;
