c095d4058f28d3dd7ca10e865fd31405d58f97f6
Jonah Williams <jonahwilliams@google.com>
2020-04-23 17:59:21 -0700

    Update cache.dart

--- packages/flutter_tools/lib/src/cache.dart
+++ packages/flutter_tools/lib/src/cache.dart
@@ -1389,8 +1389,6 @@ const List<List<String>> _windowsDesktopBinaryDirs = <List<String>>[
 const List<List<String>> _linuxDesktopBinaryDirs = <List<String>>[
   <String>['linux-x64', 'linux-x64/linux-x64-flutter-glfw.zip'],
   <String>['linux-x64', 'linux-x64/flutter-cpp-client-wrapper-glfw.zip'],
-  <String>['linux-x64-profile', 'linux-x64-profile/linux-x64-flutter-glfw.zip'],
-  <String>['linux-x64-release', 'linux-x64-release/linux-x64-flutter-glfw.zip'],
 ];
 
 const List<List<String>> _macOSDesktopBinaryDirs = <List<String>>[
