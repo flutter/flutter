027eff8b19558bb022ab420a185f661fcd3deed1
godofredoc <54371434+godofredoc@users.noreply.github.com>
2020-04-27 15:14:04 -0700

    Reenable flutter scenic test to identify crashes and follow up on fixes. (#17979)

--- testing/fuchsia/run_tests.sh
+++ testing/fuchsia/run_tests.sh
@@ -77,15 +77,13 @@ echo "$(date) START:flutter_runner_tests ----------------------------"
     --timeout-seconds 300 \
     --packages-directory packages
 
-# TODO(https://bugs.fuchsia.dev/p/fuchsia/issues/detail?id=47081)
-# Re-enable once the crash is resolved
-#./fuchsia_ctl -d $device_name test \
-#    -f flutter_aot_runner-0.far    \
-#    -f flutter_runner_scenic_tests-0.far  \
-#    -t flutter_runner_scenic_tests \
-#    --identity-file $pkey \
-#    --timeout-seconds 300 \
-#    --packages-directory packages
+./fuchsia_ctl -d $device_name test \
+    -f flutter_aot_runner-0.far    \
+    -f flutter_runner_scenic_tests-0.far  \
+    -t flutter_runner_scenic_tests \
+    --identity-file $pkey \
+    --timeout-seconds 300 \
+    --packages-directory packages
 
 # TODO(https://github.com/flutter/flutter/issues/50032) Enable after the
 # Fuchsia message loop migration is complete.
