349f821271f53c26106d27823c028a8cb8dd6980
guylivneh <guyliv@google.com>
2020-04-27 23:25:39 +0300

    Update packages/flutter/test/material/icon_button_test.dart
    Co-Authored-By: rami-a <2364772+rami-a@users.noreply.github.com>

--- packages/flutter/test/material/icon_button_test.dart
+++ packages/flutter/test/material/icon_button_test.dart
@@ -358,7 +358,7 @@ void main() {
 
     final Offset center = tester.getCenter(find.byType(IconButton));
     final TestGesture gesture = await tester.startGesture(center);
-    await tester.pump(); // start gesture
+    await tester.pump(); // Start gesture.
     await tester.pump(const Duration(
         milliseconds: 1000)); // Wait for splash to be well under way.
 
