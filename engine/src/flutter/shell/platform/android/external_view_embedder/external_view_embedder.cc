cee86c7482b6173c434188652b5d18cf3ac9af01
Emmanuel Garcia <egarciad@google.com>
2020-04-26 20:45:28 -0700

    Add initial unit tests for the android embedding (flutter/engine#17921)

--- engine/src/flutter/shell/platform/android/external_view_embedder/external_view_embedder.cc
+++ engine/src/flutter/shell/platform/android/external_view_embedder/external_view_embedder.cc
@@ -2,9 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "flutter/shell/platform/android/android_external_view_embedder.h"
+#include "flutter/shell/platform/android/external_view_embedder/external_view_embedder.h"
 
-#include "fml/trace_event.h"
+#include "flutter/fml/trace_event.h"
 
 namespace flutter {
 
