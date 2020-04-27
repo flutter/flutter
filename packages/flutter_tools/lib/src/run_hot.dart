a18e6361cf7b5b56ddc8c13fbc871dd10348e00c
Jonah Williams <jonahwilliams@google.com>
2020-04-27 15:54:59 -0700

    [flutter_tools] reland migrate FlutterView to new vmservice (#55774)

--- packages/flutter_tools/lib/src/run_hot.dart
+++ packages/flutter_tools/lib/src/run_hot.dart
@@ -514,10 +514,8 @@ class HotRunner extends ResidentRunner {
     String reason,
     bool benchmarkMode = false,
   }) async {
-    if (!_isPaused()) {
-      globals.printTrace('Refreshing active FlutterViews before restarting.');
-      await refreshViews();
-    }
+    globals.printTrace('Refreshing active FlutterViews before restarting.');
+    await refreshViews();
 
     final Stopwatch restartTimer = Stopwatch()..start();
     // TODO(aam): Add generator reset logic once we switch to using incremental
@@ -542,31 +540,36 @@ class HotRunner extends ResidentRunner {
     // Check if the isolate is paused and resume it.
     final List<Future<void>> operations = <Future<void>>[];
     for (final FlutterDevice device in flutterDevices) {
-      final Set<Isolate> uiIsolates = <Isolate>{};
+      final Set<String> uiIsolatesIds = <String>{};
       for (final FlutterView view in device.views) {
         if (view.uiIsolate == null) {
           continue;
         }
-        uiIsolates.add(view.uiIsolate);
+        uiIsolatesIds.add(view.uiIsolate.id);
         // Reload the isolate.
-        operations.add(view.uiIsolate.reload().then((ServiceObject _) {
-          final ServiceEvent pauseEvent = view.uiIsolate.pauseEvent;
-          if ((pauseEvent != null) && pauseEvent.isPauseEvent) {
+        final Future<vm_service.Isolate> reloadIsolate = device.vmService
+          .getIsolateOrNull(view.uiIsolate.id);
+        operations.add(reloadIsolate.then((vm_service.Isolate isolate) async {
+          if ((isolate != null) && isPauseEvent(isolate.pauseEvent.kind)) {
             // Resume the isolate so that it can be killed by the embedder.
-            return device.vmService.resume(view.uiIsolate.id);
+            await device.vmService.resume(view.uiIsolate.id);
           }
-          return null;
         }));
       }
+
       // The engine handles killing and recreating isolates that it has spawned
       // ("uiIsolates"). The isolates that were spawned from these uiIsolates
       // will not be restared, and so they must be manually killed.
-      for (final Isolate isolate in device?.flutterDeprecatedVmService?.vm?.isolates ?? <Isolate>[]) {
-        if (!uiIsolates.contains(isolate)) {
-          operations.add(isolate.invokeRpcRaw('kill', params: <String, dynamic>{
-            'isolateId': isolate.id,
-          }));
+      final vm_service.VM vm = await device.vmService.getVM();
+      for (final vm_service.IsolateRef isolateRef in vm.isolates) {
+        if (uiIsolatesIds.contains(isolateRef.id)) {
+          continue;
         }
+        operations.add(device.vmService.kill(isolateRef.id)
+          .catchError((dynamic error, StackTrace stackTrace) {
+            // Do nothing on a SentinelException since it means the isolate
+            // has already been killed.
+          }, test: (dynamic error) => error is vm_service.SentinelException));
       }
     }
     await Future.wait(operations);
@@ -589,13 +592,11 @@ class HotRunner extends ResidentRunner {
         } on vm_service.RPCError {
           // Do nothing, we're already subcribed.
         }
-        for (final FlutterView view in device.views) {
-          isolateNotifications.add(
-            view.owner.vm.vmService.onIsolateEvent.firstWhere((vm_service.Event event) {
-              return event.kind == vm_service.EventKind.kIsolateRunnable;
-            }),
-          );
-        }
+        isolateNotifications.add(
+          device.vmService.onIsolateEvent.firstWhere((vm_service.Event event) {
+            return event.kind == vm_service.EventKind.kIsolateRunnable;
+          }),
+        );
       }
       await Future.wait(isolateNotifications);
     }
@@ -818,11 +819,9 @@ class HotRunner extends ResidentRunner {
 
     final Stopwatch reloadTimer = Stopwatch()..start();
 
-    if (!_isPaused()) {
-      globals.printTrace('Refreshing active FlutterViews before reloading.');
-      await refreshVM();
-      await refreshViews();
-    }
+    globals.printTrace('Refreshing active FlutterViews before reloading.');
+    await refreshVM();
+    await refreshViews();
 
     final Stopwatch devFSTimer = Stopwatch()..start();
     final UpdateFSReport updatedDevFS = await _updateDevFS();
@@ -914,27 +913,19 @@ class HotRunner extends ResidentRunner {
     // Record time it took for the VM to reload the sources.
     _addBenchmarkData('hotReloadVMReloadMilliseconds', vmReloadTimer.elapsed.inMilliseconds);
     final Stopwatch reassembleTimer = Stopwatch()..start();
-    // Reload the isolate.
-    final List<Future<void>> allDevices = <Future<void>>[];
-    for (final FlutterDevice device in flutterDevices) {
-      globals.printTrace('Sending reload events to ${device.device.name}');
-      final List<Future<ServiceObject>> futuresViews = <Future<ServiceObject>>[];
-      for (final FlutterView view in device.views) {
-        globals.printTrace('Sending reload event to "${view.uiIsolate.name}"');
-        futuresViews.add(view.uiIsolate.reload());
-      }
-      allDevices.add(Future.wait(futuresViews).whenComplete(() {
-        return device.refreshViews();
-      }));
-    }
-    await Future.wait(allDevices);
+
+    // Reload the isolate data.
+    await Future.wait(<Future<void>>[
+      for (final FlutterDevice device in flutterDevices)
+        device.refreshViews()
+    ]);
 
     globals.printTrace('Evicting dirty assets');
     await _evictDirtyAssets();
 
     // Check if any isolates are paused and reassemble those
     // that aren't.
-    final List<FlutterView> reassembleViews = <FlutterView>[];
+    final Map<FlutterView, vm_service.VmService> reassembleViews = <FlutterView, vm_service.VmService>{};
     final List<Future<void>> reassembleFutures = <Future<void>>[];
     String serviceEventKind;
     int pausedIsolatesFound = 0;
@@ -943,8 +934,12 @@ class HotRunner extends ResidentRunner {
       for (final FlutterView view in device.views) {
         // Check if the isolate is paused, and if so, don't reassemble. Ignore the
         // PostPauseEvent event - the client requesting the pause will resume the app.
-        final ServiceEvent pauseEvent = view.uiIsolate.pauseEvent;
-        if (pauseEvent != null && pauseEvent.isPauseEvent && pauseEvent.kind != ServiceEvent.kPausePostRequest) {
+        final vm_service.Isolate isolate = await device.vmService
+          .getIsolateOrNull(view.uiIsolate.id);
+        final vm_service.Event pauseEvent = isolate?.pauseEvent;
+        if (pauseEvent != null
+          && isPauseEvent(pauseEvent.kind)
+          && pauseEvent.kind != vm_service.EventKind.kPausePostRequest) {
           pausedIsolatesFound += 1;
           if (serviceEventKind == null) {
             serviceEventKind = pauseEvent.kind;
@@ -952,7 +947,7 @@ class HotRunner extends ResidentRunner {
             serviceEventKind = ''; // many kinds
           }
         } else {
-          reassembleViews.add(view);
+          reassembleViews[view] = device.vmService;
           reassembleFutures.add(device.vmService.flutterReassemble(
             isolateId: view.uiIsolate.id,
           ).catchError((dynamic error) {
@@ -974,6 +969,7 @@ class HotRunner extends ResidentRunner {
     assert(reassembleViews.isNotEmpty);
 
     globals.printTrace('Reassembling application');
+
     final Future<void> reassembleFuture = Future.wait<void>(reassembleFutures);
     await reassembleFuture.timeout(
       const Duration(seconds: 2),
@@ -986,14 +982,17 @@ class HotRunner extends ResidentRunner {
         globals.printTrace('This is taking a long time; will now check for paused isolates.');
         int postReloadPausedIsolatesFound = 0;
         String serviceEventKind;
-        for (final FlutterView view in reassembleViews) {
-          await view.uiIsolate.reload();
-          final ServiceEvent pauseEvent = view.uiIsolate.pauseEvent;
-          if (pauseEvent != null && pauseEvent.isPauseEvent) {
+        for (final FlutterView view in reassembleViews.keys) {
+          final vm_service.Isolate isolate = await reassembleViews[view]
+            .getIsolateOrNull(view.uiIsolate.id);
+          if (isolate == null) {
+            continue;
+          }
+          if (isolate.pauseEvent != null && isPauseEvent(isolate.pauseEvent.kind)) {
             postReloadPausedIsolatesFound += 1;
             if (serviceEventKind == null) {
-              serviceEventKind = pauseEvent.kind;
-            } else if (serviceEventKind != pauseEvent.kind) {
+              serviceEventKind = isolate.pauseEvent.kind;
+            } else if (serviceEventKind != isolate.pauseEvent.kind) {
               serviceEventKind = ''; // many kinds
             }
           }
@@ -1069,32 +1068,33 @@ class HotRunner extends ResidentRunner {
     }
     assert(serviceEventKind != null);
     switch (serviceEventKind) {
-      case ServiceEvent.kPauseStart: message.write('paused (probably due to --start-paused)'); break;
-      case ServiceEvent.kPauseExit: message.write('paused because ${ plural ? 'they have' : 'it has' } terminated'); break;
-      case ServiceEvent.kPauseBreakpoint: message.write('paused in the debugger on a breakpoint'); break;
-      case ServiceEvent.kPauseInterrupted: message.write('paused due in the debugger'); break;
-      case ServiceEvent.kPauseException: message.write('paused in the debugger after an exception was thrown'); break;
-      case ServiceEvent.kPausePostRequest: message.write('paused'); break;
-      case '': message.write('paused for various reasons'); break;
+      case vm_service.EventKind.kPauseStart:
+        message.write('paused (probably due to --start-paused)');
+        break;
+      case vm_service.EventKind.kPauseExit:
+        message.write('paused because ${ plural ? 'they have' : 'it has' } terminated');
+        break;
+      case vm_service.EventKind.kPauseBreakpoint:
+        message.write('paused in the debugger on a breakpoint');
+        break;
+      case vm_service.EventKind.kPauseInterrupted:
+        message.write('paused due in the debugger');
+        break;
+      case vm_service.EventKind.kPauseException:
+        message.write('paused in the debugger after an exception was thrown');
+        break;
+      case vm_service.EventKind.kPausePostRequest:
+        message.write('paused');
+        break;
+      case '':
+        message.write('paused for various reasons');
+        break;
       default:
         message.write('paused');
     }
     return message.toString();
   }
 
-  bool _isPaused() {
-    for (final FlutterDevice device in flutterDevices) {
-      for (final FlutterView view in device.views) {
-        if (view.uiIsolate != null) {
-          final ServiceEvent pauseEvent = view.uiIsolate.pauseEvent;
-          if (pauseEvent != null && pauseEvent.isPauseEvent) {
-            return true;
-          }
-        }
-      }
-    }
-    return false;
-  }
 
   @override
   void printHelp({ @required bool details }) {
@@ -1134,7 +1134,7 @@ class HotRunner extends ResidentRunner {
       }
       for (final String assetPath in device.devFS.assetPathsToEvict) {
         futures.add(
-          device.views.first.uiIsolate.vmService
+          device.vmService
             .flutterEvictAsset(
               assetPath,
               isolateId: device.views.first.uiIsolate.id,
