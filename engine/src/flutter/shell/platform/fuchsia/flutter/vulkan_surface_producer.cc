5fde553614fec6ec2ec30eac69bd846166f7382b
George Wright <gw280@google.com>
2020-04-28 14:15:56 -0700

    Set the max Vulkan API version when creating the GrContext (flutter/engine#18007)

--- engine/src/flutter/shell/platform/fuchsia/flutter/vulkan_surface_producer.cc
+++ engine/src/flutter/shell/platform/fuchsia/flutter/vulkan_surface_producer.cc
@@ -118,11 +118,17 @@ bool VulkanSurfaceProducer::Initialize(scenic::Session* scenic_session) {
   backend_context.fGraphicsQueueIndex =
       logical_device_->GetGraphicsQueueIndex();
   backend_context.fMinAPIVersion = application_->GetAPIVersion();
+  backend_context.fMaxAPIVersion = application_->GetAPIVersion();
   backend_context.fFeatures = skia_features;
   backend_context.fGetProc = std::move(getProc);
   backend_context.fOwnsInstanceAndDevice = false;
 
   context_ = GrContext::MakeVulkan(backend_context);
+
+  if (context_ == nullptr) {
+    FML_LOG(ERROR) << "Failed to create GrContext.";
+    return false;
+  }
 
   // Use local limits specified in this file above instead of flutter defaults.
   context_->setResourceCacheLimits(kGrCacheMaxCount, kGrCacheMaxByteSize);
