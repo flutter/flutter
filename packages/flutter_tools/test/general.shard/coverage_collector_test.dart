ce48c4866e4aa624304cec667b37ec08e28b9fdd
Jonah Williams <campfish91@gmail.com>
2020-04-28 14:40:26 -0700

    fix coverage collector test

---
+++ packages/flutter_tools/test/general.shard/coverage_collector_test.dart
@@ -0,0 +1,50 @@
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter_tools/src/test/coverage_collector.dart';
+import 'package:vm_service/vm_service.dart' as vm_service;
+
+import '../src/common.dart';
+
+void main() {
+  testWithoutContext('Coverage collector Can handle coverage SentinelException', () async {
+    final FakeVmServiceHost fakeVmServiceHost = FakeVmServiceHost(
+      requests: <VmServiceExpectation>[
+        FakeVmServiceRequest(
+          id: '1',
+          method: 'getVM',
+          args: null,
+          jsonResponse: (vm_service.VM.parse(<String, Object>{})
+            ..isolates = <vm_service.IsolateRef>[
+              vm_service.IsolateRef.parse(<String, Object>{
+                'id': '1'
+              }),
+            ]
+          ).toJson(),
+        ),
+        const FakeVmServiceRequest(
+          id: '2',
+          method: 'getScripts',
+          args: <String, Object>{
+            'isolateId': '1',
+          },
+          jsonResponse: <String, Object>{
+            'type': 'Sentinel'
+          }
+        )
+      ],
+    );
+
+    final Map<String, Object> result = await collect(
+      null,
+      (String predicate) => true,
+      connector: (Uri uri) async {
+        return fakeVmServiceHost.vmService;
+      },
+    );
+
+    expect(result, <String, Object>{'type': 'CodeCoverage', 'coverage': <Object>[]});
+    expect(fakeVmServiceHost.hasRemainingExpectations, false);
+  });
+}
