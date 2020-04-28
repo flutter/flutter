131d77eb96bf01ffdc56fae8629338b77dae8818
jonahwilliams <jonahwilliams@google.com>
2020-04-28 11:16:55 -0700

    add test comment

--- packages/flutter_tools/test/general.shard/runner/flutter_command_runner_test.dart
+++ packages/flutter_tools/test/general.shard/runner/flutter_command_runner_test.dart
@@ -131,6 +131,7 @@ void main() {
       }, initializeFlutterRoot: false);
 
       testUsingContext('works if --local-engine is specified and --local-engine-src-path is specified', () async {
+        // Intentionally do not create a package_config to verify that it is not required.
         fs.directory('$_kArbitraryEngineRoot/src/out/ios_debug').createSync(recursive: true);
         fs.directory('$_kArbitraryEngineRoot/src/out/host_debug').createSync(recursive: true);
 
