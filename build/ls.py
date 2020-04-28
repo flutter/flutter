625ac0e2504c135b5bcaa8b337a641527bd67936
xster <xster@google.com>
2020-04-28 12:31:04 -0700

    let ls specifify a file type (#366)

--- build/ls.py
+++ build/ls.py
@@ -9,13 +9,14 @@ import argparse
 import os
 import sys
 
-def main(target_directory):
+def main(target_directory, file_extension):
   for root, dirs, files in os.walk(target_directory):
     files = [f for f in files if not f[0] == '.']
     dirs[:] = [d for d in dirs if not d[0] == '.']
     for f in files:
-      path = os.path.join(root, f)
-      print path
+      if file_extension is None or os.path.splitext(f)[-1] == file_extension:
+        path = os.path.join(root, f)
+        print path
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser(
@@ -26,6 +27,12 @@ if __name__ == '__main__':
                       type=str,
                       required=True,
                       help="The target directory")
+  parser.add_argument("--file-extension",
+                      dest="file_extension",
+                      metavar="<file-extension>",
+                      type=str,
+                      required=False,
+                      help="File extension to filter")
 
   args = parser.parse_args()
-  sys.exit(main(args.target_directory))
+  sys.exit(main(args.target_directory, args.file_extension))
