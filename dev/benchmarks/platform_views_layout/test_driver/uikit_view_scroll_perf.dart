b176042c1a8a9fdb5c96051b613895e7f0ab167b
Emmanuel Garcia <egarciad@google.com>
2020-04-28 19:24:15 -0700

    Add benchmark for hybrid composition on Android (#55609)

--- dev/benchmarks/platform_views_layout/test_driver/uikit_view_scroll_perf.dart
+++ dev/benchmarks/platform_views_layout/test_driver/uikit_view_scroll_perf.dart
@@ -2,10 +2,12 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'package:flutter/widgets.dart';
 import 'package:flutter_driver/driver_extension.dart';
+
 import 'package:platform_views_layout/main.dart' as app;
 
 void main() {
   enableFlutterDriverExtension();
-  app.main();
+  runApp(const app.PlatformViewApp());
 }
