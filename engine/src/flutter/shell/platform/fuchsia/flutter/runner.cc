604892e454c209841b5d6365b61b3b21a74310f6
Filip Filmar <fmil@google.com>
2020-04-30 13:27:57 -0700

    [fuchsia] Use dart::ComponentContext() (flutter/engine#17876)
    This functionality is added in issue #41523.  Allows runners to request
    the singleton instance of `sys::ComponentContext`, while it remains also
    accessible to component-specific code that does not have direct access
    to top-level objects.
    
    Fixes #41523

--- engine/src/flutter/shell/platform/fuchsia/flutter/runner.cc
+++ engine/src/flutter/shell/platform/fuchsia/flutter/runner.cc
@@ -146,8 +146,8 @@ static void RegisterProfilerSymbols(const char* symbols_path,
 }
 #endif  // !defined(DART_PRODUCT)
 
-Runner::Runner(async::Loop* loop)
-    : loop_(loop), context_(sys::ComponentContext::Create()) {
+Runner::Runner(async::Loop* loop, sys::ComponentContext* context)
+    : loop_(loop), context_(context) {
 #if !defined(DART_PRODUCT)
   // The VM service isolate uses the process-wide namespace. It writes the
   // vm service protocol port under /tmp. The VMServiceObject exposes that
