4e2973633d6b8e0bf0c26b33fa61254e09fd1384
liyuqian <liyuqian@google.com>
2020-05-01 15:01:42 -0700

    Reland "Remove layer integral offset snapping" (#17915)
    This reverts commit b5aedb3 and relands #17712.
    
    Fixes flutter/flutter#53288 and flutter/flutter#41654.
    
    Together with #17791, this reland addresses some of Jim's concerns in the original PR #17712.
    
    The major part of this PR is still the same as the original PR, and the performance / golden image impacts should be the same.

--- flow/layers/container_layer.h
+++ flow/layers/container_layer.h
@@ -37,6 +37,20 @@ class ContainerLayer : public Layer {
   // For OpacityLayer to restructure to have a single child.
   void ClearChildren() { layers_.clear(); }
 
+  // Try to prepare the raster cache for a given layer.
+  //
+  // The raster cache would fail if either of the followings is true:
+  // 1. The context has a platform view.
+  // 2. The context does not have a valid raster cache.
+  // 3. The layer's paint bounds does not intersect with the cull rect.
+  //
+  // We make this a static function instead of a member function that directy
+  // uses the "this" pointer as the layer because we sometimes need to raster
+  // cache a child layer and one can't access its child's protected method.
+  static void TryToPrepareRasterCache(PrerollContext* context,
+                                      Layer* layer,
+                                      const SkMatrix& matrix);
+
  private:
   std::vector<std::shared_ptr<Layer>> layers_;
 
