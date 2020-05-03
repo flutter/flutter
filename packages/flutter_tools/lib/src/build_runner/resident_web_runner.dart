2be0c41e4ee89ae4774f4d1ca36d06874a27ea59
Jonah Williams <jonahwilliams@google.com>
2020-05-02 21:49:21 -0700

    Update resident_web_runner.dart

--- packages/flutter_tools/lib/src/build_runner/resident_web_runner.dart
+++ packages/flutter_tools/lib/src/build_runner/resident_web_runner.dart
@@ -635,7 +635,7 @@ class _ResidentWebRunner extends ResidentWebRunner {
       pathToReload: getReloadPath(fullRestart: fullRestart),
       invalidatedFiles: invalidationResult.uris,
       packageConfig: invalidationResult.packageConfig,
-      trackWidgetCreation: debuggingOptions.buildInfo.trackWidgetCreation,
+      trackWidgetCreation: true,
     );
     devFSStatus.stop();
     globals.printTrace('Synced ${getSizeAsMB(report.syncedBytes)}.');
