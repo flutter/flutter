76370c6f5efe7d71c7ff071cdab77248ad88af4f
Aaron Clarke <aaclarke@google.com>
2020-05-04 10:56:43 -0700

    fixed another test

--- packages/flutter/test/material/stepper_test.dart
+++ packages/flutter/test/material/stepper_test.dart
@@ -333,7 +333,8 @@ void main() {
     );
 
     await tester.pump(const Duration(milliseconds: 100));
-    expect(scrollableState.position.pixels, greaterThan(0.0));
+    // All steps fit on the screen, so they only valid offset is 0.0.
+    expect(scrollableState.position.pixels, 0.0);
   });
 
   testWidgets('Stepper index test', (WidgetTester tester) async {
