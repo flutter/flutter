28d99854a6469fb7890b64f23500fad5f3b2695c
liyuqian <liyuqian@google.com>
2020-05-07 17:19:30 -0700

    Reland again "Remove layer integral offset snapping #17112" (#18160)
    This reverts commit a7a25d3b57f2066798ef8cd43600588e4697c9cd and relands our reland https://github.com/flutter/engine/pull/17915.
    
    Additionally, we fixed the cull rect logic in `OpacityLayer::Preroll` which is  the root cause of https://github.com/flutter/flutter/issues/56298. We've always had that root problem before but it did not trigger performance issues because we were using the OpacityLayer's `paint_bounds`, instead of its child's `paint_bounds` for preparing the layer raster cache. A correct handling of the cull rect should allow us to cull at any level.
    
    It also turns out that our ios32 (iPhone4s) performacne can regress a lot
    without snapping. My theory is that although the picture has a
    fractional top left corner, many drawing operations inside the picture
    have integral coordinations. In older hardwares, keeping those
    coordinates integral seems to be performance critical.
    
    To avoid flutter/flutter#41654, the snapping
    will still be disabled if the matrix has non-scale-translation
    transformations.

--- flow/raster_cache.h
+++ flow/raster_cache.h
@@ -62,7 +62,21 @@ class RasterCache {
     return bounds;
   }
 
+  /**
+   * @brief Snap the translation components of the matrix to integers.
+   *
+   * The snapping will only happen if the matrix only has scale and translation
+   * transformations.
+   *
+   * @param ctm the current transformation matrix.
+   * @return SkMatrix the snapped transformation matrix.
+   */
   static SkMatrix GetIntegralTransCTM(const SkMatrix& ctm) {
+    // Avoid integral snapping if the matrix has complex transformation to avoid
+    // the artifact observed in https://github.com/flutter/flutter/issues/41654.
+    if (!ctm.isScaleTranslate()) {
+      return ctm;
+    }
     SkMatrix result = ctm;
     result[SkMatrix::kMTransX] = SkScalarRoundToScalar(ctm.getTranslateX());
     result[SkMatrix::kMTransY] = SkScalarRoundToScalar(ctm.getTranslateY());
