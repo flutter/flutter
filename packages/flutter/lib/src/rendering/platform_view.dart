244677beb6fdd04e6508f2c4dd56028d93388502
Tong Mu <dkwingsmt@users.noreply.github.com>
2020-05-08 18:50:09 -0700

    Update platform_view.dart

--- packages/flutter/lib/src/rendering/platform_view.dart
+++ packages/flutter/lib/src/rendering/platform_view.dart
@@ -7,7 +7,6 @@ import 'dart:ui';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/gestures.dart';
-import 'package:flutter/rendering.dart';
 import 'package:flutter/semantics.dart';
 import 'package:flutter/services.dart';
 
