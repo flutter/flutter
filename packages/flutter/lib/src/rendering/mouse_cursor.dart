4a6b1cf3e7c1b114e8e93c729a183ac9c5c976a6
Tong Mu <dkwingsmt@users.noreply.github.com>
2020-05-08 20:41:34 -0700

    Fix import

--- packages/flutter/lib/src/rendering/mouse_cursor.dart
+++ packages/flutter/lib/src/rendering/mouse_cursor.dart
@@ -6,7 +6,6 @@ import 'dart:async';
 import 'dart:collection' show LinkedHashSet;
 
 import 'package:flutter/foundation.dart';
-import 'package:flutter/gestures.dart';
 import 'package:flutter/services.dart';
 
 import 'mouse_tracking.dart';
