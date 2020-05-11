099b1a87a86bf135ee9402d1f018c633bc574701
Kaushik Iska <iska.kaushik@gmail.com>
2020-05-11 14:52:22 -0700

    Fix latest_frame_target_time race (flutter/engine#18279)
    There are some cases where onAnimatorDraw gets called before
    onAnimatorBeginFrame, in these cases we need to update the
    latest_frame_target_time to be the target time for the current frame.
    
    This manifested as an issue in release mode Android builds:
    https://firebase.corp.google.com/project/flutter-infra/testlab/histories/bh.27c33c672e9b01f2/matrices/4903344583855476919/executions/bs.ee4f6f7ad8671a34

--- engine/src/flutter/shell/common/shell.h
+++ engine/src/flutter/shell/common/shell.h
@@ -489,8 +489,8 @@ class Shell final : public PlatformView::Delegate,
   void OnAnimatorNotifyIdle(int64_t deadline) override;
 
   // |Animator::Delegate|
-  void OnAnimatorDraw(
-      fml::RefPtr<Pipeline<flutter::LayerTree>> pipeline) override;
+  void OnAnimatorDraw(fml::RefPtr<Pipeline<flutter::LayerTree>> pipeline,
+                      fml::TimePoint frame_target_time) override;
 
   // |Animator::Delegate|
   void OnAnimatorDrawLastLayerTree() override;
