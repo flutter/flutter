5682bcb1317b118e15c44ca70abb8d793276fedd
Robert Ancell <robert.ancell@canonical.com>
2020-05-12 11:09:21 +1200

    Add FlStandardMessageCodec (flutter/engine#18213)

---
+++ engine/src/flutter/shell/platform/linux/public/flutter_linux/fl_standard_message_codec.h
@@ -0,0 +1,47 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef FLUTTER_SHELL_PLATFORM_LINUX_FL_STANDARD_MESSAGE_CODEC_H_
+#define FLUTTER_SHELL_PLATFORM_LINUX_FL_STANDARD_MESSAGE_CODEC_H_
+
+#if !defined(__FLUTTER_LINUX_INSIDE__) && !defined(FLUTTER_LINUX_COMPILATION)
+#error "Only <flutter_linux/flutter_linux.h> can be included directly."
+#endif
+
+#include "fl_message_codec.h"
+
+G_BEGIN_DECLS
+
+G_DECLARE_FINAL_TYPE(FlStandardMessageCodec,
+                     fl_standard_message_codec,
+                     FL,
+                     STANDARD_CODEC,
+                     FlMessageCodec)
+
+/**
+ * FlStandardMessageCodec:
+ *
+ * #FlStandardMessageCodec is an #FlMessageCodec that implements the Flutter
+ * standard message encoding. This encodes and decodes #FlValue of type
+ * #FL_VALUE_TYPE_NULL, #FL_VALUE_TYPE_BOOL, #FL_VALUE_TYPE_INT,
+ * #FL_VALUE_TYPE_FLOAT, #FL_VALUE_TYPE_STRING, #FL_VALUE_TYPE_UINT8_LIST,
+ * #FL_VALUE_TYPE_INT32_LIST, #FL_VALUE_TYPE_INT64_LIST,
+ * #FL_VALUE_TYPE_FLOAT_LIST, #FL_VALUE_TYPE_LIST, and #FL_VALUE_TYPE_MAP
+ *
+ * #FlStandardMessageCodec matches the StandardCodec class in the Flutter
+ * services library.
+ */
+
+/*
+ * fl_standard_message_codec_new:
+ *
+ * Creates a #FlStandardMessageCodec.
+ *
+ * Returns: a new #FlStandardMessageCodec
+ */
+FlStandardMessageCodec* fl_standard_message_codec_new();
+
+G_END_DECLS
+
+#endif  // FLUTTER_SHELL_PLATFORM_LINUX_FL_STANDARD_MESSAGE_CODEC_H_
