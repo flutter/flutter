c70213d4fc154f155b8bcdb482bf0aa2e901925d
ferhatb <ferhat@gmail.com>
2020-05-13 10:50:03 -0700

    remove unused import

--- dev/benchmarks/macrobenchmarks/lib/src/web/bench_paths.dart
+++ dev/benchmarks/macrobenchmarks/lib/src/web/bench_paths.dart
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'dart:ui';
-
 import 'package:macrobenchmarks/src/web/recorder.dart';
 import 'package:macrobenchmarks/src/web/bench_paths_recording.dart'
     as recording;
