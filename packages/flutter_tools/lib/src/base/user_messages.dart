1078dea65e43a25ca2aadec12498930fc6b8a897
Natan Portilho <natanportilho@outlook.com>
2020-05-14 14:22:31 -0300

    Adding userMessages requesting user to chose among one of the running devices

--- packages/flutter_tools/lib/src/base/user_messages.dart
+++ packages/flutter_tools/lib/src/base/user_messages.dart
@@ -225,6 +225,8 @@ class UserMessages {
   String flutterFoundSpecifiedDevices(int count, String deviceId) =>
       'Found $count devices with name or id matching $deviceId:';
   String get flutterMultipleDevicesFound => 'Multiple devices found:';
+  String flutterChooseDevice(int option, String deviceId, String deviceName) => '[$option]: $deviceId $deviceName';
+  String get flutterChoseOne => 'Please chose one:';
   String get flutterSpecifyDeviceWithAllOption =>
       'More than one device connected; please specify a device with '
       "the '-d <deviceId>' flag, or use '-d all' to act on all devices.";
