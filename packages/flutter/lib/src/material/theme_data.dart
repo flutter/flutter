46498f6ef29a080487cef4d231a73774c883147a
Hans Muller <hansmuller@google.com>
2020-05-13 17:20:22 -0700

    updated

--- packages/flutter/lib/src/material/theme_data.dart
+++ packages/flutter/lib/src/material/theme_data.dart
@@ -93,7 +93,7 @@ enum MaterialTapTargetSize {
 }
 
 /// Defines the configuration of the overall visual [Theme] for a [MaterialApp]
-/// or a widget subtree with the app.
+/// or a widget subtree within the app.
 ///
 /// The [MaterialApp] theme property can be used to configure the appearance
 /// of the entire app. Widget subtree's within an app can override the app's
@@ -104,8 +104,6 @@ enum MaterialTapTargetSize {
 /// depend exclusively on the [colorScheme] and [textTheme]. These properties
 /// are guaranteed to have non-null values.
 ///
-/// {@tool snippet}
-///
 /// The static [Theme.of] method finds the [ThemeData] value specified for the
 /// nearest [BuildContext] ancestor. This lookup is inexpensive, essentially
 /// just a single HashMap access. It can sometimes be a little confusing
@@ -114,6 +112,7 @@ enum MaterialTapTargetSize {
 /// for the subtree that appears below the new [Theme], or insert a widget
 /// that creates a new BuildContext, like [Builder].
 ///
+/// {@tool snippet}
 /// In this example, the [Container] widget uses [Theme.of] to retrieve the
 /// primary color from the theme's [colorScheme] to draw an amber square.
 /// The [Builder] widget separates the parent theme's [BuildContext] from the
@@ -139,8 +138,43 @@ enum MaterialTapTargetSize {
 /// ```
 /// {@end-tool}
 ///
+/// {@tool snippet}
+///
+/// This sample creates a [MaterialApp] widget that stores `ThemeData` and
+/// passes the `ThemeData` to descendant widgets. The [AppBar] widget uses the
+/// [primaryColor] to create a blue background. The [Text] widget uses the
+/// [TextTheme.bodyText2] to create purple text. The [FloatingActionButton] widget
+/// uses the [accentColor] to create a green background.
+///
+/// ![](https://flutter.github.io/assets-for-api-docs/assets/material/material_app_theme_data.png)
+///
+/// ```dart
+/// MaterialApp(
+///   theme: ThemeData(
+///     primaryColor: Colors.blue,
+///     accentColor: Colors.green,
+///     textTheme: TextTheme(bodyText2: TextStyle(color: Colors.purple)),
+///   ),
+///   home: Scaffold(
+///     appBar: AppBar(
+///       title: const Text('ThemeData Demo'),
+///     ),
+///     floatingActionButton: FloatingActionButton(
+///       child: const Icon(Icons.add),
+///       onPressed: () {},
+///     ),
+///     body: Center(
+///       child: Text(
+///         'Button pressed 0 times',
+///       ),
+///     ),
+///   ),
+/// )
+/// ```
+/// {@end-tool}
+///
 /// See <https://material.io/design/color/> for
-// more discussion on how to pick the right colors.
+/// more discussion on how to pick the right colors.
 
 @immutable
 class ThemeData with Diagnosticable {
