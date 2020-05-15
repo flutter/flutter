77a46486265729de710931e025d7abde91941019
Kaushik Iska <iska.kaushik@gmail.com>
2020-05-14 19:42:02 -0700

    Add ocmock to iOS unit tests xcode project (flutter/engine#18396)

--- engine/src/flutter/testing/ios/IosUnitTests/run_tests.sh
+++ engine/src/flutter/testing/ios/IosUnitTests/run_tests.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 FLUTTER_ENGINE=ios_debug_sim_unopt
 
+TESTING_DIR=$(dirname "$0")
+pushd $TESTING_DIR
+
 if [ $# -eq 1 ]; then
   FLUTTER_ENGINE=$1
 fi
@@ -15,3 +18,5 @@ set -o pipefail && xcodebuild -sdk iphonesimulator \
   -destination 'platform=iOS Simulator,name=iPhone 8,OS=13.0' \
   test \
   FLUTTER_ENGINE=$FLUTTER_ENGINE | $PRETTY
+
+popd
