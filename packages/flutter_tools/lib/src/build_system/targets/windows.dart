b2f72894ee8540ea490cd279fed7c4604be6b757
Stuart Morgan <stuartmorgan@google.com>
2020-05-17 14:57:50 -0700

    Analyzer fix

--- packages/flutter_tools/lib/src/build_system/targets/windows.dart
+++ packages/flutter_tools/lib/src/build_system/targets/windows.dart
@@ -77,7 +77,7 @@ class UnpackWindows extends Target {
       artifacts: _kWindowsArtifacts,
       engineSourcePath: engineSourcePath,
       outputDirectory: outputDirectory,
-      clientSourcePaths: [clientSourcePath],
+      clientSourcePaths: <String>[clientSourcePath],
     );
     final DepfileService depfileService = DepfileService(
       fileSystem: environment.fileSystem,
