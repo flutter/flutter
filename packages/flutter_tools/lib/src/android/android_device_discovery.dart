602d8baf34ec9d875fd6e74e99699bf62885d9f3
Jonah Williams <campfish91@gmail.com>
2020-05-19 12:37:07 -0700

    [flutter_tools] remove globals/context for android testing (#57445)

--- packages/flutter_tools/lib/src/android/android_device_discovery.dart
+++ packages/flutter_tools/lib/src/android/android_device_discovery.dart
@@ -6,8 +6,10 @@ import 'package:meta/meta.dart';
 import 'package:process/process.dart';
 
 import '../base/common.dart';
+import '../base/file_system.dart';
 import '../base/io.dart';
 import '../base/logger.dart';
+import '../base/platform.dart';
 import '../base/process.dart';
 import '../device.dart';
 import '../globals.dart' as globals;
@@ -96,6 +98,12 @@ class AndroidDevices extends PollingDeviceDiscovery {
     String text, {
     List<AndroidDevice> devices,
     List<String> diagnostics,
+    AndroidSdk androidSdk,
+    FileSystem fileSystem,
+    Logger logger,
+    Platform platform,
+    ProcessManager processManager,
+    TimeoutConfiguration timeoutConfiguration,
   }) {
     // Check for error messages from adb
     if (!text.contains('List of devices')) {
@@ -154,6 +162,12 @@ class AndroidDevices extends PollingDeviceDiscovery {
             productID: info['product'],
             modelID: info['model'] ?? deviceID,
             deviceCodeName: info['device'],
+            androidSdk: androidSdk ?? globals.androidSdk,
+            fileSystem: fileSystem ?? globals.fs,
+            logger: logger ?? globals.logger,
+            platform: platform ?? globals.platform,
+            processManager: processManager ?? globals.processManager,
+            timeoutConfiguration: timeoutConfiguration,
           ));
         }
       } else {
