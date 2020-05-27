ad352849d20b1b960004e0117eacd386b87dad8b
David Worsham <dworsham@google.com>
2020-05-26 19:29:11 -0700

    Reland "fuchsia: Implement CreateViewWithViewProvider" (flutter/engine#18615)
    The last commit was a bad push -- this one compiles.
    
    Tested: Manual test via workstation product
    BUG: 50875

--- engine/src/flutter/shell/platform/fuchsia/flutter/engine.cc
+++ engine/src/flutter/shell/platform/fuchsia/flutter/engine.cc
@@ -5,7 +5,6 @@
 #include "engine.h"
 
 #include <lib/async/cpp/task.h>
-#include <lib/ui/scenic/cpp/view_ref_pair.h>
 #include <zircon/status.h>
 #include <sstream>
 
@@ -57,6 +56,7 @@ Engine::Engine(Delegate& delegate,
                flutter::Settings settings,
                fml::RefPtr<const flutter::DartSnapshot> isolate_snapshot,
                fuchsia::ui::views::ViewToken view_token,
+               scenic::ViewRefPair view_ref_pair,
                UniqueFDIONS fdio_ns,
                fidl::InterfaceRequest<fuchsia::io::Directory> directory_request,
                FlutterRunnerProductConfiguration product_config)
@@ -115,18 +115,14 @@ Engine::Engine(Delegate& delegate,
         });
       };
 
-  auto view_ref_pair = scenic::ViewRefPair::New();
-  fuchsia::ui::views::ViewRef view_ref;
-  view_ref_pair.view_ref.Clone(&view_ref);
-
-  fuchsia::ui::views::ViewRef dart_view_ref;
-  view_ref_pair.view_ref.Clone(&dart_view_ref);
-  zx::eventpair dart_view_ref_event_pair(std::move(dart_view_ref.reference));
+  fuchsia::ui::views::ViewRef platform_view_ref, isolate_view_ref;
+  view_ref_pair.view_ref.Clone(&platform_view_ref);
+  view_ref_pair.view_ref.Clone(&isolate_view_ref);
 
   // Setup the callback that will instantiate the platform view.
   flutter::Shell::CreateCallback<flutter::PlatformView>
       on_create_platform_view = fml::MakeCopyable(
-          [debug_label = thread_label_, view_ref = std::move(view_ref),
+          [debug_label = thread_label_, view_ref = std::move(platform_view_ref),
            runner_services,
            parent_environment_service_provider =
                std::move(parent_environment_service_provider),
@@ -263,10 +259,10 @@ Engine::Engine(Delegate& delegate,
     svc->Connect(environment.NewRequest());
 
     isolate_configurator_ = std::make_unique<IsolateConfigurator>(
-        std::move(fdio_ns),                  //
-        std::move(environment),              //
-        directory_request.TakeChannel(),     //
-        std::move(dart_view_ref_event_pair)  //
+        std::move(fdio_ns),                    //
+        std::move(environment),                //
+        directory_request.TakeChannel(),       //
+        std::move(isolate_view_ref.reference)  //
     );
   }
 
