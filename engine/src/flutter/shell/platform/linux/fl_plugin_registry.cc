8c8b2bded5f37bb3bcfa1605247262e66b0e85e6
Robert Ancell <robert.ancell@canonical.com>
2020-05-27 13:39:48 +1200

    Add FlPluginRegistry and FlPluginRegistrar (flutter/engine#18453)
    * Add FlPluginRegistry and FlPluginRegistrar

---
+++ engine/src/flutter/shell/platform/linux/fl_plugin_registry.cc
@@ -0,0 +1,24 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "flutter/shell/platform/linux/public/flutter_linux/fl_plugin_registry.h"
+
+#include <gmodule.h>
+
+// Added here to stop the compiler from optimising this function away.
+G_MODULE_EXPORT GType fl_plugin_registry_get_type();
+
+G_DEFINE_INTERFACE(FlPluginRegistry, fl_plugin_registry, G_TYPE_OBJECT)
+
+void fl_plugin_registry_default_init(FlPluginRegistryInterface* self) {}
+
+G_MODULE_EXPORT FlPluginRegistrar* fl_plugin_registry_get_registrar_for_plugin(
+    FlPluginRegistry* self,
+    const gchar* name) {
+  g_return_val_if_fail(FL_IS_PLUGIN_REGISTRY(self), nullptr);
+  g_return_val_if_fail(name != nullptr, nullptr);
+
+  return FL_PLUGIN_REGISTRY_GET_IFACE(self)->get_registrar_for_plugin(self,
+                                                                      name);
+}
