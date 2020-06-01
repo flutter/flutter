a9a30208665f36d9877e575cff7f8ea5dc62855b
cg021 <gonzchristian@google.com>
2020-06-01 13:24:26 -0500

    more detailed physics description

--- packages/flutter/lib/src/material/reorderable_list.dart
+++ packages/flutter/lib/src/material/reorderable_list.dart
@@ -123,6 +123,31 @@ class ReorderableListView extends StatefulWidget {
   final ReorderCallback onReorder;
 
   /// Determines the physics of a [Scrollable] widget.
+  ///
+  /// For example, determines how the [Scrollable] will behave when the user
+  /// reaches the maximum scroll extent or when the user stops scrolling.
+  ///
+  /// When starting a physics [Simulation], the current scroll position and
+  /// velocity are used as the initial conditions for the particle in the
+  /// simulation. The movement of the particle in the simulation is then used to
+  /// determine the scroll position for the widget.
+  ///
+  /// Instead of creating your own subclasses, [parent] can be used to combine
+  /// [ScrollPhysics] objects of different types to get the desired scroll physics.
+  /// For example:
+  ///
+  /// ```dart
+  /// const BouncingScrollPhysics(parent: AlwaysScrollableScrollPhysics())
+  /// ```
+  ///
+  /// You can also use `applyTo`, which is useful when you already have
+  /// an instance of `ScrollPhysics`:
+  ///
+  /// ```dart
+  /// ScrollPhysics physics = const BouncingScrollPhysics();
+  /// // ...
+  /// physics.applyTo(const AlwaysScrollableScrollPhysics())
+  /// ```  
   final ScrollPhysics physics;
 
   @override
