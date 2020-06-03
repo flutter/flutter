c66b0db863450eb1f63c4d26d7daa7744f6eba87
Jonah Williams <jonahwilliams@google.com>
2020-06-02 17:52:20 -0700

    Revert "[flutter_tools] Put a heap size limit on the frontend_server" (#58525)
    * Revert "[flutter_tools] Put a heap size limit on the frontend_server (#58039)"

--- packages/flutter_tools/lib/src/compile.dart
+++ packages/flutter_tools/lib/src/compile.dart
@@ -263,11 +263,6 @@ class KernelCompiler {
     }
     final List<String> command = <String>[
       engineDartPath,
-      '--disable-dart-dev',
-      // This limit is in place to help track down
-      // https://github.com/flutter/flutter/issues/54420. It should be removed
-      // when the underlying issue is identified and fixed.
-      '--old_gen_heap_size=2000',
       frontendServer,
       '--sdk-root',
       sdkRoot,
@@ -653,11 +648,6 @@ class DefaultResidentCompiler implements ResidentCompiler {
     );
     final List<String> command = <String>[
       globals.artifacts.getArtifactPath(Artifact.engineDartBinary),
-      '--disable-dart-dev',
-      // This limit is in place to help track down
-      // https://github.com/flutter/flutter/issues/54420. It should be removed
-      // when the underlying issue is identified and fixed.
-      '--old_gen_heap_size=2000',
       frontendServer,
       '--sdk-root',
       sdkRoot,
