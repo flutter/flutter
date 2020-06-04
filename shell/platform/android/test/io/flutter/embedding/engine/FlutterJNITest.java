c8ab763f79269bf275644cfaa72edeba1c4b0898
cg021 <40409839+cg021@users.noreply.github.com>
2020-06-04 11:20:45 -0500

    onDisplayPlatformView JNI (#18786)
    * onDisplayPlatformView JNI
    
    * format
    
    * add todo in onDisplayPlatformView
    
    Co-authored-by: Emmanuel Garcia <egarciad@google.com>
    
    * move methods
    
    * add platformViewsController setter
    
    * remove unnecessary calls
    
    * remove override and add unit test
    
    * remove override
    
    * test
    
    * mockito
    
    * fix formatting
    
    Co-authored-by: Emmanuel Garcia <egarciad@google.com>

--- shell/platform/android/test/io/flutter/embedding/engine/FlutterJNITest.java
+++ shell/platform/android/test/io/flutter/embedding/engine/FlutterJNITest.java
@@ -1,6 +1,7 @@
 package io.flutter.embedding.engine;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.verify;
 
 import io.flutter.embedding.engine.renderer.FlutterUiDisplayListener;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -43,5 +44,22 @@ public class FlutterJNITest {
 
     // --- Verify Results ---
     assertEquals(1, callbackInvocationCount.get());
+  }
+
+  @Test
+  public void onDisplayPlatformView__callsPlatformViewsController() {
+    PlatformViewsController platformViewsController = mock(PlatformViewsController.class);
+
+    FlutterJNI flutterJNI = new FlutterJNI();
+    flutterJNI.setPlatformViewsController(platformViewsController);
+
+    // --- Execute Test ---
+    flutterJNI.onDisplayPlatformView(
+        /*viewId=*/ 1, /*x=*/ 10, /*y=*/ 20, /*width=*/ 100, /*height=*/ 200);
+
+    // --- Verify Results ---
+    verify(platformViewsController, times(1))
+        .onDisplayPlatformView(
+            /*viewId=*/ 1, /*x=*/ 10, /*y=*/ 20, /*width=*/ 100, /*height=*/ 200);
   }
 }
