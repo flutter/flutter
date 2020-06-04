85985107f7f1648459de72b5ba5be153f5d16de5
Jonah Williams <jonahwilliams@google.com>
2020-06-04 14:00:33 -0700

    use Expand-Archive and Compress-Archive in windows os utils (#58390)
    Work towards removal of package:archive and ideally more stable unzipping of artifacts. These commands are available in Powershell 5+, which we already require for windows.

--- packages/flutter_tools/test/general.shard/base/os_test.dart
+++ packages/flutter_tools/test/general.shard/base/os_test.dart
@@ -21,6 +21,17 @@ const String kExecutable = 'foo';
 const String kPath1 = '/bar/bin/$kExecutable';
 const String kPath2 = '/another/bin/$kExecutable';
 
+const String kPowershellException = r'''
+New-Object : Exception calling ".ctor" with "3" argument(s): "End of Central Directory record could not be found."
+At
+C:\Windows\system32\WindowsPowerShell\v1.0\Modules\Microsoft.PowerShell.Archive\Microsoft.PowerShell.Archive.psm1:934
+char:23
++ ... ipArchive = New-Object -TypeName System.IO.Compression.ZipArchive -Ar ...
++                 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    + CategoryInfo          : InvalidOperation: (:) [New-Object], MethodInvocationException
+    + FullyQualifiedErrorId : ConstructorInvokedThrowException,Microsoft.PowerShell.Commands.NewObjectCommand
+''';
+
 void main() {
   MockProcessManager mockProcessManager;
 
@@ -67,6 +78,7 @@ void main() {
     testWithoutContext('returns null when executable does not exist', () async {
       when(mockProcessManager.runSync(<String>['where', kExecutable]))
           .thenReturn(ProcessResult(0, 1, null, null));
+      when(mockProcessManager.canRun('pwsh.exe')).thenReturn(true);
       final OperatingSystemUtils utils = createOSUtils(FakePlatform(operatingSystem: 'windows'));
       expect(utils.which(kExecutable), isNull);
     });
@@ -74,6 +86,7 @@ void main() {
     testWithoutContext('returns exactly one result', () async {
       when(mockProcessManager.runSync(<String>['where', 'foo']))
           .thenReturn(ProcessResult(0, 0, '$kPath1\n$kPath2', null));
+      when(mockProcessManager.canRun('pwsh.exe')).thenReturn(true);
       final OperatingSystemUtils utils = createOSUtils(FakePlatform(operatingSystem: 'windows'));
       expect(utils.which(kExecutable).path, kPath1);
     });
@@ -81,6 +94,7 @@ void main() {
     testWithoutContext('returns all results for whichAll', () async {
       when(mockProcessManager.runSync(<String>['where', kExecutable]))
           .thenReturn(ProcessResult(0, 0, '$kPath1\n$kPath2', null));
+      when(mockProcessManager.canRun('pwsh.exe')).thenReturn(true);
       final OperatingSystemUtils utils = createOSUtils(FakePlatform(operatingSystem: 'windows'));
       final List<File> result = utils.whichAll(kExecutable);
       expect(result, hasLength(2));
@@ -97,6 +111,7 @@ void main() {
       when(mockFile.readAsBytesSync()).thenThrow(
         const FileSystemException('error'),
       );
+      when(mockProcessManager.canRun('pwsh.exe')).thenReturn(true);
       final OperatingSystemUtils osUtils = OperatingSystemUtils(
         fileSystem: fileSystem,
         logger: BufferLogger.test(),
@@ -116,6 +131,7 @@ void main() {
         0x01,
         0x02,
       ]));
+      when(mockProcessManager.canRun('pwsh.exe')).thenReturn(true);
       final OperatingSystemUtils osUtils = OperatingSystemUtils(
         fileSystem: fileSystem,
         logger: BufferLogger.test(),
@@ -131,6 +147,7 @@ void main() {
       final MockFile mockFile = MockFile();
       when(fileSystem.file(any)).thenReturn(mockFile);
       when(mockFile.readAsBytesSync()).thenReturn(Uint8List(0));
+      when(mockProcessManager.canRun('pwsh.exe')).thenReturn(true);
       final OperatingSystemUtils osUtils = OperatingSystemUtils(
         fileSystem: fileSystem,
         logger: BufferLogger.test(),
@@ -140,6 +157,116 @@ void main() {
 
       expect(osUtils.verifyGzip(mockFile), isFalse);
     });
+  });
+
+  testWithoutContext('Windows PowerShell Expand-Archive', () async {
+    final FakeProcessManager processManager = FakeProcessManager.list(<FakeCommand>[
+      const FakeCommand(
+        command: <String>[
+          'pwsh.exe',
+          '-command',
+          '"Expand-Archive a -DestinationPath b"',
+        ],
+      ),
+    ]);
+    final FileSystem fileSystem = MemoryFileSystem.test(style: FileSystemStyle.windows);
+    final OperatingSystemUtils osUtils = OperatingSystemUtils(
+      fileSystem: fileSystem,
+      logger: BufferLogger.test(),
+      platform: FakePlatform(operatingSystem: 'windows'),
+      processManager: processManager,
+    );
+
+    osUtils.unzip(fileSystem.file('a'), fileSystem.directory('b'));
+
+    expect(processManager.hasRemainingExpectations, false);
+  });
+
+  testWithoutContext('Windows PowerShell Expand-Archive with stderr', () async {
+    final FakeProcessManager processManager = FakeProcessManager.list(<FakeCommand>[
+      const FakeCommand(
+        command: <String>[
+          'pwsh.exe',
+          '-command',
+          '"Expand-Archive a -DestinationPath b"',
+        ],
+        stderr: kPowershellException,
+      ),
+    ]);
+    final FileSystem fileSystem = MemoryFileSystem.test(style: FileSystemStyle.windows);
+    final OperatingSystemUtils osUtils = OperatingSystemUtils(
+      fileSystem: fileSystem,
+      logger: BufferLogger.test(),
+      platform: FakePlatform(operatingSystem: 'windows'),
+      processManager: processManager,
+    );
+
+    expect(() => osUtils.unzip(fileSystem.file('a'), fileSystem.directory('b')),
+      throwsA(isA<ProcessException>()));
+
+    expect(processManager.hasRemainingExpectations, false);
+  });
+
+  testWithoutContext('Windows PowerShell Compress-Archive', () {
+    final FakeProcessManager processManager = FakeProcessManager.list(<FakeCommand>[
+      const FakeCommand(
+        command: <String>[
+          'pwsh.exe',
+          '-command',
+          '"Compress-Archive b -DestinationPath a"',
+        ],
+      ),
+    ]);
+    final FileSystem fileSystem = MemoryFileSystem.test(style: FileSystemStyle.windows);
+    final OperatingSystemUtils osUtils = OperatingSystemUtils(
+      fileSystem: fileSystem,
+      logger: BufferLogger.test(),
+      platform: FakePlatform(operatingSystem: 'windows'),
+      processManager: processManager,
+    );
+
+    osUtils.zip(fileSystem.directory('b'), fileSystem.file('a'));
+
+    expect(processManager.hasRemainingExpectations, false);
+  });
+
+  testWithoutContext('Windows PowerShell Compress-Archive with stderr', () {
+    final FakeProcessManager processManager = FakeProcessManager.list(<FakeCommand>[
+      const FakeCommand(
+        command: <String>[
+          'pwsh.exe',
+          '-command',
+          '"Compress-Archive b -DestinationPath a"',
+        ],
+        stderr: kPowershellException,
+      ),
+    ]);
+    final FileSystem fileSystem = MemoryFileSystem.test(style: FileSystemStyle.windows);
+    final OperatingSystemUtils osUtils = OperatingSystemUtils(
+      fileSystem: fileSystem,
+      logger: BufferLogger.test(),
+      platform: FakePlatform(operatingSystem: 'windows'),
+      processManager: processManager,
+    );
+
+    expect(() => osUtils.zip(fileSystem.directory('b'), fileSystem.file('a')),
+      throwsA(isA<ProcessException>()));
+
+    expect(processManager.hasRemainingExpectations, false);
+  });
+
+   testWithoutContext('Windows PowerShell verifyZip is a no-op', () {
+    final FakeProcessManager processManager = FakeProcessManager.list(<FakeCommand>[]);
+    final FileSystem fileSystem = MemoryFileSystem.test(style: FileSystemStyle.windows);
+    final OperatingSystemUtils osUtils = OperatingSystemUtils(
+      fileSystem: fileSystem,
+      logger: BufferLogger.test(),
+      platform: FakePlatform(operatingSystem: 'windows'),
+      processManager: processManager,
+    );
+
+    expect(osUtils.verifyZip(fileSystem.file('a')), true);
+    expect(processManager.hasRemainingExpectations, false);
   });
 
   testWithoutContext('stream compression level', () {
