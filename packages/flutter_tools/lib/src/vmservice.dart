34ae13773e3b76a8a165a2d86462ca55e07e3969
Jonah Williams <jonahwilliams@google.com>
2020-06-05 11:01:23 -0700

    [flutter_tools] add --write-sksl-on-exit to flutter drive (#58743)
    Allow dumping sksl files on driver exit when a file path is provided to drive's --write-sksl-on-exit

--- packages/flutter_tools/lib/src/vmservice.dart
+++ packages/flutter_tools/lib/src/vmservice.dart
@@ -4,11 +4,14 @@
 
 import 'dart:async';
 
+import 'package:file/file.dart';
 import 'package:meta/meta.dart' show required, visibleForTesting;
 import 'package:vm_service/vm_service.dart' as vm_service;
 
 import 'base/context.dart';
 import 'base/io.dart' as io;
+import 'build_info.dart';
+import 'convert.dart';
 import 'device.dart';
 import 'globals.dart' as globals;
 import 'version.dart';
@@ -799,4 +802,50 @@ bool isPauseEvent(String kind) {
          kind == vm_service.EventKind.kPauseException ||
          kind == vm_service.EventKind.kPausePostRequest ||
          kind == vm_service.EventKind.kNone;
+}
+
+// TODO(jonahwilliams): either refactor drive to use the resident runner
+// or delete it.
+Future<String> sharedSkSlWriter(Device device, Map<String, Object> data, {
+  File outputFile,
+}) async {
+  if (data.isEmpty) {
+    globals.logger.printStatus(
+      'No data was receieved. To ensure SkSL data can be generated use a '
+      'physical device then:\n'
+      '  1. Pass "--cache-sksl" as an argument to flutter run.\n'
+      '  2. Interact with the application to force shaders to be compiled.\n'
+    );
+    return null;
+  }
+  if (outputFile == null) {
+    outputFile = globals.fsUtils.getUniqueFile(
+      globals.fs.currentDirectory,
+      'flutter',
+      'sksl.json',
+    );
+  } else if (!outputFile.parent.existsSync()) {
+    outputFile.parent.createSync(recursive: true);
+  }
+  // Convert android sub-platforms to single target platform.
+  TargetPlatform targetPlatform = await device.targetPlatform;
+  switch (targetPlatform) {
+    case TargetPlatform.android_arm:
+    case TargetPlatform.android_arm64:
+    case TargetPlatform.android_x64:
+    case TargetPlatform.android_x86:
+      targetPlatform = TargetPlatform.android;
+      break;
+    default:
+      break;
+  }
+  final Map<String, Object> manifest = <String, Object>{
+    'platform': getNameForTargetPlatform(targetPlatform),
+    'name': device.name,
+    'engineRevision': globals.flutterVersion.engineRevision,
+    'data': data,
+  };
+  outputFile.writeAsStringSync(json.encode(manifest));
+  globals.logger.printStatus('Wrote SkSL data to ${outputFile.path}.');
+  return outputFile.path;
 }
