f9b415d1b95a337817f5e5f6843292efd537c281
cg021 <40409839+cg021@users.noreply.github.com>
2020-06-09 19:07:53 -0500

    Add onDisplayOverlaySurface JNI (flutter/engine#18859)

--- engine/src/flutter/shell/platform/android/test/io/flutter/embedding/engine/FlutterJNITest.java
+++ engine/src/flutter/shell/platform/android/test/io/flutter/embedding/engine/FlutterJNITest.java
@@ -65,4 +65,20 @@ public class FlutterJNITest {
         .onDisplayPlatformView(
             /*viewId=*/ 1, /*x=*/ 10, /*y=*/ 20, /*width=*/ 100, /*height=*/ 200);
   }
+
+  @Test
+  public void onDisplayOverlaySurface__callsPlatformViewsController() {
+    PlatformViewsController platformViewsController = mock(PlatformViewsController.class);
+
+    FlutterJNI flutterJNI = new FlutterJNI();
+    flutterJNI.setPlatformViewsController(platformViewsController);
+
+    // --- Execute Test ---
+    flutterJNI.onDisplayOverlaySurface(
+        /*id=*/ 1, /*x=*/ 10, /*y=*/ 20, /*width=*/ 100, /*height=*/ 200);
+
+    // --- Verify Results ---
+    verify(platformViewsController, times(1))
+        .onDisplayOverlaySurface(/*id=*/ 1, /*x=*/ 10, /*y=*/ 20, /*width=*/ 100, /*height=*/ 200);
+  }
 }
