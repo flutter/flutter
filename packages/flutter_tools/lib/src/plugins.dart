f32c309e7fc63c58cdffd220b1857c81448ab650
Marcus Tomlinson <marcus.tomlinson@canonical.com>
2020-06-10 10:00:56 +0100

    Bundle <plugin_name>_bundled_libraries too

--- packages/flutter_tools/lib/src/plugins.dart
+++ packages/flutter_tools/lib/src/plugins.dart
@@ -806,6 +806,7 @@ foreach(plugin ${FLUTTER_PLUGIN_LIST})
   add_subdirectory({{pluginsDir}}/${plugin}/linux plugins/${plugin})
   target_link_libraries(${BINARY_NAME} PRIVATE ${plugin}_plugin)
   list(APPEND BUNDLED_LIBRARIES $<TARGET_FILE:${plugin}_plugin>)
+  list(APPEND BUNDLED_LIBRARIES ${${plugin}_bundled_libraries})
 endforeach(plugin)
 ''';
 
