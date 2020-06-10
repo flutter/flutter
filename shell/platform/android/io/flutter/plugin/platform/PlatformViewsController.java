82e188d10be700447e2221b27f9e94812e419b9a
cg021 <40409839+cg021@users.noreply.github.com>
2020-06-09 19:07:53 -0500

    Add onDisplayOverlaySurface JNI (#18859)

--- shell/platform/android/io/flutter/plugin/platform/PlatformViewsController.java
+++ shell/platform/android/io/flutter/plugin/platform/PlatformViewsController.java
@@ -537,4 +537,8 @@ public class PlatformViewsController implements PlatformViewsAccessibilityDelega
   public void onDisplayPlatformView(int viewId, int x, int y, int width, int height) {
     // TODO: Implement this method. https://github.com/flutter/flutter/issues/58288
   }
+
+  public void onDisplayOverlaySurface(int id, int x, int y, int width, int height) {
+    // TODO: Implement this method. https://github.com/flutter/flutter/issues/58288
+  }
 }
