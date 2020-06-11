8aa8172d1d98e860d60351b2e5194e5fb865929b
Marcus Tomlinson <marcus.tomlinson@canonical.com>
2020-06-11 22:00:28 +0100

    Remove use of BundleUtilities in Linux build (#59080)
    * Explicitly install the Flutter library
    
    * Publish ${FLUTTER_LIBRARY} to parent scope
    
    * Bundle plugin libraries
    
    * Bundle <plugin_name>_bundled_libraries too
    
    * BUNDLED_LIBRARIES -> PLUGIN_BUNDLED_LIBRARIES
    
    * Update Linux plugin test
    
    * Address review comments

--- packages/flutter_tools/lib/src/plugins.dart
+++ packages/flutter_tools/lib/src/plugins.dart
@@ -800,9 +800,13 @@ list(APPEND FLUTTER_PLUGIN_LIST
 {{/plugins}}
 )
 
+set(PLUGIN_BUNDLED_LIBRARIES)
+
 foreach(plugin ${FLUTTER_PLUGIN_LIST})
   add_subdirectory({{pluginsDir}}/${plugin}/linux plugins/${plugin})
   target_link_libraries(${BINARY_NAME} PRIVATE ${plugin}_plugin)
+  list(APPEND PLUGIN_BUNDLED_LIBRARIES $<TARGET_FILE:${plugin}_plugin>)
+  list(APPEND PLUGIN_BUNDLED_LIBRARIES ${${plugin}_bundled_libraries})
 endforeach(plugin)
 ''';
 
