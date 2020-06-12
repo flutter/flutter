eea2ac79aeabd0b4da83d74db0274ea79c906569
Jonah Williams <jonahwilliams@google.com>
2020-06-12 16:52:18 -0700

    Update bitcode.dart

--- packages/flutter_tools/lib/src/ios/bitcode.dart
+++ packages/flutter_tools/lib/src/ios/bitcode.dart
@@ -2,4 +2,5 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+/// Whether bitcode is enabled by default for iOS builds.
 const bool kBitcodeEnabledDefault = false;
