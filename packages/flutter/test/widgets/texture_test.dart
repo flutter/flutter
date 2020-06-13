ab7a28e76e54709de3f5a32f15b942a1d1659b05
zhouliang3 <zl398833@alibaba-inc.com>
2020-06-13 11:19:01 +0800

    add test for Texture

---
+++ packages/flutter/test/widgets/texture_test.dart
@@ -0,0 +1,44 @@
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'dart:ui' as ui;
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+void main() {
+  testWidgets('Texture with defalut FilterQuality', (WidgetTester tester) async {
+    await tester.pumpWidget(
+        const Center(child: Texture(textureId: 1,))
+    );
+
+    Texture texture = tester.firstWidget(find.byType(Texture));
+    expect(texture, isNotNull);
+    expect(texture.textureId, 1);
+    expect(texture.filterQuality, FilterQuality.low);
+  });
+
+  testWidgets('Texture with FilterQuality', (WidgetTester tester) async {
+    await tester.pumpWidget(
+        const Center(child: Texture(textureId: 1, filterQuality: FilterQuality.none))
+    );
+
+    Texture texture = tester.firstWidget(find.byType(Texture));
+    expect(texture, isNotNull);
+    expect(texture.textureId, 1);
+    expect(texture.filterQuality, FilterQuality.none);
+
+    await tester.pumpWidget(
+        const Center(child: Texture(textureId: 1, filterQuality: FilterQuality.low))
+    );
+
+    texture = tester.firstWidget(find.byType(Texture));
+    expect(texture, isNotNull);
+    expect(texture.textureId, 1);
+    expect(texture.filterQuality, FilterQuality.low);
+  });
+}
