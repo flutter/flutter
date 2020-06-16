2cb3a5ca0aa89381c62ec59c60cfd113b2d8a9da
Dan Field <dnfield@google.com>
2020-06-16 15:14:04 -0700

    Instantiate image codec doc fix (flutter/engine#19062)
    Correctly describe the way aspect ratio is managed when one parameter is not specified.

--- engine/src/flutter/lib/ui/painting.dart
+++ engine/src/flutter/lib/ui/painting.dart
@@ -1704,8 +1704,9 @@ class Codec extends NativeFieldWrapperClass2 {
 /// The [targetWidth] and [targetHeight] arguments specify the size of the output
 /// image, in image pixels. If they are not equal to the intrinsic dimensions of the
 /// image, then the image will be scaled after being decoded. If only one dimension
-/// is specified, the omitted dimension will remain its original size. If both are
-/// not specified, then the image maintains its real size.
+/// is specified, the omitted dimension will be scaled to maintain the original
+/// aspect ratio. If both are not specified, then the image maintains its real
+/// size.
 ///
 /// The returned future can complete with an error if the image decoding has
 /// failed.
