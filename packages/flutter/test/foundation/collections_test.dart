1d526b917eeb668cfdc5ae2f5e61efc083792b22
Greg Spencer <gspencergoog@users.noreply.github.com>
2020-06-16 14:41:05 -0700

    Remove dependency on package:collection by moving mergeSort into foundation/collections.dart (#59521)
    This removes a dependency from Flutter (package:collection) by copying the implementation of mergeSort into Flutter's foundation/collections.dart.
    
    Also, removed a reference to UnmodifiableSetView from the shortcuts code by just returning a copy instead.

--- packages/flutter/test/foundation/collections_test.dart
+++ packages/flutter/test/foundation/collections_test.dart
@@ -3,6 +3,7 @@
 // found in the LICENSE file.
 
 // @dart = 2.8
+import 'dart:math';
 
 import 'package:flutter/src/foundation/collections.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -60,4 +61,120 @@ void main() {
     expect(binarySearch(items, 3), 2);
     expect(binarySearch(items, 12), -1);
   });
+  test('MergeSortRandom', () {
+    final Random random = Random();
+    for (int i = 0; i < 250; i += 1) {
+      final List<int> list = List<int>(i);
+      for (int j = 0; j < i; j++) {
+        list[j] = random.nextInt(i); // Expect some equal elements.
+      }
+      mergeSort(list);
+      for (int j = 1; j < i; j++) {
+        expect(list[j - 1], lessThanOrEqualTo(list[j]));
+      }
+    }
+  });
+  test('MergeSortPreservesOrder', () {
+    final Random random = Random();
+    // Small case where only insertion call is called,
+    // larger case where the internal moving insertion sort is used
+    // larger cases with multiple splittings, numbers just around a power of 2.
+    for (final int size in <int>[8, 50, 511, 512, 513]) {
+      final List<OrderedComparable> list = List<OrderedComparable>(size);
+      // Class OC compares using id.
+      // With size elements with id's in the range 0..size/4, a number of
+      // collisions are guaranteed. These should be sorted so that the 'order'
+      // part of the objects are still in order.
+      for (int i = 0; i < size; i++) {
+        list[i] = OrderedComparable(random.nextInt(size >> 2), i);
+      }
+      mergeSort(list);
+      OrderedComparable prev = list[0];
+      for (int i = 1; i < size; i++) {
+        final OrderedComparable next = list[i];
+        expect(prev.id, lessThanOrEqualTo(next.id));
+        if (next.id == prev.id) {
+          expect(prev.order, lessThanOrEqualTo(next.order));
+        }
+        prev = next;
+      }
+      // Reverse compare on part of list.
+      final List<OrderedComparable> copy = list.toList();
+      final int min = size >> 2;
+      final int max = size - min;
+      mergeSort<OrderedComparable>(
+        list,
+        start: min,
+        end: max,
+        compare: (OrderedComparable a, OrderedComparable b) => b.compareTo(a),
+      );
+      prev = list[min];
+      for (int i = min + 1; i < max; i++) {
+        final OrderedComparable next = list[i];
+        expect(prev.id, greaterThanOrEqualTo(next.id));
+        if (next.id == prev.id) {
+          expect(prev.order, lessThanOrEqualTo(next.order));
+        }
+        prev = next;
+      }
+      // Equals on OC objects is identity, so this means the parts before min,
+      // and the parts after max, didn't change at all.
+      expect(list.sublist(0, min), equals(copy.sublist(0, min)));
+      expect(list.sublist(max), equals(copy.sublist(max)));
+    }
+  });
+  test('MergeSortSpecialCases', () {
+    for (final int size in <int>[511, 512, 513]) {
+      // All equal.
+      final List<OrderedComparable> list = List<OrderedComparable>(size);
+      for (int i = 0; i < size; i++) {
+        list[i] = OrderedComparable(0, i);
+      }
+      mergeSort(list);
+      for (int i = 0; i < size; i++) {
+        expect(list[i].order, equals(i));
+      }
+      // All but one equal, first.
+      list[0] = OrderedComparable(1, 0);
+      for (int i = 1; i < size; i++) {
+        list[i] = OrderedComparable(0, i);
+      }
+      mergeSort(list);
+      for (int i = 0; i < size - 1; i++) {
+        expect(list[i].order, equals(i + 1));
+      }
+      expect(list[size - 1].order, equals(0));
+
+      // All but one equal, last.
+      for (int i = 0; i < size - 1; i++) {
+        list[i] = OrderedComparable(0, i);
+      }
+      list[size - 1] = OrderedComparable(-1, size - 1);
+      mergeSort(list);
+      expect(list[0].order, equals(size - 1));
+      for (int i = 1; i < size; i++) {
+        expect(list[i].order, equals(i - 1));
+      }
+
+      // Reversed.
+      for (int i = 0; i < size; i++) {
+        list[i] = OrderedComparable(size - 1 - i, i);
+      }
+      mergeSort(list);
+      for (int i = 0; i < size; i++) {
+        expect(list[i].id, equals(i));
+        expect(list[i].order, equals(size - 1 - i));
+      }
+    }
+  });
+}
+
+class OrderedComparable implements Comparable<OrderedComparable> {
+  OrderedComparable(this.id, this.order);
+  final int id;
+  final int order;
+  @override
+  int compareTo(OrderedComparable other) => id - other.id;
+  @override
+  String toString() => 'OverrideComparable[$id,$order]';
 }
