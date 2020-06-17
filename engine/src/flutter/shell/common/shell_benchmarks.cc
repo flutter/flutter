4badf4e81829b4ba7b645e478ff60f7cf6cbf1d0
liyuqian <liyuqian@google.com>
2020-06-16 23:38:02 -0700

    Correct BM_ShellInitializationAndShutdown (flutter/engine#18829)

--- engine/src/flutter/shell/common/shell_benchmarks.cc
+++ engine/src/flutter/shell/common/shell_benchmarks.cc
@@ -70,7 +70,11 @@ static void StartupAndShutdownShell(benchmark::State& state,
     // The ui thread could be busy processing tasks after shell created, e.g.,
     // default font manager setup. The measurement of shell shutdown should be
     // considered after those ui tasks have been done.
-    benchmarking::ScopedPauseTiming pause(state, true);
+    //
+    // However, if we're measuring the complete time from startup to shutdown,
+    // this time should still be included.
+    benchmarking::ScopedPauseTiming pause(
+        state, !measure_shutdown || !measure_startup);
     fml::AutoResetWaitableEvent latch;
     fml::TaskRunner::RunNowOrPostTask(thread_host->ui_thread->GetTaskRunner(),
                                       [&latch]() { latch.Signal(); });
