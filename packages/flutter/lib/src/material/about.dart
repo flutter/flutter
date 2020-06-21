5f0be755e549a518267e22988bff0ad23aaaabd6
Tonic Artos <tonic.artos@gmail.com>
2020-06-21 18:41:59 +0800

    Clarify generating package and license data.
    - Also, move sorting of packages to do a single time, after all
      generation is complete.

--- packages/flutter/lib/src/material/about.dart
+++ packages/flutter/lib/src/material/about.dart
@@ -595,10 +595,12 @@ class _PackagesView extends StatefulWidget {
 }
 
 class _PackagesViewState extends State<_PackagesView> {
-  final Future<_LicenseData> licenses = LicenseRegistry.licenses.fold<_LicenseData>(
-    _LicenseData(),
-    (_LicenseData prev, LicenseEntry license) => prev..addLicense(license),
-  );
+  final Future<_LicenseData> licenses = LicenseRegistry.licenses
+      .fold<_LicenseData>(
+        _LicenseData(),
+        (_LicenseData prev, LicenseEntry license) => prev..addLicense(license),
+      )
+      .then((_LicenseData licenseData) => licenseData..sortPackages());
 
   @override
   Widget build(BuildContext context) {
@@ -721,41 +723,58 @@ class _PackageListTile extends StatelessWidget {
   }
 }
 
+/// This is a collection of licenses and the packages to which they apply.
+/// [packageLicenseBindings] records the m+:n+ relationship between the license
+/// and packages as a map of package names to license indexes.
 class _LicenseData {
   final List<LicenseEntry> licenses = <LicenseEntry>[];
   final Map<String, List<int>> packageLicenseBindings = <String, List<int>>{};
   final List<String> packages = <String>[];
+
   // Special treatment for the first package since it should be the package
   // for delivered application.
   String firstPackage;
 
   void addLicense(LicenseEntry entry) {
+    // Before the license can be added, we must first record the packages to
+    // which it belongs.
     for (final String package in entry.packages) {
       _addPackage(package);
+      // Bind this license to the package using the next index value. This
+      // creates a contract that this license must be inserted at this same
+      // index value.
       packageLicenseBindings[package].add(licenses.length);
     }
-    licenses.add(entry);
+    licenses.add(entry); // Completion of the contract above.
   }
 
+  /// Add a package and initialise package license binding. This is a no-op if
+  /// the package has been seen before.
   void _addPackage(String package) {
     if (!packageLicenseBindings.containsKey(package)) {
       packageLicenseBindings[package] = <int>[];
       firstPackage ??= package;
       packages.add(package);
-      packages.sort((String a, String b) {
-        // Based on how LicenseRegistry currently behaves, the first package
-        // returned is the end user application license. This should be
-        // presented first in the list. So here we make sure that first package
-        // remains at the front regardless of alphabetical sorting.
-        if (a == firstPackage) {
-          return -1;
-        }
-        if (b == firstPackage) {
-          return 1;
-        }
-        return a.toLowerCase().compareTo(b.toLowerCase());
-      });
     }
+  }
+
+  /// Sort the packages using some comparison method, or by the default manner,
+  /// which is to put the application package first, followed by every other
+  /// package in case-insensitive alphabetical order.
+  void sortPackages([int compare(String a, String b)]) {
+    packages.sort(compare ?? (String a, String b) {
+      // Based on how LicenseRegistry currently behaves, the first package
+      // returned is the end user application license. This should be
+      // presented first in the list. So here we make sure that first package
+      // remains at the front regardless of alphabetical sorting.
+      if (a == firstPackage) {
+        return -1;
+      }
+      if (b == firstPackage) {
+        return 1;
+      }
+      return a.toLowerCase().compareTo(b.toLowerCase());
+    });
   }
 }
 
