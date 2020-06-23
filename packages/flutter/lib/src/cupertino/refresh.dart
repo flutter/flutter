611df48bd6b538bc3d3ea831112de930f7e50f5b
chenming <chen410063005@gmail.com>
2020-06-23 17:06:42 +0800

    Add example code for CupertinoSliverRefreshControl

--- packages/flutter/lib/src/cupertino/refresh.dart
+++ packages/flutter/lib/src/cupertino/refresh.dart
@@ -268,6 +268,68 @@ typedef RefreshCallback = Future<void> Function();
 /// sliver such as [CupertinoSliverNavigationBar] and your main scrollable
 /// content's sliver.
 ///
+/// {@tool snippet}
+///
+/// This example shows the usage of [CupertinoSliverRefreshControl].
+///
+/// ```dart
+/// class _CupertinoRefreshControlDemoState
+///     extends State<CupertinoRefreshControlDemo> {
+///   static const listCount = 20;
+///   var randomList = List<int>.generate(listCount, (i) => i + 1);
+///
+///   void _shuffleList() => randomList.shuffle(Random());
+///
+///   @override
+///   Widget build(BuildContext context) {
+///     return CupertinoPageScaffold(
+///       child: CustomScrollView(
+///         // If left unspecified, the [CustomScrollView] appends an
+///         // [AlwaysScrollableScrollPhysics]. Behind the scene, the ScrollableState
+///         // will attach that [AlwaysScrollableScrollPhysics] to the output of
+///         // [ScrollConfiguration.of] which will be a [ClampingScrollPhysics]
+///         // on Android.
+///         // To demonstrate the iOS behavior in this demo and to ensure that the list
+///         // always scrolls, we specifically use a [BouncingScrollPhysics] combined
+///         // with a [AlwaysScrollableScrollPhysics]
+///         physics: const BouncingScrollPhysics(
+///           parent: AlwaysScrollableScrollPhysics(),
+///         ),
+///         slivers: [
+///           CupertinoSliverNavigationBar(
+///             automaticallyImplyLeading: false,
+///             largeTitle: Text(
+///               GalleryLocalizations.of(context).demoCupertinoPullToRefreshTitle,
+///             ),
+///           ),
+///           CupertinoSliverRefreshControl(
+///             onRefresh: () {
+///               return Future<void>.delayed(const Duration(seconds: 1))
+///                 ..then<void>((_) {
+///                   if (mounted) {
+///                     setState(() => _shuffleList());
+///                   }
+///                 });
+///             },
+///           ),
+///           SliverList(
+///             delegate: SliverChildBuilderDelegate(
+///               (context, index) {
+///                 final title = GalleryLocalizations.of(context)
+///                     .starterAppDrawerItem(randomList[index]);
+///                 return ListTile(title: Text(title));
+///               },
+///               childCount: listCount,
+///             ),
+///           ),
+///         ],
+///       ),
+///     );
+///   }
+/// }
+/// ```
+/// {@end-tool}
+///
 /// See also:
 ///
 ///  * [CustomScrollView], a typical sliver holding scroll view this control
