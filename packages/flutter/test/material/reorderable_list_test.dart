7f0fc32a1c1db44f262f85db28dfeab54b7f8654
cg021 <40409839+cg021@users.noreply.github.com>
2020-06-26 15:56:52 -0500

    Update packages/flutter/test/material/reorderable_list_test.dart
    Co-authored-by: Shi-Hao Hong <shihaohong@google.com>

--- packages/flutter/test/material/reorderable_list_test.dart
+++ packages/flutter/test/material/reorderable_list_test.dart
@@ -607,13 +607,13 @@ void main() {
           await tester.pumpWidget(
             MaterialApp(
               home: Directionality(
-              textDirection: TextDirection.ltr,
-              child: NotificationListener<OverscrollNotification>(
-                onNotification: (OverscrollNotification message) { scrolled = true; return false; },
-                child: listViewDefault,
+                textDirection: TextDirection.ltr,
+                child: NotificationListener<OverscrollNotification>(
+                  onNotification: (OverscrollNotification message) { scrolled = true; return false; },
+                  child: listViewDefault,
+                ),
               ),
             ),
-            )
           );
           await tester.dragFrom(const Offset(100.0, 100.0), const Offset(0.0, 100.0));
           expect(scrolled, isFalse);
