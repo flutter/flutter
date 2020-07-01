d6bdf781cd93c6abe27c9c825d3f74128ab531d0
Tianguang Zhang <penn.zht@gmail.com>
2020-07-01 16:22:45 +0200

    Add unanimatedWidget for testing.

--- dev/benchmarks/macrobenchmarks/lib/web_benchmarks.dart
+++ dev/benchmarks/macrobenchmarks/lib/web_benchmarks.dart
@@ -71,6 +71,10 @@ final Map<String, RecorderFactory> benchmarks = <String, RecorderFactory>{
       benchmarkName: '${_galleryBenchmarkPrefix}_studies_perf',
       shouldRunPredicate: (String demo) => typeOfDemo(demo) == DemoType.study,
     ),
+    '${_galleryBenchmarkPrefix}_unanimated_perf': () => GalleryRecorder(
+      benchmarkName: '${_galleryBenchmarkPrefix}_unanimated_perf',
+      shouldRunPredicate: (String demo) => typeOfDemo(demo) == DemoType.unanimatedWidget,
+    ),
   },
 };
 
