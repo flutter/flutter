de205ec13c5bc4c76e7554a3a45f8483359ad7ce
Jonah Williams <jonahwilliams@google.com>
2020-07-06 10:36:05 -0700

    Roll tester version (#60668)
    Includes a fix to a startup race condition and log truncation that should make sure strings aren't cut off

--- dev/bots/test.dart
+++ dev/bots/test.dart
@@ -930,7 +930,7 @@ Future<void> _runFlutterWebTest(String workingDirectory, List<String> tests) asy
   );
 }
 
-const String _supportedTesterVersion = '0.0.1-dev9';
+const String _supportedTesterVersion = '0.0.2-dev5';
 
 Future<void> _pubRunTester(String workingDirectory, {
   List<String> testPaths,
@@ -979,6 +979,7 @@ Future<void> _pubRunTester(String workingDirectory, {
       'run',
       'tester',
       '-j$cpus',
+      '-v',
       '--ci',
       if (perTestTimeout != null)
         '--timeout=$perTestTimeout'
