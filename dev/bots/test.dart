80b5ee4e7c54875ccc70ca7c1b5510b7399b4fc2
Jonah Williams <jonahwilliams@google.com>
2020-07-08 14:39:55 -0700

    Roll tester (#61128)
    landing to unblock meta rollback

--- dev/bots/test.dart
+++ dev/bots/test.dart
@@ -930,7 +930,7 @@ Future<void> _runFlutterWebTest(String workingDirectory, List<String> tests) asy
   );
 }
 
-const String _supportedTesterVersion = '0.0.2-dev5';
+const String _supportedTesterVersion = '0.0.2-dev7';
 
 Future<void> _pubRunTester(String workingDirectory, {
   List<String> testPaths,
