201ae0a0f8d75d086bb369bd65c4caf7e40e2ada
Jonah Williams <jonahwilliams@google.com>
2020-07-08 14:45:35 -0700

    Update asset_bundle_test.dart

--- packages/flutter_tools/test/general.shard/asset_bundle_test.dart
+++ packages/flutter_tools/test/general.shard/asset_bundle_test.dart
@@ -13,7 +13,6 @@ import 'package:flutter_tools/src/bundle.dart';
 import 'package:flutter_tools/src/cache.dart';
 import 'package:flutter_tools/src/devfs.dart';
 import 'package:flutter_tools/src/globals.dart' as globals;
-import 'package:flutter_tools/src/project.dart';
 import 'package:mockito/mockito.dart';
 
 import '../src/common.dart';
