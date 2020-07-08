a39f19a67656f45cda0bf067f873ec67fe83afe3
Kate Lovett <katelovett@google.com>
2020-07-08 15:05:16 -0700

    Remove unused import

--- packages/flutter/test/widgets/focus_traversal_test.dart
+++ packages/flutter/test/widgets/focus_traversal_test.dart
@@ -6,7 +6,6 @@
 
 import 'dart:ui';
 
-import 'package:flutter/foundation.dart';
 import 'package:flutter/painting.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
