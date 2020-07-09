adcd2fc0b8b95079495386865956d6b936242e08
Dan Field <dnfield@google.com>
2020-07-09 09:57:26 -0700

    roll meta

--- packages/flutter_tools/lib/src/commands/update_packages.dart
+++ packages/flutter_tools/lib/src/commands/update_packages.dart
@@ -31,7 +31,6 @@ const Map<String, String> _kManuallyPinnedDependencies = <String, String>{
   'video_player': '0.10.6', // 0.10.7 fails a gallery smoke test for toString.
   'flutter_template_images': '1.0.1', // Must always exactly match flutter_tools template.
   'shelf': '0.7.5',
-  'meta': '1.1.8', // until the `unawaited` export is resolved
 };
 
 class UpdatePackagesCommand extends FlutterCommand {
