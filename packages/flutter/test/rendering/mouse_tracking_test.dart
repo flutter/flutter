1608a46eb376c0819ce36a32ddd3467a4fccc82e
Tong Mu <dkwingsmt@gmail.com>
2020-07-10 13:23:11 -0700

    Fix import

--- packages/flutter/test/rendering/mouse_tracking_test.dart
+++ packages/flutter/test/rendering/mouse_tracking_test.dart
@@ -14,7 +14,7 @@ import 'package:flutter/rendering.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:flutter/services.dart';
-import 'package:vector_math/vector_math_64.dart';
+import 'package:vector_math/vector_math_64.dart' show Matrix4;
 
 import '../flutter_test_alternative.dart';
 
