075dc9b0abaeb78edb414ef69fdafcdd8d775617
Robert Ancell <robert.ancell@canonical.com>
2020-07-10 16:18:16 +1200

    Make app-id matching work if formatted differently

--- packages/flutter_tools/lib/src/project.dart
+++ packages/flutter_tools/lib/src/project.dart
@@ -1059,7 +1059,7 @@ class LinuxProject extends FlutterProjectPlatform implements CmakeBasedProject {
   @override
   String get pluginConfigKey => LinuxPlugin.kConfigKey;
 
-  static final RegExp _applicationIdPattern = RegExp(r'''^\s*"application-id"\s*,\s*['"](.*)['"]\s*,\s*$''');
+  static final RegExp _applicationIdPattern = RegExp(r'"application-id"\s*,\s*"(.*)"\s*');
 
   Directory get _editableDirectory => parent.directory.childDirectory('linux');
 
