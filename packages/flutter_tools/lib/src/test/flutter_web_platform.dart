e546d0183e48022c9a5e491fd2f0cc11297aa139
Jonah Williams <campfish91@gmail.com>
2020-07-13 12:42:50 -0700

    fix debugger uri

--- packages/flutter_tools/lib/src/test/flutter_web_platform.dart
+++ packages/flutter_tools/lib/src/test/flutter_web_platform.dart
@@ -666,8 +666,16 @@ class BrowserManager {
 
   /// Loads [_BrowserEnvironment].
   Future<_BrowserEnvironment> _loadBrowserEnvironment() async {
+    final Uri debugUri = await _browser.debuggerUri;
+    final Uri uri =  Uri(
+      scheme: 'http',
+      host: debugUri.host,
+      port: debugUri.port,
+      path: '/devtools/inspector',
+      query: 'ws=${debugUri.host}:${debugUri.port}${debugUri.path}',
+    );
     return _BrowserEnvironment(
-        this, null, await _browser.debuggerUri, _onRestartController.stream);
+        this, null, uri, _onRestartController.stream);
   }
 
   /// Tells the browser to load a test suite from the URL [url].
