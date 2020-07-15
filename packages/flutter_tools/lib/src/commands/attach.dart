5248b5245786834191a5ff1c3a3ac52c80fd97fe
Pragya007 <pragsprincess24@gmail.com>
2020-07-16 01:09:32 +0530

    Updated instructions of 'flutter help attach'

--- packages/flutter_tools/lib/src/commands/attach.dart
+++ packages/flutter_tools/lib/src/commands/attach.dart
@@ -109,7 +109,19 @@ class AttachCommand extends FlutterCommand {
   final String name = 'attach';
 
   @override
-  final String description = 'Attach to a running application.';
+  final String description = '''There are three ways for the attach command to 
+  discover a running app:
+  1. If the platform is Fuchsia the module name must be provided, e.g. `\$
+  flutter attach --module=mod_name`. This can be called either before or after
+  the application is started, attach will poll the device if it cannot
+  immediately discover the port
+  2. On Android and iOS, just running `flutter attach` suffices. Flutter tools
+  will search for an already running Flutter app or module if available.
+  Otherwise, the tool will wait for the next Flutter app or module to launch
+  before attaching.
+  3. If the app or module is already running and the specific observatory port
+  is known, it can be explicitly provided to attach via the command-line, e.g.
+  `\$ flutter attach --debug-port 12345`''';
 
   int get debugPort {
     if (argResults['debug-port'] == null) {
