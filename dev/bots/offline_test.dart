c3a98443110b8a58dd7849b0bebf0e9f4d675ea8
jonahwilliams <jonahwilliams@google.com>
2020-07-20 13:40:43 -0700

    move to separate script

---
+++ dev/bots/offline_test.dart
@@ -0,0 +1,75 @@
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'dart:io';
+
+import 'package:path/path.dart' as path;
+
+import 'run_command.dart';
+
+final String flutterRoot = path.dirname(path.dirname(path.dirname(path.fromUri(Platform.script))));
+final String flutter = path.join(flutterRoot, 'bin', 'flutter');
+final String dart = path.join(flutterRoot, 'bin', 'cache', 'dart-sdk', 'bin', 'dart');
+
+Future<void> main() async {
+  await _runAndroidPackagingTests();
+}
+
+/// An integration test that verifies than an APK can be built offline after
+/// downloading the prebuilt zip and running the appropriate precache command.
+///
+/// This uses cgroups to block internet access, and will only work on Linux
+/// machines, though similar techniques could be used for macOS and Windows.
+Future<void> _runAndroidPackagingTests() async {
+  final String commit = (await (runAndGetStdout('git', <String>[
+    'rev-parse',
+    'HEAD'
+  ], workingDirectory: '')).join()).trim();
+  await runCommand(dart, <String>[
+    'pub',
+    'get',
+  ], workingDirectory: 'dev/bots');
+
+  // Step 1: Prepare zip packaging.
+  await runCommand(dart, <String>[
+    'dev/bots/prepare_package.dart',
+    '--branch=master',
+    '--output=flutter_zip',
+    '--revision=$commit',
+  ], workingDirectory: '');
+  await runCommand('unzip', <String>[
+    '*.zip'
+  ], workingDirectory: 'flutter_zip');
+
+  // Step 2: Invoke precache using zip packaged flutter
+  await runCommand('flutter_zip/flutter/bin/flutter', <String>[
+    'precache',
+    '--android',
+  ]);
+
+  // Step 3: Create offline cgroup.
+  await runCommand('groupadd', <String>['no-internet']);
+
+  // Step 4: add rule for dropping network activity for this cgroup.
+  await runCommand('iptables', <String>[
+    '-I',
+    'OUTPUT',
+    '1',
+    '-m',
+    'owner',
+    '--gid-owner',
+    'no-internet',
+    '-j',
+    'DROP',
+  ]);
+
+  // Step 4: flutter build apk without internet.
+  await runCommand('sg', <String>[
+    'no-internet',
+    'flutter_zip/bin/flutter',
+    'build',
+    'apk'
+  ], workingDirectory: 'flutter_zip/created_example');
+}
+
