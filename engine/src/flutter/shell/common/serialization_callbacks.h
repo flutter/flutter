63633ce5539819c73f973062a1a337664e5f3876
freiling <freiling@google.com>
2020-07-20 17:03:45 -0700

    Test Framework for SKP based shader warmup (flutter/engine#19260)
    * [fuchsia] fix Rasterizer::ScreenshotLayerTreeAsPicture on Fuchsia
    
    * [fuchsia] Add shell test for skp based shader warmup
    
    * [fuchsia] use custom image serialization when writing out SKP's
    
    This change adds custom image serialization functions for skp serialization
    which write out onlythe image metadata and not the contents of the
    images themselves. This allows SKP's to be used for shader warmup with
    a significantly reduced disk space footprint.

---
+++ engine/src/flutter/shell/common/serialization_callbacks.h
@@ -0,0 +1,27 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef FLUTTER_SHELL_COMMON_SERIALIZATION_CALLBACKS_H_
+#define FLUTTER_SHELL_COMMON_SERIALIZATION_CALLBACKS_H_
+
+#include "flutter/fml/logging.h"
+#include "include/core/SkImage.h"
+#include "include/core/SkPicture.h"
+#include "include/core/SkStream.h"
+#include "include/core/SkTypeface.h"
+
+namespace flutter {
+
+sk_sp<SkData> SerializeTypefaceWithoutData(SkTypeface* typeface, void* ctx);
+sk_sp<SkData> SerializeTypefaceWithData(SkTypeface* typeface, void* ctx);
+
+// Serializes only the metadata of the image and not the underlying pixel data.
+sk_sp<SkData> SerializeImageWithoutData(SkImage* image, void* ctx);
+sk_sp<SkImage> DeserializeImageWithoutData(const void* data,
+                                           size_t length,
+                                           void* ctx);
+
+}  // namespace flutter
+
+#endif  // FLUTTER_SHELL_COMMON_SERIALIZATION_CALLBACKS_H_
