579d287d701f504f736bcf95783904e4b30b25cd
Emmanuel Garcia <egarciad@google.com>
2020-07-21 10:45:10 -0700

    Lint

--- dev/integration_tests/hybrid_android_views/lib/future_data_handler.dart
+++ dev/integration_tests/hybrid_android_views/lib/future_data_handler.dart
@@ -6,17 +6,19 @@ import 'dart:async';
 
 import 'package:flutter_driver/driver_extension.dart';
 
+typedef DriverHandler = Future<String> Function();
+
 /// Wraps a flutter driver [DataHandler] with one that waits until a delegate is set.
 ///
 /// This allows the driver test to call [FlutterDriver.requestData] before the handler was
 /// set by the app in which case the requestData call will only complete once the app is ready
 /// for it.
 class FutureDataHandler {
-  Map<String, Completer<Function()>> _handlers = <String, Completer<Function()>>{};
+  final Map<String, Completer<DriverHandler>> _handlers = <String, Completer<DriverHandler>>{};
 
   /// Registers a lazy handler that will be invoked on the next message from the driver.
-  Completer<Function()> registerHandler(String key) {
-    _handlers[key] = Completer<Function()>();
+  Completer<DriverHandler> registerHandler(String key) {
+    _handlers[key] = Completer<DriverHandler>();
     return _handlers[key];
   }
 
@@ -25,7 +27,7 @@ class FutureDataHandler {
       return 'Unsupported driver message: $message.\n'
              'Supported messages are: ${_handlers.keys}.';
     }
-    final Function() handler = await _handlers[message].future;
+    final DriverHandler handler = await _handlers[message].future;
     return handler();
   }
 }
