5481b3f8ef3e284339ea69fbccfd1457e48b967b
Tong Mu <dkwingsmt@gmail.com>
2020-07-22 15:33:53 -0700

    Fix lint

--- dev/tools/gen_keycodes/lib/gtk_code_gen.dart
+++ dev/tools/gen_keycodes/lib/gtk_code_gen.dart
@@ -2,7 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'dart:io';
 import 'package:path/path.dart' as path;
 
 import 'base_code_gen.dart';
