ee0e1788d97987c736047db357c5e81ed35d9dad
gaaclarke <30870216+gaaclarke@users.noreply.github.com>
2020-07-22 15:38:09 -0700

    Made the linter print out more information in its output and fixed bugs (flutter/engine#19895)

--- engine/src/flutter/fml/memory/ref_counted_unittest.cc
+++ engine/src/flutter/fml/memory/ref_counted_unittest.cc
@@ -1,6 +1,7 @@
 // Copyright 2013 The Flutter Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+// FLUTTER_NOLINT
 
 // This file tests both ref_counted.h and ref_ptr.h (which the former includes).
 // TODO(vtl): Possibly we could separate these tests out better, since a lot of
