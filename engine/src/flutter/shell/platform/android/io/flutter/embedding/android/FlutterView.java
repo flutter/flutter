36d1a9f78c068002a483f7407e9023bd53aaab28
Greg Spencer <gspencergoog@users.noreply.github.com>
2020-07-22 12:36:26 -0700

    Revert "Implement delayed event synthesis key event handling for Android (#19024)" (flutter/engine#19956)
    This reverts commit adeb2599ce8c144daef5e77bd25c4da6cae7bc44 because it breaks flutter_gallery__back_button_memory and a customer test.

--- engine/src/flutter/shell/platform/android/io/flutter/embedding/android/FlutterView.java
+++ engine/src/flutter/shell/platform/android/io/flutter/embedding/android/FlutterView.java
@@ -679,7 +679,8 @@ public class FlutterView extends FrameLayout implements MouseCursorPlugin.MouseC
       return super.onKeyUp(keyCode, event);
     }
 
-    return androidKeyProcessor.onKeyUp(event) || super.onKeyUp(keyCode, event);
+    androidKeyProcessor.onKeyUp(event);
+    return super.onKeyUp(keyCode, event);
   }
 
   /**
@@ -699,7 +700,8 @@ public class FlutterView extends FrameLayout implements MouseCursorPlugin.MouseC
       return super.onKeyDown(keyCode, event);
     }
 
-    return androidKeyProcessor.onKeyDown(event) || super.onKeyDown(keyCode, event);
+    androidKeyProcessor.onKeyDown(event);
+    return super.onKeyDown(keyCode, event);
   }
 
   /**
@@ -849,7 +851,7 @@ public class FlutterView extends FrameLayout implements MouseCursorPlugin.MouseC
             this.flutterEngine.getPlatformViewsController());
     localizationPlugin = this.flutterEngine.getLocalizationPlugin();
     androidKeyProcessor =
-        new AndroidKeyProcessor(this, this.flutterEngine.getKeyEventChannel(), textInputPlugin);
+        new AndroidKeyProcessor(this.flutterEngine.getKeyEventChannel(), textInputPlugin);
     androidTouchProcessor =
         new AndroidTouchProcessor(this.flutterEngine.getRenderer(), /*trackMotionEvents=*/ false);
     accessibilityBridge =
