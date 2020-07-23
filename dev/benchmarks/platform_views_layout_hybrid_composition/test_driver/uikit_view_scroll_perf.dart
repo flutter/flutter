c5a69b9b8ad186e9fce017fd4bfb8ce63f9f4d13
Chris Yang <ychris@google.com>
2020-07-22 18:36:11 -0700

    Reland "Branch out android platform view perf testing #60238" 2nd time (#61748)

--- dev/benchmarks/platform_views_layout_hybrid_composition/test_driver/uikit_view_scroll_perf.dart
+++ dev/benchmarks/platform_views_layout_hybrid_composition/test_driver/uikit_view_scroll_perf.dart
@@ -5,11 +5,9 @@
 import 'package:flutter/widgets.dart';
 import 'package:flutter_driver/driver_extension.dart';
 
-import 'package:platform_views_layout/main.dart' as app;
+import 'package:platform_views_layout_hybrid_composition/main.dart' as app;
 
 void main() {
   enableFlutterDriverExtension();
-  runApp(
-    const app.PlatformViewApp()
-  );
+  runApp(const app.PlatformViewApp());
 }
