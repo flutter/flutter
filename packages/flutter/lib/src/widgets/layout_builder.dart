8489158b51db5fac49bb9d3c8bf9d39c364eaf88
songyiYu <songyi0207@gmail.com>
2020-07-23 22:27:30 +0900

    Implement missing method

--- packages/flutter/lib/src/widgets/layout_builder.dart
+++ packages/flutter/lib/src/widgets/layout_builder.dart
@@ -252,15 +252,53 @@ mixin RenderConstrainedLayoutBuilder<ConstraintType extends Constraints, ChildTy
 /// This example uses a [LayoutBuilder] to build a different widget depending on the available width.
 ///
 /// ```dart
-/// LayoutBuilder(
-///   builder: (BuildContext context, BoxConstraints constraints) {
-///     if (constraints.maxWidth > 600) {
-///       return _buildWideLayout();
-///     } else {
-///       return _buildNormalLayout();
-///     }
-///   },
-/// );
+/// class LayoutBuilderExample extends StatelessWidget {
+///   @override
+///   Widget build(BuildContext context) {
+///     return Scaffold(
+///       appBar: AppBar(title: Text("LayoutBuilder Example")),
+///       body: LayoutBuilder(
+///         builder: (context, constraints) {
+///           if (constraints.maxWidth > 600) {
+///             return _buildWideContainers();
+///           } else {
+///             return _buildNormalContainer();
+///           }
+///         },
+///       ),
+///     );
+///   }
+///
+///   Widget _buildNormalContainer() {
+///     return Center(
+///       child: Container(
+///         height: 100.0,
+///         width: 100.0,
+///         color: Colors.red,
+///       ),
+///     );
+///   }
+///
+///   Widget _buildWideContainers() {
+///     return Center(
+///       child: Row(
+///         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+///         children: <Widget>[
+///           Container(
+///             height: 100.0,
+///             width: 100.0,
+///             color: Colors.red,
+///           ),
+///           Container(
+///             height: 100.0,
+///             width: 100.0,
+///             color: Colors.yellow,
+///           ),
+///         ],
+///       ),
+///     );
+///   }
+/// }
 /// ```
 /// {@end-tool}
 ///
