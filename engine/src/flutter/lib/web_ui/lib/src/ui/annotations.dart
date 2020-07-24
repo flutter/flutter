d0721564eddfac0a9120ac5baf5eba32f93bf275
Siva <a-siva@users.noreply.github.com>
2020-07-24 11:00:34 -0700

    Manual merge of Dart  a3815b6590...24c7666def (flutter/engine#19967)
    * Manual merge or Dart into flutter a3815b6590...24c7666def
    
    dart-lang/sdk@24c7666def Update tests that have the current version number hardcoded
    dart-lang/sdk@f88ce7aef5 Increase Dart version to 2.10
    
    * Update language version.
    
    * Update license.
    
    * Hpdate language version.
    
    * Update license.

--- engine/src/flutter/lib/web_ui/lib/src/ui/annotations.dart
+++ engine/src/flutter/lib/web_ui/lib/src/ui/annotations.dart
@@ -4,7 +4,7 @@
 
 // TODO(dnfield): Remove unused_import ignores when https://github.com/dart-lang/sdk/issues/35164 is resolved.
 
-// @dart = 2.9
+// @dart = 2.10
 part of ui;
 
 // TODO(dnfield): Update this if/when we default this to on in the tool,
