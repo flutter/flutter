8f6b7a78b16fdd6e77445f314c3027f0bf80f3ce
Yuqian Li <liyuqian@google.com>
2020-07-24 10:52:41 -0700

    Fix analyze issues

--- packages/flutter/test/widgets/transform_test.dart
+++ packages/flutter/test/widgets/transform_test.dart
@@ -377,7 +377,6 @@ void main() {
 
   testWidgets(
     '3D transform renders the same with or without needsCompositing',
-    skip: isBrowser, // due to https://github.com/flutter/flutter/issues/42767
     (WidgetTester tester) async {
       for (double angle = 0; angle <= math.pi/4; angle += 0.01) {
         await tester.pumpWidget(RepaintBoundary(child: _generateTransform(true, angle)));
@@ -389,6 +388,7 @@ void main() {
         await expectLater(find.byType(RepaintBoundary).first, matchesReferenceImage(imageWithCompositing));
       }
     },
+    skip: isBrowser, // due to https://github.com/flutter/flutter/issues/42767
   );
 }
 
