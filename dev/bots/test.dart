a79d231dbd64f0631f8e153b8c0ab0f9b95448e5
Jason Simmons <jason-simmons@users.noreply.github.com>
2020-07-23 18:11:17 -0700

    Manual engine roll to 626244a72c5d53cc6d00c840987f9059faed511a (#62163)
    Disabled web_benchmarks_canvaskit in the bot scripts because it was
    failing in the Cirrus environment.

--- dev/bots/test.dart
+++ dev/bots/test.dart
@@ -1227,7 +1227,6 @@ Future<void> _runHostOnlyDeviceLabTests() async {
     if (Platform.isMacOS) () => _runDevicelabTest('plugin_lint_mac'),
     () => _runDevicelabTest('plugin_test', environment: gradleEnvironment),
     if (Platform.isLinux) () => _runDevicelabTest('web_benchmarks_html', environment: kChromeVariables),
-    if (Platform.isLinux) () => _runDevicelabTest('web_benchmarks_canvaskit', environment: kChromeVariables),
   ]..shuffle(math.Random(0));
 
   await _selectIndexedSubshard(tests, kDeviceLabShardCount);
