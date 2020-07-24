2683e4871124d4b35c04f161eef567608a1e2c13
Jonah Williams <jonahwilliams@google.com>
2020-07-24 12:22:45 -0700

    remove usage of dartdev command (flutter/engine#20011)
    * remove usage of dartdev command so that gclient runhooks can re-generate package_config.json

--- engine/src/flutter/tools/run_third_party_dart.py
+++ engine/src/flutter/tools/run_third_party_dart.py
@@ -5,6 +5,6 @@
 import subprocess
 
 if __name__ == '__main__':
-    subprocess.call(["src/third_party/dart/tools/sdks/dart-sdk/bin/dart", "pub", "global", "activate", "-spath", "./src/flutter/tools/generate_package_config"])
-    subprocess.call(["src/third_party/dart/tools/sdks/dart-sdk/bin/dart", "pub", "global", "run", "generate_package_config:generate_from_legacy", "src/flutter/flutter_frontend_server/.packages"])
-    subprocess.call(["src/third_party/dart/tools/sdks/dart-sdk/bin/dart", "pub", "global", "run", "generate_package_config:generate_from_legacy", "src/flutter/tools/const_finder/.packages"])
+    subprocess.call(["src/third_party/dart/tools/sdks/dart-sdk/bin/pub", "global", "activate", "-spath", "./src/flutter/tools/generate_package_config"])
+    subprocess.call(["src/third_party/dart/tools/sdks/dart-sdk/bin/pub", "global", "run", "generate_package_config:generate_from_legacy", "src/flutter/flutter_frontend_server/.packages"])
+    subprocess.call(["src/third_party/dart/tools/sdks/dart-sdk/bin/pub", "global", "run", "generate_package_config:generate_from_legacy", "src/flutter/tools/const_finder/.packages"])
