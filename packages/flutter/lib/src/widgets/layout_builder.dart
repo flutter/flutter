3483b9122da80eb53662be2631d40c5c97dfe1a9
yu songyi <songyi0207@gmail.com>
2020-07-25 17:00:14 +0900

    Fixed an example format for dartpad

--- packages/flutter/lib/src/widgets/layout_builder.dart
+++ packages/flutter/lib/src/widgets/layout_builder.dart
@@ -253,52 +253,49 @@ mixin RenderConstrainedLayoutBuilder<ConstraintType extends Constraints, ChildTy
 /// DartPad window to see [LayoutBuilder] in action!
 /// 
 /// ```dart
-/// class BasicLayoutBuilder extends StatelessWidget {
-///   @override
-///   Widget build(BuildContext context) {
-///     return Scaffold(
-///       appBar: AppBar(title: Text("LayoutBuilder Example")),
-///       body: LayoutBuilder(
-///         builder: (context, constraints) {
-///           if (constraints.maxWidth > 600) {
-///             return _buildWideContainers();
-///           } else {
-///             return _buildNormalContainer();
-///           }
-///         },
-///       ),
-///     );
-///   }
+/// Widget build(BuildContext context) {
+///   return Scaffold(
+///     appBar: AppBar(title: Text("LayoutBuilder Example")),
+///     body: LayoutBuilder(
+///       builder: (context, constraints) {
+///         if (constraints.maxWidth > 600) {
+///           return _buildWideContainers();
+///         } else {
+///           return _buildNormalContainer();
+///         }
+///       },
+///     ),
+///   );
+/// }
 ///
-///   Widget _buildNormalContainer() {
-///     return Center(
-///       child: Container(
-///         height: 100.0,
-///         width: 100.0,
-///         color: Colors.red,
-///       ),
-///     );
-///   }
+/// Widget _buildNormalContainer() {
+///   return Center(
+///     child: Container(
+///       height: 100.0,
+///       width: 100.0,
+///       color: Colors.red,
+///     ),
+///   );
+/// }
 ///
-///   Widget _buildWideContainers() {
-///     return Center(
-///       child: Row(
-///         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-///         children: <Widget>[
-///           Container(
-///             height: 100.0,
-///             width: 100.0,
-///             color: Colors.red,
-///           ),
-///           Container(
-///             height: 100.0,
-///             width: 100.0,
-///             color: Colors.yellow,
-///           ),
-///         ],
-///       ),
-///     );
-///   }
+/// Widget _buildWideContainers() {
+///   return Center(
+///     child: Row(
+///       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+///       children: <Widget>[
+///         Container(
+///           height: 100.0,
+///           width: 100.0,
+///           color: Colors.red,
+///         ),
+///         Container(
+///           height: 100.0,
+///           width: 100.0,
+///           color: Colors.yellow,
+///         ),
+///       ],
+///     ),
+///   );
 /// }
 /// ```
 /// {@end-tool}
