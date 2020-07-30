d603d4b6ce12d333b9be45deba505b591632dc74
MH Johnson <johnsonmh@google.com>
2020-07-30 19:17:02 -0400

    update docs

--- packages/flutter/lib/src/material/bottom_navigation_bar.dart
+++ packages/flutter/lib/src/material/bottom_navigation_bar.dart
@@ -113,15 +113,15 @@ enum BottomNavigationBarType {
 ///       items: const <BottomNavigationBarItem>[
 ///         BottomNavigationBarItem(
 ///           icon: Icon(Icons.home),
-///           title: Text('Home'),
+///           label: 'Home',
 ///         ),
 ///         BottomNavigationBarItem(
 ///           icon: Icon(Icons.business),
-///           title: Text('Business'),
+///           label: 'Business',
 ///         ),
 ///         BottomNavigationBarItem(
 ///           icon: Icon(Icons.school),
-///           title: Text('School'),
+///           label: 'School',
 ///         ),
 ///       ],
 ///       currentIndex: _selectedIndex,
