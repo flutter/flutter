428be907681f7043613cff7807a2c3227236403e
Jonah Williams <jonahwilliams@google.com>
2020-08-04 14:31:05 -0700

    [null-safety] revert export of Fake from test_api (#62894)

--- packages/flutter/test/widgets/basic_test.dart
+++ packages/flutter/test/widgets/basic_test.dart
@@ -10,6 +10,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter/rendering.dart';
+import '../flutter_test_alternative.dart' show Fake;
 
 void main() {
   group('PhysicalShape', () {
