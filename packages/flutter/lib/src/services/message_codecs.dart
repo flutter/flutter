9728b1c7aeeddc1faf6857677db2eb335d5071d5
Ben Li <libe@google.com>
2020-08-03 17:36:48 -0700

    Mute the readValue check for stacktrace.

--- packages/flutter/lib/src/services/message_codecs.dart
+++ packages/flutter/lib/src/services/message_codecs.dart
@@ -567,9 +567,11 @@ class StandardMethodCodec implements MethodCodec {
     final dynamic errorCode = messageCodec.readValue(buffer);
     final dynamic errorMessage = messageCodec.readValue(buffer);
     final dynamic errorDetails = messageCodec.readValue(buffer);
-    final dynamic errorStacktrace = messageCodec.readValue(buffer);
+    /// TODO(libe): Mute the readValue check for stacktrace for now.
+    /// Remove this once flutter engine is ready with the stacktrace fields.
+    final dynamic errorStacktrace = (buffer.hasRemaining) ? messageCodec.readValue(buffer) as String : null;
     if (errorCode is String && (errorMessage == null || errorMessage is String) && !buffer.hasRemaining)
-      throw PlatformException(code: errorCode, message: errorMessage as String, details: errorDetails, stacktrace: errorStacktrace as String);
+      throw PlatformException(code: errorCode, message: errorMessage as String, details: errorDetails, stacktrace: errorStacktrace);
     else
       throw const FormatException('Invalid envelope');
   }
