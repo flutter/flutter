ef326b26efe8956b6cf5e946f1625116e200a023
William Wold <wm@wmww.sh>
2020-08-04 20:04:01 -0400

    Improve FlRenderer interface in preperation for Wayland on Linux (flutter/engine#20006)

--- engine/src/flutter/shell/platform/linux/fl_renderer.h
+++ engine/src/flutter/shell/platform/linux/fl_renderer.h
@@ -40,10 +40,42 @@ struct _FlRendererClass {
                            GdkScreen* screen,
                            EGLint visual_id);
 
-  // Virtual method called when Flutter needs a surface to render to.
-  EGLSurface (*create_surface)(FlRenderer* renderer,
-                               EGLDisplay display,
-                               EGLConfig config);
+  /**
+   * Virtual method called after a GDK window has been created.
+   * This is called once. Does not need to be implemented.
+   */
+  void (*set_window)(FlRenderer* renderer, GdkWindow* window);
+
+  /**
+   * Virtual method to create a new EGL display.
+   */
+  EGLDisplay (*create_display)(FlRenderer* renderer);
+
+  /**
+   * Virtual method called when Flutter needs surfaces to render to.
+   * @renderer: an #FlRenderer.
+   * @display: display to create surfaces on.
+   * @visible: (out): the visible surface that is created.
+   * @resource: (out): the resource surface that is created.
+   * @error: (allow-none): #GError location to store the error occurring, or
+   * %NULL to ignore.
+   *
+   * Returns: %TRUE if both surfaces were created, %FALSE if there was an error.
+   */
+  gboolean (*create_surfaces)(FlRenderer* renderer,
+                              EGLDisplay display,
+                              EGLConfig config,
+                              EGLSurface* visible,
+                              EGLSurface* resource,
+                              GError** error);
+
+  /**
+   * Virtual method called when the EGL window needs to be resized.
+   * Does not need to be implemented.
+   */
+  void (*set_geometry)(FlRenderer* renderer,
+                       GdkRectangle* geometry,
+                       gint scale);
 };
 
 /**
@@ -74,6 +106,15 @@ GdkVisual* fl_renderer_get_visual(FlRenderer* renderer,
                                   GError** error);
 
 /**
+ * fl_renderer_set_window:
+ * @renderer: an #FlRenderer.
+ * @window: the GDK Window this renderer will render to.
+ *
+ * Set the window this renderer will use.
+ */
+void fl_renderer_set_window(FlRenderer* renderer, GdkWindow* window);
+
+/**
  * fl_renderer_start:
  * @renderer: an #FlRenderer.
  * @error: (allow-none): #GError location to store the error occurring, or %NULL
@@ -84,6 +125,16 @@ GdkVisual* fl_renderer_get_visual(FlRenderer* renderer,
  * Returns: %TRUE if successfully started.
  */
 gboolean fl_renderer_start(FlRenderer* renderer, GError** error);
+
+/**
+ * fl_renderer_set_geometry:
+ * @renderer: an #FlRenderer.
+ * @geometry: New size and position (unscaled) of the EGL window.
+ * @scale: Scale of the window.
+ */
+void fl_renderer_set_geometry(FlRenderer* renderer,
+                              GdkRectangle* geometry,
+                              gint scale);
 
 /**
  * fl_renderer_get_proc_address:
