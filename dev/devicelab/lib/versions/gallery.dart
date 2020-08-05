5a237dd83bf5992243972a988c0f18d056e552c1
Tianguang Zhang <penn.zht@gmail.com>
2020-08-05 16:51:04 +0200

    Add gallery version.

---
+++ dev/devicelab/lib/versions/gallery.dart
@@ -0,0 +1,7 @@
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/// The pinned version of flutter gallery, used for devicelab tests.
+const String gallery_version = 'a208eac6e6e8336ae9820e54c572c099231f1da2';
+
