ba5b6ece75fa7e8414e79852a9b587eb637bf198
pq <pquitslund@gmail.com>
2020-08-07 10:10:35 -0700

    - space

--- packages/flutter/lib/src/widgets/widget_inspector.dart
+++ packages/flutter/lib/src/widgets/widget_inspector.dart
@@ -955,7 +955,7 @@ mixin WidgetInspectorService {
   }
 
   /// Whether structured errors are enabled.
-  /// 
+  ///
   /// Structured errors provide semantic information that can be used by IDEs
   /// to enhance the display of errors with rich formatting.
   bool isStructuredErrorsEnabled() {
