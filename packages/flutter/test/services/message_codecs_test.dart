74b5051cd67aeaa1344a31f7a731e619ec82168b
LI DONGZE <lidongze91@gmail.com>
2020-08-10 13:29:58 -0700

    Add native stacktrace field for PlatformException (#62828)
    * Add native stacktrace field for PlatformException
    
    * Mute the readValue check for stacktrace.
    
    * polish
    
    * Add unit test and further polish
    
    * Added more comments
    
    * remove unnecessary import
    
    * fill in stacktrace to JSONMethodCodec and fix tests
    
    Co-authored-by: Ben Li <libe@google.com>

--- packages/flutter/test/services/message_codecs_test.dart
+++ packages/flutter/test/services/message_codecs_test.dart
@@ -9,7 +9,9 @@
 
 import 'dart:typed_data';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
+import 'package:matcher/matcher.dart';
 import '../flutter_test_alternative.dart';
 import 'message_codecs_testing.dart';
 
@@ -42,6 +44,15 @@ void main() {
       );
 
       expect(string.decodeMessage(offsetByteData), ' world');
+    });
+  });
+  group('Decode envelope', () {
+    const MethodCodec method = StandardMethodCodec();
+    const MessageCodec<String> string = StringCodec();
+    const StandardMessageCodec standard = StandardMessageCodec();
+    test('should decode native stacktrace.', () {
+      final ByteData errorData = method.encodeErrorEnvelope(code: 'errorCode', message: 'errorMessage', details: 'errorDetails', stacktrace: 'errorStacktrace',);
+      expect(() => method.decodeEnvelope(errorData), throwsA(predicate((e) => e is PlatformException && e.stacktrace == 'errorStacktrace')));
     });
   });
   group('JSON message codec', () {
@@ -158,3 +169,4 @@ void main() {
     });
   });
 }
+
