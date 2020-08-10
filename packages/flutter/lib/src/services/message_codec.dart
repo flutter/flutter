3bb0450668eb7b4b59245dc68ed5996ae3a23998
Jonah Williams <jonahwilliams@google.com>
2020-08-10 15:15:33 -0700

    Revert "Add native stacktrace field for PlatformException (#62828)"
    This reverts commit 74b5051cd67aeaa1344a31f7a731e619ec82168b.

--- packages/flutter/lib/src/services/message_codec.dart
+++ packages/flutter/lib/src/services/message_codec.dart
@@ -80,10 +80,9 @@ abstract class MethodCodec {
 
   /// Encodes an error result into a binary envelope.
   ///
-  /// The specified error [code], human-readable error [message], error
-  /// [details] correspond to the fields of [PlatformException] and error
-  /// [stacktrace] correspond to stacktrace from native platforms.
-  ByteData encodeErrorEnvelope({ required String code, String? message, dynamic details, String? stacktrace});
+  /// The specified error [code], human-readable error [message], and error
+  /// [details] correspond to the fields of [PlatformException].
+  ByteData encodeErrorEnvelope({ required String code, String? message, dynamic details });
 }
 
 
@@ -108,7 +107,6 @@ class PlatformException implements Exception {
     required this.code,
     this.message,
     this.details,
-    this.stacktrace,
   }) : assert(code != null);
 
   /// An error code.
@@ -120,18 +118,8 @@ class PlatformException implements Exception {
   /// Error details, possibly null.
   final dynamic details;
 
-  /// Native stacktrace for the error, possibly null.
-  /// This is strictly for native platform stacktrace.
-  /// The stacktrace info on dart platform can be found within the try-catch block for example:
-  /// try {
-  ///   ...
-  /// } catch (e, stacktrace) {
-  ///   print(stacktrace);
-  /// }
-  final String? stacktrace;
-
   @override
-  String toString() => 'PlatformException($code, $message, $details, $stacktrace)';
+  String toString() => 'PlatformException($code, $message, $details)';
 }
 
 /// Thrown to indicate that a platform interaction failed to find a handling
