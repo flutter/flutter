cd4192d4eeec7e4671b449a357a37ed46f2b1e66
stuartmorgan <stuartmorgan@google.com>
2020-08-10 09:41:18 -0700

    [windows] Rework controller/engine interaction in the API (#20266)
    Changes the interaction between the view controller and engine in both the C API and
    the engine API, so that there's always an engine (as on other platforms) rather than
    the engine APIs being specific to headless mode.
    
    While adjusting the C API, this does a large cleanup:
    - Renames all methods to follow a `FlutterDesktop` (prefix) + "class" name + method-style name.
      E.g., `FlutterDestkopViewControllerCreate` rather than `FlutterDesktopCreateViewController`.
      This makes it easier to see what functions operate on which conceptual "object" in the API.
    - Reorders and groups them by the object they operate on.
    
    Fixes https://github.com/flutter/flutter/issues/61966

--- shell/platform/windows/client_wrapper/flutter_engine.cc
+++ shell/platform/windows/client_wrapper/flutter_engine.cc
@@ -9,29 +9,14 @@
 
 namespace flutter {
 
-FlutterEngine::FlutterEngine(const DartProject& project)
-    : project_(std::make_unique<DartProject>(project)) {}
-
-FlutterEngine::~FlutterEngine() {
-  ShutDown();
-}
-
-bool FlutterEngine::Run(const char* entry_point) {
-  if (engine_) {
-    std::cerr << "Cannot run an already running engine. Create a new instance "
-                 "or call ShutDown first."
-              << std::endl;
-    return false;
-  }
-
+FlutterEngine::FlutterEngine(const DartProject& project) {
   FlutterDesktopEngineProperties c_engine_properties = {};
-  c_engine_properties.assets_path = project_->assets_path().c_str();
-  c_engine_properties.icu_data_path = project_->icu_data_path().c_str();
-  c_engine_properties.aot_library_path = project_->aot_library_path().c_str();
-  c_engine_properties.entry_point = entry_point;
+  c_engine_properties.assets_path = project.assets_path().c_str();
+  c_engine_properties.icu_data_path = project.icu_data_path().c_str();
+  c_engine_properties.aot_library_path = project.aot_library_path().c_str();
   std::vector<const char*> engine_switches;
   std::transform(
-      project_->engine_switches().begin(), project_->engine_switches().end(),
+      project.engine_switches().begin(), project.engine_switches().end(),
       std::back_inserter(engine_switches),
       [](const std::string& arg) -> const char* { return arg.c_str(); });
   if (engine_switches.size() > 0) {
@@ -39,21 +24,39 @@ bool FlutterEngine::Run(const char* entry_point) {
     c_engine_properties.switches_count = engine_switches.size();
   }
 
-  engine_ = UniqueEnginePtr(FlutterDesktopRunEngine(c_engine_properties),
-                            FlutterDesktopShutDownEngine);
+  engine_ = FlutterDesktopEngineCreate(c_engine_properties);
+}
+
+FlutterEngine::~FlutterEngine() {
+  ShutDown();
+}
+
+bool FlutterEngine::Run(const char* entry_point) {
   if (!engine_) {
-    std::cerr << "Failed to start engine." << std::endl;
+    std::cerr << "Cannot run an engine that failed creation." << std::endl;
     return false;
   }
-  return true;
+  if (has_been_run_) {
+    std::cerr << "Cannot run an engine more than once." << std::endl;
+    return false;
+  }
+  bool run_succeeded = FlutterDesktopEngineRun(engine_, entry_point);
+  if (!run_succeeded) {
+    std::cerr << "Failed to start engine." << std::endl;
+  }
+  has_been_run_ = true;
+  return run_succeeded;
 }
 
 void FlutterEngine::ShutDown() {
+  if (engine_ && owns_engine_) {
+    FlutterDesktopEngineDestroy(engine_);
+  }
   engine_ = nullptr;
 }
 
 std::chrono::nanoseconds FlutterEngine::ProcessMessages() {
-  return std::chrono::nanoseconds(FlutterDesktopProcessMessages(engine_.get()));
+  return std::chrono::nanoseconds(FlutterDesktopEngineProcessMessages(engine_));
 }
 
 FlutterDesktopPluginRegistrarRef FlutterEngine::GetRegistrarForPlugin(
@@ -64,7 +67,12 @@ FlutterDesktopPluginRegistrarRef FlutterEngine::GetRegistrarForPlugin(
               << std::endl;
     return nullptr;
   }
-  return FlutterDesktopGetPluginRegistrar(engine_.get(), plugin_name.c_str());
+  return FlutterDesktopEngineGetPluginRegistrar(engine_, plugin_name.c_str());
+}
+
+FlutterDesktopEngineRef FlutterEngine::RelinquishEngine() {
+  owns_engine_ = false;
+  return engine_;
 }
 
 }  // namespace flutter
--- shell/platform/windows/public/flutter_windows.h
+++ shell/platform/windows/public/flutter_windows.h
@@ -46,9 +46,6 @@ typedef struct {
   // it will be ignored in that case.
   const wchar_t* aot_library_path;
 
-  // The entry point to use in the Dart project. If null, defaults to main().
-  const char* entry_point;
-
   // The switches to pass to the Flutter engine.
   //
   // See: https://github.com/flutter/engine/blob/master/shell/common/switches.h
@@ -59,41 +56,69 @@ typedef struct {
   size_t switches_count;
 } FlutterDesktopEngineProperties;
 
-// Creates a View with the given dimensions running a Flutter Application.
+// ========== View Controller ==========
+
+// Creates a view that hosts and displays the given engine instance.
 //
-// This will set up and run an associated Flutter engine using the settings in
-// |engine_properties|.
+// This takes ownership of |engine|, so FlutterDesktopEngineDestroy should no
+// longer be called on it, as it will be called internally when the view
+// controller is destroyed. If creating the view controller fails, the engine
+// will be destroyed immediately.
 //
-// Returns a null pointer in the event of an error.
+// If |engine| is not already running, the view controller will start running
+// it automatically before displaying the window.
+//
+// The caller owns the returned reference, and is responsible for calling
+// FlutterDesktopViewControllerDestroy. Returns a null pointer in the event of
+// an error.
 FLUTTER_EXPORT FlutterDesktopViewControllerRef
-FlutterDesktopCreateViewController(
-    int width,
-    int height,
-    const FlutterDesktopEngineProperties& engine_properties);
+FlutterDesktopViewControllerCreate(int width,
+                                   int height,
+                                   FlutterDesktopEngineRef engine);
 
 // Shuts down the engine instance associated with |controller|, and cleans up
 // associated state.
 //
 // |controller| is no longer valid after this call.
-FLUTTER_EXPORT void FlutterDesktopDestroyViewController(
+FLUTTER_EXPORT void FlutterDesktopViewControllerDestroy(
     FlutterDesktopViewControllerRef controller);
 
 // Returns the handle for the engine running in FlutterDesktopViewControllerRef.
 //
 // Its lifetime is the same as the |controller|'s.
-FLUTTER_EXPORT FlutterDesktopEngineRef
-FlutterDesktopGetEngine(FlutterDesktopViewControllerRef controller);
-
-// Returns the plugin registrar handle for the plugin with the given name.
-//
-// The name must be unique across the application.
-FLUTTER_EXPORT FlutterDesktopPluginRegistrarRef
-FlutterDesktopGetPluginRegistrar(FlutterDesktopEngineRef engine,
-                                 const char* plugin_name);
-
+FLUTTER_EXPORT FlutterDesktopEngineRef FlutterDesktopViewControllerGetEngine(
+    FlutterDesktopViewControllerRef controller);
 // Returns the view managed by the given controller.
+
 FLUTTER_EXPORT FlutterDesktopViewRef
-FlutterDesktopGetView(FlutterDesktopViewControllerRef controller);
+FlutterDesktopViewControllerGetView(FlutterDesktopViewControllerRef controller);
+
+// ========== Engine ==========
+
+// Creates a Flutter engine with the given properties.
+//
+// The caller owns the returned reference, and is responsible for calling
+// FlutterDesktopEngineDestroy.
+FLUTTER_EXPORT FlutterDesktopEngineRef FlutterDesktopEngineCreate(
+    const FlutterDesktopEngineProperties& engine_properties);
+
+// Shuts down and destroys the given engine instance. Returns true if the
+// shutdown was successful, or if the engine was not running.
+//
+// |engine| is no longer valid after this call.
+FLUTTER_EXPORT bool FlutterDesktopEngineDestroy(FlutterDesktopEngineRef engine);
+
+// Starts running the given engine instance and optional entry point in the Dart
+// project. If the entry point is null, defaults to main().
+//
+// If provided, entry_point must be the name of a top-level function from the
+// same Dart library that contains the app's main() function, and must be
+// decorated with `@pragma(vm:entry-point)` to ensure the method is not
+// tree-shaken by the Dart compiler.
+//
+// Returns false if running the engine failed.
+FLUTTER_EXPORT bool FlutterDesktopEngineRun(FlutterDesktopEngineRef engine,
+                                            const char* entry_point);
 
 // Processes any pending events in the Flutter engine, and returns the
 // number of nanoseconds until the next scheduled event (or max, if none).
@@ -102,40 +127,49 @@ FlutterDesktopGetView(FlutterDesktopViewControllerRef controller);
 // a wait for native events in the runloop should never be longer than the
 // last return value from this function.
 FLUTTER_EXPORT uint64_t
-FlutterDesktopProcessMessages(FlutterDesktopEngineRef engine);
+FlutterDesktopEngineProcessMessages(FlutterDesktopEngineRef engine);
+
+// Returns the plugin registrar handle for the plugin with the given name.
+//
+// The name must be unique across the application.
+FLUTTER_EXPORT FlutterDesktopPluginRegistrarRef
+FlutterDesktopEngineGetPluginRegistrar(FlutterDesktopEngineRef engine,
+                                       const char* plugin_name);
+
+// ========== View ==========
 
 // Return backing HWND for manipulation in host application.
 FLUTTER_EXPORT HWND FlutterDesktopViewGetHWND(FlutterDesktopViewRef view);
 
+// ========== Plugin Registrar (extensions) ==========
+
+// Returns the view associated with this registrar's engine instance
+// This is a Windows-specific extension to flutter_plugin_registrar.h.
+FLUTTER_EXPORT FlutterDesktopViewRef FlutterDesktopPluginRegistrarGetView(
+    FlutterDesktopPluginRegistrarRef registrar);
+
+// ========== Freestanding Utilities ==========
+
 // Gets the DPI for a given |hwnd|, depending on the supported APIs per
 // windows version and DPI awareness mode. If nullptr is passed, returns the DPI
 // of the primary monitor.
+//
+// This uses the same logic and fallback for older Windows versions that is used
+// internally by Flutter to determine the DPI to use for displaying Flutter
+// content, so should be used by any code (e.g., in plugins) that translates
+// between Windows and Dart sizes/offsets.
 FLUTTER_EXPORT UINT FlutterDesktopGetDpiForHWND(HWND hwnd);
 
 // Gets the DPI for a given |monitor|. If the API is not available, a default
 // DPI of 96 is returned.
+//
+// See FlutterDesktopGetDpiForHWND for more information.
 FLUTTER_EXPORT UINT FlutterDesktopGetDpiForMonitor(HMONITOR monitor);
 
 // Reopens stdout and stderr and resysncs the standard library output streams.
 // Should be called if output is being directed somewhere in the runner process
 // (e.g., after an AllocConsole call).
 FLUTTER_EXPORT void FlutterDesktopResyncOutputStreams();
-
-// Runs an instance of a headless Flutter engine.
-//
-// Returns a null pointer in the event of an error.
-FLUTTER_EXPORT FlutterDesktopEngineRef FlutterDesktopRunEngine(
-    const FlutterDesktopEngineProperties& engine_properties);
-
-// Shuts down the given engine instance. Returns true if the shutdown was
-// successful. |engine| is no longer valid after this call.
-FLUTTER_EXPORT bool FlutterDesktopShutDownEngine(
-    FlutterDesktopEngineRef engine);
-
-// Returns the view associated with this registrar's engine instance
-// This is a Windows-specific extension to flutter_plugin_registrar.h.
-FLUTTER_EXPORT FlutterDesktopViewRef
-FlutterDesktopRegistrarGetView(FlutterDesktopPluginRegistrarRef registrar);
 
 #if defined(__cplusplus)
 }  // extern "C"
