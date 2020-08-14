f644d03500732b337912d460c152eaf22d11fd18
Yuqian Li <liyuqian@google.com>
2020-08-13 20:11:47 -0700

    Add a service protocol for raster cache memory (#20466)
    Related issue: https://github.com/flutter/flutter/issues/56719

--- runtime/service_protocol.h
+++ runtime/service_protocol.h
@@ -28,6 +28,7 @@ class ServiceProtocol {
   static const std::string_view kSetAssetBundlePathExtensionName;
   static const std::string_view kGetDisplayRefreshRateExtensionName;
   static const std::string_view kGetSkSLsExtensionName;
+  static const std::string_view kEstimateRasterCacheMemoryExtensionName;
 
   class Handler {
    public:
