0d0183a1c37fc0dd884ce72f45fe7e75cfc4c129
xster <xster@google.com>
2020-08-14 23:13:58 -0700

    account for macos 11 too when looking for mac sdks (#393)

--- build/mac/find_sdk.py
+++ build/mac/find_sdk.py
@@ -55,7 +55,7 @@ def main():
     sdk_dir = xcode43_sdk_path
   else:
     sdk_dir = os.path.join(out.rstrip(), 'SDKs')
-  sdks = [re.findall('^MacOSX(10\.\d+)\.sdk$', s) for s in os.listdir(sdk_dir)]
+  sdks = [re.findall('^MacOSX(1[0|1]\.\d+)\.sdk$', s) for s in os.listdir(sdk_dir)]
   sdks = [s[0] for s in sdks if s]  # [['10.5'], ['10.6']] => ['10.5', '10.6']
   sdks = [s for s in sdks  # ['10.5', '10.6'] => ['10.6']
           if parse_version(s) >= parse_version(min_sdk_version)]
