ba3bbf33138a7fb8d1e3e54f8f6cf1f38e2958fb
Parkwonhui <parkwonhui.dev@gmail.com>
2020-08-17 16:04:08 +0900

    update DecoratedBoxTransition sample code

--- packages/flutter/lib/src/widgets/transitions.dart
+++ packages/flutter/lib/src/widgets/transitions.dart
@@ -946,33 +946,24 @@ class RelativePositionedTransition extends AnimatedWidget {
 ///   super.dispose();
 /// }
 ///
-/// @override
-/// Widget build(BuildContext context) {
-///   return Container(
-///     color: Colors.white,
-///     child: Column(
-///       mainAxisAlignment: MainAxisAlignment.center,
-///       crossAxisAlignment: CrossAxisAlignment.center,
-///       mainAxisSize: MainAxisSize.min,
-///       children: <Widget>[
-///         Center(
-///           child: DecoratedBoxTransition(
-///             position: DecorationPosition.background,
-///             decoration: decorationTween.animate(_controller),
-///             child: Container(
-///               child: Container(
-///                 width: 200,
-///                 height: 200,
-///                 padding: const EdgeInsets.all(10),
-///                 child: const FlutterLogo(),
-///               ),
-///             ),
-///           ),
-///         ),
-///       ],
-///     ),
-///   );
-/// }
+///  @override
+///  Widget build(BuildContext context) {
+///    return Container(
+///      color: Colors.white,
+///      child: Center(
+///        child: DecoratedBoxTransition(
+///          position: DecorationPosition.background,
+///          decoration: decorationTween.animate(_controller),
+///          child: Container(
+///            width: 200,
+///            height: 200,
+///            padding: const EdgeInsets.all(10),
+///            child: const FlutterLogo(),
+///          ),
+///        ),
+///      ),
+///    );
+///  }
 /// ```
 /// {@end-tool}
 ///
