ccc3b766a2ecd3aa2e629b9732307edebbd41733
Mehmet Fidanboylu <mehmetf@users.noreply.github.com>
2020-08-16 21:20:26 -0700

    Enable iOS plumbing for network security and add tests (flutter/engine#20492)

---
+++ engine/src/flutter/shell/platform/darwin/ios/framework/Source/FlutterDartProjectTest.mm
@@ -0,0 +1,85 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import <XCTest/XCTest.h>
+#import "flutter/shell/platform/darwin/ios/framework/Source/FlutterDartProject_Internal.h"
+
+FLUTTER_ASSERT_ARC
+
+@interface FlutterDartProjectTest : XCTestCase
+@end
+
+@implementation FlutterDartProjectTest
+
+- (void)setUp {
+}
+
+- (void)tearDown {
+}
+
+- (void)testMainBundleSettingsAreCorrectlyParsed {
+  NSBundle* mainBundle = [NSBundle mainBundle];
+  NSDictionary* appTransportSecurity =
+      [mainBundle objectForInfoDictionaryKey:@"NSAppTransportSecurity"];
+  XCTAssertTrue([FlutterDartProject allowsArbitraryLoads:appTransportSecurity]);
+  XCTAssertEqualObjects(
+      @"[[\"invalid-site.com\",true,false],[\"sub.invalid-site.com\",false,false]]",
+      [FlutterDartProject domainNetworkPolicy:appTransportSecurity]);
+}
+
+- (void)testEmptySettingsAreCorrect {
+  XCTAssertFalse([FlutterDartProject allowsArbitraryLoads:[[NSDictionary alloc] init]]);
+  XCTAssertEqualObjects(@"", [FlutterDartProject domainNetworkPolicy:[[NSDictionary alloc] init]]);
+}
+
+- (void)testAllowsArbitraryLoads {
+  XCTAssertFalse([FlutterDartProject allowsArbitraryLoads:@{@"NSAllowsArbitraryLoads" : @false}]);
+  XCTAssertTrue([FlutterDartProject allowsArbitraryLoads:@{@"NSAllowsArbitraryLoads" : @true}]);
+}
+
+- (void)testProperlyFormedExceptionDomains {
+  NSDictionary* domainInfoOne = @{
+    @"NSIncludesSubdomains" : @false,
+    @"NSExceptionAllowsInsecureHTTPLoads" : @true,
+    @"NSExceptionMinimumTLSVersion" : @"4.0"
+  };
+  NSDictionary* domainInfoTwo = @{
+    @"NSIncludesSubdomains" : @true,
+    @"NSExceptionAllowsInsecureHTTPLoads" : @false,
+    @"NSExceptionMinimumTLSVersion" : @"4.0"
+  };
+  NSDictionary* domainInfoThree = @{
+    @"NSIncludesSubdomains" : @false,
+    @"NSExceptionAllowsInsecureHTTPLoads" : @true,
+    @"NSExceptionMinimumTLSVersion" : @"4.0"
+  };
+  NSDictionary* exceptionDomains = @{
+    @"domain.name" : domainInfoOne,
+    @"sub.domain.name" : domainInfoTwo,
+    @"sub.two.domain.name" : domainInfoThree
+  };
+  NSDictionary* appTransportSecurity = @{@"NSExceptionDomains" : exceptionDomains};
+  XCTAssertEqualObjects(@"[[\"domain.name\",false,true],[\"sub.domain.name\",true,false],"
+                        @"[\"sub.two.domain.name\",false,true]]",
+                        [FlutterDartProject domainNetworkPolicy:appTransportSecurity]);
+}
+
+- (void)testExceptionDomainsWithMissingInfo {
+  NSDictionary* domainInfoOne = @{@"NSExceptionMinimumTLSVersion" : @"4.0"};
+  NSDictionary* domainInfoTwo = @{
+    @"NSIncludesSubdomains" : @true,
+  };
+  NSDictionary* domainInfoThree = @{};
+  NSDictionary* exceptionDomains = @{
+    @"domain.name" : domainInfoOne,
+    @"sub.domain.name" : domainInfoTwo,
+    @"sub.two.domain.name" : domainInfoThree
+  };
+  NSDictionary* appTransportSecurity = @{@"NSExceptionDomains" : exceptionDomains};
+  XCTAssertEqualObjects(@"[[\"domain.name\",false,false],[\"sub.domain.name\",true,false],"
+                        @"[\"sub.two.domain.name\",false,false]]",
+                        [FlutterDartProject domainNetworkPolicy:appTransportSecurity]);
+}
+
+@end
