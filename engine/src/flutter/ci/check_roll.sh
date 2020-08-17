d737cca58ea68c86286c41fff64698e5acffedb9
Greg Spencer <gspencergoog@users.noreply.github.com>
2020-08-17 14:03:01 -0700

    Clean up the CI scripts and make it so that they can be run from anywhere (flutter/engine#20538)
    This cleans up the ci scripts so that they can be run from an arbitrary directory, and so that they don't have any bash lint issues, and are more explicit about which dart/pub/dartanalyzer executable they run.
    
    I also fixed the format script to take a "--fix" argument that will fix all of the formatting issues found, including trailing whitespace and gn files.
    
    I added a warning to the license script about untracked/ignored files in the fluttter repo because those so often trip up the license script.
    
    I added missing license information to the ci scripts too.
    
    There's now a bit of boilerplate at the beginning of each script (the follow_links function) in order to reliably find the actual location of the script: I'd put it into a common file, except that that code would be needed to reliably find the common location too, so I needed to duplicate it. It's the same boilerplate as what is used in the flutter/flutter repo for the flutter and dart scripts.
    
    I deleted the ci/test.sh script, since it seems to be obsolete (the test it tries to run doesn't exist anywhere).

--- engine/src/flutter/ci/check_roll.sh
+++ engine/src/flutter/ci/check_roll.sh
@@ -1,4 +1,36 @@
 #!/bin/bash
+#
+# Copyright 2013 The Flutter Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+set -e
+
+# Needed because if it is set, cd may print the path it changed to.
+unset CDPATH
+
+# On Mac OS, readlink -f doesn't work, so follow_links traverses the path one
+# link at a time, and then cds into the link destination and find out where it
+# ends up.
+#
+# The function is enclosed in a subshell to avoid changing the working directory
+# of the caller.
+function follow_links() (
+  cd -P "$(dirname -- "$1")"
+  file="$PWD/$(basename -- "$1")"
+  while [[ -h "$file" ]]; do
+    cd -P "$(dirname -- "$file")"
+    file="$(readlink -- "$file")"
+    cd -P "$(dirname -- "$file")"
+    file="$PWD/$(basename -- "$file")"
+  done
+  echo "$file"
+)
+
+SCRIPT_DIR=$(follow_links "$(dirname -- "${BASH_SOURCE[0]}")")
+FLUTTER_DIR="$(cd "$SCRIPT_DIR/.."; pwd -P)"
+
+cd "$FLUTTER_DIR"
 
 if git remote get-url upstream >/dev/null 2>&1; then
   UPSTREAM=upstream/master
@@ -7,7 +39,7 @@ else
 fi;
 
 FLUTTER_VERSION="$(curl -s https://raw.githubusercontent.com/flutter/flutter/master/bin/internal/engine.version)"
-BEHIND="$(git rev-list $FLUTTER_VERSION..$UPSTREAM --oneline | wc -l)"
+BEHIND="$(git rev-list "$FLUTTER_VERSION".."$UPSTREAM" --oneline | wc -l)"
 MAX_BEHIND=16 # no more than 4 bisections to identify the issue
 
 if [[ $BEHIND -le $MAX_BEHIND ]]; then
@@ -18,4 +50,3 @@ else
   echo "       please roll engine into flutter first before merging more commits into engine."
   exit 1
 fi
-
