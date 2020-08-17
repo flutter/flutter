7bc116d662330ff321b462cdac22321e760ea339
Jonah Williams <jonahwilliams@google.com>
2020-08-17 10:24:37 -0700

    Revert "Remove TODOs added for HTTP abort() change in dart sdk (#63924)"
    This reverts commit b1c17313f6cbcd1f7ec083a943b28a076809eb0a.

--- packages/flutter_test/lib/src/_binding_io.dart
+++ packages/flutter_test/lib/src/_binding_io.dart
@@ -228,7 +228,9 @@ class _MockHttpRequest extends HttpClientRequest {
     return Future<HttpClientResponse>.value(_MockHttpResponse());
   }
 
+  // TODO(zichangguo): remove the ignore after the change in dart:io lands.
   @override
+  // ignore: override_on_non_overriding_member
   void abort([Object exception, StackTrace stackTrace]) {}
 
   @override
