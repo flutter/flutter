053becdbd6dcc4aa2c5f906771493c6189a65b9f
Jonah Williams <jonahwilliams@google.com>
2020-08-19 13:25:42 -0700

    [flutter_tools] reland: enable --null-assertions for flutter framework tests (#64120)
    Reland of acdb909 with fixes to semantics.dart
    
    #61042

--- packages/flutter/lib/src/rendering/proxy_box.dart
+++ packages/flutter/lib/src/rendering/proxy_box.dart
@@ -3624,7 +3624,7 @@ class RenderSemanticsAnnotations extends RenderProxyBox {
   RenderSemanticsAnnotations({
     RenderBox child,
     bool container = false,
-    bool explicitChildNodes,
+    bool explicitChildNodes = false,
     bool excludeSemantics = false,
     bool enabled,
     bool checked,
