dc0f28672d316d5053e04d138542cc918c0f9e0d
Alexandre Ardhuin <alexandre.ardhuin@gmail.com>
2020-08-24 20:36:40 +0200

    remove double blank line

--- packages/flutter/lib/src/rendering/view.dart
+++ packages/flutter/lib/src/rendering/view.dart
@@ -2,7 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-
 import 'dart:developer';
 import 'dart:io' show Platform;
 import 'dart:ui' as ui show Scene, SceneBuilder, Window;
