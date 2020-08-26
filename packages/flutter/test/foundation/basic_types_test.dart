aa62084096030a70fb749b45c406df57dfcc5718
Matt Carroll <matthew-carroll@users.noreply.github.com>
2020-08-26 14:52:54 -0700

    Formatting fixes.

--- packages/flutter/test/foundation/basic_types_test.dart
+++ packages/flutter/test/foundation/basic_types_test.dart
@@ -5,47 +5,47 @@ void main() {
   group('lerp Duration', () {
     test('linearly interpolates between positive Durations', () {
       expect(
-        lerpDuration(const Duration(seconds: 1), const Duration(seconds: 2), 0.5), 
+        lerpDuration(const Duration(seconds: 1), const Duration(seconds: 2), 0.5),
         const Duration(milliseconds: 1500),
       );
     });
 
     test('linearly interpolates between negative Durations', () {
       expect(
-        lerpDuration(const Duration(seconds: -1), const Duration(seconds: -2), 0.5), 
+        lerpDuration(const Duration(seconds: -1), const Duration(seconds: -2), 0.5),
         const Duration(milliseconds: -1500),
       );
     });
 
     test('linearly interpolates between positive and negative Durations', () {
       expect(
-        lerpDuration(const Duration(seconds: -1), const Duration(seconds:2), 0.5), 
+        lerpDuration(const Duration(seconds: -1), const Duration(seconds:2), 0.5),
         const Duration(milliseconds: 500),
       );
     });
 
     test('starts at first Duration', () {
       expect(
-        lerpDuration(const Duration(seconds: 1), const Duration(seconds: 2), 0), 
+        lerpDuration(const Duration(seconds: 1), const Duration(seconds: 2), 0),
         const Duration(seconds: 1),
       );
     });
 
     test('ends at second Duration', () {
       expect(
-        lerpDuration(const Duration(seconds: 1), const Duration(seconds: 2), 1), 
+        lerpDuration(const Duration(seconds: 1), const Duration(seconds: 2), 1),
         const Duration(seconds: 2),
       );
     });
 
     test('time values beyond 1.0 have a multiplier effect', () {
       expect(
-        lerpDuration(const Duration(seconds: 1), const Duration(seconds: 2), 5), 
+        lerpDuration(const Duration(seconds: 1), const Duration(seconds: 2), 5),
         const Duration(seconds: 6),
       );
 
       expect(
-        lerpDuration(const Duration(seconds: -1), const Duration(seconds: -2), 5), 
+        lerpDuration(const Duration(seconds: -1), const Duration(seconds: -2), 5),
         const Duration(seconds: -6),
       );
     });
