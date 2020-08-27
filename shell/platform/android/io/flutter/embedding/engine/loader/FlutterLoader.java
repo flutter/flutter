77dd1c05b99d137b5b348598f69ec75c93e0a433
renyou <renyou@google.com>
2020-08-27 11:23:30 -0400

    Revert "Pass platform configuration to Dart VM for insecure socket policy (#20733)" (#20812)
    This reverts commit d241105d7a840a7ba88bc2c0ce4a9f415f83aaab.

--- shell/platform/android/io/flutter/embedding/engine/loader/FlutterLoader.java
+++ shell/platform/android/io/flutter/embedding/engine/loader/FlutterLoader.java
@@ -216,9 +216,10 @@ public class FlutterLoader {
       }
 
       shellArgs.add("--cache-dir-path=" + result.engineCachesPath);
-      if (!flutterApplicationInfo.clearTextPermitted) {
-        shellArgs.add("--disallow-insecure-connections");
-      }
+      // TODO(mehmetf): Announce this since it is a breaking change then enable it.
+      // if (!flutterApplicationInfo.clearTextPermitted) {
+      //   shellArgs.add("--disallow-insecure-connections");
+      // }
       if (flutterApplicationInfo.domainNetworkPolicy != null) {
         shellArgs.add("--domain-network-policy=" + flutterApplicationInfo.domainNetworkPolicy);
       }
