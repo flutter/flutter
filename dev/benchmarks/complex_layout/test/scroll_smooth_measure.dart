ef036147273cb08d2bcad9ff0ee8731192fc7fbe
Ming Lyu (CareF) <minglyu@google.com>
2020-08-27 11:06:10 -0400

    modify according to liyuqian@

--- dev/benchmarks/complex_layout/test/scroll_smooth_measure.dart
+++ dev/benchmarks/complex_layout/test/scroll_smooth_measure.dart
@@ -16,7 +16,7 @@ import 'package:e2e/e2e.dart';
 import 'package:complex_layout/main.dart' as app;
 
 class PointerDataTestBinding extends E2EWidgetsFlutterBinding {
-  // PointerData injection would usually considerred device input and therefore
+  // PointerData injection would usually be considered device input and therefore
   // blocked by [TestWidgetsFlutterBinding]. Override this behavior
   // to help events go into widget tree.
   @override
@@ -29,6 +29,7 @@ class PointerDataTestBinding extends E2EWidgetsFlutterBinding {
   }
 }
 
+/// A union of [ui.PointerDataPacket] and the time it should be sent.
 class PointerDataRecord {
   PointerDataRecord(this.timeStamp, List<ui.PointerData> data)
     : data = ui.PointerDataPacket(data: data);
@@ -36,6 +37,8 @@ class PointerDataRecord {
   final Duration timeStamp;
 }
 
+/// Generates the [PointerDataRecord] to simulate a drag operation from
+/// `center - totalMove/2` to `center + totalMove/2`.
 Iterable<PointerDataRecord> dragInputDatas(
   final Duration epoch,
   final Offset center, {
@@ -43,33 +46,33 @@ Iterable<PointerDataRecord> dragInputDatas(
   final Duration totalTime = const Duration(milliseconds: 2000),
   final double frequency = 90,
 }) sync* {
-  final Offset location = (center - totalMove / 2) * ui.window.devicePixelRatio;
+  final Offset startLocation = (center - totalMove / 2) * ui.window.devicePixelRatio;
   // The issue is about 120Hz input on 90Hz refresh rate device.
   // We test 90Hz input on 60Hz device here, which shows similar pattern.
-  final int intervalCount = totalTime.inMilliseconds * frequency ~/ 1000;
-  final Offset movePerEvent = totalMove / intervalCount.toDouble() * ui.window.devicePixelRatio;
+  final int moveEventCount = totalTime.inMilliseconds * frequency ~/ 1000;
+  final Offset movePerEvent = totalMove / moveEventCount.toDouble() * ui.window.devicePixelRatio;
   yield PointerDataRecord(epoch, <ui.PointerData>[
     ui.PointerData(
       timeStamp: epoch,
       change: ui.PointerChange.add,
-      physicalX: location.dx,
-      physicalY: location.dy,
+      physicalX: startLocation.dx,
+      physicalY: startLocation.dy,
     ),
     ui.PointerData(
       timeStamp: epoch,
       change: ui.PointerChange.down,
-      physicalX: location.dx,
-      physicalY: location.dy,
+      physicalX: startLocation.dx,
+      physicalY: startLocation.dy,
       pointerIdentifier: 1,
     ),
   ]);
-  for (int t = 0; t < intervalCount + 1; t++) {
-    final Offset position = location + movePerEvent * t.toDouble();
+  for (int t = 0; t < moveEventCount + 1; t++) {
+    final Offset position = startLocation + movePerEvent * t.toDouble();
     yield PointerDataRecord(
-      epoch + totalTime * t ~/ intervalCount,
+      epoch + totalTime * t ~/ moveEventCount,
       <ui.PointerData>[ui.PointerData(
         // integer milliseconds is observed from devices
-        timeStamp: epoch + totalTime * t ~/ intervalCount,
+        timeStamp: epoch + totalTime * t ~/ moveEventCount,
         change: ui.PointerChange.move,
         physicalX: position.dx,
         physicalY: position.dy,
@@ -81,7 +84,7 @@ Iterable<PointerDataRecord> dragInputDatas(
       )],
     );
   }
-  final Offset position = location + totalMove;
+  final Offset position = startLocation + totalMove;
   yield PointerDataRecord(epoch + totalTime, <ui.PointerData>[ui.PointerData(
     timeStamp: epoch + totalTime,
     change: ui.PointerChange.up,
@@ -166,6 +169,15 @@ Future<void> main() async {
   }, semanticsEnabled: false, variant: variant);
 }
 
+/// Calculates the smoothness measure from `scrollOffset` and `delays` list.
+///
+/// For smoothness it filters the frames with delays too longer to separate the
+/// effect of building a frame too slow. Frames that are built slow are counted
+/// to `droped_frame_count`.
+///
+/// Smoothness (jerk) is measured by the average of the absolute value for
+/// discrete 2nd derivative (`average_abs_jerk`). And the frames with
+/// estimated `jerk` larger than 0.5 is counted to `janky_count`.
 Map<String, dynamic> scrollSummary(
   List<double> scrollOffset,
   List<Duration> delays,
@@ -177,16 +189,29 @@ Map<String, dynamic> scrollSummary(
   for (int i = 1; i < scrollOffset.length-1; i += 1) {
     if (frameTimestamp[i+1] - frameTimestamp[i-1] > 40E3
         || delays[i] > const Duration(milliseconds: 16)) {
-      // filter datapoints from slow frame building or input simulation artifact
+      // filter data points from slow frame building or input simulation artifact
       lostFrame += 1;
       continue;
     }
     // Using abs rather than square because square (2-norm) amplifies the
     // effect of the data point that's relatively large, but in this metric
-    // we prefer smaller data point to have similiar effect.
+    // we prefer smaller data point to have similar effect.
     // This is also why we count the number of data that's larger than a
     // threshold (and the result is tested not sensitive to this threshold),
-    // which is effectly a 0-norm.
+    // which is effectively a 0-norm.
+    //
+    // go/tq-smooth-scrolling suggests using jerk (3rd derivative of the position)
+    // to measure the smoothness.
+    // Here we are not using 2nd derivative instead because the input is
+    // completely linear and the expected acceleration should be strictly zero.
+    // Observed acceleration is jumping from positive to negative within
+    // adjacent frames, meaning mathematically the discrete 3-rd derivative
+    // (`f[3] - 3*f[2] + 3*f[1] - f[0]`) is not a good approximation of jerk
+    // (continuous 3-rd derivative), while discrete 2nd
+    // derivative (`f[2] - 2*f[1] + f[0]`) on the other hand is a better measure
+    // of how the scrolling deviate away from linear, and given the acceleration
+    // should average to zero within two frames, it's also a good approximation
+    // for jerk in terms of physics.
     final double jerk = (scrollOffset[i-1] + scrollOffset[i+1] - 2*scrollOffset[i]).abs();
     jerkAvg += jerk;
     if (jerk > 0.5)
