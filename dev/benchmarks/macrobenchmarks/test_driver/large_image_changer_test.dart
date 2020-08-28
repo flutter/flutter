7d91a353dd8b1f3d16cdd0fe8d76df92f4712bd0
Dan Field <dfield@gmail.com>
2020-08-28 11:19:56 -0700

    Update large_image_changer_test.dart

--- dev/benchmarks/macrobenchmarks/test_driver/large_image_changer_test.dart
+++ dev/benchmarks/macrobenchmarks/test_driver/large_image_changer_test.dart
@@ -3,7 +3,7 @@
 // found in the LICENSE file.
 
 import 'package:flutter_driver/flutter_driver.dart';
-import 'package:test/test.dart';
+import '../../../test/common.dart';
 
 void main() {
   test('Animate for 20 seconds', () async {
