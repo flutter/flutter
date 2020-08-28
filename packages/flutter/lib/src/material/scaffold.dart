843e2870830b41fa317fb95622e54f354091521a
Kate Lovett <katelovett@google.com>
2020-08-28 15:19:30 -0700

    Sample app

--- packages/flutter/lib/src/material/scaffold.dart
+++ packages/flutter/lib/src/material/scaffold.dart
@@ -114,9 +114,75 @@ class ScaffoldMessenger extends StatefulWidget {
   /// A less elegant but more expedient solution is assign a [GlobalKey] to the
   /// [ScaffoldMessenger], then use the `key.currentState` property to obtain the
   /// [ScaffoldMessengerState] rather than using the [ScaffoldMessenger.of]
-  /// function.
+  /// function. The [MaterialApp.scaffoldMessengerKey] refers to the root
+  /// ScaffoldMessenger that is provided by default.
   ///
-  // TODO(Piinks): Add a sample of this case.
+  /// {@tool dartpad --template=freeform}
+  /// Sometimes [SnackBar]s are produced by code that doesn't have ready access
+  /// to a valid [BuildContext]. One such example of this is when you may want
+  /// to show a SnackBar from a method outside of the `build` function. In these
+  /// cases, you can assign a [GlobalKey] to the [ScaffoldMessenger]. This
+  /// example shows a key being used to obtain the [ScaffoldMessengerState]
+  /// provided by the [MaterialApp].
+  ///
+  /// ```dart imports
+  /// import 'package:flutter/material.dart';
+  /// ```
+  /// ```dart
+  /// void main() => runApp(MyApp());
+  ///
+  /// class MyApp extends StatefulWidget {
+  ///   @override
+  ///  _MyAppState createState() => _MyAppState();
+  /// }
+  ///
+  /// class _MyAppState extends State<MyApp> {
+  ///   final GlobalKey<ScaffoldMessengerState> _scaffoldMessengerKey = GlobalKey<ScaffoldMessengerState>();
+  ///   int _counter = 0;
+  ///
+  ///   void _incrementCounter() {
+  ///     setState(() {
+  ///       _counter++;
+  ///     });
+  ///     if (_counter % 10 == 0) {
+  ///       _scaffoldMessengerKey.currentState.showSnackBar(const SnackBar(
+  ///         content: Text('A multiple of ten!'),
+  ///       ));
+  ///     }
+  ///   }
+  ///
+  ///   @override
+  ///   Widget build(BuildContext context) {
+  ///     return MaterialApp(
+  ///       scaffoldMessengerKey: _scaffoldMessengerKey,
+  ///       home: Scaffold(
+  ///         appBar: AppBar(title: Text('ScaffoldMessenger Demo')),
+  ///         body: Center(
+  ///           child: Column(
+  ///             mainAxisAlignment: MainAxisAlignment.center,
+  ///             children: <Widget>[
+  ///               Text(
+  ///                 'You have pushed the button this many times:',
+  ///               ),
+  ///               Text(
+  ///                 '$_counter',
+  ///                 style: Theme.of(context).textTheme.headline4,
+  ///               ),
+  ///             ],
+  ///           ),
+  ///         ),
+  ///         floatingActionButton: FloatingActionButton(
+  ///           onPressed: _incrementCounter,
+  ///           tooltip: 'Increment',
+  ///           child: Icon(Icons.add),
+  ///         ),
+  ///       ),
+  ///     );
+  ///   }
+  /// }
+  ///
+  /// ```
+  /// {@end-tool}
   ///
   /// If there is no [ScaffoldMessenger] in scope, then this will throw an
   /// exception.
