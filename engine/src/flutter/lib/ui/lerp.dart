4f996a9c11334fa01ceb7bdff07cf9a34e46acaf
Chris Bracken <chris@bracken.jp>
2020-08-30 11:08:59 -0700

    lerpDouble: stricter handling of NaN and infinity (flutter/engine#20871)
    Previously, the behaviour of lerpDouble with respect to NaN and infinity
    was relatively complex and difficult to reason about. This patch
    simplifies the behaviour with respect to those conditions and adds
    documentation and tests.
    
    In general, if `a == b` or both values are null, infinite, or NaN, `a`
    is returned. Otherwise we require `a` and `b` and `t` to be finite or
    null and the result of the linear interpolation is returned.

--- engine/src/flutter/lib/ui/lerp.dart
+++ engine/src/flutter/lib/ui/lerp.dart
@@ -6,14 +6,20 @@
 
 part of dart.ui;
 
-/// Linearly interpolate between two numbers.
-// TODO(cbracken): Consider making a and b non-nullable.
-//                 https://github.com/flutter/flutter/issues/64617
+/// Linearly interpolate between two numbers, `a` and `b`, by an extrapolation
+/// factor `t`.
+///
+/// When `a` and `b` are equal or both NaN, `a` is returned.  Otherwise, if
+/// `a`, `b`, and `t` are required to be finite or null, and the result of `a +
+/// (b - a) * t` is returned, where nulls are defaulted to 0.0.
 double? lerpDouble(num? a, num? b, double t) {
-  if (a == null && b == null)
-    return null;
+  if (a == b || (a?.isNaN == true) && (b?.isNaN == true))
+    return a?.toDouble();
   a ??= 0.0;
   b ??= 0.0;
+  assert(a.isFinite, 'Cannot interpolate between finite and non-finite values');
+  assert(b.isFinite, 'Cannot interpolate between finite and non-finite values');
+  assert(t.isFinite, 't must be finite when interpolating between values');
   return a + (b - a) * t as double;
 }
 
