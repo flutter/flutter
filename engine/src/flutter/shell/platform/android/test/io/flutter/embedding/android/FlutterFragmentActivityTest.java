7b62a6c11a9e4c6f24bc694d480ec94a957797dd
xster <xster@google.com>
2020-08-31 13:43:46 -0700

    Add auto plugin registration to FlutterFragmentActivity as well (flutter/engine#20865)

--- engine/src/flutter/shell/platform/android/test/io/flutter/embedding/android/FlutterFragmentActivityTest.java
+++ engine/src/flutter/shell/platform/android/test/io/flutter/embedding/android/FlutterFragmentActivityTest.java
@@ -1,18 +1,40 @@
 package io.flutter.embedding.android;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
+import android.content.Context;
 import android.content.Intent;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import io.flutter.embedding.android.FlutterActivityLaunchConfigs.BackgroundMode;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.embedding.engine.FlutterJNI;
+import io.flutter.embedding.engine.loader.FlutterLoader;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+import java.util.List;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 
 @Config(manifest = Config.NONE)
 @RunWith(RobolectricTestRunner.class)
 public class FlutterFragmentActivityTest {
+  @Before
+  public void setUp() {
+    GeneratedPluginRegistrant.clearRegisteredEngines();
+  }
+
+  @After
+  public void tearDown() {
+    GeneratedPluginRegistrant.clearRegisteredEngines();
+  }
 
   @Test
   public void createFlutterFragment__defaultRenderModeSurface() {
@@ -42,6 +64,39 @@ public class FlutterFragmentActivityTest {
           }
         };
     assertEquals(activity.createFlutterFragment().getRenderMode(), RenderMode.texture);
+  }
+
+  @Test
+  public void itRegistersPluginsAtConfigurationTime() {
+    FlutterFragmentActivity activity =
+        Robolectric.buildActivity(FlutterFragmentActivityWithProvidedEngine.class).get();
+    assertTrue(GeneratedPluginRegistrant.getRegisteredEngines().isEmpty());
+
+    // Calling onCreate on the FlutterFragmentActivity will create a FlutterFragment and
+    // commit it to the fragment manager. This attaches the fragment to the FlutterFragmentActivity
+    // creating and configuring the engine.
+    activity.onCreate(null);
+
+    List<FlutterEngine> registeredEngines = GeneratedPluginRegistrant.getRegisteredEngines();
+    assertEquals(1, registeredEngines.size());
+    assertEquals(activity.getFlutterEngine(), registeredEngines.get(0));
+  }
+
+  static class FlutterFragmentActivityWithProvidedEngine extends FlutterFragmentActivity {
+    @Override
+    protected FlutterFragment createFlutterFragment() {
+      return FlutterFragment.createDefault();
+    }
+
+    @Nullable
+    @Override
+    public FlutterEngine provideFlutterEngine(@NonNull Context context) {
+      FlutterJNI flutterJNI = mock(FlutterJNI.class);
+      when(flutterJNI.isAttached()).thenReturn(true);
+
+      return new FlutterEngine(
+          context, mock(FlutterLoader.class), flutterJNI, new String[] {}, false);
+    }
   }
 
   private static class FakeFlutterFragmentActivity extends FlutterFragmentActivity {
