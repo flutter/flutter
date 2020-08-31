2bc3a46aa7575e1883c9f3287c22b1c8f71b8a88
Christopher Fujino <christopherfujino@gmail.com>
2020-08-31 16:05:23 -0700

    fix analyzer

--- dev/devicelab/lib/framework/utils.dart
+++ dev/devicelab/lib/framework/utils.dart
@@ -7,7 +7,6 @@ import 'dart:convert';
 import 'dart:io';
 import 'dart:math' as math;
 
-import 'package:args/args.dart';
 import 'package:meta/meta.dart';
 import 'package:path/path.dart' as path;
 import 'package:process/process.dart';
