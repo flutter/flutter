e5afee29510d99fa4429c6a42a7001950dab9d93
Jenn Magder <magder@google.com>
2020-08-31 14:21:40 -0700

    Add observatory Bonjour service to Info.plist

--- dev/devicelab/lib/framework/ios.dart
+++ dev/devicelab/lib/framework/ios.dart
@@ -5,6 +5,8 @@
 import 'dart:async';
 import 'dart:convert';
 
+import 'package:path/path.dart' as path;
+
 import 'utils.dart';
 
 typedef SimulatorFunction = Future<void> Function(String deviceId);
@@ -101,6 +103,40 @@ Future<bool> containsBitcode(String pathToBinary) async {
   });
   return !emptyBitcodeMarkerFound;
 }
+
+Future<bool> bonjourServiceFound(String appBundlePath) async =>
+  (await eval(
+    'plutil',
+    <String>[
+      '-extract',
+      'NSBonjourServices',
+      'xml1',
+      '-o',
+      '-',
+      path.join(
+        appBundlePath,
+        'Info.plist',
+      ),
+    ],
+    canFail: true,
+  )).contains('_dartobservatory._tcp');
+
+Future<bool> localNetworkUsageFound(String appBundlePath) async =>
+  await exec(
+    'plutil',
+    <String>[
+      '-extract',
+      'NSLocalNetworkUsageDescription',
+      'xml1',
+      '-o',
+      '-',
+      path.join(
+        appBundlePath,
+        'Info.plist',
+      ),
+    ],
+    canFail: true,
+  ) == 0;
 
 /// Creates and boots a new simulator, passes the new simulator's identifier to
 /// `testFunction`.
