7f912b03f649e84e08839b07a096edb2a39cbf9d
jonahwilliams <jonahwilliams@google.com>
2020-08-31 10:58:33 -0700

    Revert net test

--- packages/flutter_tools/test/general.shard/base/net_test.dart
+++ packages/flutter_tools/test/general.shard/base/net_test.dart
@@ -234,13 +234,13 @@ void main() {
 
   testWithoutContext('max attempts', () async {
     final Net net = createNet(FakeHttpClient(500));
-    dynamic error;
+    String error;
     List<int> actualResult;
     FakeAsync().run((FakeAsync time) {
       net.fetchUrl(Uri.parse('http://example.invalid/'), maxAttempts: 3).then((List<int> value) {
         actualResult = value;
       }, onError: (dynamic exception) {
-        error = exception;
+        error = 'test failed unexpectedly: $exception';
       });
       expect(testLogger.statusText, '');
       time.elapse(const Duration(milliseconds: 10000));
@@ -251,11 +251,7 @@ void main() {
       );
     });
     expect(testLogger.errorText, isEmpty);
-    expect(error, isA<Exception>()
-      .having((Exception exception) => exception.toString(),
-      'description',
-      contains('Download failed.'),
-    ));
+    expect(error, isNull);
     expect(actualResult, isNull);
   });
 
