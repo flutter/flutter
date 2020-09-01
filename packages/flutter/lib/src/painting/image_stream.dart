a5af4829e1b7a911bb2c9a6d8cc115a82d8f5a42
Dan Field <dnfield@google.com>
2020-08-31 20:51:03 -0700

    review

--- packages/flutter/lib/src/painting/image_stream.dart
+++ packages/flutter/lib/src/painting/image_stream.dart
@@ -30,8 +30,7 @@ class ImageHandle {
 
   factory ImageHandle._create(ui.Image image) {
     assert(image != null);
-    _handles[image] ??= ImageHandle._(image);
-    return _handles[image]!;
+    return _handles[image] ??= ImageHandle._(image);
   }
 
   static final Expando<ImageHandle> _handles = Expando<ImageHandle>();
@@ -518,11 +517,11 @@ abstract class ImageStreamCompleter with Diagnosticable {
     }
   }
 
-  /// Adds a listener callback that is called whenever a new concrete [ImageInfo]
-  /// object is available or an error is reported, if there are any listeners
-  /// added via [addListener]. If a concrete image is already available, or if
-  /// an error has been already reported, this object will notify the listener
-  /// synchronously.
+  /// Adds a listener callback that is called only called if there are listeners
+  /// added via [addListener] and a new concrete [ImageInfo]
+  /// object is available or an error is reported. If a concrete image is
+  /// already available, or if an error has been already reported, this object
+  /// will notify the listener synchronously.
   ///
   /// If the [ImageStreamCompleter] completes multiple images over its lifetime,
   /// this listener's [ImageStreamListener.onImage] will fire multiple times.
