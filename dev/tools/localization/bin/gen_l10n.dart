e77bfefc13b26d5db6486ff0b055184aef139a30
Shi Hao Hong <shihaohong94@gmail.com>
2020-09-02 12:57:38 +0800

    Add comment on how to use existing tool

--- dev/tools/localization/bin/gen_l10n.dart
+++ dev/tools/localization/bin/gen_l10n.dart
@@ -4,6 +4,17 @@
 
 import 'dart:io';
 
+/// Runs `flutter generate_localizations with arguments passed in.
+///
+/// This script exists as a legacy entrypoint, since existing users of
+/// gen_l10n tool used to call
+/// `dart ${FLUTTER}/dev/tools/localizations/bin/gen_l10n.dart <options>` to
+/// generate their Flutter project's localizations resources.
+///
+/// Now, the appropriate way to use this tool is to either define an `l10n.yaml`
+/// file in the Flutter project repository, or call
+/// `flutter generate_localizations <options>`, since the code has moved
+/// into `flutter_tools`.
 Future<void> main(List<String> rawArgs) async {
   final ProcessResult result = await Process.run(
     'flutter',
