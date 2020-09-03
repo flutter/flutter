2e1b51b41617a0946e86fa5368a660f55be19016
Jaime Blasco <git@jaimeblasco.com>
2020-09-03 19:50:11 +0200

    Remove trailing spaces

--- packages/flutter_tools/test/general.shard/web/devfs_web_test.dart
+++ packages/flutter_tools/test/general.shard/web/devfs_web_test.dart
@@ -755,24 +755,24 @@ void main() {
 
   test('allows frame embedding', () async {
     final WebAssetServer webAssetServer = await WebAssetServer.start(
-      null, 
-      'localhost', 
-      0,  
-      null, 
-      true, 
+      null,
+      'localhost',
+      0,
+      null,
+      true,
       true,
       const BuildInfo(
         BuildMode.debug,
         '',
         treeShakeIcons: false,
-      ), 
-      false, 
-      Uri.base, 
-      null, 
+      ),
+      false,
+      Uri.base,
+      null,
       testMode: true);
       
     expect(webAssetServer.defaultResponseHeaders['x-frame-options'], null);
-    await webAssetServer.dispose(); 
+    await webAssetServer.dispose();
   });
 }
 
