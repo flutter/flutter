ef6245d5dbdfb05dafcef86c86285a48afa7bd1c
Jonah Williams <jonahwilliams@google.com>
2020-09-04 17:01:30 -0700

    Revert "Add finders for enableFlutterDriverExtension (#64308)" (#65273)
    This reverts commit db4dda2052f1e71583425e791b4c50c54df5c15c.

--- packages/flutter_driver/test/src/real_tests/find_test.dart
+++ packages/flutter_driver/test/src/real_tests/find_test.dart
@@ -2,15 +2,11 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:flutter_driver/flutter_driver.dart';
 import 'package:flutter_driver/src/common/find.dart';
-import 'package:mockito/mockito.dart';
 
 import '../../common.dart';
 
 void main() {
-  MockDeserialize mockDeserialize;
-
   test('Ancestor finder serialize', () {
     const SerializableFinder of = ByType('Text');
     final SerializableFinder matching = ByValueKey('hello');
@@ -39,7 +35,7 @@ void main() {
       'firstMatchOnly': 'true',
     };
 
-    final Ancestor a = Ancestor.deserialize(serialized, mockDeserialize);
+    final Ancestor a = Ancestor.deserialize(serialized);
     expect(a.of, isA<ByType>());
     expect(a.matching, isA<ByValueKey>());
     expect(a.matchRoot, isTrue);
@@ -74,12 +70,10 @@ void main() {
       'firstMatchOnly': 'true',
     };
 
-    final Descendant a = Descendant.deserialize(serialized, mockDeserialize);
+    final Descendant a = Descendant.deserialize(serialized);
     expect(a.of, isA<ByType>());
     expect(a.matching, isA<ByValueKey>());
     expect(a.matchRoot, isTrue);
     expect(a.firstMatchOnly, isTrue);
   });
 }
-
-class MockDeserialize with Mock, DeserializeFinderFactory { }
