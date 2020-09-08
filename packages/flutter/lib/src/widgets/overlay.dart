005cbe874b73647a424c89b5ec3ea62f9b000bf4
Chun-Heng Tai <chtai@google.com>
2020-09-08 12:06:48 -0700

    add comments

--- packages/flutter/lib/src/widgets/overlay.dart
+++ packages/flutter/lib/src/widgets/overlay.dart
@@ -126,7 +126,9 @@ class OverlayEntry {
   /// This method removes this overlay entry from the overlay immediately. The
   /// UI will be updated in the same frame if this method is called before the
   /// overlay rebuild in this frame; otherwise, the UI will be updated in the
-  /// next frame.
+  /// next frame. This means that it is safe to call during builds, but	also
+  /// that if you do call this after the overlay rebuild, the UI will not update
+  /// until	the next frame (i.e. many milliseconds later).
   void remove() {
     assert(_overlay != null);
     final OverlayState overlay = _overlay;
