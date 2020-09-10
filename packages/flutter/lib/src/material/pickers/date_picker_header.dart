33e86ed3558b9c89f156b2172129b1b79e100cb5
Hamdi Kahloun <hamdikahlounacil@gmail.com>
2020-09-10 09:25:08 +0100

    Wrapping the spacing SizedBox in Flexible

--- packages/flutter/lib/src/material/pickers/date_picker_header.dart
+++ packages/flutter/lib/src/material/pickers/date_picker_header.dart
@@ -142,7 +142,7 @@ class DatePickerHeader extends StatelessWidget {
                  children: <Widget>[
                    const SizedBox(height: 16),
                    Flexible(child: help),
-                   const SizedBox(height: 30),
+                   const Flexible(child: SizedBox(height: 38)),
                    Row(
                      children: <Widget>[
                        Expanded(child: title),
