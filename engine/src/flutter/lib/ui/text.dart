7053de78b8613dfa4908ef44a2bffb63bb10151e
Sam Rawlins <sam.rawlins@gmail.com>
2020-09-11 12:00:02 -0700

    Fix erroneous dartdoc @tool directive. (flutter/engine#21109)

--- engine/src/flutter/lib/ui/text.dart
+++ engine/src/flutter/lib/ui/text.dart
@@ -106,7 +106,7 @@ class FontWeight {
 
 /// A feature tag and value that affect the selection of glyphs in a font.
 ///
-/// {@tool sample}
+/// {@tool sample --template=freeform}
 ///
 /// This example shows usage of several OpenType font features, including
 /// Small Caps (smcp), old-style figures, fractional ligatures and stylistic
