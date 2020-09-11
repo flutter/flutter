9ff7d7ca44e0d19ffc975a0912eb446ad2dcd5d0
Chris Bracken <chris@bracken.jp>
2020-09-11 08:55:37 -0700

    Copyright header hygiene improvements (flutter/engine#21089)
    Add copyright headers in a few files where they were missing.
    
    Trim trailing blank comment line where present, for consistency with
    other engine code.
    
    Use the standard libtxt copyright header in one file where it differed
    (extra (C) and comma compared to other files in libtxt).
    
    This also amends tools/const_finder/test/const_finder_test.dart to look
    for a const an additional four lines down to account for the copyright
    header added to the test fixture.

--- engine/src/flutter/shell/common/shell_test_external_view_embedder.cc
+++ engine/src/flutter/shell/common/shell_test_external_view_embedder.cc
@@ -1,3 +1,7 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 #include "shell_test_external_view_embedder.h"
 
 namespace flutter {
--- engine/src/flutter/tools/generate_package_config/bin/generate_from_legacy.dart
+++ engine/src/flutter/tools/generate_package_config/bin/generate_from_legacy.dart
@@ -1,6 +1,7 @@
-// Copyright (c) 2020, the Dart project authors.  Please see the AUTHORS file
-// for details. All rights reserved. Use of this source code is governed by a
-// BSD-style license that can be found in the LICENSE file.
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 // @dart=2.8
 import 'dart:convert';
 import 'dart:io';
