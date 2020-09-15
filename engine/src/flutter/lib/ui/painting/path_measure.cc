d3f54773b746bfb96b1f4d66408e591f65cd3c53
Chris Bracken <chris@bracken.jp>
2020-09-15 08:37:02 -0700

    Define _USE_MATH_DEFINES on Windows where needed (flutter/engine#21166)

--- engine/src/flutter/lib/ui/painting/path_measure.cc
+++ engine/src/flutter/lib/ui/painting/path_measure.cc
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#define _USE_MATH_DEFINES
-
 #include "flutter/lib/ui/painting/path_measure.h"
 
 #include <cmath>
