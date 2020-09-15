91fe6b0e0daa4a74592ddb1736d9405b6eaf4464
jonahwilliams <jonahwilliams@google.com>
2020-09-14 17:19:11 -0700

    [flutter_tools] remove gradle r8 verification

--- dev/bots/test.dart
+++ dev/bots/test.dart
@@ -1250,7 +1250,6 @@ Future<void> _runHostOnlyDeviceLabTests() async {
     () => _runDevicelabTest('gradle_plugin_bundle_test', environment: gradleEnvironment),
     () => _runDevicelabTest('gradle_plugin_fat_apk_test', environment: gradleEnvironment),
     () => _runDevicelabTest('gradle_plugin_light_apk_test', environment: gradleEnvironment),
-    () => _runDevicelabTest('gradle_r8_test', environment: gradleEnvironment),
 
     () => _runDevicelabTest('module_host_with_custom_build_test', environment: gradleEnvironment, testEmbeddingV2: true),
     () => _runDevicelabTest('module_custom_host_app_name_test', environment: gradleEnvironment),
