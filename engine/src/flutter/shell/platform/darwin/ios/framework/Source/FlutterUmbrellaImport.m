2fe7b47c6f62eef76ca3bb8658bd16ba0867a2a7
Jenn Magder <magder@google.com>
2020-09-15 21:04:01 -0700

    Verify Flutter clang module, add hook for verifying consumer warnings (flutter/engine#21203)

--- engine/src/flutter/shell/platform/darwin/ios/framework/Source/FlutterUmbrellaImport.m
+++ engine/src/flutter/shell/platform/darwin/ios/framework/Source/FlutterUmbrellaImport.m
@@ -4,6 +4,6 @@
 
 // The only point of this file is to ensure that the Flutter framework umbrella header can be
 // cleanly imported from an Objective-C translation unit. The target that uses this file copies the
-// headers to a path that simulats how users would actually consume the framework outside of the
+// headers to a path that simulates how users would actually import the framework outside of the
 // engine source root.
-#import "Flutter.h"
+#import <Flutter/Flutter.h>
