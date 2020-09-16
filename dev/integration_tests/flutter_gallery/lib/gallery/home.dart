4bda3a3a9996142cdebc5fdca9f0ab5d98346036
jonahwilliams <jonahwilliams@google.com>
2020-09-16 09:58:29 -0700

    Revert unrelated changes

--- dev/integration_tests/flutter_gallery/lib/gallery/home.dart
+++ dev/integration_tests/flutter_gallery/lib/gallery/home.dart
@@ -25,7 +25,7 @@ class _FlutterLogo extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
       child: Container(
-        width: 2.0,
+        width: 34.0,
         height: 34.0,
         decoration: const BoxDecoration(
           image: DecorationImage(
