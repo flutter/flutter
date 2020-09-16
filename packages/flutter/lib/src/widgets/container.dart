0767d0926feb0d6271a67b2ec7383cce877e3d06
unknown <andypythonappdeveloper@gmail.com>
2020-09-16 18:02:47 +1000

    Fix null saftey issues

--- packages/flutter/lib/src/widgets/container.dart
+++ packages/flutter/lib/src/widgets/container.dart
@@ -358,7 +358,7 @@ class Container extends StatelessWidget {
   final Matrix4? transform;
 
   /// The transform alignment
-  final Alignment transformAlignment;
+  final Alignment? transformAlignment;
 
   /// The clip behavior when [Container.decoration] is not null.
   ///
@@ -431,7 +431,7 @@ class Container extends StatelessWidget {
       current = Padding(padding: margin!, child: current);
 
     if (transform != null)
-      current = transformAlignment == null ? Transform(transform: transform, child: current) : Transform(transform: transform, child: current, alignment: transformAlignment);
+      current = transformAlignment == null ? Transform(transform: transform!, child: current) : Transform(transform: transform!, child: current, alignment: transformAlignment!);
 
     return current!;
   }
