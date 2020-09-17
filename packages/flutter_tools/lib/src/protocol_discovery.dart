4fee00798a834033f311e66ed36148d18c8fe524
Jenn Magder <magder@google.com>
2020-09-17 15:16:55 -0700

    Inform user how to fix permissions when the observatory URL isn't found on iOS 14 (#65977)

--- packages/flutter_tools/lib/src/protocol_discovery.dart
+++ packages/flutter_tools/lib/src/protocol_discovery.dart
@@ -18,6 +18,7 @@ class ProtocolDiscovery {
     this.serviceName, {
     this.portForwarder,
     this.throttleDuration,
+    this.throttleTimeout,
     this.hostPort,
     this.devicePort,
     this.ipv6,
@@ -34,6 +35,7 @@ class ProtocolDiscovery {
     DeviceLogReader logReader, {
     DevicePortForwarder portForwarder,
     Duration throttleDuration = const Duration(milliseconds: 200),
+    Duration throttleTimeout,
     @required int hostPort,
     @required int devicePort,
     @required bool ipv6,
@@ -44,6 +46,7 @@ class ProtocolDiscovery {
       kObservatoryService,
       portForwarder: portForwarder,
       throttleDuration: throttleDuration,
+      throttleTimeout: throttleTimeout,
       hostPort: hostPort,
       devicePort: devicePort,
       ipv6: ipv6,
@@ -59,6 +62,11 @@ class ProtocolDiscovery {
 
   /// The time to wait before forwarding a new observatory URIs from [logReader].
   final Duration throttleDuration;
+
+  /// The time between URIs are discovered before timing out when scraping the [logReader].
+  ///
+  /// If null, log scanning will continue indefinitely.
+  final Duration throttleTimeout;
 
   StreamSubscription<String> _deviceLogSubscription;
   _BufferedStreamController<Uri> _uriStreamController;
@@ -81,15 +89,21 @@ class ProtocolDiscovery {
   ///
   /// When a new observatory URL: is available in [logReader],
   /// the URLs are forwarded at most once every [throttleDuration].
+  /// Returns when no event has been observed for [throttleTimeout].
   ///
   /// Port forwarding is only attempted when this is invoked,
   /// for each observatory URL in the stream.
   Stream<Uri> get uris {
-    return _uriStreamController.stream
+    Stream<Uri> uriStream = _uriStreamController.stream
       .transform(_throttle<Uri>(
         waitDuration: throttleDuration,
-      ))
-      .asyncMap<Uri>(_forwardPort);
+      ));
+    if (throttleTimeout != null) {
+      // Don't throw a TimeoutException. The URL wasn't found in time, just close the stream.
+      uriStream = uriStream.timeout(throttleTimeout,
+          onTimeout: (EventSink<Uri> sink) => sink.close());
+    }
+    return uriStream.asyncMap<Uri>(_forwardPort);
   }
 
   Future<void> cancel() => _stopScrapingLogs();
