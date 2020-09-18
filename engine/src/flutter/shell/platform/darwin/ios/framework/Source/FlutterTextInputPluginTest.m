f7449e09650d30b0de787f8fea7dba266dae1e97
LongCatIsLooong <31859944+LongCatIsLooong@users.noreply.github.com>
2020-09-18 14:31:36 -0700

    Implement iOS [UITextInput firstRectForRange:] with markedText (flutter/engine#19929)

--- engine/src/flutter/shell/platform/darwin/ios/framework/Source/FlutterTextInputPluginTest.m
+++ engine/src/flutter/shell/platform/darwin/ios/framework/Source/FlutterTextInputPluginTest.m
@@ -15,7 +15,9 @@ FLUTTER_ASSERT_ARC
 @interface FlutterTextInputView ()
 @property(nonatomic, copy) NSString* autofillId;
 
+- (void)setEditableTransform:(NSArray*)matrix;
 - (BOOL)setTextInputState:(NSDictionary*)state;
+- (void)setMarkedRect:(CGRect)markedRect;
 - (void)updateEditingState;
 - (BOOL)isVisibleToAutofill;
 @end
@@ -168,6 +170,12 @@ FLUTTER_ASSERT_ARC
     [passwordView.textField description];
   }
   XCTAssert([[passwordView.textField description] containsString:@"TextField"]);
+}
+
+- (void)ensureOnlyActiveViewCanBecomeFirstResponder {
+  for (FlutterTextInputView* inputView in self.installedInputViews) {
+    XCTAssertEqual(inputView.canBecomeFirstResponder, inputView == textInputPlugin.activeView);
+  }
 }
 
 #pragma mark - EditingState tests
@@ -367,6 +375,53 @@ FLUTTER_ASSERT_ARC
                               }]]);
 }
 
+#pragma mark - UITextInput methods - Tests
+
+- (void)testUpdateFirstRectForRange {
+  FlutterTextInputView* inputView = [[FlutterTextInputView alloc] init];
+  [inputView
+      setTextInputState:@{@"text" : @"COMPOSING", @"composingBase" : @1, @"composingExtent" : @3}];
+
+  CGRect kInvalidFirstRect = CGRectMake(-1, -1, 9999, 9999);
+  FlutterTextRange* range = [FlutterTextRange rangeWithNSRange:NSMakeRange(0, 1)];
+  // yOffset = 200.
+  NSArray* yOffsetMatrix = @[ @1, @0, @0, @0, @0, @1, @0, @0, @0, @0, @1, @0, @0, @200, @0, @1 ];
+  NSArray* zeroMatrix = @[ @0, @0, @0, @0, @0, @0, @0, @0, @0, @0, @0, @0, @0, @0, @0, @0 ];
+
+  // Invalid since we don't have the transform or the rect.
+  XCTAssertTrue(CGRectEqualToRect(kInvalidFirstRect, [inputView firstRectForRange:range]));
+
+  [inputView setEditableTransform:yOffsetMatrix];
+  // Invalid since we don't have the rect.
+  XCTAssertTrue(CGRectEqualToRect(kInvalidFirstRect, [inputView firstRectForRange:range]));
+
+  // Valid rect and transform.
+  CGRect testRect = CGRectMake(0, 0, 100, 100);
+  [inputView setMarkedRect:testRect];
+
+  CGRect finalRect = CGRectOffset(testRect, 0, 200);
+  XCTAssertTrue(CGRectEqualToRect(finalRect, [inputView firstRectForRange:range]));
+  // Idempotent.
+  XCTAssertTrue(CGRectEqualToRect(finalRect, [inputView firstRectForRange:range]));
+
+  // Use an invalid matrix:
+  [inputView setEditableTransform:zeroMatrix];
+  // Invalid matrix is invalid.
+  XCTAssertTrue(CGRectEqualToRect(kInvalidFirstRect, [inputView firstRectForRange:range]));
+  XCTAssertTrue(CGRectEqualToRect(kInvalidFirstRect, [inputView firstRectForRange:range]));
+
+  // Revert the invalid matrix change.
+  [inputView setEditableTransform:yOffsetMatrix];
+  [inputView setMarkedRect:testRect];
+  XCTAssertTrue(CGRectEqualToRect(finalRect, [inputView firstRectForRange:range]));
+
+  // Use an invalid rect:
+  [inputView setMarkedRect:kInvalidFirstRect];
+  // Invalid marked rect is invalid.
+  XCTAssertTrue(CGRectEqualToRect(kInvalidFirstRect, [inputView firstRectForRange:range]));
+  XCTAssertTrue(CGRectEqualToRect(kInvalidFirstRect, [inputView firstRectForRange:range]));
+}
+
 #pragma mark - Autofill - Utilities
 
 - (NSMutableDictionary*)mutablePasswordTemplateCopy {
@@ -405,12 +460,6 @@ FLUTTER_ASSERT_ARC
   // deallocated.
   XCTAssertEqual(self.installedInputViews.count, 1);
   XCTAssertEqual(textInputPlugin.autofillContext.count, 0);
-}
-
-- (void)ensureOnlyActiveViewCanBecomeFirstResponder {
-  for (FlutterTextInputView* inputView in self.installedInputViews) {
-    XCTAssertEqual(inputView.canBecomeFirstResponder, inputView == textInputPlugin.activeView);
-  }
 }
 
 #pragma mark - Autofill - Tests
