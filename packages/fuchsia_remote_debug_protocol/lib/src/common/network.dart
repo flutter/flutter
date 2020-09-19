fe4d1e2a18b737514e35f7a1555155024553521f
Satsuki Ueno <satsukiu@google.com>
2020-09-18 17:44:54 -0700

    Fix ipv4/ipv6 address detection

--- packages/fuchsia_remote_debug_protocol/lib/src/common/network.dart
+++ packages/fuchsia_remote_debug_protocol/lib/src/common/network.dart
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'dart:io';
+import 'dart:core';
 
 /// Determines whether `address` is a valid IPv6 or IPv4 address.
 ///
@@ -17,7 +17,10 @@ void validateAddress(String address) {
 /// Returns true if `address` is a valid IPv6 address.
 bool isIpV6Address(String address) {
   try {
-    InternetAddress(address, type:InternetAddressType.IPv6);
+    // parseIpv6Address fails if there's a zone ID. Since this is still a valid
+    // IP, remove any zone ID before parsing.
+    final List<String> addressParts = address.split('%');
+    Uri.parseIPv6Address(addressParts[0]);
     return true;
   } on FormatException {
     return false;
@@ -27,7 +30,7 @@ bool isIpV6Address(String address) {
 /// Returns true if `address` is a valid IPv4 address.
 bool isIpV4Address(String address) {
   try {
-    InternetAddress(address, type:InternetAddressType.IPv4);
+    Uri.parseIPv4Address(address);
     return true;
   } on FormatException {
     return false;
