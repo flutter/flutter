923eb765f04509e414e837eb741d2e52c4c0698c
Michael Goderbauer <goderbauer@google.com>
2020-09-22 12:57:13 -0700

    Bump dartdoc to 0.34.0 (#66048)

--- dev/bots/docs.sh
+++ dev/bots/docs.sh
@@ -37,7 +37,9 @@ function script_location() {
 
 function generate_docs() {
     # Install and activate dartdoc.
-    "$PUB" global activate dartdoc 0.32.4
+    # NOTE: When updating to a new dartdoc version, please also update
+    # `dartdoc_options.yaml` to include newly introduced error and warning types.
+    "$PUB" global activate dartdoc 0.34.0
 
     # This script generates a unified doc set, and creates
     # a custom index.html, placing everything into dev/docs/doc.
