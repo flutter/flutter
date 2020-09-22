ed57e06434425163f5a526a8088c3a99142b812e
jonahwilliams <jonahwilliams@google.com>
2020-09-21 17:03:02 -0700

    test

--- packages/flutter_tools/test/integration.shard/web_run_test.dart
+++ packages/flutter_tools/test/integration.shard/web_run_test.dart
@@ -27,5 +27,5 @@ void main() {
 
   testWithoutContext('flutter run works on web devices with a unary main function', () async {
     await flutter.run(chrome: true);
-  }, skip: 'Web CI skipped');
+  });
 }
