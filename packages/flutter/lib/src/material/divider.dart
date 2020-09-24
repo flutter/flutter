9104d04c6a3ed60576610cce4549bd8320be3a2c
Jose Alba <jaguado@google.com>
2020-09-24 10:02:42 +0200

    fixed issues

--- packages/flutter/lib/src/material/divider.dart
+++ packages/flutter/lib/src/material/divider.dart
@@ -36,49 +36,49 @@ import 'theme.dart';
 /// ![](https://flutter.github.io/assets-for-api-docs/assets/material/divider.png)
 ///
 /// ```dart
-///   Widget build(BuildContext context) {
-///    return Center(
-///      child: Column(
-///        children: <Widget>[
-///          Expanded(
-///            child: Container(
-///              color: Colors.amber,
-///              child: const Center(
-///                child: Text('Above'),
-///              ),
-///            ),
-///          ),
-///          const Divider(
-///            height: 20,
-///            thickness: 5,
-///            indent: 20,
-///            endIndent: 20,
-///          ),
-///          // Subheader example from Material spec.
-///          // https://material.io/components/dividers#types
-///          Container(
-///            padding: EdgeInsets.only(left: 20),
-///            child: Align(
-///              alignment: AlignmentDirectional.centerStart,
-///              child: Text(
-///                'Subheader',
-///                style: Theme.of(context).textTheme.caption,
-///                textAlign: TextAlign.start,
-///              ),
-///            ),
-///          ),
-///          Expanded(
-///            child: Container(
-///              color: Theme.of(context).colorScheme.primary,
-///              child: const Center(
-///                child: Text('Below'),
-///              ),
-///            ),
-///          ),
-///        ],
-///      ),
-///    );
-///  }
+/// Widget build(BuildContext context) {
+///   return Center(
+///     child: Column(
+///       children: <Widget>[
+///         Expanded(
+///           child: Container(
+///             color: Colors.amber,
+///             child: const Center(
+///               child: Text('Above'),
+///             ),
+///           ),
+///         ),
+///         const Divider(
+///           height: 20,
+///           thickness: 5,
+///           indent: 20,
+///           endIndent: 20,
+///         ),
+///         // Subheader example from Material spec.
+///         // https://material.io/components/dividers#types
+///         Container(
+///           padding: const EdgeInsets.only(left: 20),
+///           child: Align(
+///             alignment: AlignmentDirectional.centerStart,
+///             child: Text(
+///               'Subheader',
+///               style: Theme.of(context).textTheme.caption,
+///               textAlign: TextAlign.start,
+///             ),
+///           ),
+///         ),
+///         Expanded(
+///           child: Container(
+///             color: Theme.of(context).colorScheme.primary,
+///             child: const Center(
+///               child: Text('Below'),
+///             ),
+///           ),
+///         ),
+///       ],
+///     ),
+///   );
+/// }
 /// ```
 /// {@end-tool}
 /// See also:
