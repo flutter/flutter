587dc5b6a146438873a1060e2ef176127e92d722
Kate Lovett <katelovett@google.com>
2020-09-24 14:25:49 -0500

    Analyzer again

--- packages/flutter/lib/src/material/scaffold.dart
+++ packages/flutter/lib/src/material/scaffold.dart
@@ -10,7 +10,6 @@ import 'dart:math' as math;
 import 'dart:ui' show lerpDouble;
 
 import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter/gestures.dart' show DragStartBehavior;
