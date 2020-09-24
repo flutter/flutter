098f40ff3062d55fe0ea21ffd246caa51ad27d9a
Robert Ancell <robert.ancell@canonical.com>
2020-09-24 15:13:59 +1200

    Try the Wayland GDK backend, the engine now supports it

--- packages/flutter_tools/templates/app/linux.tmpl/main.cc
+++ packages/flutter_tools/templates/app/linux.tmpl/main.cc
@@ -1,9 +1,8 @@
 #include "my_application.h"
 
 int main(int argc, char** argv) {
-  // Only X11 is currently supported.
-  // Wayland support is being developed: https://github.com/flutter/flutter/issues/57932.
-  gdk_set_allowed_backends("x11");
+  // We only currently support X11 and Wayland.
+  gdk_set_allowed_backends("wayland,x11");
 
   g_autoptr(MyApplication) app = my_application_new();
   return g_application_run(G_APPLICATION(app), argc, argv);
