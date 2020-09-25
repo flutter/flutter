902b294dc7c15f21f29dcb4f879cb81992f81e85
Yuqian Li <liyuqian@google.com>
2020-09-25 12:17:41 -0700

    Revert "Roll gallery to the newest version (#66584)"
    This reverts commit 2ae09caace4545d9fbbfee281196a97f6db556ce.

--- dev/devicelab/lib/versions/gallery.dart
+++ dev/devicelab/lib/versions/gallery.dart
@@ -3,4 +3,4 @@
 // found in the LICENSE file.
 
 /// The pinned version of flutter gallery, used for devicelab tests.
-const String galleryVersion = 'a48663dc66d1e65a04cde4c1f7f9b094d33935f2';
+const String galleryVersion = 'a208eac6e6e8336ae9820e54c572c099231f1da2';
