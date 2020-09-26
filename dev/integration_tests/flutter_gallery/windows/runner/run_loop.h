2bd542c4fd07ea33466b7347c141e1b43c8b2306
Jonah Williams <campfish91@gmail.com>
2020-09-26 12:39:33 -0700

    update license headers

--- dev/integration_tests/flutter_gallery/windows/runner/run_loop.h
+++ dev/integration_tests/flutter_gallery/windows/runner/run_loop.h
@@ -1,3 +1,7 @@
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 #ifndef RUNNER_RUN_LOOP_H_
 #define RUNNER_RUN_LOOP_H_
 
