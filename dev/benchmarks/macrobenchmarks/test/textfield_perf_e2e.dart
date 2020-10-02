2d9a89c69f42389dbd8aca201246a0b6501901c0
CareF <CareF.Lm@gmail.com>
2020-10-02 15:07:16 -0400

    add comment for page delay

--- dev/benchmarks/macrobenchmarks/test/textfield_perf_e2e.dart
+++ dev/benchmarks/macrobenchmarks/test/textfield_perf_e2e.dart
@@ -12,7 +12,10 @@ void main() {
   macroPerfTestE2E(
     'textfield_perf',
     kTextRouteName,
-    // The driver version doesn't have this delay.
+    // The driver version doesn't have this delay because the delay caused
+    // by the communication between the host and the test device is long enough
+    // for the driver test, but there isn't such delay in this host independent
+    // test.
     pageDelay: const Duration(milliseconds: 50),
     body: (WidgetController controller) async {
       final Finder textfield = find.byKey(const ValueKey<String>('basic-textfield'));
