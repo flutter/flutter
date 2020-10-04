ee8d8c2dcdb91f599b86c6b426ad2e9a562cf899
Jonah Williams <campfish91@gmail.com>
2020-10-04 10:48:58 -0700

    Update resident_runner.dart

--- packages/flutter_tools/lib/src/resident_runner.dart
+++ packages/flutter_tools/lib/src/resident_runner.dart
@@ -4,11 +4,11 @@
 
 import 'dart:async';
 
-import 'package:flutter_tools/src/android/android_device.dart';
 import 'package:meta/meta.dart';
 import 'package:package_config/package_config.dart';
 import 'package:vm_service/vm_service.dart' as vm_service;
 
+import 'android/android_device.dart';
 import 'application_package.dart';
 import 'artifacts.dart';
 import 'asset.dart';
