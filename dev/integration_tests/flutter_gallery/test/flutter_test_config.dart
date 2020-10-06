e1942bbbace0d3cb62160c13b24d0fc3d117c864
Jonah Williams <campfish91@gmail.com>
2020-10-06 12:40:41 -0700

    Update flutter_test_config.dart

--- dev/integration_tests/flutter_gallery/test/flutter_test_config.dart
+++ dev/integration_tests/flutter_gallery/test/flutter_test_config.dart
@@ -2,5 +2,4 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-
 export 'package:flutter_goldens/flutter_goldens.dart' show testExecutable;
