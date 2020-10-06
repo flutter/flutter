d44bcb0daea76ff33d85b6c4b9c9e14e784d782a
Yuqian Li <liyuqian@google.com>
2020-10-06 13:16:31 -0700

    Add the missing parantheses
    Fixes https://github.com/flutter/flutter/issues/67343

--- packages/flutter/lib/src/rendering/platform_view.dart
+++ packages/flutter/lib/src/rendering/platform_view.dart
@@ -205,7 +205,7 @@ class RenderAndroidView extends RenderBox with _PlatformViewGestureMixin {
 
     // Clip the texture if it's going to paint out of the bounds of the renter box
     // (see comment in _paintTexture for an explanation of when this happens).
-    if (size.width < _currentAndroidViewSize.width || size.height < _currentAndroidViewSize.height && clipBehavior != Clip.none) {
+    if ((size.width < _currentAndroidViewSize.width || size.height < _currentAndroidViewSize.height) && clipBehavior != Clip.none) {
       context.pushClipRect(true, offset, offset & size, _paintTexture, clipBehavior: clipBehavior);
       return;
     }
