2bd0e126a5835ce635a0ca3931897434e0792e7a
Emmanuel Garcia <egarciad@google.com>
2020-10-07 16:37:02 -0700

    Ensure JNI is not called from raster thread (flutter/engine#21665)

--- engine/src/flutter/shell/platform/android/external_view_embedder/external_view_embedder.cc
+++ engine/src/flutter/shell/platform/android/external_view_embedder/external_view_embedder.cc
@@ -265,15 +265,17 @@ void AndroidExternalViewEmbedder::BeginFrame(
 
   // The surface size changed. Therefore, destroy existing surfaces as
   // the existing surfaces in the pool can't be recycled.
-  if (frame_size_ != frame_size) {
+  if (frame_size_ != frame_size && raster_thread_merger->IsOnPlatformThread()) {
     surface_pool_->DestroyLayers(jni_facade_);
   }
-  frame_size_ = frame_size;
-  device_pixel_ratio_ = device_pixel_ratio;
+  surface_pool_->SetFrameSize(frame_size);
   // JNI method must be called on the platform thread.
   if (raster_thread_merger->IsOnPlatformThread()) {
     jni_facade_->FlutterViewBeginFrame();
   }
+
+  frame_size_ = frame_size;
+  device_pixel_ratio_ = device_pixel_ratio;
 }
 
 // |ExternalViewEmbedder|
--- engine/src/flutter/shell/platform/android/external_view_embedder/surface_pool.h
+++ engine/src/flutter/shell/platform/android/external_view_embedder/surface_pool.h
@@ -67,6 +67,11 @@ class SurfacePool {
   // Destroys all the layers in the pool.
   void DestroyLayers(std::shared_ptr<PlatformViewAndroidJNI> jni_facade);
 
+  // Sets the frame size used by the layers in the pool.
+  // If the current layers in the pool have a different frame size,
+  // then they are deallocated as soon as |GetLayer| is called.
+  void SetFrameSize(SkISize frame_size);
+
  private:
   // The index of the entry in the layers_ vector that determines the beginning
   // of the unused layers. For example, consider the following vector:
@@ -81,7 +86,15 @@ class SurfacePool {
   //  This indicates that entries starting from 1 can be reused meanwhile the
   //  entry at position 0 cannot be reused.
   size_t available_layer_index_ = 0;
+
+  // The layers in the pool.
   std::vector<std::shared_ptr<OverlayLayer>> layers_;
+
+  // The frame size of the layers in the pool.
+  SkISize current_frame_size_;
+
+  // The frame size to be used by future layers.
+  SkISize requested_frame_size_;
 };
 
 }  // namespace flutter
