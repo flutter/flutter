41a15db631bbe45b7aed347b950c002e437e0981
Marcin Jeleński <marcin@jelenski.net>
2020-10-08 14:45:14 +0200

    Expose finder factory to finder creator

--- packages/flutter_driver/test/src/real_tests/stubs/stub_finder_extension.dart
+++ packages/flutter_driver/test/src/real_tests/stubs/stub_finder_extension.dart
@@ -4,6 +4,7 @@
 
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_driver/driver_extension.dart';
+import 'package:flutter_driver/src/common/create_finder_factory.dart';
 import 'package:flutter_test/src/finders.dart';
 import 'package:flutter_driver/src/common/find.dart';
 
@@ -11,13 +12,17 @@ import 'stub_finder.dart';
 
 class StubFinderExtension extends FinderExtension {
   @override
-  Finder createFinder(SerializableFinder finder) {
+  Finder createFinder(
+    SerializableFinder finder,
+    CreateFinderFactory finderFactory,
+  ) {
     return find.byWidgetPredicate((Widget widget) {
       final Key key = widget.key;
-      if(key is! ValueKey<String>) {
+      if (key is! ValueKey<String>) {
         return false;
       }
-      return (key as ValueKey<String>).value == (finder as StubFinder).keyString;
+      return (key as ValueKey<String>).value ==
+          (finder as StubFinder).keyString;
     });
   }
 
