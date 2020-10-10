d8b41b8c7320a0e50c90820744094418181c6970
Dan Field <dnfield@google.com>
2020-10-09 20:39:19 -0700

    !!

--- packages/flutter/test/widgets/scrollable_test.dart
+++ packages/flutter/test/widgets/scrollable_test.dart
@@ -7,11 +7,12 @@
 import 'dart:ui' as ui;
 
 import 'package:flutter/foundation.dart';
-import 'package:flutter/scheduler.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:flutter/services.dart';
+
+import 'package:flutter_test/flutter_test.dart';
 
 Future<void> pumpTest(
   WidgetTester tester,
