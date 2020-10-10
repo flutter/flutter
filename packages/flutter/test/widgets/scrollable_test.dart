d3efeeebc411e8fe921e3fadf3d3fb3fabbf6e54
Dan Field <dnfield@google.com>
2020-10-09 20:38:46 -0700

    fix import

--- packages/flutter/test/widgets/scrollable_test.dart
+++ packages/flutter/test/widgets/scrollable_test.dart
@@ -8,7 +8,7 @@ import 'dart:ui' as ui;
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter/src/scheduler/ticker.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
