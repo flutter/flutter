fdd38ffc7edb5ddfc148881b581bb9cebfcb4a00
Shadow-NewBee <lai3ping@qq.com>
2020-10-13 21:22:39 +0800

    Solve Chinese Pinyin Composing status problem when textfield has inputFormatters

--- packages/flutter/lib/src/widgets/editable_text.dart
+++ packages/flutter/lib/src/widgets/editable_text.dart
@@ -2222,7 +2222,10 @@ class EditableTextState extends State<EditableText> with AutomaticKeepAliveClien
     // as the pre-formatting value of the previous pass (repeat call).
     final bool textChanged = _value.text != value.text || _value.composing != value.composing;
 
-    if (textChanged) {
+    // Composing text does not need be formate
+    final bool isComposing = value.composing.isValid;
+
+    if (textChanged && !isComposing) {
       // Only format when the text has changed and there are available formatters.
       // Pass through the formatter regardless of repeat status if the input value is
       // different than the stored value.
