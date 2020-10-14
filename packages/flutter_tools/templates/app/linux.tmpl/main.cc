f58c26a05b3654320926bbe5f338a16788955d3a
Robert Ancell <robert.ancell@canonical.com>
2020-10-15 11:09:38 +1300

    Update comment from review

--- packages/flutter_tools/templates/app/linux.tmpl/main.cc
+++ packages/flutter_tools/templates/app/linux.tmpl/main.cc
@@ -1,7 +1,7 @@
 #include "my_application.h"
 
 int main(int argc, char** argv) {
-  // We only currently support X11 and Wayland.
+  // The Linux engine only supports X11 and Wayland.
   gdk_set_allowed_backends("wayland,x11");
 
   g_autoptr(MyApplication) app = my_application_new();
