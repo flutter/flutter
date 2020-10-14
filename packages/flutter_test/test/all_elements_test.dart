e9a9b52fa26ee690bde489810f674fb5e2cfc414
Dan Field <dnfield@google.com>
2020-10-14 15:47:51 -0700

    whups

--- packages/flutter_test/test/all_elements_test.dart
+++ packages/flutter_test/test/all_elements_test.dart
@@ -13,22 +13,22 @@ void main() {
       textDirection: TextDirection.ltr,
       child: Row(
         children: <Widget>[
-          RichText(text: TextSpan(text: 'a')),
-          RichText(text: TextSpan(text: 'b')),
+          RichText(text: const TextSpan(text: 'a')),
+          RichText(text: const TextSpan(text: 'b')),
         ],
       ),
     ));
 
     final List<Element> elements = collectAllElementsFrom(
-      key.currentContext as Element,
+      key.currentContext! as Element,
       skipOffstage: false,
     ).toList();
 
     expect(elements.length, 3);
     expect(elements[0].widget, isA<Row>());
     expect(elements[1].widget, isA<RichText>());
-    expect((elements[1].widget as RichText).text.text, 'a');
+    expect(((elements[1].widget as RichText).text as TextSpan).text, 'a');
     expect(elements[2].widget, isA<RichText>());
-    expect((elements[2].widget as RichText).text.text, 'b');
+    expect(((elements[2].widget as RichText).text as TextSpan).text, 'b');
   });
 }
