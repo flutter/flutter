89df5f36a225bf303929d8b0d6cc09575f385bac
Robert Ancell <robert.ancell@canonical.com>
2020-10-15 11:13:03 +1300

    Just do default GTK behaviour

--- packages/flutter_tools/templates/app/linux.tmpl/main.cc
+++ packages/flutter_tools/templates/app/linux.tmpl/main.cc
@@ -1,9 +1,6 @@
 #include "my_application.h"
 
 int main(int argc, char** argv) {
-  // The Linux engine only supports X11 and Wayland.
-  gdk_set_allowed_backends("wayland,x11");
-
   g_autoptr(MyApplication) app = my_application_new();
   return g_application_run(G_APPLICATION(app), argc, argv);
 }
