926c7d4dc3bf81798061fd4362afd70322939527
Shi Hao Hong <shihaohong94@gmail.com>
2020-10-14 16:06:37 +0800

    Try to clarify list_view_with_inherited_test.dart

--- packages/flutter/test/widgets/list_view_with_inherited_test.dart
+++ packages/flutter/test/widgets/list_view_with_inherited_test.dart
@@ -5,15 +5,27 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter/widgets.dart';
 
-List<int> items = <int>[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+List<String> items = <String>[
+  'one',
+  'two',
+  'three',
+  'four',
+  'five',
+];
 
 Widget buildCard(BuildContext context, int index) {
+  // We still want to populate the list with items beyond the list
+  // provided.
+  if (index >= items.length) {
+    return const SizedBox(height: 100);
+  }
+
   return Container(
-    key: ValueKey<int>(items[index]),
+    key: ValueKey<String>(items[index]),
     height: 100.0,
     child: DefaultTextStyle(
       style: TextStyle(fontSize: 2.0 + items.length.toDouble()),
-      child: Text('${items[index]}', textDirection: TextDirection.ltr),
+      child: Text(items[index], textDirection: TextDirection.ltr),
     ),
   );
 }
@@ -30,15 +42,15 @@ Widget buildFrame() {
 void main() {
   testWidgets('ListView is a build function (smoketest)', (WidgetTester tester) async {
     await tester.pumpWidget(buildFrame());
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsOneWidget);
-    expect(find.text('2'), findsOneWidget);
-    expect(find.text('3'), findsOneWidget);
+    expect(find.text('one'), findsOneWidget);
+    expect(find.text('two'), findsOneWidget);
+    expect(find.text('three'), findsOneWidget);
+    expect(find.text('four'), findsOneWidget);
     items.removeAt(2);
     await tester.pumpWidget(buildFrame());
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsOneWidget);
-    expect(find.text('2'), findsNothing);
-    expect(find.text('3'), findsOneWidget);
+    expect(find.text('one'), findsOneWidget);
+    expect(find.text('two'), findsOneWidget);
+    expect(find.text('three'), findsNothing);
+    expect(find.text('four'), findsOneWidget);
   });
 }
