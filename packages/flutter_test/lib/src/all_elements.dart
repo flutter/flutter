61f360ad2f0b187d3a8c39b6552808369c8f26ba
Jonah Williams <jonahwilliams@google.com>
2020-10-15 08:04:20 -0700

    Revert "Improve performance of collectAllElements (#68065)"
    This reverts commit 8ba5732c4c874f08d853f30b10f1912efc992cc3.

--- packages/flutter_test/lib/src/all_elements.dart
+++ packages/flutter_test/lib/src/all_elements.dart
@@ -23,39 +23,15 @@ Iterable<Element> collectAllElementsFrom(
   return CachingIterable<Element>(_DepthFirstChildIterator(rootElement, skipOffstage));
 }
 
-/// Provides a recursive, efficient, depth first search of an element tree.
-///
-/// [Element.visitChildren] does not guarnatee order, but does guarnatee stable
-/// order. This iterator also guarantees stable order, and iterates in a left
-/// to right order:
-///
-///       1
-///     /   \
-///    2     3
-///   / \   / \
-///  4   5 6   7
-///
-/// Will iterate in order 2, 4, 5, 3, 6, 7.
-///
-/// Performance is important here because this method is on the critical path
-/// for flutter_driver and package:integration_test performance tests.
-/// Performance is measured in the all_elements_bench microbenchmark.
-/// Any changes to this implementation should check the before and after numbers
-/// on that benchmark to avoid regressions in general performance test overhead.
-///
-/// If we could use RTL order, we could save on performance, but numerous tests
-/// have been written (and developers clearly expect) that LTR order will be
-/// respected.
 class _DepthFirstChildIterator implements Iterator<Element> {
-  _DepthFirstChildIterator(Element rootElement, this.skipOffstage) {
-    _fillChildren(rootElement);
-  }
+  _DepthFirstChildIterator(Element rootElement, this.skipOffstage)
+    : _stack = _reverseChildrenOf(rootElement, skipOffstage).toList();
 
   final bool skipOffstage;
 
   late Element _current;
 
-  final List<Element> _stack = <Element>[];
+  final List<Element> _stack;
 
   @override
   Element get current => _current;
@@ -66,26 +42,20 @@ class _DepthFirstChildIterator implements Iterator<Element> {
       return false;
 
     _current = _stack.removeLast();
-    _fillChildren(_current);
+    // Stack children in reverse order to traverse first branch first
+    _stack.addAll(_reverseChildrenOf(_current, skipOffstage));
 
     return true;
   }
 
-  void _fillChildren(Element element) {
+  static Iterable<Element> _reverseChildrenOf(Element element, bool skipOffstage) {
     assert(element != null);
-    // If we did not have to follow LTR order and could instead use RTL,
-    // we could avoid reversing this and the operation would be measurably
-    // faster. Unfortunately, a lot of tests depend on LTR order.
-    final List<Element> reversed = <Element>[];
+    final List<Element> children = <Element>[];
     if (skipOffstage) {
-      element.debugVisitOnstageChildren(reversed.add);
+      element.debugVisitOnstageChildren(children.add);
     } else {
-      element.visitChildren(reversed.add);
+      element.visitChildren(children.add);
     }
-    // This is faster than _stack.addAll(reversed.reversed), presumably since
-    // we don't actually care about maintaining an iteration pointer.
-    while (reversed.isNotEmpty) {
-      _stack.add(reversed.removeLast());
-    }
+    return children.reversed;
   }
 }
