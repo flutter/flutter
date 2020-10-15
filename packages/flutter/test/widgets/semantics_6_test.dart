42f5fa5af6d7c93d4574c7d05bb7916687424bf4
Shi Hao Hong <shihaohong94@gmail.com>
2020-10-15 13:45:16 +0800

    Remove unused import

--- packages/flutter/test/widgets/semantics_6_test.dart
+++ packages/flutter/test/widgets/semantics_6_test.dart
@@ -6,7 +6,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:meta/meta.dart';
 
 import 'semantics_tester.dart';
 
