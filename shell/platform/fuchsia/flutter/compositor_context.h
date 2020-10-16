1bc025d6cbf1136c2e96c6fb041d24202ea47ff0
Kaushik Iska <iska.kaushik@gmail.com>
2020-10-15 17:22:02 -0700

    [fuchsia] External view embedder will be shared with platform view (#21850)

--- shell/platform/fuchsia/flutter/compositor_context.h
+++ shell/platform/fuchsia/flutter/compositor_context.h
@@ -21,16 +21,17 @@ namespace flutter_runner {
 // Fuchsia.
 class CompositorContext final : public flutter::CompositorContext {
  public:
-  CompositorContext(SessionConnection& session_connection,
-                    VulkanSurfaceProducer& surface_producer,
-                    flutter::SceneUpdateContext& scene_update_context);
+  CompositorContext(
+      SessionConnection& session_connection,
+      VulkanSurfaceProducer& surface_producer,
+      std::shared_ptr<flutter::SceneUpdateContext> scene_update_context);
 
   ~CompositorContext() override;
 
  private:
   SessionConnection& session_connection_;
   VulkanSurfaceProducer& surface_producer_;
-  flutter::SceneUpdateContext& scene_update_context_;
+  std::shared_ptr<flutter::SceneUpdateContext> scene_update_context_;
 
   // |flutter::CompositorContext|
   std::unique_ptr<ScopedFrame> AcquireFrame(
