c449c8ee5035a562317ab189f7ca603e233d987f
Chris Bracken <chris@bracken.jp>
2020-10-16 12:44:49 -0700

    Eliminate FLUTTER_NOLINT where possible (#21904)
    This removes most of the remaining FLUTTER_NOLINT comments and opts
    these files back into linter enforcement.
    
    I've filed https://github.com/flutter/flutter/issues/68273 to require
    that all FLUTTER_NOLINT comments be followed by a GitHub issue URL
    describing the problem to be fixed.

--- shell/common/input_events_unittests.cc
+++ shell/common/input_events_unittests.cc
@@ -1,7 +1,6 @@
 // Copyright 2013 The Flutter Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-// FLUTTER_NOLINT
 
 #include "flutter/shell/common/shell_test.h"
 #include "flutter/testing/testing.h"
