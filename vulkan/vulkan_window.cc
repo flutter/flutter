b8fd0013a8ff2cd5a75af4aecab6e8e39af592d2
Chris Bracken <chris@bracken.jp>
2020-10-16 16:08:36 -0700

    Update FLUTTER_NOLINT uses to include issue link (#21921)
    In an upcoming patch, we'll enable enforcement that all FLUTTER_NOLINT
    comments include an issue link. This migrates the remaining uses to that
    format.
    
    Bug: https://github.com/flutter/flutter/issues/68273

--- vulkan/vulkan_window.cc
+++ vulkan/vulkan_window.cc
@@ -1,7 +1,8 @@
 // Copyright 2013 The Flutter Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-// FLUTTER_NOLINT
+
+// FLUTTER_NOLINT: https://github.com/flutter/flutter/issues/68331
 
 #include "vulkan_window.h"
 
