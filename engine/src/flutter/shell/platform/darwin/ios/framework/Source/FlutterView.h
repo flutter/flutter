ce530b377084fbf3b1be9aeea9a4194eaf5fcfde
Kaushik Iska <iska.kaushik@gmail.com>
2020-10-15 19:56:13 -0700

    [ios] Refactor IOSSurface factory and unify surface creation (flutter/engine#21877)

--- engine/src/flutter/shell/platform/darwin/ios/framework/Source/FlutterView.h
+++ engine/src/flutter/shell/platform/darwin/ios/framework/Source/FlutterView.h
@@ -32,7 +32,6 @@
 
 - (instancetype)initWithDelegate:(id<FlutterViewEngineDelegate>)delegate
                           opaque:(BOOL)opaque NS_DESIGNATED_INITIALIZER;
-- (std::unique_ptr<flutter::IOSSurface>)createSurface:(std::shared_ptr<flutter::IOSContext>)context;
 
 // Set by FlutterEngine or FlutterViewController to override software rendering.
 @property(class, nonatomic) BOOL forceSoftwareRendering;
