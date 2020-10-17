f5f38e9000d4d8caf5a702561b58937ab0b414c9
godofredoc <54371434+godofredoc@users.noreply.github.com>
2020-10-16 21:51:37 -0700

    Collect logs in the background. (flutter/engine#21828)
    * Collect logs in the background.
    
    * Use fuchsia_ctl log_file option.
    
    * Delete the correct file.
    
    * Remove commented code.

--- engine/src/flutter/testing/fuchsia/run_tests.sh
+++ engine/src/flutter/testing/fuchsia/run_tests.sh
@@ -45,20 +45,12 @@ fuchsia_ctl() {
 }
 
 reboot() {
-  fuchsia_ctl ssh \
-       --timeout-seconds $ssh_timeout_seconds \
-       --identity-file $pkey \
-       -c "log_listener --dump_logs yes --file /tmp/log.txt"
   # As we are not using recipes we don't have a way to know the location
   # to upload the log to isolated. We are saving the log to a file to avoid dart
   # hanging when running the process and then just using printing the content to
   # the console.
-  fuchsia_ctl ssh \
-       --timeout-seconds $ssh_timeout_seconds \
-       --identity-file $pkey \
-       -c "cat /tmp/log.txt"
-
-
+  kill -9 $PID
+  cat /tmp/log.txt
   echo "$(date) START:REBOOT ----------------------------------------"
   # note: this will set an exit code of 255, which we can ignore.
   fuchsia_ctl ssh \
@@ -82,6 +74,9 @@ for i in {1..10}; do
       --identity-file $pkey \
       -c "echo up" && break || sleep 15;
 done
+rm -rf /tmp/log.txt
+fuchsia_ctl ssh --timeout-seconds 1800 --identity-file $pkey -c "log_listener" --log-file /tmp/log.txt &
+PID=$!
 echo "$(date) END:WAIT_DEVICE_READY ---------------------------------"
 
 echo "$(date) START:EXTRACT_PACKAGES  -------------------------------"
