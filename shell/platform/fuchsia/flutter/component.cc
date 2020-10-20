e78e405e6a2fcb79956280be019e916a12a8de05
Clement Skau <cskau-g@users.noreply.github.com>
2020-10-20 10:31:15 +0200

    Enable lazy-async-stacks by-default in all modes (Take 4) (#21802)

--- shell/platform/fuchsia/flutter/component.cc
+++ shell/platform/fuchsia/flutter/component.cc
@@ -402,9 +402,7 @@ Application::Application(
   settings_.task_observer_remove = std::bind(
       &CurrentMessageLoopRemoveAfterTaskObserver, std::placeholders::_1);
 
-  // TODO(FL-117): Re-enable causal async stack traces when this issue is
-  // addressed.
-  settings_.dart_flags = {"--no_causal_async_stacks"};
+  settings_.dart_flags = {"--no_causal_async_stacks", "--lazy_async_stacks"};
 
   // Don't collect CPU samples from Dart VM C++ code.
   settings_.dart_flags.push_back("--no_profile_vm");
