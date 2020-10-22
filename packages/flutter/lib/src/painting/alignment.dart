a5d276ab5ba5a2530ab7fd3c7b5a046397763f04
Aneesh Rao <66567899+sidrao2006@users.noreply.github.com>
2020-10-22 14:56:11 +0530

    Fix 3 for deprecation notice

--- packages/flutter/lib/src/painting/alignment.dart
+++ packages/flutter/lib/src/painting/alignment.dart
@@ -680,8 +680,8 @@ class VerticalAlignment {
 ///  * [InputDecorator.textAlignVertical], which defines the alignment of
 ///    prefix, input, and suffix within an [InputDecorator].
 @Deprecated(
-  'This class is deprecated. Use VerticalAlignment instead, which has the same interface.'
-  'This feature was deprecated after 1.24.0-1.0'
+  'This class is deprecated. Use VerticalAlignment instead, which has the same interface. '
+  'This feature was deprecated after 1.24.0-1.0.pre'
 )
 class TextAlignVertical extends VerticalAlignment {
   /// Creates a TextAlignVertical from any y value between -1.0 and 1.0.
