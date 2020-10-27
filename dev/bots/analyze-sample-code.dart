35c8469a989fb01ec8fe33f367f825b2bed1f55f
Jonah Williams <campfish91@gmail.com>
2020-10-27 10:15:53 -0700

    [null-safety] fix the snippets

--- dev/bots/analyze-sample-code.dart
+++ dev/bots/analyze-sample-code.dart
@@ -186,6 +186,7 @@ class SampleChecker {
   /// Computes the headers needed for each sample file.
   List<Line> get headers {
     return _headers ??= <String>[
+      '// @dart = 2.9',
       '// generated code',
       "import 'dart:async';",
       "import 'dart:convert';",
