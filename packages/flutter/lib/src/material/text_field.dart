f93ebaf123f93ebd91ec001ec03a0f631c1081a5
DanielEdrisian <dedrisian@gmail.com>
2020-10-27 13:44:53 -0700

    Flutter analyze docs fix

--- packages/flutter/lib/src/material/text_field.dart
+++ packages/flutter/lib/src/material/text_field.dart
@@ -434,10 +434,11 @@ class TextField extends StatefulWidget {
   /// material design text field otherwise.
   ///
   /// To retain the standard look of [CupertinoTextField], this constructor only
-  /// uses the [decoration] property's [hintText], [hintStyle], [suffix], and
-  /// [prefix] for the iOS platform, and all else is ignored in support of the
-  /// default iOS look. For instance, the [decoration.border] cannot override
-  /// the default iOS-style border.
+  /// uses the [decoration] property's [decoration.hintText], 
+  /// [decoration.hintStyle], [decoration.suffix], and [decoration.prefix] for 
+  /// the iOS platform, and all else is ignored in support of the default iOS 
+  /// look. For instance, the [decoration.border] cannot override the default 
+  /// iOS-style border.
   ///
   /// The target platform is based on the current [Theme]: [ThemeData.platform].
   const TextField.adaptive({
