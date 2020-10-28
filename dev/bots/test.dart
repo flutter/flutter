b467525f5b8a3f736ad3968ad79e6f143a03ee3e
Jonah Williams <campfish91@gmail.com>
2020-10-28 14:02:14 -0700

    Update test.dart

--- dev/bots/test.dart
+++ dev/bots/test.dart
@@ -241,8 +241,8 @@ Future<void> _runSmokeTests() async {
       ),
     ],
   );
-  
-  // The flutter-tester device cannot be run concurrently in the same project directory. 
+
+  // The flutter-tester device cannot be run concurrently in the same project directory.
   await runCommand(flutter,
     <String>['drive', '--show-test-device', '-d', 'flutter-tester', '-t', path.join('test_driver', 'success.dart')],
     workingDirectory: path.join(flutterRoot, 'packages', 'flutter_driver'),
