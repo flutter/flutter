7629758ed73e519d2cb1ad066bdf9c773bf0b604
Jonah Williams <jonahwilliams@google.com>
2020-10-28 18:15:23 -0700

    [flutter_tools] measure driver success and failure (#69226)
    Currently we only have a test that measures if flutter drive can fail. Unfortunately it has been failing for multiple years based on the fact that the error is due to a missing Gradle config.
    
    Add a smoke success test case, and update both tests to use the test API and the tester-device.

--- packages/flutter_driver/test_driver/success_test.dart
+++ packages/flutter_driver/test_driver/success_test.dart
@@ -7,9 +7,8 @@
 import '../test/common.dart';
 
 void main() {
-  // Intentionally fail the test. We want to see driver return a non-zero exit
-  // code when this happens.
-  test('it fails a test', () {
-    expect(true, isFalse);
+  // Tooling requires a single trivial test
+  test('it runs a test', () {
+    expect(true, true);
   });
 }
