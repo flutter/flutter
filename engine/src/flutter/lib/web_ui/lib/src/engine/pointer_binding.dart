6e01ca12d63adf904ed3c26127ceb6322cdee5d5
Ferhat <ferhat@gmail.com>
2020-10-29 11:06:37 -0700

    [web] Fix scroll wheel line delta on Firefox. (flutter/engine#21928)

--- engine/src/flutter/lib/web_ui/lib/src/engine/pointer_binding.dart
+++ engine/src/flutter/lib/web_ui/lib/src/engine/pointer_binding.dart
@@ -227,6 +227,8 @@ abstract class _BaseAdapter {
 }
 
 mixin _WheelEventListenerMixin on _BaseAdapter {
+  static double? _defaultScrollLineHeight;
+
   List<ui.PointerData> _convertWheelEventToPointerData(
     html.WheelEvent event
   ) {
@@ -240,8 +242,9 @@ mixin _WheelEventListenerMixin on _BaseAdapter {
     double deltaY = event.deltaY as double;
     switch (event.deltaMode) {
       case domDeltaLine:
-        deltaX *= 32.0;
-        deltaY *= 32.0;
+        _defaultScrollLineHeight ??= _computeDefaultScrollLineHeight();
+        deltaX *= _defaultScrollLineHeight!;
+        deltaY *= _defaultScrollLineHeight!;
         break;
       case domDeltaPage:
         deltaX *= ui.window.physicalSize.width;
@@ -251,6 +254,7 @@ mixin _WheelEventListenerMixin on _BaseAdapter {
       default:
         break;
     }
+
     final List<ui.PointerData> data = <ui.PointerData>[];
     _pointerDataConverter.convert(
       data,
@@ -284,6 +288,27 @@ mixin _WheelEventListenerMixin on _BaseAdapter {
         eventOptions
       ]
     );
+  }
+
+  /// For browsers that report delta line instead of pixels such as FireFox
+  /// compute line height using the default font size.
+  ///
+  /// Use Firefox to test this code path.
+  double _computeDefaultScrollLineHeight() {
+    const double kFallbackFontHeight = 16.0;
+    final html.DivElement probe = html.DivElement();
+    probe.style
+        ..fontSize = 'initial'
+        ..display = 'none';
+    html.document.body!.append(probe);
+    String fontSize = probe.getComputedStyle().fontSize;
+    double? res;
+    if (fontSize.contains('px')) {
+       fontSize = fontSize.replaceAll('px', '');
+       res = double.tryParse(fontSize);
+    }
+    probe.remove();
+    return res == null ? kFallbackFontHeight : res / 4.0;
   }
 }
 
