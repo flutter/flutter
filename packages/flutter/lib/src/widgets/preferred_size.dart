188918885385be969111971636cae41517f4316b
SergioEric <sericmorales@gmail.com>
2020-11-01 16:09:01 -0500

    dartpad example for PreferredSizeWidget #AdoptaWidget

--- packages/flutter/lib/src/widgets/preferred_size.dart
+++ packages/flutter/lib/src/widgets/preferred_size.dart
@@ -18,7 +18,6 @@ import 'framework.dart';
 ///
 /// Use [PreferredSize] to give a preferred size to an arbitrary widget.
 abstract class PreferredSizeWidget implements Widget {
-
   /// The size this widget would prefer if it were otherwise unconstrained.
   ///
   /// In many cases it's only necessary to define one preferred dimension.
@@ -33,6 +32,134 @@ abstract class PreferredSizeWidget implements Widget {
 /// This widget does not impose any constraints on its child, and it doesn't
 /// affect the child's layout in any way. It just advertises a preferred size
 /// which can be used by the parent.
+///
+/// ### Let's see 2 examples
+///
+/// The basic use of [PreferredSize] is when we need to change the
+/// `Status Bar Color` of our app but we don't want to render an actual
+/// [AppBar], and neither use [SystemChrome] of `services library`
+///
+/// {@tool dartpad --template=stateless_widget_material}
+///
+/// Here is the first example but be aware that we are assigning the `height` of the
+/// [AppBar] to **0**, so it will not be rendered in the "Darpad Preview",
+/// you will have to run the example on your real device or emulator
+///
+/// ```dart
+/// Widget build(BuildContext context) {
+///   return Scaffold(
+///     appBar: PreferredSize(
+///       preferredSize: const Size.fromHeight(0),
+///         child: AppBar(
+///           elevation: 0,
+///           brightness: Brightness.dark,
+///          )),
+///       body: Center(
+///         child: Text("Awesome Content"),
+///    ));
+/// }
+/// ```
+/// {@end-tool}
+///
+/// {@tool dartpad --template=stateless_widget_material}
+///
+/// This example explain how can we make a custom [AppBar] which has more `height`
+/// than usual. In which we will have a `title`, two [Icons] and a [TabBar] that
+/// controls body content in [Scaffold.body].
+/// 
+/// We assign the `height` parameter of our [PreferredSize] equal to 100, but you
+/// can change this value and see how our [AppBar] behaves. The benefit of using
+/// PreferredSize as an AppBar child its that we can use a different widget for creating 
+/// *custom app bars*, in this case. All this work because [Scaffold.appBar]
+/// expect a [PreferredSizeWidget] and, as is describe above AppBar implements it
+///
+/// ```dart
+/// Widget build(BuildContext context) {
+///  return DefaultTabController(
+///    length: 4,
+///    child: Scaffold(
+///        appBar: PreferredSize(
+///          preferredSize: const Size(double.infinity, 100.0),
+///          child: Container(
+///            decoration: BoxDecoration(
+///              gradient: LinearGradient(
+///                colors: [Colors.blue, Colors.pink],
+///              ),
+///            ),
+///            child: SafeArea(
+///              child: Column(
+///                mainAxisAlignment: MainAxisAlignment.end,
+///                children: [
+///                  Expanded(
+///                    child: Padding(
+///                      padding: const EdgeInsets.symmetric(horizontal: 10),
+///                      child: Row(
+///                        children: [
+///                          Text(
+///                            "Awesome App",
+///                            style: TextStyle(color: Colors.white),
+///                          ),
+///                          Spacer(),
+///                          IconButton(
+///                              icon: Icon(
+///                                Icons.search,
+///                                size: 20,
+///                              ),
+///                              color: Colors.white,
+///                              onPressed: () {}),
+///                          IconButton(
+///                              icon: Icon(
+///                                Icons.more_vert,
+///                                size: 20,
+///                              ),
+///                              color: Colors.white,
+///                              onPressed: () {}),
+///                        ],
+///                      ),
+///                    ),
+///                  ),
+///                  TabBar(
+///                    tabs: [
+///                      Icon(
+///                        Icons.camera,
+///                        size: 20,
+///                      ),
+///                      Tab(
+///                        text: "Tab 1",
+///                      ),
+///                      Tab(
+///                        text: "Tab 2",
+///                      ),
+///                      Tab(
+///                        text: "Tab 3",
+///                      ),
+///                    ],
+///                  ),
+///                ],
+///              ),
+///            ),
+///          ),
+///        ),
+///        body: TabBarView(
+///          children: [
+///            Center(
+///              child: Text("Content for Camera"),
+///            ),
+///            Center(
+///              child: Text("Content for tab 1"),
+///            ),
+///            Center(
+///              child: Text("Content for tab 2"),
+///            ),
+///            Center(
+///              child: Text("Content for tab 3"),
+///            ),
+///          ],
+///        )),
+///  );
+/// }
+/// ```
+/// {@end-tool}
 ///
 /// See also:
 ///
