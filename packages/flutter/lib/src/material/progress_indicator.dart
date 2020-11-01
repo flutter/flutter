402c4074a9b75da47eda158f9e1f5ea3631a9500
Aneesh Rao <66567899+sidrao2006@users.noreply.github.com>
2020-11-01 16:02:14 +0530

    Fix more semicolon bugs

--- packages/flutter/lib/src/material/progress_indicator.dart
+++ packages/flutter/lib/src/material/progress_indicator.dart
@@ -414,7 +414,7 @@ class _LinearProgressIndicatorPainter extends CustomPainter {
 ///   animation = Tween<double>(
 ///     begin: 0,
 ///     end: 1,
-///   ).animate(controller);
+///   ).animate(controller)
 ///     ..addListener(() {
 ///       setState(() {});
 ///     });
@@ -810,7 +810,7 @@ class _CircularProgressIndicatorPainter extends CustomPainter {
 ///   animation = Tween<double>(
 ///     begin: 0,
 ///     end: 1,
-///   ).animate(controller);
+///   ).animate(controller)
 ///     ..addListener(() {
 ///       setState(() {});
 ///     });
