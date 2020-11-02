0889bbf4bd21379d3298f92ce69fe7ef54864838
Chinmay Kabi <chinmaykabi@gmail.com>
2020-11-03 01:59:09 +0530

    Minor build fix

--- packages/flutter/lib/src/widgets/will_pop_scope.dart
+++ packages/flutter/lib/src/widgets/will_pop_scope.dart
@@ -29,9 +29,7 @@ import 'routes.dart';
 /// {@tool dartpad --template=stateless_widget_material}
 ///
 /// ```dart
-/// In this example a snackbar is shown when back button is pressed. Here
-/// `false` is returned to prevent the screen from popping. Returning `true`
-/// would pop the screen.
+/// 
 /// Widget build(BuildContext context) {
 ///   return Scaffold(
 ///     appBar: AppBar(
