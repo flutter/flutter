e2890a152ce056bf9be53488956502b69850ae86
Jonah Williams <campfish91@gmail.com>
2020-11-01 18:53:06 -0800

    [flutter_tools] add canvaskit hot reload integration test

--- packages/flutter_tools/test/integration.shard/test_driver.dart
+++ packages/flutter_tools/test/integration.shard/test_driver.dart
@@ -451,6 +451,7 @@ class FlutterRunTestDriver extends FlutterTestDriver {
     bool singleWidgetReloads = false,
     File pidFile,
     String script,
+    List<String> args,
   }) async {
     await _setupProcess(
       <String>[
@@ -471,6 +472,7 @@ class FlutterRunTestDriver extends FlutterTestDriver {
           'flutter-tester',
         if (structuredErrors)
           '--dart-define=flutter.inspector.structuredErrors=true',
+        ...?args,
       ],
       withDebugger: withDebugger,
       startPaused: startPaused,
