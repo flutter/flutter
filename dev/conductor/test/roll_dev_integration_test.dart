8e36dc2858a9b12de5862027ae70fc18afd6b9a7
Christopher Fujino <christopherfujino@gmail.com>
2020-11-03 15:04:44 -0800

    add license comment

--- dev/conductor/test/roll_dev_integration_test.dart
+++ dev/conductor/test/roll_dev_integration_test.dart
@@ -1,4 +1,6 @@
-//import 'dart:io' as io;
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
 import 'package:file/file.dart';
 import 'package:file/local.dart';
