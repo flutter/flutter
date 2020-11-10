212a86b210a33227116b0d5f4f105aa9d5adc5e2
nturgut <nurhan@google.com>
2020-11-10 15:34:38 -0800

    fixing platform channel errors

--- packages/flutter_test/lib/src/window.dart
+++ packages/flutter_test/lib/src/window.dart
@@ -155,7 +155,7 @@ class TestWindow implements ui.SingletonFlutterWindow {
 
   @override
   // ignore: unnecessary_non_null_assertion
-  ui.Locale get locale => _localeTestValue ?? platformDispatcher.locale!;
+  ui.Locale get locale => _localeTestValue ?? platformDispatcher.locale;
   ui.Locale? _localeTestValue;
   /// Hides the real locale and reports the given [localeTestValue] instead.
   set localeTestValue(ui.Locale localeTestValue) {
@@ -170,7 +170,7 @@ class TestWindow implements ui.SingletonFlutterWindow {
 
   @override
   // ignore: unnecessary_non_null_assertion
-  List<ui.Locale> get locales => _localesTestValue ?? platformDispatcher.locales!;
+  List<ui.Locale> get locales => _localesTestValue ?? platformDispatcher.locales;
   List<ui.Locale>? _localesTestValue;
   /// Hides the real locales and reports the given [localesTestValue] instead.
   set localesTestValue(List<ui.Locale> localesTestValue) {
