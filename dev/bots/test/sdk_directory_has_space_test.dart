8ed170afddbffdfbcc387c5a57e5a9b7fad26876
Christopher Fujino <christopherfujino@gmail.com>
2020-11-10 13:33:06 -0800

    update env var

--- dev/bots/test/sdk_directory_has_space_test.dart
+++ dev/bots/test/sdk_directory_has_space_test.dart
@@ -12,7 +12,7 @@ void main() {
   test('We are in a directory with a space in it', () async {
     // The Flutter SDK should be in a directory with a space in it, to make sure
     // our tools support that.
-    final String expectedName = Platform.environment['FLUTTER_SDK_PATH_WITH_SPACE'];
+    final String expectedName = Platform.environment['SDK_CHECKOUT_PATH'];
     expect(expectedName, 'flutter sdk');
     expect(expectedName, contains(' '));
     final List<String> parts = path.split(Directory.current.absolute.path);
