4d1fd2d9b4dcb85ed219fb8123262cdcccd67472
nturgut <nurhan@google.com>
2020-11-10 16:10:28 -0800

    other changes to fix the null errors

--- packages/flutter_test/lib/src/window.dart
+++ packages/flutter_test/lib/src/window.dart
@@ -154,8 +154,13 @@ class TestWindow implements ui.SingletonFlutterWindow {
   }
 
   @override
-  // ignore: unnecessary_non_null_assertion
-  ui.Locale get locale => _localeTestValue ?? platformDispatcher.locale;
+  ui.Locale? get locale {
+    if (_localeTestValue !=null) {
+      return _localeTestValue!;
+    }
+    return platformDispatcher.locale;
+  }
+
   ui.Locale? _localeTestValue;
   /// Hides the real locale and reports the given [localeTestValue] instead.
   set localeTestValue(ui.Locale localeTestValue) {
@@ -169,8 +174,14 @@ class TestWindow implements ui.SingletonFlutterWindow {
   }
 
   @override
-  // ignore: unnecessary_non_null_assertion
-  List<ui.Locale> get locales => _localesTestValue ?? platformDispatcher.locales;
+  List<ui.Locale>? get locales {
+    if (_localesTestValue !=null) {
+      return _localesTestValue!;
+    } else {
+      return platformDispatcher.locales;
+    }
+  }
+
   List<ui.Locale>? _localesTestValue;
   /// Hides the real locales and reports the given [localesTestValue] instead.
   set localesTestValue(List<ui.Locale> localesTestValue) {
