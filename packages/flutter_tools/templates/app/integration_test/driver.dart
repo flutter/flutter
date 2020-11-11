0ec03ee17cd589320cf5177eda1b74c4c20225cf
Dan Field <dnfield@google.com>
2020-11-10 21:56:50 -0800

    review

--- packages/flutter_tools/templates/app/integration_test/driver.dart
+++ packages/flutter_tools/templates/app/integration_test/driver.dart
@@ -1,18 +1,8 @@
 // This file is provided as a convenience for running integration tests via the
 // flutter drive command.
 //
-// flutter drive -t integration_test/app_test.dart --driver integration_test/driver.dart
+// flutter drive --driver integration_test/driver.dart --target integration_test/app_test.dart
 
-import 'dart:async';
-import 'dart:convert';
-import 'dart:io';
+import 'package:integration_test/integration_test_driver.dart';
 
-import 'package:flutter_driver/flutter_driver.dart';
-
-Future<void> main() async {
-  final FlutterDriver driver = await FlutterDriver.connect();
-  final String data = await driver.requestData(null);
-  await driver.close();
-  final Map<String, dynamic> result = jsonDecode(data);
-  exit(result['result'] == 'true' ? 0 : 1);
-}
+Future<void> main() => integrationDriver();
