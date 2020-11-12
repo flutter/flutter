0c796599003faf402abee08d95cfc0b50bf07e64
Jonah Williams <jonahwilliams@google.com>
2020-11-12 10:23:03 -0800

    [flutter_tools] make most integration tests null safe (#70334)

--- packages/flutter_tools/test/integration.shard/test_data/stepping_project.dart
+++ packages/flutter_tools/test/integration.shard/test_data/stepping_project.dart
@@ -9,7 +9,7 @@ class SteppingProject extends Project {
   final String pubspec = '''
   name: test
   environment:
-    sdk: '>=2.10.0 <3.0.0'
+    sdk: '>=2.12.0-0 <3.0.0'
   dependencies:
     flutter:
       sdk: flutter
