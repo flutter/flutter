1aafb3a8b9b0c36241c5f5b34ee914770f015818
Christopher Fujino <christopherfujino@gmail.com>
2020-11-13 09:59:28 -0800

    [flutter_releases] Flutter 1.22.4 framework cherrypicks (#70327)
    * App.framework must support iOS 8 for older Flutter projects
    * cherry pick fixes to https://github.com/flutter/flutter/issues/60118
    * fix to --observatory-port flag
    * update engine version
    
    Co-authored-by: Jenn Magder <magder@google.com>

--- packages/flutter_tools/test/src/context.dart
+++ packages/flutter_tools/test/src/context.dart
@@ -18,6 +18,7 @@ import 'package:flutter_tools/src/base/template.dart';
 import 'package:flutter_tools/src/base/terminal.dart';
 import 'package:flutter_tools/src/base/time.dart';
 import 'package:flutter_tools/src/build_runner/mustache_template.dart';
+import 'package:flutter_tools/src/build_info.dart';
 import 'package:flutter_tools/src/cache.dart';
 import 'package:flutter_tools/src/context_runner.dart';
 import 'package:flutter_tools/src/dart/pub.dart';
@@ -293,6 +294,9 @@ class MockSimControl extends Mock implements SimControl {
 class FakeOperatingSystemUtils implements OperatingSystemUtils {
   @override
   ProcessResult makeExecutable(File file) => null;
+
+  @override
+  HostPlatform hostPlatform = HostPlatform.linux_x64;
 
   @override
   void chmod(FileSystemEntity entity, String mode) { }
