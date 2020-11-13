6090124dbda98578c6a0d803a0e7d48582aee97a
Greg Spencer <gspencer@google.com>
2020-11-13 13:00:44 -0800

    Remove some unnecessary casts, now that changes have rolled from the engine

--- packages/flutter_localizations/test/widgets_test.dart
+++ packages/flutter_localizations/test/widgets_test.dart
@@ -689,11 +689,8 @@ void main() {
           const OnlyRTLDefaultWidgetsLocalizationsDelegate(),
         ],
         buildContent: (BuildContext context) {
-          // TODO(gspencergood): remove the casts once
-          // https://github.com/flutter/engine/pull/22473 rolls into the
-          // framework.
-          final Locale locale1 = ((ui.window.locales as dynamic) as List<Locale>).first;
-          final Locale locale2 = ((ui.window.locales as dynamic) as List<Locale>)[1];
+          final Locale locale1 = ui.window.locales.first;
+          final Locale locale2 = ui.window.locales[1];
           return Text('$locale1 $locale2');
         },
       )
