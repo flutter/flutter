35a0b9fe6827c82dfe340202aaf7293d54068bdf
Yegor <yjbanov@google.com>
2020-11-17 17:40:34 -0800

    Refactor CanvasKit image ref counting; fix a minor memory leak (#22549)
    * Refactor SkiaObjectBox ref counting
    * make CkAnimatedImage a Codec
    * disallow double dispose; better assertion messages

--- lib/web_ui/test/canvaskit/image_test.dart
+++ lib/web_ui/test/canvaskit/image_test.dart
@@ -10,6 +10,7 @@ import 'package:test/test.dart';
 import 'package:ui/src/engine.dart';
 import 'package:ui/ui.dart' as ui;
 
+import '../matchers.dart';
 import 'common.dart';
 import 'test_data.dart';
 
@@ -23,48 +24,30 @@ void testMain() {
       await ui.webOnlyInitializePlatform();
     });
 
-    test('CkAnimatedImage toString', () {
-      final SkAnimatedImage skAnimatedImage =
-          canvasKit.MakeAnimatedImageFromEncoded(kTransparentImage);
-      final CkAnimatedImage image = CkAnimatedImage(skAnimatedImage);
-      expect(image.toString(), '[1×1]');
-      image.dispose();
-    });
-
     test('CkAnimatedImage can be explicitly disposed of', () {
-      final SkAnimatedImage skAnimatedImage =
-          canvasKit.MakeAnimatedImageFromEncoded(kTransparentImage);
-      final CkAnimatedImage image = CkAnimatedImage(skAnimatedImage);
+      final CkAnimatedImage image = CkAnimatedImage.decodeFromBytes(kTransparentImage);
       expect(image.box.isDeleted, false);
       expect(image.debugDisposed, false);
       image.dispose();
       expect(image.box.isDeleted, true);
       expect(image.debugDisposed, true);
-      image.dispose();
-      expect(image.box.isDeleted, true);
-      expect(image.debugDisposed, true);
+
+      // Disallow double-dispose.
+      expect(() => image.dispose(), throwsAssertionError);
     });
 
     test('CkAnimatedImage can be cloned and explicitly disposed of', () async {
-      final SkAnimatedImage skAnimatedImage =
-          canvasKit.MakeAnimatedImageFromEncoded(kTransparentImage);
-      final CkAnimatedImage image = CkAnimatedImage(skAnimatedImage);
-      final CkAnimatedImage imageClone = image.clone();
+      final CkAnimatedImage image = CkAnimatedImage.decodeFromBytes(kTransparentImage);
+      final SkAnimatedImage skAnimatedImage = image.box.skiaObject;
+      final SkiaObjectBox<CkAnimatedImage, SkAnimatedImage> box = image.box;
+      expect(box.refCount, 1);
+      expect(box.debugGetStackTraces().length, 1);
 
-      expect(image.isCloneOf(imageClone), true);
-      expect(image.box.isDeleted, false);
-      await Future<void>.delayed(Duration.zero);
-      expect(skAnimatedImage.isDeleted(), false);
       image.dispose();
-      expect(image.box.isDeleted, true);
-      expect(imageClone.box.isDeleted, false);
-      await Future<void>.delayed(Duration.zero);
-      expect(skAnimatedImage.isDeleted(), false);
-      imageClone.dispose();
-      expect(image.box.isDeleted, true);
-      expect(imageClone.box.isDeleted, true);
+      expect(box.isDeleted, true);
       await Future<void>.delayed(Duration.zero);
       expect(skAnimatedImage.isDeleted(), true);
+      expect(box.debugGetStackTraces().length, 0);
     });
 
     test('CkImage toString', () {
@@ -86,9 +69,9 @@ void testMain() {
       image.dispose();
       expect(image.debugDisposed, true);
       expect(image.box.isDeleted, true);
-      image.dispose();
-      expect(image.debugDisposed, true);
-      expect(image.box.isDeleted, true);
+
+      // Disallow double-dispose.
+      expect(() => image.dispose(), throwsAssertionError);
     });
 
     test('CkImage can be explicitly disposed of when cloned', () async {
@@ -96,28 +79,42 @@ void testMain() {
           canvasKit.MakeAnimatedImageFromEncoded(kTransparentImage)
               .getCurrentFrame();
       final CkImage image = CkImage(skImage);
+      final SkiaObjectBox<CkImage, SkImage> box = image.box;
+      expect(box.refCount, 1);
+      expect(box.debugGetStackTraces().length, 1);
+
       final CkImage imageClone = image.clone();
+      expect(box.refCount, 2);
+      expect(box.debugGetStackTraces().length, 2);
 
       expect(image.isCloneOf(imageClone), true);
-      expect(image.box.isDeleted, false);
+      expect(box.isDeleted, false);
       await Future<void>.delayed(Duration.zero);
       expect(skImage.isDeleted(), false);
       image.dispose();
-      expect(image.box.isDeleted, true);
-      expect(imageClone.box.isDeleted, false);
+      expect(box.isDeleted, false);
       await Future<void>.delayed(Duration.zero);
       expect(skImage.isDeleted(), false);
       imageClone.dispose();
-      expect(image.box.isDeleted, true);
-      expect(imageClone.box.isDeleted, true);
+      expect(box.isDeleted, true);
       await Future<void>.delayed(Duration.zero);
       expect(skImage.isDeleted(), true);
+      expect(box.debugGetStackTraces().length, 0);
     });
 
     test('skiaInstantiateWebImageCodec throws exception if given invalid URL',
         () async {
       expect(skiaInstantiateWebImageCodec('invalid-url', null),
           throwsA(isA<ProgressEvent>()));
+    });
+
+    test('CkImage toByteData', () async {
+      final SkImage skImage =
+          canvasKit.MakeAnimatedImageFromEncoded(kTransparentImage)
+              .getCurrentFrame();
+      final CkImage image = CkImage(skImage);
+      expect((await image.toByteData()).lengthInBytes, greaterThan(0));
+      expect((await image.toByteData(format: ui.ImageByteFormat.png)).lengthInBytes, greaterThan(0));
     });
     // TODO: https://github.com/flutter/flutter/issues/60040
   }, skip: isIosSafari);
