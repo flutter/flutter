d7793f5cfb15eae2a7ab0a08f10139a4e6f72367
Greg Spencer <gspencergoog@users.noreply.github.com>
2020-11-17 21:51:37 -0800

    Fix typos and fix some env var state leakage in tests (flutter/engine#22576)
    This just fixes some typos, and a small state leak in some of the tests that set env variables.

--- engine/src/flutter/shell/platform/linux/fl_basic_message_channel.cc
+++ engine/src/flutter/shell/platform/linux/fl_basic_message_channel.cc
@@ -33,7 +33,7 @@ struct _FlBasicMessageChannelResponseHandle {
   FlBinaryMessengerResponseHandle* response_handle;
 };
 
-// Added here to stop the compiler from optimising this function away.
+// Added here to stop the compiler from optimizing this function away.
 G_MODULE_EXPORT GType fl_basic_message_channel_get_type();
 
 G_DEFINE_TYPE(FlBasicMessageChannel, fl_basic_message_channel, G_TYPE_OBJECT)
