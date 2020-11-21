4c16b3f5efec8c5e0549205c58dd677a3b9d81c0
Richard Cai <RichardJCai@users.noreply.github.com>
2020-11-21 11:43:04 -0500

    Create FlutterFrameBufferProvider class. (flutter/engine#22656)

--- engine/src/flutter/shell/platform/darwin/macos/framework/Source/FlutterSurfaceManager.mm
+++ engine/src/flutter/shell/platform/darwin/macos/framework/Source/FlutterSurfaceManager.mm
@@ -3,6 +3,7 @@
 // found in the LICENSE file.
 
 #import "flutter/shell/platform/darwin/macos/framework/Source/FlutterSurfaceManager.h"
+#import "flutter/shell/platform/darwin/macos/framework/Source/FlutterFrameBufferProvider.h"
 
 #include <OpenGL/gl.h>
 
@@ -20,9 +21,9 @@ enum {
   CALayer* _contentLayer;
 
   NSOpenGLContext* _openGLContext;
-  uint32_t _frameBufferId[kFlutterSurfaceManagerBufferCount];
-  uint32_t _backingTexture[kFlutterSurfaceManagerBufferCount];
+
   IOSurfaceRef _ioSurface[kFlutterSurfaceManagerBufferCount];
+  FlutterFrameBufferProvider* _frameBuffers[kFlutterSurfaceManagerBufferCount];
 }
 @end
 
@@ -39,25 +40,10 @@ enum {
     _contentLayer = [[CALayer alloc] init];
     [_containingLayer addSublayer:_contentLayer];
 
-    MacOSGLContextSwitch context_switch(openGLContext);
-
-    glGenFramebuffers(2, _frameBufferId);
-    glGenTextures(2, _backingTexture);
-
-    [self createFramebuffer:_frameBufferId[0] withBackingTexture:_backingTexture[0]];
-    [self createFramebuffer:_frameBufferId[1] withBackingTexture:_backingTexture[1]];
+    _frameBuffers[0] = [[FlutterFrameBufferProvider alloc] initWithOpenGLContext:_openGLContext];
+    _frameBuffers[1] = [[FlutterFrameBufferProvider alloc] initWithOpenGLContext:_openGLContext];
   }
   return self;
-}
-
-- (void)createFramebuffer:(uint32_t)fbo withBackingTexture:(uint32_t)texture {
-  glBindFramebuffer(GL_FRAMEBUFFER, fbo);
-  glBindTexture(GL_TEXTURE_RECTANGLE_ARB, texture);
-  glTexParameterf(GL_TEXTURE_RECTANGLE_ARB, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-  glTexParameterf(GL_TEXTURE_RECTANGLE_ARB, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-  glTexParameteri(GL_TEXTURE_RECTANGLE_ARB, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-  glTexParameteri(GL_TEXTURE_RECTANGLE_ARB, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-  glBindTexture(GL_TEXTURE_RECTANGLE_ARB, 0);
 }
 
 - (void)ensureSurfaceSize:(CGSize)size {
@@ -88,16 +74,16 @@ enum {
     };
     _ioSurface[i] = IOSurfaceCreate((CFDictionaryRef)options);
 
-    glBindTexture(GL_TEXTURE_RECTANGLE_ARB, _backingTexture[i]);
+    glBindTexture(GL_TEXTURE_RECTANGLE_ARB, [_frameBuffers[i] glTextureId]);
 
     CGLTexImageIOSurface2D(CGLGetCurrentContext(), GL_TEXTURE_RECTANGLE_ARB, GL_RGBA,
                            int(size.width), int(size.height), GL_BGRA, GL_UNSIGNED_INT_8_8_8_8_REV,
                            _ioSurface[i], 0 /* plane */);
     glBindTexture(GL_TEXTURE_RECTANGLE_ARB, 0);
 
-    glBindFramebuffer(GL_FRAMEBUFFER, _frameBufferId[i]);
+    glBindFramebuffer(GL_FRAMEBUFFER, [_frameBuffers[i] glFrameBufferId]);
     glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_RECTANGLE_ARB,
-                           _backingTexture[i], 0);
+                           [_frameBuffers[i] glTextureId], 0);
 
     NSAssert(glCheckFramebufferStatus(GL_FRAMEBUFFER) == GL_FRAMEBUFFER_COMPLETE,
              @"Framebuffer status check failed");
@@ -114,14 +100,12 @@ enum {
 
   std::swap(_ioSurface[kFlutterSurfaceManagerBackBuffer],
             _ioSurface[kFlutterSurfaceManagerFrontBuffer]);
-  std::swap(_frameBufferId[kFlutterSurfaceManagerBackBuffer],
-            _frameBufferId[kFlutterSurfaceManagerFrontBuffer]);
-  std::swap(_backingTexture[kFlutterSurfaceManagerBackBuffer],
-            _backingTexture[kFlutterSurfaceManagerFrontBuffer]);
+  std::swap(_frameBuffers[kFlutterSurfaceManagerBackBuffer],
+            _frameBuffers[kFlutterSurfaceManagerFrontBuffer]);
 }
 
 - (uint32_t)glFrameBufferId {
-  return _frameBufferId[kFlutterSurfaceManagerBackBuffer];
+  return [_frameBuffers[kFlutterSurfaceManagerBackBuffer] glFrameBufferId];
 }
 
 - (void)dealloc {
