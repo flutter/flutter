7df04fdbd3d1eae56795c1fc978d063ed7dbf7c3
Amir Hardon <amirh@users.noreply.github.com>
2020-11-23 12:27:36 -0800

    Revert "Migrate template to Gradle 6.7 and AGP 4.1.0 (#70808)" (#71096)
    This reverts commit 8e73bab94c6051317c9fa4279a3733b6ee2a3ef2.

--- packages/flutter_tools/lib/src/commands/build_apk.dart
+++ packages/flutter_tools/lib/src/commands/build_apk.dart
@@ -95,6 +95,7 @@ class BuildApkCommand extends BuildSubCommand {
       buildInfo,
       splitPerAbi: boolArg('split-per-abi'),
       targetArchs: stringsArg('target-platform').map<AndroidArch>(getAndroidArchForName),
+      shrink: boolArg('shrink'),
     );
     validateBuild(androidBuildInfo);
 
