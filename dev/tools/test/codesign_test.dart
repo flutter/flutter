4a04e7c6e6c3348232eefaf04aaef64f8714a0e5
Christopher Fujino <christopherfujino@gmail.com>
2020-12-02 13:42:01 -0800

    add no-op test file

---
+++ dev/tools/test/codesign_test.dart
@@ -0,0 +1,13 @@
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:dev_tools/roll_dev.dart';
+import 'package:dev_tools/repository.dart';
+
+//import './common.dart' show;
+void main() {
+  group('codesign command', () {
+
+  });
+}
