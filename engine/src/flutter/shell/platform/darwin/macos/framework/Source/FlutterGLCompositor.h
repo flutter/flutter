cb35e23e34a1ce33adac3abb7c439c868eabe173
Richard Cai <RichardJCai@users.noreply.github.com>
2020-12-03 11:57:15 -0500

    (MacOS) Add FlutterGLCompositor with support for rendering multiple layers (flutter/engine#22782)
    * Create FlutterGLCompositor.
    * Add additional state to manage frame status and CALayers to FlutterGLCompositor
    
    FlutterGLCompositor supports rendering multiple layers. The first layer is rendered using the FlutterView. Additional CALayers are created if there is more than one layer.
    
    Platform view support will be added in following PR.

---
+++ engine/src/flutter/shell/platform/darwin/macos/framework/Source/FlutterGLCompositor.h
@@ -0,0 +1,80 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <map>
+
+#include "flutter/fml/macros.h"
+#include "flutter/shell/platform/darwin/macos/framework/Source/FlutterSurfaceManager.h"
+#include "flutter/shell/platform/darwin/macos/framework/Source/FlutterViewController_Internal.h"
+#include "flutter/shell/platform/embedder/embedder.h"
+#include "third_party/skia/include/gpu/GrDirectContext.h"
+
+namespace flutter {
+
+// FlutterGLCompositor creates and manages the backing stores used for
+// rendering Flutter content and presents Flutter content and Platform views.
+// Platform views are not yet supported.
+// FlutterGLCompositor is created and destroyed by FlutterEngine.
+class FlutterGLCompositor {
+ public:
+  FlutterGLCompositor(FlutterViewController* view_controller);
+
+  // Creates a BackingStore and saves updates the backing_store_out
+  // data with the new BackingStore data.
+  // If the backing store is being requested for the first time
+  // for a given frame, we do not create a new backing store but
+  // rather return the backing store associated with the
+  // FlutterView's FlutterSurfaceManager.
+  //
+  // Any additional state allocated for the backing store and
+  // saved as user_data in the backing store must be collected
+  // in the backing_store's desctruction_callback field which will
+  // be called when the embedder collects the backing store.
+  bool CreateBackingStore(const FlutterBackingStoreConfig* config,
+                          FlutterBackingStore* backing_store_out);
+
+  // Releases the memory for any state used by the backing store.
+  bool CollectBackingStore(const FlutterBackingStore* backing_store);
+
+  // Presents the FlutterLayers by updating FlutterView(s) using the
+  // layer content.
+  // Present sets frame_started_ to false.
+  bool Present(const FlutterLayer** layers, size_t layers_count);
+
+  using PresentCallback = std::function<bool()>;
+
+  // PresentCallback is called at the end of the Present function.
+  void SetPresentCallback(const PresentCallback& present_callback);
+
+ private:
+  const FlutterViewController* view_controller_;
+  const NSOpenGLContext* open_gl_context_;
+  PresentCallback present_callback_;
+
+  // Count for how many CALayers have been created for a frame.
+  // Resets when a frame is finished.
+  // ca_layer_count_ is also used as a layerId.
+  size_t ca_layer_count_ = 0;
+
+  // Maps a layer_id (size_t) to a CALayer.
+  // The layer_id starts at 0 for a given frame
+  // and increments by 1 for each new CALayer.
+  std::map<size_t, CALayer*> ca_layer_map_;
+
+  // frame_started_ keeps track of if a layer has been
+  // created for the frame.
+  bool frame_started_ = false;
+
+  // Set frame_started_ to true and reset all layer state.
+  void StartFrame();
+
+  // Creates a CALayer and adds it to ca_layer_map_ and increments
+  // ca_layer_count_; Returns the key value (size_t) for the layer in
+  // ca_layer_map_.
+  size_t CreateCALayer();
+
+  FML_DISALLOW_COPY_AND_ASSIGN(FlutterGLCompositor);
+};
+
+}  // namespace flutter
---
+++ engine/src/flutter/shell/platform/darwin/macos/framework/Source/FlutterGLCompositor.mm
@@ -0,0 +1,142 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "flutter/shell/platform/darwin/macos/framework/Source/FlutterGLCompositor.h"
+
+#import <OpenGL/gl.h>
+
+#import "flutter/fml/logging.h"
+#import "flutter/fml/platform/darwin/cf_utils.h"
+#import "flutter/shell/platform/darwin/macos/framework/Source/FlutterBackingStoreData.h"
+#import "flutter/shell/platform/darwin/macos/framework/Source/FlutterFrameBufferProvider.h"
+#import "flutter/shell/platform/darwin/macos/framework/Source/FlutterIOSurfaceHolder.h"
+#import "third_party/skia/include/core/SkCanvas.h"
+#import "third_party/skia/include/core/SkSurface.h"
+#import "third_party/skia/include/gpu/gl/GrGLAssembleInterface.h"
+#import "third_party/skia/include/utils/mac/SkCGUtils.h"
+
+namespace flutter {
+
+FlutterGLCompositor::FlutterGLCompositor(FlutterViewController* view_controller)
+    : open_gl_context_(view_controller.flutterView.openGLContext) {
+  FML_CHECK(view_controller != nullptr) << "FlutterViewController* cannot be nullptr";
+  view_controller_ = view_controller;
+}
+
+bool FlutterGLCompositor::CreateBackingStore(const FlutterBackingStoreConfig* config,
+                                             FlutterBackingStore* backing_store_out) {
+  CGSize size = CGSizeMake(config->size.width, config->size.height);
+
+  if (!frame_started_) {
+    StartFrame();
+    // If the backing store is for the first layer, return the fbo for the
+    // FlutterView.
+    auto fbo = [view_controller_.flutterView frameBufferIDForSize:size];
+    backing_store_out->open_gl.framebuffer.name = fbo;
+  } else {
+    FlutterFrameBufferProvider* fb_provider =
+        [[FlutterFrameBufferProvider alloc] initWithOpenGLContext:open_gl_context_];
+    FlutterIOSurfaceHolder* io_surface_holder = [FlutterIOSurfaceHolder alloc];
+
+    GLuint fbo = [fb_provider glFrameBufferId];
+    GLuint texture = [fb_provider glTextureId];
+
+    size_t layer_id = CreateCALayer();
+
+    [io_surface_holder bindSurfaceToTexture:texture fbo:fbo size:size];
+    FlutterBackingStoreData* data =
+        [[FlutterBackingStoreData alloc] initWithLayerId:layer_id
+                                              fbProvider:fb_provider
+                                         ioSurfaceHolder:io_surface_holder];
+
+    backing_store_out->open_gl.framebuffer.name = fbo;
+    backing_store_out->open_gl.framebuffer.user_data = (__bridge_retained void*)data;
+  }
+
+  backing_store_out->type = kFlutterBackingStoreTypeOpenGL;
+  backing_store_out->open_gl.type = kFlutterOpenGLTargetTypeFramebuffer;
+  backing_store_out->open_gl.framebuffer.target = GL_RGBA8;
+  backing_store_out->open_gl.framebuffer.destruction_callback = [](void* user_data) {
+    if (user_data != nullptr) {
+      CFRelease(user_data);
+    }
+  };
+
+  return true;
+}
+
+bool FlutterGLCompositor::CollectBackingStore(const FlutterBackingStore* backing_store) {
+  return true;
+}
+
+bool FlutterGLCompositor::Present(const FlutterLayer** layers, size_t layers_count) {
+  for (size_t i = 0; i < layers_count; ++i) {
+    const auto* layer = layers[i];
+    FlutterBackingStore* backing_store = const_cast<FlutterBackingStore*>(layer->backing_store);
+    switch (layer->type) {
+      case kFlutterLayerContentTypeBackingStore: {
+        if (backing_store->open_gl.framebuffer.user_data) {
+          FlutterBackingStoreData* backing_store_data =
+              (__bridge FlutterBackingStoreData*)backing_store->open_gl.framebuffer.user_data;
+
+          FlutterIOSurfaceHolder* io_surface_holder = [backing_store_data ioSurfaceHolder];
+          size_t layer_id = [backing_store_data layerId];
+
+          CALayer* content_layer = ca_layer_map_[layer_id];
+
+          FML_CHECK(content_layer) << "Unable to find a content layer with layer id " << layer_id;
+
+          content_layer.frame = content_layer.superlayer.bounds;
+
+          // The surface is an OpenGL texture, which means it has origin in bottom left corner
+          // and needs to be flipped vertically
+          content_layer.transform = CATransform3DMakeScale(1, -1, 1);
+          IOSurfaceRef io_surface_contents = [io_surface_holder ioSurface];
+          [content_layer setContents:(__bridge id)io_surface_contents];
+        }
+        break;
+      }
+      case kFlutterLayerContentTypePlatformView:
+        // Add functionality in follow up PR.
+        FML_LOG(WARNING) << "Presenting PlatformViews not yet supported";
+        break;
+    };
+  }
+  // The frame has been presented, prepare FlutterGLCompositor to
+  // render a new frame.
+  frame_started_ = false;
+  return present_callback_();
+}
+
+void FlutterGLCompositor::SetPresentCallback(
+    const FlutterGLCompositor::PresentCallback& present_callback) {
+  present_callback_ = present_callback;
+}
+
+void FlutterGLCompositor::StartFrame() {
+  // First reset all the state.
+  ca_layer_count_ = 0;
+
+  // First remove all CALayers from the superlayer.
+  for (auto const& ca_layer_kvp : ca_layer_map_) {
+    [ca_layer_kvp.second removeFromSuperlayer];
+  }
+
+  // Reset layer map.
+  ca_layer_map_.clear();
+
+  frame_started_ = true;
+}
+
+size_t FlutterGLCompositor::CreateCALayer() {
+  // FlutterGLCompositor manages the lifecycle of content layers.
+  // The id for a CALayer starts at 0 and increments by 1 for
+  // any given frame.
+  CALayer* content_layer = [[CALayer alloc] init];
+  [view_controller_.flutterView.layer addSublayer:content_layer];
+  ca_layer_map_[ca_layer_count_] = content_layer;
+  return ca_layer_count_++;
+}
+
+}  // namespace flutter
