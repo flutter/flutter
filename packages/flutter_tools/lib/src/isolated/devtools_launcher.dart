8a2037d852364346b82512198a47bb98c12ae446
Kenzie Schmoll <kenzieschmoll@google.com>
2020-12-04 13:57:40 -0800

    switch to process run

--- packages/flutter_tools/lib/src/isolated/devtools_launcher.dart
+++ packages/flutter_tools/lib/src/isolated/devtools_launcher.dart
@@ -33,75 +33,55 @@ class DevtoolsServerLauncher extends DevtoolsLauncher {
   io.Process _devToolsProcess;
   Uri _devToolsUri;
 
-  static final RegExp _activateDevToolsPattern = RegExp(r'Activated devtools (\d+.\d+.\d+)');
   static final RegExp _serveDevToolsPattern =
       RegExp(r'Serving DevTools at ((http|//)[a-zA-Z0-9:/=_\-\.\[\]]+)');
 
   @override
   Future<void> launch(Uri vmServiceUri) async {
     try {
-      final Process _devToolsActivateProcess = await _processManager.start(<String>[
+      final ProcessResult _devToolsActivateProcess = await _processManager.run(<String>[
         _pubExecutable,
         'global',
         'activate',
         'devtools'
       ]);
-      _logger.printStatus('Activating DevTools...');
-      bool devtoolsActivated = false;
-      _devToolsActivateProcess.stdout
+      if (_devToolsActivateProcess.exitCode != 0) {
+        _logger.printError('Error running `pub global activate '
+            'devtools`. Could not activate devtools.');
+        return;
+      }
+
+      _devToolsProcess = await _processManager.start(<String>[
+        _pubExecutable,
+        'global',
+        'run',
+        'devtools',
+        if (vmServiceUri != null) '--vm-uri=$vmServiceUri',
+      ]);
+      final Completer<Uri> completer = Completer<Uri>();
+      _devToolsProcess.stdout
           .transform(utf8.decoder)
           .transform(const LineSplitter())
-          .listen(
-            (String line) {
-              final Match match = _activateDevToolsPattern.firstMatch(line);
-              if (match != null) {
-                devtoolsActivated = true;
-                _logger.printStatus(line);
+          .listen((String line) {
+            final Match match = _serveDevToolsPattern.firstMatch(line);
+            if (match != null) {
+              // We are trying to pull "http://127.0.0.1:9101" from "Serving
+              // DevTools at http://127.0.0.1:9101.". `match[1]` will return
+              // "http://127.0.0.1:9101.", and we need to trim the trailing period
+              // so that we don't throw an exception from `Uri.parse`.
+              String uri = match[1];
+              if (uri.endsWith('.')) {
+                uri = uri.substring(0, uri.length - 1);
               }
-            },
-            onDone: () async {
-              if (!devtoolsActivated) {
-                _logger.printError('Error running `pub global activate '
-                    'devtools`. Could not activate devtools.');
-                return;
-              }
-              _devToolsProcess = await _processManager.start(<String>[
-                _pubExecutable,
-                'global',
-                'run',
-                'devtools',
-                if (vmServiceUri != null) '--vm-uri=$vmServiceUri',
-              ]);
-              final Completer<Uri> completer = Completer<Uri>();
-              _devToolsProcess.stdout
-                  .transform(utf8.decoder)
-                  .transform(const LineSplitter())
-                  .listen((String line) {
-                    final Match match = _serveDevToolsPattern.firstMatch(line);
-                    if (match != null) {
-                      // We are trying to pull "http://127.0.0.1:9101" from "Serving
-                      // DevTools at http://127.0.0.1:9101.". `match[1]` will return
-                      // "http://127.0.0.1:9101.", and we need to trim the trailing period
-                      // so that we don't throw an exception from `Uri.parse`.
-                      String uri = match[1];
-                      if (uri.endsWith('.')) {
-                        uri = uri.substring(0, uri.length - 1);
-                      }
-                      completer.complete(Uri.parse(uri));
-                    }
-                    _logger.printStatus(line);
-                 });
-              _devToolsProcess.stderr
-                  .transform(utf8.decoder)
-                  .transform(const LineSplitter())
-                  .listen(_logger.printError);
-              _devToolsUri = await completer.future;
-            },
-          );
-      _devToolsActivateProcess.stderr
+              completer.complete(Uri.parse(uri));
+            }
+            _logger.printStatus(line);
+         });
+      _devToolsProcess.stderr
           .transform(utf8.decoder)
           .transform(const LineSplitter())
           .listen(_logger.printError);
+      _devToolsUri = await completer.future;
     } on Exception catch (e, st) {
       _logger.printError('Failed to launch DevTools: $e', stackTrace: st);
     }
