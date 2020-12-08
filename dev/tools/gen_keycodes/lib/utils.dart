7c4ab98a6b9523a767391e818d119498bef6cf60
Tong Mu <dkwingsmt@gmail.com>
2020-12-08 15:39:37 -0800

    Fix web gen

--- dev/tools/gen_keycodes/lib/utils.dart
+++ dev/tools/gen_keycodes/lib/utils.dart
@@ -146,11 +146,11 @@ void zipStrict<T1, T2>(Iterable<T1> list1, Iterable<T2> list2, void Function(T1,
   if (list1 == null && list2 == null)
     return;
   if ((list1?.length ?? 0) != (list2?.length ?? 0)) {
-    print('Mismatched lists ${list1} to ${list2}');
+    print('Mismatched lists $list1 to $list2');
     return;
   }
-  Iterator<T1> it1 = list1.iterator;
-  Iterator<T2> it2 = list2.iterator;
+  final Iterator<T1> it1 = list1.iterator;
+  final Iterator<T2> it2 = list2.iterator;
   while (it1.moveNext()) {
     it2.moveNext();
     fn(it1.current, it2.current);
