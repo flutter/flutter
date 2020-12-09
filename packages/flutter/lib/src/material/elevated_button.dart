96d1e437c2dfde0831f70cb514384d7f2ae5616d
Yazeed AlKhalaf <yazeedfady@gmail.com>
2020-12-09 16:15:12 +0300

    ❌ Remove trailing space

--- packages/flutter/lib/src/material/elevated_button.dart
+++ packages/flutter/lib/src/material/elevated_button.dart
@@ -429,7 +429,7 @@ class _ElevatedButtonWithIconChild extends StatelessWidget {
   Widget build(BuildContext context) {
     final double scale = MediaQuery.maybeOf(context)?.textScaleFactor ?? 1;
     final double gap = scale <= 1 ? 8 : lerpDouble(8, 4, math.min(scale - 1, 1))!;
-    
+
     List<Widget> _renderButtonChildren() {
       switch (iconAlignment) {
         case IconAlignment.left:
