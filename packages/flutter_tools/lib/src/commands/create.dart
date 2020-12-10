24d211e2a57bc36da816f93635bd204121657265
Chris Yang <ychris@google.com>
2020-12-10 10:26:53 -0800

    review fixes

--- packages/flutter_tools/lib/src/commands/create.dart
+++ packages/flutter_tools/lib/src/commands/create.dart
@@ -294,8 +294,9 @@ class CreateCommand extends CreateBase {
       if (!creatingNewProject && requestedPlatforms.isNotEmpty) {
         _printPluginUpdatePubspecMessage(relativePluginPath, platformsString);
       } else if (_getSupportedPlatformsInPlugin(projectDir).isEmpty){
-        _printNoPluginMessage(relativePluginPath);
+        _printNoPluginMessage();
       }
+      _printAddPlatformMessage(relativePluginPath);
     } else  {
       // Tell the user the next steps.
       final FlutterProject project = FlutterProject.fromPath(projectDirPath);
@@ -489,17 +490,20 @@ To edit platform code in an IDE see https://flutter.dev/developing-packages/#edi
 
 void _printPluginUpdatePubspecMessage(String pluginPath, String platformsString) {
   globals.printStatus('''
-
 You need to update $pluginPath/pubspec.yaml to support $platformsString.
-For more information, see https://flutter.dev/go/plugin-platforms.
 
 ''', emphasis: true, color: TerminalColor.red);
 }
 
-void _printNoPluginMessage(String pluginPath) {
+void _printNoPluginMessage() {
     globals.printError('''
+You've created a plugin project that doesn't yet support any platforms.
 
-Must specify at least one platform using --platforms.
+''');
+}
+
+void _printAddPlatformMessage(String pluginPath) {
+  globals.printStatus('''
 To add platforms, run `flutter create -t plugin --platforms <platforms> .` under $pluginPath.
 For more information, see https://flutter.dev/go/plugin-platforms.
 
