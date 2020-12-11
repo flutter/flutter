4e22c5dc7c510e1f94d94ae3e51008113ec96b4c
Mouad Debbar <mouad.debbar@gmail.com>
2020-12-11 09:28:37 -0800

    [web] Introduce flag to enable new rich text implementation (flutter/engine#22942)

--- engine/src/flutter/lib/web_ui/test/engine/web_experiments_test.dart
+++ engine/src/flutter/lib/web_ui/test/engine/web_experiments_test.dart
@@ -11,6 +11,7 @@ import 'package:test/test.dart';
 import 'package:ui/src/engine.dart';
 
 const bool _defaultUseCanvasText = true;
+const bool _defaultUseCanvasRichText = false;
 
 void main() {
   internalBootstrapBrowserTest(() => testMain);
@@ -27,47 +28,66 @@ void testMain() {
 
   test('default web experiment values', () {
     expect(WebExperiments.instance.useCanvasText, _defaultUseCanvasText);
+    expect(WebExperiments.instance.useCanvasRichText, _defaultUseCanvasRichText);
   });
 
   test('can turn on/off web experiments', () {
     WebExperiments.instance.updateExperiment('useCanvasText', true);
+    WebExperiments.instance.updateExperiment('useCanvasRichText', true);
     expect(WebExperiments.instance.useCanvasText, true);
+    expect(WebExperiments.instance.useCanvasRichText, true);
 
     WebExperiments.instance.updateExperiment('useCanvasText', false);
+    WebExperiments.instance.updateExperiment('useCanvasRichText', false);
     expect(WebExperiments.instance.useCanvasText, false);
+    expect(WebExperiments.instance.useCanvasRichText, false);
 
     WebExperiments.instance.updateExperiment('useCanvasText', null);
+    WebExperiments.instance.updateExperiment('useCanvasRichText', null);
     // Goes back to default value.
     expect(WebExperiments.instance.useCanvasText, _defaultUseCanvasText);
+    expect(WebExperiments.instance.useCanvasRichText, _defaultUseCanvasRichText);
   });
 
   test('ignores unknown experiments', () {
     expect(WebExperiments.instance.useCanvasText, _defaultUseCanvasText);
+    expect(WebExperiments.instance.useCanvasRichText, _defaultUseCanvasRichText);
     WebExperiments.instance.updateExperiment('foobarbazqux', true);
     expect(WebExperiments.instance.useCanvasText, _defaultUseCanvasText);
+    expect(WebExperiments.instance.useCanvasRichText, _defaultUseCanvasRichText);
     WebExperiments.instance.updateExperiment('foobarbazqux', false);
     expect(WebExperiments.instance.useCanvasText, _defaultUseCanvasText);
+    expect(WebExperiments.instance.useCanvasRichText, _defaultUseCanvasRichText);
   });
 
   test('can reset web experiments', () {
     WebExperiments.instance.updateExperiment('useCanvasText', false);
+    WebExperiments.instance.updateExperiment('useCanvasRichText', false);
     WebExperiments.instance.reset();
     expect(WebExperiments.instance.useCanvasText, _defaultUseCanvasText);
+    expect(WebExperiments.instance.useCanvasRichText, _defaultUseCanvasRichText);
 
     WebExperiments.instance.updateExperiment('useCanvasText', false);
+    WebExperiments.instance.updateExperiment('useCanvasRichText', false);
     WebExperiments.instance.updateExperiment('foobarbazqux', true);
     WebExperiments.instance.reset();
     expect(WebExperiments.instance.useCanvasText, _defaultUseCanvasText);
+    expect(WebExperiments.instance.useCanvasRichText, _defaultUseCanvasRichText);
   });
 
   test('js interop also works', () {
     expect(WebExperiments.instance.useCanvasText, _defaultUseCanvasText);
+    expect(WebExperiments.instance.useCanvasRichText, _defaultUseCanvasRichText);
 
     expect(() => jsUpdateExperiment('useCanvasText', true), returnsNormally);
+    expect(() => jsUpdateExperiment('useCanvasRichText', true), returnsNormally);
     expect(WebExperiments.instance.useCanvasText, true);
+    expect(WebExperiments.instance.useCanvasRichText, true);
 
     expect(() => jsUpdateExperiment('useCanvasText', null), returnsNormally);
+    expect(() => jsUpdateExperiment('useCanvasRichText', null), returnsNormally);
     expect(WebExperiments.instance.useCanvasText, _defaultUseCanvasText);
+    expect(WebExperiments.instance.useCanvasRichText, _defaultUseCanvasRichText);
   });
 
   test('js interop throws on wrong type', () {
