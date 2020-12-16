d7ab4b01cddac66ae1eedafa4aa76c0e968b8bfc
Gary Qian <garyq@google.com>
2020-12-15 22:18:07 -0700

    Disable flaky/hanging split AOT test (flutter/engine#23070)

--- engine/src/flutter/runtime/dart_isolate_unittests.cc
+++ engine/src/flutter/runtime/dart_isolate_unittests.cc
@@ -487,7 +487,9 @@ TEST_F(DartIsolateTest, InvalidLoadingUnitFails) {
   ASSERT_TRUE(root_isolate->Shutdown());
 }
 
-TEST_F(DartIsolateTest, ValidLoadingUnitSucceeds) {
+// TODO(garyq): Re-enable this test, and resolve dart-side hanging future and
+// threading. See https://github.com/flutter/flutter/issues/72312
+TEST_F(DartIsolateTest, DISABLED_ValidLoadingUnitSucceeds) {
   if (!DartVM::IsRunningPrecompiledCode()) {
     FML_LOG(INFO) << "Split AOT does not work in JIT mode";
     return;
@@ -529,6 +531,7 @@ TEST_F(DartIsolateTest, ValidLoadingUnitSucceeds) {
 
   ASSERT_TRUE(isolate->get()->LoadLoadingUnit(2, std::move(isolate_data),
                                               std::move(isolate_instructions)));
+  Wait();
 }
 
 }  // namespace testing
