9d3728041c51bca4408a4de4695906aa7ad091ed
Jia Hao Goh <jiahaog@users.noreply.github.com>
2020-12-16 23:34:52 +0800

    Newline, comment

--- packages/flutter/test/flutter_test_config.dart
+++ packages/flutter/test/flutter_test_config.dart
@@ -1,12 +1,16 @@
 // Copyright 2014 The Flutter Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-import 'dart:async' show FutureOr;
+
+import 'dart:async';
 
 import 'package:flutter/rendering.dart';
 import 'package:flutter_goldens/flutter_goldens.dart' as flutter_goldens;
 
-Future<void> testExecutable(FutureOr<void> testMain()) async {
+Future<void> testExecutable(FutureOr<void> testMain()) {
+  // Enable checks because there are many implementations of [RenderBox] in this
+  // package can benefit from the additional validations.
   debugCheckIntrinsicSizes = true;
+
   return flutter_goldens.testExecutable(testMain);
 }
