2490a9016a77e10c1c579874d3b88805863b1b6e
Muskan Ludhiyani <45829275+MuskanLudhiyani@users.noreply.github.com>
2020-12-26 17:18:57 +0530

    Update hindi.dart

--- examples/hello_world/lib/hindi.dart
+++ examples/hello_world/lib/hindi.dart
@@ -4,7 +4,10 @@ import 'package:flutter/widgets.dart';
 void main(){
 	runApp(
 		Center(
-			child: Text("नमस्ते दुनिया", textDirection: TextDirection.rtl)
+			child: Text(
+				"नमस्ते दुनिया", 
+				textDirection: TextDirection.rtl
+				 )
 			  )
 		  );
 },
