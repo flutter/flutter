c495fbb003c47842e72c765dc6d3b5ba35dd05ff
Madhur Maurya <madhurmaurya365@gmail.com>
2020-12-29 23:01:40 +0530

    Sample added to BottomNavigationBar

--- packages/flutter/lib/src/material/bottom_navigation_bar.dart
+++ packages/flutter/lib/src/material/bottom_navigation_bar.dart
@@ -69,7 +69,8 @@ enum BottomNavigationBarType {
 /// {@tool dartpad --template=stateful_widget_material}
 /// This example shows a [BottomNavigationBar] as it is used within a [Scaffold]
 /// widget. The [BottomNavigationBar] has three [BottomNavigationBarItem]
-/// widgets and the [currentIndex] is set to index 0. The selected item is
+/// widgets, which means it defaults to [BottomNavigationBarType.fixed], and
+/// the [currentIndex] is set to index 0. The selected item is
 /// amber. The `_onItemTapped` function changes the selected item's index
 /// and displays a corresponding message in the center of the [Scaffold].
 ///
@@ -133,6 +134,89 @@ enum BottomNavigationBarType {
 /// ```
 /// {@end-tool}
 ///
+/// {@tool dartpad --template=stateful_widget_material}
+/// This example shows a [BottomNavigationBar] as it is used within a [Scaffold]
+/// widget. The [BottomNavigationBar] has four [BottomNavigationBarItem]
+/// widgets, which means it defaults to [BottomNavigationBarType.shifting], and
+/// the [currentIndex] is set to index 0. The selected item is amber.
+/// With each [BottomNavigationBarItem] widget, backgroundColor property is
+/// also defined, which changes the background color of [BottomNavigationBar],
+/// according to the selected item. The `_onItemTapped` function changes the
+/// selected item's index and displays a corresponding message in the center of
+/// the [Scaffold].
+///
+/// ![A scaffold with a bottom navigation bar containing four bottom navigation
+/// bar items. The first one is selected.](https://flutter.github.io/assets-for-api-docs/assets/material/bottom_navigation_bar_shifting.png)
+///
+/// ```dart
+/// int _selectedIndex = 0;
+///  static const TextStyle optionStyle =
+///      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+///  static const List<Widget> _widgetOptions = <Widget>[
+///    Text(
+///      'Index 0: Home',
+///      style: optionStyle,
+///    ),
+///    Text(
+///      'Index 1: Business',
+///      style: optionStyle,
+///    ),
+///    Text(
+///      'Index 2: School',
+///      style: optionStyle,
+///    ),
+///    Text(
+///      'Index 3: Settings',
+///      style: optionStyle,
+///    ),
+///  ];
+///
+///  void _onItemTapped(int index) {
+///    setState(() {
+///      _selectedIndex = index;
+///    });
+///  }
+///
+///  @override
+///  Widget build(BuildContext context) {
+///    return Scaffold(
+///      appBar: AppBar(
+///        title: const Text('BottomNavigationBar Sample'),
+///      ),
+///      body: Center(
+///        child: _widgetOptions.elementAt(_selectedIndex),
+///      ),
+///      bottomNavigationBar: BottomNavigationBar(
+///        items: const <BottomNavigationBarItem>[
+///          BottomNavigationBarItem(
+///            icon: Icon(Icons.home),
+///            label: 'Home',
+///            backgroundColor: Colors.red,
+///          ),
+///          BottomNavigationBarItem(
+///            icon: Icon(Icons.business),
+///            label: 'Business',
+///            backgroundColor: Colors.green,
+///          ),
+///          BottomNavigationBarItem(
+///            icon: Icon(Icons.school),
+///            label: 'School',
+///            backgroundColor: Colors.purple,
+///          ),
+///          BottomNavigationBarItem(
+///            icon: Icon(Icons.settings),
+///            label: 'Settings',
+///            backgroundColor: Colors.pink,
+///          ),
+///        ],
+///        currentIndex: _selectedIndex,
+///        selectedItemColor: Colors.amber[800],
+///        onTap: _onItemTapped,
+///      ),
+///    );
+///  }
+/// ```
+/// {@end-tool}
 /// See also:
 ///
 ///  * [BottomNavigationBarItem]
