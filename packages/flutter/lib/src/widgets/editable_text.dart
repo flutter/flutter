4901744e62f67ad10440725d2c97e84e66ce77f5
Jesse <jessegielen@hotmail.com>
2021-01-05 23:04:06 +0100

    Add BuildContext parameter to TextEditingController.buildTextSpan (#72344)

--- packages/flutter/lib/src/widgets/editable_text.dart
+++ packages/flutter/lib/src/widgets/editable_text.dart
@@ -185,7 +185,7 @@ class TextEditingController extends ValueNotifier<TextEditingValue> {
   ///
   /// By default makes text in composing range appear as underlined. Descendants
   /// can override this method to customize appearance of text.
-  TextSpan buildTextSpan({TextStyle? style , required bool withComposing}) {
+  TextSpan buildTextSpan({TextStyle? style , required bool withComposing, required BuildContext context}) {
     assert(!value.composing.isValid || !withComposing || value.isComposingRangeValid);
     // If the composing range is out of range for the current text, ignore it to
     // preserve the tree integrity, otherwise in release mode a RangeError will
@@ -2669,6 +2669,7 @@ class EditableTextState extends State<EditableText> with AutomaticKeepAliveClien
     return widget.controller.buildTextSpan(
       style: widget.style,
       withComposing: !widget.readOnly,
+      context: context,
     );
   }
 }
