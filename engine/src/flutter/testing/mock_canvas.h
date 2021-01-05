6c644aeaff4d1f97b031a841245d70c08ee4425d
Michael Reed <mike@reedtribe.org>
2021-01-05 08:54:20 -0500

    Remove dead code for 3x3 matrices (flutter/engine#23363)
    Co-authored-by: Mike Reed <reed@google.com>

--- engine/src/flutter/testing/mock_canvas.h
+++ engine/src/flutter/testing/mock_canvas.h
@@ -144,10 +144,6 @@ class MockCanvas : public SkCanvasVirtualEnforcer<SkCanvas> {
   SaveLayerStrategy getSaveLayerStrategy(const SaveLayerRec& rec) override;
   void willRestore() override;
   void didRestore() override {}
-#ifdef SK_SUPPORT_LEGACY_CANVASMATRIX33
-  void didConcat(const SkMatrix& matrix) override;
-  void didSetMatrix(const SkMatrix& matrix) override;
-#endif
   void didConcat44(const SkM44&) override;
   void didSetM44(const SkM44&) override;
   void didScale(SkScalar x, SkScalar y) override;
