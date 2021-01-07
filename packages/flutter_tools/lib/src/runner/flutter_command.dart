07be5ead634532ccb7e3bc47d8692a9d5c1f048a
ferhatb <ferhat@gmail.com>
2021-01-07 08:58:35 -0800

    Switch flutter tool web-renderer default for web to auto

--- packages/flutter_tools/lib/src/runner/flutter_command.dart
+++ packages/flutter_tools/lib/src/runner/flutter_command.dart
@@ -461,7 +461,7 @@ abstract class FlutterCommand extends Command<void> {
 
   void usesWebRendererOption() {
     argParser.addOption('web-renderer',
-      defaultsTo: 'html',
+      defaultsTo: 'auto',
       allowed: <String>['auto', 'canvaskit', 'html'],
       help: 'The renderer implementation to use when building for the web. Possible values are:\n'
             'html - always use the HTML renderer. This renderer uses a combination of HTML, CSS, SVG, 2D Canvas, and WebGL. This is the default.\n'
