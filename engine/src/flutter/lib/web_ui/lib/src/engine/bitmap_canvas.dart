43a09f48766f4a1f5f1c329566aef1a53da26aca
Mouad Debbar <mouad.debbar@gmail.com>
2021-01-07 13:03:26 -0800

    [web] Draw shadows for text in rich paragraphs (flutter/engine#23470)

--- engine/src/flutter/lib/web_ui/lib/src/engine/bitmap_canvas.dart
+++ engine/src/flutter/lib/web_ui/lib/src/engine/bitmap_canvas.dart
@@ -833,8 +833,21 @@ class BitmapCanvas extends EngineCanvas {
   ///
   /// The text is drawn starting at coordinates ([x], [y]). It uses the current
   /// font set by the most recent call to [setCssFont].
-  void fillText(String text, double x, double y) {
-    _canvasPool.context.fillText(text, x, y);
+  void fillText(String text, double x, double y, {List<ui.Shadow>? shadows}) {
+    final html.CanvasRenderingContext2D ctx = _canvasPool.context;
+    if (shadows != null) {
+      ctx.save();
+      for (final ui.Shadow shadow in shadows) {
+        ctx.shadowColor = colorToCssString(shadow.color)!;
+        ctx.shadowBlur = shadow.blurRadius;
+        ctx.shadowOffsetX = shadow.offset.dx;
+        ctx.shadowOffsetY = shadow.offset.dy;
+
+        ctx.fillText(text, x, y);
+      }
+      ctx.restore();
+    }
+    ctx.fillText(text, x, y);
   }
 
   @override
