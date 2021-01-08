4fb92e8f16e2c73b8e79bd9c0ec094e9e4417abe
Jenn Magder <magder@google.com>
2021-01-08 14:22:31 -0800

    Move skips to tests, add verbose logging

--- packages/flutter_tools/test/integration.shard/ios_content_validation_test.dart
+++ packages/flutter_tools/test/integration.shard/ios_content_validation_test.dart
@@ -45,16 +45,20 @@ void main() {
 
         projectRoot = tempDir.childDirectory('hello').path;
 
-        processManager.runSync(<String>[
+        final ProcessResult result = processManager.runSync(<String>[
           flutterBin,
           ...getLocalEngineArguments(),
           'build',
           'ios',
+          '--verbose',
           '--no-codesign',
           '--${buildMode.name}',
           '--obfuscate',
           '--split-debug-info=foo/',
         ], workingDirectory: projectRoot);
+
+        print(result.stdout);
+        print(result.stderr);
 
         buildPath = fileSystem.directory(fileSystem.path.join(
           projectRoot,
@@ -106,7 +110,7 @@ void main() {
         // This mimics Xcode behavior and present a developer from having to install a
         // 300+MB app.
         expect(containsBitcode(outputFlutterFrameworkBinary.path, processManager), isFalse);
-      });
+      }, skip: !platform.isMacOS);
 
       testWithoutContext('Info.plist dart observatory Bonjour service', () {
         final String infoPlistPath = fileSystem.path.join(
@@ -140,7 +144,7 @@ void main() {
         );
         final bool localNetworkUsageFound = localNetworkUsage.exitCode == 0;
         expect(localNetworkUsageFound, buildMode == BuildMode.debug);
-      });
+      }, skip: !platform.isMacOS);
 
       testWithoutContext('check symbols', () {
         final ProcessResult symbols = processManager.runSync(
@@ -154,7 +158,7 @@ void main() {
         );
         final bool aotSymbolsFound = (symbols.stdout as String).contains('_kDartVmSnapshot');
         expect(aotSymbolsFound, buildMode != BuildMode.debug);
-      });
+      }, skip: !platform.isMacOS);
 
       testWithoutContext('xcode_backend embed_and_thin', () {
         outputFlutterFramework.deleteSync(recursive: true);
@@ -197,7 +201,7 @@ void main() {
         expect(xcodeBackendResult.exitCode, 0);
         expect(outputFlutterFrameworkBinary.existsSync(), isTrue);
         expect(outputAppFrameworkBinary.existsSync(), isTrue);
-      }, skip: buildMode != BuildMode.release);
+      }, skip: !platform.isMacOS || buildMode != BuildMode.release);
 
       testWithoutContext('validate obfuscation', () {
         final ProcessResult grepResult = processManager.runSync(<String>[
@@ -207,9 +211,8 @@ void main() {
           outputAppFrameworkBinary.path,
         ]);
         expect(grepResult.stdout, isNot(contains('matches')));
-      });
+      }, skip: !platform.isMacOS);
     },
-      skip: !platform.isMacOS,
       timeout: const Timeout(Duration(minutes: 5)),
     );
   }
