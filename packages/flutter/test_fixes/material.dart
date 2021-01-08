535392bfd51cf42c6266561a13d03fd17983c830
Michael Goderbauer <goderbauer@google.com>
2021-01-08 15:44:08 -0800

    added tests, removed API (#69620)

--- packages/flutter/test_fixes/material.dart
+++ packages/flutter/test_fixes/material.dart
@@ -16,4 +16,14 @@ void main() {
   // Changes made in https://github.com/flutter/flutter/pull/44189
   const StatefulElement statefulElement = StatefulElement(myWidget);
   statefulElement.inheritFromElement(ancestor);
+
+  // Changes made in https://github.com/flutter/flutter/pull/44189
+  const BuildContext buildContext = Element(myWidget);
+  buildContext.inheritFromElement(ancestor);
+  buildContext.inheritFromWidgetOfExactType(targetType);
+  buildContext.ancestorInheritedElementForWidgetOfExactType(targetType);
+  buildContext.ancestorWidgetOfExactType(targetType);
+  buildContext.ancestorStateOfType(TypeMatcher<targetType>());
+  buildContext.rootAncestorStateOfType(TypeMatcher<targetType>());
+  buildContext.ancestorRenderObjectOfType(TypeMatcher<targetType>());
 }
