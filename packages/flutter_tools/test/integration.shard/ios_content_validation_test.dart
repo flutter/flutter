94bc78a2cf291a3737a30692f86fea9cbadf10c2
Jenn Magder <magder@google.com>
2021-01-08 16:10:48 -0800

    Use Obj-C

--- packages/flutter_tools/test/integration.shard/ios_content_validation_test.dart
+++ packages/flutter_tools/test/integration.shard/ios_content_validation_test.dart
@@ -40,6 +40,8 @@ void main() {
           ...getLocalEngineArguments(),
           'create',
           '--platforms=ios',
+          '-i',
+          'objc',
           'hello',
         ], workingDirectory: tempDir.path);
 
