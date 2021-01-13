cbc6d557c7bd5f8b103254072797888f54bf025c
Justin McCandless <jmccandless@google.com>
2021-01-13 15:51:50 -0800

    Working toolbar builder

--- packages/flutter/lib/src/cupertino/desktop_text_selection_toolbar.dart
+++ packages/flutter/lib/src/cupertino/desktop_text_selection_toolbar.dart
@@ -8,16 +8,30 @@ import 'text_selection_toolbar.dart';
 // screen.
 const double _kToolbarScreenPadding = 8.0;
 
-// Colors extracted from https://developer.apple.com/design/resources/.
-// TODO(LongCatIsLooong): https://github.com/flutter/flutter/issues/41507.
-const Color _kToolbarDividerColor = Color(0xFF808080);
-
 // These values were measured from a screenshot of TextEdit on MacOS 10.15.7 on
 // a Macbook Pro.
 const double _kToolbarWidth = 222.0;
 const Color _kToolbarBorderColor = Color(0xFF505152);
 const Radius _kToolbarBorderRadius = Radius.circular(4.0);
 const Color _kToolbarBackgroundColor = Color(0xFF2D2E31);
+
+/// The type for a Function that builds a toolbar's container with the given
+/// child.
+///
+/// The anchor is provided in global coordinates.
+///
+/// See also:
+///
+///   * [CupertinoDesktopTextSelectionToolbar.toolbarBuilder], which is of this type.
+///   * [CupertinoTextSelectionToolbar.toolbarBuilder], which is similar, but for a
+///     mobile Cupertino-style toolbar.
+///   * [TextSelectionToolbar.toolbarBuilder], which is similar, but for a
+///     Material-style toolbar.
+typedef CupertinoDesktopToolbarBuilder = Widget Function(
+  BuildContext context,
+  Offset anchor,
+  Widget child,
+);
 
 /// An iOS-style text selection toolbar.
 ///
@@ -61,15 +75,29 @@ class CupertinoDesktopTextSelectionToolbar extends StatelessWidget {
   ///
   /// The given anchor and isAbove can be used to position an arrow, as in the
   /// default Cupertino toolbar.
-  final CupertinoToolbarBuilder toolbarBuilder;
+  final CupertinoDesktopToolbarBuilder toolbarBuilder;
 
-  // Builds a toolbar just like the default iOS toolbar, with the right color
-  // background and a rounded cutout with an arrow.
-  static Widget _defaultToolbarBuilder(BuildContext context, Offset anchor, bool isAbove, Widget child) {
-    // TODO(justinmc): I just removed Shape here, is this ok otherwise?
-    return DecoratedBox(
-      decoration: const BoxDecoration(color: _kToolbarDividerColor),
-      child: child,
+  // Builds a toolbar just like the default Mac toolbar, with the right color
+  // background, padding, and rounded corners.
+  static Widget _defaultToolbarBuilder(BuildContext context, Offset anchor, Widget child) {
+    return Container(
+      width: _kToolbarWidth,
+      decoration: BoxDecoration(
+        color: _kToolbarBackgroundColor,
+        border: Border.all(
+          color: _kToolbarBorderColor,
+        ),
+        borderRadius: const BorderRadius.all(_kToolbarBorderRadius)
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 0.0,
+          // This value was measured from a screenshot of TextEdit on MacOS
+          // 10.15.7 on a Macbook Pro.
+          vertical: 3.0,
+        ),
+        child: child,
+      ),
     );
   }
 
@@ -92,28 +120,10 @@ class CupertinoDesktopTextSelectionToolbar extends StatelessWidget {
         delegate: _DesktopTextSelectionToolbarLayoutDelegate(
           anchor: anchor - localAdjustment,
         ),
-        child: Container(
-          width: _kToolbarWidth,
-          decoration: BoxDecoration(
-            color: _kToolbarBackgroundColor,
-            border: Border.all(
-              color: _kToolbarBorderColor,
-            ),
-            borderRadius: const BorderRadius.all(_kToolbarBorderRadius)
-          ),
-          child: Padding(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 0.0,
-              // This value was measured from a screenshot of TextEdit on MacOS
-              // 10.15.7 on a Macbook Pro.
-              vertical: 3.0,
-            ),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: children,
-            ),
-          ),
-        ),
+        child: toolbarBuilder(context, anchor, Column(
+          mainAxisSize: MainAxisSize.min,
+          children: children,
+        )),
       ),
     );
   }
