2ed0b58a2565f6d8c7eee7d7449240a5b0fb6b79
Justin McCandless <jmccandless@google.com>
2021-01-13 15:56:46 -0800

    Delete handles

--- packages/flutter/lib/src/cupertino/desktop_text_selection.dart
+++ packages/flutter/lib/src/cupertino/desktop_text_selection.dart
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'dart:math' as math;
-
 import 'package:flutter/gestures.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
@@ -12,13 +10,6 @@ import 'package:flutter/widgets.dart';
 import 'desktop_text_selection_toolbar.dart';
 import 'desktop_text_selection_toolbar_button.dart';
 import 'localizations.dart';
-import 'theme.dart';
-
-// Read off from the output on iOS 12. This color does not vary with the
-// application's theme color.
-const double _kSelectionHandleOverlap = 1.5;
-// Extracted from https://developer.apple.com/design/resources/.
-const double _kSelectionHandleRadius = 6;
 
 // Minimal padding from tip of the selection toolbar arrow to horizontal edges of the
 // screen. Eyeballed value.
@@ -33,46 +24,11 @@ const EdgeInsets _kToolbarButtonPadding = EdgeInsets.symmetric(
   horizontal: 20.0,
 );
 
-/// Draws a single text selection handle with a bar and a ball.
-class _TextSelectionHandlePainter extends CustomPainter {
-  const _TextSelectionHandlePainter(this.color);
-
-  final Color color;
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    const double halfStrokeWidth = 1.0;
-    final Paint paint = Paint()..color = color;
-    final Rect circle = Rect.fromCircle(
-      center: const Offset(_kSelectionHandleRadius, _kSelectionHandleRadius),
-      radius: _kSelectionHandleRadius,
-    );
-    final Rect line = Rect.fromPoints(
-      const Offset(
-        _kSelectionHandleRadius - halfStrokeWidth,
-        2 * _kSelectionHandleRadius - _kSelectionHandleOverlap,
-      ),
-      Offset(_kSelectionHandleRadius + halfStrokeWidth, size.height),
-    );
-    final Path path = Path()
-      ..addOval(circle)
-    // Draw line so it slightly overlaps the circle.
-      ..addRect(line);
-    canvas.drawPath(path, paint);
-  }
-
-  @override
-  bool shouldRepaint(_TextSelectionHandlePainter oldPainter) => color != oldPainter.color;
-}
-
 class _CupertinoDesktopTextSelectionControls extends TextSelectionControls {
-  /// Returns the size of the CupertinoDesktop handle.
+  /// Desktop has no text selection handles.
   @override
   Size getHandleSize(double textLineHeight) {
-    return Size(
-      _kSelectionHandleRadius * 2,
-      textLineHeight + _kSelectionHandleRadius * 2 - _kSelectionHandleOverlap,
-    );
+    return Size.zero;
   }
 
   /// Builder for the Mac-style copy/paste text selection toolbar.
@@ -101,70 +57,16 @@ class _CupertinoDesktopTextSelectionControls extends TextSelectionControls {
     );
   }
 
-  // TODO(justinmc): Can I get rid of the handles here?
-  /// Builder for iOS text selection edges.
+  /// Desktop has no text selection handles.
   @override
   Widget buildHandle(BuildContext context, TextSelectionHandleType type, double textLineHeight) {
-    // We want a size that's a vertical line the height of the text plus a 18.0
-    // padding in every direction that will constitute the selection drag area.
-    final Size desiredSize = getHandleSize(textLineHeight);
-
-    final Widget handle = SizedBox.fromSize(
-      size: desiredSize,
-      child: CustomPaint(
-        painter: _TextSelectionHandlePainter(CupertinoTheme.of(context).primaryColor),
-      ),
-    );
-
-    // [buildHandle]'s widget is positioned at the selection cursor's bottom
-    // baseline. We transform the handle such that the SizedBox is superimposed
-    // on top of the text selection endpoints.
-    switch (type) {
-      case TextSelectionHandleType.left:
-        return handle;
-      case TextSelectionHandleType.right:
-        // Right handle is a vertical mirror of the left.
-        return Transform(
-          transform: Matrix4.identity()
-            ..translate(desiredSize.width / 2, desiredSize.height / 2)
-            ..rotateZ(math.pi)
-            ..translate(-desiredSize.width / 2, -desiredSize.height / 2),
-          child: handle,
-        );
-      // iOS doesn't draw anything for collapsed selections.
-      case TextSelectionHandleType.collapsed:
-        return const SizedBox();
-    }
+    return const SizedBox.shrink();
   }
 
-  /// Gets anchor for cupertino-style text selection handles.
-  ///
-  /// See [TextSelectionControls.getHandleAnchor].
+  /// Desktop has no text selection handles.
   @override
   Offset getHandleAnchor(TextSelectionHandleType type, double textLineHeight) {
-    final Size handleSize = getHandleSize(textLineHeight);
-    switch (type) {
-      // The circle is at the top for the left handle, and the anchor point is
-      // all the way at the bottom of the line.
-      case TextSelectionHandleType.left:
-        return Offset(
-          handleSize.width / 2,
-          handleSize.height,
-        );
-      // The right handle is vertically flipped, and the anchor point is near
-      // the top of the circle to give slight overlap.
-      case TextSelectionHandleType.right:
-        return Offset(
-          handleSize.width / 2,
-          handleSize.height - 2 * _kSelectionHandleRadius + _kSelectionHandleOverlap,
-        );
-      // A collapsed handle anchors itself so that it's centered.
-      case TextSelectionHandleType.collapsed:
-        return Offset(
-          handleSize.width / 2,
-          textLineHeight + (handleSize.height - textLineHeight) / 2,
-        );
-    }
+    return Offset.zero;
   }
 }
 
