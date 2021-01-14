ee2ac2a5afc33b25f5d163134237d7d7fca39f0b
Basit Ayantunde <rlamarrr@gmail.com>
2021-01-14 14:33:29 -0800

    removed variable-sized array (#23102)
    This is undefined behavior. I've replaced it with std::vector instead
    
    Co-authored-by: Zachary Anderson <zanderso@users.noreply.github.com>

--- vulkan/vulkan_device.cc
+++ vulkan/vulkan_device.cc
@@ -288,9 +288,11 @@ int VulkanDevice::ChooseSurfaceFormat(const VulkanSurface& surface,
     return -1;
   }
 
-  VkSurfaceFormatKHR formats[format_count];
+  std::vector<VkSurfaceFormatKHR> formats;
+  formats.resize(format_count);
+
   if (VK_CALL_LOG_ERROR(vk.GetPhysicalDeviceSurfaceFormatsKHR(
-          physical_device_, surface.Handle(), &format_count, formats)) !=
+          physical_device_, surface.Handle(), &format_count, formats.data())) !=
       VK_SUCCESS) {
     return -1;
   }
