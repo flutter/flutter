64036de22cbfdbaf1627f8788b3c7e464df33d94
Madhur Maurya <madhurmaurya365@gmail.com>
2021-01-14 18:18:19 +0530

    Removing Sample Image for BottomNavigationBarType.fixed

--- packages/flutter/lib/src/material/bottom_navigation_bar.dart
+++ packages/flutter/lib/src/material/bottom_navigation_bar.dart
@@ -75,9 +75,6 @@ enum BottomNavigationBarType {
 /// amber. The `_onItemTapped` function changes the selected item's index
 /// and displays a corresponding message in the center of the [Scaffold].
 ///
-/// ![A scaffold with a bottom navigation bar containing three bottom navigation
-/// bar items. The first one is selected.](https://flutter.github.io/assets-for-api-docs/assets/material/bottom_navigation_bar.png)
-///
 /// ```dart
 /// int _selectedIndex = 0;
 /// static const TextStyle optionStyle = TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
