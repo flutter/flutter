3f8880018fd3c99187ea954858d2a5f4dac540de
Shi Hao Hong <shihaohong94@gmail.com>
2021-01-15 15:25:56 +0800

    Add documentation for showDialog and DialogRoute

--- packages/flutter/lib/src/material/dialog.dart
+++ packages/flutter/lib/src/material/dialog.dart
@@ -950,6 +950,88 @@ Widget _buildMaterialDialogTransitions(BuildContext context, Animation<double> a
 /// Returns a [Future] that resolves to the value (if any) that was passed to
 /// [Navigator.pop] when the dialog was closed.
 ///
+/// ### State Restoration in Dialogs
+///
+/// Using this method with not enable state restoration for the dialog. In order
+/// to enable state restoration for a dialog, use `Navigator.restorablePush`
+/// or `Navigator.restorablePushNamed` with `DialogRoute`.
+///
+/// For more information about state restoration, see [RestorationManager].
+///
+/// {@tool sample --template=freeform}
+///
+/// This sample demonstrates how to create a restorable Material dialog. This is
+/// accomplished by enabling state restoration by specifying
+/// `MaterialApp.restorationScopeId` and using `Navigator.restorablePush` to
+/// push `DialogRoute` when the button is tapped.
+///
+/// To test this out:
+///   1. Turn on "Don't keep activities", which destroys the Android activity
+///      as soon as the user leaves it. This option should become available
+///      when Developer options are turned on for the device.
+///   2. Run the code sample on an Android device.
+///   3. Tap the button to open the dialog.
+///   4. Background the Flutter app, then return to it, noting that the dialog
+///      remains open.
+///
+/// ```dart imports
+/// import 'package:flutter/material.dart';
+/// ```
+///
+/// ```dart
+/// void main() {
+///   runApp(MyApp());
+/// }
+///
+/// class MyApp extends StatelessWidget {
+///   @override
+///   Widget build(BuildContext context) {
+///     return MaterialApp(
+///       restorationScopeId: 'app',
+///       title: 'Restorable Routes Demo',
+///       home: MyHomePage(title: 'Restorable Routes Demo'),
+///     );
+///   }
+/// }
+///
+/// class MyHomePage extends StatefulWidget {
+///   MyHomePage({this.title});
+///
+///   final String title;
+///
+///   @override
+///   _MyHomePageState createState() => _MyHomePageState();
+/// }
+///
+/// class _MyHomePageState extends State<MyHomePage> {
+///   static Route _dialogBuilder(BuildContext context, Object arguments) {
+///     return DialogRoute(
+///       context: context,
+///       builder: (BuildContext context) => AlertDialog(title: Text('Material Alert!')),
+///     );
+///   }
+///
+///   @override
+///   Widget build(BuildContext context) {
+///     return Scaffold(
+///       appBar: AppBar(
+///         title: Text(widget.title),
+///       ),
+///       body: Center(
+///         child: OutlineButton(
+///           onPressed: () {
+///             Navigator.of(context).restorablePush(_dialogBuilder);
+///           },
+///           child: Text('Open Dialog'),
+///         ),
+///       ),
+///     );
+///   }
+/// }
+/// ```
+///
+/// {@end-tool}
+///
 /// See also:
 ///
 ///  * [AlertDialog], for dialogs that have a row of buttons below a body.
@@ -998,6 +1080,32 @@ Future<T?> showDialog<T>({
 /// A dialog route with Material entrance and exit animations,
 /// modal barrier color, and modal barrier behavior (dialog is dismissible
 /// with a tap on the barrier).
+///
+/// This function takes a `builder` which typically builds a [Dialog] widget.
+/// Content below the dialog is dimmed with a [ModalBarrier]. The widget
+/// returned by the `builder` does not share a context with the location that
+/// `showDialog` is originally called from. Use a [StatefulBuilder] or a
+/// custom [StatefulWidget] if the dialog needs to update dynamically.
+///
+/// The `context` argument is used to look up
+/// `MaterialLocalizations.modalBarrierDismissLabel`, which is the default
+/// modal accessibility label that will be used for the modal's barrier.
+///
+/// The `barrierDismissible` argument is used to indicate whether tapping on the
+/// barrier will dismiss the dialog. It is `true` by default and cannot be `null`.
+///
+/// The `barrierColor` argument is used to specify the color of the modal
+/// barrier that darkens everything below the dialog. If `null`, the default
+/// color `Colors.black54` is used.
+///
+/// The `useSafeArea` argument is used to indicate if the dialog should only
+/// display in 'safe' areas of the screen not used by the operating system
+/// (see [SafeArea] for more details). It is `true` by default, which means
+/// the dialog will not overlap operating system areas. If it is set to `false`
+/// the dialog will only be constrained by the screen size. It can not be `null`.
+///
+/// The `settings` argument define the settings for this route. See
+/// [RouteSettings] for details.
 class DialogRoute<T> extends RawDialogRoute<T> {
   /// A dialog route with Material entrance and exit animations,
   /// modal barrier color, and modal barrier behavior (dialog is dismissible
