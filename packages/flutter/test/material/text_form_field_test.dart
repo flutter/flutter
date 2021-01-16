077dab31fa81b5b11ebfaf01fad301253b9435e2
Justin McCandless <jmccandless@google.com>
2021-01-16 00:08:51 -0800

    fix clipboard in textformfield test

--- packages/flutter/test/material/text_form_field_test.dart
+++ packages/flutter/test/material/text_form_field_test.dart
@@ -12,7 +12,27 @@ import 'package:flutter/rendering.dart';
 import '../rendering/mock_canvas.dart';
 import '../widgets/editable_text_utils.dart';
 
+class MockClipboard {
+  Object _clipboardData = <String, dynamic>{
+    'text': null,
+  };
+
+  Future<dynamic> handleMethodCall(MethodCall methodCall) async {
+    switch (methodCall.method) {
+      case 'Clipboard.getData':
+        return _clipboardData;
+      case 'Clipboard.setData':
+        _clipboardData = methodCall.arguments as Object;
+        break;
+    }
+  }
+}
+
 void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+  final MockClipboard mockClipboard = MockClipboard();
+  SystemChannels.platform.setMockMethodCallHandler(mockClipboard.handleMethodCall);
+
   setUp(() async {
     // Fill the clipboard so that the Paste option is available in the text
     // selection menu.
