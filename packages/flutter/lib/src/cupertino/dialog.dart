7e750fc1a8170ac20d8d4f1ba32b23d644dfc745
Kate Lovett <katelovett@google.com>
2021-01-19 16:44:03 -0600

    Remove deprecated CupertinoDialog (#73604)

--- packages/flutter/lib/src/cupertino/dialog.dart
+++ packages/flutter/lib/src/cupertino/dialog.dart
@@ -279,47 +279,6 @@ class CupertinoAlertDialog extends StatelessWidget {
   }
 }
 
-/// An iOS-style dialog.
-///
-/// This dialog widget does not have any opinion about the contents of the
-/// dialog. Rather than using this widget directly, consider using
-/// [CupertinoAlertDialog], which implement a specific kind of dialog.
-///
-/// Push with `Navigator.of(..., rootNavigator: true)` when using with
-/// [CupertinoTabScaffold] to ensure that the dialog appears above the tabs.
-///
-/// See also:
-///
-///  * [CupertinoAlertDialog], which is a dialog with title, contents, and
-///    actions.
-///  * <https://developer.apple.com/ios/human-interface-guidelines/views/alerts/>
-@Deprecated(
-  'Use CupertinoAlertDialog for alert dialogs. Use CupertinoPopupSurface for custom popups. '
-  'This feature was deprecated after v0.2.3.'
-)
-class CupertinoDialog extends StatelessWidget {
-  /// Creates an iOS-style dialog.
-  const CupertinoDialog({
-    Key? key,
-    this.child,
-  }) : super(key: key);
-
-  /// The widget below this widget in the tree.
-  final Widget? child;
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: SizedBox(
-        width: _kCupertinoDialogWidth,
-        child: CupertinoPopupSurface(
-          child: child,
-        ),
-      ),
-    );
-  }
-}
-
 /// Rounded rectangle surface that looks like an iOS popup surface, e.g., alert dialog
 /// and action sheet.
 ///
