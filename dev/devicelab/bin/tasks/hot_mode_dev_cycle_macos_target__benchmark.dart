444c954f439bb5dce0e9c130c7a37a424ccb5927
Jenn Magder <magder@google.com>
2021-01-21 11:31:09 -0800

    Manually close tree while devicelab staging is failing (#74434)

--- dev/devicelab/bin/tasks/hot_mode_dev_cycle_macos_target__benchmark.dart
+++ dev/devicelab/bin/tasks/hot_mode_dev_cycle_macos_target__benchmark.dart
@@ -2,9 +2,14 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'package:flutter_devicelab/framework/task_result.dart';
 import 'package:flutter_devicelab/tasks/hot_mode_tests.dart';
 import 'package:flutter_devicelab/framework/framework.dart';
 
 Future<void> main() async {
   await task(createHotModeTest(deviceIdOverride: 'macos'));
+
+  // TODO(fujino): https://github.com/flutter/flutter/issues/74431
+  throw TaskResult.failure(
+      'Tree was manually closed for Android version of this test https://github.com/flutter/flutter/issues/74431');
 }
