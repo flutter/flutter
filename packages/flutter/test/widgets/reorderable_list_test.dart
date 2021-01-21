53b3b6b78131c1a0c01b7507bc6a29dedd17c57d
darrenaustin <darrenaustin@google.com>
2021-01-20 18:54:50 -0800

    Added tests for the the inherited item styles when dragging. Thanks Hans!

--- packages/flutter/test/widgets/reorderable_list_test.dart
+++ packages/flutter/test/widgets/reorderable_list_test.dart
@@ -7,7 +7,6 @@ import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-
 void main() {
   testWidgets('ReorderableList, drag and drop, fixed height items', (WidgetTester tester) async {
     final List<int> items = List<int>.generate(8, (int index) => index);
@@ -74,56 +73,124 @@ void main() {
     expect(tester.getTopLeft(find.text('item 3')), const Offset(0, 300));
     expect(items, orderedEquals(<int>[0, 1, 2, 3, 4, 5, 6, 7]));
   });
+
+  testWidgets('ReorderableList, items inherit DefaultTextStyle, IconTheme', (WidgetTester tester) async {
+    const Color textColor = Color(0xffffffff);
+    const Color iconColor = Color(0xff0000ff);
+
+    TextStyle getIconStyle() {
+      return tester.widget<RichText>(
+        find.descendant(
+          of: find.byType(Icon),
+          matching: find.byType(RichText),
+        ),
+      ).text.style!;
+    }
+
+    TextStyle getTextStyle() {
+      return tester.widget<RichText>(
+        find.descendant(
+          of: find.text('item 0'),
+          matching: find.byType(RichText),
+        ),
+      ).text.style!;
+    }
+
+    // This SliverReorderableList has just one item: "item 0".
+    await tester.pumpWidget(
+      TestList(
+        items: List<int>.from(<int>[0]),
+        textColor: textColor,
+        iconColor: iconColor,
+      ),
+    );
+    expect(tester.getTopLeft(find.text('item 0')), Offset.zero);
+    expect(getIconStyle().color, iconColor);
+    expect(getTextStyle().color, textColor);
+
+    // Dragging item 0 causes it to be reparented in the overlay. The item
+    // should still inherit the IconTheme and DefaultTextStyle because they are
+    // InheritedThemes.
+    final TestGesture drag = await tester.startGesture(tester.getCenter(find.text('item 0')));
+    await tester.pump(kPressTimeout);
+    await drag.moveBy(const Offset(0, 50));
+    await tester.pump(kPressTimeout);
+    expect(tester.getTopLeft(find.text('item 0')), const Offset(0, 50));
+    expect(getIconStyle().color, iconColor);
+    expect(getTextStyle().color, textColor);
+
+    // Drag is complete, item 0 returns to where it was.
+    await drag.up();
+    await tester.pumpAndSettle();
+    expect(tester.getTopLeft(find.text('item 0')), Offset.zero);
+    expect(getIconStyle().color, iconColor);
+    expect(getTextStyle().color, textColor);
+  });
 }
 
 class TestList extends StatefulWidget {
   const TestList({
     Key? key,
+    this.textColor,
+    this.iconColor,
     required this.items,
   }) : super(key: key);
 
   final List<int> items;
+  final Color? textColor;
+  final Color? iconColor;
 
   @override
   _TestListState createState() => _TestListState();
 }
-
 
 class _TestListState extends State<TestList> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: StatefulBuilder(
-          builder: (BuildContext outerContext, StateSetter setState) {
-            final List<int> items = widget.items;
-            return CustomScrollView(
-              slivers: <Widget>[
-                SliverReorderableList(
-                  itemBuilder: (BuildContext context, int index) {
-                    return Container(
-                      key: ValueKey<int>(items[index]),
-                      height: 100,
-                      color: items[index].isOdd ? Colors.red : Colors.green,
-                      child: ReorderableDragStartListener(
-                        index: index,
-                        child: Text('item ${items[index]}'),
+        body: DefaultTextStyle(
+          style: TextStyle(color: widget.textColor),
+          child: IconTheme(
+            data: IconThemeData(color: widget.iconColor),
+            child: StatefulBuilder(
+                builder: (BuildContext outerContext, StateSetter setState) {
+                  final List<int> items = widget.items;
+                  return CustomScrollView(
+                    slivers: <Widget>[
+                      SliverReorderableList(
+                        itemBuilder: (BuildContext context, int index) {
+                          return Container(
+                            key: ValueKey<int>(items[index]),
+                            height: 100,
+                            color: items[index].isOdd ? Colors.red : Colors.green,
+                            child: ReorderableDragStartListener(
+                              child: Row(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: <Widget>[
+                                  Text('item ${items[index]}'),
+                                  const Icon(Icons.drag_handle),
+                                ],
+                              ),
+                              index: index,
+                            ),
+                          );
+                        },
+                        itemCount: items.length,
+                        onReorder: (int fromIndex, int toIndex) {
+                          setState(() {
+                            if (toIndex > fromIndex) {
+                              toIndex -= 1;
+                            }
+                            items.insert(toIndex, items.removeAt(fromIndex));
+                          });
+                        },
                       ),
-                    );
-                  },
-                  itemCount: items.length,
-                  onReorder: (int fromIndex, int toIndex) {
-                    setState(() {
-                      if (toIndex > fromIndex) {
-                        toIndex -= 1;
-                      }
-                      items.insert(toIndex, items.removeAt(fromIndex));
-                    });
-                  },
-                ),
-              ],
-            );
-          }
+                    ],
+                  );
+                }
+            ),
+          ),
         ),
       ),
     );
