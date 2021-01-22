21265577ccd6443fd34be743d47e22c54eed539e
Ferhat <ferhat@gmail.com>
2021-01-22 14:22:17 -0800

    [web] Fix shadows for arbitrary paths on PhysicalShape (flutter/engine#23830)

--- engine/src/flutter/lib/web_ui/test/golden_tests/engine/compositing_golden_test.dart
+++ engine/src/flutter/lib/web_ui/test/golden_tests/engine/compositing_golden_test.dart
@@ -111,6 +111,183 @@ void testMain() async {
         region: region);
   });
 
+  test('pushPhysicalShape with path and elevation', () async {
+    Path cutCornersButton = Path()
+      ..moveTo(15, 10)
+      ..lineTo(60, 10)
+      ..lineTo(60, 60)
+      ..lineTo(15, 60)
+      ..lineTo(10, 55)
+      ..lineTo(10, 15);
+
+    final SurfaceSceneBuilder builder = SurfaceSceneBuilder();
+    builder.pushPhysicalShape(
+      path: cutCornersButton,
+      clipBehavior: Clip.hardEdge,
+      color: const Color(0xFFA0FFFF),
+      elevation: 2,
+    );
+    _drawTestPicture(builder);
+    builder.pop();
+
+    builder.pushOffset(70, 0);
+    builder.pushPhysicalShape(
+      path: cutCornersButton,
+      clipBehavior: Clip.hardEdge,
+      color: const Color(0xFFA0FFFF),
+      elevation: 8,
+    );
+    _drawTestPicture(builder);
+    builder.pop();
+    builder.pop();
+
+    builder.pushOffset(140, 0);
+    builder.pushPhysicalShape(
+      path: Path()..addOval(Rect.fromLTRB(10, 10, 60, 60)),
+      clipBehavior: Clip.hardEdge,
+      color: const Color(0xFFA0FFFF),
+      elevation: 4,
+    );
+    _drawTestPicture(builder);
+    builder.pop();
+    builder.pop();
+
+    builder.pushOffset(210, 0);
+    builder.pushPhysicalShape(
+      path: Path()..addRRect(RRect.fromRectAndRadius(
+          Rect.fromLTRB(10, 10, 60, 60), Radius.circular(10.0))),
+      clipBehavior: Clip.hardEdge,
+      color: const Color(0xFFA0FFFF),
+      elevation: 4,
+    );
+    _drawTestPicture(builder);
+    builder.pop();
+    builder.pop();
+
+    html.document.body.append(builder.build().webOnlyRootElement);
+
+    await matchGoldenFile('compositing_physical_shape_path.png',
+        region: region);
+  });
+
+  test('pushPhysicalShape should update across frames', () async {
+    Path cutCornersButton = Path()
+      ..moveTo(15, 10)
+      ..lineTo(60, 10)
+      ..lineTo(60, 60)
+      ..lineTo(15, 60)
+      ..lineTo(10, 55)
+      ..lineTo(10, 15);
+
+    /// Start with shape that has elevation and red color.
+    final SurfaceSceneBuilder builder = SurfaceSceneBuilder();
+    EngineLayer oldShapeLayer = builder.pushPhysicalShape(
+      path: cutCornersButton,
+      clipBehavior: Clip.hardEdge,
+      color: const Color(0xFFFF0000),
+      elevation: 2,
+    );
+    _drawTestPicture(builder);
+    builder.pop();
+
+    html.Element viewElement = builder.build().webOnlyRootElement;
+    html.document.body.append(viewElement);
+    await matchGoldenFile('compositing_physical_update_1.png',
+        region: region);
+    viewElement.remove();
+
+    /// Update color to green.
+    final SurfaceSceneBuilder builder2 = SurfaceSceneBuilder();
+    EngineLayer oldShapeLayer2 = builder2.pushPhysicalShape(
+      path: cutCornersButton,
+      clipBehavior: Clip.hardEdge,
+      color: const Color(0xFF00FF00),
+      elevation: 2,
+      oldLayer: oldShapeLayer,
+    );
+    _drawTestPicture(builder2);
+    builder2.pop();
+
+    html.Element viewElement2 = builder2.build().webOnlyRootElement;
+    html.document.body.append(viewElement2);
+    await matchGoldenFile('compositing_physical_update_2.png',
+        region: region);
+    viewElement2.remove();
+
+    /// Update elevation.
+    final SurfaceSceneBuilder builder3 = SurfaceSceneBuilder();
+    EngineLayer oldShapeLayer3 = builder3.pushPhysicalShape(
+      path: cutCornersButton,
+      clipBehavior: Clip.hardEdge,
+      color: const Color(0xFF00FF00),
+      elevation: 6,
+      oldLayer: oldShapeLayer2,
+    );
+    _drawTestPicture(builder3);
+    builder3.pop();
+
+    html.Element viewElement3 = builder3.build().webOnlyRootElement;
+    html.document.body.append(viewElement3);
+    await matchGoldenFile('compositing_physical_update_3.png',
+        region: region, maxDiffRatePercent: 0.8);
+    viewElement3.remove();
+
+    /// Update shape from arbitrary path to rect.
+    final SurfaceSceneBuilder builder4 = SurfaceSceneBuilder();
+    EngineLayer oldShapeLayer4 = builder4.pushPhysicalShape(
+      path: Path()..addOval(Rect.fromLTRB(10, 10, 60, 60)),
+      clipBehavior: Clip.hardEdge,
+      color: const Color(0xFF00FF00),
+      elevation: 6,
+      oldLayer: oldShapeLayer3,
+    );
+    _drawTestPicture(builder4);
+    builder4.pop();
+
+    html.Element viewElement4 = builder4.build().webOnlyRootElement;
+    html.document.body.append(viewElement4);
+    await matchGoldenFile('compositing_physical_update_4.png',
+        region: region);
+    viewElement4.remove();
+
+    /// Update shape back to arbitrary path.
+    final SurfaceSceneBuilder builder5 = SurfaceSceneBuilder();
+    EngineLayer oldShapeLayer5 = builder5.pushPhysicalShape(
+      path: cutCornersButton,
+      clipBehavior: Clip.hardEdge,
+      color: const Color(0xFF00FF00),
+      elevation: 6,
+      oldLayer: oldShapeLayer4,
+    );
+    _drawTestPicture(builder5);
+    builder5.pop();
+
+    html.Element viewElement5 = builder5.build().webOnlyRootElement;
+    html.document.body.append(viewElement5);
+    await matchGoldenFile('compositing_physical_update_3.png',
+         region: region, maxDiffRatePercent: 0.4);
+    viewElement5.remove();
+
+    /// Update shadow color.
+    final SurfaceSceneBuilder builder6 = SurfaceSceneBuilder();
+    builder6.pushPhysicalShape(
+      path: cutCornersButton,
+      clipBehavior: Clip.hardEdge,
+      color: const Color(0xFF00FF00),
+      shadowColor: const Color(0xFFFF0000),
+      elevation: 6,
+      oldLayer: oldShapeLayer5,
+    );
+    _drawTestPicture(builder6);
+    builder6.pop();
+
+    html.Element viewElement6 = builder6.build().webOnlyRootElement;
+    html.document.body.append(viewElement6);
+    await matchGoldenFile('compositing_physical_update_5.png',
+        region: region);
+    viewElement6.remove();
+  });
+
   test('pushImageFilter', () async {
     final SurfaceSceneBuilder builder = SurfaceSceneBuilder();
     builder.pushImageFilter(
