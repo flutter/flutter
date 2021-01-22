ce537a2b517f34e9760aad96a15c019a6791e2ad
gaaclarke <30870216+gaaclarke@users.noreply.github.com>
2021-01-22 11:49:37 -0800

    reland of https://github.com/flutter/engine/pull/23634 (flutter/engine#23865)

--- engine/src/flutter/shell/platform/darwin/ios/framework/Source/FlutterEngineTest_mrc.mm
+++ engine/src/flutter/shell/platform/darwin/ios/framework/Source/FlutterEngineTest_mrc.mm
@@ -33,10 +33,8 @@ FLUTTER_ASSERT_NOT_ARC
   std::shared_ptr<flutter::IOSContext> engine_context = [engine iosPlatformView]->GetIosContext();
   std::shared_ptr<flutter::IOSContext> spawn_context = [spawn iosPlatformView]->GetIosContext();
   XCTAssertEqual(engine_context, spawn_context);
-  // If this assert fails it means we may be using the software or OpenGL
-  // renderer when we were expecting Metal.  For software rendering, this is
-  // expected to be nullptr.  For OpenGL, implementing this is an outstanding
-  // change see https://github.com/flutter/flutter/issues/73744.
+  // If this assert fails it means we may be using the software.  For software rendering, this is
+  // expected to be nullptr.
   XCTAssertTrue(engine_context->GetMainContext() != nullptr);
   XCTAssertEqual(engine_context->GetMainContext(), spawn_context->GetMainContext());
   [engine release];
