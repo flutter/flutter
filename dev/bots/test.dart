35000147e6275d56b24c899a847afd00d56f130a
Jenn Magder <magder@google.com>
2021-01-25 12:47:01 -0800

    Re-add tool test general per-test timeout (#74531)

--- dev/bots/test.dart
+++ dev/bots/test.dart
@@ -301,6 +301,8 @@ Future<void> _runToolTests() async {
         forceSingleCore: subshard != 'general',
         testPaths: <String>[path.join(kTest, '$subshard$kDotShard', suffix)],
         enableFlutterToolAsserts: subshard != 'general',
+        // Detect unit test time regressions (poor time delay handling, etc).
+        perTestTimeout: (subshard == 'general') ? const Duration(seconds: 2) : null,
       );
     },
   );
