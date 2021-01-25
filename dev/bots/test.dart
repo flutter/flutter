353add83e7b5e0910d9497d234c2ffbac115e3a8
Christopher Fujino <christopherfujino@gmail.com>
2021-01-25 14:04:04 -0800

    Re-land codesign test improvement (#73997)

--- dev/bots/test.dart
+++ dev/bots/test.dart
@@ -662,7 +662,7 @@ Future<void> _runFrameworkTests() async {
     await _pubRunTest(path.join(flutterRoot, 'dev', 'bots'));
     await _pubRunTest(path.join(flutterRoot, 'dev', 'devicelab'));
     await _pubRunTest(path.join(flutterRoot, 'dev', 'snippets'));
-    await _pubRunTest(path.join(flutterRoot, 'dev', 'tools'));
+    await _pubRunTest(path.join(flutterRoot, 'dev', 'tools'), forceSingleCore: true);
     await _runFlutterTest(path.join(flutterRoot, 'dev', 'integration_tests', 'android_semantics_testing'));
     await _runFlutterTest(path.join(flutterRoot, 'dev', 'manual_tests'));
     await _runFlutterTest(path.join(flutterRoot, 'dev', 'tools', 'vitool'));
