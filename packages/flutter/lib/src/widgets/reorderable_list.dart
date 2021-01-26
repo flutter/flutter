efe8a397a800bf7550e7a37ae8bd8aeaf29a88a6
Darren Austin <darrenaustin@google.com>
2021-01-25 18:54:10 -0800

    Added some tests and comments from Shi-Hao for the ReorderableLists. (#74689)

--- packages/flutter/lib/src/widgets/reorderable_list.dart
+++ packages/flutter/lib/src/widgets/reorderable_list.dart
@@ -136,6 +136,9 @@ class ReorderableList extends StatefulWidget {
   final IndexedWidgetBuilder itemBuilder;
 
   /// The number of items in the list.
+  ///
+  /// It must be a non-negative integer. When zero, nothing is displayed and
+  /// the widget occupies no space.
   final int itemCount;
 
   /// A callback used by the list to report that a list item has been dragged
