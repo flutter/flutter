d2df8e54668dbcb849768d2a0ea887266fb4842a
Jonah Williams <jonahwilliams@google.com>
2021-01-27 09:25:56 -0800

    Update cache.dart

--- packages/flutter_tools/lib/src/cache.dart
+++ packages/flutter_tools/lib/src/cache.dart
@@ -1800,7 +1800,6 @@ class ArtifactUpdater {
       _ensureExists(location);
 
       try {
-        print(tempFile.path);
         extractor(tempFile, location);
       } on Exception catch (err) {
         retries -= 1;
