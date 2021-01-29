6a9bb06c7e423fb56fee8d769305c4d39c688a9b
Sam Rawlins <srawlins@google.com>
2021-01-29 10:46:53 -0800

    Remove "unnecessary" imports in cupertino tests
    In each library where an import is removed, the library uses some elements
    provided by the import, BUT there is another import which provides all of the
    same elements, and at least one more which the library uses.
    
    In this change, we remove the imports which can be simply removed in favor of
    the other already present imports.
    
    See https://github.com/dart-lang/sdk/issues/44569 for more information.

--- packages/flutter/test/cupertino/text_selection_toolbar_test.dart
+++ packages/flutter/test/cupertino/text_selection_toolbar_test.dart
@@ -6,7 +6,6 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/rendering.dart';
-import 'package:flutter/widgets.dart';
 
 import '../widgets/editable_text_utils.dart' show textOffsetToPosition;
 
