73b5e232c58c03293f9d13f69bf04e9512ac23c5
Michael Goderbauer <goderbauer@google.com>
2021-02-01 15:01:48 -0800

    ++

--- packages/flutter_driver/test/src/real_tests/test_organisation_test.dart
+++ packages/flutter_driver/test/src/real_tests/test_organisation_test.dart
@@ -12,30 +12,30 @@ void main() {
   // `test/src/web_test` directory, see https://github.com/flutter/flutter/blob/master/dev/bots/test.dart.
   // This test ensures that we do not accidentally add tests in the the `test`
   // or `test/src` directory, which would not run on CI.
-  test('web_extension should register a function', () {
+  test('test files exist only in directories where CI expects them', () {
     final String flutterDriverPath = p.dirname(Platform.script.path);
     expect(p.basename(flutterDriverPath), 'flutter_driver');
     final String flutterDriverTestPath = p.join(flutterDriverPath, 'test');
     final Directory flutterDriverTestDir = Directory(flutterDriverTestPath);
     expect(flutterDriverTestDir.existsSync(), isTrue);
-    final List<String> filesInTestDir = flutterDriverTestDir.listSync().map((FileSystemEntity e) => p.basename(e.path)).toList();
+    final List<String> filesInTestDir = flutterDriverTestDir.listSync()
+        .map((FileSystemEntity e) => p.basename(e.path))
+        .where((String s) => p.extension(s) == '.dart')
+        .toList();
 
     // There are no test files in the `test` directory.
-    expect(filesInTestDir, unorderedEquals(<String>[
-      'common.dart',
-      'src',
-    ]));
+    expect(filesInTestDir, <String>['common.dart']);
 
     // There are no test files in the src directory.
     final String flutterDriverTestSrcPath = p.join(flutterDriverTestPath, 'src');
     final Directory flutterDriverTestSrcDir = Directory(flutterDriverTestSrcPath);
     expect(flutterDriverTestSrcDir.existsSync(), isTrue);
-    final List<String> filesInTestSrcDir = flutterDriverTestSrcDir.listSync().map((FileSystemEntity e) => p.basename(e.path)).toList();
+    final List<String> filesInTestSrcDir = flutterDriverTestSrcDir.listSync()
+        .map((FileSystemEntity e) => p.basename(e.path))
+        .where((String s) => p.extension(s) == '.dart')
+        .toList();
 
     // There are no test files in the `test/src` directory.
-    expect(filesInTestSrcDir, unorderedEquals(<String>[
-      'real_tests',
-      'web_tests',
-    ]));
+    expect(filesInTestSrcDir, unorderedEquals(<String>[]));
   });
 }
