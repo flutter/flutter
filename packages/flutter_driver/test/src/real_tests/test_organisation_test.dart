69feea43272b61b6bb4aa27a50cbd5682638c832
Michael Goderbauer <goderbauer@google.com>
2021-02-02 11:35:26 -0800

    fix test

--- packages/flutter_driver/test/src/real_tests/test_organisation_test.dart
+++ packages/flutter_driver/test/src/real_tests/test_organisation_test.dart
@@ -4,8 +4,8 @@
 
 import 'dart:io';
 
+import 'package:flutter_driver/flutter_driver.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:path/path.dart' as p;
 
 void main() {
   // On CI we only execute the tests in the `test/src/real_test` and
@@ -13,26 +13,26 @@ void main() {
   // This test ensures that we do not accidentally add tests in the the `test`
   // or `test/src` directory, which would not run on CI.
   test('test files exist only in directories where CI expects them', () {
-    final String flutterDriverPath = p.dirname(Platform.script.path);
-    expect(p.basename(flutterDriverPath), 'flutter_driver');
-    final String flutterDriverTestPath = p.join(flutterDriverPath, 'test');
-    final Directory flutterDriverTestDir = Directory(flutterDriverTestPath);
-    expect(flutterDriverTestDir.existsSync(), isTrue);
+    final String flutterDriverPath = fs.path.dirname(Platform.script.path);
+    expect(fs.path.basename(flutterDriverPath), 'flutter_driver');
+    final String flutterDriverTestPath = fs.path.join(flutterDriverPath, 'test');
+    final Directory flutterDriverTestDir = fs.directory(flutterDriverTestPath);
+    expect(flutterDriverTestDir.existsSync(), isTrue, reason: '${flutterDriverTestDir.path} does not exist.');
     final List<String> filesInTestDir = flutterDriverTestDir.listSync()
-        .map((FileSystemEntity e) => p.basename(e.path))
-        .where((String s) => p.extension(s) == '.dart')
+        .map((FileSystemEntity e) => fs.path.basename(e.path))
+        .where((String s) => fs.path.extension(s) == '.dart')
         .toList();
 
     // There are no test files in the `test` directory.
     expect(filesInTestDir, <String>['common.dart']);
 
     // There are no test files in the src directory.
-    final String flutterDriverTestSrcPath = p.join(flutterDriverTestPath, 'src');
-    final Directory flutterDriverTestSrcDir = Directory(flutterDriverTestSrcPath);
-    expect(flutterDriverTestSrcDir.existsSync(), isTrue);
+    final String flutterDriverTestSrcPath = fs.path.join(flutterDriverTestPath, 'src');
+    final Directory flutterDriverTestSrcDir = fs.directory(flutterDriverTestSrcPath);
+    expect(flutterDriverTestSrcDir.existsSync(), isTrue, reason: '${flutterDriverTestSrcDir.path} does not exist.');
     final List<String> filesInTestSrcDir = flutterDriverTestSrcDir.listSync()
-        .map((FileSystemEntity e) => p.basename(e.path))
-        .where((String s) => p.extension(s) == '.dart')
+        .map((FileSystemEntity e) => fs.path.basename(e.path))
+        .where((String s) => fs.path.extension(s) == '.dart')
         .toList();
 
     // There are no test files in the `test/src` directory.
