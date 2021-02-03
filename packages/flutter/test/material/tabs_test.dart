8fff5e72beaba90e2ff77b18391d30df70338a4f
Sam Rawlins <srawlins@google.com>
2021-02-03 13:26:58 -0800

    Remove "unnecessary" imports in test/material
    In each library where an import is removed, the library uses some elements
    provided by the import, BUT there is another import which provides all of the
    same elements, and at least one more which the library uses.
    
    In this change, we remove the imports which can be simply removed in favor of
    the other already present imports.
    
    See https://github.com/dart-lang/sdk/issues/44569 for more information.

--- packages/flutter/test/material/tabs_test.dart
+++ packages/flutter/test/material/tabs_test.dart
@@ -5,11 +5,9 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/physics.dart';
 
-import '../flutter_test_alternative.dart' show Fake;
 import '../rendering/mock_canvas.dart';
 import '../rendering/recording_canvas.dart';
 import '../widgets/semantics_tester.dart';
