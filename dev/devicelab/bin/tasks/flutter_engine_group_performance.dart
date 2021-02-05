669518e1b61c22e5cf3736a65c57ba5429aeba4c
Aaron Clarke <aaclarke@google.com>
2021-02-05 11:48:24 -0800

    responded to jenns feedback

--- dev/devicelab/bin/tasks/flutter_engine_group_performance.dart
+++ dev/devicelab/bin/tasks/flutter_engine_group_performance.dart
@@ -4,26 +4,15 @@
 
 import 'dart:io';
 
+import 'package:flutter_devicelab/framework/adb.dart';
 import 'package:flutter_devicelab/framework/framework.dart';
 import 'package:flutter_devicelab/framework/task_result.dart';
+import 'package:flutter_devicelab/tasks/perf_tests.dart' show ListStatistics;
+import 'package:path/path.dart' as path;
 
-const String _pathSeperator = '/';
 const String _bundleName = 'dev.flutter.multipleflutters';
 const String _activityName = 'MainActivity';
-
-List<String> _splitAbsPath(String path) {
-  if (path[0] != '/') {
-    throw Exception('expected absolute path');
-  }
-  assert(path[0] == '/');
-  final List<String> result = path.split(_pathSeperator);
-  result.removeWhere((String element) => element.isEmpty);
-  return result;
-}
-
-String _joinAbsPath(List<String> components) {
-  return _pathSeperator + components.join(_pathSeperator);
-}
+const int _numberOfIterations = 10;
 
 List<String> _addComponent(List<String> components, String value) {
   final List<String> result = <String>[];
@@ -53,22 +42,34 @@ Future<void> _run(String command, List<String> args, String cwd) async {
   await stderrStream;
 }
 
+Future<void> _withApkInstall(
+    String apkPath, String bundleName, Function(AndroidDevice) body) async {
+  final DeviceDiscovery devices = DeviceDiscovery();
+  final AndroidDevice device = await devices.workingDevice as AndroidDevice;
+  await device.unlock();
+  await device.adb(<String>['install', '-r', apkPath]);
+  try {
+    await body(device);
+  } finally {
+    await device.adb(<String>['uninstall', bundleName]);
+  }
+}
+
 Future<TaskResult> _doTest() async {
   try {
-    final List<String> scriptPathComponents =
-        _splitAbsPath(Platform.script.path);
+    final List<String> scriptPathComponents = path.split(Platform.script.path);
     final List<String> multipleFluttersPath =
         scriptPathComponents.sublist(0, scriptPathComponents.length - 4);
     multipleFluttersPath.addAll(<String>['benchmarks', 'multiple_flutters']);
     final String modulePath =
-        _joinAbsPath(_addComponent(multipleFluttersPath, 'module'));
+        path.joinAll(_addComponent(multipleFluttersPath, 'module'));
     final String androidPath =
-        _joinAbsPath(_addComponent(multipleFluttersPath, 'android'));
+        path.joinAll(_addComponent(multipleFluttersPath, 'android'));
 
     await _run('flutter', <String>['pub', 'get'], modulePath);
     await _run('./gradlew', <String>['assembleRelease'], androidPath);
 
-    final String apkPath = _joinAbsPath(_addComponents(
+    final String apkPath = path.joinAll(_addComponents(
         multipleFluttersPath, <String>[
       'android',
       'app',
@@ -78,33 +79,36 @@ Future<TaskResult> _doTest() async {
       'release',
       'app-release.apk'
     ]));
-    await _run(
-        'adb', <String>['install', '-r', apkPath], Directory.current.path);
-    await _run(
-        'adb',
-        <String>[
+
+    TaskResult result;
+    await _withApkInstall(apkPath, _bundleName, (AndroidDevice device) async {
+      final List<int> totalMemorySamples = <int>[];
+      for (int i = 0; i < _numberOfIterations; ++i) {
+        await device.adb(<String>[
           'shell',
           'am',
           'start',
           '-n',
           '$_bundleName/$_bundleName.$_activityName'
-        ],
-        Directory.current.path);
-    await Future<void>.delayed(const Duration(seconds: 10));
-    final ProcessResult meminfoResult = await Process.run(
-        'adb', <String>['shell', 'dumpsys', 'meminfo', _bundleName]);
-    if (meminfoResult.exitCode != 0) {
-      throw Exception('meminfo returned exit code: ${meminfoResult.exitCode}');
-    }
-    assert(meminfoResult.exitCode == 0);
-    final RegExp regex = RegExp(r'TOTAL:\s+(\d+)');
-    final int totalMemory =
-        int.parse(regex.firstMatch(meminfoResult.stdout.toString()).group(1));
-    return TaskResult.success(<String, dynamic>{
-      'totalMemory': totalMemory,
-    }, benchmarkScoreKeys: <String>[
-      'totalMemory'
-    ]);
+        ]);
+        await Future<void>.delayed(const Duration(seconds: 10));
+        final Map<String, dynamic> memoryStats =
+            await device.getMemoryStats(_bundleName);
+        final int totalMemory = memoryStats['total_kb'] as int;
+        totalMemorySamples.add(totalMemory);
+        await device.stop(_bundleName);
+      }
+      final ListStatistics totalMemoryStatistics =
+          ListStatistics(totalMemorySamples);
+
+      final Map<String, dynamic> results = <String, dynamic>{
+        ...totalMemoryStatistics.asMap('totalMemory')
+      };
+      result = TaskResult.success(results,
+          benchmarkScoreKeys: results.keys.toList());
+    });
+
+    return result ?? TaskResult.failure('no results found');
   } catch (ex) {
     return TaskResult.failure(ex.toString());
   }
