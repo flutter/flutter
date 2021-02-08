6f93239399d7c84e2cc15b486a576bf87eaa7dfd
Tomasz Gucio <tgucio@gmail.com>
2021-02-08 09:50:40 +0100

    Add RenderEditable keys listener back in attach()

--- packages/flutter/lib/src/rendering/editable.dart
+++ packages/flutter/lib/src/rendering/editable.dart
@@ -1639,6 +1639,8 @@ class RenderEditable extends RenderBox with RelayoutWhenSystemFontsChangeMixin {
     _offset.addListener(markNeedsPaint);
     _showHideCursor();
     _showCursor.addListener(_showHideCursor);
+    if (_listenerAttached)
+      RawKeyboard.instance.addListener(_handleKeyEvent);
   }
 
   @override
