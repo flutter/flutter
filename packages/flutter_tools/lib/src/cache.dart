40e7bd20c045a2d861b46ac16e00e16a102c45fb
Jonah Williams <campfish91@gmail.com>
2021-02-09 11:00:37 -0800

    [flutter_tools] catch error 32 in cache

--- packages/flutter_tools/lib/src/cache.dart
+++ packages/flutter_tools/lib/src/cache.dart
@@ -1796,7 +1796,18 @@ class ArtifactUpdater {
       final Directory destination = location.childDirectory(
         tempFile.fileSystem.path.basenameWithoutExtension(tempFile.path)
       );
-      ErrorHandlingFileSystem.deleteIfExists(destination, recursive: true);
+      try {
+        ErrorHandlingFileSystem.deleteIfExists(destination, recursive: true);
+      } on FileSystemException catch (error) {
+        if (_platform.isWindows && error.osError.errorCode == 32) {
+          throwToolExit(
+            'Failed to download $url because the local file/directory is in use '
+            'by another process. Try closing any running IDEs or editors and trying '
+            'again'
+          );
+        }
+        rethrow;
+      }
       _ensureExists(location);
 
       try {
