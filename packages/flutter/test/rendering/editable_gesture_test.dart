dea1c46a3c046766089a5d2e85bf6e169411b82e
Sam Rawlins <srawlins@google.com>
2021-02-10 17:06:03 -0800

    Remove "unnecessary" imports. (#75637)

--- packages/flutter/test/rendering/editable_gesture_test.dart
+++ packages/flutter/test/rendering/editable_gesture_test.dart
@@ -5,10 +5,7 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import '../flutter_test_alternative.dart' show Fake;
 
 void main() {
   setUp(() => _GestureBindingSpy());
