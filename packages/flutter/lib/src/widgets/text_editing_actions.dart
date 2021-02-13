f813a86b3016149be2ee49a82d4a3da0c6b6b716
Justin McCandless <jmccandless@google.com>
2021-02-12 16:02:05 -0800

    Remove example that is not necessary without Intents being invoked from visual widgets. Will be readded in another PR

--- packages/flutter/lib/src/widgets/text_editing_actions.dart
+++ packages/flutter/lib/src/widgets/text_editing_actions.dart
@@ -11,46 +11,12 @@ import 'framework.dart';
 import 'text_editing_action.dart';
 import 'text_editing_intents.dart';
 
-// TODO(justinmc): Update these examples now that the naming has changed.
 /// An [Actions] widget that handles the default text editing behavior for
 /// Flutter on the current platform.
 ///
 /// This default behavior can be overridden by placing an [Actions] widget lower
 /// in the widget tree than this. See [TextEditingShortcuts] for an example of
 /// remapping keyboard keys to an existing text editing [Intent].
-///
-/// {@tool snippet}
-///
-/// This example shows how to use an additional [Actions] widget to override
-/// the left arrow key [Intent] and make it move the cursor to the right
-/// instead.
-///
-/// ```dart
-/// final TextEditingController controller = TextEditingController(
-///   text: "Try using the keyboard's arrow keys and notice that left moves right.",
-/// );
-///
-/// @override
-/// Widget build(BuildContext context) {
-///   return Scaffold(
-///     body: Center(
-///       child: Actions(
-///         actions: <Type, Action<Intent>>{
-///           ArrowLeftTextIntent: TextEditingAction<ArrowLeftTextIntent>(
-///             onInvoke: (ArrowLeftTextIntent intent, EditableTextState editableTextState) {
-///               editableTextState.renderEditable.moveSelectionRight(SelectionChangedCause.keyboard);
-///             },
-///           ),
-///         },
-///         child: TextField(
-///           controller: controller,
-///         ),
-///       ),
-///     ),
-///   );
-/// }
-/// ```
-/// {@end-tool}
 ///
 /// See also:
 ///
