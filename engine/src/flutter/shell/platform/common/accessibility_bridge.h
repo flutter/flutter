03946537c209a71962bde948e1ea582bcddae2b9
Chris Bracken <chris@bracken.jp>
2021-02-15 13:27:11 -0800

    Adjust header guards for updated paths (flutter/engine#24424)
    In a46f817 these files were moved from shell/platform/common/cpp to
    shell/platform/common.

--- engine/src/flutter/shell/platform/common/accessibility_bridge.h
+++ engine/src/flutter/shell/platform/common/accessibility_bridge.h
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef FLUTTER_SHELL_PLATFORM_COMMON_CPP_ACCESSIBILITY_BRIDGE_H_
-#define FLUTTER_SHELL_PLATFORM_COMMON_CPP_ACCESSIBILITY_BRIDGE_H_
+#ifndef FLUTTER_SHELL_PLATFORM_COMMON_ACCESSIBILITY_BRIDGE_H_
+#define FLUTTER_SHELL_PLATFORM_COMMON_ACCESSIBILITY_BRIDGE_H_
 
 #include <unordered_map>
 
@@ -280,4 +280,4 @@ class AccessibilityBridge
 
 }  // namespace flutter
 
-#endif  // FLUTTER_SHELL_PLATFORM_COMMON_CPP_ACCESSIBILITY_BRIDGE_H_
+#endif  // FLUTTER_SHELL_PLATFORM_COMMON_ACCESSIBILITY_BRIDGE_H_
