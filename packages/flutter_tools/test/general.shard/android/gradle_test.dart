1e616e1f37ed8b4850dc0531fc3947e8c70bdbba
Sam Rawlins <srawlins@google.com>
2021-02-16 14:59:14 -0800

    Revert one import removal

--- packages/flutter_tools/test/general.shard/android/gradle_test.dart
+++ packages/flutter_tools/test/general.shard/android/gradle_test.dart
@@ -24,6 +24,7 @@ import 'package:mockito/mockito.dart';
 import 'package:process/process.dart';
 
 import '../../src/common.dart';
+import '../../src/context.dart';
 
 void main() {
   Cache.flutterRoot = getFlutterRoot();
