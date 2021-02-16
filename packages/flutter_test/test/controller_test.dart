ddd848b42557693df679c18a750a5e2ec5a00996
Sam Rawlins <srawlins@google.com>
2021-02-16 14:50:59 -0800

    Remove "unnecessary" imports in flutter_test
    In each library where an import is removed, the library uses some elements
    provided by the import, BUT there is another import which provides all of the
    same elements, and at least one more which the library uses.
    
    In this change, we remove the imports which can be simply removed in favor of
    the other already present imports.
    
    See https://github.com/dart-lang/sdk/issues/44569 for more information.

--- packages/flutter_test/test/controller_test.dart
+++ packages/flutter_test/test/controller_test.dart
@@ -6,7 +6,6 @@ import 'dart:ui';
 
 import 'package:flutter/semantics.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter/gestures.dart';
 
