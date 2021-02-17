5194bdb69a2aed898668122db053d8a191bbdaaa
Jason Simmons <jsimmons@google.com>
2021-02-16 17:36:50 -0800

    Use a null default skip value in testWidgets so it can inherit the group's skip flag
    Fixes https://github.com/flutter/flutter/issues/76135

--- packages/flutter_test/lib/src/widget_tester.dart
+++ packages/flutter_test/lib/src/widget_tester.dart
@@ -109,7 +109,7 @@ typedef WidgetTesterCallback = Future<void> Function(WidgetTester widgetTester);
 void testWidgets(
   String description,
   WidgetTesterCallback callback, {
-  bool skip = false,
+  bool? skip,
   test_package.Timeout? timeout,
   Duration? initialTimeout,
   bool semanticsEnabled = true,
