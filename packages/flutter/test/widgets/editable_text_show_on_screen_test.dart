d76801401410c992d1d6c2f43cd134b854544026
LongCat is Looong <31859944+LongCatIsLooong@users.noreply.github.com>
2021-02-23 11:55:19 -0800

    analyzer

--- packages/flutter/test/widgets/editable_text_show_on_screen_test.dart
+++ packages/flutter/test/widgets/editable_text_show_on_screen_test.dart
@@ -3,6 +3,7 @@
 // found in the LICENSE file.
 
 import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/src/foundation/constants.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter/material.dart';
