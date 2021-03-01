3ed5a7a89fff20c8a23ebc6d537c54e02d012e4a
Swayam221 <68892315+Swayam221@users.noreply.github.com>
2021-03-01 09:41:53 +0530

    changed 'constrains' to 'constraints' in ChildLayoutHelper.dryLayoutChild

--- packages/flutter/lib/src/rendering/layout_helper.dart
+++ packages/flutter/lib/src/rendering/layout_helper.dart
@@ -11,7 +11,8 @@ import 'box.dart';
 /// [BoxConstraints].
 ///
 /// The methods of [ChildLayoutHelper] adhere to this signature.
-typedef ChildLayouter = Size Function(RenderBox child, BoxConstraints constraints);
+typedef ChildLayouter = Size Function(
+    RenderBox child, BoxConstraints constraints);
 
 /// A collection of static functions to layout a [RenderBox] child with the
 /// given set of [BoxConstraints].
