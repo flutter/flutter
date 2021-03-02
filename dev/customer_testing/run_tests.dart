decdeb5c99ba8f7bb480bee5d6b70d39fc97ca4b
Casey Hillers <chillers@google.com>
2021-03-01 17:09:36 -0800

    Move test run logic to runner.dart

--- dev/customer_testing/run_tests.dart
+++ dev/customer_testing/run_tests.dart
@@ -9,7 +9,6 @@ import 'package:file/local.dart';
 import 'package:glob/glob.dart';
 import 'package:path/path.dart' as path;
 
-import 'customer_test.dart';
 import 'runner.dart';
 
 Future<void> main(List<String> arguments) async {
@@ -107,9 +106,6 @@ Future<bool> run(List<String> arguments) async {
     return help;
   }
 
-  if (verbose)
-    print('Starting run_tests.dart...');
-
   if (files.length < shardIndex)
     print('Warning: There are more shards than tests. Some shards will not run any tests.');
 
@@ -117,116 +113,13 @@ Future<bool> run(List<String> arguments) async {
     print('Error: There are more shard indexes than shards.');
     return help;
   }
-
-  // Best attempt at evenly splitting tests among the shards
-  final List<File> shardedFiles = <File>[];
-  for (int i = shardIndex; i < files.length; i += numberShards) {
-    shardedFiles.add(files[i]);
-  }
-
-  int testCount = 0;
-  int failures = 0;
-
-  if (verbose) {
-    final String s = files.length == 1 ? '' : 's';
-    final String ss = shardedFiles.length == 1 ? '' : 's';
-    print('${files.length} file$s specified. ${shardedFiles.length} test$ss in shard #$shardIndex.');
-    print('');
-  }
-
-  if (verbose) {
-    print('Tests in this shard:');
-    for (final File file in shardedFiles)
-      print(file.path);
-  }
-  print('');
-
-  for (final File file in shardedFiles) {
-    if (verbose)
-      print('Processing ${file.path}...');
-    TestFile instructions;
-    try {
-      instructions = TestFile(file);
-    } on FormatException catch (error) {
-      print('ERROR: ${error.message}');
-      print('');
-      failures += 1;
-      continue;
-    } on FileSystemException catch (error) {
-      print('ERROR: ${error.message}');
-      print('  ${file.path}');
-      print('');
-      failures += 1;
-      continue;
-    }
-
-    final Directory checkout = Directory.systemTemp.createTempSync('flutter_customer_testing.${path.basenameWithoutExtension(file.path)}.');
-    if (verbose)
-      print('Created temporary directory: ${checkout.path}');
-    try {
-      bool success;
-      bool showContacts = false;
-      for (final String fetchCommand in instructions.fetch) {
-        success = await shell(fetchCommand, checkout, verbose: verbose, silentFailure: skipOnFetchFailure);
-        if (!success) {
-          if (skipOnFetchFailure) {
-            if (verbose) {
-              print('Skipping (fetch failed).');
-            } else {
-              print('Skipping ${file.path} (fetch failed).');
-            }
-          } else {
-            print('ERROR: Failed to fetch repository.');
-            failures += 1;
-            showContacts = true;
-          }
-          break;
-        }
-      }
-      assert(success != null);
-      if (success) {
-        if (verbose)
-          print('Running tests...');
-        final Directory tests = Directory(path.join(checkout.path, 'tests'));
-        // TODO(ianh): Once we have a way to update source code, run that command in each directory of instructions.update
-        for (int iteration = 0; iteration < repeat; iteration += 1) {
-          if (verbose && repeat > 1)
-            print('Round ${iteration + 1} of $repeat.');
-          for (final String testCommand in instructions.tests) {
-            testCount += 1;
-            success = await shell(testCommand, tests, verbose: verbose);
-            if (!success) {
-              print('ERROR: One or more tests from ${path.basenameWithoutExtension(file.path)} failed.');
-              failures += 1;
-              showContacts = true;
-              break;
-            }
-          }
-        }
-        if (verbose && success)
-          print('Tests finished.');
-      }
-      if (showContacts) {
-        final String s = instructions.contacts.length == 1 ? '' : 's';
-        print('Contact$s: ${instructions.contacts.join(", ")}');
-      }
-    } finally {
-      if (verbose)
-        print('Deleting temporary directory...');
-      try {
-        checkout.deleteSync(recursive: true);
-      } on FileSystemException {
-        print('Failed to delete "${checkout.path}".');
-      }
-    }
-    if (verbose)
-      print('');
-  }
-  if (failures > 0) {
-    final String s = failures == 1 ? '' : 's';
-    print('$failures failure$s.');
-    return false;
-  }
-  print('$testCount tests all passed!');
-  return true;
+  
+  return runTests(
+    repeat: repeat,
+    skipOnFetchFailure: skipOnFetchFailure,
+    verbose: verbose,
+    numberShards: numberShards,
+    shardIndex: shardIndex,
+    files: files,
+  );
 }
