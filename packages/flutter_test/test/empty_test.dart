cec48409e3f0935ab84b45dd6984debd8b44661a
Francesco Iapicca <iapicca@gmail.com>
2021-03-04 16:14:09 +0200

    added empty test

---
+++ packages/flutter_test/test/empty_test.dart
@@ -0,0 +1,4 @@
+// !! DO NOT DELETE
+/// this test purposely empty to verify that an empty test file 
+/// won't cause `flutter test` and `flutter pub test` to fail
+/// see issue #77253 for more details
