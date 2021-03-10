d3a466aee8ba65022c9b1273bf01b7ac5b4422d5
Tong Mu <dkwingsmt@gmail.com>
2021-03-09 17:54:50 -0800

    Solve loop import

--- packages/flutter/lib/src/painting/text_span.dart
+++ packages/flutter/lib/src/painting/text_span.dart
@@ -6,7 +6,6 @@ import 'dart:ui' as ui show ParagraphBuilder;
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/gestures.dart';
-import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
 
 import 'basic_types.dart';
