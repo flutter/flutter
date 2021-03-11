4fb0ce6e63f2d5e68994ffc3ac4d1fad3eab3d36
Johannes von Bargen <johannes.vonbargen@njiuko.com>
2021-03-12 00:30:18 +0100

    Update packages/flutter/lib/src/widgets/drag_target.dart
    Co-authored-by: Kate Lovett <katelovett@google.com>

--- packages/flutter/lib/src/widgets/drag_target.dart
+++ packages/flutter/lib/src/widgets/drag_target.dart
@@ -290,7 +290,7 @@ class Draggable<T extends Object> extends StatefulWidget {
   /// Defaults to [DragAnchor.child].
   @Deprecated(
     'Use dragAnchorStrategy instead. '
-    'This feature was deprecated after 2.1.0-10.0.pre.'
+    'This feature was deprecated after v2.1.0-10.0.pre.'
   )
   final DragAnchor dragAnchor;
 
