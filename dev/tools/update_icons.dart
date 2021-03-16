d01994f24b5fdcbc61e79ad5a16057dc5c794931
Pierre-Louis <plg@google.com>
2021-03-16 15:07:16 +0100

    Make identifierRewrites public

--- dev/tools/update_icons.dart
+++ dev/tools/update_icons.dart
@@ -36,7 +36,7 @@ const Map<String, List<String>> _platformAdaptiveIdentifiers = <String, List<Str
 };
 
 // Rewrite certain Flutter IDs (reserved keywords, numbers) using prefix matching.
-const Map<String, String> _identifierRewrites = <String, String>{
+const Map<String, String> identifierRewrites = <String, String>{
   '1x': 'one_x',
   '360': 'threesixty',
   '2d': 'twod',
@@ -348,9 +348,9 @@ class _Icon {
     }
 
     flutterId = id;
-    for (final MapEntry<String, String> rewritePair in _identifierRewrites.entries) {
+    for (final MapEntry<String, String> rewritePair in identifierRewrites.entries) {
       if (id.startsWith(rewritePair.key)) {
-        flutterId = id.replaceFirst(rewritePair.key, _identifierRewrites[rewritePair.key]);
+        flutterId = id.replaceFirst(rewritePair.key, identifierRewrites[rewritePair.key]);
       }
     }
 
