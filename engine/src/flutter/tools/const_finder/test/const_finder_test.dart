2fdcceeb1f1a15b30c1594b24630c3bbee9a79d0
Alexander Aprelev <aam@google.com>
2021-03-16 15:34:18 -0700

    Roll to dart 91d7c69ed7ea (flutter/engine#25022)
    * Roll Dart SDK from b7e02a713eab to 91d7c69ed7ea
    
    Update const_finder_test expectations to accommodate daae24b4880.
    
    Changes since last roll:
    ```
    91d7c69ed7e Version 2.13.0-143.0.dev
    88c24ef66ac Roll benchmarks-internal 354c978979c5..076df10d9b77
    b250a58d8e8 Implement `int.>>>` for DDC.
    7e2cc73da8b Revert "[infra] Make worktrees work (again?)"
    a531b78369e [infra] Make worktrees work (again?)
    945ac84c16e Version 2.13.0-142.0.dev
    e8f57b66058 [vm/aot] Handle FieldInitializer pointing to a Field's Reference which is reused for a getter
    1cb203d8c9b Remove unused code and files from analysis_server:
    3162957a786 (origin/base) [gardening] Add explicit type to ensure templated function applies.
    7b35c1ecc2f (tag: 2.13.0-141.0.dev) Version 2.13.0-141.0.dev
    829ef927789 [vm] Expand compressed heap to 4GB.
    2472db00311 [vm] Inform the OS that it can reclaim free'd portions of the compressed heap.
    e2baf16ebda Support ddc module format in expression compilation in ddc.
    8e7c8c9d8b2 Roll icu dependency to 81d656878ec611cb0b42d52c82e9dae93920d9ba.
    52fccfb392b Version 2.13.0-140.0.dev
    6b575c72ec8 Prepare to publish analyzer 1.2.0 and _fe_analyzer_shared 18.0.0.
    c0b800ceb51 Issue 45318. Check for dynamic bounded and Function bounded.
    daae24b4880 [vm/aot] Retain fields used in constants
    369c36e67c2 Issue 45299. Fix yield type check for function expressions.
    c9e10c931cb [vm, compiler] Predicate boolean loads.
    87bfe9ea276 (tag: 2.13.0-139.0.dev) Version 2.13.0-139.0.dev
    cf3dcc4256c [analyzer] Prevent MOVE_FILE using absolute URIs when moving files out of lib
    c9061339678 [cfe] Multi-call const function tests to existing behavior.
    16beb348133 [vm/aot] Remove special cases of dynamic function lookups from precompiler
    55522f9e90b Fix a bug in completion when a setter does not have a corresponding getter
    d42de4ed062 [vm] Crash on impossible GDT null errors
    69ec09825a3 [vm] On Android set abort message before aborting
    b72e7b81ebd (tag: 2.13.0-138.0.dev) Version 2.13.0-138.0.dev
    d6b79dab6c6 [cfe] Correct argument count in extension member errors
    b5da327ed9a [parser] Parse and skip metadata according to spec
    ```
    
    * Update const_finder_test expectations
    
    * Update license
    
    * Update expectations

--- engine/src/flutter/tools/const_finder/test/const_finder_test.dart
+++ engine/src/flutter/tools/const_finder/test/const_finder_test.dart
@@ -125,6 +125,7 @@ void _checkNonConsts() {
     <String, dynamic>{
       'constantInstances': <dynamic>[
         <String, dynamic>{'stringValue': '1', 'intValue': 1, 'targetValue': null},
+        <String, dynamic>{'stringValue': '4', 'intValue': 4, 'targetValue': null},
         <String, dynamic>{'stringValue': '6', 'intValue': 6, 'targetValue': null},
         <String, dynamic>{'stringValue': '8', 'intValue': 8, 'targetValue': null},
         <String, dynamic>{'stringValue': '10', 'intValue': 10, 'targetValue': null},
@@ -140,12 +141,12 @@ void _checkNonConsts() {
         <String, dynamic>{
           'file': 'file://$fixtures/lib/consts_and_non.dart',
           'line': 17,
-          'column': 41,
+          'column': 26,
         },
         <String, dynamic>{
           'file': 'file://$fixtures/lib/consts_and_non.dart',
           'line': 17,
-          'column': 26,
+          'column': 41,
         },
         <String, dynamic>{
           'file': 'file://$fixtures/lib/consts_and_non.dart',
