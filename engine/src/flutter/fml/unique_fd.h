d337319b6c98bb626dedbce645565a9ec64d75bf
James Clarke <james@clarkezone.net>
2021-03-17 11:52:44 -0700

    Windows: Fix fml::GetFullHandlePath for WINUWP target (flutter/engine#24422)
    Co-authored-by: Chris Bracken <chris@bracken.jp>

--- engine/src/flutter/fml/unique_fd.h
+++ engine/src/flutter/fml/unique_fd.h
@@ -10,6 +10,9 @@
 
 #if OS_WIN
 #include <windows.h>
+#include <map>
+#include <mutex>
+#include <optional>
 #else  // OS_WIN
 #include <dirent.h>
 #include <unistd.h>
@@ -22,10 +25,46 @@ namespace internal {
 
 namespace os_win {
 
+struct DirCacheEntry {
+  std::wstring filename;
+  FILE_ID_128 id;
+};
+
+// The order of these is important.  Must come before UniqueFDTraits struct
+// else linker error.  Embedding in struct also causes linker error.
+
 struct UniqueFDTraits {
+  static std::mutex file_map_mutex;
+  static std::map<HANDLE, DirCacheEntry> file_map;
+
   static HANDLE InvalidValue() { return INVALID_HANDLE_VALUE; }
   static bool IsValid(HANDLE value) { return value != InvalidValue(); }
-  static void Free(HANDLE fd);
+  static void Free_Handle(HANDLE fd);
+
+  static void Free(HANDLE fd) {
+    RemoveCacheEntry(fd);
+
+    UniqueFDTraits::Free_Handle(fd);
+  }
+
+  static void RemoveCacheEntry(HANDLE fd) {
+    const std::lock_guard<std::mutex> lock(file_map_mutex);
+
+    file_map.erase(fd);
+  }
+
+  static void StoreCacheEntry(HANDLE fd, DirCacheEntry state) {
+    const std::lock_guard<std::mutex> lock(file_map_mutex);
+    file_map[fd] = state;
+  }
+
+  static std::optional<DirCacheEntry> GetCacheEntry(HANDLE fd) {
+    const std::lock_guard<std::mutex> lock(file_map_mutex);
+    auto found = file_map.find(fd);
+    return found == file_map.end()
+               ? std::nullopt
+               : std::optional<DirCacheEntry>{found->second};
+  }
 };
 
 }  // namespace os_win
