17e51505e7aff722ee978c3cc0574d8f0cdf92e4
chunhtai <47866232+chunhtai@users.noreply.github.com>
2021-03-18 10:22:35 -0700

    fix numeric dependencies (flutter/engine#25053)
    * fix numeric dependencies
    
    * fix
    
    * fix format

--- engine/src/flutter/third_party/accessibility/base/numerics/safe_math_shared_impl.h
+++ engine/src/flutter/third_party/accessibility/base/numerics/safe_math_shared_impl.h
@@ -15,8 +15,8 @@
 #include <limits>
 #include <type_traits>
 
+#include "ax_build/build_config.h"
 #include "base/numerics/safe_conversions.h"
-#include "build/build_config.h"
 
 #if defined(OS_ASMJS)
 // Optimized safe math instructions are incompatible with asmjs.
