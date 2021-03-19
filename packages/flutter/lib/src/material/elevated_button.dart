b24d1699b00bc80394cee51815538a04e7a30a5c
Sam Rawlins <srawlins@google.com>
2021-03-19 16:41:42 -0700

    Remove "unnecessary" imports.
    In each library where an import is removed, the library uses some elements
    provided by the import, BUT there is another import which provides all of the
    same elements, and at least one more which the library uses.
    
    In this change, we remove the imports which can be simply removed in favor of
    the other already present imports.
    
    See https://github.com/dart-lang/sdk/issues/44569 for more information.

--- packages/flutter/lib/src/material/elevated_button.dart
+++ packages/flutter/lib/src/material/elevated_button.dart
@@ -6,7 +6,6 @@ import 'dart:math' as math;
 import 'dart:ui' show lerpDouble;
 
 import 'package:flutter/foundation.dart';
-import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
 
 import 'button_style.dart';
--- packages/flutter/lib/src/material/paginated_data_table.dart
+++ packages/flutter/lib/src/material/paginated_data_table.dart
@@ -5,7 +5,6 @@
 import 'dart:math' as math;
 
 import 'package:flutter/gestures.dart' show DragStartBehavior;
-import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
 
 import 'button_bar.dart';
