69d6f3b1575d9a55283ee6add0f7ec0bae72a213
Jenn Magder <magder@google.com>
2021-03-31 13:53:34 -0700

    Fix device_port_forwarder file name typo

--- packages/flutter_tools/test/general.shard/mdns_discovery_test.dart
+++ packages/flutter_tools/test/general.shard/mdns_discovery_test.dart
@@ -7,7 +7,7 @@
 import 'package:flutter_tools/src/base/io.dart';
 import 'package:flutter_tools/src/base/logger.dart';
 import 'package:flutter_tools/src/build_info.dart';
-import 'package:flutter_tools/src/device_port_forwader.dart';
+import 'package:flutter_tools/src/device_port_forwarder.dart';
 import 'package:flutter_tools/src/ios/devices.dart';
 import 'package:flutter_tools/src/mdns_discovery.dart';
 import 'package:flutter_tools/src/project.dart';
