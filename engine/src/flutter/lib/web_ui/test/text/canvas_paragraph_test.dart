c86bcd09b19c70477083950bbbe91202e310e86b
Mouad Debbar <mouad.debbar@gmail.com>
2021-04-02 13:35:13 -0700

    [web] Fix framework material text field tests (flutter/engine#25367)

--- engine/src/flutter/lib/web_ui/test/text/canvas_paragraph_test.dart
+++ engine/src/flutter/lib/web_ui/test/text/canvas_paragraph_test.dart
@@ -320,7 +320,7 @@ void testMain() async {
       })
         ..layout(constrain(double.infinity));
 
-      // Above the line.
+      // Above the line, at the beginning.
       expect(
         paragraph.getPositionForOffset(ui.Offset(0, -5)),
         pos(0, ui.TextAffinity.downstream),
@@ -335,9 +335,9 @@ void testMain() async {
         paragraph.getPositionForOffset(ui.Offset(0, 5)),
         pos(0, ui.TextAffinity.downstream),
       );
-      // Below the line.
+      // Below the line, at the end.
       expect(
-        paragraph.getPositionForOffset(ui.Offset(0, 12)),
+        paragraph.getPositionForOffset(ui.Offset(130, 12)),
         pos(13, ui.TextAffinity.upstream),
       );
       // At the end of the line.
@@ -348,6 +348,16 @@ void testMain() async {
       // On the left half of "p" in "ipsum".
       expect(
         paragraph.getPositionForOffset(ui.Offset(74, 5)),
+        pos(7, ui.TextAffinity.downstream),
+      );
+      // On the left half of "p" in "ipsum" (above the line).
+      expect(
+        paragraph.getPositionForOffset(ui.Offset(74, -5)),
+        pos(7, ui.TextAffinity.downstream),
+      );
+      // On the left half of "p" in "ipsum" (below the line).
+      expect(
+        paragraph.getPositionForOffset(ui.Offset(74, 15)),
         pos(7, ui.TextAffinity.downstream),
       );
       // On the right half of "p" in "ipsum".
@@ -378,7 +388,7 @@ void testMain() async {
       //   "ipsum "
       //   "dolor sit"
 
-      // Above the first line.
+      // Above the first line, at the beginning.
       expect(
         paragraph.getPositionForOffset(ui.Offset(0, -5)),
         pos(0, ui.TextAffinity.downstream),
@@ -396,6 +406,11 @@ void testMain() async {
       // At the end of the first line.
       expect(
         paragraph.getPositionForOffset(ui.Offset(60, 5)),
+        pos(6, ui.TextAffinity.upstream),
+      );
+      // At the end of the first line (above the line).
+      expect(
+        paragraph.getPositionForOffset(ui.Offset(60, -5)),
         pos(6, ui.TextAffinity.upstream),
       );
       // After the end of the first line to the right.
@@ -430,9 +445,9 @@ void testMain() async {
         pos(12, ui.TextAffinity.upstream),
       );
 
-      // Below the third line "dolor sit".
+      // Below the third line "dolor sit", at the end.
       expect(
-        paragraph.getPositionForOffset(ui.Offset(0, 40)),
+        paragraph.getPositionForOffset(ui.Offset(90, 40)),
         pos(21, ui.TextAffinity.upstream),
       );
       // At the end of the third line.
@@ -473,7 +488,7 @@ void testMain() async {
       //   "ipsum "
       //   "dolor sit"
 
-      // Above the first line.
+      // Above the first line, at the beginning.
       expect(
         paragraph.getPositionForOffset(ui.Offset(0, -5)),
         pos(0, ui.TextAffinity.downstream),
@@ -520,9 +535,9 @@ void testMain() async {
         pos(12, ui.TextAffinity.upstream),
       );
 
-      // Below the third line "dolor sit".
+      // Below the third line "dolor sit", at the end.
       expect(
-        paragraph.getPositionForOffset(ui.Offset(0, 40)),
+        paragraph.getPositionForOffset(ui.Offset(90, 40)),
         pos(21, ui.TextAffinity.upstream),
       );
       // At the end of the third line.
