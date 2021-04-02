fb68d0747766285c2681342351653929e6c91b93
Jenn Magder <magder@google.com>
2021-04-01 20:24:04 -0700

    Migrate android_studio to null safety (#79504)

--- packages/flutter_tools/lib/src/android/android_sdk.dart
+++ packages/flutter_tools/lib/src/android/android_sdk.dart
@@ -13,7 +13,7 @@ import '../base/platform.dart';
 import '../base/process.dart';
 import '../base/version.dart';
 import '../convert.dart';
-import '../globals.dart' as globals;
+import '../globals_null_migrated.dart' as globals;
 import 'android_studio.dart';
 
 // ANDROID_HOME is deprecated.
