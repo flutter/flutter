0c42556f2245e908523af51c831bf4c83cc36942
Kate Lovett <katelovett@google.com>
2021-04-08 11:59:41 -0500

    Revert "improve the scrollbar behavior when viewport size changed (#76102)"
    This reverts commit 7d74794a4ea1b932ec56d900aeb0b8e138ebe287.

--- packages/flutter/test/widgets/scrollbar_test.dart
+++ packages/flutter/test/widgets/scrollbar_test.dart
@@ -1152,35 +1152,4 @@ void main() {
         ),
     );
   });
-
-  testWidgets('The bar can show or hide when the viewport size change', (WidgetTester tester) async {
-    final ScrollController scrollController = ScrollController();
-    Widget buildFrame(double height) {
-      return Directionality(
-        textDirection: TextDirection.ltr,
-        child: MediaQuery(
-          data: const MediaQueryData(),
-          child: RawScrollbar(
-            controller: scrollController,
-            isAlwaysShown: true,
-            child: SingleChildScrollView(
-                controller: scrollController,
-                child: SizedBox(width: double.infinity, height: height)
-            ),
-          ),
-        ),
-      );
-    }
-    await tester.pumpWidget(buildFrame(600.0));
-    await tester.pumpAndSettle();
-    expect(find.byType(RawScrollbar), isNot(paints..rect())); // Not shown.
-
-    await tester.pumpWidget(buildFrame(600.1));
-    await tester.pumpAndSettle();
-    expect(find.byType(RawScrollbar), paints..rect()..rect()); // Show the bar.
-
-    await tester.pumpWidget(buildFrame(600.0));
-    await tester.pumpAndSettle();
-    expect(find.byType(RawScrollbar), isNot(paints..rect())); // Hide the bar.
-  });
 }
