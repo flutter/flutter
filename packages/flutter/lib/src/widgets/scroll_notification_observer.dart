f5bf0354a70d89b95a699e0cb0eba4e1d03793cd
Hans Muller <hansmuller@chromium.org>
2021-04-09 17:51:02 -0700

    update

--- packages/flutter/lib/src/widgets/scroll_notification_observer.dart
+++ packages/flutter/lib/src/widgets/scroll_notification_observer.dart
@@ -1,3 +1,7 @@
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import 'dart:collection';
 
 import 'package:flutter/foundation.dart';
@@ -6,7 +10,12 @@ import 'framework.dart';
 import 'notification_listener.dart';
 import 'scroll_notification.dart';
 
-
+/// A [ScrollNotification] listener for [ScrollNotificationObserver].
+///
+/// [ScrollNotificationObserver] is similar to
+/// [NotificationListener]. It supports a listener list instead of
+/// just a single listener and its listeners run unconditionally, they
+/// do not require a gating boolean return value.
 typedef ScrollNotificationCallback = void Function(ScrollNotification notification);
 
 class _ScrollNotificationObserverScope extends InheritedWidget {
@@ -28,6 +37,13 @@ class _ListenerEntry extends LinkedListEntry<_ListenerEntry> {
   final ScrollNotificationCallback listener;
 }
 
+/// The listener list state for a [ScrollNotificationObserver] returned by
+/// [ScrollNotificationObserver.of].
+///
+/// [ScrollNotificationObserver] is similar to
+/// [NotificationListener]. It supports a listener list instead of
+/// just a single listener and its listeners run unconditionally, they
+/// do not require a gating boolean return value.
 class ScrollNotificationObserverState extends State<ScrollNotificationObserver> {
   LinkedList<_ListenerEntry>? _listeners = LinkedList<_ListenerEntry>();
 
@@ -44,11 +60,14 @@ class ScrollNotificationObserverState extends State<ScrollNotificationObserver> 
     return true;
   }
 
+  /// Add a [ScrollNotificationCallback] that will be called each time a descendant
+  /// scrolls.
   void addListener(ScrollNotificationCallback listener) {
     assert(_debugAssertNotDisposed());
     _listeners!.add(_ListenerEntry(listener));
   }
 
+  /// Remove the specified [ScrollNotificationCallback].
   void removeListener(ScrollNotificationCallback listener) {
     assert(_debugAssertNotDisposed());
     for (final _ListenerEntry entry in _listeners!) {
@@ -59,7 +78,7 @@ class ScrollNotificationObserverState extends State<ScrollNotificationObserver> 
     }
   }
 
-  void notifyListeners(ScrollNotification notification) {
+  void _notifyListeners(ScrollNotification notification) {
     assert(_debugAssertNotDisposed());
     if (_listeners!.isEmpty)
       return;
@@ -91,7 +110,7 @@ class ScrollNotificationObserverState extends State<ScrollNotificationObserver> 
   Widget build(BuildContext context) {
     return NotificationListener<ScrollNotification>(
       onNotification: (ScrollNotification notification) {
-        notifyListeners(notification);
+        _notifyListeners(notification);
         return false;
       },
       child: _ScrollNotificationObserverScope(
@@ -109,14 +128,43 @@ class ScrollNotificationObserverState extends State<ScrollNotificationObserver> 
   }
 }
 
+/// Notifies its listeners when a descendant scrolls.
+///
+/// To add a listener to a [ScrollNotificationObserver] ancestor:
+/// ```dart
+/// void listener(ScrollNotification notification) {
+///   // Do something, maybe setState()
+/// }
+/// ScrollNotificationObserver.of(context).addListener(listener)
+/// ```
+///
+/// To remove the listener from a [ScrollNotificationObserver] ancestor:
+/// ```dart
+/// ScrollNotificationObserver.of(context).removeListener(listener);
+///```
+///
+/// Stateful widgets that share an ancestor [ScrollNotificationObserver] typically
+/// add a listener in [State.didChangeDependencies] (removing the old one
+/// if necessary) and remove the listener in their [State.dispose] method.
+///
+/// This widget is similar to [NotificationListener]. It supports
+/// a listener list instead of just a single listener and its listeners
+/// run unconditionally, they do not require a gating boolean return value.
 class ScrollNotificationObserver extends StatefulWidget {
+  /// Create a [ScrollNotificationObserver].
+  ///
+  /// The [child] parameter must not be null.
   const ScrollNotificationObserver({
     Key? key,
     required this.child,
   }) : assert(child != null), super(key: key);
 
+  /// The subtree below this widget.
   final Widget child;
 
+  /// The closest instance of this class that encloses the given context.
+  ///
+  /// If there is no enclosing [ScrollNotificationObserver] widget, then null is returned.
   static ScrollNotificationObserverState? of(BuildContext context) {
     return context.dependOnInheritedWidgetOfExactType<_ScrollNotificationObserverScope>()?._scrollNotificationObserverState;
   }
