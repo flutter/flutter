d24469374ba86798df4e253d730a3747179b69aa
Jonah Williams <jonahwilliams@google.com>
2021-04-12 16:54:10 -0700

    [flutter_tools] symlink win32 plugins as UWP plugins (#80242)

--- packages/flutter_tools/test/general.shard/windows/plugins_test.dart
+++ packages/flutter_tools/test/general.shard/windows/plugins_test.dart
@@ -14,8 +14,23 @@ import 'package:flutter_tools/src/plugins.dart';
 import 'package:flutter_tools/src/project.dart';
 
 import '../../src/common.dart';
+import '../../src/fakes.dart';
 
 const TemplateRenderer renderer = MustacheTemplateRenderer();
+
+const String kPluginDependencies = r'''
+{
+  "info":"This is a generated file; do not edit or check into version control.",
+  "plugins":{
+    "windows":[
+      {
+        "name":"example","path":"C:\\\\example\\\\",
+        "dependencies":[]
+      }
+    ]
+  }
+}
+''';
 
 void main() {
 
@@ -64,10 +79,31 @@ void main() {
       contains('#include <test/foo.h>'),
     );
   });
+
+  testWithoutContext('Symlink injection treats UWP as Win32', () {
+    final FileSystem fileSystem = MemoryFileSystem.test();
+    setUpProject(fileSystem);
+    final FlutterProject flutterProject = FlutterProject.fromDirectoryTest(fileSystem.currentDirectory);
+    flutterProject.flutterPluginsDependenciesFile.writeAsStringSync(kPluginDependencies);
+
+    createPluginSymlinks(
+      flutterProject,
+      featureFlagsOverride: TestFeatureFlags(isWindowsUwpEnabled: true),
+    );
+
+    expect(flutterProject.windowsUwp.pluginSymlinkDirectory, exists);
+
+    final Link link = flutterProject.windowsUwp.pluginSymlinkDirectory.listSync().single as Link;
+
+    expect(link.path, '/winuwp/flutter/ephemeral/.plugin_symlinks/example');
+    expect(link.targetSync(), r'C:\\example\\');
+  });
 }
 
 void setUpProject(FileSystem fileSystem) {
   fileSystem.file('pubspec.yaml').createSync();
+  fileSystem.file('winuwp/CMakeLists.txt')
+    .createSync(recursive: true);
   fileSystem.file('winuwp/project_version')
     ..createSync(recursive: true)
     ..writeAsStringSync('0');
