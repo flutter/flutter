6688dd4d1230c5c3d8f67d71f782e19a64dd5b3a
Chase Latta <chaselatta@google.com>
2021-04-13 13:42:17 -0700

    [fuchsia] create component context in main (#25290)

--- shell/platform/fuchsia/flutter/main.cc
+++ shell/platform/fuchsia/flutter/main.cc
@@ -3,6 +3,7 @@
 // found in the LICENSE file.
 
 #include <lib/async-loop/cpp/loop.h>
+#include <lib/sys/inspect/cpp/component.h>
 #include <lib/trace-provider/provider.h>
 #include <lib/trace/event.h>
 
@@ -15,6 +16,16 @@
 
 int main(int argc, char const* argv[]) {
   std::unique_ptr<async::Loop> loop(flutter_runner::MakeObservableLoop(true));
+
+  // Create our component context which is served later.
+  auto context = sys::ComponentContext::Create();
+  sys::ComponentInspector inspector(context.get());
+
+  inspect::Node& root = inspector.inspector()->GetRoot();
+
+  // We inject the 'vm' node into the dart vm so that it can add any inspect
+  // data that it needs to the inspect tree.
+  dart::SetDartVmNode(std::make_unique<inspect::Node>(root.CreateChild("vm")));
 
   std::unique_ptr<trace::TraceProviderWithFdio> provider;
   {
@@ -29,7 +40,10 @@ int main(int argc, char const* argv[]) {
 
   FML_DLOG(INFO) << "Flutter application services initialized.";
 
-  flutter_runner::Runner runner(loop.get(), dart::ComponentContext());
+  flutter_runner::Runner runner(loop.get(), context.get());
+
+  // Wait to serve until we have finished all of our setup.
+  context->outgoing()->ServeFromStartupInfo();
 
   loop->Run();
   FML_DLOG(INFO) << "Flutter application services terminated.";
