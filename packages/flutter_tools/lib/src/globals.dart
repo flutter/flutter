ce19e3cbecfe3d7da7fe010fb3206855270a90b7
Jenn Magder <magder@google.com>
2021-04-13 15:30:21 -0700

    Migrate xcode.dart and xcode_validator.dart to null safety

--- packages/flutter_tools/lib/src/globals.dart
+++ packages/flutter_tools/lib/src/globals.dart
@@ -12,12 +12,10 @@ import 'device.dart';
 import 'doctor.dart';
 import 'fuchsia/fuchsia_sdk.dart';
 import 'globals_null_migrated.dart' as globals;
-import 'ios/ios_workflow.dart';
 import 'ios/simulators.dart';
 import 'macos/cocoapods.dart';
 import 'macos/cocoapods_validator.dart';
 import 'macos/xcdevice.dart';
-import 'macos/xcode.dart';
 import 'project.dart';
 import 'reporting/crash_reporting.dart';
 import 'runner/local_engine.dart';
@@ -44,8 +42,6 @@ LocalEngineLocator get localEngineLocator => context.get<LocalEngineLocator>();
 CocoaPods get cocoaPods => context.get<CocoaPods>();
 FuchsiaArtifacts get fuchsiaArtifacts => context.get<FuchsiaArtifacts>();
 IOSSimulatorUtils get iosSimulatorUtils => context.get<IOSSimulatorUtils>();
-IOSWorkflow get iosWorkflow => context.get<IOSWorkflow>();
-Xcode get xcode => context.get<Xcode>();
 
 XCDevice get xcdevice => context.get<XCDevice>();
 
