b37f229906ee955d68f46758b4b0cdf560a20e8c
Hans Muller <hansmuller@chromium.org>
2021-04-14 14:35:57 -0700

    fixed a typo

--- packages/flutter/lib/src/material/app_bar.dart
+++ packages/flutter/lib/src/material/app_bar.dart
@@ -214,7 +214,7 @@ class AppBar extends StatefulWidget implements PreferredSizeWidget {
        super(key: key);
 
   /// Used by [Scaffold] to compute its [AppBar]'s overall height. The returned value is
-  /// the same `preferredSize.height` unless [AppBar.toolBarHeight] was null and
+  /// the same `preferredSize.height` unless [AppBar.toolbarHeight] was null and
   /// `AppBarTheme.of(context).toolbarHeight` is non-null. In that case the
   /// return value is the sum of the theme's toolbar height and the height of
   /// the app bar's [AppBar.bottom] widget.
