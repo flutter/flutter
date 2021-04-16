8dac6a473d4b16a91fb0b83f621621af26f2fa70
Tong Mu <dkwingsmt@gmail.com>
2021-04-15 17:24:17 -0700

    Lock mode bit

--- dev/tools/gen_keycodes/lib/gtk_code_gen.dart
+++ dev/tools/gen_keycodes/lib/gtk_code_gen.dart
@@ -2,11 +2,13 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'dart:io';
+
 import 'package:path/path.dart' as path;
 
 import 'base_code_gen.dart';
-import 'physical_key_data.dart';
 import 'logical_key_data.dart';
+import 'physical_key_data.dart';
 import 'utils.dart';
 
 
@@ -19,24 +21,74 @@ class GtkCodeGenerator extends PlatformCodeGenerator {
 
   /// This generates the map of XKB scan codes to Flutter physical keys.
   String get xkbScanCodeMap {
-    final StringBuffer xkbScanCodeMap = StringBuffer();
+    final StringBuffer result = StringBuffer();
     for (final PhysicalKeyEntry entry in keyData.data) {
       if (entry.xKbScanCode != null) {
-        xkbScanCodeMap.writeln('  insert_record(table, ${toHex(entry.xKbScanCode)}, ${toHex(entry.usbHidCode)});  // ${entry.constantName}');
+        result.writeln('  insert_record(table, ${toHex(entry.xKbScanCode)}, ${toHex(entry.usbHidCode)});  // ${entry.constantName}');
       }
     }
-    return xkbScanCodeMap.toString().trimRight();
+    return result.toString().trimRight();
   }
 
   /// This generates the map of GTK keyval codes to Flutter logical keys.
   String get gtkKeyvalCodeMap {
-    final StringBuffer gtkKeyvalCodeMap = StringBuffer();
+    final StringBuffer result = StringBuffer();
     for (final LogicalKeyEntry entry in logicalData.data.values) {
       zipStrict(entry.gtkValues, entry.gtkNames, (int value, String name) {
-        gtkKeyvalCodeMap.writeln('  insert_record(table, ${toHex(value)}, ${toHex(entry.value, digits: 9)});  // ${name}');
+        result.writeln('  insert_record(table, ${toHex(value)}, ${toHex(entry.value, digits: 9)});  // $name');
       });
     }
-    return gtkKeyvalCodeMap.toString().trimRight();
+    return result.toString().trimRight();
+  }
+
+  static String constructMapFromModToKeys(
+      Map<String, List<String>> source,
+      PhysicalKeyData physicalData,
+      LogicalKeyData logicalData,
+      String debugFunctionName,
+  ) {
+    final StringBuffer result = StringBuffer();
+    source.forEach((String modifierBitName, List<String> keyNames) {
+      final String firstLogicalName = keyNames[0];
+      final List<String> physicalNames = keyNames.sublist(1);
+      final int length = physicalNames.length;
+      final LogicalKeyEntry firstLogical = logicalData.data[firstLogicalName];
+      if (firstLogical == null) {
+        print('Unrecognized first logical key $firstLogicalName specified for $debugFunctionName.');
+        return;
+      }
+      result.writeln('''
+
+  data = g_new(FlKeyEmbedderCheckedKey, 1);
+  g_hash_table_insert(table, GUINT_TO_POINTER(GDK_${modifierBitName}_MASK), data);
+  data->length = $length;
+  data->first_logical_key = ${toHex(firstLogical.value, digits: 9)};  // ${firstLogical.constantName}
+  physical_keys = g_new(uint64_t, $length);
+  data->physical_keys = physical_keys;''');
+      for (final String physicalName in physicalNames) {
+        final PhysicalKeyEntry entry = physicalData.getEntryByName(physicalName);
+        if (entry == null) {
+          print('Unrecognized physical key $physicalName specified for $debugFunctionName.');
+          return;
+        }
+        result.writeln('  *(physical_keys++) = ${toHex(entry.usbHidCode)};  // ${entry.name}');
+      }
+    });
+    return result.toString().trimRight();
+  }
+
+  String get gtkModifierBitMap {
+    final Map<String, List<String>> source = parseMapOfListOfString(File(
+      path.join(flutterRoot.path, 'dev', 'tools', 'gen_keycodes', 'data', 'gtk_modifier_bit_mapping.json')
+    ).readAsStringSync());
+    return constructMapFromModToKeys(source, keyData, logicalData, 'gtkModifierBitMap');
+  }
+
+  String get gtkModeBitMap {
+    final Map<String, List<String>> source = parseMapOfListOfString(File(
+      path.join(flutterRoot.path, 'dev', 'tools', 'gen_keycodes', 'data', 'gtk_mode_bit_mapping.json')
+    ).readAsStringSync());
+    return constructMapFromModToKeys(source, keyData, logicalData, 'gtkModeBitMap');
   }
 
   @override
@@ -50,6 +102,8 @@ class GtkCodeGenerator extends PlatformCodeGenerator {
     return <String, String>{
       'XKB_SCAN_CODE_MAP': xkbScanCodeMap,
       'GTK_KEYVAL_CODE_MAP': gtkKeyvalCodeMap,
+      'GTK_MODIFIER_BIT_MAP': gtkModifierBitMap,
+      'GTK_MODE_BIT_MAP': gtkModeBitMap,
     };
   }
 }
