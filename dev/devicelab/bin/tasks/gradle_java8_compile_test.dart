85731fbaebc827d770efa8799ffefb5db28a3576
Emmanuel Garcia <egarciad@google.com>
2021-04-16 17:21:43 -0700

    A bit more readable

--- dev/devicelab/bin/tasks/gradle_java8_compile_test.dart
+++ dev/devicelab/bin/tasks/gradle_java8_compile_test.dart
@@ -14,20 +14,23 @@ Future<void> main() async {
   await task(() async {
     try {
       await runPluginProjectTest((FlutterPluginProject pluginProject) async {
+
         section('check main plugin file exists');
-        final File pluginMainKotlinFile = File(path.join(
-          pluginProject.rootPath,
-          'android',
-          'src',
-          'main',
-          'kotlin',
+        final File pluginMainKotlinFile = File(
           path.join(
-            'com',
-            'example',
-            'aaa',
-            'AaaPlugin.kt',
-          )
-        ));
+            pluginProject.rootPath,
+            'android',
+            'src',
+            'main',
+            'kotlin',
+            path.join(
+              'com',
+              'example',
+              'aaa',
+              'AaaPlugin.kt',
+            ),
+          ),
+        );
 
         if (!pluginMainKotlinFile.existsSync()) {
           throw TaskResult.failure('Expected ${pluginMainKotlinFile.path} to exist, but it doesn\'t');
