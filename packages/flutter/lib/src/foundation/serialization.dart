160f2952d23d20011ed893f57ce7e914c0a80a31
Aaron Clarke <aaclarke@google.com>
2021-04-19 14:03:21 -0700

    updated docstring

--- packages/flutter/lib/src/foundation/serialization.dart
+++ packages/flutter/lib/src/foundation/serialization.dart
@@ -9,7 +9,7 @@ import 'package:typed_data/typed_buffers.dart' show Uint8Buffer;
 /// Write-only buffer for incrementally building a [ByteData] instance.
 ///
 /// A WriteBuffer instance can be used only once. Attempts to reuse will result
-/// in [NoSuchMethodError]s being thrown.
+/// in [StateError]s being thrown.
 ///
 /// The byte order used is [Endian.host] throughout.
 class WriteBuffer {
