4950a88c91139878e8d4cf05125bb11413c76171
Hannes Winkler <hanneswinkler2000@web.de>
2021-04-19 19:13:39 +0200

    add bundle builder test that makes sure all the right Environment defines are passed to the build system

--- packages/flutter_tools/test/general.shard/bundle_builder_test.dart
+++ packages/flutter_tools/test/general.shard/bundle_builder_test.dart
@@ -8,6 +8,8 @@ import 'package:file/memory.dart';
 import 'package:flutter_tools/src/base/file_system.dart';
 import 'package:flutter_tools/src/build_info.dart';
 import 'package:flutter_tools/src/build_system/build_system.dart';
+import 'package:flutter_tools/src/build_system/targets/common.dart';
+import 'package:flutter_tools/src/build_system/targets/icon_tree_shaker.dart';
 import 'package:flutter_tools/src/bundle.dart';
 import 'package:flutter_tools/src/project.dart';
 import 'package:flutter_tools/src/globals_null_migrated.dart' as globals;
@@ -15,6 +17,7 @@ import 'package:flutter_tools/src/globals_null_migrated.dart' as globals;
 import '../src/common.dart';
 import '../src/context.dart';
 import '../src/test_build_system.dart';
+import '../src/testbed.dart';
 
 // Tests for BundleBuilder.
 void main() {
@@ -62,5 +65,55 @@ void main() {
     FileSystem: () => MemoryFileSystem.test(),
     ProcessManager: () => FakeProcessManager.any(),
     BuildSystem: () => TestBuildSystem.all(BuildResult(success: false)),
+  });
+
+  Environment env;
+  testUsingContext('Passes correct defines to build system', () async {
+    final FlutterProject project = FlutterProject.fromDirectoryTest(globals.fs.currentDirectory);
+    final String mainPath = globals.fs.path.join('lib', 'main.dart');
+    const String assetDirPath = 'example';
+    const String depfilePath = 'example.d';
+
+    await BundleBuilder().build(
+      platform: TargetPlatform.ios,
+      buildInfo: const BuildInfo(
+        BuildMode.debug,
+        null,
+        trackWidgetCreation: true,
+        extraFrontEndOptions: <String>['test1', 'test2'],
+        extraGenSnapshotOptions: <String>['test3', 'test4'],
+        fileSystemRoots: <String>['test5', 'test6'],
+        fileSystemScheme: 'test7',
+        dartDefines: <String>['test8', 'test9'],
+        treeShakeIcons: true,
+      ),
+      project: project,
+      mainPath: mainPath,
+      assetDirPath: assetDirPath,
+      depfilePath: depfilePath,
+    );
+
+    expect(env, isNotNull);
+    expect(env.defines[kBuildMode], 'debug');
+    expect(env.defines[kTargetPlatform], 'ios');
+    expect(env.defines[kTargetFile], mainPath);
+    expect(env.defines[kTrackWidgetCreation], 'true');
+    expect(env.defines[kExtraFrontEndOptions], 'test1,test2');
+    expect(env.defines[kExtraGenSnapshotOptions], 'test3,test4');
+    expect(env.defines[kFileSystemRoots], 'test5,test6');
+    expect(env.defines[kFileSystemScheme], 'test7');
+    expect(env.defines[kDartDefines], encodeDartDefines(<String>['test8', 'test9']));
+    expect(env.defines[kIconTreeShakerFlag], 'true');
+    expect(env.defines[kDeferredComponents], 'false');
+  }, overrides: <Type, Generator>{
+    FileSystem: () => MemoryFileSystem.test(),
+    ProcessManager: () => FakeProcessManager.any(),
+    BuildSystem: () => TestBuildSystem.all(BuildResult(success: true), (Target target, Environment environment) {
+      env = environment;
+      environment.outputDir.childFile('kernel_blob.bin').createSync(recursive: true);
+      environment.outputDir.childFile('isolate_snapshot_data').createSync();
+      environment.outputDir.childFile('vm_snapshot_data').createSync();
+      environment.outputDir.childFile('LICENSE').createSync(recursive: true);
+    }),
   });
 }
