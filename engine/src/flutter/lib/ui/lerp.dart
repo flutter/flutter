964597352a90912c42e81c68d237e73acd1e09db
Abhishek Ghaskata <abhishekghaskata1999@gmail.com>
2021-04-19 23:54:01 +0530

    Remove if from docs (flutter/engine#25646)

--- engine/src/flutter/lib/ui/lerp.dart
+++ engine/src/flutter/lib/ui/lerp.dart
@@ -9,7 +9,7 @@ part of dart.ui;
 /// Linearly interpolate between two numbers, `a` and `b`, by an extrapolation
 /// factor `t`.
 ///
-/// When `a` and `b` are equal or both NaN, `a` is returned.  Otherwise, if
+/// When `a` and `b` are equal or both NaN, `a` is returned.  Otherwise,
 /// `a`, `b`, and `t` are required to be finite or null, and the result of `a +
 /// (b - a) * t` is returned, where nulls are defaulted to 0.0.
 double? lerpDouble(num? a, num? b, double t) {
