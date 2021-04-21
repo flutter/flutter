b5d521f9c72efc0eea8a3dfb40d0c289e3074991
Hans Muller <hans.muller@gmail.com>
2021-04-20 17:49:02 -0700

    VisualDensity should not reduce ButtonStyleButton horizontal padding (#79680)

--- packages/flutter/test/material/text_button_test.dart
+++ packages/flutter/test/material/text_button_test.dart
@@ -739,7 +739,7 @@ void main() {
     await buildTest(const VisualDensity(horizontal: -3.0, vertical: -3.0));
     await tester.pumpAndSettle();
     childRect = tester.getRect(find.byKey(childKey));
-    expect(box.size, equals(const Size(100, 100)));
+    expect(box.size, equals(const Size(116, 100)));
     expect(childRect, equals(const Rect.fromLTRB(350, 250, 450, 350)));
 
     await buildTest(VisualDensity.standard, useText: true);
@@ -757,7 +757,7 @@ void main() {
     await buildTest(const VisualDensity(horizontal: -3.0, vertical: -3.0), useText: true);
     await tester.pumpAndSettle();
     childRect = tester.getRect(find.byKey(childKey));
-    expect(box.size, equals(const Size(56, 36)));
+    expect(box.size, equals(const Size(72, 36)));
     expect(childRect, equals(const Rect.fromLTRB(372.0, 293.0, 428.0, 307.0)));
   });
 
