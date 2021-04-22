9c0270d960f1653ee9bda835b4590ee78512a941
Phil Quitslund <pq@users.noreply.github.com>
2021-04-22 10:14:02 -0700

    sort directives (#80901)

--- packages/flutter_tools/test/general.shard/bundle_builder_test.dart
+++ packages/flutter_tools/test/general.shard/bundle_builder_test.dart
@@ -11,8 +11,8 @@ import 'package:flutter_tools/src/build_system/build_system.dart';
 import 'package:flutter_tools/src/build_system/targets/common.dart';
 import 'package:flutter_tools/src/build_system/targets/icon_tree_shaker.dart';
 import 'package:flutter_tools/src/bundle.dart';
-import 'package:flutter_tools/src/project.dart';
 import 'package:flutter_tools/src/globals_null_migrated.dart' as globals;
+import 'package:flutter_tools/src/project.dart';
 
 import '../src/common.dart';
 import '../src/context.dart';
