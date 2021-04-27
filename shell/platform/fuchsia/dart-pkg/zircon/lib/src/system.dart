aebaf55231ed2018a739e108ae473f2606c8e835
Jody Sankey <jodysankey@users.noreply.github.com>
2021-04-27 12:24:02 -0700

    [fuchsia] Remove now unsed wrapper for zx_clock_get (#25750)

--- shell/platform/fuchsia/dart-pkg/zircon/lib/src/system.dart
+++ shell/platform/fuchsia/dart-pkg/zircon/lib/src/system.dart
@@ -190,7 +190,6 @@ class System extends NativeFieldWrapperClass2 {
 
   // Time operations.
   static int clockGetMonotonic() native 'System_ClockGetMonotonic';
-  static int clockGet(int clockId) native 'System_ClockGet';
 
   // TODO(edcoyne): Remove this, it is required to safely do an API transition across repos.
   static int reboot() { return -2; /*ZX_ERR_NOT_SUPPORTED*/ }
