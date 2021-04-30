001323a2751fd98f916306b46806737d63347556
Jonah Williams <jonahwilliams@google.com>
2021-04-29 22:49:02 -0700

    [flutter_tools] remove timeout from iOS device startup (#81229)

--- packages/flutter_tools/test/general.shard/ios/ios_device_start_prebuilt_test.dart
+++ packages/flutter_tools/test/general.shard/ios/ios_device_start_prebuilt_test.dart
@@ -178,6 +178,47 @@ void main() {
     expect(await device.stopApp(iosApp), false);
   });
 
+  testWithoutContext('IOSDevice.startApp prints warning message if discovery takes longer than configured timeout', () async {
+    final FileSystem fileSystem = MemoryFileSystem.test();
+    final BufferLogger logger = BufferLogger.test();
+    final FakeProcessManager processManager = FakeProcessManager.list(<FakeCommand>[
+      kAttachDebuggerCommand,
+    ]);
+    final IOSDevice device = setUpIOSDevice(
+      processManager: processManager,
+      fileSystem: fileSystem,
+      logger: logger,
+    );
+    final IOSApp iosApp = PrebuiltIOSApp(
+      projectBundleId: 'app',
+      bundleName: 'Runner',
+      bundleDir: fileSystem.currentDirectory,
+    );
+    final FakeDeviceLogReader deviceLogReader = FakeDeviceLogReader();
+
+    device.portForwarder = const NoOpDevicePortForwarder();
+    device.setLogReader(iosApp, deviceLogReader);
+
+    // Start writing messages to the log reader.
+    Timer.run(() async {
+      await Future<void>.delayed(const Duration(milliseconds: 1));
+      deviceLogReader.addLine('Foo');
+      deviceLogReader.addLine('Observatory listening on http://127.0.0.1:456');
+    });
+
+    final LaunchResult launchResult = await device.startApp(iosApp,
+      prebuiltApplication: true,
+      debuggingOptions: DebuggingOptions.enabled(BuildInfo.debug),
+      platformArgs: <String, dynamic>{},
+      discoveryTimeout: Duration.zero,
+    );
+
+    expect(launchResult.started, true);
+    expect(launchResult.hasObservatory, true);
+    expect(await device.stopApp(iosApp), false);
+    expect(logger.errorText, contains('iOS Observatory not discovered after 30 seconds. This is taking much longer than expected...'));
+  });
+
   testWithoutContext('IOSDevice.startApp succeeds in release mode', () async {
     final FileSystem fileSystem = MemoryFileSystem.test();
     final FakeProcessManager processManager = FakeProcessManager.list(<FakeCommand>[
