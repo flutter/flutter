d97f41caed971d4668ffe56699367ec3978db8f6
Tong Mu <dkwingsmt@users.noreply.github.com>
2021-04-30 12:35:21 -0700

    Revert "MouseRegion enter/exit event can be triggered with button pressed (#81148)" (#81557)
    This reverts commit ace61f01efe3d4e53db1845627028d901e7af3e9.

--- packages/flutter/lib/src/rendering/binding.dart
+++ packages/flutter/lib/src/rendering/binding.dart
@@ -282,13 +282,7 @@ mixin RendererBinding on BindingBase, ServicesBinding, SchedulerBinding, Gesture
         event is PointerAddedEvent ||
         event is PointerRemovedEvent) {
       assert(event.position != null);
-      _mouseTracker!.updateWithEvent(
-        event,
-        // Mouse events(enter and exit) can be triggered with or without buttons pressed.
-        // If the button is pressed, we need to re-execute the hit test instead of
-        // reusing the cached results to trigger possible events.
-        () => (hitTestResult == null || event.down) ? renderView.hitTestMouseTrackers(event.position) : hitTestResult,
-      );
+      _mouseTracker!.updateWithEvent(event, () => hitTestResult ?? renderView.hitTestMouseTrackers(event.position));
     }
     super.dispatchEvent(event, hitTestResult);
   }
