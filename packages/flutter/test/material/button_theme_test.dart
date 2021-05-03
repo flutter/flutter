d032f4c3227a09020f184cec089b9a1220d9ccdd
Abhishek01039 <abhishekghaskata1999@gmail.com>
2021-05-03 20:19:49 +0530

    Remove tralling spaces

--- packages/flutter/test/material/button_theme_test.dart
+++ packages/flutter/test/material/button_theme_test.dart
@@ -363,7 +363,7 @@ void main() {
               child: FlatButton(
                 onPressed: () {},
                 focusNode: focusNode,
-                child: const Text('FlatButton'),                
+                child: const Text('FlatButton'),
               ),
             ),
           ),
