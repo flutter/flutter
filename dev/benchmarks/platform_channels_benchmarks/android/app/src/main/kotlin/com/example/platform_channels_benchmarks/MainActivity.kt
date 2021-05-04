f16f1795cf1c5ee60ee19f1c9236e80455ec883e
Aaron Clarke <aaclarke@google.com>
2021-05-04 09:54:08 -0700

    added missing license

--- dev/benchmarks/platform_channels_benchmarks/android/app/src/main/kotlin/com/example/platform_channels_benchmarks/MainActivity.kt
+++ dev/benchmarks/platform_channels_benchmarks/android/app/src/main/kotlin/com/example/platform_channels_benchmarks/MainActivity.kt
@@ -1,3 +1,7 @@
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 package com.example.platform_channels_benchmarks
 
 import io.flutter.embedding.android.FlutterActivity
