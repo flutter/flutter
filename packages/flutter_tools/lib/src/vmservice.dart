aimaniac1989/flutter-architecture-review

[flutter_tools] remove usage of getIsolate in extension waiter (#80960)

packages/flutter_tools/lib/src/vmservice.dart
@@ -865,8 +865,8 @@ class FlutterVmService {
     try {
       final List<vm_service.IsolateRef> refs = await _getIsolateRefs(webIsolate);
       for (final vm_service.IsolateRef ref in refs) {
-        final vm_service.Isolate isolate = await service.getIsolate(ref.id);
-        if (isolate.extensionRPCs.contains(extensionName)) {
+        final vm_service.Isolate isolate = await getIsolateOrNull(ref.id);
+        if (isolate != null && isolate.extensionRPCs.contains(extensionName)) {
           return ref;
         }
       }

flutter/flutter

packages/flutter_tools/lib/src/vmservice.dart
@@ -860,8 +860,8 @@ class FlutterVmService {
           continue;
         }
 
-        final vm_service.Isolate isolate = await service.getIsolate(isolateRef.id);
-        if (isolate.extensionRPCs.contains(extensionName)) {
+        final vm_service.Isolate isolate = await getIsolateOrNull(isolateRef.id);
+        if (isolate != null && isolate.extensionRPCs.contains(extensionName)) {
           return isolateRef;
         }
       }