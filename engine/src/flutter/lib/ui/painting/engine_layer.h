411b0b66ba49a8b817f575ef7bc35a67f66e5ff3
Dan Field <dnfield@google.com>
2021-05-07 10:34:01 -0700

    Delete unused method from engine_layer.h (flutter/engine#25924)

--- engine/src/flutter/lib/ui/painting/engine_layer.h
+++ engine/src/flutter/lib/ui/painting/engine_layer.h
@@ -24,11 +24,6 @@ class EngineLayer : public RefCountedDartWrappable<EngineLayer> {
 
   size_t GetAllocationSize() const override;
 
-  static fml::RefPtr<EngineLayer> MakeRetained(
-      std::shared_ptr<flutter::ContainerLayer> layer) {
-    return fml::MakeRefCounted<EngineLayer>(layer);
-  }
-
   static void MakeRetained(Dart_Handle dart_handle,
                            std::shared_ptr<flutter::ContainerLayer> layer) {
     auto engine_layer = fml::MakeRefCounted<EngineLayer>(layer);
