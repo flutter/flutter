043de44960aed70820fb6a4dc8b19f2b111e47b4
Chinmay Garde <chinmaygarde@google.com>
2021-05-09 00:22:26 -0700

    Stub out pipeline descriptors and builders.

---
+++ impeller/impeller/compositor/pipeline_builder.h
@@ -0,0 +1,24 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#pragma once
+
+#include "flutter/fml/macros.h"
+#include "impeller/compositor/pipeline_descriptor.h"
+
+namespace impeller {
+
+class PipelineBuilder {
+ public:
+  PipelineBuilder();
+
+  ~PipelineBuilder();
+
+  PipelineDescriptor Build() const;
+
+ private:
+  FML_DISALLOW_COPY_AND_ASSIGN(PipelineBuilder);
+};
+
+}  // namespace impeller
