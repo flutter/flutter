0269e4ef4b35bb828b7f2b2463b17c3dbd902b3f
nt4f04uNd <nt4f04uNd@gmail.com>
2021-05-09 19:42:39 +0300

    init

--- packages/flutter/lib/src/material/slider.dart
+++ packages/flutter/lib/src/material/slider.dart
@@ -903,8 +903,7 @@ class _RenderSlider extends RenderBox with RelayoutWhenSystemFontsChangeMixin {
     _tap = TapGestureRecognizer()
       ..team = team
       ..onTapDown = _handleTapDown
-      ..onTapUp = _handleTapUp
-      ..onTapCancel = _endInteraction;
+      ..onTapUp = _handleTapUp;
     _overlayAnimation = CurvedAnimation(
       parent: _state.overlayController,
       curve: Curves.fastOutSlowIn,
@@ -1224,7 +1223,7 @@ class _RenderSlider extends RenderBox with RelayoutWhenSystemFontsChangeMixin {
 
   void _startInteraction(Offset globalPosition) {
     _state.showValueIndicator();
-    if (isInteractive) {
+    if (!_active && isInteractive) {
       _active = true;
       // We supply the *current* value as the start location, so that if we have
       // a tap, it consists of a call to onChangeStart with the previous value and
