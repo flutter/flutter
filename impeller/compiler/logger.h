631ed54d3fc0f3333383c18243e4248e3c82505f
Chinmay Garde <chinmaygarde@google.com>
2021-05-10 17:09:14 -0700

    Wire up one-shot conversion of GLSL to MSL.

---
+++ impeller/compiler/logger.h
@@ -0,0 +1,40 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#pragma once
+
+#include <sstream>
+#include <string>
+
+#include "flutter/fml/logging.h"
+#include "flutter/fml/macros.h"
+
+namespace impeller {
+namespace compiler {
+
+class AutoLogger {
+ public:
+  AutoLogger(std::stringstream& logger) : logger_(logger) {}
+
+  ~AutoLogger() {
+    logger_ << std::endl;
+    logger_.flush();
+
+    FML_DLOG(INFO) << logger_.str();
+  }
+
+  template <class T>
+  AutoLogger& operator<<(const T& object) {
+    logger_ << object;
+    return *this;
+  }
+
+ private:
+  std::stringstream& logger_;
+
+  FML_DISALLOW_COPY_AND_ASSIGN(AutoLogger);
+};
+
+}  // namespace compiler
+}  // namespace impeller
