da2d6f86557a70d43c6ebccd1ab7781b841b4ad4
Ferhat <ferhat@gmail.com>
2021-05-11 12:50:19 -0700

    Revert "[web] Catch image load failures in --release builds (#25602)" (flutter/engine#26073)
    This reverts commit 1e2133090840102fd45b6288a6429330527fa65a.

--- engine/src/flutter/lib/web_ui/lib/src/engine/html_image_codec.dart
+++ engine/src/flutter/lib/web_ui/lib/src/engine/html_image_codec.dart
@@ -86,7 +86,6 @@ class HtmlCodec implements ui.Codec {
       loadSubscription?.cancel();
       errorSubscription.cancel();
       completer.completeError(event);
-      throw ArgumentError('Unable to load image asset: $src');
     });
     loadSubscription = imgElement.onLoad.listen((html.Event event) {
       if (chunkCallback != null) {
