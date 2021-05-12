c199d9a336087e62a45b619c99936745209fffac
Jenn Magder <magder@google.com>
2021-05-11 18:29:20 -0700

    Fix tests

--- dev/devicelab/test/utils_test.dart
+++ dev/devicelab/test/utils_test.dart
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'dart:async';
-
 import 'package:flutter_devicelab/framework/utils.dart';
 
 import 'common.dart';
@@ -40,28 +38,6 @@ void main() {
     test('localEngine', () {
       expect(localEngine, null);
       expect(localEngineSrcPath, null);
-    });
-  });
-
-  group('evalFlutter', () {
-    test('sets environment', () async {
-      final StringBuffer capturedPrintLines = StringBuffer();
-      await runZoned<Future<void>>(
-        () async {
-          // Doesn't matter which command is run.
-          await evalFlutter('--version');
-        },
-        zoneSpecification: ZoneSpecification(
-          // Intercept print calls from utils.
-          print: (Zone self, ZoneDelegate parent, Zone zone, String line) async {
-            capturedPrintLines.writeln(line);
-          },
-        ),
-      );
-      final String capturedPrint = capturedPrintLines.toString();
-      expect(capturedPrint, contains('flutter --version in'));
-      expect(capturedPrint, contains('with environment {BOT: true, LANG: en_US.UTF-8}'));
-      expect(capturedPrint, contains('exit code: 0'));
     });
   });
 }
