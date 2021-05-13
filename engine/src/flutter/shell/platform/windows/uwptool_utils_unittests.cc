522de2b6210cdc836d22ceda4559e0e9f9426cc7
Chris Bracken <chris@bracken.jp>
2021-05-13 09:02:27 -0700

    [uwptool] Support lookup of full package name (flutter/engine#26121)
    UWP apps must be launched using their package family name, but are
    uninstalled using their full package name. This patch updates the
    Application class to store both.
    
    Further, when launching UWP apps, we now do a lookup to verify the
    package exists before attempting to launch it.

--- engine/src/flutter/shell/platform/windows/uwptool_utils_unittests.cc
+++ engine/src/flutter/shell/platform/windows/uwptool_utils_unittests.cc
@@ -26,9 +26,22 @@ TEST(ApplicationStore, GetInstalledApplications) {
   EXPECT_FALSE(apps.empty());
 
   auto ms_pos = std::find_if(apps.begin(), apps.end(), [](const auto& app) {
-    return app.GetPackageId().rfind(L"Microsoft.", 0);
+    return app.GetPackageFamily().rfind(L"Microsoft.", 0);
   });
   EXPECT_TRUE(ms_pos != apps.end());
+}
+
+// Verify that we can look up an app by family name.
+TEST(ApplicationStore, GetInstalledApplication) {
+  ApplicationStore app_store;
+  std::vector<Application> apps = app_store.GetInstalledApplications();
+  EXPECT_FALSE(apps.empty());
+
+  std::optional<Application> found_app =
+      app_store.GetInstalledApplication(apps[0].GetPackageFamily());
+  ASSERT_TRUE(found_app != std::nullopt);
+  EXPECT_EQ(found_app->GetPackageFamily(), apps[0].GetPackageFamily());
+  EXPECT_EQ(found_app->GetPackageFullName(), apps[0].GetPackageFullName());
 }
 
 }  // namespace testing
