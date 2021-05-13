0f28cd6fdb0c992cf5f0eaa1082fb7148bea5d6a
Chris Bracken <chris@bracken.jp>
2021-05-13 11:28:20 -0700

    [uwptool] Add uninstall command (#26122)
    Adds an uninstall command that uninstalls the specified application by
    its package family name.
    
    This currently attempts to remove for all users, but in future, we may
    want to restrict install and uninstall to the current user only.
    
    Also renames ApplicationStore::GetInstalledApplications() to
    ApplicationStore::GetApps()

--- shell/platform/windows/uwptool_utils.cc
+++ shell/platform/windows/uwptool_utils.cc
@@ -12,6 +12,7 @@
 #include <winrt/Windows.Management.Deployment.h>
 #include <winrt/base.h>
 
+#include <iostream>
 #include <optional>
 #include <string>
 #include <unordered_set>
@@ -19,9 +20,12 @@
 
 namespace flutter {
 
-Application::Application(const std::wstring_view package_family,
+Application::Application(const std::wstring_view package_name,
+                         const std::wstring_view package_family,
                          const std::wstring_view package_full_name)
-    : package_family_(package_family), package_full_name_(package_full_name) {}
+    : package_name_(package_name),
+      package_family_(package_family),
+      package_full_name_(package_full_name) {}
 
 int Application::Launch(const std::wstring_view args) {
   // Create the ApplicationActivationManager.
@@ -45,34 +49,67 @@ int Application::Launch(const std::wstring_view args) {
   return process_id;
 }
 
-std::vector<Application> ApplicationStore::GetInstalledApplications() const {
+bool Application::Uninstall() {
+  using winrt::Windows::Foundation::AsyncStatus;
+  using winrt::Windows::Management::Deployment::PackageManager;
+  using winrt::Windows::Management::Deployment::RemovalOptions;
+
+  PackageManager package_manager;
+  auto operation = package_manager.RemovePackageAsync(
+      package_full_name_, RemovalOptions::RemoveForAllUsers);
+  operation.get();
+
+  if (operation.Status() == AsyncStatus::Completed) {
+    return true;
+  } else if (operation.Status() == AsyncStatus::Canceled) {
+    return false;
+  } else if (operation.Status() == AsyncStatus::Error) {
+    auto result = operation.GetResults();
+    std::wcerr << L"error: uninstall failed for package " << package_full_name_
+               << L" with error: " << result.ErrorText().c_str() << std::endl;
+    return false;
+  }
+  return false;
+}
+
+std::vector<Application> ApplicationStore::GetApps() const {
   using winrt::Windows::ApplicationModel::Package;
   using winrt::Windows::Management::Deployment::PackageManager;
 
   // Find packages for the current user (default for empty string).
-  PackageManager package_manager;
   std::vector<Application> apps;
-  for (const Package& package : package_manager.FindPackagesForUser(L"")) {
-    apps.emplace_back(package.Id().FamilyName().c_str(),
-                      package.Id().FullName().c_str());
+  try {
+    PackageManager package_manager;
+    for (const Package& package : package_manager.FindPackagesForUser(L"")) {
+      apps.emplace_back(package.Id().Name().c_str(),
+                        package.Id().FamilyName().c_str(),
+                        package.Id().FullName().c_str());
+    }
+  } catch (winrt::hresult_error error) {
+    return {};
   }
   return apps;
 }
 
-std::optional<Application> ApplicationStore::GetInstalledApplication(
+std::vector<Application> ApplicationStore::GetApps(
     const std::wstring_view package_family) const {
   using winrt::Windows::ApplicationModel::Package;
   using winrt::Windows::Management::Deployment::PackageManager;
 
   // Find packages for the current user (default for empty string).
-  PackageManager package_manager;
   std::vector<Application> apps;
-  for (const Package& package :
-       package_manager.FindPackagesForUser(L"", package_family)) {
-    return std::optional(Application(package.Id().FamilyName().c_str(),
-                                     package.Id().FullName().c_str()));
+  try {
+    PackageManager package_manager;
+    for (const Package& package :
+         package_manager.FindPackagesForUser(L"", package_family)) {
+      apps.emplace_back(package.Id().Name().c_str(),
+                        package.Id().FamilyName().c_str(),
+                        package.Id().FullName().c_str());
+    }
+  } catch (winrt::hresult_error error) {
+    return {};
   }
-  return std::nullopt;
+  return apps;
 }
 
 }  // namespace flutter
--- shell/platform/windows/uwptool_utils.h
+++ shell/platform/windows/uwptool_utils.h
@@ -14,15 +14,29 @@ namespace flutter {
 // A UWP application.
 class Application {
  public:
-  explicit Application(const std::wstring_view package_family,
+  explicit Application(const std::wstring_view package_name,
+                       const std::wstring_view package_family,
                        const std::wstring_view package_full_name);
   Application(const Application& other) = default;
   Application& operator=(const Application& other) = default;
 
+  // Returns the package name.
+  //
+  // The package name is a globally unique name that represents the 'friendly'
+  // name of a package.
+  std::wstring GetPackageName() const { return package_name_; }
+
   // Returns the package family.
+  //
+  // The package family is a serialized form of the package identifiers that
+  // includes the package name and publisher.
   std::wstring GetPackageFamily() const { return package_family_; }
 
   // Returns the package full name.
+  //
+  // The package full name is a serialized form of the package identifiers that
+  // includes a particular version of the package on the computer. It encodes
+  // package name, publisher, architecture and version information.
   std::wstring GetPackageFullName() const { return package_full_name_; }
 
   // Launches the application with the specified list of launch arguments.
@@ -30,7 +44,13 @@ class Application {
   // Returns the process ID on success, or -1 on failure.
   int Launch(const std::wstring_view args);
 
+  // Uninstalls the application.
+  //
+  // Returns true on success.
+  bool Uninstall();
+
  private:
+  std::wstring package_name_;
   std::wstring package_family_;
   std::wstring package_full_name_;
 };
@@ -44,11 +64,11 @@ class ApplicationStore {
   ApplicationStore(const ApplicationStore& other) = delete;
   ApplicationStore& operator=(const ApplicationStore& other) = delete;
 
-  // Returns a list of all installed application user model IDs.
-  std::vector<Application> GetInstalledApplications() const;
+  // Returns all installed applications.
+  std::vector<Application> GetApps() const;
 
-  // Returns a list of all installed application user model IDs.
-  std::optional<Application> GetInstalledApplication(
+  // Returns all installed applications with the specified family name.
+  std::vector<Application> GetApps(
       const std::wstring_view package_family) const;
 };
 
