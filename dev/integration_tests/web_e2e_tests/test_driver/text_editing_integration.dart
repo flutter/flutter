8e203513825f34b9a6212d1ce6dde95c4694c8a6
Jonah Williams <jonahwilliams@google.com>
2021-05-14 13:32:54 -0700

    Fix analysis errors (#82569)

--- dev/integration_tests/web_e2e_tests/test_driver/text_editing_integration.dart
+++ dev/integration_tests/web_e2e_tests/test_driver/text_editing_integration.dart
@@ -7,6 +7,7 @@ import 'dart:js_util' as js_util;
 
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:web_e2e_tests/text_editing_main.dart' as app;
