238b2ced3d63772a9c92f6aae365f0f937bdcdda
Aaron Clarke <aaclarke@google.com>
2021-05-21 13:15:35 -0700

    dans feedback and noop channel for ios

--- dev/benchmarks/platform_channels_benchmarks/android/app/src/main/kotlin/com/example/platform_channels_benchmarks/MainActivity.kt
+++ dev/benchmarks/platform_channels_benchmarks/android/app/src/main/kotlin/com/example/platform_channels_benchmarks/MainActivity.kt
@@ -12,6 +12,9 @@ import io.flutter.plugin.common.StandardMessageCodec
 import java.nio.ByteBuffer
 
 class MainActivity: FlutterActivity() {
+    // We allow for the caching of a response in the binary channel case since
+    // the reply requires a direct buffer, but the input is not a direct buffer.
+    // We can't directly send the input back to the reply currently.
     private var byteBufferCache : ByteBuffer? = null
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         val reset = BasicMessageChannel(flutterEngine.dartExecutor, "dev.flutter.echo.reset", StandardMessageCodec.INSTANCE)
