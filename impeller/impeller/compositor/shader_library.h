36f41b561ce8581f82795a06c64fb15de54ef918
Chinmay Garde <chinmaygarde@google.com>
2021-05-22 18:01:55 -0700

    Move shader function to its own TU

--- impeller/impeller/compositor/shader_library.h
+++ impeller/impeller/compositor/shader_library.h
@@ -11,39 +11,12 @@
 #include <string_view>
 #include <unordered_map>
 
-#include "flutter/fml/hash_combine.h"
 #include "flutter/fml/macros.h"
-#include "impeller/compositor/comparable.h"
-#include "impeller/shader_glue/shader_types.h"
+#include "impeller/compositor/shader_function.h"
 
 namespace impeller {
 
 class Context;
-
-class ShaderFunction : public Comparable<ShaderFunction> {
- public:
-  virtual ~ShaderFunction();
-
-  ShaderStage GetStage() const;
-
-  id<MTLFunction> GetMTLFunction() const;
-
-  // Comparable<ShaderFunction>
-  std::size_t GetHash() const override;
-
-  // Comparable<ShaderFunction>
-  bool IsEqual(const ShaderFunction& other) const override;
-
- private:
-  friend class ShaderLibrary;
-
-  id<MTLFunction> function_ = nullptr;
-  ShaderStage stage_;
-
-  ShaderFunction(id<MTLFunction> function, ShaderStage stage);
-
-  FML_DISALLOW_COPY_AND_ASSIGN(ShaderFunction);
-};
 
 class ShaderLibrary {
  public:
