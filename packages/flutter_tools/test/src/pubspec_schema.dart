336f9cbc1e7c5dc07bfa3f2dc7330be7ace1059c
Jenn Magder <magder@google.com>
2021-05-26 14:23:56 -0700

    Migrate pubspec_schema to null safety

--- packages/flutter_tools/test/src/pubspec_schema.dart
+++ packages/flutter_tools/test/src/pubspec_schema.dart
@@ -2,19 +2,23 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// @dart = 2.8
-
 import 'package:flutter_tools/src/flutter_manifest.dart';
 import 'package:flutter_tools/src/globals_null_migrated.dart' as globals;
-import 'package:meta/meta.dart';
 import 'package:yaml/yaml.dart';
 
 import 'common.dart';
 
 /// Check if the pubspec.yaml file under the `projectDir` is valid for a plugin project.
-void validatePubspecForPlugin({@required String projectDir, @required String pluginClass, @required List<String> expectedPlatforms, List<String> unexpectedPlatforms = const <String>[], String androidIdentifier, String webFileName}) {
-    final FlutterManifest manifest = FlutterManifest.createFromPath(projectDir+'/pubspec.yaml', fileSystem: globals.fs, logger: globals.logger);
-    final YamlMap platformsMap = YamlMap.wrap(manifest.supportedPlatforms);
+void validatePubspecForPlugin({
+  required String projectDir,
+  required String pluginClass,
+  required List<String> expectedPlatforms,
+  List<String> unexpectedPlatforms = const <String>[],
+  String? androidIdentifier,
+  String? webFileName,
+}) {
+  final FlutterManifest manifest = FlutterManifest.createFromPath(projectDir+'/pubspec.yaml', fileSystem: globals.fs, logger: globals.logger)!;
+    final YamlMap platformsMap = YamlMap.wrap(manifest.supportedPlatforms!);
     for (final String platform in expectedPlatforms) {
       expect(platformsMap[platform], isNotNull);
       expect(platformsMap[platform]['pluginClass'], pluginClass);
