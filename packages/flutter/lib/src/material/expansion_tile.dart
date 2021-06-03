29a7f5f02d74ea5833f55c551854bd408bf62d0f
J-P Nurmi <jpnurmi@gmail.com>
2021-06-03 13:41:36 +0200

    Add simple example snippet to the ExpansionTile docs

--- packages/flutter/lib/src/material/expansion_tile.dart
+++ packages/flutter/lib/src/material/expansion_tile.dart
@@ -30,6 +30,53 @@ const Duration _kExpand = Duration(milliseconds: 200);
 /// (i.e. the trailing edge). This can be changed using [controlAffinity]. This maps
 /// to the [leading] and [trailing] properties of [ExpansionTile].
 ///
+/// {@tool dartpad --template=stateful_widget_scaffold}
+///
+/// This example demonstrates different configurations of ExpansionTile.
+///
+/// ```dart
+/// bool _customTileExpanded = false;
+///
+/// @override
+/// Widget build(BuildContext context) {
+///   return Column(
+///     children: <Widget>[
+///       const ExpansionTile(
+///         title: Text('ExpansionTile 1'),
+///         subtitle: Text('Trailing expansion arrow icon'),
+///         children: <Widget>[
+///           ListTile(title: Text('This is tile number 1')),
+///         ],
+///       ),
+///       ExpansionTile(
+///         title: const Text('ExpansionTile 2'),
+///         subtitle: const Text('Custom expansion arrow icon'),
+///         trailing: Icon(
+///           _customTileExpanded
+///               ? Icons.arrow_drop_down_circle
+///               : Icons.arrow_drop_down,
+///         ),
+///         children: const <Widget>[
+///           ListTile(title: Text('This is tile number 2')),
+///         ],
+///         onExpansionChanged: (expanded) {
+///           setState(() => _customTileExpanded = expanded);
+///         },
+///       ),
+///       const ExpansionTile(
+///         title: Text('ExpansionTile 3'),
+///         subtitle: Text('Leading expansion arrow icon'),
+///         controlAffinity: ListTileControlAffinity.leading,
+///         children: <Widget>[
+///           ListTile(title: Text('This is tile number 3')),
+///         ],
+///       ),
+///     ],
+///   );
+/// }
+/// ```
+/// {@end-tool}
+///
 /// See also:
 ///
 ///  * [ListTile], useful for creating expansion tile [children] when the
