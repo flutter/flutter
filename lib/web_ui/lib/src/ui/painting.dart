43522424c07e244e20a6fc2962172609b9dc2b67
Dan Field <dnfield@google.com>
2021-06-04 16:14:01 -0700

    Docs, debugDisposed for ImmutableBuffer (#26591)

--- lib/web_ui/lib/src/ui/painting.dart
+++ lib/web_ui/lib/src/ui/painting.dart
@@ -712,6 +712,15 @@ class ImmutableBuffer {
 
   Uint8List? _list;
   final int length;
+
+  bool get debugDisposed {
+    late bool disposed;
+    assert(() {
+      disposed = _list == null;
+      return true;
+    }());
+    return disposed;
+  }
   void dispose() => _list = null;
 }
 
