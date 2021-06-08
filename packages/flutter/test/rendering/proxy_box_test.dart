c0659e7065426b952238ee905332cae99005b9a0
Ren You <renyou@google.com>
2021-06-08 05:30:56 -0700

    Revert "Always push layer for RenderAnimatedOpacityMixin (#83145)"
    This reverts commit 048baa75913ede225cf140b725dda87f9dc2bebd.

--- packages/flutter/test/rendering/proxy_box_test.dart
+++ packages/flutter/test/rendering/proxy_box_test.dart
@@ -306,7 +306,7 @@ void main() {
     expect(renderAnimatedOpacity.needsCompositing, false);
   });
 
-  test('RenderAnimatedOpacity does composite if it is opaque', () {
+  test('RenderAnimatedOpacity does not composite if it is opaque', () {
     final Animation<double> opacityAnimation = AnimationController(
       vsync: FakeTickerProvider(),
     )..value = 1.0;
@@ -318,7 +318,7 @@ void main() {
     );
 
     layout(renderAnimatedOpacity, phase: EnginePhase.composite);
-    expect(renderAnimatedOpacity.needsCompositing, true);
+    expect(renderAnimatedOpacity.needsCompositing, false);
   });
 
   test('RenderAnimatedOpacity reuses its layer', () {
