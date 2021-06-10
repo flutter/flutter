8fc4556d51657dbdcdd15b7308b9dded7bab3708
Casey Rogers <caseycrogers@berkeley.edu>
2021-06-10 15:44:30 -0700

    hijack helloworld

--- examples/hello_world/lib/main.dart
+++ examples/hello_world/lib/main.dart
@@ -2,15 +2,28 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-void main() =>
-  runApp(
-    const Center(
-      child:
-        Text('Hello, world!',
-          key: Key('title'),
-          textDirection: TextDirection.ltr,
+void main() => runApp(
+      MaterialApp(
+        home: Material(
+          color: Colors.white,
+          child: DraggableScrollableSheet(
+            snapPoints: [],
+            builder: (BuildContext context, ScrollController controller) =>
+                Material(
+              color: Colors.blueAccent,
+              child: ListView.builder(
+                controller: controller,
+                itemBuilder: (BuildContext context, int i) => Text(
+                  i.toString(),
+                  style: const TextStyle(fontSize: 32),
+                ),
+                itemCount: 50,
+              ),
+            ),
+          ),
         ),
       ),
     );
