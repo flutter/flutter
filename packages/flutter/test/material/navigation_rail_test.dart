65859b6a0bbd30281357f409f228514273f3ad35
Wilson Wilson <wilsonwilsondev@gmail.com>
2021-06-11 09:50:52 +0100

    Added null test for onDestinationSelected

--- packages/flutter/test/material/navigation_rail_test.dart
+++ packages/flutter/test/material/navigation_rail_test.dart
@@ -1917,6 +1917,21 @@ void main() {
     await tester.tap(find.text('Ghi'));
     expect(selectedIndex, 2);
   });
+  testWidgets('onDestinationSelected is not called if null', (WidgetTester tester) async {
+    const int selectedIndex = 0;
+    
+    await _pumpNavigationRail(
+      tester,
+      navigationRail: NavigationRail(
+        selectedIndex: selectedIndex,
+        destinations: _destinations(),
+        labelType: NavigationRailLabelType.all,
+      ),
+    );
+
+    await tester.tap(find.text('Def'));
+    expect(selectedIndex, 0);
+  });
 
   testWidgets('Changing destinations animate when [labelType]=selected', (WidgetTester tester) async {
     int selectedIndex = 0;
