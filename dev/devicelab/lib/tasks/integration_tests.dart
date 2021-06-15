b95231e7fca45287b3e7e05ef144c8093b5d8e40
Jenn Magder <magder@google.com>
2021-06-14 18:04:02 -0700

    Set up iOS native integration_tests in ui example project (#84596)

--- dev/devicelab/lib/tasks/integration_tests.dart
+++ dev/devicelab/lib/tasks/integration_tests.dart
@@ -6,6 +6,7 @@
 
 import '../framework/devices.dart';
 import '../framework/framework.dart';
+import '../framework/ios.dart';
 import '../framework/task_result.dart';
 import '../framework/utils.dart';
 
@@ -189,6 +190,19 @@ class IntegrationTest {
         testTarget,
       ];
       await flutter('test', options: options);
+
+      if (device is IosDevice) {
+        section('Run integration_test from Xcode');
+
+        final List<String> options = <String>[
+          'ios',
+          '--config-only',
+          testTarget,
+        ];
+        await flutter('build', options: options);
+
+        await runNativeIosXcodeTests(device, testDirectory);
+      }
 
       return TaskResult.success(null);
     });
