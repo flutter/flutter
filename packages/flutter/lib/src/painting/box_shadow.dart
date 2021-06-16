c04cd9c71310b01a69687e54ee4ca3742007b808
deepak-penaganti <deepak_24x365@live.com>
2021-06-17 01:45:33 +0530

    Support BlurStyle values in BoxShadow (#52999)

--- packages/flutter/lib/src/painting/box_shadow.dart
+++ packages/flutter/lib/src/painting/box_shadow.dart
@@ -36,10 +36,14 @@ class BoxShadow extends ui.Shadow {
     Offset offset = Offset.zero,
     double blurRadius = 0.0,
     this.spreadRadius = 0.0,
+    this.blurStyle = BlurStyle.normal,
   }) : super(color: color, offset: offset, blurRadius: blurRadius);
 
   /// The amount the box should be inflated prior to applying the blur.
   final double spreadRadius;
+
+  /// The style(inner/normal/outer/solid) of the blur.
+  final BlurStyle blurStyle;
 
   /// Create the [Paint] object that corresponds to this shadow description.
   ///
@@ -51,7 +55,7 @@ class BoxShadow extends ui.Shadow {
   Paint toPaint() {
     final Paint result = Paint()
       ..color = color
-      ..maskFilter = MaskFilter.blur(BlurStyle.normal, blurSigma);
+      ..maskFilter = MaskFilter.blur(blurStyle, blurSigma);
     assert(() {
       if (debugDisableShadows)
         result.maskFilter = null;
@@ -68,6 +72,7 @@ class BoxShadow extends ui.Shadow {
       offset: offset * factor,
       blurRadius: blurRadius * factor,
       spreadRadius: spreadRadius * factor,
+      blurStyle: blurStyle,
     );
   }
 
@@ -91,6 +96,7 @@ class BoxShadow extends ui.Shadow {
       offset: Offset.lerp(a.offset, b.offset, t)!,
       blurRadius: ui.lerpDouble(a.blurRadius, b.blurRadius, t)!,
       spreadRadius: ui.lerpDouble(a.spreadRadius, b.spreadRadius, t)!,
+      blurStyle: a.blurStyle == BlurStyle.normal ? b.blurStyle : a.blurStyle,
     );
   }
 
@@ -123,12 +129,13 @@ class BoxShadow extends ui.Shadow {
         && other.color == color
         && other.offset == offset
         && other.blurRadius == blurRadius
-        && other.spreadRadius == spreadRadius;
+        && other.spreadRadius == spreadRadius
+        && other.blurStyle == blurStyle;
   }
 
   @override
-  int get hashCode => hashValues(color, offset, blurRadius, spreadRadius);
+  int get hashCode => hashValues(color, offset, blurRadius, spreadRadius, blurStyle);
 
   @override
-  String toString() => 'BoxShadow($color, $offset, ${debugFormatDouble(blurRadius)}, ${debugFormatDouble(spreadRadius)})';
+  String toString() => 'BoxShadow($color, $offset, ${debugFormatDouble(blurRadius)}, ${debugFormatDouble(spreadRadius)}), $blurStyle';
 }
