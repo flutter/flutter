e021747729e5c91bf50229467176227a3a49f486
Tong Mu <dkwingsmt@gmail.com>
2021-06-17 02:01:52 -0700

    Impl
    Fix compile
    
    Revert include
    
    std::map. secondary logical.
    
    Sort. Comment add map
    
    testing_key_codes
    
    Add if def
    
    Fix ios format. Move key_codes to embedder.
    
    Format ios
    
    ios license
    
    Fix framework plane values
    
    Apply suggestions from code review
    
    Co-authored-by: Greg Spencer <gspencergoog@users.noreply.github.com>
    
    Apply suggestions from code review
    
    Co-authored-by: Greg Spencer <gspencergoog@users.noreply.github.com>
    
    Add kStartOfPlatformPlanes
    
    Remove none
    
    Describe Breakage
    
    Typo
    
    Fix none

--- dev/tools/gen_keycodes/bin/gen_keycodes.dart
+++ dev/tools/gen_keycodes/bin/gen_keycodes.dart
@@ -15,6 +15,7 @@ import 'package:gen_keycodes/keyboard_maps_code_gen.dart';
 import 'package:gen_keycodes/logical_key_data.dart';
 import 'package:gen_keycodes/macos_code_gen.dart';
 import 'package:gen_keycodes/physical_key_data.dart';
+import 'package:gen_keycodes/testing_key_codes_gen.dart';
 import 'package:gen_keycodes/utils.dart';
 import 'package:gen_keycodes/web_code_gen.dart';
 import 'package:gen_keycodes/windows_code_gen.dart';
@@ -71,7 +72,20 @@ String readDataFile(String fileName) {
   return File(path.join(dataRoot, fileName)).readAsStringSync();
 }
 
+bool _assertsEnabled() {
+  bool enabledAsserts = false;
+  assert(() {
+    enabledAsserts = true;
+    return true;
+  }());
+  return enabledAsserts;
+}
+
 Future<void> main(List<String> rawArguments) async {
+  if (!_assertsEnabled()) {
+    print('The gen_keycodes script must be run with --enable-asserts.');
+    return;
+  }
   final ArgParser argParser = ArgParser();
   argParser.addOption(
     'engine-root',
@@ -207,6 +221,14 @@ Future<void> main(List<String> rawArguments) async {
   }
   print('Writing ${'key maps'.padRight(15)}${mapsFile.absolute}');
   await mapsFile.writeAsString(KeyboardMapsCodeGenerator(physicalData, logicalData).generate());
+
+  final File keyCodesFile = File(path.join(PlatformCodeGenerator.engineRoot,
+      'shell', 'platform', 'embedder', 'test_utils', 'key_codes.h'));
+  if (!mapsFile.existsSync()) {
+    mapsFile.createSync(recursive: true);
+  }
+  print('Writing ${'engine key codes'.padRight(15)}${mapsFile.absolute}');
+  await keyCodesFile.writeAsString(KeyCodesCcGenerator(physicalData, logicalData).generate());
 
   final Map<String, PlatformCodeGenerator> platforms = <String, PlatformCodeGenerator>{
     'android': AndroidCodeGenerator(
