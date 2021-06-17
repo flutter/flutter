7e75bfeeb147e486204441718c794f6323669061
Tong Mu <dkwingsmt@users.noreply.github.com>
2021-06-17 04:59:58 -0700

    Fix windows keyboard: Extended key, delegate order (flutter/engine#26757)
    * Adjust order
    
    * Update code
    
    * Fix Extended
    
    * Add tests
    
    * Better constants
    
    * Format
    
    * Comment
    
    * Link

--- engine/src/flutter/shell/platform/windows/keyboard_key_channel_handler.cc
+++ engine/src/flutter/shell/platform/windows/keyboard_key_channel_handler.cc
@@ -32,6 +32,15 @@ static constexpr char kKeyDown[] = "keydown";
 // emitting a warning on the console about unhandled events.
 static constexpr int kMaxPendingEvents = 1000;
 
+// The bit for a scancode indicating the key is extended.
+//
+// Win32 defines some keys to be "extended", such as ShiftRight, which shares
+// the same scancode as its non-extended counterpart, such as ShiftLeft.  In
+// Chromium's scancode table, from which Flutter's physical key list is
+// derived, these keys are marked with this bit.  See
+// https://chromium.googlesource.com/codesearch/chromium/src/+/refs/heads/master/ui/events/keycodes/dom/dom_code_data.inc
+static constexpr int kScancodeExtended = 0xe000;
+
 // Re-definition of the modifiers for compatibility with the Flutter framework.
 // These have to be in sync with the framework's RawKeyEventDataWindows
 // modifiers definition.
@@ -119,7 +128,8 @@ void KeyboardKeyChannelHandler::KeyboardHook(
   rapidjson::Document event(rapidjson::kObjectType);
   auto& allocator = event.GetAllocator();
   event.AddMember(kKeyCodeKey, key, allocator);
-  event.AddMember(kScanCodeKey, scancode, allocator);
+  event.AddMember(kScanCodeKey, scancode | (extended ? kScancodeExtended : 0),
+                  allocator);
   event.AddMember(kCharacterCodePointKey, character, allocator);
   event.AddMember(kKeyMapKey, kWindowsKeyMap, allocator);
   event.AddMember(kModifiersKey, GetModsForKeyState(), allocator);
