3ad0f541d0c532123555c933c9c4c8c2eea6219c
Jonah Williams <campfish91@gmail.com>
2021-06-24 11:56:44 -0700

    delete splash temp

--- examples/splash/android/app/src/main/kotlin/com/example/splash/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.splash
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- examples/splash/linux/flutter/generated_plugin_registrant.cc
+++
@@ -1,9 +0,0 @@
-//
-//  Generated file. Do not edit.
-//
-
-#include "generated_plugin_registrant.h"
-
-
-void fl_register_plugins(FlPluginRegistry* registry) {
-}
