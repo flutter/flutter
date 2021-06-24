d68c8dbcdd4686a3ed6d59b1f65da6f5e5d09b7c
Anirban-Chand <anirbanchand54@gmail.com>
2021-06-24 21:19:32 +0530

    Create bengali.dart file
    'Hello World' program in flutter for Bengalees all over the world. 
    নমস্কার বাঙালী 🙏

---
+++ examples/hello_world/lib/bengali.dart
@@ -0,0 +1,11 @@
+// Hello World Program for Bengalees
+// নমস্কার বাঙালী
+import 'package:flutter/material.dart';
+void main() => runApp(MaterialApp(debugShowCheckedModeBanner: false, title: 'নমস্কার বাঙালী', home: Home()));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(title: Text('নমস্কার বাঙালী'),), body: Center(child: Text('নমস্কার বিশ্ব 🙏 ', style: TextStyle(fontSize: 40, fontWeight: FontWeight.w800),)));
+  }
+}
