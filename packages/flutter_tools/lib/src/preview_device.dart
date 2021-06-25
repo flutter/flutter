1f911f427c77e13b4b81eaf9e1c1603a7dc81902
Jonah Williams <jonahwilliams@google.com>
2021-06-25 11:07:04 -0700

    Update preview_device.dart

--- packages/flutter_tools/lib/src/preview_device.dart
+++ packages/flutter_tools/lib/src/preview_device.dart
@@ -30,6 +30,11 @@ BundleBuilder _defaultBundleBuilder() {
   return BundleBuilder();
 }
 
+/// A device type that runs a prebuilt desktop binary alongside a locally compiled kernel file.
+///
+/// This could be used to support debug local development without plugins on machines that
+/// have not completed the SDK setup. These feautres are not fully implemented and the
+/// device is not currently discoverable.
 class PreviewDevice extends Device {
   PreviewDevice({
     @required Platform platform,
