cfea27e117ca0212918562ff780dd8f3f60df4dc
Chase Latta <638538+chaselatta@users.noreply.github.com>
2021-06-28 13:48:21 -0700

    Allow fuchsia_archive to accept a cml file and cmx file (#27012)
    This adds support for adding a cml file to a fuchsia package. The
    cml file will be compiled to a cm file and included in the archive.

---
+++ tools/fuchsia/compile_cml.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+#
+# Copyright 2013 The Flutter Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+""" Compiles a cml manifest file.
+"""
+
+import argparse
+import os
+import subprocess
+import sys
+
+def main():
+  parser = argparse.ArgumentParser()
+
+  parser.add_argument('--cmc-bin', dest='cmc_bin', action='store', required=True)
+  parser.add_argument(
+      '--output', dest='output', action='store', required=True)
+  parser.add_argument(
+      '--manifest-file', dest='manifest_file', action='store', required=True)
+  parser.add_argument(
+      '--includepath', dest='includepath', action='store', required=True
+  )
+
+  args = parser.parse_args()
+
+  assert os.path.exists(args.cmc_bin)
+  assert os.path.exists(args.manifest_file)
+
+  print(args.includepath)
+  subprocess.check_output([
+      args.cmc_bin,
+      'compile',
+      '--output',
+      args.output,
+      args.manifest_file,
+      '--includepath',
+      args.includepath,
+  ])
+
+  return 0
+
+
+if __name__ == '__main__':
+  sys.exit(main())
