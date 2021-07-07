1e86cd92e248ec99ea59b44ba3dd422b60f7bf0a
Kaushik Iska <iska.kaushik@gmail.com>
2021-07-07 11:51:02 -0400

    Make rasterFinishWallTime required (flutter/engine#27142)

--- engine/src/flutter/lib/ui/platform_dispatcher.dart
+++ engine/src/flutter/lib/ui/platform_dispatcher.dart
@@ -1182,7 +1182,7 @@ class FrameTiming {
     required int buildFinish,
     required int rasterStart,
     required int rasterFinish,
-    int rasterFinishWallTime = -1,
+    required int rasterFinishWallTime,
     int frameNumber = -1,
   }) {
     return FrameTiming._(<int>[
@@ -1191,7 +1191,7 @@ class FrameTiming {
       buildFinish,
       rasterStart,
       rasterFinish,
-      if (rasterFinishWallTime == -1) rasterFinish else rasterFinishWallTime,
+      rasterFinishWallTime,
       frameNumber,
     ]);
   }
