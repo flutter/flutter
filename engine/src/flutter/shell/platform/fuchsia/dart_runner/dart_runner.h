801650fbb81b905d30f876d54985a27452197b95
Chase Latta <638538+chaselatta@users.noreply.github.com>
2021-07-08 15:19:21 -0700

    [fuchsia] make dart_runner work with cfv2 (flutter/engine#27226)
    Adds support for running the dart runner as a CFv2 component.
    The runner has been refactored to launch in both a v1 environemnt
    as well as a v2 environment.

--- engine/src/flutter/shell/platform/fuchsia/dart_runner/dart_runner.h
+++ engine/src/flutter/shell/platform/fuchsia/dart_runner/dart_runner.h
@@ -5,6 +5,7 @@
 #ifndef FLUTTER_SHELL_PLATFORM_FUCHSIA_DART_RUNNER_DART_RUNNER_H_
 #define FLUTTER_SHELL_PLATFORM_FUCHSIA_DART_RUNNER_DART_RUNNER_H_
 
+#include <fuchsia/component/runner/cpp/fidl.h>
 #include <fuchsia/sys/cpp/fidl.h>
 #include <lib/fidl/cpp/binding_set.h>
 #include <lib/sys/cpp/component_context.h>
@@ -13,7 +14,8 @@
 
 namespace dart_runner {
 
-class DartRunner : public fuchsia::sys::Runner {
+class DartRunner : public fuchsia::sys::Runner,
+                   public fuchsia::component::runner::ComponentRunner {
  public:
   explicit DartRunner(sys::ComponentContext* context);
   ~DartRunner() override;
@@ -26,9 +28,17 @@ class DartRunner : public fuchsia::sys::Runner {
       ::fidl::InterfaceRequest<fuchsia::sys::ComponentController> controller)
       override;
 
+  // |fuchsia::component::runner::ComponentRunner|
+  void Start(
+      fuchsia::component::runner::ComponentStartInfo start_info,
+      fidl::InterfaceRequest<fuchsia::component::runner::ComponentController>
+          controller) override;
+
   // Not owned by DartRunner.
   sys::ComponentContext* context_;
   fidl::BindingSet<fuchsia::sys::Runner> bindings_;
+  fidl::BindingSet<fuchsia::component::runner::ComponentRunner>
+      component_runner_bindings_;
 
 #if !defined(AOT_RUNTIME)
   dart_utils::MappedResource vm_snapshot_data_;
