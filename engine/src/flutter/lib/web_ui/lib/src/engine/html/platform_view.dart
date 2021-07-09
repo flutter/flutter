7f4a254e762806844259a340930b62f106c386f0
Mouad Debbar <mouad.debbar@gmail.com>
2021-07-09 12:26:01 -0700

    [web] Librarify html renderer files (flutter/engine#27232)

--- engine/src/flutter/lib/web_ui/lib/src/engine/html/platform_view.dart
+++ engine/src/flutter/lib/web_ui/lib/src/engine/html/platform_view.dart
@@ -2,7 +2,11 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-part of engine;
+import 'dart:html' as html;
+
+import 'package:ui/src/engine.dart' show createPlatformViewSlot;
+
+import 'surface.dart';
 
 /// A surface containing a platform view, which is an HTML element.
 class PersistedPlatformView extends PersistedLeafSurface {
@@ -18,9 +22,6 @@ class PersistedPlatformView extends PersistedLeafSurface {
   html.Element createElement() {
     return createPlatformViewSlot(viewId);
   }
-
-  @override
-  Matrix4? get localTransformInverse => null;
 
   @override
   void apply() {
--- engine/src/flutter/lib/web_ui/lib/src/engine/picture.dart
+++ engine/src/flutter/lib/web_ui/lib/src/engine/picture.dart
@@ -2,7 +2,14 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-part of engine;
+import 'dart:async';
+import 'dart:html' as html;
+
+import 'package:ui/ui.dart' as ui;
+
+import 'html/bitmap_canvas.dart';
+import 'html/recording_canvas.dart';
+import 'html_image_codec.dart';
 
 /// An implementation of [ui.PictureRecorder] backed by a [RecordingCanvas].
 class EnginePictureRecorder implements ui.PictureRecorder {
