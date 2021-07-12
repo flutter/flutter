eea70f58fe6c0b55496347fd8f2b99408425195c
Chinmay Garde <chinmaygarde@google.com>
2021-07-11 17:49:59 -0700

    Generate code for simple named binders.

--- impeller/compositor/texture_descriptor.mm
+++ impeller/compositor/texture_descriptor.mm
@@ -6,44 +6,6 @@
 
 namespace impeller {
 
-std::optional<PixelFormat> FormatForImageResultComponents(Image::Format comp) {
-  switch (comp) {
-    case Image::Format::Invalid:
-      return std::nullopt;
-    case Image::Format::Grey:
-      return std::nullopt;
-    case Image::Format::GreyAlpha:
-      return std::nullopt;
-    case Image::Format::RGB:
-      return std::nullopt;
-    case Image::Format::RGBA:
-      return PixelFormat::kPixelFormat_R8G8B8A8_UNormInt;
-  }
-  return std::nullopt;
-}
-
-std::optional<TextureDescriptor> TextureDescriptor::MakeFromImageResult(
-    const Image& result) {
-  if (!result.IsValid()) {
-    return std::nullopt;
-  }
-
-  const auto pixel_format = FormatForImageResultComponents(result.GetFormat());
-  if (!pixel_format.has_value()) {
-    FML_DLOG(ERROR) << "Unknown image format.";
-    return std::nullopt;
-  }
-
-  TextureDescriptor desc;
-  desc.format = pixel_format.value();
-  desc.size = result.GetSize();
-  desc.mip_count = result.GetSize().MipCount();
-
-  if (!desc.IsValid()) {
-    return std::nullopt;
-  }
-
-  return desc;
-}
+//
 
 }  // namespace impeller
