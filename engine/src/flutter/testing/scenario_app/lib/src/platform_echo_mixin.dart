919219937bdebdc126383991202e5a883831998e
Zachary Anderson <zanderso@users.noreply.github.com>
2021-07-13 11:15:12 -0700

    Revert "NNBD migration for scenario_app (#27362)" (flutter/engine#27364)
    This reverts commit 43e7fb916c676e23ae47a39bf53f00bc9f16e498.

--- engine/src/flutter/testing/scenario_app/lib/src/platform_echo_mixin.dart
+++ engine/src/flutter/testing/scenario_app/lib/src/platform_echo_mixin.dart
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// @dart = 2.6
 import 'dart:typed_data';
 import 'dart:ui';
 
@@ -12,8 +13,8 @@ mixin PlatformEchoMixin on Scenario {
   @override
   void onPlatformMessage(
     String name,
-    ByteData? data,
-    PlatformMessageResponseCallback? callback,
+    ByteData data,
+    PlatformMessageResponseCallback callback,
   ) {
     window.sendPlatformMessage(name, data, null);
   }
