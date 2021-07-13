8d2f69819622f62b1d6df89c34e44ba30df2784b
Dan Field <dnfield@google.com>
2021-07-13 14:36:13 -0700

    Set ANDROID_HOME in run_gradle.py (flutter/engine#27371)

--- engine/src/flutter/testing/scenario_app/android/run_gradle.py
+++ engine/src/flutter/testing/scenario_app/android/run_gradle.py
@@ -11,13 +11,21 @@ import os
 import sys
 import subprocess
 
+SCRIPT_PATH = os.path.dirname(os.path.realpath(__file__))
+ANDROID_HOME = os.path.join(SCRIPT_PATH, '..', '..', '..', '..', 'third_party',
+    'android_tools', 'sdk')
+
 def main():
+  if not os.path.isdir(ANDROID_HOME):
+    raise Exception('%s (ANDROID_HOME) is not a directory' % ANDROID_HOME)
+
   BAT = '.bat' if sys.platform.startswith(('cygwin', 'win')) else ''
   android_dir = os.path.abspath(os.path.dirname(__file__))
   gradle_bin = os.path.join('.', 'gradlew%s' % BAT)
   result = subprocess.check_output(
     args=[gradle_bin] + sys.argv[1:],
     cwd=android_dir,
+    env=dict(os.environ, ANDROID_HOME=ANDROID_HOME),
   )
   return 0
 
