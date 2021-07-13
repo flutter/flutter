43e7fb916c676e23ae47a39bf53f00bc9f16e498
Dan Field <dnfield@google.com>
2021-07-13 10:56:06 -0700

    NNBD migration for scenario_app (flutter/engine#27362)

--- engine/src/flutter/testing/scenario_app/lib/src/scenario.dart
+++ engine/src/flutter/testing/scenario_app/lib/src/scenario.dart
@@ -2,7 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// @dart = 2.6
 import 'dart:typed_data';
 import 'dart:ui';
 
@@ -59,7 +58,7 @@ abstract class Scenario {
   /// See [PlatformDispatcher.onPlatformMessage].
   void onPlatformMessage(
     String name,
-    ByteData data,
-    PlatformMessageResponseCallback callback,
+    ByteData? data,
+    PlatformMessageResponseCallback? callback,
   ) {}
 }
