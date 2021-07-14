210e6240d90a6cc588833fcf3c6a2ca93cc1634f
Renzo-Olivares <rmolivares@renzo-olivares.dev>
2021-07-14 01:47:54 -0700

    Remove trailin space

--- packages/flutter/test/material/text_field_test.dart
+++ packages/flutter/test/material/text_field_test.dart
@@ -833,7 +833,6 @@ void main() {
         ),
       ),
     );
-    
     final EditableTextState editableTextState = tester.state(find.byType(EditableText));
 
     // Double tap to select the first word.
