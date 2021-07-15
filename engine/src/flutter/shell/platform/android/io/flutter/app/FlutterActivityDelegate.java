e71aa68555e2bd1a20fa5e7bab2f1ffd174003e2
kartik garasia <kgarasia1225@gmail.com>
2021-07-16 02:54:52 +0530

    JavaDoc error fix  (flutter/engine#27080)
    Javadoc error Local engine build fails while building android targets
    Related to issue #85346

--- engine/src/flutter/shell/platform/android/io/flutter/app/FlutterActivityDelegate.java
+++ engine/src/flutter/shell/platform/android/io/flutter/app/FlutterActivityDelegate.java
@@ -43,7 +43,7 @@ import java.util.ArrayList;
  * The most obvious example of when this may come in handy is if an application wishes to subclass
  * the Android v4 support library's {@code FragmentActivity}.
  *
- * <h3>Usage:</h3>
+ * <p><b>Usage:</b></p>
  *
  * <p>To wire this class up to your activity, simply forward the events defined in {@link
  * FlutterActivityEvents} from your activity to an instance of this class. Optionally, you can make
