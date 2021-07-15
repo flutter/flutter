a3dea1cc76cca90cd8f98a703e6a24f87f005484
Renzo-Olivares <rmolivares@renzo-olivares.dev>
2021-07-15 11:02:11 -0700

    Update test not to use select all on web

--- packages/flutter/test/material/text_field_test.dart
+++ packages/flutter/test/material/text_field_test.dart
@@ -846,10 +846,15 @@ void main() {
     expect(controller.selection.extentOffset, 7);
 
     // Use toolbar to select all text.
-    await tester.tap(find.text('Select all'));
-    await tester.pump();
-    expect(controller.selection.baseOffset, 0);
-    expect(controller.selection.extentOffset, controller.text.length);
+    if(isContextMenuProvidedByPlatform){
+      controller.selection = TextSelection(baseOffset: 0, extentOffset: controller.text.length);
+      expect(controller.selection.extentOffset, controller.text.length);
+    }else {
+      await tester.tap(find.text('Select all'));
+      await tester.pump();
+      expect(controller.selection.baseOffset, 0);
+      expect(controller.selection.extentOffset, controller.text.length);
+    }
 
     await expectLater(
       find.byType(MaterialApp),
