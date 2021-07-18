0d1d63125bc69fbe1565e2881807b7ee10da1b16
michaellee8 <ckmichael8@gmail.com>
2021-07-18 22:15:45 +0800

    feat: add methods for filtering

--- packages/flutter_tools/lib/src/base/json_helper.dart
+++ packages/flutter_tools/lib/src/base/json_helper.dart
@@ -11,13 +11,15 @@ import '../convert.dart';
 class JsonHelper {
   const JsonHelper(this._jsonObject);
 
-
-  factory JsonHelper.fromJson(String str){
+  /// Create a JsonHelper by parsing a json [String].
+  factory JsonHelper.fromJson(String str) {
     return JsonHelper(jsonDecode(str));
   }
 
   final dynamic _jsonObject;
 
+  /// Transverse the object by [String] key if such object is a [Map], or [int]
+  /// index if such object is a [List].
   JsonHelper operator [](dynamic keyOrIndex) {
     if (keyOrIndex is int) {
       final List<dynamic> l = _jsonObject as List<dynamic>;
@@ -30,6 +32,63 @@ class JsonHelper {
     throw const JsonHelperException('keyOrIndex must be int or String');
   }
 
+  /// Filter a [List] by its elements.
+  ///
+  /// If func throws a TypeError, it is treated as if it returns false.
+  JsonHelper filterList(bool Function(dynamic) func) {
+    final List<dynamic> arr = asList;
+    return JsonHelper(arr.where(_wrapTypeErrorToFalse<dynamic>(func)));
+  }
+
+  /// Filter a [Map] by its entries.
+  ///
+  /// If func throws a TypeError, it is treated as if it returns false.
+  JsonHelper filterMap(bool Function(MapEntry<String, dynamic>) func) {
+    final Map<String, dynamic> map = asMap;
+    return JsonHelper(
+      Map<String, dynamic>.fromEntries(map.entries.where(
+        _wrapTypeErrorToFalse<MapEntry<String, dynamic>>(func),
+      )),
+    );
+  }
+
+  /// Filter a [List] by its indices.
+  ///
+  /// If func throws a TypeError, it is treated as if it returns false.
+  JsonHelper filterByIndex(bool Function(int) func) {
+    final List<dynamic> arr = <dynamic>[];
+    final bool Function(int) f = _wrapTypeErrorToFalse<int>(func);
+    for (int i = 0; i < asList.length; i++) {
+      if (f(i)) {
+        arr.add(asList[i]);
+      }
+    }
+    return JsonHelper(arr);
+  }
+
+  /// Filter a [Map] by its keys.
+  ///
+  /// If func throws a TypeError, it is treated as if it returns false.
+  JsonHelper filterByKey(bool Function(String) func) {
+    final Map<String, dynamic> map = asMap;
+    final bool Function(String) wrappedFunc =
+        _wrapTypeErrorToFalse<String>(func);
+    bool f(MapEntry<String, dynamic> mn) => wrappedFunc(mn.key);
+    return JsonHelper(
+      Map<String, dynamic>.fromEntries(map.entries.where(f)),
+    );
+  }
+
+  bool Function(T) _wrapTypeErrorToFalse<T>(bool Function(T) func) {
+    return (T t) {
+      try {
+        return func(t);
+      } on TypeError {
+        return false;
+      }
+    };
+  }
+
   int get asInt => _jsonObject as int;
 
   double get asDouble => _jsonObject as double;
@@ -40,7 +99,16 @@ class JsonHelper {
 
   bool get asBool => _jsonObject as bool;
 
-  List<dynamic> get asList => _jsonObject as List<dynamic>;
+  List<dynamic> get asList {
+    if (_jsonObject is List<dynamic>) {
+      return _jsonObject as List<dynamic>;
+    }
+    if (_jsonObject is Iterable<dynamic>) {
+      final Iterable<dynamic> iter = _jsonObject as Iterable<dynamic>;
+      return iter.toList();
+    }
+    throw const JsonHelperException('jsonObject cannot be converted to List');
+  }
 
   Map<String, dynamic> get asMap => _jsonObject as Map<String, dynamic>;
 
--- packages/flutter_tools/test/general.shard/base/json_helper_test.dart
+++ packages/flutter_tools/test/general.shard/base/json_helper_test.dart
@@ -8,46 +8,92 @@ import '../../src/common.dart';
 
 void main() {
   group('JsonHelper', () {
-    test('can decode nested maps', () {
-      const String str = '{"a":{"b":1}}';
-      expect(JsonHelper.fromJson(str)['a']['b'].asInt, equals(1));
+    group('can decode', () {
+      test('nested maps', () {
+        const String str = '{"a":{"b":1}}';
+        expect(JsonHelper.fromJson(str)['a']['b'].asInt, equals(1));
+      });
+      test('nested arrays', () {
+        const String str = '[1, [2,3, [4,5]]]';
+        expect(JsonHelper.fromJson(str)[1][2][1].asInt, equals(5));
+      });
+      test('nested arrays and maps', () {
+        expect(
+          JsonHelper.fromJson('{"a":[1,2,{"b":"c"}]}')['a'][2]['b'].asString,
+          equals('c'),
+        );
+      });
+      test('json double', () {
+        final JsonHelper helper = JsonHelper.fromJson('0.1');
+        expect(helper.asDouble, equals(0.1));
+        expect(() => helper.asInt, throwsA(isA<TypeError>()));
+      });
+      test('json int', () {
+        final JsonHelper helper = JsonHelper.fromJson('1');
+        expect(helper.asInt, equals(1));
+        expect(() => helper.asDouble, throwsA(isA<TypeError>()));
+      });
+      test('json num', () {
+        final JsonHelper helper = JsonHelper.fromJson('1.1111');
+        expect(helper.asNum, equals(1.1111));
+      });
+      test('json string', () {
+        final JsonHelper helper = JsonHelper.fromJson('"str"');
+        expect(helper.asString, equals('str'));
+      });
+      test('json bool', () {
+        final JsonHelper helper = JsonHelper.fromJson('true');
+        expect(helper.asBool, equals(true));
+      });
+      test('json array', () {
+        final JsonHelper helper = JsonHelper.fromJson('[{},{},123]');
+        expect(helper.asList, isA<List<dynamic>>());
+      });
+      test('json object', () {
+        final JsonHelper helper = JsonHelper.fromJson('{"123":{},"abc":{}}');
+        expect(helper.asMap, isA<Map<String, dynamic>>());
+      });
     });
-    test('can decode nested arrays', () {
-      const String str = '[1, [2,3, [4,5]]]';
-      expect(JsonHelper.fromJson(str)[1][2][1].asInt, equals(5));
-    });
-    test('can decode nested arrays and maps', (){
-      expect(JsonHelper.fromJson('{"a":[1,2,{"b":"c"}]}')['a'][2]['b'].asString, equals('c'));
-    });
-    test('can decode json double', () {
-      final JsonHelper helper = JsonHelper.fromJson('0.1');
-      expect(helper.asDouble, equals(0.1));
-      expect(() => helper.asInt, throwsA(isA<TypeError>()));
-    });
-    test('can decode json int', () {
-      final JsonHelper helper = JsonHelper.fromJson('1');
-      expect(helper.asInt, equals(1));
-      expect(() => helper.asDouble, throwsA(isA<TypeError>()));
-    });
-    test('can decode json num', () {
-      final JsonHelper helper = JsonHelper.fromJson('1.1111');
-      expect(helper.asNum, equals(1.1111));
-    });
-    test('can decode json string', () {
-      final JsonHelper helper = JsonHelper.fromJson('"str"');
-      expect(helper.asString, equals('str'));
-    });
-    test('can decode json bool', () {
-      final JsonHelper helper = JsonHelper.fromJson('true');
-      expect(helper.asBool, equals(true));
-    });
-    test('can decode json array', () {
-      final JsonHelper helper = JsonHelper.fromJson('[{},{},123]');
-      expect(helper.asList, isA<List<dynamic>>());
-    });
-    test('can decode json object', () {
-      final JsonHelper helper = JsonHelper.fromJson('{"123":{},"abc":{}}');
-      expect(helper.asMap, isA<Map<String,dynamic>>());
+    group('can filter', () {
+      test('by list element', () {
+        final JsonHelper helper =
+            JsonHelper.fromJson('[1,0.1,3,4,"abc","def"]');
+        expect(
+          helper.filterList((dynamic p) => (p as int).isOdd).asList,
+          equals(<dynamic>[1, 3]),
+        );
+      });
+      test('by map entry', () {
+        final JsonHelper helper =
+            JsonHelper.fromJson('{"k1":"abc","a2":23,"k3":123}');
+        expect(
+          helper
+              .filterMap((MapEntry<String, dynamic> p) =>
+                  p.key.contains('k') && (p.value as int) > 100)
+              .asMap,
+          equals(<String, dynamic>{'k3': 123}),
+        );
+      });
+      test('by list index', () {
+        final JsonHelper helper =
+            JsonHelper.fromJson('[1,0.1,3,4,"abc","def"]');
+        expect(
+          helper.filterByIndex((int i) => i < 2).asList,
+          equals(<dynamic>[1, 0.1]),
+        );
+      });
+      test('by map key', () {
+        final JsonHelper helper =
+            JsonHelper.fromJson('{"k1":"abc","a2":123,"k3":123}');
+        expect(
+          helper
+              .filterByKey(
+                (String k) => k.contains('k'),
+              )
+              .asMap,
+          equals(<String, dynamic>{'k3': 123, 'k1': 'abc'}),
+        );
+      });
     });
   });
 }
