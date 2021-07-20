1636d2216050e9e136677533809d743c6116f054
Greg Spencer <gspencer@google.com>
2021-07-20 13:49:34 -0700

    Turn off test on Windows

--- packages/flutter_tools/test/integration.shard/cache_test.dart
+++ packages/flutter_tools/test/integration.shard/cache_test.dart
@@ -61,7 +61,9 @@ class FakeLogger extends Logger {
 
   @override
   Status startProgress(String message,
-      {String progressId, int progressIndicatorPadding = kDefaultStatusPadding}) {
+    {String progressId,
+    int progressIndicatorPadding = kDefaultStatusPadding,
+  }) {
     return SilentStatus(
       stopwatch: _stopwatchFactory.createStopwatch(),
     )..start();
@@ -81,25 +83,31 @@ class FakeLogger extends Logger {
   final Terminal terminal = Terminal.test(supportsColor: false, supportsEmoji: false);
 }
 
-final String dart =
-    fileSystem.path.join(getFlutterRoot(), 'bin', platform.isWindows ? 'dart.bat' : 'dart');
+final String dart = fileSystem.path
+    .join(getFlutterRoot(), 'bin', platform.isWindows ? 'dart.bat' : 'dart');
 
 void main() {
   group('Cache.lock', () {
-    testWithoutContext('should log a message to stderr when lock is not acquired', () async {
-      final String oldRoot = Cache.flutterRoot;
-      final Directory tempDir = fileSystem.systemTempDirectory.createTempSync('cache_test.');
-      final FakeLogger logger = FakeLogger();
-      try {
-        Cache.flutterRoot = tempDir.absolute.path;
-        final Cache cache = Cache.test(
-            fileSystem: fileSystem, processManager: FakeProcessManager.any(), logger: logger);
-        final File cacheFile = fileSystem
-            .file(fileSystem.path.join(Cache.flutterRoot, 'bin', 'cache', 'lockfile'))
-          ..createSync(recursive: true);
-        final File script = fileSystem
-            .file(fileSystem.path.join(Cache.flutterRoot, 'bin', 'cache', 'test_lock.dart'));
-        script.writeAsStringSync(r'''
+    // Windows locking is too flaky for this to work reliably.
+    if (!platform.isWindows) {
+      testWithoutContext(
+          'should log a message to stderr when lock is not acquired', () async {
+        final String oldRoot = Cache.flutterRoot;
+        final Directory tempDir = fileSystem.systemTempDirectory.createTempSync('cache_test.');
+        final FakeLogger logger = FakeLogger();
+        try {
+          Cache.flutterRoot = tempDir.absolute.path;
+          final Cache cache = Cache.test(
+            fileSystem: fileSystem,
+            processManager: FakeProcessManager.any(),
+            logger: logger,
+          );
+          final File cacheFile = fileSystem.file(fileSystem.path
+              .join(Cache.flutterRoot, 'bin', 'cache', 'lockfile'))
+            ..createSync(recursive: true);
+          final File script = fileSystem.file(fileSystem.path
+              .join(Cache.flutterRoot, 'bin', 'cache', 'test_lock.dart'));
+          script.writeAsStringSync(r'''
 import 'dart:async';
 import 'dart:io';
 
@@ -111,23 +119,26 @@ Future<void> main(List<String> args) async {
   exit(0);
 }
 ''');
-        final Process process = await const LocalProcessManager()
-            .start(<String>[dart, script.absolute.path, cacheFile.absolute.path]);
-        await Future<void>.delayed(const Duration(milliseconds: 500));
-        await cache.lock();
-        process.kill(io.ProcessSignal.sigkill);
-      } finally {
-        try {
-          tempDir.deleteSync(recursive: true);
-        } on FileSystemException {
-          // Ignore filesystem exceptions, because Windows has a poor file
-          // locking paradigm.
+          final Process process = await const LocalProcessManager().start(
+            <String>[dart, script.absolute.path, cacheFile.absolute.path],
+          );
+          await Future<void>.delayed(const Duration(milliseconds: 500));
+          await cache.lock();
+          process.kill(io.ProcessSignal.sigkill);
+        } finally {
+          try {
+            tempDir.deleteSync(recursive: true);
+          } on FileSystemException {
+            // Ignore filesystem exceptions when trying to delete tempdir.
+          }
+          Cache.flutterRoot = oldRoot;
         }
-        Cache.flutterRoot = oldRoot;
-      }
-      expect(logger.errors.single,
-          equals('Waiting for another flutter command to release the startup lock...'));
-      expect(logger.status, isEmpty);
-    });
+        expect(logger.status, isEmpty);
+        expect(
+          logger.errors.single,
+          equals('Waiting for another flutter command to release the startup lock...'),
+        );
+      });
+    }
   });
 }
