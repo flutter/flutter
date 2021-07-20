95461d288a6155faa88a43ac433637898831d37c
Chinmay Garde <chinmaygarde@google.com>
2021-07-20 13:14:18 -0700

    Add aiks image stubs.

---
+++ engine/src/flutter/impeller/aiks/image.cc
@@ -0,0 +1,13 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "flutter/impeller/aiks/image.h"
+
+namespace impeller {
+
+Image::Image() = default;
+
+Image::~Image() = default;
+
+}  // namespace impeller
