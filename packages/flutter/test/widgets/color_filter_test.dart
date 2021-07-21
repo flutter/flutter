c603b896a6edf3bd52ce8fa5182d94ee9e30cf51
Kate Lovett <katelovett@google.com>
2021-07-21 12:28:36 -0500

    Add tags

--- packages/flutter/test/widgets/color_filter_test.dart
+++ packages/flutter/test/widgets/color_filter_test.dart
@@ -2,6 +2,10 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// This file is run as part of a reduced test set in CI on Mac and Windows
+// machines.
+@Tags(<String>['reduced-test-set'])
+
 @TestOn('!chrome')
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
