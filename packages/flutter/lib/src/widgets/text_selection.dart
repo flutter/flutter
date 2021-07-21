685a0aeb1258cccd8e36667b913296785ab855d7
Renzo-Olivares <rmolivares@renzo-olivares.dev>
2021-07-21 11:51:53 -0700

    remove erroneous whitespace

--- packages/flutter/lib/src/widgets/text_selection.dart
+++ packages/flutter/lib/src/widgets/text_selection.dart
@@ -851,7 +851,7 @@ class _TextSelectionHandleOverlayState
       firstSelectedGraphemeExtent = selectedGraphemes.characters.first.length;
       lastSelectedGraphemeExtent = selectedGraphemes.characters.last.length;
     }
-    
+
     final Rect? startHandleRect = widget.renderObject.getRectForComposingRange(TextRange(start: widget.selection.start, end: widget.selection.start + firstSelectedGraphemeExtent));
     final Rect? endHandleRect = widget.renderObject.getRectForComposingRange(TextRange(start: widget.selection.end - lastSelectedGraphemeExtent, end: widget.selection.end));
 
