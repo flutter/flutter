afc8afab232f71f0789b4ad4d5e8a6ffc2dfefb9
Casey Rogers <caseycrogers@berkeley.edu>
2021-07-21 11:45:38 -0700

    removed unnecessary imports?

--- packages/flutter/lib/src/widgets/draggable_scrollable_sheet.dart
+++ packages/flutter/lib/src/widgets/draggable_scrollable_sheet.dart
@@ -6,8 +6,6 @@ import 'dart:math' as math;
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/gestures.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 
 import 'basic.dart';
 import 'framework.dart';
