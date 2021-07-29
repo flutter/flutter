4ac5989c9912a690dfda32324b95f13a1302fdc4
Tong Mu <dkwingsmt@gmail.com>
2021-07-29 14:27:15 -0700

    Safer code

--- packages/flutter/lib/src/services/raw_keyboard.dart
+++ packages/flutter/lib/src/services/raw_keyboard.dart
@@ -285,9 +285,7 @@ abstract class RawKeyEvent with Diagnosticable {
   /// Creates a concrete [RawKeyEvent] class from a message in the form received
   /// on the [SystemChannels.keyEvent] channel.
   factory RawKeyEvent.fromMessage(Map<String, dynamic> message) {
-    final RawKeyEventData data;
     String? character;
-
     RawKeyEventData _dataFromWeb() {
       final String? key = message['key'] as String?;
       if (key != null && key.isNotEmpty && key.length == 1) {
@@ -300,6 +298,8 @@ abstract class RawKeyEvent with Diagnosticable {
         metaState: message['metaState'] as int? ?? 0,
       );
     }
+
+    final RawKeyEventData data;
     if (kIsWeb) {
       data = _dataFromWeb();
     } else {
