8803824c641dcbe47526fe75b7621d634742444f
Chris Bracken <chris@bracken.jp>
2021-07-31 11:09:12 -0700

    Migrate all Python hashbangs to Python 3 (flutter/engine#27838)
    Migrates all `#!/usr/bin/env python` hashbang lines to use python3.
    
    Also updates the licence tool to treat python3 scripts on par with
    python 2 scripts.
    
    Issue: https://github.com/flutter/flutter/issues/83043

--- engine/src/flutter/build/git_revision.py
+++ engine/src/flutter/build/git_revision.py
@@ -1,13 +1,13 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 #
 # Copyright 2013 The Flutter Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
 """Get the Git HEAD revision of a specified Git repository."""
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
+
+
+
 
 import sys
 import subprocess
