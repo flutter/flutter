bf06b19b409b40c718798bdf276027b19aca61d0
Alexander Aprelev <aam@google.com>
2021-08-02 07:57:49 -0700

    Ensure correct child isolate initialization in enable-isolate-groups dart vm configuration. (#26011)
    * Ensure child isolate doesn't load kernel
    
    * Guard changes to isolate initialization by enable-isolate-groups flag check

--- runtime/isolate_configuration.cc
+++ runtime/isolate_configuration.cc
@@ -51,7 +51,9 @@ class KernelIsolateConfiguration : public IsolateConfiguration {
     if (DartVM::IsRunningPrecompiledCode()) {
       return false;
     }
-    return isolate.PrepareForRunningFromKernel(std::move(kernel_));
+    return isolate.PrepareForRunningFromKernel(std::move(kernel_),
+                                               /*child_isolate=*/false,
+                                               /*last_piece=*/true);
   }
 
   // |IsolateConfiguration|
@@ -98,7 +100,8 @@ class KernelListIsolateConfiguration final : public IsolateConfiguration {
       }
       const bool last_piece = i + 1 == resolved_kernel_pieces_.size();
       if (!isolate.PrepareForRunningFromKernel(
-              std::move(resolved_kernel_pieces_[i]), last_piece)) {
+              std::move(resolved_kernel_pieces_[i]), /*child_isolate=*/false,
+              last_piece)) {
         return false;
       }
     }
