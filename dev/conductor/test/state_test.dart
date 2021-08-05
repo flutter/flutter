784f19667d30f33debc672654f350fdf987a6e42
Christopher Fujino <christopherfujino@gmail.com>
2021-08-05 13:14:46 -0700

    finish test

--- dev/conductor/test/state_test.dart
+++ dev/conductor/test/state_test.dart
@@ -10,14 +10,59 @@ import 'package:file/memory.dart';
 import './common.dart';
 
 void main() {
-  test('writeStateToFile', () {
+  test('writeStateToFile() pretty-prints JSON with 2 spaces', () {
     final MemoryFileSystem fileSystem = MemoryFileSystem.test();
-    final File stateFile = fileSystem.file('/path/to/statefile.json');
-    final pb.ConductorState state = pb.ConductorState();
-    writeStateToFile(
-        stateFile,
-        state,
-        <String>[],
+    final File stateFile = fileSystem.file('/path/to/statefile.json')
+      ..createSync(recursive: true);
+    const String candidateBranch = 'flutter-2.3-candidate.0';
+    final pb.ConductorState state = pb.ConductorState(
+      releaseChannel: 'stable',
+      releaseVersion: '2.3.4',
+      incrementLevel: 'z',
+      engine: pb.Repository(
+        candidateBranch: candidateBranch,
+        upstream: pb.Remote(
+          name: 'upstream',
+          url: 'https://github.com/flutter/engine.git',
+        ),
+      ),
+      framework: pb.Repository(
+        candidateBranch: candidateBranch,
+        upstream: pb.Remote(
+          name: 'upstream',
+          url: 'https://github.com/flutter/flutter.git',
+        ),
+      ),
     );
+    writeStateToFile(
+      stateFile,
+      state,
+      <String>['[status] hello world'],
+    );
+    final String serializedState = stateFile.readAsStringSync();
+    const String expectedString = '''
+{
+  "releaseChannel": "stable",
+  "releaseVersion": "2.3.4",
+  "engine": {
+    "candidateBranch": "flutter-2.3-candidate.0",
+    "upstream": {
+      "name": "upstream",
+      "url": "https://github.com/flutter/engine.git"
+    }
+  },
+  "framework": {
+    "candidateBranch": "flutter-2.3-candidate.0",
+    "upstream": {
+      "name": "upstream",
+      "url": "https://github.com/flutter/flutter.git"
+    }
+  },
+  "logs": [
+    "[status] hello world"
+  ],
+  "incrementLevel": "z"
+}''';
+    expect(serializedState, expectedString);
   });
 }
