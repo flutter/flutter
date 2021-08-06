483cab138b8270647328c442dbb6742d3eacacac
Kate Lovett <katelovett@google.com>
2021-08-06 17:58:47 -0500

    Analyzer therapy

--- packages/flutter/lib/src/widgets/overscroll_indicator.dart
+++ packages/flutter/lib/src/widgets/overscroll_indicator.dart
@@ -738,7 +738,7 @@ class _StretchingOverscrollIndicatorState extends State<StretchingOverscrollIndi
         confirmationNotification.dispatch(context);
         _accepted = confirmationNotification._accepted;
       }
-      
+
       assert(notification.metrics.axis == widget.axis);
       if (_accepted) {
         if (notification.velocity != 0.0) {
