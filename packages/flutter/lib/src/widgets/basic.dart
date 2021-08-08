21c8c27035c88f061b18edd431421e1f58c70db4
Viren Khatri <werainkhatri.work@gmail.com>
2021-08-08 22:22:24 +0530

    refactoring sample code

--- packages/flutter/lib/src/widgets/basic.dart
+++ packages/flutter/lib/src/widgets/basic.dart
@@ -7157,7 +7157,7 @@ class IgnorePointer extends SingleChildRenderObjectWidget {
 ///         height: 100.0,
 ///         child: ElevatedButton(
 ///           onPressed: () {},
-///           child: null,
+///           child: Container(),
 ///         ),
 ///       ),
 ///       SizedBox(
@@ -7170,7 +7170,7 @@ class IgnorePointer extends SingleChildRenderObjectWidget {
 ///               primary: Colors.blue.shade200,
 ///             ),
 ///             onPressed: () {},
-///             child: null,
+///             child: Container(),
 ///           ),
 ///         ),
 ///       ),
