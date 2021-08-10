4696d1e923a2bfa47ab7a5428f2ee49de28f9600
LongCat is Looong <31859944+LongCatIsLooong@users.noreply.github.com>
2021-08-09 17:46:35 -0700

    [TextInput] minor fixes
    - Make the invalid `TextSelection` a singleton
    - Equality check does not account for text affinity unless it's
    collapsed and valid (which makes more sense according to its documentation).
    - Fix `EditableText.scrollController`'s lifecycle.
    - Dispose animation controllers

--- packages/flutter/test/material/text_field_test.dart
+++ packages/flutter/test/material/text_field_test.dart
@@ -3969,8 +3969,8 @@ void main() {
         error.toStringDeep(),
         equalsIgnoringHashCodes(
           'FlutterError\n'
-          '   invalid text selection: TextSelection(baseOffset: 10,\n'
-          '   extentOffset: 10, affinity: TextAffinity.downstream,\n'
+          '   invalid text selection: TextSelection.collapsed(offset: 10,\n'
+          '   affinity: TextAffinity.downstream,\n'
           '   isDirectional: false)\n',
         ),
       );
