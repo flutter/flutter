aa368d4e8b258aa84d076640644245c9dedfcf96
Kate Lovett <katelovett@google.com>
2021-08-11 14:46:35 -0500

    Rude IDE junk

--- packages/flutter/lib/src/widgets/overscroll_indicator.dart
+++ packages/flutter/lib/src/widgets/overscroll_indicator.dart
@@ -5,7 +5,6 @@
 import 'dart:async' show Timer;
 import 'dart:math' as math;
 
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/physics.dart' show nearEqual, Tolerance;
 import 'package:flutter/rendering.dart';
