4c754f371f74800fd784dfe4e4b40ce0cecfe4d7
nt4f04uNd <nt4f04uNd@gmail.com>
2021-08-17 23:51:12 +0300

    changed test name

--- packages/flutter/test/widgets/nested_scroll_view_test.dart
+++ packages/flutter/test/widgets/nested_scroll_view_test.dart
@@ -2445,7 +2445,7 @@ void main() {
     expect(position.pixels, 0.0);
   });
 
-  testWidgets('NestedScrollView respects precisionErrorTolerance while growing outer position', (WidgetTester tester) async {
+  testWidgets("NestedScrollView doesn't crash due to precision error", (WidgetTester tester) async {
     // Regression test for https://github.com/flutter/flutter/issues/63825
 
     await tester.pumpWidget(MaterialApp(
