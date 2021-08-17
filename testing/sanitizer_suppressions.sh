e9bf2994e7a64bea3cf894f7bb744e5a138ad8f6
Brandon DeRosier <bdero@google.com>
2021-08-17 14:34:24 -0700

    Use the suppressions script to source runtime suppressions in `run_tests.py` (#28140)

--- testing/sanitizer_suppressions.sh
+++ testing/sanitizer_suppressions.sh
@@ -1,16 +1,16 @@
-CURRENT_DIRECTORY="$(pwd)/$(dirname "$0")"
+TESTING_DIRECTORY=$(dirname "${BASH_SOURCE[0]}")
 
-TSAN_SUPPRESSIONS_FILE="${CURRENT_DIRECTORY}/tsan_suppressions.txt"
+TSAN_SUPPRESSIONS_FILE="${TESTING_DIRECTORY}/tsan_suppressions.txt"
 export TSAN_OPTIONS="suppressions=${TSAN_SUPPRESSIONS_FILE}"
 echo "Using Thread Sanitizer suppressions in ${TSAN_SUPPRESSIONS_FILE}"
 
-LSAN_SUPPRESSIONS_FILE="${CURRENT_DIRECTORY}/lsan_suppressions.txt"
+LSAN_SUPPRESSIONS_FILE="${TESTING_DIRECTORY}/lsan_suppressions.txt"
 export LSAN_OPTIONS="suppressions=${LSAN_SUPPRESSIONS_FILE}"
 echo "Using Leak Sanitizer suppressions in ${LSAN_SUPPRESSIONS_FILE}"
 
-UBSAN_SUPPRESSIONS_FILE="${CURRENT_DIRECTORY}/ubsan_suppressions.txt"
+UBSAN_SUPPRESSIONS_FILE="${TESTING_DIRECTORY}/ubsan_suppressions.txt"
 export UBSAN_OPTIONS="suppressions=${UBSAN_SUPPRESSIONS_FILE}"
 echo "Using Undefined Behavior suppressions in ${UBSAN_SUPPRESSIONS_FILE}"
 
 
-export ASAN_OPTIONS="detect_leaks=1"
+export ASAN_OPTIONS="detect_leaks=0:detect_container_overflow=0"
