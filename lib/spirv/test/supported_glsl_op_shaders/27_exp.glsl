6d6ce34330e23df5ae0eae2e36bf15573d728cf8
Anthony <591699+clocksmith@users.noreply.github.com>
2021-08-18 12:58:25 -0400

    Add SPIR-V FragmentShader API to painting.dart (#26996)
    Add SPIR-V FragmentShader API to painting.dart

---
+++ lib/spirv/test/supported_glsl_op_shaders/27_exp.glsl
@@ -0,0 +1,22 @@
+#version 320 es
+
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+precision highp float;
+
+layout(location = 0) out vec4 fragColor;
+
+layout(location = 0) uniform float a;
+
+void main() {
+    fragColor = vec4(
+        0.0,
+        // e^0.0 = 1.0
+        exp(a * 0.0),
+        0.0,
+        // e^2.0 - 6.38905609893 = 7.38905609893 - 6.38905609893 = 1.0
+        exp(a * 2.0) - 6.38905609893
+    );
+}
