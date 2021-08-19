d483dac399169bc185a118c9eb6bab220ced839d
godofredoc <54371434+godofredoc@users.noreply.github.com>
2021-08-18 18:03:31 -0700

    Define WEB_UI_DIR using ENGINE_PATH. (flutter/engine#28057)
    This is to minimize the number of environment variables being passed to
    scripts from recipes.

--- engine/src/flutter/lib/web_ui/dev/web_engine_analysis.sh
+++ engine/src/flutter/lib/web_ui/dev/web_engine_analysis.sh
@@ -8,7 +8,7 @@ set -x
 # See: https://flutter.googlesource.com/recipes/+/refs/heads/master/recipes/web_engine.py
 
 echo "Engine path $ENGINE_PATH"
-
+WEB_UI_DIR="$ENGINE_PATH/src/flutter/lib/web_ui"
 DART_SDK_DIR="${ENGINE_PATH}/src/out/host_debug_unopt/dart-sdk"
 DART_PATH="$DART_SDK_DIR/bin/dart"
 
