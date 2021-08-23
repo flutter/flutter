a858cceed82918945e1be37ea5fdb3c41d555c96
Chris Bracken <chris@bracken.jp>
2021-08-23 13:59:43 -0700

    Eliminate Android-specific Vulkan support (flutter/engine#28239)
    This eliminates Vulkan support for Android. As of flutter/engine#27980
    (684ad82) we are no longer running Android-Vulkan builds. This
    eliminates the --enable-vulkan flag from tools/gn and any code that is
    only reachable when that flag is enabled.
    
    Note that after this patch, `shell_enable_vulkan` is always false,
    however the //flutter/shell/gpu:gpu_surface_vulkan target and source
    files remain since they are still used when `test_enable_vulkan` is
    true, which is the case when `is_fuchsia` is true. Note that these files
    are *not* built as part of a regular fuchsia build (see the
    `shell_gpu_configuration` in //shell/platform/fuchsia/flutter/BUILD.gn),
    but may be enabled once the Fuchsia embedder is migrated to the Embedder
    API.
    
    Also updates TODOs to dworsham, who is the committer who'll be
    transitioning the Fuchsia embedder to the embedding API.

--- engine/src/flutter/shell/platform/android/platform_view_android.cc
+++ engine/src/flutter/shell/platform/android/platform_view_android.cc
@@ -18,10 +18,6 @@
 #include "flutter/shell/platform/android/surface/android_surface.h"
 #include "flutter/shell/platform/android/surface/snapshot_surface_producer.h"
 
-#if SHELL_ENABLE_VULKAN
-#include "flutter/shell/platform/android/android_surface_vulkan.h"
-#endif  // SHELL_ENABLE_VULKAN
-
 #include "flutter/shell/platform/android/context/android_context.h"
 #include "flutter/shell/platform/android/jni/platform_view_android_jni.h"
 #include "flutter/shell/platform/android/platform_message_response_android.h"
@@ -43,11 +39,6 @@ std::unique_ptr<AndroidSurface> AndroidSurfaceFactoryImpl::CreateSurface() {
                                                       jni_facade_);
     case AndroidRenderingAPI::kOpenGLES:
       return std::make_unique<AndroidSurfaceGL>(android_context_, jni_facade_);
-    case AndroidRenderingAPI::kVulkan:
-#if SHELL_ENABLE_VULKAN
-      return std::make_unique<AndroidSurfaceVulkan>(android_context_,
-                                                    jni_facade_);
-#endif  // SHELL_ENABLE_VULKAN
     default:
       FML_DCHECK(false);
       return nullptr;
@@ -63,13 +54,9 @@ static std::shared_ptr<flutter::AndroidContext> CreateAndroidContext(
   if (use_software_rendering) {
     return std::make_shared<AndroidContext>(AndroidRenderingAPI::kSoftware);
   }
-#if SHELL_ENABLE_VULKAN
-  return std::make_shared<AndroidContext>(AndroidRenderingAPI::kVulkan);
-#else   // SHELL_ENABLE_VULKAN
   return std::make_unique<AndroidContextGL>(
       AndroidRenderingAPI::kOpenGLES,
       fml::MakeRefCounted<AndroidEnvironmentGL>());
-#endif  // SHELL_ENABLE_VULKAN
 }
 
 PlatformViewAndroid::PlatformViewAndroid(
