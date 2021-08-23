2e4ece58b6a098253b6199953588411c8488647e
Keyong Han <keyonghan@google.com>
2021-08-23 16:16:58 -0700

    take care of null

--- dev/devicelab/test/metrics_center_test.dart
+++ dev/devicelab/test/metrics_center_test.dart
@@ -18,15 +18,25 @@ void main() {
           'average_frame_build_time_millis': 0.4550425531914895,
           '90th_percentile_frame_build_time_millis': 0.473
         },
-        'BenchmarkScoreKeys': <String>[
-          'average_frame_build_time_millis',
-          '90th_percentile_frame_build_time_millis'
-        ]
+        'BenchmarkScoreKeys': <String>['average_frame_build_time_millis', '90th_percentile_frame_build_time_millis']
       };
       final List<MetricPoint> metricPoints = parse(results);
 
       expect(metricPoints[0].value, equals(0.4550425531914895));
       expect(metricPoints[1].value, equals(0.473));
+    });
+
+    test('succeeds - null ResultData', () {
+      final Map<String, dynamic> results = <String, dynamic>{
+        'CommitBranch': 'master',
+        'CommitSha': 'abc',
+        'BuilderName': 'test',
+        'ResultData': null,
+        'BenchmarkScoreKeys': null
+      };
+      final List<MetricPoint> metricPoints = parse(results);
+
+      expect(metricPoints.length, 0);
     });
   });
 }
