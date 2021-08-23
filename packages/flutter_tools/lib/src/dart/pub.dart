0623868fce95a32205b87fa2eb530781d987d091
Sigurd Meldgaard <sigurdm@google.com>
2021-08-23 13:02:49 +0000

    Don't send analytics for the dart tool

--- packages/flutter_tools/lib/src/dart/pub.dart
+++ packages/flutter_tools/lib/src/dart/pub.dart
@@ -426,7 +426,7 @@ class _DefaultPub implements Pub {
         'permissions for the current user.'
       );
     }
-    return <String>[sdkPath, 'pub', ...arguments];
+    return <String>[sdkPath, '--no-analytics', 'pub', ...arguments];
   }
 
   // Returns the environment value that should be used when running pub.
