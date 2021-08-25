29413aa16bbe73afbfc3efa2e58f5274b8e650ce
Gary Qian <garyq@google.com>
2021-08-25 16:34:08 -0700

    Configurable adb

--- dev/integration_tests/deferred_components_test/run_release_test.sh
+++ dev/integration_tests/deferred_components_test/run_release_test.sh
@@ -4,9 +4,9 @@
 # found in the LICENSE file.
 
 # Store the time to prevent capturing logs from previous runs.
-script_start_time=$(adb shell 'date +"%m-%d %H:%M:%S.0"')
+script_start_time=$($2 shell 'date +"%m-%d %H:%M:%S.0"')
 
-adb uninstall "io.flutter.integration.deferred_components_test"
+$2 uninstall "io.flutter.integration.deferred_components_test"
 
 rm -f build/app/outputs/bundle/release/app-release.apks
 rm -f build/app/outputs/bundle/release/run_logcat.log
@@ -16,12 +16,12 @@ flutter build appbundle
 java -jar $1 build-apks --bundle=build/app/outputs/bundle/release/app-release.aab --output=build/app/outputs/bundle/release/app-release.apks --local-testing
 java -jar $1 install-apks --apks=build/app/outputs/bundle/release/app-release.apks
 
-adb shell "
+$2 shell "
 am start -n io.flutter.integration.deferred_components_test/.MainActivity
 sleep 12
 exit
 "
-adb logcat -d -t "$script_start_time" -s "flutter" > build/app/outputs/bundle/release/run_logcat.log
+$2 logcat -d -t "$script_start_time" -s "flutter" > build/app/outputs/bundle/release/run_logcat.log
 echo ""
 if cat build/app/outputs/bundle/release/run_logcat.log | grep -q "Running deferred code"; then
   echo "All tests passed."
