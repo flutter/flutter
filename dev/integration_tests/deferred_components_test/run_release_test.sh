39539db023180ed80af3834122b2adccc10e6ad8
Gary Qian <garyq@google.com>
2021-08-30 13:54:47 -0700

    Assign params to variables for readability

--- dev/integration_tests/deferred_components_test/run_release_test.sh
+++ dev/integration_tests/deferred_components_test/run_release_test.sh
@@ -14,25 +14,28 @@
 # The modified bundletool which waives the density requirement is at:
 # https://chrome-infra-packages.appspot.com/p/flutter/android/bundletool/+/vFt1jA0cUeZLmUCVR5NG2JVB-SgJ18GH_pVYKMOlfUIC
 
-# Store the time to prevent capturing logs from previous runs.
-script_start_time=$($2 shell 'date +"%m-%d %H:%M:%S.0"')
+bundletool_jar_path=$1
+adb_path=$2
 
-$2 uninstall "io.flutter.integration.deferred_components_test"
+# Store the time to prevent capturing logs from previous runs.
+script_start_time=$($adb_path shell 'date +"%m-%d %H:%M:%S.0"')
+
+$adb_path uninstall "io.flutter.integration.deferred_components_test"
 
 rm -f build/app/outputs/bundle/release/app-release.apks
 rm -f build/app/outputs/bundle/release/run_logcat.log
 
 flutter build appbundle
 
-java -jar $1 build-apks --bundle=build/app/outputs/bundle/release/app-release.aab --output=build/app/outputs/bundle/release/app-release.apks --local-testing
-java -jar $1 install-apks --apks=build/app/outputs/bundle/release/app-release.apks
+java -jar $bundletool_jar_path build-apks --bundle=build/app/outputs/bundle/release/app-release.aab --output=build/app/outputs/bundle/release/app-release.apks --local-testing
+java -jar $bundletool_jar_path install-apks --apks=build/app/outputs/bundle/release/app-release.apks
 
-$2 shell "
+$adb_path shell "
 am start -n io.flutter.integration.deferred_components_test/.MainActivity
 sleep 12
 exit
 "
-$2 logcat -d -t "$script_start_time" -s "flutter" > build/app/outputs/bundle/release/run_logcat.log
+$adb_path logcat -d -t "$script_start_time" -s "flutter" > build/app/outputs/bundle/release/run_logcat.log
 echo ""
 if cat build/app/outputs/bundle/release/run_logcat.log | grep -q "Running deferred code"; then
   echo "All tests passed."
