efbc5bed9b6739bafe6d7d0be00d93e9515350c1
creativecreatorormaybenot <creativecreatorormaybenot@gmail.com>
2021-09-04 18:46:49 +0000

    Remove imports

--- packages/flutter/lib/src/material/radio_theme.dart
+++ packages/flutter/lib/src/material/radio_theme.dart
@@ -4,11 +4,11 @@
 
 import 'dart:ui' show lerpDouble;
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
 
-import 'constants.dart';
 import 'material_state.dart';
-import 'radio.dart';
 import 'theme.dart';
 import 'theme_data.dart';
 
