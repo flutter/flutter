f16c9d311165d674cc3ef7e621e100bece9f7b1f
Renzo-Olivares <rmolivares@renzo-olivares.dev>
2021-09-07 14:12:17 -0700

    updates

--- packages/flutter/lib/src/services/text_input.dart
+++ packages/flutter/lib/src/services/text_input.dart
@@ -801,7 +801,7 @@ abstract class TextEditingDelta with TextEditingDeltaUtils {
     final int tbEnd = tb.length;
 
     // This delta is explicitly a non text update.
-    bool isNonTextUpdate = start == -1 && start == end;
+    final bool isNonTextUpdate = start == -1 && start == end;
     final TextRange newComposing = TextRange(
       start: encoded['composingBase'] as int? ?? -1,
       end: encoded['composingExtent'] as int? ?? -1,
