9c832a1320adb59ce7c282e4f249c52edab867e5
nt4f04uNd <nt4f04uNd@gmail.com>
2021-09-09 23:12:02 +0300

    Correct outdated weak_ptr docs (flutter/engine#28356)

--- engine/src/flutter/fml/memory/weak_ptr.h
+++ engine/src/flutter/fml/memory/weak_ptr.h
@@ -246,8 +246,8 @@ class WeakPtrFactory {
     flag_->Invalidate();
   }
 
-  // Gets a new weak pointer, which will be valid until either
-  // |InvalidateWeakPtrs()| is called or this object is destroyed.
+  // Gets a new weak pointer, which will be valid until this object is
+  // destroyed.
   WeakPtr<T> GetWeakPtr() const {
     return WeakPtr<T>(ptr_, flag_.Clone(), checker_);
   }
@@ -282,8 +282,8 @@ class TaskRunnerAffineWeakPtrFactory {
     flag_->Invalidate();
   }
 
-  // Gets a new weak pointer, which will be valid until either
-  // |InvalidateWeakPtrs()| is called or this object is destroyed.
+  // Gets a new weak pointer, which will be valid until this object is
+  // destroyed.
   TaskRunnerAffineWeakPtr<T> GetWeakPtr() const {
     return TaskRunnerAffineWeakPtr<T>(ptr_, flag_.Clone(), checker_);
   }
