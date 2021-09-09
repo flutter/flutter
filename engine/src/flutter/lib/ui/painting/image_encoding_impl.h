d8d7fa38dbbe95f053c9e19929d0e26135a68b7f
ColdPaleLight <31977171+ColdPaleLight@users.noreply.github.com>
2021-09-10 00:42:02 +0800

    Prevent app from accessing the GPU in the background in EncodeImage (flutter/engine#28369)

---
+++ engine/src/flutter/lib/ui/painting/image_encoding_impl.h
@@ -0,0 +1,57 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef FLUTTER_LIB_UI_PAINTING_IMAGE_ENCODING_IMPL_H_
+#define FLUTTER_LIB_UI_PAINTING_IMAGE_ENCODING_IMPL_H_
+
+#include "flutter/lib/ui/ui_dart_state.h"
+#include "third_party/skia/include/core/SkCanvas.h"
+#include "third_party/skia/include/core/SkImage.h"
+#include "third_party/skia/include/core/SkSurface.h"
+
+namespace flutter {
+
+template <typename SyncSwitch>
+sk_sp<SkImage> ConvertToRasterUsingResourceContext(
+    sk_sp<SkImage> image,
+    fml::WeakPtr<GrDirectContext> resource_context,
+    const std::shared_ptr<const SyncSwitch>& is_gpu_disabled_sync_switch) {
+  sk_sp<SkSurface> surface;
+  SkImageInfo surface_info = SkImageInfo::MakeN32Premul(image->dimensions());
+
+  is_gpu_disabled_sync_switch->Execute(
+      typename SyncSwitch::Handlers()
+          .SetIfTrue([&surface, &surface_info] {
+            surface = SkSurface::MakeRaster(surface_info);
+          })
+          .SetIfFalse([&surface, &surface_info, resource_context] {
+            if (resource_context) {
+              surface = SkSurface::MakeRenderTarget(
+                  resource_context.get(), SkBudgeted::kNo, surface_info);
+            } else {
+              surface = SkSurface::MakeRaster(surface_info);
+            }
+          }));
+
+  if (surface == nullptr || surface->getCanvas() == nullptr) {
+    FML_LOG(ERROR) << "Could not create a surface to copy the texture into.";
+    return nullptr;
+  }
+
+  surface->getCanvas()->drawImage(image, 0, 0);
+  surface->getCanvas()->flush();
+
+  auto snapshot = surface->makeImageSnapshot();
+
+  if (snapshot == nullptr) {
+    FML_LOG(ERROR) << "Could not snapshot image to encode.";
+    return nullptr;
+  }
+
+  return snapshot->makeRasterImage();
+}
+
+}  // namespace flutter
+
+#endif  // FLUTTER_LIB_UI_PAINTING_IMAGE_ENCODING_IMPL_H_
--- engine/src/flutter/lib/ui/painting/image_encoding_unittests.cc
+++ engine/src/flutter/lib/ui/painting/image_encoding_unittests.cc
@@ -3,13 +3,16 @@
 // found in the LICENSE file.
 
 #include "flutter/lib/ui/painting/image_encoding.h"
+#include "flutter/lib/ui/painting/image_encoding_impl.h"
 
 #include "flutter/common/task_runners.h"
 #include "flutter/fml/synchronization/waitable_event.h"
+#include "flutter/lib/ui/painting/image.h"
 #include "flutter/runtime/dart_vm.h"
 #include "flutter/shell/common/shell_test.h"
 #include "flutter/shell/common/thread_host.h"
 #include "flutter/testing/testing.h"
+#include "gmock/gmock.h"
 
 namespace flutter {
 namespace testing {
@@ -18,8 +21,27 @@ namespace {
 fml::AutoResetWaitableEvent message_latch;
 };
 
+class MockSyncSwitch {
+ public:
+  struct Handlers {
+    Handlers& SetIfTrue(const std::function<void()>& handler) {
+      true_handler = std::move(handler);
+      return *this;
+    }
+    Handlers& SetIfFalse(const std::function<void()>& handler) {
+      false_handler = std::move(handler);
+      return *this;
+    }
+    std::function<void()> true_handler = [] {};
+    std::function<void()> false_handler = [] {};
+  };
+
+  MOCK_CONST_METHOD1(Execute, void(const Handlers& handlers));
+  MOCK_METHOD1(SetSwitch, void(bool value));
+};
+
 TEST_F(ShellTest, EncodeImageGivesExternalTypedData) {
-  auto nativeEncodeImage = [&](Dart_NativeArguments args) {
+  auto native_encode_image = [&](Dart_NativeArguments args) {
     auto image_handle = Dart_GetNativeArgument(args, 0);
     image_handle =
         Dart_GetField(image_handle, Dart_NewStringFromCString("_image"));
@@ -59,9 +81,74 @@ TEST_F(ShellTest, EncodeImageGivesExternalTypedData) {
                            CreateNewThread()        // io
   );
 
-  AddNativeCallback("EncodeImage", CREATE_NATIVE_ENTRY(nativeEncodeImage));
+  AddNativeCallback("EncodeImage", CREATE_NATIVE_ENTRY(native_encode_image));
   AddNativeCallback("ValidateExternal",
                     CREATE_NATIVE_ENTRY(nativeValidateExternal));
+
+  std::unique_ptr<Shell> shell =
+      CreateShell(std::move(settings), std::move(task_runners));
+
+  ASSERT_TRUE(shell->IsSetup());
+  auto configuration = RunConfiguration::InferFromSettings(settings);
+  configuration.SetEntrypoint("encodeImageProducesExternalUint8List");
+
+  shell->RunEngine(std::move(configuration), [&](auto result) {
+    ASSERT_EQ(result, Engine::RunStatus::Success);
+  });
+
+  message_latch.Wait();
+  DestroyShell(std::move(shell), std::move(task_runners));
+}
+
+TEST_F(ShellTest, EncodeImageAccessesSyncSwitch) {
+  Settings settings = CreateSettingsForFixture();
+  TaskRunners task_runners("test",                  // label
+                           GetCurrentTaskRunner(),  // platform
+                           CreateNewThread(),       // raster
+                           CreateNewThread(),       // ui
+                           CreateNewThread()        // io
+  );
+
+  auto native_encode_image = [&](Dart_NativeArguments args) {
+    auto image_handle = Dart_GetNativeArgument(args, 0);
+    image_handle =
+        Dart_GetField(image_handle, Dart_NewStringFromCString("_image"));
+    ASSERT_FALSE(Dart_IsError(image_handle)) << Dart_GetError(image_handle);
+    ASSERT_FALSE(Dart_IsNull(image_handle));
+    auto format_handle = Dart_GetNativeArgument(args, 1);
+
+    intptr_t peer = 0;
+    Dart_Handle result = Dart_GetNativeInstanceField(
+        image_handle, tonic::DartWrappable::kPeerIndex, &peer);
+    ASSERT_FALSE(Dart_IsError(result));
+    CanvasImage* canvas_image = reinterpret_cast<CanvasImage*>(peer);
+
+    int64_t format = -1;
+    result = Dart_IntegerToInt64(format_handle, &format);
+    ASSERT_FALSE(Dart_IsError(result));
+
+    auto io_manager = UIDartState::Current()->GetIOManager();
+    fml::AutoResetWaitableEvent latch;
+
+    task_runners.GetIOTaskRunner()->PostTask([&]() {
+      auto is_gpu_disabled_sync_switch =
+          std::make_shared<const MockSyncSwitch>();
+      EXPECT_CALL(*is_gpu_disabled_sync_switch, Execute)
+          .WillOnce([](const MockSyncSwitch::Handlers& handlers) {
+            handlers.true_handler();
+          });
+      ConvertToRasterUsingResourceContext(canvas_image->image(),
+                                          io_manager->GetResourceContext(),
+                                          is_gpu_disabled_sync_switch);
+      latch.Signal();
+    });
+
+    latch.Wait();
+
+    message_latch.Signal();
+  };
+
+  AddNativeCallback("EncodeImage", CREATE_NATIVE_ENTRY(native_encode_image));
 
   std::unique_ptr<Shell> shell =
       CreateShell(std::move(settings), std::move(task_runners));
