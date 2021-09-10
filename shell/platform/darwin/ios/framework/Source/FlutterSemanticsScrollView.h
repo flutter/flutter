48ce284dff9cd876486542b0c8df850c9ffd5903
chunhtai <47866232+chunhtai@users.noreply.github.com>
2021-09-10 11:32:02 -0700

    Fixes accessibility issue that bridge fails to clean up FlutterScrollableSemanticsObject (#28495)

---
+++ shell/platform/darwin/ios/framework/Source/FlutterSemanticsScrollView.h
@@ -0,0 +1,30 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef SHELL_PLATFORM_IOS_FRAMEWORK_SOURCE_FLUTTER_SEMANTICS_SCROLL_VIEW_H_
+#define SHELL_PLATFORM_IOS_FRAMEWORK_SOURCE_FLUTTER_SEMANTICS_SCROLL_VIEW_H_
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+@class SemanticsObject;
+
+/**
+ * A UIScrollView to represent Flutter scrollable in iOS accessibility
+ * services.
+ *
+ * This class is hidden from the user and can't be interacted with. It
+ * sends all of selector calls from accessibility services to the
+ * owner SemanticsObject.
+ */
+@interface FlutterSemanticsScrollView : UIScrollView
+
+- (instancetype)init NS_UNAVAILABLE;
+- (instancetype)initWithFrame:(CGRect)frame NS_UNAVAILABLE;
+- (instancetype)initWithCoder:(NSCoder*)coder NS_UNAVAILABLE;
+- (instancetype)initWithSemanticsObject:(SemanticsObject*)semanticsObject;
+
+@end
+NS_ASSUME_NONNULL_END
+#endif  // SHELL_PLATFORM_IOS_FRAMEWORK_SOURCE_FLUTTER_SEMANTICS_SCROLL_VIEW_H_
