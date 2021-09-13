e10e8ca58e80ac4a29192a147ab26e226f7fa91b
Emmanuel Garcia <egarciad@google.com>
2021-09-13 15:31:54 -0700

    Don't use rFD in pre-Q versions (flutter/engine#28593)

--- engine/src/flutter/shell/platform/android/test/io/flutter/embedding/android/FlutterActivityTest.java
+++ engine/src/flutter/shell/platform/android/test/io/flutter/embedding/android/FlutterActivityTest.java
@@ -21,6 +21,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -438,6 +439,32 @@ public class FlutterActivityTest {
     assertNull(splashScreen);
   }
 
+  @Test
+  public void fullyDrawn() {
+    Intent intent =
+        FlutterActivityWithReportFullyDrawn.createDefaultIntent(RuntimeEnvironment.application);
+    ActivityController<FlutterActivityWithReportFullyDrawn> activityController =
+        Robolectric.buildActivity(FlutterActivityWithReportFullyDrawn.class, intent);
+    FlutterActivityWithReportFullyDrawn flutterActivity = activityController.get();
+
+    // See https://github.com/flutter/flutter/issues/46172, and
+    // https://github.com/flutter/flutter/issues/88767.
+    for (int version = Build.VERSION_CODES.JELLY_BEAN; version < Build.VERSION_CODES.Q; version++) {
+      TestUtils.setApiVersion(version);
+      flutterActivity.onFlutterUiDisplayed();
+      assertFalse(
+          "reportFullyDrawn isn't used in API level " + version, flutterActivity.isFullyDrawn());
+    }
+
+    for (int version = Build.VERSION_CODES.Q; version < Build.VERSION_CODES.S; version++) {
+      TestUtils.setApiVersion(version);
+      flutterActivity.onFlutterUiDisplayed();
+      assertTrue(
+          "reportFullyDrawn is used in API level " + version, flutterActivity.isFullyDrawn());
+      flutterActivity.resetFullyDrawn();
+    }
+  }
+
   static class FlutterActivityWithProvidedEngine extends FlutterActivity {
     @Override
     @SuppressLint("MissingSuperCall")
@@ -475,6 +502,23 @@ public class FlutterActivityTest {
     @Override
     public RenderMode getRenderMode() {
       return RenderMode.texture;
+    }
+  }
+
+  private static class FlutterActivityWithReportFullyDrawn extends FlutterActivity {
+    private boolean fullyDrawn = false;
+
+    @Override
+    public void reportFullyDrawn() {
+      fullyDrawn = true;
+    }
+
+    public boolean isFullyDrawn() {
+      return fullyDrawn;
+    }
+
+    public void resetFullyDrawn() {
+      fullyDrawn = false;
     }
   }
 
