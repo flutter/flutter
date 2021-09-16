3c8e59646b1876cd3f59753043474030b3ba1503
Renzo-Olivares <rmolivares@renzo-olivares.dev>
2021-09-16 12:28:22 -0700

    updates

--- packages/flutter/test/services/delta_text_input_test.dart
+++ packages/flutter/test/services/delta_text_input_test.dart
@@ -33,49 +33,6 @@ void main() {
     test(
       'DeltaTextInputClient send the correct configuration to the platform and responds to updateEditingValueWithDeltas method correctly',
       () async {
-        final FakeAutofillClient client1 = FakeAutofillClient(const TextEditingValue(text: 'test1'));
-        final FakeAutofillClient client2 = FakeAutofillClient(const TextEditingValue(text: 'test2'));
-
-        client1.textInputConfiguration = TextInputConfiguration(
-          autofillConfiguration: AutofillConfiguration(
-            uniqueIdentifier: client1.autofillId,
-            autofillHints: const <String>['client1'],
-            currentEditingValue: client1.currentTextEditingValue,
-          ),
-        );
-
-        client2.textInputConfiguration = TextInputConfiguration(
-          autofillConfiguration: AutofillConfiguration(
-            uniqueIdentifier: client2.autofillId,
-            autofillHints: const <String>['client2'],
-            currentEditingValue: client2.currentTextEditingValue,
-          ),
-        );
-
-        scope.register(client1);
-        scope.register(client2);
-        client1.currentAutofillScope = scope;
-        client2.currentAutofillScope = scope;
-
-        scope.attach(client1, client1.textInputConfiguration);
-
-        final Map<String, dynamic> expectedConfiguration = client1.textInputConfiguration.toJson();
-        expectedConfiguration['fields'] = <Map<String, dynamic>>[
-          client1.textInputConfiguration.toJson(),
-          client2.textInputConfiguration.toJson(),
-        ];
-
-        fakeTextChannel.validateOutgoingMethodCalls(<MethodCall>[
-          MethodCall('TextInput.setClient', <dynamic>[1, expectedConfiguration]),
-        ]);
-
-        const TextEditingValue text2 = TextEditingValue(text: 'Text 2');
-        fakeTextChannel.incoming?.call(MethodCall(
-          'TextInputClient.updateEditingStateWithTag',
-          <dynamic>[0, <String, dynamic>{ client2.autofillId : text2.toJSON() }],
-        ));
-
-        expect(client2.currentTextEditingValue, text2);
       },
     );
   });
