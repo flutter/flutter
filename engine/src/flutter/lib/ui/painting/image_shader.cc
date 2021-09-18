d0e2dc2d028c92676c34d6339bb265fcfda06fdf
Jason Simmons <jason-simmons@users.noreply.github.com>
2021-09-17 20:18:02 -0700

    Wrap ImageShader::sk_image_ with a SkiaGPUObject (flutter/engine#28698)

--- engine/src/flutter/lib/ui/painting/image_shader.cc
+++ engine/src/flutter/lib/ui/painting/image_shader.cc
@@ -45,7 +45,7 @@ void ImageShader::initWithImage(CanvasImage* image,
         ToDart("ImageShader constructor called with non-genuine Image."));
     return;
   }
-  sk_image_ = image->image();
+  sk_image_ = UIDartState::CreateGPUObject(image->image());
   tmx_ = tmx;
   tmy_ = tmy;
   local_matrix_ = ToSkMatrix(matrix4);
@@ -63,8 +63,9 @@ sk_sp<SkShader> ImageShader::shader(SkSamplingOptions sampling) {
   }
   if (!cached_shader_.skia_object() || cached_sampling_ != sampling) {
     cached_sampling_ = sampling;
-    cached_shader_ = UIDartState::CreateGPUObject(
-        sk_image_->makeShader(tmx_, tmy_, sampling, &local_matrix_));
+    cached_shader_ =
+        UIDartState::CreateGPUObject(sk_image_.skia_object()->makeShader(
+            tmx_, tmy_, sampling, &local_matrix_));
   }
   return cached_shader_.skia_object();
 }
