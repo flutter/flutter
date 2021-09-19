3d7de8019fb316377cd25e21bde3160b988029a9
Pierre-Louis <plg@google.com>
2021-09-19 19:55:29 +0200

    x

--- packages/flutter_tools/lib/src/commands/daemon.dart
+++ packages/flutter_tools/lib/src/commands/daemon.dart
@@ -50,6 +50,9 @@ class DaemonCommand extends FlutterCommand {
   final String description = 'Run a persistent, JSON-RPC based server to communicate with devices.';
 
   @override
+  final String category = FlutterCommandCategories.tools;
+  
+  @override
   final bool hidden;
 
   @override
