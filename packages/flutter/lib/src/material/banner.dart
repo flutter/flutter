933841f4e7827c0e395d7bef041656784128c76d
Calamity210 <aayanraja210@gmail.com>
2021-09-20 09:48:46 -0400

    Correct material import

--- packages/flutter/lib/src/material/banner.dart
+++ packages/flutter/lib/src/material/banner.dart
@@ -2,12 +2,11 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:flutter/src/material/material.dart';
 import 'package:flutter/widgets.dart';
 
-import '../../material.dart';
 import 'banner_theme.dart';
 import 'divider.dart';
+import 'material.dart';
 import 'scaffold.dart';
 import 'theme.dart';
 
