495c1eac71bc6d60e574388da746dbb293c4979e
Christopher Fujino <christopherfujino@gmail.com>
2021-09-23 12:26:41 -0700

    cleanup ui app

--- dev/conductor/ui/test/widget_test.dart
+++ dev/conductor/ui/test/widget_test.dart
@@ -1,9 +1,6 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility that Flutter provides. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
 import 'package:conductor_ui/main.dart';
 import 'package:conductor_core/proto.dart' as pb;
@@ -16,10 +13,8 @@ void main() {
     final pb.ConductorState state = pb.ConductorState(
       releaseChannel: channelName,
     );
-    // Build our app and trigger a frame.
     await tester.pumpWidget(MyApp(state));
 
-    // Verify that our counter starts at 0.
     expect(find.text('Flutter Conductor'), findsOneWidget);
     expect(find.textContaining(channelName), findsOneWidget);
     expect(find.text('1'), findsNothing);
