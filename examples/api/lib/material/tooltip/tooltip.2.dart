da4fce1dc4f03518cb46d87ff66f4c5193f988b6
Kenzie Schmoll <kenzieschmoll@google.com>
2021-09-24 10:50:38 -0700

    Add an example for a tooltip with richMessage

--- examples/api/lib/material/tooltip/tooltip.2.dart
+++ examples/api/lib/material/tooltip/tooltip.2.dart
@@ -12,17 +12,10 @@
 //***************************************************************************
 //* ▼▼▼▼▼▼▼▼ description ▼▼▼▼▼▼▼▼ (do not modify or remove section marker)
 
-// This example covers most of the attributes available in Tooltip.
-// `decoration` has been used to give a gradient and borderRadius to Tooltip.
-// `height` has been used to set a specific height of the Tooltip.
-// `preferBelow` is false, the tooltip will prefer showing above [Tooltip]'s child widget.
-// However, it may show the tooltip below if there's not enough space
-// above the widget.
-// `textStyle` has been used to set the font size of the 'message'.
-// `showDuration` accepts a Duration to continue showing the message after the long
-// press has been released or the mouse pointer exits the child widget.
-// `waitDuration` accepts a Duration for which a mouse pointer has to hover over the child
-// widget before the tooltip is shown.
+// This example shows a rich [Tooltip] that specifies the [richMessage]
+// parameter instead of the [message] parameter (only one of these may be
+// non-null. Any [InlineSpan] can be specified for the [richMessage] attribute,
+// including [WidgetSpan].
 
 //* ▲▲▲▲▲▲▲▲ description ▲▲▲▲▲▲▲▲ (do not modify or remove section marker)
 //***************************************************************************
@@ -61,21 +54,17 @@ class MyStatelessWidget extends StatelessWidget {
 
   Widget build(BuildContext context) {
     return Tooltip(
-      message: 'I am a Tooltip',
+      richMessage: TextSpan(
+        text: 'I am a rich tooltip. ',
+        style: TextStyle(color: Colors.red),
+        children: [
+          TextSpan(
+            text: 'I am another span of this rich tooltip'
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+        ],
+      ),
       child: const Text('Tap this text and hold down to show a tooltip.'),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(25),
-        gradient:
-        const LinearGradient(colors: <Color>[Colors.amber, Colors.red]),
-      ),
-      height: 50,
-      padding: const EdgeInsets.all(8.0),
-      preferBelow: false,
-      textStyle: const TextStyle(
-        fontSize: 24,
-      ),
-      showDuration: const Duration(seconds: 2),
-      waitDuration: const Duration(seconds: 1),
     );
   }
 
