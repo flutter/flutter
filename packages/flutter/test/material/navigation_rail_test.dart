6be65c744e418760f6f3f60268257c9776a740ca
mahesh jamdade <maheshmn121@gmail.com>
2021-09-25 20:39:27 +0530

    format sematics tree

--- packages/flutter/test/material/navigation_rail_test.dart
+++ packages/flutter/test/material/navigation_rail_test.dart
@@ -2188,52 +2188,53 @@ TestSemantics _expectedSemantics() {
       TestSemantics(
         textDirection: TextDirection.ltr,
         children:<TestSemantics>[ 
-        TestSemantics(
-          textDirection: TextDirection.ltr,
-          children: <TestSemantics>[
-            TestSemantics(
-              children: <TestSemantics>[
-                TestSemantics(
-                  flags: <SemanticsFlag>[SemanticsFlag.scopesRoute],
-                  children: <TestSemantics>[
-                    TestSemantics(
-                      flags: <SemanticsFlag>[
-                        SemanticsFlag.isSelected,
-                        SemanticsFlag.isFocusable,
-                      ],
-                      actions: <SemanticsAction>[SemanticsAction.tap],
-                      label: 'Abc\nTab 1 of 4',
-                      textDirection: TextDirection.ltr,
-                    ),
-                    TestSemantics(
-                      flags: <SemanticsFlag>[SemanticsFlag.isFocusable],
-                      actions: <SemanticsAction>[SemanticsAction.tap],
-                      label: 'Def\nTab 2 of 4',
-                      textDirection: TextDirection.ltr,
-                    ),
-                    TestSemantics(
-                      flags: <SemanticsFlag>[SemanticsFlag.isFocusable],
-                      actions: <SemanticsAction>[SemanticsAction.tap],
-                      label: 'Ghi\nTab 3 of 4',
-                      textDirection: TextDirection.ltr,
-                    ),
-                    TestSemantics(
-                      flags: <SemanticsFlag>[SemanticsFlag.isFocusable],
-                      actions: <SemanticsAction>[SemanticsAction.tap],
-                      label: 'Jkl\nTab 4 of 4',
-                      textDirection: TextDirection.ltr,
-                    ),
-                    TestSemantics(
-                      label: 'body',
-                      textDirection: TextDirection.ltr,
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ],
-        ),
-      ])
+          TestSemantics(
+            textDirection: TextDirection.ltr,
+            children: <TestSemantics>[
+              TestSemantics(
+                children: <TestSemantics>[
+                  TestSemantics(
+                    flags: <SemanticsFlag>[SemanticsFlag.scopesRoute],
+                    children: <TestSemantics>[
+                      TestSemantics(
+                        flags: <SemanticsFlag>[
+                          SemanticsFlag.isSelected,
+                          SemanticsFlag.isFocusable,
+                        ],
+                        actions: <SemanticsAction>[SemanticsAction.tap],
+                        label: 'Abc\nTab 1 of 4',
+                        textDirection: TextDirection.ltr,
+                      ),
+                      TestSemantics(
+                        flags: <SemanticsFlag>[SemanticsFlag.isFocusable],
+                        actions: <SemanticsAction>[SemanticsAction.tap],
+                        label: 'Def\nTab 2 of 4',
+                        textDirection: TextDirection.ltr,
+                      ),
+                      TestSemantics(
+                        flags: <SemanticsFlag>[SemanticsFlag.isFocusable],
+                        actions: <SemanticsAction>[SemanticsAction.tap],
+                        label: 'Ghi\nTab 3 of 4',
+                        textDirection: TextDirection.ltr,
+                      ),
+                      TestSemantics(
+                        flags: <SemanticsFlag>[SemanticsFlag.isFocusable],
+                        actions: <SemanticsAction>[SemanticsAction.tap],
+                        label: 'Jkl\nTab 4 of 4',
+                        textDirection: TextDirection.ltr,
+                      ),
+                      TestSemantics(
+                        label: 'body',
+                        textDirection: TextDirection.ltr,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ]
+      )
     ],
   );
 }
