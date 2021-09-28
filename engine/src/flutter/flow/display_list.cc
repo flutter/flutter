05fd262e612227e9e81ade06fdd1118a314ba622
Jim Graham <flar@google.com>
2021-09-27 17:58:01 -0700

    use all 16 matrix entries in Canvas.transform() to enable 3D matrix concatenation (flutter/engine#28856)

--- engine/src/flutter/flow/display_list.cc
+++ engine/src/flutter/flow/display_list.cc
@@ -306,53 +306,50 @@ struct SkewOp final : DLOp {
 };
 // 4 byte header + 24 byte payload uses 28 bytes but is rounded up to 32 bytes
 // (4 bytes unused)
-struct Transform2x3Op final : DLOp {
-  static const auto kType = DisplayListOpType::kTransform2x3;
+struct Transform2DAffineOp final : DLOp {
+  static const auto kType = DisplayListOpType::kTransform2DAffine;
 
-  Transform2x3Op(SkScalar mxx,
-                 SkScalar mxy,
-                 SkScalar mxt,
-                 SkScalar myx,
-                 SkScalar myy,
-                 SkScalar myt)
+  // clang-format off
+  Transform2DAffineOp(SkScalar mxx, SkScalar mxy, SkScalar mxt,
+                      SkScalar myx, SkScalar myy, SkScalar myt)
       : mxx(mxx), mxy(mxy), mxt(mxt), myx(myx), myy(myy), myt(myt) {}
+  // clang-format on
 
   const SkScalar mxx, mxy, mxt;
   const SkScalar myx, myy, myt;
 
   void dispatch(Dispatcher& dispatcher) const {
-    dispatcher.transform2x3(mxx, mxy, mxt, myx, myy, myt);
+    dispatcher.transform2DAffine(mxx, mxy, mxt,  //
+                                 myx, myy, myt);
   }
 };
-// 4 byte header + 36 byte payload packs evenly into 40 bytes
-struct Transform3x3Op final : DLOp {
-  static const auto kType = DisplayListOpType::kTransform3x3;
+// 4 byte header + 64 byte payload uses 68 bytes which is rounded up to 72 bytes
+// (4 bytes unused)
+struct TransformFullPerspectiveOp final : DLOp {
+  static const auto kType = DisplayListOpType::kTransformFullPerspective;
 
-  Transform3x3Op(SkScalar mxx,
-                 SkScalar mxy,
-                 SkScalar mxt,
-                 SkScalar myx,
-                 SkScalar myy,
-                 SkScalar myt,
-                 SkScalar px,
-                 SkScalar py,
-                 SkScalar pt)
-      : mxx(mxx),
-        mxy(mxy),
-        mxt(mxt),
-        myx(myx),
-        myy(myy),
-        myt(myt),
-        px(px),
-        py(py),
-        pt(pt) {}
+  // clang-format off
+  TransformFullPerspectiveOp(
+      SkScalar mxx, SkScalar mxy, SkScalar mxz, SkScalar mxt,
+      SkScalar myx, SkScalar myy, SkScalar myz, SkScalar myt,
+      SkScalar mzx, SkScalar mzy, SkScalar mzz, SkScalar mzt,
+      SkScalar mwx, SkScalar mwy, SkScalar mwz, SkScalar mwt)
+      : mxx(mxx), mxy(mxy), mxz(mxz), mxt(mxt),
+        myx(myx), myy(myy), myz(myz), myt(myt),
+        mzx(mzx), mzy(mzy), mzz(mzz), mzt(mzt),
+        mwx(mwx), mwy(mwy), mwz(mwz), mwt(mwt) {}
+  // clang-format on
 
-  const SkScalar mxx, mxy, mxt;
-  const SkScalar myx, myy, myt;
-  const SkScalar px, py, pt;
+  const SkScalar mxx, mxy, mxz, mxt;
+  const SkScalar myx, myy, myz, myt;
+  const SkScalar mzx, mzy, mzz, mzt;
+  const SkScalar mwx, mwy, mwz, mwt;
 
   void dispatch(Dispatcher& dispatcher) const {
-    dispatcher.transform3x3(mxx, mxy, mxt, myx, myy, myt, px, py, pt);
+    dispatcher.transformFullPerspective(mxx, mxy, mxz, mxt,  //
+                                        myx, myy, myz, myt,  //
+                                        mzx, mzy, mzz, mzt,  //
+                                        mwx, mwy, mwz, mwt);
   }
 };
 
@@ -1183,25 +1180,42 @@ void DisplayListBuilder::rotate(SkScalar degrees) {
 void DisplayListBuilder::skew(SkScalar sx, SkScalar sy) {
   Push<SkewOp>(0, 1, sx, sy);
 }
-void DisplayListBuilder::transform2x3(SkScalar mxx,
-                                      SkScalar mxy,
-                                      SkScalar mxt,
-                                      SkScalar myx,
-                                      SkScalar myy,
-                                      SkScalar myt) {
-  Push<Transform2x3Op>(0, 1, mxx, mxy, mxt, myx, myy, myt);
+
+// clang-format off
+
+// 2x3 2D affine subset of a 4x4 transform in row major order
+void DisplayListBuilder::transform2DAffine(
+    SkScalar mxx, SkScalar mxy, SkScalar mxt,
+    SkScalar myx, SkScalar myy, SkScalar myt) {
+  if (!(mxx == 1 && mxy == 0 && mxt == 0 &&
+        myx == 0 && myy == 1 && myt == 0)) {
+    Push<Transform2DAffineOp>(0, 1,
+                              mxx, mxy, mxt,
+                              myx, myy, myt);
+  }
 }
-void DisplayListBuilder::transform3x3(SkScalar mxx,
-                                      SkScalar mxy,
-                                      SkScalar mxt,
-                                      SkScalar myx,
-                                      SkScalar myy,
-                                      SkScalar myt,
-                                      SkScalar px,
-                                      SkScalar py,
-                                      SkScalar pt) {
-  Push<Transform3x3Op>(0, 1, mxx, mxy, mxt, myx, myy, myt, px, py, pt);
+// full 4x4 transform in row major order
+void DisplayListBuilder::transformFullPerspective(
+    SkScalar mxx, SkScalar mxy, SkScalar mxz, SkScalar mxt,
+    SkScalar myx, SkScalar myy, SkScalar myz, SkScalar myt,
+    SkScalar mzx, SkScalar mzy, SkScalar mzz, SkScalar mzt,
+    SkScalar mwx, SkScalar mwy, SkScalar mwz, SkScalar mwt) {
+  if (                        mxz == 0 &&
+                              myz == 0 &&
+      mzx == 0 && mzy == 0 && mzz == 1 && mzt == 0 &&
+      mwx == 0 && mwy == 0 && mwz == 0 && mwt == 1) {
+    transform2DAffine(mxx, mxy, mxt,
+                      myx, myy, myt);
+  } else {
+    Push<TransformFullPerspectiveOp>(0, 1,
+                                     mxx, mxy, mxz, mxt,
+                                     myx, myy, myz, myt,
+                                     mzx, mzy, mzz, mzt,
+                                     mwx, mwy, mwz, mwt);
+  }
 }
+
+// clang-format on
 
 void DisplayListBuilder::clipRect(const SkRect& rect,
                                   SkClipOp clip_op,
