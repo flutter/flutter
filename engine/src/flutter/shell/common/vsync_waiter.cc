1859f97b41ac02d4dae8cf73d803a725e1b7879b
Kaushik Iska <iska.kaushik@gmail.com>
2021-09-29 13:48:02 -0400

    vsync_waiter_fallback should schedule firecallback for future (flutter/engine#28817)

--- engine/src/flutter/shell/common/vsync_waiter.cc
+++ engine/src/flutter/shell/common/vsync_waiter.cc
@@ -7,8 +7,10 @@
 #include "flow/frame_timings.h"
 #include "flutter/fml/task_runner.h"
 #include "flutter/fml/trace_event.h"
+#include "fml/logging.h"
 #include "fml/message_loop_task_queues.h"
 #include "fml/task_queue_id.h"
+#include "fml/time/time_point.h"
 
 namespace flutter {
 
@@ -95,6 +97,8 @@ void VsyncWaiter::ScheduleSecondaryCallback(uintptr_t id,
 void VsyncWaiter::FireCallback(fml::TimePoint frame_start_time,
                                fml::TimePoint frame_target_time,
                                bool pause_secondary_tasks) {
+  FML_DCHECK(fml::TimePoint::Now() >= frame_start_time);
+
   Callback callback;
   std::vector<fml::closure> secondary_callbacks;
 
@@ -137,7 +141,7 @@ void VsyncWaiter::FireCallback(fml::TimePoint frame_start_time,
       ui_task_queue_id = task_runners_.GetUITaskRunner()->GetTaskQueueId();
     }
 
-    task_runners_.GetUITaskRunner()->PostTaskForTime(
+    task_runners_.GetUITaskRunner()->PostTask(
         [ui_task_queue_id, callback, flow_identifier, frame_start_time,
          frame_target_time, pause_secondary_tasks]() {
           FML_TRACE_EVENT("flutter", kVsyncTraceName, "StartTime",
@@ -151,8 +155,7 @@ void VsyncWaiter::FireCallback(fml::TimePoint frame_start_time,
           if (pause_secondary_tasks) {
             ResumeDartMicroTasks(ui_task_queue_id);
           }
-        },
-        frame_start_time);
+        });
   }
 
   for (auto& secondary_callback : secondary_callbacks) {
