0cc9bf7990370e7488bc0b1e71d6cda1b285119a
Alexander Biggs <akbiggs@users.noreply.github.com>
2021-09-30 19:41:02 -0400

    [fuchsia] Dart Runner for CF v2 components. (#28923)
    Bug: fxb/79871
    
    Implements the ComponentRunner protocol for the Dart runner, making the Dart runner compatible with CF v2.

--- shell/platform/fuchsia/dart_runner/dart_runner.h
+++ shell/platform/fuchsia/dart_runner/dart_runner.h
@@ -5,6 +5,7 @@
 #ifndef FLUTTER_SHELL_PLATFORM_FUCHSIA_DART_RUNNER_DART_RUNNER_H_
 #define FLUTTER_SHELL_PLATFORM_FUCHSIA_DART_RUNNER_DART_RUNNER_H_
 
+#include <fuchsia/component/runner/cpp/fidl.h>
 #include <fuchsia/sys/cpp/fidl.h>
 #include <lib/fidl/cpp/binding_set.h>
 #include <lib/sys/cpp/component_context.h>
@@ -13,18 +14,24 @@
 
 namespace dart_runner {
 
-class DartRunner : public fuchsia::sys::Runner {
+class DartRunner : public fuchsia::sys::Runner,
+                   public fuchsia::component::runner::ComponentRunner {
  public:
   explicit DartRunner(sys::ComponentContext* context);
   ~DartRunner() override;
 
  private:
   // |fuchsia::sys::Runner| implementation:
-  void StartComponent(
-      fuchsia::sys::Package package,
-      fuchsia::sys::StartupInfo startup_info,
-      ::fidl::InterfaceRequest<fuchsia::sys::ComponentController> controller)
-      override;
+  void StartComponent(fuchsia::sys::Package package,
+                      fuchsia::sys::StartupInfo startup_info,
+                      fidl::InterfaceRequest<fuchsia::sys::ComponentController>
+                          controller) override;
+
+  // |fuchsia::component::runner::ComponentRunner| implementation:
+  void Start(
+      fuchsia::component::runner::ComponentStartInfo start_info,
+      fidl::InterfaceRequest<fuchsia::component::runner::ComponentController>
+          controller) override;
 
   // Not owned by DartRunner.
   sys::ComponentContext* context_;
