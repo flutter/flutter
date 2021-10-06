31501f67055b55df78bd28ec404c963baeb3f2b2
Dan Field <dnfield@google.com>
2021-10-06 00:34:18 -0700

    analysis

--- dev/integration_tests/flutter_gallery/lib/delayed_main.dart
+++ dev/integration_tests/flutter_gallery/lib/delayed_main.dart
@@ -9,8 +9,8 @@ import 'gallery/app.dart';
 Object? createGarbage() {
   final List<dynamic> garbage = <dynamic>[];
   for (int index = 0; index < 1000; index += 1) {
-    List<int>? moreGarbage = List<int>.filled(1000, index);
-    if (index % 2 == 0) {
+    final List<int>? moreGarbage = List<int>.filled(1000, index);
+    if (index.isOdd) {
       garbage.add(moreGarbage);
     }
   }
@@ -23,9 +23,9 @@ void main() async {
   final List<dynamic> garbage = <dynamic>[];
   for (int index = 0; index < 20; index += 1) {
     final Object? moreGarbage = createGarbage();
-    if (index % 2 == 0) {
+    if (index.isOdd) {
       garbage.add(moreGarbage);
-      await Future<void>.delayed(Duration(milliseconds: 7));
+      await Future<void>.delayed(const Duration(milliseconds: 7));
     }
   }
 
