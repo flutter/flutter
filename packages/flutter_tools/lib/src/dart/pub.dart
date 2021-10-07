ae23e78237ccd97b45dede303dad526cbb353a1b
Christopher Fujino <christopherfujino@gmail.com>
2021-10-07 12:40:05 -0700

    remove forbidden import

--- packages/flutter_tools/lib/src/dart/pub.dart
+++ packages/flutter_tools/lib/src/dart/pub.dart
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'dart:io' as io;
-
 import 'package:package_config/package_config.dart';
 import 'package:process/process.dart';
 
