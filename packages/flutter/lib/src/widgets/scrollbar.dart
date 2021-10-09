5735810a07ca3d15764ad5ef525e02c34aec1428
nt4f04uNd <nt4f04uNd@gmail.com>
2021-10-09 03:50:28 +0300

    Allow to click through scrollbar when gestures are disabled

--- packages/flutter/lib/src/widgets/scrollbar.dart
+++ packages/flutter/lib/src/widgets/scrollbar.dart
@@ -90,6 +90,7 @@ class ScrollbarPainter extends ChangeNotifier implements CustomPainter {
     double minLength = _kMinThumbExtent,
     double? minOverscrollLength,
     ScrollbarOrientation? scrollbarOrientation,
+    bool ignorePointer = false,
   }) : assert(color != null),
        assert(radius == null || shape == null),
        assert(thickness != null),
@@ -114,7 +115,8 @@ class ScrollbarPainter extends ChangeNotifier implements CustomPainter {
        _trackColor = trackColor,
        _trackBorderColor = trackBorderColor,
        _scrollbarOrientation = scrollbarOrientation,
-       _minOverscrollLength = minOverscrollLength ?? minLength {
+       _minOverscrollLength = minOverscrollLength ?? minLength,
+       _ignorePointer = ignorePointer {
     fadeoutOpacityAnimation.addListener(notifyListeners);
   }
 
@@ -338,6 +340,17 @@ class ScrollbarPainter extends ChangeNotifier implements CustomPainter {
       return;
 
     _scrollbarOrientation = value;
+    notifyListeners();
+  }
+
+  /// Whether the painter will be ignored during hit testing.
+  bool get ignorePointer => _ignorePointer;
+  bool _ignorePointer;
+  set ignorePointer(bool value) {
+    if (ignorePointer == value)
+      return;
+
+    _ignorePointer = value;
     notifyListeners();
   }
 
@@ -623,6 +636,9 @@ class ScrollbarPainter extends ChangeNotifier implements CustomPainter {
       // We have not computed the scrollbar position yet.
       return false;
     }
+    if (ignorePointer) {
+      return false;
+    }
 
     if (!_lastMetricsAreScrollable) {
       return false;
@@ -659,6 +675,9 @@ class ScrollbarPainter extends ChangeNotifier implements CustomPainter {
     if (_thumbRect == null) {
       return false;
     }
+    if (ignorePointer) {
+      return false;
+    }
     // The thumb is not able to be hit when transparent.
     if (fadeoutOpacityAnimation.value == 0.0) {
       return false;
@@ -688,6 +707,9 @@ class ScrollbarPainter extends ChangeNotifier implements CustomPainter {
     if (_thumbRect == null) {
       return null;
     }
+    if (ignorePointer) {
+      return false;
+    }
     // The thumb is not able to be hit when transparent.
     if (fadeoutOpacityAnimation.value == 0.0) {
       return false;
@@ -716,7 +738,8 @@ class ScrollbarPainter extends ChangeNotifier implements CustomPainter {
         || padding != oldDelegate.padding
         || minLength != oldDelegate.minLength
         || minOverscrollLength != oldDelegate.minOverscrollLength
-        || scrollbarOrientation != oldDelegate.scrollbarOrientation;
+        || scrollbarOrientation != oldDelegate.scrollbarOrientation
+        || ignorePointer != oldDelegate.ignorePointer;
   }
 
   @override
@@ -1200,14 +1223,15 @@ class RawScrollbarState<T extends RawScrollbar> extends State<T> with TickerProv
     );
     scrollbarPainter = ScrollbarPainter(
       color: widget.thumbColor ?? const Color(0x66BCBCBC),
-      minLength: widget.minThumbLength,
-      minOverscrollLength: widget.minOverscrollLength ?? widget.minThumbLength,
-      thickness: widget.thickness ?? _kScrollbarThickness,
       fadeoutOpacityAnimation: _fadeoutOpacityAnimation,
+      thickness: widget.thickness ?? _kScrollbarThickness,
+      radius: widget.radius,
       scrollbarOrientation: widget.scrollbarOrientation,
       mainAxisMargin: widget.mainAxisMargin,
       shape: widget.shape,
-      crossAxisMargin: widget.crossAxisMargin
+      crossAxisMargin: widget.crossAxisMargin,
+      minLength: widget.minThumbLength,
+      minOverscrollLength: widget.minOverscrollLength ?? widget.minThumbLength,
     );
   }
 
@@ -1342,7 +1366,8 @@ class RawScrollbarState<T extends RawScrollbar> extends State<T> with TickerProv
       ..shape = widget.shape
       ..crossAxisMargin = widget.crossAxisMargin
       ..minLength = widget.minThumbLength
-      ..minOverscrollLength = widget.minOverscrollLength ?? widget.minThumbLength;
+      ..minOverscrollLength = widget.minOverscrollLength ?? widget.minThumbLength
+      ..ignorePointer = !enableGestures;
   }
 
   @override
