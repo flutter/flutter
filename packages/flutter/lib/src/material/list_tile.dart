6505eaa0c4e7b929c15a499fa03d972895378ac1
Hans Muller <hansmuller@chromium.org>
2021-10-12 11:32:50 -0700

    updated per review feedback

--- packages/flutter/lib/src/material/list_tile.dart
+++ packages/flutter/lib/src/material/list_tile.dart
@@ -264,10 +264,12 @@ class ListTileThemeData with Diagnosticable {
 ///
 /// The [Drawer] widget specifies a tile theme for its children which sets
 /// [style] to [ListTileStyle.drawer].
-// ignore: avoid_implementing_value_types
 class ListTileTheme extends InheritedTheme {
   /// Creates a list tile theme that defines the color and style parameters for
   /// descendant [ListTile]s.
+  ///
+  /// Only the [data] parameter should be used. The other parameters are
+  /// redundant (are now obsolete) and will be deprecated in a future update.
   const ListTileTheme({
     Key? key,
     ListTileThemeData? data,
