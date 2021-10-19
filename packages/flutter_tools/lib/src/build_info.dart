0407aa44804c97bcc7164818e5aabbc2db4f2fd1
Gary Qian <garyq@google.com>
2021-10-19 02:20:39 -0700

    Reland "Add multidex flag and automatic multidex support (#90944)"  (#92049)

--- packages/flutter_tools/lib/src/build_info.dart
+++ packages/flutter_tools/lib/src/build_info.dart
@@ -304,6 +304,7 @@ class AndroidBuildInfo {
     ],
     this.splitPerAbi = false,
     this.fastStart = false,
+    this.multidexEnabled = false,
   });
 
   // The build info containing the mode and flavor.
@@ -321,6 +322,9 @@ class AndroidBuildInfo {
 
   /// Whether to bootstrap an empty application.
   final bool fastStart;
+
+  /// Whether to enable multidex support for apps with more than 64k methods.
+  final bool multidexEnabled;
 }
 
 /// A summary of the compilation strategy used for Dart.
