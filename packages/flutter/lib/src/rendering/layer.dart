c5df75905a5666bf41ee75ace3540d75c905e842
Michael Goderbauer <goderbauer@google.com>
2021-10-27 15:19:28 -0700

    fix doc

--- packages/flutter/lib/src/rendering/layer.dart
+++ packages/flutter/lib/src/rendering/layer.dart
@@ -2144,7 +2144,7 @@ class LayerLink {
   /// The total size of the content of the connected [LeaderLayer].
   ///
   /// Generally this should be set by the [RenderObject] that paints on the
-  /// registered [leader] layer (for instance a [RenderLeaderLayer] that shares
+  /// registered [LeaderLayer] (for instance a [RenderLeaderLayer] that shares
   /// this link with its followers). This size may be outdated before and during
   /// layout.
   Size? leaderSize;
