01f441639c0f3acd368c86bfb43995b408a931da
Wizz.Xu <wizz.xu@outlook.com>
2021-10-28 12:15:47 +0800

    Update packages/flutter_tools/lib/src/commands/analyze_base.dart
    Co-authored-by: Jenn Magder <magder@google.com>

--- packages/flutter_tools/lib/src/commands/analyze_base.dart
+++ packages/flutter_tools/lib/src/commands/analyze_base.dart
@@ -140,7 +140,7 @@ class PackageDependency {
     for (final List<String> targetSources in values.values) {
       for (final String source in targetSources) {
         assert(globals.fs.path.isAbsolute(source));
-        if (globals.fs.path.isWithin(Cache.flutterRoot ?? '', source)) {
+        if (flutterRoot != null && globals.fs.path.isWithin(flutterRoot, source)) {
           return true;
         }
       }
