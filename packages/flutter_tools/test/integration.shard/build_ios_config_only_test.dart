cac955cba4dc0b7c4d5ba06056dcadf0f1ace0a5
eric <wanglikun7342@gmail.com>
2021-11-03 11:25:44 +0800

    feat: mirgate integration.shard/build_ios_config_only_test.dart to null safety

--- packages/flutter_tools/test/integration.shard/build_ios_config_only_test.dart
+++ packages/flutter_tools/test/integration.shard/build_ios_config_only_test.dart
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// @dart = 2.8
-
 import 'package:file_testing/file_testing.dart';
 import 'package:flutter_tools/src/base/file_system.dart';
 import 'package:flutter_tools/src/base/io.dart';
