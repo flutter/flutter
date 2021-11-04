5e719202fc607086694b0c60c603314c6ce8864c
Andrei Diaconu <adiaconu@microsoft.com>
2021-11-05 01:03:18 +0200

    Update multidex_project test project targetSdkVersion to 31

--- packages/flutter_tools/test/integration.shard/test_data/multidex_project.dart
+++ packages/flutter_tools/test/integration.shard/test_data/multidex_project.dart
@@ -180,7 +180,7 @@ class MultidexProject extends Project {
           // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
           applicationId "com.example.multidextest2"
           minSdkVersion 16
-          targetSdkVersion 30
+          targetSdkVersion 31
           versionCode flutterVersionCode.toInteger()
           versionName flutterVersionName
       }
