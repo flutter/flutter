f9e8eeab413418600c8f181e5d4c50d32d10e9ae
camsim99 <camillesimon90@gmail.com>
2021-11-04 09:32:52 -0700

    Update integration test

--- packages/flutter_tools/test/integration.shard/android_plugin_compilesdkversion_mismatch_test.dart
+++ packages/flutter_tools/test/integration.shard/android_plugin_compilesdkversion_mismatch_test.dart
@@ -5,18 +5,25 @@
 // @dart = 2.8
 
 import 'package:file_testing/file_testing.dart';
+import 'package:flutter_tools/src/base/bot_detector.dart';
 import 'package:flutter_tools/src/base/file_system.dart';
+import 'package:flutter_tools/src/base/io.dart';
 import 'package:flutter_tools/src/base/logger.dart';
-
+import 'package:flutter_tools/src/base/platform.dart';
+import 'package:flutter_tools/src/cache.dart';
+import 'package:flutter_tools/src/dart/pub.dart';
+import 'package:flutter_tools/src/reporting/reporting.dart';
 
 import '../src/common.dart';
-import '../src/context.dart';
+import '../src/fakes.dart';
 import 'test_utils.dart';
 
 void main() {
+
   Directory tempDir;
 
-  setUp(() async {
+  setUp(()  {
+    Cache.flutterRoot = getFlutterRoot();
     tempDir = createResolvedTempDirectorySync('flutter_plugin_test.');
   });
 
@@ -31,7 +38,7 @@ void main() {
       'flutter',
     );
 
-    final RegExp androidCompileSdkVersionRegExp = RegExp(r'compileSdkVersion [0-9]+');
+    final RegExp androidCompileSdkVersionRegExp = RegExp(r'compileSdkVersion ([0-9]+|flutter.compileSdkVersion)');
 
     // Create dummy plugin
     processManager.runSync(<String>[
@@ -43,17 +50,17 @@ void main() {
       'test_plugin',
     ], workingDirectory: tempDir.path);
 
-    final Directory pluginAppDir = tempDir.childDirectory('test_plugin').childDirectory('example');
-    final File pluginGradleFile = pluginAppDir.childDirectory('android').childDirectory('app').childFile('build.gradle');
+    final Directory pluginAppDir = tempDir.childDirectory('test_plugin');
+    final File pluginGradleFile = pluginAppDir.childDirectory('android').childFile('build.gradle');
     expect(pluginGradleFile, exists);
     final String pluginBuildGradle = pluginGradleFile.readAsStringSync();
 
 
-    // Bump up plugin compileSdkVersion to 31
-    String pluginNewGradleFile = pluginBuildGradle.replaceAll(
-      androidCompileSdkVersionRegExp, 'compileSdkVersion 31');
-    pluginGradleFile.writeAsStringSync(pluginNewGradleFile);
-
+    // Bump up plugin compileSdkVersion to 30
+    String newPluginGradleFile = pluginBuildGradle.replaceAll(
+      androidCompileSdkVersionRegExp, 'compileSdkVersion 30');
+    pluginGradleFile.writeAsStringSync(newPluginGradleFile);
+  
     // Create dummy project
     processManager.runSync(<String>[
       flutterBin,
@@ -63,25 +70,48 @@ void main() {
       'test_project',
     ], workingDirectory: tempDir.path);
 
-    final Directory projectAppDir = tempDir.childDirectory('test_project'); //todo check
-    final File pubspecFile = projectAppDir.childFile('pubspec.yaml'); //check exists
-    final File projectGradleFile = pluginAppDir.childDirectory('android').childDirectory('app').childFile('build.gradle'); //check exists
+    final Directory projectAppDir = tempDir.childDirectory('test_project');
+    final File pubspecFile = projectAppDir.childFile('pubspec.yaml');
+    expect(pubspecFile, exists);
+    final File projectGradleFile = projectAppDir.childDirectory('android').childDirectory('app').childFile('build.gradle');
+    expect(projectGradleFile, exists);
     final String projectBuildGradle = projectGradleFile.readAsStringSync();
     final String pubspecFileString = pubspecFile.readAsStringSync();
-    final RegExp pubspecDependenciesRegExp = RegExp(r'dependencies:');
+    final RegExp pubspecDependenciesRegExp = RegExp('^dependencies:\n');
 
-    // Bump down project compileSdkVersion to 30
+    // Bump down project compileSdkVersion to 28
     String newProjectGradleFile = projectBuildGradle.replaceAll(
-      androidCompileSdkVersionRegExp, 'compileSdkVersion 30');
+      androidCompileSdkVersionRegExp, 'compileSdkVersion 28');
     projectGradleFile.writeAsStringSync(newProjectGradleFile);
 
     // Add dummy plugin as dependency to dummy project
-    String newPubspecFile= pubspecFileString.replaceAll(
-      pubspecDependenciesRegExp, 'dependencies:\n ../test_plugin');
+    String newPubspecFile= pubspecFileString.replaceFirst(
+      'dependencies:', 'dependencies:\n  test_plugin:\n    path: ../test_plugin');
+    print(newPubspecFile);
     projectGradleFile.writeAsStringSync(newPubspecFile);
 
+    // Run flutter pub get to update the dependencies
+    final BufferLogger logger = BufferLogger.test();
+    final Pub pub = Pub(
+        fileSystem: fileSystem,
+        logger: logger,
+        processManager: processManager,
+        platform: FakePlatform(),
+        botDetector: const FakeBotDetector(false),
+        usage: TestUsage(),
+      );
+
+    await pub.get(
+      context: PubContext.flutterTests,
+      directory: projectAppDir.path);
+
+    await for (var entity in
+      projectAppDir.list(recursive: true, followLinks: false)) {
+    print(entity.path);
+  }
+
     // Run flutter build apk to build dummy project
-    processManager.runSync(<String>[
+    final ProcessResult result = await processManager.runSync(<String>[
       flutterBin,
       ...getLocalEngineArguments(),
       'build',
@@ -90,11 +120,11 @@ void main() {
     ], workingDirectory: projectAppDir.path);
 
     // Check error message is thrown
-    expect(testLogger.statusText,
-      contains("Warning: The plugin test_plugin requires Android SDK version 31.")
+    expect(result.stderr,
+      contains("Warning: The plugin test_plugin requires Android SDK version 29.")
       );
-    expect(testLogger.errorText,
+    expect(result.stdout,
       contains("One or more plugins require a higher Android SDK version.\nFix this issue by adding the following to /flutter_plugin_test/test_project/android/app/build.gradle:\nandroid {\n  compileSdkVersion 31\n    ...\n}\n")
       );
-  });
+   });
 }
