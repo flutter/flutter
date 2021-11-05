0afef6df4b39e79d38f52b87e7f46e855b99209b
Chris Yang <ychris@google.com>
2021-11-05 14:43:01 -0700

    ios test script checks for `ios_test_flutter` artifacts (flutter/engine#29554)

--- engine/src/flutter/testing/run_tests.py
+++ engine/src/flutter/testing/run_tests.py
@@ -297,12 +297,13 @@ def EnsureDebugUnoptSkyPackagesAreBuilt():
 def EnsureIosTestsAreBuilt(ios_out_dir):
   """Builds the engine variant and the test dylib containing the XCTests"""
   tmp_out_dir = os.path.join(out_dir, ios_out_dir)
+  ios_test_lib = os.path.join(tmp_out_dir, 'libios_test_flutter.dylib')
   message = []
   message.append('gn --ios --unoptimized --runtime-mode=debug --no-lto --simulator')
   message.append('autoninja -C %s ios_test_flutter' % ios_out_dir)
-  final_message = '%s doesn\'t exist. Please run the following commands: \n%s' % (
-      ios_out_dir, '\n'.join(message))
-  assert os.path.exists(tmp_out_dir), final_message
+  final_message = '%s or %s doesn\'t exist. Please run the following commands: \n%s' % (
+      ios_out_dir, ios_test_lib, '\n'.join(message))
+  assert os.path.exists(tmp_out_dir) and os.path.exists(ios_test_lib), final_message
 
 
 def AssertExpectedXcodeVersion():
