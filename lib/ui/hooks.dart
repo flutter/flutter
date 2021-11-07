df686d2638b35ce6ea771586b374fae800d0624c
Zachary Anderson <zanderso@users.noreply.github.com>
2021-11-06 20:43:44 -0700

    Revert "Reland Display Features support (Foldable and Cutout) (#29447)" (#29574)
    This reverts commit 3364409b917f8b2d54de5a3fcae480e4cb07290a.

--- lib/ui/hooks.dart
+++ lib/ui/hooks.dart
@@ -26,9 +26,6 @@ void _updateWindowMetrics(
   double systemGestureInsetBottom,
   double systemGestureInsetLeft,
   double physicalTouchSlop,
-  List<double> displayFeaturesBounds,
-  List<int> displayFeaturesType,
-  List<int> displayFeaturesState,
 ) {
   PlatformDispatcher.instance._updateWindowMetrics(
     id,
@@ -48,9 +45,6 @@ void _updateWindowMetrics(
     systemGestureInsetBottom,
     systemGestureInsetLeft,
     physicalTouchSlop,
-    displayFeaturesBounds,
-    displayFeaturesType,
-    displayFeaturesState,
   );
 }
 
--- shell/platform/fuchsia/flutter/gfx_platform_view.cc
+++ shell/platform/fuchsia/flutter/gfx_platform_view.cc
@@ -253,9 +253,6 @@ void GfxPlatformView::OnScenicEvent(
         0.0f,  // p_physical_system_gesture_inset_bottom
         0.0f,  // p_physical_system_gesture_inset_left,
         -1.0,  // p_physical_touch_slop,
-        {},    // p_physical_display_features_bounds
-        {},    // p_physical_display_features_type
-        {},    // p_physical_display_features_state
     });
   }
 }
