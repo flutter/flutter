13ca08c7e21e94955c23457799dfabee7567ead5
Andrei Diaconu <adiaconu@microsoft.com>
2021-11-08 13:01:08 +0200

    Revert "Revert "Reland: display features (#93117)" (#93204)"
    This reverts commit 2468f315e0a7a3e3fabaec38913a8dc8c0b619db.

--- packages/flutter_tools/test/general.shard/project_test.dart
+++ packages/flutter_tools/test/general.shard/project_test.dart
@@ -958,7 +958,7 @@ String gradleFileWithApplicationId(String id) {
   return '''
 apply plugin: 'com.android.application'
 android {
-    compileSdkVersion 30
+    compileSdkVersion 31
 
     defaultConfig {
         applicationId '$id'
@@ -975,7 +975,7 @@ version '1.0-SNAPSHOT'
 apply plugin: 'com.android.library'
 
 android {
-    compileSdkVersion 30
+    compileSdkVersion 31
 }
 ''';
 }
