b7cb641d6aaadb272481848da6d56d318f7b5e20
Chinmay Garde <chinmaygarde@google.com>
2021-11-08 14:16:09 -0800

    Fixup names of pipeline descriptors.

--- engine/src/flutter/impeller/renderer/pipeline_builder.h
+++ engine/src/flutter/impeller/renderer/pipeline_builder.h
@@ -84,7 +84,7 @@ struct PipelineBuilder {
       // Configure the sole color attachments pixel format.
       // TODO(csg): This can be easily reflected but we are sticking to the
       // convention that the first stage output is the color output.
-      PipelineColorAttachment color0;
+      ColorAttachmentDescriptor color0;
       color0.format = PixelFormat::kB8G8R8A8UNormInt;
       color0.blending_enabled = true;
       desc.SetColorAttachmentDescriptor(0u, std::move(color0));
