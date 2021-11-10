ea96bc2489ddf3437b8e64503e6400138a1caa8a
LongCat is Looong <31859944+LongCatIsLooong@users.noreply.github.com>
2021-11-10 09:28:16 -0800

    Fix fuchsia default text editing keybindings

--- packages/flutter/lib/src/widgets/default_text_editing_shortcuts.dart
+++ packages/flutter/lib/src/widgets/default_text_editing_shortcuts.dart
@@ -224,7 +224,84 @@ class DefaultTextEditingShortcuts extends Shortcuts {
   //   * Meta + backspace
   static const Map<ShortcutActivator, Intent> _androidShortcuts = _commonShortcuts;
 
-  static const Map<ShortcutActivator, Intent> _fuchsiaShortcuts = _androidShortcuts;
+  // Fuchsia keyboard HID usage values are defined in (page 53):
+  // https://www.usb.org/sites/default/files/documents/hut1_12v2.pdf
+  static const int _kFuchsiaKeyIdPlane = LogicalKeyboardKey.fuchsiaPlane;
+
+  static const LogicalKeyboardKey _kBackspace = LogicalKeyboardKey(42 | _kFuchsiaKeyIdPlane);
+  static const LogicalKeyboardKey _kDelete = LogicalKeyboardKey(76 | _kFuchsiaKeyIdPlane);
+  static const LogicalKeyboardKey _kArrowLeft = LogicalKeyboardKey(80 | _kFuchsiaKeyIdPlane);
+  static const LogicalKeyboardKey _kArrowRight = LogicalKeyboardKey(79 | _kFuchsiaKeyIdPlane);
+  static const LogicalKeyboardKey _kArrowDown = LogicalKeyboardKey(81 | _kFuchsiaKeyIdPlane);
+  static const LogicalKeyboardKey _kArrowUp = LogicalKeyboardKey(82 | _kFuchsiaKeyIdPlane);
+
+  static const Map<ShortcutActivator, Intent> _fuchsiaShortcuts = <ShortcutActivator, Intent>{
+    SingleActivator(_kBackspace): DeleteCharacterIntent(forward: false),
+    SingleActivator(_kBackspace, control: true): DeleteToNextWordBoundaryIntent(forward: false),
+    SingleActivator(_kBackspace, alt: true): DeleteToLineBreakIntent(forward: false),
+    SingleActivator(_kDelete): DeleteCharacterIntent(forward: true),
+    SingleActivator(_kDelete, control: true): DeleteToNextWordBoundaryIntent(forward: true),
+    SingleActivator(_kDelete, alt: true): DeleteToLineBreakIntent(forward: true),
+    SingleActivator(_kArrowDown, alt: true): ExtendSelectionToDocumentBoundaryIntent(forward: true, collapseSelection: true),
+    SingleActivator(_kArrowLeft, alt: true): ExtendSelectionToLineBreakIntent(forward: false, collapseSelection: true),
+    SingleActivator(_kArrowRight, alt: true): ExtendSelectionToLineBreakIntent(forward: true, collapseSelection: true),
+    SingleActivator(_kArrowUp, alt: true): ExtendSelectionToDocumentBoundaryIntent(forward: false, collapseSelection: true),
+    SingleActivator(_kArrowDown, shift: true, alt: true): ExtendSelectionToDocumentBoundaryIntent(forward: true, collapseSelection: false),
+    SingleActivator(_kArrowLeft, shift: true, alt: true): ExtendSelectionToLineBreakIntent(forward: false, collapseSelection: false),
+    SingleActivator(_kArrowRight, shift: true, alt: true): ExtendSelectionToLineBreakIntent(forward: true, collapseSelection: false),
+    SingleActivator(_kArrowUp, shift: true, alt: true): ExtendSelectionToDocumentBoundaryIntent(forward: false, collapseSelection: false),
+    SingleActivator(_kArrowDown): ExtendSelectionVerticallyToAdjacentLineIntent(forward: true, collapseSelection: true),
+    SingleActivator(_kArrowLeft): ExtendSelectionByCharacterIntent(forward: false, collapseSelection: true),
+    SingleActivator(_kArrowRight): ExtendSelectionByCharacterIntent(forward: true, collapseSelection: true),
+    SingleActivator(_kArrowUp): ExtendSelectionVerticallyToAdjacentLineIntent(forward: false, collapseSelection: true),
+    SingleActivator(_kArrowLeft, control: true): ExtendSelectionToNextWordBoundaryIntent(forward: false, collapseSelection: true),
+    SingleActivator(_kArrowRight, control: true): ExtendSelectionToNextWordBoundaryIntent(forward: true, collapseSelection: true),
+    SingleActivator(_kArrowLeft, shift: true, control: true): ExtendSelectionToNextWordBoundaryIntent(forward: false, collapseSelection: false),
+    SingleActivator(_kArrowRight, shift: true, control: true): ExtendSelectionToNextWordBoundaryIntent(forward: true, collapseSelection: false),
+    SingleActivator(_kArrowDown, shift: true): ExtendSelectionVerticallyToAdjacentLineIntent(forward: true, collapseSelection: false),
+    SingleActivator(_kArrowLeft, shift: true): ExtendSelectionByCharacterIntent(forward: false, collapseSelection: false),
+    SingleActivator(_kArrowRight, shift: true): ExtendSelectionByCharacterIntent(forward: true, collapseSelection: false),
+    SingleActivator(_kArrowUp, shift: true): ExtendSelectionVerticallyToAdjacentLineIntent(forward: false, collapseSelection: false),
+  };
+
+  static const Map<ShortcutActivator, Intent> defaultEditingShortcuts =
+      <ShortcutActivator, Intent>{
+    SingleActivator(kBackspace): DeleteTextIntent(),
+    SingleActivator(kBackspace, control: true): DeleteByWordTextIntent(),
+    SingleActivator(kBackspace, alt: true): DeleteByLineTextIntent(),
+    SingleActivator(kDelete): DeleteForwardTextIntent(),
+    SingleActivator(kDelete, control: true): DeleteForwardByWordTextIntent(),
+    SingleActivator(kDelete, alt: true): DeleteForwardByLineTextIntent(),
+    SingleActivator(kArrowDown, alt: true): MoveSelectionToEndTextIntent(),
+    SingleActivator(kArrowLeft, alt: true): MoveSelectionLeftByLineTextIntent(),
+    SingleActivator(kArrowRight, alt: true):
+        MoveSelectionRightByLineTextIntent(),
+    SingleActivator(kArrowUp, alt: true): MoveSelectionToStartTextIntent(),
+    SingleActivator(kArrowDown, shift: true, alt: true):
+        ExpandSelectionToEndTextIntent(),
+    SingleActivator(kArrowLeft, shift: true, alt: true):
+        ExpandSelectionLeftByLineTextIntent(),
+    SingleActivator(kArrowRight, shift: true, alt: true):
+        ExpandSelectionRightByLineTextIntent(),
+    SingleActivator(kArrowUp, shift: true, alt: true):
+        ExpandSelectionToStartTextIntent(),
+    SingleActivator(kArrowDown): MoveSelectionDownTextIntent(),
+    SingleActivator(kArrowLeft): MoveSelectionLeftTextIntent(),
+    SingleActivator(kArrowRight): MoveSelectionRightTextIntent(),
+    SingleActivator(kArrowUp): MoveSelectionUpTextIntent(),
+    SingleActivator(kArrowLeft, control: true):
+        MoveSelectionLeftByWordTextIntent(),
+    SingleActivator(kArrowRight, control: true):
+        MoveSelectionRightByWordTextIntent(),
+    SingleActivator(kArrowLeft, shift: true, control: true):
+        ExtendSelectionLeftByWordTextIntent(),
+    SingleActivator(kArrowRight, shift: true, control: true):
+        ExtendSelectionRightByWordTextIntent(),
+    SingleActivator(kArrowDown, shift: true): ExtendSelectionDownTextIntent(),
+    SingleActivator(kArrowLeft, shift: true): ExtendSelectionLeftTextIntent(),
+    SingleActivator(kArrowRight, shift: true): ExtendSelectionRightTextIntent(),
+    SingleActivator(kArrowUp, shift: true): ExtendSelectionUpTextIntent(),
+  };
 
   // The following key combinations have no effect on text editing on this
   // platform:
