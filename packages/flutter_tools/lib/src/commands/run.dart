858a32871264543681560ac30c6a939dd17b6974
Zachary Anderson <zanderso@users.noreply.github.com>
2021-11-09 22:22:14 -0800

    Revert "Exit on deprecated v1 embedding when trying to run or build (#92901)" (#93365)
    This reverts commit 6153bbef3e8e8202a0f32650406c879a40775175.

--- packages/flutter_tools/lib/src/commands/run.dart
+++ packages/flutter_tools/lib/src/commands/run.dart
@@ -250,7 +250,6 @@ class RunCommand extends RunCommandBase {
     // without needing to know the port.
     addPublishPort(verboseHelp: verboseHelp);
     addMultidexOption();
-    addIgnoreDeprecationOption();
     argParser
       ..addFlag('enable-software-rendering',
         negatable: false,
@@ -341,10 +340,6 @@ class RunCommand extends RunCommandBase {
 
   @override
   final String name = 'run';
-
-  @override
-  DeprecationBehavior get deprecationBehavior => boolArg('ignore-deprecation') ? DeprecationBehavior.ignore : _deviceDeprecationBehavior;
-  DeprecationBehavior _deviceDeprecationBehavior = DeprecationBehavior.none;
 
   @override
   final String description = 'Run your Flutter app on an attached device.';
@@ -478,10 +473,6 @@ class RunCommand extends RunCommandBase {
       throwToolExit(
         '--${FlutterOptions.kDeviceUser} is only supported for Android. At least one Android device is required.'
       );
-    }
-
-    if (devices.any((Device device) => device is AndroidDevice)) {
-      _deviceDeprecationBehavior = DeprecationBehavior.exit;
     }
     // Only support "web mode" with a single web device due to resident runner
     // refactoring required otherwise.
