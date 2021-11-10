2ea8b497daf65e47b0e4877f1384d6c70f676fbd
Aaron Clarke <aaclarke@google.com>
2021-11-10 10:10:14 -0800

    sort imports

--- dev/benchmarks/multiple_flutters/module/lib/main.dart
+++ dev/benchmarks/multiple_flutters/module/lib/main.dart
@@ -3,8 +3,8 @@
 // found in the LICENSE file.
 
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter/semantics.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() => runApp(const MyApp(Colors.blue));
 
