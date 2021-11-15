0728dd2587a40e78fef5490e25f59890249e5531
Viren Khatri <werainkhatri.work@gmail.com>
2021-11-16 03:03:13 +0530

    fixes doc reference

--- packages/flutter/lib/src/material/icon_button.dart
+++ packages/flutter/lib/src/material/icon_button.dart
@@ -144,8 +144,8 @@ class IconButton extends StatelessWidget {
 
   /// The size of the icon inside the button.
   ///
-  /// If null, uses [IconTheme.size]. If it is also null,
-  /// the default size is 24.0.
+  /// If null, uses [IconThemeData.size]. If it is also null, the default size
+  /// is 24.0.
   ///
   /// The size given here is passed down to the widget in the [icon] property
   /// via an [IconTheme]. Setting the size here instead of in, for example, the
