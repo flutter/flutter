ea5201389503be8b5d6b87ccd133502b26924762
Kate Lovett <katelovett@google.com>
2021-11-15 14:40:32 -0600

    Close tree if unapproved golden images get in (#93516)

--- packages/flutter_goldens/test/flutter_goldens_test.dart
+++ packages/flutter_goldens/test/flutter_goldens_test.dart
@@ -153,6 +153,45 @@ void main() {
       );
     });
 
+    test('throws for error state from imgtestAdd', () {
+      final File goldenFile = fs.file('/workDirectory/temp/golden_file_test.png')
+        ..createSync(recursive: true);
+      platform = FakePlatform(
+          environment: <String, String>{
+            'FLUTTER_ROOT': _kFlutterRoot,
+            'GOLDCTL' : 'goldctl',
+          },
+          operatingSystem: 'macos'
+      );
+
+      skiaClient = SkiaGoldClient(
+        workDirectory,
+        fs: fs,
+        process: process,
+        platform: platform,
+        httpClient: fakeHttpClient,
+      );
+
+      process.fallbackProcessResult = ProcessResult(123, 1, 'fail', 'fail');
+      const RunInvocation goldctlInvocation = RunInvocation(
+        <String>[
+          'goldctl',
+          'imgtest', 'add',
+          '--work-dir', '/workDirectory/temp',
+          '--test-name', 'golden_file_test',
+          '--png-file', '/workDirectory/temp/golden_file_test.png',
+          '--passfail',
+        ],
+        null,
+      );
+      process.processResults[goldctlInvocation] = ProcessResult(123, 1, 'fail', 'fail');
+
+      expect(
+        skiaClient.imgtestAdd('golden_file_test', goldenFile),
+        throwsException,
+      );
+    });
+
     test('correctly inits tryjob for luci', () async {
       platform = FakePlatform(
         environment: <String, String>{
