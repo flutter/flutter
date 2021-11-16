04fadf56521a248d819490b5a6f3683c22421dd6
Chinmay Garde <chinmaygarde@google.com>
2021-11-16 15:58:29 -0800

    WIP on stroke rendering.

--- engine/src/flutter/impeller/aiks/paint.h
+++ engine/src/flutter/impeller/aiks/paint.h
@@ -4,16 +4,23 @@
 
 #pragma once
 
-#include "flutter/fml/macros.h"
+#include <memory>
 
+#include "flutter/fml/macros.h"
 #include "impeller/entity/contents.h"
 #include "impeller/geometry/color.h"
 
 namespace impeller {
 
 struct Paint {
+  enum class Style {
+    kFill,
+    kStroke,
+  };
+
   Color color;
   Scalar stroke_width = 0.0;
+  Style style = Style::kFill;
 
   std::shared_ptr<Contents> CreateContentsForEntity() const;
 };
