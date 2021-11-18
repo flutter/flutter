0b267aac6794712a6d9d0f31dda3f844325d0258
Jason Simmons <jason-simmons@users.noreply.github.com>
2021-11-18 12:35:17 -0800

    Build script for the expat XML parser (#526)
    See https://github.com/flutter/flutter/issues/91384

---
+++ build/secondary/third_party/expat/expat_config/expat_config.h
@@ -0,0 +1,14 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#define BYTEORDER 1234
+#define HAVE_INTTYPES_H 1
+#define HAVE_MEMORY_H 1
+#define HAVE_STDINT_H 1
+#define HAVE_STDLIB_H 1
+#define HAVE_STRING_H 1
+#define STDC_HEADERS 1
+#define XML_CONTEXT_BYTES 1024
+#define XML_DTD 1
+#define XML_NS 1
