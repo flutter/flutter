9f1db6328627830335a554692ceb9393d7508b51
guidezpl <6655696+guidezpl@users.noreply.github.com>
2021-11-19 13:09:03 +0100

    remove warning and fix insert_chart (outlined) dartdoc

--- packages/flutter/lib/src/material/icons.dart
+++ packages/flutter/lib/src/material/icons.dart
@@ -97,8 +97,7 @@ class PlatformAdaptiveIcons implements Icons {
 ///
 /// Use with the [Icon] class to show specific icons.
 ///
-/// Icons are identified by their name as listed below. **Do not use codepoints
-/// directly, as they are subject to change.**
+/// Icons are identified by their name as listed below, e.g. [Icons.airplane].
 ///
 /// To use this class, make sure you set `uses-material-design: true` in your
 /// project's `pubspec.yaml` file in the `flutter` section. This ensures that
