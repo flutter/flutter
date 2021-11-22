ce150d13da140c12971b8863d3c0a1bc0497be53
Konoshenko Vlad <konoshenko@surfstudio.ru>
2021-11-22 23:00:28 +0300

    fix: change whereType to direct compare with null

--- packages/flutter_tools/test/general.shard/crash_reporting_test.dart
+++ packages/flutter_tools/test/general.shard/crash_reporting_test.dart
@@ -47,21 +47,24 @@ void main() {
 #1      _rootRunUnary (dart:async/zone.dart:1141:38)''');
   });
 
-  Future<void> verifyCrashReportSent(RequestInfo crashInfo, {
+  Future<void> verifyCrashReportSent(
+    RequestInfo crashInfo, {
     int crashes = 1,
   }) async {
     // Verify that we sent the crash report.
     expect(crashInfo.method, 'POST');
-    expect(crashInfo.uri, Uri(
-      scheme: 'https',
-      host: 'clients2.google.com',
-      port: 443,
-      path: '/cr/report',
-      queryParameters: <String, String>{
-        'product': 'Flutter_Tools',
-        'version': 'test-version',
-      },
-    ));
+    expect(
+        crashInfo.uri,
+        Uri(
+          scheme: 'https',
+          host: 'clients2.google.com',
+          port: 443,
+          path: '/cr/report',
+          queryParameters: <String, String>{
+            'product': 'Flutter_Tools',
+            'version': 'test-version',
+          },
+        ));
     expect(crashInfo.fields?['uuid'], testUsage.clientId);
     expect(crashInfo.fields?['product'], 'Flutter_Tools');
     expect(crashInfo.fields?['version'], 'test-version');
@@ -69,18 +72,22 @@ void main() {
     expect(crashInfo.fields?['osVersion'], 'Linux');
     expect(crashInfo.fields?['type'], 'DartError');
     expect(crashInfo.fields?['error_runtime_type'], 'StateError');
-    expect(crashInfo.fields?['error_message'], 'Bad state: Test bad state error');
+    expect(
+        crashInfo.fields?['error_message'], 'Bad state: Test bad state error');
     expect(crashInfo.fields?['comments'], 'crash');
 
     expect(logger.traceText, contains('Sending crash report to Google.'));
-    expect(logger.traceText, contains('Crash report sent (report ID: test-report-id)'));
+    expect(logger.traceText,
+        contains('Crash report sent (report ID: test-report-id)'));
   }
 
-  testWithoutContext('CrashReporter.informUser provides basic instructions', () async {
+  testWithoutContext('CrashReporter.informUser provides basic instructions',
+      () async {
     final CrashReporter crashReporter = CrashReporter(
       fileSystem: fs,
       logger: logger,
-      flutterProjectFactory: FlutterProjectFactory(fileSystem: fs, logger: logger),
+      flutterProjectFactory:
+          FlutterProjectFactory(fileSystem: fs, logger: logger),
       client: FakeHttpClient.any(),
     );
 
@@ -88,15 +95,17 @@ void main() {
 
     await crashReporter.informUser(
       CrashDetails(
-        command: 'arg1 arg2 arg3',
-        error: Exception('Dummy exception'),
-        stackTrace: StackTrace.current,
-        doctorText: 'Fake doctor text'),
+          command: 'arg1 arg2 arg3',
+          error: Exception('Dummy exception'),
+          stackTrace: StackTrace.current,
+          doctorText: 'Fake doctor text'),
       file,
     );
 
-    expect(logger.errorText, contains('A crash report has been written to ${file.path}.'));
-    expect(logger.statusText, contains('https://github.com/flutter/flutter/issues/new'));
+    expect(logger.errorText,
+        contains('A crash report has been written to ${file.path}.'));
+    expect(logger.statusText,
+        contains('https://github.com/flutter/flutter/issues/new'));
   });
 
   testWithoutContext('suppress analytics', () async {
@@ -146,9 +155,12 @@ void main() {
       await verifyCrashReportSent(requestInfo);
     });
 
-    testWithoutContext('should print an explanatory message when there is a SocketException', () async {
+    testWithoutContext(
+        'should print an explanatory message when there is a SocketException',
+        () async {
       final CrashReportSender crashReportSender = CrashReportSender(
-        client: CrashingCrashReportSender(const SocketException('no internets')),
+        client:
+            CrashingCrashReportSender(const SocketException('no internets')),
         usage: testUsage,
         platform: platform,
         logger: logger,
@@ -162,10 +174,13 @@ void main() {
         command: 'crash',
       );
 
-      expect(logger.errorText, contains('Failed to send crash report due to a network error'));
+      expect(logger.errorText,
+          contains('Failed to send crash report due to a network error'));
     });
 
-    testWithoutContext('should print an explanatory message when there is an HttpException', () async {
+    testWithoutContext(
+        'should print an explanatory message when there is an HttpException',
+        () async {
       final CrashReportSender crashReportSender = CrashReportSender(
         client: CrashingCrashReportSender(const HttpException('no internets')),
         usage: testUsage,
@@ -181,10 +196,13 @@ void main() {
         command: 'crash',
       );
 
-      expect(logger.errorText, contains('Failed to send crash report due to a network error'));
+      expect(logger.errorText,
+          contains('Failed to send crash report due to a network error'));
     });
 
-    testWithoutContext('should print an explanatory message when there is a ClientException', () async {
+    testWithoutContext(
+        'should print an explanatory message when there is a ClientException',
+        () async {
       final CrashReportSender crashReportSender = CrashReportSender(
         client: CrashingCrashReportSender(const HttpException('no internets')),
         usage: testUsage,
@@ -200,10 +218,12 @@ void main() {
         command: 'crash',
       );
 
-      expect(logger.errorText, contains('Failed to send crash report due to a network error'));
+      expect(logger.errorText,
+          contains('Failed to send crash report due to a network error'));
     });
 
-    testWithoutContext('should send only one crash report when sent many times', () async {
+    testWithoutContext('should send only one crash report when sent many times',
+        () async {
       final RequestInfo requestInfo = RequestInfo();
 
       final CrashReportSender crashReportSender = CrashReportSender(
@@ -246,7 +266,8 @@ void main() {
       await verifyCrashReportSent(requestInfo, crashes: 4);
     });
 
-    testWithoutContext('should not send a crash report if on a user-branch', () async {
+    testWithoutContext('should not send a crash report if on a user-branch',
+        () async {
       String? method;
       Uri? uri;
 
@@ -292,7 +313,8 @@ void main() {
       final Platform environmentPlatform = FakePlatform(
         environment: <String, String>{
           'HOME': '/',
-          'FLUTTER_CRASH_SERVER_BASE_URL': 'https://localhost:12345/fake_server',
+          'FLUTTER_CRASH_SERVER_BASE_URL':
+              'https://localhost:12345/fake_server',
         },
         script: Uri(scheme: 'data'),
       );
@@ -314,16 +336,18 @@ void main() {
 
       // Verify that we sent the crash report.
       expect(uri, isNotNull);
-      expect(uri, Uri(
-        scheme: 'https',
-        host: 'localhost',
-        port: 12345,
-        path: '/fake_server',
-        queryParameters: <String, String>{
-          'product': 'Flutter_Tools',
-          'version': 'test-version',
-        },
-      ));
+      expect(
+          uri,
+          Uri(
+            scheme: 'https',
+            host: 'localhost',
+            port: 12345,
+            path: '/fake_server',
+            queryParameters: <String, String>{
+              'product': 'Flutter_Tools',
+              'version': 'test-version',
+            },
+          ));
     });
   });
 }
@@ -335,47 +359,50 @@ class RequestInfo {
 }
 
 class MockCrashReportSender extends MockClient {
-  MockCrashReportSender(RequestInfo crashInfo) : super((Request request) async {
-    MockCrashReportSender.sendCalls++;
-    crashInfo.method = request.method;
-    crashInfo.uri = request.url;
+  MockCrashReportSender(RequestInfo crashInfo)
+      : super((Request request) async {
+          MockCrashReportSender.sendCalls++;
+          crashInfo.method = request.method;
+          crashInfo.uri = request.url;
 
-    // A very ad-hoc multipart request parser. Good enough for this test.
-    String? boundary = request.headers['Content-Type'];
-    boundary = boundary?.substring(boundary.indexOf('boundary=') + 9);
-    crashInfo.fields = Map<String, String>.fromIterable(
-      utf8.decode(request.bodyBytes)
-        .split('--$boundary')
-        .map<List<String>?>((String part) {
-        final Match? nameMatch = RegExp(r'name="(.*)"').firstMatch(part);
-        if (nameMatch == null) {
-          return null;
-        }
-        final String name = nameMatch[1]!;
-        final String value = part.split('\n').skip(2).join('\n').trim();
-        return <String>[name, value];
-      }).whereType<List<String>>(),
-      key: (dynamic key) {
-        final List<String> pair = key as List<String>;
-        return pair[0];
-      },
-      value: (dynamic value) {
-        final List<String> pair = value as List<String>;
-        return pair[1];
-      },
-    );
+          // A very ad-hoc multipart request parser. Good enough for this test.
+          String? boundary = request.headers['Content-Type'];
+          boundary = boundary?.substring(boundary.indexOf('boundary=') + 9);
+          crashInfo.fields = Map<String, String>.fromIterable(
+            utf8
+                .decode(request.bodyBytes)
+                .split('--$boundary')
+                .map<List<String>?>((String part) {
+              final Match? nameMatch = RegExp(r'name="(.*)"').firstMatch(part);
+              if (nameMatch == null) {
+                return null;
+              }
+              final String name = nameMatch[1]!;
+              final String value = part.split('\n').skip(2).join('\n').trim();
+              return <String>[name, value];
+            }).where((element) => element != null),
+            key: (dynamic key) {
+              final List<String> pair = key as List<String>;
+              return pair[0];
+            },
+            value: (dynamic value) {
+              final List<String> pair = value as List<String>;
+              return pair[1];
+            },
+          );
 
-    return Response(
-      'test-report-id',
-      200,
-    );
-  });
+          return Response(
+            'test-report-id',
+            200,
+          );
+        });
 
   static int sendCalls = 0;
 }
 
 class CrashingCrashReportSender extends MockClient {
-  CrashingCrashReportSender(Object exception) : super((Request request) async {
-    throw exception;
-  });
+  CrashingCrashReportSender(Object exception)
+      : super((Request request) async {
+          throw exception;
+        });
 }
