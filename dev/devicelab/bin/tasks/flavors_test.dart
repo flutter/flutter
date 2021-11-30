4013adf1e1bfd0fd5dfcb6739055fa1dc6d98bb3
Jenn Magder <magder@google.com>
2021-11-29 20:43:35 -0800

    dead code

--- dev/devicelab/bin/tasks/flavors_test.dart
+++ dev/devicelab/bin/tasks/flavors_test.dart
@@ -10,14 +10,13 @@ import 'package:flutter_devicelab/tasks/integration_tests.dart';
 Future<void> main() async {
   deviceOperatingSystem = DeviceOperatingSystem.android;
   await task(() async {
-    // This is a test that we can use to close the tree until
-    // https://github.com/flutter/flutter/issues/74529 is addressed.
-
-    throw TaskResult.failure('Failing to close the tree for https://github.com/flutter/flutter/issues/94356');
-
     await createFlavorsTest().call();
     await createIntegrationTestFlavorsTest().call();
 
-    return TaskResult.success(null);
+    // This is a test that we can use to close the tree until
+    // https://github.com/flutter/flutter/issues/74529 is addressed.
+
+    return TaskResult.failure('Failing to close the tree for https://github.com/flutter/flutter/issues/94356');
+    // return TaskResult.success(null);
   });
 }
