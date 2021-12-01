3f688f6e83ae54bf4ee45fc34957b51b9898cf5f
Zachary Anderson <zanderso@users.noreply.github.com>
2021-11-30 16:16:11 -0800

    Reopen the tree by fixing flavors_test (#94436)

--- dev/devicelab/bin/tasks/flavors_test.dart
+++ dev/devicelab/bin/tasks/flavors_test.dart
@@ -13,10 +13,6 @@ Future<void> main() async {
     await createFlavorsTest().call();
     await createIntegrationTestFlavorsTest().call();
 
-    // This is a test that we can use to close the tree until
-    // https://github.com/flutter/flutter/issues/74529 is addressed.
-
-    return TaskResult.failure('Failing to close the tree for https://github.com/flutter/flutter/issues/94356');
-    // return TaskResult.success(null);
+    return TaskResult.success(null);
   });
 }
