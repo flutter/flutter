ece4d1295ca28cfcbbbba54e1b75fe39624e7114
陈昱 <30322203+ksballetba@users.noreply.github.com>
2021-12-03 05:19:01 +0800

    Make fragment leftover from an attach/detach race slightly safer (#29908)

--- shell/platform/android/io/flutter/embedding/android/FlutterFragment.java
+++ shell/platform/android/io/flutter/embedding/android/FlutterFragment.java
@@ -784,7 +784,9 @@ public class FlutterFragment extends Fragment
   // possible.
   @ActivityCallThrough
   public void onPostResume() {
-    delegate.onPostResume();
+    if (stillAttachedForEvent("onPostResume")) {
+      delegate.onPostResume();
+    }
   }
 
   @Override
