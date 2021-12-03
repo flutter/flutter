b109f59983b2f505f2aca3132ae53ff958cbaee4
Anna Gringauze <annagrin@google.com>
2021-12-03 15:53:30 -0800

    Fixed format issues

--- packages/flutter_tools/lib/src/run_hot.dart
+++ packages/flutter_tools/lib/src/run_hot.dart
@@ -611,7 +611,7 @@ class HotRunner extends ResidentRunner {
             // new isolates, which will be configured by the editor as they are
             // started.
             final List<Future<void>> breakpointAndExceptionRemoval = <Future<void>>[
-              device.vmService.service.setIsolatePauseMode(isolate.id, 
+              device.vmService.service.setIsolatePauseMode(isolate.id,
                 exceptionPauseMode: vm_service.ExceptionPauseMode.kNone),
               for (final vm_service.Breakpoint breakpoint in isolate.breakpoints)
                 device.vmService.service.removeBreakpoint(isolate.id, breakpoint.id)
