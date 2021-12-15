2fe684e031daf89541525f42a52f29a8443b9650
crisboarna <cristian.boarna@gmail.com>
2021-12-15 08:52:19 +0200

    feat(flutter_tools): changed timeoutexception used

--- packages/flutter_tools/lib/src/http_host_validator.dart
+++ packages/flutter_tools/lib/src/http_host_validator.dart
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:webdriver/async_core.dart';
+import 'dart:async';
 
 import 'base/io.dart';
 import 'base/platform.dart';
