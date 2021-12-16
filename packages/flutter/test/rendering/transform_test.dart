86dc8eeade195db8b706e89a83f5e873015a16e0
Mohammad Taheri <44123678+mohammadtaherri@users.noreply.github.com>
2021-12-16 12:40:00 +0330

    Add some tests
    This commit adds following tests:
    -RenderTransform - identity with filter quality
    -RenderTransform - identity with internal offset and filter quality
    -RenderTransform - translation with filter quality
    -RenderTransform - translation with internal offset and filter quality
    -RenderTransform - rotation with filter quality
    -RenderTransform - rotation with internal offset and filter quality
    -RenderTransform - perspective - globalToLocal with filter quality
    -RenderTransform - perspective - localToGlobal with filter quality

--- packages/flutter/test/rendering/transform_test.dart
+++ packages/flutter/test/rendering/transform_test.dart
@@ -31,12 +31,53 @@ void main() {
     expect(inner.localToGlobal(const Offset(25.0, 75.0)), equals(const Offset(25.0, 75.0)));
     expect(inner.localToGlobal(const Offset(50.0, 50.0)), equals(const Offset(50.0, 50.0)));
   });
+  
+  test('RenderTransform - identity with filter quality', () {
+    RenderBox inner;
+    final RenderBox sizer = RenderTransform(
+      transform: Matrix4.identity(),
+      alignment: Alignment.center,
+      filterQuality: FilterQuality.medium,
+      child: inner = RenderSizedBox(const Size(100.0, 100.0)),
+    );
+    layout(sizer, constraints: BoxConstraints.tight(const Size(100.0, 100.0)), alignment: Alignment.topLeft);
+    expect(inner.globalToLocal(Offset.zero), equals(Offset.zero));
+    expect(inner.globalToLocal(const Offset(100.0, 100.0)), equals(const Offset(100.0, 100.0)));
+    expect(inner.globalToLocal(const Offset(25.0, 75.0)), equals(const Offset(25.0, 75.0)));
+    expect(inner.globalToLocal(const Offset(50.0, 50.0)), equals(const Offset(50.0, 50.0)));
+    expect(inner.localToGlobal(Offset.zero), equals(Offset.zero));
+    expect(inner.localToGlobal(const Offset(100.0, 100.0)), equals(const Offset(100.0, 100.0)));
+    expect(inner.localToGlobal(const Offset(25.0, 75.0)), equals(const Offset(25.0, 75.0)));
+    expect(inner.localToGlobal(const Offset(50.0, 50.0)), equals(const Offset(50.0, 50.0)));
+  });
 
   test('RenderTransform - identity with internal offset', () {
     RenderBox inner;
     final RenderBox sizer = RenderTransform(
       transform: Matrix4.identity(),
       alignment: Alignment.center,
+      child: RenderPadding(
+        padding: const EdgeInsets.only(left: 20.0),
+        child: inner = RenderSizedBox(const Size(80.0, 100.0)),
+      ),
+    );
+    layout(sizer, constraints: BoxConstraints.tight(const Size(100.0, 100.0)), alignment: Alignment.topLeft);
+    expect(inner.globalToLocal(Offset.zero), equals(const Offset(-20.0, 0.0)));
+    expect(inner.globalToLocal(const Offset(100.0, 100.0)), equals(const Offset(80.0, 100.0)));
+    expect(inner.globalToLocal(const Offset(25.0, 75.0)), equals(const Offset(5.0, 75.0)));
+    expect(inner.globalToLocal(const Offset(50.0, 50.0)), equals(const Offset(30.0, 50.0)));
+    expect(inner.localToGlobal(Offset.zero), equals(const Offset(20.0, 0.0)));
+    expect(inner.localToGlobal(const Offset(100.0, 100.0)), equals(const Offset(120.0, 100.0)));
+    expect(inner.localToGlobal(const Offset(25.0, 75.0)), equals(const Offset(45.0, 75.0)));
+    expect(inner.localToGlobal(const Offset(50.0, 50.0)), equals(const Offset(70.0, 50.0)));
+  });
+  
+  test('RenderTransform - identity with internal offset and filter quality', () {
+    RenderBox inner;
+    final RenderBox sizer = RenderTransform(
+      transform: Matrix4.identity(),
+      alignment: Alignment.center,
+      filterQuality: FilterQuality.medium,
       child: RenderPadding(
         padding: const EdgeInsets.only(left: 20.0),
         child: inner = RenderSizedBox(const Size(80.0, 100.0)),
@@ -70,6 +111,25 @@ void main() {
     expect(inner.localToGlobal(const Offset(25.0, 75.0)), equals(const Offset(75.0, 275.0)));
     expect(inner.localToGlobal(const Offset(50.0, 50.0)), equals(const Offset(100.0, 250.0)));
   });
+  
+  test('RenderTransform - translation with filter quality', () {
+    RenderBox inner;
+    final RenderBox sizer = RenderTransform(
+      transform: Matrix4.translationValues(50.0, 200.0, 0.0),
+      alignment: Alignment.center,
+      filterQuality: FilterQuality.medium,
+      child: inner = RenderSizedBox(const Size(100.0, 100.0)),
+    );
+    layout(sizer, constraints: BoxConstraints.tight(const Size(100.0, 100.0)), alignment: Alignment.topLeft);
+    expect(inner.globalToLocal(Offset.zero), equals(const Offset(-50.0, -200.0)));
+    expect(inner.globalToLocal(const Offset(100.0, 100.0)), equals(const Offset(50.0, -100.0)));
+    expect(inner.globalToLocal(const Offset(25.0, 75.0)), equals(const Offset(-25.0, -125.0)));
+    expect(inner.globalToLocal(const Offset(50.0, 50.0)), equals(const Offset(0.0, -150.0)));
+    expect(inner.localToGlobal(Offset.zero), equals(const Offset(50.0, 200.0)));
+    expect(inner.localToGlobal(const Offset(100.0, 100.0)), equals(const Offset(150.0, 300.0)));
+    expect(inner.localToGlobal(const Offset(25.0, 75.0)), equals(const Offset(75.0, 275.0)));
+    expect(inner.localToGlobal(const Offset(50.0, 50.0)), equals(const Offset(100.0, 250.0)));
+  });
 
   test('RenderTransform - translation with internal offset', () {
     RenderBox inner;
@@ -91,6 +151,29 @@ void main() {
     expect(inner.localToGlobal(const Offset(25.0, 75.0)), equals(const Offset(95.0, 275.0)));
     expect(inner.localToGlobal(const Offset(50.0, 50.0)), equals(const Offset(120.0, 250.0)));
   });
+  
+  test('RenderTransform - translation with internal offset and filter quality', () {
+    RenderBox inner;
+    final RenderBox sizer = RenderTransform(
+      transform: Matrix4.translationValues(50.0, 200.0, 0.0),
+      alignment: Alignment.center,
+      filterQuality: FilterQuality.medium,
+      child: RenderPadding(
+        padding: const EdgeInsets.only(left: 20.0),
+        child: inner = RenderSizedBox(const Size(80.0, 100.0)),
+      ),
+    );
+    layout(sizer, constraints: BoxConstraints.tight(const Size(100.0, 100.0)), alignment: Alignment.topLeft);
+    expect(inner.globalToLocal(Offset.zero), equals(const Offset(-70.0, -200.0)));
+    expect(inner.globalToLocal(const Offset(100.0, 100.0)), equals(const Offset(30.0, -100.0)));
+    expect(inner.globalToLocal(const Offset(25.0, 75.0)), equals(const Offset(-45.0, -125.0)));
+    expect(inner.globalToLocal(const Offset(50.0, 50.0)), equals(const Offset(-20.0, -150.0)));
+    expect(inner.localToGlobal(Offset.zero), equals(const Offset(70.0, 200.0)));
+    expect(inner.localToGlobal(const Offset(100.0, 100.0)), equals(const Offset(170.0, 300.0)));
+    expect(inner.localToGlobal(const Offset(25.0, 75.0)), equals(const Offset(95.0, 275.0)));
+    expect(inner.localToGlobal(const Offset(50.0, 50.0)), equals(const Offset(120.0, 250.0)));
+  });
+
 
   test('RenderTransform - rotation', () {
     RenderBox inner;
@@ -109,12 +192,53 @@ void main() {
     expect(round(inner.localToGlobal(const Offset(25.0, 75.0))), equals(const Offset(75.0, 25.0)));
     expect(round(inner.localToGlobal(const Offset(50.0, 50.0))), equals(const Offset(50.0, 50.0)));
   });
+  
+  test('RenderTransform - rotation with filter quality', () {
+    RenderBox inner;
+    final RenderBox sizer = RenderTransform(
+      transform: Matrix4.rotationZ(math.pi),
+      alignment: Alignment.center,
+      filterQuality: FilterQuality.medium,
+      child: inner = RenderSizedBox(const Size(100.0, 100.0)),
+    );
+    layout(sizer, constraints: BoxConstraints.tight(const Size(100.0, 100.0)), alignment: Alignment.topLeft);
+    expect(round(inner.globalToLocal(Offset.zero)), equals(const Offset(100.0, 100.0)));
+    expect(round(inner.globalToLocal(const Offset(100.0, 100.0))), equals(Offset.zero));
+    expect(round(inner.globalToLocal(const Offset(25.0, 75.0))), equals(const Offset(75.0, 25.0)));
+    expect(round(inner.globalToLocal(const Offset(50.0, 50.0))), equals(const Offset(50.0, 50.0)));
+    expect(round(inner.localToGlobal(Offset.zero)), equals(const Offset(100.0, 100.0)));
+    expect(round(inner.localToGlobal(const Offset(100.0, 100.0))), equals(Offset.zero));
+    expect(round(inner.localToGlobal(const Offset(25.0, 75.0))), equals(const Offset(75.0, 25.0)));
+    expect(round(inner.localToGlobal(const Offset(50.0, 50.0))), equals(const Offset(50.0, 50.0)));
+  });
 
   test('RenderTransform - rotation with internal offset', () {
     RenderBox inner;
     final RenderBox sizer = RenderTransform(
       transform: Matrix4.rotationZ(math.pi),
       alignment: Alignment.center,
+      child: RenderPadding(
+        padding: const EdgeInsets.only(left: 20.0),
+        child: inner = RenderSizedBox(const Size(80.0, 100.0)),
+      ),
+    );
+    layout(sizer, constraints: BoxConstraints.tight(const Size(100.0, 100.0)), alignment: Alignment.topLeft);
+    expect(round(inner.globalToLocal(Offset.zero)), equals(const Offset(80.0, 100.0)));
+    expect(round(inner.globalToLocal(const Offset(100.0, 100.0))), equals(const Offset(-20.0, 0.0)));
+    expect(round(inner.globalToLocal(const Offset(25.0, 75.0))), equals(const Offset(55.0, 25.0)));
+    expect(round(inner.globalToLocal(const Offset(50.0, 50.0))), equals(const Offset(30.0, 50.0)));
+    expect(round(inner.localToGlobal(Offset.zero)), equals(const Offset(80.0, 100.0)));
+    expect(round(inner.localToGlobal(const Offset(100.0, 100.0))), equals(const Offset(-20.0, 0.0)));
+    expect(round(inner.localToGlobal(const Offset(25.0, 75.0))), equals(const Offset(55.0, 25.0)));
+    expect(round(inner.localToGlobal(const Offset(50.0, 50.0))), equals(const Offset(30.0, 50.0)));
+  });
+  
+  test('RenderTransform - rotation with internal offset and filter quality', () {
+    RenderBox inner;
+    final RenderBox sizer = RenderTransform(
+      transform: Matrix4.rotationZ(math.pi),
+      alignment: Alignment.center,
+      filterQuality: FilterQuality.medium,
       child: RenderPadding(
         padding: const EdgeInsets.only(left: 20.0),
         child: inner = RenderSizedBox(const Size(80.0, 100.0)),
@@ -150,12 +274,50 @@ void main() {
       equals(100 - round(inner.globalToLocal(const Offset(25.0, 83.0))).dy),
     );
   });
+  
+   test('RenderTransform - perspective - globalToLocal with filter quality', () {
+    RenderBox inner;
+    final RenderBox sizer = RenderTransform(
+      transform: rotateAroundXAxis(math.pi * 0.25), // at pi/4, we are about 70 pixels high
+      alignment: Alignment.center,
+      filterQuality: FilterQuality.medium,
+      child: inner = RenderSizedBox(const Size(100.0, 100.0)),
+    );
+    layout(sizer, constraints: BoxConstraints.tight(const Size(100.0, 100.0)), alignment: Alignment.topLeft);
+
+    expect(round(inner.globalToLocal(const Offset(25.0, 50.0))), equals(const Offset(25.0, 50.0)));
+    expect(inner.globalToLocal(const Offset(25.0, 17.0)).dy, greaterThan(0.0));
+    expect(inner.globalToLocal(const Offset(25.0, 17.0)).dy, lessThan(10.0));
+    expect(inner.globalToLocal(const Offset(25.0, 83.0)).dy, greaterThan(90.0));
+    expect(inner.globalToLocal(const Offset(25.0, 83.0)).dy, lessThan(100.0));
+    expect(
+      round(inner.globalToLocal(const Offset(25.0, 17.0))).dy,
+      equals(100 - round(inner.globalToLocal(const Offset(25.0, 83.0))).dy),
+    );
+  });
 
   test('RenderTransform - perspective - localToGlobal', () {
     RenderBox inner;
     final RenderBox sizer = RenderTransform(
       transform: rotateAroundXAxis(math.pi * 0.4999), // at pi/2, we're seeing the box on its edge,
       alignment: Alignment.center,
+      child: inner = RenderSizedBox(const Size(100.0, 100.0)),
+    );
+    layout(sizer, constraints: BoxConstraints.tight(const Size(100.0, 100.0)), alignment: Alignment.topLeft);
+
+    // the inner widget has a height of about half a pixel at this rotation, so
+    // everything should end up around the middle of the outer box.
+    expect(inner.localToGlobal(const Offset(25.0, 50.0)), equals(const Offset(25.0, 50.0)));
+    expect(round(inner.localToGlobal(const Offset(25.0, 75.0))), equals(const Offset(25.0, 50.0)));
+    expect(round(inner.localToGlobal(const Offset(25.0, 100.0))), equals(const Offset(25.0, 50.0)));
+  });
+  
+  test('RenderTransform - perspective - localToGlobal with filter quality', () {
+    RenderBox inner;
+    final RenderBox sizer = RenderTransform(
+      transform: rotateAroundXAxis(math.pi * 0.4999), // at pi/2, we're seeing the box on its edge,
+      alignment: Alignment.center,
+      filterQuality: FilterQuality.medium,
       child: inner = RenderSizedBox(const Size(100.0, 100.0)),
     );
     layout(sizer, constraints: BoxConstraints.tight(const Size(100.0, 100.0)), alignment: Alignment.topLeft);
