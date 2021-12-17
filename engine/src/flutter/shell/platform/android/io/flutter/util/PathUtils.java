8d18d929e581b6693350a42c262c76eef086f576
eggfly <lihaohua.cafebabe@bytedance.com>
2021-12-17 14:09:09 +0800

    [android] Provide a path instead of throwing NPE when getDir() suites returns null (flutter/engine#30367)

--- engine/src/flutter/shell/platform/android/io/flutter/util/PathUtils.java
+++ engine/src/flutter/shell/platform/android/io/flutter/util/PathUtils.java
@@ -6,21 +6,52 @@ package io.flutter.util;
 
 import android.content.Context;
 import android.os.Build;
+import java.io.File;
 
 public final class PathUtils {
   public static String getFilesDir(Context applicationContext) {
-    return applicationContext.getFilesDir().getPath();
+    File filesDir = applicationContext.getFilesDir();
+    if (filesDir == null) {
+      filesDir = new File(getDataDirPath(applicationContext), "files");
+    }
+    return filesDir.getPath();
   }
 
   public static String getDataDirectory(Context applicationContext) {
-    return applicationContext.getDir("flutter", Context.MODE_PRIVATE).getPath();
+    final String name = "flutter";
+    File flutterDir = applicationContext.getDir(name, Context.MODE_PRIVATE);
+    if (flutterDir == null) {
+      flutterDir = new File(getDataDirPath(applicationContext), "app_" + name);
+    }
+    return flutterDir.getPath();
   }
 
   public static String getCacheDirectory(Context applicationContext) {
+    File cacheDir;
     if (Build.VERSION.SDK_INT >= 21) {
-      return applicationContext.getCodeCacheDir().getPath();
+      cacheDir = applicationContext.getCodeCacheDir();
+      if (cacheDir == null) {
+        cacheDir = applicationContext.getCacheDir();
+      }
     } else {
-      return applicationContext.getCacheDir().getPath();
+      cacheDir = applicationContext.getCacheDir();
+    }
+    if (cacheDir == null) {
+      // This can happen if the disk is full. This code path is used to set up dart:io's
+      // `Directory.systemTemp`. It's unknown if the application will ever try to
+      // use that or not, so do not throw here. In this case, this directory does
+      // not exist because the disk is full, and the application will later get an
+      // exception when it tries to actually write.
+      cacheDir = new File(getDataDirPath(applicationContext), "cache");
+    }
+    return cacheDir.getPath();
+  }
+
+  private static String getDataDirPath(Context applicationContext) {
+    if (Build.VERSION.SDK_INT >= 24) {
+      return applicationContext.getDataDir().getPath();
+    } else {
+      return applicationContext.getApplicationInfo().dataDir;
     }
   }
 }
