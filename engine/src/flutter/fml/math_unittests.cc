e2bcd4d7c62ee6814190596a69ae8d4c8d374dc3
Chinmay Garde <chinmaygarde@google.com>
2021-12-20 16:12:43 -0800

    Add //flutter/fml/math.h with common math constants. (flutter/engine#30425)
    This came up as I was [debugging failures on Windows](https://ci.chromium.org/p/flutter/builders/try/Windows%20Host%20Engine/30618?) due to use of the
    non-portable constants.
    
    Fixes https://github.com/flutter/flutter/issues/82216

---
+++ engine/src/flutter/fml/math_unittests.cc
@@ -0,0 +1,27 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <cmath>
+
+#include "flutter/fml/math.h"
+#include "gtest/gtest.h"
+
+namespace flutter {
+namespace testing {
+
+TEST(MathTest, Constants) {
+  // Don't use the constants in cmath as those aren't portable.
+  EXPECT_FLOAT_EQ(std::log2(math::kE), math::kLog2_E);
+  EXPECT_FLOAT_EQ(std::log10(math::kE), math::kLog10_E);
+  EXPECT_FLOAT_EQ(std::log(2.0f), math::klogE_2);
+  EXPECT_FLOAT_EQ(math::kPi / 2.0f, math::kPiOver2);
+  EXPECT_FLOAT_EQ(math::kPi / 4.0f, math::kPiOver4);
+  EXPECT_FLOAT_EQ(1.0f / math::kPi, math::k1OverPi);
+  EXPECT_FLOAT_EQ(2.0f / math::kPi, math::k2OverPi);
+  EXPECT_FLOAT_EQ(std::sqrt(2.0f), math::kSqrt2);
+  EXPECT_FLOAT_EQ(1.0f / std::sqrt(2.0f), math::k1OverSqrt2);
+}
+
+}  // namespace testing
+}  // namespace flutter
