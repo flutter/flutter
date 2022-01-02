e53d37a736e8cc55e03600d9ea399e543f4ab639
alex <alexandazar@gmail.com>
2022-01-01 16:26:29 -0800

    Update example code for InteractiveViewer.builder

--- examples/api/lib/widgets/interactive_viewer/interactive_viewer.builder.0.dart
+++ examples/api/lib/widgets/interactive_viewer/interactive_viewer.builder.0.dart
@@ -5,7 +5,6 @@
 // Flutter code sample for InteractiveViewer.builder
 
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:vector_math/vector_math_64.dart' show Quad, Vector3;
 
 void main() => runApp(const IVBuilderExampleApp());
@@ -38,71 +37,31 @@ class _IVBuilderExampleState extends State<_IVBuilderExample> {
   static const double _cellWidth = 200.0;
   static const double _cellHeight = 26.0;
 
-  // Returns true iff the given cell is currently visible. Caches viewport
-  // calculations.
-  Quad? _cachedViewport;
-  late int _firstVisibleRow;
-  late int _firstVisibleColumn;
-  late int _lastVisibleRow;
-  late int _lastVisibleColumn;
-  bool _isCellVisible(int row, int column, Quad viewport) {
-    if (viewport != _cachedViewport) {
-      final Rect aabb = _axisAlignedBoundingBox(viewport);
-      _cachedViewport = viewport;
-      _firstVisibleRow = (aabb.top / _cellHeight).floor();
-      _firstVisibleColumn = (aabb.left / _cellWidth).floor();
-      _lastVisibleRow = (aabb.bottom / _cellHeight).floor();
-      _lastVisibleColumn = (aabb.right / _cellWidth).floor();
-    }
-    return row >= _firstVisibleRow &&
-        row <= _lastVisibleRow &&
-        column >= _firstVisibleColumn &&
-        column <= _lastVisibleColumn;
-  }
-
-  // Returns the axis aligned bounding box for the given Quad, which might not
-  // be axis aligned.
-  Rect _axisAlignedBoundingBox(Quad quad) {
-    double? xMin;
-    double? xMax;
-    double? yMin;
-    double? yMax;
+  // Returns the axis aligned bounding box for the given Quad, which might not be axis aligned.
+  Rect axisAlignedBoundingBox(Quad quad) {
+    double xMin = quad.point0.x;
+    double xMax = quad.point0.x;
+    double yMin = quad.point0.y;
+    double yMax = quad.point0.y;
     for (final Vector3 point in <Vector3>[
-      quad.point0,
       quad.point1,
       quad.point2,
       quad.point3
     ]) {
-      if (xMin == null || point.x < xMin) {
+      if (point.x < xMin) {
         xMin = point.x;
-      }
-      if (xMax == null || point.x > xMax) {
+      } else if (point.x > xMax) {
         xMax = point.x;
       }
-      if (yMin == null || point.y < yMin) {
+
+      if (point.y < yMin) {
         yMin = point.y;
-      }
-      if (yMax == null || point.y > yMax) {
+      } else if (point.y > yMax) {
         yMax = point.y;
       }
     }
-    return Rect.fromLTRB(xMin!, yMin!, xMax!, yMax!);
-  }
 
-  void _onChangeTransformation() {
-    setState(() {});
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    _transformationController.addListener(_onChangeTransformation);
-  }
-
-  @override
-  void dispose() {
-    _transformationController.removeListener(_onChangeTransformation);
-    super.dispose();
+    return Rect.fromLTRB(xMin, yMin, xMax, yMax);
   }
 
   @override
@@ -111,23 +70,18 @@ class _IVBuilderExampleState extends State<_IVBuilderExample> {
       child: LayoutBuilder(
         builder: (BuildContext context, BoxConstraints constraints) {
           return InteractiveViewer.builder(
-            alignPanAxis: true,
-            scaleEnabled: false,
+            boundaryMargin: const EdgeInsets.all(double.infinity),
             transformationController: _transformationController,
             builder: (BuildContext context, Quad viewport) {
               // A simple extension of Table that builds cells.
               return _TableBuilder(
-                  rowCount: 60,
-                  columnCount: 6,
                   cellWidth: _cellWidth,
+                  cellHeight: _cellHeight,
+                  viewport: axisAlignedBoundingBox(viewport),
                   builder: (BuildContext context, int row, int column) {
-                    if (!_isCellVisible(row, column, viewport)) {
-                      debugPrint('removing cell ($row, $column)');
-                      return Container(height: _cellHeight);
-                    }
-                    debugPrint('building cell ($row, $column)');
                     return Container(
                       height: _cellHeight,
+                      width: _cellWidth,
                       color: row % 2 + column % 2 == 1
                           ? Colors.white
                           : Colors.grey.withOpacity(0.1),
@@ -150,38 +104,41 @@ typedef _CellBuilder = Widget Function(
 
 class _TableBuilder extends StatelessWidget {
   const _TableBuilder({
-    required this.rowCount,
-    required this.columnCount,
     required this.cellWidth,
+    required this.cellHeight,
+    required this.viewport,
     required this.builder,
-  })  : assert(rowCount > 0),
-        assert(columnCount > 0);
+  });
 
-  final int rowCount;
-  final int columnCount;
   final double cellWidth;
+  final double cellHeight;
+  final Rect viewport;
   final _CellBuilder builder;
 
   @override
   Widget build(BuildContext context) {
-    return Table(
-      // ignore: prefer_const_literals_to_create_immutables
-      columnWidths: <int, TableColumnWidth>{
-        for (int column = 0; column < columnCount; column++)
-          column: FixedColumnWidth(cellWidth),
-      },
-      // ignore: prefer_const_literals_to_create_immutables
-      children: <TableRow>[
-        for (int row = 0; row < rowCount; row++)
-          // ignore: prefer_const_constructors
-          TableRow(
-            // ignore: prefer_const_literals_to_create_immutables
-            children: <Widget>[
-              for (int column = 0; column < columnCount; column++)
-                builder(context, row, column),
-            ],
-          ),
-      ],
-    );
+    int firstRow = (viewport.top / cellHeight).floor();
+    int lastRow = (viewport.bottom / cellHeight).ceil();
+    int firstCol = (viewport.left / cellWidth).floor();
+    int lastCol = (viewport.right / cellWidth).ceil();
+
+    int totalCells = (lastRow - firstRow) * (lastCol - firstCol);
+    debugPrint("Total cells: $totalCells");
+
+    return SizedBox(
+        // Stack needs constraints, even though we then Clip.none outside of them.
+        width: 1,
+        height: 1,
+        child: Stack(
+          clipBehavior: Clip.none,
+          children: <Widget>[
+            for (int row = firstRow; row < lastRow; row++)
+              for (int col = firstCol; col < lastCol; col++)
+                Positioned(
+                    left: col * cellWidth,
+                    top: row * cellHeight,
+                    child: builder(context, row, col)),
+          ],
+        ));
   }
 }
