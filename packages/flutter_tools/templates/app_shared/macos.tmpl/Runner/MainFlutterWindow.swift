721229ceab09e500bdee3d11cbe14c312e20cef8
Rundong Du <rundongdu@google.com>
2022-01-04 09:56:27 -0800

    Fix FlutterViewController.init in macOS template
    In `Runner/MainFlutterWindow.swift` of Flutter's macOS template, there
    is a call to `FlutterViewController.init()`, which is not defined for
    the `FlutterViewController` class and will be dispatched to the
    `NSViewController.init()` method by objc_msgSend. However,
    `NSViewController.init()` is neither documented nor appearing in the
    header file, which makes `FlutterViewController.init()` depending on a
    private initializer.
    
    This change fixes the issue by calling
    `FlutterViewController.init(nibName: nil, bundle: nil)` directly.

--- packages/flutter_tools/templates/app_shared/macos.tmpl/Runner/MainFlutterWindow.swift
+++ packages/flutter_tools/templates/app_shared/macos.tmpl/Runner/MainFlutterWindow.swift
@@ -3,7 +3,7 @@ import FlutterMacOS
 
 class MainFlutterWindow: NSWindow {
   override func awakeFromNib() {
-    let flutterViewController = FlutterViewController.init()
+    let flutterViewController = FlutterViewController.init(nibName: nil, bundle: nil)
     let windowFrame = self.frame
     self.contentViewController = flutterViewController
     self.setFrame(windowFrame, display: true)
