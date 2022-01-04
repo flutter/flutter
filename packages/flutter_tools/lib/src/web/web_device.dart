a3f3fca8c0f9d31737f1c2f8af5ce02eb6cb144e
wangying.666 <wangying.666@bytedance.com>
2022-01-04 19:28:57 +0800

    fix test error

--- packages/flutter_tools/lib/src/web/web_device.dart
+++ packages/flutter_tools/lib/src/web/web_device.dart
@@ -2,11 +2,11 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:flutter_tools/src/base/common.dart';
 import 'package:meta/meta.dart';
 import 'package:process/process.dart';
 
 import '../application_package.dart';
+import '../base/common.dart';
 import '../base/file_system.dart';
 import '../base/io.dart';
 import '../base/logger.dart';
