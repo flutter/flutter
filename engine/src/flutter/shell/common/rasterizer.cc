41819f25e7349e638e136a68d1cc114ff10a3ed7
Kaushik Iska <iska.kaushik@gmail.com>
2022-01-05 17:14:31 -0500

    Revert "Only provide frame damage to rasterizer if partial repaint is enabled (#30461)" (flutter/engine#30696)

--- engine/src/flutter/shell/common/rasterizer.cc
+++ engine/src/flutter/shell/common/rasterizer.cc
@@ -560,25 +560,22 @@ RasterStatus Rasterizer::DrawToSurfaceUnsafe(
         external_view_embedder_ &&
         (!raster_thread_merger_ || raster_thread_merger_->IsMerged());
 
-    std::unique_ptr<FrameDamage> damage;
+    FrameDamage damage;
     if (!disable_partial_repaint && frame->framebuffer_info().existing_damage) {
-      damage = std::make_unique<FrameDamage>();
-      damage->SetPreviousLayerTree(last_layer_tree_.get());
-      damage->AddAdditonalDamage(*frame->framebuffer_info().existing_damage);
+      damage.SetPreviousLayerTree(last_layer_tree_.get());
+      damage.AddAdditonalDamage(*frame->framebuffer_info().existing_damage);
     }
 
     RasterStatus raster_status =
-        compositor_frame->Raster(layer_tree, false, damage.get());
+        compositor_frame->Raster(layer_tree, false, &damage);
     if (raster_status == RasterStatus::kFailed ||
         raster_status == RasterStatus::kSkipAndRetry) {
       return raster_status;
     }
 
     SurfaceFrame::SubmitInfo submit_info;
-    if (damage) {
-      submit_info.frame_damage = damage->GetFrameDamage();
-      submit_info.buffer_damage = damage->GetBufferDamage();
-    }
+    submit_info.frame_damage = damage.GetFrameDamage();
+    submit_info.buffer_damage = damage.GetBufferDamage();
 
     frame->set_submit_info(submit_info);
 
