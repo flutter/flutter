0f05e03a16948301d67f794231e8d21bf9d9b30c
Jim Graham <flar@google.com>
2022-01-04 21:36:33 -0800

    stabilize the way we navigate multiple pages for E2E macrobenchmarks

--- dev/benchmarks/macrobenchmarks/test/opacity_peephole_grid_of_opacity_perf_e2e.dart
+++ dev/benchmarks/macrobenchmarks/test/opacity_peephole_grid_of_opacity_perf_e2e.dart
@@ -7,9 +7,12 @@ import 'package:macrobenchmarks/common.dart';
 import 'util.dart';
 
 void main() {
-  macroPerfTestE2E(
+  macroPerfTestMultiPageE2E(
     'opacity_peephole_grid_of_opacity_perf',
-    kOpacityPeepholeGridOfOpacityRouteName,
+    <ScrollableButtonRoute>[
+      ScrollableButtonRoute(kScrollableName, kOpacityPeepholeRouteName),
+      ScrollableButtonRoute(kOpacityScrollableName, kOpacityPeepholeGridOfOpacityRouteName)
+    ],
     pageDelay: const Duration(seconds: 1),
     duration: const Duration(seconds: 10),
   );
