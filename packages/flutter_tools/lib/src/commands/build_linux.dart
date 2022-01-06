18c59cdb1cf5b42dc2f8ded6eba36a2ea4c26b20
Jenn Magder <magder@google.com>
2022-01-05 16:45:12 -0800

    Migrate build commands to null safety (#95649)

--- packages/flutter_tools/lib/src/commands/build_linux.dart
+++ packages/flutter_tools/lib/src/commands/build_linux.dart
@@ -2,10 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// @dart = 2.8
-
-import 'package:meta/meta.dart';
-
 import '../base/analyze_size.dart';
 import '../base/common.dart';
 import '../base/os.dart';
@@ -21,7 +17,7 @@ import 'build.dart';
 /// A command to build a linux desktop target through a build shell script.
 class BuildLinuxCommand extends BuildSubCommand {
   BuildLinuxCommand({
-    @required OperatingSystemUtils operatingSystemUtils,
+    required OperatingSystemUtils operatingSystemUtils,
     bool verboseHelp = false,
   }) : _operatingSystemUtils = operatingSystemUtils,
        super(verboseHelp: verboseHelp) {
@@ -63,7 +59,7 @@ class BuildLinuxCommand extends BuildSubCommand {
     final BuildInfo buildInfo = await getBuildInfo();
     final FlutterProject flutterProject = FlutterProject.current();
     final TargetPlatform targetPlatform =
-        getTargetPlatformForName(stringArg('target-platform'));
+        getTargetPlatformForName(stringArg('target-platform')!);
     final bool needCrossBuild =
         getNameForHostPlatformArch(_operatingSystemUtils.hostPlatform)
             != getNameForTargetPlatformArch(targetPlatform);
@@ -97,7 +93,7 @@ class BuildLinuxCommand extends BuildSubCommand {
       ),
       needCrossBuild: needCrossBuild,
       targetPlatform: targetPlatform,
-      targetSysroot: stringArg('target-sysroot'),
+      targetSysroot: stringArg('target-sysroot')!,
     );
     return FlutterCommandResult.success();
   }
