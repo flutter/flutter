9490917408a5715ac7a02b1c27f86c089f9cafe6
Jami Couch <fbcouch@gmail.com>
2022-01-11 13:00:12 -0600

    Support Scribble Handwriting (#75472)

--- packages/flutter/test/cupertino/text_field_test.dart
+++ packages/flutter/test/cupertino/text_field_test.dart
@@ -2368,7 +2368,7 @@ void main() {
     );
 
     final RenderEditable renderEditable = tester.renderObject<RenderEditable>(
-      find.byElementPredicate((Element element) => element.renderObject is RenderEditable),
+      find.byElementPredicate((Element element) => element.renderObject is RenderEditable).last,
     );
 
     List<TextSelectionPoint> lastCharEndpoint = renderEditable.getEndpointsForSelection(
@@ -3166,7 +3166,7 @@ void main() {
 
       expect(
         tester.renderObject<RenderEditable>(
-          find.byElementPredicate((Element element) => element.renderObject is RenderEditable),
+          find.byElementPredicate((Element element) => element.renderObject is RenderEditable).last,
         ).text!.style!.color,
         isSameColorAs(CupertinoColors.white),
       );
