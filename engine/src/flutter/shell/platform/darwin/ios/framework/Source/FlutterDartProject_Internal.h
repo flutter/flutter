fef332723eabc82ffd15143ee41d84b8bb37989c
ColdPaleLight <31977171+ColdPaleLight@users.noreply.github.com>
2022-01-12 02:25:10 +0800

    Remove unused setPersistentIsolateData function (flutter/engine#30795)

--- engine/src/flutter/shell/platform/darwin/ios/framework/Source/FlutterDartProject_Internal.h
+++ engine/src/flutter/shell/platform/darwin/ios/framework/Source/FlutterDartProject_Internal.h
@@ -36,19 +36,6 @@ flutter::Settings FLTDefaultSettingsForBundle(NSBundle* bundle = nil);
 + (NSString*)domainNetworkPolicy:(NSDictionary*)appTransportSecurity;
 + (bool)allowsArbitraryLoads:(NSDictionary*)appTransportSecurity;
 
-/**
- * The embedder can specify data that the isolate can request synchronously on launch. Engines
- * launched using this configuration can access the persistent isolate data via the
- * `PlatformDispatcher.getPersistentIsolateData` accessor.
- *
- * @param data The persistent isolate data. This data is persistent for the duration of the Flutter
- *             application and is available even after isolate restarts. Because of this lifecycle,
- *             the size of this data must be kept to a minimum and platform channels used for
- *             communication that does not require synchronous embedder to isolate communication
- *             close to isolate launch.
- **/
-- (void)setPersistentIsolateData:(NSData*)data;
-
 @end
 
 NS_ASSUME_NONNULL_END
