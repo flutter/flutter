2e84c6b402a691ecde8717fdf386ffea239908b8
MxSoul <max.miracle.soul@gmail.com>
2022-01-12 16:25:39 +0800

    Roll back comment changes

--- packages/flutter/lib/src/widgets/scroll_simulation.dart
+++ packages/flutter/lib/src/widgets/scroll_simulation.dart
@@ -195,11 +195,11 @@ class BouncingScrollSimulation extends Simulation with ScrollSimulationMixin {
 ///
 ///  * [BouncingScrollSimulation], which implements iOS scroll physics.
 ///
-/// This class is based on Scroller.java from Android:
-///   https://android.googlesource.com/platform/frameworks/base/+/master/core/java/android/widget
-///
-/// The "See..." comments below refer to Scroller methods and values. Some
-/// simplifications have been made.
+// This class is based on Scroller.java from Android:
+//   https://android.googlesource.com/platform/frameworks/base/+/master/core/java/android/widget
+//
+// The "See..." comments below refer to Scroller methods and values. Some
+// simplifications have been made.
 class ClampingScrollSimulation extends Simulation {
   /// Creates a scroll physics simulation that matches Android scrolling.
   ClampingScrollSimulation({
@@ -236,38 +236,38 @@ class ClampingScrollSimulation extends Simulation {
     return friction * 61774.04968;
   }
 
-  /// See getSplineFlingDuration(). Returns a value in seconds.
+  // See getSplineFlingDuration(). Returns a value in seconds.
   double _flingDuration(double velocity) {
-    /// See mPhysicalCoeff
+    // See mPhysicalCoeff
     final double scaledFriction = friction * _decelerationForFriction(0.84);
 
-    /// See getSplineDeceleration().
+    // See getSplineDeceleration().
     final double deceleration = math.log(0.35 * velocity.abs() / scaledFriction);
 
     return math.exp(deceleration / (_kDecelerationRate - 1.0));
   }
 
-  /// Based on a cubic curve fit to the Scroller.computeScrollOffset() values
-  /// produced for an initial velocity of 4000. The value of Scroller.getDuration()
-  /// and Scroller.getFinalY() were 686ms and 961 pixels respectively.
-  ///
-  /// Algebra courtesy of Wolfram Alpha.
-  ///
-  /// f(x) = scrollOffset, x is time in milliseconds
-  /// f(x) = 3.60882×10^-6 x^3 - 0.00668009 x^2 + 4.29427 x - 3.15307
-  /// f(x) = 3.60882×10^-6 x^3 - 0.00668009 x^2 + 4.29427 x, so f(0) is 0
-  /// f(686ms) = 961 pixels
-  /// Scale to f(0 <= t <= 1.0), x = t * 686
-  /// f(t) = 1165.03 t^3 - 3143.62 t^2 + 2945.87 t
-  /// Scale f(t) so that 0.0 <= f(t) <= 1.0
-  /// f(t) = (1165.03 t^3 - 3143.62 t^2 + 2945.87 t) / 961.0
-  ///      = 1.2 t^3 - 3.27 t^2 + 3.065 t
+  // Based on a cubic curve fit to the Scroller.computeScrollOffset() values
+  // produced for an initial velocity of 4000. The value of Scroller.getDuration()
+  // and Scroller.getFinalY() were 686ms and 961 pixels respectively.
+  //
+  // Algebra courtesy of Wolfram Alpha.
+  //
+  // f(x) = scrollOffset, x is time in milliseconds
+  // f(x) = 3.60882×10^-6 x^3 - 0.00668009 x^2 + 4.29427 x - 3.15307
+  // f(x) = 3.60882×10^-6 x^3 - 0.00668009 x^2 + 4.29427 x, so f(0) is 0
+  // f(686ms) = 961 pixels
+  // Scale to f(0 <= t <= 1.0), x = t * 686
+  // f(t) = 1165.03 t^3 - 3143.62 t^2 + 2945.87 t
+  // Scale f(t) so that 0.0 <= f(t) <= 1.0
+  // f(t) = (1165.03 t^3 - 3143.62 t^2 + 2945.87 t) / 961.0
+  //      = 1.2 t^3 - 3.27 t^2 + 3.065 t
   static const double _initialVelocityPenetration = 3.065;
   static double _flingDistancePenetration(double t) {
     return (1.2 * t * t * t) - (3.27 * t * t) + (_initialVelocityPenetration * t);
   }
 
-  /// The derivative of the _flingDistancePenetration() function.
+  // The derivative of the _flingDistancePenetration() function.
   static double _flingVelocityPenetration(double t) {
     return (3.6 * t * t) - (6.54 * t) + _initialVelocityPenetration;
   }
