a8d519f4819d0f2b66ad1c52695e23e7af94802f
Alexander Aprelev <aam@google.com>
2022-01-13 09:11:45 -0800

    Remove use of '--enable-isolate-groups' dart vm flag. (#30839)
    The flag was removed after isolate groups were enabled by default.
    
    Fixes https://github.com/flutter/flutter/issues/95331

--- runtime/dart_isolate.cc
+++ runtime/dart_isolate.cc
@@ -566,7 +566,7 @@ bool DartIsolate::LoadKernel(std::shared_ptr<const fml::Mapping> mapping,
 
   tonic::DartState::Scope scope(this);
 
-  if (!child_isolate || !Dart_IsVMFlagSet("--enable-isolate-groups")) {
+  if (!child_isolate) {
     // Use root library provided by kernel in favor of one provided by snapshot.
     Dart_SetRootLibrary(Dart_Null());
 
