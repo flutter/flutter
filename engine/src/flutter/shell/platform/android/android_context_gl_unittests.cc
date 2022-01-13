03e2fe4a87b62ab1b7265755de48485fbe1bc73a
ColdPaleLight <31977171+ColdPaleLight@users.noreply.github.com>
2022-01-14 02:17:01 +0800

    Only creates 'onscreen_surface_' when it's not available in 'AndroidSurfaceGL::CreateSnapshotSurface' (flutter/engine#30590)
    * Only creates 'onscreen_surface_' when it's not available in 'AndroidSurfaceGL::CreatePbufferSurface'
    
    * Rename the function and add some unit tests
    
    * Update android_surface_gl.h
    
    Co-authored-by: Dan Field <dfield@gmail.com>

--- engine/src/flutter/shell/platform/android/android_context_gl_unittests.cc
+++ engine/src/flutter/shell/platform/android/android_context_gl_unittests.cc
@@ -4,12 +4,62 @@
 #include "flutter/shell/common/thread_host.h"
 #include "flutter/shell/platform/android/android_context_gl.h"
 #include "flutter/shell/platform/android/android_environment_gl.h"
+#include "flutter/shell/platform/android/android_surface_gl.h"
+#include "flutter/shell/platform/android/jni/platform_view_android_jni.h"
+#include "gmock/gmock.h"
 #include "gtest/gtest.h"
 
 namespace flutter {
 namespace testing {
 namespace android {
 namespace {
+class MockPlatformViewAndroidJNI : public PlatformViewAndroidJNI {
+ public:
+  MOCK_METHOD2(FlutterViewHandlePlatformMessage,
+               void(std::unique_ptr<flutter::PlatformMessage> message,
+                    int responseId));
+  MOCK_METHOD2(FlutterViewHandlePlatformMessageResponse,
+               void(int responseId, std::unique_ptr<fml::Mapping> data));
+  MOCK_METHOD3(FlutterViewUpdateSemantics,
+               void(std::vector<uint8_t> buffer,
+                    std::vector<std::string> strings,
+                    std::vector<std::vector<uint8_t>> string_attribute_args));
+  MOCK_METHOD2(FlutterViewUpdateCustomAccessibilityActions,
+               void(std::vector<uint8_t> actions_buffer,
+                    std::vector<std::string> strings));
+  MOCK_METHOD0(FlutterViewOnFirstFrame, void());
+  MOCK_METHOD0(FlutterViewOnPreEngineRestart, void());
+  MOCK_METHOD2(SurfaceTextureAttachToGLContext,
+               void(JavaLocalRef surface_texture, int textureId));
+  MOCK_METHOD1(SurfaceTextureUpdateTexImage,
+               void(JavaLocalRef surface_texture));
+  MOCK_METHOD2(SurfaceTextureGetTransformMatrix,
+               void(JavaLocalRef surface_texture, SkMatrix& transform));
+  MOCK_METHOD1(SurfaceTextureDetachFromGLContext,
+               void(JavaLocalRef surface_texture));
+  MOCK_METHOD8(FlutterViewOnDisplayPlatformView,
+               void(int view_id,
+                    int x,
+                    int y,
+                    int width,
+                    int height,
+                    int viewWidth,
+                    int viewHeight,
+                    MutatorsStack mutators_stack));
+  MOCK_METHOD5(FlutterViewDisplayOverlaySurface,
+               void(int surface_id, int x, int y, int width, int height));
+  MOCK_METHOD0(FlutterViewBeginFrame, void());
+  MOCK_METHOD0(FlutterViewEndFrame, void());
+  MOCK_METHOD0(FlutterViewCreateOverlaySurface,
+               std::unique_ptr<PlatformViewAndroidJNI::OverlayMetadata>());
+  MOCK_METHOD0(FlutterViewDestroyOverlaySurfaces, void());
+  MOCK_METHOD1(FlutterViewComputePlatformResolvedLocale,
+               std::unique_ptr<std::vector<std::string>>(
+                   std::vector<std::string> supported_locales_data));
+  MOCK_METHOD0(GetDisplayRefreshRate, double());
+  MOCK_METHOD1(RequestDartDeferredLibrary, bool(int loading_unit_id));
+};
+
 TaskRunners MakeTaskRunners(const std::string& thread_label,
                             const ThreadHost& thread_host) {
   fml::MessageLoop::EnsureInitializedForCurrentThread();
@@ -61,6 +111,52 @@ TEST(AndroidContextGl, CreateSingleThread) {
   EXPECT_NE(context.get(), nullptr);
   context.reset();
   EXPECT_TRUE(main_context->abandoned());
+}
+
+TEST(AndroidSurfaceGL, CreateSnapshopSurfaceWhenOnscreenSurfaceIsNotNull) {
+  GrMockOptions main_context_options;
+  sk_sp<GrDirectContext> main_context =
+      GrDirectContext::MakeMock(&main_context_options);
+  auto environment = fml::MakeRefCounted<AndroidEnvironmentGL>();
+  std::string thread_label =
+      ::testing::UnitTest::GetInstance()->current_test_info()->name();
+  ThreadHost thread_host(thread_label, ThreadHost::Type::UI |
+                                           ThreadHost::Type::RASTER |
+                                           ThreadHost::Type::IO);
+  TaskRunners task_runners = MakeTaskRunners(thread_label, thread_host);
+  auto android_context = std::make_shared<AndroidContextGL>(
+      AndroidRenderingAPI::kOpenGLES, environment, task_runners);
+  auto jni = std::make_shared<MockPlatformViewAndroidJNI>();
+  auto android_surface =
+      std::make_unique<AndroidSurfaceGL>(android_context, jni);
+  auto window = fml::MakeRefCounted<AndroidNativeWindow>(
+      nullptr, /*is_fake_window=*/true);
+  android_surface->SetNativeWindow(window);
+  auto onscreen_surface = android_surface->GetOnscreenSurface();
+  EXPECT_NE(onscreen_surface, nullptr);
+  android_surface->CreateSnapshotSurface();
+  EXPECT_EQ(onscreen_surface, android_surface->GetOnscreenSurface());
+}
+
+TEST(AndroidSurfaceGL, CreateSnapshopSurfaceWhenOnscreenSurfaceIsNull) {
+  GrMockOptions main_context_options;
+  sk_sp<GrDirectContext> main_context =
+      GrDirectContext::MakeMock(&main_context_options);
+  auto environment = fml::MakeRefCounted<AndroidEnvironmentGL>();
+  std::string thread_label =
+      ::testing::UnitTest::GetInstance()->current_test_info()->name();
+  ThreadHost thread_host(thread_label, ThreadHost::Type::UI |
+                                           ThreadHost::Type::RASTER |
+                                           ThreadHost::Type::IO);
+  TaskRunners task_runners = MakeTaskRunners(thread_label, thread_host);
+  auto android_context = std::make_shared<AndroidContextGL>(
+      AndroidRenderingAPI::kOpenGLES, environment, task_runners);
+  auto jni = std::make_shared<MockPlatformViewAndroidJNI>();
+  auto android_surface =
+      std::make_unique<AndroidSurfaceGL>(android_context, jni);
+  EXPECT_EQ(android_surface->GetOnscreenSurface(), nullptr);
+  android_surface->CreateSnapshotSurface();
+  EXPECT_NE(android_surface->GetOnscreenSurface(), nullptr);
 }
 }  // namespace android
 }  // namespace testing
