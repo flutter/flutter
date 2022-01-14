96ab49fe61ff6f708883f79e7070b4312b8fe805
creativecreatorormaybenot <creativecreatorormaybenot@gmail.com>
2022-01-14 01:24:15 +0000

    Update examples/api/lib/material/theme_data/theme_data.0.dart
    Co-authored-by: Greg Spencer <gspencergoog@users.noreply.github.com>

--- examples/api/lib/material/theme_data/theme_data.0.dart
+++ examples/api/lib/material/theme_data/theme_data.0.dart
@@ -2,24 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// Template: dev/snippets/config/templates/stateless_widget_scaffold.tmpl
-//
-// Comment lines marked with "▼▼▼" and "▲▲▲" are used for authoring
-// of samples, and may be ignored if you are just exploring the sample.
-
 // Flutter code sample for ThemeData
-//
-//***************************************************************************
-//* ▼▼▼▼▼▼▼▼ description ▼▼▼▼▼▼▼▼ (do not modify or remove section marker)
-
-// This sample shows how to display a [Container] widget that uses [Theme.of]
-// to retrieve the primary color from the theme's [colorScheme] to draw an
-// amber square.
-// The [Builder] widget separates the parent theme's [BuildContext] from the
-// child's [BuildContext].
-
-//* ▲▲▲▲▲▲▲▲ description ▲▲▲▲▲▲▲▲ (do not modify or remove section marker)
-//***************************************************************************
 
 import 'package:flutter/material.dart';
 
