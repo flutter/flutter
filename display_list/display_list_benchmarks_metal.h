323a794941b0fed625721f9b08a8c22c306486ed
George Wright <gw280@google.com>
2022-01-19 13:39:37 -0800

    Refactor DisplayList Benchmarks (#30913)
    Refactor DisplayList Benchmarks so that we allocate the CanvasProvider inside the test body. This allows for easier to understand memory management and object lifetimes, and fixes a leak on iOS/macOS.

---
+++ display_list/display_list_benchmarks_metal.h
@@ -0,0 +1,33 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef FLUTTER_FLOW_DISPLAY_LIST_BENCHMARKS_METAL_H_
+#define FLUTTER_FLOW_DISPLAY_LIST_BENCHMARKS_METAL_H_
+
+#include "flutter/display_list/display_list_benchmarks_canvas_provider.h"
+#include "flutter/testing/test_metal_surface.h"
+
+namespace flutter {
+namespace testing {
+
+class MetalCanvasProvider : public CanvasProvider {
+ public:
+  virtual ~MetalCanvasProvider() = default;
+
+  void InitializeSurface(const size_t width, const size_t height) override;
+  sk_sp<SkSurface> GetSurface() override;
+  sk_sp<SkSurface> MakeOffscreenSurface(const size_t width,
+                                        const size_t height) override;
+  const std::string BackendName() override { return "Metal"; }
+
+ private:
+  std::unique_ptr<TestMetalContext> metal_context_;
+  std::unique_ptr<TestMetalSurface> metal_surface_;
+  std::unique_ptr<TestMetalSurface> metal_offscreen_surface_;
+};
+
+}  // namespace testing
+}  // namespace flutter
+
+#endif  // FLUTTER_FLOW_DISPLAY_LIST_BENCHMARKS_METAL_H_
