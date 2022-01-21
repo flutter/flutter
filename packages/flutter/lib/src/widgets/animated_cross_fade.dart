b28d12bae7797da762bdd8c859e2a0680a02ebb8
Taha Tesser <tessertaha@gmail.com>
2022-01-21 10:56:52 +0200

    Remove trailing spaces

--- packages/flutter/lib/src/widgets/animated_cross_fade.dart
+++ packages/flutter/lib/src/widgets/animated_cross_fade.dart
@@ -211,8 +211,8 @@ class AnimatedCrossFade extends StatefulWidget {
   /// When true, this is equivalent to wrapping bottom widget with an [ExcludeFocus]
   /// widget while it is on the bottom of the crosss-fade stack.
   ///
-  /// Defaults to true, when it is false, the bottom widget in the crosss-fade stack can 
-  /// remain in focus until the top widget requests focus, this is useful for when 
+  /// Defaults to true, when it is false, the bottom widget in the crosss-fade stack can
+  /// remain in focus until the top widget requests focus, this is useful for when
   /// animating between different [TextField]s so the keyboard remain open during the
   /// cross-fade animation.
   final bool excludeBottomFocus;
