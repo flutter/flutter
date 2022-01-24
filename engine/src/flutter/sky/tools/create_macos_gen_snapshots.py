55f1e4764194caf10b506a91152c3a95293c6307
Zachary Anderson <zanderso@users.noreply.github.com>
2022-01-24 13:14:05 -0800

    Add scripts for cross-arch macOS framework (flutter/engine#31014)

--- engine/src/flutter/sky/tools/create_macos_gen_snapshots.py
+++ engine/src/flutter/sky/tools/create_macos_gen_snapshots.py
@@ -11,28 +11,45 @@ import os
 
 
 def main():
-  parser = argparse.ArgumentParser(description='Copies architecture-dependent gen_snapshot binaries to output dir')
+  parser = argparse.ArgumentParser(
+      description='Copies architecture-dependent gen_snapshot binaries to output dir'
+  )
 
   parser.add_argument('--dst', type=str, required=True)
+  parser.add_argument('--x64-out-dir', type=str)
   parser.add_argument('--arm64-out-dir', type=str)
   parser.add_argument('--armv7-out-dir', type=str)
 
   args = parser.parse_args()
 
+  if args.x64_out_dir:
+    generate_gen_snapshot(
+        args.x64_out_dir,
+        os.path.join(args.dst, 'gen_snapshot_x64')
+    )
+
   if args.arm64_out_dir:
-    generate_gen_snapshot(args.arm64_out_dir, os.path.join(args.dst, 'gen_snapshot_arm64'))
+    generate_gen_snapshot(
+        os.path.join(args.arm64_out_dir, 'clang_x64'),
+        os.path.join(args.dst, 'gen_snapshot_arm64')
+    )
 
   if args.armv7_out_dir:
-    generate_gen_snapshot(args.armv7_out_dir, os.path.join(args.dst, 'gen_snapshot_armv7'))
+    generate_gen_snapshot(
+        os.path.join(args.armv7_out_dir, 'clang_x64'),
+        os.path.join(args.dst, 'gen_snapshot_armv7')
+    )
 
 
 def generate_gen_snapshot(directory, destination):
-  gen_snapshot_dir = os.path.join(directory, 'clang_x64', 'gen_snapshot')
+  gen_snapshot_dir = os.path.join(directory, 'gen_snapshot')
   if not os.path.isfile(gen_snapshot_dir):
     print('Cannot find gen_snapshot at %s' % gen_snapshot_dir)
     sys.exit(1)
 
-  subprocess.check_call(['xcrun', 'bitcode_strip', '-r', gen_snapshot_dir, '-o', destination])
+  subprocess.check_call(
+      ['xcrun', 'bitcode_strip', '-r', gen_snapshot_dir, '-o', destination]
+  )
 
 
 if __name__ == '__main__':
