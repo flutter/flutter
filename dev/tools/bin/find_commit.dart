890099b9e59b95cbaec1849215d269d2b0eda7de
Godofredo Contreras <godofredoc@google.com>
2022-01-26 00:28:36 -0800

    More logs.

--- dev/tools/bin/find_commit.dart
+++ dev/tools/bin/find_commit.dart
@@ -8,13 +8,13 @@
 // contemporary commit is the one that was public at the time of the last commit
 // on the master branch before the current commit's branch was created.
 
+import 'dart:convert';
 import 'dart:io';
 
 const bool debugLogging = true;
 
 void log(String message) {
-  if (debugLogging)
-    print(message);
+  if (debugLogging) print(message);
 }
 
 class Commit {
@@ -28,7 +28,7 @@ class Commit {
   static Commit parse(String line) {
     log('fff[$line]');
     final int space = line.indexOf(' ');
-    return Commit(line.substring(0, space), DateTime.parse(line.substring(space+1, line.length).trimRight()));
+    return Commit(line.substring(0, space), DateTime.parse(line.substring(space + 1, line.length).trimRight()));
   }
 
   static List<Commit> parseList(String lines) {
@@ -46,10 +46,13 @@ String findCommit({
   final Commit anchor;
   if (primaryBranch == primaryTrunk) {
     log('on $primaryTrunk, using last commit as anchor');
-    anchor = Commit.parse(git(primaryRepoDirectory, <String>['log', Commit.formatArgument, '--max-count=1', primaryBranch, '--']));
+    anchor = Commit.parse(
+        git(primaryRepoDirectory, <String>['log', Commit.formatArgument, '--max-count=1', primaryBranch, '--']));
   } else {
-    final List<Commit> branchCommits = Commit.parseList(git(primaryRepoDirectory, <String>['log', Commit.formatArgument, primaryBranch, '--']));
-    final List<Commit> trunkCommits = Commit.parseList(git(primaryRepoDirectory, <String>['log', Commit.formatArgument, primaryTrunk, '--']));
+    final List<Commit> branchCommits =
+        Commit.parseList(git(primaryRepoDirectory, <String>['log', Commit.formatArgument, primaryBranch, '--']));
+    final List<Commit> trunkCommits =
+        Commit.parseList(git(primaryRepoDirectory, <String>['log', Commit.formatArgument, primaryTrunk, '--']));
     if (branchCommits.isEmpty || trunkCommits.isEmpty || branchCommits.first.hash != trunkCommits.first.hash)
       throw StateError('Branch $primaryBranch does not seem to have a common history with trunk $primaryTrunk.');
     if (branchCommits.last.hash == trunkCommits.last.hash) {
@@ -57,8 +60,9 @@ String findCommit({
       anchor = trunkCommits.last;
     } else {
       int index = 0;
-      while (branchCommits.length > index && trunkCommits.length > index && branchCommits[index].hash == trunkCommits[index].hash)
-        index += 1;
+      while (branchCommits.length > index &&
+          trunkCommits.length > index &&
+          branchCommits[index].hash == trunkCommits[index].hash) index += 1;
       log('$primaryBranch branched from $primaryTrunk ${branchCommits.length - index} commits ago, trunk has advanced by ${trunkCommits.length - index} commits since then.');
       anchor = trunkCommits[index - 1];
     }
@@ -74,22 +78,25 @@ String findCommit({
 }
 
 String git(String workingDirectory, List<String> arguments) {
-  final ProcessResult result = Process.runSync('git', arguments, workingDirectory: workingDirectory);
+  final ProcessResult result = Process.runSync(
+    'git',
+    arguments,
+    workingDirectory: workingDirectory,
+    stdoutEncoding: utf8,
+  );
   if (result.exitCode != 0 || '${result.stderr}'.isNotEmpty)
     throw ProcessException('git', arguments, '${result.stdout}${result.stderr}', result.exitCode);
-  return result.stdout;
+  return '${result.stdout}';
 }
 
 void main(List<String> arguments) {
   if (arguments.isEmpty || arguments.length > 2 || arguments.contains('--help') || arguments.contains('-h')) {
-    print(
-      'Usage: dart find_commit.dart [<path-to-primary-repo>] <path-to-secondary-repo>\n'
-      'This script will find the commit in the secondary repo that was contemporary\n'
-      'when the commit in the primary repo was created. If that commit is on a\n'
-      "branch, then the date of the branch's creation is used instead.\n"
-      'If <path-to-primary-repo> is omitted, the current directory is used for the\n'
-      'primary repo.'
-    );
+    print('Usage: dart find_commit.dart [<path-to-primary-repo>] <path-to-secondary-repo>\n'
+        'This script will find the commit in the secondary repo that was contemporary\n'
+        'when the commit in the primary repo was created. If that commit is on a\n'
+        "branch, then the date of the branch's creation is used instead.\n"
+        'If <path-to-primary-repo> is omitted, the current directory is used for the\n'
+        'primary repo.');
   } else {
     final String primaryRepo = arguments.length == 1 ? '.' : arguments.first;
     final String secondaryRepo = arguments.last;
