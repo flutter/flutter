23d0ff68c804111ed9660e096776b5d69432b699
Godofredo Contreras <godofredoc@google.com>
2022-01-25 23:37:59 -0800

    More logs.

--- dev/tools/bin/find_commit.dart
+++ dev/tools/bin/find_commit.dart
@@ -23,7 +23,7 @@ class Commit {
   final String hash;
   final DateTime timestamp;
 
-  static String formatArgument = '--format="%H %cI"';
+  static String formatArgument = '--pretty="format:%H %cI"';
 
   static Commit parse(String line) {
     log('fff[$line]');
