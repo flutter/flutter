36e55dcf3f8c5098a9f52dc7987707dd61c41ae5
Christopher Fujino <christopherfujino@gmail.com>
2022-01-28 16:40:12 -0800

    [flutter_tools] remove dummy implementation from abstract class .isEnabled() (#96888)

--- packages/flutter_tools/test/general.shard/features_test.dart
+++ packages/flutter_tools/test/general.shard/features_test.dart
@@ -107,8 +107,7 @@ void main() {
     testWithoutContext('Flutter Windows desktop help string', () {
       expect(flutterWindowsDesktopFeature.generateHelpMessage(),
       'Enable or disable beta-quality support for desktop on Windows. '
-      'This setting will take effect on the master, dev, beta, and stable channels. '
-      'Newer beta versions are available on the beta channel.');
+      'This setting will take effect on the master, dev, beta, and stable channels.');
     });
 
     testWithoutContext('help string on multiple channels', () {
