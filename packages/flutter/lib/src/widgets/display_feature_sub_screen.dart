82c5b1a0f6d89892aca892e3d49f2c7d570da4ce
Andrei Diaconu <adiaconu@microsoft.com>
2022-02-02 13:15:39 +0200

    Remove unneded semicolon

--- packages/flutter/lib/src/widgets/display_feature_sub_screen.dart
+++ packages/flutter/lib/src/widgets/display_feature_sub_screen.dart
@@ -235,7 +235,7 @@ class DisplayFeatureSubScreen extends StatelessWidget {
         } else {
           newSubScreens.add(screen);
         }
-      };
+      }
       subScreens = newSubScreens;
     }
     return subScreens;
