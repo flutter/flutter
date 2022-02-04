78c339655f5d4deaf5813485f1f969c194e15f77
Chingjun Lau <chingjun@google.com>
2022-02-04 10:31:21 -0800

    Update for more proper test

--- packages/flutter_tools/test/general.shard/device_test.dart
+++ packages/flutter_tools/test/general.shard/device_test.dart
@@ -9,7 +9,9 @@ import 'package:flutter_tools/src/base/io.dart';
 import 'package:flutter_tools/src/base/logger.dart';
 import 'package:flutter_tools/src/base/terminal.dart';
 import 'package:flutter_tools/src/base/user_messages.dart';
+import 'package:flutter_tools/src/base/utils.dart';
 import 'package:flutter_tools/src/build_info.dart';
+import 'package:flutter_tools/src/convert.dart';
 import 'package:flutter_tools/src/device.dart';
 import 'package:flutter_tools/src/project.dart';
 import 'package:test/fake.dart';
@@ -520,7 +522,9 @@ void main() {
         dartFlags: 'c',
         deviceVmServicePort: 1234,
       );
-      final DebuggingOptions deserialized = DebuggingOptions.fromJson(original.toJson(), BuildInfo.debug);
+      final String jsonString = json.encode(original.toJson());
+      final Map<String, dynamic> decoded = castStringKeyedMap(json.decode(jsonString))!;
+      final DebuggingOptions deserialized = DebuggingOptions.fromJson(decoded, BuildInfo.debug);
       expect(deserialized.startPaused, original.startPaused);
       expect(deserialized.disableServiceAuthCodes, original.disableServiceAuthCodes);
       expect(deserialized.enableDds, original.enableDds);
