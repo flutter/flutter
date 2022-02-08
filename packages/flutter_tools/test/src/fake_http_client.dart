cd5f7e2e60b249117465078ac95bdbc84c0b6f74
Brian Quinlan <bquinlan@google.com>
2022-02-07 18:24:28 -0800

    Updated formatting.

--- packages/flutter_tools/test/src/fake_http_client.dart
+++ packages/flutter_tools/test/src/fake_http_client.dart
@@ -60,8 +60,7 @@ String _toMethodString(HttpMethod method) {
 ///
 /// This should only be used when the http client cannot be set directly, such as
 /// when testing `package:http` code.
-Future<void> overrideHttpClients(
-    Future<void> Function() callback, FakeHttpClient httpClient) async {
+Future<void> overrideHttpClients(Future<void> Function() callback,  FakeHttpClient httpClient) async {
   final HttpOverrides overrides = _FakeHttpClientOverrides(httpClient);
   await HttpOverrides.runWithHttpOverrides(callback, overrides);
 }
@@ -84,8 +83,7 @@ class _FakeHttpClientOverrides extends HttpOverrides {
 /// empty response. If [responseError] is non-null, will throw this instead
 /// of returning the response when closing the request.
 class FakeRequest {
-  const FakeRequest(
-    this.uri, {
+  const FakeRequest(this.uri, {
     this.method = HttpMethod.get,
     this.response = FakeResponse.empty,
     this.responseError,
@@ -129,12 +127,10 @@ class FakeHttpClient implements HttpClient {
   /// This does not enforce any order on the requests, but if multiple
   /// requests match then the first will be selected;
   FakeHttpClient.list(List<FakeRequest> requests)
-      : _requests = requests.toList();
+    : _requests = requests.toList();
 
   /// Creates an HTTP client that always returns an empty 200 request.
-  FakeHttpClient.any()
-      : _any = true,
-        _requests = <FakeRequest>[];
+  FakeHttpClient.any() : _any = true, _requests = <FakeRequest>[];
 
   bool _any = false;
   final List<FakeRequest> _requests;
@@ -155,39 +151,30 @@ class FakeHttpClient implements HttpClient {
   String? userAgent;
 
   @override
-  void addCredentials(
-      Uri url, String realm, HttpClientCredentials credentials) {
+  void addCredentials(Uri url, String realm, HttpClientCredentials credentials) {
     throw UnimplementedError();
   }
 
   @override
-  void addProxyCredentials(
-      String host, int port, String realm, HttpClientCredentials credentials) {
+  void addProxyCredentials(String host, int port, String realm, HttpClientCredentials credentials) {
     throw UnimplementedError();
   }
 
   @override
   // ignore: override_on_non_overriding_member
-  Future<ConnectionTask> Function(Uri url, String proxyHost, int proxyPort)?
-      connectionFactory;
+  Future<ConnectionTask> Function(Uri url, String proxyHost, int proxyPort)? connectionFactory;
 
   @override
   Future<bool> Function(Uri url, String scheme, String realm)? authenticate;
 
   @override
-  Future<bool> Function(String host, int port, String scheme, String realm)?
-      authenticateProxy;
+  Future<bool> Function(String host, int port, String scheme, String realm)? authenticateProxy;
 
   @override
-  bool Function(X509Certificate cert, String host, int port)?
-      badCertificateCallback;
+  bool Function(X509Certificate cert, String host, int port)? badCertificateCallback;
 
   @override
-  // ignore: override_on_non_overriding_member
-  Function(String line)? keyLog;
-
-  @override
-  void close({bool force = false}) {}
+  void close({bool force = false}) { }
 
   @override
   Future<HttpClientRequest> delete(String host, int port, String path) {
@@ -226,8 +213,7 @@ class FakeHttpClient implements HttpClient {
   }
 
   @override
-  Future<HttpClientRequest> open(
-      String method, String host, int port, String path) {
+  Future<HttpClientRequest> open(String method, String host, int port, String path) {
     final Uri uri = Uri(host: host, port: port, path: path);
     return openUrl(method, uri);
   }
@@ -272,8 +258,7 @@ class FakeHttpClient implements HttpClient {
 
   int _requestCount = 0;
 
-  _FakeHttpClientRequest _findRequest(
-      HttpMethod method, Uri uri, StackTrace stackTrace) {
+  _FakeHttpClientRequest _findRequest(HttpMethod method, Uri uri, StackTrace stackTrace) {
     // Ensure the fake client throws similar errors to the real client.
     if (uri.host.isEmpty) {
       throw ArgumentError('No host specified in URI $uri');
@@ -293,16 +278,16 @@ class FakeHttpClient implements HttpClient {
     }
     FakeRequest? matchedRequest;
     for (final FakeRequest request in _requests) {
-      if (request.method == method &&
-          request.uri.toString() == uri.toString()) {
+      if (request.method == method && request.uri.toString() == uri.toString()) {
         matchedRequest = request;
         break;
       }
     }
     if (matchedRequest == null) {
       throw StateError(
-          'Unexpected request for $method to $uri after $_requestCount requests.\n'
-          'Pending requests: ${_requests.join(',')}');
+        'Unexpected request for $method to $uri after $_requestCount requests.\n'
+        'Pending requests: ${_requests.join(',')}'
+      );
     }
     _requestCount += 1;
     _requests.remove(matchedRequest);
@@ -318,8 +303,7 @@ class FakeHttpClient implements HttpClient {
 }
 
 class _FakeHttpClientRequest implements HttpClientRequest {
-  _FakeHttpClientRequest(this._response, this._uri, this._method,
-      this._responseError, this._expectedBody, this._stackTrace);
+  _FakeHttpClientRequest(this._response, this._uri, this._method, this._responseError, this._expectedBody, this._stackTrace);
 
   final FakeResponse _response;
   final String _method;
@@ -358,7 +342,7 @@ class _FakeHttpClientRequest implements HttpClientRequest {
   }
 
   @override
-  void addError(Object error, [StackTrace? stackTrace]) {}
+  void addError(Object error, [StackTrace? stackTrace]) { }
 
   @override
   Future<void> addStream(Stream<List<int>> stream) async {
@@ -368,15 +352,17 @@ class _FakeHttpClientRequest implements HttpClientRequest {
   }
 
   @override
+  // ignore: override_on_non_overriding_member
+  Function(String line)? keyLog;
+
+  @override
   Future<HttpClientResponse> close() async {
     final Completer<void> completer = Completer<void>();
     Timer.run(() {
-      if (_expectedBody != null &&
-          !const ListEquality<int>().equals(_expectedBody, _body)) {
-        completer.completeError(
-            StateError(
-                'Expected a request with the following body:\n$_expectedBody\n but found:\n$_body'),
-            _stackTrace);
+      if (_expectedBody != null && !const ListEquality<int>().equals(_expectedBody, _body)) {
+        completer.completeError(StateError(
+          'Expected a request with the following body:\n$_expectedBody\n but found:\n$_body'
+        ), _stackTrace);
       } else {
         completer.complete();
       }
@@ -398,7 +384,7 @@ class _FakeHttpClientRequest implements HttpClientRequest {
   Future<HttpClientResponse> get done => throw UnimplementedError();
 
   @override
-  Future<void> flush() async {}
+  Future<void> flush() async { }
 
   @override
   final HttpHeaders headers = _FakeHttpHeaders(<String, List<String>>{});
@@ -430,11 +416,9 @@ class _FakeHttpClientRequest implements HttpClientRequest {
   }
 }
 
-class _FakeHttpClientResponse extends Stream<List<int>>
-    implements HttpClientResponse {
+class _FakeHttpClientResponse extends Stream<List<int>> implements HttpClientResponse {
   _FakeHttpClientResponse(this._response)
-      : headers =
-            _FakeHttpHeaders(Map<String, List<String>>.from(_response.headers));
+      : headers = _FakeHttpHeaders(Map<String, List<String>>.from(_response.headers));
 
   final FakeResponse _response;
 
@@ -442,8 +426,7 @@ class _FakeHttpClientResponse extends Stream<List<int>>
   X509Certificate get certificate => throw UnimplementedError();
 
   @override
-  HttpClientResponseCompressionState get compressionState =>
-      throw UnimplementedError();
+  HttpClientResponseCompressionState get compressionState => throw UnimplementedError();
 
   @override
   HttpConnectionInfo get connectionInfo => throw UnimplementedError();
@@ -472,12 +455,10 @@ class _FakeHttpClientResponse extends Stream<List<int>>
     void Function()? onDone,
     bool? cancelOnError,
   }) {
-    final Stream<List<int>> response =
-        Stream<List<int>>.fromIterable(<List<int>>[
+    final Stream<List<int>> response = Stream<List<int>>.fromIterable(<List<int>>[
       _response.body,
     ]);
-    return response.listen(onData,
-        onError: onError, onDone: onDone, cancelOnError: cancelOnError);
+    return response.listen(onData, onError: onError, onDone: onDone, cancelOnError: cancelOnError);
   }
 
   @override
@@ -487,8 +468,7 @@ class _FakeHttpClientResponse extends Stream<List<int>>
   String get reasonPhrase => 'OK';
 
   @override
-  Future<HttpClientResponse> redirect(
-      [String? method, Uri? url, bool? followLoops]) {
+  Future<HttpClientResponse> redirect([String? method, Uri? url, bool? followLoops]) {
     throw UnimplementedError();
   }
 
@@ -519,10 +499,10 @@ class _FakeHttpHeaders extends HttpHeaders {
   }
 
   @override
-  void forEach(void Function(String name, List<String> values) action) {}
+  void forEach(void Function(String name, List<String> values) action) { }
 
   @override
-  void noFolding(String name) {}
+  void noFolding(String name) {  }
 
   @override
   void remove(String name, Object value) {
