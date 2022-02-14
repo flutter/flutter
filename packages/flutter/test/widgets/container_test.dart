6ef21cc4ca91e11429bed411f48bcaf2e04f16ff
Reza Mirzaee <rmirzaee98@gmail.com>
2022-02-14 11:23:31 +0330

    add test

--- packages/flutter/test/widgets/container_test.dart
+++ packages/flutter/test/widgets/container_test.dart
@@ -633,7 +633,11 @@ void main() {
     expect(tapped, false);
   });
 
+<<<<<<< HEAD
   testWidgets('Container discards alignment when the child parameter is null and constraints is not Tight', (WidgetTester tester) async {
+=======
+  testWidgets('Container discards alignment when child, width and height parameters are null', (WidgetTester tester) async {
+>>>>>>> a93da1f156 (add test)
     await tester.pumpWidget(
       Container(
         decoration: const BoxDecoration(
