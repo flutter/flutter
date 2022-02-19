54a9d652828941dbebbe8de32cf667e89578b13d
Zachary Anderson <zanderso@users.noreply.github.com>
2022-02-19 10:09:26 -0800

    Revert "Reland "Enable caching of CPU samples collected at application startup (#89600)"" (#98803)

--- packages/flutter_tools/test/general.shard/fuchsia/fuchsia_device_test.dart
+++ packages/flutter_tools/test/general.shard/fuchsia/fuchsia_device_test.dart
@@ -944,7 +944,6 @@ class FakeDartDevelopmentService extends Fake implements DartDevelopmentService 
     int hostPort,
     bool ipv6,
     bool disableServiceAuthCodes,
-    bool cacheStartupProfile = false,
   }) async {}
 
   @override
