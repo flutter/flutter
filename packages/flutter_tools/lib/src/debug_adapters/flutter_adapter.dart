80ed693d33c07bd4a1c5d4995a52482a865776e9
Danny Tuppeny <danny@tuppeny.com>
2022-02-22 16:18:17 +0000

    [dap] Remove some code that's not longer required

--- packages/flutter_tools/lib/src/debug_adapters/flutter_adapter.dart
+++ packages/flutter_tools/lib/src/debug_adapters/flutter_adapter.dart
@@ -5,7 +5,7 @@
 import 'dart:async';
 import 'dart:math' as math;
 
-import 'package:dds/dap.dart' hide PidTracker, PackageConfigUtils;
+import 'package:dds/dap.dart' hide PidTracker;
 import 'package:meta/meta.dart';
 import 'package:vm_service/vm_service.dart' as vm;
 
@@ -19,7 +19,7 @@ import 'mixins.dart';
 
 /// A DAP Debug Adapter for running and debugging Flutter applications.
 class FlutterDebugAdapter extends DartDebugAdapter<FlutterLaunchRequestArguments, FlutterAttachRequestArguments>
-    with PidTracker, PackageConfigUtils {
+    with PidTracker {
   FlutterDebugAdapter(
     ByteStreamServerChannel channel, {
     required this.fileSystem,
