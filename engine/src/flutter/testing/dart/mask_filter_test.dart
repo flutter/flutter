80605e564d19f024a5d85a5254312d2dc3fa2d7a
Jim Graham <flar@google.com>
2022-02-25 17:00:11 -0800

    detect cases when Skia nops filters by returning nullptr to prevent crashes (flutter/engine#31689)

---
+++ engine/src/flutter/testing/dart/mask_filter_test.dart
@@ -0,0 +1,28 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'dart:ui';
+
+import 'package:litetest/litetest.dart';
+
+void main() {
+  test('MaskFilter - NOP blur does not crash', () async {
+    final PictureRecorder recorder = PictureRecorder();
+    final Canvas canvas = Canvas(recorder);
+    final Paint paint = Paint()
+      ..color = const Color(0xff00AA00)
+      ..maskFilter = const MaskFilter.blur(BlurStyle.normal, 0);
+    canvas.saveLayer(const Rect.fromLTRB(-100, -100, 200, 200), paint);
+    canvas.drawRect(const Rect.fromLTRB(0, 0, 100, 100), Paint());
+    canvas.restore();
+    final Picture picture = recorder.endRecording();
+
+    final SceneBuilder builder = SceneBuilder();
+    builder.addPicture(Offset.zero, picture);
+
+    final Scene scene = builder.build();
+    expect(scene != null, true);
+    await scene.toImage(100, 100);
+  });
+}
