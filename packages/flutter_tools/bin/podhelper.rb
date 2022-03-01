8b3a3863f9ea4fd9e27fe9e796901ba317cfd52a
Jenn Magder <magder@google.com>
2022-03-01 11:22:04 -0800

    Handle hidden dot files in iOS framework bundles

--- packages/flutter_tools/bin/podhelper.rb
+++ packages/flutter_tools/bin/podhelper.rb
@@ -60,6 +60,7 @@ def flutter_additional_ios_build_settings(target)
     # Profile can't be derived from the CocoaPods build configuration. Use release framework (for linking only).
     configuration_engine_dir = build_configuration.type == :debug ? debug_framework_dir : release_framework_dir
     Dir.new(configuration_engine_dir).each_child do |xcframework_file|
+      continue if xcframework_file.start_with?(".") # Hidden file, possibly on external disk.
       if xcframework_file.end_with?("-simulator") # ios-arm64_x86_64-simulator
         build_configuration.build_settings['FRAMEWORK_SEARCH_PATHS[sdk=iphonesimulator*]'] = "\"#{configuration_engine_dir}/#{xcframework_file}\" $(inherited)"
       elsif xcframework_file.start_with?("ios-") # ios-armv7_arm64
