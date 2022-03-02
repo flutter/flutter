48018ebf7640c9da43a336c25896821e8d9b5222
godofredoc <godofredoc@google.com>
2022-03-02 08:26:11 -0800

    Fix web_engine_archive target. (flutter/engine#31751)

--- engine/src/flutter/build/zip.py
+++ engine/src/flutter/build/zip.py
@@ -5,6 +5,7 @@
 # found in the LICENSE file.
 
 import argparse
+import json
 import zipfile
 import os
 import sys
@@ -20,11 +21,20 @@ def _zip_dir(path, zip_file, prefix):
 
 def main(args):
   zip_file = zipfile.ZipFile(args.output, 'w', zipfile.ZIP_DEFLATED)
-  for path, archive_name in args.input_pairs:
-    if os.path.isdir(path):
-      _zip_dir(path, zip_file, archive_name)
-    else:
-      zip_file.write(path, archive_name)
+  if args.source_file:
+    with open(args.source_file) as source_file:
+      file_dict_list = json.load(source_file)
+      for file_dict in file_dict_list:
+        if os.path.isdir(file_dict['source']):
+          _zip_dir(file_dict['source'], zip_file, file_dict['destination'])
+        else:
+          zip_file.write(file_dict['source'], file_dict['destination'])
+  else:
+    for path, archive_name in args.input_pairs:
+      if os.path.isdir(path):
+        _zip_dir(path, zip_file, archive_name)
+      else:
+        zip_file.write(path, archive_name)
   zip_file.close()
 
 
@@ -35,4 +45,6 @@ if __name__ == '__main__':
       help='The name of the output zip file.')
   parser.add_argument('-i', dest='input_pairs', nargs=2, action='append',
       help='The input file and its destination location in the zip archive.')
+  parser.add_argument('-f', dest='source_file', action='store',
+      help='The path to the file list to zip.')
   sys.exit(main(parser.parse_args()))
