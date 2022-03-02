c353ae5c6bab5814a12001e79ce19517ec1c6f72
Brandon DeRosier <bdero@google.com>
2022-03-02 12:39:34 -0800

    Point: Add reflect and make scalar ops more flexible (flutter/engine#51)

--- engine/src/flutter/impeller/geometry/geometry_unittests.cc
+++ engine/src/flutter/impeller/geometry/geometry_unittests.cc
@@ -3,6 +3,7 @@
 // found in the LICENSE file.
 
 #include "impeller/geometry/geometry_unittests.h"
+#include <limits>
 #include "flutter/testing/testing.h"
 #include "impeller/geometry/path.h"
 #include "impeller/geometry/path_builder.h"
@@ -279,6 +280,38 @@ TEST(GeometryTest, CanPerformAlgebraicPointOps) {
   }
 }
 
+TEST(GeometryTest, CanPerformAlgebraicPointOpsWithArithmeticTypes) {
+  // LHS
+  {
+    IPoint p1(1, 2);
+    IPoint p2 = p1 * 2.0f;
+    ASSERT_EQ(p2.x, 2u);
+    ASSERT_EQ(p2.y, 4u);
+  }
+
+  {
+    IPoint p1(2, 6);
+    IPoint p2 = p1 / 2.0f;
+    ASSERT_EQ(p2.x, 1u);
+    ASSERT_EQ(p2.y, 3u);
+  }
+
+  // RHS
+  {
+    IPoint p1(1, 2);
+    IPoint p2 = 2.0f * p1;
+    ASSERT_EQ(p2.x, 2u);
+    ASSERT_EQ(p2.y, 4u);
+  }
+
+  {
+    IPoint p1(2, 6);
+    IPoint p2 = 12.0f / p1;
+    ASSERT_EQ(p2.x, 6u);
+    ASSERT_EQ(p2.y, 2u);
+  }
+}
+
 TEST(GeometryTest, PointIntegerCoercesToFloat) {
   // Integer on LHS, float on RHS
   {
@@ -496,6 +529,31 @@ TEST(GeometryTest, PointCrossProduct) {
     Point p(1, 2);
     Scalar s = p.Cross(Point(3, -4));
     ASSERT_FLOAT_EQ(s, -10);
+  }
+}
+
+TEST(GeometryTest, PointReflect) {
+  {
+    Point axis = Point(0, 1);
+    Point a(2, 3);
+    auto reflected = a.Reflect(axis);
+    auto expected = Point(2, -3);
+    ASSERT_POINT_NEAR(reflected, expected);
+  }
+
+  {
+    Point axis = Point(1, 1).Normalize();
+    Point a(1, 0);
+    auto reflected = a.Reflect(axis);
+    auto expected = Point(0, -1);
+    ASSERT_POINT_NEAR(reflected, expected);
+  }
+
+  {
+    Point axis = Point(1, 1).Normalize();
+    Point a(-1, -1);
+    auto reflected = a.Reflect(axis);
+    ASSERT_POINT_NEAR(reflected, -a);
   }
 }
 
