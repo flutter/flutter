34143e5c00f9623143e1b8d716de0248c64ff347
Jelly <jelly.legend@gmail.com>
2022-03-05 08:29:33 +0100

    Add additional tests

--- packages/flutter/test/foundation/isolates_test.dart
+++ packages/flutter/test/foundation/isolates_test.dart
@@ -3,6 +3,7 @@
 // found in the LICENSE file.
 
 import 'dart:io';
+import 'dart:isolate';
 import 'package:file/file.dart';
 import 'package:file/local.dart';
 import 'package:flutter/foundation.dart';
@@ -17,6 +18,22 @@ int test2(int value) {
   throw 2;
 }
 
+int test3(int value) {
+  Isolate.exit();
+}
+
+int test4(int value) {
+  Isolate.current.kill();
+
+  return value + 1;
+}
+
+int test5(int value) {
+  Isolate.current.kill(priority: Isolate.immediate);
+
+  return value + 1;
+}
+
 Future<int> test1Async(int value) async {
   return value + 1;
 }
@@ -25,13 +42,61 @@ Future<int> test2Async(int value) async {
   throw 2;
 }
 
+Future<int> test3Async(int value) async {
+  Isolate.exit();
+}
+
+Future<int> test4Async(int value) async {
+  Isolate.current.kill();
+
+  return value + 1;
+}
+
+Future<int> test5Async(int value) async {
+  Isolate.current.kill(priority: Isolate.immediate);
+
+  return value + 1;
+}
+
+Future<int> test1CallCompute(int value) {
+  return compute(test1, value);
+}
+
+Future<int> test2CallCompute(int value) {
+  return compute(test2, value);
+}
+
+Future<int> test3CallCompute(int value) {
+  return compute(test3, value);
+}
+
+Future<int> test4CallCompute(int value) {
+  return compute(test4, value);
+}
+
+Future<int> test5CallCompute(int value) {
+  return compute(test5, value);
+}
+
 void main() {
   test('compute()', () async {
     expect(await compute(test1, 0), 1);
     expect(compute(test2, 0), throwsException);
+    expect(compute(test3, 0), throwsException);
+    expect(await compute(test4, 0), 1);
+    expect(compute(test5, 0), throwsException);
 
     expect(await compute(test1Async, 0), 1);
     expect(compute(test2Async, 0), throwsException);
+    expect(compute(test3Async, 0), throwsException);
+    expect(await compute(test4Async, 0), 1);
+    expect(compute(test5Async, 0), throwsException);
+
+    expect(await compute(test1CallCompute, 0), 1);
+    expect(compute(test2CallCompute, 0), throwsException);
+    expect(compute(test3CallCompute, 0), throwsException);
+    expect(await compute(test4CallCompute, 0), 1);
+    expect(compute(test5CallCompute, 0), throwsException);
   }, skip: kIsWeb); // [intended] isn't supported on the web.
 
   test('compute closes all ports', () async {
