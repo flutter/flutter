144fc5563bc6922d4e955e7f4cdc047de849deea
Aman Verma <verma1090aman@gmail.com>
2022-03-07 02:08:05 +0530

    fix style

--- packages/flutter/lib/src/material/bottom_navigation_bar.dart
+++ packages/flutter/lib/src/material/bottom_navigation_bar.dart
@@ -127,79 +127,78 @@ enum BottomNavigationBarLandscapeLayout {
 /// respectively.
 /// ```dart
 /// int _selectedIndex = 1;
-///   FontWeight _fontWeight = FontWeight.normal;
-///   final ScrollController _homeController = ScrollController();
+/// FontWeight _fontWeight = FontWeight.normal;
+/// final ScrollController _homeController = ScrollController();
+/// / Widget _listViewBody() {
+///   return ListView.separated(
+///       controller: _homeController,
+///       itemBuilder: (BuildContext context, int index) {
+///         return Center(
+///           child: Text(
+///             'Item $index',
+///             style: TextStyle(
+///               fontWeight: _fontWeight,
+///             ),
+///           ),
+///         );
+///       },
+///       separatorBuilder: (BuildContext context, int index) => const Divider(
+///             thickness: 1,
+///           ),
+///       itemCount: 50);
+/// }
 ///
-///   Widget _listViewBody() {
-///     return ListView.separated(
-///         controller: _homeController,
-///         itemBuilder: (BuildContext context, int index) {
-///           return Center(
-///             child: Text(
-///               'Item $index',
-///               style: TextStyle(
-///                 fontWeight: _fontWeight,
-///               ),
-///             ),
-///           );
-///         },
-///         separatorBuilder: (BuildContext context, int index) => const Divider(
-///               thickness: 1,
-///             ),
-///         itemCount: 50);
-///   }
-///
-///   @override
-///   Widget build(BuildContext context) {
-///     return Scaffold(
-///       appBar: AppBar(
-///         title: const Text('BottomNavigationBar Sample'),
-///       ),
-///       body: _listViewBody(),
-///       bottomNavigationBar: BottomNavigationBar(
-///           items: const <BottomNavigationBarItem>[
-///             BottomNavigationBarItem(
-///               icon: Icon(Icons.arrow_circle_up),
-///               label: 'Back top',
-///             ),
-///             BottomNavigationBarItem(
-///               icon: Icon(Icons.format_bold),
-///               label: 'Bold Fonts',
-///             ),
-///             BottomNavigationBarItem(
-///               icon: Icon(Icons.format_clear),
-///               label: 'Normalize Fonts',
-///             ),
-///           ],
-///           currentIndex: _selectedIndex,
-///           selectedItemColor: Colors.amber[800],
-///           onTap: (int index) {
-///             switch (index) {
-///               case 0:
-///                 _homeController.animateTo(
-///                   0.0,
-///                   duration: const Duration(milliseconds: 500),
-///                   curve: Curves.easeOut,
-///                 );
-///                 break;
-///               case 1:
-///                 setState(() {
-///                   _fontWeight = FontWeight.bold;
-///                 });
-///                 break;
-///               case 2:
-///                 setState(() {
-///                   _fontWeight = FontWeight.normal;
-///                 });
-///                 break;
-///             }
-///             setState(() {
-///               _selectedIndex = index;
-///             });
-///           }),
-///     );
-///   }
-///```
+/// @override
+/// Widget build(BuildContext context) {
+///   return Scaffold(
+///     appBar: AppBar(
+///       title: const Text('BottomNavigationBar Sample'),
+///     ),
+///     body: _listViewBody(),
+///     bottomNavigationBar: BottomNavigationBar(
+///         items: const <BottomNavigationBarItem>[
+///           BottomNavigationBarItem(
+///             icon: Icon(Icons.arrow_circle_up),
+///             label: 'Back top',
+///           ),
+///           BottomNavigationBarItem(
+///             icon: Icon(Icons.format_bold),
+///             label: 'Bold Fonts',
+///           ),
+///           BottomNavigationBarItem(
+///             icon: Icon(Icons.format_clear),
+///             label: 'Normalize Fonts',
+///           ),
+///         ],
+///         currentIndex: _selectedIndex,
+///         selectedItemColor: Colors.amber[800],
+///         onTap: (int index) {
+///           switch (index) {
+///             case 0:
+///               _homeController.animateTo(
+///                 0.0,
+///                 duration: const Duration(milliseconds: 500),
+///                 curve: Curves.easeOut,
+///               );
+///               break;
+///             case 1:
+///               setState(() {
+///                 _fontWeight = FontWeight.bold;
+///               });
+///               break;
+///             case 2:
+///               setState(() {
+///                 _fontWeight = FontWeight.normal;
+///               });
+///               break;
+///           }
+///           setState(() {
+///             _selectedIndex = index;
+///           });
+///         }),
+///   );
+/// }
+/// ```
 /// {@end-tool}
 /// See also:
 ///
