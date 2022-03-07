939339d413b81d50005fb64a838044a149ba30e4
Anthony <591699+clocksmith@users.noreply.github.com>
2022-03-07 11:41:58 -0500

    fix typos in tests

--- packages/flutter/test/material/ink_sparkle_test.dart
+++ packages/flutter/test/material/ink_sparkle_test.dart
@@ -5,8 +5,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import '../rendering/mock_canvas.dart';
-
 // This file is run as part of a reduced test set in CI on Mac and Windows
 // machines.
 @Tags(<String>['reduced-test-set'])
@@ -14,9 +12,13 @@ import '../rendering/mock_canvas.dart';
 void main() {
 
   // TODO(clocksmith): Mock math.Random().nextDouble() to return 0.1337.
+  Mock
 
   // From InkSparkle._animationDuration.
   const double animationDurationMicros = 617 * 1000;
+
+  // Animation progress is captured at 0, 50, and 100.
+  // Change this to 25, or another factor of 100, for more granular tests.
   const int testIntervalPercent = 50;
 
   testWidgets('InkSparkle renders with sparkles', (WidgetTester tester) async {
@@ -37,8 +39,8 @@ void main() {
     await tester.tap(buttonFinder);
 
     // Warm up shader. Compilation is of the order of 10 milliseconds and 
-    // Animation is < 1000 milliseconds. Use 2000 milliseconds to prevent
-    // flakiness.
+    // Animation is < 1000 milliseconds. Use 2000 milliseconds as a safety
+    // net to prevent flakiness.
     await tester.pumpAndSettle(const Duration(milliseconds: 2000));
 
     final Offset topLeft = tester.getTopLeft(buttonFinder);
@@ -50,31 +52,31 @@ void main() {
     for (int i = 0; i <= 100; i += testIntervalPercent) {
       await expectLater(
         find.byType(MaterialApp),
-        matchesGoldenFile('ink_sparkle_top_left_${testIntervalPercent}.png'),
+        matchesGoldenFile('ink_sparkle_top_left_$i.png'),
       );
       await tester.pump(Duration(microseconds: (testIntervalPercent * animationDurationMicros).round()));
     }
     await tester.pumpAndSettle(const Duration(milliseconds: 1000));
 
-    // Test tapping the center quadrant of the button.
+    // Test tapping the center of the button.
     final Offset centerTarget = topLeft + (bottomRight - topLeft) * 0.5;
     await tester.tapAt(centerTarget);
     for (int i = 0; i <= 100; i += testIntervalPercent) {
       await expectLater(
         find.byType(MaterialApp),
-        matchesGoldenFile('ink_sparkle_center_${testIntervalPercent}.png'),
+        matchesGoldenFile('ink_sparkle_center_$i.png'),
       );
       await tester.pump(Duration(microseconds: (testIntervalPercent * animationDurationMicros).round()));
     }
     await tester.pumpAndSettle(const Duration(milliseconds: 1000));
 
-    // Test tapping the center quadrant of the button.
+    // Test tapping the bottom right quadrant of the button.
     final Offset bottomRightTarget = topLeft + (bottomRight - topLeft) * 0.8;
     await tester.tapAt(bottomRightTarget);
     for (int i = 0; i <= 100; i += testIntervalPercent) {
       await expectLater(
         find.byType(MaterialApp),
-        matchesGoldenFile('ink_sparkle_center_bottom_right_${testIntervalPercent}.png'),
+        matchesGoldenFile('ink_sparkle_center_bottom_right_$i.png'),
       );
       await tester.pump(Duration(microseconds: (testIntervalPercent * animationDurationMicros).round()));
     }
