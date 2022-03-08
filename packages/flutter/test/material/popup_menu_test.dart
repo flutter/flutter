0938529dbb118da40e9bbd09f00fa29234efc322
Andrei Diaconu <adiaconu@microsoft.com>
2022-03-08 12:19:15 +0200

    Remove unused import

--- packages/flutter/test/material/popup_menu_test.dart
+++ packages/flutter/test/material/popup_menu_test.dart
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'dart:ui' show window, SemanticsFlag;
+import 'dart:ui' show SemanticsFlag;
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/gestures.dart';
