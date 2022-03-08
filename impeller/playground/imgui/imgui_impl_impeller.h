74dff11d574aa465bc5cfbf0e95d6d5a7c885481
Chinmay Garde <chinmaygarde@google.com>
2022-03-08 13:03:03 -0800

    Add a script that checks that all source files have a valid license block. (#63)
    Also fixes the files with missing licenses. This check is somewhat easy with
    Impeller than in the engine because all source files must have the same license
    block.
    
    Resolves an action item in the umbrella issue https://github.com/flutter/flutter/issues/97686.

--- impeller/playground/imgui/imgui_impl_impeller.h
+++ impeller/playground/imgui/imgui_impl_impeller.h
@@ -1,3 +1,7 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 #pragma once
 
 #include <memory>
@@ -5,8 +9,10 @@
 #include "third_party/imgui/imgui.h"
 
 namespace impeller {
+
 class Context;
 class RenderPass;
+
 }  // namespace impeller
 
 IMGUI_IMPL_API bool ImGui_ImplImpeller_Init(
