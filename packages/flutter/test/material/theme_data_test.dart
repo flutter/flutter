9a26dd7c98b338378ccc3cb03efb44c4c687abd5
guidezpl <6655696+guidezpl@users.noreply.github.com>
2022-03-16 15:25:32 +0100

    add all tests except lerping

--- packages/flutter/test/material/theme_data_test.dart
+++ packages/flutter/test/material/theme_data_test.dart
@@ -6,6 +6,85 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+@immutable
+class MyThemeExtensionA extends ThemeExtension<MyThemeExtensionA> {
+  const MyThemeExtensionA({
+    required this.color1,
+    required this.color2,
+  });
+
+  final Color? color1;
+  final Color? color2;
+
+  @override
+  MyThemeExtensionA copyWith({Color? color1, Color? color2}) {
+    return MyThemeExtensionA(
+      color1: color1 ?? this.color1,
+      color2: color2 ?? this.color2,
+    );
+  }
+
+  @override
+  MyThemeExtensionA lerp(ThemeExtension<MyThemeExtensionA>? other, double t) {
+    if (other is! MyThemeExtensionA) {
+      return this;
+    }
+    return MyThemeExtensionA(
+      color1: Color.lerp(color1, other.color1, t),
+      color2: Color.lerp(color2, other.color2, t),
+    );
+  }
+
+  // Optional
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) {
+      return true;
+    }
+    if (other.runtimeType != runtimeType) {
+      return false;
+    }
+    return other is MyThemeExtensionA &&
+        other.color1 == color1 &&
+        other.color2 == color2;
+  }
+
+  // Optional
+  @override
+  int get hashCode {
+    return hashList(<Object?>[
+      color1,
+      color2,
+    ]);
+  }
+}
+
+@immutable
+class MyThemeExtensionB extends ThemeExtension<MyThemeExtensionB> {
+  const MyThemeExtensionB({
+    required this.textStyle,
+  });
+
+  final TextStyle? textStyle;
+
+  @override
+  MyThemeExtensionB copyWith({Color? color, TextStyle? textStyle}) {
+    return MyThemeExtensionB(
+      textStyle: textStyle ?? this.textStyle,
+    );
+  }
+
+  @override
+  MyThemeExtensionB lerp(ThemeExtension<MyThemeExtensionB>? other, double t) {
+    if (other is! MyThemeExtensionB) {
+      return this;
+    }
+    return MyThemeExtensionB(
+      textStyle: TextStyle.lerp(textStyle, other.textStyle, t),
+    );
+  }
+}
+
 void main() {
   test('Theme data control test', () {
     final ThemeData dark = ThemeData.dark();
@@ -338,6 +417,74 @@ void main() {
     expect(contracted.minHeight, equals(0));
     expect(expanded.maxWidth, equals(double.infinity));
     expect(expanded.maxHeight, equals(double.infinity));
+  });
+
+  group('Theme extensions', () {
+    const Key containerKey = Key('container');
+
+    test('copyWith, ==, hashCode basics', () {
+      const MyThemeExtensionA extensionA = MyThemeExtensionA(
+        color1: Colors.black,
+        color2: Colors.amber,
+      );
+      expect(extensionA, extensionA.copyWith());
+      expect(extensionA.hashCode, extensionA.copyWith().hashCode);
+
+      const MyThemeExtensionB extensionB = MyThemeExtensionB(textStyle: TextStyle(fontSize: 50));
+      // Since MyThemeExtensionB doesn't override operator == or hashCode
+      expect(extensionB, isNot(extensionB.copyWith()));
+      expect(extensionB.hashCode, isNot(extensionB.copyWith().hashCode));
+    });
+
+    testWidgets('can be obtained', (WidgetTester tester) async {
+      await tester.pumpWidget(
+        MaterialApp(
+          theme: ThemeData(
+            extensions: const <Object, ThemeExtension<dynamic>>{
+              MyThemeExtensionA: MyThemeExtensionA(
+                color1: Colors.black,
+                color2: Colors.amber,
+              ),
+              MyThemeExtensionB: MyThemeExtensionB(
+                textStyle: TextStyle(fontSize: 50),
+              )
+            },
+          ),
+          home: Container(key: containerKey),
+        ),
+      );
+
+      final ThemeData theme = Theme.of(
+        tester.element(find.byKey(containerKey)),
+      );
+
+      expect(theme.extension<MyThemeExtensionA>()!.color1, Colors.black);
+      expect(theme.extension<MyThemeExtensionA>()!.color2, Colors.amber);
+      expect(theme.extension<MyThemeExtensionB>()!.textStyle, const TextStyle(fontSize: 50));
+    });
+
+    testWidgets('can use copyWith', (WidgetTester tester) async {
+      await tester.pumpWidget(
+        MaterialApp(
+          theme: ThemeData(
+            extensions: <Object, ThemeExtension<dynamic>>{
+              MyThemeExtensionA: const MyThemeExtensionA(
+                color1: Colors.black,
+                color2: Colors.amber,
+              ).copyWith(color1: Colors.blue),
+            },
+          ),
+          home: Container(key: containerKey),
+        ),
+      );
+
+      final ThemeData theme = Theme.of(
+        tester.element(find.byKey(containerKey)),
+      );
+
+      expect(theme.extension<MyThemeExtensionA>()!.color1, Colors.blue);
+      expect(theme.extension<MyThemeExtensionA>()!.color2, Colors.amber);
+    });
   });
 
   test('copyWith, ==, hashCode basics', () {
