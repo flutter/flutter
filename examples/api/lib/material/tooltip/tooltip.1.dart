8c0ea15c635dac0666602719af0247510a325280
Taha Tesser <tessertaha@gmail.com>
2022-03-22 15:26:41 +0200

    `Tooltip`: Add an example for `TooltipTriggerMode.manual` and add tests for existing `Tooltip` examples

--- examples/api/lib/material/tooltip/tooltip.1.dart
+++ examples/api/lib/material/tooltip/tooltip.1.dart
@@ -9,7 +9,7 @@ import 'package:flutter/material.dart';
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   static const String _title = 'Flutter Code Sample';
 
@@ -20,15 +20,15 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(title: const Text(_title)),
         body: const Center(
-          child: MyStatelessWidget(),
+          child: TooltipSample(),
         ),
       ),
     );
   }
 }
 
-class MyStatelessWidget extends StatelessWidget {
-  const MyStatelessWidget({Key? key}) : super(key: key);
+class TooltipSample extends StatelessWidget {
+  const TooltipSample({super.key});
 
   @override
   Widget build(BuildContext context) {
