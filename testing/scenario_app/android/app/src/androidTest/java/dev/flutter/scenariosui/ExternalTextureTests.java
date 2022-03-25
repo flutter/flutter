6096dd189b8217bbdc5cc13f4dbd6f1960e3132b
Dwayne Slater <ds84182@gmail.com>
2022-03-25 10:59:54 -0700

    Add test for external textures on Android (#31642)

---
+++ testing/scenario_app/android/app/src/androidTest/java/dev/flutter/scenariosui/ExternalTextureTests.java
@@ -0,0 +1,111 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package dev.flutter.scenariosui;
+
+import android.content.Intent;
+import android.graphics.Rect;
+import android.os.Build.VERSION_CODES;
+import androidx.annotation.NonNull;
+import androidx.test.filters.LargeTest;
+import androidx.test.filters.SdkSuppress;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+import dev.flutter.scenarios.ExternalTextureFlutterActivity;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class ExternalTextureTests {
+  private static final int SURFACE_WIDTH = 192;
+  private static final int SURFACE_HEIGHT = 256;
+
+  Intent intent;
+
+  @Rule @NonNull
+  public ActivityTestRule<ExternalTextureFlutterActivity> activityRule =
+      new ActivityTestRule<>(
+          ExternalTextureFlutterActivity.class,
+          /*initialTouchMode=*/ false,
+          /*launchActivity=*/ false);
+
+  @Before
+  public void setUp() {
+    intent = new Intent(Intent.ACTION_MAIN);
+  }
+
+  @Test
+  public void testCanvasSurface() throws Exception {
+    intent.putExtra("scenario", "display_texture");
+    intent.putExtra("surface_renderer", "canvas");
+    ScreenshotUtil.capture(activityRule.launchActivity(intent));
+  }
+
+  @Test
+  @SdkSuppress(minSdkVersion = VERSION_CODES.LOLLIPOP)
+  public void testMediaSurface() throws Exception {
+    intent.putExtra("scenario", "display_texture");
+    intent.putExtra("surface_renderer", "media");
+    ScreenshotUtil.capture(activityRule.launchActivity(intent));
+  }
+
+  @Test
+  @SdkSuppress(minSdkVersion = VERSION_CODES.LOLLIPOP)
+  public void testRotatedMediaSurface_90() throws Exception {
+    intent.putExtra("scenario", "display_texture");
+    intent.putExtra("surface_renderer", "media");
+    intent.putExtra("rotation", 90);
+    ScreenshotUtil.capture(activityRule.launchActivity(intent));
+  }
+
+  @Test
+  @SdkSuppress(minSdkVersion = VERSION_CODES.LOLLIPOP)
+  public void testRotatedMediaSurface_180() throws Exception {
+    intent.putExtra("scenario", "display_texture");
+    intent.putExtra("surface_renderer", "media");
+    intent.putExtra("rotation", 180);
+    ScreenshotUtil.capture(activityRule.launchActivity(intent));
+  }
+
+  @Test
+  @SdkSuppress(minSdkVersion = VERSION_CODES.LOLLIPOP)
+  public void testRotatedMediaSurface_270() throws Exception {
+    intent.putExtra("scenario", "display_texture");
+    intent.putExtra("surface_renderer", "media");
+    intent.putExtra("rotation", 270);
+    ScreenshotUtil.capture(activityRule.launchActivity(intent));
+  }
+
+  @Test
+  @SdkSuppress(minSdkVersion = VERSION_CODES.M)
+  public void testCroppedMediaSurface_bottomLeft() throws Exception {
+    intent.putExtra("scenario", "display_texture");
+    intent.putExtra("surface_renderer", "image");
+    intent.putExtra("crop", new Rect(0, 0, SURFACE_WIDTH / 2, SURFACE_HEIGHT / 2));
+    ScreenshotUtil.capture(activityRule.launchActivity(intent));
+  }
+
+  @Test
+  @SdkSuppress(minSdkVersion = VERSION_CODES.M)
+  public void testCroppedMediaSurface_topRight() throws Exception {
+    intent.putExtra("scenario", "display_texture");
+    intent.putExtra("surface_renderer", "image");
+    intent.putExtra(
+        "crop", new Rect(SURFACE_WIDTH / 2, SURFACE_HEIGHT / 2, SURFACE_WIDTH, SURFACE_HEIGHT));
+    ScreenshotUtil.capture(activityRule.launchActivity(intent));
+  }
+
+  @Test
+  @SdkSuppress(minSdkVersion = VERSION_CODES.M)
+  public void testCroppedRotatedMediaSurface_bottomLeft_90() throws Exception {
+    intent.putExtra("scenario", "display_texture");
+    intent.putExtra("surface_renderer", "image");
+    intent.putExtra("crop", new Rect(0, 0, SURFACE_WIDTH / 2, SURFACE_HEIGHT / 2));
+    intent.putExtra("rotation", 90);
+    ScreenshotUtil.capture(activityRule.launchActivity(intent));
+  }
+}
