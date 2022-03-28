83f102353cf00216a7f4b16a795e2907cecd2eb8
Brandon DeRosier <bdero@google.com>
2022-03-28 11:54:09 -0700

    Wire up stroke cap/join/miter limit display list ops (flutter/engine#105)

--- engine/src/flutter/impeller/display_list/display_list_unittests.cc
+++ engine/src/flutter/impeller/display_list/display_list_unittests.cc
@@ -5,6 +5,7 @@
 #include "flutter/display_list/display_list_builder.h"
 #include "flutter/testing/testing.h"
 #include "impeller/display_list/display_list_playground.h"
+#include "third_party/skia/include/core/SkPathBuilder.h"
 
 namespace impeller {
 namespace testing {
@@ -23,6 +24,51 @@ TEST_F(DisplayListTest, CanDrawTextBlob) {
   builder.setColor(SK_ColorBLUE);
   builder.drawTextBlob(SkTextBlob::MakeFromString("Hello", CreateTestFont()),
                        100, 100);
+  ASSERT_TRUE(OpenPlaygroundHere(builder.Build()));
+}
+
+TEST_F(DisplayListTest, CapsAndJoins) {
+  flutter::DisplayListBuilder builder;
+
+  builder.setStyle(SkPaint::Style::kStroke_Style);
+  builder.setStrokeWidth(30);
+  builder.setColor(SK_ColorRED);
+
+  auto path =
+      SkPathBuilder{}.moveTo(-50, 0).lineTo(0, -50).lineTo(50, 0).snapshot();
+
+  builder.translate(100, 100);
+  {
+    builder.setStrokeCap(SkPaint::Cap::kButt_Cap);
+    builder.setStrokeJoin(SkPaint::Join::kMiter_Join);
+    builder.setStrokeMiter(4);
+    builder.drawPath(path);
+  }
+
+  {
+    builder.save();
+    builder.translate(0, 100);
+    // The joint in the path is 45 degrees. A miter length of 1 convert to a
+    // bevel in this case.
+    builder.setStrokeMiter(1);
+    builder.drawPath(path);
+    builder.restore();
+  }
+
+  builder.translate(150, 0);
+  {
+    builder.setStrokeCap(SkPaint::Cap::kSquare_Cap);
+    builder.setStrokeJoin(SkPaint::Join::kBevel_Join);
+    builder.drawPath(path);
+  }
+
+  builder.translate(150, 0);
+  {
+    builder.setStrokeCap(SkPaint::Cap::kRound_Cap);
+    builder.setStrokeJoin(SkPaint::Join::kRound_Join);
+    builder.drawPath(path);
+  }
+
   ASSERT_TRUE(OpenPlaygroundHere(builder.Build()));
 }
 
