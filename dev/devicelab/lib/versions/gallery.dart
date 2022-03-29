43029bebc485eca7b380208f1c857e557ae875bf
Michael Goderbauer <goderbauer@google.com>
2022-03-29 12:15:32 -0700

    Roll gallery to 03dedee4bc24878fec1c1d1338daffd5b792a29d (#100989)

--- dev/devicelab/lib/versions/gallery.dart
+++ dev/devicelab/lib/versions/gallery.dart
@@ -3,4 +3,4 @@
 // found in the LICENSE file.
 
 /// The pinned version of flutter gallery, used for devicelab tests.
-const String galleryVersion = 'd63bd9200b6a8ad69d47bdc5e4059fadb9c962c1';
+const String galleryVersion = '03dedee4bc24878fec1c1d1338daffd5b792a29d';
