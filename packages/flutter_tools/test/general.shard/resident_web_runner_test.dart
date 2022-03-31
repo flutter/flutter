e52b777a2c35231225eeb1581434e98ee90899a2
David Iglesias <ditman@gmail.com>
2022-03-30 23:00:09 -0700

    [web] flutter.js initialization with ui.webOnlyWarmupEngine (#100177)

--- packages/flutter_tools/test/general.shard/resident_web_runner_test.dart
+++ packages/flutter_tools/test/general.shard/resident_web_runner_test.dart
@@ -569,7 +569,7 @@ void main() {
     final String entrypointContents = fileSystem.file(webDevFS.mainUri).readAsStringSync();
     expect(entrypointContents, contains('// Flutter web bootstrap script'));
     expect(entrypointContents, contains("import 'dart:ui' as ui;"));
-    expect(entrypointContents, contains('await ui.webOnlyInitializePlatform();'));
+    expect(entrypointContents, contains('await ui.webOnlyWarmupEngine('));
 
     expect(logger.statusText, contains('Restarted application in'));
     expect(result.code, 0);
