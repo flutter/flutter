f80986abc9f5a2a0bd98b09e4765f6e834862a18
Jenn Magder <magder@google.com>
2022-04-01 13:41:56 -0700

    Add --use-application-binary to "flutter install"

--- packages/flutter_tools/lib/src/commands/install.dart
+++ packages/flutter_tools/lib/src/commands/install.dart
@@ -5,6 +5,7 @@
 import '../android/android_device.dart';
 import '../application_package.dart';
 import '../base/common.dart';
+import '../base/file_system.dart';
 import '../base/io.dart';
 import '../device.dart';
 import '../globals.dart' as globals;
@@ -15,9 +16,18 @@ class InstallCommand extends FlutterCommand with DeviceBasedDevelopmentArtifacts
     requiresPubspecYaml();
     usesDeviceUserOption();
     usesDeviceTimeoutOption();
-    argParser.addFlag('uninstall-only',
-      help: 'Uninstall the app if already on the device. Skip install.',
-    );
+    argParser
+      ..addFlag(
+        'uninstall-only',
+        help: 'Uninstall the app if already on the device. Skip install.',
+      )
+      ..addOption(
+        'use-application-binary',
+        help: 'Specify a pre-built application binary to use when running. For Android applications, '
+            'this must be the path to an APK. For iOS applications, the path to an IPA. Other device types '
+            'do not yet support prebuilt application binaries.',
+        valueHelp: 'path/to/app.apk',
+      );
   }
 
   @override
@@ -34,6 +44,9 @@ class InstallCommand extends FlutterCommand with DeviceBasedDevelopmentArtifacts
   bool get uninstallOnly => boolArg('uninstall-only');
   String? get userIdentifier => stringArg(FlutterOptions.kDeviceUser);
 
+  String? get _applicationBinaryPath => stringArg('use-application-binary');
+  File? get _applicationBinary => _applicationBinaryPath == null ? null : globals.fs.file(_applicationBinaryPath);
+
   @override
   Future<void> validateCommand() async {
     await super.validateCommand();
@@ -44,6 +57,9 @@ class InstallCommand extends FlutterCommand with DeviceBasedDevelopmentArtifacts
     if (userIdentifier != null && device is! AndroidDevice) {
       throwToolExit('--${FlutterOptions.kDeviceUser} is only supported for Android');
     }
+    if (_applicationBinaryPath != null && !(_applicationBinary?.existsSync() ?? true)) {
+      throwToolExit('Prebuilt binary $_applicationBinaryPath does not exist');
+    }
   }
 
   @override
@@ -51,6 +67,7 @@ class InstallCommand extends FlutterCommand with DeviceBasedDevelopmentArtifacts
     final Device targetDevice = device!;
     final ApplicationPackage? package = await applicationPackages?.getPackageForPlatform(
       await targetDevice.targetPlatform,
+      applicationBinary: _applicationBinary,
     );
     if (package == null) {
       throwToolExit('Could not find or build package');
