b38999d39a53957d251b872b174c58c8d3db37fa
Justin McCandless <jmccandless@google.com>
2022-04-06 15:50:10 -0700

    TextFormField support

--- packages/flutter/lib/src/material/text_form_field.dart
+++ packages/flutter/lib/src/material/text_form_field.dart
@@ -147,6 +147,7 @@ class TextFormField extends FormField<String> {
     String? restorationId,
     bool enableIMEPersonalizedLearning = true,
     MouseCursor? mouseCursor,
+    TextSelectionToolbarBuilder? buildContextualMenu,
   }) : assert(initialValue == null || controller == null),
        assert(textAlign != null),
        assert(autofocus != null),
@@ -238,6 +239,7 @@ class TextFormField extends FormField<String> {
                scrollController: scrollController,
                enableIMEPersonalizedLearning: enableIMEPersonalizedLearning,
                mouseCursor: mouseCursor,
+               buildContextualMenu: buildContextualMenu,
              ),
            );
          },
