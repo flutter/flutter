7c2f1bd04f013abf8c14d7b0ebb35b0f2b90035b
Gary Qian <garyq@google.com>
2022-04-06 13:39:45 -0700

    ïmproved status

--- packages/flutter_tools/lib/src/migrate/migrate_manifest.dart
+++ packages/flutter_tools/lib/src/migrate/migrate_manifest.dart
@@ -100,6 +100,16 @@ class MigrateManifest {
     return output;
   }
 
+  List<String> resolvedConflictFiles(Directory workingDir) {
+    final List<String> output = <String>[];
+    for (final String localPath in conflictFiles) {
+      if (MigrateUtils.conflictsResolved(workingDir.childFile(localPath).readAsStringSync())) {
+        output.add(localPath);
+      }
+    }
+    return output;
+  }
+
   /// A list of local paths of files that were automatically merged.
   List<String> get mergedFiles {
     final List<String> output = <String>[];
