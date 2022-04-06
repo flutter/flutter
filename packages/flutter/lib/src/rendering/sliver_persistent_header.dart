5c141cca926d55cf0e01b265c38da87a278fd05f
Kate Lovett <katelovett@google.com>
2022-04-06 15:26:45 -0500

    Init, needs tests. Needs to check sliver app bars

--- packages/flutter/lib/src/rendering/sliver_persistent_header.dart
+++ packages/flutter/lib/src/rendering/sliver_persistent_header.dart
@@ -376,6 +376,7 @@ abstract class RenderSliverScrollingPersistentHeader extends RenderSliverPersist
   void performLayout() {
     final SliverConstraints constraints = this.constraints;
     final double maxExtent = this.maxExtent;
+    print('scrolling');
     layoutChild(constraints.scrollOffset, maxExtent);
     final double paintExtent = maxExtent - constraints.scrollOffset;
     geometry = SliverGeometry(
@@ -419,8 +420,10 @@ abstract class RenderSliverPinnedPersistentHeader extends RenderSliverPersistent
   @override
   void performLayout() {
     final SliverConstraints constraints = this.constraints;
+    print('Pinned, constraints: $constraints');
     final double maxExtent = this.maxExtent;
     final bool overlapsContent = constraints.overlap > 0.0;
+    print('pinned');
     layoutChild(constraints.scrollOffset, maxExtent, overlapsContent: overlapsContent);
     final double effectiveRemainingPaintExtent = math.max(0, constraints.remainingPaintExtent - constraints.overlap);
     final double layoutExtent = (maxExtent - constraints.scrollOffset).clamp(0.0, effectiveRemainingPaintExtent);
@@ -682,7 +685,7 @@ abstract class RenderSliverFloatingPersistentHeader extends RenderSliverPersiste
       _effectiveScrollOffset = constraints.scrollOffset;
     }
     final bool overlapsContent = _effectiveScrollOffset! < constraints.scrollOffset;
-
+    print('floating pinned');
     layoutChild(
       _effectiveScrollOffset!,
       maxExtent,
