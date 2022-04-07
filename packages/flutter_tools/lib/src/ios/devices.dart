ed10440ca3a16cbbb47c39ce3d08f8219f6ffb44
Christopher Fujino <christopherfujino@gmail.com>
2022-04-07 16:58:10 -0700

    wip

--- packages/flutter_tools/lib/src/ios/devices.dart
+++ packages/flutter_tools/lib/src/ios/devices.dart
@@ -432,7 +432,8 @@ class IOSDevice extends Device {
       }
 
       _logger.printTrace('Application launched on the device. Waiting for observatory url.');
-      final Timer timer = Timer(discoveryTimeout ?? const Duration(seconds: 30), () {
+      final Timer timer = Timer(Duration.zero, () { // TODO
+      //final Timer timer = Timer(discoveryTimeout ?? const Duration(seconds: 30), () {
         _logger.printError('iOS Observatory not discovered after 30 seconds. This is taking much longer than expected...');
         iosDeployDebugger?.stopAndDumpBacktrace();
       });
