fe2578fe43c1e1ad2970b7c7704b68fe12edb437
danagbemava-nc <daniel@nevercode.io>
2022-04-07 12:58:19 +0000

    remove unnecessary variable

--- examples/api/lib/widgets/binding/widget_binding_observer.0.dart
+++ examples/api/lib/widgets/binding/widget_binding_observer.0.dart
@@ -11,14 +11,11 @@ void main() => runApp(const MyApp());
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  static const String _title = 'WidgetBindingsObserver Sample';
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: _title,
       home: Scaffold(
-        appBar: AppBar(title: const Text(_title)),
+        appBar: AppBar(title: const Text('WidgetBindingsObserver Sample')),
         body: const WidgetBindingsObserverSample(),
       ),
     );
