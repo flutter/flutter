3759297f9c576446f1ec06c438ef22ba077bdd2e
Mateus Felipe <mateusfccp@gmail.com>
2022-04-11 08:25:45 -0300

    Provide license

--- packages/flutter/lib/src/gestures/details_with_position.dart
+++ packages/flutter/lib/src/gestures/details_with_position.dart
@@ -1,3 +1,8 @@
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+
 import 'dart:ui';
 
 /// Details object for gesture callbacks that provides local and global positions.
