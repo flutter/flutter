600f4881f367aa94029d7bd686dbea3852d93182
Christopher Fujino <christopherfujino@gmail.com>
2022-04-12 09:44:07 -0700

    fix analysis

--- packages/flutter_tools/test/general.shard/ios/ios_device_start_prebuilt_test.dart
+++ packages/flutter_tools/test/general.shard/ios/ios_device_start_prebuilt_test.dart
@@ -10,7 +10,6 @@ import 'dart:convert';
 import 'package:file/memory.dart';
 import 'package:flutter_tools/src/artifacts.dart';
 import 'package:flutter_tools/src/base/file_system.dart';
-import 'package:flutter_tools/src/base/io.dart';
 import 'package:flutter_tools/src/base/logger.dart';
 import 'package:flutter_tools/src/base/platform.dart';
 import 'package:flutter_tools/src/build_info.dart';
