c39d7b80811c4903213d9559c764386ab26ada02
fzyzcjy <5236035+fzyzcjy@users.noreply.github.com>
2022-04-14 08:36:50 +0800

    Update image_filter_test.dart

--- packages/flutter/test/widgets/image_filter_test.dart
+++ packages/flutter/test/widgets/image_filter_test.dart
@@ -9,6 +9,7 @@
 import 'dart:typed_data';
 import 'dart:ui';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter_test/flutter_test.dart';
