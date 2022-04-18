0f47acbe4f7d46740ed9cb10ee554b421d144612
Jesús S Guerrero <jesus_sguerrero@hotmail.com>
2022-04-18 16:39:16 -0700

    test validator task dummy #2885

--- packages/flutter_tools/test/general.shard/analyze_project_test.dart
+++ packages/flutter_tools/test/general.shard/analyze_project_test.dart
@@ -2,9 +2,39 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'package:file/memory.dart';
 import 'package:flutter_tools/src/analyze_project.dart';
+import 'package:flutter_tools/src/analyze_project_validator.dart';
+import 'package:flutter_tools/src/project.dart';
 
 import '../src/common.dart';
+
+class ProjectValidatorTaskImpl extends ProjectValidatorTask {
+
+  @override
+  List<ProjectValidatorResult> start(FlutterProject project) {
+    final ProjectValidatorResult error = ProjectValidatorResult('result_1');
+    error.setError('this is an error');
+    final ProjectValidatorResult success = ProjectValidatorResult('result_2');
+    success.setSuccess('correct');
+    final ProjectValidatorResult warning = ProjectValidatorResult('result_3');
+    warning.setSuccess('this passed', warning: 'with a warning');
+
+    return [
+      error,
+      success,
+      warning,
+    ];
+  }
+
+  @override
+  List<SupportedPlatform> get supportedPlatforms {
+    return [
+      SupportedPlatform.ios,
+      SupportedPlatform.android,
+    ];
+  }
+}
 
 void main() {
   group('ProjectValidatorResult', () {
@@ -38,6 +68,24 @@ void main() {
       result.setError('my error');
       expect(result.toString(), 'Error: my error');
       expect(result.currentStatus(), Status.error);
+    });
+  });
+
+  group('ProjectValidatorTask', () {
+    late ProjectValidatorTaskImpl task;
+
+    setUp(() {
+      task = ProjectValidatorTaskImpl();
+    });
+
+    testWithoutContext('error status', () {
+      MemoryFileSystem fs = MemoryFileSystem.test();
+      final FlutterProject project = FlutterProject.fromDirectoryTest(fs.currentDirectory);
+      final List<ProjectValidatorResult> results = task.start(project);
+      expect(results.length, 3);
+      expect(results[0].toString(), 'Error: this is an error');
+      expect(results[1].toString(), 'result_2: correct');
+      expect(results[2].toString(), 'result_3: this passed. Warning: with a warning');
     });
   });
 }
