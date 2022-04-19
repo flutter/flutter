ca9183b6cb57fdf90d52ac95541a45c042f10a7e
Jonah Williams <jonahwilliams@google.com>
2022-04-19 09:10:04 -0700

    Update image_filter.dart

--- packages/flutter/lib/src/widgets/image_filter.dart
+++ packages/flutter/lib/src/widgets/image_filter.dart
@@ -21,7 +21,7 @@ import 'framework.dart';
 /// cannot tell the difference between an intentional no-op and a filter that is
 /// only incidentally a no-op. Consider an ImageFilter.matrix that is animated
 /// and happens to pass through the identity matrix. If the framework identified it
-/// as a no-op it could drop and then recreate the layer during the animation which
+/// as a no-op it would drop and then recreate the layer during the animation which
 /// would be more expensive than keeping it around.
 ///
 /// {@youtube 560 315 https://www.youtube.com/watch?v=7Lftorq4i2o}
