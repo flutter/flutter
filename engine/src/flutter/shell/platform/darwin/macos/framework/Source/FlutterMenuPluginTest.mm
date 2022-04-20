1be09fac9ba41f57f5f158fcb17b12f4b71cac79
Greg Spencer <gspencergoog@users.noreply.github.com>
2022-04-20 09:54:36 -0700

    Add Menu.isPluginAvailable to allow query of whether menus are available on the platform. (flutter/engine#32795)
    Added a "Menu.isPluginAvailable" call to allow the framework to query whether a plugin for creating platform menus is available on the current platform.
    
    This will allow the framework MenuBar.adaptive constructor to determine, based on the availability on the platform of the plugin, whether or not to render platform menus. This replaces the old way, which was just to hard code which platforms support it (i.e. just macOS), but that won't work well if someone adds a plugin that supports a particular platform that isn't macOS.

--- engine/src/flutter/shell/platform/darwin/macos/framework/Source/FlutterMenuPluginTest.mm
+++ engine/src/flutter/shell/platform/darwin/macos/framework/Source/FlutterMenuPluginTest.mm
@@ -106,6 +106,15 @@
     ],
   };
 
+  __block id available = @NO;
+  [plugin handleMethodCall:[FlutterMethodCall methodCallWithMethodName:@"Menu.isPluginAvailable"
+                                                             arguments:nil]
+                    result:^(id _Nullable result) {
+                      available = result;
+                    }];
+
+  EXPECT_TRUE(available);
+
   [plugin handleMethodCall:[FlutterMethodCall methodCallWithMethodName:@"Menu.setMenus"
                                                              arguments:testMenus]
                     result:^(id _Nullable result){
