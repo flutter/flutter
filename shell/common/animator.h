238eb9d7f55f0ca24d2a1e706b02109ef76844cf
Dan Field <dnfield@google.com>
2022-04-21 13:28:03 -0700

    Remove redundant trace events (#32812)

--- shell/common/animator.h
+++ shell/common/animator.h
@@ -94,8 +94,6 @@ class Animator final {
 
   void AwaitVSync();
 
-  const char* FrameParity();
-
   // Clear |trace_flow_ids_| if |frame_scheduled_| is false.
   void ScheduleMaybeClearTraceFlowIds();
 
