32db775148562d2d16a544ea0141c5f53905436b
J-P Nurmi <jpnurmi@gmail.com>
2022-04-26 19:39:03 +0200

    [Linux] fix and test light vs. dark theme detection (flutter/engine#32618)

---
+++ engine/src/flutter/shell/platform/linux/fl_settings.cc
@@ -0,0 +1,49 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "flutter/shell/platform/linux/fl_settings.h"
+#include "flutter/shell/platform/linux/fl_gnome_settings.h"
+
+G_DEFINE_INTERFACE(FlSettings, fl_settings, G_TYPE_OBJECT)
+
+enum {
+  SIGNAL_CHANGED,
+  SIGNAL_LAST_SIGNAL,
+};
+
+static guint signals[SIGNAL_LAST_SIGNAL];
+
+static void fl_settings_default_init(FlSettingsInterface* iface) {
+  /**
+   * FlSettings::changed:
+   * @settings: an #FlSettings
+   *
+   * This signal is emitted after the settings have been changed.
+   */
+  signals[SIGNAL_CHANGED] =
+      g_signal_new("changed", G_TYPE_FROM_INTERFACE(iface), G_SIGNAL_RUN_LAST,
+                   0, NULL, NULL, NULL, G_TYPE_NONE, 0);
+}
+
+FlClockFormat fl_settings_get_clock_format(FlSettings* self) {
+  return FL_SETTINGS_GET_IFACE(self)->get_clock_format(self);
+}
+
+FlColorScheme fl_settings_get_color_scheme(FlSettings* self) {
+  return FL_SETTINGS_GET_IFACE(self)->get_color_scheme(self);
+}
+
+gdouble fl_settings_get_text_scaling_factor(FlSettings* self) {
+  return FL_SETTINGS_GET_IFACE(self)->get_text_scaling_factor(self);
+}
+
+void fl_settings_emit_changed(FlSettings* self) {
+  g_return_if_fail(FL_IS_SETTINGS(self));
+  g_signal_emit(self, signals[SIGNAL_CHANGED], 0);
+}
+
+FlSettings* fl_settings_new() {
+  // TODO(jpnurmi): add support for other desktop environments
+  return FL_SETTINGS(fl_gnome_settings_new());
+}
---
+++ engine/src/flutter/shell/platform/linux/testing/mock_settings.h
@@ -0,0 +1,39 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef FLUTTER_SHELL_PLATFORM_LINUX_TESTING_MOCK_SETTINGS_H_
+#define FLUTTER_SHELL_PLATFORM_LINUX_TESTING_MOCK_SETTINGS_H_
+
+#include "flutter/shell/platform/linux/fl_settings.h"
+
+#include "gmock/gmock.h"
+
+namespace flutter {
+namespace testing {
+
+// Mock for FlSettings.
+class MockSettings {
+ public:
+  MockSettings();
+  ~MockSettings();
+
+  operator FlSettings*();
+
+  MOCK_METHOD1(fl_settings_get_clock_format,
+               FlClockFormat(FlSettings* settings));
+
+  MOCK_METHOD1(fl_settings_get_color_scheme,
+               FlColorScheme(FlSettings* settings));
+
+  MOCK_METHOD1(fl_settings_get_text_scaling_factor,
+               gdouble(FlSettings* settings));
+
+ private:
+  FlSettings* instance_ = nullptr;
+};
+
+}  // namespace testing
+}  // namespace flutter
+
+#endif  // FLUTTER_SHELL_PLATFORM_LINUX_TESTING_MOCK_SETTINGS_H_
