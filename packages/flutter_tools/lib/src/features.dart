d3316dbe9b5b3e7eb32e1a49ed398048fd570d46
Tim Sneath <timsneath@google.com>
2022-04-28 16:34:08 -0700

    Update features.dart (#102766)

--- packages/flutter_tools/lib/src/features.dart
+++ packages/flutter_tools/lib/src/features.dart
@@ -90,7 +90,6 @@ const Feature flutterMacOSDesktopFeature = Feature(
   name: 'support for desktop on macOS',
   configSetting: 'enable-macos-desktop',
   environmentOverride: 'FLUTTER_MACOS',
-  extraHelpText: 'Newer beta versions are available on the beta channel.',
   master: FeatureChannelSetting(
     available: true,
   ),
@@ -107,7 +106,6 @@ const Feature flutterLinuxDesktopFeature = Feature(
   name: 'support for desktop on Linux',
   configSetting: 'enable-linux-desktop',
   environmentOverride: 'FLUTTER_LINUX',
-  extraHelpText: 'Newer beta versions are available on the beta channel.',
   master: FeatureChannelSetting(
     available: true,
   ),
