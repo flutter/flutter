692f5f51918d55f71eadd016c6d6c67c5576ced9
Aman Verma <verma1090aman@gmail.com>
2022-05-03 01:29:24 +0530

    fix merge conflicts

--- packages/flutter/test/material/chip_input_test.dart
+++ packages/flutter/test/material/chip_input_test.dart
@@ -5,8 +5,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import '../rendering/mock_canvas.dart';
-
 /// Adds the basic requirements for a Chip.
 Widget wrapForChip({
   required Widget child,
