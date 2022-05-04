bf37e5312b289ceefe51ed2aa87854c75014bac1
Taha Tesser <tessertaha@gmail.com>
2022-05-05 01:15:21 +0300

    Improve `Hero` examples

--- examples/api/lib/widgets/heroes/hero.1.dart
+++ examples/api/lib/widgets/heroes/hero.1.dart
@@ -18,13 +18,8 @@ class HeroApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: const Text('Hero Sample')),
-        body: const Center(
-          child: HeroExample(),
-        ),
-      ),
+    return const MaterialApp(
+      home: HeroExample(),
     );
   }
 }
@@ -34,43 +29,45 @@ class HeroExample extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        ListTile(
-          leading: Hero(
-            tag: 'hero-default-tween',
-            child: _box(size: 50.0, color: Colors.red[700]!.withOpacity(0.5)),
+    return Scaffold(
+      appBar: AppBar(title: const Text('Hero Sample')),
+      body: Column(
+        children: <Widget>[
+          ListTile(
+            leading: Hero(
+              tag: 'hero-default-tween',
+              child: BoxWidget(
+                size: const Size(50.0, 50.0),
+                color:Colors.red[700]!.withOpacity(0.5),
+              ),
+            ),
+            title: const Text(
+              'This red icon will use a default rect tween during the hero flight.',
+            ),
           ),
-          title: const Text(
-            'This red icon will use a default rect tween during the hero flight.',
+          const SizedBox(height: 10.0),
+          ListTile(
+            leading: Hero(
+              tag: 'hero-custom-tween',
+              createRectTween: (Rect? begin, Rect? end) {
+                return MaterialRectCenterArcTween(begin: begin, end: end);
+              },
+              child: BoxWidget(
+                size: const Size(50.0, 50.0),
+                color:Colors.blue[700]!.withOpacity(0.5),
+              ),
+            ),
+            title: const Text(
+              'This blue icon will use a custom rect tween during the hero flight.',
+            ),
           ),
-        ),
-        const SizedBox(height: 10.0),
-        ListTile(
-          leading: Hero(
-            tag: 'hero-custom-tween',
-            createRectTween: (Rect? begin, Rect? end) {
-              return MaterialRectCenterArcTween(begin: begin, end: end);
-            },
-            child: _box(size: 50.0, color: Colors.blue[700]!.withOpacity(0.5)),
+          const SizedBox(height: 10),
+          ElevatedButton(
+            onPressed: () => _gotoDetailsPage(context),
+            child: const Text('Tap to trigger hero flight'),
           ),
-          title: const Text(
-            'This blue icon will use a custom rect tween during the hero flight.',
-          ),
-        ),
-        const SizedBox(height: 10),
-        ElevatedButton(
-          onPressed: () => _gotoDetailsPage(context),
-          child: const Text('Tap to trigger hero flight'),
-        ),
-      ],
-    );
-  }
-
-  Widget _box({double? size, Color? color}) {
-    return Container(
-      color: color,
-      child: FlutterLogo(size: size),
+        ],
+      ),
     );
   }
 
@@ -89,15 +86,15 @@ class HeroExample extends StatelessWidget {
                 createRectTween: (Rect? begin, Rect? end) {
                   return MaterialRectCenterArcTween(begin: begin, end: end);
                 },
-                child: _box(
-                  size: 400.0,
+                child: BoxWidget(
+                  size: const Size(400.0, 400.0),
                   color: Colors.blue[700]!.withOpacity(0.5),
                 ),
               ),
               Hero(
                 tag: 'hero-default-tween',
-                child: _box(
-                  size: 400.0,
+                child: BoxWidget(
+                  size: const Size(400.0, 400.0),
                   color: Colors.red[700]!.withOpacity(0.5),
                 ),
               ),
@@ -106,5 +103,25 @@ class HeroExample extends StatelessWidget {
         ),
       ),
     ));
+  }
+}
+
+class BoxWidget extends StatelessWidget {
+  const BoxWidget({
+    Key? key,
+    required this.size,
+    required this.color,
+  }) : super(key: key);
+
+  final Size size;
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: size.width,
+      height: size.height,
+      color: color,
+    );
   }
 }
