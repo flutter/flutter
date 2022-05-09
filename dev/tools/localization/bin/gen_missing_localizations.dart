6504f2896ccb26e2fcdb31d67e0e9c5e218d6756
Greg Spencer <gspencergoog@users.noreply.github.com>
2022-05-09 09:25:52 -0700

    Adds `menuBarMenuLabel`, and removes unneeded key localizations (#102100)
    When I added localizations for shortcut keys, I added some that actually can't be shortcut keys, so I'm removing them again. These are mostly Japanese-specific keys that don't even appear on modern keyboards for the most part.
    
    Also, added menuBarMenuLabel for an accessibility label for menu bar menus.
    
    I modified the code for the localization generation scripts to add a --remove-undefined flag that will remove any localizations that don't appear in the canonical locale.

--- dev/tools/localization/bin/gen_missing_localizations.dart
+++ dev/tools/localization/bin/gen_missing_localizations.dart
@@ -27,11 +27,15 @@ import '../localizations_utils.dart';
 import '../localizations_validator.dart';
 
 Future<void> main(List<String> rawArgs) async {
+  bool removeUndefined = false;
+  if (rawArgs.contains('--remove-undefined')) {
+    removeUndefined = true;
+  }
   checkCwdIsRepoRoot('gen_missing_localizations');
 
   final String localizationPath = path.join('packages', 'flutter_localizations', 'lib', 'src', 'l10n');
-  updateMissingResources(localizationPath, 'material');
-  updateMissingResources(localizationPath, 'cupertino');
+  updateMissingResources(localizationPath, 'material', removeUndefined: removeUndefined);
+  updateMissingResources(localizationPath, 'cupertino', removeUndefined: removeUndefined);
 }
 
 Map<String, dynamic> loadBundle(File file) {
@@ -73,7 +77,7 @@ bool isPluralVariation(String key, Map<String, dynamic> bundle) {
   return bundle.containsKey('${prefix}Other');
 }
 
-void updateMissingResources(String localizationPath, String groupPrefix) {
+void updateMissingResources(String localizationPath, String groupPrefix, {bool removeUndefined = false}) {
   final Directory localizationDir = Directory(localizationPath);
   final RegExp filenamePattern = RegExp('${groupPrefix}_(\\w+)\\.arb');
 
@@ -91,14 +95,52 @@ void updateMissingResources(String localizationPath, String groupPrefix) {
         final File arbFile = File(entityPath);
         final Map<String, dynamic> localeBundle = loadBundle(arbFile);
         final Set<String> localeResources = resourceKeys(localeBundle);
+        // Whether or not the resources were modified and need to be updated.
+        bool shouldWrite = false;
+
+        // Remove any localizations that are not defined in the canonical
+        // locale. This allows unused localizations to be removed if
+        // --remove-undefined is passed.
+        if (removeUndefined) {
+          bool isIncluded(String key) {
+            return !isPluralVariation(key, localeBundle)
+                && !intentionallyOmitted(key, localeBundle);
+          }
+
+          // Find any resources in this locale that don't appear in the
+          // canonical locale, and skipping any which should not be included
+          // (plurals and intentionally omitted).
+          final Set<String> extraResources = localeResources
+              .difference(requiredKeys)
+              .where(isIncluded)
+              .toSet();
+
+          // Remove them.
+          localeBundle.removeWhere((String key, dynamic value) {
+            final bool found = extraResources.contains(key);
+            if (found) {
+              shouldWrite = true;
+            }
+            return found;
+          });
+          if (shouldWrite) {
+            print('Updating $entityPath by removing extra entries for $extraResources');
+          }
+        }
+
+        // Add in any resources that are in the canonical locale and not present
+        // in this locale.
         final Set<String> missingResources = requiredKeys.difference(localeResources).where(
           (String key) => !isPluralVariation(key, localeBundle) && !intentionallyOmitted(key, localeBundle)
         ).toSet();
         if (missingResources.isNotEmpty) {
           localeBundle.addEntries(missingResources.map((String k) =>
             MapEntry<String, String>(k, englishBundle[k].toString())));
+          shouldWrite = true;
+          print('Updating $entityPath with missing entries for $missingResources');
+        }
+        if (shouldWrite) {
           writeBundle(arbFile, localeBundle);
-          print('Updated $entityPath with missing entries for $missingResources');
         }
       }
     }
