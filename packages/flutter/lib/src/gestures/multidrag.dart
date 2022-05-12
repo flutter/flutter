df0d0372b41514f0476c12eeb8e21494900b3ea2
newtaDev <newta.dev@gmail.com>
2022-05-12 23:55:37 +0530

    Added space before `{`

--- packages/flutter/lib/src/gestures/multidrag.dart
+++ packages/flutter/lib/src/gestures/multidrag.dart
@@ -509,7 +509,7 @@ class VerticalMultiDragGestureRecognizer extends MultiDragGestureRecognizer {
 
 class _DelayedPointerState extends MultiDragPointerState {
   _DelayedPointerState(super.initialPosition, Duration delay, super.kind, super. deviceGestureSettings)
-      : assert(delay != null){
+      : assert(delay != null) {
     _timer = Timer(delay, _delayPassed);
   }
 
