bcd8bce5a79e81d161243c0d6e19811bee154d12
ColdPaleLight <31977171+ColdPaleLight@users.noreply.github.com>
2022-05-12 16:39:04 +0800

    Refactor clip layers to share common code (flutter/engine#32889)

--- engine/src/flutter/flow/layers/clip_path_layer.cc
+++ engine/src/flutter/flow/layers/clip_path_layer.cc
@@ -3,86 +3,33 @@
 // found in the LICENSE file.
 
 #include "flutter/flow/layers/clip_path_layer.h"
-#include "flutter/flow/paint_utils.h"
 
 namespace flutter {
 
 ClipPathLayer::ClipPathLayer(const SkPath& clip_path, Clip clip_behavior)
-    : clip_path_(clip_path), clip_behavior_(clip_behavior) {
-  FML_DCHECK(clip_behavior != Clip::none);
-}
-
-void ClipPathLayer::Diff(DiffContext* context, const Layer* old_layer) {
-  DiffContext::AutoSubtreeRestore subtree(context);
-  auto* prev = static_cast<const ClipPathLayer*>(old_layer);
-  if (!context->IsSubtreeDirty()) {
-    FML_DCHECK(prev);
-    if (clip_behavior_ != prev->clip_behavior_ ||
-        clip_path_ != prev->clip_path_) {
-      context->MarkSubtreeDirty(context->GetOldLayerPaintRegion(old_layer));
-    }
-  }
-  if (context->PushCullRect(clip_path_.getBounds())) {
-    DiffChildren(context, prev);
-  }
-  context->SetLayerPaintRegion(this, context->CurrentSubtreeRegion());
-}
+    : ClipShapeLayer(clip_path, clip_behavior) {}
 
 void ClipPathLayer::Preroll(PrerollContext* context, const SkMatrix& matrix) {
   TRACE_EVENT0("flutter", "ClipPathLayer::Preroll");
-
-  SkRect previous_cull_rect = context->cull_rect;
-  SkRect clip_path_bounds = clip_path_.getBounds();
-  if (!context->cull_rect.intersect(clip_path_bounds)) {
-    context->cull_rect.setEmpty();
-  }
-  Layer::AutoPrerollSaveLayerState save =
-      Layer::AutoPrerollSaveLayerState::Create(context, UsesSaveLayer());
-  context->mutators_stack.PushClipPath(clip_path_);
-
-  // Collect inheritance information on our children in Preroll so that
-  // we can pass it along by default.
-  context->subtree_can_inherit_opacity = true;
-
-  SkRect child_paint_bounds = SkRect::MakeEmpty();
-  PrerollChildren(context, matrix, &child_paint_bounds);
-  if (child_paint_bounds.intersect(clip_path_bounds)) {
-    set_paint_bounds(child_paint_bounds);
-  }
-
-  // If we use a SaveLayer then we can accept opacity on behalf
-  // of our children and apply it in the saveLayer.
-  if (UsesSaveLayer()) {
-    context->subtree_can_inherit_opacity = true;
-  }
-
-  context->mutators_stack.Pop();
-  context->cull_rect = previous_cull_rect;
+  ClipShapeLayer::Preroll(context, matrix);
 }
 
 void ClipPathLayer::Paint(PaintContext& context) const {
   TRACE_EVENT0("flutter", "ClipPathLayer::Paint");
-  FML_DCHECK(needs_painting(context));
+  ClipShapeLayer::Paint(context);
+}
 
-  SkAutoCanvasRestore save(context.internal_nodes_canvas, true);
-  context.internal_nodes_canvas->clipPath(clip_path_,
-                                          clip_behavior_ != Clip::hardEdge);
+const SkRect& ClipPathLayer::clip_shape_bounds() const {
+  return clip_shape().getBounds();
+}
 
-  if (!UsesSaveLayer()) {
-    PaintChildren(context);
-    return;
-  }
+void ClipPathLayer::OnMutatorsStackPushClipShape(
+    MutatorsStack& mutators_stack) {
+  mutators_stack.PushClipPath(clip_shape());
+}
 
-  AutoCachePaint cache_paint(context);
-  TRACE_EVENT0("flutter", "Canvas::saveLayer");
-  context.internal_nodes_canvas->saveLayer(paint_bounds(), cache_paint.paint());
-
-  PaintChildren(context);
-
-  context.internal_nodes_canvas->restore();
-  if (context.checkerboard_offscreen_layers) {
-    DrawCheckerboard(context.internal_nodes_canvas, paint_bounds());
-  }
+void ClipPathLayer::OnCanvasClipShape(SkCanvas* canvas) const {
+  canvas->clipPath(clip_shape(), clip_behavior() != Clip::hardEdge);
 }
 
 }  // namespace flutter
--- engine/src/flutter/flow/layers/clip_path_layer.h
+++ engine/src/flutter/flow/layers/clip_path_layer.h
@@ -5,29 +5,27 @@
 #ifndef FLUTTER_FLOW_LAYERS_CLIP_PATH_LAYER_H_
 #define FLUTTER_FLOW_LAYERS_CLIP_PATH_LAYER_H_
 
-#include "flutter/flow/layers/container_layer.h"
+#include "flutter/flow/layers/clip_shape_layer.h"
 
 namespace flutter {
 
-class ClipPathLayer : public ContainerLayer {
+class ClipPathLayer : public ClipShapeLayer<SkPath> {
  public:
   explicit ClipPathLayer(const SkPath& clip_path,
                          Clip clip_behavior = Clip::antiAlias);
-
-  void Diff(DiffContext* context, const Layer* old_layer) override;
 
   void Preroll(PrerollContext* context, const SkMatrix& matrix) override;
 
   void Paint(PaintContext& context) const override;
 
-  bool UsesSaveLayer() const {
-    return clip_behavior_ == Clip::antiAliasWithSaveLayer;
-  }
+ protected:
+  const SkRect& clip_shape_bounds() const override;
+
+  void OnMutatorsStackPushClipShape(MutatorsStack& mutators_stack) override;
+
+  void OnCanvasClipShape(SkCanvas* canvas) const override;
 
  private:
-  SkPath clip_path_;
-  Clip clip_behavior_;
-
   FML_DISALLOW_COPY_AND_ASSIGN(ClipPathLayer);
 };
 
