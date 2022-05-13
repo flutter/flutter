88e82b6bb2550536c91f5153ffcd70bce6b3b852
Chris Bracken <chris@bracken.jp>
2022-05-13 12:04:35 -0700

    [macOS] Update template icons to Big Sur style
    Updates the AppIcon assets in the macOS app template to meet Apple's
    icon guidelines as of macOS Big Sur. Assets were moved to the
    [flutter_template_images][1] package, which includes them as of version
    4.1.0.
    
    Flutter packages were upgraded. `charcode` no longer occurs in the
    transitive closure of dependencies and thus was removed from the
    allowlist in dev/bots/allowlist.dart.
    
    Ref: https://developer.apple.com/design/human-interface-guidelines/macos/icons-and-images/app-icon/
    
    Issue: https://github.com/flutter/flutter/issues/103371
    
    [1]: https://github.com/flutter/packages/tree/main/packages/flutter_template_images

--- packages/flutter_tools/lib/src/commands/update_packages.dart
+++ packages/flutter_tools/lib/src/commands/update_packages.dart
@@ -30,7 +30,7 @@ const Map<String, String> kManuallyPinnedDependencies = <String, String>{
   // Add pinned packages here. Please leave a comment explaining why.
   'archive': '3.1.11', // Breaking changes in 3.2.0, see https://github.com/flutter/flutter/issues/98536
   'flutter_gallery_assets': '1.0.2', // Tests depend on the exact version.
-  'flutter_template_images': '4.0.0', // Must always exactly match flutter_tools template.
+  'flutter_template_images': '4.1.0', // Must always exactly match flutter_tools template.
   // "shelf" is pinned to avoid the performance regression from a reverted
   // feature from https://github.com/dart-lang/shelf/issues/189 . This can be
   // removed when a new major version of shelf is published.
