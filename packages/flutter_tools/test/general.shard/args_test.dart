011f234110b08b50f4947abbbb49f8df284c43f6
Aman Verma <verma1090aman@gmail.com>
2022-05-15 02:37:18 +0530

    add missing space around =>

--- packages/flutter_tools/test/general.shard/args_test.dart
+++ packages/flutter_tools/test/general.shard/args_test.dart
@@ -36,7 +36,7 @@ void main() {
     command.argParser.addFlag('key-false');
     // argResults will be null at this point, if attempt to read them is made,
     // exception `Null check operator used on a null value` would be thrown.
-    expect(()=>command.boolArg('key'), throwsA( const TypeMatcher<TypeError>()));
+    expect(() => command.boolArg('key'), throwsA( const TypeMatcher<TypeError>()));
 
     runner.addCommand(command);
     await runner.run(<String>['dummy', '--key']);
@@ -61,7 +61,7 @@ void main() {
     command.argParser.addOption('key');
     // argResults will be null at this point, if attempt to read them is made,
     // exception `Null check operator used on a null value` would be thrown
-    expect(()=>command.stringArg('key'), throwsA( const TypeMatcher<TypeError>()));
+    expect(() => command.stringArg('key'), throwsA( const TypeMatcher<TypeError>()));
 
     runner.addCommand(command);
     await runner.run(<String>['dummy', '--key=value']);
