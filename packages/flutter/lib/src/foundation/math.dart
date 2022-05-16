75403bc0023adf57a55675ac4afd5de8e386457a
Aaron Clarke <aaclarke@google.com>
2022-05-16 10:14:01 -0700

    trim whitespace

--- packages/flutter/lib/src/foundation/math.dart
+++ packages/flutter/lib/src/foundation/math.dart
@@ -6,7 +6,7 @@
 ///
 /// This is faster because it avoids polymorphism, boxing, and special cases for
 /// floating point numbers.
-/// 
+//
 // See also: //dev/benchmarks/microbenchmarks/lib/foundation/clamp.dart
 double clampDouble(double x, double min, double max) {
   assert(min <= max && !max.isNaN && !min.isNaN);
