1662a14bc8946ff6691f441cbcede6d2503aeba5
Dan Field <dnfield@google.com>
2022-05-17 11:32:11 -0700

    More missing clipBehavior respects (#103931)

--- packages/flutter/lib/src/rendering/object.dart
+++ packages/flutter/lib/src/rendering/object.dart
@@ -2788,6 +2788,11 @@ abstract class RenderObject extends AbstractNode with DiagnosticableTreeMixin im
   ///
   /// This is used in the semantics phase to avoid including children
   /// that are not physically visible.
+  ///
+  /// RenderObjects that respect a [Clip] behavior when painting _must_ respect
+  /// that same behavior when describing this value. For example, if passing
+  /// [Clip.none] to [PaintingContext.pushClipRect] as the `clipBehavior`, then
+  /// the implementation of this method must return null.
   Rect? describeApproximatePaintClip(covariant RenderObject child) => null;
 
   /// Returns a rect in this object's coordinate system that describes
