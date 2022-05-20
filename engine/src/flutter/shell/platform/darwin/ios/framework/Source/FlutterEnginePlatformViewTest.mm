a73d5b5fbe951552451a62000453c3e76fe82478
Dan Field <dnfield@google.com>
2022-05-19 23:58:05 -0700

    Reland Enable MSAA behind a flag for iOS (flutter/engine#33505)

--- engine/src/flutter/shell/platform/darwin/ios/framework/Source/FlutterEnginePlatformViewTest.mm
+++ engine/src/flutter/shell/platform/darwin/ios/framework/Source/FlutterEnginePlatformViewTest.mm
@@ -19,6 +19,7 @@ namespace flutter {
 namespace {
 
 class FakeDelegate : public PlatformView::Delegate {
+ public:
   void OnPlatformViewCreated(std::unique_ptr<Surface> surface) override {}
   void OnPlatformViewDestroyed() override {}
   void OnPlatformViewScheduleFrame() override {}
@@ -47,7 +48,6 @@ class FakeDelegate : public PlatformView::Delegate {
   void UpdateAssetResolverByType(std::unique_ptr<AssetResolver> updated_asset_resolver,
                                  AssetResolver::AssetResolverType type) override {}
 
- private:
   flutter::Settings settings_;
 };
 
@@ -85,6 +85,30 @@ flutter::FakeDelegate fake_delegate;
 
 - (fml::WeakPtr<flutter::PlatformView>)platformViewReplacement {
   return weak_factory->GetWeakPtr();
+}
+
+- (void)testMsaaSampleCount {
+  // Default should be 1.
+  XCTAssertEqual(platform_view->GetIosContext()->GetMsaaSampleCount(), MsaaSampleCount::kNone);
+
+  // Verify the platform view creates a new context with updated msaa_samples.
+  // Need to use Metal, since this is ignored for Software/GL.
+  fake_delegate.settings_.msaa_samples = 4;
+
+  auto thread_task_runner = fml::MessageLoop::GetCurrent().GetTaskRunner();
+  flutter::TaskRunners runners(/*label=*/self.name.UTF8String,
+                               /*platform=*/thread_task_runner,
+                               /*raster=*/thread_task_runner,
+                               /*ui=*/thread_task_runner,
+                               /*io=*/thread_task_runner);
+  auto msaa_4x_platform_view = std::make_unique<flutter::PlatformViewIOS>(
+      /*delegate=*/fake_delegate,
+      /*rendering_api=*/flutter::IOSRenderingAPI::kMetal,
+      /*platform_views_controller=*/nil,
+      /*task_runners=*/runners);
+
+  XCTAssertEqual(msaa_4x_platform_view->GetIosContext()->GetMsaaSampleCount(),
+                 MsaaSampleCount::kFour);
 }
 
 - (void)testCallsNotifyLowMemory {
