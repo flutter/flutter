30addcb956f31917cd55e8bfd9eb8416f1c8d16d
hellohuanlin <41930132+hellohuanlin@users.noreply.github.com>
2022-05-23 17:58:13 -0700

    Add Focus support for iOS platform view (#103019)

--- packages/flutter/test/services/fake_platform_views.dart
+++ packages/flutter/test/services/fake_platform_views.dart
@@ -340,6 +340,13 @@ class FakeIosPlatformViewsController {
     _registeredViewTypes.add(viewType);
   }
 
+  void invokeViewFocused(int viewId) {
+    final MethodCodec codec = SystemChannels.platform_views.codec;
+    final ByteData data = codec.encodeMethodCall(MethodCall('viewFocused', viewId));
+    ServicesBinding.instance.defaultBinaryMessenger
+        .handlePlatformMessage(SystemChannels.platform_views.name, data, (ByteData? data) {});
+  }
+
   Future<dynamic> _onMethodCall(MethodCall call) {
     switch(call.method) {
       case 'create':
