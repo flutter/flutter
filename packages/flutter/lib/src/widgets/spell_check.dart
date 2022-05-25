5cdbf36ae80f1bc9843e22ff44ead5181bdf9891
camsim99 <camillesimon90@gmail.com>
2022-05-25 13:12:12 -0700

    Fix imports

--- packages/flutter/lib/src/widgets/spell_check.dart
+++ packages/flutter/lib/src/widgets/spell_check.dart
@@ -2,12 +2,11 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:flutter/src/services/platform_channel.dart';
-import 'package:flutter/src/services/system_channels.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flutter/src/services/platform_channel.dart';
+import 'package:flutter/src/services/system_channels.dart';
 
 ////////////////////////////////////////////////////////////////////////////////
 ///                            START OF PR #1.1                              ///
