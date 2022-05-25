537f84bd7351517c0dcd93403683fe680c8e2e86
Gary Qian <garyq@google.com>
2022-05-25 15:19:19 -0700

    Cleanup

--- packages/flutter_tools/test/integration.shard/flutter_build_preview_sdk.dart
+++ packages/flutter_tools/test/integration.shard/flutter_build_preview_sdk.dart
@@ -88,10 +88,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId "com.example.buildtest"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://docs.flutter.dev/deployment/android#reviewing-the-build-configuration.
         minSdkVersion flutter.minSdkVersion
         targetSdkVersion flutter.targetSdkVersion
         versionCode flutterVersionCode.toInteger()
@@ -100,8 +97,6 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig signingConfigs.debug
         }
     }
