e71eb183d8b0651bf0ce5b4114e6aceb3c50d086
Pierre-Louis <6655696+guidezpl@users.noreply.github.com>
2022-05-25 19:57:57 +0200

    Use `curly_braces_in_flow_control_structures` for `rendering` (#104611)
    * Use `curly_braces_in_flow_control_structures` for `rendering`
    
    * fix comments
    
    * fix dangling

--- packages/flutter/test/rendering/editable_test.dart
+++ packages/flutter/test/rendering/editable_test.dart
@@ -899,8 +899,9 @@ void main() {
       editable.paintCount = 0;
 
       final AbstractNode? parent = editable.parent;
-      if (parent is RenderConstrainedBox)
+      if (parent is RenderConstrainedBox) {
         parent.child = null;
+      }
     });
 
     test('paints in the correct order', () {
