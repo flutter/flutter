1650a195245d4e7f16590056c3db5d132361684b
chunhtai <47866232+chunhtai@users.noreply.github.com>
2022-05-26 15:40:09 -0700

    SelectableRegion does not merge child semantics nodes (#104659)

--- packages/flutter/lib/src/widgets/selectable_region.dart
+++ packages/flutter/lib/src/widgets/selectable_region.dart
@@ -780,6 +780,7 @@ class _SelectableRegionState extends State<SelectableRegion> with TextSelectionD
         child: Actions(
           actions: _actions,
           child: Focus(
+            includeSemantics: false,
             focusNode: widget.focusNode,
             child: SelectionContainer(
               registrar: this,
