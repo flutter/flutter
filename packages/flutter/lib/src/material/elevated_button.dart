20896c6538aca42a1856abf5cdb8a7ed68e1b209
Hans Muller <hansmuller@chromium.org>
2022-05-28 08:36:06 -0700

    Revert "Added MaterialStatesController, updated InkWell et al. (#103167)"
    This reverts commit 180566f2e59928912c893cc0e8a12f97e653c206.

--- packages/flutter/lib/src/material/elevated_button.dart
+++ packages/flutter/lib/src/material/elevated_button.dart
@@ -71,7 +71,6 @@ class ElevatedButton extends ButtonStyleButton {
     super.focusNode,
     super.autofocus = false,
     super.clipBehavior = Clip.none,
-    super.statesController,
     required super.child,
   });
 
