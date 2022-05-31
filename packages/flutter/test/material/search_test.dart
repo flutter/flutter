38ed6835f275d9573b2bec6e0787ec6f28c6311e
Taha Tesser <tessertaha@gmail.com>
2022-05-31 15:35:35 +0300

    Update test

--- packages/flutter/test/material/search_test.dart
+++ packages/flutter/test/material/search_test.dart
@@ -918,7 +918,7 @@ void main() {
     await tester.tap(find.text('Paste'));
     await tester.pump();
     expect(textField.controller!.text.length, 15);
-  });
+  }, skip: kIsWeb);
 }
 
 class TestHomePage extends StatelessWidget {
