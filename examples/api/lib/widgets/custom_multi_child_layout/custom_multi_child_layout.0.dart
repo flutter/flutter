30a0096ba4cc04aa067b8400021e8ca842ed3d76
Youssef Attia <yattia@google.com>
2022-06-01 13:05:42 -0700

    Add an example for CustomMultiChildLayout widget

---
+++ examples/api/lib/widgets/custom_multi_child_layout/custom_multi_child_layout.0.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+
+// Flutter code sample for CustomMultiChildLayout
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  static const String _title = 'Flutter Code Sample';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: _title,
+      home: Scaffold(
+        appBar: AppBar(title: const Text(_title)),
+        body: const ExampleWidget(),
+      ),
+    );
+  }
+}
+
+class _SnakeLayoutDelegate extends MultiChildLayoutDelegate {
+  _SnakeLayoutDelegate(
+      {required this.ids, required this.numCols, required this.rowHeight});
+  // In our case we only care about ids since we just need something to refer to in the LayoutId widget in the build of the ExampleWidget
+  final List<String> ids;
+  final int numCols;
+  // Not sure how to access the greatest height of children to make a max height so here I hardcode the rowHeight value
+  final double rowHeight;
+  @override
+  void performLayout(Size size) {
+    // Distribute children between numCols
+    final double columnWidth = size.width / numCols;
+    Offset childPosition = Offset.zero;
+    // The layout direction changes depending on which row the children are being drawn on
+    int direction = 1;
+    // Position each child
+    ids.asMap().forEach((int idx, String id) {
+      final Size currentSize = layoutChild(
+          id, BoxConstraints(maxHeight: rowHeight, maxWidth: columnWidth));
+      positionChild(id, childPosition);
+      // Logic for checking if a new row has been reached and the appropriate behavior
+      if ((idx + 1) % numCols == 0) {
+        childPosition += Offset(0, currentSize.height);
+        direction *= -1;
+      } else {
+        childPosition += Offset(direction * currentSize.width, 0);
+      }
+    });
+  }
+
+  @override
+  bool shouldRelayout(covariant _SnakeLayoutDelegate oldDelegate) {
+    return oldDelegate.numCols != numCols || oldDelegate.rowHeight != rowHeight;
+  }
+}
+
+class ExampleWidget extends StatelessWidget {
+  const ExampleWidget({super.key});
+  static const Map<String, Color> _colors = <String, Color>{
+    'Red': Colors.red,
+    'Green': Colors.green,
+    'Blue': Colors.blue,
+    'Cyan': Colors.cyan,
+    'Purple': Colors.purple,
+    'Pink': Colors.pink,
+    'Yellow': Colors.yellow,
+  };
+  @override
+  Widget build(BuildContext context) {
+    return CustomMultiChildLayout(
+      delegate: _SnakeLayoutDelegate(
+        ids: _colors.keys.toList(),
+        numCols: 3,
+        rowHeight: 100.0,
+      ),
+      children: <Widget>[
+        // Create all of the colored boxes in the colors map.
+        for (MapEntry<String, Color> entry in _colors.entries)
+          LayoutId(
+            id: entry.key,
+            child: Container(
+              key: Key(entry.key),
+              color: entry.value,
+              width: 200.0,
+              height: 200.0,
+              alignment: Alignment.center,
+              child: Text(entry.key),
+            ),
+          ),
+      ],
+    );
+  }
+}
---
+++ examples/api/test/widgets/custom_multi_child_layout/custom_multi_child_layout.0_test.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_api_samples/widgets/custom_multi_child_layout/custom_multi_child_layout.0.dart'
+    as example;
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('has all items on screen', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      const example.MyApp(),
+    );
+
+    final Finder containerFinder = find.byType(Container);
+    expect(containerFinder, findsNWidgets(7));
+  });
+  testWidgets('has correct constraints when shrunk',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(const Center(
+        child: SizedBox(
+            key: Key('parent'),
+            width: 500,
+            height: 700,
+            child: example.MyApp())));
+
+    final Finder parent = find.byKey(const Key('parent'));
+    final Size parentSize = tester.getSize(parent);
+    final Size childSize = tester.getSize(find.byKey(const Key('Blue')));
+
+    expect(childSize.width.roundToDouble(),
+        (parentSize.width / 3).roundToDouble());
+    expect(childSize.height.roundToDouble(), 100.0);
+  });
+  testWidgets('has correct size maximum constraints',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(const Center(
+        child: SizedBox(
+            key: Key('parent'),
+            width: 800,
+            height: 700,
+            child: example.MyApp())));
+
+    final Size childSize = tester.getSize(find.byKey(const Key('Blue')));
+
+    expect(childSize.width.roundToDouble(), 200.0);
+  });
+  testWidgets('row behavior is correct',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(const Center(
+        child: SizedBox(
+            key: Key('parent'),
+            width: 800,
+            height: 700,
+            child: example.MyApp())));
+
+    final Finder child = find.byKey(const Key('Pink'));
+    final Size childSize = tester.getSize(find.byKey(const Key('Pink')));
+    final Offset childPosition = tester.getBottomRight(child);
+    expect(childPosition, Offset(childSize.width, childSize.height*2+56));
+    
+    final Finder child1 = find.byKey(const Key('Yellow'));
+    final Size childSize1 = tester.getSize(find.byKey(const Key('Yellow')));
+    final Offset childPosition1 = tester.getBottomRight(child1);
+    expect(childPosition1, Offset(childSize1.width, childSize1.height*3+56));
+    
+    final Finder child2 = find.byKey(const Key('Purple'));
+    final Size childSize2 = tester.getSize(find.byKey(const Key('Purple')));
+    final Offset childPosition2 = tester.getBottomRight(child2);
+    expect(childPosition2, Offset(childSize2.width*2, childSize2.height*2+56));
+  });
+}
