df4f473c9e1e9ffaf04c1a80d1448756abf28929
Youssef Attia <youssef.attia.ya@gmail.com>
2022-06-02 10:05:53 -0700

    License whitespace fix

--- examples/api/lib/widgets/custom_multi_child_layout/custom_multi_child_layout.0.dart
+++ examples/api/lib/widgets/custom_multi_child_layout/custom_multi_child_layout.0.dart
@@ -1,5 +1,5 @@
-// Copyright 2014 The Flutter Authors. All rights reserved.  Use of this source
-// code is governed by a BSD-style license that can be
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 // Flutter code sample for CustomMultiChildLayout. This example creates a
