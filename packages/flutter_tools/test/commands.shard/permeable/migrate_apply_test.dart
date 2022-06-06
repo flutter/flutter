8c3a60bcbd30e1bbc5a5a96e633629fcaf00f984
Gary Qian <garyq@google.com>
2022-06-06 14:55:49 -0700

    Analyzer

--- packages/flutter_tools/test/commands.shard/permeable/migrate_apply_test.dart
+++ packages/flutter_tools/test/commands.shard/permeable/migrate_apply_test.dart
@@ -10,8 +10,8 @@ import 'package:flutter_tools/src/base/platform.dart';
 import 'package:flutter_tools/src/base/terminal.dart';
 import 'package:flutter_tools/src/cache.dart';
 import 'package:flutter_tools/src/commands/migrate.dart';
-import 'package:flutter_tools/src/migrate/migrate_utils.dart';
 import 'package:flutter_tools/src/globals.dart' as globals;
+import 'package:flutter_tools/src/migrate/migrate_utils.dart';
 
 import '../../src/common.dart';
 import '../../src/context.dart';
