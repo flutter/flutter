63da40b28b293770d13ef6115710925474c094b1
Darren Austin <darrenaustin@google.com>
2022-06-07 16:12:28 -0700

    Updated tokens to v0.99. (#105545)

--- packages/flutter/lib/src/material/icon_button.dart
+++ packages/flutter/lib/src/material/icon_button.dart
@@ -726,7 +726,7 @@ class _IconButtonDefaultMouseCursor extends MaterialStateProperty<MouseCursor> w
 // These defaults are generated from the Material Design Token
 // database by the script dev/tools/gen_defaults/bin/gen_defaults.dart.
 
-// Generated version v0_98
+// Generated version v0_99
 class _TokenDefaultsM3 extends ButtonStyle {
   _TokenDefaultsM3(this.context)
     : super(
