984c86b591bc21c8892c50930a62aea2962bf999
Alexandre Ardhuin <alexandre.ardhuin@gmail.com>
2022-06-08 22:29:28 +0200

    exports public API types from foundation/debug.dart

--- packages/flutter/lib/src/cupertino/text_theme.dart
+++ packages/flutter/lib/src/cupertino/text_theme.dart
@@ -3,7 +3,6 @@
 // found in the LICENSE file.
 
 import 'package:flutter/foundation.dart';
-import 'package:flutter/services.dart' show Brightness;
 import 'package:flutter/widgets.dart';
 
 import 'colors.dart';
