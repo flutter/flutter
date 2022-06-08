6d6127deddc7385ac6830384bad0e9536dc8ca96
Alexandre Ardhuin <alexandre.ardhuin@gmail.com>
2022-06-08 22:40:38 +0200

    exports public API types from foundation/binding.dart

--- packages/flutter/lib/src/foundation/binding.dart
+++ packages/flutter/lib/src/foundation/binding.dart
@@ -18,6 +18,8 @@ import 'object.dart';
 import 'platform.dart';
 import 'print.dart';
 
+export 'basic_types.dart' show AsyncCallback, AsyncValueGetter, AsyncValueSetter;
+
 // Examples can assume:
 // mixin BarBinding on BindingBase { }
 
