14bd33cd5ea668f4d0b261aec4abc366bef177cb
Huan Lin <hellohuanlin@gmail.com>
2022-06-10 14:09:45 -0700

    update license

--- dev/integration_tests/ios_platform_view_tests/ios/PlatformViewUITests/PlatformViewUITestsLaunchTests.m
+++ dev/integration_tests/ios_platform_view_tests/ios/PlatformViewUITests/PlatformViewUITestsLaunchTests.m
@@ -1,9 +1,6 @@
-//
-//  PlatformViewUITestsLaunchTests.m
-//  PlatformViewUITests
-//
-//  Created by Huan Lin on 6/10/22.
-//
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
 #import <XCTest/XCTest.h>
 
--- dev/integration_tests/ios_platform_view_tests/ios/Runner/ViewFactory.h
+++ dev/integration_tests/ios_platform_view_tests/ios/Runner/ViewFactory.h
@@ -1,9 +1,6 @@
-//
-//  ViewFactory.h
-//  Runner
-//
-//  Created by Huan Lin on 6/10/22.
-//
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
 #import <Flutter/Flutter.h>
 #import <UIKit/UIKit.h>
