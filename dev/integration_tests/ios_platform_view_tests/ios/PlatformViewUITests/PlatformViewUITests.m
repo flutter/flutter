9a2df741f5b6ead0874e32046b4397c7b0a201b2
Huan Lin <hellohuanlin@gmail.com>
2022-06-10 14:07:34 -0700

    add xcuitest

---
+++ dev/integration_tests/ios_platform_view_tests/ios/PlatformViewUITests/PlatformViewUITests.m
@@ -0,0 +1,46 @@
+//
+//  PlatformViewUITests.m
+//  PlatformViewUITests
+//
+//  Created by Huan Lin on 6/10/22.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PlatformViewUITests : XCTestCase
+
+@end
+
+@implementation PlatformViewUITests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+
+    // In UI tests it is usually best to stop immediately when a failure occurs.
+    self.continueAfterFailure = NO;
+
+    // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // UI tests must launch the application that they test.
+    XCUIApplication *app = [[XCUIApplication alloc] init];
+    [app launch];
+
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testLaunchPerformance {
+    if (@available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *)) {
+        // This measures how long it takes to launch your application.
+        [self measureWithMetrics:@[[[XCTApplicationLaunchMetric alloc] init]] block:^{
+            [[[XCUIApplication alloc] init] launch];
+        }];
+    }
+}
+
+@end
---
+++ dev/integration_tests/ios_platform_view_tests/ios/PlatformViewUITests/PlatformViewUITestsLaunchTests.m
@@ -0,0 +1,37 @@
+//
+//  PlatformViewUITestsLaunchTests.m
+//  PlatformViewUITests
+//
+//  Created by Huan Lin on 6/10/22.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PlatformViewUITestsLaunchTests : XCTestCase
+
+@end
+
+@implementation PlatformViewUITestsLaunchTests
+
++ (BOOL)runsForEachTargetApplicationUIConfiguration {
+    return YES;
+}
+
+- (void)setUp {
+    self.continueAfterFailure = NO;
+}
+
+- (void)testLaunch {
+    XCUIApplication *app = [[XCUIApplication alloc] init];
+    [app launch];
+
+    // Insert steps here to perform after app launch but before taking a screenshot,
+    // such as logging into a test account or navigating somewhere in the app
+
+    XCTAttachment *attachment = [XCTAttachment attachmentWithScreenshot:XCUIScreen.mainScreen.screenshot];
+    attachment.name = @"Launch Screen";
+    attachment.lifetime = XCTAttachmentLifetimeKeepAlways;
+    [self addAttachment:attachment];
+}
+
+@end
