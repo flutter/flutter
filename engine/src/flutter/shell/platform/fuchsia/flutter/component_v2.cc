82929278575b35f1c386f315e5e287b3182e8a71
Alexander Markov <alexmarkov@google.com>
2022-06-10 10:12:56 -0700

    Remove uses of --lazy-async-stack Dart VM flag (flutter/engine#33964)

--- engine/src/flutter/shell/platform/fuchsia/flutter/component_v2.cc
+++ engine/src/flutter/shell/platform/fuchsia/flutter/component_v2.cc
@@ -488,7 +488,7 @@ ComponentV2::ComponentV2(
     std::cout << message << std::endl;
   };
 
-  settings_.dart_flags = {"--lazy_async_stacks"};
+  settings_.dart_flags = {};
 
   // Don't collect CPU samples from Dart VM C++ code.
   settings_.dart_flags.push_back("--no_profile_vm");
