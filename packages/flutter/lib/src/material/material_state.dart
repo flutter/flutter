a16d93f66cbad08b940aeee51b36eb0c53b6ecb0
Hans Muller <hans.muller@gmail.com>
2022-06-10 08:12:49 -0700

    Reland Added MaterialStatesController, updated InkWell et al. #103167 (#105656)

--- packages/flutter/lib/src/material/material_state.dart
+++ packages/flutter/lib/src/material/material_state.dart
@@ -716,3 +716,26 @@ class MaterialStatePropertyAll<T> implements MaterialStateProperty<T> {
   @override
   String toString() => 'MaterialStatePropertyAll($value)';
 }
+
+/// Manages a set of [MaterialState]s and notifies listeners of changes.
+///
+/// Used by widgets that expose their internal state for the sake of
+/// extensions that add support for additional states. See
+/// [TextButton.statesController] for example.
+///
+/// The controller's [value] is its current set of states. Listeners
+/// are notified whenever the [value] changes. The [value] should only be
+/// changed with [update]; it should not be modified directly.
+class MaterialStatesController extends ValueNotifier<Set<MaterialState>> {
+  /// Creates a MaterialStatesController.
+  MaterialStatesController([Set<MaterialState>? value]) : super(<MaterialState>{...?value});
+
+  /// Adds [state] to [value] if [add] is true, and removes it otherwise,
+  /// and notifies listeners if [value] has changed.
+  void update(MaterialState state, bool add) {
+    final bool valueChanged = add ? value.add(state) : value.remove(state);
+    if (valueChanged) {
+      notifyListeners();
+    }
+  }
+}
