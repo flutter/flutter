a0e8a5be6f054b0ce007f8f8d53e1f2e41b2c6f1
Sam Rawlins <srawlins@google.com>
2022-06-16 16:12:06 -0700

    Ignore body_might_complete_normally_catch_error violations (#105795)

--- packages/flutter_test/lib/src/test_default_binary_messenger.dart
+++ packages/flutter_test/lib/src/test_default_binary_messenger.dart
@@ -141,6 +141,9 @@ class TestDefaultBinaryMessenger extends BinaryMessenger {
     if (resultFuture != null) {
       _pendingMessages.add(resultFuture);
       resultFuture
+        // TODO(srawlins): Fix this static issue,
+        // https://github.com/flutter/flutter/issues/105750.
+        // ignore: body_might_complete_normally_catch_error
         .catchError((Object error) { /* errors are the responsibility of the caller */ })
         .whenComplete(() => _pendingMessages.remove(resultFuture));
     }
