73b6704a57cda53ce786652e14c3004eaff2f26d
Jonah Williams <jonahwilliams@google.com>
2022-06-16 21:23:08 -0700

    Update package:archive and pin test_api (#106157)

--- packages/flutter_tools/lib/src/commands/update_packages.dart
+++ packages/flutter_tools/lib/src/commands/update_packages.dart
@@ -28,13 +28,13 @@ import '../runner/flutter_command.dart';
 /// in ways that prevent them from every upgrading Flutter again!
 const Map<String, String> kManuallyPinnedDependencies = <String, String>{
   // Add pinned packages here. Please leave a comment explaining why.
-  'archive': '3.1.11', // Breaking changes in 3.2.0, see https://github.com/flutter/flutter/issues/98536
   'flutter_gallery_assets': '1.0.2', // Tests depend on the exact version.
   'flutter_template_images': '4.1.0', // Must always exactly match flutter_tools template.
   'video_player': '2.2.11',
   // Could potentially break color scheme tests on upgrade,
   // so pin and manually update as needed.
   'material_color_utilities': '0.1.5',
+  'test_api': '0.4.9',
 };
 
 class UpdatePackagesCommand extends FlutterCommand {
