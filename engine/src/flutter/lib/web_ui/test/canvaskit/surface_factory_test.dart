727c66750ec60d4fb0251ce069bcc45042ac4f55
Zachary Anderson <zanderso@users.noreply.github.com>
2022-06-22 11:52:00 -0700

    Revert "[canvaskit] Rework embedded view manager (#34081)" (flutter/engine#34232)
    This reverts commit 3da9e6d12fc83cdc2d1cfd8af9097c0b1d57df74.

--- engine/src/flutter/lib/web_ui/test/canvaskit/surface_factory_test.dart
+++ engine/src/flutter/lib/web_ui/test/canvaskit/surface_factory_test.dart
@@ -7,6 +7,7 @@ import 'package:test/test.dart';
 import 'package:ui/src/engine.dart';
 import 'package:ui/ui.dart' as ui;
 
+import '../matchers.dart';
 import 'common.dart';
 
 const MethodCodec codec = StandardMethodCodec();
@@ -20,27 +21,32 @@ void testMain() {
     setUpCanvasKitTest();
 
     test('cannot be created with size less than 1', () {
-      expect(SurfaceFactory(-1).maximumSurfaces, 1);
-      expect(SurfaceFactory(0).maximumSurfaces, 1);
-      expect(SurfaceFactory(1).maximumSurfaces, 1);
-      expect(SurfaceFactory(2).maximumSurfaces, 2);
+      expect(() => SurfaceFactory(-1), throwsAssertionError);
+      expect(() => SurfaceFactory(0), throwsAssertionError);
+      expect(SurfaceFactory(1), isNotNull);
+      expect(SurfaceFactory(2), isNotNull);
     });
 
-    test('getOverlay', () {
+    test('getSurface', () {
       final SurfaceFactory factory = SurfaceFactory(3);
       expect(factory.baseSurface, isNotNull);
-
-      expect(factory.debugSurfaceCount, equals(1));
-
-      // Get a surface from the factory, it should be unique.
-      final Surface? newSurface = factory.getOverlay();
-      expect(newSurface, isNot(equals(factory.baseSurface)));
+      expect(factory.backupSurface, isNotNull);
+      expect(factory.baseSurface, isNot(equals(factory.backupSurface)));
 
       expect(factory.debugSurfaceCount, equals(2));
 
-      // Get another surface from the factory. Now we are at maximum capacity.
-      final Surface? anotherSurface = factory.getOverlay();
+      // Get a surface from the factory, it should be unique.
+      final Surface newSurface = factory.getSurface();
+      expect(newSurface, isNot(equals(factory.baseSurface)));
+      expect(newSurface, isNot(equals(factory.backupSurface)));
+
+      expect(factory.debugSurfaceCount, equals(3));
+
+      // Get another surface from the factory. Now we are at maximum capacity,
+      // so it should return the backup surface.
+      final Surface anotherSurface = factory.getSurface();
       expect(anotherSurface, isNot(equals(factory.baseSurface)));
+      expect(anotherSurface, equals(factory.backupSurface));
 
       expect(factory.debugSurfaceCount, equals(3));
     });
@@ -49,12 +55,12 @@ void testMain() {
       final SurfaceFactory factory = SurfaceFactory(3);
 
       // Create a new surface and immediately release it.
-      final Surface? surface = factory.getOverlay();
-      factory.releaseSurface(surface!);
+      final Surface surface = factory.getSurface();
+      factory.releaseSurface(surface);
 
       // If we create a new surface, it should be the same as the one we
       // just created.
-      final Surface? newSurface = factory.getOverlay();
+      final Surface newSurface = factory.getSurface();
       expect(newSurface, equals(surface));
     });
 
@@ -62,9 +68,10 @@ void testMain() {
       final SurfaceFactory factory = SurfaceFactory(3);
 
       expect(factory.isLive(factory.baseSurface), isTrue);
+      expect(factory.isLive(factory.backupSurface), isTrue);
 
-      final Surface? surface = factory.getOverlay();
-      expect(factory.isLive(surface!), isTrue);
+      final Surface surface = factory.getSurface();
+      expect(factory.isLive(surface), isTrue);
 
       factory.releaseSurface(surface);
       expect(factory.isLive(surface), isFalse);
@@ -86,18 +93,23 @@ void testMain() {
       // Create a few overlay surfaces
       final List<Surface> overlays = <Surface>[];
       for (int i = 0; i < 3; i++) {
-        overlays.add(originalFactory.getOverlay()!
+        overlays.add(originalFactory.getSurface()
           ..acquireFrame(const ui.Size(10, 10))
           ..addToScene());
       }
-      expect(originalFactory.debugSurfaceCount, 4);
+      expect(originalFactory.debugSurfaceCount, 5);
+
+      originalFactory.backupSurface.acquireFrame(const ui.Size(10, 10));
+      originalFactory.backupSurface.addToScene();
+      expect(originalFactory.backupSurface.htmlCanvas!.isConnected, isTrue);
 
       // Trigger hot restart clean-up logic and check that we indeed clean up.
       debugEmulateHotRestart();
       expect(SurfaceFactory.debugUninitializedInstance, isNull);
       expectDisposed(originalFactory.baseSurface);
+      expectDisposed(originalFactory.backupSurface);
       overlays.forEach(expectDisposed);
-      expect(originalFactory.debugSurfaceCount, 1);
+      expect(originalFactory.debugSurfaceCount, 2);
     });
     // TODO(hterkelsen): https://github.com/flutter/flutter/issues/60040
   }, skip: isIosSafari);
