adb8487e3606f36d8894b4fe97d8034573e328c5
Alexandre Ardhuin <alexandre.ardhuin@gmail.com>
2022-06-22 15:54:18 +0200

    Export public API types from foundation/scheduler/gestures/semantics

--- packages/flutter/test/widgets/semantics_tester.dart
+++ packages/flutter/test/widgets/semantics_tester.dart
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'dart:ui' show SemanticsFlag;
+import 'dart:ui' show LocaleStringAttribute, SemanticsFlag, SpellOutStringAttribute;
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/physics.dart';
