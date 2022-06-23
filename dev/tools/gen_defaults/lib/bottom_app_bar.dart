5365ee9705a4fe6cfc7d3d99192d059c0fd07e69
Talat El Beick <talatelbeick@gmail.com>
2022-06-23 23:21:18 +0200

    add token template

---
+++ dev/tools/gen_defaults/lib/bottom_app_bar.dart
@@ -0,0 +1,30 @@
+import 'template.dart';
+
+class BottomAppBarTemplate extends TokenTemplate {
+  const BottomAppBarTemplate(super.fileName, super.tokens);
+
+
+  @override
+  String generate() => '''
+// Generated version ${tokens["version"]}
+class _TokenDefaultsM3 extends BottomAppBarTheme {
+  const _TokenDefaultsM3(this.context) 
+    : super(
+      elevation: ${elevation('md.comp.bottom-app-bar.container')},
+      height: ${tokens['md.comp.bottom-app-bar.container.height']},
+    );
+
+  final BuildContext context;
+
+  @override
+  Color? get color => ${componentColor('md.comp.bottom-app-bar.container')};
+  
+  @override
+  Color? get surfaceTintColor => ${componentColor('md.comp.bottom-app-bar.container.surface-tint-layer')};
+  
+  @override
+  NotchedShape? get shape => const AutomaticNotchedShape(${shape('md.comp.bottom-app-bar.container')});
+}
+  ''';
+}
+
