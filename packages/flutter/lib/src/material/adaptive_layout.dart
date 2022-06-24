2101893eb70fe70b0631616d159b705f8f1e3d3b
Youssef Attia <yattia@google.com>
2022-06-24 16:53:01 -0700

    update package dependency

--- packages/flutter/lib/src/material/adaptive_layout.dart
+++ packages/flutter/lib/src/material/adaptive_layout.dart
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import '../widgets/slot_layout.dart';
 
 /// A parent Widget takes in multiple [SlotLayout] components and places them
