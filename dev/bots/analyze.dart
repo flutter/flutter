568d05ead4df1f75cd8c8cc4e187a4ff8b0995ef
Christopher Fujino <christopherfujino@gmail.com>
2022-06-27 14:09:10 -0700

    [flutter_tools] refactor packages_autoroller.dart script (#106580)

--- dev/bots/analyze.dart
+++ dev/bots/analyze.dart
@@ -1778,7 +1778,7 @@ const Set<String> kExecutableAllowlist = <String>{
   'dev/bots/docs.sh',
 
   'dev/conductor/bin/conductor',
-  'dev/conductor/bin/roll-packages',
+  'dev/conductor/bin/packages_autoroller',
   'dev/conductor/core/lib/src/proto/compile_proto.sh',
 
   'dev/customer_testing/ci.sh',
