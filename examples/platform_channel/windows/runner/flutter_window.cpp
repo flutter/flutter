7c8911b94e2253e7291f34778652c3f2a9e2f8b0
Stuart Morgan <stuartmorgan@google.com>
2022-06-28 12:02:59 -0400

    First half: battery level

--- examples/platform_channel/windows/runner/flutter_window.cpp
+++ examples/platform_channel/windows/runner/flutter_window.cpp
@@ -1,8 +1,21 @@
 #include "flutter_window.h"
 
+#include <flutter/method_channel.h>
+#include <flutter/standard_method_codec.h>
+#include <windows.h>
+
+#include <memory>
 #include <optional>
 
 #include "flutter/generated_plugin_registrant.h"
+
+static int GetBatteryLevel() {
+  SYSTEM_POWER_STATUS status;
+  if (GetSystemPowerStatus(&status) == 0 || status.BatteryLifePercent == 255) {
+    return -1;
+  }
+  return status.BatteryLifePercent;
+}
 
 FlutterWindow::FlutterWindow(const flutter::DartProject& project)
     : project_(project) {}
@@ -25,6 +38,26 @@ bool FlutterWindow::OnCreate() {
     return false;
   }
   RegisterPlugins(flutter_controller_->engine());
+
+  flutter::MethodChannel<> channel(
+      flutter_controller_->engine()->messenger(), "samples.flutter.io/battery",
+      &flutter::StandardMethodCodec::GetInstance());
+  channel.SetMethodCallHandler(
+      [](const flutter::MethodCall<>& call,
+         std::unique_ptr<flutter::MethodResult<>> result) {
+        if (call.method_name() == "getBatteryLevel") {
+          int battery_level = GetBatteryLevel();
+
+          if (battery_level != -1) {
+            result->Success(battery_level);
+          } else {
+            result->Error("UNAVAILABLE", "Battery level not available.");
+          }
+        } else {
+          result->NotImplemented();
+        }
+      });
+
   SetChildContent(flutter_controller_->view()->GetNativeWindow());
   return true;
 }
