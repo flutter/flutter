3bf00e7a1ac5a55f5cb18b80e5d7ee209d54cee7
Márk Tolmács <mark@lazycat.hu>
2022-06-28 18:30:46 +0200

    Use git exec candidate list on all platforms for build (flutter/engine#34294)
    * Use git exec candidate list on all platforms for build
    
    build/git_revision.py now uses a git executable name search list on all
    platforms, avoiding issues with some Windows installations.
    
    * Moving python import to top to satisfy linter
    
    * Fixing format error and removing unneeded check
    
    * Adding git candidate search to githooks setup.py
    
    * Adding missing COM initialization to
    flutter_tester executable on Windows
    
    * Revert "Adding missing COM initialization to"
    
    Reason: Added to incorrect branch
    
    This reverts commit 1b0da56ffd9af41d457f5a609022d4b0387cd935.
    
    * Format fix for setup.py

--- engine/src/flutter/tools/githooks/setup.py
+++ engine/src/flutter/tools/githooks/setup.py
@@ -10,6 +10,7 @@ Sets up githooks.
 import os
 import subprocess
 import sys
+from shutil import which  # Natively supported since python 3.3
 
 SRC_ROOT = os.path.dirname(
     os.path.dirname(
@@ -19,17 +20,13 @@ SRC_ROOT = os.path.dirname(
 FLUTTER_DIR = os.path.join(SRC_ROOT, 'flutter')
 
 
-def IsWindows():
-  os_id = sys.platform
-  return os_id.startswith('win32') or os_id.startswith('cygwin')
-
-
 def Main(argv):
-  git = 'git'
   githooks = os.path.join(FLUTTER_DIR, 'tools', 'githooks')
-  if IsWindows():
-    git = 'git.bat'
-    githooks = os.path.join(githooks, 'windows')
+  git_candidates = ['git', 'git.sh', 'git.bat']
+  git = next(filter(which, git_candidates), None)
+  if git is None:
+    candidates = "', '".join(git_candidates)
+    raise IOError(f"Looks like GIT is not on the path. Tried '{candidates}'")
   result = subprocess.run([
       git,
       'config',
