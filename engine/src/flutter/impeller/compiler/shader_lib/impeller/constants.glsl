214c13830ac599957fd3ba87dc9695210acca0e4
Brandon DeRosier <bdero@google.com>
2022-06-30 11:00:42 -0700

    [Impeller] Add shader library; reduce branching in advanced blends (flutter/engine#34349)

---
+++ engine/src/flutter/impeller/compiler/shader_lib/impeller/constants.glsl
@@ -0,0 +1,10 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CONSTANTS_GLSL_
+#define CONSTANTS_GLSL_
+
+const float kEhCloseEnough = 0.000001;
+
+#endif
