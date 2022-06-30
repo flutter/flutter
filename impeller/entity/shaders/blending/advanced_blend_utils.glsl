1486cc56eff63ba921e00305923c87c794b624c5
Brandon DeRosier <bdero@google.com>
2022-06-30 11:00:42 -0700

    [Impeller] Add shader library; reduce branching in advanced blends (#34349)

--- impeller/entity/shaders/blending/advanced_blend_utils.glsl
+++ impeller/entity/shaders/blending/advanced_blend_utils.glsl
@@ -2,31 +2,15 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-const float kEhCloseEnough = 0.000001;
-
-vec3 ComponentIsValue(vec3 n, float value) {
-  vec3 diff = abs(n - value);
-  return vec3(diff.r < kEhCloseEnough,  //
-              diff.g < kEhCloseEnough,  //
-              diff.b < kEhCloseEnough);
-}
-
-vec3 MixComponents(vec3 a, vec3 b, vec3 weight, float cutoff) {
-  return vec3(mix(a.x, b.x, weight.x > cutoff),  //
-              mix(a.y, b.y, weight.y > cutoff),  //
-              mix(a.z, b.z, weight.z > cutoff));
-}
-
-vec3 MixHalf(vec3 a, vec3 b, vec3 weight) {
-  return MixComponents(a, b, weight, 0.5);
-}
+#include <impeller/constants.glsl>
+#include <impeller/branching.glsl>
 
 vec3 BlendScreen(vec3 dst, vec3 src) {
   return dst + src - (dst * src);
 }
 
 vec3 BlendHardLight(vec3 dst, vec3 src) {
-  return MixHalf(dst * (2 * src), BlendScreen(dst, 2 * src - 1), src);
+  return IPVec3Choose(dst * (2 * src), BlendScreen(dst, 2 * src - 1), src);
 }
 
 //------------------------------------------------------------------------------
