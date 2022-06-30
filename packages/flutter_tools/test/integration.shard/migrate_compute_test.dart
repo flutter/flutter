f9d9b388ee6daf9bad094d9e3f5683319dde3a95
Gary Qian <garyq@google.com>
2022-06-30 13:55:11 -0700

    Passing create test

--- packages/flutter_tools/test/integration.shard/migrate_compute_test.dart
+++ packages/flutter_tools/test/integration.shard/migrate_compute_test.dart
@@ -38,12 +38,9 @@ void main() {
       platform: globals.platform,
       processManager: globals.processManager,
     );
-    print('1');
     await MigrateProject.installProject('version:1.22.6_stable', currentDir);
-    print('2');
     final FlutterProjectFactory flutterFactory = FlutterProjectFactory(logger: logger, fileSystem: fileSystem);
     final FlutterProject flutterProject = flutterFactory.fromDirectory(currentDir);
-    print('3');
     context = MigrateContext(
       migrateResult: MigrateResult.empty(),
       flutterProject: flutterProject,
@@ -54,17 +51,15 @@ void main() {
       status: logger.startSpinner(),
       migrateUtils: utils,
     );
-    print('4');
     targetFlutterDirectory = fileSystem.directory('targetFlutterDir');
   });
 
   group('MigrateFlutterProject', () {
-    print('5');
     testUsingContext('MigrateBaseFlutterProject creates', () async {
-      final Directory workingDir = fileSystem.directory('migrate_working_dir');
-      final Directory baseDir = fileSystem.directory('base_dir');
+      final Directory workingDir = createResolvedTempDirectorySync('migrate_working_dir.');
+      final Directory baseDir = createResolvedTempDirectorySync('base_dir.');
+      context.migrateResult.generatedBaseTemplateDirectory = baseDir;
       workingDir.createSync(recursive: true);
-    print('6');
       MigrateBaseFlutterProject baseProject = MigrateBaseFlutterProject(
         path: null,
         directory: baseDir,
@@ -74,17 +69,20 @@ void main() {
         platformWhitelist: null,
       );
 
-    print('7');
-      baseProject.createProject(
+      await baseProject.createProject(
         context,
         <String>['5391447fae6209bb21a89e6a5a6583cac1af9b4b'], //revisionsList
-        <String, List<MigratePlatformConfig>>{'5391447fae6209bb21a89e6a5a6583cac1af9b4b': <MigratePlatformConfig>[MigratePlatformConfig(platform: SupportedPlatform.android)]}, //revisionToConfigs
+        <String, List<MigratePlatformConfig>>{
+          '5391447fae6209bb21a89e6a5a6583cac1af9b4b': <MigratePlatformConfig>[
+            MigratePlatformConfig(platform: SupportedPlatform.android)
+            MigratePlatformConfig(platform: SupportedPlatform.ios)
+          ],
+        }, //revisionToConfigs
         '5391447fae6209bb21a89e6a5a6583cac1af9b4b', //fallbackRevision
         '5391447fae6209bb21a89e6a5a6583cac1af9b4b', //targetRevision
         targetFlutterDirectory, //targetFlutterDirectory
       );
 
-    print('8');
       expect(baseDir.childFile('pubspec.yaml').existsSync(), true);
     });
 
