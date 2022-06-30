6c6ae063814c8bb3b655eba21411053b71d13b42
stuartmorgan <stuartmorgan@google.com>
2022-06-30 10:54:07 -0400

    Add Windows to the platform_channels example (#106754)

--- examples/platform_channel/windows/runner/main.cpp
+++ examples/platform_channel/windows/runner/main.cpp
@@ -10,7 +10,7 @@
 #include "utils.h"
 
 int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
-                      _In_ wchar_t *command_line, _In_ int show_command) {
+                      _In_ wchar_t* command_line, _In_ int show_command) {
   // Attach to console when present (e.g., 'flutter run') or create a
   // new console when running with a debugger.
   if (!::AttachConsole(ATTACH_PARENT_PROCESS) && ::IsDebuggerPresent()) {
@@ -23,15 +23,14 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
 
   flutter::DartProject project(L"data");
 
-  std::vector<std::string> command_line_arguments =
-      GetCommandLineArguments();
+  std::vector<std::string> command_line_arguments = GetCommandLineArguments();
 
   project.set_dart_entrypoint_arguments(std::move(command_line_arguments));
 
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
   Win32Window::Size size(1280, 720);
-  if (!window.CreateAndShow(L"flutter_gallery", origin, size)) {
+  if (!window.CreateAndShow(L"platform_channel", origin, size)) {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
