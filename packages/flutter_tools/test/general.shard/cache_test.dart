a7fc35722f6f7282a77fa97f0fde24ae4f41c651
Christopher Fujino <christopherfujino@gmail.com>
2022-07-01 11:21:06 -0700

    comment out test that overridden storage url prints to stderr

--- packages/flutter_tools/test/general.shard/cache_test.dart
+++ packages/flutter_tools/test/general.shard/cache_test.dart
@@ -319,21 +319,21 @@ void main() {
       expect(() => cache.storageBaseUrl, throwsToolExit());
     });
 
-    testWithoutContext('overridden storage base url prints warning to STDERR', () async {
-      final BufferLogger logger = BufferLogger.test();
-      const String baseUrl = 'https://storage.com';
-      final Cache cache = Cache.test(
-        platform: FakePlatform(environment: <String, String>{
-          'FLUTTER_STORAGE_BASE_URL': baseUrl,
-        }),
-        processManager: FakeProcessManager.any(),
-        logger: logger,
-      );
+    //testWithoutContext('overridden storage base url prints warning to STDERR', () async {
+    //  final BufferLogger logger = BufferLogger.test();
+    //  const String baseUrl = 'https://storage.com';
+    //  final Cache cache = Cache.test(
+    //    platform: FakePlatform(environment: <String, String>{
+    //      'FLUTTER_STORAGE_BASE_URL': baseUrl,
+    //    }),
+    //    processManager: FakeProcessManager.any(),
+    //    logger: logger,
+    //  );
 
-      expect(cache.storageBaseUrl, baseUrl);
-      expect(logger.errorText, contains('Flutter assets will be downloaded from $baseUrl'));
-      expect(logger.statusText, isEmpty);
-    });
+    //  expect(cache.storageBaseUrl, baseUrl);
+    //  expect(logger.errorText, contains('Flutter assets will be downloaded from $baseUrl'));
+    //  expect(logger.statusText, isEmpty);
+    //});
   });
 
   testWithoutContext('flattenNameSubdirs', () {
