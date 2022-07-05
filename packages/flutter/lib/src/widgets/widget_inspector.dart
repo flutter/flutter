fc9a3e3ec2c00bf008aac905076eaa19992fa566
Dan Chevalier <chevalier.dan@gmail.com>
2022-07-05 15:51:48 -0400

    remove unused import show

--- packages/flutter/lib/src/widgets/widget_inspector.dart
+++ packages/flutter/lib/src/widgets/widget_inspector.dart
@@ -3,7 +3,7 @@
 // found in the LICENSE file.
 
 import 'dart:async';
-import 'dart:collection' show HashMap, SplayTreeSet;
+import 'dart:collection' show HashMap;
 import 'dart:convert';
 import 'dart:developer' as developer;
 import 'dart:math' as math;
