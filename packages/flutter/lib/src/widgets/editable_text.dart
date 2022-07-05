ab642d5c525e674b890740f4299de78c2f1f66a7
Matej Knopp <matej.knopp@gmail.com>
2022-07-05 15:49:34 +0200

    Update comment

--- packages/flutter/lib/src/widgets/editable_text.dart
+++ packages/flutter/lib/src/widgets/editable_text.dart
@@ -4332,10 +4332,10 @@ class _UpdateTextSelectionAction<T extends DirectionalCaretMovementIntent> exten
 
     final _TextBoundary textBoundary = getTextBoundariesForIntent(intent);
 
-    // textBoundary selection is only updated after rebuild; If the text value is same,
-    // use selection from state which is more recent. This is necessary on macOS
-    // where alt+up sends moveBackward: and `moveToBeginningOfParagraph:` selectors at
-    // the same time.
+    // "textBoundary's selection is only updated after rebuild; if the text
+    // is the same, use the selection from state, which is more recent.
+    // This is necessary on macOS where alt+up sends the moveBackward:
+    // and moveToBeginningOfParagraph: selectors at the same time.
     final TextSelection textBoundarySelection =
         textBoundary.textEditingValue.text == state._value.text
             ? state._value.selection
