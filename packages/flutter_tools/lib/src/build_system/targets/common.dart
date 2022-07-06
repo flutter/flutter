f48a1b760a2430d43ef2b3f30b9e8c1384745f78
Alexander Aprelev <aam@google.com>
2022-07-06 13:20:59 -0700

    Ensure build is done in incremental mode.
    This allows for faster bootstrapping of the compiler from previously created dill file.
    
    Fixes https://github.com/flutter/flutter/issues/107183

--- packages/flutter_tools/lib/src/build_system/targets/common.dart
+++ packages/flutter_tools/lib/src/build_system/targets/common.dart
@@ -226,6 +226,7 @@ class KernelSnapshot extends Target {
       trackWidgetCreation: trackWidgetCreation && buildMode != BuildMode.release,
       targetModel: targetModel,
       outputFilePath: environment.buildDir.childFile('app.dill').path,
+      initializeFromDill: environment.buildDir.childFile('app.dill').path,
       packagesPath: packagesFile.path,
       linkPlatformKernelIn: forceLinkPlatform || buildMode.isPrecompiled,
       mainPath: targetFileAbsolute,
