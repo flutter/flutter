ed0834f65c6acffb23a1a7cbea80dd039da0d887
Youssef Attia <yattia@google.com>
2022-07-06 10:21:32 -0700

    formatting

--- packages/flutter/lib/src/material/adaptive_scaffold.dart
+++ packages/flutter/lib/src/material/adaptive_scaffold.dart
@@ -4,8 +4,6 @@ import 'slot_layout_config.dart';
 import 'navigation_bar.dart';
 import 'package:flutter/widgets.dart';
 
-
-
 class AdaptiveScaffold extends StatefulWidget {
   const AdaptiveScaffold({
     this.destinations,
@@ -15,7 +13,7 @@ class AdaptiveScaffold extends StatefulWidget {
     this.bodyRatio,
     this.breakpoints = const [0, 480, 1024],
     this.displayAnimations = true,
-    super.key
+    super.key,
   });
 
   final List<NavigationDestination>? destinations;
@@ -25,7 +23,6 @@ class AdaptiveScaffold extends StatefulWidget {
   final double? bodyRatio;
   final List<int> breakpoints;
   final bool displayAnimations;
-
 
   @override
   State<AdaptiveScaffold> createState() => _AdaptiveScaffoldState();
@@ -60,6 +57,7 @@ AnimatedWidget leftOutIn(child, animation) {
     child: child,
   );
 }
+
 AnimatedWidget leftInOut(child, animation) {
   return SlideTransition(
     position: Tween<Offset>(
@@ -82,7 +80,7 @@ AnimatedWidget rightOutIn(Widget child, animation) {
 
 FadeTransition fadeIn(Widget child, animation) {
   return FadeTransition(
-    opacity: CurvedAnimation(parent:animation, curve: Curves.easeInCubic),
+    opacity: CurvedAnimation(parent: animation, curve: Curves.easeInCubic),
     child: child,
   );
 }
@@ -90,46 +88,92 @@ FadeTransition fadeIn(Widget child, animation) {
 class _AdaptiveScaffoldState extends State<AdaptiveScaffold> {
   @override
   Widget build(BuildContext context) {
-    Widget Function(Widget, AnimationController?)? inAnimation0 = (widget.secondaryBody==null || widget.secondaryBody![0]==null && widget.displayAnimations)?fadeIn:null;
-    Widget Function(Widget, AnimationController?)? inAnimation1 = (widget.secondaryBody==null || widget.secondaryBody![1]==null && widget.displayAnimations)?fadeIn:null;
-    Widget Function(Widget, AnimationController?)? inAnimation2 = (widget.secondaryBody==null || widget.secondaryBody![2]==null && widget.displayAnimations)?fadeIn:null;
+    Widget Function(Widget, AnimationController?)? inAnimation0 = (widget.secondaryBody == null || widget.secondaryBody![0] == null && widget.displayAnimations) ? fadeIn : null;
+    Widget Function(Widget, AnimationController?)? inAnimation1 = (widget.secondaryBody == null || widget.secondaryBody![1] == null && widget.displayAnimations) ? fadeIn : null;
+    Widget Function(Widget, AnimationController?)? inAnimation2 = (widget.secondaryBody == null || widget.secondaryBody![2] == null && widget.displayAnimations) ? fadeIn : null;
 
     return Directionality(
       textDirection: TextDirection.ltr,
       child: AdaptiveLayout(
         bodyRatio: widget.bodyRatio,
         bodyAnimated: widget.displayAnimations,
-        primaryNavigation: widget.destinations!=null && widget.selectedIndex!=null ? SlotLayout(
-          config: {
-            widget.breakpoints[0]: SlotLayoutConfig(overtakeAnimation: widget.displayAnimations ? leftInOut:null, key: const Key('primaryNavigation0'), child: const SizedBox(width: 0, height: 0,)),
-            widget.breakpoints[1]: SlotLayoutConfig(inAnimation: widget.displayAnimations ? leftOutIn:null, key: const Key('primaryNavigation1'), child: SizedBox(width: 75, height:MediaQuery.of(context).size.height, child: NavigationRail(selectedIndex:widget.selectedIndex, destinations: widget.destinations!.map(_toRailDestination).toList(),))),
-            widget.breakpoints[2]: SlotLayoutConfig(inAnimation: widget.displayAnimations ? leftOutIn:null, key: const Key('primaryNavigation2'), child: SizedBox(width: 150, height:MediaQuery.of(context).size.height, child: NavigationRail(extended: true, selectedIndex:widget.selectedIndex, destinations: widget.destinations!.map(_toRailDestination).toList(),))),
-          },
-        ):null,
-        bottomNavigation: widget.destinations!=null && widget.selectedIndex!=null ? SlotLayout(
-          config: {
-            widget.breakpoints[0]: SlotLayoutConfig(inAnimation: widget.displayAnimations ? bottomToTop:null, key: const Key('botnav1'), child: BottomNavigationBar(
-                unselectedItemColor: Colors.grey,
-                selectedItemColor: Colors.black,
-                items: widget.destinations!.map(_toBottomNavItem).toList(),
-              )),
-            widget.breakpoints[1]: SlotLayoutConfig(overtakeAnimation: widget.displayAnimations ? topToBottom:null, key: const Key('botnavnone'), child: const SizedBox(width: 0, height: 0)),
-          },
-        ):null,
-        body: widget.body!=null ? SlotLayout(
-          config: {
-            if(widget.body![0]!=null) widget.breakpoints[0]: SlotLayoutConfig(inAnimation:inAnimation0, key: const Key('body0'), child: widget.body![0]!),
-            if(widget.body![1]!=null) widget.breakpoints[1]: SlotLayoutConfig(inAnimation:inAnimation1, key: const Key('body1'), child: widget.body![1]!),
-            if(widget.body![2]!=null) widget.breakpoints[2]: SlotLayoutConfig(inAnimation:inAnimation2, key: const Key('body2'), child: widget.body![2]!),
-          },
-        ):null,
-        secondaryBody: widget.secondaryBody!=null ? SlotLayout(
-          config: {
-            if(widget.secondaryBody![0]!=null) widget.breakpoints[0]: SlotLayoutConfig(key: const Key('sbody0'), child: widget.secondaryBody![0]!),
-            if(widget.secondaryBody![1]!=null) widget.breakpoints[1]: SlotLayoutConfig(key: const Key('sbody1'), child: widget.secondaryBody![1]!),
-            if(widget.secondaryBody![2]!=null) widget.breakpoints[2]: SlotLayoutConfig(key: const Key('sbody2'), child: widget.secondaryBody![2]!),
-          },
-        ):null,
+        primaryNavigation: widget.destinations != null && widget.selectedIndex != null
+            ? SlotLayout(
+                config: {
+                  widget.breakpoints[0]: SlotLayoutConfig(
+                    overtakeAnimation: widget.displayAnimations ? leftInOut : null,
+                    key: const Key('primaryNavigation0'),
+                    child: const SizedBox(
+                      width: 0,
+                      height: 0,
+                    ),
+                  ),
+                  widget.breakpoints[1]: SlotLayoutConfig(
+                    inAnimation: widget.displayAnimations ? leftOutIn : null,
+                    key: const Key('primaryNavigation1'),
+                    child: SizedBox(
+                      width: 75,
+                      height: MediaQuery.of(context).size.height,
+                      child: NavigationRail(
+                        selectedIndex: widget.selectedIndex,
+                        destinations: widget.destinations!.map(_toRailDestination).toList(),
+                      ),
+                    ),
+                  ),
+                  widget.breakpoints[2]: SlotLayoutConfig(
+                    inAnimation: widget.displayAnimations ? leftOutIn : null,
+                    key: const Key('primaryNavigation2'),
+                    child: SizedBox(
+                      width: 150,
+                      height: MediaQuery.of(context).size.height,
+                      child: NavigationRail(
+                        extended: true,
+                        selectedIndex: widget.selectedIndex,
+                        destinations: widget.destinations!.map(_toRailDestination).toList(),
+                      ),
+                    ),
+                  ),
+                },
+              )
+            : null,
+        bottomNavigation: widget.destinations != null && widget.selectedIndex != null
+            ? SlotLayout(
+                config: {
+                  widget.breakpoints[0]: SlotLayoutConfig(
+                    inAnimation: widget.displayAnimations ? bottomToTop : null,
+                    key: const Key('botnav1'),
+                    child: BottomNavigationBar(
+                      unselectedItemColor: Colors.grey,
+                      selectedItemColor: Colors.black,
+                      items: widget.destinations!.map(_toBottomNavItem).toList(),
+                    ),
+                  ),
+                  widget.breakpoints[1]: SlotLayoutConfig(
+                    overtakeAnimation: widget.displayAnimations ? topToBottom : null,
+                    key: const Key('botnavnone'),
+                    child: const SizedBox(width: 0, height: 0),
+                  ),
+                },
+              )
+            : null,
+        body: widget.body != null
+            ? SlotLayout(
+                config: {
+                  if (widget.body![0] != null) widget.breakpoints[0]: SlotLayoutConfig(inAnimation: inAnimation0, key: const Key('body0'), child: widget.body![0]!),
+                  if (widget.body![1] != null) widget.breakpoints[1]: SlotLayoutConfig(inAnimation: inAnimation1, key: const Key('body1'), child: widget.body![1]!),
+                  if (widget.body![2] != null) widget.breakpoints[2]: SlotLayoutConfig(inAnimation: inAnimation2, key: const Key('body2'), child: widget.body![2]!),
+                },
+              )
+            : null,
+        secondaryBody: widget.secondaryBody != null
+            ? SlotLayout(
+                config: {
+                  if (widget.secondaryBody![0] != null) widget.breakpoints[0]: SlotLayoutConfig(key: const Key('sbody0'), child: widget.secondaryBody![0]!),
+                  if (widget.secondaryBody![1] != null) widget.breakpoints[1]: SlotLayoutConfig(key: const Key('sbody1'), child: widget.secondaryBody![1]!),
+                  if (widget.secondaryBody![2] != null) widget.breakpoints[2]: SlotLayoutConfig(key: const Key('sbody2'), child: widget.secondaryBody![2]!),
+                },
+              )
+            : null,
       ),
     );
   }
