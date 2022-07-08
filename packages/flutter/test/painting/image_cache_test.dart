bd7543d040461af2cd4510d712e65c5c65e5d105
Dan Field <dnfield@google.com>
2022-07-07 21:17:21 -0700

    Remove listeners from pending images when clearing cache

--- packages/flutter/test/painting/image_cache_test.dart
+++ packages/flutter/test/painting/image_cache_test.dart
@@ -602,4 +602,27 @@ void main() {
     imageInfo.dispose();
     expect(testImage.debugGetOpenHandleStackTraces()!.length, 0);
   }, skip: kIsWeb); // https://github.com/flutter/flutter/issues/87442
+
+  test('clear does not leave pending images stuck', () async {
+    final ui.Image testImage = await createTestImage(width: 8, height: 8);
+
+    final TestImageStreamCompleter completer1 = TestImageStreamCompleter();
+
+    imageCache.putIfAbsent(testImage, () {
+      return completer1;
+    });
+
+    expect(imageCache.statusForKey(testImage).pending, true);
+    expect(imageCache.statusForKey(testImage).live, true);
+    expect(imageCache.statusForKey(testImage).keepAlive, false);
+
+    imageCache.clear();
+
+    // No one else is listening to the completer. It should not be considered
+    // live anymore.
+
+    expect(imageCache.statusForKey(testImage).pending, false);
+    expect(imageCache.statusForKey(testImage).live, false);
+    expect(imageCache.statusForKey(testImage).keepAlive, false);
+  });
 }
