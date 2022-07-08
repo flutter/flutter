25490eb3b561028855a7a20f71bc3820d76faf2f
Tong Mu <dkwingsmt@users.noreply.github.com>
2022-07-08 04:45:23 -0700

    Revert "Add new pointer signal for cancelling scroll inertia (#34402)" (flutter/engine#34535)
    This reverts commit 0a1ee488baaddc9429c75e4a463d73ac5a4e2330.

--- engine/src/flutter/shell/platform/embedder/embedder.cc
+++ engine/src/flutter/shell/platform/embedder/embedder.cc
@@ -1711,8 +1711,6 @@ inline flutter::PointerData::SignalKind ToPointerDataSignalKind(
       return flutter::PointerData::SignalKind::kNone;
     case kFlutterPointerSignalKindScroll:
       return flutter::PointerData::SignalKind::kScroll;
-    case kFlutterPointerSignalKindScrollInertiaCancel:
-      return flutter::PointerData::SignalKind::kScrollInertiaCancel;
   }
   return flutter::PointerData::SignalKind::kNone;
 }
