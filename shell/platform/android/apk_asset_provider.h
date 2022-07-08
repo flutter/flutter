e6b17e091ab88e18ac8cbac4f07fa66cc77a5741
ColdPaleLight <zhangzhijian.123@bytedance.com>
2022-07-08 09:36:04 +0800

    Fix issue about multi-engine loading assets throws exception after engines restart (#34473)

--- shell/platform/android/apk_asset_provider.h
+++ shell/platform/android/apk_asset_provider.h
@@ -14,17 +14,37 @@
 
 namespace flutter {
 
+class APKAssetProviderInternal {
+ public:
+  virtual std::unique_ptr<fml::Mapping> GetAsMapping(
+      const std::string& asset_name) const = 0;
+
+ protected:
+  virtual ~APKAssetProviderInternal() = default;
+};
+
 class APKAssetProvider final : public AssetResolver {
  public:
   explicit APKAssetProvider(JNIEnv* env,
                             jobject assetManager,
                             std::string directory);
-  ~APKAssetProvider() override;
+
+  explicit APKAssetProvider(std::shared_ptr<APKAssetProviderInternal> impl);
+
+  ~APKAssetProvider() = default;
+
+  // Returns a new 'std::unique_ptr<APKAssetProvider>' with the same 'impl_' as
+  // this provider.
+  std::unique_ptr<APKAssetProvider> Clone() const;
+
+  // Obtain a raw pointer to the APKAssetProviderInternal.
+  //
+  // This method is intended for use in tests. Callers must not
+  // delete the returned pointer.
+  APKAssetProviderInternal* GetImpl() const { return impl_.get(); }
 
  private:
-  fml::jni::ScopedJavaGlobalRef<jobject> java_asset_manager_;
-  AAssetManager* assetManager_;
-  const std::string directory_;
+  std::shared_ptr<APKAssetProviderInternal> impl_;
 
   // |flutter::AssetResolver|
   bool IsValid() const override;
