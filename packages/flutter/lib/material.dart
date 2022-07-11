c9e315d504d6bd17027bd2691e5b1a6c2d7a3f13
Justin McCandless <jmccandless@google.com>
2022-07-11 13:10:29 -0700

    Missed adding a new file to material.dart, fixes docs check I hope

--- packages/flutter/lib/material.dart
+++ packages/flutter/lib/material.dart
@@ -155,6 +155,7 @@ export 'src/material/text_form_field.dart';
 export 'src/material/text_selection.dart';
 export 'src/material/text_selection_theme.dart';
 export 'src/material/text_selection_toolbar.dart';
+export 'src/material/text_selection_toolbar_buttons_builder.dart';
 export 'src/material/text_selection_toolbar_text_button.dart';
 export 'src/material/text_theme.dart';
 export 'src/material/theme.dart';
