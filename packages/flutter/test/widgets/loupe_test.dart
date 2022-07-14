bbced5ed2b03c8338cb502d95a4ea8072ba27d30
Anthony Oleinik <oleina@google.com>
2022-07-14 11:49:13 -0700

    tests for raw_loupe

--- packages/flutter/test/widgets/loupe_test.dart
+++ packages/flutter/test/widgets/loupe_test.dart
@@ -1,7 +1,56 @@
+import 'package:fake_async/fake_async.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+
+class _MockAnimationController extends AnimationController {
+  _MockAnimationController()
+      : super(duration: const Duration(minutes: 1), vsync: const TestVSync());
+  int forwardCalls = 0;
+  int reverseCalls = 0;
+
+  @override
+  TickerFuture forward({double? from}) {
+    forwardCalls++;
+    return super.forward(from: from);
+  }
+
+  @override
+  TickerFuture reverse({double? from}) {
+    reverseCalls++;
+    return super.reverse(from: from);
+  }
+}
+
 void main() {
+  Future<T> runFakeAsync<T>(Future<T> Function(FakeAsync time) f) async {
+    return FakeAsync().run((FakeAsync time) async {
+      bool pump = true;
+      final Future<T> future = f(time).whenComplete(() => pump = false);
+      while (pump) {
+        time.flushMicrotasks();
+      }
+      return future;
+    });
+  }
+
+  Future<BuildContext> contextTrap(WidgetTester tester,
+      {Widget Function(Widget child)? wrapper}) async {
+    late BuildContext outerContext;
+
+    Widget identity(Widget child) {
+      return child;
+    }
+
+    await tester.pumpWidget(
+        (wrapper ?? identity)(Builder(builder: (BuildContext context) {
+      outerContext = context;
+      return Container();
+    })));
+
+    return outerContext;
+  }
+
   Widget wrapWithApp({required Widget child}) {
     return MaterialApp(
         home: Scaffold(
@@ -10,7 +59,8 @@ void main() {
   }
 
   group('Raw Loupe', () {
-    testWidgets('should respond to decoration', (WidgetTester tester) async {
+    testWidgets('should render with correct focal point and decoration',
+        (WidgetTester tester) async {
       final Key appKey = UniqueKey();
       const Size loupeSize = Size(100, 100);
       const Offset loupeFocalPoint = Offset(50, 50);
@@ -73,32 +123,240 @@ void main() {
     });
 
     group('transition states', () {
-      group('show', () {});
-      // SHOW + HIDE should auto respond to transition messages when no controller
-      // SHOW + HIDE should forward animation controller when controller
-    });
+      final AnimationController animationController = AnimationController(
+          vsync: const TestVSync(), duration: const Duration(minutes: 2));
+      final LoupeController loupeController = LoupeController();
 
-    // Should show what is in the focal point
-    // Should be able to magnify what is in the focal point
+      tearDown(() {
+        animationController.value = 0;
+        loupeController.hide();
+
+        if (loupeController.overlayEntry != null) {
+          loupeController.overlayEntry!.remove();
+          loupeController.overlayEntry = null;
+        }
+      });
+
+      testWidgets(
+          'should auto respond to transition messages when no animation controller',
+          (WidgetTester tester) async {
+        await runFakeAsync((FakeAsync async) async {
+          final RawLoupe testLoupe = RawLoupe(
+            controller: loupeController,
+            size: const Size(100, 100),
+          );
+
+          final BuildContext context = await contextTrap(tester,
+              wrapper: (Widget child) => wrapWithApp(child: child));
+
+          loupeController.show(
+            context: context,
+            builder: (BuildContext context) => testLoupe,
+          );
+
+          WidgetsBinding.instance.scheduleFrame();
+          await tester.pump();
+
+          expect(loupeController.status.value, AnimationStatus.completed);
+
+          loupeController.hide();
+          WidgetsBinding.instance.scheduleFrame();
+          await tester.pump();
+
+          expect(loupeController.status.value, AnimationStatus.dismissed);
+        });
+      });
+
+      testWidgets(
+          'should only signal complete when animation controller complete',
+          (WidgetTester tester) async {
+        await runFakeAsync((FakeAsync async) async {
+          final RawLoupe testLoupe = RawLoupe(
+            controller: loupeController,
+            transitionAnimationController: animationController,
+            size: const Size(100, 100),
+          );
+
+          final BuildContext context = await contextTrap(tester,
+              wrapper: (Widget child) => wrapWithApp(child: child));
+
+          loupeController.show(
+            context: context,
+            builder: (BuildContext context) => testLoupe,
+          );
+
+          WidgetsBinding.instance.scheduleFrame();
+          await tester.pump();
+
+          // No time has passed, so the animation controller has not completed.
+          expect(loupeController.status.value, AnimationStatus.forward);
+
+          async.elapse(animationController.duration!);
+          await tester.pumpAndSettle();
+
+          expect(loupeController.status.value, AnimationStatus.completed);
+
+          loupeController.hide();
+
+          WidgetsBinding.instance.scheduleFrame();
+          await tester.pump();
+
+          expect(loupeController.status.value, AnimationStatus.reverse);
+
+          async.elapse(animationController.duration!);
+          await tester.pumpAndSettle();
+
+          expect(loupeController.status.value, AnimationStatus.dismissed);
+        });
+      });
+    });
   });
 
   group('loupe controller', () {
+    final LoupeController loupeController = LoupeController();
+
+    tearDown(() {
+      loupeController.overlayEntry?.remove();
+      loupeController.status.value = AnimationStatus.dismissed;
+      loupeController.overlayEntry = null;
+    });
+
     group('show', () {
-      // should insert below when asked
-      // should insert into overlay
-      // if loupe is already shown, should hide without animating
+      testWidgets('should insert below below widget',
+          (WidgetTester tester) async {
+        final BuildContext context = await contextTrap(tester,
+            wrapper: (Widget child) => wrapWithApp(child: child));
+
+        final Widget fakeLoupe = Placeholder(key: UniqueKey());
+        final Widget fakeBefore = Placeholder(key: UniqueKey());
+
+        final OverlayEntry fakeBeforeOverlayEntry =
+            OverlayEntry(builder: (_) => fakeBefore);
+
+        Overlay.of(context)!.insert(fakeBeforeOverlayEntry);
+        loupeController.show(
+            context: context,
+            builder: (_) => fakeLoupe,
+            below: fakeBeforeOverlayEntry);
+
+        WidgetsBinding.instance.scheduleFrame();
+        await tester.pumpAndSettle();
+
+        final Iterable<Element> allOverlayChildren = find
+            .descendant(
+                of: find.byType(Overlay), matching: find.byType(Placeholder))
+            .evaluate();
+
+        // Expect the loupe to be the first child, even though it was inserted
+        // after the fakeBefore.
+        expect(allOverlayChildren.last.widget.key, fakeBefore.key);
+        expect(allOverlayChildren.first.widget.key, fakeLoupe.key);
+      });
+
+      testWidgets('should re-insert without animating if loupe already shown',
+          (WidgetTester tester) async {
+        await runFakeAsync((FakeAsync async) async {
+          final _MockAnimationController animationController =
+              _MockAnimationController();
+
+          final RawLoupe testLoupe = RawLoupe(
+            controller: loupeController,
+            transitionAnimationController: animationController,
+            size: const Size(100, 100),
+          );
+
+          final BuildContext context = await contextTrap(tester,
+              wrapper: (Widget child) => wrapWithApp(child: child));
+
+          loupeController.show(
+            context: context,
+            builder: (BuildContext context) => testLoupe,
+          );
+
+          WidgetsBinding.instance.scheduleFrame();
+          await tester.pump();
+
+          async.elapse(animationController.duration!);
+          await tester.pumpAndSettle();
+
+          loupeController.show(context: context, builder: (_) => testLoupe);
+
+          WidgetsBinding.instance.scheduleFrame();
+          await tester.pump();
+
+          expect(animationController.reverseCalls, 0,
+              reason:
+                  'should not have called reverse on animation controller due to force remove');
+        });
+      });
     });
 
     group('signalShow', () {
-      // should wait for animation to complete for future to complete
-      // should do nothing if loupe already shown
+      testWidgets('Should do nothing if loupe already shown',
+          (WidgetTester tester) async {
+        await runFakeAsync((FakeAsync async) async {
+          final _MockAnimationController animationController =
+              _MockAnimationController();
+
+          final RawLoupe testLoupe = RawLoupe(
+            controller: loupeController,
+            transitionAnimationController: animationController,
+            size: const Size(100, 100),
+          );
+
+          final BuildContext context = await contextTrap(tester,
+              wrapper: (Widget child) => wrapWithApp(child: child));
+
+          loupeController.show(
+            context: context,
+            builder: (BuildContext context) => testLoupe,
+          );
+
+          WidgetsBinding.instance.scheduleFrame();
+          await tester.pump();
+
+          async.elapse(animationController.duration!);
+          await tester.pumpAndSettle();
+
+          loupeController.signalShow();
+          WidgetsBinding.instance.scheduleFrame();
+          await tester.pump();
+
+          expect(animationController.forwardCalls, 1,
+              reason: 'should have only called forward for the inital show');
+        });
+      });
     });
 
-    group('hide', () {
-      // should wait for animation to complete for future to complete
-      // should do nothing if loupe already hidden
-    });
+    group('shift within bounds', () {
+      final List<Rect> boundsRects = <Rect>[
+        const Rect.fromLTRB(0, 0, 100, 100),
+        const Rect.fromLTRB(0, 0, 100, 100),
+        const Rect.fromLTRB(0, 0, 100, 100),
+        const Rect.fromLTRB(0, 0, 100, 100),
+      ];
+      final List<Rect> inputRects = <Rect>[
+        const Rect.fromLTRB(-100, -100, -80, -80),
+        const Rect.fromLTRB(0, 0, 20, 20),
+        const Rect.fromLTRB(110, 0, 120, 10),
+        const Rect.fromLTRB(110, 110, 120, 120)
+      ];
+      final List<Rect> outputRects = <Rect>[
+        const Rect.fromLTRB(0, 0, 20, 20),
+        const Rect.fromLTRB(0, 0, 20, 20),
+        const Rect.fromLTRB(90, 0, 100, 10),
+        const Rect.fromLTRB(90, 90, 100, 100)
+      ];
 
-    // SHIFT WITHIN BOUNDS JUST TEST A BUNCH
+      for (int i = 0; i < boundsRects.length; i++) {
+        test(
+            'should shift ${inputRects[i]} to ${outputRects[i]} for bounds ${boundsRects[i]}',
+            () {
+          final Rect outputRect = LoupeController.shiftWithinBounds(
+              bounds: boundsRects[i], rect: inputRects[i]);
+          expect(outputRect, outputRects[i]);
+        });
+      }
+    });
   });
 }
