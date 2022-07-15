76de50891716add4bb27c48dcf7fb177ca20968f
Anthony Oleinik <oleina@google.com>
2022-07-15 16:10:55 -0700

    added reduced-test-set to golden tests

--- packages/flutter/test/material/loupe_test.dart
+++ packages/flutter/test/material/loupe_test.dart
@@ -1,6 +1,7 @@
 // Copyright 2014 The Flutter Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+@Tags(<String>['reduced-test-set'])
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
