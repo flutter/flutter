24cddf9ba8673d39448a09e395896b39408b1d99
zhangzhijian.123 <zhangzhijian.123@bytedance.com>
2022-07-20 16:45:45 +0800

    Clean code

--- dev/benchmarks/macrobenchmarks/lib/src/color_filter_with_unstable_child.dart
+++ dev/benchmarks/macrobenchmarks/lib/src/color_filter_with_unstable_child.dart
@@ -18,15 +18,8 @@ class _ColorFilterWithUnstableChildPageState extends State<ColorFilterWithUnstab
   @override
   void initState() {
     super.initState();
-
     _controller = AnimationController(vsync: this, duration: const Duration(seconds: 2));
-    // Animations are typically implemented using the AnimatedBuilder widget.
-    // This code uses a manual listener for historical reasons and will remain
-    // in order to preserve compatibility with the history of measurements for
-    // this benchmark.
-    _offsetY = Tween<double>(begin: 0, end: -1000.0).animate(_controller)..addListener(() {
-      setState(() {});
-    });
+    _offsetY = Tween<double>(begin: 0, end: -1000.0).animate(_controller);
     _controller.repeat();
   }
 
@@ -38,19 +31,24 @@ class _ColorFilterWithUnstableChildPageState extends State<ColorFilterWithUnstab
 
   @override
   Widget build(BuildContext context) {
-    return Stack(children: List<Widget>.generate(50, (int i) => Positioned(
-      left: 0,
-      top: (200 * i).toDouble() + _offsetY.value,
-      child: ColorFiltered(
-        colorFilter: ColorFilter.mode(Colors.green[300]!, BlendMode.luminosity),
-        child: RepaintBoundary(
-          child: Container(
-            // Slightly change width to invalidate raster cache.
-            width: 1000 - (_offsetY.value / 100),
-            height: 100, color: Colors.red,
-          ),
-        ),
-      ),
-    )));
+    return AnimatedBuilder(
+      animation: _offsetY,
+      builder: (BuildContext context, Widget? child) {
+        return Stack(children: List<Widget>.generate(50, (int i) => Positioned(
+          left: 0,
+          top: (200 * i).toDouble() + _offsetY.value,
+            child: ColorFiltered(
+              colorFilter: ColorFilter.mode(Colors.green[300]!, BlendMode.luminosity),
+              child: RepaintBoundary(
+                child: Container(
+                  // Slightly change width to invalidate raster cache.
+                  width: 1000 - (_offsetY.value / 100),
+                  height: 100, color: Colors.red,
+                ),
+              ),
+            ),
+        )));
+      }
+    );
   }
 }
