e414922ad42cad41dea36e780e33e4ebf043ad1c
Jesús S Guerrero <jesus_sguerrero@hotmail.com>
2022-07-20 10:27:59 -0700

    print stdout

--- dev/bots/analyze.dart
+++ dev/bots/analyze.dart
@@ -1674,6 +1674,7 @@ Future<void> _checkConsumerDependencies() async {
       exit(result.exitCode);
     }
     print('Package name: $package');
+    print(result.stdout.toString());
     final Map<String, Object?> rawJson = json.decode(result.stdout as String) as Map<String, Object?>;
     final Map<String, Map<String, Object?>> dependencyTree = <String, Map<String, Object?>>{
       for (final Map<String, Object?> package in (rawJson['packages']! as List<Object?>).cast<Map<String, Object?>>())
