700ccf2fd06aeb57d71a1b3504df76161dca0c00
Justin McCandless <jmccandless@google.com>
2022-07-21 17:10:21 -0700

    Organize context_menu.dart into two separate files

---
+++ packages/flutter/lib/src/widgets/context_menu_button_data.dart
@@ -0,0 +1,74 @@
+import 'framework.dart';
+
+/// The buttons that can appear in a context menu by default.
+enum ContextMenuButtonType {
+  /// A button that cuts the current text selection.
+  cut,
+
+  /// A button that copies the current text selection.
+  copy,
+
+  /// A button that pastes the clipboard contents into the focused text field.
+  paste,
+
+  /// A button that selects all the contents of the focused text field.
+  selectAll,
+
+  /// Anything other than the default button types.
+  custom,
+}
+
+/// The type and callback for a context menu button.
+@immutable
+class ContextMenuButtonData {
+  /// Creates an instance of [ContextMenuButtonData].
+  const ContextMenuButtonData({
+    required this.onPressed,
+    this.type = ContextMenuButtonType.custom,
+    this.label,
+  });
+
+  /// The callback to be called when the button is pressed.
+  final VoidCallback onPressed;
+
+  /// The type of button this represents.
+  final ContextMenuButtonType type;
+
+  /// The label to display on the button.
+  ///
+  /// If a [type] other than [ContextMenuButtonType.custom] is given
+  /// and a label is not provided, then the default label for that type for the
+  /// platform will be looked up.
+  final String? label;
+
+  /// Creates a new [ContextMenuButtonData] with the provided parameters
+  /// overridden.
+  ContextMenuButtonData copyWith({
+    VoidCallback? onPressed,
+    ContextMenuButtonType? type,
+    String? label,
+  }) {
+    return ContextMenuButtonData(
+      onPressed: onPressed ?? this.onPressed,
+      type: type ?? this.type,
+      label: label ?? this.label,
+    );
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (other.runtimeType != runtimeType) {
+      return false;
+    }
+    return other is ContextMenuButtonData
+        && other.label == label
+        && other.onPressed == onPressed
+        && other.type == type;
+  }
+
+  @override
+  int get hashCode => Object.hash(label, onPressed, type);
+
+  @override
+  String toString() => 'ContextMenuButtonData $type, $label';
+}
