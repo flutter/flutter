549f70c9146c5286f4aeca163724b4fbb402eec0
Rich Kadel <richkadel@google.com>
2022-07-22 16:42:48 -0700

    Remove outdated Fuchsia concepts (#107335)
    Fuchsia will soon remove all support for Component Framework version 1
    components (recognized by component manifests ending in `.cmx`).
    Notably, some of the `flutter` tool commands for Fuchsia devices--
    notably, but not limited to, those related to CFv1--are outdated, and
    either do not work today or soon won't work.
    
    This PR removes the outdated components and commands, replacing some
    with the newer version, or simply removing the non-working features,
    in some cases.

--- packages/flutter_tools/lib/src/commands/build.dart
+++ packages/flutter_tools/lib/src/commands/build.dart
@@ -14,7 +14,6 @@ import 'build_aar.dart';
 import 'build_apk.dart';
 import 'build_appbundle.dart';
 import 'build_bundle.dart';
-import 'build_fuchsia.dart';
 import 'build_ios.dart';
 import 'build_ios_framework.dart';
 import 'build_macos_framework.dart';
@@ -43,7 +42,6 @@ class BuildCommand extends FlutterCommand {
       verboseHelp: verboseHelp
     ));
     _addSubcommand(BuildWindowsCommand(verboseHelp: verboseHelp));
-    _addSubcommand(BuildFuchsiaCommand(verboseHelp: verboseHelp));
   }
 
   void _addSubcommand(BuildSubCommand command) {
@@ -83,8 +81,11 @@ abstract class BuildSubCommand extends FlutterCommand {
   @protected
   void displayNullSafetyMode(BuildInfo buildInfo) {
     globals.printStatus('');
-    if (buildInfo.nullSafetyMode ==  NullSafetyMode.sound) {
-      globals.printStatus('💪 Building with sound null safety 💪', emphasis: true);
+    if (buildInfo.nullSafetyMode == NullSafetyMode.sound) {
+      globals.printStatus(
+        '💪 Building with sound null safety 💪',
+        emphasis: true,
+      );
     } else {
       globals.printStatus(
         'Building without sound null safety',
