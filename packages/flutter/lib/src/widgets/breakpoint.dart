3ee8eab61ff44978e2449eeeeac2ff23e5223f1e
Youssef Attia <yattia@google.com>
2022-07-22 11:27:37 -0700

    More breakpoint documentation

--- packages/flutter/lib/src/widgets/breakpoint.dart
+++ packages/flutter/lib/src/widgets/breakpoint.dart
@@ -1,6 +1,10 @@
 import 'framework.dart';
 
+/// Breakpoint used in [SlotLayout] and [AdaptiveScaffold].
 abstract class Breakpoint {
+  /// Returns a [Breakpoint].
   const Breakpoint();
+  /// Whether the breakpoint is active under some conditions related to the
+  /// context of the screen.
   bool isActive(BuildContext context);
 }
