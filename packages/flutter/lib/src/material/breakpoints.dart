ff4d6e5fca4d227b8d25d5e059a90805f9f25239
Youssef Attia <yattia@google.com>
2022-07-22 13:17:55 -0700

    fixed breakpoints being needlessly verbose

--- packages/flutter/lib/src/material/breakpoints.dart
+++ packages/flutter/lib/src/material/breakpoints.dart
@@ -1,110 +1,51 @@
 import 'package:flutter/widgets.dart';
 import 'theme.dart';
 
-// ignore: avoid_classes_with_only_static_members
+const List<TargetPlatform> _desktop = <TargetPlatform>[TargetPlatform.fuchsia, TargetPlatform.linux, TargetPlatform.macOS, TargetPlatform.windows];
+const List<TargetPlatform> _mobile = <TargetPlatform>[TargetPlatform.iOS, TargetPlatform.android];
+
 /// Standard Breakpoints.
 class Breakpoints {
   /// Small breakpoint. Open bounded.
-  static const Breakpoint small =  _SmallBreakpoint();
+  static const Breakpoint small =  _Breakpoint(begin: 0);
   /// Small breakpoint for non-mobile. Open bounded.
-  static const Breakpoint smallDesktop =  _SmallDesktopBreakpoint();
+  static const Breakpoint smallDesktop =  _Breakpoint(begin: 0, platform: _desktop);
+  /// Small breakpoint for non-mobile. Close bounded.
+  static const Breakpoint onlySmallDesktop =  _Breakpoint(begin: 0, end: 800, platform: _desktop);
   /// Small breakpoint for mobile. Open bounded.
-  static const Breakpoint smallMobile =  _SmallMobileBreakpoint();
+  static const Breakpoint smallMobile =  _Breakpoint(begin: 0, platform: _mobile);
   /// Medium breakpoint. Close bounded.
-  static const Breakpoint medium = _MediumBreakpoint();
+  static const Breakpoint medium = _Breakpoint(begin: 800, end: 1000);
   /// Medium breakpoint for non-mobile. Close bounded.
-  static const Breakpoint mediumDesktop = _MediumDesktopBreakpoint();
+  static const Breakpoint mediumDesktop = _Breakpoint(begin: 800, end: 1000, platform: _desktop);
   /// Medium breakpoint for mobile. Close bounded.
-  static const Breakpoint mediumMobile = _MediumMobileBreakpoint();
+  static const Breakpoint mediumMobile = _Breakpoint(begin: 800, end: 1000, platform: _mobile);
   /// Large breakpoint. Open bounded.
-  static const Breakpoint large = _LargeBreakpoint();
+  static const Breakpoint large = _Breakpoint(begin: 1000);
   /// Large breakpoint for non-mobile. Open bounded.
-  static const Breakpoint largeDesktop = _LargeDesktopBreakpoint();
+  static const Breakpoint largeDesktop = _Breakpoint(begin: 1000, platform: _desktop);
   /// Large breakpoint for mobile. Open bounded.
-  static const Breakpoint largeMobile = _LargeMobileBreakpoint();
-  /// Always-on breakpoint. Can act as fallback.
-  static const Breakpoint standard = _StandardBreakpoint();
+  static const Breakpoint largeMobile = _Breakpoint(begin: 1000, platform: _mobile);
 }
 
 
-class _StandardBreakpoint extends Breakpoint {
-  const _StandardBreakpoint();
+class _Breakpoint extends Breakpoint {
+  const _Breakpoint({this.begin, this.end, this.platform});
+  final double? begin;
+  final double? end;
+  final List<TargetPlatform>? platform;
 
-  @override bool isActive(BuildContext context) {
-    return true;
-  }
-}
-class _SmallBreakpoint extends Breakpoint {
-  const _SmallBreakpoint();
-
-  @override bool isActive(BuildContext context) {
-    return MediaQuery.of(context).size.width > 0;
-  }
-}
-class _SmallDesktopBreakpoint extends Breakpoint {
-  const _SmallDesktopBreakpoint();
-
-  @override bool isActive(BuildContext context) {
-    return Theme.of(context).platform != TargetPlatform.iOS &&
-        Theme.of(context).platform != TargetPlatform.android &&
-        MediaQuery.of(context).size.width > 0;
-  }
-}
-class _SmallMobileBreakpoint extends Breakpoint {
-  const _SmallMobileBreakpoint();
-
-  @override bool isActive(BuildContext context) {
-    return (Theme.of(context).platform == TargetPlatform.iOS || Theme.of(context).platform == TargetPlatform.android) &&
-        MediaQuery.of(context).size.width > 0;
-  }
-}
-class _MediumBreakpoint extends Breakpoint {
-  const _MediumBreakpoint();
-
-  @override bool isActive(BuildContext context) {
-    return MediaQuery.of(context).size.width > 800 && MediaQuery.of(context).size.width < 1000;
-  }
-}
-class _MediumDesktopBreakpoint extends Breakpoint {
-  const _MediumDesktopBreakpoint();
-
-  @override bool isActive(BuildContext context) {
-    return Theme.of(context).platform != TargetPlatform.iOS &&
-        Theme.of(context).platform != TargetPlatform.android &&
-        MediaQuery.of(context).size.width > 800 &&
-        MediaQuery.of(context).size.width < 1000;
-  }
-}
-class _MediumMobileBreakpoint extends Breakpoint {
-  const _MediumMobileBreakpoint();
-
-  @override bool isActive(BuildContext context) {
-    return (Theme.of(context).platform == TargetPlatform.iOS || Theme.of(context).platform == TargetPlatform.android) &&
-        MediaQuery.of(context).size.width > 800 &&
-        MediaQuery.of(context).size.width < 1000;
-  }
-}
-class _LargeBreakpoint extends Breakpoint {
-  const _LargeBreakpoint();
-
-  @override bool isActive(BuildContext context) {
-    return MediaQuery.of(context).size.width > 1000;
-  }
-}
-class _LargeDesktopBreakpoint extends Breakpoint {
-  const _LargeDesktopBreakpoint();
-
-  @override bool isActive(BuildContext context) {
-    return Theme.of(context).platform != TargetPlatform.iOS &&
-        Theme.of(context).platform != TargetPlatform.android &&
-        MediaQuery.of(context).size.width > 1000;
-  }
-}
-class _LargeMobileBreakpoint extends Breakpoint {
-  const _LargeMobileBreakpoint();
-
-  @override bool isActive(BuildContext context) {
-    return (Theme.of(context).platform != TargetPlatform.iOS || Theme.of(context).platform != TargetPlatform.android) &&
-        MediaQuery.of(context).size.width > 1000;
+  @override
+  bool isActive(BuildContext context) {
+    bool size = false;
+    final bool isRightPlatform = platform?.contains(Theme.of(context).platform) ?? true;
+    if (begin != null && end != null) {
+      size = MediaQuery.of(context).size.width >= begin! && MediaQuery.of(context).size.width < end!;
+    } else if (begin != null && end == null) {
+      size = MediaQuery.of(context).size.width >= begin!;
+    } else if (begin == null && end != null) {
+      size = MediaQuery.of(context).size.width < end!;
+    }
+    return size && isRightPlatform;
   }
 }
