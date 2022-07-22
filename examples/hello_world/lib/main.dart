5748c9a4f2a5f310daa95fdcda2f5241b400fc21
Jonah Williams <jonahwilliams@google.com>
2022-07-22 15:35:23 -0700

    ++

--- examples/hello_world/lib/main.dart
+++ examples/hello_world/lib/main.dart
@@ -2,31 +2,15 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'dart:ui' as ui;
 import 'package:flutter/widgets.dart';
 
 void main() =>
   runApp(
-    Center(
+    const Center(
       child:
-        CustomPaint(
-          painter: Foo(),
-          size: Size(100, 100),
-        )
+        Text('Hello, world!',
+          key: Key('title'),
+          textDirection: TextDirection.ltr,
+        ),
       ),
     );
-
-
-class Foo extends CustomPainter {
-  static final ui.FragmentProgram program = ui.FragmentProgram.fromAsset('shaders/example.frag');
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    canvas.drawRect(Offset.zero & size, Paint()..shader = program.shader());
-  }
-
-  @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) {
-    return oldDelegate != this;
-  }
-}
--- examples/hello_world/shaders/example.frag
+++
@@ -1,7 +0,0 @@
-#version 320 es
-
-layout(location = 0) out vec4 fragColor;
-
-void main() {
-    fragColor = vec4(1.0, 0.0, 0.0, 0.0);
-}
