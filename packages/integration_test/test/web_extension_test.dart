965d221d8ad3d8c01d75eca94f87aa5fe7f1c697
Yegor <yjbanov@google.com>
2022-07-22 17:48:06 -0700

    [web] define $flutterDriverResult variable early in driver test initalization (#107337)

---
+++ packages/integration_test/test/web_extension_test.dart
@@ -0,0 +1,21 @@
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+@Tags(<String>['web'])
+import 'dart:js' as js;
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding();
+
+  test('IntegrationTestWidgetsFlutterBinding on the web should register certain global properties', () {
+    expect(js.context.hasProperty(r'$flutterDriver'), true);
+    expect(js.context[r'$flutterDriver'], isNotNull);
+
+    expect(js.context.hasProperty(r'$flutterDriverResult'), true);
+    expect(js.context[r'$flutterDriverResult'], isNull);
+  });
+}
