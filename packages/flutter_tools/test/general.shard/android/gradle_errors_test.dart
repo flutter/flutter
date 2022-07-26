ee48416a847e308d3c894eacfd5d4c23d4e86f53
Zachary Anderson <zanderso@users.noreply.github.com>
2022-07-25 22:12:46 -0700

    Revert "Upgrade Gradle and AGP versions to 7.5/7.2 and migrate examples/tests (#108197)"
    This reverts commit ca6cecf034d9c18fcb6ea70309cc855e353aa0f8.

--- packages/flutter_tools/test/general.shard/android/gradle_errors_test.dart
+++ packages/flutter_tools/test/general.shard/android/gradle_errors_test.dart
@@ -907,10 +907,10 @@ A problem occurred evaluating project ':app'.
           '│ To fix this issue, replace the following content:                                │\n'
           '│ /android/build.gradle:                                                           │\n'
           "│     - classpath 'com.android.tools.build:gradle:<current-version>'               │\n"
-          "│     + classpath 'com.android.tools.build:gradle:7.2.0'                           │\n"
+          "│     + classpath 'com.android.tools.build:gradle:7.1.2'                           │\n"
           '│ /android/gradle/wrapper/gradle-wrapper.properties:                               │\n'
           '│     - https://services.gradle.org/distributions/gradle-<current-version>-all.zip │\n'
-          '│     + https://services.gradle.org/distributions/gradle-7.5-all.zip               │\n'
+          '│     + https://services.gradle.org/distributions/gradle-7.4-all.zip               │\n'
           '└──────────────────────────────────────────────────────────────────────────────────┘\n'
         )
       );
