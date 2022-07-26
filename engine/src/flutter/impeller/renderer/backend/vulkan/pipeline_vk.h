383e58603975a8e9308a86c2942eb7485b051ffa
Kaushik Iska <iska.kaushik@gmail.com>
2022-07-26 10:56:04 -0400

    [Impeller] [vulkan] Wire-up creating pipeline (flutter/engine#34890)

--- engine/src/flutter/impeller/renderer/backend/vulkan/pipeline_vk.h
+++ engine/src/flutter/impeller/renderer/backend/vulkan/pipeline_vk.h
@@ -4,25 +4,49 @@
 
 #pragma once
 
+#include <memory>
+
 #include "flutter/fml/macros.h"
 #include "impeller/base/backend_cast.h"
+#include "impeller/renderer/backend/vulkan/vk.h"
 #include "impeller/renderer/pipeline.h"
 
 namespace impeller {
 
+class PipelineCreateInfoVK {
+ public:
+  PipelineCreateInfoVK(vk::UniquePipeline pipeline,
+                       vk::UniqueRenderPass render_pass);
+
+  bool IsValid() const;
+
+  vk::UniquePipeline GetPipeline();
+
+  vk::UniqueRenderPass GetRenderPass();
+
+ private:
+  bool is_valid_ = false;
+  vk::UniquePipeline pipeline_;
+  vk::UniqueRenderPass render_pass_;
+};
+
 class PipelineVK final : public Pipeline,
                          public BackendCast<PipelineVK, Pipeline> {
  public:
+  PipelineVK(std::weak_ptr<PipelineLibrary> library,
+             PipelineDescriptor desc,
+             std::unique_ptr<PipelineCreateInfoVK> create_info);
+
   // |Pipeline|
   ~PipelineVK() override;
 
  private:
-  friend class PipelineLibraryMTL;
-
-  PipelineVK(std::weak_ptr<PipelineLibrary> library, PipelineDescriptor desc);
+  friend class PipelineLibraryVK;
 
   // |Pipeline|
   bool IsValid() const override;
+
+  std::unique_ptr<PipelineCreateInfoVK> pipeline_info_;
 
   FML_DISALLOW_COPY_AND_ASSIGN(PipelineVK);
 };
