4f387559d5fa6c52d95a4993d7c53c02a1409645
Anthony Oleinik <oleina@google.com>
2022-07-27 13:55:19 -0700

    fixed small bug where ios would hide and never sho

--- packages/flutter/lib/src/widgets/text_selection.dart
+++ packages/flutter/lib/src/widgets/text_selection.dart
@@ -1301,9 +1301,13 @@ class SelectionOverlay {
   /// Update the current magnifier with new selection data, so the magnifier
   /// can respond accordingly.
   ///
-  /// If there is no magnifier shown, this does nothing.
+  /// If there magnifier is not shown, this still updates the magnifier position
+  /// because the magnifier may have hidden itself and is looking for a cue to reshow
+  /// itself.
+  ///
+  /// If there is no magnifier in the overlay, this does nothing,
   void updateMagnifier(MagnifierOverlayInfoBearer magnifierOverlayInfoBearer) {
-    if (!_magnifierController.shown) {
+    if (_magnifierController.overlayEntry == null) {
       return;
     }
 
