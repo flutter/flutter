41b89f929c25682f244a83b5141fe1b84e4a2635
Chris Bracken <chris@bracken.jp>
2022-07-30 19:07:20 +0100

    [Windows] Merge SettingsPluginWin32 and superclass (flutter/engine#35019)
    This merges SettingsPluginWin32 into SettingsPlugin.
    
    With the removal of the UWP embedder, we can merge Win32-specific
    implementation classes with their abstract superclasses where those
    superclasses existed only to support both UWP and Win32.
    
    No new tests since this is purely a restructuring of existing code
    within the Win32 embedder, with no expected change in behaviour.
    
    Issue: https://github.com/flutter/flutter/issues/108386

--- engine/src/flutter/shell/platform/windows/settings_plugin.cc
+++ engine/src/flutter/shell/platform/windows/settings_plugin.cc
@@ -5,6 +5,7 @@
 #include "flutter/shell/platform/windows/settings_plugin.h"
 
 #include "flutter/shell/platform/common/json_message_codec.h"
+#include "flutter/shell/platform/windows/system_utils.h"
 
 namespace flutter {
 
@@ -17,6 +18,14 @@ constexpr char kPlatformBrightness[] = "platformBrightness";
 
 constexpr char kPlatformBrightnessDark[] = "dark";
 constexpr char kPlatformBrightnessLight[] = "light";
+
+constexpr wchar_t kGetPreferredBrightnessRegKey[] =
+    L"Software\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize";
+constexpr wchar_t kGetPreferredBrightnessRegValue[] = L"AppsUseLightTheme";
+
+constexpr wchar_t kGetTextScaleFactorRegKey[] =
+    L"Software\\Microsoft\\Accessibility";
+constexpr wchar_t kGetTextScaleFactorRegValue[] = L"TextScaleFactor";
 }  // namespace
 
 SettingsPlugin::SettingsPlugin(BinaryMessenger* messenger,
@@ -43,6 +52,83 @@ void SettingsPlugin::SendSettings() {
                        allocator);
   }
   channel_->Send(settings);
+}
+
+void SettingsPlugin::StartWatching() {
+  if (preferred_brightness_reg_hkey_ != nullptr) {
+    WatchPreferredBrightnessChanged();
+  }
+  if (text_scale_factor_reg_hkey_ != nullptr) {
+    WatchTextScaleFactorChanged();
+  }
+}
+
+void SettingsPlugin::StopWatching() {
+  preferred_brightness_changed_watcher_ = nullptr;
+  text_scale_factor_changed_watcher_ = nullptr;
+}
+
+bool SettingsPlugin::GetAlwaysUse24HourFormat() {
+  return Prefer24HourTime(GetUserTimeFormat());
+}
+
+float SettingsPlugin::GetTextScaleFactor() {
+  DWORD text_scale_factor;
+  DWORD text_scale_factor_size = sizeof(text_scale_factor);
+  LONG result = RegGetValue(
+      HKEY_CURRENT_USER, kGetTextScaleFactorRegKey, kGetTextScaleFactorRegValue,
+      RRF_RT_REG_DWORD, nullptr, &text_scale_factor, &text_scale_factor_size);
+
+  if (result == 0) {
+    return text_scale_factor / 100.0;
+  } else {
+    // The current OS does not have text scale factor.
+    return 1.0;
+  }
+}
+
+SettingsPlugin::PlatformBrightness SettingsPlugin::GetPreferredBrightness() {
+  DWORD use_light_theme;
+  DWORD use_light_theme_size = sizeof(use_light_theme);
+  LONG result = RegGetValue(HKEY_CURRENT_USER, kGetPreferredBrightnessRegKey,
+                            kGetPreferredBrightnessRegValue, RRF_RT_REG_DWORD,
+                            nullptr, &use_light_theme, &use_light_theme_size);
+
+  if (result == 0) {
+    return use_light_theme ? SettingsPlugin::PlatformBrightness::kLight
+                           : SettingsPlugin::PlatformBrightness::kDark;
+  } else {
+    // The current OS does not support dark mode. (Older Windows 10 or before
+    // Windows 10)
+    return SettingsPlugin::PlatformBrightness::kLight;
+  }
+}
+
+void SettingsPlugin::WatchPreferredBrightnessChanged() {
+  preferred_brightness_changed_watcher_ =
+      std::make_unique<EventWatcher>([this]() {
+        task_runner_->PostTask([this]() {
+          SendSettings();
+          WatchPreferredBrightnessChanged();
+        });
+      });
+
+  RegNotifyChangeKeyValue(
+      preferred_brightness_reg_hkey_, FALSE, REG_NOTIFY_CHANGE_LAST_SET,
+      preferred_brightness_changed_watcher_->GetHandle(), TRUE);
+}
+
+void SettingsPlugin::WatchTextScaleFactorChanged() {
+  text_scale_factor_changed_watcher_ = std::make_unique<EventWatcher>([this]() {
+    task_runner_->PostTask([this]() {
+      SendSettings();
+      WatchTextScaleFactorChanged();
+    });
+  });
+
+  RegNotifyChangeKeyValue(
+      text_scale_factor_reg_hkey_, FALSE, REG_NOTIFY_CHANGE_LAST_SET,
+      text_scale_factor_changed_watcher_->GetHandle(), TRUE);
 }
 
 }  // namespace flutter
