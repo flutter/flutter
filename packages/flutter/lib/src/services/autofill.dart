a261b886f0133a8b16837a5c97673ab6fc9458f0
Kevin Moore <kevmoo@users.noreply.github.com>
2022-07-29 17:11:41 -0700

    Fix doc comment line accidentally removed (#108654)
    Fixing issue in commit c92e001400fcf9064001e993fc6a36e3fb0f56b5

--- packages/flutter/lib/src/services/autofill.dart
+++ packages/flutter/lib/src/services/autofill.dart
@@ -22,6 +22,7 @@ class AutofillHints {
   ///
   /// This hint will be translated to the below values on different platforms:
   ///
+  /// * Android: [AUTOFILL_HINT_POSTAL_ADDRESS_LOCALITY](https://developer.android.com/reference/androidx/autofill/HintConstants#AUTOFILL_HINT_POSTAL_ADDRESS_LOCALITY).
   /// * iOS: [addressCity](https://developer.apple.com/documentation/uikit/uitextcontenttype).
   /// * Otherwise, the hint string will be used as-is.
   static const String addressCity = 'addressCity';
