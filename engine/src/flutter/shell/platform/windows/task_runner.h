374899e94b09779ae87c2acb5e7681fa755fa770
Chris Bracken <chris@bracken.jp>
2022-08-01 20:09:01 +0100

    [Windows] Merge TaskRunnerWin32 into TaskRunner (flutter/engine#35029)
    With the removal of the UWP embedder, we can merge Win32-specific
    implementation classes with their abstract superclasses where those
    superclasses existed only to support both UWP and Win32.
    
    No new tests since this is purely a restructuring of existing code
    within the Win32 embedder, with no expected change in behaviour.
    
    Issue: https://github.com/flutter/flutter/issues/108386

--- engine/src/flutter/shell/platform/windows/task_runner.h
+++ engine/src/flutter/shell/platform/windows/task_runner.h
@@ -14,21 +14,30 @@
 #include <variant>
 
 #include "flutter/shell/platform/embedder/embedder.h"
+#include "flutter/shell/platform/windows/task_runner_window.h"
 
 namespace flutter {
 
 typedef uint64_t (*CurrentTimeProc)();
 
-class TaskRunner {
+// A custom task runner that integrates with user32 GetMessage semantics so
+// that host app can own its own message loop and flutter still gets to process
+// tasks on a timely basis.
+class TaskRunner : public TaskRunnerWindow::Delegate {
  public:
   using TaskTimePoint = std::chrono::steady_clock::time_point;
   using TaskExpiredCallback = std::function<void(const FlutterTask*)>;
   using TaskClosure = std::function<void()>;
 
-  virtual ~TaskRunner() = default;
+  // Creates a new task runner with the current thread, current time
+  // provider, and callback for tasks that are ready to be run.
+  TaskRunner(CurrentTimeProc get_current_time,
+             const TaskExpiredCallback& on_task_expired);
+
+  virtual ~TaskRunner();
 
   // Returns `true` if the current thread is this runner's thread.
-  virtual bool RunsTasksOnCurrentThread() const = 0;
+  virtual bool RunsTasksOnCurrentThread() const;
 
   // Post a Flutter engine task to the event loop for delayed execution.
   void PostFlutterTask(FlutterTask flutter_task,
@@ -47,32 +56,8 @@ class TaskRunner {
     }
   }
 
-  // Creates a new task runner with the current thread, current time
-  // provider, and callback for tasks that are ready to be run.
-  static std::unique_ptr<TaskRunner> Create(
-      CurrentTimeProc get_current_time,
-      const TaskExpiredCallback& on_task_expired);
-
- protected:
-  TaskRunner(CurrentTimeProc get_current_time,
-             const TaskExpiredCallback& on_task_expired);
-
-  // Schedules timers to call `ProcessTasks()` at the runner's thread.
-  virtual void WakeUp() = 0;
-
-  // Executes expired task, and returns the duration until the next task
-  // deadline if exists, otherwise returns `std::chrono::nanoseconds::max()`.
-  //
-  // Each platform implementations must call this to schedule the tasks.
+  // |TaskRunnerWindow::Delegate|
   std::chrono::nanoseconds ProcessTasks();
-
-  // Returns the current TaskTimePoint that can be used to determine whether a
-  // task is expired.
-  //
-  // Tests can override this to mock up the time.
-  virtual TaskTimePoint GetCurrentTimeForTask() const {
-    return TaskTimePoint::clock::now();
-  }
 
  private:
   typedef std::variant<FlutterTask, TaskClosure> TaskVariant;
@@ -95,6 +80,17 @@ class TaskRunner {
   // Enqueues the given task.
   void EnqueueTask(Task task);
 
+  // Schedules timers to call `ProcessTasks()` at the runner's thread.
+  virtual void WakeUp();
+
+  // Returns the current TaskTimePoint that can be used to determine whether a
+  // task is expired.
+  //
+  // Tests can override this to mock up the time.
+  virtual TaskTimePoint GetCurrentTimeForTask() const {
+    return TaskTimePoint::clock::now();
+  }
+
   // Returns a TaskTimePoint computed from the given target time from Flutter.
   TaskTimePoint TimePointFromFlutterTime(
       uint64_t flutter_target_time_nanos) const;
@@ -103,6 +99,8 @@ class TaskRunner {
   TaskExpiredCallback on_task_expired_;
   std::mutex task_queue_mutex_;
   std::priority_queue<Task, std::deque<Task>, Task::Comparer> task_queue_;
+  DWORD main_thread_id_;
+  std::shared_ptr<TaskRunnerWindow> task_runner_window_;
 
   TaskRunner(const TaskRunner&) = delete;
   TaskRunner& operator=(const TaskRunner&) = delete;
