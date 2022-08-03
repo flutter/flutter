8fa73e0ac6e778c08f06477b6f7fcee948cb0b31
Jonah Williams <jonahwilliams@google.com>
2022-08-03 13:27:04 -0700

    [impeller] use cullRect for drawAtlas coverage (flutter/engine#35133)

--- engine/src/flutter/impeller/aiks/canvas.cc
+++ engine/src/flutter/impeller/aiks/canvas.cc
@@ -346,8 +346,8 @@ void Canvas::DrawAtlas(std::shared_ptr<Image> atlas,
   contents->SetTexture(atlas->GetTexture());
   contents->SetSamplerDescriptor(std::move(sampler));
   contents->SetBlendMode(blend_mode);
+  contents->SetCullRect(cull_rect);
   contents->SetAlpha(paint.color.alpha);
-  // TODO(jonahwilliams): set cull rect.
 
   Entity entity;
   entity.SetTransformation(GetCurrentTransformation());
