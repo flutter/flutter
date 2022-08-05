73305c504ddb4d85128aa407b92a1fa23f9870da
Pierre-Louis <6655696+guidezpl@users.noreply.github.com>
2022-08-05 19:34:04 +0200

    Make it possible to obtain `FontWeight` integer value (flutter/engine#35183)

--- engine/src/flutter/testing/dart/text_test.dart
+++ engine/src/flutter/testing/dart/text_test.dart
@@ -19,7 +19,7 @@ Future<Uint8List> readFile(String fileName) async {
   return file.readAsBytes();
 }
 
-void testFontWeightLerp() {
+void testFontWeight() {
   test('FontWeight.lerp works with non-null values', () {
     expect(FontWeight.lerp(FontWeight.w400, FontWeight.w600, .5), equals(FontWeight.w500));
   });
@@ -34,6 +34,18 @@ void testFontWeightLerp() {
 
   test('FontWeight.lerp returns FontWeight.w400 if b is null', () {
     expect(FontWeight.lerp(FontWeight.w400, null, 1), equals(FontWeight.w400));
+  });
+
+  test('FontWeights have the correct value', () {
+    expect(FontWeight.w100.value, 100);
+    expect(FontWeight.w200.value, 200);
+    expect(FontWeight.w300.value, 300);
+    expect(FontWeight.w400.value, 400);
+    expect(FontWeight.w500.value, 500);
+    expect(FontWeight.w600.value, 600);
+    expect(FontWeight.w700.value, 700);
+    expect(FontWeight.w800.value, 800);
+    expect(FontWeight.w900.value, 900);
   });
 }
 
@@ -277,7 +289,7 @@ void testFontVariation() {
 }
 
 void main() {
-  testFontWeightLerp();
+  testFontWeight();
   testParagraphStyle();
   testTextStyle();
   testTextHeightBehavior();
