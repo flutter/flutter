245cf92334a54d7354bdd7673b74cd37dd446c4c
Hari07 <harinandan.alukkal@gmail.com>
2022-08-07 17:59:55 +0530

    Added strokecap option on progress on circular/refresh progress indicator
    Fixed typo in existing test
    
    Added ability to mock strokecap
    
    Wrote test for Refresh Progress Indicator

--- packages/flutter/test/material/progress_indicator_test.dart
+++ packages/flutter/test/material/progress_indicator_test.dart
@@ -433,14 +433,28 @@ void main() {
     expect(layers1, isNot(equals(layers2)));
   });
 
-  testWidgets('CircularProgressIndicator stoke width', (WidgetTester tester) async {
-    await tester.pumpWidget(Theme(data: theme, child: const CircularProgressIndicator()));
+  testWidgets('CircularProgressIndicator stroke width', (WidgetTester tester) async {
+    await tester.pumpWidget(const CircularProgressIndicator());
 
     expect(find.byType(CircularProgressIndicator), paints..arc(strokeWidth: 4.0));
 
     await tester.pumpWidget(Theme(data: theme, child: const CircularProgressIndicator(strokeWidth: 16.0)));
 
     expect(find.byType(CircularProgressIndicator), paints..arc(strokeWidth: 16.0));
+  });
+
+  testWidgets('CircularProgressIndicator stroke cap', (WidgetTester tester) async {
+    await tester.pumpWidget(const CircularProgressIndicator());
+
+    expect(find.byType(CircularProgressIndicator), paints..arc(strokeCap: StrokeCap.square));
+
+    await tester.pumpWidget(const CircularProgressIndicator(strokeCap: StrokeCap.round,));
+
+    expect(find.byType(CircularProgressIndicator), paints..arc(strokeCap: StrokeCap.round));
+
+    await tester.pumpWidget(const CircularProgressIndicator(strokeCap: StrokeCap.butt,));
+
+    expect(find.byType(CircularProgressIndicator), paints..arc(strokeCap: StrokeCap.butt));
   });
 
   testWidgets('CircularProgressIndicator paint colors', (WidgetTester tester) async {
@@ -567,6 +581,20 @@ void main() {
     ));
     expect(themeBackgroundMaterial.type, MaterialType.circle);
     expect(themeBackgroundMaterial.color, blue);
+  });
+
+  testWidgets('RefreshProgressIndicator stroke cap', (WidgetTester tester) async {
+    await tester.pumpWidget(const RefreshProgressIndicator());
+
+    expect(find.byType(RefreshProgressIndicator), paints..arc(strokeCap: StrokeCap.square));
+
+    await tester.pumpWidget(const RefreshProgressIndicator(strokeCap: StrokeCap.round,));
+
+    expect(find.byType(RefreshProgressIndicator), paints..arc(strokeCap: StrokeCap.round));
+
+    await tester.pumpWidget(const RefreshProgressIndicator(strokeCap: StrokeCap.butt,));
+
+    expect(find.byType(RefreshProgressIndicator), paints..arc(strokeCap: StrokeCap.butt));
   });
 
   testWidgets('Indeterminate RefreshProgressIndicator keeps spinning until end of time (approximate)', (WidgetTester tester) async {
