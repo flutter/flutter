0e5b7ac32d0288cc20bdf91466400c5a9cd07e82
Abdelkhalek Allan <abdelkhalek.allan1@gmail.com>
2022-08-08 03:50:25 +0300

    git config -l --show-origin

--- dev/benchmarks/multiple_flutters/module/lib/main.dart
+++ dev/benchmarks/multiple_flutters/module/lib/main.dart
@@ -4,6 +4,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter/semantics.dart';
+// ignore: depend_on_referenced_packages
 import 'package:google_fonts/google_fonts.dart';
 
 void main() => runApp(const MyApp(Colors.blue));
