60cc118aeb4efbfb5eb28e55e624aad89c2cec92
Chris Bracken <chris@bracken.jp>
2022-08-09 11:35:32 -0700

    Support fixture tests for Windows embedder (flutter/engine#35273)
    This adds support for running end-to-end tests that use a live engine to
    run Dart test fixtures. This enables testing the public Windows C API in
    //flutter/shell/platform/windows/public/flutter_windows.h
    
    This only adds support for a single test entrypoint (main). A followup
    patch will add support for this. See:
    https://github.com/flutter/flutter/issues/93537
    
    Issue: https://github.com/flutter/flutter/issues/87299

--- engine/src/flutter/shell/platform/windows/flutter_windows_unittests.cc
+++ engine/src/flutter/shell/platform/windows/flutter_windows_unittests.cc
@@ -5,13 +5,17 @@
 #include "flutter/shell/platform/windows/public/flutter_windows.h"
 
 #include <cstring>
+#include <thread>
 
+#include "flutter/shell/platform/windows/testing/windows_test.h"
+#include "flutter/shell/platform/windows/testing/windows_test_config_builder.h"
+#include "flutter/shell/platform/windows/testing/windows_test_context.h"
 #include "gtest/gtest.h"
 
 namespace flutter {
 namespace testing {
 
-TEST(FlutterWindowsTest, GetTextureRegistrar) {
+TEST(WindowsNoFixtureTest, GetTextureRegistrar) {
   FlutterDesktopEngineProperties properties;
   memset(&properties, 0, sizeof(FlutterDesktopEngineProperties));
   properties.assets_path = L"";
@@ -21,6 +25,20 @@ TEST(FlutterWindowsTest, GetTextureRegistrar) {
   auto texture_registrar = FlutterDesktopEngineGetTextureRegistrar(engine);
   EXPECT_NE(texture_registrar, nullptr);
   FlutterDesktopEngineDestroy(engine);
+}
+
+TEST_F(WindowsTest, LaunchMain) {
+  auto& context = GetContext();
+  WindowsConfigBuilder builder(context);
+  ViewControllerPtr controller{builder.LaunchEngine()};
+  ASSERT_NE(controller, nullptr);
+
+  // Run for 1 second, then shut down.
+  //
+  // TODO(cbracken): Support registring a native function we can use to
+  // determine that execution has made it to a specific point in the Dart
+  // code. https://github.com/flutter/flutter/issues/109242
+  std::this_thread::sleep_for(std::chrono::seconds(1));
 }
 
 }  // namespace testing
