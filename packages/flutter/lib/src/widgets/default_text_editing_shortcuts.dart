45d14567d17b6b7ffbcd7b71cd6d90ff9f76f98e
Tomasz Gucio <tgucio@gmail.com>
2022-08-10 19:18:19 +0200

    Update macOS intents

--- packages/flutter/lib/src/widgets/default_text_editing_shortcuts.dart
+++ packages/flutter/lib/src/widgets/default_text_editing_shortcuts.dart
@@ -554,8 +554,8 @@ Intent? intentForMacOSSelector(String selectorName) {
     // TODO(knopp): Page Up/Down intents are missing (https://github.com/flutter/flutter/pull/105497)
     'scrollPageUp:': ScrollToDocumentBoundaryIntent(forward: false),
     'scrollPageDown:': ScrollToDocumentBoundaryIntent(forward: true),
-    'pageUpAndModifySelection': ExpandSelectionToDocumentBoundaryIntent(forward: false),
-    'pageDownAndModifySelection:': ExpandSelectionToDocumentBoundaryIntent(forward: true),
+    'pageUpAndModifySelection': ExtendSelectionVerticallyToAdjacentPageIntent(forward: false, collapseSelection: false),
+    'pageDownAndModifySelection:': ExtendSelectionVerticallyToAdjacentPageIntent(forward: true, collapseSelection: false),
 
     // Escape key when there's no IME selection popup.
     'cancelOperation:': DismissIntent(),
