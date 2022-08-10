8f1be066e548f1c81a3e9e873660e33f57ca119f
Chinmay Garde <chinmaygarde@google.com>
2022-08-09 18:31:05 -0700

    [Impeller] Enable support for Apple Silicon Macs and tvOS devices. (flutter/engine#35287)

--- engine/src/flutter/impeller/renderer/backend/metal/allocator_mtl.mm
+++ engine/src/flutter/impeller/renderer/backend/metal/allocator_mtl.mm
@@ -14,11 +14,50 @@
 
 namespace impeller {
 
+static bool DeviceSupportsMemorylessTargets(id<MTLDevice> device) {
+  // Refer to the "Memoryless render targets" feature in the table below:
+  // https://developer.apple.com/metal/Metal-Feature-Set-Tables.pdf
+  if (@available(ios 13.0, tvos 13.0, macos 10.15, *)) {
+    return [device supportsFamily:MTLGPUFamilyApple2];
+  } else {
+#if FML_OS_IOS
+    // This is perhaps redundant. But, just in case we somehow get into a case
+    // where Impeller runs on iOS versions less than 8.0 and/or without A8
+    // GPUs, we explicitly check feature set support.
+    return [device supportsFeatureSet:MTLFeatureSet_iOS_GPUFamily2_v1];
+#else
+    // MacOS devices with Apple GPUs are only available with macos 10.15 and
+    // above. So, if we are here, it is safe to assume that memory-less targets
+    // are not supported.
+    return false;
+#endif
+  }
+  FML_UNREACHABLE();
+}
+
+static bool DeviceHasUnifiedMemoryArchitecture(id<MTLDevice> device) {
+  if (@available(ios 13.0, tvos 13.0, macOS 10.15, *)) {
+    return [device hasUnifiedMemory];
+  } else {
+#if FML_OS_IOS
+    // iOS devices where the availability check can fail always have had UMA.
+    return true;
+#else
+    // Mac devices where the availability check can fail have never had UMA.
+    return false;
+#endif
+  }
+  FML_UNREACHABLE();
+}
+
 AllocatorMTL::AllocatorMTL(id<MTLDevice> device, std::string label)
     : device_(device), allocator_label_(std::move(label)) {
   if (!device_) {
     return;
   }
+
+  supports_memoryless_targets_ = DeviceSupportsMemorylessTargets(device_);
+  supports_uma_ = DeviceHasUnifiedMemoryArchitecture(device_);
 
   is_valid_ = true;
 }
@@ -29,56 +68,86 @@ bool AllocatorMTL::IsValid() const {
   return is_valid_;
 }
 
-static MTLResourceOptions ToMTLResourceOptions(StorageMode type) {
+static MTLResourceOptions ToMTLResourceOptions(StorageMode type,
+                                               bool supports_memoryless_targets,
+                                               bool supports_uma) {
   switch (type) {
     case StorageMode::kHostVisible:
 #if FML_OS_IOS
       return MTLResourceStorageModeShared;
 #else
-      return MTLResourceStorageModeManaged;
+      if (supports_uma) {
+        return MTLResourceStorageModeShared;
+      } else {
+        return MTLResourceStorageModeManaged;
+      }
 #endif
     case StorageMode::kDevicePrivate:
       return MTLResourceStorageModePrivate;
     case StorageMode::kDeviceTransient:
-#if FML_OS_IOS
-      return MTLResourceStorageModeMemoryless;
-#else
-      return MTLResourceStorageModePrivate;
-#endif
+      if (supports_memoryless_targets) {
+        // Device may support but the OS has not been updated.
+        if (@available(macOS 11.0, *)) {
+          return MTLResourceStorageModeMemoryless;
+        } else {
+          return MTLResourceStorageModePrivate;
+        }
+      } else {
+        return MTLResourceStorageModePrivate;
+      }
+      FML_UNREACHABLE();
   }
-
-  return MTLResourceStorageModePrivate;
+  FML_UNREACHABLE();
 }
 
-static MTLStorageMode ToMTLStorageMode(StorageMode mode) {
+static MTLStorageMode ToMTLStorageMode(StorageMode mode,
+                                       bool supports_memoryless_targets,
+                                       bool supports_uma) {
   switch (mode) {
     case StorageMode::kHostVisible:
 #if FML_OS_IOS
       return MTLStorageModeShared;
 #else
-      return MTLStorageModeManaged;
+      if (supports_uma) {
+        return MTLStorageModeShared;
+      } else {
+        return MTLStorageModeManaged;
+      }
 #endif
     case StorageMode::kDevicePrivate:
       return MTLStorageModePrivate;
     case StorageMode::kDeviceTransient:
-#if FML_OS_IOS
-      return MTLStorageModeMemoryless;
-#else
-      return MTLStorageModePrivate;
-#endif
+      if (supports_memoryless_targets) {
+        // Device may support but the OS has not been updated.
+        if (@available(macOS 11.0, *)) {
+          return MTLStorageModeMemoryless;
+        } else {
+          return MTLStorageModePrivate;
+        }
+      } else {
+        return MTLStorageModePrivate;
+      }
+      FML_UNREACHABLE();
   }
-  return MTLStorageModeShared;
+  FML_UNREACHABLE();
 }
 
 std::shared_ptr<DeviceBuffer> AllocatorMTL::CreateBuffer(StorageMode mode,
                                                          size_t length) {
-  auto buffer = [device_ newBufferWithLength:length
-                                     options:ToMTLResourceOptions(mode)];
+  const auto resource_options =
+      ToMTLResourceOptions(mode, supports_memoryless_targets_, supports_uma_);
+  const auto storage_mode =
+      ToMTLStorageMode(mode, supports_memoryless_targets_, supports_uma_);
+
+  auto buffer = [device_ newBufferWithLength:length options:resource_options];
   if (!buffer) {
     return nullptr;
   }
-  return std::shared_ptr<DeviceBufferMTL>(
-      new DeviceBufferMTL(buffer, length, mode));
+  return std::shared_ptr<DeviceBufferMTL>(new DeviceBufferMTL(buffer,       //
+                                                              length,       //
+                                                              mode,         //
+                                                              storage_mode  //
+                                                              ));
 }
 
 std::shared_ptr<Texture> AllocatorMTL::CreateTexture(
@@ -95,7 +164,8 @@ std::shared_ptr<Texture> AllocatorMTL::CreateTexture(
     return nullptr;
   }
 
-  mtl_texture_desc.storageMode = ToMTLStorageMode(mode);
+  mtl_texture_desc.storageMode =
+      ToMTLStorageMode(mode, supports_memoryless_targets_, supports_uma_);
   auto texture = [device_ newTextureWithDescriptor:mtl_texture_desc];
   if (!texture) {
     return nullptr;
