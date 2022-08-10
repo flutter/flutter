d1ef3e70c721a2bf4c372b0e6ce3187ad006ebc9
camsim99 <camillesimon90@gmail.com>
2022-08-10 10:12:37 -0700

    [openpay] Create an OpenPay benchmark (#107838)

--- dev/devicelab/lib/tasks/perf_tests.dart
+++ dev/devicelab/lib/tasks/perf_tests.dart
@@ -223,6 +223,17 @@ TaskFunction createPictureCacheComplexityScoringPerfTest() {
   ).run;
 }
 
+TaskFunction createOpenPayScrollPerfTest({bool measureCpuGpu = true}) {
+  return PerfTest(
+    openpayDirectory.path,
+    'test_driver/scroll_perf.dart',
+    'openpay_scroll_perf',
+    measureCpuGpu: measureCpuGpu,
+    testDriver: 'test_driver/scroll_perf_test.dart',
+    saveTraceFile: true,
+  ).run;
+}
+
 TaskFunction createFlutterGalleryStartupTest({String target = 'lib/main.dart'}) {
   return StartupTest(
     '${flutterDirectory.path}/dev/integration_tests/flutter_gallery',
