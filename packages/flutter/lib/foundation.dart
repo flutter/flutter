117760a03d97ab347802393d2318806e51d59018
Polina Cherkasova <polina.c@live.com>
2022-08-11 15:54:57 -0700

    -

--- packages/flutter/lib/foundation.dart
+++ packages/flutter/lib/foundation.dart
@@ -34,6 +34,7 @@ export 'src/foundation/isolates.dart';
 export 'src/foundation/key.dart';
 export 'src/foundation/licenses.dart';
 export 'src/foundation/math.dart';
+export 'src/foundation/memory_allocations.dart';
 export 'src/foundation/node.dart';
 export 'src/foundation/object.dart';
 export 'src/foundation/observer_list.dart';
---
+++ packages/flutter/test/foundation/memory_allocations_test.dart
@@ -0,0 +1,17 @@
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/src/foundation/memory_allocations.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+
+  test('test', () {
+    final MemoryAllocations ma = MemoryAllocations.instance;
+    expect(
+      1,
+      1,
+    );
+  });
+}
