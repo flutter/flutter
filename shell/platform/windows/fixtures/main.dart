3747cf318c58b3e6c97d5690e4c8fffd70d33e21
Chris Bracken <chris@bracken.jp>
2022-08-11 17:17:38 -0700

    [Windows] Support native functions in test fixtures (#35357)
    Adds the ability to register native functions for use in test fixtures.
    This allows registering native C++ functions that can be invoked from
    Dart code to perform the following common actions:
    
    * Signal a waiting latch in the C++ part of the test.
    * Pass data back to the C++ part of the test.
    * Allow the C++ part of the test to pass data to the test.
    
    Fixes: https://github.com/flutter/flutter/issues/109242
    Fixes: https://github.com/flutter/flutter/issues/87299

--- shell/platform/windows/fixtures/main.dart
+++ shell/platform/windows/fixtures/main.dart
@@ -2,11 +2,34 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// Signals a waiting latch in the native test.
+void signal() native 'Signal';
+
+// Signals a waiting latch in the native test, passing a boolean value.
+void signalBoolValue(bool value) native 'SignalBoolValue';
+
+// Signals a waiting latch in the native test, which returns a value to the fixture.
+bool signalBoolReturn() native 'SignalBoolReturn';
+
 void main() {
-  print('Hello windows engine test main!');
 }
 
 @pragma('vm:entry-point')
 void customEntrypoint() {
-  print('Hello windows engine test customEntrypoint!');
+}
+
+@pragma('vm:entry-point')
+void verifyNativeFunction() {
+  signal();
+}
+
+@pragma('vm:entry-point')
+void verifyNativeFunctionWithParameters() {
+  signalBoolValue(true);
+}
+
+@pragma('vm:entry-point')
+void verifyNativeFunctionWithReturn() {
+  bool value = signalBoolReturn();
+  signalBoolValue(value);
 }
--- shell/platform/windows/flutter_windows_unittests.cc
+++ shell/platform/windows/flutter_windows_unittests.cc
@@ -6,14 +6,19 @@
 
 #include <thread>
 
+#include "flutter/fml/synchronization/count_down_latch.h"
+#include "flutter/fml/synchronization/waitable_event.h"
 #include "flutter/shell/platform/windows/testing/windows_test.h"
 #include "flutter/shell/platform/windows/testing/windows_test_config_builder.h"
 #include "flutter/shell/platform/windows/testing/windows_test_context.h"
 #include "gtest/gtest.h"
+#include "third_party/tonic/converter/dart_converter.h"
 
 namespace flutter {
 namespace testing {
 
+// Verify that we can fetch a texture registrar.
+// Prevent regression: https://github.com/flutter/flutter/issues/86617
 TEST(WindowsNoFixtureTest, GetTextureRegistrar) {
   FlutterDesktopEngineProperties properties = {};
   properties.assets_path = L"";
@@ -25,33 +30,21 @@ TEST(WindowsNoFixtureTest, GetTextureRegistrar) {
   FlutterDesktopEngineDestroy(engine);
 }
 
+// Verify we can successfully launch main().
 TEST_F(WindowsTest, LaunchMain) {
   auto& context = GetContext();
   WindowsConfigBuilder builder(context);
   ViewControllerPtr controller{builder.Run()};
   ASSERT_NE(controller, nullptr);
-
-  // Run for 1 second, then shut down.
-  //
-  // TODO(cbracken): Support registring a native function we can use to
-  // determine that execution has made it to a specific point in the Dart
-  // code. https://github.com/flutter/flutter/issues/109242
-  std::this_thread::sleep_for(std::chrono::seconds(1));
 }
 
+// Verify we can successfully launch a custom entry point.
 TEST_F(WindowsTest, LaunchCustomEntrypoint) {
   auto& context = GetContext();
   WindowsConfigBuilder builder(context);
   builder.SetDartEntrypoint("customEntrypoint");
   ViewControllerPtr controller{builder.Run()};
   ASSERT_NE(controller, nullptr);
-
-  // Run for 1 second, then shut down.
-  //
-  // TODO(cbracken): Support registring a native function we can use to
-  // determine that execution has made it to a specific point in the Dart
-  // code. https://github.com/flutter/flutter/issues/109242
-  std::this_thread::sleep_for(std::chrono::seconds(1));
 }
 
 // Verify that engine launches with the custom entrypoint specified in the
@@ -66,13 +59,6 @@ TEST_F(WindowsTest, LaunchCustomEntrypointInEngineRunInvocation) {
   ASSERT_NE(engine, nullptr);
 
   ASSERT_TRUE(FlutterDesktopEngineRun(engine.get(), "customEntrypoint"));
-
-  // Run for 1 second, then shut down.
-  //
-  // TODO(cbracken): Support registring a native function we can use to
-  // determine that execution has made it to a specific point in the Dart
-  // code. https://github.com/flutter/flutter/issues/109242
-  std::this_thread::sleep_for(std::chrono::seconds(1));
 }
 
 // Verify that engine fails to launch when a conflicting entrypoint in
@@ -88,6 +74,79 @@ TEST_F(WindowsTest, LaunchConflictingCustomEntrypoints) {
   ASSERT_NE(engine, nullptr);
 
   ASSERT_FALSE(FlutterDesktopEngineRun(engine.get(), "conflictingEntrypoint"));
+}
+
+// Verify that native functions can be registered and resolved.
+TEST_F(WindowsTest, VerifyNativeFunction) {
+  auto& context = GetContext();
+  WindowsConfigBuilder builder(context);
+  builder.SetDartEntrypoint("verifyNativeFunction");
+
+  fml::AutoResetWaitableEvent latch;
+  auto native_entry =
+      CREATE_NATIVE_ENTRY([&](Dart_NativeArguments args) { latch.Signal(); });
+  context.AddNativeFunction("Signal", native_entry);
+
+  ViewControllerPtr controller{builder.Run()};
+  ASSERT_NE(controller, nullptr);
+
+  // Wait until signal has been called.
+  latch.Wait();
+}
+
+// Verify that native functions that pass parameters can be registered and
+// resolved.
+TEST_F(WindowsTest, VerifyNativeFunctionWithParameters) {
+  auto& context = GetContext();
+  WindowsConfigBuilder builder(context);
+  builder.SetDartEntrypoint("verifyNativeFunctionWithParameters");
+
+  bool bool_value = false;
+  fml::AutoResetWaitableEvent latch;
+  auto native_entry = CREATE_NATIVE_ENTRY([&](Dart_NativeArguments args) {
+    auto handle = Dart_GetNativeBooleanArgument(args, 0, &bool_value);
+    ASSERT_FALSE(Dart_IsError(handle));
+    latch.Signal();
+  });
+  context.AddNativeFunction("SignalBoolValue", native_entry);
+
+  ViewControllerPtr controller{builder.Run()};
+  ASSERT_NE(controller, nullptr);
+
+  // Wait until signalBoolValue has been called.
+  latch.Wait();
+  EXPECT_TRUE(bool_value);
+}
+
+// Verify that native functions that return values can be registered and
+// resolved.
+TEST_F(WindowsTest, VerifyNativeFunctionWithReturn) {
+  auto& context = GetContext();
+  WindowsConfigBuilder builder(context);
+  builder.SetDartEntrypoint("verifyNativeFunctionWithReturn");
+
+  bool bool_value_to_return = true;
+  fml::CountDownLatch latch(2);
+  auto bool_return_entry = CREATE_NATIVE_ENTRY([&](Dart_NativeArguments args) {
+    Dart_SetBooleanReturnValue(args, bool_value_to_return);
+    latch.CountDown();
+  });
+  context.AddNativeFunction("SignalBoolReturn", bool_return_entry);
+
+  bool bool_value_passed = false;
+  auto bool_pass_entry = CREATE_NATIVE_ENTRY([&](Dart_NativeArguments args) {
+    auto handle = Dart_GetNativeBooleanArgument(args, 0, &bool_value_passed);
+    ASSERT_FALSE(Dart_IsError(handle));
+    latch.CountDown();
+  });
+  context.AddNativeFunction("SignalBoolValue", bool_pass_entry);
+
+  ViewControllerPtr controller{builder.Run()};
+  ASSERT_NE(controller, nullptr);
+
+  // Wait until signalBoolReturn and signalBoolValue have been called.
+  latch.Wait();
+  EXPECT_TRUE(bool_value_passed);
 }
 
 }  // namespace testing
