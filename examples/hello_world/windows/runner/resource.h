8e3528cbb5eadc3f94ed8d59e52a8c0a8caa6fe4
schectman <schectman@google.com>
2022-08-15 12:00:19 -0400

    Add license to generated files

--- examples/hello_world/windows/runner/resource.h
+++ examples/hello_world/windows/runner/resource.h
@@ -1,3 +1,7 @@
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 //{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ generated include file.
 // Used by Runner.rc
