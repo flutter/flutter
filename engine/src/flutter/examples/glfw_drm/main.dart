da10edb0b09153393620ae6c16818e6e371bc431
Bernardo Eilert Trevisan <43152751+betrevisan@users.noreply.github.com>
2022-08-15 09:45:07 -0700

    GLFW example with dirty region management (flutter/engine#35057)
    * Update the GLFW example callbacks.
    
    * Update the dart example.
    
    * Update CMakeLists
    
    * Update library dependencies in the BUILD file.
    
    * Documenting.
    
    * Formatting.
    
    * Update the callback name.
    
    * Move glfw_drm example to a new folder.
    
    * Create new example folder for drm_embedder.
    
    * Add the flutter spinkit dependency
    
    * Update BUILD files.
    
    * Do not compile glfw_drm example if running it on mac.
    
    * Update the executable name for the glfw_drm example.
    
    * Avoid looking for the surface and display every time since they are always the same.
    
    * Formatting.
    
    * GN formatting.
    
    * Update the way the user malloc's the existing damage
    
    * Update comment
    
    * Update example and add gitignore.
    
    Co-authored-by: Bernardo Eilert Trevisan <btrevisan@google.com>

---
+++ engine/src/flutter/examples/glfw_drm/main.dart
@@ -0,0 +1,80 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart'
+    show debugDefaultTargetPlatformOverride;
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+
+void main() {
+  // This is a hack to make Flutter think you are running on Google Fuchsia,
+  // otherwise you will get an error about running from an unsupported platform.
+  debugDefaultTargetPlatformOverride = TargetPlatform.fuchsia;
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: RepaintBoundary(
+          child: SpinKitRotatingCircle(color: Colors.blue, size: 50.0),
+        ),
+      ),
+    );
+  }
+}
