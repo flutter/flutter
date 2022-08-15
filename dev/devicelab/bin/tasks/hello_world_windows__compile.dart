e63764e89f0853f50e11e5a2cfc87c59a5c211f1
schectman <schectman@google.com>
2022-08-15 12:00:19 -0400

    Add license to generated files

--- dev/devicelab/bin/tasks/hello_world_windows__compile.dart
+++ dev/devicelab/bin/tasks/hello_world_windows__compile.dart
@@ -1,9 +1,9 @@
-// Copyright 2022 The Flutter Authors. All rights reserved.
+// Copyright 2014 The Flutter Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 import 'package:flutter_devicelab/framework/devices.dart';
-import 'package:flutter_devicelab/framework/framework.dart'; 
+import 'package:flutter_devicelab/framework/framework.dart';
 import 'package:flutter_devicelab/tasks/perf_tests.dart';
 
 Future<void> main() async {
