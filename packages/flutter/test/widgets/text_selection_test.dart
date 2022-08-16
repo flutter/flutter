880736ac179025abbfe4c5f39a640a1c53cd5f31
Renzo Olivares <roliv@google.com>
2022-08-16 00:25:28 -0700

    get test running

--- packages/flutter/test/widgets/text_selection_test.dart
+++ packages/flutter/test/widgets/text_selection_test.dart
@@ -25,7 +25,7 @@ void main() {
   late int dragEndCount;
   const Offset forcePressOffset = Offset(400.0, 50.0);
 
-  void handleTapDown(TapDownDetails details) { tapCount++; }
+  void handleTapDown(TapDownDetails details, int tapCount) { tapCount++; }
   void handleSingleTapUp(TapUpDetails details) { singleTapUpCount++; }
   void handleSingleTapCancel() { singleTapCancelCount++; }
   void handleSingleLongTapStart(LongPressStartDetails details) { singleLongTapStartCount++; }
@@ -33,7 +33,7 @@ void main() {
   void handleForcePressStart(ForcePressDetails details) { forcePressStartCount++; }
   void handleForcePressEnd(ForcePressDetails details) { forcePressEndCount++; }
   void handleDragSelectionStart(DragStartDetails details) { dragStartCount++; }
-  void handleDragSelectionUpdate(DragStartDetails _, DragUpdateDetails details) { dragUpdateCount++; }
+  void handleDragSelectionUpdate(DragUpdateDetails details, int tapCount) { dragUpdateCount++; }
   void handleDragSelectionEnd(DragEndDetails details) { dragEndCount++; }
 
   setUp(() {
