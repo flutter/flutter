93b6a38b1bca2715c5c0b8efeadf341eeff80bc3
Bernardo <bernaferrari2@gmail.com>
2022-08-17 00:09:38 -0300

    Reword Border paintInterior.

--- packages/flutter/lib/src/painting/box_border.dart
+++ packages/flutter/lib/src/painting/box_border.dart
@@ -182,9 +182,10 @@ abstract class BoxBorder extends ShapeBorder {
 
   @override
   void paintInterior(Canvas canvas, Rect rect, Paint paint, { TextDirection? textDirection }) {
-    // If `ShapeDecoration(shape: Border.all())`, ShapeDecoration doesn't
-    // provide any borderRadius, so the paint is always a rectangle with
-    // sharp edges.
+    // For `ShapeDecoration(shape: Border.all())`, a rectangle with sharp edges
+    // is always painted. There is no BorderRadius parameter for
+    // ShapeDecoration or Border, only for BoxDecoration, which doesn't
+    // call this method.
     canvas.drawRect(rect, paint);
   }
 
