bb955cd595981c4855bac7d5d882d8f0ec9ed31a
Tomasz Gucio <tgucio@gmail.com>
2022-08-18 00:41:00 +0200

    Provide source len to WideCharToMultiByte()

--- examples/platform_channel/windows/runner/utils.cpp
+++ examples/platform_channel/windows/runner/utils.cpp
@@ -49,16 +49,18 @@ std::string Utf8FromUtf16(const wchar_t* utf16_string) {
   if (utf16_string == nullptr) {
     return std::string();
   }
-  int target_length =
-      ::WideCharToMultiByte(CP_UTF8, WC_ERR_INVALID_CHARS, utf16_string, -1,
-                            nullptr, 0, nullptr, nullptr);
+  int source_length = static_cast<int>(wcslen(utf16_string));
+  int target_length = ::WideCharToMultiByte(
+      CP_UTF8, WC_ERR_INVALID_CHARS, utf16_string,
+      source_length, nullptr, 0, nullptr, nullptr);
   std::string utf8_string;
   if (target_length == 0 || target_length > utf8_string.max_size()) {
     return utf8_string;
   }
   utf8_string.resize(target_length);
   int converted_length = ::WideCharToMultiByte(
-      CP_UTF8, WC_ERR_INVALID_CHARS, utf16_string, -1, utf8_string.data(),
+      CP_UTF8, WC_ERR_INVALID_CHARS, utf16_string,
+      source_length, utf8_string.data(),
       target_length, nullptr, nullptr);
   if (converted_length == 0) {
     return std::string();
--- packages/flutter_tools/templates/app_shared/windows.tmpl/runner/utils.cpp
+++ packages/flutter_tools/templates/app_shared/windows.tmpl/runner/utils.cpp
@@ -45,9 +45,10 @@ std::string Utf8FromUtf16(const wchar_t* utf16_string) {
   if (utf16_string == nullptr) {
     return std::string();
   }
+  int source_length = static_cast<int>(wcslen(utf16_string));
   int target_length = ::WideCharToMultiByte(
       CP_UTF8, WC_ERR_INVALID_CHARS, utf16_string,
-      -1, nullptr, 0, nullptr, nullptr);
+      source_length, nullptr, 0, nullptr, nullptr);
   std::string utf8_string;
   if (target_length == 0 || target_length > utf8_string.max_size()) {
     return utf8_string;
@@ -55,7 +56,7 @@ std::string Utf8FromUtf16(const wchar_t* utf16_string) {
   utf8_string.resize(target_length);
   int converted_length = ::WideCharToMultiByte(
       CP_UTF8, WC_ERR_INVALID_CHARS, utf16_string,
-      -1, utf8_string.data(),
+      source_length, utf8_string.data(),
       target_length, nullptr, nullptr);
   if (converted_length == 0) {
     return std::string();
