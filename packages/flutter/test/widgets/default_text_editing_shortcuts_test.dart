8e8f48a0fac7372e8758e18058a5907c098df516
Chun-Heng Tai <chtai@google.com>
2022-08-25 15:39:20 -0700

    analyze

--- packages/flutter/test/widgets/default_text_editing_shortcuts_test.dart
+++ packages/flutter/test/widgets/default_text_editing_shortcuts_test.dart
@@ -2,11 +2,31 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'editable_text_shortcuts_test.dart';
+Future<void> sendKeyCombination(
+    WidgetTester tester,
+    SingleActivator activator,
+    ) async {
+  final List<LogicalKeyboardKey> modifiers = <LogicalKeyboardKey>[
+    if (activator.control) LogicalKeyboardKey.control,
+    if (activator.shift) LogicalKeyboardKey.shift,
+    if (activator.alt) LogicalKeyboardKey.alt,
+    if (activator.meta) LogicalKeyboardKey.meta,
+  ];
+  for (final LogicalKeyboardKey modifier in modifiers) {
+    await tester.sendKeyDownEvent(modifier);
+  }
+  await tester.sendKeyDownEvent(activator.trigger);
+  await tester.sendKeyUpEvent(activator.trigger);
+  await tester.pump();
+  for (final LogicalKeyboardKey modifier in modifiers.reversed) {
+    await tester.sendKeyUpEvent(modifier);
+  }
+}
 
 void main() {
   Widget buildSpyAboveEditableText({
@@ -376,7 +396,7 @@ void main() {
       await tester.pump();
       expect(state.lastIntent, isA<ExtendSelectionToLineBreakIntent>());
     }, variant: macOSOnly);
-  });
+  }, skip: kIsWeb); // [intended] specific tests target non-web.
 }
 
 class ActionSpy extends StatefulWidget {
