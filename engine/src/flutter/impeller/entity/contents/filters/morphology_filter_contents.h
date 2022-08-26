8bc606fb63d2fa49732a2496b4d07a4a8c5efe59
ColdPaleLight <coldpalelight@gmail.com>
2022-08-26 10:11:18 +0800

    [Impeller] Implement morphology image filter (flutter/engine#35584)

---
+++ engine/src/flutter/impeller/entity/contents/filters/morphology_filter_contents.h
@@ -0,0 +1,48 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#pragma once
+
+#include <memory>
+#include <optional>
+#include "impeller/entity/contents/filters/filter_contents.h"
+#include "impeller/entity/contents/filters/inputs/filter_input.h"
+
+namespace impeller {
+
+class DirectionalMorphologyFilterContents final : public FilterContents {
+ public:
+  DirectionalMorphologyFilterContents();
+
+  ~DirectionalMorphologyFilterContents() override;
+
+  void SetRadius(Radius radius);
+
+  void SetDirection(Vector2 direction);
+
+  void SetMorphType(MorphType morph_type);
+
+  // |FilterContents|
+  std::optional<Rect> GetFilterCoverage(
+      const FilterInput::Vector& inputs,
+      const Entity& entity,
+      const Matrix& effect_transform) const override;
+
+ private:
+  // |FilterContents|
+  std::optional<Snapshot> RenderFilter(
+      const FilterInput::Vector& input_textures,
+      const ContentContext& renderer,
+      const Entity& entity,
+      const Matrix& effect_transform,
+      const Rect& coverage) const override;
+
+  Radius radius_;
+  Vector2 direction_;
+  MorphType morph_type_;
+
+  FML_DISALLOW_COPY_AND_ASSIGN(DirectionalMorphologyFilterContents);
+};
+
+}  // namespace impeller
