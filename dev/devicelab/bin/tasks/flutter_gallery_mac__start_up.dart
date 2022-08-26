d65d64d194d855514369f5d6fad92ae6e104e963
Jenn Magder <magder@google.com>
2022-08-26 13:48:36 -0700

    Revert "[macOS] Bring up "flutter_gallery" devicelab, start up test for x86. (#110370)" (#110372)
    This reverts commit 4770e348595551832a4762ea0eabe2cca59b668c.

--- dev/devicelab/bin/tasks/flutter_gallery_mac__start_up.dart
+++ dev/devicelab/bin/tasks/flutter_gallery_mac__start_up.dart
@@ -2,11 +2,11 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:flutter_devicelab/framework/devices.dart';
 import 'package:flutter_devicelab/framework/framework.dart';
 import 'package:flutter_devicelab/tasks/perf_tests.dart';
 
 Future<void> main() async {
-  deviceOperatingSystem = DeviceOperatingSystem.macos;
+  // It's intended to use the Gallery startup test as a smoke test on macOS
+  // Catalina.
   await task(createFlutterGalleryStartupTest());
 }
