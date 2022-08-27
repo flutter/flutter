fe65b5b605501dbb44f62372f1d39e7103bac03b
Tomasz Gucio <tgucio@gmail.com>
2022-08-27 13:51:36 +0200

    ui.Image createTestImageSync({ Add ()

--- packages/flutter_test/lib/src/image.dart
+++ packages/flutter_test/lib/src/image.dart
@@ -12,6 +12,7 @@ final Map<int, ui.Image> _cache = <int, ui.Image>{};
 
 /// Creates an arbitrarily sized image for testing.
 ///
+/// If [color] isn't provided, fully transparent black color is used.
 /// If the [cache] parameter is set to true, the image will be cached for the
 /// rest of this suite. This is normally desirable, assuming a test suite uses
 /// images with the same dimensions in most tests, as it will save on memory
@@ -27,6 +28,7 @@ final Map<int, ui.Image> _cache = <int, ui.Image>{};
 Future<ui.Image> createTestImage({
   int width = 1,
   int height = 1,
+  ui.Color color = const ui.Color(0x00000000),
   bool cache = true,
 }) => TestAsyncUtils.guard(() async {
   assert(width != null && width > 0);
@@ -38,23 +40,58 @@ Future<ui.Image> createTestImage({
     return _cache[cacheKey]!.clone();
   }
 
-  final ui.Image image = await _createImage(width, height);
+  final ui.Image image = await _createImage(width, height, color);
   if (cache) {
     _cache[cacheKey] = image.clone();
   }
   return image;
 });
 
-Future<ui.Image> _createImage(int width, int height) async {
-  final Completer<ui.Image> completer = Completer<ui.Image>();
-  ui.decodeImageFromPixels(
-    Uint8List.fromList(List<int>.filled(width * height * 4, 0)),
-    width,
-    height,
-    ui.PixelFormat.rgba8888,
-    (ui.Image image) {
-      completer.complete(image);
-    },
-  );
-  return completer.future;
+Future<ui.Image> _createImage(int width, int height, ui.Color color) {
+  final ui.PictureRecorder recorder = ui.PictureRecorder();
+  final ui.Canvas pictureCanvas = ui.Canvas(recorder);
+  pictureCanvas.drawColor(color, ui.BlendMode.src);
+  final ui.Picture picture = recorder.endRecording();
+  return picture.toImage(width, height);
 }
+
+/// Creates an arbitrarily sized image for testing.
+///
+/// If [color] isn't provided, fully transparent black color is used.
+/// If the [cache] parameter is set to true, the image will be cached for the
+/// rest of this suite. This is normally desirable, assuming a test suite uses
+/// images with the same dimensions in most tests, as it will save on memory
+/// usage and CPU time over the course of the suite. However, it should be
+/// avoided for images that are used only once in a test suite, especially if
+/// the image is large, as it will require holding on to the memory for that
+/// image for the duration of the suite.
+ui.Image createTestImageSync({
+  int width = 1,
+  int height = 1,
+  ui.Color color = const ui.Color(0x00000000),
+  bool cache = true,
+}) {
+  assert(width != null && width > 0);
+  assert(height != null && height > 0);
+  assert(cache != null);
+
+  final int cacheKey = Object.hash(width, height);
+  if (cache && _cache.containsKey(cacheKey)) {
+    return _cache[cacheKey]!.clone();
+  }
+
+  final ui.Image image = _createImage(width, height, color);
+  if (cache) {
+    _cache[cacheKey] = image.clone();
+  }
+  return image;
+}
+
+ui.Image _createImageSync(int width, int height, ui.Color color) {
+  final ui.PictureRecorder recorder = ui.PictureRecorder();
+  final ui.Canvas pictureCanvas = ui.Canvas(recorder);
+  pictureCanvas.drawColor(color, ui.BlendMode.src);
+  final ui.Picture picture = recorder.endRecording();
+  return picture.toImageSync(width, height);
+}
+
