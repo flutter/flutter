d2901e4363a223c168fdc7d567ddf0624537bf61
jonahwilliams <jonahwilliams@google.com>
2022-08-29 11:39:51 -0700

    ++

--- packages/flutter_tools/test/general.shard/android/gradle_errors_test.dart
+++ packages/flutter_tools/test/general.shard/android/gradle_errors_test.dart
@@ -11,7 +11,6 @@ import 'package:flutter_tools/src/base/file_system.dart';
 import 'package:flutter_tools/src/base/logger.dart';
 import 'package:flutter_tools/src/base/platform.dart';
 import 'package:flutter_tools/src/base/terminal.dart';
-// import 'package:flutter_tools/src/globals.dart' as globals;
 import 'package:flutter_tools/src/project.dart';
 import 'package:test/fake.dart';
 
