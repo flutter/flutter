384a205ea2ca412f4415675b58a092481758ca80
ColdPaleLight <zhangzhijian.123@bytedance.com>
2022-08-30 05:52:20 +0800

    [Impeller] Implement 'Picture::ToImage' (#35624)

--- impeller/aiks/picture.cc
+++ impeller/aiks/picture.cc
@@ -20,36 +20,56 @@ std::optional<Snapshot> Picture::Snapshot(AiksContext& context) {
     return std::nullopt;
   }
 
-  const auto translate = Matrix::MakeTranslation(-coverage->origin);
+  const auto translate = Matrix::MakeTranslation(-coverage.value().origin);
+  auto texture =
+      RenderToTexture(context, ISize(coverage.value().size), translate);
+  return impeller::Snapshot{
+      .texture = std::move(texture),
+      .transform = Matrix::MakeTranslation(coverage.value().origin)};
+}
+
+std::shared_ptr<Image> Picture::ToImage(AiksContext& context, ISize size) {
+  if (size.IsEmpty()) {
+    return nullptr;
+  }
+  auto texture = RenderToTexture(context, size);
+  return texture ? std::make_shared<Image>(texture) : nullptr;
+}
+
+std::shared_ptr<Texture> Picture::RenderToTexture(
+    AiksContext& context,
+    ISize size,
+    std::optional<const Matrix> translate) {
+  FML_DCHECK(!size.IsEmpty());
+
   pass->IterateAllEntities([&translate](auto& entity) -> bool {
-    entity.SetTransformation(translate * entity.GetTransformation());
+    auto matrix = translate.has_value()
+                      ? translate.value() * entity.GetTransformation()
+                      : entity.GetTransformation();
+    entity.SetTransformation(matrix);
     return true;
   });
 
   // This texture isn't host visible, but we might want to add host visible
   // features to Image someday.
-  auto target = RenderTarget::CreateOffscreen(
-      *context.GetContext(),
-      ISize(coverage->size.width, coverage->size.height));
+  auto target = RenderTarget::CreateOffscreen(*context.GetContext(), size);
   if (!target.IsValid()) {
     VALIDATION_LOG << "Could not create valid RenderTarget.";
-    return std::nullopt;
+    return nullptr;
   }
 
   if (!context.Render(*this, target)) {
     VALIDATION_LOG << "Could not render Picture to Texture.";
-    return std::nullopt;
+    return nullptr;
   }
 
   auto texture = target.GetRenderTargetTexture();
   if (!texture) {
     VALIDATION_LOG << "RenderTarget has no target texture.";
-    return std::nullopt;
+    return nullptr;
   }
 
-  return impeller::Snapshot{
-      .texture = std::move(texture),
-      .transform = translate.MakeTranslation(coverage->origin)};
-};
+  return texture;
+}
 
 }  // namespace impeller
