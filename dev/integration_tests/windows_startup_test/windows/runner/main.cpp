c60cf97c1904883accf43ad844d7ff11cf446846
Loïc Sharma <737941+loic-sharma@users.noreply.github.com>
2022-08-30 07:26:21 -0700

    [Windows] Hide app until first frame is drawn (#109816)

--- dev/integration_tests/windows_startup_test/windows/runner/main.cpp
+++ dev/integration_tests/windows_startup_test/windows/runner/main.cpp
@@ -5,6 +5,7 @@
 #include <flutter/dart_project.h>
 #include <flutter/flutter_view_controller.h>
 #include <windows.h>
+#include <windef.h>
 
 #include "flutter_window.h"
 #include "utils.h"
@@ -31,7 +32,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
   Win32Window::Size size(1280, 720);
-  if (!window.CreateAndShow(L"windows_startup_test", origin, size)) {
+  if (!window.Create(L"windows_startup_test", origin, size)) {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
