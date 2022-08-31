a07235c81a8858145cd8d63dbbf84e6964005877
Alexander Biggs <akbiggs@users.noreply.github.com>
2022-08-31 13:55:13 -0700

    [fuchsia] Workflow scripts. (flutter/engine#35822)
    - Adds a new script to build and copy Flutter on Fuchsia.
    - Rename sync_to_fuchsia.sh to checkout_fuchsia_revision.sh
      so it's more clear what git action is being performed (checkout
      instead of rebase). Update this script to `gclient sync -D` afterwards
      since not doing that will lead to subtle build issues. This script
      is only used by workflow docs, not infra, so renaming it is fine.
    
    Once submitted I will update the documentation.

--- engine/src/flutter/tools/fuchsia/devshell/checkout_fuchsia_revision.sh
+++ engine/src/flutter/tools/fuchsia/devshell/checkout_fuchsia_revision.sh
@@ -11,11 +11,24 @@
 ### for more details.
 ###
 ### Example:
-###   $ ./sync_to_fuchsia.sh
+###   $ ./checkout_fuchsia_revision.sh
 
+set -e  # Fail on any error.
 source "$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"/lib/vars.sh || exit $?
 
 ensure_fuchsia_dir
+ensure_engine_dir
+
+engine-info "Fetching upstream/main to make sure we recognize Fuchsia's version of Flutter Engine..."
+git -C "$ENGINE_DIR"/flutter fetch upstream
 
 fuchsia_flutter_git_revision="$(cat $FUCHSIA_DIR/integration/jiri.lock | grep -A 1 "\"package\": \"flutter/fuchsia\"" | grep "git_revision" | tr ":" "\n" | sed -n 3p | tr "\"" "\n" | sed -n 1p)"
-git checkout $fuchsia_flutter_git_revision
+engine-info "Checking out Fuchsia's revision of Flutter Engine ($fuchsia_flutter_git_revision)..."
+git -C "$ENGINE_DIR"/flutter checkout $fuchsia_flutter_git_revision
+
+engine-info "Syncing the Flutter Engine dependencies..."
+pushd $ENGINE_DIR
+gclient sync -D
+popd
+
+echo "Done. You're now working on Fuchsia's version of Flutter Engine ($fuchsia_flutter_git_revision)."
