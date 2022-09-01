cb5952ca89ed488be3c0378f7894e3d395a46a87
Polina Cherkasova <polina.c@live.com>
2022-08-31 17:30:11 -0700

    Update memory_allocations.dart

--- packages/flutter/lib/src/foundation/memory_allocations.dart
+++ packages/flutter/lib/src/foundation/memory_allocations.dart
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'dart:ui' as ui;
+// import 'dart:ui' as ui;
 
 import 'assertions.dart';
 import 'diagnostics.dart';
