f64fe164a8ec30d800112091e7b3a676ab01dbcf
camsim99 <camillesimon90@gmail.com>
2022-09-02 09:35:50 -0700

    Test

--- dev/integration_tests/spell_check/integration_test/integration_test.dart
+++ dev/integration_tests/spell_check/integration_test/integration_test.dart
@@ -147,7 +147,8 @@ Future<void> main() async {
 
     await tester.pumpWidget(const MyApp());
 
-    await tester.enterText(find.byType(EditableText), 'Hey jijiijijij! Hey!');
+    await tester.enterText(find.byType(EditableText), 'Hey cfabiueqqocnakoef! Hey!');
+    await tester.pumpAndSettle();
     await tester.pumpAndSettle();
 
     final RenderEditable renderEditable = findRenderEditable(tester, EditableText);
@@ -157,7 +158,7 @@ Future<void> main() async {
         style: style,
         children: <TextSpan>[
           TextSpan(style: style, text: 'Hey '),
-          TextSpan(style: misspelledTextStyle, text: 'jijiijijij'),
+          TextSpan(style: misspelledTextStyle, text: 'cfabiueqqocnakoef'),
           TextSpan(style: style, text: '! Hey!'),
         ]);
 
