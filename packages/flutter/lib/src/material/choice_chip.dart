5cfb9e34d4fde92a7fb6166069335cecc3ebddfc
Samuel <samuelong135@gmail.com>
2022-09-03 17:16:01 +0800

    added iconTheme to RawChip property

--- packages/flutter/lib/src/material/choice_chip.dart
+++ packages/flutter/lib/src/material/choice_chip.dart
@@ -178,6 +178,7 @@ class ChoiceChip extends StatelessWidget
       surfaceTintColor: surfaceTintColor,
       selectedShadowColor: selectedShadowColor,
       avatarBorder: avatarBorder,
+      iconTheme: iconTheme,
     );
   }
 }
