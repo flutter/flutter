5f6667140942afd9111f797ceed7f96339f792cb
Samuel <samuelong135@gmail.com>
2022-09-03 16:43:58 +0800

    added tests

--- packages/flutter/test/material/chip_choice_test.dart
+++ packages/flutter/test/material/chip_choice_test.dart
@@ -120,4 +120,11 @@ void main() {
     await tester.pumpWidget(wrapForChip(child: const ChoiceChip(label: label, selected: false, clipBehavior: Clip.antiAlias)));
     checkChipMaterialClipBehavior(tester, Clip.antiAlias);
   });
+  testWidgets('ChoiceChip passes iconTheme property to RawChip', (WidgetTester tester) async {
+    const IconThemeData iconTheme = IconThemeData(color: Colors.red);
+    await tester.pumpWidget(wrapForChip(child: const ChoiceChip(label: Text('Test'), selected: true, iconTheme: iconTheme,)));
+
+    final RawChip rawChip = tester.widget(find.byType(RawChip));
+    expect(rawChip.iconTheme, iconTheme);
+  });
 }
