6076d6c06df941e74b5f9899b997953334de09d3
a-wallen <stephenwallen@google.com>
2022-09-05 10:19:46 -0700

    Add outlet to platform view to change increment text

--- examples/platform_view/macos/Runner/PlatformViewController.swift
+++ examples/platform_view/macos/Runner/PlatformViewController.swift
@@ -10,9 +10,17 @@ import Cocoa
 class PlatformViewController: NSViewController {
     var count: Int = 0
 
+    @IBOutlet weak var label: NSTextField!
+
+    var labelText: String {
+      get {
+        return "Button tapped \(self.count) time\(self.count != 1 ? "s" : "")."
+      }
+    }
+
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do view setup here.
+      super.viewDidLoad()
+      self.label.stringValue = labelText
     }
 
     @IBAction func pop(_ sender: Any) {
@@ -21,5 +29,6 @@ class PlatformViewController: NSViewController {
 
     @IBAction func increment(_ sender: Any) {
       self.count += 1
+      self.label.stringValue = labelText
     }
 }
