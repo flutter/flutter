4494fefa7e15792f0a8062a23791541e979252bc
a-wallen <stephenwallen@google.com>
2022-09-05 10:13:02 -0700

    Add action binding to handle increment

--- examples/platform_view/macos/Runner/PlatformViewController.swift
+++ examples/platform_view/macos/Runner/PlatformViewController.swift
@@ -8,10 +8,14 @@
 import Cocoa
 
 class PlatformViewController: NSViewController {
+    var count: Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do view setup here.
     }
     
+    @IBAction func increment(_ sender: Any) {
+      self.count += 1
+    }
 }
