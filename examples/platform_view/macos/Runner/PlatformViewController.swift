eac4587c6d2f5a00d76aca64926ae2c288aefbb3
a-wallen <stephenwallen@google.com>
2022-09-05 10:04:07 -0700

    Create CustomView, “PlatformView”

---
+++ examples/platform_view/macos/Runner/PlatformViewController.swift
@@ -0,0 +1,17 @@
+//
+//  PlatformViewController.swift
+//  Runner
+//
+//  Created by Alex Wallen on 9/5/22.
+//
+
+import Cocoa
+
+class PlatformViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
