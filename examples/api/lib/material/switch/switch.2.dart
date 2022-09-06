82ec2d626cb3ae4b3117540b08ecc9052114f2b9
Qun Cheng <quncheng@google.com>
2022-09-06 12:59:57 -0700

    Modified comments

--- examples/api/lib/material/switch/switch.2.dart
+++ examples/api/lib/material/switch/switch.2.dart
@@ -48,7 +48,7 @@ class _SwitchExampleState extends State<SwitchExample> {
   );
 
   final MaterialStateProperty<Icon?> thumbIcon = MaterialStateProperty.resolveWith<Icon?>((Set<MaterialState> states) {
-    // Thumb image when the switch is selected.
+    // Thumb icon when the switch is selected.
     if (states.contains(MaterialState.selected)) {
       return const Icon(Icons.check);
     }
