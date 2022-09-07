043ffc063dc0497d2d997c7408fa00fba3ea20d9
Jason Simmons <jason-simmons@users.noreply.github.com>
2022-09-07 11:27:16 -0700

    Roll to a patched version of libcxxabi that is compatible with the latest Clang Mac/iOS toolchain (flutter/engine#35873)

--- engine/src/flutter/testing/symbols/verify_exported.dart
+++ engine/src/flutter/testing/symbols/verify_exported.dart
@@ -81,7 +81,9 @@ int _checkIos(String outPath, String nmPath, Iterable<String> builds) {
     final Iterable<NmEntry> unexpectedEntries = NmEntry.parse(nmResult.stdout as String).where((NmEntry entry) {
       return !(((entry.type == '(__DATA,__common)' || entry.type == '(__DATA,__const)') && entry.name.startsWith('_Flutter'))
           || (entry.type == '(__DATA,__objc_data)'
-              && (entry.name.startsWith(r'_OBJC_METACLASS_$_Flutter') || entry.name.startsWith(r'_OBJC_CLASS_$_Flutter'))));
+              && (entry.name.startsWith(r'_OBJC_METACLASS_$_Flutter') || entry.name.startsWith(r'_OBJC_CLASS_$_Flutter')))
+          // TODO(107887): This should not be neccesary after bitcode support is removed from the engine.
+          || (entry.type == '(__TEXT,__text)' && entry.name == '___gxx_personality_v0'));
     });
     if (unexpectedEntries.isNotEmpty) {
       print('ERROR: $libFlutter exports unexpected symbols:');
