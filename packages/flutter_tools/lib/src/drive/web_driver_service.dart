716d79269d3eb33c150adf8ccec8c66d9006f203
Liam Appelbe <liama@google.com>
2022-09-07 12:51:56 -0700

    Fix another cast error (copied from #110711)

--- packages/flutter_tools/lib/src/drive/web_driver_service.dart
+++ packages/flutter_tools/lib/src/drive/web_driver_service.dart
@@ -96,15 +96,15 @@ class WebDriverService extends DriverService {
     );
 
     bool isAppStarted = false;
-    await Future.any<Object>(<Future<Object>>[
+    await Future.any(<Future<Object?>>[
       runFuture.then((int? result) {
         _runResult = result;
         return null;
-      } as FutureOr<Object> Function(int?)),
+      }),
       appStartedCompleter.future.then((_) {
         isAppStarted = true;
         return null;
-      } as FutureOr<Object> Function(void)),
+      }),
     ]);
 
     if (_runResult != null) {
