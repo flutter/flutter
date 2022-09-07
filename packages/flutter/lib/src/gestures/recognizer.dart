8fa394d0e6accf12f2dc573418d49549cec93476
Renzo Olivares <roliv@google.com>
2022-09-07 12:26:33 -0700

    remove erroneous log

--- packages/flutter/lib/src/gestures/recognizer.dart
+++ packages/flutter/lib/src/gestures/recognizer.dart
@@ -443,7 +443,6 @@ abstract class OneSequenceGestureRecognizer extends GestureRecognizer {
       GestureBinding.instance.pointerRouter.removeRoute(pointer, handleEvent);
       _trackedPointers.remove(pointer);
       if (_trackedPointers.isEmpty) {
-        print('tracked pointers empty');
         didStopTrackingLastPointer(pointer);
       }
     }
