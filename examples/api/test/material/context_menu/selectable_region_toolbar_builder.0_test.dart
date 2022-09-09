a8a972a4db82e5b10ad8e9b465760d0cde1dd904
Justin McCandless <jmccandless@google.com>
2022-09-08 17:04:50 -0700

    Fix example tests

--- examples/api/test/material/context_menu/selectable_region_toolbar_builder.0_test.dart
+++ examples/api/test/material/context_menu/selectable_region_toolbar_builder.0_test.dart
@@ -16,7 +16,7 @@ void main() {
     // Allow the selection overlay geometry to be created.
     await tester.pump();
 
-    expect(find.byType(AdaptiveTextSelectionToolbar), findsNothing);
+    expect(find.byType(AdaptiveTextSelectionToolbarButtonItems), findsNothing);
 
     // Right clicking the Text in the SelectionArea shows the custom context
     // menu.
@@ -29,13 +29,13 @@ void main() {
     await gesture.up();
     await tester.pumpAndSettle();
 
-    expect(find.byType(AdaptiveTextSelectionToolbar), findsOneWidget);
+    expect(find.byType(AdaptiveTextSelectionToolbarButtonItems), findsOneWidget);
     expect(find.text('Print'), findsOneWidget);
 
     // Tap to dismiss.
     await tester.tapAt(tester.getCenter(find.byType(Scaffold)));
     await tester.pumpAndSettle();
 
-    expect(find.byType(AdaptiveTextSelectionToolbar), findsNothing);
+    expect(find.byType(AdaptiveTextSelectionToolbarButtonItems), findsNothing);
   });
 }
