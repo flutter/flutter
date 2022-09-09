2ec154200e69d97a6b229b8b21365128013af48c
stuartmorgan <stuartmorgan@google.com>
2022-09-09 07:40:30 -0400

    Remove context assertion from Android platform views using Virtual Display (flutter/engine#35997)

--- engine/src/flutter/testing/scenario_app/android/app/src/main/java/dev/flutter/scenarios/PlatformViewsActivity.java
+++ engine/src/flutter/testing/scenario_app/android/app/src/main/java/dev/flutter/scenarios/PlatformViewsActivity.java
@@ -8,8 +8,12 @@ import androidx.annotation.NonNull;
 import io.flutter.embedding.engine.FlutterEngine;
 
 public class PlatformViewsActivity extends TestActivity {
+  // WARNING: These strings must all be exactly the same length to avoid
+  // breaking the 'create' method's manual encoding in the test. See the
+  // TODO(stuartmorgan) about encoding alignment in platform_view.dart
   public static final String TEXT_VIEW_PV = "scenarios/textPlatformView";
   public static final String SURFACE_VIEW_PV = "scenarios/surfacePlatformV";
+  public static final String SURFACE_VIEW_BAD_CONTEXT_PV = "scenarios/surfaceVBadCntxt";
   public static final String TEXTURE_VIEW_PV = "scenarios/texturePlatformV";
 
   @Override
@@ -23,7 +27,12 @@ public class PlatformViewsActivity extends TestActivity {
     flutterEngine
         .getPlatformViewsController()
         .getRegistry()
-        .registerViewFactory(SURFACE_VIEW_PV, new SurfacePlatformViewFactory());
+        .registerViewFactory(SURFACE_VIEW_PV, new SurfacePlatformViewFactory(true));
+
+    flutterEngine
+        .getPlatformViewsController()
+        .getRegistry()
+        .registerViewFactory(SURFACE_VIEW_BAD_CONTEXT_PV, new SurfacePlatformViewFactory(false));
 
     flutterEngine
         .getPlatformViewsController()
--- engine/src/flutter/testing/scenario_app/lib/src/platform_view.dart
+++ engine/src/flutter/testing/scenario_app/lib/src/platform_view.dart
@@ -1144,6 +1144,11 @@ void addPlatformView(
       valueString,
       'width'.length,
       ...utf8.encode('width'),
+      // This is missing the 64-bit boundary alignment, making the entire
+      // message encoding fragile to changes before this point. Do not add new
+      // variable-length values such as strings before this point.
+      // TODO(stuartmorgan): Fix this to use the actual encoding logic,
+      // including alignment: https://github.com/flutter/flutter/issues/111188
       valueFloat64,
       ..._to64(width),
       valueString,
