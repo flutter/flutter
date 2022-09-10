c403f17592d84c146559ac0668095a6d14a03fab
a-wallen <stephenwallen@google.com>
2022-09-09 17:29:30 -0700

    Handle FlutterViewControllerSegue - (sets message channels for bi-directional communication). Native View side is visibly working

--- examples/flutter_view/macos/Runner/MainViewController.swift
+++ examples/flutter_view/macos/Runner/MainViewController.swift
@@ -7,12 +7,17 @@
 
 import Foundation
 import AppKit
+import FlutterMacOS
 
 class MainViewController: NSViewController, NativeViewControllerDelegate {
 
+  static let emptyString: String = ""
   static let ping: String = "ping"
+  static let channel: String = "increment"
 
   var nativeViewController: NativeViewController?
+  var flutterViewController: FlutterViewController?
+  var messageChannel: FlutterBasicMessageChannel?
 
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -23,6 +28,26 @@ class MainViewController: NSViewController, NativeViewControllerDelegate {
       self.nativeViewController = segue.destinationController as? NativeViewController
       self.nativeViewController?.delegate = self
     }
+
+    if segue.identifier == "FlutterViewControllerSegue" {
+      self.flutterViewController = segue.destinationController as? FlutterViewController
+
+      RegisterMethodChannel(registry: self.flutterViewController!)
+
+      weak var weakSelf = self
+      messageChannel?.setMessageHandler({ (message, reply) in
+        weakSelf?.nativeViewController?.didReceiveIncrement()
+        reply(MainViewController.emptyString)
+      })
+    }
+  }
+
+  func RegisterMethodChannel(registry: FlutterPluginRegistry) {
+    let registrar = registry.registrar(forPlugin: "")
+    messageChannel = FlutterBasicMessageChannel(
+      name: MainViewController.channel,
+      binaryMessenger: registrar.messenger,
+      codec: FlutterStringCodec.sharedInstance())
   }
 
   func didTapIncrementButton() {
