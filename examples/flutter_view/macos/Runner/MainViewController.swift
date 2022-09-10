789a106ae4eb5425f2026c52ba2c6a5c8554c9f5
a-wallen <stephenwallen@google.com>
2022-09-09 17:21:19 -0700

    Create code behind MainViewController

---
+++ examples/flutter_view/macos/Runner/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  Main.swift
+//  Runner
+//
+//  Created by Alex Wallen on 9/8/22.
+//
+
+import Foundation
+import AppKit
+
+class MainViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+
+}
+
+
