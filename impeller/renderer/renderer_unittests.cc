29f9b7ead27df111821ba4d1b7efbf4b5e794ee5
Dan Field <dnfield@google.com>
2022-09-13 09:51:59 -0700

    [Impeller] Refactor compute unit tests to own TU (#36114)

--- impeller/renderer/renderer_unittests.cc
+++ impeller/renderer/renderer_unittests.cc
@@ -15,9 +15,6 @@
 #include "impeller/fixtures/instanced_draw.vert.h"
 #include "impeller/fixtures/mipmaps.frag.h"
 #include "impeller/fixtures/mipmaps.vert.h"
-#if IMPELLER_ENABLE_METAL || IMPELLER_ENABLE_VULKAN
-#include "impeller/fixtures/sample.comp.h"
-#endif
 #include "impeller/fixtures/test_texture.frag.h"
 #include "impeller/fixtures/test_texture.vert.h"
 #include "impeller/geometry/path_builder.h"
@@ -738,51 +735,6 @@ TEST_P(RendererTest, TheImpeller) {
   };
   OpenPlaygroundHere(callback);
 }
-
-#if IMPELLER_ENABLE_METAL || IMPELLER_ENABLE_VULKAN
-TEST_P(RendererTest, CanCreateComputePass) {
-  if (GetParam() == PlaygroundBackend::kOpenGLES) {
-    GTEST_SKIP_("Compute is not supported on GL.");
-  }
-  if (GetParam() == PlaygroundBackend::kVulkan) {
-    GTEST_SKIP_("Compute is not supported on Vulkan yet.");
-  }
-
-  using CS = SampleComputeShader;
-  auto context = GetContext();
-  ASSERT_TRUE(context);
-  using SamplePipelineBuilder = ComputePipelineBuilder<CS>;
-  auto pipeline_desc =
-      SamplePipelineBuilder::MakeDefaultPipelineDescriptor(*context);
-  ASSERT_TRUE(pipeline_desc.has_value());
-  auto compute_pipeline =
-      context->GetPipelineLibrary()->GetPipeline(pipeline_desc).get();
-  ASSERT_TRUE(compute_pipeline);
-
-  auto cmd_buffer = context->CreateCommandBuffer();
-  auto pass = cmd_buffer->CreateComputePass();
-  ASSERT_TRUE(pass && pass->IsValid());
-
-  ComputeCommand cmd;
-  cmd.label = "Compute";
-  cmd.pipeline = compute_pipeline;
-
-  std::vector<CS::Input0> input_0;
-  std::vector<CS::Input1> input_1;
-  input_0.push_back(CS::Input0{Vector4(2.0, 3.0, 4.0, 5.0)});
-  input_1.push_back(CS::Input1{Vector4(6.0, 7.0, 8.0, 9.0)});
-
-  std::vector<CS::Output> output(5);
-  CS::BindInput0(cmd,
-                 pass->GetTransientsBuffer().EmplaceStorageBuffer(input_0));
-  CS::BindInput1(cmd,
-                 pass->GetTransientsBuffer().EmplaceStorageBuffer(input_1));
-  CS::BindOutput(cmd, pass->GetTransientsBuffer().EmplaceStorageBuffer(output));
-
-  ASSERT_TRUE(pass->AddCommand(std::move(cmd)));
-  ASSERT_TRUE(pass->EncodeCommands());
-}
-#endif
 
 }  // namespace testing
 }  // namespace impeller
