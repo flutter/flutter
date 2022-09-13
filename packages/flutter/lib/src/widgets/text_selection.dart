f2b36efe226d72c1f9bb1818de895f71dd679a82
Renzo Olivares <roliv@google.com>
2022-09-13 10:31:24 -0700

    fix text field focus tests

--- packages/flutter/lib/src/widgets/text_selection.dart
+++ packages/flutter/lib/src/widgets/text_selection.dart
@@ -2742,7 +2742,7 @@ class _TextSelectionGestureDetectorState extends State<TextSelectionGestureDetec
   }
 
   void _handleTapUp(TapUpDetails details, TapStatus status) {
-    if (status.consecutiveTapCount == 1) {
+    if (status.consecutiveTapCount.isOdd) {
       widget.onSingleTapUp?.call(details, status);
     }
     _isDoubleTap = false;
