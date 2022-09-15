5962b1a9d24294bafd383ee073bc66061dc53f1b
Renzo Olivares <roliv@google.com>
2022-09-15 01:47:14 -0700

    add tests for drag on touch devices

--- packages/flutter/lib/src/widgets/text_selection.dart
+++ packages/flutter/lib/src/widgets/text_selection.dart
@@ -2170,7 +2170,7 @@ class TextSelectionGestureDetectorBuilder {
             case PointerDeviceKind.touch:
             case PointerDeviceKind.unknown:
               if(renderEditable.hasFocus
-                  && editableText.textEditingValue.selection.isCollapsed
+                  && _dragStartSelection!.isCollapsed
                   && _lastTapWasOnSelection(dragStartGlobalPosition, _dragStartSelection)
               ) {
                 return renderEditable.selectPositionAt(
