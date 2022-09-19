8c35f585a41dfe4b91991ef0827528603a485c6b
Justin McCandless <jmccandless@google.com>
2022-09-19 11:55:44 -0700

    Clean up test

--- packages/flutter/test/services/scribble_test.dart
+++ packages/flutter/test/services/scribble_test.dart
@@ -10,192 +10,167 @@ import 'text_input_utils.dart';
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
 
-  group('Scribble interactions', () {
-    tearDown(() {
-      // TODO(justinmc): Remove.
-      TextInputConnection.debugResetId();
+  test('ScribbleClient showToolbar method is called', () async {
+    final FakeScribbleElement targetElement = FakeScribbleElement(elementIdentifier: 'target');
+    Scribble.client = targetElement;
+
+    expect(targetElement.latestMethodCall, isEmpty);
+
+    // Send showToolbar message.
+    final ByteData? messageBytes =
+        const JSONMessageCodec().encodeMessage(<String, dynamic>{
+      'args': <dynamic>[1, 0, 1],
+      'method': 'Scribble.showToolbar',
     });
+    await ServicesBinding.instance.defaultBinaryMessenger.handlePlatformMessage(
+      'flutter/scribble',
+      messageBytes,
+      (ByteData? _) {},
+    );
 
-    test('ScribbleClient showToolbar method is called', () async {
-      final FakeScribbleElement targetElement = FakeScribbleElement(elementIdentifier: 'target');
-      Scribble.client = targetElement;
+    expect(targetElement.latestMethodCall, 'showToolbar');
+  });
 
-      expect(targetElement.latestMethodCall, isEmpty);
+  test('ScribbleClient removeTextPlaceholder method is called', () async {
+    final FakeScribbleElement targetElement = FakeScribbleElement(elementIdentifier: 'target');
+    Scribble.client = targetElement;
 
-      // Send showToolbar message.
-      final ByteData? messageBytes =
-          const JSONMessageCodec().encodeMessage(<String, dynamic>{
-        'args': <dynamic>[1, 0, 1],
-        'method': 'Scribble.showToolbar',
-      });
-      await ServicesBinding.instance.defaultBinaryMessenger.handlePlatformMessage(
-        'flutter/scribble',
-        messageBytes,
-        (ByteData? _) {},
-      );
+    expect(targetElement.latestMethodCall, isEmpty);
 
-      expect(targetElement.latestMethodCall, 'showToolbar');
+    // Send removeTextPlaceholder message.
+    final ByteData? messageBytes =
+        const JSONMessageCodec().encodeMessage(<String, dynamic>{
+      'args': <dynamic>[1, 0, 1],
+      'method': 'Scribble.removeTextPlaceholder',
     });
+    await ServicesBinding.instance.defaultBinaryMessenger.handlePlatformMessage(
+      'flutter/scribble',
+      messageBytes,
+      (ByteData? _) {},
+    );
 
-    test('ScribbleClient removeTextPlaceholder method is called', () async {
-      final FakeScribbleElement targetElement = FakeScribbleElement(elementIdentifier: 'target');
-      Scribble.client = targetElement;
+    expect(targetElement.latestMethodCall, 'removeTextPlaceholder');
+  });
 
-      expect(targetElement.latestMethodCall, isEmpty);
+  test('ScribbleClient insertTextPlaceholder method is called', () async {
+    final FakeScribbleElement targetElement = FakeScribbleElement(elementIdentifier: 'target');
+    Scribble.client = targetElement;
 
-      // Send removeTextPlaceholder message.
-      final ByteData? messageBytes =
-          const JSONMessageCodec().encodeMessage(<String, dynamic>{
-        'args': <dynamic>[1, 0, 1],
-        'method': 'Scribble.removeTextPlaceholder',
-      });
-      await ServicesBinding.instance.defaultBinaryMessenger.handlePlatformMessage(
-        'flutter/scribble',
-        messageBytes,
-        (ByteData? _) {},
-      );
+    expect(targetElement.latestMethodCall, isEmpty);
 
-      expect(targetElement.latestMethodCall, 'removeTextPlaceholder');
+    // Send insertTextPlaceholder message.
+    final ByteData? messageBytes =
+        const JSONMessageCodec().encodeMessage(<String, dynamic>{
+      'args': <dynamic>[1, 0, 1],
+      'method': 'Scribble.insertTextPlaceholder',
     });
+    await ServicesBinding.instance.defaultBinaryMessenger.handlePlatformMessage(
+      'flutter/scribble',
+      messageBytes,
+      (ByteData? _) {},
+    );
 
-    test('ScribbleClient insertTextPlaceholder method is called', () async {
-      final FakeScribbleElement targetElement = FakeScribbleElement(elementIdentifier: 'target');
-      Scribble.client = targetElement;
+    expect(targetElement.latestMethodCall, 'insertTextPlaceholder');
+  });
 
-      expect(targetElement.latestMethodCall, isEmpty);
+  test('ScribbleClient scribbleInteractionBegan and scribbleInteractionFinished', () async {
+    Scribble.ensureInitialized();
 
-      // Send insertTextPlaceholder message.
-      final ByteData? messageBytes =
-          const JSONMessageCodec().encodeMessage(<String, dynamic>{
-        'args': <dynamic>[1, 0, 1],
-        'method': 'Scribble.insertTextPlaceholder',
-      });
-      await ServicesBinding.instance.defaultBinaryMessenger.handlePlatformMessage(
-        'flutter/scribble',
-        messageBytes,
-        (ByteData? _) {},
-      );
+    expect(Scribble.scribbleInProgress, isFalse);
 
-      expect(targetElement.latestMethodCall, 'insertTextPlaceholder');
+    // Send scribbleInteractionBegan message.
+    ByteData? messageBytes =
+        const JSONMessageCodec().encodeMessage(<String, dynamic>{
+      'args': <dynamic>[1, 0, 1],
+      'method': 'Scribble.scribbleInteractionBegan',
     });
+    await ServicesBinding.instance.defaultBinaryMessenger.handlePlatformMessage(
+      'flutter/scribble',
+      messageBytes,
+      (ByteData? _) {},
+    );
 
-    test('ScribbleClient scribbleInteractionBegan and scribbleInteractionFinished', () async {
-      // Assemble a TextInputConnection so we can verify its change in state.
-      //final FakeScribbleClient client = FakeScribbleClient();
-      //const TextInputConfiguration configuration = TextInputConfiguration();
-      //final TextInputConnection connection = TextInput.attach(client, configuration);
-      Scribble.ensureInitialized();
+    expect(Scribble.scribbleInProgress, isTrue);
 
-      expect(Scribble.scribbleInProgress, isFalse);
-
-      // Send scribbleInteractionBegan message.
-      ByteData? messageBytes =
-          const JSONMessageCodec().encodeMessage(<String, dynamic>{
-        'args': <dynamic>[1, 0, 1],
-        'method': 'Scribble.scribbleInteractionBegan',
-      });
-      await ServicesBinding.instance.defaultBinaryMessenger.handlePlatformMessage(
-        'flutter/scribble',
-        messageBytes,
-        (ByteData? _) {},
-      );
-
-      expect(Scribble.scribbleInProgress, isTrue);
-
-      // Send scribbleInteractionFinished message.
-      messageBytes =
-          const JSONMessageCodec().encodeMessage(<String, dynamic>{
-        'args': <dynamic>[1, 0, 1],
-        'method': 'Scribble.scribbleInteractionFinished',
-      });
-      await ServicesBinding.instance.defaultBinaryMessenger.handlePlatformMessage(
-        'flutter/scribble',
-        messageBytes,
-        (ByteData? _) {},
-      );
-
-      expect(Scribble.scribbleInProgress, isFalse);
+    // Send scribbleInteractionFinished message.
+    messageBytes =
+        const JSONMessageCodec().encodeMessage(<String, dynamic>{
+      'args': <dynamic>[1, 0, 1],
+      'method': 'Scribble.scribbleInteractionFinished',
     });
+    await ServicesBinding.instance.defaultBinaryMessenger.handlePlatformMessage(
+      'flutter/scribble',
+      messageBytes,
+      (ByteData? _) {},
+    );
 
-    test('ScribbleClient focusElement', () async {
-      // Assemble a TextInputConnection so we can verify its change in state.
-      /*
-      final FakeScribbleClient client = FakeScribbleClient(TextEditingValue.empty);
-      const TextInputConfiguration configuration = TextInputConfiguration();
-      TextInput.attach(client, configuration);
-      */
+    expect(Scribble.scribbleInProgress, isFalse);
+  });
 
-      final FakeScribbleElement targetElement = FakeScribbleElement(elementIdentifier: 'target');
-      Scribble.registerScribbleElement(targetElement.elementIdentifier, targetElement);
-      final FakeScribbleElement otherElement = FakeScribbleElement(elementIdentifier: 'other');
-      Scribble.registerScribbleElement(otherElement.elementIdentifier, otherElement);
+  test('ScribbleClient focusElement', () async {
+    final FakeScribbleElement targetElement = FakeScribbleElement(elementIdentifier: 'target');
+    Scribble.registerScribbleElement(targetElement.elementIdentifier, targetElement);
+    final FakeScribbleElement otherElement = FakeScribbleElement(elementIdentifier: 'other');
+    Scribble.registerScribbleElement(otherElement.elementIdentifier, otherElement);
 
-      expect(targetElement.latestMethodCall, isEmpty);
-      expect(otherElement.latestMethodCall, isEmpty);
+    expect(targetElement.latestMethodCall, isEmpty);
+    expect(otherElement.latestMethodCall, isEmpty);
 
-      // Send focusElement message.
-      final ByteData? messageBytes =
-          const JSONMessageCodec().encodeMessage(<String, dynamic>{
-        'args': <dynamic>[targetElement.elementIdentifier, 0.0, 0.0],
-        'method': 'Scribble.focusElement',
-      });
-      await ServicesBinding.instance.defaultBinaryMessenger.handlePlatformMessage(
-        'flutter/scribble',
-        messageBytes,
-        (ByteData? _) {},
-      );
-
-      Scribble.unregisterScribbleElement(targetElement.elementIdentifier);
-      Scribble.unregisterScribbleElement(otherElement.elementIdentifier);
-
-      expect(targetElement.latestMethodCall, 'onScribbleFocus');
-      expect(otherElement.latestMethodCall, isEmpty);
+    // Send focusElement message.
+    final ByteData? messageBytes =
+        const JSONMessageCodec().encodeMessage(<String, dynamic>{
+      'args': <dynamic>[targetElement.elementIdentifier, 0.0, 0.0],
+      'method': 'Scribble.focusElement',
     });
+    await ServicesBinding.instance.defaultBinaryMessenger.handlePlatformMessage(
+      'flutter/scribble',
+      messageBytes,
+      (ByteData? _) {},
+    );
 
-    test('ScribbleClient requestElementsInRect', () async {
-      // Assemble a TextInputConnection so we can verify its change in state.
-      /*
-      final FakeScribbleClient client = FakeScribbleClient(TextEditingValue.empty);
-      const TextInputConfiguration configuration = TextInputConfiguration();
-      TextInput.attach(client, configuration);
-      */
+    Scribble.unregisterScribbleElement(targetElement.elementIdentifier);
+    Scribble.unregisterScribbleElement(otherElement.elementIdentifier);
 
-      final List<FakeScribbleElement> targetElements = <FakeScribbleElement>[
-        FakeScribbleElement(elementIdentifier: 'target1', bounds: const Rect.fromLTWH(0.0, 0.0, 100.0, 100.0)),
-        FakeScribbleElement(elementIdentifier: 'target2', bounds: const Rect.fromLTWH(0.0, 100.0, 100.0, 100.0)),
-      ];
-      final List<FakeScribbleElement> otherElements = <FakeScribbleElement>[
-        FakeScribbleElement(elementIdentifier: 'other1', bounds: const Rect.fromLTWH(100.0, 0.0, 100.0, 100.0)),
-        FakeScribbleElement(elementIdentifier: 'other2', bounds: const Rect.fromLTWH(100.0, 100.0, 100.0, 100.0)),
-      ];
+    expect(targetElement.latestMethodCall, 'onScribbleFocus');
+    expect(otherElement.latestMethodCall, isEmpty);
+  });
 
-      void registerElements(FakeScribbleElement element) => Scribble.registerScribbleElement(element.elementIdentifier, element);
-      void unregisterElements(FakeScribbleElement element) => Scribble.unregisterScribbleElement(element.elementIdentifier);
+  test('ScribbleClient requestElementsInRect', () async {
+    final List<FakeScribbleElement> targetElements = <FakeScribbleElement>[
+      FakeScribbleElement(elementIdentifier: 'target1', bounds: const Rect.fromLTWH(0.0, 0.0, 100.0, 100.0)),
+      FakeScribbleElement(elementIdentifier: 'target2', bounds: const Rect.fromLTWH(0.0, 100.0, 100.0, 100.0)),
+    ];
+    final List<FakeScribbleElement> otherElements = <FakeScribbleElement>[
+      FakeScribbleElement(elementIdentifier: 'other1', bounds: const Rect.fromLTWH(100.0, 0.0, 100.0, 100.0)),
+      FakeScribbleElement(elementIdentifier: 'other2', bounds: const Rect.fromLTWH(100.0, 100.0, 100.0, 100.0)),
+    ];
 
-      <FakeScribbleElement>[...targetElements, ...otherElements].forEach(registerElements);
+    void registerElements(FakeScribbleElement element) => Scribble.registerScribbleElement(element.elementIdentifier, element);
+    void unregisterElements(FakeScribbleElement element) => Scribble.unregisterScribbleElement(element.elementIdentifier);
 
-      // Send requestElementsInRect message.
-      final ByteData? messageBytes =
-          const JSONMessageCodec().encodeMessage(<String, dynamic>{
-        'args': <dynamic>[0.0, 50.0, 50.0, 100.0],
-        'method': 'Scribble.requestElementsInRect',
-      });
-      ByteData? responseBytes;
-      await ServicesBinding.instance.defaultBinaryMessenger.handlePlatformMessage(
-        'flutter/scribble',
-        messageBytes,
-        (ByteData? response) {
-          responseBytes = response;
-        },
-      );
+    <FakeScribbleElement>[...targetElements, ...otherElements].forEach(registerElements);
 
-      <FakeScribbleElement>[...targetElements, ...otherElements].forEach(unregisterElements);
-
-      final List<List<dynamic>> responses = (const JSONMessageCodec().decodeMessage(responseBytes) as List<dynamic>).cast<List<dynamic>>();
-      expect(responses.first.length, 2);
-      expect(responses.first.first, containsAllInOrder(<dynamic>[targetElements.first.elementIdentifier, 0.0, 0.0, 100.0, 100.0]));
-      expect(responses.first.last, containsAllInOrder(<dynamic>[targetElements.last.elementIdentifier, 0.0, 100.0, 100.0, 100.0]));
+    // Send requestElementsInRect message.
+    final ByteData? messageBytes =
+        const JSONMessageCodec().encodeMessage(<String, dynamic>{
+      'args': <dynamic>[0.0, 50.0, 50.0, 100.0],
+      'method': 'Scribble.requestElementsInRect',
     });
+    ByteData? responseBytes;
+    await ServicesBinding.instance.defaultBinaryMessenger.handlePlatformMessage(
+      'flutter/scribble',
+      messageBytes,
+      (ByteData? response) {
+        responseBytes = response;
+      },
+    );
+
+    <FakeScribbleElement>[...targetElements, ...otherElements].forEach(unregisterElements);
+
+    final List<List<dynamic>> responses = (const JSONMessageCodec().decodeMessage(responseBytes) as List<dynamic>).cast<List<dynamic>>();
+    expect(responses.first.length, 2);
+    expect(responses.first.first, containsAllInOrder(<dynamic>[targetElements.first.elementIdentifier, 0.0, 0.0, 100.0, 100.0]));
+    expect(responses.first.last, containsAllInOrder(<dynamic>[targetElements.last.elementIdentifier, 0.0, 100.0, 100.0, 100.0]));
   });
 }
 
