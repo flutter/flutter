aefebd8e279196409ea8afb0d47c24768c409d97
Liam Appelbe <liama@google.com>
2022-09-21 10:53:57 -0700

    Temporarily increase timeout

--- packages/flutter_tools/test/general.shard/coverage_collector_test.dart
+++ packages/flutter_tools/test/general.shard/coverage_collector_test.dart
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+@Timeout.factor(10)
+
 import 'dart:convert' show jsonEncode;
 import 'dart:io' show Directory, File;
 
