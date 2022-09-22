7dd927c9b9a4dbdc380ad201f3965a45c774662d
xiaomiao <xieguo@bytedance.com>
2022-09-23 04:09:31 +0800

    [Impeller] Add render target builder. (flutter/engine#36314)

--- engine/src/flutter/impeller/entity/contents/content_context.cc
+++ engine/src/flutter/impeller/entity/contents/content_context.cc
@@ -11,6 +11,7 @@
 #include "impeller/renderer/formats.h"
 #include "impeller/renderer/render_pass.h"
 #include "impeller/renderer/render_target.h"
+#include "impeller/renderer/render_target_builder.h"
 
 namespace impeller {
 
@@ -245,12 +246,15 @@ std::shared_ptr<Texture> ContentContext::MakeSubpass(
     SubpassCallback subpass_callback) const {
   auto context = GetContext();
 
-  RenderTarget subpass_target;
-  if (context->SupportsOffscreenMSAA()) {
-    subpass_target = RenderTarget::CreateOffscreenMSAA(*context, texture_size);
-  } else {
-    subpass_target = RenderTarget::CreateOffscreen(*context, texture_size);
-  }
+  RenderTargetType render_target_type = context->SupportsOffscreenMSAA()
+                                            ? RenderTargetType::kOffscreenMSAA
+                                            : RenderTargetType::kOffscreen;
+
+  RenderTarget subpass_target = RenderTargetBuilder()
+                                    .SetSize(texture_size)
+                                    .SetRenderTargetType(render_target_type)
+                                    .Build(*context);
+
   auto subpass_texture = subpass_target.GetRenderTargetTexture();
   if (!subpass_texture) {
     return nullptr;
