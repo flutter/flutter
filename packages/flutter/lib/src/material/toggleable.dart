0a640a157520e04f64b4e0ea3aacb4936cedac15
MH Johnson <johnsonmh@google.com>
2022-09-22 14:36:07 -0400

    Revert "remove down position from toggleable ripple"
    This reverts commit 5f366440bc097d7caa1780af69c9224c3222fcb0.

--- packages/flutter/lib/src/material/toggleable.dart
+++ packages/flutter/lib/src/material/toggleable.dart
@@ -29,8 +29,7 @@ const Duration _kReactionFadeDuration = Duration(milliseconds: 50);
 /// This mixin is used to implement the material components for [Switch],
 /// [Checkbox], and [Radio] controls.
 @optionalTypeArgs
-mixin ToggleableStateMixin<S extends StatefulWidget>
-    on TickerProviderStateMixin<S> {
+mixin ToggleableStateMixin<S extends StatefulWidget> on TickerProviderStateMixin<S> {
   /// Used by subclasses to manipulate the visual value of the control.
   ///
   /// Some controls respond to user input by updating their visual value. For
@@ -246,9 +245,7 @@ mixin ToggleableStateMixin<S extends StatefulWidget>
 
   void _handleTapEnd([TapUpDetails? _]) {
     if (_downPosition != null) {
-      setState(() {
-        _downPosition = null;
-      });
+      setState(() { _downPosition = null; });
     }
     _reactionController.reverse();
   }
@@ -256,9 +253,7 @@ mixin ToggleableStateMixin<S extends StatefulWidget>
   bool _focused = false;
   void _handleFocusHighlightChanged(bool focused) {
     if (focused != _focused) {
-      setState(() {
-        _focused = focused;
-      });
+      setState(() { _focused = focused; });
       if (focused) {
         _reactionFocusFadeController.forward();
       } else {
@@ -270,9 +265,7 @@ mixin ToggleableStateMixin<S extends StatefulWidget>
   bool _hovering = false;
   void _handleHoverChanged(bool hovering) {
     if (hovering != _hovering) {
-      setState(() {
-        _hovering = hovering;
-      });
+      setState(() { _hovering = hovering; });
       if (hovering) {
         _reactionHoverFadeController.forward();
       } else {
@@ -290,11 +283,11 @@ mixin ToggleableStateMixin<S extends StatefulWidget>
   ///  * [MaterialState.focused], if the Toggleable has input focus
   ///  * [MaterialState.selected], if [value] is true or null
   Set<MaterialState> get states => <MaterialState>{
-        if (!isInteractive) MaterialState.disabled,
-        if (_hovering) MaterialState.hovered,
-        if (_focused) MaterialState.focused,
-        if (value ?? true) MaterialState.selected,
-      };
+    if (!isInteractive) MaterialState.disabled,
+    if (_hovering) MaterialState.hovered,
+    if (_focused) MaterialState.focused,
+    if (value ?? true) MaterialState.selected,
+  };
 
   /// Typically wraps a `painter` that draws the actual visuals of the
   /// Toggleable with logic to toggle it.
@@ -350,8 +343,7 @@ mixin ToggleableStateMixin<S extends StatefulWidget>
 /// Subclasses must implement the [paint] method to draw the actual visuals of
 /// the Toggleable. In their [paint] method subclasses may call
 /// [paintRadialReaction] to draw a radial ink reaction for this control.
-abstract class ToggleablePainter extends ChangeNotifier
-    implements CustomPainter {
+abstract class ToggleablePainter extends ChangeNotifier implements CustomPainter {
   /// The visual value of the control.
   ///
   /// Usually set to [ToggleableStateMixin.position].
@@ -556,9 +548,7 @@ abstract class ToggleablePainter extends ChangeNotifier
     Offset offset = Offset.zero,
     required Offset origin,
   }) {
-    if (!reaction.isDismissed ||
-        !reactionFocusFade.isDismissed ||
-        !reactionHoverFade.isDismissed) {
+    if (!reaction.isDismissed || !reactionFocusFade.isDismissed || !reactionHoverFade.isDismissed) {
       final Paint reactionPaint = Paint()
         ..color = Color.lerp(
           Color.lerp(
@@ -569,6 +559,7 @@ abstract class ToggleablePainter extends ChangeNotifier
           focusColor,
           reactionFocusFade.value,
         )!;
+      final Offset center = Offset.lerp(downPosition ?? origin, origin, reaction.value)!;
       final Animatable<double> radialReactionRadiusTween = Tween<double>(
         begin: 0.0,
         end: splashRadius,
@@ -577,10 +568,11 @@ abstract class ToggleablePainter extends ChangeNotifier
           ? splashRadius
           : radialReactionRadiusTween.evaluate(reaction);
       if (reactionRadius > 0.0) {
-        canvas.drawCircle(origin + offset, reactionRadius, reactionPaint);
+        canvas.drawCircle(center + offset, reactionRadius, reactionPaint);
       }
     }
   }
+
 
   @override
   void dispose() {
