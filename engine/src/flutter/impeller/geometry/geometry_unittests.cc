dc3b1eb7c726fb8251c79e4cac0faf02c3d323ed
xiaomiao <xieguo@bytedance.com>
2022-09-22 08:37:29 +0800

    [Impeller] Add matrix makerow and makecolumn. (flutter/engine#36312)

--- engine/src/flutter/impeller/geometry/geometry_unittests.cc
+++ engine/src/flutter/impeller/geometry/geometry_unittests.cc
@@ -30,6 +30,34 @@ TEST(GeometryTest, ScalarNearlyEqual) {
       1.0f, 1.0f + std::numeric_limits<float>::epsilon() * 4));
 }
 
+TEST(GeometryTest, MakeColumn) {
+  auto matrix = Matrix::MakeColumn(1, 2, 3, 4,     //
+                                   5, 6, 7, 8,     //
+                                   9, 10, 11, 12,  //
+                                   13, 14, 15, 16);
+
+  auto expect = Matrix{1,  2,  3,  4,   //
+                       5,  6,  7,  8,   //
+                       9,  10, 11, 12,  //
+                       13, 14, 15, 16};
+
+  ASSERT_TRUE(matrix == expect);
+}
+
+TEST(GeometryTest, MakeRow) {
+  auto matrix = Matrix::MakeRow(1, 2, 3, 4,     //
+                                5, 6, 7, 8,     //
+                                9, 10, 11, 12,  //
+                                13, 14, 15, 16);
+
+  auto expect = Matrix{1, 5, 9,  13,  //
+                       2, 6, 10, 14,  //
+                       3, 7, 11, 15,  //
+                       4, 8, 12, 16};
+
+  ASSERT_TRUE(matrix == expect);
+}
+
 TEST(GeometryTest, RotationMatrix) {
   auto rotation = Matrix::MakeRotationZ(Radians{kPiOver4});
   auto expect = Matrix{0.707,  0.707, 0, 0,  //
