a17c906b4ee1653cf4011096d8834883cc2a1d6b
a-wallen <stephenwallen@google.com>
2022-09-23 08:34:47 -1000

    Add integration tests for demo resize app

--- dev/integration_tests/window_resize/integration_test/integration_test.dart
+++ dev/integration_tests/window_resize/integration_test/integration_test.dart
@@ -1,4 +1,8 @@
-import 'package:flutter/widgets.dart' as widgets show Container, Size, runApp;
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/widgets.dart' as widgets show Size, runApp;
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:window_resize/main.dart' as app;
