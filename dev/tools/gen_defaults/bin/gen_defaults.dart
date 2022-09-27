1bae2692b17ee5268f5f1b56a0d9a1ae1f0596bf
Casey Hillers <caseyhillers@gmail.com>
2022-09-27 06:56:06 -0700

    Revert "Add support for Material 3 `Divider` and `VerticalDivider` (#112378)"
    This reverts commit 1d5f455c4c883145e7b15379f57152e474b05147.

--- dev/tools/gen_defaults/bin/gen_defaults.dart
+++ dev/tools/gen_defaults/bin/gen_defaults.dart
@@ -24,7 +24,6 @@ import 'package:gen_defaults/button_template.dart';
 import 'package:gen_defaults/card_template.dart';
 import 'package:gen_defaults/checkbox_template.dart';
 import 'package:gen_defaults/dialog_template.dart';
-import 'package:gen_defaults/divider_template.dart';
 import 'package:gen_defaults/fab_template.dart';
 import 'package:gen_defaults/filter_chip_template.dart';
 import 'package:gen_defaults/icon_button_template.dart';
@@ -66,7 +65,6 @@ Future<void> main(List<String> args) async {
     'date_picker_docked.json',
     'date_picker_modal.json',
     'dialog.json',
-    'divider.json',
     'dialog_fullscreen.json',
     'elevation.json',
     'fab_extended_primary.json',
@@ -123,7 +121,6 @@ Future<void> main(List<String> args) async {
   CardTemplate('Card', '$materialLib/card.dart', tokens).updateFile();
   CheckboxTemplate('Checkbox', '$materialLib/checkbox.dart', tokens).updateFile();
   DialogTemplate('Dialog', '$materialLib/dialog.dart', tokens).updateFile();
-  DividerTemplate('Divider', '$materialLib/divider.dart', tokens).updateFile();
   FABTemplate('FAB', '$materialLib/floating_action_button.dart', tokens).updateFile();
   FilterChipTemplate('ChoiceChip', '$materialLib/choice_chip.dart', tokens).updateFile();
   FilterChipTemplate('FilterChip', '$materialLib/filter_chip.dart', tokens).updateFile();
--- examples/api/lib/material/divider/vertical_divider.0.dart
+++ examples/api/lib/material/divider/vertical_divider.0.dart
@@ -11,19 +11,22 @@ void main() => runApp(const MyApp());
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  static const String _title = 'Flutter Code Sample';
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: _title,
       home: Scaffold(
-        appBar: AppBar(title: const Text('VerticalDivider Sample')),
-        body: const DividerExample(),
+        appBar: AppBar(title: const Text(_title)),
+        body: const MyStatelessWidget(),
       ),
     );
   }
 }
 
-class DividerExample extends StatelessWidget {
-  const DividerExample({super.key});
+class MyStatelessWidget extends StatelessWidget {
+  const MyStatelessWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
