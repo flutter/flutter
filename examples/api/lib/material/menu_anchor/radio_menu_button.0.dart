4f5ae35ef2085f14ade89328b9d0aef634d44cee
Greg Spencer <gspencer@google.com>
2022-10-03 12:42:07 -0700

    Add example tests

--- examples/api/lib/material/menu_anchor/radio_menu_button.0.dart
+++ examples/api/lib/material/menu_anchor/radio_menu_button.0.dart
@@ -10,9 +10,7 @@ import 'package:flutter/services.dart';
 void main() => runApp(const MenuApp());
 
 class MyRadioMenu extends StatefulWidget {
-  const MyRadioMenu({super.key, required this.message});
-
-  final String message;
+  const MyRadioMenu({super.key});
 
   @override
   State<MyRadioMenu> createState() => _MyRadioMenuState();
@@ -21,10 +19,26 @@ class MyRadioMenu extends StatefulWidget {
 class _MyRadioMenuState extends State<MyRadioMenu> {
   final FocusNode _buttonFocusNode = FocusNode(debugLabel: 'Menu Button');
   Color _backgroundColor = Colors.red;
+  late ShortcutRegistryEntry _entry;
+
+  static const SingleActivator _redShortcut = SingleActivator(LogicalKeyboardKey.keyR, control: true);
+  static const SingleActivator _greenShortcut = SingleActivator(LogicalKeyboardKey.keyG, control: true);
+  static const SingleActivator _blueShortcut = SingleActivator(LogicalKeyboardKey.keyB, control: true);
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    _entry = ShortcutRegistry.of(context).addAll(<ShortcutActivator, VoidCallbackIntent>{
+        _redShortcut: VoidCallbackIntent(() => _setBackgroundColor(Colors.red)),
+        _greenShortcut: VoidCallbackIntent(() => _setBackgroundColor(Colors.green)),
+        _blueShortcut: VoidCallbackIntent(() => _setBackgroundColor(Colors.blue)),
+      });
+  }
 
   @override
   void dispose() {
     _buttonFocusNode.dispose();
+    _entry.dispose();
     super.dispose();
   }
 
@@ -36,64 +50,54 @@ class _MyRadioMenuState extends State<MyRadioMenu> {
 
   @override
   Widget build(BuildContext context) {
-    return CallbackShortcuts(
-      bindings: <ShortcutActivator, VoidCallback>{
-        const SingleActivator(LogicalKeyboardKey.keyR, control: true): () {
-          _setBackgroundColor(Colors.red);
-        },
-        const SingleActivator(LogicalKeyboardKey.keyG, control: true): () {
-          _setBackgroundColor(Colors.green);
-        },
-        const SingleActivator(LogicalKeyboardKey.keyB, control: true): () {
-          _setBackgroundColor(Colors.blue);
-        },
-      },
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          MenuAnchor(
-            childFocusNode: _buttonFocusNode,
-            menuChildren: <Widget>[
-              RadioMenuButton<Color>(
-                value: Colors.red,
-                groupValue: _backgroundColor,
-                onChanged: _setBackgroundColor,
-                child: const Text('Red Background'),
-              ),
-              RadioMenuButton<Color>(
-                value: Colors.green,
-                groupValue: _backgroundColor,
-                onChanged: _setBackgroundColor,
-                child: const Text('Green Background'),
-              ),
-              RadioMenuButton<Color>(
-                value: Colors.blue,
-                groupValue: _backgroundColor,
-                onChanged: _setBackgroundColor,
-                child: const Text('BlueBackground'),
-              ),
-            ],
-            builder: (BuildContext context, MenuController controller, Widget? child) {
-              return TextButton(
-                focusNode: _buttonFocusNode,
-                onPressed: () {
-                  if (controller.isOpen) {
-                    controller.close();
-                  } else {
-                    controller.open();
-                  }
-                },
-                child: const Text('OPEN MENU'),
-              );
-            },
-          ),
-          Expanded(
-            child: ColoredBox(
-              color: _backgroundColor,
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        MenuAnchor(
+          childFocusNode: _buttonFocusNode,
+          menuChildren: <Widget>[
+            RadioMenuButton<Color>(
+              value: Colors.red,
+              shortcut: _redShortcut,
+              groupValue: _backgroundColor,
+              onChanged: _setBackgroundColor,
+              child: const Text('Red Background'),
             ),
+            RadioMenuButton<Color>(
+              value: Colors.green,
+              shortcut: _greenShortcut,
+              groupValue: _backgroundColor,
+              onChanged: _setBackgroundColor,
+              child: const Text('Green Background'),
+            ),
+            RadioMenuButton<Color>(
+              value: Colors.blue,
+              shortcut: _blueShortcut,
+              groupValue: _backgroundColor,
+              onChanged: _setBackgroundColor,
+              child: const Text('Blue Background'),
+            ),
+          ],
+          builder: (BuildContext context, MenuController controller, Widget? child) {
+            return TextButton(
+              focusNode: _buttonFocusNode,
+              onPressed: () {
+                if (controller.isOpen) {
+                  controller.close();
+                } else {
+                  controller.open();
+                }
+              },
+              child: const Text('OPEN MENU'),
+            );
+          },
+        ),
+        Expanded(
+          child: Container(
+            color: _backgroundColor,
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
@@ -101,12 +105,10 @@ class _MyRadioMenuState extends State<MyRadioMenu> {
 class MenuApp extends StatelessWidget {
   const MenuApp({super.key});
 
-  static const String kMessage = '"Talk less. Smile more." - A. Burr';
-
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(body: MyRadioMenu(message: kMessage)),
+      home: Scaffold(body: MyRadioMenu()),
     );
   }
 }
