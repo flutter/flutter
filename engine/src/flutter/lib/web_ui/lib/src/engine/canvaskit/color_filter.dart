9878e914747ef8c1416fc5bc727665e1eaf34c73
alanwutang11 <50225138+alanwutang11@users.noreply.github.com>
2022-10-04 12:14:02 -0700

    Fix filter null exceptions (flutter/engine#36575)

--- engine/src/flutter/lib/web_ui/lib/src/engine/canvaskit/color_filter.dart
+++ engine/src/flutter/lib/web_ui/lib/src/engine/canvaskit/color_filter.dart
@@ -81,8 +81,26 @@ class CkBlendModeColorFilter extends CkColorFilter {
   final ui.Color color;
   final ui.BlendMode blendMode;
 
+  static Float32List get identityTransform => _identityTransform ?? _computeIdentityTransform();
+  static Float32List? _identityTransform;
+
+  static Float32List _computeIdentityTransform() {
+    final Float32List result = Float32List(20);
+    const List<int> translationIndices = <int>[0, 6, 12, 18];
+    for (final int i in translationIndices) {
+      result[i] = 1;
+    }
+    _identityTransform = result;
+    return result;
+  }
+
   @override
   SkColorFilter _initRawColorFilter() {
+    /// Return the identity matrix when the color opacity is 0. Replicates
+    /// effect of applying no filter
+    if (color.opacity == 0) {
+      return canvasKit.ColorFilter.MakeMatrix(identityTransform);
+    }
     final SkColorFilter? filter = canvasKit.ColorFilter.MakeBlend(
       toSharedSkColor1(color),
       toSkBlendMode(blendMode),
