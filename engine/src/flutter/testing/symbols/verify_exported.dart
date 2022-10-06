21a3edd2ea1732002c4a9262fb256004228742ed
Jenn Magder <magder@google.com>
2022-10-06 16:08:05 -0700

    Use llvm version of libcxxabi instead of patched mirror (flutter/engine#36634)

--- engine/src/flutter/testing/symbols/verify_exported.dart
+++ engine/src/flutter/testing/symbols/verify_exported.dart
@@ -96,9 +96,7 @@ int _checkIos(String outPath, String nmPath, Iterable<String> builds) {
     final Iterable<NmEntry> unexpectedEntries = NmEntry.parse(nmResult.stdout as String).where((NmEntry entry) {
       return !(((entry.type == '(__DATA,__common)' || entry.type == '(__DATA,__const)') && entry.name.startsWith('_Flutter'))
           || (entry.type == '(__DATA,__objc_data)'
-              && (entry.name.startsWith(r'_OBJC_METACLASS_$_Flutter') || entry.name.startsWith(r'_OBJC_CLASS_$_Flutter')))
-          // TODO(107887): This should not be neccesary after bitcode support is removed from the engine.
-          || (entry.type == '(__TEXT,__text)' && entry.name == '___gxx_personality_v0'));
+              && (entry.name.startsWith(r'_OBJC_METACLASS_$_Flutter') || entry.name.startsWith(r'_OBJC_CLASS_$_Flutter'))));
     });
     if (unexpectedEntries.isNotEmpty) {
       print('ERROR: $libFlutter exports unexpected symbols:');
