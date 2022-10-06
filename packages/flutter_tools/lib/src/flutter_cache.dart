8d692112449f460271df9d2225aa4f8f68dc781c
Harry Terkelsen <het@google.com>
2022-10-06 15:16:02 -0700

    Add TODO to delete dead code later

--- packages/flutter_tools/lib/src/flutter_cache.dart
+++ packages/flutter_tools/lib/src/flutter_cache.dart
@@ -208,6 +208,8 @@ class FlutterWebSdk extends CachedArtifact {
 
     // If the flutter_web_sdk folder doesn't already contain CanvasKit, then
     // download it from CIPD.
+    // TODO(hterkelsen): This whole section can be removed when we are always building
+    //   CanvasKit as past of flutter_web_sdk. See https://github.com/flutter/flutter/issues/113073
     final File expectedCanvasKitFile = fileSystem.file(fileSystem.path.join(location.path, 'canvaskit', 'canvaskit.wasm'));
     if (!expectedCanvasKitFile.existsSync()) {
       final String canvasKitVersion = cache.getVersionFor('canvaskit')!;
