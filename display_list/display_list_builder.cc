623d2d91802efe3cdaf5790cfae669dccc7f9669
Jason Simmons <jason-simmons@users.noreply.github.com>
2022-10-10 15:29:19 -0700

    Apply path effects from a DlPaint to a DisplayListBuilder (#36669)

--- display_list/display_list_builder.cc
+++ display_list/display_list_builder.cc
@@ -359,10 +359,9 @@ void DisplayListBuilder::setAttributesFromDlPaint(
   if (flags.applies_image_filter()) {
     setImageFilter(paint.getImageFilter().get());
   }
-  // Waiting for https://github.com/flutter/engine/pull/32159
-  // if (flags.applies_path_effect()) {
-  //   setPathEffect(sk_ref_sp(paint.getPathEffect()));
-  // }
+  if (flags.applies_path_effect()) {
+    setPathEffect(paint.getPathEffect().get());
+  }
   if (flags.applies_mask_filter()) {
     setMaskFilter(paint.getMaskFilter().get());
   }
