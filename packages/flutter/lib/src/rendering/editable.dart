fe5a652f0db235d3875fa631d00e2bb94f4ea2ce
Renzo Olivares <roliv@google.com>
2022-10-14 12:48:42 -0700

    update docs

--- packages/flutter/lib/src/rendering/editable.dart
+++ packages/flutter/lib/src/rendering/editable.dart
@@ -2066,7 +2066,7 @@ class RenderEditable extends RenderBox with RelayoutWhenSystemFontsChangeMixin, 
     selectWordsInRange(from: _lastTapDownPosition!, cause: cause);
   }
 
-  /// Selects the set words of a paragraph that intersect a range of global positions.
+  /// Selects the set words of a paragraph that intersect a given range of global positions.
   /// The set of words selected are not strictly bounded by the range of global positions.
   ///
   /// The first and last endpoints of the selection will always be at the
