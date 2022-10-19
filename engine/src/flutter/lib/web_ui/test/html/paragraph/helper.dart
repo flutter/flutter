93585f2cd286c5eafe8f0306f0ff38bebc044559
Mouad Debbar <mdebbar@google.com>
2022-10-19 12:04:56 -0400

    [web] Some cleanup for text tests (flutter/engine#36425)

--- engine/src/flutter/lib/web_ui/test/html/paragraph/helper.dart
+++ engine/src/flutter/lib/web_ui/test/html/paragraph/helper.dart
@@ -2,9 +2,13 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'dart:async';
+
 import 'package:ui/src/engine.dart';
 import 'package:ui/ui.dart';
 import 'package:web_engine_tester/golden_tester.dart';
+
+typedef CanvasTest = FutureOr<void> Function(EngineCanvas canvas);
 
 const LineBreakType prohibited = LineBreakType.prohibited;
 const LineBreakType opportunity = LineBreakType.opportunity;
@@ -31,6 +35,11 @@ const Color lightBlue = Color(0xFFB3E5FC);
 const Color blue = Color(0xFF0000FF);
 const Color yellow = Color(0xFFFFEB3B);
 const Color lightPurple = Color(0xFFE1BEE7);
+
+final EngineParagraphStyle ahemStyle = EngineParagraphStyle(
+  fontFamily: 'ahem',
+  fontSize: 10,
+);
 
 ParagraphConstraints constrain(double width) {
   return ParagraphConstraints(width: width);
@@ -73,10 +82,7 @@ Future<void> takeScreenshot(
   try {
     sceneElement.append(canvas.rootElement);
     domDocument.body!.append(sceneElement);
-    await matchGoldenFile(
-      '$fileName.png',
-      region: region,
-    );
+    await matchGoldenFile('$fileName.png', region: region);
   } finally {
     // The page is reused across tests, so remove the element after taking the
     // Scuba screenshot.
@@ -108,4 +114,8 @@ void fillBoxes(EngineCanvas canvas, Offset offset, List<TextBox> boxes, Color co
     final Rect rect = box.toRect().shift(offset);
     canvas.drawRect(rect, SurfacePaintData()..color = color);
   }
+}
+
+String getSpanText(CanvasParagraph paragraph, ParagraphSpan span) {
+  return paragraph.plainText.substring(span.start, span.end);
 }
