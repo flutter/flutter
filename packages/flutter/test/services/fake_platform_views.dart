6928314d505d2bb4777be05e45d7808a5aa91d2a
stuartmorgan <stuartmorgan@google.com>
2022-10-25 16:34:41 -0400

    Wait for non-empty layout in platform view placeholder (#112402) (#113040)
    Co-authored-by: Kevin Chisholm <kevinjchisholm@google.com>
    Co-authored-by: godofredoc <godofredoc@google.com>

--- packages/flutter/test/services/fake_platform_views.dart
+++ packages/flutter/test/services/fake_platform_views.dart
@@ -124,6 +124,9 @@ class FakeAndroidViewController implements AndroidViewController {
   @override
   Future<void> create({Size? size}) async {
     assert(!_createCalledSuccessfully);
+    if (requiresSize && size != null) {
+      assert(!size.isEmpty);
+    }
     _createCalledSuccessfully = size != null || !requiresSize;
   }
 
