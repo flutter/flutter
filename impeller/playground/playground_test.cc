e39c4dfe72bc64e3a293cdb066e9cc788478c14f
Brandon DeRosier <bdero@google.com>
2022-10-26 15:09:45 -0700

    [Impeller] Fix stuttering in playgrounds due to precision issue (#37035)

--- impeller/playground/playground_test.cc
+++ impeller/playground/playground_test.cc
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "flutter/fml/time/time_point.h"
+
 #include "impeller/playground/playground_test.h"
 
 namespace impeller {
@@ -22,6 +24,8 @@ void PlaygroundTest::SetUp() {
   }
 
   SetupWindow(GetParam());
+
+  start_time_ = fml::TimePoint::Now().ToEpochDelta();
 }
 
 void PlaygroundTest::TearDown() {
@@ -57,6 +61,10 @@ static std::string FormatWindowTitle(const std::string& test_name) {
 // |Playground|
 std::string PlaygroundTest::GetWindowTitle() const {
   return FormatWindowTitle(flutter::testing::GetCurrentTestName());
+}
+
+Scalar PlaygroundTest::GetSecondsElapsed() const {
+  return (fml::TimePoint::Now().ToEpochDelta() - start_time_).ToSecondsF();
 }
 
 }  // namespace impeller
