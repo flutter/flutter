16c64e199bfa9e346d916eb7ad093df9a5cc42dd
Maurice Parrish <10687576+bparrishMines@users.noreply.github.com>
2022-10-26 16:20:50 -0400

    test

--- packages/flutter/test/widgets/platform_view_test.dart
+++ packages/flutter/test/widgets/platform_view_test.dart
@@ -2599,6 +2599,48 @@ void main() {
       },
     );
 
+    testWidgets('PlatformViewLink includes offset in create call when using texture layer', (WidgetTester tester) async {
+      late FakeAndroidViewController controller;
+
+      final PlatformViewLink platformViewLink = PlatformViewLink(
+        viewType: 'webview',
+        onCreatePlatformView: (PlatformViewCreationParams params) {
+          controller = FakeAndroidViewController(params.id, requiresSize: true);
+          controller.create();
+          // This test should be simulating one of the texture-based display
+          // modes, where `create` is a no-op when not provided a size, and
+          // creation is triggered via a later call to setSize, or to `create`
+          // with a size.
+          expect(controller.awaitingCreation, true);
+          return controller;
+        },
+        surfaceFactory: (BuildContext context, PlatformViewController controller) {
+          return PlatformViewSurface(
+            gestureRecognizers: const <Factory<OneSequenceGestureRecognizer>>{},
+            controller: controller,
+            hitTestBehavior: PlatformViewHitTestBehavior.opaque,
+          );
+        },
+      );
+
+      TestWidgetsFlutterBinding.instance.window.physicalSizeTestValue = const Size(400, 200);
+      TestWidgetsFlutterBinding.instance.window.devicePixelRatioTestValue = 1.0;
+
+      await tester.pumpWidget(
+        Container(
+          constraints: const BoxConstraints.expand(),
+          alignment: Alignment.center,
+          child: SizedBox(
+            width: 100,
+            height: 50,
+            child: platformViewLink,
+          ),
+        )
+      );
+
+      expect(controller.createPosition, const Offset(150, 75));
+    });
+
     testWidgets(
       'PlatformViewLink does not double-call create for Android Hybrid Composition',
       (WidgetTester tester) async {
