51acda81f289ddaea04ea9f8b587dc20f1479764
Mitchell Goodwin <58190796+MitchellGoodwin@users.noreply.github.com>
2022-10-26 11:49:26 -0700

    Update Cupertino text input padding (#113958)
    * Update Cupertino text input padding
    
    * Remove comment

--- packages/flutter/lib/src/cupertino/text_field.dart
+++ packages/flutter/lib/src/cupertino/text_field.dart
@@ -214,7 +214,7 @@ class CupertinoTextField extends StatefulWidget {
     this.controller,
     this.focusNode,
     this.decoration = _kDefaultRoundedBorderDecoration,
-    this.padding = const EdgeInsets.all(6.0),
+    this.padding = const EdgeInsets.all(7.0),
     this.placeholder,
     this.placeholderStyle = const TextStyle(
       fontWeight: FontWeight.w400,
@@ -380,7 +380,7 @@ class CupertinoTextField extends StatefulWidget {
     this.controller,
     this.focusNode,
     this.decoration,
-    this.padding = const EdgeInsets.all(6.0),
+    this.padding = const EdgeInsets.all(7.0),
     this.placeholder,
     this.placeholderStyle = _kDefaultPlaceholderStyle,
     this.prefix,
