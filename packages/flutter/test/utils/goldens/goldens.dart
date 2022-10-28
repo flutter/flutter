6ff5bd0824976df3d00af6deaa323cf43e4bf5e9
Kate Lovett <katelovett@google.com>
2022-10-28 13:50:24 -0500

    ++

--- packages/flutter/test/utils/goldens/goldens.dart
+++ packages/flutter/test/utils/goldens/goldens.dart
@@ -2,6 +2,10 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// This tag must be at the top of every test file that executes golden file
+// tests. It is analyzer enforced.
+@Tags(<String>['reduced-test-set'])
+
 import 'package:flutter_test/flutter_test.dart';
 import '_goldens_io.dart'
   if (dart.library.html) '_goldens_web.dart' as flutter_goldens;
