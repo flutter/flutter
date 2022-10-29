491f827ea84dae3c63e50493a3a25e378c7cf441
fzyzcjy <5236035+fzyzcjy@users.noreply.github.com>
2022-10-30 06:07:03 +0800

    Fix wrong `VSYNC` event (flutter/engine#36775)

--- engine/src/flutter/shell/common/vsync_waiter_fallback.cc
+++ engine/src/flutter/shell/common/vsync_waiter_fallback.cc
@@ -35,7 +35,7 @@ VsyncWaiterFallback::~VsyncWaiterFallback() = default;
 
 // |VsyncWaiter|
 void VsyncWaiterFallback::AwaitVSync() {
-  TRACE_EVENT0("flutter", "VSYNC");
+  TRACE_EVENT0("flutter", "VsyncCallback");
 
   constexpr fml::TimeDelta kSingleFrameInterval =
       fml::TimeDelta::FromSecondsF(1.0 / 60.0);
