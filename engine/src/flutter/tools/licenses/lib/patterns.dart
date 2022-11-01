49a958f03a49b5adcdcb499636b33856f537f375
Loïc Sharma <737941+loic-sharma@users.noreply.github.com>
2022-11-01 15:14:48 -0700

    Roll ANGLE, vulkan-deps, and shaderc (flutter/engine#36389)
    This change updates ANGLE to a version that contains [angle#3906325](https://chromium-review.googlesource.com/c/angle/angle/+/3906325), which fixes the COM error reported in  https://github.com/flutter/flutter/issues/110948. This also updates vulkan-deps and shaderc as these are dependencies of ANGLE.
    
    Part of https://github.com/flutter/flutter/issues/110948

--- engine/src/flutter/tools/licenses/lib/patterns.dart
+++ engine/src/flutter/tools/licenses/lib/patterns.dart
@@ -1649,7 +1649,7 @@ final List<RegExp> csLicenses = <RegExp>[
     r'^(?:\1\2)?GNU General Public License for more details.\n'
     r'^(?:\1\2)?\n*'
     r'^(?:\1\2)?You should have received a copy of the GNU General Public License\n'
-    r'^(?:\1\2)?along with this program.  If not, see <http://www.gnu.org/licenses/>.  \*/\n'
+    r'^(?:\1\2)?along with this program.  If not, see <https?://www.gnu.org/licenses/>.  \*/\n'
     r'^(?:\1\2)?\n*' +
     kIndent +
     r'As a special exception, you may create a larger work that contains\n'
