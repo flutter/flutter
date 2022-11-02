6a66aa282f8423de2628b7fcb30d353ce248cbe3
Darren Austin <darrenaustin@google.com>
2022-11-02 03:54:19 -0700

    Add Material 3 support for BottomAppBar (reland #106525) (#114439)
    * Revert "Revert "Add Material 3 support for BottomAppBar" (#114421)"
    
    This reverts commit 210a2aa371debc4c06e24b74bcb7f4a842427147.
    
    * Regenerated the defaults from tokens and fixed tests.
    
    * Fixed the tests.
    
    * Updated the shape token template to optimize the a common case.

--- dev/tools/gen_defaults/bin/gen_defaults.dart
+++ dev/tools/gen_defaults/bin/gen_defaults.dart
@@ -20,6 +20,7 @@ import 'dart:io';
 import 'package:gen_defaults/action_chip_template.dart';
 import 'package:gen_defaults/app_bar_template.dart';
 import 'package:gen_defaults/banner_template.dart';
+import 'package:gen_defaults/bottom_app_bar_template.dart';
 import 'package:gen_defaults/bottom_sheet_template.dart';
 import 'package:gen_defaults/button_template.dart';
 import 'package:gen_defaults/card_template.dart';
@@ -121,6 +122,7 @@ Future<void> main(List<String> args) async {
   ActionChipTemplate('ActionChip', '$materialLib/action_chip.dart', tokens).updateFile();
   AppBarTemplate('AppBar', '$materialLib/app_bar.dart', tokens).updateFile();
   BannerTemplate('Banner', '$materialLib/banner.dart', tokens).updateFile();
+  BottomAppBarTemplate('BottomAppBar', '$materialLib/bottom_app_bar.dart', tokens).updateFile();
   BottomSheetTemplate('BottomSheet', '$materialLib/bottom_sheet.dart', tokens).updateFile();
   ButtonTemplate('md.comp.elevated-button', 'ElevatedButton', '$materialLib/elevated_button.dart', tokens).updateFile();
   ButtonTemplate('md.comp.filled-button', 'FilledButton', '$materialLib/filled_button.dart', tokens).updateFile();
