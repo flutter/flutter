b065148586edb69997eb4a8c34f1eeea1aaf38e2
Casey Hillers <caseyhillers@gmail.com>
2022-11-03 15:13:51 -0700

    Revert "Fix text field label animation duration and curve (#105966)"
    This reverts commit d5c53b82e910d157b0e041c635904a970f473c62.

--- packages/flutter/lib/src/widgets/text_selection.dart
+++ packages/flutter/lib/src/widgets/text_selection.dart
@@ -2652,7 +2652,6 @@ class _TextSelectionGestureDetectorState extends State<TextSelectionGestureDetec
     if (!_isDoubleTap) {
       widget.onSingleTapUp?.call(details);
       _lastTapOffset = details.globalPosition;
-      _doubleTapTimer?.cancel();
       _doubleTapTimer = Timer(kDoubleTapTimeout, _doubleTapTimeout);
     }
     _isDoubleTap = false;
