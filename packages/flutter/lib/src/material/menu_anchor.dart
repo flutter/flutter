f35dcca2225e2063868b118d0559615c7a7543a6
Phil Quitslund <pq@users.noreply.github.com>
2022-11-03 10:56:59 -0700

    fixes for incoming linter (#113794)
    * fixes for incomming linter
    
    * + todo
    
    * todo format fix
    
    * justify ignore
    
    * --

--- packages/flutter/lib/src/material/menu_anchor.dart
+++ packages/flutter/lib/src/material/menu_anchor.dart
@@ -483,7 +483,7 @@ class _MenuAnchorState extends State<MenuAnchor> {
       // close it first.
       _close();
     }
-    assert(_debugMenuInfo('Opening ${this} at ${position ?? Offset.zero} with alignment offset ${widget.alignmentOffset ?? Offset.zero}'));
+    assert(_debugMenuInfo('Opening $this at ${position ?? Offset.zero} with alignment offset ${widget.alignmentOffset ?? Offset.zero}'));
     _parent?._closeChildren(); // Close all siblings.
     assert(_overlayEntry == null);
 
@@ -553,6 +553,8 @@ class _MenuAnchorState extends State<MenuAnchor> {
   }
 
   void _closeChildren({bool inDispose = false}) {
+    // TODO(pquitslund): remove when interpolated `$this` parsing is fixed (https://github.com/dart-lang/sdk/issues/50263)
+    // ignore: unnecessary_brace_in_string_interps
     assert(_debugMenuInfo('Closing children of ${this}${inDispose ? ' (dispose)' : ''}'));
     for (final _MenuAnchorState child in List<_MenuAnchorState>.from(_anchorChildren)) {
       child._close(inDispose: inDispose);
