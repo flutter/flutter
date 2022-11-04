71ba073d3e425736f1e51ae3f4c27f3ef955a890
Gary Qian <garyq@google.com>
2022-11-04 11:40:04 -0700

    Cleanup

--- packages/flutter_tools/gradle/flutter.gradle.kts
+++ packages/flutter_tools/gradle/flutter.gradle.kts
@@ -2,13 +2,11 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// import org.gradle.kotlin.dsl.`kotlin-dsl`
-
-// Apply the plugin
 apply<FlutterPluginKts>()
 
 class FlutterPluginKts : Plugin<Project> {
     override fun apply(project: Project) {
+        // Use withGroovyBuilder and getProperty() to access Groovy metaprogramming.
         project.withGroovyBuilder {
             getProperty("android").withGroovyBuilder {
                 getProperty("defaultConfig").withGroovyBuilder {
