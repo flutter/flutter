80bf355192c923dabcbe20125f22a8bda869e869
chunhtai <47866232+chunhtai@users.noreply.github.com>
2022-11-04 10:55:28 -0700

    Support keyboard selection in SelectabledRegion (#112584)
    * Support keyboard selection in selectable region
    
    * fix some comments
    
    * addressing comments

--- packages/flutter/test/widgets/editable_text_shortcuts_test.dart
+++ packages/flutter/test/widgets/editable_text_shortcuts_test.dart
@@ -8,27 +8,7 @@ import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'clipboard_utils.dart';
-
-Future<void> sendKeyCombination(
-  WidgetTester tester,
-  SingleActivator activator,
-) async {
-  final List<LogicalKeyboardKey> modifiers = <LogicalKeyboardKey>[
-    if (activator.control) LogicalKeyboardKey.control,
-    if (activator.shift) LogicalKeyboardKey.shift,
-    if (activator.alt) LogicalKeyboardKey.alt,
-    if (activator.meta) LogicalKeyboardKey.meta,
-  ];
-  for (final LogicalKeyboardKey modifier in modifiers) {
-    await tester.sendKeyDownEvent(modifier);
-  }
-  await tester.sendKeyDownEvent(activator.trigger);
-  await tester.sendKeyUpEvent(activator.trigger);
-  await tester.pump();
-  for (final LogicalKeyboardKey modifier in modifiers.reversed) {
-    await tester.sendKeyUpEvent(modifier);
-  }
-}
+import 'keyboard_utils.dart';
 
 Iterable<SingleActivator> allModifierVariants(LogicalKeyboardKey trigger) {
   const Iterable<bool> trueFalse = <bool>[false, true];
