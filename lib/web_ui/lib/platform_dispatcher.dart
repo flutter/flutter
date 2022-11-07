a81a715ce6b23120b5b19df1f756a1e693a0f160
Alex Wallen <wallenstephen@outlook.com>
2022-11-07 09:23:52 -1000

    Revert "Remove deprecated calls to updateSemantics in `PlatformDispatcher` (#36673)" (#37388)
    This reverts commit 4d8f07b8f851e9de85f124d4d131906d854a0e9d.

--- lib/web_ui/lib/platform_dispatcher.dart
+++ lib/web_ui/lib/platform_dispatcher.dart
@@ -83,6 +83,14 @@ abstract class PlatformDispatcher {
   VoidCallback? get onAccessibilityFeaturesChanged;
   set onAccessibilityFeaturesChanged(VoidCallback? callback);
 
+  @Deprecated('''
+    In a multi-view world, the platform dispatcher can no longer provide apis
+    to update semantics since each view will host its own semantics tree.
+
+    Semantics updates must be passed to an individual [FlutterView]. To update
+    semantics, use PlatformDispatcher.instance.views to get a [FlutterView] and
+    call `updateSemantics`.
+  ''')
   void updateSemantics(SemanticsUpdate update);
 
   Locale get locale;
