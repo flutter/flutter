45ad44de85d494165bbe7f75e3f5ffb97f2938b2
Mouad Debbar <mdebbar@google.com>
2022-11-08 13:24:07 -0500

    [web] Use v8BreakIterator where possible (flutter/engine#37317)
    * [web] Use v8BreakIterator where possible
    
    * address review comments

--- engine/src/flutter/lib/web_ui/test/text/line_breaker_test_helper.dart
+++ engine/src/flutter/lib/web_ui/test/text/line_breaker_test_helper.dart
@@ -3,11 +3,11 @@
 // found in the LICENSE file.
 
 /// Parses raw test data into a list of [TestCase] objects.
-List<TestCase> parseRawTestData(String rawTestData) {
+List<TestCase> parseRawTestData(String rawTestData, {required bool isV8}) {
   return rawTestData
       .split('\n')
       .where(isValidTestCase)
-      .map(_checkReplacement)
+      .map((String line) => _checkReplacement(line, isV8: isV8))
       .map(_parse)
       .toList();
 }
@@ -16,7 +16,7 @@ bool isValidTestCase(String line) {
   return line.startsWith('×');
 }
 
-String _checkReplacement(String line) {
+String _checkReplacement(String line, {required bool isV8}) {
   String replacement = line;
 
   // Special cases for rules LB8, LB11, LB13, LB14, LB15, LB16, LB17 to allow
@@ -28,38 +28,91 @@ String _checkReplacement(String line) {
         .replaceAllMapped(spacesRegex, (Match m) => 'SPACE (SP) ÷ [${m.group(1)}.');
   }
 
-  // Some test cases contradict rule LB25, so we are fixing them with the few
-  // regexes below.
+  if (!isV8) {
+    // Some test cases contradict rule LB25, so we are fixing them with the few
+    // regexes below.
 
-  final RegExp lb25Regex1 = RegExp(r'\((CP_CP30|CL)\)(.*?) ÷ \[999\.0\] (PERCENT|DOLLAR)');
-  if (replacement.contains(lb25Regex1)) {
-    replacement = replacement
-        .replaceAll(' ÷ 0024', ' × 0024') // DOLLAR SIGN (PR)
-        .replaceAll(' ÷ 0025', ' × 0025') // PERCENT SIGN (PO)
-        .replaceAllMapped(
-          lb25Regex1,
-          (Match m) => '(${m.group(1)})${m.group(2)} × [999.0] ${m.group(3)}',
-        );
+    final RegExp lb25Regex1 = RegExp(r'\((CP_CP30|CL)\)(.*?) ÷ \[999\.0\] (PERCENT|DOLLAR)');
+    if (replacement.contains(lb25Regex1)) {
+      replacement = replacement
+          .replaceAll(' ÷ 0024', ' × 0024') // DOLLAR SIGN (PR)
+          .replaceAll(' ÷ 0025', ' × 0025') // PERCENT SIGN (PO)
+          .replaceAllMapped(
+            lb25Regex1,
+            (Match m) => '(${m.group(1)})${m.group(2)} × [999.0] ${m.group(3)}',
+          );
+    }
+    final RegExp lb25Regex2 = RegExp(r'\((IS|SY)\)(.*?) ÷ \[999\.0\] (DIGIT)');
+    if (replacement.contains(lb25Regex2)) {
+      replacement = replacement
+          .replaceAll(' ÷ 0030', ' × 0030') // DIGIT ZERO (NU)
+          .replaceAllMapped(
+            lb25Regex2,
+            (Match m) => '(${m.group(1)})${m.group(2)} × [999.0] ${m.group(3)}',
+          );
+    }
+    final RegExp lb25Regex3 = RegExp(r'\((PR|PO)\)(.*?) ÷ \[999\.0\] (LEFT)');
+    if (replacement.contains(lb25Regex3)) {
+      replacement = replacement
+          .replaceAll(' ÷ 0028', ' × 0028') // LEFT PARENTHESIS (OP_OP30)
+          .replaceAll(' ÷ 007B', ' × 007B') // LEFT CURLY BRACKET (OP_OP30)
+          .replaceAll(' ÷ 2329', ' × 2329') // LEFT-POINTING ANGLE BRACKET (OP)
+          .replaceAllMapped(
+            lb25Regex3,
+            (Match m) => '(${m.group(1)})${m.group(2)} × [999.0] ${m.group(3)}',
+          );
+    }
   }
-  final RegExp lb25Regex2 = RegExp(r'\((IS|SY)\)(.*?) ÷ \[999\.0\] (DIGIT)');
-  if (replacement.contains(lb25Regex2)) {
-    replacement = replacement
-        .replaceAll(' ÷ 0030', ' × 0030') // DIGIT ZERO (NU)
-        .replaceAllMapped(
-          lb25Regex2,
-          (Match m) => '(${m.group(1)})${m.group(2)} × [999.0] ${m.group(3)}',
-        );
-  }
-  final RegExp lb25Regex3 = RegExp(r'\((PR|PO)\)(.*?) ÷ \[999\.0\] (LEFT)');
-  if (replacement.contains(lb25Regex3)) {
-    replacement = replacement
-        .replaceAll(' ÷ 0028', ' × 0028') // LEFT PARENTHESIS (OP_OP30)
-        .replaceAll(' ÷ 007B', ' × 007B') // LEFT CURLY BRACKET (OP_OP30)
-        .replaceAll(' ÷ 2329', ' × 2329') // LEFT-POINTING ANGLE BRACKET (OP)
-        .replaceAllMapped(
-          lb25Regex3,
-          (Match m) => '(${m.group(1)})${m.group(2)} × [999.0] ${m.group(3)}',
-        );
+
+  if (isV8) {
+    // v8BreakIterator deviates from the spec around Hiragana and Katakana
+    // letters.
+
+    final RegExp hiragana21Regex = RegExp(r' × \[21\.03\] (HIRAGANA LETTER|KATAKANA LETTER|KATAKANA-HIRAGANA)');
+    if (replacement.contains(hiragana21Regex) && !replacement.contains('(BB)') && !replacement.contains('(PR)')) {
+      replacement = replacement
+          .replaceAll(' × 3041', ' ÷ 3041') // HIRAGANA LETTER (CJ)
+          .replaceAll(' × 30E5', ' ÷ 30E5') // KATAKANA LETTER (CJ)
+          .replaceAll(' × 30FC', ' ÷ 30FC') // KATAKANA-HIRAGANA PROLONGED SOUND MARK (CJ)
+          .replaceAllMapped(
+            hiragana21Regex,
+            (Match m) => ' ÷ [21.03] ${m.group(1)}',
+          );
+    }
+    if (replacement.contains(' × [16.0] HIRAGANA LETTER')) {
+      replacement = replacement
+          .replaceAll(' × 3041', ' ÷ 3041') // HIRAGANA LETTER (CJ)
+          .replaceAll(
+            ' × [16.0] HIRAGANA LETTER',
+            ' ÷ [16.0] HIRAGANA LETTER',
+          );
+    }
+    final RegExp hiraganaPercentRegex = RegExp(r'HIRAGANA .*? ÷ \[999\.0\] PERCENT');
+    if (replacement.contains(hiraganaPercentRegex)) {
+      replacement = replacement
+          .replaceAll(' ÷ 0025', ' × 0025') // PERCENT SIGN (PO)
+          .replaceAll(
+            ' ÷ [999.0] PERCENT',
+            ' × [999.0] PERCENT',
+          );
+    }
+
+    // v8BreakIterator also deviates from the spec around hyphens, commas and
+    // full stops.
+
+    final RegExp hyphenRegex = RegExp(r'\((HY|IS)\)(.*?) ÷ \[999\.0\] (DIGIT|NUMBER|SECTION|THAI|<reserved-50005>)');
+    if (replacement.contains(hyphenRegex)) {
+      replacement = replacement
+          .replaceAll(' ÷ 0030', ' × 0030') // DIGIT ZERO (NU)
+          .replaceAll(' ÷ 0023', ' × 0023') // NUMBER SIGN (AL)
+          .replaceAll(' ÷ 00A7', ' × 00A7') // SECTION SIGN (AI_AL)
+          .replaceAll(' ÷ 0E01', ' × 0E01') // THAI CHARACTER KO KAI (SA_AL)
+          .replaceAll(' ÷ 50005', ' × 50005') // <reserved-50005> (XX_AL)
+          .replaceAllMapped(
+            hyphenRegex,
+            (Match m) => '(${m.group(1)})${m.group(2)} × [999.0] ${m.group(3)}',
+          );
+    }
   }
 
   return replacement;
