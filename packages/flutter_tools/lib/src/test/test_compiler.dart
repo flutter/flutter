e32f59bbbb9e41886cc35477a9c30dd77ed8df89
Danny Tuppeny <danny@tuppeny.com>
2022-11-08 13:05:16 +0000

    Revert "Don't create a copy of dill file alongside tests during compilation"
    This reverts commit eddf9d8b56b128b67048f3d8010a0b268270375f.

--- packages/flutter_tools/lib/src/test/test_compiler.dart
+++ packages/flutter_tools/lib/src/test/test_compiler.dart
@@ -184,7 +184,9 @@ class TestCompiler {
         await _shutdown();
       } else {
         if (shouldCopyDillFile) {
+          final String path = request.mainUri.toFilePath(windows: globals.platform.isWindows);
           final File outputFile = globals.fs.file(outputPath);
+          final File kernelReadyToRun = await outputFile.copy('$path.dill');
           final File testCache = globals.fs.file(testFilePath);
           if (firstCompile || !testCache.existsSync() || (testCache.lengthSync() < outputFile.lengthSync())) {
             // The idea is to keep the cache file up-to-date and include as
@@ -195,7 +197,7 @@ class TestCompiler {
             }
             await outputFile.copy(testFilePath);
           }
-          request.result.complete(outputFile.path);
+          request.result.complete(kernelReadyToRun.path);
         } else {
           request.result.complete(outputPath);
         }
