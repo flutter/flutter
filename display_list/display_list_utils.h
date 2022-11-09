f8048be39a7c1a3821d3e648122a5c12278b7610
Kaushik Iska <iska.kaushik@gmail.com>
2022-11-09 05:57:29 -0500

    [Reland] Add rects to accumulator rather than bounds (#37435) (#37451)
    * [Reland] Add rects to accumulator rather than bounds (#37435)
    
    When the accumulator is an `RTreeBoundsAccumulator` rather than a `RectBoundsAccumulator` just accumulating the bounds results in incorrect results as the `rtree` would need to be aware of the constituent non-overlapping rectangles. This would work fine for `RectBoundsAccumulator` as it would just adjust its bounds based on the passed rects.
    
    Fixes: https://github.com/flutter/flutter/issues/113251
    
    * Add a test for nested display list rtree

--- display_list/display_list_utils.h
+++ display_list/display_list_utils.h
@@ -337,6 +337,11 @@ class ClipBoundsDispatchHelper : public virtual Dispatcher,
   void intersect(const SkRect& clipBounds, bool is_aa);
 };
 
+enum class BoundsAccumulatorType {
+  kRect,
+  kRTree,
+};
+
 class BoundsAccumulator {
  public:
   /// function definition for modifying the bounds of a rectangle
@@ -393,6 +398,8 @@ class BoundsAccumulator {
   virtual bool restore(
       std::function<bool(const SkRect& original, SkRect& modified)> map,
       const SkRect* clip = nullptr) = 0;
+
+  virtual BoundsAccumulatorType type() const = 0;
 };
 
 class RectBoundsAccumulator final : public virtual BoundsAccumulator {
@@ -412,6 +419,10 @@ class RectBoundsAccumulator final : public virtual BoundsAccumulator {
   SkRect bounds() const {
     FML_DCHECK(saved_rects_.empty());
     return rect_.bounds();
+  }
+
+  BoundsAccumulatorType type() const override {
+    return BoundsAccumulatorType::kRect;
   }
 
  private:
@@ -454,6 +465,10 @@ class RTreeBoundsAccumulator final : public virtual BoundsAccumulator {
       const SkRect* clip = nullptr) override;
 
   sk_sp<DlRTree> rtree() const;
+
+  BoundsAccumulatorType type() const override {
+    return BoundsAccumulatorType::kRTree;
+  }
 
  private:
   std::vector<SkRect> rects_;
