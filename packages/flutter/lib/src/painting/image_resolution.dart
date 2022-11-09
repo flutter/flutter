52708a677bdd634b69b32fa62cd1a7239ce7d7b4
Andrew Kolos <andrewrkolos@gmail.com>
2022-11-08 16:21:12 -0800

    remove accidental lint ignores

--- packages/flutter/lib/src/painting/image_resolution.dart
+++ packages/flutter/lib/src/painting/image_resolution.dart
@@ -1,4 +1,3 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
 // Copyright 2014 The Flutter Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
