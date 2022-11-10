c1ceda41efd715a22ec90d6e5bfdd025b842c35d
Jenn Magder <magder@google.com>
2022-11-10 14:09:20 -0800

    Include stdout in codesign failure output (#115115)

--- packages/flutter_tools/test/general.shard/build_system/targets/ios_test.dart
+++ packages/flutter_tools/test/general.shard/build_system/targets/ios_test.dart
@@ -836,13 +836,17 @@ void main() {
         lipoCommandNonFatResult,
         lipoVerifyArm64Command,
         FakeCommand(command: <String>[
-          'codesign',
-          '--force',
-          '--sign',
-          'ABC123',
-          '--timestamp=none',
-          binary.path,
-        ], exitCode: 1, stderr: 'codesign error'),
+            'codesign',
+            '--force',
+            '--sign',
+            'ABC123',
+            '--timestamp=none',
+            binary.path,
+          ],
+          exitCode: 1,
+          stderr: 'codesign error',
+          stdout: 'codesign info',
+        ),
       ]);
 
       await expectLater(
@@ -850,7 +854,7 @@ void main() {
         throwsA(isException.having(
           (Exception exception) => exception.toString(),
           'description',
-          contains('Failed to codesign output/Flutter.framework/Flutter with identity ABC123.\ncodesign error'),
+          contains('Failed to codesign output/Flutter.framework/Flutter with identity ABC123.\ncodesign info\ncodesign error'),
         )),
       );
 
