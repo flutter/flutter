18d4a6a9831197e8f49d0f97a3ad332454ca2e8f
darrenaustin <darrenaustin@google.com>
2022-11-10 14:00:14 -0800

    Updated example from review comments.

--- examples/api/lib/material/segmented_button/segmented_button.0.dart
+++ examples/api/lib/material/segmented_button/segmented_button.0.dart
@@ -48,7 +48,7 @@ class SingleChoice extends StatefulWidget {
 
 class _SingleChoiceState extends State<SingleChoice> {
 
-  Calendar _selected = Calendar.day;
+  Calendar calendarView = Calendar.day;
 
   @override
   Widget build(BuildContext context) {
@@ -59,10 +59,13 @@ class _SingleChoiceState extends State<SingleChoice> {
         ButtonSegment<Calendar>(value: Calendar.month, label: Text('Month'), icon: Icon(Icons.calendar_view_month)),
         ButtonSegment<Calendar>(value: Calendar.year, label: Text('Year'), icon: Icon(Icons.calendar_today)),
       ],
-      selected: <Calendar>{_selected},
-      onSelectionChanged: (Set<Calendar> selected) {
+      selected: <Calendar>{calendarView},
+      onSelectionChanged: (Set<Calendar> newSelection) {
         setState(() {
-          _selected = selected.first;
+          // By default there is only a single segment that can be
+          // selected at one time, so its value is always the first
+          // item in the selected set.
+          calendarView = newSelection.first;
         });
       },
     );
@@ -79,7 +82,7 @@ class MultipleChoice extends StatefulWidget {
 }
 
 class _MultipleChoiceState extends State<MultipleChoice> {
-  Set<Sizes> _selected = <Sizes>{Sizes.large, Sizes.extraLarge};
+  Set<Sizes> selection = <Sizes>{Sizes.large, Sizes.extraLarge};
 
   @override
   Widget build(BuildContext context) {
@@ -91,10 +94,10 @@ class _MultipleChoiceState extends State<MultipleChoice> {
         ButtonSegment<Sizes>(value: Sizes.large, label: Text('L'),),
         ButtonSegment<Sizes>(value: Sizes.extraLarge, label: Text('XL')),
       ],
-      selected: _selected,
-      onSelectionChanged: (Set<Sizes> selected) {
+      selected: selection,
+      onSelectionChanged: (Set<Sizes> newSelection) {
         setState(() {
-          _selected = selected;
+          selection = newSelection;
         });
       },
       multiSelectionEnabled: true,
