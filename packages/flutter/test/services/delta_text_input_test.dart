87eafb874774d4299202a4afedcc4e2cd0092951
Xilai Zhang <xilaizhang@google.com>
2022-11-11 10:01:11 -0800

    Revert "Revert "Revert "Scribble mixin (#104128)" (#114647)" (#114698)" (#115146)
    This reverts commit 3cde69e8d9dff7e081cb6daa6cd633c49176cb32.

--- packages/flutter/test/services/delta_text_input_test.dart
+++ packages/flutter/test/services/delta_text_input_test.dart
@@ -272,6 +272,21 @@ class FakeDeltaTextInputClient implements DeltaTextInputClient {
   }
 
   @override
+  void insertTextPlaceholder(Size size) {
+    latestMethodCall = 'insertTextPlaceholder';
+  }
+
+  @override
+  void removeTextPlaceholder() {
+    latestMethodCall = 'removeTextPlaceholder';
+  }
+
+  @override
+  void showToolbar() {
+    latestMethodCall = 'showToolbar';
+  }
+
+  @override
   void performSelector(String selectorName) {
     latestMethodCall = 'performSelector';
   }
