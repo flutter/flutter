ca7f823bdf57b5c31464175c15c1c32015268211
asiva <asiva@google.com>
2022-11-11 13:49:25 -0800

    Format.

--- packages/flutter/test/foundation/_compute_caller_unsound_null_safety.dart
+++ packages/flutter/test/foundation/_compute_caller_unsound_null_safety.dart
@@ -1,7 +1,7 @@
 // Copyright 2014 The Flutter Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-  
+
 // Running in unsound null-safety mode is intended to test for potential miscasts
 // or invalid assertions.
 
