3a056663df5d9ad8c9d3ed094d01ac6e292a65ea
Chris Bracken <chris@bracken.jp>
2022-11-16 09:56:22 -0800

    [macOS] Merge FlutterMetalCompositor, superclass (flutter/engine#37648)
    Now that OpenGL support in Flutter on macOS has been removed, the only
    FlutterCompositor implementation is the Metal-based compositor. This
    patch merges it into the FlutterCompositor base class since there's no
    longer any reason to keep the interface separate from its implementation.
    
    This patch will be followed by similar patches for the renderer, surface
    manager, and external textures.
    
    Issue: https://github.com/flutter/flutter/issues/108304
    Issue: https://github.com/flutter/flutter/issues/114445

--- engine/src/flutter/shell/platform/darwin/macos/framework/Source/FlutterCompositor.h
+++ engine/src/flutter/shell/platform/darwin/macos/framework/Source/FlutterCompositor.h
@@ -9,6 +9,7 @@
 #include <list>
 
 #include "flutter/fml/macros.h"
+#import "flutter/shell/platform/darwin/macos/framework/Source/FlutterPlatformViewController.h"
 #include "flutter/shell/platform/darwin/macos/framework/Source/FlutterView.h"
 #include "flutter/shell/platform/darwin/macos/framework/Source/FlutterViewProvider.h"
 #include "flutter/shell/platform/embedder/embedder.h"
@@ -25,52 +26,57 @@ class FlutterCompositor {
   // The view_provider is used to query FlutterViews from view IDs,
   // which are used for presenting and creating backing stores.
   // It must not be null, and is typically FlutterViewEngineProvider.
-  explicit FlutterCompositor(id<FlutterViewProvider> view_provider);
+  explicit FlutterCompositor(
+      id<FlutterViewProvider> view_provider,
+      FlutterPlatformViewController* platform_views_controller,
+      id<MTLDevice> mtl_device);
 
-  virtual ~FlutterCompositor() = default;
+  ~FlutterCompositor() = default;
 
-  // Creates a BackingStore and saves updates the backing_store_out
-  // data with the new BackingStore data.
-  // If the backing store is being requested for the first time
-  // for a given frame, this compositor does not create a new backing
-  // store but rather returns the backing store associated with the
-  // FlutterView's FlutterSurfaceManager.
+  // Creates a backing store and saves updates the backing_store_out data with
+  // the new FlutterBackingStore data.
   //
-  // Any additional state allocated for the backing store and
-  // saved as user_data in the backing store must be collected
-  // in the backing_store's destruction_callback field which will
-  // be called when the embedder collects the backing store.
-  virtual bool CreateBackingStore(const FlutterBackingStoreConfig* config,
-                                  FlutterBackingStore* backing_store_out) = 0;
+  // If the backing store is being requested for the first time for a given
+  // frame, this compositor does not create a new backing store but rather
+  // returns the backing store associated with the FlutterView's
+  // FlutterSurfaceManager.
+  //
+  // Any additional state allocated for the backing store and saved as
+  // user_data in the backing store must be collected in the backing_store's
+  // destruction_callback field which will be called when the embedder collects
+  // the backing store.
+  bool CreateBackingStore(const FlutterBackingStoreConfig* config,
+                          FlutterBackingStore* backing_store_out);
 
-  // Releases the memory for any state used by the backing store.
-  virtual bool CollectBackingStore(
-      const FlutterBackingStore* backing_store) = 0;
+  // Releases the memory for any resources that were allocated for the
+  // specified backing store.
+  bool CollectBackingStore(const FlutterBackingStore* backing_store);
 
   // Presents the FlutterLayers by updating the FlutterView specified by
-  // `view_id` using the layer content.
-  // Present sets frame_started_ to false.
-  virtual bool Present(uint64_t view_id,
-                       const FlutterLayer** layers,
-                       size_t layers_count) = 0;
+  // `view_id` using the layer content. Sets frame_started_ to false.
+  bool Present(uint64_t view_id,
+               const FlutterLayer** layers,
+               size_t layers_count);
 
+  // Callback triggered at the end of the Present function. has_flutter_content
+  // is true when Flutter content was rendered, otherwise false.
   using PresentCallback = std::function<bool(bool has_flutter_content)>;
 
-  // PresentCallback is called at the end of the Present function.
+  // Registers a callback to be triggered at the end of the Present function.
+  // If a callback was previously registered, it will be replaced.
   void SetPresentCallback(const PresentCallback& present_callback);
 
-  // Denotes the current status of the frame being composited.
-  // Started: A new frame has begun and we have cleared the old layer tree
-  //          and are now creating backingstore(s) for the embedder to use.
+  // The status of the frame being composited.
+  // Started: A new frame has begun and we have cleared the old layer tree and
+  //     are now creating backingstore(s) for the embedder to use.
   // Presenting: the embedder has finished rendering into the provided
-  //             backingstore(s) and we are creating the layer tree for the
-  //             system compositor to present with.
-  // Ended: The frame has been presented and we are no longer processing
-  //        it.
+  //     backingstore(s) and we are creating the layer tree for the system
+  //     compositor to present with.
+  // Ended: The frame has been presented and we are no longer processing it.
   typedef enum { kStarted, kPresenting, kEnded } FrameStatus;
 
  protected:
-  // Get the view associated with the view ID.
+  // Returns the view associated with the view ID.
   //
   // Returns nil if the ID is invalid.
   FlutterView* GetView(uint64_t view_id);
@@ -94,11 +100,24 @@ class FlutterCompositor {
       CATransform3D transform = CATransform3DIdentity);
 
  private:
+  // Presents the platform view layer represented by `layer`. `layer_index` is
+  // used to position the layer in the z-axis. If the layer does not have a
+  // superview, it will become subview of `default_base_view`.
+  void PresentPlatformView(FlutterView* default_base_view,
+                           const FlutterLayer* layer,
+                           size_t layer_position);
+
   // A list of the active CALayer objects for the frame that need to be removed.
   std::list<CALayer*> active_ca_layers_;
 
   // Where the compositor can query FlutterViews. Must not be null.
   id<FlutterViewProvider> const view_provider_;
+
+  // The controller used to manage creation and deletion of platform views.
+  const FlutterPlatformViewController* platform_view_controller_;
+
+  // The Metal device used to draw graphics.
+  const id<MTLDevice> mtl_device_;
 
   // Callback set by the embedder to be called when the layer tree has been
   // correctly set up for this frame.
--- engine/src/flutter/shell/platform/darwin/macos/framework/Source/FlutterCompositor.mm
+++ engine/src/flutter/shell/platform/darwin/macos/framework/Source/FlutterCompositor.mm
@@ -3,13 +3,139 @@
 // found in the LICENSE file.
 
 #import "flutter/shell/platform/darwin/macos/framework/Source/FlutterCompositor.h"
+
 #include "flutter/fml/logging.h"
+#import "flutter/shell/platform/darwin/macos/framework/Source/FlutterIOSurfaceHolder.h"
 
 namespace flutter {
 
-FlutterCompositor::FlutterCompositor(id<FlutterViewProvider> view_provider)
-    : view_provider_(view_provider) {
-  FML_CHECK(view_provider != nullptr) << "FlutterViewProvider* cannot be nullptr";
+FlutterCompositor::FlutterCompositor(id<FlutterViewProvider> view_provider,
+                                     FlutterPlatformViewController* platform_view_controller,
+                                     id<MTLDevice> mtl_device)
+    : view_provider_(view_provider),
+      platform_view_controller_(platform_view_controller),
+      mtl_device_(mtl_device) {
+  FML_CHECK(view_provider != nullptr) << "view_provider cannot be nullptr";
+}
+
+bool FlutterCompositor::CreateBackingStore(const FlutterBackingStoreConfig* config,
+                                           FlutterBackingStore* backing_store_out) {
+  // TODO(dkwingsmt): This class only supports single-view for now. As more
+  // classes are gradually converted to multi-view, it should get the view ID
+  // from somewhere.
+  FlutterView* view = GetView(kFlutterDefaultViewId);
+  if (!view) {
+    return false;
+  }
+
+  CGSize size = CGSizeMake(config->size.width, config->size.height);
+
+  backing_store_out->metal.struct_size = sizeof(FlutterMetalBackingStore);
+  backing_store_out->metal.texture.struct_size = sizeof(FlutterMetalTexture);
+
+  if (GetFrameStatus() != FrameStatus::kStarted) {
+    StartFrame();
+    // If the backing store is for the first layer, return the MTLTexture for the
+    // FlutterView.
+    FlutterMetalRenderBackingStore* backingStore =
+        reinterpret_cast<FlutterMetalRenderBackingStore*>([view backingStoreForSize:size]);
+    backing_store_out->metal.texture.texture =
+        (__bridge FlutterMetalTextureHandle)backingStore.texture;
+  } else {
+    FlutterIOSurfaceHolder* io_surface_holder = [[FlutterIOSurfaceHolder alloc] init];
+    [io_surface_holder recreateIOSurfaceWithSize:size];
+    auto texture_descriptor =
+        [MTLTextureDescriptor texture2DDescriptorWithPixelFormat:MTLPixelFormatBGRA8Unorm
+                                                           width:size.width
+                                                          height:size.height
+                                                       mipmapped:NO];
+    texture_descriptor.usage =
+        MTLTextureUsageShaderRead | MTLTextureUsageRenderTarget | MTLTextureUsageShaderWrite;
+
+    backing_store_out->metal.texture.texture = (__bridge_retained FlutterMetalTextureHandle)
+        [mtl_device_ newTextureWithDescriptor:texture_descriptor
+                                    iosurface:[io_surface_holder ioSurface]
+                                        plane:0];
+
+    backing_store_out->metal.texture.user_data = (__bridge_retained void*)io_surface_holder;
+  }
+
+  backing_store_out->type = kFlutterBackingStoreTypeMetal;
+  backing_store_out->metal.texture.destruction_callback = [](void* user_data) {
+    if (user_data != nullptr) {
+      CFRelease(user_data);
+    }
+  };
+
+  return true;
+}
+
+bool FlutterCompositor::CollectBackingStore(const FlutterBackingStore* backing_store) {
+  // If we allocated this MTLTexture ourselves, user_data is not null, and we will need
+  // to release it manually.
+  if (backing_store->metal.texture.user_data != nullptr &&
+      backing_store->metal.texture.texture != nullptr) {
+    CFRelease(backing_store->metal.texture.texture);
+  }
+  return true;
+}
+
+bool FlutterCompositor::Present(uint64_t view_id,
+                                const FlutterLayer** layers,
+                                size_t layers_count) {
+  FlutterView* view = GetView(view_id);
+  if (!view) {
+    return false;
+  }
+
+  SetFrameStatus(FrameStatus::kPresenting);
+
+  bool has_flutter_content = false;
+  for (size_t i = 0; i < layers_count; ++i) {
+    const auto* layer = layers[i];
+    FlutterBackingStore* backing_store = const_cast<FlutterBackingStore*>(layer->backing_store);
+
+    switch (layer->type) {
+      case kFlutterLayerContentTypeBackingStore: {
+        if (backing_store->metal.texture.user_data) {
+          FlutterIOSurfaceHolder* io_surface_holder =
+              (__bridge FlutterIOSurfaceHolder*)backing_store->metal.texture.user_data;
+          IOSurfaceRef io_surface = [io_surface_holder ioSurface];
+          InsertCALayerForIOSurface(view, io_surface);
+        }
+        has_flutter_content = true;
+        break;
+      }
+      case kFlutterLayerContentTypePlatformView: {
+        PresentPlatformView(view, layer, i);
+        break;
+      }
+    };
+  }
+
+  return EndFrame(has_flutter_content);
+}
+
+void FlutterCompositor::PresentPlatformView(FlutterView* default_base_view,
+                                            const FlutterLayer* layer,
+                                            size_t layer_position) {
+  // TODO (https://github.com/flutter/flutter/issues/96668)
+  // once the issue is fixed, this check will pass.
+  FML_DCHECK([[NSThread currentThread] isMainThread])
+      << "Must be on the main thread to present platform views";
+
+  int64_t platform_view_id = layer->platform_view->identifier;
+  NSView* platform_view = [platform_view_controller_ platformViewWithID:platform_view_id];
+
+  FML_DCHECK(platform_view) << "Platform view not found for id: " << platform_view_id;
+
+  CGFloat scale = [[NSScreen mainScreen] backingScaleFactor];
+  platform_view.frame = CGRectMake(layer->offset.x / scale, layer->offset.y / scale,
+                                   layer->size.width / scale, layer->size.height / scale);
+  if (platform_view.superview == nil) {
+    [default_base_view addSubview:platform_view];
+  }
+  platform_view.layer.zPosition = layer_position;
 }
 
 void FlutterCompositor::SetPresentCallback(
