2a09f742fee619e1ea78c3a3e134f4cccfb1c03c
Andrew Kolos <andrewrkolos@gmail.com>
2022-11-16 16:26:26 -0800

    fix

--- packages/flutter/lib/src/services/asset_bundle.dart
+++ packages/flutter/lib/src/services/asset_bundle.dart
@@ -269,20 +269,31 @@ abstract class CachingAssetBundle extends AssetBundle {
       return _structuredDataBinaryCache[key]! as Future<T>;
     }
 
-    final Completer<T> completer = Completer<T>();
-    _structuredDataBinaryCache[key] = completer.future;
+    Completer<T>? completer;
+    Future<T>? result;
+
+    void handleError(Object error, StackTrace? stack) {
+      assert(completer != null);
+      assert(result == null);
+      completer!.completeError(error, stack);
+    }
 
     load(key)
-      .then((ByteData data) async {
-        final T parsed = await parser(data);
-        completer.complete(parsed);
-        _structuredDataBinaryCache[key] = SynchronousFuture<T>(parsed);
-      })
-      .onError((Object error, StackTrace stack) {
-        assert(completer != null);
-        completer.completeError(error, stack);
-      });
+      .then<T>(parser, onError: handleError)
+      .then<void>((T value) {
+        result = SynchronousFuture<T>(value);
+        _structuredDataBinaryCache[key] = result!;
+        if (completer != null) {
+          completer.complete(value);
+        }
+      }, onError: handleError);
 
+    if (result != null) {
+      return result!;
+    }
+
+    completer = Completer<T>();
+    _structuredDataBinaryCache[key] = completer.future;
     return completer.future;
   }
 
