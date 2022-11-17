d955a72c560421425948e895d6ba464c9f602ce6
Chris Bracken <chris@bracken.jp>
2022-11-17 11:46:27 -0800

    [macOS] Merge FlutterSurfaceManager and impls (#37701)
    Previously, FlutterSurfaceManager was a protocol with two concrete
    implementations: FlutterGLSurfaceManager and FlutterMetalSurfaceManager.
    Most of the implementation was in a shared superclass,
    FlutterIOSurfaceManager, which called into the OpenGL or Metal-specific
    subclass when backend-specific operations (such as allocating textures)
    was required. It did so via a delegate pattern, wherein the subclasses
    both implemented the FlutterIOSurfaceManagerDelegate protocol that
    exposed the backend-specific functionality.
    
    Now that only the Metal implementation remains, the delegate code can be
    inlined into the calling functions, and the class hierarchy can be
    squashed into a single concrete implementation class,
    FlutterSurfaceManager, similar to how it was originally implemented in
    https://github.com/flutter/engine/pull/21525 before we had two backends.
    
    Issue: https://github.com/flutter/flutter/issues/108304
    Issue: https://github.com/flutter/flutter/issues/114445

--- shell/platform/darwin/macos/framework/Source/FlutterSurfaceManager.h
+++ shell/platform/darwin/macos/framework/Source/FlutterSurfaceManager.h
@@ -9,13 +9,25 @@
 #import "flutter/shell/platform/darwin/macos/framework/Source/FlutterIOSurfaceHolder.h"
 
 /**
- * Manages the render surfaces and their corresponding backing stores.
+ * Manages render surfaces and corresponding backing stores used by the engine.
+ *
+ * The backing store when rendering with on Metal is a Metal texture. There are two IOSurfaces
+ * created during initialization, FlutterSurfaceManager manages the lifecycle of these.
  */
-@protocol FlutterSurfaceManager
+@interface FlutterSurfaceManager : NSObject
 
 /**
- * Updates the backing store size of the managed IOSurfaces to `size`. If the surfaces are already
- * of the same size, this is a no-op.
+ * Initializes and returns a surface manager that renders to a child layer (referred to as the
+ * content layer) of the containing layer and applies the transform to the contents of the content
+ * layer.
+ */
+- (nullable instancetype)initWithDevice:(nonnull id<MTLDevice>)device
+                           commandQueue:(nonnull id<MTLCommandQueue>)commandQueue
+                                  layer:(nonnull CALayer*)containingLayer;
+
+/**
+ * Updates the backing store size of the managed IOSurfaces the specified size. If the surfaces are
+ * already of this size, this is a no-op.
  */
 - (void)ensureSurfaceSize:(CGSize)size;
 
@@ -28,69 +40,5 @@
  * Returns the backing store for the back buffer.
  */
 - (nonnull FlutterRenderBackingStore*)renderBuffer;
-
-@end
-
-/**
- * Methods for managing the IOSurfaces held by FlutterIOSurfaceManager.
- */
-@protocol FlutterIOSurfaceManagerDelegate
-
-/**
- * Tells the delegate that the front and back IOSurfaces are swapped.
- */
-- (void)onSwapBuffers;
-
-/**
- * Tells the delegate that the IOSurfaces have been resized. `bufferIndex` is to indicate the front
- * vs back buffer. `size` is the new size of the IOSurface.
- */
-- (void)onUpdateSurface:(nonnull FlutterIOSurfaceHolder*)surface
-            bufferIndex:(size_t)index
-                   size:(CGSize)size;
-
-/**
- * Tells the delegate that IOSurface with given index has been released. Delegate should free
- * all resources associated with the surface
- */
-- (void)onSurfaceReleased:(size_t)index;
-
-@end
-
-/**
- * Manages IOSurfaces for the FlutterEngine to render to.
- *
- * The backing store when rendering with on Metal its a Metal texture. There are two IOSurfaces
- * created during initialization, FlutterSurfaceManager manages the lifecycle of these.
- */
-@interface FlutterIOSurfaceManager : NSObject <FlutterSurfaceManager>
-
-/**
- * The object that acts as the delegate for the FlutterIOSurfaceManager. See:
- * FlutterIOSurfaceManagerDelegate.
- */
-@property(nullable, nonatomic, weak) id<FlutterIOSurfaceManagerDelegate> delegate;
-
-/**
- * Initializes and returns an IOSurface manager that renders to a child layer (referred to as the
- * content layer) of the containing layer and applies the transform to the contents of the content
- * layer.
- */
-- (nullable instancetype)initWithLayer:(nonnull CALayer*)containingLayer
-                      contentTransform:(CATransform3D)transform;
-
-@end
-
-/**
- * FlutterSurfaceManager implementation where the IOSurfaces managed are backed by a Metal textures.
- */
-@interface FlutterMetalSurfaceManager : FlutterIOSurfaceManager <FlutterIOSurfaceManagerDelegate>
-
-/**
- * Creates two IOSurfaces backed by Metal textures.
- */
-- (nullable instancetype)initWithDevice:(nonnull id<MTLDevice>)device
-                           commandQueue:(nonnull id<MTLCommandQueue>)commandQueue
-                                  layer:(nonnull CALayer*)containingLayer;
 
 @end
