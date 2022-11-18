07f9e8ee26d038590d4f165a8ad7fdc1d0e42772
fzyzcjy <5236035+fzyzcjy@users.noreply.github.com>
2022-11-19 03:38:10 +0800

    Tiny improvement of RouteSettings display (#114481)
    * impl
    
    * Update packages/flutter/lib/src/widgets/navigator.dart
    
    Co-authored-by: chunhtai <47866232+chunhtai@users.noreply.github.com>
    
    * Update navigator_test.dart
    
    Co-authored-by: chunhtai <47866232+chunhtai@users.noreply.github.com>

--- packages/flutter/lib/src/widgets/navigator.dart
+++ packages/flutter/lib/src/widgets/navigator.dart
@@ -548,7 +548,7 @@ class RouteSettings {
   final Object? arguments;
 
   @override
-  String toString() => '${objectRuntimeType(this, 'RouteSettings')}("$name", $arguments)';
+  String toString() => '${objectRuntimeType(this, 'RouteSettings')}(${name == null ? 'none' : '"$name"'}, $arguments)';
 }
 
 /// Describes the configuration of a [Route].
