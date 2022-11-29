9f5d0cbd8d8ea713f20375949d9793ba34736194
Qun Cheng <quncheng@google.com>
2022-11-29 10:22:25 -0800

    Removed a unused import

--- packages/flutter/lib/src/material/menu_anchor.dart
+++ packages/flutter/lib/src/material/menu_anchor.dart
@@ -5,7 +5,6 @@
 import 'dart:math' as math;
 
 import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:flutter/services.dart';
