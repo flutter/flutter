88d09c4db18b365715d8b090ebdbd1e706049652
Qun Cheng <quncheng@google.com>
2022-11-29 10:42:21 -0800

    Removed unused import

--- packages/flutter/lib/src/material/dropdown_menu_theme.dart
+++ packages/flutter/lib/src/material/dropdown_menu_theme.dart
@@ -4,7 +4,6 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
 
 import 'input_decorator.dart';
 import 'menu_style.dart';
