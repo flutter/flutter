f077227a92665bec4451eacfdc602ceaa9ed8642
Tae Hyung Kim <taehykim@google.com>
2022-11-29 11:17:10 -0800

    fix lint

--- packages/flutter_tools/lib/src/localizations/gen_l10n_types.dart
+++ packages/flutter_tools/lib/src/localizations/gen_l10n_types.dart
@@ -2,10 +2,10 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:flutter_tools/src/base/logger.dart';
 import 'package:intl/locale.dart';
 
 import '../base/file_system.dart';
+import '../base/logger.dart';
 import '../convert.dart';
 import 'localizations_utils.dart';
 import 'message_parser.dart';
