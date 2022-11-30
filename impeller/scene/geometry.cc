787044d2c2d3b33b894ed8ea9e8c31a2f0c675fe
Brandon DeRosier <bdero@google.com>
2022-11-29 23:38:10 -0800

    [Impeller Scene] Command encoding (#37977)

--- impeller/scene/geometry.cc
+++ impeller/scene/geometry.cc
@@ -6,6 +6,12 @@
 
 #include <memory>
 
+#include "impeller/geometry/point.h"
+#include "impeller/geometry/vector.h"
+#include "impeller/renderer/formats.h"
+#include "impeller/renderer/vertex_buffer_builder.h"
+#include "impeller/scene/shaders/geometry.vert.h"
+
 namespace impeller {
 namespace scene {
 
@@ -27,9 +33,19 @@ void CuboidGeometry::SetSize(Vector3 size) {
   size_ = size;
 }
 
-VertexBuffer CuboidGeometry::GetVertexBuffer(
-    std::shared_ptr<Allocator>& allocator) const {
-  return {};
+VertexBuffer CuboidGeometry::GetVertexBuffer(Allocator& allocator) const {
+  VertexBufferBuilder<GeometryVertexShader::PerVertexData, uint16_t> builder;
+  // Layout: position, normal, tangent, uv
+  builder.AddVertices({
+      // Front.
+      {Vector3(0, 0, 0), Vector3(0, 0, -1), Vector3(1, 0, 0), Point(0, 0)},
+      {Vector3(1, 0, 0), Vector3(0, 0, -1), Vector3(1, 0, 0), Point(1, 0)},
+      {Vector3(1, 1, 0), Vector3(0, 0, -1), Vector3(1, 0, 0), Point(1, 1)},
+      {Vector3(1, 1, 0), Vector3(0, 0, -1), Vector3(1, 0, 0), Point(1, 1)},
+      {Vector3(0, 1, 0), Vector3(0, 0, -1), Vector3(1, 0, 0), Point(0, 1)},
+      {Vector3(0, 0, 0), Vector3(0, 0, -1), Vector3(1, 0, 0), Point(0, 0)},
+  });
+  return builder.CreateVertexBuffer(allocator);
 }
 
 }  // namespace scene
