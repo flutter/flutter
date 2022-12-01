193405c09cba99fc6f0753396981cadfb0712018
Aleksandr Denisov <denisov-alexandr@yandex.ru>
2022-12-01 22:24:04 +0100

    Remove glitches when scrolling on old Android TV devices (flutter/engine#37493)

--- engine/src/flutter/shell/platform/android/android_egl_surface.cc
+++ engine/src/flutter/shell/platform/android/android_egl_surface.cc
@@ -72,8 +72,8 @@ static bool HasExtension(const char* extensions, const char* name) {
 class AndroidEGLSurfaceDamage {
  public:
   void init(EGLDisplay display, EGLContext context) {
-    if (GetAPILevel() < 28) {
-      // Disable partial repaint for devices older than Android 9. There
+    if (GetAPILevel() < 29) {
+      // Disable partial repaint for devices older than Android 10. There
       // are old devices that have extensions below available but the
       // implementation causes glitches (i.e. Xperia Z3 with Android 6).
       partial_redraw_supported_ = false;
