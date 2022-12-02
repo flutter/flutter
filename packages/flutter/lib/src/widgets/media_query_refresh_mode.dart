372c25529fba4840b35ba4f10f554f1ab0a10605
luckysmg <2539699336@qq.com>
2022-12-02 10:31:14 +0800

    Update doc

--- packages/flutter/lib/src/widgets/media_query_refresh_mode.dart
+++ packages/flutter/lib/src/widgets/media_query_refresh_mode.dart
@@ -7,6 +7,7 @@ import 'media_query.dart';
 
 /// A widget that can optionally avoid refreshing [MediaQueryData] that delivered
 /// to subtree when parent [MediaQueryData] dependency has changed.
+/// This can prevent subtree from being rebuilt when unnecessary.
 class MediaQueryRefreshMode extends StatefulWidget {
   /// The [enabled] and [child] arguments must not be null.
   const MediaQueryRefreshMode({
@@ -16,6 +17,9 @@ class MediaQueryRefreshMode extends StatefulWidget {
   });
 
   /// Enables or disables to refresh the [MediaQueryData] delivered to subtree.
+  ///
+  /// Default is true. If set false, when parent update [MediaQueryData], the refresh signal
+  /// will not deliver to subtree, so the subtree will not rebuild.
   final bool enabled;
 
   /// The widget below this widget in the tree.
