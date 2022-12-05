fd338b4c8c760b9cfa697d4161293b0badc8f97f
barringb <barringb@gmail.com>
2022-12-05 10:55:19 -0800

    Update IntegrationTestPlugin.m

--- packages/integration_test/ios/Classes/IntegrationTestPlugin.m
+++ packages/integration_test/ios/Classes/IntegrationTestPlugin.m
@@ -46,10 +46,6 @@ static NSString *const kMethodRevertImage = @"revertFlutterImage";
 
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar> *)registrar {
   [[IntegrationTestPlugin instance] setupChannels:registrar.messenger];
-  // No initialization happens here because of the way XCTest loads the testing
-  // bundles.  Setup on static variables can be disregarded when a new static
-  // instance of IntegrationTestPlugin is allocated when the bundle is reloaded.
-  // See also: https://github.com/flutter/plugins/pull/2465
 }
 
 - (void)setupChannels:(id<FlutterBinaryMessenger>)binaryMessenger {
