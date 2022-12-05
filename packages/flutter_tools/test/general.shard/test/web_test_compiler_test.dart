ebcf8eff247ebaac472067e2428fcf7768f9412d
Jackson Gardner <jacksongardner@google.com>
2022-12-05 15:04:11 -0800

    License header.

--- packages/flutter_tools/test/general.shard/test/web_test_compiler_test.dart
+++ packages/flutter_tools/test/general.shard/test/web_test_compiler_test.dart
@@ -1,3 +1,7 @@
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import 'package:file/memory.dart';
 import 'package:flutter_tools/src/artifacts.dart';
 import 'package:flutter_tools/src/base/config.dart';
