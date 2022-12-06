27c5ad762eaa9307334b7a7611839701567063f4
Greg Spencer <gspencer@google.com>
2022-12-05 16:28:09 -0800

    Fix Test

--- examples/api/test/widgets/transitions/listenable_builder.0_test.dart
+++ examples/api/test/widgets/transitions/listenable_builder.0_test.dart
@@ -11,7 +11,8 @@ void main() {
     await tester.pumpWidget(const example.ListenableBuilderExample());
 
     Finder findContainer() => find.descendant(of: find.byType(example.FocusListenerContainer), matching: find.byType(Container)).first;
-    bool containerHasFocus() => Focus.of(tester.element(findContainer())).hasFocus;
+    Finder findChild() => find.descendant(of: findContainer(), matching: find.byType(Column)).first;
+    bool childHasFocus() => Focus.of(tester.element(findChild())).hasFocus;
     Container getContainer() => tester.widget(findContainer()) as Container;
     ShapeDecoration getDecoration() => getContainer().decoration! as ShapeDecoration;
     OutlinedBorder getBorder() => getDecoration().shape as OutlinedBorder;
@@ -22,20 +23,20 @@ void main() {
 
     await tester.tap(find.byType(TextField).first);
     await tester.pumpAndSettle();
-    expect(containerHasFocus(), isFalse);
+    expect(childHasFocus(), isFalse);
     expect(getBorder().side.width, equals(1));
     expect(getContainer().color, isNull);
     expect(getDecoration().color, isNull);
 
     await tester.tap(find.byType(TextField).at(1));
     await tester.pumpAndSettle();
-    expect(containerHasFocus(), isTrue);
+    expect(childHasFocus(), isTrue);
     expect(getBorder().side.width, equals(4));
     expect(getDecoration().color, equals(Colors.blue.shade50));
 
     await tester.tap(find.byType(TextField).at(2));
     await tester.pumpAndSettle();
-    expect(containerHasFocus(), isTrue);
+    expect(childHasFocus(), isTrue);
     expect(getBorder().side.width, equals(4));
     expect(getDecoration().color, equals(Colors.blue.shade50));
   });
