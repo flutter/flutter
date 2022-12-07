52668a7252f7a26749fb5a88f30795ea1d63c35f
fzyzcjy <5236035+fzyzcjy@users.noreply.github.com>
2022-12-08 02:46:06 +0800

    Reland fix wrong VSYNC event (flutter/engine#37865)

--- engine/src/flutter/shell/common/vsync_waiter_fallback.cc
+++ engine/src/flutter/shell/common/vsync_waiter_fallback.cc
@@ -35,13 +35,17 @@ VsyncWaiterFallback::~VsyncWaiterFallback() = default;
 
 // |VsyncWaiter|
 void VsyncWaiterFallback::AwaitVSync() {
-  TRACE_EVENT0("flutter", "VSYNC");
-
   constexpr fml::TimeDelta kSingleFrameInterval =
       fml::TimeDelta::FromSecondsF(1.0 / 60.0);
   auto frame_start_time =
       SnapToNextTick(fml::TimePoint::Now(), phase_, kSingleFrameInterval);
   auto frame_target_time = frame_start_time + kSingleFrameInterval;
+
+  TRACE_EVENT2_INT("flutter", "PlatformVsync", "frame_start_time",
+                   frame_start_time.ToEpochDelta().ToMicroseconds(),
+                   "frame_target_time",
+                   frame_target_time.ToEpochDelta().ToMicroseconds());
+
   std::weak_ptr<VsyncWaiterFallback> weak_this =
       std::static_pointer_cast<VsyncWaiterFallback>(shared_from_this());
 
