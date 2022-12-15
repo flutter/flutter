4fc6f7168bcf1c8099f88ae274338641f23f4939
Chris Bracken <chris@bracken.jp>
2022-12-15 08:25:01 -0800

    [linux] Allow overriding asset, ICU data path (flutter/engine#38296)
    Support setting a custom asset directory and ICU data path.
    
    Fixes: https://github.com/flutter/flutter/issues/117103
    Issue: https://github.com/flutter/flutter/issues/117102

--- engine/src/flutter/shell/platform/linux/public/flutter_linux/fl_dart_project.h
+++ engine/src/flutter/shell/platform/linux/public/flutter_linux/fl_dart_project.h
@@ -74,6 +74,17 @@ gboolean fl_dart_project_get_enable_mirrors(FlDartProject* project)
 const gchar* fl_dart_project_get_aot_library_path(FlDartProject* project);
 
 /**
+ * fl_dart_project_set_assets_path:
+ * @project: an #FlDartProject.
+ * @path: the absolute path to the assets directory.
+ *
+ * Sets the path to the directory containing the assets used in the Flutter
+ * application. By default, this is the data/flutter_assets subdirectory
+ * relative to the executable directory.
+ */
+void fl_dart_project_set_assets_path(FlDartProject* project, gchar* path);
+
+/**
  * fl_dart_project_get_assets_path:
  * @project: an #FlDartProject.
  *
@@ -84,6 +95,16 @@ const gchar* fl_dart_project_get_aot_library_path(FlDartProject* project);
  * "/projects/my_dart_project/data/flutter_assets".
  */
 const gchar* fl_dart_project_get_assets_path(FlDartProject* project);
+
+/**
+ * fl_dart_project_set_icu_data_path:
+ * @project: an #FlDartProject.
+ * @path: the absolute path to the ICU data file.
+ *
+ * Sets the path to the ICU data file used in the Flutter application. By
+ * default, this is data/icudtl.dat relative to the executable directory.
+ */
+void fl_dart_project_set_icu_data_path(FlDartProject* project, gchar* path);
 
 /**
  * fl_dart_project_get_icu_data_path:
