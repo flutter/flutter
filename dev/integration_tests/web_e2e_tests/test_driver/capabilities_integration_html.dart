ff6bcc7b8756a90ba6e9c13a6f6d59d9ecee9b9d
alanwutang11 <alanwumail11@gmail.com>
2022-12-16 11:03:33 -0800

    simplified tests

--- dev/integration_tests/web_e2e_tests/test_driver/capabilities_integration_html.dart
+++ dev/integration_tests/web_e2e_tests/test_driver/capabilities_integration_html.dart
@@ -2,20 +2,14 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
-import 'package:web_e2e_tests/capabilities_main.dart' as app;
-
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
   testWidgets('Capabilities integration test', (WidgetTester tester) async {
-    app.main();
     await tester.pumpAndSettle();
-    final Finder textFinder = find.byKey(const Key('isCanvasKit bool is false'));
-    expect(textFinder, findsOneWidget);
-    final Text text = tester.widget(textFinder);
-    expect(text.data, 'The app is not canvasKit');
+    expect(isCanvasKit, false);
   });
 }
