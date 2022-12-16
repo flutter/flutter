4cade4f92c871658a9ac292eef47a5a103603081
Loïc Sharma <sharma.loic@gmail.com>
2022-12-15 17:18:27 -0800

    Try NSLog

--- examples/hello_world/macos/Runner/MainFlutterWindow.swift
+++ examples/hello_world/macos/Runner/MainFlutterWindow.swift
@@ -7,17 +7,17 @@ import FlutterMacOS
 
 class MainFlutterWindow: NSWindow {
   override func awakeFromNib() {
-    print("MainFlutterWindow.awakeFromNit: creating view controller...");
+    NSLog("MainFlutterWindow.awakeFromNit: creating view controller...");
     let flutterViewController = FlutterViewController.init()
     let windowFrame = self.frame
     self.contentViewController = flutterViewController
     self.setFrame(windowFrame, display: true)
-    print("MainFlutterWindow.awakeFromNit: registering plugins...");
+    NSLog("MainFlutterWindow.awakeFromNit: registering plugins...");
 
     RegisterGeneratedPlugins(registry: flutterViewController)
-    print("MainFlutterWindow.awakeFromNit: registered plugins");
+    NSLog("MainFlutterWindow.awakeFromNit: registered plugins");
 
     super.awakeFromNib()
-    print("MainFlutterWindow.awakeFromNit: done");
+    NSLog("MainFlutterWindow.awakeFromNit: done");
   }
 }
