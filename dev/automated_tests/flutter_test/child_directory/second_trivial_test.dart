f58d07211e6f4e3ca9fdd677530507502b1a9a27
retabaitas <114511115+retabaitas@users.noreply.github.com>
2022-12-18 11:49:19 +0100

    v3.3.0

--- dev/automated_tests/flutter_test/child_directory/second_trivial_test.dart
+++ dev/automated_tests/flutter_test/child_directory/second_trivial_test.dart
@@ -2,8 +2,10 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// ignore_for_file: depend_on_referenced_packages
+
 import 'package:test/test.dart' hide TypeMatcher, isInstanceOf;
 
 void main() {
-  test('trivial', () { });
+  test('trivial', () {});
 }
