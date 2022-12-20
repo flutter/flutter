8a1d09da90bb43f9f22ed800448b33c365f4dbf8
Michael Goderbauer <goderbauer@google.com>
2022-12-19 18:54:39 -0800

    Prep for dangling_library_doc_comments and library_annotations lint

--- packages/flutter/test/widgets/draggable_test.dart
+++ packages/flutter/test/widgets/draggable_test.dart
@@ -7,6 +7,7 @@
 // https://github.com/flutter/flutter/issues/85160
 // Fails with "flutter test --test-randomize-ordering-seed=123"
 @Tags(<String>['no-shuffle'])
+library;
 
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
