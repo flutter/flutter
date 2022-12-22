23582159af46fe219bcf4da7ad894865ff7b3f91
David Iglesias <ditman@gmail.com>
2022-12-21 17:15:06 -0800

    [web] Render in custom target (#37738)
    * Introduce FullScreenApplicationDom, and wire it to meta viewport, event handlers and hot restart.
    
    * Move internal stylesheet to HostNode from ViewEmbedder.
    
    * Add setHostStyles and Attribute to ApplicationDom. Use it in the embedder.
    
    * Move HotRestartCacheHandler to its own file.
    
    * Remove Safari hack for visualViewport.
    
    * No need to keep a ref to the viewport meta in full-screen.
    
    * Add applicationDom.attachGlassPane and use it in the Embedder.
    
    * Remove empty method bodies.
    
    * Add attachResourcesHost and use it from the embedder.
    
    * Removed some unused code.
    
    * Some more cleanup.
    
    * Add ResizeObserver JS interop API.
    
    * Add the CustomElementApplicationDom and wire it to the ViewEmbedder.
    
    * Add the DimensionsProvider classes.
    
    * Reimplement engine.window using the DimensionsProvider.
    
    * Delegate window metrics to engine window in html scene object.
    
    * Wire DimensionsProvider into engine.window.
    
    * Moved ApplicationDom into its own subdir.
    
    * Make DimensionsProvider also an Observer. Expose onResize Stream.
    
    * Delegate onResize and dpr from window to DimensionsObserver object.
    
    * Remove or make most ApplicationDom methods private. Expose single initializeHost.
    
    * Hook the new API.
    
    * dart format
    
    * ApplicationDom -> EmbeddingStrategy.
    
    * Attach pointer move events to glassPaneElement
    
    * Use offset positions for mouse events (relative to host element) rather than client (relative to viewport)
    
    * Update TouchAdapter to understand scrolling (simulate offsetX/Y)
    
    * Remove locale change handling from the embedding strategy.
    
    Also, remove DomSubscription handling from the
    hot_restart_cache_handler, now that it is not needed.
    
    * Move locale handling from the embedder to the platform dispatcher
    
    * Move some styles from host to glassPane so we are more friendly with external CSS.
    
    * Make analyzer fixes
    
    * Ensure DimensionsProvider is available in tests.
    
    * Initialize the view DimensionsProvider next to where the EmbeddingStrategy is decided (more logical)
    
    * Bring back the logic to support Firefox 83.
    
    * Fix pointer_binding test for new anchor point in the DOM.
    
    * Fix pointer_binding_test in Firefox.
    
    * Add an iterable way of accessing 'rules'
    
    From a CSSStyleSheet object.
    
    Also add the cssText getter for a CSSRule so we can parse it later.
    
    * Merge latest changes to host_node stylesheet.
    
    * Add an id to the StyleSheet element that we add, so it can be selected
      later (in tests).
    * Use the methods coming from browser_detection.dart to determine the
      browser runtime, instead of re-implementing them within the method.
    * Merge the Edge stylesheet into the general one.
    * Update tests so they can look at the CSS Rules that were added.
    
    * Format test
    
    * Try to use insertRule for -ms-reveal, and fallback in tests.
    
    * Test hot_restart_cache_handler
    
    Simplify API a little bit, make clear method private.
    
    * Test dimensions_provider.
    
    * Test full_page_dimensions_provider
    
    * Test custom_element_dimensions_provider
    
    * Test embedding_strategy. Make getDomCache util public.
    
    * Fixes and tests for *_embedding_strategy.
    
    * Move default text colors to our innermost style inside host_node (apply only to flt-scene-host). Remove code from the embedding strategies, and adjust tests.
    
    * Safari expands shorthand properties in CSSOM.
    
    Check individually for both font-family and font-size in Safari, rather
    than font in the host_node_test.
    
    * Add computeEventOffsetToTarget function, and use it.
    
    * Address PR comments.
    
    * Update licenses_flutter.
    
    * Remove DomCSSRuleList class and instead use Iterable of DomCSSRule
    
    * Make the embeddingStrategy final instead of late
    
    * Attach mouse/pointermove events to domWindow.
    
    * Rename DimensionsProvider.onHotRestart to .close, and slightly improve docs.
    
    * Fix compute physicalX/Y for TalkBack events.
    
    Extracted compute function to a helper file.
    
    * Clarify what does (and does not) support 3D transforms in the event_position_helper file.
    
    * Update licenses file

---
+++ lib/web_ui/lib/src/engine/view_embedder/dimensions_provider/custom_element_dimensions_provider.dart
@@ -0,0 +1,88 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'dart:async';
+
+import 'package:ui/src/engine/dom.dart';
+import 'package:ui/src/engine/window.dart';
+import 'package:ui/ui.dart' as ui show Size;
+
+import 'dimensions_provider.dart';
+
+/// This class provides observable, real-time dimensions of a host element.
+///
+/// All the measurements returned from this class are potentially *expensive*,
+/// and should be cached as needed. Every call to every method on this class
+/// WILL perform actual DOM measurements.
+class CustomElementDimensionsProvider extends DimensionsProvider {
+  /// Creates a [CustomElementDimensionsProvider] from a [_hostElement].
+  CustomElementDimensionsProvider(this._hostElement) {
+    // Hook up a resize observer on the hostElement (if supported!).
+    _hostElementResizeObserver = createDomResizeObserver((
+      List<DomResizeObserverEntry> entries,
+      DomResizeObserver _,
+    ) {
+      entries
+          .map((DomResizeObserverEntry entry) =>
+              ui.Size(entry.contentRect.width, entry.contentRect.height))
+          .forEach(_broadcastSize);
+    });
+
+    assert(() {
+      if (_hostElementResizeObserver == null) {
+        domWindow.console.warn('ResizeObserver API not supported. '
+            'Flutter will not resize with its hostElement.');
+      }
+      return true;
+    }());
+
+    _hostElementResizeObserver?.observe(_hostElement);
+  }
+
+  // The host element that will be used to retrieve (and observe) app size measurements.
+  final DomElement _hostElement;
+
+  // Handle resize events
+  late DomResizeObserver? _hostElementResizeObserver;
+  final StreamController<ui.Size> _onResizeStreamController =
+      StreamController<ui.Size>.broadcast();
+
+  // Broadcasts the last seen `Size`.
+  void _broadcastSize(ui.Size size) {
+    _onResizeStreamController.add(size);
+  }
+
+  @override
+  void close() {
+    _hostElementResizeObserver?.disconnect();
+    // ignore:unawaited_futures
+    _onResizeStreamController.close();
+  }
+
+  @override
+  Stream<ui.Size> get onResize => _onResizeStreamController.stream;
+
+  @override
+  ui.Size computePhysicalSize() {
+    final double devicePixelRatio = getDevicePixelRatio();
+
+    return ui.Size(
+      _hostElement.clientWidth * devicePixelRatio,
+      _hostElement.clientHeight * devicePixelRatio,
+    );
+  }
+
+  @override
+  WindowPadding computeKeyboardInsets(
+    double physicalHeight,
+    bool isEditingOnMobile,
+  ) {
+    return const WindowPadding(
+      top: 0,
+      right: 0,
+      bottom: 0,
+      left: 0,
+    );
+  }
+}
