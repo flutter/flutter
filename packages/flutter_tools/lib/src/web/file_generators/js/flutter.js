4a94af02ce10cb02a3bc7c25d29b54e8a655d3e3
Mouad Debbar <mdebbar@google.com>
2022-12-27 14:29:25 -0500

    Fix paths fetched by flutter.js

--- packages/flutter_tools/lib/src/web/file_generators/js/flutter.js
+++ packages/flutter_tools/lib/src/web/file_generators/js/flutter.js
@@ -9,6 +9,9 @@ _flutter.loader = null;
 
 (function () {
   "use strict";
+
+  const baseUri = document.baseURI || "/";
+
   /**
    * Wraps `promise` in a timeout of the given `duration` in ms.
    *
@@ -109,8 +112,7 @@ _flutter.loader = null;
       }
       const {
         serviceWorkerVersion,
-        serviceWorkerUrl = "flutter_service_worker.js?v=" +
-          serviceWorkerVersion,
+        serviceWorkerUrl = `${baseUri}flutter_service_worker.js?v=${serviceWorkerVersion}`,
         timeoutMillis = 4000,
       } = settings;
 
@@ -228,7 +230,7 @@ _flutter.loader = null;
      * Returns undefined when an `onEntrypointLoaded` callback is supplied in `options`.
      */
     async loadEntrypoint(options) {
-      const { entrypointUrl = "main.dart.js", onEntrypointLoaded } =
+      const { entrypointUrl = `${baseUri}main.dart.js`, onEntrypointLoaded } =
         options || {};
 
       return this._loadEntrypoint(entrypointUrl, onEntrypointLoaded);
