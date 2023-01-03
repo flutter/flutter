58acb6c5b8fcf0104705b0805d1bb43091115837
Sigurd Meldgaard <sigurdm@google.com>
2023-01-03 14:43:08 +0000

    Don't print message that command is being run

--- packages/flutter_tools/lib/src/dart/pub.dart
+++ packages/flutter_tools/lib/src/dart/pub.dart
@@ -381,8 +381,6 @@ class _DefaultPub implements Pub {
     String? flutterRootOverride,
   }) async {
     int exitCode;
-
-    _logger.printStatus('Running "flutter pub $command" in ${_fileSystem.path.basename(directory)}...');
     final List<String> pubCommand = _pubCommand(arguments);
     final Map<String, String> pubEnvironment = await _createPubEnvironment(context, flutterRootOverride);
     try {
