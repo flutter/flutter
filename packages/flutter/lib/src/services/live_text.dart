402a7bbadba16d4586ebe3d40ac2b25adcfa7846
luckysmg <2539699336@qq.com>
2023-01-04 15:04:21 +0800

    modify comments

--- packages/flutter/lib/src/services/live_text.dart
+++ packages/flutter/lib/src/services/live_text.dart
@@ -4,13 +4,19 @@
 
 import 'system_channels.dart';
 
-/// Utility methods for interacting with the system's live text.
+/// Utility methods for interacting with the system's Live Text.
+///
+/// For example, the Live Text input feature of iOS turns the keyboard into a camera view for
+/// directly inserting text obtained through OCR into the active field.
+/// See also:
+/// https://developer.apple.com/documentation/uikit/uiresponder/3778577-capturetextfromcamera?changes=_5
+/// https://support.apple.com/guide/iphone/use-live-text-iphcf0b71b0e/ios
 class LiveText {
   // This class is not meant to be instantiated or extended; this constructor
   // prevents instantiation and extension.
   LiveText._();
 
-  /// Get this device live text input availability.
+  /// Returns true if the Live Text input feature is available on the current device.
   static Future<bool> isLiveTextInputAvailable() async {
     final bool supportLiveTextInput =
         await SystemChannels.platform.invokeMethod('LiveText.isLiveTextInputAvailable') ?? false;
