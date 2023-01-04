b186adc8bd999b8ce43c6e596ff9802752e4f465
Vyacheslav Egorov <vegorov@google.com>
2023-01-04 09:33:27 +0100

    Fix imports

--- packages/flutter_tools/test/integration.shard/flutter_build_apk_verbose_test.dart
+++ packages/flutter_tools/test/integration.shard/flutter_build_apk_verbose_test.dart
@@ -2,7 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:file_testing/file_testing.dart';
 import 'package:flutter_tools/src/base/file_system.dart';
 import 'package:flutter_tools/src/base/io.dart';
 
