072a9ca3773cfc8245d3b35e8cd5f70b3654845e
yaakovschectman <109111084+yaakovschectman@users.noreply.github.com>
2023-01-05 09:13:02 -0500

    Add `TextProvider` and `TextEdit` patterns to `AXPlatformNodeWin` (#38646)
    * Add TextProvider and TextEdit patterns to AXPlatformNodeWin
    
    * Formatting

--- third_party/accessibility/ax/platform/ax_platform_node_win_unittest.cc
+++ third_party/accessibility/ax/platform/ax_platform_node_win_unittest.cc
@@ -3509,11 +3509,13 @@ TEST_F(AXPlatformNodeWinTest, GetPatternProviderSupportedPatterns) {
 
   Init(update);
 
-  EXPECT_EQ(PatternSet({UIA_ScrollItemPatternId}),
+  EXPECT_EQ(PatternSet({UIA_ScrollItemPatternId, UIA_TextPatternId,
+                        UIA_TextEditPatternId}),
             GetSupportedPatternsFromNodeId(root_id));
 
   EXPECT_EQ(PatternSet({UIA_ScrollItemPatternId, UIA_ValuePatternId,
-                        UIA_ExpandCollapsePatternId}),
+                        UIA_ExpandCollapsePatternId, UIA_TextPatternId,
+                        UIA_TextEditPatternId}),
             GetSupportedPatternsFromNodeId(text_field_with_combo_box_id));
 
   EXPECT_EQ(PatternSet({UIA_ScrollItemPatternId, UIA_ValuePatternId,
