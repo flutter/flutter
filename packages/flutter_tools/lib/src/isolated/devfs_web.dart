2c05a35507cf345ee00b73d34b0349885df796ae
Nicholas Shahan <nshahan@google.com>
2023-01-09 10:01:34 -0800

    cleanup the late modifier

--- packages/flutter_tools/lib/src/isolated/devfs_web.dart
+++ packages/flutter_tools/lib/src/isolated/devfs_web.dart
@@ -926,10 +926,10 @@ class WebDevFS implements DevFS {
   }
 
   @visibleForTesting
-  late final File requireJS = (() {
-    // TODO(118119) Remove late and the initilizing function once the file
-    // location change in the Dart SDK has landed and rolled to the engine and
-    // flutter repos. There is no long-term need for the fallback logic.
+  final File requireJS = (() {
+    // TODO(118119) Remove the initilizing function once the file location 
+    // change in the Dart SDK has landed and rolled to the engine and flutter 
+    // repos. There is no long-term need for the fallback logic.
     final File oldFile = globals.fs.file(globals.fs.path.join(
       globals.artifacts!.getArtifactPath(Artifact.engineDartSdkPath, platform: TargetPlatform.web_javascript),
       'lib',
