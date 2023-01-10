61276cb2df66d90a6280cfa66f9f6a55d75a1cf1
Kate Lovett <katelovett@google.com>
2023-01-09 18:18:12 -0600

    Undo axes flip on Mac when shift+scroll-wheel (flutter/engine#38338)
    * Undo axes flipping when using scroll wheel with shift key
    
    * Formatting
    
    * ++
    
    * Fix test
    
    * Fix for test
    
    * Update shell/platform/darwin/macos/framework/Source/FlutterViewControllerTest.mm
    
    * ++
    
    * Fix format
    
    * Review feedback

--- engine/src/flutter/shell/platform/darwin/macos/framework/Source/FlutterViewController.mm
+++ engine/src/flutter/shell/platform/darwin/macos/framework/Source/FlutterViewController.mm
@@ -643,8 +643,23 @@ static void CommonInit(FlutterViewController* controller) {
       pixelsPerLine = 40.0;
     }
     double scaleFactor = self.flutterView.layer.contentsScale;
-    flutterEvent.scroll_delta_x = -event.scrollingDeltaX * pixelsPerLine * scaleFactor;
-    flutterEvent.scroll_delta_y = -event.scrollingDeltaY * pixelsPerLine * scaleFactor;
+    // When mouse input is received while shift is pressed (regardless of
+    // any other pressed keys), Mac automatically flips the axis. Other
+    // platforms do not do this, so we flip it back to normalize the input
+    // received by the framework. The keyboard+mouse-scroll mechanism is exposed
+    // in the ScrollBehavior of the framework so developers can customize the
+    // behavior.
+    // At time of change, Apple does not expose any other type of API or signal
+    // that the X/Y axes have been flipped.
+    double scaledDeltaX = -event.scrollingDeltaX * pixelsPerLine * scaleFactor;
+    double scaledDeltaY = -event.scrollingDeltaY * pixelsPerLine * scaleFactor;
+    if (event.modifierFlags & NSShiftKeyMask) {
+      flutterEvent.scroll_delta_x = scaledDeltaY;
+      flutterEvent.scroll_delta_y = scaledDeltaX;
+    } else {
+      flutterEvent.scroll_delta_x = scaledDeltaX;
+      flutterEvent.scroll_delta_y = scaledDeltaY;
+    }
   }
   [_engine sendPointerEvent:flutterEvent];
 
