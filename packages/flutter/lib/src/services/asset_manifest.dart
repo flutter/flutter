668315d8163d0748b93a3e728d7ccf9cfc8ff4a7
Andrew Kolos <andrewrkolos@gmail.com>
2023-01-12 15:15:15 -0800

    lints

--- packages/flutter/lib/src/services/asset_manifest.dart
+++ packages/flutter/lib/src/services/asset_manifest.dart
@@ -1,4 +1,7 @@
-import 'package:flutter/services.dart';
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+import 'asset_bundle.dart';
 
 const String _kAssetManifestFilename = 'AssetManifest.bin';
 
