2bff9b8d85561ae1ead26f9cb68b31fa571cc885
gaaclarke <30870216+gaaclarke@users.noreply.github.com>
2023-01-12 13:05:56 -0800

    fixed glfw example for arm64 (flutter/engine#38426)
    * fixed glfw example for arm64
    
    * jenn feedback

--- engine/src/flutter/examples/glfw/run.sh
+++ engine/src/flutter/examples/glfw/run.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -e # Exit if any program returns an error.
 
+if uname -m | grep "arm64"; then
+    variant="host_debug_unopt_arm64"
+else
+    variant="host_debug_unopt"
+fi
+
 #################################################################
 # Make the host C++ project.
 #################################################################
@@ -8,7 +14,7 @@ if [ ! -d debug ]; then
     mkdir debug
 fi
 cd debug
-cmake -DCMAKE_BUILD_TYPE=Debug ..
+cmake -DCMAKE_BUILD_TYPE=Debug -DFLUTTER_ENGINE_VARIANT=$variant ..
 make
 
 #################################################################
@@ -17,11 +23,12 @@ make
 if [ ! -d myapp ]; then
     flutter create myapp
 fi
+
 cd myapp
 cp ../../main.dart lib/main.dart
 flutter build bundle \
         --local-engine-src-path ../../../../../ \
-        --local-engine=host_debug_unopt
+        --local-engine=$variant
 cd -
 
 #################################################################
