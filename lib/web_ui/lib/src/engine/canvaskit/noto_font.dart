928772eba867e55841e706ae9f7cd347de896ed0
Mouad Debbar <mdebbar@google.com>
2023-01-17 19:59:14 +0000

    [web] Reduce code size impact of fallback font data (#38787)
    * [web] Reduce code size impact of fallback font data
    
    * fix rangeEnd vs rangeLength mistake

--- lib/web_ui/lib/src/engine/canvaskit/noto_font.dart
+++ lib/web_ui/lib/src/engine/canvaskit/noto_font.dart
@@ -2,48 +2,33 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-class NotoFont {
-  const NotoFont(this.name, this.url, this._rangeStarts, this._rangeEnds);
+import '../text/unicode_range.dart';
 
-  factory NotoFont.fromFlatRanges(String name, String url, List<int> flatRanges) {
-    final List<int> starts = <int>[];
-    final List<int> ends = <int>[];
-    for (int i = 0; i < flatRanges.length; i += 2) {
-      starts.add(flatRanges[i]);
-      ends.add(flatRanges[i+1]);
-    }
-    return NotoFont(name, url, starts, ends);
-  }
+class NotoFont {
+  NotoFont(this.name, this.url, this._packedRanges);
 
   final String name;
   final String url;
-  // A sorted list of Unicode range start points.
-  final List<int> _rangeStarts;
+  final String _packedRanges;
+  // A sorted list of Unicode ranges.
+  late final List<CodeunitRange> _ranges = _unpackFontRange(_packedRanges);
 
-  // A sorted list of Unicode range end points.
-  final List<int> _rangeEnds;
-
-  List<CodeunitRange> computeUnicodeRanges() {
-    final List<CodeunitRange> result = <CodeunitRange>[];
-    for (int i = 0; i < _rangeStarts.length; i++) {
-      result.add(CodeunitRange(_rangeStarts[i], _rangeEnds[i]));
-    }
-    return result;
-  }
+  List<CodeunitRange> computeUnicodeRanges() => _ranges;
 
   // Returns `true` if this font has a glyph for the given [codeunit].
   bool contains(int codeUnit) {
-    // Binary search through the starts and ends to see if there
+    // Binary search through the unicode ranges to see if there
     // is a range that contains the codeunit.
     int min = 0;
-    int max = _rangeStarts.length - 1;
+    int max = _ranges.length - 1;
     while (min <= max) {
       final int mid = (min + max) ~/ 2;
-      if (_rangeStarts[mid] > codeUnit) {
+      final CodeunitRange range = _ranges[mid];
+      if (range.start > codeUnit) {
         max = mid - 1;
       } else {
-        // _rangeStarts[mid] <= codeUnit
-        if (_rangeEnds[mid] >= codeUnit) {
+        // range.start <= codeUnit
+        if (range.end >= codeUnit) {
           return true;
         }
         min = mid + 1;
@@ -58,7 +43,6 @@ class CodeunitRange {
 
   final int start;
   final int end;
-
 
   bool contains(int codeUnit) {
     return start <= codeUnit && codeUnit <= end;
@@ -78,4 +62,47 @@ class CodeunitRange {
 
   @override
   String toString() => '[$start, $end]';
+}
+
+final int _kCharPipe = '|'.codeUnitAt(0);
+final int _kCharSemicolon = ';'.codeUnitAt(0);
+
+class MutableInt {
+  MutableInt(this.value);
+
+  int value;
+}
+
+List<CodeunitRange> _unpackFontRange(String packedRange) {
+    final MutableInt i = MutableInt(0);
+    final List<CodeunitRange> ranges = <CodeunitRange>[];
+
+    while (i.value < packedRange.length) {
+      final int rangeStart = _consumeInt36(packedRange, i, until: _kCharPipe);
+      final int rangeLength = _consumeInt36(packedRange, i, until: _kCharSemicolon);
+      final int rangeEnd = rangeStart + rangeLength;
+      ranges.add(CodeunitRange(rangeStart, rangeEnd));
+    }
+    return ranges;
+}
+
+int _consumeInt36(String packedData, MutableInt index, {required int until}) {
+  // The implementation is similar to:
+  //
+  // ```dart
+  // return int.tryParse(packedData.substring(index, indexOfUntil), radix: 36);
+  // ```
+  //
+  // But using substring is slow when called too many times. This custom
+  // implementation parses the integer without extra memory.
+
+  int result = 0;
+  while (true) {
+    final int charCode = packedData.codeUnitAt(index.value);
+    index.value++;
+    if (charCode == until) {
+      return result;
+    }
+    result = result * 36 + getIntFromCharCode(charCode);
+  }
 }
