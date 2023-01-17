efc54a0fa5336342b25f75330f57c5e119049bf9
schectman <schectman@google.com>
2023-01-17 16:59:50 -0500

    Organize test script

--- dev/devicelab/bin/tasks/windows_app_template_test.dart
+++ dev/devicelab/bin/tasks/windows_app_template_test.dart
@@ -6,20 +6,29 @@ import 'dart:io';
 import 'package:flutter_devicelab/framework/framework.dart';
 import 'package:flutter_devicelab/framework/task_result.dart';
 import 'package:flutter_devicelab/framework/utils.dart';
+import 'package:path/path.dart';
 
+const int _kLicenseLines = 4;
+const List<String> _kFilesToMatch = <String>[
+  'main.cpp.tmpl',
+  'utils.cpp',
+  'utils.h',
+  'win32_window.cpp',
+  'win32_window.h',
+];
 const String _kIntegrationTestsRelativePath = '/dev/integration_tests';
 const String _kTemplateRelativePath = '/packages/flutter_tools/templates/app_shared/windows.tmpl/runner';
 const String _kWindowsRunnerSubPath = '/windows/runner';
-const List<String> _kFilesToMatch = ['utils.h', 'utils.cpp'];
-const int _kLicenseLines = 4;
+const String _kProjectNameKey = '{{projectName}}';
 
 Future<void> main() async {
   await task(() async {
     final String integrationTestsPath = '${flutterDirectory.path}$_kIntegrationTestsRelativePath';
     final String templatePath = '${flutterDirectory.path}$_kTemplateRelativePath';
     final Iterable<Directory>subDirs = (await Directory(integrationTestsPath).list().toList()).whereType<Directory>();
-    bool match = true;
+    bool fileContentsMatch = true;
     for (final Directory testPath in subDirs) {
+      final String projectName = basename(testPath.path);
       final String runnerPath = '${testPath.path}$_kWindowsRunnerSubPath';
       final Directory runner = Directory(runnerPath);
       if (!runner.existsSync()) {
@@ -27,30 +36,38 @@ Future<void> main() async {
       }
       for (final String fileName in _kFilesToMatch) {
         final String templateFilePath = '$templatePath/$fileName';
-        final String templateFile = (await File(templateFilePath).readAsLines()).join('\n'); // Normalize line endings
-        final String appFilePath = '$runnerPath/$fileName';
-        final String appFile = (await File(appFilePath).readAsLines()).sublist(_kLicenseLines).join('\n'); // Skip license
+        String templateFile = await _fileContents(templateFilePath);
+        String appFilePath = '$runnerPath/$fileName';
+        if (fileName.endsWith('.tmpl')) {
+          appFilePath = appFilePath.substring(0, appFilePath.length - 4); // Remove '.tmpl' from app file path
+          templateFile = templateFile.replaceAll(_kProjectNameKey, projectName); // Substitute template project name
+        }
+        final String appFile = await _fileContents(appFilePath, linesToSkip: _kLicenseLines);
         if (appFile != templateFile) {
+          fileContentsMatch = false;
           print('File $fileName mismatched for integration test $testPath');
-          print('=====$templateFilePath======');
-          print(appFile);
           print('=====$appFilePath======');
+          print(appFile);
+          print('=====$templateFilePath======');
           print(templateFile);
-          int i;
-          for (i = 0; i < appFile.length; i++) {
-            if (i >= templateFile.length || templateFile.codeUnitAt(i) != appFile.codeUnitAt(i)) {
+          int indexOfDifference;
+          for (indexOfDifference = 0; indexOfDifference < appFile.length; indexOfDifference++) {
+            if (indexOfDifference >= templateFile.length || templateFile.codeUnitAt(indexOfDifference) != appFile.codeUnitAt(indexOfDifference)) {
               break;
             }
           }
           print('==========');
-          print('Diff at character #$i');
-          match = false;
+          print('Diff at character #$indexOfDifference');
         }
       }
     }
-    if (!match) {
+    if (!fileContentsMatch) {
       return TaskResult.failure(null);
     }
     return TaskResult.success(null);
   });
+}
+
+Future<String> _fileContents(String path, {int linesToSkip: 0}) async {
+  return (await File(path).readAsLines()).sublist(linesToSkip).join('\n');
 }
