aa613065695b548f5cfa946ab88f58407be690c5
Bruno Leroux <leroux_bruno@yahoo.fr>
2023-01-17 22:31:59 +0100

    Restore intentional spaces that illustrate bigger buttons

--- packages/flutter/test/widgets/focus_traversal_test.dart
+++ packages/flutter/test/widgets/focus_traversal_test.dart
@@ -1474,7 +1474,7 @@ void main() {
       /// Layout is:
       ///           [0, 1]
       ///    [0, 0] [    ] [0, 2]
-      ///    [1, 0] [1, 1] [1, 2]
+      ///    [  1,  0 ] [1, 1] [1, 2]
       ///    [2, 0] [2, 1] [2, 2]
       await tester.pumpWidget(
         Directionality(
