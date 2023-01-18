5c9ef849e01d865c7bb847df546107a24e73df9f
a-wallen <stephenwallen@google.com>
2023-01-18 13:58:13 -0800

    Add install test on macOS

--- dev/devicelab/bin/tasks/flavors_test_macos.dart
+++ dev/devicelab/bin/tasks/flavors_test_macos.dart
@@ -5,6 +5,7 @@
 import 'package:flutter_devicelab/framework/devices.dart';
 import 'package:flutter_devicelab/framework/framework.dart';
 import 'package:flutter_devicelab/framework/task_result.dart';
+import 'package:flutter_devicelab/framework/utils.dart';
 import 'package:flutter_devicelab/tasks/integration_tests.dart';
 
 Future<void> main() async {
@@ -12,6 +13,26 @@ Future<void> main() async {
   await task(() async {
     await createFlavorsTest().call();
     await createIntegrationTestFlavorsTest().call();
+
+    await inDirectory('${flutterDirectory.path}/dev/integration_tests/flavors', () async {
+      final StringBuffer stderr = StringBuffer();
+
+      await evalFlutter(
+        'install',
+        canFail: true,
+        stderr: stderr,
+        options: <String>[
+          '--d', 'macos',
+          '--flavor', 'free'
+        ],
+      );
+
+      final String stderrString = stderr.toString();
+      if (!stderrString.contains('Host and target are the same. Nothing to install.')) {
+        print(stderrString);
+        return TaskResult.failure('Installing a macOS app on macOS should no-op');
+      }
+    });
 
     return TaskResult.success(null);
   });
