57d9f0d9d5f5cd10836f8dbf8455fbc8c94c568d
schectman <schectman@google.com>
2023-01-18 13:13:45 -0500

    Utilize path.join

--- dev/devicelab/bin/tasks/windows_app_template_test.dart
+++ dev/devicelab/bin/tasks/windows_app_template_test.dart
@@ -16,28 +16,28 @@ const List<String> _kFilesToMatch = <String>[
   'win32_window.cpp',
   'win32_window.h',
 ];
-const String _kIntegrationTestsRelativePath = '/dev/integration_tests';
-const String _kTemplateRelativePath = '/packages/flutter_tools/templates/app_shared/windows.tmpl/runner';
-const String _kWindowsRunnerSubPath = '/windows/runner';
+const String _kIntegrationTestsRelativePath = 'dev/integration_tests';
+const String _kTemplateRelativePath = 'packages/flutter_tools/templates/app_shared/windows.tmpl/runner';
+const String _kWindowsRunnerSubPath = 'windows/runner';
 const String _kProjectNameKey = '{{projectName}}';
 
 Future<void> main() async {
   await task(() async {
-    final String integrationTestsPath = '${flutterDirectory.path}$_kIntegrationTestsRelativePath';
-    final String templatePath = '${flutterDirectory.path}$_kTemplateRelativePath';
+    final String integrationTestsPath = join(flutterDirectory.path, _kIntegrationTestsRelativePath);
+    final String templatePath = join(flutterDirectory.path, _kTemplateRelativePath);
     final Iterable<Directory>subDirs = (await Directory(integrationTestsPath).list().toList()).whereType<Directory>();
     bool fileContentsMatch = true;
     for (final Directory testPath in subDirs) {
       final String projectName = basename(testPath.path);
-      final String runnerPath = '${testPath.path}$_kWindowsRunnerSubPath';
+      final String runnerPath = join(testPath.path, _kWindowsRunnerSubPath);
       final Directory runner = Directory(runnerPath);
       if (!runner.existsSync()) {
         continue;
       }
       for (final String fileName in _kFilesToMatch) {
-        final String templateFilePath = '$templatePath/$fileName';
+        final String templateFilePath = join(templatePath, fileName);
         String templateFile = await _fileContents(templateFilePath);
-        String appFilePath = '$runnerPath/$fileName';
+        String appFilePath = join(runnerPath, fileName);
         if (fileName.endsWith('.tmpl')) {
           appFilePath = appFilePath.substring(0, appFilePath.length - 4); // Remove '.tmpl' from app file path
           templateFile = templateFile.replaceAll(_kProjectNameKey, projectName); // Substitute template project name
