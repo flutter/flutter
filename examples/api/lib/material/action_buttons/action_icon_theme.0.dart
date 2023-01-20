ae1016785674767ab88c0c73177e524823ad6fe5
predatorx7 <smushaheed@outlook.com>
2023-01-20 16:47:52 +0530

    Add missing license header in action_icon_theme.0.dart

--- examples/api/lib/material/action_buttons/action_icon_theme.0.dart
+++ examples/api/lib/material/action_buttons/action_icon_theme.0.dart
@@ -1,3 +1,9 @@
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Flutter code sample for [ActionIconTheme].
+
 import 'package:flutter/material.dart';
 
 void main() {
