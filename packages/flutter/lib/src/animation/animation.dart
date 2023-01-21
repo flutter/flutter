8a071f999c6f70fa9a47b08d5166eb190e4cbc80
Michael Goderbauer <goderbauer@google.com>
2023-01-20 18:20:24 -0800

    Remove unnecessary null checks in `flutter/{animation,semantics,scheduler}` (#118922)
    * Remove unnecessary null checks in flutter/animation
    
    * Remove unnecessary null checks in flutter/semantics
    
    * Remove unnecessary null checks in flutter/scheduler

--- packages/flutter/lib/src/animation/animation.dart
+++ packages/flutter/lib/src/animation/animation.dart
@@ -269,7 +269,6 @@ abstract class Animation<T> extends Listenable implements ValueListenable<T> {
   /// * "&#x23ED;": [AnimationStatus.completed] ([value] == 1.0)
   /// * "&#x23EE;": [AnimationStatus.dismissed] ([value] == 0.0)
   String toStringDetails() {
-    assert(status != null);
     switch (status) {
       case AnimationStatus.forward:
         return '\u25B6'; // >
