99966cbc2761c93a5c8bec69f784e941669787b5
Ahmed Ashour <asashour@yahoo.com>
2023-01-24 11:12:29 +0100

    Remove superfluous words from comments.

--- packages/flutter_tools/templates/app_shared/windows.tmpl/runner/win32_window.h
+++ packages/flutter_tools/templates/app_shared/windows.tmpl/runner/win32_window.h
@@ -32,7 +32,7 @@ class Win32Window {
   // |origin| and |size|. New windows are created on the default monitor. Window
   // sizes are specified to the OS in physical pixels, hence to ensure a
   // consistent size this function will scale the inputted width and height as
-  // as appropriate for the default monitor. The window is invisible until
+  // appropriate for the default monitor. The window is invisible until
   // |Show| is called. Returns true if the window was created successfully.
   bool Create(const std::wstring& title, const Point& origin, const Size& size);
 
