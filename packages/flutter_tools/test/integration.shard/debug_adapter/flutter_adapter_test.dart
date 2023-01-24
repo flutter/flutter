36f0b73035f2ad6da206f96659d4a9df0f87b160
Danny Tuppeny <danny@tuppeny.com>
2023-01-24 17:30:31 +0000

    [flutter_tools] Use base DAP detach and ensure correct output
    This is a minor tidy up to call a more specific method when detaching, which ensures the final output reads "Detached" instead of "Exited".

--- packages/flutter_tools/test/integration.shard/debug_adapter/flutter_adapter_test.dart
+++ packages/flutter_tools/test/integration.shard/debug_adapter/flutter_adapter_test.dart
@@ -561,11 +561,18 @@ void main() {
         dap.client.setBreakpoint(breakpointFilePath, breakpointLine),
       ], eagerError: true);
 
-      // Detach.
-      await dap.client.terminate();
+      // Detach and expected resume and correct output.
+      await Future.wait(<Future<void>>[
+        // We should print "Detached" instead of "Exited".
+        dap.client.outputEvents.firstWhere((OutputEventBody event) => event.output.startsWith('\nDetached.')),
+        // We should still get terminatedEvent (this signals the DAP server terminating).
+        dap.client.event('terminated'),
+        // We should get additional output from the test process confirming the process resumed.
+        testProcess.output.first,
+        // Trigger the detach.
+        dap.client.terminate(),
+      ]);
 
-      // Ensure we get additional output (confirming the process resumed).
-      await testProcess.output.first;
     });
   });
 }
