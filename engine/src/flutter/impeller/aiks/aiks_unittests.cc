02b021056318a5048ff94c000ebf6696c844b93f
Brandon DeRosier <bdero@google.com>
2023-01-25 12:01:50 -0800

    [Impeller] Make text glyph offsets respect the current transform (flutter/engine#39119)

--- engine/src/flutter/impeller/aiks/aiks_unittests.cc
+++ engine/src/flutter/impeller/aiks/aiks_unittests.cc
@@ -18,6 +18,7 @@
 #include "impeller/entity/contents/scene_contents.h"
 #include "impeller/entity/contents/tiled_texture_contents.h"
 #include "impeller/geometry/color.h"
+#include "impeller/geometry/constants.h"
 #include "impeller/geometry/geometry_unittests.h"
 #include "impeller/geometry/matrix.h"
 #include "impeller/geometry/path_builder.h"
@@ -1179,6 +1180,20 @@ TEST_P(AiksTest, CanRenderTextOutsideBoundaries) {
     }
     canvas.Restore();
   }
+
+  ASSERT_TRUE(OpenPlaygroundHere(canvas.EndRecordingAsPicture()));
+}
+
+TEST_P(AiksTest, TextRotated) {
+  Canvas canvas;
+  canvas.Transform(Matrix(0.5, -0.3, 0, -0.002,  //
+                          0, 1, 0, 0,            //
+                          0, 0, 0.3, 0,          //
+                          100, 100, 0, 1.3));
+
+  ASSERT_TRUE(RenderTextInCanvas(
+      GetContext(), canvas, "the quick brown fox jumped over the lazy dog!.?",
+      "Roboto-Regular.ttf"));
 
   ASSERT_TRUE(OpenPlaygroundHere(canvas.EndRecordingAsPicture()));
 }
