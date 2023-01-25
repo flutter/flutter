789695ddf34cfb1e69f5d59938365d4c3e06c73e
tahatesser <tessertaha@gmail.com>
2023-01-25 15:05:13 +0300

    Remove unnecessary comma

--- examples/api/lib/material/checkbox_list_tile/checkbox_list_tile.0.dart
+++ examples/api/lib/material/checkbox_list_tile/checkbox_list_tile.0.dart
@@ -31,7 +31,7 @@ class _CheckboxListTileExampleState extends State<CheckboxListTileExample> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('CheckboxListTile Sample'),),
+      appBar: AppBar(title: const Text('CheckboxListTile Sample')),
       body: Center(
         child: CheckboxListTile(
           title: const Text('Animate Slowly'),
