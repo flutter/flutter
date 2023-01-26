707a94cb7b05e36c1d72cbdcaedc1e4bdd578caa
godofredoc <godofredoc@google.com>
2023-01-25 21:25:02 -0800

    Move windows-x64-flutter.zip to windows-x64-debug location. (#119177)
    The dependency is currently uploaded to two different locations. This
    change is required to remove the duplicate uploads and their
    corresponding tech debt.

--- packages/flutter_tools/lib/src/flutter_cache.dart
+++ packages/flutter_tools/lib/src/flutter_cache.dart
@@ -845,7 +845,7 @@ class IosUsbArtifacts extends CachedArtifact {
 // remove from existing host folder.
 // https://github.com/flutter/flutter/issues/38935
 const List<List<String>> _windowsDesktopBinaryDirs = <List<String>>[
-  <String>['windows-x64', 'windows-x64/windows-x64-flutter.zip'],
+  <String>['windows-x64', 'windows-x64-debug/windows-x64-flutter.zip'],
   <String>['windows-x64', 'windows-x64/flutter-cpp-client-wrapper.zip'],
   <String>['windows-x64-profile', 'windows-x64-profile/windows-x64-flutter.zip'],
   <String>['windows-x64-release', 'windows-x64-release/windows-x64-flutter.zip'],
