1cb65559a344a82524feb44c12a52cc41d3f9bf7
Jonah Williams <jonahwilliams@google.com>
2023-01-27 10:50:56 -0800

    [impellerc] remove remaining remap sampler support (flutter/engine#39198)

--- engine/src/flutter/impeller/compiler/impellerc_main.cc
+++ engine/src/flutter/impeller/compiler/impellerc_main.cc
@@ -73,7 +73,6 @@ bool Main(const fml::CommandLine& command_line) {
       switches.source_file_name, options.type, options.source_language,
       switches.entry_point);
   options.json_format = switches.json_format;
-  options.remap_samplers = switches.remap_samplers;
   options.gles_language_version = switches.gles_language_version;
 
   Reflector::Options reflector_options;
