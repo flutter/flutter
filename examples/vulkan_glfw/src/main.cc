189a69d9918d4a27bf7958855326169ebb554f05
Constantine Shablya <nanokatze@gmail.com>
2023-01-27 17:55:06 +0200

    Fix Vulkan embedder example (#39052)

--- examples/vulkan_glfw/src/main.cc
+++ examples/vulkan_glfw/src/main.cc
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <chrono>
 #include <cstdlib>
 #include <iostream>
 #include <optional>
