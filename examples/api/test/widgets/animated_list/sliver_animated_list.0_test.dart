a39697811611221d98e1a320ed585d3765553617
jehhxuxu <jessica.mariane@ifood.com.br>
2023-01-31 14:28:38 -0300

    fix: sliver_animated_list.0_test.dart

--- examples/api/test/widgets/animated_list/sliver_animated_list.0_test.dart
+++ examples/api/test/widgets/animated_list/sliver_animated_list.0_test.dart
@@ -1,3 +1,7 @@
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import 'package:flutter/material.dart';
 import 'package:flutter_api_samples/widgets/animated_list/sliver_animated_list.0.dart'
     as example;
