35752cd4eb763908beb474482b710d286b3308e9
Kate Lovett <katelovett@google.com>
2023-02-01 09:35:10 -0600

    Apply suggestions from code review
    Co-authored-by: Michael Goderbauer <goderbauer@google.com>

--- packages/flutter/test/material/navigation_bar_test.dart
+++ packages/flutter/test/material/navigation_bar_test.dart
@@ -5,7 +5,6 @@
 // This file is run as part of a reduced test set in CI on Mac and Windows
 // machines.
 @Tags(<String>['reduced-test-set'])
-library;
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/gestures.dart';
