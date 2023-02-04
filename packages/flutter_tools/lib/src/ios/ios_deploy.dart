d2a1d5ac7653bc5150f8ba5007d86599e5c17b18
林洵锋 <linxunfeng@yeah.net>
2023-02-04 11:35:55 +0800

    Update packages/flutter_tools/lib/src/ios/ios_deploy.dart
    Co-authored-by: Jenn Magder <magder@google.com>

--- packages/flutter_tools/lib/src/ios/ios_deploy.dart
+++ packages/flutter_tools/lib/src/ios/ios_deploy.dart
@@ -344,9 +344,9 @@ class IOSDeployDebugger {
         _monitorIOSDeployFailure(line, _logger);
 
         // (lldb)    platform select remote-'ios' --sysroot
-        // Need to get the current lldb prompt and reassigns it to lldbRun to
-        // avoid lldbRun matching failure caused by the custom lldb prompt.
-        // Example: "(mylldb)    platform select remote-'ios' --sysroot"
+        // Use the configurable custom lldb prompt in the regex. The developer can set this prompt to anything. 
+        // For example `settings set prompt "(mylldb)"` in ~/.lldbinit results in:
+        // "(mylldb)    platform select remote-'ios' --sysroot"
         if (_lldbPlatformSelect.hasMatch(line)) {
           final String platformSelect = _lldbPlatformSelect.stringMatch(line) ?? '';
           if (platformSelect.isEmpty) {
