75b1b76f5ac5c47d722095096b4ab5d5e78c650d
Greg Price <greg@zulip.com>
2023-02-07 11:52:46 -0800

    Make ClampingScrollSimulation ballistic and more like Android
    Fixes #120338.
    Fixes #119875.
    Fixes #113424.
    
    Fixes most of #16371, as the combined symptom of those three.
    (There's still #120345, and possibly other contributing factors,
    remaining.)
    
    This replaces the implementation of ClampingScrollSimulation with
    a new version that better matches the Android scroll physics it's
    intended to match.
    
    The new implementation is a version of the Android curve which has
    been adjusted so that it behaves in a physically reasonable way,
    in order to satisfy an invariant which the Flutter scroll protocol
    relies on.
    
    Because the old ClampingScrollSimulation didn't satisfy that
    invariant either, it suffered from a bug (#120338) where
    restarting the simulation with new metrics would have the side
    effect of adding friction, making a fling go about 15% less far.
    So this fixes #120338.
    
    The new implementation also goes the same total distance as the
    Android scroll physics, whereas the existing version would go about
    7% less far (stacking with the 15% reduction when the other issue
    was triggered.)  That fixes #119875.
    
    Finally, the new implementation comes to a smooth stop, like Android
    does, whereas the existing version would speed slightly up before
    abruptly stopping.  That fixes #113424.
    
    The new implementation's curve is the unique curve that both goes the
    same total distance as Android's for every possible starting velocity,
    and meets the Flutter scroll protocol's ballistic invariant.  So in
    that sense this is the canonical way of fixing up the Android behavior
    to something physically reasonable, or something that works correctly
    in Flutter.

--- packages/flutter/test/widgets/scroll_simulation_test.dart
+++ packages/flutter/test/widgets/scroll_simulation_test.dart
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'dart:math' as math;
+
 import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -22,5 +24,99 @@ void main() {
     checkInitialConditions(4534.0, 1073.553798);
     checkInitialConditions(75.0, 614.2093);
     checkInitialConditions(5469.0, 182.114534);
+  });
+
+  test('ClampingScrollSimulation only decelerates, never speeds up', () {
+    // Regression test for https://github.com/flutter/flutter/issues/113424
+    final ClampingScrollSimulation simulation =
+    ClampingScrollSimulation(position: 0, velocity: 8000.0);
+    double time = 0.0;
+    double velocity = simulation.dx(time);
+    while (!simulation.isDone(time)) {
+      expect(time, lessThan(3.0));
+      time += 1 / 60;
+      final double nextVelocity = simulation.dx(time);
+      expect(nextVelocity, lessThanOrEqualTo(velocity));
+      velocity = nextVelocity;
+    }
+  });
+
+  test('ClampingScrollSimulation is ballistic', () {
+    // Regression test for https://github.com/flutter/flutter/issues/120338
+    final ClampingScrollSimulation undisturbed =
+        ClampingScrollSimulation(position: 0, velocity: 8000.0);
+    double time = 0.0;
+    ClampingScrollSimulation restarted = undisturbed;
+    while (!undisturbed.isDone(time) || !restarted.isDone(time)) {
+      expect(time, lessThan(3.0));
+
+      const double delta = 1 / 90;
+      time += delta;
+      restarted = ClampingScrollSimulation(
+          position: restarted.x(delta), velocity: restarted.dx(delta));
+      expect(restarted.x(0),  moreOrLessEquals(undisturbed.x(time)));
+      expect(restarted.dx(0), moreOrLessEquals(undisturbed.dx(time)));
+    }
+  });
+
+
+  test('ClampingScrollSimulation satisfies a physical acceleration formula', () {
+    // Different regression test for https://github.com/flutter/flutter/issues/120338
+    //
+    // This one provides a formula for the particle's acceleration as a function
+    // of its velocity, and checks that it behaves according to that formula.
+    // The point isn't that it's this specific formula, but just that there's
+    // some formula which depends only on velocity, not time, so that the
+    // physical metaphor makes sense.
+
+    // Copied from the implementation.
+    final double kDecelerationRate = math.log(0.78) / math.log(0.9);
+
+    // Same as the referenceVelocity in _flingDuration.
+    const double referenceVelocity = .015 * 9.80665 * 39.37 * 160.0 * 0.84 / 0.35;
+
+    // The value of _duration when velocity == referenceVelocity.
+    final double referenceDuration = kDecelerationRate * 0.35;
+
+    // The rate of deceleration when dx(time) == referenceVelocity.
+    final double referenceDeceleration = (kDecelerationRate - 1) * referenceVelocity / referenceDuration;
+
+    double acceleration(double velocity) {
+      return - velocity.sign
+                 * referenceDeceleration *
+                 math.pow(velocity.abs() / referenceVelocity,
+                          (kDecelerationRate - 2) / (kDecelerationRate - 1));
+    }
+
+    double jerk(double velocity) {
+      return referenceVelocity / referenceDuration / referenceDuration
+               * (kDecelerationRate - 1) * (kDecelerationRate - 2)
+               * math.pow(velocity.abs() / referenceVelocity,
+                          (kDecelerationRate - 3) / (kDecelerationRate - 1));
+    }
+
+    void checkAcceleration(double position, double velocity) {
+      final ClampingScrollSimulation simulation =
+          ClampingScrollSimulation(position: position, velocity: velocity);
+      double time = 0.0;
+      const double delta = 1/60;
+      for (; time < 2.0; time += delta) {
+        final double difference = simulation.dx(time + delta) - simulation.dx(time);
+        final double predictedDifference = delta * acceleration(simulation.dx(time + delta/2));
+        final double maxThirdDerivative = jerk(simulation.dx(time + delta));
+        expect((difference - predictedDifference).abs(),
+            lessThan(maxThirdDerivative * math.pow(delta, 2)/2));
+      }
+    }
+
+    checkAcceleration(51.0, 2866.91537);
+    checkAcceleration(584.0, 2617.294734);
+    checkAcceleration(345.0, 1982.785934);
+    checkAcceleration(0.0, 1831.366634);
+    checkAcceleration(-156.2, 1541.57665);
+    checkAcceleration(4.0, 1139.250439);
+    checkAcceleration(4534.0, 1073.553798);
+    checkAcceleration(75.0, 614.2093);
+    checkAcceleration(5469.0, 182.114534);
   });
 }
