1c225675c5d61e7ceba9c7c9762fdb59e2c41f2d
Qun Cheng <36861262+QuncCccccc@users.noreply.github.com>
2023-02-07 11:16:24 -0800

    Update to v0.158 of the token database. (#120149)
    * Update to v0.158 of the token database.
    
    * Update checkbox template
    
    * Fix DatePickerTheme test
    
    ---------
    
    Co-authored-by: Qun Cheng <quncheng@google.com>

--- packages/flutter/lib/src/material/tabs.dart
+++ packages/flutter/lib/src/material/tabs.dart
@@ -1902,7 +1902,7 @@ class _TabsDefaultsM2 extends TabBarTheme {
 // Design token database by the script:
 //   dev/tools/gen_defaults/bin/gen_defaults.dart.
 
-// Token database version: v0_152
+// Token database version: v0_158
 
 class _TabsDefaultsM3 extends TabBarTheme {
   _TabsDefaultsM3(this.context)
