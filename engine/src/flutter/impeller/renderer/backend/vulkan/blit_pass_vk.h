c21bff3f8fc4293e91ccd5225b2dc38b7f1d5a1f
Kaushik Iska <iska.kaushik@gmail.com>
2023-02-09 16:24:42 -0500

    [Impeller] Support blit passes on Vulkan. (flutter/engine#39438)
    fixes: https://github.com/flutter/flutter/issues/112649

--- engine/src/flutter/impeller/renderer/backend/vulkan/blit_pass_vk.h
+++ engine/src/flutter/impeller/renderer/backend/vulkan/blit_pass_vk.h
@@ -5,19 +5,25 @@
 #pragma once
 
 #include "flutter/fml/macros.h"
+#include "impeller/renderer/backend/vulkan/blit_command_vk.h"
+#include "impeller/renderer/backend/vulkan/fenced_command_buffer_vk.h"
 #include "impeller/renderer/blit_pass.h"
 
 namespace impeller {
 
 class BlitPassVK final : public BlitPass {
  public:
+  explicit BlitPassVK(std::shared_ptr<FencedCommandBufferVK> command_buffer);
+
   // |BlitPass|
   ~BlitPassVK() override;
 
  private:
   friend class CommandBufferVK;
 
-  BlitPassVK();
+  std::shared_ptr<FencedCommandBufferVK> command_buffer_;
+  std::vector<std::unique_ptr<BlitEncodeVK>> commands_;
+  std::string label_;
 
   // |BlitPass|
   bool IsValid() const override;
