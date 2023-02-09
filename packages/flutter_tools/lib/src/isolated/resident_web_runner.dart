74eae911f4f75be238b128b39e586624283153d0
Michael Thomsen <mit@google.com>
2023-02-09 19:40:32 +0100

    Clean up null safety messages (#120350)

--- packages/flutter_tools/lib/src/isolated/resident_web_runner.dart
+++ packages/flutter_tools/lib/src/isolated/resident_web_runner.dart
@@ -608,10 +608,8 @@ class ResidentWebRunner extends ResidentRunner {
           ..writeAsStringSync(websocketUri.toString());
       }
       _logger.printStatus('Debug service listening on $websocketUri');
-      _logger.printStatus('');
-      if (debuggingOptions.buildInfo.nullSafetyMode ==  NullSafetyMode.sound) {
-        _logger.printStatus('💪 Running with sound null safety 💪', emphasis: true);
-      } else {
+      if (debuggingOptions.buildInfo.nullSafetyMode !=  NullSafetyMode.sound) {
+        _logger.printStatus('');
         _logger.printStatus(
           'Running without sound null safety ⚠️',
           emphasis: true,
