f09b73e01a8de682bc5badaff8cdb92b9fd2ebac
Ricardo Amador <32242716+ricardoamador@users.noreply.github.com>
2023-02-09 14:20:18 -0800

    Cherry-pick changes to support the use_emulator flag that made it into candidate branch (#120305)
    * Android defines target update (#119766)
    
    * Round 2 with dimensions
    
    * Try with stringified keys
    
    * Forgot a comma
    
    * trying a list of maps.
    
    * Try key value pairs
    
    * Try with kvm only
    
    * try with swarming_dimensions
    
    * Updated with fix in swarming.star
    
    * Attempting to remove the bring up property to verify test is running.
    
    * Remove android from target tags to avoid adb command automatically running in test_utils call.
    
    * Remove the duplicated dependencies in the defines target that exist in the platform
    
    * Devicelab android emulator (#113472)
    
    * Testing whether emulator is possible.
    
    * Adding changes to see if emulator can be used from recipe.
    
    * adding emulator support.
    
    * Add the emulator flag for testing.
    
    * Using string for boolean since it cannot be parsed in properties
    
    * Checking to see if these changes are being used.
    
    * Updated bool back to string
    
    * Remove trailing whitespace from file.
    
    * Retrigger run with new recipe code
    
    * Bringup true?
    
    * Remove bringup so the test runs.

--- dev/devicelab/bin/run.dart
+++ dev/devicelab/bin/run.dart
@@ -62,6 +62,9 @@ Future<void> main(List<String> rawArgs) async {
   /// Path to write test results to.
   final String? resultsPath = args['results-file'] as String?;
 
+  /// Use an emulator for this test if it is an android test.
+  final bool useEmulator = (args['use-emulator'] as bool?) ?? false;
+
   if (args.wasParsed('list')) {
     for (int i = 0; i < taskNames.length; i++) {
       print('${(i + 1).toString().padLeft(3)} - ${taskNames[i]}');
@@ -107,6 +110,7 @@ Future<void> main(List<String> rawArgs) async {
       gitBranch: gitBranch,
       luciBuilder: luciBuilder,
       resultsPath: resultsPath,
+      useEmulator: useEmulator,
     );
   }
 }
@@ -318,6 +322,11 @@ ArgParser createArgParser(List<String> taskNames) {
       help: 'Whether to send a SIGKILL signal to any Dart processes that are still '
             'running when a task is completed. If any Dart processes are terminated '
             'in this way, the test is considered to have failed.',
+    )
+    ..addFlag(
+      'use-emulator',
+      help: 'If this is an android test, use an emulator to run the test instead of '
+            'a physical device.'
     )
     ..addMultiOption(
       'test',
