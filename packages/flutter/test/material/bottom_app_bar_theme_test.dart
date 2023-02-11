0db47bdd07afedaf62de17b88a815833a593e21f
Casey Hillers <chillers@google.com>
2023-02-11 03:44:56 -0800

    Revert "Fix BottomAppBar & BottomSheet M3 shadow (#119819)" (#120492)
    This reverts commit 0a97ef85c8706330c94abb57841680d19292fcc7.

--- packages/flutter/test/material/bottom_app_bar_theme_test.dart
+++ packages/flutter/test/material/bottom_app_bar_theme_test.dart
@@ -173,10 +173,10 @@ void main() {
         home: const Scaffold(body: BottomAppBar()),
       ));
 
-      final Material material = tester.widget(find.byType(Material).at(1));
+      final PhysicalShape widget = _getBabRenderObject(tester);
 
-      expect(material.color, theme.colorScheme.surface);
-      expect(material.elevation, equals(3.0));
+      expect(widget.color, theme.colorScheme.surface);
+      expect(widget.elevation, equals(3.0));
     });
 
     testWidgets('BAB theme overrides surfaceTintColor - M3', (WidgetTester tester) async {
