39f9c3374b08802c3d862039d33e558dfa85dcc1
William Hesse <whesse@google.com>
2023-02-13 17:57:16 +0100

    Change run_tests.sh to use python3 (flutter/engine#39586)

--- engine/src/flutter/testing/run_tests.sh
+++ engine/src/flutter/testing/run_tests.sh
@@ -5,4 +5,4 @@ set -o pipefail -e;
 BUILD_VARIANT="${1:-host_debug_unopt}"
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-python "${CURRENT_DIR}/run_tests.py" --variant="${BUILD_VARIANT}" --type=engine,dart,benchmarks
+python3 "${CURRENT_DIR}/run_tests.py" --variant="${BUILD_VARIANT}" --type=engine,dart,benchmarks
