0a6bb06020e383c03aa372def65e8d74393928ee
Christopher Fujino <christopherfujino@gmail.com>
2023-02-13 18:53:35 -0800

    fix tests

--- packages/flutter_tools/test/integration.shard/test_driver.dart
+++ packages/flutter_tools/test/integration.shard/test_driver.dart
@@ -194,10 +194,13 @@ abstract class FlutterTestDriver {
     // it forcefully and it won't terminate child processes, so we need to ensure
     // it's running before terminating.
     await resume().timeout(defaultTimeout)
-        .catchError((Object e) {
-          _debugPrint('Ignoring failure to resume during shutdown');
-          return null;
-        });
+        .then(
+          (Isolate? isolate) => isolate,
+          onError: (Object e) {
+            _debugPrint('Ignoring failure to resume during shutdown');
+            return null;
+          },
+        );
 
     _debugPrint('Sending SIGTERM to $_processPid..');
     io.Process.killPid(_processPid!);
@@ -472,13 +475,16 @@ abstract class FlutterTestDriver {
     });
     final Future<T> future = callback().whenComplete(longWarning.cancel);
 
-    return future.catchError((Object error) {
-      if (!timeoutExpired) {
-        timeoutExpired = true;
-        _debugPrint(messages.toString());
-      }
-      throw error; // ignore: only_throw_errors
-    }).whenComplete(() => subscription.cancel());
+    return future.then(
+      (T t) => t,
+      onError: (Object error) {
+        if (!timeoutExpired) {
+          timeoutExpired = true;
+          _debugPrint(messages.toString());
+        }
+        throw error; // ignore: only_throw_errors
+      },
+    ).whenComplete(() => subscription.cancel());
   }
 }
 
