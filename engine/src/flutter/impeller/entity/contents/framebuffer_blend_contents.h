0ecfed28d370a26799f8cf6d6d50e8e578bd1910
Jonah Williams <jonahwilliams@google.com>
2023-02-14 13:27:07 -0800

    [Impeller] read from framebuffer for advanced blends on iOS. (flutter/engine#39567)
    * [impeller] read from framebuffer for advanced blends
    
    * ++
    
    * update to contents
    
    * move to separate contents
    
    * ++
    
    * set stencil depth duh
    
    * ++
    
    * ++
    
    * remove debug info
    
    * update licenses
    
    * Add FML_OS_PHYSICAL_IOS
    
    * ++
    
    * ++

---
+++ engine/src/flutter/impeller/entity/contents/framebuffer_blend_contents.h
@@ -0,0 +1,43 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#pragma once
+
+#include <functional>
+#include <memory>
+#include <vector>
+
+#include "flutter/fml/macros.h"
+#include "flutter/impeller/renderer/texture.h"
+#include "impeller/entity/contents/color_source_contents.h"
+#include "impeller/entity/entity.h"
+
+namespace impeller {
+
+class FramebufferBlendContents final : public ColorSourceContents {
+ public:
+  FramebufferBlendContents();
+
+  ~FramebufferBlendContents() override;
+
+  void SetBlendMode(BlendMode blend_mode);
+
+  void SetChildContents(std::shared_ptr<Contents> child_contents);
+
+ private:
+  // |Contents|
+  std::optional<Rect> GetCoverage(const Entity& entity) const override;
+
+  // |Contents|
+  bool Render(const ContentContext& renderer,
+              const Entity& entity,
+              RenderPass& pass) const override;
+
+  BlendMode blend_mode_;
+  std::shared_ptr<Contents> child_contents_;
+
+  FML_DISALLOW_COPY_AND_ASSIGN(FramebufferBlendContents);
+};
+
+}  // namespace impeller
