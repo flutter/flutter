62a86040bf188ee7be41d91625e8a94475a10945
Michael Goderbauer <goderbauer@google.com>
2023-02-15 12:32:06 -0800

    Update embedding

--- dev/integration_tests/channels/android/app/src/main/java/com/example/channels/MainActivity.java
+++ dev/integration_tests/channels/android/app/src/main/java/com/example/channels/MainActivity.java
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package com.yourcompany.channels;
+package com.example.channels;
 
 import java.io.ByteArrayOutputStream;
 import java.nio.ByteBuffer;
