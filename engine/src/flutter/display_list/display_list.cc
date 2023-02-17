93a392b0e2fbeb0f24a19dc4ece5a4cbafe0136a
Jim Graham <flar@google.com>
2023-02-16 21:18:23 -0800

    Cached DisplayList opacity inheritance fix (flutter/engine#39690)
    * only indicate opacity inheritance when DL is actually cached
    
    * unit test
    
    * use CacheInfo struct to simplify return values

--- engine/src/flutter/display_list/display_list.cc
+++ engine/src/flutter/display_list/display_list.cc
@@ -294,9 +294,7 @@ static bool CompareOps(uint8_t* ptrA,
   return true;
 }
 
-void DisplayList::RenderTo(DisplayListBuilder* builder,
-                           SkScalar opacity) const {
-  // TODO(100983): Opacity is not respected and attributes are not reset.
+void DisplayList::RenderTo(DisplayListBuilder* builder) const {
   if (!builder) {
     return;
   }
@@ -308,6 +306,7 @@ void DisplayList::RenderTo(DisplayListBuilder* builder,
 }
 
 void DisplayList::RenderTo(SkCanvas* canvas, SkScalar opacity) const {
+  FML_DCHECK(can_apply_group_opacity() || opacity >= SK_Scalar1);
   DisplayListCanvasDispatcher dispatcher(canvas, opacity);
   if (has_rtree()) {
     Dispatch(dispatcher, canvas->getLocalClipBounds());
--- engine/src/flutter/flow/raster_cache.cc
+++ engine/src/flutter/flow/raster_cache.cc
@@ -110,9 +110,9 @@ bool RasterCache::UpdateCacheEntry(
   return entry.image != nullptr;
 }
 
-int RasterCache::MarkSeen(const RasterCacheKeyID& id,
-                          const SkMatrix& matrix,
-                          bool visible) const {
+RasterCache::CacheInfo RasterCache::MarkSeen(const RasterCacheKeyID& id,
+                                             const SkMatrix& matrix,
+                                             bool visible) const {
   RasterCacheKey key = RasterCacheKey(id, matrix);
   Entry& entry = cache_[key];
   entry.encountered_this_frame = true;
@@ -120,7 +120,7 @@ int RasterCache::MarkSeen(const RasterCacheKeyID& id,
   if (visible || entry.accesses_since_visible > 0) {
     entry.accesses_since_visible++;
   }
-  return entry.accesses_since_visible;
+  return {entry.accesses_since_visible, entry.image != nullptr};
 }
 
 int RasterCache::GetAccessCount(const RasterCacheKeyID& id,
