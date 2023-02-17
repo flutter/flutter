45465026cffb2ebaaf5bfd8e94aa93c53387bfcb
Kevin Chisholm <kevinjchisholm@google.com>
2023-02-16 18:24:53 -0600

    Revert "Remove references to Observatory (#118577)"
    This reverts commit 2df140f40d8c771ecef4f21cbdf635ec1235b4ac.

--- packages/flutter_tools/lib/src/commands/daemon.dart
+++ packages/flutter_tools/lib/src/commands/daemon.dart
@@ -1014,9 +1014,7 @@ class DeviceDomain extends Domain {
     );
     return <String, Object?>{
       'started': result.started,
-      'vmServiceUri': result.vmServiceUri?.toString(),
-      // TODO(bkonyi): remove once clients have migrated to relying on vmServiceUri.
-      'observatoryUri': result.vmServiceUri?.toString(),
+      'observatoryUri': result.observatoryUri?.toString(),
     };
   }
 
