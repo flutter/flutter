e4453a8db50fea692a72d6186790343f75b6cf76
Mateus Felipe C. C. Pinto <mateusfccp@gmail.com>
2023-02-21 14:24:11 -0300

    allow passing --file-reporter option to test running refs #69425 (#120716)
    * allow passing --file-reporter option to test running refs #69425
    
    * Add trailing comma to help to meet style requirements
    
    * Add space between tests for clarity
    
    ---------
    
    Co-authored-by: daniel-v <dvarga@skawa.hu>

--- packages/flutter_tools/lib/src/test/runner.dart
+++ packages/flutter_tools/lib/src/test/runner.dart
@@ -45,6 +45,7 @@ abstract class FlutterTestRunner {
     bool web = false,
     String? randomSeed,
     String? reporter,
+    String? fileReporter,
     String? timeout,
     bool runSkipped = false,
     int? shardIndex,
@@ -82,6 +83,7 @@ class _FlutterTestRunnerImpl implements FlutterTestRunner {
     bool web = false,
     String? randomSeed,
     String? reporter,
+    String? fileReporter,
     String? timeout,
     bool runSkipped = false,
     int? shardIndex,
@@ -103,6 +105,8 @@ class _FlutterTestRunnerImpl implements FlutterTestRunner {
         ...<String>['-r', 'json']
       else if (reporter != null)
         ...<String>['-r', reporter],
+      if (fileReporter != null)
+        '--file-reporter=$fileReporter',
       if (timeout != null)
         ...<String>['--timeout', timeout],
       '--concurrency=$concurrency',
