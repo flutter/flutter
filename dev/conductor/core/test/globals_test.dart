1417bbbd3a0a1bcedd4161c3f5237223ba12c330
Kevin Chisholm <kevinjchisholm@google.com>
2023-02-23 11:55:43 -0600

    update globals_test.dart

--- dev/conductor/core/test/globals_test.dart
+++ dev/conductor/core/test/globals_test.dart
@@ -20,10 +20,6 @@ void main() {
     const String candidateBranch = 'flutter-1.2-candidate.3';
     const String workingBranch = 'cherrypicks-$candidateBranch';
     const String dartRevision = 'fe9708ab688dcda9923f584ba370a66fcbc3811f';
-    const String engineCherrypick1 = 'a5a25cd702b062c24b2c67b8d30b5cb33e0ef6f0';
-    const String engineCherrypick2 = '94d06a2e1d01a3b0c693b94d70c5e1df9d78d249';
-    const String frameworkCherrypick =
-        'a5a25cd702b062c24b2c67b8d30b5cb33e0ef6f0';
 
     final RegExp titlePattern = RegExp(r'&title=(.*)&');
     final RegExp bodyPattern = RegExp(r'&body=(.*)$');
@@ -34,18 +30,11 @@ void main() {
       state = pb.ConductorState(
         engine: pb.Repository(
           candidateBranch: candidateBranch,
-          cherrypicks: <pb.Cherrypick>[
-            pb.Cherrypick(trunkRevision: engineCherrypick1),
-            pb.Cherrypick(trunkRevision: engineCherrypick2),
-          ],
           dartRevision: dartRevision,
           workingBranch: workingBranch,
         ),
         framework: pb.Repository(
           candidateBranch: candidateBranch,
-          cherrypicks: <pb.Cherrypick>[
-            pb.Cherrypick(trunkRevision: frameworkCherrypick),
-          ],
           workingBranch: workingBranch,
         ),
         releaseChannel: releaseChannel,
@@ -90,8 +79,6 @@ void main() {
 ## Scheduled Cherrypicks
 
 - Roll dart revision: dart-lang/sdk@${dartRevision.substring(0, 9)}
-- commit: flutter/engine@${engineCherrypick1.substring(0, 9)}
-- commit: flutter/engine@${engineCherrypick2.substring(0, 9)}
 ''';
       expect(
         Uri.decodeQueryComponent(bodyPattern.firstMatch(link)?.group(1) ?? ''),
@@ -117,12 +104,11 @@ void main() {
           Uri.decodeQueryComponent(
               titlePattern.firstMatch(link)?.group(1) ?? ''),
           '[flutter_releases] Flutter $releaseChannel $releaseVersion Framework Cherrypicks');
-      final String expectedBody = '''
+      const String expectedBody = '''
 # Flutter $releaseChannel $releaseVersion Framework
 
 ## Scheduled Cherrypicks
 
-- commit: ${frameworkCherrypick.substring(0, 9)}
 ''';
       expect(
         Uri.decodeQueryComponent(bodyPattern.firstMatch(link)?.group(1) ?? ''),
