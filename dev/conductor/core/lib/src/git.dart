4d7260f5e841194d79bdcd50c0a36b1b92119285
Kevin Chisholm <kevinjchisholm@google.com>
2023-02-22 18:30:17 -0600

    Update dev/conductor/core/lib/src/git.dart
    Co-authored-by: Christopher Fujino <fujino@google.com>

--- dev/conductor/core/lib/src/git.dart
+++ dev/conductor/core/lib/src/git.dart
@@ -9,6 +9,7 @@ import 'package:process/process.dart';
 import './globals.dart';
 
 /// A wrapper around git process calls that can be mocked for unit testing.
+///
 /// The `Git` class is a relatively (compared to `Repository`) lightweight
 /// abstraction over invocations to the `git` cli tool. The main
 /// motivation for creating this class was so that it could be overridden in
