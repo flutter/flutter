11ab75b1da01331853e388e93b3b69d1fce4e484
Rexios <rexios@rexios.dev>
2023-02-22 19:25:39 -0500

    Documentation

--- packages/flutter/lib/src/widgets/selectable_region.dart
+++ packages/flutter/lib/src/widgets/selectable_region.dart
@@ -1259,7 +1259,13 @@ class _DirectionallyExtendCaretSelectionAction<T extends DirectionalCaretMovemen
   }
 }
 
+/// A delegate for the [SelectionContainer]s of [SelectableRegion]s and their
+/// children.
 class SelectableRegionContainerDelegate extends MultiSelectableSelectionContainerDelegate {
+  /// Create a [SelectableRegionContainerDelegate]
+  /// 
+  /// The [selectionSeparator] is the string that will be used to separate
+  /// the selected text of the children of this delegate.
   SelectableRegionContainerDelegate({super.selectionSeparator = ''});
 
   final Set<Selectable> _hasReceivedStartEvent = <Selectable>{};
