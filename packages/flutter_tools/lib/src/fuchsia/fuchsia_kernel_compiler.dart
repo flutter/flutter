8a080ddec72bbc602fb9d85a402c36d263acfb00
Christopher Fujino <christopherfujino@gmail.com>
2023-02-24 15:57:45 -0800

    foo

--- packages/flutter_tools/lib/src/fuchsia/fuchsia_kernel_compiler.dart
+++ packages/flutter_tools/lib/src/fuchsia/fuchsia_kernel_compiler.dart
@@ -48,7 +48,6 @@ class FuchsiaKernelCompiler {
       throwToolExit('Fuchsia platform file not found at "$platformDill"');
     }
     List<String> flags = <String>[
-      '--no-sound-null-safety',
       '--target',
       'flutter_runner',
       '--platform',
