ace0fea537571f144e5011e01266c5c64165e31f
Skandar Munir Ahmed <sikandernoori@gmail.com>
2023-02-28 00:28:50 +0200

    reword comments

--- packages/flutter/lib/src/cupertino/localizations.dart
+++ packages/flutter/lib/src/cupertino/localizations.dart
@@ -82,7 +82,7 @@ abstract class CupertinoLocalizations {
   /// Day of month that is shown in [CupertinoDatePicker] spinner corresponding
   /// to the given day index.
   ///
-  /// if weekDay is provided then it will also show Day of week alongside day
+  /// If weekDay is provided then it will also show weekday name alongside the numerical day.
   ///
   /// Examples: datePickerDayOfMonth(1) in:
   ///
