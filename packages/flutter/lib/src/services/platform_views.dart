533c7453c25c4d5b84d0f0a85211b8da07fbf170
Rulong Chen（陈汝龙） <26625149+0xZOne@users.noreply.github.com>
2023-02-27 22:52:40 +0800

    Limit the PlatformView ID within the range of 32-bit integers. (#121203)
    Limit the PlatformView ID within the range of 32-bit integers.

--- packages/flutter/lib/src/services/platform_views.dart
+++ packages/flutter/lib/src/services/platform_views.dart
@@ -44,7 +44,16 @@ class PlatformViewsRegistry {
   ///
   /// Typically a platform view identifier is passed to a platform view widget
   /// which creates the platform view and manages its lifecycle.
-  int getNextPlatformViewId() => _nextPlatformViewId++;
+  int getNextPlatformViewId() {
+    // On the Android side, the interface exposed to users uses 32-bit integers.
+    // See https://github.com/flutter/engine/pull/39476 for more details.
+
+    // We can safely assume that a Flutter application will not require more
+    // than MAX_INT32 platform views during its lifetime.
+    const int MAX_INT32 = 0x7FFFFFFF;
+    assert(_nextPlatformViewId <= MAX_INT32);
+    return _nextPlatformViewId++;
+  }
 }
 
 /// Callback signature for when a platform view was created.
