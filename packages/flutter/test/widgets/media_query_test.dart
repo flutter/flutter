1fa659d5d2b92c6973834a8a3c5f2e6e33451173
Michael Goderbauer <goderbauer@google.com>
2023-03-01 18:03:04 -0800

    Update media_query_test.dart

--- packages/flutter/test/widgets/media_query_test.dart
+++ packages/flutter/test/widgets/media_query_test.dart
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'dart:ui' show Brightness, DisplayFeature, DisplayFeatureState, DisplayFeatureType, GestureSettings, PlatformDispatcher, ViewPadding;
+import 'dart:ui' show Brightness, DisplayFeature, DisplayFeatureState, DisplayFeatureType, GestureSettings, PlatformDispatcher, ViewConfiguration, ViewPadding;
 
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
