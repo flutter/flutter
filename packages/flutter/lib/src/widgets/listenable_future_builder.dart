4d397871b2c8a5d2999f0142ff5b3d224661b618
Christian Findlay <16697547+MelbourneDeveloper@users.noreply.github.com>
2023-03-05 16:31:56 +1100

    Doco etc.

--- packages/flutter/lib/src/widgets/listenable_future_builder.dart
+++ packages/flutter/lib/src/widgets/listenable_future_builder.dart
@@ -21,6 +21,99 @@ typedef AsyncListenableBuilder<T extends Listenable> = Widget Function(
   AsyncSnapshot<T> listenableSnapshot,
 );
 
+/// A widget that awaits initialization and then rebuilds when the it receives
+/// a notification from the `Listenable`. Use this widget with `ChangeNotifier`
+/// or `ValueNotifier` to rebuild the widget when the value changes.
+///
+/// Given a [AsyncListenableBuilder<T>] and a [builder] which builds widgets from
+/// the state of the `Listenable`, this class will automatically register itself as a
+/// listener of the [Listenable] and call the [builder] with updated values
+/// when the value changes.
+///
+/// ## Performance optimizations
+///
+/// If your [builder] function contains a subtree that does not depend on the
+/// value of the [Listenable], it's more efficient to build that subtree
+/// once instead of rebuilding it on every animation tick.
+///
+/// If you pass the pre-built subtree as the [child] parameter, the
+/// [ListenableFutureBuilder] will pass it back to your [builder] function so
+/// that you can incorporate it into your build.
+///
+/// Using this pre-built child is entirely optional, but can improve
+/// performance significantly in some cases and is therefore a good practice.
+///
+/// {@tool snippet}
+///
+/// This sample demonstrates how you could use a [ListenableFutureBuilder] to wait
+/// for the initialization to complete, show a [CircularProgressIndicator] while
+/// waiting, and trigger rebuild on state changes without the need for setState
+///
+/// ```dart
+// import 'package:flutter/material.dart';
+// import 'package:notifier_builder/notifier_future_builder.dart';
+
+// void main() {
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) => MaterialApp(
+//         theme: ThemeData(
+//           primarySwatch: Colors.blue,
+//         ),
+//         home: ListenableFutureBuilder(
+//           listenable: getValueNotifier,
+//           builder: (context, child, notifierSnapshot) => Scaffold(
+//             appBar: AppBar(
+//               title: const Text('ListenableFutureBuilder Example'),
+//             ),
+//             body: Center(
+//               child: notifierSnapshot.connectionState == ConnectionState.done
+//                   ? Column(
+//                       mainAxisAlignment: MainAxisAlignment.center,
+//                       children: <Widget>[
+//                         const Text(
+//                           'You have pushed the button this many times:',
+//                         ),
+//                         Text(
+//                           '${notifierSnapshot.data!.value}',
+//                           style: Theme.of(context).textTheme.headlineMedium,
+//                         ),
+//                       ],
+//                     )
+//                   : const CircularProgressIndicator.adaptive(),
+//             ),
+//             floatingActionButton: FloatingActionButton(
+//               onPressed: () => notifierSnapshot.data?.value++,
+//               tooltip: 'Increment',
+//               child: const Icon(Icons.add),
+//             ),
+//           ),
+//         ),
+//       );
+// }
+
+// ///This gets a [ValueNotifier<int>] after 3 seconds
+// Future<ValueNotifier<int>> getValueNotifier() =>
+//     Future<ValueNotifier<int>>.delayed(
+//       const Duration(seconds: 3),
+//       () => ValueNotifier<int>(0),
+//     );
+/// ```
+/// {@end-tool}
+///
+/// See also:
+///
+///  * [AnimatedBuilder], which also triggers rebuilds from a [Listenable]
+///    without passing back a specific value from a [ValueListenable].
+///  * [ValueListenableBuilder], which also triggers rebuilds from a [ValueListenable]
+///  * [NotificationListener], which lets you rebuild based on [Notification]
+///    coming from its descendant widgets rather than a [ValueListenable] that
+///    you have a direct reference to.
 class ListenableFutureBuilder<T extends Listenable> extends StatefulWidget {
   const ListenableFutureBuilder({
     required this.future,
@@ -32,11 +125,7 @@ class ListenableFutureBuilder<T extends Listenable> extends StatefulWidget {
   ///Set this to a fixed function. The widget will only call this once
   final Future<T> Function() future;
 
-  final Widget Function(
-    BuildContext context,
-    Widget? child,
-    AsyncSnapshot<T> notifier,
-  ) builder;
+  final AsyncListenableBuilder builder;
 
   final Widget? child;
 
@@ -71,9 +160,8 @@ class _ListenableFutureBuilderState<T extends Listenable>
   void _handleChange() => setState(() {});
 
   void _subscribe() {
-    final callbackIdentity = Object();
+    final Object callbackIdentity = Object();
     _activeCallbackIdentity = callbackIdentity;
-    // ignore: discarded_futures
     widget.future().then<void>(
       (data) {
         if (_activeCallbackIdentity == callbackIdentity) {
@@ -83,7 +171,6 @@ class _ListenableFutureBuilderState<T extends Listenable>
           });
         }
       },
-      // ignore: avoid_types_on_closure_parameters
       onError: (Object error, StackTrace stackTrace) {
         if (_activeCallbackIdentity == callbackIdentity) {
           setState(() {
