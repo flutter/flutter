30ed2b0c762c57d3cc2db9010a6f2c278eb17d8f
Kate Lovett <katelovett@google.com>
2023-03-08 14:12:04 -0600

    Update packages/flutter/lib/src/widgets/scroll_physics.dart

--- packages/flutter/lib/src/widgets/scroll_physics.dart
+++ packages/flutter/lib/src/widgets/scroll_physics.dart
@@ -70,8 +70,7 @@ enum ScrollDecelerationRate {
 ///
 /// When implementing a subclass, you must override [applyTo] so that it returns
 /// an appropriate instance of your subclass.  Otherwise, classes like
-/// [Scrollable] that consume a [ScrollPosition] may lose your customizations
-/// when they attempt to combine them with a default [ScrollPhysics] object.
+/// [Scrollable] that inform a [ScrollPosition] will combine them with the default [ScrollPhysics] object instead of your custom subclass.
 @immutable
 class ScrollPhysics {
   /// Creates an object with the default scroll physics.
