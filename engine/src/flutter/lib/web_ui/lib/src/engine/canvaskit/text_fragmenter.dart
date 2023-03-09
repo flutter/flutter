a38986884210058bc96e6b5a2b43893f8f8e2707
Mouad Debbar <mdebbar@google.com>
2023-03-09 15:38:04 -0500

    [web] Use CK's malloced array correctly (flutter/engine#40181)
    [web] Use CK's malloced array correctly

--- engine/src/flutter/lib/web_ui/lib/src/engine/canvaskit/text_fragmenter.dart
+++ engine/src/flutter/lib/web_ui/lib/src/engine/canvaskit/text_fragmenter.dart
@@ -43,7 +43,7 @@ final Map<IntlSegmenterGranularity, DomSegmenter> _intlSegmenters = <IntlSegment
   IntlSegmenterGranularity.word: createIntlSegmenter(granularity: 'word'),
 };
 
-Uint32List fragmentUsingIntlSegmenter(
+SkUint32List fragmentUsingIntlSegmenter(
   String text,
   IntlSegmenterGranularity granularity,
 ) {
@@ -56,7 +56,9 @@ Uint32List fragmentUsingIntlSegmenter(
   }
   breaks.add(text.length);
 
-  return mallocUint32List(breaks.length).toTypedArray()..setAll(0, breaks);
+  final SkUint32List mallocedList = mallocUint32List(breaks.length);
+  mallocedList.toTypedArray().setAll(0, breaks);
+  return mallocedList;
 }
 
 // These are the soft/hard line break values expected by Skia's SkParagraph.
@@ -65,12 +67,13 @@ const int _kHardLineBreak = 1;
 
 final DomV8BreakIterator _v8LineBreaker = createV8BreakIterator();
 
-Uint32List fragmentUsingV8LineBreaker(String text) {
+SkUint32List fragmentUsingV8LineBreaker(String text) {
   final List<LineBreakFragment> fragments =
       breakLinesUsingV8BreakIterator(text, _v8LineBreaker);
 
   final int size = (fragments.length + 1) * 2;
-  final Uint32List typedArray = mallocUint32List(size).toTypedArray();
+  final SkUint32List mallocedList = mallocUint32List(size);
+  final Uint32List typedArray = mallocedList.toTypedArray();
 
   typedArray[0] = 0; // start index
   typedArray[1] = _kSoftLineBreak; // break type
@@ -84,5 +87,5 @@ Uint32List fragmentUsingV8LineBreaker(String text) {
         : _kSoftLineBreak;
   }
 
-  return typedArray;
+  return mallocedList;
 }
--- engine/src/flutter/lib/web_ui/test/canvaskit/text_fragmenter_test.dart
+++ engine/src/flutter/lib/web_ui/test/canvaskit/text_fragmenter_test.dart
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'dart:typed_data';
-
 import 'package:test/bootstrap/browser.dart';
 import 'package:test/test.dart';
 import 'package:ui/src/engine.dart';
@@ -17,25 +15,33 @@ void main() {
 void testMain() {
   setUpCanvasKitTest();
 
+  late SkUint32List breaks;
+
+  tearDown(() {
+    if (browserSupportsCanvaskitChromium) {
+      free(breaks);
+    }
+  });
+
   group('$fragmentUsingIntlSegmenter', () {
     test('fragments text into words', () {
-      final Uint32List breaks = fragmentUsingIntlSegmenter(
+      breaks = fragmentUsingIntlSegmenter(
         'Hello world 你好世界',
         IntlSegmenterGranularity.word,
       );
       expect(
-        breaks,
+        breaks.toTypedArray(),
         orderedEquals(<int>[0, 5, 6, 11, 12, 14, 16]),
       );
     });
 
     test('fragments multi-line text into words', () {
-      final Uint32List breaks = fragmentUsingIntlSegmenter(
+      breaks = fragmentUsingIntlSegmenter(
         'Lorem ipsum\ndolor 你好世界 sit\namet',
         IntlSegmenterGranularity.word,
       );
       expect(
-        breaks,
+        breaks.toTypedArray(),
         orderedEquals(<int>[
           0, 5, 6, 11, 12, // "Lorem ipsum\n"
           17, 18, 20, 22, 23, 26, 27, // "dolor 你好世界 sit\n"
@@ -47,12 +53,12 @@ void testMain() {
     test('fragments text into grapheme clusters', () {
       // The smiley emoji has a length of 2.
       // The family emoji has a length of 11.
-      final Uint32List breaks = fragmentUsingIntlSegmenter(
+      breaks = fragmentUsingIntlSegmenter(
         'Lorem🙂ipsum👨‍👩‍👧‍👦',
         IntlSegmenterGranularity.grapheme,
       );
       expect(
-        breaks,
+        breaks.toTypedArray(),
         orderedEquals(<int>[
           0, 1, 2, 3, 4, 5, 7, // "Lorem🙂"
           8, 9, 10, 11, 12, 23, // "ipsum👨‍👩‍👧‍👦"
@@ -63,12 +69,12 @@ void testMain() {
     test('fragments multi-line text into grapheme clusters', () {
       // The smiley emojis have a length of 2 each.
       // The family emoji has a length of 11.
-      final Uint32List breaks = fragmentUsingIntlSegmenter(
+      breaks = fragmentUsingIntlSegmenter(
         'Lorem🙂\nipsum👨‍👩‍👧‍👦dolor\n😄',
         IntlSegmenterGranularity.grapheme,
       );
       expect(
-        breaks,
+        breaks.toTypedArray(),
         orderedEquals(<int>[
           0, 1, 2, 3, 4, 5, 7, 8, // "Lorem🙂\n"
           9, 10, 11, 12, 13, 24, // "ipsum👨‍👩‍👧‍👦"
@@ -83,11 +89,11 @@ void testMain() {
     const int kHard = 1;
 
     test('fragments text into soft and hard line breaks', () {
-      final Uint32List breaks = fragmentUsingV8LineBreaker(
+      breaks = fragmentUsingV8LineBreaker(
         'Lorem-ipsum 你好🙂\nDolor sit',
       );
       expect(
-        breaks,
+        breaks.toTypedArray(),
         orderedEquals(<int>[
           0, kSoft,
           6, kSoft, // "Lorem-"
