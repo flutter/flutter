7f250234104ae74bd36115780d5fca012d15ee6c
LongCatIsLooong <31859944+LongCatIsLooong@users.noreply.github.com>
2023-03-10 15:42:18 -0800

    Revert "Make FlutterTest the default test font (#40188)" (#40237)
    This reverts commit 9270e3de510eaee878920ffb0542fc78d2cdc9aa.

--- testing/dart/paragraph_builder_test.dart
+++ testing/dart/paragraph_builder_test.dart
@@ -7,6 +7,9 @@ import 'dart:ui';
 import 'package:litetest/litetest.dart';
 
 void main() {
+  // The actual values for font measurements will vary by platform slightly.
+  const double epsillon = 0.0001;
+
   test('Should be able to build and layout a paragraph', () {
     final ParagraphBuilder builder = ParagraphBuilder(ParagraphStyle());
     builder.addText('Hello');
@@ -37,10 +40,10 @@ void main() {
 
     final List<TextBox> boxes = paragraph.getBoxesForRange(0, 3);
     expect(boxes.length, 1);
-    expect(boxes.first.left, 0.0);
-    expect(boxes.first.top, 0.0);
-    expect(boxes.first.right, 42.0);
-    expect(boxes.first.bottom, 14.0);
+    expect(boxes.first.left, 0);
+    expect(boxes.first.top, closeTo(0, epsillon));
+    expect(boxes.first.right, closeTo(42, epsillon));
+    expect(boxes.first.bottom, closeTo(14, epsillon));
     expect(boxes.first.direction, TextDirection.ltr);
   });
 
@@ -57,13 +60,13 @@ void main() {
     final List<LineMetrics> metrics = paragraph.computeLineMetrics();
     expect(metrics.length, 1);
     expect(metrics.first.hardBreak, true);
-    expect(metrics.first.ascent, 10.5);
-    expect(metrics.first.descent, 3.5);
-    expect(metrics.first.unscaledAscent, 10.5);
+    expect(metrics.first.ascent, closeTo(11.200042724609375, epsillon));
+    expect(metrics.first.descent, closeTo(2.799957275390625, epsillon));
+    expect(metrics.first.unscaledAscent, closeTo(11.200042724609375, epsillon));
     expect(metrics.first.height, 14.0);
     expect(metrics.first.width, 70.0);
     expect(metrics.first.left, 0.0);
-    expect(metrics.first.baseline, 10.5);
+    expect(metrics.first.baseline, closeTo(11.200042724609375, epsillon));
     expect(metrics.first.lineNumber, 0);
   });
 }
