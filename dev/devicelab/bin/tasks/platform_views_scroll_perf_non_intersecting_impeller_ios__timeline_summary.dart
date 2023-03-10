7e6804b048b90cf1957bdedc8f4f645003c52f9c
Jonah Williams <jonahwilliams@google.com>
2023-03-10 08:16:30 -0800

    Revert "[Impeller] Temporary flag flip for devicelab tests to use Impeller redux. (#122340)" (#122362)
    Revert "[Impeller] Temporary flag flip for devicelab tests to use Impeller redux."

--- dev/devicelab/bin/tasks/platform_views_scroll_perf_non_intersecting_impeller_ios__timeline_summary.dart
+++ dev/devicelab/bin/tasks/platform_views_scroll_perf_non_intersecting_impeller_ios__timeline_summary.dart
@@ -8,6 +8,5 @@ import 'package:flutter_devicelab/tasks/perf_tests.dart';
 
 Future<void> main() async {
   deviceOperatingSystem = DeviceOperatingSystem.ios;
-  // ignore: avoid_redundant_argument_values
   await task(createUiKitViewScrollPerfNonIntersectingTest(enableImpeller: true));
 }
