d1fdc5b524dd6eef8d3e7080377a6364ce292fac
Taha Tesser <tessertaha@gmail.com>
2023-03-13 17:13:57 +0200

    Refactor `SliverAppBar.medium` & `SliverAppBar.large` to fix several issues

--- packages/flutter/test/widgets/nested_scroll_view_test.dart
+++ packages/flutter/test/widgets/nested_scroll_view_test.dart
@@ -2679,8 +2679,8 @@ void main() {
             return <Widget>[
               SliverOverlapAbsorber(
                 handle: NestedScrollView.sliverOverlapAbsorberHandleFor(context),
-                sliver: SliverAppBar.medium(
-                  title: const Text('AppBar Title'),
+                sliver: const SliverAppBar.medium(
+                  title: Text('AppBar Title'),
                 ),
               ),
             ];
@@ -2706,11 +2706,11 @@ void main() {
     ));
 
     // There are two widgets for the title.
-    final Finder expandedTitle = find.text('AppBar Title').last;
+    final Finder expandedTitle = find.text('AppBar Title').first;
     final Finder expandedTitleClip = find.ancestor(
       of: expandedTitle,
       matching: find.byType(ClipRect),
-    );
+    ).first;
 
     // Default, fully expanded app bar.
     expect(nestedScrollView.currentState?.outerController.offset, 0);
@@ -2789,11 +2789,11 @@ void main() {
     ));
 
     // There are two widgets for the title.
-    final Finder expandedTitle = find.text('AppBar Title').last;
+    final Finder expandedTitle = find.text('AppBar Title').first;
     final Finder expandedTitleClip = find.ancestor(
       of: expandedTitle,
       matching: find.byType(ClipRect),
-    );
+    ).first;
 
     // Default, fully expanded app bar.
     expect(nestedScrollView.currentState?.outerController.offset, 0);
