61b65d5259e0549e0bbaefc028b03e8fe9f9d764
Casey Hillers <chillers@google.com>
2023-03-14 11:11:42 -0700

    rm trailing whitespace

--- packages/flutter/lib/src/widgets/semantics_debugger.dart
+++ packages/flutter/lib/src/widgets/semantics_debugger.dart
@@ -366,8 +366,8 @@ class _SemanticsDebuggerPainter extends CustomPainter {
     final Rect rect = node.rect;
     if (!rect.isEmpty) {
       // To minmize changes to goldens tests, use a static color.
-      final Color lineColor = const bool.fromEnvironment('FLUTTER_TEST') 
-        ? const Color(0xFF027DFD) // Flutter Blue 
+      final Color lineColor = const bool.fromEnvironment('FLUTTER_TEST')
+        ? const Color(0xFF027DFD) // Flutter Blue
         : Color(0xFF000000 + math.Random(node.id).nextInt(0xFFFFFF));
       final Rect innerRect = rect.deflate(rank * 1.0);
       if (innerRect.isEmpty) {
