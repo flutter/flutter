7fa88fb225f87d07303765db09b9dace9fea4b3e
pdblasi-google <109253501+pdblasi-google@users.noreply.github.com>
2023-03-14 13:03:20 -0700

    Updates `flutter_localizations/test` to stop using `TestWindow` (#122564)
    Reland: Updates `flutter_localizations/test` to stop using `TestWindow`

--- packages/flutter_localizations/test/material/date_picker_test.dart
+++ packages/flutter_localizations/test/material/date_picker_test.dart
@@ -227,10 +227,9 @@ void main() {
     const Size kCommonScreenSizeLandscape = Size(1770, 1070);
 
     Future<void> showPicker(WidgetTester tester, Locale locale, Size size) async {
-      tester.binding.window.physicalSizeTestValue = size;
-      addTearDown(tester.binding.window.clearPhysicalSizeTestValue);
-      tester.binding.window.devicePixelRatioTestValue = 1.0;
-      addTearDown(tester.binding.window.clearDevicePixelRatioTestValue);
+      tester.view.physicalSize = size;
+      tester.view.devicePixelRatio = 1.0;
+      addTearDown(tester.view.reset);
       await tester.pumpWidget(
         MaterialApp(
           home: Builder(
