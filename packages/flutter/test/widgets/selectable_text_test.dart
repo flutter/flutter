358cc5a737954d3e0bd807bea568bbf3d84c24c7
Renzo Olivares <rmolivares@renzo-olivares.dev>
2023-03-15 14:47:28 -0700

    fix selectable text tests

--- packages/flutter/test/widgets/selectable_text_test.dart
+++ packages/flutter/test/widgets/selectable_text_test.dart
@@ -5302,7 +5302,7 @@ void main() {
       await tester.tapAt(textOffsetToPosition(tester, testValue.indexOf('e')));
       await tester.pump(const Duration(milliseconds: 30));
       await tester.tapAt(textOffsetToPosition(tester, testValue.indexOf('e')));
-      await tester.pump(const Duration(milliseconds: 30));
+      await tester.pumpAndSettle();
 
       final TextSelection selection = TextSelection(
         baseOffset: testValue.indexOf('d'),
