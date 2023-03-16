06d17b239c81760c6da3ca8efefb89dd5a7780be
Robert Ancell <robert.ancell@canonical.com>
2023-03-17 10:23:24 +1300

    Fix incorrect usage of fl_method_error_response_new in documentation example (flutter/engine#40288)

--- engine/src/flutter/shell/platform/linux/public/flutter_linux/fl_method_channel.h
+++ engine/src/flutter/shell/platform/linux/public/flutter_linux/fl_method_channel.h
@@ -44,14 +44,19 @@ G_DECLARE_FINAL_TYPE(FlMethodChannel,
  *     g_autoptr(GError) bar_error = NULL;
  *     g_autoptr(FlValue) result =
  *         do_bar (fl_method_call_get_args (method_call), &bar_error);
- *     if (result == NULL)
+ *     if (result == NULL) {
  *       response =
  *         FL_METHOD_RESPONSE (fl_method_error_response_new ("bar error",
- *                                                           bar_error->message));
- *     else
- *       response = FL_METHOD_RESPONSE (fl_method_success_response_new
- * (result)); } else response = FL_METHOD_RESPONSE
- * (fl_method_not_implemented_response_new ());
+ *                                                           bar_error->message,
+ *                                                           nullptr);
+ *     } else {
+ *       response =
+ *         FL_METHOD_RESPONSE (fl_method_success_response_new (result));
+ *     }
+ *   } else {
+ *     response =
+ *       FL_METHOD_RESPONSE (fl_method_not_implemented_response_new ());
+ *   }
  *
  *   g_autoptr(GError) error = NULL;
  *   if (!fl_method_call_respond(method_call, response, &error))
