c295f291f5fe6444c884f08281ec51b7bbed803c
camsim99 <camillesimon90@gmail.com>
2023-03-16 13:43:45 -0700

    Add warning

--- packages/flutter/lib/src/widgets/editable_text.dart
+++ packages/flutter/lib/src/widgets/editable_text.dart
@@ -2415,7 +2415,22 @@ class EditableTextState extends State<EditableText> with AutomaticKeepAliveClien
       // Only enable spell check if a non-disabled configuration is provided
       // and if that configuration does not specify a spell check service,
       // a native spell checker must be supported.
-      print("Spell check was enabled with spellCheckConfiguration, but the current platform does not have a supported spell check service, and none was provided.");
+      assert(() { 
+        if (!spellCheckServiceIsConfigured) {
+          FlutterError.reportError( 
+            FlutterErrorDetails( 
+              exception: FlutterError( 
+                'Spell check was enabled with spellCheckConfiguration, but the '
+                'current platform does not have a supported spell check '
+                'service, and none was provided.',
+              ), 
+              library: 'widget library', 
+              stack: StackTrace.current, 
+            ), 
+          ); 
+        } 
+        return true; 
+      }()); 
       return const SpellCheckConfiguration.disabled();
     }
 
