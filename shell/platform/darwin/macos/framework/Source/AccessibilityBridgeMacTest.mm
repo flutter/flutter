c4eb291b61a72d9dae16e75318261630455e5835
Chris Bracken <chris@bracken.jp>
2023-03-15 18:25:08 -0700

    [macOS,Windows] Use static casts where possible (#40334)
    [macOS,Windows] Use static casts where possible

--- shell/platform/darwin/macos/framework/Source/AccessibilityBridgeMacTest.mm
+++ shell/platform/darwin/macos/framework/Source/AccessibilityBridgeMacTest.mm
@@ -71,7 +71,7 @@ TEST(AccessibilityBridgeMacTest, sendsAccessibilityCreateNotificationToWindowOfF
   // Setting up bridge so that the AccessibilityBridgeMacDelegateSpy
   // can query semantics information from.
   engine.semanticsEnabled = YES;
-  auto bridge = std::reinterpret_pointer_cast<AccessibilityBridgeMacSpy>(
+  auto bridge = std::static_pointer_cast<AccessibilityBridgeMacSpy>(
       viewController.accessibilityBridge.lock());
   FlutterSemanticsNode root;
   root.id = 0;
@@ -119,7 +119,7 @@ TEST(AccessibilityBridgeMacTest, doesNotSendAccessibilityCreateNotificationWhenH
   // Setting up bridge so that the AccessibilityBridgeMacDelegateSpy
   // can query semantics information from.
   engine.semanticsEnabled = YES;
-  auto bridge = std::reinterpret_pointer_cast<AccessibilityBridgeMacSpy>(
+  auto bridge = std::static_pointer_cast<AccessibilityBridgeMacSpy>(
       viewController.accessibilityBridge.lock());
   FlutterSemanticsNode root;
   root.id = 0;
@@ -166,7 +166,7 @@ TEST(AccessibilityBridgeMacTest, doesNotSendAccessibilityCreateNotificationWhenN
   // Setting up bridge so that the AccessibilityBridgeMacDelegateSpy
   // can query semantics information from.
   engine.semanticsEnabled = YES;
-  auto bridge = std::reinterpret_pointer_cast<AccessibilityBridgeMacSpy>(
+  auto bridge = std::static_pointer_cast<AccessibilityBridgeMacSpy>(
       viewController.accessibilityBridge.lock());
   FlutterSemanticsNode root;
   root.id = 0;
