c1ee85eb2d1124dc2c2fc2451ae61e80596d5e60
Christopher Fujino <fujino@google.com>
2023-03-17 11:23:18 -0700

    Revert "[flutter_tools] Remove sound null safety flag (#120936)"
    This reverts commit 7c3088cf22c8a863ca38e3890851fd8dc776768d.

--- packages/flutter_tools/test/commands.shard/hermetic/run_test.dart
+++ packages/flutter_tools/test/commands.shard/hermetic/run_test.dart
@@ -1005,6 +1005,7 @@ void main() {
       '--trace-skia',
       '--trace-systrace',
       '--verbose-system-logs',
+      '--null-assertions',
       '--native-null-assertions',
       '--enable-impeller',
       '--trace-systrace',
@@ -1021,6 +1022,7 @@ void main() {
     expect(options.traceSkia, true);
     expect(options.traceSystrace, true);
     expect(options.verboseSystemLogs, true);
+    expect(options.nullAssertions, true);
     expect(options.nativeNullAssertions, true);
     expect(options.traceSystrace, true);
     expect(options.enableImpeller, true);
