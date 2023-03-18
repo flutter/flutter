09793bd5271f134b47c6e2d702b43c9e4f6d2fd2
Bartek Pacia <barpac02@gmail.com>
2023-03-18 15:28:19 +0100

    create a GradleLazyCleanTaskMigration

---
+++ packages/flutter_tools/lib/src/android/migrations/gradle_lazy_clean_task_migration.dart
@@ -0,0 +1,50 @@
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import '../../base/file_system.dart';
+import '../../base/project_migrator.dart';
+import '../../project.dart';
+
+const String _eagerCleanTaskDeclaration = '''
+task clean(type: Delete) {
+    delete rootProject.buildDir
+}
+''' ;
+
+const String _lazyCleanTaskDeclaration = '''
+tasks.register("clean", Delete) {
+    delete rootProject.buildDir
+}
+''';
+
+/// Migrate the Gradle "clean" task to use modern, lazy declaration style.
+class GradleLazyCleanTaskMigrator extends ProjectMigrator {
+  GradleLazyCleanTaskMigrator(
+  AndroidProject project,
+    super.logger,
+  )   : _topLevelGradleBuildFile = project.hostAppGradleRoot.childFile('build.gradle');
+
+  final File _topLevelGradleBuildFile;
+
+  @override
+  void migrate() {
+    if (!_topLevelGradleBuildFile.existsSync()) {
+      logger.printTrace('Top-level Gradle build file not found, skipping "clean" task migration.');
+      return;
+    }
+
+    processFileLines(_topLevelGradleBuildFile);
+  }
+
+  @override
+  String migrateFileContents(String fileContents) {
+    String newContents = fileContents;
+    if (newContents.contains(_eagerCleanTaskDeclaration)) {
+      logger.printTrace('Migrating "clean" Gradle task to lazy declaration style.');
+      newContents = newContents.replaceAll(_eagerCleanTaskDeclaration, _lazyCleanTaskDeclaration);
+    }
+    
+    return newContents;
+  }
+}
