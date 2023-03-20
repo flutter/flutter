6a274fe3515dda0e80aca9c01e7f9fdd40455f1d
Dan Field <dnfield@google.com>
2023-03-20 13:48:39 -0700

    Disable MTLCommandBufferErrorOptionEncoderExecutionStatus in release mode (#40453)

--- impeller/renderer/backend/metal/command_buffer_mtl.mm
+++ impeller/renderer/backend/metal/command_buffer_mtl.mm
@@ -111,6 +111,7 @@ static bool LogMTLCommandBufferErrorIfPresent(id<MTLCommandBuffer> buffer) {
 }
 
 static id<MTLCommandBuffer> CreateCommandBuffer(id<MTLCommandQueue> queue) {
+#ifndef FLUTTER_RELEASE
   if (@available(iOS 14.0, macOS 11.0, *)) {
     auto desc = [[MTLCommandBufferDescriptor alloc] init];
     // Degrades CPU performance slightly but is well worth the cost for typical
@@ -118,6 +119,7 @@ static id<MTLCommandBuffer> CreateCommandBuffer(id<MTLCommandQueue> queue) {
     desc.errorOptions = MTLCommandBufferErrorOptionEncoderExecutionStatus;
     return [queue commandBufferWithDescriptor:desc];
   }
+#endif  // FLUTTER_RELEASE
   return [queue commandBuffer];
 }
 
