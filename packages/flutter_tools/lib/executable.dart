0b14ef7139e71634a8b01af0935bb61643238625
Janice Collins <jcollins@google.com>
2023-03-22 15:12:23 -0700

    fix nit

--- packages/flutter_tools/lib/executable.dart
+++ packages/flutter_tools/lib/executable.dart
@@ -152,7 +152,7 @@ List<FlutterCommand> generateCommands({
         platform: globals.platform,
       ),
     ],
-    suppressAnalytics : globals.flutterUsage.suppressAnalytics,
+    suppressAnalytics: globals.flutterUsage.suppressAnalytics,
   ),
   AssembleCommand(verboseHelp: verboseHelp, buildSystem: globals.buildSystem),
   AttachCommand(
