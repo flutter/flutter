860daad59f2096e45fe1c57a553baa6472f08eca
Chris Bracken <chris@bracken.jp>
2023-03-22 15:43:58 -0700

    Add comment on manual registration

--- examples/platform_channel/macos/Runner/MainFlutterWindow.swift
+++ examples/platform_channel/macos/Runner/MainFlutterWindow.swift
@@ -12,7 +12,12 @@ class MainFlutterWindow: NSWindow {
     self.contentViewController = flutterViewController
     self.setFrame(windowFrame, display: true)
 
+    // Since the plugin lives in the app itself, registration isn't auto-generated into
+    // RegisterGeneratedPlugins in GeneratedPluginRegistrant.swift, so we register the plugin
+    // manually.
     BatteryLevelPlugin.register(with: flutterViewController.registrar(forPlugin: "BatteryLevel"))
+
+    RegisterGeneratedPlugins(registry: flutterViewController)
 
     super.awakeFromNib()
   }
