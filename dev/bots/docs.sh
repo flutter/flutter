726b7202a037af19b83b25af74883617cf678106
Janice Collins <jcollins@google.com>
2023-03-24 12:50:57 -0700

    Update dartdoc to 6.2.1. (#123420)
    Update dartdoc to 6.2.1.

--- dev/bots/docs.sh
+++ dev/bots/docs.sh
@@ -20,7 +20,7 @@ function generate_docs() {
     # Install and activate dartdoc.
     # When updating to a new dartdoc version, please also update
     # `dartdoc_options.yaml` to include newly introduced error and warning types.
-    "$DART" pub global activate dartdoc 6.2.0
+    "$DART" pub global activate dartdoc 6.2.1
 
     # Install and activate the snippets tool, which resides in the
     # assets-for-api-docs repo:
