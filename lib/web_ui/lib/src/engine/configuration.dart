91cc9ce7de9689ad304864931058f89540e6ef93
Harry Terkelsen <hterkelsen@users.noreply.github.com>
2023-03-24 11:41:33 -0700

    Reland "Default the CanvasKit base URL to local artifacts. (#40293)" (#40470)
    This reverts commit bb971ab55f413303f9b3429babd075d9e35e0a43.

--- lib/web_ui/lib/src/engine/configuration.dart
+++ lib/web_ui/lib/src/engine/configuration.dart
@@ -50,11 +50,6 @@ import 'package:meta/meta.dart';
 import 'canvaskit/renderer.dart';
 import 'dom.dart';
 
-/// The version of CanvasKit used by the web engine by default.
-// DO NOT EDIT THE NEXT LINE OF CODE MANUALLY
-// See `lib/web_ui/README.md` for how to roll CanvasKit to a new version.
-const String _canvaskitVersion = '0.38.0';
-
 /// The Web Engine configuration for the current application.
 FlutterConfiguration get configuration =>
   _configuration ??= FlutterConfiguration.legacy(_jsConfiguration);
@@ -183,7 +178,7 @@ class FlutterConfiguration {
   String get canvasKitBaseUrl => _configuration?.canvasKitBaseUrl ?? _defaultCanvasKitBaseUrl;
   static const String _defaultCanvasKitBaseUrl = String.fromEnvironment(
     'FLUTTER_WEB_CANVASKIT_URL',
-    defaultValue: 'https://unpkg.com/canvaskit-wasm@$_canvaskitVersion/bin/',
+    defaultValue: 'canvaskit/',
   );
 
   /// The variant of CanvasKit to download.
