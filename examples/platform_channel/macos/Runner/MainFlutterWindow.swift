421c0fd72fc20d3d4f26e0eb58b3f10164d761fa
Chris Bracken <chris@bracken.jp>
2023-03-27 14:51:47 -0700

    [macOS] platform_channel sample style cleanups
    Applies a few Swift style cleanups to the macOS platform_channel sample.
    
    Also adds ChannelName, BatteryState, ErrorCode enums to bring the
    example a bit closer to the iOS Swift example. Will follow up with a
    patch that applies relevant style fixes to the iOS Swift example.
    
    No test changes since app semantics have not changed.
    
    Issue: https://github.com/flutter/flutter/issues/79204

--- examples/platform_channel/macos/Runner/MainFlutterWindow.swift
+++ examples/platform_channel/macos/Runner/MainFlutterWindow.swift
@@ -5,37 +5,54 @@
 import Cocoa
 import FlutterMacOS
 
-class MainFlutterWindow: NSWindow, FlutterStreamHandler, PowerSourceStateChangeDelegate {
+enum ChannelName {
+  static let battery = "samples.flutter.io/battery"
+  static let charging = "samples.flutter.io/charging"
+}
+
+enum BatteryState {
+  static let charging = "charging"
+  static let discharging = "discharging"
+  static let unavailable = "UNAVAILABLE"
+}
+
+enum ErrorCode {
+  static let noBattery = "NO_BATTERY"
+  static let unavailable = "UNAVAILABLE"
+}
+
+class MainFlutterWindow: NSWindow {
   private let powerSource = PowerSource()
-  private let stateChangeSource = PowerSourceStateChangeHandler()
+  private let stateChangeHandler = PowerSourceStateChangeHandler()
   private var eventSink: FlutterEventSink?
 
   override func awakeFromNib() {
     let flutterViewController = FlutterViewController.init()
     let windowFrame = self.frame
     self.contentViewController = flutterViewController
+    self.displayIfNeeded()
     self.setFrame(windowFrame, display: true)
 
     // Register battery method channel.
     let registrar = flutterViewController.registrar(forPlugin: "BatteryLevel")
     let batteryChannel = FlutterMethodChannel(
-      name: "samples.flutter.io/battery",
+      name: ChannelName.battery,
       binaryMessenger: registrar.messenger)
-    batteryChannel.setMethodCallHandler({ [weak self] (call, result) in
+    batteryChannel.setMethodCallHandler { [powerSource = self.powerSource] (call, result) in
       switch call.method {
       case "getBatteryLevel":
-        if self?.powerSource.hasBattery() == false {
-          result(FlutterError(
-            code: "NO_BATTERY",
-            message: "Device does not have a battery",
-            details: nil))
-          return
-        }
-        let level = self?.powerSource.getCurrentCapacity()
-        if level == -1 {
+        guard powerSource.hasBattery() else {
           result(
             FlutterError(
-              code: "UNAVAILABLE",
+              code: ErrorCode.noBattery,
+              message: "Device does not have a battery",
+              details: nil))
+          return
+        }
+        guard let level = powerSource.getCurrentCapacity() else {
+          result(
+            FlutterError(
+              code: ErrorCode.unavailable,
               message: "Battery info unavailable",
               details: nil))
           return
@@ -44,11 +61,11 @@ class MainFlutterWindow: NSWindow, FlutterStreamHandler, PowerSourceStateChangeD
       default:
         result(FlutterMethodNotImplemented)
       }
-    })
+    }
 
     // Register charging event channel.
     let chargingChannel = FlutterEventChannel(
-      name: "samples.flutter.io/charging",
+      name: ChannelName.charging,
       binaryMessenger: registrar.messenger)
     chargingChannel.setStreamHandler(self)
 
@@ -57,36 +74,40 @@ class MainFlutterWindow: NSWindow, FlutterStreamHandler, PowerSourceStateChangeD
     super.awakeFromNib()
   }
 
+  /// Emit a power status event to the registered event sink.
+  func emitPowerStatusEvent() {
+    if let sink = self.eventSink {
+      switch self.powerSource.getPowerState() {
+      case .ac:
+        sink(BatteryState.charging)
+      case .battery:
+        sink(BatteryState.discharging)
+      case .unknown:
+        sink(BatteryState.unavailable)
+      }
+    }
+  }
+}
+
+extension MainFlutterWindow: FlutterStreamHandler {
   func onListen(withArguments arguments: Any?, eventSink events: @escaping FlutterEventSink)
     -> FlutterError?
   {
     self.eventSink = events
     self.emitPowerStatusEvent()
-    self.stateChangeSource.delegate = self
+    self.stateChangeHandler.delegate = self
     return nil
   }
 
   func onCancel(withArguments arguments: Any?) -> FlutterError? {
-    self.stateChangeSource.delegate = nil
+    self.stateChangeHandler.delegate = nil
     self.eventSink = nil
     return nil
   }
+}
 
-  func onPowerSourceStateChanged() {
+extension MainFlutterWindow: PowerSourceStateChangeDelegate {
+  func didChangePowerSourceState() {
     self.emitPowerStatusEvent()
   }
-
-  func emitPowerStatusEvent() {
-    if let sink = self.eventSink {
-      switch self.powerSource.getPowerState() {
-      case .ac:
-        sink("charging")
-      case .battery:
-        sink("discharging")
-      case .unknown:
-        sink("UNAVAILABLE")
-      }
-    }
-  }
-
 }
