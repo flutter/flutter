0d8d295c411974570290ea8cef7a6a8758b27cf1
David Iglesias <ditman@gmail.com>
2023-03-28 13:21:56 -0700

    Reland "[web] Accepts assetBase through JS config. (#40615)" (#40677)
    Reland "[web] Accepts assetBase through JS config. (#40615)"

--- lib/web_ui/lib/src/engine/assets.dart
+++ lib/web_ui/lib/src/engine/assets.dart
@@ -24,26 +24,48 @@ const Map<String, String> testFontUrls = <String, String>{
 
 /// This class downloads assets over the network.
 ///
-/// The assets are resolved relative to [assetsDir] inside the directory
-/// containing the currently executing JS script.
+/// Assets are resolved relative to [assetsDir] inside the absolute base
+/// specified by [assetBase] (optional).
+///
+/// By default, URLs are relative to the `<base>` of the current website.
 class AssetManager {
-  /// Initializes [AssetManager] with path to assets relative to baseUrl.
-  const AssetManager({this.assetsDir = _defaultAssetsDir});
+  /// Initializes [AssetManager] with paths.
+  AssetManager({
+    this.assetsDir = _defaultAssetsDir,
+    String? assetBase,
+  })  : assert(
+          assetBase == null || assetBase.endsWith('/'),
+          '`assetBase` must end with a `/` character.',
+        ),
+        _assetBase = assetBase;
 
   static const String _defaultAssetsDir = 'assets';
 
   /// The directory containing the assets.
   final String assetsDir;
 
-  String? get _baseUrl {
-    return domWindow.document
-        .querySelectorAll('meta')
-        .where((DomElement domNode) => domInstanceOfString(domNode,
-                'HTMLMetaElement'))
-        .map((DomElement domNode) => domNode as DomHTMLMetaElement)
-        .firstWhereOrNull(
-            (DomHTMLMetaElement element) => element.name == 'assetBase')
-        ?.content;
+  /// The absolute base URL for assets.
+  String? _assetBase;
+
+  // Cache a value for `_assetBase` so we don't hit the DOM multiple times.
+  String get _baseUrl => _assetBase ??= _deprecatedAssetBase ?? '';
+
+  // Retrieves the `assetBase` value from the DOM.
+  //
+  // This warns the user and points them to the new initializeEngine style.
+  String? get _deprecatedAssetBase {
+    final DomHTMLMetaElement? meta = domWindow.document
+        .querySelector('meta[name=assetBase]') as DomHTMLMetaElement?;
+
+    final String? fallbackBaseUrl = meta?.content;
+
+    if (fallbackBaseUrl != null) {
+      // Warn users that they're using a deprecated configuration style...
+      domWindow.console.warn('The `assetBase` meta tag is now deprecated.\n'
+          'Use engineInitializer.initializeEngine(config) instead.\n'
+          'See: https://docs.flutter.dev/development/platform-integration/web/initialization');
+    }
+    return fallbackBaseUrl;
   }
 
   /// Returns the URL to load the asset from, given the asset key.
@@ -67,7 +89,7 @@ class AssetManager {
     if (Uri.parse(asset).hasScheme) {
       return Uri.encodeFull(asset);
     }
-    return Uri.encodeFull('${_baseUrl ?? ''}$assetsDir/$asset');
+    return Uri.encodeFull('$_baseUrl$assetsDir/$asset');
   }
 
   /// Loads an asset and returns the server response.
@@ -90,7 +112,7 @@ class AssetManager {
 }
 
 /// An asset manager that gives fake empty responses for assets.
-class WebOnlyMockAssetManager implements AssetManager {
+class WebOnlyMockAssetManager extends AssetManager {
   /// Mock asset directory relative to base url.
   String defaultAssetsDir = '';
 
@@ -114,9 +136,6 @@ class WebOnlyMockAssetManager implements AssetManager {
   String get assetsDir => defaultAssetsDir;
 
   @override
-  String get _baseUrl => '';
-
-  @override
   String getAssetUrl(String asset) => asset;
 
   @override
@@ -127,7 +146,7 @@ class WebOnlyMockAssetManager implements AssetManager {
         status: 200,
         payload: MockHttpFetchPayload(
           byteBuffer: _toByteData(utf8.encode(defaultAssetManifest)).buffer,
-        )
+        ),
       );
     }
     if (asset == getAssetUrl('FontManifest.json')) {
@@ -136,7 +155,7 @@ class WebOnlyMockAssetManager implements AssetManager {
         status: 200,
         payload: MockHttpFetchPayload(
           byteBuffer: _toByteData(utf8.encode(defaultFontManifest)).buffer,
-        )
+        ),
       );
     }
 
