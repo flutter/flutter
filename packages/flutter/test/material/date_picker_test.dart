624907c46dad0561202d9977ee8c7b6f474aa80a
Hans Muller <hansmuller@google.com>
2023-03-29 16:47:42 -0700

    M3 DatePicker landscape header text style is now TextTheme.headlineSmall

--- packages/flutter/test/material/date_picker_test.dart
+++ packages/flutter/test/material/date_picker_test.dart
@@ -1387,6 +1387,34 @@ void main() {
       expect(find.byType(TextField), findsNothing);
     });
   });
+
+  group('Landscape input-only date picker headers use headlineSmall', () {
+    // Regression test for https://github.com/flutter/flutter/issues/122056
+
+    // Common screen size roughly based on a Pixel 1
+    const Size kCommonScreenSizePortrait = Size(1070, 1770);
+    const Size kCommonScreenSizeLandscape = Size(1770, 1070);
+
+    Future<void> showPicker(WidgetTester tester, Size size) async {
+      addTearDown(tester.view.reset);
+      tester.view.physicalSize = size;
+      await prepareDatePicker(tester, (Future<DateTime?> date) async {
+        await tester.tap(find.byIcon(Icons.edit_outlined));
+        await tester.pumpAndSettle();
+      },
+      useMaterial3: true);
+    }
+
+    testWidgets('portrait', (WidgetTester tester) async {
+      await showPicker(tester, kCommonScreenSizePortrait);
+      expect(tester.widget<Text>(find.text('Fri, Jan 15')).style?.fontSize, 32);
+    });
+
+    testWidgets('landscape', (WidgetTester tester) async {
+      await showPicker(tester, kCommonScreenSizeLandscape);
+      expect(tester.widget<Text>(find.text('Fri, Jan 15')).style?.fontSize, 24);
+    });
+  });
 }
 
 class _RestorableDatePickerDialogTestWidget extends StatefulWidget {
