20e9dc54da0e48eb50f07b01598e07d6b080f646
Jenn Magder <magder@google.com>
2023-03-30 13:26:43 -0700

    Setup channels during IntegrationTest registration on iOS (#123729)
    Setup channels during IntegrationTest registration on iOS

--- packages/integration_test/ios/Classes/IntegrationTestPlugin.h
+++ packages/integration_test/ios/Classes/IntegrationTestPlugin.h
@@ -22,9 +22,7 @@ NS_ASSUME_NONNULL_BEGIN
 @property (copy, readonly) NSDictionary<NSString *, UIImage *> *capturedScreenshotsByName;
 
 /** Fetches the singleton instance of the plugin. */
-+ (IntegrationTestPlugin *)instance;
-
-- (void)setupChannels:(id<FlutterBinaryMessenger>)binaryMessenger;
++ (instancetype)instance;
 
 - (instancetype)init NS_UNAVAILABLE;
 
