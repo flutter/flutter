a28aae918eaefb266482c1a3b33955d2f26ed119
Greg Price <greg@zulip.com>
2023-03-30 11:28:51 -0700

    Remove no-shuffle from framework tests, part 1: easy cases (#123751)
    Remove no-shuffle from framework tests, part 1: easy cases

--- packages/flutter/test/cupertino/date_picker_test.dart
+++ packages/flutter/test/cupertino/date_picker_test.dart
@@ -2,15 +2,10 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// no-shuffle:
-//   //TODO(gspencergoog): Remove this tag once this test's state leaks/test
-//   dependencies have been fixed.
-//   https://github.com/flutter/flutter/issues/85160
-//   Fails with "flutter test --test-randomize-ordering-seed=456"
 // reduced-test-set:
 //   This file is run as part of a reduced test set in CI on Mac and Windows
 //   machines.
-@Tags(<String>['reduced-test-set', 'no-shuffle'])
+@Tags(<String>['reduced-test-set'])
 library;
 
 import 'dart:ui';
