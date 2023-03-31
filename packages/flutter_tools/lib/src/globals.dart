d4869d8da12c124edc5023fbb1e95f7dc5303173
eliasyishak <42216813+eliasyishak@users.noreply.github.com>
2023-03-31 13:28:29 -0400

    Remove print statement

--- packages/flutter_tools/lib/src/globals.dart
+++ packages/flutter_tools/lib/src/globals.dart
@@ -87,7 +87,14 @@ Analytics getDefaultAnalytics() {
   
   // Ensure that the consent message has been displayed
   if (defaultAnalytics.shouldShowMessage) {
-    print(defaultAnalytics.getConsentMessage);
+    logger.printStatus(defaultAnalytics.getConsentMessage);
+
+    // Invoking this will onboard the flutter tool onto
+    // the package on the developer's machine and will
+    // allow for events to be sent to Google Analytics
+    // on subsequent runs of the flutter tool (ie. no events
+    // will be sent on the first run to allow developers to
+    // opt out of collection)
     defaultAnalytics.clientShowedMessage();
   }
   
