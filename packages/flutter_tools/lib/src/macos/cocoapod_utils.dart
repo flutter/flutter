70ecad5ae5a04e858421ec0b6678d5b285e7fa85
Jenn Magder <magder@google.com>
2023-04-03 12:58:03 -0700

    Add readlink -f flag to CocoaPods script to workaround Xcode 14.3 issue

--- packages/flutter_tools/lib/src/macos/cocoapod_utils.dart
+++ packages/flutter_tools/lib/src/macos/cocoapod_utils.dart
@@ -28,6 +28,7 @@ Future<void> processPodsIfNeeded(
     paths: <String>[
       xcodeProject.xcodeProjectInfoFile.path,
       xcodeProject.podfile.path,
+      xcodeProject.podRunnerFrameworksScript.path, // for CocoaPodsScriptReadlink migration.
       globals.fs.path.join(
         Cache.flutterRoot!,
         'packages',
