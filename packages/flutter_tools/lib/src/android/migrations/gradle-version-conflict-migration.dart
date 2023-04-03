ccbecbdfb4bad0b501655ab2f3de1d79b2077b32
Gray Mackall <mackall@google.com>
2023-04-03 14:13:01 -0700

    Migrator for ugprading gradle when we detect a conflict with the AS version, tests still todo

---
+++ packages/flutter_tools/lib/src/android/migrations/gradle-version-conflict-migration.dart
@@ -0,0 +1,66 @@
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import '../../base/file_system.dart';
+import '../../base/project_migrator.dart';
+import '../../base/version.dart';
+import '../../globals.dart' as globals;
+import '../../project.dart';
+
+final Version _androidStudioFlamingo = Version(2022, 2, 1);
+final RegExp _gradleVersionMatch = RegExp(
+  r'distributionUrl=https\\://services.gradle.org/distributions/gradle-(\d|\.)+-(?:all|bin).zip\n');
+final Version _lowestSupportedGradleVersion = Version(7, 3, 0);
+const String _newVersionFullDependency = r'distributionUrl=https\://services.gradle.org/distributions/gradle-7.4-all.zip';
+
+/// Migrate to a newer version of gradle when the existing does not support
+/// the version of Java provided by the detected Android Studio version.
+///
+/// For more info see the Gradle-Java compatibility matrix:
+/// https://docs.gradle.org/current/userguide/compatibility.html
+class GradleVersionConflictMigration extends ProjectMigrator {
+  GradleVersionConflictMigration(
+      AndroidProject project,
+      super.logger,
+      ) : _gradleWrapperProperties = project.hostAppGradleRoot
+      .childDirectory('gradle').childDirectory('wrapper').childFile('gradle-wrapper.properties');
+  
+  final File _gradleWrapperProperties;
+
+  @override
+  void migrate() {
+    if (!_gradleWrapperProperties.existsSync()) {
+      logger.printTrace('gradle-wrapper.properties not found, skipping gradle version compatibility check.');
+      return;
+    }
+
+    if (globals.androidStudio == null) {
+      logger.printTrace('Android Studio version could not be detected, '
+          'skipping gradle version compatibility check.');
+      return;
+    } else if (globals.androidStudio!.version.compareTo(_androidStudioFlamingo) < 0) {
+      //Version of Android Studio is less than impacted version, no migration necessary.
+      return;
+    }
+
+    processFileLines(_gradleWrapperProperties);
+  }
+
+  @override
+  String migrateLine(String line) {
+    final RegExpMatch? match = _gradleVersionMatch.firstMatch(line);
+    if (match == null) {
+      return line;
+    }
+    final String existingVersionString = match[1]!; //TODO: make sure this is safe
+    final Version existingVersion = Version.parse(existingVersionString)!;
+    if (existingVersion.compareTo(_lowestSupportedGradleVersion) < 0) {
+      logger.printTrace('Conflict detected between versions of Android Studio '
+          'and gradle, upgrading gradle version from $existingVersion to 7.4');
+      return _newVersionFullDependency;
+    } else {
+      return line;
+    }
+  }
+}
