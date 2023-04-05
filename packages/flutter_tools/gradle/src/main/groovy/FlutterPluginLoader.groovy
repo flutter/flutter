9179784c291230a29593c2712ab7e4ddff7db1f6
Bartek Pacia <barpac02@gmail.com>
2023-04-05 21:03:11 +0200

    Revert "venture into creating FlutterSettingsPlugin (for loading Flutter plugins)"
    This reverts commit 97289df079046a5c738e0e3a4c175ab4f96ce575.

--- packages/flutter_tools/gradle/src/main/groovy/FlutterPluginLoader.groovy
+++
@@ -1,9 +0,0 @@
-import org.gradle.api.Plugin
-import org.gradle.api.initialization.Settings
-
-class FlutterSettingsPlugin implements Plugin<Settings> {
-    @Override
-    void apply(Settings settings) {
-        println("Hello from the FlutterSettingsPlugin")
-    }
-}
