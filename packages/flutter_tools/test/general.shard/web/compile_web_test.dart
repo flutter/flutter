7ad1bd947ef151f70f1c1b92d06eed1842827a7b
Kevin Moore <kevmoo@users.noreply.github.com>
2023-04-06 13:49:26 -0700

    flutter tool: log dart2wasm vs dart2js as appropriate during build (#124331)
    flutter tool: log dart2wasm vs dart2js as appropriate during build

--- packages/flutter_tools/test/general.shard/web/compile_web_test.dart
+++ packages/flutter_tools/test/general.shard/web/compile_web_test.dart
@@ -78,7 +78,7 @@ void main() {
     // Sends timing event.
     final TestTimingEvent timingEvent = testUsage.timings.single;
     expect(timingEvent.category, 'build');
-    expect(timingEvent.variableName, 'dart2js');
+    expect(timingEvent.variableName, 'dart2wasm');
   });
 
   testUsingContext('WebBuilder throws tool exit on failure', () async {
