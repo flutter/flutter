c381c2a08fcffd0412a98b43261c16b6ad77aea2
Gayathri Devi Srinivasan <47923940+GayathrideviGA@users.noreply.github.com>
2023-04-13 16:03:08 +0530

    Added Test Cases for [Material 3] Hide check mark in ChoiceChip #124779

--- examples/api/test/material/choice_chip/choice_chip.0_test.dart
+++ examples/api/test/material/choice_chip/choice_chip.0_test.dart
@@ -3,11 +3,13 @@
 // found in the LICENSE file.
 
 import 'package:flutter/material.dart';
-import 'package:flutter_api_samples/material/choice_chip/choice_chip.0.dart' as example;
+import 'package:flutter_api_samples/material/choice_chip/choice_chip.0.dart'
+    as example;
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  testWidgets('Can choose an item using ChoiceChip', (WidgetTester tester) async {
+  testWidgets('Can choose an item using ChoiceChip',
+      (WidgetTester tester) async {
     await tester.pumpWidget(
       const example.ChipApp(),
     );
@@ -20,5 +22,55 @@ void main() {
 
     chosenChip = tester.widget(find.byType(ChoiceChip).at(0));
     expect(chosenChip.selected, true);
+  });
+
+  testWidgets('should show checkmark and set correct color',
+      (WidgetTester tester) async {
+    final checkmarkColor = Colors.green;
+
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Scaffold(
+          body: Center(
+            child: ChoiceChip(
+              label: const Text('Choice Chip'),
+              selected: true,
+              showCheckmark: true,
+              checkmarkColor: checkmarkColor,
+              onSelected: (value) {},
+            ),
+          ),
+        ),
+      ),
+    );
+
+    final checkmarkFinder = find.byIcon(Icons.check);
+    final checkmarkIcon = checkmarkFinder.evaluate().first.widget as Icon;
+    final checkmarkOpacity = (checkmarkIcon.color?.opacity ?? 0).toDouble();
+
+    expect(checkmarkFinder, findsOneWidget);
+    expect(checkmarkOpacity, equals(1.0));
+    expect(checkmarkIcon.color, equals(checkmarkColor));
+  });
+
+  testWidgets('should hide checkmark', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Scaffold(
+          body: Center(
+            child: ChoiceChip(
+              label: const Text('Choice Chip'),
+              selected: true,
+              showCheckmark: false,
+              onSelected: (value) {},
+            ),
+          ),
+        ),
+      ),
+    );
+
+    final checkmarkFinder = find.byIcon(Icons.check);
+
+    expect(checkmarkFinder, findsNothing);
   });
 }
