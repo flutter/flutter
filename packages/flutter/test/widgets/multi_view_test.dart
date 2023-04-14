06151ad8032455826f27777c2dc9e47c49895f31
Michael Goderbauer <goderbauer@google.com>
2023-04-14 15:20:12 -0700

    fix most of rendering

--- packages/flutter/test/widgets/multi_view_test.dart
+++ packages/flutter/test/widgets/multi_view_test.dart
@@ -2,9 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'dart:math' as math;
-
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
