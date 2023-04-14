d1d577c2ae2dd838cadf5e8ab2757e15c79a7575
Yegor <yjbanov@google.com>
2023-04-13 19:57:19 -0700

    [web:canvaskit] move shaders to UniqueRef; fix minor memory leaks (flutter/engine#41172)
    [web:canvaskit] move shaders to UniqueRef; fix minor memory leaks

--- engine/src/flutter/lib/web_ui/test/canvaskit/shader_test.dart
+++ engine/src/flutter/lib/web_ui/test/canvaskit/shader_test.dart
@@ -25,7 +25,7 @@ void testMain() {
         ui.Offset.zero,
         testColors,
       ) as CkGradientSweep;
-      expect(gradient.createDefault(), isNotNull);
+      expect(gradient.getSkShader(ui.FilterQuality.none), isNotNull);
     });
 
     test('Linear gradient', () {
@@ -34,7 +34,7 @@ void testMain() {
         const ui.Offset(0, 1),
         testColors,
       ) as CkGradientLinear;
-      expect(gradient.createDefault(), isNotNull);
+      expect(gradient.getSkShader(ui.FilterQuality.none), isNotNull);
     });
 
     test('Radial gradient', () {
@@ -43,7 +43,7 @@ void testMain() {
         10,
         testColors,
       ) as CkGradientRadial;
-      expect(gradient.createDefault(), isNotNull);
+      expect(gradient.getSkShader(ui.FilterQuality.none), isNotNull);
     });
 
     test('Conical gradient', () {
@@ -57,10 +57,10 @@ void testMain() {
         const ui.Offset(10, 10),
         40,
       ) as CkGradientConical;
-      expect(gradient.createDefault(), isNotNull);
+      expect(gradient.getSkShader(ui.FilterQuality.none), isNotNull);
     });
 
-    test('Image shader', () {
+    test('Image shader initialize/dispose cycle', () {
       final SkImage skImage = canvasKit.MakeAnimatedImageFromEncoded(kTransparentImage)!.makeImageAtCurrentFrame();
       final CkImage image = CkImage(skImage);
       final CkImageShader imageShader = ui.ImageShader(
@@ -71,9 +71,61 @@ void testMain() {
       ) as CkImageShader;
       expect(imageShader, isA<CkImageShader>());
 
+      final UniqueRef<SkShader> ref = imageShader.ref!;
       expect(imageShader.debugDisposed, false);
+      expect(imageShader.getSkShader(ui.FilterQuality.none), same(ref.nativeObject));
+      expect(ref.isDisposed, false);
+      expect(image.debugDisposed, false);
       imageShader.dispose();
       expect(imageShader.debugDisposed, true);
+      expect(ref.isDisposed, true);
+      expect(imageShader.ref, isNull);
+      expect(image.debugDisposed, true);
+    });
+
+    test('Image shader withQuality', () {
+      final SkImage skImage = canvasKit.MakeAnimatedImageFromEncoded(kTransparentImage)!.makeImageAtCurrentFrame();
+      final CkImage image = CkImage(skImage);
+      final CkImageShader imageShader = ui.ImageShader(
+        image,
+        ui.TileMode.clamp,
+        ui.TileMode.repeated,
+        Float64List.fromList(Matrix4.diagonal3Values(1, 2, 3).storage),
+      ) as CkImageShader;
+      expect(imageShader, isA<CkImageShader>());
+
+      final UniqueRef<SkShader> ref1 = imageShader.ref!;
+      expect(imageShader.getSkShader(ui.FilterQuality.none), same(ref1.nativeObject));
+
+      // Request the same quality as the default quality (none).
+      expect(imageShader.getSkShader(ui.FilterQuality.none), isNotNull);
+      final UniqueRef<SkShader> ref2 = imageShader.ref!;
+      expect(ref1, same(ref2));
+      expect(ref1.isDisposed, false);
+      expect(image.debugDisposed, false);
+
+      // Change quality to medium.
+      expect(imageShader.getSkShader(ui.FilterQuality.medium), isNotNull);
+      final UniqueRef<SkShader> ref3 = imageShader.ref!;
+      expect(ref1, isNot(same(ref3)));
+      expect(ref1.isDisposed, true, reason: 'The previous reference must be released to avoid a memory leak');
+      expect(image.debugDisposed, false);
+      expect(imageShader.ref!.nativeObject, same(ref3.nativeObject));
+
+      // Ask for medium again.
+      expect(imageShader.getSkShader(ui.FilterQuality.medium), isNotNull);
+      final UniqueRef<SkShader> ref4 = imageShader.ref!;
+      expect(ref4, same(ref3));
+      expect(ref3.isDisposed, false);
+      expect(image.debugDisposed, false);
+      expect(imageShader.ref!.nativeObject, same(ref4.nativeObject));
+
+      // Done with the shader.
+      imageShader.dispose();
+      expect(imageShader.debugDisposed, true);
+      expect(ref4.isDisposed, true);
+      expect(imageShader.ref, isNull);
+      expect(image.debugDisposed, true);
     });
   });
 }
