74af9ecc0f0bd21bd67a000f441ae27e80f296d1
Justin McCandless <jmccandless@google.com>
2023-04-14 10:52:33 -0700

    Update test to consider that buildButtonItems now needs a real context for localizations

--- packages/flutter/test/cupertino/spell_check_suggestions_toolbar_test.dart
+++ packages/flutter/test/cupertino/spell_check_suggestions_toolbar_test.dart
@@ -10,8 +10,14 @@ import 'package:flutter_test/flutter_test.dart';
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
 
-  test('buildButtonItems builds a "No Replacements Found" button when no suggestions', () {
-    final _FakeEditableTextState editableTextState = _FakeEditableTextState();
+  testWidgets('buildButtonItems builds a "No Replacements Found" button when no suggestions', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      CupertinoApp(
+        home: _FakeEditableText(),
+      ),
+    );
+    final _FakeEditableTextState editableTextState =
+        tester.state(find.byType(_FakeEditableText));
     final List<ContextMenuButtonItem>? buttonItems =
         CupertinoSpellCheckSuggestionsToolbar.buildButtonItems(editableTextState);
 
@@ -19,6 +25,19 @@ void main() {
     expect(buttonItems!.length, 1);
     expect(buttonItems.first.label, 'No Replacements Found');
   });
+}
+
+class _FakeEditableText extends EditableText {
+  _FakeEditableText() : super(
+    controller: TextEditingController(),
+    focusNode: FocusNode(),
+    backgroundCursorColor: CupertinoColors.white,
+    cursorColor: CupertinoColors.white,
+    style: const TextStyle(),
+  );
+
+  @override
+  _FakeEditableTextState createState() => _FakeEditableTextState();
 }
 
 class _FakeEditableTextState extends EditableTextState {
