45ff27bde1f261048813f84ae86212e9dc06f028
gaaclarke <30870216+gaaclarke@users.noreply.github.com>
2023-04-17 10:49:05 -0700

    Adds AndroidSurfaceGLImpeller unit tests (flutter/engine#40979)
    followup to https://github.com/flutter/engine/pull/40944
    issue https://github.com/flutter/flutter/issues/105323
    
    [C++, Objective-C, Java style guides]: https://github.com/flutter/engine/blob/main/CONTRIBUTING.md#style

--- engine/src/flutter/shell/platform/android/platform_view_android.cc
+++ engine/src/flutter/shell/platform/android/platform_view_android.cc
@@ -54,8 +54,9 @@ std::unique_ptr<AndroidSurface> AndroidSurfaceFactoryImpl::CreateSurface() {
             android_context_, jni_facade_, enable_vulkan_validation_);
 
 #else
-        return std::make_unique<AndroidSurfaceGLImpeller>(android_context_,
-                                                          jni_facade_);
+        return std::make_unique<AndroidSurfaceGLImpeller>(
+            android_context_, jni_facade_,
+            std::make_unique<impeller::egl::Display>());
 #endif
       } else {
         return std::make_unique<AndroidSurfaceGLSkia>(android_context_,
