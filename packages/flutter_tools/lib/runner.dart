ff21774d73b24ea7b57bc7ac227507c846bdb296
godofredoc <godofredoc@google.com>
2023-04-19 09:41:23 -0700

    Revert "Update helper message for `--suppress-analytics` (#124810)"
    This reverts commit 3476b966524ff313baf3e31d1818cde33d6fa37f.

--- packages/flutter_tools/lib/runner.dart
+++ packages/flutter_tools/lib/runner.dart
@@ -88,6 +88,14 @@ Future<int> run(
           await globals.analytics.setTelemetry(value);
         }
 
+        // If the user has opted out of legacy analytics, we will continue
+        // to opt them out of unified analytics and inform them
+        if (!globals.flutterUsage.enabled && globals.analytics.telemetryEnabled) {
+          await globals.analytics.setTelemetry(false);
+          globals.logger.printStatus(
+              'Please note that analytics reporting was already disabled, and will continue to be disabled.\n');
+        }
+
         await runner.run(args);
 
         // Triggering [runZoned]'s error callback does not necessarily mean that
@@ -277,11 +285,6 @@ Future<int> _exit(int code, {required ShutdownHooks shutdownHooks}) async {
   // Ensure that the consent message has been displayed for unified analytics
   if (globals.analytics.shouldShowMessage) {
     globals.logger.printStatus(globals.analytics.getConsentMessage);
-    if (!globals.flutterUsage.enabled) {
-      globals.printStatus(
-          'Please note that analytics reporting was already disabled, '
-          'and will continue to be disabled.\n');
-    }
 
     // Because the legacy analytics may have also sent a message,
     // the conditional below will print additional messaging informing
@@ -293,7 +296,8 @@ Future<int> _exit(int code, {required ShutdownHooks shutdownHooks}) async {
               'the flutter tool is migrating to a new analytics system. '
               'Disabling analytics collection will disable both the legacy '
               'and new analytics collection systems. '
-              'You can disable analytics reporting by running `flutter --disable-telemetry`\n');
+              'You can disable analytics reporting by running either `flutter --disable-telemetry` '
+              'or `flutter config --no-analytics\n');
     }
 
     // Invoking this will onboard the flutter tool onto
