41003fb57b5c4418265412f78a322501d0ba05ab
Kate Lovett <katelovett@google.com>
2023-04-19 17:10:59 -0500

    analyzer

--- packages/flutter/lib/src/widgets/scroll_controller.dart
+++ packages/flutter/lib/src/widgets/scroll_controller.dart
@@ -3,7 +3,6 @@
 // found in the LICENSE file.
 
 import 'package:flutter/animation.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 
 import 'scroll_context.dart';
