0a8fe3538d1f0e94e48268fc26a74085ab46c185
Jason Simmons <jason-simmons@users.noreply.github.com>
2023-04-20 08:40:22 -0700

    [Impeller] Change the default color format for the GLES backend to RGBA (flutter/engine#41342)
    This provides compatibility with BlitCopyTextureToBufferCommandGLES::Encode and glReadPixels (which only guarantees support for RGBA).

--- engine/src/flutter/impeller/renderer/backend/gles/context_gles.cc
+++ engine/src/flutter/impeller/renderer/backend/gles/context_gles.cc
@@ -69,7 +69,7 @@ ContextGLES::ContextGLES(std::unique_ptr<ProcTableGLES> gl,
             .SetSupportsTextureToTextureBlits(
                 reactor_->GetProcTable().BlitFramebuffer.IsAvailable())
             .SetSupportsFramebufferFetch(false)
-            .SetDefaultColorFormat(PixelFormat::kB8G8R8A8UNormInt)
+            .SetDefaultColorFormat(PixelFormat::kR8G8B8A8UNormInt)
             .SetDefaultStencilFormat(PixelFormat::kS8UInt)
             .SetSupportsCompute(false, false)
             .SetSupportsReadFromResolve(false)
