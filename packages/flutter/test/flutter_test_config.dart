2a47166592a5bd024a2bf1b4e42bde116fc3d863
Polina Cherkasova <polina.c@live.com>
2023-04-20 13:37:29 -0700

    Update flutter_test_config.dart

--- packages/flutter/test/flutter_test_config.dart
+++ packages/flutter/test/flutter_test_config.dart
@@ -9,6 +9,7 @@ import 'package:flutter_test/flutter_test.dart';
 
 import '_goldens_io.dart'
   if (dart.library.html) '_goldens_web.dart' as flutter_goldens;
+import 'foundation/leak_tracking.dart';
 
 Future<void> testExecutable(FutureOr<void> Function() testMain) {
   // Enable checks because there are many implementations of [RenderBox] in this
@@ -18,6 +19,9 @@ Future<void> testExecutable(FutureOr<void> Function() testMain) {
   // Make tap() et al fail if the given finder specifies a widget that would not
   // receive the event.
   WidgetController.hitTestWarningShouldBeFatal = true;
+
+  // Disable warning.
+  LeakTrackingTestConfig.warnForNonSupportedPlatforms = false;
 
   // Enable golden file testing using Skia Gold.
   return flutter_goldens.testExecutable(testMain);
