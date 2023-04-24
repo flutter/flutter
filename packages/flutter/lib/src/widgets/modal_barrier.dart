318eb751bd7e2e26ac070174416d46b395b2cf8f
fzyzcjy <5236035+fzyzcjy@users.noreply.github.com>
2023-04-24 18:08:11 +0800

    Update packages/flutter/lib/src/widgets/modal_barrier.dart
    Co-authored-by: Michael Goderbauer <goderbauer@google.com>

--- packages/flutter/lib/src/widgets/modal_barrier.dart
+++ packages/flutter/lib/src/widgets/modal_barrier.dart
@@ -394,7 +394,9 @@ class _AnyTapGestureRecognizer extends BaseTapGestureRecognizer {
   @protected
   @override
   void handleTapUp({PointerDownEvent? down, PointerUpEvent? up}) {
-    invokeCallback('onTapUp', () => onAnyTapUp?.call());
+    if (onAnyTapUp != null) {
+      invokeCallback('onAnyTapUp', onAnyTapUp!);
+    }
   }
 
   @protected
