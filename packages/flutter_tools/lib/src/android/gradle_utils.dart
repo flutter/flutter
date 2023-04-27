0e3fde4dcf518dfd4b5c2c8dba998a2122168c0e
Dan Field <dnfield@google.com>
2023-04-27 10:26:28 -0700

    Bump the default minSdkVersion to 19 (#125515)
    See https://docs.flutter.dev/reference/supported-platforms
    
    I don't expect this to break anything, but if it does we can revert and figure out what else needs to happen first.
    
    Without this change, engine changes upstream will get flagged in default flutter created apps.

--- packages/flutter_tools/lib/src/android/gradle_utils.dart
+++ packages/flutter_tools/lib/src/android/gradle_utils.dart
@@ -39,7 +39,7 @@ const String templateKotlinGradlePluginVersion = '1.7.10';
 // `.android` is always regenerated after flutter pub get, so new versions are picked up after a
 // Flutter upgrade.
 const String compileSdkVersion = '33';
-const String minSdkVersion = '16';
+const String minSdkVersion = '19';
 const String targetSdkVersion = '33';
 const String ndkVersion = '23.1.7779620';
 
