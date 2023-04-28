eb68df6c89f9fc1e9eecfde6e14b7a552138955e
Greg Spencer <gspencergoog@users.noreply.github.com>
2023-04-28 10:43:36 -0700

    Determine lifecycle by looking at window focus also (#41094)
    ## Description
    
    This incorporates additional signal from `Activity.onWindowFocusChanged` to help decide if the application is `resumed` or `inactive`.
    
    When the user pulls down the notification shade or opens the app switcher in iOS, then iOS sends a notification to the application that it no longer has input focus (is no longer "active" in Apple terminology). 
    
    However, Android (at least on a Pixel) doesn't send `onPause` and `onResume` events for these things, as one might expect. Instead, this PR changes things so that we listen to `Activity.onWindowFocusChanged` and see if any of the windows still have focus.
    
    If it doesn't have focus, then the lifecycle switches to `inactive` (even if `onPause` hasn't been called), and if it does have focus (and `onResume` hasn't been called) then we should go to `resumed`.
    
    State changes are determined and deduped in the `LifecycleChannel` class.
    
    Here's the old state table:
    
    | Android State | Flutter state |
    | ------------- | ------------- |
    | Resumed  | resumed |
    | Paused  | inactive |
    | Stopped | paused |
    | Detached | detached |
    
    Here's the new state table:
    
    | Android State | Window focused | Flutter state |
    | ------------- | ------------- | ------------- |
    | Resumed  | true  | resumed |
    | Resumed  | false  | _inactive_ * |
    | Paused  | true  | inactive |
    | Paused  | false  | inactive |
    | Stopped  | true  | paused |
    | Stopped | false  | paused |
    | Detached | true  | detached |
    | Detached | false  | detached |
    
    * = This is the relevant change in this PR.
    
    ("Window focused" means one or more windows managed by Flutter are focused)
    
    The `inactive` state is for when the application is running and visible, but doesn't have the input focus.  An example where this currently happens are when a phone call is in progress on top of the app, or on some OEMs when going into the app switcher (I've tested on Realme and it does that, at least).  With the PR, it will also go into `inactive` when the app has lost input focus, but is still in the Android `onResume` state. This means that on phones that don't pause the app when they go into the app switcher or the notification window shade (Pixel, others), the app will go into `inactive` when it didn't before. If developers weren't doing anything special in the `inactive` state before, then this PR will have no change for them. If they were, they will go into that state more often (but more consistently across OEMs).
    
    ## Related Issues
    
      - Fixes  https://github.com/flutter/flutter/issues/124591
    
    ## Tests
    
      - Added unit tests for handling `onWindowFocusChanged`.

--- shell/platform/android/io/flutter/embedding/engine/systemchannels/LifecycleChannel.java
+++ shell/platform/android/io/flutter/embedding/engine/systemchannels/LifecycleChannel.java
@@ -5,39 +5,106 @@
 package io.flutter.embedding.engine.systemchannels;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.VisibleForTesting;
 import io.flutter.Log;
 import io.flutter.embedding.engine.dart.DartExecutor;
 import io.flutter.plugin.common.BasicMessageChannel;
 import io.flutter.plugin.common.StringCodec;
 
-/** TODO(mattcarroll): fill in javadoc for LifecycleChannel. */
+/**
+ * A {@link BasicMessageChannel} that communicates lifecycle events to the framework.
+ *
+ * <p>The activity listens to the Android lifecycle events, in addition to the focus events for
+ * windows, and this channel combines that information to decide if the application is the inactive,
+ * resumed, paused, or detached state.
+ */
 public class LifecycleChannel {
   private static final String TAG = "LifecycleChannel";
+  private static final String CHANNEL_NAME = "flutter/lifecycle";
 
-  @NonNull public final BasicMessageChannel<String> channel;
+  // These should stay in sync with the AppLifecycleState enum in the framework.
+  private static final String RESUMED = "AppLifecycleState.resumed";
+  private static final String INACTIVE = "AppLifecycleState.inactive";
+  private static final String PAUSED = "AppLifecycleState.paused";
+  private static final String DETACHED = "AppLifecycleState.detached";
+
+  private String lastAndroidState = "";
+  private String lastFlutterState = "";
+  private boolean lastFocus = false;
+
+  @NonNull private final BasicMessageChannel<String> channel;
 
   public LifecycleChannel(@NonNull DartExecutor dartExecutor) {
-    this.channel =
-        new BasicMessageChannel<>(dartExecutor, "flutter/lifecycle", StringCodec.INSTANCE);
+    this(new BasicMessageChannel<String>(dartExecutor, CHANNEL_NAME, StringCodec.INSTANCE));
   }
 
-  public void appIsInactive() {
-    Log.v(TAG, "Sending AppLifecycleState.inactive message.");
-    channel.send("AppLifecycleState.inactive");
+  @VisibleForTesting
+  public LifecycleChannel(@NonNull BasicMessageChannel<String> channel) {
+    this.channel = channel;
+  }
+
+  // Here's the state table this implements:
+  //
+  // | Android State | Window focused | Flutter state |
+  // |---------------|----------------|---------------|
+  // | Resumed       |     true       |    resumed    |
+  // | Resumed       |     false      |    inactive   |
+  // | Paused        |     true       |    inactive   |
+  // | Paused        |     false      |    inactive   |
+  // | Stopped       |     true       |    paused     |
+  // | Stopped       |     false      |    paused     |
+  // | Detached      |     true       |    detached   |
+  // | Detached      |     false      |    detached   |
+  private void sendState(String state, boolean hasFocus) {
+    if (lastAndroidState == state && hasFocus == lastFocus) {
+      // No inputs changed, so Flutter state could not have changed.
+      return;
+    }
+    String newState;
+    if (state == RESUMED) {
+      // Focus is only taken into account when the Android state is "Resumed".
+      // In all other states, focus is ignored, because we can't know what order
+      // Android lifecycle notifications and window focus notifications events
+      // will arrive in, and those states don't send input events anyhow.
+      newState = hasFocus ? RESUMED : INACTIVE;
+    } else {
+      newState = state;
+    }
+    // Keep the last reported values for future updates.
+    lastAndroidState = state;
+    lastFocus = hasFocus;
+    if (newState == lastFlutterState) {
+      // No change in the resulting Flutter state, so don't report anything.
+      return;
+    }
+    Log.v(TAG, "Sending " + newState + " message.");
+    channel.send(newState);
+    lastFlutterState = newState;
+  }
+
+  // Called if at least one window in the app has focus.
+  public void aWindowIsFocused() {
+    sendState(lastAndroidState, true);
+  }
+
+  // Called if no windows in the app have focus.
+  public void noWindowsAreFocused() {
+    sendState(lastAndroidState, false);
   }
 
   public void appIsResumed() {
-    Log.v(TAG, "Sending AppLifecycleState.resumed message.");
-    channel.send("AppLifecycleState.resumed");
+    sendState(RESUMED, lastFocus);
+  }
+
+  public void appIsInactive() {
+    sendState(INACTIVE, lastFocus);
   }
 
   public void appIsPaused() {
-    Log.v(TAG, "Sending AppLifecycleState.paused message.");
-    channel.send("AppLifecycleState.paused");
+    sendState(PAUSED, lastFocus);
   }
 
   public void appIsDetached() {
-    Log.v(TAG, "Sending AppLifecycleState.detached message.");
-    channel.send("AppLifecycleState.detached");
+    sendState(DETACHED, lastFocus);
   }
 }
