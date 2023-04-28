a433f88d168195486f5dc13dde7e843fd1ee2544
Qun Cheng <36861262+QuncCccccc@users.noreply.github.com>
2023-04-28 12:05:31 -0700

    `Checkbox.fillColor` should be applied to checkbox's background color when it is unchecked. (#125643)

--- dev/tools/gen_defaults/lib/checkbox_template.dart
+++ dev/tools/gen_defaults/lib/checkbox_template.dart
@@ -20,27 +20,49 @@ class _${blockName}DefaultsM3 extends CheckboxThemeData {
   final ColorScheme _colors;
 
   @override
+  MaterialStateBorderSide? get side {
+    return MaterialStateBorderSide.resolveWith((Set<MaterialState> states) {
+      if (states.contains(MaterialState.disabled)) {
+        if (states.contains(MaterialState.selected)) {
+          return const BorderSide(width: ${tokens['md.comp.checkbox.unselected.disabled.outline.width']}, color: Colors.transparent);
+        }
+        return BorderSide(width: ${tokens['md.comp.checkbox.unselected.disabled.outline.width']}, color: ${componentColor('md.comp.checkbox.unselected.disabled.outline')}.withOpacity(${tokens['md.comp.checkbox.unselected.disabled.container.opacity']}));
+      }
+      if (states.contains(MaterialState.selected)) {
+        return const BorderSide(width: ${tokens['md.comp.checkbox.selected.outline.width']}, color: Colors.transparent);
+      }
+      if (states.contains(MaterialState.error)) {
+        return BorderSide(width: ${tokens['md.comp.checkbox.unselected.disabled.outline.width']}, color: ${componentColor('md.comp.checkbox.unselected.error.outline')});
+      }
+      if (states.contains(MaterialState.pressed)) {
+        return BorderSide(width: ${tokens['md.comp.checkbox.unselected.pressed.outline.width']}, color: ${componentColor('md.comp.checkbox.unselected.pressed.outline')});
+      }
+      if (states.contains(MaterialState.hovered)) {
+        return BorderSide(width: ${tokens['md.comp.checkbox.unselected.hover.outline.width']}, color: ${componentColor('md.comp.checkbox.unselected.hover.outline')});
+      }
+      if (states.contains(MaterialState.focused)) {
+        return BorderSide(width: ${tokens['md.comp.checkbox.unselected.focus.outline.width']}, color: ${componentColor('md.comp.checkbox.unselected.focus.outline')});
+      }
+      return BorderSide(width: ${tokens['md.comp.checkbox.unselected.outline.width']}, color: ${componentColor('md.comp.checkbox.unselected.outline')});
+    });
+  }
+
+  @override
   MaterialStateProperty<Color> get fillColor {
     return MaterialStateProperty.resolveWith((Set<MaterialState> states) {
       if (states.contains(MaterialState.disabled)) {
-        return ${componentColor('md.comp.checkbox.selected.disabled.container')};
-      }
-      if (states.contains(MaterialState.error)) {
-        return ${componentColor('md.comp.checkbox.unselected.error.outline')};
+        if (states.contains(MaterialState.selected)) {
+          return ${componentColor('md.comp.checkbox.selected.disabled.container')};
+        }
+        return Colors.transparent;
       }
       if (states.contains(MaterialState.selected)) {
+        if (states.contains(MaterialState.error)) {
+          return ${componentColor('md.comp.checkbox.selected.error.container')};
+        }
         return ${componentColor('md.comp.checkbox.selected.container')};
       }
-      if (states.contains(MaterialState.pressed)) {
-        return ${componentColor('md.comp.checkbox.unselected.pressed.outline')};
-      }
-      if (states.contains(MaterialState.hovered)) {
-        return ${componentColor('md.comp.checkbox.unselected.hover.outline')};
-      }
-      if (states.contains(MaterialState.focused)) {
-        return ${componentColor('md.comp.checkbox.unselected.focus.outline')};
-      }
-      return ${componentColor('md.comp.checkbox.unselected.outline')};
+      return Colors.transparent;
     });
   }
 
