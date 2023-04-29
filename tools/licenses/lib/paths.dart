8167f909bc8ddc2fc2a274de991d02c9ce1141f8
Jason Simmons <jason-simmons@users.noreply.github.com>
2023-04-29 10:33:06 -0700

    Exclude Skia's Vello directory from the license crawl (#41617)

--- tools/licenses/lib/paths.dart
+++ tools/licenses/lib/paths.dart
@@ -178,6 +178,7 @@ final Set<String> skippedPaths = <String>{
   r'third_party/skia/third_party/libjpeg-turbo', // we use our own version
   r'third_party/skia/third_party/libpng', // we use our own version
   r'third_party/skia/third_party/lua', // not linked in
+  r'third_party/skia/third_party/vello', // not linked in
   r'third_party/skia/tools', // contains nothing that ends up in the binary executable
   r'third_party/swiftshader', // only used on hosts for tests
   r'third_party/tinygltf',
