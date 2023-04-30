2a12a5b4c644bd5ebf4379a53ca0c2f6b7f61a7b
Tim Maffett <timmaffett@gmail.com>
2023-04-30 10:43:38 -0700

    test only that custom iconTheme made it to rawChip via getIconTheme

--- packages/flutter/test/material/chip_theme_test.dart
+++ packages/flutter/test/material/chip_theme_test.dart
@@ -327,8 +327,7 @@ void main() {
     );
 
     expect(tester.getSize(find.byType(RawChip)), const Size(400, 250)); // icon + label + padding + labelPadding
-    expect(tester.widget<IconTheme>(find.widgetWithIcon(IconTheme, Icons.ac_unit).first).data.color, customIconTheme.color);
-    expect(getIconTheme(tester).color, customIconTheme.color, reason:'Chip Icon Theme does not reflect custom iconTheme color.');
+    expect(getIconTheme(tester), customIconTheme, reason:'Chip Icon Theme does not reflect custom iconTheme.');
   });
 
   testWidgets('Chip uses constructor parameters', (WidgetTester tester) async {
@@ -377,8 +376,7 @@ void main() {
     );
 
     expect(tester.getSize(find.byType(RawChip)), const Size(400, 250)); // icon + label + padding + labelPadding
-    expect(tester.widget<IconTheme>(find.widgetWithIcon(IconTheme, Icons.cabin).first).data.color, customIconTheme.color);
-    expect(getIconTheme(tester).color, customIconTheme.color, reason:'Chip Icon Theme does not reflect custom iconTheme color.');
+    expect(getIconTheme(tester), customIconTheme, reason:'Chip Icon Theme does not reflect custom iconTheme.');
   });
 
   testWidgets('ChipTheme.fromDefaults', (WidgetTester tester) async {
