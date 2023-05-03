14330f6ba79f5e4717ce5dda5a7793e8688e06e8
Justin McCandless <jmccandless@google.com>
2023-05-03 15:27:04 -0700

    Allow multiple independent range matchers and link builders, and base everything on this at the core

--- packages/flutter/lib/src/widgets/linked_text.dart
+++ packages/flutter/lib/src/widgets/linked_text.dart
@@ -1,5 +1,3 @@
-import 'package:flutter/foundation.dart';
-
 import 'basic.dart';
 import 'framework.dart';
 import 'gesture_detector.dart';
@@ -16,7 +14,8 @@ typedef UriStringCallback = void Function(String urlString);
 /// Typically used for styling a link in some inline text.
 typedef LinkBuilder = InlineSpan Function(
   String linkText,
-  UriStringCallback? onTap,
+  // TODO(justinmc): Rename to LinkCallback?
+  //UriStringCallback? onTap
 );
 
 // TODO(justinmc): Change name to something link-agnostic?
@@ -25,13 +24,18 @@ typedef LinkBuilder = InlineSpan Function(
 ///
 /// By default, any URLs in the text are made interactive.
 class LinkedText extends StatelessWidget {
+  /// Creates an instance of [LinkedText] from the given [text], highlighting
+  /// any URLs by default.
   const LinkedText({
     super.key,
     this.onTap,
     required this.text,
   });
 
+  /// Called whenever a link in the [text] is tapped.
   final UriStringCallback? onTap;
+
+  /// The text in which to highlight URLs and to display.
   final String text;
 
   @override
@@ -46,72 +50,80 @@ class LinkedText extends StatelessWidget {
   }
 }
 
-// TODO(justinmc): Make agnostic to URLs, just "links", which could be GitHub PRs, etc.
-/// A [TextSpan] that makes parts of the [text] interactive.
-class InlineLinkedText extends TextSpan {
-  // TODO(justinmc): Need to support multiple simultaneous types of links. For
-  // example, URLs are blue and do one thing, Twitter handles are red and do another.
-  /// Create an instance of [InlineLinkedText].
-  ///
-  /// If [ranges] is given, then makes the text indicated by that interactive.
-  ///
-  /// Otherwise, finds URLs in the text and makes them interactive.
-  InlineLinkedText({
-    super.style,
-    required String text,
-    Iterable<TextRange>? ranges,
-    UriStringCallback? onTap,
-    LinkBuilder? linkBuilder,
-  }) : super(
-         children: _spansFromRanges(
-           text: text,
-           linkBuilder: linkBuilder,
-           onTap: onTap,
-           ranges: ranges ?? _rangesFromText(
-             text: text,
-             regExp: _urlRegExp,
-           ),
-         ),
-       );
+/// Finds [TextRange]s in the given [String].
+typedef RangesFinder = Iterable<TextRange> Function(String text);
 
-  /// Create an instance of [InlineLinkedText] where the text matched by the
-  /// given [regExp] is made interactive.
+class _TextLinkerSingle {
+  const _TextLinkerSingle({
+    required this.textRange,
+    required this.linkBuilder,
+  });
+
+  final LinkBuilder linkBuilder;
+  final TextRange textRange;
+
+  InlineSpan getSpan(String text) {
+    return linkBuilder(
+      text.substring(textRange.start, textRange.end),
+    );
+  }
+
+  /// Returns a list of [InlineSpan]s representing all of the [text].
   ///
-  /// By default, [regExp] matches URLs.
-  ///
-  /// See also:
-  ///
-  ///  * [InlineLinkifier.new], which can be passed [TextRange]s directly.
-  InlineLinkedText.regExp({
-    super.style,
-    required String text,
-    UriStringCallback ? onTap,
-    LinkBuilder? linkBuilder,
-    RegExp? regExp,
-  }) : super(
-         children: _spansFromRanges(
-           text: text,
-           linkBuilder: linkBuilder,
-           onTap: onTap,
-           ranges: _rangesFromText(
-             text: text,
-             regExp: regExp ?? _urlRegExp,
-           ),
-         ),
-       );
+  /// Ranges matched by [textLinkerSingles] are built with their respective
+  /// [LinkBuilder], and other text is represented with a simple [TextSpan].
+  static List<InlineSpan> getSpansForMany(Iterable<_TextLinkerSingle> textLinkerSingles, String text) {
+    final List<InlineSpan> spans = <InlineSpan>[];
+    int index = 0;
+    for (final _TextLinkerSingle textLinkerSingle in textLinkerSingles) {
+      // Ignore overlapping ranges.
+      if (index > textLinkerSingle.textRange.start) {
+        continue;
+      }
+      if (index < textLinkerSingle.textRange.start) {
+        spans.add(TextSpan(
+          text: text.substring(index, textLinkerSingle.textRange.start),
+        ));
+      }
+      spans.add(textLinkerSingle.linkBuilder(
+        text.substring(
+          textLinkerSingle.textRange.start,
+          textLinkerSingle.textRange.end,
+        ),
+      ));
+
+      index = textLinkerSingle.textRange.end;
+    }
+    if (index < text.length) {
+      spans.add(TextSpan(
+        text: text.substring(index),
+      ));
+    }
+
+    return spans;
+  }
+}
+
+// TODO(justinmc): Think about which links need to go here vs. on InlineTextLinker.
+/// Specifies a way to find and style parts of a String.
+class TextLinker {
+  /// Creates an instance of [TextLinker].
+  const TextLinker({
+    // TODO(justinmc): Change "range" naming to always be "textRange"?
+    required this.rangesFinder,
+    required this.linkBuilder,
+  });
+
+  /// Builds an [InlineSpan] to display the text that it's passed.
+  final LinkBuilder linkBuilder;
+
+  /// Returns [TextRange]s that should be built with [linkBuilder].
+  final RangesFinder rangesFinder;
 
   // TODO(justinmc): Consider revising this regexp.
   static final RegExp _urlRegExp = RegExp(r'((http|https|ftp):\/\/)?([a-zA-Z\-]*\.)?[a-zA-Z0-9\-]*\.[a-zA-Z]*');
 
-  static InlineSpan _defaultLinkBuilder(String linkText, UriStringCallback? onTap) {
-    return _InlineLink(
-      onTap: onTap,
-      text: linkText,
-    );
-  }
-
-  // Turns all matches from the regExp into a list of TextRanges, allowing
-  // compatibility with _spansFromRanges.
+  // Turns all matches from the regExp into a list of TextRanges.
   static Iterable<TextRange> _rangesFromText({
     required String text,
     required RegExp regExp,
@@ -125,51 +137,146 @@ class InlineLinkedText extends TextSpan {
     });
   }
 
-  // Highlights the text in the given `ranges` and makes them interactive.
-  static List<InlineSpan> _spansFromRanges({
-    required String text,
-    required Iterable<TextRange> ranges,
-    LinkBuilder? linkBuilder = _defaultLinkBuilder,
-    UriStringCallback? onTap,
-  }) {
-    // Sort ranges to facilitate ignoring overlapping ranges.
-    final List<TextRange> rangesList = ranges.toList()
-        ..sort((TextRange a, TextRange b) => a.start.compareTo(b.start));
+  /// Returns a flat list of [InlineSpan]s for multiple [TextLinker]s.
+  ///
+  /// Similar to [getSpans], but for multiple [TextLinker]s instead of just one.
+  static List<InlineSpan> getSpansForMany(Iterable<TextLinker> textLinkers, String text) {
+    final List<_TextLinkerSingle> combinedRanges = textLinkers
+        .fold<List<_TextLinkerSingle>>(
+          <_TextLinkerSingle>[],
+          (List<_TextLinkerSingle> previousValue, TextLinker value) {
+            final Iterable<TextRange> ranges = value.rangesFinder(text);
+            for (final TextRange range in ranges) {
+              previousValue.add(_TextLinkerSingle(
+                textRange: range,
+                linkBuilder: value.linkBuilder,
+              ));
+            }
+            return previousValue;
+        });
 
-    final List<InlineSpan> spans = <InlineSpan>[];
-    int index = 0;
-    for (final TextRange range in rangesList) {
-      // Ignore overlapping ranges.
-      if (index > range.start) {
-        continue;
-      }
-      if (index < range.start) {
-        spans.add(TextSpan(
-          text: text.substring(index, range.start),
-        ));
-      }
-      // TODO(justinmc): Allow custom style if don't pass whole builder fn.
-      spans.add(linkBuilder!(
-        text.substring(range.start, range.end),
-        onTap,
-      ));
-      spans.add(_InlineLink(
-        onTap: onTap,
-        text: text.substring(range.start, range.end),
-      ));
+    return _TextLinkerSingle.getSpansForMany(combinedRanges, text);
+  }
 
-      index = range.end;
-    }
-    if (index < text.length) {
-      spans.add(TextSpan(
-        text: text.substring(index),
-      ));
-    }
+  /// Creates a [RangesFinder] that finds all the matches of the given [RegExp].
+  static RangesFinder rangesFinderFromRegExp(RegExp regExp) {
+    return (String text) {
+      return _rangesFromText(
+        text: text,
+        regExp: _urlRegExp,
+      );
+    };
+  }
 
-    return spans;
+  /// A [RangesFinder] that returns [TextRange]s for URLs.
+  static final RangesFinder urlRangesFinder = rangesFinderFromRegExp(_urlRegExp);
+
+  /// Builds the [InlineSpan]s for the given text.
+  ///
+  /// Builds [linkBuilder] for any ranges found by [rangesFinder]. All other
+  /// text is presented in a plain [TextSpan].
+  List<InlineSpan> getSpans(String text) {
+    final Iterable<TextRange> textRanges = rangesFinder(text);
+    final Iterable<_TextLinkerSingle> textLinkerSingles = textRanges.map((TextRange textRange) {
+      return _TextLinkerSingle(
+        textRange: textRange,
+        linkBuilder: linkBuilder,
+      );
+    });
+    return _TextLinkerSingle.getSpansForMany(textLinkerSingles, text);
   }
 }
 
+// TODO(justinmc): Make agnostic to URLs, just "links", which could be GitHub PRs, etc.
+/// A [TextSpan] that makes parts of the [text] interactive.
+class InlineLinkedText extends TextSpan {
+  /// Create an instance of [InlineLinkedText].
+  ///
+  /// By default, highlights URLs in the [text] and makes them tappable with
+  /// [onTap].
+  ///
+  /// If [ranges] is given, then makes those ranges in the text interactive
+  /// instead of URLs.
+  ///
+  /// [linkBuilder] can be used to specify a custom [InlineSpan] for each
+  /// [TextRange] in [ranges].
+  ///
+  /// See also:
+  ///
+  ///  * [InlineLinkedText.regExp], which automatically finds ranges that match
+  ///    the given [RegExp].
+  ///  * [InlineLinkedText.textLinkers], which uses [TextLinker]s to allow
+  ///    specifying an arbitrary number of [ranges] and [linkBuilders].
+  InlineLinkedText({
+    super.style,
+    required String text,
+    Iterable<TextRange>? ranges,
+    UriStringCallback? onTap,
+    LinkBuilder? linkBuilder,
+  }) : super(
+         children: TextLinker(
+           rangesFinder: ranges == null
+               ? TextLinker.urlRangesFinder
+               : (String text) => ranges,
+           linkBuilder: linkBuilder ?? getDefaultLinkBuilder(onTap),
+         ).getSpans(text),
+       );
+
+  /// Create an instance of [InlineLinkedText] where the text matched by the
+  /// given [regExp] is made interactive.
+  ///
+  /// See also:
+  ///
+  ///  * [InlineLinkifier.new], which can be passed [TextRange]s directly or
+  ///    otherwise matches URLs by default.
+  ///  * [InlineLinkedText.textLinkers], which uses [TextLinker]s to allow
+  ///    specifying an arbitrary number of [ranges] and [linkBuilders].
+  InlineLinkedText.regExp({
+    super.style,
+    required RegExp regExp,
+    required String text,
+    UriStringCallback? onTap,
+    LinkBuilder? linkBuilder,
+  }) : super(
+         children: TextLinker(
+           rangesFinder: TextLinker.rangesFinderFromRegExp(regExp),
+           linkBuilder: linkBuilder ?? getDefaultLinkBuilder(onTap),
+         ).getSpans(text),
+       );
+
+  /// Create an instance of [InlineLinkedText] where
+  ///
+  /// Useful for independently matching different types of strings with
+  /// different behaviors. For example, highlighting both URLs and Twitter
+  /// handles with different style and/or behavior.
+  ///
+  /// See also:
+  ///
+  ///  * [InlineLinkifier.new], which can be passed [TextRange]s directly or
+  ///    otherwise matches URLs by default.
+  ///  * [InlineLinkedText.regExp], which automatically finds ranges that match
+  ///    the given [RegExp].
+  InlineLinkedText.textLinkers({
+    super.style,
+    required String text,
+    required Iterable<TextLinker> textLinkers,
+  }) : super(
+         children: TextLinker.getSpansForMany(textLinkers, text),
+       );
+
+  /// Returns a [LinkBuilder] that simply highlights the given text and provides
+  /// an [onTap] handler.
+  static LinkBuilder getDefaultLinkBuilder([UriStringCallback? onTap]) {
+    return (String linkText) {
+      return _InlineLink(
+        onTap: onTap,
+        text: linkText,
+      );
+    };
+  }
+}
+
+/// An inline text link.
 class _InlineLink extends WidgetSpan {
   _InlineLink({
     required String text,
@@ -188,6 +295,7 @@ class _InlineLink extends WidgetSpan {
   );
 }
 
+/// A text link widget that can be displayed inline with [WidgetSpan].
 class _TextLink extends StatelessWidget {
   const _TextLink({
     required this.uriString,
