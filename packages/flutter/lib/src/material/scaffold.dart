faef8b7365be9ba5ad925da810bcae39c2a9590d
Kate Lovett <katelovett@google.com>
2023-05-05 13:50:10 -0500

    Apply suggestions from code review

--- packages/flutter/lib/src/material/scaffold.dart
+++ packages/flutter/lib/src/material/scaffold.dart
@@ -1436,9 +1436,6 @@ class _FloatingActionButtonTransitionState extends State<_FloatingActionButtonTr
 /// within the [Scaffold]. The [FloatingActionButton] is connected to a
 /// callback that increments a counter.
 ///
-/// ![The Scaffold has a white background with a blue AppBar at the top. A blue
-/// FloatingActionButton is positioned at the bottom right corner of the
-/// Scaffold.](https://flutter.github.io/assets-for-api-docs/assets/material/scaffold.png)
 ///
 /// ** See code in examples/api/lib/material/scaffold/scaffold.0.dart **
 /// {@end-tool}
@@ -1491,14 +1488,14 @@ class _FloatingActionButtonTransitionState extends State<_FloatingActionButtonTr
 ///
 /// If [Scaffold.bottomSheet] is a [DraggableScrollableSheet],
 /// [Scaffold.floatingActionButton] is set, and the bottom sheet is dragged to
-/// cover > 70% of the scaffold's height, two things happen in parallel:
+/// cover greater than 70% of the Scaffold's height, two things happen in parallel:
 ///
-/// * Scaffold starts to show scrim (see [ScaffoldState.showBodyScrim]), and
-/// * Floating action button starts to scale down with an ease in curve, and
-///   disappears when the bottom sheet covers the entire scaffold.
+///   * Scaffold starts to show scrim (see [ScaffoldState.showBodyScrim]), and
+///   * [Scaffold.floatingActionButton] is scaled down through an animation with a [Curves.easeIn], and
+///     disappears when the bottom sheet covers the entire Scaffold.
 ///
-/// And as soon as the bottom sheet is dragged down to cover <= 70%, the scrim
-/// disappears and Floating action button scaled back to normal.
+/// And as soon as the bottom sheet is dragged down to cover less than 70% of the [Scaffold], the scrim
+/// disappears and [Scaffold.floatingActionButton] animates back to its normal size.
 ///
 /// ## Troubleshooting
 ///
