dff78bfdaed9ca3145818f60d20934011ae6d09b
Keyong Han <keyonghan@google.com>
2023-05-09 14:29:41 -0700

    restore unrelated format changes

--- dev/devicelab/lib/tasks/gallery.dart
+++ dev/devicelab/lib/tasks/gallery.dart
@@ -47,7 +47,9 @@ TaskFunction createGalleryTransitionE2ETest({
   bool? enableImpeller,
 }) {
   return GalleryTransitionTest(
-    testFile: semanticsEnabled ? 'transitions_perf_e2e_with_semantics' : 'transitions_perf_e2e',
+    testFile: semanticsEnabled
+        ? 'transitions_perf_e2e_with_semantics'
+        : 'transitions_perf_e2e',
     needFullTimeline: false,
     timelineSummaryFile: 'e2e_perf_summary',
     transitionDurationFile: null,
@@ -64,7 +66,9 @@ TaskFunction createGalleryTransitionHybridBuildTest(
   return GalleryTransitionBuildTest(
     args,
     semanticsEnabled: semanticsEnabled,
-    driverFile: semanticsEnabled ? 'transitions_perf_hybrid_with_semantics_test' : 'transitions_perf_hybrid_test',
+    driverFile: semanticsEnabled
+        ? 'transitions_perf_hybrid_with_semantics_test'
+        : 'transitions_perf_hybrid_test',
   ).call;
 }
 
@@ -76,6 +80,7 @@ TaskFunction createGalleryTransitionHybridTest({bool semanticsEnabled = false}) 
 }
 
 class GalleryTransitionTest {
+
   GalleryTransitionTest({
     this.semanticsEnabled = false,
     this.testFile = 'transitions_perf',
@@ -124,8 +129,9 @@ class GalleryTransitionTest {
         applicationBinaryPath = 'build/app/outputs/flutter-apk/app-profile.apk';
       }
 
-      final String testDriver =
-          driverFile ?? (semanticsEnabled ? '${testFile}_with_semantics_test' : '${testFile}_test');
+      final String testDriver = driverFile ?? (semanticsEnabled
+          ? '${testFile}_with_semantics_test'
+          : '${testFile}_test');
       section('DRIVE START');
       await flutter('drive', options: <String>[
         '--no-dds',
@@ -136,10 +142,11 @@ class GalleryTransitionTest {
           '--trace-startup',
         if (applicationBinaryPath != null)
           '--use-application-binary=$applicationBinaryPath'
-        else ...<String>[
-          '-t',
-          'test_driver/$testFile.dart',
-        ],
+        else
+          ...<String>[
+            '-t',
+            'test_driver/$testFile.dart',
+          ],
         '--driver',
         'test_driver/$testDriver.dart',
         '-d',
@@ -149,8 +156,7 @@ class GalleryTransitionTest {
       ]);
     });
 
-    final String testOutputDirectory =
-        Platform.environment['FLUTTER_TEST_OUTPUTS_DIR'] ?? '${galleryDirectory.path}/build';
+    final String testOutputDirectory = Platform.environment['FLUTTER_TEST_OUTPUTS_DIR'] ?? '${galleryDirectory.path}/build';
     final Map<String, dynamic> summary = json.decode(
       file('$testOutputDirectory/$timelineSummaryFile.json').readAsStringSync(),
     ) as Map<String, dynamic>;
@@ -168,14 +174,16 @@ class GalleryTransitionTest {
     }
 
     final bool isAndroid = deviceOperatingSystem == DeviceOperatingSystem.android;
-    return TaskResult.success(
-      summary,
+    return TaskResult.success(summary,
       detailFiles: <String>[
-        if (transitionDurationFile != null) '$testOutputDirectory/$transitionDurationFile.json',
-        if (timelineTraceFile != null) '$testOutputDirectory/$timelineTraceFile.json',
+        if (transitionDurationFile != null)
+          '$testOutputDirectory/$transitionDurationFile.json',
+        if (timelineTraceFile != null)
+          '$testOutputDirectory/$timelineTraceFile.json',
       ],
       benchmarkScoreKeys: <String>[
-        if (transitionDurationFile != null) 'missed_transition_count',
+        if (transitionDurationFile != null)
+          'missed_transition_count',
         'average_frame_build_time_millis',
         'worst_frame_build_time_millis',
         '90th_percentile_frame_build_time_millis',
@@ -242,8 +250,7 @@ class GalleryTransitionBuildTest extends BuildTestTask {
   final String? transitionDurationFile;
   final String? driverFile;
 
-  final String testOutputDirectory =
-      Platform.environment['FLUTTER_TEST_OUTPUTS_DIR'] ?? '${galleryDirectory.path}/build';
+  final String testOutputDirectory = Platform.environment['FLUTTER_TEST_OUTPUTS_DIR'] ?? '${galleryDirectory.path}/build';
 
   @override
   void copyArtifacts() {
