dd4f0691542081fc178d74a20a578e4c6688449f
Christian Findlay <16697547+MelbourneDeveloper@users.noreply.github.com>
2023-05-09 16:29:58 +1000

    Fix up the imports

--- packages/flutter/lib/src/material/chip_set.dart
+++ packages/flutter/lib/src/material/chip_set.dart
@@ -2,7 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'chip.dart';
 
 ///Builds a custom chip widget for a given item of type T
 ///- BuildContext context: The build context in which the widget is being built.
