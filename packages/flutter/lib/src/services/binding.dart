13b45b6b93f85d785af327e372869b164a46ce24
schectman <schectman@google.com>
2023-05-10 09:28:59 -0400

    Mark for testing
    Using @visibleForTesting because the tests are not contained in a class.

--- packages/flutter/lib/src/services/binding.dart
+++ packages/flutter/lib/src/services/binding.dart
@@ -420,6 +420,7 @@ mixin ServicesBinding on BindingBase, SchedulerBinding {
   /// Alert the engine that the binding is registered. This instructs the engine to
   /// register its top level window handler on Windows. This signals that the app
   /// is able to process "System.requestAppExit" signals from the engine.
+  @visibleForTesting
   Future<void> initializationComplete() async {
     SystemChannels.platform.invokeMethod('System.initializationComplete');
   }
