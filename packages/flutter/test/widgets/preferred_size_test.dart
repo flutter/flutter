b86c734bee8c53a9bbef9e9931f9413424af10ce
Navaron Bracke <brackenavaron@gmail.com>
2023-05-11 12:42:40 +0200

    fix header

--- packages/flutter/test/widgets/preferred_size_test.dart
+++ packages/flutter/test/widgets/preferred_size_test.dart
@@ -1,3 +1,7 @@
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 // ignore_for_file: unused_element
 
 // Regression test for https://github.com/flutter/flutter/issues/126512
