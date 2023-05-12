e87d7155d21c1ac24f9becc6ce3db47b0e11df5f
Mouad Debbar <mdebbar@google.com>
2023-05-12 15:59:30 -0400

    [web] Fix crash on old versions of Chrome (Intl.Segmenter missing) (flutter/engine#41986)
    `Chrome < v87` have the `v8BreakIterator` API but don't have `Intl.Segmenter`. This caused crashes for some apps (see b/281377168).
    
    This PR enhances our feature detection to take into account this API mismatch.

--- engine/src/flutter/lib/web_ui/test/engine/browser_detect_test.dart
+++ engine/src/flutter/lib/web_ui/test/engine/browser_detect_test.dart
@@ -157,17 +157,22 @@ void testMain() {
   });
 
   group('browserSupportsCanvasKitChromium', () {
-    late dynamic oldV8BreakIterator = v8BreakIterator;
+    dynamic oldV8BreakIterator = v8BreakIterator;
+    dynamic oldIntlSegmenter = intlSegmenter;
+
     setUp(() {
       oldV8BreakIterator = v8BreakIterator;
+      oldIntlSegmenter = intlSegmenter;
     });
     tearDown(() {
       v8BreakIterator = oldV8BreakIterator;
+      intlSegmenter = oldIntlSegmenter;
       debugResetBrowserSupportsImageDecoder();
     });
 
     test('Detect browsers that support CanvasKit Chromium', () {
       v8BreakIterator = Object(); // Any non-null value.
+      intlSegmenter = Object(); // Any non-null value.
       browserSupportsImageDecoder = true;
 
       expect(browserSupportsCanvaskitChromium, isTrue);
@@ -175,6 +180,7 @@ void testMain() {
 
     test('Detect browsers that do not support image codecs', () {
       v8BreakIterator = Object(); // Any non-null value.
+      intlSegmenter = Object(); // Any non-null value.
       browserSupportsImageDecoder = false;
 
       // TODO(mdebbar): we don't check image codecs for now.
@@ -184,6 +190,7 @@ void testMain() {
 
     test('Detect browsers that do not support v8BreakIterator', () {
       v8BreakIterator = null;
+      intlSegmenter = Object(); // Any non-null value.
       browserSupportsImageDecoder = true;
 
       expect(browserSupportsCanvaskitChromium, isFalse);
@@ -191,7 +198,15 @@ void testMain() {
 
     test('Detect browsers that support neither', () {
       v8BreakIterator = null;
+      intlSegmenter = Object(); // Any non-null value.
       browserSupportsImageDecoder = false;
+
+      expect(browserSupportsCanvaskitChromium, isFalse);
+    });
+
+    test('Detect browsers that support v8BreakIterator but no Intl.Segmenter', () {
+      v8BreakIterator = Object(); // Any non-null value.
+      intlSegmenter = null;
 
       expect(browserSupportsCanvaskitChromium, isFalse);
     });
@@ -211,3 +226,9 @@ external dynamic get v8BreakIterator;
 
 @JS('window.Intl.v8BreakIterator')
 external set v8BreakIterator(dynamic x);
+
+@JS('window.Intl.Segmenter')
+external dynamic get intlSegmenter;
+
+@JS('window.Intl.Segmenter')
+external set intlSegmenter(dynamic x);
