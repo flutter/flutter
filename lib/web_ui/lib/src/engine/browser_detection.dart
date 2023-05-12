2ac8552637424684edb543607cd8c96e4d4dc0c9
Mouad Debbar <mdebbar@google.com>
2023-05-12 15:59:30 -0400

    [web] Fix crash on old versions of Chrome (Intl.Segmenter missing) (#41986)
    `Chrome < v87` have the `v8BreakIterator` API but don't have `Intl.Segmenter`. This caused crashes for some apps (see b/281377168).
    
    This PR enhances our feature detection to take into account this API mismatch.

--- lib/web_ui/lib/src/engine/browser_detection.dart
+++ lib/web_ui/lib/src/engine/browser_detection.dart
@@ -268,4 +268,5 @@ int _detectWebGLVersion() {
 }
 
 /// Whether the current browser supports the Chromium variant of CanvasKit.
-bool get browserSupportsCanvaskitChromium => domIntl.v8BreakIterator != null;
+bool get browserSupportsCanvaskitChromium =>
+    domIntl.v8BreakIterator != null && domIntl.Segmenter != null;
