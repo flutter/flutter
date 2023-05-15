8d07db38c55433eb433f448defa17a6b492200d2
Chris Yang <ychris@google.com>
2023-05-15 12:05:10 -0700

    Upload xcresults to LUCI cloud storage (flutter/engine#41647)
    Taking over from https://github.com/flutter/engine/pull/41644
    
    fixes: https://github.com/flutter/flutter/issues/125823
    
    Steps to verify
    
    [C++, Objective-C, Java style guides]: https://github.com/flutter/engine/blob/main/CONTRIBUTING.md#style

--- engine/src/flutter/testing/scenario_app/run_ios_tests.sh
+++ engine/src/flutter/testing/scenario_app/run_ios_tests.sh
@@ -41,25 +41,58 @@ fi
 # Can also be set via Simulator app Device > Rotate Device Automatically
 defaults write com.apple.iphonesimulator RotateWindowWhenSignaledByGuest -int 1
 
-cd $SRC_DIR/out/$FLUTTER_ENGINE/scenario_app/Scenarios
+SCENARIO_PATH=$SRC_DIR/out/$FLUTTER_ENGINE/scenario_app/Scenarios
+pushd .
+cd $SCENARIO_PATH
+
+RESULT_BUNDLE_FOLDER="ios_scenario_xcresult"
+RESULT_BUNDLE_PATH="${SCENARIO_PATH}/${RESULT_BUNDLE_FOLDER}"
+
+# Zip and upload xcresult to luci.
+# First parameter ($1) is the zip output name.
+zip_and_upload_xcresult_to_luci () {
+  # We don't want the zip to contain the abusolute path,
+  # so use relative path (./$RESULT_BUNDLE_FOLDER) instead.
+  zip -q -r $1 "./$RESULT_BUNDLE_FOLDER"
+  mv -f $1 $FLUTTER_TEST_OUTPUTS_DIR
+  exit 1
+}
 
 echo "Running simulator tests with Skia"
 echo ""
 
-set -o pipefail && xcodebuild -sdk iphonesimulator \
+mktemp -d $RESULT_BUNDLE_PATH
+
+if set -o pipefail && xcodebuild -sdk iphonesimulator \
   -scheme Scenarios \
+  -resultBundlePath "$RESULT_BUNDLE_PATH/ios_scenario.xcresult" \
   -destination 'platform=iOS Simulator,OS=16.2,name=iPhone SE (3rd generation)' \
   clean test \
-  FLUTTER_ENGINE="$FLUTTER_ENGINE"
+  FLUTTER_ENGINE="$FLUTTER_ENGINE"; then
+  echo "test success."
+else
+  echo "test failed."
+  zip_and_upload_xcresult_to_luci "ios_scenario_xcresult.zip"
+fi
+rm -rf $RESULT_BUNDLE_PATH
 
 echo "Running simulator tests with Impeller"
 echo ""
 
 # Skip testFontRenderingWhenSuppliedWithBogusFont: https://github.com/flutter/flutter/issues/113250
-set -o pipefail && xcodebuild -sdk iphonesimulator \
+if set -o pipefail && xcodebuild -sdk iphonesimulator \
   -scheme Scenarios \
+  -resultBundlePath "$RESULT_BUNDLE_PATH/ios_scenario.xcresult" \
   -destination 'platform=iOS Simulator,OS=16.2,name=iPhone SE (3rd generation)' \
   clean test \
   FLUTTER_ENGINE="$FLUTTER_ENGINE" \
   -skip-testing "ScenariosUITests/BogusFontTextTest/testFontRenderingWhenSuppliedWithBogusFont" \
-  INFOPLIST_FILE="Scenarios/Info_Impeller.plist" # Plist with FLTEnableImpeller=YES
+  INFOPLIST_FILE="Scenarios/Info_Impeller.plist"; then # Plist with FLTEnableImpeller=YES
+  echo "test success."
+else
+  echo "test failed."
+  zip_and_upload_xcresult_to_luci "ios_scenario_impeller_xcresult.zip"
+fi
+rm -rf $RESULT_BUNDLE_PATH
+
+popd
