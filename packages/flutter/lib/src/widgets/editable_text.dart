b4c9c4758fcd70d037c5a1646f99595e56da2fdc
Justin McCandless <jmccandless@google.com>
2023-05-17 15:38:53 -0700

    Edge case handling

--- packages/flutter/lib/src/widgets/editable_text.dart
+++ packages/flutter/lib/src/widgets/editable_text.dart
@@ -5109,32 +5109,35 @@ class _CodePointBoundary extends TextBoundary {
 
   @override
   int? getLeadingTextBoundaryAt(int position) {
-    if (position > _text.length || position < 0) {
-      return null;
-    }
-    if (position == 0) {
+    final int validatedPosition = position.clamp(0, _text.length);
+    if (validatedPosition == 0) {
       return 0;
     }
+    if (validatedPosition == _text.length) {
+      return _text.length;
+    }
     if (_text.length <= 1) {
-      return position;
+      return validatedPosition;
     }
 
-    return _breaksSurrogatePair(position) ? position - 1 : position;
+    return _breaksSurrogatePair(validatedPosition)
+        ? validatedPosition - 1
+        : validatedPosition;
   }
 
   @override
   int? getTrailingTextBoundaryAt(int position) {
-    if (position < 0 || position >= _text.length) {
-      return null;
-    }
-    if (position == _text.length - 1) {
+    final int validatedPosition = position.clamp(0, _text.length - 1);
+    if (validatedPosition == _text.length - 1) {
       return _text.length;
     }
     if (_text.length <= 1) {
-      return position;
+      return validatedPosition;
     }
 
-    return _breaksSurrogatePair(position + 1) ? position + 2 : position + 1;
+    return _breaksSurrogatePair(validatedPosition + 1)
+        ? validatedPosition + 2
+        : validatedPosition + 1;
   }
 }
 
