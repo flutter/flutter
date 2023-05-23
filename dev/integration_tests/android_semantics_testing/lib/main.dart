80eeb2fc6353b907a2653ab6f8e708a9fca2215a
chunhtai <47866232+chunhtai@users.noreply.github.com>
2023-05-23 15:33:19 -0700

    Migrates android semanitcs integration to integration test (#127128)
    I think the flake is due to setclipboard or semantics update race condition. I migrated the test to use integration test package which relies less on timing
    
    fixes https://github.com/flutter/flutter/issues/124636

--- dev/integration_tests/android_semantics_testing/lib/main.dart
+++ dev/integration_tests/android_semantics_testing/lib/main.dart
@@ -2,13 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'dart:async';
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/scheduler.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter_driver/driver_extension.dart';
 
 import 'src/tests/controls_page.dart';
 import 'src/tests/headings_page.dart';
@@ -16,47 +10,7 @@ import 'src/tests/popup_page.dart';
 import 'src/tests/text_field_page.dart';
 
 void main() {
-  timeDilation = 0.05; // remove animations.
-  enableFlutterDriverExtension(handler: dataHandler);
   runApp(const TestApp());
-}
-
-const MethodChannel kSemanticsChannel = MethodChannel('semantics');
-
-Future<String> dataHandler(String? message) async {
-  if (message != null && message.contains('getSemanticsNode')) {
-    final Completer<String> completer = Completer<String>();
-    final int id = int.tryParse(message.split('#')[1]) ?? 0;
-    Future<void> completeSemantics([Object? _]) async {
-      final dynamic result = await kSemanticsChannel.invokeMethod<dynamic>('getSemanticsNode', <String, dynamic>{
-        'id': id,
-      });
-      completer.complete(json.encode(result));
-    }
-    if (SchedulerBinding.instance.hasScheduledFrame) {
-      SchedulerBinding.instance.addPostFrameCallback(completeSemantics);
-    } else {
-      completeSemantics();
-    }
-    return completer.future;
-  }
-  if (message != null && message.contains('setClipboard')) {
-    final Completer<String> completer = Completer<String>();
-    final String str = message.split('#')[1];
-    Future<void> completeSetClipboard([Object? _]) async {
-      await kSemanticsChannel.invokeMethod<dynamic>('setClipboard', <String, dynamic>{
-        'message': str,
-      });
-      completer.complete('');
-    }
-    if (SchedulerBinding.instance.hasScheduledFrame) {
-      SchedulerBinding.instance.addPostFrameCallback(completeSetClipboard);
-    } else {
-      completeSetClipboard();
-    }
-    return completer.future;
-  }
-  throw UnimplementedError();
 }
 
 Map<String, WidgetBuilder> routes = <String, WidgetBuilder>{
