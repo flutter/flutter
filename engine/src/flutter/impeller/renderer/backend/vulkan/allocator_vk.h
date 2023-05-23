5ed6890c1491f0c763c23d604a6fdad95baa2651
gaaclarke <30870216+gaaclarke@users.noreply.github.com>
2023-05-23 10:03:19 -0700

    [Impeller] Removed last ivars to raw vk::Device's. (flutter/engine#42215)
    Removes the last ivars that hold onto a `Device` directly.  This have recently been the cause of crashes, so the safer thing is to remove them all.
    
    For the Allocator and the Sampler library, removing the device ivars is likely just a precaution since they should have only been accessed from the raster thread and killed before the Device.  The fence waiter however was potentially accessing the device from a separate thread, so that was much more risky and likely to cause crashes.
    
    Testing: The existing `impeller_unittests` verify this is setup correctly.
    
    [C++, Objective-C, Java style guides]: https://github.com/flutter/engine/blob/main/CONTRIBUTING.md#style

--- engine/src/flutter/impeller/renderer/backend/vulkan/allocator_vk.h
+++ engine/src/flutter/impeller/renderer/backend/vulkan/allocator_vk.h
@@ -10,6 +10,7 @@
 #include "impeller/core/allocator.h"
 #include "impeller/renderer/backend/vulkan/context_vk.h"
 #include "impeller/renderer/backend/vulkan/device_buffer_vk.h"
+#include "impeller/renderer/backend/vulkan/device_holder.h"
 #include "impeller/renderer/backend/vulkan/vk.h"
 
 #include <memory>
@@ -27,14 +28,14 @@ class AllocatorVK final : public Allocator {
   fml::RefPtr<vulkan::VulkanProcTable> vk_;
   VmaAllocator allocator_ = {};
   std::weak_ptr<Context> context_;
-  vk::Device device_;
+  std::weak_ptr<DeviceHolder> device_holder_;
   ISize max_texture_size_;
   bool is_valid_ = false;
 
   AllocatorVK(std::weak_ptr<Context> context,
               uint32_t vulkan_api_version,
               const vk::PhysicalDevice& physical_device,
-              const vk::Device& logical_device,
+              const std::shared_ptr<DeviceHolder>& device_holder,
               const vk::Instance& instance,
               PFN_vkGetInstanceProcAddr get_instance_proc_address,
               PFN_vkGetDeviceProcAddr get_device_proc_address);
--- engine/src/flutter/impeller/renderer/backend/vulkan/fence_waiter_vk.cc
+++ engine/src/flutter/impeller/renderer/backend/vulkan/fence_waiter_vk.cc
@@ -11,7 +11,8 @@
 
 namespace impeller {
 
-FenceWaiterVK::FenceWaiterVK(vk::Device device) : device_(device) {
+FenceWaiterVK::FenceWaiterVK(std::weak_ptr<DeviceHolder> device_holder)
+    : device_holder_(device_holder) {
   waiter_thread_ = std::make_unique<std::thread>([&]() { Main(); });
   is_valid_ = true;
 }
@@ -51,7 +52,12 @@ void FenceWaiterVK::Main() {
 
     wait_set_cv_.wait(lock, [&]() { return !wait_set_.empty() || terminate_; });
 
-    auto wait_set = TrimAndCreateWaitSetLocked();
+    auto device_holder = device_holder_.lock();
+    if (!device_holder) {
+      break;
+    }
+
+    auto wait_set = TrimAndCreateWaitSetLocked(device_holder);
 
     lock.unlock();
 
@@ -62,7 +68,7 @@ void FenceWaiterVK::Main() {
       continue;
     }
 
-    auto result = device_.waitForFences(
+    auto result = device_holder->GetDevice().waitForFences(
         wait_set->size(),                        // fences count
         wait_set->data(),                        // fences
         false,                                   // wait for all
@@ -74,8 +80,8 @@ void FenceWaiterVK::Main() {
   }
 }
 
-std::optional<std::vector<vk::Fence>>
-FenceWaiterVK::TrimAndCreateWaitSetLocked() {
+std::optional<std::vector<vk::Fence>> FenceWaiterVK::TrimAndCreateWaitSetLocked(
+    std::shared_ptr<DeviceHolder> device_holder) {
   if (terminate_) {
     return std::nullopt;
   }
@@ -83,7 +89,7 @@ FenceWaiterVK::TrimAndCreateWaitSetLocked() {
   std::vector<vk::Fence> fences;
   fences.reserve(wait_set_.size());
   for (auto it = wait_set_.begin(); it != wait_set_.end();) {
-    switch (device_.getFenceStatus(it->first->Get())) {
+    switch (device_holder->GetDevice().getFenceStatus(it->first->Get())) {
       case vk::Result::eSuccess:  // Signalled.
         it->second();
         it = wait_set_.erase(it);
