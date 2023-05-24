35089601cbba0e4c988087a96e2ce2f7788cd660
Mouad Debbar <mdebbar@google.com>
2023-05-24 17:05:20 -0400

    fix doc reference

--- packages/flutter/lib/src/widgets/_platform_selectable_region_context_menu_io.dart
+++ packages/flutter/lib/src/widgets/_platform_selectable_region_context_menu_io.dart
@@ -40,7 +40,7 @@ class PlatformSelectableRegionContextMenu extends StatelessWidget {
   /// Detaches the `client` from the platform-appropriate selection context menus.
   static void detach(SelectionContainerDelegate client) => throw UnimplementedError();
 
-  /// Override this to provide a custom implementation of [ui_web.platformViewRegistry.registerViewFactory].
+  /// Override this to provide a custom implementation of `ui_web.platformViewRegistry.registerViewFactory`.
   ///
   /// This should only be used for testing.
   @visibleForTesting
