3e8bbafd3a4d1149046efa5e08251ff28a023a36
Renzo Olivares <rmolivares@renzo-olivares.dev>
2023-05-24 09:52:37 -0700

    updates

--- packages/flutter/lib/src/material/bottom_sheet.dart
+++ packages/flutter/lib/src/material/bottom_sheet.dart
@@ -1333,7 +1333,6 @@ class _BottomSheetGestureDetector extends StatelessWidget {
 }
 
 
-
 // BEGIN GENERATED TOKEN PROPERTIES - BottomSheet
 
 // Do not edit by hand. The code between the "BEGIN GENERATED" and
