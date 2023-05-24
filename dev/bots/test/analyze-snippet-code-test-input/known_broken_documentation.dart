f46812c6ac1148471658eb468ff182ddef0bc24b
pq <pquitslund@gmail.com>
2023-05-24 10:20:06 -0700

    update test expectations to anticipate fixed linter

--- dev/bots/test/analyze-snippet-code-test-input/known_broken_documentation.dart
+++ dev/bots/test/analyze-snippet-code-test-input/known_broken_documentation.dart
@@ -145,7 +145,7 @@
 ///
 /// ```dart
 /// import 'dart:io'; // error (unused_import)
-/// final Widget p = Placeholder(); // error (undefined_class, undefined_function, avoid_dynamic_calls)
+/// final Widget p = Placeholder(); // error (undefined_class, undefined_function)
 /// ```
 ///
 /// ```dart
