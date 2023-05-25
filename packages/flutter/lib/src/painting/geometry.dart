1b800fd49665bab5ef94f4b40845e99fd2c60400
LongCatIsLooong <31859944+LongCatIsLooong@users.noreply.github.com>
2023-05-25 00:24:45 -0700

    Remove Tooltip mouse tracker listener & update hovering/MouseRegion logic & animation (#119199)
    Fixes https://github.com/flutter/flutter/issues/117627
    
    ### Behavior changes:
    1. If fade in/fade out animation is already in progress, hiding/showing the tooltip will immediately take effect without waiting for `waitDuration`.
    2. A PointerDownEvent that doesn't become a part of a "trigger" gesture dismisses the tooltip, even for hovered ones.
    3. The OverlayEntry is now updated only when the previous tooltip was completely dismissed. This can be fixed by OverlayPortal but I'm not sure what the correct behavior is.

--- packages/flutter/lib/src/painting/geometry.dart
+++ packages/flutter/lib/src/painting/geometry.dart
@@ -49,14 +49,14 @@ Offset positionDependentBox({
   final bool fitsBelow = target.dy + verticalOffset + childSize.height <= size.height - margin;
   final bool fitsAbove = target.dy - verticalOffset - childSize.height >= margin;
   final bool tooltipBelow = preferBelow ? fitsBelow || !fitsAbove : !(fitsAbove || !fitsBelow);
-  double y;
+  final double y;
   if (tooltipBelow) {
     y = math.min(target.dy + verticalOffset, size.height - margin);
   } else {
     y = math.max(target.dy - verticalOffset - childSize.height, margin);
   }
   // HORIZONTAL DIRECTION
-  double x;
+  final double x;
   if (size.width - margin * 2.0 < childSize.width) {
     x = (size.width - childSize.width) / 2.0;
   } else {
