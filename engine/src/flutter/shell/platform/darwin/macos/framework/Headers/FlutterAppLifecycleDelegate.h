2af9be9d9755553575bdf7e0900d9565123b0e16
Greg Spencer <gspencergoog@users.noreply.github.com>
2023-05-26 16:24:33 -0700

    Adding app lifecycle notification for macOS and Linux, add `hidden` state. (flutter/engine#40542)
    ## Description
    
    This adds app lifecycle notification for macOS and Linux, and adds a new `hidden` state to the `AppLifecycleState` enum.
    
    To be functional, this needs a corresponding framework change: https://github.com/flutter/flutter/pull/123274
    
    ## Related Issues
     - https://github.com/flutter/flutter/issues/30735
    
    ## Tests
     - Added tests for state changes.

---
+++ engine/src/flutter/shell/platform/darwin/macos/framework/Headers/FlutterAppLifecycleDelegate.h
@@ -0,0 +1,137 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef FLUTTER_FLUTTERAPPLIFECYCLEDELEGATE_H_
+#define FLUTTER_FLUTTERAPPLIFECYCLEDELEGATE_H_
+
+#import <Cocoa/Cocoa.h>
+#include <Foundation/Foundation.h>
+
+#import "FlutterMacros.h"
+#import "FlutterPluginMacOS.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+#pragma mark -
+/**
+ * Protocol for listener of lifecycle events from the NSApplication, typically a
+ * FlutterPlugin.
+ */
+@protocol FlutterAppLifecycleDelegate <NSObject>
+
+@optional
+/**
+ * Called when the |FlutterAppDelegate| gets the applicationWillFinishLaunching
+ * notification.
+ */
+- (void)handleWillFinishLaunching:(NSNotification*)notification;
+
+/**
+ * Called when the |FlutterAppDelegate| gets the applicationDidFinishLaunching
+ * notification.
+ */
+- (void)handleDidFinishLaunching:(NSNotification*)notification;
+
+/**
+ * Called when the |FlutterAppDelegate| gets the applicationWillBecomeActive
+ * notification.
+ */
+- (void)handleWillBecomeActive:(NSNotification*)notification;
+
+/**
+ * Called when the |FlutterAppDelegate| gets the applicationDidBecomeActive
+ * notification.
+ */
+- (void)handleDidBecomeActive:(NSNotification*)notification;
+
+/**
+ * Called when the |FlutterAppDelegate| gets the applicationWillResignActive
+ * notification.
+ */
+- (void)handleWillResignActive:(NSNotification*)notification;
+
+/**
+ * Called when the |FlutterAppDelegate| gets the applicationWillResignActive
+ * notification.
+ */
+- (void)handleDidResignActive:(NSNotification*)notification;
+
+/**
+ * Called when the |FlutterAppDelegate| gets the applicationWillHide
+ * notification.
+ */
+- (void)handleWillHide:(NSNotification*)notification;
+
+/**
+ * Called when the |FlutterAppDelegate| gets the applicationDidHide
+ * notification.
+ */
+- (void)handleDidHide:(NSNotification*)notification;
+
+/**
+ * Called when the |FlutterAppDelegate| gets the applicationWillUnhide
+ * notification.
+ */
+- (void)handleWillUnhide:(NSNotification*)notification;
+
+/**
+ * Called when the |FlutterAppDelegate| gets the applicationDidUnhide
+ * notification.
+ */
+- (void)handleDidUnhide:(NSNotification*)notification;
+
+/**
+ * Called when the |FlutterAppDelegate| gets the applicationDidUnhide
+ * notification.
+ */
+- (void)handleDidChangeScreenParameters:(NSNotification*)notification;
+
+/**
+ * Called when the |FlutterAppDelegate| gets the applicationDidUnhide
+ * notification.
+ */
+- (void)handleDidChangeOcclusionState:(NSNotification*)notification API_AVAILABLE(macos(10.9));
+
+/**
+ * Called when the |FlutterAppDelegate| gets the applicationWillTerminate
+ * notification.
+ *
+ * Applications should not rely on always receiving all possible notifications.
+ *
+ * For example, if the application is killed with a task manager, a kill signal,
+ * the user pulls the power from the device, or there is a rapid unscheduled
+ * disassembly of the device, no notification will be sent before the
+ * application is suddenly terminated, and this notification may be skipped.
+ */
+- (void)handleWillTerminate:(NSNotification*)notification;
+@end
+
+#pragma mark -
+
+/**
+ * Propagates `NSAppDelegate` callbacks to registered delegates.
+ */
+FLUTTER_DARWIN_EXPORT
+@interface FlutterAppLifecycleRegistrar : NSObject <FlutterAppLifecycleDelegate>
+
+/**
+ * Registers `delegate` to receive lifecycle callbacks via this
+ * FlutterAppLifecycleDelegate as long as it is alive.
+ *
+ * `delegate` will only be referenced weakly.
+ */
+- (void)addDelegate:(NSObject<FlutterAppLifecycleDelegate>*)delegate;
+
+/**
+ * Unregisters `delegate` so that it will no longer receive life cycle callbacks
+ * via this FlutterAppLifecycleDelegate.
+ *
+ * `delegate` will only be referenced weakly.
+ */
+- (void)removeDelegate:(NSObject<FlutterAppLifecycleDelegate>*)delegate;
+@end
+
+NS_ASSUME_NONNULL_END
+
+#endif  // FLUTTER_FLUTTERAPPLIFECYCLEDELEGATE_H_
