d73aac24349344007f1ae95053ad75d7ffdb18cf
gaaclarke <30870216+gaaclarke@users.noreply.github.com>
2023-05-26 12:05:19 -0700

    Started executing vulkan unit tests with validation on macos (flutter/engine#42337)
    fixes https://github.com/flutter/flutter/issues/127634
    
    This PR depends on a buildroot change at https://github.com/flutter/buildroot/pull/737
    
    [C++, Objective-C, Java style guides]: https://github.com/flutter/engine/blob/main/CONTRIBUTING.md#style

--- engine/src/flutter/testing/run_tests.py
+++ engine/src/flutter/testing/run_tests.py
@@ -75,19 +75,23 @@ def run_cmd(
   print(f'Running command "{command_string}"')
 
   start_time = time.time()
-  collect_output = forbidden_output or allowed_failure_output
-  stdout_pipe = sys.stdout if not collect_output else subprocess.PIPE
-  stderr_pipe = sys.stderr if not collect_output else subprocess.PIPE
 
   process = subprocess.Popen(
       cmd,
-      stdout=stdout_pipe,
-      stderr=stderr_pipe,
+      stdout=subprocess.PIPE,
+      stderr=subprocess.STDOUT,
       env=env,
       universal_newlines=True,
       **kwargs
   )
-  stdout, stderr = process.communicate()
+  output = ''
+
+  for line in iter(process.stdout.readline, ''):
+    output += line
+    sys.stdout.write(line)
+
+  sys.stdout.flush()
+  process.wait()
   end_time = time.time()
 
   if process.returncode != 0 and not expect_failure:
@@ -97,18 +101,11 @@ def run_cmd(
         f'Failed Command:\n\n{command_string}\n\nExit Code: {process.returncode}\n'
     )
 
-    if stdout:
-      print(f'STDOUT: \n{stdout}')
-
-    if stderr:
-      print(f'STDERR: \n{stderr}')
-
     print_divider('!')
 
     allowed_failure = False
     for allowed_string in allowed_failure_output:
-      if (stdout and allowed_string in stdout) or (stderr and
-                                                   allowed_string in stderr):
+      if allowed_string in output:
         allowed_failure = True
 
     if not allowed_failure:
@@ -116,13 +113,8 @@ def run_cmd(
           f'Command "{command_string}" exited with code {process.returncode}.'
       )
 
-  if stdout or stderr:
-    print(stdout)
-    print(stderr)
-
   for forbidden_string in forbidden_output:
-    if (stdout and forbidden_string in stdout) or (stderr and
-                                                   forbidden_string in stderr):
+    if forbidden_string in output:
       raise RuntimeError(
           f'command "{command_string}" contained forbidden string {forbidden_string}'
       )
@@ -489,7 +481,12 @@ def run_cc_tests(build_dir, executable_filter, coverage, capture_core_dump):
             '1',  # Validates accesses to threadgroup memory.
         'MTL_SHADER_VALIDATION_TEXTURE_USAGE':
             '1',  # Validates that texture references are not nil.
+        # Note: built from //third_party/swiftshader
         'VK_ICD_FILENAMES': os.path.join(build_dir, 'vk_swiftshader_icd.json'),
+        # Note: built from //third_party/vulkan_validation_layers:vulkan_gen_json_files
+        # and //third_party/vulkan_validation_layers.
+        'VK_LAYER_PATH': os.path.join(build_dir, 'vulkan-data'),
+        'VK_INSTANCE_LAYERS': 'VK_LAYER_KHRONOS_validation',
     }
     if is_aarm64():
       extra_env.update({
@@ -501,7 +498,7 @@ def run_cc_tests(build_dir, executable_filter, coverage, capture_core_dump):
         build_dir,
         'impeller_unittests',
         executable_filter,
-        shuffle_flags,
+        shuffle_flags + ['--enable_vulkan_validation'],
         coverage=coverage,
         extra_env=extra_env,
         # TODO(117122): Remove this allowlist.
