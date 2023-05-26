41e9b4f8e8e21204807ee1281cfd29ca28551c2f
Jackson Gardner <jacksongardner@google.com>
2023-05-26 10:56:05 -0700

    Don't use a factory constructor on the finalization registry. (#42350)
    The factory constructor breaks the Google3 build for some reason. See b/284478971

--- lib/web_ui/test/canvaskit/canvaskit_api_test.dart
+++ lib/web_ui/test/canvaskit/canvaskit_api_test.dart
@@ -1907,7 +1907,7 @@ void _paragraphTests() {
     // FinalizationRegistry because it depends on GC, which cannot be controlled,
     // So the test simply tests that a FinalizationRegistry can be constructed
     // and its `register` method can be called.
-    final DomFinalizationRegistry registry = DomFinalizationRegistry((String arg) {}.toJS);
+    final DomFinalizationRegistry registry = createDomFinalizationRegistry((String arg) {}.toJS);
     registry.register(Object(), Object());
   });
 }
