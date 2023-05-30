4a55fca1f0c777356c6a8c7277d4060ceb0a6d18
Reid Baker <reidbaker@google.com>
2023-05-30 16:50:13 -0400

    Update engine testing to use api 33 for android tests.  (flutter/engine#42351)
    #flutter/flutter/127682
    The plurality of android users are on api 33. We should be testing on our most used android api.

--- engine/src/flutter/ci/firebase_testlab.py
+++ engine/src/flutter/ci/firebase_testlab.py
@@ -54,7 +54,7 @@ def run_firebase_test(apk, results_dir):
           '--results-dir',
           results_dir,
           '--device',
-          'model=redfin,version=30',
+          'model=oriole,version=33',
       ],
       stdout=subprocess.PIPE,
       stderr=subprocess.STDOUT,
