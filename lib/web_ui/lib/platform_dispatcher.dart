81839ac81a644b58c598cbf3560eb9e76aafceeb
Greg Spencer <gspencergoog@users.noreply.github.com>
2023-05-30 13:45:46 -0700

    Reland: "Adding app lifecycle notification for macOS and Linux, add `hidden` state." (#40542) (#42418)
    ## Description
    
    This reverts commit 57e491a to re-land #40542 to fix the lint warning which caused the original PR to be reverted (the lint warnings were turned on after the original was landed, which is why it wasn't caught earlier).

--- lib/web_ui/lib/platform_dispatcher.dart
+++ lib/web_ui/lib/platform_dispatcher.dart
@@ -104,7 +104,7 @@ abstract class PlatformDispatcher {
   VoidCallback? get onLocaleChanged;
   set onLocaleChanged(VoidCallback? callback);
 
-  String get initialLifecycleState => 'AppLifecycleState.resumed';
+  String get initialLifecycleState => '';
 
   bool get alwaysUse24HourFormat;
 
@@ -247,10 +247,11 @@ class FrameTiming {
 }
 
 enum AppLifecycleState {
+  detached,
   resumed,
   inactive,
+  hidden,
   paused,
-  detached,
 }
 
 enum AppExitResponse {
