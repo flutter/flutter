d64b094e4c2f125460d3fe092a2baf77ae80ce83
Bartek Pacia <barpac02@gmail.com>
2023-05-31 23:08:25 +0200

    hack around `com.android.build.tools` not being resoled at runtime in FGP

--- packages/flutter_tools/gradle/src/main/groovy/flutter.groovy
+++ packages/flutter_tools/gradle/src/main/groovy/flutter.groovy
@@ -913,7 +913,7 @@ class FlutterPlugin implements Plugin<Project> {
                     // for only the output APK, not for the variant itself. Skipping this step simply
                     // causes Gradle to use the value of variant.versionCode for the APK.
                     // For more, see https://developer.android.com/studio/build/configure-apk-splits
-                    def abiVersionCode = ABI_VERSION.get(output.getFilter(OutputFile.ABI))
+                    def abiVersionCode = ABI_VERSION.get("ABI")
                     if (abiVersionCode != null) {
                         output.versionCodeOverride =
                             abiVersionCode * 1000 + variant.versionCode
@@ -1056,7 +1056,7 @@ class FlutterPlugin implements Plugin<Project> {
                             ? outputDirectory.get()
                             : outputDirectory
                         String filename = "app"
-                        String abi = output.getFilter(OutputFile.ABI)
+                        String abi = output.getFilter("ABI")
                         if (abi != null && !abi.isEmpty()) {
                             filename += "-${abi}"
                         }
