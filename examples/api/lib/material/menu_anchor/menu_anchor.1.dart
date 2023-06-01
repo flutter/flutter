c777bafa5a884b02b8bb0952f0fa3a11f204c321
Hans Muller <hans.muller@gmail.com>
2023-06-01 15:27:34 -0700

    Updated Menu examples (#128080)

--- examples/api/lib/material/menu_anchor/menu_anchor.1.dart
+++ examples/api/lib/material/menu_anchor/menu_anchor.1.dart
@@ -201,8 +201,9 @@ class ContextMenuApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(body: MyContextMenu(message: kMessage)),
+    return MaterialApp(
+      theme: ThemeData(useMaterial3: true),
+      home: const Scaffold(body: MyContextMenu(message: kMessage)),
     );
   }
 }
