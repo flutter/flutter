f6c1d9510e71d3eaed461392488aa47740af7d0d
Andrew Kolos <andrewrkolos@gmail.com>
2023-06-05 11:26:56 -0700

    lint

--- packages/flutter_tools/test/general.shard/android/android_sdk_test.dart
+++ packages/flutter_tools/test/general.shard/android/android_sdk_test.dart
@@ -5,10 +5,8 @@
 import 'package:file/memory.dart';
 import 'package:flutter_tools/src/android/android_sdk.dart';
 import 'package:flutter_tools/src/android/android_studio.dart';
-import 'package:flutter_tools/src/android/java.dart';
 import 'package:flutter_tools/src/base/config.dart';
 import 'package:flutter_tools/src/base/file_system.dart';
-import 'package:flutter_tools/src/base/logger.dart';
 import 'package:flutter_tools/src/base/platform.dart';
 import 'package:flutter_tools/src/globals.dart' as globals;
 import 'package:test/fake.dart';
