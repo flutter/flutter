84b0450da7ad08af405604fe0cd9354d7c9e9f92
Pierre-Louis Guidez <plg@google.com>
2023-06-05 15:03:48 +0200

    update

--- packages/flutter/lib/src/material/action_chip.dart
+++ packages/flutter/lib/src/material/action_chip.dart
@@ -214,8 +214,6 @@ class ActionChip extends StatelessWidget implements ChipAttributes, TappableChip
 // Design token database by the script:
 //   dev/tools/gen_defaults/bin/gen_defaults.dart.
 
-// Token database version: v0_162
-
 class _ActionChipDefaultsM3 extends ChipThemeData {
   _ActionChipDefaultsM3(this.context, this.isEnabled, this._chipVariant)
     : super(
