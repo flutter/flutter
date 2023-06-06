3ca91bfd2e5fb0e5f15f834921fa621ddb9c3e0a
Jackson Gardner <jacksongardner@google.com>
2023-06-06 11:38:56 -0700

    Revert a few more changes.

--- dev/benchmarks/macrobenchmarks/lib/src/web/recorder.dart
+++ dev/benchmarks/macrobenchmarks/lib/src/web/recorder.dart
@@ -3,11 +3,8 @@
 // found in the LICENSE file.
 
 import 'dart:async';
-import 'dart:js_interop';
-// The analyzer currently thinks `js_interop_unsafe` is unused, but it is used
-// for `JSObject.[]=`.
-// ignore: unused_import
-import 'dart:js_interop_unsafe';
+import 'dart:html' as html;
+import 'dart:js_util' as js_util;
 import 'dart:math' as math;
 import 'dart:ui';
 
@@ -18,7 +15,6 @@ import 'package:flutter/scheduler.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:meta/meta.dart';
-import 'package:web/web.dart' as web;
 
 /// The default number of samples from warm-up iterations.
 ///
@@ -659,8 +655,7 @@ class Timeseries {
     final double dirtyStandardDeviation = _computeStandardDeviationForPopulation(name, candidateValues);
 
     // Any value that's higher than this is considered an outlier.
-    // Two standard deviations captures 95% of a normal distribution.
-    final double outlierCutOff = dirtyAverage + dirtyStandardDeviation * 2;
+    final double outlierCutOff = dirtyAverage + dirtyStandardDeviation;
 
     // Candidates with outliers removed.
     final Iterable<double> cleanValues = candidateValues.where((double value) => value <= outlierCutOff);
@@ -1258,7 +1253,7 @@ void startMeasureFrame(Profile profile) {
 
   if (!profile.isWarmingUp) {
     // Tell the browser to mark the beginning of the frame.
-    web.window.performance.mark('measured_frame_start#$_currentFrameNumber'.toJS);
+    html.window.performance.mark('measured_frame_start#$_currentFrameNumber');
 
     _isMeasuringFrame = true;
   }
@@ -1281,11 +1276,11 @@ void endMeasureFrame() {
 
   if (_isMeasuringFrame) {
     // Tell the browser to mark the end of the frame, and measure the duration.
-    web.window.performance.mark('measured_frame_end#$_currentFrameNumber'.toJS);
-    web.window.performance.measure(
-      'measured_frame'.toJS,
-      'measured_frame_start#$_currentFrameNumber'.toJS,
-      'measured_frame_end#$_currentFrameNumber'.toJS,
+    html.window.performance.mark('measured_frame_end#$_currentFrameNumber');
+    html.window.performance.measure(
+      'measured_frame',
+      'measured_frame_start#$_currentFrameNumber',
+      'measured_frame_end#$_currentFrameNumber',
     );
 
     // Increment the current frame number.
@@ -1315,10 +1310,7 @@ void registerEngineBenchmarkValueListener(String name, EngineBenchmarkValueListe
 
   if (_engineBenchmarkListeners.isEmpty) {
     // The first listener is being registered. Register the global listener.
-    web.window['_flutter_internal_on_benchmark'.toJS] =
-        // Upcast to [Object] to export.
-        // ignore: unnecessary_cast
-        (_dispatchEngineBenchmarkValue as Object).toJS;
+    js_util.setProperty(html.window, '_flutter_internal_on_benchmark', _dispatchEngineBenchmarkValue);
   }
 
   _engineBenchmarkListeners[name] = listener;
@@ -1329,7 +1321,7 @@ void stopListeningToEngineBenchmarkValues(String name) {
   _engineBenchmarkListeners.remove(name);
   if (_engineBenchmarkListeners.isEmpty) {
     // The last listener unregistered. Remove the global listener.
-    web.window['_flutter_internal_on_benchmark'.toJS] = null;
+    js_util.setProperty(html.window, '_flutter_internal_on_benchmark', null);
   }
 }
 
