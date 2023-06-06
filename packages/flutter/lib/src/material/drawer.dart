dd9452a572d505def576c17bb1136d95a31fc700
Leigha Jarett <leigha.jarett@gmail.com>
2023-06-06 15:23:19 -0400

    Adding example for migrating to navigation drawer (#128295)
    Fixes https://github.com/flutter/flutter/issues/127214

--- packages/flutter/lib/src/material/drawer.dart
+++ packages/flutter/lib/src/material/drawer.dart
@@ -130,6 +130,12 @@ const Duration _kBaseSettleDuration = Duration(milliseconds: 246);
 /// ```
 /// {@end-tool}
 ///
+/// {@tool snippet}
+/// This example shows how to migrate the above [Drawer] to a [NavigationDrawer].
+///
+/// See code in examples/api/lib/material/navigation_drawer/navigation_drawer.1.dart **
+/// {@end-tool}
+///
 /// An open drawer may be closed with a swipe to close gesture, pressing the
 /// escape key, by tapping the scrim, or by calling pop route function such as
 /// [Navigator.pop]. For example a drawer item might close the drawer when tapped:
