bd72a3197cccf0c330fb159d554249ba47509015
Greg Spencer <gspencer@google.com>
2023-06-06 16:22:35 -0700

    Disable context menu in example on macOS and iOS

--- examples/api/lib/material/menu_anchor/menu_anchor.1.dart
+++ examples/api/lib/material/menu_anchor/menu_anchor.1.dart
@@ -3,6 +3,7 @@
 // found in the LICENSE file.
 
 import 'package:flutter/foundation.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
