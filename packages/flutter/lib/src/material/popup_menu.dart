6d619b46ac981d5e428df89087d9901b86e0751c
Mahdi Bagheri <53684884+mhbdev@users.noreply.github.com>
2023-06-06 14:24:02 +0330

    Update popup_menu.dart

--- packages/flutter/lib/src/material/popup_menu.dart
+++ packages/flutter/lib/src/material/popup_menu.dart
@@ -339,8 +339,7 @@ class PopupMenuItemState<T, W extends PopupMenuItem<T>> extends State<W> {
   /// the menu route.
   @protected
   void handleTap() {
-    // Need to pop the navigator first in case onTap may push new route 
-    // onto navigator.
+    // Need to pop the navigator first in case onTap may push new route onto navigator.
     Navigator.pop<T>(context, widget.value);
 
     widget.onTap?.call();
