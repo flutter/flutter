a645ff310cb5e245debdbacf4b4841dbc2bd6299
Leigha Jarett <leigha.jarett@gmail.com>
2023-06-06 13:33:53 -0400

    Removing comment

--- examples/api/lib/material/navigation_drawer/navigation_drawer.1.dart
+++ examples/api/lib/material/navigation_drawer/navigation_drawer.1.dart
@@ -2,11 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// Builds an adaptive navigation widget layout. When the screen width is less than
-// 450, A [NavigationBar] will be displayed. Otherwise, a [NavigationRail] will be
-// displayed on the left side, and also a button to open the [NavigationDrawer].
-// All of these navigation widgets are built from an identical list of data.
-
 import 'package:flutter/material.dart';
 
 /// Flutter code sample for [NavigationDrawer].
