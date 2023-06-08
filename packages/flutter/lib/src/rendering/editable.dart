29419abd704cf4147718aecaa10c8d6d83b624d7
hellohuanlin <41930132+hellohuanlin@users.noreply.github.com>
2023-06-08 13:03:49 -0700

    [floating_cursor_selection]add more comments on the tricky part (#127227)
    Many parts of the floating cursor selection feature is pretty tricky. Some took me a while to figure out. So I added some comments to explain a bit for future readers.
    
    *List which issues are fixed by this PR. You must list at least one issue.*
    
    https://github.com/flutter/flutter/issues/30476
    
    *If you had to change anything in the [flutter/tests] repo, include a link to the migration guide as per the [breaking change policy].*

--- packages/flutter/lib/src/rendering/editable.dart
+++ packages/flutter/lib/src/rendering/editable.dart
@@ -1269,7 +1269,9 @@ class RenderEditable extends RenderBox with RelayoutWhenSystemFontsChangeMixin, 
   // [assembleSemanticsNode] invocations.
   LinkedHashMap<Key, SemanticsNode>? _cachedChildNodes;
 
-  /// Returns a list of rects that bound the given selection.
+  /// Returns a list of rects that bound the given selection, and the text
+  /// direction. The text direction is used by the engine to calculate
+  /// the closest position to a given point.
   ///
   /// See [TextPainter.getBoxesForSelection] for more details.
   List<TextBox> getBoxesForSelection(TextSelection selection) {
