b1fa42d81e612adea819f2e9103b6bb1f0d6c07d
Mitchell Kember <mk12360@gmail.com>
2023-06-08 18:56:49 -0700

    [fuchsia] Bump the target API level to 12, and pass it to fidlc (flutter/engine#42667)
    This makes the GN build pass the target Fuchsia API level to fidlc.
    Currently it relies on the default of HEAD, meaning all FIDL changes are
    immediately visible when the SDK rolls. This was never the intention:
    flutter should be targeting a specific API level.
    
    This unblocks https://fxrev.dev/864297 which makes the fidlc --available
    flag required, and which uncovered this problem.
    
    I also copied the changes to gen_response_file.py from
    https://fxrev.dev/865020 to make it forward --available to fidlc.

--- engine/src/flutter/tools/fuchsia/fidl/gen_response_file.py
+++ engine/src/flutter/tools/fuchsia/fidl/gen_response_file.py
@@ -4,128 +4,53 @@
 # found in the LICENSE file.
 
 import argparse
-import os
-import string
-import sys
-
-
-def read_libraries(libraries_path):
-  with open(libraries_path) as f:
-    lines = f.readlines()
-    return [l.rstrip("\n") for l in lines]
-
-
-def write_libraries(libraries_path, libraries):
-  directory = os.path.dirname(libraries_path)
-  if not os.path.exists(directory):
-    os.makedirs(directory)
-  with open(libraries_path, "w+") as f:
-    for library in libraries:
-      f.write(library)
-      f.write("\n")
+from pathlib import Path
 
 
 def main():
   parser = argparse.ArgumentParser(
-      description="Generate response file for FIDL frontend"
+      description="Generate response file for FIDL frontend. "
+      "Arguments not mentioned here are forwarded as is to fidlc."
   )
   parser.add_argument(
       "--out-response-file",
-      help="The path for the response file to generate",
+      help="The path for for the response file to generate",
+      type=Path,
       required=True
   )
   parser.add_argument(
       "--out-libraries",
-      help="The path for the libraries file to generate",
+      help="The path for for the libraries file to generate",
+      type=Path,
       required=True
   )
   parser.add_argument(
-      "--json", help="The path for the JSON file to generate, if any"
+      "--sources", help="List of FIDL source files", nargs="+", required=True
   )
-  parser.add_argument(
-      "--tables", help="The path for the tables file to generate, if any"
-  )
-  parser.add_argument(
-      "--deprecated-fuchsia-only-c-client",
-      help="The path for the C simple client file to generate, if any"
-  )
-  parser.add_argument(
-      "--deprecated-fuchsia-only-c-header",
-      help="The path for the C header file to generate, if any"
-  )
-  parser.add_argument(
-      "--deprecated-fuchsia-only-c-server",
-      help="The path for the C simple server file to generate, if any"
-  )
-  parser.add_argument(
-      "--name", help="The name for the generated FIDL library, if any"
-  )
-  parser.add_argument(
-      "--depfile", help="The name for the generated depfile, if any"
-  )
-  parser.add_argument("--sources", help="List of FIDL source files", nargs="*")
   parser.add_argument(
       "--dep-libraries", help="List of dependent libraries", nargs="*"
   )
-  parser.add_argument(
-      "--experimental-flag", help="List of experimental flags", action="append"
-  )
-  args = parser.parse_args()
+  args, args_to_forward = parser.parse_known_args()
 
-  target_libraries = []
+  # Each line contains a library's source files separated by spaces.
+  # We use a dict instead of a set to maintain insertion order.
+  dep_lines = {}
+  for path in args.dep_libraries or []:
+    with open(path) as f:
+      for line in f:
+        dep_lines[line.rstrip()] = True
+  libraries = list(dep_lines)
+  libraries.append(" ".join(sorted(args.sources)))
 
-  for dep_libraries_path in args.dep_libraries or []:
-    dep_libraries = read_libraries(dep_libraries_path)
-    for library in dep_libraries:
-      if library in target_libraries:
-        continue
-      target_libraries.append(library)
+  args.out_libraries.parent.mkdir(parents=True, exist_ok=True)
+  with open(args.out_libraries, "w") as f:
+    print("\n".join(libraries), file=f)
 
-  target_libraries.append(" ".join(sorted(args.sources)))
-  write_libraries(args.out_libraries, target_libraries)
-
-  response_file = []
-
-  if args.json:
-    response_file.append("--json %s" % args.json)
-
-  if args.tables:
-    response_file.append("--tables %s" % args.tables)
-
-  if args.deprecated_fuchsia_only_c_client:
-    response_file.append(
-        "--deprecated-fuchsia-only-c-client %s" %
-        args.deprecated_fuchsia_only_c_client
-    )
-
-  if args.deprecated_fuchsia_only_c_header:
-    response_file.append(
-        "--deprecated-fuchsia-only-c-header %s" %
-        args.deprecated_fuchsia_only_c_header
-    )
-
-  if args.deprecated_fuchsia_only_c_server:
-    response_file.append(
-        "--deprecated-fuchsia-only-c-server %s" %
-        args.deprecated_fuchsia_only_c_server
-    )
-
-  if args.name:
-    response_file.append("--name %s" % args.name)
-
-  if args.depfile:
-    response_file.append("--depfile %s" % args.depfile)
-
-  if args.experimental_flag:
-    for experimental_flag in args.experimental_flag:
-      response_file.append("--experimental %s" % experimental_flag)
-
-  response_file.extend(["--files %s" % library for library in target_libraries])
-
-  with open(args.out_response_file, "w+") as f:
-    f.write(" ".join(response_file))
-    f.write("\n")
+  args.out_response_file.parent.mkdir(parents=True, exist_ok=True)
+  with open(args.out_response_file, "w") as f:
+    fidlc_args = args_to_forward + ["--files " + line for line in libraries]
+    print(" ".join(fidlc_args), file=f)
 
 
 if __name__ == "__main__":
-  sys.exit(main())
+  main()
