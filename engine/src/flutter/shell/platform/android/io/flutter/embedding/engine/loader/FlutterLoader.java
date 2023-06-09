5d2c84a85c996a960acb4cf15986a27691cc2ca1
gaaclarke <30870216+gaaclarke@users.noreply.github.com>
2023-06-09 18:35:29 +0000

    [Impeller] Reland: Added a switch to turn on vulkan (flutter/engine#42669)
    relands https://github.com/flutter/engine/pull/42585
    
    ## Pre-launch Checklist
    
    - [x] I read the [Contributor Guide] and followed the process outlined
    there for submitting PRs.
    - [x] I read the [Tree Hygiene] wiki page, which explains my
    responsibilities.
    - [x] I read and followed the [Flutter Style Guide] and the [C++,
    Objective-C, Java style guides].
    - [ ] I listed at least one issue that this PR fixes in the description
    above.
    - [ ] I added new tests to check the change I am making or feature I am
    adding, or Hixie said the PR is test-exempt. See [testing the engine]
    for instructions on writing and running engine tests.
    - [x] I updated/added relevant documentation (doc comments with `///`).
    - [x] I signed the [CLA].
    - [x] All existing and new tests are passing.
    
    If you need help, consider asking for advice on the #hackers-new channel
    on [Discord].
    
    <!-- Links -->
    [Contributor Guide]:
    https://github.com/flutter/flutter/wiki/Tree-hygiene#overview
    [Tree Hygiene]: https://github.com/flutter/flutter/wiki/Tree-hygiene
    [Flutter Style Guide]:
    https://github.com/flutter/flutter/wiki/Style-guide-for-Flutter-repo
    [C++, Objective-C, Java style guides]:
    https://github.com/flutter/engine/blob/main/CONTRIBUTING.md#style
    [testing the engine]:
    https://github.com/flutter/flutter/wiki/Testing-the-engine
    [CLA]: https://cla.developers.google.com/
    [flutter/tests]: https://github.com/flutter/tests
    [breaking change policy]:
    https://github.com/flutter/flutter/wiki/Tree-hygiene#handling-breaking-changes
    [Discord]: https://github.com/flutter/flutter/wiki/Chat

--- engine/src/flutter/shell/platform/android/io/flutter/embedding/engine/loader/FlutterLoader.java
+++ engine/src/flutter/shell/platform/android/io/flutter/embedding/engine/loader/FlutterLoader.java
@@ -43,6 +43,8 @@ public class FlutterLoader {
       "io.flutter.embedding.android.EnableImpeller";
   private static final String ENABLE_VULKAN_VALIDATION_META_DATA_KEY =
       "io.flutter.embedding.android.EnableVulkanValidation";
+  private static final String IMPELLER_BACKEND_META_DATA_KEY =
+      "io.flutter.embedding.android.ImpellerBackend";
 
   /**
    * Set whether leave or clean up the VM after the last shell shuts down. It can be set from app's
@@ -325,6 +327,8 @@ public class FlutterLoader {
         if (metaData.getBoolean(ENABLE_VULKAN_VALIDATION_META_DATA_KEY, false)) {
           shellArgs.add("--enable-vulkan-validation");
         }
+        String backend = metaData.getString(IMPELLER_BACKEND_META_DATA_KEY, "opengles");
+        shellArgs.add("--impeller-backend=" + backend);
       }
 
       final String leakVM = isLeakVM(metaData) ? "true" : "false";
