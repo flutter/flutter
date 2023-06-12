c5d4f682ef4d191548b439fbba2370df895416ac
Christopher Fujino <christopherfujino@gmail.com>
2023-06-12 13:32:14 -0700

    migrate test

--- packages/flutter_tools/test/commands.shard/hermetic/doctor_test.dart
+++ packages/flutter_tools/test/commands.shard/hermetic/doctor_test.dart
@@ -806,7 +806,6 @@ class FakeAndroidWorkflow extends Fake implements AndroidWorkflow {
   final bool appliesToHostPlatform;
 }
 
-
 class NoOpDoctor implements Doctor {
   @override
   bool get canLaunchAnything => true;
