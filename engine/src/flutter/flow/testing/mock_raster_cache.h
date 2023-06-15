b50910b277f9961fba326d4d7bcef640ace7a0f4
Matej Knopp <matej.knopp@gmail.com>
2023-06-15 17:25:53 +0200

    Raster cache should preserve RTree for overlay layers (flutter/engine#42552)
    Fixes https://github.com/flutter/flutter/issues/116069
    
    When raster cache paints to a layer over platform view, it will draw the
    individual rectangles from the content RTree, to ensure that the
    destination layer RTree is not clobbered. This is necessary for raster
    cache to not break unobstructed platform views and/or hit testing.
    
    When raster cache paints in the root (bottom-most) layer the behavior it
    just copies the entire image as it did before. This is to minimise risk
    of performance regressions.
    
    ## Pre-launch Checklist
    
    - [X] I read the [Contributor Guide] and followed the process outlined
    there for submitting PRs.
    - [X] I read the [Tree Hygiene] wiki page, which explains my
    responsibilities.
    - [X] I read and followed the [Flutter Style Guide] and the [C++,
    Objective-C, Java style guides].
    - [X] I listed at least one issue that this PR fixes in the description
    above.
    - [X] I added new tests to check the change I am making or feature I am
    adding, or Hixie said the PR is test-exempt. See [testing the engine]
    for instructions on writing and running engine tests.
    - [X] I updated/added relevant documentation (doc comments with `///`).
    - [X] I signed the [CLA].
    - [X] All existing and new tests are passing.
    
    If you need help, consider asking for advice on the #hackers-new channel
    on [Discord].
    
    <!-- Links -->
    [Contributor Guide]:
    https://github.com/flutter/flutter/wiki/Tree-hygiene#overview
    [Tree Hygiene]: https://github.com/flutter/flutter/wiki/Tree-hygiene
    [Flutter Style Guide]:
    https://github.com/flutter/flutter/wiki/Style-guide-for-Flutter-repo
    [C++, Objective-C, Java style guides]:
    https://github.com/flutter/engine/blob/main/CONTRIBUTING.md#style
    [testing the engine]:
    https://github.com/flutter/flutter/wiki/Testing-the-engine
    [CLA]: https://cla.developers.google.com/
    [flutter/tests]: https://github.com/flutter/tests
    [breaking change policy]:
    https://github.com/flutter/flutter/wiki/Tree-hygiene#handling-breaking-changes
    [Discord]: https://github.com/flutter/flutter/wiki/Chat

--- engine/src/flutter/flow/testing/mock_raster_cache.h
+++ engine/src/flutter/flow/testing/mock_raster_cache.h
@@ -29,7 +29,9 @@ class MockRasterCacheResult : public RasterCacheResult {
  public:
   explicit MockRasterCacheResult(SkRect device_rect);
 
-  void draw(DlCanvas& canvas, const DlPaint* paint = nullptr) const override{};
+  void draw(DlCanvas& canvas,
+            const DlPaint* paint = nullptr,
+            bool preserve_rtree = false) const override{};
 
   SkISize image_dimensions() const override {
     return SkSize::Make(device_rect_.width(), device_rect_.height()).toCeil();
