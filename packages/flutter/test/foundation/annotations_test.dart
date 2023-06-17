9bbc21bf6dea8469c1c92fb20a4ef15ed4527260
Myles Moylan <moylanm@oregonstate.edu>
2023-06-17 10:26:10 -0700

    revise test names

--- packages/flutter/test/foundation/annotations_test.dart
+++ packages/flutter/test/foundation/annotations_test.dart
@@ -2,12 +2,12 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  test('test DocumentationIcon object', () {
+  test('test DocumentationIcon constructor', () {
     const DocumentationIcon docIcon = DocumentationIcon('Test String');
     expect(docIcon.url, contains('Test String'));
   });
 
-  test('test Summary object', () {
+  test('test Summary constructor', () {
     const Summary summary = Summary('Test String');
     expect(summary.text, contains('Test String'));
   });
