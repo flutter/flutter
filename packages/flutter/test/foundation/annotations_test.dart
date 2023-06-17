0ca3f58cb23947c2817a9030eeb731e48d577d6e
Myles Moylan <moylanm@oregonstate.edu>
2023-06-17 10:26:42 -0700

    add test for Category annotation

--- packages/flutter/test/foundation/annotations_test.dart
+++ packages/flutter/test/foundation/annotations_test.dart
@@ -2,6 +2,15 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
+  test('test Category constructor', () {
+    const List<String> sections = [
+       'First section',
+       'Second section',
+       'Third section'
+      ];
+    const Category category = Category(sections);
+    expect(category.sections, sections);
+  });
   test('test DocumentationIcon constructor', () {
     const DocumentationIcon docIcon = DocumentationIcon('Test String');
     expect(docIcon.url, contains('Test String'));
