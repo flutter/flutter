5e04fce9712fd5344216bd970fa29eb94e49a2b5
Yegor Jbanov <yjbanov@google.com>
2023-06-21 11:21:28 -0700

    merge with JS-interop changes

--- packages/flutter/lib/src/foundation/_timeline_web.dart
+++ packages/flutter/lib/src/foundation/_timeline_web.dart
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:js/js.dart';
+import 'dart:js_interop';
 
 /// Returns the current timestamp in microseconds from a monotonically
 /// increasing clock.
