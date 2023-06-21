a356194af04e73101f34d5467d1376bb6b457265
Bruno Leroux <leroux_bruno@yahoo.fr>
2023-06-21 07:49:06 +0200

    [Android] Return the keyboard pressed state (flutter/engine#42758)
    ## Description
    
    This PR updates the Android engine in order to answer to keyboard pressed state queries from the framework (as implemented in https://github.com/flutter/flutter/pull/122885).
    
    This is a rework of https://github.com/flutter/engine/pull/41695 which was reverted in https://github.com/flutter/engine/pull/42346.
    
    This issue with https://github.com/flutter/engine/pull/41695 was that the framework side did not get an answer when the channel was setup in the engine without registering a handler (on the engine side) to handle framework requests. The issue was reproducible when the engine initialization was managed by the app (see https://github.com/flutter/flutter/issues/122441#issuecomment-1582052616 for a repro).
    
    This PR fixes this issue by changing `flutter/keyboard` lifecycle: the engine now creates the channel and registers a handler just after the channel creation.
    In order to avoid regression, this PR also updates the channel implemenation (see `KeyboardChannel`) to return an empty `HashMap` when there is no handler registered.
    
    ## Related Issue
    
    Android engine implementation for https://github.com/flutter/flutter/issues/87391
    (see https://github.com/flutter/engine/pull/42346 for Linux implementation)
    Fixes https://github.com/flutter/flutter/issues/122441
    
    ## Tests
    
    Adds 3 tests.

---
+++ engine/src/flutter/shell/platform/android/test/io/flutter/embedding/engine/systemchannels/KeyboardChannelTest.java
@@ -0,0 +1,67 @@
+package io.flutter.embedding.android;
+
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import io.flutter.embedding.engine.dart.DartExecutor;
+import io.flutter.embedding.engine.systemchannels.KeyboardChannel;
+import io.flutter.plugin.common.BinaryMessenger;
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugin.common.StandardMethodCodec;
+import java.nio.ByteBuffer;
+import java.util.HashMap;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.robolectric.annotation.Config;
+
+@Config(manifest = Config.NONE)
+@RunWith(AndroidJUnit4.class)
+public class KeyboardChannelTest {
+
+  private static BinaryMessenger.BinaryReply sendToBinaryMessageHandler(
+      BinaryMessenger.BinaryMessageHandler binaryMessageHandler, String method, Object args) {
+    MethodCall methodCall = new MethodCall(method, args);
+    ByteBuffer encodedMethodCall = StandardMethodCodec.INSTANCE.encodeMethodCall(methodCall);
+    BinaryMessenger.BinaryReply reply = mock(BinaryMessenger.BinaryReply.class);
+    binaryMessageHandler.onMessage((ByteBuffer) encodedMethodCall.flip(), reply);
+    return reply;
+  }
+
+  @Test
+  public void respondsToGetKeyboardStateChannelMessage() {
+    ArgumentCaptor<BinaryMessenger.BinaryMessageHandler> binaryMessageHandlerCaptor =
+        ArgumentCaptor.forClass(BinaryMessenger.BinaryMessageHandler.class);
+    DartExecutor mockBinaryMessenger = mock(DartExecutor.class);
+    KeyboardChannel.KeyboardMethodHandler mockHandler =
+        mock(KeyboardChannel.KeyboardMethodHandler.class);
+    KeyboardChannel keyboardChannel = new KeyboardChannel(mockBinaryMessenger);
+
+    verify(mockBinaryMessenger, times(1))
+        .setMessageHandler(any(String.class), binaryMessageHandlerCaptor.capture());
+
+    BinaryMessenger.BinaryMessageHandler binaryMessageHandler =
+        binaryMessageHandlerCaptor.getValue();
+
+    keyboardChannel.setKeyboardMethodHandler(mockHandler);
+    sendToBinaryMessageHandler(binaryMessageHandler, "getKeyboardState", null);
+
+    verify(mockHandler, times(1)).getKeyboardState();
+  }
+
+  @Test
+  public void repliesWhenNoKeyboardChannelHandler() {
+    // Regression test for https://github.com/flutter/flutter/issues/122441#issuecomment-1582052616.
+
+    KeyboardChannel keyboardChannel = new KeyboardChannel(mock(DartExecutor.class));
+    MethodCall methodCall = new MethodCall("getKeyboardState", null);
+    MethodChannel.Result result = mock(MethodChannel.Result.class);
+    keyboardChannel.parsingMethodHandler.onMethodCall(methodCall, result);
+
+    verify(result).success(new HashMap());
+  }
+}
