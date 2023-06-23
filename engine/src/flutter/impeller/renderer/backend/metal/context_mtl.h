85b2589b20d4a965975837202017f61b331bb1b8
Brandon DeRosier <bdero@google.com>
2023-06-23 16:59:33 -0700

    [Impeller] Label present waiter command buffer in SurfaceMTL (flutter/engine#43165)
    Just to curb confusion and make it searchable. I double-take this lingering command buffer in frame captures quite a bit.

--- engine/src/flutter/impeller/renderer/backend/metal/context_mtl.h
+++ engine/src/flutter/impeller/renderer/backend/metal/context_mtl.h
@@ -84,7 +84,7 @@ class ContextMTL final : public Context,
   // |Context|
   bool UpdateOffscreenLayerPixelFormat(PixelFormat format) override;
 
-  id<MTLCommandBuffer> CreateMTLCommandBuffer() const;
+  id<MTLCommandBuffer> CreateMTLCommandBuffer(const std::string& label) const;
 
   const std::shared_ptr<fml::ConcurrentTaskRunner>& GetWorkerTaskRunner() const;
 
