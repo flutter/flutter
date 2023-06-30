f8f602c12a2fe1744ad235540dc7b287a3d80546
Brandon DeRosier <bdero@google.com>
2023-06-29 17:22:10 -0700

    [Impeller] Assign missing user to TODO. (#43351)
    The linter is failing on this in my unrelated PR for some reason: https://github.com/flutter/engine/pull/43348
    
    ```
    â Failures for clang-tidy on /Volumes/Work/s/w/ir/cache/builder/src/flutter/impeller/renderer/backend/metal/vertex_descriptor_mtl.mm:
    /Volumes/Work/s/w/ir/cache/builder/src/flutter/impeller/renderer/backend/metal/vertex_descriptor_mtl.mm:177:3: error: missing username/bug in TODO [google-readability-todo,-warnings-as-errors]
      // TODO: its odd that we offset buffers from the max index on metal
      ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
      // TODO(chrome-bot): its odd that we offset buffers from the max index on metal
    Suppressed 1476 warnings (1475 in non-user code, 1 NOLINT).
    Use -header-filter=.* to display errors from all non-system headers. Use -system-headers to display errors from system headers as well.
    1 warning treated as error
    ```

--- impeller/renderer/backend/metal/vertex_descriptor_mtl.mm
+++ impeller/renderer/backend/metal/vertex_descriptor_mtl.mm
@@ -174,8 +174,8 @@ bool VertexDescriptorMTL::SetStageInputsAndLayout(
     const std::vector<ShaderStageBufferLayout>& layouts) {
   auto descriptor = descriptor_ = [MTLVertexDescriptor vertexDescriptor];
 
-  // TODO: its odd that we offset buffers from the max index on metal
-  // but not on GLES or Vulkan. We should probably consistently start
+  // TODO(jonahwilliams): its odd that we offset buffers from the max index on
+  // metal but not on GLES or Vulkan. We should probably consistently start
   // these at zero?
   for (size_t i = 0; i < inputs.size(); i++) {
     const auto& input = inputs[i];
