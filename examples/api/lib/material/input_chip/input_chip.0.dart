5d5f69660a37dddb93f1107c6b6e2fb0ced2ffb9
AdamMusa <adammusa2222@gmail.com>
2023-07-05 22:52:11 +0100

    Update input_chip.0.dart
    undo dart format

--- examples/api/lib/material/input_chip/input_chip.0.dart
+++ examples/api/lib/material/input_chip/input_chip.0.dart
@@ -15,7 +15,8 @@ class ChipApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-          colorSchemeSeed: const Color(0xff6750a4), useMaterial3: true),
+        colorSchemeSeed: const Color(0xff6750a4),
+        useMaterial3: true),
       home: const InputChipExample(),
     );
   }
