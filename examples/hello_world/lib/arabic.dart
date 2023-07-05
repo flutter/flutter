d38d6dd0bdbba74d97d1d4b1b6b94b1a686bf295
AdamMusa <adammusa2222@gmail.com>
2023-07-05 22:12:50 +0100

    Update arabic.dart
    undo the minimum hello world example app

--- examples/hello_world/lib/arabic.dart
+++ examples/hello_world/lib/arabic.dart
@@ -1,14 +1,5 @@
 // Copyright 2014 The Flutter Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-
-import 'package:flutter/material.dart';
-
-void main() => runApp(
-  MaterialApp(
-     title: 'برنامج أهلا بالعالم',
-      theme: ThemeData(
-        primarySwatch: Colors.grey,
-        useMaterial3: true,
-      ),
-    home: const Scaffold(body: Center(child: Text('برنامج أهلا بالعالم', textDirection: TextDirection.rtl)))));
+import 'package:flutter/widgets.dart';
+void main() => runApp(const Center(child: Text('برنامج أهلا بالعالم', textDirection: TextDirection.rtl)));
