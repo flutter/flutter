a333c1c99d926cc061de6ccaee08131d04cf74c7
Rydmike <m.rydstrom@gmail.com>
2023-07-10 18:43:00 +0300

    Fix default icon color constants reversed brightness documentation (#130231)
    Fixes the reversed default icon color constants brightness documentation for `kDefaultIconLightColor` and `kDefaultIconDarkColor`.
    
    FIX: https://github.com/flutter/flutter/issues/130230

--- packages/flutter/lib/src/material/constants.dart
+++ packages/flutter/lib/src/material/constants.dart
@@ -51,11 +51,11 @@ const EdgeInsets kTabLabelPadding = EdgeInsets.symmetric(horizontal: 16.0);
 const EdgeInsets kMaterialListPadding = EdgeInsets.symmetric(vertical: 8.0);
 
 /// The default color for [ThemeData.iconTheme] when [ThemeData.brightness] is
-/// [Brightness.light]. This color is used in [IconButton] to detect whether
+/// [Brightness.dark]. This color is used in [IconButton] to detect whether
 /// [IconTheme.of(context).color] is the same as the default color of [ThemeData.iconTheme].
 const Color kDefaultIconLightColor = Colors.white;
 
 /// The default color for [ThemeData.iconTheme] when [ThemeData.brightness] is
-/// [Brightness.dark]. This color is used in [IconButton] to detect whether
+/// [Brightness.light]. This color is used in [IconButton] to detect whether
 /// [IconTheme.of(context).color] is the same as the default color of [ThemeData.iconTheme].
 const Color kDefaultIconDarkColor = Colors.black87;
