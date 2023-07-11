6b1be4903ed0b45b34ab071d919468af5c878671
AdamMusa <adammusa2222@gmail.com>
2023-07-11 08:38:25 +0100

    Update main.dart

--- examples/image_list/lib/main.dart
+++ examples/image_list/lib/main.dart
@@ -132,6 +132,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
+        primarySwatch: Colors.blue,
         useMaterial3: true,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page', port: port),
