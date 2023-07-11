116a2d901ee476c064a47fa7b7a054cf5893d938
Dan Field <dnfield@google.com>
2023-07-11 11:54:08 -0700

    Remove stray semicolons from embedded_views.cc (flutter/engine#43566)

--- engine/src/flutter/flow/embedded_views.cc
+++ engine/src/flutter/flow/embedded_views.cc
@@ -47,32 +47,32 @@ void ExternalViewEmbedder::SubmitFrame(
     const std::shared_ptr<impeller::AiksContext>& aiks_context,
     std::unique_ptr<SurfaceFrame> frame) {
   frame->Submit();
-};
+}
 
 void MutatorsStack::PushClipRect(const SkRect& rect) {
   std::shared_ptr<Mutator> element = std::make_shared<Mutator>(rect);
   vector_.push_back(element);
-};
+}
 
 void MutatorsStack::PushClipRRect(const SkRRect& rrect) {
   std::shared_ptr<Mutator> element = std::make_shared<Mutator>(rrect);
   vector_.push_back(element);
-};
+}
 
 void MutatorsStack::PushClipPath(const SkPath& path) {
   std::shared_ptr<Mutator> element = std::make_shared<Mutator>(path);
   vector_.push_back(element);
-};
+}
 
 void MutatorsStack::PushTransform(const SkMatrix& matrix) {
   std::shared_ptr<Mutator> element = std::make_shared<Mutator>(matrix);
   vector_.push_back(element);
-};
+}
 
 void MutatorsStack::PushOpacity(const int& alpha) {
   std::shared_ptr<Mutator> element = std::make_shared<Mutator>(alpha);
   vector_.push_back(element);
-};
+}
 
 void MutatorsStack::PushBackdropFilter(
     const std::shared_ptr<const DlImageFilter>& filter,
@@ -80,11 +80,11 @@ void MutatorsStack::PushBackdropFilter(
   std::shared_ptr<Mutator> element =
       std::make_shared<Mutator>(filter, filter_rect);
   vector_.push_back(element);
-};
+}
 
 void MutatorsStack::Pop() {
   vector_.pop_back();
-};
+}
 
 void MutatorsStack::PopTo(size_t stack_count) {
   while (vector_.size() > stack_count) {
@@ -95,22 +95,22 @@ void MutatorsStack::PopTo(size_t stack_count) {
 const std::vector<std::shared_ptr<Mutator>>::const_reverse_iterator
 MutatorsStack::Top() const {
   return vector_.rend();
-};
+}
 
 const std::vector<std::shared_ptr<Mutator>>::const_reverse_iterator
 MutatorsStack::Bottom() const {
   return vector_.rbegin();
-};
+}
 
 const std::vector<std::shared_ptr<Mutator>>::const_iterator
 MutatorsStack::Begin() const {
   return vector_.begin();
-};
+}
 
 const std::vector<std::shared_ptr<Mutator>>::const_iterator MutatorsStack::End()
     const {
   return vector_.end();
-};
+}
 
 bool ExternalViewEmbedder::SupportsDynamicThreadMerging() {
   return false;
