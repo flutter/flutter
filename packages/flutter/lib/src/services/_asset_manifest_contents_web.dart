96d1ccb84665c31115b1ac8bcbf0ec513170a0cd
Andrew Kolos <andrewrkolos@gmail.com>
2023-07-12 13:11:16 -0700

    Update _asset_manifest_contents_web.dart

--- packages/flutter/lib/src/services/_asset_manifest_contents_web.dart
+++ packages/flutter/lib/src/services/_asset_manifest_contents_web.dart
@@ -1,4 +1,4 @@
 import 'dart:js' as js;
 
-final String assetManifestContents =
-  js.context['_flutter_base64EncodedAssetManifest'] as String;
+final String? assetManifestContents =
+  js.context['_flutter_base64EncodedAssetManifest'] as String?;
