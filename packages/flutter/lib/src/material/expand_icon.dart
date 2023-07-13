4f2e9f6339be55e5f4cce97aa5f61269581f7730
Gianluca Bettega <gian_bettega@hotmail.com>
2023-07-12 21:40:29 -0300

    possibility to change the icon of ExpandIcon

--- packages/flutter/lib/src/material/expand_icon.dart
+++ packages/flutter/lib/src/material/expand_icon.dart
@@ -39,6 +39,7 @@ class ExpandIcon extends StatefulWidget {
     this.color,
     this.disabledColor,
     this.expandedColor,
+    this.icon = Icons.expand_more,
   });
 
   /// Whether the icon is in an expanded state.
@@ -94,21 +95,31 @@ class ExpandIcon extends StatefulWidget {
   /// and for [dark theme](https://material.io/design/color/dark-theme.html#ui-application)
   final Color? expandedColor;
 
+  /// The icon to be displayed.
+  ///
+  /// Defaults to [Icons.expand_more].
+  ///
+  /// This property must not be null.
+  final IconData icon;
+
   @override
   State<ExpandIcon> createState() => _ExpandIconState();
 }
 
-class _ExpandIconState extends State<ExpandIcon> with SingleTickerProviderStateMixin {
+class _ExpandIconState extends State<ExpandIcon>
+    with SingleTickerProviderStateMixin {
   late AnimationController _controller;
   late Animation<double> _iconTurns;
 
-  static final Animatable<double> _iconTurnTween = Tween<double>(begin: 0.0, end: 0.5)
-    .chain(CurveTween(curve: Curves.fastOutSlowIn));
+  static final Animatable<double> _iconTurnTween =
+      Tween<double>(begin: 0.0, end: 0.5)
+          .chain(CurveTween(curve: Curves.fastOutSlowIn));
 
   @override
   void initState() {
     super.initState();
-    _controller = AnimationController(duration: kThemeAnimationDuration, vsync: this);
+    _controller =
+        AnimationController(duration: kThemeAnimationDuration, vsync: this);
     _iconTurns = _controller.drive(_iconTurnTween);
     // If the widget is initially expanded, rotate the icon without animating it.
     if (widget.isExpanded) {
@@ -164,8 +175,11 @@ class _ExpandIconState extends State<ExpandIcon> with SingleTickerProviderStateM
   Widget build(BuildContext context) {
     assert(debugCheckHasMaterial(context));
     assert(debugCheckHasMaterialLocalizations(context));
-    final MaterialLocalizations localizations = MaterialLocalizations.of(context);
-    final String onTapHint = widget.isExpanded ? localizations.expandedIconTapHint : localizations.collapsedIconTapHint;
+    final MaterialLocalizations localizations =
+        MaterialLocalizations.of(context);
+    final String onTapHint = widget.isExpanded
+        ? localizations.expandedIconTapHint
+        : localizations.collapsedIconTapHint;
 
     return Semantics(
       onTapHint: widget.onPressed == null ? null : onTapHint,
@@ -177,7 +191,7 @@ class _ExpandIconState extends State<ExpandIcon> with SingleTickerProviderStateM
         onPressed: widget.onPressed == null ? null : _handlePressed,
         icon: RotationTransition(
           turns: _iconTurns,
-          child: const Icon(Icons.expand_more),
+          child: Icon(widget.icon),
         ),
       ),
     );
--- packages/flutter/test/material/expand_icon_test.dart
+++ packages/flutter/test/material/expand_icon_test.dart
@@ -5,7 +5,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-Widget wrap({ required Widget child, ThemeData? theme }) {
+Widget wrap({required Widget child, ThemeData? theme}) {
   return MaterialApp(
     theme: theme,
     home: Center(
@@ -96,7 +96,8 @@ void main() {
     expect(iconTheme.data.color, equals(Colors.white38));
   });
 
-  testWidgets('ExpandIcon test isExpanded does not trigger callback', (WidgetTester tester) async {
+  testWidgets('ExpandIcon test isExpanded does not trigger callback',
+      (WidgetTester tester) async {
     bool expanded = false;
 
     await tester.pumpWidget(wrap(
@@ -119,7 +120,9 @@ void main() {
     expect(expanded, isFalse);
   });
 
-  testWidgets('ExpandIcon is rotated initially if isExpanded is true on first build', (WidgetTester tester) async {
+  testWidgets(
+      'ExpandIcon is rotated initially if isExpanded is true on first build',
+      (WidgetTester tester) async {
     bool expanded = true;
 
     await tester.pumpWidget(wrap(
@@ -130,12 +133,13 @@ void main() {
         },
       ),
     ));
-    final RotationTransition rotation = tester.firstWidget(find.byType(RotationTransition));
+    final RotationTransition rotation =
+        tester.firstWidget(find.byType(RotationTransition));
     expect(rotation.turns.value, 0.5);
   });
 
   testWidgets('ExpandIcon default size is 24', (WidgetTester tester) async {
-    final ExpandIcon expandIcon =  ExpandIcon(
+    final ExpandIcon expandIcon = ExpandIcon(
       onPressed: (bool isExpanded) {},
     );
 
@@ -147,8 +151,9 @@ void main() {
     expect(icon.size, 24);
   });
 
-  testWidgets('ExpandIcon has the correct given size', (WidgetTester tester) async {
-    ExpandIcon expandIcon =  ExpandIcon(
+  testWidgets('ExpandIcon has the correct given size',
+      (WidgetTester tester) async {
+    ExpandIcon expandIcon = ExpandIcon(
       size: 36,
       onPressed: (bool isExpanded) {},
     );
@@ -160,7 +165,7 @@ void main() {
     ExpandIcon icon = tester.firstWidget(find.byWidget(expandIcon));
     expect(icon.size, 36);
 
-    expandIcon =  ExpandIcon(
+    expandIcon = ExpandIcon(
       size: 48,
       onPressed: (bool isExpanded) {},
     );
@@ -173,44 +178,51 @@ void main() {
     expect(icon.size, 48);
   });
 
-  testWidgets('ExpandIcon has correct semantic hints', (WidgetTester tester) async {
+  testWidgets('ExpandIcon has correct semantic hints',
+      (WidgetTester tester) async {
     final SemanticsHandle handle = tester.ensureSemantics();
-    const DefaultMaterialLocalizations localizations = DefaultMaterialLocalizations();
+    const DefaultMaterialLocalizations localizations =
+        DefaultMaterialLocalizations();
     await tester.pumpWidget(wrap(
       theme: ThemeData(useMaterial3: false),
       child: ExpandIcon(
         isExpanded: true,
-        onPressed: (bool _) { },
+        onPressed: (bool _) {},
       ),
     ));
 
-    expect(tester.getSemantics(find.byType(ExpandIcon)), matchesSemantics(
-      hasTapAction: true,
-      hasEnabledState: true,
-      isEnabled: true,
-      isFocusable: true,
-      isButton: true,
-      onTapHint: localizations.expandedIconTapHint,
-    ));
+    expect(
+        tester.getSemantics(find.byType(ExpandIcon)),
+        matchesSemantics(
+          hasTapAction: true,
+          hasEnabledState: true,
+          isEnabled: true,
+          isFocusable: true,
+          isButton: true,
+          onTapHint: localizations.expandedIconTapHint,
+        ));
 
     await tester.pumpWidget(wrap(
       child: ExpandIcon(
-        onPressed: (bool _) { },
+        onPressed: (bool _) {},
       ),
     ));
 
-    expect(tester.getSemantics(find.byType(ExpandIcon)), matchesSemantics(
-      hasTapAction: true,
-      hasEnabledState: true,
-      isEnabled: true,
-      isFocusable: true,
-      isButton: true,
-      onTapHint: localizations.collapsedIconTapHint,
-    ));
+    expect(
+        tester.getSemantics(find.byType(ExpandIcon)),
+        matchesSemantics(
+          hasTapAction: true,
+          hasEnabledState: true,
+          isEnabled: true,
+          isFocusable: true,
+          isButton: true,
+          onTapHint: localizations.collapsedIconTapHint,
+        ));
     handle.dispose();
   });
 
-  testWidgets('ExpandIcon uses custom icon color and expanded icon color', (WidgetTester tester) async {
+  testWidgets('ExpandIcon uses custom icon color and expanded icon color',
+      (WidgetTester tester) async {
     bool expanded = false;
     IconTheme iconTheme;
 
@@ -271,7 +283,8 @@ void main() {
     expect(iconTheme.data.color, equals(Colors.indigo));
   });
 
-  testWidgets('ExpandIcon uses custom disabled icon color', (WidgetTester tester) async {
+  testWidgets('ExpandIcon uses custom disabled icon color',
+      (WidgetTester tester) async {
     IconTheme iconTheme;
 
     await tester.pumpWidget(wrap(
@@ -313,5 +326,29 @@ void main() {
     await tester.pumpAndSettle();
     iconTheme = tester.firstWidget(find.byType(IconTheme).last);
     expect(iconTheme.data.color, equals(Colors.cyan));
+  });
+
+  testWidgets('ExpandIcon default icon should be [Icons.expand_more]',
+      (WidgetTester tester) async {
+    final ExpandIcon expandIcon = ExpandIcon(
+      onPressed: (bool isExpanded) {},
+    );
+    await tester.pumpWidget(wrap(
+      child: expandIcon,
+    ));
+    expect(expandIcon.icon, Icons.expand_more);
+  });
+
+  testWidgets('ExpandIcon icon should be Icons.ac_unit',
+      (WidgetTester tester) async {
+    final ExpandIcon expandIcon = ExpandIcon(
+      size: 36,
+      icon: Icons.ac_unit,
+      onPressed: (bool isExpanded) {},
+    );
+    await tester.pumpWidget(wrap(
+      child: expandIcon,
+    ));
+    expect(expandIcon.icon, Icons.ac_unit);
   });
 }
