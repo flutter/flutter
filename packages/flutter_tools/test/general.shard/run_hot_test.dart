080ba48ad90e01d1e3a2b46862c5c518da144dcd
Christopher Fujino <christopherfujino@gmail.com>
2023-07-13 16:28:35 -0700

    add failing test

--- packages/flutter_tools/test/general.shard/run_hot_test.dart
+++ packages/flutter_tools/test/general.shard/run_hot_test.dart
@@ -5,7 +5,9 @@
 import 'package:flutter_tools/src/devfs.dart';
 import 'package:flutter_tools/src/resident_runner.dart';
 import 'package:flutter_tools/src/run_hot.dart';
+import 'package:flutter_tools/src/vmservice.dart';
 import 'package:test/fake.dart';
+import 'package:vm_service/vm_service.dart' as vm_service;
 
 import '../src/common.dart';
 
@@ -29,10 +31,30 @@ class _FakeHotRunner extends Fake implements HotRunner {
 
 class _FakeDevFS extends Fake implements DevFS {
   @override
-  final Uri? baseUri 
+  final Uri? baseUri = Uri();
 }
 
 class _FakeFlutterDevice extends Fake implements FlutterDevice {
   @override
   final DevFS? devFS = _FakeDevFS();
+
+  @override
+  final FlutterVmService? vmService = _FakeFlutterVmService();
+}
+
+class _FakeFlutterVmService extends Fake implements FlutterVmService {
+  @override
+  final vm_service.VmService service = _FakeVmService();
+}
+
+class _FakeVmService extends Fake implements vm_service.VmService {
+  @override
+  Future<_FakeVm> getVM() async => _FakeVm();
+}
+
+class _FakeVm extends Fake implements vm_service.VM {
+  final List<vm_service.IsolateRef> _isolates = <vm_service.IsolateRef>[];
+
+  @override
+  List<vm_service.IsolateRef>? get isolates => _isolates;
 }
