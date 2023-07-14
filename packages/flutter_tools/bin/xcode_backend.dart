bf0bdeb7856d350c0a21c08cc2c39f5d4460f3df
Daco Harkes <dacoharkes@google.com>
2023-07-14 08:33:13 +0200

    Fix printing native assets as null

--- packages/flutter_tools/bin/xcode_backend.dart
+++ packages/flutter_tools/bin/xcode_backend.dart
@@ -378,7 +378,7 @@ class Context {
       '-dTrackWidgetCreation=${environment['TRACK_WIDGET_CREATION'] ?? ''}',
       '-dDartObfuscation=${environment['DART_OBFUSCATION'] ?? ''}',
       '-dAction=${environment['ACTION'] ?? ''}',
-      '-dNativeAssets=${environment['NATIVE_ASSETS']}',
+      '-dNativeAssets=${environment['NATIVE_ASSETS'] ?? ''}',
       '--ExtraGenSnapshotOptions=${environment['EXTRA_GEN_SNAPSHOT_OPTIONS'] ?? ''}',
       '--DartDefines=${environment['DART_DEFINES'] ?? ''}',
       '--ExtraFrontEndOptions=${environment['EXTRA_FRONT_END_OPTIONS'] ?? ''}',
