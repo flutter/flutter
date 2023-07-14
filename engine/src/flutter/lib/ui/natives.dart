3101af7d591cee9c86c74646aedea7b07f48add6
Ian Hickson <ian@hixie.ch>
2023-07-13 17:34:12 -0700

    Remove unimplemented API call saveCompilationTrace (flutter/engine#43656)
    Fixes https://github.com/flutter/flutter/issues/59205

--- engine/src/flutter/lib/ui/natives.dart
+++ engine/src/flutter/lib/ui/natives.dart
@@ -85,29 +85,6 @@ Future<developer.ServiceExtensionResponse> _getImpellerEnabled(
 
 const bool _kReleaseMode = bool.fromEnvironment('dart.vm.product');
 
-/// Returns runtime Dart compilation trace as a UTF-8 encoded memory buffer.
-///
-/// The buffer contains a list of symbols compiled by the Dart JIT at runtime up
-/// to the point when this function was called. This list can be saved to a text
-/// file and passed to tools such as `flutter build` or Dart `gen_snapshot` in
-/// order to pre-compile this code offline.
-///
-/// The list has one symbol per line of the following format:
-/// `<namespace>,<class>,<symbol>\n`.
-///
-/// Here are some examples:
-///
-/// ```csv
-/// dart:core,Duration,get:inMilliseconds
-/// package:flutter/src/widgets/binding.dart,::,runApp
-/// file:///.../my_app.dart,::,main
-/// ```
-///
-/// This function is only effective in debug and dynamic modes, and will throw in AOT mode.
-List<int> saveCompilationTrace() {
-  throw UnimplementedError();
-}
-
 @Native<Void Function(Handle)>(symbol: 'DartRuntimeHooks::ScheduleMicrotask')
 external void _scheduleMicrotask(void Function() callback);
 
