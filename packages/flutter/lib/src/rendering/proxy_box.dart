baf670d9a9fe37cfc07443d32a91ba0bf684f9c8
Qun Cheng <chengqunq@gmail.com>
2023-07-24 17:18:39 -0700

    Remove extra comments

--- packages/flutter/lib/src/rendering/proxy_box.dart
+++ packages/flutter/lib/src/rendering/proxy_box.dart
@@ -4306,9 +4306,6 @@ class RenderSemanticsAnnotations extends RenderProxyBox {
     if (_properties.button != null) {
       config.isButton = _properties.button!;
     }
-    // if (_properties.collapsed != null) {
-    //   config.isCollapsed = _properties.collapsed;
-    // }
     if (_properties.expanded != null) {
       config.isExpanded = _properties.expanded;
     }
