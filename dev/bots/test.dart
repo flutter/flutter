5e19623c6826d3437973501c8358e88a75beb312
p-mazhnik <pavel.mazhnik@gmail.com>
2023-07-25 09:24:15 +0300

    [web] provide serviceWorkerVersion to the getNewServiceWorker function

--- dev/bots/test.dart
+++ dev/bots/test.dart
@@ -1223,6 +1223,7 @@ Future<void> _runWebLongRunningTests() async {
     () => runWebServiceWorkerTestWithCachingResources(headless: true, testType: ServiceWorkerTestType.withFlutterJsTrustedTypesOn),
     () => runWebServiceWorkerTestWithGeneratedEntrypoint(headless: true),
     () => runWebServiceWorkerTestWithBlockedServiceWorkers(headless: true),
+    () => runWebServiceWorkerTestWithCustomServiceWorkerVersion(headless: true),
     () => _runWebStackTraceTest('profile', 'lib/stack_trace.dart'),
     () => _runWebStackTraceTest('release', 'lib/stack_trace.dart'),
     () => _runWebStackTraceTest('profile', 'lib/framework_stack_trace.dart'),
