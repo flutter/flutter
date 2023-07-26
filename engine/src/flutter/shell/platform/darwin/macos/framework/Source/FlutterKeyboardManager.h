ae000cbf8ca5b8606e16fbf81bcdaba6c19eb931
Bruno Leroux <leroux_bruno@yahoo.fr>
2023-07-26 09:14:19 +0200

    [macOS] Return keyboard pressed state (flutter/engine#42878)
    ## Description
    
    This PR updates the macOS engine in order to answer to keyboard pressed state queries from the framework (as implemented in https://github.com/flutter/flutter/pull/122885).
    
    ## Related Issue
    
    macOS engine implementation for https://github.com/flutter/flutter/issues/87391
    Similar to:
    - Linux: https://github.com/flutter/engine/pull/42346
    - Android: https://github.com/flutter/engine/pull/42758
    
    ## Tests
    
    Adds 2 tests.

--- engine/src/flutter/shell/platform/darwin/macos/framework/Source/FlutterKeyboardManager.h
+++ engine/src/flutter/shell/platform/darwin/macos/framework/Source/FlutterKeyboardManager.h
@@ -57,4 +57,12 @@
 - (void)syncModifiersIfNeeded:(NSEventModifierFlags)modifierFlags
                     timestamp:(NSTimeInterval)timestamp;
 
+/**
+ * Returns the keyboard pressed state.
+ *
+ * Returns the keyboard pressed state. The dictionary contains one entry per
+ * pressed keys, mapping from the logical key to the physical key.
+ */
+- (nonnull NSDictionary*)getPressedState;
+
 @end
