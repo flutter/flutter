215c82b6870ade26a9969c5408dd9b2e6413c906
Mouad Debbar <mdebbar@google.com>
2023-07-26 13:22:56 -0400

    [web] Remove usage of `ui.webOnlyInitializePlatform()`

--- packages/flutter_tools/lib/src/web/bootstrap.dart
+++ packages/flutter_tools/lib/src/web/bootstrap.dart
@@ -229,7 +229,8 @@ String generateTestEntrypoint({
 
   Future<void> main() async {
     ui_web.debugEmulateFlutterTesterEnvironment = true;
-    await ui.webOnlyInitializePlatform();
+    // await ui.webOnlyInitializePlatform();
+    await ui_web.bootstrapEngine();
     webGoldenComparator = DefaultWebGoldenComparator(Uri.parse('${Uri.file(absolutePath)}'));
     (ui.window as dynamic).debugOverrideDevicePixelRatio(3.0);
     (ui.window as dynamic).webOnlyDebugPhysicalSizeOverride = const ui.Size(2400, 1800);
