46fd0f1382ee42d37db6ba7013a1e394a9f91b68
Andrew Kolos <andrewrkolos@gmail.com>
2023-07-26 09:57:02 -0700

    fix documented of loadAssetManifest in _load_asset_manifest_io.dart

--- packages/flutter/lib/src/painting/_load_asset_manifest_io.dart
+++ packages/flutter/lib/src/painting/_load_asset_manifest_io.dart
@@ -5,11 +5,6 @@
 import 'package:flutter/services.dart';
 
 /// Loads the contents of the asset manifest generated at build time.
-// This implementation, for non-web apps, is null since loading the asset
-// manifest has a higher probability of working.
-// We want to use the same signature as the web counterpart, so we
-// declare this as final rather than const.
-// ignore: prefer_const_declarations
 Future<AssetManifest> loadAssetManifest(AssetBundle bundle) {
   return AssetManifest.loadFromAssetBundle(bundle);
 }
