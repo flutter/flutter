61732e76dc7284fbfe522b899ed6f757945e8420
Ian Hickson <ian@hixie.ch>
2023-07-26 11:09:59 -0700

    Revert "ImageDecoration.lerp" (#131347)
    Reverts flutter/flutter#130533
    
    Tree breakage.

--- packages/flutter/lib/src/material/dialog.dart
+++ packages/flutter/lib/src/material/dialog.dart
@@ -2,9 +2,10 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'dart:ui' show clampDouble, lerpDouble;
+import 'dart:ui';
 
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart' show clampDouble;
 
 import 'color_scheme.dart';
 import 'colors.dart';
