b542cbb21cdde82882d742eff06c53409c117053
Mouad Debbar <mdebbar@google.com>
2023-07-26 11:15:55 -0400

    [web] Provide convenient default factories for platform views (flutter/engine#43828)
    Convenient default factories for creating DOM element from a given tag name.
    
    Required by https://github.com/flutter/flutter/pull/130513
    
    Part of https://github.com/flutter/flutter/issues/127030

--- engine/src/flutter/lib/web_ui/lib/ui_web/src/ui_web/platform_view_registry.dart
+++ engine/src/flutter/lib/web_ui/lib/ui_web/src/ui_web/platform_view_registry.dart
@@ -19,6 +19,20 @@ final PlatformViewRegistry platformViewRegistry = PlatformViewRegistry();
 
 /// A registry for factories that create platform views.
 class PlatformViewRegistry {
+  /// The view type of the built-in factory that creates visible platform view
+  /// DOM elements.
+  ///
+  /// There's no need to register this view type with [PlatformViewRegistry]
+  /// because it is registered by default.
+  static const String defaultVisibleViewType = '_default_document_create_element_visible';
+
+  /// The view type of the built-in factory that creates invisible platform view
+  /// DOM elements.
+  ///
+  /// There's no need to register this view type with [PlatformViewRegistry]
+  /// because it is registered by default.
+  static const String defaultInvisibleViewType = '_default_document_create_element_invisible';
+
   /// Register [viewType] as being created by the given [viewFactory].
   ///
   /// [viewFactory] can be any function that takes an integer and optional
