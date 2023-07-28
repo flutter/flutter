6074af64f38eda7f0c58728a0f5e9bd578d92452
Qun Cheng <chengqunq@gmail.com>
2023-07-27 19:32:10 -0700

    Deprecate useMaterial3 parameter in copyWith()

--- packages/flutter/lib/src/material/theme_data.dart
+++ packages/flutter/lib/src/material/theme_data.dart
@@ -1525,7 +1525,6 @@ class ThemeData with Diagnosticable {
     TargetPlatform? platform,
     ScrollbarThemeData? scrollbarTheme,
     InteractiveInkFeatureFactory? splashFactory,
-    bool? useMaterial3,
     VisualDensity? visualDensity,
     // COLOR
     // [colorScheme] is the preferred way to configure colors. The other color
@@ -1637,6 +1636,12 @@ class ThemeData with Diagnosticable {
       'This feature was deprecated after v3.3.0-0.6.pre.',
     )
     Color? bottomAppBarColor,
+    @Deprecated(
+      'Use ThemeData.useMaterial3 instead. '
+      'Setting this value causes issues on changing the default theme. '
+      'This feature was deprecated after v3.13.0-0.2.pre.',
+    )
+    bool? useMaterial3,
   }) {
     cupertinoOverrideTheme = cupertinoOverrideTheme?.noDefault();
     return ThemeData.raw(
