813260504f50fa5b6ece10e5b68d12f5d2a7e85d
Qun Cheng <chengqunq@gmail.com>
2023-07-27 23:01:39 -0700

    Remove unnecessary import

--- packages/flutter/test/material/search_anchor_test.dart
+++ packages/flutter/test/material/search_anchor_test.dart
@@ -5,7 +5,6 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import '../rendering/mock_canvas.dart';
