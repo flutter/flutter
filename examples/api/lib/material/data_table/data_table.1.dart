a6078bcfd9bb84ebf3984d69a519b170d043e1b9
Chinmay Kabi <chinmay.kabi@gmail.com>
2023-07-29 16:46:44 +0530

    test: add test for data table example

--- examples/api/lib/material/data_table/data_table.1.dart
+++ examples/api/lib/material/data_table/data_table.1.dart
@@ -30,13 +30,12 @@ class DataTableExample extends StatefulWidget {
 }
 
 class _DataTableExampleState extends State<DataTableExample> {
-  static const int numItems = 10;
+  static const int numItems = 20;
   List<bool> selected = List<bool>.generate(numItems, (int index) => false);
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: double.infinity,
+    return SingleChildScrollView(
       child: DataTable(
         columns: const <DataColumn>[
           DataColumn(
@@ -46,7 +45,8 @@ class _DataTableExampleState extends State<DataTableExample> {
         rows: List<DataRow>.generate(
           numItems,
           (int index) => DataRow(
-            color: MaterialStateProperty.resolveWith<Color?>((Set<MaterialState> states) {
+            color: MaterialStateProperty.resolveWith<Color?>(
+                (Set<MaterialState> states) {
               // All rows will have the same selected color.
               if (states.contains(MaterialState.selected)) {
                 return Theme.of(context).colorScheme.primary.withOpacity(0.08);
---
+++ examples/api/test/material/data_table/data_table.1_test.dart
@@ -0,0 +1,25 @@
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter_api_samples/material/data_table/data_table.1.dart' as example;
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('DataTable is scrollable', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      const example.DataTableExampleApp(),
+    );
+
+    expect(find.text('Row 0').hitTestable(), findsOneWidget);
+    expect(find.text('Row 19').hitTestable(), findsNothing);
+
+    await tester.ensureVisible(find.text('Row 19'));
+
+    expect(find.text('Row 0').hitTestable(), findsNothing);
+    expect(find.text('Row 19').hitTestable(), findsOneWidget);
+  });
+}
+
+
+
