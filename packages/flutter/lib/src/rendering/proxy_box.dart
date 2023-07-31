6b19bb1fbecb020a3740992a2ee4189a0d4499f6
Qun Cheng <36861262+QuncCccccc@users.noreply.github.com>
2023-07-31 12:09:27 -0700

    Add Expanded/Collapsed State for Semantics (#131233)

--- packages/flutter/lib/src/rendering/proxy_box.dart
+++ packages/flutter/lib/src/rendering/proxy_box.dart
@@ -4319,6 +4319,9 @@ class RenderSemanticsAnnotations extends RenderProxyBox {
     if (_properties.button != null) {
       config.isButton = _properties.button!;
     }
+    if (_properties.expanded != null) {
+      config.isExpanded = _properties.expanded;
+    }
     if (_properties.link != null) {
       config.isLink = _properties.link!;
     }
