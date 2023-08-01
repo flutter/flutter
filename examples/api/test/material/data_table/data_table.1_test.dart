b0b19e519c0f1223582ab9418a76699ed792182c
Taha Tesser <tessertaha@gmail.com>
2023-08-01 14:54:40 +0300

    Update examples/api/test/material/data_table/data_table.1_test.dart

--- examples/api/test/material/data_table/data_table.1_test.dart
+++ examples/api/test/material/data_table/data_table.1_test.dart
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'package:flutter/material.dart';
 import 'package:flutter_api_samples/material/data_table/data_table.1.dart' as example;
 import 'package:flutter_test/flutter_test.dart';
 
