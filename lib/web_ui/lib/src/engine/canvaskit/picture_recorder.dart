2fe2600caf14c0f6f4346eea2b66f19ec862dc59
Harry Terkelsen <hterkelsen@users.noreply.github.com>
2023-08-01 16:07:19 -0700

    [canvaskit] Enable CanvasKit to compute tight SkPicture bounds (#43361)

--- lib/web_ui/lib/src/engine/canvaskit/picture_recorder.dart
+++ lib/web_ui/lib/src/engine/canvaskit/picture_recorder.dart
@@ -11,12 +11,10 @@ import 'canvaskit_api.dart';
 import 'picture.dart';
 
 class CkPictureRecorder implements ui.PictureRecorder {
-  ui.Rect? _cullRect;
   SkPictureRecorder? _skRecorder;
   CkCanvas? _recordingCanvas;
 
   CkCanvas beginRecording(ui.Rect bounds) {
-    _cullRect = bounds;
     final SkPictureRecorder recorder = _skRecorder = SkPictureRecorder();
     final Float32List skRect = toSkRect(bounds);
     final SkCanvas skCanvas = recorder.beginRecording(skRect);
@@ -36,7 +34,7 @@ class CkPictureRecorder implements ui.PictureRecorder {
     final SkPicture skPicture = recorder.finishRecordingAsPicture();
     recorder.delete();
     _skRecorder = null;
-    final CkPicture result = CkPicture(skPicture, _cullRect);
+    final CkPicture result = CkPicture(skPicture);
     // We invoke the handler here, not in the picture constructor, because we want
     // [result.approximateBytesUsed] to be available for the handler.
     ui.Picture.onCreate?.call(result);
