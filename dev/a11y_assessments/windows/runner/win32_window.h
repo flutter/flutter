dfde4b4462f3876c1f9a411b3cccc0cc7934855a
Chun-Heng Tai <chtai@google.com>
2023-08-01 16:15:14 -0700

    update

--- dev/a11y_assessments/windows/runner/win32_window.h
+++ dev/a11y_assessments/windows/runner/win32_window.h
@@ -1,3 +1,7 @@
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 #ifndef RUNNER_WIN32_WINDOW_H_
 #define RUNNER_WIN32_WINDOW_H_
 
