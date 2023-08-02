07fe469c0cd05f079514bbdd69aca9d96a992aa3
Andrew Kolos <andrewrkolos@gmail.com>
2023-08-01 18:24:08 -0700

    fix infinite recursion

--- packages/flutter/lib/src/services/_load_asset_manifest_io.dart
+++ packages/flutter/lib/src/services/_load_asset_manifest_io.dart
@@ -6,6 +6,6 @@ import 'asset_bundle.dart';
 import 'asset_manifest.dart';
 
 /// Loads the contents of the asset manifest generated at build time.
-Future<AssetManifest> loadAssetManifest(AssetBundle bundle) {
-  return AssetManifest.loadFromAssetBundle(bundle);
+Future<AssetManifest> loadAssetManifest(AssetBundle bundle) async {
+  return AssetManifest.fromByteData(await bundle.load('AssetManifest.bin'));
 }
