561536147bb978f62aad15984a085c3cd857d287
Dan Chevalier <chevalier.dan@gmail.com>
2023-08-02 13:57:42 -0400

    small changes

--- packages/flutter/test_fixes/widgets/app.dart
+++ packages/flutter/test_fixes/widgets/app.dart
@@ -1,10 +1,10 @@
+
 // Copyright 2014 The Flutter Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// import 'package:flutter/app.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
-  WidgetsApp.debugShowWidgetInspectorOverride = true;
+  WidgetsApp.debugShowWidgetInspectorOverrideNotifier = true;
 }
