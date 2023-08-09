d53430438a23038bc629082c9334673482318073
Andrea Cioni <supercionci@hotmail.it>
2023-08-09 19:18:45 +0200

    add textInputAction

--- examples/api/lib/material/input_chip/input_chip.1.dart
+++ examples/api/lib/material/input_chip/input_chip.1.dart
@@ -247,6 +247,7 @@ class ChipsInputState<T> extends State<ChipsInput<T>> {
     return TextField(
       minLines: 1,
       maxLines: 10,
+      textInputAction: TextInputAction.done,
       style: const TextStyle(
         height: 2.5,
       ),
