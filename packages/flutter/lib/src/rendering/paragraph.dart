8c1338c5a33a1301d08f4f00767b8ca1eff35dec
LongCatIsLooong <31859944+LongCatIsLooong@users.noreply.github.com>
2023-08-08 21:43:57 -0700

    Use pattern matching to avoid strange type annotations (#131964)
    Addresses https://github.com/flutter/flutter/pull/131640#discussion_r1284861384

--- packages/flutter/lib/src/rendering/paragraph.dart
+++ packages/flutter/lib/src/rendering/paragraph.dart
@@ -725,12 +725,13 @@ class RenderParagraph extends RenderBox with ContainerRenderObjectMixin<RenderBo
   @override
   bool hitTestChildren(BoxHitTestResult result, { required Offset position }) {
     final TextPosition textPosition = _textPainter.getPositionForOffset(position);
-    final Object? span = _textPainter.text!.getSpanForPosition(textPosition);
-    if (span is HitTestTarget) {
-      result.add(HitTestEntry(span));
-      return true;
+    switch (_textPainter.text!.getSpanForPosition(textPosition)) {
+      case final HitTestTarget span:
+        result.add(HitTestEntry(span));
+        return true;
+      case _:
+        return hitTestInlineChildren(result, position);
     }
-    return hitTestInlineChildren(result, position);
   }
 
   bool _needsClipping = false;
