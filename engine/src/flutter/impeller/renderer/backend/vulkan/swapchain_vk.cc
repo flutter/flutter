839d63312cd434238e00c056690780b1980d4970
Matan Lurey <matanlurey@users.noreply.github.com>
2023-08-09 19:14:35 -0700

    [Impeller] Replace Vulkan rotation checks with polling (flutter/engine#44361)
    tl;dr: Reverts https://github.com/flutter/engine/pull/43214. closes
    https://github.com/flutter/flutter/issues/129459.
    
    
    ![7v5zfd](https://github.com/flutter/engine/assets/168174/b87a0726-cddd-415e-a46c-2c65d6f5c9d5)
    
    ---
    
    In https://github.com/flutter/engine/pull/43214, @jonahwilliams used
    `vkQueuePresentKHR`[^1] [to
    check](https://github.com/jonahwilliams/engine/blob/a0df46add166202fe68a853391d379e69ddc7c18/impeller/renderer/backend/vulkan/swapchain_impl_vk.cc#L465-L467)
    if device orientations had changed, and if so, the swap-chain was torn
    down and replaced with a new instance (that would now have a correct
    rotation for ... compositing reasons I don't quite understand).
    
    Unfortunately `vkQueuePresentKHR` is (a) only present on Android 10+
    _and_ (b) ... isn't implemented consistently across Android devices. For
    example, the popular Samsung Galaxy S10 doesn't return
    `VK_SUBOPTIMAL_KHR`, leading to the same rotation bugs described in
    https://github.com/flutter/flutter/issues/129459 (i.e. even with
    https://github.com/flutter/engine/pull/43214).
    
    This PR implements the polling technique recommended by Android[^2], in
    this case on every frame. We should expect this to have a performance
    penalty of ~`0.2`ms per frame, but should at least give us consistent
    fidelity on Vulkan + Android for the time-being.
    
    
    ![flutter_04](https://github.com/flutter/engine/assets/168174/969f5c5e-7f6a-4156-8de8-1351b32f2a2f)
    
    <details>
    <summary>
    Some additional screenshots generated while debugging
    </summary>
    <img src
    ="https://github.com/flutter/engine/assets/168174/900e02a8-aa51-4592-9690-c650092130a2"
    />
    <img
    src="https://github.com/flutter/engine/assets/168174/e7b9e5c0-86b0-407f-aa51-2f76afda4f03)"
    />
    <img
    src="https://github.com/flutter/engine/assets/168174/6b611090-97f5-4589-9ef9-6ba778efc6b7"
    />
    </details>
    
    
    [^1]:
    https://developer.android.com/games/optimize/vulkan-prerotation#detect_device_orientation_changes
    [^2]:
    https://developer.android.com/games/optimize/vulkan-prerotation#using_polling

--- engine/src/flutter/impeller/renderer/backend/vulkan/swapchain_vk.cc
+++ engine/src/flutter/impeller/renderer/backend/vulkan/swapchain_vk.cc
@@ -12,8 +12,7 @@ namespace impeller {
 std::shared_ptr<SwapchainVK> SwapchainVK::Create(
     const std::shared_ptr<Context>& context,
     vk::UniqueSurfaceKHR surface) {
-  auto impl = SwapchainImplVK::Create(context, std::move(surface),
-                                      /*was_rotated=*/false);
+  auto impl = SwapchainImplVK::Create(context, std::move(surface));
   if (!impl || !impl->IsValid()) {
     return nullptr;
   }
@@ -46,13 +45,12 @@ std::unique_ptr<Surface> SwapchainVK::AcquireNextDrawable() {
   // This swapchain implementation indicates that it is out of date. Tear it
   // down and make a new one.
   auto context = impl_->GetContext();
-  auto was_rotated = impl_->GetIsRotated();
   auto [surface, old_swapchain] = impl_->DestroySwapchain();
 
-  auto new_impl = SwapchainImplVK::Create(context,             //
-                                          std::move(surface),  //
-                                          was_rotated,         //
-                                          *old_swapchain       //
+  auto new_impl = SwapchainImplVK::Create(context,                   //
+                                          std::move(surface),        //
+                                          *old_swapchain,            //
+                                          impl_->GetLastTransform()  //
   );
   if (!new_impl || !new_impl->IsValid()) {
     VALIDATION_LOG << "Could not update swapchain.";
