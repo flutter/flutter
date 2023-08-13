03865c9a1248b8cd2a36b37590c949ad04d5223a
Kevin Moore <kevmoo@users.noreply.github.com>
2023-08-12 17:29:10 -0700

    Allow latest pkg:material_color_utilities

--- packages/flutter_tools/lib/src/commands/update_packages.dart
+++ packages/flutter_tools/lib/src/commands/update_packages.dart
@@ -33,8 +33,8 @@ const Map<String, String> kManuallyPinnedDependencies = <String, String>{
   'flutter_template_images': '4.2.0', // Must always exactly match flutter_tools template.
   'video_player': '2.2.11',
   // Keep pinned to latest until 1.0.0.
-  'material_color_utilities': '0.5.0',
-  // https://github.com/flutter/flutter/issues/115660
+  'material_color_utilities': '0.8.0',
+  // https://github.com/flutter/flutter/issues/111304
   'archive': '3.3.2',
   // https://github.com/flutter/devtools/issues/3951
   'leak_tracker': '9.0.8',
