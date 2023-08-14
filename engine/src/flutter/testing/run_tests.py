63c2a5ce3d5c7baa7e1be52b9d5be26996a96dc3
Dan Field <dnfield@google.com>
2023-08-14 11:36:02 -0700

    Revert "Make run_tests.py assert that the ios test dylib is at least as new as libFlutter.dylib" (flutter/engine#44690)
    Reverts flutter/engine#41720
    
    Fixes https://github.com/flutter/flutter/issues/131899

--- engine/src/flutter/testing/run_tests.py
+++ engine/src/flutter/testing/run_tests.py
@@ -615,15 +615,6 @@ def ensure_ios_tests_are_built(ios_out_dir):
   assert os.path.exists(tmp_out_dir
                        ) and os.path.exists(ios_test_lib), final_message
 
-  ios_test_lib_time = os.path.getmtime(ios_test_lib)
-  flutter_dylib = os.path.join(tmp_out_dir, 'libFlutter.dylib')
-  flutter_dylib_time = os.path.getmtime(flutter_dylib)
-
-  final_message = '%s is older than %s. Please run the following commands: \n%s' % (
-      ios_test_lib, flutter_dylib, '\n'.join(message)
-  )
-  assert flutter_dylib_time <= ios_test_lib_time, final_message
-
 
 def assert_expected_xcode_version():
   """Checks that the user has a version of Xcode installed"""
