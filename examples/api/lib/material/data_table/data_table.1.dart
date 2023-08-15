93a0c5f0b911e5f0bc6f7638a774d5254df46d4d
Anianonymous <hegdeanirudh2003@gmail.com>
2023-08-15 16:19:41 +0530

    DataTable example is scrollable

--- examples/api/lib/material/data_table/data_table.1.dart
+++ examples/api/lib/material/data_table/data_table.1.dart
@@ -30,40 +30,45 @@ class DataTableExample extends StatefulWidget {
 }
 
 class _DataTableExampleState extends State<DataTableExample> {
-  static const int numItems = 10;
+  //initialize numItems=100
+  // static const int numItems = 10;
+  static const int numItems = 100;
   List<bool> selected = List<bool>.generate(numItems, (int index) => false);
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: double.infinity,
-      child: DataTable(
-        columns: const <DataColumn>[
-          DataColumn(
-            label: Text('Number'),
-          ),
-        ],
-        rows: List<DataRow>.generate(
-          numItems,
-          (int index) => DataRow(
-            color: MaterialStateProperty.resolveWith<Color?>((Set<MaterialState> states) {
-              // All rows will have the same selected color.
-              if (states.contains(MaterialState.selected)) {
-                return Theme.of(context).colorScheme.primary.withOpacity(0.08);
-              }
-              // Even rows will have a grey color.
-              if (index.isEven) {
-                return Colors.grey.withOpacity(0.3);
-              }
-              return null; // Use default value for other states and odd rows.
-            }),
-            cells: <DataCell>[DataCell(Text('Row $index'))],
-            selected: selected[index],
-            onSelectChanged: (bool? value) {
-              setState(() {
-                selected[index] = value!;
-              });
-            },
+    //added SingleChildScrollView widget to scroll the data-cells
+    return SingleChildScrollView(
+      child:SizedBox(
+        width: double.infinity,
+        child: DataTable(
+          columns: const <DataColumn>[
+            DataColumn(
+              label: Text('Number'),
+            ),
+          ],
+          rows: List<DataRow>.generate(
+            numItems,
+                (int index) => DataRow(
+              color: MaterialStateProperty.resolveWith<Color?>((Set<MaterialState> states) {
+                // All rows will have the same selected color.
+                if (states.contains(MaterialState.selected)) {
+                  return Theme.of(context).colorScheme.primary.withOpacity(0.08);
+                }
+                // Even rows will have a grey color.
+                if (index.isEven) {
+                  return Colors.grey.withOpacity(0.3);
+                }
+                return null; // Use default value for other states and odd rows.
+              }),
+              cells: <DataCell>[DataCell(Text('Row $index'))],
+              selected: selected[index],
+              onSelectChanged: (bool? value) {
+                setState(() {
+                  selected[index] = value!;
+                });
+              },
+            ),
           ),
         ),
       ),
