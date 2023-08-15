95dc531ec2ede4d4f62efa4b3d93831cee7bd358
Gray Mackall <mackall@google.com>
2023-08-15 13:18:00 -0700

    third time? is the charm for runCommandSyntax

--- dev/bots/test.dart
+++ dev/bots/test.dart
@@ -1005,12 +1005,15 @@ Future<void> _runFrameworkTests() async {
       '--exclude-tags=web',
     ]);
     //Run java unit tests for integration_test
+    await runCommand('pwd', <String>[]);
     await runCommand(
-        path.join(flutterRoot, 'packages', 'integration_test', 'example', 'android', 'gradlew'),
-        <String>[
-          ':integration_test:testDebugUnitTest',
-          '--tests "dev.flutter.plugins.integration_test.FlutterDeviceScreenshotTest"'
-        ],
+      path.join(flutterRoot, 'packages', 'integration_test', 'example', 'android', 'gradlew'),
+      <String>[
+        ':integration_test:testDebugUnitTest',
+        '--tests',
+        'dev.flutter.plugins.integration_test.FlutterDeviceScreenshotTest'
+      ],
+      workingDirectory: path.join(flutterRoot, 'packages', 'integration_test', 'example', 'android'),
     );
     await _runFlutterTest(path.join(flutterRoot, 'packages', 'flutter_goldens'));
     await _runFlutterTest(path.join(flutterRoot, 'packages', 'flutter_localizations'));
