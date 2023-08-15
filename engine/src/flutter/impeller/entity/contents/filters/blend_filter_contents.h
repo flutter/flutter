4367ca66ae378597ec708ac16fb65a4b1797ca8a
Jonah Williams <jonahwilliams@google.com>
2023-08-15 12:40:09 -0700

    [Impeller] Make porter duff blended atlas calls faster. (flutter/engine#44059)
    This implements part of https://github.com/flutter/flutter/issues/131345
    
    ---
    
    Makes all porter-duff draw atlas blending avoid subpass creation, by tweaking the existing porter duff shader to support per-vertex colors and output opacity. This requires a small trick: What the blend shaders and what (drawAtlas/drawVertices) consider the source and destination is reversed. That is a blend mode color filter treats the color as the source whereas drawVertices/drawAtlas treat their per-vertex colors as the destination. Therefore I cannot simply make all color inputs per vertex and use the exact same shaders :(
    
    However, for the porter duff blends we should just be able to reverse the blend mode (srcover -> dstOver, xor->xor) (I'm pretty sure I did this correctly) so that the blending is still correct. I don't think we can do this for the advanced blends, but supporting those is less important than the common atlas blending parameters like dstIn.
    
    This PR only updates drawAtlas.

--- engine/src/flutter/impeller/entity/contents/filters/blend_filter_contents.h
+++ engine/src/flutter/impeller/entity/contents/filters/blend_filter_contents.h
@@ -4,10 +4,32 @@
 
 #pragma once
 
+#include <optional>
 #include "impeller/entity/contents/filters/color_filter_contents.h"
 #include "impeller/entity/contents/filters/inputs/filter_input.h"
+#include "impeller/geometry/color.h"
 
 namespace impeller {
+
+constexpr std::array<std::array<Scalar, 5>, 15> kPorterDuffCoefficients = {{
+    {0, 0, 0, 0, 0},    // Clear
+    {1, 0, 0, 0, 0},    // Source
+    {0, 0, 1, 0, 0},    // Destination
+    {1, 0, 1, -1, 0},   // SourceOver
+    {1, -1, 1, 0, 0},   // DestinationOver
+    {0, 1, 0, 0, 0},    // SourceIn
+    {0, 0, 0, 1, 0},    // DestinationIn
+    {1, -1, 0, 0, 0},   // SourceOut
+    {0, 0, 1, -1, 0},   // DestinationOut
+    {0, 1, 1, -1, 0},   // SourceATop
+    {1, -1, 0, 1, 0},   // DestinationATop
+    {1, -1, 1, -1, 0},  // Xor
+    {1, 0, 1, 0, 0},    // Plus
+    {0, 0, 0, 0, 1},    // Modulate
+    {0, 0, 1, 0, -1},   // Screen
+}};
+
+std::optional<BlendMode> InvertPorterDuffBlend(BlendMode blend_mode);
 
 class BlendFilterContents : public ColorFilterContents {
  public:
