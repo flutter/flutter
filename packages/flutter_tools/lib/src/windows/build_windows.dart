785820cd9505045dc73eb961cb57fd173ae59c23
Matan Lurey <matanlurey@users.noreply.github.com>
2023-08-16 13:05:00 -0700

    Updated.

--- packages/flutter_tools/lib/src/windows/build_windows.dart
+++ packages/flutter_tools/lib/src/windows/build_windows.dart
@@ -247,9 +247,9 @@ void _writeGeneratedFlutterConfig(
   };
   final LocalEngineInfo? localEngineInfo = globals.artifacts?.localEngineInfo;
   if (localEngineInfo != null) {
-    final String engineOutPath = localEngineInfo.targetOutPath;
+    final String targetOutPath = localEngineInfo.targetOutPath;
     // Get the engine source root $ENGINE/src/out/foo_bar_baz -> $ENGINE/src
-    environment['FLUTTER_ENGINE'] = globals.fs.path.dirname(globals.fs.path.dirname(engineOutPath));
+    environment['FLUTTER_ENGINE'] = globals.fs.path.dirname(globals.fs.path.dirname(targetOutPath));
     environment['LOCAL_ENGINE'] = localEngineInfo.localTargetName;
     environment['LOCAL_ENGINE_HOST'] = localEngineInfo.localHostName;
   }
