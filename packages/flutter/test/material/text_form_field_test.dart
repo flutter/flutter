456807f6d036cdad62b5053d1b1cec2da8423059
Polina Cherkasova <polina.c@live.com>
2023-08-16 14:26:57 -0700

    Update text_form_field_test.dart

--- packages/flutter/test/material/text_form_field_test.dart
+++ packages/flutter/test/material/text_form_field_test.dart
@@ -247,9 +247,9 @@ void main() {
   },
     variant: const TargetPlatformVariant(<TargetPlatform>{ TargetPlatform.linux, TargetPlatform.windows }),
     skip: kIsWeb, // [intended] we don't supply the cut/copy/paste buttons on the web.
-    // // TODO(polina-c): remove after fixing
-    // // https://github.com/flutter/flutter/issues/130467
-    // leakTrackingTestConfig: const LeakTrackingTestConfig(allowAllNotDisposed: true, allowAllNotGCed: true),
+    // TODO(polina-c): remove after fixing
+    // https://github.com/flutter/flutter/issues/130467
+    leakTrackingTestConfig: const LeakTrackingTestConfig(allowAllNotDisposed: true, allowAllNotGCed: true),
   );
 
   testWidgetsWithLeakTracking(
