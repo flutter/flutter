4c67c2bcc3c3756d95f61a25719d6b808e4eceff
Louise Hsu <louisehsu@google.com>
2023-08-17 14:57:46 -0700

    remove trailing space lol

--- packages/flutter/lib/src/widgets/editable_text.dart
+++ packages/flutter/lib/src/widgets/editable_text.dart
@@ -2475,7 +2475,7 @@ class EditableTextState extends State<EditableText> with AutomaticKeepAliveClien
 
   /// Look up the current selection,
   /// as in the "Look Up" edit menu button on iOS.
-  /// 
+  ///
   /// Currently this is only implemented for iOS.
   ///
   /// Throws an error if the selection is empty or collapsed.
