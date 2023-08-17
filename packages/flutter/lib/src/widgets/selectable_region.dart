70ccd8de284eb105c0124273b90f04e96bcd666e
Renzo Olivares <rmolivares@renzo-olivares.dev>
2023-08-17 09:54:06 -0700

    should hide toolbar on touch tap up

--- packages/flutter/lib/src/widgets/selectable_region.dart
+++ packages/flutter/lib/src/widgets/selectable_region.dart
@@ -352,6 +352,7 @@ class SelectableRegionState extends State<SelectableRegion> with TextSelectionDe
               _showToolbar(location: details.globalPosition);
             }
           } else {
+            hideToolbar();
             _selectPositionAt(offset: details.globalPosition);
           }
         };
