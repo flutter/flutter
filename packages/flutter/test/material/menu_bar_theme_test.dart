3f0ed98f5324d8927115d4e60f2670df151f0552
Polina Cherkasova <polinach@google.com>
2023-08-17 12:11:24 -0700

    Leaks fix. (#132778)
    Fixes https://github.com/flutter/flutter/issues/132769

--- packages/flutter/test/material/menu_bar_theme_test.dart
+++ packages/flutter/test/material/menu_bar_theme_test.dart
@@ -2,10 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// Fails with "flutter test --test-randomize-ordering-seed=20230817"
-@Tags(<String>['no-shuffle'])
-library;
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:leak_tracker_flutter_testing/leak_tracker_flutter_testing.dart';
