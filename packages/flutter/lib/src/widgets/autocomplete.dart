b860e48ec948f579b04f78f64faaf293927967eb
Tomasz Gucio <tgucio@gmail.com>
2023-08-18 08:41:50 +0200

    Autocomplete

--- packages/flutter/lib/src/widgets/autocomplete.dart
+++ packages/flutter/lib/src/widgets/autocomplete.dart
@@ -446,6 +446,7 @@ class _RawAutocompleteState<T extends Object> extends State<RawAutocomplete<T>> 
     }
 
     _floatingOptions?.remove();
+    _floatingOptions?.dispose();
     if (_shouldShowOptions) {
       final OverlayEntry newFloatingOptions = OverlayEntry(
         builder: (BuildContext context) {
@@ -562,6 +563,7 @@ class _RawAutocompleteState<T extends Object> extends State<RawAutocomplete<T>> 
       _focusNode.dispose();
     }
     _floatingOptions?.remove();
+    _floatingOptions?.dispose();
     _floatingOptions = null;
     super.dispose();
   }
