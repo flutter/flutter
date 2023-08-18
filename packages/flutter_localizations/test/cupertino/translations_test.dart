fffba2f660a5b52419f8f32e57c0cb7f32736046
Andrey Suvorov <32621121+AndreySuworow@users.noreply.github.com>
2023-08-18 19:38:54 +0300

    fixes l10n for CupertinoDatePicker in monthYear mode (#130934)
    This PR fixes l10n issue when months names are being used in incorrect form in CupertinoDatePicker in CupertinoDatePickerMode.yearMonth (#130930).
    
    The idea of this proposal is to add an optional parameter `standalone` for `CupertinoLocalizations.datePickerMonth` to be able to choose when to use months names in base form (intl DateSymbols.STANDALONEMONTHS) and when in day-dependent form (intl DateSymbols.MONTHS)
    
    <details>
    <summary>Before</summary>
    
    <img width="366" alt="image" src="https://github.com/flutter/flutter/assets/32621121/1dd54fa7-6dd9-4053-889b-57134c145432">
    
    <img width="387" alt="image" src="https://github.com/flutter/flutter/assets/32621121/c176070e-73e4-49d3-883b-ba31eca6d1d7">
    
    </details>
    
    <details>
    <summary>After</summary>
    
    <img width="369" alt="image" src="https://github.com/flutter/flutter/assets/32621121/255594f1-219d-4bd4-9b75-1012912f8ab0">
    
    <img width="378" alt="image" src="https://github.com/flutter/flutter/assets/32621121/16bbb41f-3f62-4446-bf41-e27140b649a9">
    
    </details>

--- packages/flutter_localizations/test/cupertino/translations_test.dart
+++ packages/flutter_localizations/test/cupertino/translations_test.dart
@@ -34,6 +34,11 @@ void main() {
       expect(localizations.datePickerMonth(11), isNotNull);
       expect(localizations.datePickerMonth(12), isNotNull);
 
+      expect(localizations.datePickerStandaloneMonth(1), isNotNull);
+      expect(localizations.datePickerStandaloneMonth(2), isNotNull);
+      expect(localizations.datePickerStandaloneMonth(11), isNotNull);
+      expect(localizations.datePickerStandaloneMonth(12), isNotNull);
+
       expect(localizations.datePickerDayOfMonth(0), isNotNull);
       expect(localizations.datePickerDayOfMonth(1), isNotNull);
       expect(localizations.datePickerDayOfMonth(2), isNotNull);
