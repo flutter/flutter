3d2922e72d8d44ab26857f1b3cee8e4de3e00776
Victoria Ashworth <vashworth@google.com>
2023-08-22 12:59:09 -0500

    add mac log to debug

--- dev/devicelab/bin/tasks/native_ui_tests_macos.dart
+++ dev/devicelab/bin/tasks/native_ui_tests_macos.dart
@@ -2,7 +2,11 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'dart:io';
+
+import 'package:flutter_devicelab/framework/devices.dart';
 import 'package:flutter_devicelab/framework/framework.dart';
+import 'package:flutter_devicelab/framework/host_agent.dart';
 import 'package:flutter_devicelab/framework/ios.dart';
 import 'package:flutter_devicelab/framework/task_result.dart';
 import 'package:flutter_devicelab/framework/utils.dart';
@@ -12,28 +16,49 @@ Future<void> main() async {
   await task(() async {
     final String projectDirectory = '${flutterDirectory.path}/dev/integration_tests/flutter_gallery';
 
-    await inDirectory(projectDirectory, () async {
-      section('Build gallery app');
+    IOSink? sink;
+    deviceOperatingSystem = DeviceOperatingSystem.ios;
+    Device? device;
+    try {
+      device = await devices.workingDevice;
+    } on DeviceException {
+      device = null;
+    }
 
-      await flutter(
-        'build',
-        options: <String>[
-          'macos',
-          '-v',
-          '--debug',
-        ],
-      );
-    });
+    try {
+      await inDirectory(projectDirectory, () async {
+        if (device != null && device.canStreamLogs && hostAgent.dumpDirectory != null) {
+          sink = File(path.join(hostAgent.dumpDirectory!.path, '${device.deviceId}.log')).openWrite();
+          await device.startLoggingToSink(sink!);
+        }
 
-    section('Run platform unit tests');
+        section('Build gallery app');
 
-    if (!await runXcodeTests(
-      platformDirectory: path.join(projectDirectory, 'macos'),
-      destination: 'platform=macOS',
-      testName: 'native_ui_tests_macos',
-      skipCodesign: true,
-    )) {
-      return TaskResult.failure('Platform unit tests failed');
+        await flutter(
+          'build',
+          options: <String>[
+            'macos',
+            '-v',
+            '--debug',
+          ],
+        );
+      });
+
+      section('Run platform unit tests');
+
+      if (!await runXcodeTests(
+        platformDirectory: path.join(projectDirectory, 'macos'),
+        destination: 'platform=macOS',
+        testName: 'native_ui_tests_macos',
+        skipCodesign: true,
+      )) {
+        return TaskResult.failure('Platform unit tests failed');
+      }
+    } finally {
+      if (device != null && device.canStreamLogs) {
+        await device.stopLoggingToSink();
+        await sink?.close();
+      }
     }
 
     return TaskResult.success(null);
