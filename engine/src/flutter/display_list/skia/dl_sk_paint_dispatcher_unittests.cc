84a60cdc3445261b8b948b153653a400ea6b8ceb
Matan Lurey <matanlurey@users.noreply.github.com>
2023-08-22 15:12:46 -0700

    Further filter/clear `<SkPaint>.setDither(true)`, this time in `DlSkPaintDispatchHelper` (flutter/engine#44912)
    Closes https://github.com/flutter/flutter/issues/132860.
    
    - If `setDither(true)` is called, and an existing `setColorSource` is a gradient, it is ignored.
    - If `setColorSource(...)` is called, and it is a gradient, and dithering was previously set, it is cleared.
    
    I'm not sure this is fool proof.

--- engine/src/flutter/display_list/skia/dl_sk_paint_dispatcher_unittests.cc
+++ engine/src/flutter/display_list/skia/dl_sk_paint_dispatcher_unittests.cc
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "display_list/effects/dl_color_source.h"
 #include "flutter/display_list/skia/dl_sk_paint_dispatcher.h"
 
 #include "flutter/display_list/utils/dl_receiver_utils.h"
@@ -21,6 +22,17 @@ class MockDispatchHelper final : public virtual DlOpReceiver,
   void restore() override { DlSkPaintDispatchHelper::restore_opacity(); }
 };
 
+static const DlColor kTestColors[2] = {0xFF000000, 0xFFFFFFFF};
+static const float kTestStops[2] = {0.0f, 1.0f};
+static const auto kTestLinearGradient =
+    DlColorSource::MakeLinear(SkPoint::Make(0.0f, 0.0f),
+                              SkPoint::Make(100.0f, 100.0f),
+                              2,
+                              kTestColors,
+                              kTestStops,
+                              DlTileMode::kClamp,
+                              nullptr);
+
 // Regression test for https://github.com/flutter/flutter/issues/100176.
 TEST(DisplayListUtils, OverRestore) {
   MockDispatchHelper helper;
@@ -29,6 +41,63 @@ TEST(DisplayListUtils, OverRestore) {
   // There should be a protection here for over-restore to keep the program from
   // crashing.
   helper.restore();
+}
+
+// https://github.com/flutter/flutter/issues/132860.
+TEST(DisplayListUtils, SetDitherIgnoredIfColorSourceNotGradient) {
+  MockDispatchHelper helper;
+  helper.setDither(true);
+  EXPECT_FALSE(helper.paint().isDither());
+}
+
+// https://github.com/flutter/flutter/issues/132860.
+TEST(DisplayListUtils, SetColorSourceClearsDitherIfNotGradient) {
+  MockDispatchHelper helper;
+  helper.setDither(true);
+  helper.setColorSource(nullptr);
+  EXPECT_FALSE(helper.paint().isDither());
+}
+
+// https://github.com/flutter/flutter/issues/132860.
+TEST(DisplayListUtils, SetDitherTrueThenSetColorSourceDithersIfGradient) {
+  MockDispatchHelper helper;
+
+  // A naive implementation would ignore the dither flag here since the current
+  // color source is not a gradient.
+  helper.setDither(true);
+  helper.setColorSource(kTestLinearGradient.get());
+  EXPECT_TRUE(helper.paint().isDither());
+}
+
+// https://github.com/flutter/flutter/issues/132860.
+TEST(DisplayListUtils, SetDitherTrueThenRenderNonGradientThenRenderGradient) {
+  MockDispatchHelper helper;
+
+  // "Render" a dithered non-gradient.
+  helper.setDither(true);
+  EXPECT_FALSE(helper.paint().isDither());
+
+  // "Render" a gradient.
+  helper.setColorSource(kTestLinearGradient.get());
+  EXPECT_TRUE(helper.paint().isDither());
+}
+
+// https://github.com/flutter/flutter/issues/132860.
+TEST(DisplayListUtils, SetDitherTrueThenGradientTHenNonGradientThenGradient) {
+  MockDispatchHelper helper;
+
+  // "Render" a dithered gradient.
+  helper.setDither(true);
+  helper.setColorSource(kTestLinearGradient.get());
+  EXPECT_TRUE(helper.paint().isDither());
+
+  // "Render" a non-gradient.
+  helper.setColorSource(nullptr);
+  EXPECT_FALSE(helper.paint().isDither());
+
+  // "Render" a gradient again.
+  helper.setColorSource(kTestLinearGradient.get());
+  EXPECT_TRUE(helper.paint().isDither());
 }
 
 }  // namespace testing
