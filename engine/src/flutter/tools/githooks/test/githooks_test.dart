8d8a108d8aa650d99fd0faaf8dca4bac47eb4153
Matan Lurey <matanlurey@users.noreply.github.com>
2023-08-22 18:02:23 -0700

    Enable clang-tidy for pre-push (opt-out), exclude `performance-unnecessary-value-param` (flutter/engine#44936)
    Closes https://github.com/flutter/flutter/issues/132687.
    
    Zach, this is a pretty naive attempt, so feel free to suggest alternatives. I considered for example just using `--checks=-{{LINT}}` as well, but figured that might be less discoverable (especially for non-core folks) than just a file called `.clang-tidy-for-githooks`.
    
    /cc @jonahwilliams

--- engine/src/flutter/tools/githooks/test/githooks_test.dart
+++ engine/src/flutter/tools/githooks/test/githooks_test.dart
@@ -5,7 +5,9 @@
 import 'dart:io' as io;
 
 import 'package:githooks/githooks.dart';
+import 'package:githooks/src/pre_push_command.dart';
 import 'package:litetest/litetest.dart';
+import 'package:path/path.dart' as path;
 
 void main() {
   test('Fails gracefully without a command', () async {
@@ -66,4 +68,86 @@ void main() {
     }
     expect(result, equals(1));
   });
+
+  group('findMostRelevantCompileCommands', () {
+    late io.Directory fakeEngineRoot;
+    late io.Directory fakeFlutterRoot;
+
+    // We can't use standard setUp because this package uses 'litetest'.
+    void setUp() {
+      fakeEngineRoot = io.Directory.systemTemp.createTempSync('flutter_tools_githooks_test');
+      fakeFlutterRoot = io.Directory(path.join(fakeEngineRoot.path, 'flutter'));
+      fakeFlutterRoot.createSync(recursive: true);
+    }
+
+    void createHostFor(String target, {DateTime? lastModified}) {
+      final io.Directory host = io.Directory(path.join(fakeEngineRoot.path, 'out', target));
+      host.createSync(recursive: true);
+
+      final io.File compileCommands = io.File(path.join(host.path, 'compile_commands.json'));
+      compileCommands.createSync();
+      if (lastModified != null) {
+        compileCommands.setLastModifiedSync(lastModified);
+      }
+    }
+
+    test('returns null if there are no built outputs', () {
+      setUp();
+
+      expect(
+        PrePushCommand.findMostRelevantCompileCommands(fakeFlutterRoot.path, verbose: false),
+        isNull,
+      );
+    });
+
+    test('returns the most recently modified compile_commands.json', () {
+      setUp();
+
+      // Assume host_debug_unopt was created on 8/5, and then *_arm64 on 8/6.
+      createHostFor('host_debug_unopt', lastModified: DateTime(2023, 8, 5));
+      createHostFor('host_debug_unopt_arm64', lastModified: DateTime(2023, 8, 6));
+
+      expect(
+        PrePushCommand.findMostRelevantCompileCommands(fakeFlutterRoot.path, verbose: false)!.path,
+        equals(path.join(fakeEngineRoot.path, 'out', 'host_debug_unopt_arm64', 'compile_commands.json')),
+      );
+    });
+
+    test('in verbose mode, if there are multiple outputs, prints all of them', () {
+      final StringBuffer outBuffer = StringBuffer();
+      io.IOOverrides.runZoned(() {
+        setUp();
+
+        createHostFor('host_debug_unopt', lastModified: DateTime(2023, 8, 5));
+        createHostFor('host_debug_unopt_arm64', lastModified: DateTime(2023, 8, 6));
+        PrePushCommand.findMostRelevantCompileCommands(fakeFlutterRoot.path, verbose: true);
+      }, stdout: () => _BufferedStdOut(outBuffer));
+
+      final String outString = outBuffer.toString();
+      expect(outString, contains('out/host_debug_unopt/compile_commands.json'));
+      expect(outString, contains('out/host_debug_unopt_arm64/compile_commands.json'));
+    });
+  });
+}
+
+final class _BufferedStdOut implements io.Stdout {
+  _BufferedStdOut(this.buffer);
+
+  final StringBuffer buffer;
+
+  // We don't need to implement any other methods.
+  @override
+  dynamic noSuchMethod(Invocation invocation) {
+    return super.noSuchMethod(invocation);
+  }
+
+  @override
+  void write(Object? obj) {
+    buffer.write(obj);
+  }
+
+  @override
+  void writeln([Object? obj = '']) {
+    buffer.writeln(obj);
+  }
 }
