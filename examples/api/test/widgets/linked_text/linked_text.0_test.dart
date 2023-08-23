5c920d7386a08899b9253106151fdd5511c42c0d
Justin McCandless <jmccandless@google.com>
2023-08-23 12:14:52 -0700

    Test first example

--- examples/api/test/widgets/linked_text/linked_text.0_test.dart
+++ examples/api/test/widgets/linked_text/linked_text.0_test.dart
@@ -2,16 +2,24 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:flutter/painting.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_api_samples/widgets/linked_text/linked_text.0.dart' as example;
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  testWidgets('pressing shouldPop button changes shouldPop', (WidgetTester tester) async {
+  testWidgets('tapping a link shows a dialog with the tapped uri', (WidgetTester tester) async {
     await tester.pumpWidget(
       const example.LinkedTextApp(),
     );
 
-    expect(find.byType(InlineLinkedText), findsNWidgets(3));
+    final Finder textFinder = find.descendant(
+      of: find.byType(LinkedText),
+      matching: find.byType(RichText),
+    );
+    expect(textFinder, findsOneWidget);
+
+    await tester.tapAt(tester.getCenter(textFinder));
+    await tester.pumpAndSettle();
+    expect(find.text('You tapped: www.flutter.dev'), findsOneWidget);
   });
 }
