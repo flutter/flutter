9881f4846b5a1553b8f5e2b082a4b686b9738863
Ian Hickson <ian@hixie.ch>
2023-08-22 20:22:59 -0700

    Make web tests start with an empty title (flutter/engine#43846)
    Fixes https://github.com/flutter/flutter/issues/39159

--- engine/src/flutter/lib/web_ui/dev/test_platform.dart
+++ engine/src/flutter/lib/web_ui/dev/test_platform.dart
@@ -544,7 +544,6 @@ class BrowserPlatform extends PlatformPlugin {
         <!DOCTYPE html>
         <html>
         <head>
-          <title>${htmlEscape.convert(test)} Test</title>
           <meta name="assetBase" content="/">
           <script>
             window.flutterConfiguration = {
