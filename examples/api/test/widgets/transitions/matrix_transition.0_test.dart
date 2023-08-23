629b1ca9c1d7647174ae1dbade37f69b056c38a9
gmilou <139406084+gmilou@users.noreply.github.com>
2023-08-23 08:16:15 -0700

    Update examples/api/test/widgets/transitions/matrix_transition.0_test.dart
    Co-authored-by: Taha Tesser <tessertaha@gmail.com>

--- examples/api/test/widgets/transitions/matrix_transition.0_test.dart
+++ examples/api/test/widgets/transitions/matrix_transition.0_test.dart
@@ -8,9 +8,49 @@ import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   testWidgets('Shows Flutter logo inside a MatrixTransition', (WidgetTester tester) async {
-    await tester.pumpWidget(const example.MatrixTransitionExampleApp());
+    await tester.pumpWidget(
+      const example.MatrixTransitionExampleApp(),
+    );
 
-    expect(find.byType(MatrixTransition), findsOneWidget);
-    expect(find.byType(FlutterLogo), findsOneWidget);
+    final Finder transformFinder = find.ancestor(
+      of: find.byType(FlutterLogo),
+      matching: find.byType(MatrixTransition),
+    );
+    expect(transformFinder, findsOneWidget);
+  });
+
+  testWidgets('MatrixTransition animates', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      const example.MatrixTransitionExampleApp(),
+    );
+
+    final Finder transformFinder = find.ancestor(
+      of: find.byType(FlutterLogo),
+      matching: find.byType(Transform),
+    );
+
+    Transform transformBox = tester.widget(transformFinder);
+    Matrix4 actualTransform = transformBox.transform;
+
+    // Check initial transform.
+    expect(actualTransform, Matrix4.fromList(<double>[
+      1.0, 0.0, 0.0, 0.0,
+      0.0, 1.0, 0.0, 0.0,
+      0.0, 0.0, 1.0, 0.0,
+      0.0, 0.0, 0.004, 1.0,
+    ])..transpose());
+
+    // Animate half way.
+    await tester.pump(const Duration(seconds: 1));
+    transformBox = tester.widget(transformFinder);
+    actualTransform = transformBox.transform;
+
+    // The transform should be updated.
+    expect(actualTransform, isNot(Matrix4.fromList(<double>[
+      1.0, 0.0, 0.0, 0.0,
+      0.0, 1.0, 0.0, 0.0,
+      0.0, 0.0, 1.0, 0.0,
+      0.0, 0.0, 0.004, 1.0,
+    ])..transpose()));
   });
 }
