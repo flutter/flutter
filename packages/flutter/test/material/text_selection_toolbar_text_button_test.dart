537367dcb518bf42b5f2ec30d46370e3ce5985cd
Renzo Olivares <rmolivares@renzo-olivares.dev>
2023-08-24 13:37:41 -0700

    Add tests

--- packages/flutter/test/material/text_selection_toolbar_text_button_test.dart
+++ packages/flutter/test/material/text_selection_toolbar_text_button_test.dart
@@ -123,5 +123,105 @@ void main() {
         customForegroundColor,
       );
     });
+
+    testWidgetsWithLeakTracking('background color by default', (WidgetTester tester) async {
+      // Regression test for https://github.com/flutter/flutter/issues/133027
+      await tester.pumpWidget(
+        MaterialApp(
+          theme: ThemeData(
+            colorScheme: colorScheme,
+          ),
+          home: Scaffold(
+            body: Center(
+              child: TextSelectionToolbarTextButton(
+                padding: TextSelectionToolbarTextButton.getPadding(0, 1),
+                child: const Text('button'),
+              ),
+            ),
+          ),
+        ),
+      );
+
+      expect(find.byType(TextButton), findsOneWidget);
+
+      final TextButton textButton = tester.widget(find.byType(TextButton));
+      // The background color is hardcoded to grey or white by default, not the
+      // default value from ColorScheme.surface.
+      expect(
+        textButton.style!.backgroundColor!.resolve(<MaterialState>{}),
+        switch (colorScheme.brightness) {
+          Brightness.light => const Color(0xffffffff),
+          Brightness.dark => const Color(0xff424242),
+        },
+      );
+    });
+
+    testWidgetsWithLeakTracking('textButtonTheme should not override default background color', (WidgetTester tester) async {
+      // Regression test for https://github.com/flutter/flutter/issues/133027
+      await tester.pumpWidget(
+        MaterialApp(
+          theme: ThemeData(
+            colorScheme: colorScheme,
+            textButtonTheme: const TextButtonThemeData(
+              style: ButtonStyle(
+                backgroundColor: MaterialStatePropertyAll<Color>(Colors.blue),
+              ),
+            ),
+          ),
+          home: Scaffold(
+            body: Center(
+              child: TextSelectionToolbarTextButton(
+                padding: TextSelectionToolbarTextButton.getPadding(0, 1),
+                child: const Text('button'),
+              ),
+            ),
+          ),
+        ),
+      );
+
+      expect(find.byType(TextButton), findsOneWidget);
+
+      final TextButton textButton = tester.widget(find.byType(TextButton));
+      // The background color is hardcoded to grey or white by default, not the
+      // default value from ColorScheme.surface.
+      expect(
+        textButton.style!.backgroundColor!.resolve(<MaterialState>{}),
+        switch (colorScheme.brightness) {
+          Brightness.light => const Color(0xffffffff),
+          Brightness.dark => const Color(0xff424242),
+        },
+      );
+    });
+
+    testWidgetsWithLeakTracking('custom background color', (WidgetTester tester) async {
+      // Regression test for https://github.com/flutter/flutter/issues/133027
+      const Color customBackgroundColor = Colors.red;
+
+      await tester.pumpWidget(
+        MaterialApp(
+          theme: ThemeData(
+            colorScheme: colorScheme.copyWith(
+              surface: customBackgroundColor,
+            ),
+          ),
+          home: Scaffold(
+            body: Center(
+              child: TextSelectionToolbarTextButton(
+                padding: TextSelectionToolbarTextButton.getPadding(0, 1),
+                child: const Text('button'),
+              ),
+            ),
+          ),
+        ),
+      );
+
+      expect(find.byType(TextButton), findsOneWidget);
+
+      final TextButton textButton = tester.widget(find.byType(TextButton));
+      expect(
+        textButton.style!.backgroundColor!.resolve(<MaterialState>{}),
+        customBackgroundColor,
+      );
+    });
   }
 }
