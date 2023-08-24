cfed1b12359042557f3fe4542c58cdde5190d5aa
Polina Cherkasova <polina.c@live.com>
2023-08-24 15:37:11 -0700

    Update drawer.dart

--- packages/flutter/lib/src/material/drawer.dart
+++ packages/flutter/lib/src/material/drawer.dart
@@ -471,6 +471,7 @@ class DrawerControllerState extends State<DrawerController> with SingleTickerPro
   void dispose() {
     _historyEntry?.remove();
     _controller.dispose();
+    _focusScopeNode.dispose();
     super.dispose();
   }
 
