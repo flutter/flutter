16f563221a5de2f416ef924b8285108326a8d3e3
Polina Cherkasova <polina.c@live.com>
2023-08-25 14:06:38 -0700

    Update focus_manager.dart

--- packages/flutter/lib/src/widgets/focus_manager.dart
+++ packages/flutter/lib/src/widgets/focus_manager.dart
@@ -1467,6 +1467,10 @@ class FocusManager with DiagnosticableTreeMixin, ChangeNotifier {
   /// handlers, callers must call [registerGlobalHandlers]. See the
   /// documentation in that method for caveats to watch out for.
   FocusManager() {
+    if (kFlutterMemoryAllocationsEnabled) {
+      maybeDispatchObjectCreation();
+    }
+
     rootScope._manager = this;
   }
 
