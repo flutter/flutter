faaa2ff5173725ecdf3af1766117abb1d863c780
Yatendra Kumar <yatendraroria2001@gmail.com>
2023-08-26 01:31:05 +0530

    Fix: Complete Documentation for RasterStatus::kSkipAndRetry (#44880)
    ## Description
    
    This PR completes the documentation for `RasterStatus::kSkipAndRetry` in the `compositor_context.h` file. The previous comment ended abruptly, leaving ambiguity regarding the behavior when the thread merger is disabled.
    
    ### Changes:
    - Added clarification on the behavior when the thread merger is disabled.
    - Explained the potential performance implications when threads are not merged.
    
    ## Related Issues
    
    https://github.com/flutter/flutter/issues/131814
    
    ## Tests
    
    No tests were added as this PR only involves documentation changes.
    
    [C++, Objective-C, Java style guides]: https://github.com/flutter/engine/blob/main/CONTRIBUTING.md#style

--- flow/compositor_context.h
+++ flow/compositor_context.h
@@ -40,9 +40,11 @@ enum class RasterStatus {
   // This is currently used to wait for the thread merger to merge
   // the raster and platform threads.
   //
-  // Since the thread merger may be disabled,
+  // Since the thread merger may be disabled, the system will proceed
+  // with separate threads for rasterization and platform tasks,
+  // potentially leading to different performance characteristics.
   kSkipAndRetry,
-  // Frame has been successfully rasterized, but "there are additional items in
+  // Frame has been successfully rasterized, but there are additional items in
   // the pipeline waiting to be consumed. This is currently
   // only used when thread configuration change occurs.
   kEnqueuePipeline,
