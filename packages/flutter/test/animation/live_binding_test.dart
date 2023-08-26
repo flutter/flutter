ecf050498d7d2a88cd79a5e3896f25802e676c4e
Tong Mu <dkwingsmt@gmail.com>
2023-08-25 17:22:09 -0700

    Async implementation

--- packages/flutter/test/animation/live_binding_test.dart
+++ packages/flutter/test/animation/live_binding_test.dart
@@ -23,6 +23,7 @@ void main() {
 
   testWidgetsWithLeakTracking('Should show event indicator for pointer events', (WidgetTester tester) async {
     final AnimationSheetBuilder animationSheet = AnimationSheetBuilder(frameSize: const Size(200, 200), allLayers: true);
+    addTearDown(animationSheet.dispose);
     final List<Offset> taps = <Offset>[];
     Widget target({bool recording = true}) => Container(
       padding: const EdgeInsets.fromLTRB(20, 10, 25, 20),
@@ -81,6 +82,7 @@ void main() {
 
   testWidgetsWithLeakTracking('Should show event indicator for pointer events with setSurfaceSize', (WidgetTester tester) async {
     final AnimationSheetBuilder animationSheet = AnimationSheetBuilder(frameSize: const Size(200, 200), allLayers: true);
+    addTearDown(animationSheet.dispose);
     final List<Offset> taps = <Offset>[];
     Widget target({bool recording = true}) => Container(
       padding: const EdgeInsets.fromLTRB(20, 10, 25, 20),
@@ -131,13 +133,10 @@ void main() {
     await tester.pumpFrames(target(), const Duration(milliseconds: 50));
     expect(taps, isEmpty);
 
-    final ui.Image image = await animationSheet.collate(6);
-
     await expectLater(
-      image,
+      animationSheet.collate(6),
       matchesGoldenFile('LiveBinding.press.animation.2.png'),
     );
-    image.dispose();
   },
     skip: isBrowser, // [intended] https://github.com/flutter/flutter/issues/56001
     // TODO(polina-c): remove after fixing https://github.com/flutter/flutter/issues/133071
