e4b66c60bc4be9657bab868dc1c1c3b2047227fd
Matan Lurey <matanlurey@users.noreply.github.com>
2023-08-29 16:04:22 -0700

    Refactor `Stopwatch` into `StopwatchVisualizer` and `SkStopwatchVisualizer` (flutter/engine#45200)
    Partial work towards https://github.com/flutter/flutter/issues/126009.
    
    Still working as before:
    <img width="1210" alt="Screenshot 2023-08-28 at 6 32 45 PM"
    src="https://github.com/flutter/engine/assets/168174/38728015-d0d4-4933-bd31-d2326c76aeee">
    
    (There are some `PerformanceOverlayLayerDefault.*` tests that don't run
    locally, so I guess I'll let CI run those)

--- engine/src/flutter/flow/stopwatch_unittests.cc
+++ engine/src/flutter/flow/stopwatch_unittests.cc
@@ -42,5 +42,20 @@ TEST(Instrumentation, GetFrameBudgetFromUpdaterTest) {
   EXPECT_EQ(frame_budget_90fps, actual_frame_budget);
 }
 
+TEST(Instrumentation, GetLapByIndexTest) {
+  fml::Milliseconds frame_budget_90fps = fml::RefreshRateToFrameBudget(90);
+  FixedRefreshRateStopwatch stopwatch(frame_budget_90fps);
+  stopwatch.SetLapTime(fml::TimeDelta::FromMilliseconds(10));
+  EXPECT_EQ(stopwatch.GetLap(1), fml::TimeDelta::FromMilliseconds(10));
+}
+
+TEST(Instrumentation, GetCurrentSampleTest) {
+  fml::Milliseconds frame_budget_90fps = fml::RefreshRateToFrameBudget(90);
+  FixedRefreshRateStopwatch stopwatch(frame_budget_90fps);
+  stopwatch.Start();
+  stopwatch.Stop();
+  EXPECT_EQ(stopwatch.GetCurrentSample(), size_t(1));
+}
+
 }  // namespace testing
 }  // namespace flutter
