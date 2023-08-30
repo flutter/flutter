6c7e807bda62ae5331842aff42e2060b78c46613
Camille Simon <43054281+camsim99@users.noreply.github.com>
2023-08-30 10:50:31 -0700

    Update packages/flutter_tools/lib/src/android/gradle_utils.dart
    Co-authored-by: Gray Mackall <34871572+gmackall@users.noreply.github.com>

--- packages/flutter_tools/lib/src/android/gradle_utils.dart
+++ packages/flutter_tools/lib/src/android/gradle_utils.dart
@@ -574,7 +574,7 @@ bool validateJavaAgp(Logger logger,
   return false;
   }
 
-  // Returns compatibility information for concerning the compatible minimum AGP
+  // Returns compatibility information concerning the minimum AGP
   // version for the specified Java version.
   JavaAgpCompat? getMinimumAgpVersionForJavaVersion(Logger logger,
     {required String? javaV}) {
