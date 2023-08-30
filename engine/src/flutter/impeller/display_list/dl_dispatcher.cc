732d38e0aefa4a2d60cf8b3509039075ccd288d3
Jonah Williams <jonahwilliams@google.com>
2023-08-29 18:30:59 -0700

    [Impeller] transform text path offsets so color sources match expected offsets for gradients. (flutter/engine#45255)
    Gradient offsets are computed based on path offsets + gradient positions, so we need to shift the points instead of just adding a concat to the canvas. Fixes https://github.com/flutter/flutter/issues/132972
    
    ### Skia
    
    ![flutter_03](https://github.com/flutter/engine/assets/8975114/182dff72-cd48-4f97-a1e3-ea924f4a0eee)
    
    ### Impeller (No Patch)
    
    ![flutter_04](https://github.com/flutter/engine/assets/8975114/bb421d86-a588-49b9-879f-209738821898)
    
    ### Impeller (W/ Patch)
    
    ![flutter_05](https://github.com/flutter/engine/assets/8975114/f2d601b4-d15f-487b-aa65-a61bfc98bde9)

--- engine/src/flutter/impeller/display_list/dl_dispatcher.cc
+++ engine/src/flutter/impeller/display_list/dl_dispatcher.cc
@@ -1118,12 +1118,10 @@ void DlDispatcher::drawTextBlob(const sk_sp<SkTextBlob>& blob,
   const auto text_frame = maybe_text_frame.value();
   if (paint_.style == Paint::Style::kStroke ||
       paint_.color_source.GetType() != ColorSource::Type::kColor) {
-    auto path = skia_conversions::PathDataFromTextBlob(blob);
     auto bounds = blob->bounds();
-    canvas_.Save();
-    canvas_.Translate({x + bounds.left(), y + bounds.top(), 0.0});
+    auto path = skia_conversions::PathDataFromTextBlob(blob);
+    path.Shift(Point(x + bounds.left(), y + bounds.top()));
     canvas_.DrawPath(path, paint_);
-    canvas_.Restore();
     return;
   }
 
