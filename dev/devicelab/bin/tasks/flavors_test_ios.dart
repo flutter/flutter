5649161bd7fc591e313c12f35fccfe4c8e211dbe
Victoria Ashworth <15619084+vashworth@users.noreply.github.com>
2023-09-01 17:17:48 -0500

    Revert "fix install checks in flavors devicelab tests" (#133894)
    Reverts flutter/flutter#133719
    
    `Windows_android flavors_test_win` failed twice in a row in the tree

--- dev/devicelab/bin/tasks/flavors_test_ios.dart
+++ dev/devicelab/bin/tasks/flavors_test_ios.dart
@@ -14,35 +14,30 @@ Future<void> main() async {
     await createFlavorsTest().call();
     await createIntegrationTestFlavorsTest().call();
     // test install and uninstall of flavors app
-    final TaskResult installTestsResult = await inDirectory(
-      '${flutterDirectory.path}/dev/integration_tests/flavors',
-      () async {
-        await flutter(
-          'install',
-          options: <String>['--flavor', 'paid'],
-        );
-        await flutter(
-          'install',
-          options: <String>['--flavor', 'paid', '--uninstall-only'],
-        );
-        final StringBuffer stderr = StringBuffer();
-        await evalFlutter(
-          'install',
-          canFail: true,
-          stderr: stderr,
-          options: <String>['--flavor', 'bogus'],
-        );
+    await inDirectory('${flutterDirectory.path}/dev/integration_tests/flavors', () async {
+      await flutter(
+        'install',
+        options: <String>['--flavor', 'paid'],
+      );
+      await flutter(
+        'install',
+        options: <String>['--flavor', 'paid', '--uninstall-only'],
+      );
+      final StringBuffer stderr = StringBuffer();
+      await evalFlutter(
+        'install',
+        canFail: true,
+        stderr: stderr,
+        options: <String>['--flavor', 'bogus'],
+      );
 
-        final String stderrString = stderr.toString();
-        if (!stderrString.contains('The Xcode project defines schemes: free, paid')) {
-          print(stderrString);
-          return TaskResult.failure('Should not succeed with bogus flavor');
-        }
+      final String stderrString = stderr.toString();
+      if (!stderrString.contains('install failed, bogus flavor not found')) {
+        print(stderrString);
+        return TaskResult.failure('Should not succeed with bogus flavor');
+      }
+    });
 
-        return TaskResult.success(null);
-      },
-    );
-
-    return installTestsResult;
+    return TaskResult.success(null);
   });
 }
