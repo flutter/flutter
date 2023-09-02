be8bb23633add0823ab580c9c1b06bf5a6af0410
Jonah Williams <jonahwilliams@google.com>
2023-09-01 18:01:04 -0700

    [Impeller] Make paths externally immutable, update all tests to use PathBuilder to create Path. (flutter/engine#45393)
    This is a design problem in the Flutter API, that in general you can't assume a Path hasn't changed from the last time you used it. We're so close, so just move the mutation methods to PathBuilder :)
    
    Fixes https://github.com/flutter/flutter/issues/133880

--- engine/src/flutter/impeller/display_list/dl_dispatcher.cc
+++ engine/src/flutter/impeller/display_list/dl_dispatcher.cc
@@ -1118,8 +1118,8 @@ void DlDispatcher::drawTextBlob(const sk_sp<SkTextBlob> blob,
   if (paint_.style == Paint::Style::kStroke ||
       paint_.color_source.GetType() != ColorSource::Type::kColor) {
     auto bounds = blob->bounds();
-    auto path = skia_conversions::PathDataFromTextBlob(blob);
-    path.Shift(Point(x + bounds.left(), y + bounds.top()));
+    auto path = skia_conversions::PathDataFromTextBlob(
+        blob, Point(x + bounds.left(), y + bounds.top()));
     canvas_.DrawPath(path, paint_);
     return;
   }
