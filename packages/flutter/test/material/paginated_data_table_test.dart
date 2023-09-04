6a902afc0c9b8cfb41a49cce1524e835a6dedf7f
Polina Cherkasova <polina.c@live.com>
2023-09-03 18:53:01 -0700

    -

--- packages/flutter/test/material/paginated_data_table_test.dart
+++ packages/flutter/test/material/paginated_data_table_test.dart
@@ -6,7 +6,6 @@ import 'package:flutter/gestures.dart' show DragStartBehavior;
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:leak_tracker_flutter_testing/leak_tracker_flutter_testing.dart';
 
 import 'data_table_test_utils.dart';
 
