353a63352226071bbfdf00b842e8e078f3437a0f
Andrew Kolos <andrewrkolos@gmail.com>
2023-09-06 22:16:00 -0700

    minor doc string tweak

--- packages/flutter/lib/src/services/flavor.dart
+++ packages/flutter/lib/src/services/flavor.dart
@@ -2,6 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-/// The flavor this app was built with. Equivalent to the value of the
+/// The flavor this app was built with. Equivalent to the value argued to the
 /// `--flavor` option at build time.
 const String flavor = String.fromEnvironment('FLUTTER_APP_FLAVOR');
