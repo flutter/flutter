033f7c918f42e72cda5b88e653a11727765fa897
Konstantin Sokolovskyi <sokolovskyi.konstantin@gmail.com>
2023-09-07 12:22:49 +0200

    Revert _routeInformation disposal, Marked leak in router.dart

--- packages/flutter/lib/src/widgets/router.dart
+++ packages/flutter/lib/src/widgets/router.dart
@@ -734,7 +734,6 @@ class _RouterState<T> extends State<Router<T>> with RestorationMixin {
     widget.backButtonDispatcher?.removeCallback(_handleBackButtonDispatcherNotification);
     widget.routerDelegate.removeListener(_handleRouterDelegateNotification);
     _currentRouterTransaction = null;
-    _routeInformation.dispose();
     super.dispose();
   }
 
