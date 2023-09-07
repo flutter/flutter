410c1fe38901ce440675bfb0c8636842189e8cf6
Andrew Kolos <andrewrkolos@gmail.com>
2023-09-07 13:27:43 -0700

    comment formatting
    Co-authored-by: Christopher Fujino <fujino@google.com>

--- packages/flutter/lib/src/services/flavor.dart
+++ packages/flutter/lib/src/services/flavor.dart
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-/// The flavor this app was built with. Equivalent to the value argued to the
-/// `--flavor` option at build time.
+/// The flavor this app was built with.
+///
+/// Equivalent to the value argued to the `--flavor` option at build time.
 const String flavor = String.fromEnvironment('FLUTTER_APP_FLAVOR');
