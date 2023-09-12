446d7765888cb127623e55fe37e5d88aad5acbfc
Chinmay Garde <chinmaygarde@google.com>
2023-09-12 16:14:22 -0700

    [Impeller] Fix swapchain recreation for non-polling cases. (flutter/engine#45740)
    Earlier, there was no recovery from vk::Result::eSuboptimalKHR. This made the playground be stuck with a swapchain image size of 1x1 with suboptimal error code on acquisition indefinitely.
    
    Towards fixing the playgrounds. There are still some validation errors.

--- engine/src/flutter/impeller/renderer/backend/vulkan/swapchain_impl_vk.cc
+++ engine/src/flutter/impeller/renderer/backend/vulkan/swapchain_impl_vk.cc
@@ -375,15 +375,20 @@ SwapchainImplVK::AcquireResult SwapchainImplVK::AcquireNextDrawable() {
       nullptr               // fence
   );
 
-  if (acq_result == vk::Result::eErrorOutOfDateKHR) {
-    return AcquireResult{true /* out of date */};
-  }
-
-  if (acq_result != vk::Result::eSuccess &&
-      acq_result != vk::Result::eSuboptimalKHR) {
-    VALIDATION_LOG << "Could not acquire next swapchain image: "
-                   << vk::to_string(acq_result);
-    return {};
+  switch (acq_result) {
+    case vk::Result::eSuccess:
+      // Keep going.
+      break;
+    case vk::Result::eSuboptimalKHR:
+    case vk::Result::eErrorOutOfDateKHR:
+      // A recoverable error. Just say we are out of date.
+      return AcquireResult{true /* out of date */};
+      break;
+    default:
+      // An unrecoverable error.
+      VALIDATION_LOG << "Could not acquire next swapchain image: "
+                     << vk::to_string(acq_result);
+      return AcquireResult{false /* out of date */};
   }
 
   if (index >= images_.size()) {
