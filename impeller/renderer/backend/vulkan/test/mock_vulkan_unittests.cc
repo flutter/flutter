1f068b42e3bfd5ae21e449b2472304c279850ef4
Matan Lurey <matanlurey@users.noreply.github.com>
2023-09-12 09:43:53 -0700

    [Impeller] Make `CreateMockVulkanContext()` thread-safe (#45687)
    I started using `CreateMockVulkanContext()` in
    https://github.com/flutter/engine/pull/45654, and discovered that it was
    not thread-safe. This PR does a little bit of refactoring/TLC to ensure
    that functions and buffers can be accessed across threads (like the
    _real_ `ContextVK`).
    
    The attached test (_test for a test fixture, hoorah!_) fails
    consistently before the changes, and passes after. Note that if you try
    testing this with `--gtest_repeat=10000` (at least that's the threshold
    for me), it'll fail - that's actually due to
    https://github.com/flutter/flutter/issues/134482 which in turn is being
    fixed in https://github.com/flutter/engine/pull/45686.
    
    (Unblocks https://github.com/flutter/flutter/issues/133198)
    
    ---------
    
    Co-authored-by: gaaclarke <30870216+gaaclarke@users.noreply.github.com>

---
+++ impeller/renderer/backend/vulkan/test/mock_vulkan_unittests.cc
@@ -0,0 +1,37 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "flutter/testing/testing.h"  // IWYU pragma: keep
+#include "gtest/gtest.h"
+#include "impeller/renderer/backend/vulkan/command_pool_vk.h"
+#include "impeller/renderer/backend/vulkan/test/mock_vulkan.h"
+
+namespace impeller {
+namespace testing {
+
+TEST(MockVulkanContextTest, IsThreadSafe) {
+  // In a typical app, there is a single ContextVK per app, shared b/w threads.
+  //
+  // This test ensures that the (mock) ContextVK is thread-safe.
+  auto const context = CreateMockVulkanContext();
+
+  // Spawn two threads, and have them create a CommandPoolVK each.
+  std::thread thread1([&context]() {
+    auto const pool = CommandPoolVK::GetThreadLocal(context.get());
+    EXPECT_TRUE(pool);
+  });
+
+  std::thread thread2([&context]() {
+    auto const pool = CommandPoolVK::GetThreadLocal(context.get());
+    EXPECT_TRUE(pool);
+  });
+
+  thread1.join();
+  thread2.join();
+
+  context->Shutdown();
+}
+
+}  // namespace testing
+}  // namespace impeller
