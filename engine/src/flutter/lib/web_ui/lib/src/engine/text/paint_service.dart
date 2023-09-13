5579e6a2de133081b187c8009132991ac178a586
Mouad Debbar <mdebbar@google.com>
2023-09-13 17:13:05 -0400

    [web] Use DOM to render paragraphs with letter spacing (flutter/engine#45651)
    <**_Only applies to the HTML renderer_**>
    
    When a paragraph has letter spacing that's greater than 0, we attempt to render each character individually on the canvas, while adding extra gaps between the characters (because the browser's canvas API doesn't support letter spacing).
    
    This works well-ish in English but causes issues with international text. It also makes text rendering very slow.
    
    With this PR, the idea is to force all paragraphs with letter spacing to be rendered to a DOM element.
    
    Fixes https://github.com/flutter/flutter/issues/51234
    Fixes https://github.com/flutter/flutter/issues/71220

--- engine/src/flutter/lib/web_ui/lib/src/engine/text/paint_service.dart
+++ engine/src/flutter/lib/web_ui/lib/src/engine/text/paint_service.dart
@@ -4,7 +4,6 @@
 
 import 'package:ui/ui.dart' as ui;
 
-import '../dom.dart';
 import '../html/bitmap_canvas.dart';
 import '../html/painting.dart';
 import 'canvas_paragraph.dart';
@@ -78,23 +77,7 @@ class TextPaintService {
     final EngineTextStyle style = fragment.style;
 
     final String text = fragment.getText(paragraph);
-    final double? letterSpacing = style.letterSpacing;
-    if (letterSpacing == null || letterSpacing == 0.0) {
-      canvas.drawText(text, x, y,
-          style: style.foreground?.style, shadows: style.shadows);
-    } else {
-      // TODO(mdebbar): Implement letter-spacing on canvas more efficiently:
-      //                https://github.com/flutter/flutter/issues/51234
-      double charX = x;
-      final int len = text.length;
-      for (int i = 0; i < len; i++) {
-        final String char = text[i];
-        canvas.drawText(char, charX.roundToDouble(), y,
-            style: style.foreground?.style,
-            shadows: style.shadows);
-        charX += letterSpacing + canvas.measureText(char).width!;
-      }
-    }
+    canvas.drawText(text, x, y, style: style.foreground?.style, shadows: style.shadows);
 
     canvas.tearDownPaint();
   }
