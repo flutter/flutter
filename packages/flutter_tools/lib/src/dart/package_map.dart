1f61b7501d2d5c9e71ed758f934e40c910254795
Daco Harkes <dacoharkes@google.com>
2023-09-13 09:12:23 +0200

    Fix analysis

--- packages/flutter_tools/lib/src/dart/package_map.dart
+++ packages/flutter_tools/lib/src/dart/package_map.dart
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'dart:typed_data';
-
 import 'package:package_config/package_config.dart';
 
 import '../base/common.dart';
