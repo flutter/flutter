e802145bdeff4697bce2a09c8f67a10d419356b0
Polina Cherkasova <polina.c@live.com>
2023-09-19 10:59:48 -0700

    Update editable_text_test.dart

--- packages/flutter/test/widgets/editable_text_test.dart
+++ packages/flutter/test/widgets/editable_text_test.dart
@@ -3785,14 +3785,7 @@ void main() {
       await tester.pumpAndSettle();
 
       verifyAutocorrectionRectVisibility(expectVisible: false);
-    },
-    leakTrackingTestConfig: LeakTrackingTestConfig.debugNotDisposed(),
-    // leakTrackingTestConfig: const LeakTrackingTestConfig(
-    //   // TODO(ksokolovskyi): remove after fixing
-    //   // https://github.com/flutter/flutter/issues/134386
-    //   notDisposedAllowList: <String, int?> {'LeaderLayer': 5},
-    // ),
-  );
+    });
 
   testWidgetsWithLeakTracking('Changing controller updates EditableText', (WidgetTester tester) async {
     final TextEditingController controller1 =
