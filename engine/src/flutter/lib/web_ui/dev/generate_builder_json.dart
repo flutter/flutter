7f3c15c54377de2d4d13ed7339518fe8d61fc2c1
Jackson Gardner <jacksongardner@google.com>
2023-09-19 09:27:49 -0700

    Update CI to Chrome 117 (flutter/engine#45842)

--- engine/src/flutter/lib/web_ui/dev/generate_builder_json.dart
+++ engine/src/flutter/lib/web_ui/dev/generate_builder_json.dart
@@ -154,7 +154,7 @@ Iterable<dynamic> _getTestStepsForPlatform(
           if (suite.runConfig.browser == BrowserName.chrome)
             <String, dynamic>{
               'dependency': 'chrome_and_driver',
-              'version': 'version:115.0',
+              'version': 'version:117.0',
             },
           if (suite.runConfig.browser == BrowserName.firefox)
             <String, dynamic>{
