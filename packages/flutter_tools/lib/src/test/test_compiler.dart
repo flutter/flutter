7962b5481bd7aac5cbfd8de0459a76e840035320
Xilai Zhang <xilaizhang@google.com>
2023-09-19 17:06:45 -0700

    [flutter roll] Revert "Native assets support for Linux" (#135069)
    Reverts flutter/flutter#134031
    
    context: b/301051367
    Looked at the error message from the broken TAP target, but seems like the failure might be non trivial to resolve. Would it be okay if we revert this for now while it is being triaged?

--- packages/flutter_tools/lib/src/test/test_compiler.dart
+++ packages/flutter_tools/lib/src/test/test_compiler.dart
@@ -16,7 +16,6 @@ import '../bundle.dart';
 import '../compile.dart';
 import '../flutter_plugins.dart';
 import '../globals.dart' as globals;
-import '../linux/native_assets.dart';
 import '../macos/native_assets.dart';
 import '../native_assets.dart';
 import '../project.dart';
@@ -182,15 +181,7 @@ class TestCompiler {
           flutterTester: true,
           fileSystem: globals.fs,
           buildRunner: buildRunner,
-        );
-      } else if (globals.platform.isLinux) {
-        (nativeAssetsYaml, _) = await buildNativeAssetsLinux(
-          buildMode: BuildMode.debug,
-          projectUri: projectUri,
-          flutterTester: true,
-          fileSystem: globals.fs,
-          buildRunner: buildRunner,
-        );
+      );
       } else {
         await ensureNoNativeAssetsOrOsIsSupported(
           projectUri,
