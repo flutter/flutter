53451f43a08dbb8b7f62c8f6cdee751008fa0bd8
Yegor <yjbanov@google.com>
2023-09-21 12:38:20 -0700

    a few web tweaks for a11y assessment app (#134479)
    Mostly tweaks for better focus management, namely:
    
    * Use `autofocus` throughout so the a11y focus is transferred to a logical place when overlaid content pops up (screen transitions, dialogs).
    * Consolidate "enabled" and "disabled" widgets into the same screen. Otherwise, when only a disabled widget is shown, there's nothing to focus on and the screen reader is lost.

--- dev/a11y_assessments/lib/main.dart
+++ dev/a11y_assessments/lib/main.dart
@@ -2,12 +2,23 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 import 'use_cases/use_cases.dart';
 
+// TODO(yjbanov): https://github.com/flutter/flutter/issues/83809
+//                Currently this app (as most Flutter Web apps) relies on the
+//                `autofocus` property to guide the a11y focus when navigating
+//                across routes (screen transitions, dialogs, etc). We may want
+//                to revisit this after we figure out a long-term story for a11y
+//                focus. See also https://github.com/flutter/flutter/issues/97747
 void main() {
   runApp(const App());
+  if (kIsWeb) {
+    SemanticsBinding.instance.ensureSemantics();
+  }
 }
 
 class App extends StatelessWidget {
@@ -36,12 +47,13 @@ class App extends StatelessWidget {
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
-  Widget _buildUseCaseItem(UseCase useCase) {
+  Widget _buildUseCaseItem(int index, UseCase useCase) {
     return Padding(
       padding: const EdgeInsets.all(10),
       child: Builder(
           builder: (BuildContext context) {
             return TextButton(
+              autofocus: index == 0,
               key: Key(useCase.name),
               onPressed: () => Navigator.of(context).pushNamed(useCase.route),
               child: Text(useCase.name),
@@ -57,7 +69,10 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(title: const Text('Accessibility Assessments')),
       body: Center(
         child: ListView(
-          children: useCases.map<Widget>(_buildUseCaseItem).toList(),
+          children: List<Widget>.generate(
+            useCases.length,
+            (int index) => _buildUseCaseItem(index, useCases[index]),
+          ),
         ),
       ),
     );
--- dev/a11y_assessments/lib/use_cases/use_cases.dart
+++ dev/a11y_assessments/lib/use_cases/use_cases.dart
@@ -5,12 +5,10 @@
 import 'package:flutter/widgets.dart';
 
 import 'check_box_list_tile.dart';
-import 'check_box_list_tile_disabled.dart';
 import 'date_picker.dart';
 import 'dialog.dart';
 import 'slider.dart';
 import 'text_field.dart';
-import 'text_field_disabled.dart';
 import 'text_field_password.dart';
 
 abstract class UseCase {
@@ -21,11 +19,9 @@ abstract class UseCase {
 
 final List<UseCase> useCases = <UseCase>[
   CheckBoxListTile(),
-  CheckBoxListTileDisabled(),
   DialogUseCase(),
   SliderUseCase(),
   TextFieldUseCase(),
-  TextFieldDisabledUseCase(),
   TextFieldPasswordUseCase(),
   DatePickerUseCase(),
 ];
