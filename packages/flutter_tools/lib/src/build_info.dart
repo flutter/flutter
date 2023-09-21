b523c83a34aac1095e2502d79e6d534d0c472d49
Daco Harkes <dacoharkes@google.com>
2023-09-21 11:53:25 +0200

    Fix analysis

--- packages/flutter_tools/lib/src/build_info.dart
+++ packages/flutter_tools/lib/src/build_info.dart
@@ -185,7 +185,7 @@ class BuildInfo {
   final bool assumeInitializeFromDillUpToDate;
 
   /// If set, builds native assets with `build.dart` from all packages.
-  /// 
+  ///
   /// Set to false in g3, because native assets are not build via `build.dart`.
   final bool buildNativeAssets;
 
