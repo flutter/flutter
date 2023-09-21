172411fc8e03a7487f0bad658bff88b64a8bee9b
Matan Lurey <matanlurey@users.noreply.github.com>
2023-09-20 18:41:27 -0700

    Tidy up `DlPaint` and friends. (flutter/engine#46120)
    Closes https://github.com/flutter/flutter/issues/135058.
    
    Work towards https://github.com/flutter/flutter/issues/134969.
    
    All fixes are generated by `clang-tidy --fix`, and manual search/replace
    if that wasn't sufficient.

--- engine/src/flutter/display_list/dl_paint.cc
+++ engine/src/flutter/display_list/dl_paint.cc
@@ -7,33 +7,33 @@
 namespace flutter {
 
 DlPaint::DlPaint(DlColor color)
-    : blendMode_(static_cast<unsigned>(DlBlendMode::kDefaultMode)),
-      drawStyle_(static_cast<unsigned>(DlDrawStyle::kDefaultStyle)),
-      strokeCap_(static_cast<unsigned>(DlStrokeCap::kDefaultCap)),
-      strokeJoin_(static_cast<unsigned>(DlStrokeJoin::kDefaultJoin)),
-      isAntiAlias_(false),
-      isDither_(false),
-      isInvertColors_(false),
+    : blend_mode_(static_cast<unsigned>(DlBlendMode::kDefaultMode)),
+      draw_style_(static_cast<unsigned>(DlDrawStyle::kDefaultStyle)),
+      stroke_cap_(static_cast<unsigned>(DlStrokeCap::kDefaultCap)),
+      stroke_join_(static_cast<unsigned>(DlStrokeJoin::kDefaultJoin)),
+      is_anti_alias_(false),
+      is_dither_(false),
+      is_invert_colors_(false),
       color_(color),
-      strokeWidth_(kDefaultWidth),
-      strokeMiter_(kDefaultMiter) {}
+      stroke_width_(kDefaultWidth),
+      stroke_miter_(kDefaultMiter) {}
 
 bool DlPaint::operator==(DlPaint const& other) const {
-  return blendMode_ == other.blendMode_ &&            //
-         drawStyle_ == other.drawStyle_ &&            //
-         strokeCap_ == other.strokeCap_ &&            //
-         strokeJoin_ == other.strokeJoin_ &&          //
-         isAntiAlias_ == other.isAntiAlias_ &&        //
-         isDither_ == other.isDither_ &&              //
-         isInvertColors_ == other.isInvertColors_ &&  //
-         color_ == other.color_ &&                    //
-         strokeWidth_ == other.strokeWidth_ &&        //
-         strokeMiter_ == other.strokeMiter_ &&        //
-         Equals(colorSource_, other.colorSource_) &&  //
-         Equals(colorFilter_, other.colorFilter_) &&  //
-         Equals(imageFilter_, other.imageFilter_) &&  //
-         Equals(maskFilter_, other.maskFilter_) &&    //
-         Equals(pathEffect_, other.pathEffect_);
+  return blend_mode_ == other.blend_mode_ &&              //
+         draw_style_ == other.draw_style_ &&              //
+         stroke_cap_ == other.stroke_cap_ &&              //
+         stroke_join_ == other.stroke_join_ &&            //
+         is_anti_alias_ == other.is_anti_alias_ &&        //
+         is_dither_ == other.is_dither_ &&                //
+         is_invert_colors_ == other.is_invert_colors_ &&  //
+         color_ == other.color_ &&                        //
+         stroke_width_ == other.stroke_width_ &&          //
+         stroke_miter_ == other.stroke_miter_ &&          //
+         Equals(color_source_, other.color_source_) &&    //
+         Equals(color_filter_, other.color_filter_) &&    //
+         Equals(image_filter_, other.image_filter_) &&    //
+         Equals(mask_filter_, other.mask_filter_) &&      //
+         Equals(path_effect_, other.path_effect_);
 }
 
 const DlPaint DlPaint::kDefault;
