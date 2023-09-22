3ade44e1c73178aa591f98cd107619e2ad3c6bf8
Dan Field <dnfield@google.com>
2023-09-21 17:31:00 -0700

    [Impeller] fail if software backend is chosen and Impeller is enabled on iOS (flutter/engine#46124)
    Fixes https://github.com/flutter/flutter/issues/127408
    
    This is also related to https://github.com/flutter/engine/pull/44346, which made it fatal to explicitly request both impeller and the software backend.
    
    Before landing this, we need to update some google internal tests that end up in this state to explicitly request Skia (or to get into a mode where they can actually use metal).

--- engine/src/flutter/shell/platform/darwin/ios/ios_context.mm
+++ engine/src/flutter/shell/platform/darwin/ios/ios_context.mm
@@ -2,14 +2,15 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import "flutter/shell/platform/darwin/ios/ios_context.h"
+#include "flutter/shell/platform/darwin/ios/ios_context.h"
+#include "flutter/shell/platform/darwin/ios/rendering_api_selection.h"
 
 #include "flutter/fml/logging.h"
-#import "flutter/shell/platform/darwin/ios/ios_context_software.h"
+#include "flutter/shell/platform/darwin/ios/ios_context_software.h"
 
 #if SHELL_ENABLE_METAL
-#import "flutter/shell/platform/darwin/ios/ios_context_metal_impeller.h"
-#import "flutter/shell/platform/darwin/ios/ios_context_metal_skia.h"
+#include "flutter/shell/platform/darwin/ios/ios_context_metal_impeller.h"
+#include "flutter/shell/platform/darwin/ios/ios_context_metal_skia.h"
 #endif  // SHELL_ENABLE_METAL
 
 namespace flutter {
@@ -25,6 +26,11 @@ std::unique_ptr<IOSContext> IOSContext::Create(
     std::shared_ptr<const fml::SyncSwitch> is_gpu_disabled_sync_switch) {
   switch (api) {
     case IOSRenderingAPI::kSoftware:
+      FML_CHECK(backend != IOSRenderingBackend::kImpeller)
+          << "Software rendering is incompatible with Impeller.\n"
+             "Software rendering may have been automatically selected when running on a simulator "
+             "in an environment that does not support Metal. Enabling GPU pass through in your "
+             "environment may fix this. If that is not possible, then disable Impeller.";
       return std::make_unique<IOSContextSoftware>();
 #if SHELL_ENABLE_METAL
     case IOSRenderingAPI::kMetal:
