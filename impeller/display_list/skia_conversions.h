82c78ad8405eb0f726137be6d2eef1efb753dd76
Jonah Williams <jonahwilliams@google.com>
2023-09-27 12:48:19 -0700

    [Impeller] Match Skia gradient clamping behavior (and document). (#44825)
    Skia clamps any gradient stops to values of 0.0 to 1.0. Implement this behavior in Impeller and document it in dart:ui (Framework also needs to be documented). This also matches the w3c gradient behavior: https://www.w3.org/TR/2000/CR-SVG-20000802/pservers.html - almost. We might be slightly off with how we're inserting additional stops for 0.0 and 1.0, but at least its closer.
    
    Fixes https://github.com/flutter/flutter/issues/132792

--- impeller/display_list/skia_conversions.h
+++ impeller/display_list/skia_conversions.h
@@ -5,6 +5,7 @@
 #pragma once
 
 #include "display_list/dl_color.h"
+#include "display_list/effects/dl_color_source.h"
 #include "impeller/core/formats.h"
 #include "impeller/geometry/color.h"
 #include "impeller/geometry/path.h"
@@ -46,6 +47,24 @@ Path PathDataFromTextBlob(const sk_sp<SkTextBlob>& blob,
                           Point shift = Point(0, 0));
 
 std::optional<impeller::PixelFormat> ToPixelFormat(SkColorType type);
+
+/// @brief Convert display list colors + stops into impeller colors and stops,
+/// taking care to ensure that the stops monotonically increase from 0.0 to 1.0.
+///
+/// The general process is:
+/// * Ensure that the first gradient stop value is 0.0. If not, insert a new
+///   stop with a value of 0.0 and use the first gradient color as this new
+///   stops color.
+/// * Ensure the last gradient stop value is 1.0. If not, insert a new stop
+///   with a value of 1.0 and use the last gradient color as this stops color.
+/// * Clamp all gradient values between the values of 0.0 and 1.0.
+/// * For all stop values, ensure that the values are monotonically increasing
+///   by clamping each value to a minimum of the previous stop value and itself.
+///   For example, with stop values of 0.0, 0.5, 0.4, 1.0, we would clamp such
+///   that the values were 0.0, 0.5, 0.5, 1.0.
+void ConvertStops(const flutter::DlGradientColorSourceBase* gradient,
+                  std::vector<Color>& colors,
+                  std::vector<float>& stops);
 
 }  // namespace skia_conversions
 }  // namespace impeller
