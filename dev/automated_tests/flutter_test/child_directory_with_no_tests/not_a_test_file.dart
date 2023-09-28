34cc40a2579995271081bd664efcb91575bc31b0
Andrew Kolos <andrewrkolos@gmail.com>
2023-09-28 06:40:40 -0700

    add missing license header

--- dev/automated_tests/flutter_test/child_directory_with_no_tests/not_a_test_file.dart
+++ dev/automated_tests/flutter_test/child_directory_with_no_tests/not_a_test_file.dart
@@ -1,4 +1,8 @@
-// File that does not end in "_test"
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// File that does not end in "_test".
 
 import 'package:flutter_test/flutter_test.dart';
 
