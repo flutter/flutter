aa8dd4faa7854e548f976c3f42d2c4f921bb49fa
Matan Lurey <matanlurey@users.noreply.github.com>
2023-09-30 09:43:58 -0700

    [Impeller] Make some Open GL errors non-fatal, check in debug mode (not unopt). (flutter/engine#46434)
    Closes https://github.com/flutter/flutter/issues/135767.

--- engine/src/flutter/impeller/renderer/backend/gles/proc_table_gles.cc
+++ engine/src/flutter/impeller/renderer/backend/gles/proc_table_gles.cc
@@ -32,6 +32,20 @@ const char* GLErrorToString(GLenum value) {
   return "Unknown.";
 }
 
+bool GLErrorIsFatal(GLenum value) {
+  switch (value) {
+    case GL_NO_ERROR:
+      return false;
+    case GL_INVALID_ENUM:
+    case GL_INVALID_VALUE:
+    case GL_INVALID_OPERATION:
+    case GL_INVALID_FRAMEBUFFER_OPERATION:
+    case GL_OUT_OF_MEMORY:
+      return true;
+  }
+  return false;
+}
+
 ProcTableGLES::Resolver WrappedResolver(
     const ProcTableGLES::Resolver& resolver) {
   return [resolver](const char* function_name) -> void* {
