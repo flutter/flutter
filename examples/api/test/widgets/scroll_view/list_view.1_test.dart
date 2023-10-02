f6d866e6376b0c695b47a5955c4696f8056576d2
Gildásio Filho <gildasiogx@gmail.com>
2023-10-02 18:56:14 -0300

    Create list_view.1_test.dart

---
+++ examples/api/test/widgets/scroll_view/list_view.1_test.dart
@@ -0,0 +1,31 @@
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+import 'package:flutter_api_samples/widgets/scroll_view/list_view.1.dart'
+    as example;
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets(
+    'tapping Reverse button should reverse ListView',
+    (WidgetTester tester) async {
+      await tester.pumpWidget(
+        const example.ListViewExampleApp(),
+      );
+
+      final Finder listView = find.byType(ListView);
+      final Finder reverseFinder = find.text('Reverse items');
+      expect(listView, findsOneWidget);
+      expect(reverseFinder, findsOneWidget);
+      final Finder keepAliveFinder = find.byType(KeepAlive);
+      KeepAlive firstWidget = tester.firstWidget(keepAliveFinder);
+      expect(firstWidget.data, '1');
+      await tester.tap(reverseFinder);
+      await tester.pump();
+      firstWidget = tester.firstWidget(keepAliveFinder);
+      expect(firstWidget.data, '5');
+    },
+  );
+}
