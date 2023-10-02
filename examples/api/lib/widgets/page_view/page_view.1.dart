6e299192aba898e6680cf02d90b1f964cc3769e3
Gildásio Filho <gildasiogx@gmail.com>
2023-10-02 18:26:49 -0300

    Create page_view.1.dart

---
+++ examples/api/lib/widgets/page_view/page_view.1.dart
@@ -0,0 +1,100 @@
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+
+/// Flutter code sample for a [PageView] using the `findChildIndexCallback` argument
+
+void main() => runApp(const PageViewExampleApp());
+
+class PageViewExampleApp extends StatelessWidget {
+  const PageViewExampleApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text('PageView Sample')),
+        body: const PageViewExample(),
+      ),
+    );
+  }
+}
+
+class PageViewExample extends StatefulWidget {
+  const PageViewExample({super.key});
+
+  @override
+  State<PageViewExample> createState() => _PageViewExampleState();
+}
+
+class _PageViewExampleState extends State<PageViewExample> {
+  List<String> items = <String>['1', '2', '3', '4', '5'];
+
+  void _reverse() {
+    setState(() {
+      items = items.reversed.toList();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: PageView.custom(
+          childrenDelegate: SliverChildBuilderDelegate(
+            (BuildContext context, int index) {
+              return KeepAlive(
+                data: items[index],
+                key: ValueKey<String>(items[index]),
+              );
+            },
+            childCount: items.length,
+            findChildIndexCallback: (Key key) {
+              final ValueKey<String> valueKey = key as ValueKey<String>;
+              final String data = valueKey.value;
+              final int index = items.indexOf(data);
+              if (index >= 0) {
+                return index;
+              }
+              return null;
+            },
+          ),
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            TextButton(
+              onPressed: () => _reverse(),
+              child: const Text('Reverse items'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class KeepAlive extends StatefulWidget {
+  const KeepAlive({super.key, required this.data});
+
+  final String data;
+
+  @override
+  State<KeepAlive> createState() => _KeepAliveState();
+}
+
+class _KeepAliveState extends State<KeepAlive>
+    with AutomaticKeepAliveClientMixin {
+  @override
+  bool get wantKeepAlive => true;
+
+  @override
+  Widget build(BuildContext context) {
+    super.build(context);
+    return Text(widget.data);
+  }
+}
