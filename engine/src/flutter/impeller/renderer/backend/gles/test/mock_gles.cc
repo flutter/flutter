b6dfcba94b2f3dd649845c42b60bcbff251b91d8
Matan Lurey <matanlurey@users.noreply.github.com>
2023-10-03 16:23:25 -0600

    [Impeller] Implement a `MockGLES`, that provides trampolines for `ProcGLESTable` (flutter/engine#46433)
    Unblocks testing https://github.com/flutter/flutter/issues/135715. See
    `README.md` for details.
    
    I made one non-testing change to the proc_table itself, which is if a
    function call will fail, we will print out what is about to fail in the
    validation log. It was useful when debugging the test creation itself,
    and it's only enabled when GL call checks are enabled anyway.
    
    @gaaclarke I originally implemented it with `FML_THREAD_LOCAL`, but
    figured doing a global lock essentially was the same thing, and would
    prevent parallel test runs from stepping on each-other in weird ways?
    
    /cc @chinmaygarde for visibility.

---
+++ engine/src/flutter/impeller/renderer/backend/gles/test/mock_gles.cc
@@ -0,0 +1,147 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <memory>
+
+#include "GLES3/gl3.h"
+#include "fml/logging.h"
+#include "impeller/renderer/backend/gles/proc_table_gles.h"
+#include "impeller/renderer/backend/gles/test/mock_gles.h"
+
+namespace impeller {
+namespace testing {
+
+// OpenGLES is not thread safe.
+//
+// This mutex is used to ensure that only one test is using the mock at a time.
+static std::mutex g_test_lock;
+
+static std::weak_ptr<MockGLES> g_mock_gles;
+
+// Has friend visibility into MockGLES to record calls.
+void RecordGLCall(const char* name) {
+  if (auto mock_gles = g_mock_gles.lock()) {
+    mock_gles->RecordCall(name);
+  }
+}
+
+template <typename T, typename U>
+struct CheckSameSignature : std::false_type {};
+
+template <typename Ret, typename... Args>
+struct CheckSameSignature<Ret(Args...), Ret(Args...)> : std::true_type {};
+
+// This is a stub function that does nothing/records nothing.
+void doNothing() {}
+
+auto const kMockVendor = (unsigned char*)"MockGLES";
+auto const kMockVersion = (unsigned char*)"3.0";
+auto const kExtensions = std::vector<unsigned char*>{
+    (unsigned char*)"GL_KHR_debug"  //
+};
+
+const unsigned char* mockGetString(GLenum name) {
+  switch (name) {
+    case GL_VENDOR:
+      return kMockVendor;
+    case GL_VERSION:
+      return kMockVersion;
+    case GL_SHADING_LANGUAGE_VERSION:
+      return kMockVersion;
+    default:
+      return (unsigned char*)"";
+  }
+}
+
+static_assert(CheckSameSignature<decltype(mockGetString),  //
+                                 decltype(glGetString)>::value);
+
+const unsigned char* mockGetStringi(GLenum name, GLuint index) {
+  switch (name) {
+    case GL_EXTENSIONS:
+      return kExtensions[index];
+    default:
+      return (unsigned char*)"";
+  }
+}
+
+static_assert(CheckSameSignature<decltype(mockGetStringi),  //
+                                 decltype(glGetStringi)>::value);
+
+void mockGetIntegerv(GLenum name, int* value) {
+  switch (name) {
+    case GL_NUM_EXTENSIONS: {
+      *value = kExtensions.size();
+    } break;
+    case GL_MAX_COMBINED_TEXTURE_IMAGE_UNITS:
+      *value = 8;
+      break;
+    default:
+      *value = 0;
+      break;
+  }
+}
+
+static_assert(CheckSameSignature<decltype(mockGetIntegerv),  //
+                                 decltype(glGetIntegerv)>::value);
+
+GLenum mockGetError() {
+  return GL_NO_ERROR;
+}
+
+static_assert(CheckSameSignature<decltype(mockGetError),  //
+                                 decltype(glGetError)>::value);
+
+void mockPopDebugGroupKHR() {
+  RecordGLCall("PopDebugGroupKHR");
+}
+
+static_assert(CheckSameSignature<decltype(mockPopDebugGroupKHR),  //
+                                 decltype(glPopDebugGroupKHR)>::value);
+
+void mockPushDebugGroupKHR(GLenum source,
+                           GLuint id,
+                           GLsizei length,
+                           const GLchar* message) {
+  RecordGLCall("PushDebugGroupKHR");
+}
+
+static_assert(CheckSameSignature<decltype(mockPushDebugGroupKHR),  //
+                                 decltype(glPushDebugGroupKHR)>::value);
+
+std::shared_ptr<MockGLES> MockGLES::Init() {
+  // If we cannot obtain a lock, MockGLES is already being used elsewhere.
+  FML_CHECK(g_test_lock.try_lock())
+      << "MockGLES is already being used by another test.";
+  auto mock_gles = std::shared_ptr<MockGLES>(new MockGLES());
+  g_mock_gles = mock_gles;
+  return mock_gles;
+}
+
+const ProcTableGLES::Resolver kMockResolver = [](const char* name) {
+  if (strcmp(name, "glPopDebugGroupKHR") == 0) {
+    return reinterpret_cast<void*>(&mockPopDebugGroupKHR);
+  } else if (strcmp(name, "glPushDebugGroupKHR") == 0) {
+    return reinterpret_cast<void*>(&mockPushDebugGroupKHR);
+  } else if (strcmp(name, "glGetString") == 0) {
+    return reinterpret_cast<void*>(&mockGetString);
+  } else if (strcmp(name, "glGetStringi") == 0) {
+    return reinterpret_cast<void*>(&mockGetStringi);
+  } else if (strcmp(name, "glGetIntegerv") == 0) {
+    return reinterpret_cast<void*>(&mockGetIntegerv);
+  } else if (strcmp(name, "glGetError") == 0) {
+    return reinterpret_cast<void*>(&mockGetError);
+  } else {
+    return reinterpret_cast<void*>(&doNothing);
+  }
+};
+
+MockGLES::MockGLES() : proc_table_(kMockResolver) {}
+
+MockGLES::~MockGLES() {
+  g_test_lock.unlock();
+}
+
+}  // namespace testing
+}  // namespace impeller
---
+++ engine/src/flutter/impeller/renderer/backend/gles/test/mock_gles.h
@@ -0,0 +1,57 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <memory>
+#include "fml/macros.h"
+#include "impeller/renderer/backend/gles/proc_table_gles.h"
+
+namespace impeller {
+namespace testing {
+
+/// @brief      Provides a mocked version of the |ProcTableGLES| class.
+///
+/// Typically, Open GLES at runtime will be provided the host's GLES bindings
+/// (as function pointers). This class maintains a set of function pointers that
+/// appear to be GLES functions, but are actually just stubs that record
+/// invocations.
+///
+/// See `README.md` for more information.
+class MockGLES final {
+ public:
+  /// @brief      Returns an initialized |MockGLES| instance.
+  ///
+  /// This method overwrites mocked global GLES function pointers to record
+  /// invocations on this instance of |MockGLES|. As such, it should only be
+  /// called once per test.
+  static std::shared_ptr<MockGLES> Init();
+
+  /// @brief      Returns a configured |ProcTableGLES| instance.
+  const ProcTableGLES& GetProcTable() const { return proc_table_; }
+
+  /// @brief      Returns a vector of the names of all recorded calls.
+  ///
+  /// Calls are cleared after this method is called.
+  std::vector<std::string> GetCapturedCalls() {
+    std::vector<std::string> calls = captured_calls_;
+    captured_calls_.clear();
+    return calls;
+  }
+
+  ~MockGLES();
+
+ private:
+  friend void RecordGLCall(const char* name);
+
+  MockGLES();
+
+  void RecordCall(const char* name) { captured_calls_.emplace_back(name); }
+
+  const ProcTableGLES proc_table_;
+  std::vector<std::string> captured_calls_;
+
+  FML_DISALLOW_COPY_AND_ASSIGN(MockGLES);
+};
+
+}  // namespace testing
+}  // namespace impeller
