14b687d91262a977df928f06c47e7e302fd3c3ee
jonahwilliams <jonahwilliams@google.com>
2023-10-11 13:35:30 -0700

    ++

--- examples/hello_world/lib/main.dart
+++ examples/hello_world/lib/main.dart
@@ -1,65 +1,16 @@
-import 'package:flutter/material.dart';
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
-void main() {
-  runApp(const MyApp());
-}
+import 'package:flutter/widgets.dart';
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  late final controller = OverlayPortalController();
-
-  @override
-  Widget build(BuildContext context) {
-    return OverlayPortal(
-      controller: controller,
-      overlayChildBuilder: (context) => Positioned.fill(
-        child: GestureDetector(
-          onTap: () => controller.hide(),
-          child: TweenAnimationBuilder(
-            duration: const Duration(milliseconds: 2000),
-            tween: Tween(begin: 0.0, end: 1.0),
-            builder: (context, value, child) {
-              return Opacity(
-                opacity: value,
-                child: Container(
-                  color: Colors.black.withOpacity(value),
-                ),
-              );
-            },
-          ),
-        ),
-      ),
-      child: Scaffold(
-        body: Center(
-          child: OutlinedButton(
-            onPressed: () => controller.show(),
-            child: const Text("Hello, world"),
-          ),
+void main() =>
+  runApp(
+    const Center(
+      child:
+        Text('Hello, world!',
+          key: Key('title'),
+          textDirection: TextDirection.ltr,
         ),
       ),
     );
-  }
-}
