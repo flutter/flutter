bef97b06dbc1413c3cf865a9e47d47659e39e929
Victoria Ashworth <vashworth@google.com>
2023-10-12 15:48:50 -0500

    temporarily run screenshot test in Mac_ios microbenchmarks_ios_xcode_debug

--- dev/devicelab/bin/tasks/microbenchmarks_ios_xcode_debug.dart
+++ dev/devicelab/bin/tasks/microbenchmarks_ios_xcode_debug.dart
@@ -2,20 +2,56 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'dart:io';
+
 import 'package:flutter_devicelab/framework/devices.dart';
 import 'package:flutter_devicelab/framework/framework.dart';
-import 'package:flutter_devicelab/tasks/microbenchmarks.dart';
+import 'package:flutter_devicelab/framework/host_agent.dart';
+import 'package:flutter_devicelab/framework/task_result.dart';
+import 'package:flutter_devicelab/framework/utils.dart';
+import 'package:path/path.dart' as path;
 
-/// Runs microbenchmarks on iOS.
 Future<void> main() async {
-  // XcodeDebug workflow is used for CoreDevices (iOS 17+ and Xcode 15+). Use
-  // FORCE_XCODE_DEBUG environment variable to force the use of XcodeDebug
-  // workflow in CI to test from older versions since devicelab has not yet been
-  // updated to iOS 17 and Xcode 15.
-  deviceOperatingSystem = DeviceOperatingSystem.ios;
-  await task(createMicrobenchmarkTask(
-    environment: <String, String>{
-      'FORCE_XCODE_DEBUG': 'true',
-    },
-  ));
+  await task(() async {
+    try {
+      final Directory? dumpDirectory = hostAgent.dumpDirectory;
+      if (dumpDirectory == null) {
+        return TaskResult.success(null);
+      }
+
+      // On command failure try uploading screenshot of failing command.
+      final String screenshotPath = path.join(
+        dumpDirectory.path,
+        'device-screenshot-${DateTime.now().toLocal().toIso8601String()}.png',
+      );
+
+      deviceOperatingSystem = DeviceOperatingSystem.ios;
+      final String deviceId = (await devices.workingDevice).deviceId;
+      print('Taking screenshot of working device $deviceId at $screenshotPath');
+      final int exitCode = await flutter(
+        'screenshot',
+        options: <String>[
+          '--out',
+          screenshotPath,
+          '-d', deviceId,
+        ],
+      );
+
+      if (exitCode != 0) {
+        return TaskResult.failure('Failed to take screenshot.');
+      }
+
+      final File screenshot = File(screenshotPath);
+
+      if (!screenshot.existsSync() || screenshot.readAsBytesSync().isEmpty) {
+        return TaskResult.failure('Screenshot not created.');
+      }
+
+      return TaskResult.success(null);
+    } on TaskResult catch (taskResult) {
+      return taskResult;
+    } catch (e) {
+      return TaskResult.failure(e.toString());
+    }
+  });
 }
