3ef32f040765f6da3e5ea94b4471d90e0fae3081
Greg Spencer <gspencergoog@users.noreply.github.com>
2023-10-13 17:25:18 -0700

    Change some usage of RawKeyEvent to KeyEvent in preparation for deprecation (#136420)
    ## Description
    
    This converts some usages of `RawKeyEvent` to `KeyEvent` to prepare the repo for deprecation of `RawKeyEvent`, and swaps out the `raw_keyboard.dart` manual test for `hardware_keyboard.dart`.
    
    ## Related Issues
     - https://github.com/flutter/flutter/issues/136419
    
    ## Tests
     - Just refactoring code, no semantic changes.

---
+++ dev/manual_tests/lib/hardware_keyboard.dart
@@ -0,0 +1,94 @@
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'Hardware Key Demo',
+    home: Scaffold(
+      appBar: AppBar(
+        title: const Text('Hardware Key Demo'),
+      ),
+      body: const Center(
+        child: HardwareKeyboardDemo(),
+      ),
+    ),
+  ));
+}
+
+class HardwareKeyboardDemo extends StatefulWidget {
+  const HardwareKeyboardDemo({super.key});
+
+  @override
+  State<HardwareKeyboardDemo> createState() => _HardwareKeyboardDemoState();
+}
+
+class _HardwareKeyboardDemoState extends State<HardwareKeyboardDemo> {
+  final FocusNode _focusNode = FocusNode();
+  KeyEvent? _event;
+
+  @override
+  void dispose() {
+    _focusNode.dispose();
+    super.dispose();
+  }
+
+  KeyEventResult _handleKeyEvent(FocusNode node, KeyEvent event) {
+    setState(() {
+      _event = event;
+    });
+    return KeyEventResult.ignored;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final TextTheme textTheme = Theme.of(context).textTheme;
+    return Focus(
+      focusNode: _focusNode,
+      onKeyEvent: _handleKeyEvent,
+      autofocus: true,
+      child: AnimatedBuilder(
+        animation: _focusNode,
+        builder: (BuildContext context, Widget? child) {
+          if (!_focusNode.hasFocus) {
+            return GestureDetector(
+              onTap: () {
+                _focusNode.requestFocus();
+              },
+              child: Text('Tap to focus', style: textTheme.headlineMedium),
+            );
+          }
+
+          if (_event == null) {
+            return Text('Press a key', style: textTheme.headlineMedium);
+          }
+
+          final List<Widget> dataText = <Widget>[
+            Text('${_event.runtimeType}'),
+            if (_event?.character?.isNotEmpty ?? false) Text('character produced: "${_event?.character}"'),
+          ];
+          dataText.add(Text('logical: ${_event?.logicalKey}'));
+          dataText.add(Text('physical: ${_event?.physicalKey}'));
+          if (_event?.character != null) {
+            dataText.add(Text('character: ${_event?.character}'));
+          }
+          final List<String> pressed = <String>['Pressed:'];
+          for (final LogicalKeyboardKey key in HardwareKeyboard.instance.logicalKeysPressed) {
+            pressed.add(key.debugName!);
+          }
+          dataText.add(Text(pressed.join(' ')));
+          return DefaultTextStyle(
+            style: textTheme.titleMedium!,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: dataText,
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
