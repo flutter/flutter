2568c94a360a82c15ddf4ffeeb33e03a5cf9e297
Polina Cherkasova <polina.c@live.com>
2023-10-16 20:23:00 -0700

    Update widget_tester.dart

--- packages/flutter_test/lib/src/widget_tester.dart
+++ packages/flutter_test/lib/src/widget_tester.dart
@@ -121,7 +121,7 @@ E? _lastWhereOrNull<E>(Iterable<E> list, bool Function(E) test) {
 /// When [experimentalLeakTracking] is set, it is used to configure leak tracking.
 /// Otherwise [LeakTrackingForTests.settings] is used.
 /// You can adjust [LeakTrackingForTests.settings] in flutter_test_config.dart for your
-/// package or folders, or in 'setUpAll' for the test library or group.
+/// package or folders, or in 'setUpAll' for a test library or group.
 /// If you set it for group, remember the original value to a local variable
 /// and restore it in `tearDownAll`.
 /// To pause leak tracking just for one test, set [experimentalLeakTracking] to
