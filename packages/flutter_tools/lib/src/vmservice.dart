7bffff7bcfb4cf8016c2f3b4c56571e516c08346
Sam Rawlins <srawlins@google.com>
2023-10-18 15:19:18 -0700

    Null-assert the value given to a Completer expecting a non-null value (#136776)
    The code with out the null assertion is legal as per the type signature, but will throw a runtime exception if the nullable value is null. To make this exception more explicit, the value must be null-checked before completing the completer with the value.
    
    The analyzer will soon enforce such checks. See https://github.com/dart-lang/sdk/issues/53253.
    
    Fixes https://github.com/flutter/flutter/issues/136775

--- packages/flutter_tools/lib/src/vmservice.dart
+++ packages/flutter_tools/lib/src/vmservice.dart
@@ -964,7 +964,7 @@ class FlutterVmService {
       if (event.kind == vm_service.EventKind.kServiceExtensionAdded
           && event.extensionRPC == extensionName) {
         isolateEvents.cancel();
-        extensionAdded.complete(event.isolate);
+        extensionAdded.complete(event.isolate!);
       }
     });
 
