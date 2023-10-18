b630473316212680d4e80531d3d6027cd0d9d687
Daco Harkes <dacoharkes@google.com>
2023-10-18 10:00:50 +0200

    Fix fakes

--- packages/flutter_tools/test/commands.shard/permeable/build_bundle_test.dart
+++ packages/flutter_tools/test/commands.shard/permeable/build_bundle_test.dart
@@ -524,7 +524,7 @@ class FakeBundleBuilder extends Fake implements BundleBuilder {
     String? applicationKernelFilePath,
     String? depfilePath,
     String? assetDirPath,
-    Uri? nativeAssets,
+    bool buildNativeAssets = true,
     @visibleForTesting BuildSystem? buildSystem,
   }) async {}
 }
