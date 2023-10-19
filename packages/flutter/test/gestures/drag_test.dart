8b05823c0aedd2ecbfc4fdd2419b1e0a9a67ae87
xu-baolin <xubaolin@oppo.com>
2023-10-19 14:56:42 +0800

    ++

--- packages/flutter/test/gestures/drag_test.dart
+++ packages/flutter/test/gestures/drag_test.dart
@@ -468,9 +468,9 @@ void main() {
 
   testGesture('Drag with multiple pointers in down behavior - default', (GestureTester tester) {
     final HorizontalDragGestureRecognizer drag1 =
-      HorizontalDragGestureRecognizer()..dragStartBehavior = DragStartBehavior.down;
+      HorizontalDragGestureRecognizer() ..dragStartBehavior = DragStartBehavior.down;
     final VerticalDragGestureRecognizer drag2 =
-      VerticalDragGestureRecognizer()..dragStartBehavior = DragStartBehavior.down;
+      VerticalDragGestureRecognizer() ..dragStartBehavior = DragStartBehavior.down;
     addTearDown(() => drag1.dispose);
     addTearDown(() => drag2.dispose);
 
