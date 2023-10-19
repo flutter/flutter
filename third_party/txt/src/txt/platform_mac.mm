bfd2ffb9a8bc6e00c4c18e17b9f9393c3ba81473
Chris Yang <ychris@google.com>
2023-10-19 10:52:31 -0700

    [iOS] Load additional Cupertino system fonts (#46857)
    Manually loads darwin system fonts. Darwin system fonts returns different typeface in different sizes. This PR loads the font in different sizes then register them as different fonts. These new fonts can be access through the framework.
    
    Fixes https://github.com/flutter/flutter/issues/63507
    
    [C++, Objective-C, Java style guides]: https://github.com/flutter/engine/blob/main/CONTRIBUTING.md#style

--- third_party/txt/src/txt/platform_mac.mm
+++ third_party/txt/src/txt/platform_mac.mm
@@ -5,7 +5,9 @@
 #include <TargetConditionals.h>
 
 #include "flutter/fml/platform/darwin/platform_version.h"
+#include "third_party/skia/include/ports/SkTypeface_mac.h"
 #include "txt/platform.h"
+#include "txt/platform_mac.h"
 
 #if TARGET_OS_EMBEDDED || TARGET_OS_SIMULATOR
 #include <UIKit/UIKit.h>
@@ -14,6 +16,15 @@
 #include <AppKit/AppKit.h>
 #define FONT_CLASS NSFont
 #endif  // TARGET_OS_EMBEDDED
+
+// Apple system font larger than size 29 returns SFProDisplay typeface.
+static const CGFloat kSFProDisplayBreakPoint = 29;
+// Apple system font smaller than size 16 returns SFProText typeface.
+static const CGFloat kSFProTextBreakPoint = 16;
+// Font name represents the "SF Pro Display" system font on Apple platforms.
+static const std::string kSFProDisplayName = "CupertinoSystemDisplay";
+// Font name represents the "SF Pro Text" system font on Apple platforms.
+static const std::string kSFProTextName = "CupertinoSystemText";
 
 namespace txt {
 
@@ -27,6 +38,36 @@ std::vector<std::string> GetDefaultFontFamilies() {
 
 sk_sp<SkFontMgr> GetDefaultFontManager(uint32_t font_initialization_data) {
   return SkFontMgr::RefDefault();
+}
+
+void RegisterSystemFonts(const DynamicFontManager& dynamic_font_manager) {
+  // iOS loads different system fonts when size is greater than 28 or lower
+  // than 17. The "familyName" property returned from CoreText stays the same
+  // despite the typeface is different.
+  //
+  // Below code manually loads and registers them as two different fonts
+  // so Flutter app can access them on macOS and iOS.
+  //
+  // Darwin system fonts from 17 to 28 also have dynamic spacing based on sizes.
+  // These two fonts do not match the spacings when sizes are from 17 to 28.
+  // The spacing should be handled by the app or the framework.
+  //
+  // See https://www.wwdcnotes.com/notes/wwdc20/10175/ for Apple's document on
+  // this topic.
+  sk_sp<SkTypeface> large_system_font = SkMakeTypefaceFromCTFont(
+      (CTFontRef)CFAutorelease(CTFontCreateUIFontForLanguage(
+          kCTFontUIFontSystem, kSFProDisplayBreakPoint, NULL)));
+  if (large_system_font) {
+    dynamic_font_manager.font_provider().RegisterTypeface(large_system_font,
+                                                          kSFProDisplayName);
+  }
+  sk_sp<SkTypeface> regular_system_font = SkMakeTypefaceFromCTFont(
+      (CTFontRef)CFAutorelease(CTFontCreateUIFontForLanguage(
+          kCTFontUIFontSystem, kSFProTextBreakPoint, NULL)));
+  if (regular_system_font) {
+    dynamic_font_manager.font_provider().RegisterTypeface(regular_system_font,
+                                                          kSFProTextName);
+  }
 }
 
 }  // namespace txt
