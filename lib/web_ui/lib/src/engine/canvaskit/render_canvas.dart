6c164f02958c3543c9debbe3072c327f1e3d695d
Harry Terkelsen <1961493+harryterkelsen@users.noreply.github.com>
2023-10-24 13:21:58 -0700

    Reland "Use a single OffscreenCanvas for rendering in CanvasKit (#45744)" (#47241)
    Using a single GL context avoids several issues with managing GL context
    lifecycle.
    
    ## Pre-launch Checklist
    
    - [x] I read the [Contributor Guide] and followed the process outlined
    there for submitting PRs.
    - [x] I read the [Tree Hygiene] wiki page, which explains my
    responsibilities.
    - [x] I read and followed the [Flutter Style Guide] and the [C++,
    Objective-C, Java style guides].
    - [x] I listed at least one issue that this PR fixes in the description
    above.
    - [x] I added new tests to check the change I am making or feature I am
    adding, or the PR is [test-exempt]. See [testing the engine] for
    instructions on writing and running engine tests.
    - [x] I updated/added relevant documentation (doc comments with `///`).
    - [x] I signed the [CLA].
    - [x] All existing and new tests are passing.
    
    If you need help, consider asking for advice on the #hackers-new channel
    on [Discord].
    
    <!-- Links -->
    [Contributor Guide]:
    https://github.com/flutter/flutter/wiki/Tree-hygiene#overview
    [Tree Hygiene]: https://github.com/flutter/flutter/wiki/Tree-hygiene
    [test-exempt]:
    https://github.com/flutter/flutter/wiki/Tree-hygiene#tests
    [Flutter Style Guide]:
    https://github.com/flutter/flutter/wiki/Style-guide-for-Flutter-repo
    [C++, Objective-C, Java style guides]:
    https://github.com/flutter/engine/blob/main/CONTRIBUTING.md#style
    [testing the engine]:
    https://github.com/flutter/flutter/wiki/Testing-the-engine
    [CLA]: https://cla.developers.google.com/
    [flutter/tests]: https://github.com/flutter/tests
    [breaking change policy]:
    https://github.com/flutter/flutter/wiki/Tree-hygiene#handling-breaking-changes
    [Discord]: https://github.com/flutter/flutter/wiki/Chat

---
+++ lib/web_ui/lib/src/engine/canvaskit/render_canvas.dart
@@ -0,0 +1,113 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'dart:js_interop';
+
+import 'package:ui/ui.dart' as ui;
+
+import '../dom.dart';
+import '../window.dart';
+
+/// A visible (on-screen) canvas that can display bitmaps produced by CanvasKit
+/// in the (off-screen) SkSurface which is backed by an OffscreenCanvas.
+///
+/// In a typical frame, the content will be rendered via CanvasKit in an
+/// OffscreenCanvas, and then the contents will be transferred to the
+/// RenderCanvas via `transferFromImageBitmap()`.
+///
+/// If we need more RenderCanvases, for example in the case where there are
+/// platform views and we need overlays to render the frame correctly, then
+/// we will create multiple RenderCanvases, but crucially still only have
+/// one OffscreenCanvas which transfers bitmaps to all of the RenderCanvases.
+///
+/// To render into the OffscreenCanvas with CanvasKit we need to create a
+/// WebGL context, which is not only expensive, but the browser has a limit
+/// on the maximum amount of WebGL contexts which can be live at once. Using
+/// a single OffscreenCanvas and multiple RenderCanvases allows us to only
+/// create a single WebGL context.
+class RenderCanvas {
+  RenderCanvas() {
+    canvasElement.setAttribute('aria-hidden', 'true');
+    canvasElement.style.position = 'absolute';
+    _updateLogicalHtmlCanvasSize();
+    htmlElement.append(canvasElement);
+  }
+
+  /// The root HTML element for this canvas.
+  ///
+  /// This element contains the canvas used to draw the UI. Unlike the canvas,
+  /// this element is permanent. It is never replaced or deleted, until this
+  /// canvas is disposed of via [dispose].
+  ///
+  /// Conversely, the canvas that lives inside this element can be swapped, for
+  /// example, when the screen size changes, or when the WebGL context is lost
+  /// due to the browser tab becoming dormant.
+  final DomElement htmlElement = createDomElement('flt-canvas-container');
+
+  /// The underlying `<canvas>` element used to display the pixels.
+  final DomCanvasElement canvasElement = createDomCanvasElement();
+  int _pixelWidth = 0;
+  int _pixelHeight = 0;
+
+  late final DomCanvasRenderingContextBitmapRenderer renderContext =
+      canvasElement.contextBitmapRenderer;
+
+  double _currentDevicePixelRatio = -1;
+
+  /// Sets the CSS size of the canvas so that canvas pixels are 1:1 with device
+  /// pixels.
+  ///
+  /// The logical size of the canvas is not based on the size of the window
+  /// but on the size of the canvas, which, due to `ceil()` above, may not be
+  /// the same as the window. We do not round/floor/ceil the logical size as
+  /// CSS pixels can contain more than one physical pixel and therefore to
+  /// match the size of the window precisely we use the most precise floating
+  /// point value we can get.
+  void _updateLogicalHtmlCanvasSize() {
+    final double logicalWidth = _pixelWidth / window.devicePixelRatio;
+    final double logicalHeight = _pixelHeight / window.devicePixelRatio;
+    final DomCSSStyleDeclaration style = canvasElement.style;
+    style.width = '${logicalWidth}px';
+    style.height = '${logicalHeight}px';
+    _currentDevicePixelRatio = window.devicePixelRatio;
+  }
+
+  /// Render the given [bitmap] with this [RenderCanvas].
+  ///
+  /// The canvas will be resized to accomodate the bitmap immediately before
+  /// rendering it.
+  void render(DomImageBitmap bitmap) {
+    _ensureSize(ui.Size(bitmap.width.toDartDouble, bitmap.height.toDartDouble));
+    renderContext.transferFromImageBitmap(bitmap);
+  }
+
+  /// Ensures that this canvas can draw a frame of the given [size].
+  void _ensureSize(ui.Size size) {
+    // Check if the frame is the same size as before, and if so, we don't need
+    // to resize the canvas.
+    if (size.width.ceil() == _pixelWidth &&
+        size.height.ceil() == _pixelHeight) {
+      // The existing canvas doesn't need to be resized (unless the device pixel
+      // ratio changed).
+      if (window.devicePixelRatio != _currentDevicePixelRatio) {
+        _updateLogicalHtmlCanvasSize();
+      }
+      return;
+    }
+
+    // If the canvas is too large or too small, resize it to the exact size of
+    // the frame. We cannot allow the canvas to be larger than the screen
+    // because then when we call `transferFromImageBitmap()` the bitmap will
+    // be scaled to cover the entire canvas.
+    _pixelWidth = size.width.ceil();
+    _pixelHeight = size.height.ceil();
+    canvasElement.width = _pixelWidth.toDouble();
+    canvasElement.height = _pixelHeight.toDouble();
+    _updateLogicalHtmlCanvasSize();
+  }
+
+  void dispose() {
+    htmlElement.remove();
+  }
+}
--- lib/web_ui/test/engine/scene_view_test.dart
+++ lib/web_ui/test/engine/scene_view_test.dart
@@ -19,9 +19,8 @@ void main() {
 }
 
 class StubPictureRenderer implements PictureRenderer {
-  final DomCanvasElement scratchCanvasElement = createDomCanvasElement(
-      width: 500, height: 500
-  );
+  final DomCanvasElement scratchCanvasElement =
+      createDomCanvasElement(width: 500, height: 500);
 
   @override
   Future<DomImageBitmap> renderPicture(ScenePicture picture) async {
@@ -63,9 +62,11 @@ void testMain() {
     final List<DomElement> children = sceneElement.children.toList();
     expect(children.length, 1);
     final DomElement containerElement = children.first;
-    expect(containerElement.tagName, equalsIgnoringCase('flt-canvas-container'));
+    expect(
+        containerElement.tagName, equalsIgnoringCase('flt-canvas-container'));
 
-    final List<DomElement> containerChildren = containerElement.children.toList();
+    final List<DomElement> containerChildren =
+        containerElement.children.toList();
     expect(containerChildren.length, 1);
     final DomElement canvasElement = containerChildren.first;
     final DomCSSStyleDeclaration style = canvasElement.style;
@@ -81,12 +82,11 @@ void testMain() {
     debugOverrideDevicePixelRatio(2.0);
 
     final PlatformView platformView = PlatformView(
-      1,
-      const ui.Size(100, 120),
-      const PlatformViewStyling(
-        position: PlatformViewPosition.offset(ui.Offset(50, 80)),
-      )
-    );
+        1,
+        const ui.Size(100, 120),
+        const PlatformViewStyling(
+          position: PlatformViewPosition.offset(ui.Offset(50, 80)),
+        ));
     final EngineRootLayer rootLayer = EngineRootLayer();
     rootLayer.slices.add(PlatformViewSlice(<PlatformView>[platformView], null));
     final EngineScene scene = EngineScene(rootLayer);
@@ -96,7 +96,8 @@ void testMain() {
     final List<DomElement> children = sceneElement.children.toList();
     expect(children.length, 1);
     final DomElement containerElement = children.first;
-    expect(containerElement.tagName, equalsIgnoringCase('flt-platform-view-slot'));
+    expect(
+        containerElement.tagName, equalsIgnoringCase('flt-platform-view-slot'));
 
     final DomCSSStyleDeclaration style = containerElement.style;
     expect(style.left, '25px');
