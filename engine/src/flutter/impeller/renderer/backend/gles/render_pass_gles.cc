48d3914a0b053ac9ac5780e45142e7f661f53633
Jason Simmons <jason-simmons@users.noreply.github.com>
2023-10-27 07:37:29 -0700

    [Impeller] Fix leak of framebuffers used in GLES MSAA rendering (flutter/engine#47362)

--- engine/src/flutter/impeller/renderer/backend/gles/render_pass_gles.cc
+++ engine/src/flutter/impeller/renderer/backend/gles/render_pass_gles.cc
@@ -488,6 +488,13 @@ struct RenderPassData {
     FML_DCHECK(gl.BlitFramebuffer.IsAvailable());
 
     GLuint draw_fbo = GL_NONE;
+    fml::ScopedCleanupClosure delete_draw_fbo([&gl, &draw_fbo, fbo]() {
+      if (draw_fbo != GL_NONE) {
+        gl.BindFramebuffer(GL_FRAMEBUFFER, fbo);
+        gl.DeleteFramebuffers(1u, &draw_fbo);
+      }
+    });
+
     gl.GenFramebuffers(1u, &draw_fbo);
     gl.BindFramebuffer(GL_FRAMEBUFFER, draw_fbo);
 
