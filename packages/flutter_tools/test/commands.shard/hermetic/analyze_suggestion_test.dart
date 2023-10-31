a5a478b4dfa0c7891a767386e45b27be9ca18640
Daco Harkes <dacoharkes@google.com>
2023-10-31 14:27:26 +0100

    Fix formatting (#137613)
    Badly formatted code causes distraction when reading, and costs people energy when understanding code.

--- packages/flutter_tools/test/commands.shard/hermetic/analyze_suggestion_test.dart
+++ packages/flutter_tools/test/commands.shard/hermetic/analyze_suggestion_test.dart
@@ -20,7 +20,7 @@ import '../../src/test_flutter_command_runner.dart';
 
 class ProjectValidatorDummy extends ProjectValidator {
   @override
-  Future<List<ProjectValidatorResult>> start(FlutterProject project, {Logger? logger, FileSystem? fileSystem}) async{
+  Future<List<ProjectValidatorResult>> start(FlutterProject project, {Logger? logger, FileSystem? fileSystem}) async {
     return <ProjectValidatorResult>[
       const ProjectValidatorResult(name: 'pass', value: 'value', status: StatusProjectValidator.success),
       const ProjectValidatorResult(name: 'fail', value: 'my error', status: StatusProjectValidator.error),
@@ -39,7 +39,7 @@ class ProjectValidatorDummy extends ProjectValidator {
 
 class ProjectValidatorSecondDummy extends ProjectValidator {
   @override
-  Future<List<ProjectValidatorResult>> start(FlutterProject project, {Logger? logger, FileSystem? fileSystem}) async{
+  Future<List<ProjectValidatorResult>> start(FlutterProject project, {Logger? logger, FileSystem? fileSystem}) async {
     return <ProjectValidatorResult>[
       const ProjectValidatorResult(name: 'second', value: 'pass', status: StatusProjectValidator.success),
       const ProjectValidatorResult(name: 'other fail', value: 'second fail', status: StatusProjectValidator.error),
@@ -57,7 +57,7 @@ class ProjectValidatorSecondDummy extends ProjectValidator {
 
 class ProjectValidatorCrash extends ProjectValidator {
   @override
-  Future<List<ProjectValidatorResult>> start(FlutterProject project, {Logger? logger, FileSystem? fileSystem}) async{
+  Future<List<ProjectValidatorResult>> start(FlutterProject project, {Logger? logger, FileSystem? fileSystem}) async {
     throw Exception('my exception');
   }
 
