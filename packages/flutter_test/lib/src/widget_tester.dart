40bd5933823d5e20793313e63681db3b9df79498
Polina Cherkasova <polina.c@live.com>
2023-10-31 19:41:13 -0700

    Update widget_tester.dart

--- packages/flutter_test/lib/src/widget_tester.dart
+++ packages/flutter_test/lib/src/widget_tester.dart
@@ -9,7 +9,7 @@ import 'package:flutter/material.dart' show Tooltip;
 import 'package:flutter/rendering.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:flutter/services.dart';
-import 'package:leak_tracker_flutter_testing/leak_tracker_flutter_testing.dart';
+import 'package:leak_tracker/leak_tracker.dart';
 import 'package:matcher/expect.dart' as matcher_expect;
 import 'package:meta/meta.dart';
 import 'package:test_api/scaffolding.dart' as test_package;
