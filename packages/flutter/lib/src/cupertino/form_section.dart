90bee5ee76e051644628648437d2d0b68cd531c2
Jasper van Riet <JaspervanRiet@users.noreply.github.com>
2023-11-02 17:40:59 +0100

    Remove duplicate padding on CupertinoFormSection (#137039)
    Fixes #128419, removes the duplicate padding that exists around the header and footer on `CupertinoFormSection`. This PR takes the implementation in #129065 and adds tests. I added the ones suggested in the PR review, as well as one more to test that `margin` is passed correctly from `CupertinoFormSection` to `CupertinoListSection`.
    
    Note: I can't quite figure out if this is also a fix to #121543.
    
    Potential review questions:
    
    - Is the use of `offsetMoreOrLessEquals` here correct? I used it because of the vertical positioning. The horizontal positioning is always exact.
    
    ### Screenshots
    <details>
      <summary>Before</summary>
      
    ![image](https://github.com/flutter/flutter/assets/5138348/1bce10c9-706a-40c8-a581-2dcb574ed937)
    </details>
    <details>
      <summary>After</summary>
      
    ![image](https://github.com/flutter/flutter/assets/5138348/cd1d529b-7be5-45df-af31-0f7760dc3fe9)
    </details>

--- packages/flutter/lib/src/cupertino/form_section.dart
+++ packages/flutter/lib/src/cupertino/form_section.dart
@@ -11,12 +11,6 @@ import 'list_section.dart';
 // iOS 14.2 SDK.
 const EdgeInsetsDirectional _kFormDefaultInsetGroupedRowsMargin = EdgeInsetsDirectional.fromSTEB(20.0, 0.0, 20.0, 10.0);
 
-// Standard header margin, determined from SwiftUI's Forms in iOS 14.2 SDK.
-const EdgeInsetsDirectional _kFormDefaultHeaderMargin = EdgeInsetsDirectional.fromSTEB(20.0, 16.0, 20.0, 10.0);
-
-// Standard footer margin, determined from SwiftUI's Forms in iOS 14.2 SDK.
-const EdgeInsetsDirectional _kFormDefaultFooterMargin = EdgeInsetsDirectional.fromSTEB(20.0, 0.0, 20.0, 10.0);
-
 /// An iOS-style form section.
 ///
 /// The base constructor for [CupertinoFormSection] constructs an
@@ -205,10 +199,7 @@ class CupertinoFormSection extends StatelessWidget {
               fontSize: 13.0,
               color: CupertinoColors.secondaryLabel.resolveFrom(context),
             ),
-            child: Padding(
-              padding: _kFormDefaultHeaderMargin,
-              child: header,
-            ));
+            child: header!);
 
     final Widget? footerWidget = footer == null
         ? null
@@ -217,10 +208,7 @@ class CupertinoFormSection extends StatelessWidget {
               fontSize: 13.0,
               color: CupertinoColors.secondaryLabel.resolveFrom(context),
             ),
-            child: Padding(
-              padding: _kFormDefaultFooterMargin,
-              child: footer,
-            ));
+            child: footer!);
 
     return _type == CupertinoListSectionType.base
         ? CupertinoListSection(
