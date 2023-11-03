e60bd1af4bcb825465d625d65eba48cdcda46973
Binni Goel <41873024+droidbg@users.noreply.github.com>
2023-11-03 18:54:38 +0530

    [leak-tracking] Add more leak tracking in test/painting #3 (#136170)

--- packages/flutter/test/painting/paint_image_test.dart
+++ packages/flutter/test/painting/paint_image_test.dart
@@ -7,6 +7,7 @@ import 'dart:ui' as ui;
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:leak_tracker_flutter_testing/leak_tracker_flutter_testing.dart';
 
 class TestCanvas implements Canvas {
   final List<Invocation> invocations = <Invocation>[];
@@ -160,7 +161,7 @@ void main() {
     expect(command.positionalArguments[2], equals(const Rect.fromLTRB(20.0, 40.0, 860.0, 840.0)));
   });
 
-  testWidgets('Reports Image painting', (WidgetTester tester) async {
+  testWidgetsWithLeakTracking('Reports Image painting', (WidgetTester tester) async {
     late ImageSizeInfo imageSizeInfo;
     int count = 0;
     debugOnPaintImage = (ImageSizeInfo info) {
@@ -199,7 +200,7 @@ void main() {
     debugOnPaintImage = null;
   });
 
-  testWidgets('Reports Image painting - change per frame', (WidgetTester tester) async {
+  testWidgetsWithLeakTracking('Reports Image painting - change per frame', (WidgetTester tester) async {
     late ImageSizeInfo imageSizeInfo;
     int count = 0;
     debugOnPaintImage = (ImageSizeInfo info) {
@@ -242,7 +243,7 @@ void main() {
     debugOnPaintImage = null;
   });
 
-  testWidgets('Reports Image painting - no debug label', (WidgetTester tester) async {
+  testWidgetsWithLeakTracking('Reports Image painting - no debug label', (WidgetTester tester) async {
     late ImageSizeInfo imageSizeInfo;
     int count = 0;
     debugOnPaintImage = (ImageSizeInfo info) {
