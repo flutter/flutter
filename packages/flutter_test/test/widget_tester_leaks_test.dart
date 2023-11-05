479fa43c8c9fc759036e4439a6df5e4859efca7d
Polina Cherkasova <polina.c@live.com>
2023-11-05 13:47:34 -0800

    Update widget_tester_leaks_test.dart

--- packages/flutter_test/test/widget_tester_leaks_test.dart
+++ packages/flutter_test/test/widget_tester_leaks_test.dart
@@ -2,23 +2,12 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'dart:async';
-import 'dart:io';
 
-import 'package:collection/collection.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/foundation.dart';
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
-import 'package:flutter/scheduler.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:leak_tracker/leak_tracker.dart';
-import 'package:leak_tracker_flutter_testing/leak_tracker_flutter_testing.dart';
 import 'package:leak_tracker_testing/leak_tracker_testing.dart';
-import 'package:matcher/expect.dart' as matcher;
-import 'package:matcher/src/expect/async_matcher.dart';
 
 import 'utils/leaking_classes.dart'; // ignore: implementation_imports
 
@@ -79,7 +68,7 @@ void main() {
   experimentalLeakTesting: LeakTesting.settings.withDisposalStackTrace(),
     (widgetTester) async {
       expect(LeakTracking.isStarted, true);
-      expect(LeakTracking.phase.name, _test4TrackingOnWithCreationStackTrace);
+      expect(LeakTracking.phase.name, _test5TrackingOnWithDisposalStackTrace);
       expect(LeakTracking.phase.ignoreLeaks, false);
       await widgetTester.pumpWidget(StatelessLeakingWidget());
     },
