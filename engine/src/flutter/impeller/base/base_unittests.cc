204b5f8fc4fec069beedea15934f72df9d687280
Chinmay Garde <chinmaygarde@google.com>
2023-11-07 13:27:43 -0800

    [Impeller] Support static thread safety analysis with condition variables. (flutter/engine#47763)
    Fixes https://github.com/flutter/flutter/issues/134843

--- engine/src/flutter/impeller/base/base_unittests.cc
+++ engine/src/flutter/impeller/base/base_unittests.cc
@@ -76,5 +76,72 @@ TEST(StringsTest, CanSPrintF) {
   ASSERT_EQ(SPrintF("%sx%.2f", "Hello", 12.122222), "Hellox12.12");
 }
 
+struct CVTest {
+  Mutex mutex;
+  ConditionVariable cv;
+  uint32_t rando_ivar IPLR_GUARDED_BY(mutex) = 0;
+};
+
+TEST(ConditionVariableTest, WaitUntil) {
+  CVTest test;
+  // test.rando_ivar = 12; // <--- Static analysis error
+  for (size_t i = 0; i < 2; ++i) {
+    test.mutex.Lock();  // <--- Static analysis error without this.
+    auto result = test.cv.WaitUntil(
+        test.mutex,
+        std::chrono::high_resolution_clock::now() +
+            std::chrono::milliseconds{10},
+        [&]() IPLR_REQUIRES(test.mutex) {
+          test.rando_ivar = 12;  // <-- Static analysics error without the
+                                 // IPLR_REQUIRES on the pred.
+          return false;
+        });
+    test.mutex.Unlock();
+    ASSERT_FALSE(result);
+  }
+  Lock lock(test.mutex);  // <--- Static analysis error without this.
+  // The predicate never returns true. So return has to be due to a non-spurious
+  // wake.
+  ASSERT_EQ(test.rando_ivar, 12u);
+}
+
+TEST(ConditionVariableTest, WaitFor) {
+  CVTest test;
+  // test.rando_ivar = 12; // <--- Static analysis error
+  for (size_t i = 0; i < 2; ++i) {
+    test.mutex.Lock();  // <--- Static analysis error without this.
+    auto result = test.cv.WaitFor(
+        test.mutex, std::chrono::milliseconds{10},
+        [&]() IPLR_REQUIRES(test.mutex) {
+          test.rando_ivar = 12;  // <-- Static analysics error without the
+                                 // IPLR_REQUIRES on the pred.
+          return false;
+        });
+    test.mutex.Unlock();
+    ASSERT_FALSE(result);
+  }
+  Lock lock(test.mutex);  // <--- Static analysis error without this.
+  // The predicate never returns true. So return has to be due to a non-spurious
+  // wake.
+  ASSERT_EQ(test.rando_ivar, 12u);
+}
+
+TEST(ConditionVariableTest, WaitForever) {
+  CVTest test;
+  // test.rando_ivar = 12; // <--- Static analysis error
+  for (size_t i = 0; i < 2; ++i) {
+    test.mutex.Lock();  // <--- Static analysis error without this.
+    test.cv.Wait(test.mutex, [&]() IPLR_REQUIRES(test.mutex) {
+      test.rando_ivar = 12;  // <-- Static analysics error without
+                             // the IPLR_REQUIRES on the pred.
+      return true;
+    });
+    test.mutex.Unlock();
+  }
+  Lock lock(test.mutex);  // <--- Static analysis error without this.
+  // The wake only happens when the predicate returns true.
+  ASSERT_EQ(test.rando_ivar, 12u);
+}
+
 }  // namespace testing
 }  // namespace impeller
