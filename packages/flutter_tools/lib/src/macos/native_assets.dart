228b6560ef8c6c38c5aeaea9006f48685ccb1786
Daco Harkes <dacoharkes@google.com>
2023-11-07 13:12:43 +0100

    Fix analysis

--- packages/flutter_tools/lib/src/macos/native_assets.dart
+++ packages/flutter_tools/lib/src/macos/native_assets.dart
@@ -7,7 +7,6 @@ import 'package:native_assets_builder/native_assets_builder.dart'
 import 'package:native_assets_cli/native_assets_cli.dart' hide BuildMode;
 import 'package:native_assets_cli/native_assets_cli.dart' as native_assets_cli;
 
-import '../base/common.dart';
 import '../base/file_system.dart';
 import '../build_info.dart';
 import '../globals.dart' as globals;
