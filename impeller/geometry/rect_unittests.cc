b5a151fc3ec9de888f1b3899a35e3ef0f35c118e
Jim Graham <flar@google.com>
2023-11-07 20:37:19 -0800

    [Impeller] Add Rect::GetNormalizingTransform to handle UV coordinate conversion (#47775)
    Three places in the code were manually computing the UV coordinates relative to a texture coverage rectangle while also transforming the points. This change will make it easier both to compute the UV conversion matrix and also to consolidate it with the transform that was already being applied to streamline the total computations.

--- impeller/geometry/rect_unittests.cc
+++ impeller/geometry/rect_unittests.cc
@@ -14,14 +14,14 @@ namespace testing {
 TEST(RectTest, RectOriginSizeGetters) {
   {
     Rect r = Rect::MakeOriginSize({10, 20}, {50, 40});
-    ASSERT_EQ(r.GetOrigin(), Point(10, 20));
-    ASSERT_EQ(r.GetSize(), Size(50, 40));
+    EXPECT_EQ(r.GetOrigin(), Point(10, 20));
+    EXPECT_EQ(r.GetSize(), Size(50, 40));
   }
 
   {
     Rect r = Rect::MakeLTRB(10, 20, 50, 40);
-    ASSERT_EQ(r.GetOrigin(), Point(10, 20));
-    ASSERT_EQ(r.GetSize(), Size(40, 20));
+    EXPECT_EQ(r.GetOrigin(), Point(10, 20));
+    EXPECT_EQ(r.GetSize(), Size(40, 20));
   }
 }
 
@@ -44,14 +44,152 @@ TEST(RectTest, RectMakeSize) {
     Size s(100, 200);
     IRect r = IRect::MakeSize(s);
     IRect expected = IRect::MakeLTRB(0, 0, 100, 200);
-    ASSERT_EQ(r, expected);
+    EXPECT_EQ(r, expected);
   }
 
   {
     ISize s(100, 200);
     IRect r = IRect::MakeSize(s);
     IRect expected = IRect::MakeLTRB(0, 0, 100, 200);
-    ASSERT_EQ(r, expected);
+    EXPECT_EQ(r, expected);
+  }
+}
+
+TEST(RectTest, RectGetNormalizingTransform) {
+  {
+    // Checks for expected matrix values
+
+    auto r = Rect::MakeXYWH(100, 200, 200, 400);
+
+    EXPECT_EQ(r.GetNormalizingTransform(),
+              Matrix::MakeScale({0.005, 0.0025, 1.0}) *
+                  Matrix::MakeTranslation({-100, -200}));
+  }
+
+  {
+    // Checks for expected transformation of points relative to the rect
+
+    auto r = Rect::MakeLTRB(300, 500, 400, 700);
+    auto m = r.GetNormalizingTransform();
+
+    // The 4 corners of the rect => (0, 0) to (1, 1)
+    EXPECT_EQ(m * Point(300, 500), Point(0, 0));
+    EXPECT_EQ(m * Point(400, 500), Point(1, 0));
+    EXPECT_EQ(m * Point(400, 700), Point(1, 1));
+    EXPECT_EQ(m * Point(300, 700), Point(0, 1));
+
+    // The center => (0.5, 0.5)
+    EXPECT_EQ(m * Point(350, 600), Point(0.5, 0.5));
+
+    // Outside the 4 corners => (-1, -1) to (2, 2)
+    EXPECT_EQ(m * Point(200, 300), Point(-1, -1));
+    EXPECT_EQ(m * Point(500, 300), Point(2, -1));
+    EXPECT_EQ(m * Point(500, 900), Point(2, 2));
+    EXPECT_EQ(m * Point(200, 900), Point(-1, 2));
+  }
+
+  {
+    // Checks for behavior with empty rects
+
+    auto zero = Matrix::MakeScale({0.0, 0.0, 1.0});
+
+    // Empty for width and/or height == 0
+    EXPECT_EQ(Rect::MakeXYWH(10, 10, 0, 10).GetNormalizingTransform(), zero);
+    EXPECT_EQ(Rect::MakeXYWH(10, 10, 10, 0).GetNormalizingTransform(), zero);
+    EXPECT_EQ(Rect::MakeXYWH(10, 10, 0, 0).GetNormalizingTransform(), zero);
+
+    // Empty for width and/or height < 0
+    EXPECT_EQ(Rect::MakeXYWH(10, 10, -1, 10).GetNormalizingTransform(), zero);
+    EXPECT_EQ(Rect::MakeXYWH(10, 10, 10, -1).GetNormalizingTransform(), zero);
+    EXPECT_EQ(Rect::MakeXYWH(10, 10, -1, -1).GetNormalizingTransform(), zero);
+  }
+
+  {
+    // Checks for behavior with non-finite rects
+
+    auto z = Matrix::MakeScale({0.0, 0.0, 1.0});
+    auto nan = std::numeric_limits<Scalar>::quiet_NaN();
+    auto inf = std::numeric_limits<Scalar>::infinity();
+
+    // Non-finite for width and/or height == nan
+    EXPECT_EQ(Rect::MakeXYWH(10, 10, nan, 10).GetNormalizingTransform(), z);
+    EXPECT_EQ(Rect::MakeXYWH(10, 10, 10, nan).GetNormalizingTransform(), z);
+    EXPECT_EQ(Rect::MakeXYWH(10, 10, nan, nan).GetNormalizingTransform(), z);
+
+    // Non-finite for width and/or height == inf
+    EXPECT_EQ(Rect::MakeXYWH(10, 10, inf, 10).GetNormalizingTransform(), z);
+    EXPECT_EQ(Rect::MakeXYWH(10, 10, 10, inf).GetNormalizingTransform(), z);
+    EXPECT_EQ(Rect::MakeXYWH(10, 10, inf, inf).GetNormalizingTransform(), z);
+
+    // Non-finite for width and/or height == -inf
+    EXPECT_EQ(Rect::MakeXYWH(10, 10, -inf, 10).GetNormalizingTransform(), z);
+    EXPECT_EQ(Rect::MakeXYWH(10, 10, 10, -inf).GetNormalizingTransform(), z);
+    EXPECT_EQ(Rect::MakeXYWH(10, 10, -inf, -inf).GetNormalizingTransform(), z);
+
+    // Non-finite for origin X and/or Y == nan
+    EXPECT_EQ(Rect::MakeXYWH(nan, 10, 10, 10).GetNormalizingTransform(), z);
+    EXPECT_EQ(Rect::MakeXYWH(10, nan, 10, 10).GetNormalizingTransform(), z);
+    EXPECT_EQ(Rect::MakeXYWH(nan, nan, 10, 10).GetNormalizingTransform(), z);
+
+    // Non-finite for origin X and/or Y == inf
+    EXPECT_EQ(Rect::MakeXYWH(inf, 10, 10, 10).GetNormalizingTransform(), z);
+    EXPECT_EQ(Rect::MakeXYWH(10, inf, 10, 10).GetNormalizingTransform(), z);
+    EXPECT_EQ(Rect::MakeXYWH(inf, inf, 10, 10).GetNormalizingTransform(), z);
+
+    // Non-finite for origin X and/or Y == -inf
+    EXPECT_EQ(Rect::MakeXYWH(-inf, 10, 10, 10).GetNormalizingTransform(), z);
+    EXPECT_EQ(Rect::MakeXYWH(10, -inf, 10, 10).GetNormalizingTransform(), z);
+    EXPECT_EQ(Rect::MakeXYWH(-inf, -inf, 10, 10).GetNormalizingTransform(), z);
+  }
+}
+
+TEST(RectTest, IRectGetNormalizingTransform) {
+  {
+    // Checks for expected matrix values
+
+    auto r = IRect::MakeXYWH(100, 200, 200, 400);
+
+    EXPECT_EQ(r.GetNormalizingTransform(),
+              Matrix::MakeScale({0.005, 0.0025, 1.0}) *
+                  Matrix::MakeTranslation({-100, -200}));
+  }
+
+  {
+    // Checks for expected transformation of points relative to the rect
+
+    auto r = IRect::MakeLTRB(300, 500, 400, 700);
+    auto m = r.GetNormalizingTransform();
+
+    // The 4 corners of the rect => (0, 0) to (1, 1)
+    EXPECT_EQ(m * Point(300, 500), Point(0, 0));
+    EXPECT_EQ(m * Point(400, 500), Point(1, 0));
+    EXPECT_EQ(m * Point(400, 700), Point(1, 1));
+    EXPECT_EQ(m * Point(300, 700), Point(0, 1));
+
+    // The center => (0.5, 0.5)
+    EXPECT_EQ(m * Point(350, 600), Point(0.5, 0.5));
+
+    // Outside the 4 corners => (-1, -1) to (2, 2)
+    EXPECT_EQ(m * Point(200, 300), Point(-1, -1));
+    EXPECT_EQ(m * Point(500, 300), Point(2, -1));
+    EXPECT_EQ(m * Point(500, 900), Point(2, 2));
+    EXPECT_EQ(m * Point(200, 900), Point(-1, 2));
+  }
+
+  {
+    // Checks for behavior with empty rects
+
+    auto zero = Matrix::MakeScale({0.0, 0.0, 1.0});
+
+    // Empty for width and/or height == 0
+    EXPECT_EQ(IRect::MakeXYWH(10, 10, 0, 10).GetNormalizingTransform(), zero);
+    EXPECT_EQ(IRect::MakeXYWH(10, 10, 10, 0).GetNormalizingTransform(), zero);
+    EXPECT_EQ(IRect::MakeXYWH(10, 10, 0, 0).GetNormalizingTransform(), zero);
+
+    // Empty for width and/or height < 0
+    EXPECT_EQ(IRect::MakeXYWH(10, 10, -1, 10).GetNormalizingTransform(), zero);
+    EXPECT_EQ(IRect::MakeXYWH(10, 10, 10, -1).GetNormalizingTransform(), zero);
+    EXPECT_EQ(IRect::MakeXYWH(10, 10, -1, -1).GetNormalizingTransform(), zero);
   }
 }
 
