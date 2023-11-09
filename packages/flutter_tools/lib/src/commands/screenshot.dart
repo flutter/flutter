596c92313180350860b35866b9d257897244a5dc
Victor Eronmosele <victoreronmosele@gmail.com>
2023-11-10 00:49:21 +0100

    Update logic for flutter screenshot outside project directory

--- packages/flutter_tools/lib/src/commands/screenshot.dart
+++ packages/flutter_tools/lib/src/commands/screenshot.dart
@@ -78,7 +78,7 @@ class ScreenshotCommand extends FlutterCommand {
         if (vmServiceUrl != null) {
           throwToolExit('VM Service URI cannot be provided for screenshot type $screenshotType');
         }
-        device = await findTargetDevice();
+        device = await findTargetDevice(includeDevicesUnsupportedByProject: true);
         if (device == null) {
           throwToolExit('Must have a connected device for screenshot type $screenshotType');
         }
@@ -118,15 +118,6 @@ class ScreenshotCommand extends FlutterCommand {
 
     return success ? FlutterCommandResult.success()
                    : FlutterCommandResult.fail();
-  }
-
-  @override
-  Future<Device?> findTargetDevice({
-    bool includeDevicesUnsupportedByProject = false,
-  }) async {
-    return super.findTargetDevice(
-      includeDevicesUnsupportedByProject: true,
-    );
   }
 
   Future<void> runScreenshot(File? outputFile) async {
