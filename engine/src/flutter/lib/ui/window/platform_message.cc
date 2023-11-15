2c058e08ff0ebde1022523941ecbc73984b6debd
Matan Lurey <matanlurey@users.noreply.github.com>
2023-11-15 11:21:55 -0800

    Make `lib/ui/{text|window}/...` compatible with `.clang_tidy`. (flutter/engine#48000)
    I just ... deleted `text_box.h` as it appears unused and unreferenced?
    
    ---
    
    Work towards https://github.com/flutter/flutter/issues/134969.
    
    All changes were made automatically (i.e. with `--fix`).

--- engine/src/flutter/lib/ui/window/platform_message.cc
+++ engine/src/flutter/lib/ui/window/platform_message.cc
@@ -13,13 +13,13 @@ PlatformMessage::PlatformMessage(std::string channel,
                                  fml::RefPtr<PlatformMessageResponse> response)
     : channel_(std::move(channel)),
       data_(std::move(data)),
-      hasData_(true),
+      has_data_(true),
       response_(std::move(response)) {}
 PlatformMessage::PlatformMessage(std::string channel,
                                  fml::RefPtr<PlatformMessageResponse> response)
     : channel_(std::move(channel)),
       data_(),
-      hasData_(false),
+      has_data_(false),
       response_(std::move(response)) {}
 
 PlatformMessage::~PlatformMessage() = default;
