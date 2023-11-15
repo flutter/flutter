9022127be4b4f5d878cb9128901cdb92501da968
Polina Cherkasova <polina.c@live.com>
2023-11-15 14:48:22 -0800

    -

--- packages/flutter_test/lib/src/widget_tester.dart
+++ packages/flutter_test/lib/src/widget_tester.dart
@@ -205,55 +205,7 @@ void testWidgets(
   }
 }
 
-WidgetTesterCallback _wrapWithLeakTracking(
-  String description,
-  WidgetTesterCallback callback,
-  LeakTesting? leakTesting,
-) {
-  // This cannot be done just once, because, if a test file starts with a group,
-  // the tear down will happen after group, not after all tests.
-  // It cannot be done in `tearDown`, because user defined tear down, tha may do disposal
-  // will happen after `tearDown` defined here.
-  // This is done unconditionally, even if settings.ignore is true,
-  // because settings.ignore may be different for different tests
-  // and it is not known at this point if there are tests with leak tracking enabled.
-  tearDownAll(() async {
-    await _mayBeFinalizeLeakTracking();
-  });
 
-  Future<void> wrappedCallBack(WidgetTester tester) async {
-    final LeakTesting settings = leakTesting ?? LeakTesting.settings;
-
-    if (settings.ignore) {
-      await callback(tester);
-      return;
-    }
-
-    if (!_isPlatformSupported) {
-      _maybePrintPlatformWarning();
-      await callback(tester);
-      return;
-    }
-
-    final PhaseSettings phase = PhaseSettings(
-      name: description,
-      leakDiagnosticConfig: settings.leakDiagnosticConfig,
-      ignoredLeaks: settings.ignoredLeaks,
-      baselining: settings.baselining,
-      ignoreLeaks: settings.ignore,
-    );
-
-    if (!LeakTracking.isStarted) {
-      _setUpLeakTracking();
-    }
-
-    LeakTracking.phase = phase;
-    await callback(tester);
-    LeakTracking.phase = const PhaseSettings.ignored();
-  }
-
-  return wrappedCallBack;
-}
 
 bool _notSupportedWarningPrinted = false;
 bool get _isPlatformSupported => !kIsWeb;
