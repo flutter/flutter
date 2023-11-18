4a4e50ed4bf70c08ff90a934f62c133b910eb970
TabooSun <taboosun1996@gmail.com>
2023-11-18 12:27:18 +0800

    Add test

--- packages/flutter_tools/test/general.shard/generate_localizations_test.dart
+++ packages/flutter_tools/test/general.shard/generate_localizations_test.dart
@@ -96,6 +96,7 @@ void main() {
       bool areResourceAttributeRequired = false,
       bool suppressWarnings = false,
       bool relaxSyntax = false,
+      bool useNamedArgument = false,
       void Function(Directory)? setup,
     }
   ) {
@@ -128,6 +129,7 @@ void main() {
       areResourceAttributesRequired: areResourceAttributeRequired,
       suppressWarnings: suppressWarnings,
       useRelaxedSyntax: relaxSyntax,
+      useNamedArgument: useNamedArgument,
     )
       ..loadResources()
       ..writeOutputFiles(isFromYaml: isFromYaml);
@@ -2462,5 +2464,44 @@ NumberFormat.decimalPatternDigits(
 }''';
     setupLocalizations(<String, String>{ 'en': dollarSignWithSelect });
     expect(getGeneratedFileContent(locale: 'en'), contains(r'\$nice_bug\nHello Bug! Manifistation #1 $_temp0'));
+  });
+
+  testWithoutContext('can generate method with named argument', () {
+    const String arbFile = '''
+{
+  "helloName": "Hello {name}!",
+  "@helloName": {
+    "description": "A more personal greeting",
+    "placeholders": {
+      "name": {
+        "type": "String",
+        "description": "The name of the person to greet"
+      }
+    }
+  },
+  "helloNameAndAge": "Hello {name}! You are {age} years old.",
+  "@helloNameAndAge": {
+    "description": "A more personal greeting",
+    "placeholders": {
+      "name": {
+        "type": "String",
+        "description": "The name of the person to greet"
+      },
+      "age": {
+        "type": "int",
+        "description": "The age of the person to greet"
+      }
+    }
+  }
+}
+    ''';
+    setupLocalizations(<String, String>{ 'en': arbFile }, useNamedArgument: true);
+    final String localizationsFile = getGeneratedFileContent(locale: 'en');
+    expect(localizationsFile, containsIgnoringWhitespace(r'''
+String helloName({required String name}) {
+  '''));
+    expect(localizationsFile, containsIgnoringWhitespace(r'''
+String helloNameAndAge({required String name, required int age}) {
+  '''));
   });
 }
