c92e337f92355660672f68c2ca11477862bdcd02
hangyu <108393416+hangyujin@users.noreply.github.com>
2023-11-27 10:57:26 -0800

    Update navigator_test.dart

--- packages/flutter/test/widgets/navigator_test.dart
+++ packages/flutter/test/widgets/navigator_test.dart
@@ -4376,7 +4376,7 @@ void main() {
     tester.binding.defaultBinaryMessenger.setMockDecodedMessageHandler<dynamic>(SystemChannels.accessibility, null);
   },
     variant:  const TargetPlatformVariant(<TargetPlatform>{ TargetPlatform.android, TargetPlatform.iOS }),
-    skip: isBrowser,
+    skip: isBrowser, // [intended] only non-web Android and ios supports move a11y focus back to last item.
   );
 
   group('RouteSettings.toString', () {
