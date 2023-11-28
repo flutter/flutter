4168cf737fd739362ad082bb22555fdd86bfd25f
gaaclarke <30870216+gaaclarke@users.noreply.github.com>
2023-11-28 10:17:18 -0800

    [Impeller] implements incorporating the gaussian blur snapshot transform (#48426)
    fixes https://github.com/flutter/flutter/issues/139085
    
    ## Pre-launch Checklist
    
    - [x] I read the [Contributor Guide] and followed the process outlined
    there for submitting PRs.
    - [x] I read the [Tree Hygiene] wiki page, which explains my
    responsibilities.
    - [x] I read and followed the [Flutter Style Guide] and the [C++,
    Objective-C, Java style guides].
    - [x] I listed at least one issue that this PR fixes in the description
    above.
    - [x] I added new tests to check the change I am making or feature I am
    adding, or the PR is [test-exempt]. See [testing the engine] for
    instructions on writing and running engine tests.
    - [x] I updated/added relevant documentation (doc comments with `///`).
    - [x] I signed the [CLA].
    - [x] All existing and new tests are passing.
    
    If you need help, consider asking for advice on the #hackers-new channel
    on [Discord].
    
    <!-- Links -->
    [Contributor Guide]:
    https://github.com/flutter/flutter/wiki/Tree-hygiene#overview
    [Tree Hygiene]: https://github.com/flutter/flutter/wiki/Tree-hygiene
    [test-exempt]:
    https://github.com/flutter/flutter/wiki/Tree-hygiene#tests
    [Flutter Style Guide]:
    https://github.com/flutter/flutter/wiki/Style-guide-for-Flutter-repo
    [C++, Objective-C, Java style guides]:
    https://github.com/flutter/engine/blob/main/CONTRIBUTING.md#style
    [testing the engine]:
    https://github.com/flutter/flutter/wiki/Testing-the-engine
    [CLA]: https://cla.developers.google.com/
    [flutter/tests]: https://github.com/flutter/tests
    [breaking change policy]:
    https://github.com/flutter/flutter/wiki/Tree-hygiene#handling-breaking-changes
    [Discord]: https://github.com/flutter/flutter/wiki/Chat

--- impeller/entity/contents/filters/gaussian_blur_filter_contents.cc
+++ impeller/entity/contents/filters/gaussian_blur_filter_contents.cc
@@ -183,7 +183,9 @@ std::optional<Rect> GaussianBlurFilterContents::GetFilterCoverage(
 
   Scalar blur_radius = CalculateBlurRadius(sigma_);
   Vector3 blur_radii =
-      effect_transform.Basis() * Vector3{blur_radius, blur_radius, 0.0};
+      (inputs[0]->GetTransform(entity).Basis() * effect_transform.Basis() *
+       Vector3{blur_radius, blur_radius, 0.0})
+          .Abs();
   return input_coverage.value().Expand(Point(blur_radii.x, blur_radii.y));
 }
 
@@ -257,7 +259,7 @@ std::optional<Entity> GaussianBlurFilterContents::RenderFilter(
   return Entity::FromSnapshot(
       Snapshot{
           .texture = pass3_out_texture,
-          .transform = entity.GetTransform() *
+          .transform = input_snapshot->transform *
                        Matrix::MakeTranslation({-padding.x, -padding.y, 0}) *
                        Matrix::MakeScale(padded_size /
                                          Vector2(pass1_out_texture->GetSize())),
