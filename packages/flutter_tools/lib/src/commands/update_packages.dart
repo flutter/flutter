9dab9e3f707c46a2c22735841f234a3f64a4d446
Ian Hickson <ian@hixie.ch>
2023-11-29 12:12:59 -0800

    Roll dependencies (#139203)
    Removes video_player and scoped_model pins, rolls everything else to latest.

--- packages/flutter_tools/lib/src/commands/update_packages.dart
+++ packages/flutter_tools/lib/src/commands/update_packages.dart
@@ -31,17 +31,11 @@ const Map<String, String> kManuallyPinnedDependencies = <String, String>{
   // Add pinned packages here. Please leave a comment explaining why.
   'flutter_gallery_assets': '1.0.2', // Tests depend on the exact version.
   'flutter_template_images': '4.2.0', // Must always exactly match flutter_tools template.
-  'video_player': '2.2.11',
-  // Keep pinned to latest until 1.0.0.
-  'material_color_utilities': '0.8.0',
-  // https://github.com/flutter/flutter/issues/115660
-  'archive': '3.3.2',
-  // https://github.com/flutter/devtools/issues/3951
-  'leak_tracker': '9.0.16',
-    // https://github.com/flutter/devtools/issues/3951
-  'leak_tracker_testing': '1.0.5',
-  // https://github.com/flutter/devtools/issues/3951
-  'leak_tracker_flutter_testing': '1.0.10',
+  'material_color_utilities': '0.8.0', // Keep pinned to latest until 1.0.0.
+  'archive': '3.3.2', // https://github.com/flutter/flutter/issues/115660
+  'leak_tracker': '9.0.16', // https://github.com/flutter/devtools/issues/3951
+  'leak_tracker_testing': '1.0.5', // https://github.com/flutter/devtools/issues/3951
+  'leak_tracker_flutter_testing': '1.0.10', // https://github.com/flutter/devtools/issues/3951
 };
 
 class UpdatePackagesCommand extends FlutterCommand {
