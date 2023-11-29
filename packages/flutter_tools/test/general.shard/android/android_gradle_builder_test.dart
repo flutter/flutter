60d5c8abc5d362c2fc57d5623a58ca68cf836738
Elias Yishak <42216813+eliasyishak@users.noreply.github.com>
2023-11-29 12:42:52 -0500

    Migration for the `sendTiming` events for `package:unified_analytics` (#138896)
    Related to tracker issue:
    - https://github.com/flutter/flutter/issues/128251
    
    <img width="278" alt="image" src="https://github.com/flutter/flutter/assets/42216813/cee7b9be-48d6-48e5-8c39-de28d0a1f0de">
    
    The image above shows all of the instances where we have `sendTiming`. All of the call sites have been updated to use the new `Event.timing` event from `package:unified_analytics`.

--- packages/flutter_tools/test/general.shard/android/android_gradle_builder_test.dart
+++ packages/flutter_tools/test/general.shard/android/android_gradle_builder_test.dart
@@ -145,11 +145,21 @@ void main() {
 
       expect(
         fakeAnalytics.sentEvents,
-        unorderedEquals(<Event>[
+        containsAll(<Event>[
           Event.flutterBuildInfo(label: 'app-not-using-android-x', buildType: 'gradle'),
           Event.flutterBuildInfo(label: 'gradle-random-event-label-failure', buildType: 'gradle'),
         ]),
       );
+
+      expect(
+        analyticsTimingEventExists(
+          sentEvents: fakeAnalytics.sentEvents,
+          workflow: 'build',
+          variableName: 'gradle',
+        ),
+        true,
+      );
+
     }, overrides: <Type, Generator>{
       AndroidStudio: () => FakeAndroidStudio(),
     });
@@ -328,7 +338,7 @@ void main() {
       ));
       expect(testUsage.events, hasLength(4));
 
-      expect(fakeAnalytics.sentEvents, hasLength(4));
+      expect(fakeAnalytics.sentEvents, hasLength(7));
       expect(fakeAnalytics.sentEvents, contains(
         Event.flutterBuildInfo(
           label: 'gradle-random-event-label-failure',
@@ -431,7 +441,7 @@ void main() {
       ));
       expect(testUsage.events, hasLength(2));
 
-      expect(fakeAnalytics.sentEvents, hasLength(2));
+      expect(fakeAnalytics.sentEvents, hasLength(3));
       expect(fakeAnalytics.sentEvents, contains(
         Event.flutterBuildInfo(
           label: 'gradle-random-event-label-failure',
@@ -878,8 +888,18 @@ BuildVariant: paidProfile
         project: FlutterProject.fromDirectoryTest(fileSystem.currentDirectory),
       );
       expect(actual, <String>['freeDebug', 'paidDebug', 'freeRelease', 'paidRelease', 'freeProfile', 'paidProfile']);
+
+      expect(
+        analyticsTimingEventExists(
+          sentEvents: fakeAnalytics.sentEvents,
+          workflow: 'print',
+          variableName: 'android build variants',
+        ),
+        true,
+      );
     }, overrides: <Type, Generator>{
       AndroidStudio: () => FakeAndroidStudio(),
+      Analytics: () => fakeAnalytics,
     });
 
     testUsingContext('getBuildOptions returns empty list if gradle returns error', () async {
@@ -941,10 +961,20 @@ Gradle Crashed
         'freeDebug',
         project: FlutterProject.fromDirectoryTest(fileSystem.currentDirectory),
       );
+
+      expect(
+        analyticsTimingEventExists(
+          sentEvents: fakeAnalytics.sentEvents,
+          workflow: 'outputs',
+          variableName: 'app link settings',
+        ),
+        true,
+      );
     }, overrides: <Type, Generator>{
       AndroidStudio: () => FakeAndroidStudio(),
       FileSystem: () => fileSystem,
       ProcessManager: () => processManager,
+      Analytics: () => fakeAnalytics,
     });
 
     testUsingContext("doesn't indicate how to consume an AAR when printHowToConsumeAar is false", () async {
@@ -1010,8 +1040,18 @@ Gradle Crashed
         isFalse,
       );
       expect(processManager, hasNoRemainingExpectations);
+
+      expect(
+        analyticsTimingEventExists(
+          sentEvents: fakeAnalytics.sentEvents,
+          workflow: 'build',
+          variableName: 'gradle-aar',
+        ),
+        true,
+      );
     }, overrides: <Type, Generator>{
       AndroidStudio: () => FakeAndroidStudio(),
+      Analytics: () => fakeAnalytics,
     });
 
     testUsingContext('Verbose mode for AARs includes Gradle stacktrace and sets debug log level', () async {
