49c98ff97c4ffec8c71a8989326fc5e2f46e3d27
Camille Simon <43054281+camsim99@users.noreply.github.com>
2023-11-30 16:04:42 -0800

    [Android] Check for text to paste before trying to retrieve data from URI (flutter/engine#48166)
    Adds a check to retrieve explicit text from Clipboard items via [`getText`](https://developer.android.com/reference/android/content/ClipData.Item#getText()) before attempting to retrieve data if the Clipboard item (`ClipData.Item`) has an associated URI. Also adds more error handling for edge cases concerning URIs (the URI not having the content scheme or being null).
    
    Fixes https://github.com/flutter/flutter/issues/74320. Some content providers will not send URIs containing the `text/*` pattern or have the content scheme (`content://`), but ultimately we should expect Clipboard items to either fall **not** into one of those categories or have an explicit textual value available via `getText`. In case this is not true, though, checks for non-content URIs and null URIs are added, as well :)
    
    Tested on a Samsung tablet with pasting from MS Word, MS Excel, and Samsung Notes.
    
    [C++, Objective-C, Java style guides]: https://github.com/flutter/engine/blob/main/CONTRIBUTING.md#style

--- engine/src/flutter/shell/platform/android/io/flutter/plugin/platform/PlatformPlugin.java
+++ engine/src/flutter/shell/platform/android/io/flutter/plugin/platform/PlatformPlugin.java
@@ -13,6 +13,7 @@ import android.content.ClipboardManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.AssetFileDescriptor;
+import android.net.Uri;
 import android.os.Build;
 import android.view.HapticFeedbackConstants;
 import android.view.SoundEffectConstants;
@@ -520,21 +521,46 @@ public class PlatformPlugin {
 
     if (!clipboard.hasPrimaryClip()) return null;
 
-    CharSequence charSequence = null;
+    CharSequence itemText = null;
     try {
       ClipData clip = clipboard.getPrimaryClip();
       if (clip == null) return null;
       if (format == null || format == PlatformChannel.ClipboardContentFormat.PLAIN_TEXT) {
         ClipData.Item item = clip.getItemAt(0);
-        AssetFileDescriptor assetFileDescriptor = null;
-        if (item.getUri() != null)
-          assetFileDescriptor =
-              activity
-                  .getContentResolver()
-                  .openTypedAssetFileDescriptor(item.getUri(), "text/*", null);
-        charSequence = item.coerceToText(activity);
-        if (assetFileDescriptor != null) assetFileDescriptor.close();
-        return charSequence;
+        // First, try getting clipboard data as text; no further processing
+        // required if so.
+        itemText = item.getText();
+        if (itemText == null) {
+          // Clipboard data does not contain text, so check whether or not it
+          // contains a URI to extract text from.
+          Uri itemUri = item.getUri();
+
+          if (itemUri == null) {
+            Log.w(
+                TAG, "Clipboard item contained no textual content nor a URI to retrieve it from.");
+            return null;
+          }
+
+          // Will only try to extract text from URI if it has the content scheme.
+          String uriScheme = itemUri.getScheme();
+
+          if (!uriScheme.equals("content")) {
+            Log.w(
+                TAG,
+                "Clipboard item contains a Uri with scheme '" + uriScheme + "'that is unhandled.");
+            return null;
+          }
+
+          AssetFileDescriptor assetFileDescriptor =
+              activity.getContentResolver().openTypedAssetFileDescriptor(itemUri, "text/*", null);
+
+          // Safely return clipboard data coerced into text; will return either
+          // itemText or text retrieved from its URI.
+          itemText = item.coerceToText(activity);
+          if (assetFileDescriptor != null) assetFileDescriptor.close();
+        }
+
+        return itemText;
       }
     } catch (SecurityException e) {
       Log.w(
@@ -545,10 +571,11 @@ public class PlatformPlugin {
           e);
       return null;
     } catch (FileNotFoundException e) {
+      Log.w(TAG, "Clipboard text was unable to be received from content URI.");
       return null;
     } catch (IOException e) {
-      Log.w(TAG, "Failed to close AssetFileDescriptor while accessing clipboard data.", e);
-      return charSequence;
+      Log.w(TAG, "Failed to close AssetFileDescriptor while trying to read text from URI.", e);
+      return itemText;
     }
 
     return null;
