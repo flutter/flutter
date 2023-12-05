8faa7c170574604a55521bbf1ec942d9bb40840f
hangyu <108393416+hangyujin@users.noreply.github.com>
2023-12-05 11:30:47 -0800

    1

--- packages/flutter/lib/src/cupertino/route.dart
+++ packages/flutter/lib/src/cupertino/route.dart
@@ -626,6 +626,14 @@ class _CupertinoBackGestureDetectorState<T> extends State<_CupertinoBackGestureD
   @override
   void dispose() {
     _recognizer.dispose();
+
+    // If this is disposed during a drag. Call navigator.didStopUserGesture.
+    if(_backGestureController != null) {
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        _backGestureController!.navigator.didStopUserGesture();
+        _backGestureController = null;
+      });
+    }
     super.dispose();
   }
 
