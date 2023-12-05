2923201bc337b9c5e6ffcf7906add0ecafcaa53d
Chinmay Garde <chinmaygarde@google.com>
2023-12-04 20:07:23 -0800

    [Impeller] Remove duplicate entity test suite instantiations. (#48654)
    And make it illegal to do so in the future. Now, making such a mistake will lead to a linker error complaining about duplicate definitions of `kYouInstantiatedEntityTestMultipleTimes`.
    
    Fixes https://github.com/flutter/flutter/issues/139090

--- impeller/playground/playground_test.h
+++ impeller/playground/playground_test.h
@@ -55,14 +55,16 @@ class PlaygroundTest : public Playground,
   PlaygroundTest& operator=(const PlaygroundTest&) = delete;
 };
 
-#define INSTANTIATE_PLAYGROUND_SUITE(playground)                            \
-  INSTANTIATE_TEST_SUITE_P(                                                 \
-      Play, playground,                                                     \
-      ::testing::Values(PlaygroundBackend::kMetal,                          \
-                        PlaygroundBackend::kOpenGLES,                       \
-                        PlaygroundBackend::kVulkan),                        \
-      [](const ::testing::TestParamInfo<PlaygroundTest::ParamType>& info) { \
-        return PlaygroundBackendToString(info.param);                       \
+#define INSTANTIATE_PLAYGROUND_SUITE(playground)                             \
+  [[maybe_unused]] const char* kYouInstantiated##playground##MultipleTimes = \
+      "";                                                                    \
+  INSTANTIATE_TEST_SUITE_P(                                                  \
+      Play, playground,                                                      \
+      ::testing::Values(PlaygroundBackend::kMetal,                           \
+                        PlaygroundBackend::kOpenGLES,                        \
+                        PlaygroundBackend::kVulkan),                         \
+      [](const ::testing::TestParamInfo<PlaygroundTest::ParamType>& info) {  \
+        return PlaygroundBackendToString(info.param);                        \
       });
 
 }  // namespace impeller
