de9b4354c97dccf85576f992ff5390c8226fedd4
eliasyishak <42216813+eliasyishak@users.noreply.github.com>
2023-12-06 12:42:09 -0700

    Caching `FlutterProject` in `assemble.dart`

--- packages/flutter_tools/lib/src/commands/assemble.dart
+++ packages/flutter_tools/lib/src/commands/assemble.dart
@@ -127,6 +127,8 @@ class AssembleCommand extends FlutterCommand {
 
   final BuildSystem _buildSystem;
 
+  late final FlutterProject _flutterProject = FlutterProject.current();
+
   @override
   String get description => 'Assemble and build Flutter resources.';
 
@@ -138,11 +140,10 @@ class AssembleCommand extends FlutterCommand {
 
   @override
   Future<CustomDimensions> get usageValues async {
-    final FlutterProject flutterProject = FlutterProject.current();
     try {
       return CustomDimensions(
         commandBuildBundleTargetPlatform: _environment.defines[kTargetPlatform],
-        commandBuildBundleIsModule: flutterProject.isModule,
+        commandBuildBundleIsModule: _flutterProject.isModule,
       );
     } on Exception {
       // We've failed to send usage.
@@ -152,13 +153,12 @@ class AssembleCommand extends FlutterCommand {
 
   @override
   Future<Event> unifiedAnalyticsUsageValues(String commandPath) async {
-    final FlutterProject flutterProject = FlutterProject.current();
     try {
       return Event.commandUsageValues(
         workflow: commandPath,
         commandHasTerminal: hasTerminal,
         buildBundleTargetPlatform: _environment.defines[kTargetPlatform],
-        buildBundleIsModule: flutterProject.isModule,
+        buildBundleIsModule: _flutterProject.isModule,
       );
     } on Exception {
       // We've failed to send usage.
@@ -225,22 +225,21 @@ class AssembleCommand extends FlutterCommand {
 
   /// The environmental configuration for a build invocation.
   Environment _createEnvironment() {
-    final FlutterProject flutterProject = FlutterProject.current();
     String? output = stringArg('output');
     if (output == null) {
       throwToolExit('--output directory is required for assemble.');
     }
     // If path is relative, make it absolute from flutter project.
     if (globals.fs.path.isRelative(output)) {
-      output = globals.fs.path.join(flutterProject.directory.path, output);
+      output = globals.fs.path.join(_flutterProject.directory.path, output);
     }
     final Artifacts artifacts = globals.artifacts!;
     final Environment result = Environment(
       outputDir: globals.fs.directory(output),
-      buildDir: flutterProject.directory
+      buildDir: _flutterProject.directory
           .childDirectory('.dart_tool')
           .childDirectory('flutter_build'),
-      projectDir: flutterProject.directory,
+      projectDir: _flutterProject.directory,
       defines: _parseDefines(stringsArg('define')),
       inputs: _parseDefines(stringsArg('input')),
       cacheDir: globals.cache.getRoot(),
@@ -283,7 +282,7 @@ class AssembleCommand extends FlutterCommand {
     }
 
     results[kDeferredComponents] = 'false';
-    if (FlutterProject.current().manifest.deferredComponents != null && isDeferredComponentsTargets() && !isDebug()) {
+    if (_flutterProject.manifest.deferredComponents != null && isDeferredComponentsTargets() && !isDebug()) {
       results[kDeferredComponents] = 'true';
     }
     if (argumentResults.wasParsed(FlutterOptions.kExtraFrontEndOptions)) {
@@ -314,7 +313,7 @@ class AssembleCommand extends FlutterCommand {
         "Try re-running 'flutter build ios' or the appropriate build command."
       );
     }
-    if (FlutterProject.current().manifest.deferredComponents != null
+    if (_flutterProject.manifest.deferredComponents != null
         && decodedDefines.contains('validate-deferred-components=true')
         && deferredTargets.isNotEmpty
         && !isDebug()) {
