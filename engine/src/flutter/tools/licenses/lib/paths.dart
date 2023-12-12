09ba20cb6a42f537a16840f52a382bbdebbd536e
Matan Lurey <matanlurey@users.noreply.github.com>
2023-12-12 14:23:02 -0800

    Move `third_party/swiftshader`, roll buildroot `DEPS`. (flutter/engine#48946)
    Progress towards https://github.com/flutter/flutter/issues/67373, synced with https://github.com/flutter/buildroot/commit/0141e94b34cbd3db77f28796587334857fa985da.

--- engine/src/flutter/tools/licenses/lib/paths.dart
+++ engine/src/flutter/tools/licenses/lib/paths.dart
@@ -91,6 +91,7 @@ final Set<String> skippedPaths = <String>{
   r'flutter/third_party/skia/third_party/vello', // not linked in
   r'flutter/third_party/skia/tools', // contains nothing that ends up in the binary executable
   r'flutter/third_party/stb',
+  r'flutter/third_party/swiftshader', // only used on hosts for tests
   r'flutter/third_party/test_shaders', // for tests only
   r'flutter/third_party/tinygltf',
   r'flutter/third_party/txt/third_party/fonts',
@@ -185,7 +186,6 @@ final Set<String> skippedPaths = <String>{
   r'third_party/protobuf', // build-time dependency only
   r'third_party/root_certificates/certdata.pem',
   r'third_party/root_certificates/certdata.txt',
-  r'third_party/swiftshader', // only used on hosts for tests
   r'third_party/vulkan-deps/glslang/LICENSE', // excluded to make sure we don't accidentally apply it as a default license
   r'third_party/vulkan-deps/glslang/src/LICENSE.txt', // redundant with licenses inside files
   r'third_party/vulkan-deps/glslang/src/glslang/OSDependent/Web', // we only use glslang in impellerc, not in web apps
