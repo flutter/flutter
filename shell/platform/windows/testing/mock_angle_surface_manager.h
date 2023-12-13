d2021410fa2350e4d2c82577308e8ea39d0fb96e
Loïc Sharma <737941+loic-sharma@users.noreply.github.com>
2023-12-13 09:42:52 -0800

    [Windows] Refactor surface manager mocking (#48953)
    See: https://github.com/flutter/engine/pull/48849#discussion_r1424535869

---
+++ shell/platform/windows/testing/mock_angle_surface_manager.h
@@ -0,0 +1,41 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef FLUTTER_SHELL_PLATFORM_WINDOWS_TESTING_MOCK_ANGLE_SURFACE_MANAGER_H_
+#define FLUTTER_SHELL_PLATFORM_WINDOWS_TESTING_MOCK_ANGLE_SURFACE_MANAGER_H_
+
+#include "flutter/fml/macros.h"
+#include "flutter/shell/platform/windows/angle_surface_manager.h"
+#include "gmock/gmock.h"
+
+namespace flutter {
+namespace testing {
+
+/// Mock for the |AngleSurfaceManager| base class.
+class MockAngleSurfaceManager : public AngleSurfaceManager {
+ public:
+  MockAngleSurfaceManager() : AngleSurfaceManager(false) {}
+
+  MOCK_METHOD(bool,
+              CreateSurface,
+              (WindowsRenderTarget*, EGLint, EGLint),
+              (override));
+  MOCK_METHOD(void,
+              ResizeSurface,
+              (WindowsRenderTarget*, EGLint, EGLint, bool),
+              (override));
+  MOCK_METHOD(void, DestroySurface, (), (override));
+
+  MOCK_METHOD(bool, MakeCurrent, (), (override));
+  MOCK_METHOD(bool, ClearCurrent, (), (override));
+  MOCK_METHOD(void, SetVSyncEnabled, (bool), (override));
+
+ private:
+  FML_DISALLOW_COPY_AND_ASSIGN(MockAngleSurfaceManager);
+};
+
+}  // namespace testing
+}  // namespace flutter
+
+#endif  // FLUTTER_SHELL_PLATFORM_WINDOWS_TESTING_MOCK_ANGLE_SURFACE_MANAGER_H_
