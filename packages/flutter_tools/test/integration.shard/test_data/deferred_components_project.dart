f277034b56eb1dd925ab85a08e0c3909187ce37a
Gray Mackall <34871572+gmackall@users.noreply.github.com>
2023-12-12 18:18:42 -0800

    yet another agp version (and a kgp)

--- packages/flutter_tools/test/integration.shard/test_data/deferred_components_project.dart
+++ packages/flutter_tools/test/integration.shard/test_data/deferred_components_project.dart
@@ -107,14 +107,14 @@ class BasicDeferredComponentsConfig extends DeferredComponentsConfig {
   @override
   String get androidBuild => r'''
   buildscript {
-      ext.kotlin_version = '1.3.50'
+      ext.kotlin_version = '1.7.10'
       repositories {
           google()
           mavenCentral()
       }
 
       dependencies {
-          classpath 'com.android.tools.build:gradle:4.1.0'
+          classpath 'com.android.tools.build:gradle:7.3.0'
           classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
       }
   }
