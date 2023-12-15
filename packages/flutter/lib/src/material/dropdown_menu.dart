28d79836de4b52dcbe06b25c81c5ba8c87189d1f
Faisal Ansari <faisal.ansari@cupidknot.com>
2023-12-15 19:24:48 +0530

    Update dropdown_menu.dart

--- packages/flutter/lib/src/material/dropdown_menu.dart
+++ packages/flutter/lib/src/material/dropdown_menu.dart
@@ -4,7 +4,7 @@
 
 import 'dart:math' as math;
 
-import 'package:flutter/material.dart';
+
 import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
