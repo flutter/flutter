96902557ef45818dd4e1a20ee7bfca16706968b6
Taha Tesser <tessertaha@gmail.com>
2023-12-18 13:04:30 +0200

    Fix `shape` and `collapsedShape` isn't applied to `ExpansionTile`'s splash ink

--- packages/flutter/lib/src/material/expansion_tile.dart
+++ packages/flutter/lib/src/material/expansion_tile.dart
@@ -475,6 +475,9 @@ class ExpansionTile extends StatefulWidget {
 
   /// {@macro flutter.material.Material.clipBehavior}
   ///
+  /// If this is not null and a custom collapsed or expanded shape is provided,
+  /// specified clip behavior will be used to clip the expansion tile.
+  ///
   /// If this property is null, the [ExpansionTileThemeData.clipBehavior] is used. If that
   /// is also null, a [Clip.none] is used
   ///
@@ -656,10 +659,11 @@ class _ExpansionTileState extends State<ExpansionTile> with SingleTickerProvider
   Widget _buildChildren(BuildContext context, Widget? child) {
     final ThemeData theme = Theme.of(context);
     final ExpansionTileThemeData expansionTileTheme = ExpansionTileTheme.of(context);
+    final Color backgroundColor = _backgroundColor.value ?? expansionTileTheme.backgroundColor ?? Colors.transparent;
     final ShapeBorder expansionTileBorder = _border.value ?? const Border(
-            top: BorderSide(color: Colors.transparent),
-            bottom: BorderSide(color: Colors.transparent),
-          );
+      top: BorderSide(color: Colors.transparent),
+      bottom: BorderSide(color: Colors.transparent),
+    );
     final Clip clipBehavior = widget.clipBehavior ?? expansionTileTheme.clipBehavior ?? Clip.none;
     final MaterialLocalizations localizations = MaterialLocalizations.of(context);
     final String onTapHint = _isExpanded
@@ -679,12 +683,13 @@ class _ExpansionTileState extends State<ExpansionTile> with SingleTickerProvider
         break;
     }
 
-    return Container(
-      clipBehavior: clipBehavior,
-      decoration: ShapeDecoration(
-        color: _backgroundColor.value ?? expansionTileTheme.backgroundColor ?? Colors.transparent,
-        shape: expansionTileBorder,
-      ),
+    final Decoration decoration = ShapeDecoration(
+      color: backgroundColor,
+      shape: expansionTileBorder,
+    );
+
+    final Widget tile = Padding(
+      padding: decoration.padding,
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: <Widget>[
@@ -719,6 +724,23 @@ class _ExpansionTileState extends State<ExpansionTile> with SingleTickerProvider
           ),
         ],
       ),
+    );
+
+    final bool isShapeProvided = widget.shape != null || expansionTileTheme.shape != null
+      || widget.collapsedShape != null || expansionTileTheme.collapsedShape != null;
+
+    if (isShapeProvided) {
+      return Material(
+        clipBehavior: clipBehavior,
+        color: backgroundColor,
+        shape: expansionTileBorder,
+        child: tile,
+      );
+    }
+
+    return DecoratedBox(
+      decoration: decoration,
+      child: tile,
     );
   }
 
