50077b5722312e7e7589b52c9c8dc7bebee43456
Godofredo Contreras <godofredoc@google.com>
2023-12-21 16:26:17 -0800

    Run codesign test only on mac.

--- dev/bots/test/codesign_test.dart
+++ dev/bots/test/codesign_test.dart
@@ -2,6 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+@TestOn('mac-os')
+library;
+
 import '../../../packages/flutter_tools/test/src/fake_process_manager.dart';
 import '../test.dart';
 import './common.dart';
