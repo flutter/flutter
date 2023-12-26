5241f0efa768ab30693e4762ad6a984aecf9ad8f
Polina Cherkasova <polina.c@live.com>
2023-12-26 14:39:41 -0800

    Update widget_tester_leaks_test.dart

--- packages/flutter_test/test/widget_tester_leaks_test.dart
+++ packages/flutter_test/test/widget_tester_leaks_test.dart
@@ -7,7 +7,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:leak_tracker_flutter_testing/leak_tracker_flutter_testing.dart';
 
-final List<LeakTestCase> tests = <LeakTestCase>[
+final List<LeakTestCase> _tests = <LeakTestCase>[
   LeakTestCase(
     name: 'no leaks',
     body: (PumpWidgetsCallback? pumpWidgets,
@@ -63,16 +63,45 @@ final List<LeakTestCase> tests = <LeakTestCase>[
   ),
 ];
 
-final Map<String, LeakTesting> _usedSettings = <String, LeakTesting>{};
+class _TestExecution {
+  _TestExecution({required this.settingName, required this.test});
+
+  final String settingName;
+  final LeakTestCase test;
+
+  String get name => '${test.name}, $settingName';
+}
+
+final List<_TestExecution> _testExecutions = <_TestExecution>[];
 
 void main() {
-  for (final LeakTestCase test in tests) {
+  LeakTesting.collectedLeaksReporter = (Leaks leaks) => _verifyLeaks(leaks);
+  LeakTesting.enable();
+
+  LeakTesting.settings = LeakTesting.settings
+  .withTrackedAll()
+  .withTracked(allNotDisposed: true, allNotGCed: true)
+  .withIgnored(createdByTestHelpers: true);
+
+  for (final LeakTestCase test in _tests) {
     for (final  MapEntry<String, LeakTesting> settings in leakTestingSettingsCases.entries) {
-      final String testName = '${test.name}, ${settings.key}';
-      _usedSettings[settings.key] = settings.value;
-      testWidgets(testName, experimentalLeakTesting: settings.value, (WidgetTester tester) async {
+      if (settings.value.leakDiagnosticConfig.collectRetainingPathForNotGCed) {
+        // Retaining path requires vm to be started, so skipping.
+        continue;
+      }
+      final _TestExecution execution = _TestExecution(settingName: settings.key, test: test);
+      _testExecutions.add(execution);
+      testWidgets(execution.name, experimentalLeakTesting: settings.value, (WidgetTester tester) async {
         await test.body(tester.pumpWidget, tester.runAsync);
       });
     }
+  }
+}
+
+void _verifyLeaks(Leaks leaks) {
+  for (final _TestExecution execution in _testExecutions) {
+    final Leaks testLeaks = leaks.byPhase[execution.name] ?? Leaks.empty();
+    final LeakTesting settings = leakTestingSettingsCases[execution.settingName]!;
+    execution.test.verifyLeaks(testLeaks, settings, testDescription: execution.name);
   }
 }
