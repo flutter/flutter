08d81193cffbd833f78beca11554789cfc4e8311
NobodyForNothing <82763757+NobodyForNothing@users.noreply.github.com>
2023-12-29 17:43:18 +0100

    update dependencies and widget

--- examples/hello_world/lib/main.dart
+++ examples/hello_world/lib/main.dart
@@ -2,15 +2,40 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-void main() =>
+void main() {
+  final TextEditingController controller = TextEditingController();
   runApp(
-    const Center(
-      child:
-        Text('Hello, world!',
-          key: Key('title'),
-          textDirection: TextDirection.ltr,
+      MaterialApp(
+        home: Scaffold(
+          body: StatefulBuilder(
+            builder: (BuildContext context, _) =>
+                RepaintBoundary(
+                  child: ListView(
+                    children: <Widget>[
+                      TextFormField(
+                        minLines: 1,
+                        maxLines: 4,
+                        controller: controller,
+                        // onChanged: Material.of(context).markNeedsPaint,
+                      ),
+                      Ink(
+                        decoration: const ShapeDecoration(
+                          shape: RoundedRectangleBorder(
+                              side: BorderSide(
+                                  color: Colors.red
+                              )
+                          ),
+                        ),
+                        child: const SizedBox(height: 1,),
+                      ),
+                    ],
+                  ),
+                ),
+          ),
         ),
-      ),
+      )
     );
+}
