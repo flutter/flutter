10cd2d57f5e1124465bd7b8df8ffc922285f1eea
eliasyishak <42216813+eliasyishak@users.noreply.github.com>
2024-01-05 11:59:40 -0500

    Importing analytics packages in files

--- packages/flutter_tools/lib/src/commands/packages.dart
+++ packages/flutter_tools/lib/src/commands/packages.dart
@@ -3,6 +3,7 @@
 // found in the LICENSE file.
 
 import 'package:args/args.dart';
+import 'package:unified_analytics/unified_analytics.dart';
 
 import '../base/common.dart';
 import '../base/os.dart';
