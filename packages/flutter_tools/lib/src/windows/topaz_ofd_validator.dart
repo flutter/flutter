ec27c0c936485def1d231a8c1e41ec2a2aa17e27
schectman <schectman@google.com>
2024-01-09 14:21:36 -0500

    PR Feedback

--- packages/flutter_tools/lib/src/windows/topaz_ofd_validator.dart
+++ packages/flutter_tools/lib/src/windows/topaz_ofd_validator.dart
@@ -2,10 +2,12 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'package:process/process.dart';
+
 import '../base/io.dart';
 import '../doctor_validator.dart';
 
-const String kCoreProcessPattern = r'Topaz OFD\\Warsaw\\core.exe';
+const String kCoreProcessPattern = r'Topaz OFD\\Warsaw\\core\.exe';
 
 class TopazOfdValidator extends DoctorValidator {
   const TopazOfdValidator({required this.processLister}) : super('Topaz OFD');
@@ -14,12 +16,23 @@ class TopazOfdValidator extends DoctorValidator {
 
   @override
   Future<ValidationResult> validate() async {
-    final String tasks = await processLister.getProcessesWithPath('core');
+    final ProcessResult processResult = await processLister.getProcessesWithPath('core');
+    final int exitCode = processResult.exitCode;
+    if (exitCode != 0) {
+      return ValidationResult(
+        ValidationType.missing,
+        <ValidationMessage>[
+          ValidationMessage.hint('Check for Topaz OFD did not complete normally. Returned exit code $exitCode'),
+        ],
+        statusInfo: 'Get-Process failed to complete',
+      );
+    }
+    final String tasks = processResult.stdout as String;
     final RegExp pattern = RegExp(kCoreProcessPattern, multiLine: true, caseSensitive: false);
     final bool matches = pattern.hasMatch(tasks);
     if (matches) {
       return const ValidationResult(
-        ValidationType.partial,
+        ValidationType.missing,
         <ValidationMessage>[
           ValidationMessage.hint('The Topaz OFD Security Module process has been found running. If you are unable to build, you will need to disable it.'),
         ],
@@ -27,15 +40,18 @@ class TopazOfdValidator extends DoctorValidator {
     } else {
       return const ValidationResult(
         ValidationType.success,
-        <ValidationMessage>[]);
+        <ValidationMessage>[],
+      );
     }
   }
 }
 
 class ProcessLister {
-  Future<String> getProcessesWithPath(String? filter) async {
+  const ProcessLister();
+
+  Future<ProcessResult> getProcessesWithPath(String? filter) async {
     final String argument = filter != null ? 'Get-Process $filter | Format-List Path' : 'Get-Process | Format-List Path';
-    final ProcessResult taskResult = await Process.run('powershell', <String>['-command', argument]);
-    return taskResult.stdout as String;
+    const ProcessManager processManager = LocalProcessManager();
+    return processManager.run(<String>['powershell', '-command', argument]);
   }
 }
--- packages/flutter_tools/test/general.shard/windows/topaz_ofd_validator_test.dart
+++ packages/flutter_tools/test/general.shard/windows/topaz_ofd_validator_test.dart
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'dart:io';
+
 import 'package:flutter_tools/src/doctor_validator.dart';
 import 'package:flutter_tools/src/windows/topaz_ofd_validator.dart';
 import 'package:test/fake.dart';
@@ -16,6 +18,10 @@ FakeProcessLister ofdNotRunning() {
   return FakeProcessLister(result: r'Path: "C:\Program Files\Google\Chrome\Application\chrome.exe');
 }
 
+FakeProcessLister failure() {
+  return FakeProcessLister(result: r'Path: "C:\Program Files\Google\Chrome\Application\chrome.exe', exitCode: 10);
+}
+
 void main() {
   testWithoutContext('Successfully checks for Topaz OFD when it is not running', () async {
     final FakeProcessLister processLister = ofdNotRunning();
@@ -28,17 +34,25 @@ void main() {
     final FakeProcessLister processLister = ofdRunning();
     final TopazOfdValidator validator = TopazOfdValidator(processLister: processLister);
     final ValidationResult result = await validator.validate();
-    expect(result.type, ValidationType.partial);
+    expect(result.type, ValidationType.missing);
     expect(result.statusInfo, 'Topaz OFD may be running');
+  });
+  testWithoutContext('Reports failure of Get-Process', () async {
+    final FakeProcessLister processLister = failure();
+    final TopazOfdValidator validator = TopazOfdValidator(processLister: processLister);
+    final ValidationResult result = await validator.validate();
+    expect(result.type, ValidationType.missing);
+    expect(result.statusInfo, 'Get-Process failed to complete');
   });
 }
 
 class FakeProcessLister extends Fake implements ProcessLister {
-  FakeProcessLister({required this.result});
+  FakeProcessLister({required this.result, this.exitCode = 0});
   final String result;
+  final int exitCode;
 
   @override
-  Future<String> getProcessesWithPath(String? filter) async {
-    return result;
+  Future<ProcessResult> getProcessesWithPath(String? filter) async {
+    return ProcessResult(0, exitCode, result, null);
   }
 }
