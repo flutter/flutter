1179c4102fbaea18caab4c5b20d1ce87e836676e
Brandon DeRosier <bdero@google.com>
2024-01-08 16:05:15 -0800

    [Impeller] Document mip bias. (#49602)
    Add doc strings explaining what the mip bias does and where it came
    from.

--- impeller/compiler/shader_lib/impeller/texture.glsl
+++ impeller/compiler/shader_lib/impeller/texture.glsl
@@ -15,6 +15,9 @@
 /// If `y_coord_scale` < 0.0, the Y coordinate is flipped. This is useful
 /// for Impeller graphics backends that use a flipped framebuffer coordinate
 /// space.
+///
+/// A negative mip bias is applied to improve the sharpness of scaled down
+/// images when mip sampling is enabled. See `kDefaultMipBias` for more detail.
 vec4 IPSample(sampler2D texture_sampler, vec2 coords, float y_coord_scale) {
   return texture(texture_sampler, IPRemapCoords(coords, y_coord_scale),
                  kDefaultMipBias);
