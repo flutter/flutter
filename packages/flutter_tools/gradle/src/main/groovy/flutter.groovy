8bb03aa8d912e44baf1ef1cbe0325f75e0acf813
hangyu <108393416+hangyujin@users.noreply.github.com>
2024-01-09 18:17:49 -0800

    Update flutter.groovy

--- packages/flutter_tools/gradle/src/main/groovy/flutter.groovy
+++ packages/flutter_tools/gradle/src/main/groovy/flutter.groovy
@@ -829,6 +829,13 @@ class FlutterPlugin implements Plugin<Project> {
                                 output.processResourcesProvider.get() : output.processResources
                         def manifest = new XmlParser().parse(processResources.manifestFile)
                         manifest.application.activity.each { activity ->
+                            activity."meta-data".each { metadata ->
+                                def nameAttribute = metadata.attributes().find { it.key == 'android:name' }?.value == 'flutter_deeplinking_enabled'
+                                def valueAttribute = metadata.attributes().find { it.key == 'android:value' }?.value == 'true'
+                                if (nameAttribute && valueAttribute) {
+                                    appLinkSettings.deeplinkingFlagEnabled = true
+                                }
+                            }
                             activity."intent-filter".each { appLinkIntent ->
                                 // Print out the host attributes in data tags.
                                 def schemes = [] as Set<String>
@@ -868,6 +875,22 @@ class FlutterPlugin implements Plugin<Project> {
                                                 appLinkSettings.deeplinks.add(new Deeplink(scheme: scheme, host: host, path: path))
                                             }
                                         }
+                                    }
+                                }
+                                if (appLinkIntent.attributes().find { it.key == 'android:autoVerify' }?.value == 'true') {
+                                    appLinkSettings.hasAutoVerify = true
+                                }
+                                appLinkIntent.'action'.each { action ->
+                                    if (action.attributes().find { it.key == 'android:name' }?.value == 'android.intent.action.VIEW') {
+                                        appLinkSettings.hasActionView = true
+                                    }
+                                }
+                                appLinkIntent.'category'.each { category ->
+                                    if (category.attributes().find { it.key == 'android:name' }?.value == 'android.intent.category.DEFAULT') {
+                                        appLinkSettings.hasDefaultCategory = true
+                                    }
+                                    if (category.attributes().find { it.key == 'android:name' }?.value == 'android.intent.category.BROWSABLE') {
+                                        appLinkSettings.hasBrowsableCategory = true
                                     }
                                 }
                             }
@@ -1347,6 +1370,11 @@ class AppLinkSettings {
 
     String applicationId
     Set<Deeplink> deeplinks
+    boolean deeplinkingFlagEnabled
+    boolean hasAutoVerify
+    boolean hasActionView
+    boolean hasDefaultCategory
+    boolean hasBrowsableCategory
 
 }
 
