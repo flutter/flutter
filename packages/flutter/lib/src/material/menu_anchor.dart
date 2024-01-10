4b5fcff26ddf4e66946c95d069767d352b54ce64
Qun Cheng <chengqunq@gmail.com>
2024-01-10 14:27:49 -0800

    Remove unused import

--- packages/flutter/lib/src/material/menu_anchor.dart
+++ packages/flutter/lib/src/material/menu_anchor.dart
@@ -9,7 +9,6 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter/widgets.dart';
 
 import 'button_style.dart';
 import 'button_style_button.dart';
