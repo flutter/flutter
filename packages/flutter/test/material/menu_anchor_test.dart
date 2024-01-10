e9e3ade411f3bb970634411f650d77d552711d4e
Qun Cheng <chengqunq@gmail.com>
2024-01-10 14:54:05 -0800

    Remove unused import

--- packages/flutter/test/material/menu_anchor_test.dart
+++ packages/flutter/test/material/menu_anchor_test.dart
@@ -8,7 +8,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:meta/meta_meta.dart';
 
 import '../widgets/semantics_tester.dart';
 
