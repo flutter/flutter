4fa8327ffe5d38a7eac20df3a95ca26a4fb4e5cb
Jesse Seales <103135467+sealesj@users.noreply.github.com>
2024-01-11 18:59:47 +0000

    Osv-scanner integration (flutter/engine#49470)
    This change integrates directly with osv-scanner rather than using the OSV API to query each dependency for vulnerabilities.
    
    - use [lockfile format](https://github.com/flutter/engine/pull/49203) for third party deps rather than a flat file
    - let osv-scanner do the work of checking for vulns rather than API calls to OSV database
    - let osv-scanner create and populate the SARIF results 
    
    A successful run of the vulnerability scanner can be seen in the tests of this PR.
    
    *If you had to change anything in the [flutter/tests] repo, include a link to the migration guide as per the [breaking change policy].*
    
    [C++, Objective-C, Java style guides]: https://github.com/flutter/engine/blob/main/CONTRIBUTING.md#style

--- engine/src/flutter/ci/deps_parser.py
+++ engine/src/flutter/ci/deps_parser.py
@@ -4,13 +4,14 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 #
-# Usage: deps_parser.py --deps <DEPS file> --output <flattened deps>
+# Usage: deps_parser.py --deps <DEPS file> --output <lockfile>
 #
 # This script parses the DEPS file, extracts the fully qualified dependencies
 # and writes the to a file. This file will be later used to validate the dependencies
 # are pinned to a hash.
 
 import argparse
+import json
 import os
 import re
 import sys
@@ -19,7 +20,7 @@ SCRIPT_DIR = os.path.dirname(sys.argv[0])
 CHECKOUT_ROOT = os.path.realpath(os.path.join(SCRIPT_DIR, '..'))
 
 CHROMIUM_README_FILE = 'third_party/accessibility/README.md'
-CHROMIUM_README_COMMIT_LINE = 4  # the fifth line will always contain the commit hash
+CHROMIUM_README_COMMIT_LINE = 4  # The fifth line will always contain the commit hash.
 CHROMIUM = 'https://chromium.googlesource.com/chromium/src'
 
 
@@ -37,7 +38,7 @@ class VarImpl:
     """Implements the Var syntax."""
     if var_name in self._local_scope.get('vars', {}):
       return self._local_scope['vars'][var_name]
-    # Inject default values for env variables
+    # Inject default values for env variables.
     if var_name in self._env_vars:
       return self._env_vars[var_name]
     raise Exception('Var is not defined: %s' % var_name)
@@ -59,16 +60,26 @@ def parse_deps_file(deps_file):
 
   # Extract the deps and filter.
   deps = local_scope.get('deps', {})
-  filtered_deps = []
+  filtered_osv_deps = []
   for _, dep in deps.items():
     # We currently do not support packages or cipd which are represented
     # as dictionaries.
-    if isinstance(dep, str):
-      filtered_deps.append(dep)
-  return filtered_deps
+    if not isinstance(dep, str):
+      continue
+
+    dep_split = dep.rsplit('@', 1)
+    filtered_osv_deps.append({
+        'package': {'name': dep_split[0], 'commit': dep_split[1]}
+    })
+
+  osv_result = {
+      'packageSource': {'path': deps_file, 'type': 'lockfile'},
+      'packages': filtered_osv_deps
+  }
+  return osv_result
 
 
-def parse_readme(deps):
+def parse_readme():
   """
   Opens the Flutter Accessibility Library README and uses the commit hash
   found in the README to check for viulnerabilities.
@@ -76,24 +87,30 @@ def parse_readme(deps):
   """
   file_path = os.path.join(CHECKOUT_ROOT, CHROMIUM_README_FILE)
   with open(file_path) as file:
-    # read the content of the file opened
+    # Read the content of the file opened.
     content = file.readlines()
     commit_line = content[CHROMIUM_README_COMMIT_LINE]
     commit = re.search(r'(?<=\[).*(?=\])', commit_line)
-    deps.append(CHROMIUM + '@' + commit.group())
-    return deps
+
+    osv_result = {
+        'packageSource': {'path': file_path, 'type': 'lockfile'},
+        'packages': [{'package': {'name': CHROMIUM, 'commit': commit.group()}}]
+    }
+
+    return osv_result
 
 
 def write_manifest(deps, manifest_file):
-  print('\n'.join(sorted(deps)))
+  output = {'results': deps}
+  print(json.dumps(output, indent=2))
   with open(manifest_file, 'w') as manifest:
-    manifest.write('\n'.join(sorted(deps)))
+    json.dump(output, manifest, indent=2)
 
 
 def parse_args(args):
   args = args[1:]
   parser = argparse.ArgumentParser(
-      description='A script to flatten a gclient DEPS file.'
+      description='A script to extract DEPS into osv-scanner lockfile compatible format.'
   )
 
   parser.add_argument(
@@ -107,8 +124,8 @@ def parse_args(args):
       '--output',
       '-o',
       type=str,
-      help='Output flattened deps file.',
-      default=os.path.join(CHECKOUT_ROOT, 'deps_flatten.txt')
+      help='Output lockfile.',
+      default=os.path.join(CHECKOUT_ROOT, 'osv-lockfile.json')
   )
 
   return parser.parse_args(args)
@@ -116,9 +133,9 @@ def parse_args(args):
 
 def main(argv):
   args = parse_args(argv)
-  deps = parse_deps_file(args.deps)
-  deps = parse_readme(deps)
-  write_manifest(deps, args.output)
+  deps_deps = parse_deps_file(args.deps)
+  readme_deps = parse_readme()
+  write_manifest([deps_deps, readme_deps], args.output)
   return 0
 
 
