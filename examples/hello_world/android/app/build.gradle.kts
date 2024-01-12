a63610466222994ca3df8d5ac7ec80f075dcbb93
Bartek Pacia <barpac02@gmail.com>
2024-01-12 04:06:16 +0100

    examples/hello_world: migrate to KTS

---
+++ examples/hello_world/android/app/build.gradle.kts
@@ -0,0 +1,50 @@
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import java.io.FileInputStream
+import java.util.Properties
+
+plugins {
+    id("com.android.application")
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+val localProperties = Properties()
+val localPropertiesFile = rootProject.file("local.properties")
+localPropertiesFile.inputStream().bufferedReader(Charsets.UTF_8).use { inputStream ->
+    localProperties.load(inputStream)
+}
+
+var flutterVersionCode = localProperties.getProperty("flutter.versionCode") ?: "1"
+var flutterVersionName = localProperties.getProperty("flutter.versionName") ?: "1.0"
+
+android {
+    namespace = "io.flutter.examples.hello_world"
+    compileSdk = 33
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    defaultConfig {
+        applicationId = "io.flutter.examples.hello_world"
+        minSdk = 21
+        targetSdk = 33
+        versionCode = flutterVersionCode.toInt()
+        versionName = flutterVersionName
+    }
+
+    buildTypes {
+        named("release") {
+            // TODO: Add your own signing config for the release build.
+            // Signing with the debug keys for now, so `flutter run --release` works.
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+}
+
+flutter {
+    source = "../.."
+}
--- examples/hello_world/android/settings.gradle.kts
+++ examples/hello_world/android/settings.gradle.kts
@@ -31,8 +31,8 @@ buildscript {
 }
 
 plugins {
-    id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "7.3.0" apply false
+    id("dev.flutter.flutter-plugin-loader")
+    id("com.android.application") version "8.2.1" apply false
     id("org.jetbrains.kotlin.android") version "1.7.10" apply false
 }
 
