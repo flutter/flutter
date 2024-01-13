c6b8349d0a5e16abed471aaba3a5e44b472ccd05
Chinmay Garde <chinmaygarde@google.com>
2024-01-13 13:28:26 -0800

    Remove pthread based thread local support. (flutter/engine#49297)
    The thread_local storage class was not available pre-C++11. Even when C++11 was available, the C++ runtime in versions of iOS <= 9.0 was not capable of supporting this storage class. So we ended up using pthread directly in that case. The unique pointer support was added later. Now that the storage class has been supported on all Flutter platforms for a while, we can remove the fallback and remove a bunch of code. The ThreadLocalUniquePtr can be removed too but that can be attempted in a separate migration.

--- engine/src/flutter/fml/thread_local.cc
+++ engine/src/flutter/fml/thread_local.cc
@@ -3,39 +3,3 @@
 // found in the LICENSE file.
 
 #include "flutter/fml/thread_local.h"
-
-#if FML_THREAD_LOCAL_PTHREADS
-
-#include <cstring>
-
-#include "flutter/fml/logging.h"
-
-namespace fml {
-namespace internal {
-
-ThreadLocalPointer::ThreadLocalPointer(void (*destroy)(void*)) {
-  FML_CHECK(pthread_key_create(&key_, destroy) == 0);
-}
-
-ThreadLocalPointer::~ThreadLocalPointer() {
-  FML_CHECK(pthread_key_delete(key_) == 0);
-}
-
-void* ThreadLocalPointer::get() const {
-  return pthread_getspecific(key_);
-}
-
-void* ThreadLocalPointer::swap(void* ptr) {
-  void* old_ptr = get();
-  int err = pthread_setspecific(key_, ptr);
-  if (err) {
-    FML_CHECK(false) << "pthread_setspecific failed (" << err
-                     << "): " << strerror(err);
-  }
-  return old_ptr;
-}
-
-}  // namespace internal
-}  // namespace fml
-
-#endif  // FML_THREAD_LOCAL_PTHREADS
