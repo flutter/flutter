fbd42cff099e5248cdb3dfad9decf4933495abfd
Bartek Pacia <barpac02@gmail.com>
2024-01-14 21:36:13 +0100

    update dependency locks

--- examples/hello_world/android/settings.gradle.kts
+++ examples/hello_world/android/settings.gradle.kts
@@ -24,6 +24,13 @@ pluginManagement {
     }
 }
 
+buildscript {
+    dependencyLocking {
+        lockFile = file("${rootProject.projectDir}/buildscript-gradle.lockfile")
+        lockAllConfigurations()
+    }
+}
+
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
     id("com.android.application") version "7.4.2" apply false
