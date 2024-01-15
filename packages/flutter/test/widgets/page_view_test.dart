483c95dcb083f22a025aab888bb493f815110ccd
Polina Cherkasova <polina.c@live.com>
2024-01-14 17:28:24 -0800

    -

--- packages/flutter/test/widgets/page_view_test.dart
+++ packages/flutter/test/widgets/page_view_test.dart
@@ -419,7 +419,10 @@ void main() {
     expect(previousPageCompleted, true);
   });
 
-  testWidgets('PageView in zero-size container', (WidgetTester tester) async {
+  testWidgets('PageView in zero-size container',
+  // TODO(polina-c): Remove when PageView is fixed, https://github.com/flutter/flutter/issues/141119
+  experimentalLeakTesting: LeakTesting.settings.withIgnoredAll(),
+  (WidgetTester tester) async {
     await tester.pumpWidget(Directionality(
       textDirection: TextDirection.ltr,
       child: Center(
