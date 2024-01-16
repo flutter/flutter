flutter/flutter

commit ade9f18fa00883339efb5386db061e4514527868

[Impeller] Vulkan runtime effects/fragment program API (#49543)
Fixes https://github.com/flutter/flutter/issues/122823
Fixes https://github.com/flutter/flutter/issues/129659
Fixes https://github.com/flutter/flutter/issues/123741

This patch makes runtime stage/fragment program stuff work on Vulkan for Android.

It will need flutter/flutter#140976 for that to become a reality for flutter_tools users.

Compiling with relaxed Vulkan semantics still has an issue: shaders that use `sampler2D` with an explicitly set `location` on the `layout` will fail to compile with an error documented in https://github.com/flutter/flutter/issues/141219.

I think there might still be some issues with fragment programs on Vulkan, but this should at least be a good starting point and unblocks ink_sparkle.frag usage in the framework.

I've deleted some runtime_stage related code that would never get used - for example, enum related code that indicates we might support a bunch of data types that we do not and probably never will support in this API.

impeller/entity/contents/content_context.cc
@@ -490,4 +490,18 @@ void ContentContext::SetWireframe(bool wireframe) {
   wireframe_ = wireframe;
 }
 
+std::shared_ptr<Pipeline<PipelineDescriptor>>
+ContentContext::GetCachedRuntimeEffectPipeline(
+    const std::string& unique_entrypoint_name,
+    const ContentContextOptions& options,
+    const std::function<std::shared_ptr<Pipeline<PipelineDescriptor>>()>&
+        create_callback) const {
+  RuntimeEffectPipelineKey key{unique_entrypoint_name, options};
+  auto it = runtime_effect_pipelines_.find(key);
+  if (it == runtime_effect_pipelines_.end()) {
+    it = runtime_effect_pipelines_.insert(it, {key, create_callback()});
+  }
+  return it->second;
+}
+
 }  // namespace impeller

impeller/entity/contents/content_context.h
@@ -730,6 +730,21 @@ class ContentContext {
     return render_target_cache_;
   }
 
+  /// RuntimeEffect pipelines must be obtained via this method to avoid
+  /// re-creating them every frame.
+  ///
+  /// The unique_entrypoint_name comes from RuntimeEffect::GetEntrypoint.
+  /// Impellerc generates a unique entrypoint name for runtime effect shaders
+  /// based on the input file name and shader stage.
+  ///
+  /// The create_callback is synchronously invoked exactly once if a cached
+  /// pipeline is not found.
+  std::shared_ptr<Pipeline<PipelineDescriptor>> GetCachedRuntimeEffectPipeline(
+      const std::string& unique_entrypoint_name,
+      const ContentContextOptions& options,
+      const std::function<std::shared_ptr<Pipeline<PipelineDescriptor>>()>&
+          create_callback) const;
+
   /// @brief Retrieve the currnent host buffer for transient storage.
   ///
   /// This is only safe to use from the raster threads. Other threads should
@@ -739,6 +754,32 @@ class ContentContext {
  private:
   std::shared_ptr<Context> context_;
   std::shared_ptr<LazyGlyphAtlas> lazy_glyph_atlas_;
+
+  struct RuntimeEffectPipelineKey {
+    std::string unique_entrypoint_name;
+    ContentContextOptions options;
+
+    struct Hash {
+      std::size_t operator()(const RuntimeEffectPipelineKey& key) const {
+        return fml::HashCombine(key.unique_entrypoint_name,
+                                ContentContextOptions::Hash{}(key.options));
+      }
+    };
+
+    struct Equal {
+      constexpr bool operator()(const RuntimeEffectPipelineKey& lhs,
+                                const RuntimeEffectPipelineKey& rhs) const {
+        return lhs.unique_entrypoint_name == rhs.unique_entrypoint_name &&
+               ContentContextOptions::Equal{}(lhs.options, rhs.options);
+      }
+    };
+  };
+
+  mutable std::unordered_map<RuntimeEffectPipelineKey,
+                             std::shared_ptr<Pipeline<PipelineDescriptor>>,
+                             RuntimeEffectPipelineKey::Hash,
+                             RuntimeEffectPipelineKey::Equal>
+      runtime_effect_pipelines_;
 
   template <class PipelineT>
   class Variants {

impeller/entity/contents/runtime_effect_contents.cc
@@ -47,19 +47,8 @@ static ShaderType GetShaderType(RuntimeUniformType type) {
       return ShaderType::kSampledImage;
     case kFloat:
       return ShaderType::kFloat;
-    case kBoolean:
-    case kSignedByte:
-    case kUnsignedByte:
-    case kSignedShort:
-    case kUnsignedShort:
-    case kSignedInt:
-    case kUnsignedInt:
-    case kSignedInt64:
-    case kUnsignedInt64:
-    case kHalfFloat:
-    case kDouble:
-      VALIDATION_LOG << "Unsupported uniform type.";
-      return ShaderType::kVoid;
+    case kStruct:
+      return ShaderType::kStruct;
   }
 }
 
@@ -79,15 +68,6 @@ static std::shared_ptr<ShaderMetadata> MakeShaderMetadata(
 bool RuntimeEffectContents::Render(const ContentContext& renderer,
                                    const Entity& entity,
                                    RenderPass& pass) const {
-  // TODO(jonahwilliams): FragmentProgram API is not fully wired up on Android.
-  // Disable until this is complete so that integration tests and benchmarks can
-  // run m3 applications.
-  if (renderer.GetContext()->GetBackendType() ==
-      Context::BackendType::kVulkan) {
-    FML_DLOG(WARNING)
-        << "Fragment programs not supported on Vulkan. Content dropped.";
-    return true;
-  }
   auto context = renderer.GetContext();
   auto library = context->GetShaderLibrary();
 
@@ -147,7 +127,8 @@ bool RuntimeEffectContents::Render(const ContentContext& renderer,
       GetGeometry()->GetPositionBuffer(renderer, entity, pass);
 
   //--------------------------------------------------------------------------
-  /// Get or create runtime stage pipeline.
+  /// Set up the command. Defer setting up the pipeline until the descriptor set
+  /// layouts are known from the uniforms.
   ///
 
   const auto& caps = context->GetCapabilities();
@@ -155,41 +136,9 @@ bool RuntimeEffectContents::Render(const ContentContext& renderer,
   const auto stencil_attachment_format = caps->GetDefaultStencilFormat();
 
   using VS = RuntimeEffectVertexShader;
-  PipelineDescriptor desc;
-  desc.SetLabel("Runtime Stage");
-  desc.AddStageEntrypoint(
-      library->GetFunction(VS::kEntrypointName, ShaderStage::kVertex));
-  desc.AddStageEntrypoint(library->GetFunction(runtime_stage_->GetEntrypoint(),
-                                               ShaderStage::kFragment));
-  auto vertex_descriptor = std::make_shared<VertexDescriptor>();
-  vertex_descriptor->SetStageInputs(VS::kAllShaderStageInputs,
-                                    VS::kInterleavedBufferLayout);
-  desc.SetVertexDescriptor(std::move(vertex_descriptor));
-  desc.SetColorAttachmentDescriptor(
-      0u, {.format = color_attachment_format, .blending_enabled = true});
-
-  StencilAttachmentDescriptor stencil0;
-  stencil0.stencil_compare = CompareFunction::kEqual;
-  desc.SetStencilAttachmentDescriptors(stencil0);
-  desc.SetStencilPixelFormat(stencil_attachment_format);
-
-  auto options = OptionsFromPassAndEntity(pass, entity);
-  if (geometry_result.prevent_overdraw) {
-    options.stencil_compare = CompareFunction::kEqual;
-    options.stencil_operation = StencilOperation::kIncrementClamp;
-  }
-  options.primitive_type = geometry_result.type;
-  options.ApplyToPipelineDescriptor(desc);
-
-  auto pipeline = context->GetPipelineLibrary()->GetPipeline(desc).Get();
-  if (!pipeline) {
-    VALIDATION_LOG << "Failed to get or create runtime effect pipeline.";
-    return false;
-  }
 
   Command cmd;
   DEBUG_COMMAND_INFO(cmd, "RuntimeEffectContents");
-  cmd.pipeline = pipeline;
   cmd.stencil_reference = entity.GetClipDepth();
   cmd.BindVertices(std::move(geometry_result.vertex_buffer));
 
@@ -209,6 +158,9 @@ bool RuntimeEffectContents::Render(const ContentContext& renderer,
   size_t minimum_sampler_index = 100000000;
   size_t buffer_index = 0;
   size_t buffer_offset = 0;
+
+  std::vector<DescriptorSetLayout> descriptor_set_layouts;
+
   for (const auto& uniform : runtime_stage_->GetUniforms()) {
     std::shared_ptr<ShaderMetadata> metadata = MakeShaderMetadata(uniform);
 
@@ -227,6 +179,10 @@ bool RuntimeEffectContents::Render(const ContentContext& renderer,
         break;
       }
       case kFloat: {
+        FML_DCHECK(renderer.GetContext()->GetBackendType() !=
+                   Context::BackendType::kVulkan)
+            << "Uniform " << uniform.name
+            << " had unexpected type kFloat for Vulkan backend.";
         size_t alignment =
             std::max(uniform.bit_width / 8, DefaultUniformAlignment());
         auto buffer_view = renderer.GetTransientsBuffer().Emplace(
@@ -242,20 +198,41 @@ bool RuntimeEffectContents::Render(const ContentContext& renderer,
         buffer_offset += uniform.GetSize();
         break;
       }
-      case kBoolean:
-      case kSignedByte:
-      case kUnsignedByte:
-      case kSignedShort:
-      case kUnsignedShort:
-      case kSignedInt:
-      case kUnsignedInt:
-      case kSignedInt64:
-      case kUnsignedInt64:
-      case kHalfFloat:
-      case kDouble:
-        VALIDATION_LOG << "Unsupported uniform type for " << uniform.name
-                       << ".";
-        return true;
+      case kStruct: {
+        FML_DCHECK(renderer.GetContext()->GetBackendType() ==
+                   Context::BackendType::kVulkan);
+        descriptor_set_layouts.emplace_back(DescriptorSetLayout{
+            static_cast<uint32_t>(uniform.location),
+            DescriptorType::kUniformBuffer,
+            ShaderStage::kFragment,
+        });
+        ShaderUniformSlot uniform_slot;
+        uniform_slot.name = uniform.name.c_str();
+        uniform_slot.binding = uniform.location;
+
+        std::vector<float> uniform_buffer;
+        uniform_buffer.reserve(uniform.struct_layout.size());
+        size_t uniform_byte_index = 0u;
+        for (const auto& byte_type : uniform.struct_layout) {
+          if (byte_type == 0) {
+            uniform_buffer.push_back(0.f);
+          } else if (byte_type == 1) {
+            uniform_buffer.push_back(reinterpret_cast<float*>(
+                uniform_data_->data())[uniform_byte_index++]);
+          } else {
+            FML_UNREACHABLE();
+          }
+        }
+
+        size_t alignment = std::max(sizeof(float) * uniform_buffer.size(),
+                                    DefaultUniformAlignment());
+
+        auto buffer_view = renderer.GetTransientsBuffer().Emplace(
+            reinterpret_cast<const void*>(uniform_buffer.data()), alignment,
+            alignment);
+        cmd.BindResource(ShaderStage::kFragment, uniform_slot, ShaderMetadata{},
+                         buffer_view);
+      }
     }
   }
 
@@ -273,6 +250,19 @@ bool RuntimeEffectContents::Render(const ContentContext& renderer,
 
         SampledImageSlot image_slot;
         image_slot.name = uniform.name.c_str();
+
+        uint32_t sampler_binding_location = 0u;
+        if (!descriptor_set_layouts.empty()) {
+          sampler_binding_location = descriptor_set_layouts.back().binding + 1;
+        }
+
+        descriptor_set_layouts.emplace_back(DescriptorSetLayout{
+            sampler_binding_location,
+            DescriptorType::kSampledImage,
+            ShaderStage::kFragment,
+        });
+
+        image_slot.binding = sampler_binding_location;
         image_slot.texture_index = uniform.location - minimum_sampler_index;
         cmd.BindResource(ShaderStage::kFragment, image_slot, *metadata,
                          input.texture, sampler);
@@ -284,6 +274,51 @@ bool RuntimeEffectContents::Render(const ContentContext& renderer,
         continue;
     }
   }
+
+  /// Now that the descriptor set layouts are known, get the pipeline.
+
+  auto options = OptionsFromPassAndEntity(pass, entity);
+  if (geometry_result.prevent_overdraw) {
+    options.stencil_compare = CompareFunction::kEqual;
+    options.stencil_operation = StencilOperation::kIncrementClamp;
+  }
+  options.primitive_type = geometry_result.type;
+
+  auto create_callback =
+      [&]() -> std::shared_ptr<Pipeline<PipelineDescriptor>> {
+    PipelineDescriptor desc;
+    desc.SetLabel("Runtime Stage");
+    desc.AddStageEntrypoint(
+        library->GetFunction(VS::kEntrypointName, ShaderStage::kVertex));
+    desc.AddStageEntrypoint(library->GetFunction(
+        runtime_stage_->GetEntrypoint(), ShaderStage::kFragment));
+    auto vertex_descriptor = std::make_shared<VertexDescriptor>();
+    vertex_descriptor->SetStageInputs(VS::kAllShaderStageInputs,
+                                      VS::kInterleavedBufferLayout);
+    vertex_descriptor->RegisterDescriptorSetLayouts(VS::kDescriptorSetLayouts);
+    vertex_descriptor->RegisterDescriptorSetLayouts(
+        descriptor_set_layouts.data(), descriptor_set_layouts.size());
+    desc.SetVertexDescriptor(std::move(vertex_descriptor));
+    desc.SetColorAttachmentDescriptor(
+        0u, {.format = color_attachment_format, .blending_enabled = true});
+
+    StencilAttachmentDescriptor stencil0;
+    stencil0.stencil_compare = CompareFunction::kEqual;
+    desc.SetStencilAttachmentDescriptors(stencil0);
+    desc.SetStencilPixelFormat(stencil_attachment_format);
+
+    options.ApplyToPipelineDescriptor(desc);
+    auto pipeline = context->GetPipelineLibrary()->GetPipeline(desc).Get();
+    if (!pipeline) {
+      VALIDATION_LOG << "Failed to get or create runtime effect pipeline.";
+      return nullptr;
+    }
+
+    return pipeline;
+  };
+
+  cmd.pipeline = renderer.GetCachedRuntimeEffectPipeline(
+      runtime_stage_->GetEntrypoint(), options, create_callback);
 
   pass.AddCommand(std::move(cmd));
 

impeller/entity/entity_unittests.cc
@@ -2132,10 +2132,6 @@ TEST_P(EntityTest, YUVToRGBFilter) {
 }
 
 TEST_P(EntityTest, RuntimeEffect) {
-  if (!BackendSupportsFragmentProgram()) {
-    GTEST_SKIP_("This backend doesn't support runtime effects.");
-  }
-
   auto runtime_stages =
       OpenAssetAsRuntimeStage("runtime_stage_example.frag.iplr");
   auto runtime_stage =
@@ -2144,6 +2140,7 @@ TEST_P(EntityTest, RuntimeEffect) {
   ASSERT_TRUE(runtime_stage->IsDirty());
 
   bool first_frame = true;
+  Pipeline<PipelineDescriptor>* first_pipeline = nullptr;
   auto callback = [&](ContentContext& context, RenderPass& pass) -> bool {
     if (first_frame) {
       first_frame = false;
@@ -2170,7 +2167,13 @@ TEST_P(EntityTest, RuntimeEffect) {
 
     Entity entity;
     entity.SetContents(contents);
-    return contents->Render(context, entity, pass);
+    bool result = contents->Render(context, entity, pass);
+    if (!first_pipeline) {
+      first_pipeline = pass.GetCommands().back().pipeline.get();
+    } else {
+      EXPECT_EQ(pass.GetCommands().back().pipeline.get(), first_pipeline);
+    }
+    return result;
   };
   ASSERT_TRUE(OpenPlaygroundHere(callback));
 }

impeller/fixtures/BUILD.gn
@@ -64,6 +64,7 @@ impellerc("runtime_stages") {
   shader_target_flags = [
     "--runtime-stage-metal",
     "--runtime-stage-gles",
+    "--runtime-stage-vulkan",
   ]
 
   iplr = true

lib/ui/fixtures/shaders/BUILD.gn
@@ -41,6 +41,7 @@ if (enable_unittests) {
     shader_target_flags = [
       "--runtime-stage-metal",
       "--runtime-stage-gles",
+      "--runtime-stage-vulkan",
     ]
     intermediates_subdir = "iplr-remap"
     sl_file_extension = "iplr"

impeller/aiks/aiks_unittests.cc
@@ -2704,10 +2704,6 @@ TEST_P(AiksTest, CanRenderForegroundAdvancedBlendWithMaskBlur) {
 
 // Regression test for https://github.com/flutter/flutter/issues/126701 .
 TEST_P(AiksTest, CanRenderClippedRuntimeEffects) {
-  if (!BackendSupportsFragmentProgram()) {
-    GTEST_SKIP_("This backend doesn't support runtime effects.");
-  }
-
   auto runtime_stages =
       OpenAssetAsRuntimeStage("runtime_stage_example.frag.iplr");
 
@@ -2741,10 +2737,6 @@ TEST_P(AiksTest, CanRenderClippedRuntimeEffects) {
 }
 
 TEST_P(AiksTest, DrawPaintTransformsBounds) {
-  if (!BackendSupportsFragmentProgram()) {
-    GTEST_SKIP_("This backend doesn't support runtime effects.");
-  }
-
   auto runtime_stages = OpenAssetAsRuntimeStage("gradient.frag.iplr");
   auto runtime_stage = runtime_stages[RuntimeStageBackend::kMetal];
   ASSERT_TRUE(runtime_stage);

commit 10aa56b6d63392c3ef8869425ef5c7cb6aa52fa5

[Impeller] Fix hot reload for shaders on Impeller (#49739)
I broke this in https://github.com/flutter/engine/pull/49543

While investigating https://github.com/flutter/flutter/issues/141235 I
realized that the specific things that were wrong there were probably
artifacts of some intermediate issue in the previous patch - they no
longer appear. However, I had broken hot reload.

This patch fixes it and updates the `EntityTest.RuntimeEffect` to
artificially run the rendering callback a few times simulating a hot
reload. The test fails without my changes here.

ci/licenses_golden/excluded_files
@@ -143,6 +143,7 @@
 ../../../flutter/impeller/display_list/skia_conversions_unittests.cc
 ../../../flutter/impeller/docs
 ../../../flutter/impeller/entity/contents/checkerboard_contents_unittests.cc
+../../../flutter/impeller/entity/contents/content_context_unittests.cc
 ../../../flutter/impeller/entity/contents/filters/directional_gaussian_blur_filter_contents_unittests.cc
 ../../../flutter/impeller/entity/contents/filters/gaussian_blur_filter_contents_unittests.cc
 ../../../flutter/impeller/entity/contents/filters/inputs/filter_input_unittests.cc

impeller/entity/BUILD.gn
@@ -266,6 +266,7 @@ impeller_component("entity_unittests") {
 
   sources = [
     "contents/checkerboard_contents_unittests.cc",
+    "contents/content_context_unittests.cc",
     "contents/filters/directional_gaussian_blur_filter_contents_unittests.cc",
     "contents/filters/gaussian_blur_filter_contents_unittests.cc",
     "contents/filters/inputs/filter_input_unittests.cc",

impeller/entity/contents/content_context.cc
@@ -504,4 +504,16 @@ ContentContext::GetCachedRuntimeEffectPipeline(
   return it->second;
 }
 
+void ContentContext::ClearCachedRuntimeEffectPipeline(
+    const std::string& unique_entrypoint_name) const {
+  for (auto it = runtime_effect_pipelines_.begin();
+       it != runtime_effect_pipelines_.end();) {
+    if (it->first.unique_entrypoint_name == unique_entrypoint_name) {
+      it = runtime_effect_pipelines_.erase(it);
+    } else {
+      it++;
+    }
+  }
+}
+
 }  // namespace impeller

impeller/entity/contents/content_context.h
@@ -745,6 +745,11 @@ class ContentContext {
       const std::function<std::shared_ptr<Pipeline<PipelineDescriptor>>()>&
           create_callback) const;
 
+  /// Used by hot reload/hot restart to clear a cached pipeline from
+  /// GetCachedRuntimeEffectPipeline.
+  void ClearCachedRuntimeEffectPipeline(
+      const std::string& unique_entrypoint_name) const;
+
   /// @brief Retrieve the currnent host buffer for transient storage.
   ///
   /// This is only safe to use from the raster threads. Other threads should

impeller/entity/contents/content_context_unittests.cc
@@ -1,0 +1,139 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <cstdint>
+
+#include "gtest/gtest.h"
+
+#include "impeller/core/allocator.h"
+#include "impeller/core/device_buffer_descriptor.h"
+#include "impeller/entity/contents/content_context.h"
+#include "impeller/geometry/color.h"
+#include "impeller/renderer/capabilities.h"
+#include "impeller/renderer/pipeline.h"
+#include "impeller/renderer/pipeline_descriptor.h"
+
+namespace impeller {
+namespace testing {
+
+class FakeAllocator : public Allocator {
+ public:
+  FakeAllocator() : Allocator() {}
+
+  uint16_t MinimumBytesPerRow(PixelFormat format) const override { return 0; }
+  ISize GetMaxTextureSizeSupported() const override { return ISize(); }
+
+  std::shared_ptr<DeviceBuffer> OnCreateBuffer(
+      const DeviceBufferDescriptor& desc) override {
+    return nullptr;
+  }
+  std::shared_ptr<Texture> OnCreateTexture(
+      const TextureDescriptor& desc) override {
+    return nullptr;
+  }
+};
+
+class FakeContext : public Context {
+ public:
+  FakeContext() : Context(), allocator_(std::make_shared<FakeAllocator>()) {}
+
+  BackendType GetBackendType() const override { return BackendType::kVulkan; }
+  std::string DescribeGpuModel() const override { return ""; }
+  bool IsValid() const override { return false; }
+  const std::shared_ptr<const Capabilities>& GetCapabilities() const override {
+    return capabilities_;
+  }
+  std::shared_ptr<Allocator> GetResourceAllocator() const override {
+    return allocator_;
+  }
+  std::shared_ptr<ShaderLibrary> GetShaderLibrary() const { return nullptr; }
+  std::shared_ptr<SamplerLibrary> GetSamplerLibrary() const { return nullptr; }
+  std::shared_ptr<PipelineLibrary> GetPipelineLibrary() const {
+    return nullptr;
+  }
+  std::shared_ptr<CommandBuffer> CreateCommandBuffer() const { return nullptr; }
+  void Shutdown() {}
+
+ private:
+  std::shared_ptr<Allocator> allocator_;
+  std::shared_ptr<const Capabilities> capabilities_;
+};
+
+class FakePipeline : public Pipeline<PipelineDescriptor> {
+ public:
+  FakePipeline() : Pipeline({}, PipelineDescriptor{}) {}
+
+  bool IsValid() const override { return false; }
+};
+
+static std::shared_ptr<FakePipeline> CreateFakePipelineCallback() {
+  return std::make_shared<FakePipeline>();
+}
+
+TEST(ContentContext, CachesPipelines) {
+  auto context = std::make_shared<FakeContext>();
+  ContentContext content_context(context, nullptr);
+  ContentContextOptions optionsA{.blend_mode = BlendMode::kSourceOver};
+  ContentContextOptions optionsB{.blend_mode = BlendMode::kSource};
+
+  auto pipelineA = content_context.GetCachedRuntimeEffectPipeline(
+      "A", optionsA, CreateFakePipelineCallback);
+
+  auto pipelineA2 = content_context.GetCachedRuntimeEffectPipeline(
+      "A", optionsA, CreateFakePipelineCallback);
+
+  auto pipelineA3 = content_context.GetCachedRuntimeEffectPipeline(
+      "A", optionsB, CreateFakePipelineCallback);
+
+  auto pipelineB = content_context.GetCachedRuntimeEffectPipeline(
+      "B", optionsB, CreateFakePipelineCallback);
+
+  ASSERT_EQ(pipelineA.get(), pipelineA2.get());
+  ASSERT_NE(pipelineA.get(), pipelineA3.get());
+  ASSERT_NE(pipelineB.get(), pipelineA.get());
+}
+
+TEST(ContentContext, InvalidatesAllPipelinesWithSameUniqueNameOnClear) {
+  auto context = std::make_shared<FakeContext>();
+  ContentContext content_context(context, nullptr);
+  ContentContextOptions optionsA{.blend_mode = BlendMode::kSourceOver};
+  ContentContextOptions optionsB{.blend_mode = BlendMode::kSource};
+
+  auto pipelineA = content_context.GetCachedRuntimeEffectPipeline(
+      "A", optionsA, CreateFakePipelineCallback);
+
+  auto pipelineA2 = content_context.GetCachedRuntimeEffectPipeline(
+      "A", optionsB, CreateFakePipelineCallback);
+
+  auto pipelineB = content_context.GetCachedRuntimeEffectPipeline(
+      "B", optionsB, CreateFakePipelineCallback);
+
+  ASSERT_TRUE(pipelineA);
+  ASSERT_TRUE(pipelineA2);
+  ASSERT_TRUE(pipelineB);
+
+  ASSERT_EQ(pipelineA, content_context.GetCachedRuntimeEffectPipeline(
+                           "A", optionsA, CreateFakePipelineCallback));
+  ASSERT_EQ(pipelineA2, content_context.GetCachedRuntimeEffectPipeline(
+                            "A", optionsB, CreateFakePipelineCallback));
+  ASSERT_EQ(pipelineB, content_context.GetCachedRuntimeEffectPipeline(
+                           "B", optionsB, CreateFakePipelineCallback));
+
+  content_context.ClearCachedRuntimeEffectPipeline("A");
+
+  ASSERT_NE(pipelineA, content_context.GetCachedRuntimeEffectPipeline(
+                           "A", optionsA, CreateFakePipelineCallback));
+  ASSERT_NE(pipelineA2, content_context.GetCachedRuntimeEffectPipeline(
+                            "A", optionsB, CreateFakePipelineCallback));
+  ASSERT_EQ(pipelineB, content_context.GetCachedRuntimeEffectPipeline(
+                           "B", optionsB, CreateFakePipelineCallback));
+
+  content_context.ClearCachedRuntimeEffectPipeline("B");
+
+  ASSERT_NE(pipelineB, content_context.GetCachedRuntimeEffectPipeline(
+                           "B", optionsB, CreateFakePipelineCallback));
+}
+
+}  // namespace testing
+}  // namespace impeller

impeller/entity/contents/runtime_effect_contents.cc
@@ -80,7 +80,21 @@ bool RuntimeEffectContents::Render(const ContentContext& renderer,
   std::shared_ptr<const ShaderFunction> function = library->GetFunction(
       runtime_stage_->GetEntrypoint(), ShaderStage::kFragment);
 
+  //--------------------------------------------------------------------------
+  /// Resolve geometry and content context options.
+  ///
+
+  auto geometry_result =
+      GetGeometry()->GetPositionBuffer(renderer, entity, pass);
+  auto options = OptionsFromPassAndEntity(pass, entity);
+  if (geometry_result.prevent_overdraw) {
+    options.stencil_compare = CompareFunction::kEqual;
+    options.stencil_operation = StencilOperation::kIncrementClamp;
+  }
+  options.primitive_type = geometry_result.type;
+
   if (function && runtime_stage_->IsDirty()) {
+    renderer.ClearCachedRuntimeEffectPipeline(runtime_stage_->GetEntrypoint());
     context->GetPipelineLibrary()->RemovePipelinesWithEntryPoint(function);
     library->UnregisterFunction(runtime_stage_->GetEntrypoint(),
                                 ShaderStage::kFragment);
@@ -118,13 +132,6 @@ bool RuntimeEffectContents::Render(const ContentContext& renderer,
 
     runtime_stage_->SetClean();
   }
-
-  //--------------------------------------------------------------------------
-  /// Resolve geometry.
-  ///
-
-  auto geometry_result =
-      GetGeometry()->GetPositionBuffer(renderer, entity, pass);
 
   //--------------------------------------------------------------------------
   /// Set up the command. Defer setting up the pipeline until the descriptor set
@@ -275,14 +282,6 @@ bool RuntimeEffectContents::Render(const ContentContext& renderer,
   }
 
   /// Now that the descriptor set layouts are known, get the pipeline.
-
-  auto options = OptionsFromPassAndEntity(pass, entity);
-  if (geometry_result.prevent_overdraw) {
-    options.stencil_compare = CompareFunction::kEqual;
-    options.stencil_operation = StencilOperation::kIncrementClamp;
-  }
-  options.primitive_type = geometry_result.type;
-
   auto create_callback =
       [&]() -> std::shared_ptr<Pipeline<PipelineDescriptor>> {
     PipelineDescriptor desc;

impeller/entity/entity_unittests.cc
@@ -2140,14 +2140,10 @@ TEST_P(EntityTest, RuntimeEffect) {
   ASSERT_TRUE(runtime_stage);
   ASSERT_TRUE(runtime_stage->IsDirty());
 
-  bool first_frame = true;
+  bool expect_dirty = true;
   Pipeline<PipelineDescriptor>* first_pipeline = nullptr;
   auto callback = [&](ContentContext& context, RenderPass& pass) -> bool {
-    if (first_frame) {
-      first_frame = false;
-    } else {
-      assert(runtime_stage->IsDirty() == false);
-    }
+    EXPECT_EQ(runtime_stage->IsDirty(), expect_dirty);
 
     auto contents = std::make_shared<RuntimeEffectContents>();
     contents->SetGeometry(Geometry::MakeCover());
@@ -2169,13 +2165,38 @@ TEST_P(EntityTest, RuntimeEffect) {
     Entity entity;
     entity.SetContents(contents);
     bool result = contents->Render(context, entity, pass);
-    if (!first_pipeline) {
+
+    if (expect_dirty) {
+      EXPECT_NE(first_pipeline, pass.GetCommands().back().pipeline.get());
       first_pipeline = pass.GetCommands().back().pipeline.get();
     } else {
       EXPECT_EQ(pass.GetCommands().back().pipeline.get(), first_pipeline);
     }
+
+    expect_dirty = false;
     return result;
   };
+
+  // Simulate some renders and hot reloading of the shader.
+  auto content_context = GetContentContext();
+  {
+    RenderTarget target;
+    testing::MockRenderPass mock_pass(GetContext(), target);
+    callback(*content_context, mock_pass);
+    callback(*content_context, mock_pass);
+
+    // Dirty the runtime stage.
+    runtime_stages = OpenAssetAsRuntimeStage("runtime_stage_example.frag.iplr");
+    runtime_stage =
+        runtime_stages[PlaygroundBackendToRuntimeStageBackend(GetBackend())];
+
+    ASSERT_TRUE(runtime_stage->IsDirty());
+    expect_dirty = true;
+
+    callback(*content_context, mock_pass);
+    callback(*content_context, mock_pass);
+  }
+
   ASSERT_TRUE(OpenPlaygroundHere(callback));
 }
 