dbe326192aebc83ac406d734d2cc04512f0ba26c
Daco Harkes <dacoharkes@google.com>
2024-01-19 20:41:52 +0100

    Update packages/flutter_tools/lib/src/macos/native_assets_host.dart
    Co-authored-by: Jenn Magder <magder@google.com>

--- packages/flutter_tools/lib/src/macos/native_assets_host.dart
+++ packages/flutter_tools/lib/src/macos/native_assets_host.dart
@@ -22,30 +22,30 @@ Future<void> createInfoPlist(
 ) async {
   final File infoPlistFile = target.childFile('Info.plist');
   await infoPlistFile.writeAsString('''
-  <?xml version="1.0" encoding="UTF-8"?>
-  <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-  <plist version="1.0">
-  <dict>
-    <key>CFBundleDevelopmentRegion</key>
-    <string>en</string>
-    <key>CFBundleExecutable</key>
-    <string>$name</string>
-    <key>CFBundleIdentifier</key>
-    <string>io.flutter.flutter.native_assets.$name</string>
-    <key>CFBundleInfoDictionaryVersion</key>
-    <string>6.0</string>
-    <key>CFBundleName</key>
-    <string>$name</string>
-    <key>CFBundlePackageType</key>
-    <string>FMWK</string>
-    <key>CFBundleShortVersionString</key>
-    <string>1.0</string>
-    <key>CFBundleSignature</key>
-    <string>????</string>
-    <key>CFBundleVersion</key>
-    <string>1.0</string>
-  </dict>
-  </plist>
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+	<key>CFBundleDevelopmentRegion</key>
+	 <string>en</string>
+	<key>CFBundleExecutable</key>
+	<string>$name</string>
+	<key>CFBundleIdentifier</key>
+	<string>io.flutter.flutter.native_assets.$name</string>
+	<key>CFBundleInfoDictionaryVersion</key>
+	<string>6.0</string>
+	<key>CFBundleName</key>
+	<string>$name</string>
+	<key>CFBundlePackageType</key>
+	<string>FMWK</string>
+	<key>CFBundleShortVersionString</key>
+	<string>1.0</string>
+	<key>CFBundleSignature</key>
+	<string>????</string>
+	<key>CFBundleVersion</key>
+	<string>1.0</string>
+</dict>
+</plist>
   ''');
 }
 
