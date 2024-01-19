0de3d3621656021ea38451bbcbfb662f0421db0f
Bruno Leroux <leroux_bruno@yahoo.fr>
2024-01-19 10:17:06 +0100

    [Android] Fix KeyEmbedderResponder throws a NullPointerException (flutter/engine#49860)
    ## Description
    
    This PR fixes a `NullPointerException` thrown from `KeyEmbedderResponder` when pressing a key during a hot restart (at this time the framework might not be ready and null reply is sent back).
    
    ## Related Issue
    
    Fixes https://github.com/flutter/flutter/issues/141662.
    
    ## Tests
    
    Adds 1 test.

--- engine/src/flutter/shell/platform/android/io/flutter/embedding/android/KeyEmbedderResponder.java
+++ engine/src/flutter/shell/platform/android/io/flutter/embedding/android/KeyEmbedderResponder.java
@@ -8,6 +8,7 @@ import android.view.InputDevice;
 import android.view.KeyEvent;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import io.flutter.Log;
 import io.flutter.embedding.android.KeyboardMap.PressingGoal;
 import io.flutter.embedding.android.KeyboardMap.TogglingGoal;
 import io.flutter.plugin.common.BinaryMessenger;
@@ -398,9 +399,13 @@ public class KeyEmbedderResponder implements KeyboardManager.Responder {
             ? null
             : message -> {
               Boolean handled = false;
-              message.rewind();
-              if (message.capacity() != 0) {
-                handled = message.get() != 0;
+              if (message != null) {
+                message.rewind();
+                if (message.capacity() != 0) {
+                  handled = message.get() != 0;
+                }
+              } else {
+                Log.w(TAG, "A null reply was received when sending a key event to the framework.");
               }
               onKeyEventHandledCallback.onKeyEventHandled(handled);
             };
