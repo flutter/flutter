26e2e43a8ea6ee72a4b8d28b806cc54611219f05
Jonas Uekötter <ueman@users.noreply.github.com>
2024-01-22 19:26:37 +0100

    Improve documentation for FlutterVersion class

--- packages/flutter/lib/src/services/flutter_version.dart
+++ packages/flutter/lib/src/services/flutter_version.dart
@@ -2,8 +2,12 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-/// [FlutterVersion] contains various meta information about whith which Flutter build this app was compiled.
-/// This contains the information that is output by executing `flutter --version`.
+/// [FlutterVersion] contains various meta information about with which 
+/// Flutter version this app was compiled.
+/// It tries to mimic the information that is seen when executing `flutter --version`.
+///
+/// When this Flutter version was build from a fork, or when Flutter runs in a
+/// custom embedder, these values might be unreliable.
 ///
 /// See also:
 ///  - [Platform.version](https://api.dart.dev/stable/dart-io/Platform/version.html)
@@ -11,22 +15,28 @@ abstract final class FlutterVersion {
   const FlutterVersion._();
 
   /// The Flutter version used to compile the app.
-  static const String version = String.fromEnvironment('FLUTTER_VERSION');
+  static const String? version = String.fromEnvironment('FLUTTER_VERSION') != '' ?
+    String.fromEnvironment('FLUTTER_VERSION') : null;
 
   /// The Flutter channel used to compile the app.
-  static const String channel = String.fromEnvironment('FLUTTER_CHANNEL');
+  static const String? channel = String.fromEnvironment('FLUTTER_CHANNEL') != '' ?
+    String.fromEnvironment('FLUTTER_CHANNEL') : null;
 
   /// The link to the Git URL from which Flutter is obtained.
-  static const String gitUrl = String.fromEnvironment('FLUTTER_GIT_URL');
+  static const String? gitUrl = String.fromEnvironment('FLUTTER_GIT_URL') != '' ?
+    String.fromEnvironment('FLUTTER_GIT_URL') : null;
 
   /// The Flutter framework revision.
-  static const String frameworkRevision = String.fromEnvironment('FLUTTER_FRAMEWORK_REVISION');
+  static const String? frameworkRevision = String.fromEnvironment('FLUTTER_FRAMEWORK_REVISION') != '' ?
+    String.fromEnvironment('FLUTTER_FRAMEWORK_REVISION') : null;
 
   /// The Flutter engine revision.
-  static const String engineRevision = String.fromEnvironment('FLUTTER_ENGINE_REVISION');
+  static const String? engineRevision = String.fromEnvironment('FLUTTER_ENGINE_REVISION') != '' ?
+    String.fromEnvironment('FLUTTER_ENGINE_REVISION') : null;
 
-  // This is included, since [Platform.version](https://api.dart.dev/stable/dart-io/Platform/version.html)
+  // This is included since [Platform.version](https://api.dart.dev/stable/dart-io/Platform/version.html)
   // is not included on web platforms.
   /// The Dart version used to compile the app.
-  static const String dartVersion = String.fromEnvironment('FLUTTER_DART_VERSION');
+  static const String? dartVersion = String.fromEnvironment('FLUTTER_DART_VERSION') != '' ?
+    String.fromEnvironment('FLUTTER_DART_VERSION') : null;
 }
