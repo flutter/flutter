9d2aa524dd2da297bb8b5608823f6a02d6a20b1a
Justin McCandless <jmccandless@google.com>
2024-01-22 11:14:34 -0800

    Remove redundant tests that were moved to cover all platforms in EditableText

--- packages/flutter/test/material/text_selection_test.dart
+++ packages/flutter/test/material/text_selection_test.dart
@@ -37,66 +37,6 @@ void main() {
     );
   });
 
-  group('canSelectAll', () {
-    Widget createEditableText({
-      required Key key,
-      String? text,
-      TextSelection? selection,
-    }) {
-      final TextEditingController controller = TextEditingController(text: text)
-        ..selection = selection ?? const TextSelection.collapsed(offset: -1);
-      addTearDown(controller.dispose);
-      final FocusNode focusNode = FocusNode();
-      addTearDown(focusNode.dispose);
-
-      return MaterialApp(
-        home: EditableText(
-          key: key,
-          controller: controller,
-          focusNode: focusNode,
-          style: const TextStyle(),
-          cursorColor: Colors.black,
-          backgroundCursorColor: Colors.black,
-        ),
-      );
-    }
-
-    testWidgets('should return false when there is no text', (WidgetTester tester) async {
-      final GlobalKey<EditableTextState> key = GlobalKey();
-      await tester.pumpWidget(createEditableText(key: key));
-      expect(materialTextSelectionControls.canSelectAll(key.currentState!), false);
-    });
-
-    testWidgets('should return true when there is text and collapsed selection', (WidgetTester tester) async {
-      final GlobalKey<EditableTextState> key = GlobalKey();
-      await tester.pumpWidget(createEditableText(
-        key: key,
-        text: '123',
-      ));
-      expect(materialTextSelectionControls.canSelectAll(key.currentState!), true);
-    });
-
-    testWidgets('should return true when there is text and partial uncollapsed selection', (WidgetTester tester) async {
-      final GlobalKey<EditableTextState> key = GlobalKey();
-      await tester.pumpWidget(createEditableText(
-        key: key,
-        text: '123',
-        selection: const TextSelection(baseOffset: 1, extentOffset: 2),
-      ));
-      expect(materialTextSelectionControls.canSelectAll(key.currentState!), true);
-    });
-
-    testWidgets('should return false when there is text and full selection', (WidgetTester tester) async {
-      final GlobalKey<EditableTextState> key = GlobalKey();
-      await tester.pumpWidget(createEditableText(
-        key: key,
-        text: '123',
-        selection: const TextSelection(baseOffset: 0, extentOffset: 3),
-      ));
-      expect(materialTextSelectionControls.canSelectAll(key.currentState!), false);
-    });
-  });
-
   group('Text selection menu overflow (Android)', () {
     testWidgets('All menu items show when they fit.', (WidgetTester tester) async {
       final TextEditingController controller = TextEditingController(text: 'abc def ghi');
