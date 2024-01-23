b2465340881fd07cfa895e99a926174174c9e144
gaaclarke <30870216+gaaclarke@users.noreply.github.com>
2024-01-23 10:16:55 -0800

    [Impeller] turned on validation errors for one golden image test (flutter/engine#49955)
    issue https://github.com/flutter/flutter/issues/142017
    
    This just turns on the validations for one test since not everything passes yet with them turned on across the board.
    
    [C++, Objective-C, Java style guides]: https://github.com/flutter/engine/blob/main/CONTRIBUTING.md#style

--- engine/src/flutter/impeller/golden_tests/vulkan_screenshotter.h
+++ engine/src/flutter/impeller/golden_tests/vulkan_screenshotter.h
@@ -18,7 +18,7 @@ namespace testing {
 /// playground backend.
 class VulkanScreenshotter : public Screenshotter {
  public:
-  VulkanScreenshotter();
+  explicit VulkanScreenshotter(bool enable_validations);
 
   std::unique_ptr<Screenshot> MakeScreenshot(
       AiksContext& aiks_context,
