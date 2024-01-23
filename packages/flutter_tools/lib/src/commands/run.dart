cfaa4c21b947888782e9709c1c6b34f66fe47b91
Louise Hsu <louisehsu@google.com>
2024-01-23 11:10:39 -0800

    oopsies

--- packages/flutter_tools/lib/src/commands/run.dart
+++ packages/flutter_tools/lib/src/commands/run.dart
@@ -593,7 +593,7 @@ class RunCommand extends RunCommandBase {
       throwToolExit(null);
     }
 
-    if (devices!.length == 1 && devices!.first is! MacOSDesignedForIPadDevice) {
+    if (devices!.length == 1 && devices!.first is MacOSDesignedForIPadDevice) {
       throwToolExit('Mac Designed for iPad is currently not supported for flutter run -d.');
     }
 
