b21165369f7462a6acf291493c870d7cec757163
Qun Cheng <chengqunq@gmail.com>
2024-01-23 13:58:56 -0800

    ListView.builder version

--- examples/api/lib/material/search_anchor/carousel_example.dart
+++ examples/api/lib/material/search_anchor/carousel_example.dart
@@ -2,9 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'dart:math';
-import 'dart:ui';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
@@ -47,139 +44,177 @@ class _CarouselExampleState extends State<CarouselExample> {
   int nextItem = 0;
   bool? isScrolling;
   ScrollDirection? scrollDirection;
-  late double screenWidth;
-  late PageController pageController;
+  late ScrollController scrollController;
 
   @override
   void initState() {
     super.initState();
+    // scrollOffset = scrollController.offset;
+    scrollController = ScrollController(
+        onAttach: (ScrollPosition position) {
+          position.isScrollingNotifier.addListener(() {
+            if (isScrolling != scrollController.position.isScrollingNotifier.value) {
+              setState(() {
+                if (scrollController.position.userScrollDirection == ScrollDirection.idle) {
+
+                }
+                scrollDirection = scrollController.position.userScrollDirection;
+                isScrolling = scrollController.position.isScrollingNotifier.value;
+              });
+            }
+          });
+          // scrollDirection = position.userScrollDirection;
+        }
+    );
     itemKeys = List<GlobalKey>.generate(6, (int index) => GlobalKey());
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      pageController.addListener(() { setState(() { }); });
+    scrollController.addListener(() {
+      final double delta = (scrollController.offset - scrollOffset).abs();
+      nextItem = (delta / maxItemWidth).ceil();
     });
+    // WidgetsBinding.instance.addPostFrameCallback((_) {
+    //   scrollController.addListener(swipeToRight);
+    //   scrollOffset = scrollController.offset;
+    // });
   }
 
-  @override
-  void didChangeDependencies() {
-    screenWidth = MediaQuery.of(context).size.width;
-    // Min # of items: 3
-    final double minItems = max(3, screenWidth / maxItemWidth);
-    pageController = PageController(
-      viewportFraction: 1 / (screenWidth / maxItemWidth),
-    );
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      pageController.addListener(() { setState(() { }); });
-    });
+  // bool swipeToRight() {
+  //   // setState(() {
+  //   //
+  //   // });
+  //   double currentOffset = scrollController.offset;
+  //   double delta = currentOffset - scrollOffset;
+  //   ScrollDirection direction = scrollController.position.userScrollDirection;
+  //   if (direction == ScrollDirection.forward) {
+  //     // scrollController.animateTo(scrollOffset, duration: Duration(milliseconds: 100), curve: Curves.linear);
+  //     // scrollOffset += 200.0;
+  //     return true;
+  //   }
+  //   if (direction == ScrollDirection.reverse) {
+  //
+  //     return false;
+  //   }
+  //   return false;
+  // }
 
-    super.didChangeDependencies();
-  }
-
-  Rect? getRect(int index) {
-    final BuildContext? keyContext = itemKeys[index].currentContext;
-    if (keyContext != null) {
-      final RenderBox box = keyContext.findRenderObject()! as RenderBox;
-      final Size boxSize = box.size;
-      final Offset boxLocation = box.localToGlobal(Offset.zero);
-      return boxLocation & boxSize;
-    }
-    return null;
-  }
-
-  Widget buildCarouselItem(
-      { required int index, required double itemWidth, AlignmentDirectional? alignment }) {
-    return Align(
-      key: itemKeys[index],
-      alignment: alignment ?? Alignment.center,
-      child: Container(
-        color: Colors.orange,
-        padding: const EdgeInsets.all(4.0),
-        constraints: BoxConstraints.tight(Size(itemWidth, 200.0)),
-        child: Material(
-          clipBehavior: Clip.antiAlias,
-          color: Theme.of(context).colorScheme.surface,
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
-          child: Image(
-            fit: BoxFit.cover,
-            image: images[index],
-          ),
-        ),
-      ),
-    );
-  }
+  // bool showLargeSize(int index) {
+  //   double screenWidth = MediaQuery.of(context).size.width;
+  //   double maxWidthForLargeItems = screenWidth - midItemWidth - minItemWidth;
+  //   final BuildContext? keyContext = itemKeys[index].currentContext;
+  //   if (keyContext != null) {
+  //     final RenderBox searchBarBox = keyContext.findRenderObject()! as RenderBox;
+  //     final Size boxSize = searchBarBox.size;
+  //     final Offset boxLocation = searchBarBox.localToGlobal(Offset.zero);
+  //     // print('-----------------');
+  //     // print('screen width: ${MediaQuery.of(context).size.width}');
+  //     // print('current index: $index');
+  //     // print('Is visible? ${(boxLocation.dx + boxSize.width < maxWidthForLargeItems) && boxLocation.dx >= 0}');
+  //     // print(boxSize);
+  //     // print(boxLocation);
+  //     // print('-----------------');
+  //     return (boxLocation.dx + boxSize.width < maxWidthForLargeItems) && boxLocation.dx >= 0;
+  //   }
+  //   return true;
+  // }
 
   @override
   Widget build(BuildContext context) {
+    // print(isScrolling);
+    // int largeItemsCount = (maxWidthForLargeItems / maxItemWidth).floor();
+    // print(largeItemsCount);
+    // if (isScrolling ?? false) {
+    //   // scrollController.animateTo(scrollOffset + nextItem * 200, duration: Duration(milliseconds: 100), curve: Curves.linear);
+    //   nextItem += 1;
+    // }
+    if (scrollDirection == ScrollDirection.reverse) { // To the right
+      scrollController.animateTo(scrollOffset + nextItem * maxItemWidth, duration: Duration(milliseconds: 100), curve: Curves.linear);
+      scrollOffset = scrollOffset + nextItem * maxItemWidth;
+    }
+    if (scrollDirection == ScrollDirection.forward && scrollOffset > 0) { // To the left
+      scrollController.animateTo(scrollOffset - nextItem * maxItemWidth, duration: Duration(milliseconds: 100), curve: Curves.linear);
+      scrollOffset = scrollOffset - nextItem * maxItemWidth;
+    }
+
+    // if (scrollTriggered == false) {
+    //   scrollController.animateTo(scrollOffset - nextItem * 200, duration: Duration(milliseconds: 100), curve: Curves.linear);
+    // }
+    // scrollTriggered = null;
+
+    // List<Widget> carouselItems = List<Widget>.generate(6, (int index) {
+    //   double itemWidth;
+    //   if (index < largeItemsCount) {
+    //     itemWidth = maxItemWidth;
+    //   } else {
+    //     itemWidth = midItemWidth;
+    //   }
+    //   return Center(
+    //     key: itemKeys[index],
+    //     child: AnimatedContainer(
+    //       color: Colors.orange,
+    //       padding: const EdgeInsets.all(4.0),
+    //       constraints: BoxConstraints.tight(Size(itemWidth, 200.0)),
+    //       duration: const Duration(seconds: 1),
+    //       child: Material(
+    //         clipBehavior: Clip.antiAlias,
+    //         color: Theme.of(context).colorScheme.surface,
+    //         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+    //         child: Image(
+    //           fit: BoxFit.cover,
+    //           image: images[index],
+    //         ),
+    //       ),
+    //     ),
+    //   );
+    // });
 
     return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: PageView.builder(
-            padEnds: false,
-            controller: pageController,
-            itemCount: images.length,
-            itemBuilder: (BuildContext context, int index) {
-              final Rect? boxRect = getRect(index);
-              if (boxRect != null && boxRect.left < 0) {
-                print('first item: ${boxRect.left}');
-                final double firstItemWidth = clampDouble(boxRect.right, minItemWidth, maxItemWidth);
-                return CarouselItem(
-                  itemKey: itemKeys[index],
-                  itemWidth: firstItemWidth,
-                  image: images[index],
-                  alignment: AlignmentDirectional.centerEnd,
+          body: Center(
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              controller: scrollController,
+              itemCount: images.length,
+              itemBuilder: (BuildContext context, int index) {
+                // bool isLarge = index == 0 || showLargeSize(index);
+                // print('//////////// SHOW LARGE SIZE? $isLarge');
+                // bool? isMid = index > 0 && !isLarge && showLargeSize(index - 1);
+                // double itemWidth = 200.0;
+                // if (isLarge) {
+                //   itemWidth = maxItemWidth;
+                // } else if (isMid) {
+                //   itemWidth = midItemWidth;
+                // } else {
+                //   itemWidth = minItemWidth;
+                // }
+                return Center(
+                  key: itemKeys[index],
+                  child: AnimatedContainer(
+                    color: Colors.orange,
+                    padding: const EdgeInsets.all(4.0),
+                    constraints: BoxConstraints.tight(Size(maxItemWidth, 200.0)),
+                    duration: const Duration(seconds: 1),
+                    child: Material(
+                      clipBehavior: Clip.antiAlias,
+                      color: Theme.of(context).colorScheme.surface,
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+                      child: Image(
+                        fit: BoxFit.cover,
+                        image: images[index],
+                      ),
+                    ),
+                  ),
                 );
-              }
-
-              // current index is second last item
-              // double bufferForNext = 0;
-              // if (index + 1 < images.length) {
-              //   final Rect? lastBoxRect = getRect(index + 1);
-              //   if (lastBoxRect != null && screenWidth - lastBoxRect.left < minItemWidth) {
-              //     print('$index th box');
-              //     bufferForNext = minItemWidth - screenWidth + lastBoxRect.left;
-              //     double secondLastItemWidth = clampDouble(maxItemWidth - bufferForNext, midItemWidth, maxItemWidth);
-              //     print('$secondLastItemWidth ${getRect(index)?.right} ${getRect(index + 1)?.left}');
-              //     return CarouselItem(
-              //       itemKey: itemKeys[index],
-              //       itemWidth: secondLastItemWidth,
-              //       image: images[index],
-              //       alignment: AlignmentDirectional.centerStart
-              //     );
-              //   }
-              // }
-
-              double lastItemWidth = minItemWidth;
-              if (boxRect != null && boxRect.right >= screenWidth) {
-                // print('last visible index: $index $boxRect');
-                lastItemWidth = clampDouble(screenWidth - boxRect.left, minItemWidth, maxItemWidth);
-                return CarouselItem(
-                  itemKey: itemKeys[index],
-                  itemWidth: lastItemWidth,
-                  image: images[index],
-                  alignment: AlignmentDirectional.centerStart,
-                );
-              }
-              return CarouselItem(
-                itemKey: itemKeys[index],
-                itemWidth: maxItemWidth,
-                image: images[index],
-                alignment: AlignmentDirectional.centerStart,
-              );
-            },
-          ),
-        )
+              },
+            ),
+          )
       ),
     );
   }
 }
 
 class CarouselItem extends StatefulWidget {
-  const CarouselItem({super.key, required this.itemKey, required this.itemWidth, required this.image, this.alignment });
+  const CarouselItem({super.key, required this.child});
 
-  final GlobalKey itemKey;
-  final double itemWidth;
-  final ImageProvider image;
-  final AlignmentDirectional? alignment;
+  final Widget child;
 
   @override
   State<CarouselItem> createState() => _CarouselItemState();
@@ -188,23 +223,12 @@ class CarouselItem extends StatefulWidget {
 class _CarouselItemState extends State<CarouselItem> {
   @override
   Widget build(BuildContext context) {
-    return Align(
-      key: widget.itemKey,
-      alignment: widget.alignment ?? Alignment.center,
-      child: Container(
-        // duration: const Duration(milliseconds: 50),
-        color: Colors.orange,
-        padding: const EdgeInsets.all(4.0),
-        constraints: BoxConstraints.tight(Size(widget.itemWidth, 200.0)),
-        child: Material(
-          clipBehavior: Clip.antiAlias,
-          color: Theme.of(context).colorScheme.surface,
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
-          child: Image(
-            fit: BoxFit.cover,
-            image: widget.image,
-          ),
-        ),
+    return Container(
+      alignment: Alignment.center,
+      child: Material(
+        color: Theme.of(context).colorScheme.surface,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+        child: widget.child,
       ),
     );
   }
