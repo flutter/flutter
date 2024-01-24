af711da3ae63b8e0bd85155445b2483bf4f65726
Mitchell Goodwin <mitchgoodwin@google.com>
2024-01-24 10:48:43 -0800

    Clean up imports

--- packages/flutter/lib/src/widgets/widget_state_mixin.dart
+++ packages/flutter/lib/src/widgets/widget_state_mixin.dart
@@ -5,8 +5,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
 
-import 'widget_state.dart';
-
 /// Mixin for [State] classes that require knowledge of changing [WidgetState]
 /// values for their child widgets.
 ///
