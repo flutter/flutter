eb648460e785ad91f003c114795d54217bb8c8ef
Maximilian Fischer <github@maaeps.de>
2024-01-25 22:53:38 +0100

    Improve Wrap docs by adding an example

--- packages/flutter/lib/src/widgets/basic.dart
+++ packages/flutter/lib/src/widgets/basic.dart
@@ -5303,10 +5303,6 @@ class Expanded extends Flexible {
 /// space in between. If there is not enough space to fit the child, [Wrap]
 /// creates a new _run_ adjacent to the existing children in the cross axis.
 ///
-/// To cause a child to expand to fill the remaining space in the current run,
-/// wrap the child in a [Wrapped] widget and size it according to the give
-/// constraints.
-///
 /// After all the children have been allocated to runs, the children within the
 /// runs are positioned according to the [alignment] in the main axis and
 /// according to the [crossAxisAlignment] in the cross axis.
@@ -5345,6 +5341,13 @@ class Expanded extends Flexible {
 ///   ],
 /// )
 /// ```
+/// {@end-tool}
+///
+/// {@tool dartpad}
+/// This example shows a typical application of the [Wrap] widget. With user
+/// inputs and a [TextField] that prefers to be in the current run.
+///
+/// ** See code in examples/api/lib/widgets/wrap/wrap.0.dart
 /// {@end-tool}
 ///
 /// See also:
