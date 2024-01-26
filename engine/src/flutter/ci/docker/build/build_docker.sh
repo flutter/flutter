fe3389e3df72fb4057f5ac031cc1a19d2ff8d563
Matan Lurey <matanlurey@users.noreply.github.com>
2024-01-26 15:36:04 -0800

    Delete `ci/docker`, assuming it is not used. (flutter/engine#50103)
    I tried grepping for `docker` and couldn't find anything relevant:
    
    ![Screenshot 2024-01-26 at 2 52 59â¯PM](https://github.com/flutter/engine/assets/168174/be75b61f-3c27-4475-8481-e096dff4197a)
    
    (Maybe this is "secretly" used by another repo, but there is no way for me to tell that)

--- engine/src/flutter/ci/docker/build/build_docker.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# pull to make sure we are not rebuilding for nothing
-docker pull gcr.io/flutter-cirrus/build-engine-image:latest
-
-docker build --tag gcr.io/flutter-cirrus/build-engine-image:latest .
-
--- engine/src/flutter/ci/docker/build/push_docker.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-docker push gcr.io/flutter-cirrus/build-engine-image:latest
-
