de748872cc489446e6bd2fc6a29e7dccb6885edc
camsim99 <camillesimon90@gmail.com>
2024-01-29 15:30:50 -0800

    delete code to revert

--- packages/flutter_tools/gradle/src/main/groovy/flutter.groovy
+++ packages/flutter_tools/gradle/src/main/groovy/flutter.groovy
@@ -222,9 +222,6 @@ class FlutterPlugin implements Plugin<Project> {
             }
         }
 
-        // Load shared gradle functions
-        project.apply from: Paths.get(flutterRoot.absolutePath, "packages", "flutter_tools", "gradle", "src", "main", "groovy", "native_plugin_loader.groovy")
-
         project.extensions.create("flutter", FlutterExtension)
         this.addFlutterTasks(project)
 
