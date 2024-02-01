8e494b0e71b23eeee0d96aa8c91806bba727b721
Polina Cherkasova <polina.c@live.com>
2024-01-31 20:22:07 -0800

    -

--- packages/flutter/test/animation/live_binding_test.dart
+++ packages/flutter/test/animation/live_binding_test.dart
@@ -78,10 +78,7 @@ void main() {
     // Currently skipped due to daily flake: https://github.com/flutter/flutter/issues/87588
   }, skip: true); // Typically skip: isBrowser https://github.com/flutter/flutter/issues/42767
 
-  testWidgets('Should show event indicator for pointer events with setSurfaceSize',
-  // TODO(polina-c): clean up leaks, https://github.com/flutter/flutter/issues/134787 [leaks-to-clean]
-  experimentalLeakTesting: LeakTesting.settings.withIgnoredAll(),
-  (WidgetTester tester) async {
+  testWidgets('Should show event indicator for pointer events with setSurfaceSize', (WidgetTester tester) async {
     final AnimationSheetBuilder animationSheet = AnimationSheetBuilder(frameSize: const Size(200, 200), allLayers: true);
     addTearDown(animationSheet.dispose);
     final List<Offset> taps = <Offset>[];
