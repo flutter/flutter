9eac3e784feeab50bafabecb892997693ff45f7b
Daco Harkes <dacoharkes@google.com>
2024-02-05 12:01:48 +0100

    Undo move for build_system/targets

--- packages/flutter_tools/test/general.shard/isolated/build_system/targets/native_assets_test.dart
+++ packages/flutter_tools/test/general.shard/isolated/build_system/targets/native_assets_test.dart
@@ -10,8 +10,8 @@ import 'package:flutter_tools/src/base/logger.dart';
 import 'package:flutter_tools/src/build_info.dart';
 import 'package:flutter_tools/src/build_system/build_system.dart';
 import 'package:flutter_tools/src/build_system/exceptions.dart';
+import 'package:flutter_tools/src/build_system/targets/native_assets.dart';
 import 'package:flutter_tools/src/features.dart';
-import 'package:flutter_tools/src/isolated/native_assets/build_system/targets/native_assets.dart';
 import 'package:flutter_tools/src/isolated/native_assets/native_assets.dart';
 import 'package:native_assets_cli/native_assets_cli_internal.dart'
     as native_assets_cli;
