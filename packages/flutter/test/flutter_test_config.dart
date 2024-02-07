3d126e72685aeaec46715e9cbdef894709ed9645
Polina Cherkasova <polina.c@live.com>
2024-02-07 11:13:18 -0800

    Update flutter_test_config.dart

--- packages/flutter/test/flutter_test_config.dart
+++ packages/flutter/test/flutter_test_config.dart
@@ -13,22 +13,6 @@ import 'package:leak_tracker_flutter_testing/leak_tracker_flutter_testing.dart';
 import '_goldens_io.dart' if (dart.library.html) '_goldens_web.dart'
     as flutter_goldens;
 
-/// If true, leak tracking is enabled for all `testWidgets`.
-///
-/// By default it is false.
-/// To enable the leak tracking, either pass the compilation flag
-/// `--dart-define LEAK_TRACKING=true` or invoke `export LEAK_TRACKING=true`.
-///
-/// See documentation for [testWidgets] on how to except individual tests.
-bool _isLeakTrackingEnabled() {
-  if (kIsWeb) {
-    return false;
-  }
-  // The values can be different, one is compile time, another is run time.
-  return const bool.fromEnvironment('LEAK_TRACKING') ||
-      (bool.tryParse(Platform.environment['LEAK_TRACKING'] ?? '') ?? false);
-}
-
 /// Test configuration for each test library in this directory.
 ///
 /// See https://api.flutter.dev/flutter/flutter_test/flutter_test-library.html.
@@ -41,16 +25,14 @@ Future<void> testExecutable(FutureOr<void> Function() testMain) {
   // receive the event.
   WidgetController.hitTestWarningShouldBeFatal = true;
 
-  if (_isLeakTrackingEnabled()) {
-    LeakTesting.enable();
+  LeakTesting.enable();
 
-    LeakTracking.warnForUnsupportedPlatforms = false;
+  LeakTracking.warnForUnsupportedPlatforms = false;
 
-    LeakTesting.settings = LeakTesting.settings.withIgnored(
-      createdByTestHelpers: true,
-      allNotGCed: true,
-    );
-  }
+  LeakTesting.settings = LeakTesting.settings.withIgnored(
+    createdByTestHelpers: true,
+    allNotGCed: true,
+  );
 
   // Enable golden file testing using Skia Gold.
   return flutter_goldens.testExecutable(testMain);
