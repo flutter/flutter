45137ea6ba29f7bb9f7d653ee3efeee432128df3
Michael Goderbauer <goderbauer@google.com>
2024-02-07 15:19:19 -0800

    Report display sizes in physical pixels on MacOS (#50221)
    Fixes https://github.com/flutter/flutter/issues/142629
    
    `Display.size`'s [documentation](https://main-api.flutter.dev/flutter/dart-ui/Display/size.html) says (emphasize mine):
    
    > The **physical** size of this display.
    
    But we have actually been reporting the size in logical pixels - up until now!

--- shell/platform/darwin/macos/framework/Source/FlutterEngine_Internal.h
+++ shell/platform/darwin/macos/framework/Source/FlutterEngine_Internal.h
@@ -215,6 +215,10 @@ typedef NS_ENUM(NSInteger, FlutterAppExitResponse) {
 - (void)announceAccessibilityMessage:(NSString*)message
                         withPriority:(NSAccessibilityPriorityLevel)priority;
 
+/**
+ * Returns an array of screen objects representing all of the screens available on the system.
+ */
+- (NSArray<NSScreen*>*)screens;
 @end
 
 @interface FlutterEngine (Tests)
