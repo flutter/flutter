4d44e5e705bc7711e5d46431af41354415fa73be
Tirth <pateltirth454@gmail.com>
2024-02-07 16:29:31 +0530

    Update packages/flutter/lib/src/material/button_style_button.dart
    Co-authored-by: Taha Tesser <tessertaha@gmail.com>

--- packages/flutter/lib/src/material/button_style_button.dart
+++ packages/flutter/lib/src/material/button_style_button.dart
@@ -36,7 +36,8 @@ import 'theme_data.dart';
 /// Defaults to [IconAlignment.start].
 ///
 /// {@tool dartpad}
-/// This sample demonstrates the usage of iconAlignment.
+/// This sample demonstrates how to use `iconAlignment` to align the button icon to the start
+// or the end of the button.
 ///
 /// ** See code in examples/api/lib/material/button_style_button/button_style_button.icon_alignment.0.dart **
 /// {@end-tool}
