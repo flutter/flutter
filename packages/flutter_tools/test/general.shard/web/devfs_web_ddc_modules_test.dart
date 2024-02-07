854817d0e894a77872c9ba4241afb581bac18b0b
MarkZ <markzipan@google.com>
2024-02-07 11:51:27 -0800

    removing unused import

--- packages/flutter_tools/test/general.shard/web/devfs_web_ddc_modules_test.dart
+++ packages/flutter_tools/test/general.shard/web/devfs_web_ddc_modules_test.dart
@@ -16,7 +16,6 @@ import 'package:flutter_tools/src/build_system/tools/shader_compiler.dart';
 import 'package:flutter_tools/src/compile.dart';
 import 'package:flutter_tools/src/convert.dart';
 import 'package:flutter_tools/src/devfs.dart';
-import 'package:flutter_tools/src/device.dart';
 import 'package:flutter_tools/src/globals.dart' as globals;
 import 'package:flutter_tools/src/html_utils.dart';
 import 'package:flutter_tools/src/isolated/devfs_web.dart';
