9a19e1114b11e4cb54043856597d1cef4a95b202
Bruno Leroux <leroux_bruno@yahoo.fr>
2024-02-07 10:21:07 +0100

    Remove unused import

--- packages/flutter/test/material/input_decorator_test.dart
+++ packages/flutter/test/material/input_decorator_test.dart
@@ -14,7 +14,6 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:leak_tracker_flutter_testing/leak_tracker_flutter_testing.dart';
 
 Widget buildInputDecoratorM2({
   InputDecoration decoration = const InputDecoration(),
