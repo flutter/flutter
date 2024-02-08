888bc7af1f29d1f7530e9221d31ec7b9921f4a68
Kris Pypen <kris.pypen@gmail.com>
2024-02-08 10:02:17 +0100

    adding a test

--- packages/flutter_test/lib/src/_goldens_io.dart
+++ packages/flutter_test/lib/src/_goldens_io.dart
@@ -175,18 +175,18 @@ mixin LocalComparisonOutput {
 /// Returns a [ComparisonResult] to describe the pixel differential of the
 /// [test] and [master] image bytes provided.
 Future<ComparisonResult> compareLists(List<int>? test, List<int>? master) async {
-  if (listEquals(test, master)) {
-    return ComparisonResult(
-      passed: true,
-      diffPercent: 0.0,
-    );
-  }
-
   if (test == null || master == null || test.isEmpty || master.isEmpty) {
     return ComparisonResult(
       passed: false,
       diffPercent: 1.0,
       error: 'Pixel test failed, null image provided.',
+    );
+  }
+
+  if (listEquals(test, master)) {
+    return ComparisonResult(
+      passed: true,
+      diffPercent: 0.0,
     );
   }
 
