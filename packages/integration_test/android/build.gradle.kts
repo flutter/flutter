4b1e94cc10af8f2cec0ee88b4fd98e9f4bcd0d79
Bartek Pacia <barpac02@gmail.com>
2024-02-08 16:01:54 +0000

    fix integration_test/example app build

--- packages/integration_test/android/build.gradle.kts
+++ packages/integration_test/android/build.gradle.kts
@@ -37,7 +37,7 @@ android {
     }
 
     defaultConfig {
-        minSdkVersion = 21
+        minSdk = 21
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("lib-proguard-rules.txt")
     }
