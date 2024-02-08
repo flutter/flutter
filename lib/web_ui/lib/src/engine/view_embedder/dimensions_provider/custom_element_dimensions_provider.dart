7b579dc30979532b7fe70af24770a05099e98334
David Iglesias <ditman@gmail.com>
2024-02-08 15:35:04 -0800

    Reland "[web] Fix Scene clip bounds. Trigger resize on DPR Change." (#50457)
    ## Changes
    
    1. Reverts commit https://github.com/flutter/engine/commit/9938df6a8bd33cb3a44e87d4297c96488c27739b, to re-land:
       * https://github.com/flutter/engine/pull/50161
    2. Adds a fix to the way the HTML Scene computes its bounds that should be compatible with the current imlementation of Flutter's Golden tests.
    
    ## Issues
    
    * Fixes https://github.com/flutter/flutter/issues/129182
    
    ## Testing
    
    ### Goldens
    
    Verified locally that all the golden tests that caused this to roll back looked acceptable:
    
    * [ð¾ goldens-check.zip](https://github.com/flutter/engine/files/14203071/goldens-check.zip)
    
    ### Test app
    
    * Redeployed at:
      * https://dit-astral-test.web.app/?renderer=html (HTML, full screen)
      * https://dit-astral-test.web.app/?renderer=html&embedded=true (CK, embedded) 
      * https://dit-astral-test.web.app/?renderer=canvaskit (CK, full screen)
      * https://dit-astral-test.web.app/?renderer=canvaskit&embedded=true (CK, embedded) 
    
    ## Next steps
    
    During the investigation of this rollback, we found some opportunities to improve our web tests in the repo. Tracking issue:
    
    * https://github.com/flutter/flutter/issues/143124
    
    [C++, Objective-C, Java style guides]: https://github.com/flutter/engine/blob/main/CONTRIBUTING.md#style

--- lib/web_ui/lib/src/engine/view_embedder/dimensions_provider/custom_element_dimensions_provider.dart
+++ lib/web_ui/lib/src/engine/view_embedder/dimensions_provider/custom_element_dimensions_provider.dart
@@ -4,6 +4,7 @@
 
 import 'dart:async';
 
+import 'package:ui/src/engine/display.dart';
 import 'package:ui/src/engine/dom.dart';
 import 'package:ui/src/engine/window.dart';
 import 'package:ui/ui.dart' as ui show Size;
@@ -12,21 +13,36 @@ import 'dimensions_provider.dart';
 
 /// This class provides observable, real-time dimensions of a host element.
 ///
+/// This class needs a `Stream` of `devicePixelRatio` changes, like the one
+/// provided by [DisplayDprStream], because html resize observers do not report
+/// DPR changes.
+///
 /// All the measurements returned from this class are potentially *expensive*,
 /// and should be cached as needed. Every call to every method on this class
 /// WILL perform actual DOM measurements.
+///
+/// This broadcasts `null` size events, to match the implementation of the
+/// FullPageDimensionsProvider, but it could broadcast the size coming from the
+/// DomResizeObserverEntry. Further changes in the engine are required for this
+/// to be effective.
 class CustomElementDimensionsProvider extends DimensionsProvider {
   /// Creates a [CustomElementDimensionsProvider] from a [_hostElement].
-  CustomElementDimensionsProvider(this._hostElement) {
+  CustomElementDimensionsProvider(this._hostElement, {
+    Stream<double>? onDprChange,
+  }) {
+    // Send a resize event when the page DPR changes.
+    _dprChangeStreamSubscription = onDprChange?.listen((_) {
+      _broadcastSize(null);
+    });
+
     // Hook up a resize observer on the hostElement (if supported!).
     _hostElementResizeObserver = createDomResizeObserver((
       List<DomResizeObserverEntry> entries,
       DomResizeObserver _,
     ) {
-      entries
-          .map((DomResizeObserverEntry entry) =>
-              ui.Size(entry.contentRect.width, entry.contentRect.height))
-          .forEach(_broadcastSize);
+      for (final DomResizeObserverEntry _ in entries) {
+        _broadcastSize(null);
+      }
     });
 
     assert(() {
@@ -45,11 +61,12 @@ class CustomElementDimensionsProvider extends DimensionsProvider {
 
   // Handle resize events
   late DomResizeObserver? _hostElementResizeObserver;
-  final StreamController<ui.Size> _onResizeStreamController =
-      StreamController<ui.Size>.broadcast();
+  late StreamSubscription<double>? _dprChangeStreamSubscription;
+  final StreamController<ui.Size?> _onResizeStreamController =
+      StreamController<ui.Size?>.broadcast();
 
   // Broadcasts the last seen `Size`.
-  void _broadcastSize(ui.Size size) {
+  void _broadcastSize(ui.Size? size) {
     _onResizeStreamController.add(size);
   }
 
@@ -58,16 +75,17 @@ class CustomElementDimensionsProvider extends DimensionsProvider {
     super.close();
     _hostElementResizeObserver?.disconnect();
     // ignore:unawaited_futures
+    _dprChangeStreamSubscription?.cancel();
+    // ignore:unawaited_futures
     _onResizeStreamController.close();
   }
 
   @override
-  Stream<ui.Size> get onResize => _onResizeStreamController.stream;
+  Stream<ui.Size?> get onResize => _onResizeStreamController.stream;
 
   @override
   ui.Size computePhysicalSize() {
-    final double devicePixelRatio = getDevicePixelRatio();
-
+    final double devicePixelRatio = EngineFlutterDisplay.instance.devicePixelRatio;
     return ui.Size(
       _hostElement.clientWidth * devicePixelRatio,
       _hostElement.clientHeight * devicePixelRatio,
