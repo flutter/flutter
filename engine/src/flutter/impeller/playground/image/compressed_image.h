7afad7b0c2017e87fbba829b23328d1e8c2e8e6c
Dan Field <dnfield@google.com>
2024-02-08 16:42:24 -0800

    [Impeller] Move impeller/image to impeller/playground/image (flutter/engine#50480)
    Deletes usage in Scene of this code, which was the only non-test location that used it.
    
    This code imports Skia based code to decompress PNG images, and it's not clear when we'd need it outside of tests at this point.
    
    It is otherwise a pretty thin wrapper around a texture allocation.
    
    Fixes https://github.com/flutter/flutter/issues/143194

--- engine/src/flutter/impeller/playground/image/compressed_image.h
+++ engine/src/flutter/impeller/playground/image/compressed_image.h
@@ -2,15 +2,15 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef FLUTTER_IMPELLER_IMAGE_COMPRESSED_IMAGE_H_
-#define FLUTTER_IMPELLER_IMAGE_COMPRESSED_IMAGE_H_
+#ifndef FLUTTER_IMPELLER_PLAYGROUND_IMAGE_COMPRESSED_IMAGE_H_
+#define FLUTTER_IMPELLER_PLAYGROUND_IMAGE_COMPRESSED_IMAGE_H_
 
 #include <memory>
 
 #include "flutter/fml/macros.h"
 #include "flutter/fml/mapping.h"
 #include "impeller/geometry/size.h"
-#include "impeller/image/decompressed_image.h"
+#include "impeller/playground/image/decompressed_image.h"
 
 namespace impeller {
 
@@ -32,4 +32,4 @@ class CompressedImage {
 
 }  // namespace impeller
 
-#endif  // FLUTTER_IMPELLER_IMAGE_COMPRESSED_IMAGE_H_
+#endif  // FLUTTER_IMPELLER_PLAYGROUND_IMAGE_COMPRESSED_IMAGE_H_
