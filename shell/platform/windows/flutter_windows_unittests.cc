abb51c48e26110060d591ed8341bf8bf7987e7ea
Loïc Sharma <737941+loic-sharma@users.noreply.github.com>
2024-02-13 14:35:42 -0800

    [Windows] Refactor window destroyed app lifecycle event (#50565)
    Improves Windows's lifecycle event for window destruction:
    
    1. Made `FlutterWindowsView` generate the "window hidden" event when the HWND is destroyed instead of the `FlutterWindow`.
        1. Before the window would submit this event using a destroyed view:
            1. The `FlutterWindowsView` destructor runs to completion.
            2. The view owns the window, so the `FlutterWindow` is destroyed
            3. The window's destructor generates a "window hidden" event
            4. The window uses the destroyed view to notify the engine of the event
    2. Adds an app lifecycle integration test to verify the `resumed` and `hidden` events are sent when an app is launched and closed.
    3. Removed the `FlutterWindowTest` type as it was unused
    
    Prepares for https://github.com/flutter/flutter/issues/137267
    
    [C++, Objective-C, Java style guides]: https://github.com/flutter/engine/blob/main/CONTRIBUTING.md#style

--- shell/platform/windows/flutter_windows_unittests.cc
+++ shell/platform/windows/flutter_windows_unittests.cc
@@ -10,13 +10,16 @@
 
 #include "flutter/fml/synchronization/count_down_latch.h"
 #include "flutter/fml/synchronization/waitable_event.h"
+#include "flutter/shell/platform/common/app_lifecycle_state.h"
 #include "flutter/shell/platform/embedder/test_utils/proc_table_replacement.h"
 #include "flutter/shell/platform/windows/egl/manager.h"
 #include "flutter/shell/platform/windows/testing/engine_modifier.h"
 #include "flutter/shell/platform/windows/testing/windows_test.h"
 #include "flutter/shell/platform/windows/testing/windows_test_config_builder.h"
 #include "flutter/shell/platform/windows/testing/windows_test_context.h"
+#include "flutter/shell/platform/windows/windows_lifecycle_manager.h"
 #include "flutter/testing/stream_capture.h"
+#include "gmock/gmock.h"
 #include "gtest/gtest.h"
 #include "third_party/tonic/converter/dart_converter.h"
 
@@ -34,6 +37,14 @@ class HalfBrokenEGLManager : public egl::Manager {
   CreateWindowSurface(HWND hwnd, size_t width, size_t height) override {
     return nullptr;
   }
+};
+
+class MockWindowsLifecycleManager : public WindowsLifecycleManager {
+ public:
+  MockWindowsLifecycleManager(FlutterWindowsEngine* engine)
+      : WindowsLifecycleManager(engine) {}
+
+  MOCK_METHOD(void, SetLifecycleState, (AppLifecycleState), (override));
 };
 
 }  // namespace
@@ -329,6 +340,52 @@ TEST_F(WindowsTest, SurfaceOptional) {
       FlutterDesktopViewControllerCreate(0, 0, engine.release())};
 
   ASSERT_NE(controller, nullptr);
+}
+
+// Verify the app produces the expected lifecycle events.
+TEST_F(WindowsTest, Lifecycle) {
+  auto& context = GetContext();
+  WindowsConfigBuilder builder(context);
+  EnginePtr engine{builder.InitializeEngine()};
+  auto windows_engine = reinterpret_cast<FlutterWindowsEngine*>(engine.get());
+  EngineModifier modifier{windows_engine};
+
+  auto lifecycle_manager =
+      std::make_unique<MockWindowsLifecycleManager>(windows_engine);
+  auto lifecycle_manager_ptr = lifecycle_manager.get();
+  modifier.SetLifecycleManager(std::move(lifecycle_manager));
+
+  EXPECT_CALL(*lifecycle_manager_ptr,
+              SetLifecycleState(AppLifecycleState::kResumed))
+      .WillOnce([lifecycle_manager_ptr](AppLifecycleState state) {
+        lifecycle_manager_ptr->WindowsLifecycleManager::SetLifecycleState(
+            state);
+      });
+
+  EXPECT_CALL(*lifecycle_manager_ptr,
+              SetLifecycleState(AppLifecycleState::kHidden))
+      .WillOnce([lifecycle_manager_ptr](AppLifecycleState state) {
+        lifecycle_manager_ptr->WindowsLifecycleManager::SetLifecycleState(
+            state);
+      });
+
+  // Create a controller. This launches the engine and sets the app lifecycle
+  // to the "resumed" state.
+  ViewControllerPtr controller{
+      FlutterDesktopViewControllerCreate(0, 0, engine.release())};
+
+  FlutterDesktopViewRef view =
+      FlutterDesktopViewControllerGetView(controller.get());
+  ASSERT_NE(view, nullptr);
+
+  HWND hwnd = FlutterDesktopViewGetHWND(view);
+  ASSERT_NE(hwnd, nullptr);
+
+  // Give the window a non-zero size to show it. This does not change the app
+  // lifecycle directly. However, destroying the view will now result in a
+  // "hidden" app lifecycle event.
+  ::MoveWindow(hwnd, /* X */ 0, /* Y */ 0, /* nWidth*/ 100, /* nHeight*/ 100,
+               /* bRepaint*/ false);
 }
 
 }  // namespace testing
--- shell/platform/windows/flutter_windows_view.cc
+++ shell/platform/windows/flutter_windows_view.cc
@@ -97,9 +97,13 @@ FlutterWindowsView::FlutterWindowsView(
 }
 
 FlutterWindowsView::~FlutterWindowsView() {
-  // The engine renders into the view's surface. The engine must be
-  // shutdown before the view's resources can be destroyed.
   if (engine_) {
+    // The view owns the child window.
+    // Notify the engine the view's child window will no longer be visible.
+    engine_->OnWindowStateEvent(GetWindowHandle(), WindowStateEvent::kHide);
+
+    // The engine renders into the view's surface. The engine must be
+    // shutdown before the view's resources can be destroyed.
     engine_->Stop();
   }
 
