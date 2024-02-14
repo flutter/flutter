6108bbdf9677c3ab469fd6af4570a42920309bbe
Daco Harkes <dacoharkes@google.com>
2024-02-14 19:13:44 +0100

    fix formatting

--- packages/flutter_tools/lib/src/isolated/native_assets/ios/native_assets.dart
+++ packages/flutter_tools/lib/src/isolated/native_assets/ios/native_assets.dart
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:native_assets_builder/native_assets_builder.dart' 
+import 'package:native_assets_builder/native_assets_builder.dart'
     hide NativeAssetsBuildRunner;
 import 'package:native_assets_cli/native_assets_cli_internal.dart'
     hide BuildMode;
