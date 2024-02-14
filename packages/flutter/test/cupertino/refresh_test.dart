d15a6f4b9980711ebfec62e1697ef6f8a517ce7a
Polina Cherkasova <polina.c@live.com>
2024-02-14 15:25:02 -0800

    Update refresh_test.dart

--- packages/flutter/test/cupertino/refresh_test.dart
+++ packages/flutter/test/cupertino/refresh_test.dart
@@ -10,7 +10,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:leak_tracker_flutter_testing/leak_tracker_flutter_testing.dart';
 
 void main() {
   late FakeBuilder mockHelper;
