a6101bd6757dee3f6b4d7ef02027fb8b716e68ea
Polina Cherkasova <polina.c@live.com>
2024-02-15 15:15:52 -0800

    Update flutter_test_config.dart

--- packages/flutter/test/flutter_test_config.dart
+++ packages/flutter/test/flutter_test_config.dart
@@ -53,7 +53,6 @@ Future<void> testExecutable(FutureOr<void> Function() testMain) {
     );
   }
 
-  // TODO(polina-c): convert to `tearDown` once the fix for https://github.com/flutter/flutter/issues/143522 [leaks-to-clean]
   tearDown(() => disposeFlutterSingletons());
 
   // Enable golden file testing using Skia Gold.
