c6e5a4cd96df2cca19298684de3fc63e4eb509b4
Polina Cherkasova <polina.c@live.com>
2024-02-14 20:08:31 -0800

    Update live_binding_test.dart

--- packages/flutter/test/animation/live_binding_test.dart
+++ packages/flutter/test/animation/live_binding_test.dart
@@ -9,7 +9,6 @@ library;
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:leak_tracker_flutter_testing/leak_tracker_flutter_testing.dart';
 
 void main() {
   /*
