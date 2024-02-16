9d97bc4e203e577d87553ca76d3cedfcfc64a371
Tirth <pateltirth454@gmail.com>
2024-02-17 02:20:07 +0530

    Added Missing Field Name in Doc Comment in SnackBarThemeData (#143588)
    Added Missing Field Name in Doc Comment in SnackBarThemeData.

--- packages/flutter/lib/src/material/snack_bar_theme.dart
+++ packages/flutter/lib/src/material/snack_bar_theme.dart
@@ -154,7 +154,7 @@ class SnackBarThemeData with Diagnosticable {
   /// If null, [SnackBarAction] falls back to [Colors.transparent].
   final Color? actionBackgroundColor;
 
-  /// Overrides default value for [SnackBarAction.].
+  /// Overrides default value for [SnackBarAction.disabledBackgroundColor].
   ///
   /// If null, [SnackBarAction] falls back to [Colors.transparent].
   final Color? disabledActionBackgroundColor;
