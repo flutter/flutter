28d3d09b878e8f526016ca922ba03fc27e07af1a
Michael Goderbauer <goderbauer@google.com>
2024-02-16 10:25:29 -0800

    Fix implementation imports outside of lib

--- packages/flutter_tools/test/general.shard/resident_runner_test.dart
+++ packages/flutter_tools/test/general.shard/resident_runner_test.dart
@@ -30,7 +30,7 @@ import 'package:flutter_tools/src/run_cold.dart';
 import 'package:flutter_tools/src/run_hot.dart';
 import 'package:flutter_tools/src/version.dart';
 import 'package:flutter_tools/src/vmservice.dart';
-import 'package:unified_analytics/src/enums.dart';
+import 'package:unified_analytics/src/enums.dart' show DashEvent; // ignore: implementation_imports
 import 'package:unified_analytics/unified_analytics.dart';
 import 'package:vm_service/vm_service.dart' as vm_service;
 
