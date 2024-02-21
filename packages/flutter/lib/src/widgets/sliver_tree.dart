d8314e0160ec564c53aa31ba12f576c2be066098
Kate Lovett <katelovett@google.com>
2024-02-21 10:05:47 -0600

    Switching branches

--- packages/flutter/lib/src/widgets/sliver_tree.dart
+++ packages/flutter/lib/src/widgets/sliver_tree.dart
@@ -2,6 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'dart:math' as math;
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/rendering.dart';
 
 import 'basic.dart';
@@ -862,12 +865,197 @@ class RenderSliverTree extends RenderSliverVariedExtentList {
     }
   }
 
+  int _calculateLeadingGarbage(int firstIndex) {
+    RenderBox? walker = firstChild;
+    int leadingGarbage = 0;
+    while (walker != null && indexOf(walker) < firstIndex) {
+      leadingGarbage += 1;
+      walker = childAfter(walker);
+    }
+    return leadingGarbage;
+  }
+
+  int _calculateTrailingGarbage(int targetLastIndex) {
+    RenderBox? walker = lastChild;
+    int trailingGarbage = 0;
+    while (walker != null && indexOf(walker) > targetLastIndex) {
+      trailingGarbage += 1;
+      walker = childBefore(walker);
+    }
+    return trailingGarbage;
+  }
+
+  BoxConstraints _getChildConstraints(int index) {
+    final double extent = itemExtentBuilder(index, _currentLayoutDimensions);
+    return constraints.asBoxConstraints(
+      minExtent: extent,
+      maxExtent: extent,
+    );
+  }
+
+  _TreeNodeParentData _getParentData(RenderBox child) {
+    return child.parentData! as _TreeNodeParentData;
+  }
+
+
+  late SliverLayoutDimensions _currentLayoutDimensions;
+
   @override
   void performLayout() {
+    final SliverConstraints constraints = this.constraints;
     assert(constraints.axisDirection == AxisDirection.down);
-    // account for animation offset
-    // check RenderSliverList and RenderSliver*Groups for gotchas
-    super.performLayout();
+    childManager.didStartLayout();
+    childManager.setDidUnderflow(false);
+
+    // final double itemFixedExtent = itemExtent ?? 0;
+    final double scrollOffset = constraints.scrollOffset + constraints.cacheOrigin;
+    assert(scrollOffset >= 0.0);
+    final double remainingExtent = constraints.remainingCacheExtent;
+    assert(remainingExtent >= 0.0);
+    final double targetEndScrollOffset = scrollOffset + remainingExtent;
+
+    _currentLayoutDimensions = SliverLayoutDimensions(
+        scrollOffset: constraints.scrollOffset,
+        precedingScrollExtent: constraints.precedingScrollExtent,
+        viewportMainAxisExtent: constraints.viewportMainAxisExtent,
+        crossAxisExtent: constraints.crossAxisExtent
+    );
+
+    final int firstIndex = getMinChildIndexForScrollOffset(scrollOffset, -1);
+    // This is an estimate. Animation offsets may necessitate more children
+    final int? targetLastIndex = targetEndScrollOffset.isFinite ?
+        getMaxChildIndexForScrollOffset(targetEndScrollOffset, -1) : null;
+
+    // TODO(Piinks): Move garbage collection to later when firstChild == null,
+    // after we have accounted for animation offsets
+    if (firstChild != null) {
+      final int leadingGarbage = _calculateLeadingGarbage(firstIndex);
+      final int trailingGarbage = targetLastIndex != null ? _calculateTrailingGarbage(targetLastIndex) : 0;
+      collectGarbage(leadingGarbage, trailingGarbage);
+    } else {
+      collectGarbage(0, 0);
+    }
+
+    if (firstChild == null) {
+      // TODO(Piinks): collect garbage
+      // collectGarbage(0, 0);
+      if (!addInitialChild(index: firstIndex, layoutOffset: indexToLayoutOffset(-1, firstIndex))) {
+        // There are either no children, or we are past the end of all our children.
+        final double max;
+        if (firstIndex <= 0) {
+          max = 0.0;
+        } else {
+          max = computeMaxScrollOffset(constraints, -1);
+        }
+        geometry = SliverGeometry(
+          scrollExtent: max,
+          maxPaintExtent: max,
+        );
+        childManager.didFinishLayout();
+        return;
+      }
+    }
+
+    RenderBox? trailingChildWithLayout;
+
+    // Layout children that come before the pre-existing firstChild.
+    // This happens when, after the last frame, the user scrolled back up,
+    // scrolling the firstChild of the last frame down the screen.
+    for (int index = indexOf(firstChild!) - 1; index >= firstIndex; --index) {
+      final RenderBox? child = insertAndLayoutLeadingChild(_getChildConstraints(index));
+      if (child == null) {
+        // Items before the previous firstChild are no longer present.
+        // Reset the scroll offset to offset all items prior and up to the
+        // missing item. Let parent re-layout everything.
+        geometry = SliverGeometry(scrollOffsetCorrection: indexToLayoutOffset(-1, index));
+        return;
+      }
+      final _TreeNodeParentData childParentData = child.parentData! as _TreeNodeParentData;
+      childParentData.layoutOffset = indexToLayoutOffset(-1, index);
+      assert(childParentData.index == index);
+      trailingChildWithLayout ??= child;
+    }
+
+    // If we have managed to not lay anything out yet. Layout first child.
+    if (trailingChildWithLayout == null) {
+      firstChild!.layout(_getChildConstraints(indexOf(firstChild!)));
+      final _TreeNodeParentData childParentData = firstChild!.parentData! as _TreeNodeParentData;
+      childParentData.layoutOffset = indexToLayoutOffset(-1, firstIndex);
+      trailingChildWithLayout = firstChild;
+    }
+
+    // Lay out the children that follow after now.
+    double estimatedMaxScrollOffset = double.infinity;
+    for (int index = indexOf(trailingChildWithLayout!) + 1; targetLastIndex == null || index <= targetLastIndex; ++index) {
+      RenderBox? child = childAfter(trailingChildWithLayout!);
+      if (child == null || indexOf(child) != index) {
+        child = insertAndLayoutChild(_getChildConstraints(index), after: trailingChildWithLayout);
+        if (child == null) {
+          // We have run out of children.
+          estimatedMaxScrollOffset = indexToLayoutOffset(-1, index);
+          break;
+        }
+      } else {
+        child.layout(_getChildConstraints(index));
+      }
+      trailingChildWithLayout = child;
+      final _TreeNodeParentData childParentData = child.parentData! as _TreeNodeParentData;
+      assert(childParentData.index == index);
+      childParentData.layoutOffset = indexToLayoutOffset(-1, childParentData.index!);
+    }
+
+    final int lastIndex = indexOf(lastChild!);
+    final double leadingScrollOffset = indexToLayoutOffset(-1, firstIndex);
+    final double trailingScrollOffset = indexToLayoutOffset(-1, lastIndex + 1);
+
+    assert(firstIndex == 0 || childScrollOffset(firstChild!)! - scrollOffset <= precisionErrorTolerance);
+    assert(debugAssertChildListIsNonEmptyAndContiguous());
+    assert(indexOf(firstChild!) == firstIndex);
+    assert(targetLastIndex == null || lastIndex <= targetLastIndex);
+
+    estimatedMaxScrollOffset = math.min(
+      estimatedMaxScrollOffset,
+      estimateMaxScrollOffset(
+        constraints,
+        firstIndex: firstIndex,
+        lastIndex: lastIndex,
+        leadingScrollOffset: leadingScrollOffset,
+        trailingScrollOffset: trailingScrollOffset,
+      ),
+    );
+
+    final double paintExtent = calculatePaintOffset(
+      constraints,
+      from: leadingScrollOffset,
+      to: trailingScrollOffset,
+    );
+
+    final double cacheExtent = calculateCacheOffset(
+      constraints,
+      from: leadingScrollOffset,
+      to: trailingScrollOffset,
+    );
+
+    final double targetEndScrollOffsetForPaint = constraints.scrollOffset + constraints.remainingPaintExtent;
+    final int? targetLastIndexForPaint = targetEndScrollOffsetForPaint.isFinite ?
+        getMaxChildIndexForScrollOffset(targetEndScrollOffsetForPaint, -1) : null;
+
+    geometry = SliverGeometry(
+      scrollExtent: estimatedMaxScrollOffset,
+      paintExtent: paintExtent,
+      cacheExtent: cacheExtent,
+      maxPaintExtent: estimatedMaxScrollOffset,
+      // Conservative to avoid flickering away the clip during scroll.
+      hasVisualOverflow: (targetLastIndexForPaint != null && lastIndex >= targetLastIndexForPaint)
+        || constraints.scrollOffset > 0.0,
+    );
+
+    // We may have started the layout while scrolled to the end, which would not
+    // expose a new child.
+    if (estimatedMaxScrollOffset == trailingScrollOffset) {
+      childManager.setDidUnderflow(true);
+    }
+    childManager.didFinishLayout();
   }
 
   @override
