405f6e861c808a0c517f1e3405be794e04fc6b56
Matan Lurey <matanlurey@users.noreply.github.com>
2024-02-21 07:34:13 -0800

    Disallow files additional named `GeneratedPluginRegistrant.java`. (flutter/engine#50796)
    Closes https://github.com/flutter/flutter/issues/143782.
    
    Also introduces a test that the test itself passes.
    
    I considered adding this to `ci/analyze.sh`, but it seems better suited for `ci/format.sh` (which is also on git-hooks). Wdut?

--- engine/src/flutter/ci/format.sh
+++ engine/src/flutter/ci/format.sh
@@ -32,6 +32,29 @@ SRC_DIR="$(cd "$SCRIPT_DIR/../.."; pwd -P)"
 DART_SDK_DIR="${SRC_DIR}/third_party/dart/tools/sdks/dart-sdk"
 DART="${DART_SDK_DIR}/bin/dart"
 
+# Check if a file named **/GeneratedPluginRegistrant.java exists in the project.
+# If it does, fail the build and print a message to the user pointing them to
+# the file and instructing them to remove it.
+#
+# See: https://github.com/flutter/flutter/issues/143782.
+
+# The expected path to the file. Any *other* path is unexpected.
+EXPECTED_PATHS=("./shell/platform/android/test/io/flutter/plugins/GeneratedPluginRegistrant.java")
+
+# Find all files named GeneratedPluginRegistrant.java in the project.
+GENERATED_PLUGIN_REGISTRANT_PATHS=$(find "$SRC_DIR/flutter" -name "GeneratedPluginRegistrant.java")
+
+# Check for GeneratedPluginRegistrant.java in unexpected locations
+for expected_path in "${EXPECTED_PATHS[@]}"; do
+    found_files=$(find . -name "GeneratedPluginRegistrant.java" -not -path "$expected_path")
+
+    for file in $found_files; do
+        echo "Error: Unexpected GeneratedPluginRegistrant.java found: $file"
+        echo "Please remove the unexpected file and see: https://github.com/flutter/flutter/issues/143782"
+        exit 1
+    done
+done
+
 cd "$SCRIPT_DIR"
 "$DART" \
   --disable-dart-dev \
