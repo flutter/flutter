db496611c3dc45957e1b994e3ae89fbd1adb892b
Jonah Williams <jonahwilliams@google.com>
2024-02-21 09:51:56 -0800

    [Impeller] cache onscreen render targets. (#50751)
    Currently we cache render pass objects to offscreen render targets only. Caching onscreen doesn't work as 1) the render targets are stored on the texture_vk object and 2) the texture_vk object is recreated for each frame (see https://github.com/flutter/engine/blob/main/impeller/renderer/backend/vulkan/surface_vk.cc#L14 ).
    
    To make sure the render pass is cached, move the storage to the texture source.
    
    part of https://github.com/flutter/flutter/issues/141750

--- impeller/renderer/backend/vulkan/test/swapchain_unittests.cc
+++ impeller/renderer/backend/vulkan/test/swapchain_unittests.cc
@@ -6,6 +6,7 @@
 #include "gtest/gtest.h"
 #include "impeller/renderer/backend/vulkan/swapchain_vk.h"
 #include "impeller/renderer/backend/vulkan/test/mock_vulkan.h"
+#include "impeller/renderer/backend/vulkan/texture_vk.h"
 #include "vulkan/vulkan_enums.hpp"
 
 namespace impeller {
@@ -50,6 +51,44 @@ TEST(SwapchainTest, RecreateSwapchainWhenSizeChanges) {
   auto image_b = swapchain->AcquireNextDrawable();
   expected_size = ISize{100, 100};
   EXPECT_EQ(image_b->GetSize(), expected_size);
+}
+
+TEST(SwapchainTest, CachesRenderPassOnSwapchainImage) {
+  auto const context = MockVulkanContextBuilder().Build();
+
+  auto surface = CreateSurface(*context);
+  auto swapchain =
+      SwapchainVK::Create(context, std::move(surface), ISize{1, 1});
+
+  EXPECT_TRUE(swapchain->IsValid());
+
+  // We should create 3 swapchain images with the current mock setup. However,
+  // we will only ever return the first image, so the render pass and
+  // framebuffer will be cached after one call to AcquireNextDrawable.
+  auto drawable = swapchain->AcquireNextDrawable();
+  RenderTarget render_target = drawable->GetTargetRenderPassDescriptor();
+
+  auto texture = render_target.GetRenderTargetTexture();
+  auto& texture_vk = TextureVK::Cast(*texture);
+  EXPECT_EQ(texture_vk.GetFramebuffer(), nullptr);
+  EXPECT_EQ(texture_vk.GetRenderPass(), nullptr);
+
+  auto command_buffer = context->CreateCommandBuffer();
+  auto render_pass = command_buffer->CreateRenderPass(render_target);
+
+  render_pass->EncodeCommands();
+
+  EXPECT_NE(texture_vk.GetFramebuffer(), nullptr);
+  EXPECT_NE(texture_vk.GetRenderPass(), nullptr);
+
+  {
+    auto drawable = swapchain->AcquireNextDrawable();
+    auto texture = render_target.GetRenderTargetTexture();
+    auto& texture_vk = TextureVK::Cast(*texture);
+
+    EXPECT_NE(texture_vk.GetFramebuffer(), nullptr);
+    EXPECT_NE(texture_vk.GetRenderPass(), nullptr);
+  }
 }
 
 }  // namespace testing
