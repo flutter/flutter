21bb5f4bdafb8770226230b0548b345d903f728e
Brandon DeRosier <bdero@google.com>
2024-02-20 19:03:27 -0800

    [Flutter GPU] Fix symbol export for windows. (flutter/engine#50755)
    Symbols weren't getting exported on Windows because the `FML_OS_WIN`
    macro wasn't being imported.

--- engine/src/flutter/lib/gpu/export.h
+++ engine/src/flutter/lib/gpu/export.h
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "flutter/fml/build_config.h"
+
 #ifndef FLUTTER_LIB_GPU_EXPORT_H_
 #define FLUTTER_LIB_GPU_EXPORT_H_
 
