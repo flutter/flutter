dcdfb1caaf002765447e93550233c9af3926ff3d
Tong Mu <dkwingsmt@gmail.com>
2024-02-27 11:41:42 -0800

    Doc

--- packages/flutter/test/widgets/binding_live_test.dart
+++ packages/flutter/test/widgets/binding_live_test.dart
@@ -10,27 +10,32 @@ void main() {
   LiveTestWidgetsFlutterBinding();
   testWidgets('ReportTiming callback records the sendFramesToEngine when it was scheduled', (WidgetTester tester) async {
     // This test needs LiveTestWidgetsFlutterBinding for multiple reasons.
-    // First, this was the environment that this bug was discovered.
-    // Second, LiveTestWidgetsFlutterBinding doesn't override
-    // scheduleWarmUpFrame, so that the test can start with no frames rendered
-    // allowing deferFirstFrame to work (see below).
-
-    // At the beginning of a `testWidgets`, `TestWidgetsFlutterBinding` pumps a
-    // fixed dummy widget using `runApp`, then `resetFirstFrameSent`. Although
-    // the dummy widget is rendered, its reportTiming callback from the engine
-    // has not arrived yet.
     //
-    // Note that the `runApp` call also schedules a warm up frame, which would
-    // have directly rendered the frame if the test had used
-    // `AutomatedTestWidgetsFlutterBinding`.
+    // First, this was the environment that this bug was discovered.
+    //
+    // Second, unlike `AutomatedTestWidgetsFlutterBinding`, which overrides
+    // `scheduleWarmUpFrame` to execute the handlers synchronously,
+    // `LiveTestWidgetsFlutterBinding` still calls them asynchronously. This
+    // allows `runApp`, which also schedules a warm-up frame, to bind a widget
+    // without rendering a frame, which is needed to for `deferFirstFrame` to
+    // take effect.
+
+    // Before `testWidgets` executes the test body, it pumps a frame with a
+    // fixed dummy widget, then calls `resetFirstFrameSent`. The pumped frame
+    // schedules a reportTiming call that has yet to arrive.
+    //
+    // This puts the test in an inconsistent state: a reportTiming callback is
+    // supposed to happen only after a frame is rendered, but due to
+    // `resetFirstFrameSent`, the framework thinks no frames have been rendered.
+
     expect(tester.binding.sendFramesToEngine, true);
     // Push the widget with runApp instead of tester.pump, avoiding rendering a
     // frame, which is needed for `deferFirstFrame` later to work.
     runApp(const DummyWidget());
-    // Ensure that no widget tree is built and nothing is rendered.
+    // Verify that no widget tree is built and nothing is rendered.
     expect(find.text('First frame'), findsNothing);
-    // Defer the first frame, making sendFramesToEngine false, and widget tree
-    // will be built but not sent to the engine.
+    // Defer the first frame, making sendFramesToEngine false, so that widget
+    // tree will be built but not sent to the engine.
     tester.binding.deferFirstFrame();
     expect(tester.binding.sendFramesToEngine, false);
     // Wait for the reportTiming callback (which completes the future below) to
