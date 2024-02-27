3b99e7ab98d9018811ae40eb3ed0a622f7750887
Dan Field <dnfield@google.com>
2024-02-26 20:37:23 -0800

    Actually use Impeller in scenario_app tests (flutter/engine#50977)
    Right now, the scenario_app tests that claim to use Impeller are not actually using Impeller. This is for a few reasons:
    
    - The arguments passed via instrumentation do not end up by default on the `Intent` for the `Activity` that is under test.
    - The arguments passed via instrumentation were in the wrong order and not getting sent to instrumentation at all.
    
    This patch updates existing tests to use a new `@Rule` that reads the arguments from the instrumentation's argument `Bundle` and injects them into the `Intent` that we actually pass to the `Activity` under test. It also updates the argument order in the script and adds a verification that Impeller prints that it is being used at least once.

--- engine/src/flutter/testing/scenario_app/android/app/src/androidTest/java/dev/flutter/TestRunner.java
+++ engine/src/flutter/testing/scenario_app/android/app/src/androidTest/java/dev/flutter/TestRunner.java
@@ -8,10 +8,21 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.test.runner.AndroidJUnitRunner;
 import dev.flutter.scenariosui.ScreenshotUtil;
+import io.flutter.FlutterInjector;
 
 public class TestRunner extends AndroidJUnitRunner {
   @Override
   public void onCreate(@Nullable Bundle arguments) {
+    String[] engineArguments = null;
+    if ("true".equals(arguments.getString("enable-impeller"))) {
+      // Set up the global settings object so that Impeller is enabled for all tests.
+      engineArguments = new String[] {"--enable-impeller=true"};
+    }
+    // For consistency, just always initilaize FlutterJNI etc.
+    FlutterInjector.instance().flutterLoader().startInitialization(getTargetContext());
+    FlutterInjector.instance()
+        .flutterLoader()
+        .ensureInitializationComplete(getTargetContext(), engineArguments);
     ScreenshotUtil.onCreate();
     super.onCreate(arguments);
   }
