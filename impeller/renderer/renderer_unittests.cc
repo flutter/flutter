c11cccfc909acaa3bc8e181b05ec7ee53957c5da
Jason Simmons <jason-simmons@users.noreply.github.com>
2024-02-28 12:54:06 -0800

    [Impeller] Mark subpass framebuffer fetch tests as unsupported on GLES (#50982)
    Currently the GLES backend may claim the framebuffer fetch capability but its implementation does not support the multisample subpass input used in this test.  Do not run this on GLES until there is clarification of the level of framebuffer fetch support intended for the GLES backend.

--- impeller/renderer/renderer_unittests.cc
+++ impeller/renderer/renderer_unittests.cc
@@ -1328,6 +1328,13 @@ std::shared_ptr<Pipeline<PipelineDescriptor>> CreateDefaultPipeline(
 }
 
 TEST_P(RendererTest, CanSepiaToneWithSubpasses) {
+  // The GLES framebuffer fetch implementation currently does not support this.
+  // TODO(chinmaygarde): revisit after the GLES framebuffer fetch capabilities
+  // are clarified.
+  if (GetParam() == PlaygroundBackend::kOpenGLES) {
+    GTEST_SKIP_("Not supported on GLES.");
+  }
+
   // Define shader types
   using TextureVS = TextureVertexShader;
   using TextureFS = TextureFragmentShader;
@@ -1416,6 +1423,13 @@ TEST_P(RendererTest, CanSepiaToneWithSubpasses) {
 }
 
 TEST_P(RendererTest, CanSepiaToneThenSwizzleWithSubpasses) {
+  // The GLES framebuffer fetch implementation currently does not support this.
+  // TODO(chinmaygarde): revisit after the GLES framebuffer fetch capabilities
+  // are clarified.
+  if (GetParam() == PlaygroundBackend::kOpenGLES) {
+    GTEST_SKIP_("Not supported on GLES.");
+  }
+
   // Define shader types
   using TextureVS = TextureVertexShader;
   using TextureFS = TextureFragmentShader;
