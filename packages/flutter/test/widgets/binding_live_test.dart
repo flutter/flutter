662f3ef2c532332c215c0f8afbc5fdc0764a0a8a
Tong Mu <dkwingsmt@gmail.com>
2024-02-28 14:18:09 -0800

    Fix compile

--- packages/flutter/test/widgets/binding_live_test.dart
+++ packages/flutter/test/widgets/binding_live_test.dart
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -45,7 +46,7 @@ void main() {
     await tester.pump(const Duration(milliseconds: 1));
     await tester.binding.waitUntilFirstFrameRasterized;
     expect(find.text('First frame'), findsOne);
-  }, skip: kIsWeb, // [intended] Web doesn't use LiveTestWidgetsFlutterBinding
+  }, skip: kIsWeb); // [intended] Web doesn't use LiveTestWidgetsFlutterBinding
 }
 
 class DummyWidget extends StatelessWidget {
