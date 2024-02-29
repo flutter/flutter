7f9366050ed2d4ec1d35a2bb4d95e434bf902a1f
Michael Goderbauer <goderbauer@google.com>
2024-02-29 11:08:25 -0800

    Use dartpad's main channel for master/main docs (#144329)
    Dartpad doesn't have a "master" channel anymore, it got renamed to "main". Sadly, specifying "master" is now falling back to "stable" which breaks some of our examples in the docs that require a more current Flutter version, e.g. https://main-api.flutter.dev/flutter/material/TextButton-class.html

--- dev/tools/test/create_api_docs_test.dart
+++ dev/tools/test/create_api_docs_test.dart
@@ -441,7 +441,7 @@ void main() {
   </code>
 </pre>
 ''');
-            const String queryParams = 'split=1&run=true&sample_id=widgets.Listener.123&sample_channel=master&channel=master';
+            const String queryParams = 'split=1&run=true&sample_id=widgets.Listener.123&channel=main';
             widgetsDir.childFile('Listener-class.html').writeAsStringSync('''
 <iframe class="snippet-dartpad" src="https://dartpad.dev/embed-flutter.html?$queryParams">
 </iframe>
