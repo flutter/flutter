e845098fbe9d8ddedd3bacce4f5d4920b203d396
Jason Simmons <jason-simmons@users.noreply.github.com>
2024-02-29 19:15:16 -0800

    [Impeller] Fix vertex counts in some display list tests (flutter/engine#51111)

--- engine/src/flutter/impeller/display_list/dl_unittests.cc
+++ engine/src/flutter/impeller/display_list/dl_unittests.cc
@@ -1520,8 +1520,9 @@ TEST_P(DisplayListTest, DrawVerticesSolidColorTrianglesWithIndices) {
   std::vector<uint16_t> indices = {0, 1, 2, 0, 2, 3};
 
   auto vertices = flutter::DlVertices::Make(
-      flutter::DlVertexMode::kTriangles, 6, positions.data(),
-      /*texture_coordinates=*/nullptr, /*colors=*/nullptr, 6, indices.data());
+      flutter::DlVertexMode::kTriangles, positions.size(), positions.data(),
+      /*texture_coordinates=*/nullptr, /*colors=*/nullptr, indices.size(),
+      indices.data());
 
   flutter::DisplayListBuilder builder;
   flutter::DlPaint paint;
@@ -1541,8 +1542,9 @@ TEST_P(DisplayListTest, DrawVerticesPremultipliesColors) {
   std::vector<flutter::DlColor> colors = {color, color, color, color};
 
   auto vertices = flutter::DlVertices::Make(
-      flutter::DlVertexMode::kTriangles, 6, positions.data(),
-      /*texture_coordinates=*/nullptr, colors.data(), 6, indices.data());
+      flutter::DlVertexMode::kTriangles, positions.size(), positions.data(),
+      /*texture_coordinates=*/nullptr, colors.data(), indices.size(),
+      indices.data());
 
   flutter::DisplayListBuilder builder;
   flutter::DlPaint paint;
