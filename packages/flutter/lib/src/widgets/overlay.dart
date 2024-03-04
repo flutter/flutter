89b56a2e24235ede123aea6d4a87d33ed6095dc8
LongCatIsLooong <31859944+LongCatIsLooong@users.noreply.github.com>
2024-03-04 13:58:17 -0800

    Remove unnecessary (and the only) `RenderObject.markParentNeedsLayout` override (#144466)
    Nobody calls it except for `markNeedsLayout`, and the render object is a guaranteed relayout boundary.

--- packages/flutter/lib/src/widgets/overlay.dart
+++ packages/flutter/lib/src/widgets/overlay.dart
@@ -2188,8 +2188,8 @@ class _DeferredLayout extends SingleChildRenderObjectWidget {
 //
 // This `RenderObject` must be a child of a `_RenderTheater`. It guarantees that:
 //
-// 1. It's a relayout boundary, and `markParentNeedsLayout` is overridden such
-//    that it never dirties its `_RenderTheater`.
+// 1. It's a relayout boundary, so calling `markNeedsLayout` on it never dirties
+//    its `_RenderTheater`.
 //
 // 2. Its `layout` implementation is overridden such that `performLayout` does
 //    not do anything when its called from `layout`, preventing the parent
@@ -2229,19 +2229,6 @@ final class _RenderDeferredLayoutBox extends RenderProxyBox with _RenderTheaterM
   void redepthChildren() {
     _layoutSurrogate.redepthChild(this);
     super.redepthChildren();
-  }
-
-  bool _callingMarkParentNeedsLayout = false;
-  @override
-  void markParentNeedsLayout() {
-    // No re-entrant calls.
-    if (_callingMarkParentNeedsLayout) {
-      return;
-    }
-    _callingMarkParentNeedsLayout = true;
-    markNeedsLayout();
-    _layoutSurrogate.markNeedsLayout();
-    _callingMarkParentNeedsLayout = false;
   }
 
   @override
