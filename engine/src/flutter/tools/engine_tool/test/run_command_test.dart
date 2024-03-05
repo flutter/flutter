5a25c24c438c650d4a3c092b0760a07e17e49de1
John McCutchan <john@johnmccutchan.com>
2024-03-05 10:49:05 -0800

    Add device selection to `et run` (flutter/engine#51184)
    - `et run` now detects the target device automatically and builds the
    related engine builds.
    - `et run -- -d <device-id>` also works.

--- engine/src/flutter/tools/engine_tool/test/run_command_test.dart
+++ engine/src/flutter/tools/engine_tool/test/run_command_test.dart
@@ -11,6 +11,7 @@ import 'package:engine_repo_tools/engine_repo_tools.dart';
 import 'package:engine_tool/src/commands/command_runner.dart';
 import 'package:engine_tool/src/environment.dart';
 import 'package:engine_tool/src/logger.dart';
+import 'package:engine_tool/src/run_utils.dart';
 import 'package:litetest/litetest.dart';
 import 'package:platform/platform.dart';
 import 'package:process_fakes/process_fakes.dart';
@@ -63,10 +64,16 @@ void main() {
         processRunner: ProcessRunner(
           processManager: FakeProcessManager(onStart: (List<String> command) {
             runHistory.add(command);
-            return FakeProcess();
+            switch (command) {
+              case ['flutter', 'devices', '--machine']:
+                return FakeProcess(stdout: fixtures.attachedDevices());
+              default:
+                return FakeProcess();
+            }
           }, onRun: (List<String> command) {
-            runHistory.add(command);
-            return io.ProcessResult(81, 0, '', '');
+            // Should not be executed.
+            assert(false);
+            return io.ProcessResult(81, 1, '', '');
           }),
         ),
         logger: logger,
@@ -85,8 +92,69 @@ void main() {
     final int result =
         await runner.run(<String>['run', '--', '--weird_argument']);
     expect(result, equals(0));
-    expect(runHistory.length, greaterThanOrEqualTo(5));
-    expect(runHistory[4],
+    expect(runHistory.length, greaterThanOrEqualTo(6));
+    expect(runHistory[5],
         containsStringsInOrder(<String>['flutter', 'run', '--weird_argument']));
+  });
+
+  test('parse devices list', () async {
+    final Logger logger = Logger.test();
+    final (Environment env, _) = linuxEnv(logger);
+    final List<RunTarget> targets =
+        parseDevices(env, fixtures.attachedDevices());
+    expect(targets.length, equals(4));
+    final RunTarget android = targets[0];
+    expect(android.name, contains('gphone64'));
+    expect(android.buildConfigFor('debug'), equals('android_debug_arm64'));
+  });
+
+  test('default device', () async {
+    final Logger logger = Logger.test();
+    final (Environment env, _) = linuxEnv(logger);
+    final List<RunTarget> targets =
+        parseDevices(env, fixtures.attachedDevices());
+    expect(targets.length, equals(4));
+    final RunTarget? defaultTarget = defaultDevice(env, targets);
+    expect(defaultTarget, isNotNull);
+    expect(defaultTarget!.name, contains('gphone64'));
+    expect(
+        defaultTarget.buildConfigFor('debug'), equals('android_debug_arm64'));
+  });
+
+  test('device select', () async {
+    final Logger logger = Logger.test();
+    final (Environment env, _) = linuxEnv(logger);
+    RunTarget target = selectRunTarget(env, fixtures.attachedDevices())!;
+    expect(target.name, contains('gphone64'));
+    target = selectRunTarget(env, fixtures.attachedDevices(), 'mac')!;
+    expect(target.name, contains('macOS'));
+  });
+
+  test('flutter run device select', () async {
+    final Logger logger = Logger.test();
+    final (Environment env, List<List<String>> runHistory) = linuxEnv(logger);
+    final ToolCommandRunner runner = ToolCommandRunner(
+      environment: env,
+      configs: configs,
+    );
+    // Request that the emulator device is used. The emulator is an Android
+    // ARM64 device.
+    final int result =
+        await runner.run(<String>['run', '--', '-d', 'emulator']);
+    expect(result, equals(0));
+    expect(runHistory.length, greaterThanOrEqualTo(6));
+    // Observe that we selected android_debug_arm64 as the target.
+    expect(
+        runHistory[5],
+        containsStringsInOrder(<String>[
+          'flutter',
+          'run',
+          '--local-engine',
+          'android_debug_arm64',
+          '--local-engine-host',
+          'host_debug',
+          '-d',
+          'emulator'
+        ]));
   });
 }
