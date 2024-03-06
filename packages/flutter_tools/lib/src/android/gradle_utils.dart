c940b7fc525a800526fa642e413420e1a92f9fdd
Gray Mackall <mackall@google.com>
2024-03-06 08:59:26 -0800

    don't change the ephemeral .android min, because users wouldn't be able to override that

--- packages/flutter_tools/lib/src/android/gradle_utils.dart
+++ packages/flutter_tools/lib/src/android/gradle_utils.dart
@@ -40,7 +40,7 @@ const String templateKotlinGradlePluginVersion = '1.7.10';
 //
 // Please see the README before changing any of these values.
 const String compileSdkVersion = '34';
-const String minSdkVersion = '28';
+const String minSdkVersion = '21';
 const String targetSdkVersion = '34';
 const String ndkVersion = '23.1.7779620';
 
