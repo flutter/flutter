7541e902f1b4e5a25867768b9f96c726dcce6c65
gaaclarke <30870216+gaaclarke@users.noreply.github.com>
2024-03-08 15:58:03 -0800

    [Impeller] implement mask blur for textures (#51183)
    fixes https://github.com/flutter/flutter/issues/144266
    
    ## Pre-launch Checklist
    
    - [x] I read the [Contributor Guide] and followed the process outlined
    there for submitting PRs.
    - [x] I read the [Tree Hygiene] wiki page, which explains my
    responsibilities.
    - [x] I read and followed the [Flutter Style Guide] and the [C++,
    Objective-C, Java style guides].
    - [x] I listed at least one issue that this PR fixes in the description
    above.
    - [x] I added new tests to check the change I am making or feature I am
    adding, or the PR is [test-exempt]. See [testing the engine] for
    instructions on writing and running engine tests.
    - [x] I updated/added relevant documentation (doc comments with `///`).
    - [x] I signed the [CLA].
    - [x] All existing and new tests are passing.
    
    If you need help, consider asking for advice on the #hackers-new channel
    on [Discord].
    
    <!-- Links -->
    [Contributor Guide]:
    https://github.com/flutter/flutter/wiki/Tree-hygiene#overview
    [Tree Hygiene]: https://github.com/flutter/flutter/wiki/Tree-hygiene
    [test-exempt]:
    https://github.com/flutter/flutter/wiki/Tree-hygiene#tests
    [Flutter Style Guide]:
    https://github.com/flutter/flutter/wiki/Style-guide-for-Flutter-repo
    [C++, Objective-C, Java style guides]:
    https://github.com/flutter/engine/blob/main/CONTRIBUTING.md#style
    [testing the engine]:
    https://github.com/flutter/flutter/wiki/Testing-the-engine
    [CLA]: https://cla.developers.google.com/
    [flutter/tests]: https://github.com/flutter/tests
    [breaking change policy]:
    https://github.com/flutter/flutter/wiki/Tree-hygiene#handling-breaking-changes
    [Discord]: https://github.com/flutter/flutter/wiki/Chat

--- impeller/aiks/paint.cc
+++ impeller/aiks/paint.cc
@@ -9,6 +9,7 @@
 #include "impeller/entity/contents/color_source_contents.h"
 #include "impeller/entity/contents/filters/color_filter_contents.h"
 #include "impeller/entity/contents/filters/filter_contents.h"
+#include "impeller/entity/contents/filters/gaussian_blur_filter_contents.h"
 #include "impeller/entity/contents/solid_color_contents.h"
 #include "impeller/entity/geometry/geometry.h"
 
@@ -119,6 +120,34 @@ std::shared_ptr<Contents> Paint::WithColorFilter(
   }
   return color_filter->WrapWithGPUColorFilter(FilterInput::Make(input),
                                               absorb_opacity);
+}
+
+std::shared_ptr<FilterContents> Paint::MaskBlurDescriptor::CreateMaskBlur(
+    std::shared_ptr<TextureContents> texture_contents) const {
+  Scalar expand_amount = GaussianBlurFilterContents::CalculateBlurRadius(
+      GaussianBlurFilterContents::ScaleSigma(sigma.sigma));
+  texture_contents->SetSourceRect(
+      texture_contents->GetSourceRect().Expand(expand_amount, expand_amount));
+  auto mask = std::make_shared<SolidColorContents>();
+  mask->SetColor(Color::White());
+  std::optional<Rect> coverage = texture_contents->GetCoverage({});
+  std::shared_ptr<Geometry> geometry;
+  if (coverage) {
+    texture_contents->SetDestinationRect(
+        coverage.value().Expand(expand_amount, expand_amount));
+    geometry = Geometry::MakeRect(coverage.value());
+  }
+  mask->SetGeometry(geometry);
+  auto descriptor = texture_contents->GetSamplerDescriptor();
+  texture_contents->SetSamplerDescriptor(descriptor);
+  std::shared_ptr<FilterContents> blurred_mask =
+      FilterContents::MakeGaussianBlur(FilterInput::Make(mask), sigma, sigma,
+                                       Entity::TileMode::kDecal, style,
+                                       geometry);
+
+  return ColorFilterContents::MakeBlend(
+      BlendMode::kSourceIn,
+      {FilterInput::Make(blurred_mask), FilterInput::Make(texture_contents)});
 }
 
 std::shared_ptr<FilterContents> Paint::MaskBlurDescriptor::CreateMaskBlur(
