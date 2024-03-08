705f6bd1171b023e81aa6db512a8daf39a66749f
Polina Cherkasova <polina.c@live.com>
2024-03-08 11:59:33 -0800

    -

--- packages/flutter_tools/lib/src/update_packages_pins.dart
+++ packages/flutter_tools/lib/src/update_packages_pins.dart
@@ -25,4 +25,25 @@ const Map<String, String> kManuallyPinnedDependencies = <String, String>{
   'leak_tracker_flutter_testing': '3.0.3', // https://github.com/flutter/devtools/issues/3951
   'path_provider_android':
       '2.2.1', // https://github.com/flutter/flutter/issues/140796
+
+
+  'web': '0.4.2', // https://github.com/flutter/flutter/issues/144787
+  'metrics_center': '1.0.12', // https://github.com/flutter/flutter/issues/144787
+  'yaml_edit': '2.1.1',
+  'devtools_shared': '6.0.4',
+  'built_value': '8.9.0',
+  'dds': '3.1.2',
+  'unified_analytics': '5.8.4',
+  'url_launcher': '6.2.4',
+  'url_launcher_android': '6.2.2',
+  'url_launcher_platform_interface': '2.3.1',
+  'url_launcher_ios': '6.2.4',
+  'provider': '6.1.1',
+  'camera_avfoundation': '0.9.14',
+  'camera_platform_interface': '2.7.3',
+  'video_player': '2.8.2',
+  'video_player_android': '2.4.11',
+  'video_player_web': '2.1.3',
+  'ffi': '2.1.2',
+  'win32': '5.2.0',
 };
