08cbcd38e68c307328210a4195941359c87f0096
victorgalo <vtrgalo@gmail.com>
2024-03-09 21:26:08 +0100

    Update packages/flutter/test/semantics/semantics_update_test.dart
    Co-authored-by: chunhtai <47866232+chunhtai@users.noreply.github.com>

--- packages/flutter/test/semantics/semantics_update_test.dart
+++ packages/flutter/test/semantics/semantics_update_test.dart
@@ -82,9 +82,7 @@ void main() {
 
     SemanticsUpdateBuilderSpy.observations.clear();
     handle.dispose();
-  },
-    skip: true, // [intended] Skipped temporarily until headingLevel is added in engine (see issue 41435).
-  );
+  }, skip: true); // https://github.com/flutter/flutter/issues/97894
 
   testWidgets('Semantics update receives attributed text', (WidgetTester tester) async {
     final SemanticsHandle handle = tester.ensureSemantics();
