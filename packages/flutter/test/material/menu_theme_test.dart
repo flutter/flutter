95f37363c3a84f604bb37f03728a32ad90615496
Polina Cherkasova <polina.c@live.com>
2024-03-10 20:49:36 -0700

    -

--- packages/flutter/test/material/menu_theme_test.dart
+++ packages/flutter/test/material/menu_theme_test.dart
@@ -4,9 +4,13 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:leak_tracker_flutter_testing/leak_tracker_flutter_testing.dart';
 
 
 void main() {
+  // TODO(polina-c): clean up leaks, https://github.com/flutter/flutter/issues/134787 [leaks-to-clean]
+  LeakTesting.settings = LeakTesting.settings.withIgnoredAll();
+
   void onPressed(TestMenu item) {}
 
   Finder findMenuPanels(Axis orientation) {
