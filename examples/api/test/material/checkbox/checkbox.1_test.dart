d2498b7b34b50b45e155951d3d14b9d3f3d7355a
EliasElfarri <elias.elfarri@inline-test.no>
2024-03-12 13:36:23 +0100

    fixes indentation and spacing

--- examples/api/test/material/checkbox/checkbox.1_test.dart
+++ examples/api/test/material/checkbox/checkbox.1_test.dart
@@ -13,58 +13,56 @@ void main() {
       const example.CheckboxExampleApp(),
     );
 
-
-  expect(find.byType(Checkbox), findsNWidgets(3));
+    expect(find.byType(Checkbox), findsNWidgets(3));
     Checkbox checkbox = tester.widget(find.byType(Checkbox).first);
     Checkbox checkboxWithError = tester.widget(find.byType(Checkbox).at(1));
     Checkbox checkboxDisabled = tester.widget(find.byType(Checkbox).last);
 
-  // Verify the initial state of the checkboxes.
-  expect(checkbox.value, isTrue);
-  expect(checkboxWithError.value, isTrue);
-  expect(checkboxDisabled.value, isTrue);
+    // Verify the initial state of the checkboxes.
+    expect(checkbox.value, isTrue);
+    expect(checkboxWithError.value, isTrue);
+    expect(checkboxDisabled.value, isTrue);
 
-  expect(checkboxWithError.isError, isTrue);
-  expect(checkboxDisabled.onChanged, null);
+    expect(checkboxWithError.isError, isTrue);
+    expect(checkboxDisabled.onChanged, null);
 
-  expect(checkbox.tristate, isTrue);
-  expect(checkboxWithError.tristate, isTrue);
-  expect(checkboxDisabled.tristate, isTrue);
+    expect(checkbox.tristate, isTrue);
+    expect(checkboxWithError.tristate, isTrue);
+    expect(checkboxDisabled.tristate, isTrue);
 
+    // Tap the first Checkbox and verify the state change.
+    await tester.tap(find.byType(Checkbox).first);
+    await tester.pump();
+    checkbox = tester.widget(find.byType(Checkbox).first);
+    checkboxWithError = tester.widget(find.byType(Checkbox).at(1));
+    checkboxDisabled = tester.widget(find.byType(Checkbox).last);
 
-  // Tap the first Checkbox and verify the state change.
-  await tester.tap(find.byType(Checkbox).first);
-  await tester.pump();
-  checkbox = tester.widget(find.byType(Checkbox).first);
-  checkboxWithError = tester.widget(find.byType(Checkbox).at(1));
-  checkboxDisabled = tester.widget(find.byType(Checkbox).last);
+    expect(checkbox.value, isNull);
+    expect(checkboxWithError.value, isNull);
+    expect(checkboxDisabled.value, isNull);
 
-  expect(checkbox.value, isNull);
-  expect(checkboxWithError.value, isNull);
-  expect(checkboxDisabled.value, isNull);
+    // Tap the second Checkbox and verify the state change.
+    await tester.tap(find.byType(Checkbox).at(1));
+    await tester.pump();
 
-  // Tap the second Checkbox and verify the state change.
-  await tester.tap(find.byType(Checkbox).at(1));
-  await tester.pump();
+    checkbox = tester.widget(find.byType(Checkbox).first);
+    checkboxWithError = tester.widget(find.byType(Checkbox).at(1));
+    checkboxDisabled = tester.widget(find.byType(Checkbox).last);
 
-  checkbox = tester.widget(find.byType(Checkbox).first);
-  checkboxWithError = tester.widget(find.byType(Checkbox).at(1));
-  checkboxDisabled = tester.widget(find.byType(Checkbox).last);
+    expect(checkbox.value, isFalse);
+    expect(checkboxWithError.value, isFalse);
+    expect(checkboxDisabled.value, isFalse);
 
-  expect(checkbox.value, isFalse);
-  expect(checkboxWithError.value, isFalse);
-  expect(checkboxDisabled.value, isFalse);
+    // Tap the third Checkbox and verify that should remain unchanged.
+    await tester.tap(find.byType(Checkbox).last);
+    await tester.pump();
 
-  // Tap the third Checkbox and verify that should remain unchanged.
-  await tester.tap(find.byType(Checkbox).last);
-  await tester.pump();
+    checkbox = tester.widget(find.byType(Checkbox).first);
+    checkboxWithError = tester.widget(find.byType(Checkbox).at(1));
+    checkboxDisabled = tester.widget(find.byType(Checkbox).last);
 
-  checkbox = tester.widget(find.byType(Checkbox).first);
-  checkboxWithError = tester.widget(find.byType(Checkbox).at(1));
-  checkboxDisabled = tester.widget(find.byType(Checkbox).last);
-
-  expect(checkbox.value, isFalse);
-  expect(checkboxWithError.value, isFalse);
-  expect(checkboxDisabled.value, isFalse);
-});
+    expect(checkbox.value, isFalse);
+    expect(checkboxWithError.value, isFalse);
+    expect(checkboxDisabled.value, isFalse);
+  });
 }
