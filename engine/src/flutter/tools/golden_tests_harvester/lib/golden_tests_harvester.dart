e889b287f1518711fd728af075462bed403b4fe3
Matan Lurey <matanlurey@users.noreply.github.com>
2024-03-12 20:45:17 -0700

    Refactor `golden_tests_harvester`, throw when not `--dry-run`, add tests. (flutter/engine#51364)
    Closes https://github.com/flutter/flutter/issues/144948.
    
    I went a little farther, as I'm not looking forward to debugging this in the future without tests or examples.

--- engine/src/flutter/tools/golden_tests_harvester/lib/golden_tests_harvester.dart
+++ engine/src/flutter/tools/golden_tests_harvester/lib/golden_tests_harvester.dart
@@ -2,42 +2,105 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'dart:io';
+import 'dart:io' as io;
 
 import 'package:path/path.dart' as p;
-import 'package:skia_gold_client/skia_gold_client.dart';
 
-import 'logger.dart';
+import 'src/digests_json_format.dart';
 
-export 'logger.dart';
-
-/// Reads the digest inside of [workDirectory], sending tests to
-/// [skiaGoldClient].
-Future<void> harvest(SkiaGoldClient skiaGoldClient, Directory workDirectory,
-    List<Object?> entries) async {
-  await skiaGoldClient.auth();
-
+/// Uploads the images of digests  in [workDirectory] to Skia Gold.
+///
+/// The directory is expected to match the following structure:
+/// ```txt
+/// workDirectory/
+///   - digest.json
+///   - test_name_1.png
+///   - test_name_2.png
+///   - ...
+/// ```
+///
+/// The format of `digest.json` is expected to match the following:
+/// ```jsonc
+/// {
+///   "dimensions": {
+///     // Key-value pairs of dimensions to provide to Skia Gold.
+///     // For example:
+///     "platform": "linux",
+///   },
+///   "entries": [
+///     // Each entry is a test-run with the following format:
+///     {
+///       // Path must be a direct sibling of digest.json.
+///       "filename": "test_name_1.png",
+///
+///       // Called `screenshotSize` in Skia Gold (width * height).
+///       "width": 100,
+///       "height": 100,
+///
+///       // Called `differentPixelsRate` in Skia Gold.
+///       "maxDiffPixelsPercent": 0.01,
+///
+///       // Called `pixelColorDelta` in Skia Gold.
+///       "maxColorDelta": 0
+///     }
+///   ]
+/// }
+/// ```
+Future<void> harvest({
+  required io.Directory workDirectory,
+  required AddImageToSkiaGold addImg,
+  required StringSink stderr,
+}) async {
+  final io.File file = io.File(p.join(workDirectory.path, 'digest.json'));
+  if (!file.existsSync()) {
+    // Check if the directory exists or if the file is just missing.
+    if (!workDirectory.existsSync()) {
+      throw ArgumentError('Directory not found: ${workDirectory.path}.');
+    }
+    // Lookup sibling files to help the user understand what's missing.
+    final List<io.FileSystemEntity> files = workDirectory.listSync();
+    throw StateError(
+      'File "digest.json" not found in ${workDirectory.path}.\n\n'
+      'Found files: ${files.map((io.FileSystemEntity e) => p.basename(e.path)).join(', ')}',
+    );
+  }
+  final Digests digests = Digests.parse(file.readAsStringSync());
   final List<Future<void>> pendingComparisons = <Future<void>>[];
-  for (final Object? entry in entries) {
-    final Map<String, Object?> map = (entry as Map<String, Object?>?)!;
-    final String filename = (map['filename'] as String?)!;
-    final int width = (map['width'] as int?)!;
-    final int height = (map['height'] as int?)!;
-    final double maxDiffPixelsPercent =
-        (map['maxDiffPixelsPercent'] as double?)!;
-    final int maxColorDelta = (map['maxColorDelta'] as int?)!;
-    final File goldenImage = File(p.join(workDirectory.path, filename));
-    final Future<void> future = skiaGoldClient
-        .addImg(filename, goldenImage,
-            screenshotSize: width * height,
-            differentPixelsRate: maxDiffPixelsPercent,
-            pixelColorDelta: maxColorDelta)
-        .catchError((dynamic err) {
-      Logger.instance.log('skia gold comparison failed: $err');
-      throw Exception('Failed comparison: $filename');
+  for (final DigestEntry entry in digests.entries) {
+    final io.File goldenFile = io.File(p.join(workDirectory.path, entry.filename));
+    final Future<void> future = addImg(
+      entry.filename,
+      goldenFile,
+      screenshotSize: entry.width * entry.height,
+      differentPixelsRate: entry.maxDiffPixelsPercent,
+      pixelColorDelta: entry.maxColorDelta,
+    ).catchError((Object e) {
+      stderr.writeln('Failed to add image to Skia Gold: $e');
+      throw FailedComparisonException(entry.filename);
     });
     pendingComparisons.add(future);
   }
 
   await Future.wait(pendingComparisons);
 }
+
+/// An exception thrown when a comparison fails.
+final class FailedComparisonException implements Exception {
+  /// Creates a new instance of [FailedComparisonException].
+  const FailedComparisonException(this.testName);
+
+  /// The test name that failed.
+  final String testName;
+
+  @override
+  String toString() => 'Failed comparison: $testName';
+}
+
+/// A function that uploads an image to Skia Gold.
+typedef AddImageToSkiaGold = Future<void> Function(
+  String testName,
+  io.File goldenFile, {
+  double differentPixelsRate,
+  int pixelColorDelta,
+  required int screenshotSize,
+});
