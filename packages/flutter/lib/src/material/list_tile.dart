48f10e6e867c6d2a1e01558c63a597a3740b4aaa
hangyujin <108393416+hangyujin@users.noreply.github.com>
2024-03-18 21:49:56 -0700

    lint

--- packages/flutter/lib/src/material/list_tile.dart
+++ packages/flutter/lib/src/material/list_tile.dart
@@ -674,7 +674,7 @@ class ListTile extends StatelessWidget {
   /// The minimum height allocated for the [ListTile] widget.
   ///
   /// If this is null, default tile heights are 56.0, 72.0, and 88.0 for one,
-  /// two, and three lines of text respectively. If [isDensity] is true, these
+  /// two, and three lines of text respectively. If `isDense` is true, these
   /// defaults are changed to 48.0, 64.0, and 76.0. A visual density value or
   /// a large title will also adjust the default tile heights.
   /// {@endtemplate}
