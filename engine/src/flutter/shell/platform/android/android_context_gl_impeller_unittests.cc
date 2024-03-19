61f5cf69d4a381f8be7bb2cdad8a9346649d9b4e
Chinmay Garde <chinmaygarde@google.com>
2024-03-18 20:47:23 -0700

    Add missing license blocks. (flutter/engine#51496)
    These are files that the license script ignores.

--- engine/src/flutter/shell/platform/android/android_context_gl_impeller_unittests.cc
+++ engine/src/flutter/shell/platform/android/android_context_gl_impeller_unittests.cc
@@ -1,3 +1,7 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 #include "flutter/shell/platform/android/android_context_gl_impeller.h"
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
