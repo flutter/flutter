dcd175ecb5488b61eb0ac2c2b7f00a23fb4881d9
Chinmay Garde <chinmaygarde@google.com>
2024-03-18 20:47:23 -0700

    Add missing license blocks. (flutter/engine#51496)
    These are files that the license script ignores.

--- engine/src/flutter/shell/platform/android/android_shell_holder_unittests.cc
+++ engine/src/flutter/shell/platform/android/android_shell_holder_unittests.cc
@@ -1,4 +1,9 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 #include <memory>
+
 #include "flutter/shell/platform/android/android_shell_holder.h"
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
--- engine/src/flutter/shell/platform/android/apk_asset_provider_unittests.cc
+++ engine/src/flutter/shell/platform/android/apk_asset_provider_unittests.cc
@@ -1,3 +1,7 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 #include "flutter/shell/platform/android/apk_asset_provider.h"
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
