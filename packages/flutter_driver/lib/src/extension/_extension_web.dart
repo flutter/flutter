935592f8e82b93a6ba03a4df63a7a2b8f6c56891
Jackson Gardner <jacksongardner@google.com>
2024-03-20 10:45:27 -0700

    More migrations off of dart:js.

--- packages/flutter_driver/lib/src/extension/_extension_web.dart
+++ packages/flutter_driver/lib/src/extension/_extension_web.dart
@@ -3,7 +3,6 @@
 // found in the LICENSE file.
 
 import 'dart:convert';
-import 'dart:js';
 import 'dart:js_interop';
 import 'dart:js_interop_unsafe';
 
@@ -24,13 +23,13 @@ void registerWebServiceExtension(Future<Map<String, dynamic>> Function(Map<Strin
   // Define the result variable because packages/flutter_driver/lib/src/driver/web_driver.dart
   // checks for this value to become non-null when waiting for the result. If this value is
   // undefined at the time of the check, WebDriver throws an exception.
-  context[r'$flutterDriverResult'] = null;
+  _window.setProperty(r'$flutterDriverResult'.toJS, null);
 
-  _window.setProperty(r'$flutterDriver'.toJS, (JSString message) async {
+  _window.setProperty(r'$flutterDriver'.toJS, (JSAny message) async {
     final Map<String, String> params = Map<String, String>.from(
-        jsonDecode(message.toDart) as Map<String, dynamic>);
+        jsonDecode((message as JSString).toDart) as Map<String, dynamic>);
     final Map<String, dynamic> result = Map<String, dynamic>.from(
         await call(params));
-    context[r'$flutterDriverResult'] = json.encode(result);
+    _window.setProperty(r'$flutterDriverResult'.toJS, json.encode(result).toJS);
   }.toJS);
 }
--- packages/integration_test/lib/src/_extension_web.dart
+++ packages/integration_test/lib/src/_extension_web.dart
@@ -24,9 +24,9 @@ void registerWebServiceExtension(Future<Map<String, dynamic>> Function(Map<Strin
   // undefined at the time of the check, WebDriver throws an exception.
   _window.setProperty(r'$flutterDriverResult'.toJS, null);
 
-  _window.setProperty(r'$flutterDriver'.toJS, (JSString message) async {
+  _window.setProperty(r'$flutterDriver'.toJS, (JSAny message) async {
     try {
-      final Map<String, dynamic> messageJson = jsonDecode(message.toDart) as Map<String, dynamic>;
+      final Map<String, dynamic> messageJson = jsonDecode((message as JSString).toDart) as Map<String, dynamic>;
       final Map<String, String> params = messageJson.cast<String, String>();
       final Map<String, dynamic> result = await callback(params);
       _window.setProperty(r'$flutterDriverResult'.toJS, json.encode(result).toJS);
