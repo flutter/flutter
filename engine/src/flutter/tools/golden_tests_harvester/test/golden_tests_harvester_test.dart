4779d2f61b4fa5332ebb0f729b6ebf66e61a6e0c
gaaclarke <30870216+gaaclarke@users.noreply.github.com>
2024-03-20 15:49:16 -0700

    [golden_test_harvester]: Put back sending the dimensions to the SkiaGoldClient (flutter/engine#51536)
    fixes https://github.com/flutter/flutter/issues/145413
    
    ## Pre-launch Checklist
    
    - [x] I read the [Contributor Guide] and followed the process outlined
    there for submitting PRs.
    - [x] I read the [Tree Hygiene] wiki page, which explains my
    responsibilities.
    - [x] I read and followed the [Flutter Style Guide] and the [C++,
    Objective-C, Java style guides].
    - [x] I listed at least one issue that this PR fixes in the description
    above.
    - [x] I added new tests to check the change I am making or feature I am
    adding, or the PR is [test-exempt]. See [testing the engine] for
    instructions on writing and running engine tests.
    - [x] I updated/added relevant documentation (doc comments with `///`).
    - [x] I signed the [CLA].
    - [x] All existing and new tests are passing.
    
    If you need help, consider asking for advice on the #hackers-new channel
    on [Discord].
    
    <!-- Links -->
    [Contributor Guide]:
    https://github.com/flutter/flutter/wiki/Tree-hygiene#overview
    [Tree Hygiene]: https://github.com/flutter/flutter/wiki/Tree-hygiene
    [test-exempt]:
    https://github.com/flutter/flutter/wiki/Tree-hygiene#tests
    [Flutter Style Guide]:
    https://github.com/flutter/flutter/wiki/Style-guide-for-Flutter-repo
    [C++, Objective-C, Java style guides]:
    https://github.com/flutter/engine/blob/main/CONTRIBUTING.md#style
    [testing the engine]:
    https://github.com/flutter/flutter/wiki/Testing-the-engine
    [CLA]: https://cla.developers.google.com/
    [flutter/tests]: https://github.com/flutter/tests
    [breaking change policy]:
    https://github.com/flutter/flutter/wiki/Tree-hygiene#handling-breaking-changes
    [Discord]: https://github.com/flutter/flutter/wiki/Chat

--- engine/src/flutter/tools/golden_tests_harvester/test/golden_tests_harvester_test.dart
+++ engine/src/flutter/tools/golden_tests_harvester/test/golden_tests_harvester_test.dart
@@ -10,8 +10,10 @@ import 'package:litetest/litetest.dart';
 import 'package:path/path.dart' as p;
 
 void main() async {
-  Future<void> withTempDirectory(FutureOr<void> Function(io.Directory) callback) async {
-    final io.Directory tempDirectory = await io.Directory.systemTemp.createTemp('golden_tests_harvester_test.');
+  Future<void> withTempDirectory(
+      FutureOr<void> Function(io.Directory) callback) async {
+    final io.Directory tempDirectory = await io.Directory.systemTemp
+        .createTemp('golden_tests_harvester_test.');
     try {
       await callback(tempDirectory);
     } finally {
@@ -23,27 +25,26 @@ void main() async {
     await withTempDirectory((io.Directory tempDirectory) async {
       final StringSink stderr = StringBuffer();
       final ArgumentError error = await _expectThrow<ArgumentError>(() async {
-        await harvest(
-          workDirectory: io.Directory(p.join(tempDirectory.path, 'non_existent')),
-          addImg: _alwaysThrowsAddImg,
-          stderr: stderr,
-        );
+        await Harvester.create(
+            io.Directory(p.join(tempDirectory.path, 'non_existent')),
+            stderr,
+            addImageToSkiaGold: _alwaysThrowsAddImg);
       });
       expect(error.message, contains('non_existent'));
       expect(stderr.toString(), isEmpty);
     });
   });
 
-  test('should require a file named "digest.json" in the working directory', () async {
+  test('should require a file named "digest.json" in the working directory',
+      () async {
     await withTempDirectory((io.Directory tempDirectory) async {
       final StringSink stderr = StringBuffer();
 
       final StateError error = await _expectThrow<StateError>(() async {
-        await harvest(
-          workDirectory: tempDirectory,
-          addImg: _alwaysThrowsAddImg,
-          stderr: stderr,
-        );
+        await Harvester.create(
+            tempDirectory,
+            stderr,
+            addImageToSkiaGold: _alwaysThrowsAddImg);
       });
       expect(error.toString(), contains('digest.json'));
       expect(stderr.toString(), isEmpty);
@@ -53,15 +54,17 @@ void main() async {
   test('should throw if "digest.json" is in an unexpected format', () async {
     await withTempDirectory((io.Directory tempDirectory) async {
       final StringSink stderr = StringBuffer();
-      final io.File digestsFile = io.File(p.join(tempDirectory.path, 'digest.json'));
-      await digestsFile.writeAsString('{"dimensions": "not a map", "entries": []}');
+      final io.File digestsFile =
+          io.File(p.join(tempDirectory.path, 'digest.json'));
+      await digestsFile
+          .writeAsString('{"dimensions": "not a map", "entries": []}');
 
-      final FormatException error = await _expectThrow<FormatException>(() async {
-        await harvest(
-          workDirectory: tempDirectory,
-          addImg: _alwaysThrowsAddImg,
-          stderr: stderr,
-        );
+      final FormatException error =
+          await _expectThrow<FormatException>(() async {
+        await Harvester.create(
+            tempDirectory,
+            stderr,
+            addImageToSkiaGold: _alwaysThrowsAddImg);
       });
       expect(error.message, contains('dimensions'));
       expect(stderr.toString(), isEmpty);
@@ -70,7 +73,8 @@ void main() async {
 
   test('should fail eagerly if addImg fails', () async {
     await withTempDirectory((io.Directory tempDirectory) async {
-      final io.File digestsFile = io.File(p.join(tempDirectory.path, 'digest.json'));
+      final io.File digestsFile =
+          io.File(p.join(tempDirectory.path, 'digest.json'));
       final StringSink stderr = StringBuffer();
       await digestsFile.writeAsString('''
         {
@@ -87,12 +91,13 @@ void main() async {
         }
       ''');
 
-      final FailedComparisonException error = await _expectThrow<FailedComparisonException>(() async {
-        await harvest(
-          workDirectory: tempDirectory,
-          addImg: _alwaysThrowsAddImg,
-          stderr: stderr,
-        );
+      final FailedComparisonException error =
+          await _expectThrow<FailedComparisonException>(() async {
+        final Harvester harvester = await Harvester.create(
+            tempDirectory,
+            stderr,
+            addImageToSkiaGold: _alwaysThrowsAddImg);
+        await harvest(harvester);
       });
       expect(error.testName, 'test_name_1.png');
       expect(stderr.toString(), contains('IntentionalError'));
@@ -101,7 +106,8 @@ void main() async {
 
   test('should invoke addImg per test', () async {
     await withTempDirectory((io.Directory tempDirectory) async {
-      final io.File digestsFile = io.File(p.join(tempDirectory.path, 'digest.json'));
+      final io.File digestsFile =
+          io.File(p.join(tempDirectory.path, 'digest.json'));
       await digestsFile.writeAsString('''
         {
           "dimensions": {},
@@ -125,29 +131,73 @@ void main() async {
       ''');
       final List<String> addImgCalls = <String>[];
       final StringSink stderr = StringBuffer();
-      await harvest(
-        workDirectory: tempDirectory,
-        addImg: (
-          String testName,
-          io.File goldenFile, {
-          required int screenshotSize,
-          double differentPixelsRate = 0.01,
-          int pixelColorDelta = 0,
-        }) async {
-          addImgCalls.add('$testName $screenshotSize $differentPixelsRate $pixelColorDelta');
-        },
-        stderr: stderr,
-      );
+
+      final Harvester harvester =
+          await Harvester.create(tempDirectory, stderr, addImageToSkiaGold: (
+        String testName,
+        io.File goldenFile, {
+        required int screenshotSize,
+        double differentPixelsRate = 0.01,
+        int pixelColorDelta = 0,
+      }) async {
+        addImgCalls.add(
+            '$testName $screenshotSize $differentPixelsRate $pixelColorDelta');
+      });
+      await harvest(harvester);
       expect(addImgCalls, <String>[
         'test_name_1.png 10000 0.01 0',
         'test_name_2.png 40000 0.02 1',
       ]);
     });
+  });
 
+  test('client has dimensions', () async {
+    await withTempDirectory((io.Directory tempDirectory) async {
+      final StringSink stderr = StringBuffer();
+      final io.File digestsFile =
+          io.File(p.join(tempDirectory.path, 'digest.json'));
+      await digestsFile
+          .writeAsString('{"dimensions": {"key":"value"}, "entries": []}');
+      final Harvester harvester = await Harvester.create(tempDirectory, stderr);
+      expect(harvester is SkiaGoldHarvester, true);
+      final SkiaGoldHarvester skiaGoldHarvester =
+          harvester as SkiaGoldHarvester;
+      expect(skiaGoldHarvester.client.dimensions,
+          <String, String>{'key': 'value'});
+    });
+  });
+
+  test('throws without GOLDCTL', () async {
+    await withTempDirectory((io.Directory tempDirectory) async {
+      final StringSink stderr = StringBuffer();
+      final io.File digestsFile =
+          io.File(p.join(tempDirectory.path, 'digest.json'));
+      await digestsFile.writeAsString('''
+{
+  "dimensions": {"key":"value"},
+  "entries": [
+    {
+      "filename": "foo.png",
+      "width": 100,
+      "height": 100,
+      "maxDiffPixelsPercent": 0.01,
+      "maxColorDelta": 0
+    }
+  ]}
+''');
+      final Harvester harvester = await Harvester.create(tempDirectory, stderr);
+      final StateError error = await _expectThrow<StateError>(() async {
+        await harvest(harvester);
+      });
+      expect(error.message, contains('GOLDCTL'));
+      expect(stderr.toString(), isEmpty);
+    });
   });
 }
 
-FutureOr<T> _expectThrow<T extends Object>(FutureOr<void> Function() callback) async {
+
+FutureOr<T> _expectThrow<T extends Object>(
+    FutureOr<void> Function() callback) async {
   try {
     await callback();
     fail('Expected an exception of type $T');
