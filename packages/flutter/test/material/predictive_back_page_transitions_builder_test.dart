f8ea6f034d90d59da0602b784834349e8d49fab4
Justin McCandless <jmccandless@google.com>
2024-03-21 10:57:28 -0700

    Test multiple platforms and cancel.

--- packages/flutter/test/material/predictive_back_page_transitions_builder_test.dart
+++ packages/flutter/test/material/predictive_back_page_transitions_builder_test.dart
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -9,9 +10,20 @@ import 'package:flutter_test/flutter_test.dart';
 void main() {
   final TestWidgetsFlutterBinding binding = TestWidgetsFlutterBinding.ensureInitialized();
 
-  // TODO(justinmc): Test what happens on other platforms.
-  // TODO(justinmc): Test cancel.
-  testWidgets('PageTransitionsTheme override builds a _OpenUpwardsPageTransition', (WidgetTester tester) async {
+  Finder findPredictiveBackPageTransition() {
+    return find.descendant(
+      of: find.byType(MaterialApp),
+      matching: find.byWidgetPredicate((Widget w) => '${w.runtimeType}' == '_PredictiveBackPageTransition'),
+    );
+  }
+  Finder findFallbackPageTransition() {
+    return find.descendant(
+      of: find.byType(MaterialApp),
+      matching: find.byWidgetPredicate((Widget w) => '${w.runtimeType}' == '_ZoomPageTransition'),
+    );
+  }
+
+  testWidgets('PredictiveBackPageTransitionsBuilder supports predictive back on Android', (WidgetTester tester) async {
     final Map<String, WidgetBuilder> routes = <String, WidgetBuilder>{
       '/': (BuildContext context) => Material(
         child: TextButton(
@@ -25,28 +37,16 @@ void main() {
     await tester.pumpWidget(
       MaterialApp(
         theme: ThemeData(
-          pageTransitionsTheme: const PageTransitionsTheme(
+          pageTransitionsTheme: PageTransitionsTheme(
             builders: <TargetPlatform, PageTransitionsBuilder>{
-              TargetPlatform.android: PredictiveBackPageTransitionsBuilder(),
+              for (final TargetPlatform platform in TargetPlatform.values)
+                platform: const PredictiveBackPageTransitionsBuilder(),
             },
           ),
         ),
         routes: routes,
       ),
     );
-
-    Finder findPredictiveBackPageTransition() {
-      return find.descendant(
-        of: find.byType(MaterialApp),
-        matching: find.byWidgetPredicate((Widget w) => '${w.runtimeType}' == '_PredictiveBackPageTransition'),
-      );
-    }
-    Finder findFallbackPageTransition() {
-      return find.descendant(
-        of: find.byType(MaterialApp),
-        matching: find.byWidgetPredicate((Widget w) => '${w.runtimeType}' == '_ZoomPageTransition'),
-      );
-    }
 
     expect(find.text('push'), findsOneWidget);
     expect(find.text('page b'), findsNothing);
@@ -60,6 +60,12 @@ void main() {
     expect(find.text('page b'), findsOneWidget);
     expect(findPredictiveBackPageTransition(), findsNothing);
     expect(findFallbackPageTransition(), findsOneWidget);
+
+    // Only Android supports backGesture channel methods. Other platforms will
+    // do nothing.
+    if (defaultTargetPlatform != TargetPlatform.android) {
+      return;
+    }
 
     // Start a system pop gesture, which will switch to using
     // _PredictiveBackPageTransition for the page transition.
@@ -120,8 +126,114 @@ void main() {
 
     expect(findPredictiveBackPageTransition(), findsNothing);
     expect(findFallbackPageTransition(), findsOneWidget);
+    expect(find.text('push'), findsOneWidget);
+    expect(find.text('page b'), findsNothing);
+  }, variant: TargetPlatformVariant.all());
+
+  testWidgets('PredictiveBackPageTransitionsBuilder supports canceling a predictive back gesture', (WidgetTester tester) async {
+    final Map<String, WidgetBuilder> routes = <String, WidgetBuilder>{
+      '/': (BuildContext context) => Material(
+        child: TextButton(
+          child: const Text('push'),
+          onPressed: () { Navigator.of(context).pushNamed('/b'); },
+        ),
+      ),
+      '/b': (BuildContext context) => const Text('page b'),
+    };
+
+    await tester.pumpWidget(
+      MaterialApp(
+        theme: ThemeData(
+          pageTransitionsTheme: PageTransitionsTheme(
+            builders: <TargetPlatform, PageTransitionsBuilder>{
+              for (final TargetPlatform platform in TargetPlatform.values)
+                platform: const PredictiveBackPageTransitionsBuilder(),
+            },
+          ),
+        ),
+        routes: routes,
+      ),
+    );
 
     expect(find.text('push'), findsOneWidget);
     expect(find.text('page b'), findsNothing);
-  }, variant: TargetPlatformVariant.only(TargetPlatform.android));
+    expect(findPredictiveBackPageTransition(), findsNothing);
+    expect(findFallbackPageTransition(), findsOneWidget);
+
+    await tester.tap(find.text('push'));
+    await tester.pumpAndSettle();
+
+    expect(find.text('push'), findsNothing);
+    expect(find.text('page b'), findsOneWidget);
+    expect(findPredictiveBackPageTransition(), findsNothing);
+    expect(findFallbackPageTransition(), findsOneWidget);
+
+    // Only Android supports backGesture channel methods. Other platforms will
+    // do nothing.
+    if (defaultTargetPlatform != TargetPlatform.android) {
+      return;
+    }
+
+    // Start a system pop gesture, which will switch to using
+    // _PredictiveBackPageTransition for the page transition.
+    final ByteData? startMessage = const JSONMessageCodec().encodeMessage(<String, dynamic>{
+      'method': 'startBackGesture',
+      'args': <String, dynamic>{
+        'x': 5.0,
+        'y': 300.0,
+        'progress': 0.0,
+        'swipeEdge': 0, // left
+      },
+    });
+    await binding.defaultBinaryMessenger.handlePlatformMessage(
+      'flutter/backgesture',
+      startMessage,
+      (ByteData? _) {},
+    );
+    await tester.pump();
+
+    expect(findPredictiveBackPageTransition(), findsOneWidget);
+    expect(findFallbackPageTransition(), findsNothing);
+    final Offset startPageBOffset = tester.getTopLeft(find.text('page b'));
+    expect(startPageBOffset.dx, 0.0);
+
+    // Drag the system back gesture.
+    final ByteData? updateMessage = const JSONMessageCodec().encodeMessage(<String, dynamic>{
+      'method': 'updateBackGestureProgress',
+      'args': <String, dynamic>{
+        'x': 100.0,
+        'y': 300.0,
+        'progress': 0.35,
+        'swipeEdge': 0, // left
+      },
+    });
+    await binding.defaultBinaryMessenger.handlePlatformMessage(
+      'flutter/backgesture',
+      updateMessage,
+      (ByteData? _) {},
+    );
+    await tester.pumpAndSettle();
+
+    expect(findPredictiveBackPageTransition(), findsNWidgets(2));
+    expect(findFallbackPageTransition(), findsNothing);
+
+    final Offset updatePageBOffset = tester.getTopLeft(find.text('page b'));
+    expect(updatePageBOffset.dx, greaterThan(startPageBOffset.dx));
+
+    // Cancel the system back gesture.
+    final ByteData? commitMessage = const JSONMessageCodec().encodeMessage(<String, dynamic>{
+      'method': 'cancelBackGesture',
+    });
+    await binding.defaultBinaryMessenger.handlePlatformMessage(
+      'flutter/backgesture',
+      commitMessage,
+      (ByteData? _) {},
+    );
+    await tester.pumpAndSettle();
+
+    expect(find.text('push'), findsNothing);
+    expect(find.text('page b'), findsOneWidget);
+    expect(findPredictiveBackPageTransition(), findsNothing);
+    expect(findFallbackPageTransition(), findsOneWidget);
+  }, variant: TargetPlatformVariant.all());
 }
