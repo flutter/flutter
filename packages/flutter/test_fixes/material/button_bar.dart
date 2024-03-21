6788147b8f65f66ea4b1a4bce7b34decb57fe193
Taha Tesser <tessertaha@gmail.com>
2024-03-21 11:00:14 +0200

    Deprecate `ButtonBar`, `ButtonBarThemeData`, and `ThemeData.buttonBarTheme`

---
+++ packages/flutter/test_fixes/material/button_bar.dart
@@ -0,0 +1,14 @@
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+
+void main() {
+  // Changes made in TBD.
+  ButtonBar();
+
+  // Changes made in TBD.
+  ThemeData theme = ThemeData();
+  theme = ThemeData(buttonBarTheme: ButtonBarThemeData());
+}
