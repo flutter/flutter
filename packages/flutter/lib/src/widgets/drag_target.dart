5f9362e976a18cd8a3f9e0bb1d1e8a370e8c8359
Tim Lehmenn <hi@timcreated.it>
2024-03-23 19:54:26 +0100

    use overlay coordinate space for draggable feedback

--- packages/flutter/lib/src/widgets/drag_target.dart
+++ packages/flutter/lib/src/widgets/drag_target.dart
@@ -830,6 +830,7 @@ class _DragAvatar<T extends Object> extends Drag {
   final List<_DragTargetState<Object>> _enteredTargets = <_DragTargetState<Object>>[];
   Offset _position;
   Offset? _lastOffset;
+  Offset? _overlayOffset;
   OverlayEntry? _entry;
 
   @override
@@ -855,6 +856,10 @@ class _DragAvatar<T extends Object> extends Drag {
 
   void updateDrag(Offset globalPosition) {
     _lastOffset = globalPosition - dragStartPoint;
+    final RenderBox box = overlayState.context.findRenderObject()! as RenderBox;
+    final Offset overlaySpaceOffset = box.globalToLocal(globalPosition);
+    _overlayOffset = overlaySpaceOffset - dragStartPoint;
+
     _entry!.markNeedsBuild();
     final HitTestResult result = HitTestResult();
     WidgetsBinding.instance.hitTestInView(result, globalPosition + feedbackOffset, viewId);
@@ -936,15 +941,13 @@ class _DragAvatar<T extends Object> extends Drag {
     _entry!.dispose();
     _entry = null;
     // TODO(ianh): consider passing _entry as well so the client can perform an animation.
-    onDragEnd?.call(velocity ?? Velocity.zero, _lastOffset!, wasAccepted);
+    onDragEnd?.call(velocity ?? Velocity.zero, _position, wasAccepted);
   }
 
   Widget _build(BuildContext context) {
-    final RenderBox box = overlayState.context.findRenderObject()! as RenderBox;
-    final Offset overlayTopLeft = box.localToGlobal(Offset.zero);
     return Positioned(
-      left: _lastOffset!.dx - overlayTopLeft.dx,
-      top: _lastOffset!.dy - overlayTopLeft.dy,
+      left: _overlayOffset!.dx,
+      top: _overlayOffset!.dy,
       child: ExcludeSemantics(
         excluding: ignoringFeedbackSemantics,
         child: IgnorePointer(
