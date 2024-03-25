96bd7a58c484574f8b6b1a715729bc429043efb7
Gustl22 <git@reb0.org>
2024-03-25 08:59:14 +0100

    create duration picker

---
+++ dev/integration_tests/flutter_gallery/lib/duration_main.dart
@@ -0,0 +1,71 @@
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Thanks for checking out Flutter!
+// Like what you see? Tweet us @FlutterDev
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(const TestDurationApp());
+
+class TestDurationApp extends StatelessWidget {
+  const TestDurationApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+      ),
+      home: const Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Row(
+        children: [
+          IconButton(
+            icon: const Icon(Icons.access_time),
+            onPressed: () {
+              showTimePicker(context: context, initialTime: TimeOfDay.now());
+            },
+          ),
+          IconButton(
+            icon: const Icon(Icons.timelapse),
+            onPressed: () {
+              final duration = Duration();
+              showDurationPicker(
+                  context: context, initialTime: TimeOfDay.now());
+            },
+          ),
+          IconButton(
+            icon: const Icon(Icons.date_range),
+            onPressed: () {
+              showDatePicker(
+                  context: context,
+                  firstDate: DateTime.now(),
+                  lastDate: DateTime(DateTime.now().year + 1));
+            },
+          ),
+          IconButton(
+            icon: const Icon(Icons.date_range),
+            onPressed: () {
+              showDateRangePicker(
+                  context: context,
+                  firstDate: DateTime.now(),
+                  lastDate: DateTime(DateTime.now().year + 1));
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
