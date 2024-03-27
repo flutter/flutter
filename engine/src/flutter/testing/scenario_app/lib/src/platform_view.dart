34f5c82ffca8e55919dbfb8c018d223761b4017c
hellohuanlin <41930132+hellohuanlin@users.noreply.github.com>
2024-03-27 09:46:19 -0700

    [ios]ignore single edge pixel instead of rounding (flutter/engine#51687)
    The previous PR https://github.com/flutter/flutter/issues/143420 rounds out the layers and rounds in the platform views. This results in missing pixel on the edge of the intersection when there's fractional coordinate (as shown in the screenshot below), because platform view is below the layers. 
    
    It turns out that we have to round out both platform view and layers, because: 
    - rounding in platform view rects will result in missing pixels on the edge of the intersection. 
    - rounding in layer rects will result in missing pixels on the edge of the layer that's on top of the platform view. 
    
    This PR simply skips the single (or partial) pixel on the edge, which is a special case, while still preserve the `roundOut` behavior for general non-edge cases. 
    
    Before the fix, notice a very thin gray line cutting through the purple box: 
    
    <img src="https://github.com/flutter/engine/assets/41930132/1482d81a-337e-4841-ac08-eff08bbc71ef" height="500">
    
    Then after the fix, the gray line is gone: 
    
    <img src="https://github.com/flutter/engine/assets/41930132/0eddae69-ab62-4de6-8932-c67cc5aced73" height="500">
    
    *List which issues are fixed by this PR. You must list at least one issue.*
    
    https://github.com/flutter/flutter/issues/143420
    
    *If you had to change anything in the [flutter/tests] repo, include a link to the migration guide as per the [breaking change policy].*
    
    [C++, Objective-C, Java style guides]: https://github.com/flutter/engine/blob/main/CONTRIBUTING.md#style

--- engine/src/flutter/testing/scenario_app/lib/src/platform_view.dart
+++ engine/src/flutter/testing/scenario_app/lib/src/platform_view.dart
@@ -367,10 +367,10 @@ class PlatformViewMaxOverlaysScenario extends Scenario
 }
 
 /// A platform view with adjacent surrounding layers should not create overlays.
-class PlatformViewSurroundingLayersScenario extends Scenario
+class PlatformViewSurroundingLayersFractionalCoordinateScenario extends Scenario
     with _BasePlatformViewScenarioMixin {
   /// Creates the PlatformView scenario.
-  PlatformViewSurroundingLayersScenario(
+  PlatformViewSurroundingLayersFractionalCoordinateScenario(
     super.view, {
     required this.id,
   });
@@ -423,6 +423,55 @@ class PlatformViewSurroundingLayersScenario extends Scenario
     // Rect at the bottom of platform view
     canvas.drawRect(
       rect.shift(const Offset(0, 100)),
+      Paint()..color = const Color(0x22FF0000),
+    );
+
+    final Picture picture = recorder.endRecording();
+    builder.addPicture(Offset.zero, picture);
+
+    // Pop the (0.5, 0.5) offset.
+    builder.pop();
+
+    final Scene scene = builder.build();
+    view.render(scene);
+    scene.dispose();
+  }
+}
+
+/// A platform view partially intersect with a layer, both with fractional coordinates.
+class PlatformViewPartialIntersectionFractionalCoordinateScenario extends Scenario
+    with _BasePlatformViewScenarioMixin {
+  /// Creates the PlatformView scenario.
+  PlatformViewPartialIntersectionFractionalCoordinateScenario(
+    super.view, {
+    required this.id,
+  });
+
+  /// The platform view identifier.
+  final int id;
+
+  @override
+  void onBeginFrame(Duration duration) {
+    final SceneBuilder builder = SceneBuilder();
+
+    // Simulate partial pixel offsets as we would see while scrolling.
+    // All objects in the scene below are then on sub-pixel boundaries.
+    builder.pushOffset(0.5, 0.5);
+
+    // a platform view from (0, 0) to (100, 100)
+    addPlatformView(
+      id,
+      width: 100,
+      height: 100,
+      dispatcher: view.platformDispatcher,
+      sceneBuilder: builder,
+    );
+
+    final PictureRecorder recorder = PictureRecorder();
+    final Canvas canvas = Canvas(recorder);
+
+    canvas.drawRect(
+      const Rect.fromLTWH(50, 50, 100, 100),
       Paint()..color = const Color(0x22FF0000),
     );
 
