b7dddee939f2429e5fd3574a63f9dfac4f486a33
Matan Lurey <matanlurey@users.noreply.github.com>
2024-03-27 09:46:22 -0700

    Fail pre-submit if a negative image is encountered as part of `goldctl imgtest add`. (#51685)
    `flutter/engine`-side fix for https://github.com/flutter/flutter/issues/145043.
    
    - Before this PR, if a negative image was encountered, we'd silently pass pre-submit, merge, and turn the tree red.
    - After this PR, a negative image both makes pre and post-submit red.
    
    Added tests, and fixed up some unrelated tests that were accidentally setting `pid` instead of `exitCode`. Oops!
    
    /cc @zanderso and @eyebrowsoffire (current engine sheriff).

--- testing/skia_gold_client/lib/src/errors.dart
+++ testing/skia_gold_client/lib/src/errors.dart
@@ -51,3 +51,26 @@ final class SkiaGoldProcessError extends Error {
     ].join('\n');
   }
 }
+
+/// Error thrown when the Skia Gold process exits due to a negative image.
+final class SkiaGoldNegativeImageError extends SkiaGoldProcessError {
+  /// Creates a new [SkiaGoldNegativeImageError] from the provided origin.
+  ///
+  /// See [SkiaGoldProcessError.new] for more information.
+  SkiaGoldNegativeImageError({
+    required this.testName,
+    required super.command,
+    required super.stdout,
+    required super.stderr,
+  });
+
+  /// Name of the test that produced the negative image.
+  final String testName;
+
+  @override
+  String get message => 'Negative image detected for test: "$testName".\n\n'
+      'The flutter/engine workflow should never produce negative images; it is '
+      'possible that someone accidentally (or without knowing our policy) '
+      'marked a test as negative.\n\n'
+      'See https://github.com/flutter/flutter/issues/145043 for details.';
+}
