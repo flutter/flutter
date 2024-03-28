5844e4be09007b731ddc4490a2f1c44ebc35ed44
Jackson Gardner <jacksongardner@google.com>
2024-03-28 12:57:14 -0700

    Remove unused import.

--- packages/flutter/test/widgets/animated_size_test.dart
+++ packages/flutter/test/widgets/animated_size_test.dart
@@ -6,8 +6,6 @@ import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import '../assertion_helper.dart';
-
 class TestPaintingContext implements PaintingContext {
   final List<Invocation> invocations = <Invocation>[];
 
