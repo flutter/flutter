bdcd94ecdc78beefb134bef3874acbba2757ff0a
Tim Lehmenn <hi@timcreated.it>
2024-03-28 16:29:22 +0100

    added expected header to new test file and formatted it

--- packages/flutter/test/src/widgets/drag_target_test.dart
+++ packages/flutter/test/src/widgets/drag_target_test.dart
@@ -1,11 +1,14 @@
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
 
-  testWidgets('Draggable feedback matches pointer unscaled MaterialApp',
-      (WidgetTester tester) async {
+  testWidgets('Draggable feedback matches pointer in unscaled MaterialApp', (WidgetTester tester) async {
     await tester.pumpWidget(MaterialApp(
       home: Scaffold(
         body: Draggable<int>(
@@ -24,20 +27,17 @@ void main() {
       ),
     ));
 
-    final TestGesture gesture =
-        await tester.startGesture(tester.getCenter(find.byType(Container)));
+    final TestGesture gesture = await tester.startGesture(tester.getCenter(find.byType(Container)));
     await gesture.moveBy(const Offset(100, 100));
     await tester.pump();
     final Offset appTopLeft = tester.getTopLeft(find.byType(MaterialApp));
     final Finder finder = find.byType(Container);
     expect(finder, findsNWidgets(2));
     expect(tester.getTopLeft(finder.at(0)), appTopLeft);
-    expect(
-        tester.getTopLeft(finder.at(1)), appTopLeft + const Offset(100, 100));
+    expect(tester.getTopLeft(finder.at(1)), appTopLeft + const Offset(100, 100));
   });
 
-  testWidgets('Draggable feedback matches pointer in scaled MaterialApp',
-      (WidgetTester tester) async {
+  testWidgets('Draggable feedback matches pointer in scaled MaterialApp', (WidgetTester tester) async {
     await tester.pumpWidget(Transform.scale(
       scale: 0.5,
       child: MaterialApp(
@@ -59,15 +59,13 @@ void main() {
       ),
     ));
 
-    final TestGesture gesture =
-        await tester.startGesture(tester.getCenter(find.byType(Container)));
+    final TestGesture gesture = await tester.startGesture(tester.getCenter(find.byType(Container)));
     await gesture.moveBy(const Offset(100, 100));
     await tester.pump();
     final Offset appTopLeft = tester.getTopLeft(find.byType(MaterialApp));
     final Finder finder = find.byType(Container);
     expect(finder, findsNWidgets(2));
     expect(tester.getTopLeft(finder.at(0)), appTopLeft);
-    expect(
-        tester.getTopLeft(finder.at(1)), appTopLeft + const Offset(100, 100));
+    expect(tester.getTopLeft(finder.at(1)), appTopLeft + const Offset(100, 100));
   });
 }
