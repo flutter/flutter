c125e756c568a63c42f9402b5e632c194f55a226
Polina Cherkasova <polina.c@live.com>
2024-03-29 08:29:47 -0700

    Update update_packages_pins.dart

--- packages/flutter_tools/lib/src/update_packages_pins.dart
+++ packages/flutter_tools/lib/src/update_packages_pins.dart
@@ -23,4 +23,5 @@ const Map<String, String> kManuallyPinnedDependencies = <String, String>{
   'leak_tracker_flutter_testing': '3.0.5', // https://github.com/flutter/devtools/issues/3951
   'path_provider_android':
       '2.2.1', // https://github.com/flutter/flutter/issues/140796
+  'frontend_server_client': '3.2.0',
 };
