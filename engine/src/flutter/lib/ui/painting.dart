61573f8d69ce0d9056a6993faf7e546bbad8d58f
Matan Lurey <matanlurey@users.noreply.github.com>
2024-03-29 14:46:05 -0700

    Implement `Paint.from(other)` for `dart:ui`. (flutter/engine#51110)
    (Tenatively) Closes https://github.com/flutter/flutter/issues/142871.
    
    I personally think this is a reasonable request, and we (framework authors) can make some users happy in a fairly simple way (https://github.com/flutter/flutter/issues/142871, https://github.com/flutter/flutter/issues/40497).
    
    Some questions:
    
    1. Is the web implementation good enough or would we want to "ship" with an optimized impl?
    2. Can folks imagine other edge cases to test beyond correctness and deep/immutable copies?

--- engine/src/flutter/lib/ui/painting.dart
+++ engine/src/flutter/lib/ui/painting.dart
@@ -1092,6 +1092,22 @@ final class Paint {
   /// their defaults.
   Paint();
 
+  /// Constructs a new [Paint] object with the same fields as [other].
+  ///
+  /// Any changes made to the object returned will not affect [other], and
+  /// changes to [other] will not affect the object returned.
+  ///
+  /// Backends (for example web versus native) may have different performance
+  /// characteristics. If the code is performance-sensitive, consider profiling
+  /// and falling back to reusing a single [Paint] object if necessary.
+  Paint.from(Paint other) {
+    // Every field on Paint is deeply immutable, so to create a copy of a Paint
+    // object, we copy the underlying data buffer and the list of objects (which
+    // are also deeply immutable).
+    _data.buffer.asUint32List().setAll(0, other._data.buffer.asUint32List());
+    _objects = other._objects?.toList();
+  }
+
   // Paint objects are encoded in two buffers:
   //
   // * _data is binary data in four-byte fields, each of which is either a
