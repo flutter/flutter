37ddb7ff33928bbb87582ac2977ae0641d199df2
Jonah Williams <jonahwilliams@google.com>
2024-04-03 19:45:35 -0700

    [Impeller] dont inc shared_ptr count for every command/pipeline in render_pass_vk (flutter/engine#51874)
    We have to pay to inc/dec the shared_ptr, but all pipelines are persisted indefinitely so we shouldn't need to worry about dangling ptrs.

--- engine/src/flutter/impeller/renderer/backend/vulkan/render_pass_vk.h
+++ engine/src/flutter/impeller/renderer/backend/vulkan/render_pass_vk.h
@@ -49,7 +49,7 @@ class RenderPassVK final : public RenderPass {
   size_t vertex_count_ = 0u;
   bool has_index_buffer_ = false;
   bool has_label_ = false;
-  std::shared_ptr<Pipeline<PipelineDescriptor>> pipeline_;
+  const Pipeline<PipelineDescriptor>* pipeline_;
   bool pipeline_uses_input_attachments_ = false;
   std::shared_ptr<SamplerVK> immutable_sampler_;
 
