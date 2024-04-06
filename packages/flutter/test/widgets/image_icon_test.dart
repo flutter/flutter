c209e3550ee8184f9d0ef3bb7da4f73e9308abf1
Dimilkalathiya <kalathiyadimil@gmail.com>
2024-04-06 13:42:45 +0530

    removing addTearDown since we have teardownAll

--- packages/flutter/test/widgets/image_icon_test.dart
+++ packages/flutter/test/widgets/image_icon_test.dart
@@ -37,7 +37,6 @@ void main() async {
         child: ImageIcon(imageProvider),
       ),
     );
-    addTearDown(imageProvider.evict);
     final RenderBox renderObject = tester.renderObject(find.byType(ImageIcon));
     expect(renderObject.size, equals(const Size.square(24.0)));
     expect(find.byType(Image), findsOneWidget);
@@ -53,7 +52,6 @@ void main() async {
         ),
       ),
     );
-    addTearDown(imageProvider.evict);
     expect(tester.widget<Image>(find.byType(Image)).color!.alpha, equals(128));
   });
 
