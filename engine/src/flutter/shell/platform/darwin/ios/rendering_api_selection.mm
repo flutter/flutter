6ff8c03a4bc2e60bad8efa410cb32b939e176a15
Jenn Magder <magder@google.com>
2024-04-08 15:25:01 -0700

    Remove #if SHELL_ENABLE_METAL checks in darwin code (flutter/engine#51947)
    Same as https://github.com/flutter/engine/pull/51636 except:
    
    1. The original change caused scuba changes because those simulators are running on Macs without GPU passthrough, and so fallback to software rendering when `MTLCreateSystemDefaultDevice()` fails.  Re-add that `MTLCreateSystemDefaultDevice` check
    
    https://github.com/flutter/engine/pull/51636/files#diff-1db60a8a74c1cfb40c970541dd1f7f0f6301853a2ba17451f5117f154036d732L22-L31
    
    2. Set `enable_metal` to always true for `macos/BUILD.gn`
     
    [C++, Objective-C, Java style guides]: https://github.com/flutter/engine/blob/main/CONTRIBUTING.md#style

--- engine/src/flutter/shell/platform/darwin/ios/rendering_api_selection.mm
+++ engine/src/flutter/shell/platform/darwin/ios/rendering_api_selection.mm
@@ -5,11 +5,9 @@
 #import "flutter/shell/platform/darwin/ios/rendering_api_selection.h"
 
 #include <Foundation/Foundation.h>
+#include <Metal/Metal.h>
 #include <QuartzCore/CAEAGLLayer.h>
 #import <QuartzCore/CAMetalLayer.h>
-#if SHELL_ENABLE_METAL
-#include <Metal/Metal.h>
-#endif  // SHELL_ENABLE_METAL
 #import <TargetConditionals.h>
 
 #include "flutter/fml/logging.h"
@@ -18,7 +16,6 @@
 
 namespace flutter {
 
-#if SHELL_ENABLE_METAL
 bool ShouldUseMetalRenderer() {
   bool ios_version_supports_metal = false;
   if (@available(iOS METAL_IOS_VERSION_BASELINE, *)) {
@@ -28,7 +25,6 @@ bool ShouldUseMetalRenderer() {
   }
   return ios_version_supports_metal;
 }
-#endif  // SHELL_ENABLE_METAL
 
 IOSRenderingAPI GetRenderingAPIForProcess(bool force_software) {
 #if TARGET_OS_SIMULATOR
@@ -42,12 +38,10 @@ IOSRenderingAPI GetRenderingAPIForProcess(bool force_software) {
   }
 #endif  // TARGET_OS_SIMULATOR
 
-#if SHELL_ENABLE_METAL
   static bool should_use_metal = ShouldUseMetalRenderer();
   if (should_use_metal) {
     return IOSRenderingAPI::kMetal;
   }
-#endif  // SHELL_ENABLE_METAL
 
   // When Metal isn't available we use Skia software rendering since it performs
   // a little better than emulated OpenGL. Also, omitting an OpenGL backend
