d7b7c5ba4ecdc3eb70e8b98cf158c384870aa331
Jonah Williams <jonahwilliams@google.com>
2024-04-10 12:43:23 -0700

    [Android] post image task to main handler. (#52025)
    Fixes https://github.com/flutter/flutter/issues/146465

--- shell/platform/android/io/flutter/embedding/engine/renderer/FlutterRenderer.java
+++ shell/platform/android/io/flutter/embedding/engine/renderer/FlutterRenderer.java
@@ -17,6 +17,7 @@ import android.media.Image;
 import android.media.ImageReader;
 import android.os.Build;
 import android.os.Handler;
+import android.os.Looper;
 import android.view.Surface;
 import androidx.annotation.Keep;
 import androidx.annotation.NonNull;
@@ -484,7 +485,8 @@ public class FlutterRenderer implements TextureRegistry {
 
       public PerImageReader(ImageReader reader) {
         this.reader = reader;
-        reader.setOnImageAvailableListener(onImageAvailableListener, new Handler());
+        reader.setOnImageAvailableListener(
+            onImageAvailableListener, new Handler(Looper.getMainLooper()));
       }
 
       PerImage queueImage(Image image) {
