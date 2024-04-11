3fb3bd5417186ade63f99e7505842dad371d5224
Godofredo Contreras <godofredoc@google.com>
2024-04-11 16:36:05 -0700

    Fix one more driver test.

--- dev/integration_tests/flutter_gallery/test_driver/transitions_perf_e2e_test.dart
+++ dev/integration_tests/flutter_gallery/test_driver/transitions_perf_e2e_test.dart
@@ -2,13 +2,19 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'package:flutter_driver/flutter_driver.dart';
 import 'package:integration_test/integration_test_driver.dart' as driver;
 
-Future<void> main() => driver.integrationDriver(
-  responseDataCallback: (Map<String, dynamic>? data, {String? testOutputDirectory, String? testOutputFilename}) async {
-    await driver.writeResponseData(
-      data!['performance'] as Map<String, dynamic>,
-      testOutputFilename: 'e2e_perf_summary',
-    );
-  }
-);
+Future<void> main(List<String> args) async {
+  final String testOutputDirectory = getTestOutputDirectory(args);
+  driver.integrationDriver(
+    responseDataCallback: (Map<String, dynamic>? data, {String? testOutputDirectory, String? testOutputFilename}) async {
+      await driver.writeResponseData(
+        data!['performance'] as Map<String, dynamic>,
+        testOutputFilename: 'e2e_perf_summary',
+        testOutputDirectory: testOutputDirectory,
+      );
+    },
+    testOutputDirectory: testOutputDirectory,
+  );
+}
