6c7a79843979192ea15e981db27bee3ae30342a2
yaakovschectman <109111084+yaakovschectman@users.noreply.github.com>
2024-04-11 10:25:27 -0400

    Composite multiple layers in Windows software rendering (flutter/engine#51759)
    Blend pixels per-alpha when presenting multiple layers from the software
    compositor.
    
    Part of https://github.com/flutter/flutter/issues/143375
    
    ## Pre-launch Checklist
    
    - [x] I read the [Contributor Guide] and followed the process outlined
    there for submitting PRs.
    - [x] I read the [Tree Hygiene] wiki page, which explains my
    responsibilities.
    - [x] I read and followed the [Flutter Style Guide] and the [C++,
    Objective-C, Java style guides].
    - [x] I listed at least one issue that this PR fixes in the description
    above.
    - [x] I added new tests to check the change I am making or feature I am
    adding, or the PR is [test-exempt]. See [testing the engine] for
    instructions on writing and running engine tests.
    - [x] I updated/added relevant documentation (doc comments with `///`).
    - [ ] I signed the [CLA].
    - [x] All existing and new tests are passing.
    
    If you need help, consider asking for advice on the #hackers-new channel
    on [Discord].
    
    <!-- Links -->
    [Contributor Guide]:
    https://github.com/flutter/flutter/wiki/Tree-hygiene#overview
    [Tree Hygiene]: https://github.com/flutter/flutter/wiki/Tree-hygiene
    [test-exempt]:
    https://github.com/flutter/flutter/wiki/Tree-hygiene#tests
    [Flutter Style Guide]:
    https://github.com/flutter/flutter/wiki/Style-guide-for-Flutter-repo
    [C++, Objective-C, Java style guides]:
    https://github.com/flutter/engine/blob/main/CONTRIBUTING.md#style
    [testing the engine]:
    https://github.com/flutter/flutter/wiki/Testing-the-engine
    [CLA]: https://cla.developers.google.com/
    [flutter/tests]: https://github.com/flutter/tests
    [breaking change policy]:
    https://github.com/flutter/flutter/wiki/Tree-hygiene#handling-breaking-changes
    [Discord]: https://github.com/flutter/flutter/wiki/Chat
    
    ---------
    
    Co-authored-by: Loïc Sharma <737941+loic-sharma@users.noreply.github.com>

--- engine/src/flutter/shell/platform/windows/compositor_software_unittests.cc
+++ engine/src/flutter/shell/platform/windows/compositor_software_unittests.cc
@@ -107,5 +107,114 @@ TEST_F(CompositorSoftwareTest, PresentEmpty) {
   EXPECT_TRUE(compositor.Present(view(), nullptr, 0));
 }
 
+// Test compositing an upper layer on a base layer, each 2x2 pixels.
+// Base layer has opaque pixel values:
+// BLACK  RED
+// GREEN  WHITE
+// Overlay layer has pixel values:
+// RED: 127   WHITE: 0
+// BLUE: 127  BLACK: 255
+TEST_F(CompositorSoftwareTest, PresentMultiLayers) {
+  UseEngineWithView();
+
+  CompositorSoftware compositor;
+
+  FlutterBackingStoreConfig config = {sizeof(config), {2, 2}};
+  FlutterBackingStore backing_store0 = {sizeof(FlutterBackingStore), nullptr};
+  FlutterBackingStore backing_store1 = {sizeof(FlutterBackingStore), nullptr};
+
+  ASSERT_TRUE(compositor.CreateBackingStore(config, &backing_store0));
+  ASSERT_TRUE(compositor.CreateBackingStore(config, &backing_store1));
+
+  uint32_t pixels0[4] = {0xff000000, 0xff0000ff, 0xff00ff00, 0xffffffff};
+  uint32_t pixels1[4] = {0x7f0000ff, 0x00ffffff, 0x7fff0000, 0xff000000};
+
+  std::memcpy(const_cast<void*>(backing_store0.software.allocation), pixels0,
+              sizeof(uint32_t) * 4);
+  std::memcpy(const_cast<void*>(backing_store1.software.allocation), pixels1,
+              sizeof(uint32_t) * 4);
+
+  FlutterLayer layer0 = {};
+  layer0.type = kFlutterLayerContentTypeBackingStore;
+  layer0.backing_store = &backing_store0;
+  layer0.offset = {0, 0};
+  layer0.size = {2, 2};
+
+  FlutterLayer layer1 = layer0;
+  layer1.backing_store = &backing_store1;
+  const FlutterLayer* layer_ptr[2] = {&layer0, &layer1};
+
+  EXPECT_CALL(*view(), PresentSoftwareBitmap)
+      .WillOnce([&](const void* allocation, size_t row_bytes, size_t height) {
+        auto pixel_data = static_cast<const uint32_t*>(allocation);
+        EXPECT_EQ(row_bytes, 2 * sizeof(uint32_t));
+        EXPECT_EQ(height, 2);
+        EXPECT_EQ(pixel_data[0], 0xff00007f);
+        EXPECT_EQ(pixel_data[1], 0xff0000ff);
+        EXPECT_EQ(pixel_data[2], 0xff7f8000);
+        EXPECT_EQ(pixel_data[3], 0xff000000);
+        return true;
+      });
+  EXPECT_TRUE(compositor.Present(view(), layer_ptr, 2));
+
+  ASSERT_TRUE(compositor.CollectBackingStore(&backing_store0));
+  ASSERT_TRUE(compositor.CollectBackingStore(&backing_store1));
+}
+
+// Test compositing layers with offsets.
+// 0th layer is a single red pixel in the top-left.
+// 1st layer is a row of two blue pixels on the second row.
+TEST_F(CompositorSoftwareTest, PresentOffsetLayers) {
+  UseEngineWithView();
+
+  CompositorSoftware compositor;
+
+  FlutterBackingStoreConfig config0 = {sizeof(FlutterBackingStoreConfig),
+                                       {1, 1}};
+  FlutterBackingStore backing_store0 = {sizeof(FlutterBackingStore), nullptr};
+  FlutterBackingStoreConfig config1 = {sizeof(FlutterBackingStoreConfig),
+                                       {2, 1}};
+  FlutterBackingStore backing_store1 = {sizeof(FlutterBackingStore), nullptr};
+
+  ASSERT_TRUE(compositor.CreateBackingStore(config0, &backing_store0));
+  ASSERT_TRUE(compositor.CreateBackingStore(config1, &backing_store1));
+
+  uint32_t pixels0 = 0xff0000ff;
+  uint32_t pixels1[2] = {0xffff0000, 0xffff0000};
+
+  std::memcpy(const_cast<void*>(backing_store0.software.allocation), &pixels0,
+              sizeof(uint32_t) * 1);
+  std::memcpy(const_cast<void*>(backing_store1.software.allocation), pixels1,
+              sizeof(uint32_t) * 2);
+
+  FlutterLayer layer0 = {};
+  layer0.type = kFlutterLayerContentTypeBackingStore;
+  layer0.backing_store = &backing_store0;
+  layer0.offset = {0, 0};
+  layer0.size = {1, 1};
+
+  FlutterLayer layer1 = layer0;
+  layer1.backing_store = &backing_store1;
+  layer1.offset = {0, 1};
+  layer1.size = {2, 1};
+  const FlutterLayer* layer_ptr[2] = {&layer0, &layer1};
+
+  EXPECT_CALL(*view(), PresentSoftwareBitmap)
+      .WillOnce([&](const void* allocation, size_t row_bytes, size_t height) {
+        auto pixel_data = static_cast<const uint32_t*>(allocation);
+        EXPECT_EQ(row_bytes, 2 * sizeof(uint32_t));
+        EXPECT_EQ(height, 2);
+        EXPECT_EQ(pixel_data[0], 0xff0000ff);
+        EXPECT_EQ(pixel_data[1], 0xff000000);
+        EXPECT_EQ(pixel_data[2], 0xffff0000);
+        EXPECT_EQ(pixel_data[3], 0xffff0000);
+        return true;
+      });
+  EXPECT_TRUE(compositor.Present(view(), layer_ptr, 2));
+
+  ASSERT_TRUE(compositor.CollectBackingStore(&backing_store0));
+  ASSERT_TRUE(compositor.CollectBackingStore(&backing_store1));
+}
+
 }  // namespace testing
 }  // namespace flutter
