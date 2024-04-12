ee7c09551726ff5305610220d472971a5f5c0c30
Godofredo Contreras <godofredoc@google.com>
2024-04-11 17:49:01 -0700

    Fix another driver.

--- packages/integration_test/example/test_driver/integration_test.dart
+++ packages/integration_test/example/test_driver/integration_test.dart
@@ -2,6 +2,10 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'package:flutter_driver/flutter_driver.dart';
 import 'package:integration_test/integration_test_driver.dart';
 
-Future<void> main() => integrationDriver(writeResponseOnFailure: true);
+Future<void> main(List<String> args) async {
+  final String testOutputDirectory = getTestOutputDirectory(args);
+  integrationDriver(writeResponseOnFailure: true, testOutputDirectory: testOutputDirectory);
+}
