3a0216538104378c8a0271a22019229bb6d4230b
Kostia Sokolovskyi <sokolovskyi.konstantin@gmail.com>
2024-04-13 15:01:51 +0200

    Add test for animated_fractionally_sized_box.0.dart API example.

--- dev/bots/check_code_samples.dart
+++ dev/bots/check_code_samples.dart
@@ -467,7 +467,6 @@ final Set<String> _knownMissingTests = <String>{
   'examples/api/test/widgets/focus_scope/focus.0_test.dart',
   'examples/api/test/widgets/focus_scope/focus.1_test.dart',
   'examples/api/test/widgets/focus_scope/focus_scope.0_test.dart',
-  'examples/api/test/widgets/implicit_animations/animated_fractionally_sized_box.0_test.dart',
   'examples/api/test/widgets/scroll_view/custom_scroll_view.1_test.dart',
   'examples/api/test/widgets/inherited_notifier/inherited_notifier.0_test.dart',
   'examples/api/test/animation/curves/curve2_d.0_test.dart',
