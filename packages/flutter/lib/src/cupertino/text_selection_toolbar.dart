30232881d5dfe41bff7bb6ef2dc451e1aa3bb378
Polina Cherkasova <polina.c@live.com>
2024-04-14 13:41:44 -0700

    Update text_selection_toolbar.dart

--- packages/flutter/lib/src/cupertino/text_selection_toolbar.dart
+++ packages/flutter/lib/src/cupertino/text_selection_toolbar.dart
@@ -845,8 +845,8 @@ class _CupertinoTextSelectionToolbarItemsElement extends RenderObjectElement {
   @override
   void visitChildren(ElementVisitor visitor) {
     slotToChild.values.forEach(visitor);
-    for (final Object child in _children) {
-      if (child is Element && !_forgottenChildren.contains(child)) {
+    for (final Element child in _children) {
+      if (!_forgottenChildren.contains(child)) {
         visitor(child);
       }
     }
@@ -887,12 +887,13 @@ class _CupertinoTextSelectionToolbarItemsElement extends RenderObjectElement {
     _mountChild(toolbarItems.nextButton, _CupertinoTextSelectionToolbarItemsSlot.nextButton);
 
     // Mount list children.
+    _children = List<Element>.filled(toolbarItems.children.length, _NullElement.instance);
     Element? previousChild;
-    _children = List<Element>.generate(toolbarItems.children.length, (int i) {
-      final Element result = inflateWidget(toolbarItems.children[i], IndexedSlot<Element?>(i, previousChild));
-      previousChild = result;
-      return result;
-    }, growable: false);
+    for (int i = 0; i < _children.length; i += 1) {
+      final Element newChild = inflateWidget(toolbarItems.children[i], IndexedSlot<Element?>(i, previousChild));
+      _children[i] = newChild;
+      previousChild = newChild;
+    }
   }
 
   @override
@@ -905,8 +906,7 @@ class _CupertinoTextSelectionToolbarItemsElement extends RenderObjectElement {
     }
     // Visit list children.
     _children
-        .where((Object child) => child is Element && !_forgottenChildren.contains(child) && _shouldPaint(child))
-        .map((Object child) => child as Element)
+        .where((Element child) => !_forgottenChildren.contains(child) && _shouldPaint(child))
         .forEach(visitor);
   }
 
@@ -1274,4 +1274,18 @@ enum _CupertinoTextSelectionToolbarItemsSlot {
   nextButton,
 }
 
-class _NullElement {}
+class _NullElement extends Element {
+  _NullElement() : super(const _NullWidget());
+
+  static _NullElement instance = _NullElement();
+
+  @override
+  bool get debugDoingBuild => throw UnimplementedError();
+}
+
+class _NullWidget extends Widget {
+  const _NullWidget();
+
+  @override
+  Element createElement() => throw UnimplementedError();
+}
