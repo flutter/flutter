03e4268c37b1a318ea08260ee6bbf16570cd9485
Tim Lehmenn <hi@timcreated.it>
2024-04-14 14:03:31 +0200

    removed unneccessary import

--- packages/flutter/test/widgets/draggable_test.dart
+++ packages/flutter/test/widgets/draggable_test.dart
@@ -13,7 +13,6 @@ import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'semantics_tester.dart';
