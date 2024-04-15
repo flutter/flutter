3b36ccf5d9b93bdf718a9aa36d2b95e5eb258ff2
Ian Hickson <ian@hixie.ch>
2024-04-15 16:05:41 -0700

    Update update_packages_pins.dart

--- packages/flutter_tools/lib/src/update_packages_pins.dart
+++ packages/flutter_tools/lib/src/update_packages_pins.dart
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// Th constant below is in its own library so that the test exemption bot knows
+// The constant below is in its own library so that the test exemption bot knows
 // that changing a pin does not require a new test. These pins are already
 // tested as part of the analysis shard.
 
