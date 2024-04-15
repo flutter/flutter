31168ca17944a13943dbc6c95f210e1848bb7fcc
Polina Cherkasova <polina.c@live.com>
2024-04-15 09:09:46 -0700

    Update tabs_test.dart

--- packages/flutter/test/material/tabs_test.dart
+++ packages/flutter/test/material/tabs_test.dart
@@ -7,7 +7,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:leak_tracker_flutter_testing/leak_tracker_flutter_testing.dart';
 
 import '../widgets/semantics_tester.dart';
 import 'feedback_tester.dart';
