4609c4240611d2c4739552e3ea919d25bcdb11fb
Mairramer <mairramer.dasilva28@hotmail.com>
2024-04-16 15:22:00 -0300

    fix

--- packages/flutter/test/widgets/form_test.dart
+++ packages/flutter/test/widgets/form_test.dart
@@ -1240,7 +1240,7 @@ void main() {
     await tester.enterText(find.byType(TextFormField).first, 'foo');
     await tester.pumpAndSettle();
 
-    // Click in the second TextFormField to trigger validation
+    // Click in the second TextFormField to trigger validation.
     await tester.tap(find.byType(TextFormField).last);
     await tester.pumpAndSettle();
 
