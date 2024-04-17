bde9ca60bfe8f82a645be5d13f911188db277211
Jaivik22 <thisisjaivik@gmail.com>
2024-04-17 17:21:16 +0530

    dwwwxd

--- packages/flutter_tools/bin/tool_backend.bat
+++
@@ -1,9 +0,0 @@
-@ECHO off
-REM Copyright 2014 The Flutter Authors. All rights reserved.
-REM Use of this source code is governed by a BSD-style license that can be
-REM found in the LICENSE file.
-
-set FLUTTER_BIN_DIR=%FLUTTER_ROOT%\bin
-set DART_BIN_DIR=%FLUTTER_BIN_DIR%\cache\dart-sdk\bin
-
-"%DART_BIN_DIR%\dart" "%FLUTTER_ROOT%\packages\flutter_tools\bin\tool_backend.dart" %*
