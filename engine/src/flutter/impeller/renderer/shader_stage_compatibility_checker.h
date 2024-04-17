60c631540323811adb5cbf3b6f9d041c10b8a383
gaaclarke <30870216+gaaclarke@users.noreply.github.com>
2024-04-17 08:18:40 -0700

    [Impeller] added static check that fragment shader and vertex shaders slots match (flutter/engine#52174)
    fixes https://github.com/flutter/flutter/issues/146452
    
    ## Pre-launch Checklist
    
    - [x] I read the [Contributor Guide] and followed the process outlined
    there for submitting PRs.
    - [x] I read the [Tree Hygiene] wiki page, which explains my
    responsibilities.
    - [x] I read and followed the [Flutter Style Guide] and the [C++,
    Objective-C, Java style guides].
    - [x] I listed at least one issue that this PR fixes in the description
    above.
    - [x] I added new tests to check the change I am making or feature I am
    adding, or the PR is [test-exempt]. See [testing the engine] for
    instructions on writing and running engine tests.
    - [x] I updated/added relevant documentation (doc comments with `///`).
    - [x] I signed the [CLA].
    - [x] All existing and new tests are passing.
    
    If you need help, consider asking for advice on the #hackers-new channel
    on [Discord].
    
    <!-- Links -->
    [Contributor Guide]:
    https://github.com/flutter/flutter/wiki/Tree-hygiene#overview
    [Tree Hygiene]: https://github.com/flutter/flutter/wiki/Tree-hygiene
    [test-exempt]:
    https://github.com/flutter/flutter/wiki/Tree-hygiene#tests
    [Flutter Style Guide]:
    https://github.com/flutter/flutter/wiki/Style-guide-for-Flutter-repo
    [C++, Objective-C, Java style guides]:
    https://github.com/flutter/engine/blob/main/CONTRIBUTING.md#style
    [testing the engine]:
    https://github.com/flutter/flutter/wiki/Testing-the-engine
    [CLA]: https://cla.developers.google.com/
    [flutter/tests]: https://github.com/flutter/tests
    [breaking change policy]:
    https://github.com/flutter/flutter/wiki/Tree-hygiene#handling-breaking-changes
    [Discord]: https://github.com/flutter/flutter/wiki/Chat

---
+++ engine/src/flutter/impeller/renderer/shader_stage_compatibility_checker.h
@@ -0,0 +1,76 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef FLUTTER_IMPELLER_RENDERER_SHADER_STAGE_COMPATIBILITY_CHECKER_H_
+#define FLUTTER_IMPELLER_RENDERER_SHADER_STAGE_COMPATIBILITY_CHECKER_H_
+
+namespace impeller {
+/// This is a classed use to check that the input slots of fragment shaders
+/// match the output slots of the vertex shaders.
+/// If they don't match it will result in linker errors when creating the
+/// pipeline.  It's not used at runtime.
+template <typename VertexShaderT, typename FragmentShaderT>
+class ShaderStageCompatibilityChecker {
+ public:
+  static constexpr bool CompileTimeStrEqual(const char* str1,
+                                            const char* str2) {
+    return *str1 == *str2 &&
+           (*str1 == '\0' || CompileTimeStrEqual(str1 + 1, str2 + 1));
+  }
+
+  /// Returns `true` if the shader input slots for the fragment shader match the
+  /// ones declared as outputs in the vertex shader.
+  static constexpr bool Check() {
+    constexpr size_t num_outputs = VertexShaderT::kAllShaderStageOutputs.size();
+    constexpr size_t num_inputs = FragmentShaderT::kAllShaderStageInputs.size();
+
+    if (num_inputs > num_outputs) {
+      return false;
+    }
+
+    for (size_t i = 0; i < num_inputs; ++i) {
+      const ShaderStageIOSlot* input_slot =
+          FragmentShaderT::kAllShaderStageInputs[i];
+      for (size_t j = 0; j < num_outputs; ++j) {
+        const ShaderStageIOSlot* output_slot =
+            VertexShaderT::kAllShaderStageOutputs[j];
+        if (input_slot->location == output_slot->location) {
+          if (!CompileTimeStrEqual(input_slot->name, output_slot->name) ||
+              input_slot->set != output_slot->set ||
+              input_slot->binding != output_slot->binding ||
+              input_slot->type != output_slot->type ||
+              input_slot->bit_width != output_slot->bit_width ||
+              input_slot->vec_size != output_slot->vec_size ||
+              input_slot->columns != output_slot->columns ||
+              input_slot->offset != output_slot->offset) {
+            return false;
+          }
+        }
+      }
+    }
+
+    return true;
+  }
+};
+
+// The following shaders don't define output slots.
+// TODO(https://github.com/flutter/flutter/issues/146852): Make impellerc emit
+// an empty array for output slots.
+struct CheckerboardVertexShader;
+struct ClipVertexShader;
+
+template <typename FragmentShaderT>
+class ShaderStageCompatibilityChecker<CheckerboardVertexShader,
+                                      FragmentShaderT> {
+ public:
+  static constexpr bool Check() { return true; }
+};
+
+template <typename FragmentShaderT>
+class ShaderStageCompatibilityChecker<ClipVertexShader, FragmentShaderT> {
+ public:
+  static constexpr bool Check() { return true; }
+};
+}  // namespace impeller
+#endif  // FLUTTER_IMPELLER_RENDERER_SHADER_STAGE_COMPATIBILITY_CHECKER_H_
