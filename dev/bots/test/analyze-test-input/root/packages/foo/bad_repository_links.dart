5a951bf2f11362e03dce00b20f0dcb1cd04e2b1f
Pierre-Louis Guidez <plg@google.com>
2024-04-18 15:41:50 +0200

    add missing license

--- dev/bots/test/analyze-test-input/root/packages/foo/bad_repository_links.dart
+++ dev/bots/test/analyze-test-input/root/packages/foo/bad_repository_links.dart
@@ -1,3 +1,7 @@
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 // Check out https://android.googlesource.com/+/master/file1
 // Check out https://chromium.googlesource.com/+/master/file1
 // Check out https://cs.opensource.google.com/+/master/file1
