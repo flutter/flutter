0b287631d358ed590738d7c1ce442c438dae0fc5
Amir Panahandeh <amirpanahandeh@yahoo.com>
2024-04-19 12:09:09 +0330

    Update packages/flutter/lib/src/widgets/scrollable.dart
    Co-authored-by: Kate Lovett <katelovett@google.com>

--- packages/flutter/lib/src/widgets/scrollable.dart
+++ packages/flutter/lib/src/widgets/scrollable.dart
@@ -231,7 +231,6 @@ class Scrollable extends StatefulWidget {
   ///
   /// This defaults to [HitTestBehavior.opaque] which means it prevents targets
   /// behind this [Scrollable] from receiving events.
-  ///
   /// {@endtemplate}
   ///
   /// See also:
