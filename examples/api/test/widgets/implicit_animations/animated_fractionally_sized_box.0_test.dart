88e1030af9d01c389f0e1a1701a16c2c3cb40b31
Kostia Sokolovskyi <sokolovskyi.konstantin@gmail.com>
2024-04-20 14:24:08 +0200

    Apply review suggestions.

--- examples/api/test/widgets/implicit_animations/animated_fractionally_sized_box.0_test.dart
+++ examples/api/test/widgets/implicit_animations/animated_fractionally_sized_box.0_test.dart
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_api_samples/widgets/implicit_animations/animated_fractionally_sized_box.0.dart'
     as example;
@@ -15,42 +17,109 @@ void main() {
         const example.AnimatedFractionallySizedBoxExampleApp(),
       );
 
-      FractionallySizedBox fractionallySizedBox = tester.widget(
-        find.descendant(
-          of: find.byType(AnimatedFractionallySizedBox),
-          matching: find.byType(FractionallySizedBox),
-        ),
+      final Finder fractionallySizedBoxFinder = find.descendant(
+        of: find.byType(AnimatedFractionallySizedBox),
+        matching: find.byType(FractionallySizedBox),
       );
-      expect(fractionallySizedBox.widthFactor, 0.75);
-      expect(fractionallySizedBox.heightFactor, 0.25);
-      expect(fractionallySizedBox.alignment, Alignment.bottomRight);
 
-      await tester.tap(find.byType(FractionallySizedBox));
+      const double beginWidthFactor = 0.75;
+      const double endWidthFactor = 0.25;
+      const double beginHeightFactor = 0.25;
+      const double endHeightFactor = 0.75;
+      const Alignment beginAlignment = Alignment.bottomRight;
+      const Alignment endAlignment = Alignment.topLeft;
+
+      FractionallySizedBox fractionallySizedBox = tester.widget(
+        fractionallySizedBoxFinder,
+      );
+      expect(fractionallySizedBox.widthFactor, beginWidthFactor);
+      expect(fractionallySizedBox.heightFactor, beginHeightFactor);
+      expect(fractionallySizedBox.alignment, beginAlignment);
+
+      // Tap on the AnimatedFractionallySizedBoxExample to start the forward
+      // animation.
+      await tester.tap(
+        find.byType(example.AnimatedFractionallySizedBoxExample),
+      );
       await tester.pump();
 
-      fractionallySizedBox = tester.widget(
-        find.descendant(
-          of: find.byType(AnimatedFractionallySizedBox),
-          matching: find.byType(FractionallySizedBox),
-        ),
-      );
-      expect(fractionallySizedBox.widthFactor, 0.75);
-      expect(fractionallySizedBox.heightFactor, 0.25);
-      expect(fractionallySizedBox.alignment, Alignment.bottomRight);
+      fractionallySizedBox = tester.widget(fractionallySizedBoxFinder);
+      expect(fractionallySizedBox.widthFactor, beginWidthFactor);
+      expect(fractionallySizedBox.heightFactor, beginHeightFactor);
+      expect(fractionallySizedBox.alignment, beginAlignment);
 
-      // Advance animation to the end by the 1-second duration specified in
-      // the example app.
-      await tester.pump(const Duration(seconds: 1));
-
-      fractionallySizedBox = tester.widget(
-        find.descendant(
-          of: find.byType(AnimatedFractionallySizedBox),
-          matching: find.byType(FractionallySizedBox),
-        ),
+      // Advance animation to the middle.
+      await tester.pump(
+        example.AnimatedFractionallySizedBoxExampleApp.duration ~/ 2,
       );
-      expect(fractionallySizedBox.widthFactor, 0.25);
-      expect(fractionallySizedBox.heightFactor, 0.75);
-      expect(fractionallySizedBox.alignment, Alignment.topLeft);
+
+      final double t =
+          example.AnimatedFractionallySizedBoxExampleApp.curve.transform(0.5);
+
+      fractionallySizedBox = tester.widget(fractionallySizedBoxFinder);
+      expect(
+        fractionallySizedBox.widthFactor,
+        lerpDouble(beginWidthFactor, endWidthFactor, t),
+      );
+      expect(
+        fractionallySizedBox.heightFactor,
+        lerpDouble(beginHeightFactor, endHeightFactor, t),
+      );
+      expect(
+        fractionallySizedBox.alignment,
+        Alignment.lerp(beginAlignment, endAlignment, t),
+      );
+
+      // Advance animation to the end.
+      await tester.pump(
+        example.AnimatedFractionallySizedBoxExampleApp.duration ~/ 2,
+      );
+
+      fractionallySizedBox = tester.widget(fractionallySizedBoxFinder);
+      expect(fractionallySizedBox.widthFactor, endWidthFactor);
+      expect(fractionallySizedBox.heightFactor, endHeightFactor);
+      expect(fractionallySizedBox.alignment, endAlignment);
+
+      // Tap on the AnimatedFractionallySizedBoxExample again to start the
+      // reverse animation.
+      await tester.tap(
+        find.byType(example.AnimatedFractionallySizedBoxExample),
+      );
+      await tester.pump();
+
+      fractionallySizedBox = tester.widget(fractionallySizedBoxFinder);
+      expect(fractionallySizedBox.widthFactor, endWidthFactor);
+      expect(fractionallySizedBox.heightFactor, endHeightFactor);
+      expect(fractionallySizedBox.alignment, endAlignment);
+
+      // Advance animation to the middle.
+      await tester.pump(
+        example.AnimatedFractionallySizedBoxExampleApp.duration ~/ 2,
+      );
+
+      fractionallySizedBox = tester.widget(fractionallySizedBoxFinder);
+      expect(
+        fractionallySizedBox.widthFactor,
+        lerpDouble(endWidthFactor, beginWidthFactor, t),
+      );
+      expect(
+        fractionallySizedBox.heightFactor,
+        lerpDouble(endHeightFactor, beginHeightFactor, t),
+      );
+      expect(
+        fractionallySizedBox.alignment,
+        Alignment.lerp(endAlignment, beginAlignment, t),
+      );
+
+      // Advance animation to the end.
+      await tester.pump(
+        example.AnimatedFractionallySizedBoxExampleApp.duration ~/ 2,
+      );
+
+      fractionallySizedBox = tester.widget(fractionallySizedBoxFinder);
+      expect(fractionallySizedBox.widthFactor, beginWidthFactor);
+      expect(fractionallySizedBox.heightFactor, beginHeightFactor);
+      expect(fractionallySizedBox.alignment, beginAlignment);
     },
   );
 }
