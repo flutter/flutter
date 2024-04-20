3197439fe1ffeb6e1ca08222f72747dff57c6db5
Kostia Sokolovskyi <sokolovskyi.konstantin@gmail.com>
2024-04-20 14:11:06 +0200

    Apply review suggestions.

--- examples/api/test/widgets/implicit_animations/animated_positioned.0_test.dart
+++ examples/api/test/widgets/implicit_animations/animated_positioned.0_test.dart
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_api_samples/widgets/implicit_animations/animated_positioned.0.dart'
     as example;
@@ -15,42 +17,75 @@ void main() {
         const example.AnimatedPositionedExampleApp(),
       );
 
-      Positioned positioned = tester.widget(
-        find.descendant(
-          of: find.byType(AnimatedPositioned),
-          matching: find.byType(Positioned),
-        ),
+      final Finder positionedFinder = find.descendant(
+        of: find.byType(AnimatedPositioned),
+        matching: find.byType(Positioned),
       );
-      expect(positioned.width, 50.0);
-      expect(positioned.height, 200.0);
-      expect(positioned.top, 150.0);
 
+      const double beginWidth = 50.0;
+      const double endWidth = 200.0;
+      const double beginHeight = 200.0;
+      const double endHeight = 50.0;
+      const double beginTop = 150.0;
+      const double endTop = 50.0;
+
+      Positioned positioned = tester.widget(positionedFinder);
+      expect(positioned.width, beginWidth);
+      expect(positioned.height, beginHeight);
+      expect(positioned.top, beginTop);
+
+      // Tap on the 'Tap me' text to start the forward animation.
       await tester.tap(find.text('Tap me'));
       await tester.pump();
 
-      positioned = tester.widget(
-        find.descendant(
-          of: find.byType(AnimatedPositioned),
-          matching: find.byType(Positioned),
-        ),
-      );
-      expect(positioned.width, 50.0);
-      expect(positioned.height, 200.0);
-      expect(positioned.top, 150.0);
+      positioned = tester.widget(positionedFinder);
+      expect(positioned.width, beginWidth);
+      expect(positioned.height, beginHeight);
+      expect(positioned.top, beginTop);
 
-      // Advance animation to the end by the 2-second duration specified in
-      // the example app.
-      await tester.pump(const Duration(seconds: 2));
+      // Advance animation to the middle.
+      await tester.pump(example.AnimatedPositionedExampleApp.duration ~/ 2);
 
-      positioned = tester.widget(
-        find.descendant(
-          of: find.byType(AnimatedPositioned),
-          matching: find.byType(Positioned),
-        ),
-      );
-      expect(positioned.width, 200.0);
-      expect(positioned.height, 50.0);
-      expect(positioned.top, 50.0);
+      final double t =
+          example.AnimatedPositionedExampleApp.curve.transform(0.5);
+
+      positioned = tester.widget(positionedFinder);
+      expect(positioned.width, lerpDouble(beginWidth, endWidth, t));
+      expect(positioned.height, lerpDouble(beginHeight, endHeight, t));
+      expect(positioned.top, lerpDouble(beginTop, endTop, t));
+
+      // Advance animation to the end.
+      await tester.pump(example.AnimatedPositionedExampleApp.duration ~/ 2);
+
+      positioned = tester.widget(positionedFinder);
+      expect(positioned.width, endWidth);
+      expect(positioned.height, endHeight);
+      expect(positioned.top, endTop);
+
+      // Tap on the 'Tap me' text again to start the reverse animation.
+      await tester.tap(find.text('Tap me'));
+      await tester.pump();
+
+      positioned = tester.widget(positionedFinder);
+      expect(positioned.width, endWidth);
+      expect(positioned.height, endHeight);
+      expect(positioned.top, endTop);
+
+      // Advance animation to the middle.
+      await tester.pump(example.AnimatedPositionedExampleApp.duration ~/ 2);
+
+      positioned = tester.widget(positionedFinder);
+      expect(positioned.width, lerpDouble(endWidth, beginWidth, t));
+      expect(positioned.height, lerpDouble(endHeight, beginHeight, t));
+      expect(positioned.top, lerpDouble(endTop, beginTop, t));
+
+      // Advance animation to the end.
+      await tester.pump(example.AnimatedPositionedExampleApp.duration ~/ 2);
+
+      positioned = tester.widget(positionedFinder);
+      expect(positioned.width, beginWidth);
+      expect(positioned.height, beginHeight);
+      expect(positioned.top, beginTop);
     },
   );
 }
