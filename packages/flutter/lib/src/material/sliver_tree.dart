59765096fefa2466633080db8bcbfb6a47470cef
Kate Lovett <katelovett@google.com>
2024-04-22 13:25:44 -0500

    Imports

--- packages/flutter/lib/src/material/sliver_tree.dart
+++ packages/flutter/lib/src/material/sliver_tree.dart
@@ -3,8 +3,10 @@
 // found in the LICENSE file.
 
 import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
+
+import 'icons.dart';
 
 const double _kDefaultRowExtent = 40.0;
 
