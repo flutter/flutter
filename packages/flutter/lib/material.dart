99d7ff3eeec1755e9f27a6a9db94b574acad3536
Kate Lovett <katelovett@google.com>
2024-04-22 13:12:13 -0500

    Move sliver_tree to material for stinkin icon

--- packages/flutter/lib/material.dart
+++ packages/flutter/lib/material.dart
@@ -159,6 +159,7 @@ export 'src/material/selection_area.dart';
 export 'src/material/shadows.dart';
 export 'src/material/slider.dart';
 export 'src/material/slider_theme.dart';
+export 'src/material/sliver_tree.dart';
 export 'src/material/snack_bar.dart';
 export 'src/material/snack_bar_theme.dart';
 export 'src/material/spell_check_suggestions_toolbar.dart';
