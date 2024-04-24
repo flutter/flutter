fcd3884e1ed58da6120e3fca7f07448f5bbc6020
Jonah Williams <jonahwilliams@google.com>
2024-04-24 10:58:08 -0700

    [Impeller] make drawAtlas always use porterduff or vertices_uber shader (flutter/engine#52348)
    The drawAtlas component of https://github.com/flutter/engine/pull/52345 . DrawAtlas did not have the same advanced blend/overlapping issues as draw vertices because of the sorting and generation of the sub-atlas, but given that the vertices uber shader exists we can just use that instead.
    
    Will eventually allow deleting the other drawVertices special case shaders.

--- engine/src/flutter/impeller/entity/contents/atlas_contents.h
+++ engine/src/flutter/impeller/entity/contents/atlas_contents.h
@@ -9,26 +9,11 @@
 #include <memory>
 #include <vector>
 
-#include "flutter/fml/macros.h"
 #include "impeller/core/sampler_descriptor.h"
 #include "impeller/entity/contents/contents.h"
 #include "impeller/entity/entity.h"
 
 namespace impeller {
-
-struct SubAtlasResult {
-  // Sub atlas values.
-  std::vector<Rect> sub_texture_coords;
-  std::vector<Color> sub_colors;
-  std::vector<Matrix> sub_transforms;
-
-  // Result atlas values.
-  std::vector<Rect> result_texture_coords;
-  std::vector<Matrix> result_transforms;
-
-  // Size of the sub-atlass.
-  ISize size;
-};
 
 class AtlasContents final : public Contents {
  public:
@@ -62,11 +47,6 @@ class AtlasContents final : public Contents {
 
   const std::vector<Color>& GetColors() const;
 
-  /// @brief Compress a drawAtlas call with blending into a smaller sized atlas.
-  ///        This atlas has no overlapping to ensure
-  ///        blending behaves as if it were done in the fragment shader.
-  std::shared_ptr<SubAtlasResult> GenerateSubAtlas() const;
-
   // |Contents|
   std::optional<Rect> GetCoverage(const Entity& entity) const override;
 
@@ -91,74 +71,6 @@ class AtlasContents final : public Contents {
   AtlasContents(const AtlasContents&) = delete;
 
   AtlasContents& operator=(const AtlasContents&) = delete;
-};
-
-class AtlasTextureContents final : public Contents {
- public:
-  explicit AtlasTextureContents(const AtlasContents& parent);
-
-  ~AtlasTextureContents() override;
-
-  // |Contents|
-  std::optional<Rect> GetCoverage(const Entity& entity) const override;
-
-  // |Contents|
-  bool Render(const ContentContext& renderer,
-              const Entity& entity,
-              RenderPass& pass) const override;
-
-  void SetAlpha(Scalar alpha);
-
-  void SetCoverage(Rect coverage);
-
-  void SetTexture(std::shared_ptr<Texture> texture);
-
-  void SetUseDestination(bool value);
-
-  void SetSubAtlas(const std::shared_ptr<SubAtlasResult>& subatlas);
-
- private:
-  const AtlasContents& parent_;
-  Scalar alpha_ = 1.0;
-  Rect coverage_;
-  std::shared_ptr<Texture> texture_;
-  bool use_destination_ = false;
-  std::shared_ptr<SubAtlasResult> subatlas_;
-
-  AtlasTextureContents(const AtlasTextureContents&) = delete;
-
-  AtlasTextureContents& operator=(const AtlasTextureContents&) = delete;
-};
-
-class AtlasColorContents final : public Contents {
- public:
-  explicit AtlasColorContents(const AtlasContents& parent);
-
-  ~AtlasColorContents() override;
-
-  // |Contents|
-  std::optional<Rect> GetCoverage(const Entity& entity) const override;
-
-  // |Contents|
-  bool Render(const ContentContext& renderer,
-              const Entity& entity,
-              RenderPass& pass) const override;
-
-  void SetAlpha(Scalar alpha);
-
-  void SetCoverage(Rect coverage);
-
-  void SetSubAtlas(const std::shared_ptr<SubAtlasResult>& subatlas);
-
- private:
-  const AtlasContents& parent_;
-  Scalar alpha_ = 1.0;
-  Rect coverage_;
-  std::shared_ptr<SubAtlasResult> subatlas_;
-
-  AtlasColorContents(const AtlasColorContents&) = delete;
-
-  AtlasColorContents& operator=(const AtlasColorContents&) = delete;
 };
 
 }  // namespace impeller
