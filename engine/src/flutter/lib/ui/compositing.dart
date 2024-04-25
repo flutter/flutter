8984f4f0cf0b3af6e294b63660b9afce2493ffe2
Ian Hickson <ian@hixie.ch>
2024-04-24 20:05:20 -0700

    Document the new binding hooks for SceneBuilder, PictureRecorder, Canvas (flutter/engine#52374)
    See https://github.com/flutter/flutter/pull/147271 for context.
    
    Do not land until https://github.com/flutter/flutter/pull/147271 has landed.

--- engine/src/flutter/lib/ui/compositing.dart
+++ engine/src/flutter/lib/ui/compositing.dart
@@ -233,6 +233,18 @@ class ShaderMaskEngineLayer extends _EngineLayerWrapper {
 /// To draw graphical operations onto a [Scene], first create a
 /// [Picture] using a [PictureRecorder] and a [Canvas], and then add
 /// it to the scene using [addPicture].
+///
+/// ## Use with the Flutter framework
+///
+/// The Flutter framework's [RendererBinding] provides a hook for creating
+/// [SceneBuilder] objects ([RendererBinding.createSceneBuilder]) that allows
+/// tests to hook into the scene creation logic. When creating a [SceneBuilder]
+/// in the context of the Flutter framework, consider calling
+/// [RendererBinding.createSceneBuilder] instead of calling the
+/// [SceneBuilder.new] constructor directly.
+///
+/// This does not apply when using the `dart:ui` API directly, without using the
+/// Flutter framework bindings, `flutter_test` framework, et al.
 abstract class SceneBuilder {
   factory SceneBuilder() = _NativeSceneBuilder;
 
