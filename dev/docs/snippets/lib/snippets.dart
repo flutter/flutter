01c1d709b5d6eed7718de6b647503ee9f7d6335f
Greg Spencer <gspencer@google.com>
2024-05-01 10:05:05 -0700

    Move snippets package back into flutter repo

---
+++ dev/docs/snippets/lib/snippets.dart
@@ -0,0 +1,13 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+library snippets;
+
+export 'src/analysis.dart';
+export 'src/configuration.dart';
+export 'src/data_types.dart';
+export 'src/import_sorter.dart';
+export 'src/snippet_generator.dart';
+export 'src/snippet_parser.dart';
+export 'src/util.dart';
