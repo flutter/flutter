3ec84538cade621f3c041d3d058b16c86a661e40
Juanjo Tugores <tugorez@users.noreply.github.com>
2024-05-02 14:27:23 -0700

    Put the view focus functionality behind a (disabled by default) flag (flutter/engine#52527)
    Put the view focus functionality behind a (disabled by default) flag
    
    Should unblock https://github.com/flutter/flutter/pull/143259
    
    [C++, Objective-C, Java style guides]: https://github.com/flutter/engine/blob/main/CONTRIBUTING.md#style

--- engine/src/flutter/lib/web_ui/lib/src/engine/platform_dispatcher/view_focus_binding.dart
+++ engine/src/flutter/lib/web_ui/lib/src/engine/platform_dispatcher/view_focus_binding.dart
@@ -3,12 +3,20 @@
 // found in the LICENSE file.
 
 import 'dart:async';
+import 'package:meta/meta.dart';
 import 'package:ui/src/engine.dart';
 import 'package:ui/ui.dart' as ui;
 
 /// Tracks the [FlutterView]s focus changes.
 final class ViewFocusBinding {
   ViewFocusBinding(this._viewManager, this._onViewFocusChange);
+
+
+  /// Wether [FlutterView] focus changes will be reported and performed.
+  ///
+  /// DO NOT rely on this bit as it will go away soon. You're warned :)!
+  @visibleForTesting
+  static bool isEnabled = false;
 
   final FlutterViewManager _viewManager;
   final ui.ViewFocusChangeCallback _onViewFocusChange;
@@ -35,6 +43,9 @@ final class ViewFocusBinding {
   }
 
   void changeViewFocus(int viewId, ui.ViewFocusState state) {
+    if (!isEnabled) {
+      return;
+    }
     final DomElement? viewElement = _viewManager[viewId]?.dom.rootElement;
 
     if (state == ui.ViewFocusState.focused) {
@@ -69,6 +80,10 @@ final class ViewFocusBinding {
   });
 
   void _handleFocusChange(DomElement? focusedElement) {
+    if (!isEnabled) {
+      return;
+    }
+
     final int? viewId = _viewId(focusedElement);
     if (viewId == _lastViewId) {
       return;
