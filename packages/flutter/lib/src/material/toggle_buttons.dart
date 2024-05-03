6345192745b5275fae042f6ae7fd48548111bfb6
Nate Wilson <nate.w5687@gmail.com>
2024-05-03 11:01:16 -0600

    revert toggle_buttons.dart

--- packages/flutter/lib/src/material/toggle_buttons.dart
+++ packages/flutter/lib/src/material/toggle_buttons.dart
@@ -822,18 +822,24 @@ class ToggleButtons extends StatelessWidget {
       );
     });
 
-    final Widget child = Flex(
-      direction: direction,
-      mainAxisSize: MainAxisSize.min,
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      verticalDirection: verticalDirection,
-      children: buttons,
-    );
+    if (direction == Axis.vertical) {
+      return IntrinsicWidth(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          verticalDirection: verticalDirection,
+          children: buttons,
+        ),
+      );
+    }
 
-    return switch (direction) {
-      Axis.horizontal => IntrinsicWidth(child: child),
-      Axis.vertical   => IntrinsicHeight(child: child),
-    };
+    return IntrinsicHeight(
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: buttons,
+      ),
+    );
   }
 
   @override
