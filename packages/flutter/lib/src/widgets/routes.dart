f12940387f3784207c876fafcdab2de7c0dac283
Dimil Kalathiya <102401667+Dimilkalathiya@users.noreply.github.com>
2024-05-08 05:09:56 +0530

    fixes `RawDialogRoute` memory leak (#147817)
    part of #141198 
    
    - Fixes memory leak on `RawDialogRoute`
    - Adds opt-in test

--- packages/flutter/lib/src/widgets/routes.dart
+++ packages/flutter/lib/src/widgets/routes.dart
@@ -2286,10 +2286,7 @@ class RawDialogRoute<T> extends PopupRoute<T> {
     if (_transitionBuilder == null) {
       // Some default transition.
       return FadeTransition(
-        opacity: CurvedAnimation(
-          parent: animation,
-          curve: Curves.linear,
-        ),
+        opacity: animation,
         child: child,
       );
     }
