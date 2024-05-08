f41c65ee2235f28a1fd464edab328ca4599fe099
Kostia Sokolovskyi <sokolovskyi.konstantin@gmail.com>
2024-05-08 15:55:59 +0200

    Apply review suggestions.

--- examples/api/test/widgets/shortcuts/logical_key_set.0_test.dart
+++ examples/api/test/widgets/shortcuts/logical_key_set.0_test.dart
@@ -8,125 +8,111 @@ import 'package:flutter_api_samples/widgets/shortcuts/logical_key_set.0.dart'
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  group('LogicalKeySetExampleApp', () {
-    Future<void> sendKeyCombination(
-      WidgetTester tester,
-      List<LogicalKeyboardKey> keys,
-    ) async {
-      for (final LogicalKeyboardKey key in keys) {
-        await tester.sendKeyDownEvent(key);
-      }
-      for (final LogicalKeyboardKey key in keys.reversed) {
-        await tester.sendKeyUpEvent(key);
-      }
+  Future<void> sendKeyCombination(
+    WidgetTester tester,
+    List<LogicalKeyboardKey> keys,
+  ) async {
+    for (final LogicalKeyboardKey key in keys) {
+      await tester.sendKeyDownEvent(key);
     }
+    for (final LogicalKeyboardKey key in keys.reversed) {
+      await tester.sendKeyUpEvent(key);
+    }
+  }
 
-    testWidgets('displays correct labels', (WidgetTester tester) async {
-      await tester.pumpWidget(
-        const example.LogicalKeySetExampleApp(),
-      );
+  testWidgets('Verify correct labels are displayed', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      const example.LogicalKeySetExampleApp(),
+    );
 
-      expect(find.text('LogicalKeySet Sample'), findsOneWidget);
-      expect(
-        find.text('Add to the counter by pressing Ctrl+C'),
-        findsOneWidget,
+    expect(find.text('LogicalKeySet Sample'), findsOneWidget);
+    expect(
+      find.text('Add to the counter by pressing Ctrl+C'),
+      findsOneWidget,
+    );
+    expect(find.text('count: 0'), findsOneWidget);
+  });
+
+  testWidgets('LCtrl-C or C-LCtrl combination press updates counter', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      const example.LogicalKeySetExampleApp(),
+    );
+
+    for (int counter = 0; counter < 10; counter++) {
+      expect(find.text('count: $counter'), findsOneWidget);
+
+      await sendKeyCombination(
+        tester,
+        counter.isEven
+            ? <LogicalKeyboardKey>[
+                LogicalKeyboardKey.controlLeft,
+                LogicalKeyboardKey.keyC,
+              ]
+            : <LogicalKeyboardKey>[
+                LogicalKeyboardKey.keyC,
+                LogicalKeyboardKey.controlLeft,
+              ],
       );
+      await tester.pump();
+    }
+  });
+
+  testWidgets('RCtrl-C combination press updates counter', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      const example.LogicalKeySetExampleApp(),
+    );
+
+    for (int counter = 0; counter < 10; counter++) {
+      expect(find.text('count: $counter'), findsOneWidget);
+
+      await sendKeyCombination(
+        tester,
+        <LogicalKeyboardKey>[
+          LogicalKeyboardKey.controlRight,
+          LogicalKeyboardKey.keyC,
+        ],
+      );
+      await tester.pump();
+    }
+  });
+
+  testWidgets('LCtrl-A-C combination press does not update counter', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      const example.LogicalKeySetExampleApp(),
+    );
+
+    for (int counter = 0; counter < 10; counter++) {
       expect(find.text('count: 0'), findsOneWidget);
-    });
 
-    testWidgets(
-      'updates counter when LCtrl-C or C-LCtrl combination pressed',
-      (WidgetTester tester) async {
-        await tester.pumpWidget(
-          const example.LogicalKeySetExampleApp(),
-        );
+      await sendKeyCombination(
+        tester,
+        <LogicalKeyboardKey>[
+          LogicalKeyboardKey.controlLeft,
+          LogicalKeyboardKey.keyA,
+          LogicalKeyboardKey.keyC,
+        ],
+      );
+      await tester.pump();
+    }
+  });
 
-        for (int counter = 0; counter < 10; counter++) {
-          expect(find.text('count: $counter'), findsOneWidget);
-
-          await sendKeyCombination(
-            tester,
-            counter.isEven
-                ? <LogicalKeyboardKey>[
-                    LogicalKeyboardKey.controlLeft,
-                    LogicalKeyboardKey.keyC,
-                  ]
-                : <LogicalKeyboardKey>[
-                    LogicalKeyboardKey.keyC,
-                    LogicalKeyboardKey.controlLeft,
-                  ],
-          );
-          await tester.pump();
-        }
-      },
+  testWidgets('RCtrl-A-C combination press does not update counter', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      const example.LogicalKeySetExampleApp(),
     );
 
-    testWidgets(
-      'updates counter when RCtrl-C combination pressed',
-      (WidgetTester tester) async {
-        await tester.pumpWidget(
-          const example.LogicalKeySetExampleApp(),
-        );
+    for (int counter = 0; counter < 10; counter++) {
+      expect(find.text('count: 0'), findsOneWidget);
 
-        for (int counter = 0; counter < 10; counter++) {
-          expect(find.text('count: $counter'), findsOneWidget);
-
-          await sendKeyCombination(
-            tester,
-            <LogicalKeyboardKey>[
-              LogicalKeyboardKey.controlRight,
-              LogicalKeyboardKey.keyC,
-            ],
-          );
-          await tester.pump();
-        }
-      },
-    );
-
-    testWidgets(
-      'does not update counter when LCtrl-A-C combination pressed',
-      (WidgetTester tester) async {
-        await tester.pumpWidget(
-          const example.LogicalKeySetExampleApp(),
-        );
-
-        for (int counter = 0; counter < 10; counter++) {
-          expect(find.text('count: 0'), findsOneWidget);
-
-          await sendKeyCombination(
-            tester,
-            <LogicalKeyboardKey>[
-              LogicalKeyboardKey.controlLeft,
-              LogicalKeyboardKey.keyA,
-              LogicalKeyboardKey.keyC,
-            ],
-          );
-          await tester.pump();
-        }
-      },
-    );
-
-    testWidgets(
-      'does not update counter when RCtrl-A-C combination pressed',
-      (WidgetTester tester) async {
-        await tester.pumpWidget(
-          const example.LogicalKeySetExampleApp(),
-        );
-
-        for (int counter = 0; counter < 10; counter++) {
-          expect(find.text('count: 0'), findsOneWidget);
-
-          await sendKeyCombination(
-            tester,
-            <LogicalKeyboardKey>[
-              LogicalKeyboardKey.controlRight,
-              LogicalKeyboardKey.keyA,
-              LogicalKeyboardKey.keyC,
-            ],
-          );
-          await tester.pump();
-        }
-      },
-    );
+      await sendKeyCombination(
+        tester,
+        <LogicalKeyboardKey>[
+          LogicalKeyboardKey.controlRight,
+          LogicalKeyboardKey.keyA,
+          LogicalKeyboardKey.keyC,
+        ],
+      );
+      await tester.pump();
+    }
   });
 }
