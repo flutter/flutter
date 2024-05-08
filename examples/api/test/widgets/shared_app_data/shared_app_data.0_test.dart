dc6f581473d5deb502413e5568978a176c0ef37f
Kostia Sokolovskyi <sokolovskyi.konstantin@gmail.com>
2024-05-08 16:21:28 +0200

    Apply review suggestions.

--- examples/api/test/widgets/shared_app_data/shared_app_data.0_test.dart
+++ examples/api/test/widgets/shared_app_data/shared_app_data.0_test.dart
@@ -8,108 +8,97 @@ import 'package:flutter_api_samples/widgets/shared_app_data/shared_app_data.0.da
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  group('SharedAppDataExampleApp', () {
-    testWidgets('displays correct labels', (WidgetTester tester) async {
-      await tester.pumpWidget(
-        const example.SharedAppDataExampleApp(),
+  testWidgets('Verify correct labels are displayed', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      const example.SharedAppDataExampleApp(),
+    );
+
+    expect(find.text('SharedAppData Sample'), findsOneWidget);
+    expect(find.text('foo: initial'), findsOneWidget);
+    expect(find.text('bar: initial'), findsOneWidget);
+    expect(find.text('change foo'), findsOneWidget);
+    expect(find.text('change bar'), findsOneWidget);
+  });
+
+  testWidgets('foo value can be updated', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      const example.SharedAppDataExampleApp(),
+    );
+
+    int counter = 0;
+
+    while (counter < 10) {
+      counter++;
+
+      await tester.tap(
+        find.ancestor(
+          of: find.text('change foo'),
+          matching: find.byType(ElevatedButton),
+        ),
       );
+      await tester.pump();
 
-      expect(find.text('SharedAppData Sample'), findsOneWidget);
-      expect(find.text('foo: initial'), findsOneWidget);
-      expect(find.text('bar: initial'), findsOneWidget);
-      expect(find.text('change foo'), findsOneWidget);
-      expect(find.text('change bar'), findsOneWidget);
-    });
+      expect(find.text('foo: FOO $counter'), findsOneWidget);
+    }
+  });
 
-    testWidgets(
-      "updates foo value on 'change foo' button tap",
-      (WidgetTester tester) async {
-        await tester.pumpWidget(
-          const example.SharedAppDataExampleApp(),
-        );
-
-        int counter = 0;
-
-        while (counter < 10) {
-          counter++;
-
-          await tester.tap(
-            find.ancestor(
-              of: find.text('change foo'),
-              matching: find.byType(ElevatedButton),
-            ),
-          );
-          await tester.pump();
-
-          expect(find.text('foo: FOO $counter'), findsOneWidget);
-        }
-      },
+  testWidgets('bar value can be updated', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      const example.SharedAppDataExampleApp(),
     );
 
-    testWidgets(
-      "updates bar value on 'change bar' button tap",
-      (WidgetTester tester) async {
-        await tester.pumpWidget(
-          const example.SharedAppDataExampleApp(),
-        );
+    int counter = 0;
 
-        int counter = 0;
+    while (counter < 10) {
+      counter++;
 
-        while (counter < 10) {
-          counter++;
+      await tester.tap(
+        find.ancestor(
+          of: find.text('change bar'),
+          matching: find.byType(ElevatedButton),
+        ),
+      );
+      await tester.pump();
 
-          await tester.tap(
-            find.ancestor(
-              of: find.text('change bar'),
-              matching: find.byType(ElevatedButton),
-            ),
-          );
-          await tester.pump();
+      expect(find.text('bar: BAR $counter'), findsOneWidget);
+    }
+  });
 
-          expect(find.text('bar: BAR $counter'), findsOneWidget);
-        }
-      },
+  testWidgets('foo and bar values update independently of one another', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      const example.SharedAppDataExampleApp(),
     );
 
-    testWidgets(
-      'updates foo and bar values independently of one another',
-      (WidgetTester tester) async {
-        await tester.pumpWidget(
-          const example.SharedAppDataExampleApp(),
-        );
+    int fooCounter = 0;
+    int barCounter = 0;
 
-        int fooCounter = 0;
-        int barCounter = 0;
+    for (int i = 0; i < 20; i++) {
+      if (i.isEven) {
+        fooCounter++;
+      } else {
+        barCounter++;
+      }
 
-        for (int i = 0; i < 20; i++) {
-          if (i.isEven) {
-            fooCounter++;
-          } else {
-            barCounter++;
-          }
+      await tester.tap(
+        find.ancestor(
+          of: i.isEven ? find.text('change foo') : find.text('change bar'),
+          matching: find.byType(ElevatedButton),
+        ),
+      );
+      await tester.pump();
 
-          await tester.tap(
-            find.ancestor(
-              of: i.isEven ? find.text('change foo') : find.text('change bar'),
-              matching: find.byType(ElevatedButton),
-            ),
-          );
-          await tester.pump();
-
-          expect(
-            find.text(
-              'foo: ${fooCounter == 0 ? 'initial' : 'FOO $fooCounter'}',
-            ),
-            findsOneWidget,
-          );
-          expect(
-            find.text(
-              'bar: ${barCounter == 0 ? 'initial' : 'BAR $barCounter'}',
-            ),
-            findsOneWidget,
-          );
-        }
-      },
-    );
+      expect(
+        find.text(
+          'foo: ${fooCounter == 0 ? 'initial' : 'FOO $fooCounter'}',
+        ),
+        findsOneWidget,
+      );
+      expect(
+        find.text(
+          'bar: ${barCounter == 0 ? 'initial' : 'BAR $barCounter'}',
+        ),
+        findsOneWidget,
+      );
+    }
   });
 }
--- examples/api/test/widgets/shared_app_data/shared_app_data.1_test.dart
+++ examples/api/test/widgets/shared_app_data/shared_app_data.1_test.dart
@@ -8,46 +8,41 @@ import 'package:flutter_api_samples/widgets/shared_app_data/shared_app_data.1.da
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  group('SharedAppDataExampleApp', () {
-    example.SharedObject getSharedObject(WidgetTester tester) {
-      final BuildContext context = tester.element(
-        find.byType(example.CustomWidget),
-      );
-      return example.SharedObject.of(context);
-    }
+  example.SharedObject getSharedObject(WidgetTester tester) {
+    final BuildContext context = tester.element(
+      find.byType(example.CustomWidget),
+    );
+    return example.SharedObject.of(context);
+  }
 
-    testWidgets('displays correct labels', (WidgetTester tester) async {
-      await tester.pumpWidget(
-        const example.SharedAppDataExampleApp(),
-      );
+  testWidgets('Verify correct labels are displayed', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      const example.SharedAppDataExampleApp(),
+    );
 
+    final example.SharedObject sharedObject = getSharedObject(tester);
+
+    expect(find.text('SharedAppData Sample'), findsOneWidget);
+    expect(find.text('Replace $sharedObject'), findsOneWidget);
+  });
+
+  testWidgets('Button tap resets SharedObject', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      const example.SharedAppDataExampleApp(),
+    );
+
+    for (int i = 0; i < 10; i++) {
       final example.SharedObject sharedObject = getSharedObject(tester);
 
-      expect(find.text('SharedAppData Sample'), findsOneWidget);
-      expect(find.text('Replace $sharedObject'), findsOneWidget);
-    });
+      final Finder buttonFinder = find.ancestor(
+        of: find.text('Replace $sharedObject'),
+        matching: find.byType(ElevatedButton),
+      );
 
-    testWidgets(
-      'resets SharedObject on button tap',
-      (WidgetTester tester) async {
-        await tester.pumpWidget(
-          const example.SharedAppDataExampleApp(),
-        );
+      expect(buttonFinder, findsOneWidget);
 
-        for (int i = 0; i < 10; i++) {
-          final example.SharedObject sharedObject = getSharedObject(tester);
-
-          final Finder buttonFinder = find.ancestor(
-            of: find.text('Replace $sharedObject'),
-            matching: find.byType(ElevatedButton),
-          );
-
-          expect(buttonFinder, findsOneWidget);
-
-          await tester.tap(buttonFinder);
-          await tester.pump();
-        }
-      },
-    );
+      await tester.tap(buttonFinder);
+      await tester.pump();
+    }
   });
 }
