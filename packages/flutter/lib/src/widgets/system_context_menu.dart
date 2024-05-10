ba9adb98ee7c2e3bce4900674f2678904e6f2a1b
Justin McCandless <jmccandless@google.com>
2024-05-10 12:00:42 -0700

    Already tested this

--- packages/flutter/lib/src/widgets/system_context_menu.dart
+++ packages/flutter/lib/src/widgets/system_context_menu.dart
@@ -113,7 +113,6 @@ class _SystemContextMenuState extends State<SystemContextMenu> {
   @override
   void didUpdateWidget(SystemContextMenu oldWidget) {
     super.didUpdateWidget(oldWidget);
-    // TODO(justinmc): Make sure you've tested moving the anchor.
     if (widget.anchor != oldWidget.anchor) {
       _systemContextMenuController.show(widget.anchor);
     }
