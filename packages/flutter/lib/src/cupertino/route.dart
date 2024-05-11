0ceda72c54926187d174440aa7dadc3ca706b0ea
Dimilkalathiya <kalathiyadimil@gmail.com>
2024-05-11 10:54:31 +0530

    re-init animation always

--- packages/flutter/lib/src/cupertino/route.dart
+++ packages/flutter/lib/src/cupertino/route.dart
@@ -1383,13 +1383,11 @@ class CupertinoDialogRoute<T> extends RawDialogRoute<T> {
   CurvedAnimation? _fadeAnimation;
 
   void _setAnimation(Animation<double> animation) {
-    if (_fadeAnimation?.parent != animation) {
-      _fadeAnimation?.dispose();
-      _fadeAnimation = CurvedAnimation(
-      parent: animation,
-      curve: Curves.easeOut,
-      );
-    }
+    _fadeAnimation?.dispose();
+    _fadeAnimation = CurvedAnimation(
+    parent: animation,
+    curve: Curves.easeOut,
+    );
   }
 
   @override
