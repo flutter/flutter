b314a7712f532725c75ed7756144f06ad1e92fdd
NobodyForNothing <82763757+NobodyForNothing@users.noreply.github.com>
2024-05-14 20:21:32 +0200

    fix unnecessary import

--- examples/api/test/widgets/transitions/default_text_style.0_test.dart
+++ examples/api/test/widgets/transitions/default_text_style.0_test.dart
@@ -3,7 +3,6 @@
 // found in the LICENSE file.
 
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:flutter_api_samples/widgets/transitions/default_text_style_transition.0.dart' as example;
 import 'package:flutter_test/flutter_test.dart';
 
