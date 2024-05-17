dda0c491506de066deafc97fa0d56138b237b1e3
NobodyForNothing <82763757+NobodyForNothing@users.noreply.github.com>
2024-05-17 18:50:32 +0200

    test input decoration material_state PrefixIconConstraints example 0

---
+++ examples/api/test/material/input_decorator/input_decoration.prefix_icon_constraints.0_test.dart
@@ -0,0 +1,39 @@
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+import 'package:flutter_api_samples/material/input_decorator/input_decoration.prefix_icon_constraints.0.dart' as example;
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('Shows all decorations', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      const example.PrefixIconConstraintsExampleApp(),
+    );
+    expect(find.text('InputDecoration Sample'), findsOneWidget);
+
+    expect(find.byType(TextField), findsNWidgets(2));
+    expect(find.byIcon(Icons.search), findsNWidgets(2));
+    expect(find.text('Normal Icon Constraints'), findsOneWidget);
+    expect(find.text('Smaller Icon Constraints'), findsOneWidget);
+
+    final Finder normalIcon = find.descendant(
+      of: find.ancestor(
+        of: find.text('Normal Icon Constraints'),
+        matching: find.byType(TextField)
+      ),
+      matching: find.byIcon(Icons.search)
+    );
+    final Finder smallerIcon = find.descendant(
+      of: find.ancestor(
+        of: find.text('Smaller Icon Constraints'),
+        matching: find.byType(TextField)
+      ),
+      matching: find.byIcon(Icons.search)
+    );
+
+    expect(tester.getSize(normalIcon).longestSide,
+      greaterThan(tester.getSize(smallerIcon).longestSide));
+  });
+}
