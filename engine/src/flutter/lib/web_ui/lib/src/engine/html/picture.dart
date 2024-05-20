12d11fc8d48d2903930a1bdccb7a7411a77a0db0
Harry Terkelsen <1961493+harryterkelsen@users.noreply.github.com>
2024-05-20 16:59:06 -0700

    [canvaskit] Refactor HtmlImageCodec to generalize to different renderers (flutter/engine#52905)
    Refactor `HtmlImageCodec` to decouple it from the html renderer so other
    renderers can create a `ui.Image` via `Image.decode()`.
    
    ## Pre-launch Checklist
    
    - [x] I read the [Contributor Guide] and followed the process outlined
    there for submitting PRs.
    - [x] I read the [Tree Hygiene] wiki page, which explains my
    responsibilities.
    - [x] I read and followed the [Flutter Style Guide] and the [C++,
    Objective-C, Java style guides].
    - [x] I listed at least one issue that this PR fixes in the description
    above.
    - [x] I added new tests to check the change I am making or feature I am
    adding, or the PR is [test-exempt]. See [testing the engine] for
    instructions on writing and running engine tests.
    - [x] I updated/added relevant documentation (doc comments with `///`).
    - [x] I signed the [CLA].
    - [x] All existing and new tests are passing.
    
    If you need help, consider asking for advice on the #hackers-new channel
    on [Discord].
    
    <!-- Links -->
    [Contributor Guide]:
    https://github.com/flutter/flutter/wiki/Tree-hygiene#overview
    [Tree Hygiene]: https://github.com/flutter/flutter/wiki/Tree-hygiene
    [test-exempt]:
    https://github.com/flutter/flutter/wiki/Tree-hygiene#tests
    [Flutter Style Guide]:
    https://github.com/flutter/flutter/wiki/Style-guide-for-Flutter-repo
    [C++, Objective-C, Java style guides]:
    https://github.com/flutter/engine/blob/main/CONTRIBUTING.md#style
    [testing the engine]:
    https://github.com/flutter/flutter/wiki/Testing-the-engine
    [CLA]: https://cla.developers.google.com/
    [flutter/tests]: https://github.com/flutter/tests
    [breaking change policy]:
    https://github.com/flutter/flutter/wiki/Tree-hygiene#handling-breaking-changes
    [Discord]: https://github.com/flutter/flutter/wiki/Chat

--- engine/src/flutter/lib/web_ui/lib/src/engine/html/picture.dart
+++ engine/src/flutter/lib/web_ui/lib/src/engine/html/picture.dart
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'dart:async';
 import 'dart:math' as math;
 import 'dart:typed_data';
 
@@ -10,15 +11,132 @@ import 'package:ui/ui.dart' as ui;
 import '../dom.dart';
 import '../engine_canvas.dart';
 import '../frame_reference.dart';
-import '../picture.dart';
 import '../util.dart';
 import '../vector_math.dart';
 import 'bitmap_canvas.dart';
 import 'debug_canvas_reuse_overlay.dart';
 import 'dom_canvas.dart';
+import 'image.dart';
 import 'path/path_metrics.dart';
+import 'recording_canvas.dart';
 import 'surface.dart';
 import 'surface_stats.dart';
+
+class EnginePictureRecorder implements ui.PictureRecorder {
+  EnginePictureRecorder();
+
+  RecordingCanvas? _canvas;
+  late ui.Rect cullRect;
+  bool _isRecording = false;
+
+  RecordingCanvas beginRecording(ui.Rect bounds) {
+    assert(!_isRecording);
+    cullRect = bounds;
+    _isRecording = true;
+    return _canvas = RecordingCanvas(cullRect);
+  }
+
+  @override
+  bool get isRecording => _isRecording;
+
+  @override
+  EnginePicture endRecording() {
+    if (!_isRecording) {
+      // The mobile version returns an empty picture in this case. To match the
+      // behavior we produce a blank picture too.
+      beginRecording(ui.Rect.largest);
+    }
+    _isRecording = false;
+    _canvas!.endRecording();
+    final EnginePicture result = EnginePicture(_canvas, cullRect);
+    // We invoke the handler here, not in the Picture constructor, because we want
+    // [result.approximateBytesUsed] to be available for the handler.
+    ui.Picture.onCreate?.call(result);
+    return result;
+  }
+}
+
+/// An implementation of [ui.Picture] which is backed by a [RecordingCanvas].
+class EnginePicture implements ui.Picture {
+  /// This class is created by the engine, and should not be instantiated
+  /// or extended directly.
+  ///
+  /// To create a [Picture], use a [PictureRecorder].
+  EnginePicture(this.recordingCanvas, this.cullRect);
+
+  @override
+  Future<ui.Image> toImage(int width, int height) async {
+    final ui.Rect imageRect =
+        ui.Rect.fromLTRB(0, 0, width.toDouble(), height.toDouble());
+    final BitmapCanvas canvas = BitmapCanvas.imageData(imageRect);
+    recordingCanvas!.apply(canvas, imageRect);
+    final String imageDataUrl = canvas.toDataUrl();
+    final DomHTMLImageElement imageElement = createDomHTMLImageElement()
+      ..src = imageDataUrl
+      ..width = width.toDouble()
+      ..height = height.toDouble();
+
+    // The image loads asynchronously. We need to wait before returning,
+    // otherwise the returned HtmlImage will be temporarily unusable.
+    final Completer<ui.Image> onImageLoaded = Completer<ui.Image>.sync();
+
+    // Ignoring the returned futures from onError and onLoad because we're
+    // communicating through the `onImageLoaded` completer.
+    late final DomEventListener errorListener;
+    errorListener = createDomEventListener((DomEvent event) {
+      onImageLoaded.completeError(event);
+      imageElement.removeEventListener('error', errorListener);
+    });
+    imageElement.addEventListener('error', errorListener);
+    late final DomEventListener loadListener;
+    loadListener = createDomEventListener((DomEvent event) {
+      onImageLoaded.complete(HtmlImage(
+        imageElement,
+        width,
+        height,
+      ));
+      imageElement.removeEventListener('load', loadListener);
+    });
+    imageElement.addEventListener('load', loadListener);
+    return onImageLoaded.future;
+  }
+
+  @override
+  ui.Image toImageSync(int width, int height) {
+    throw UnsupportedError(
+        'toImageSync is not supported on the HTML backend. Use drawPicture instead, or toImage.');
+  }
+
+  bool _disposed = false;
+
+  @override
+  void dispose() {
+    ui.Picture.onDispose?.call(this);
+    _disposed = true;
+  }
+
+  @override
+  bool get debugDisposed {
+    bool? result;
+    assert(() {
+      result = _disposed;
+      return true;
+    }());
+
+    if (result != null) {
+      return result!;
+    }
+
+    throw StateError(
+        'Picture.debugDisposed is only available when asserts are enabled.');
+  }
+
+  @override
+  int get approximateBytesUsed => 0;
+
+  final RecordingCanvas? recordingCanvas;
+  final ui.Rect? cullRect;
+}
 
 // TODO(yjbanov): this is currently very naive. We probably want to cache
 //                fewer large canvases than small canvases. We could also
@@ -228,8 +346,7 @@ class PersistedPicture extends PersistedLeafSurface {
     if (parent!.projectedClip == null) {
       _exactLocalCullRect = localPaintBounds;
     } else {
-      _exactLocalCullRect =
-          localPaintBounds!.intersect(parent!.projectedClip!);
+      _exactLocalCullRect = localPaintBounds!.intersect(parent!.projectedClip!);
     }
     if (_exactLocalCullRect!.width <= 0 || _exactLocalCullRect!.height <= 0) {
       _exactLocalCullRect = ui.Rect.zero;
