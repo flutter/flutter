8ab8fa936fb71bb123c2a51ade1100e17404fb7d
Nate <nate.w5687@gmail.com>
2024-05-23 17:13:18 -0600

    update `FocusManager` platform check to include iOS

--- packages/flutter/lib/src/widgets/focus_manager.dart
+++ packages/flutter/lib/src/widgets/focus_manager.dart
@@ -1521,18 +1521,28 @@ class FocusManager with DiagnosticableTreeMixin, ChangeNotifier {
     if (kFlutterMemoryAllocationsEnabled) {
       ChangeNotifier.maybeDispatchObjectCreation(this);
     }
-    if (kIsWeb || defaultTargetPlatform != TargetPlatform.android) {
-      // It appears that some Android keyboard implementations can cause
-      // app lifecycle state changes: adding this listener would cause the
-      // text field to unfocus as the user is trying to type.
-      //
-      // Until this is resolved, we won't be adding the listener to Android apps.
-      // https://github.com/flutter/flutter/pull/142930#issuecomment-1981750069
+    if (_respondToWindowFocus) {
       _appLifecycleListener = _AppLifecycleListener(_appLifecycleChange);
       WidgetsBinding.instance.addObserver(_appLifecycleListener!);
     }
     rootScope._manager = this;
   }
+
+  /// It appears that some Android keyboard implementations can cause
+  /// app lifecycle state changes: adding the app lifecycle listener would
+  /// cause the text field to unfocus as the user is trying to type.
+  ///
+  /// Additionally, on iOS, input fields aren't automatically populated
+  /// with relevant data when using autofill.
+  ///
+  /// Until these are resolved, we won't be adding the listener to mobile platforms.
+  /// https://github.com/flutter/flutter/issues/148475#issuecomment-2118407411
+  /// https://github.com/flutter/flutter/pull/142930#issuecomment-1981750069
+  bool get _respondToWindowFocus => kIsWeb || switch (defaultTargetPlatform) {
+    TargetPlatform.android || TargetPlatform.iOS => false,
+    TargetPlatform.fuchsia || TargetPlatform.linux => true,
+    TargetPlatform.windows || TargetPlatform.macOS => true,
+  };
 
   /// Registers global input event handlers that are needed to manage focus.
   ///
