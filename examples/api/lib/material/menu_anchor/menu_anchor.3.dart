aff469dbbb1e2142ad52847722abab3cf47c2164
Aditya Dwivedi <dwikaljit@gmail.com>
2024-05-28 04:40:56 +0530

    added the SimpleCascadingMenuApp example.

---
+++ examples/api/lib/material/menu_anchor/menu_anchor.3.dart
@@ -0,0 +1,94 @@
+// Copyright 2024 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+
+/// Flutter code sample for [SimpleCascadingMenuApp].
+
+void main() => runApp(const SimpleCascadingMenuApp());
+
+/// A Simple Cascading Menu example using the [MenuAnchor] Widget.
+class MyCascadingMenu extends StatefulWidget {
+  const MyCascadingMenu({super.key});
+
+  @override
+  State<MyCascadingMenu> createState() => _MyCascadingMenuState();
+}
+
+class _MyCascadingMenuState extends State<MyCascadingMenu> {
+  /// [FocusNode] for Menu Button.
+  final FocusNode _buttonFocusNode = FocusNode(debugLabel: 'Menu Button');
+
+  @override
+  void dispose() {
+    /// Disposes the focus node when state is disposed.
+    _buttonFocusNode.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MenuAnchor(
+      /// Sets the focus node for Menu Button.
+      childFocusNode: _buttonFocusNode,
+
+      /// Defines Menu Items
+      menuChildren: <Widget>[
+        MenuItemButton(
+          child: const Text('Revert'),
+          onPressed: () {},
+        ),
+        MenuItemButton(
+          child: const Text('Setting'),
+          onPressed: () {},
+        ),
+        MenuItemButton(
+          child: const Text('Send Feedback'),
+          onPressed: () {},
+        ),
+      ],
+
+      /// Builds the Menu Button.
+      builder: (_, MenuController controller, Widget? child) {
+        return IconButton(
+          focusNode: _buttonFocusNode,
+
+          /// Opens or closes the menu based on its current state.
+          onPressed: () {
+            if (controller.isOpen) {
+              controller.close();
+            } else {
+              controller.open();
+            }
+          },
+
+          /// Displays Menu Button Icon.
+          icon: const Icon(Icons.more_vert),
+        );
+      },
+    );
+  }
+}
+
+/// Top Level Application Widget
+class SimpleCascadingMenuApp extends StatelessWidget {
+  const SimpleCascadingMenuApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('MenuAnchor Simple Example'),
+          actions: const [
+            MyCascadingMenu(),
+          ],
+        ),
+      ),
+    );
+  }
+}
