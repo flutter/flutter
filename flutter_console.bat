bf4d0350ae0621d07f81f1a75be3afd78e1e54f6
Andrew Shin <163041654+As90909w@users.noreply.github.com>
2024-05-27 10:32:14 -0700

    Update flutter_console.bat

--- flutter_console.bat
+++ flutter_console.bat
@@ -27,7 +27,8 @@ echo  Run "flutter help" to see all available commands.
 echo.
 echo  Want to use an IDE to interact with Flutter? https://flutter.dev/ide-setup/
 echo.
-echo  Want to run the "flutter" command from any Command Prompt or PowerShell window?
+echo  Want to Set up flutter from a PowerShell command?
+echo  run the "flutter" command from any Command Prompt or PowerShell window!
 echo  Add Flutter to your PATH: https://flutter.dev/setup-windows/#update-your-path
 echo.
 echo  ===============================================================================
