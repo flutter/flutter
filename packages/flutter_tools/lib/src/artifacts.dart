589465fe8b534a2c1f2b181e0ba905293cead1eb
Kevin Moore <kevmoo@users.noreply.github.com>
2024-05-28 08:33:15 -0700

    [tool] Use kebabCase directly (#149150)
    Drop silly snakeCase(..., '-') bits

--- packages/flutter_tools/lib/src/artifacts.dart
+++ packages/flutter_tools/lib/src/artifacts.dart
@@ -841,7 +841,7 @@ class CachedArtifacts implements Artifacts {
         if (mode == BuildMode.debug || mode == null) {
           return _fileSystem.path.join(engineDir, platformName);
         }
-        final String suffix = mode != BuildMode.debug ? '-${snakeCase(mode.cliName, '-')}' : '';
+        final String suffix = mode != BuildMode.debug ? '-${kebabCase(mode.cliName)}' : '';
         return _fileSystem.path.join(engineDir, platformName + suffix);
       case TargetPlatform.fuchsia_arm64:
       case TargetPlatform.fuchsia_x64:
@@ -855,7 +855,7 @@ class CachedArtifacts implements Artifacts {
       case TargetPlatform.android_x64:
       case TargetPlatform.android_x86:
         assert(mode != null, 'Need to specify a build mode for platform $platform.');
-        final String suffix = mode != BuildMode.debug ? '-${snakeCase(mode!.cliName, '-')}' : '';
+        final String suffix = mode != BuildMode.debug ? '-${kebabCase(mode!.cliName)}' : '';
         return _fileSystem.path.join(engineDir, platformName + suffix);
       case TargetPlatform.android:
         assert(false, 'cannot use TargetPlatform.android to look up artifacts');
--- packages/flutter_tools/lib/src/base/utils.dart
+++ packages/flutter_tools/lib/src/base/utils.dart
@@ -30,13 +30,18 @@ String camelCase(String str) {
 
 /// Convert `fooBar` to `foo-bar`.
 String kebabCase(String str) {
-  return snakeCase(str, '-');
+  return _reCase(str, '-');
 }
 
 final RegExp _upperRegex = RegExp(r'[A-Z]');
 
 /// Convert `fooBar` to `foo_bar`.
-String snakeCase(String str, [ String sep = '_' ]) {
+String snakeCase(String str) {
+  return _reCase(str, '_');
+}
+
+/// Convert `fooBar` to `foo[sep]bar`.
+String _reCase(String str, String sep) {
   return str.replaceAllMapped(_upperRegex,
       (Match m) => '${m.start == 0 ? '' : sep}${m[0]!.toLowerCase()}');
 }
