b0f48020f5ca5489b0e5379743175fb51980fbcc
ValentinVignal <valentin.vignal.dev@outlook.fr>
2024-05-29 11:02:07 +0800

    test: Add test for standard_fab_location.0.dart

--- dev/bots/check_code_samples.dart
+++ dev/bots/check_code_samples.dart
@@ -323,7 +323,6 @@ final Set<String> _knownMissingTests = <String>{
   'examples/api/test/material/menu_anchor/menu_anchor.2_test.dart',
   'examples/api/test/material/stepper/stepper.controls_builder.0_test.dart',
   'examples/api/test/material/flexible_space_bar/flexible_space_bar.0_test.dart',
-  'examples/api/test/material/floating_action_button_location/standard_fab_location.0_test.dart',
   'examples/api/test/material/chip/deletable_chip_attributes.on_deleted.0_test.dart',
   'examples/api/test/material/icon_button/icon_button.3_test.dart',
   'examples/api/test/material/expansion_panel/expansion_panel_list.expansion_panel_list_radio.0_test.dart',
