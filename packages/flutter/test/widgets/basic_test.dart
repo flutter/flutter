20c187846365d209443a62b736c89e3daeb16059
Ian Hickson <ian@hixie.ch>
2024-05-30 15:31:06 -0700

    Remove redundant usages of zones in skia_client.dart
    The SkiaGoldHttpOverrides don't have any effect since we never build
    our own HttpClient, it's always passed in.
    
    This is part 18 of a broken down version of the #140101 refactor.

--- packages/flutter/test/widgets/basic_test.dart
+++ packages/flutter/test/widgets/basic_test.dart
@@ -750,7 +750,7 @@ void main() {
     // golden file can be approved at any time.
     await tester.pumpWidget(RepaintBoundary(
       child: Container(
-        color: const Color(0xFFF40125),
+        color: const Color(0xFF161145),
       ),
     ));
 
