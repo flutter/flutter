cbe5184edf9d85c41961cd00a464155ad8a3f023
Jonny Wang <jonnywang@google.com>
2024-05-30 11:26:47 -0700

    Fuchsia api level 18 (flutter/engine#53115)
    b/335285098
    ## Pre-launch Checklist
    
    - [x] I read the [Contributor Guide] and followed the process outlined
    there for submitting PRs.
    - [x] I read the [Tree Hygiene] wiki page, which explains my
    responsibilities.
    - [x] I read and followed the [Flutter Style Guide] and the [C++,
    Objective-C, Java style guides].
    - [x] I listed at least one issue that this PR fixes in the description
    above.
    - [x] I added new tests to check the change I am making or feature I am
    adding, or the PR is [test-exempt]. See [testing the engine] for
    instructions on writing and running engine tests.
    - [x] I updated/added relevant documentation (doc comments with `///`).
    - [x] I signed the [CLA].
    - [x] All existing and new tests are passing.
    
    If you need help, consider asking for advice on the #hackers-new channel
    on [Discord].
    
    <!-- Links -->
    [Contributor Guide]:
    https://github.com/flutter/flutter/wiki/Tree-hygiene#overview
    [Tree Hygiene]: https://github.com/flutter/flutter/wiki/Tree-hygiene
    [test-exempt]:
    https://github.com/flutter/flutter/wiki/Tree-hygiene#tests
    [Flutter Style Guide]:
    https://github.com/flutter/flutter/wiki/Style-guide-for-Flutter-repo
    [C++, Objective-C, Java style guides]:
    https://github.com/flutter/engine/blob/main/CONTRIBUTING.md#style
    [testing the engine]:
    https://github.com/flutter/flutter/wiki/Testing-the-engine
    [CLA]: https://cla.developers.google.com/
    [flutter/tests]: https://github.com/flutter/tests
    [breaking change policy]:
    https://github.com/flutter/flutter/wiki/Tree-hygiene#handling-breaking-changes
    [Discord]: https://github.com/flutter/flutter/wiki/Chat

--- engine/src/flutter/tools/fuchsia/build_fuchsia_artifacts_test.py
+++ engine/src/flutter/tools/fuchsia/build_fuchsia_artifacts_test.py
@@ -11,7 +11,7 @@ class BuildFuchsiaArtifactsTest(unittest.TestCase):
     # It's expected to update this test each time the fuchsia_target_api_level
     # in //flutter/build/config/fuchsia/gn_configs.gni is changed, so we don't
     # accidentally publish the artifacts with an incorrect api level suffix.
-    self.assertEqual(build_fuchsia_artifacts.ReadTargetAPILevel(), '17')
+    self.assertEqual(build_fuchsia_artifacts.ReadTargetAPILevel(), '18')
 
 
 if __name__ == '__main__':
