d754f20ec8247f82470470bb4ce42cad0ad2094d
Dawid Wenderski <dawidope@gmail.com>
2024-05-31 22:30:09 +0200

    Fix incorrect behavior of ScrollViewKeyboardDismissBehavior.onDrag for ScrollViewers with Drawer (#148948)
    Fixes https://github.com/flutter/flutter/issues/141542
    Fixes https://github.com/flutter/flutter/issues/103544
    Fixes https://github.com/flutter/flutter/issues/54277

--- packages/flutter/lib/src/widgets/scroll_view.dart
+++ packages/flutter/lib/src/widgets/scroll_view.dart
@@ -498,9 +498,9 @@ abstract class ScrollView extends StatelessWidget {
       return NotificationListener<ScrollUpdateNotification>(
         child: scrollableResult,
         onNotification: (ScrollUpdateNotification notification) {
-          final FocusScopeNode focusScope = FocusScope.of(context);
-          if (notification.dragDetails != null && focusScope.hasFocus) {
-            focusScope.unfocus();
+          final FocusScopeNode currentScope = FocusScope.of(context);
+          if (notification.dragDetails != null && !currentScope.hasPrimaryFocus && currentScope.hasFocus) {
+            FocusManager.instance.primaryFocus?.unfocus();
           }
           return false;
         },
