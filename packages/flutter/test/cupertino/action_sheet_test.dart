e5ab067850587a8cf8c4e066b45dfe192afa759c
Tong Mu <dkwingsmt@gmail.com>
2024-05-31 16:47:24 -0700

    Fix tests

--- packages/flutter/test/cupertino/action_sheet_test.dart
+++ packages/flutter/test/cupertino/action_sheet_test.dart
@@ -677,7 +677,7 @@ void main() {
     await tester.tap(find.text('Go'));
     await tester.pump();
 
-    expect(findScrollableActionsSectionRenderBox(tester).size.height, moreOrLessEquals(112.3));
+    expect(findScrollableActionsSectionRenderBox(tester).size.height, moreOrLessEquals(84.3));
   });
 
   testWidgets('3 action buttons with cancel button', (WidgetTester tester) async {
@@ -711,7 +711,7 @@ void main() {
     await tester.tap(find.text('Go'));
     await tester.pump();
 
-    expect(findScrollableActionsSectionRenderBox(tester).size.height, moreOrLessEquals(168.6));
+    expect(findScrollableActionsSectionRenderBox(tester).size.height, moreOrLessEquals(84.3));
   });
 
   testWidgets('4+ action buttons with cancel button', (WidgetTester tester) async {
