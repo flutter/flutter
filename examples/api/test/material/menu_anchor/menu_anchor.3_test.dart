ec25f74cf8330e6560a56475da49430e6b6cf543
Aditya Dwivedi <dwikaljit@gmail.com>
2024-06-02 16:55:09 +0530

    remove unused import dart:async

--- examples/api/test/material/menu_anchor/menu_anchor.3_test.dart
+++ examples/api/test/material/menu_anchor/menu_anchor.3_test.dart
@@ -2,7 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_api_samples/material/menu_anchor/menu_anchor.3.dart' as example;
 import 'package:flutter_test/flutter_test.dart';
