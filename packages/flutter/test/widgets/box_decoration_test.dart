696c1964eb7d369a3723542d68463f67718e2a08
Polina Cherkasova <polina.c@live.com>
2024-06-04 15:34:16 -0700

    Update box_decoration_test.dart

--- packages/flutter/test/widgets/box_decoration_test.dart
+++ packages/flutter/test/widgets/box_decoration_test.dart
@@ -9,7 +9,6 @@ import 'dart:ui' as ui show Image;
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:leak_tracker_flutter_testing/leak_tracker_flutter_testing.dart';
 
 import '../image_data.dart';
 
