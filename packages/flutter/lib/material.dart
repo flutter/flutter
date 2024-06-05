b4a75e9edb8d57fe16fcc330390446dc420000a5
Qun Cheng <36861262+QuncCccccc@users.noreply.github.com>
2024-06-05 21:03:21 +0000

    Create CarouselView widget - Part 1 (#148094)
    This PR is to create an ["uncontained" Carousel](https://m3.material.io/components/carousel/specs#477de3a1-c9df-4742-baf3-bcd5eeb3764c).
    
    https://github.com/flutter/flutter/assets/36861262/947e6b2c-219f-4c8b-aba1-4a1a010221a7
    
    The rest of the layouts can be achieved by using `Carousel.weighted`: https://github.com/QuncCccccc/flutter/pull/2. The branch of `Caorusel.weighted` PR is based on this PR for relatively easer review. Will request reviews for the part 2 PR once this one is successfully merged in master.

--- packages/flutter/lib/material.dart
+++ packages/flutter/lib/material.dart
@@ -46,6 +46,7 @@ export 'src/material/button_theme.dart';
 export 'src/material/calendar_date_picker.dart';
 export 'src/material/card.dart';
 export 'src/material/card_theme.dart';
+export 'src/material/carousel.dart';
 export 'src/material/checkbox.dart';
 export 'src/material/checkbox_list_tile.dart';
 export 'src/material/checkbox_theme.dart';
