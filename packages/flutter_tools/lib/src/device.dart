4eb9a649c997b1b1c388033c7c2d7625227ef019
Mouad Debbar <mdebbar@google.com>
2024-06-05 17:55:34 -0400

    [web] Change `--web-renderer` default from `auto` to `canvaskit`

--- packages/flutter_tools/lib/src/device.dart
+++ packages/flutter_tools/lib/src/device.dart
@@ -993,7 +993,7 @@ class DebuggingOptions {
     this.webEnableExpressionEvaluation = false,
     this.webHeaders = const <String, String>{},
     this.webLaunchUrl,
-    this.webRenderer = WebRendererMode.auto,
+    WebRendererMode? webRenderer,
     this.webUseWasm = false,
     this.vmserviceOutFile,
     this.fastStart = false,
@@ -1007,7 +1007,8 @@ class DebuggingOptions {
     this.enableEmbedderApi = false,
     this.usingCISystem = false,
     this.debugLogsDirectoryPath,
-   }) : debuggingEnabled = true;
+   })  : debuggingEnabled = true,
+        webRenderer = webRenderer ?? WebRendererMode.getDefault(useWasm: webUseWasm);
 
   DebuggingOptions.disabled(this.buildInfo, {
       this.dartEntrypointArgs = const <String>[],
@@ -1024,7 +1025,7 @@ class DebuggingOptions {
       this.webBrowserFlags = const <String>[],
       this.webLaunchUrl,
       this.webHeaders = const <String, String>{},
-      this.webRenderer = WebRendererMode.auto,
+      WebRendererMode? webRenderer,
       this.webUseWasm = false,
       this.cacheSkSL = false,
       this.traceAllowlist,
@@ -1062,7 +1063,8 @@ class DebuggingOptions {
       webEnableExpressionEvaluation = false,
       nullAssertions = false,
       nativeNullAssertions = false,
-      serveObservatory = false;
+      serveObservatory = false,
+      webRenderer = webRenderer ?? WebRendererMode.getDefault(useWasm: webUseWasm);
 
   DebuggingOptions._({
     required this.buildInfo,
