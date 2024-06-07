8f46540364f70aae5b4569dc17e79a06a26f7bf1
Qun Cheng <chengqunq@gmail.com>
2024-06-07 15:55:55 -0700

    Update test

--- dev/a11y_assessments/lib/main.dart
+++ dev/a11y_assessments/lib/main.dart
@@ -20,19 +20,25 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final ColorScheme colorScheme = ColorScheme.fromSeed(
-      seedColor: const Color(0xff6750a4),
-      brightness: MediaQuery.platformBrightnessOf(context),
-      contrastLevel: MediaQuery.highContrastOf(context) ? 1.0 : 0.0,
-    );
+    final ThemeData lightTheme = ThemeData(
+      colorScheme: ColorScheme.fromSeed(
+        seedColor: const Color(0xff6750a4),
+        contrastLevel: MediaQuery.highContrastOf(context) ? 1.0 : 0.0,
+    ));
+    final ThemeData darkTheme = ThemeData(
+      colorScheme: ColorScheme.fromSeed(
+        brightness: Brightness.dark,
+        seedColor: const Color(0xff6750a4),
+        contrastLevel: MediaQuery.highContrastOf(context) ? 1.0 : 0.0,
+    ));
 
     final Map<String, WidgetBuilder> routes = Map<String, WidgetBuilder>.fromEntries(
       useCases.map((UseCase useCase) => MapEntry<String, WidgetBuilder>(useCase.route, useCase.build)),
     );
     return MaterialApp(
       title: 'Accessibility Assessments',
-      theme: ThemeData().copyWith(colorScheme: colorScheme),
-      darkTheme: ThemeData.dark().copyWith(colorScheme: colorScheme),
+      theme: lightTheme,
+      darkTheme: darkTheme,
       routes: <String, WidgetBuilder>{
         '/': (_) => const HomePage(),
         ...routes
