925f730bff877c770498bc16243cfd303ecaea2c
Elliott Brooks <21270878+elliette@users.noreply.github.com>
2024-06-10 16:43:14 -0700

    Fix test failures

--- packages/flutter/lib/src/widgets/service_extensions.dart
+++ packages/flutter/lib/src/widgets/service_extensions.dart
@@ -369,10 +369,10 @@ enum WidgetInspectorServiceExtensions {
   /// Name of service extension that, when called, will return the
   /// [DiagnosticsNode] data for the root [Element] of the widget tree.
   ///
-  /// If the parameter [isSummaryTree] is true, the tree will only include
+  /// If the parameter `isSummaryTree` is true, the tree will only include
   /// [Element]s that were created by user code.
   ///
-  /// If the parameter [withPreviews] is true, text previews will be included
+  /// If the parameter `withPreviews` is true, text previews will be included
   /// for [Element]s with a corresponding [RenderObject] of type
   /// [RenderParagraph].
   ///
