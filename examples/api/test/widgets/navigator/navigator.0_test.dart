0e36249ef21564cbced4d10315a95a58e84b8c21
ValentinVignal <valentin.vignal.dev@outlook.fr>
2024-06-12 18:01:00 +0800

    fix: Remove unused import

--- examples/api/test/widgets/navigator/navigator.0_test.dart
+++ examples/api/test/widgets/navigator/navigator.0_test.dart
@@ -2,7 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:flutter/widgets.dart';
 import 'package:flutter_api_samples/widgets/navigator/navigator.0.dart' as example;
 import 'package:flutter_test/flutter_test.dart';
 
