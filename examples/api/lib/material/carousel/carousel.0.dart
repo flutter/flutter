5a691f0e24b90f53ae752e6c296fb448c17d9f14
Qun Cheng <chengqunq@gmail.com>
2024-06-13 16:47:06 -0700

    Fix PR comments in example

--- examples/api/lib/material/carousel/carousel.0.dart
+++ examples/api/lib/material/carousel/carousel.0.dart
@@ -40,13 +40,7 @@ class CarouselExample extends StatefulWidget {
 }
 
 class _CarouselExampleState extends State<CarouselExample> {
-  late final CarouselController controller;
-
-  @override
-  void initState() {
-    controller = CarouselController(initialItem: 1);
-    super.initState();
-  }
+  final CarouselController controller = CarouselController(initialItem: 1);
 
   @override
   void dispose() {
@@ -65,7 +59,7 @@ class _CarouselExampleState extends State<CarouselExample> {
           child: CarouselView.weighted(
             controller: controller,
             itemSnapping: true,
-            layoutWeights: const <int>[1,7,1],
+            layoutWeights: const <int>[1, 7, 1],
             children: ImageInfo.values.map((ImageInfo image) {
               return HeroLayoutCard(imageInfo: image);
             }).toList(),
@@ -93,7 +87,7 @@ class _CarouselExampleState extends State<CarouselExample> {
         ConstrainedBox(
           constraints: const BoxConstraints(maxHeight: 200),
           child: CarouselView.weighted(
-            layoutWeights: const <int>[3,3,3,2,1],
+            layoutWeights: const <int>[3, 3, 3, 2, 1],
             allowFullyExpand: false,
             children: CardInfo.values.map((CardInfo info) {
               return ColoredBox(
