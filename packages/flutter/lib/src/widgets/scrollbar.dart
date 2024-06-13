d65e7c5eb130a0458506ec321a40ef853613f2b8
Hans Muller <hans.muller@gmail.com>
2024-06-13 10:20:58 -0700

    RawScrollbar: don't listen for drag gestures when scrolling is not possible (#149925)
    Fixes https://github.com/flutter/flutter/issues/149803

--- packages/flutter/lib/src/widgets/scrollbar.dart
+++ packages/flutter/lib/src/widgets/scrollbar.dart
@@ -1953,9 +1953,17 @@ class RawScrollbarState<T extends RawScrollbar> extends State<T> with TickerProv
     instance.dragStartBehavior = DragStartBehavior.down;
   }
 
+  bool _canHandleScrollGestures() {
+    return enableGestures
+      && _effectiveScrollController != null
+      && _effectiveScrollController!.positions.length == 1
+      && _effectiveScrollController!.position.hasContentDimensions
+      && _effectiveScrollController!.position.maxScrollExtent > 0.0;
+  }
+
   Map<Type, GestureRecognizerFactory> get _gestures {
     final Map<Type, GestureRecognizerFactory> gestures = <Type, GestureRecognizerFactory>{};
-    if (_effectiveScrollController == null || !enableGestures) {
+    if (!_canHandleScrollGestures()) {
       return gestures;
     }
 
