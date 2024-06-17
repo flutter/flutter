63a91528a20ed98497c84f3db5ded74e54197580
Polina Cherkasova <polina.c@live.com>
2024-06-17 00:42:36 -0700

    Update decoration_image_lerp_test.dart

--- packages/flutter/test/painting/decoration_image_lerp_test.dart
+++ packages/flutter/test/painting/decoration_image_lerp_test.dart
@@ -19,7 +19,7 @@ import 'package:leak_tracker_flutter_testing/leak_tracker_flutter_testing.dart';
 void main() {
   testWidgets('ImageDecoration.lerp',
   // // TODO(polina-c): make sure images are disposed, https://github.com/flutter/flutter/issues/141388 [leaks-to-clean]
-  // experimentalLeakTesting: LeakTesting.settings.withIgnoredAll(),
+  experimentalLeakTesting: LeakTesting.settings.withCreationStackTrace(),
   (WidgetTester tester) async {
     final MemoryImage green = MemoryImage(Uint8List.fromList(<int>[
       0x89, 0x50, 0x4e, 0x47, 0x0d, 0x0a, 0x1a, 0x0a,  0x00, 0x00, 0x00, 0x0d, 0x49, 0x48, 0x44, 0x52,
