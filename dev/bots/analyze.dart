a6305443370c32b17b2d2e4d8810458869f9d4d0
Kate Lovett <katelovett@google.com>
2024-06-17 11:35:35 -0500

    Update dev/bots/analyze.dart

--- dev/bots/analyze.dart
+++ dev/bots/analyze.dart
@@ -1308,7 +1308,6 @@ Future<void> verifyRepositoryLinks(String workingDirectory) async {
     'dart-lang/webdev',
     'eseidelGoogle/bezier_perf',
     'flutter/devtools', // TODO(guidezpl): remove when https://github.com/flutter/devtools/issues/7551 is closed
-    'flutter/flutter', // TODO(fujino): https://github.com/flutter/flutter/issues/121564
     'flutter/flutter_gallery_assets', // TODO(guidezpl): remove when subtask in https://github.com/flutter/flutter/issues/121564 is complete
     'flutter/flutter-intellij', // TODO(guidezpl): remove when https://github.com/flutter/flutter-intellij/issues/7342 is closed
     'flutter/platform_tests', // TODO(guidezpl): remove when subtask in https://github.com/flutter/flutter/issues/121564 is complete
