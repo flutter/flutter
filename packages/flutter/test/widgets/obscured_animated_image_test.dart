bfdcb51643b3b139ea0e7b30b164ca08615488f0
Polina Cherkasova <polinach@google.com>
2024-06-20 06:44:11 -0700

    Clean leaky tests. (#150335)

--- packages/flutter/test/widgets/obscured_animated_image_test.dart
+++ packages/flutter/test/widgets/obscured_animated_image_test.dart
@@ -8,7 +8,6 @@ import 'dart:ui' as ui show Image;
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:leak_tracker_flutter_testing/leak_tracker_flutter_testing.dart';
 
 import '../image_data.dart';
 import '../painting/fake_codec.dart';
@@ -18,10 +17,7 @@ Future<void> main() async {
   final FakeCodec fakeCodec = await FakeCodec.fromData(Uint8List.fromList(kAnimatedGif));
   final FakeImageProvider fakeImageProvider = FakeImageProvider(fakeCodec);
 
-  testWidgets('Obscured image does not animate',
-  // TODO(polina-c): dispose ImageStreamCompleterHandle, https://github.com/flutter/flutter/issues/145599 [leaks-to-clean]
-  experimentalLeakTesting: LeakTesting.settings.withIgnoredAll(),
-  (WidgetTester tester) async {
+  testWidgets('Obscured image does not animate', (WidgetTester tester) async {
     final GlobalKey imageKey = GlobalKey();
     await tester.pumpWidget(
       MaterialApp(
@@ -47,5 +43,6 @@ Future<void> main() async {
     await tester.pump(const Duration(milliseconds: 100));
     final ui.Image? image4 = renderImage.image;
     expect(image3, same(image4));
+    imageCache.clear();
   });
 }
