9efa084d12029c9e803294a524d9137bbc5bb220
yinxu <yinxulolol@gmail.com>
2024-06-24 17:13:51 +0800

    update

--- packages/flutter/test/widgets/page_view_test.dart
+++ packages/flutter/test/widgets/page_view_test.dart
@@ -1373,8 +1373,7 @@ void main() {
     });
   });
 
-  testWidgets('Get the page value before the content dimension is determined, do not throw an assertion, and return null', (WidgetTester tester) async {
-    // Regression test for https://github.com/flutter/flutter/issues/146986
+  testWidgets('Get the page value before the content dimension is determined,do not throw an assertion and return null', (WidgetTester tester) async {
     final PageController controller = PageController();
     addTearDown(controller.dispose);
     double? currentPage;
