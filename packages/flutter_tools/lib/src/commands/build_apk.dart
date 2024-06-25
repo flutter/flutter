a5a3577451a507df2b045c83a745653fd673a52f
Matan Lurey <matanlurey@users.noreply.github.com>
2024-06-25 15:41:23 -0700

    Read `AndroidManifest.xml` and emit `manifest-impeller-(enabled|disabled)` analytics (#150791)
    Work towards https://github.com/flutter/flutter/issues/132712.
    
    After this PR, after a completed `flutter build apk` command, we:
    
    - Emit a `manifest-impeller-disabled` command if `io.flutter.embedding.android.EnableImpeller` is `'false'`.
    - Emit a `manifest-impeller-disabled` command if `io.flutter.embedding.android.EnableImpeller` is _missing_.
    - Emit a `manifest-impeller-enabled` command if `io.flutter.embedding.android.EnableImpeller` is `'true'`.
    
    We will need to change the default (see `_impellerEnabledByDefault` in `project.dart`) before releasing, otherwise we will misreport `manifest-impeller-disabled` at a much higher rate than actual. If there is a way to instead compute the default instead of hard-coding, that would have been good.
    
    See <https://docs.flutter.dev/perf/impeller#android> for details on the key-value pair.
    
    ---
    
    I also did a tad of TLC, by removing the (now-defunct) `Usage` events for `flutter build ios`, so they are consistent.
    
    /cc @zanderso, @chinmaygarde, @jonahwilliams

--- packages/flutter_tools/lib/src/commands/build_apk.dart
+++ packages/flutter_tools/lib/src/commands/build_apk.dart
@@ -137,12 +137,26 @@ class BuildApkCommand extends BuildSubCommand {
     validateBuild(androidBuildInfo);
     displayNullSafetyMode(androidBuildInfo.buildInfo);
     globals.terminal.usesTerminalUi = true;
+    final FlutterProject project = FlutterProject.current();
     await androidBuilder?.buildApk(
-      project: FlutterProject.current(),
+      project: project,
       target: targetFile,
       androidBuildInfo: androidBuildInfo,
       configOnly: configOnly,
     );
+
+    // When an app is successfully built, record to analytics whether Impeller
+    // is enabled or disabled. Note that 'computeImpellerEnabled' will default
+    // to false if not enabled explicitly in the manifest.
+    final bool impellerEnabled = project.android.computeImpellerEnabled();
+    final String buildLabel = impellerEnabled
+          ? 'manifest-impeller-enabled'
+          : 'manifest-impeller-disabled';
+    globals.analytics.send(Event.flutterBuildInfo(
+      label: buildLabel,
+      buildType: 'android',
+    ));
+
     return FlutterCommandResult.success();
   }
 }
