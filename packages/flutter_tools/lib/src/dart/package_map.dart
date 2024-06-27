adb963b3196341dc04b9e759c0bdc66543a9b9b4
Sigurd Meldgaard <sigurdm@google.com>
2024-06-27 07:29:21 +0000

    Reinstate .packages check

--- packages/flutter_tools/lib/src/dart/package_map.dart
+++ packages/flutter_tools/lib/src/dart/package_map.dart
@@ -32,6 +32,11 @@ File? findPackageConfigFile(Directory dir) {
     if (fileSystem.file(candidatePackageConfigFile).existsSync()) {
       return candidatePackageConfigFile;
     }
+    // TODO(sigurdm): we should not need to check this file, it is obsolete.
+    final File candidatePackagesFile = fileSystem.file(fileSystem.path.join(candidateDir, '.packages'));
+    if (fileSystem.file(candidatePackagesFile).existsSync()) {
+      return candidatePackagesFile;
+    }
     final String parentDir = fileSystem.path.dirname(candidateDir);
     if (fileSystem.identicalSync(parentDir, candidateDir)) {
       return null;
