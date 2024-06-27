14c80929d7de7a7dc9f090755a7bc002687f7878
abdalmonem <abnhamoda@gmail.com>
2024-06-27 11:50:17 +0300

    re-init

--- packages/flutter/lib/src/material/segmented_button.dart
+++ packages/flutter/lib/src/material/segmented_button.dart
@@ -134,7 +134,6 @@ class SegmentedButton<T> extends StatefulWidget {
     this.style,
     this.showSelectedIcon = true,
     this.selectedIcon,
-    this.direction = Axis.horizontal,
   })  : assert(segments.length > 0),
         assert(selected.length > 0 || emptySelectionAllowed),
         assert(selected.length < 2 || multiSelectionEnabled);
@@ -147,11 +146,6 @@ class SegmentedButton<T> extends StatefulWidget {
   /// If you need more than five options, consider using [FilterChip] or
   /// [ChoiceChip] widgets.
   final List<ButtonSegment<T>> segments;
-
-  /// How the [segments] get aligned, by default its [Axis.horizontal]
-  /// if [Axis.vertical] the segments buttons will aligned in the "y" axis
-  /// and the first item in [segments] will be on the top.
-  final Axis direction;
 
   /// The set of [ButtonSegment.value]s that indicate which [segments] are
   /// selected.
@@ -801,28 +795,17 @@ class _RenderSegmentedButton<T> extends RenderBox with
     double start = 0.0;
     while (child != null) {
       final _SegmentedButtonContainerBoxParentData childParentData = child.parentData! as _SegmentedButtonContainerBoxParentData;
-      late final Offset childOffset;
-      late final Rect childRect, rChildRect;
-      if (direction == Axis.vertical) {
-        childOffset = Offset(0.0, start);
-        childParentData.offset = childOffset;
-        childRect = Rect.fromLTWH(0.0, childParentData.offset.dy, child.size.width, child.size.height);
-        rChildRect = RRect.fromRectAndCorners(childRect);
-        childParentData.surroundingRect = rChildRect;
-        start += child.size.height;
-      } else {
-        childOffset = Offset(start, 0.0);
-        childParentData.offset = childOffset;
-        childRect = Rect.fromLTWH(start, 0.0, child.size.width, child.size.height);
-        rChildRect = RRect.fromRectAndCorners(childRect);
-        childParentData.surroundingRect = rChildRect;
-        start += child.size.width;
-      }
+      final Offset childOffset = Offset(start, 0.0);
+      childParentData.offset = childOffset;
+      final Rect childRect = Rect.fromLTWH(start, 0.0, child.size.width, child.size.height);
+      final RRect rChildRect = RRect.fromRectAndCorners(childRect);
+      childParentData.surroundingRect = rChildRect;
+      start += child.size.width;
       child = nextChild(child);
     }
   }
 
-  Size _calculateHorizontalChildSize(BoxConstraints constraints) {
+  Size _calculateChildSize(BoxConstraints constraints) {
     double maxHeight = 0;
     RenderBox? child = firstChild;
     double childWidth;
@@ -845,37 +828,7 @@ class _RenderSegmentedButton<T> extends RenderBox with
     return Size(childWidth, maxHeight);
   }
 
-  Size _calculateVerticalChildSize(BoxConstraints constraints) {
-    double maxWidth = 0;
-    RenderBox? child = firstChild;
-    double childHeight;
-    if (_isExpanded) {
-      childHeight = constraints.maxWidth / childCount;
-    } else {
-      childHeight = constraints.minWidth / childCount;
-      while (child != null) {
-        childHeight = math.max(childHeight, child.getMaxIntrinsicHeight(double.infinity));
-        child = childAfter(child);
-      }
-      childHeight = math.min(childHeight, constraints.maxWidth / childCount);
-    }
-    child = firstChild;
-    while (child != null) {
-      final double boxHeight = child.getMaxIntrinsicWidth(maxWidth);
-      maxWidth = math.max(maxWidth, boxHeight);
-      child = childAfter(child);
-    }
-    return Size(maxWidth, childHeight);
-  }
-
-  Size _calculateChildSize(BoxConstraints constraints) {
-    return direction == Axis.horizontal ? _calculateHorizontalChildSize(constraints) : _calculateVerticalChildSize(constraints);
-  }
-
   Size _computeOverallSizeFromChildSize(Size childSize) {
-    if (direction == Axis.vertical) {
-      return constraints.constrain(Size(childSize.width, childSize.height * childCount));
-    }
     return constraints.constrain(Size(childSize.width * childCount, childSize.height));
   }
 
@@ -981,15 +934,9 @@ class _RenderSegmentedButton<T> extends RenderBox with
         final BorderSide divider = segments[index - 1].enabled || segments[index].enabled
           ? enabledBorder.side.copyWith(strokeAlign: 0.0)
           : disabledBorder.side.copyWith(strokeAlign: 0.0);
-        if (direction == Axis.horizontal) {
-          final Offset top = Offset(dividerPos, borderRect.top);
-          final Offset bottom = Offset(dividerPos, borderRect.bottom);
-          context.canvas.drawLine(top, bottom, divider.toPaint());
-        } else if (direction == Axis.vertical) {
-          final Offset start = Offset(borderRect.left, childRect.top);
-          final Offset end = Offset(borderRect.right, childRect.top);
-          context.canvas.drawLine(start, end, divider.toPaint());
-        }
+        final Offset top = Offset(dividerPos, borderRect.top);
+        final Offset bottom = Offset(dividerPos, borderRect.bottom);
+        context.canvas.drawLine(top, bottom, divider.toPaint());
       }
 
       previousChild = child;
