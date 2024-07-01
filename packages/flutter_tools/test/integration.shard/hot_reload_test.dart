3e17c297b285fa40efb10f3a589b644c48a05038
Andrew Kolos <andrewrkolos@gmail.com>
2024-06-30 21:39:55 -0700

    fix experiment: what happens when a test hangs

--- packages/flutter_tools/test/integration.shard/hot_reload_test.dart
+++ packages/flutter_tools/test/integration.shard/hot_reload_test.dart
@@ -81,6 +81,7 @@ void main() {
   });
 
   testWithoutContext('hot restart works without error', () async {
+    await Future<void>.delayed(const Duration(hours: 1));
     await flutter.run(verbose: true);
     await flutter.hotRestart();
   });
