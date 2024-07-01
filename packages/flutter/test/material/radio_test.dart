7f50483f354f49d0ec90aa2fc11a83f43592caf2
Greg Spencer <gspencer@google.com>
2024-07-01 14:46:45 -0700

    Fix some more tests.

--- packages/flutter/test/material/radio_test.dart
+++ packages/flutter/test/material/radio_test.dart
@@ -205,7 +205,7 @@ void main() {
         ),
       ),
     ));
-    final bool isApple = defaultTargetPlatform == TargetPlatform.iOS ||
+    final bool isCupertino = defaultTargetPlatform == TargetPlatform.iOS ||
                          defaultTargetPlatform == TargetPlatform.macOS;
     expect(
       semantics,
@@ -217,10 +217,11 @@ void main() {
           SemanticsFlag.isEnabled,
           SemanticsFlag.isFocusable,
           SemanticsFlag.isChecked,
-          if (isApple) SemanticsFlag.isSelected,
+          if (isCupertino) SemanticsFlag.isSelected,
         ],
         actions: <SemanticsAction>[
           SemanticsAction.tap,
+          if (defaultTargetPlatform != TargetPlatform.iOS) SemanticsAction.focus,
         ],
       ),
     );
