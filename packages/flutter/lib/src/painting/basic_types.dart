8741026d4328fb7964b9318e3553a58d161be132
Michael Goderbauer <goderbauer@google.com>
2024-07-02 09:58:46 -0700

    Update basic_types.dart

--- packages/flutter/lib/src/painting/basic_types.dart
+++ packages/flutter/lib/src/painting/basic_types.dart
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-/// @docImport 'package:flutter/rendering.dart';
 /// @docImport 'package:flutter/material.dart';
+/// @docImport 'package:flutter/rendering.dart';
 ///
 /// @docImport 'text_span.dart';
 /// @docImport 'text_style.dart';
