9fb199cf1984afba872153af214b2df19f81b753
Michael Goderbauer <goderbauer@google.com>
2024-07-02 10:30:39 -0700

    docimports for cupertino (#151149)
    Part of https://github.com/flutter/flutter/issues/150800
    
    Cupertino is fully clean after this!

--- packages/flutter/lib/src/cupertino/checkbox.dart
+++ packages/flutter/lib/src/cupertino/checkbox.dart
@@ -2,6 +2,12 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+/// @docImport 'package:flutter/material.dart';
+///
+/// @docImport 'slider.dart';
+/// @docImport 'switch.dart';
+library;
+
 import 'package:flutter/widgets.dart';
 
 import 'colors.dart';
