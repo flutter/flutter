00fc40cfcd5b1a72e0dd4957c51f709700206f74
Michal-MK <michalhz159@gmail.com>
2024-07-02 22:43:22 +0200

    Fix Linux analyze test

--- packages/flutter/lib/src/widgets/scroll_position.dart
+++ packages/flutter/lib/src/widgets/scroll_position.dart
@@ -275,7 +275,7 @@ abstract class ScrollPosition extends ViewportOffset with ScrollMetrics {
   /// This is value relates to the current [ScrollActivity], which determines
   /// if additional touch input should be received by the scroll view or its children.
   /// If the position is overscrolled, as is allowed by [BouncingScrollPhysics],
-  /// children of the scroll view will receive pointer events as the scroll view 
+  /// children of the scroll view will receive pointer events as the scroll view
   /// settles back from the overscrolled state.
   bool get shouldIgnorePointer => !outOfRange && (activity?.shouldIgnorePointer ?? true);
 
