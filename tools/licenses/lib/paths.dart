7f72aebebf017b362bdadef6bc20fb5824e2f045
Matan Lurey <matanlurey@users.noreply.github.com>
2024-07-03 11:40:23 -0700

    Move `//third_party/android_embedding_dependencies` to `//flutter/third_party`. (#53587)
    ... as part of the buildmoot effort.
    
    I'll rebase after @gmackall's definitely-will-work-this-time bump of the dependencies.

--- tools/licenses/lib/paths.dart
+++ tools/licenses/lib/paths.dart
@@ -31,6 +31,7 @@ final Set<String> skippedPaths = <String>{
   r'flutter/lib/web_ui/dev', // these are build tools; they do not end up in Engine artifacts
   r'flutter/prebuilts',
   r'flutter/sky/packages/sky_engine/LICENSE',
+  r'flutter/third_party/android_embedding_dependencies', // Not shipped. Used only for the build-time classpath, and for the in-tree testing framework for Android
   r'flutter/third_party/android_tools', // excluded on advice
   r'flutter/third_party/angle/android',
   r'flutter/third_party/angle/doc',
@@ -220,7 +221,6 @@ final Set<String> skippedPaths = <String>{
   r'fuchsia/sdk/mac/NOTICE.fuchsia',
   r'fuchsia/sdk/mac/tools',
   r'out', // output of build
-  r'third_party/android_embedding_dependencies', // Not shipped. Used only for the build-time classpath, and for the in-tree testing framework for Android
   r'third_party/android_tools', // excluded on advice
   r'third_party/java', // only used for Android builds
   r'third_party/libxml', // dependency of the testing system that we don't actually use
