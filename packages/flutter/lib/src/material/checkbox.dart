e986f1d6be2b1432b0dc1b68ff1f560577b200e9
Victor Sanni <victorsanniay@gmail.com>
2024-07-08 15:54:10 -0700

    (take 2) Revert checkbox.adaptive

--- packages/flutter/lib/src/material/checkbox.dart
+++ packages/flutter/lib/src/material/checkbox.dart
@@ -106,10 +106,11 @@ class Checkbox extends StatefulWidget {
   /// graphics expected on iOS. On other platforms, this creates a Material
   /// design [Checkbox].
   ///
-  /// If a [CupertinoCheckbox] is created, [hoverColor], [overlayColor], and
-  /// [splashRadius] are ignored because a Cupertino-design checkbox does not
-  /// have a radial reaction overlay. [materialTapTargetSize] and [visualDensity]
-  /// are also ignored because both are Material-design-specific configurations.
+  /// If a [CupertinoCheckbox] is created, the following parameters are ignored:
+  /// [mouseCursor], [fillColor], [hoverColor], [overlayColor], [splashRadius],
+  /// [materialTapTargetSize], [visualDensity], [isError]. However, [shape] and
+  /// [side] will still affect the [CupertinoCheckbox] and should be handled if
+  /// native fidelity is important.
   ///
   /// The target platform is based on the current [Theme]: [ThemeData.platform].
   const Checkbox.adaptive({
@@ -469,17 +470,6 @@ class _CheckboxState extends State<Checkbox> with TickerProviderStateMixin, Togg
 
   @override
   Widget build(BuildContext context) {
-    final CheckboxThemeData checkboxTheme = CheckboxTheme.of(context);
-
-    // Colors need to be resolved in selected and non selected states separately
-    final Set<MaterialState> activeStates = states..add(MaterialState.selected);
-    final Set<MaterialState> inactiveStates = states..remove(MaterialState.selected);
-    if (widget.isError) {
-      activeStates.add(MaterialState.error);
-      inactiveStates.add(MaterialState.error);
-    }
-    final Set<MaterialState> checkStates = widget.isError ? (states..add(MaterialState.error)) : states;
-
     switch (widget._checkboxType) {
       case _CheckboxType.material:
         break;
@@ -498,22 +488,19 @@ class _CheckboxState extends State<Checkbox> with TickerProviderStateMixin, Togg
               value: value,
               tristate: tristate,
               onChanged: onChanged,
-              mouseCursor: widget.mouseCursor ?? checkboxTheme.mouseCursor?.resolve(states),
               activeColor: widget.activeColor,
-              fillColor: widget.fillColor ?? checkboxTheme.fillColor,
-              checkColor: widget.checkColor ?? checkboxTheme.checkColor?.resolve(checkStates),
+              checkColor: widget.checkColor,
               focusColor: widget.focusColor,
               focusNode: widget.focusNode,
               autofocus: widget.autofocus,
-              side: widget.side ?? checkboxTheme.side,
-              shape: widget.shape ?? checkboxTheme.shape,
-              isError: widget.isError,
-              semanticLabel: widget.semanticLabel,
+              side: widget.side,
+              shape: widget.shape,
             );
         }
     }
 
     assert(debugCheckHasMaterial(context));
+    final CheckboxThemeData checkboxTheme = CheckboxTheme.of(context);
     final CheckboxThemeData defaults = Theme.of(context).useMaterial3
       ? _CheckboxDefaultsM3(context)
       : _CheckboxDefaultsM2(context);
@@ -535,6 +522,13 @@ class _CheckboxState extends State<Checkbox> with TickerProviderStateMixin, Togg
         ?? MaterialStateMouseCursor.clickable.resolve(states);
     });
 
+    // Colors need to be resolved in selected and non selected states separately
+    final Set<MaterialState> activeStates = states..add(MaterialState.selected);
+    final Set<MaterialState> inactiveStates = states..remove(MaterialState.selected);
+    if (widget.isError) {
+      activeStates.add(MaterialState.error);
+      inactiveStates.add(MaterialState.error);
+    }
     final Color? activeColor = widget.fillColor?.resolve(activeStates)
       ?? _widgetFillColor.resolve(activeStates)
       ?? checkboxTheme.fillColor?.resolve(activeStates);
@@ -592,6 +586,7 @@ class _CheckboxState extends State<Checkbox> with TickerProviderStateMixin, Togg
         : effectiveInactivePressedOverlayColor;
     }
 
+    final Set<MaterialState> checkStates = widget.isError ? (states..add(MaterialState.error)) : states;
     final Color effectiveCheckColor = widget.checkColor
       ?? checkboxTheme.checkColor?.resolve(checkStates)
       ?? defaults.checkColor!.resolve(checkStates)!;
