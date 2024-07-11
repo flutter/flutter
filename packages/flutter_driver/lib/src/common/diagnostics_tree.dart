c4a77832d25f943912dc964f0391ad8a3258f1af
Michael Goderbauer <goderbauer@google.com>
2024-07-11 11:30:26 -0700

    docimports for flutter_driver (#151267)
    Part of https://github.com/flutter/flutter/issues/150800

--- packages/flutter_driver/lib/src/common/diagnostics_tree.dart
+++ packages/flutter_driver/lib/src/common/diagnostics_tree.dart
@@ -2,6 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+/// @docImport 'package:flutter/foundation.dart';
+library;
+
 import 'enum_util.dart';
 import 'find.dart';
 import 'message.dart';
