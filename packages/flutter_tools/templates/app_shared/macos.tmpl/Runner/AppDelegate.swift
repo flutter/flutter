15ab883c4f74ea71ec911133736fd319cadf9490
Chris Bracken <chris@bracken.jp>
2024-07-11 11:21:06 -0700

    [macOS] support secure restorable state by default
    By default, Flutter apps only do default AppKit app serialisation of
    Window location etc. and by default, state serialisation in AppKit apps
    is compatible with NSSecureCoding. AppKit apps generated since Xcode
    13.2 include this method in the app delegate generated by the default
    app template.
    
    Background
    ==========
    
    This method was added to opt into having [de]serialization  require a
    coder implementing the `NSSecureCoding` protocol. Apple wasn't able to
    force this across the board, because `NSSecureCoding` limits certain
    behaviours during deserialisation, which some third-party apps have have
    previously relied on.
    
    Specific background on the sorts of vulnerabilities that
    `NSSecureCoding` was designed to prevent are described in the
    `NSSecureCoding` documentation:
    https://developer.apple.com/documentation/foundation/nssecurecoding?language=objc
    
    A demonstration of a root privilege escalation and SIP bypass
    vulnerability is described in the following blog post:
    https://sector7.computest.nl/post/2022-08-process-injection-breaking-all-macos-security-layers-with-a-single-vulnerability/
    
    Issue: https://github.com/flutter/flutter/issues/150062

--- packages/flutter_tools/templates/app_shared/macos.tmpl/Runner/AppDelegate.swift
+++ packages/flutter_tools/templates/app_shared/macos.tmpl/Runner/AppDelegate.swift
@@ -6,4 +6,11 @@ class AppDelegate: FlutterAppDelegate {
   override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
     return true
   }
+
+  override func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+    // If you are customizing app state serialization and deserialization,
+    // ensure that such serialization is compatible with NSSecureCoding, or
+    // return false here.
+    return true
+  }
 }
