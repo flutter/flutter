b27d82aa16f7c5ec1f759277351e61a434bc749b
Victor Sanni <victorsanniay@gmail.com>
2024-07-16 10:47:18 -0700

    Add `semanticLabel` to `CupertinoCheckbox` (#151789)

--- packages/flutter/lib/src/widgets/image_icon.dart
+++ packages/flutter/lib/src/widgets/image_icon.dart
@@ -57,7 +57,7 @@ class ImageIcon extends StatelessWidget {
 
   /// Semantic label for the icon.
   ///
-  /// Announced in accessibility modes (e.g TalkBack/VoiceOver).
+  /// Announced by assistive technologies (e.g TalkBack/VoiceOver).
   /// This label does not show in the UI.
   ///
   ///  * [SemanticsProperties.label], which is set to [semanticLabel] in the
