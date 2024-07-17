0a1d550e5fd1fb991ec7a2b1d5c9829184e1038d
flyboy <yinxulolol@gmail.com>
2024-07-17 22:06:13 +0800

    Add `ListTileControlAffinity` to `ListTileTheme` (#150695)
    fixes https://github.com/flutter/flutter/issues/146414
    
    --- 
    
    I saw @abikko submitted a PR https://github.com/flutter/flutter/pull/146630, but it was not completed due to CLA and lack of test cases.
    I am willing to complete this PR in combination with @abikko's code (I don't know if this is allowed) 
    
     This PR adds contorlAffinity to ListTileTheme so that [CheckboxListTile], [RadioListTile], [SwitchListTile], and [ExpansionTile] can read and use it 
    
     For example: If ListTileTheme in Theme sets contorlAffinity, then [CheckboxListTile] can directly use contorlAffinity in ListTileTheme. Of course, if contorlAffinity is also set in [CheckboxListTile], the property in [CheckboxListTile] will be used first.

--- packages/flutter/test/material/radio_list_tile_test.dart
+++ packages/flutter/test/material/radio_list_tile_test.dart
@@ -1547,4 +1547,39 @@ void main() {
       );
     });
   });
+
+  testWidgets('RadioListTile uses ListTileTheme controlAffinity', (WidgetTester tester) async {
+    Widget buildListTile(ListTileControlAffinity controlAffinity) {
+      return MaterialApp(
+        home: Material(
+          child: ListTileTheme(
+            data: ListTileThemeData(
+              controlAffinity: controlAffinity,
+            ),
+            child: RadioListTile<double>(
+              value: 0.5,
+              groupValue: 1.0,
+              title: const Text('RadioListTile'),
+              onChanged: (double? value) {},
+            ),
+          ),
+        ),
+      );
+    }
+
+    await tester.pumpWidget(buildListTile(ListTileControlAffinity.leading));
+    final Finder leading = find.text('RadioListTile');
+    final Offset offsetLeading = tester.getTopLeft(leading);
+    expect(offsetLeading, const Offset(72.0, 16.0));
+
+    await tester.pumpWidget(buildListTile(ListTileControlAffinity.trailing));
+    final Finder trailing = find.text('RadioListTile');
+    final Offset offsetTrailing = tester.getTopLeft(trailing);
+    expect(offsetTrailing, const Offset(16.0, 16.0));
+
+    await tester.pumpWidget(buildListTile(ListTileControlAffinity.platform));
+    final Finder platform = find.text('RadioListTile');
+    final Offset offsetPlatform = tester.getTopLeft(platform);
+    expect(offsetPlatform, const Offset(72.0, 16.0));
+  });
 }
