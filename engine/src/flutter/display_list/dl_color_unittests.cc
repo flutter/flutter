2d58c396154a5fe588bf9064cf8a31e9605d025f
Jim Graham <flar@google.com>
2024-07-17 16:11:55 -0700

    [Impeller] Convert basic AIKS golden tests to use DisplayList (flutter/engine#53969)
    Converts several AIKS golden tests to use DisplayList as the mechanism.
    
    In order to convert some of the tests, new factory methods were added to DlColor and tested with new unit tests (an earlier golden test conversion PR had a version of this as well).
    
    Also, a new DisplayList record op was created for ClipOval to handle the AIKS clipping golden tests, but this new recording op is not used from Flutter `ui` code (no plumbing to call it from `lib/ui/painting` or to convert any other DisplayList call to use the new record). An earlier attempt to add the new recording op caused a large number of golden changes upstream so this version will only be used for internal tests and support to use it from apps will follow in more targeted PRs to better manage golden changes. This PR should not result in any changes to goldens outside of internal engine tests.

--- engine/src/flutter/display_list/dl_color_unittests.cc
+++ engine/src/flutter/display_list/dl_color_unittests.cc
@@ -45,5 +45,190 @@ TEST(DisplayListColor, DlColorDirectlyComparesToSkColor) {
   EXPECT_EQ(DlColor::kBlue(), SK_ColorBLUE);
 }
 
+TEST(DisplayListColor, DlColorFloatConstructor) {
+  EXPECT_EQ(DlColor::ARGB(1.0f, 1.0f, 1.0f, 1.0f), DlColor(0xFFFFFFFF));
+  EXPECT_EQ(DlColor::ARGB(0.0f, 0.0f, 0.0f, 0.0f), DlColor(0x00000000));
+  EXPECT_EQ(DlColor::ARGB(0.5f, 0.5f, 0.5f, 0.5f), DlColor(0x80808080));
+  EXPECT_EQ(DlColor::ARGB(1.0f, 0.0f, 0.5f, 1.0f), DlColor(0xFF0080FF));
+
+  EXPECT_EQ(DlColor::RGBA(1.0f, 1.0f, 1.0f, 1.0f), DlColor(0xFFFFFFFF));
+  EXPECT_EQ(DlColor::RGBA(0.0f, 0.0f, 0.0f, 0.0f), DlColor(0x00000000));
+  EXPECT_EQ(DlColor::RGBA(0.5f, 0.5f, 0.5f, 0.5f), DlColor(0x80808080));
+  EXPECT_EQ(DlColor::RGBA(1.0f, 0.0f, 0.5f, 1.0f), DlColor(0xFFFF0080));
+}
+
+TEST(DisplayListColor, DlColorComponentGetters) {
+  {
+    DlColor test(0xFFFFFFFF);
+
+    EXPECT_EQ(test.getAlpha(), 0xFF);
+    EXPECT_EQ(test.getRed(), 0xFF);
+    EXPECT_EQ(test.getGreen(), 0xFF);
+    EXPECT_EQ(test.getBlue(), 0xFF);
+
+    EXPECT_EQ(test.getAlphaF(), 1.0f);
+    EXPECT_EQ(test.getRedF(), 1.0f);
+    EXPECT_EQ(test.getGreenF(), 1.0f);
+    EXPECT_EQ(test.getBlueF(), 1.0f);
+  }
+
+  {
+    DlColor test = DlColor::ARGB(1.0f, 1.0f, 1.0f, 1.0f);
+
+    EXPECT_EQ(test.getAlpha(), 0xFF);
+    EXPECT_EQ(test.getRed(), 0xFF);
+    EXPECT_EQ(test.getGreen(), 0xFF);
+    EXPECT_EQ(test.getBlue(), 0xFF);
+
+    EXPECT_EQ(test.getAlphaF(), 1.0f);
+    EXPECT_EQ(test.getRedF(), 1.0f);
+    EXPECT_EQ(test.getGreenF(), 1.0f);
+    EXPECT_EQ(test.getBlueF(), 1.0f);
+  }
+
+  {
+    DlColor test(0x00000000);
+
+    EXPECT_EQ(test.getAlpha(), 0x00);
+    EXPECT_EQ(test.getRed(), 0x00);
+    EXPECT_EQ(test.getGreen(), 0x00);
+    EXPECT_EQ(test.getBlue(), 0x00);
+
+    EXPECT_EQ(test.getAlphaF(), 0.0f);
+    EXPECT_EQ(test.getRedF(), 0.0f);
+    EXPECT_EQ(test.getGreenF(), 0.0f);
+    EXPECT_EQ(test.getBlueF(), 0.0f);
+  }
+
+  {
+    DlColor test = DlColor::ARGB(0.0f, 0.0f, 0.0f, 0.0f);
+
+    EXPECT_EQ(test.getAlpha(), 0x00);
+    EXPECT_EQ(test.getRed(), 0x00);
+    EXPECT_EQ(test.getGreen(), 0x00);
+    EXPECT_EQ(test.getBlue(), 0x00);
+
+    EXPECT_EQ(test.getAlphaF(), 0.0f);
+    EXPECT_EQ(test.getRedF(), 0.0f);
+    EXPECT_EQ(test.getGreenF(), 0.0f);
+    EXPECT_EQ(test.getBlueF(), 0.0f);
+  }
+
+  {
+    DlColor test(0x7F7F7F7F);
+
+    EXPECT_EQ(test.getAlpha(), 0x7F);
+    EXPECT_EQ(test.getRed(), 0x7F);
+    EXPECT_EQ(test.getGreen(), 0x7F);
+    EXPECT_EQ(test.getBlue(), 0x7F);
+
+    const DlScalar half = 127.0f * (1.0f / 255.0f);
+
+    EXPECT_EQ(test.getAlphaF(), half);
+    EXPECT_EQ(test.getRedF(), half);
+    EXPECT_EQ(test.getGreenF(), half);
+    EXPECT_EQ(test.getBlueF(), half);
+  }
+
+  {
+    DlColor test = DlColor::ARGB(0.5f, 0.5f, 0.5f, 0.5f);
+
+    EXPECT_EQ(test.getAlpha(), 0x80);
+    EXPECT_EQ(test.getRed(), 0x80);
+    EXPECT_EQ(test.getGreen(), 0x80);
+    EXPECT_EQ(test.getBlue(), 0x80);
+
+    const DlScalar half = 128.0f * (1.0f / 255.0f);
+
+    EXPECT_EQ(test.getAlphaF(), half);
+    EXPECT_EQ(test.getRedF(), half);
+    EXPECT_EQ(test.getGreenF(), half);
+    EXPECT_EQ(test.getBlueF(), half);
+  }
+
+  {
+    DlColor test(0x1F2F3F4F);
+
+    EXPECT_EQ(test.getAlpha(), 0x1F);
+    EXPECT_EQ(test.getRed(), 0x2F);
+    EXPECT_EQ(test.getGreen(), 0x3F);
+    EXPECT_EQ(test.getBlue(), 0x4F);
+
+    EXPECT_EQ(test.getAlphaF(), 0x1f * (1.0f / 255.0f));
+    EXPECT_EQ(test.getRedF(), 0x2f * (1.0f / 255.0f));
+    EXPECT_EQ(test.getGreenF(), 0x3f * (1.0f / 255.0f));
+    EXPECT_EQ(test.getBlueF(), 0x4f * (1.0f / 255.0f));
+  }
+
+  {
+    DlColor test = DlColor::ARGB(0.1f, 0.2f, 0.3f, 0.4f);
+
+    EXPECT_EQ(test.getAlpha(), round(0.1f * 255));
+    EXPECT_EQ(test.getRed(), round(0.2f * 255));
+    EXPECT_EQ(test.getGreen(), round(0.3f * 255));
+    EXPECT_EQ(test.getBlue(), round(0.4f * 255));
+
+    // Unfortunately conversion from float to 8-bit back to float is lossy
+    EXPECT_EQ(test.getAlphaF(), round(0.1f * 255) * (1.0f / 255.0f));
+    EXPECT_EQ(test.getRedF(), round(0.2f * 255) * (1.0f / 255.0f));
+    EXPECT_EQ(test.getGreenF(), round(0.3f * 255) * (1.0f / 255.0f));
+    EXPECT_EQ(test.getBlueF(), round(0.4f * 255) * (1.0f / 255.0f));
+  }
+
+  {
+    DlColor test = DlColor::RGBA(0.2f, 0.3f, 0.4f, 0.1f);
+
+    EXPECT_EQ(test.getAlpha(), round(0.1f * 255));
+    EXPECT_EQ(test.getRed(), round(0.2f * 255));
+    EXPECT_EQ(test.getGreen(), round(0.3f * 255));
+    EXPECT_EQ(test.getBlue(), round(0.4f * 255));
+
+    // Unfortunately conversion from float to 8-bit back to float is lossy
+    EXPECT_EQ(test.getAlphaF(), round(0.1f * 255) * (1.0f / 255.0f));
+    EXPECT_EQ(test.getRedF(), round(0.2f * 255) * (1.0f / 255.0f));
+    EXPECT_EQ(test.getGreenF(), round(0.3f * 255) * (1.0f / 255.0f));
+    EXPECT_EQ(test.getBlueF(), round(0.4f * 255) * (1.0f / 255.0f));
+  }
+}
+
+TEST(DisplayListColor, DlColorOpaqueTransparent) {
+  auto test_argb = [](int a, int r, int g, int b) {
+    ASSERT_TRUE(a >= 0 && a <= 255);
+    ASSERT_TRUE(r >= 0 && r <= 255);
+    ASSERT_TRUE(g >= 0 && g <= 255);
+    ASSERT_TRUE(b >= 0 && b <= 255);
+
+    int argb = ((a << 24) | (r << 16) | (g << 8) | b);
+    bool is_opaque = (a == 255);
+    bool is_transprent = (a == 0);
+
+    EXPECT_EQ(DlColor(argb).isOpaque(), is_opaque);
+    EXPECT_EQ(DlColor(argb).isTransparent(), is_transprent);
+
+    DlScalar aF = a * (1.0f / 255.0f);
+    DlScalar rF = r * (1.0f / 255.0f);
+    DlScalar gF = g * (1.0f / 255.0f);
+    DlScalar bF = b * (1.0f / 255.0f);
+
+    EXPECT_EQ(DlColor::ARGB(aF, rF, gF, bF).isOpaque(), is_opaque);
+    EXPECT_EQ(DlColor::ARGB(aF, rF, gF, bF).isTransparent(), is_transprent);
+
+    EXPECT_EQ(DlColor::RGBA(rF, gF, bF, aF).isOpaque(), is_opaque);
+    EXPECT_EQ(DlColor::RGBA(rF, gF, bF, aF).isTransparent(), is_transprent);
+  };
+
+  for (int r = 0; r <= 255; r += 15) {
+    for (int g = 0; g <= 255; g += 15) {
+      for (int b = 0; b <= 255; b += 15) {
+        test_argb(0, r, g, b);
+        for (int a = 15; a < 255; a += 15) {
+          test_argb(a, r, g, b);
+        }
+        test_argb(255, r, g, b);
+      }
+    }
+  }
+}
+
 }  // namespace testing
 }  // namespace flutter
