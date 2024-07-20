a8287881f8c3948e6a006dc22ccdd7a664f2026f
LongCat is Looong <31859944+LongCatIsLooong@users.noreply.github.com>
2024-07-19 19:58:53 -0700

    Remove unnecessary dart:ui imports

--- packages/flutter/lib/src/material/constants.dart
+++ packages/flutter/lib/src/material/constants.dart
@@ -2,7 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-/// @docImport 'dart:ui';
 /// @docImport 'package:flutter/cupertino.dart';
 ///
 /// @docImport 'app_bar.dart';
