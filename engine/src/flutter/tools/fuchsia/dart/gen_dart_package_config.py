08c7b8d6e2788e34dfaeca98589c9eaeaded5b6c
Chris Bracken <chris@bracken.jp>
2024-07-22 10:38:07 -0700

    Prepend third_party/pyyaml to python sys.path (flutter/engine#54023)
    Previously we were appending Flutter's fork of third_party/pyyaml to the Python sys.path. In the case where a bot image or local install already has pyyaml installed elsewhere on the path, ours will fail to be picked up. Instead prepend to the path.
    
    Issue spotted by jsimmons@.
    
    Related: https://github.com/flutter/engine/pull/54001
    
    [C++, Objective-C, Java style guides]: https://github.com/flutter/engine/blob/main/CONTRIBUTING.md#style

--- engine/src/flutter/tools/fuchsia/dart/gen_dart_package_config.py
+++ engine/src/flutter/tools/fuchsia/dart/gen_dart_package_config.py
@@ -15,7 +15,7 @@ import re
 import sys
 
 THIS_DIR = os.path.abspath(os.path.dirname(__file__))
-sys.path += [os.path.join(THIS_DIR, '..', '..', '..', 'third_party', 'pyyaml', 'lib')]
+sys.path.insert(0, os.path.join(THIS_DIR, '..', '..', '..', 'third_party', 'pyyaml', 'lib'))
 import yaml
 
 DEFAULT_LANGUAGE_VERSION = '2.8'
