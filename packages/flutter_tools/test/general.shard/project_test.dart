f33ffc00ea338eedc3e537be58f91f76c0ea236a
Jenn Magder <magder@google.com>
2024-07-22 16:54:24 -0700

    Use Xcode build setting PRODUCT_NAME to find app and archive paths (#140242)
    1. Instead of getting the `FULL_PRODUCT_NAME` Xcode build setting (`Runner.app`) instead use `PRODUCT_NAME` since most places really want the product name, and the extension stripping wasn't correct when the name contained periods.
    2. Don't instruct the user to open the `xcarchive` in Xcode if it doesn't exist.
    
    Fixes https://github.com/flutter/flutter/issues/140212

--- packages/flutter_tools/test/general.shard/project_test.dart
+++ packages/flutter_tools/test/general.shard/project_test.dart
@@ -1188,9 +1188,9 @@ plugins {
         mockXcodeProjectInterpreter = FakeXcodeProjectInterpreter();
       });
 
-      testUsingContext('app product name defaults to Runner.app', () async {
+      testUsingContext('app product name defaults to Runner', () async {
         final FlutterProject project = await someProject();
-        expect(await project.ios.hostAppBundleName(null), 'Runner.app');
+        expect(await project.ios.productName(null), 'Runner');
       }, overrides: <Type, Generator>{
         FileSystem: () => fs,
         ProcessManager: () => FakeProcessManager.any(),
@@ -1202,11 +1202,11 @@ plugins {
         project.ios.xcodeProject.createSync();
         const XcodeProjectBuildContext buildContext = XcodeProjectBuildContext(scheme: 'Runner');
         mockXcodeProjectInterpreter.buildSettingsByBuildContext[buildContext] = <String, String>{
-          'FULL_PRODUCT_NAME': 'My App.app',
+          'PRODUCT_NAME': 'My App',
         };
         mockXcodeProjectInterpreter.xcodeProjectInfo = XcodeProjectInfo(<String>[], <String>[], <String>['Runner'], logger);
 
-        expect(await project.ios.hostAppBundleName(null), 'My App.app');
+        expect(await project.ios.productName(null), 'My App');
       }, overrides: <Type, Generator>{
         FileSystem: () => fs,
         ProcessManager: () => FakeProcessManager.any(),
