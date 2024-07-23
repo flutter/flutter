53b17d4168b85d90cab8501e781789e87509c555
DBowen33 <denis.bowen@utexas.edu>
2024-07-23 19:03:52 +0000

    added functionality to where SR will communicate button pressed

--- dev/a11y_assessments/test/text_button_test.dart
+++ dev/a11y_assessments/test/text_button_test.dart
@@ -3,6 +3,7 @@
 // found in the LICENSE file.
 
 import 'package:a11y_assessments/use_cases/text_button.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'test_utils.dart';
@@ -10,6 +11,16 @@ import 'test_utils.dart';
 void main() {
   testWidgets('text button can run', (WidgetTester tester) async {
     await pumpsUseCase(tester, TextButtonUseCase());
-    expect(find.text('Action'), findsExactly(2));
+    expect(find.text('Action'), findsOneWidget);
+    expect(find.text('Action Disabled'), findsOneWidget);
+  });
+
+  testWidgets('snackbar shows on button press', (WidgetTester tester) async {
+    await pumpsUseCase(tester, TextButtonUseCase());
+
+    await tester.tap(find.text('Action'));
+    await tester.pump();
+
+    expect(find.widgetWithText(SnackBar, 'Text button is pressed'), findsOneWidget);
   });
 }
