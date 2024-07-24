272b495161144961b85bdab6caaf818a02e1329d
Renzo Olivares <roliv@google.com>
2024-07-24 12:22:32 -0700

    Fix tests, finalized selection event should not change extendSelectionInProgress

--- packages/flutter/lib/src/widgets/selectable_region.dart
+++ packages/flutter/lib/src/widgets/selectable_region.dart
@@ -2711,7 +2711,6 @@ abstract class MultiSelectableSelectionContainerDelegate extends SelectionContai
         _extendSelectionInProgress = false;
         result = handleSelectionEdgeUpdate(event as SelectionEdgeUpdateEvent);
       case SelectionEventType.selectionFinalized:
-        _extendSelectionInProgress = false;
         result = handleSelectionFinalized(event as SelectionFinalizedSelectionEvent);
       case SelectionEventType.clear:
         _extendSelectionInProgress = false;
