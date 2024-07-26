94d02a219e5f1845cbbacfe007d0698abdbc7eea
ValentinVignal <valentin.vignal.dev@outlook.fr>
2024-07-26 15:53:54 +0800

    test: Add tests for deletable_chip_attributes.on_deleted.0.dart

---
+++ examples/api/test/material/chip/deletable_chip_attributes.on_deleted.0_test.dart
@@ -0,0 +1,65 @@
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+import 'package:flutter_api_samples/material/chip/deletable_chip_attributes.on_deleted.0.dart' as example;
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('RawChip.deleteIconBoxConstraints updates delete icon size constraints', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      const example.OnDeletedExampleApp(),
+    );
+
+    expect(find.widgetWithText(AppBar, 'DeletableChipAttributes.onDeleted Sample'), findsOne);
+    expect(find.widgetWithText(Chip, 'Aaron Burr'), findsOne);
+    expect(find.widgetWithText(Chip, 'Alexander Hamilton'), findsOne);
+    expect(find.widgetWithText(Chip, 'Eliza Hamilton'), findsOne);
+    expect(find.widgetWithText(Chip, 'James Madison'), findsOne);
+
+    // Delete Alexander Hamilton.
+    await tester.tap(
+      find.descendant(of: find.widgetWithText(Chip, 'Alexander Hamilton'), matching: find.byIcon(Icons.cancel)),
+    );
+    await tester.pump();
+
+    expect(find.widgetWithText(Chip, 'Aaron Burr'), findsOne);
+    expect(find.widgetWithText(Chip, 'Alexander Hamilton'), findsNothing);
+    expect(find.widgetWithText(Chip, 'Eliza Hamilton'), findsOne);
+    expect(find.widgetWithText(Chip, 'James Madison'), findsOne);
+
+    // Delete James Madison.
+    await tester.tap(
+      find.descendant(of: find.widgetWithText(Chip, 'James Madison'), matching: find.byIcon(Icons.cancel)),
+    );
+    await tester.pump();
+
+    expect(find.widgetWithText(Chip, 'Aaron Burr'), findsOne);
+    expect(find.widgetWithText(Chip, 'Alexander Hamilton'), findsNothing);
+    expect(find.widgetWithText(Chip, 'Eliza Hamilton'), findsOne);
+    expect(find.widgetWithText(Chip, 'James Madison'), findsNothing);
+
+    // Delete Aaron Burr.
+    await tester.tap(
+      find.descendant(of: find.widgetWithText(Chip, 'Aaron Burr'), matching: find.byIcon(Icons.cancel)),
+    );
+    await tester.pump();
+
+    expect(find.widgetWithText(Chip, 'Aaron Burr'), findsNothing);
+    expect(find.widgetWithText(Chip, 'Alexander Hamilton'), findsNothing);
+    expect(find.widgetWithText(Chip, 'Eliza Hamilton'), findsOne);
+    expect(find.widgetWithText(Chip, 'James Madison'), findsNothing);
+
+    // Delete Eliza Hamilton.
+    await tester.tap(
+      find.descendant(of: find.widgetWithText(Chip, 'Eliza Hamilton'), matching: find.byIcon(Icons.cancel)),
+    );
+    await tester.pump();
+
+    expect(find.widgetWithText(Chip, 'Aaron Burr'), findsNothing);
+    expect(find.widgetWithText(Chip, 'Alexander Hamilton'), findsNothing);
+    expect(find.widgetWithText(Chip, 'Eliza Hamilton'), findsNothing);
+    expect(find.widgetWithText(Chip, 'James Madison'), findsNothing);
+  });
+}
