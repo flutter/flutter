4eaf5125bc6cef326f158292ca85ca3f06466fc1
Gray Mackall <mackall@google.com>
2024-07-29 14:06:18 -0700

    try this (wip)

--- packages/flutter_tools/gradle/src/main/kotlin/flutter.gradle.kts
+++ packages/flutter_tools/gradle/src/main/kotlin/flutter.gradle.kts
@@ -4,21 +4,38 @@
 
 apply<FlutterPluginKts>()
 
+//class FlutterPluginKts : Plugin<Project> {
+//    override fun apply(project: Project) {
+//        // Use withGroovyBuilder and getProperty() to access Groovy metaprogramming.
+//        project.withGroovyBuilder {
+//            getProperty("android").withGroovyBuilder {
+//                getProperty("defaultConfig").withGroovyBuilder {
+//                    var baseApplicationName: String = "android.app.Application"
+//                    if (project.hasProperty("base-application-name")) {
+//                        baseApplicationName = project.property("base-application-name").toString()
+//                    }
+//                    // Setting to android.app.Application is the same as omitting the attribute.
+//                    getProperty("manifestPlaceholders").withGroovyBuilder {
+//                        setProperty("applicationName", baseApplicationName)
+//                    }
+//                }
+//            }
+//        }
+//    }
+//}
+
 class FlutterPluginKts : Plugin<Project> {
     override fun apply(project: Project) {
-        // Use withGroovyBuilder and getProperty() to access Groovy metaprogramming.
-        project.withGroovyBuilder {
-            getProperty("android").withGroovyBuilder {
-                getProperty("defaultConfig").withGroovyBuilder {
-                    var baseApplicationName: String = "android.app.Application"
-                    if (project.hasProperty("base-application-name")) {
-                        baseApplicationName = project.property("base-application-name").toString()
-                    }
-                    // Setting to android.app.Application is the same as omitting the attribute.
-                    getProperty("manifestPlaceholders").withGroovyBuilder {
-                        setProperty("applicationName", baseApplicationName)
-                    }
-                }
+        // Use the Android extension directly for more type-safe access.
+        val androidExtension = project.extensions.findByType(com.android.build.gradle.AppExtension::class.java)
+
+        androidExtension?.let { android ->
+            android.defaultConfig.let { defaultConfig ->
+                // Read project property in a type-safe manner.
+                val baseApplicationName = project.findProperty("base-application-name")?.toString() ?: "android.app.Application"
+
+                // Setting to android.app.Application is the same as omitting the attribute.
+                defaultConfig.manifestPlaceholders["applicationName"] = baseApplicationName
             }
         }
     }
