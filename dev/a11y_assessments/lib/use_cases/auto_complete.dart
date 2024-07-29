ba07f8a9cdb150819d3d0f94076fbcae778d1737
Joy Serquina <serquina@google.com>
2024-07-29 19:47:26 +0000

    refactor(flutter/a11y_assessments): ipmlementing title widget as recommended
    Updates previous fix to use Title widget instead of SystemChrome.

--- dev/a11y_assessments/lib/use_cases/auto_complete.dart
+++ dev/a11y_assessments/lib/use_cases/auto_complete.dart
@@ -47,30 +47,33 @@ class _MainWidgetState extends State<_MainWidget> {
 
   @override
   Widget build(BuildContext context) {
-    setWebTitle('Autocomplete', Theme.of(context).colorScheme.primary.value);
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Semantics(headingLevel: 1, child: const Text('AutoComplete Demo')),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-                'Type below to autocomplete the following possible results: $_kOptions.'),
-            Autocomplete<String>(
-              optionsBuilder: (TextEditingValue textEditingValue) {
-                if (textEditingValue.text == '') {
-                  return const Iterable<String>.empty();
-                }
-                return _kOptions.where((String option) {
-                  return option.contains(textEditingValue.text.toLowerCase());
-                });
-              },
-              fieldViewBuilder: _fieldViewBuilder,
-            ),
-          ],
+    return Title(
+      color: appSeedColor,
+      title: 'Autocomplete',
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Semantics(headingLevel: 1, child: const Text('AutoComplete Demo')),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                  'Type below to autocomplete the following possible results: $_kOptions.'),
+              Autocomplete<String>(
+                optionsBuilder: (TextEditingValue textEditingValue) {
+                  if (textEditingValue.text == '') {
+                    return const Iterable<String>.empty();
+                  }
+                  return _kOptions.where((String option) {
+                    return option.contains(textEditingValue.text.toLowerCase());
+                  });
+                },
+                fieldViewBuilder: _fieldViewBuilder,
+              ),
+            ],
+          ),
         ),
       ),
     );
