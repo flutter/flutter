c0908f857ee234fafd3ea4944ad8b7e97c94f058
gaaclarke <30870216+gaaclarke@users.noreply.github.com>
2024-07-30 08:35:18 -0700

    [Impeller] makes the gaussian down sample scalar fixed by adjusting the downsample padding (flutter/engine#54150)
    This removes jitter in the gaussian blur.
    
    Previous version of the shimmer test show this drops the average RMSE 30%.
    
    testing:
      - positive change shown in average RMSE in ShimmerTest
      - existing golden tests
    
    Part of a series of gaussian blur changes:
    1) https://github.com/flutter/engine/pull/54148
    1) https://github.com/flutter/engine/pull/54116
    1) https://github.com/flutter/engine/pull/54150
    1) https://github.com/flutter/engine/pull/54149
    
    [C++, Objective-C, Java style guides]: https://github.com/flutter/engine/blob/main/CONTRIBUTING.md#style

--- engine/src/flutter/impeller/entity/contents/filters/gaussian_blur_filter_contents.cc
+++ engine/src/flutter/impeller/entity/contents/filters/gaussian_blur_filter_contents.cc
@@ -303,7 +303,24 @@ DownsamplePassArgs CalculateDownsamplePassArgs(
     Rect source_rect_padded = source_rect.Expand(padding);
     Vector2 downsampled_size = source_rect_padded.GetSize() * downsample_scalar;
     ISize subpass_size =
-        ISize(round(downsampled_size.x), round(downsampled_size.y));
+        ISize(ceil(downsampled_size.x), ceil(downsampled_size.y));
+    Vector2 divisible_size(CeilToDivisible(source_rect_padded.GetSize().width,
+                                           1.0 / downsample_scalar.x),
+                           CeilToDivisible(source_rect_padded.GetSize().height,
+                                           1.0 / downsample_scalar.y));
+    // Only make the padding divisible if we already have padding.  If we don't
+    // have padding adding more can add artifacts to hard blur edges.
+    Vector2 divisible_padding(
+        padding.x > 0
+            ? padding.x +
+                  (divisible_size.x - source_rect_padded.GetSize().width) / 2.0
+            : 0.f,
+        padding.y > 0
+            ? padding.y +
+                  (divisible_size.y - source_rect_padded.GetSize().height) / 2.0
+            : 0.f);
+    source_rect_padded = source_rect.Expand(divisible_padding);
+
     Vector2 effective_scalar =
         Vector2(subpass_size) / source_rect_padded.GetSize();
     Quad uvs = GaussianBlurFilterContents::CalculateUVs(
@@ -312,8 +329,8 @@ DownsamplePassArgs CalculateDownsamplePassArgs(
         .subpass_size = subpass_size,
         .uvs = uvs,
         .effective_scalar = effective_scalar,
-        .transform =
-            input_snapshot.transform * Matrix::MakeTranslation(-padding),
+        .transform = input_snapshot.transform *
+                     Matrix::MakeTranslation(-divisible_padding),
     };
   }
 }
