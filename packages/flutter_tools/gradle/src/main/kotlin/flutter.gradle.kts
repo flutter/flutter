f970da0c536e958553aebe0ca0a0982459289543
Gray Mackall <mackall@google.com>
2024-07-31 12:57:35 -0700

    re add dependencies block

--- packages/flutter_tools/gradle/src/main/kotlin/flutter.gradle.kts
+++ packages/flutter_tools/gradle/src/main/kotlin/flutter.gradle.kts
@@ -2,6 +2,25 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// This buildscript block supplies dependencies for this file's own import
+// declarations above. It exists solely for compatibility with projects that
+// have not migrated to declaratively apply the Flutter Gradle Plugin;
+// for those that have, FGP's `build.gradle.kts`  takes care of this.
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        // When bumping, also update:
+        //  * ndkVersion in FlutterExtension in packages/flutter_tools/gradle/src/main/groovy/flutter.groovy
+        //  * AGP version in the buildscript block in packages/flutter_tools/gradle/src/main/kotlin/dependency_version_checker.gradle.kts
+        //  * AGP version constants in packages/flutter_tools/lib/src/android/gradle_utils.dart
+        //  * AGP version in dependencies block in packages/flutter_tools/gradle/build.gradle.kts
+        classpath("com.android.tools.build:gradle:7.3.0")
+    }
+}
+
 apply<FlutterPluginKts>()
 
 class FlutterPluginKts : Plugin<Project> {
