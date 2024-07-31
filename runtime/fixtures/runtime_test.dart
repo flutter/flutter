e7bbdcf143938ec3fa614536a7cfa771f96bf0e4
Matan Lurey <matanlurey@users.noreply.github.com>
2024-07-31 15:47:58 -0700

    Cleanup dart analyze diagnostics in ignored directories. (#54262)
    Towards https://github.com/flutter/flutter/issues/152636.
    
    I also deleted some code that was TODO'd 10 years ago.

--- runtime/fixtures/runtime_test.dart
+++ runtime/fixtures/runtime_test.dart
@@ -2,9 +2,10 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// ignore_for_file: avoid_print
+
 import 'dart:async';
 import 'dart:isolate';
-import 'dart:ui';
 
 import 'split_lib_test.dart' deferred as splitlib;
 
@@ -17,30 +18,28 @@ void sayHi() {
 
 @pragma('vm:entry-point')
 void throwExceptionNow() {
-  throw 'Hello';
+  throw AssertionError('Hello');
 }
 
 @pragma('vm:entry-point')
-void canRegisterNativeCallback() async {
+Future<void> canRegisterNativeCallback() async {
   print('In function canRegisterNativeCallback');
   notifyNative();
   print('Called native method from canRegisterNativeCallback');
 }
 
-Future<void>? splitLoadFuture = null;
+Future<void>? splitLoadFuture;
 
 @pragma('vm:entry-point')
 void canCallDeferredLibrary() {
   print('In function canCallDeferredLibrary');
-  splitLoadFuture = splitlib.loadLibrary()
-    .then((_) {
-        print('Deferred load complete');
-        notifySuccess(splitlib.splitAdd(10, 23) == 33);
-      })
-    .catchError((_) {
-        print('Deferred load error');
-        notifySuccess(false);
-      });
+  splitLoadFuture = splitlib.loadLibrary().then((_) {
+    print('Deferred load complete');
+    notifySuccess(splitlib.splitAdd(10, 23) == 33);
+  }).catchError((_) {
+    print('Deferred load error');
+    notifySuccess(false);
+  });
   notifyNative();
 }
 
@@ -48,7 +47,7 @@ void canCallDeferredLibrary() {
 external void notifyNative();
 
 @pragma('vm:entry-point')
-void testIsolateShutdown() {  }
+void testIsolateShutdown() {}
 
 @pragma('vm:external-name', 'NotifyNative')
 external void notifyResult(bool success);
@@ -56,44 +55,47 @@ external void notifyResult(bool success);
 external void passMessage(String message);
 
 void secondaryIsolateMain(String message) {
-  print('Secondary isolate got message: ' + message);
+  print('Secondary isolate got message: $message');
   passMessage('Hello from code is secondary isolate.');
   notifyNative();
 }
 
 @pragma('vm:entry-point')
 void testCanLaunchSecondaryIsolate() {
-  final onExit = RawReceivePort((_) { notifyNative(); });
-  Isolate.spawn(secondaryIsolateMain, 'Hello from root isolate.', onExit: onExit.sendPort);
+  final onExit = RawReceivePort((_) {
+    notifyNative();
+  });
+  Isolate.spawn(secondaryIsolateMain, 'Hello from root isolate.',
+      onExit: onExit.sendPort);
 }
 
-
 @pragma('vm:entry-point')
-void testIsolateStartupFailure() async {
-  Future mainTest(dynamic _) async {
-    Future testSuccessfullIsolateLaunch() async {
+Future<void> testIsolateStartupFailure() async {
+  Future<void> mainTest(void _) async {
+    Future<void> testSuccessfullIsolateLaunch() async {
       final onMessage = ReceivePort();
       final onExit = ReceivePort();
 
       final messages = StreamIterator<dynamic>(onMessage);
       final exits = StreamIterator<dynamic>(onExit);
 
-      await Isolate.spawn((SendPort port) => port.send('good'),
-          onMessage.sendPort, onExit: onExit.sendPort);
+      await Isolate.spawn(
+          (SendPort port) => port.send('good'), onMessage.sendPort,
+          onExit: onExit.sendPort);
       if (!await messages.moveNext()) {
-        throw 'Failed to receive message';
+        throw AssertionError('Failed to receive message');
       }
       if (messages.current != 'good') {
-        throw 'Failed to receive correct message';
+        throw AssertionError('Failed to receive correct message');
       }
       if (!await exits.moveNext()) {
-        throw 'Failed to receive onExit';
+        throw AssertionError('Failed to receive onExit');
       }
       messages.cancel();
       exits.cancel();
     }
 
-    Future testUnsuccessfullIsolateLaunch() async {
+    Future<void> testUnsuccessfullIsolateLaunch() async {
       IsolateSpawnException? error;
       try {
         await Isolate.spawn((_) {}, null);
@@ -101,7 +103,7 @@ void testIsolateStartupFailure() async {
         error = e;
       }
       if (error == null) {
-        throw 'Expected isolate spawn to fail.';
+        throw AssertionError('Expected isolate spawn to fail.');
       }
     }
 
@@ -115,6 +117,7 @@ void testIsolateStartupFailure() async {
   // test in an isolate.
   Isolate.spawn(mainTest, null);
 }
+
 @pragma('vm:external-name', 'MakeNextIsolateSpawnFail')
 external void makeNextIsolateSpawnFail();
 
@@ -132,35 +135,35 @@ void trampoline() {
 external void notifySuccess(bool success);
 
 @pragma('vm:entry-point')
-void testCanConvertEmptyList(List<int> args){
-  notifySuccess(args.length == 0);
+void testCanConvertEmptyList(List<int> args) {
+  notifySuccess(args.isEmpty);
 }
 
 @pragma('vm:entry-point')
-void testCanConvertListOfStrings(List<String> args){
+void testCanConvertListOfStrings(List<String> args) {
   notifySuccess(args.length == 4 &&
-                args[0] == 'tinker' &&
-                args[1] == 'tailor' &&
-                args[2] == 'soldier' &&
-                args[3] == 'sailor');
+      args[0] == 'tinker' &&
+      args[1] == 'tailor' &&
+      args[2] == 'soldier' &&
+      args[3] == 'sailor');
 }
 
 @pragma('vm:entry-point')
-void testCanConvertListOfDoubles(List<double> args){
+void testCanConvertListOfDoubles(List<double> args) {
   notifySuccess(args.length == 4 &&
-                args[0] == 1.0 &&
-                args[1] == 2.0 &&
-                args[2] == 3.0 &&
-                args[3] == 4.0);
+      args[0] == 1.0 &&
+      args[1] == 2.0 &&
+      args[2] == 3.0 &&
+      args[3] == 4.0);
 }
 
 @pragma('vm:entry-point')
-void testCanConvertListOfInts(List<int> args){
+void testCanConvertListOfInts(List<int> args) {
   notifySuccess(args.length == 4 &&
-                args[0] == 1 &&
-                args[1] == 2 &&
-                args[2] == 3 &&
-                args[3] == 4);
+      args[0] == 1 &&
+      args[1] == 2 &&
+      args[2] == 3 &&
+      args[3] == 4);
 }
 
 bool didCallRegistrantBeforeEntrypoint = false;
@@ -168,17 +171,14 @@ bool didCallRegistrantBeforeEntrypoint = false;
 // Test the Dart plugin registrant.
 @pragma('vm:entry-point')
 class _PluginRegistrant {
-
   @pragma('vm:entry-point')
   static void register() {
     if (didCallRegistrantBeforeEntrypoint) {
-      throw '_registerPlugins is being called twice';
+      throw AssertionError('_registerPlugins is being called twice');
     }
     didCallRegistrantBeforeEntrypoint = true;
   }
-
 }
-
 
 @pragma('vm:entry-point')
 void mainForPluginRegistrantTest() {
@@ -195,19 +195,19 @@ void mainForPlatformIsolates() {
 }
 
 @pragma('vm:entry-point')
-void emptyMain(args) {}
+void emptyMain(List<Object?> args) {}
 
 @pragma('vm:entry-point')
 Function createEntryPointForPlatIsoSendAndRecvTest() {
   final port = RawReceivePort();
-  port.handler = (message) {
+  port.handler = (Object? message) {
     port.close();
-    (message as SendPort).send('Hello from root isolate!');
+    (message! as SendPort).send('Hello from root isolate!');
   };
   final SendPort sendPort = port.sendPort;
   return () {
     final replyPort = RawReceivePort();
-    replyPort.handler = (message) {
+    replyPort.handler = (Object? message) {
       replyPort.close();
       passMessage('Platform isolate received: $message');
     };
@@ -217,5 +217,5 @@ Function createEntryPointForPlatIsoSendAndRecvTest() {
 
 @pragma('vm:entry-point')
 void mainForPlatformIsolatesThrowError() {
-  throw 'Error from platform isolate';
+  throw AssertionError('Error from platform isolate');
 }
