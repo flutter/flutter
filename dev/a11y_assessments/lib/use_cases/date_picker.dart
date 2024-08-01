aafb2f6964da5a4a7656cad115adfe7a21b0643e
DBowen33 <denis.bowen@utexas.edu>
2024-08-01 20:46:01 +0000

    revert unneccessary fix

--- dev/a11y_assessments/lib/use_cases/date_picker.dart
+++ dev/a11y_assessments/lib/use_cases/date_picker.dart
@@ -38,6 +38,7 @@ class _MainWidgetState extends State<_MainWidget> {
         child: TextButton(
           onPressed: () => showDatePicker(
             context: context,
+            initialEntryMode: DatePickerEntryMode.calendarOnly,
             initialDate: DateTime.now(),
             firstDate: DateTime.now().subtract(const Duration(days: 365)),
             lastDate: DateTime.now().add(const Duration(days: 365)),
