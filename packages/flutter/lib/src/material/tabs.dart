12171a90e76ce704e5aff24ed5a19058f4b22368
SuicaLondon <suicadevinlondon@gmail.com>
2024-08-05 11:56:20 +0100

    fit: add guard statement for TabBarView

--- packages/flutter/lib/src/material/tabs.dart
+++ packages/flutter/lib/src/material/tabs.dart
@@ -2019,6 +2019,7 @@ class _TabBarViewState extends State<TabBarView> {
         viewportFraction: widget.viewportFraction,
       );
     } else {
+      if (!_pageController!.position.hasContentDimensions) return;
       _pageController!.jumpToPage(_currentIndex!);
     }
   }
