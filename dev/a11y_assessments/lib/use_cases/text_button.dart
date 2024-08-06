f67a7054c7ddb95d741a7481a150edf6500593a6
Joy Serquina <serquina@google.com>
2024-08-06 22:33:55 +0000

    rebase(flutter/a11y_assessments): rebase to text_button changes from https://github.com/flutter/flutter/pull/152185
    Updates my PR with merged changes from PR 1528185.

--- dev/a11y_assessments/lib/use_cases/text_button.dart
+++ dev/a11y_assessments/lib/use_cases/text_button.dart
@@ -29,8 +29,6 @@ class MainWidgetState extends State<MainWidget> {
 
   @override
   Widget build(BuildContext context) {
-<<<<<<< HEAD
-    setWebTitle('TextButton Demo', Theme.of(context).colorScheme.primary.value);
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
@@ -42,26 +40,10 @@ class MainWidgetState extends State<MainWidget> {
           children: <Widget>[
             MergeSemantics(
               child: Row(
-=======
-    return Title(
-      color: appSeedColor,
-      title: 'TextButton Demo',
-      child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title: const Text('TextButton'),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Row(
->>>>>>> 63c334f08f (refactor(flutter/a11y_assessments): update previous fixes to use title widget)
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
                   const Text('This is a TextButton:'),
                   TextButton(
-<<<<<<< HEAD
                     onPressed: () {
                       setState(() {
                         _count++;
@@ -75,34 +57,17 @@ class MainWidgetState extends State<MainWidget> {
             ),
             const MergeSemantics(
               child: Row(
-=======
-                    onPressed: () {  },
-                    child: const Text('Action'),
-                  ),
-                ],
-              ),
-              const Row(
->>>>>>> 63c334f08f (refactor(flutter/a11y_assessments): update previous fixes to use title widget)
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
                   Text('This is a disabled TextButton:'),
                   TextButton(
                     onPressed: null,
-<<<<<<< HEAD
                     child: Text('Action Disabled'),
                   ),
                 ],
               ),
             ),
           ],
-=======
-                    child: Text('Action'),
-                  ),
-                ],
-              ),
-            ],
-          ),
->>>>>>> 63c334f08f (refactor(flutter/a11y_assessments): update previous fixes to use title widget)
         ),
       ),
     );
