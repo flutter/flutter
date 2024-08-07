fa9fa1177c551fcead51413094f9b090b135f99a
Qun Cheng <chengqunq@gmail.com>
2024-08-07 10:19:44 -0700

    Update test

--- packages/flutter/test/cupertino/sliding_segmented_control_test.dart
+++ packages/flutter/test/cupertino/sliding_segmented_control_test.dart
@@ -2,6 +2,11 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// reduced-test-set:
+//   This file is run as part of a reduced test set in CI on Mac and Windows
+//   machines.
+@Tags(<String>['reduced-test-set'])
+library;
 import 'dart:collection';
 
 import 'package:flutter/cupertino.dart';
