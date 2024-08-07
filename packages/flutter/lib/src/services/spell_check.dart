04afabfc9fea0a1309903183bd618cde632a9b9e
Justin McCandless <jmccandless@google.com>
2024-08-07 13:47:07 -0700

    Docs clarification

--- packages/flutter/lib/src/services/spell_check.dart
+++ packages/flutter/lib/src/services/spell_check.dart
@@ -103,6 +103,10 @@ abstract class SpellCheckService {
   ///
   /// Returns a [Future] that resolves with a [List] of [SuggestionSpan]s for
   /// all misspelled words in the given [String] for the given [Locale].
+  ///
+  /// A return value that resolves to null indicates that fetching the spell
+  /// check suggestions was unsuccessful. If fetching the suggestions succeeded
+  /// but none were found, the [Future] should resolve to an empty list.
   Future<List<SuggestionSpan>?> fetchSpellCheckSuggestions(
     Locale locale, String text
   );
