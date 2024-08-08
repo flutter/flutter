d8600f7d955cdf54f3ff820a8bdd31639a7897e9
Jason Simmons <jason-simmons@users.noreply.github.com>
2024-08-08 11:37:18 -0700

    Fix include of GrVkImageInfo header for the Fuchsia build (flutter/engine#54449)
    See https://skia.googlesource.com/skia/+/5fb01f00262bbbef99571953fb5a987b0bed4d45

--- engine/src/flutter/shell/platform/fuchsia/flutter/vulkan_surface.cc
+++ engine/src/flutter/shell/platform/fuchsia/flutter/vulkan_surface.cc
@@ -20,6 +20,7 @@
 #include "third_party/skia/include/gpu/GrDirectContext.h"
 #include "third_party/skia/include/gpu/ganesh/SkSurfaceGanesh.h"
 #include "third_party/skia/include/gpu/ganesh/vk/GrVkBackendSurface.h"
+#include "third_party/skia/include/gpu/vk/GrVkTypes.h"
 #include "third_party/skia/include/gpu/vk/VulkanTypes.h"
 #include "vulkan/vulkan_core.h"
 #include "vulkan/vulkan_fuchsia.h"
