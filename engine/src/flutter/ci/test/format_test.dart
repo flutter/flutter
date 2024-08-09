cd8520ecd88b1e90a9f04629debac52b43309d30
Eric Seidel <eric@shorebird.dev>
2024-08-09 08:16:08 -0700

    Update pre-commit formatter to use java from repo rather than path. (flutter/engine#54450)
    This removes the recently added requirement of contributors having
    `java` on their path.
    
    Also removed the mention of java being needed in setup (I don't have
    java
    installed and have built the engine from mac for years).
    
    Fixes https://github.com/flutter/flutter/issues/152968
    Fixes https://github.com/flutter/flutter/issues/129221

--- engine/src/flutter/ci/test/format_test.dart
+++ engine/src/flutter/ci/test/format_test.dart
@@ -197,10 +197,7 @@ void main() {
     } finally {
       fixture.gitRemove();
     }
-    // TODO(mtolmacs): Fails if Java dependency is unavailable,
-    // https://github.com/flutter/flutter/issues/129221
-    // Java dependency is missing from some Mac bots so only skip those.
-  }, skip: io.Platform.isMacOS);
+  });
 
   test('Can fix Python formatting errors', () {
     final TestFileFixture fixture = TestFileFixture(target.FormatCheck.python);
