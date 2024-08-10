37a0842af9afc05836c6f6b2cc6928e9678c909d
Elvis Okereke <57872669+ElvisOkereke@users.noreply.github.com>
2024-08-10 12:55:26 -0400

    testFix 10

--- packages/flutter/test/material/paginated_data_table_test.dart
+++ packages/flutter/test/material/paginated_data_table_test.dart
@@ -1439,7 +1439,7 @@ void main() {
       expect(find.byWidgetPredicate((widget) => widget is Text && widget.data == 'Rows per page:'), findsOneWidget);
       final Text rowsPerPageText = tester.widget(find.byWidgetPredicate((widget) => widget is Text && widget.data == 'Rows per page:'));
       final TextStyle? selectedTextStyle = rowsPerPageText.style;
-      expect(selectedTextStyle.color, equals(footerStyleColor));
+      expect(selectedTextStyle.color?, equals(footerStyleColor));
   });
 
   testWidgets('PaginatedDataTable headerBackgroundColor and footerBackgroundColor set properly', (WidgetTester tester) async {
