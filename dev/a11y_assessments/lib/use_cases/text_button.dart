85b3d97673360f4b0ecbee384074795b4dac8266
Joy Serquiña <44146839+essjay05@users.noreply.github.com>
2024-08-13 15:22:28 -0700

    fix(flutter/a11y assessments): h1 missing a11y from each page on the web app (#152198)
    Adds Semantic Widget to wrap the contents of each page's AppBar title attribute and adds header: true and headingLevel: 1 as Semantic attributes so the content of the title is compiled as an h1 tag to meet accessibility guidelines that each page must have a h1 tag present. Also updates the test cases for the home page and each use case page to check for its respective h1.
    
    [Before Screenshot - Accessibility Assessments Home Page](https://screenshot.googleplex.com/4i9LuiGwvLnEcZ8)
    [Before Screenshot - Checkbox List Title -- note: each use case page is missing an h1](https://screenshot.googleplex.com/3qQjfqvAMTehRsm)
    [After Screenshot - Accessibility Assessments Home Page](https://screenshot.googleplex.com/APSJJXBmwNBP35m)
    [After Screenshot - Checkbox List Title-- note: change is similar across each Accessibility use case page](https://screenshot.googleplex.com/6EGgZnTusEgeN5L)
    
    Fixes b/338035526

--- dev/a11y_assessments/lib/use_cases/text_button.dart
+++ dev/a11y_assessments/lib/use_cases/text_button.dart
@@ -32,7 +32,7 @@ class MainWidgetState extends State<MainWidget> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text('TextButton'),
+        title: Semantics(headingLevel: 1, child: const Text('TextButton Demo')),
       ),
       body: Center(
         child: Column(
