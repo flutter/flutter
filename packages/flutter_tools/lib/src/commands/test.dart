f4ace441710f2b0517a108601e16deb238909ea7
Sigurd Meldgaard <sigurdm@google.com>
2024-08-16 11:47:11 +0000

    Add TODO

--- packages/flutter_tools/lib/src/commands/test.dart
+++ packages/flutter_tools/lib/src/commands/test.dart
@@ -693,6 +693,8 @@ class TestCommand extends FlutterCommand with DeviceBasedDevelopmentArtifacts {
     required BuildMode buildMode,
   }) async {
     final AssetBundle assetBundle = AssetBundleFactory.instance.createBundle();
+    // TODO(sigurdm): parametrize packageConfigPath to support testing
+    // workspaces.
     final int build = await assetBundle.build(
       packageConfigPath: '.dart_tool/package_config.json',
       flavor: flavor,
