1fde0ad466faf37d350b02d4cb89033ff588adfa
gaaclarke <30870216+gaaclarke@users.noreply.github.com>
2024-08-19 16:16:55 -0700

    Changes DlColor to support wide gamut colors (flutter/engine#54473)
    issue: https://github.com/flutter/flutter/issues/127855
    integration test: https://github.com/flutter/engine/pull/54415
    
    This is the engine side changes required for wide gamut framework support.  It changes the internal representation of DlColor to be floats.  It will be married with https://github.com/flutter/engine/pull/54415 when it lands in https://github.com/flutter/engine/pull/54567.
    
    [C++, Objective-C, Java style guides]: https://github.com/flutter/engine/blob/main/CONTRIBUTING.md#style

--- engine/src/flutter/display_list/dl_vertices.h
+++ engine/src/flutter/display_list/dl_vertices.h
@@ -147,9 +147,7 @@ class DlVertices {
     ///
     /// fails if colors have already been supplied or if they were not
     /// promised by the flags.has_colors.
-    void store_colors(const uint32_t colors[]) {
-      store_colors(reinterpret_cast<const DlColor*>(colors));
-    }
+    void store_colors(const uint32_t colors[]);
 
     /// @brief Copies the indicated list of 16-bit indices as vertex indices.
     ///
