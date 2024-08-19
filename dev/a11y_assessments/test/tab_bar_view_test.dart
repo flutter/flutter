2054fd7d4cf1282b412742cac55089d69bacb032
hangyu <jhy03261997@gmail.com>
2024-08-19 11:13:11 -0700

    Update tab_bar_view_test.dart

--- dev/a11y_assessments/test/tab_bar_view_test.dart
+++ dev/a11y_assessments/test/tab_bar_view_test.dart
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:a11y_assessments/use_cases/tab_bar.dart';
+import 'package:a11y_assessments/use_cases/tab_bar_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
