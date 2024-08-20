c5eecacb0049b203b5f1358f9f0a1c838b94e2d7
Sigurd Meldgaard <sigurdm@google.com>
2024-08-20 12:49:57 +0200

    Revert "Revert "Load parent package config" (#153752)"
    This reverts commit 276674e7605b6b8947b1832d796e6cf4d6a98df8.

--- packages/flutter_tools/lib/src/project.dart
+++ packages/flutter_tools/lib/src/project.dart
@@ -214,15 +214,6 @@ class FlutterProject {
   /// The `pubspec.yaml` file of this project.
   File get pubspecFile => directory.childFile('pubspec.yaml');
 
-  /// The `.packages` file of this project.
-  File get packagesFile => directory.childFile('.packages');
-
-  /// The `package_config.json` file of the project.
-  ///
-  /// This is the replacement for .packages which contains language
-  /// version information.
-  File get packageConfigFile => directory.childDirectory('.dart_tool').childFile('package_config.json');
-
   /// The `.metadata` file of this project.
   File get metadataFile => directory.childFile('.metadata');
 
