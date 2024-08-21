d570edfbc0ceee66c761c46711c0476eda9f52a4
Joshix-1 <57299889+Joshix-1@users.noreply.github.com>
2024-08-21 11:35:43 +0200

    Fix documentation of CircularProgressIndicator.adaptive
    The value gets used in _buildCupertinoIndicator

--- packages/flutter/lib/src/material/progress_indicator.dart
+++ packages/flutter/lib/src/material/progress_indicator.dart
@@ -574,8 +574,7 @@ class CircularProgressIndicator extends ProgressIndicator {
   /// [CupertinoActivityIndicator] in [TargetPlatform.iOS] & [TargetPlatform.macOS] and [CircularProgressIndicator] in
   /// material theme/non-Apple platforms.
   ///
-  /// The [value], [valueColor], [strokeWidth], [semanticsLabel], and
-  /// [semanticsValue] will be ignored in iOS & macOS.
+  /// [valueColor], [strokeWidth], [semanticsLabel], and [semanticsValue] will be ignored in iOS & macOS.
   ///
   /// {@macro flutter.material.ProgressIndicator.ProgressIndicator}
   const CircularProgressIndicator.adaptive({
