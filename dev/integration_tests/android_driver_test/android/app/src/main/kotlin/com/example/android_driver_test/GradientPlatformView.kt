0bb091d0fdb84f1bbca20fcd2d49459adf65a93b
Matan Lurey <matan@lurey.org>
2024-08-21 13:44:17 -0700

    ++

--- dev/integration_tests/android_driver_test/android/app/src/main/kotlin/com/example/android_driver_test/GradientPlatformView.kt
+++ dev/integration_tests/android_driver_test/android/app/src/main/kotlin/com/example/android_driver_test/GradientPlatformView.kt
@@ -1,3 +1,9 @@
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+@file:Suppress("PackageName")
+
 package com.example.android_driver_test
 
 import android.content.Context
