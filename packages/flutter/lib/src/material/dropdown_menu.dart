01536a63125fecaad38055ccdfbd989189915c37
PurplePolyhedron <120297255+PurplePolyhedron@users.noreply.github.com>
2024-08-22 01:00:15 +0800

    Disable Dropdown menu search during keyboard navigation (#152378)
    fixes https://github.com/flutter/flutter/issues/152375
    
    PR https://github.com/flutter/flutter/pull/147294 fix keyboard navigation issue https://github.com/flutter/flutter/issues/147253 by prioritising current selection in default search.
    
    This revert PR https://github.com/flutter/flutter/pull/147294 and fix the issue by disabling `DropdownMenu` search during keyboard navigation instead.
    
    also fixes https://github.com/flutter/flutter/issues/151878 as it is a regression caused by PR https://github.com/flutter/flutter/pull/147294.
    
    Reason for reverting https://github.com/flutter/flutter/pull/147294 : it doesn't fix the keyboard navigation issue if a custom searchCallback(Issue https://github.com/flutter/flutter/issues/152375) is provided, and "prioritising current selection" is ambiguous for a custom searchCallback, so it would be tricky to have searchCallback behave the same way as the default search. Additionally the original search function was agnostic about the state of `DropdownMenu` other than the entry value type, might be good to keep it that way unless needed.

--- packages/flutter/lib/src/material/dropdown_menu.dart
+++ packages/flutter/lib/src/material/dropdown_menu.dart
@@ -489,6 +489,7 @@ class _DropdownMenuState<T> extends State<DropdownMenu<T>> {
   late List<GlobalKey> buttonItemKeys;
   final MenuController _controller = MenuController();
   bool _enableFilter = false;
+  late bool _enableSearch;
   late List<DropdownMenuEntry<T>> filteredEntries;
   List<Widget>? _initialMenu;
   int? currentHighlight;
@@ -504,6 +505,7 @@ class _DropdownMenuState<T> extends State<DropdownMenu<T>> {
     } else {
       _localTextEditingController = TextEditingController();
     }
+    _enableSearch = widget.enableSearch;
     filteredEntries = widget.dropdownMenuEntries;
     buttonItemKeys = List<GlobalKey>.generate(filteredEntries.length, (int index) => GlobalKey());
     _menuHasEnabledItem = filteredEntries.any((DropdownMenuEntry<T> entry) => entry.enabled);
@@ -543,6 +545,7 @@ class _DropdownMenuState<T> extends State<DropdownMenu<T>> {
     }
     if (oldWidget.enableSearch != widget.enableSearch) {
       if (!widget.enableSearch) {
+        _enableSearch = widget.enableSearch;
         currentHighlight = null;
       }
     }
@@ -698,6 +701,7 @@ class _DropdownMenuState<T> extends State<DropdownMenu<T>> {
         return;
       }
       _enableFilter = false;
+      _enableSearch = false;
       currentHighlight ??= 0;
       currentHighlight = (currentHighlight! - 1) % filteredEntries.length;
       while (!filteredEntries[currentHighlight!].enabled) {
@@ -717,6 +721,7 @@ class _DropdownMenuState<T> extends State<DropdownMenu<T>> {
         return;
       }
       _enableFilter = false;
+      _enableSearch = false;
       currentHighlight ??= -1;
       currentHighlight = (currentHighlight! + 1) % filteredEntries.length;
       while (!filteredEntries[currentHighlight!].enabled) {
@@ -757,7 +762,7 @@ class _DropdownMenuState<T> extends State<DropdownMenu<T>> {
       filteredEntries = widget.dropdownMenuEntries;
     }
 
-    if (widget.enableSearch) {
+    if (_enableSearch) {
       if (widget.searchCallback != null) {
         currentHighlight = widget.searchCallback!(filteredEntries, _localTextEditingController!.text);
       } else {
@@ -856,6 +861,7 @@ class _DropdownMenuState<T> extends State<DropdownMenu<T>> {
             setState(() {
               filteredEntries = widget.dropdownMenuEntries;
               _enableFilter = widget.enableFilter;
+              _enableSearch = widget.enableSearch;
             });
           },
           inputFormatters: widget.inputFormatters,
