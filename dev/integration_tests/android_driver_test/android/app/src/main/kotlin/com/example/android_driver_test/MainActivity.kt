52aad72a7d14339343863c000a2b828e0611cd1f
Matan Lurey <matan@lurey.org>
2024-08-21 13:10:32 -0700

    Update tests and plugins.

--- dev/integration_tests/android_driver_test/android/app/src/main/kotlin/com/example/android_driver_test/MainActivity.kt
+++ dev/integration_tests/android_driver_test/android/app/src/main/kotlin/com/example/android_driver_test/MainActivity.kt
@@ -12,11 +12,15 @@ import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity : FlutterActivity() {
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         // Intentionally do not use GeneratedPluginRegistrant.
+
+        flutterEngine
+            .platformViewsController
+            .registry
+            .registerViewFactory("blue_orange_gradient_platform_view", BlueOrangeGradientPlatformViewFactory())
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
