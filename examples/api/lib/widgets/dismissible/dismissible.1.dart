9364a9219a64ff64786831ad131b656b461ee825
ndelanou <nicolas@delanou.com>
2024-08-21 09:25:15 +0200

    add test for API example

--- examples/api/lib/widgets/dismissible/dismissible.1.dart
+++ examples/api/lib/widgets/dismissible/dismissible.1.dart
@@ -35,49 +35,74 @@ class _DismissibleExampleAppState extends State<DismissibleExampleApp> {
   }
 }
 
-class DismissibleExample extends StatelessWidget {
+class DismissibleExample extends StatefulWidget {
   const DismissibleExample({super.key});
+
+  @override
+  State<DismissibleExample> createState() => _DismissibleExampleState();
+}
+
+class _DismissibleExampleState extends State<DismissibleExample> {
+  late List<_Dismissible> dismissibleWidgets = <_Dismissible>[
+    _Dismissible(
+      index: 0,
+      title: 'Default behavior',
+      description: '`shouldDismiss: null`',
+      onDismissed: _dismissItem,
+    ),
+    _Dismissible(
+      index: 1,
+      title: 'Default behavior',
+      description: '`shouldDismiss: (_) => null`',
+      shouldDismiss: (_) => null,
+      onDismissed: _dismissItem,
+    ),
+    _Dismissible(
+      index: 2,
+      title: 'Never dismiss',
+      description: '`shouldDismiss: (_) => false`',
+      shouldDismiss: (_) => false,
+      onDismissed: _dismissItem,
+    ),
+    _Dismissible(
+      index: 3,
+      title: 'Always dismiss',
+      description: '`shouldDismiss: (_) => true`',
+      shouldDismiss: (_) => true,
+      onDismissed: _dismissItem,
+    ),
+    _Dismissible(
+      index: 4,
+      title: 'Only accept if threshold is reached (Disable flinging)',
+      shouldDismiss: (AcceptDismissDetails details) => details.reached ? null : false,
+      onDismissed: _dismissItem,
+    ),
+    _Dismissible(
+      index: 5,
+      title: 'Accept dismiss before threshold',
+      description: '`details.progress >= 0.2`',
+      shouldDismiss: (AcceptDismissDetails details) {
+        if (details.progress >= 0.2) {
+          return true;
+        }
+        return null;
+      },
+      onDismissed: _dismissItem,
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return ListView(
       padding: const EdgeInsets.symmetric(vertical: 16),
-      children: <Widget>[
-        const _Dismissible(
-          title: 'Default behavior',
-          description: '`shouldDismiss: null`',
-        ),
-        _Dismissible(
-          title: 'Default behavior',
-          description: '`shouldDismiss: (_) => null`',
-          shouldDismiss: (_) => null,
-        ),
-        _Dismissible(
-          title: 'Never dismiss',
-          description: '`shouldDismiss: (_) => false`',
-          shouldDismiss: (_) => false,
-        ),
-        _Dismissible(
-          title: 'Always dismiss',
-          description: '`shouldDismiss: (_) => true`',
-          shouldDismiss: (_) => true,
-        ),
-        _Dismissible(
-          title: 'Only accept if threshold is reached (Disable flinging)',
-          shouldDismiss: (AcceptDismissDetails details) => details.reached ? null : false,
-        ),
-        _Dismissible(
-          title: 'Accept dismiss before threshold',
-          description: '`details.progress >= 0.1`',
-          shouldDismiss: (AcceptDismissDetails details) {
-            if (details.progress >= 0.1) {
-              return true;
-            }
-            return null;
-          },
-        ),
-      ],
+      children: dismissibleWidgets,
     );
+  }
+
+  void _dismissItem(_Dismissible dismissedItem) {
+    setState(() {
+      dismissibleWidgets = dismissibleWidgets.where((_Dismissible item) => item != dismissedItem).toList();
+    });
   }
 }
 
@@ -87,15 +112,19 @@ class DismissibleExample extends StatelessWidget {
 /// Change background color to red when the item is reached based on
 /// [DismissUpdateDetails.reached] value of [Dismissible.onUpdate] events.
 class _Dismissible extends StatefulWidget {
-  const _Dismissible({
+  _Dismissible({
+    required this.index,
     required this.title,
+    required this.onDismissed,
     this.description,
     this.shouldDismiss,
-  });
+  }) : super(key: ValueKey<int>(index));
 
+  final int index;
   final String title;
   final String? description;
   final AcceptDismissCallback? shouldDismiss;
+  final ValueChanged<_Dismissible> onDismissed;
 
   @override
   State<_Dismissible> createState() => _DismissibleState();
@@ -103,12 +132,11 @@ class _Dismissible extends StatefulWidget {
 
 class _DismissibleState extends State<_Dismissible> {
   bool reached = false;
-  final UniqueKey _key = UniqueKey();
 
   @override
   Widget build(BuildContext context) {
     return Dismissible(
-      key: _key,
+      key: Key('dismissible-${widget.index}'),
       background: ColoredBox(
         color: reached ? Colors.red : Colors.blue,
       ),
@@ -122,6 +150,7 @@ class _DismissibleState extends State<_Dismissible> {
         title: Text(widget.title),
         subtitle: widget.description != null ? Text(widget.description!) : null,
       ),
+      onDismissed: (_) => widget.onDismissed(widget),
     );
   }
 }
---
+++ examples/api/test/widgets/dismissible/dismissible.1_test.dart
@@ -0,0 +1,260 @@
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+import 'package:flutter_api_samples/widgets/dismissible/dismissible.1.dart'
+    as example;
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  Future<void> initWidget(WidgetTester tester) async {
+    await tester.pumpWidget(const example.DismissibleExampleApp());
+  }
+
+  Future<void> reset(WidgetTester tester) async {
+    final Finder fab = find.byType(FloatingActionButton);
+    await tester.tap(fab);
+    await tester.pumpAndSettle();
+  }
+  
+  Future<void> dismissHorizontally({
+    required WidgetTester tester,
+    required Finder finder,
+    double dragRatio = 0.8,
+    AxisDirection direction = AxisDirection.left,
+  }) async {
+    final double width = (tester.renderObject(finder) as RenderBox).size.width;
+    final double dx = width * dragRatio;
+
+    final Offset offset = switch (direction) {
+      AxisDirection.left => Offset(-dx, 0.0),
+      AxisDirection.right => Offset(dx, 0.0),
+      _ => throw ArgumentError('$direction is not supported'),
+    };
+
+    await tester.drag(finder, offset);
+  }
+
+  Future<void> testDragBehavior(WidgetTester tester, {
+    required Key key,
+    Matcher expectedBelowThreshold = findsOneWidget,
+    Matcher expectedAboveThreshold = findsNothing,
+  }) async {
+    await tester.scrollUntilVisible(find.byKey(key), 100);
+
+    expect(find.byKey(key), findsOneWidget);
+
+    // Drag below the threshold
+    await dismissHorizontally(
+      tester: tester,
+      finder: find.byKey(key),
+      dragRatio: 0.3
+    );
+
+    await tester.pumpAndSettle();
+
+    expect(find.byKey(key), expectedBelowThreshold);
+
+    await reset(tester);
+
+    // Drag above the threshold
+    await dismissHorizontally(
+      tester: tester,
+      finder: find.byKey(key),
+    );
+
+    await tester.pumpAndSettle();
+
+    expect(find.byKey(key), expectedAboveThreshold);
+  }
+
+  group('Test Drag behaviour', () {
+
+    testWidgets(
+      '0 - Default begavior - `shouldDismiss: null`',
+      (WidgetTester tester) async {
+        await initWidget(tester);
+
+        const ValueKey<int> key = ValueKey<int>(0);
+
+        await testDragBehavior(tester, key: key);
+      },
+    );
+
+    testWidgets(
+      '1 - Default begavior - `shouldDismiss: (_) => null`',
+      (WidgetTester tester) async {
+        await initWidget(tester);
+
+        const ValueKey<int> key = ValueKey<int>(1);
+
+        await testDragBehavior(tester, key: key);
+      },
+    );
+
+    testWidgets(
+      '2 - Never dismiss - `shouldDismiss: (_) => false`',
+      (WidgetTester tester) async {
+        await initWidget(tester);
+
+        const ValueKey<int> key = ValueKey<int>(2);
+
+        await testDragBehavior(tester, key: key, expectedAboveThreshold: findsOneWidget);
+      },
+    );
+
+    testWidgets(
+      '3 - Always dismiss - `shouldDismiss: (_) => true`',
+      (WidgetTester tester) async {
+        await initWidget(tester);
+
+        const ValueKey<int> key = ValueKey<int>(3);
+
+        await testDragBehavior(tester, key: key, expectedBelowThreshold: findsNothing);
+      },
+    );
+
+    testWidgets(
+      '4 - Only accept if threshold is reached (Disable flinging)',
+      (WidgetTester tester) async {
+        await initWidget(tester);
+
+        const ValueKey<int> key = ValueKey<int>(4);
+
+        await testDragBehavior(tester, key: key);
+      },
+    );
+
+    testWidgets(
+      '5 - Accept dismiss before threshold - `details.progress >= 0.2`',
+      (WidgetTester tester) async {
+        await initWidget(tester);
+
+        const ValueKey<int> key = ValueKey<int>(5);
+
+        await testDragBehavior(tester, key: key, expectedBelowThreshold: findsNothing);
+      },
+    );
+  });
+
+  Future<void> flingHorizontally({
+    required WidgetTester tester,
+    required Finder finder,
+    double speed = 1000,
+    AxisDirection direction = AxisDirection.left,
+  }) async {
+    final double width = (tester.renderObject(finder) as RenderBox).size.width;
+    final double dx = width * 0.1;
+
+    final Offset offset = switch (direction) {
+      AxisDirection.left => Offset(-dx, 0.0),
+      AxisDirection.right => Offset(dx, 0.0),
+      _ => throw ArgumentError('$direction is not supported'),
+    };
+
+    await tester.fling(finder, offset, speed);
+  }
+
+  Future<void> testFlingBehavior(WidgetTester tester, {
+    required Key key,
+    Matcher expectedBelowThreshold = findsOneWidget,
+    Matcher expectedAboveThreshold = findsNothing,
+  }) async {
+    await tester.scrollUntilVisible(find.byKey(key), 100);
+
+    expect(find.byKey(key), findsOneWidget);
+
+    // Fling below fling velocity threshold
+    await flingHorizontally(
+      tester: tester,
+      finder: find.byKey(key),
+      speed: 200
+    );
+
+    await tester.pumpAndSettle();
+
+    expect(find.byKey(key), expectedBelowThreshold);
+
+    await reset(tester);
+
+    // Fling above fling velocity threshold
+    await flingHorizontally(
+      tester: tester,
+      finder: find.byKey(key),
+    );
+
+    await tester.pumpAndSettle();
+
+    expect(find.byKey(key), expectedAboveThreshold);
+  }
+
+  group('Test Fling behaviour', () {
+
+    testWidgets(
+      '0 - Default begavior - `shouldDismiss: null`',
+      (WidgetTester tester) async {
+        await initWidget(tester);
+
+        const ValueKey<int> key = ValueKey<int>(0);
+
+        await testFlingBehavior(tester, key: key);
+      },
+    );
+
+    testWidgets(
+      '1 - Default begavior - `shouldDismiss: (_) => null`',
+      (WidgetTester tester) async {
+        await initWidget(tester);
+
+        const ValueKey<int> key = ValueKey<int>(1);
+
+        await testFlingBehavior(tester, key: key);
+      },
+    );
+
+    testWidgets(
+      '2 - Never dismiss - `shouldDismiss: (_) => false`',
+      (WidgetTester tester) async {
+        await initWidget(tester);
+
+        const ValueKey<int> key = ValueKey<int>(2);
+
+        await testFlingBehavior(tester, key: key, expectedAboveThreshold: findsOneWidget);
+      },
+    );
+
+    testWidgets(
+      '3 - Always dismiss - `shouldDismiss: (_) => true`',
+      (WidgetTester tester) async {
+        await initWidget(tester);
+
+        const ValueKey<int> key = ValueKey<int>(3);
+
+        await testFlingBehavior(tester, key: key, expectedBelowThreshold: findsNothing);
+      },
+    );
+
+    testWidgets(
+      '4 - Only accept if threshold is reached (Disable flinging)',
+      (WidgetTester tester) async {
+        await initWidget(tester);
+
+        const ValueKey<int> key = ValueKey<int>(4);
+
+        await testFlingBehavior(tester, key: key, expectedAboveThreshold: findsOneWidget);
+      },
+    );
+
+    testWidgets(
+      '5 - Accept dismiss before threshold - `details.progress >= 0.2`',
+      (WidgetTester tester) async {
+        await initWidget(tester);
+
+        const ValueKey<int> key = ValueKey<int>(5);
+
+        await testFlingBehavior(tester, key: key);
+      },
+    );
+  });
+}
