6683d179dd646566b9efa3d57e6e2758d56b4c3b
Matan Lurey <matan@lurey.org>
2024-08-21 10:00:13 -0700

    Rename, and start on platform view.

--- dev/integration_tests/android_driver_test/android/app/src/main/kotlin/com/example/android_driver_test/MainActivity.kt
+++ dev/integration_tests/android_driver_test/android/app/src/main/kotlin/com/example/android_driver_test/MainActivity.kt
@@ -11,8 +11,14 @@ import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity : FlutterActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        // Intentionally do not use GeneratedPluginRegistrant.
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
