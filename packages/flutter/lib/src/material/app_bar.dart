9e2f6af0c9aad36fcfbc3c1952ccaac36ac84a57
Mairramer <mairramer.dasilva28@hotmail.com>
2024-08-22 08:53:43 -0300

    main fix

--- packages/flutter/lib/src/material/app_bar.dart
+++ packages/flutter/lib/src/material/app_bar.dart
@@ -768,7 +768,6 @@ class AppBar extends StatefulWidget implements PreferredSizeWidget {
 }
 
 class _AppBarState extends State<AppBar> {
-  final Map<String?, double> _scrollPositions = <String?, double>{};
   ScrollNotificationObserverState? _scrollNotificationObserver;
   bool _scrolledUnder = false;
 
@@ -791,27 +790,11 @@ class _AppBarState extends State<AppBar> {
       _scrollNotificationObserver!.removeListener(_handleScrollNotification);
       _scrollNotificationObserver = null;
     }
-    _scrollPositions.clear();
     super.dispose();
   }
 
   void _handleScrollNotification(ScrollNotification notification) {
-    // Store the scroll position of the widget that sent the notification.
-    // The key is the widget's string representation to avoid storing the widget itself.
-    if (notification.context?.widget != null) {
-      _scrollPositions[notification.context?.widget.toString()] = notification.metrics.pixels;
-    }
-
     if (notification is ScrollUpdateNotification && widget.notificationPredicate(notification)) {
-      // Check if any scroll positions on the vertical axis have been updated.
-      // If any have, then the app bar should not be scrolled under.
-
-      if (_scrollPositions.isNotEmpty && notification.metrics.axis == Axis.vertical) {
-        if (_scrollPositions.values.toList().any((double element) => element > 0) && _scrolledUnder) {
-          return;
-        }
-      }
-
       final bool oldScrolledUnder = _scrolledUnder;
       final ScrollMetrics metrics = notification.metrics;
       switch (metrics.axisDirection) {
