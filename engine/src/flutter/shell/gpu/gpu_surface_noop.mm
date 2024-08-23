1ba45ac86eccb85fa303fbf52a74ecb9878d56c5
Jonah Williams <jonahwilliams@google.com>
2024-08-23 10:06:16 -0700

    [Engine] Add no op surface (flutter/engine#54694)
    Still needs to be tested in metalless environment. This should probably print out some kind of error message that tells folks that this is WAI, but I'm not sure if that would be too disruptive for g3.
    
    Fixes https://github.com/flutter/flutter/issues/153883

---
+++ engine/src/flutter/shell/gpu/gpu_surface_noop.mm
@@ -0,0 +1,97 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "flutter/shell/gpu/gpu_surface_noop.h"
+
+#import <Metal/Metal.h>
+#import <QuartzCore/QuartzCore.h>
+
+#include "flow/surface.h"
+#include "flow/surface_frame.h"
+#include "flutter/common/settings.h"
+#include "flutter/fml/mapping.h"
+#include "flutter/fml/trace_event.h"
+
+static_assert(!__has_feature(objc_arc), "ARC must be disabled.");
+
+namespace flutter {
+
+GPUSurfaceNoop::GPUSurfaceNoop() = default;
+
+GPUSurfaceNoop::~GPUSurfaceNoop() = default;
+
+// |Surface|
+bool GPUSurfaceNoop::IsValid() {
+  return true;
+}
+
+Surface::SurfaceData GPUSurfaceNoop::GetSurfaceData() const {
+  return Surface::SurfaceData{};
+}
+
+// |Surface|
+std::unique_ptr<SurfaceFrame> GPUSurfaceNoop::AcquireFrame(const SkISize& frame_size) {
+  auto callback = [](const SurfaceFrame&, DlCanvas*) { return true; };
+  auto submit_callback = [](const SurfaceFrame&) { return true; };
+  SurfaceFrame::FramebufferInfo framebuffer_info;
+
+  return std::make_unique<SurfaceFrame>(
+      /*surface=*/nullptr,
+      /*framebuffer_info=*/framebuffer_info,
+      /*encode_callback=*/callback,
+      /*submit_callback=*/submit_callback,
+      /*frame_size=*/frame_size,
+      /*context_result=*/nullptr,
+      /*display_list_fallback=*/true);
+}
+
+std::unique_ptr<SurfaceFrame> GPUSurfaceNoop::AcquireFrameFromMTLTexture(
+    const SkISize& frame_size) {
+  auto callback = [](const SurfaceFrame&, DlCanvas*) { return true; };
+  auto submit_callback = [](const SurfaceFrame&) { return true; };
+  SurfaceFrame::FramebufferInfo framebuffer_info;
+
+  return std::make_unique<SurfaceFrame>(
+      /*surface=*/nullptr,
+      /*framebuffer_info=*/framebuffer_info,
+      /*encode_callback=*/callback,
+      /*submit_callback=*/submit_callback,
+      /*frame_size=*/frame_size,
+      /*context_result=*/nullptr,
+      /*display_list_fallback=*/true);
+}
+
+// |Surface|
+SkMatrix GPUSurfaceNoop::GetRootTransformation() const {
+  // This backend does not currently support root surface transformations. Just
+  // return identity.
+  return {};
+}
+
+// |Surface|
+GrDirectContext* GPUSurfaceNoop::GetContext() {
+  return nullptr;
+}
+
+// |Surface|
+std::unique_ptr<GLContextResult> GPUSurfaceNoop::MakeRenderContextCurrent() {
+  // This backend has no such concept.
+  return std::make_unique<GLContextDefaultResult>(true);
+}
+
+bool GPUSurfaceNoop::AllowsDrawingWhenGpuDisabled() const {
+  return true;
+}
+
+// |Surface|
+bool GPUSurfaceNoop::EnableRasterCache() const {
+  return false;
+}
+
+// |Surface|
+std::shared_ptr<impeller::AiksContext> GPUSurfaceNoop::GetAiksContext() const {
+  return nullptr;
+}
+
+}  // namespace flutter
---
+++ engine/src/flutter/shell/platform/darwin/ios/ios_surface_noop.h
@@ -0,0 +1,46 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef FLUTTER_SHELL_PLATFORM_DARWIN_IOS_IOS_SURFACE_NOOP_H_
+#define FLUTTER_SHELL_PLATFORM_DARWIN_IOS_IOS_SURFACE_NOOP_H_
+
+#include "flutter/fml/platform/darwin/scoped_nsobject.h"
+#import "flutter/shell/platform/darwin/ios/ios_context.h"
+#import "flutter/shell/platform/darwin/ios/ios_surface.h"
+
+#include "third_party/skia/include/core/SkSurface.h"
+
+@class CALayer;
+
+namespace flutter {
+
+/// @brief A rendering surface that accepts rendering intent but does not render
+///        anything.
+///
+/// This is useful for running on platforms that need an engine instance and
+/// don't have the required drivers.
+class IOSSurfaceNoop final : public IOSSurface {
+ public:
+  explicit IOSSurfaceNoop(std::shared_ptr<IOSContext> context);
+
+  ~IOSSurfaceNoop() override;
+
+  // |IOSSurface|
+  bool IsValid() const override;
+
+  // |IOSSurface|
+  void UpdateStorageSizeIfNecessary() override;
+
+  // |IOSSurface|
+  std::unique_ptr<Surface> CreateGPUSurface(GrDirectContext* gr_context = nullptr) override;
+
+ private:
+  IOSSurfaceNoop(const IOSSurfaceNoop&) = delete;
+
+  IOSSurfaceNoop& operator=(const IOSSurfaceNoop&) = delete;
+};
+
+}  // namespace flutter
+
+#endif  // FLUTTER_SHELL_PLATFORM_DARWIN_IOS_IOS_SURFACE_NOOP_H_
