b283226d26f8a0261f13f905e2a6e95e87766aa3
Andrew Kolos <andrewrkolos@gmail.com>
2024-08-23 11:03:43 -0700

    zap whitespace

--- packages/flutter_tools/test/general.shard/resident_runner_helpers.dart
+++ packages/flutter_tools/test/general.shard/resident_runner_helpers.dart
@@ -157,7 +157,7 @@ class FakeDartDevelopmentServiceException implements DartDevelopmentServiceExcep
   @override
   final String message;
   static const String defaultMessage = 'A DDS instance is already connected at http://localhost:8181';
-  
+
   @override
   Map<String, Object?> toJson() {
     throw UnimplementedError();
