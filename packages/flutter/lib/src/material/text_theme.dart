226bdfb56c24c166777b59f1aaa71456180b7fff
amalkrishna <amalkrishna263@outlook.com>
2024-08-25 19:36:24 +0530

    Remove redundant docImport

--- packages/flutter/lib/src/material/text_theme.dart
+++ packages/flutter/lib/src/material/text_theme.dart
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-/// @docImport 'package:flutter/widgets.dart';
-///
 /// @docImport 'elevated_button.dart';
 /// @docImport 'material.dart';
 /// @docImport 'outlined_button.dart';
