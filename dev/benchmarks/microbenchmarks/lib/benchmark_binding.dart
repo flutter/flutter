733ab74de75620cbde2d02f0f8c39e6b45c3650d
John McDole <codefu@google.com>
2024-08-29 13:51:00 -0700

    Compile all micros together for faster execution

---
+++ dev/benchmarks/microbenchmarks/lib/benchmark_binding.dart
@@ -0,0 +1,19 @@
+import 'package:flutter_test/flutter_test.dart';
+
+class BenchmarkingBinding extends LiveTestWidgetsFlutterBinding {
+  BenchmarkingBinding();
+
+  final Stopwatch drawFrameWatch = Stopwatch();
+
+  @override
+  void handleBeginFrame(Duration? rawTimeStamp) {
+    drawFrameWatch.start();
+    super.handleBeginFrame(rawTimeStamp);
+  }
+
+  @override
+  void handleDrawFrame() {
+    super.handleDrawFrame();
+    drawFrameWatch.stop();
+  }
+}
