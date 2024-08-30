6596a35625fceba574601b44ddc48a62dcf7de60
gaaclarke <30870216+gaaclarke@users.noreply.github.com>
2024-08-30 11:22:04 -0700

    Reland: Hooks up framework wide gamut to engine wide gamut (flutter/engine#54906)
    relands https://github.com/flutter/engine/pull/54567
    depends on https://github.com/flutter/engine/pull/54905
    
    This was reverted because the PR that it depends on was reverted.  There is nothing to be addressed here.
    
    [C++, Objective-C, Java style guides]: https://github.com/flutter/engine/blob/main/CONTRIBUTING.md#style

--- engine/src/flutter/impeller/display_list/aiks_dl_vertices_unittests.cc
+++ engine/src/flutter/impeller/display_list/aiks_dl_vertices_unittests.cc
@@ -12,6 +12,7 @@
 #include "flutter/display_list/dl_color.h"
 #include "flutter/display_list/dl_paint.h"
 #include "flutter/testing/testing.h"
+#include "impeller/display_list/dl_dispatcher.h"
 #include "impeller/display_list/dl_image_impeller.h"
 
 namespace impeller {
@@ -375,7 +376,10 @@ TEST_P(AiksTest, DrawVerticesWithInvalidIndices) {
   builder.DrawRect(SkRect::MakeLTRB(0, 0, 400, 400), paint);
   builder.DrawVertices(vertices, flutter::DlBlendMode::kSrc, paint);
 
-  ASSERT_TRUE(OpenPlaygroundHere(builder.Build()));
+  AiksContext renderer(GetContext(), nullptr);
+  std::shared_ptr<Texture> image =
+      DisplayListToTexture(builder.Build(), {1024, 768}, renderer);
+  EXPECT_TRUE(image);
 }
 
 // All four vertices should form a solid red rectangle with no gaps.
