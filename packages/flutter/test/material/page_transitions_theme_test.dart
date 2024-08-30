d3de88c55db3d76694515b2589e592fcc9713b44
dy0gu <dbsimoes20@gmail.com>
2024-08-30 11:43:58 +0100

    Remove golden file tag

--- packages/flutter/test/material/page_transitions_theme_test.dart
+++ packages/flutter/test/material/page_transitions_theme_test.dart
@@ -2,11 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// This file is run as part of a reduced test set in CI on Mac and Windows
-// machines.
-@Tags(<String>['reduced-test-set'])
-library;
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
