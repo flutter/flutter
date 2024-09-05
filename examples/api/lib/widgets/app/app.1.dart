98ec2c6556beb236498216c54809119162ead40c
Justin McCandless <jmccandless@google.com>
2024-09-05 15:05:17 -0700

    Fix the example

--- examples/api/lib/widgets/app/app.1.dart
+++ examples/api/lib/widgets/app/app.1.dart
@@ -1,21 +1,16 @@
-// Copyright 2014 The Flutter Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-/// Flutter code sample for [WidgetsApp.router] that shows how to set up nested
-/// [Navigator]s.
+// This is an example of Navigator 2.0 with state restoration. If the app is
+// unloaded by the OS while backgrounded and then the user returns to it, it
+// will properly restore the entire navigation stack for both Navigators.
 
 void main() async {
   runApp(const WidgetsAppExample());
 }
 
 class WidgetsAppExample extends StatefulWidget {
-  const WidgetsAppExample({
-    super.key,
-  });
+  const WidgetsAppExample({super.key});
 
   @override
   State<WidgetsAppExample> createState() => _WidgetsAppExampleState();
@@ -27,39 +22,61 @@ class _WidgetsAppExampleState extends State<WidgetsAppExample> {
 
   @override
   Widget build(BuildContext context) {
-    // TODO(justinmc): This should include state restoration.
     return WidgetsApp.router(
+      restorationScopeId: 'widgets-app',
       color: Colors.red,
       localizationsDelegates: const <LocalizationsDelegate<dynamic>>[
         DefaultMaterialLocalizations.delegate,
       ],
-      title: 'WidgetsApp.router Example',
+      title: 'Nested navigation with state restoration example',
       routeInformationParser: _routeInformationParser,
       routerDelegate: _routerDelegate,
     );
   }
 }
 
-class _MyRouteInformationParser extends RouteInformationParser<_MyPageConfiguration> {
+class _MyRouteInformationParser extends RouteInformationParser<_MyNavigationConfiguration> {
   @override
-  SynchronousFuture<_MyPageConfiguration> parseRouteInformation(RouteInformation routeInformation) {
-    return SynchronousFuture<_MyPageConfiguration>(_MyPageConfiguration.values.firstWhere((_MyPageConfiguration pageConfiguration) {
-      return pageConfiguration.uriString == routeInformation.uri.toString();
-    },
-      orElse: () => _MyPageConfiguration.unknown,
-    ));
+  SynchronousFuture<_MyNavigationConfiguration> parseRouteInformation(RouteInformation routeInformation) {
+    // If no navigation stack info was saved, then just make the given URI the
+    // only route in the new stack.
+    if (routeInformation.state == null) {
+      final _MyPage page = _MyPage.values.firstWhere((_MyPage pageConfiguration) {
+        return pageConfiguration.uriString == routeInformation.uri.toString();
+      },
+        orElse: () => _MyPage.unknown,
+      );
+      return SynchronousFuture<_MyNavigationConfiguration>(_MyNavigationConfiguration.single(page));
+    }
+
+    // Otherwise, restore the whole given navigation stack.
+    final Map<Object?, Object?> state = routeInformation.state! as Map<Object?, Object?>;
+    final List<Object?>? pageNameObjects = state['pages'] as List<Object?>?;
+    assert(pageNameObjects != null && pageNameObjects.isNotEmpty);
+    final List<String> pageNames = pageNameObjects!.cast<String>();
+    final List<_MyPage> pages = pageNames.map(_MyPage.fromName).toList();
+    return SynchronousFuture<_MyNavigationConfiguration>(_MyNavigationConfiguration(pages: pages));
   }
 
   @override
-  RouteInformation? restoreRouteInformation(_MyPageConfiguration configuration) {
-    return RouteInformation(uri: configuration.uri);
+  RouteInformation? restoreRouteInformation(_MyNavigationConfiguration configuration) {
+    return RouteInformation(
+      uri: configuration.uri,
+      // Pass the state of the navigation stack in the state parameter.
+      state: <String, List<String>>{
+        'pages': configuration.pages.map((_MyPage page) => page.toString()).toList(),
+      },
+    );
   }
 }
 
-class _MyRouterDelegate extends RouterDelegate<_MyPageConfiguration> {
+class _MyRouterDelegate extends RouterDelegate<_MyNavigationConfiguration>
+    with ChangeNotifier, PopNavigatorRouterDelegateMixin<_MyNavigationConfiguration> {
   final Set<VoidCallback> _listeners = <VoidCallback>{};
-  final List<_MyPageConfiguration> _pages = <_MyPageConfiguration>[];
-  final GlobalKey<NavigatorState> _navigatorKey = GlobalKey<NavigatorState>();
+  final List<_MyPage> _pages = <_MyPage>[];
+
+  @override
+  final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
   void _notifyListeners() {
     for (final VoidCallback listener in _listeners) {
@@ -67,27 +84,24 @@ class _MyRouterDelegate extends RouterDelegate<_MyPageConfiguration> {
     }
   }
 
-  // Why not just do `Navigator.of(context).pushNamed('/')`? That is handled by
-  // Navigator.onGenerateRoute, which would interfere with this example's use of
-  // Navigator.pages.
-  void _onNavigateToHome() {
+  void _onNavigateToLeaf() {
     _pages.clear();
-    _pages.add(_MyPageConfiguration.home);
+    _pages.add(_MyPage.home);
+    _pages.add(_MyPage.leaf);
     _notifyListeners();
   }
 
-  void _onNavigateToLeaf() {
+  void _onNavigateToHome() {
     _pages.clear();
-    _pages.add(_MyPageConfiguration.home);
-    _pages.add(_MyPageConfiguration.leaf);
+    _pages.add(_MyPage.home);
     _notifyListeners();
   }
 
   void _onNavigateToNested() {
     _pages.clear();
-    _pages.add(_MyPageConfiguration.home);
-    _pages.add(_MyPageConfiguration.leaf);
-    _pages.add(_MyPageConfiguration.nestedNavigator);
+    _pages.add(_MyPage.home);
+    _pages.add(_MyPage.leaf);
+    _pages.add(_MyPage.nestedNavigator);
     _notifyListeners();
   }
 
@@ -104,39 +118,54 @@ class _MyRouterDelegate extends RouterDelegate<_MyPageConfiguration> {
   // Called for a system back event.
   @override
   Future<bool> popRoute() {
-    // If you did this manually by removing from _pages, it wouldn't work in the
-    // case where there's a nested Navigator.
-    final NavigatorState navigator = _navigatorKey.currentState!;
+    // If you did this manually, by removing from _pages, it wouldn't work in
+    // the case where there's a nested Navigator.
+    final NavigatorState navigator = navigatorKey.currentState!;
     return navigator.maybePop();
   }
 
   @override
-  Future<void> setNewRoutePath(_MyPageConfiguration configuration) {
-    _pages.add(configuration);
+  Future<void> setNewRoutePath(_MyNavigationConfiguration configuration) {
+    assert(configuration.pages.length == 1);
+    _pages.add(configuration.pages.first);
     _notifyListeners();
     return SynchronousFuture<void>(null);
   }
 
   @override
+  Future<void> setRestoredRoutePath(_MyNavigationConfiguration configuration) {
+    assert(_pages.isEmpty);
+
+    _pages.addAll(configuration.pages);
+    _notifyListeners();
+    return SynchronousFuture<void>(null);
+  }
+
+  @override
+  _MyNavigationConfiguration? get currentConfiguration {
+    return _MyNavigationConfiguration(pages: _pages);
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Navigator(
-      key: _navigatorKey,
+      key: navigatorKey,
       restorationScopeId: 'root',
       onDidRemovePage: (Page<void> page) {
-        final _MyPageConfiguration pageConfiguration = _MyPageConfiguration.fromName(page.name!);
-        assert(!_pages.contains(pageConfiguration) || _pages.length > 1);
-        _pages.remove(pageConfiguration);
+        final _MyPage myPage = _MyPage.fromName(page.name!);
+        assert(!_pages.contains(myPage) || _pages.length > 1);
+        _pages.remove(myPage);
       },
-      pages: _pages.map((_MyPageConfiguration page) => switch (page) {
-        _MyPageConfiguration.unknown => _MyUnknownPage(),
-        _MyPageConfiguration.home => _MyHomePage(
+      pages: _pages.map((_MyPage myPage) => switch (myPage) {
+        _MyPage.unknown => _MyUnknownPage(),
+        _MyPage.home => _MyHomePage(
           onNavigateToLeaf: _onNavigateToLeaf,
         ),
-        _MyPageConfiguration.leaf => _MyLeafPage(
+        _MyPage.leaf => _MyLeafPage(
           onNavigateToHome: _onNavigateToHome,
           onNavigateToNested: _onNavigateToNested,
         ),
-        _MyPageConfiguration.nestedNavigator => _MyNestedNavigatorPage(
+        _MyPage.nestedNavigator => _MyNestedNavigatorPage(
           onNavigateToRootNavigator: _onNavigateToLeaf,
         ),
       }).toList(),
@@ -146,7 +175,6 @@ class _MyRouterDelegate extends RouterDelegate<_MyPageConfiguration> {
 
 class _MyUnknownPage extends MaterialPage<dynamic> {
   _MyUnknownPage() : super(
-    key: const ValueKey<String>('_MyUnknownPage'),
     restorationId: 'unknown-page',
     child: Scaffold(
       backgroundColor: const Color(0xff660000),
@@ -158,14 +186,13 @@ class _MyUnknownPage extends MaterialPage<dynamic> {
   );
 
   @override
-  String get name => _MyPageConfiguration.unknown.name;
+  String get name => _MyPage.unknown.name;
 }
 
 class _MyHomePage extends MaterialPage<dynamic> {
   _MyHomePage({
     required VoidCallback onNavigateToLeaf,
   }) : super(
-    key: const ValueKey<String>('_MyHomePage'),
     restorationId: 'home-page',
     child: Scaffold(
       backgroundColor: const Color(0xffddddff),
@@ -180,7 +207,7 @@ class _MyHomePage extends MaterialPage<dynamic> {
   );
 
   @override
-  String get name => _MyPageConfiguration.home.name;
+  String get name => _MyPage.home.name;
 }
 
 class _MyLeafPage extends MaterialPage<dynamic> {
@@ -188,7 +215,6 @@ class _MyLeafPage extends MaterialPage<dynamic> {
     required VoidCallback onNavigateToHome,
     required VoidCallback onNavigateToNested,
   }) : super(
-    key: const ValueKey<String>('_MyLeafPage'),
     restorationId: 'leaf-page',
     child: Scaffold(
       backgroundColor: const Color(0xffaaaaff),
@@ -212,14 +238,13 @@ class _MyLeafPage extends MaterialPage<dynamic> {
   );
 
   @override
-  String get name => _MyPageConfiguration.leaf.name;
+  String get name => _MyPage.leaf.name;
 }
 
 class _MyNestedNavigatorPage extends MaterialPage<dynamic> {
   _MyNestedNavigatorPage({
     required VoidCallback onNavigateToRootNavigator,
   }) : super(
-    key: const ValueKey<String>('_MyNestedNavigatorPage'),
     restorationId: 'nested-navigator-page',
     child: _MyNestedNavigator(
       onNavigateToRootNavigator: onNavigateToRootNavigator,
@@ -227,7 +252,7 @@ class _MyNestedNavigatorPage extends MaterialPage<dynamic> {
   );
 
   @override
-  String get name => _MyPageConfiguration.nestedNavigator.name;
+  String get name => _MyPage.nestedNavigator.name;
 }
 
 class _MyNestedNavigator extends StatefulWidget {
@@ -241,45 +266,112 @@ class _MyNestedNavigator extends StatefulWidget {
   State<_MyNestedNavigator> createState() => _MyNestedNavigatorState();
 }
 
-class _MyNestedNavigatorState extends State<_MyNestedNavigator> {
-  final List<_MyNestedPageConfiguration> _pages = <_MyNestedPageConfiguration>[
-    _MyNestedPageConfiguration.home,
-  ];
+class _MyNestedNavigatorState extends State<_MyNestedNavigator> with RestorationMixin {
+  final _RestorablePages _pages = _RestorablePages();
 
   void _onNavigateToLeaf() {
-    assert(!_pages.contains(_MyNestedPageConfiguration.leaf), 'Should not ever be two leaf pages on the navigation stack.');
+    assert(!_pages.value.contains(_MyNestedPage.leaf), 'Should not ever be two leaf pages on the navigation stack.');
     setState(() {
-      _pages.add(_MyNestedPageConfiguration.leaf);
+      _pages.value = <_MyNestedPage>[
+        ..._pages.value,
+        _MyNestedPage.leaf,
+      ];
     });
   }
 
   void _onNavigateToHome() {
     setState(() {
-      _pages.clear();
-      _pages.add(_MyNestedPageConfiguration.home);
+      _pages.value = <_MyNestedPage>[
+        _MyNestedPage.home,
+      ];
     });
+  }
+
+  // Begin RestorationMixin.
+
+  @override
+  String? get restorationId => 'nested-navigator-page';
+
+  @override
+  void restoreState(RestorationBucket? oldBucket, bool initialRestore) {
+    registerForRestoration(_pages, 'pages');
+  }
+
+  /// End RestorationMixin.
+
+  @override
+  void dispose() {
+    _pages.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Navigator(
-      restorationScopeId: 'nested',
-      onDidRemovePage: (Page<void> page) {
-        final _MyNestedPageConfiguration pageConfiguration = _MyNestedPageConfiguration.fromName(page.name!);
-        assert(!_pages.contains(pageConfiguration) || _pages.length > 1);
-        _pages.remove(pageConfiguration);
+    return NavigatorPopHandler(
+      onPop: () {
+        assert(_pages.value.isNotEmpty);
+        setState(() {
+          _pages.value = _pages.value.take(_pages.value.length - 1);
+        });
       },
-      pages: _pages.map((_MyNestedPageConfiguration page) => switch (page) {
-        _MyNestedPageConfiguration.unknown => _MyUnknownPage(),
-        _MyNestedPageConfiguration.home => _MyNestedHomePage(
-          onNavigateToLeaf: _onNavigateToLeaf,
-          onNavigateToRootNavigator: widget.onNavigateToRootNavigator,
-        ),
-        _MyNestedPageConfiguration.leaf => _MyNestedLeafPage(
-          onNavigateToHome: _onNavigateToHome,
-        ),
-      }).toList(),
-  );
+      child: Navigator(
+        restorationScopeId: 'nested-navigator',
+        onDidRemovePage: (Page<void> page) {
+          final _MyNestedPage pageConfiguration = _MyNestedPage.fromName(page.name!);
+          assert(!_pages.value.contains(pageConfiguration) || _pages.value.length > 1);
+          _pages.value = _pages.value
+              .skipWhile((_MyNestedPage nestedPageConfiguration) {
+                return nestedPageConfiguration == pageConfiguration;
+              });
+        },
+        pages: _pages.value.map((_MyNestedPage page) => switch (page) {
+          _MyNestedPage.unknown => _MyUnknownPage(),
+          _MyNestedPage.home => _MyNestedHomePage(
+            onNavigateToLeaf: _onNavigateToLeaf,
+            onNavigateToRootNavigator: widget.onNavigateToRootNavigator,
+          ),
+          _MyNestedPage.leaf => _MyNestedLeafPage(
+            onNavigateToHome: _onNavigateToHome,
+          ),
+        }).toList(),
+      ),
+    );
+  }
+}
+
+class _RestorablePages extends RestorableValue<Iterable<_MyNestedPage>> {
+  @override
+  List<_MyNestedPage> createDefaultValue() => <_MyNestedPage>[
+    _MyNestedPage.home,
+  ];
+
+  @override
+  void didUpdateValue(Iterable<_MyNestedPage>? oldValue) {
+    if (oldValue == null || oldValue != value) {
+      notifyListeners();
+    }
+  }
+
+  @override
+  List<_MyNestedPage> fromPrimitives(Object? data) {
+    if (data != null) {
+      final String dataString = data as String;
+      final List<String> listOfStrings = dataString.split(',');
+      return listOfStrings.map((String nestedPageConfigurationName) {
+        return _MyNestedPage.values
+            .firstWhere((_MyNestedPage nestedPageConfiguration) {
+              return nestedPageConfigurationName == nestedPageConfiguration.name;
+            });
+      }).toList();
+    }
+    return <_MyNestedPage>[];
+  }
+
+  @override
+  Object toPrimitives() {
+    return value
+        .map((_MyNestedPage nestedPageConfiguration) => nestedPageConfiguration.name)
+        .join(',');
   }
 }
 
@@ -288,7 +380,6 @@ class _MyNestedHomePage extends MaterialPage<dynamic> {
     required VoidCallback onNavigateToLeaf,
     required VoidCallback onNavigateToRootNavigator,
   }) : super(
-    key: const ValueKey<String>('_MyNestedHomePage'),
     restorationId: 'nested-home-page',
     child: Scaffold(
       backgroundColor: const Color(0xffddffdd),
@@ -312,14 +403,13 @@ class _MyNestedHomePage extends MaterialPage<dynamic> {
   );
 
   @override
-  String get name => _MyPageConfiguration.home.name;
+  String get name => _MyPage.home.name;
 }
 
 class _MyNestedLeafPage extends MaterialPage<dynamic> {
   _MyNestedLeafPage({
     required VoidCallback onNavigateToHome,
   }) : super(
-    key: const ValueKey<String>('_MyNestedLeafPage'),
     restorationId: 'nested-leaf-page',
     child: Scaffold(
       backgroundColor: const Color(0xffaaffaa),
@@ -334,41 +424,67 @@ class _MyNestedLeafPage extends MaterialPage<dynamic> {
   );
 
   @override
-  String get name => _MyPageConfiguration.leaf.name;
+  String get name => _MyPage.leaf.name;
 }
 
-enum _MyPageConfiguration {
+class _MyNavigationConfiguration {
+  _MyNavigationConfiguration({
+    required this.pages,
+  }) : assert(pages.isNotEmpty);
+
+  factory _MyNavigationConfiguration.single(
+    _MyPage page,
+  ) {
+    return _MyNavigationConfiguration(pages: <_MyPage>[page]);
+  }
+
+  final List<_MyPage> pages;
+
+  String  get uriString => pages.first.uriString;
+  Uri get uri => pages.first.uri;
+
+  @override
+  String toString() {
+    return '_MyNavigationConfiguration(pages: $pages)';
+  }
+}
+
+enum _MyPage {
   home(uriString: '/'),
   leaf(uriString: '/leaf'),
   nestedNavigator(uriString: '/nested_navigator'),
   unknown(uriString: '/404');
 
-  const _MyPageConfiguration({
+  const _MyPage({
     required this.uriString,
   });
 
   final String uriString;
 
-  static _MyPageConfiguration fromName(String testName) {
-    return values.firstWhere((_MyPageConfiguration page) => page.name == testName);
+  static _MyPage fromName(String testName) {
+    return values.firstWhere((_MyPage page) => page.name == testName);
   }
 
+
   Uri get uri => Uri.parse(uriString);
+
+  @override
+  String toString() => name;
 }
 
-enum _MyNestedPageConfiguration {
+enum _MyNestedPage {
   home(uriString: '/'),
   leaf(uriString: '/leaf'),
   unknown(uriString: '/404');
 
-  const _MyNestedPageConfiguration({
+  const _MyNestedPage({
     required this.uriString,
   });
 
   final String uriString;
 
-  static _MyNestedPageConfiguration fromName(String testName) {
-    return values.firstWhere((_MyNestedPageConfiguration page) => page.name == testName);
+  static _MyNestedPage fromName(String testName) {
+    return values.firstWhere((_MyNestedPage page) => page.name == testName);
   }
 
   Uri get uri => Uri.parse(uriString);
