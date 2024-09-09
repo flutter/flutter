d55d5f9832c9c2673790174e60f76044dfe24f6c
Matan Lurey <matan@lurey.org>
2024-09-09 12:50:23 -0700

    ++

--- dev/integration_tests/native_driver_test/android/app/src/main/kotlin/com/example/native_driver_test/SmileyFaceTexturePlugin.kt
+++ dev/integration_tests/native_driver_test/android/app/src/main/kotlin/com/example/native_driver_test/SmileyFaceTexturePlugin.kt
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+@file:Suppress("PackageName")
+
 package com.example.native_driver_test
 
 import android.graphics.Color
