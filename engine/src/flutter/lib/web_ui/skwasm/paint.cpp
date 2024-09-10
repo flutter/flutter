d7e52c7710867f6a4b81588f65206cde3fc6c63c
Yegor <yjbanov@google.com>
2024-09-09 21:10:26 -0700

    [skwasm] use temporary RawPaint objects (attempt #2) (flutter/engine#55035)
    Relands https://github.com/flutter/engine/pull/54917. The change is the same as before, except now the native resources for `SkwasmColorFilter` and `SkwasmImageFilter` classes are no longer GC'd. Instead, we use manually managed native handles (vended and scoped by `withRawColorFilter` and `withRawImageFilter`). The bug in the previous PR was that filter objects were disposed with the paint while the framework continued holding onto them. When GC kicked the finalization registry, it attempted to double-free the filters.

--- engine/src/flutter/lib/web_ui/skwasm/paint.cpp
+++ engine/src/flutter/lib/web_ui/skwasm/paint.cpp
@@ -12,78 +12,28 @@
 
 using namespace Skwasm;
 
-SKWASM_EXPORT SkPaint* paint_create() {
+SKWASM_EXPORT SkPaint* paint_create(bool isAntiAlias,
+                                    SkBlendMode blendMode,
+                                    SkColor color,
+                                    SkPaint::Style style,
+                                    SkScalar strokeWidth,
+                                    SkPaint::Cap strokeCap,
+                                    SkPaint::Join strokeJoin,
+                                    SkScalar strokeMiterLimit) {
   auto paint = new SkPaint();
-
-  // Antialias defaults to true in flutter.
-  paint->setAntiAlias(true);
+  paint->setAntiAlias(isAntiAlias);
+  paint->setBlendMode(blendMode);
+  paint->setStyle(style);
+  paint->setStrokeWidth(strokeWidth);
+  paint->setStrokeCap(strokeCap);
+  paint->setStrokeJoin(strokeJoin);
+  paint->setColor(color);
+  paint->setStrokeMiter(strokeMiterLimit);
   return paint;
 }
 
 SKWASM_EXPORT void paint_dispose(SkPaint* paint) {
   delete paint;
-}
-
-SKWASM_EXPORT void paint_setBlendMode(SkPaint* paint, SkBlendMode mode) {
-  paint->setBlendMode(mode);
-}
-
-// No getter for blend mode, as it's non trivial. Cache on the dart side.
-
-SKWASM_EXPORT void paint_setStyle(SkPaint* paint, SkPaint::Style style) {
-  paint->setStyle(style);
-}
-
-SKWASM_EXPORT SkPaint::Style paint_getStyle(SkPaint* paint) {
-  return paint->getStyle();
-}
-
-SKWASM_EXPORT void paint_setStrokeWidth(SkPaint* paint, SkScalar width) {
-  paint->setStrokeWidth(width);
-}
-
-SKWASM_EXPORT SkScalar paint_getStrokeWidth(SkPaint* paint) {
-  return paint->getStrokeWidth();
-}
-
-SKWASM_EXPORT void paint_setStrokeCap(SkPaint* paint, SkPaint::Cap cap) {
-  paint->setStrokeCap(cap);
-}
-
-SKWASM_EXPORT SkPaint::Cap paint_getStrokeCap(SkPaint* paint) {
-  return paint->getStrokeCap();
-}
-
-SKWASM_EXPORT void paint_setStrokeJoin(SkPaint* paint, SkPaint::Join join) {
-  paint->setStrokeJoin(join);
-}
-
-SKWASM_EXPORT SkPaint::Join paint_getStrokeJoin(SkPaint* paint) {
-  return paint->getStrokeJoin();
-}
-
-SKWASM_EXPORT void paint_setAntiAlias(SkPaint* paint, bool antiAlias) {
-  paint->setAntiAlias(antiAlias);
-}
-
-SKWASM_EXPORT bool paint_getAntiAlias(SkPaint* paint) {
-  return paint->isAntiAlias();
-}
-
-SKWASM_EXPORT void paint_setColorInt(SkPaint* paint, SkColor colorInt) {
-  paint->setColor(colorInt);
-}
-
-SKWASM_EXPORT SkColor paint_getColorInt(SkPaint* paint) {
-  return paint->getColor();
-}
-
-SKWASM_EXPORT void paint_setMiterLimit(SkPaint* paint, SkScalar miterLimit) {
-  paint->setStrokeMiter(miterLimit);
-}
-
-SKWASM_EXPORT SkScalar paint_getMiterLimit(SkPaint* paint) {
-  return paint->getStrokeMiter();
 }
 
 SKWASM_EXPORT void paint_setShader(SkPaint* paint, SkShader* shader) {
