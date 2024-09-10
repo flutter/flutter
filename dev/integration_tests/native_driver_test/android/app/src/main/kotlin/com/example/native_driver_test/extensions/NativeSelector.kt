4706a2f42b7b6c6403bc30249c0bc65654d36d84
Matan Lurey <matan@lurey.org>
2024-09-09 17:53:31 -0700

    ++

--- dev/integration_tests/native_driver_test/android/app/src/main/kotlin/com/example/native_driver_test/extensions/NativeSelector.kt
+++ dev/integration_tests/native_driver_test/android/app/src/main/kotlin/com/example/native_driver_test/extensions/NativeSelector.kt
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+@file:Suppress("PackageName")
+
 package com.example.native_driver_test.extensions
 
 import android.view.View
