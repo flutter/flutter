66b8baa6ea0631bbe87619c803e84bc5ad2871ee
Denis Bowen <42016383+DBowen33@users.noreply.github.com>
2024-09-13 12:38:28 -0500

    Update dev/a11y_assessments/lib/use_cases/text_button.dart
    Co-authored-by: Nate Wilson <nathan.wilson1232@gmail.com>

--- dev/a11y_assessments/lib/use_cases/text_button.dart
+++ dev/a11y_assessments/lib/use_cases/text_button.dart
@@ -56,7 +56,7 @@ class MainWidgetState extends State<MainWidget> {
                   // Validate returns true if the form is valid, or false otherwise.
                   if (_formKey.currentState!.validate()) {
                     // If the form is valid, display a snackbar. In the real world,
-                    // you'd often call a server or save the information in a database.
+                    // this might also send a request to a server.
                     ScaffoldMessenger.of(context).showSnackBar(
                       const SnackBar(content: Text('Form submitted')),
                     );
