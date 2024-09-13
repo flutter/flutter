0ac849cc16babbd4ba730b3522b8f988e74aa129
Reid Baker <reidbaker@google.com>
2024-09-13 11:31:52 -0400

    Add copyright notices to java test files (flutter/engine#55155)
    Fixes flutter/flutter/issues/155118

--- engine/src/flutter/shell/platform/android/test/io/flutter/embedding/engine/systemchannels/LifecycleChannelTest.java
+++ engine/src/flutter/shell/platform/android/test/io/flutter/embedding/engine/systemchannels/LifecycleChannelTest.java
@@ -1,3 +1,7 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 package io.flutter.embedding.engine.systemchannels;
 
 import static org.junit.Assert.assertEquals;
