44cb734ba7f51bc49b73fe8c0504e322275e1933
Renzo Olivares <roliv@google.com>
2024-09-13 14:25:08 -0700

    Add CollapseSelectionEvent

--- packages/flutter/lib/src/widgets/scrollable.dart
+++ packages/flutter/lib/src/widgets/scrollable.dart
@@ -1472,6 +1472,7 @@ class _ScrollableSelectionContainerDelegate extends MultiSelectableSelectionCont
         _selectableEndEdgeUpdateRecords.remove(selectable);
         _selectableStartEdgeUpdateRecords.remove(selectable);
       case SelectionEventType.selectAll:
+      case SelectionEventType.collapseSelection:
       case SelectionEventType.selectWord:
       case SelectionEventType.selectParagraph:
         _selectableEndEdgeUpdateRecords[selectable] = state.position.pixels;
