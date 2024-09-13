0cc27b4e22559e95ba6cbd3683fdf30b32c41ed9
Jackson Gardner <jacksongardner@google.com>
2024-09-13 10:55:07 -0700

    [skwasm] Scene builder optimizations for platform view placement (flutter/engine#54949)
    This PR refactors the scene builder's logic in order to more aggressively merge flutter content and platform view content together. This essentially covers the case discussed in this flutter issue: https://github.com/flutter/flutter/issues/149863
    
    This optimization ensures that each picture or platform view is applied to the lowest possible slice in the scene, which avoids the proliferation of redundant slices and overlays in the scene.

--- engine/src/flutter/lib/web_ui/test/engine/scene_builder_test.dart
+++ engine/src/flutter/lib/web_ui/test/engine/scene_builder_test.dart
@@ -16,7 +16,7 @@ void main() {
 
 void testMain() {
   setUpAll(() {
-    LayerBuilder.debugRecorderFactory = (ui.Rect rect) {
+    LayerSliceBuilder.debugRecorderFactory = () {
       final StubSceneCanvas canvas = StubSceneCanvas();
       final StubPictureRecorder recorder = StubPictureRecorder(canvas);
       return (recorder, canvas);
@@ -24,7 +24,7 @@ void testMain() {
   });
 
   tearDownAll(() {
-    LayerBuilder.debugRecorderFactory = null;
+    LayerSliceBuilder.debugRecorderFactory = null;
   });
 
   group('EngineSceneBuilder', () {
@@ -35,23 +35,23 @@ void testMain() {
       sceneBuilder.addPicture(ui.Offset.zero, StubPicture(pictureRect));
 
       final EngineScene scene = sceneBuilder.build() as EngineScene;
-      final List<LayerSlice> slices = scene.rootLayer.slices;
+      final List<LayerSlice?> slices = scene.rootLayer.slices;
       expect(slices.length, 1);
-      expect(slices[0], pictureSliceWithRect(pictureRect));
+      expect(slices[0], layerSlice(withPictureRect: pictureRect));
     });
 
     test('two pictures', () {
       final EngineSceneBuilder sceneBuilder = EngineSceneBuilder();
 
       const ui.Rect pictureRect1 = ui.Rect.fromLTRB(100, 100, 200, 200);
-      const ui.Rect pictureRect2 = ui.Rect.fromLTRB(300, 400, 400, 400);
+      const ui.Rect pictureRect2 = ui.Rect.fromLTRB(300, 300, 400, 400);
       sceneBuilder.addPicture(ui.Offset.zero, StubPicture(pictureRect1));
       sceneBuilder.addPicture(ui.Offset.zero, StubPicture(pictureRect2));
 
       final EngineScene scene = sceneBuilder.build() as EngineScene;
-      final List<LayerSlice> slices = scene.rootLayer.slices;
+      final List<LayerSlice?> slices = scene.rootLayer.slices;
       expect(slices.length, 1);
-      expect(slices[0], pictureSliceWithRect(const ui.Rect.fromLTRB(100, 100, 400, 400)));
+      expect(slices[0], layerSlice(withPictureRect: const ui.Rect.fromLTRB(100, 100, 400, 400)));
     });
 
     test('picture + platform view (overlapping)', () {
@@ -68,10 +68,11 @@ void testMain() {
       );
 
       final EngineScene scene = sceneBuilder.build() as EngineScene;
-      final List<LayerSlice> slices = scene.rootLayer.slices;
-      expect(slices.length, 2);
-      expect(slices[0], pictureSliceWithRect(pictureRect));
-      expect(slices[1], platformViewSliceWithViews(<PlatformView>[
+      final List<LayerSlice?> slices = scene.rootLayer.slices;
+      expect(slices.length, 1);
+      expect(slices[0], layerSlice(
+        withPictureRect: pictureRect,
+        withPlatformViews: <PlatformView>[
         PlatformView(1, platformViewRect, const PlatformViewStyling())
       ]));
     });
@@ -90,12 +91,12 @@ void testMain() {
       sceneBuilder.addPicture(ui.Offset.zero, StubPicture(pictureRect));
 
       final EngineScene scene = sceneBuilder.build() as EngineScene;
-      final List<LayerSlice> slices = scene.rootLayer.slices;
+      final List<LayerSlice?> slices = scene.rootLayer.slices;
       expect(slices.length, 2);
-      expect(slices[0], platformViewSliceWithViews(<PlatformView>[
+      expect(slices[0], layerSlice(withPlatformViews: <PlatformView>[
         PlatformView(1, platformViewRect, const PlatformViewStyling())
       ]));
-      expect(slices[1], pictureSliceWithRect(pictureRect));
+      expect(slices[1], layerSlice(withPictureRect: pictureRect));
     });
 
     test('platform view sandwich (overlapping)', () {
@@ -114,13 +115,14 @@ void testMain() {
       sceneBuilder.addPicture(ui.Offset.zero, StubPicture(pictureRect2));
 
       final EngineScene scene = sceneBuilder.build() as EngineScene;
-      final List<LayerSlice> slices = scene.rootLayer.slices;
-      expect(slices.length, 3);
-      expect(slices[0], pictureSliceWithRect(pictureRect1));
-      expect(slices[1], platformViewSliceWithViews(<PlatformView>[
+      final List<LayerSlice?> slices = scene.rootLayer.slices;
+      expect(slices.length, 2);
+      expect(slices[0], layerSlice(
+        withPictureRect: pictureRect1,
+        withPlatformViews: <PlatformView>[
         PlatformView(1, platformViewRect, const PlatformViewStyling())
       ]));
-      expect(slices[2], pictureSliceWithRect(pictureRect2));
+      expect(slices[1], layerSlice(withPictureRect: pictureRect2));
     });
 
     test('platform view sandwich (non-overlapping)', () {
@@ -139,14 +141,15 @@ void testMain() {
       sceneBuilder.addPicture(ui.Offset.zero, StubPicture(pictureRect2));
 
       final EngineScene scene = sceneBuilder.build() as EngineScene;
-      final List<LayerSlice> slices = scene.rootLayer.slices;
+      final List<LayerSlice?> slices = scene.rootLayer.slices;
 
       // The top picture does not overlap with the platform view, so it should
       // be grouped into the slice below it to reduce the number of canvases we
       // need.
-      expect(slices.length, 2);
-      expect(slices[0], pictureSliceWithRect(const ui.Rect.fromLTRB(50, 50, 200, 200)));
-      expect(slices[1], platformViewSliceWithViews(<PlatformView>[
+      expect(slices.length, 1);
+      expect(slices[0], layerSlice(
+        withPictureRect: const ui.Rect.fromLTRB(50, 50, 200, 200),
+        withPlatformViews: <PlatformView>[
         PlatformView(1, platformViewRect, const PlatformViewStyling())
       ]));
     });
@@ -169,34 +172,99 @@ void testMain() {
       sceneBuilder.addPicture(ui.Offset.zero, StubPicture(const ui.Rect.fromLTRB(0, 0, 100, 100)));
 
       final EngineScene scene = sceneBuilder.build() as EngineScene;
-      final List<LayerSlice> slices = scene.rootLayer.slices;
-      expect(slices.length, 3);
-      expect(slices[0], pictureSliceWithRect(pictureRect1));
-      expect(slices[1], platformViewSliceWithViews(<PlatformView>[
+      final List<LayerSlice?> slices = scene.rootLayer.slices;
+      expect(slices.length, 2);
+      expect(slices[0], layerSlice(
+        withPictureRect: pictureRect1,
+        withPlatformViews: <PlatformView>[
         PlatformView(1, platformViewRect, const PlatformViewStyling(position: PlatformViewPosition.offset(ui.Offset(150, 150))))
       ]));
-      expect(slices[2], pictureSliceWithRect(const ui.Rect.fromLTRB(200, 200, 300, 300)));
+      expect(slices[1], layerSlice(withPictureRect: const ui.Rect.fromLTRB(200, 200, 300, 300)));
+    });
+
+    test('grid view test', () {
+      // This test case covers a grid of elements, where each element is a platform
+      // view that has flutter content underneath it and on top of it.
+      // See a detailed explanation of this use-case in the following flutter issue:
+      // https://github.com/flutter/flutter/issues/149863
+      final EngineSceneBuilder sceneBuilder = EngineSceneBuilder();
+
+      const double padding = 10;
+      const double tileSize = 50;
+      final List<PlatformView> expectedPlatformViews = <PlatformView>[];
+      for (int x = 0; x < 10; x++) {
+        for (int y = 0; y < 10; y++) {
+          final ui.Offset offset = ui.Offset(
+            padding + (tileSize + padding) * x,
+            padding + (tileSize + padding) * y,
+          );
+          sceneBuilder.pushOffset(offset.dx, offset.dy);
+          sceneBuilder.addPicture(
+            ui.Offset.zero,
+            StubPicture(const ui.Rect.fromLTWH(0, 0, tileSize, tileSize))
+          );
+          sceneBuilder.addPlatformView(
+            1,
+            offset: const ui.Offset(5, 5),
+            width: tileSize - 10,
+            height: tileSize - 10,
+          );
+          sceneBuilder.addPicture(
+            const ui.Offset(10, 10),
+            StubPicture(const ui.Rect.fromLTWH(0, 0, tileSize - 20, tileSize - 20)),
+          );
+          sceneBuilder.pop();
+          expectedPlatformViews.add(PlatformView(
+            1,
+            const ui.Rect.fromLTRB(5.0, 5.0, tileSize - 5.0, tileSize - 5.0),
+            PlatformViewStyling(position: PlatformViewPosition.offset(offset))
+          ));
+        }
+      }
+
+      final EngineScene scene = sceneBuilder.build() as EngineScene;
+      final List<LayerSlice?> slices = scene.rootLayer.slices;
+
+      // It is important that the optimizations of the scene builder result in
+      // there only being two scene slices.
+      expect(slices.length, 2);
+      expect(slices[0], layerSlice(
+        withPictureRect: const ui.Rect.fromLTRB(
+          padding,
+          padding,
+          10 * (padding + tileSize),
+          10 * (padding + tileSize)
+        ),
+        withPlatformViews: expectedPlatformViews,
+      ));
+      expect(slices[1], layerSlice(withPictureRect: const ui.Rect.fromLTRB(
+        padding + 10,
+        padding + 10,
+        10 * (padding + tileSize) - 10,
+        10 * (padding + tileSize) - 10,
+      )));
     });
   });
 }
 
-PictureSliceMatcher pictureSliceWithRect(ui.Rect rect) => PictureSliceMatcher(rect);
-PlatformViewSliceMatcher platformViewSliceWithViews(List<PlatformView> views)
-  => PlatformViewSliceMatcher(views);
+LayerSliceMatcher layerSlice({
+  ui.Rect withPictureRect = ui.Rect.zero,
+  List<PlatformView> withPlatformViews = const <PlatformView>[],
+}) => LayerSliceMatcher(withPictureRect, withPlatformViews);
+class LayerSliceMatcher extends Matcher {
+  LayerSliceMatcher(this.expectedPictureRect, this.expectedPlatformViews);
 
-class PictureSliceMatcher extends Matcher {
-  PictureSliceMatcher(this.expectedRect);
-
-  final ui.Rect expectedRect;
+  final ui.Rect expectedPictureRect;
+  final List<PlatformView> expectedPlatformViews;
 
   @override
   Description describe(Description description) {
-    return description.add('<picture slice with cullRect: $expectedRect>');
+    return description.add('<picture slice with cullRect: $expectedPictureRect and platform views: $expectedPlatformViews>');
   }
 
   @override
   bool matches(dynamic item, Map<dynamic, dynamic> matchState) {
-    if (item is! PictureSlice) {
+    if (item is! LayerSlice) {
       return false;
     }
     final ScenePicture picture = item.picture;
@@ -204,50 +272,28 @@ class PictureSliceMatcher extends Matcher {
       return false;
     }
 
-    if (picture.cullRect != expectedRect) {
+    if (picture.cullRect != expectedPictureRect) {
       return false;
     }
 
-    return true;
-  }
-}
-
-class PlatformViewSliceMatcher extends Matcher {
-  PlatformViewSliceMatcher(this.expectedPlatformViews);
-
-  final List<PlatformView> expectedPlatformViews;
-
-  @override
-  Description describe(Description description) {
-    return description.add('<platform view slice with platform views: $expectedPlatformViews>');
-  }
-
-  @override
-  bool matches(dynamic item, Map<dynamic, dynamic> matchState) {
-    if (item is! PlatformViewSlice) {
+    if (item.platformViews.length != expectedPlatformViews.length) {
       return false;
     }
 
-    if (item.views.length != expectedPlatformViews.length) {
-      return false;
-    }
-
-    for (int i = 0; i < item.views.length; i++) {
+    for (int i = 0; i < item.platformViews.length; i++) {
       final PlatformView expectedView = expectedPlatformViews[i];
-      final PlatformView actualView = item.views[i];
+      final PlatformView actualView = item.platformViews[i];
       if (expectedView.viewId != actualView.viewId) {
-        print('viewID mismatch');
         return false;
       }
       if (expectedView.bounds != actualView.bounds) {
-        print('bounds mismatch');
         return false;
       }
       if (expectedView.styling != actualView.styling) {
-        print('styling mismatch');
         return false;
       }
     }
+
     return true;
   }
 }
