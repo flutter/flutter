3c0fd7ca994db7e3b57ec860215a4b0650fe24fe
Reid Baker <reidbaker@google.com>
2024-09-13 11:31:52 -0400

    Add copyright notices to java test files (#55155)
    Fixes flutter/flutter/issues/155118

--- shell/platform/android/test/io/flutter/embedding/engine/FlutterShellArgsTest.java
+++ shell/platform/android/test/io/flutter/embedding/engine/FlutterShellArgsTest.java
@@ -1,3 +1,7 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 package test.io.flutter.embedding.engine;
 
 import static org.junit.Assert.assertEquals;
