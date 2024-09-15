b018ce6abc725c49932c54e15f6419473b45dc2d
PurplePolyhedron <120297255+PurplePolyhedron@users.noreply.github.com>
2024-09-16 00:50:46 +1000

    Update search_anchor_test.dart

--- packages/flutter/test/material/search_anchor_test.dart
+++ packages/flutter/test/material/search_anchor_test.dart
@@ -3454,8 +3454,9 @@ void main() {
     expect(tester.takeException(), isNull);
     ChangeNotifier.debugAssertNotDisposed(controller);
   });
+
   // Regression test for https://github.com/flutter/flutter/issues/155180.
-  testWidgets('SearchAnchor close menu when disposed', (WidgetTester tester) async {
+  testWidgets('SearchAnchor closes menu when disposed', (WidgetTester tester) async {
     bool disposed = false;
     late StateSetter setState;
     await tester.pumpWidget(
