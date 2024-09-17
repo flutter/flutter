8b470e060bdd26712e833d081162f757dc851dad
gaaclarke <30870216+gaaclarke@users.noreply.github.com>
2024-09-17 15:24:05 -0700

    Fixed solid blur style math (#55194)
    fixes https://github.com/flutter/flutter/issues/155075
    
    [C++, Objective-C, Java style guides]: https://github.com/flutter/engine/blob/main/CONTRIBUTING.md#style

--- impeller/display_list/aiks_dl_blur_unittests.cc
+++ impeller/display_list/aiks_dl_blur_unittests.cc
@@ -291,6 +291,7 @@ static sk_sp<DisplayList> MaskBlurVariantTest(
   builder.Scale(test_context.GetContentScale().x,
                 test_context.GetContentScale().y);
   builder.Scale(0.8f, 0.8f);
+  builder.Translate(50.f, 50.f);
 
   DlPaint draw_paint;
   draw_paint.setColor(
