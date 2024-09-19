26ab2a7c8503438a1ba22ebf99e8f05e8656058e
kinda <kinda.dardese3@gmail.com>
2024-09-19 22:59:48 +0300

    Initial commit for Flutter project setup
    - Added basic project files and configurations
    - Set up Android and iOS directories
    - Included .gitignore and project metadata

---
+++ flutter_application_1/README.md
@@ -0,0 +1,3 @@
+# flutter_application_1
+
+A new Flutter project.
---
+++ flutter_application_1/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello World!'),
+        ),
+      ),
+    );
+  }
+}
