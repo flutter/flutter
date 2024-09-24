f7a4718dfd0a29fd1e0d3a9d83fb1c6253f98692
Jonah Williams <jonahwilliams@google.com>
2024-09-24 16:38:52 -0700

    [Impeller] fix Impeller on windows. (#55323)
    Fixes https://github.com/flutter/flutter/issues/141482
    
    Since the introduction of the flutter compositor, the windows embedder will create an offscreen framebuffer and then blit this to the onscreen framebuffer. Therefore, the onscreen framebuffer should not be constructed as a multisample framebuffer - and the EGL config can match skia.
    
    Also cleans up selection of the impeller PixelFormat, which might not have matched the selected compositor format because it was hardcoded to RGBA_8888

--- shell/platform/windows/flutter_windows_unittests.cc
+++ shell/platform/windows/flutter_windows_unittests.cc
@@ -31,7 +31,7 @@ namespace {
 // An EGL manager that initializes EGL but fails to create surfaces.
 class HalfBrokenEGLManager : public egl::Manager {
  public:
-  HalfBrokenEGLManager() : egl::Manager(/*enable_impeller = */ false) {}
+  HalfBrokenEGLManager() : egl::Manager() {}
 
   std::unique_ptr<egl::WindowSurface>
   CreateWindowSurface(HWND hwnd, size_t width, size_t height) override {
