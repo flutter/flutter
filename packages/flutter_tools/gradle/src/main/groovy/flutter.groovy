59923eaf67350fa7e5c2ec7db51b13a05c99c18c
Mohellebi abdessalem <mohellebiabdessalem@gmail.com>
2024-09-24 21:40:20 +0100

    Update flutter.groovy

--- packages/flutter_tools/gradle/src/main/groovy/flutter.groovy
+++ packages/flutter_tools/gradle/src/main/groovy/flutter.groovy
@@ -1423,7 +1423,7 @@ class FlutterPlugin implements Plugin<Project> {
                         filename += "-${buildModeFor(variant.buildType)}"
                         project.copy {
                             from new File("$outputDirectoryStr/${output.outputFileName}")
-                            into new File("${project.buildDir}/outputs/flutter-apk")
+                            into new File("${project.layout.buildDirectory.dir("/outputs/flutter-apk").get()}")
                             rename {
                                 return "${filename}.apk"
                             }
@@ -1431,7 +1431,7 @@ class FlutterPlugin implements Plugin<Project> {
                     }
                 }
                 // Copy the native assets created by build.dart and placed here by flutter assemble.
-                String nativeAssetsDir = "${project.buildDir}/../native_assets/android/jniLibs/lib/"
+                String nativeAssetsDir = project.layout.buildDirectory.dir("/../native_assets/android/jniLibs/lib/").get()
                 project.android.sourceSets.main.jniLibs.srcDir(nativeAssetsDir)
             }
             configurePlugins(project)
