c7e3dd6594b5b59b1b7ceddc75a27e076b81d741
Harry Terkelsen <1961493+harryterkelsen@users.noreply.github.com>
2024-09-25 16:47:18 -0700

    Reland "[canvaskit] Further improve overlay optimization by splitting pictures" (flutter/engine#55402)
    This enhances the overlay optimization by delaying combining pictures to get tighter bounds for the pictures that make up the scene, enabling more sophisticated optimization since we can determine if they intersect with platform views on a per-picture basis.
    
    Fixes https://github.com/flutter/flutter/issues/149863
    
    On a Macbook in Chrome in an example app with an infinite scrolling grid of platform views, this brings the ratio of dropped frames from 93% to 55% (roughly 4 fps to 30 fps).
    
    This is a reland of https://github.com/flutter/engine/pull/54878 with a fix for scenes with pictures that are eventually entirely clipped out.
    
    [C++, Objective-C, Java style guides]: https://github.com/flutter/engine/blob/main/CONTRIBUTING.md#style

--- engine/src/flutter/lib/web_ui/test/ui/scene_builder_test.dart
+++ engine/src/flutter/lib/web_ui/test/ui/scene_builder_test.dart
@@ -32,14 +32,12 @@ Future<void> testMain() async {
       sceneBuilder.pushOffset(150, 150);
       sceneBuilder.addPicture(ui.Offset.zero, drawPicture((ui.Canvas canvas) {
         canvas.drawCircle(
-          ui.Offset.zero,
-          50,
-          ui.Paint()..color = const ui.Color(0xFF00FF00)
-        );
+            ui.Offset.zero, 50, ui.Paint()..color = const ui.Color(0xFF00FF00));
       }));
 
       await renderScene(sceneBuilder.build());
-      await matchGoldenFile('scene_builder_centered_circle.png', region: region);
+      await matchGoldenFile('scene_builder_centered_circle.png',
+          region: region);
     });
 
     test('Test transform layer', () async {
@@ -54,93 +52,83 @@ Future<void> testMain() async {
       sceneBuilder.pushTransform(transform.toFloat64());
       sceneBuilder.addPicture(ui.Offset.zero, drawPicture((ui.Canvas canvas) {
         canvas.drawRRect(
-          ui.RRect.fromRectAndRadius(
-            ui.Rect.fromCircle(center: ui.Offset.zero, radius: 50),
-            const ui.Radius.circular(10)
-          ),
-          ui.Paint()..color = const ui.Color(0xFF0000FF)
-        );
+            ui.RRect.fromRectAndRadius(
+                ui.Rect.fromCircle(center: ui.Offset.zero, radius: 50),
+                const ui.Radius.circular(10)),
+            ui.Paint()..color = const ui.Color(0xFF0000FF));
       }));
 
       await renderScene(sceneBuilder.build());
-      await matchGoldenFile('scene_builder_rotated_rounded_square.png', region: region);
+      await matchGoldenFile('scene_builder_rotated_rounded_square.png',
+          region: region);
     });
 
     test('Test clipRect layer', () async {
       final ui.SceneBuilder sceneBuilder = ui.SceneBuilder();
       sceneBuilder.pushClipRect(const ui.Rect.fromLTRB(0, 0, 150, 150));
       sceneBuilder.addPicture(ui.Offset.zero, drawPicture((ui.Canvas canvas) {
-        canvas.drawCircle(
-          const ui.Offset(150, 150),
-          50,
-          ui.Paint()..color = const ui.Color(0xFFFF0000)
-        );
+        canvas.drawCircle(const ui.Offset(150, 150), 50,
+            ui.Paint()..color = const ui.Color(0xFFFF0000));
       }));
 
       await renderScene(sceneBuilder.build());
-      await matchGoldenFile('scene_builder_circle_clip_rect.png', region: region);
+      await matchGoldenFile('scene_builder_circle_clip_rect.png',
+          region: region);
     });
 
     test('Test clipRRect layer', () async {
       final ui.SceneBuilder sceneBuilder = ui.SceneBuilder();
-      sceneBuilder.pushClipRRect(ui.RRect.fromRectAndRadius(
-        const ui.Rect.fromLTRB(0, 0, 150, 150),
-        const ui.Radius.circular(25),
-      ), clipBehavior: ui.Clip.antiAlias);
+      sceneBuilder.pushClipRRect(
+          ui.RRect.fromRectAndRadius(
+            const ui.Rect.fromLTRB(0, 0, 150, 150),
+            const ui.Radius.circular(25),
+          ),
+          clipBehavior: ui.Clip.antiAlias);
       sceneBuilder.addPicture(ui.Offset.zero, drawPicture((ui.Canvas canvas) {
-        canvas.drawCircle(
-          const ui.Offset(150, 150),
-          50,
-          ui.Paint()..color = const ui.Color(0xFFFF00FF)
-        );
+        canvas.drawCircle(const ui.Offset(150, 150), 50,
+            ui.Paint()..color = const ui.Color(0xFFFF00FF));
       }));
 
       await renderScene(sceneBuilder.build());
-      await matchGoldenFile('scene_builder_circle_clip_rrect.png', region: region);
+      await matchGoldenFile('scene_builder_circle_clip_rrect.png',
+          region: region);
     });
 
     test('Test clipPath layer', () async {
       final ui.SceneBuilder sceneBuilder = ui.SceneBuilder();
       final ui.Path path = ui.Path();
-      path.addOval(ui.Rect.fromCircle(center: const ui.Offset(150, 150), radius: 60));
+      path.addOval(
+          ui.Rect.fromCircle(center: const ui.Offset(150, 150), radius: 60));
       sceneBuilder.pushClipPath(path);
       sceneBuilder.addPicture(ui.Offset.zero, drawPicture((ui.Canvas canvas) {
         canvas.drawRect(
-          ui.Rect.fromCircle(center: const ui.Offset(150, 150), radius: 50),
-          ui.Paint()..color = const ui.Color(0xFF00FFFF)
-        );
+            ui.Rect.fromCircle(center: const ui.Offset(150, 150), radius: 50),
+            ui.Paint()..color = const ui.Color(0xFF00FFFF));
       }));
 
       await renderScene(sceneBuilder.build());
-      await matchGoldenFile('scene_builder_rectangle_clip_circular_path.png', region: region);
+      await matchGoldenFile('scene_builder_rectangle_clip_circular_path.png',
+          region: region);
     });
 
     test('Test opacity layer', () async {
       final ui.SceneBuilder sceneBuilder = ui.SceneBuilder();
       sceneBuilder.addPicture(ui.Offset.zero, drawPicture((ui.Canvas canvas) {
         canvas.drawRect(
-          ui.Rect.fromCircle(center: const ui.Offset(150, 150), radius: 50),
-          ui.Paint()..color = const ui.Color(0xFF00FF00)
-        );
+            ui.Rect.fromCircle(center: const ui.Offset(150, 150), radius: 50),
+            ui.Paint()..color = const ui.Color(0xFF00FF00));
       }));
 
       sceneBuilder.pushOpacity(0x7F, offset: const ui.Offset(150, 150));
       sceneBuilder.addPicture(ui.Offset.zero, drawPicture((ui.Canvas canvas) {
         final ui.Paint paint = ui.Paint()..color = const ui.Color(0xFFFF0000);
-        canvas.drawCircle(
-          const ui.Offset(-25, 0),
-          50,
-          paint
-        );
-        canvas.drawCircle(
-          const ui.Offset(25, 0),
-          50,
-          paint
-        );
+        canvas.drawCircle(const ui.Offset(-25, 0), 50, paint);
+        canvas.drawCircle(const ui.Offset(25, 0), 50, paint);
       }));
 
       await renderScene(sceneBuilder.build());
-      await matchGoldenFile('scene_builder_opacity_circles_on_square.png', region: region);
+      await matchGoldenFile('scene_builder_opacity_circles_on_square.png',
+          region: region);
     });
 
     test('shader mask layer', () async {
@@ -148,48 +136,39 @@ Future<void> testMain() async {
 
       sceneBuilder.addPicture(ui.Offset.zero, drawPicture((ui.Canvas canvas) {
         final ui.Paint paint = ui.Paint()..color = const ui.Color(0xFFFF0000);
-        canvas.drawCircle(
-          const ui.Offset(125, 150),
-          50,
-          paint
-        );
-        canvas.drawCircle(
-          const ui.Offset(175, 150),
-          50,
-          paint
-        );
+        canvas.drawCircle(const ui.Offset(125, 150), 50, paint);
+        canvas.drawCircle(const ui.Offset(175, 150), 50, paint);
       }));
 
       final ui.Shader shader = ui.Gradient.linear(
-        ui.Offset.zero,
-        const ui.Offset(50, 50), <ui.Color>[
-          const ui.Color(0xFFFFFFFF),
-          const ui.Color(0x00000000),
-        ]);
-      sceneBuilder.pushShaderMask(
-        shader,
-        const ui.Rect.fromLTRB(125, 125, 175, 175),
-        ui.BlendMode.srcATop
-      );
+          ui.Offset.zero, const ui.Offset(50, 50), <ui.Color>[
+        const ui.Color(0xFFFFFFFF),
+        const ui.Color(0x00000000),
+      ]);
+      sceneBuilder.pushShaderMask(shader,
+          const ui.Rect.fromLTRB(125, 125, 175, 175), ui.BlendMode.srcATop);
 
       sceneBuilder.addPicture(ui.Offset.zero, drawPicture((ui.Canvas canvas) {
         canvas.drawRect(
-          ui.Rect.fromCircle(center: const ui.Offset(150, 150), radius: 50),
-          ui.Paint()..color = const ui.Color(0xFF00FF00)
-        );
+            ui.Rect.fromCircle(center: const ui.Offset(150, 150), radius: 50),
+            ui.Paint()..color = const ui.Color(0xFF00FF00));
       }));
 
       await renderScene(sceneBuilder.build());
       await matchGoldenFile('scene_builder_shader_mask.png', region: region);
-    }, skip: isFirefox && isHtml); // https://github.com/flutter/flutter/issues/86623
+    },
+        skip: isFirefox &&
+            isHtml); // https://github.com/flutter/flutter/issues/86623
 
     test('backdrop filter layer', () async {
       final ui.SceneBuilder sceneBuilder = ui.SceneBuilder();
 
       sceneBuilder.addPicture(ui.Offset.zero, drawPicture((ui.Canvas canvas) {
         // Create a red and blue checkerboard pattern
-        final ui.Paint redPaint = ui.Paint()..color = const ui.Color(0xFFFF0000);
-        final ui.Paint bluePaint = ui.Paint()..color = const ui.Color(0xFF0000FF);
+        final ui.Paint redPaint = ui.Paint()
+          ..color = const ui.Color(0xFFFF0000);
+        final ui.Paint bluePaint = ui.Paint()
+          ..color = const ui.Color(0xFF0000FF);
         for (double y = 0; y < 300; y += 10) {
           for (double x = 0; x < 300; x += 10) {
             final ui.Paint paint = ((x + y) % 20 == 0) ? redPaint : bluePaint;
@@ -204,15 +183,13 @@ Future<void> testMain() async {
       ));
 
       sceneBuilder.addPicture(ui.Offset.zero, drawPicture((ui.Canvas canvas) {
-        canvas.drawCircle(
-          const ui.Offset(150, 150),
-          50,
-          ui.Paint()..color = const ui.Color(0xFF00FF00)
-        );
+        canvas.drawCircle(const ui.Offset(150, 150), 50,
+            ui.Paint()..color = const ui.Color(0xFF00FF00));
       }));
 
       await renderScene(sceneBuilder.build());
-      await matchGoldenFile('scene_builder_backdrop_filter.png', region: region);
+      await matchGoldenFile('scene_builder_backdrop_filter.png',
+          region: region);
     });
 
     test('empty backdrop filter layer with clip', () async {
@@ -223,8 +200,10 @@ Future<void> testMain() async {
 
       sceneBuilder.addPicture(ui.Offset.zero, drawPicture((ui.Canvas canvas) {
         // Create a red and blue checkerboard pattern
-        final ui.Paint redPaint = ui.Paint()..color = const ui.Color(0xFFFF0000);
-        final ui.Paint bluePaint = ui.Paint()..color = const ui.Color(0xFF0000FF);
+        final ui.Paint redPaint = ui.Paint()
+          ..color = const ui.Color(0xFFFF0000);
+        final ui.Paint bluePaint = ui.Paint()
+          ..color = const ui.Color(0xFF0000FF);
         for (double y = 0; y < 300; y += 10) {
           for (double x = 0; x < 300; x += 10) {
             final ui.Paint paint = ((x + y) % 20 == 0) ? redPaint : bluePaint;
@@ -240,7 +219,8 @@ Future<void> testMain() async {
         sigmaY: 3.0,
       ));
       await renderScene(sceneBuilder.build());
-      await matchGoldenFile('scene_builder_empty_backdrop_filter_with_clip.png', region: region);
+      await matchGoldenFile('scene_builder_empty_backdrop_filter_with_clip.png',
+          region: region);
     });
 
     test('image filter layer', () async {
@@ -251,11 +231,8 @@ Future<void> testMain() async {
       ));
 
       sceneBuilder.addPicture(ui.Offset.zero, drawPicture((ui.Canvas canvas) {
-        canvas.drawCircle(
-          const ui.Offset(150, 150),
-          50,
-          ui.Paint()..color = const ui.Color(0xFF00FF00)
-        );
+        canvas.drawCircle(const ui.Offset(150, 150), 50,
+            ui.Paint()..color = const ui.Color(0xFF00FF00));
       }));
 
       await renderScene(sceneBuilder.build());
@@ -290,23 +267,110 @@ Future<void> testMain() async {
     test('color filter layer', () async {
       final ui.SceneBuilder sceneBuilder = ui.SceneBuilder();
       const ui.ColorFilter sepia = ui.ColorFilter.matrix(<double>[
-        0.393, 0.769, 0.189, 0, 0,
-        0.349, 0.686, 0.168, 0, 0,
-        0.272, 0.534, 0.131, 0, 0,
-        0,     0,     0,     1, 0,
+        0.393,
+        0.769,
+        0.189,
+        0,
+        0,
+        0.349,
+        0.686,
+        0.168,
+        0,
+        0,
+        0.272,
+        0.534,
+        0.131,
+        0,
+        0,
+        0,
+        0,
+        0,
+        1,
+        0,
       ]);
       sceneBuilder.pushColorFilter(sepia);
 
       sceneBuilder.addPicture(ui.Offset.zero, drawPicture((ui.Canvas canvas) {
-        canvas.drawCircle(
-          const ui.Offset(150, 150),
-          50,
-          ui.Paint()..color = const ui.Color(0xFF00FF00)
-        );
+        canvas.drawCircle(const ui.Offset(150, 150), 50,
+            ui.Paint()..color = const ui.Color(0xFF00FF00));
       }));
 
       await renderScene(sceneBuilder.build());
       await matchGoldenFile('scene_builder_color_filter.png', region: region);
+    });
+
+    test('overlapping pictures in opacity layer', () async {
+      final ui.SceneBuilder sceneBuilder = ui.SceneBuilder();
+      sceneBuilder.pushOpacity(128);
+      sceneBuilder.addPicture(ui.Offset.zero, drawPicture((ui.Canvas canvas) {
+        canvas.drawCircle(const ui.Offset(100, 150), 100,
+            ui.Paint()..color = const ui.Color(0xFFFF0000));
+      }));
+      sceneBuilder.addPicture(ui.Offset.zero, drawPicture((ui.Canvas canvas) {
+        canvas.drawCircle(const ui.Offset(200, 150), 100,
+            ui.Paint()..color = const ui.Color(0xFFFF0000));
+      }));
+      sceneBuilder.pop();
+
+      await renderScene(sceneBuilder.build());
+      await matchGoldenFile('scene_builder_overlapping_pictures_in_opacity.png',
+          region: region);
+    });
+
+    test('picture clipped out in final scene', () async {
+      final ui.SceneBuilder sceneBuilder = ui.SceneBuilder();
+      sceneBuilder.pushClipRect(const ui.Rect.fromLTRB(0, 0, 125, 300));
+      sceneBuilder.addPicture(ui.Offset.zero, drawPicture((ui.Canvas canvas) {
+        canvas.drawCircle(const ui.Offset(50, 150), 50,
+            ui.Paint()..color = const ui.Color(0xFFFF0000));
+      }));
+      sceneBuilder.addPicture(ui.Offset.zero, drawPicture((ui.Canvas canvas) {
+        canvas.drawCircle(const ui.Offset(200, 150), 50,
+            ui.Paint()..color = const ui.Color(0xFFFF0000));
+      }));
+      sceneBuilder.pop();
+
+      await renderScene(sceneBuilder.build());
+      await matchGoldenFile('scene_builder_picture_clipped_out.png',
+          region: region);
+    });
+
+    test('picture clipped but scrolls back in', () async {
+      // Frame 1: Clip out the right circle
+      final ui.SceneBuilder sceneBuilder = ui.SceneBuilder();
+      sceneBuilder.pushClipRect(const ui.Rect.fromLTRB(0, 0, 125, 300));
+      // Save this offsetLayer to add back in so we are using the same
+      // picture layers on the next scene.
+      final ui.OffsetEngineLayer offsetLayer = sceneBuilder.pushOffset(0, 0);
+      sceneBuilder.addPicture(ui.Offset.zero, drawPicture((ui.Canvas canvas) {
+        canvas.drawCircle(const ui.Offset(50, 150), 50,
+            ui.Paint()..color = const ui.Color(0xFFFF0000));
+      }));
+      sceneBuilder.addPicture(ui.Offset.zero, drawPicture((ui.Canvas canvas) {
+        canvas.drawCircle(const ui.Offset(200, 150), 50,
+            ui.Paint()..color = const ui.Color(0xFFFF0000));
+      }));
+      sceneBuilder.pop();
+      sceneBuilder.pop();
+      await renderScene(sceneBuilder.build());
+
+      // Frame 2: Clip out the left circle
+      final ui.SceneBuilder sceneBuilder2 = ui.SceneBuilder();
+      sceneBuilder2.pushClipRect(const ui.Rect.fromLTRB(150, 0, 300, 300));
+      sceneBuilder2.addRetained(offsetLayer);
+      sceneBuilder2.pop();
+      await renderScene(sceneBuilder2.build());
+
+      // Frame 3: Clip out the right circle again
+      final ui.SceneBuilder sceneBuilder3 = ui.SceneBuilder();
+      sceneBuilder3.pushClipRect(const ui.Rect.fromLTRB(0, 0, 125, 300));
+      sceneBuilder3.addRetained(offsetLayer);
+      sceneBuilder3.pop();
+      await renderScene(sceneBuilder3.build());
+
+      await matchGoldenFile(
+          'scene_builder_picture_clipped_out_then_clipped_in.png',
+          region: region);
     });
   });
 }
