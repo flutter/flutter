62c0e50682e31c03d6f6fc77f1c254b84723d8f3
Jonah Williams <jonahwilliams@google.com>
2024-09-25 15:06:37 -0700

    [engine] set platform thread name to ui. (#55362)
    When running with merged platform and ui threads, set the dart thread name of the main thread to io.futter.ui. Also change the thread mask settings to avoid creating an unused UI thread.

--- shell/platform/android/android_shell_holder.cc
+++ shell/platform/android/android_shell_holder.cc
@@ -89,17 +89,17 @@ AndroidShellHolder::AndroidShellHolder(
   static size_t thread_host_count = 1;
   auto thread_label = std::to_string(thread_host_count++);
 
-  auto mask =
-      ThreadHost::Type::kUi | ThreadHost::Type::kRaster | ThreadHost::Type::kIo;
+  auto mask = ThreadHost::Type::kRaster | ThreadHost::Type::kIo;
+  if (!settings.merged_platform_ui_thread) {
+    mask |= ThreadHost::Type::kUi;
+  }
 
   flutter::ThreadHost::ThreadHostConfig host_config(
       thread_label, mask, AndroidPlatformThreadConfigSetter);
-  if (!settings.merged_platform_ui_thread) {
-    host_config.ui_config = fml::Thread::ThreadConfig(
-        flutter::ThreadHost::ThreadHostConfig::MakeThreadName(
-            flutter::ThreadHost::Type::kUi, thread_label),
-        fml::Thread::ThreadPriority::kDisplay);
-  }
+  host_config.ui_config = fml::Thread::ThreadConfig(
+      flutter::ThreadHost::ThreadHostConfig::MakeThreadName(
+          flutter::ThreadHost::Type::kUi, thread_label),
+      fml::Thread::ThreadPriority::kDisplay);
   host_config.raster_config = fml::Thread::ThreadConfig(
       flutter::ThreadHost::ThreadHostConfig::MakeThreadName(
           flutter::ThreadHost::Type::kRaster, thread_label),
