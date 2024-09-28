656362e419a10bc3c6405e3039e195bd5d28f9fd
Matan Lurey <matanlurey@users.noreply.github.com>
2024-09-27 17:27:20 -0700

    Migrate `try/finally` calls to `addTearDown`. (flutter/engine#55499)
    I also removed other no-longer-enforced cruft. No behavioral changes.

--- engine/src/flutter/tools/engine_tool/test/test_command_test.dart
+++ engine/src/flutter/tools/engine_tool/test/test_command_test.dart
@@ -6,7 +6,6 @@ import 'dart:convert' as convert;
 
 import 'package:engine_build_configs/engine_build_configs.dart';
 import 'package:engine_tool/src/commands/command_runner.dart';
-import 'package:engine_tool/src/environment.dart';
 import 'package:platform/platform.dart';
 import 'package:test/test.dart';
 
@@ -14,38 +13,38 @@ import 'fixtures.dart' as fixtures;
 import 'utils.dart';
 
 void main() {
-  final BuilderConfig linuxTestConfig = BuilderConfig.fromJson(
+  final linuxTestConfig = BuilderConfig.fromJson(
     path: 'ci/builders/linux_test_config.json',
     map: convert.jsonDecode(fixtures.testConfig('Linux', Platform.linux))
         as Map<String, Object?>,
   );
 
-  final BuilderConfig macTestConfig = BuilderConfig.fromJson(
+  final macTestConfig = BuilderConfig.fromJson(
     path: 'ci/builders/mac_test_config.json',
     map: convert.jsonDecode(fixtures.testConfig('Mac-12', Platform.macOS))
         as Map<String, Object?>,
   );
 
-  final BuilderConfig winTestConfig = BuilderConfig.fromJson(
+  final winTestConfig = BuilderConfig.fromJson(
     path: 'ci/builders/win_test_config.json',
     map: convert.jsonDecode(fixtures.testConfig('Windows-11', Platform.windows))
         as Map<String, Object?>,
   );
 
-  final Map<String, BuilderConfig> configs = <String, BuilderConfig>{
+  final configs = <String, BuilderConfig>{
     'linux_test_config': linuxTestConfig,
     'mac_test_config': macTestConfig,
     'win_test_config': winTestConfig,
   };
 
   test('test command executes test', () async {
-    final TestEnvironment testEnvironment = TestEnvironment.withTestEngine(
+    final testEnvironment = TestEnvironment.withTestEngine(
       // This test needs specific instrumentation. Ideally all tests should
       // use per-test environments and not rely on global state, but that is a
       // larger change (https://github.com/flutter/flutter/issues/148420).
-      cannedProcesses: <CannedProcess>[
+      cannedProcesses: [
         CannedProcess(
-          (List<String> command) => command.contains('desc'),
+          (command) => command.contains('desc'),
           stdout: '''
             {
               "//flutter/fml:display_list_unittests": {
@@ -58,34 +57,33 @@ void main() {
         ),
       ],
     );
-    try {
-      final Environment env = testEnvironment.environment;
-      final ToolCommandRunner runner = ToolCommandRunner(
-        environment: env,
-        configs: configs,
-      );
-      final int result = await runner.run(<String>[
-        'test',
-        '//flutter/display_list:display_list_unittests',
-      ]);
-      expect(result, equals(0));
-      expect(testEnvironment.processHistory.length, greaterThan(3));
-      final int offset = testEnvironment.processHistory.length - 1;
-      expect(testEnvironment.processHistory[offset].command[0],
-          endsWith('display_list_unittests'));
-    } finally {
-      testEnvironment.cleanup();
-    }
+    addTearDown(testEnvironment.cleanup);
+
+    final runner = ToolCommandRunner(
+      environment: testEnvironment.environment,
+      configs: configs,
+    );
+    final result = await runner.run(<String>[
+      'test',
+      '//flutter/display_list:display_list_unittests',
+    ]);
+    expect(result, equals(0));
+    expect(testEnvironment.processHistory.length, greaterThan(3));
+    final offset = testEnvironment.processHistory.length - 1;
+    expect(
+      testEnvironment.processHistory[offset].command[0],
+      endsWith('display_list_unittests'),
+    );
   });
 
   test('test command skips non-testonly executables', () async {
-    final TestEnvironment testEnvironment = TestEnvironment.withTestEngine(
+    final testEnvironment = TestEnvironment.withTestEngine(
       // This test needs specific instrumentation. Ideally all tests should
       // use per-test environments and not rely on global state, but that is a
       // larger change (https://github.com/flutter/flutter/issues/148420).
-      cannedProcesses: <CannedProcess>[
+      cannedProcesses: [
         CannedProcess(
-          (List<String> command) => command.contains('desc'),
+          (command) => command.contains('desc'),
           stdout: '''
             {
               "//flutter/fml:display_list_unittests": {
@@ -103,22 +101,20 @@ void main() {
         ),
       ],
     );
-    try {
-      final Environment env = testEnvironment.environment;
-      final ToolCommandRunner runner = ToolCommandRunner(
-        environment: env,
-        configs: configs,
-      );
-      final int result = await runner.run(<String>[
-        'test',
-        '//...',
-      ]);
-      expect(result, equals(0));
-      expect(testEnvironment.processHistory.where((ExecutedProcess process) {
-        return process.command[0].contains('protoc');
-      }), isEmpty);
-    } finally {
-      testEnvironment.cleanup();
-    }
+    addTearDown(testEnvironment.cleanup);
+
+    final env = testEnvironment.environment;
+    final runner = ToolCommandRunner(
+      environment: env,
+      configs: configs,
+    );
+    final result = await runner.run(<String>[
+      'test',
+      '//...',
+    ]);
+    expect(result, equals(0));
+    expect(testEnvironment.processHistory.where((ExecutedProcess process) {
+      return process.command[0].contains('protoc');
+    }), isEmpty);
   });
 }
--- engine/src/flutter/tools/engine_tool/test/utils_test.dart
+++ engine/src/flutter/tools/engine_tool/test/utils_test.dart
@@ -7,61 +7,69 @@ import 'package:test/test.dart';
 import 'utils.dart';
 
 void main() async {
-  final List<CannedProcess> cannedProcesses = <CannedProcess>[
-    CannedProcess((List<String> command) => command.contains('ulfuls'),
-        stdout: 'Ashita ga aru sa'),
-    CannedProcess((List<String> command) => command.contains('quruli'),
-        stdout: 'Tokyo'),
-    CannedProcess((List<String> command) => command.contains('elizaveta'),
-        stdout: 'Moshimo ano toki'),
-    CannedProcess((List<String> command) => command.contains('scott_murphy'),
-        stdout: 'Donna toki mo'),
+  final cannedProcesses = <CannedProcess>[
+    CannedProcess(
+      (command) => command.contains('ulfuls'),
+      stdout: 'Ashita ga aru sa',
+    ),
+    CannedProcess(
+      (command) => command.contains('quruli'),
+      stdout: 'Tokyo',
+    ),
+    CannedProcess(
+      (command) => command.contains('elizaveta'),
+      stdout: 'Moshimo ano toki',
+    ),
+    CannedProcess(
+      (command) => command.contains('scott_murphy'),
+      stdout: 'Donna toki mo',
+    ),
   ];
 
   test('containsCommand passes if command matched', () async {
-    final TestEnvironment testEnvironment = TestEnvironment.withTestEngine(
+    final testEnvironment = TestEnvironment.withTestEngine(
       cannedProcesses: cannedProcesses,
     );
-    try {
-      await testEnvironment.environment.processRunner.runProcess(
-          <String>['ulfuls', '--lyrics'],
-          workingDirectory: testEnvironment.environment.engine.srcDir,
-          failOk: true);
-      await testEnvironment.environment.processRunner.runProcess(
-          <String>['quruli', '--lyrics'],
-          workingDirectory: testEnvironment.environment.engine.srcDir,
-          failOk: true);
-      final List<ExecutedProcess> history = testEnvironment.processHistory;
-      expect(history, containsCommand((List<String> command) {
-        return command.isNotEmpty && command[0] == 'quruli';
-      }));
-      expect(history, containsCommand((List<String> command) {
-        return command.length > 1 && command[1] == '--lyrics';
-      }));
-    } finally {
-      testEnvironment.cleanup();
-    }
+    addTearDown(testEnvironment.cleanup);
+
+    await testEnvironment.environment.processRunner.runProcess(
+      ['ulfuls', '--lyrics'],
+      workingDirectory: testEnvironment.environment.engine.srcDir,
+      failOk: true,
+    );
+    await testEnvironment.environment.processRunner.runProcess(
+      ['quruli', '--lyrics'],
+      workingDirectory: testEnvironment.environment.engine.srcDir,
+      failOk: true,
+    );
+    final history = testEnvironment.processHistory;
+    expect(history, containsCommand((command) {
+      return command.isNotEmpty && command[0] == 'quruli';
+    }));
+    expect(history, containsCommand((command) {
+      return command.length > 1 && command[1] == '--lyrics';
+    }));
   });
 
   test('doesNotContainCommand passes if command not matched', () async {
-    final TestEnvironment testEnvironment = TestEnvironment.withTestEngine(
+    final testEnvironment = TestEnvironment.withTestEngine(
       cannedProcesses: cannedProcesses,
     );
-    try {
-      await testEnvironment.environment.processRunner.runProcess(
-          <String>['elizaveta', '--lyrics'],
-          workingDirectory: testEnvironment.environment.engine.srcDir,
-          failOk: true);
-      await testEnvironment.environment.processRunner.runProcess(
-          <String>['scott_murphy', '--lyrics'],
-          workingDirectory: testEnvironment.environment.engine.srcDir,
-          failOk: true);
-      final List<ExecutedProcess> history = testEnvironment.processHistory;
-      expect(history, doesNotContainCommand((List<String> command) {
-        return command.length > 1 && command[1] == '--not-an-option';
-      }));
-    } finally {
-      testEnvironment.cleanup();
-    }
+    addTearDown(testEnvironment.cleanup);
+
+    await testEnvironment.environment.processRunner.runProcess(
+      ['elizaveta', '--lyrics'],
+      workingDirectory: testEnvironment.environment.engine.srcDir,
+      failOk: true,
+    );
+    await testEnvironment.environment.processRunner.runProcess(
+      ['scott_murphy', '--lyrics'],
+      workingDirectory: testEnvironment.environment.engine.srcDir,
+      failOk: true,
+    );
+    final history = testEnvironment.processHistory;
+    expect(history, doesNotContainCommand((command) {
+      return command.length > 1 && command[1] == '--not-an-option';
+    }));
   });
 }
