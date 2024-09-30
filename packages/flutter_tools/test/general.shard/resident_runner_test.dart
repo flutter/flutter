98409e7770b135d0c0d7c7823ba1e1fa378b40ac
Andrew Kolos <andrewrkolos@gmail.com>
2024-09-30 10:53:08 -0700

    when `ResidentRunner.tryInitLogReader` fails, only log warning on Android (#155800)

--- packages/flutter_tools/test/general.shard/resident_runner_test.dart
+++ packages/flutter_tools/test/general.shard/resident_runner_test.dart
@@ -2024,6 +2024,50 @@ flutter:
     }) async => FakeVmServiceHost(requests: <VmServiceExpectation>[]).vmService,
   }));
 
+  testUsingContext('FlutterDevice does not throw when unable to initiate log reader due to VM service disconnection', () async {
+    fakeVmServiceHost = FakeVmServiceHost(
+      requests: <VmServiceExpectation>[
+        const FakeVmServiceRequest(
+          method: 'getVM',
+          error: FakeRPCError(
+            code: RPCErrorCodes.kServerError,
+            error: 'Service connection disposed',
+          ),
+        ),
+      ],
+    );
+    final TestFlutterDevice flutterDevice = TestFlutterDevice(device);
+    flutterDevice.vmService = fakeVmServiceHost!.vmService;
+    await flutterDevice.tryInitLogReader();
+
+    final BufferLogger logger = globals.logger as BufferLogger;
+    expect(
+      logger.traceText,
+      contains(
+        'VmService.getVm call failed: getVM: (-32000) '
+        'Service connection disposed\n',
+      ),
+    );
+    // We should not print a warning since the device does not have a connected
+    // adb log reader.
+    // TODO(andrewkolos): This test is a bit fragile, and is something that
+    //  should be corrected in a follow-up PR (see
+    //  https://github.com/flutter/flutter/issues/155795).
+    expect(logger.errorText, isEmpty);
+  }, overrides: <Type, Generator>{
+    VMServiceConnector: () => (Uri httpUri, {
+      ReloadSources? reloadSources,
+      Restart? restart,
+      CompileExpression? compileExpression,
+      GetSkSLMethod? getSkSLMethod,
+      FlutterProject? flutterProject,
+      PrintStructuredErrorLogMethod? printStructuredErrorLogMethod,
+      io.CompressionOptions? compression,
+      Device? device,
+      required Logger logger,
+    }) async => FakeVmServiceHost(requests: <VmServiceExpectation>[]).vmService,
+  });
+
   testUsingContext('Uses existing DDS URI from exception field', () => testbed.run(() async {
     fakeVmServiceHost = FakeVmServiceHost(requests: <VmServiceExpectation>[]);
     final FakeDevice device = FakeDevice()
