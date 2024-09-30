c7943c5f8f28b54d03c1552090eefe3c2bc42760
Renzo Olivares <rmolivares@renzo-olivares.dev>
2024-09-30 15:44:20 -0700

    Move platform specific text selection behavior out of styled TextField classes (#155774)
    Fixes #129726

--- packages/flutter/lib/src/material/text_field.dart
+++ packages/flutter/lib/src/material/text_field.dart
@@ -59,41 +59,11 @@ class _TextFieldSelectionGestureDetectorBuilder extends TextSelectionGestureDete
   final _TextFieldState _state;
 
   @override
-  void onForcePressStart(ForcePressDetails details) {
-    super.onForcePressStart(details);
-    if (delegate.selectionEnabled && shouldShowSelectionToolbar) {
-      editableText.showToolbar();
-    }
-  }
-
-  @override
-  void onForcePressEnd(ForcePressDetails details) {
-    // Not required.
-  }
-
-  @override
   bool get onUserTapAlwaysCalled => _state.widget.onTapAlwaysCalled;
 
   @override
   void onUserTap() {
     _state.widget.onTap?.call();
-  }
-
-  @override
-  void onSingleLongTapStart(LongPressStartDetails details) {
-    super.onSingleLongTapStart(details);
-    if (delegate.selectionEnabled) {
-      switch (Theme.of(_state.context).platform) {
-        case TargetPlatform.iOS:
-        case TargetPlatform.macOS:
-          break;
-        case TargetPlatform.android:
-        case TargetPlatform.fuchsia:
-        case TargetPlatform.linux:
-        case TargetPlatform.windows:
-          Feedback.forLongPress(_state.context);
-      }
-    }
   }
 }
 
