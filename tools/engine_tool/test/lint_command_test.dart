b0e094b82e4098fe12be1d857ca26f66f93a04c5
Matan Lurey <matanlurey@users.noreply.github.com>
2024-10-01 09:29:51 -0700

    Migrate off a global test fixture for `build` and `lint`. (#55540)
    Partial work towards https://github.com/flutter/flutter/issues/148420, unblocks https://github.com/flutter/engine/pull/55537.
    
    These tests can now be more precise, and changing the fixtures no longer has side-effects on tests across the entire repository. There are about 11 other usages (there were ~50 in these) after this PR that I'll get to, as well as the hard-coded `gn desc` output, before retiring `fixtures.dart`

--- tools/engine_tool/test/lint_command_test.dart
+++ tools/engine_tool/test/lint_command_test.dart
@@ -2,11 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'dart:convert' as convert;
 import 'dart:ffi' as ffi show Abi;
 import 'dart:io' as io;
 
-import 'package:engine_build_configs/engine_build_configs.dart';
 import 'package:engine_repo_tools/engine_repo_tools.dart';
 import 'package:engine_tool/src/commands/command_runner.dart';
 import 'package:engine_tool/src/environment.dart';
@@ -16,56 +14,25 @@ import 'package:process_fakes/process_fakes.dart';
 import 'package:process_runner/process_runner.dart';
 import 'package:test/test.dart';
 
-import 'fixtures.dart' as fixtures;
-
 void main() {
-  final Engine engine;
-  try {
-    engine = Engine.findWithin();
-  } catch (e) {
-    io.stderr.writeln(e);
-    io.exitCode = 1;
-    return;
-  }
-  final BuilderConfig linuxTestConfig = BuilderConfig.fromJson(
-    path: 'ci/builders/linux_test_config.json',
-    map: convert.jsonDecode(fixtures.testConfig('Linux', Platform.linux))
-        as Map<String, Object?>,
-  );
-
-  final BuilderConfig macTestConfig = BuilderConfig.fromJson(
-    path: 'ci/builders/mac_test_config.json',
-    map: convert.jsonDecode(fixtures.testConfig('Mac-12', Platform.macOS))
-        as Map<String, Object?>,
-  );
-
-  final BuilderConfig winTestConfig = BuilderConfig.fromJson(
-    path: 'ci/builders/win_test_config.json',
-    map: convert.jsonDecode(fixtures.testConfig('Windows-11', Platform.windows))
-        as Map<String, Object?>,
-  );
-
-  final Map<String, BuilderConfig> configs = <String, BuilderConfig>{
-    'linux_test_config': linuxTestConfig,
-    'mac_test_config': macTestConfig,
-    'win_test_config': winTestConfig,
-  };
+  final engine = Engine.findWithin();
 
   (Environment, List<List<String>>) macEnv(Logger logger) {
-    final List<List<String>> runHistory = <List<String>>[];
+    final runHistory = <List<String>>[];
     return (
       Environment(
         abi: ffi.Abi.macosArm64,
         engine: engine,
         platform: FakePlatform(
-            operatingSystem: Platform.macOS,
-            resolvedExecutable: io.Platform.resolvedExecutable,
-            pathSeparator: '/'),
+          operatingSystem: Platform.macOS,
+          resolvedExecutable: io.Platform.resolvedExecutable,
+          pathSeparator: '/',
+        ),
         processRunner: ProcessRunner(
-            processManager: FakeProcessManager(onStart: (List<String> command) {
+            processManager: FakeProcessManager(onStart: (command) {
           runHistory.add(command);
           return FakeProcess();
-        }, onRun: (List<String> command) {
+        }, onRun: (command) {
           // Should not be executed.
           assert(false);
           return io.ProcessResult(81, 1, '', '');
@@ -81,7 +48,7 @@ void main() {
     final (Environment env, List<List<String>> runHistory) = macEnv(logger);
     final ToolCommandRunner runner = ToolCommandRunner(
       environment: env,
-      configs: configs,
+      configs: {},
     );
     final int result = await runner.run(<String>['lint']);
     expect(result, equals(0));
