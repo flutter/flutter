fede7401210fdf7da15c61d81cae00367980fe99
Nate Wilson <nate.w5687@gmail.com>
2024-10-02 14:39:17 -0600

    update flutter_tools/lib/src/build_system/targets/assets.dart
    Co-authored-by: andrewkolos <andrewrkolos@gmail.com>

--- packages/flutter_tools/lib/src/build_system/targets/assets.dart
+++ packages/flutter_tools/lib/src/build_system/targets/assets.dart
@@ -302,14 +302,14 @@ DevFSContent? processSkSLBundle(String? bundlePath, {
     throw Exception('SkSL bundle was invalid');
   }
 
-  final TargetPlatform? bundleTargetPlatform = switch (bundle) {
-    {'platform': final String platform} => getTargetPlatformForName(platform),
-    _ => null,
+  final TargetPlatform? bundlePlatform = switch (bundle['platform'] as String?) {
+    final String platform => getTargetPlatformForName(platform),
+    null => null,
   };
 
-  if (bundleTargetPlatform != targetPlatform) {
+  if (bundlePlatform != targetPlatform) {
     logger.printError(
-      'The SkSL bundle was created for $bundleTargetPlatform, but the current '
+      'The SkSL bundle was created for $bundlePlatform, but the current '
       'platform is $targetPlatform. This may lead to less efficient shader '
       'caching.'
     );
