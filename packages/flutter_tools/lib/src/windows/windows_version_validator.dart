e5f21b4897216b19af913ac7774dc06ad6c0cb77
rekire <git@reki.re>
2024-10-03 11:50:29 +0200

    Migrate to wmic and take other information directly from registry

--- packages/flutter_tools/lib/src/windows/windows_version_validator.dart
+++ packages/flutter_tools/lib/src/windows/windows_version_validator.dart
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'dart:ffi';
+import 'package:ffi/ffi.dart';
 import 'package:process/process.dart';
 
 import '../base/io.dart';
@@ -77,18 +79,17 @@ class WindowsVersionValidator extends DoctorValidator {
     if (matches.length == 1 &&
         !kUnsupportedVersions.contains(matches.elementAt(0).group(1))) {
       windowsVersionStatus = ValidationType.success;
-      final Map<String, String> details = await _versionExtractor.getDetails();
-      if (details.isEmpty) {
-        final bool isWindows10 = int.parse(matches.elementAt(0).group(3)!) > 20000;
-        if (isWindows10) {
-          statusInfo = 'Windows 10';
+      final Map<String, String?> details = await _versionExtractor.getDetails();
+      String? caption = details['Caption'];
+      if (caption == null || caption.isEmpty) {
+        final bool isWindows11 = int.parse(matches.elementAt(0).group(3)!) > 20000;
+        if (isWindows11) {
+          caption = 'Windows 11 or higher';
         } else {
-          statusInfo = 'Windows 11 or higher';
+          caption = 'Windows 10';
         }
-      } else {
-        statusInfo = '${details['OsName']} ${details['OSDisplayVersion']} '
-            '(${details['WindowsVersion']})';
       }
+      statusInfo = '$caption, ${details['DisplayVersion']}, ${details['ReleaseId']}';
 
       // Check if the Topaz OFD security module is running, and warn the user if it is.
       // See https://github.com/flutter/flutter/issues/121366
@@ -132,15 +133,115 @@ class VersionExtractor {
 
   final ProcessManager processManager;
 
-  Future<Map<String, String>> getDetails() async {
-    const String argument = 'Get-ComputerInfo -Property OsName, OSDisplayVersion, WindowsVersion | ConvertTo-Json';
-    final ProcessResult getProcessesResult = await processManager.run(
-        <String>['powershell', '-command', argument]);
-    if (getProcessesResult.exitCode != 0) {
-      return <String, String>{};
+  Future<Map<String, String?>> getDetails() async {
+    final ProcessResult getProcessesResult = await processManager.run(<String>['wmic', 'os', 'get', 'Caption']);
+
+    String? caption;
+    if (getProcessesResult.exitCode == 0) {
+      String? output = getProcessesResult.stdout as String?;
+      if (output != null) {
+        final List<String> parts = output.split('\n');
+        if (parts.length >= 2) {
+          caption = parts[1].replaceAll('Microsoft Windows', '').trim();
+        }
+      }
     }
-    final String json = getProcessesResult.stdout as String;
-    return (jsonDecode(json) as Map<String, dynamic>)
-        .map((String key, dynamic value) => MapEntry<String, String>(key, value.toString()));
+
+    return {
+      'Caption': caption,
+      'ReleaseId': readRegistryValue(HKEY_LOCAL_MACHINE, CURRENT_VERSION_KEY, 'ReleaseId'),
+      'DisplayVersion': readRegistryValue(HKEY_LOCAL_MACHINE, CURRENT_VERSION_KEY, 'DisplayVersion'),
+    };
   }
+}
+
+typedef RegOpenKeyExNative = Int32 Function(
+    IntPtr hKey,
+    Pointer<Utf16> lpSubKey,
+    Int32 ulOptions,
+    Int32 samDesired,
+    Pointer<IntPtr> phkResult);
+typedef RegOpenKeyExDart = int Function(
+    int hKey,
+    Pointer<Utf16> lpSubKey,
+    int ulOptions,
+    int samDesired,
+    Pointer<IntPtr> phkResult);
+typedef RegQueryValueExNative = Int32 Function(
+    IntPtr hKey,
+    Pointer<Utf16> lpValueName,
+    Pointer<Int32> lpReserved,
+    Pointer<Int32> lpType,
+    Pointer<Void> lpData,
+    Pointer<Int32> lpcbData);
+typedef RegQueryValueExDart = int Function(
+    int hKey,
+    Pointer<Utf16> lpValueName,
+    Pointer<Int32> lpReserved,
+    Pointer<Int32> lpType,
+    Pointer<Void> lpData,
+    Pointer<Int32> lpcbData);
+
+typedef RegCloseKeyNative = Int32 Function(IntPtr hKey);
+typedef RegCloseKeyDart = int Function(int hKey);
+
+base class KEY_VALUE_BASIC_INFORMATION extends Struct {
+  @Uint32()
+  external int TitleIndex;
+  @Uint32()
+  external int Type;
+  @Uint32()
+  external int NameLength;
+  @Uint32()
+  external int DataLength;
+}
+
+const int HKEY_LOCAL_MACHINE = 0x80000002;
+const int KEY_READ = 0x20019;
+const int REG_SZ = 1;
+const int REG_DWORD = 4;
+const String CURRENT_VERSION_KEY = r'SOFTWARE\Microsoft\Windows NT\CurrentVersion';
+
+String? readRegistryValue(int hKey, String subKey, String valueName) {
+  final advapi32 = DynamicLibrary.open('advapi32.dll');
+  final RegOpenKeyExDart RegOpenKeyEx = advapi32.lookupFunction<RegOpenKeyExNative, RegOpenKeyExDart>('RegOpenKeyExW');
+  final RegQueryValueExDart RegQueryValueEx = advapi32.lookupFunction<RegQueryValueExNative, RegQueryValueExDart>('RegQueryValueExW');
+  final RegCloseKeyDart RegCloseKey = advapi32.lookupFunction<RegCloseKeyNative, RegCloseKeyDart>('RegCloseKey');
+
+  final Pointer<Utf16> subKeyPtr = subKey.toNativeUtf16();
+  final Pointer<Utf16> valueNamePtr = valueName.toNativeUtf16();
+  final Pointer<IntPtr> hSubKeyPtr = calloc<IntPtr>();
+
+  final openResult = RegOpenKeyEx(HKEY_LOCAL_MACHINE, subKeyPtr, 0, KEY_READ, hSubKeyPtr);
+  if (openResult != 0) {
+    calloc.free(hSubKeyPtr);
+    return null;
+  }
+
+  final hSubKey = hSubKeyPtr.value;
+
+  final Pointer<Int32> dataSizePtr = calloc<Int32>();
+  final Pointer<Int32> typePtr = calloc<Int32>();
+  RegQueryValueEx(hSubKey, valueNamePtr, nullptr, typePtr, nullptr, dataSizePtr);
+  final dataSize = dataSizePtr.value;
+
+  final Pointer<Uint8> dataPtr = calloc<Uint8>(dataSize);
+  RegQueryValueEx(hSubKey, valueNamePtr, nullptr, typePtr, dataPtr.cast(), dataSizePtr);
+
+  String? result = null;
+  if (typePtr.value == REG_SZ) {
+    result = dataPtr.cast<Utf16>().toDartString(length: dataSize ~/ 2);
+  } else if (typePtr.value == REG_DWORD) {
+    result = dataPtr.cast<Uint32>().value.toString();
+  }
+
+  RegCloseKey(hSubKey);
+  calloc.free(subKeyPtr);
+  calloc.free(valueNamePtr);
+  calloc.free(hSubKeyPtr);
+  calloc.free(dataSizePtr);
+  calloc.free(typePtr);
+  calloc.free(dataPtr);
+
+  return result;
 }
