6ec5b07ef9e99919c9849c378c8e6eee4ede20b4
Jason Simmons <jason-simmons@users.noreply.github.com>
2024-10-03 08:28:47 -0700

    [Impeller] Fix handling of perspective matrices in Matrix::Translate (flutter/engine#55536)
    Fixes https://github.com/flutter/flutter/issues/155450

--- engine/src/flutter/impeller/geometry/matrix.h
+++ engine/src/flutter/impeller/geometry/matrix.h
@@ -245,7 +245,7 @@ struct Matrix {
                   m[0] * t.x + m[4] * t.y + m[8]  * t.z + m[12],
                   m[1] * t.x + m[5] * t.y + m[9]  * t.z + m[13],
                   m[2] * t.x + m[6] * t.y + m[10] * t.z + m[14],
-                  m[15]);
+                  m[3] * t.x + m[7] * t.y + m[11] * t.z + m[15]);
     // clang-format on
   }
 
