92a1627c5a44b38f1148843b99b416c1c3ac6c89
Harry Terkelsen <het@google.com>
2024-10-03 12:54:20 -0700

    wip

--- packages/flutter/lib/src/widgets/web_image.dart
+++ packages/flutter/lib/src/widgets/web_image.dart
@@ -2,7 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'basic.dart';
 import 'framework.dart';
 import 'image.dart';
 
@@ -16,109 +15,29 @@ import 'image.dart';
 /// If the [src] of the image is same-origin, or if we are able to make a
 /// cross-origin request for the image bytes, then this widget works exactly
 /// the same as [Image.network].
-class WebImage extends StatelessWidget {
+class WebImage extends StatefulWidget {
 
   /// Creates a web image.
   const WebImage(
     this.src, {
     super.key,
-    this.scale = 1.0,
-    this.frameBuilder,
-    this.loadingBuilder,
-    this.errorBuilder,
-    this.semanticLabel,
-    this.excludeFromSemantics = false,
-    this.width,
-    this.height,
-    this.color,
-    this.opacity,
-    this.colorBlendMode,
-    this.fit,
-    this.alignment = Alignment.center,
-    this.repeat = ImageRepeat.noRepeat,
-    this.centerSlice,
-    this.matchTextDirection = false,
-    this.gaplessPlayback = false,
-    this.filterQuality = FilterQuality.medium,
-    this.isAntiAlias = false,
-    this.headers,
-    this.cacheWidth,
-    this.cacheHeight,
-  })  : assert(cacheWidth == null || cacheWidth > 0),
-        assert(cacheHeight == null || cacheHeight > 0);
+  });
 
   final String src;
 
-  final double scale;
+  @override
+  State<WebImage> createState() => _WebImageState();
+}
 
-  final ImageFrameBuilder? frameBuilder;
-
-  final ImageLoadingBuilder? loadingBuilder;
-
-  final ImageErrorWidgetBuilder? errorBuilder;
-
-  final String? semanticLabel;
-
-  final bool excludeFromSemantics;
-
-  final double? width;
-
-  final double? height;
-
-  final Color? color;
-
-  final Animation<double>? opacity;
-
-  final BlendMode? colorBlendMode;
-
-  final BoxFit? fit;
-
-  final Alignment alignment;
-
-  final ImageRepeat repeat;
-
-  final Rect? centerSlice;
-
-  final bool matchTextDirection;
-
-  final bool gaplessPlayback;
-
-  final FilterQuality filterQuality;
-
-  final bool isAntiAlias;
-
-  final Map<String, String>? headers;
-
-  final int? cacheWidth;
-
-  final int? cacheHeight;
+class _WebImageState extends State<WebImage> {
+  @override
+  void initState() {
+    super.initState();
+    // Check if `widget.src` can be fetched.
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Image.network(src,
-      key: key,
-      scale: scale,
-      frameBuilder: frameBuilder,
-      loadingBuilder: loadingBuilder,
-      errorBuilder: errorBuilder,
-      semanticLabel: semanticLabel,
-      excludeFromSemantics: excludeFromSemantics,
-      width: width,
-      height: height,
-      color: color,
-      opacity: opacity,
-      colorBlendMode: colorBlendMode,
-      fit: fit,
-      alignment: alignment,
-      repeat: repeat,
-      centerSlice: centerSlice,
-      matchTextDirection: matchTextDirection,
-      gaplessPlayback: gaplessPlayback,
-      filterQuality: filterQuality,
-      isAntiAlias: isAntiAlias,
-      headers: headers,
-      cacheWidth: cacheWidth,
-      cacheHeight: cacheHeight,
-    );
+    return Image.network(widget.src);
   }
 }
