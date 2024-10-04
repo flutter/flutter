a59ea01da9e64bb5c0952dd2bb680f3594b7ee11
Matan Lurey <matanlurey@users.noreply.github.com>
2024-10-03 17:24:20 -0700

    Add missing license headers to `engine_tool`. (flutter/engine#55636)
    Address post-submit feedback on https://github.com/flutter/engine/pull/55537#pullrequestreview-2346684376.

--- engine/src/flutter/tools/engine_tool/test/src/matchers.dart
+++ engine/src/flutter/tools/engine_tool/test/src/matchers.dart
@@ -1,3 +1,7 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import 'package:engine_tool/src/logger.dart';
 import 'package:logging/logging.dart';
 import 'package:test/test.dart';
