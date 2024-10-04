186a0e66a2ffcbc3267480e9a553b2c89638153e
Gray Mackall <mackall@google.com>
2024-10-04 09:29:25 -0700

    change visible for testing to internal

--- packages/flutter_tools/gradle/kt/src/main/kotlin/com/flutter/gradle/BaseApplicationNameHandler.kt
+++ packages/flutter_tools/gradle/kt/src/main/kotlin/com/flutter/gradle/BaseApplicationNameHandler.kt
@@ -2,16 +2,13 @@ package com.flutter.gradle
 
 import com.android.build.api.dsl.ApplicationExtension
 import org.gradle.api.Project
-import org.jetbrains.annotations.VisibleForTesting
 
 // TODO(gmackall): maybe migrate this to a package-level function when FGP conversion is done.
 class BaseApplicationNameHandler {
     companion object {
-        @VisibleForTesting
-        const val DEFAULT_BASE_APPLICATION_NAME: String = "android.app.Application"
+        internal const val DEFAULT_BASE_APPLICATION_NAME: String = "android.app.Application"
 
-        @VisibleForTesting
-        const val GRADLE_BASE_APPLICATION_NAME_PROPERTY: String = "base-application-name"
+        internal const val GRADLE_BASE_APPLICATION_NAME_PROPERTY: String = "base-application-name"
 
         @JvmStatic fun setBaseName(project: Project) {
             val androidComponentsExtension: ApplicationExtension = project.extensions.getByType(ApplicationExtension::class.java)
