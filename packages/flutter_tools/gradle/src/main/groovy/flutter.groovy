90a4911804eb81baba7554f4d27a67b56862b8b2
Mohellebi abdessalem <mohellebiabdessalem@gmail.com>
2024-10-09 21:51:07 +0100

    Update flutter.groovy

--- packages/flutter_tools/gradle/src/main/groovy/flutter.groovy
+++ packages/flutter_tools/gradle/src/main/groovy/flutter.groovy
@@ -3,7 +3,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import com.android.build.api.variant.VariantOutputConfiguration
 import groovy.json.JsonGenerator
 import groovy.xml.QName
 import java.nio.file.Paths
