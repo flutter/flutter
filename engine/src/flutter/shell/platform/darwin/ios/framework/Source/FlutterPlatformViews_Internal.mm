bc8d80a83e5f487a5de9157e6417e660222d49ff
hellohuanlin <41930132+hellohuanlin@users.noreply.github.com>
2024-10-15 17:04:32 -0700

    Revert "[ios][platform_view] Fix Platform view gesture recognizer wit… (flutter/engine#55889)
    â¦h iPad pencil getting stuck (#55724)"
    
    This reverts commit 914da60e2d9f72d3d5d387d440faf2c645ac43c2.
    
    Revert since a customer reported the platform view touch is broken after the change. 
    
    *List which issues are fixed by this PR. You must list at least one issue.*
    Reopens https://github.com/flutter/engine/pull/55724
    
    *If you had to change anything in the [flutter/tests] repo, include a link to the migration guide as per the [breaking change policy].*
    
    [C++, Objective-C, Java style guides]: https://github.com/flutter/engine/blob/main/CONTRIBUTING.md#style

--- engine/src/flutter/shell/platform/darwin/ios/framework/Source/FlutterPlatformViews_Internal.mm
+++ engine/src/flutter/shell/platform/darwin/ios/framework/Source/FlutterPlatformViews_Internal.mm
@@ -654,14 +654,14 @@ static BOOL _preparedOnce = NO;
 
 - (BOOL)gestureRecognizer:(UIGestureRecognizer*)gestureRecognizer
     shouldBeRequiredToFailByGestureRecognizer:(UIGestureRecognizer*)otherGestureRecognizer {
-  // The forwarding gesture recognizer should always get all touch events, so it should not
-  // require other gesture recognizer to fail.
-  return otherGestureRecognizer != _forwardingRecognizer;
+  // The forwarding gesture recognizer should always get all touch events, so it should not be
+  // required to fail by any other gesture recognizer.
+  return otherGestureRecognizer != _forwardingRecognizer && otherGestureRecognizer != self;
 }
 
 - (BOOL)gestureRecognizer:(UIGestureRecognizer*)gestureRecognizer
     shouldRequireFailureOfGestureRecognizer:(UIGestureRecognizer*)otherGestureRecognizer {
-  return otherGestureRecognizer == _forwardingRecognizer;
+  return otherGestureRecognizer == self;
 }
 
 - (void)touchesBegan:(NSSet<UITouch*>*)touches withEvent:(UIEvent*)event {
