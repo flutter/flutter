3563e6b8a686fef1abc23d294c4ec86281f09f49
Emmanuel Bonke <55477248+Coder-Manuel@users.noreply.github.com>
2024-10-17 22:43:20 +0300

    add `dividerThickness` in `PaginatedDataTable` (#156442)
    Transitioned from `DataTable` to `PaginatedDataTable` and noted that the latter is missing the `dividerThickness`,  hence this PR adds `dividerThickness` to `PaginatedDataTable`.

--- packages/flutter/lib/src/material/paginated_data_table.dart
+++ packages/flutter/lib/src/material/paginated_data_table.dart
@@ -122,6 +122,7 @@ class PaginatedDataTable extends StatefulWidget {
     this.controller,
     this.primary,
     this.headingRowColor,
+    this.dividerThickness,
     this.showEmptyRows = true,
   }) : assert(actions == null || (header != null)),
        assert(columns.isNotEmpty),
@@ -132,6 +133,7 @@ class PaginatedDataTable extends StatefulWidget {
        dataRowMinHeight = dataRowHeight ?? dataRowMinHeight,
        dataRowMaxHeight = dataRowHeight ?? dataRowMaxHeight,
        assert(rowsPerPage > 0),
+       assert(dividerThickness == null || dividerThickness >= 0),
        assert(() {
          if (onRowsPerPageChanged != null) {
            assert(availableRowsPerPage.contains(rowsPerPage));
@@ -238,6 +240,13 @@ class PaginatedDataTable extends StatefulWidget {
 
   /// The index of the first row to display when the widget is first created.
   final int? initialFirstRowIndex;
+
+
+  /// {@macro flutter.material.dataTable.dividerThickness}
+  ///
+  /// If null, [DataTableThemeData.dividerThickness] is used. This value
+  /// defaults to 1.0.
+  final double? dividerThickness;
 
   /// Invoked when the user switches to another page.
   ///
@@ -610,6 +619,7 @@ class PaginatedDataTableState extends State<PaginatedDataTable> {
                     sortColumnIndex: widget.sortColumnIndex,
                     sortAscending: widget.sortAscending,
                     onSelectAll: widget.onSelectAll,
+                    dividerThickness: widget.dividerThickness,
                     // Make sure no decoration is set on the DataTable
                     // from the theme, as its already wrapped in a Card.
                     decoration: const BoxDecoration(),
