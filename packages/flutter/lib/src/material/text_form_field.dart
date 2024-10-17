be6a736bcbe3d3234bf9a601f6ed0fd75fa096fb
Bruno Leroux <leroux_bruno@yahoo.fr>
2024-10-17 13:11:47 +0200

    Add TextFormField.errorBuilder to display a custom error widget

--- packages/flutter/lib/src/material/text_form_field.dart
+++ packages/flutter/lib/src/material/text_form_field.dart
@@ -16,6 +16,14 @@ import 'theme.dart';
 
 export 'package:flutter/services.dart' show SmartDashesType, SmartQuotesType;
 
+/// Signature for a callback that builds an error widget.
+///
+/// See also:
+///
+/// [TextFormField.errorBuilder], which is of this type, and passes the result error
+/// given by [TextFormField.validator].
+typedef TextFormFieldErrorBuilder = Widget Function(String errorText);
+
 /// A [FormField] that contains a [TextField].
 ///
 /// This is a convenience widget that wraps a [TextField] widget in a
@@ -149,6 +157,7 @@ class TextFormField extends FormField<String> {
     ValueChanged<String>? onFieldSubmitted,
     super.onSaved,
     super.validator,
+    this.errorBuilder,
     List<TextInputFormatter>? inputFormatters,
     bool? enabled,
     bool? ignorePointers,
@@ -203,12 +212,15 @@ class TextFormField extends FormField<String> {
          autovalidateMode: autovalidateMode ?? AutovalidateMode.disabled,
          builder: (FormFieldState<String> field) {
            final _TextFormFieldState state = field as _TextFormFieldState;
-           final InputDecoration effectiveDecoration = (decoration ?? const InputDecoration())
+           InputDecoration effectiveDecoration = (decoration ?? const InputDecoration())
                .applyDefaults(Theme.of(field.context).inputDecorationTheme);
            void onChangedHandler(String value) {
              field.didChange(value);
              onChanged?.call(value);
            }
+           effectiveDecoration = errorBuilder != null
+             ? effectiveDecoration.copyWith(error: field.errorText != null ? errorBuilder(field.errorText!) : null)
+             : effectiveDecoration.copyWith(errorText: field.errorText);
            return UnmanagedRestorationScope(
              bucket: field.bucket,
              child: TextField(
@@ -216,7 +228,7 @@ class TextFormField extends FormField<String> {
                restorationId: restorationId,
                controller: state._effectiveController,
                focusNode: focusNode,
-               decoration: effectiveDecoration.copyWith(errorText: field.errorText),
+               decoration: effectiveDecoration,
                keyboardType: keyboardType,
                textInputAction: textInputAction,
                style: style,
@@ -297,6 +309,14 @@ class TextFormField extends FormField<String> {
   /// value: when they have inserted or deleted text or reset the form.
   /// {@endtemplate}
   final ValueChanged<String>? onChanged;
+
+  /// Function that returns the widget representing the error to display.
+  /// It is passed the form field validator error string as input.
+  /// The resulting widget is passed to [InputDecoration.error].
+  ///
+  /// If null, the validator error string is passed to
+  /// [InputDecoration.errorText].
+  final TextFormFieldErrorBuilder? errorBuilder;
 
   static Widget _defaultContextMenuBuilder(BuildContext context, EditableTextState editableTextState) {
     return AdaptiveTextSelectionToolbar.editableText(
