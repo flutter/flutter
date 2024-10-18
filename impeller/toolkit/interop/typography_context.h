bde72d889c202974ced91da8053ae9ed44b89caf
Chinmay Garde <chinmaygarde@google.com>
2024-10-18 14:46:58 -0700

    [Impeller] libImpeller: Allow custom font registrations. (#55934)
    Fixes https://github.com/flutter/flutter/issues/156361
    
    cc @lyceel. A bit of miscommunication on the priority of this one. I
    thought the default font factories were sufficient for the next release.
    But it turns out that was not true on the board which is a custom
    platform.
    
    In any case, this should unblock custom font registrations. See the
    `CanCreateParagraphsWithCustomFont` case for an example. If you don't
    provide the family name alias for the custom font, the name will be read
    from the font data. This example uses the custom "WhatTheFlutter" custom
    font family in "wtf.otf".
    
    <img width="1136" alt="Screenshot 2024-10-17 at 1 26 36 PM"
    src="https://github.com/user-attachments/assets/dbbef2fd-f854-4330-8c82-fc8378489769">

--- impeller/toolkit/interop/typography_context.h
+++ impeller/toolkit/interop/typography_context.h
@@ -7,6 +7,7 @@
 
 #include <memory>
 
+#include "flutter/third_party/skia/modules/skparagraph/include/TypefaceFontProvider.h"
 #include "flutter/third_party/txt/src/txt/font_collection.h"
 #include "impeller/toolkit/interop/impeller.h"
 #include "impeller/toolkit/interop/object.h"
@@ -29,8 +30,22 @@ class TypographyContext final
 
   const std::shared_ptr<txt::FontCollection>& GetFontCollection() const;
 
+  //----------------------------------------------------------------------------
+  /// @brief      Registers custom font data. If an alias for the family name is
+  ///             provided, subsequent lookups will need to use that same alias.
+  ///             If not, the family name will be read from the font data.
+  ///
+  /// @param[in]  font_data          The font data
+  /// @param[in]  family_name_alias  The family name alias
+  ///
+  /// @return     If the font data could be successfully registered.
+  ///
+  bool RegisterFont(std::unique_ptr<fml::Mapping> font_data,
+                    const char* family_name_alias);
+
  private:
   std::shared_ptr<txt::FontCollection> collection_;
+  sk_sp<skia::textlayout::TypefaceFontProvider> asset_font_manager_;
 };
 
 }  // namespace impeller::interop
