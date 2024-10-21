619d3b7e87b60132199d5441c89099542b1ad6ff
Mohellebi abdessalem <mohellebiabdessalem@gmail.com>
2024-10-21 18:32:42 +0100

    add the import for `UnknownTaskException`

--- packages/flutter_tools/gradle/src/main/groovy/flutter.groovy
+++ packages/flutter_tools/gradle/src/main/groovy/flutter.groovy
@@ -14,6 +14,7 @@ import org.gradle.api.JavaVersion
 import org.gradle.api.Project
 import org.gradle.api.Plugin
 import org.gradle.api.Task
+import org.gradle.api.UnknownTaskException
 import org.gradle.api.file.CopySpec
 import org.gradle.api.file.FileCollection
 import org.gradle.api.logging.LogLevel
