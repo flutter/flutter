8c937f9db0768dfcca4f84dac3efeaf357bfab79
Loic Sharma <sharma.loic@gmail.com>
2024-10-21 16:19:58 -0700

    Feedback

--- packages/flutter/lib/src/widgets/safe_area.dart
+++ packages/flutter/lib/src/widgets/safe_area.dart
@@ -11,8 +11,10 @@ import 'debug.dart';
 import 'framework.dart';
 import 'media_query.dart';
 
-/// A widget that insets its child by sufficient padding to avoid intrusions by
-/// the operating system.
+/// A widget that insets its child with sufficient padding to avoid intrusions
+/// by the operating system.
+///
+/// {@youtube 560 315 https://www.youtube.com/watch?v=lkF0TQJO0bA}
 ///
 /// For example, this will indent the child by enough to avoid the status bar at
 /// the top of the screen.
@@ -22,8 +24,6 @@ import 'media_query.dart';
 ///
 /// When a [minimum] padding is specified, the greater of the minimum padding
 /// or the safe area padding will be applied.
-///
-/// {@youtube 560 315 https://www.youtube.com/watch?v=lkF0TQJO0bA}
 ///
 /// {@tool snippet}
 ///
@@ -42,19 +42,23 @@ import 'media_query.dart';
 /// ```
 /// {@end-tool}
 ///
+/// ### MediaQuery impact
+///
+/// The padding on the [MediaQuery] for the [child] will be suitably adjusted
+/// to zero out any sides that were avoided by this widget.
+///
+/// {@youtube 560 315 https://www.youtube.com/watch?v=ceCo8U0XHqw}
+///
 /// See also:
 ///
 ///  * [SliverSafeArea], for insetting slivers to avoid operating system
 ///    intrusions.
 ///  * [Padding], for insetting widgets in general.
-///  * [MediaQuery], from which the window padding is obtained.
+///  * [MediaQuery], from which the view padding is obtained.
 ///  * [dart:ui.FlutterView.padding], which reports the padding from the operating
 ///    system.
 class SafeArea extends StatelessWidget {
   /// Creates a widget that avoids operating system interfaces.
-  ///
-  /// The [left], [top], [right], [bottom], and [minimum] arguments must not be
-  /// null.
   const SafeArea({
     super.key,
     this.left = true,
