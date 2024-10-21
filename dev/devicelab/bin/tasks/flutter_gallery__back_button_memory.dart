493b22f140bacb26f3092eef12dc5d55967fd952
Jason Simmons <jason-simmons@users.noreply.github.com>
2024-10-21 14:50:04 -0700

    Revert "[devicelab] handle missed lifecycle messages. (#156596)" (#157293)
    This reverts commit 66eed33c6960250cac949da7bf5a52d9884f76d2.
    
    With https://github.com/flutter/engine/pull/55933 this should not be needed.

--- dev/devicelab/bin/tasks/flutter_gallery__back_button_memory.dart
+++ dev/devicelab/bin/tasks/flutter_gallery__back_button_memory.dart
@@ -32,10 +32,7 @@ class BackButtonMemoryTest extends MemoryTest {
       // Push back button, wait for it to be seen by the Flutter app.
       prepareForNextMessage('AppLifecycleState.paused');
       await device!.shellExec('input', <String>['keyevent', 'KEYCODE_BACK']);
-
-      // Note: post UI/platform merge, we consistently miss this message. From
-      // local logcat it seems to be printed but it does not reach the tool.
-      await receivedNextMessage?.timeout(const Duration(seconds: 4), onTimeout: () {});
+      await receivedNextMessage;
 
       // Give Android time to settle (e.g. run GCs) after closing the app.
       await Future<void>.delayed(const Duration(milliseconds: 100));
