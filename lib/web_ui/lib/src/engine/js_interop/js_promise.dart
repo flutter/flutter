5d3a474b4a9adc04125f2cb5d1fefc6577f12399
David Iglesias <ditman@gmail.com>
2024-10-22 16:09:17 -0700

    [web] Remove old futureToPromise. (#55847)
    This PR removes the old `futureToPromise` implementation (which was barely used), and replaces it by a more standard js_interop approach.
    
    Fixes https://github.com/flutter/flutter/issues/155732
    
    [C++, Objective-C, Java style guides]: https://github.com/flutter/engine/blob/main/CONTRIBUTING.md#style

--- lib/web_ui/lib/src/engine/js_interop/js_promise.dart
+++ lib/web_ui/lib/src/engine/js_interop/js_promise.dart
@@ -6,32 +6,30 @@
 library js_promise;
 
 import 'dart:js_interop';
+import 'dart:js_interop_unsafe';
 
-import 'package:js/js_util.dart' as js_util;
-
-import '../util.dart';
-
-extension CallExtension on JSFunction {
-  external void call(JSAny? this_, JSAny? object);
-}
-
-@JS('Promise')
-external JSAny get _promiseConstructor;
-
-JSPromise<JSAny?> createPromise(JSFunction executor) =>
-  js_util.callConstructor(
-    _promiseConstructor,
-    <Object>[executor],
-  );
-
-
-JSPromise<JSAny?> futureToPromise<T extends JSAny?>(Future<T> future) {
-  return createPromise((JSFunction resolver, JSFunction rejecter) {
-    future.then(
-      (T value) => resolver.call(null, value),
-      onError: (Object? error) {
-        printWarning('Rejecting promise with error: $error');
-        rejecter.call(null, null);
+/// This is the same as package:js_interop's FutureToPromise (.toJS), but with
+/// a more descriptive error message.
+extension CustomFutureOfJSAnyToJSPromise<T extends JSAny?> on Future<T> {
+  /// A [JSPromise] that either resolves with the result of the completed
+  /// [Future] or rejects with an object that contains its error.
+  JSPromise<T> get toPromise {
+    // TODO(ditman): Move to js_interop's .toJS, https://github.com/dart-lang/sdk/issues/56898
+    return JSPromise<T>((JSFunction resolve, JSFunction reject) {
+      then((JSAny? value) {
+        resolve.callAsFunction(resolve, value);
+      }, onError: (Object error, StackTrace stackTrace) {
+        final errorConstructor = globalContext['Error']! as JSFunction;
+        var userError = '$error\n';
+        // Only append the stack trace string if it looks like a DDC one...
+        final stackTraceString = stackTrace.toString();
+        if (!stackTraceString.startsWith('\n')) {
+          userError += '\nDart stack trace:\n$stackTraceString';
+        }
+        final wrapper =
+            errorConstructor.callAsConstructor<JSObject>(userError.toJS);
+        reject.callAsFunction(reject, wrapper);
       });
-  }.toJS);
+    }.toJS);
+  }
 }
