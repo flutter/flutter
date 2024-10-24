b0b00e7f66f0912db0af9722ceb1de447919280b
mitchellgoodwin <mitchgoodwin@google.com>
2024-10-24 16:01:15 -0700

    add examples

---
+++ examples/api/lib/cupertino/sheet/cupertino_sheet.0.dart
@@ -0,0 +1,85 @@
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/cupertino.dart';
+
+/// Flutter code sample for [CupertinoSheetRoute].
+
+void main() {
+  runApp(const CupertinoSheetApp());
+}
+
+class CupertinoSheetApp extends StatelessWidget {
+  const CupertinoSheetApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const CupertinoApp(
+      title: 'Cupertino Sheet',
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      navigationBar: const CupertinoNavigationBar(
+        middle: Text('Sheet Example'),
+        automaticBackgroundVisibility: false,
+      ),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            CupertinoButton.filled(
+              onPressed: () {
+                Navigator.of(context).push(CupertinoSheetRoute<void>(
+                  pageBuilder: (BuildContext context) => const SheetScaffold()
+                ));
+              },
+              child: const Text('Open Bottom Sheet'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+class SheetScaffold extends StatelessWidget {
+  const SheetScaffold({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text('The sheet'),
+            CupertinoButton.filled(
+              onPressed: () {
+                Navigator.of(context).maybePop();
+              },
+              child: const Text('Go back'),
+            ),
+            CupertinoButton.filled(
+              onPressed: () {
+                Navigator.of(context).push(CupertinoSheetRoute<void>(
+                  pageBuilder: (BuildContext context) => const SheetScaffold()
+                ));
+              },
+              child: const Text('Push Sheet'),
+            ),
+          ],
+        ),
+      )
+    );
+  }
+}
