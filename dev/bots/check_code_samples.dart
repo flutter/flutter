5b8c664e66cb98c1e3d97f4e09c7e4d294152d04
ValentinVignal <valentin.vignal.dev@outlook.fr>
2024-10-25 12:22:44 +0800

    test: Add tests  for focusable_action_detector.0.dart

--- dev/bots/check_code_samples.dart
+++ dev/bots/check_code_samples.dart
@@ -337,6 +337,5 @@ final Set<String> _knownMissingTests = <String>{
   'examples/api/test/widgets/overscroll_indicator/glowing_overscroll_indicator.1_test.dart',
   'examples/api/test/widgets/overscroll_indicator/glowing_overscroll_indicator.0_test.dart',
   'examples/api/test/widgets/restoration/restoration_mixin.0_test.dart',
-  'examples/api/test/widgets/actions/focusable_action_detector.0_test.dart',
   'examples/api/test/widgets/focus_scope/focus_scope.0_test.dart',
 };
