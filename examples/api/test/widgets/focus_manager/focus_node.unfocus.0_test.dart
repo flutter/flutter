fd2ed1e97ad0ad45d6f70810950e0a0fcb6e5c2a
Valentin Vignal <32538273+ValentinVignal@users.noreply.github.com>
2024-10-28 18:47:23 +0800

    Add test for `focus_node.unfocus.0.dart` (#157673)
    Contributes to https://github.com/flutter/flutter/issues/130459
    
    It adds a test for
    - `examples/api/lib/widgets/focus_manager/focus_node.unfocus.0.dart`

---
+++ examples/api/test/widgets/focus_manager/focus_node.unfocus.0_test.dart
@@ -0,0 +1,78 @@
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_api_samples/widgets/focus_manager/focus_node.unfocus.0.dart'
+    as example;
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets(
+    'Unfocusing with UnfocusDisposition.scope gives the focus to the parent scope',
+    (WidgetTester tester) async {
+      await tester.pumpWidget(
+        const example.UnfocusExampleApp(),
+      );
+
+      // Focuses the first text field.
+      await tester.tap(find.byType(TextField).first);
+      await tester.pump();
+
+      // Changes the focus to the unfocus button.
+      for (int i = 0; i < 6; i++) {
+        await tester.sendKeyEvent(LogicalKeyboardKey.tab);
+        await tester.pump();
+      }
+
+      await tester.sendKeyEvent(LogicalKeyboardKey.enter);
+      await tester.pump();
+
+      await tester.sendKeyEvent(LogicalKeyboardKey.tab);
+      await tester.pump();
+
+      // After pressing tab once, the focus is on the first text field.
+      final EditableText firstEditableText = tester.firstWidget(find.byType(EditableText));
+      expect(firstEditableText.focusNode.hasFocus, true);
+    },
+  );
+
+  testWidgets(
+    'Unfocusing with UnfocusDisposition.previouslyFocusedChild gives the focus the previously focused child',
+    (WidgetTester tester) async {
+      await tester.pumpWidget(
+        const example.UnfocusExampleApp(),
+      );
+
+      // Focuses the first text field.
+      await tester.tap(find.byType(TextField).first);
+      await tester.pump();
+
+      // Changes the focus to the second radio button.
+      for (int i = 0; i < 5; i++) {
+        await tester.sendKeyEvent(LogicalKeyboardKey.tab);
+        await tester.pump();
+      }
+
+      await tester.sendKeyEvent(LogicalKeyboardKey.space);
+      await tester.pumpAndSettle();
+
+      // Changes the focus to the unfocus button.
+      await tester.sendKeyEvent(LogicalKeyboardKey.tab);
+      await tester.pump();
+
+      await tester.sendKeyEvent(LogicalKeyboardKey.enter);
+      await tester.pumpAndSettle();
+
+      // After pressing tab twice, the focus is on the first text field.
+      for (int i = 0; i < 2; i++) {
+        await tester.sendKeyEvent(LogicalKeyboardKey.tab);
+        await tester.pump();
+      }
+
+      final EditableText firstEditableText = tester.firstWidget(find.byType(EditableText));
+      expect(firstEditableText.focusNode.hasFocus, true);
+    },
+  );
+}
