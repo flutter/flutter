999797a2f6902af810c3e920237de9a854894f98
Jonah Williams <jonahwilliams@google.com>
2024-10-29 11:43:22 -0700

    [Impeller] disable Adreno 506. (#56210)
    Fixes https://github.com/flutter/flutter/issues/155185

--- impeller/renderer/backend/vulkan/driver_info_vk.cc
+++ impeller/renderer/backend/vulkan/driver_info_vk.cc
@@ -338,13 +338,17 @@ bool DriverInfoVK::IsKnownBadDriver() const {
   if (adreno_gpu_.has_value()) {
     auto adreno = adreno_gpu_.value();
     switch (adreno) {
-      // see:
+      // See:
       // https://github.com/flutter/flutter/issues/154103
       //
       // Reports "VK_INCOMPLETE" when compiling certain entity shader with
       // vkCreateGraphicsPipelines, which is not a valid return status.
       // See https://github.com/flutter/flutter/issues/155185 .
       case AdrenoGPU::kAdreno630:
+      // See:
+      // https://github.com/flutter/flutter/issues/155185
+      // Unknown crashes but device is not easily acquirable.
+      case AdrenoGPU::kAdreno506:
         return true;
       default:
         return false;
