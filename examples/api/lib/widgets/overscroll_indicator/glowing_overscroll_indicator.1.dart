5a5e70b006175ef1f9a790b1a85ed48246e6880f
Nate Wilson <nate.w5687@gmail.com>
2024-10-30 13:18:40 -0600

    Apply suggestions from code review
    Co-authored-by: Taha Tesser <tessertaha@gmail.com>

--- examples/api/lib/widgets/overscroll_indicator/glowing_overscroll_indicator.1.dart
+++ examples/api/lib/widgets/overscroll_indicator/glowing_overscroll_indicator.1.dart
@@ -27,8 +27,8 @@ class GlowingOverscrollIndicatorExampleApp extends StatelessWidget {
 
 const Set<PointerDeviceKind> allPointers = <PointerDeviceKind>{...PointerDeviceKind.values};
 
-/// Passing this class into [MaterialApp.scrollBehavior] ensures that a
-/// [GlowingOverscrollIndicator] is created, regardless of the target platform.
+// Passing this class into the MaterialApp constructor ensures that a
+// GlowingOverscrollIndicator is created, regardless of the target platform.
 class AlwaysGlow extends MaterialScrollBehavior {
   const AlwaysGlow();
 
