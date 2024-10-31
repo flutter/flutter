213d216553963b768c5e7a7a5feea3766e7767a7
jonahwilliams <jonahwilliams@google.com>
2024-10-31 07:51:35 -0700

    Remove timeout.

--- dev/integration_tests/ui/test_driver/solid_color_test.dart
+++ dev/integration_tests/ui/test_driver/solid_color_test.dart
@@ -19,9 +19,8 @@ void main() {
     // RGBA Encoded Bytes.
     final Uint8List data = (await driver.screenshot(format: ScreenshotFormat.rawStraightRgba)) as Uint8List;
 
-    //
     expect(data[0] << 24 | data[1] << 16 | data[2] << 8 | data[3], 0xFF0000FF);
-  });
+  }, timeout: Timeout.none);
 
   tearDownAll(() async {
     await driver.close();
