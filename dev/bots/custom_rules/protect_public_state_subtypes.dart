04360719277bb276e9f62c63e253b87caef16635
Nate Wilson <nate.w5687@gmail.com>
2024-11-01 13:41:41 -0600

    add license comment

--- dev/bots/custom_rules/protect_public_state_subtypes.dart
+++ dev/bots/custom_rules/protect_public_state_subtypes.dart
@@ -1,3 +1,7 @@
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import 'package:analyzer/dart/analysis/results.dart';
 import 'package:analyzer/dart/ast/ast.dart';
 import 'package:analyzer/dart/ast/visitor.dart';
