2e94b3ac91b24723eaca3efdbff7eb11ead9a141
Robert Ancell <robert.ancell@canonical.com>
2024-11-06 10:17:13 +1300

    Remove unnecessary method for getting engine switches (command line). (flutter/engine#56265)
    This method simply wrapped a common method, which is simpler to use
    directly. The test only tested this common method, which already has
    tests in common/.

--- engine/src/flutter/shell/platform/linux/fl_engine_private.h
+++ engine/src/flutter/shell/platform/linux/fl_engine_private.h
@@ -417,16 +417,6 @@ gboolean fl_engine_unregister_external_texture(FlEngine* engine,
 void fl_engine_update_accessibility_features(FlEngine* engine, int32_t flags);
 
 /**
- * fl_engine_get_switches:
- * @engine: an #FlEngine.
- *
- * Determines the switches that should be passed to the Flutter engine.
- *
- * Returns: an array of switches to pass to the Flutter engine.
- */
-GPtrArray* fl_engine_get_switches(FlEngine* engine);
-
-/**
  * fl_engine_request_app_exit:
  * @engine: an #FlEngine.
  *
