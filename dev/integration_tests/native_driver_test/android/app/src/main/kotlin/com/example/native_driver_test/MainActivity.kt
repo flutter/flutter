41bd5d3b6c7affc3b20b8eeb81acafe60de07586
Yaakov Schectman <schectman@google.com>
2024-11-06 05:42:19 -0500

    Set layout mode for API<35

--- dev/integration_tests/native_driver_test/android/app/src/main/kotlin/com/example/native_driver_test/MainActivity.kt
+++ dev/integration_tests/native_driver_test/android/app/src/main/kotlin/com/example/native_driver_test/MainActivity.kt
@@ -6,7 +6,10 @@
 
 package com.example.native_driver_test
 
+import android.content.pm.ActivityInfo.WindowLayout
+import android.os.Build
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
@@ -45,5 +48,10 @@ class MainActivity : FlutterActivity() {
         windowInsetsController.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
         windowInsetsController.hide(WindowInsetsCompat.Type.systemBars())
         actionBar?.hide()
+
+        if (Build.VERSION.SDK_INT >= 30) {
+            window.attributes.layoutInDisplayCutoutMode =
+                WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_ALWAYS;
+        }
     }
 }
