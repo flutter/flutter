1af0aae96be74058084cc84e0d2f581f3847b36f
Bruno Leroux <bruno.leroux@gmail.com>
2024-11-06 08:54:47 +0100

    Fix RawScrollbar examples and desktop test

--- examples/api/lib/widgets/scrollbar/raw_scrollbar.0.dart
+++ examples/api/lib/widgets/scrollbar/raw_scrollbar.0.dart
@@ -32,11 +32,11 @@ class RawScrollbarExample extends StatefulWidget {
 }
 
 class _RawScrollbarExampleState extends State<RawScrollbarExample> {
-  final ScrollController _firstController = ScrollController();
+  final ScrollController _controller = ScrollController();
 
   @override
   void dispose() {
-    _firstController.dispose();
+    _controller.dispose();
     super.dispose();
   }
 
@@ -46,47 +46,52 @@ class _RawScrollbarExampleState extends State<RawScrollbarExample> {
       return Row(
         children: <Widget>[
           SizedBox(
-              width: constraints.maxWidth / 2,
-              // When using the PrimaryScrollController and a Scrollbar
-              // together, only one ScrollPosition can be attached to the
-              // PrimaryScrollController at a time. Providing a
-              // unique scroll controller to this scroll view prevents it
-              // from attaching to the PrimaryScrollController.
-              child: Scrollbar(
-                thumbVisibility: true,
-                controller: _firstController,
-                child: ListView.builder(
-                    controller: _firstController,
-                    itemCount: 100,
-                    itemBuilder: (BuildContext context, int index) {
-                      return Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text('Scrollable 1 : Index $index'),
-                      );
-                    }),
-              )),
+            width: constraints.maxWidth / 2,
+            // When using the PrimaryScrollController and a Scrollbar
+            // together, only one ScrollPosition can be attached to the
+            // PrimaryScrollController at a time. Providing a
+            // unique scroll controller to this scroll view prevents it
+            // from attaching to the PrimaryScrollController.
+            child: Scrollbar(
+              thumbVisibility: true,
+              controller: _controller,
+              child: ListView.builder(
+                controller: _controller,
+                itemCount: 100,
+                itemBuilder: (BuildContext context, int index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text('Scrollable 1 : Index $index'),
+                  );
+                },
+              ),
+            ),
+          ),
           SizedBox(
-              width: constraints.maxWidth / 2,
-              // This vertical scroll view has primary set to true, so it is
-              // using the PrimaryScrollController. On mobile platforms, the
-              // PrimaryScrollController automatically attaches to vertical
-              // ScrollViews, unlike on Desktop platforms, where the primary
-              // parameter is required.
-              child: Scrollbar(
-                thumbVisibility: true,
-                child: ListView.builder(
-                    primary: true,
-                    itemCount: 100,
-                    itemBuilder: (BuildContext context, int index) {
-                      return Container(
-                          height: 50,
-                          color: index.isEven ? Colors.amberAccent : Colors.blueAccent,
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Text('Scrollable 2 : Index $index'),
-                          ));
-                    }),
-              )),
+            width: constraints.maxWidth / 2,
+            // This vertical scroll view has primary set to true, so it is
+            // using the PrimaryScrollController. On mobile platforms, the
+            // PrimaryScrollController automatically attaches to vertical
+            // ScrollViews, unlike on Desktop platforms, where the primary
+            // parameter is required.
+            child: Scrollbar(
+              thumbVisibility: true,
+              child: ListView.builder(
+                primary: true,
+                itemCount: 100,
+                itemBuilder: (BuildContext context, int index) {
+                  return Container(
+                    height: 50,
+                    color: index.isEven ? Colors.amberAccent : Colors.blueAccent,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text('Scrollable 2 : Index $index'),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ),
         ],
       );
     });
