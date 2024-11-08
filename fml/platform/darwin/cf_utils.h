b7134d373ef8f73da4fc5764a1d5bd6d02265ff0
Chris Bracken <chris@bracken.jp>
2024-11-08 12:55:23 -0800

    fml: Use CFRef where possible and add docs (#56436)
    Document where CFRef takes over or hands back ownership of the underlying CoreFoundation object memory.
    
    Migrates manual CoreFoundation object management to CFRef in:
    * impeller/golden_tests/metal_screenshot.mm
    * shell/platform/darwin/graphics/FlutterDarwinContextMetalSkia.mm
    * shell/platform/darwin/graphics/FlutterDarwinExternalTextureMetal.mm
    * shell/platform/darwin/ios/framework/Source/FlutterView.mm
    * shell/platform/darwin/macos/framework/Source/FlutterSurface.mm
    
    Adds a `Retain()` method to take shared ownership of the underlying object, as opposed to Reset, where ownership is transferred to the CFRef wrapper.
    
    Adds a `Get()` method to make dealing with bridged Objective-C casts more convenient:
    ```objc
      fml::CFRef<CFStringRef> cfString(...);
      NSString* aString = (__bridge NSString*)cfString.Get();
    ```
    as opposed to:
    ```objc
      fml::CFRef<CFStringRef> cfString(...);
      NSString* aString = (__bridge NSString*)static_cast<CFStringRef>(cfString);
    ```
    
    I considered making use of `fml::scoped_policy::OwnershipPolicy` to add a second parameter to the ctor and `Reset` but, but I think documentation and addition of a `Retain()` method makes things a little clearer at the call site. It's also more consistent with `sk_cfp`, which we use in some Skia bits of the codebase.
    
    [C++, Objective-C, Java style guides]: https://github.com/flutter/engine/blob/main/CONTRIBUTING.md#style

--- fml/platform/darwin/cf_utils.h
+++ fml/platform/darwin/cf_utils.h
@@ -11,53 +11,103 @@
 
 namespace fml {
 
+/// Default retain and release implementations for CFRef.
+template <typename T>
+struct CFRefTraits {
+  static void Retain(T instance) { CFRetain(instance); }
+  static void Release(T instance) { CFRelease(instance); }
+};
+
+/// RAII-based smart pointer wrapper for CoreFoundation objects.
+///
+/// CFRef takes over ownership of the object it wraps and ensures that retain
+/// and release are called as appropriate on creation, assignment, and disposal.
 template <class T>
 class CFRef {
  public:
+  /// Creates a new null CFRef.
   CFRef() : instance_(nullptr) {}
 
-  // NOLINTNEXTLINE(google-explicit-constructor)
-  CFRef(T instance) : instance_(instance) {}
+  /// Takes over ownership of `instance`, which is expected to be already
+  /// retained.
+  explicit CFRef(T instance) : instance_(instance) {}
 
+  /// Copy ctor: Creates a retained copy of the CoreFoundation object owned by
+  /// `other`.
   CFRef(const CFRef& other) : instance_(other.instance_) {
     if (instance_) {
-      CFRetain(instance_);
+      CFRefTraits<T>::Retain(instance_);
     }
   }
 
+  /// Move ctor: Takes over ownership of the CoreFoundation object owned
+  /// by `other`. The object owned by `other` is set to null.
   CFRef(CFRef&& other) : instance_(other.instance_) {
     other.instance_ = nullptr;
   }
 
+  /// Takes over ownership of the CoreFoundation object owned by `other`.
   CFRef& operator=(CFRef&& other) {
     Reset(other.Release());
     return *this;
   }
 
+  /// Releases the underlying CoreFoundation object, if non-null.
   ~CFRef() {
-    if (instance_ != nullptr) {
-      CFRelease(instance_);
+    if (instance_) {
+      CFRefTraits<T>::Release(instance_);
     }
     instance_ = nullptr;
   }
 
+  /// Takes over ownership of `instance`, null by default. The object is
+  /// expected to be already retained if non-null.
+  ///
+  /// Releases the previous object, if non-null.
   void Reset(T instance = nullptr) {
-    if (instance_ != nullptr) {
-      CFRelease(instance_);
+    if (instance_) {
+      CFRefTraits<T>::Release(instance_);
     }
-
     instance_ = instance;
   }
 
+  /// Retains a shared copy of `instance`. The previous object is released if
+  /// non-null. Has no effect if `instance` is the currently-held object.
+  void Retain(T instance = nullptr) {
+    if (instance_ == instance) {
+      return;
+    }
+    if (instance) {
+      CFRefTraits<T>::Retain(instance);
+    }
+    Reset(instance);
+  }
+
+  /// Returns and transfers ownership of the underlying CoreFoundation object
+  /// to the caller. The caller is responsible for calling `CFRelease` when done
+  /// with the object.
   [[nodiscard]] T Release() {
     auto instance = instance_;
     instance_ = nullptr;
     return instance;
   }
 
+  /// Returns the underlying CoreFoundation object. Ownership of the returned
+  /// object follows The Get Rule.
+  ///
+  /// See:
+  /// https://developer.apple.com/library/archive/documentation/CoreFoundation/Conceptual/CFMemoryMgmt/Concepts/Ownership.html#//apple_ref/doc/uid/20001148-SW1
+  T Get() const { return instance_; }
+
+  /// Returns the underlying CoreFoundation object. Ownership of the returned
+  /// object follows The Get Rule.
+  ///
+  /// See:
+  /// https://developer.apple.com/library/archive/documentation/CoreFoundation/Conceptual/CFMemoryMgmt/Concepts/Ownership.html#//apple_ref/doc/uid/20001148-SW1
   // NOLINTNEXTLINE(google-explicit-constructor)
   operator T() const { return instance_; }
 
+  /// Returns true if the underlying CoreFoundation object is non-null.
   explicit operator bool() const { return instance_ != nullptr; }
 
  private:
