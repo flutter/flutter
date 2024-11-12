8f10d972315a1b909bcf613ff5e212724b8780a7
Matan Lurey <matan@lurey.org>
2024-11-12 10:57:45 -0800

    Migrate more tests, update throwing pub error.

--- packages/flutter_tools/test/src/throwing_pub.dart
+++ packages/flutter_tools/test/src/throwing_pub.dart
@@ -9,6 +9,19 @@ final class ThrowingPub implements Pub {
 
   @override
   Never noSuchMethod(Invocation invocation) {
-    throw UnsupportedError('Attempted to invoke pub during test.');
+    throw UnsupportedError(
+      'Attempted to invoke pub during test, which otherwise was unexpected. '
+      'This error may be caused by either changing the implementation details '
+      'of the Flutter CLI in where the "Pub" class is now being used, or '
+      'adding a unit test that transitively depends on "Pub".\n'
+      '\n'
+      'Possible options for resolution:\n'
+      ' 1. Refactor the code or test to not rely on "Pub".\n'
+      ' 2. Create and use a test-appropriate Fake (grep for "implements Pub") '
+      '    for example code across the test/ repo. It is possible that the '
+      '    file you are editing already has an appropriate Fake.\n'
+      ' 3. Use "FakePubWithPrimedDeps" if your code will transitively call '
+      '    "dart pub deps --json" and you need a realistic output.',
+    );
   }
 }
