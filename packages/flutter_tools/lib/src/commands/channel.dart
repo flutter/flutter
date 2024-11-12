3570ae130113f68396d72e34d4dd0ee721cbe12d
Matan Lurey <matan@lurey.org>
2024-11-12 13:51:09 -0800

    Fix.

--- packages/flutter_tools/lib/src/commands/channel.dart
+++ packages/flutter_tools/lib/src/commands/channel.dart
@@ -13,18 +13,17 @@ import '../version.dart';
 import 'upgrade.dart' show precacheArtifacts;
 
 class ChannelCommand extends FlutterCommand {
-  ChannelCommand({bool verboseHelp = false}) {
+  ChannelCommand({ bool verboseHelp = false }) {
     argParser.addFlag(
       'all',
       abbr: 'a',
-      help:
-          'Include all the available branches (including local branches) when listing channels.',
+      help: 'Include all the available branches (including local branches) when listing channels.',
       hide: !verboseHelp,
     );
     argParser.addFlag(
       'cache-artifacts',
       help: 'After switching channels, download all required binary artifacts. '
-          'This is the equivalent of running "flutter precache" with the "--all-platforms" flag.',
+            'This is the equivalent of running "flutter precache" with the "--all-platforms" flag.',
       defaultsTo: true,
     );
   }
@@ -50,8 +49,7 @@ class ChannelCommand extends FlutterCommand {
   String get invocation => '${runner?.executableName} $name [<channel-name>]';
 
   @override
-  Future<Set<DevelopmentArtifact>> get requiredArtifacts async =>
-      const <DevelopmentArtifact>{};
+  Future<Set<DevelopmentArtifact>> get requiredArtifacts async => const <DevelopmentArtifact>{};
 
   @override
   Future<FlutterCommandResult> runCommand() async {
@@ -71,16 +69,10 @@ class ChannelCommand extends FlutterCommand {
     }
   }
 
-  Future<void> _listChannels(
-      {required bool showAll, required bool verbose}) async {
+  Future<void> _listChannels({ required bool showAll, required bool verbose }) async {
     // Beware: currentBranch could contain PII. See getBranchName().
-    final String currentChannel =
-        globals.flutterVersion.channel; // limited to known branch names
-    assert(
-        kOfficialChannels.contains(currentChannel) ||
-            kObsoleteBranches.containsKey(currentChannel) ||
-            currentChannel == kUserBranch,
-        'potential PII leak in channel name: "$currentChannel"');
+    final String currentChannel = globals.flutterVersion.channel; // limited to known branch names
+    assert(kOfficialChannels.contains(currentChannel) || kObsoleteBranches.containsKey(currentChannel) || currentChannel == kUserBranch, 'potential PII leak in channel name: "$currentChannel"');
     final String currentBranch = globals.flutterVersion.getBranchName();
     final Set<String> seenUnofficialChannels = <String>{};
     final List<String> rawOutput = <String>[];
@@ -126,8 +118,7 @@ class ChannelCommand extends FlutterCommand {
           currentIndicator = '*';
           currentChannelIsOfficial = true;
         }
-        globals.printStatus(
-            '$currentIndicator $channel (${kChannelDescriptions[channel]})');
+        globals.printStatus('$currentIndicator $channel (${kChannelDescriptions[channel]})');
       }
     }
 
@@ -145,8 +136,7 @@ class ChannelCommand extends FlutterCommand {
     }
 
     if (!currentChannelIsOfficial) {
-      assert(currentChannel == kUserBranch,
-          'Current channel is "$currentChannel", which is not an official branch. (Current branch is "$currentBranch".)');
+      assert(currentChannel == kUserBranch, 'Current channel is "$currentChannel", which is not an official branch. (Current branch is "$currentBranch".)');
       globals.printStatus('');
       globals.printStatus('Currently not on an official channel.');
     }
@@ -156,20 +146,16 @@ class ChannelCommand extends FlutterCommand {
     globals.printStatus("Switching to flutter channel '$branchName'...");
     if (kObsoleteBranches.containsKey(branchName)) {
       final String alternative = kObsoleteBranches[branchName]!;
-      globals.printStatus(
-          "This channel is obsolete. Consider switching to the '$alternative' channel instead.");
+      globals.printStatus("This channel is obsolete. Consider switching to the '$alternative' channel instead.");
     } else if (!kOfficialChannels.contains(branchName)) {
-      globals.printStatus(
-          'This is not an official channel. For a list of available channels, try "flutter channel".');
+      globals.printStatus('This is not an official channel. For a list of available channels, try "flutter channel".');
     }
     await _checkout(branchName);
     if (boolArg('cache-artifacts')) {
       await precacheArtifacts(Cache.flutterRoot);
     }
-    globals
-        .printStatus("Successfully switched to flutter channel '$branchName'.");
-    globals.printStatus(
-        "To ensure that you're on the latest build from this channel, run 'flutter upgrade'");
+    globals.printStatus("Successfully switched to flutter channel '$branchName'.");
+    globals.printStatus("To ensure that you're on the latest build from this channel, run 'flutter upgrade'");
   }
 
   static Future<void> upgradeChannel(FlutterVersion currentVersion) async {
@@ -190,13 +176,7 @@ class ChannelCommand extends FlutterCommand {
 
     if (runResult.processResult.exitCode == 0) {
       runResult = await globals.processUtils.run(
-        <String>[
-          'git',
-          'show-ref',
-          '--verify',
-          '--quiet',
-          'refs/heads/$branchName'
-        ],
+        <String>['git', 'show-ref', '--verify', '--quiet', 'refs/heads/$branchName'],
         workingDirectory: Cache.flutterRoot,
       );
       if (runResult.processResult.exitCode == 0) {
@@ -208,14 +188,7 @@ class ChannelCommand extends FlutterCommand {
       } else {
         // branch does not exist, we have to create it
         runResult = await globals.processUtils.run(
-          <String>[
-            'git',
-            'checkout',
-            '--track',
-            '-b',
-            branchName,
-            'origin/$branchName'
-          ],
+          <String>['git', 'checkout', '--track', '-b', branchName, 'origin/$branchName'],
           workingDirectory: Cache.flutterRoot,
         );
       }
