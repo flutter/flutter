77a0e3ebeb431f412d5e784222aa7182375e5385
Qun Cheng <chengqunq@gmail.com>
2024-11-11 22:58:07 -0800

    Create Android U page transition

--- packages/flutter/lib/src/material/page_transitions_theme.dart
+++ packages/flutter/lib/src/material/page_transitions_theme.dart
@@ -307,7 +307,6 @@ class _ZoomPageTransition extends StatelessWidget {
       },
       child: ZoomPageTransitionsBuilder._snapshotAwareDelegatedTransition(
         context,
-        animation,
         secondaryAnimation,
         child,
         allowSnapshotting,
@@ -534,6 +533,72 @@ class _ZoomExitTransitionState extends State<_ZoomExitTransition> with _ZoomTran
   }
 }
 
+class _FadeForwardsPageTransition extends StatelessWidget {
+  const _FadeForwardsPageTransition({
+    required this.animation,
+    required this.secondaryAnimation,
+    this.backgroundColor,
+    this.child,
+  });
+
+  final Animation<double> animation;
+
+  final Animation<double> secondaryAnimation;
+
+  final Color? backgroundColor;
+
+  final Widget? child;
+
+  // The new page slides forwards from right to left.
+  static final Animatable<Offset> _forwardTranslationTween = Tween<Offset>(
+    begin: const Offset(0.25, 0.0),
+    end: Offset.zero,
+  ).chain(CurveTween(curve: FadeForwardsPageTransitionsBuilder._transitionCurve));
+
+  static final Animatable<Offset> _backwardTranslationTween = Tween<Offset>(
+    begin: Offset.zero,
+    end: const Offset(0.25, 0.0),
+  ).chain(CurveTween(curve: FadeForwardsPageTransitionsBuilder._transitionCurve));
+
+  @override
+  Widget build(BuildContext context) {
+    return DualTransitionBuilder(
+      animation: animation,
+      forwardBuilder: (
+        BuildContext context,
+        Animation<double> animation,
+        Widget? child
+      ) {
+        return Opacity(
+          opacity: FadeForwardsPageTransitionsBuilder._fadeInTransition.evaluate(animation),
+          child: SlideTransition(
+            position: _forwardTranslationTween.animate(animation),
+            child: child,
+          ),
+        );
+      },
+      reverseBuilder: (
+        BuildContext context,
+        Animation<double> animation,
+        Widget? child
+      ) {
+        return Opacity(
+          opacity: FadeForwardsPageTransitionsBuilder._fadeOutTransition.evaluate(animation),
+          child: SlideTransition(
+            position: _backwardTranslationTween.animate(animation),
+            child: child,
+          ),
+        );
+      },
+      child: FadeForwardsPageTransitionsBuilder._delegatedTransition(
+        context,
+        secondaryAnimation,
+        backgroundColor,
+        child,
+      ));
+  }
+}
+
 /// Used by [PageTransitionsTheme] to define a [MaterialPageRoute] page
 /// transition animation.
 ///
@@ -646,6 +711,123 @@ class OpenUpwardsPageTransitionsBuilder extends PageTransitionsBuilder {
   }
 }
 
+/// Used by [PageTransitionsTheme] to define a horizontal [MaterialPageRoute] page
+/// transition animation that looks like the default page transition
+/// used on Android U.
+///
+/// See also:
+///
+///  * [FadeUpwardsPageTransitionsBuilder], which defines a page transition
+///    that's similar to the one provided by Android O.
+///  * [OpenUpwardsPageTransitionsBuilder], which defines a page transition
+///    that's similar to the one provided by Andoird P.
+///  * [ZoomPageTransitionsBuilder], which defines the default page transition
+///    that's similar to the one provided in Android Q.
+///  * [CupertinoPageTransitionsBuilder], which defines a horizontal page
+///    transition that matches native iOS page transitions.
+///  * [PredictiveBackPageTransitionsBuilder], which defines a page
+///    transition that allows peeking behind the current route on Android.
+class FadeForwardsPageTransitionsBuilder extends PageTransitionsBuilder {
+  /// Constructs a page transition animation that matches the transition used on
+  /// Android U.
+  const FadeForwardsPageTransitionsBuilder({
+    this.backgroundColor,
+  });
+
+  final Color? backgroundColor;
+
+  @override
+  DelegatedTransitionBuilder? get delegatedTransition => (BuildContext context, Animation<double> animation, Animation<double> secondaryAnimation, bool allowSnapshotting, Widget? child)
+      => _delegatedTransition(context, animation, backgroundColor, child);
+
+
+  // Used by all of the transition animations.
+  static const Curve _transitionCurve = Curves.easeInOutCubicEmphasized;
+
+  // The old page slides frowards from left to right as the new page disappears.
+  static final Animatable<Offset> _secondaryForwardTranslationTween = Tween<Offset>(
+    begin: const Offset(-0.25, 0.0),
+    end: Offset.zero,
+  ).chain(CurveTween(curve: _transitionCurve));
+
+  // The old page slides frowards from right to left as the new page appears.
+  static final Animatable<Offset> _secondaryBackwardTranslationTween = Tween<Offset>(
+    begin: Offset.zero,
+    end: const Offset(-0.25, 0.0),
+  ).chain(CurveTween(curve: _transitionCurve));
+
+  // The new page fades in.
+  static final Animatable<double> _fadeInTransition = Tween<double>(
+    begin: 0.0,
+    end: 1.0,
+  ).chain(CurveTween(curve: const Interval(0.0, 0.75)));
+
+  // The old page fades out.
+  static final Animatable<double> _fadeOutTransition = Tween<double>(
+    begin: 1.0,
+    end: 0.0,
+  ).chain(CurveTween(curve: const Interval(0.0, 0.25)));
+
+  static Widget _delegatedTransition(
+    BuildContext context,
+    Animation<double> secondaryAnimation,
+    Color? backgroundColor,
+    Widget? child,
+  ) => DualTransitionBuilder(
+      animation: ReverseAnimation(secondaryAnimation),
+      forwardBuilder: (
+        BuildContext context,
+        Animation<double> animation,
+        Widget? child
+      ) {
+        return ColoredBox(
+          color: backgroundColor ?? Theme.of(context).colorScheme.surface,
+          child: Opacity(
+            opacity: _fadeInTransition.evaluate(animation),
+            child: SlideTransition(
+              position: _secondaryForwardTranslationTween.animate(animation),
+              child: child,
+            ),
+          ),
+        );
+      },
+      reverseBuilder: (
+        BuildContext context,
+        Animation<double> animation,
+        Widget? child
+      ) {
+        return ColoredBox(
+          color: backgroundColor ?? Theme.of(context).colorScheme.surface,
+          child: Opacity(
+            opacity: _fadeOutTransition.evaluate(animation),
+            child: SlideTransition(
+              position: _secondaryBackwardTranslationTween.animate(animation),
+              child: child,
+            ),
+          ),
+        );
+      },
+      child: child,
+    );
+
+
+  @override
+  Widget buildTransitions<T>(
+    PageRoute<T>? route,
+    BuildContext? context,
+    Animation<double> animation,
+    Animation<double> secondaryAnimation,
+    Widget child,
+  ) {
+    return _FadeForwardsPageTransition(
+      animation: animation,
+      secondaryAnimation: secondaryAnimation,
+      backgroundColor: backgroundColor,
+      child: child,
+    );
+  }
+}
+
 /// Used by [PageTransitionsTheme] to define a zooming [MaterialPageRoute] page
 /// transition animation that looks like the default page transition used on
 /// Android Q.
@@ -716,11 +898,11 @@ class ZoomPageTransitionsBuilder extends PageTransitionsBuilder {
 
   @override
   DelegatedTransitionBuilder? get delegatedTransition => (BuildContext context, Animation<double> animation, Animation<double> secondaryAnimation, bool allowSnapshotting, Widget? child)
-      => _snapshotAwareDelegatedTransition(context, animation, secondaryAnimation, child, allowSnapshotting && this.allowSnapshotting, allowEnterRouteSnapshotting, backgroundColor);
+      => _snapshotAwareDelegatedTransition(context, secondaryAnimation, child, allowSnapshotting && this.allowSnapshotting, allowEnterRouteSnapshotting, backgroundColor);
 
   // A transition builder that takes into account the snapshotting properties of
   // ZoomPageTransitionsBuilder.
-  static Widget _snapshotAwareDelegatedTransition(BuildContext context, Animation<double> animation, Animation<double> secondaryAnimation, Widget? child, bool allowSnapshotting, bool allowEnterRouteSnapshotting, Color? backgroundColor) {
+  static Widget _snapshotAwareDelegatedTransition(BuildContext context, Animation<double> secondaryAnimation, Widget? child, bool allowSnapshotting, bool allowEnterRouteSnapshotting, Color? backgroundColor) {
     final Color enterTransitionBackgroundColor = backgroundColor ?? Theme.of(context).colorScheme.surface;
     return DualTransitionBuilder(
       animation: ReverseAnimation(secondaryAnimation),
