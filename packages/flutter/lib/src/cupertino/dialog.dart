2ff1f98761842a4849180f58149caf0738e7d33b
Srivats Venkataraman <42980667+srivats22@users.noreply.github.com>
2024-11-13 08:01:57 -0500

    updated comment
    Co-authored-by: Taha Tesser <tessertaha@gmail.com>

--- packages/flutter/lib/src/cupertino/dialog.dart
+++ packages/flutter/lib/src/cupertino/dialog.dart
@@ -1237,7 +1237,8 @@ class CupertinoActionSheetAction extends StatefulWidget {
 
   /// The cursor that will be shown when hovering over the button.
   ///
-  /// If null, defaults to [SystemMouseCursors.click] on web and [MouseCursor.defer] on other platforms.
+  /// If null, defaults to [SystemMouseCursors.click] on web and
+  /// [MouseCursor.defer] on other platforms.
   final MouseCursor? mouseCursor;
 
   /// The widget below this widget in the tree.
