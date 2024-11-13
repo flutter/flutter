820738d1261880dee98410081f20786d682ac355
郑健 <850314810@qq.com>
2024-11-13 16:52:19 +0800

    Remove unnecessary directory my_project from repository

--- my_project/android/app/src/main/kotlin/com/example/my_project/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.my_project
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- my_project/macos/Runner/AppDelegate.swift
+++
@@ -1,9 +0,0 @@
-import Cocoa
-import FlutterMacOS
-
-@main
-class AppDelegate: FlutterAppDelegate {
-  override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
-    return true
-  }
-}
