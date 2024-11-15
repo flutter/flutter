b17731544f71661aeff5b0b24818e115b0e5d148
Matan Lurey <matan@lurey.org>
2024-11-15 13:45:00 -0800

    Switch flutter_build_apk_health_tests to use a subset of current tests.

--- packages/flutter_tools/test/integration.shard/isolated/native_assets_agp_version_test.dart
+++ packages/flutter_tools/test/integration.shard/isolated/native_assets_agp_version_test.dart
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// TODO(matanlurey): Remove after debugging https://github.com/flutter/flutter/issues/159000.
+@Tags(<String>['flutter-build-apk'])
 @Timeout(Duration(minutes: 10))
 library;
 
