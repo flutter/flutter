87374b9ca73696711582ab038bdd33dd109697b2
pranavo72bex <pranav072bex@ioepc.edu.np>
2024-11-16 07:43:57 +0545

    fix :  indentation

--- packages/flutter/test/material/time_picker_test.dart
+++ packages/flutter/test/material/time_picker_test.dart
@@ -1508,30 +1508,30 @@ void main() {
         );
       });
 
-    testWidgets('TimePicker dialog displays centered separator between hour and minute selectors', (WidgetTester tester) async {
-          tester.view.physicalSize = const Size(400, 800);
-          tester.view.devicePixelRatio = 1.0;
+      testWidgets('TimePicker dialog displays centered separator between hour and minute selectors', (WidgetTester tester) async {
+            tester.view.physicalSize = const Size(400, 800);
+            tester.view.devicePixelRatio = 1.0;
 
-          await tester.pumpWidget(
-            MaterialApp(
-              theme: ThemeData(useMaterial3: true),
-              home: const MediaQuery(
-                data: MediaQueryData(),
-                child: Material(
-                  child: TimePickerDialog(
-                    initialTime: TimeOfDay(hour: 12, minute: 0),
+            await tester.pumpWidget(
+              MaterialApp(
+                theme: ThemeData(useMaterial3: true),
+                home: const MediaQuery(
+                  data: MediaQueryData(),
+                  child: Material(
+                    child: TimePickerDialog(
+                      initialTime: TimeOfDay(hour: 12, minute: 0),
+                    ),
                   ),
                 ),
               ),
-            ),
-          );
-          await tester.pumpAndSettle();
+            );
+            await tester.pumpAndSettle();
 
-          await expectLater(
-            find.byType(Dialog),
-            matchesGoldenFile('m3_time_picker.dialog.separator.alignment.png'),
-          );
-        });
+            await expectLater(
+              find.byType(Dialog),
+              matchesGoldenFile('m3_time_picker.dialog.separator.alignment.png'),
+            );
+      });
 
       testWidgets('provides semantics information for text fields', (WidgetTester tester) async {
         final SemanticsTester semantics = SemanticsTester(tester);
