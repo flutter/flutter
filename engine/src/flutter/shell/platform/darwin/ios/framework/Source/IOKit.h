20e0482db740a5995eda83135c04f87c97eac55b
Chris Bracken <chris@bracken.jp>
2024-11-17 14:30:19 -0800

    iOS: Clean up uses of string literal constants (flutter/engine#56658)
    Three changes related to string constants:
    
    1. Uses the kIOSurfaceColorSpace constant, which is a CFStringRef pointing to the string "IOSurfaceColorSpace"
    
    2. Uses the kCVPixelFormatType_32BGRA enum value from the CoreVideo headers (which is equal to 'BGRA') in place of hardcoding the string.
    
    From the headers:
    ```
    kCVPixelFormatType_32BGRA         = 'BGRA',     /* 32 bit BGRA */
    ```
    
    3. Declares kIOServicePlane as a `constexpr const char*` rather than `static const char*`, this ensures only a single instance is created, rather than one per translation unit into which the header is included. This string is part of IOKit, but see the comment at the top of the header as to why it's apparently needed.
    
    No test changes since there are no semantic changes.
    
    [C++, Objective-C, Java style guides]: https://github.com/flutter/engine/blob/main/CONTRIBUTING.md#style

--- engine/src/flutter/shell/platform/darwin/ios/framework/Source/IOKit.h
+++ engine/src/flutter/shell/platform/darwin/ios/framework/Source/IOKit.h
@@ -25,7 +25,7 @@ extern "C" {
 #define IOKIT
 #include <device/device_types.h>
 
-static const char* kIOServicePlane = "IOService";
+constexpr const char* kIOServicePlane = "IOService";
 
 typedef io_object_t io_registry_entry_t;
 typedef io_object_t io_service_t;
