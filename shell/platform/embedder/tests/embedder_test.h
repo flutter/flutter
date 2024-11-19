7eb87547cbc6139dab7e5ae9bfda9514114b1546
Chris Bracken <chris@bracken.jp>
2024-11-19 12:20:08 -0800

    EmbedderTest: templatise GetEmbedderContext (#56709)
    In many embedder tests, we want to get at the appropriate backend-specific `EmbedderTestContext` subclass (`EmbedderTestContextGL`, etc.) in order to make backend-specific setup calls such as `SetGLFBOCallback()` or others. Formerly, this required casting the returned `EmbedderTestContext&` to the appropriate subclass in each test.
    
    This templatises the `GetEmbedderContext()` method to return the appropriate backend-specific subclass directly.
    
    Issue: https://github.com/flutter/flutter/issues/158998
    
    [C++, Objective-C, Java style guides]: https://github.com/flutter/engine/blob/main/CONTRIBUTING.md#style

--- shell/platform/embedder/tests/embedder_test.h
+++ shell/platform/embedder/tests/embedder_test.h
@@ -16,29 +16,69 @@
 
 namespace flutter::testing {
 
+class EmbedderTestContextGL;
+class EmbedderTestContextMetal;
+class EmbedderTestContextSoftware;
+class EmbedderTestContextVulkan;
+
 class EmbedderTest : public ThreadTest {
  public:
   EmbedderTest();
 
   std::string GetFixturesDirectory() const;
 
-  EmbedderTestContext& GetEmbedderContext(EmbedderTestContextType type);
+  template <typename T>
+  T& GetEmbedderContext() {
+    static_assert(false, "Unsupported test context type");
+  }
 
- private:
-  std::map<EmbedderTestContextType, std::unique_ptr<EmbedderTestContext>>
-      embedder_contexts_;
+  template <>
+  EmbedderTestContextGL& GetEmbedderContext<EmbedderTestContextGL>() {
+    return reinterpret_cast<EmbedderTestContextGL&>(GetGLContext());
+  }
 
-  std::unique_ptr<EmbedderTestContext> CreateSoftwareContext();
-  std::unique_ptr<EmbedderTestContext> CreateGLContext();
-  std::unique_ptr<EmbedderTestContext> CreateMetalContext();
-  std::unique_ptr<EmbedderTestContext> CreateVulkanContext();
+  template <>
+  EmbedderTestContextMetal& GetEmbedderContext<EmbedderTestContextMetal>() {
+    return reinterpret_cast<EmbedderTestContextMetal&>(GetMetalContext());
+  }
+
+  template <>
+  EmbedderTestContextSoftware&
+  GetEmbedderContext<EmbedderTestContextSoftware>() {
+    return reinterpret_cast<EmbedderTestContextSoftware&>(GetSoftwareContext());
+  }
+
+  template <>
+  EmbedderTestContextVulkan& GetEmbedderContext<EmbedderTestContextVulkan>() {
+    return reinterpret_cast<EmbedderTestContextVulkan&>(GetVulkanContext());
+  }
+
+ protected:
+  // We return the base class here and reinterpret_cast in the template
+  // specializations because we're using forward declarations rather than
+  // including the headers directly, and thus the relationship between the base
+  // class and subclasses is unknown to the compiler here. We avoid including
+  // the headers directly because the Metal headers include Objective-C types,
+  // and thus cannot be included in pure C++ translation units.
+  EmbedderTestContext& GetGLContext();
+  EmbedderTestContext& GetMetalContext();
+  EmbedderTestContext& GetSoftwareContext();
+  EmbedderTestContext& GetVulkanContext();
+
+  std::unique_ptr<EmbedderTestContext> gl_context_;
+  std::unique_ptr<EmbedderTestContext> metal_context_;
+  std::unique_ptr<EmbedderTestContext> software_context_;
+  std::unique_ptr<EmbedderTestContext> vulkan_context_;
 
   FML_DISALLOW_COPY_AND_ASSIGN(EmbedderTest);
 };
 
 class EmbedderTestMultiBackend
     : public EmbedderTest,
-      public ::testing::WithParamInterface<EmbedderTestContextType> {};
+      public ::testing::WithParamInterface<EmbedderTestContextType> {
+ public:
+  EmbedderTestContext& GetEmbedderContext(EmbedderTestContextType type);
+};
 
 }  // namespace flutter::testing
 
