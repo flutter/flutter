56686525b2715e9e4d3c696d70b52220a4a3742b
Robert Ancell <robert.ancell@canonical.com>
2024-11-21 09:32:20 +1300

    Split channel messaging out of handlers (flutter/engine#56667)
    Split the channel messaging out of the system channels to make them
    simpler to understand and refactor.
    
    The new channel classes could be automatically generated in a second
    phase, e.g. using Pigeon to reduce code usage.
    
    The new classes don't have tests as they will already be covered by the
    existing code.

--- engine/src/flutter/shell/platform/linux/fl_keyboard_handler.cc
+++ engine/src/flutter/shell/platform/linux/fl_keyboard_handler.cc
@@ -4,11 +4,7 @@
 
 #include "flutter/shell/platform/linux/fl_keyboard_handler.h"
 
-#include "flutter/shell/platform/linux/public/flutter_linux/fl_method_channel.h"
-#include "flutter/shell/platform/linux/public/flutter_linux/fl_standard_method_codec.h"
-
-static constexpr char kChannelName[] = "flutter/keyboard";
-static constexpr char kGetKeyboardStateMethod[] = "getKeyboardState";
+#include "flutter/shell/platform/linux/fl_keyboard_channel.h"
 
 struct _FlKeyboardHandler {
   GObject parent_instance;
@@ -16,14 +12,16 @@ struct _FlKeyboardHandler {
   FlKeyboardManager* keyboard_manager;
 
   // The channel used by the framework to query the keyboard pressed state.
-  FlMethodChannel* channel;
+  FlKeyboardChannel* channel;
 };
 
 G_DEFINE_TYPE(FlKeyboardHandler, fl_keyboard_handler, G_TYPE_OBJECT);
 
 // Returns the keyboard pressed state.
-static FlMethodResponse* get_keyboard_state(FlKeyboardHandler* self) {
-  g_autoptr(FlValue) result = fl_value_new_map();
+static FlValue* get_keyboard_state(gpointer user_data) {
+  FlKeyboardHandler* self = FL_KEYBOARD_HANDLER(user_data);
+
+  FlValue* result = fl_value_new_map();
 
   GHashTable* pressing_records =
       fl_keyboard_manager_get_pressed_state(self->keyboard_manager);
@@ -39,28 +37,8 @@ static FlMethodResponse* get_keyboard_state(FlKeyboardHandler* self) {
                           fl_value_new_int(logical_key));
       },
       result);
-  return FL_METHOD_RESPONSE(fl_method_success_response_new(result));
-}
 
-// Called when a method call on flutter/keyboard is received from Flutter.
-static void method_call_handler(FlMethodChannel* channel,
-                                FlMethodCall* method_call,
-                                gpointer user_data) {
-  FlKeyboardHandler* self = FL_KEYBOARD_HANDLER(user_data);
-
-  const gchar* method = fl_method_call_get_name(method_call);
-
-  g_autoptr(FlMethodResponse) response = nullptr;
-  if (strcmp(method, kGetKeyboardStateMethod) == 0) {
-    response = get_keyboard_state(self);
-  } else {
-    response = FL_METHOD_RESPONSE(fl_method_not_implemented_response_new());
-  }
-
-  g_autoptr(GError) error = nullptr;
-  if (!fl_method_call_respond(method_call, response, &error)) {
-    g_warning("Failed to send method call response: %s", error->message);
-  }
+  return result;
 }
 
 static void fl_keyboard_handler_dispose(GObject* object) {
@@ -76,6 +54,9 @@ static void fl_keyboard_handler_class_init(FlKeyboardHandlerClass* klass) {
   G_OBJECT_CLASS(klass)->dispose = fl_keyboard_handler_dispose;
 }
 
+static FlKeyboardChannelVTable keyboard_channel_vtable = {
+    .get_keyboard_state = get_keyboard_state};
+
 static void fl_keyboard_handler_init(FlKeyboardHandler* self) {}
 
 FlKeyboardHandler* fl_keyboard_handler_new(
@@ -87,10 +68,7 @@ FlKeyboardHandler* fl_keyboard_handler_new(
   self->keyboard_manager = FL_KEYBOARD_MANAGER(g_object_ref(keyboard_manager));
 
   // Setup the flutter/keyboard channel.
-  g_autoptr(FlStandardMethodCodec) codec = fl_standard_method_codec_new();
   self->channel =
-      fl_method_channel_new(messenger, kChannelName, FL_METHOD_CODEC(codec));
-  fl_method_channel_set_method_call_handler(self->channel, method_call_handler,
-                                            self, nullptr);
+      fl_keyboard_channel_new(messenger, &keyboard_channel_vtable, self);
   return self;
 }
--- engine/src/flutter/shell/platform/linux/fl_platform_handler.cc
+++ engine/src/flutter/shell/platform/linux/fl_platform_handler.cc
@@ -7,33 +7,12 @@
 #include <gtk/gtk.h>
 #include <cstring>
 
-#include "flutter/shell/platform/linux/public/flutter_linux/fl_json_method_codec.h"
+#include "flutter/shell/platform/linux/fl_platform_channel.h"
 #include "flutter/shell/platform/linux/public/flutter_linux/fl_method_channel.h"
 
-static constexpr char kChannelName[] = "flutter/platform";
-static constexpr char kBadArgumentsError[] = "Bad Arguments";
+static constexpr char kInProgressError[] = "In Progress";
 static constexpr char kUnknownClipboardFormatError[] =
     "Unknown Clipboard Format";
-static constexpr char kInProgressError[] = "In Progress";
-static constexpr char kGetClipboardDataMethod[] = "Clipboard.getData";
-static constexpr char kSetClipboardDataMethod[] = "Clipboard.setData";
-static constexpr char kClipboardHasStringsMethod[] = "Clipboard.hasStrings";
-static constexpr char kExitApplicationMethod[] = "System.exitApplication";
-static constexpr char kRequestAppExitMethod[] = "System.requestAppExit";
-static constexpr char kInitializationCompleteMethod[] =
-    "System.initializationComplete";
-static constexpr char kPlaySoundMethod[] = "SystemSound.play";
-static constexpr char kSystemNavigatorPopMethod[] = "SystemNavigator.pop";
-static constexpr char kTextKey[] = "text";
-static constexpr char kValueKey[] = "value";
-
-static constexpr char kExitTypeKey[] = "type";
-static constexpr char kExitTypeCancelable[] = "cancelable";
-static constexpr char kExitTypeRequired[] = "required";
-
-static constexpr char kExitResponseKey[] = "response";
-static constexpr char kExitResponseCancel[] = "cancel";
-static constexpr char kExitResponseExit[] = "exit";
 
 static constexpr char kTextPlainFormat[] = "text/plain";
 
@@ -43,38 +22,23 @@ static constexpr char kSoundTypeClick[] = "SystemSoundType.click";
 struct _FlPlatformHandler {
   GObject parent_instance;
 
-  FlMethodChannel* channel;
+  FlPlatformChannel* channel;
+
   FlMethodCall* exit_application_method_call;
-  GCancellable* cancellable;
+
   bool app_initialization_complete;
+
+  GCancellable* cancellable;
 };
 
 G_DEFINE_TYPE(FlPlatformHandler, fl_platform_handler, G_TYPE_OBJECT)
-
-// Sends the method call response to Flutter.
-static void send_response(FlMethodCall* method_call,
-                          FlMethodResponse* response) {
-  g_autoptr(GError) error = nullptr;
-  if (!fl_method_call_respond(method_call, response, &error)) {
-    g_warning("Failed to send method call response: %s", error->message);
-  }
-}
 
 // Called when clipboard text received.
 static void clipboard_text_cb(GtkClipboard* clipboard,
                               const gchar* text,
                               gpointer user_data) {
   g_autoptr(FlMethodCall) method_call = FL_METHOD_CALL(user_data);
-
-  g_autoptr(FlValue) result = nullptr;
-  if (text != nullptr) {
-    result = fl_value_new_map();
-    fl_value_set_string_take(result, kTextKey, fl_value_new_string(text));
-  }
-
-  g_autoptr(FlMethodResponse) response =
-      FL_METHOD_RESPONSE(fl_method_success_response_new(result));
-  send_response(method_call, response);
+  fl_platform_channel_respond_clipboard_get_data(method_call, text);
 }
 
 // Called when clipboard text received during has_strings.
@@ -82,50 +46,25 @@ static void clipboard_text_has_strings_cb(GtkClipboard* clipboard,
                                           const gchar* text,
                                           gpointer user_data) {
   g_autoptr(FlMethodCall) method_call = FL_METHOD_CALL(user_data);
-
-  g_autoptr(FlValue) result = fl_value_new_map();
-  fl_value_set_string_take(
-      result, kValueKey,
-      fl_value_new_bool(text != nullptr && strlen(text) > 0));
-
-  g_autoptr(FlMethodResponse) response =
-      FL_METHOD_RESPONSE(fl_method_success_response_new(result));
-  send_response(method_call, response);
+  fl_platform_channel_respond_clipboard_has_strings(
+      method_call, text != nullptr && strlen(text) > 0);
 }
 
 // Called when Flutter wants to copy to the clipboard.
-static FlMethodResponse* clipboard_set_data(FlPlatformHandler* self,
-                                            FlValue* args) {
-  if (fl_value_get_type(args) != FL_VALUE_TYPE_MAP) {
-    return FL_METHOD_RESPONSE(fl_method_error_response_new(
-        kBadArgumentsError, "Argument map missing or malformed", nullptr));
-  }
-
-  FlValue* text_value = fl_value_lookup_string(args, kTextKey);
-  if (text_value == nullptr ||
-      fl_value_get_type(text_value) != FL_VALUE_TYPE_STRING) {
-    return FL_METHOD_RESPONSE(fl_method_error_response_new(
-        kBadArgumentsError, "Missing clipboard text", nullptr));
-  }
-
+static FlMethodResponse* clipboard_set_data(FlMethodCall* method_call,
+                                            const gchar* text,
+                                            gpointer user_data) {
   GtkClipboard* clipboard =
       gtk_clipboard_get_default(gdk_display_get_default());
-  gtk_clipboard_set_text(clipboard, fl_value_get_string(text_value), -1);
+  gtk_clipboard_set_text(clipboard, text, -1);
 
   return FL_METHOD_RESPONSE(fl_method_success_response_new(nullptr));
 }
 
 // Called when Flutter wants to paste from the clipboard.
-static FlMethodResponse* clipboard_get_data_async(FlPlatformHandler* self,
-                                                  FlMethodCall* method_call) {
-  FlValue* args = fl_method_call_get_args(method_call);
-
-  if (fl_value_get_type(args) != FL_VALUE_TYPE_STRING) {
-    return FL_METHOD_RESPONSE(fl_method_error_response_new(
-        kBadArgumentsError, "Expected string", nullptr));
-  }
-
-  const gchar* format = fl_value_get_string(args);
+static FlMethodResponse* clipboard_get_data(FlMethodCall* method_call,
+                                            const gchar* format,
+                                            gpointer user_data) {
   if (strcmp(format, kTextPlainFormat) != 0) {
     return FL_METHOD_RESPONSE(fl_method_error_response_new(
         kUnknownClipboardFormatError, "GTK clipboard API only supports text",
@@ -143,9 +82,8 @@ static FlMethodResponse* clipboard_get_data_async(FlPlatformHandler* self,
 
 // Called when Flutter wants to know if the content of the clipboard is able to
 // be pasted, without actually accessing the clipboard content itself.
-static FlMethodResponse* clipboard_has_strings_async(
-    FlPlatformHandler* self,
-    FlMethodCall* method_call) {
+static FlMethodResponse* clipboard_has_strings(FlMethodCall* method_call,
+                                               gpointer user_data) {
   GtkClipboard* clipboard =
       gtk_clipboard_get_default(gdk_display_get_default());
   gtk_clipboard_request_text(clipboard, clipboard_text_has_strings_cb,
@@ -153,31 +91,6 @@ static FlMethodResponse* clipboard_has_strings_async(
 
   // Will respond later.
   return nullptr;
-}
-
-// Get the exit response from a System.requestAppExit method call.
-static gchar* get_exit_response(FlMethodResponse* response) {
-  if (response == nullptr) {
-    return nullptr;
-  }
-
-  g_autoptr(GError) error = nullptr;
-  FlValue* result = fl_method_response_get_result(response, &error);
-  if (result == nullptr) {
-    g_warning("Error returned from System.requestAppExit: %s", error->message);
-    return nullptr;
-  }
-  if (fl_value_get_type(result) != FL_VALUE_TYPE_MAP) {
-    g_warning("System.requestAppExit result argument map missing or malformed");
-    return nullptr;
-  }
-
-  FlValue* response_value = fl_value_lookup_string(result, kExitResponseKey);
-  if (fl_value_get_type(response_value) != FL_VALUE_TYPE_STRING) {
-    g_warning("Invalid response from System.requestAppExit");
-    return nullptr;
-  }
-  return g_strdup(fl_value_get_string(response_value));
 }
 
 // Quit this application
@@ -212,88 +125,56 @@ static void request_app_exit_response_cb(GObject* object,
   FlPlatformHandler* self = FL_PLATFORM_HANDLER(user_data);
 
   g_autoptr(GError) error = nullptr;
-  g_autoptr(FlMethodResponse) method_response =
-      fl_method_channel_invoke_method_finish(FL_METHOD_CHANNEL(object), result,
-                                             &error);
-  g_autofree gchar* exit_response = nullptr;
-  if (method_response == nullptr) {
+  FlPlatformChannelExitResponse exit_response;
+  if (!fl_platform_channel_system_request_app_exit_finish(
+          object, result, &exit_response, &error)) {
     if (g_error_matches(error, G_IO_ERROR, G_IO_ERROR_CANCELLED)) {
       return;
     }
     g_warning("Failed to complete System.requestAppExit: %s", error->message);
-  } else {
-    exit_response = get_exit_response(method_response);
-  }
-  // If something went wrong, then just exit.
-  if (exit_response == nullptr) {
-    exit_response = g_strdup(kExitResponseExit);
+    quit_application();
+    return;
   }
 
-  if (g_str_equal(exit_response, kExitResponseExit)) {
+  if (exit_response == FL_PLATFORM_CHANNEL_EXIT_RESPONSE_EXIT) {
     quit_application();
-  } else if (g_str_equal(exit_response, kExitResponseCancel)) {
-    // Canceled - no action to take.
   }
 
   // If request was due to a request from Flutter, pass result back.
   if (self->exit_application_method_call != nullptr) {
-    g_autoptr(FlValue) exit_result = fl_value_new_map();
-    fl_value_set_string_take(exit_result, kExitResponseKey,
-                             fl_value_new_string(exit_response));
-    g_autoptr(FlMethodResponse) exit_response =
-        FL_METHOD_RESPONSE(fl_method_success_response_new(exit_result));
-    if (!fl_method_call_respond(self->exit_application_method_call,
-                                exit_response, &error)) {
-      g_warning("Failed to send response to System.exitApplication: %s",
-                error->message);
-    }
-    g_clear_object(&self->exit_application_method_call);
+    fl_platform_channel_respond_system_exit_application(
+        self->exit_application_method_call, exit_response);
   }
 }
 
 // Send a request to Flutter to exit the application, but only if it's ready for
 // a request.
-static void request_app_exit(FlPlatformHandler* self, const char* type) {
-  g_autoptr(FlValue) args = fl_value_new_map();
+static void request_app_exit(FlPlatformHandler* self,
+                             FlPlatformChannelExitType type) {
   if (!self->app_initialization_complete ||
-      g_str_equal(type, kExitTypeRequired)) {
+      type == FL_PLATFORM_CHANNEL_EXIT_TYPE_REQUIRED) {
     quit_application();
     return;
   }
 
-  fl_value_set_string_take(args, kExitTypeKey, fl_value_new_string(type));
-  fl_method_channel_invoke_method(self->channel, kRequestAppExitMethod, args,
-                                  self->cancellable,
-                                  request_app_exit_response_cb, self);
+  fl_platform_channel_system_request_app_exit(
+      self->channel, type, self->cancellable, request_app_exit_response_cb,
+      self);
 }
 
 // Called when the Dart app has finished initialization and is ready to handle
 // requests. For the Flutter framework, this means after the ServicesBinding has
 // been initialized and it sends a System.initializationComplete message.
-static FlMethodResponse* system_intitialization_complete(
-    FlPlatformHandler* self,
-    FlMethodCall* method_call) {
+static void system_initialization_complete(gpointer user_data) {
+  FlPlatformHandler* self = FL_PLATFORM_HANDLER(user_data);
   self->app_initialization_complete = TRUE;
-  return FL_METHOD_RESPONSE(fl_method_success_response_new(nullptr));
 }
 
 // Called when Flutter wants to exit the application.
-static FlMethodResponse* system_exit_application(FlPlatformHandler* self,
-                                                 FlMethodCall* method_call) {
-  FlValue* args = fl_method_call_get_args(method_call);
-  if (fl_value_get_type(args) != FL_VALUE_TYPE_MAP) {
-    return FL_METHOD_RESPONSE(fl_method_error_response_new(
-        kBadArgumentsError, "Argument map missing or malformed", nullptr));
-  }
-
-  FlValue* type_value = fl_value_lookup_string(args, kExitTypeKey);
-  if (type_value == nullptr ||
-      fl_value_get_type(type_value) != FL_VALUE_TYPE_STRING) {
-    return FL_METHOD_RESPONSE(fl_method_error_response_new(
-        kBadArgumentsError, "Missing type argument", nullptr));
-  }
-  const char* type = fl_value_get_string(type_value);
-
+static FlMethodResponse* system_exit_application(FlMethodCall* method_call,
+                                                 FlPlatformChannelExitType type,
+                                                 gpointer user_data) {
+  FlPlatformHandler* self = FL_PLATFORM_HANDLER(user_data);
   // Save method call to respond to when our request to Flutter completes.
   if (self->exit_application_method_call != nullptr) {
     return FL_METHOD_RESPONSE(fl_method_error_response_new(
@@ -305,12 +186,10 @@ static FlMethodResponse* system_exit_application(FlPlatformHandler* self,
   // Requested to immediately quit if the app hasn't yet signaled that it is
   // ready to handle requests, or if the type of exit requested is "required".
   if (!self->app_initialization_complete ||
-      g_str_equal(type, kExitTypeRequired)) {
+      type == FL_PLATFORM_CHANNEL_EXIT_TYPE_REQUIRED) {
     quit_application();
-    g_autoptr(FlValue) exit_result = fl_value_new_map();
-    fl_value_set_string_take(exit_result, kExitResponseKey,
-                             fl_value_new_string(kExitResponseExit));
-    return FL_METHOD_RESPONSE(fl_method_success_response_new(exit_result));
+    return fl_platform_channel_make_system_request_app_exit_response(
+        FL_PLATFORM_CHANNEL_EXIT_RESPONSE_EXIT);
   }
 
   // Send the request back to Flutter to follow the standard process.
@@ -321,14 +200,7 @@ static FlMethodResponse* system_exit_application(FlPlatformHandler* self,
 }
 
 // Called when Flutter wants to play a sound.
-static FlMethodResponse* system_sound_play(FlPlatformHandler* self,
-                                           FlValue* args) {
-  if (fl_value_get_type(args) != FL_VALUE_TYPE_STRING) {
-    return FL_METHOD_RESPONSE(fl_method_error_response_new(
-        kBadArgumentsError, "Expected string", nullptr));
-  }
-
-  const gchar* type = fl_value_get_string(args);
+static void system_sound_play(const gchar* type, gpointer user_data) {
   if (strcmp(type, kSoundTypeAlert) == 0) {
     GdkDisplay* display = gdk_display_get_default();
     if (display != nullptr) {
@@ -339,47 +211,11 @@ static FlMethodResponse* system_sound_play(FlPlatformHandler* self,
   } else {
     g_warning("Ignoring unknown sound type %s in SystemSound.play.\n", type);
   }
-
-  return FL_METHOD_RESPONSE(fl_method_success_response_new(nullptr));
 }
 
 // Called when Flutter wants to quit the application.
-static FlMethodResponse* system_navigator_pop(FlPlatformHandler* self) {
+static void system_navigator_pop(gpointer user_data) {
   quit_application();
-  return FL_METHOD_RESPONSE(fl_method_success_response_new(nullptr));
-}
-
-// Called when a method call is received from Flutter.
-static void method_call_cb(FlMethodChannel* channel,
-                           FlMethodCall* method_call,
-                           gpointer user_data) {
-  FlPlatformHandler* self = FL_PLATFORM_HANDLER(user_data);
-
-  const gchar* method = fl_method_call_get_name(method_call);
-  FlValue* args = fl_method_call_get_args(method_call);
-
-  g_autoptr(FlMethodResponse) response = nullptr;
-  if (strcmp(method, kSetClipboardDataMethod) == 0) {
-    response = clipboard_set_data(self, args);
-  } else if (strcmp(method, kGetClipboardDataMethod) == 0) {
-    response = clipboard_get_data_async(self, method_call);
-  } else if (strcmp(method, kClipboardHasStringsMethod) == 0) {
-    response = clipboard_has_strings_async(self, method_call);
-  } else if (strcmp(method, kExitApplicationMethod) == 0) {
-    response = system_exit_application(self, method_call);
-  } else if (strcmp(method, kInitializationCompleteMethod) == 0) {
-    response = system_intitialization_complete(self, method_call);
-  } else if (strcmp(method, kPlaySoundMethod) == 0) {
-    response = system_sound_play(self, args);
-  } else if (strcmp(method, kSystemNavigatorPopMethod) == 0) {
-    response = system_navigator_pop(self);
-  } else {
-    response = FL_METHOD_RESPONSE(fl_method_not_implemented_response_new());
-  }
-
-  if (response != nullptr) {
-    send_response(method_call, response);
-  }
 }
 
 static void fl_platform_handler_dispose(GObject* object) {
@@ -402,17 +238,24 @@ static void fl_platform_handler_init(FlPlatformHandler* self) {
   self->cancellable = g_cancellable_new();
 }
 
+static FlPlatformChannelVTable platform_channel_vtable = {
+    .clipboard_set_data = clipboard_set_data,
+    .clipboard_get_data = clipboard_get_data,
+    .clipboard_has_strings = clipboard_has_strings,
+    .system_exit_application = system_exit_application,
+    .system_initialization_complete = system_initialization_complete,
+    .system_sound_play = system_sound_play,
+    .system_navigator_pop = system_navigator_pop,
+};
+
 FlPlatformHandler* fl_platform_handler_new(FlBinaryMessenger* messenger) {
   g_return_val_if_fail(FL_IS_BINARY_MESSENGER(messenger), nullptr);
 
   FlPlatformHandler* self = FL_PLATFORM_HANDLER(
       g_object_new(fl_platform_handler_get_type(), nullptr));
 
-  g_autoptr(FlJsonMethodCodec) codec = fl_json_method_codec_new();
   self->channel =
-      fl_method_channel_new(messenger, kChannelName, FL_METHOD_CODEC(codec));
-  fl_method_channel_set_method_call_handler(self->channel, method_call_cb, self,
-                                            nullptr);
+      fl_platform_channel_new(messenger, &platform_channel_vtable, self);
   self->app_initialization_complete = FALSE;
 
   return self;
@@ -421,5 +264,5 @@ FlPlatformHandler* fl_platform_handler_new(FlBinaryMessenger* messenger) {
 void fl_platform_handler_request_app_exit(FlPlatformHandler* self) {
   g_return_if_fail(FL_IS_PLATFORM_HANDLER(self));
   // Request a cancellable exit.
-  request_app_exit(self, kExitTypeCancelable);
+  request_app_exit(self, FL_PLATFORM_CHANNEL_EXIT_TYPE_CANCELABLE);
 }
