ad3df910071660a475337a763167a37270e97322
Qun Cheng <chengqunq@gmail.com>
2024-11-20 12:26:52 -0800

    Add example explanation

--- examples/api/lib/material/page_transitions_theme/page_transitions_theme.3.dart
+++ examples/api/lib/material/page_transitions_theme/page_transitions_theme.3.dart
@@ -4,7 +4,11 @@
 
 import 'package:flutter/material.dart';
 
-/// Flutter code sample for [PageTransitionsTheme].
+/// Flutter code sample for the default Android U page transition theme
+/// [FadeForwardsPageTransitionsBuilder]. Tapping each list tile navigates to
+/// a second page, which slides in from right to left while fading in.
+/// Simultaneously, the first page slides out in the same direction while
+/// fading out.
 
 void main() => runApp(const PageTransitionsThemeApp());
 
