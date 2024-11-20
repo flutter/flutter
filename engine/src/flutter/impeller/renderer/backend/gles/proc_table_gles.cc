ba6208b594ae8eadede6ebeaab91da53b24dec24
Jason Simmons <jason-simmons@users.noreply.github.com>
2024-11-20 12:26:04 -0800

    Revert "Added assert for opengles thread safety (#56585)" (flutter/engine#56730)
    This reverts commit cc3305f94cc491fade890e2a93b4b95fdb7ee38a.
    
    The implementation of the assert assumes that there is a single raster thread ID that will remain constant throughout the lifetime of the process.  That is not true for scenarios like recreating the engine after suspending and resuming an Android app, or instantiating multiple engines within one process.

--- engine/src/flutter/impeller/renderer/backend/gles/proc_table_gles.cc
+++ engine/src/flutter/impeller/renderer/backend/gles/proc_table_gles.cc
@@ -144,10 +144,6 @@ ProcTableGLES::ProcTableGLES(  // NOLINT(google-readability-function-size)
 
   capabilities_ = std::make_shared<CapabilitiesGLES>(*this);
 
-  // This this will force glUseProgram to only be used on one thread in debug
-  // builds to identify threading violations in the engine.
-  UseProgram.enforce_one_thread = true;
-
   is_valid_ = true;
 }
 
