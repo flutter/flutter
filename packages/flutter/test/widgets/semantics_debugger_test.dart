60bf8603082e9d073e7bfd5482b352edfbb4b28d
Chun-Heng Tai <chtai@google.com>
2024-11-20 10:15:57 -0800

    fix import

--- packages/flutter/test/widgets/semantics_debugger_test.dart
+++ packages/flutter/test/widgets/semantics_debugger_test.dart
@@ -4,7 +4,6 @@
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
