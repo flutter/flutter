8aabbdf368f03adc6718d781421b11f53c03d9f8
Jonah Williams <jonahwilliams@google.com>
2024-11-22 13:10:00 -0800

    [Impeller] delete Impeller sim opt out. (flutter/engine#56706)
    Impeller only on simulators.

--- engine/src/flutter/common/settings.h
+++ engine/src/flutter/common/settings.h
@@ -226,10 +226,10 @@ struct Settings {
 #if FML_OS_ANDROID || FML_OS_IOS || FML_OS_IOS_SIMULATOR
   // On iOS devices, Impeller is the default with no opt-out and this field is
   // const.
-#if FML_OS_IOS && !FML_OS_IOS_SIMULATOR
+#if FML_OS_IOS || FML_OS_IOS_SIMULATOR
   static constexpr const
-#endif  // FML_OS_IOS && !FML_OS_IOS_SIMULATOR
-      bool enable_impeller = true;
+#endif                              // FML_OS_IOS && !FML_OS_IOS_SIMULATOR
+      bool enable_impeller = true;  // NOLINT(readability-identifier-naming)
 #else
   bool enable_impeller = false;
 #endif
