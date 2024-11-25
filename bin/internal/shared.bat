ac316f206c51f8db4856f5b3f2527e043c0315cf
Ben Konyi <bkonyi@google.com>
2024-11-25 11:27:54 -0500

    [ Tool ] Fix "Error: Unable to find git in your PATH" when Command Processor AutoRun registry key is defined
    cmd.exe will read from the AutoRun registry key at launch and execute
    the commands listed in the key. Since `FOR /F IN (command) ...` causes a
    new terminal instance to start, the AutoRun commands will be run again,
    possibly changing the current working directory. In this case, the `git
    rev-parse HEAD` check run in `shared.bat` could fail, which we were
    assuming meant `git` was not on the user's PATH.
    
    This change ensures that `git rev-parse HEAD` will always run in
    %FLUTTER_ROOT% and explicitly adds a separate check for `git` using
    `WHERE git` to more accurately determine if `git` is on the PATH.
    
    Fixes https://github.com/flutter/flutter/issues/159018

--- bin/internal/shared.bat
+++ bin/internal/shared.bat
@@ -52,19 +52,23 @@ GOTO :after_subroutine
     CALL "%bootstrap_path%"
   )
 
-  REM Check that git exists and get the revision
-  SET git_exists=false
-  2>NUL (
-    PUSHD "%flutter_root%"
-    FOR /f %%r IN ('git rev-parse HEAD') DO (
-      SET git_exists=true
-      SET revision=%%r
-    )
-    POPD
+  REM Check that git exists and get the revision.
+  WHERE git >NUL 2>&1
+  IF "%ERRORLEVEL%" NEQ "0" (
+    REM Could not find git. Exit without /B to avoid retrying.
+    ECHO Error: Unable to find git in your PATH. && EXIT 1
   )
-  REM If git didn't execute we don't have git. Exit without /B to avoid retrying.
-  if %git_exists% == false echo Error: Unable to find git in your PATH. && EXIT 1
-  SET compilekey="%revision%:%FLUTTER_TOOL_ARGS%"
+  2>NUL (
+    REM 'FOR /f' spawns a new terminal instance to run the command, so we need
+    REM to 'PUSHD %FLUTTER_ROOT%' before getting the git revision. If an
+    REM 'AutoRun' command is defined in the user's registry, we might not be in
+    REM the directory we expect to be in.
+    REM
+    REM See https://github.com/flutter/flutter/issues/159018
+    FOR /f %%r IN ('PUSHD %FLUTTER_ROOT% ^& $git rev-parse HEAD') DO (
+      SET compilekey="%%r:%FLUTTER_TOOL_ARGS%"
+    )
+  )
 
   REM Invalidate cache if:
   REM  * SNAPSHOT_PATH is not a file, or
