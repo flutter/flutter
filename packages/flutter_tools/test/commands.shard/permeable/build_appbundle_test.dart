77bde3c0a57f8ccd91fedfed4e06b5435a39cc3e
Matan Lurey <matanlurey@users.noreply.github.com>
2024-11-26 13:01:20 -0800

    Report usage of `deferred-components` to analytics. (#159307)
    Towards https://github.com/flutter/flutter/issues/159212.
    
    /cc @jtmcdole
    
    ---------
    
    Co-authored-by: Andrew Kolos <andrewrkolos@gmail.com>

--- packages/flutter_tools/test/commands.shard/permeable/build_appbundle_test.dart
+++ packages/flutter_tools/test/commands.shard/permeable/build_appbundle_test.dart
@@ -148,6 +148,49 @@ void main() {
       Analytics: () => fakeAnalytics,
       ProcessInfo: () => processInfo,
     });
+
+    testUsingContext('use of the deferred components feature sends a build info event indicating so', () async {
+      final String projectPath = await createProject(
+        tempDir,
+        arguments: <String>[
+          '--empty',
+          '--no-pub',
+          '--template=app',
+        ],
+      );
+
+      // Add deferred manifest.
+      final File pubspec = globals.localFileSystem
+        .directory(projectPath)
+        .childFile('pubspec.yaml');
+      final String modifiedContents = pubspec
+        .readAsStringSync()
+        .replaceAll('flutter:', 'flutter:\n  deferred-components:');
+      pubspec.writeAsStringSync(modifiedContents);
+      printOnFailure(pubspec.readAsStringSync());
+
+      final Directory oldCwd = globals.localFileSystem.currentDirectory;
+      try {
+        globals.localFileSystem.currentDirectory = globals.localFileSystem.directory(projectPath);
+        await runBuildAppBundleCommand(projectPath);
+      } finally {
+        globals.localFileSystem.currentDirectory = oldCwd;
+      }
+
+      expect(
+        fakeAnalytics.sentEvents,
+        contains(
+          Event.flutterBuildInfo(
+            label: 'build-appbundle-deferred-components',
+            buildType: 'android',
+          ),
+        ),
+      );
+    }, overrides: <Type, Generator>{
+      AndroidBuilder: () => FakeAndroidBuilder(),
+      Analytics: () => fakeAnalytics,
+      ProcessInfo: () => processInfo,
+    });
   });
 
   group('Gradle', () {
