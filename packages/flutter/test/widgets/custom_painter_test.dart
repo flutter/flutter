6e92331520d328a64a9b3f106b0c9c57b414bc57
LongCatIsLooong <31859944+LongCatIsLooong@users.noreply.github.com>
2024-11-27 09:55:59 -0800

    Update custom_painter_test.dart

--- packages/flutter/test/widgets/custom_painter_test.dart
+++ packages/flutter/test/widgets/custom_painter_test.dart
@@ -356,7 +356,7 @@ void _defineTests() {
     final Set<SemanticsAction> allActions = SemanticsAction.values.toSet()
       ..remove(SemanticsAction.customAction) // customAction is not user-exposed.
       ..remove(SemanticsAction.showOnScreen) // showOnScreen is not user-exposed
-      // TODO(LongCatIsLooong): clean up after `SemanticsAction.scrollToOffset` is added to dart:ui.
+      // TODO(LongCatIsLooong): change to `SemanticsAction.scrollToOffset` when available.
       // https://github.com/flutter/flutter/issues/159515.
       ..removeWhere((SemanticsAction action) => action.index == 1 << 23);
 
@@ -381,7 +381,7 @@ void _defineTests() {
     final SemanticsOwner semanticsOwner = tester.binding.pipelineOwner.semanticsOwner!;
     int expectedLength = 1;
     for (final SemanticsAction action in allActions) {
-      // TODO(LongCatIsLooong): clean up after `SemanticsAction.scrollToOffset` is added to dart:ui.
+      // TODO(LongCatIsLooong): remove after `SemanticsAction.scrollToOffset` is added to dart:ui.
       // https://github.com/flutter/flutter/issues/159515.
       // ignore: exhaustive_cases
       switch (action) {
