e456810117d81c4f8e6bda00b825ae99160a3067
Mohellebi abdessalem <mohellebiabdessalem@gmail.com>
2024-11-29 09:12:29 +0100

    Update deferred_components_project.dart

--- packages/flutter_tools/test/integration.shard/test_data/deferred_components_project.dart
+++ packages/flutter_tools/test/integration.shard/test_data/deferred_components_project.dart
@@ -101,7 +101,7 @@ localPropertiesFile.withReader("UTF-8") { reader -> properties.load(reader) }
 
 def flutterSdkPath = properties.getProperty("flutter.sdk")
 assert flutterSdkPath != null, "flutter.sdk not set in local.properties"
-apply from: "$flutterSdkPath/packages/flutter_tools/gradle/app_plugin_loader.gradle"
+apply from: "$flutterSdkPath/packages/flutter_tools/gradle/app_plugin_loader.gradle.kts"
 ''';
 
 @override
