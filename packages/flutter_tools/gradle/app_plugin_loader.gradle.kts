00cc87a552455612b003192695ac6411c8cde071
Mohellebi abdessalem <mohellebiabdessalem@gmail.com>
2024-11-29 11:33:10 +0100

    Update app_plugin_loader.gradle.kts

--- packages/flutter_tools/gradle/app_plugin_loader.gradle.kts
+++ packages/flutter_tools/gradle/app_plugin_loader.gradle.kts
@@ -2,16 +2,15 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// This file exists solely for compatibility with projects that have
+// This file exists solely for the compatibility with projects that have
 // not migrated to the declarative apply of the Flutter App Plugin Loader Gradle Plugin.
-println(
-    """
-    You are applying Flutter's app_plugin_loader Gradle plugin 
-    imperatively using the apply script method, which is deprecated and will be 
-    removed in a future release. Migrate to applying Gradle plugins with the 
-    declarative plugins block: https://flutter.dev/to/flutter-gradle-plugin-apply
-    """.trimIndent()
+
+logger.error(
+    "You are applying Flutter's app_plugin_loader Gradle plugin " +
+    "imperatively using the apply script method, which is deprecated and will be " +
+    "removed in a future release. Migrate to applying Gradle plugins with the " +
+    "declarative plugins block: https://flutter.dev/to/flutter-gradle-plugin-apply\n"
 )
 
-val pathToThisDirectory = buildscript.sourceFile?.parentFile
-apply(from = "${pathToThisDirectory}/src/main/groovy/app_plugin_loader.groovy")
+val pathToThisDirectory = buildscript.sourceFile.parentFile
+apply(from = "$pathToThisDirectory/src/main/groovy/app_plugin_loader.groovy")
