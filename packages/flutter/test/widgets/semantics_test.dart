1460c429f721c77c41d7e7035e78f9dbae7a0f13
LongCatIsLooong <31859944+LongCatIsLooong@users.noreply.github.com>
2024-12-02 13:48:58 -0800

    Ignore exhaustiveness check for some semantics tests, for now (#159517)
    ~This also includes the engine hash change from
    https://github.com/flutter/flutter/pull/159453/files for testing
    purposes. Will revert if all tests pass~ all tests passed with the
    engine hash from #159453:
    https://github.com/flutter/flutter/pull/159517/commits/0f26acb438024948ddf5ea2cc7a17d1ed9f15193
    
    TODOs: https://github.com/flutter/flutter/issues/159515
    context: https://github.com/flutter/flutter/issues/159456
    
    
    ## Pre-launch Checklist
    
    - [ ] I read the [Contributor Guide] and followed the process outlined
    there for submitting PRs.
    - [ ] I read the [Tree Hygiene] wiki page, which explains my
    responsibilities.
    - [ ] I read and followed the [Flutter Style Guide], including [Features
    we expect every widget to implement].
    - [ ] I signed the [CLA].
    - [ ] I listed at least one issue that this PR fixes in the description
    above.
    - [ ] I updated/added relevant documentation (doc comments with `///`).
    - [ ] I added new tests to check the change I am making, or this PR is
    [test-exempt].
    - [ ] I followed the [breaking change policy] and added [Data Driven
    Fixes] where supported.
    - [ ] All existing and new tests are passing.
    
    If you need help, consider asking for advice on the #hackers-new channel
    on [Discord].
    
    <!-- Links -->
    [Contributor Guide]:
    https://github.com/flutter/flutter/blob/main/docs/contributing/Tree-hygiene.md#overview
    [Tree Hygiene]:
    https://github.com/flutter/flutter/blob/main/docs/contributing/Tree-hygiene.md
    [test-exempt]:
    https://github.com/flutter/flutter/blob/main/docs/contributing/Tree-hygiene.md#tests
    [Flutter Style Guide]:
    https://github.com/flutter/flutter/blob/main/docs/contributing/Style-guide-for-Flutter-repo.md
    [Features we expect every widget to implement]:
    https://github.com/flutter/flutter/blob/main/docs/contributing/Style-guide-for-Flutter-repo.md#features-we-expect-every-widget-to-implement
    [CLA]: https://cla.developers.google.com/
    [flutter/tests]: https://github.com/flutter/tests
    [breaking change policy]:
    https://github.com/flutter/flutter/blob/main/docs/contributing/Tree-hygiene.md#handling-breaking-changes
    [Discord]:
    https://github.com/flutter/flutter/blob/main/docs/contributing/Chat.md
    [Data Driven Fixes]:
    https://github.com/flutter/flutter/blob/main/docs/contributing/Data-driven-Fixes.md

--- packages/flutter/test/widgets/semantics_test.dart
+++ packages/flutter/test/widgets/semantics_test.dart
@@ -525,6 +525,8 @@ void main() {
       ..remove(SemanticsAction.moveCursorBackwardByWord)
       ..remove(SemanticsAction.customAction) // customAction is not user-exposed.
       ..remove(SemanticsAction.showOnScreen) // showOnScreen is not user-exposed
+      // TODO(LongCatIsLooong): change to `SemanticsAction.scrollToOffset` when available.
+      // https://github.com/flutter/flutter/issues/159515.
       ..removeWhere((SemanticsAction action) => action.index == 1 << 23);
 
     const int expectedId = 1;
@@ -543,6 +545,9 @@ void main() {
     final SemanticsOwner semanticsOwner = tester.binding.pipelineOwner.semanticsOwner!;
     int expectedLength = 1;
     for (final SemanticsAction action in allActions) {
+      // TODO(LongCatIsLooong): remove after `SemanticsAction.scrollToOffset` is added to dart:ui.
+      // https://github.com/flutter/flutter/issues/159515.
+      // ignore: exhaustive_cases
       switch (action) {
         case SemanticsAction.moveCursorBackwardByCharacter:
         case SemanticsAction.moveCursorForwardByCharacter:
