5e91b9a0fec6ae5ec2b34ffc8a38a8c09f997346
Andrew Kolos <andrewrkolos@gmail.com>
2024-12-01 17:17:16 -0800

    lint

--- packages/flutter_tools/test/commands.shard/permeable/widget_preview_test.dart
+++ packages/flutter_tools/test/commands.shard/permeable/widget_preview_test.dart
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'dart:io' as io show Directory, IOOverrides;
+import 'dart:io' as io show IOOverrides;
 
 import 'package:args/command_runner.dart';
 import 'package:file_testing/file_testing.dart';
