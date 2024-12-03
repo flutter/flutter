deee4a6cd9f03f73ec77e97163af68aca027c4cb
Mohellebi abdessalem <mohellebiabdessalem@gmail.com>
2024-12-03 20:16:51 +0100

    Update android_gradle_deprecated_plugin_apply_test.dart

--- packages/flutter_tools/test/integration.shard/android_gradle_deprecated_plugin_apply_test.dart
+++ packages/flutter_tools/test/integration.shard/android_gradle_deprecated_plugin_apply_test.dart
@@ -56,7 +56,7 @@ localPropertiesFile.withReader("UTF-8") { reader -> properties.load(reader) }
 
 def flutterSdkPath = properties.getProperty("flutter.sdk")
 assert flutterSdkPath != null, "flutter.sdk not set in local.properties"
-apply from: "$flutterSdkPath/packages/flutter_tools/gradle/app_plugin_loader.gradle.kts"
+apply from: "$flutterSdkPath/packages/flutter_tools/gradle/app_plugin_loader.gradle"
 '''
     );
   buildGradle.writeAsStringSync(r'''
