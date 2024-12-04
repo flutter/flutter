f431f6146229d338d3394cfcef1a8e6ad501d34a
John McCutchan <john@johnmccutchan.com>
2024-12-03 19:27:30 -0800

    Android Integration Tests run under an Android Instrumentation
    - Allows us to make calls to ui-automator methods
    - TODO: Generate the activityTest in a new project
    - TODO: flutter test integration_test/... does the right thing
    
    ❯ ./gradlew :app:assembleDebugAndroidTest -Ptarget=integration_test/test_entry.dart && ./gradlew :app:connectedDebugAndroidTest --info -Ptarget=integration_test/test_entry.dart

--- packages/integration_test/android/src/main/java/dev/flutter/plugins/integration_test/IntegrationTestPlugin.java
+++ packages/integration_test/android/src/main/java/dev/flutter/plugins/integration_test/IntegrationTestPlugin.java
@@ -17,23 +17,52 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 import java.io.IOException;
 import java.util.Map;
+import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
+import androidx.test.uiautomator.UiDevice;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.runner.AndroidJUnitRunner;
 
 /** IntegrationTestPlugin */
 public class IntegrationTestPlugin implements MethodCallHandler, FlutterPlugin, ActivityAware {
   private static final String CHANNEL = "plugins.flutter.io/integration_test";
   private static final SettableFuture<Map<String, String>> testResultsSettable =
       SettableFuture.create();
+  public static final Future<Map<String, String>> testResults = testResultsSettable;
+  private final SettableFuture<String[]> testListSettable = SettableFuture.create();
+  private final Future<String[]> testListFuture = testListSettable;
 
   private MethodChannel methodChannel;
   private Activity flutterActivity;
-  public static final Future<Map<String, String>> testResults = testResultsSettable;
+  private FlutterTestJUnitRunner instrumentation;
+
+  public IntegrationTestPlugin() {
+    System.out.println("JOHN IntegrationTestPlugin");
+    instrumentation = (FlutterTestJUnitRunner)InstrumentationRegistry.getInstrumentation();
+    if (instrumentation == null) {
+      return;
+    }
+    System.out.println("JOHN Registering plugin with instrumentation");
+    instrumentation.registerPlugin(this);
+  }
+
+  String[] getDartTestList() {
+    try {
+      return this.testListFuture.get();
+    }  catch (ExecutionException | InterruptedException e) {
+      System.out.println("JOHN failed to get rest getDartTestList execpteion=" + e);
+    }
+    return new String[0];
+  }
 
   /** Plugin registration. */
 
   @Override
   public void onAttachedToEngine(FlutterPluginBinding binding) {
     onAttachedToEngine(binding.getApplicationContext(), binding.getBinaryMessenger());
+    UiDevice device = UiDevice.getInstance(instrumentation);
+
   }
 
   private void onAttachedToEngine(Context unusedApplicationContext, BinaryMessenger messenger) {
@@ -69,6 +98,7 @@ public class IntegrationTestPlugin implements MethodCallHandler, FlutterPlugin, 
 
   @Override
   public void onMethodCall(MethodCall call, Result result) {
+    System.out.println("JOHN IntegrestionTestPlugin onMethodCall " + call.method);
     switch (call.method) {
       case "allTestsFinished":
         final Map<String, String> results = call.argument("results");
@@ -108,6 +138,16 @@ public class IntegrationTestPlugin implements MethodCallHandler, FlutterPlugin, 
           return;
         }
         FlutterDeviceScreenshot.captureView(flutterActivity, methodChannel, result);
+        return;
+      case "populateTestList":
+          final ArrayList<String> testList = call.argument("testList");
+          final String[] tests = new String[testList.size()];
+          for (int i = 0; i < testList.size(); i++) {
+            tests[i] = testList.get(i);
+          }
+          System.out.println("JOHN populateTEstList JAVA SIDE: " + tests);
+          testListSettable.set(tests);
+          result.success(null);
         return;
       default:
         result.notImplemented();
