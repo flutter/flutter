8f82bfdbdffe56489bdc49fbbe6f0326fe3fc3bf
Camille Simon <camillesimon@google.com>
2024-12-03 16:26:04 -0800

    use path.join

--- dev/devicelab/bin/tasks/android_release_builds_exclude_dev_dependencies_test.dart
+++ dev/devicelab/bin/tasks/android_release_builds_exclude_dev_dependencies_test.dart
@@ -9,6 +9,7 @@ import 'package:flutter_devicelab/framework/apk_utils.dart';
 import 'package:flutter_devicelab/framework/framework.dart';
 import 'package:flutter_devicelab/framework/task_result.dart';
 import 'package:flutter_devicelab/framework/utils.dart';
+import 'package:path/path.dart' as path;
 
 Future<void> main() async {
   await task(() async {
@@ -20,7 +21,7 @@ Future<void> main() async {
         await FlutterPluginProject.create(tempDir, 'dev_dependency_plugin', options: <String>['--platforms=android', '--org=$devDependencyPluginOrg']);
 
         // Add devDependencyPlugin as dependency of flutterProject.
-        await flutterProject.addPlugin('dev_dependency_plugin', options: <String>['--path', '${tempDir.path}/dev_dependency_plugin']);
+        await flutterProject.addPlugin('dev_dependency_plugin', options: <String>['--path', path.join(tempDir.path, 'dev_dependency_plugin')]);
 
         final List<String> buildModesToTest = <String>['debug', 'profile', 'release'];
         for (final String buildMode in buildModesToTest) {
@@ -34,7 +35,7 @@ Future<void> main() async {
               '--target-platform=android-arm',
             ]);
 
-            final File apk = File('${flutterProject.rootPath}/build/app/outputs/flutter-apk/app-debug.apk');
+            final File apk = File(path.join(flutterProject.rootPath, 'build', 'app', 'outputs', 'flutter-apk', 'app-debug.apk'));
             if (!apk.existsSync()) {
               throw TaskResult.failure("Expected ${apk.path} to exist, but it doesn't");
             }
