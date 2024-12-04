18229cbd349b7afd01b62e5ee9c4ff6a693c510a
LongCatIsLooong <31859944+LongCatIsLooong@users.noreply.github.com>
2024-12-04 14:21:32 -0800

    fix tests

--- packages/flutter/test/widgets/scrollable_semantics_test.dart
+++ packages/flutter/test/widgets/scrollable_semantics_test.dart
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/gestures.dart' show DragStartBehavior;
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
