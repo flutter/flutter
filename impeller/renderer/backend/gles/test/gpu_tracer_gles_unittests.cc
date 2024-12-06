9eb1c7d2f16288a4ff15798ab4ace6809b2689ba
gaaclarke <30870216+gaaclarke@users.noreply.github.com>
2024-12-06 09:17:57 -0800

    Replaces bespoke call captures from mock gles with gmock (#56995)
    test exempt: test only code
    
    ## Pre-launch Checklist
    
    - [x] I read the [Contributor Guide] and followed the process outlined
    there for submitting PRs.
    - [x] I read the [Tree Hygiene] wiki page, which explains my
    responsibilities.
    - [x] I read and followed the [Flutter Style Guide] and the [C++,
    Objective-C, Java style guides].
    - [x] I listed at least one issue that this PR fixes in the description
    above.
    - [x] I added new tests to check the change I am making or feature I am
    adding, or the PR is [test-exempt]. See [testing the engine] for
    instructions on writing and running engine tests.
    - [x] I updated/added relevant documentation (doc comments with `///`).
    - [x] I signed the [CLA].
    - [x] All existing and new tests are passing.
    
    If you need help, consider asking for advice on the #hackers-new channel
    on [Discord].
    
    <!-- Links -->
    [Contributor Guide]:
    https://github.com/flutter/flutter/blob/master/docs/contributing/Tree-hygiene.md#overview
    [Tree Hygiene]:
    https://github.com/flutter/flutter/blob/master/docs/contributing/Tree-hygiene.md
    [test-exempt]:
    https://github.com/flutter/flutter/blob/master/docs/contributing/Tree-hygiene.md#tests
    [Flutter Style Guide]:
    https://github.com/flutter/flutter/blob/master/docs/contributing/Style-guide-for-Flutter-repo.md
    [C++, Objective-C, Java style guides]:
    https://github.com/flutter/engine/blob/main/CONTRIBUTING.md#style
    [testing the engine]:
    https://github.com/flutter/engine/blob/main/docs/testing/Testing-the-engine.md
    [CLA]: https://cla.developers.google.com/
    [flutter/tests]: https://github.com/flutter/tests
    [breaking change policy]:
    https://github.com/flutter/flutter/blob/master/docs/contributing/Tree-hygiene.md#handling-breaking-changes
    [Discord]:
    https://github.com/flutter/flutter/blob/master/docs/contributing/Chat.md

--- impeller/renderer/backend/gles/test/gpu_tracer_gles_unittests.cc
+++ impeller/renderer/backend/gles/test/gpu_tracer_gles_unittests.cc
@@ -10,38 +10,48 @@
 namespace impeller {
 namespace testing {
 
+using ::testing::_;
+
 #ifdef IMPELLER_DEBUG
 TEST(GPUTracerGLES, CanFormatFramebufferErrorMessage) {
-  auto const extensions = std::vector<const unsigned char*>{
-      reinterpret_cast<const unsigned char*>("GL_KHR_debug"),                 //
-      reinterpret_cast<const unsigned char*>("GL_EXT_disjoint_timer_query"),  //
+  auto const extensions = std::vector<const char*>{
+      "GL_KHR_debug",                 //
+      "GL_EXT_disjoint_timer_query",  //
   };
-  auto mock_gles = MockGLES::Init(extensions);
+  auto mock_gles_impl = std::make_unique<MockGLESImpl>();
+
+  {
+    ::testing::InSequence seq;
+    auto gen_queries = [](GLsizei n, GLuint* ids) {
+      for (int i = 0; i < n; ++i) {
+        ids[i] = i + 1;
+      }
+    };
+    EXPECT_CALL(*mock_gles_impl, GenQueriesEXT(_, _)).WillOnce(gen_queries);
+    EXPECT_CALL(*mock_gles_impl, BeginQueryEXT(GL_TIME_ELAPSED_EXT, _));
+    EXPECT_CALL(*mock_gles_impl, EndQueryEXT(GL_TIME_ELAPSED_EXT));
+    EXPECT_CALL(*mock_gles_impl,
+                GetQueryObjectuivEXT(_, GL_QUERY_RESULT_AVAILABLE_EXT, _))
+        .WillOnce([](GLuint id, GLenum target, GLuint* result) {
+          *result = GL_TRUE;
+        });
+    EXPECT_CALL(*mock_gles_impl,
+                GetQueryObjectui64vEXT(_, GL_QUERY_RESULT_EXT, _))
+        .WillOnce([](GLuint id, GLenum target, GLuint64* result) {
+          *result = 1000u;
+        });
+    EXPECT_CALL(*mock_gles_impl, DeleteQueriesEXT(_, _));
+    EXPECT_CALL(*mock_gles_impl, GenQueriesEXT(_, _)).WillOnce(gen_queries);
+    EXPECT_CALL(*mock_gles_impl, BeginQueryEXT(GL_TIME_ELAPSED_EXT, _));
+  }
+  std::shared_ptr<MockGLES> mock_gles =
+      MockGLES::Init(std::move(mock_gles_impl), extensions);
   auto tracer =
       std::make_shared<GPUTracerGLES>(mock_gles->GetProcTable(), true);
   tracer->RecordRasterThread();
   tracer->MarkFrameStart(mock_gles->GetProcTable());
   tracer->MarkFrameEnd(mock_gles->GetProcTable());
-
-  auto calls = mock_gles->GetCapturedCalls();
-
-  std::vector<std::string> expected = {"glGenQueriesEXT", "glBeginQueryEXT",
-                                       "glEndQueryEXT"};
-  for (auto i = 0; i < 3; i++) {
-    EXPECT_EQ(calls[i], expected[i]);
-  }
-
-  // Begin second frame, which prompts the tracer to query the result
-  // from the previous frame.
   tracer->MarkFrameStart(mock_gles->GetProcTable());
-
-  calls = mock_gles->GetCapturedCalls();
-  std::vector<std::string> expected_b = {"glGetQueryObjectuivEXT",
-                                         "glGetQueryObjectui64vEXT",
-                                         "glDeleteQueriesEXT"};
-  for (auto i = 0; i < 3; i++) {
-    EXPECT_EQ(calls[i], expected_b[i]);
-  }
 }
 
 #endif  // IMPELLER_DEBUG
