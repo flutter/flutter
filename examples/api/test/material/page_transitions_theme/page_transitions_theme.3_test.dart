9c3bec4dda52016c7def2240c105d31b77c2885d
Qun Cheng <36861262+QuncCccccc@users.noreply.github.com>
2024-12-06 16:56:57 -0800

    Create new page transition for M3 (#158881)
    This PR is to add a new page transition for Material 3. 
    
    The new builder matches the latest Android page transition behavior;
    while the new page slides in from right to left, it fades in at the same
    time and the old page slides out from right to left, fading out at the
    same time. When both pages are fading in/out, the black background might
    show up, so I added a `ColoredBox` for the slides-out page whose color
    defaults to `ColorScheme.surface`. The `backgroundColor` property can be
    used to customize the default transition color.
    
    This demo shows the forward and backward behaviors.
    
    
    https://github.com/user-attachments/assets/a806f25d-8564-4cad-8dfc-eb4585294181
    
    Fixes: https://github.com/flutter/flutter/issues/142352
    
    ## Pre-launch Checklist
    
    - [x] I read the [Contributor Guide] and followed the process outlined
    there for submitting PRs.
    - [x] I read the [Tree Hygiene] wiki page, which explains my
    responsibilities.
    - [x] I read and followed the [Flutter Style Guide], including [Features
    we expect every widget to implement].
    - [x] I signed the [CLA].
    - [x] I listed at least one issue that this PR fixes in the description
    above.
    - [x] I updated/added relevant documentation (doc comments with `///`).
    - [x] I added new tests to check the change I am making, or this PR is
    [test-exempt].
    - [x] I followed the [breaking change policy] and added [Data Driven
    Fixes] where supported.
    - [x] All existing and new tests are passing.

---
+++ examples/api/test/material/page_transitions_theme/page_transitions_theme.3_test.dart
@@ -0,0 +1,30 @@
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+import 'package:flutter_api_samples/material/page_transitions_theme/page_transitions_theme.3.dart' as example;
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('Page transition', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      const example.PageTransitionsThemeApp(),
+    );
+
+    final Finder homePage = find.byType(example.HomePage);
+    expect(homePage, findsOneWidget);
+
+    final Finder kitten0 = find.widgetWithText(ListTile, 'Kitten 0');
+    expect(kitten0, findsOneWidget);
+
+    await tester.tap(kitten0);
+    await tester.pumpAndSettle();
+    expect(find.widgetWithText(AppBar, 'Kitten 0'), findsOneWidget);
+
+    await tester.tap(find.byType(BackButton));
+    await tester.pumpAndSettle();
+
+    expect(find.widgetWithText(ListTile, 'Kitten 0'), findsOneWidget);
+  });
+}
