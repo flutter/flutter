572a69c6e92788b8f5e7275e2e17f436311bd694
Matan Lurey <matan@lurey.org>
2024-12-09 11:14:25 -0800

    ++

--- packages/integration_test/test/flutter_test_config.dart
+++ packages/integration_test/test/flutter_test_config.dart
@@ -4,7 +4,7 @@
 
 import 'dart:async';
 
-import '_goldens_io.dart' if (dart.library.html) '_goldens_web.dart' as flutter_goldens;
+import '_goldens_io.dart' if (dart.library.js_interop) '_goldens_web.dart' as flutter_goldens;
 
 Future<void> testExecutable(FutureOr<void> Function() testMain) {
   // Enable golden file testing using Skia Gold.
