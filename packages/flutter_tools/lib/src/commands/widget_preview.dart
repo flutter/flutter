4103f1eab61d3729a7c90230d2da44a53e1b5eab
Andrew Kolos <andrewrkolos@gmail.com>
2024-12-11 11:24:12 -0800

    restore flutter_gen

--- packages/flutter_tools/lib/src/commands/widget_preview.dart
+++ packages/flutter_tools/lib/src/commands/widget_preview.dart
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'dart:convert';
+
 import 'package:args/args.dart';
 import 'package:meta/meta.dart';
 
@@ -244,6 +246,56 @@ class WidgetPreviewStartCommand extends FlutterCommand
       project: widgetPreviewScaffoldProject,
       offline: offline,
       outputMode: PubOutputMode.summaryOnly,
+    );
+
+    if (rootProject.manifest.generateSyntheticPackage) {
+      maybeAddFlutterGenToPackageConfig(
+        rootProject: rootProject,
+      );
+    }
+  }
+
+  /// Manually adds an entry for package:flutter_gen to the preview scaffold's
+  /// package_config.json if the target project makes use of localization.
+  ///
+  /// The Flutter Tool does this when running a Flutter project with
+  /// localization instead of modifying the user's pubspec.yaml to depend on it
+  /// as a path dependency. Unfortunately, the preview scaffold still needs to
+  /// add it directly to its package_config.json as the generated package name
+  /// isn't actually flutter_gen, which pub doesn't really like, and using the
+  /// actual package name will break applications which import
+  /// package:flutter_gen.
+  ///
+  // TODO(andrewkolos): package:flutter_gen is deprecated (see
+  //  https://docs.flutter.dev/release/breaking-changes/flutter-generate-i10n-source).
+  //  This code should be deleted once package:flutter_gen is removed.
+  void maybeAddFlutterGenToPackageConfig({
+    required FlutterProject rootProject,
+  }) {
+    if (!rootProject.manifest.generateSyntheticPackage) {
+      return;
+    }
+    final FlutterProject widgetPreviewScaffoldProject =
+        rootProject.widgetPreviewScaffoldProject;
+    final File packageConfig = widgetPreviewScaffoldProject.packageConfig;
+    final String previewPackageConfigPath = packageConfig.path;
+    if (!packageConfig.existsSync()) {
+      throw StateError(
+        "Could not find preview project's package_config.json at "
+        '$previewPackageConfigPath',
+      );
+    }
+    final Map<String, Object?> packageConfigJson = json.decode(
+      packageConfig.readAsStringSync(),
+    ) as Map<String, Object?>;
+    (packageConfigJson['packages'] as List<dynamic>?)!
+        .cast<Map<String, String>>()
+        .add(flutterGenPackageConfigEntry);
+    packageConfig.writeAsStringSync(
+      json.encode(packageConfigJson),
+    );
+    globals.logger.printStatus(
+      'Added flutter_gen dependency to $previewPackageConfigPath',
     );
   }
 }
