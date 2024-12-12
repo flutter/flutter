943499763cad63d096530f6ca63525384ee81591
Ben Konyi <bkonyi@google.com>
2024-12-12 11:01:49 -0500

    Move widget previews to package:flutter/widgets.dart

--- packages/flutter/lib/src/widgets/widget_preview.dart
+++ packages/flutter/lib/src/widgets/widget_preview.dart
@@ -4,6 +4,39 @@
 
 import 'package:flutter/widgets.dart';
 
+/// Annotation used to mark functions that return widget previews.
+///
+/// {@tool snippet}
+///
+/// Functions annotated with `@Preview()` should return a `List<WidgetPreview>`
+/// and be public, top-level functions.
+///
+/// ```dart
+/// @Preview()
+/// List<WidgetPreview> myFirstPreview() {
+///   return <WidgetPreview>[
+///     WidgetPreview(
+///       name: 'Preview 1',
+///       child: const Text('Foo'),
+///     ),
+///     WidgetPreview(
+///       name: 'Preview 2',
+///       child: MyWidget(),
+///     ),
+///   ];
+/// }
+/// ```
+/// {@end-tool}
+///
+/// See also:
+///
+///  * [WidgetPreview], a data class used to specify widget previews.
+// TODO(bkonyi): link to actual documentation when available.
+class Preview {
+  /// Annotation used to mark functions that return widget previews.
+  const Preview();
+}
+
 /// Wraps a [Widget], initializing various state and properties to allow for
 /// previewing of the [Widget] in the widget previewer.
 ///
