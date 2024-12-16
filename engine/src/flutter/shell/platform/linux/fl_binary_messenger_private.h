292f8d74af9b605cdfdf251a886e56fd5c9fdcff
Robert Ancell <robert.ancell@canonical.com>
2024-12-17 09:02:45 +1300

    Migrate FlBinaryMessenger using embedder API instead of mock engine. (flutter/engine#57214)
    This allows us to remove most of the remaining mock engine code.

--- engine/src/flutter/shell/platform/linux/fl_binary_messenger_private.h
+++ engine/src/flutter/shell/platform/linux/fl_binary_messenger_private.h
@@ -7,6 +7,7 @@
 
 #include <glib-object.h>
 
+#include "flutter/shell/platform/embedder/embedder.h"
 #include "flutter/shell/platform/linux/public/flutter_linux/fl_engine.h"
 
 G_BEGIN_DECLS
@@ -21,6 +22,24 @@ G_BEGIN_DECLS
  * Returns: a new #FlBinaryMessenger.
  */
 FlBinaryMessenger* fl_binary_messenger_new(FlEngine* engine);
+
+/**
+ * fl_binary_messenger_handle_message:
+ * @messenger: an #FlBinaryMessenger.
+ * @channel: channel message received on.
+ * @message: message data.
+ * @response_handle: handle to provide to
+ * fl_engine_send_platform_message_response().
+ *
+ * Handles a message received from the engine. Available for testing purposes.
+ *
+ * Returns: %TRUE if the message is handled.
+ */
+gboolean fl_binary_messenger_handle_message(
+    FlBinaryMessenger* messenger,
+    const gchar* channel,
+    GBytes* message,
+    const FlutterPlatformMessageResponseHandle* response_handle);
 
 /**
  * fl_binary_messenger_shutdown:
