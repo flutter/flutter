0f76641bf8a7b8066caa8d6772c5bca4eb8b3f84
Matan Lurey <matan@lurey.org>
2024-12-18 15:59:06 -0800

    Update.

--- dev/tools/bin/engine_hash.sh
+++ dev/tools/bin/engine_hash.sh
@@ -13,19 +13,57 @@
 #
 # -------------------------------------------------------------------------- #
 
-# Test for fusion repository
-if [ -f "$FLUTTER_ROOT/DEPS" ] && [ -f "$FLUTTER_ROOT/engine/src/.gn" ]; then
-    BRANCH=$(git -C "$FLUTTER_ROOT" rev-parse --abbrev-ref HEAD)
+# Needed because if it is set, cd may print the path it changed to.
+unset CDPATH
 
-    # In a fusion repository; the engine.version comes from the git hashes.
-    if [ -z "${LUCI_CONTEXT}" ]; then
-      ENGINE_VERSION=$(git -C "$FLUTTER_ROOT" merge-base HEAD upstream/master)
-    else
-      ENGINE_VERSION=$(git -C "$FLUTTER_ROOT" rev-parse HEAD)
-    fi
+# On Mac OS, readlink -f doesn't work, so follow_links traverses the path one
+# link at a time, and then cds into the link destination and find out where it
+# ends up.
+#
+# The returned filesystem path must be a format usable by Dart's URI parser,
+# since the Dart command line tool treats its argument as a file URI, not a
+# filename. For instance, multiple consecutive slashes should be reduced to a
+# single slash, since double-slashes indicate a URI "authority", and these are
+# supposed to be filenames. There is an edge case where this will return
+# multiple slashes: when the input resolves to the root directory. However, if
+# that were the case, we wouldn't be running this shell, so we don't do anything
+# about it.
+#
+# The function is enclosed in a subshell to avoid changing the working directory
+# of the caller.
+function follow_links() (
+  cd -P "$(dirname -- "$1")"
+  file="$PWD/$(basename -- "$1")"
+  while [[ -h "$file" ]]; do
+    cd -P "$(dirname -- "$file")"
+    file="$(readlink -- "$file")"
+    cd -P "$(dirname -- "$file")"
+    file="$PWD/$(basename -- "$file")"
+  done
+  echo "$file"
+)
+
+PROG_NAME="$(follow_links "${BASH_SOURCE[0]}")"
+BIN_DIR="$(cd "${PROG_NAME%/*}" ; pwd -P)"
+FLUTTER_ROOT="$(cd "${BIN_DIR}/../../.." ; pwd -P)"
+
+# Allow using a mock git for testing.
+if [ -z "$GIT" ]; then
+    # By default, use git on PATH.
+    GIT_BIN="git"
 else
-    # Non-fusion repository - these files will exist
+    # Use the provide GIT executable.
+    GIT_BIN="$GIT"
+fi
+
+# Test for fusion repository
+if [ -f "$FLUTTER_ROOT/DEPS" ]; then
+    ENGINE_VERSION=$($GIT_BIN -C "$FLUTTER_ROOT" merge-base HEAD upstream/master)
+elif [ -f "$FLUTTER_ROOT/bin/internal/engine.version" ]; then
     ENGINE_VERSION=$(cat "$FLUTTER_ROOT/bin/internal/engine.version")
+else
+    >&2 echo "Not a valid FLUTTER_ROOT: $FLUTTER_ROOT"
+    exit 1
 fi
 
 echo $ENGINE_VERSION
---
+++ dev/tools/test/engine_hash_test.dart
@@ -0,0 +1,70 @@
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+@TestOn('posix')
+library;
+
+import 'dart:io' as io;
+
+import 'package:path/path.dart' as p;
+import 'package:test/test.dart';
+
+/// Tests that `/dev/tools/bin/engine_hash.sh` _appears_ to work.
+void main() {
+  late final io.File engineHashSh;
+
+  setUpAll(() {
+    engineHashSh = io.File(p.join(p.current, 'bin', 'engine_hash.sh'));
+    if (!engineHashSh.existsSync()) {
+      fail('No engine_hash.sh at "${p.absolute(engineHashSh.path)}".');
+    }
+  });
+
+  late io.Directory tmpFlutterRoot;
+
+  setUp(() {
+    tmpFlutterRoot = io.Directory.systemTemp.createTempSync('engine_hash_test.');
+
+    // Create engine_hash.sh at the same component it would be in the real root.
+    io.Directory(p.join(tmpFlutterRoot.path, 'dev', 'tools', 'bin')).createSync(recursive: true);
+    engineHashSh.copySync(p.join(tmpFlutterRoot.path, 'dev', 'tools', 'bin', 'engine_hash.sh'));
+
+    // Create FLUTTER_ROOT/DEPS.
+    io.File(p.join(tmpFlutterRoot.path, 'DEPS')).createSync();
+  });
+
+  tearDown(() {
+    tmpFlutterRoot.deleteSync(recursive: true);
+  });
+
+  test('omission of FLUTTER_ROOT/DEPS falls back to engine.version', () {
+    io.File(p.join(tmpFlutterRoot.path, 'bin', 'internal', 'engine.version'))
+      ..createSync(recursive: true)
+      ..writeAsStringSync('12345');
+    io.File(p.join(tmpFlutterRoot.path, 'DEPS')).deleteSync();
+
+    final io.ProcessResult result = io.Process.runSync(
+      p.join(tmpFlutterRoot.path, 'dev', 'tools', 'bin', 'engine_hash.sh'),
+      <String>[],
+    );
+    expect(result.exitCode, 0, reason: result.stderr.toString());
+    expect(result.stdout, '12345\n');
+  });
+
+  test('uses git -C merge-base HEAD upstream/master', () {
+    final io.ProcessResult result = io.Process.runSync(
+      p.join(tmpFlutterRoot.path, 'dev', 'tools', 'bin', 'engine_hash.sh'),
+      <String>[],
+      environment: <String, String>{
+        'GIT': p.join(p.current, 'test', 'mock_git.sh'),
+      },
+    );
+    expect(result.exitCode, 0, reason: result.stderr.toString());
+    expect(result.stdout, stringContainsInOrder(<String>[
+      'Mock Git: -C',
+      'engine_hash_test',
+      'merge-base HEAD upstream/master',
+    ]));
+  });
+}
