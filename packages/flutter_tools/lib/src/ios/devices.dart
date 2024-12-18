d91022ddc7263953308bf903b0f6ec5bd5f9ba05
Alex Li <github@alexv525.com>
2024-12-18 17:49:41 +0800

    🔊 Add wirelessly connected log

--- packages/flutter_tools/lib/src/ios/devices.dart
+++ packages/flutter_tools/lib/src/ios/devices.dart
@@ -574,6 +574,10 @@ class IOSDevice extends Device {
         // user to allow local network permissions.
         if (isWirelesslyConnected) {
           _logger.printError(
+            '\nYour debugging device seems wirelessly connected. '
+            'Consider plugging it in and trying again.',
+          );
+          _logger.printError(
             '\nClick "Allow" to the prompt asking if you would like to find and connect devices on your local network. '
             'This is required for wireless debugging. If you selected "Don\'t Allow", '
             'you can turn it on in Settings > Your App Name > Local Network. '
