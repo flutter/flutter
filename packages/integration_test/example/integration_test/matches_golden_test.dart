76e1071d6e512102a34b37475da46a6303de56cc
Matan Lurey <matan@lurey.org>
2024-12-17 18:26:07 -0800

    Tweak the test.

--- packages/integration_test/example/integration_test/matches_golden_test.dart
+++ packages/integration_test/example/integration_test/matches_golden_test.dart
@@ -9,25 +9,26 @@ import 'package:integration_test/integration_test.dart';
 import 'package:integration_test_example/main.dart' as app;
 
 /// To run:
-/// 
+///
 /// ```sh
 /// # Be in this directory
 /// cd dev/packages/integration_test/example
-/// 
+///
 /// flutter test integration_test/matches_golden_test.dart
 /// ```
-/// 
+///
 /// To run on a particular device, see `flutter -d`.
 void main() {
-  // Used in order to skip actually checking the file and just going through the motions.
-  autoUpdateGoldenFiles = true;
-
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  // TODO(matanlurey): Make this automatic as part of the bootstrap.
   VmServiceProxyGoldenFileComparator.useIfRunningOnDevice();
 
   testWidgets('can use matchesGoldenFile with integration_test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     app.main();
+
+    // TODO(matanlurey): Is this necessary?
     await tester.pumpAndSettle();
 
     // Take a screenshot.
