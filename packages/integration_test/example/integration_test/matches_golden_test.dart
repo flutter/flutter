288aa1a2afa7791ef04ab6c73aec69a95f571501
Matan Lurey <matan@lurey.org>
2024-12-19 15:28:19 -0800

    Add reduced-test-set.

--- packages/integration_test/example/integration_test/matches_golden_test.dart
+++ packages/integration_test/example/integration_test/matches_golden_test.dart
@@ -2,6 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+@Tags(<String>['reduced-test-set'])
+library;
+
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
