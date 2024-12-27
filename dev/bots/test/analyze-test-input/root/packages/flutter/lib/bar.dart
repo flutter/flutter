c57126ad94b3d0085e37269040fcf06fdf13782a
LongCatIsLooong <31859944+LongCatIsLooong@users.noreply.github.com>
2024-12-27 15:26:24 -0800

    Turn deprecation message analyze tests back on (#160554)
    Additionally:
    - Deprecation message analyzer: use AST more instead of regex 
    - Deprecation message analyzer: removes some tests since they are in the
    formatter's jurisdiction now
    - update the analyzer test fixture to fix a couple line numbers
    - `@_debugOnly` set `multiline` back to false. The source we get back
    from the `ASTNode` doesn't have any line feed characters.
    
    ## Pre-launch Checklist
    
    - [ ] I read the [Contributor Guide] and followed the process outlined
    there for submitting PRs.
    - [ ] I read the [Tree Hygiene] wiki page, which explains my
    responsibilities.
    - [ ] I read and followed the [Flutter Style Guide], including [Features
    we expect every widget to implement].
    - [ ] I signed the [CLA].
    - [ ] I listed at least one issue that this PR fixes in the description
    above.
    - [ ] I updated/added relevant documentation (doc comments with `///`).
    - [ ] I added new tests to check the change I am making, or this PR is
    [test-exempt].
    - [ ] I followed the [breaking change policy] and added [Data Driven
    Fixes] where supported.
    - [ ] All existing and new tests are passing.
    
    If you need help, consider asking for advice on the #hackers-new channel
    on [Discord].
    
    <!-- Links -->
    [Contributor Guide]:
    https://github.com/flutter/flutter/blob/main/docs/contributing/Tree-hygiene.md#overview
    [Tree Hygiene]:
    https://github.com/flutter/flutter/blob/main/docs/contributing/Tree-hygiene.md
    [test-exempt]:
    https://github.com/flutter/flutter/blob/main/docs/contributing/Tree-hygiene.md#tests
    [Flutter Style Guide]:
    https://github.com/flutter/flutter/blob/main/docs/contributing/Style-guide-for-Flutter-repo.md
    [Features we expect every widget to implement]:
    https://github.com/flutter/flutter/blob/main/docs/contributing/Style-guide-for-Flutter-repo.md#features-we-expect-every-widget-to-implement
    [CLA]: https://cla.developers.google.com/
    [flutter/tests]: https://github.com/flutter/tests
    [breaking change policy]:
    https://github.com/flutter/flutter/blob/main/docs/contributing/Tree-hygiene.md#handling-breaking-changes
    [Discord]:
    https://github.com/flutter/flutter/blob/main/docs/contributing/Chat.md
    [Data Driven Fixes]:
    https://github.com/flutter/flutter/blob/main/docs/contributing/Data-driven-Fixes.md

--- dev/bots/test/analyze-test-input/root/packages/flutter/lib/bar.dart
+++ dev/bots/test/analyze-test-input/root/packages/flutter/lib/bar.dart
@@ -15,6 +15,14 @@ class Foo {
 
   @_debugOnly
   final Map<String, String>? bar = kDebugMode ? null : <String, String>{};
+
+  // dart format off
+  // Checks the annotation works for multiline expressions.
+  @_debugOnly
+  final Map<String, String>? multiline = kDebugMode
+    ? <String, String>{}
+    : null;
+  // dart format on
 }
 
 /// Simply avoid this
