ec66fe4669bb17dfbaf2dac972a1511e9b8beaf2
Michael Goderbauer <goderbauer@google.com>
2024-12-30 10:31:41 -0800

    Fix docImport issues (#160918)
    Work towards https://github.com/flutter/flutter/issues/150800

--- packages/flutter/lib/src/material/page_transitions_theme.dart
+++ packages/flutter/lib/src/material/page_transitions_theme.dart
@@ -3,6 +3,7 @@
 // found in the LICENSE file.
 
 /// @docImport 'app.dart';
+/// @docImport 'color_scheme.dart';
 /// @docImport 'page.dart';
 /// @docImport 'predictive_back_page_transitions_builder.dart';
 library;
