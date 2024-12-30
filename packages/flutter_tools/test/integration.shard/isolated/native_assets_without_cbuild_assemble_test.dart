5e4748c6eaacecc216cc36921e24c6f8d184ff3f
Daco Harkes <dacoharkes@google.com>
2024-12-30 11:16:30 +0100

    add more prints

--- packages/flutter_tools/test/integration.shard/isolated/native_assets_without_cbuild_assemble_test.dart
+++ packages/flutter_tools/test/integration.shard/isolated/native_assets_without_cbuild_assemble_test.dart
@@ -103,7 +103,10 @@ void _testBuildCommand({
 import 'package:native_assets_cli/native_assets_cli.dart';
 
 void main(List<String> args) async {
-  await build(args, (config, output) async {});
+  await build(args, (config, output) async {
+    print('Doing nothing!');
+  });
+  print('Done doing nothing!');
 }
 ''');
 
