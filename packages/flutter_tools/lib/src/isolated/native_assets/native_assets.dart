43db5cb7e194d78e486addb7d735e0e4ae198f3b
Daco Harkes <dacoharkes@google.com>
2025-01-03 14:09:41 +0100

    try different range

--- packages/flutter_tools/lib/src/isolated/native_assets/native_assets.dart
+++ packages/flutter_tools/lib/src/isolated/native_assets/native_assets.dart
@@ -230,7 +230,7 @@ class FlutterNativeAssetsBuildRunnerImpl implements FlutterNativeAssetsBuildRunn
     hookEnvironment: filteredEnvironment(<String>{
       ...NativeAssetsBuildRunner.hookEnvironmentVariablesFilter,
       ...(const LocalPlatform().environment.keys.toList()..sort()).where((String key) {
-        final bool keep = key.toLowerCase().startsWith(RegExp(r'[a-g]'));
+        final bool keep = key.toLowerCase().startsWith(RegExp(r'[h-m]'));
         _logger.info('Native assets environment: ${keep ? 'keeping' : 'removing'} $key');
         return keep;
       }),
