91acfa00b083f70e0759f72347019487425bcc83
Mohellebi abdessalem <mohellebiabdessalem@gmail.com>
2025-01-08 19:22:25 +0100

    Update and rename app_plugin_loader.gradle to app_plugin_loader.gradle.kts

--- packages/flutter_tools/gradle/app_plugin_loader.gradle.kts
+++ packages/flutter_tools/gradle/app_plugin_loader.gradle.kts
@@ -6,8 +6,9 @@
 // not migrated to the declarative apply of the Flutter App Plugin Loader Gradle Plugin.
 // The old (imperative) apply of this plugin is not possible anymore.
 
-throw new GradleException("You are applying Flutter's app_plugin_loader Gradle plugin \
-imperatively using the apply script method, which is not possible anymore. \
-Migrate to applying Gradle plugins with the declarative plugins block: \
-https://flutter.dev/to/flutter-gradle-plugin-apply\n\
-")
+throw GradleException(
+    "You are applying Flutter's app_plugin_loader Gradle plugin "+
+            "imperatively using the apply script method, which is not possible anymore. "+
+            "Migrate to applying Gradle plugins with the declarative plugins block: "+
+            "https://flutter.dev/to/flutter-gradle-plugin-apply\n"
+)
