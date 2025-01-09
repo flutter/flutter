c1165a5938b73c7fc133567734dd9bedfcf4327c
Chun-Heng Tai <chtai@google.com>
2025-01-09 12:13:46 -0800

    update

--- engine/src/flutter/lib/ui/semantics/semantics_update_builder_unittests.cc
+++ engine/src/flutter/lib/ui/semantics/semantics_update_builder_unittests.cc
@@ -102,7 +102,7 @@ TEST_F(SemanticsUpdateBuilderTest, CanHandleSemanticsRole) {
     ASSERT_EQ(nodes.size(), (size_t)1);
     auto node = nodes.find(0)->second;
     // Should match the updateNode in ui_test.dart.
-    ASSERT_EQ(node.role, SemanticsRole::kTab);
+    ASSERT_EQ(node.role, SemanticsRole::kNone);
     message_latch->Signal();
   };
 
@@ -121,7 +121,7 @@ TEST_F(SemanticsUpdateBuilderTest, CanHandleSemanticsRole) {
 
   ASSERT_TRUE(shell->IsSetup());
   auto configuration = RunConfiguration::InferFromSettings(settings);
-  configuration.SetEntrypoint("sendSemanticsUpdateWithRole");
+  configuration.SetEntrypoint("sendSemanticsUpdate");
 
   shell->RunEngine(std::move(configuration), [](auto result) {
     ASSERT_EQ(result, Engine::RunStatus::Success);
