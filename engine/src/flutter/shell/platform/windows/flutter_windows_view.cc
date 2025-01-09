e26995a1551b7077756c87990d8db992c82379dc
Harlen Batagelo <hbatagelo@gmail.com>
2025-01-09 09:45:56 -0300

    Check if the view is valid when the key event callback is called

--- engine/src/flutter/shell/platform/windows/flutter_windows_view.cc
+++ engine/src/flutter/shell/platform/windows/flutter_windows_view.cc
@@ -536,12 +536,15 @@ void FlutterWindowsView::SendKey(int key,
                                  KeyEventCallback callback) {
   engine_->keyboard_key_handler()->KeyboardHook(
       key, scancode, action, character, extended, was_down,
-      [=, callback = std::move(callback)](bool handled) {
-        if (!handled) {
-          engine_->text_input_plugin()->KeyboardHook(
-              key, scancode, action, character, extended, was_down);
+      [=, engine = engine_, view_id = view_id_,
+       callback = std::move(callback)](bool handled) {
+        if (engine->view(view_id)) {
+          if (!handled) {
+            engine->text_input_plugin()->KeyboardHook(
+                key, scancode, action, character, extended, was_down);
+          }
+          callback(handled);
         }
-        callback(handled);
       });
 }
 
