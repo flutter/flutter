a4c5d2a0c97d02a9fecba26efba697626703dd78
Gray Mackall <mackall@google.com>
2025-01-10 14:51:08 -0800

    analyze round 5 i think?

--- packages/flutter_tools/test/integration.shard/android_run_flutter_gradle_plugin_tests_test.dart
+++ packages/flutter_tools/test/integration.shard/android_run_flutter_gradle_plugin_tests_test.dart
@@ -2,7 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-
 import 'dart:io';
 
 import 'package:flutter_tools/src/base/file_system.dart';
@@ -17,13 +16,17 @@ void main() {
   testUsingContext('Flutter Gradle Plugin unit tests pass', () async {
     final String gradleFileName = Platform.isWindows ? 'gradlew.bat' : 'gradlew';
     final String gradleExecutable = Platform.isWindows ? '.\\$gradleFileName' : './$gradleFileName';
-    final Directory flutterGradlePluginDirectory = fileSystem.directory(getFlutterRoot())
+    final Directory flutterGradlePluginDirectory = fileSystem
+        .directory(getFlutterRoot())
         .childDirectory('packages')
         .childDirectory('flutter_tools')
         .childDirectory('gradle');
     globals.gradleUtils?.injectGradleWrapperIfNeeded(flutterGradlePluginDirectory);
     makeExecutable(flutterGradlePluginDirectory.childFile(gradleFileName));
-    final RunResult runResult = await globals.processUtils.run(<String>[gradleExecutable, 'test'], workingDirectory: flutterGradlePluginDirectory.path);
+    final RunResult runResult = await globals.processUtils.run(<String>[
+      gradleExecutable,
+      'test',
+    ], workingDirectory: flutterGradlePluginDirectory.path);
     expect(runResult.exitCode, 0);
   });
 }
