bc5164ee6c1d1e8167ef02a1ba13409f3a0640cb
Aaron Clarke <aaclarke@google.com>
2025-01-15 12:39:05 -0800

    switched to playground test to use real glyph atlas

--- engine/src/flutter/impeller/entity/contents/text_contents_unittests.cc
+++ engine/src/flutter/impeller/entity/contents/text_contents_unittests.cc
@@ -5,6 +5,7 @@
 #include "flutter/impeller/renderer/testing/mocks.h"
 #include "flutter/testing/testing.h"
 #include "impeller/entity/contents/text_contents.h"
+#include "impeller/playground/playground_test.h"
 #include "impeller/typographer/backends/skia/text_frame_skia.h"
 #include "impeller/typographer/backends/skia/typographer_context_skia.h"
 #include "third_party/googletest/googletest/include/gtest/gtest.h"
@@ -13,8 +14,12 @@
 namespace impeller {
 namespace testing {
 
+using TextContentsTest = PlaygroundTest;
+INSTANTIATE_PLAYGROUND_SUITE(TextContentsTest);
+
 using ::testing::Return;
 
+namespace {
 std::shared_ptr<TextFrame> MakeTextFrame(const std::string& text,
                                          const std::string_view& font_fixture,
                                          Scalar font_size) {
@@ -33,59 +38,70 @@ std::shared_ptr<TextFrame> MakeTextFrame(const std::string& text,
   return MakeTextFrameFromTextBlobSkia(blob);
 }
 
-TEST(TextContentsTest, SimpleComputeVertexData) {
+std::shared_ptr<GlyphAtlas> CreateGlyphAtlas(
+    Context& context,
+    const TypographerContext* typographer_context,
+    HostBuffer& host_buffer,
+    GlyphAtlas::Type type,
+    Scalar scale,
+    const std::shared_ptr<GlyphAtlasContext>& atlas_context,
+    const std::shared_ptr<TextFrame>& frame) {
+  frame->SetPerFrameData(scale, /*offset=*/{0, 0},
+                         /*glyph_properties=*/std::nullopt);
+  return typographer_context->CreateGlyphAtlas(context, type, host_buffer,
+                                               atlas_context, {frame});
+}
+}  // namespace
+
+TEST_P(TextContentsTest, SimpleComputeVertexData) {
   GlyphAtlasPipeline::VertexShader::PerVertexData data[6];
 
   std::shared_ptr<TextFrame> text_frame =
       MakeTextFrame("1", "ahem.ttf", /*font_size=*/50);
 
-  TextureDescriptor texture_descriptor;
-  texture_descriptor.size = ISize(1024, 1024);
-  std::shared_ptr<MockTexture> mock_texture =
-      std::make_shared<MockTexture>(texture_descriptor);
-  EXPECT_CALL(*mock_texture, GetSize())
-      .WillOnce(Return(texture_descriptor.size));
-  std::shared_ptr<GlyphAtlas> atlas =
-      std::make_shared<GlyphAtlas>(GlyphAtlas::Type::kAlphaBitmap, 0);
-  atlas->SetTexture(mock_texture);
+  auto context = TypographerContextSkia::Make();
+  auto atlas_context =
+      context->CreateGlyphAtlasContext(GlyphAtlas::Type::kAlphaBitmap);
+  auto host_buffer = HostBuffer::Create(GetContext()->GetResourceAllocator(),
+                                        GetContext()->GetIdleWaiter());
+  ASSERT_TRUE(context && context->IsValid());
+  auto atlas = CreateGlyphAtlas(*GetContext(), context.get(), *host_buffer,
+                                GlyphAtlas::Type::kAlphaBitmap, /*scale=*/1.0f,
+                                atlas_context, text_frame);
 
-  text_frame->SetPerFrameData(/*scale=*/1.0, /*offset=*/Vector2(0, 0),
-                              /*glyph_properties=*/std::nullopt);
-
-  TypographerContextSkia::CollectNewGlyphs(atlas, {text_frame});
-
+  ISize texture_size = atlas->GetTexture()->GetSize();
   TextContents::ComputeVertexData(
       data, text_frame, /*scale=*/1.0, /*entity_transform=*/Matrix(),
       /*basis_transform=*/Matrix(), /*offset=*/Vector2(0, 0),
       /*glyph_properties=*/std::nullopt, atlas);
 
-  EXPECT_NEAR(data[0].uv.x * texture_descriptor.size.width, -0.5, 0.001);
-  EXPECT_NEAR(data[0].uv.y * texture_descriptor.size.height, -0.5, 0.001);
+  EXPECT_NEAR(data[0].uv.x * texture_size.width, 0.5, 0.001);
+  EXPECT_NEAR(data[0].uv.y * texture_size.height, 0.5, 0.001);
   EXPECT_NEAR(data[0].position.x, -1.0, 0.001);
   EXPECT_NEAR(data[0].position.y, -41.0, 0.001);
 
-  EXPECT_NEAR(data[1].uv.x * texture_descriptor.size.width, 0.5, 0.001);
-  EXPECT_NEAR(data[1].uv.y * texture_descriptor.size.height, -0.5, 0.001);
+  EXPECT_NEAR(data[1].uv.x * texture_size.width, 53.5, 0.001);
+  EXPECT_NEAR(data[1].uv.y * texture_size.height, 0.5, 0.001);
   EXPECT_NEAR(data[1].position.x, 51.0, 0.001);
   EXPECT_NEAR(data[1].position.y, -41.0, 0.001);
 
-  EXPECT_NEAR(data[2].uv.x * texture_descriptor.size.width, -0.5, 0.001);
-  EXPECT_NEAR(data[2].uv.y * texture_descriptor.size.height, 0.5, 0.001);
+  EXPECT_NEAR(data[2].uv.x * texture_size.width, 0.5, 0.001);
+  EXPECT_NEAR(data[2].uv.y * texture_size.height, 53.5, 0.001);
   EXPECT_NEAR(data[2].position.x, -1.0, 0.001);
   EXPECT_NEAR(data[2].position.y, 11.0, 0.001);
 
-  EXPECT_NEAR(data[3].uv.x * texture_descriptor.size.width, 0.5, 0.001);
-  EXPECT_NEAR(data[3].uv.y * texture_descriptor.size.height, -0.5, 0.001);
+  EXPECT_NEAR(data[3].uv.x * texture_size.width, 53.5, 0.001);
+  EXPECT_NEAR(data[3].uv.y * texture_size.height, 0.5, 0.001);
   EXPECT_NEAR(data[3].position.x, 51.0, 0.001);
   EXPECT_NEAR(data[3].position.y, -41.0, 0.001);
 
-  EXPECT_NEAR(data[4].uv.x * texture_descriptor.size.width, -0.5, 0.001);
-  EXPECT_NEAR(data[4].uv.y * texture_descriptor.size.height, 0.5, 0.001);
+  EXPECT_NEAR(data[4].uv.x * texture_size.width, 0.5, 0.001);
+  EXPECT_NEAR(data[4].uv.y * texture_size.height, 53.5, 0.001);
   EXPECT_NEAR(data[4].position.x, -1.0, 0.001);
   EXPECT_NEAR(data[4].position.y, 11.0, 0.001);
 
-  EXPECT_NEAR(data[5].uv.x * texture_descriptor.size.width, 0.5, 0.001);
-  EXPECT_NEAR(data[5].uv.y * texture_descriptor.size.height, 0.5, 0.001);
+  EXPECT_NEAR(data[5].uv.x * texture_size.width, 53.5, 0.001);
+  EXPECT_NEAR(data[5].uv.y * texture_size.height, 53.5, 0.001);
   EXPECT_NEAR(data[5].position.x, 51.0, 0.001);
   EXPECT_NEAR(data[5].position.y, 11.0, 0.001);
 }
