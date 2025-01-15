2cfb2b5df7ab6384b8f4e00570b195d6cf670d5b
Jason Simmons <jsimmons@google.com>
2025-01-15 03:21:45 +0000

    final var

--- engine/src/flutter/lib/web_ui/dev/steps/copy_artifacts_step.dart
+++ engine/src/flutter/lib/web_ui/dev/steps/copy_artifacts_step.dart
@@ -201,7 +201,7 @@ class CopyArtifactsStep implements PipelineStep {
 
     for (final io.File imageFile in testImagesDir.listSync(recursive: true).whereType<io.File>()) {
       // Skip files that are used by Skia to test handling of invalid input.
-      String imageBaseName = pathlib.basename(imageFile.path);
+      final String imageBaseName = pathlib.basename(imageFile.path);
       if (imageBaseName.contains('invalid') || imageBaseName.contains('missing_eof')) {
         continue;
       }
