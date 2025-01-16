cc57a8614a942f29f468494e39f1a4f8d875cd33
Christopher Fujino <christopherfujino@gmail.com>
2025-01-15 17:23:18 -0800

    fix compile_proto.sh

--- dev/conductor/core/lib/src/proto/conductor_state.pbjson.dart
+++ dev/conductor/core/lib/src/proto/conductor_state.pbjson.dart
@@ -8,7 +8,7 @@
 //
 // @dart = 2.12
 
-// ignore_for_file: annotate_overrides, camel_case_types
+// ignore_for_file: annotate_overrides, camel_case_types, comment_references
 // ignore_for_file: constant_identifier_names, library_prefixes
 // ignore_for_file: non_constant_identifier_names, prefer_final_fields
 // ignore_for_file: unnecessary_import, unnecessary_this, unused_import
@@ -34,8 +34,8 @@ const ReleasePhase$json = {
 };
 
 /// Descriptor for `ReleasePhase`. Decode as a `google.protobuf.EnumDescriptorProto`.
-final $typed_data.Uint8List releasePhaseDescriptor = $convert
-    .base64Decode('CgxSZWxlYXNlUGhhc2USHAoYQVBQTFlfRU5HSU5FX0NIRVJSWVBJQ0tTEAASFAoQVkVSSUZZX0'
+final $typed_data.Uint8List releasePhaseDescriptor =
+    $convert.base64Decode('CgxSZWxlYXNlUGhhc2USHAoYQVBQTFlfRU5HSU5FX0NIRVJSWVBJQ0tTEAASFAoQVkVSSUZZX0'
         'VOR0lORV9DSRABEh8KG0FQUExZX0ZSQU1FV09SS19DSEVSUllQSUNLUxACEhMKD1BVQkxJU0hf'
         'VkVSU0lPThADEhIKDlZFUklGWV9SRUxFQVNFEAUSFQoRUkVMRUFTRV9DT01QTEVURUQQBiIECA'
         'QQBA==');
@@ -52,8 +52,8 @@ const CherrypickState$json = {
 };
 
 /// Descriptor for `CherrypickState`. Decode as a `google.protobuf.EnumDescriptorProto`.
-final $typed_data.Uint8List cherrypickStateDescriptor = $convert
-    .base64Decode('Cg9DaGVycnlwaWNrU3RhdGUSCwoHUEVORElORxAAEhkKFVBFTkRJTkdfV0lUSF9DT05GTElDVB'
+final $typed_data.Uint8List cherrypickStateDescriptor =
+    $convert.base64Decode('Cg9DaGVycnlwaWNrU3RhdGUSCwoHUEVORElORxAAEhkKFVBFTkRJTkdfV0lUSF9DT05GTElDVB'
         'ABEg0KCUNPTVBMRVRFRBACEg0KCUFCQU5ET05FRBAD');
 
 @$core.Deprecated('Use releaseTypeDescriptor instead')
@@ -68,8 +68,8 @@ const ReleaseType$json = {
 };
 
 /// Descriptor for `ReleaseType`. Decode as a `google.protobuf.EnumDescriptorProto`.
-final $typed_data.Uint8List releaseTypeDescriptor = $convert
-    .base64Decode('CgtSZWxlYXNlVHlwZRISCg5TVEFCTEVfSU5JVElBTBAAEhEKDVNUQUJMRV9IT1RGSVgQARIQCg'
+final $typed_data.Uint8List releaseTypeDescriptor =
+    $convert.base64Decode('CgtSZWxlYXNlVHlwZRISCg5TVEFCTEVfSU5JVElBTBAAEhEKDVNUQUJMRV9IT1RGSVgQARIQCg'
         'xCRVRBX0lOSVRJQUwQAhIPCgtCRVRBX0hPVEZJWBAD');
 
 @$core.Deprecated('Use remoteDescriptor instead')
@@ -96,8 +96,8 @@ const Cherrypick$json = {
 };
 
 /// Descriptor for `Cherrypick`. Decode as a `google.protobuf.DescriptorProto`.
-final $typed_data.Uint8List cherrypickDescriptor = $convert
-    .base64Decode('CgpDaGVycnlwaWNrEiQKDXRydW5rUmV2aXNpb24YASABKAlSDXRydW5rUmV2aXNpb24SKAoPYX'
+final $typed_data.Uint8List cherrypickDescriptor =
+    $convert.base64Decode('CgpDaGVycnlwaWNrEiQKDXRydW5rUmV2aXNpb24YASABKAlSDXRydW5rUmV2aXNpb24SKAoPYX'
         'BwbGllZFJldmlzaW9uGAIgASgJUg9hcHBsaWVkUmV2aXNpb24SNgoFc3RhdGUYAyABKA4yIC5j'
         'b25kdWN0b3Jfc3RhdGUuQ2hlcnJ5cGlja1N0YXRlUgVzdGF0ZQ==');
 
@@ -111,22 +111,15 @@ const Repository$json = {
     {'1': 'checkoutPath', '3': 4, '4': 1, '5': 9, '10': 'checkoutPath'},
     {'1': 'upstream', '3': 5, '4': 1, '5': 11, '6': '.conductor_state.Remote', '10': 'upstream'},
     {'1': 'mirror', '3': 6, '4': 1, '5': 11, '6': '.conductor_state.Remote', '10': 'mirror'},
-    {
-      '1': 'cherrypicks',
-      '3': 7,
-      '4': 3,
-      '5': 11,
-      '6': '.conductor_state.Cherrypick',
-      '10': 'cherrypicks'
-    },
+    {'1': 'cherrypicks', '3': 7, '4': 3, '5': 11, '6': '.conductor_state.Cherrypick', '10': 'cherrypicks'},
     {'1': 'dartRevision', '3': 8, '4': 1, '5': 9, '10': 'dartRevision'},
     {'1': 'workingBranch', '3': 9, '4': 1, '5': 9, '10': 'workingBranch'},
   ],
 };
 
 /// Descriptor for `Repository`. Decode as a `google.protobuf.DescriptorProto`.
-final $typed_data.Uint8List repositoryDescriptor = $convert
-    .base64Decode('CgpSZXBvc2l0b3J5EigKD2NhbmRpZGF0ZUJyYW5jaBgBIAEoCVIPY2FuZGlkYXRlQnJhbmNoEi'
+final $typed_data.Uint8List repositoryDescriptor =
+    $convert.base64Decode('CgpSZXBvc2l0b3J5EigKD2NhbmRpZGF0ZUJyYW5jaBgBIAEoCVIPY2FuZGlkYXRlQnJhbmNoEi'
         'gKD3N0YXJ0aW5nR2l0SGVhZBgCIAEoCVIPc3RhcnRpbmdHaXRIZWFkEiYKDmN1cnJlbnRHaXRI'
         'ZWFkGAMgASgJUg5jdXJyZW50R2l0SGVhZBIiCgxjaGVja291dFBhdGgYBCABKAlSDGNoZWNrb3'
         'V0UGF0aBIzCgh1cHN0cmVhbRgFIAEoCzIXLmNvbmR1Y3Rvcl9zdGF0ZS5SZW1vdGVSCHVwc3Ry'
@@ -142,40 +135,20 @@ const ConductorState$json = {
     {'1': 'releaseChannel', '3': 1, '4': 1, '5': 9, '10': 'releaseChannel'},
     {'1': 'releaseVersion', '3': 2, '4': 1, '5': 9, '10': 'releaseVersion'},
     {'1': 'engine', '3': 4, '4': 1, '5': 11, '6': '.conductor_state.Repository', '10': 'engine'},
-    {
-      '1': 'framework',
-      '3': 5,
-      '4': 1,
-      '5': 11,
-      '6': '.conductor_state.Repository',
-      '10': 'framework'
-    },
+    {'1': 'framework', '3': 5, '4': 1, '5': 11, '6': '.conductor_state.Repository', '10': 'framework'},
     {'1': 'createdDate', '3': 6, '4': 1, '5': 3, '10': 'createdDate'},
     {'1': 'lastUpdatedDate', '3': 7, '4': 1, '5': 3, '10': 'lastUpdatedDate'},
     {'1': 'logs', '3': 8, '4': 3, '5': 9, '10': 'logs'},
-    {
-      '1': 'currentPhase',
-      '3': 9,
-      '4': 1,
-      '5': 14,
-      '6': '.conductor_state.ReleasePhase',
-      '10': 'currentPhase'
-    },
+    {'1': 'currentPhase', '3': 9, '4': 1, '5': 14, '6': '.conductor_state.ReleasePhase', '10': 'currentPhase'},
     {'1': 'conductorVersion', '3': 10, '4': 1, '5': 9, '10': 'conductorVersion'},
-    {
-      '1': 'releaseType',
-      '3': 11,
-      '4': 1,
-      '5': 14,
-      '6': '.conductor_state.ReleaseType',
-      '10': 'releaseType'
-    },
+    {'1': 'releaseType', '3': 11, '4': 1, '5': 14, '6': '.conductor_state.ReleaseType', '10': 'releaseType'},
+    {'1': 'isMonorepo', '3': 12, '4': 1, '5': 8, '10': 'isMonorepo'},
   ],
 };
 
 /// Descriptor for `ConductorState`. Decode as a `google.protobuf.DescriptorProto`.
-final $typed_data.Uint8List conductorStateDescriptor = $convert
-    .base64Decode('Cg5Db25kdWN0b3JTdGF0ZRImCg5yZWxlYXNlQ2hhbm5lbBgBIAEoCVIOcmVsZWFzZUNoYW5uZW'
+final $typed_data.Uint8List conductorStateDescriptor =
+    $convert.base64Decode('Cg5Db25kdWN0b3JTdGF0ZRImCg5yZWxlYXNlQ2hhbm5lbBgBIAEoCVIOcmVsZWFzZUNoYW5uZW'
         'wSJgoOcmVsZWFzZVZlcnNpb24YAiABKAlSDnJlbGVhc2VWZXJzaW9uEjMKBmVuZ2luZRgEIAEo'
         'CzIbLmNvbmR1Y3Rvcl9zdGF0ZS5SZXBvc2l0b3J5UgZlbmdpbmUSOQoJZnJhbWV3b3JrGAUgAS'
         'gLMhsuY29uZHVjdG9yX3N0YXRlLlJlcG9zaXRvcnlSCWZyYW1ld29yaxIgCgtjcmVhdGVkRGF0'
@@ -183,4 +156,5 @@ final $typed_data.Uint8List conductorStateDescriptor = $convert
         'RlZERhdGUSEgoEbG9ncxgIIAMoCVIEbG9ncxJBCgxjdXJyZW50UGhhc2UYCSABKA4yHS5jb25k'
         'dWN0b3Jfc3RhdGUuUmVsZWFzZVBoYXNlUgxjdXJyZW50UGhhc2USKgoQY29uZHVjdG9yVmVyc2'
         'lvbhgKIAEoCVIQY29uZHVjdG9yVmVyc2lvbhI+CgtyZWxlYXNlVHlwZRgLIAEoDjIcLmNvbmR1'
-        'Y3Rvcl9zdGF0ZS5SZWxlYXNlVHlwZVILcmVsZWFzZVR5cGU=');
+        'Y3Rvcl9zdGF0ZS5SZWxlYXNlVHlwZVILcmVsZWFzZVR5cGUSHgoKaXNNb25vcmVwbxgMIAEoCF'
+        'IKaXNNb25vcmVwbw==');
