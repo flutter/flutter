d3f97339683985b1136fbf53e833b628631046d1
Jason Simmons <jsimmons@google.com>
2025-01-16 22:56:04 +0000

    formatting

--- packages/flutter_tools/lib/src/isolated/devfs_web.dart
+++ packages/flutter_tools/lib/src/isolated/devfs_web.dart
@@ -1031,7 +1031,7 @@ class WebDevFS implements DevFS {
               entrypoint: entrypoint,
               nullAssertions: nullAssertions,
               nativeNullAssertions: nativeNullAssertions,
-          loaderRootDirectory: _baseUri.toString(),
+              loaderRootDirectory: _baseUri.toString(),
             ),
       );
       // TODO(zanderso): refactor the asset code in this and the regular devfs to
