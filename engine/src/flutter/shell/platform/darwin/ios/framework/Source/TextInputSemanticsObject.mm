a5f8d646a76a1131dfd0049edf9d983b8b128d13
importRyan <ryan@macrofactorapp.com>
2025-01-22 17:30:56 +0900

    fix: remove unnecessary assertion

--- engine/src/flutter/shell/platform/darwin/ios/framework/Source/TextInputSemanticsObject.mm
+++ engine/src/flutter/shell/platform/darwin/ios/framework/Source/TextInputSemanticsObject.mm
@@ -42,7 +42,14 @@ static const UIAccessibilityTraits kUIAccessibilityTraitUndocumentedEmptyLine = 
   NSAssert([range isKindOfClass:[FlutterTextRange class]],
            @"Expected a FlutterTextRange for range (got %@).", [range class]);
   NSRange textRange = ((FlutterTextRange*)range).range;
-  NSAssert(textRange.location != NSNotFound, @"Expected a valid text range.");
+  if (textRange.location == NSNotFound) {
+    // Avoids [crashes](https://github.com/flutter/flutter/issues/138464) from an assertion
+    // against NSNotFound.
+    // TODO(hellohuanlin): This is a temp workaround, but we should look into why
+    // framework is providing NSNotFound to the engine.
+    // https://github.com/flutter/flutter/issues/160100
+    return nil;
+  }
   return [self.text substringWithRange:textRange];
 }
 
