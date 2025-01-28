9c514455cd4258a03dd20f178d4c8aaef127271b
Andrew Kolos <andrewrkolos@gmail.com>
2025-01-27 23:06:08 -0800

    make test against message more precise

--- packages/flutter_tools/test/general.shard/android/android_workflow_test.dart
+++ packages/flutter_tools/test/general.shard/android/android_workflow_test.dart
@@ -529,7 +529,13 @@ Review licenses that have not been accepted (y/N)?
 
     final ValidationMessage cmdlineMessage = validationResult.messages.last;
     expect(cmdlineMessage.type, ValidationMessageType.error);
-    expect(cmdlineMessage.message, startsWith('cmdline-tools component is missing.\n'));
+    expect(
+      cmdlineMessage.message,
+      'cmdline-tools component is missing.\n'
+      'Try installing or updating Android Studio.\n'
+      'Alternatively, download the tools from https://developer.android.com/studio#command-line-tools-only and make sure to set the ANDROID_HOME environment variable.\n'
+      'See https://developer.android.com/studio/command-line for more details.',
+    );
   });
 
   testUsingContext('detects minimum required java version', () async {
