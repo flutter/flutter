9bd733cae50d4fee61cb1ed57438db384bc90770
zijiehe@ <68449066+zijiehe-google-com@users.noreply.github.com>
2025-01-28 15:20:49 -0800

    Update dart_component_controller.cc

--- engine/src/flutter/shell/platform/fuchsia/dart_runner/dart_component_controller.cc
+++ engine/src/flutter/shell/platform/fuchsia/dart_runner/dart_component_controller.cc
@@ -223,7 +223,7 @@ bool DartComponentController::CreateAndBindNamespace() {
   }
 
   auto composed_service_dir = std::make_unique<vfs::ComposedServiceDir>();
-  composed_service_dir->set_fallback(std::move(dart_public_dir));
+  composed_service_dir->SetFallback(std::move(dart_public_dir));
 
   // Request an event from the directory to ensure it is servicing requests.
   dart_outgoing_dir_ptr_->Open3(
