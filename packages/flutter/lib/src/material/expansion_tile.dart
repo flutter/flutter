5c59caf83544368fc624215ccaa82616135dfdb0
Abdelhalim Elmenshawi <66807087+Halim-Elmenshawi@users.noreply.github.com>
2025-01-29 18:51:49 +0100

    Update packages/flutter/lib/src/material/expansion_tile.dart
    Co-authored-by: Qun Cheng <36861262+QuncCccccc@users.noreply.github.com>

--- packages/flutter/lib/src/material/expansion_tile.dart
+++ packages/flutter/lib/src/material/expansion_tile.dart
@@ -411,7 +411,7 @@ class ExpansionTile extends StatefulWidget {
   /// The color to display behind the expanded children section of the tile.
   ///
   /// If this property is null, the value from [ExpansionTileThemeData.childrenPaddingColor] is
-  /// used if specified. If both are null, then [backgroundColor] will be used.
+  /// used. If both are null, then [backgroundColor] will be used.
   final Color? childrenPaddingColor;
 
   /// The icon color of tile's expansion arrow icon when the sublist is expanded.
