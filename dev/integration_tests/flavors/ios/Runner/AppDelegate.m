ef9ee574a468a80db56d5be0cf1df4ae3ab0487f
Sameri11 <51940183+Sameri11@users.noreply.github.com>
2025-01-29 17:29:46 +0500

    Cleanup for flavor tests

--- dev/integration_tests/flavors/ios/Runner/AppDelegate.m
+++ dev/integration_tests/flavors/ios/Runner/AppDelegate.m
@@ -10,13 +10,6 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
   [GeneratedPluginRegistrant registerWithRegistry:self];
   // Override point for customization after application launch.
-  FlutterViewController* controller = (FlutterViewController*)self.window.rootViewController;
-  FlutterMethodChannel* flavorChannel = [FlutterMethodChannel methodChannelWithName:@"flavor" binaryMessenger:controller];
-
-  [flavorChannel setMethodCallHandler:^(FlutterMethodCall *call, FlutterResult result) {
-    NSString* flavor = (NSString*)[[NSBundle mainBundle].infoDictionary valueForKey:@"Flavor"];
-    result(flavor);
-  }];
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
