bf67471ea60cdd5c50d86c1a6746011de76ad99b
Ben Konyi <bkonyi@google.com>
2025-01-29 11:21:27 -0500

    Kick Google Testing

--- packages/flutter/lib/widgets.dart
+++ packages/flutter/lib/widgets.dart
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-/// The Flutter widgets framework.
+/// The Flutter widgets framework. KICK
 ///
 /// To use, import `package:flutter/widgets.dart`.
 ///
