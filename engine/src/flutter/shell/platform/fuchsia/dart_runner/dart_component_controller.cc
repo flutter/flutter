3dda1234fd88efc49d4dd8768311cad93b3235a5
zijiehe-google-com <zijiehe@google.com>
2025-01-29 14:56:37 -0800

    format

--- engine/src/flutter/shell/platform/fuchsia/dart_runner/dart_component_controller.cc
+++ engine/src/flutter/shell/platform/fuchsia/dart_runner/dart_component_controller.cc
@@ -223,8 +223,8 @@ bool DartComponentController::CreateAndBindNamespace() {
   }
 
   auto composed_service_dir = std::make_unique<vfs::ComposedServiceDir>();
-  composed_service_dir->SetFallback(fidl::ClientEnd<fuchsia_io::Directory>(
-      dart_public_dir.TakeChannel()));
+  composed_service_dir->SetFallback(
+      fidl::ClientEnd<fuchsia_io::Directory>(dart_public_dir.TakeChannel()));
 
   // Request an event from the directory to ensure it is servicing requests.
   dart_outgoing_dir_ptr_->Open3(
