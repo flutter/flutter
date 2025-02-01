43677623bcc434ef5081c48d7fb68caddf38f65c
Justin McCandless <jmccandless@google.com>
2025-01-31 16:53:00 -0800

    analyzer

--- packages/flutter/test/widgets/system_context_menu_controller_test.dart
+++ packages/flutter/test/widgets/system_context_menu_controller_test.dart
@@ -3,7 +3,6 @@
 // found in the LICENSE file.
 
 import 'package:flutter/services.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import '../services/text_input_utils.dart';
