36703d08b77460c1de30e5243753f4c33951d417
huycozy <huy@nevercode.io>
2025-02-01 11:16:06 +0700

    Fix DropdownMenu example enderFlex overflowed error
    Signed-off-by: huycozy <huy@nevercode.io>

--- examples/api/test/material/dropdown_menu/dropdown_menu.0_test.dart
+++ examples/api/test/material/dropdown_menu/dropdown_menu.0_test.dart
@@ -3,6 +3,7 @@
 // found in the LICENSE file.
 
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter_api_samples/material/dropdown_menu/dropdown_menu.0.dart' as example;
 import 'package:flutter_test/flutter_test.dart';
 
@@ -69,5 +70,41 @@ void main() {
     await tester.tap(iconMenu);
     await tester.pumpAndSettle();
     expect(FocusScope.of(tester.element(iconMenu)).hasFocus, isTrue);
+  });
+
+  testWidgets('DropdownMenu on small screen', (WidgetTester tester) async {
+    await tester.pumpWidget(const example.DropdownMenuExample());
+
+    final Finder colorMenu = find.byType(DropdownMenu<example.ColorLabel>);
+    final Finder iconMenu = find.byType(DropdownMenu<example.IconLabel>);
+    expect(colorMenu, findsOneWidget);
+    expect(iconMenu, findsOneWidget);
+
+    Finder findMenuItem(String label) {
+      return find.widgetWithText(MenuItemButton, label).last;
+    }
+
+    await tester.tap(colorMenu);
+    await tester.pumpAndSettle();
+    final Finder menuBlue = findMenuItem('Blue');
+    await tester.ensureVisible(menuBlue);
+    await tester.tap(menuBlue);
+    await tester.pumpAndSettle();
+
+    await tester.tap(iconMenu);
+    await tester.pumpAndSettle();
+    final Finder menuSmile = findMenuItem('Smile');
+    await tester.ensureVisible(menuSmile);
+    await tester.tap(menuSmile);
+    await tester.pumpAndSettle();
+
+    expect(find.text('You selected a Blue Smile'), findsOneWidget);
+
+    // resize the screen to small screen and make sure no overflowed error appears
+    tester.binding.window.physicalSizeTestValue = const Size(200, 160);
+    tester.binding.window.devicePixelRatioTestValue = 1.0;
+    addTearDown(tester.binding.window.clearPhysicalSizeTestValue);
+
+    expect(find.byType(RenderFlex), findsNothing);
   });
 }
