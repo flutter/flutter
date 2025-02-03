a6210a9c4e0187de6ff99c886b40a4e2bbced6e9
Srujan Gaddam <srujzs@google.com>
2025-02-03 13:24:35 -0800

    Unify implementation of hot reload errors test

--- packages/flutter_tools/test/web.shard/hot_reload_web_errors_test.dart
+++ packages/flutter_tools/test/web.shard/hot_reload_web_errors_test.dart
@@ -5,60 +5,21 @@
 @Tags(<String>['flutter-test-driver'])
 library;
 
-import 'package:file/file.dart';
+import 'dart:io';
 
-import '../integration.shard/test_data/hot_reload_const_project.dart';
-import '../integration.shard/test_driver.dart';
-import '../integration.shard/test_utils.dart';
+import '../integration.shard/test_data/hot_reload_errors_common.dart';
 import '../src/common.dart';
 
 void main() {
-  late Directory tempDir;
-  final HotReloadConstProject project = HotReloadConstProject();
-  late FlutterRunTestDriver flutter;
-
-  final List<String> additionalCommandArgs = <String>[
-    '--extra-front-end-options=--dartdevc-canary,--dartdevc-module-format=ddc',
-  ];
-
-  setUp(() async {
-    tempDir = createResolvedTempDirectorySync('hot_reload_test.');
-    await project.setUpIn(tempDir);
-    flutter = FlutterRunTestDriver(tempDir);
-  });
-
-  tearDown(() async {
-    await flutter.stop();
-    tryToDelete(tempDir);
-  });
-
-  testWithoutContext(
-    'hot reload displays an error message when removing a field from a const class',
-    () async {
-      await flutter.run(chrome: true, additionalCommandArgs: additionalCommandArgs);
-      project.removeFieldFromConstClass();
-
-      expect(
-        flutter.hotReload(),
-        throwsA(
-          isA<Exception>().having(
-            (Exception e) => e.toString(),
-            'message',
-            // TODO(srujzs): Change this to "Try performing a hot restart instead." once we emit
-            // that string in the delta inspector. Then unify this test with
-            // hot_reload_errors_test.dart.
-            contains('Const class cannot remove fields'),
-          ),
-        ),
-      );
-    },
+  testAll(
+    chrome: true,
+    additionalCommandArgs: <String>[
+      '--extra-front-end-options=--dartdevc-canary,--dartdevc-module-format=ddc',
+    ],
+    // TODO(srujzs): Remove this custom message once we have the delta inspector emitting the same
+    // string as the VM.
+    constClassFieldRemovalErrorMessage: 'Const class cannot remove fields',
+    // https://github.com/flutter/flutter/issues/162567
+    skip: Platform.isWindows,
   );
-
-  testWithoutContext('hot restart succeeds when removing a field from a const class', () async {
-    // We should have used `recompile-restart` to avoid the errors the DDC delta
-    // inspector emits.
-    await flutter.run(chrome: true, additionalCommandArgs: additionalCommandArgs);
-    project.removeFieldFromConstClass();
-    await flutter.hotRestart();
-  });
 }
