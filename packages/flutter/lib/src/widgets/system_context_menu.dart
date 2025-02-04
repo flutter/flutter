69a549c978eda68c1196377a6a6d408e8e1dba73
Justin McCandless <jmccandless@google.com>
2025-02-04 13:08:06 -0800

    Docs nits from review

--- packages/flutter/lib/src/widgets/system_context_menu.dart
+++ packages/flutter/lib/src/widgets/system_context_menu.dart
@@ -173,14 +173,14 @@ class _SystemContextMenuState extends State<SystemContextMenu> {
   }
 }
 
-/// Describes a context menu button that will be rendered in the system context
-/// menu and not by Flutter itself.
+/// Describes a context menu button that will be rendered in the iOS system
+/// context menu and not by Flutter itself.
 ///
 /// See also:
 ///
 ///  * [SystemContextMenu], a widget that can be used to display the system
 ///    context menu.
-///  * [IOSSystemContextMenuItem], which performs a similar role but at the
+///  * [IOSSystemContextMenuItemData], which performs a similar role but at the
 ///    method channel level and mirrors the requirements of the method channel
 ///    API.
 ///  * [ContextMenuButtonItem], which performs a similar role for Flutter-drawn
