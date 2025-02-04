f98aa34693533055b32dc43e14fd7bb08cbf7549
LouiseHsu <louisehsu@google.com>
2025-02-03 16:07:19 -0800

    Add Benchmarks and examples to compare swiftui and flutter  (#160681)
    Partially addresses https://github.com/flutter/flutter/issues/154138,
    specifically
    [#162025](https://github.com/flutter/flutter/issues/162025),
    [#162026](https://github.com/flutter/flutter/issues/162026),
    [#162028](https://github.com/flutter/flutter/issues/162028),
    [#162029](https://github.com/flutter/flutter/issues/162029)
    
    ## Pre-launch Checklist
    
    - [x] I read the [Contributor Guide] and followed the process outlined
    there for submitting PRs.
    - [x] I read the [Tree Hygiene] wiki page, which explains my
    responsibilities.
    - [x] I read and followed the [Flutter Style Guide], including [Features
    we expect every widget to implement].
    - [x] I signed the [CLA].
    - [x] I listed at least one issue that this PR fixes in the description
    above.
    - [x] I updated/added relevant documentation (doc comments with `///`).
    - [x] I added new tests to check the change I am making, or this PR is
    [test-exempt].
    - [x] I followed the [breaking change policy] and added [Data Driven
    Fixes] where supported.
    - [x] All existing and new tests are passing.

---
+++ dev/benchmarks/imitation_game_swiftui/hello_world_swiftui/ContentView.swift
@@ -0,0 +1,49 @@
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var items: [String] = Array(repeating: "Hello", count: 50)
+    @State private var isLoadingMore = false
+    private let fetchThreshold = 5
+
+    var body: some View {
+        NavigationView { // Add a NavigationView for better view management
+            List {
+                ForEach(items.indices, id: \.self) { index in
+                    Text(items[index])
+                        .onAppear {
+                            if index == items.count - fetchThreshold {
+                                loadMoreContent()
+                            }
+                        }
+                }
+
+                if isLoadingMore {
+                    ProgressView()
+                        .frame(maxWidth: .infinity) // Center the indicator
+                }
+            }
+            .navigationTitle("Infinite Scroll") // Set a title for clarity
+        }
+    }
+
+    func loadMoreContent() {
+        if isLoadingMore {
+            return
+        }
+
+        isLoadingMore = true
+
+        let newItems = Array(repeating: "Hello", count: 20) // Fetch 20 more items
+        items.append(contentsOf: newItems)
+        isLoadingMore = false
+    }
+}
+
+
+#Preview {
+    ContentView()
+}
