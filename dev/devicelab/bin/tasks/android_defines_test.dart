ad7e4a7fd0071f791efa085a1798cb1055e5fcfc
Reid Baker <reidbaker@google.com>
2025-02-06 12:23:03 -1000

    take over another already scheduled ci task, reconfigure .ci.yaml to match simplier emulator integration test

--- dev/devicelab/bin/tasks/android_defines_test.dart
+++ dev/devicelab/bin/tasks/android_defines_test.dart
@@ -8,6 +8,7 @@ import 'package:flutter_devicelab/tasks/integration_tests.dart';
 
 /// Verify that dart defines work on Android.
 Future<void> main() async {
+  // DO NOT MERGE.
   deviceOperatingSystem = DeviceOperatingSystem.android;
-  await task(dartDefinesTask());
+  await task(createDisplayCutoutTest());
 }
