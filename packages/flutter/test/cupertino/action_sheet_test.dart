0a151c2ae0324c2b23eb1abd8d8ddd3e7db1d105
Chun-Heng Tai <chtai@google.com>
2025-02-06 10:51:09 -0800

    update

--- packages/flutter/test/cupertino/action_sheet_test.dart
+++ packages/flutter/test/cupertino/action_sheet_test.dart
@@ -11,7 +11,6 @@ import 'dart:ui';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
