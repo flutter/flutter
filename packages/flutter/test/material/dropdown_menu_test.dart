1bb35d2ffa1ed9c06264fe2b3772934f2ed55c66
yiiim <ybz975218925@live.com>
2025-02-07 21:50:37 +0800

    fix test

--- packages/flutter/test/material/dropdown_menu_test.dart
+++ packages/flutter/test/material/dropdown_menu_test.dart
@@ -3954,19 +3954,25 @@ void main() {
   });
 
   // Regression test for https://github.com/flutter/flutter/issues/162539
-  testWidgets('When requestFocusOnTap is true, the TextField should gain focus after being tapped.', (WidgetTester tester) async {
-    await tester.pumpWidget(
-      MaterialApp(
-        home: Scaffold(
-          body: DropdownMenu<TestMenu>(dropdownMenuEntries: menuChildren, requestFocusOnTap: true),
+  testWidgets(
+    'When requestFocusOnTap is true, the TextField should gain focus after being tapped.',
+    (WidgetTester tester) async {
+      await tester.pumpWidget(
+        MaterialApp(
+          home: Scaffold(
+            body: DropdownMenu<TestMenu>(
+              dropdownMenuEntries: menuChildren,
+              requestFocusOnTap: true,
+            ),
+          ),
         ),
-      ),
-    );
-    await tester.tap(find.byType(TextField));
-    await tester.pumpAndSettle();
-    final Element textField = tester.firstElement(find.byType(TextField));
-    expect(Focus.of(textField).hasFocus, isTrue);
-  });
+      );
+      await tester.tap(find.byType(TextField));
+      await tester.pumpAndSettle();
+      final Element textField = tester.firstElement(find.byType(TextField));
+      expect(Focus.of(textField).hasFocus, isTrue);
+    },
+  );
 }
 
 enum TestMenu {
