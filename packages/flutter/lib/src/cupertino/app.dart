3c952ba3cc03e23bd32a42823dea32ce5e54c03a
jesswrd <jesswon@google.com>
2025-02-06 16:21:31 -0800

    added brightness comment

--- packages/flutter/lib/src/cupertino/app.dart
+++ packages/flutter/lib/src/cupertino/app.dart
@@ -652,6 +652,8 @@ class _CupertinoAppState extends State<CupertinoApp> {
   Widget build(BuildContext context) {
     final CupertinoThemeData effectiveThemeData = (widget.theme ?? const CupertinoThemeData())
         .resolveFrom(context);
+
+    // Prefer theme brightness if set, otherwise check system brightness.
     final Brightness brightness =
         effectiveThemeData.brightness ?? MediaQuery.platformBrightnessOf(context);
 
