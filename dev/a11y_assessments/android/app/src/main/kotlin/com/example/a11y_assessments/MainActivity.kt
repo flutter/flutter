b2d191b861626f70bf372e7f19e3673fb291bfaf
YoheiFF <146219546+YoheiFF@users.noreply.github.com>
2025-02-09 23:37:37 +0900

    初期構築

--- dev/a11y_assessments/android/app/src/main/kotlin/com/example/a11y_assessments/MainActivity.kt
+++ dev/a11y_assessments/android/app/src/main/kotlin/com/example/a11y_assessments/MainActivity.kt
@@ -1,5 +1,6 @@
-package com.example.a11y_assessments
+package com.example.myapp
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterFragmentActivity() {
+}
