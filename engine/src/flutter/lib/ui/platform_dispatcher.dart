37fbbf56234feb3e6a5a75a94ac0f849d58577d4
Renzo Olivares <roliv@google.com>
2025-02-11 12:33:34 -0800

    et format

--- engine/src/flutter/lib/ui/platform_dispatcher.dart
+++ engine/src/flutter/lib/ui/platform_dispatcher.dart
@@ -1886,7 +1886,7 @@ class _PlatformConfiguration {
   /// configuration updates from the embedder yet. The _getScaledFontSize
   /// function should not be called in either case.
   final int? configurationId;
-  
+
   final TypographySettings? typographySettings;
 }
 
