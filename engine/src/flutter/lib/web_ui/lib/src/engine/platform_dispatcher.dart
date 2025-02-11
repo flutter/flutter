d32226a4ff9c29b7eaee5826240511a4b5288921
Renzo Olivares <roliv@google.com>
2025-02-11 12:32:33 -0800

    Move TypographySettings to dart:ui and start basic web implementation

--- engine/src/flutter/lib/web_ui/lib/src/engine/platform_dispatcher.dart
+++ engine/src/flutter/lib/web_ui/lib/src/engine/platform_dispatcher.dart
@@ -66,6 +66,12 @@ class EnginePlatformDispatcher extends ui.PlatformDispatcher {
     locales: parseBrowserLanguages(),
     textScaleFactor: findBrowserTextScaleFactor(),
     accessibilityFeatures: computeAccessibilityFeatures(),
+    typographySettings: ui.TypographySettings(
+      lineHeight: 5.0,
+      letterSpacing: 5.0,
+      wordSpacing: 5.0,
+      paragraphSpacing: 5.0,
+    ),
   );
 
   /// Compute accessibility features based on the current value of high contrast flag
@@ -758,6 +764,9 @@ class EnginePlatformDispatcher extends ui.PlatformDispatcher {
       await renderer.renderScene(scene, target);
     }
   }
+
+  @override
+  ui.TypographySettings? get typographySettings => configuration.typographySettings;
 
   /// Additional accessibility features that may be enabled by the platform.
   @override
@@ -1678,6 +1687,7 @@ class PlatformConfiguration {
     this.locales = const <ui.Locale>[],
     this.defaultRouteName = '/',
     this.systemFontFamily,
+    this.typographySettings,
   });
 
   PlatformConfiguration copyWith({
@@ -1689,6 +1699,7 @@ class PlatformConfiguration {
     List<ui.Locale>? locales,
     String? defaultRouteName,
     String? systemFontFamily,
+    ui.TypographySettings? typographySettings,
   }) {
     return PlatformConfiguration(
       accessibilityFeatures: accessibilityFeatures ?? this.accessibilityFeatures,
@@ -1699,6 +1710,7 @@ class PlatformConfiguration {
       locales: locales ?? this.locales,
       defaultRouteName: defaultRouteName ?? this.defaultRouteName,
       systemFontFamily: systemFontFamily ?? this.systemFontFamily,
+      typographySettings: typographySettings ?? this.typographySettings,
     );
   }
 
@@ -1710,6 +1722,7 @@ class PlatformConfiguration {
   final List<ui.Locale> locales;
   final String defaultRouteName;
   final String? systemFontFamily;
+  final ui.TypographySettings? typographySettings;
 }
 
 /// Helper class to hold navigation target information for AT focus restoration
