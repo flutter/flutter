8805f88162532a6db5a715f2dfbcc42bd5e9ba71
Camille Simon <camillesimon@google.com>
2025-02-14 13:40:14 -0800

    jsoning

--- packages/flutter_tools/gradle/src/main/kotlin/AppLinkSettings.kt
+++ packages/flutter_tools/gradle/src/main/kotlin/AppLinkSettings.kt
@@ -4,9 +4,27 @@
 
 package com.flutter.gradle
 
+import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.buildJsonObject
+import kotlinx.serialization.json.put
+import kotlinx.serialization.json.putJsonArray
+
 data class AppLinkSettings(
     val applicationId: String
 ) {
     var deeplinkingFlagEnabled = false
     val deeplinks = mutableSetOf<Deeplink>()
+
+    fun toJson(): JsonObject {
+        return buildJsonObject {
+            put("applicationId", applicationId)
+            put("deeplinkingFlagEnabled", deeplinkingFlagEnabled)
+            putJsonArray("deeplinks") {
+                for (deeplink:Deeplink in deeplinks) {
+                    add(deeplink.toJson())
+                }
+            }
+
+        }
+    }
 }
