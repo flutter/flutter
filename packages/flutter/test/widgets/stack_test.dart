cb485eaf813eff4da3ec67ef3f52f2cdab824f33
Simon Hadenius <simonhadenius@gmail.com>
2025-02-15 21:47:06 +0100

    fix formatting

--- packages/flutter/test/widgets/stack_test.dart
+++ packages/flutter/test/widgets/stack_test.dart
@@ -489,163 +489,145 @@ void main() {
     }
   });
 
-  testWidgets('IndexedStack excludes focus for hidden children if maintainInteractivity is set to false', (WidgetTester tester) async {
-    const List<Widget> children = <Widget>[
-      Focus(child: Text('child 0')),
-      Focus(child: Text('child 1')),
-    ];
+  testWidgets(
+    'IndexedStack excludes focus for hidden children if maintainInteractivity is set to false',
+    (WidgetTester tester) async {
+      const List<Widget> children = <Widget>[
+        Focus(child: Text('child 0')),
+        Focus(child: Text('child 1')),
+      ];
 
-    Future<void> pumpIndexedStack(int? activeIndex) async {
+      Future<void> pumpIndexedStack(int? activeIndex) async {
+        await tester.pumpWidget(
+          Directionality(
+            textDirection: TextDirection.ltr,
+            child: IndexedStack(
+              maintainInteractivity: false,
+              index: activeIndex,
+              children: children,
+            ),
+          ),
+        );
+      }
+
+      Future<void> requestFocusAndPump(FocusNode node) async {
+        node.requestFocus();
+        await tester.pump();
+      }
+
+      await pumpIndexedStack(0);
+
+      final Element child0 = tester.element(find.text('child 0', skipOffstage: false));
+      final Element child1 = tester.element(find.text('child 1', skipOffstage: false));
+      final FocusNode child0FocusNode = Focus.of(child0);
+      final FocusNode child1FocusNode = Focus.of(child1);
+
+      await requestFocusAndPump(child0FocusNode);
+
+      expect(child0FocusNode.hasFocus, true);
+      expect(child1FocusNode.hasFocus, false);
+
+      await requestFocusAndPump(child1FocusNode);
+
+      expect(child0FocusNode.hasFocus, true);
+      expect(child1FocusNode.hasFocus, false);
+
+      await pumpIndexedStack(1);
+      await requestFocusAndPump(child1FocusNode);
+
+      expect(child0FocusNode.hasFocus, false);
+      expect(child1FocusNode.hasFocus, true);
+
+      await requestFocusAndPump(child0FocusNode);
+
+      expect(child0FocusNode.hasFocus, false);
+      expect(child1FocusNode.hasFocus, true);
+    },
+  );
+
+  testWidgets(
+    'IndexedStack: hidden children can be focused if maintainInteractivity is set to true',
+    (WidgetTester tester) async {
+      const List<Widget> children = <Widget>[
+        Focus(child: Text('child 0')),
+        Focus(child: Text('child 1')),
+      ];
+
+      Future<void> pumpIndexedStack(int? activeIndex) async {
+        await tester.pumpWidget(
+          Directionality(
+            textDirection: TextDirection.ltr,
+            child: IndexedStack(index: activeIndex, children: children),
+          ),
+        );
+      }
+
+      Future<void> requestFocusAndPump(FocusNode node) async {
+        node.requestFocus();
+        await tester.pump();
+      }
+
+      await pumpIndexedStack(0);
+
+      final Element child0 = tester.element(find.text('child 0', skipOffstage: false));
+      final Element child1 = tester.element(find.text('child 1', skipOffstage: false));
+      final FocusNode child0FocusNode = Focus.of(child0);
+      final FocusNode child1FocusNode = Focus.of(child1);
+
+      await requestFocusAndPump(child0FocusNode);
+
+      expect(child0FocusNode.hasFocus, true);
+      expect(child1FocusNode.hasFocus, false);
+
+      await requestFocusAndPump(child1FocusNode);
+
+      expect(child0FocusNode.hasFocus, false);
+      expect(child1FocusNode.hasFocus, true);
+    },
+  );
+
+  testWidgets(
+    'IndexedStack: hidden children can not receive tap events if maintainInteractivity is set to false',
+    (WidgetTester tester) async {
+      bool tapped = false;
+      final List<Widget> children = <Widget>[
+        const Text('child'),
+        GestureDetector(onTap: () => tapped = true, child: const Text('hiddenChild')),
+      ];
+
       await tester.pumpWidget(
         Directionality(
           textDirection: TextDirection.ltr,
-          child: IndexedStack(
-            maintainInteractivity: false,
-            index: activeIndex,
-            children: children,
-          ),
+          child: IndexedStack(maintainInteractivity: false, children: children),
         ),
       );
-    }
-    Future<void> requestFocusAndPump(FocusNode node) async {
-      node.requestFocus();
+
+      await tester.tap(find.text('hiddenChild', skipOffstage: false), warnIfMissed: false);
       await tester.pump();
-    }
 
-    await pumpIndexedStack(0);
+      expect(tapped, false);
+    },
+  );
 
-    final Element child0 = tester.element(
-      find.text('child 0', skipOffstage: false),
-    );
-    final Element child1 = tester.element(
-      find.text('child 1', skipOffstage: false),
-    );
-    final FocusNode child0FocusNode = Focus.of(child0);
-    final FocusNode child1FocusNode = Focus.of(child1);
+  testWidgets(
+    'IndexedStack: hidden children can not receive tap events if maintainInteractivity is set to true',
+    (WidgetTester tester) async {
+      bool tapped = false;
+      final List<Widget> children = <Widget>[
+        const Text('child'),
+        GestureDetector(onTap: () => tapped = true, child: const Text('hiddenChild')),
+      ];
 
-    await requestFocusAndPump(child0FocusNode);
-
-    expect(child0FocusNode.hasFocus, true);
-    expect(child1FocusNode.hasFocus, false);
-
-    await requestFocusAndPump(child1FocusNode);
-
-    expect(child0FocusNode.hasFocus, true);
-    expect(child1FocusNode.hasFocus, false);
-
-    await pumpIndexedStack(1);
-    await requestFocusAndPump(child1FocusNode);
-
-    expect(child0FocusNode.hasFocus, false);
-    expect(child1FocusNode.hasFocus, true);
-
-    await requestFocusAndPump(child0FocusNode);
-
-    expect(child0FocusNode.hasFocus, false);
-    expect(child1FocusNode.hasFocus, true);
-  });
-
-  testWidgets('IndexedStack: hidden children can be focused if maintainInteractivity is set to true', (WidgetTester tester) async {
-    const List<Widget> children = <Widget>[
-      Focus(child: Text('child 0')),
-      Focus(child: Text('child 1')),
-    ];
-
-    Future<void> pumpIndexedStack(int? activeIndex) async {
       await tester.pumpWidget(
-        Directionality(
-          textDirection: TextDirection.ltr,
-          child: IndexedStack(
-            index: activeIndex,
-            children: children,
-          ),
-        ),
+        Directionality(textDirection: TextDirection.ltr, child: IndexedStack(children: children)),
       );
-    }
-    Future<void> requestFocusAndPump(FocusNode node) async {
-      node.requestFocus();
+
+      await tester.tap(find.text('hiddenChild', skipOffstage: false), warnIfMissed: false);
       await tester.pump();
-    }
 
-    await pumpIndexedStack(0);
-
-    final Element child0 = tester.element(
-      find.text('child 0', skipOffstage: false),
-    );
-    final Element child1 = tester.element(
-      find.text('child 1', skipOffstage: false),
-    );
-    final FocusNode child0FocusNode = Focus.of(child0);
-    final FocusNode child1FocusNode = Focus.of(child1);
-
-    await requestFocusAndPump(child0FocusNode);
-
-    expect(child0FocusNode.hasFocus, true);
-    expect(child1FocusNode.hasFocus, false);
-
-    await requestFocusAndPump(child1FocusNode);
-
-    expect(child0FocusNode.hasFocus, false);
-    expect(child1FocusNode.hasFocus, true);
-  });
-
-  testWidgets('IndexedStack: hidden children can not receive tap events if maintainInteractivity is set to false', (WidgetTester tester) async {
-    bool tapped = false;
-    final List<Widget> children = <Widget>[
-      const Text('child'),
-      GestureDetector(
-        onTap: () => tapped = true,
-        child: const Text('hiddenChild'),
-      ),
-    ];
-
-    await tester.pumpWidget(
-      Directionality(
-        textDirection: TextDirection.ltr,
-        child: IndexedStack(
-          maintainInteractivity: false,
-          children: children,
-        ),
-      ),
-    );
-
-    await tester.tap(
-      find.text('hiddenChild', skipOffstage: false),
-      warnIfMissed: false,
-    );
-    await tester.pump();
-
-    expect(tapped, false);
-  });
-
-  testWidgets('IndexedStack: hidden children can not receive tap events if maintainInteractivity is set to true', (WidgetTester tester) async {
-    bool tapped = false;
-    final List<Widget> children = <Widget>[
-      const Text('child'),
-      GestureDetector(
-        onTap: () => tapped = true,
-        child: const Text('hiddenChild'),
-      ),
-    ];
-
-    await tester.pumpWidget(
-      Directionality(
-        textDirection: TextDirection.ltr,
-        child: IndexedStack(
-          children: children,
-        ),
-      ),
-    );
-
-    await tester.tap(
-      find.text('hiddenChild', skipOffstage: false),
-      warnIfMissed: false,
-    );
-    await tester.pump();
-
-    expect(tapped, false);
-  });
-
+      expect(tapped, false);
+    },
+  );
 
   testWidgets('Stack clip test', (WidgetTester tester) async {
     await tester.pumpWidget(
