666adac499c701efce1d1fdf279d85a6861061d6
Brendan <2bndy5@gmail.com>
2025-02-18 10:38:41 -0800

    alias magic numbers as constexpr
    add explanatory context in comments

--- engine/src/flutter/shell/platform/linux/fl_view.cc
+++ engine/src/flutter/shell/platform/linux/fl_view.cc
@@ -63,11 +63,17 @@ struct _FlView {
   // Accessible tree from Flutter, exposed as an AtkPlug.
   FlViewAccessible* view_accessible;
 
-  // Signal subscripton for cursor changes.
+  // Signal subscription for cursor changes.
   guint cursor_changed_cb_id;
 
   GCancellable* cancellable;
 };
+
+// 8 corresponds to mouse back button on both x11 and wayland
+static constexpr guint kMouseButtonBack = 8;
+
+// 9 corresponds to mouse forward button on both x11 and wayland
+static constexpr guint kMouseButtonForward = 9;
 
 enum { SIGNAL_FIRST_FRAME, LAST_SIGNAL };
 
@@ -146,10 +152,10 @@ static gboolean get_mouse_button(GdkEvent* event, int64_t* button) {
     case 3:
       *button = kFlutterPointerButtonMouseSecondary;
       return TRUE;
-    case 8:
+    case kMouseButtonBack:
       *button = kFlutterPointerButtonMouseBack;
       return TRUE;
-    case 9:
+    case kMouseButtonForward:
       *button = kFlutterPointerButtonMouseForward;
       return TRUE;
     default:
