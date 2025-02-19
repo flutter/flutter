ae7e03508b72f2b49349a45acc61771c202cf5cb
Jonas Uekoetter <jonas.uekotter@wolt.com>
2025-02-19 20:44:18 +0100

    make tests async

--- packages/flutter/test/services/flutter_version_test.dart
+++ packages/flutter/test/services/flutter_version_test.dart
@@ -6,30 +6,30 @@ import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  test('FlutterVersion.version contains the current version', () async {
+  test('FlutterVersion.version contains the current version', () {
     expect(FlutterVersion.version, const String.fromEnvironment('FLUTTER_VERSION'));
   });
 
-  test('FlutterVersion.channel contains the current channel', () async {
+  test('FlutterVersion.channel contains the current channel', () {
     expect(FlutterVersion.channel, const String.fromEnvironment('FLUTTER_CHANNEL'));
   });
 
-  test('FlutterVersion.gitUrl contains the current git URL', () async {
+  test('FlutterVersion.gitUrl contains the current git URL', () {
     expect(FlutterVersion.gitUrl, const String.fromEnvironment('FLUTTER_GIT_URL'));
   });
 
-  test('FlutterVersion.frameworkRevision contains the current framework revision', () async {
+  test('FlutterVersion.frameworkRevision contains the current framework revision', () {
     expect(
       FlutterVersion.frameworkRevision,
       const String.fromEnvironment('FLUTTER_FRAMEWORK_REVISION'),
     );
   });
 
-  test('FlutterVersion.engineRevision contains the current engine revision', () async {
+  test('FlutterVersion.engineRevision contains the current engine revision', () {
     expect(FlutterVersion.engineRevision, const String.fromEnvironment('FLUTTER_ENGINE_REVISION'));
   });
 
-  test('FlutterVersion.dartVersion contains the current Dart version', () async {
+  test('FlutterVersion.dartVersion contains the current Dart version', () {
     expect(FlutterVersion.dartVersion, const String.fromEnvironment('FLUTTER_DART_VERSION'));
   });
 }
