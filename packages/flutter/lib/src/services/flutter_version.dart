7c080153c71e4252fec143b96c1ca4054c6a44ef
Jonas Uekoetter <jonas.uekotter@wolt.com>
2025-02-19 20:44:00 +0100

    Use bool.hasEnvironment(...)

--- packages/flutter/lib/src/services/flutter_version.dart
+++ packages/flutter/lib/src/services/flutter_version.dart
@@ -18,31 +18,25 @@ abstract final class FlutterVersion {
 
   /// The Flutter version used to compile the app.
   static const String? version =
-      String.fromEnvironment('FLUTTER_VERSION') != ''
-          ? String.fromEnvironment('FLUTTER_VERSION')
-          : null;
+      bool.hasEnvironment('FLUTTER_VERSION') ? String.fromEnvironment('FLUTTER_VERSION') : null;
 
   /// The Flutter channel used to compile the app.
   static const String? channel =
-      String.fromEnvironment('FLUTTER_CHANNEL') != ''
-          ? String.fromEnvironment('FLUTTER_CHANNEL')
-          : null;
+      bool.hasEnvironment('FLUTTER_CHANNEL') ? String.fromEnvironment('FLUTTER_CHANNEL') : null;
 
   /// The link to the Git URL from which Flutter is obtained.
   static const String? gitUrl =
-      String.fromEnvironment('FLUTTER_GIT_URL') != ''
-          ? String.fromEnvironment('FLUTTER_GIT_URL')
-          : null;
+      bool.hasEnvironment('FLUTTER_GIT_URL') ? String.fromEnvironment('FLUTTER_GIT_URL') : null;
 
   /// The Flutter framework revision.
   static const String? frameworkRevision =
-      String.fromEnvironment('FLUTTER_FRAMEWORK_REVISION') != ''
+      bool.hasEnvironment('FLUTTER_FRAMEWORK_REVISION')
           ? String.fromEnvironment('FLUTTER_FRAMEWORK_REVISION')
           : null;
 
   /// The Flutter engine revision.
   static const String? engineRevision =
-      String.fromEnvironment('FLUTTER_ENGINE_REVISION') != ''
+      bool.hasEnvironment('FLUTTER_ENGINE_REVISION')
           ? String.fromEnvironment('FLUTTER_ENGINE_REVISION')
           : null;
 
@@ -50,7 +44,7 @@ abstract final class FlutterVersion {
   // is not included on web platforms.
   /// The Dart version used to compile the app.
   static const String? dartVersion =
-      String.fromEnvironment('FLUTTER_DART_VERSION') != ''
+      bool.hasEnvironment('FLUTTER_DART_VERSION')
           ? String.fromEnvironment('FLUTTER_DART_VERSION')
           : null;
 }
