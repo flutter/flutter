cbdb5ced0f56e99dfa3ac1dc6440e1e90e7d39d9
hangyu <jhy03261997@gmail.com>
2025-02-20 16:27:18 -0800

    fix tests

--- packages/flutter/lib/src/semantics/semantics.dart
+++ packages/flutter/lib/src/semantics/semantics.dart
@@ -2929,6 +2929,13 @@ class SemanticsNode with DiagnosticableTreeMixin {
   /// {@endtemplate}
   SemanticsRole get role => _role;
   SemanticsRole _role = _kEmptyConfig.role;
+  set role(SemanticsRole value) {
+    if (_role == value) {
+      return;
+    }
+    _role = value;
+    _markDirty();
+  }
 
   bool _canPerformAction(SemanticsAction action) => _actions.containsKey(action);
 
