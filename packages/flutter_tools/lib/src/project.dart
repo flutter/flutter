83fb67ff6d00d6722ff7072b9468936de602f9ac
Matan Lurey <matan@lurey.org>
2025-02-20 18:06:11 -0800

    Revert "Reverts "Avoid implicitly setting `determineDevDependencies: true` (it's not a safe operation) (#163711)" (#163762)"
    This reverts commit e6730613c97cc3628ecda39c20c60ecb5938366f.

--- packages/flutter_tools/lib/src/project.dart
+++ packages/flutter_tools/lib/src/project.dart
@@ -366,7 +366,12 @@ class FlutterProject {
     if (!directory.existsSync() || isPlugin) {
       return;
     }
-    await refreshPluginsList(this, iosPlatform: iosPlatform, macOSPlatform: macOSPlatform);
+    await refreshPluginsList(
+      this,
+      iosPlatform: iosPlatform,
+      macOSPlatform: macOSPlatform,
+      determineDevDependencies: releaseMode ?? false,
+    );
     if (androidPlatform) {
       await android.ensureReadyForPlatformSpecificTooling(deprecationBehavior: deprecationBehavior);
     }
