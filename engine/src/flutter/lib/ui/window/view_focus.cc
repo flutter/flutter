1dcc80c93f6ddff0875aa8cab110b0349b49e9af
Matej Knopp <matej.knopp@gmail.com>
2025-02-24 19:41:11 +0100

    remove include guards

--- engine/src/flutter/lib/ui/window/view_focus.cc
+++ engine/src/flutter/lib/ui/window/view_focus.cc
@@ -2,9 +2,4 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef FLUTTER_LIB_UI_WINDOW_VIEW_FOCUS_CC_
-#define FLUTTER_LIB_UI_WINDOW_VIEW_FOCUS_CC_
-
 namespace flutter {}
-
-#endif  // FLUTTER_LIB_UI_WINDOW_VIEW_FOCUS_CC_
