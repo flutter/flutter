5488ed29b175b297525fe71289b65c1c20fb87bd
Aaron Clarke <aaclarke@google.com>
2025-02-25 15:52:26 -0800

    fixed preprocessor guard

--- engine/src/flutter/impeller/compiler/shader_lib/impeller/conical_gradient_uniform_fill.glsl
+++ engine/src/flutter/impeller/compiler/shader_lib/impeller/conical_gradient_uniform_fill.glsl
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CONICAL_GRADIENT_GLSL_
-#define CONICAL_GRADIENT_GLSL_
+#ifndef CONICAL_GRADIENT_UNIFORM_FILL_GLSL_
+#define CONICAL_GRADIENT_UNIFORM_FILL_GLSL_
 
 precision highp float;
 
@@ -60,4 +60,4 @@ vec4 DoConicalGradientUniformFill(vec2 res) {
   return result_color;
 }
 
-#endif  // CONICAL_GRADIENT_GLSL_
+#endif  // CONICAL_GRADIENT_UNIFORM_FILL_GLSL_
