112eb2c7c82055cd1a777c8ba3cba79f9db28459
Gray Mackall <mackall@google.com>
2025-02-26 12:38:31 -0800

    re add an empty FlutterApplication to unbreak, and give deprecation notice

---
+++ engine/src/flutter/shell/platform/android/io/flutter/app/FlutterApplication.java
@@ -0,0 +1,12 @@
+package io.flutter.app;
+
+import android.app.Application;
+
+/**
+ * Empty implementation of the {@link Application} class, provided to avoid breaking older Flutter
+ * projects. Flutter projects which need to extend an Application should migrate to extending
+ * {@link android.app.Application} instead.
+ */
+@Deprecated
+public class FlutterApplication extends Application {
+}
