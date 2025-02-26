505793d469972479b1787d9700c1ecfcf6208b3d
Sarbagya Dhaubanjar <sarbagyastha@gmail.com>
2025-02-26 08:23:24 +0545

    Refactor date picker test to use a helper function for retrieving last day text

--- examples/api/test/material/date_picker/custom_calendar_date_picker.0_test.dart
+++ examples/api/test/material/date_picker/custom_calendar_date_picker.0_test.dart
@@ -8,37 +8,35 @@ import 'package:flutter_api_samples/material/date_picker/custom_calendar_date_pi
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
+  Text getLastDayText(WidgetTester tester) {
+    final Finder dayFinder = find.descendant(of: find.byType(Ink), matching: find.byType(Text));
+    return tester.widget(dayFinder.last);
+  }
+
   testWidgets('Days are based on the calendar delegate', (WidgetTester tester) async {
     await tester.pumpWidget(const example.CalendarDatePickerApp());
 
     final Finder nextMonthButton = find.byIcon(Icons.chevron_right);
 
-    Text lastDayText = tester.getLastDayText();
+    Text lastDayText = getLastDayText(tester);
     expect(lastDayText.data, equals('21'));
 
     await tester.tap(nextMonthButton);
     await tester.pumpAndSettle();
 
-    lastDayText = tester.getLastDayText();
+    lastDayText = getLastDayText(tester);
     expect(lastDayText.data, equals('28'));
 
     await tester.tap(nextMonthButton);
     await tester.pumpAndSettle();
 
-    lastDayText = tester.getLastDayText();
+    lastDayText = getLastDayText(tester);
     expect(lastDayText.data, equals('21'));
 
     await tester.tap(nextMonthButton);
     await tester.pumpAndSettle();
 
-    lastDayText = tester.getLastDayText();
+    lastDayText = getLastDayText(tester);
     expect(lastDayText.data, equals('28'));
   });
-}
-
-extension on WidgetTester {
-  Text getLastDayText() {
-    final Finder dayFinder = find.descendant(of: find.byType(Ink), matching: find.byType(Text));
-    return widget(dayFinder.last);
-  }
 }
