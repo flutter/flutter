eb3b580e81f759a183b306164224bf60105ef669
Gray Mackall <mackall@google.com>
2025-02-26 15:13:08 -0800

    doc explaining

--- dev/integration_tests/spell_check/android/app/src/main/kotlin/com/example/sc_int_test/MainApplication.kt
+++ dev/integration_tests/spell_check/android/app/src/main/kotlin/com/example/sc_int_test/MainApplication.kt
@@ -2,4 +2,7 @@ package com.example.spell_check
 
 import io.flutter.app.FlutterApplication
 
+// The spell check application was arbitrarily chosen to also test the ability
+// to extend `io.flutter.app.FlutterApplication`. This should be removed when the
+// class itself is removed.
 class MainApplication : FlutterApplication()
