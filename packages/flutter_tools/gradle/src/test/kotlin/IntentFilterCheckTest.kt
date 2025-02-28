480d64bef2d8ba3b61c1e8c8b0576574607c05b9
Reid Baker <reidbaker@google.com>
2025-02-28 16:20:05 -0500

    formatting

--- packages/flutter_tools/gradle/src/test/kotlin/IntentFilterCheckTest.kt
+++ packages/flutter_tools/gradle/src/test/kotlin/IntentFilterCheckTest.kt
@@ -1,29 +1,26 @@
 package com.flutter.gradle
 
 import kotlin.test.Test
-import kotlin.test.assertContains
 import kotlin.test.assertEquals
-import kotlin.test.assertFalse
 import kotlin.test.assertTrue
 
 class IntentFilterCheckTest {
+    @Test
+    fun canCreateIntentFilterJson() {
+        val intentFilterCheck = IntentFilterCheck()
+        intentFilterCheck.hasActionView = true
+        intentFilterCheck.hasDefaultCategory = true
 
-  @Test
-  fun canCreateIntentFilterJson() {
-    val intentFilterCheck = IntentFilterCheck()
-    intentFilterCheck.hasActionView = true
-    intentFilterCheck.hasDefaultCategory = true
+        val intentJson = intentFilterCheck.toJson()
 
-    val intentJson = intentFilterCheck.toJson()
+        assertTrue(intentJson.containsKey("hasAutoVerify"))
+        assertTrue(intentJson.containsKey("hasActionView"))
+        assertTrue(intentJson.containsKey("hasDefaultCategory"))
+        assertTrue(intentJson.containsKey("hasBrowsableCategory"))
 
-    assertTrue(intentJson.containsKey("hasAutoVerify"))
-    assertTrue(intentJson.containsKey("hasActionView"))
-    assertTrue(intentJson.containsKey("hasDefaultCategory"))
-    assertTrue(intentJson.containsKey("hasBrowsableCategory"))
-
-    assertEquals("false", intentJson.get(key = "hasAutoVerify").toString())
-    assertEquals("true", intentJson.get(key = "hasActionView").toString())
-    assertEquals("true", intentJson.get(key = "hasDefaultCategory").toString())
-    assertEquals("false", intentJson.get(key = "hasBrowsableCategory").toString())
-  }
+        assertEquals("false", intentJson.get(key = "hasAutoVerify").toString())
+        assertEquals("true", intentJson.get(key = "hasActionView").toString())
+        assertEquals("true", intentJson.get(key = "hasDefaultCategory").toString())
+        assertEquals("false", intentJson.get(key = "hasBrowsableCategory").toString())
+    }
 }
