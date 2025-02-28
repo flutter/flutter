d7e56366701cdd9432658c20de258f78c39bde76
Hannes Hultergård <47989573+Hannnes1@users.noreply.github.com>
2025-02-28 20:01:33 +0100

    Add action for configuring default action of EditableText.onTapUpOutside (#162575)
    This PR adds an `Action` for configuring a default action of
    `EditableText.onTapUpOutside`. This is the equivalent to what
    https://github.com/flutter/flutter/pull/150125 did for
    `EditableText.onTapOutside`.
    
    Fixes https://github.com/flutter/flutter/issues/162212
    
    ## Pre-launch Checklist
    
    - [x] I read the [Contributor Guide] and followed the process outlined
    there for submitting PRs.
    - [x] I read the [Tree Hygiene] wiki page, which explains my
    responsibilities.
    - [x] I read and followed the [Flutter Style Guide], including [Features
    we expect every widget to implement].
    - [x] I signed the [CLA].
    - [x] I listed at least one issue that this PR fixes in the description
    above.
    - [x] I updated/added relevant documentation (doc comments with `///`).
    - [x] I added new tests to check the change I am making, or this PR is
    [test-exempt].
    - [x] I followed the [breaking change policy] and added [Data Driven
    Fixes] where supported.
    - [x] All existing and new tests are passing.
    
    <!-- Links -->
    [Contributor Guide]:
    https://github.com/flutter/flutter/blob/main/docs/contributing/Tree-hygiene.md#overview
    [Tree Hygiene]:
    https://github.com/flutter/flutter/blob/main/docs/contributing/Tree-hygiene.md
    [test-exempt]:
    https://github.com/flutter/flutter/blob/main/docs/contributing/Tree-hygiene.md#tests
    [Flutter Style Guide]:
    https://github.com/flutter/flutter/blob/main/docs/contributing/Style-guide-for-Flutter-repo.md
    [Features we expect every widget to implement]:
    https://github.com/flutter/flutter/blob/main/docs/contributing/Style-guide-for-Flutter-repo.md#features-we-expect-every-widget-to-implement
    [CLA]: https://cla.developers.google.com/
    [flutter/tests]: https://github.com/flutter/tests
    [breaking change policy]:
    https://github.com/flutter/flutter/blob/main/docs/contributing/Tree-hygiene.md#handling-breaking-changes
    [Discord]:
    https://github.com/flutter/flutter/blob/main/docs/contributing/Chat.md
    [Data Driven Fixes]:
    https://github.com/flutter/flutter/blob/main/docs/contributing/Data-driven-Fixes.md
    
    ---------
    
    Co-authored-by: Victor Sanni <victorsanniay@gmail.com>

---
+++ examples/api/test/widgets/text_editing_intents/editable_text_tap_up_outside_intent.0_test.dart
@@ -0,0 +1,50 @@
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+import 'package:flutter_api_samples/widgets/text_editing_intents/editable_text_tap_up_outside_intent.0.dart'
+    as example;
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('Unfocuses TextField on tap', (WidgetTester tester) async {
+    await tester.pumpWidget(const example.SampleApp());
+
+    final Finder finder = find.byType(TextField);
+    final TextField textField = tester.firstWidget(finder);
+
+    await tester.tap(finder);
+
+    await tester.pump();
+
+    expect(textField.focusNode!.hasFocus, true);
+
+    // Tap the center of the Scaffold, outside the TextField.
+    await tester.tap(find.byType(Scaffold));
+
+    await tester.pump();
+
+    expect(textField.focusNode!.hasFocus, false);
+  });
+
+  testWidgets('Does not unfocus TextField on scroll', (WidgetTester tester) async {
+    await tester.pumpWidget(const example.SampleApp());
+
+    final Finder finder = find.byType(TextField);
+    final TextField textField = tester.firstWidget(finder);
+
+    await tester.tap(finder);
+
+    await tester.pump();
+
+    expect(textField.focusNode!.hasFocus, true);
+
+    // Tap the center of the Scaffold, outside the TextField.
+    await tester.drag(find.byType(Scaffold), const Offset(0.0, -100.0));
+
+    await tester.pump();
+
+    expect(textField.focusNode!.hasFocus, true);
+  });
+}
