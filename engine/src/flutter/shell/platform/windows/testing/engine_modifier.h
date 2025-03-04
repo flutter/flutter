774605171825a4af2b3a1db4034dbd3705a8b9f6
Matthew Kosarek <matt.kosarek@canonical.com>
2025-03-04 10:22:20 -0500

    feature: make the text input plugin use the correct view on the Windows platform (#163847)
    ## What's new?
    - Updates the `TextInput.setClient` method to expect a view ID, which is
    already being sent up by clients. This makes it so that the IME info
    shows on the text input correctly across views :tada:. Also - text input
    on windows works properly across views (although it was working before
    too)
    - Using the view ID in `TextInputPlugin::HandleMethodCall` to resolve
    the view
    - Update tests to no longer assume that we are using the implicit view
    id
    - Add two tests to ensure that the view id is set
    
    ## What's fixed?
    - Partially fixes: https://github.com/flutter/flutter/issues/142845
    
    ## Pre-launch Checklist
    
    - [x] I read the [Contributor Guide] and followed the process outlined
    there for submitting PRs.
    - [x] I read the [Tree Hygiene] wiki page, which explains my
    responsibilities.
    - [x] I read and followed the [Flutter Style Guide], including [Features
    we expect every widget to implement].
    - [x] I signed the [CLA].
    - [x] I listed at least one issue that this PR fixes in the description
    above.
    - [x] I updated/added relevant documentation (doc comments with `///`).
    - [x] I added new tests to check the change I am making, or this PR is
    [test-exempt].
    - [x] I followed the [breaking change policy] and added [Data Driven
    Fixes] where supported.
    - [x] All existing and new tests are passing.
    
    If you need help, consider asking for advice on the #hackers-new channel
    on [Discord].
    
    <!-- Links -->
    [Contributor Guide]:
    https://github.com/flutter/flutter/blob/main/docs/contributing/Tree-hygiene.md#overview
    [Tree Hygiene]:
    https://github.com/flutter/flutter/blob/main/docs/contributing/Tree-hygiene.md
    [test-exempt]:
    https://github.com/flutter/flutter/blob/main/docs/contributing/Tree-hygiene.md#tests
    [Flutter Style Guide]:
    https://github.com/flutter/flutter/blob/main/docs/contributing/Style-guide-for-Flutter-repo.md
    [Features we expect every widget to implement]:
    https://github.com/flutter/flutter/blob/main/docs/contributing/Style-guide-for-Flutter-repo.md#features-we-expect-every-widget-to-implement
    [CLA]: https://cla.developers.google.com/
    [flutter/tests]: https://github.com/flutter/tests
    [breaking change policy]:
    https://github.com/flutter/flutter/blob/main/docs/contributing/Tree-hygiene.md#handling-breaking-changes
    [Discord]:
    https://github.com/flutter/flutter/blob/main/docs/contributing/Chat.md
    [Data Driven Fixes]:
    https://github.com/flutter/flutter/blob/main/docs/contributing/Data-driven-Fixes.md

--- engine/src/flutter/shell/platform/windows/testing/engine_modifier.h
+++ engine/src/flutter/shell/platform/windows/testing/engine_modifier.h
@@ -43,6 +43,11 @@ class EngineModifier {
     engine_->views_[kImplicitViewId] = view;
   }
 
+  /// Associate a view with a view id.
+  void SetViewById(FlutterWindowsView* view, FlutterViewId viewId) {
+    engine_->views_[viewId] = view;
+  }
+
   /// Reset the start_time field that is used to align vsync events.
   void SetStartTime(uint64_t start_time_nanos) {
     engine_->start_time_ = std::chrono::nanoseconds(start_time_nanos);
