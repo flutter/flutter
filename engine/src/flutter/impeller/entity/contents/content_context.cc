24a44bc69daf4b157fbb36212a98bd03d249007b
Aaron Clarke <aaclarke@google.com>
2025-03-05 11:14:24 -0800

    added shaders

--- engine/src/flutter/impeller/entity/contents/content_context.cc
+++ engine/src/flutter/impeller/entity/contents/content_context.cc
@@ -329,6 +329,7 @@ ContentContext::ContentContext(
     solid_fill_pipelines_.CreateDefault(*context_, options);
     texture_pipelines_.CreateDefault(*context_, options);
     fast_gradient_pipelines_.CreateDefault(*context_, options);
+    line_pipelines_.CreateDefault(*context_, options);
 
     if (context_->GetCapabilities()->SupportsSSBO()) {
       linear_gradient_ssbo_fill_pipelines_.CreateDefault(*context_, options);
@@ -1157,6 +1158,10 @@ PipelineRef ContentContext::GetFramebufferBlendSoftLightPipeline(
 PipelineRef ContentContext::GetDrawVerticesUberShader(
     ContentContextOptions opts) const {
   return GetPipeline(vertices_uber_shader_, opts);
+}
+
+PipelineRef ContentContext::GetLinePipeline(ContentContextOptions opts) const {
+  return GetPipeline(line_pipelines_, opts);
 }
 
 #ifdef IMPELLER_ENABLE_OPENGLES
