6ea03fac59e5671bacf0293ae3c226cc71513301
Matthew Kosarek <matt.kosarek@canonical.com>
2025-03-11 10:24:26 -0400

    bugfix: get rid of lifecycle state

--- engine/src/flutter/shell/platform/windows/flutter_windows_engine.cc
+++ engine/src/flutter/shell/platform/windows/flutter_windows_engine.cc
@@ -501,7 +501,6 @@ bool FlutterWindowsEngine::Run(std::string_view entrypoint) {
                                     displays.data(), displays.size());
 
   SendSystemLocales();
-  SetLifecycleState(flutter::AppLifecycleState::kResumed);
 
   settings_plugin_->StartWatching();
   settings_plugin_->SendSettings();
@@ -812,12 +811,6 @@ void FlutterWindowsEngine::SetNextFrameCallback(fml::closure callback) {
         self->task_runner_->PostTask(std::move(self->next_frame_callback_));
       },
       this);
-}
-
-void FlutterWindowsEngine::SetLifecycleState(flutter::AppLifecycleState state) {
-  if (lifecycle_manager_) {
-    lifecycle_manager_->SetLifecycleState(state);
-  }
 }
 
 void FlutterWindowsEngine::ForwardToHostWindowController(HWND hwnd,
