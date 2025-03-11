c64bf3e756b4f7a8a82a5ebb118a46a5fe7e5598
Matej Knopp <matej.knopp@gmail.com>
2025-03-11 13:31:34 +0100

    Add multiwindow example app (windows)

---
+++ examples/multi_window_ref_app/lib/app/window_controller_render.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'regular_window_content.dart';
+import 'window_manager_model.dart';
+import 'window_settings.dart';
+
+class WindowControllerRender extends StatelessWidget {
+  const WindowControllerRender(
+      {required this.controller,
+      required this.onDestroyed,
+      required this.onError,
+      required this.windowSettings,
+      required this.windowManagerModel,
+      required super.key});
+
+  final WindowController controller;
+  final VoidCallback onDestroyed;
+  final VoidCallback onError;
+  final WindowSettings windowSettings;
+  final WindowManagerModel windowManagerModel;
+
+  @override
+  Widget build(BuildContext context) {
+    switch (controller.type) {
+      case WindowArchetype.regular:
+        return RegularWindow(
+            key: key,
+            controller: controller as RegularWindowController,
+            child: RegularWindowContent(
+                window: controller as RegularWindowController,
+                windowSettings: windowSettings,
+                windowManagerModel: windowManagerModel));
+      default:
+        throw UnimplementedError(
+            "The provided window type does not have an implementation");
+    }
+  }
+}
