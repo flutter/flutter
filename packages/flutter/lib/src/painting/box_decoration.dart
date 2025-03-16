4f863115eeabc5b6491548f912dc68a614fc6526
Dibash Poudel <dbspoudel@gmail.com>
2025-03-16 12:36:25 +1100

    improve assertion messages for circle shape in BoxBorder and BoxDecoration

--- packages/flutter/lib/src/painting/box_decoration.dart
+++ packages/flutter/lib/src/painting/box_decoration.dart
@@ -132,7 +132,7 @@ class BoxDecoration extends Decoration {
   bool debugAssertIsValid() {
     assert(
       shape != BoxShape.circle || borderRadius == null,
-      'borderRadius is not allowed when shape is BoxShape.circle. Remove either the shape or borderRadius argument.',
+      'A circle cannot have a border radius. Remove either the shape or the borderRadius argument.',
     ); // Can't have a border radius if you're a circle.
     return super.debugAssertIsValid();
   }
@@ -430,7 +430,7 @@ class _BoxDecorationPainter extends BoxPainter {
       case BoxShape.circle:
         assert(
           _decoration.borderRadius == null,
-          'borderRadius is not allowed when shape is BoxShape.circle. Remove either the shape or borderRadius argument.',
+          'A circle cannot have a border radius. Remove either the shape or the borderRadius argument.',
         );
         final Offset center = rect.center;
         final double radius = rect.shortestSide / 2.0;
@@ -542,7 +542,7 @@ class _BoxDecorationPainter extends BoxPainter {
       case BoxShape.circle:
         assert(
           _decoration.borderRadius == null,
-          'borderRadius is not allowed when shape is BoxShape.circle. Remove either the shape or borderRadius argument.',
+          'A circle cannot have a border radius. Remove either the shape or the borderRadius argument.',
         );
         final Offset center = rect.center;
         final double radius = rect.shortestSide / 2.0;
