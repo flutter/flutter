38745fb385f83a544ff8ee2fe4c329bf59cae235
LongCat is Looong <31859944+LongCatIsLooong@users.noreply.github.com>
2025-03-17 10:33:02 -0700

    `OverlayPortal.childLayoutBuilder` should rebuild when OverlayPortal rebuilds.
    The builder callback can capture free variables. So the it must be
    re-evaluated every time `OverlayPortal` rebuilds.

--- packages/flutter/lib/src/widgets/overlay.dart
+++ packages/flutter/lib/src/widgets/overlay.dart
@@ -2615,7 +2615,7 @@ class _OverlayChildLayoutBuilder extends AbstractLayoutBuilder<OverlayChildLayou
   ) => _RenderLayoutBuilder();
 
   @override
-  bool updateShouldRebuild(_OverlayChildLayoutBuilder oldWidget) => oldWidget.builder != builder;
+  bool updateShouldRebuild(_OverlayChildLayoutBuilder oldWidget) => true;
 }
 
 // A RenderBox that:
@@ -2709,14 +2709,10 @@ class _RenderLayoutBuilder extends RenderProxyBox
   int? _callbackId;
   @override
   void performLayout() {
-    late OverlayChildLayoutInfo newLayoutInfo;
     // The invokeLayoutCallback allows arbitrary access to the sizes of
     // RenderBoxes the we know that have finished doing layout.
-    invokeLayoutCallback((_) => newLayoutInfo = _computeNewLayoutInfo());
-    if (newLayoutInfo != _layoutInfo) {
-      _layoutInfo = newLayoutInfo;
-      rebuildIfNecessary();
-    }
+    invokeLayoutCallback((_) => _layoutInfo = _computeNewLayoutInfo());
+    rebuildIfNecessary();
     assert(_callbackId == null);
     _callbackId ??= SchedulerBinding.instance.scheduleFrameCallback(
       _frameCallback,
