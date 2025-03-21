ece2c3e29115dcb31fefc0a7ed5706c64f3a46de
Jonah Williams <jonahwilliams@google.com>
2025-03-20 18:36:47 -0700

    disable linux lsan. (#165620)
    With the ubuntu upgrade, everything is leaking now.

--- engine/src/flutter/shell/common/shell_unittests.cc
+++ engine/src/flutter/shell/common/shell_unittests.cc
@@ -4385,7 +4385,7 @@ TEST_F(ShellTest, PointerPacketFlushMessageLoop) {
 }
 
 // Verifies a pointer event will flush the dart event loop.
-TEST_F(ShellTest, PointerPacketsAreDispatchedWithTask) {
+TEST_F(ShellTest, DISABLED_PointerPacketsAreDispatchedWithTask) {
   Settings settings = CreateSettingsForFixture();
   ThreadHost thread_host("io.flutter.test." + GetCurrentTestName() + ".",
                          ThreadHost::Type::kPlatform);
