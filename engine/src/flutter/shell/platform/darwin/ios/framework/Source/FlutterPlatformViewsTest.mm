c70794aae5faa128b41b1c06d88b5b76a54fd393
Huan Lin <hellohuanlin@gmail.com>
2025-03-20 20:52:05 -0700

    nit

--- engine/src/flutter/shell/platform/darwin/ios/framework/Source/FlutterPlatformViewsTest.mm
+++ engine/src/flutter/shell/platform/darwin/ios/framework/Source/FlutterPlatformViewsTest.mm
@@ -4624,9 +4624,9 @@ fml::RefPtr<fml::TaskRunner> GetDefaultTaskRunner() {
   FlutterEngine* engine = [[FlutterEngine alloc] initWithName:@"foobar"];
   [engine run];
   XCTAssertTrue(engine.platformView != nullptr);
-  auto ios_context = engine.platformView->GetIosContext();
+  std::shared_ptr<flutter::IOSContext> ios_context = engine.platformView->GetIosContext();
 
-  auto pool = flutter::OverlayLayerPool{};
+  flutter::OverlayLayerPool pool;
 
   // Add layers to the pool.
   pool.CreateLayer(ios_context, MTLPixelFormatBGRA8Unorm, 1);
@@ -4649,15 +4649,15 @@ fml::RefPtr<fml::TaskRunner> GetDefaultTaskRunner() {
   FlutterEngine* engine = [[FlutterEngine alloc] initWithName:@"foobar"];
   [engine run];
   XCTAssertTrue(engine.platformView != nullptr);
-  auto ios_context = engine.platformView->GetIosContext();
+  std::shared_ptr<flutter::IOSContext> ios_context = engine.platformView->GetIosContext();
 
-  auto pool = flutter::OverlayLayerPool{};
+  flutter::OverlayLayerPool pool;
 
   // Add layers to the pool.
   pool.CreateLayer(ios_context, MTLPixelFormatBGRA8Unorm, 1);
   XCTAssertEqual(pool.size(), 1u);
 
-  auto layer = pool.GetNextLayer();
+  std::shared_ptr<flutter::OverlayLayer> layer = pool.GetNextLayer();
 
   layer->UpdateViewState(nil, SkRect::MakeXYWH(1, 2, 3, 4), 0, 0);
   // Should not update the view state (e.g. overlay_view_wrapper's frame) when FlutterView is nil.
