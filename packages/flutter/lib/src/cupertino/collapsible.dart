9731fb27e9a8b6349642a387e6393ad5b0210245
Victor Sanni <victorsanniay@gmail.com>
2025-03-21 16:05:28 -0700

    Further tweaks

--- packages/flutter/lib/src/cupertino/collapsible.dart
+++ packages/flutter/lib/src/cupertino/collapsible.dart
@@ -8,6 +8,7 @@ library;
 import 'package:flutter/widgets.dart';
 
 import 'colors.dart';
+import 'icons.dart';
 import 'list_tile.dart';
 
 /// The curve of the animation used to expand or collapse the [CupertinoCollapsible].
@@ -24,47 +25,11 @@ class CupertinoCollapsible extends StatefulWidget {
     super.key,
     required this.title,
     required this.child,
-    this.subtitle,
-    this.additionalInfo,
-    this.leading,
-    this.trailing,
     this.controller,
-    this.backgroundColor,
-    this.backgroundColorActivated,
-    this.padding,
   });
 
   /// A [title] is used to convey the central information. Usually a [Text].
   final Widget title;
-
-  /// A [subtitle] is used to display additional information. It is located
-  /// below [title]. Usually a [Text] widget.
-  final Widget? subtitle;
-
-  /// Similar to [subtitle], an [additionalInfo] is used to display additional
-  /// information. However, instead of being displayed below [title], it is
-  /// displayed on the right, before [trailing]. Usually a [Text] widget.
-  final Widget? additionalInfo;
-
-  /// A widget displayed at the start of the [CupertinoListTile]. This is
-  /// typically an `Icon` or an `Image`.
-  final Widget? leading;
-
-  /// A widget displayed at the end of the [CupertinoListTile]. This is usually
-  /// a right chevron icon (e.g. `CupertinoListTileChevron`), or an `Icon`.
-  final Widget? trailing;
-
-  /// The [backgroundColor] of the tile in normal state. Once the tile is
-  /// tapped, the background color switches to [backgroundColorActivated]. It is
-  /// set to match the iOS look by default.
-  final Color? backgroundColor;
-
-  /// The [backgroundColorActivated] is the background color of the tile after
-  /// the tile was tapped. It is set to match the iOS look by default.
-  final Color? backgroundColorActivated;
-
-  /// Padding of the content inside [CupertinoListTile].
-  final EdgeInsetsGeometry? padding;
 
   /// If provided, the controller can be used to expand and collapse tiles.
   ///
@@ -85,7 +50,6 @@ class _CupertinoCollapsibleState extends State<CupertinoCollapsible> {
   static final Animatable<double> _quarterTween = Tween<double>(begin: 0.0, end: 0.25);
 
   late Animation<double> _iconTurns;
-
   late ExpansibleController _tileController;
 
   @override
@@ -96,23 +60,32 @@ class _CupertinoCollapsibleState extends State<CupertinoCollapsible> {
 
   Widget? _buildIcon(BuildContext context, Animation<double> animation) {
     _iconTurns = animation.drive(_quarterTween.chain(_easeInTween));
+    // Replicate the Icon logic here to get a slightly bolder icon.
     return RotationTransition(
       turns: _iconTurns,
-      child: const CupertinoListTileChevron(color: CupertinoColors.activeBlue),
+      child: Text.rich(
+        TextSpan(
+          text: String.fromCharCode(CupertinoIcons.right_chevron.codePoint),
+          style: TextStyle(
+            inherit: false,
+            color: CupertinoColors.activeBlue,
+            fontSize: 15.0,
+            fontWeight: FontWeight.w900,
+            fontFamily: CupertinoIcons.right_chevron.fontFamily,
+            package: CupertinoIcons.right_chevron.fontPackage,
+          ),
+        ),
+      ),
     );
   }
 
   Widget _buildHeader(BuildContext context, Animation<double> animation) {
     return CupertinoListTile(
       onTap: _tileController.isExpanded ? _tileController.collapse : _tileController.expand,
-      leading: widget.leading,
       title: widget.title,
-      subtitle: widget.subtitle,
-      additionalInfo: widget.additionalInfo,
-      trailing: widget.trailing ?? _buildIcon(context, animation),
-      padding: widget.padding,
-      backgroundColor: widget.backgroundColor,
-      backgroundColorActivated: widget.backgroundColorActivated,
+      trailing: _buildIcon(context, animation),
+      padding: const EdgeInsets.only(right: 20.0),
+      backgroundColorActivated: CupertinoColors.transparent,
     );
   }
 
