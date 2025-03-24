55833bf873e1111fa74a9ccdc53884e00657927c
Matej Knopp <matej.knopp@gmail.com>
2025-03-24 20:56:03 +0100

    Licenses

--- dev/integration_tests/macos_integration_test/macos/RunnerUITests/RunnerUITests.m
+++ dev/integration_tests/macos_integration_test/macos/RunnerUITests/RunnerUITests.m
@@ -1,9 +1,6 @@
-//
-//  integration1UITests.m
-//  integration1UITests
-//
-//  Created by Matej Knopp on 3/21/25.
-//
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
 #import <XCTest/XCTest.h>
 
