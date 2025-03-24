flutter/flutter

commit 158e55eca4984b48b1cc3340a26361ab28104847

replace deprecated [UIScreen mainScreen] in iOS (#162785)
<!--
Thanks for filing a pull request!
Reviewers are typically assigned within a week of filing a request.
To learn more about code review, see our documentation on Tree Hygiene:
https://github.com/flutter/flutter/blob/main/docs/contributing/Tree-hygiene.md
-->

1. Replaced `mainScreen` in `FlutterPlatformViewsController` with the
`screeScale` of the `screen` that the `flutterViewController` is
attached to. Updated API usage of `FlutterClippingMaskViewPool` and
`OverlayLayerPool` by providing the correct `screenScale` from the
screen that the `flutterViewController` is attached to.
2. Replaced `mainScreen` in `OverlayLayerPool` by adding an API for the
`screenScale` to be provided by the class user.
3. Replaced `mainScreen` in `FlutterClippingMaskViewPool` by adding an
API for the `screenScale` to be provided by the class user.
4. Replaced `mainScreen` in `FlutterPlatformViewsTest` by adding a
mocked `FlutterViewController` which has a mocked `screen` with a mocked
`screenScale`. The `flutterViewController` is then provided the to the
`flutterPlatformViewsController`.

All of the above changes allow platform views and their overlays
displayed on an external screen to be properly scaled when rendered.

Fixes #130832. I believe this fixed #130825 since
FlutterPlatformViews_Internal.mm does not exist but the definition of
the interfaces declared in FlutterPlatformViews_Internal.h are part of
the files modified by this pr. This pr also replaces `mainScreen` in
`overlay_layer_pool.mm`.

## Pre-launch Checklist

- [x] I read the [Contributor Guide] and followed the process outlined
there for submitting PRs.
- [x] I read the [Tree Hygiene] wiki page, which explains my
responsibilities.
- [x] I read and followed the [Flutter Style Guide], including [Features
we expect every widget to implement].
- [x] I signed the [CLA].
- [x] I listed at least one issue that this PR fixes in the description
above.

engine/src/flutter/shell/platform/darwin/ios/framework/Source/overlay_layer_pool.mm
@@ -5,6 +5,7 @@
 #include "flutter/shell/platform/darwin/ios/framework/Source/overlay_layer_pool.h"
 
 #import "flutter/shell/platform/darwin/ios/framework/Source/FlutterOverlayView.h"
+#import "flutter/shell/platform/darwin/ios/framework/Source/FlutterView.h"
 #import "flutter/shell/platform/darwin/ios/ios_surface.h"
 
 namespace flutter {
@@ -22,7 +23,7 @@ void OverlayLayer::UpdateViewState(UIView* flutter_view,
                                    SkRect rect,
                                    int64_t view_id,
                                    int64_t overlay_id) {
-  auto screenScale = [UIScreen mainScreen].scale;
+  auto screenScale = ((FlutterView*)flutter_view).screen.scale;
   // Set the size of the overlay view wrapper.
   // This wrapper view masks the overlay view.
   overlay_view_wrapper.frame = CGRectMake(rect.x() / screenScale, rect.y() / screenScale,
@@ -54,7 +55,8 @@ std::shared_ptr<OverlayLayer> OverlayLayerPool::GetNextLayer() {
 
 void OverlayLayerPool::CreateLayer(GrDirectContext* gr_context,
                                    const std::shared_ptr<IOSContext>& ios_context,
-                                   MTLPixelFormat pixel_format) {
+                                   MTLPixelFormat pixel_format,
+                                   CGFloat screenScale) {
   FML_DCHECK([[NSThread currentThread] isMainThread]);
   std::shared_ptr<OverlayLayer> layer;
   UIView* overlay_view;
@@ -72,7 +74,6 @@ void OverlayLayerPool::CreateLayer(GrDirectContext* gr_context,
     layer = std::make_shared<OverlayLayer>(overlay_view, overlay_view_wrapper,
                                            std::move(ios_surface), std::move(surface));
   } else {
-    CGFloat screenScale = [UIScreen mainScreen].scale;
     overlay_view = [[FlutterOverlayView alloc] initWithContentsScale:screenScale
                                                          pixelFormat:pixel_format];
     overlay_view_wrapper = [[FlutterOverlayView alloc] initWithContentsScale:screenScale

engine/src/flutter/shell/platform/darwin/ios/framework/Source/FlutterPlatformViews.mm
@@ -209,10 +209,6 @@ static BOOL _preparedOnce = NO;
   CGRect rectSoFar_;
 }
 
-- (instancetype)initWithFrame:(CGRect)frame {
-  return [self initWithFrame:frame screenScale:[UIScreen mainScreen].scale];
-}
-
 - (instancetype)initWithFrame:(CGRect)frame screenScale:(CGFloat)screenScale {
   if (self = [super initWithFrame:frame]) {
     self.backgroundColor = UIColor.clearColor;
@@ -473,12 +469,11 @@ static BOOL _preparedOnce = NO;
   return self;
 }
 
-- (FlutterClippingMaskView*)getMaskViewWithFrame:(CGRect)frame {
+- (FlutterClippingMaskView*)getMaskViewWithFrame:(CGRect)frame screenScale:(CGFloat)screenScale {
   FML_DCHECK(self.pool.count <= self.capacity);
   if (self.pool.count == 0) {
     // The pool is empty, alloc a new one.
-    return [[FlutterClippingMaskView alloc] initWithFrame:frame
-                                              screenScale:UIScreen.mainScreen.scale];
+    return [[FlutterClippingMaskView alloc] initWithFrame:frame screenScale:screenScale];
   }
   FlutterClippingMaskView* maskView = [self.pool anyObject];
   maskView.frame = frame;

engine/src/flutter/shell/platform/darwin/ios/framework/Source/FlutterPlatformViewsController.mm
@@ -11,6 +11,7 @@
 #include "flutter/fml/synchronization/count_down_latch.h"
 #import "flutter/shell/platform/darwin/ios/framework/Source/FlutterOverlayView.h"
 #import "flutter/shell/platform/darwin/ios/framework/Source/FlutterView.h"
+#import "flutter/shell/platform/darwin/ios/framework/Source/UIViewController+FlutterScreenAndSceneIfLoaded.h"
 #include "flutter/shell/platform/darwin/ios/framework/Source/overlay_layer_pool.h"
 #import "flutter/shell/platform/darwin/ios/ios_surface.h"
 
@@ -248,7 +249,8 @@ static bool ClipRRectContainsPlatformViewBoundingRect(const SkRRect& clip_rrect,
 /// Runs on the platform thread.
 - (void)createLayerWithIosContext:(const std::shared_ptr<flutter::IOSContext>&)iosContext
                         grContext:(GrDirectContext*)grContext
-                      pixelFormat:(MTLPixelFormat)pixelFormat;
+                      pixelFormat:(MTLPixelFormat)pixelFormat
+                      screenScale:(CGFloat)screenScale;
 
 /// Removes overlay views and platform views that aren't needed in the current frame.
 /// Must run on the platform thread.
@@ -561,7 +563,9 @@ static bool ClipRRectContainsPlatformViewBoundingRect(const SkRRect& clip_rrect,
   CGRect frame =
       CGRectMake(-clipView.frame.origin.x, -clipView.frame.origin.y,
                  CGRectGetWidth(self.flutterView.bounds), CGRectGetHeight(self.flutterView.bounds));
-  clipView.maskView = [self.maskViewPool getMaskViewWithFrame:frame];
+  clipView.maskView = [self.maskViewPool
+      getMaskViewWithFrame:frame
+               screenScale:[self.flutterViewController flutterScreenIfViewLoaded].scale];
 }
 
 - (void)applyMutators:(const flutter::MutatorsStack&)mutatorsStack
@@ -582,7 +586,7 @@ static bool ClipRRectContainsPlatformViewBoundingRect(const SkRRect& clip_rrect,
     [self.maskViewPool insertViewToPoolIfNeeded:(FlutterClippingMaskView*)(clipView.maskView)];
     clipView.maskView = nil;
   }
-  CGFloat screenScale = [UIScreen mainScreen].scale;
+  CGFloat screenScale = [self.flutterViewController flutterScreenIfViewLoaded].scale;
   auto iter = mutatorsStack.Begin();
   while (iter != mutatorsStack.End()) {
     switch ((*iter)->GetType()) {
@@ -698,7 +702,7 @@ static bool ClipRRectContainsPlatformViewBoundingRect(const SkRRect& clip_rrect,
   // when we apply the transforms matrix in |applyMutators:embeddedView:boundingRect|, we need
   // to remember to do a reverse translate.
   const SkRect& rect = params.finalBoundingRect();
-  CGFloat screenScale = [UIScreen mainScreen].scale;
+  CGFloat screenScale = [self.flutterViewController flutterScreenIfViewLoaded].scale;
   clippingView.frame = CGRectMake(rect.x() / screenScale, rect.y() / screenScale,
                                   rect.width() / screenScale, rect.height() / screenScale);
   [self applyMutators:mutatorStack embeddedView:touchInterceptor boundingRect:rect];
@@ -860,7 +864,8 @@ static bool ClipRRectContainsPlatformViewBoundingRect(const SkRRect& clip_rrect,
         for (auto i = 0u; i < missingLayerCount; i++) {
           [self createLayerWithIosContext:iosContext
                                 grContext:grContext
-                              pixelFormat:((FlutterView*)self.flutterView).pixelFormat];
+                              pixelFormat:((FlutterView*)self.flutterView).pixelFormat
+                              screenScale:((FlutterView*)self.flutterView).screen.scale];
         }
         latch->CountDown();
       });
@@ -965,8 +970,9 @@ static bool ClipRRectContainsPlatformViewBoundingRect(const SkRRect& clip_rrect,
 
 - (void)createLayerWithIosContext:(const std::shared_ptr<flutter::IOSContext>&)iosContext
                         grContext:(GrDirectContext*)grContext
-                      pixelFormat:(MTLPixelFormat)pixelFormat {
-  self.layerPool->CreateLayer(grContext, iosContext, pixelFormat);
+                      pixelFormat:(MTLPixelFormat)pixelFormat
+                      screenScale:(CGFloat)screenScale {
+  self.layerPool->CreateLayer(grContext, iosContext, pixelFormat, screenScale);
 }
 
 - (void)removeUnusedLayers:(const std::vector<std::shared_ptr<flutter::OverlayLayer>>&)unusedLayers

engine/src/flutter/shell/platform/darwin/ios/framework/Source/FlutterPlatformViews_Internal.h
@@ -67,7 +67,7 @@
 - (instancetype)initWithCapacity:(NSInteger)capacity;
 
 // Reuse a maskView from the pool, or allocate a new one.
-- (FlutterClippingMaskView*)getMaskViewWithFrame:(CGRect)frame;
+- (FlutterClippingMaskView*)getMaskViewWithFrame:(CGRect)frame screenScale:(CGFloat)screenScale;
 
 // Insert the `maskView` into the pool.
 - (void)insertViewToPoolIfNeeded:(FlutterClippingMaskView*)maskView;

engine/src/flutter/shell/platform/darwin/ios/framework/Source/overlay_layer_pool.h
@@ -65,7 +65,8 @@ class OverlayLayerPool {
   /// This method can only be called on the Platform thread.
   void CreateLayer(GrDirectContext* gr_context,
                    const std::shared_ptr<IOSContext>& ios_context,
-                   MTLPixelFormat pixel_format);
+                   MTLPixelFormat pixel_format,
+                   CGFloat screenScale);
 
   /// @brief Removes unused layers from the pool. Returns the unused layers.
   std::vector<std::shared_ptr<OverlayLayer>> RemoveUnusedLayers();

commit 4cb18b3e21712c1157451c6909c9c74452586381

[ios][pv]fix platform view crash due to screenScale=0 (#165525)
This is a regression introduced by:
https://github.com/flutter/flutter/pull/162785/files#diff-b08b98ecaa5bd434f8b4181793f2dad9389c32c5b8cdedb52b01a2d3355f51a9R26

It's reported by one of the customers. We are not able to reproduce it. 

It's likely that `flutterView` itself is `nil` (rather than
`flutterView.screen` being `nil`), because we reset `flutterView` when
backgrounding the app

*List which issues are fixed by this PR. You must list at least one
issue. An issue is not required if the PR fixes something trivial like a
typo.*

b/403598670

*If you had to change anything in the [flutter/tests] repo, include a
link to the migration guide as per the [breaking change policy].*

## Pre-launch Checklist

- [x] I read the [Contributor Guide] and followed the process outlined
there for submitting PRs.
- [x] I read the [Tree Hygiene] wiki page, which explains my
responsibilities.
- [x] I read and followed the [Flutter Style Guide], including [Features
we expect every widget to implement].
- [x] I signed the [CLA].
- [x] I listed at least one issue that this PR fixes in the description
above.
- [x] I updated/added relevant documentation (doc comments with `///`).
- [x] I added new tests to check the change I am making, or this PR is
[test-exempt].
- [x] I followed the [breaking change policy] and added [Data Driven
Fixes] where supported.
- [x] All existing and new tests are passing.

If you need help, consider asking for advice on the #hackers-new channel
on [Discord].

<!-- Links -->
[Contributor Guide]:
https://github.com/flutter/flutter/blob/main/docs/contributing/Tree-hygiene.md#overview
[Tree Hygiene]:
https://github.com/flutter/flutter/blob/main/docs/contributing/Tree-hygiene.md
[test-exempt]:
https://github.com/flutter/flutter/blob/main/docs/contributing/Tree-hygiene.md#tests
[Flutter Style Guide]:
https://github.com/flutter/flutter/blob/main/docs/contributing/Style-guide-for-Flutter-repo.md

engine/src/flutter/shell/platform/darwin/ios/framework/Source/overlay_layer_pool.mm
@@ -23,6 +23,11 @@ void OverlayLayer::UpdateViewState(UIView* flutter_view,
                                    SkRect rect,
                                    int64_t view_id,
                                    int64_t overlay_id) {
+  // There can be a race where UpdateViewState() is called when flutter_view is nil when app is
+  // backgrounded.
+  if (!flutter_view) {
+    return;
+  }
   auto screenScale = ((FlutterView*)flutter_view).screen.scale;
   // Set the size of the overlay view wrapper.
   // This wrapper view masks the overlay view.

engine/src/flutter/shell/platform/darwin/ios/framework/Source/FlutterPlatformViewsTest.mm
@@ -4624,9 +4624,9 @@ fml::RefPtr<fml::TaskRunner> GetDefaultTaskRunner() {
   FlutterEngine* engine = [[FlutterEngine alloc] initWithName:@"foobar"];
   [engine run];
   XCTAssertTrue(engine.platformView != nullptr);
-  auto ios_context = engine.platformView->GetIosContext();
+  std::shared_ptr<flutter::IOSContext> ios_context = engine.platformView->GetIosContext();
 
-  auto pool = flutter::OverlayLayerPool{};
+  flutter::OverlayLayerPool pool;
 
   // Add layers to the pool.
   pool.CreateLayer(ios_context, MTLPixelFormatBGRA8Unorm, 1);
@@ -4642,6 +4642,26 @@ fml::RefPtr<fml::TaskRunner> GetDefaultTaskRunner() {
   auto unused_layers = pool.RemoveUnusedLayers();
   XCTAssertEqual(unused_layers.size(), 2u);
   XCTAssertEqual(pool.size(), 1u);
+}
+
+- (void)testLayerUpdateViewStateWithNilFlutterViewShouldNotCrash {
+  // Create an IOSContext.
+  FlutterEngine* engine = [[FlutterEngine alloc] initWithName:@"foobar"];
+  [engine run];
+  XCTAssertTrue(engine.platformView != nullptr);
+  std::shared_ptr<flutter::IOSContext> ios_context = engine.platformView->GetIosContext();
+
+  flutter::OverlayLayerPool pool;
+
+  // Add layers to the pool.
+  pool.CreateLayer(ios_context, MTLPixelFormatBGRA8Unorm, 1);
+  XCTAssertEqual(pool.size(), 1u);
+
+  std::shared_ptr<flutter::OverlayLayer> layer = pool.GetNextLayer();
+
+  layer->UpdateViewState(nil, SkRect::MakeXYWH(1, 2, 3, 4), 0, 0);
+  // Should not update the view state (e.g. overlay_view_wrapper's frame) when FlutterView is nil.
+  XCTAssertTrue(CGRectEqualToRect(layer->overlay_view_wrapper.frame, CGRectZero));
 }
 
 - (void)testFlutterPlatformViewControllerSubmitFramePreservingFrameDamage {