cc4598fdcd3e6a735086e1c5852a35dbbb4de408
Reid Baker <reidbaker@google.com>
2025-03-24 14:05:38 -0400

    Move code to utils and utils test

--- packages/flutter_tools/gradle/src/main/kotlin/FlutterPluginUtils.kt
+++ packages/flutter_tools/gradle/src/main/kotlin/FlutterPluginUtils.kt
@@ -2,8 +2,12 @@ package com.flutter.gradle
 
 import com.android.build.gradle.AbstractAppExtension
 import com.android.build.gradle.BaseExtension
+import com.android.build.gradle.api.BaseVariantOutput
+import com.android.build.gradle.tasks.ProcessAndroidResources
 import com.android.builder.model.BuildType
 import groovy.lang.Closure
+import groovy.util.Node
+import groovy.util.XmlParser
 import org.gradle.api.GradleException
 import org.gradle.api.JavaVersion
 import org.gradle.api.Project
@@ -849,6 +853,205 @@ object FlutterPluginUtils {
             doLast {
                 androidExtension.applicationVariants.forEach { variant ->
                     println("BuildVariant: ${variant.name}")
+                }
+            }
+        }
+    }
+
+    private fun findProcessResources(baseVariantOutput: BaseVariantOutput): ProcessAndroidResources {
+        // Semantic change, baseVariant does not have a hasProperty method but that
+        // is what the groovy code was checking.
+        // val processResources = project.hasProperty(FlutterPluginConstants.PROP_PROCESS_RESOURCES_PROVIDER) ?
+        // baseVariantOutput.processResourcesProvider.get() : baseVariantOutput.processResources
+        return baseVariantOutput.processResources
+    }
+
+    /**
+     * Adds required tasks for the AppLinkSettings feature.
+     *
+     * Should only be called if the build target is an app, as opposed to an aar/module.
+     *
+     * Add a task that can be called on Flutter projects that outputs app link related project
+     * settings into a json file.
+     * See https://developer.android.com/training/app-links/ for more information about app link.
+     * The json will be saved in path stored in outputPath parameter.
+     *
+     * An example json:
+     *  {
+     *      applicationId: "com.example.app",
+     *          deeplinks: [
+     *              {"scheme":"http", "host":"example.com", "path":".*"},
+     *              {"scheme":"https","host":"example.com","path":".*"}
+     *          ]
+     *  }
+     * The output file is parsed and used by devtool.
+     */
+    @JvmStatic
+    @JvmName("addTasksForOutputsAppLinkSettings")
+    internal fun addTasksForOutputsAppLinkSettings(project: Project) {
+        // Integration test for AppLinkSettings task defined in
+        // flutter/flutter/packages/flutter_tools/test/integration.shard/android_gradle_outputs_app_link_settings_test.dart
+        val android = FlutterPluginUtils.getAndroidExtensionOrNull(project)
+        if (android == null) {
+            project.logger.info("addTasksForOutputsAppLinkSettings called on project without android extension")
+            return
+        }
+        android.applicationVariants.configureEach {
+            val variant = this
+            project.tasks.register("output${FlutterPluginUtils.capitalize(variant.name)}AppLinkSettings") {
+                val task = this
+                task.description =
+                    "stores app links settings for the given build variant of this Android project into a json file."
+                variant.outputs.configureEach {
+                    val baseVariantOutput = this
+                    // Deeplinks are defined in AndroidManifest.xml and is only available after
+                    // processResourcesProvider.
+                    dependsOn(findProcessResources(baseVariantOutput))
+                }
+                doLast {
+                    // We are configuring the same object before a doLast and in a doLast.
+                    // without a clear reason why. That is not good.
+                    variant.outputs.configureEach {
+                        val baseVariantOutput = this
+
+                        val appLinkSettings = AppLinkSettings(variant.applicationId)
+                        // TODO use import groovy.xml.XmlParser instead.
+                        // It is not namespace aware because it makes querying nodes cumbersome.
+                        val manifest: Node =
+                            XmlParser(false, false).parse(findProcessResources(baseVariantOutput).manifestFile)
+                        // The new import would use getProperty like
+                        // manifest.getProperty("application").let { applicationNode -> ...
+                        val applicationNode: Node? =
+                            manifest.children().find { node ->
+                                node is Node && node.name() == "application"
+                            } as Node?
+
+                        applicationNode?.let { appNode ->
+                            val activities: List<Any?> =
+                                appNode.children().filter { item ->
+                                    item is Node && item.name() == "activity"
+                                }
+
+                            activities.forEach { activity ->
+                                if (activity is Node) {
+                                    val metaDataItems: List<Any?> =
+                                        activity.children().filter { metaItem ->
+                                            metaItem is Node && metaItem.name() == "meta-data"
+                                        }
+                                    metaDataItems.forEach { metaDataItem ->
+                                        if (metaDataItem is Node) {
+                                            val nameAttribute: Boolean =
+                                                metaDataItem.attribute("android:name") == "flutter_deeplinking_enabled"
+                                            val valueAttribute: Boolean =
+                                                metaDataItem.attribute("android:value") == "true"
+                                            if (nameAttribute && valueAttribute) {
+                                                appLinkSettings.deeplinkingFlagEnabled = true
+                                            }
+                                        }
+                                    }
+                                    val intentFilterItems: List<Any?> =
+                                        activity.children().filter { filterItem ->
+                                            filterItem is Node && filterItem.name() == "intent-filter"
+                                        }
+                                    intentFilterItems.forEach { appLinkIntent ->
+                                        if (appLinkIntent is Node) {
+                                            // Print out the host attributes in data tags.
+                                            val schemes: MutableSet<String?> = mutableSetOf()
+                                            val hosts: MutableSet<String?> = mutableSetOf()
+                                            val paths: MutableSet<String?> = mutableSetOf()
+                                            val intentFilterCheck = IntentFilterCheck()
+                                            if (appLinkIntent.attribute("android:autoVerify") == "true") {
+                                                intentFilterCheck.hasAutoVerify = true
+                                            }
+
+                                            val actionItems: List<Any?> =
+                                                appLinkIntent.children().filter { item ->
+                                                    item is Node && item.name() == "action"
+                                                }
+                                            // Weird that any action item causes intentFilterCheck to always be true
+                                            // and we keep looping.
+                                            actionItems.forEach { action ->
+                                                if (action is Node) {
+                                                    if (action.attribute("android:name") == "android.intent.action.VIEW") {
+                                                        intentFilterCheck.hasActionView = true
+                                                    }
+                                                }
+                                            }
+                                            val categoryItems: List<Any?> =
+                                                appLinkIntent.children().filter { item ->
+                                                    item is Node && item.name() == "category"
+                                                }
+                                            categoryItems.forEach { category ->
+                                                if (category is Node) {
+                                                    if (category.attribute("android:name") == "android.intent.category.DEFAULT") {
+                                                        intentFilterCheck.hasDefaultCategory = true
+                                                    }
+                                                    if (category.attribute("android:name") == "android.intent.category.BROWSABLE") {
+                                                        intentFilterCheck.hasBrowsableCategory =
+                                                            true
+                                                    }
+                                                }
+                                            }
+                                            val dataItems: List<Any?> =
+                                                appLinkIntent.children().filter { item ->
+                                                    item is Node && item.name() == "data"
+                                                }
+                                            dataItems.forEach { data ->
+                                                if (data is Node) {
+                                                    data.attributes().forEach { entry ->
+                                                        when ((entry.key)) {
+                                                            "android:scheme" -> schemes.add(entry.value.toString())
+                                                            "android:host" -> hosts.add(entry.value.toString())
+                                                            // All path patterns add to paths.
+                                                            "android:pathAdvancedPattern" ->
+                                                                paths.add(
+                                                                    entry.value.toString()
+                                                                )
+
+                                                            "android:pathPattern" -> paths.add(entry.value.toString())
+                                                            "android:path" -> paths.add(entry.value.toString())
+                                                            "android:pathPrefix" -> paths.add(entry.value.toString() + ".*")
+                                                            "android:pathSuffix" -> paths.add(".*" + entry.value.toString())
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                            if (hosts.isNotEmpty() || paths.isNotEmpty()) {
+                                                if (schemes.isEmpty()) {
+                                                    schemes.add(null)
+                                                }
+                                                if (hosts.isEmpty()) {
+                                                    hosts.add(null)
+                                                }
+                                                if (paths.isEmpty()) {
+                                                    paths.add(".*")
+                                                }
+                                                // Sets are not ordered this is dangerous.
+                                                schemes.forEach { scheme ->
+                                                    hosts.forEach { host ->
+                                                        paths.forEach { path ->
+                                                            appLinkSettings.deeplinks.add(
+                                                                Deeplink(
+                                                                    scheme,
+                                                                    host,
+                                                                    path,
+                                                                    intentFilterCheck
+                                                                )
+                                                            )
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+
+                                    File(project.property("outputPath").toString()).writeText(
+                                        appLinkSettings.toJson().toString()
+                                    )
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
--- packages/flutter_tools/gradle/src/test/kotlin/FlutterPluginUtilsTest.kt
+++ packages/flutter_tools/gradle/src/test/kotlin/FlutterPluginUtilsTest.kt
@@ -2,8 +2,11 @@ package com.flutter.gradle
 
 import com.android.build.gradle.AbstractAppExtension
 import com.android.build.gradle.BaseExtension
+import com.android.build.gradle.api.ApplicationVariant
+import com.android.build.gradle.api.BaseVariantOutput
 import com.android.build.gradle.internal.dsl.CmakeOptions
 import com.android.build.gradle.internal.dsl.DefaultConfig
+import com.android.build.gradle.tasks.ProcessAndroidResources
 import com.android.builder.model.BuildType
 import io.mockk.called
 import io.mockk.every
@@ -11,6 +14,8 @@ import io.mockk.mockk
 import io.mockk.slot
 import io.mockk.verify
 import org.gradle.api.Action
+import org.gradle.api.DomainObjectCollection
+import org.gradle.api.DomainObjectSet
 import org.gradle.api.GradleException
 import org.gradle.api.NamedDomainObjectContainer
 import org.gradle.api.Project
@@ -18,6 +23,7 @@ import org.gradle.api.Task
 import org.gradle.api.UnknownTaskException
 import org.gradle.api.artifacts.dsl.DependencyHandler
 import org.gradle.api.logging.Logger
+import org.gradle.api.tasks.TaskContainer
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.api.io.TempDir
 import java.io.File
@@ -1180,5 +1186,168 @@ class FlutterPluginUtilsTest {
                     Pair("dev_dependency", false)
                 )
             )
+    }
+
+
+    @Test
+    fun addTasksForOutputsAppLinkSettingsNoAndroid(
+        @TempDir tempDir: Path
+    ) {
+        val mockProject = mockk<Project>()
+        val mockLogger = mockk<Logger>()
+        every { mockProject.logger } returns mockLogger
+        every { mockLogger.info(any()) } returns Unit
+        every { mockProject.extensions.findByName("android") } returns null
+
+        FlutterPluginUtils.addTasksForOutputsAppLinkSettings(mockProject)
+        // Consider matching on part of the error.
+        verify(exactly = 1) { mockLogger.info(any()) }
+    }
+
+    val manifestText =
+        """
+            <manifest xmlns:android="http://schemas.android.com/apk/res/android">
+                <!-- Permissions do not break parsing -->
+                <uses-permission android:name="android.permission.INTERNET"/>
+
+                <application android:label="Flutter Task Helper Test" android:icon="@mipmap/ic_launcher">
+                    <activity android:name="com.example.FlutterActivity1"
+                              android:exported="true"
+                              android:theme="@android:style/Theme.Black.NoTitleBar">
+                        <intent-filter>
+                            <action android:name="android.intent.action.MAIN"/>
+                            <category android:name="android.intent.category.LAUNCHER"/>
+                        </intent-filter>
+                    </activity>
+                    <activity android:name="com.example.FlutterActivity2"
+                              android:exported="false"
+                              android:theme="@android:style/Theme.Black.NoTitleBar">
+                        <intent-filter>
+                          <action android:name="android.intent.action.VIEW" />
+                          <category android:name="android.intent.category.DEFAULT" />
+                          <category android:name="android.intent.category.BROWSABLE" />
+                          <data
+                            android:scheme="poc"
+                            android:host="deeplink.flutter.dev"
+                            android:pathPrefix="some.prefix"
+                            />
+                        </intent-filter>
+                        <meta-data android:name="flutter_deeplinking_enabled" android:value="true" />
+                    </activity>
+                    <meta-data
+                        android:name="flutterEmbedding"
+                        android:value="2" />
+
+                </application>
+        </manifest>
+        """.trimIndent()
+
+    @Test
+    fun addTasksForOutputsAppLinkSettingsActual(
+        @TempDir tempDir: Path
+    ) {
+        val mockProject = mockk<Project>()
+        val mockLogger = mockk<Logger>()
+        every { mockProject.logger } returns mockLogger
+        every { mockLogger.info(any()) } returns Unit
+        every { mockLogger.warn(any()) } returns Unit
+        val mockAbstractAppExtension = mockk<AbstractAppExtension>()
+        every { mockProject.extensions.findByName("android") } returns mockAbstractAppExtension
+
+        val testVariants: DomainObjectSet<ApplicationVariant> = mockk<DomainObjectSet<ApplicationVariant>>()
+        val variant1 = mockk<ApplicationVariant>()
+        every { variant1.name } returns "one"
+        every { variant1.applicationId } returns "com.example.FlutterActivity1"
+        val variant2 = mockk<ApplicationVariant>()
+        every { variant2.name } returns "two"
+        every { variant2.applicationId } returns "com.example.FlutterActivity2"
+        val variants = mutableListOf(variant1, variant2)
+        // Capture the "action" that needs to be run for each variant.
+        val actionSlot = slot<Action<ApplicationVariant>>()
+        every { testVariants.configureEach(capture(actionSlot)) } answers {
+            // Execute the action for each variant.
+            variants.forEach { variant ->
+                actionSlot.captured.execute(variant)
+            }
+        }
+        every { mockAbstractAppExtension.applicationVariants } returns testVariants
+
+        // Consider breaking out into a test helper.
+        val descriptionSlot = slot<String>()
+        val registerTaskSlot = slot<Action<Task>>()
+        val registerTaskList: MutableList<Task> = mutableListOf()
+        every { mockProject.tasks } returns
+                mockk<TaskContainer> {
+                    val registerTaskNameSlot = slot<String>()
+                    every { register(capture(registerTaskNameSlot), capture(registerTaskSlot)) } answers registerAnswer@{
+                        val mockRegisterTask =
+                            mockk<Task> {
+                                every { name } returns registerTaskNameSlot.captured
+                                every { description = capture(descriptionSlot) } returns Unit
+                                every { dependsOn(any<ProcessAndroidResources>()) } returns mockk()
+                                val doLastActionSlot = slot<Action<Task>>()
+                                every { doLast(capture(doLastActionSlot)) } answers doLastAnswer@{
+                                    // We need to capture the task as well
+                                    doLastActionSlot.captured.execute(mockk())
+                                    return@doLastAnswer mockk()
+                                }
+                            }
+                        registerTaskList.add(mockRegisterTask)
+                        registerTaskSlot.captured.execute(mockRegisterTask)
+                        return@registerAnswer mockk()
+                    }
+
+                    every { named(any<String>()) } returns
+                            mockk {
+                                every { configure(any<Action<Task>>()) } returns mockk()
+                            }
+                }
+        variants.forEach { variant ->
+            val testOutputs: DomainObjectCollection<BaseVariantOutput> = mockk<DomainObjectCollection<BaseVariantOutput>>()
+            val baseVariantSlot = slot<Action<BaseVariantOutput>>()
+            val baseVariantOutput = mockk<BaseVariantOutput>()
+            val mockProcessResources = mockk<ProcessAndroidResources>()
+            // Create a real file in a temp directory.
+            val manifest =
+                tempDir
+                    .resolve("${tempDir.toAbsolutePath()}/AndroidManifest.xml")
+                    .toFile()
+            manifest.writeText(manifestText)
+
+            every { mockProcessResources.manifestFile } returns manifest
+            every { baseVariantOutput.processResources } returns mockProcessResources
+            every { testOutputs.configureEach(capture(baseVariantSlot)) } answers {
+                // Execute the action for each output.
+                baseVariantSlot.captured.execute(baseVariantOutput)
+            }
+            every { variant.outputs } returns testOutputs
+        }
+        val outputFile =
+            tempDir
+                .resolve("${tempDir.toAbsolutePath()}/app-link-settings-build-variant.json")
+                .toFile()
+        every { mockProject.property("outputPath") } returns outputFile
+
+        FlutterPluginUtils.addTasksForOutputsAppLinkSettings(mockProject)
+
+        verify(exactly = 0) { mockLogger.info(any()) }
+        assert(descriptionSlot.captured.contains("stores app links settings for the given build variant"))
+        assertEquals(variants.size, registerTaskList.size)
+        for (i in 0 until variants.size) {
+            assertEquals("output${FlutterPluginUtils.capitalize(variants[i].name)}AppLinkSettings", registerTaskList[i].name)
+            verify(exactly = 1) { registerTaskList[i].dependsOn(any<ProcessAndroidResources>()) }
+        }
+        // Output assertions are minimal which ensures code is running but is not exhaustive testing.
+        // Integration test for more exhaustive behavior is defined in
+        // flutter/flutter/packages/flutter_tools/test/integration.shard/android_gradle_outputs_app_link_settings_test.dart
+        val outputFileText = outputFile.readText()
+        // Only variant2 since that one has app links.
+        assertContains(outputFileText, variant2.applicationId)
+        // Host.
+        assertContains(outputFileText, "deeplink.flutter.dev")
+        // pathPrefix used in variant2 combined with prefix logic.
+        assertContains(outputFileText, "some.prefix.*")
+        // Deep linking
+        assertContains(outputFileText, "deeplinkingFlagEnabled\":true")
     }
 }
