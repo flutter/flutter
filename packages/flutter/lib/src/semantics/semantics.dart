ae30e5b21154d67f1fb03cf4967d77b203bbbdb7
Qun Cheng <chengqunq@gmail.com>
2025-03-26 16:18:31 -0700

    A menu is possible to have empty content

--- packages/flutter/lib/src/semantics/semantics.dart
+++ packages/flutter/lib/src/semantics/semantics.dart
@@ -115,7 +115,7 @@ sealed class _DebugSemanticsRoleChecks {
     SemanticsRole.row => _semanticsRow,
     SemanticsRole.columnHeader => _semanticsColumnHeader,
     SemanticsRole.radioGroup => _semanticsRadioGroup,
-    SemanticsRole.menu => _semanticsMenu,
+    SemanticsRole.menu => _noCheckRequired,
     SemanticsRole.menuBar => _semanticsMenuBar,
     SemanticsRole.menuItem => _semanticsMenuItem,
     SemanticsRole.menuItemCheckbox => _semanticsMenuItemCheckbox,
@@ -240,14 +240,6 @@ sealed class _DebugSemanticsRoleChecks {
 
     node.visitChildren(validateRadioGroupChildren);
     return error;
-  }
-
-  static FlutterError? _semanticsMenu(SemanticsNode node) {
-    if (node.childrenCount < 1) {
-      return FlutterError('a menu cannot be empty');
-    }
-
-    return null;
   }
 
   static FlutterError? _semanticsMenuBar(SemanticsNode node) {
