d99107ce52b6f6c205f736700fe5cea31c9acbc3
Qun Cheng <chengqunq@gmail.com>
2025-03-26 12:38:54 -0700

    Update

--- packages/flutter/lib/src/widgets/raw_menu_anchor.dart
+++ packages/flutter/lib/src/widgets/raw_menu_anchor.dart
@@ -8,7 +8,6 @@ library;
 import 'dart:ui' as ui;
 
 import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:flutter/services.dart';
