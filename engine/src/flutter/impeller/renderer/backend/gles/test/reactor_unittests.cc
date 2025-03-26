5efbd2e648d2a2c614c495d28325761d62cc5750
Jonah Williams <jonahwilliams@google.com>
2025-03-25 17:08:40 -0700

    [Impeller] disable GLES tracing unless opted in. (#165887)
    GLES tracing may add substantial overhead on some devices. Since this is
    only useful when using frame capture tools, and its unlikely anyone that
    isn't an engine developer (or who knows how to compile the engine) will
    use them - we can turn off with a define.
    
    If there are other usecases we need to cover later this can be made to
    use the new flag system.

--- engine/src/flutter/impeller/renderer/backend/gles/test/reactor_unittests.cc
+++ engine/src/flutter/impeller/renderer/backend/gles/test/reactor_unittests.cc
@@ -98,13 +98,7 @@ TEST(ReactorGLES, UntrackedHandle) {
 
 TEST(ReactorGLES, NameUntrackedHandle) {
   auto mock_gles_impl = std::make_unique<NiceMock<MockGLESImpl>>();
-
-  EXPECT_CALL(*mock_gles_impl, GenTextures(1, _))
-      .WillOnce([](GLsizei size, GLuint* queries) { queries[0] = 1234; });
-  EXPECT_CALL(*mock_gles_impl,
-              ObjectLabelKHR(_, 1234, _, ::testing::StrEq("hello, joe!")))
-      .Times(1);
-  ON_CALL(*mock_gles_impl, IsTexture).WillByDefault(::testing::Return(GL_TRUE));
+  NiceMock<MockGLESImpl>* raw_mock_gles = mock_gles_impl.get();
 
   std::shared_ptr<MockGLES> mock_gles =
       MockGLES::Init(std::move(mock_gles_impl));
@@ -114,6 +108,13 @@ TEST(ReactorGLES, NameUntrackedHandle) {
   if (!proc_table->SupportsDebugLabels()) {
     GTEST_SKIP() << "This device doesn't support labelling.";
   }
+
+  EXPECT_CALL(*raw_mock_gles, GenTextures(1, _))
+      .WillOnce([](GLsizei size, GLuint* queries) { queries[0] = 1234; });
+  EXPECT_CALL(*raw_mock_gles,
+              ObjectLabelKHR(_, 1234, _, ::testing::StrEq("hello, joe!")))
+      .Times(1);
+  ON_CALL(*raw_mock_gles, IsTexture).WillByDefault(::testing::Return(GL_TRUE));
 
   auto worker = std::make_shared<TestWorker>();
   auto reactor = std::make_shared<ReactorGLES>(std::move(proc_table));
