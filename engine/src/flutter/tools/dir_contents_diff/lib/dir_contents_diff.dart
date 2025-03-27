6d0a701bbfda8ee62df36b3d9a848d03937487a9
Jonah Williams <jonahwilliams@google.com>
2025-03-26 20:32:02 -0700

    Update dir_contents_diff.dart

--- engine/src/flutter/tools/dir_contents_diff/lib/dir_contents_diff.dart
+++ engine/src/flutter/tools/dir_contents_diff/lib/dir_contents_diff.dart
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'dart:io';
-
 /// The main entrypoint for the program, returns `exitCode`.
 void run(List<String> args) {
   if (args.length != 1) {
