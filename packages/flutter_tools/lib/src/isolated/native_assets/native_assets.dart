82c7ea417f2e591487433cfdfb8d6884cd7a2e6d
Moritz <mosum@google.com>
2025-03-27 15:02:26 +0100

    Use build dir even if no assets present

--- packages/flutter_tools/lib/src/isolated/native_assets/native_assets.dart
+++ packages/flutter_tools/lib/src/isolated/native_assets/native_assets.dart
@@ -41,15 +41,16 @@ Future<DartHookResult> runFlutterSpecificHooks({
   required Uri projectUri,
   required FileSystem fileSystem,
 }) async {
-  if (!await _hookRunRequired(buildRunner)) {
-    return DartHookResult.empty();
-  }
   final Uri buildUri = nativeAssetsBuildUri(projectUri, targetPlatform.osName);
   final Directory buildDir = fileSystem.directory(buildUri);
   if (!await buildDir.exists()) {
     // Ensure the folder exists so the native build system can copy it even
     // if there's no native assets.
     await buildDir.create(recursive: true);
+  }
+
+  if (!await _hookRunRequired(buildRunner)) {
+    return DartHookResult.empty();
   }
 
   final List<String> supportedAssetTypes = <String>[
