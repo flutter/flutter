a68ec1cb139c1b66cb4fa8f66026cca02a94169b
Victor Sanni <victorsanniay@gmail.com>
2025-03-26 19:50:50 -0700

    Add scroll and fade transition styles

--- packages/flutter/lib/src/cupertino/collapsible.dart
+++ packages/flutter/lib/src/cupertino/collapsible.dart
@@ -16,7 +16,19 @@ import 'list_tile.dart';
 /// The same curve is used for both expansion and collapse animations.
 ///
 /// Based on iOS 17 manual testing.
-const Curve _kCupertinoCollapsibleAnimationCurve = Curves.easeInOut;
+const Curve _kAnimationCurve = Curves.easeInOut;
+
+/// The height of the header of the collapsible, which is a [CupertinoListTile].
+const double _kHeaderHeight = 44.0;
+
+///
+enum CollapsibleTransitionMode {
+  ///
+  fadeTransition,
+
+  ///
+  scrollTransition,
+}
 
 ///
 class CupertinoCollapsible extends StatefulWidget {
@@ -26,6 +38,7 @@ class CupertinoCollapsible extends StatefulWidget {
     required this.title,
     required this.child,
     this.controller,
+    this.transitionMode = CollapsibleTransitionMode.fadeTransition,
   });
 
   /// A [title] is used to convey the central information. Usually a [Text].
@@ -41,6 +54,9 @@ class CupertinoCollapsible extends StatefulWidget {
   /// The body of the collapsible.
   final Widget child;
 
+  ///
+  final CollapsibleTransitionMode transitionMode;
+
   @override
   State<CupertinoCollapsible> createState() => _CupertinoCollapsibleState();
 }
@@ -48,6 +64,9 @@ class CupertinoCollapsible extends StatefulWidget {
 class _CupertinoCollapsibleState extends State<CupertinoCollapsible> {
   static final Animatable<double> _easeInTween = CurveTween(curve: Curves.easeIn);
   static final Animatable<double> _quarterTween = Tween<double>(begin: 0.0, end: 0.25);
+  final OverlayPortalController _fadeController = OverlayPortalController();
+  final GlobalKey _headerKey = GlobalKey();
+  late Offset headerOffset;
 
   late Animation<double> _iconTurns;
   late ExpansibleController _tileController;
@@ -79,9 +98,22 @@ class _CupertinoCollapsibleState extends State<CupertinoCollapsible> {
     );
   }
 
+  void _onHeaderTap() {
+    final RenderBox headerBox = _headerKey.currentContext!.findRenderObject()! as RenderBox;
+    headerOffset = headerBox.localToGlobal(Offset.zero);
+    _fadeController.show();
+
+    if (_tileController.isExpanded) {
+      _tileController.collapse();
+    } else {
+      _tileController.expand();
+    }
+  }
+
   Widget _buildHeader(BuildContext context, Animation<double> animation) {
     return CupertinoListTile(
-      onTap: _tileController.isExpanded ? _tileController.collapse : _tileController.expand,
+      key: _headerKey,
+      onTap: _onHeaderTap,
       title: widget.title,
       trailing: _buildIcon(context, animation),
       padding: const EdgeInsets.only(right: 20.0),
@@ -89,13 +121,62 @@ class _CupertinoCollapsibleState extends State<CupertinoCollapsible> {
     );
   }
 
+  Widget _buildCollapsible(
+    BuildContext context,
+    Widget header,
+    Widget body,
+    Animation<double> animation,
+  ) {
+    final Widget child = Column(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        header,
+        Opacity(
+          opacity:
+              animation.isAnimating &&
+                      widget.transitionMode == CollapsibleTransitionMode.fadeTransition
+                  ? 0.0
+                  : 1.0,
+          child: body,
+        ),
+      ],
+    );
+
+    if (widget.transitionMode == CollapsibleTransitionMode.scrollTransition) {
+      return child;
+    }
+
+    return LayoutBuilder(
+      builder: (BuildContext context, BoxConstraints constraints) {
+        return OverlayPortal(
+          controller: _fadeController,
+          overlayChildBuilder: (BuildContext context) {
+            return Positioned(
+              top: headerOffset.dy + _kHeaderHeight,
+              left: headerOffset.dx,
+              child: ConstrainedBox(
+                constraints: constraints,
+                child: Visibility(
+                  visible: animation.isAnimating,
+                  child: FadeTransition(opacity: animation, child: widget.child),
+                ),
+              ),
+            );
+          },
+          child: child,
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Expansible(
       controller: _tileController,
-      curve: _kCupertinoCollapsibleAnimationCurve,
+      curve: _kAnimationCurve,
       headerBuilder: _buildHeader,
       bodyBuilder: (BuildContext context, Animation<double> animation) => widget.child,
+      expansibleBuilder: _buildCollapsible,
     );
   }
 }
