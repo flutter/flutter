b39f25dae11976d2e92b49f66633846bf8da5b7e
Aaron Clarke <aaclarke@google.com>
2025-03-27 13:02:53 -0700

    simplified math

--- engine/src/flutter/impeller/entity/contents/line_contents.cc
+++ engine/src/flutter/impeller/entity/contents/line_contents.cc
@@ -173,7 +173,9 @@ std::vector<uint8_t> LineContents::CreateCurveData(Scalar width,
                                                    Scalar scale) {
   std::vector<uint8_t> curve_data;
   curve_data.reserve(kCurveResolution);
-  double slope = 1.0 / ((radius * 2) / (scale * width + radius));
+  // More simply written as rise / run:
+  // double slope = 1.0 / ((radius * 2) / (scale * width + radius));
+  double slope = (scale * width + radius) / (radius * 2);
   for (int i = 0; i < kCurveResolution; ++i) {
     double norm = (static_cast<double>(i)) / 31.0;
     double scaled = slope * norm;
