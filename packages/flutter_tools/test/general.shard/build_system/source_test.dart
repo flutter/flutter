1f49aeaa3540ff3a184c5ef94205fc63de466054
Loïc Sharma <737941+loic-sharma@users.noreply.github.com>
2025-03-31 22:15:25 -0700

    [tool] Improve using project files in build targets (#166211)
    The Flutter tool's build system executes targets that take inputs and
    produces outputs. Previously, targets would hardcode paths if they
    needed to use Flutter project files as inputs/outputs. For example:
    
    ```dart
    class FooTarget extends Target {
      @override
      List<Source> get inputs => <Source>[
        Source.pattern('{PROJECT_DIR}/foo/bar'),
      ];
    }
    ```
    
    This is problematic as the
    [`FlutterProject`](https://github.com/flutter/flutter/blob/05b5e79105441acb55e98a778ff7854cd191de8c/packages/flutter_tools/lib/src/project.dart#L89)
    is the source of truth for where a Flutter project's files are located:
    
    1. If we change a project file's location, we need to update
    `FlutterProject` as well as any hardcoded target inputs/outputs.
    2. Project files' location can be dynamic. For example, a Flutter app
    puts iOS files in the `ios/` directory, but a Flutter module puts iOS
    files in the `.ios/` directory. Targets need to duplicate
    `FlutterProject`'s logic to determine the project file's location.
    
    As a result, hardcoding project file paths in targets can be
    error-prone.
    
    This introduces a new `Source` factory that lets you use the
    `FlutterProject` to create the source:
    
    ```dart
    class FooTarget extends Target {
      @override
      List<Source> get inputs => <Source>[
        Source.fromProject((FlutterProject project) => project.fooFile),
      ];
    }
    ```
    
    Part of https://github.com/flutter/flutter/issues/163874
    
    Next pull request: https://github.com/flutter/flutter/pull/165916
    
    ## Pre-launch Checklist
    
    - [x] I read the [Contributor Guide] and followed the process outlined
    there for submitting PRs.
    - [x] I read the [Tree Hygiene] wiki page, which explains my
    responsibilities.
    - [x] I read and followed the [Flutter Style Guide], including [Features
    we expect every widget to implement].
    - [x] I signed the [CLA].
    - [x] I listed at least one issue that this PR fixes in the description
    above.
    - [x] I updated/added relevant documentation (doc comments with `///`).
    - [x] I added new tests to check the change I am making, or this PR is
    [test-exempt].
    - [x] I followed the [breaking change policy] and added [Data Driven
    Fixes] where supported.
    - [x] All existing and new tests are passing.
    
    If you need help, consider asking for advice on the #hackers-new channel
    on [Discord].
    
    <!-- Links -->
    [Contributor Guide]:
    https://github.com/flutter/flutter/blob/main/docs/contributing/Tree-hygiene.md#overview
    [Tree Hygiene]:
    https://github.com/flutter/flutter/blob/main/docs/contributing/Tree-hygiene.md
    [test-exempt]:
    https://github.com/flutter/flutter/blob/main/docs/contributing/Tree-hygiene.md#tests
    [Flutter Style Guide]:
    https://github.com/flutter/flutter/blob/main/docs/contributing/Style-guide-for-Flutter-repo.md
    [Features we expect every widget to implement]:
    https://github.com/flutter/flutter/blob/main/docs/contributing/Style-guide-for-Flutter-repo.md#features-we-expect-every-widget-to-implement
    [CLA]: https://cla.developers.google.com/
    [flutter/tests]: https://github.com/flutter/tests
    [breaking change policy]:
    https://github.com/flutter/flutter/blob/main/docs/contributing/Tree-hygiene.md#handling-breaking-changes
    [Discord]:
    https://github.com/flutter/flutter/blob/main/docs/contributing/Chat.md
    [Data Driven Fixes]:
    https://github.com/flutter/flutter/blob/main/docs/contributing/Data-driven-Fixes.md

--- packages/flutter_tools/test/general.shard/build_system/source_test.dart
+++ packages/flutter_tools/test/general.shard/build_system/source_test.dart
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'package:file_testing/file_testing.dart';
 import 'package:flutter_tools/src/artifacts.dart';
 import 'package:flutter_tools/src/base/file_system.dart';
 import 'package:flutter_tools/src/base/platform.dart';
@@ -9,6 +10,7 @@ import 'package:flutter_tools/src/build_info.dart';
 import 'package:flutter_tools/src/build_system/build_system.dart';
 import 'package:flutter_tools/src/build_system/exceptions.dart';
 import 'package:flutter_tools/src/globals.dart' as globals;
+import 'package:flutter_tools/src/project.dart';
 
 import '../../src/common.dart';
 import '../../src/fake_process_manager.dart';
@@ -280,6 +282,64 @@ void main() {
       fizzSource.accept(visitor);
 
       expect(visitor.sources.single.path, contains('engine.stamp'));
+    }),
+  );
+
+  test(
+    'can substitute project file',
+    () => testbed.run(() {
+      final String path = globals.fs.file('.flutter-plugins-dependencies').absolute.path;
+      globals.fs.file(path).createSync(recursive: true);
+      final Source pluginsSource = Source.fromProject(
+        (FlutterProject project) => project.flutterPluginsDependenciesFile,
+      );
+      pluginsSource.accept(visitor);
+
+      expect(visitor.sources.single.absolute.path, path);
+      expect(visitor.sources.single, exists);
+    }),
+  );
+
+  test(
+    'can substitute nonexistent project file',
+    () => testbed.run(() {
+      final String path = globals.fs.file('.flutter-plugins-dependencies').absolute.path;
+      final Source pluginsSource = Source.fromProject(
+        (FlutterProject project) => project.flutterPluginsDependenciesFile,
+      );
+      pluginsSource.accept(visitor);
+
+      expect(visitor.sources.single.absolute.path, path);
+      expect(visitor.sources.single, isNot(exists));
+    }),
+  );
+
+  test(
+    'can substitute optional project file',
+    () => testbed.run(() {
+      final String path = globals.fs.file('.flutter-plugins-dependencies').absolute.path;
+      globals.fs.file(path).createSync(recursive: true);
+      final Source pluginsSource = Source.fromProject(
+        (FlutterProject project) => project.flutterPluginsDependenciesFile,
+        optional: true,
+      );
+      pluginsSource.accept(visitor);
+
+      expect(visitor.sources.single.absolute.path, path);
+      expect(visitor.sources.single, exists);
+    }),
+  );
+
+  test(
+    'skips nonexistent optional project file',
+    () => testbed.run(() {
+      final Source pluginsSource = Source.fromProject(
+        (FlutterProject project) => project.flutterPluginsDependenciesFile,
+        optional: true,
+      );
+      pluginsSource.accept(visitor);
+
+      expect(visitor.sources.isEmpty, isTrue);
     }),
   );
 }
