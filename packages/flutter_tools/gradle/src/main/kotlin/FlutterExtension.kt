4fa1ac376075e47a4a75e356a02ac28e6deb6c8c
jesswrd <jesswon@google.com>
2025-04-02 10:15:29 -0700

    will likely need to update other areas

--- packages/flutter_tools/gradle/src/main/kotlin/FlutterExtension.kt
+++ packages/flutter_tools/gradle/src/main/kotlin/FlutterExtension.kt
@@ -15,7 +15,7 @@ import org.gradle.api.GradleException
  */
 open class FlutterExtension {
     /** Sets the compileSdkVersion used by default in Flutter app projects. */
-    val compileSdkVersion: Int = 35
+    val compileSdkVersion: Int = 36
 
     /** Sets the minSdkVersion used by default in Flutter app projects. */
     val minSdkVersion: Int = 21
@@ -26,7 +26,7 @@ open class FlutterExtension {
      *
      * See https://developer.android.com/guide/topics/manifest/uses-sdk-element.
      */
-    val targetSdkVersion: Int = 35
+    val targetSdkVersion: Int = 36
 
     /**
      * Sets the ndkVersion used by default in Flutter app projects.
--- packages/flutter_tools/lib/src/android/gradle_utils.dart
+++ packages/flutter_tools/lib/src/android/gradle_utils.dart
@@ -45,9 +45,9 @@ const String templateKotlinGradlePluginVersion = '2.1.0';
 // so new versions are picked up after a Flutter upgrade.
 //
 // Please see the README before changing any of these values.
-const String compileSdkVersion = '35';
+const String compileSdkVersion = '36';
 const String minSdkVersion = '21';
-const String targetSdkVersion = '35';
+const String targetSdkVersion = '36';
 const String ndkVersion = '26.1.10909125';
 
 // Update these when new major versions of Java are supported by new Gradle
