de62dd3f9b6bbc92fe4342e671d5b7539af87c3d
Jonah Williams <jonahwilliams@google.com>
2025-04-04 13:26:26 -0700

    Update capabilities_vk.cc

--- engine/src/flutter/impeller/renderer/backend/vulkan/capabilities_vk.cc
+++ engine/src/flutter/impeller/renderer/backend/vulkan/capabilities_vk.cc
@@ -665,7 +665,7 @@ bool CapabilitiesVK::SupportsTextureToTextureBlits() const {
 
 // |Capabilities|
 bool CapabilitiesVK::SupportsFramebufferFetch() const {
-  return supports_framebuffer_fetch_;
+  return has_framebuffer_fetch_;
 }
 
 // |Capabilities|
