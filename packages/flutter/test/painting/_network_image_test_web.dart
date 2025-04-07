2f218596aca2044f67eb60dbcee345e83ce18613
Tong Mu <dkwingsmt@gmail.com>
2025-04-07 16:12:13 -0700

    Fix typo

--- packages/flutter/test/painting/_network_image_test_web.dart
+++ packages/flutter/test/painting/_network_image_test_web.dart
@@ -365,7 +365,7 @@ void runTests() {
     expect(find.byType(PlatformViewLink), findsOneWidget);
   });
 
-  testWidgets('Does not crash when disposed between frazmes', (WidgetTester tester) async {
+  testWidgets('Does not crash when disposed between frames', (WidgetTester tester) async {
     final TestHttpRequest testHttpRequest =
         TestHttpRequest()
           ..status = 200
