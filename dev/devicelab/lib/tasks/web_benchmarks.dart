aad5a3526ced7189eee04731b1a98ce8e2fdad90
MarkZ <markzipan@google.com>
2025-04-08 12:38:47 -0700

    removing unused libs

--- dev/devicelab/lib/tasks/web_benchmarks.dart
+++ dev/devicelab/lib/tasks/web_benchmarks.dart
@@ -3,7 +3,7 @@
 // found in the LICENSE file.
 
 import 'dart:async';
-import 'dart:convert' show LineSplitter, json, utf8;
+import 'dart:convert' show json;
 import 'dart:io' as io;
 
 import 'package:logging/logging.dart';
