8d5821cf64e59a8b1a08d94bf50fb0496d16650c
Jonah Williams <jonahwilliams@google.com>
2025-04-09 15:42:50 -0700

    bump max tasks to a huge number. (#166876)
    64 isn't a big enough number, go higher!

--- engine/src/flutter/impeller/renderer/context.h
+++ engine/src/flutter/impeller/renderer/context.h
@@ -58,7 +58,7 @@ class Context {
   /// This number was arbitrarily chosen. The idea is that this is a somewhat
   /// rare situation where tasks happen to get executed in that tiny amount of
   /// time while an app is being backgrounded but still executing.
-  static constexpr int32_t kMaxTasksAwaitingGPU = 64;
+  static constexpr int32_t kMaxTasksAwaitingGPU = 1024;
 
   //----------------------------------------------------------------------------
   /// @brief      Destroys an Impeller context.
