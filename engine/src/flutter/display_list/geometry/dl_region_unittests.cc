06c572c73023775b57c1475c1fc2645e183ed29f
Jim Graham <flar@google.com>
2025-04-09 15:11:16 -0700

    [Impeller] Tessellate paths directly using PathReceiver (#166759)
    Currently Impeller asks incoming paths to convert themselves into the
    impeller::Path class before tessellating their outlines. This wastes
    conversion time when the path is only needed to iterate the path
    segments. We now tessellate them directly from a path segment dispatcher
    saving the conversion cost.

--- engine/src/flutter/display_list/geometry/dl_region_unittests.cc
+++ engine/src/flutter/display_list/geometry/dl_region_unittests.cc
@@ -3,6 +3,8 @@
 // found in the LICENSE file.
 
 #include "flutter/display_list/geometry/dl_region.h"
+
+#include "flutter/display_list/geometry/dl_geometry_conversions.h"
 #include "gtest/gtest.h"
 
 #include "third_party/skia/include/core/SkRegion.h"
