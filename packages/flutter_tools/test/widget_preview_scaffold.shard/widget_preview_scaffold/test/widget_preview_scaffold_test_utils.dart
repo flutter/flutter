39d564e1fec0fbcbe33c71e3534ddf2b24e46e5b
Ben Konyi <bkonyi@google.com>
2025-04-10 12:20:11 -0400

    [ Widget Preview ] Add support for "soft" restart of individual previews (#166846)
    A soft restart simply removes the previewed widget from the widget tree
    for a frame before re-inserting it on the next frame. This has the
    effect of re-running local initializers in `State` objects, which would
    normally require a hot restart to accomplish. This reduces the number of
    cases where a full hot restart of the preview environment would be
    needed to pick up state-related changes.
    
    Fixes https://github.com/flutter/flutter/issues/166450
    
    **Updated Controls:**
    
    
    ![image](https://github.com/user-attachments/assets/616f8fc4-0b57-4a1c-85e7-ef36126cdffe)
    
    **Demo:** updates a string initialized in `initState`, triggering a hot
    reload. The updated string isn't rendered until the restart button is
    pressed, causing the widget to be reloaded with the new state
    initialized.
    
    
    https://github.com/user-attachments/assets/ab4abf8a-7823-491a-ad90-f83d877600ec

--- packages/flutter_tools/test/widget_preview_scaffold.shard/widget_preview_scaffold/test/widget_preview_scaffold_test_utils.dart
+++ packages/flutter_tools/test/widget_preview_scaffold.shard/widget_preview_scaffold/test/widget_preview_scaffold_test_utils.dart
@@ -2,68 +2,37 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:widget_preview_scaffold/src/widget_preview_rendering.dart';
 
-abstract class WidgetPreviewScaffoldTestUtils {
-  static final Set<String> _ignoreDiffSet = <String>{
-    // The pubspec can't be compared directly to the template since the SDK version is populated
-    // when the template is hydrated based on the current SDK version.
-    'pubspec.yaml',
-    'lib/src/generated_preview.dart',
-  };
+class WidgetPreviewerWidgetScaffolding extends StatelessWidget {
+  const WidgetPreviewerWidgetScaffolding({super.key, required this.child});
 
-  /// Checks to see if the widget_preview_scaffold template files have been updated.
-  ///
-  /// Returns true if the widget_preview_scaffold project should be regenerated.
-  static bool checkForTemplateUpdates({
-    required Directory widgetPreviewScaffoldProject,
-    required Directory widgetPreviewScaffoldTemplateDir,
-  }) {
-    bool updateDetected = false;
-    for (final FileSystemEntity entity in Directory(
-      widgetPreviewScaffoldTemplateDir.absolute.path,
-    ).listSync(recursive: true)) {
-      final String scaffoldPath =
-          entity.path
-              .replaceAll('.tmpl', '')
-              .split('widget_preview_scaffold/')
-              .last;
-      if (_ignoreDiffSet.contains(scaffoldPath)) {
-        continue;
-      }
-      final String resolvedScaffoldPath =
-          '${widgetPreviewScaffoldProject.absolute.path}$scaffoldPath';
-      if (entity is Directory) {
-        if (!Directory(resolvedScaffoldPath).existsSync()) {
-          stdout.writeln(
-            'ERROR: Failed to find directory at $resolvedScaffoldPath.',
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return WidgetsApp(
+      color: Colors.blue,
+      home: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+          return WidgetPreviewerWindowConstraints(
+            constraints: constraints,
+            child: child,
           );
-          updateDetected = true;
-        }
-      } else if (entity is File) {
-        final File scaffoldFile = File(resolvedScaffoldPath);
-        if (!scaffoldFile.existsSync()) {
-          stdout.writeln(
-            'ERROR: Failed to find file at $resolvedScaffoldPath.',
-          );
-          updateDetected = true;
-          continue;
-        }
-        final String templateContent = entity.readAsStringSync();
-        final String scaffoldContent = scaffoldFile.readAsStringSync();
-        if (templateContent != scaffoldContent) {
-          stdout.writeln(
-            'ERROR: The contents of $resolvedScaffoldPath do not match the contents of the template at '
-            '${entity.path}.',
-          );
-          updateDetected = true;
-        }
-      } else {
-        throw StateError(
-          'Unexpected FileSystemEntity type: ${entity.runtimeType}',
-        );
-      }
-    }
-    return updateDetected;
+        },
+      ),
+      pageRouteBuilder:
+          <T>(RouteSettings settings, WidgetBuilder builder) =>
+              PageRouteBuilder<T>(
+                settings: settings,
+                pageBuilder:
+                    (
+                      BuildContext context,
+                      Animation<double> animation,
+                      Animation<double> secondaryAnimation,
+                    ) => builder(context),
+              ),
+    );
   }
 }
