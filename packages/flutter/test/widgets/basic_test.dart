1ca558521309e45cd0869c3f5c86a3c083e785a4
Goddchen <goddchen@gmail.com>
2025-04-11 07:46:29 +0200

    test(basic): remove unnecessary import
    Signed-off-by: Goddchen <goddchen@gmail.com>

--- packages/flutter/test/widgets/basic_test.dart
+++ packages/flutter/test/widgets/basic_test.dart
@@ -15,7 +15,6 @@ import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:matcher/matcher.dart';
 
 import 'semantics_tester.dart';
 
