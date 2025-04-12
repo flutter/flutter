72f2f1a552180974ead610f50491aa1e8671ccf9
Jonah Williams <jonahwilliams@google.com>
2025-04-11 18:24:16 -0700

    [Impeller] various iOS cleanups. (#166859)
    * delete lazy drawable holder which is unused.
    * Add some docs/test to PassBindingsCache
    * Add StencilRef to PassBindingsCache - mostly to reduce redundant ops
    in the debugger.

--- engine/src/flutter/impeller/renderer/backend/metal/pass_bindings_cache_mtl.h
+++ engine/src/flutter/impeller/renderer/backend/metal/pass_bindings_cache_mtl.h
@@ -30,26 +30,56 @@ struct PassBindingsCacheMTL {
 
   PassBindingsCacheMTL(PassBindingsCacheMTL&&) = delete;
 
+  /// @brief Set the command encoder for this pass bindings cache.
+  ///
+  /// The encoder must be set before any state adjusting commands can be called.
   void SetEncoder(id<MTLRenderCommandEncoder> encoder);
 
+  /// @brief Set the render pipeline state for the current encoder.
+  ///
+  /// If this matches the previous render pipeline state, no update
+  /// is performed.
   void SetRenderPipelineState(id<MTLRenderPipelineState> pipeline);
 
+  /// @brief Set the depth and stencil state for the current encoder.
+  ///
+  /// If this matches the previous depth and stencil state, no update
+  /// is performed.
   void SetDepthStencilState(id<MTLDepthStencilState> depth_stencil);
 
+  /// @brief Set the buffer for the given shader stage, binding, and offset.
+  ///
+  /// If the buffer is already bound, only the offset is updated.
   bool SetBuffer(ShaderStage stage,
                  uint64_t index,
                  uint64_t offset,
                  id<MTLBuffer> buffer);
 
+  /// @brief Set the texture for the given stage and binding.
+  ///
+  /// If the same texture is already bound at the index for this stage, no
+  /// state updates are performed.
   bool SetTexture(ShaderStage stage, uint64_t index, id<MTLTexture> texture);
 
+  /// @brief Set the sampler for the given stage and binding.
+  ///
+  /// If the same sampler is already bound at the index for this stage, no
+  /// state updates are performed.
   bool SetSampler(ShaderStage stage,
                   uint64_t index,
                   id<MTLSamplerState> sampler);
 
+  /// @brief Set the viewport if the value is different from the current encoder
+  ///        state
   void SetViewport(const Viewport& viewport);
 
+  /// @brief Set the encoder scissor rect if the value is different from the
+  ///        current encoder state.
   void SetScissor(const IRect& scissor);
+
+  /// @brief Set the encoder's stencil reference if the value is different from
+  ///        the current encoder state.
+  void SetStencilRef(uint32_t stencil_ref);
 
  private:
   struct BufferOffsetPair {
@@ -68,6 +98,7 @@ struct PassBindingsCacheMTL {
   std::map<ShaderStage, SamplerMap> samplers_;
   std::optional<Viewport> viewport_;
   std::optional<IRect> scissor_;
+  std::optional<uint32_t> stencil_ref_;
 };
 
 }  // namespace impeller
