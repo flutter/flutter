9942c03cb84e655b71a356eae1be61c0e74ea3ef
jonahwilliams <jonahwilliams@google.com>
2025-04-11 17:39:42 -0700

    formatting.

--- engine/src/flutter/common/input/text_input_connection.h
+++ engine/src/flutter/common/input/text_input_connection.h
@@ -11,34 +11,34 @@
 namespace flutter {
 
 class TextInputConnection {
-  public:
-    TextInputConnection() = default;
+ public:
+  TextInputConnection() = default;
 
-    virtual ~TextInputConnection() {}
+  virtual ~TextInputConnection() {}
 
-    virtual std::string GetCurrentText() = 0;
+  virtual std::string GetCurrentText() = 0;
 
-    virtual void SetCurrentText(std::string_view text) = 0;
+  virtual void SetCurrentText(std::string_view text) = 0;
 
-    virtual void SetUpdateCallback(fml::closure callback) {}
+  virtual void SetUpdateCallback(fml::closure callback) {}
 
-    TextInputConnection(const TextInputConnection&) = delete;
-    TextInputConnection& operator= (const TextInputConnection&) = delete;
+  TextInputConnection(const TextInputConnection&) = delete;
+  TextInputConnection& operator=(const TextInputConnection&) = delete;
 };
 
 class TextInputConnectionFactory {
-  public:
-    TextInputConnectionFactory() = default;
+ public:
+  TextInputConnectionFactory() = default;
 
-    virtual ~TextInputConnectionFactory() {}
+  virtual ~TextInputConnectionFactory() {}
 
-    virtual std::shared_ptr<TextInputConnection> CreateTextInputConnection() = 0;
+  virtual std::shared_ptr<TextInputConnection> CreateTextInputConnection() = 0;
 
-    TextInputConnectionFactory(const TextInputConnectionFactory&) = delete;
-    TextInputConnectionFactory& operator= (const TextInputConnectionFactory&) = delete;
+  TextInputConnectionFactory(const TextInputConnectionFactory&) = delete;
+  TextInputConnectionFactory& operator=(const TextInputConnectionFactory&) =
+      delete;
 };
 
 }  // namespace flutter
 
-#endif // FLUTTER_COMMON_INPUT_TEXT_INPUT_CONNECTION_H_
-
+#endif  // FLUTTER_COMMON_INPUT_TEXT_INPUT_CONNECTION_H_
