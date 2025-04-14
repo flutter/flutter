62ee2106917900156dfb51739f889ded5a6d69d5
Koji Wakamiya <koji.wakamiya@gmail.com>
2025-04-15 08:17:46 +0900

    Apply suggestions from code review

--- engine/src/flutter/lib/web_ui/test/engine/text_editing_test.dart
+++ engine/src/flutter/lib/web_ui/test/engine/text_editing_test.dart
@@ -1998,7 +1998,7 @@ Future<void> testMain() async {
 
     test('Do not update composingBase and composingExtent while composing Text', () {
       showKeyboard(inputType: 'text');
-      // 'text' is input as a composing target
+      // 'text' is input as a composing target.
       editingStrategy!.composingText = 'test';
       sendFrameworkMessage(
         codec.encodeMethodCall(
@@ -2010,7 +2010,7 @@ Future<void> testMain() async {
         ),
       );
 
-      // 'text' is composed as Japanese, and baseOffset and extentOffset are updated
+      // 'text' is composed as Japanese, and baseOffset and extentOffset are updated.
       final EditingState editingState = EditingState(text: 'test', baseOffset: 0, extentOffset: 1);
       editingStrategy!.setEditingState(editingState);
 
