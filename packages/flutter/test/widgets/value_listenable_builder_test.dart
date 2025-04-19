2738543263302c35d5f7af37f40b5a9fdac564fa
gnassro <gnassro@outlook.com>
2025-04-19 19:48:54 +0100

    add test

--- packages/flutter/test/widgets/value_listenable_builder_test.dart
+++ packages/flutter/test/widgets/value_listenable_builder_test.dart
@@ -115,6 +115,108 @@ void main() {
     expect(find.text('Gilfoyle'), findsNothing);
     expect(valueListenable.hasListeners, false);
   });
+
+  group('rebuildOnlyOnValueChange', () {
+    late ValueNotifier<int> parentNotifier;
+    late ValueNotifier<int> notifier;
+
+    setUp(() {
+      parentNotifier = ValueNotifier<int>(0);
+      notifier = ValueNotifier<int>(0);
+    });
+
+    tearDown(() {
+      parentNotifier.dispose();
+      notifier.dispose();
+    });
+
+    testWidgets('calls builder only on value changes when true', (WidgetTester tester) async {
+      int buildCount = 0;
+
+      Widget builder(BuildContext context, int value, Widget? child) {
+        buildCount++;
+        return Text('$value');
+      }
+
+      await tester.pumpWidget(
+        Directionality(
+          textDirection: TextDirection.ltr,
+          child: ValueListenableBuilder<int>(
+            valueListenable: parentNotifier,
+            builder: (BuildContext context, int value, Widget? child) {
+              return ValueListenableBuilder<int>(
+                valueListenable: notifier,
+                rebuildOnlyOnValueChange: true,
+                builder: builder,
+              );
+            }
+          ),
+        ),
+      );
+
+      expect(find.text('0'), findsOneWidget);
+      expect(buildCount, 1);
+
+      parentNotifier.value = 1;
+      await tester.pump();
+      expect(find.text('0'), findsOneWidget);
+      expect(buildCount, 1);
+
+      notifier.value = 1;
+      await tester.pump();
+      expect(find.text('1'), findsOneWidget);
+      expect(buildCount, 2);
+
+      parentNotifier.value = 2;
+      await tester.pump();
+      expect(find.text('1'), findsOneWidget);
+      expect(buildCount, 2);
+
+    });
+
+    testWidgets('calls builder on every build when false', (WidgetTester tester) async {
+      int buildCount = 0;
+
+      Widget builder(BuildContext context, int value, Widget? child) {
+        buildCount++;
+        return Text('$value');
+      }
+
+      await tester.pumpWidget(
+        Directionality(
+          textDirection: TextDirection.ltr,
+          child: ValueListenableBuilder<int>(
+              valueListenable: parentNotifier,
+              builder: (BuildContext context, int value, Widget? child) {
+              return ValueListenableBuilder<int>(
+                valueListenable: notifier,
+                builder: builder,
+              );
+            }
+          ),
+        ),
+      );
+
+      expect(find.text('0'), findsOneWidget);
+      expect(buildCount, 1);
+
+      parentNotifier.value = 3;
+      await tester.pump();
+      expect(find.text('0'), findsOneWidget);
+      expect(buildCount, 2);
+
+      notifier.value = 1;
+      await tester.pump();
+      expect(find.text('1'), findsOneWidget);
+      expect(buildCount, 3);
+
+      parentNotifier.value = 4;
+      await tester.pump();
+      expect(find.text('1'), findsOneWidget);
+      expect(buildCount, 4);
+
+    });
+  });
 }
 
 class SpyStringValueNotifier extends ValueNotifier<String?> {
