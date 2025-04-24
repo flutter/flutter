42fbf02324fb960136e73b7bf9eefbe8a2655168
Justin McCandless <jmccandless@google.com>
2025-04-24 15:49:32 -0700

    Unnecessary media query test

--- packages/flutter/test/widgets/media_query_test.dart
+++ packages/flutter/test/widgets/media_query_test.dart
@@ -263,67 +263,6 @@ void main() {
   );
 
   testWidgets(
-    'MediaQueryData.fromView picks up new view data (supportsShowingSystemContextMenu) when rebuilding',
-    (WidgetTester tester) async {
-      addTearDown(() async {
-        tester.platformDispatcher.clearAllTestValues();
-        tester.platformDispatcher.resetSupportsShowingSystemContextMenu();
-        // Pump one more root view to replace it with one that reads the
-        // updated reset value of supportsShowingSystemContextMenu for
-        // subsequent tests..
-        await tester.pumpWidget(
-          wrapWithView: false,
-          View(view: tester.view, child: const SizedBox.shrink()),
-        );
-      });
-
-      final List<MediaQueryData> datas1 = <MediaQueryData>[];
-
-      await tester.pumpWidget(
-        MediaQuery.fromView(
-          view: tester.view,
-          child: Builder(
-            builder: (BuildContext context) {
-              datas1.add(MediaQuery.of(context));
-              return const Placeholder();
-            },
-          ),
-        ),
-      );
-
-      expect(datas1, hasLength(1));
-      expect(datas1.first.supportsShowingSystemContextMenu, isFalse);
-
-      tester.platformDispatcher.supportsShowingSystemContextMenu = true;
-
-      final List<MediaQueryData> datas2 = <MediaQueryData>[];
-
-      // Rebuild the MediaQuery widget.
-      await tester.pumpWidget(
-        // Don't wrap with the global View so that the change to
-        // platformDispatcher is read.
-        wrapWithView: false,
-        View(
-          view: tester.view,
-          child: MediaQuery.fromView(
-            view: tester.view,
-            child: Builder(
-              builder: (BuildContext context) {
-                datas2.add(MediaQuery.of(context));
-                return const Placeholder();
-              },
-            ),
-          ),
-        ),
-      );
-
-      expect(datas1, hasLength(1));
-      expect(datas2, hasLength(1));
-      expect(datas2.first.supportsShowingSystemContextMenu, isTrue);
-    },
-  );
-
-  testWidgets(
     'MediaQuery.fromView injects a new MediaQuery with data from view, preserving platform-specific data',
     (WidgetTester tester) async {
       const MediaQueryData platformData = MediaQueryData(
