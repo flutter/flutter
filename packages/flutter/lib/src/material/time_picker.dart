c1f6533211de158d69023f28227fc1bcef49d1f3
pranavo72bex <pranav072bex@ioepc.edu.np>
2025-05-02 10:03:01 +0545

    fix: time selector separator in timepicker is not centered vertically

--- packages/flutter/lib/src/material/time_picker.dart
+++ packages/flutter/lib/src/material/time_picker.dart
@@ -518,11 +518,12 @@ class _TimeSelectorSeparator extends StatelessWidget {
       child: SizedBox(
         width: timeOfDayFormat == TimeOfDayFormat.frenchCanadian ? 36 : 24,
         height: height,
-        child: Text(
-          _timeSelectorSeparatorValue(timeOfDayFormat),
-          style: effectiveStyle,
-          textScaler: TextScaler.noScaling,
-          textAlign: TextAlign.center,
+        child: Center(
+          child: Text(
+            _timeSelectorSeparatorValue(timeOfDayFormat),
+            style: effectiveStyle,
+            textScaler: TextScaler.noScaling,
+          ),
         ),
       ),
     );
