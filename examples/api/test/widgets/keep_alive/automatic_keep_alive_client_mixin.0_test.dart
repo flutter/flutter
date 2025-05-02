be291bdd55a36fa9de968cf6f5f59825908c1616
Valentin Vignal <32538273+ValentinVignal@users.noreply.github.com>
2025-05-02 15:03:35 +0800

    Add documentation examples to `KeepAlive` `AutomaticKeepAlive` and `AutomaticKeepAliveClientMixin` (#168137)
    Part of https://github.com/flutter/flutter/issues/153860
    
    
    
    https://github.com/user-attachments/assets/8fd233da-d1b1-417a-8854-8d16c3ad195b
    
    
    
    https://github.com/user-attachments/assets/93197949-b962-483e-bb8f-02db5206674c
    
    
    
    https://github.com/user-attachments/assets/ad4f4091-6ed3-4a13-bcf3-f8572ce87481
    
    
    
    ## Pre-launch Checklist
    
    - [x] I read the [Contributor Guide] and followed the process outlined
    there for submitting PRs.
    - [x] I read the [Tree Hygiene] wiki page, which explains my
    responsibilities.
    - [x] I read and followed the [Flutter Style Guide], including [Features
    we expect every widget to implement].
    - [x] I signed the [CLA].
    - [x] I listed at least one issue that this PR fixes in the description
    above.
    - [x] I updated/added relevant documentation (doc comments with `///`).
    - [x] I added new tests to check the change I am making, or this PR is
    [test-exempt].
    - [x] I followed the [breaking change policy] and added [Data Driven
    Fixes] where supported.
    - [x] All existing and new tests are passing.
    
    If you need help, consider asking for advice on the #hackers-new channel
    on [Discord].
    
    <!-- Links -->
    [Contributor Guide]:
    https://github.com/flutter/flutter/blob/main/docs/contributing/Tree-hygiene.md#overview
    [Tree Hygiene]:
    https://github.com/flutter/flutter/blob/main/docs/contributing/Tree-hygiene.md
    [test-exempt]:
    https://github.com/flutter/flutter/blob/main/docs/contributing/Tree-hygiene.md#tests
    [Flutter Style Guide]:
    https://github.com/flutter/flutter/blob/main/docs/contributing/Style-guide-for-Flutter-repo.md
    [Features we expect every widget to implement]:
    https://github.com/flutter/flutter/blob/main/docs/contributing/Style-guide-for-Flutter-repo.md#features-we-expect-every-widget-to-implement
    [CLA]: https://cla.developers.google.com/
    [flutter/tests]: https://github.com/flutter/tests
    [breaking change policy]:
    https://github.com/flutter/flutter/blob/main/docs/contributing/Tree-hygiene.md#handling-breaking-changes
    [Discord]:
    https://github.com/flutter/flutter/blob/main/docs/contributing/Chat.md
    [Data Driven Fixes]:
    https://github.com/flutter/flutter/blob/main/docs/contributing/Data-driven-Fixes.md

---
+++ examples/api/test/widgets/keep_alive/automatic_keep_alive_client_mixin.0_test.dart
@@ -0,0 +1,66 @@
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+import 'package:flutter_api_samples/widgets/keep_alive/automatic_keep_alive_client_mixin.0.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('The state is maintained when the item is scrolled out of view', (
+    WidgetTester tester,
+  ) async {
+    await tester.pumpWidget(const AutomaticKeepAliveClientMixinExampleApp());
+
+    expect(find.text('Item 0: 0'), findsOne);
+
+    await tester.tap(find.widgetWithIcon(IconButton, Icons.add).first);
+    await tester.pump();
+
+    expect(find.text('Item 0: 1'), findsOne);
+
+    // Scrolls all the way down to the bottom of the list.
+    await tester.fling(find.byType(ListView), const Offset(0, -6000), 1000);
+    await tester.pumpAndSettle();
+
+    expect(find.text('Item 99: 0'), findsOne);
+
+    // Scrolls all the way back to the top of the list.
+    await tester.fling(find.byType(ListView), const Offset(0, 6000), 1000);
+    await tester.pumpAndSettle();
+
+    expect(find.text('Item 0: 1'), findsOne, reason: 'The state of item 0 should be maintained');
+  });
+
+  testWidgets('The state is not maintained when the item is scrolled out of view', (
+    WidgetTester tester,
+  ) async {
+    await tester.pumpWidget(const AutomaticKeepAliveClientMixinExampleApp());
+
+    await tester.tap(find.byType(Switch));
+    await tester.pumpAndSettle();
+
+    expect(find.text('Item 0: 0'), findsOne);
+
+    await tester.tap(find.widgetWithIcon(IconButton, Icons.add).first);
+    await tester.pump();
+
+    expect(find.text('Item 0: 1'), findsOne);
+
+    // Scrolls all the way down to the bottom of the list.
+    await tester.fling(find.byType(ListView), const Offset(0, -6000), 1000);
+    await tester.pumpAndSettle();
+
+    expect(find.text('Item 99: 0'), findsOne);
+
+    // Scrolls all the way back to the top of the list.
+    await tester.fling(find.byType(ListView), const Offset(0, 6000), 1000);
+    await tester.pumpAndSettle();
+
+    expect(
+      find.text('Item 0: 0'),
+      findsOne,
+      reason: 'The state of item 0 should not be maintained',
+    );
+  });
+}
