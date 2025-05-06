d747fdab5c8535c7533ebb4de600bb8d0764bdcc
Jackson Gardner <jacksongardner@google.com>
2025-05-06 15:30:24 -0700

    [skwasm] Dispose underlying picture recorder when ending recording. (#168384)
    The picture recorder doesn't have an explicit disposal function, so
    instead we should dispose it when `endRecording` is called.
    
    This addresses https://github.com/flutter/flutter/issues/168190

--- engine/src/flutter/lib/web_ui/lib/src/engine/canvaskit/picture_recorder.dart
+++ engine/src/flutter/lib/web_ui/lib/src/engine/canvaskit/picture_recorder.dart
@@ -4,13 +4,10 @@
 
 import 'dart:typed_data';
 
+import 'package:ui/src/engine.dart';
 import 'package:ui/ui.dart' as ui;
 
-import 'canvas.dart';
-import 'canvaskit_api.dart';
-import 'picture.dart';
-
-class CkPictureRecorder implements ui.PictureRecorder {
+class CkPictureRecorder implements ScenePictureRecorder {
   SkPictureRecorder? _skRecorder;
   CkCanvas? _recordingCanvas;
 
@@ -43,4 +40,7 @@ class CkPictureRecorder implements ui.PictureRecorder {
 
   @override
   bool get isRecording => _skRecorder != null;
+
+  @override
+  bool get debugDisposed => _skRecorder == null;
 }
