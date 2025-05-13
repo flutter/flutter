f3a180656274819d6b5dfaba392aa1fd6198fcf9
ash2moon <muhatashim@google.com>
2025-05-13 10:47:09 -0700

    Use live region in error text input decorator for Android (#165531)
    Resolves partly https://github.com/flutter/flutter/issues/165510
    
    
    **Context:** This issue originates from
    https://github.com/flutter/flutter/issues/99715, where it was reported
    that `liveRegion` alone was insufficient for announcing form validation
    errors. While `liveRegion` announces the first error encountered,
    subsequent submissions with the same error message on Android would not
    trigger a re-announcement.
    
    **Original Solution:** Pull request
    https://github.com/flutter/flutter/pull/123373 addressed this by
    implementing the `announce` event to ensure error messages were
    consistently announced, even for repeated submissions.
    
    **Native Android Behavior (Jetpack Compose):** In native Android
    development using Jetpack Compose, setting the `isError` property of a
    `TextField` to `true` triggers Talkback to announce "Error invalid
    input." This announcement occurs *only* on the initial change to the
    error state. Subsequent errors, even if the `isError` property remains
    `true`, are not re-announced. This behavior closely mirrors the
    functionality of `liveRegion`, with the key difference being that
    `liveRegion` also announces the specific error text, in addition to the
    general error state. Testing in a native Jetpack Compose application
    confirms this behavior and provides a valuable comparison point against
    the current Flutter form example.
    
    **Suggested Action:** **Fork** the behavior in
    https://github.com/flutter/flutter/pull/123373. Reinstate the use of
    `liveRegion` for error announcements within `widgets/Form` for Android
    and keep other platforms the same.
    
    ## Pre-launch Checklist
    
    - [x] I read the [Contributor Guide] and followed the process outlined
    there for submitting PRs.
    - [x] I read the [Tree Hygiene] wiki page, which explains my
    responsibilities.
    - [x] I read and followed the [Flutter Style Guide], including [Features
    we expect every widget to implement].
    - [x] I signed the [CLA].
    - [x] I listed at least one issue that this PR fixes in the description
    above.
    - [x] I updated/added relevant documentation (doc comments with `///`).
    - [x] I added new tests to check the change I am making, or this PR is
    [test-exempt].
    - [x] I followed the [breaking change policy] and added [Data Driven
    Fixes] where supported.
    - [x] All existing and new tests are passing.
    
    If you need help, consider asking for advice on the #hackers-new channel
    on [Discord].
    
    <!-- Links -->
    [Contributor Guide]:
    https://github.com/flutter/flutter/blob/main/docs/contributing/Tree-hygiene.md#overview
    [Tree Hygiene]:
    https://github.com/flutter/flutter/blob/main/docs/contributing/Tree-hygiene.md
    [test-exempt]:
    https://github.com/flutter/flutter/blob/main/docs/contributing/Tree-hygiene.md#tests
    [Flutter Style Guide]:
    https://github.com/flutter/flutter/blob/main/docs/contributing/Style-guide-for-Flutter-repo.md
    [Features we expect every widget to implement]:
    https://github.com/flutter/flutter/blob/main/docs/contributing/Style-guide-for-Flutter-repo.md#features-we-expect-every-widget-to-implement
    [CLA]: https://cla.developers.google.com/
    [flutter/tests]: https://github.com/flutter/tests
    [breaking change policy]:
    https://github.com/flutter/flutter/blob/main/docs/contributing/Tree-hygiene.md#handling-breaking-changes
    [Discord]:
    https://github.com/flutter/flutter/blob/main/docs/contributing/Chat.md
    [Data Driven Fixes]:
    https://github.com/flutter/flutter/blob/main/docs/contributing/Data-driven-Fixes.md
    
    ---------
    
    Co-authored-by: Reid Baker <1063596+reidbaker@users.noreply.github.com>

--- engine/src/flutter/lib/ui/window.dart
+++ engine/src/flutter/lib/ui/window.dart
@@ -931,6 +931,7 @@ class AccessibilityFeatures {
   static const int _kReduceMotionIndex = 1 << 4;
   static const int _kHighContrastIndex = 1 << 5;
   static const int _kOnOffSwitchLabelsIndex = 1 << 6;
+  static const int _kNoAnnounceIndex = 1 << 7;
 
   // A bitfield which represents each enabled feature.
   final int _index;
@@ -968,6 +969,20 @@ class AccessibilityFeatures {
   /// Only supported on iOS.
   bool get onOffSwitchLabels => _kOnOffSwitchLabelsIndex & _index != 0;
 
+  /// Whether accessibility announcements (like [SemanticsService.announce])
+  /// are allowed on the current platform.
+  ///
+  /// Returns `false` on Android, where platform announcements are deprecated
+  /// by the underlying platform.
+  ///
+  /// Returns `true` on all other platforms (iOS, web, desktop) where such
+  /// announcements are generally supported without discouragement.
+  ///
+  /// Use this flag to conditionally avoid making announcements on Android.
+  // This is an inverted check on _index since there are many more platforms
+  // that support announce whereas don't.
+  bool get announce => _kNoAnnounceIndex & _index == 0;
+
   @override
   String toString() {
     final List<String> features = <String>[];
@@ -991,6 +1006,9 @@ class AccessibilityFeatures {
     }
     if (onOffSwitchLabels) {
       features.add('onOffSwitchLabels');
+    }
+    if (announce) {
+      features.add('announce');
     }
     return 'AccessibilityFeatures$features';
   }
