75f8872932a9f194eebaa693a18921b7a2217f04
Nicholas Shahan <nshahan@google.com>
2025-05-13 15:49:46 -0700

    exclude release and profile runs

--- packages/flutter_tools/lib/src/runner/flutter_command.dart
+++ packages/flutter_tools/lib/src/runner/flutter_command.dart
@@ -1356,7 +1356,8 @@ abstract class FlutterCommand extends Command<void> {
 
     // TODO(natebiggs): Delete this when new DDC module system is the default.
     if (argParser.options.containsKey(FlutterOptions.kWebExperimentalHotReload) &&
-        boolArg(FlutterOptions.kWebExperimentalHotReload)) {
+        boolArg(FlutterOptions.kWebExperimentalHotReload) &&
+        getBuildMode() == BuildMode.debug) {
       extraFrontEndOptions.addAll(<String>['--dartdevc-canary', '--dartdevc-module-format=ddc']);
     }
 
