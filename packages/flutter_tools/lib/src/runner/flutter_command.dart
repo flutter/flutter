e4af50adf00f6cc6487e9fe08d9ccf482e0827f4
Nicholas Shahan <nshahan@google.com>
2025-05-14 11:25:43 -0700

    exclude wasm explicitly

--- packages/flutter_tools/lib/src/runner/flutter_command.dart
+++ packages/flutter_tools/lib/src/runner/flutter_command.dart
@@ -1354,7 +1354,12 @@ abstract class FlutterCommand extends Command<void> {
     // TODO(natebiggs): Delete this when new DDC module system is the default.
     if (argParser.options.containsKey(FlutterOptions.kWebExperimentalHotReload) &&
         boolArg(FlutterOptions.kWebExperimentalHotReload) &&
-        getBuildMode() == BuildMode.debug) {
+        getBuildMode() == BuildMode.debug &&
+        !(argParser.options.containsKey(FlutterOptions.kAnalyzeSize) &&
+            boolArg(FlutterOptions.kWebWasmFlag))) {
+      // These flags are only valid to be passed when we are compiling with DDC.
+      // There isn't a direct flag for this compiler so we try to deduce it from
+      // the debug mode, and not WASM.
       extraFrontEndOptions.addAll(<String>['--dartdevc-canary', '--dartdevc-module-format=ddc']);
     }
 
