6769c7040027a34ccdf427538bfaccb7d5cf7c70
ValentinVignal <valentin.vignal.dev@outlook.fr>
2025-05-14 21:42:37 +0800

    Add doc imports

--- packages/flutter/lib/src/widgets/scroll_delegate.dart
+++ packages/flutter/lib/src/widgets/scroll_delegate.dart
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+/// @docImport 'automatic_keep_alive.dart';
 /// @docImport 'scroll_view.dart';
 /// @docImport 'sliver.dart';
 /// @docImport 'spacer.dart';
