441c042ffdbb909e8ce5af2522a34086553248a4
Jonah Williams <jonahwilliams@google.com>
2025-05-14 18:53:18 -0700

    [display_list] paint cleanup. (#168082)
    Reduce the number of places where we convert flutter paint to DLpaint
    and remove unused tonic overrides. Also does a pass on dl_paint/builder
    to remove places we accidentally incremented/decrement shared_ptr ref
    counts

--- engine/src/flutter/lib/ui/painting/paint_unittests.cc
+++ engine/src/flutter/lib/ui/painting/paint_unittests.cc
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "flutter/display_list/dl_op_flags.h"
 #include "flutter/lib/ui/painting/paint.h"
 #include "flutter/shell/common/shell_test.h"
 #include "flutter/shell/common/thread_host.h"
@@ -21,7 +22,9 @@ TEST_F(ShellTest, ConvertPaintToDlPaint) {
         Dart_GetField(dart_paint, tonic::ToDart("_objects"));
     Dart_Handle paint_data = Dart_GetField(dart_paint, tonic::ToDart("_data"));
     Paint ui_paint(paint_objects, paint_data);
-    ui_paint.toDlPaint(dl_paint, DlTileMode::kClamp);
+
+    ui_paint.paint(dl_paint, DisplayListOpFlags::kDrawRectFlags,
+                   DlTileMode::kClamp);
     message_latch->Signal();
   };
 
@@ -49,13 +52,22 @@ TEST_F(ShellTest, ConvertPaintToDlPaint) {
   message_latch->Wait();
   DestroyShell(std::move(shell), task_runners);
 
-  ASSERT_EQ(dl_paint.getBlendMode(), DlBlendMode::kModulate);
-  ASSERT_EQ(static_cast<uint32_t>(dl_paint.getColor().argb()), 0x11223344u);
-  ASSERT_EQ(*dl_paint.getColorFilter(),
-            *DlColorFilter::MakeBlend(DlColor(0x55667788), DlBlendMode::kXor));
-  ASSERT_EQ(*dl_paint.getMaskFilter(),
-            DlBlurMaskFilter(DlBlurStyle::kInner, 0.75));
-  ASSERT_EQ(dl_paint.getDrawStyle(), DlDrawStyle::kStroke);
+  EXPECT_EQ(dl_paint.getBlendMode(), DlBlendMode::kModulate);
+  EXPECT_EQ(static_cast<uint32_t>(dl_paint.getColor().argb()), 0x11223344u);
+  if (dl_paint.getColorFilter()) {
+    std::shared_ptr<const DlColorFilter> expected_filter =
+        DlColorFilter::MakeBlend(DlColor(0x55667788), DlBlendMode::kXor);
+    EXPECT_EQ(*dl_paint.getColorFilter(), *expected_filter);
+  } else {
+    FAIL() << "color filter was nullptr";
+  }
+  if (dl_paint.getMaskFilter()) {
+    EXPECT_EQ(*dl_paint.getMaskFilter(),
+              DlBlurMaskFilter(DlBlurStyle::kInner, 0.75));
+  } else {
+    FAIL() << "mask filter was nullptr";
+  }
+  EXPECT_EQ(dl_paint.getDrawStyle(), DlDrawStyle::kStroke);
 }
 
 }  // namespace testing
