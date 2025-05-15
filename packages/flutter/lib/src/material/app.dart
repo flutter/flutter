e97c587eaeb0414d544c13e60d8fa45a3bbf2e5b
Elliott Brooks <21270878+elliette@users.noreply.github.com>
2025-05-15 10:24:23 -0700

    Update comment

--- packages/flutter/lib/src/material/app.dart
+++ packages/flutter/lib/src/material/app.dart
@@ -970,8 +970,8 @@ class _MaterialAppState extends State<MaterialApp> {
     return _MaterialInspectorButton.toggle(
       onPressed: onPressed,
       semanticsLabel: semanticsLabel,
-      // This icon is also used for the Cupertino-styled button and for DevTools.
-      // It should be updated in all 3 places if changed.
+      // This unicode icon is also used for the Cupertino-styled button and for
+      // DevTools. It should be updated in all 3 places if changed.
       icon: const IconData(0x1F74A),
       isDarkTheme: _isDarkTheme(context),
       toggledOn: selectionOnTapEnabled,
