0fe5b617de107d1b5efd9a29c8f0d10d306f678d
Bruno Leroux <bruno.leroux@gmail.com>
2025-05-16 22:55:01 +0200

    Normalize input decoration theme

--- dev/benchmarks/macrobenchmarks/lib/src/web/material3.dart
+++ dev/benchmarks/macrobenchmarks/lib/src/web/material3.dart
@@ -1998,7 +1998,7 @@ class _MenusState extends State<Menus> {
                 label: const Text('Color'),
                 enableFilter: true,
                 dropdownMenuEntries: colorEntries,
-                inputDecorationTheme: const InputDecorationTheme(filled: true),
+                inputDecorationTheme: const InputDecorationThemeData(filled: true),
                 onSelected: (ColorLabel? color) {
                   setState(() {
                     selectedColor = color;
