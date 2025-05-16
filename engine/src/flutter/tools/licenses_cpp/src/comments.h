ffb41d7f2282a58ee1a9af2a962a195fc85fe678
Aaron Clarke <aaclarke@google.com>
2025-05-16 15:50:59 -0700

    added comments unit test

--- engine/src/flutter/tools/licenses_cpp/src/comments.h
+++ engine/src/flutter/tools/licenses_cpp/src/comments.h
@@ -6,7 +6,9 @@
 #define FLUTTER_TOOLS_LICENSES_CPP_SRC_COMMENTS_H_
 
 #include <cstdlib>
+#include <functional>
 
-void lex(const char* buffer, size_t size);
+
+void lex(const char* buffer, size_t size, std::function<void(const char*)> callback);
 
 #endif  // FLUTTER_TOOLS_LICENSES_CPP_SRC_LICENSE_CHECKER_H_
