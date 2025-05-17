f6841a8899c62657005d60a76fc3666c5df1dd97
Matej Knopp <matej.knopp@gmail.com>
2025-05-17 18:27:12 +0200

    [flutter_tools] Add --no-codesign support for macOS build

--- packages/flutter_tools/test/commands.shard/hermetic/build_macos_test.dart
+++ packages/flutter_tools/test/commands.shard/hermetic/build_macos_test.dart
@@ -1017,4 +1017,57 @@ STDERR STUFF
       OperatingSystemUtils: () => FakeOperatingSystemUtils(hostPlatform: HostPlatform.darwin_x64),
     },
   );
+
+  testUsingContext(
+    'macos build --no-codesign skips codesigning',
+    () async {
+      final BuildCommand command = BuildCommand(
+        androidSdk: FakeAndroidSdk(),
+        buildSystem: TestBuildSystem.all(BuildResult(success: true)),
+        logger: logger,
+        fileSystem: fileSystem,
+        osUtils: FakeOperatingSystemUtils(),
+      );
+      fakeProcessManager.addCommands(<FakeCommand>[
+        const FakeCommand(
+          command: <String>[
+            '/usr/bin/env',
+            'xcrun',
+            'xcodebuild',
+            '-workspace',
+            '/macos/Runner.xcworkspace',
+            '-configuration',
+            'Release',
+            '-scheme',
+            'Runner',
+            '-derivedDataPath',
+            '/build/macos',
+            '-destination',
+            'platform=macOS',
+            'OBJROOT=/build/macos/Build/Intermediates.noindex',
+            'SYMROOT=/build/macos/Build/Products',
+            '-quiet',
+            'COMPILER_INDEX_STORE_ENABLE=NO',
+            'CODE_SIGNING_ALLOWED=NO',
+            'CODE_SIGNING_REQUIRED=NO',
+            'CODE_SIGNING_IDENTITY=""',
+          ],
+        ),
+      ]);
+      createMinimalMockProjectFiles();
+
+      await createTestCommandRunner(
+        command,
+      ).run(const <String>['build', 'macos', '--no-pub', '--no-codesign']);
+      expect(fakeProcessManager, hasNoRemainingExpectations);
+      expect(logger.statusText, contains('Codesigning disabled with --no-codesign'));
+    },
+    overrides: <Type, Generator>{
+      FileSystem: () => fileSystem,
+      Logger: () => logger,
+      ProcessManager: () => fakeProcessManager,
+      Pub: FakePubWithPrimedDeps.new,
+      Platform: () => macosPlatform,
+    },
+  );
 }
