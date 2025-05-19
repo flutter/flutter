3661509b7371b83e5fafcd6b019f29396ea08296
Aaron Clarke <aaclarke@google.com>
2025-05-19 11:15:10 -0700

    got temp directories made for tests

--- engine/src/flutter/tools/licenses_cpp/src/license_checker_unittests.cc
+++ engine/src/flutter/tools/licenses_cpp/src/license_checker_unittests.cc
@@ -1,9 +1,56 @@
 // Copyright 2013 The Flutter Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+#include <filesystem>
 #include "flutter/tools/licenses_cpp/src/license_checker.h"
+#include "flutter/third_party/abseil-cpp/absl/status/statusor.h"
 #include "gtest/gtest.h"
 
-TEST(LicenseCheckerTest, Simple) {
-  EXPECT_TRUE(true);
+namespace fs = std::filesystem;
+
+class LicenseCheckerTest : public testing::Test {
+ public:
+  void SetUp() override {
+    std::error_code err;
+    temp_dir_base_ = fs::temp_directory_path(err);
+    ASSERT_FALSE(err);
+  }
+
+  void TearDown() override {
+    if (should_delete_temp_dir_) {
+      fs::remove_all(temp_dir_);
+    }
+  }
+
+  absl::StatusOr<fs::path> MakeTempDir() {
+    static std::atomic<int32_t> count = 0;
+    std::stringstream ss;
+    ss << "LicenseCheckerTest_" << std::time(nullptr) << "_" << count.fetch_add(1);
+    temp_dir_= temp_dir_base_ / ss.str();
+    std::error_code err;
+    fs::create_directory(temp_dir_, err);
+    if (!err) {
+      should_delete_temp_dir_ = true;
+    } else {
+      return absl::InternalError("can't make temp dir");
+    }
+
+    return temp_dir_;
+  }
+
+ private:
+  fs::path temp_dir_base_;
+  fs::path temp_dir_;
+  bool should_delete_temp_dir_;
+};
+
+TEST_F(LicenseCheckerTest, Simple) {
+  absl::StatusOr<fs::path> temp_path = MakeTempDir();
+  ASSERT_TRUE(temp_path.ok());
+
+  fs::current_path(*temp_path);
+  ASSERT_EQ(std::system("git init"), 0);
+  ASSERT_EQ(std::system("echo \"Hello world!\" > main.cc"), 0);
+  ASSERT_EQ(std::system("git add main.cc"), 0);
+  ASSERT_EQ(std::system("git commit -m \"test\""), 0);
 }
