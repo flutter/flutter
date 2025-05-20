b3e3cba5ae86f8b4a9d7240debabe7e5740cb879
Aaron Clarke <aaclarke@google.com>
2025-05-20 12:58:04 -0700

    switched the catalog match to return kNotFound

--- engine/src/flutter/tools/licenses_cpp/src/catalog.h
+++ engine/src/flutter/tools/licenses_cpp/src/catalog.h
@@ -10,21 +10,34 @@
 #include "flutter/third_party/re2/re2/re2.h"
 #include "flutter/third_party/re2/re2/set.h"
 
+#include <optional>
+
+/// A storage of licenses that can be matched against.
+/// The in memory representation of the `data/headers` and `data/licenses`
+/// directories.  This represents a 2 tiered search, first the sector is used
+/// to determine what matcher should be used, then a match is performend on
+/// that. This approach was chosen to minimize the size of the RE2::Set.
 class Catalog {
  public:
   static absl::StatusOr<Catalog> Open(std::string_view data_dir);
+
   static absl::StatusOr<Catalog> Make(
-      std::vector<std::pair<std::string_view, std::string_view>> entries);
-  absl::StatusOr<bool> HasMatch(std::string_view query);
+      std::vector<std::vector<std::string_view>> entries);
+
+  /// @brief Tries to identify a match for the `query` across the `Catalog`.
+  /// @param query The text that will be matched against. @return
+  /// absl::StatusCode::kNotFound when a match can't be found.
+  /// absl::StatusCode::kInvalidArgument if more than one match comes up from
+  /// the selector.
+  absl::StatusOr<std::string> FindMatch(std::string_view query);
 
  private:
   explicit Catalog(RE2::Set selector,
                    std::vector<std::unique_ptr<RE2>> matchers,
-                   std::vector<std::string> selector_pieces);
+                   std::vector<std::string> names);
   RE2::Set selector_;
   std::vector<std::unique_ptr<RE2>> matchers_;
-  /// Storage of the parts of `selector_` for debugging purposes.
-  std::vector<std::string> selector_pieces_;
+  std::vector<std::string> names_;
 };
 
 #endif  // FLUTTER_TOOLS_LICENSES_CPP_SRC_CATALOG_H_
