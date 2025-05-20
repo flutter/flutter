2b82d93dede92e11662c2d63e188ab771b787df6
Aaron Clarke <aaclarke@google.com>
2025-05-20 12:22:30 -0700

    got catalog working

--- engine/src/flutter/tools/licenses_cpp/src/catalog_unittests.cc
+++ engine/src/flutter/tools/licenses_cpp/src/catalog_unittests.cc
@@ -5,11 +5,22 @@
 #include "gtest/gtest.h"
 
 TEST(CatalogTest, Simple) {
-  Catalog::Entries entries;
-  entries["foo"] = std::make_unique<RE2>("(.*foo.*)");
-  Catalog catalog(std::make_unique<RE2>("(.*foo.*)"), std::move(entries));
-
-  absl::StatusOr<bool> has_match = catalog.HasMatch("foo");
+  absl::StatusOr<Catalog> catalog = Catalog::Make({{".*foo.*", ".*foo.*"}});
+  ASSERT_TRUE(catalog.ok());
+  absl::StatusOr<bool> has_match = catalog->HasMatch("foo");
   ASSERT_TRUE(has_match.ok());
   ASSERT_TRUE(*has_match);
+}
+
+TEST(CatalogTest, MultipleMatch) {
+  absl::StatusOr<Catalog> catalog =
+      Catalog::Make({{".*foo.*", ""}, {".*oo.*", ""}});
+  ASSERT_TRUE(catalog.ok());
+  absl::StatusOr<bool> has_match = catalog->HasMatch("foo");
+  ASSERT_FALSE(has_match.ok());
+  ASSERT_TRUE(RE2::PartialMatch(has_match.status().message(),
+                                "Multiple unique matches found"))
+      << has_match.status().message();
+  ASSERT_TRUE(RE2::PartialMatch(has_match.status().message(), "foo"))
+      << has_match.status().message();
 }
