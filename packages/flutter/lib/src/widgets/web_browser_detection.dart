7de9fbc6e75d4a8031781cdf5dd6512891074e5b
Renzo Olivares <roliv@google.com>
2025-05-20 13:37:05 -0700

    Address reviewer comments

--- packages/flutter/lib/src/widgets/web_browser_detection.dart
+++ packages/flutter/lib/src/widgets/web_browser_detection.dart
@@ -2,4 +2,4 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-export '_web_browser_detection_io.dart' if (dart.library.js_util) '_web_browser_detection.dart';
+export '_web_browser_detection_io.dart' if (dart.library.js_util) '_web_browser_detection_web.dart';
