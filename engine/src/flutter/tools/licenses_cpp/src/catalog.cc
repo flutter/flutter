d7408b7009241d8a122f57ca2b2eff16c998ab89
Aaron Clarke <aaclarke@google.com>
2025-05-20 12:22:30 -0700

    got catalog working

--- engine/src/flutter/tools/licenses_cpp/src/catalog.cc
+++ engine/src/flutter/tools/licenses_cpp/src/catalog.cc
@@ -8,31 +8,54 @@ absl::StatusOr<Catalog> Catalog::Open(std::string_view data_dir) {
   return absl::UnimplementedError("");
 }
 
-Catalog::Catalog(std::unique_ptr<RE2> selector, Catalog::Entries entries)
-    : selector_(std::move(selector)), entries_(std::move(entries)) {}
+absl::StatusOr<Catalog> Catalog::Make(
+    std::vector<std::pair<std::string_view, std::string_view>> entries) {
+  RE2::Set selector(RE2::Options(), RE2::Anchor::UNANCHORED);
+  std::vector<std::unique_ptr<RE2>> matchers;
+  std::vector<std::string> selector_pieces;
+
+  for (const std::pair<std::string_view, std::string_view>& entry : entries) {
+    std::string err;
+    int idx = selector.Add(entry.first, &err);
+    selector_pieces.push_back(std::string(entry.first));
+    if (idx < 0) {
+      return absl::InvalidArgumentError(
+          absl::StrCat("Unable to add set entry: ", entry.first, " ", err));
+    }
+    matchers.push_back(std::make_unique<RE2>(entry.second));
+  }
+
+  bool did_compile = selector.Compile();
+  if (!did_compile) {
+    return absl::OutOfRangeError("RE2::Set ran out of memory.");
+  }
+  return Catalog(std::move(selector), std::move(matchers),
+                 std::move(selector_pieces));
+}
+
+Catalog::Catalog(RE2::Set selector,
+                 std::vector<std::unique_ptr<RE2>> matchers,
+                 std::vector<std::string> selector_pieces)
+    : selector_(std::move(selector)),
+      matchers_(std::move(matchers)),
+      selector_pieces_(std::move(selector_pieces)) {}
 
 absl::StatusOr<bool> Catalog::HasMatch(std::string_view query) {
-  std::vector<std::string> results;
-  std::string result;
-  re2::StringPiece selector_query(query);
-  while (RE2::FindAndConsume(&selector_query, *selector_, &result)) {
-    results.push_back(result);
+  std::vector<int> selector_results;
+  if (!selector_.Match(query, &selector_results)) {
+    return false;
   }
-  if (results.size() > 1) {
+  if (selector_results.size() > 1) {
     std::stringstream ss;
     ss << "Multiple unique matches found:" << std::endl;
-    for (const std::string& entry : results) {
-      ss << "  " << entry;
+    for (int idx : selector_results) {
+      ss << "  " << selector_pieces_[idx] << std::endl;
     }
     return absl::InternalError(ss.str());
   }
-  if (results.size() <= 0) {
+  if (selector_results.size() <= 0) {
     return false;
   }
 
-  Entries::iterator it = entries_.find(results[0]);
-  if (it == entries_.end()) {
-    return absl::InternalError("No matching entry for selector result.");
-  }
-  return RE2::FullMatch(query, *it->second);
+  return RE2::FullMatch(query, *matchers_[selector_results[0]]);
 }
--- engine/src/flutter/tools/licenses_cpp/src/catalog.h
+++ engine/src/flutter/tools/licenses_cpp/src/catalog.h
@@ -8,18 +8,23 @@
 #include "flutter/third_party/abseil-cpp/absl/container/flat_hash_map.h"
 #include "flutter/third_party/abseil-cpp/absl/status/statusor.h"
 #include "flutter/third_party/re2/re2/re2.h"
+#include "flutter/third_party/re2/re2/set.h"
 
 class Catalog {
  public:
-  using Entries = absl::flat_hash_map<std::string, std::unique_ptr<RE2>>;
-
   static absl::StatusOr<Catalog> Open(std::string_view data_dir);
-  Catalog(std::unique_ptr<RE2> selector, Entries entries);
+  static absl::StatusOr<Catalog> Make(
+      std::vector<std::pair<std::string_view, std::string_view>> entries);
   absl::StatusOr<bool> HasMatch(std::string_view query);
 
  private:
-  std::unique_ptr<RE2> selector_;
-  Entries entries_;
+  explicit Catalog(RE2::Set selector,
+                   std::vector<std::unique_ptr<RE2>> matchers,
+                   std::vector<std::string> selector_pieces);
+  RE2::Set selector_;
+  std::vector<std::unique_ptr<RE2>> matchers_;
+  /// Storage of the parts of `selector_` for debugging purposes.
+  std::vector<std::string> selector_pieces_;
 };
 
 #endif  // FLUTTER_TOOLS_LICENSES_CPP_SRC_CATALOG_H_
