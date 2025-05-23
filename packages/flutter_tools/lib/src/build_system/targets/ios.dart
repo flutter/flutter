6c41322529c12f9e2a0f15cc5ac26f181a1cac9e
Victoria Ashworth <vashworth@google.com>
2025-05-23 17:08:05 -0500

    Validate build configuration during Xcode build phase

--- packages/flutter_tools/lib/src/build_system/targets/ios.dart
+++ packages/flutter_tools/lib/src/build_system/targets/ios.dart
@@ -484,37 +484,6 @@ class DebugIosLLDBInit extends IosLLDBInit {
   BuildMode get buildMode => BuildMode.debug;
 }
 
-class CheckDevDependenciesIos extends CheckDevDependencies {
-  const CheckDevDependenciesIos();
-
-  @override
-  String get name => 'check_dev_dependencies_ios';
-
-  @override
-  List<Source> get inputs {
-    return <Source>[
-      ...super.inputs,
-      const Source.pattern(
-        '{FLUTTER_ROOT}/packages/flutter_tools/lib/src/build_system/targets/ios.dart',
-      ),
-
-      // The generated Xcode properties file contains
-      // the FLUTTER_DEV_DEPENDENCIES_ENABLED configuration.
-      // This target should re-run whenever that value changes.
-      Source.fromProject((FlutterProject project) => project.ios.generatedXcodePropertiesFile),
-    ];
-  }
-
-  @override
-  String get debugBuildCommand => 'flutter build ios --config-only --debug';
-
-  @override
-  String get profileBuildCommand => 'flutter build ios --config-only --profile';
-
-  @override
-  String get releaseBuildCommand => 'flutter build ios --config-only --release';
-}
-
 /// The base class for all iOS bundle targets.
 ///
 /// This is responsible for setting up the basic App.framework structure, including:
@@ -667,7 +636,6 @@ class DebugIosApplicationBundle extends IosAssetBundle {
 
   @override
   List<Target> get dependencies => <Target>[
-    const CheckDevDependenciesIos(),
     const DebugUniversalFramework(),
     const DebugIosLLDBInit(),
     ...super.dependencies,
@@ -700,7 +668,6 @@ class ProfileIosApplicationBundle extends _IosAssetBundleWithDSYM {
 
   @override
   List<Target> get dependencies => const <Target>[
-    CheckDevDependenciesIos(),
     AotAssemblyProfile(),
     InstallCodeAssets(),
   ];
@@ -715,7 +682,6 @@ class ReleaseIosApplicationBundle extends _IosAssetBundleWithDSYM {
 
   @override
   List<Target> get dependencies => const <Target>[
-    CheckDevDependenciesIos(),
     AotAssemblyRelease(),
     InstallCodeAssets(),
   ];
