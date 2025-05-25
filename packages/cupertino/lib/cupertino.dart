abe793ea2aeeed7d2f6f5967354de66b797fca65
rkishan516 <rkishan516@gmail.com>
2025-05-25 17:15:52 +0530

    Feat: Add material and cupertino packages

---
+++ packages/cupertino/lib/cupertino.dart
@@ -0,0 +1,25 @@
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/// Flutter widgets implementing the current iOS design language.
+///
+/// To use, import `package:cupertino/cupertino.dart`.
+///
+/// This library is designed for apps that run on iOS. For apps that may also
+/// run on other operating systems, we encourage use of other widgets, for
+/// example the [Material
+/// Design](https://docs.flutter.dev/ui/widgets/material) set.
+///
+/// {@youtube 560 315 https://www.youtube.com/watch?v=3PdUaidHc-E}
+///
+/// See also:
+///
+///  * [flutter.dev/widgets/cupertino](https://docs.flutter.dev/ui/widgets/cupertino)
+///    for a catalog of all Cupertino widgets.
+///  * [flutter.dev/widgets](https://docs.flutter.dev/ui/widgets)
+///    for a catalog of commonly-used Flutter widgets.
+
+library cupertino;
+
+export 'package:flutter/widgets.dart';
---
+++ packages/material/lib/material.dart
@@ -0,0 +1,19 @@
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/// Flutter widgets implementing Material Design.
+///
+/// To use, import `package:material/material.dart`.
+///
+/// {@youtube 560 315 https://www.youtube.com/watch?v=DL0Ix1lnC4w}
+///
+/// See also:
+///
+///  * [docs.flutter.dev/ui/widgets/material](https://docs.flutter.dev/ui/widgets/material)
+///    for a catalog of commonly-used Material component widgets.
+///  * [m3.material.io](https://m3.material.io/) for the Material 3 specification
+///  * [m2.material.io](https://m2.material.io/) for the Material 2 specification
+library material;
+
+export 'package:flutter/widgets.dart';
