412025f5be8ca7d5d51b500363262cec41914f34
hangyu <jhy03261997@gmail.com>
2025-05-27 16:00:17 -0700

    fix some tests
    Update flutter_platform_node_delegate_unittests.cc
    
    update test
    
    fix tests

--- engine/src/flutter/shell/platform/common/flutter_platform_node_delegate_unittests.cc
+++ engine/src/flutter/shell/platform/common/flutter_platform_node_delegate_unittests.cc
@@ -19,14 +19,17 @@ TEST(FlutterPlatformNodeDelegateTest, NodeDelegateHasUniqueId) {
   // Add node 0: root.
   FlutterSemanticsNode2 node0{sizeof(FlutterSemanticsNode2), 0};
   std::vector<int32_t> node0_children{1};
+  FlutterSemanticsFlags emptyFlags = FlutterSemanticsFlags{};
   node0.child_count = node0_children.size();
   node0.children_in_traversal_order = node0_children.data();
   node0.children_in_hit_test_order = node0_children.data();
+  node0.flags2 = &emptyFlags;
 
   // Add node 1: text child of node 0.
   FlutterSemanticsNode2 node1{sizeof(FlutterSemanticsNode2), 1};
   node1.label = "prefecture";
   node1.value = "Kyoto";
+  node1.flags2 = &emptyFlags;
 
   bridge->AddFlutterSemanticsNodeUpdate(node0);
   bridge->AddFlutterSemanticsNodeUpdate(node1);
@@ -114,6 +117,7 @@ TEST(FlutterPlatformNodeDelegateTest, canGetAXNode) {
 TEST(FlutterPlatformNodeDelegateTest, canCalculateBoundsCorrectly) {
   std::shared_ptr<TestAccessibilityBridge> bridge =
       std::make_shared<TestAccessibilityBridge>();
+  FlutterSemanticsFlags flags = FlutterSemanticsFlags{0};
   FlutterSemanticsNode2 root;
   root.id = 0;
   root.label = "root";
@@ -123,6 +127,7 @@ TEST(FlutterPlatformNodeDelegateTest, canCalculateBoundsCorrectly) {
   root.decreased_value = "";
   root.tooltip = "";
   root.child_count = 1;
+  root.flags2 = &flags;
   int32_t children[] = {1};
   root.children_in_traversal_order = children;
   root.custom_accessibility_actions_count = 0;
@@ -139,6 +144,7 @@ TEST(FlutterPlatformNodeDelegateTest, canCalculateBoundsCorrectly) {
   child1.decreased_value = "";
   child1.tooltip = "";
   child1.child_count = 0;
+  child1.flags2 = &flags;
   child1.custom_accessibility_actions_count = 0;
   child1.rect = {0, 0, 50, 50};  // LTRB
   child1.transform = {0.5, 0, 0, 0, 0.5, 0, 0, 0, 1};
@@ -160,6 +166,7 @@ TEST(FlutterPlatformNodeDelegateTest, canCalculateBoundsCorrectly) {
 TEST(FlutterPlatformNodeDelegateTest, canCalculateOffScreenBoundsCorrectly) {
   std::shared_ptr<TestAccessibilityBridge> bridge =
       std::make_shared<TestAccessibilityBridge>();
+  FlutterSemanticsFlags flags = FlutterSemanticsFlags{0};
   FlutterSemanticsNode2 root;
   root.id = 0;
   root.label = "root";
@@ -169,6 +176,7 @@ TEST(FlutterPlatformNodeDelegateTest, canCalculateOffScreenBoundsCorrectly) {
   root.decreased_value = "";
   root.tooltip = "";
   root.child_count = 1;
+  root.flags2 = &flags;
   int32_t children[] = {1};
   root.children_in_traversal_order = children;
   root.custom_accessibility_actions_count = 0;
@@ -185,6 +193,7 @@ TEST(FlutterPlatformNodeDelegateTest, canCalculateOffScreenBoundsCorrectly) {
   child1.decreased_value = "";
   child1.tooltip = "";
   child1.child_count = 0;
+  child1.flags2 = &flags;
   child1.custom_accessibility_actions_count = 0;
   child1.rect = {90, 90, 100, 100};  // LTRB
   child1.transform = {2, 0, 0, 0, 2, 0, 0, 0, 1};
@@ -206,6 +215,7 @@ TEST(FlutterPlatformNodeDelegateTest, canCalculateOffScreenBoundsCorrectly) {
 TEST(FlutterPlatformNodeDelegateTest, canUseOwnerBridge) {
   std::shared_ptr<TestAccessibilityBridge> bridge =
       std::make_shared<TestAccessibilityBridge>();
+  FlutterSemanticsFlags flags = FlutterSemanticsFlags{0};
   FlutterSemanticsNode2 root;
   root.id = 0;
   root.label = "root";
@@ -215,6 +225,7 @@ TEST(FlutterPlatformNodeDelegateTest, canUseOwnerBridge) {
   root.decreased_value = "";
   root.tooltip = "";
   root.child_count = 1;
+  root.flags2 = &flags;
   int32_t children[] = {1};
   root.children_in_traversal_order = children;
   root.custom_accessibility_actions_count = 0;
@@ -231,6 +242,7 @@ TEST(FlutterPlatformNodeDelegateTest, canUseOwnerBridge) {
   child1.decreased_value = "";
   child1.tooltip = "";
   child1.child_count = 0;
+  child1.flags2 = &flags;
   child1.custom_accessibility_actions_count = 0;
   child1.rect = {0, 0, 50, 50};  // LTRB
   child1.transform = {0.5, 0, 0, 0, 0.5, 0, 0, 0, 1};
@@ -253,6 +265,7 @@ TEST(FlutterPlatformNodeDelegateTest, canUseOwnerBridge) {
 TEST(FlutterPlatformNodeDelegateTest, selfIsLowestPlatformAncestor) {
   std::shared_ptr<TestAccessibilityBridge> bridge =
       std::make_shared<TestAccessibilityBridge>();
+  FlutterSemanticsFlags flags = FlutterSemanticsFlags{0};
   FlutterSemanticsNode2 root;
   root.id = 0;
   root.label = "root";
@@ -262,6 +275,7 @@ TEST(FlutterPlatformNodeDelegateTest, selfIsLowestPlatformAncestor) {
   root.decreased_value = "";
   root.tooltip = "";
   root.child_count = 0;
+  root.flags2 = &flags;
   root.children_in_traversal_order = nullptr;
   root.custom_accessibility_actions_count = 0;
   bridge->AddFlutterSemanticsNodeUpdate(root);
