e0c9f5470c91262f2b8d20f4525be559d2088d0e
Gustl22 <git@reb0.org>
2025-05-27 11:25:30 +0200

    update examples

---
+++ examples/hello_world/lib/overlay_selectable_example.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+/// Flutter code sample for [Overlay].
+
+void main() {
+  // debugRepaintRainbowEnabled = true;
+  // debugPrintMarkNeedsLayoutStacks = true;
+  // debugPrintMarkNeedsPaintStacks = true;
+  // debugPrintLayouts = true;
+  runApp(const OverlayApp());
+}
+
+class OverlayApp extends StatelessWidget {
+  const OverlayApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: OverlayExample());
+  }
+}
+
+class OverlayExample extends StatefulWidget {
+  const OverlayExample({super.key});
+
+  @override
+  State<OverlayExample> createState() => _OverlayExampleState();
+}
+
+class _OverlayExampleState extends State<OverlayExample> {
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Overlay(
+        initialEntries: [
+          OverlayEntry(
+            builder: (BuildContext context) {
+              // Add a Material widget HERE to provide a context for the Ink
+              return SelectionArea(
+                child: Column(
+                  children: [
+                    // Must be at least two selectable texts
+                    Text('rootA'),
+                    Text('rootB'),
+                  ],
+                ),
+              );
+            },
+            opaque: true,
+            canSizeOverlay: true,
+            maintainState: true,
+          ),
+          OverlayEntry(
+            builder: (BuildContext context) {
+              return SelectionArea(
+                child: Column(
+                  children: [
+                    // Must be at least two selectable texts
+                    Text('subA'),
+                    Text('subB'),
+                  ],
+                ),
+              );
+            },
+            opaque: true,
+            canSizeOverlay: true,
+            maintainState: true,
+          ),
+        ],
+      ),
+    );
+  }
+}
