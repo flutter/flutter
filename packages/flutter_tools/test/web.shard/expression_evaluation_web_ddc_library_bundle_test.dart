a1ce9de6c187c9f373a355b53ffef140ebce51ac
Nicholas Shahan <nshahan@google.com>
2025-05-29 12:00:57 -0700

    [tests] Increase linux timeout and skip on others (#169627)
    With this change the web.shard test suite runs all existing tests on all
    platforms with the version of the web debug builds that is the flutter
    tools default (does not support stateful hot reload).
    
    The linux bot configuration will also run some of the tests using the
    new web debug build (supports stateful hot reload). The timeout has been
    increased for this configuration to allow the additional tests to run to
    completion.
    
    The mac and windows configurations skip all tests for the new web debug
    builds.
    
    In the future the new build output will become the flutter tools default
    and the balance of where the tests tests are run will flip until the
    ability to opt out of the new build output is removed.
    
    This change is not ideal because we are still not fully testing all
    features with the two versions of the web debug builds that are
    available to users. If given the opportunity we should add additional
    bot configurations to run all the tests with the output that supports
    stateful hot reload.
    
    Fixes: https://github.com/flutter/flutter/issues/169304
    Issue: https://github.com/flutter/flutter/issues/169634

--- packages/flutter_tools/test/web.shard/expression_evaluation_web_ddc_library_bundle_test.dart
+++ packages/flutter_tools/test/web.shard/expression_evaluation_web_ddc_library_bundle_test.dart
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+@TestOn('linux') // https://github.com/flutter/flutter/issues/169304
 @Tags(<String>['flutter-test-driver'])
-@Skip('https://github.com/flutter/flutter/issues/169304')
 library;
 
 import '../src/common.dart';
