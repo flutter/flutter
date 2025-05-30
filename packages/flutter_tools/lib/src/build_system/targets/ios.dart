39583533bd8a6294d63e3c1f603813d927b7c582
Aaron Clarke <aaclarke@google.com>
2025-05-30 15:38:59 -0700

    switch the targets to be optional

--- packages/flutter_tools/lib/src/build_system/targets/ios.dart
+++ packages/flutter_tools/lib/src/build_system/targets/ios.dart
@@ -474,13 +474,8 @@ class _IssueLaunchRootViewControllerAccess extends Target {
       const Source.pattern(
         '{FLUTTER_ROOT}/packages/flutter_tools/lib/src/build_system/targets/ios.dart',
       ),
-      Source.fromProject((FlutterProject project) {
-        if (project.ios.appDelegateSwift.existsSync()) {
-          return project.ios.appDelegateSwift;
-        } else {
-          return project.ios.appDelegateObjc;
-        }
-      }),
+      Source.fromProject((FlutterProject project) => project.ios.appDelegateObjc, optional: true),
+      Source.fromProject((FlutterProject project) => project.ios.appDelegateSwift, optional: true),
     ];
   }
 
