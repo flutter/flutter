ca8f62412506dc65368fa33711f6c8009dc5783d
Luca Randazzo <likir09@resrc.club>
2025-06-02 09:08:07 +0200

    fix: format test

--- packages/flutter_tools/test/integration.shard/flutter_build_apk_split_per_abi_test.dart
+++ packages/flutter_tools/test/integration.shard/flutter_build_apk_split_per_abi_test.dart
@@ -30,26 +30,31 @@ void main() {
       '--split-per-abi',
     ], workingDirectory: workingDirectory);
 
-    expect(buildResult.exitCode, 0,
-        reason:
-            'Expected `flutter build apk --debug --split-per-abi` to succeed.\n'
-            'stdout:\n${buildResult.stdout}\n'
-            'stderr:\n${buildResult.stderr}');
+    expect(
+      buildResult.exitCode,
+      0,
+      reason:
+          'Expected `flutter build apk --debug --split-per-abi` to succeed.\n'
+          'stdout:\n${buildResult.stdout}\n'
+          'stderr:\n${buildResult.stderr}',
+    );
 
-    final File metadataFile = fileSystem.directory(workingDirectory)
+    final File metadataFile = fileSystem
+        .directory(workingDirectory)
         .childDirectory('build')
         .childDirectory('app')
         .childDirectory('outputs')
         .childDirectory('apk')
         .childDirectory('debug')
         .childFile('output-metadata.json');
-    expect(metadataFile, exists,
-        reason:
-            'Expected output-metadata.json file to exist at ${metadataFile.path}');
+    expect(
+      metadataFile,
+      exists,
+      reason: 'Expected output-metadata.json file to exist at ${metadataFile.path}',
+    );
 
     final Map<String, dynamic> decodedJson =
-        jsonDecode(await metadataFile.readAsString())
-            as Map<String, dynamic>;
+        jsonDecode(await metadataFile.readAsString()) as Map<String, dynamic>;
 
     final List<dynamic> elements = decodedJson['elements'] as List<dynamic>;
 
@@ -58,9 +63,11 @@ void main() {
       final Map<String, dynamic> element = rawElement as Map<String, dynamic>;
 
       final List<dynamic>? filters = element['filters'] as List<dynamic>?;
-      expect(filters, isNotNull,
-          reason:
-              'Could not find filters in ${metadataFile.path} entry: $element');
+      expect(
+        filters,
+        isNotNull,
+        reason: 'Could not find filters in ${metadataFile.path} entry: $element',
+      );
 
       String? abi;
       for (final dynamic rawFilter in filters!) {
@@ -70,14 +77,18 @@ void main() {
           break;
         }
       }
-      expect(abi, isNotNull,
-          reason:
-              'Could not find an ABI filter in ${metadataFile.path} entry: $element');
+      expect(
+        abi,
+        isNotNull,
+        reason: 'Could not find an ABI filter in ${metadataFile.path} entry: $element',
+      );
 
       final int? versionCode = element['versionCode'] as int?;
-      expect(versionCode, isNotNull,
-          reason:
-              'Could not find versionCode in ${metadataFile.path} entry: $element');
+      expect(
+        versionCode,
+        isNotNull,
+        reason: 'Could not find versionCode in ${metadataFile.path} entry: $element',
+      );
 
       actualVersionCodes[abi!] = versionCode!;
     }
@@ -94,15 +105,19 @@ void main() {
       final String abi = kv.key;
       final int abiIndex = kv.value;
 
-      expect(actualVersionCodes, contains(abi),
-          reason:
-              '${metadataFile.path} did not contain an entry for ABI="$abi"');
+      expect(
+        actualVersionCodes,
+        contains(abi),
+        reason: '${metadataFile.path} did not contain an entry for ABI="$abi"',
+      );
 
       final int actual = actualVersionCodes[abi]!;
       final int expected = abiIndex * 1000 + 1;
-      expect(actual, expected,
-          reason:
-              'For ABI="$abi", expected versionCode=$expected, but got $actual instead');
+      expect(
+        actual,
+        expected,
+        reason: 'For ABI="$abi", expected versionCode=$expected, but got $actual instead',
+      );
     }
   });
 }
