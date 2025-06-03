acd607807a6115c738a6fcf380071d94e8394882
Srujan Gaddam <srujzs@google.com>
2025-06-02 21:27:35 -0700

    Try logging using flags even though it only kind of worked locally

--- packages/flutter_tools/lib/src/web/chrome.dart
+++ packages/flutter_tools/lib/src/web/chrome.dart
@@ -229,31 +229,11 @@ class ChromiumLauncher {
         '--no-sandbox',
         '--window-size=2400,1800',
       ],
-      '--enable-logging',
-      '--v=stderr',
+      '--enable-logging=stderr',
+      '--v=1',
       ...webBrowserFlags,
       url,
     ];
-
-    // Periodically print updated contents. For some reason, the console logs
-    // don't get printed to stderr or stdout no matter the combination of
-    // --enable-logging and --v flags we provide. We also can't seem to use
-    // RandomAccessFiles as the next byte seems to always report EOF.
-    final String chromeDebugLog = '${userDataDir.path}/chrome_debug.log';
-    final io.File chromeDebugLogFile = io.File(chromeDebugLog);
-    String previousString = '';
-    Timer.periodic(const Duration(seconds: 10), (_) {
-      if (chromeDebugLogFile.existsSync()) {
-        _logger.printTrace('Found $chromeDebugLog');
-        final String newString = chromeDebugLogFile.readAsStringSync();
-        if (newString != previousString) {
-          _logger.printTrace(newString.substring(previousString.length));
-        }
-        previousString = newString;
-      } else {
-        _logger.printTrace('Could not find $chromeDebugLog');
-      }
-    });
 
     final Process process = await _spawnChromiumProcess(args, chromeExecutable);
 
