f22bbdac0b45dfa96307f52cbc36b8ba24db3c07
Sigurd Meldgaard <sigurdm@google.com>
2025-06-03 08:15:06 +0000

    Unused import

--- packages/flutter_tools/lib/src/dart/pub.dart
+++ packages/flutter_tools/lib/src/dart/pub.dart
@@ -8,7 +8,6 @@ library;
 import 'dart:async';
 
 import 'package:meta/meta.dart';
-import 'package:package_config/package_config.dart';
 import 'package:process/process.dart';
 import '../base/bot_detector.dart';
 import '../base/common.dart';
