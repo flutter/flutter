4a30e4c028c4926e1d9c97f27196c8a44e40c6e7
Renzo Olivares <roliv@google.com>
2025-06-04 13:44:46 -0700

    remove unused import

--- packages/flutter/test/widgets/sliversemantics_test.dart
+++ packages/flutter/test/widgets/sliversemantics_test.dart
@@ -1,7 +1,6 @@
 // Copyright 2014 The Flutter Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
