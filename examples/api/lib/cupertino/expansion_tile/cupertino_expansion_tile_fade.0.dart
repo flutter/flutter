5b39d17e49b39299949b3546e5a0f50ddbd7e073
Dipali Thakare <dipalithakare96@gmail.com>
2025-06-10 12:49:23 +0530

    [cupertino] Add sample and tests for CupertinoExpansionTile with fade and scroll transitions

---
+++ examples/api/lib/cupertino/expansion_tile/cupertino_expansion_tile_fade.0.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/cupertino.dart';
+
+/// Flutter code sample for [CupertinoExpansionTile].
+void main() => runApp(const CupertinoExpansionTileFadeApp());
+
+class CupertinoExpansionTileFadeApp extends StatelessWidget {
+  const CupertinoExpansionTileFadeApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const CupertinoApp(
+      home: CupertinoPageScaffold(
+        navigationBar: CupertinoNavigationBar(middle: Text('Cupertino Expansion Tile - Fade')),
+        child: SafeArea(child: ExpansionTileExample()),
+      ),
+    );
+  }
+}
+
+class ExpansionTileExample extends StatefulWidget {
+  const ExpansionTileExample({super.key});
+
+  @override
+  State<ExpansionTileExample> createState() => _ExpansionTileExampleState();
+}
+
+class _ExpansionTileExampleState extends State<ExpansionTileExample> {
+  late ExpansibleController _controller;
+  bool _isExpanded = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = ExpansibleController();
+    _controller.addListener(() {
+      setState(() {
+        _isExpanded = _controller.isExpanded;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      padding: const EdgeInsets.all(16),
+      children: <Widget>[
+        CupertinoExpansionTile(
+          title: Text(
+            _isExpanded ? 'Collapse me' : 'Tap to expand',
+            style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+          ),
+          controller: _controller,
+          child: Container(
+            padding: const EdgeInsets.all(16),
+            color: CupertinoColors.white,
+            child: const Text(
+              'This is the expanded content of the CupertinoExpansionTile. '
+              'You can place anything here: text, images, buttons, etc.',
+              style: TextStyle(fontSize: 16, color: CupertinoColors.black),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
