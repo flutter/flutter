7c73aac609067079a679f08f0e12333cb9bea315
Dipali Thakare <dipalithakare96@gmail.com>
2025-06-10 17:04:21 +0530

    Add missing license header to example and test files

--- examples/api/test/cupertino/expansion_tile/cupertino_expansion_tile_fade.0_test.dart
+++ examples/api/test/cupertino/expansion_tile/cupertino_expansion_tile_fade.0_test.dart
@@ -1,3 +1,7 @@
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import 'package:flutter_api_samples/cupertino/expansion_tile/cupertino_expansion_tile_fade.0.dart';
 import 'package:flutter_test/flutter_test.dart';
 
