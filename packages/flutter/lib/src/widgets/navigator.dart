6db76dcebf73f2728fec2f7a221e1d6c0ff80816
Alex Medinsh <alex.medinsh@gmail.com>
2025-06-11 09:35:29 +0300

    check willHandlePopInternally

--- packages/flutter/lib/src/widgets/navigator.dart
+++ packages/flutter/lib/src/widgets/navigator.dart
@@ -5633,9 +5633,8 @@ class NavigatorState extends State<Navigator> with TickerProviderStateMixin, Res
         (_RouteEntry e) => _RouteEntry.isPresentPredicate(e) && e != candidate,
       );
 
-      if (next != null && predicate(next.route)) {
+      if (next != null && !next.route.willHandlePopInternally && predicate(next.route)) {
         pop<T>(result);
-        return;
       } else {
         pop();
       }
