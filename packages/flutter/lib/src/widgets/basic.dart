b39ecf698f595f7f84192a48f78c6841599ffe1b
Renzo Olivares <roliv@google.com>
2025-06-10 23:39:57 -0700

    fix tests

--- packages/flutter/lib/src/widgets/basic.dart
+++ packages/flutter/lib/src/widgets/basic.dart
@@ -4175,10 +4175,16 @@ sealed class SemanticsBase extends SingleChildRenderObjectWidget {
     }
 
     final bool containsText =
-        properties.attributedLabel != null ||
         properties.label != null ||
+        properties.attributedLabel != null ||
         properties.value != null ||
+        properties.attributedValue != null ||
+        properties.increasedValue != null ||
+        properties.attributedIncreasedValue != null ||
+        properties.decreasedValue != null ||
+        properties.attributedDecreasedValue != null ||
         properties.hint != null ||
+        properties.attributedHint != null ||
         properties.tooltip != null;
 
     if (!containsText) {
