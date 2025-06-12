3f9c6320f7b6e47233646a2d5a0df55dfbce3a3f
Aaron Clarke <aaclarke@google.com>
2025-06-12 10:48:02 -0700

    update guard

--- engine/src/flutter/tools/licenses_cpp/src/comments_util.h
+++ engine/src/flutter/tools/licenses_cpp/src/comments_util.h
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef FLUTTER_TOOLS_LICENSES_CPP_SRC_COMMENTSUTIL_H_
-#define FLUTTER_TOOLS_LICENSES_CPP_SRC_COMMENTSUTIL_H_
+#ifndef FLUTTER_TOOLS_LICENSES_CPP_SRC_COMMENTS_UTIL_H_
+#define FLUTTER_TOOLS_LICENSES_CPP_SRC_COMMENTS_UTIL_H_
 
 #include <string>
 
@@ -13,4 +13,4 @@ class CommentsUtil {
   static void AddTrimLine(std::string* buffer, const char* text, size_t length);
 };
 
-#endif  // FLUTTER_TOOLS_LICENSES_CPP_SRC_COMMENTSUTIL_H_
+#endif  // FLUTTER_TOOLS_LICENSES_CPP_SRC_COMMENTS_UTIL_H_
