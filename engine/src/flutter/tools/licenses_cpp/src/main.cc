9f6c0bdefe980385d3e54582a28152c4663d4d9b
gaaclarke <30870216+gaaclarke@users.noreply.github.com>
2025-06-13 11:44:50 -0700

    licenses_cpp: Started writing the licenses file (#170539)
    This writes the licenses and associates them with packages. It has
    simple deduplication. This also adds linux ci tests. Writing the file
    adds about 7 seconds, now we are at 37s. Too much is getting written now
    though so that should come down once the output is corrected.
    
    ## Pre-launch Checklist
    
    - [x] I read the [Contributor Guide] and followed the process outlined
    there for submitting PRs.
    - [x] I read the [Tree Hygiene] wiki page, which explains my
    responsibilities.
    - [x] I read and followed the [Flutter Style Guide], including [Features
    we expect every widget to implement].
    - [x] I signed the [CLA].
    - [x] I listed at least one issue that this PR fixes in the description
    above.
    - [x] I updated/added relevant documentation (doc comments with `///`).
    - [x] I added new tests to check the change I am making, or this PR is
    [test-exempt].
    - [x] I followed the [breaking change policy] and added [Data Driven
    Fixes] where supported.
    - [x] All existing and new tests are passing.
    
    If you need help, consider asking for advice on the #hackers-new channel
    on [Discord].
    
    <!-- Links -->
    [Contributor Guide]:
    https://github.com/flutter/flutter/blob/main/docs/contributing/Tree-hygiene.md#overview
    [Tree Hygiene]:
    https://github.com/flutter/flutter/blob/main/docs/contributing/Tree-hygiene.md
    [test-exempt]:
    https://github.com/flutter/flutter/blob/main/docs/contributing/Tree-hygiene.md#tests
    [Flutter Style Guide]:
    https://github.com/flutter/flutter/blob/main/docs/contributing/Style-guide-for-Flutter-repo.md
    [Features we expect every widget to implement]:
    https://github.com/flutter/flutter/blob/main/docs/contributing/Style-guide-for-Flutter-repo.md#features-we-expect-every-widget-to-implement
    [CLA]: https://cla.developers.google.com/
    [flutter/tests]: https://github.com/flutter/tests
    [breaking change policy]:
    https://github.com/flutter/flutter/blob/main/docs/contributing/Tree-hygiene.md#handling-breaking-changes
    [Discord]:
    https://github.com/flutter/flutter/blob/main/docs/contributing/Chat.md
    [Data Driven Fixes]:
    https://github.com/flutter/flutter/blob/main/docs/contributing/Data-driven-Fixes.md

--- engine/src/flutter/tools/licenses_cpp/src/main.cc
+++ engine/src/flutter/tools/licenses_cpp/src/main.cc
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <fstream>
+
 #include "flutter/third_party/abseil-cpp/absl/flags/flag.h"
 #include "flutter/third_party/abseil-cpp/absl/flags/parse.h"
 #include "flutter/third_party/abseil-cpp/absl/flags/usage.h"
@@ -17,6 +19,10 @@ ABSL_FLAG(std::optional<std::string>,
           data_dir,
           std::nullopt,
           "[REQUIRED] The directory with the licenses.");
+ABSL_FLAG(std::optional<std::string>,
+          licenses_path,
+          std::nullopt,
+          "[REQUIRED] The path to write the licenses collection to.");
 ABSL_FLAG(int, v, 0, "Set the verbosity of logs.");
 
 int main(int argc, char** argv) {
@@ -29,8 +35,16 @@ int main(int argc, char** argv) {
 
   std::optional<std::string> working_dir = absl::GetFlag(FLAGS_working_dir);
   std::optional<std::string> data_dir = absl::GetFlag(FLAGS_working_dir);
-  if (working_dir.has_value() && data_dir.has_value()) {
-    return LicenseChecker::Run(working_dir.value(), data_dir.value());
+  std::optional<std::string> licenses_path = absl::GetFlag(FLAGS_licenses_path);
+  if (working_dir.has_value() && data_dir.has_value() &&
+      licenses_path.has_value()) {
+    std::ofstream licenses;
+    licenses.open(licenses_path.value());
+    if (licenses.bad()) {
+      std::cerr << "Unable to write to '" << licenses_path.value() << "'.";
+      return 1;
+    }
+    return LicenseChecker::Run(working_dir.value(), licenses, data_dir.value());
   }
 
   if (!working_dir.has_value()) {
