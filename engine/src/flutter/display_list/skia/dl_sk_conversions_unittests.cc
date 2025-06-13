e6d87233739d692ce02408f575812361cacfc8cc
Jason Simmons <jsimmons@google.com>
2025-06-13 13:06:43 -0700

    Support wide gamut colors when applying a DlColor to an SkPaint
    Fixes https://github.com/flutter/flutter/issues/170203

--- engine/src/flutter/display_list/skia/dl_sk_conversions_unittests.cc
+++ engine/src/flutter/display_list/skia/dl_sk_conversions_unittests.cc
@@ -31,17 +31,18 @@ TEST(DisplayListImageFilter, LocalImageSkiaNull) {
 
 TEST(DisplayListSkConversions, ToSkColor) {
   // Red
-  ASSERT_EQ(ToSk(DlColor::kRed()), SK_ColorRED);
+  ASSERT_EQ(ToSk(DlColor::kRed()), SkColors::kRed);
 
   // Green
-  ASSERT_EQ(ToSk(DlColor::kGreen()), SK_ColorGREEN);
+  ASSERT_EQ(ToSk(DlColor::kGreen()), SkColors::kGreen);
 
   // Blue
-  ASSERT_EQ(ToSk(DlColor::kBlue()), SK_ColorBLUE);
+  ASSERT_EQ(ToSk(DlColor::kBlue()), SkColors::kBlue);
 
   // Half transparent grey
   auto const grey_hex_half_opaque = 0x7F999999;
-  ASSERT_EQ(ToSk(DlColor(grey_hex_half_opaque)), SkColor(grey_hex_half_opaque));
+  ASSERT_EQ(ToSkColor(DlColor(grey_hex_half_opaque)),
+            SkColor(grey_hex_half_opaque));
 }
 
 TEST(DisplayListSkConversions, ToSkTileMode) {
