7dd676c3d4adf70f6cb0125032b60ecf5a2d0d68
rkishan516 <rkishan516@gmail.com>
2025-06-13 21:30:33 +0530

    fix: UpdateNode default values

--- packages/flutter/lib/src/material/progress_indicator.dart
+++ packages/flutter/lib/src/material/progress_indicator.dart
@@ -148,8 +148,9 @@ abstract class ProgressIndicator extends StatefulWidget {
     }
     return Semantics(
       label: semanticsLabel,
-      role:
-          expandedSemanticsValue != null ? SemanticsRole.progressBar : SemanticsRole.loadingSpinner,
+      role: expandedSemanticsValue != null
+          ? SemanticsRole.progressBar
+          : SemanticsRole.loadingSpinner,
       minValue: '0',
       maxValue: '100',
       value: expandedSemanticsValue,
