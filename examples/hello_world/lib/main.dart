6cec84262997d1911b664014a773cb7af3cba93a
Jiaming Meng <jackmeng0814@gmail.com>
2025-06-18 18:04:19 -0400

    Update main.dart

--- examples/hello_world/lib/main.dart
+++ examples/hello_world/lib/main.dart
@@ -6,12 +6,6 @@ import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(
-    const Center(
-      child: Text(
-        'Hello, world!',
-        key: Key('title'),
-        textDirection: TextDirection.ltr,
-      ),
-    ),
+    const Center(child: Text('Hello, world!', key: Key('title'), textDirection: TextDirection.ltr)),
   );
 }
