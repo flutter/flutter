4e5acc076949a79b3a404aa62f93f9d4677c73e4
Jessy Yameogo <jessy.yameogo@gmail.com>
2025-06-18 16:43:07 -0400

    addressed comments

--- packages/flutter_tools/lib/src/isolated/resident_web_runner.dart
+++ packages/flutter_tools/lib/src/isolated/resident_web_runner.dart
@@ -320,7 +320,7 @@ Please provide a valid TCP port (an integer between 0 and 65535, inclusive).
         // Use Chrome-based connection only if we have a connected ChromiumDevice
         // Otherwise, use DWDS WebSocket connection
         final bool useDwdsWebSocketConnection =
-            !(device!.device is ChromiumDevice &&
+            !(_chromiumLauncher != null &&
                 nonWebServerConnectedDeviceIds.contains(device!.device!.id));
 
         device!.devFS = WebDevFS(
