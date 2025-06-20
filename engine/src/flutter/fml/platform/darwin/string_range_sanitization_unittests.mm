a136edf5da47e6d6f619ddcf4b23565d4015388d
LongCatIsLooong <31859944+LongCatIsLooong@users.noreply.github.com>
2025-06-19 19:54:09 -0700

    fix tests

--- engine/src/flutter/fml/platform/darwin/string_range_sanitization_unittests.mm
+++ engine/src/flutter/fml/platform/darwin/string_range_sanitization_unittests.mm
@@ -14,19 +14,18 @@ TEST(StringRangeSanitizationTest, CanHandleUnicode) {
 }
 
 TEST(StringRangeSanitizationTest, HandlesInvalidRanges) {
-  auto ns_not_found = static_cast<unsigned long>(NSNotFound);
-  EXPECT_EQ(fml::RangeForCharacterAtIndex(@"😠", 3).location, ns_not_found);
-  EXPECT_EQ(fml::RangeForCharacterAtIndex(@"😠", -1).location, ns_not_found);
-  EXPECT_EQ(fml::RangeForCharacterAtIndex(nil, 0).location, ns_not_found);
-  EXPECT_EQ(fml::RangeForCharactersInRange(@"😠", NSMakeRange(1, 2)).location, ns_not_found);
-  EXPECT_EQ(fml::RangeForCharactersInRange(@"😠", NSMakeRange(3, 0)).location, ns_not_found);
-  EXPECT_EQ(fml::RangeForCharactersInRange(nil, NSMakeRange(0, 0)).location, ns_not_found);
+  EXPECT_EQ(fml::RangeForCharacterAtIndex(@"😠", 3).location,  0UL);
+  EXPECT_EQ(fml::RangeForCharacterAtIndex(@"😠", -1).location,  0UL);
+  EXPECT_EQ(fml::RangeForCharacterAtIndex(nil, 0).location, 0UL);
+  EXPECT_EQ(fml::RangeForCharactersInRange(@"😠", NSMakeRange(1, 2)).location,  0UL);
+  EXPECT_EQ(fml::RangeForCharactersInRange(@"😠", NSMakeRange(3, 0)).location,  0UL);
+  EXPECT_EQ(fml::RangeForCharactersInRange(nil, NSMakeRange(0, 0)).location, 0UL);
 }
 
 TEST(StringRangeSanitizationTest, CanHandleUnicodeRange) {
   auto result = fml::RangeForCharactersInRange(@"😠", NSMakeRange(1, 0));
   EXPECT_EQ(result.location, 0UL);
-  EXPECT_EQ(result.length, 0UL);
+  EXPECT_EQ(result.length, 2UL);
 }
 
 TEST(StringRangeSanitizationTest, HandlesEndOfRange) {
