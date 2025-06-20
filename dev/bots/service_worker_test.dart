2150b182901ae6eeb77b720791b583c060ac8dec
Jackson Gardner <jacksongardner@google.com>
2025-06-20 16:20:57 -0700

    Both canvaskit variants.

--- dev/bots/service_worker_test.dart
+++ dev/bots/service_worker_test.dart
@@ -33,6 +33,8 @@ final String _targetPath = path.join(_testAppDirectory, _target);
 // Some paths are not guaranteed to actually be requested by the time we finish the test, so we just
 // ignore them and not make them part of the tracked requests or expectations.
 final Set<String> _ignoreRequestPaths = <String>{
+  'canvaskit/canvaskit.js',
+  'canvaskit/canvaskit.wasm',
   'canvaskit/chromium/canvaskit.js',
   'canvaskit/chromium/canvaskit.wasm',
 };
