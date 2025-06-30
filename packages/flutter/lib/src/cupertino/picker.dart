49e1c29cf8bdf7c598c9c92b8c170def6ae8e761
Alex Medinsh <alex.medinsh@gmail.com>
2025-06-30 20:54:07 +0300

    Add `CupertinoPicker` ticking sound (#170641)
    This PR adds a ticking sound to the `CupertinoPicker` on iOS.
    
    Fixes https://github.com/flutter/flutter/issues/37329
    
    ## Pre-launch Checklist
    
    - [x] I read the [Contributor Guide] and followed the process outlined
    there for submitting PRs.
    - [x] I read the [Tree Hygiene] wiki page, which explains my
    responsibilities.
    - [x] I read and followed the [Flutter Style Guide], including [Features
    we expect every widget to implement].
    - [x] I signed the [CLA].
    - [x] I listed at least one issue that this PR fixes in the description
    above.
    - [x] I updated/added relevant documentation (doc comments with `///`).
    - [x] I added new tests to check the change I am making, or this PR is
    [test-exempt].
    - [x] I followed the [breaking change policy] and added [Data Driven
    Fixes] where supported.
    - [x] All existing and new tests are passing.

--- packages/flutter/lib/src/cupertino/picker.dart
+++ packages/flutter/lib/src/cupertino/picker.dart
@@ -279,6 +279,7 @@ class _CupertinoPickerState extends State<CupertinoPicker> {
         if (index != _lastHapticIndex) {
           _lastHapticIndex = index;
           HapticFeedback.selectionClick();
+          SystemSound.play(SystemSoundType.tick);
         }
       case TargetPlatform.android:
       case TargetPlatform.fuchsia:
