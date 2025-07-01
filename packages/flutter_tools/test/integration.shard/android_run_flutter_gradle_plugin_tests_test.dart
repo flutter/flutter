8ae14a59c801f6fcba06778651ff13583b652dd4
Mohellebi abdessalem <mohellebiabdessalem@gmail.com>
2025-07-01 21:09:10 +0100

    new method

--- packages/flutter_tools/test/integration.shard/android_run_flutter_gradle_plugin_tests_test.dart
+++ packages/flutter_tools/test/integration.shard/android_run_flutter_gradle_plugin_tests_test.dart
@@ -12,52 +12,52 @@ import '../src/common.dart';
 import '../src/context.dart';
 import 'test_utils.dart';
 
+// List of Gradle versions to test
+const List<String> gradleVersionsToTest = <String>['8.4', '8.6', '8.12'];
+
 void main() {
+  for (final String gradleVersion in gradleVersionsToTest) {
+    testUsingContext('Flutter Gradle Plugin unit tests using Gradle $gradleVersion', () async {
+      final String gradleFileName = Platform.isWindows ? 'gradlew.bat' : 'gradlew';
+      final String gradleExecutable = Platform.isWindows ? '.\\$gradleFileName' : './$gradleFileName';
+      final Directory pluginDir = fileSystem
+          .directory(getFlutterRoot())
+          .childDirectory('packages')
+          .childDirectory('flutter_tools')
+          .childDirectory('gradle');
+      final File wrapperProps = pluginDir
+          .childDirectory('gradle')
+          .childDirectory('wrapper')
+          .childFile('gradle-wrapper.properties');
 
-  testUsingContext('Flutter Gradle Plugin unit tests using gradle 8.12', () async {
-    final String gradleFileName = Platform.isWindows ? 'gradlew.bat' : 'gradlew';
-    final String gradleExecutable = Platform.isWindows ? '.\\$gradleFileName' : './$gradleFileName';
-    final Directory pluginDir = fileSystem
-        .directory(getFlutterRoot())
-        .childDirectory('packages')
-        .childDirectory('flutter_tools')
-        .childDirectory('gradle');
-    final File wrapperProps = pluginDir
-        .childDirectory('gradle')
-        .childDirectory('wrapper')
-        .childFile('gradle-wrapper.properties');
+      final String originalWrapperContent = wrapperProps.readAsStringSync();
 
-    final String originalWrapperContent = wrapperProps.readAsStringSync();
 
-      // Modify gradle-wrapper.properties
-      final String updatedContent = originalWrapperContent.replaceAllMapped(
-        RegExp(r'distributionUrl=.*?/gradle-(.*?)-'),
-        (match) => match.group(0)!.replaceAll(match.group(1)!, '8.12'),
-      );
-      wrapperProps.writeAsStringSync(updatedContent);
+        // Modify gradle-wrapper.properties to use current version
+        final String updatedContent = originalWrapperContent.replaceAllMapped(
+          RegExp(r'distributionUrl=.*?/gradle-(.*?)-'),
+          (Match match) => match.group(0)!.replaceAll(match.group(1)!, gradleVersion),
+        );
+        wrapperProps.writeAsStringSync(updatedContent);
 
-      // Inject and make executable
-      globals.gradleUtils?.injectGradleWrapperIfNeeded(pluginDir);
-      makeExecutable(pluginDir.childFile(gradleFileName));
+        // Inject and make executable
+        globals.gradleUtils?.injectGradleWrapperIfNeeded(pluginDir);
+        makeExecutable(pluginDir.childFile(gradleFileName));
 
-      final RunResult runResult = await globals.processUtils.run(<String>[
-        gradleExecutable,
-        'test',
-      ], workingDirectory: pluginDir.path);
-      expect(runResult.processResult, const ProcessResultMatcher());
-    }
+        final RunResult runResult = await globals.processUtils.run(<String>[
+          gradleExecutable,
+          'test',
+        ], workingDirectory: pluginDir.path);
+        expect(runResult.processResult, const ProcessResultMatcher());
 
-  
-  );
-
+    });
+  }
 }
 
 void makeExecutable(File file) {
   if (Platform.isWindows) {
-    // no op.
-    return;
+    return; // no-op
   }
-
   final ProcessResult result = processManager.runSync(<String>['chmod', '+x', file.path]);
   expect(result, const ProcessResultMatcher());
 }
