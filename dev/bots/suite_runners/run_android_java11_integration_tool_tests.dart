6ee574522f053c066cd84fe3b0a5c05d4d44e292
Reid Baker <reidbaker@google.com>
2025-07-02 12:17:30 -0400

    add back java 11 tests as empty

---
+++ dev/bots/suite_runners/run_android_java11_integration_tool_tests.dart
@@ -0,0 +1,6 @@
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Empty until ci can be modified.
+Future<void> androidJava11IntegrationToolTestsRunner() async {}
