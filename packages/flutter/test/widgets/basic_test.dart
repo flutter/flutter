395d91b16c6225efa1a727371fec5339228c5df2
hangyu <jhy03261997@gmail.com>
2025-07-03 22:29:29 -0700

    add form role
    Update semantics_test.dart
    
    Update licenses_flutter
    
    Update packages/flutter/lib/src/widgets/form.dart
    
    Update form.dart
    
    Update engine/src/flutter/lib/web_ui/lib/src/engine/semantics/form.dart
    
    Update engine/src/flutter/lib/web_ui/lib/src/engine/semantics/form.dart
    
    Update form.dart
    
    Update form.dart
    
    add tests
    
    lint
    
    Add form role
    
    Co-Authored-By: chunhtai <47866232+chunhtai@users.noreply.github.com>
    Co-Authored-By: Mouad Debbar <mouad.debbar@gmail.com>

--- packages/flutter/test/widgets/basic_test.dart
+++ packages/flutter/test/widgets/basic_test.dart
@@ -751,6 +751,18 @@ void main() {
       expect(listItemNode.role, SemanticsRole.listItem);
     });
 
+    testWidgets('Semantics can use form', (WidgetTester tester) async {
+      final UniqueKey key1 = UniqueKey();
+      await tester.pumpWidget(
+        MaterialApp(
+          home: Scaffold(body: Semantics(key: key1, role: SemanticsRole.form, container: true)),
+        ),
+      );
+      final SemanticsNode formNode = tester.getSemantics(find.byKey(key1));
+
+      expect(formNode.role, SemanticsRole.form);
+    });
+
     testWidgets('Semantics can merge attributed strings with non attributed string', (
       WidgetTester tester,
     ) async {
