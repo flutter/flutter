0c85aedd81f04dfb240306e85d4aec76d6a7fa39
Reid Baker <reidbaker@google.com>
2025-07-08 10:52:34 -0400

    bump lockfile generator to use kotlin 2.1.0

--- dev/tools/bin/generate_gradle_lockfiles.dart
+++ dev/tools/bin/generate_gradle_lockfiles.dart
@@ -292,7 +292,7 @@ buildscript {
 plugins {
     id "dev.flutter.flutter-plugin-loader" version "1.0.0"
     id "com.android.application" version "8.7.0" apply false
-    id "org.jetbrains.kotlin.android" version "1.8.10" apply false
+    id "org.jetbrains.kotlin.android" version "2.1.0" apply false
 }
 
 include ":app"
@@ -379,7 +379,7 @@ buildscript {
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
     id("com.android.application") version "8.7.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.22" apply false
+    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
 }
 
 include(":app")
