b7f4dbf3a9b9bf5443f81eabb65e96a36a657f40
salemiranloye <salemiranloye@gmail.com>
2025-07-08 15:49:24 -0700

    Development Proxy & Web Configuration File
    Co-authored-by: Sydney Bao <sydneybao@google.com>
    
    Update devfs_config.dart
    
    Sydneybao1 (#30)
    
    * mono commit
    
    Co-authored-by: Sydney Bao <sydneybao@google.com>
    
    * Update devfs_config.dart
    
    * fix devfs_config
    
    ---------
    
    Co-authored-by: salemiranloye <salemiranloye@gmail.com>
    Co-authored-by: Sydney Bao <sydneybao@google.com>
    
    adding missing licenses
    
    remove variable from comment
    
    remove old test
    
    fix shard test
    
    change hash
    
    formatting
    
    null check
    
    fix null check
    
    change io to file system
    
    Fix test
    
    Fix comma test
    
    move file from isolated
    
    Rename Class
    
    fix analyze
    
    remove source span

--- packages/flutter_tools/lib/src/commands/drive.dart
+++ packages/flutter_tools/lib/src/commands/drive.dart
@@ -29,6 +29,7 @@ import '../ios/devices.dart';
 import '../resident_runner.dart';
 import '../runner/flutter_command.dart'
     show FlutterCommandCategory, FlutterCommandResult, FlutterOptions;
+import '../web/devfs_config.dart';
 import '../web/web_device.dart';
 import 'run.dart';
 
@@ -304,7 +305,14 @@ class DriveCommand extends RunCommandBase {
       _logger.printError('Screenshot not supported for ${device.displayName}.');
     }
 
-    final bool web = device is WebServerDevice || device is ChromiumDevice;
+    final WebDevServerConfig? devConfig = (device is WebServerDevice || device is ChromiumDevice)
+        ? await loadDevConfig(
+            hostname: stringArg('web-hostname'),
+            port: stringArg('web-port'),
+            tlsCertPath: stringArg('web-tls-cert-path'),
+            tlsCertKeyPath: stringArg('web-tls-cert-key-path'),
+          )
+        : null;
     _flutterDriverFactory ??= FlutterDriverFactory(
       applicationPackageFactory: ApplicationPackageFactory.instance!,
       logger: _logger,
@@ -322,9 +330,11 @@ class DriveCommand extends RunCommandBase {
       logger: _logger,
       throwOnError: false,
     );
-    final DriverService driverService = _flutterDriverFactory!.createDriverService(web);
+    final DriverService driverService = _flutterDriverFactory!.createDriverService(
+      devConfig != null,
+    );
     final BuildInfo buildInfo = await getBuildInfo();
-    final DebuggingOptions debuggingOptions = await createDebuggingOptions(web);
+    final DebuggingOptions debuggingOptions = await createDebuggingOptions(devConfig: devConfig);
     final File? applicationBinary = applicationBinaryPath == null
         ? null
         : _fileSystem.file(applicationBinaryPath);
@@ -342,7 +352,7 @@ class DriveCommand extends RunCommandBase {
           mainPath: targetFile,
           platformArgs: <String, Object>{
             if (traceStartup) 'trace-startup': traceStartup,
-            if (web) '--no-launch-chrome': true,
+            if (devConfig != null) '--no-launch-chrome': true,
           },
         );
       } else {
