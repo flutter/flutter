06ce4bb18772302187db8ec571fe12e34dd71bb8
Nate Biggs <biggs0125@gmail.com>
2025-07-09 17:25:52 -0400

    Add analytics events for wasm dry runs on web builds (#171818)
    Follow up to https://github.com/flutter/flutter/pull/171682
    Depends on https://github.com/dart-lang/tools/pull/2125
    
    ---------
    
    Co-authored-by: Nate Biggs <natebiggs@google.com>

--- packages/flutter_tools/lib/src/web/compile.dart
+++ packages/flutter_tools/lib/src/web/compile.dart
@@ -88,7 +88,7 @@ class WebBuilder {
     final Stopwatch sw = Stopwatch()..start();
     try {
       final BuildResult result = await _buildSystem.build(
-        globals.buildTargets.webServiceWorker(_fileSystem, compilerConfigs),
+        globals.buildTargets.webServiceWorker(_fileSystem, compilerConfigs, _analytics),
         Environment(
           projectDir: flutterProject.directory,
           outputDir: outputDirectory,
