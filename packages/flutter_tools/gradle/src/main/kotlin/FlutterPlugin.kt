3fd55a4940276dcf443551896369df6af40e1e54
Mohellebi abdessalem <mohellebiabdessalem@gmail.com>
2025-07-12 11:25:40 +0100

    Update FlutterPlugin.kt

--- packages/flutter_tools/gradle/src/main/kotlin/FlutterPlugin.kt
+++ packages/flutter_tools/gradle/src/main/kotlin/FlutterPlugin.kt
@@ -757,8 +757,14 @@ class FlutterPlugin : Plugin<Project> {
                     with(compileTask.assets)
                     // TODO(gmackall): Replace with filePermissions.user.read/write = true once
                     //   minimum supported Gradle version is 8.3.
-                    @Suppress("DEPRECATION")
-                    fileMode = 420 // corresponds to unix 0644 in base 8
+                    // @Suppress("DEPRECATION")
+                    // fileMode = 420 // corresponds to unix 0644 in base 8
+                    filePermissions {
+                        user {
+                            read = true
+                            write = true
+                        }
+                    }
                     if (isUsedAsSubproject) {
                         // TODO(gmackall): above is always false, can delete
                         dependsOn(packageAssets)
