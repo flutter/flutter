778c0e986076911af66edbf68f7295f9a87d86de
Mohellebi abdessalem <mohellebiabdessalem@gmail.com>
2025-07-12 12:05:56 +0100

    remove todo

--- packages/flutter_tools/gradle/src/main/kotlin/FlutterPlugin.kt
+++ packages/flutter_tools/gradle/src/main/kotlin/FlutterPlugin.kt
@@ -755,10 +755,6 @@ class FlutterPlugin : Plugin<Project> {
                 ) {
                     dependsOn(compileTask)
                     with(compileTask.assets)
-                    // TODO(gmackall): Replace with filePermissions.user.read/write = true once
-                    //   minimum supported Gradle version is 8.3.
-                    // @Suppress("DEPRECATION")
-                    // fileMode = 420 // corresponds to unix 0644 in base 8
                     filePermissions {
                         user {
                             read = true
