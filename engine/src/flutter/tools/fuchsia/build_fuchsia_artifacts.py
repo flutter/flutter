f287e11c4983e1f6f144d9af9ec20b1bf1e99348
John McDole <codefu@google.com>
2025-07-14 13:14:52 -0700

    Move logic out to shared file

--- engine/src/flutter/tools/fuchsia/build_fuchsia_artifacts.py
+++ engine/src/flutter/tools/fuchsia/build_fuchsia_artifacts.py
@@ -9,7 +9,6 @@
 
 import argparse
 import errno
-import json
 import os
 import platform
 import re
@@ -17,6 +16,7 @@ import shutil
 import subprocess
 import sys
 import tempfile
+from get_content_hash import get_content_hash
 
 _script_dir = os.path.abspath(os.path.join(os.path.realpath(__file__), '..'))
 _src_root_dir = os.path.join(_script_dir, '..', '..', '..')
@@ -416,25 +416,7 @@ def main():
   else:
     # When content hashing is enabled, the engine version will be a content
     # hash instead of a git revision.
-    ci_config_path = os.path.join(_src_root_dir, 'flutter', 'ci', 'builders', 'linux_fuchsia.json')
-    upload_content_hash = False
-    if os.path.exists(ci_config_path):
-      with open(ci_config_path, 'r') as f:
-        ci_config = json.load(f)
-        upload_content_hash = ci_config.get('luci_flags', {}).get('upload_content_hash', False)
-    if upload_content_hash:
-      script_path = os.path.join(
-          _src_root_dir, '..', '..', 'bin', 'internal', 'content_aware_hash.sh'
-      )
-      if os.path.exists(script_path):
-        command = [script_path]
-        try:
-          content_hash = subprocess.check_output(command, text=True).strip()
-          print('Using content hash %s for engine version' % content_hash)
-        except subprocess.CalledProcessError as e:
-          print('Error getting content hash, falling back to git hash: %s' % e)
-      else:
-        print('Could not find content_aware_hash.sh at %s' % script_path)
+    content_hash = get_content_hash()
 
   # Create and optionally upload CIPD package
   if args.cipd_dry_run or args.upload:
--- engine/src/flutter/tools/fuchsia/merge_and_upload_debug_symbols.py
+++ engine/src/flutter/tools/fuchsia/merge_and_upload_debug_symbols.py
@@ -18,12 +18,11 @@ import subprocess
 import sys
 import tarfile
 import tempfile
+from get_content_hash import get_content_hash
 
 # Path to the engine root checkout. This is used to calculate absolute
 # paths if relative ones are passed to the script.
 BUILD_ROOT_DIR = os.path.abspath(os.path.join(os.path.realpath(__file__), '..', '..', '..', '..'))
-_script_dir = os.path.abspath(os.path.join(os.path.realpath(__file__), '..'))
-_src_root_dir = os.path.join(_script_dir, '..', '..', '..')
 
 
 def IsLinux():
@@ -230,25 +229,7 @@ def main():
   else:
     # When content hashing is enabled, the engine version will be a content
     # hash instead of a git revision.
-    ci_config_path = os.path.join(_src_root_dir, 'flutter', 'ci', 'builders', 'linux_fuchsia.json')
-    upload_content_hash = False
-    if os.path.exists(ci_config_path):
-      with open(ci_config_path, 'r') as f:
-        ci_config = json.load(f)
-        upload_content_hash = ci_config.get('luci_flags', {}).get('upload_content_hash', False)
-    if upload_content_hash:
-      script_path = os.path.join(
-          _src_root_dir, '..', '..', 'bin', 'internal', 'content_aware_hash.sh'
-      )
-      if os.path.exists(script_path):
-        command = [script_path]
-        try:
-          content_hash = subprocess.check_output(command, text=True).strip()
-          print('Using content hash %s for engine version' % content_hash)
-        except subprocess.CalledProcessError as e:
-          print('Error getting content hash, falling back to git hash: %s' % e)
-      else:
-        print('Could not find content_aware_hash.sh at %s' % script_path)
+    content_hash = get_content_hash()
 
   ProcessCIPDPackage(should_upload, cipd_def, engine_version, content_hash, out_dir, arch)
   return 0
