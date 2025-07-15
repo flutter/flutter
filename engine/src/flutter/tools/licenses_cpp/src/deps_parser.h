a8d1f2f13e7c07713b404da52652309cec25c080
Aaron Clarke <aaclarke@google.com>
2025-07-14 17:52:49 -0700

    fixed deps parser guard

--- engine/src/flutter/tools/licenses_cpp/src/deps_parser.h
+++ engine/src/flutter/tools/licenses_cpp/src/deps_parser.h
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef FLUTTER_TOOLS_LICENSES_CPP_DEPS_PARSER_H_
-#define FLUTTER_TOOLS_LICENSES_CPP_DEPS_PARSER_H_
+#ifndef FLUTTER_TOOLS_LICENSES_CPP_SRC_DEPS_PARSER_H_
+#define FLUTTER_TOOLS_LICENSES_CPP_SRC_DEPS_PARSER_H_
 
 #include <string>
 #include <string_view>
@@ -17,4 +17,4 @@ class DepsParser {
   std::vector<std::string> Parse(std::string_view input);
 };
 
-#endif  // FLUTTER_TOOLS_LICENSES_CPP_DEPS_PARSER_H_
+#endif  // FLUTTER_TOOLS_LICENSES_CPP_SRC_DEPS_PARSER_H_
