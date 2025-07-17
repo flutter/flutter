8f9a17ef64cad6796579beca8e1212cbbff89432
andrii <andro999b@gmail.com>
2025-07-17 11:52:09 +0300

    add test for vertical in horizontal list case

--- packages/flutter/test/widgets/focus_traversal_test.dart
+++ packages/flutter/test/widgets/focus_traversal_test.dart
@@ -2778,6 +2778,7 @@ void main() {
         await tester.pumpWidget(
           MaterialApp(
             home: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
                 Focus(focusNode: topNode, child: Container(height: 100)),
                 Expanded(
@@ -2786,8 +2787,9 @@ void main() {
                       return SizedBox(
                         height: 100,
                         child: ListView(
+                          scrollDirection: Axis.horizontal,
                           children: <Widget>[
-                            Focus(focusNode: node, child: Container(height: 100))
+                            Focus(focusNode: node, child: const SizedBox(height: 100, width: 100,))
                           ],
                         ),
                       );
@@ -2823,6 +2825,76 @@ void main() {
         await tester.sendKeyEvent(LogicalKeyboardKey.arrowUp);
         await tester.pump();
         expect(topNode.hasPrimaryFocus, isTrue);
+      }
+    );
+
+    testWidgets(
+      'Vertical list inside horizontal list should not prevent focus traversal',
+      (WidgetTester tester) async {
+        final List<FocusNode> nodes = List<FocusNode>.generate(
+          3,
+          (int index) => FocusNode(debugLabel: 'Item ${index + 1}'),
+        ).toList();
+        addTearDown(() {
+          for (final FocusNode node in nodes) {
+            node.dispose();
+          }
+        });
+        final FocusNode leftNode = FocusNode(debugLabel: 'Left');
+        addTearDown(leftNode.dispose);
+        final FocusNode rightNode = FocusNode(debugLabel: 'Right');
+        addTearDown(rightNode.dispose);
+
+        await tester.pumpWidget(
+          MaterialApp(
+            home: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Focus(focusNode: leftNode, child: Container(height: 100)),
+                Expanded(
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: nodes.map<Widget>((FocusNode node) {
+                      return SizedBox(
+                        width: 100,
+                        child: ListView(
+                          children: <Widget>[
+                            Focus(focusNode: node, child: const SizedBox(height: 100, width: 100,))
+                          ],
+                        ),
+                      );
+                    }).toList(),
+                  ),
+                ),
+                Focus(focusNode: rightNode, child: Container(height: 100)),
+              ],
+            ),
+          ),
+        );
+
+        await tester.sendKeyEvent(LogicalKeyboardKey.arrowRight);
+        await tester.pump();
+        expect(leftNode.hasPrimaryFocus, isTrue);
+
+        for (int i = 0; i < nodes.length; i++) {
+          await tester.sendKeyEvent(LogicalKeyboardKey.arrowRight);
+          await tester.pump();
+          expect(nodes[i].hasPrimaryFocus, isTrue, reason: 'Node $i expect to receive focuse');
+        }
+
+        await tester.sendKeyEvent(LogicalKeyboardKey.arrowRight);
+        await tester.pump();
+        expect(rightNode.hasPrimaryFocus, isTrue);
+
+        for (int i = nodes.length - 1; i >=0 ; i--) {
+          await tester.sendKeyEvent(LogicalKeyboardKey.arrowLeft);
+          await tester.pump();
+          expect(nodes[i].hasPrimaryFocus, isTrue, reason: 'Node $i expect to receive focuse');
+        }
+
+        await tester.sendKeyEvent(LogicalKeyboardKey.arrowLeft);
+        await tester.pump();
+        expect(leftNode.hasPrimaryFocus, isTrue);
       }
     );
 
