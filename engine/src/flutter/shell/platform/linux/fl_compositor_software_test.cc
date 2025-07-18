562262f4d3464207cd18fcef3b26d22270395c97
Robert Ancell <robert.ancell@canonical.com>
2025-07-18 13:09:27 +1200

    Remove GtkGLArea and render directly into GtkDrawingArea
    This simplifies the OpenGL/software case and allows us more control over the buffering.

--- engine/src/flutter/shell/platform/linux/fl_compositor_software_test.cc
+++ engine/src/flutter/shell/platform/linux/fl_compositor_software_test.cc
@@ -19,6 +19,7 @@ TEST(FlCompositorSoftwareTest, Render) {
       image_data, CAIRO_FORMAT_ARGB32, 1024, 1024, 1024 * 4);
   cairo_t* cr = cairo_create(surface);
   fl_compositor_present_layers(FL_COMPOSITOR(compositor), nullptr, 0);
-  fl_compositor_software_render(compositor, cr, 1024, 1024, 1);
+  g_autoptr(GdkWindow) window = gdk_window_new(nullptr, nullptr, 0);
+  fl_compositor_render(FL_COMPOSITOR(compositor), cr, window);
   cairo_surface_destroy(surface);
 }
