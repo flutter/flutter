753e74c8cd0bfad101c696fff1a783a1af93babc
Alex Talebi <31685655+SalehTZ@users.noreply.github.com>
2025-07-21 08:03:15 +0330

    replaced `$runtimeType `with `$EdgeInsetsDirectional` and `$_MixedEdgeInsets`
    more details: https://github.com/flutter/flutter/pull/172096#discussion_r2217762492

--- packages/flutter/lib/src/painting/edge_insets.dart
+++ packages/flutter/lib/src/painting/edge_insets.dart
@@ -894,7 +894,7 @@ class EdgeInsetsDirectional extends EdgeInsetsGeometry {
 
   @override
   EdgeInsets resolve(TextDirection? direction) {
-    assert(debugCheckCanResolveTextDirection(direction, '$runtimeType'));
+    assert(debugCheckCanResolveTextDirection(direction, '$EdgeInsetsDirectional'));
     return switch (direction!) {
       TextDirection.rtl => EdgeInsets.fromLTRB(end, top, start, bottom),
       TextDirection.ltr => EdgeInsets.fromLTRB(start, top, end, bottom),
@@ -1006,7 +1006,7 @@ class _MixedEdgeInsets extends EdgeInsetsGeometry {
 
   @override
   EdgeInsets resolve(TextDirection? direction) {
-    assert(debugCheckCanResolveTextDirection(direction, '$runtimeType'));
+    assert(debugCheckCanResolveTextDirection(direction, '$_MixedEdgeInsets'));
     return switch (direction!) {
       TextDirection.rtl => EdgeInsets.fromLTRB(_end + _left, _top, _start + _right, _bottom),
       TextDirection.ltr => EdgeInsets.fromLTRB(_start + _left, _top, _end + _right, _bottom),
