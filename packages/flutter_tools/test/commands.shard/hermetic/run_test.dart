f6d57fbe9e09ea8d3d6ba6fe6c664622bddda546
Matan Lurey <matanlurey@users.noreply.github.com>
2025-07-25 12:14:01 -0700

    Rename `AppRunLogger`, stop writing status messages that break JSON (#172591)
    Closes https://github.com/flutter/flutter/issues/118907.
    
    It looks like some of the work around splitting had already happened
    (`DaemonLogger`), so this ... seems right?

--- packages/flutter_tools/test/commands.shard/hermetic/run_test.dart
+++ packages/flutter_tools/test/commands.shard/hermetic/run_test.dart
@@ -539,7 +539,7 @@ void main() {
             FileSystem: () => fs,
             ProcessManager: () => FakeProcessManager.any(),
             Stdio: () => FakeStdio(),
-            Logger: () => AppRunLogger(parent: logger),
+            Logger: () => MachineOutputLogger(parent: logger),
           },
         );
 
@@ -565,7 +565,7 @@ void main() {
             FileSystem: () => fs,
             ProcessManager: () => FakeProcessManager.any(),
             Stdio: () => FakeStdio(),
-            Logger: () => AppRunLogger(parent: logger),
+            Logger: () => MachineOutputLogger(parent: logger),
           },
         );
       });
