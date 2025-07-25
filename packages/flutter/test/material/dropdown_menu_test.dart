905d0bee044e3404c86fdeae116a3b31aa368d6c
AlsoShantanuBorkar <100504385+AlsoShantanuBorkar@users.noreply.github.com>
2025-07-25 14:06:32 +0530

    remove: unused import

--- packages/flutter/test/material/dropdown_menu_test.dart
+++ packages/flutter/test/material/dropdown_menu_test.dart
@@ -1,8 +1,6 @@
 // Copyright 2014 The Flutter Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-
-import 'dart:developer';
 import 'dart:ui';
 
 import 'package:flutter/foundation.dart';
