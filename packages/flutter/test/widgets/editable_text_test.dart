e0a69808c3383ed96d81b9bf59e466c737ed4939
Renzo Olivares <roliv@google.com>
2025-07-30 17:20:07 -0700

    fix analyzer

--- packages/flutter/test/widgets/editable_text_test.dart
+++ packages/flutter/test/widgets/editable_text_test.dart
@@ -16656,7 +16656,7 @@ void main() {
       Directionality(
         textDirection: TextDirection.ltr,
         child: MediaQuery(
-          data: MediaQueryData(
+          data: const MediaQueryData(
             typographySettings: TypographySettings(
               lineHeight: 2.0,
               letterSpacing: 2.0,
