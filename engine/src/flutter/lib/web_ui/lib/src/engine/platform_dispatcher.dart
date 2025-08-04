1487561bc39ab8a3e172508039bada08fc048617
Renzo Olivares <roliv@google.com>
2025-08-04 16:41:24 -0700

    clean up

--- engine/src/flutter/lib/web_ui/lib/src/engine/platform_dispatcher.dart
+++ engine/src/flutter/lib/web_ui/lib/src/engine/platform_dispatcher.dart
@@ -1059,11 +1059,12 @@ class EnginePlatformDispatcher extends ui.PlatformDispatcher {
     style.width = 'auto';
     style.height = 'auto';
     style.whiteSpace = 'nowrap';
-    // Set text spacing properties to some unreasonable defaults.
-    style.lineHeight = '100px';
-    style.letterSpacing = '100px';
-    style.wordSpacing = '100px';
-    style.margin = '100px';
+    // Set text spacing properties defaults.
+    const double spacingDefault = 100.0;
+    style.lineHeight = '${spacingDefault}px';
+    style.letterSpacing = '${spacingDefault}px';
+    style.wordSpacing = '${spacingDefault}px';
+    style.margin = '${spacingDefault}px';
     domDocument.body!.append(_typographyMeasurementElement!);
 
     _typographySettingsObserver = createDomResizeObserver((
@@ -1090,7 +1091,6 @@ class EnginePlatformDispatcher extends ui.PlatformDispatcher {
         _typographyMeasurementElement!,
         'margin-bottom',
       )?.toDouble();
-      const double spacingDefault = 100.0;
       if (lineHeight == spacingDefault &&
           wordSpacing == spacingDefault &&
           letterSpacing == spacingDefault &&
