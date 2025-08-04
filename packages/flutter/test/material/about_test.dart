d3e2ee9dc9fa12273123bd6108847424c57d3427
Ahmed Mohamed Sameh <ahmedsameha1@gmail.com>
2025-08-04 19:28:20 +0300

    Make sure that a LicensePage doesn't crash in 0x0 environment (#172610)
    This is my attempt to handle
    https://github.com/flutter/flutter/issues/6537 for the LicensePage UI
    control.

--- packages/flutter/test/material/about_test.dart
+++ packages/flutter/test/material/about_test.dart
@@ -1902,6 +1902,20 @@ void main() {
     final Finder xText = find.text('X');
     expect(tester.getSize(xText).isEmpty, isTrue);
   });
+
+  testWidgets('LicensePage renders at zero area', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: Center(
+          child: SizedBox.shrink(
+            child: Scaffold(body: LicensePage(applicationName: 'X')),
+          ),
+        ),
+      ),
+    );
+    final Finder xText = find.text('X');
+    expect(tester.getSize(xText).isEmpty, isTrue);
+  });
 }
 
 class FakeLicenseEntry extends LicenseEntry {
