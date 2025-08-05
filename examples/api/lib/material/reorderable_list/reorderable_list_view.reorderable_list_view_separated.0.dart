c93e6a863ecca0e870e7693df34d1c5976a3d055
Stefan Humm <humm@mercari.com>
2025-08-06 00:01:50 +0900

    use ColoredBox in example

--- examples/api/lib/material/reorderable_list/reorderable_list_view.reorderable_list_view_separated.0.dart
+++ examples/api/lib/material/reorderable_list/reorderable_list_view.reorderable_list_view_separated.0.dart
@@ -42,12 +42,10 @@ class _ReorderableExampleState extends State<ReorderableExample> {
       padding: const EdgeInsets.symmetric(horizontal: 40),
       itemCount: _items.length,
       itemBuilder: (BuildContext context, int index) {
-        return Container(
+        return ColoredBox(
           key: Key('item_$index'),
           color: _items[index].isOdd ? oddItemColor : evenItemColor,
-          child: ListTile(
-            title: Text('Item ${_items[index]}'),
-          ),
+          child: ListTile(title: Text('Item ${_items[index]}')),
         );
       },
       separatorBuilder: (BuildContext context, int index) {
