882088629bac1fd1748175b6b689e202278d9a0f
Bruno Leroux <bruno.leroux@gmail.com>
2025-08-06 15:01:45 +0200

    Fix InputDecorator label padding

--- packages/flutter/lib/src/material/input_decorator.dart
+++ packages/flutter/lib/src/material/input_decorator.dart
@@ -1019,9 +1019,8 @@ class _RenderDecoration extends RenderBox
         constraints.maxWidth -
             (decoration.inputGap * 2 +
                 iconWidth +
-                contentPadding.horizontal +
-                prefixIconSize.width +
-                suffixIconSpace),
+                (prefixIcon == null ? contentPadding.start : prefixIconSize.width) +
+                (suffixIcon == null ? contentPadding.end : suffixIconSpace)),
       );
 
       // Increase the available width for the label when it is scaled down.
