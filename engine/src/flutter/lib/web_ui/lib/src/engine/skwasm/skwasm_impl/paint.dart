b55b3849975f3eeb46fd3a5b201513cc8fd8c7aa
Jackson Gardner <jacksongardner@google.com>
2025-08-07 15:24:33 -0700

    Fix analysis issue.

--- engine/src/flutter/lib/web_ui/lib/src/engine/skwasm/skwasm_impl/paint.dart
+++ engine/src/flutter/lib/web_ui/lib/src/engine/skwasm/skwasm_impl/paint.dart
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'dart:ffi';
-
 import 'package:ui/src/engine.dart';
 import 'package:ui/src/engine/skwasm/skwasm_impl.dart';
 import 'package:ui/ui.dart' as ui;
