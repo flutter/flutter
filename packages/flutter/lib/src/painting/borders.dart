fcaf2bafbc744cb1f3e3e934202ee602b4ddbc4d
ParkJuneWoo <krca0220@gmail.com>
2025-08-09 14:43:40 +0900

    Fix code to match latest upstream master changes

--- packages/flutter/lib/src/painting/borders.dart
+++ packages/flutter/lib/src/painting/borders.dart
@@ -335,7 +335,7 @@ class BorderSide with Diagnosticable {
     }
     if (a.style == b.style && a.strokeAlign == b.strokeAlign) {
       return BorderSide(
-        color: Color.lerp(a.color, b.color, t),
+        color: Color.lerp(a.color, b.color, t)!,
         width: width,
         style: a.style, // == b.style
         strokeAlign: a.strokeAlign, // == b.strokeAlign
@@ -351,13 +351,13 @@ class BorderSide with Diagnosticable {
     };
     if (a.strokeAlign != b.strokeAlign) {
       return BorderSide(
-        color: Color.lerp(colorA, colorB, t),
+        color: Color.lerp(colorA, colorB, t)!,
         width: width,
         strokeAlign: ui.lerpDouble(a.strokeAlign, b.strokeAlign, t)!,
       );
     }
     return BorderSide(
-      color: Color.lerp(colorA, colorB, t),
+      color: Color.lerp(colorA, colorB, t)!,
       width: width,
       strokeAlign: a.strokeAlign, // == b.strokeAlign
     );
