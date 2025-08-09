01372fdb704b35637636b92b6ccdcfc6d1163837
Mohellebi abdessalem <mohellebiabdessalem@gmail.com>
2025-08-09 10:47:38 +0100

    Update devices.dart

--- packages/flutter_tools/lib/src/ios/devices.dart
+++ packages/flutter_tools/lib/src/ios/devices.dart
@@ -500,7 +500,7 @@ class IOSDevice extends Device {
             debuggingOptions.usingCISystem && debuggingOptions.disablePortPublication,
       );
       if (!buildResult.success) {
-        _logger.printError('Could not build the precompiled application for the device.');
+        _logger.printError('Error: could not code sign the application.');
         await diagnoseXcodeBuildFailure(
           buildResult,
           analytics: _analytics,
