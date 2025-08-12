fbba4becf6592f1bff5107d78d76a464ad14b427
Ben Konyi <bkonyi@google.com>
2025-08-12 10:55:33 -0400

    [ Tool ] Fix crash from possible DDS startup race (#173362)
    It's possible (but unlikely) that two DDS instances can try and start at
    the same time (e.g., a "flutter run" is initiated while an existing
    "flutter attach" is waiting for a target to attach to). This can lead to
    DDS failing to initialize for one of the processes when the VM service
    disconnects it after the other process successfully invokes the
    "_yieldControlToDDS" RPC.
    
    This change adds some retry logic to avoid crashing in this scenario.
    
    Fixes https://github.com/flutter/flutter/issues/169265

--- packages/flutter_tools/test/integration.shard/test_driver.dart
+++ packages/flutter_tools/test/integration.shard/test_driver.dart
@@ -186,7 +186,7 @@ abstract final class FlutterTestDriver {
     String extension, {
     Map<String, Object?> args = const <String, Object>{},
   }) async {
-    final int? port = _vmServiceWsUri != null ? vmServicePort : _attachPort;
+    final int port = _attachPort ?? vmServicePort!;
     final VmService vmService = await vmServiceConnectUri('ws://localhost:$port/ws');
     final Isolate isolate = await waitForExtension(vmService, extension);
     return vmService.callServiceExtension(extension, isolateId: isolate.id, args: args);
@@ -649,16 +649,18 @@ final class FlutterRunTestDriver extends FlutterTestDriver {
           timeout: appStartTimeout,
         );
 
-        if (withDebugger) {
+        if (withDebugger || attachPort != null) {
           final Map<String, Object?> debugPort = await _waitFor(
             event: 'app.debugPort',
             timeout: appStartTimeout,
           );
           final wsUriString = (debugPort['params']! as Map<String, Object?>)['wsUri']! as String;
           _vmServiceWsUri = Uri.parse(wsUriString);
-          await connectToVmService(pauseOnExceptions: pauseOnExceptions);
-          if (!startPaused) {
-            await resume();
+          if (withDebugger) {
+            await connectToVmService(pauseOnExceptions: pauseOnExceptions);
+            if (!startPaused) {
+              await resume();
+            }
           }
         }
 
