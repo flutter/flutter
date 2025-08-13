2e290df55d0798b6f8e08eb95235a0ceb2b1cb80
Ben Konyi <bkonyi@google.com>
2025-08-13 11:16:23 -0400

    Remove unused imports

--- packages/flutter_tools/test/integration.shard/widget_preview_test.dart
+++ packages/flutter_tools/test/integration.shard/widget_preview_test.dart
@@ -11,7 +11,6 @@ import 'package:flutter_tools/src/base/io.dart';
 import 'package:flutter_tools/src/base/logger.dart';
 import 'package:flutter_tools/src/commands/widget_preview.dart';
 import 'package:flutter_tools/src/globals.dart' as globals;
-import 'package:flutter_tools/src/runner/flutter_command_runner.dart';
 import 'package:flutter_tools/src/widget_preview/dtd_services.dart';
 import 'package:process/process.dart';
 
