d392e4b48ae3c16a2e3e0b141b805eb342428cd8
oneflutter <flutter070@gmail.com>
2025-08-13 11:36:24 +0530

    Fix: Announce Snackbar message for accessibility compliance

--- dev/a11y_assessments/test/snack_bar_test.dart
+++ dev/a11y_assessments/test/snack_bar_test.dart
@@ -7,21 +7,25 @@ import 'package:flutter_test/flutter_test.dart';
 
 import 'test_utils.dart';
 
+import 'package:a11y_assessments/use_cases/snack_bar.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'test_utils.dart';
+
 void main() {
   testWidgets('snack bar can run', (WidgetTester tester) async {
     await pumpsUseCase(tester, SnackBarUseCase());
     const String snackBarText = 'Awesome Snackbar!';
     expect(find.text(snackBarText), findsNothing);
     await tester.tap(find.text('Show Snackbar'));
-    expect(find.text(snackBarText), findsNothing);
     await tester.pump();
     expect(find.text(snackBarText), findsOneWidget);
   });
 
   testWidgets('snack bar demo page has one h1 tag', (WidgetTester tester) async {
     await pumpsUseCase(tester, SnackBarUseCase());
-    final Finder findHeadingLevelOnes = find.bySemanticsLabel('SnackBar Demo');
     await tester.pumpAndSettle();
+    final Finder findHeadingLevelOnes = find.bySemanticsLabel('SnackBar Demo');
     expect(findHeadingLevelOnes, findsOne);
   });
 }
