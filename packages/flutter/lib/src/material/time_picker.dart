c2cbb9be090c9091cf3cd8c534bcc57f92640c12
Jaineel Mamtora <jaineelmamtora99@gmail.com>
2025-08-13 17:06:20 +0530

    fix: inconsistent horizontal spacing between hours and mins in time picker for non-english language

--- packages/flutter/lib/src/material/time_picker.dart
+++ packages/flutter/lib/src/material/time_picker.dart
@@ -1837,7 +1837,7 @@ class _TimePickerInputState extends State<_TimePickerInput> with RestorationMixi
                   children: <Widget>[
                     Expanded(
                       child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
                         children: <Widget>[
                           Padding(
                             padding: const EdgeInsets.only(bottom: 10),
@@ -1872,7 +1872,7 @@ class _TimePickerInputState extends State<_TimePickerInput> with RestorationMixi
                     ),
                     Expanded(
                       child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
                         children: <Widget>[
                           Padding(
                             padding: const EdgeInsets.only(bottom: 10),
