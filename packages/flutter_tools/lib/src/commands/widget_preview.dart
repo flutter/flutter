f2834fece21d7763909b961e905918fcb475504c
Ben Konyi <bkonyi@google.com>
2025-08-15 14:02:07 -0400

    Fix tests

--- packages/flutter_tools/lib/src/commands/widget_preview.dart
+++ packages/flutter_tools/lib/src/commands/widget_preview.dart
@@ -254,8 +254,6 @@ final class WidgetPreviewStartCommand extends WidgetPreviewSubCommandBase with C
     // Start the timer tracking how long it takes to launch the preview environment.
     previewAnalytics.initializeLaunchStopwatch();
 
-    registerShutdownHooks();
-
     final String? customPreviewScaffoldOutput = stringArg(kWidgetPreviewScaffoldOutputDir);
     final Directory widgetPreviewScaffold = customPreviewScaffoldOutput != null
         ? fs.directory(customPreviewScaffoldOutput)
@@ -332,6 +330,7 @@ final class WidgetPreviewStartCommand extends WidgetPreviewSubCommandBase with C
       throwToolExit('Failed to launch the widget previewer.', exitCode: result);
     }
 
+    await _previewDetector.dispose();
     return FlutterCommandResult.success();
   }
 
@@ -344,12 +343,6 @@ final class WidgetPreviewStartCommand extends WidgetPreviewSubCommandBase with C
   void onHotRestartRequest() {
     logger.printStatus('Triggering restart based on request from preview environment.');
     _widgetPreviewApp?.restart(fullRestart: true);
-  }
-
-  void registerShutdownHooks() {
-    shutdownHooks.addShutdownHook(() async {
-      await _previewDetector.dispose();
-    });
   }
 
   /// Configures the Dart Tooling Daemon connection.
