1d1f1d8fa91efecb58dd0d5c366c2571daf75f10
Gray Mackall <mackall@google.com>
2025-08-15 15:19:39 -0700

    driver test

---
+++ dev/integration_tests/android_engine_test/lib/hcpp/platform_view_overlay_layer_cleared.dart
@@ -0,0 +1,120 @@
+import 'dart:convert';
+
+import 'package:android_driver_extensions/extension.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_driver/driver_extension.dart';
+
+import '../src/allow_list_devices.dart';
+
+void main() {
+  ensureAndroidDevice();
+  enableFlutterDriverExtension(
+    handler: (String? command) async {
+      return json.encode(<String, Object?>{
+        'supported': await HybridAndroidViewController.checkIfSupported(),
+      });
+    },
+    commands: <CommandExtension>[nativeDriverCommands],
+  );
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  // We'll use this to control the visibility of the texture layer.
+  bool _showTexture = true;
+
+  void _toggleTexture() {
+    setState(() {
+      _showTexture = !_showTexture;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text('HCPP Platform View Bug Demo')),
+        body: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: ElevatedButton(
+                key: const ValueKey<String>('ToggleTexture'),
+                onPressed: _toggleTexture,
+                child: Text(_showTexture ? 'Hide Texture' : 'Show Texture'),
+              ),
+            ),
+            Expanded(
+              child: Center(
+                child: Stack(
+                  children: <Widget>[
+                    // The platform view, which is the base layer.
+                    Center(
+                      child: SizedBox(
+                        width: 300,
+                        height: 300,
+                        child: PlatformViewLink(
+                          viewType: 'changing_color_button_platform_view',
+                          surfaceFactory:
+                              (BuildContext context, PlatformViewController controller) {
+                                return AndroidViewSurface(
+                                  controller: controller as AndroidViewController,
+                                  hitTestBehavior: PlatformViewHitTestBehavior.transparent,
+                                  gestureRecognizers:
+                                      const <Factory<OneSequenceGestureRecognizer>>{},
+                                );
+                              },
+                          onCreatePlatformView: (params) {
+                            return PlatformViewsService.initHybridAndroidView(
+                                id: params.id,
+                                viewType: 'changing_color_button_platform_view',
+                                layoutDirection: TextDirection.ltr,
+                                creationParamsCodec: const StandardMessageCodec(),
+                              )
+                              ..addOnPlatformViewCreatedListener(params.onPlatformViewCreated)
+                              ..create();
+                          },
+                        ),
+                      ),
+                    ),
+
+                    // The texture layer on top. We'll use an Opacity widget
+                    // to make it semi-transparent for a clearer demonstration.
+                    if (_showTexture)
+                      const Center(
+                        child: SizedBox(
+                          width: 275,
+                          height: 275,
+                          child: Opacity(
+                            opacity: 0.5,
+                            child: Texture(
+                              // This texture ID needs to be provided by the platform.
+                              // For this example, we'll use a placeholder. In a real
+                              // test, you'd need a mock to provide this.
+                              textureId: 1,
+                            ),
+                          ),
+                        ),
+                      ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
