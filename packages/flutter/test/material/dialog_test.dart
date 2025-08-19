8ab3fc6995c79809b4d8823622b0ee953897b59b
Ahmed Mohamed Sameh <ahmedsameha1@gmail.com>
2025-08-19 12:53:16 +0300

    Make sure that a Dialog doesn't crash in 0x0 environment

--- packages/flutter/test/material/dialog_test.dart
+++ packages/flutter/test/material/dialog_test.dart
@@ -3021,6 +3021,14 @@ void main() {
     expect(find.byType(BackButton), findsNothing);
     expect(find.byType(CloseButton), findsOneWidget);
   });
+
+  testWidgets('Dialog does not crash at zero area', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: SizedBox.shrink(child: Dialog(child: Text('X'))),
+      ),
+    );
+  });
 }
 
 @pragma('vm:entry-point')
