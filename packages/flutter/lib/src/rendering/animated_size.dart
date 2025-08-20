b5debb5bc256074fe4b99fa1541c6a61686073e8
Lewin Pauli <lpauli@huth.org>
2025-08-20 17:20:57 +0200

    fiy analyze issue: RenderAnimatedSize computeDryBaseline replace computeDryLayout with getDryLayout

--- packages/flutter/lib/src/rendering/animated_size.dart
+++ packages/flutter/lib/src/rendering/animated_size.dart
@@ -412,8 +412,8 @@ class RenderAnimatedSize extends RenderAligningShiftedBox {
     }
     // For RenderAnimatedSize, we need to account for the animated size,
     // not just the child size, to match the offset calculation in alignChild().
-  final Size childSize = child.getDryLayout(constraints);
-  final Size mySize = getDryLayout(constraints);
+    final Size childSize = child.getDryLayout(constraints);
+    final Size mySize = getDryLayout(constraints);
     final Offset offset = resolvedAlignment.alongOffset((mySize - childSize) as Offset);
     return result + offset.dy;
   }
