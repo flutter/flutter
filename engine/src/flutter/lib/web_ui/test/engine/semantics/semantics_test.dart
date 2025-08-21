4482ee9429ebf70ac8ca2e37e9bdf2b57ea68b45
Qun Cheng <quncheng@google.com>
2025-08-21 14:54:27 -0700

    Rename overlayPortalParent to traversalOwner

--- engine/src/flutter/lib/web_ui/test/engine/semantics/semantics_test.dart
+++ engine/src/flutter/lib/web_ui/test/engine/semantics/semantics_test.dart
@@ -5670,7 +5670,7 @@ void updateNode(
   int platformViewId = -1, // -1 means not a platform view
   int scrollChildren = 0,
   int scrollIndex = 0,
-  int overlayPortalParent = 0,
+  int traversalOwner = 0,
   double scrollPosition = 0.0,
   double scrollExtentMax = 0.0,
   double scrollExtentMin = 0.0,
@@ -5717,7 +5717,7 @@ void updateNode(
     platformViewId: platformViewId,
     scrollChildren: scrollChildren,
     scrollIndex: scrollIndex,
-    overlayPortalParent: overlayPortalParent,
+    traversalOwner: traversalOwner,
     scrollPosition: scrollPosition,
     scrollExtentMax: scrollExtentMax,
     scrollExtentMin: scrollExtentMin,
