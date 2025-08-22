e46a04601b27388f5be1ef2c8d18b9809c6dd5d7
Elijah Okoroh <okorohelijah@gmail.com>
2025-08-22 07:16:40 -0700

    remove -i test for template=app

--- dev/devicelab/bin/tasks/plugin_test_ios.dart
+++ dev/devicelab/bin/tasks/plugin_test_ios.dart
@@ -8,14 +8,6 @@ import 'package:flutter_devicelab/tasks/plugin_tests.dart';
 Future<void> main() async {
   await task(
     combine(<TaskFunction>[
-      PluginTest('ios', <String>['-i', 'objc', '--platforms=ios']).call,
-      PluginTest('ios', <String>['-i', 'swift', '--platforms=ios']).call,
-      // Test that app builds with Flutter as a transitive dependency.
-      PluginTest('ios', <String>[
-        '-i',
-        'objc',
-        '--platforms=ios',
-      ], cocoapodsTransitiveFlutterDependency: true).call,
       // Test that Dart-only plugins are supported.
       PluginTest('ios', <String>['--platforms=ios'], dartOnlyPlugin: true).call,
       // Test that shared darwin directories are supported.
