20bbf24bc55fd2d3c31ebdd9e394295f12fde736
Matt Kosarek <matt.kosarek@canonical.com>
2025-08-28 14:29:07 -0400

    Fixing driver in test

--- dev/devicelab/bin/tasks/windowing_test.dart
+++ dev/devicelab/bin/tasks/windowing_test.dart
@@ -8,5 +8,5 @@ import 'package:flutter_devicelab/tasks/integration_tests.dart';
 
 Future<void> main() async {
   deviceOperatingSystem = DeviceOperatingSystem.windows;
-  await task(createWindowsStartupDriverTest(deviceIdOverride: 'windows'));
+  await task(createWindowingDriverTest());
 }
