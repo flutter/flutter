073e611f99c880c0dc48eb6335e33ea51ef7ee3e
Mohellebi abdessalem <mohellebiabdessalem@gmail.com>
2025-08-29 17:04:35 +0100

    Create FlutterPluginConstantsTest.kt

---
+++ packages/flutter_tools/gradle/src/test/kotlin/FlutterPluginConstantsTest.kt
@@ -0,0 +1,28 @@
+package com.flutter.gradle
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class FlutterPluginConstantsTest {
+    @Test
+    fun `test PLATFORM_ARCH_MAP contains correct mappings`() {
+        val map = FlutterPluginConstants.PLATFORM_ARCH_MAP
+        assertEquals("armeabi-v7a", map["android-arm"])
+        assertEquals("arm64-v8a", map["android-arm64"])
+        assertEquals("x86_64", map["android-x64"])
+    }
+
+    @Test
+    fun `test ABI_VERSION contains correct version codes`() {
+        val versionMap = FlutterPluginConstants.ABI_VERSION
+        assertEquals(1, versionMap["armeabi-v7a"])
+        assertEquals(2, versionMap["arm64-v8a"])
+        assertEquals(4, versionMap["x86_64"])
+    }
+
+    @Test
+    fun `test DEFAULT_PLATFORMS list contains all supported platforms`() {
+        val platforms = FlutterPluginConstants.DEFAULT_PLATFORMS
+        assertEquals(listOf("android-arm", "android-arm64", "android-x64"), platforms)
+    }
+}
