8a548a743078b251c1e946b5ba5d60306be3ebaf
Chun-Heng Tai <chtai@google.com>
2025-08-29 13:51:20 -0700

    update

--- engine/src/flutter/shell/platform/android/platform_view_android_delegate/platform_view_android_delegate.h
+++ engine/src/flutter/shell/platform/android/platform_view_android_delegate/platform_view_android_delegate.h
@@ -16,7 +16,8 @@ namespace flutter {
 
 class PlatformViewAndroidDelegate {
  public:
-  static constexpr size_t kBytesPerNode = 51 * sizeof(int32_t);
+  static constexpr size_t kBytesPerNode =
+      51 * sizeof(int32_t);  // The # fields in SemanticsNode
   static constexpr size_t kBytesPerChild = sizeof(int32_t);
   static constexpr size_t kBytesPerCustomAction = sizeof(int32_t);
   static constexpr size_t kBytesPerAction = 4 * sizeof(int32_t);
