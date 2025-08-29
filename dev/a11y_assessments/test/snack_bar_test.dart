a88efbc18f92af0d8d71420c28b8ae85f8ef4d5d
oneflutter <flutter070@gmail.com>
2025-08-29 16:06:01 +0530

    fix: resolve analyzer warnings in snack_bar_test.dart
    - Added explicit type annotations to comply with `always_specify_types`
    - Avoided dynamic method call by explicitly casting and typing map entries
    - Fixes `avoid_dynamic_calls` analyzer lint

--- dev/a11y_assessments/test/snack_bar_test.dart
+++ dev/a11y_assessments/test/snack_bar_test.dart
@@ -2,11 +2,10 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-
 import 'package:a11y_assessments/use_cases/snack_bar.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'test_utils.dart';
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
@@ -15,16 +14,25 @@ void main() {
     testWidgets('snack bar announces message', (WidgetTester tester) async {
       final List<Map<String, dynamic>> log = <Map<String, dynamic>>[];
 
-      Future<dynamic> handleMessage(dynamic mockMessage) async {
-        final Map<dynamic, dynamic> message = mockMessage as Map<dynamic, dynamic>;
-        final Map<String, dynamic> castedMessage = Map<String, dynamic>.from(message);
-        log.add(castedMessage);
+      Future<void> handleMessage(Object? mockMessage) async {
+        if (mockMessage is Map<Object?, Object?>) {
+          final Map<String, dynamic> casted = mockMessage.map(
+                (Object? key, Object? value) =>
+                MapEntry<String, dynamic>(key.toString(), value),
+          );
+          log.add(casted);
+        }
       }
 
       TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
-          .setMockDecodedMessageHandler<dynamic>(SystemChannels.accessibility, handleMessage);
+          .setMockDecodedMessageHandler<Object?>(SystemChannels.accessibility, handleMessage);
 
-      await pumpsUseCase(tester, SnackBarUseCase());
+      await tester.pumpWidget(
+        const MediaQuery(
+          data: MediaQueryData(supportsAnnounce: true),
+          child: MaterialApp(home: MainWidget()),
+        ),
+      );
 
       const String snackBarText = 'Awesome Snackbar!';
       expect(find.text(snackBarText), findsNothing);
@@ -34,9 +42,13 @@ void main() {
 
       expect(log, isNotEmpty);
       expect(
-        log.firstWhere((message) =>
-        message['type'] == 'announce' &&
-            message['data']['message'] == snackBarText),
+        log.firstWhere(
+              (Map<String, dynamic> message) {
+            final Map<String, dynamic> data = message['data'] as Map<String, dynamic>;
+            return message['type'] == 'announce' && data['message'] == snackBarText;
+          },
+          orElse: () => <String, dynamic>{},
+        ),
         isNotNull,
       );
     });
