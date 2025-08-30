e6c5176b93c2ab044a0fe0bb4af84657443bcb3f
Ahmed R. Asar <ahmed.ragab.a.asar@gmail.com>
2025-08-30 14:31:26 +0300

    Fix `DropdownMenu` entry selection with enableFilter

--- packages/flutter/lib/src/material/dropdown_menu.dart
+++ packages/flutter/lib/src/material/dropdown_menu.dart
@@ -872,7 +872,9 @@ class _DropdownMenuState<T> extends State<DropdownMenu<T>> {
                     text: entry.label,
                     selection: TextSelection.collapsed(offset: entry.label.length),
                   );
-                  _selectedEntryIndex = i;
+                  _selectedEntryIndex = widget.dropdownMenuEntries.indexWhere(
+                    (DropdownMenuEntry<T> entry) => entry.value == filteredEntries[i].value,
+                  );
                   currentHighlight = widget.enableSearch ? i : null;
                   widget.onSelected?.call(entry.value);
                   _enableFilter = false;
@@ -965,7 +967,9 @@ class _DropdownMenuState<T> extends State<DropdownMenu<T>> {
           text: entry.label,
           selection: TextSelection.collapsed(offset: entry.label.length),
         );
-        _selectedEntryIndex = currentHighlight;
+        _selectedEntryIndex = widget.dropdownMenuEntries.indexWhere(
+          (DropdownMenuEntry<T> entry) => entry.value == filteredEntries[currentHighlight!].value,
+        );
         widget.onSelected?.call(entry.value);
       }
     } else {
