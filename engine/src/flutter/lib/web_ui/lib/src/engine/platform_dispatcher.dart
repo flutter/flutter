a609dbd9dae7d59f7162cfbd0fb3c3474266e6d2
Renzo Olivares <roliv@google.com>
2025-09-03 08:14:10 -0700

    fix test

--- engine/src/flutter/lib/web_ui/lib/src/engine/platform_dispatcher.dart
+++ engine/src/flutter/lib/web_ui/lib/src/engine/platform_dispatcher.dart
@@ -1053,11 +1053,11 @@ class EnginePlatformDispatcher extends ui.PlatformDispatcher {
       _typographyMeasurementElement!,
       'line-height',
     )?.toDouble();
-    final double? fontSize = parseFontSize(_typographyMeasurementElement!)?.toDouble();
-    late final double? lineHeightFactor;
-    if (fontSize != null && lineHeight != null) {
-      lineHeightFactor = lineHeight / fontSize;
-    }
+    // final double? fontSize = parseFontSize(_typographyMeasurementElement!)?.toDouble();
+    // late final double? lineHeightFactor;
+    // if (fontSize != null && lineHeight != null) {
+    //   lineHeightFactor = lineHeight / fontSize;
+    // }
     final double? wordSpacing = parseStyleProperty(
       _typographyMeasurementElement!,
       'word-spacing',
@@ -1075,7 +1075,7 @@ class EnginePlatformDispatcher extends ui.PlatformDispatcher {
       'margin-bottom',
     )?.toDouble();
     return ui.TypographySettings(
-      lineHeight: lineHeightFactor,
+      lineHeight: lineHeight,
       letterSpacing: letterSpacing,
       wordSpacing: wordSpacing,
       paragraphSpacing: paragraphSpacing,
@@ -1107,18 +1107,18 @@ class EnginePlatformDispatcher extends ui.PlatformDispatcher {
     style.wordSpacing = '${spacingDefault}px';
     style.margin = '0px 0px ${spacingDefault}px 0px';
     domDocument.body!.append(_typographyMeasurementElement!);
-    final double? typographyMeasurementElementFontSize = parseFontSize(
-      _typographyMeasurementElement!,
-    )?.toDouble();
-    final double defaultLineHeightFactor =
-        spacingDefault / typographyMeasurementElementFontSize!; //needs adjustment.
+    // final double? typographyMeasurementElementFontSize = parseFontSize(
+    //   _typographyMeasurementElement!,
+    // )?.toDouble();
+    // final double defaultLineHeightFactor =
+    //     spacingDefault / typographyMeasurementElementFontSize!; //needs adjustment.
 
     _typographySettingsObserver = createDomResizeObserver((
       List<DomResizeObserverEntry> entries,
       DomResizeObserver observer,
     ) {
       final ui.TypographySettings computedTypographySettings = _computeTypographySettings();
-      if (computedTypographySettings.lineHeight == defaultLineHeightFactor &&
+      if (computedTypographySettings.lineHeight == spacingDefault &&
           computedTypographySettings.wordSpacing == spacingDefault &&
           computedTypographySettings.letterSpacing == spacingDefault &&
           computedTypographySettings.paragraphSpacing == spacingDefault) {
