48b053aadb1ffc8ef9260f885b738b084b422a2d
Ben Konyi <bkonyi@google.com>
2025-09-04 14:15:18 -0400

    [ Tool ] Remove leftover Android x86 deprecation warning constant (#174941)
    Android x86 support has already been removed.
    
    Found during a post merge investigation of
    https://github.com/flutter/flutter/issues/157543

--- packages/flutter_tools/lib/src/android/gradle.dart
+++ packages/flutter_tools/lib/src/android/gradle.dart
@@ -132,11 +132,6 @@ String getAarTaskFor(BuildInfo buildInfo) {
   return _taskFor('assembleAar', buildInfo);
 }
 
-@visibleForTesting
-const androidX86DeprecationWarning =
-    'Support for Android x86 targets will be removed in the next stable release after 3.27. '
-    'See https://github.com/flutter/flutter/issues/157543 for details.';
-
 /// Returns the output APK file names for a given [AndroidBuildInfo].
 ///
 /// For example, when [AndroidBuildInfo.splitPerAbi] is `true`, multiple APKs are created.
