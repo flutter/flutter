be481714fe5b0853dfc13f78f622fae1008ec30c
Renzo Olivares <roliv@google.com>
2025-09-04 12:17:41 -0700

    attempt 3

--- engine/src/flutter/lib/web_ui/lib/src/engine/platform_dispatcher.dart
+++ engine/src/flutter/lib/web_ui/lib/src/engine/platform_dispatcher.dart
@@ -1053,11 +1053,13 @@ class EnginePlatformDispatcher extends ui.PlatformDispatcher {
       _typographyMeasurementElement!,
       'line-height',
     )?.toDouble();
-    // final double? fontSize = parseFontSize(_typographyMeasurementElement!)?.toDouble();
-    // late final double? lineHeightFactor;
-    // if (fontSize != null && lineHeight != null) {
-    //   lineHeightFactor = lineHeight / fontSize;
-    // }
+    final double? fontSize = parseFontSize(_typographyMeasurementElement!)?.toDouble();
+    late final double? lineHeightFactor;
+    if (fontSize != null && lineHeight != null) {
+      lineHeightFactor = lineHeight / fontSize;
+    } else {
+      lineHeightFactor = null;
+    }
     final double? wordSpacing = parseStyleProperty(
       _typographyMeasurementElement!,
       'word-spacing',
@@ -1075,7 +1077,7 @@ class EnginePlatformDispatcher extends ui.PlatformDispatcher {
       'margin-bottom',
     )?.toDouble();
     return ui.TypographySettings(
-      lineHeight: lineHeight,
+      lineHeight: lineHeightFactor,
       letterSpacing: letterSpacing,
       wordSpacing: wordSpacing,
       paragraphSpacing: paragraphSpacing,
