b452433813fa56f66870306ff79c5f249be80a04
Mohellebi abdessalem <mohellebiabdessalem@gmail.com>
2025-09-17 18:12:08 +0100

    revert

--- engine/src/flutter/shell/platform/android/io/flutter/plugin/platform/PlatformPlugin.java
+++ engine/src/flutter/shell/platform/android/io/flutter/plugin/platform/PlatformPlugin.java
@@ -231,30 +231,33 @@ public class PlatformPlugin {
     // Set up a listener to notify the framework when the system ui has changed.
     View decorView = activity.getWindow().getDecorView();
     decorView.setOnSystemUiVisibilityChangeListener(
-        visibility -> {
-          // `platformChannel.systemChromeChanged` may trigger a callback that eventually results
-          // in a call to `setSystemUiVisibility`.
-          // `setSystemUiVisibility` must not be called in the same frame as when
-          // `onSystemUiVisibilityChange` is received though.
-          //
-          // As such, post `platformChannel.systemChromeChanged` to the view handler to ensure
-          // that downstream callbacks are trigged on the next frame.
-          decorView.post(
-              () -> {
-                if ((visibility & View.SYSTEM_UI_FLAG_FULLSCREEN) == 0) {
-                  // The system bars are visible. Make any desired adjustments to
-                  // your UI, such as showing the action bar or other navigational
-                  // controls. Another common action is to set a timer to dismiss
-                  // the system bars and restore the fullscreen mode that was
-                  // previously enabled.
-                  platformChannel.systemChromeChanged(true);
-                } else {
-                  // The system bars are NOT visible. Make any desired adjustments
-                  // to your UI, such as hiding the action bar or other
-                  // navigational controls.
-                  platformChannel.systemChromeChanged(false);
-                }
-              });
+        new View.OnSystemUiVisibilityChangeListener() {
+          @Override
+          public void onSystemUiVisibilityChange(int visibility) {
+            // `platformChannel.systemChromeChanged` may trigger a callback that eventually results
+            // in a call to `setSystemUiVisibility`.
+            // `setSystemUiVisibility` must not be called in the same frame as when
+            // `onSystemUiVisibilityChange` is received though.
+            //
+            // As such, post `platformChannel.systemChromeChanged` to the view handler to ensure
+            // that downstream callbacks are trigged on the next frame.
+            decorView.post(
+                () -> {
+                  if ((visibility & View.SYSTEM_UI_FLAG_FULLSCREEN) == 0) {
+                    // The system bars are visible. Make any desired adjustments to
+                    // your UI, such as showing the action bar or other navigational
+                    // controls. Another common action is to set a timer to dismiss
+                    // the system bars and restore the fullscreen mode that was
+                    // previously enabled.
+                    platformChannel.systemChromeChanged(true);
+                  } else {
+                    // The system bars are NOT visible. Make any desired adjustments
+                    // to your UI, such as hiding the action bar or other
+                    // navigational controls.
+                    platformChannel.systemChromeChanged(false);
+                  }
+                });
+          }
         });
   }
 
