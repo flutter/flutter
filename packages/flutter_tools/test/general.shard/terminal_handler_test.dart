60043758da6cb3bae5b45ba91e09d8ccc6ebbbb6
Ben Konyi <bkonyi@google.com>
2025-09-18 16:04:10 -0400

    [ Tool ] Serve DevTools from DDS, remove ResidentDevToolsHandler (#174580)
    This PR contains the initial work to prepare for the removal of
    instances of DevTools being served manually by the flutter_tool, instead
    relying on DDS to serve DevTools. This will be consistent with how the
    standalone Dart VM serves DevTools, tying the DevTools lifecycle to a
    live DDS instance. This will allow for the removal of much of the logic
    needed to properly manage the lifecycle of the DevTools server in a
    future PR. Also, by serving DevTools from DDS, users will no longer need
    to forward a secondary port in remote workflows as DevTools will be
    available on the DDS port.
    
    There's two remaining circumstances that will prevent us from removing
    `DevtoolsRunner` completely:
    
      - The daemon's `devtools.serve` endpoint
    - `flutter drive`'s `--profile-memory` flag used for recording memory
    profiles

--- packages/flutter_tools/test/general.shard/terminal_handler_test.dart
+++ packages/flutter_tools/test/general.shard/terminal_handler_test.dart
@@ -18,7 +18,6 @@ import 'package:flutter_tools/src/compile.dart';
 import 'package:flutter_tools/src/convert.dart';
 import 'package:flutter_tools/src/devfs.dart';
 import 'package:flutter_tools/src/device.dart';
-import 'package:flutter_tools/src/resident_devtools_handler.dart';
 import 'package:flutter_tools/src/resident_runner.dart';
 import 'package:flutter_tools/src/vmservice.dart';
 import 'package:test/fake.dart';
@@ -744,11 +743,13 @@ void main() {
     testWithoutContext('v - launchDevToolsInBrowser', () async {
       final TerminalHandler terminalHandler = setUpTerminalHandler(<FakeVmServiceRequest>[]);
       final runner = terminalHandler.residentRunner as FakeResidentRunner;
-      final devtoolsHandler = runner.residentDevtoolsHandler as FakeResidentDevtoolsHandler;
-
-      expect(devtoolsHandler.calledLaunchDevToolsInBrowser, isFalse);
+      for (final FlutterDevice? device in runner.flutterDevices) {
+        expect(device!.device!.dds.calledLaunchDevToolsInBrowser, isFalse);
+      }
       await terminalHandler.processTerminalInput('v');
-      expect(devtoolsHandler.calledLaunchDevToolsInBrowser, isTrue);
+      for (final FlutterDevice? device in runner.flutterDevices) {
+        expect(device!.device!.dds.calledLaunchDevToolsInBrowser, isTrue);
+      }
     });
 
     testWithoutContext('v - does not launchDevToolsInBrowser on web in profile mode', () async {
@@ -759,11 +760,11 @@ void main() {
       );
 
       final runner = terminalHandler.residentRunner as FakeResidentRunner;
-      final devtoolsHandler = runner.residentDevtoolsHandler as FakeResidentDevtoolsHandler;
+      final DartDevelopmentService dds = runner.flutterDevices.single.device!.dds;
 
-      expect(devtoolsHandler.calledLaunchDevToolsInBrowser, isFalse);
+      expect(dds.calledLaunchDevToolsInBrowser, isFalse);
       await terminalHandler.processTerminalInput('v');
-      expect(devtoolsHandler.calledLaunchDevToolsInBrowser, isFalse);
+      expect(dds.calledLaunchDevToolsInBrowser, isFalse);
     });
 
     testWithoutContext('w,W - debugDumpApp without service protocol is skipped', () async {
@@ -1292,21 +1293,6 @@ class FakeResidentRunner extends ResidentHandlers {
       calledReload = true;
     }
     return OperationResult(reloadExitCode, '', fatal: fatalReloadError);
-  }
-
-  // TODO(bkonyi): remove when ready to serve DevTools from DDS.
-  @override
-  ResidentDevtoolsHandler get residentDevtoolsHandler => _residentDevtoolsHandler;
-  final ResidentDevtoolsHandler _residentDevtoolsHandler = FakeResidentDevtoolsHandler();
-}
-
-// TODO(bkonyi): remove when ready to serve DevTools from DDS.
-class FakeResidentDevtoolsHandler extends Fake implements ResidentDevtoolsHandler {
-  var calledLaunchDevToolsInBrowser = false;
-
-  @override
-  bool launchDevToolsInBrowser({List<FlutterDevice?>? flutterDevices}) {
-    return calledLaunchDevToolsInBrowser = true;
   }
 }
 
