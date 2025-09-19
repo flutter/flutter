ce595168b8c28456cbf52471d9f891d2ec4ebbd8
Valentin Vignal <32538273+ValentinVignal@users.noreply.github.com>
2025-09-20 01:16:21 +0800

    Add `CupertinoLinearActivityIndicator` (#170108)
    Fixes https://github.com/flutter/flutter/issues/156167
    
    Continuation of https://github.com/flutter/flutter/pull/156287
    
    <img width="712" alt="Screenshot 2025-06-06 at 10 00 32 AM"
    src="https://github.com/user-attachments/assets/d878287f-8236-4acf-80a5-6ecb6d2a09ec"
    />
    
    Native version:
    
    
    ![image](https://github.com/user-attachments/assets/bb28cd01-c7be-481b-bb22-834b65dd35f5)
    
    ---
    
    Following team discussions, we've decided to adopt the stadium shape:
    
    https://github.com/flutter/flutter/pull/170108#discussion_r2150889845
    https://github.com/flutter/flutter/pull/170108#discussion_r2299927441
    
    
    
    ## Pre-launch Checklist
    
    - [x] I read the [Contributor Guide] and followed the process outlined
    there for submitting PRs.
    - [x] I read the [Tree Hygiene] wiki page, which explains my
    responsibilities.
    - [x] I read and followed the [Flutter Style Guide], including [Features
    we expect every widget to implement].
    - [x] I signed the [CLA].
    - [x] I listed at least one issue that this PR fixes in the description
    above.
    - [x] I updated/added relevant documentation (doc comments with `///`).
    - [x] I added new tests to check the change I am making, or this PR is
    [test-exempt].
    - [x] I followed the [breaking change policy] and added [Data Driven
    Fixes] where supported.
    - [x] All existing and new tests are passing.
    
    If you need help, consider asking for advice on the #hackers-new channel
    on [Discord].
    
    <!-- Links -->
    [Contributor Guide]:
    https://github.com/flutter/flutter/blob/main/docs/contributing/Tree-hygiene.md#overview
    [Tree Hygiene]:
    https://github.com/flutter/flutter/blob/main/docs/contributing/Tree-hygiene.md
    [test-exempt]:
    https://github.com/flutter/flutter/blob/main/docs/contributing/Tree-hygiene.md#tests
    [Flutter Style Guide]:
    https://github.com/flutter/flutter/blob/main/docs/contributing/Style-guide-for-Flutter-repo.md
    [Features we expect every widget to implement]:
    https://github.com/flutter/flutter/blob/main/docs/contributing/Style-guide-for-Flutter-repo.md#features-we-expect-every-widget-to-implement
    [CLA]: https://cla.developers.google.com/
    [flutter/tests]: https://github.com/flutter/tests
    [breaking change policy]:
    https://github.com/flutter/flutter/blob/main/docs/contributing/Tree-hygiene.md#handling-breaking-changes
    [Discord]:
    https://github.com/flutter/flutter/blob/main/docs/contributing/Chat.md
    [Data Driven Fixes]:
    https://github.com/flutter/flutter/blob/main/docs/contributing/Data-driven-Fixes.md

---
+++ examples/api/lib/cupertino/activity_indicator/cupertino_linear_activity_indicator.0.dart
@@ -0,0 +1,74 @@
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/cupertino.dart';
+
+/// Flutter code sample for [CupertinoLinearActivityIndicator].
+
+void main() => runApp(const CupertinoLinearActivityIndicatorApp());
+
+class CupertinoLinearActivityIndicatorApp extends StatelessWidget {
+  const CupertinoLinearActivityIndicatorApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const CupertinoApp(
+      theme: CupertinoThemeData(brightness: Brightness.light),
+      home: CupertinoIndicatorExample(),
+    );
+  }
+}
+
+class CupertinoIndicatorExample extends StatelessWidget {
+  const CupertinoIndicatorExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+        middle: Text('CupertinoLinearActivityIndicator Sample'),
+      ),
+      child: Padding(
+        padding: EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CupertinoLinearActivityIndicator(progress: 0),
+                SizedBox(height: 10),
+                Text('Progress: 0'),
+              ],
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CupertinoLinearActivityIndicator(progress: 0.2),
+                SizedBox(height: 10),
+                Text('Progress: 0.2', textAlign: TextAlign.center),
+              ],
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CupertinoLinearActivityIndicator(progress: 0.4, height: 10),
+                SizedBox(height: 10),
+                Text('Height: 10', textAlign: TextAlign.center),
+              ],
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CupertinoLinearActivityIndicator(progress: 0.6, color: CupertinoColors.activeGreen),
+                SizedBox(height: 10),
+                Text('Color: green', textAlign: TextAlign.center),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
