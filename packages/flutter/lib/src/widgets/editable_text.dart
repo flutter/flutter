42efb19dae61601bdd3fa9108e2a841e4c5ed263
Renzo Olivares <roliv@google.com>
2025-09-18 17:27:59 -0700

    fix tests

--- packages/flutter/lib/src/widgets/editable_text.dart
+++ packages/flutter/lib/src/widgets/editable_text.dart
@@ -3239,19 +3239,19 @@ class EditableTextState extends State<EditableText>
     super.didChangeDependencies();
 
     // Apply platform settings to text style.
-    final ui.TypographySettings typographySettings = MediaQuery.typographySettingsOf(context);
+    final ui.TypographySettings? typographySettings = MediaQuery.maybeTypographySettingsOf(context);
     final bool boldText = MediaQuery.boldTextOf(context);
     if (!boldText &&
-        typographySettings.lineHeight == null &&
-        typographySettings.letterSpacing == null &&
-        typographySettings.wordSpacing == null) {
+        typographySettings?.lineHeight == null &&
+        typographySettings?.letterSpacing == null &&
+        typographySettings?.wordSpacing == null) {
       _style = widget.style;
     } else {
       _style = widget.style.merge(
         TextStyle(
-          height: typographySettings.lineHeight,
-          letterSpacing: typographySettings.letterSpacing,
-          wordSpacing: typographySettings.wordSpacing,
+          height: typographySettings?.lineHeight,
+          letterSpacing: typographySettings?.letterSpacing,
+          wordSpacing: typographySettings?.wordSpacing,
           fontWeight: boldText ? FontWeight.bold : null,
         ),
       );
