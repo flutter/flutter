f6b5034672ae921e949aba386c18dffbcce67d4e
flutter-zl <zhongliu88889@gmail.com>
2025-09-23 14:08:37 -0700

    web_ui semantics: reset autocomplete to off when not email

--- engine/src/flutter/lib/web_ui/lib/src/engine/semantics/text_field.dart
+++ engine/src/flutter/lib/web_ui/lib/src/engine/semantics/text_field.dart
@@ -372,6 +372,7 @@ class SemanticTextField extends SemanticRole {
       } else {
         input.removeAttribute('inputmode');
         input.removeAttribute('autocapitalize');
+        input.autocomplete = 'off';
       }
     }
   }
