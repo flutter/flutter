7535c496d9e957626a4dace25c562f8281595e8d
Mouad Debbar <mdebbar@google.com>
2025-09-23 11:02:26 -0400

    [web] Fix assertion thrown when hot restarting during animation

--- engine/src/flutter/lib/web_ui/test/engine/frame_service_test.dart
+++ engine/src/flutter/lib/web_ui/test/engine/frame_service_test.dart
@@ -14,12 +14,15 @@ void main() {
 
 void testMain() {
   group('FrameService', () {
-    setUp(() {
+    void resetFrameService() {
       FrameService.debugOverrideFrameService(null);
       expect(FrameService.instance.runtimeType, FrameService);
       EnginePlatformDispatcher.instance.onBeginFrame = null;
       EnginePlatformDispatcher.instance.onDrawFrame = null;
-    });
+    }
+
+    setUp(resetFrameService);
+    tearDownAll(resetFrameService);
 
     test('instance is valid and can be overridden', () {
       final defaultInstance = FrameService.instance;
@@ -150,6 +153,40 @@ void testMain() {
       expect(isRenderingInOnBeginFrame, isTrue);
       expect(isRenderingInOnDrawFrame, isTrue);
       expect(valueInOnFinishedRenderingFrame, isFalse);
+    });
+
+    test('Frame is cancelled after a hot restart', () async {
+      final instance = FrameService.instance;
+
+      final frameCompleter = Completer<void>();
+      instance.onFinishedRenderingFrame = () {
+        frameCompleter.complete();
+      };
+
+      expect(instance.isFrameScheduled, isFalse);
+      instance.scheduleFrame();
+      expect(instance.isFrameScheduled, isTrue);
+      // Perform a hot restart immediately after scheduling the frame.
+      debugEmulateHotRestart();
+
+      // Wait for 1 second for the frame to be rendered.
+      bool timedOut = false;
+      await frameCompleter.future.timeout(
+        const Duration(seconds: 1),
+        onTimeout: () {
+          timedOut = true;
+        },
+      );
+      // No frame should've been rendered because the animation frame callback should've been
+      // cancelled on hot restart.
+      expect(timedOut, isTrue);
+      expect(instance.frameData.frameNumber, isZero);
+      expect(frameCompleter.isCompleted, isFalse);
+      // ... and no frame is scheduled.
+      expect(instance.isFrameScheduled, isFalse);
+
+      // To avoid leaving an uncompleted completer, let's complete it.
+      frameCompleter.complete();
     });
   });
 }
