e137101de8e01e3e73a5c6eb4e594f9fac4ffa46
Camille Simon <camillesimon@google.com>
2025-09-23 09:53:00 -0700

    delete fromIntent

--- docs/engine/Android-Flutter-Shell-Arguments.md
+++ docs/engine/Android-Flutter-Shell-Arguments.md
@@ -8,7 +8,22 @@
 | `--disable-service-auth-codes` | Disable the requirement for authentication codes for communicating with the VM service. |
 | `--endless-trace-buffer` | Enable an endless trace buffer so that old events can be viewed. |
 | `--use-test-fonts` | Will make font resolution default to the Ahem test font on all platforms. Only available on the desktop test shells. |
-
+| `--enable-dart-profiling` | Enable Dart profiling. Profiling information can be viewed from Dart / Flutter DevTools. |
+| `--profile-startup` | Make the profiler discard new samples once the profiler sample buffer is full. When this flag is not set, the profiler sample buffer is used as a ring buffer, meaning that once it is full, new samples start overwriting the oldest ones. This switch is only meaningful when set in conjunction with --enable-dart-profiling. |
+| `--enable-software-rendering` | Enable rendering using the Skia software backend. This is useful when testing Flutter on emulators. By default, Flutter will attempt to either use OpenGL, Metal, or Vulkan. |
+| `--skia-deterministic-rendering` | Skips the call to SkGraphics::Init(), thus avoiding swapping out some Skia function pointers based on available CPU features. This is used to obtain 100% deterministic behavior in Skia rendering. |
+| `--trace-skia` | Trace Skia calls. This is useful when debugging the GPU thread. By default, Skia tracing is not enabled to reduce the number of traced events. |
+| `--trace-skia-allowlist=<value>` | Filters out all Skia trace event categories except those that are specified in this comma separated list. |
+| `--trace-systrace` | Trace to the system tracer (instead of the timeline) on platforms where such a tracer is available. Currently only supported on Android and Fuchsia. |
+| `--trace-to-file=<value>` | Write the timeline trace to a file at the specified path. The file will be in Perfetto's proto format; it will be possible to load the file into Perfetto's trace viewer. |
+| `--profile-microtasks` | Enable collection of information about each microtask. Information about completed microtasks will be written to the "Microtask" timeline stream. Information about queued microtasks will be accessible from Dart / Flutter DevTools. |
+| `--enable-impeller=true` or `--enable-impeller=false` | Enable the Impeller renderer on supported platforms. Ignored if Impeller is not supported on the platform. |
+| `--enable-vulkan-validation` | Enable loading Vulkan validation layers. The layers must be available to the application and loadable. On non-Vulkan backends, this flag does nothing. |
+| `--dump-skp-on-shader-compilation` | Automatically dump the skp that triggers new shader compilations. This is useful for writing custom ShaderWarmUp to reduce jank. By default, this is not enabled to reduce the overhead. |
+| `--cache-sksl` | Only cache the shader in SkSL instead of binary or GLSL. This should only be used during development phases. The generated SkSLs can later be used in the release build for shader precompilation at launch in order to eliminate the shader-compile jank. |
+| `--purge-persistent-cache` | Remove all existing persistent cache. This is mainly for debugging purposes such as reproducing the shader compilation jank. |
+| `--verbose-logging` | By default, only errors are logged. This flag enables logging at all severity levels. This is NOT a per shell flag and affects log levels for all shells in the process. |
+| `--dart-flags=<value>` | Flags passed directly to the Dart VM without being interpreted by the Flutter shell. |
 
 # TODO(camsim99): Note that all command line args could be settable in manifest (at least those deleted from FlutterShellArgs). Please audit and figure out what makes sense. I can use other platforms as guidance.
 ## Flags that can be set in the manifest:
--- engine/src/flutter/shell/platform/android/io/flutter/embedding/engine/FlutterShellArgs.java
+++ engine/src/flutter/shell/platform/android/io/flutter/embedding/engine/FlutterShellArgs.java
@@ -25,110 +25,24 @@ public class FlutterShellArgs {
   public static final String ARG_DISABLE_SERVICE_AUTH_CODES = "--disable-service-auth-codes";
   public static final String ARG_ENDLESS_TRACE_BUFFER = "--endless-trace-buffer";
   public static final String ARG_USE_TEST_FONTS = "--use-test-fonts";
-  public static final String ARG_KEY_ENABLE_DART_PROFILING = "enable-dart-profiling";
   public static final String ARG_ENABLE_DART_PROFILING = "--enable-dart-profiling";
-  public static final String ARG_KEY_PROFILE_STARTUP = "profile-startup";
   public static final String ARG_PROFILE_STARTUP = "--profile-startup";
-  public static final String ARG_KEY_ENABLE_SOFTWARE_RENDERING = "enable-software-rendering";
   public static final String ARG_ENABLE_SOFTWARE_RENDERING = "--enable-software-rendering";
-  public static final String ARG_KEY_SKIA_DETERMINISTIC_RENDERING = "skia-deterministic-rendering";
   public static final String ARG_SKIA_DETERMINISTIC_RENDERING = "--skia-deterministic-rendering";
-  public static final String ARG_KEY_TRACE_SKIA = "trace-skia";
   public static final String ARG_TRACE_SKIA = "--trace-skia";
-  public static final String ARG_KEY_TRACE_SKIA_ALLOWLIST = "trace-skia-allowlist";
   public static final String ARG_TRACE_SKIA_ALLOWLIST = "--trace-skia-allowlist=";
-  public static final String ARG_KEY_TRACE_SYSTRACE = "trace-systrace";
   public static final String ARG_TRACE_SYSTRACE = "--trace-systrace";
-  public static final String ARG_KEY_TRACE_TO_FILE = "trace-to-file";
   public static final String ARG_TRACE_TO_FILE = "--trace-to-file";
-  public static final String ARG_KEY_PROFILE_MICROTASKS = "profile-microtasks";
   public static final String ARG_PROFILE_MICROTASKS = "--profile-microtasks";
-  public static final String ARG_KEY_TOGGLE_IMPELLER = "enable-impeller";
-  public static final String ARG_ENABLE_IMPELLER = "--enable-impeller=true";
-  public static final String ARG_DISABLE_IMPELLER = "--enable-impeller=false";
-  public static final String ARG_KEY_ENABLE_VULKAN_VALIDATION = "enable-vulkan-validation";
+  public static final String ARG_ENABLE_IMPELLER = "--enable-impeller=";
   public static final String ARG_ENABLE_VULKAN_VALIDATION = "--enable-vulkan-validation";
-  public static final String ARG_KEY_DUMP_SHADER_SKP_ON_SHADER_COMPILATION =
-      "dump-skp-on-shader-compilation";
   public static final String ARG_DUMP_SHADER_SKP_ON_SHADER_COMPILATION =
       "--dump-skp-on-shader-compilation";
-  public static final String ARG_KEY_CACHE_SKSL = "cache-sksl";
   public static final String ARG_CACHE_SKSL = "--cache-sksl";
-  public static final String ARG_KEY_PURGE_PERSISTENT_CACHE = "purge-persistent-cache";
   public static final String ARG_PURGE_PERSISTENT_CACHE = "--purge-persistent-cache";
-  public static final String ARG_KEY_VERBOSE_LOGGING = "verbose-logging";
   public static final String ARG_VERBOSE_LOGGING = "--verbose-logging";
   public static final String ARG_VM_SERVICE_PORT = "--vm-service-port=";
-  public static final String ARG_KEY_DART_FLAGS = "dart-flags";
-  public static final String ARG_DART_FLAGS = "--dart-flags";
-
-  @NonNull
-  public static FlutterShellArgs fromIntent(@NonNull Intent intent) {
-    // Before adding more entries to this list, consider that arbitrary
-    // Android applications can generate intents with extra data and that
-    // there are many security-sensitive args in the binary.
-    ArrayList<String> args = new ArrayList<>();
-
-    if (intent.getBooleanExtra(ARG_KEY_ENABLE_DART_PROFILING, false)) {
-      args.add(ARG_ENABLE_DART_PROFILING);
-    }
-    if (intent.getBooleanExtra(ARG_KEY_PROFILE_STARTUP, false)) {
-      args.add(ARG_PROFILE_STARTUP);
-    }
-    if (intent.getBooleanExtra(ARG_KEY_ENABLE_SOFTWARE_RENDERING, false)) {
-      args.add(ARG_ENABLE_SOFTWARE_RENDERING);
-    }
-    if (intent.getBooleanExtra(ARG_KEY_SKIA_DETERMINISTIC_RENDERING, false)) {
-      args.add(ARG_SKIA_DETERMINISTIC_RENDERING);
-    }
-    if (intent.getBooleanExtra(ARG_KEY_TRACE_SKIA, false)) {
-      args.add(ARG_TRACE_SKIA);
-    }
-    String traceSkiaAllowlist = intent.getStringExtra(ARG_KEY_TRACE_SKIA_ALLOWLIST);
-    if (traceSkiaAllowlist != null) {
-      args.add(ARG_TRACE_SKIA_ALLOWLIST + traceSkiaAllowlist);
-    }
-    if (intent.getBooleanExtra(ARG_KEY_TRACE_SYSTRACE, false)) {
-      args.add(ARG_TRACE_SYSTRACE);
-    }
-    if (intent.hasExtra(ARG_KEY_TRACE_TO_FILE)) {
-      args.add(ARG_TRACE_TO_FILE + "=" + intent.getStringExtra(ARG_KEY_TRACE_TO_FILE));
-    }
-    if (intent.hasExtra(ARG_KEY_PROFILE_MICROTASKS)) {
-      args.add(ARG_PROFILE_MICROTASKS);
-    }
-    if (intent.hasExtra(ARG_KEY_TOGGLE_IMPELLER)) {
-      if (intent.getBooleanExtra(ARG_KEY_TOGGLE_IMPELLER, false)) {
-        args.add(ARG_ENABLE_IMPELLER);
-      } else {
-        args.add(ARG_DISABLE_IMPELLER);
-      }
-    }
-    if (intent.getBooleanExtra(ARG_KEY_ENABLE_VULKAN_VALIDATION, false)) {
-      args.add(ARG_ENABLE_VULKAN_VALIDATION);
-    }
-    if (intent.getBooleanExtra(ARG_KEY_DUMP_SHADER_SKP_ON_SHADER_COMPILATION, false)) {
-      args.add(ARG_DUMP_SHADER_SKP_ON_SHADER_COMPILATION);
-    }
-    if (intent.getBooleanExtra(ARG_KEY_CACHE_SKSL, false)) {
-      args.add(ARG_CACHE_SKSL);
-    }
-    if (intent.getBooleanExtra(ARG_KEY_PURGE_PERSISTENT_CACHE, false)) {
-      args.add(ARG_PURGE_PERSISTENT_CACHE);
-    }
-    if (intent.getBooleanExtra(ARG_KEY_VERBOSE_LOGGING, false)) {
-      args.add(ARG_VERBOSE_LOGGING);
-    }
-
-    // NOTE: all flags provided with this argument are subject to filtering
-    // based on a list of allowed flags in shell/common/switches.cc. If any
-    // flag provided is not allowed, the process will immediately terminate.
-    if (intent.hasExtra(ARG_KEY_DART_FLAGS)) {
-      args.add(ARG_DART_FLAGS + "=" + intent.getStringExtra(ARG_KEY_DART_FLAGS));
-    }
-
-    return new FlutterShellArgs(args);
-  }
+  public static final String ARG_DART_FLAGS = "--dart-flags=";
 
   @NonNull private Set<String> args;
 
