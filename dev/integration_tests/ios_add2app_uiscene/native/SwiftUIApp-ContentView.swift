6a91fde2edbcc308b46a460af051ebb21a8d9f63
Victoria Ashworth <vashworth@google.com>
2025-09-29 14:24:46 -0500

    fix licenses

--- dev/integration_tests/ios_add2app_uiscene/native/SwiftUIApp-ContentView.swift
+++ dev/integration_tests/ios_add2app_uiscene/native/SwiftUIApp-ContentView.swift
@@ -1,8 +1,11 @@
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import Flutter
 import SwiftUI
 
 struct FlutterViewControllerRepresentable: UIViewControllerRepresentable {
-  // Access the AppDelegate through the view environment.
   @Environment(AppDelegate.self) var appDelegate
 
   func makeUIViewController(context: Context) -> some UIViewController {
--- dev/integration_tests/ios_add2app_uiscene/native/SwiftUIApp-FlutterSceneDelegate.swift
+++ dev/integration_tests/ios_add2app_uiscene/native/SwiftUIApp-FlutterSceneDelegate.swift
@@ -1,9 +1,6 @@
-//
-//  xcode_swiftuiApp.swift
-//  xcode_swiftui
-//
-//  Created by Victoria Ashworth on 9/28/25.
-//
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
 import Flutter
 import FlutterPluginRegistrant
