c1feea1b6210d812a9c9a2ed43aca2b5ed75a025
Ahmed Mohamed Sameh <ahmedsameha1@gmail.com>
2025-10-01 21:33:20 +0300

    Make sure that a MenuBar doesn't crash in 0x0 environment

--- packages/flutter/test/material/menu_anchor_test.dart
+++ packages/flutter/test/material/menu_anchor_test.dart
@@ -5096,6 +5096,17 @@ void main() {
 
     expect(tester.getRect(findMenuPanels()).width, 800.0 - reservedPadding.horizontal);
   });
+
+  testWidgets('MenuBar does not crash at zero area', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: Center(
+          child: SizedBox.shrink(child: MenuBar(children: <Widget>[Text('X')])),
+        ),
+      ),
+    );
+    expect(tester.getSize(find.byType(MenuBar)), Size.zero);
+  });
 }
 
 List<Widget> createTestMenus({
