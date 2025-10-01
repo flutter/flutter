dbbc64a323bdb03f8d29ff7755c2e6fdf1f40e63
Valentin Vignal <32538273+ValentinVignal@users.noreply.github.com>
2025-10-02 02:54:58 +0800

    Migrate to `WidgetStateOutlinedBorder` (#176270)
    Follow up https://github.com/flutter/flutter/pull/176164
    
    Migrate the remaining files from `MaterialStateOutlinedBorder ` to
    `WidgetStateOutlinedBorder `. This PR only focus on
    `WidgetStateOutlinedBorder`.
    
    - This minimizes conflicts and reduces the size of the PR for easier
    reviews and follow up
    - I'll work on the other elements of
    `packages/flutter/lib/src/material/material_state.dart` into other PRs
    
    ## Pre-launch Checklist
    
    - [x] I read the [Contributor Guide] and followed the process outlined
    there for submitting PRs.
    - [x] I read the [Tree Hygiene] wiki page, which explains my
    responsibilities.
    - [x] I read and followed the [Flutter Style Guide], including [Features
    we expect every widget to implement].
    - [x] I signed the [CLA].
    - [ ] I listed at least one issue that this PR fixes in the description
    above.
    - [x] I updated/added relevant documentation (doc comments with `///`).
    - [x] I added new tests to check the change I am making, or this PR is
    [test-exempt].
    - [x] I followed the [breaking change policy] and added [Data Driven
    Fixes] where supported.
    - [x] All existing and new tests are passing.
    
    If you need help, consider asking for advice on the #hackers-new channel
    on [Discord].
    
    **Note**: The Flutter team is currently trialing the use of [Gemini Code
    Assist for
    GitHub](https://developers.google.com/gemini-code-assist/docs/review-github-code).
    Comments from the `gemini-code-assist` bot should not be taken as
    authoritative feedback from the Flutter team. If you find its comments
    useful you can update your code accordingly, but if you are unsure or
    disagree with the feedback, please feel free to wait for a Flutter team
    member's review for guidance on which automated comments should be
    addressed.
    
    <!-- Links -->
    [Contributor Guide]:
    https://github.com/flutter/flutter/blob/main/docs/contributing/Tree-hygiene.md#overview
    [Tree Hygiene]:
    https://github.com/flutter/flutter/blob/main/docs/contributing/Tree-hygiene.md
    [test-exempt]:
    https://github.com/flutter/flutter/blob/main/docs/contributing/Tree-hygiene.md#tests
    [Flutter Style Guide]:
    https://github.com/flutter/flutter/blob/main/docs/contributing/Style-guide-for-Flutter-repo.md
    [Features we expect every widget to implement]:
    https://github.com/flutter/flutter/blob/main/docs/contributing/Style-guide-for-Flutter-repo.md#features-we-expect-every-widget-to-implement
    [CLA]: https://cla.developers.google.com/
    [flutter/tests]: https://github.com/flutter/tests
    [breaking change policy]:
    https://github.com/flutter/flutter/blob/main/docs/contributing/Tree-hygiene.md#handling-breaking-changes
    [Discord]:
    https://github.com/flutter/flutter/blob/main/docs/contributing/Chat.md
    [Data Driven Fixes]:
    https://github.com/flutter/flutter/blob/main/docs/contributing/Data-driven-Fixes.md

--- packages/flutter/test/material/chip_theme_test.dart
+++ packages/flutter/test/material/chip_theme_test.dart
@@ -1045,7 +1045,7 @@ void main() {
       brightness: Brightness.light,
       secondaryColor: Colors.blue,
       labelStyle: const TextStyle(),
-    ).copyWith(shape: _MaterialStateOutlinedBorder(getShape));
+    ).copyWith(shape: _TestWidgetStateOutlinedBorder(getShape));
 
     Widget chipWidget({bool selected = false}) {
       return MaterialApp(
@@ -1073,7 +1073,7 @@ void main() {
       return null;
     }
 
-    final ChipThemeData chipTheme = ChipThemeData(shape: _MaterialStateOutlinedBorder(getShape));
+    final ChipThemeData chipTheme = ChipThemeData(shape: _TestWidgetStateOutlinedBorder(getShape));
 
     Widget chipWidget({bool selected = false}) {
       return MaterialApp(
@@ -1526,8 +1526,8 @@ void main() {
   });
 }
 
-class _MaterialStateOutlinedBorder extends StadiumBorder implements MaterialStateOutlinedBorder {
-  const _MaterialStateOutlinedBorder(this.resolver);
+class _TestWidgetStateOutlinedBorder extends StadiumBorder implements WidgetStateOutlinedBorder {
+  const _TestWidgetStateOutlinedBorder(this.resolver);
 
   final WidgetPropertyResolver<OutlinedBorder?> resolver;
 
