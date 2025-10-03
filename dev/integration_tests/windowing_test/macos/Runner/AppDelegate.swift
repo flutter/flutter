cdebcb789a0c858a9b6b55d07ba2fad6c5a13860
Matej Knopp <matej.knopp@gmail.com>
2025-10-03 11:49:07 +0200

    Fix app delegates

--- dev/integration_tests/windowing_test/macos/Runner/AppDelegate.swift
+++ dev/integration_tests/windowing_test/macos/Runner/AppDelegate.swift
@@ -3,10 +3,6 @@ import FlutterMacOS
 
 @main
 class AppDelegate: FlutterAppDelegate {
-  override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
-    return true
-  }
-
   override func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
     return true
   }
