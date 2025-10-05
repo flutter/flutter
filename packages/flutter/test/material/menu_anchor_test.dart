372e21b24a60f621cf78d229d937f3d763a646b7
Ahmed Mohamed Sameh <ahmedsameha1@gmail.com>
2025-10-05 13:01:19 +0300

    Make sure that a SubmenuButton doesn't crash in 0x0 environment

--- packages/flutter/test/material/menu_anchor_test.dart
+++ packages/flutter/test/material/menu_anchor_test.dart
@@ -5096,6 +5096,17 @@ void main() {
 
     expect(tester.getRect(findMenuPanels()).width, 800.0 - reservedPadding.horizontal);
   });
+
+  testWidgets('SubmenuButton does not crash at zero area', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: Center(
+          child: SizedBox.shrink(child: SubmenuButton(menuChildren: <Widget>[], child: null)),
+        ),
+      ),
+    );
+    expect(tester.getSize(find.byType(SubmenuButton)), Size.zero);
+  });
 }
 
 List<Widget> createTestMenus({
