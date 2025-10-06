648c327d09fab67e1d01f275fa09b4b8a81b7991
herculezzz.rossi <herculezzz.rossi@gmail.com>
2025-10-06 22:12:10 +0000

    “Semana8”

---
+++ lib/components.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ComponentsPage extends StatelessWidget {
+ const ComponentsPage({super.key});
+
+ @override
+ Widget build(BuildContext context) {
+   return Scaffold(
+     appBar: AppBar(title: const Text('Componentes')),
+     body: const Center(),
+   );
+ }
+}
