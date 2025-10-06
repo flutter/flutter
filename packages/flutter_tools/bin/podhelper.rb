920a927087e29f5a1227b4d57558b308f11b4a85
Victoria Ashworth <vashworth@google.com>
2025-10-06 11:40:43 -0500

    remove accidently committed files

--- packages/flutter_tools/bin/podhelper.rb
+++ packages/flutter_tools/bin/podhelper.rb
@@ -69,7 +69,7 @@ def flutter_additional_ios_build_settings(target)
     # Build both x86_64 and arm64 simulator archs for all dependencies. If a single plugin does not support arm64 simulators,
     # the app and all frameworks will fall back to x86_64. Unfortunately that case is not detectable in this script.
     # Therefore all pods must have a x86_64 slice available, or linking a x86_64 app will fail.
-    build_configuration.build_settings['ONLY_ACTIVE_ARCH'] = 'YES' if build_configuration.type == :debug
+    build_configuration.build_settings['ONLY_ACTIVE_ARCH'] = 'NO' if build_configuration.type == :debug
 
     # Workaround https://github.com/CocoaPods/CocoaPods/issues/11402, do not sign resource bundles.
     if target_is_resource_bundle
@@ -90,10 +90,7 @@ def flutter_additional_ios_build_settings(target)
 
     # Profile can't be derived from the CocoaPods build configuration. Use release framework (for linking only).
     # TODO(stuartmorgan): Handle local engines here; see https://github.com/flutter/flutter/issues/132228
-    # configuration_engine_dir = build_configuration.type == :debug ? debug_framework_dir : release_framework_dir
-
-    configuration_engine_dir = "/Users/vashworth/Development/flutter/engine/src/out/ios_debug_sim_unopt_arm64/Flutter.xcframework"
-
+    configuration_engine_dir = build_configuration.type == :debug ? debug_framework_dir : release_framework_dir
     Dir.new(configuration_engine_dir).each_child do |xcframework_file|
       next if xcframework_file.start_with?('.') # Hidden file, possibly on external disk.
       if xcframework_file.end_with?('-simulator') # ios-arm64_x86_64-simulator
