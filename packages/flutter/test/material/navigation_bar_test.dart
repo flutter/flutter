cc826dd103b6cae9bcb4b5c7ee8622638192120e
Ahmed Mohamed Sameh <ahmedsameha1@gmail.com>
2025-10-08 21:03:12 +0300

    Make sure that a NavigationBar doesn't crash in 0x0 environment

--- packages/flutter/test/material/navigation_bar_test.dart
+++ packages/flutter/test/material/navigation_bar_test.dart
@@ -1742,6 +1742,24 @@ void main() {
         .bottom;
     expect(safeAreaBottomPadding, equals(0));
   });
+
+  testWidgets('NavigationBar does not crash at zero area', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Center(
+          child: SizedBox.shrink(
+            child: NavigationBar(
+              destinations: const <Widget>[
+                NavigationDestination(icon: Icon(Icons.add), label: 'X'),
+                NavigationDestination(icon: Icon(Icons.abc), label: 'Y'),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+    expect(tester.getSize(find.byType(NavigationBar)), Size.zero);
+  });
 }
 
 Widget _buildWidget(Widget child, {bool? useMaterial3}) {
