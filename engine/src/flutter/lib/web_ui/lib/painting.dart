c99931690d277d3fec41e5fe0e7a80f8821d9d28
Aaron Clarke <aaclarke@google.com>
2025-10-10 11:58:11 -0700

    updated web api

--- engine/src/flutter/lib/web_ui/lib/painting.dart
+++ engine/src/flutter/lib/web_ui/lib/painting.dart
@@ -992,6 +992,19 @@ abstract class FragmentProgram {
   FragmentShader fragmentShader();
 }
 
+base class UniformFloatSlot {
+  UniformFloatSlot(this.name, this.index);
+  void set(double val) {
+    throw UnsupportedError('UniformFloatSlot is not supported on the web.');
+  }
+
+  int get shaderIndex => -1;
+
+  final String name;
+
+  final int index;
+}
+
 abstract class FragmentShader implements Shader {
   void setFloat(int index, double value);
 
@@ -1002,4 +1015,6 @@ abstract class FragmentShader implements Shader {
 
   @override
   bool get debugDisposed;
+
+  UniformFloatSlot getUniformFloat(String name, [int? index]) => UniformFloatSlot(name, index ?? 0);
 }
