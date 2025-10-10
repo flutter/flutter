2a06112d80b497576129b80e67a620139f34115f
Mouad Debbar <mdebbar@google.com>
2025-10-10 16:40:12 -0400

    more tries

--- dev/bots/service_worker_test.dart
+++ dev/bots/service_worker_test.dart
@@ -83,10 +83,12 @@ self.addEventListener('fetch', (event) => {
 
     server = await _startServer(headless: headless);
     await _waitForAppToRequest(server, 'main.dart.js');
+    await _waitForAppToRequest(server, 'CLOSE');
     _requestedPaths.clear();
     print('== RELOADING PAGE ==');
     await server.chrome.reloadPage();
     await _waitForAppToRequest(server, 'flutter_service_worker.js');
+    await _waitForAppToRequest(server, 'CLOSE');
 
     expect(
       _requestedPaths,
@@ -103,10 +105,12 @@ self.addEventListener('fetch', (event) => {
     _requestedPaths.clear();
     server = await _startServer(headless: headless);
     await _waitForAppToRequest(server, 'main.dart.js');
+    await _waitForAppToRequest(server, 'CLOSE');
     _requestedPaths.clear();
     print('== RELOADING PAGE ==');
     await server.chrome.reloadPage();
     await _waitForAppToRequest(server, 'main.dart.js');
+    await _waitForAppToRequest(server, 'CLOSE');
 
     expect(
       _requestedPaths,
