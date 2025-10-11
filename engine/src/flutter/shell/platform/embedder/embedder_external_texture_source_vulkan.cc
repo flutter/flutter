482325f67bb01a6124b3f5397d626239cd428389
xiaowei guan <60122246+xiaowei-guan@users.noreply.github.com>
2025-10-11 10:42:02 +0800

    [Tizen] Support render texture for vulkan + impeller (#17)
    Add new struct FlutterVulkanTexture  for embedder :
    ```
    typedef struct {
      /// Handle to the VkImage that is owned by the embedder. The engine will
      /// bind this image for writing the frame.
      FlutterVulkanImageHandle image;
      /// The VkDeviceMemory that backs the iamge.
      FlutterVulkanDeviceMemoryHandle image_memory;
      /// The VkFormat of the image (for example: VK_FORMAT_R8G8B8A8_UNORM).
      uint32_t format;
      /// User data to be returned on the invocation of the destruction callback.
      void* user_data;
      /// Callback invoked (on an engine managed thread) that asks the embedder to
      /// collect the texture.
      VoidCallback destruction_callback;
      /// Optional parameters for texture height/width, default is 0, non-zero means
      /// the texture has the specified width/height.
      /// Width of the texture.
      size_t width;
      /// Height of the texture.
      size_t height;
    } FlutterVulkanTexture;
    ```
    The implement of [texture
    source](https://github.com/flutter-tizen/flutter/pull/17/files#diff-7955a8522a753162869f2e8ca0017a83f4854b60800c844202e70c3aa00ff0c9R5-R204)
    refer to the solution of android
    platform(https://github.com/flutter-tizen/flutter/blob/flutter-3.35.3/engine/src/flutter/impeller/renderer/backend/vulkan/android/ahb_texture_source_vk.cc)
    
    and I have submitted the code [Support render texture for
    embedder](https://github.com/xiaowei-guan/embedder/commit/6a94746b5a006730e6f5280d5a4fecf8eef53b4d),
    I will create a new PR after[ support vulkan
    backend](https://github.com/flutter-tizen/embedder/pull/110) PR
    released.

---
+++ engine/src/flutter/shell/platform/embedder/embedder_external_texture_source_vulkan.cc
@@ -0,0 +1,204 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "flutter/shell/platform/embedder/embedder_external_texture_source_vulkan.h"
+
+#include "impeller/renderer/backend/vulkan/allocator_vk.h"
+#include "impeller/renderer/backend/vulkan/context_vk.h"
+#include "impeller/renderer/backend/vulkan/texture_source_vk.h"
+#include "impeller/renderer/backend/vulkan/yuv_conversion_library_vk.h"
+#include "vulkan/vulkan.hpp"
+
+namespace flutter {
+
+bool RequiresYCBCRConversion(impeller::vk::Format format) {
+  switch (format) {
+    case impeller::vk::Format::eG8B8R83Plane420Unorm:
+    case impeller::vk::Format::eG8B8R82Plane420Unorm:
+    case impeller::vk::Format::eG8B8R83Plane422Unorm:
+    case impeller::vk::Format::eG8B8R82Plane422Unorm:
+    case impeller::vk::Format::eG8B8R83Plane444Unorm:
+      return true;
+    default:
+      // NOTE: NOT EXHAUSTIVE.
+      break;
+  }
+  return false;
+}
+
+EmbedderExternalTextureSourceVulkan::EmbedderExternalTextureSourceVulkan(
+    const std::shared_ptr<impeller::Context>& p_context,
+    FlutterVulkanTexture* embedder_desc)
+    : TextureSourceVK(ToTextureDescriptor(embedder_desc)) {
+  const auto& context = impeller::ContextVK::Cast(*p_context);
+  const auto& device = context.GetDevice();
+  texture_image_ =
+      impeller::vk::Image(reinterpret_cast<VkImage>(embedder_desc->image));
+  texture_device_memory_ = impeller::vk::DeviceMemory(
+      reinterpret_cast<VkDeviceMemory>(embedder_desc->image_memory));
+
+  needs_yuv_conversion_ = RequiresYCBCRConversion(
+      static_cast<impeller::vk::Format>(embedder_desc->format));
+  std::shared_ptr<impeller::YUVConversionVK> yuv_conversion;
+  if (needs_yuv_conversion_) {
+    // Figure out how to perform YUV conversions.
+    yuv_conversion = CreateYUVConversion(context, embedder_desc);
+    if (!yuv_conversion || !yuv_conversion->IsValid()) {
+      VALIDATION_LOG << "Fail to create yuv conversion";
+      return;
+    }
+  }
+
+  // Create image view for the newly created image.
+  if (!CreateTextureImageView(device, embedder_desc, yuv_conversion)) {
+    VALIDATION_LOG << "Fail to create texture image view";
+    return;
+  }
+
+  yuv_conversion_ = std::move(yuv_conversion);
+  is_valid_ = true;
+}
+
+impeller::PixelFormat ToPixelFormat(int32_t vk_format) {
+  switch (vk_format) {
+    case VK_FORMAT_UNDEFINED:
+      return impeller::PixelFormat::kUnknown;
+    case VK_FORMAT_R8G8B8A8_UNORM:
+      return impeller::PixelFormat::kR8G8B8A8UNormInt;
+    case VK_FORMAT_R8G8B8A8_SRGB:
+      return impeller::PixelFormat::kR8G8B8A8UNormIntSRGB;
+    case VK_FORMAT_B8G8R8A8_UNORM:
+      return impeller::PixelFormat::kB8G8R8A8UNormInt;
+    case VK_FORMAT_B8G8R8A8_SRGB:
+      return impeller::PixelFormat::kB8G8R8A8UNormIntSRGB;
+    case VK_FORMAT_R32G32B32A32_SFLOAT:
+      return impeller::PixelFormat::kR32G32B32A32Float;
+    case VK_FORMAT_R16G16B16A16_SFLOAT:
+      return impeller::PixelFormat::kR16G16B16A16Float;
+    case VK_FORMAT_S8_UINT:
+      return impeller::PixelFormat::kS8UInt;
+    case VK_FORMAT_D24_UNORM_S8_UINT:
+      return impeller::PixelFormat::kD24UnormS8Uint;
+    case VK_FORMAT_D32_SFLOAT_S8_UINT:
+      return impeller::PixelFormat::kD32FloatS8UInt;
+    case VK_FORMAT_R8_UNORM:
+      return impeller::PixelFormat::kR8UNormInt;
+    case VK_FORMAT_R8G8_UNORM:
+      return impeller::PixelFormat::kR8G8UNormInt;
+    default:
+      return impeller::PixelFormat::kUnknown;
+  }
+}
+
+impeller::TextureDescriptor
+EmbedderExternalTextureSourceVulkan::ToTextureDescriptor(
+    FlutterVulkanTexture* embedder_desc) {
+  const auto size =
+      impeller::ISize{static_cast<int64_t>(embedder_desc->width),
+                      static_cast<int64_t>(embedder_desc->height)};
+  impeller::TextureDescriptor desc;
+  desc.storage_mode = impeller::StorageMode::kDevicePrivate;
+  desc.format = ToPixelFormat(embedder_desc->format);
+  desc.size = size;
+  desc.type = impeller::TextureType::kTexture2D;
+  desc.sample_count = impeller::SampleCount::kCount1;
+  desc.compression_type = impeller::CompressionType::kLossless;
+  desc.mip_count = 1u;
+  desc.usage = impeller::TextureUsage::kRenderTarget;
+  return desc;
+}
+
+std::shared_ptr<impeller::YUVConversionVK>
+EmbedderExternalTextureSourceVulkan::CreateYUVConversion(
+    const impeller::ContextVK& context,
+    FlutterVulkanTexture* embedder_desc) {
+  impeller::YUVConversionDescriptorVK conversion_chain;
+  auto& conversion_info = conversion_chain.get();
+
+  conversion_info.format =
+      static_cast<impeller::vk::Format>(embedder_desc->format);
+  conversion_info.ycbcrModel =
+      impeller::vk::SamplerYcbcrModelConversion::eYcbcr709;
+  conversion_info.ycbcrRange = impeller::vk::SamplerYcbcrRange::eItuFull;
+  conversion_info.components = {impeller::vk::ComponentSwizzle::eIdentity,
+                                impeller::vk::ComponentSwizzle::eIdentity,
+                                impeller::vk::ComponentSwizzle::eIdentity,
+                                impeller::vk::ComponentSwizzle::eIdentity};
+  conversion_info.xChromaOffset = impeller::vk::ChromaLocation::eCositedEven;
+  conversion_info.yChromaOffset = impeller::vk::ChromaLocation::eCositedEven;
+  conversion_info.chromaFilter = impeller::vk::Filter::eNearest;
+  conversion_info.forceExplicitReconstruction = false;
+  return context.GetYUVConversionLibrary()->GetConversion(conversion_chain);
+}
+
+bool EmbedderExternalTextureSourceVulkan::CreateTextureImageView(
+    const impeller::vk::Device& device,
+    FlutterVulkanTexture* embedder_desc,
+    const std::shared_ptr<impeller::YUVConversionVK>& yuv_conversion_wrapper) {
+  impeller::vk::StructureChain<impeller::vk::ImageViewCreateInfo,
+                               impeller::vk::SamplerYcbcrConversionInfo>
+      view_chain;
+  auto& view_info = view_chain.get();
+  view_info.image = texture_image_;
+  view_info.viewType = impeller::vk::ImageViewType::e2D;
+  view_info.format = static_cast<impeller::vk::Format>(embedder_desc->format);
+  view_info.subresourceRange.aspectMask =
+      impeller::vk::ImageAspectFlagBits::eColor;
+  view_info.subresourceRange.baseMipLevel = 0u;
+  view_info.subresourceRange.baseArrayLayer = 0u;
+  view_info.subresourceRange.levelCount = 1;
+  view_info.subresourceRange.layerCount = 1;
+
+  if (RequiresYCBCRConversion(
+          static_cast<impeller::vk::Format>(embedder_desc->format))) {
+    view_chain.get<impeller::vk::SamplerYcbcrConversionInfo>().conversion =
+        yuv_conversion_wrapper->GetConversion();
+  } else {
+    view_chain.unlink<impeller::vk::SamplerYcbcrConversionInfo>();
+  }
+  auto image_view = device.createImageViewUnique(view_info);
+  if (image_view.result != impeller::vk::Result::eSuccess) {
+    return false;
+  }
+  texture_image_view_ = std::move(image_view.value);
+  return true;
+}
+
+// |TextureSourceVK|
+EmbedderExternalTextureSourceVulkan::~EmbedderExternalTextureSourceVulkan() =
+    default;
+
+bool EmbedderExternalTextureSourceVulkan::IsValid() const {
+  return is_valid_;
+}
+
+// |TextureSourceVK|
+impeller::vk::Image EmbedderExternalTextureSourceVulkan::GetImage() const {
+  return texture_image_;
+}
+
+// |TextureSourceVK|
+impeller::vk::ImageView EmbedderExternalTextureSourceVulkan::GetImageView()
+    const {
+  return texture_image_view_.get();
+}
+
+// |TextureSourceVK|
+impeller::vk::ImageView
+EmbedderExternalTextureSourceVulkan::GetRenderTargetView() const {
+  return texture_image_view_.get();
+}
+
+// |TextureSourceVK|
+bool EmbedderExternalTextureSourceVulkan::IsSwapchainImage() const {
+  return is_swapchain_image_;
+}
+
+// |TextureSourceVK|
+std::shared_ptr<impeller::YUVConversionVK>
+EmbedderExternalTextureSourceVulkan::GetYUVConversion() const {
+  return needs_yuv_conversion_ ? yuv_conversion_ : nullptr;
+}
+
+}  // namespace flutter
---
+++ engine/src/flutter/shell/platform/embedder/embedder_external_texture_vulkan.h
@@ -0,0 +1,64 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef FLUTTER_SHELL_PLATFORM_EMBEDDER_EMBEDDER_EXTERNAL_TEXTURE_VULKAN_H_
+#define FLUTTER_SHELL_PLATFORM_EMBEDDER_EMBEDDER_EXTERNAL_TEXTURE_VULKAN_H_
+
+#include "flutter/common/graphics/texture.h"
+#include "flutter/fml/macros.h"
+#include "flutter/shell/platform/embedder/embedder.h"
+#include "include/core/SkTypes.h"
+#include "include/gpu/vk/VulkanTypes.h"
+#include "third_party/skia/include/core/SkSize.h"
+
+namespace flutter {
+
+class EmbedderExternalTextureVulkan : public flutter::Texture {
+ public:
+  using ExternalTextureCallback = std::function<
+      std::unique_ptr<FlutterVulkanTexture>(int64_t, size_t, size_t)>;
+  EmbedderExternalTextureVulkan(int64_t texture_identifier,
+                                const ExternalTextureCallback& callback);
+
+  ~EmbedderExternalTextureVulkan();
+
+ private:
+  const ExternalTextureCallback& external_texture_callback_;
+
+  sk_sp<DlImage> last_image_;
+
+  sk_sp<DlImage> ResolveTexture(int64_t texture_id,
+                                GrDirectContext* context,
+                                impeller::AiksContext* aiks_context,
+                                const SkISize& size);
+  sk_sp<DlImage> ResolveTextureSkia(int64_t texture_id,
+                                    GrDirectContext* context,
+                                    const SkISize& size);
+  sk_sp<DlImage> ResolveTextureImpeller(int64_t texture_id,
+                                        impeller::AiksContext* aiks_context,
+                                        const SkISize& size);
+
+  // |flutter::Texture|
+  void Paint(PaintContext& context,
+             const DlRect& bounds,
+             bool freeze,
+             const DlImageSampling sampling) override;
+
+  // |flutter::Texture|
+  void OnGrContextCreated() override;
+
+  // |flutter::Texture|
+  void OnGrContextDestroyed() override;
+
+  // |flutter::Texture|
+  void MarkNewFrameAvailable() override;
+
+  // |flutter::Texture|
+  void OnTextureUnregistered() override;
+
+  FML_DISALLOW_COPY_AND_ASSIGN(EmbedderExternalTextureVulkan);
+};
+}  // namespace flutter
+
+#endif  // FLUTTER_SHELL_PLATFORM_EMBEDDER_EMBEDDER_EXTERNAL_TEXTURE_VULKAN_H_
