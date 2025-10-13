e022f3e0d8cd0bb14c65179010b6fa0e5914316c
Benson Luk <97480502+b-luk@users.noreply.github.com>
2025-10-13 10:39:55 -0700

    Delete RegisterCodecsWithSkia() function from shell.cc
    Skia automatically registers codecs in SkCodec.cpp based on build flags.
    The registrations in shell.cc are a no-op.

--- engine/src/flutter/shell/common/shell.cc
+++ engine/src/flutter/shell/common/shell.cc
@@ -85,20 +85,6 @@ std::unique_ptr<Engine> CreateEngine(
                                   runtime_stage_backend);
 }
 
-void RegisterCodecsWithSkia() {
-  // These are in the order they will be attempted to be decoded from.
-  // If we have data to back it up, we can order these by "frequency used in
-  // the wild" for a very small performance bump, but for now we mirror the
-  // order Skia had them in.
-  SkCodecs::Register(SkPngDecoder::Decoder());
-  SkCodecs::Register(SkJpegDecoder::Decoder());
-  SkCodecs::Register(SkWebpDecoder::Decoder());
-  SkCodecs::Register(SkGifDecoder::Decoder());
-  SkCodecs::Register(SkBmpDecoder::Decoder());
-  SkCodecs::Register(SkWbmpDecoder::Decoder());
-  SkCodecs::Register(SkIcoDecoder::Decoder());
-}
-
 // Though there can be multiple shells, some settings apply to all components in
 // the process. These have to be set up before the shell or any of its
 // sub-components can be initialized. In a perfect world, this would be empty.
@@ -131,7 +117,6 @@ void PerformInitializationTasks(Settings& settings) {
     } else {
       FML_DLOG(INFO) << "Skia deterministic rendering is enabled.";
     }
-    RegisterCodecsWithSkia();
 
     if (settings.icu_initialization_required) {
       if (!settings.icu_data_path.empty()) {
