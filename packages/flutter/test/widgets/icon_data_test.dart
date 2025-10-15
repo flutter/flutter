f9849c6c7685aad2824f6ce70c1c88ba88e1f0bd
Justin McCandless <jmccandless@google.com>
2025-10-15 15:41:39 -0700

    Unnecessary Material imports

--- packages/flutter/test/widgets/icon_data_test.dart
+++ packages/flutter/test/widgets/icon_data_test.dart
@@ -3,7 +3,6 @@
 // found in the LICENSE file.
 
 import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 
