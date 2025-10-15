842e3d01140d7398b978e42f205eda69ac674309
Kishan Rathore <34465683+rkishan516@users.noreply.github.com>
2025-10-16 04:11:10 +0530

    Refactor: migrate fade upwards page transition builder to widgets (#175560)
    Changes:
    
    * Move FadeUpwardsPageTransitionsBuilder from
    `material/page_transitions_theme.dart` to
    `widget/page_transitions_builder.dart`
    
    part of: https://github.com/flutter/flutter/issues/172929
    
    ## Pre-launch Checklist
    
    - [x] I read the [Contributor Guide] and followed the process outlined
    there for submitting PRs.
    - [x] I read the [Tree Hygiene] wiki page, which explains my
    responsibilities.
    - [x] I read and followed the [Flutter Style Guide], including [Features
    we expect every widget to implement].
    - [x] I signed the [CLA].
    - [x] I listed at least one issue that this PR fixes in the description
    above.
    - [x] I updated/added relevant documentation (doc comments with `///`).
    - [x] I added new tests to check the change I am making, or this PR is
    [test-exempt].
    - [x] I followed the [breaking change policy] and added [Data Driven
    Fixes] where supported.
    - [x] All existing and new tests are passing.

--- packages/flutter/test/widgets/page_transitions_builder_test.dart
+++ packages/flutter/test/widgets/page_transitions_builder_test.dart
@@ -175,6 +175,153 @@ void main() {
     expect(find.text('Navigate'), findsNothing);
     expect(find.text('Second Page'), findsOneWidget);
   });
+
+  testWidgets('FadeUpwardsPageTransitionsBuilder test', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      Directionality(
+        textDirection: TextDirection.ltr,
+        child: Navigator(
+          onGenerateRoute: (RouteSettings settings) {
+            return _CustomPageRoute<void>(
+              settings: settings,
+              transitionsBuilder: const FadeUpwardsPageTransitionsBuilder(),
+              builder: (BuildContext context) {
+                if (settings.name == '/') {
+                  return ColoredBox(
+                    color: const Color(0xFF2196F3),
+                    child: Center(
+                      child: GestureDetector(
+                        onTap: () {
+                          Navigator.of(context).pushNamed('/second');
+                        },
+                        child: const Text(
+                          'Page 1',
+                          style: TextStyle(color: Color(0xFFFFFFFF), fontSize: 24),
+                        ),
+                      ),
+                    ),
+                  );
+                }
+                return const ColoredBox(
+                  color: Color(0xFF4CAF50),
+                  child: Center(
+                    child: Text('Page 2', style: TextStyle(color: Color(0xFFFFFFFF), fontSize: 24)),
+                  ),
+                );
+              },
+            );
+          },
+        ),
+      ),
+    );
+
+    final Offset widget1TopLeft = tester.getTopLeft(find.text('Page 1'));
+
+    await tester.tap(find.text('Page 1'));
+    await tester.pump();
+    await tester.pump(const Duration(milliseconds: 1));
+
+    FadeTransition widget2Opacity = tester
+        .element(find.text('Page 2'))
+        .findAncestorWidgetOfExactType<FadeTransition>()!;
+    Offset widget2TopLeft = tester.getTopLeft(find.text('Page 2'));
+
+    expect(widget1TopLeft.dx == widget2TopLeft.dx, true);
+    expect(widget1TopLeft.dy < widget2TopLeft.dy, true);
+    expect(widget2Opacity.opacity.value < 0.01, true);
+
+    await tester.pump(const Duration(milliseconds: 300));
+
+    expect(find.text('Page 1'), findsNothing);
+    expect(find.text('Page 2'), isOnstage);
+
+    tester.state<NavigatorState>(find.byType(Navigator)).pop();
+    await tester.pump();
+    await tester.pump(const Duration(milliseconds: 1));
+
+    widget2Opacity = tester
+        .element(find.text('Page 2'))
+        .findAncestorWidgetOfExactType<FadeTransition>()!;
+    widget2TopLeft = tester.getTopLeft(find.text('Page 2'));
+
+    expect(widget1TopLeft.dy < widget2TopLeft.dy, true);
+    expect(widget2Opacity.opacity.value < 1.0, true);
+
+    await tester.pump(const Duration(milliseconds: 300));
+
+    expect(find.text('Page 1'), isOnstage);
+    expect(find.text('Page 2'), findsNothing);
+  });
+
+  testWidgets(
+    'FadeUpwardsPageTransitionsBuilder test with Material PageTransitionTheme',
+    (WidgetTester tester) async {
+      await tester.pumpWidget(
+        MaterialApp(
+          home: const Material(child: Text('Page 1')),
+          theme: ThemeData(
+            pageTransitionsTheme: const PageTransitionsTheme(
+              builders: <TargetPlatform, PageTransitionsBuilder>{
+                TargetPlatform.android: FadeUpwardsPageTransitionsBuilder(),
+              },
+            ),
+          ),
+          routes: <String, WidgetBuilder>{
+            '/next': (BuildContext context) {
+              return const Material(child: Text('Page 2'));
+            },
+          },
+        ),
+      );
+
+      final Offset widget1TopLeft = tester.getTopLeft(find.text('Page 1'));
+
+      tester.state<NavigatorState>(find.byType(Navigator)).pushNamed('/next');
+      await tester.pump();
+      await tester.pump(const Duration(milliseconds: 1));
+
+      FadeTransition widget2Opacity = tester
+          .element(find.text('Page 2'))
+          .findAncestorWidgetOfExactType<FadeTransition>()!;
+      Offset widget2TopLeft = tester.getTopLeft(find.text('Page 2'));
+      final Size widget2Size = tester.getSize(find.text('Page 2'));
+
+      // Android transition is vertical only.
+      expect(widget1TopLeft.dx == widget2TopLeft.dx, true);
+      // Page 1 is above page 2 mid-transition.
+      expect(widget1TopLeft.dy < widget2TopLeft.dy, true);
+      // Animation begins 3/4 of the way up the page.
+      expect(widget2TopLeft.dy < widget2Size.height / 4.0, true);
+      // Animation starts with page 2 being near transparent.
+      expect(widget2Opacity.opacity.value < 0.01, true);
+
+      await tester.pump(const Duration(milliseconds: 300));
+
+      // Page 2 covers page 1.
+      expect(find.text('Page 1'), findsNothing);
+      expect(find.text('Page 2'), isOnstage);
+
+      tester.state<NavigatorState>(find.byType(Navigator)).pop();
+      await tester.pump();
+      await tester.pump(const Duration(milliseconds: 1));
+
+      widget2Opacity = tester
+          .element(find.text('Page 2'))
+          .findAncestorWidgetOfExactType<FadeTransition>()!;
+      widget2TopLeft = tester.getTopLeft(find.text('Page 2'));
+
+      // Page 2 starts to move down.
+      expect(widget1TopLeft.dy < widget2TopLeft.dy, true);
+      // Page 2 starts to lose opacity.
+      expect(widget2Opacity.opacity.value < 1.0, true);
+
+      await tester.pump(const Duration(milliseconds: 300));
+
+      expect(find.text('Page 1'), isOnstage);
+      expect(find.text('Page 2'), findsNothing);
+    },
+    variant: TargetPlatformVariant.only(TargetPlatform.android),
+  );
 }
 
 class _CustomPageRoute<T> extends PageRoute<T> {
