30273da9fcde8bd0beb153f44650e9a136cb10b7
Mouad Debbar <mdebbar@google.com>
2025-10-15 23:12:10 -0400

    CLOSE

--- dev/bots/service_worker_test.dart
+++ dev/bots/service_worker_test.dart
@@ -87,7 +87,7 @@ self.addEventListener('fetch', (event) => {
     _requestedPaths.clear();
     print('== RELOADING PAGE ==');
     await server.chrome.reloadPage();
-    await _waitForAppToRequest(server, 'flutter_service_worker.js');
+    await _waitForAppToRequest(server, 'CLOSE');
 
     expect(
       _requestedPaths,
