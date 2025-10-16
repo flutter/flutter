9b66e8879bb07ef08e1ad779cb9c66585908e2a5
Renzo Olivares <roliv@google.com>
2025-10-16 10:19:56 -0700

    Address nits

--- packages/flutter/test/widgets/selectable_region_test.dart
+++ packages/flutter/test/widgets/selectable_region_test.dart
@@ -50,7 +50,7 @@ void main() {
     await TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger.handlePlatformMessage(
       'flutter/lifecycle',
       message,
-      (_) {},
+      (ByteData? data) {},
     );
   }
 
