6896e8f002b20203b97155d88b9831e6d822dd0f
David Hicks <davidhicks980@gmail.com>
2025-10-20 06:08:36 -0400

    fix problems

--- packages/flutter/test/material/menu_anchor_test.dart
+++ packages/flutter/test/material/menu_anchor_test.dart
@@ -4549,7 +4549,7 @@ void main() {
         // Test expanded state.
         await tester.tap(find.text('ABC'));
         await tester.pump();
-        var expandedMatcher = hasSemantics(
+        final Matcher expandedMatcher = hasSemantics(
           TestSemantics.root(
             children: <TestSemantics>[
               TestSemantics(
@@ -4629,7 +4629,7 @@ void main() {
         await tester.tap(find.text('ABC'));
         await tester.pump();
 
-        var collapsedMatcher = hasSemantics(
+        final Matcher collapsedMatcher = hasSemantics(
           TestSemantics.root(
             children: <TestSemantics>[
               TestSemantics(
