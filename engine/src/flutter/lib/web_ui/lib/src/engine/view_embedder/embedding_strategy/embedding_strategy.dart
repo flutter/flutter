d798069aa3a2689fd19bab2f0927f56821fc3557
Chun-Heng Tai <chtai@google.com>
2025-10-20 14:23:27 -0700

    comment

--- engine/src/flutter/lib/web_ui/lib/src/engine/view_embedder/embedding_strategy/embedding_strategy.dart
+++ engine/src/flutter/lib/web_ui/lib/src/engine/view_embedder/embedding_strategy/embedding_strategy.dart
@@ -27,10 +27,12 @@ abstract class EmbeddingStrategy {
     }
   }
 
-  /// Sets locale for the embedded view.
+  /// Sets the locale for the embedded view.
   ///
-  /// This is typically called from the framework side after it resolve
-  /// the application locale.
+  /// This method is typically called by the Flutter framework after it has
+  /// resolved the application's locale. It configures the embedded view to
+  /// reflect the given locale, which is important for accessibility and for
+  /// the browser to select appropriate fonts and other locale-specific resources.
   void setLocale(ui.Locale locale);
 
   /// The DOM element in which the Flutter view is embedded.
