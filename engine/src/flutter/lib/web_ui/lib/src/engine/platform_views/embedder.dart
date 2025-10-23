a19152b8882080d4fa4b2a3c8e086755da012cb2
Mouad Debbar <mdebbar@google.com>
2025-10-23 13:22:16 -0400

    [web] Refactor LazyPath and separate immutable paths from path builders

--- engine/src/flutter/lib/web_ui/lib/src/engine/platform_views/embedder.dart
+++ engine/src/flutter/lib/web_ui/lib/src/engine/platform_views/embedder.dart
@@ -228,13 +228,13 @@ class PlatformViewEmbedder {
                   '${rrect.bottom}px ${rrect.left}px '
                   'round ${rrect.blRadiusX}px)';
             } else {
-              final path = ui.Path() as LayerPath;
+              final path = ui.Path() as LazyPath;
               path.addRRect(mutator.rrect!);
-              clipView.style.clipPath = 'path("${path.toSvgString()}")';
+              clipView.style.clipPath = 'path("${path.builtPath.toSvgString()}")';
+              path.dispose();
             }
           } else if (mutator.path != null) {
-            final path = (mutator.path! as LazyPath).builtPath as LayerPath;
-            clipView.style.clipPath = 'path("${path.toSvgString()}")';
+            clipView.style.clipPath = 'path("${mutator.path!.builtPath.toSvgString()}")';
           }
           _resetAnchor(clipView);
           head = clipView;
@@ -692,7 +692,8 @@ class Mutator {
   const Mutator.clipRect(ui.Rect rect) : this._(MutatorType.clipRect, rect, null, null, null, null);
   const Mutator.clipRRect(ui.RRect rrect)
     : this._(MutatorType.clipRRect, null, rrect, null, null, null);
-  const Mutator.clipPath(ui.Path path) : this._(MutatorType.clipPath, null, null, path, null, null);
+  const Mutator.clipPath(LazyPath path)
+    : this._(MutatorType.clipPath, null, null, path, null, null);
   const Mutator.transform(Matrix4 matrix)
     : this._(MutatorType.transform, null, null, null, matrix, null);
   const Mutator.opacity(int alpha) : this._(MutatorType.opacity, null, null, null, null, alpha);
@@ -702,7 +703,7 @@ class Mutator {
   final MutatorType type;
   final ui.Rect? rect;
   final ui.RRect? rrect;
-  final ui.Path? path;
+  final LazyPath? path;
   final Matrix4? matrix;
   final int? alpha;
 
@@ -760,7 +761,7 @@ class MutatorsStack extends Iterable<Mutator> {
     pushClipRRect(rsuperellipse.toApproximateRRect());
   }
 
-  void pushClipPath(ui.Path path) {
+  void pushClipPath(LazyPath path) {
     _mutators.add(Mutator.clipPath(path));
   }
 
