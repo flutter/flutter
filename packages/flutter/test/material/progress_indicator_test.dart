c25906cb4ffd38fa19f7ee5f658ee44101aeaeb5
Ahmed Mohamed Sameh <ahmedsameha1@gmail.com>
2025-10-26 15:33:51 +0300

    Make sure that a CircularProgressIndicator doesn't crash in 0x0 environment

--- packages/flutter/test/material/progress_indicator_test.dart
+++ packages/flutter/test/material/progress_indicator_test.dart
@@ -1969,6 +1969,15 @@ void main() {
       paints..arc(startAngle: 1.5707963267948966, sweepAngle: 0.001),
     );
   });
+
+  testWidgets('CircularProgressIndicator does not crash at zero area', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: Center(child: SizedBox.shrink(child: CircularProgressIndicator())),
+      ),
+    );
+    expect(tester.getSize(find.byType(CircularProgressIndicator)), Size.zero);
+  });
 }
 
 class _RefreshProgressIndicatorGolden extends StatefulWidget {
