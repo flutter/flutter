1c84032fea41e2956a462d6bad3852a6017456cc
Reid Baker <reidbaker@google.com>
2025-10-27 14:19:36 -0400

    Revert "Resolve resolve native Flutter dependencies in Android Studio (#167332)"
    This reverts commit 0c0406a6158eb07d28a70350a22801ee8f6870c9.

--- packages/flutter_tools/gradle/src/main/kotlin/FlutterPlugin.kt
+++ packages/flutter_tools/gradle/src/main/kotlin/FlutterPlugin.kt
@@ -23,7 +23,6 @@ import org.gradle.api.artifacts.dsl.DependencyHandler
 import org.gradle.api.file.Directory
 import org.gradle.api.plugins.PluginContainer
 import org.gradle.api.tasks.Copy
-import org.gradle.api.tasks.TaskInstantiationException
 import org.gradle.api.tasks.TaskProvider
 import org.gradle.api.tasks.bundling.Jar
 import org.gradle.internal.os.OperatingSystem
@@ -101,7 +100,6 @@ class FlutterPlugin : Plugin<Project> {
             repositories.maven {
                 url = uri(repository!!)
             }
-            maybeAddAndroidStudioNativeConfiguration(plugins, dependencies)
         }
 
         project.apply {
@@ -329,23 +327,19 @@ class FlutterPlugin : Plugin<Project> {
     }
 
     private fun addTaskForLockfileGeneration(rootProject: Project) {
-        try {
-            rootProject.tasks.register("generateLockfiles") {
-                doLast {
-                    rootProject.subprojects.forEach { subproject ->
-                        val gradlew: String =
-                            getExecutableNameForPlatform("${rootProject.projectDir}/gradlew")
-                        val execOps = rootProject.serviceOf<ExecOperations>()
-                        execOps.exec {
-                            workingDir(rootProject.projectDir)
-                            executable(gradlew)
-                            args(":${subproject.name}:dependencies", "--write-locks")
-                        }
+        rootProject.tasks.register("generateLockfiles") {
+            doLast {
+                rootProject.subprojects.forEach { subproject ->
+                    val gradlew: String =
+                        getExecutableNameForPlatform("${rootProject.projectDir}/gradlew")
+                    val execOps = rootProject.serviceOf<ExecOperations>()
+                    execOps.exec {
+                        workingDir(rootProject.projectDir)
+                        executable(gradlew)
+                        args(":${subproject.name}:dependencies", "--write-locks")
                     }
                 }
             }
-        } catch (e: TaskInstantiationException) {
-            // ignored
         }
     }
 
@@ -821,19 +815,4 @@ class FlutterPlugin : Plugin<Project> {
      * This property is set by Android Studio when it invokes a Gradle task.
      */
     private fun isInvokedFromAndroidStudio(): Boolean = project?.hasProperty("android.injected.invoked.from.ide") == true
-
-    private fun shouldAddAndroidStudioNativeConfiguration(plugins: PluginContainer): Boolean =
-        plugins.hasPlugin("com.android.application") && isInvokedFromAndroidStudio()
-
-    private fun maybeAddAndroidStudioNativeConfiguration(
-        plugins: PluginContainer,
-        dependencies: DependencyHandler
-    ) {
-        if (shouldAddAndroidStudioNativeConfiguration(plugins)) {
-            dependencies.add("compileOnly", "io.flutter:flutter_embedding_debug:$engineVersion")
-            dependencies.add("compileOnly", "io.flutter:armeabi_v7a_debug:$engineVersion")
-            dependencies.add("compileOnly", "io.flutter:arm64_v8a_debug:$engineVersion")
-            dependencies.add("compileOnly", "io.flutter:x86_64_debug:$engineVersion")
-        }
-    }
 }
