a5a8310f71150a30dee2a4b288ea5af769a72476
Reid Baker <reidbaker@google.com>
2025-10-27 14:41:26 -0400

    Remove unused dependencies

--- packages/flutter_tools/gradle/src/main/kotlin/FlutterPlugin.kt
+++ packages/flutter_tools/gradle/src/main/kotlin/FlutterPlugin.kt
@@ -19,9 +19,7 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.Task
 import org.gradle.api.UnknownTaskException
-import org.gradle.api.artifacts.dsl.DependencyHandler
 import org.gradle.api.file.Directory
-import org.gradle.api.plugins.PluginContainer
 import org.gradle.api.tasks.Copy
 import org.gradle.api.tasks.TaskProvider
 import org.gradle.api.tasks.bundling.Jar
