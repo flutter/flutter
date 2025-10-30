93ec125407116100a4a2e66e4df91df01fc8d141
Camille Simon <camillesimon@google.com>
2025-10-30 10:19:36 -0700

    format

--- engine/src/flutter/shell/platform/android/test/io/flutter/embedding/engine/FlutterShellArgsTest.java
+++ engine/src/flutter/shell/platform/android/test/io/flutter/embedding/engine/FlutterShellArgsTest.java
@@ -5,7 +5,6 @@
 package io.flutter.embedding.engine;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
@@ -17,47 +16,50 @@ import org.junit.Test;
 
 public class FlutterShellArgsTest {
 
-    @Test
-    public void allFlags_containsAllFlags() {
-        // Count the number of declared flags in FlutterShellArgs.
-        int declaredFlagsCount = 0;
-        for (Field field : FlutterShellArgs.class.getDeclaredFields()) {
-            if (FlutterShellArgs.Flag.class.isAssignableFrom(field.getType()) && 
-                Modifier.isStatic(field.getModifiers()) &&
-                Modifier.isFinal(field.getModifiers())) {
-                declaredFlagsCount++;
-            }
-        }
-
-        // Check that the number of declared flags matches the size of ALL_FLAGS.
-        assertEquals("If you are adding a new Flag to FlutterShellArgs, please make sure it is added to ALL_FLAGS as well. Otherwise, the flag will be silently ignored when specified.", 
-                     declaredFlagsCount, 
-                     FlutterShellArgs.ALL_FLAGS.size());
+  @Test
+  public void allFlags_containsAllFlags() {
+    // Count the number of declared flags in FlutterShellArgs.
+    int declaredFlagsCount = 0;
+    for (Field field : FlutterShellArgs.class.getDeclaredFields()) {
+      if (FlutterShellArgs.Flag.class.isAssignableFrom(field.getType())
+          && Modifier.isStatic(field.getModifiers())
+          && Modifier.isFinal(field.getModifiers())) {
+        declaredFlagsCount++;
+      }
     }
 
-        @Test
-    public void containsCommandLineArgument_returnsTrueWhenValidArgSpecified() {
-        assertTrue(FlutterShellArgs.containsCommandLineArgument("--enable-software-rendering"));
-    }
+    // Check that the number of declared flags matches the size of ALL_FLAGS.
+    assertEquals(
+        "If you are adding a new Flag to FlutterShellArgs, please make sure it is added to ALL_FLAGS as well. Otherwise, the flag will be silently ignored when specified.",
+        declaredFlagsCount,
+        FlutterShellArgs.ALL_FLAGS.size());
+  }
 
-    @Test
-    public void containsCommandLineArgument_returnsFalseWhenInvalidArgSpecified() {
-        assertFalse("Should return false for an invalid command line argument",
-                    FlutterShellArgs.containsCommandLineArgument("--invalid-argument"));
-    }
+  @Test
+  public void containsCommandLineArgument_returnsTrueWhenValidArgSpecified() {
+    assertTrue(FlutterShellArgs.containsCommandLineArgument("--enable-software-rendering"));
+  }
 
-    @Test
-    public void getFlagByMetaDataKey_returnsExpectedFlagWhenValidKeySpecified() {
-        FlutterShellArgs.Flag flag = FlutterShellArgs.getFlagByMetaDataKey("io.flutter.embedding.android.EnableSoftwareRendering");
-        assertNotNull(flag);
-        assertEquals(
-                     "--enable-software-rendering", 
-                     flag.commandLineArgument);
-    }
+  @Test
+  public void containsCommandLineArgument_returnsFalseWhenInvalidArgSpecified() {
+    assertFalse(
+        "Should return false for an invalid command line argument",
+        FlutterShellArgs.containsCommandLineArgument("--invalid-argument"));
+  }
 
-    @Test
-    public void getFlagByMetaDataKey_returnsNullWhenInvalidKeySpecified() {
-        FlutterShellArgs.Flag flag = FlutterShellArgs.getFlagByMetaDataKey("io.flutter.embedding.android.InvalidMetaDataKey");
-        assertNull("Should return null for an invalid meta-data key", flag);
-    }
+  @Test
+  public void getFlagByMetaDataKey_returnsExpectedFlagWhenValidKeySpecified() {
+    FlutterShellArgs.Flag flag =
+        FlutterShellArgs.getFlagByMetaDataKey(
+            "io.flutter.embedding.android.EnableSoftwareRendering");
+    assertNotNull(flag);
+    assertEquals("--enable-software-rendering", flag.commandLineArgument);
+  }
+
+  @Test
+  public void getFlagByMetaDataKey_returnsNullWhenInvalidKeySpecified() {
+    FlutterShellArgs.Flag flag =
+        FlutterShellArgs.getFlagByMetaDataKey("io.flutter.embedding.android.InvalidMetaDataKey");
+    assertNull("Should return null for an invalid meta-data key", flag);
+  }
 }
