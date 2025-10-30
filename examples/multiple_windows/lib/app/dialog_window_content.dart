9e82f9064db2359098ed1c2dabb0614f86dbc14b
Matt Kosarek <matt.kosarek@canonical.com>
2025-10-30 13:17:39 -0400

    Implement dialogs of dialogs in the multi window reference app

--- examples/multiple_windows/lib/app/dialog_window_content.dart
+++ examples/multiple_windows/lib/app/dialog_window_content.dart
@@ -28,6 +28,26 @@ class DialogWindowContent extends StatelessWidget {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
+                ElevatedButton(
+                  onPressed: () {
+                    final UniqueKey key = UniqueKey();
+                    windowManager.add(
+                      KeyedWindow(
+                        key: key,
+                        controller: DialogWindowController(
+                          preferredSize: windowSettings.dialogSize,
+                          delegate: CallbackDialogWindowControllerDelegate(
+                            onDestroyed: () => windowManager.remove(key),
+                          ),
+                          parent: window,
+                          title: 'Dialog',
+                        ),
+                      ),
+                    );
+                  },
+                  child: const Text('Create Modal Dialog'),
+                ),
+                const SizedBox(height: 20),
                 ListenableBuilder(
                   listenable: window,
                   builder: (BuildContext context, Widget? _) {
@@ -42,7 +62,7 @@ class DialogWindowContent extends StatelessWidget {
                     );
                   },
                 ),
-                const SizedBox(height: 16.0),
+                const SizedBox(height: 20),
                 ElevatedButton(
                   onPressed: () {
                     window.destroy();
