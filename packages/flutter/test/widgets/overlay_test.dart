a01dd7c90ac7f1f3de430851ac43695e035114df
rkishan516 <rkishan516@gmail.com>
2025-10-31 08:12:17 +0530

    fix: add depend method with option to cleanup

--- packages/flutter/test/widgets/overlay_test.dart
+++ packages/flutter/test/widgets/overlay_test.dart
@@ -1997,10 +1997,7 @@ class FakeBuildContext extends BuildContext {
   }
 
   @override
-  T? dependOnInheritedWidgetOfExactType<T extends InheritedWidget>({
-    Object? aspect,
-    bool trackForCleanup = false,
-  }) {
+  T? dependOnInheritedWidgetOfExactType<T extends InheritedWidget>({Object? aspect}) {
     throw UnimplementedError();
   }
 
