a3e94ebbeeef9021d8a1b9f275cdf9e1227795a2
Aaron Clarke <aaclarke@google.com>
2025-11-03 10:12:24 -0800

    refactored imagecodec::decode args

--- engine/src/flutter/lib/ui/painting/image_decoder.h
+++ engine/src/flutter/lib/ui/painting/image_decoder.h
@@ -34,15 +34,19 @@ class ImageDecoder {
 
   using ImageResult = std::function<void(sk_sp<DlImage>, std::string)>;
 
+  struct Options {
+    uint32_t target_width;
+    uint32_t target_height;
+    int32_t destination_format;
+  };
+
   // Takes an image descriptor and returns a handle to a texture resident on the
   // GPU. All image decompression and resizes are done on a worker thread
   // concurrently. Texture upload is done on the IO thread and the result
   // returned back on the UI thread. On error, the texture is null but the
   // callback is guaranteed to return on the UI thread.
   virtual void Decode(fml::RefPtr<ImageDescriptor> descriptor,
-                      uint32_t target_width,
-                      uint32_t target_height,
-                      int32_t destination_format,
+                      const Options& options,
                       const ImageResult& result) = 0;
 
   fml::TaskRunnerAffineWeakPtr<ImageDecoder> GetWeakPtr() const;
