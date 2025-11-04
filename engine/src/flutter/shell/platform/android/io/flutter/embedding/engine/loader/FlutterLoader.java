1a737b1cefc549f371e51a5faa0e2179527e1329
Camille Simon <camillesimon@google.com>
2025-11-04 08:49:42 -0800

    Fix annotation

--- engine/src/flutter/shell/platform/android/io/flutter/embedding/engine/loader/FlutterLoader.java
+++ engine/src/flutter/shell/platform/android/io/flutter/embedding/engine/loader/FlutterLoader.java
@@ -4,6 +4,7 @@
 
 package io.flutter.embedding.engine.loader;
 
+import android.annotation.SuppressLint;
 import android.app.ActivityManager;
 import android.content.Context;
 import android.content.pm.ApplicationInfo;
