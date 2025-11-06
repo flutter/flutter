3ca0b33b76122f3dc48dfa60ca740178ee7a83db
Aaron Clarke <aaclarke@google.com>
2025-11-06 15:56:31 -0800

    moved to driver

---
+++ dev/integration_tests/high_bitrate_images/test_driver/integration_test.dart
@@ -0,0 +1,9 @@
+import 'package:integration_test/integration_test_driver.dart';
+import 'package:flutter_goldens/flutter_goldens.dart';
+import 'package:flutter_test/flutter_test.dart' show goldenFileComparator;
+
+// Future<void> main() async {
+//   await testExecutable(integrationDriver);
+// }
+
+Future<void> main() async => integrationDriver();
