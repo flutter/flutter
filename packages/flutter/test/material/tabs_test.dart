de47b7f3ecb4701370e480075e62916b8c5945f4
Ahmed Mohamed Sameh <ahmedsameha1@gmail.com>
2025-11-12 22:17:55 +0200

    Make sure that a TabBarView doesn't crash in 0x0 environment (#178202)
    This is my attempt to handle
    https://github.com/flutter/flutter/issues/6537 for the TabBarView
    widget.

--- packages/flutter/test/material/tabs_test.dart
+++ packages/flutter/test/material/tabs_test.dart
@@ -9347,6 +9347,24 @@ void main() {
     expect(tester.takeException(), isNull);
   });
 
+  testWidgets('TabBarView does not crash at zero area', (WidgetTester tester) async {
+    tester.view.physicalSize = Size.zero;
+    final TabController controller = TabController(length: 2, vsync: tester);
+    addTearDown(tester.view.reset);
+    addTearDown(controller.dispose);
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Center(
+          child: TabBarView(controller: controller, children: const <Widget>[Text('X'), Text('Y')]),
+        ),
+      ),
+    );
+    expect(tester.getSize(find.byType(TabBarView)), Size.zero);
+    controller.animateTo(1);
+    await tester.pump();
+    await tester.pumpAndSettle();
+  });
+
   testWidgets('TabPageSelectorIndicator does not crash at zero area', (WidgetTester tester) async {
     await tester.pumpWidget(
       const MaterialApp(
