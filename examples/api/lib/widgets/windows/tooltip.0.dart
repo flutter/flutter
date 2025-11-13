f95365efe75c1c3beedab75ca7a26e2385f87ecf
Matt Kosarek <matt.kosarek@canonical.com>
2025-11-13 10:57:52 -0500

    Add proper license headers

--- examples/api/lib/widgets/windows/tooltip.0.dart
+++ examples/api/lib/widgets/windows/tooltip.0.dart
@@ -1,3 +1,7 @@
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 // TODO(mattkae): remove invalid_use_of_internal_member ignore comment when this API is stable.
 // ignore_for_file: invalid_use_of_internal_member
 import 'package:flutter/material.dart';
--- packages/flutter/test/widgets/window_positioner_test.dart
+++ packages/flutter/test/widgets/window_positioner_test.dart
@@ -1,6 +1,7 @@
 // Copyright 2014 The Flutter Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+
 // ignore_for_file: invalid_use_of_internal_member
 
 import 'dart:ui';
