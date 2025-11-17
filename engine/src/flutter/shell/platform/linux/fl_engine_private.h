afb77dc6a88407a03ede81357680e52af30e978c
Jonathan Gilbert <logic@deltaq.org>
2025-11-16 21:52:27 -0600

    Updated the wording of the GError **error parameter in the documentation of all applicable functions.

--- engine/src/flutter/shell/platform/linux/fl_engine_private.h
+++ engine/src/flutter/shell/platform/linux/fl_engine_private.h
@@ -97,8 +97,8 @@ FlDisplayMonitor* fl_engine_get_display_monitor(FlEngine* engine);
  * @engine: an #FlEngine.
  * @error: (allow-none): #GError location to store the error occurring, or %NULL
  * to ignore. If `error` is not %NULL, `*error` must be initialized (typically
- * %NULL, but an error from a previous call using GLib error handling is explicitly
- * valid).
+ * %NULL, but an error from a previous call using GLib error handling is
+ * explicitly valid).
  *
  * Starts the Flutter engine.
  *
@@ -168,7 +168,9 @@ FlutterViewId fl_engine_add_view(FlEngine* engine,
  * @engine: an #FlEngine.
  * @result: a #GAsyncResult.
  * @error: (allow-none): #GError location to store the error occurring, or %NULL
- * to ignore. If `error` is not %NULL, `*error` must not be uninitialized.
+ * to ignore. If `error` is not %NULL, `*error` must be initialized (typically
+ * %NULL, but an error from a previous call using GLib error handling is
+ * explicitly valid).
  *
  * Completes request started with fl_engine_add_view().
  *
@@ -212,7 +214,9 @@ void fl_engine_remove_view(FlEngine* engine,
  * @engine: an #FlEngine.
  * @result: a #GAsyncResult.
  * @error: (allow-none): #GError location to store the error occurring, or %NULL
- * to ignore. If `error` is not %NULL, `*error` must not be uninitialized.
+ * to ignore. If `error` is not %NULL, `*error` must be initialized (typically
+ * %NULL, but an error from a previous call using GLib error handling is
+ * explicitly valid).
  *
  * Completes request started with fl_engine_remove_view().
  *
@@ -424,7 +428,9 @@ void fl_engine_send_key_event(FlEngine* engine,
  * @result: a #GAsyncResult.
  * @handled: location to write if this event was handled by the engine.
  * @error: (allow-none): #GError location to store the error occurring, or %NULL
- * to ignore. If `error` is not %NULL, `*error` must not be uninitialized.
+ * to ignore. If `error` is not %NULL, `*error` must be initialized (typically
+ * %NULL, but an error from a previous call using GLib error handling is
+ * explicitly valid).
  *
  * Completes request started with fl_engine_send_key_event().
  *
@@ -455,7 +461,9 @@ void fl_engine_dispatch_semantics_action(FlEngine* engine,
  * @handle: handle that was provided in #FlEnginePlatformMessageHandler.
  * @response: (allow-none): response to send or %NULL for an empty response.
  * @error: (allow-none): #GError location to store the error occurring, or %NULL
- * to ignore. If `error` is not %NULL, `*error` must not be uninitialized.
+ * to ignore. If `error` is not %NULL, `*error` must be initialized (typically
+ * %NULL, but an error from a previous call using GLib error handling is
+ * explicitly valid).
  *
  * Responds to a platform message.
  *
@@ -491,7 +499,9 @@ void fl_engine_send_platform_message(FlEngine* engine,
  * @engine: an #FlEngine.
  * @result: a #GAsyncResult.
  * @error: (allow-none): #GError location to store the error occurring, or %NULL
- * to ignore. If `error` is not %NULL, `*error` must not be uninitialized.
+ * to ignore. If `error` is not %NULL, `*error` must be initialized (typically
+ * %NULL, but an error from a previous call using GLib error handling is
+ * explicitly valid).
  *
  * Completes request started with fl_engine_send_platform_message().
  *
