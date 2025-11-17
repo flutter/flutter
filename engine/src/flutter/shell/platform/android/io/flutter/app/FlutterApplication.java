b40950f81b2fe2106146f50a36660e8c134f6d1d
Mohellebi abdessalem <116356835+AbdeMohlbi@users.noreply.github.com>
2025-11-17 16:34:01 +0100

    Update engine/src/flutter/shell/platform/android/io/flutter/app/FlutterApplication.java
    Co-authored-by: Reid Baker <1063596+reidbaker@users.noreply.github.com>

--- engine/src/flutter/shell/platform/android/io/flutter/app/FlutterApplication.java
+++ engine/src/flutter/shell/platform/android/io/flutter/app/FlutterApplication.java
@@ -12,7 +12,7 @@ import android.app.Application;
  * Application} instead.
  *
  * <p>For more information on the removal of Flutter's v1 Android embedding, see: <a
- * href="https://docs.flutter.dev/release/breaking-changes/v1-android-embedding">...</a>.
+ * href="https://docs.flutter.dev/release/breaking-changes/v1-android-embedding">https://docs.flutter.dev/release/breaking-changes/v1-android-embedding</a>.
  */
 @Deprecated
 public class FlutterApplication extends Application {}
