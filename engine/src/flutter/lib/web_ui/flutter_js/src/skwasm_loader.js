8de32a89f73b3cebe6f0f58f31142a6067d32d63
Jackson Gardner <jacksongardner@google.com>
2025-11-18 10:20:29 -0800

    Disable multi-threading for wimp.

--- engine/src/flutter/lib/web_ui/flutter_js/src/skwasm_loader.js
+++ engine/src/flutter/lib/web_ui/flutter_js/src/skwasm_loader.js
@@ -18,7 +18,9 @@ export const loadSkwasm = async (deps, config, browserEnvironment, baseUrl) => {
   const wasmInstantiator = createWasmInstantiator(resolveUrlWithSegments(baseUrl, `${fileStem}.wasm`));
   const skwasm = await import(skwasmUrl);
   return await skwasm.default({
-    skwasmSingleThreaded: !browserEnvironment.crossOriginIsolated || config.forceSingleThreadedSkwasm,
+    // As of right now, multi-threaded wimp is unstable and crashy.
+    // See https://github.com/flutter/flutter/issues/178749 for more details.
+    skwasmSingleThreaded: config.enableWimp || !browserEnvironment.crossOriginIsolated || config.forceSingleThreadedSkwasm,
     instantiateWasm: wasmInstantiator,
     locateFile: (filename, scriptDirectory) => {
       // The wasm workers API has a separate .ww.js file that bootstraps the
