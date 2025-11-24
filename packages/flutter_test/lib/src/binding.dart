38c248b17a406609d465e537ec7766bd93d59b7f
Flop <38378650+hgraceb@users.noreply.github.com>
2025-11-25 05:24:54 +0800

    Disposes test restoration manager when accessed by bindings (#176519)
    Fixes #169119
    
    ## Details
    
    The error caused by drag is due to the reference to
    `ServicesBinding.instance.restorationManager` in the
    `ScrollableState.saveOffset` method.
    
    
    https://github.com/flutter/flutter/blob/b76ff07eba7d829898612120a202a5a2ad398f19/packages/flutter/lib/src/widgets/scrollable.dart#L648-L656
    
    The TestWidgetsFlutterBinding now only cleans up the
    `restorationManager` when `reset` is called. Therefore, we need to find
    an appropriate place to clean up the `restorationManager`.
    
    
    https://github.com/flutter/flutter/blob/b76ff07eba7d829898612120a202a5a2ad398f19/packages/flutter_test/lib/src/binding.dart#L239-L263
    
    
    https://github.com/flutter/flutter/blob/b76ff07eba7d829898612120a202a5a2ad398f19/packages/flutter_test/lib/src/widget_tester.dart#L188-L193
    
    ## Pre-launch Checklist
    
    - [x] I read the [Contributor Guide] and followed the process outlined
    there for submitting PRs.
    - [x] I read the [Tree Hygiene] wiki page, which explains my
    responsibilities.
    - [x] I read and followed the [Flutter Style Guide], including [Features
    we expect every widget to implement].
    - [x] I signed the [CLA].
    - [x] I listed at least one issue that this PR fixes in the description
    above.
    - [ ] I updated/added relevant documentation (doc comments with `///`).
    - [x] I added new tests to check the change I am making, or this PR is
    [test-exempt].
    - [ ] I followed the [breaking change policy] and added [Data Driven
    Fixes] where supported.
    - [x] All existing and new tests are passing.
    
    ---------
    
    Co-authored-by: Victor Sanni <victorsanniay@gmail.com>

--- packages/flutter_test/lib/src/binding.dart
+++ packages/flutter_test/lib/src/binding.dart
@@ -1067,6 +1067,8 @@ abstract class TestWidgetsFlutterBinding extends BindingBase
         Container(key: UniqueKey(), child: _postTestMessage),
       ); // Unmount any remaining widgets.
       await pump();
+      _restorationManager?.dispose();
+      _restorationManager = null;
       if (registerTestTextInput) {
         _testTextInput.unregister();
       }
