01fab7e68d4494533c8f08246ea7fe96db376b60
Elijah Okoroh <okorohelijah@google.com>
2025-11-24 14:23:13 -0800

    update podspec platform

--- dev/devicelab/lib/tasks/plugin_tests.dart
+++ dev/devicelab/lib/tasks/plugin_tests.dart
@@ -500,7 +500,7 @@ end
     }
     final String versionString = target == 'ios'
         ? "s.platform = :ios, '13.0'"
-        : "s.platform = :osx, '10.11'";
+        : "s.platform = :osx, '10.15'";
     String podspecContent = podspec.readAsStringSync();
     if (!podspecContent.contains(versionString)) {
       throw TaskResult.failure(
