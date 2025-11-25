a483762ddc4ccebbdf43a111647e1a69d4cd92d5
Mohellebi abdessalem <116356835+AbdeMohlbi@users.noreply.github.com>
2025-11-25 09:43:09 +0100

    Update FlutterApplication.java

--- engine/src/flutter/shell/platform/android/io/flutter/app/FlutterApplication.java
+++ engine/src/flutter/shell/platform/android/io/flutter/app/FlutterApplication.java
@@ -9,7 +9,7 @@ import android.app.Application;
 /**
  * Empty implementation of the {@link Application} class, provided to avoid breaking older Flutter
  * projects. Flutter projects which need to extend an Application should migrate to extending {@link
- * Application} instead.
+ * android.app.Application} instead.
  *
  * <p>For more information on the removal of Flutter's v1 Android embedding, see: <a
  * href="https://docs.flutter.dev/release/breaking-changes/v1-android-embedding">https://docs.flutter.dev/release/breaking-changes/v1-android-embedding</a>.
