32c30be9e4c8e5d75c3c1a17a374893b8dec3afd
Loïc Sharma <737941+loic-sharma@users.noreply.github.com>
2025-11-25 09:03:45 -0800

    Ignore sample on deprecated raw keyboard API

--- examples/api/lib/widgets/hardware_keyboard/key_event_manager.0.dart
+++ examples/api/lib/widgets/hardware_keyboard/key_event_manager.0.dart
@@ -2,11 +2,12 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-
 // TODO(gspencergoog): Delete this example when deprecated RawKeyEvent API is
 // removed.
+// ignore_for_file: deprecated_member_use
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 /// Flutter code sample for [KeyEventManager.keyMessageHandler].
 
