767532c52f6b5805bfd1e156b33b56ccc044fd04
Camille Simon <camillesimon@google.com>
2025-11-26 05:11:31 -0800

    add missing imports

--- examples/flutter_view/android/app/src/main/java/com/example/view/MainActivity.java
+++ examples/flutter_view/android/app/src/main/java/com/example/view/MainActivity.java
@@ -15,11 +15,14 @@ import io.flutter.embedding.android.FlutterView;
 import io.flutter.embedding.engine.FlutterEngine;
 import io.flutter.embedding.engine.dart.DartExecutor;
 import io.flutter.embedding.engine.dart.DartExecutor.DartEntrypoint;
+import io.flutter.Log;
 import io.flutter.plugin.common.BasicMessageChannel;
 import io.flutter.plugin.common.BasicMessageChannel.MessageHandler;
 import io.flutter.plugin.common.BasicMessageChannel.Reply;
 import io.flutter.plugin.common.StringCodec;
+import java.util.Arrays;
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private static FlutterEngine flutterEngine;
