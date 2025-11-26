156127270be0eeabffc4ddfbbebc07dd9c172f98
Loïc Sharma <737941+loic-sharma@users.noreply.github.com>
2025-11-26 15:15:35 -0800

    Format

--- examples/api/test/material/button_style/button_style.icon_alignment.0_test.dart
+++ examples/api/test/material/button_style/button_style.icon_alignment.0_test.dart
@@ -8,100 +8,97 @@ import 'package:flutter_api_samples/material/button_style/button_style.icon_alig
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  testWidgets(
-    'ButtonStyle.iconAlignment updates button icons alignment',
-    (WidgetTester tester) async {
-      await tester.pumpWidget(
-        const example.ButtonStyleIconAlignmentApp(),
+  testWidgets('ButtonStyle.iconAlignment updates button icons alignment', (
+    WidgetTester tester,
+  ) async {
+    await tester.pumpWidget(const example.ButtonStyleIconAlignmentApp());
+
+    Finder findButtonMaterial(String text) {
+      return find
+          .ancestor(of: find.text(text), matching: find.byType(Material))
+          .first;
+    }
+
+    void expectedLeftIconPosition({
+      required double iconOffset,
+      required double textButtonIconOffset,
+    }) {
+      expect(
+        tester.getTopLeft(findButtonMaterial('ElevatedButton')).dx,
+        tester.getTopLeft(find.byIcon(Icons.sunny)).dx - iconOffset,
       );
+      expect(
+        tester.getTopLeft(findButtonMaterial('FilledButton')).dx,
+        tester.getTopLeft(find.byIcon(Icons.beach_access)).dx - iconOffset,
+      );
+      expect(
+        tester.getTopLeft(findButtonMaterial('FilledButton Tonal')).dx,
+        tester.getTopLeft(find.byIcon(Icons.cloud)).dx - iconOffset,
+      );
+      expect(
+        tester.getTopLeft(findButtonMaterial('OutlinedButton')).dx,
+        tester.getTopLeft(find.byIcon(Icons.light)).dx - iconOffset,
+      );
+      expect(
+        tester.getTopLeft(findButtonMaterial('TextButton')).dx,
+        tester.getTopLeft(find.byIcon(Icons.flight_takeoff)).dx -
+            textButtonIconOffset,
+      );
+    }
 
-      Finder findButtonMaterial(String text) {
-        return find
-            .ancestor(of: find.text(text), matching: find.byType(Material))
-            .first;
-      }
+    void expectedRightIconPosition({
+      required double iconOffset,
+      required double textButtonIconOffset,
+    }) {
+      expect(
+        tester.getTopRight(findButtonMaterial('ElevatedButton')).dx,
+        tester.getTopRight(find.byIcon(Icons.sunny)).dx + iconOffset,
+      );
+      expect(
+        tester.getTopRight(findButtonMaterial('FilledButton')).dx,
+        tester.getTopRight(find.byIcon(Icons.beach_access)).dx + iconOffset,
+      );
+      expect(
+        tester.getTopRight(findButtonMaterial('FilledButton Tonal')).dx,
+        tester.getTopRight(find.byIcon(Icons.cloud)).dx + iconOffset,
+      );
+      expect(
+        tester.getTopRight(findButtonMaterial('OutlinedButton')).dx,
+        tester.getTopRight(find.byIcon(Icons.light)).dx + iconOffset,
+      );
+      expect(
+        tester.getTopRight(findButtonMaterial('TextButton')).dx,
+        tester.getTopRight(find.byIcon(Icons.flight_takeoff)).dx +
+            textButtonIconOffset,
+      );
+    }
 
-      void expectedLeftIconPosition({
-        required double iconOffset,
-        required double textButtonIconOffset,
-      }) {
-        expect(
-          tester.getTopLeft(findButtonMaterial('ElevatedButton')).dx,
-          tester.getTopLeft(find.byIcon(Icons.sunny)).dx - iconOffset,
-        );
-        expect(
-          tester.getTopLeft(findButtonMaterial('FilledButton')).dx,
-          tester.getTopLeft(find.byIcon(Icons.beach_access)).dx - iconOffset,
-        );
-        expect(
-          tester.getTopLeft(findButtonMaterial('FilledButton Tonal')).dx,
-          tester.getTopLeft(find.byIcon(Icons.cloud)).dx - iconOffset,
-        );
-        expect(
-          tester.getTopLeft(findButtonMaterial('OutlinedButton')).dx,
-          tester.getTopLeft(find.byIcon(Icons.light)).dx - iconOffset,
-        );
-        expect(
-          tester.getTopLeft(findButtonMaterial('TextButton')).dx,
-          tester.getTopLeft(find.byIcon(Icons.flight_takeoff)).dx -
-              textButtonIconOffset,
-        );
-      }
+    // Test initial icon alignment in LTR.
+    expectedLeftIconPosition(iconOffset: 16, textButtonIconOffset: 12);
 
-      void expectedRightIconPosition({
-        required double iconOffset,
-        required double textButtonIconOffset,
-      }) {
-        expect(
-          tester.getTopRight(findButtonMaterial('ElevatedButton')).dx,
-          tester.getTopRight(find.byIcon(Icons.sunny)).dx + iconOffset,
-        );
-        expect(
-          tester.getTopRight(findButtonMaterial('FilledButton')).dx,
-          tester.getTopRight(find.byIcon(Icons.beach_access)).dx + iconOffset,
-        );
-        expect(
-          tester.getTopRight(findButtonMaterial('FilledButton Tonal')).dx,
-          tester.getTopRight(find.byIcon(Icons.cloud)).dx + iconOffset,
-        );
-        expect(
-          tester.getTopRight(findButtonMaterial('OutlinedButton')).dx,
-          tester.getTopRight(find.byIcon(Icons.light)).dx + iconOffset,
-        );
-        expect(
-          tester.getTopRight(findButtonMaterial('TextButton')).dx,
-          tester.getTopRight(find.byIcon(Icons.flight_takeoff)).dx +
-              textButtonIconOffset,
-        );
-      }
+    // Update icon alignment to end.
+    await tester.tap(find.text('end'));
+    await tester.pumpAndSettle();
 
-      // Test initial icon alignment in LTR.
-      expectedLeftIconPosition(iconOffset: 16, textButtonIconOffset: 12);
+    // Test icon alignment end in LTR.
+    expectedRightIconPosition(iconOffset: 24, textButtonIconOffset: 16);
 
-      // Update icon alignment to end.
-      await tester.tap(find.text('end'));
-      await tester.pumpAndSettle();
+    // Reset icon alignment to start.
+    await tester.tap(find.text('start'));
+    await tester.pumpAndSettle();
 
-      // Test icon alignment end in LTR.
-      expectedRightIconPosition(iconOffset: 24, textButtonIconOffset: 16);
+    // Change text direction to RTL.
+    await tester.tap(find.text('RTL'));
+    await tester.pumpAndSettle();
 
-      // Reset icon alignment to start.
-      await tester.tap(find.text('start'));
-      await tester.pumpAndSettle();
+    // Test icon alignment start in LTR.
+    expectedRightIconPosition(iconOffset: 16, textButtonIconOffset: 12);
 
-      // Change text direction to RTL.
-      await tester.tap(find.text('RTL'));
-      await tester.pumpAndSettle();
+    // Update icon alignment to end.
+    await tester.tap(find.text('end'));
+    await tester.pumpAndSettle();
 
-      // Test icon alignment start in LTR.
-      expectedRightIconPosition(iconOffset: 16, textButtonIconOffset: 12);
-
-      // Update icon alignment to end.
-      await tester.tap(find.text('end'));
-      await tester.pumpAndSettle();
-
-      // Test icon alignment end in LTR.
-      expectedLeftIconPosition(iconOffset: 24, textButtonIconOffset: 16);
-    },
-  );
+    // Test icon alignment end in LTR.
+    expectedLeftIconPosition(iconOffset: 24, textButtonIconOffset: 16);
+  });
 }
