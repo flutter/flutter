7e8e479d69232a573325ddcd6ce98aa1cc7b9ea4
Loïc PÉRON <loic.peron@inetum.com>
2025-11-28 10:50:50 +0100

    AXPlatformNodeDelegate::IsIAccessibleExEnabled()

--- engine/src/flutter/third_party/accessibility/ax/platform/ax_platform_node_win.cc
+++ engine/src/flutter/third_party/accessibility/ax/platform/ax_platform_node_win.cc
@@ -2599,7 +2599,7 @@ IFACEMETHODIMP AXPlatformNodeWin::QueryService(REFGUID guidService,
                                                void** object) {
   COM_OBJECT_VALIDATE_1_ARG(object);
 
-  if (guidService == IID_IAccessible || (guidService == IID_IAccessibleEx && _IAccessibleExEnabled)) {
+  if (guidService == IID_IAccessible || (guidService == IID_IAccessibleEx && delegate_->IsIAccessibleExEnabled())) {
     return QueryInterface(riid, object);
   }
 
