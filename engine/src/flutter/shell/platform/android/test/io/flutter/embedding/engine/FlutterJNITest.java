f0a9c7fc7f1057a703f9ec2518ce5f9b60f5e51e
Chun-Heng Tai <chtai@google.com>
2025-12-04 10:09:23 -0800

    update

--- engine/src/flutter/shell/platform/android/test/io/flutter/embedding/engine/FlutterJNITest.java
+++ engine/src/flutter/shell/platform/android/test/io/flutter/embedding/engine/FlutterJNITest.java
@@ -9,6 +9,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
