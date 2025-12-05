754162562882e7a5b671db246b9c74d6f0a84e58
mitchellgoodwin <mitchgoodwin@google.com>
2025-12-05 15:00:00 -0800

    Lints

--- packages/flutter/lib/src/cupertino/sheet.dart
+++ packages/flutter/lib/src/cupertino/sheet.dart
@@ -271,7 +271,7 @@ Future<T?> showCupertinoSheet<T>({
     );
   }
 
-  final CupertinoSheetRoute<T> route = effectiveBuilder != null
+  final route = effectiveBuilder != null
       ? CupertinoSheetRoute<T>(
           builder: (BuildContext context) => nestedNavigationContent(effectiveBuilder),
           enableDrag: enableDrag,
