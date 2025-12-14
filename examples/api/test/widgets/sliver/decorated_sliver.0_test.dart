3f969f01d8de2cf756e9357927b3b89ee7111270
Brahim Guaali <brahim.guaali@talabat.com>
2025-12-14 16:50:04 +0400

    Fix formatting in test file
    Fixes #166822

--- examples/api/test/widgets/sliver/decorated_sliver.0_test.dart
+++ examples/api/test/widgets/sliver/decorated_sliver.0_test.dart
@@ -18,67 +18,66 @@ void main() {
     expect(blueSkyText, findsOneWidget);
   });
 
-  testWidgets(
-    'Verify the DecoratedSliver has a LinearGradient',
-    (WidgetTester tester) async {
-      await tester.pumpWidget(const example.SliverDecorationExampleApp());
+  testWidgets('Verify the DecoratedSliver has a LinearGradient', (
+    WidgetTester tester,
+  ) async {
+    await tester.pumpWidget(const example.SliverDecorationExampleApp());
 
-      final DecoratedSliver decoratedSliver = tester.widget<DecoratedSliver>(
-        find.byType(DecoratedSliver),
-      );
-      expect(
-        decoratedSliver.decoration,
-        const BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: <Color>[
-              Color(0xFF111133),
-              Color(0xFF1A237E),
-              Color(0xFF283593),
-              Color(0xFF3949AB),
-              Color(0xFF3F51B5),
-              Color(0xFF1976D2),
-              Color(0xFF1E88E5),
-              Color(0xFF42A5F5),
-            ],
-          ),
+    final DecoratedSliver decoratedSliver = tester.widget<DecoratedSliver>(
+      find.byType(DecoratedSliver),
+    );
+    expect(
+      decoratedSliver.decoration,
+      const BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: <Color>[
+            Color(0xFF111133),
+            Color(0xFF1A237E),
+            Color(0xFF283593),
+            Color(0xFF3949AB),
+            Color(0xFF3F51B5),
+            Color(0xFF1976D2),
+            Color(0xFF1E88E5),
+            Color(0xFF42A5F5),
+          ],
         ),
-      );
-    },
-  );
+      ),
+    );
+  });
 
-  testWidgets(
-    'Verify the moon section has a RadialGradient',
-    (WidgetTester tester) async {
-      await tester.pumpWidget(const example.SliverDecorationExampleApp());
+  testWidgets('Verify the moon section has a RadialGradient', (
+    WidgetTester tester,
+  ) async {
+    await tester.pumpWidget(const example.SliverDecorationExampleApp());
 
-      final Container moonContainer = tester.widget<Container>(
-        find.ancestor(
-          of: find.text('A moon on a night sky'),
-          matching: find.byType(Container),
-        ).first,
-      );
-      expect(
-        moonContainer.decoration,
-        const BoxDecoration(
-          gradient: RadialGradient(
-            center: Alignment(-0.5, -0.6),
-            radius: 0.15,
-            colors: <Color>[Color(0xFFEEEEEE), Color(0xFF111133)],
-            stops: <double>[0.4, 0.8],
-          ),
+    final Container moonContainer = tester.widget<Container>(
+      find
+          .ancestor(
+            of: find.text('A moon on a night sky'),
+            matching: find.byType(Container),
+          )
+          .first,
+    );
+    expect(
+      moonContainer.decoration,
+      const BoxDecoration(
+        gradient: RadialGradient(
+          center: Alignment(-0.5, -0.6),
+          radius: 0.15,
+          colors: <Color>[Color(0xFFEEEEEE), Color(0xFF111133)],
+          stops: <double>[0.4, 0.8],
         ),
-      );
-    },
-  );
+      ),
+    );
+  });
 
-  testWidgets(
-    'Verify that SliverMainAxisGroup is used to group the slivers',
-    (WidgetTester tester) async {
-      await tester.pumpWidget(const example.SliverDecorationExampleApp());
+  testWidgets('Verify that SliverMainAxisGroup is used to group the slivers', (
+    WidgetTester tester,
+  ) async {
+    await tester.pumpWidget(const example.SliverDecorationExampleApp());
 
-      expect(find.byType(SliverMainAxisGroup), findsOneWidget);
-    },
-  );
+    expect(find.byType(SliverMainAxisGroup), findsOneWidget);
+  });
 }
