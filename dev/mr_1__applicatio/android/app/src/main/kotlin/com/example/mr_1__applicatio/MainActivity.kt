f77cc8ed95648206a476aa1fc961cd1e707a1beb
Junior Thompson <juniorthompson235@gmail.com>
2025-12-19 16:17:59 -0500

    initial commit

---
+++ dev/mr_1__applicatio/android/app/src/main/kotlin/com/example/mr_1__applicatio/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.mr_1__applicatio
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ dev/my_1_application/android/app/src/main/kotlin/com/example/my_1_application/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.my_1_application
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
