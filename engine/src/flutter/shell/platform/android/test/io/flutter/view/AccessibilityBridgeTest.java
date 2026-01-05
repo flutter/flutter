flutter/flutter

commit 8463d5e87dea5bc72989f35e9010b6e4543d0bfe

[hcpp/hc] Fix talkback for HC and HCPP Android platform views (#168939)
See the description here:
https://github.com/flutter/flutter/issues/113626#issuecomment-2884997160

Fixes https://github.com/flutter/flutter/issues/113626

Also, I think we can probably improve the behavior here to respect what
the original view's accessibility importance was, by storing what we are
overwriting at creation time, and then using that later when we undo
what we do at creation time. As is it seems like there would be no way
for a developer to _actually_ set their view to be not important for
accessibility, we are basically forcing them in to auto. Can look into
that in a follow up.

## Pre-launch Checklist

- [x] I read the [Contributor Guide] and followed the process outlined
there for submitting PRs.
- [x] I read the [Tree Hygiene] wiki page, which explains my
responsibilities.
- [x] I read and followed the [Flutter Style Guide], including [Features
we expect every widget to implement].
- [x] I signed the [CLA].
- [x] I listed at least one issue that this PR fixes in the description
above.
- [x] I updated/added relevant documentation (doc comments with `///`).
- [x] I added new tests to check the change I am making, or this PR is
[test-exempt].
- [x] I followed the [breaking change policy] and added [Data Driven
Fixes] where supported.
- [ ] All existing and new tests are passing.

If you need help, consider asking for advice on the #hackers-new channel
on [Discord].

<!-- Links -->
[Contributor Guide]:
https://github.com/flutter/flutter/blob/main/docs/contributing/Tree-hygiene.md#overview
[Tree Hygiene]:
https://github.com/flutter/flutter/blob/main/docs/contributing/Tree-hygiene.md
[test-exempt]:
https://github.com/flutter/flutter/blob/main/docs/contributing/Tree-hygiene.md#tests
[Flutter Style Guide]:
https://github.com/flutter/flutter/blob/main/docs/contributing/Style-guide-for-Flutter-repo.md
[Features we expect every widget to implement]:

engine/src/flutter/shell/platform/android/io/flutter/view/AccessibilityBridge.java
@@ -989,6 +989,10 @@ public class AccessibilityBridge extends AccessibilityNodeProvider {
         //
         // See the case above for how virtual displays are handled.
         if (!platformViewsAccessibilityDelegate.usesVirtualDisplay(child.platformViewId)) {
+          assert embeddedView != null;
+          // The embedded view is initially marked as not important at creation in the platform
+          // view controller, so we must explicitly mark it as important here.
+          embeddedView.setImportantForAccessibility(View.IMPORTANT_FOR_ACCESSIBILITY_AUTO);
           result.addChild(embeddedView);
           continue;
         }

engine/src/flutter/shell/platform/android/test/io/flutter/view/AccessibilityBridgeTest.java
@@ -1741,6 +1741,7 @@ public class AccessibilityBridgeTest {
     AccessibilityNodeInfo result = accessibilityBridge.createAccessibilityNodeInfo(0);
     assertNotNull(result);
     assertEquals(result.getChildCount(), 1);
+    verify(embeddedView).setImportantForAccessibility(View.IMPORTANT_FOR_ACCESSIBILITY_AUTO);
     assertEquals(result.getClassName(), "android.view.View");
   }
 

engine/src/flutter/shell/platform/android/BUILD.gn
@@ -346,6 +346,7 @@ android_java_sources = [
   "io/flutter/plugin/platform/PlatformViewsAccessibilityDelegate.java",
   "io/flutter/plugin/platform/PlatformViewsController.java",
   "io/flutter/plugin/platform/PlatformViewsController2.java",
+  "io/flutter/plugin/platform/PlatformViewsControllerDelegator.java",
   "io/flutter/plugin/platform/SingleViewFakeWindowViewGroup.java",
   "io/flutter/plugin/platform/SingleViewPresentation.java",
   "io/flutter/plugin/platform/SingleViewWindowManager.java",

engine/src/flutter/shell/platform/android/io/flutter/embedding/android/FlutterView.java
@@ -68,6 +68,7 @@ import io.flutter.plugin.editing.TextInputPlugin;
 import io.flutter.plugin.localization.LocalizationPlugin;
 import io.flutter.plugin.mouse.MouseCursorPlugin;
 import io.flutter.plugin.platform.PlatformViewsController;
+import io.flutter.plugin.platform.PlatformViewsControllerDelegator;
 import io.flutter.util.ViewUtils;
 import io.flutter.view.AccessibilityBridge;
 import java.lang.reflect.InvocationTargetException;
@@ -1143,13 +1144,16 @@ public class FlutterView extends FrameLayout
     keyboardManager = new KeyboardManager(this);
     androidTouchProcessor =
         new AndroidTouchProcessor(this.flutterEngine.getRenderer(), /*trackMotionEvents=*/ false);
+
     accessibilityBridge =
         new AccessibilityBridge(
             this,
             flutterEngine.getAccessibilityChannel(),
             (AccessibilityManager) getContext().getSystemService(Context.ACCESSIBILITY_SERVICE),
             getContext().getContentResolver(),
-            this.flutterEngine.getPlatformViewsController());
+            new PlatformViewsControllerDelegator(
+                this.flutterEngine.getPlatformViewsController(),
+                this.flutterEngine.getPlatformViewsController2()));
     accessibilityBridge.setOnAccessibilityChangeListener(onAccessibilityChangeListener);
     resetWillNotDraw(
         accessibilityBridge.isAccessibilityEnabled(),

engine/src/flutter/shell/platform/android/io/flutter/plugin/platform/PlatformViewsControllerDelegator.java
@@ -1,0 +1,50 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package io.flutter.plugin.platform;
+
+import android.view.View;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import io.flutter.view.AccessibilityBridge;
+
+public class PlatformViewsControllerDelegator implements PlatformViewsAccessibilityDelegate {
+
+  PlatformViewsController platformViewsController;
+  PlatformViewsController2 platformViewsController2;
+
+  public PlatformViewsControllerDelegator(
+      PlatformViewsController platformViewsController,
+      PlatformViewsController2 platformViewsController2) {
+    this.platformViewsController = platformViewsController;
+    this.platformViewsController2 = platformViewsController2;
+  }
+
+  @Nullable
+  @Override
+  public View getPlatformViewById(int viewId) {
+    return platformViewsController2.getPlatformViewById(viewId) != null
+        ? platformViewsController2.getPlatformViewById(viewId)
+        : platformViewsController.getPlatformViewById(viewId);
+  }
+
+  @Override
+  public boolean usesVirtualDisplay(int id) {
+    return platformViewsController2.getPlatformViewById(id) != null
+        ? platformViewsController2.usesVirtualDisplay(id)
+        : platformViewsController.usesVirtualDisplay(id);
+  }
+
+  @Override
+  public void attachAccessibilityBridge(@NonNull AccessibilityBridge accessibilityBridge) {
+    platformViewsController.attachAccessibilityBridge(accessibilityBridge);
+    platformViewsController2.attachAccessibilityBridge(accessibilityBridge);
+  }
+
+  @Override
+  public void detachAccessibilityBridge() {
+    platformViewsController.detachAccessibilityBridge();
+    platformViewsController2.detachAccessibilityBridge();
+  }
+}

engine/src/flutter/shell/platform/android/test/io/flutter/plugin/platform/PlatformViewsControllerDelegatorTest.java
@@ -1,0 +1,103 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package io.flutter.plugin.platform;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import android.view.View;
+import io.flutter.view.AccessibilityBridge;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PlatformViewsControllerDelegatorTest {
+  @Test
+  public void getPlatformViewById_returnsFromPVC2ifPresent() {
+    PlatformViewsController platformViewsController = mock(PlatformViewsController.class);
+    PlatformViewsController2 platformViewsController2 = mock(PlatformViewsController2.class);
+    View pv1 = mock(View.class);
+    View pv2 = mock(View.class);
+    when(platformViewsController.getPlatformViewById(0)).thenReturn(pv1);
+    when(platformViewsController2.getPlatformViewById(0)).thenReturn(pv2);
+
+    PlatformViewsControllerDelegator delegator =
+        new PlatformViewsControllerDelegator(platformViewsController, platformViewsController2);
+    View returnedView = delegator.getPlatformViewById(0);
+
+    Assert.assertEquals(pv2, returnedView);
+    Assert.assertNotEquals(pv1, returnedView);
+  }
+
+  @Test
+  public void getPlatformViewById_returnsFromPVC1ifNotPresentInPVC2() {
+    PlatformViewsController platformViewsController = mock(PlatformViewsController.class);
+    PlatformViewsController2 platformViewsController2 = mock(PlatformViewsController2.class);
+    View pv1 = mock(View.class);
+    when(platformViewsController.getPlatformViewById(0)).thenReturn(pv1);
+
+    PlatformViewsControllerDelegator delegator =
+        new PlatformViewsControllerDelegator(platformViewsController, platformViewsController2);
+    View returnedView = delegator.getPlatformViewById(0);
+
+    Assert.assertEquals(pv1, returnedView);
+  }
+
+  @Test
+  public void usesVirtualDisplay_returnsFromPVC2() {
+    PlatformViewsController platformViewsController = mock(PlatformViewsController.class);
+    PlatformViewsController2 platformViewsController2 = mock(PlatformViewsController2.class);
+    View pv1 = mock(View.class);
+    when(platformViewsController2.getPlatformViewById(0)).thenReturn(pv1);
+    when(platformViewsController.usesVirtualDisplay(0)).thenReturn(true);
+    when(platformViewsController2.usesVirtualDisplay(0)).thenReturn(false);
+
+    PlatformViewsControllerDelegator delegator =
+        new PlatformViewsControllerDelegator(platformViewsController, platformViewsController2);
+
+    Assert.assertFalse(delegator.usesVirtualDisplay(0));
+  }
+
+  @Test
+  public void usesVirtualDisplay_returnsFromPVC1ifNotPresentInPVC2() {
+    PlatformViewsController platformViewsController = mock(PlatformViewsController.class);
+    PlatformViewsController2 platformViewsController2 = mock(PlatformViewsController2.class);
+    when(platformViewsController2.getPlatformViewById(0)).thenReturn(null);
+    when(platformViewsController.usesVirtualDisplay(0)).thenReturn(true);
+    when(platformViewsController2.usesVirtualDisplay(0)).thenReturn(false);
+
+    PlatformViewsControllerDelegator delegator =
+        new PlatformViewsControllerDelegator(platformViewsController, platformViewsController2);
+
+    Assert.assertTrue(delegator.usesVirtualDisplay(0));
+  }
+
+  @Test
+  public void attachAccessibilityBridge_attachesForBothPVCs() {
+    PlatformViewsController platformViewsController = mock(PlatformViewsController.class);
+    PlatformViewsController2 platformViewsController2 = mock(PlatformViewsController2.class);
+    AccessibilityBridge accessibilityBridge = mock(AccessibilityBridge.class);
+
+    PlatformViewsControllerDelegator delegator =
+        new PlatformViewsControllerDelegator(platformViewsController, platformViewsController2);
+    delegator.attachAccessibilityBridge(accessibilityBridge);
+
+    verify(platformViewsController).attachAccessibilityBridge(accessibilityBridge);
+    verify(platformViewsController2).attachAccessibilityBridge(accessibilityBridge);
+  }
+
+  @Test
+  public void detachAccessibilityBridge_detachesForBothPVCs() {
+    PlatformViewsController platformViewsController = mock(PlatformViewsController.class);
+    PlatformViewsController2 platformViewsController2 = mock(PlatformViewsController2.class);
+
+    PlatformViewsControllerDelegator delegator =
+        new PlatformViewsControllerDelegator(platformViewsController, platformViewsController2);
+    delegator.detachAccessibilityBridge();
+
+    verify(platformViewsController).detachAccessibilityBridge();
+    verify(platformViewsController2).detachAccessibilityBridge();
+  }
+}

commit 2838c0e7d9c6379e2b6322068f0c937d56f686cd

Add a null check for embedded view being added to accessibility tree (#180539)
Fixes 
https://github.com/flutter/flutter/issues/180381
https://github.com/flutter/flutter/issues/179922

which track a regression from
https://github.com/flutter/flutter/pull/168939

Wraps in a null check, and if the embeddedView is null we fall back to
adding the semantics node we are looking at in the loop as a child.

## Pre-launch Checklist

- [x] I read the [Contributor Guide] and followed the process outlined
there for submitting PRs.
- [x] I read the [Tree Hygiene] wiki page, which explains my
responsibilities.
- [x] I read and followed the [Flutter Style Guide], including [Features
we expect every widget to implement].
- [x] I signed the [CLA].
- [x] I listed at least one issue that this PR fixes in the description
above.
- [x] I updated/added relevant documentation (doc comments with `///`).
- [x] I added new tests to check the change I am making, or this PR is
[test-exempt].
- [x] I followed the [breaking change policy] and added [Data Driven
Fixes] where supported.
- [x] All existing and new tests are passing.

If you need help, consider asking for advice on the #hackers-new channel
on [Discord].

**Note**: The Flutter team is currently trialing the use of [Gemini Code
Assist for
GitHub](https://developers.google.com/gemini-code-assist/docs/review-github-code).
Comments from the `gemini-code-assist` bot should not be taken as
authoritative feedback from the Flutter team. If you find its comments
useful you can update your code accordingly, but if you are unsure or
disagree with the feedback, please feel free to wait for a Flutter team
member's review for guidance on which automated comments should be
addressed.

<!-- Links -->
[Contributor Guide]:
https://github.com/flutter/flutter/blob/main/docs/contributing/Tree-hygiene.md#overview
[Tree Hygiene]:
https://github.com/flutter/flutter/blob/main/docs/contributing/Tree-hygiene.md
[test-exempt]:

engine/src/flutter/shell/platform/android/io/flutter/view/AccessibilityBridge.java
@@ -1174,12 +1174,13 @@ public class AccessibilityBridge extends AccessibilityNodeProvider {
         //
         // See the case above for how virtual displays are handled.
         if (!platformViewsAccessibilityDelegate.usesVirtualDisplay(child.platformViewId)) {
-          assert embeddedView != null;
-          // The embedded view is initially marked as not important at creation in the platform
-          // view controller, so we must explicitly mark it as important here.
-          embeddedView.setImportantForAccessibility(View.IMPORTANT_FOR_ACCESSIBILITY_AUTO);
-          result.addChild(embeddedView);
-          continue;
+          if (embeddedView != null) {
+            // The embedded view is initially marked as not important at creation in the platform
+            // view controller, so we must explicitly mark it as important here.
+            embeddedView.setImportantForAccessibility(View.IMPORTANT_FOR_ACCESSIBILITY_AUTO);
+            result.addChild(embeddedView);
+            continue;
+          }
         }
       }
       result.addChild(rootAccessibilityView, child.id);

engine/src/flutter/shell/platform/android/test/io/flutter/view/AccessibilityBridgeTest.java
@@ -2114,6 +2114,46 @@ public class AccessibilityBridgeTest {
   }
 
   @Test
+  public void itDoesNotCrashWhenEmbeddedViewIsNull() {
+    PlatformViewsAccessibilityDelegate accessibilityDelegate =
+        mock(PlatformViewsAccessibilityDelegate.class);
+    AccessibilityViewEmbedder accessibilityViewEmbedder = mock(AccessibilityViewEmbedder.class);
+    AccessibilityBridge accessibilityBridge =
+        setUpBridge(
+            /* rootAccessibilityView= */ null,
+            /* accessibilityChannel= */ null,
+            /* accessibilityManager= */ null,
+            /* contentResolver= */ null,
+            accessibilityViewEmbedder,
+            accessibilityDelegate);
+
+    TestSemanticsNode root = new TestSemanticsNode();
+    root.id = 0;
+
+    TestSemanticsNode platformView = new TestSemanticsNode();
+    platformView.id = 1;
+    platformView.platformViewId = 1;
+    root.addChild(platformView);
+
+    when(accessibilityDelegate.usesVirtualDisplay(1)).thenReturn(false);
+    when(accessibilityDelegate.getPlatformViewById(1)).thenReturn(null);
+
+    TestSemanticsUpdate testSemanticsUpdate = root.toUpdate();
+    testSemanticsUpdate.sendUpdateToBridge(accessibilityBridge);
+
+    // This should not crash.
+    AccessibilityNodeInfo result = accessibilityBridge.createAccessibilityNodeInfo(0);
+
+    // Verify that we fell back to adding the child as a virtual node (standard semantics node)
+    // instead of trying to add the null embedded view.
+    boolean hasChild = false;
+    for (int i = 0; i < result.getChildCount(); i++) {
+      hasChild = true;
+    }
+    assertTrue("Should have added the virtual child node", hasChild);
+  }
+
+  @Test
   public void testItSetsDisableAnimationsFlagBasedOnTransitionAnimationScale() {
     AccessibilityChannel mockChannel = mock(AccessibilityChannel.class);
     ContentResolver mockContentResolver = mock(ContentResolver.class);