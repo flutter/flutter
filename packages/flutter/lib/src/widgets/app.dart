eec0e2fa5cfd90aa07bf6d348e476cfd74514762
Victor Sanni <victorsanniay@gmail.com>
2026-01-07 13:58:42 -0800

    Raw tooltip with smaller API surface that exposes tooltip widget (#177678)
    The raw widget used to make a
    [Tooltip](https://api.flutter.dev/flutter/material/Tooltip-class.html).
    
    Design doc: flutter.dev/go/codeshare-tooltip
    
    ## Constructor
    ```dart
    class RawTooltip extends StatefulWidget {
      const RawTooltip({
        super.key,
        required this.semanticsTooltip,
        required this.tooltipBuilder,
        this.enableTapToDismiss = true,
        this.triggerMode = TooltipTriggerMode.longPress,
        this.enableFeedback = true,
        this.onTriggered,
        this.hoverDelay = Duration.zero,
        this.touchDelay = const Duration(milliseconds: 1500),
        this.dismissDelay = const Duration(milliseconds: 100),
        this.animationStyle = _kDefaultAnimationStyle,
        this.positionDelegate,
        required this.child,
      });
    ```
    
    ## Properties
    ```dart
      final String semanticsTooltip;
      final TooltipComponentBuilder tooltipBuilder;
      final Duration hoverDelay;
      final Duration touchDelay;
      final Duration dismissDelay;
      final bool enableTapToDismiss;
      final TooltipTriggerMode triggerMode;
      final bool enableFeedback;
      final TooltipTriggeredCallback? onTriggered;
      final AnimationStyle animationStyle;
      final TooltipPositionDelegate? positionDelegate;
      final Widget child;
    ```
    
    Part of [☂️ Reinforcement: Add more basic components to the core
    framework](https://github.com/flutter/flutter/issues/97496)
    Part of [☂️ Reinforcement: Refactor widgets from design into the core
    before decoupling](https://github.com/flutter/flutter/issues/53059)
    Fixes [Custom Overlay for Tooltip Widget
    ](https://github.com/flutter/flutter/issues/45034)
    
    ---------
    
    Co-authored-by: Tong Mu <dkwingsmt@users.noreply.github.com>
    Co-authored-by: chunhtai <47866232+chunhtai@users.noreply.github.com>

--- packages/flutter/lib/src/widgets/app.dart
+++ packages/flutter/lib/src/widgets/app.dart
@@ -21,6 +21,7 @@ import 'banner.dart';
 import 'basic.dart';
 import 'binding.dart';
 import 'default_text_editing_shortcuts.dart';
+import 'focus_manager.dart';
 import 'focus_scope.dart';
 import 'focus_traversal.dart';
 import 'framework.dart';
@@ -30,6 +31,7 @@ import 'navigator.dart';
 import 'notification_listener.dart';
 import 'pages.dart';
 import 'performance_overlay.dart';
+import 'raw_tooltip.dart';
 import 'restoration.dart';
 import 'router.dart';
 import 'scrollable_helpers.dart';
@@ -1766,6 +1768,21 @@ class _WidgetsAppState extends State<WidgetsApp> with WidgetsBindingObserver {
       }
       return true;
     }());
+
+    // TODO(victorsanni): https://github.com/flutter/flutter/issues/180319
+    // Use actions and shortcuts to dismiss tooltips when esc is pressed instead
+    // of using a Focus widget.
+    result = Focus(
+      canRequestFocus: false,
+      onKeyEvent: (FocusNode node, KeyEvent event) {
+        if ((event is! KeyDownEvent && event is! KeyRepeatEvent) ||
+            event.logicalKey != LogicalKeyboardKey.escape) {
+          return KeyEventResult.ignored;
+        }
+        return RawTooltip.dismissAllToolTips() ? KeyEventResult.handled : KeyEventResult.ignored;
+      },
+      child: result,
+    );
 
     final Widget? title;
     if (widget.onGenerateTitle != null) {
