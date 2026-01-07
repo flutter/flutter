7d9198a83de74d02dbafa7e02080096207279374
Bruno Leroux <bruno.leroux@gmail.com>
2026-01-07 10:08:32 +0100

    Fix DropdownMenuEntry.style not resolved when entry is highlighted (#178873)
    ## Description
    
    This PR adds logic to resolve `DropdownMenuEntry.style` properties with
    the correct `WidgetState` when an item is highlighted.
    
    When `MenuItemButton` are highlighted the focused state is not added
    automatically because the focus does not move to the items (it stays on
    the `TextField` in order to let the user enters text to filter the items
    list). This PR sets the `MenuItemButton.statesController` with a forced
    focused state to let `MenuItemButton` know that the focused state should
    be use to resolve the properties.
    
    ## Related Issue
    
    Fixes [DropdownMenuEntry style's text style is not resolving with
    states](https://github.com/flutter/flutter/issues/177363)
    
    ## Tests
    
    - Adds 1 tests.

--- packages/flutter/test/material/dropdown_menu_test.dart
+++ packages/flutter/test/material/dropdown_menu_test.dart
@@ -506,6 +506,60 @@ void main() {
         paints..rect(color: defaultOverlayColor.withAlpha(0)),
       );
     });
+
+    // Regression test for https://github.com/flutter/flutter/issues/177363.
+    testWidgets('textStyle property is resolved when item is highlighted', (
+      WidgetTester tester,
+    ) async {
+      const TestMenu selectedItem = TestMenu.mainMenu3;
+      const TestMenu nonSelectedItem = TestMenu.mainMenu2;
+
+      final customButtonStyle = ButtonStyle(
+        textStyle: WidgetStateProperty.resolveWith(
+          (Set<WidgetState> states) => TextStyle(
+            fontWeight: states.contains(WidgetState.focused) ? FontWeight.bold : FontWeight.normal,
+          ),
+        ),
+      );
+
+      final menuEntries = <DropdownMenuEntry<TestMenu>>[];
+      for (final item in menuChildren) {
+        menuEntries.add(
+          DropdownMenuEntry<TestMenu>(
+            value: item.value,
+            label: item.label,
+            style: customButtonStyle,
+          ),
+        );
+      }
+
+      TextStyle? getItemLabelStyle(String label) {
+        final RenderObject paragraph = tester
+            .element<StatelessElement>(
+              find.descendant(of: findMenuItemButton(label), matching: find.text(label)),
+            )
+            .renderObject!;
+        return (paragraph as RenderParagraph).text.style;
+      }
+
+      await tester.pumpWidget(
+        MaterialApp(
+          home: Scaffold(
+            body: DropdownMenu<TestMenu>(
+              initialSelection: selectedItem,
+              dropdownMenuEntries: menuEntries,
+            ),
+          ),
+        ),
+      );
+
+      // Open the menu.
+      await tester.tap(find.byType(DropdownMenu<TestMenu>));
+      await tester.pump();
+
+      expect(getItemLabelStyle(selectedItem.label)?.fontWeight, FontWeight.bold);
+      expect(getItemLabelStyle(nonSelectedItem.label)?.fontWeight, FontWeight.normal);
+    });
   });
 
   testWidgets('Inner TextField is disabled when DropdownMenu is disabled', (
