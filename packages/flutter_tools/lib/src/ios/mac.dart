407c4e287f97b8243e85fa19516d98c3f5ae094c
saqibjaved <Saqibjaved19078@gmail.com>
2026-01-09 05:46:07 +0500

    Fix analyzer lint warnings for CI
    Remove obvious type annotations as required by CI analyzer

--- packages/flutter_tools/lib/src/ios/mac.dart
+++ packages/flutter_tools/lib/src/ios/mac.dart
@@ -720,9 +720,9 @@ Future<void> removeExtendedAttributes(
   // Remove specific extended attributes that cause code signing failures.
   // We remove com.apple.FinderInfo and com.apple.provenance, but preserve
   // com.apple.xcode.CreatedByBuildSystem which Xcode uses to manage build directories.
-  final List<String> attributesToRemove = <String>['com.apple.FinderInfo', 'com.apple.provenance'];
+  final attributesToRemove = <String>['com.apple.FinderInfo', 'com.apple.provenance'];
 
-  for (final String attribute in attributesToRemove) {
+  for (final attribute in attributesToRemove) {
     final bool success = await processUtils.exitsHappy(<String>[
       'xattr',
       '-r',
