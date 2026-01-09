3529d6c7b4ace65a5a4848942fe3153a3e5eb37f
John "codefu" McDole <codefu@google.com>
2026-01-09 15:47:35 -0800

    fix: there are no riscv fuchsia artifacts (#180779)
    In #178711, `packages/flutter_tools/lib/src/flutter_cache.dart` was
    updated to add 'riscv' downloading in the `FlutterRunnerDebugSymbols`.
    This class is not well named and is responsible for downloading "the
    debug symbols for flutter runner for Fuchsia development."
    
    We do not produce fuchsia riscv.
    
    Fixes #180775

--- packages/flutter_tools/test/general.shard/cache_test.dart
+++ packages/flutter_tools/test/general.shard/cache_test.dart
@@ -574,7 +574,6 @@ void main() {
     expect(packageResolver.resolved, <List<String>>[
       <String>['fuchsia-debug-symbols-x64', '123456'],
       <String>['fuchsia-debug-symbols-arm64', '123456'],
-      <String>['fuchsia-debug-symbols-riscv64', '123456'],
     ]);
   });
 
