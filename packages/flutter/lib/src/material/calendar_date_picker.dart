5d18c946190cea8f7f27df9b6f6056e589b901b5
Franklyn Roberto <58437929+Franklyn-R-Silva@users.noreply.github.com>
2026-01-10 09:08:16 -0300

    Update packages/flutter/lib/src/material/calendar_date_picker.dart
    Co-authored-by: Hannah Jin <jhy03261997@gmail.com>

--- packages/flutter/lib/src/material/calendar_date_picker.dart
+++ packages/flutter/lib/src/material/calendar_date_picker.dart
@@ -245,9 +245,7 @@ class _CalendarDatePickerState extends State<CalendarDatePicker> {
   // Auxiliary method for handling the difference between platforms
   void _announce(String message) {
     if (Theme.of(context).platform == TargetPlatform.android) {
-      setState(() {
         _announcementText = message;
-      });
     } else {
       // Maintains the mandatory announcement for other platforms
       SemanticsService.sendAnnouncement(View.of(context), message, Directionality.of(context));
