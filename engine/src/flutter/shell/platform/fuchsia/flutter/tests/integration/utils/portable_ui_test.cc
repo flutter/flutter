e381a831452d1b7509d1fa09cadf44fa074004ea
巢鹏 <chaopeng@google.com>
2026-01-15 15:13:18 -0500

    Add route for 'tmp' to Flutter runner

--- engine/src/flutter/shell/platform/fuchsia/flutter/tests/integration/utils/portable_ui_test.cc
+++ engine/src/flutter/shell/platform/fuchsia/flutter/tests/integration/utils/portable_ui_test.cc
@@ -101,6 +101,12 @@ void PortableUITest::SetUpRealmBase() {
                        Protocol{kPointerInjectorRegistryName}},
       .source = kTestUIStackRef,
       .targets = {ParentRef(), kFlutterJitRunnerRef}});
+
+  // Route "tmp" to Flutter runner
+  realm_builder_.AddRoute(Route{
+      .capabilities = {Storage{"tmp"}},
+      .source = ParentRef(),
+      .targets = {kFlutterJitRunnerRef});
 }
 
 void PortableUITest::ProcessViewGeometryResponse(
