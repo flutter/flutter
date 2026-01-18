4945640b7d35d914aeeb3310d4064038bc4100ad
Mairramer <mairramer.dasilva28@hotmail.com>
2026-01-18 09:51:46 -0300

    format code

--- examples/api/test/material/date_picker/show_date_picker.2_test.dart
+++ examples/api/test/material/date_picker/show_date_picker.2_test.dart
@@ -3,55 +3,65 @@
 // found in the LICENSE file.
 
 import 'package:flutter/material.dart';
-import 'package:flutter_api_samples/material/date_picker/show_date_picker.2.dart' as example;
+import 'package:flutter_api_samples/material/date_picker/show_date_picker.2.dart'
+    as example;
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  testWidgets('renders selected date text according to formatType and separator (input mode)', (
-    WidgetTester tester,
-  ) async {
-    await tester.pumpWidget(const MaterialApp(home: example.DatePickerSample()));
+  testWidgets(
+    'renders selected date text according to formatType and separator (input mode)',
+    (WidgetTester tester) async {
+      await tester.pumpWidget(
+        const MaterialApp(home: example.DatePickerSample()),
+      );
 
-    expect(find.text('No date selected'), findsOneWidget);
+      expect(find.text('No date selected'), findsOneWidget);
 
-    // Open date picker.
-    await tester.tap(find.byType(OutlinedButton));
-    await tester.pumpAndSettle();
+      // Open date picker.
+      await tester.tap(find.byType(OutlinedButton));
+      await tester.pumpAndSettle();
 
-    final Finder textField = find.byType(TextField);
-    expect(textField, findsOneWidget);
+      final Finder textField = find.byType(TextField);
+      expect(textField, findsOneWidget);
 
-    // Enter date: 30/07/2021.
-    await tester.enterText(textField, '30072021');
-    await tester.pump();
+      // Enter date: 30/07/2021.
+      await tester.enterText(textField, '30072021');
+      await tester.pump();
 
-    await tester.tap(find.text('OK'));
-    await tester.pumpAndSettle();
+      await tester.tap(find.text('OK'));
+      await tester.pumpAndSettle();
 
-    expect(find.text('30.07.2021'), findsOneWidget);
+      expect(find.text('30.07.2021'), findsOneWidget);
 
-    // Change format to monthDayYear.
-    await tester.tap(find.byType(DropdownButtonFormField<example.DateInputFormat>));
-    await tester.pumpAndSettle();
-    await tester.tap(find.text('mm/dd/yyyy').last);
-    await tester.pumpAndSettle();
+      // Change format to monthDayYear.
+      await tester.tap(
+        find.byType(DropdownButtonFormField<example.DateInputFormat>),
+      );
+      await tester.pumpAndSettle();
+      await tester.tap(find.text('mm/dd/yyyy').last);
+      await tester.pumpAndSettle();
 
-    expect(find.text('07.30.2021'), findsOneWidget);
+      expect(find.text('07.30.2021'), findsOneWidget);
 
-    // Change separator to dash.
-    await tester.tap(find.byType(DropdownButtonFormField<example.DateSeparator>));
-    await tester.pumpAndSettle();
-    await tester.tap(find.text('-').last);
-    await tester.pumpAndSettle();
+      // Change separator to dash.
+      await tester.tap(
+        find.byType(DropdownButtonFormField<example.DateSeparator>),
+      );
+      await tester.pumpAndSettle();
+      await tester.tap(find.text('-').last);
+      await tester.pumpAndSettle();
 
-    expect(find.text('07-30-2021'), findsOneWidget);
+      expect(find.text('07-30-2021'), findsOneWidget);
 
-    // Change format to yearMonthDay.
-    await tester.tap(find.byType(DropdownButtonFormField<example.DateInputFormat>));
-    await tester.pumpAndSettle();
-    await tester.tap(find.text('yyyy/mm/dd').last);
-    await tester.pumpAndSettle();
+      // Change format to yearMonthDay.
+      await tester.tap(
+        find.byType(DropdownButtonFormField<example.DateInputFormat>),
+      );
+      await tester.pumpAndSettle();
+      await tester.tap(find.text('yyyy/mm/dd').last);
+      await tester.pumpAndSettle();
 
-    expect(find.text('2021-07-30'), findsOneWidget);
-  });
+      expect(find.text('2021-07-30'), findsOneWidget);
+    },
+  );
 }
