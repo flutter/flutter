d44cc36258bfb5c982b6d133b1e162d3eda84f17
Benson Luk <97480502+b-luk@users.noreply.github.com>
2026-01-20 09:36:03 -0800

    dart format

--- packages/flutter/test_fixes/material/widget_state.dart
+++ packages/flutter/test_fixes/material/widget_state.dart
@@ -51,7 +51,7 @@ void main() {
       });
 
   final Color backgroundColor = MaterialStatePropertyAll<Color>(
-    Colors.blue.withValues(alpha: 0.12)
+    Colors.blue.withValues(alpha: 0.12),
   );
 
   final MaterialStatesController statesController = MaterialStatesController(
