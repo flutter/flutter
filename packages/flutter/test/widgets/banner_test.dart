7ea324728627cc8151cdea00f71708870a4b0573
Kazbek Sultanov <kazbek.sultanov.doc@gmail.com>
2026-01-22 13:48:31 +0400

    removed material import

--- packages/flutter/test/widgets/banner_test.dart
+++ packages/flutter/test/widgets/banner_test.dart
@@ -4,7 +4,7 @@
 
 import 'dart:math' as math;
 
-import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:leak_tracker_flutter_testing/leak_tracker_flutter_testing.dart';
 
