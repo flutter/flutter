53b7014dd29ed3923de50eb485c1285b6f552f10
Navaron Bracke <brackenavaron@gmail.com>
2026-01-24 14:14:47 +0100

    remove material from debug_test.dart

--- dev/bots/check_tests_cross_imports.dart
+++ dev/bots/check_tests_cross_imports.dart
@@ -185,7 +185,6 @@ class TestsCrossImportChecker {
     'packages/flutter/test/widgets/editable_text_show_on_screen_test.dart',
     'packages/flutter/test/widgets/system_context_menu_test.dart',
     'packages/flutter/test/widgets/scrollable_fling_test.dart',
-    'packages/flutter/test/widgets/debug_test.dart',
     'packages/flutter/test/widgets/transformed_scrollable_test.dart',
     'packages/flutter/test/widgets/run_app_async_test.dart',
     'packages/flutter/test/widgets/scrollable_in_overlay_test.dart',
