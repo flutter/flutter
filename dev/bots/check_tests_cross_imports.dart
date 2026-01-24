d2c55f401ddcfd7b788b9dea0098732791ab7035
Navaron Bracke <brackenavaron@gmail.com>
2026-01-24 16:15:11 +0100

    remove material from scroll_activity_test.dart

--- dev/bots/check_tests_cross_imports.dart
+++ dev/bots/check_tests_cross_imports.dart
@@ -214,7 +214,6 @@ class TestsCrossImportChecker {
     'packages/flutter/test/widgets/page_transitions_builder_test.dart',
     'packages/flutter/test/widgets/selectable_region_context_menu_test.dart',
     'packages/flutter/test/widgets/sliversemantics_test.dart',
-    'packages/flutter/test/widgets/scroll_activity_test.dart',
     'packages/flutter/test/widgets/tap_region_test.dart',
     'packages/flutter/test/widgets/lookup_boundary_test.dart',
     'packages/flutter/test/widgets/reassemble_test.dart',
