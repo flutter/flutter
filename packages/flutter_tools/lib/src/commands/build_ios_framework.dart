50d0fe3f534b72af121de0f9650c70924e0b6ed3
Daco Harkes <dacoharkes@google.com>
2026-01-26 20:28:19 +0100

    [native_assets] Fix `flutter build ios-framework`

--- packages/flutter_tools/lib/src/commands/build_ios_framework.dart
+++ packages/flutter_tools/lib/src/commands/build_ios_framework.dart
@@ -318,27 +318,31 @@ class BuildIOSFrameworkCommand extends BuildFrameworkCommand {
         ' └─Moving to ${globals.fs.path.relative(modeDirectory.path)}',
       );
 
-      // Copy the native assets. The native assets have already been signed in
-      // buildNativeAssetsMacOS.
-      final Directory nativeAssetsDirectory = globals.fs
-          .directory(getBuildDirectory())
-          .childDirectory('native_assets/ios/');
-      if (await nativeAssetsDirectory.exists()) {
-        final ProcessResult rsyncResult = await globals.processManager.run(<Object>[
-          'rsync',
-          '-av',
-          '--filter',
-          '- .DS_Store',
-          '--filter',
-          '- native_assets.yaml',
-          '--filter',
-          '- native_assets.json',
-          nativeAssetsDirectory.path,
-          modeDirectory.path,
-        ]);
-        if (rsyncResult.exitCode != 0) {
-          throwToolExit('Failed to copy native assets:\n${rsyncResult.stderr}');
-        }
+      // Package native assets.
+      final Directory nativeAssetSimulatorDirectory = globals.fs.directory(
+        project.directory.uri.resolve(
+          '${getBuildDirectory()}/native_assets/ios_${EnvironmentType.simulator.name}/',
+        ),
+      );
+      final Directory nativeAssetDeviceDirectory = globals.fs.directory(
+        project.directory.uri.resolve(
+          '${getBuildDirectory()}/native_assets/ios_${EnvironmentType.physical.name}/',
+        ),
+      );
+      final Iterable<Directory> frameworkDirectoriesSimulator = nativeAssetSimulatorDirectory
+          .listSync()
+          .whereType<Directory>();
+      for (final frameworkDirectorySimulator in frameworkDirectoriesSimulator) {
+        final Directory frameworkDirectoryDevice = nativeAssetDeviceDirectory.childDirectory(
+          frameworkDirectorySimulator.basename,
+        );
+        final frameworks = [frameworkDirectoryDevice, frameworkDirectorySimulator];
+        await BuildFrameworkCommand.produceXCFramework(
+          frameworks,
+          frameworkDirectoryDevice.basename.replaceAll('.framework', ''),
+          modeDirectory,
+          globals.processManager,
+        );
       }
 
       try {
@@ -514,6 +518,9 @@ end
             ).map((DarwinArch e) => e.name).join(' '),
             kSdkRoot: await globals.xcode!.sdkLocation(sdkType),
             ...buildInfo.toBuildSystemEnvironment(),
+            kNativeAssetsInstallDirectory: project.directory.uri
+                .resolve('${getBuildDirectory()}/native_assets/ios_${sdkType.name}/')
+                .toString(),
           },
           artifacts: globals.artifacts!,
           fileSystem: globals.fs,
--- packages/flutter_tools/lib/src/commands/build_macos_framework.dart
+++ packages/flutter_tools/lib/src/commands/build_macos_framework.dart
@@ -7,7 +7,6 @@ import 'package:meta/meta.dart';
 import '../artifacts.dart';
 import '../base/common.dart';
 import '../base/file_system.dart';
-import '../base/io.dart';
 import '../base/logger.dart';
 import '../base/process.dart';
 import '../build_info.dart';
@@ -118,26 +117,21 @@ class BuildMacOSFrameworkCommand extends BuildFrameworkCommand {
 
       globals.logger.printStatus(' └─Moving to ${globals.fs.path.relative(modeDirectory.path)}');
 
-      // Copy the native assets.
+      // Package native assets.
       final Directory nativeAssetsDirectory = globals.fs
           .directory(getBuildDirectory())
           .childDirectory('native_assets/macos/');
-      if (await nativeAssetsDirectory.exists()) {
-        final ProcessResult rsyncResult = await globals.processManager.run(<Object>[
-          'rsync',
-          '-av',
-          '--filter',
-          '- .DS_Store',
-          '--filter',
-          '- native_assets.yaml',
-          '--filter',
-          '- native_assets.json',
-          nativeAssetsDirectory.path,
-          modeDirectory.path,
-        ]);
-        if (rsyncResult.exitCode != 0) {
-          throwToolExit('Failed to copy native assets:\n${rsyncResult.stderr}');
-        }
+      final Iterable<Directory> frameworkDirectories = nativeAssetsDirectory
+          .listSync()
+          .whereType<Directory>();
+      for (final frameworkDirectory in frameworkDirectories) {
+        final frameworks = [frameworkDirectory];
+        await BuildFrameworkCommand.produceXCFramework(
+          frameworks,
+          frameworkDirectory.basename.replaceAll('.framework', ''),
+          modeDirectory,
+          globals.processManager,
+        );
       }
 
       // Delete the intermediaries since they would have been copied into our
