233226e4c9d3cc0f3d0fade0ec4ddad6f68e8958
gaaclarke <30870216+gaaclarke@users.noreply.github.com>
2026-01-26 11:05:11 -0800

    Adds impeller backend to skia gold client

--- engine/src/flutter/testing/dart/impeller_enabled.dart
+++ engine/src/flutter/testing/dart/impeller_enabled.dart
@@ -5,3 +5,18 @@
 import 'dart:io';
 
 bool get impellerEnabled => Platform.executableArguments.contains('--enable-impeller');
+
+String? get impellerBackend {
+  if (!impellerEnabled) {
+    return null;
+  }
+  for (final String arg in Platform.executableArguments) {
+    if (arg.startsWith('--impeller-backend=')) {
+      return arg.substring('--impeller-backend='.length);
+    }
+  }
+  if (Platform.isMacOS || Platform.isIOS) {
+    return 'metal';
+  }
+  return 'vulkan';
+}
