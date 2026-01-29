1f4a817d6405a2f804b0f691a99f754cc1ce2254
Matt Boetger <boetger@google.com>
2026-01-29 13:13:00 -0800

    add flag for content sizing

--- engine/src/flutter/shell/platform/android/external_view_embedder/external_view_embedder_2.cc
+++ engine/src/flutter/shell/platform/android/external_view_embedder/external_view_embedder_2.cc
@@ -246,10 +246,10 @@ void AndroidExternalViewEmbedder2::PrepareFlutterView(
   }
   surface_pool_->SetFrameSize(frame_size);
 
-  task_runners_.GetPlatformTaskRunner()->PostTask(fml::MakeCopyable(
-        [jni_facade = jni_facade_, frame_size = frame_size]() {
-          jni_facade->MaybeResizeSurfaceView(frame_size.width, frame_size.height);
-        }));
+  task_runners_.GetPlatformTaskRunner()->PostTask(
+      fml::MakeCopyable([jni_facade = jni_facade_, frame_size = frame_size]() {
+        jni_facade->MaybeResizeSurfaceView(frame_size.width, frame_size.height);
+      }));
 
   frame_size_ = frame_size;
   device_pixel_ratio_ = device_pixel_ratio;
