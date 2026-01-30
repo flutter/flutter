89d32348c6dfa2126d6c79145228d28ea120cb4f
ilyosbek22 <ilyosbek.ibrokhimov@talabat.com>
2026-01-30 16:31:08 +0400

    removed raw_radio_test.dart from check_tests_cross_imports.dart

--- dev/bots/check_tests_cross_imports.dart
+++ dev/bots/check_tests_cross_imports.dart
@@ -245,7 +245,6 @@ class TestsCrossImportChecker {
     'packages/flutter/test/widgets/rotated_box_test.dart',
     'packages/flutter/test/widgets/single_child_scroll_view_test.dart',
     'packages/flutter/test/widgets/pinned_header_sliver_test.dart',
-    'packages/flutter/test/widgets/raw_radio_test.dart',
     'packages/flutter/test/widgets/syncing_test.dart',
     'packages/flutter/test/widgets/form_test.dart',
   };
