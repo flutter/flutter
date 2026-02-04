3a9568f51dff00ccb4b4176815555557bdcc8ea0
Nicholas Shahan <nshahan@google.com>
2026-02-04 13:52:16 -0800

    [flutter_tools] Deprecate web hot reload flag (#181884)
    Warn when the flag is passed, and prompt to file an issue if disabling
    is required for a web development workflow.
    
    The flag is enabled by default. It is still functional, but will be
    removed in an upcoming release.

--- packages/flutter_tools/lib/src/commands/run.dart
+++ packages/flutter_tools/lib/src/commands/run.dart
@@ -699,6 +699,26 @@ class RunCommand extends RunCommandBase {
       throwToolExit('Skwasm renderer requires --wasm');
     }
 
+    if (argResults?.wasParsed(FlutterOptions.kWebExperimentalHotReload) ?? false) {
+      final bool webEnableHotReload = boolArg(FlutterOptions.kWebExperimentalHotReload);
+      if (webEnableHotReload) {
+        globals.printWarning(
+          'Hot reload on the web is now enabled by default. '
+          'The "--${FlutterOptions.kWebExperimentalHotReload}" flag is deprecated '
+          'and will be removed in an upcoming release.',
+        );
+      } else {
+        globals.printWarning(
+          'Hot reload on the web is now enabled by default. '
+          'The "--no-${FlutterOptions.kWebExperimentalHotReload}" flag is deprecated '
+          'and will be removed in an upcoming release. '
+          'If your web development workflow depends on disabling hot reload, '
+          'please open an issue explaining why at '
+          'https://github.com/dart-lang/sdk/issues/new?template=5_web_hot_reload.yml.',
+        );
+      }
+    }
+
     final String? flavor = stringArg('flavor');
     final bool flavorsSupportedOnEveryDevice = devices!.every(
       (Device device) => device.supportsFlavors,
