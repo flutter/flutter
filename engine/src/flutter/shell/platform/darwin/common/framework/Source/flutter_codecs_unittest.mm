a10210513a186f73bfb3e61354ec4ed2c7b2c906
LongCatIsLooong <31859944+LongCatIsLooong@users.noreply.github.com>
2026-02-04 10:53:52 -0800

    Simplifies the "simple" value handling and prevent unpaired surrogates from crashing the program.
    This is also for strict concurrency migration (#181684) because we won't
    be able to test isolation contracts in objective-c (although it is
    unlikely that the codecs stuff need extra isolation keywords since they
    should be thread safe but it might need to be marked as sendable).

--- engine/src/flutter/shell/platform/darwin/common/framework/Source/flutter_codecs_unittest.mm
+++ engine/src/flutter/shell/platform/darwin/common/framework/Source/flutter_codecs_unittest.mm
@@ -8,67 +8,7 @@
 
 FLUTTER_ASSERT_ARC
 
-TEST(FlutterStringCodec, CanEncodeAndDecodeNil) {
-  FlutterStringCodec* codec = [FlutterStringCodec sharedInstance];
-  ASSERT_TRUE([codec encode:nil] == nil);
-  ASSERT_TRUE([codec decode:nil] == nil);
-}
 
-TEST(FlutterStringCodec, CanEncodeAndDecodeEmptyString) {
-  FlutterStringCodec* codec = [FlutterStringCodec sharedInstance];
-  ASSERT_TRUE([[codec encode:@""] isEqualTo:[NSData data]]);
-  ASSERT_TRUE([[codec decode:[NSData data]] isEqualTo:@""]);
-}
-
-TEST(FlutterStringCodec, CanEncodeAndDecodeAsciiString) {
-  NSString* value = @"hello world";
-  FlutterStringCodec* codec = [FlutterStringCodec sharedInstance];
-  NSData* encoded = [codec encode:value];
-  NSString* decoded = [codec decode:encoded];
-  ASSERT_TRUE([value isEqualTo:decoded]);
-}
-
-TEST(FlutterStringCodec, CanEncodeAndDecodeNonAsciiString) {
-  NSString* value = @"hello \u263A world";
-  FlutterStringCodec* codec = [FlutterStringCodec sharedInstance];
-  NSData* encoded = [codec encode:value];
-  NSString* decoded = [codec decode:encoded];
-  ASSERT_TRUE([value isEqualTo:decoded]);
-}
-
-TEST(FlutterStringCodec, CanEncodeAndDecodeNonBMPString) {
-  NSString* value = @"hello \U0001F602 world";
-  FlutterStringCodec* codec = [FlutterStringCodec sharedInstance];
-  NSData* encoded = [codec encode:value];
-  NSString* decoded = [codec decode:encoded];
-  ASSERT_TRUE([value isEqualTo:decoded]);
-}
-
-TEST(FlutterJSONCodec, ThrowsOnInvalidEncode) {
-  NSString* value = [[NSString alloc] initWithBytes:"\xdf\xff"
-                                             length:2
-                                           encoding:NSUTF16StringEncoding];
-  FlutterJSONMessageCodec* codec = [FlutterJSONMessageCodec sharedInstance];
-  EXPECT_EXIT([codec encode:value], testing::KilledBySignal(SIGABRT), "failed to convert to UTF8");
-}
-
-TEST(FlutterJSONCodec, CanDecodeZeroLength) {
-  FlutterJSONMessageCodec* codec = [FlutterJSONMessageCodec sharedInstance];
-  ASSERT_TRUE([codec decode:[NSData data]] == nil);
-}
-
-TEST(FlutterJSONCodec, ThrowsOnInvalidDecode) {
-  NSString* value = @"{{{";
-  FlutterJSONMessageCodec* codec = [FlutterJSONMessageCodec sharedInstance];
-  EXPECT_EXIT([codec decode:[value dataUsingEncoding:value.fastestEncoding]],
-              testing::KilledBySignal(SIGABRT), "No string key for value in object around line 1");
-}
-
-TEST(FlutterJSONCodec, CanEncodeAndDecodeNil) {
-  FlutterJSONMessageCodec* codec = [FlutterJSONMessageCodec sharedInstance];
-  ASSERT_TRUE([codec encode:nil] == nil);
-  ASSERT_TRUE([codec decode:nil] == nil);
-}
 
 TEST(FlutterJSONCodec, CanEncodeAndDecodeArray) {
   NSArray* value = @[ [NSNull null], @"hello", @3.14, @47, @{@"a" : @"nested"} ];
