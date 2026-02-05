4f1ff47077e67ac9a79e3e269b0111761cbcfd0d
Victor Sanni <victorsanniay@gmail.com>
2026-02-05 10:46:41 -0800

    Mediaquery inherited model

--- packages/flutter/lib/src/widgets/autocomplete.dart
+++ packages/flutter/lib/src/widgets/autocomplete.dart
@@ -584,14 +584,11 @@ class _RawAutocompleteState<T extends Object> extends State<RawAutocomplete<T>> 
     final Size fieldSize = layoutInfo.childSize;
     final Matrix4 invertTransform = layoutInfo.childPaintTransform.clone()..invert();
 
-final EdgeInsets mediaQueryPadding = MediaQuery.paddingOf(context);
-final EdgeInsets viewInsets = MediaQuery.viewInsetsOf(context);
+    final EdgeInsets mediaQueryPadding = MediaQuery.paddingOf(context);
+    final EdgeInsets viewInsets = MediaQuery.viewInsetsOf(context);
 
-final Rect overlayRect = mediaQueryPadding.deflateRect(
-  viewInsets.deflateRect(Offset.zero & layoutInfo.overlaySize),
-);
-    final Rect overlayRect = mediaQueryData.padding.deflateRect(
-      mediaQueryData.viewInsets.deflateRect(Offset.zero & layoutInfo.overlaySize),
+    final Rect overlayRect = mediaQueryPadding.deflateRect(
+      viewInsets.deflateRect(Offset.zero & layoutInfo.overlaySize),
     );
 
     // This may not work well if the paint transform has rotation in it.
