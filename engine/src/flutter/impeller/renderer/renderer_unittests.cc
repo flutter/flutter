c4878a736014d10469d69d3eb73679223f5676b7
Andrew Marshall <planetmarshalluk@gmail.com>
2026-02-06 21:56:24 +0000

    fix rebase

--- engine/src/flutter/impeller/renderer/renderer_unittests.cc
+++ engine/src/flutter/impeller/renderer/renderer_unittests.cc
@@ -94,7 +94,6 @@ TEST_P(RendererTest, CanCreateBoxPrimitive) {
   ASSERT_TRUE(bridge && boston);
   raw_ptr<const Sampler> sampler = context->GetSamplerLibrary()->GetSampler({});
   ASSERT_TRUE(sampler);
-  auto [data_host_buffer, indexes_host_buffer] = createHostBuffers(context);
 
   auto [data_host_buffer, indexes_host_buffer] = createHostBuffers(context);
   SinglePassCallback callback = [&](RenderPass& pass) {
