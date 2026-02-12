5adfbcdcf8285650ffa3807a06c19d6121868920
Soufian El-Fouzari <elfouzari.soufian@gmail.com>
2026-02-12 16:35:19 +0100

    Add Test for German Hello World

---
+++ examples/hello_world/test/hello_test_german.dart
@@ -0,0 +1,21 @@
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import '../lib/german.dart' as hello_world;
+
+void main() {
+  testWidgets('Hallo Welt test', (WidgetTester tester) async {
+    hello_world
+        .main(); // startet die App und bereitet einen Fram vor
+    await tester.pump(); // Führt den vorbereiteten Frame aus
+
+    expect(
+      find.byKey(const Key('title')), // sucht für ein Widget mit den Schlüssel 'title'
+      findsOneWidget, // Der Test besteht, sofern ein Widget mit folgendem Schlüssel gefunden wurde
+    ); 
+    
+  });
+}
