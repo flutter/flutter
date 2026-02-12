d268c2e960fe33a6a1f8c1ef567281216f13d9e2
Soufian El-Fouzari <elfouzari.soufian@gmail.com>
2026-02-12 16:31:15 +0100

    Add Test for Arabic Hello World

---
+++ examples/hello_world/test/hello_test_arabic.dart
@@ -0,0 +1,23 @@
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// جميع الحقوق محفوظة لمطوري Flutter لعام 2014.
+// يخضع استخدام هذا الكود لرخصة من نوع BSD ويمكن العثور عليها في ملف LICENSE.
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import '../lib/arabic.dart' as hello_world;
+
+void main() {
+  testWidgets('اختبار مرحبا بالعالم', (WidgetTester tester) async {
+    hello_world.main(); // تشغيل التطبيق
+    await tester.pump(); // انتظار بناء واجهة المستخدم
+
+    expect(
+      find.byKey(const Key('title')), // البحث عن العنصر باستخدام المفتاح
+      findsOneWidget, // ينجح الاختبار إذا تم العثور على عنصر واحد
+    );
+
+  });
+}
