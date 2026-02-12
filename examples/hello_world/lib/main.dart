874aa281e31112fe6d0c3cc753268db3884e9b56
Soufian El-Fouzari <elfouzari.soufian@gmail.com>
2026-02-12 16:32:56 +0100

    Formatted main function

--- examples/hello_world/lib/main.dart
+++ examples/hello_world/lib/main.dart
@@ -6,6 +6,10 @@ import 'package:flutter/widgets.dart';
 
 void main() => runApp(
   const Center(
-    child: Text('Hello, world!', key: Key('title'), textDirection: TextDirection.ltr),
+    child: Text(
+      'Hello, world!',
+      key: Key('title'),
+      textDirection: TextDirection.ltr,
+    ),
   ),
 );
