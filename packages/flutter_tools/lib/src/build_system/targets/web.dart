811555080c962723f4e1b266c99c241860e55998
flutteractionsbot <154381524+flutteractionsbot@users.noreply.github.com>
2026-02-17 13:17:10 -0800

    [CP-beta]Fix `flutter build web` ignoring `--web-define` flag (#182345)
    This pull request is created by [automatic cherry pick workflow](https://github.com/flutter/flutter/blob/main/docs/releases/Flutter-Cherrypick-Process.md#automatically-creates-a-cherry-pick-request)
    Please fill in the form below, and a flutter domain expert will evaluate this cherry pick request.
    
    ### Issue Link:
    What is the link to the issue this cherry-pick is addressing?
    
    https://github.com/flutter/flutter/issues/182076
    
    ### Impact Description:
    What is the impact (ex. visual jank on Samsung phones, app crash, cannot ship an iOS app)?
    Does it impact development (ex. flutter doctor crashes when Android Studio is installed),
    or the shipping of production apps (the app crashes on launch).
    This information is for domain experts and release engineers to understand the consequences of saying yes or no to the cherry pick.
    
    The `--web-define` feature works during development (i.e. `flutter run`), but doesn't work for production (i.e. `flutter build web`).
    
    ### Changelog Description:
    Explain this cherry pick:
    * In one line that is accessible to most Flutter developers.
    * That describes the state prior to the fix.
    * That includes which platforms are impacted.
    See [best practices](https://github.com/flutter/flutter/blob/main/docs/releases/Hotfix-Documentation-Best-Practices.md) for examples.
    
    Fix `flutter build web` ignoring `--web-define` flag
    
    ### Workaround:
    Is there a workaround for this issue?
    
    No workaround.
    
    ### Risk:
    What is the risk level of this cherry-pick?
    
    ### Test Coverage:
    Are you confident that your fix is well-tested by automated tests?
    
    ### Validation Steps:
    What are the steps to validate that this fix works?
    
    Follow repro steps in https://github.com/flutter/flutter/issues/182076

--- packages/flutter_tools/lib/src/build_system/targets/web.dart
+++ packages/flutter_tools/lib/src/build_system/targets/web.dart
@@ -726,6 +726,13 @@ _flutter.buildConfig = ${jsonEncode(buildConfig)};
 
     final String buildConfig = buildConfigString(environment);
 
+    // Extract web-define variables from the environment. These are stored with
+    // the [kWebDefinePrefix] prefix by [WebBuilder.buildWeb].
+    final webDefines = <String, String>{
+      for (final MapEntry(:key, :value) in environment.defines.entries)
+        if (key.startsWith(kWebDefinePrefix)) key.substring(kWebDefinePrefix.length): value,
+    };
+
     // Insert a random hash into the requests for service_worker.js. This is not a content hash,
     // because it would need to be the hash for the entire bundle and not just the resource
     // in question.
@@ -737,6 +744,7 @@ _flutter.buildConfig = ${jsonEncode(buildConfig)};
       serviceWorkerVersion: serviceWorkerVersion,
       flutterJsFile: flutterJsFile,
       buildConfig: buildConfig,
+      webDefines: webDefines,
     );
 
     final File outputFlutterBootstrapJs = fileSystem.file(
@@ -760,6 +768,7 @@ _flutter.buildConfig = ${jsonEncode(buildConfig)};
           flutterJsFile: flutterJsFile,
           buildConfig: buildConfig,
           flutterBootstrapJs: bootstrapContent,
+          webDefines: webDefines,
         );
         final File outputIndexHtml = fileSystem.file(
           fileSystem.path.join(environment.outputDir.path, relativePath),
