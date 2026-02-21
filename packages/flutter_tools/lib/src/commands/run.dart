29d9ac6476e025eccca0db9bb7c147c6195a0fe5
Codefarmer <abadasamuelosp@gmail.com>
2026-02-21 15:19:51 +0100

    chore: read baseHref from fileConfig to ensure consistency

--- packages/flutter_tools/lib/src/commands/run.dart
+++ packages/flutter_tools/lib/src/commands/run.dart
@@ -413,7 +413,7 @@ abstract class RunCommandBase extends FlutterCommand with DeviceBasedDevelopment
       stringArg('web-tls-cert-key-path') ?? fileConfig.https?.certKeyPath,
     );
 
-    final String? baseHref = stringArg('base-href');
+    final String? baseHref = stringArg('base-href') ?? fileConfig.baseHref;
     if (baseHref != null && !(baseHref.startsWith('/') && baseHref.endsWith('/'))) {
       throwToolExit(
         'Received a --base-href value of "$baseHref"\n'
