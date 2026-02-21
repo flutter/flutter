4fe1518450408cf2ff26f316480d119048a75c39
Victor Sanni <victorsanniay@gmail.com>
2026-02-20 18:24:31 -0800

    Shortcircuit if Tooltip message and richMessage are empty (#182524)
    Regressed when migrating to `RawTooltip`. 
    
    The unit tests added in https://github.com/flutter/flutter/pull/87638 to
    prevent this from happening were faulty, I've updated them to reflect
    this feature.
    
    Fixes [[flutter 3.41.x] NavigationDestination shows empty
    tooltip](https://github.com/flutter/flutter/issues/182495)

--- packages/flutter_test/lib/src/finders.dart
+++ packages/flutter_test/lib/src/finders.dart
@@ -374,20 +374,22 @@ class CommonFinders {
   /// nodes that are [Offstage] or that are from inactive [Route]s.
   Finder byTooltip(Pattern message, {bool skipOffstage = true}) {
     return byWidgetPredicate((Widget widget) {
-      // In cases where Tooltip.excludeFromSemantics is true, Tooltip provides
-      // no semantics tooltip to RawTooltip, so its message must be checked
-      // directly.
-      if (widget is Tooltip && (widget.excludeFromSemantics ?? false)) {
-        return (message is RegExp
-            ? ((widget.message != null && message.hasMatch(widget.message!)) ||
-                  (widget.richMessage != null &&
-                      message.hasMatch(widget.richMessage!.toPlainText())))
-            : ((widget.message ?? widget.richMessage?.toPlainText()) == message));
+      // Compare RawTooltip's semantics tooltip with the given message.
+      // However, Tooltip's message needs to be checked directly if:
+      // 1. Tooltip.excludeFromSemantics is true, since in this case Tooltip
+      //    provides no semantics tooltip to the underlying RawTooltip.
+      // 2. Tooltip.message and Tooltip.richMessage are empty, since in this
+      //    case no RawTooltip is created.
+      if (widget is Tooltip) {
+        final String tooltipMessage = widget.message ?? widget.richMessage!.toPlainText();
+        if ((widget.excludeFromSemantics ?? false) || tooltipMessage.isEmpty) {
+          return message is RegExp ? message.hasMatch(tooltipMessage) : tooltipMessage == message;
+        }
       }
       return widget is RawTooltip &&
           (message is RegExp
               ? message.hasMatch(widget.semanticsTooltip ?? '')
-              : (widget.semanticsTooltip == message));
+              : widget.semanticsTooltip == message);
     }, skipOffstage: skipOffstage);
   }
 
