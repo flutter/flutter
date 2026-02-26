638f0362222804d145ebebd7f4343568dc18b4f3
Robert Ancell <robert.ancell@canonical.com>
2026-02-26 15:44:02 +1300

    Add comment on how resize is performed

--- engine/src/flutter/shell/platform/linux/fl_view.cc
+++ engine/src/flutter/shell/platform/linux/fl_view.cc
@@ -129,6 +129,8 @@ static gboolean redraw_cb(gpointer user_data) {
     GtkWidget* toplevel =
         gtk_widget_get_toplevel(GTK_WIDGET(self->render_area));
     if (GTK_IS_WINDOW(toplevel)) {
+      // Resize to smallest size, so that the window will shrink to fit the new
+      // size of the render area.
       gtk_window_resize(GTK_WINDOW(toplevel), 1, 1);
     }
     return FALSE;
