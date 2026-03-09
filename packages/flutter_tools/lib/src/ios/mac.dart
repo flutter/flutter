dacc606047abeb402885406d0f012b515f119c72
Alex Medinsh <alex.medinsh@gmail.com>
2026-03-09 19:39:02 +0200

    todo

--- packages/flutter_tools/lib/src/ios/mac.dart
+++ packages/flutter_tools/lib/src/ios/mac.dart
@@ -213,7 +213,7 @@ Future<XcodeBuildResult> buildXcodeProject({
   }
   final String? configuration = projectInfo.buildConfigurationFor(buildInfo, scheme);
   if (configuration == null) {
-    // TODO: Update message
+    // TODO(alex-medinsh): Update message?
     globals.printError('');
     globals.printError(
       'The Xcode project defines build configurations: ${projectInfo.buildConfigurations.join(', ')}',
