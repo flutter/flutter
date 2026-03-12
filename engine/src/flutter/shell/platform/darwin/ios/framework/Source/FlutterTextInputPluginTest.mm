flutter/flutter

commit 4ea3451537178462dd678481caf272f50c67518c

[ios][engine] Fix autofill context cleanup and view lifecycle management (#173598)
fix https://github.com/flutter/flutter/issues/145681

I created a PR based on the survey described in
[comment](https://github.com/flutter/flutter/issues/145681#issuecomment-3151444768).
The existing processing is not deleted, but the method to be executed is
moved. If `_autofillContext` is not empty, the timing of removing
`activeView` and `_inputHider` is delayed.

## Pre-launch Checklist

- [x] I read the [Contributor Guide] and followed the process outlined
there for submitting PRs.
- [x] I read the [Tree Hygiene] wiki page, which explains my
responsibilities.
- [x] I read and followed the [Flutter Style Guide], including [Features
we expect every widget to implement].
- [x] I signed the [CLA].
- [x] I listed at least one issue that this PR fixes in the description
above.
- [x] I updated/added relevant documentation (doc comments with `///`).
- [x] I added new tests to check the change I am making, or this PR is
[test-exempt].
- [x] I followed the [breaking change policy] and added [Data Driven
Fixes] where supported.
- [x] All existing and new tests are passing.

If you need help, consider asking for advice on the #hackers-new channel
on [Discord].

**Note**: The Flutter team is currently trialing the use of [Gemini Code
Assist for
GitHub](https://developers.google.com/gemini-code-assist/docs/review-github-code).
Comments from the `gemini-code-assist` bot should not be taken as
authoritative feedback from the Flutter team. If you find its comments
useful you can update your code accordingly, but if you are unsure or
disagree with the feedback, please feel free to wait for a Flutter team
member's review for guidance on which automated comments should be
addressed.

<!-- Links -->
[Contributor Guide]:
https://github.com/flutter/flutter/blob/main/docs/contributing/Tree-hygiene.md#overview
[Tree Hygiene]:
https://github.com/flutter/flutter/blob/main/docs/contributing/Tree-hygiene.md

engine/src/flutter/shell/platform/darwin/ios/framework/Source/FlutterTextInputPlugin.mm
@@ -2497,6 +2497,7 @@ static BOOL IsSelectionRectBoundaryCloserToPoint(CGPoint point,
 // The current password-autofillable input fields that have yet to be saved.
 @property(nonatomic, readonly)
     NSMutableDictionary<NSString*, FlutterTextInputView*>* autofillContext;
+@property(nonatomic, readonly) BOOL pendingInputHiderRemoval;
 @property(nonatomic, retain) FlutterTextInputView* activeView;
 @property(nonatomic, retain) FlutterTextInputViewAccessibilityHider* inputHider;
 @property(nonatomic, readonly, weak) id<FlutterViewResponder> viewResponder;
@@ -2511,6 +2512,7 @@ static BOOL IsSelectionRectBoundaryCloserToPoint(CGPoint point,
 
 @implementation FlutterTextInputPlugin {
   NSTimer* _enableFlutterTextInputViewAccessibilityTimer;
+  BOOL _pendingInputHiderRemoval;
 }
 
 - (instancetype)initWithDelegate:(id<FlutterTextInputDelegate>)textInputDelegate {
@@ -2539,7 +2541,7 @@ static BOOL IsSelectionRectBoundaryCloserToPoint(CGPoint point,
 }
 
 - (void)dealloc {
-  [self hideTextInput];
+  [self reset];
 }
 
 - (void)removeEnableFlutterTextInputViewAccessibilityTimer {
@@ -2554,6 +2556,8 @@ static BOOL IsSelectionRectBoundaryCloserToPoint(CGPoint point,
 }
 
 - (void)reset {
+  [_autofillContext removeAllObjects];
+  [self clearTextInputClient];
   [self hideTextInput];
 }
 
@@ -2818,22 +2822,6 @@ static BOOL IsSelectionRectBoundaryCloserToPoint(CGPoint point,
 - (void)showTextInput {
   _activeView.viewResponder = _viewResponder;
   [self addToInputParentViewIfNeeded:_activeView];
-  // Adds a delay to prevent the text view from receiving accessibility
-  // focus in case it is activated during semantics updates.
-  //
-  // One common case is when the app navigates to a page with an auto
-  // focused text field. The text field will activate the FlutterTextInputView
-  // with a semantics update sent to the engine. The voiceover will focus
-  // the newly attached active view while performing accessibility update.
-  // This results in accessibility focus stuck at the FlutterTextInputView.
-  if (!_enableFlutterTextInputViewAccessibilityTimer) {
-    _enableFlutterTextInputViewAccessibilityTimer =
-        [NSTimer scheduledTimerWithTimeInterval:kUITextInputAccessibilityEnablingDelaySeconds
-                                         target:[FlutterTimerProxy proxyWithTarget:self]
-                                       selector:@selector(enableActiveViewAccessibility)
-                                       userInfo:nil
-                                        repeats:NO];
-  }
   [_activeView becomeFirstResponder];
 }
 
@@ -2845,11 +2833,7 @@ static BOOL IsSelectionRectBoundaryCloserToPoint(CGPoint point,
 }
 
 - (void)hideTextInput {
-  [self removeEnableFlutterTextInputViewAccessibilityTimer];
-  _activeView.accessibilityEnabled = NO;
   [_activeView resignFirstResponder];
-  [_activeView removeFromSuperview];
-  [_inputHider removeFromSuperview];
 }
 
 - (void)triggerAutofillSave:(BOOL)saveEntries {
@@ -2866,6 +2850,14 @@ static BOOL IsSelectionRectBoundaryCloserToPoint(CGPoint point,
   }
 
   [self cleanUpViewHierarchy:YES clearText:!saveEntries delayRemoval:NO];
+
+  // Trigger removal of input hider if needed.
+  if (_pendingInputHiderRemoval) {
+    [_activeView removeFromSuperview];
+    [_inputHider removeFromSuperview];
+    _pendingInputHiderRemoval = NO;
+  }
+
   [self addToInputParentViewIfNeeded:_activeView];
 }
 
@@ -2917,6 +2909,23 @@ static BOOL IsSelectionRectBoundaryCloserToPoint(CGPoint point,
   // text fields immediately (which seems to make the keyboard flicker).
   // See: https://github.com/flutter/flutter/issues/64628.
   [self cleanUpViewHierarchy:NO clearText:YES delayRemoval:YES];
+
+  // Adds a delay to prevent the text view from receiving accessibility
+  // focus in case it is activated during semantics updates.
+  //
+  // One common case is when the app navigates to a page with an auto
+  // focused text field. The text field will activate the FlutterTextInputView
+  // with a semantics update sent to the engine. The voiceover will focus
+  // the newly attached active view while performing accessibility update.
+  // This results in accessibility focus stuck at the FlutterTextInputView.
+  if (!_enableFlutterTextInputViewAccessibilityTimer) {
+    _enableFlutterTextInputViewAccessibilityTimer =
+        [NSTimer scheduledTimerWithTimeInterval:kUITextInputAccessibilityEnablingDelaySeconds
+                                         target:[FlutterTimerProxy proxyWithTarget:self]
+                                       selector:@selector(enableActiveViewAccessibility)
+                                       userInfo:nil
+                                        repeats:NO];
+  }
 }
 
 // Creates and shows an input field that is not password related and has no autofill
@@ -3099,6 +3108,17 @@ static BOOL IsSelectionRectBoundaryCloserToPoint(CGPoint point,
 - (void)clearTextInputClient {
   [_activeView setTextInputClient:0];
   _activeView.frame = CGRectZero;
+
+  [self removeEnableFlutterTextInputViewAccessibilityTimer];
+  _activeView.accessibilityEnabled = NO;
+
+  if (_autofillContext.count == 0) {
+    [_activeView removeFromSuperview];
+    [_inputHider removeFromSuperview];
+  } else {
+    // If _autofillContext is not empty, triggerAutofillSave will be called to clean up the views.
+    _pendingInputHiderRemoval = YES;
+  }
 }
 
 - (void)updateConfig:(NSDictionary*)dictionary {

engine/src/flutter/shell/platform/darwin/ios/framework/Source/FlutterTextInputPluginTest.mm
@@ -69,6 +69,7 @@ FLUTTER_ASSERT_ARC
 @property(nonatomic, readonly) UIView* keyboardView;
 @property(nonatomic, assign) UIView* cachedFirstResponder;
 @property(nonatomic, readonly) CGRect keyboardRect;
+@property(nonatomic, readonly) BOOL pendingInputHiderRemoval;
 @property(nonatomic, readonly)
     NSMutableDictionary<NSString*, FlutterTextInputView*>* autofillContext;
 
@@ -168,6 +169,14 @@ class MockPlatformViewDelegate : public PlatformView::Delegate {
       [FlutterMethodCall methodCallWithMethodName:@"TextInput.setClient"
                                         arguments:@[ [NSNumber numberWithInt:clientId], config ]];
   [textInputPlugin handleMethodCall:setClientCall
+                             result:^(id _Nullable result){
+                             }];
+}
+
+- (void)setClientClear {
+  FlutterMethodCall* clearClientCall =
+      [FlutterMethodCall methodCallWithMethodName:@"TextInput.clearClient" arguments:@[]];
+  [textInputPlugin handleMethodCall:clearClientCall
                              result:^(id _Nullable result){
                              }];
 }
@@ -972,6 +981,21 @@ class MockPlatformViewDelegate : public PlatformView::Delegate {
   XCTAssertEqual(selectedTextRange.location, 5ul);
   XCTAssertEqual(selectedTextRange.length, 0ul);
   XCTAssertEqual(inputView.markedTextRange, nil);
+}
+
+- (void)testFlutterTextInputViewIsNotClearWhenKeyboardShowAndHide {
+  // Regression test for https://github.com/flutter/flutter/issues/172250.
+  FlutterTextInputView* inputView = [[FlutterTextInputView alloc] initWithOwner:textInputPlugin];
+  [inputView setMarkedText:@"test text" selectedRange:NSMakeRange(0, 5)];
+  XCTAssertEqualObjects(inputView.text, @"test text");
+
+  // Showing keyboard does not trigger clearing of marked text.
+  [self setTextInputShow];
+  XCTAssertEqualObjects(inputView.text, @"test text");
+
+  // Hiding keyboard does not trigger clearing of marked text.
+  [self setTextInputHide];
+  XCTAssertEqualObjects(inputView.text, @"test text");
 }
 
 - (void)testFlutterTextInputViewOnlyRespondsToInsertionPointColorBelowIOS17 {
@@ -2676,6 +2700,42 @@ class MockPlatformViewDelegate : public PlatformView::Delegate {
                                  withTag:@"field2"]);
 }
 
+- (void)testAutofillContextPersistsAfterClearClient {
+  NSMutableDictionary* field1 = self.mutableTemplateCopy;
+  [field1 setValue:@{
+    @"uniqueIdentifier" : @"field1",
+    @"hints" : @[ @"username" ],
+    @"editingValue" : @{@"text" : @""}
+  }
+            forKey:@"autofill"];
+
+  NSMutableDictionary* field2 = self.mutablePasswordTemplateCopy;
+  [field2 setValue:@{
+    @"uniqueIdentifier" : @"field2",
+    @"hints" : @[ @"password" ],
+    @"editingValue" : @{@"text" : @""}
+  }
+            forKey:@"autofill"];
+
+  NSMutableDictionary* config = [field1 mutableCopy];
+  [config setValue:@[ field1, field2 ] forKey:@"fields"];
+
+  // Verify initial state.
+  [self setClientId:123 configuration:config];
+  XCTAssertEqual(textInputPlugin.autofillContext.count, 2ul);
+  XCTAssertFalse(textInputPlugin.pendingInputHiderRemoval);
+
+  // Retain autofill context.
+  [self setClientClear];
+  XCTAssertEqual(textInputPlugin.autofillContext.count, 2ul);
+  XCTAssertTrue(textInputPlugin.pendingInputHiderRemoval);
+
+  // Consume autofill context.
+  [self commitAutofillContextAndVerify];
+  XCTAssertEqual(textInputPlugin.autofillContext.count, 0ul);
+  XCTAssertFalse(textInputPlugin.pendingInputHiderRemoval);
+}
+
 - (void)testPasswordAutofillHack {
   NSDictionary* config = self.mutableTemplateCopy;
   [config setValue:@"YES" forKey:@"obscureText"];
@@ -2812,21 +2872,55 @@ class MockPlatformViewDelegate : public PlatformView::Delegate {
   XCTAssertNil(textInputPlugin.activeView.textInputDelegate);
 }
 
+- (void)testAutoFillDoesNotTriggerOnShowAndHideKeyboard {
+  // Regression test for https://github.com/flutter/flutter/issues/145681.
+  NSMutableDictionary* configuration = self.mutableTemplateCopy;
+  [configuration setValue:@{
+    @"uniqueIdentifier" : @"field1",
+    @"hints" : @[ UITextContentTypePassword ],
+    @"editingValue" : @{@"text" : @""}
+  }
+                   forKey:@"autofill"];
+  [configuration setValue:@[ [configuration copy] ] forKey:@"fields"];
+  [self setClientId:123 configuration:configuration];
+
+  [self setTextInputShow];
+  XCTAssertEqual(self.viewsVisibleToAutofill.count, 1ul);
+
+  // Hiding keyboard does not trigger showing autofill prompt.
+  [self setTextInputHide];
+  XCTAssertEqual(self.viewsVisibleToAutofill.count, 1ul);
+
+  [self commitAutofillContextAndVerify];
+}
+
 #pragma mark - Accessibility - Tests
 
-- (void)testUITextInputAccessibilityNotHiddenWhenShowed {
+- (void)testUITextInputAccessibilityNotHiddenWhenKeyboardIsShownAndHidden {
   [self setClientId:123 configuration:self.mutableTemplateCopy];
 
-  // Send show text input method call.
-  [self setTextInputShow];
   // Find all the FlutterTextInputViews we created.
   NSArray<FlutterTextInputView*>* inputFields = self.installedInputViews;
 
   // The input view should not be hidden.
   XCTAssertEqual([inputFields count], 1u);
 
+  // Send show text input method call.
+  [self setTextInputShow];
+
+  inputFields = self.installedInputViews;
+
+  XCTAssertEqual([inputFields count], 1u);
+
   // Send hide text input method call.
   [self setTextInputHide];
+
+  inputFields = self.installedInputViews;
+
+  XCTAssertEqual([inputFields count], 1u);
+
+  // Send clear text client method call.
+  [self setClientClear];
 
   inputFields = self.installedInputViews;
 

commit 1712c83ea7d3a3fa28ae44645e708b0dc7626b8b

[ios][engine] Fix keyboard flicker when switching text fields (#182661)
fix https://github.com/flutter/flutter/issues/180842

The issue was caused by https://github.com/flutter/flutter/pull/173598.

When there is a single TextField, the MethodChannel calls are as
follows:

```
handleMethodCall: TextInput.setClient
handleMethodCall: TextInput.setEditableSizeAndTransform
handleMethodCall: TextInput.setMarkedTextRect
handleMethodCall: TextInput.setCaretRect
handleMethodCall: TextInput.setStyle
handleMethodCall: TextInput.setEditingState
handleMethodCall: TextInput.show
handleMethodCall: TextInput.requestAutofill
handleMethodCall: TextInput.clearClient
handleMethodCall: TextInput.hide
```

However, when there are two TextFields, the calls become:


```
handleMethodCall: TextInput.setClient
handleMethodCall: TextInput.setEditableSizeAndTransform
handleMethodCall: TextInput.setMarkedTextRect
handleMethodCall: TextInput.setCaretRect
handleMethodCall: TextInput.setStyle
handleMethodCall: TextInput.setEditingState
handleMethodCall: TextInput.show
handleMethodCall: TextInput.requestAutofill
handleMethodCall: TextInput.clearClient
handleMethodCall: TextInput.setClient
handleMethodCall: TextInput.setEditableSizeAndTransform
handleMethodCall: TextInput.setMarkedTextRect
handleMethodCall: TextInput.setCaretRect
handleMethodCall: TextInput.setStyle
handleMethodCall: TextInput.setEditingState
handleMethodCall: TextInput.show
handleMethodCall: TextInput.requestAutofill
handleMethodCall: TextInput.clearClient
handleMethodCall: TextInput.hide
```

In the multi-field case, `clearTextInputClient` is called before
`setTextInputClient` for the next field. Calling `removeFromSuperview`
during `clearTextInputClient` causes the keyboard to briefly dismiss and
reappear (flicker).

This PR defers the `removeFromSuperview` call from
`clearTextInputClient` to `hideTextInput`, using a pending flag
(`_pendingInputViewRemoval`). The input view is removed only after

engine/src/flutter/shell/platform/darwin/ios/framework/Source/FlutterTextInputPlugin.mm
@@ -2497,7 +2497,15 @@ static BOOL IsSelectionRectBoundaryCloserToPoint(CGPoint point,
 // The current password-autofillable input fields that have yet to be saved.
 @property(nonatomic, readonly)
     NSMutableDictionary<NSString*, FlutterTextInputView*>* autofillContext;
-@property(nonatomic, readonly) BOOL pendingInputHiderRemoval;
+// Whether the client disconnected while an autofill context was active.
+// The removeFromSuperview call is delayed until triggerAutofillSave
+// to avoid prematurely ending the autofill session.
+@property(nonatomic, readonly) BOOL pendingAutofillRemoval;
+// Whether the client disconnected without sending a hideText message.
+// This can indicate that the focus is being switched to a different
+// text field and to prevent flickering the removeFromSuperview
+// call should be delayed until hideTextInput.
+@property(nonatomic, readonly) BOOL pendingInputViewRemoval;
 @property(nonatomic, retain) FlutterTextInputView* activeView;
 @property(nonatomic, retain) FlutterTextInputViewAccessibilityHider* inputHider;
 @property(nonatomic, readonly, weak) id<FlutterViewResponder> viewResponder;
@@ -2512,7 +2520,8 @@ static BOOL IsSelectionRectBoundaryCloserToPoint(CGPoint point,
 
 @implementation FlutterTextInputPlugin {
   NSTimer* _enableFlutterTextInputViewAccessibilityTimer;
-  BOOL _pendingInputHiderRemoval;
+  BOOL _pendingInputViewRemoval;
+  BOOL _pendingAutofillRemoval;
 }
 
 - (instancetype)initWithDelegate:(id<FlutterTextInputDelegate>)textInputDelegate {
@@ -2834,6 +2843,16 @@ static BOOL IsSelectionRectBoundaryCloserToPoint(CGPoint point,
 
 - (void)hideTextInput {
   [_activeView resignFirstResponder];
+
+  // Remove the input view from the view hierarchy after resigning first responder.
+  // This flag is set by clearTextInputClient when autofillContext is empty.
+  // Removing after resignFirstResponder (not during clearTextInputClient) prevents
+  // keyboard flicker when switching between text fields.
+  if (_pendingInputViewRemoval) {
+    [_activeView removeFromSuperview];
+    [_inputHider removeFromSuperview];
+    _pendingInputViewRemoval = NO;
+  }
 }
 
 - (void)triggerAutofillSave:(BOOL)saveEntries {
@@ -2852,10 +2871,11 @@ static BOOL IsSelectionRectBoundaryCloserToPoint(CGPoint point,
   [self cleanUpViewHierarchy:YES clearText:!saveEntries delayRemoval:NO];
 
   // Trigger removal of input hider if needed.
-  if (_pendingInputHiderRemoval) {
+  if (_pendingAutofillRemoval) {
     [_activeView removeFromSuperview];
     [_inputHider removeFromSuperview];
-    _pendingInputHiderRemoval = NO;
+    _pendingAutofillRemoval = NO;
+    _pendingInputViewRemoval = NO;
   }
 
   [self addToInputParentViewIfNeeded:_activeView];
@@ -2873,6 +2893,11 @@ static BOOL IsSelectionRectBoundaryCloserToPoint(CGPoint point,
 
 - (void)setTextInputClient:(int)client withConfiguration:(NSDictionary*)configuration {
   [self resetAllClientIds];
+
+  // Reset pending removal flags set by the previous clearTextInputClient call.
+  _pendingAutofillRemoval = NO;
+  _pendingInputViewRemoval = NO;
+
   // Hide all input views from autofill, only make those in the new configuration visible
   // to autofill.
   [self changeInputViewsAutofillVisibility:NO];
@@ -3112,12 +3137,29 @@ static BOOL IsSelectionRectBoundaryCloserToPoint(CGPoint point,
   [self removeEnableFlutterTextInputViewAccessibilityTimer];
   _activeView.accessibilityEnabled = NO;
 
+  // Schedule the removal of the input view and input hider.
+  // The removal is deferred to avoid keyboard flicker when switching between
+  // text fields, where clearTextInputClient is called before the next
+  // setTextInputClient.
+  // See: https://github.com/flutter/flutter/issues/180842
   if (_autofillContext.count == 0) {
-    [_activeView removeFromSuperview];
-    [_inputHider removeFromSuperview];
+    _pendingAutofillRemoval = NO;
+    if (_activeView.isFirstResponder) {
+      // Still first responder: defer removal to hideTextInput,
+      // right after resignFirstResponder dismisses the keyboard.
+      _pendingInputViewRemoval = YES;
+    } else {
+      // Already resigned (hideTextInput was called first):
+      // safe to remove immediately since the keyboard is already dismissed.
+      [_activeView removeFromSuperview];
+      [_inputHider removeFromSuperview];
+      _pendingInputViewRemoval = NO;
+    }
   } else {
-    // If _autofillContext is not empty, triggerAutofillSave will be called to clean up the views.
-    _pendingInputHiderRemoval = YES;
+    // Autofill context exists: removal will be performed in triggerAutofillSave,
+    // which is called by finishAutofillContext to save autofill entries.
+    _pendingAutofillRemoval = YES;
+    _pendingInputViewRemoval = NO;
   }
 }
 

engine/src/flutter/shell/platform/darwin/ios/framework/Source/FlutterTextInputPluginTest.mm
@@ -69,7 +69,8 @@ FLUTTER_ASSERT_ARC
 @property(nonatomic, readonly) UIView* keyboardView;
 @property(nonatomic, assign) UIView* cachedFirstResponder;
 @property(nonatomic, readonly) CGRect keyboardRect;
-@property(nonatomic, readonly) BOOL pendingInputHiderRemoval;
+@property(nonatomic, readonly) BOOL pendingAutofillRemoval;
+@property(nonatomic, readonly) BOOL pendingInputViewRemoval;
 @property(nonatomic, readonly)
     NSMutableDictionary<NSString*, FlutterTextInputView*>* autofillContext;
 
@@ -2723,17 +2724,115 @@ class MockPlatformViewDelegate : public PlatformView::Delegate {
   // Verify initial state.
   [self setClientId:123 configuration:config];
   XCTAssertEqual(textInputPlugin.autofillContext.count, 2ul);
-  XCTAssertFalse(textInputPlugin.pendingInputHiderRemoval);
+  XCTAssertFalse(textInputPlugin.pendingAutofillRemoval);
 
   // Retain autofill context.
   [self setClientClear];
   XCTAssertEqual(textInputPlugin.autofillContext.count, 2ul);
-  XCTAssertTrue(textInputPlugin.pendingInputHiderRemoval);
+  XCTAssertTrue(textInputPlugin.pendingAutofillRemoval);
 
   // Consume autofill context.
   [self commitAutofillContextAndVerify];
   XCTAssertEqual(textInputPlugin.autofillContext.count, 0ul);
-  XCTAssertFalse(textInputPlugin.pendingInputHiderRemoval);
+  XCTAssertFalse(textInputPlugin.pendingAutofillRemoval);
+}
+
+- (void)testSetClientResetsPendingAutofillRemoval {
+  // When autofill context exists and clearClient sets pendingAutofillRemoval,
+  // a subsequent setClient should reset the flag because a new client is
+  // connecting and the deferred removal is no longer needed.
+  NSMutableDictionary* field = self.mutablePasswordTemplateCopy;
+  [field setValue:@{
+    @"uniqueIdentifier" : @"field1",
+    @"hints" : @[ @"password" ],
+    @"editingValue" : @{@"text" : @""}
+  }
+           forKey:@"autofill"];
+  [field setValue:@[ field ] forKey:@"fields"];
+
+  // Set up autofill context.
+  [self setClientId:123 configuration:field];
+  XCTAssertGreaterThan(textInputPlugin.autofillContext.count, 0ul);
+
+  // clearClient with autofill context sets pendingAutofillRemoval.
+  [self setClientClear];
+  XCTAssertTrue(textInputPlugin.pendingAutofillRemoval);
+
+  // A new setClient resets the pending autofill removal flag.
+  [self setClientId:456 configuration:self.mutableTemplateCopy];
+  XCTAssertFalse(textInputPlugin.pendingAutofillRemoval);
+  XCTAssertFalse(textInputPlugin.pendingInputViewRemoval);
+}
+
+- (void)testPendingInputViewRemovalAfterClearClient {
+  // When autofillContext is empty and the view is first responder,
+  // clearClient should set pendingInputViewRemoval,
+  // and hideTextInput should consume it.
+  NSDictionary* config = self.mutableTemplateCopy;
+
+  // Verify initial state.
+  [self setClientId:123 configuration:config];
+  XCTAssertEqual(textInputPlugin.autofillContext.count, 0ul);
+  XCTAssertFalse(textInputPlugin.pendingInputViewRemoval);
+
+  // Stub isFirstResponder to simulate the view being first responder.
+  // In the test environment, becomeFirstResponder does not work.
+  id mockActiveView = OCMPartialMock(textInputPlugin.activeView);
+  OCMStub([mockActiveView isFirstResponder]).andReturn(YES);
+
+  // clearClient with no autofill context sets pendingInputViewRemoval.
+  [self setClientClear];
+  XCTAssertTrue(textInputPlugin.pendingInputViewRemoval);
+  XCTAssertFalse(textInputPlugin.pendingAutofillRemoval);
+
+  // hideTextInput consumes the flag and removes the view.
+  [self setTextInputHide];
+  XCTAssertFalse(textInputPlugin.pendingInputViewRemoval);
+  XCTAssertNil(textInputPlugin.activeView.superview);
+}
+
+- (void)testHideBeforeClearClientRemovesViewImmediately {
+  // When hideTextInput is called before clearTextInputClient,
+  // the view is no longer first responder. clearClient should
+  // remove the view immediately since no keyboard dismiss animation is needed.
+  NSDictionary* config = self.mutableTemplateCopy;
+
+  [self setClientId:123 configuration:config];
+  [self setTextInputShow];
+  XCTAssertNotNil(textInputPlugin.activeView.superview);
+
+  // Hide first: resignFirstResponder, but no removeFromSuperview.
+  [self setTextInputHide];
+  XCTAssertNotNil(textInputPlugin.activeView.superview);
+
+  // clearClient after hide: view is not first responder,
+  // so removeFromSuperview should happen immediately.
+  [self setClientClear];
+  XCTAssertFalse(textInputPlugin.pendingInputViewRemoval);
+  XCTAssertNil(textInputPlugin.activeView.superview);
+}
+
+- (void)testSetClientResetsPendingInputViewRemoval {
+  // When clearClient sets pendingInputViewRemoval (no autofill, first responder),
+  // a subsequent setClient should reset the flag because a new client is
+  // connecting and the deferred removal is no longer needed.
+  NSDictionary* config = self.mutableTemplateCopy;
+
+  // Field 1: setClient → show → clearClient (sets pendingInputViewRemoval).
+  [self setClientId:123 configuration:config];
+  [self setTextInputShow];
+
+  // Stub isFirstResponder to simulate the view being first responder.
+  // In the test environment, becomeFirstResponder does not work.
+  id mockActiveView = OCMPartialMock(textInputPlugin.activeView);
+  OCMStub([mockActiveView isFirstResponder]).andReturn(YES);
+
+  [self setClientClear];
+  XCTAssertTrue(textInputPlugin.pendingInputViewRemoval);
+
+  // Field 2: setClient resets the stale flag.
+  [self setClientId:456 configuration:config];
+  XCTAssertFalse(textInputPlugin.pendingInputViewRemoval);
 }
 
 - (void)testPasswordAutofillHack {