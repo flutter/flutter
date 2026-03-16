b7bb3aea55dd01f0e35843218618878bbfd50f2e
Xiaowei Guan <xiaowei.guan@samsung.com>
2026-03-17 00:18:49 +0800

    Add pipeline_compile_queue_vulkan.h

---
+++ engine/src/flutter/impeller/renderer/backend/vulkan/pipeline_compile_queue_vulkan.h
@@ -0,0 +1,42 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef FLUTTER_IMPELLER_RENDERER_BACKEND_VULKAN_PIPELINE_COMPILE_QUEUE_VULKAN_H_
+#define FLUTTER_IMPELLER_RENDERER_BACKEND_VULKAN_PIPELINE_COMPILE_QUEUE_VULKAN_H_
+
+#include "flutter/fml/closure.h"
+#include "flutter/fml/task_runner.h"
+#include "impeller/renderer/pipeline_compile_queue.h"
+
+namespace impeller {
+
+class PipelineCompileQueueVulkan : public PipelineCompileQueue {
+ public:
+  static std::shared_ptr<PipelineCompileQueueVulkan> Create(
+      std::shared_ptr<fml::BasicTaskRunner> worker_task_runner);
+
+  explicit PipelineCompileQueueVulkan(
+      std::shared_ptr<fml::BasicTaskRunner> worker_task_runner);
+
+  ~PipelineCompileQueueVulkan() override;
+
+  PipelineCompileQueueVulkan(const PipelineCompileQueueVulkan&) = delete;
+
+  PipelineCompileQueueVulkan& operator=(const PipelineCompileQueueVulkan&) =
+      delete;
+
+  //----------------------------------------------------------------------------
+  /// @brief      Post a job to the worker task runner.
+  ///
+  /// @param[in]  job   The job
+  ///
+  void PostJob(const fml::closure& job) override;
+
+ private:
+  std::shared_ptr<fml::BasicTaskRunner> worker_task_runner_;
+};
+
+}  // namespace impeller
+
+#endif  // FLUTTER_IMPELLER_RENDERER_BACKEND_VULKAN_PIPELINE_COMPILE_QUEUE_VULKAN_H_
--- engine/src/flutter/impeller/renderer/pipeline_compile_queue.cc
+++ engine/src/flutter/impeller/renderer/pipeline_compile_queue.cc
@@ -9,16 +9,6 @@
 
 namespace impeller {
 
-std::shared_ptr<PipelineCompileQueue> PipelineCompileQueue::Create(
-    std::shared_ptr<fml::BasicTaskRunner> worker_task_runner) {
-  return std::shared_ptr<PipelineCompileQueue>(
-      new PipelineCompileQueue(std::move(worker_task_runner)));
-}
-
-PipelineCompileQueue::PipelineCompileQueue(
-    std::shared_ptr<fml::BasicTaskRunner> worker_task_runner)
-    : worker_task_runner_(std::move(worker_task_runner)) {}
-
 PipelineCompileQueue::~PipelineCompileQueue() {
   FinishAllJobs();
 }
@@ -113,14 +103,6 @@ void PipelineCompileQueue::PerformJobEagerly(const PipelineDescriptor& desc) {
   if (auto job = TakeJob(desc)) {
     job();
   }
-}
-
-void PipelineCompileQueue::PostJob(const fml::closure& job) {
-  if (!job) {
-    return;
-  }
-
-  worker_task_runner_->PostTask(job);
 }
 
 }  // namespace impeller
