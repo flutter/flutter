c2f08b5309faff30763837210113036e9a92c2a7
gaaclarke <30870216+gaaclarke@users.noreply.github.com>
2026-03-17 18:55:36 -0700

    added to content context

--- engine/src/flutter/impeller/entity/contents/pipelines.h
+++ engine/src/flutter/impeller/entity/contents/pipelines.h
@@ -65,6 +65,7 @@
 #include "impeller/entity/tiled_texture_fill.frag.h"
 #include "impeller/entity/vertices_uber_1.frag.h"
 #include "impeller/entity/vertices_uber_2.frag.h"
+#include "impeller/entity/uber_sdf.frag.h"
 #include "impeller/entity/yuv_to_rgb_filter.frag.h"
 #include "impeller/renderer/pipeline.h"
 
@@ -160,6 +161,7 @@ using TextureStrictSrcPipeline = RenderPipelineHandle<TextureFillVertexShader, T
 using TiledTexturePipeline = RenderPipelineHandle<TextureUvFillVertexShader, TiledTextureFillFragmentShader>;
 using VerticesUber1Shader = RenderPipelineHandle<PorterDuffBlendVertexShader, VerticesUber1FragmentShader>;
 using VerticesUber2Shader = RenderPipelineHandle<PorterDuffBlendVertexShader, VerticesUber2FragmentShader>;
+using UberSDFPipeline = RenderPipelineHandle<CircleVertexShader, UberSdfFragmentShader>;
 using YUVToRGBFilterPipeline = RenderPipelineHandle<FilterPositionVertexShader, YuvToRgbFilterFragmentShader>;
 // clang-format on
 
