39ef3700c74a5fe6637a4c086f94c53e470aba73
Matej Knopp <matej.knopp@gmail.com>
2026-03-18 21:01:40 +0100

    Fixes

--- engine/src/flutter/shell/platform/windows/host_window.cc
+++ engine/src/flutter/shell/platform/windows/host_window.cc
@@ -234,7 +234,7 @@ std::unique_ptr<HostWindow> HostWindow::CreateTooltipWindow(
     HWND parent) {
   return std::unique_ptr<HostWindowTooltip>(new HostWindowTooltip(
       window_manager, engine, FromWindowConstraints(preferred_constraints),
-      is_sized_to_content, get_position_callback, parent));
+      get_position_callback, parent));
 }
 
 HostWindow::HostWindow(WindowManager* window_manager,
