a872960cadee9cccfce28e844f5980bf9f9d12e5
Camille Simon <camillesimon@google.com>
2026-03-19 10:58:37 -0700

    fix merge

--- dev/devicelab/lib/tasks/perf_tests.dart
+++ dev/devicelab/lib/tasks/perf_tests.dart
@@ -828,49 +828,9 @@ Future<void> _resetPlist(String testDirectory) async {
   await exec('git', <String>['checkout', file.path]);
 }
 
-void _addMetadataToManifest(String testDirectory, List<(String, String)> keyPairs) {
-  final String manifestPath = path.join(
-    testDirectory,
-    'android',
-    'app',
-    'src',
-    'main',
-    'AndroidManifest.xml',
-  );
-  final file = File(manifestPath);
-
-  if (!file.existsSync()) {
-    throw Exception('AndroidManifest.xml not found at $manifestPath');
-  }
-
-  final String xmlStr = file.readAsStringSync();
-  final xmlDoc = XmlDocument.parse(xmlStr);
-  final XmlElement applicationNode = xmlDoc.findAllElements('application').first;
-
-  // Check if the meta-data node already exists.
-  for (final (String key, String value) in keyPairs) {
-    final Iterable<XmlElement> existingMetaData = applicationNode
-        .findAllElements('meta-data')
-        .where((XmlElement node) => node.getAttribute('android:name') == key);
-
-    if (existingMetaData.isNotEmpty) {
-      final XmlElement existingEntry = existingMetaData.first;
-      existingEntry.setAttribute('android:value', value);
-    } else {
-      final metaData = XmlElement(XmlName('meta-data'), <XmlAttribute>[
-        XmlAttribute(XmlName('android:name'), key),
-        XmlAttribute(XmlName('android:value'), value),
-      ]);
-      applicationNode.children.add(metaData);
-    }
-  }
-
-  file.writeAsStringSync(xmlDoc.toXmlString(pretty: true, indent: '    '));
-}
-
 void _addHcppSupportToManifest(String testDirectory) {
   final keyPairs = <(String, String)>[('io.flutter.embedding.android.EnableHcpp', 'true')];
-  _addMetadataToManifest(testDirectory, keyPairs);
+  addMetadataToManifest(testDirectory, keyPairs);
 }
 
 void _addMergedPlatformThreadSupportToManifest(String testDirectory) {
