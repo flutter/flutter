ab9ffe050df7a66abdf1a0cbb199eb0167914299
mayanksharma9 <mayanksharma.ms3@gmail.com>
2026-03-23 09:04:37 -0700

    Replace deprecated Color.alpha in Title assert
    Fixes flutter/flutter#184002

--- packages/flutter/lib/src/widgets/title.dart
+++ packages/flutter/lib/src/widgets/title.dart
@@ -13,17 +13,16 @@ class Title extends StatefulWidget {
   /// Creates a widget that describes this app to the Android operating system.
   ///
   /// [title] will default to the empty string if not supplied.
-  /// [color] must be an opaque color (i.e. color.alpha must be 255 (0xFF)).
+  /// [color] must be an opaque color (i.e. color.a must be 1.0).
   /// [color] and [child] are required arguments.
   Title({super.key, this.title = '', required this.color, required this.child})
-    : assert(color.alpha == 0xFF);
+    : assert(color.a == 1.0);
 
   /// A one-line description of this app for use in the window manager.
   final String title;
 
   /// A color that the window manager should use to identify this app. Must be
-  /// an opaque color (i.e. color.alpha must be 255 (0xFF)), and must not be
-  /// null.
+  /// an opaque color (i.e. color.a must be 1.0), and must not be null.
   final Color color;
 
   /// The widget below this widget in the tree.
