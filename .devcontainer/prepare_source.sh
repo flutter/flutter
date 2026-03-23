6040927a61734ec8bed8129d24d58b34ba1f7b68
richard.cruceanu <richard.cruceanu@exfo.com>
2026-03-23 14:15:21 -0400

    improved build files

--- .devcontainer/prepare_source.sh
+++ .devcontainer/prepare_source.sh
@@ -6,5 +6,10 @@ sudo chown -R dev:dev /builds
 cd /builds
 
 git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
+echo 'export PATH="$PATH:/builds/depot_tools"' >> /home/dev/.bashrc
 export PATH="$PATH:/builds/depot_tools"
 
+cd flutter
+cp engine/scripts/standard.gclient .gclient
+gclient sync -D
+./engine/src/flutter/build/install-build-deps-linux-desktop.sh
