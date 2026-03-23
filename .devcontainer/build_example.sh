2fa098ecc6efd240a968abfb94acc29d828ca916
richard.cruceanu <richard.cruceanu@exfo.com>
2026-03-23 15:30:11 -0400

    added config

--- .devcontainer/build_example.sh
+++ .devcontainer/build_example.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
 
 cd /builds/flutter/examples/multiple_windows/
+/builds/flutter/bin/flutter config --enable-windowing
 /builds/flutter/bin/flutter build linux --release
-cp /builds/flutter/engine/src/out/host_release/libflutter_linux_gtk.so /builds/flutter/examples/multiple_windows/build/linux/x64/release/bundle/lib/libflutter_linux_gtk.so
-/builds/flutter/examples/multiple_windows/build/linux/x64/release/bundle
+cp /builds/flutter/engine/src/out/host_release/libflutter_linux_gtk.so build/linux/x64/release/bundle/lib/libflutter_linux_gtk.so
+build/linux/x64/release/bundle/multiple_windows
