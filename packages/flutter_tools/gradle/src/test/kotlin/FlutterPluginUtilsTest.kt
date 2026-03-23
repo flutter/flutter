3e42d11ad2e1c090d31da7f18ac3525894fcdece
Camille Simon <camillesimon@google.com>
2026-03-23 14:40:05 -0700

    ktlint format?

--- packages/flutter_tools/gradle/src/test/kotlin/FlutterPluginUtilsTest.kt
+++ packages/flutter_tools/gradle/src/test/kotlin/FlutterPluginUtilsTest.kt
@@ -4,11 +4,10 @@
 
 package com.flutter.gradle
 
+import com.android.build.api.dsl.ApplicationExtension
 import com.android.build.api.variant.AndroidComponentsExtension
 import com.android.build.api.variant.Sources
 import com.android.build.api.variant.Variant
-import com.android.build.gradle.AbstractAppExtension
-import com.android.build.api.dsl.ApplicationExtension
 import com.android.build.gradle.BaseExtension
 import com.android.build.gradle.internal.dsl.CmakeOptions
 import com.android.build.gradle.internal.dsl.DefaultConfig
