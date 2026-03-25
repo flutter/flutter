443b38c4948f53e307b83cfc1cb103b618bc52d3
Srujan Gaddam <srujzs@google.com>
2026-03-25 09:42:34 -0700

    Ignore incoming deprecated_web_configuration lint
    https://github.com/dart-lang/sdk/issues/61875
    
    This lint is intended to instruct users to use
    dart.library.js_interop when differentiating between
    VM and web. However, there are some cases where the
    user intends to differentiate between Wasm and JS, and
    so the lint is allowed to be ignored until we have a
    better environment variable users can use.
    
    This is needed to land the lint without Flutter failures.

--- engine/src/flutter/lib/web_ui/lib/src/engine/renderer.dart
+++ engine/src/flutter/lib/web_ui/lib/src/engine/renderer.dart
@@ -10,6 +10,7 @@ import 'dart:typed_data';
 import 'package:meta/meta.dart';
 import 'package:ui/src/engine.dart';
 import 'package:ui/src/engine/skwasm/skwasm_impl.dart'
+    // ignore: deprecated_web_configuration
     if (dart.library.html) 'package:ui/src/engine/skwasm/skwasm_stub.dart';
 import 'package:ui/ui.dart' as ui;
 import 'package:ui/ui_web/src/ui_web.dart' as ui_web;
