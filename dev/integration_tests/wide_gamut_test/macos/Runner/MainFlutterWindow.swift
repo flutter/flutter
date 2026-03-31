e5a43a7d7adca1f2bf3be053f256bc4dfb5e1cbc
Evan Walley <evanwall@buffalo.edu>
2026-03-31 13:25:42 -0700

    Fix wide gamut macos integration test

---
+++ dev/integration_tests/wide_gamut_test/macos/Runner/MainFlutterWindow.swift
@@ -0,0 +1,43 @@
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import Cocoa
+import FlutterMacOS
+import Foundation
+
+@objc protocol PrivateFlutterView {
+    func setEnableWideGamut(_ enabled: Bool)
+}
+
+class WideGamutViewController: FlutterViewController {
+    
+    // Override the method in the view controller to force wide-gamut support.
+    @objc(updateWideGamutForScreen)
+    func updateWideGamutForScreen() {
+        if let flutterView = self.view as AnyObject as? PrivateFlutterView {
+            flutterView.setEnableWideGamut(true) // Always force true
+        }
+    }
+    
+    override func viewWillAppear() {
+        super.viewWillAppear()
+        // Force the surface to 10-bit during the initial view loading sequence.
+        self.updateWideGamutForScreen()
+    }
+}
+
+class MainFlutterWindow: NSWindow {
+  override func awakeFromNib() {
+    let flutterViewController = WideGamutViewController()
+    
+    let windowFrame = self.frame
+    self.contentViewController = flutterViewController
+    self.setFrame(windowFrame, display: true)
+
+    RegisterGeneratedPlugins(registry: flutterViewController)
+
+    super.awakeFromNib()
+  }
+}
+
