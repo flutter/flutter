2d9cf219a95aaad4900a3e29101575866833029b
Evan Walley <evanwall@buffalo.edu>
2026-03-31 13:49:52 -0700

    gem comment

--- dev/integration_tests/wide_gamut_test/macos/Runner/MainFlutterWindow.swift
+++ dev/integration_tests/wide_gamut_test/macos/Runner/MainFlutterWindow.swift
@@ -2,35 +2,31 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import Cocoa
-import FlutterMacOS
-import Foundation
-
 @objc protocol PrivateFlutterView {
-    func setEnableWideGamut(_ enabled: Bool)
+  func setEnableWideGamut(_ enabled: Bool)
 }
 
 class WideGamutViewController: FlutterViewController {
-    
-    // Override the method in the view controller to force wide-gamut support.
-    @objc(updateWideGamutForScreen)
-    func updateWideGamutForScreen() {
-        if let flutterView = self.view as AnyObject as? PrivateFlutterView {
-            flutterView.setEnableWideGamut(true) // Always force true
-        }
+
+  // Override the method in the view controller to force wide-gamut support.
+  @objc(updateWideGamutForScreen)
+  func updateWideGamutForScreen() {
+    if let flutterView = self.view as? PrivateFlutterView {
+      flutterView.setEnableWideGamut(true) // Always force true
     }
-    
-    override func viewWillAppear() {
-        super.viewWillAppear()
-        // Force the surface to 10-bit during the initial view loading sequence.
-        self.updateWideGamutForScreen()
-    }
+  }
+
+  override func viewWillAppear() {
+    super.viewWillAppear()
+    // Force the surface to 10-bit during the initial view loading sequence.
+    self.updateWideGamutForScreen()
+  }
 }
 
 class MainFlutterWindow: NSWindow {
   override func awakeFromNib() {
     let flutterViewController = WideGamutViewController()
-    
+
     let windowFrame = self.frame
     self.contentViewController = flutterViewController
     self.setFrame(windowFrame, display: true)
@@ -40,4 +36,3 @@ class MainFlutterWindow: NSWindow {
     super.awakeFromNib()
   }
 }
-
