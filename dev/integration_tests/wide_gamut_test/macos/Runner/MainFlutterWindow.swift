ab40d9bfa99f83155f7e1190e177425b1a8093c5
Evan Walley <evanwall@buffalo.edu>
2026-03-31 14:25:50 -0700

    re-add imports

--- dev/integration_tests/wide_gamut_test/macos/Runner/MainFlutterWindow.swift
+++ dev/integration_tests/wide_gamut_test/macos/Runner/MainFlutterWindow.swift
@@ -2,6 +2,10 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import Cocoa
+import FlutterMacOS
+import Foundation
+
 @objc protocol PrivateFlutterView {
   func setEnableWideGamut(_ enabled: Bool)
 }
