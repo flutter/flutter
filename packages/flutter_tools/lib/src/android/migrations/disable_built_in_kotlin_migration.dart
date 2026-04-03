888cdebdd89266c7a4f51a4863217fa148592efb
jesswrd <jesswon@google.com>
2026-04-03 13:01:06 -0700

    Added todo

--- packages/flutter_tools/lib/src/android/migrations/disable_built_in_kotlin_migration.dart
+++ packages/flutter_tools/lib/src/android/migrations/disable_built_in_kotlin_migration.dart
@@ -55,12 +55,11 @@ class DisableBuiltInKotlinMigration extends ProjectMigrator {
       return;
     }
 
+    // TODO(jesswon): Remove once try/catch is added to the write processFile: https://github.com/flutter/flutter/issues/184595
     try {
       processFileLines(_gradlePropertiesFile);
     } on FileSystemException catch (e) {
-      logger.printError(
-        'Failed to process/migrate the gradle.properties during migration: $e',
-      );
+      logger.printError('Failed to process/migrate the gradle.properties during migration: $e');
     }
   }
 
