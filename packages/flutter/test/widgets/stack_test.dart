2cfb15636019a2076d89e5497364a02137011192
Victor Sanni <victorsanniay@gmail.com>
2026-04-09 15:21:15 -0700

    linux analyze

--- packages/flutter/test/widgets/stack_test.dart
+++ packages/flutter/test/widgets/stack_test.dart
@@ -7,7 +7,6 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:leak_tracker_flutter_testing/leak_tracker_flutter_testing.dart';
 
-
 class TestPaintingContext implements PaintingContext {
   final List<Invocation> invocations = <Invocation>[];
 
@@ -757,5 +756,4 @@ void main() {
       );
     },
   );
-
 }
