c4d15c28b1ca7d4d0f7aad2e51f3c7fb4f478e3a
Jim Graham <flar@google.com>
2026-04-14 02:58:39 -0700

    gemini feedback and more shape types

--- engine/src/flutter/impeller/display_list/aiks_dl_sdf_primitive_unittests.cc
+++ engine/src/flutter/impeller/display_list/aiks_dl_sdf_primitive_unittests.cc
@@ -17,11 +17,96 @@
 
 namespace {
 
-typedef std::function<void(flutter::DisplayListBuilder& builder,
-                           flutter::DlPoint center,
-                           flutter::DlScalar radius,
-                           flutter::DlPaint& paint)>
-    RenderFunction;
+using RenderFunction = std::function<void(flutter::DisplayListBuilder& builder,
+                                          flutter::DlPoint center,
+                                          flutter::DlScalar radius,
+                                          flutter::DlPaint& paint)>;
+
+flutter::DlRect MakeRect(flutter::DlPoint center,
+                         flutter::DlScalar width,
+                         flutter::DlScalar height) {
+  return flutter::DlRect::MakeLTRB(center.x - width, center.y - height,  //
+                                   center.x + width, center.y + height);
+}
+
+RenderFunction RenderSquare = [](flutter::DisplayListBuilder& builder,
+                                 flutter::DlPoint center,
+                                 flutter::DlScalar radius,
+                                 flutter::DlPaint& paint) {
+  builder.DrawRect(MakeRect(center, radius, radius), paint);
+};
+
+RenderFunction RenderRectangle = [](flutter::DisplayListBuilder& builder,
+                                    flutter::DlPoint center,
+                                    flutter::DlScalar radius,
+                                    flutter::DlPaint& paint) {
+  builder.DrawRect(MakeRect(center, radius, radius * 0.9f), paint);
+};
+
+RenderFunction RenderCircle = [](flutter::DisplayListBuilder& builder,
+                                 flutter::DlPoint center,
+                                 flutter::DlScalar radius,
+                                 flutter::DlPaint& paint) {
+  builder.DrawCircle(center, radius, paint);
+};
+
+RenderFunction RenderOval = [](flutter::DisplayListBuilder& builder,
+                               flutter::DlPoint center,
+                               flutter::DlScalar radius,
+                               flutter::DlPaint& paint) {
+  builder.DrawOval(MakeRect(center, radius, radius * 0.9f), paint);
+};
+
+RenderFunction RenderLine = [](flutter::DisplayListBuilder& builder,
+                               flutter::DlPoint center,
+                               flutter::DlScalar radius,
+                               flutter::DlPaint& paint) {
+  flutter::DlVector2 offset(radius, 0);
+  builder.DrawLine(center - offset, center + offset, paint);
+};
+
+enum class RenderType {
+  kSquare,
+  kRectangle,
+  kCircle,
+  kOval,
+  kLine,
+  kCount,
+};
+
+const char* ToName(RenderType type) {
+  switch (type) {
+    case RenderType::kSquare:
+      return "Square";
+    case RenderType::kRectangle:
+      return "Rectangle";
+    case RenderType::kCircle:
+      return "Circle";
+    case RenderType::kOval:
+      return "Oval";
+    case RenderType::kLine:
+      return "Line";
+    case RenderType::kCount:
+      FML_UNREACHABLE();
+  }
+}
+
+const RenderFunction& ToFunction(RenderType type) {
+  switch (type) {
+    case RenderType::kSquare:
+      return RenderSquare;
+    case RenderType::kRectangle:
+      return RenderRectangle;
+    case RenderType::kCircle:
+      return RenderCircle;
+    case RenderType::kOval:
+      return RenderOval;
+    case RenderType::kLine:
+      return RenderLine;
+    case RenderType::kCount:
+      FML_UNREACHABLE();
+  }
+}
 
 struct RenderParams {
   flutter::DlPoint center;
@@ -30,6 +115,7 @@ struct RenderParams {
   flutter::DlScalar skew_x;
   flutter::DlScalar skew_y;
   flutter::DlScalar rotation;
+  RenderType render_type;
 
   flutter::DlRadians Radians() const {
     return flutter::DlRadians(rotation * impeller::kPi * 2.0f);
@@ -37,39 +123,26 @@ struct RenderParams {
 };
 
 void RenderPrimitive(flutter::DisplayListBuilder& builder,
-                     const RenderParams& params,
-                     RenderFunction& render) {
+                     const RenderParams& params) {
   using Matrix = impeller::Matrix;
 
-  Matrix transform;
-  transform = transform * Matrix::MakeTranslation(params.center);
-  transform = transform * Matrix::MakeRotationZ(params.Radians());
-  transform =
-      transform * Matrix::MakeScale({params.scale_x, params.scale_y, 1.0f});
-  transform = transform * Matrix::MakeSkew(params.skew_x, params.skew_y);
+  Matrix transform =  //
+      Matrix::MakeTranslation(params.center) *
+      Matrix::MakeRotationZ(params.Radians()) *
+      Matrix::MakeScale({params.scale_x, params.scale_y, 1.0f}) *
+      Matrix::MakeSkew(params.skew_x, params.skew_y);
   builder.Transform(transform);
 
   flutter::DlPaint paint;
   paint.setColor(flutter::DlColor::kBlue());
   paint.setStrokeWidth(20.0f);
-  render(builder, flutter::DlPoint(), 100, paint);
+  const RenderFunction& renderer = ToFunction(params.render_type);
+  renderer(builder, flutter::DlPoint(), 100, paint);
   paint.setColor(flutter::DlColor::kWhite());
   paint.setDrawStyle(flutter::DlDrawStyle::kStroke);
   paint.setStrokeWidth(0.0f);
-  render(builder, flutter::DlPoint(), 90, paint);
+  renderer(builder, flutter::DlPoint(), 90, paint);
 }
-
-RenderFunction RenderRect = [](flutter::DisplayListBuilder& builder,
-                               flutter::DlPoint center,
-                               flutter::DlScalar radius,
-                               flutter::DlPaint& paint) {
-  flutter::DlScalar width = radius;
-  flutter::DlScalar height = radius * 0.9f;
-  flutter::DlRect rect =
-      flutter::DlRect::MakeLTRB(center.x - width, center.y - height,  //
-                                center.x + width, center.y + height);
-  builder.DrawRect(rect, paint);
-};
 
 }  // namespace
 
@@ -89,6 +162,7 @@ TEST_P(AiksTest, SdfPrimitivePlayground) {
       .skew_x = 0.0f,
       .skew_y = 0.0f,
       .rotation = 0.0f,
+      .render_type = RenderType::kRectangle,
   };
 
   auto callback = [&]() -> sk_sp<flutter::DisplayList> {
@@ -99,12 +173,18 @@ TEST_P(AiksTest, SdfPrimitivePlayground) {
       ImGui::SliderFloat("X Skew", &params.skew_x, 0, 1);
       ImGui::SliderFloat("Y Skew", &params.skew_y, 0, 1);
       ImGui::SliderFloat("Rotation", &params.rotation, 0, 1);
+      ImGui::ListBox(
+          "Shape Type", reinterpret_cast<int*>(&params.render_type),
+          [](void* data, int index) {
+            return ToName(static_cast<RenderType>(index));
+          },
+          nullptr, static_cast<int>(RenderType::kCount), -1);
       ImGui::End();
     }
 
     flutter::DisplayListBuilder builder;
     builder.Scale(GetContentScale().x, GetContentScale().y);
-    RenderPrimitive(builder, params, RenderRect);
+    RenderPrimitive(builder, params);
     return builder.Build();
   };
 
@@ -120,11 +200,12 @@ TEST_P(AiksTest, CanRenderSkewedSdfRectangle) {
       .skew_x = 0.5f,
       .skew_y = 0.5f,
       .rotation = 0.1f,
+      .render_type = RenderType::kRectangle,
   };
 
   flutter::DisplayListBuilder builder;
   builder.Scale(GetContentScale().x, GetContentScale().y);
-  RenderPrimitive(builder, params, RenderRect);
+  RenderPrimitive(builder, params);
 
   ASSERT_TRUE(OpenPlaygroundHere(builder.Build()));
 }
