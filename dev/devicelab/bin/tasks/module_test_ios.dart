67f4c283b676a407e2db0d28e4178711550b3227
Loïc Sharma <737941+loic-sharma@users.noreply.github.com>
2026-04-17 16:04:56 -0700

    Fix module_test_ios

--- dev/devicelab/bin/tasks/module_test_ios.dart
+++ dev/devicelab/bin/tasks/module_test_ios.dart
@@ -604,7 +604,7 @@ dependencies:
             RegExp('flutter.*--local-engine-src-path=bogus assemble'),
           ) || // Verbose output
           !xcodebuildOutput.contains(
-            'Unable to detect a Flutter engine build directory in bogus',
+            'No Flutter engine build directory found at: bogus',
           )) {
         return TaskResult.failure(
           'Host Objective-C app build succeeded though flutter script failed',
