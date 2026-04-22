86b6d4da577e049fcbfb749829e3e9b372c0c804
Jason Simmons <jsimmons@google.com>
2026-04-22 23:14:13 +0000

    test comments

--- engine/src/flutter/fml/safe_math_unittests.cc
+++ engine/src/flutter/fml/safe_math_unittests.cc
@@ -11,19 +11,23 @@ namespace flutter {
 namespace testing {
 
 TEST(SafeMathTest, MultiplySizeT) {
+  // Multiplication with no overflow.
   fml::SafeMath safe1;
-  safe1.mul(std::numeric_limits<size_t>::max(),
-            std::numeric_limits<size_t>::max());
-  EXPECT_TRUE(safe1.overflow_detected());
+  EXPECT_EQ(safe1.mul(1000, 2000), static_cast<size_t>(2000000));
+  EXPECT_FALSE(safe1.overflow_detected());
 
+  // Overflow detection when multiplying size_t values at or near the maximum.
   fml::SafeMath safe2;
-  EXPECT_EQ(safe2.mul(1000, 2000), static_cast<size_t>(2000000));
-  EXPECT_FALSE(safe2.overflow_detected());
+  safe2.mul(std::numeric_limits<size_t>::max(),
+            std::numeric_limits<size_t>::max());
+  EXPECT_TRUE(safe2.overflow_detected());
 
   fml::SafeMath safe3;
   safe3.mul(std::numeric_limits<size_t>::max() >> 2, 5);
   EXPECT_TRUE(safe3.overflow_detected());
 
+  // Overflow detection for a result that slightly exceeds the range of a
+  // uint64_t.
   if (sizeof(size_t) == sizeof(uint64_t)) {
     fml::SafeMath safe4;
     safe4.mul(static_cast<size_t>(1ULL << 32), static_cast<size_t>(1ULL << 32));
