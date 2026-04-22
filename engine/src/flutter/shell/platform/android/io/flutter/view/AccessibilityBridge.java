ca87e698c84bc2b277bcb56634139241145a1230
Qun Cheng <quncheng@google.com>
2026-04-22 13:25:10 -0700

    Add unit test when a missing child is not sent to update

--- engine/src/flutter/shell/platform/android/io/flutter/view/AccessibilityBridge.java
+++ engine/src/flutter/shell/platform/android/io/flutter/view/AccessibilityBridge.java
@@ -3061,8 +3061,6 @@ public class AccessibilityBridge extends AccessibilityNodeProvider {
         inverseTransform = new float[16];
       }
       if (hitTestTransform == null) {
-        // hitTestTransform has not been initialized via updateWith() yet.
-        // Give a default so Matrix.invertM doesn't throw.
         hitTestTransform = new float[16];
         Matrix.setIdentityM(hitTestTransform, 0);
       }
