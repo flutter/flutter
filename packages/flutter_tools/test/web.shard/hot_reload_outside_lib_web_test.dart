d4ed575d972e6d74d1da7865b3b83007ceca7e52
Matej Resetar <matej.resetar@leancode.pl>
2026-04-23 12:26:55 +0200

    Add regression test for hot reload with entrypoint outside lib

--- packages/flutter_tools/test/web.shard/hot_reload_outside_lib_web_test.dart
+++ packages/flutter_tools/test/web.shard/hot_reload_outside_lib_web_test.dart
@@ -2,26 +2,27 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+@Tags(<String>['flutter-test-driver'])
+library;
+
 import 'dart:async';
 
 import 'package:file/file.dart';
 import 'package:flutter_tools/src/web/web_device.dart' show GoogleChromeDevice;
 
-import '../../integration.shard/test_data/hot_reload_outside_lib_project.dart';
-import '../../integration.shard/test_driver.dart';
-import '../../integration.shard/test_utils.dart';
-import '../../src/common.dart';
+import '../integration.shard/test_data/hot_reload_outside_lib_project.dart';
+import '../integration.shard/test_driver.dart';
+import '../integration.shard/test_utils.dart';
+import '../src/common.dart';
 
-/// Tests that hot restart applies code changes for entrypoints outside `lib/`.
-///
 /// Regression test for https://github.com/flutter/flutter/issues/175318.
-void testAll() {
+void main() {
   late Directory tempDir;
   final project = HotReloadOutsideLibProject();
   late FlutterRunTestDriver flutter;
 
   setUp(() async {
-    tempDir = createResolvedTempDirectorySync('hot_restart_outside_lib_test.');
+    tempDir = createResolvedTempDirectorySync('hot_reload_outside_lib_test.');
     await project.setUpIn(tempDir);
     flutter = FlutterRunTestDriver(tempDir);
   });
@@ -32,14 +33,18 @@ void testAll() {
     tryToDelete(tempDir);
   });
 
-  testWithoutContext('hot restart applies code changes for an entrypoint outside lib/', () async {
+  testWithoutContext('hot reload applies code changes for an entrypoint outside lib/', () async {
     await flutter.run(
       device: GoogleChromeDevice.kChromeDeviceId,
       script: 'integration_test/main.dart',
-      additionalCommandArgs: <String>['--verbose', '--no-web-resources-cdn'],
+      additionalCommandArgs: <String>[
+        '--verbose',
+        '--no-web-resources-cdn',
+        '--web-experimental-hot-reload',
+      ],
     );
 
-    // Uncomment the print statement and hot restart to verify the change is actually applied.
+    // Uncomment the print statement and hot reload to verify the change is actually applied.
     final completer = Completer<void>();
     final StreamSubscription<String> subscription = flutter.stdout.listen((String line) {
       printOnFailure(line);
@@ -49,7 +54,7 @@ void testAll() {
     });
     project.uncommentHotReloadPrint();
     try {
-      await flutter.hotRestart();
+      await flutter.hotReload();
       await completer.future.timeout(const Duration(seconds: 15));
     } finally {
       await subscription.cancel();
