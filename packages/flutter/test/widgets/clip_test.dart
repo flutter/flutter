d56a21cd99519f1c23ebbc878ec7fc9c003b707f
Ahmed Mohamed Sameh <ahmedsameha1@gmail.com>
2026-04-26 20:36:27 +0300

    Make sure that a ClipPath doesn't crash in 0x0 environment

--- packages/flutter/test/widgets/clip_test.dart
+++ packages/flutter/test/widgets/clip_test.dart
@@ -978,4 +978,18 @@ void main() {
     );
     expect(tester.getSize(find.byType(ClipOval)), Size.zero);
   });
+
+  testWidgets('ClipPath does not crash at zero area', (WidgetTester tester) async {
+    tester.view.physicalSize = Size.zero;
+    final clip = ValueNotifier<Rect>(const Rect.fromLTWH(50.0, 50.0, 100.0, 100.0));
+    addTearDown(tester.view.reset);
+    addTearDown(clip.dispose);
+    await tester.pumpWidget(
+      const Directionality(
+        textDirection: TextDirection.ltr,
+        child: Center(child: ClipPath(child: Placeholder())),
+      ),
+    );
+    expect(tester.getSize(find.byType(ClipPath)), Size.zero);
+  });
 }
