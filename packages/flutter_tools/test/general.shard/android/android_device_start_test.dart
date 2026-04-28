11d2e80549bb5d5d692c1ff97c5e7e35f219faee
Gray Mackall <34871572+gmackall@users.noreply.github.com>
2026-04-28 10:33:49 -0700

    fix hcpp flag not passed in release

--- packages/flutter_tools/test/general.shard/android/android_device_start_test.dart
+++ packages/flutter_tools/test/general.shard/android/android_device_start_test.dart
@@ -128,6 +128,90 @@ void main() {
     });
   }
 
+  testWithoutContext(
+    'AndroidDevice.startApp forwards Impeller and HCPP flags in release mode',
+    () async {
+      final device = AndroidDevice(
+        '1234',
+        modelID: 'TestModel',
+        fileSystem: fileSystem,
+        processManager: processManager,
+        logger: BufferLogger.test(),
+        platform: FakePlatform(),
+        androidSdk: androidSdk,
+      );
+      final File apkFile = fileSystem.file('app-debug.apk')..createSync();
+      final apk = AndroidApk(
+        id: 'FlutterApp',
+        applicationPackage: apkFile,
+        launchActivity: 'FlutterActivity',
+        versionCode: 1,
+      );
+
+      processManager.addCommand(kAdbVersionCommand);
+      processManager.addCommand(kStartServer);
+      processManager.addCommand(
+        const FakeCommand(
+          command: <String>['adb', '-s', '1234', 'shell', 'getprop'],
+          stdout: '[ro.product.cpu.abi]: [arm64-v8a]',
+        ),
+      );
+      processManager.addCommand(
+        const FakeCommand(
+          command: <String>['adb', '-s', '1234', 'shell', 'am', 'force-stop', 'FlutterApp'],
+        ),
+      );
+      processManager.addCommand(
+        const FakeCommand(
+          command: <String>['adb', '-s', '1234', 'install', '-t', '-r', 'app-debug.apk'],
+        ),
+      );
+      processManager.addCommand(kShaCommand);
+      processManager.addCommand(
+        const FakeCommand(
+          command: <String>[
+            'adb',
+            '-s',
+            '1234',
+            'shell',
+            'am',
+            'start',
+            '-a',
+            'android.intent.action.MAIN',
+            '-c',
+            'android.intent.category.LAUNCHER',
+            '-f',
+            '0x20000000',
+            '--ez',
+            'enable-dart-profiling',
+            'true',
+            '--ez',
+            'enable-impeller',
+            'true',
+            '--ez',
+            'enable-hcpp-and-surface-control',
+            'true',
+            'FlutterActivity',
+          ],
+        ),
+      );
+
+      final LaunchResult launchResult = await device.startApp(
+        apk,
+        prebuiltApplication: true,
+        debuggingOptions: DebuggingOptions.disabled(
+          BuildInfo.release,
+          enableImpeller: ImpellerStatus.enabled,
+          enableHcpp: true,
+        ),
+        platformArgs: <String, dynamic>{},
+      );
+
+      expect(launchResult.started, true);
+      expect(processManager, hasNoRemainingExpectations);
+    },
+  );
+
   testWithoutContext('AndroidDevice.startApp forwards all supported debugging options', () async {
     final device = AndroidDevice(
       '1234',
@@ -224,6 +308,7 @@ void main() {
           '--ez', 'purge-persistent-cache', 'true',
           '--ez', 'enable-impeller', 'true',
           '--ez', 'enable-flutter-gpu', 'true',
+          '--ez', 'enable-hcpp-and-surface-control', 'true',
           '--ez', 'enable-checked-mode', 'true',
           '--ez', 'verify-entry-points', 'true',
           '--ez', 'start-paused', 'true',
@@ -260,6 +345,7 @@ void main() {
         enableImpeller: ImpellerStatus.enabled,
         enableFlutterGpu: true,
         profileStartup: true,
+        enableHcpp: true,
       ),
       platformArgs: <String, dynamic>{},
       userIdentifier: '10',
