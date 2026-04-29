5a2e65b78216f72149405541862247f9645d6118
Qun Cheng <quncheng@google.com>
2026-04-29 11:59:56 -0700

    Revert solution adding an additional map

--- packages/flutter/lib/src/semantics/semantics.dart
+++ packages/flutter/lib/src/semantics/semantics.dart
@@ -4854,7 +4854,6 @@ class SemanticsOwner extends ChangeNotifier {
   final Set<SemanticsNode> _detachedNodes = <SemanticsNode>{};
   final Map<Object, SemanticsNode> _traversalParentNodes = <Object, SemanticsNode>{};
   final Map<Object, Set<SemanticsNode>> _traversalChildNodes = <Object, Set<SemanticsNode>>{};
-  final Map<Object, Set<SemanticsNode>> _forceToUpdateChildNodes = <Object, Set<SemanticsNode>>{};
 
   /// The root node of the semantics tree, if any.
   ///
@@ -4968,10 +4967,6 @@ class SemanticsOwner extends ChangeNotifier {
         for (final Set<SemanticsNode> childSet in _traversalChildNodes.values) {
           childSet.removeWhere((SemanticsNode oldNode) => node == oldNode);
         }
-        // Clean up _forceToUpdateChildNodes map
-        _forceToUpdateChildNodes.removeWhere(
-          (Object key, Set<SemanticsNode> nodeSet) => nodeSet.contains(node),
-        );
       }
     }
 
@@ -4979,24 +4974,6 @@ class SemanticsOwner extends ChangeNotifier {
     final SemanticsUpdateBuilder builder = SemanticsBinding.instance.createSemanticsUpdateBuilder();
 
     final updatedVisitedNodes = <SemanticsNode>[];
-
-    // If the node is a traversal child and its traversal parent is not in the tree
-    // yet, store these children nodes and should be updated later when the traversal
-    // parent is added to the tree.
-    if (!kIsWeb) {
-      for (final node in visitedNodes) {
-        if (node._isTraversalChild &&
-            !_traversalParentNodes.containsKey(node.traversalChildIdentifier)) {
-          if (node.parent == null ||
-              node.traversalParent == null ||
-              node.parent == node.traversalParent) {
-            continue;
-          }
-          _forceToUpdateChildNodes[node.traversalChildIdentifier!] ??= <SemanticsNode>{};
-          _forceToUpdateChildNodes[node.traversalChildIdentifier!]!.add(node);
-        }
-      }
-    }
 
     for (final node in visitedNodes) {
       final bool isTraversalParent = node._isTraversalParent;
@@ -5044,11 +5021,10 @@ class SemanticsOwner extends ChangeNotifier {
           // If the node is a traversal parent, then its grafted traversal
           // children should also be sent for update.
           final Set<SemanticsNode>? forceUpdateNodes =
-              _forceToUpdateChildNodes[node.traversalParentIdentifier];
+              _traversalChildNodes[node.traversalParentIdentifier];
           if (forceUpdateNodes != null) {
             for (final SemanticsNode child in forceUpdateNodes) {
               if (child.attached && !child._dirty && !updatedVisitedNodes.contains(child)) {
-                child._markDirty();
                 updatedVisitedNodes.add(child);
               }
             }
