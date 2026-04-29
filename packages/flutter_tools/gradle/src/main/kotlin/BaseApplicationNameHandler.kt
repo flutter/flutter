7f6b37da44d0a120fc562b5e59bcc82a279f5990
jesswrd <jesswon@google.com>
2026-04-29 02:45:27 -0700

    more tests should pass

--- packages/flutter_tools/gradle/src/main/kotlin/BaseApplicationNameHandler.kt
+++ packages/flutter_tools/gradle/src/main/kotlin/BaseApplicationNameHandler.kt
@@ -26,6 +26,8 @@ object BaseApplicationNameHandler {
             baseApplicationName = project.property(GRADLE_BASE_APPLICATION_NAME_PROPERTY).toString()
         }
 
-        androidComponentsExtension.defaultConfig.manifestPlaceholders["applicationName"] = baseApplicationName
+        androidComponentsExtension.defaultConfig {
+            manifestPlaceholders["applicationName"] = baseApplicationName
+        }
     }
 }
