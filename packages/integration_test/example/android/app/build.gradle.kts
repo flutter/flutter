e968497949f27bd0fa4139a26aba998c9c5278df
Andy Wolff <awolff@google.com>
2026-05-05 11:54:00 -0700

    Update integration_test/example/android/app/build.gradle.kts dep versions to match those in integration_test/android/build.gradle.kts, fixing a dep conflict

--- packages/integration_test/example/android/app/build.gradle.kts
+++ packages/integration_test/example/android/app/build.gradle.kts
@@ -49,6 +49,6 @@ flutter {
 
 dependencies {
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test:runner:1.6.2")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+    androidTestImplementation("androidx.test:runner:1.2+")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.2+")
 }
