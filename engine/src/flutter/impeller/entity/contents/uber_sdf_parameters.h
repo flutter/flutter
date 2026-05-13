96c5935da1468dcf70ce399d262abfbf3ef43778
Evan Walley <evanwall@buffalo.edu>
2026-05-12 20:42:02 -0700

    More comments

--- engine/src/flutter/impeller/entity/contents/uber_sdf_parameters.h
+++ engine/src/flutter/impeller/entity/contents/uber_sdf_parameters.h
@@ -79,23 +79,74 @@ struct UberSDFParameters {
   /// The stroke parameters. If std::nullopt, the shape is filled.
   std::optional<StrokeParameters> stroke;
 
+  /// The degree (n) of the superellipse curve for the top octant of each
+  /// quadrant.
   Vector4 superellipse_degrees_top;
+
+  /// The degree (n) of the superellipse curve for the right octant of each
+  /// quadrant.
   Vector4 superellipse_degrees_right;
+
+  /// The semi-axis length of the superellipse curve for the top octant of each
+  /// quadrant.
   Vector4 superellipse_semi_axes_top;
+
+  /// The semi-axis length of the superellipse curve for the right octant of
+  /// each quadrant.
   Vector4 superellipse_semi_axes_right;
+
+  /// The angular span of the circular cap for the top octant of each quadrant.
   Vector4 angle_spans_top;
+
+  /// The angular span of the circular cap for the right octant of each
+  /// quadrant.
   Vector4 angle_spans_right;
+
+  /// The geometric offset 'c' used to connect the two octants of each quadrant.
   Vector4 octant_offsets_c;
+
+  /// The horizontal corner radii for rounded shapes and circular caps of
+  /// superellipses.
   Vector4 radii_width;
+
+  /// The vertical corner radii for rounded shapes and circular caps of
+  /// superellipses.
   Vector4 radii_height;
+
+  /// The X coordinates of the circular cap centers for the top octant of each
+  /// quadrant.
   Vector4 circle_centers_top_x;
+
+  /// The Y coordinates of the circular cap centers for the top octant of each
+  /// quadrant.
   Vector4 circle_centers_top_y;
+
+  /// The X coordinates of the circular cap centers for the right octant of each
+  /// quadrant.
   Vector4 circle_centers_right_x;
+
+  /// The Y coordinates of the circular cap centers for the right octant of each
+  /// quadrant.
   Vector4 circle_centers_right_y;
+
+  /// The X scaling factors used to transform normalized superellipses to their
+  /// true size.
   Vector4 superellipse_scales_x;
+
+  /// The Y scaling factors used to transform normalized superellipses to their
+  /// true size.
   Vector4 superellipse_scales_y;
+
+  /// The X coordinates of the geometric centers for each of the four corner
+  /// quadrants.
   Vector4 quadrant_centers_x;
+
+  /// The Y coordinates of the geometric centers for each of the four corner
+  /// quadrants.
   Vector4 quadrant_centers_y;
+
+  /// The local coordinate split points (top, bottom, left, right) dividing the
+  /// quadrants.
   Vector4 quadrant_splits;
 };
 
