d84201b73ebb67b39e46f1efa78ec4b819d06d0a
Aleksandr Lozhkovoi <lozhkovoi@gmail.com>
2026-05-17 13:48:36 +0200

    Support custom Flutter Gradle plugin repository

--- packages/flutter_tools/gradle/settings.gradle.kts
+++ packages/flutter_tools/gradle/settings.gradle.kts
@@ -1,6 +1,32 @@
+pluginManagement {
+    repositories {
+        val flutterAndroidGradlePluginRepository: String? =
+            System.getenv("FLUTTER_ANDROID_GRADLE_PLUGIN_REPOSITORY")
+                ?.trim()
+                ?.takeIf { it.isNotEmpty() }
+        if (flutterAndroidGradlePluginRepository != null) {
+            maven {
+                url = uri(flutterAndroidGradlePluginRepository)
+            }
+        }
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
+        val flutterAndroidGradlePluginRepository: String? =
+            System.getenv("FLUTTER_ANDROID_GRADLE_PLUGIN_REPOSITORY")
+                ?.trim()
+                ?.takeIf { it.isNotEmpty() }
+        if (flutterAndroidGradlePluginRepository != null) {
+            maven {
+                url = uri(flutterAndroidGradlePluginRepository)
+            }
+        }
         google()
         mavenCentral()
     }
