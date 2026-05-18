a1d02adbd996dfbd7d1d60d2ae27e0beeea36c4e
Matej Knopp <matej.knopp@gmail.com>
2026-05-18 22:41:25 +0200

    Rename.

--- packages/flutter/lib/src/rendering/object.dart
+++ packages/flutter/lib/src/rendering/object.dart
@@ -1322,7 +1322,7 @@ base class PipelineOwner with DiagnosticableTreeMixin {
       }());
       final List<RenderObject> dirtyNodes = _nodesNeedingPaint;
       if (dirtyNodes.isNotEmpty && rootNode is RenderView) {
-        (rootNode! as RenderView).markRequiresCompositing();
+        (rootNode! as RenderView).markNeedsCompositeFrame();
       }
       _nodesNeedingPaint = <RenderObject>[];
 
