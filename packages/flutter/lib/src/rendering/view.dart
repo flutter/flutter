f5e57bfcb477fb3457746089adf6ebe9c0a4b2d6
Matej Knopp <matej.knopp@gmail.com>
2026-05-18 22:17:24 +0200

    Add check before writing to timeline.

--- packages/flutter/lib/src/rendering/view.dart
+++ packages/flutter/lib/src/rendering/view.dart
@@ -360,7 +360,7 @@ class RenderView extends RenderObject with RenderObjectWithChildMixin<RenderBox>
   /// the [configuration] must be set to a non-null value, and the
   /// [prepareInitialFrame] method must have been called.
   void compositeFrame() {
-    if (!kReleaseMode) {
+    if (!kReleaseMode && _requiresCompositing) {
       FlutterTimeline.startSync('COMPOSITING');
     }
     try {
@@ -384,7 +384,7 @@ class RenderView extends RenderObject with RenderObjectWithChildMixin<RenderBox>
         return true;
       }());
     } finally {
-      if (!kReleaseMode) {
+      if (!kReleaseMode && _requiresCompositing) {
         FlutterTimeline.finishSync();
       }
       _requiresCompositing = false;
