902df09e4fc56e04583d3462ab95112408f0ed39
Qun Cheng <quncheng@google.com>
2026-05-19 15:24:33 -0700

    Update assert

--- packages/flutter/lib/src/semantics/semantics.dart
+++ packages/flutter/lib/src/semantics/semantics.dart
@@ -5021,7 +5021,7 @@ class SemanticsOwner extends ChangeNotifier {
     final SemanticsUpdateBuilder builder = SemanticsBinding.instance.createSemanticsUpdateBuilder();
 
     for (final node in visitedNodes) {
-      assert(node.parent?._dirty != true); // could be null (no parent) or false (not dirty)
+      assert(node == this.rootSemanticsNode || (node.parent != null && node.parent!._dirty != true));
       // The _serialize() method marks the node as not dirty, and
       // recurses through the tree to do a deep serialization of all
       // contiguous dirty nodes. This means that when we return here,
