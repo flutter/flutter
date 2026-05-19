a1185693b59f423d2ad4f3472ce0f79f9a2aab7c
Aaron Clarke <aaclarke@google.com>
2026-05-19 20:39:21 +0000

    moved to a driver test

---
+++ examples/texture/test_driver/texture_test_test.dart
@@ -0,0 +1,69 @@
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter_driver/flutter_driver.dart';
+import 'package:test/test.dart' hide TypeMatcher, isInstanceOf;
+
+void main() {
+  group('Texture rendering integration test', () {
+    late FlutterDriver driver;
+
+    setUpAll(() async {
+      driver = await FlutterDriver.connect();
+    });
+
+    tearDownAll(() async {
+      await driver.close();
+    });
+
+    test('renders red color when Red button is pressed', () async {
+      // Find the "Red" button.
+      final SerializableFinder redButton = find.text('Red');
+      await driver.waitFor(redButton);
+
+      // Take initial screenshot before changing the color to Red.
+      final List<int> initialScreenshot = await driver.screenshot(
+        format: ScreenshotFormat.rawStraightRgba,
+      );
+
+      final int initialRedPixels = countRedPixels(initialScreenshot);
+      // ignore: avoid_print
+      print('Initial red pixel count: $initialRedPixels');
+      // Initially, there shouldn't be a significant number of pixels matching the texture's red color.
+      expect(initialRedPixels, lessThan(100));
+
+      // Tap the "Red" button.
+      await driver.tap(redButton);
+
+      // Wait a short duration to ensure a frame is rasterized after the tap.
+      await Future<void>.delayed(const Duration(seconds: 1));
+
+      // Take another screenshot.
+      final List<int> afterScreenshot = await driver.screenshot(
+        format: ScreenshotFormat.rawStraightRgba,
+      );
+
+      final int afterRedPixels = countRedPixels(afterScreenshot);
+      // ignore: avoid_print
+      print('After red pixel count: $afterRedPixels');
+
+      // The texture size is 300x300. Even with scaling/different device pixel ratio,
+      // there should be a substantial block of red pixels (at least several thousands).
+      expect(afterRedPixels, greaterThan(10000));
+    });
+  });
+}
+
+int countRedPixels(List<int> data) {
+  var count = 0;
+  for (var i = 0; i < data.length; i += 4) {
+    final int r = data[i];
+    final int g = data[i + 1];
+    final int b = data[i + 2];
+    if ((r - 0xf2).abs() < 5 && (g - 0x5d).abs() < 5 && (b - 0x50).abs() < 5) {
+      count++;
+    }
+  }
+  return count;
+}
