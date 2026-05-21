cf4f6ff6ec4b410827dbd29b2509713e88b31147
Vincent Ong <mvincentong@gmail.com>
2026-05-21 13:23:26 +0800

    Make ImageProvider sample web-compatible

--- examples/api/lib/painting/image_provider/image_provider.0.dart
+++ examples/api/lib/painting/image_provider/image_provider.0.dart
@@ -3,11 +3,11 @@
 // found in the LICENSE file.
 
 import 'dart:async';
-import 'dart:io';
 import 'dart:ui' as ui;
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 @immutable
 class CustomNetworkImage extends ImageProvider<Uri> {
@@ -30,51 +30,20 @@ class CustomNetworkImage extends ImageProvider<Uri> {
     return SynchronousFuture<Uri>(result);
   }
 
-  static HttpClient get _httpClient {
-    HttpClient? client;
-    assert(() {
-      if (debugNetworkImageHttpClientProvider != null) {
-        client = debugNetworkImageHttpClientProvider!();
-      }
-      return true;
-    }());
-    return client ?? HttpClient()
-      ..autoUncompress = false;
-  }
-
   @override
   ImageStreamCompleter loadImage(Uri key, ImageDecoderCallback decode) {
     final StreamController<ImageChunkEvent> chunkEvents =
         StreamController<ImageChunkEvent>();
     debugPrint('Fetching "$key"...');
     return MultiFrameImageStreamCompleter(
-      codec: _httpClient
-          .getUrl(key)
-          .then<HttpClientResponse>(
-            (HttpClientRequest request) => request.close(),
-          )
-          .then<Uint8List>((HttpClientResponse response) {
-            return consolidateHttpClientResponseBytes(
-              response,
-              onBytesReceived: (int cumulative, int? total) {
-                chunkEvents.add(
-                  ImageChunkEvent(
-                    cumulativeBytesLoaded: cumulative,
-                    expectedTotalBytes: total,
-                  ),
-                );
-              },
-            );
-          })
+      codec: _loadAsync(key, chunkEvents, decode: decode)
           .catchError((Object e, StackTrace stack) {
             scheduleMicrotask(() {
               PaintingBinding.instance.imageCache.evict(key);
             });
-            return Future<Uint8List>.error(e, stack);
+            return Future<ui.Codec>.error(e, stack);
           })
-          .whenComplete(chunkEvents.close)
-          .then<ui.ImmutableBuffer>(ui.ImmutableBuffer.fromUint8List)
-          .then<ui.Codec>(decode),
+          .whenComplete(chunkEvents.close),
       chunkEvents: chunkEvents.stream,
       scale: 1.0,
       debugLabel: '"key"',
@@ -83,6 +52,53 @@ class CustomNetworkImage extends ImageProvider<Uri> {
         DiagnosticsProperty<Uri>('URL', key),
       ],
     );
+  }
+
+  Future<ui.Codec> _loadAsync(
+    Uri key,
+    StreamController<ImageChunkEvent> chunkEvents, {
+    required ImageDecoderCallback decode,
+  }) async {
+    final http.Client client = http.Client();
+    try {
+      final http.StreamedResponse response = await client.send(
+        http.Request('GET', key),
+      );
+      if (response.statusCode != 200) {
+        await response.stream.drain<List<int>>(<int>[]);
+        throw NetworkImageLoadException(
+          statusCode: response.statusCode,
+          uri: key,
+        );
+      }
+
+      final List<List<int>> chunks = <List<int>>[];
+      int cumulativeBytesLoaded = 0;
+      await for (final List<int> chunk in response.stream) {
+        chunks.add(chunk);
+        cumulativeBytesLoaded += chunk.length;
+        chunkEvents.add(
+          ImageChunkEvent(
+            cumulativeBytesLoaded: cumulativeBytesLoaded,
+            expectedTotalBytes: response.contentLength,
+          ),
+        );
+      }
+
+      if (cumulativeBytesLoaded == 0) {
+        throw Exception('NetworkImage is an empty file: $key');
+      }
+      final Uint8List imageBytes = Uint8List(cumulativeBytesLoaded);
+      int offset = 0;
+      for (final List<int> chunk in chunks) {
+        imageBytes.setRange(offset, offset + chunk.length, chunk);
+        offset += chunk.length;
+      }
+
+      return await decode(await ui.ImmutableBuffer.fromUint8List(imageBytes));
+    } finally {
+      client.close();
+    }
   }
 
   @override
--- examples/api/test/painting/image_provider/image_provider.0_test.dart
+++ examples/api/test/painting/image_provider/image_provider.0_test.dart
@@ -3,21 +3,27 @@
 // found in the LICENSE file.
 
 import 'package:flutter/foundation.dart';
+import 'package:flutter/painting.dart';
 import 'package:flutter_api_samples/painting/image_provider/image_provider.0.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  testWidgets('$CustomNetworkImage', (WidgetTester tester) async {
+  test('$CustomNetworkImage', () async {
     const String expectedUrl =
         'https://flutter.github.io/assets-for-api-docs/assets/widgets/flamingos.jpg?dpr=3.0&locale=en-US&platform=android&width=800.0&height=600.0&bidi=ltr';
-    final List<String> log = <String>[];
-    final DebugPrintCallback originalDebugPrint = debugPrint;
-    debugPrint = (String? message, {int? wrapWidth}) {
-      log.add('$message');
-    };
-    await tester.pumpWidget(const ExampleApp());
-    expect(tester.takeException().toString(), 'Exception: Invalid image data');
-    expect(log, <String>['Fetching "$expectedUrl"...']);
-    debugPrint = originalDebugPrint;
+    final Uri key =
+        await const CustomNetworkImage(
+          'https://flutter.github.io/assets-for-api-docs/assets/widgets/flamingos.jpg',
+        ).obtainKey(
+          const ImageConfiguration(
+            devicePixelRatio: 3.0,
+            locale: Locale('en', 'US'),
+            platform: TargetPlatform.android,
+            size: Size(800.0, 600.0),
+            textDirection: TextDirection.ltr,
+          ),
+        );
+
+    expect(key.toString(), expectedUrl);
   });
 }
