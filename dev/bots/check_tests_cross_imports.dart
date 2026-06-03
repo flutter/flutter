9a9241726b081e07567e2646e2d3ff524efcfaa2
marlonjd <burak.karahan@mail.ru>
2026-06-03 10:05:50 +0300

    fix(test): remove migrated rendering cross-import entry

--- dev/bots/check_tests_cross_imports.dart
+++ dev/bots/check_tests_cross_imports.dart
@@ -163,7 +163,6 @@ class TestsCrossImportChecker {
     'packages/flutter/test/rendering/proxy_getters_and_setters_test.dart',
     'packages/flutter/test/rendering/view_chrome_style_test.dart',
     'packages/flutter/test/rendering/proxy_box_test.dart',
-    'packages/flutter/test/rendering/sliver_tree_test.dart',
     'packages/flutter/test/rendering/object_test.dart',
   };
   static final Set<String> knownSchedulerCrossImports = <String>{};
