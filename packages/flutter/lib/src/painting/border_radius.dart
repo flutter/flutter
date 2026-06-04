901d0e157ad9dd8f65e8e081c8c140cfabae01d7
Eden Amiga <71780707+edenpro2@users.noreply.github.com>
2026-06-04 19:28:15 +0300

    Make BorderRadius.circular constructor const
    Updated BorderRadius.circular constructor to be const for better performance.
    
    Why does it need to always recreate the radius?

--- packages/flutter/lib/src/painting/border_radius.dart
+++ packages/flutter/lib/src/painting/border_radius.dart
@@ -358,7 +358,7 @@ class BorderRadius extends BorderRadiusGeometry {
   /// Creates a border radius where all radii are [Radius.circular(radius)].
   ///
   /// Consider using the `const` [BorderRadius.all] constructor for better performance.
-  BorderRadius.circular(double radius) : this.all(Radius.circular(radius));
+  const BorderRadius.circular(double radius) : this.all(Radius.circular(radius));
 
   /// Creates a vertically symmetric border radius where the top and bottom
   /// sides of the rectangle have the same radii.
