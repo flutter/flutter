flutter/flutter

commit 1e492c7f4210d75811a66e0fbe71e813f8cfb729

fix(tool): initialize asset isModified state on startup to prevent 2x hot restart slowdown (#187488)
Prior to this fix, the first hot restart would incorrectly detect all
assets as modified and sync/recompile all of them. This was because
DevFS.updateBundle bypassed the asset loop during the initial
application startup, which prevented the stateful isModified getter from
being initialized.

This fix accesses isModified for each asset in the bundle during startup
to initialize the cache, matching the behavior before the asset
processing refactor.

Fixes https://github.com/flutter/flutter/issues/187391

packages/flutter_tools/lib/src/devfs.dart
@@ -36,13 +36,13 @@ DevFSConfig? get devFSConfig => context.get<DevFSConfig>();
 
 /// Common superclass for content copied to the device.
 abstract class DevFSContent {
-  /// Return true if this is the first time this method is called
-  /// or if the entry has been modified since this method was last called.
+  /// Return true if the entry has been modified since it was last marked clean.
   bool get isModified;
 
-  /// Return true if this is the first time this method is called
-  /// or if the entry has been modified after the given time
-  /// or if the given time is null.
+  /// Mark the content as clean, resetting the [isModified] state.
+  void markClean();
+
+  /// Return true if the entry has been modified after the given time.
   bool isModifiedAfter(DateTime time);
 
   int get size;
@@ -76,66 +76,72 @@ class DevFSFileContent extends DevFSContent {
     return file as File;
   }
 
-  void _stat() {
-    final File? linkTarget = _linkTarget;
+  (FileStat?, File?) _statFile() {
+    File? linkTarget = _linkTarget;
     if (linkTarget != null) {
       // Stat the cached symlink target.
       final FileStat fileStat = linkTarget.statSync();
-      if (fileStat.type == FileSystemEntityType.notFound) {
-        _linkTarget = null;
-      } else {
-        _fileStat = fileStat;
-        return;
+      if (fileStat.type != FileSystemEntityType.notFound) {
+        return (fileStat, linkTarget);
       }
+      linkTarget = null;
     }
     final FileStat fileStat = file.statSync();
-    _fileStat = fileStat.type == FileSystemEntityType.notFound ? null : fileStat;
-    if (_fileStat != null && _fileStat?.type == FileSystemEntityType.link) {
-      // Resolve, stat, and maybe cache the symlink target.
-      final String resolved = file.resolveSymbolicLinksSync();
-      final File linkTarget = file.fileSystem.file(resolved);
-      // Stat the link target.
-      final FileStat fileStat = linkTarget.statSync();
-      if (fileStat.type == FileSystemEntityType.notFound) {
-        _fileStat = null;
-        _linkTarget = null;
-      } else if (devFSConfig?.cacheSymlinks ?? false) {
-        _linkTarget = linkTarget;
+    if (fileStat.type == FileSystemEntityType.notFound) {
+      return (null, null);
+    }
+    if (fileStat.type == FileSystemEntityType.link) {
+      try {
+        final String resolved = file.resolveSymbolicLinksSync();
+        final File resolvedFile = file.fileSystem.file(resolved);
+        // Stat the link target.
+        final FileStat fileStat = resolvedFile.statSync();
+        if (fileStat.type == FileSystemEntityType.notFound) {
+          return (null, null);
+        }
+        return (fileStat, resolvedFile);
+      } on FileSystemException {
+        return (null, null);
       }
     }
+    return (fileStat, null);
   }
 
   @override
   bool get isModified {
-    final FileStat? oldFileStat = _fileStat;
-    _stat();
-    final FileStat? newFileStat = _fileStat;
-    if (oldFileStat == null && newFileStat == null) {
+    final (FileStat? currentStat, _) = _statFile();
+    if (_fileStat == null && currentStat == null) {
       return false;
     }
-    return oldFileStat == null ||
-        newFileStat == null ||
-        newFileStat.modified.isAfter(oldFileStat.modified);
+    return _fileStat == null ||
+        currentStat == null ||
+        currentStat.modified.isAfter(_fileStat!.modified);
+  }
+
+  @override
+  void markClean() {
+    final (FileStat? fileStat, File? linkTarget) = _statFile();
+    _fileStat = fileStat;
+    if (linkTarget != null && (devFSConfig?.cacheSymlinks ?? false)) {
+      _linkTarget = linkTarget;
+    } else {
+      _linkTarget = null;
+    }
   }
 
   @override
   bool isModifiedAfter(DateTime time) {
-    final FileStat? oldFileStat = _fileStat;
-    _stat();
-    final FileStat? newFileStat = _fileStat;
-    if (oldFileStat == null && newFileStat == null) {
+    final (FileStat? currentStat, _) = _statFile();
+    if (_fileStat == null && currentStat == null) {
       return false;
     }
-    return oldFileStat == null || newFileStat == null || newFileStat.modified.isAfter(time);
+    return _fileStat == null || currentStat == null || currentStat.modified.isAfter(time);
   }
 
   @override
   int get size {
-    if (_fileStat == null) {
-      _stat();
-    }
-    // Can still be null if the file wasn't found.
-    return _fileStat?.size ?? 0;
+    final FileStat? stat = _fileStat ?? _statFile().$1;
+    return stat?.size ?? 0;
   }
 
   @override
@@ -155,12 +161,12 @@ class DevFSByteContent extends DevFSContent {
 
   List<int> get bytes => _bytes;
 
-  /// Return true only once so that the content is written to the device only once.
   @override
-  bool get isModified {
-    final bool modified = _isModified;
+  bool get isModified => _isModified;
+
+  @override
+  void markClean() {
     _isModified = false;
-    return modified;
   }
 
   @override
@@ -213,12 +219,12 @@ class DevFSStringCompressingBytesContent extends DevFSContent {
 
   late final List<int> bytes = _compressor.convert(utf8.encode(_string));
 
-  /// Return true only once so that the content is written to the device only once.
   @override
-  bool get isModified {
-    final bool modified = _isModified;
+  bool get isModified => _isModified;
+
+  @override
+  void markClean() {
     _isModified = false;
-    return modified;
   }
 
   @override
@@ -714,6 +720,9 @@ class DevFS {
     void Function()? onFontManifestUpdated,
   }) async {
     if (bundleFirstUpload && !syncAllAssetsOnFirstUpload) {
+      for (final AssetBundleEntry entry in bundle.entries.values) {
+        entry.content.markClean();
+      }
       return 0;
     }
 
@@ -721,10 +730,12 @@ class DevFS {
     final pendingAssetBuilds = <Future<void>>[];
     var syncedBytes = 0;
 
+    final syncedEntries = <AssetBundleEntry>[];
     bundle.entries.forEach((String archivePath, AssetBundleEntry entry) {
       if (!bundleFirstUpload && !entry.content.isModified) {
         return;
       }
+      syncedEntries.add(entry);
 
       // Modified shaders must be recompiled per-target platform.
       final Uri deviceUri = fileSystem.path.toUri(
@@ -791,6 +802,9 @@ class DevFS {
     });
 
     await Future.wait(pendingAssetBuilds);
+    for (final entry in syncedEntries) {
+      entry.content.markClean();
+    }
     return syncedBytes;
   }
 }

packages/flutter_tools/test/general.shard/devfs_test.dart
@@ -61,6 +61,9 @@ void main() {
 
     expect(content.bytes, orderedEquals(<int>[4, 5, 6]));
     expect(content.isModified, isTrue);
+    expect(content.isModified, isTrue);
+    content.markClean();
+    expect(content.isModified, isFalse);
     expect(content.isModified, isFalse);
   });
 
@@ -70,6 +73,9 @@ void main() {
     expect(content.string, 'some string');
     expect(content.bytes, orderedEquals(utf8.encode('some string')));
     expect(content.isModified, isTrue);
+    expect(content.isModified, isTrue);
+    content.markClean();
+    expect(content.isModified, isFalse);
     expect(content.isModified, isFalse);
   });
 
@@ -77,6 +83,7 @@ void main() {
     final FileSystem fileSystem = MemoryFileSystem.test();
     final File file = fileSystem.file('foo.txt');
     final content = DevFSFileContent(file);
+    content.markClean();
     expect(content.isModified, isFalse);
     expect(content.isModified, isFalse);
 
@@ -90,6 +97,8 @@ void main() {
     file.writeAsBytesSync(<int>[2, 3, 4], flush: true);
 
     expect(content.isModified, isTrue);
+    expect(content.isModified, isTrue);
+    content.markClean();
     expect(content.isModified, isFalse);
     expect(await content.contentsAsBytes(), <int>[2, 3, 4]);
 
@@ -98,6 +107,8 @@ void main() {
 
     file.deleteSync();
     expect(content.isModified, isTrue);
+    expect(content.isModified, isTrue);
+    content.markClean();
     expect(content.isModified, isFalse);
     expect(content.isModified, isFalse);
   });
@@ -108,6 +119,9 @@ void main() {
     expect(content.equals('uncompressed string'), isTrue);
     expect(content.bytes, isNotNull);
     expect(content.isModified, isTrue);
+    expect(content.isModified, isTrue);
+    content.markClean();
+    expect(content.isModified, isFalse);
     expect(content.isModified, isFalse);
   });
 
@@ -992,6 +1006,68 @@ void main() {
         // In the broken code, this could fail if updateBundle returned before the .then callback finished.
         expect(dirtyEntries, hasLength(1));
         expect(await dirtyEntries.values.first.contentsAsBytes(), utf8.encode('compiled'));
+      },
+    );
+
+    testWithoutContext(
+      'DevFS.updateBundle initializes isModified state of assets during first upload when sync is skipped',
+      () async {
+        final FileSystem fileSystem = MemoryFileSystem.test();
+        final dirtyEntries = <Uri, DevFSContent>{};
+        final assetBundle = FakeBundle();
+        final assetContent = DevFSByteContent(<int>[1, 2, 3, 4]);
+        assetBundle.entries['asset.txt'] = AssetBundleEntry(
+          assetContent,
+          kind: AssetKind.regular,
+          transformers: const [],
+        );
+
+        const shaderCompiler = FakeShaderCompiler();
+        final assetTransformer = DevelopmentAssetTransformer(
+          fileSystem: fileSystem,
+          transformer: AssetTransformer(
+            processManager: FakeProcessManager.any(),
+            fileSystem: fileSystem,
+            dartBinaryPath: 'dart',
+            buildMode: BuildMode.debug,
+          ),
+          logger: BufferLogger.test(),
+        );
+
+        // Perform the first upload with sync skipped.
+        final int firstUploadSyncedBytes = await DevFS.updateBundle(
+          bundle: assetBundle,
+          dirtyEntries: dirtyEntries,
+          assetDirectory: 'assets',
+          assetTransformer: assetTransformer,
+          shaderCompiler: shaderCompiler,
+          fileSystem: fileSystem,
+          rootDirectoryPath: '/',
+          assetPathsToEvict: <String>{},
+          shaderPathsToEvict: <String>{},
+          bundleFirstUpload: true,
+        );
+
+        expect(firstUploadSyncedBytes, 0);
+        expect(dirtyEntries, isEmpty);
+
+        // Perform a subsequent hot restart update (where bundleFirstUpload is false).
+        // Since the asset has not been modified since the first upload, it should not be synced.
+        final int secondUploadSyncedBytes = await DevFS.updateBundle(
+          bundle: assetBundle,
+          dirtyEntries: dirtyEntries,
+          assetDirectory: 'assets',
+          assetTransformer: assetTransformer,
+          shaderCompiler: shaderCompiler,
+          fileSystem: fileSystem,
+          rootDirectoryPath: '/',
+          assetPathsToEvict: <String>{},
+          shaderPathsToEvict: <String>{},
+          bundleFirstUpload: false,
+        );
+
+        expect(secondUploadSyncedBytes, 0);
+        expect(dirtyEntries, isEmpty);
       },
     );
   });

packages/flutter_tools/lib/src/commands/test.dart
@@ -845,9 +845,7 @@ class TestCommand extends FlutterCommand with DeviceBasedDevelopmentArtifacts {
         .map((AssetBundleEntry asset) => asset.content)
         .whereType<DevFSFileContent>();
     for (final entry in files) {
-      // Calling isModified to access file stats first in order for isModifiedAfter
-      // to work.
-      if (entry.isModified && entry.isModifiedAfter(lastModified)) {
+      if (entry.isModifiedAfter(lastModified)) {
         return true;
       }
     }

packages/flutter_tools/test/general.shard/asset_bundle_test.dart
@@ -629,6 +629,7 @@ flutter:
         );
 
         expect(bundle.entries['my-asset.txt']!.content.isModified, isTrue);
+        bundle.entries['my-asset.txt']!.content.markClean();
 
         await bundle.build(
           packageConfigPath: '.dart_tool/package_config.json',

commit 33213e68e7823e113ba5ac4949b0bbd2de0e786b

Compare isModifiedAfter against the given time
DevFSFileContent.isModifiedAfter returned true whenever the cached
_fileStat was null, ignoring the time argument entirely. _fileStat is the
sync baseline written only by markClean(); the entries of a freshly built
AssetBundle are never marked clean, so _fileStat is always null for them
and isModifiedAfter always reported them as modified.

flutter test's _needsRebuild() relies on isModifiedAfter to decide
whether build/unit_test_assets is up to date. With the broken short
circuit it always rebuilt the directory (delete + non-atomic rewrite) on
every invocation. When several flutter test invocations run in the same
project directory, one invocation's delete window overlaps another's
asset reads, surfacing as a transient

    Exception: Asset 'shaders/ink_sparkle.frag' not found

that fails an unrelated test.

isModifiedAfter is handed an explicit reference time and must not consult
the sync baseline. Determine modification purely from the file's current
modification time, matching the other DevFSContent implementations.

Regression from #187488, which made the stat accessors pure. Before that
the mutating _stat() populated _fileStat as a side effect of reads during
the asset build, so _needsRebuild happened to work. Fixes
flutter/flutter#187725.

packages/flutter_tools/lib/src/devfs.dart
@@ -131,11 +131,12 @@ class DevFSFileContent extends DevFSContent {
 
   @override
   bool isModifiedAfter(DateTime time) {
+    // Whether the file changed after [time] only depends on the file's current
+    // modification time, not on the cached [_fileStat]. Falling back to the
+    // cached stat (e.g. `_fileStat == null`) would report a freshly created
+    // DevFSFileContent as modified even when its file is older than [time].
     final (FileStat? currentStat, _) = _statFile();
-    if (_fileStat == null && currentStat == null) {
-      return false;
-    }
-    return _fileStat == null || currentStat == null || currentStat.modified.isAfter(time);
+    return currentStat != null && currentStat.modified.isAfter(time);
   }
 
   @override

packages/flutter_tools/test/general.shard/devfs_test.dart
@@ -113,6 +113,25 @@ void main() {
     expect(content.isModified, isFalse);
   });
 
+  testWithoutContext(
+    'DevFSFileContent.isModifiedAfter only depends on the file modification time',
+    () async {
+      final FileSystem fileSystem = MemoryFileSystem.test();
+      final File file = fileSystem.file('foo.txt')..writeAsBytesSync(<int>[1, 2, 3], flush: true);
+      // A freshly constructed DevFSFileContent has no cached stat, as is the
+      // case for the entries of a newly built asset bundle. Such an entry must
+      // not be reported as modified when its file is older than the given time.
+      final content = DevFSFileContent(file);
+
+      final DateTime modified = file.statSync().modified;
+      expect(content.isModifiedAfter(modified.add(const Duration(seconds: 5))), isFalse);
+      expect(content.isModifiedAfter(modified.subtract(const Duration(seconds: 5))), isTrue);
+
+      file.deleteSync();
+      expect(content.isModifiedAfter(modified.subtract(const Duration(seconds: 5))), isFalse);
+    },
+  );
+
   testWithoutContext('DevFSStringCompressingBytesContent', () {
     final content = DevFSStringCompressingBytesContent('uncompressed string');
 