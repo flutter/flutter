0d9af2aaf1731439da231f41a66f7d008f5f5af0
Luca Tagliabue <lu.tagliabue@reply.it>
2026-06-11 15:09:11 +0200

    Use null-aware elements in dev/devicelab/lib/integration_tests.dart

--- dev/devicelab/lib/tasks/integration_tests.dart
+++ dev/devicelab/lib/tasks/integration_tests.dart
@@ -8,7 +8,7 @@ import '../framework/talkback.dart' hide adbPath;
 import '../framework/task_result.dart';
 import '../framework/utils.dart';
 
-TaskFunction createChannelsIntegrationTest() {
+TaskFunction createChannelsIdentegrationTest() {
   return IntegrationTest(
     '${flutterDirectory.path}/dev/integration_tests/channels',
     'integration_test/main_test.dart',
@@ -295,9 +295,9 @@ class DriverTest {
       // Make the device ID available in the driver code, so tools like ADB can
       // reference it if needed.
       final env = <String, String>{
-        if (environment != null) ...environment!,
+        ...?environment,
         'FLUTTER_DEVICE_ID_NUMBER': deviceId,
-        if (devicelabAdbPath != null) 'FLUTTER_ADB_PATH': devicelabAdbPath,
+        'FLUTTER_ADB_PATH': ?devicelabAdbPath,
       };
 
       final options = <String>[
