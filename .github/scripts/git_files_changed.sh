945eec7d0f0fc67103e38749b2095b86e3b2912e
John McDole <codefu@google.com>
2026-06-12 11:33:13 -0700

    Add headers

--- .github/scripts/git_files_changed.sh
+++ .github/scripts/git_files_changed.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
-#
+# Copyright 2014 The Flutter Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
 # git_files_changed.sh <hash|ref>
 # Returns a list of files that change from the given hash or ref compared to HEAD.
 #
