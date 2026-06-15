76c1c25e613a7a9f680ceeea4be145f9e48ab0d8
Ben Konyi <bkonyi@google.com>
2026-06-15 14:07:02 +0000

    [VMService] Format raw_command.py

--- engine/src/flutter/build/raw_command.py
+++ engine/src/flutter/build/raw_command.py
@@ -6,12 +6,13 @@ import sys
 import subprocess
 
 def main():
-    if len(sys.argv) < 2:
-        print("Usage: python3 raw_command.py <command> [args...]")
-        return 1
+  if len(sys.argv) < 2:
+    print("Usage: python3 raw_command.py <command> [args...]")
+    return 1
 
-    # Run the command directly, forwarding all arguments
-    return subprocess.call(sys.argv[1:])
+  # Run the command directly, forwarding all arguments
+  return subprocess.call(sys.argv[1:])
+
 
 if __name__ == '__main__':
-    sys.exit(main())
+  sys.exit(main())
