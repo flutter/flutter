586e491b41bf3d5135e1e861c14041d47c5ca1ca
Ben Konyi <bkonyi@google.com>
2026-06-16 16:33:30 +0000

    Log VM Service startup failures in profile/release modes

--- engine/src/flutter/runtime/dart_isolate.cc
+++ engine/src/flutter/runtime/dart_isolate.cc
@@ -1037,7 +1037,7 @@ Dart_Isolate DartIsolate::DartCreateAndStartServiceIsolate(
           error                                   // error (out)
           )) {
     // Error is populated by call to startup.
-    FML_DLOG(ERROR) << *error;
+    FML_LOG(ERROR) << "DartServiceIsolate::Startup failed: " << *error;
     return nullptr;
   }
 
