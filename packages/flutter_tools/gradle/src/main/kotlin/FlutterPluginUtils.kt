9994a92d7dab5d43e4ab11d233102e93eef9d9a9
jesswrd <jesswon@google.com>
2026-06-25 17:50:22 -0700

    clean: Delete redundant hasNoSubprojectsApplyingKgp helper

--- packages/flutter_tools/gradle/src/main/kotlin/FlutterPluginUtils.kt
+++ packages/flutter_tools/gradle/src/main/kotlin/FlutterPluginUtils.kt
@@ -672,25 +672,7 @@ object FlutterPluginUtils {
         return SubprojectPluginState(hasKgpPlugin, hasAppPlugin, hasLibPlugin)
     }
 
-    /**
-     * Checks if all subprojects within the root project have migrated away from the legacy
-     * Kotlin Gradle Plugin (KGP) to Built-in Kotlin.
-     */
-    @JvmStatic
-    @JvmName("hasNoSubprojectsApplyingKgp")
-    internal fun hasNoSubprojectsApplyingKgp(project: Project): Boolean =
-        project.rootProject.subprojects.all { subproject ->
-            val subprojectPluginState = getSubprojectPluginState(subproject)
 
-            // Non-Android subprojects (those not applying com.android.application or com.android.library)
-            // are ignored as they do not affect the Android Kotlin build configuration.
-            // For Android subprojects, we verify they do not apply the legacy KGP.
-            if (subprojectPluginState == null || (!subprojectPluginState.hasAppPlugin && !subprojectPluginState.hasLibPlugin)) {
-                true
-            } else {
-                !subprojectPluginState.hasKgpPlugin
-            }
-        }
 
     /**
      * Determines if the Gradle property `android.builtInKotlin` is enabled globally across the multi-project Gradle build.
