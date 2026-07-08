87f96a2e82952c860fd1ee9ed50b1914536da014
Kevin Moore <kevmoo@google.com>
2026-07-08 03:02:41 +0000

    LOAD: ignore avoid_print in web test selector

--- packages/flutter_test/lib/src/_test_selector_web.dart
+++ packages/flutter_test/lib/src/_test_selector_web.dart
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// ignore_for_file: avoid_print
+
 import 'dart:async';
 import 'dart:js_interop';
 import 'dart:ui' as ui;
