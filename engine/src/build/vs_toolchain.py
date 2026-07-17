797fc90e56e3641d4fee2705f01390bd2496ed07
LongCatIsLooong <31859944+LongCatIsLooong@users.noreply.github.com>
2026-07-17 02:33:20 -0700

    add debug prints to ShouldUpdateToolchain

--- engine/src/build/vs_toolchain.py
+++ engine/src/build/vs_toolchain.py
@@ -442,15 +442,21 @@ def _GetDesiredVsToolchainHashes():
 
 def ShouldUpdateToolchain():
   """Check if the toolchain should be upgraded."""
+  import sys
+  sys.stderr.write("DEBUG: json_data_file = %s\n" % json_data_file)
+  sys.stderr.write("DEBUG: ENV DEPOT_TOOLS_WIN_TOOLCHAIN = %s\n" % os.environ.get('DEPOT_TOOLS_WIN_TOOLCHAIN'))
+  sys.stderr.write("DEBUG: ENV GYP_MSVS_VERSION = %s\n" % os.environ.get('GYP_MSVS_VERSION'))
   if not os.path.exists(json_data_file):
+    sys.stderr.write("DEBUG: json_data_file does not exist\n")
     return True
   with open(json_data_file, 'r') as tempf:
     toolchain_data = json.load(tempf)
   version = toolchain_data['version']
   env_version = GetVisualStudioVersion()
-  # If there's a mismatch between the version set in the environment and the one
-  # in the json file then the toolchain should be updated.
-  return version != env_version
+  sys.stderr.write("DEBUG: json version = %s, env version = %s\n" % (version, env_version))
+  result = version != env_version
+  sys.stderr.write("DEBUG: ShouldUpdateToolchain returning %s\n" % result)
+  return result
 
 
 def Update(force=False, no_download=False):
