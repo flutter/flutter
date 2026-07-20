e7e764a50ff645339dc2c8f90f61c6665e92231b
Matej Knopp <matej.knopp@gmail.com>
2026-07-20 18:21:25 +0200

    Undo change.

--- packages/flutter/lib/src/widgets/app.dart
+++ packages/flutter/lib/src/widgets/app.dart
@@ -1733,6 +1733,10 @@ class _WidgetsAppState extends State<WidgetsApp> with WidgetsBindingObserver {
       result = routing!;
     }
 
+    if (isWindowingEnabled) {
+      result = WindowManager(child: result);
+    }
+
     if (widget.textStyle != null) {
       result = DefaultTextStyle(style: widget.textStyle!, child: result);
     }
@@ -1773,10 +1777,6 @@ class _WidgetsAppState extends State<WidgetsApp> with WidgetsBindingObserver {
       }
       return true;
     }());
-
-    if (isWindowingEnabled) {
-      result = WindowManager(child: result);
-    }
 
     // TODO(victorsanni): https://github.com/flutter/flutter/issues/180319
     // Use actions and shortcuts to dismiss tooltips when esc is pressed instead
