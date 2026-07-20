7a19e6349e3d46739543c61cc1211ff59fb3390b
Jason Simmons <jsimmons@google.com>
2026-07-20 21:00:14 +0000

    comment

--- engine/src/flutter/lib/ui/painting/image_encoding_impeller.cc
+++ engine/src/flutter/lib/ui/painting/image_encoding_impeller.cc
@@ -160,8 +160,9 @@ void ImageEncodingImpeller::ConvertDlImageToSkImage(
     return;
   }
 
-  auto dimensions =
-      DlISize(texture->GetSize().width, texture->GetSize().height);
+  // Use the size of the texture allocated by the Impeller back end (which may
+  // differ from the original image size).
+  auto dimensions = DlISize(texture->GetSize());
   auto color_type = ToSkColorType(texture->GetTextureDescriptor().format);
 
   if (dimensions.IsEmpty()) {
