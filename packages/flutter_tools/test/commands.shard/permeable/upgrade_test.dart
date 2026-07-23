f73d7cdbe7f5df6e470fdd4f29722a57b261d20f
Jason Simmons <jason-simmons@users.noreply.github.com>
2026-07-23 15:44:18 +0000

    Disable execution order shuffling for the flutter_tools upgrade_test suite (#189920)
    This test is failing when run with today's randomized order seed.
    
    See https://github.com/flutter/flutter/issues/189919

--- packages/flutter_tools/test/commands.shard/permeable/upgrade_test.dart
+++ packages/flutter_tools/test/commands.shard/permeable/upgrade_test.dart
@@ -2,6 +2,11 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// TODO(team-tools): Remove when the execution order dependency is resolved.
+// See https://github.com/flutter/flutter/issues/189919
+@Tags(<String>['no-shuffle'])
+library;
+
 import 'package:args/command_runner.dart';
 import 'package:file/memory.dart';
 import 'package:flutter_tools/src/base/file_system.dart';
