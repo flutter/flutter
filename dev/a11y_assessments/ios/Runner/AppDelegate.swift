d5b0963d0409b661bd0a78c9f31afe4066be3b6d
louisehsu <louiseh0313@gmail.com>
2026-07-23 17:45:48 -0700

    i hate it when the bot is right

--- dev/a11y_assessments/ios/Runner/AppDelegate.swift
+++ dev/a11y_assessments/ios/Runner/AppDelegate.swift
@@ -6,14 +6,16 @@ import Flutter
 import UIKit
 
 @main
-@objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
+@objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+}
 
+extension AppDelegate: FlutterImplicitEngineDelegate {
   func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngineBridge) {
     GeneratedPluginRegistrant.register(with: engineBridge.pluginRegistry)
   }
