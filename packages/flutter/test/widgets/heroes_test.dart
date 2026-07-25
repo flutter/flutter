ffdab49bf5fd0fd4ba3069260a680746103e5118
MTtankkeo <ttankkeo112@gmail.com>
2026-07-26 08:29:51 +0900

    Fix HeroController leak in resize test

--- packages/flutter/test/widgets/heroes_test.dart
+++ packages/flutter/test/widgets/heroes_test.dart
@@ -3928,6 +3928,9 @@ Future<void> main() async {
       createRectTween: (begin, end) => RectTween(begin: begin, end: end),
     );
 
+    // Dispose the HeroController when the test finishes.
+    addTearDown(heroController.dispose);
+
     final navigatorKey = GlobalKey<NavigatorState>();
     const shuttleKey = ValueKey<String>('hero-shuttle');
 
