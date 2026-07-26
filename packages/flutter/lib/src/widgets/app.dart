96d2356ea87b0217883c6b5b8866c115f0bb9ea1
Matej Knopp <matej.knopp@gmail.com>
2026-07-26 17:46:40 +0200

    Rebase

--- packages/flutter/lib/src/widgets/app.dart
+++ packages/flutter/lib/src/widgets/app.dart
@@ -1732,10 +1732,6 @@ class _WidgetsAppState extends State<WidgetsApp> with WidgetsBindingObserver {
       result = routing!;
     }
 
-    if (isWindowingEnabled) {
-      result = WindowManager(child: result);
-    }
-
     if (widget.textStyle != null) {
       result = DefaultTextStyle(style: widget.textStyle!, child: result);
     }
