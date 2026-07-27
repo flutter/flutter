492ff40aa884893f205492632f4e516248ab9f5e
Ben Konyi <bkonyi@google.com>
2026-07-27 22:01:18 +0000

    [flutter_tools] Fix PathNotFoundException during native assets cleanup
    Avoid crashing with a PathNotFoundException if a file is deleted by the OS or another process after being listed but before being deleted by the tool.
    
    Use ErrorHandlingFileSystem.deleteIfExists instead of entity.delete.

--- packages/flutter_tools/lib/src/isolated/native_assets/native_assets.dart
+++ packages/flutter_tools/lib/src/isolated/native_assets/native_assets.dart
@@ -12,6 +12,7 @@ import 'package:logging/logging.dart' as logging;
 import 'package:package_config/package_config_types.dart';
 
 import '../../base/common.dart';
+import '../../base/error_handling_io.dart';
 import '../../base/file_system.dart';
 import '../../base/logger.dart';
 import '../../base/platform.dart';
@@ -831,7 +832,7 @@ Future<List<File>> _copyNativeCodeAssetsForOS(
     targetDir.createSync(recursive: true);
   }
   await for (final FileSystemEntity entity in targetDir.list()) {
-    await entity.delete(recursive: true);
+    ErrorHandlingFileSystem.deleteIfExists(entity, recursive: true);
   }
 
   if (assetTargetLocations.isEmpty) {
