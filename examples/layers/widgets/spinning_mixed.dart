4f8a51575e0f63c2239b9623de8129c502f3daea
Justin McCandless <jmccandless@google.com>
2026-07-29 13:33:40 -0700

    Analyzer

--- examples/layers/widgets/spinning_mixed.dart
+++ examples/layers/widgets/spinning_mixed.dart
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:material_ui/material_ui.dart';
 import 'package:flutter/rendering.dart';
+import 'package:material_ui/material_ui.dart';
 
 import '../rendering/src/solid_color_box.dart';
 
