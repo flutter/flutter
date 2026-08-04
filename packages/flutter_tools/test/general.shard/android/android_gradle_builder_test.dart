e88c09fe01540860ba2c9ed7c12d3781811a7aff
Camille Simon <camillesimon@google.com>
2026-08-04 09:22:30 -0700

    hopefully fix stack overflow

--- packages/flutter_tools/test/general.shard/android/android_gradle_builder_test.dart
+++ packages/flutter_tools/test/general.shard/android/android_gradle_builder_test.dart
@@ -32,6 +32,7 @@ import 'package:unified_analytics/unified_analytics.dart';
 
 import '../../src/common.dart';
 import '../../src/context.dart';
+import '../../src/context.dart' as test_context show testUsingContext;
 import '../../src/fake_process_manager.dart';
 import '../../src/fakes.dart';
 
@@ -84,7 +85,7 @@ void main() {
       dynamic Function() body, {
       Map<Type, Generator> overrides = const <Type, Generator>{},
     }) {
-      testUsingContext(
+      test_context.testUsingContext(
         description,
         body,
         overrides: <Type, Generator>{
@@ -3227,7 +3228,7 @@ Gradle Crashed
                 'The Java version used for the build is 21.0.0, which is incompatible with Gradle 8.0.\n'
                 'To fix this, you can either:\n'
                 "  1. Upgrade your project's Gradle version (typically in gradle-wrapper.properties to a version matching the range: compatible Gradle versions for Java 21.0.0 are 8.4 or newer).\n"
-                '  2. Use a different Java version for Flutter by running `flutter config --jdk-dir=<path>`.'
+                '  2. Use a different Java version for Flutter by running `flutter config --jdk-dir=<path>`.',
           ),
         );
         expect(processManager, hasNoRemainingExpectations);
