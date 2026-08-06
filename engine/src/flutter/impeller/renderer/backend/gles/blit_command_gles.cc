eba2c5ef91978575d2b10f1c650daf37e939550c
Brandon DeRosier <x@bdero.me>
2026-08-05 17:20:47 -0700

    Address review: DCHECK array layer count, TODO for Dart surfacing

--- engine/src/flutter/impeller/renderer/backend/gles/blit_command_gles.cc
+++ engine/src/flutter/impeller/renderer/backend/gles/blit_command_gles.cc
@@ -182,6 +182,9 @@ bool BlitCopyBufferToTextureCommandGLES::Encode(
       texture_target = GL_TEXTURE_CUBE_MAP_POSITIVE_X + slice;
       break;
     case TextureType::kTexture2DArray:
+      // TODO(bdero): Surface array-texture support up to the Flutter GPU Dart
+      // API so callers get a clear error at texture creation instead of a
+      // failed upload on contexts without array support.
       if (!reactor.GetProcTable().GetCapabilities()->SupportsTextureArrays()) {
         VALIDATION_LOG
             << "2D array textures are not supported on this context.";
