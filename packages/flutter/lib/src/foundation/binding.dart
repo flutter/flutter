8ced64091bd778ff67538bd8f28a88092a0d4881
Chun-Heng Tai <chtai@google.com>
2026-08-05 19:13:06 -0700

    update

--- packages/flutter/lib/src/foundation/binding.dart
+++ packages/flutter/lib/src/foundation/binding.dart
@@ -290,22 +290,12 @@ abstract class BindingBase {
       _debugBindingZone = Zone.current;
       return true;
     }());
-    _setupListenableHooks();
+    _initListenable();
   }
 
-  void _setupListenableHooks() {
+  void _initListenable() {
     Listenable.onError = (Object error, StackTrace? stackTrace, ErrorContext context) {
       switch (context) {
-        case ErrorContext.assertion:
-          if (error is FlutterError) {
-            throw error;
-          }
-          final String message = switch (error) {
-            StateError(message: final String msg) => msg,
-            AssertionError(message: final Object? msg) => msg?.toString() ?? error.toString(),
-            _ => error.toString(),
-          };
-          throw FlutterError(message);
         case ErrorContext.listener:
           FlutterError.reportError(
             FlutterErrorDetails(
@@ -315,6 +305,16 @@ abstract class BindingBase {
               context: ErrorDescription('while dispatching notifications for $Listenable'),
             ),
           );
+        default:
+          if (error is FlutterError) {
+            throw error;
+          }
+          final String message = switch (error) {
+            StateError(message: final String msg) => msg,
+            AssertionError(message: final Object? msg) => msg?.toString() ?? error.toString(),
+            _ => error.toString(),
+          };
+          throw FlutterError(message);
       }
     };
   }
