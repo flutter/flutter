33251d93150e26a4d6e95d523283eb5177477c1b
Benson Luk <97480502+b-luk@users.noreply.github.com>
2026-08-06 12:15:07 -0700

    Add text rendering test to engine_integration_golden_test

--- dev/integration_tests/engine_integration_golden_test/integration_test/engine_integration_golden_test.dart
+++ dev/integration_tests/engine_integration_golden_test/integration_test/engine_integration_golden_test.dart
@@ -2,7 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:engine_integration_golden_test/primitive_shape_main.dart' as primitive_shape;
+import 'package:engine_integration_golden_test/primitive_shape_main.dart'
+    as primitive_shape;
+import 'package:engine_integration_golden_test/text_main.dart' as text_main;
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
@@ -17,6 +19,18 @@ void main() {
     await expectLater(
       find.byKey(const Key('primitive_shape_canvas')),
       matchesGoldenFile('primitive_shape_canvas_snapshot.png'),
+    );
+  });
+
+  testWidgets('renders text', (
+    WidgetTester tester,
+  ) async {
+    await tester.pumpWidget(const text_main.TextRenderingApp());
+    await tester.pumpAndSettle();
+
+    await expectLater(
+      find.byKey(const Key('text_rendering_canvas')),
+      matchesGoldenFile('text_rendering_canvas_snapshot.png'),
     );
   });
 }
