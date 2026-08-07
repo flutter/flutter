9f48e447dbd91bc961b0768a7865ead974b884aa
Justin McCandless <jmccandless@google.com>
2026-08-07 09:40:35 -0700

    Migrate example image_list to material_ui (#190363)
    Migrates the image_list example to material_ui.
    
    Part of https://github.com/flutter/flutter/issues/190093.

--- dev/bots/check_examples_cross_imports.dart
+++ dev/bots/check_examples_cross_imports.dart
@@ -569,7 +569,6 @@ class ExamplesCrossImportChecker {
     'examples/api/test/widgets/inherited_notifier/inherited_notifier.0_test.dart',
     'examples/api/test/widgets/text_magnifier/text_magnifier.0_test.dart',
     'examples/flutter_view/lib/main.dart',
-    'examples/image_list/lib/main.dart',
     'examples/multiple_windows/lib/app/main_window.dart',
     'examples/multiple_windows/lib/app/tooltip_button.dart',
     'examples/multiple_windows/lib/app/tooltip_window_edit_dialog.dart',
