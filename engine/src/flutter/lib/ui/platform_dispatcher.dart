e3db8a18e14aa95d85d815d139554e0f26ed31e3
Matej Knopp <matej.knopp@gmail.com>
2026-08-12 21:25:16 +0200

    Reformat.

--- engine/src/flutter/lib/ui/platform_dispatcher.dart
+++ engine/src/flutter/lib/ui/platform_dispatcher.dart
@@ -67,11 +67,8 @@ typedef MarkAllViewsNeedRenderCallback = void Function();
   'Migrate to ChannelBuffers.setListener instead. '
   'This feature was deprecated after v3.11.0-20.0.pre.',
 )
-typedef PlatformMessageCallback = void Function(
-  String name,
-  ByteData? data,
-  PlatformMessageResponseCallback? callback,
-);
+typedef PlatformMessageCallback =
+    void Function(String name, ByteData? data, PlatformMessageResponseCallback? callback);
 
 // Signature for _setNeedsReportTimings.
 typedef _SetNeedsReportTimingsFunc = void Function(bool value);
