7a7687567a44b69db9c8d8a910a3259e44f918b4
Ben Konyi <bkonyi@google.com>
2026-08-13 02:54:27 +0000

    Fix directives_ordering lint in main.dart

--- dev/flutter_analyzer_plugin/lib/main.dart
+++ dev/flutter_analyzer_plugin/lib/main.dart
@@ -5,12 +5,12 @@
 import 'package:analysis_server_plugin/plugin.dart';
 import 'package:analysis_server_plugin/registry.dart';
 import 'src/rules/avoid_future_catch_error.dart';
+import 'src/rules/deprecation_syntax.dart';
 import 'src/rules/no_double_clamp.dart';
 import 'src/rules/no_stopwatches.dart';
+import 'src/rules/no_sync_async_star.dart';
 import 'src/rules/protect_public_state_subtypes.dart';
 import 'src/rules/render_box_intrinsics.dart';
-import 'src/rules/deprecation_syntax.dart';
-import 'src/rules/no_sync_async_star.dart';
 
 final FlutterAnalyzerPlugin plugin = FlutterAnalyzerPlugin();
 
