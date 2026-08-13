d7cbd53d94c4aee63a96a522f81d62a8c02f170b
piedcipher <pateltirth454@gmail.com>
2026-08-13 21:53:16 +0530

    Add YouTube link to Carousel widget documentation

--- packages/flutter/lib/src/material/carousel.dart
+++ packages/flutter/lib/src/material/carousel.dart
@@ -24,6 +24,8 @@ import 'theme.dart';
 
 /// A Material Design carousel widget.
 ///
+/// {@youtube 560 315 https://www.youtube.com/watch?v=GQ8ajYVF0bo}
+///
 /// The [CarouselView] presents a scrollable list of items, each of which can dynamically
 /// change size based on the chosen layout.
 ///
