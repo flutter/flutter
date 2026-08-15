bd086a53186a93d73960f54aefbfe294aba61697
LongCatIsLooong <31859944+LongCatIsLooong@users.noreply.github.com>
2026-08-15 02:20:22 -0700

    compiles

--- engine/src/flutter/shell/platform/darwin/macos/framework/Source/FlutterRunLoop.swift
+++ engine/src/flutter/shell/platform/darwin/macos/framework/Source/FlutterRunLoop.swift
@@ -4,105 +4,50 @@
 
 import Foundation
 
-/// Interface for scheduling tasks on the main thread run loop.
+/// Scheduling tasks on the main thread run loop, potentially with a delay.
+///
+/// This implementation does not guarantee the order of execution between tasks.
+/// In other words, a task may run before another task with an earlier fire date.
 ///
 /// The main difference between using `FlutterRunLoop` to schedule tasks compared
 /// to `DispatchQueue.async` or `RunLoop.perform(_:)` is that `FlutterRunLoop`
 /// schedules the task in both common run loop mode and a private run loop mode,
 /// which allows it to run in a mode where it only processes Flutter messages
 /// (`pollFlutterMessagesOnce()`).
-@objc final class FlutterRunLoop: NSObject, @unchecked Sendable {
-  private static let flutterRunLoopMode = CFRunLoopMode("FlutterRunLoopMode" as CFString)
+@objc final class FlutterRunLoop: NSObject, Sendable {
+  private static let flutterRunLoopMode = RunLoop.Mode("FlutterRunLoopMode")
+  private static let flutterCFRunLoopMode = flutterRunLoopMode.cfRunLoopMode
 
   @objc @MainActor
   static let mainRunLoop = FlutterRunLoop()
-
-  private let runLoop: CFRunLoop = CFRunLoopGetCurrent()
-  private let taskQueue = TaskQueue()
-  // This keeps the SourceContextInfo alive.
-  private let sourceContextInfo: SourceContextInfo
-  private let source: CFRunLoopSource
-  private let timer: CFRunLoopTimer
+  private let lockScope = LockScope()
+  private let runLoop = SendableCFRunLoop()
 
   @MainActor
   private override init() {
-    var timerContext = CFRunLoopTimerContext(
-      version: 0,
-      info: Unmanaged.passUnretained(taskQueue).toOpaque(),
-      retain: nil,
-      release: nil,
-      copyDescription: nil
-    )
-
-    guard
-      let createdTimer = CFRunLoopTimerCreate(
-        kCFAllocatorDefault,
-        CFAbsoluteTime.greatestFiniteMagnitude,  // fireDate.
-        CFAbsoluteTime.greatestFiniteMagnitude,  // interval.
-        0,  // flags.
-        0,  // order.
-        { timer, info in
-          let taskQueue = Unmanaged<TaskQueue>.fromOpaque(info!).takeUnretainedValue()
-          taskQueue.runExpiredTasksAndRearm(timer: timer!)
-        },
-        &timerContext
-      )
-    else {
-      fatalError("Failed to create CFRunLoopTimer")
-    }
-    self.timer = createdTimer
-    self.sourceContextInfo = SourceContextInfo(taskQueue: taskQueue, timer: timer)
-
-    var sourceContext = CFRunLoopSourceContext(
-      version: 0,
-      info: Unmanaged.passUnretained(sourceContextInfo).toOpaque(),
-      retain: nil,
-      release: nil,
-      copyDescription: nil,
-      equal: nil,
-      hash: nil,
-      schedule: nil,
-      cancel: nil,
-      perform: { info in
-        let info = Unmanaged<SourceContextInfo>.fromOpaque(info!).takeUnretainedValue()
-        info.taskQueue.runExpiredTasksAndRearm(timer: info.timer)
-      }
-    )
-    guard let createdSource = CFRunLoopSourceCreate(kCFAllocatorDefault, 0, &sourceContext) else {
-      fatalError("Failed to create CFRunLoopSource")
-    }
-    self.source = createdSource
     super.init()
-
-    CFRunLoopAddSource(runLoop, source, .commonModes)
-    CFRunLoopAddSource(runLoop, source, Self.flutterRunLoopMode)
-
-    CFRunLoopAddTimer(runLoop, timer, .commonModes)
-    CFRunLoopAddTimer(runLoop, timer, Self.flutterRunLoopMode)
+    runLoop.add(timer: lockScope.timer, forModes: [.common, Self.flutterRunLoopMode])
   }
 
-  deinit {
-    CFRunLoopTimerInvalidate(timer)
-    CFRunLoopRemoveTimer(runLoop, timer, .commonModes)
-    CFRunLoopRemoveTimer(runLoop, timer, Self.flutterRunLoopMode)
-    CFRunLoopSourceInvalidate(source)
-    CFRunLoopRemoveSource(runLoop, source, .commonModes)
-    CFRunLoopRemoveSource(runLoop, source, Self.flutterRunLoopMode)
-  }
-
-  // Schedules a block to be executed on the main thread.
+  /// Schedules a block to be executed on the main thread after the given delay.
   @objc func perform(afterDelay delay: TimeInterval, block: @MainActor @escaping () -> Void) {
-    let nextFireTime = taskQueue.add(
-      task: TaskQueue.Task(block: block, targetTime: CFAbsoluteTimeGetCurrent() + delay))
     if delay > 0 {
-      CFRunLoopTimerSetNextFireDate(timer, nextFireTime)
+      let task = LockScope.Task(block: block, targetDate: .now + delay)
+      lockScope.addTaskAndRearm(task)
     } else {
-      CFRunLoopSourceSignal(source)
-      CFRunLoopWakeUp(runLoop)
+      runLoop.perform(inModes: [.common, Self.flutterRunLoopMode]) { [self] in
+        MainActor.assumeIsolated {
+          for task in lockScope.popExpiredTasksAndRearm() {
+            task.block()
+          }
+          // This class does not guarantee the execution order of expired tasks.
+          block()
+        }
+      }
     }
   }
 
-  // Schedules a block to be executed on the main thread after a delay.
+  /// Schedules a block to be executed on the main thread.
   @objc(performBlock:)
   func perform(_ block: @MainActor @escaping () -> Void) {
     perform(afterDelay: 0, block: block)
@@ -110,77 +55,135 @@ import Foundation
 
   /// Executes single iteration of the run loop in the mode where only Flutter
   /// messages are processed.
+  ///
+  /// Must be called on the main thread.
+  @MainActor
   @objc func pollFlutterMessagesOnce() {
-    CFRunLoopRunInMode(Self.flutterRunLoopMode, 0.1, true)
+    CFRunLoopRunInMode(Self.flutterCFRunLoopMode, 0.1, true)
   }
 }
 
-// The internal interface of this TaskQueue is Sendable because
-// all mutable variables are protected by a NSLock.
-private final class TaskQueue: @unchecked Sendable {
+/// A Sendable class that synchronizes non-thead-safe calls using a NSLock.
+private final class LockScope: @unchecked Sendable {
   private let lock = NSLock()
+  private let unsafeTaskQueue = UnsafeTaskQueue()
+  let timer: Timer
 
-  struct Task {
-    let block: @MainActor () -> Void
-    let targetTime: CFAbsoluteTime
-  }
-
-  // (target time of the first task to expire, unsorted task queue)
-  private var tasks: [Task] = []
-  private var earliestDeadline = CFAbsoluteTime.greatestFiniteMagnitude
-
-  func add(task: Task) -> CFAbsoluteTime {
-    lock.withLock {
-      tasks.append(task)
-      earliestDeadline = min(earliestDeadline, task.targetTime)
-      return earliestDeadline
+  init() {
+    self.timer = Timer(
+      fire: .distantFuture,
+      interval: .greatestFiniteMagnitude,
+      repeats: false
+    ) { [lock, unsafeTaskQueue] timer in
+      MainActor.assumeIsolated {
+        for task in LockScope.popExpiredTasksAndRearm(
+          lock: lock, unsafeTaskQueue: unsafeTaskQueue, timer: timer)
+        {
+          task.block()
+        }
+      }
     }
   }
 
-  // Returns a tuple representing the new earliest deadline
-  // and an unordered list of expired tasks.
-  private func popTasks(expiringBy time: CFAbsoluteTime) -> (CFAbsoluteTime, [Task]) {
+  /// Adds the given task to the task queue and updates the Timer's fire date
+  /// accordingly.
+  func addTaskAndRearm(_ task: Task) {
     lock.withLock {
-      guard time >= earliestDeadline else {
-        return (earliestDeadline, [])
+      timer.fireDate = unsafeTaskQueue.add(task: task)
+    }
+  }
+
+  private static func popExpiredTasksAndRearm(
+    lock: NSLock, unsafeTaskQueue: UnsafeTaskQueue, timer: Timer
+  ) -> [Task] {
+    lock.withLock {
+      let now = Date.now
+      let (tasks, newFireDate) = unsafeTaskQueue.popTasks(expiringBy: now)
+      timer.fireDate = newFireDate
+      return tasks
+    }
+  }
+
+  /// Pops all expired task from the task queue and updates the Timer's fire date.
+  @MainActor
+  func popExpiredTasksAndRearm() -> [Task] {
+    Self.popExpiredTasksAndRearm(lock: lock, unsafeTaskQueue: unsafeTaskQueue, timer: timer)
+  }
+}
+
+extension LockScope {
+  struct Task {
+    let block: @MainActor () -> Void
+    let targetDate: Date
+  }
+
+  final class UnsafeTaskQueue {
+    // Unsorted task queue.
+    private var tasks: [Task] = []
+    private var earliestDeadline: Date = .distantFuture
+
+    func add(task: Task) -> Date {
+      tasks.append(task)
+      earliestDeadline = min(earliestDeadline, task.targetDate)
+      return earliestDeadline
+    }
+
+    // Returns an unordered list of expired tasks, and the new earliest deadline.
+    func popTasks(
+      expiringBy date: Date,
+    ) -> ([Task], Date) {
+      guard date >= earliestDeadline else {
+        return ([], earliestDeadline)
       }
 
       var newQueue: [Task] = []
-      var newEarliestDeadline = CFAbsoluteTime.greatestFiniteMagnitude
+      var newEarliestDeadline = Date.distantFuture
       var expiredTasks: [Task] = []
 
       for task in tasks {
-        if task.targetTime <= time {
+        if task.targetDate <= date {
           expiredTasks.append(task)
         } else {
           newQueue.append(task)
-          newEarliestDeadline = min(newEarliestDeadline, task.targetTime)
+          newEarliestDeadline = min(newEarliestDeadline, task.targetDate)
         }
       }
 
       earliestDeadline = newEarliestDeadline
       tasks = newQueue
-      return (newEarliestDeadline, expiredTasks)
-    }
-  }
-
-  @MainActor
-  func runExpiredTasksAndRearm(timer: CFRunLoopTimer) {
-    let (nextFireDate, expiredTasks) = popTasks(expiringBy: CFAbsoluteTimeGetCurrent())
-
-    CFRunLoopTimerSetNextFireDate(timer, nextFireDate)
-    for task in expiredTasks {
-      task.block()
+      return (expiredTasks, earliestDeadline)
     }
   }
 }
 
-private final class SourceContextInfo {
-  init(taskQueue: TaskQueue, timer: CFRunLoopTimer) {
-    self.taskQueue = taskQueue
-    self.timer = timer
+// Unlike NSRunLoop, CFRunLoop APIs are "generally" thread-safe, but
+// "If you are performing operations that alter the configuration of the run loop,
+// however, it is still good practice to do so from the thread that owns the run loop
+// whenever possible."
+private final class SendableCFRunLoop: @unchecked Sendable {
+  private let runLoop: CFRunLoop = CFRunLoopGetCurrent()
+
+  // Calls CFRunLoopPerformBlock on the run loop and then wake up the run loop.
+  func perform(inModes modes: [RunLoop.Mode], block: @escaping @Sendable () -> Void) {
+    let cfModes = modes.map { $0.cfRunLoopMode } as NSArray
+    CFRunLoopPerformBlock(runLoop, cfModes, block)
+    CFRunLoopWakeUp(runLoop)
   }
 
-  let taskQueue: TaskQueue
-  let timer: CFRunLoopTimer
+  func add(timer: Timer, forModes modes: [RunLoop.Mode]) {
+    for mode in modes {
+      CFRunLoopAddTimer(runLoop, timer, mode.cfRunLoopMode)
+    }
+  }
+
+}
+
+extension RunLoop.Mode {
+  fileprivate var cfRunLoopMode: CFRunLoopMode {
+    switch self {
+    case .default: .defaultMode
+    case .common: .commonModes
+    case let mode: CFRunLoopMode(mode.rawValue as CFString)
+    }
+  }
 }
