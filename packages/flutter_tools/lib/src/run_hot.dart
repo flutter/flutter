7811534f038c01ebbd9c3eae5544655958ce12d1
Ben Konyi <bkonyi@google.com>
2026-08-18 15:51:14 +0000

    [tool] Use local function for file invalidation check in findInvalidated

--- packages/flutter_tools/lib/src/run_hot.dart
+++ packages/flutter_tools/lib/src/run_hot.dart
@@ -1487,15 +1487,18 @@ class ProjectFileInvalidator {
     ];
     final invalidatedFiles = <Uri>[];
 
-    final bool Function(DateTime) isInvalidated;
-    if (_platform.isWindows) {
-      final lastCompiledTruncated = DateTime.fromMillisecondsSinceEpoch(
-        (lastCompiled.millisecondsSinceEpoch ~/ 1000) * 1000,
-        isUtc: lastCompiled.isUtc,
-      );
-      isInvalidated = (DateTime updatedAt) => !updatedAt.isBefore(lastCompiledTruncated);
-    } else {
-      isInvalidated = (DateTime updatedAt) => updatedAt.isAfter(lastCompiled);
+    final bool isWindows = _platform.isWindows;
+    final DateTime? lastCompiledTruncated = isWindows
+        ? DateTime.fromMillisecondsSinceEpoch(
+            (lastCompiled.millisecondsSinceEpoch ~/ 1000) * 1000,
+            isUtc: lastCompiled.isUtc,
+          )
+        : null;
+    bool isInvalidated(DateTime updatedAt) {
+      if (isWindows) {
+        return !updatedAt.isBefore(lastCompiledTruncated!);
+      }
+      return updatedAt.isAfter(lastCompiled);
     }
 
     if (asyncScanning) {
