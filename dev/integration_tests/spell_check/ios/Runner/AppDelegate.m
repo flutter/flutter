928ae7e8c0704a11264732c33858653ccdfb3777
Elijah Okoroh <okorohelijah@google.com>
2026-08-18 21:31:54 -0700

    fix

--- dev/integration_tests/spell_check/ios/Runner/AppDelegate.m
+++ dev/integration_tests/spell_check/ios/Runner/AppDelegate.m
@@ -12,7 +12,7 @@
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
-- (void)didInitializeImplicitFlutterEngine:(NSObject<FlutterImplicitEngineBridge>*)engineBridge {
+- (void)didInitializeImplicitFlutterEngine:(NSObject<FlutterImplicitEngineBridge> *)engineBridge {
   [GeneratedPluginRegistrant registerWithRegistry:engineBridge.pluginRegistry];
 }
 
