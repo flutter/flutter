flutter/flutter

commit 7bff211f1c1f7b99f010d76bfd5b0d9dca808707

[Android] Inject command line flags into manifest in release mode when not running pre-built binary (#190222)
When a Flutter Android app is run in release mode WITHOUT a prebuilt
binary, send the flags to the embedding via manifest injection instead
of `Intent` extras.

Updated a pre-existing integration test to run in release mode to prove
that passing flags via the command line still works as expected in
release mode. Successful run:
https://ci.chromium.org/ui/p/flutter/builders/try/Linux_android_emu%20android_engine_flags_release_test/5238/overview

Part of https://github.com/flutter/flutter/issues/190461.

## Pre-launch Checklist

- [x] I read the [Contributor Guide] and followed the process outlined
there for submitting PRs.
- [x] I read the [AI contribution guidelines] and understand my
responsibilities, or I am not using AI tools.
- [x] I read the [Tree Hygiene] wiki page, which explains my
responsibilities.
- [x] I read and followed the [Flutter Style Guide], including [Features
we expect every widget to implement].
- [x] I signed the [CLA].
- [x] I listed at least one issue that this PR fixes in the description
above.
- [x] I updated/added relevant documentation (doc comments with `///`).
- [x] I added new tests to check the change I am making, or this PR is
[test-exempt].
- [x] I followed the [breaking change policy] and added [Data Driven
Fixes] where supported.
- [ ] All existing and new tests are passing.

If you need help, consider asking for advice on the #hackers-new channel
on [Discord].

If this change needs to override an active code freeze, provide a
comment explaining why. The code freeze workflow can be overridden by
code reviewers. See pinned issues for any active code freezes with
guidance.

**Note**: The Flutter team is currently trialing the use of [Gemini Code
Assist for
GitHub](https://developers.google.com/gemini-code-assist/docs/review-github-code).
Comments from the `gemini-code-assist` bot should not be taken as

engine/src/flutter/shell/platform/android/io/flutter/embedding/engine/FlutterEngineConnectionRegistry.java
@@ -14,6 +14,7 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.lifecycle.Lifecycle;
+import io.flutter.BuildConfig;
 import io.flutter.Log;
 import io.flutter.embedding.android.ExclusiveAppComponent;
 import io.flutter.embedding.engine.loader.FlutterLoader;
@@ -342,10 +343,10 @@ import java.util.Set;
     // developers about the new method for doing so if this was attempted.
     // TODO(camsim99): Remove this warning after a stable release has passed:
     // https://github.com/flutter/flutter/issues/179274.
-    if (useSoftwareRendering) {
+    if (useSoftwareRendering && BuildConfig.RELEASE) {
       Log.i(
           TAG,
-          "If you are attempting to set --enable-software-rendering via Intent extras to launch a Flutter component outside of using the Flutter CLI, note that support for setting engine flags on Android via Intent will soon be dropped; see https://github.com/flutter/flutter/issues/172553 for more information on this breaking change. To migrate, set the "
+          "If you are attempting to set --enable-software-rendering via Intent extras to launch a Flutter component outside of using the Flutter CLI, note that support for setting engine flags on Android via Intents in release mode will soon be dropped; see https://github.com/flutter/flutter/issues/172553 for more information on this breaking change. To migrate, set the "
               + FlutterEngineFlags.ENABLE_SOFTWARE_RENDERING.metadataKey
               + " metadata in the application manifest. See https://github.com/flutter/flutter/blob/main/docs/engine/Flutter-Android-Engine-Flags.md for more info.");
     } else {

dev/devicelab/lib/tasks/android_engine_flags_test.dart
@@ -19,8 +19,8 @@ TaskFunction androidEngineFlagsTest(String buildMode) {
   final isReleaseMode = buildMode == 'release';
   final List<TaskFunction> tests = [
     _testInvalidFlag(buildMode),
+    _testCommandLineFlagPrecedence(buildMode),
     if (isReleaseMode) _testIllegalFlagInReleaseMode(),
-    if (!isReleaseMode) _testCommandLineFlagPrecedence(),
   ];
 
   return () async {
@@ -186,7 +186,7 @@ TaskFunction _testIllegalFlagInReleaseMode() {
   };
 }
 
-TaskFunction _testCommandLineFlagPrecedence() {
+TaskFunction _testCommandLineFlagPrecedence(String buildMode) {
   return () async {
     final Directory tempDir = Directory.systemTemp.createTempSync(
       'android_flutter_shell_args_test.',
@@ -207,7 +207,10 @@ TaskFunction _testCommandLineFlagPrecedence() {
       late Process run;
 
       await inDirectory(testProjectPath, () async {
-        run = await startFlutter('run', options: <String>['--test-flag', '--verbose']);
+        run = await startFlutter(
+          'run',
+          options: <String>['--$buildMode', '--test-flag', '--verbose'],
+        );
       });
 
       final StreamSubscription<void> stdoutSubscription = run.stdout

engine/src/flutter/shell/platform/android/io/flutter/embedding/engine/FlutterEngineFlags.java
@@ -240,6 +240,13 @@ public final class FlutterEngineFlags {
   private static final Flag IMPELLER_LAZY_SHADER_MODER =
       new Flag("--impeller-lazy-shader-mode", "ImpellerLazyShaderInitialization", true);
 
+  /**
+   * Fake flag used for integration testing of the Android embedding processing engine flags.
+   *
+   * <p>Settable via the command line and the manifest.
+   */
+  @VisibleForTesting public static final Flag TEST_FLAG = new Flag("--test-flag", "TestFlag", true);
+
   // Manifest flags NOT allowed in release mode:
 
   /**
@@ -295,13 +302,6 @@ public final class FlutterEngineFlags {
    */
   private static final Flag ENABLE_VULKAN_VALIDATION =
       new Flag("--enable-vulkan-validation", "EnableVulkanValidation");
-
-  /**
-   * Fake flag used for integration testing of the Android embedding processing engine flags.
-   *
-   * <p>Only settable via the manifest.
-   */
-  @VisibleForTesting public static final Flag TEST_FLAG = new Flag("--test-flag", "TestFlag");
 
   /**
    * Set whether leave or clean up the VM after the last shell shuts down. It can be set from app's

engine/src/flutter/shell/platform/android/io/flutter/embedding/engine/loader/FlutterLoader.java
@@ -34,6 +34,8 @@ import java.util.*;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;
+import org.json.JSONArray;
+import org.json.JSONException;
 
 /** Finds Flutter resources in an application APK and also loads Flutter's native library. */
 public class FlutterLoader {
@@ -48,6 +50,16 @@ public class FlutterLoader {
   private static final String DEFAULT_LIBRARY = "libflutter.so";
   private static final String DEFAULT_KERNEL_BLOB = "kernel_blob.bin";
   private static final String VMSERVICE_SNAPSHOT_LIBRARY = "libvmservice_snapshot.so";
+
+  // The manifest metadata key for engine flags specified via the command line
+  // that are injected into the application merged manifest to be loaded here in the
+  // Flutter Android embedding.
+  //
+  // The key is set in the custom Gradle task found in
+  // packages/flutter_tools/gradle/src/main/kotlin/tasks/GenerateEngineFlagsManifestTask.kt.
+  // The command line flags set there are later loaded by ensureInitializationComplete.
+  private static final String ANDROID_ENGINE_SHELL_ARGS_KEY =
+      "io.flutter.app.androidEngineShellArgs";
 
   private static FlutterLoader instance;
 
@@ -400,6 +412,56 @@ public class FlutterLoader {
           // false (disabled) to ensure flags are only enabled when explicitly requested.
           if (applicationMetaData.getBoolean(metadataKey, false)) {
             shellArgs.add(arg);
+          }
+        }
+
+        // Add engine flags specified by the command line that have been injected into
+        // the manifest. These settings will take precedent over any flag configurations
+        // specified by appplication manifest metadata.
+        if (isRelease) {
+          String androidEngineShellArgsValue =
+              applicationMetaData.getString(ANDROID_ENGINE_SHELL_ARGS_KEY);
+          if (androidEngineShellArgsValue != null && !androidEngineShellArgsValue.isEmpty()) {
+            try {
+              JSONArray shellArgsJson = new JSONArray(androidEngineShellArgsValue);
+              for (int i = 0; i < shellArgsJson.length(); i++) {
+                String arg = shellArgsJson.getString(i);
+
+                FlutterEngineFlags.Flag flag = FlutterEngineFlags.getFlagByEngineArgument(arg);
+                if (flag == null) {
+                  // TODO(camsim99): Reject unknown flags specified on the command line:
+                  // https://github.com/flutter/flutter/issues/182557.
+                  shellArgs.add(arg);
+                  continue;
+                } else if (flag.equals(FlutterEngineFlags.TEST_FLAG)) {
+                  Log.w(
+                      TAG,
+                      "For testing purposes only: test flag specified on the command line was loaded by the FlutterLoader.");
+                  continue;
+                } else if (!flag.allowedInRelease) {
+                  Log.e(
+                      TAG,
+                      "Flag " + arg + " is not allowed in release builds and will be ignored.");
+                  continue;
+                } else if (flag.equals(FlutterEngineFlags.AOT_SHARED_LIBRARY_NAME)
+                    || flag.equals(FlutterEngineFlags.DEPRECATED_AOT_SHARED_LIBRARY_NAME)) {
+                  // Perform security check for path containing application's compiled Dart
+                  // code and potentially user-provided compiled native code.
+                  String aotSharedLibraryPath = arg.substring(flag.engineArgument.length());
+                  maybeAddAotSharedLibraryNameArg(
+                      applicationContext, aotSharedLibraryPath, shellArgs);
+                  continue;
+                }
+                shellArgs.add(arg);
+              }
+            } catch (JSONException j) {
+              Log.e(
+                  TAG,
+                  "Exception parsing shell arguments "
+                      + androidEngineShellArgsValue
+                      + " from manifest: "
+                      + j);
+            }
           }
         }
       }

engine/src/flutter/shell/platform/android/test/io/flutter/embedding/engine/loader/FlutterLoaderTest.java
@@ -18,6 +18,7 @@ import static org.mockito.Mockito.clearInvocations;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.eq;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -1350,6 +1351,157 @@ public class FlutterLoaderTest {
             + expectedArg
             + "' was found in the arguments passed to FlutterJNI.init",
         arguments.contains(expectedArg));
+  }
+
+  @Test
+  public void itSetsSingleCommandLineFlagFromManifestMetadataInReleaseMode() {
+    testMultipleFlagsFromManifestMetadata(
+        "[\"--enable-impeller=true\"]", new String[] {"--enable-impeller=true"}, true, true);
+  }
+
+  @Test
+  public void itSetsMultipleCommandLineFlagsFromManifestMetadataInReleaseMode() {
+    testMultipleFlagsFromManifestMetadata(
+        "[\"--enable-impeller=true\",\"--enable-dart-profiling\"]",
+        new String[] {"--enable-impeller=true", "--enable-dart-profiling"},
+        true,
+        true);
+  }
+
+  @Test
+  public void
+      itSetsMultipleCommandLineFlagsWithSpecialCharactersFromManifestMetadataInReleaseMode() {
+    testMultipleFlagsFromManifestMetadata(
+        "[\"--flutter-assets-dir=\\\"path/<to>/'a'& file\\\"\",\"--enable-impeller=true\"]",
+        new String[] {"--flutter-assets-dir=\"path/<to>/'a'& file\"", "--enable-impeller=true"},
+        true,
+        true);
+  }
+
+  @Test
+  public void itIgnoresCommandLineFlagsFromManifestMetadataInDebugMode() {
+    testMultipleFlagsFromManifestMetadata(
+        "[\"--enable-impeller=true\"]", new String[] {"--enable-impeller=true"}, false, false);
+  }
+
+  @Test
+  public void itDoesNotReadAndroidEngineShellArgsFromManifestInDebugMode() {
+    FlutterJNI mockFlutterJNI = mock(FlutterJNI.class);
+    FlutterLoader flutterLoader = new FlutterLoader(mockFlutterJNI);
+    Bundle metadata = spy(new Bundle());
+
+    metadata.putString(
+        "io.flutter.app.androidEngineShellArgs",
+        "[\"--enable-impeller=true\",\"--enable-dart-profiling\"]");
+    ctx.getApplicationInfo().metaData = metadata;
+
+    FlutterLoader.Settings settings = new FlutterLoader.Settings();
+    assertFalse(flutterLoader.initialized());
+    flutterLoader.startInitialization(ctx, settings);
+    flutterLoader.ensureInitializationComplete(ctx, null, false);
+    shadowOf(getMainLooper()).idle();
+
+    verify(metadata, never()).getString("io.flutter.app.androidEngineShellArgs");
+
+    ArgumentCaptor<String[]> shellArgsCaptor = ArgumentCaptor.forClass(String[].class);
+    verify(mockFlutterJNI, times(1))
+        .init(
+            eq(ctx),
+            shellArgsCaptor.capture(),
+            anyString(),
+            anyString(),
+            anyString(),
+            anyLong(),
+            anyInt());
+    List<String> arguments = Arrays.asList(shellArgsCaptor.getValue());
+    assertFalse(arguments.contains("--enable-impeller=true"));
+    assertFalse(arguments.contains("--enable-dart-profiling"));
+  }
+
+  @Test
+  public void itSetsCommandLineFlagsFromManifestMetadataInReleaseModeAndTakesPrecedence() {
+    String expectedImpellerArgFromMetadata = "--enable-impeller=false";
+    String expectedImpellerArgFromShellArgs = "--enable-impeller=true";
+
+    FlutterJNI mockFlutterJNI = mock(FlutterJNI.class);
+    FlutterLoader flutterLoader = new FlutterLoader(mockFlutterJNI);
+    Bundle metadata = new Bundle();
+
+    // Place metadata key and value into the metadata bundle used to mock the manifest.
+    metadata.putBoolean("io.flutter.embedding.android.EnableImpeller", false);
+
+    // Mock metadata put into the manifest by the Flutter tool when command line flag specified.
+    metadata.putString(
+        "io.flutter.app.androidEngineShellArgs", "[\"" + expectedImpellerArgFromShellArgs + "\"]");
+
+    ctx.getApplicationInfo().metaData = metadata;
+
+    FlutterLoader.Settings settings = new FlutterLoader.Settings();
+    assertFalse(flutterLoader.initialized());
+    flutterLoader.startInitialization(ctx, settings);
+    flutterLoader.ensureInitializationComplete(ctx, null, true);
+    shadowOf(getMainLooper()).idle();
+
+    ArgumentCaptor<String[]> shellArgsCaptor = ArgumentCaptor.forClass(String[].class);
+    verify(mockFlutterJNI, times(1))
+        .init(
+            eq(ctx),
+            shellArgsCaptor.capture(),
+            anyString(),
+            anyString(),
+            anyString(),
+            anyLong(),
+            anyInt());
+    List<String> arguments = Arrays.asList(shellArgsCaptor.getValue());
+
+    // Verify that the command line argument takes precedence over the manifest metadata.
+    assertTrue(
+        arguments.indexOf(expectedImpellerArgFromMetadata)
+            < arguments.indexOf(expectedImpellerArgFromShellArgs));
+  }
+
+  private void testMultipleFlagsFromManifestMetadata(
+      String jsonFlags, String[] expectedArgs, boolean shouldBeSet, boolean isReleaseMode) {
+    FlutterJNI mockFlutterJNI = mock(FlutterJNI.class);
+    FlutterLoader flutterLoader = new FlutterLoader(mockFlutterJNI);
+    Bundle metadata = new Bundle();
+
+    metadata.putString("io.flutter.app.androidEngineShellArgs", jsonFlags);
+    ctx.getApplicationInfo().metaData = metadata;
+
+    FlutterLoader.Settings settings = new FlutterLoader.Settings();
+    assertFalse(flutterLoader.initialized());
+    flutterLoader.startInitialization(ctx, settings);
+    flutterLoader.ensureInitializationComplete(ctx, null, isReleaseMode);
+    shadowOf(getMainLooper()).idle();
+
+    ArgumentCaptor<String[]> shellArgsCaptor = ArgumentCaptor.forClass(String[].class);
+    verify(mockFlutterJNI, times(1))
+        .init(
+            eq(ctx),
+            shellArgsCaptor.capture(),
+            anyString(),
+            anyString(),
+            anyString(),
+            anyLong(),
+            anyInt());
+    List<String> arguments = Arrays.asList(shellArgsCaptor.getValue());
+
+    for (String expectedArg : expectedArgs) {
+      if (shouldBeSet) {
+        assertTrue(
+            "Expected argument '"
+                + expectedArg
+                + "' was not found in the arguments passed to FlutterJNI.init",
+            arguments.contains(expectedArg));
+      } else {
+        assertFalse(
+            "Unexpected argument '"
+                + expectedArg
+                + "' was found in the arguments passed to FlutterJNI.init",
+            arguments.contains(expectedArg));
+      }
+    }
   }
 
   private void testFlagFromMetadataPresentInReleaseMode(

packages/flutter_tools/gradle/src/main/kotlin/FlutterPlugin.kt
@@ -303,6 +303,9 @@ class FlutterPlugin : Plugin<Project> {
             FlutterPluginUtils.addTaskForPrintBuildVariants(projectToAddTasksTo)
             FlutterPluginUtils.addTaskForPrintNdkVersion(projectToAddTasksTo)
             FlutterPluginUtils.addTasksForOutputsAppLinkSettings(projectToAddTasksTo)
+
+            // Task required to pass command line flags for apps to the Flutter Android embedding.
+            FlutterPluginUtils.addTaskForGeneratingEngineShellArgumentManifest(projectToAddTasksTo)
         }
         // Only applies to app projects. For module (aar) projects the host app's manifest is
         // the source of truth for HCPP; see addTasksForEnableHcppManifest for why injecting

packages/flutter_tools/gradle/src/main/kotlin/FlutterPluginUtils.kt
@@ -14,6 +14,7 @@ import com.android.builder.model.BuildType
 import com.flutter.gradle.plugins.PluginHandler
 import com.flutter.gradle.tasks.DeepLinkJsonFromManifestTask
 import com.flutter.gradle.tasks.EnableHcppManifestTask
+import com.flutter.gradle.tasks.GenerateEngineFlagsManifestTask
 import com.flutter.gradle.tasks.PrintTask
 import com.flutter.gradle.tasks.ValidateCompileSdkVersionTask
 import groovy.lang.Closure
@@ -1160,6 +1161,38 @@ object FlutterPluginUtils {
                     DeepLinkJsonFromManifestTask::manifestFile,
                     DeepLinkJsonFromManifestTask::updatedManifest
                 ).toTransform(SingleArtifact.MERGED_MANIFEST) // (3) Indicate the artifact and operation type.
+        }
+    }
+
+    /**
+     * Creates a task to generate an AndroidManifest.xml containing the engine shell arguments
+     * and adds it to the variant's manifests.
+     */
+    @JvmStatic
+    @JvmName("addTaskForGeneratingEngineShellArgumentManifest")
+    internal fun addTaskForGeneratingEngineShellArgumentManifest(project: Project) {
+        val engineShellArgsJson = project.findProperty("flutter.engineShellArgs") as? String ?: return
+        val androidComponents = project.extensions.getByType(AndroidComponentsExtension::class.java)
+
+        androidComponents.onVariants { variant ->
+            val capitalizeVariantName = capitalize(variant.name)
+            val generateManifestTaskProvider =
+                project.tasks.register(
+                    "generateEngineFlagsManifest$capitalizeVariantName",
+                    GenerateEngineFlagsManifestTask::class.java
+                ) {
+                    this.engineShellArgsJson.set(engineShellArgsJson)
+                    this.manifestOutputFile.set(
+                        project.layout.buildDirectory.file(
+                            "intermediates/flutter/${variant.name}/AndroidManifest.xml"
+                        )
+                    )
+                }
+
+            variant.sources.manifests?.addGeneratedManifestFile(
+                generateManifestTaskProvider,
+                GenerateEngineFlagsManifestTask::manifestOutputFile
+            )
         }
     }
 

packages/flutter_tools/gradle/src/main/kotlin/tasks/GenerateEngineFlagsManifestTask.kt
@@ -1,0 +1,52 @@
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package com.flutter.gradle.tasks
+
+import org.gradle.api.DefaultTask
+import org.gradle.api.file.RegularFileProperty
+import org.gradle.api.provider.Property
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.OutputFile
+import org.gradle.api.tasks.TaskAction
+import java.nio.charset.StandardCharsets
+import java.util.Base64
+
+abstract class GenerateEngineFlagsManifestTask : DefaultTask() {
+    @get:Input
+    abstract val engineShellArgsJson: Property<String>
+
+    @get:OutputFile
+    abstract val manifestOutputFile: RegularFileProperty
+
+    @TaskAction
+    fun generateManifest() {
+        val base64Encoded = engineShellArgsJson.get()
+        val decodedJsonStr = String(Base64.getDecoder().decode(base64Encoded), StandardCharsets.UTF_8)
+        val escapedValue = escapeXml(decodedJsonStr)
+        val metaDataTags = "<meta-data android:name=\"io.flutter.app.androidEngineShellArgs\" android:value=\"$escapedValue\" />"
+
+        val manifestContent =
+            """
+            <?xml version="1.0" encoding="utf-8"?>
+            <manifest xmlns:android="http://schemas.android.com/apk/res/android">
+                <application>
+                    $metaDataTags
+                </application>
+            </manifest>
+            """.trimIndent()
+
+        val outputFile = manifestOutputFile.get().asFile
+        outputFile.parentFile.mkdirs()
+        outputFile.writeText(manifestContent)
+    }
+
+    private fun escapeXml(str: String): String =
+        str
+            .replace("&", "&amp;")
+            .replace("<", "&lt;")
+            .replace(">", "&gt;")
+            .replace("\"", "&quot;")
+            .replace("'", "&apos;")
+}

commit 45b3e376588302fb461de92df3a3729273eca87a

[Android] Fix respecting`--enable-software-rendering` in the embedding (#191321)
Fixes regression caused by
https://github.com/flutter/flutter/pull/190222 that accidentally stopped
respecting the `enable-software-rendering` Intent flag in the Android
embedding.

## Pre-launch Checklist

- [x] I read the [Contributor Guide] and followed the process outlined
there for submitting PRs.
- [x] I read the [AI contribution guidelines] and understand my
responsibilities, or I am not using AI tools.
- [x] I read the [Tree Hygiene] wiki page, which explains my
responsibilities.
- [x] I read and followed the [Flutter Style Guide], including [Features
we expect every widget to implement].
- [x] I signed the [CLA].
- [x] I listed at least one issue that this PR fixes in the description
above.
- [ ] I updated/added relevant documentation (doc comments with `///`).
- [x] I added new tests to check the change I am making, or this PR is
[test-exempt].
- [x] I followed the [breaking change policy] and added [Data Driven
Fixes] where supported.
- [ ] All existing and new tests are passing.

If you need help, consider asking for advice on the #hackers-new channel
on [Discord].

If this change needs to override an active code freeze, provide a
comment explaining why. The code freeze workflow can be overridden by
code reviewers. See pinned issues for any active code freezes with
guidance.

**Note**: The Flutter team is currently trialing the use of [Gemini Code
Assist for
GitHub](https://developers.google.com/gemini-code-assist/docs/review-github-code).
Comments from the `gemini-code-assist` bot should not be taken as
authoritative feedback from the Flutter team. If you find its comments
useful you can update your code accordingly, but if you are unsure or
disagree with the feedback, please feel free to wait for a Flutter team
member's review for guidance on which automated comments should be
addressed.

<!-- Links -->
[Contributor Guide]:

engine/src/flutter/shell/platform/android/io/flutter/embedding/engine/FlutterEngineConnectionRegistry.java
@@ -349,7 +349,9 @@ import java.util.Set;
           "If you are attempting to set --enable-software-rendering via Intent extras to launch a Flutter component outside of using the Flutter CLI, note that support for setting engine flags on Android via Intents in release mode will soon be dropped; see https://github.com/flutter/flutter/issues/172553 for more information on this breaking change. To migrate, set the "
               + FlutterEngineFlags.ENABLE_SOFTWARE_RENDERING.metadataKey
               + " metadata in the application manifest. See https://github.com/flutter/flutter/blob/main/docs/engine/Flutter-Android-Engine-Flags.md for more info.");
-    } else {
+    }
+
+    if (!useSoftwareRendering) {
       // Check manifest for software rendering configuration.
       useSoftwareRendering = flutterLoader.getSofwareRenderingEnabledViaManifest();
     }

engine/src/flutter/shell/platform/android/test/io/flutter/embedding/engine/FlutterEngineConnectionRegistryTest.java
@@ -145,7 +145,7 @@ public class FlutterEngineConnectionRegistryTest {
   }
 
   @Test
-  public void attachToActivityConfiguresSoftwareRendering() {
+  public void attachToActivityConfiguresSoftwareRenderingViaManifest() {
     Context context = mock(Context.class);
     FlutterEngine flutterEngine = mock(FlutterEngine.class);
     PlatformViewsController platformViewsController = mock(PlatformViewsController.class);
@@ -171,6 +171,68 @@ public class FlutterEngineConnectionRegistryTest {
     registry.attachToActivity(appComponent, lifecycle);
 
     verify(platformViewsController).setSoftwareRendering(true);
+  }
+
+  @Test
+  public void attachToActivityConfiguresSoftwareRenderingViaIntent() {
+    Context context = mock(Context.class);
+    FlutterEngine flutterEngine = mock(FlutterEngine.class);
+    PlatformViewsController platformViewsController = mock(PlatformViewsController.class);
+    FlutterLoader flutterLoader = mock(FlutterLoader.class);
+    ExclusiveAppComponent<Activity> appComponent = mock(ExclusiveAppComponent.class);
+    Activity activity = mock(Activity.class);
+    Lifecycle lifecycle = mock(Lifecycle.class);
+
+    when(flutterEngine.getPlatformViewsController()).thenReturn(platformViewsController);
+    PlatformViewsControllerDelegator platformViewsControllerDelegator =
+        mock(PlatformViewsControllerDelegator.class);
+    when(flutterEngine.getPlatformViewsControllerDelegator())
+        .thenReturn(platformViewsControllerDelegator);
+    when(flutterEngine.getDartExecutor()).thenReturn(mock(DartExecutor.class));
+    when(flutterEngine.getRenderer()).thenReturn(mock(FlutterRenderer.class));
+    FlutterEngineConnectionRegistry registry =
+        new FlutterEngineConnectionRegistry(context, flutterEngine, flutterLoader, null);
+
+    when(flutterLoader.getSofwareRenderingEnabledViaManifest()).thenReturn(false);
+    when(appComponent.getAppComponent()).thenReturn(activity);
+    Intent intent = mock(Intent.class);
+    when(intent.getBooleanExtra("enable-software-rendering", false)).thenReturn(true);
+    when(activity.getIntent()).thenReturn(intent);
+
+    registry.attachToActivity(appComponent, lifecycle);
+
+    verify(platformViewsController).setSoftwareRendering(true);
+  }
+
+  @Test
+  public void attachToActivityDisablesSoftwareRendering() {
+    Context context = mock(Context.class);
+    FlutterEngine flutterEngine = mock(FlutterEngine.class);
+    PlatformViewsController platformViewsController = mock(PlatformViewsController.class);
+    FlutterLoader flutterLoader = mock(FlutterLoader.class);
+    ExclusiveAppComponent<Activity> appComponent = mock(ExclusiveAppComponent.class);
+    Activity activity = mock(Activity.class);
+    Lifecycle lifecycle = mock(Lifecycle.class);
+
+    when(flutterEngine.getPlatformViewsController()).thenReturn(platformViewsController);
+    PlatformViewsControllerDelegator platformViewsControllerDelegator =
+        mock(PlatformViewsControllerDelegator.class);
+    when(flutterEngine.getPlatformViewsControllerDelegator())
+        .thenReturn(platformViewsControllerDelegator);
+    when(flutterEngine.getDartExecutor()).thenReturn(mock(DartExecutor.class));
+    when(flutterEngine.getRenderer()).thenReturn(mock(FlutterRenderer.class));
+    FlutterEngineConnectionRegistry registry =
+        new FlutterEngineConnectionRegistry(context, flutterEngine, flutterLoader, null);
+
+    when(flutterLoader.getSofwareRenderingEnabledViaManifest()).thenReturn(false);
+    when(appComponent.getAppComponent()).thenReturn(activity);
+    Intent intent = mock(Intent.class);
+    when(intent.getBooleanExtra("enable-software-rendering", false)).thenReturn(false);
+    when(activity.getIntent()).thenReturn(intent);
+
+    registry.attachToActivity(appComponent, lifecycle);
+
+    verify(platformViewsController).setSoftwareRendering(false);
   }
 
   private static class FakeFlutterPlugin implements FlutterPlugin {