a8cdc5992cf3b7c790e1ab96d15e289a0a70be8a
Istiak-Ahmed78 <aistiak2078@gmail.com>
2026-08-21 14:09:41 +0600

    Drop material/cupertino menu_anchor.dart changes for framework-only PR
    Framework PR #185280-split only contains the 5 framework/test files (focus_manager.dart, focus_traversal.dart, view.dart, focus_manager_test.dart, focus_traversal_test.dart). The material and cupertino menu_anchor.dart changes are deferred to a separate flutter/packages PR.

--- packages/flutter/lib/src/material/menu_anchor.dart
+++ packages/flutter/lib/src/material/menu_anchor.dart
@@ -757,7 +757,7 @@ class _MenuAnchorState extends State<MenuAnchor> with SingleTickerProviderStateM
     }
     final FocusTraversalPolicy policy =
         FocusTraversalGroup.maybeOf(_menuScopeNode.context!) ?? ReadingOrderTraversalPolicy();
-    final FocusNode lastFocus = policy.findLastFocus(_menuScopeNode, ignoreCurrentFocus: true) ?? _menuScopeNode;
+    final FocusNode lastFocus = policy.findLastFocus(_menuScopeNode, ignoreCurrentFocus: true);
     lastFocus.requestFocus();
   }
 
