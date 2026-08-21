d525d4d19eb1fb40888031668690ea494e2b86e4
Ben Konyi <bkonyi@google.com>
2026-08-21 20:03:21 +0000

    Refactor `FlutterDevice.connect` and VM service discovery (#191221)
    ## Description
    
    Refactors `FlutterDevice.connect` in `packages/flutter_tools` to accept
    a single resolved `Uri` instead of a `Stream<Uri>`.
    
    - Replaces `FlutterDevice.vmServiceUris` with `Future<Uri>?
    vmServiceUri`.
    - Eliminates stream subscription, `_isListeningForVmServiceUri`, and
    stream state management from `resident_runner.dart`.
    - Moves single VM service candidate resolution into
    `VMServiceDiscoveryForAttach.firstValidUri` and
    `AttachCommand._discoverVmService`.
    - Updates `attach.dart`, `run_cold.dart`, `run_hot.dart`, and associated
    test suites.
    
    ## Tests
    - `packages/flutter_tools/test/commands.shard/hermetic/attach_test.dart`
    - `packages/flutter_tools/test/general.shard/cold_test.dart`
    -
    `packages/flutter_tools/test/general.shard/device_vm_service_discovery_for_attach_test.dart`
    - `packages/flutter_tools/test/general.shard/resident_runner_test.dart`
    -
    `packages/flutter_tools/test/general.shard/resident_web_runner_test.dart`
    - `packages/flutter_tools/test/general.shard/run_hot_test.dart`
    -
    `packages/flutter_tools/test/general.shard/proxied_devices/proxied_devices_test.dart`

--- packages/flutter_tools/test/general.shard/resident_runner_helpers.dart
+++ packages/flutter_tools/test/general.shard/resident_runner_helpers.dart
@@ -14,7 +14,6 @@ import 'package:flutter_tools/src/compile.dart';
 import 'package:flutter_tools/src/devfs.dart';
 import 'package:flutter_tools/src/device.dart';
 import 'package:flutter_tools/src/device_port_forwarder.dart';
-import 'package:flutter_tools/src/project.dart';
 import 'package:flutter_tools/src/resident_runner.dart';
 import 'package:flutter_tools/src/run_cold.dart';
 import 'package:flutter_tools/src/run_hot.dart';
@@ -173,19 +172,15 @@ class FakeDartDevelopmentServiceException implements DartDevelopmentServiceExcep
 }
 
 class TestFlutterDevice extends FlutterDevice {
-  TestFlutterDevice(super.device, {Stream<Uri>? vmServiceUris})
-    : _vmServiceUris = vmServiceUris,
-      super(
+  TestFlutterDevice(super.device, {Future<Uri>? vmServiceUri})
+    : super(
         generator: FakeResidentCompiler(),
         targetPlatform: .unsupported,
         buildInfo: BuildInfo.debug,
         developmentShaderCompiler: const FakeShaderCompiler(),
-      );
-
-  final Stream<Uri>? _vmServiceUris;
-
-  @override
-  Stream<Uri> get vmServiceUris => _vmServiceUris!;
+      ) {
+    this.vmServiceUri = vmServiceUri;
+  }
 }
 
 class ThrowingForwardingFileSystem extends ForwardingFileSystem {
@@ -223,7 +218,7 @@ class FakeFlutterDevice extends Fake implements FlutterDevice {
   TargetPlatform targetPlatform = TargetPlatform.android;
 
   @override
-  Stream<Uri?> get vmServiceUris => Stream<Uri?>.value(testUri);
+  Future<Uri>? get vmServiceUri => testUri != null ? Future<Uri>.value(testUri!) : null;
 
   @override
   FlutterVmService? get vmService => vmServiceHost?.call()?.vmService;
@@ -262,14 +257,12 @@ class FakeFlutterDevice extends Fake implements FlutterDevice {
 
   @override
   Future<void> connect({
+    required Uri vmServiceUri,
     ReloadSources? reloadSources,
     Restart? restart,
     CompileExpression? compileExpression,
-    FlutterProject? flutterProject,
     PrintStructuredErrorLogMethod? printStructuredErrorLogMethod,
     required DebuggingOptions debuggingOptions,
-    int? hostVmServicePort,
-    bool? ipv6 = false,
   }) async {
     if (connectError != null) {
       throw connectError!;
@@ -308,24 +301,25 @@ class FakeDelegateFlutterDevice extends FlutterDevice {
     super.device,
     BuildInfo buildInfo,
     ResidentCompiler residentCompiler,
-    this.fakeDevFS,
-  ) : super(
+    this.fakeDevFS, {
+    Future<Uri>? vmServiceUri,
+  }) : super(
         targetPlatform: .unsupported,
         buildInfo: buildInfo,
         generator: residentCompiler,
         developmentShaderCompiler: const FakeShaderCompiler(),
-      );
+      ) {
+    this.vmServiceUri = vmServiceUri ?? Future<Uri>.value(testUri);
+  }
 
   @override
   Future<void> connect({
+    required Uri vmServiceUri,
     ReloadSources? reloadSources,
     Restart? restart,
     CompileExpression? compileExpression,
-    FlutterProject? flutterProject,
     PrintStructuredErrorLogMethod? printStructuredErrorLogMethod,
     required DebuggingOptions debuggingOptions,
-    int? hostVmServicePort,
-    bool? ipv6 = false,
   }) async {}
 
   final DevFS fakeDevFS;
