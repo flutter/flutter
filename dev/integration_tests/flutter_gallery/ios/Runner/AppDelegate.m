74bc2120dc40d16e166759cc1b0d821cace90dea
louisehsu <louiseh0313@gmail.com>
2026-08-24 10:35:57 -0700

    [UIScene] Migrate dev/integration_tests/flutter_gallery to UIScene lifecycle

--- dev/integration_tests/flutter_gallery/ios/Runner/AppDelegate.m
+++ dev/integration_tests/flutter_gallery/ios/Runner/AppDelegate.m
@@ -8,7 +8,12 @@
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-  [GeneratedPluginRegistrant registerWithRegistry:self];
+  // Override point for customization after application launch.
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
+
+- (void)didInitializeImplicitFlutterEngine:(NSObject<FlutterImplicitEngineBridge> *)engineBridge {
+  [GeneratedPluginRegistrant registerWithRegistry:engineBridge.pluginRegistry];
+}
+
 @end
