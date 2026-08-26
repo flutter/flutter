48e7067237739cbac691c259fa8bd8a6a7338662
Ben Konyi <bkonyi@google.com>
2026-08-26 23:23:04 +0000

    [flutter_tools] Fix flutter drive device selection for unsupported project platforms
    Fixes an issue where flutter drive targeted devices unsupported by the
    project when building from source due to an inverted
    includeDevicesUnsupportedByProject condition in DriveCommand.
    
    Also adds defensive handling in DefaultDriverService.start when
    ApplicationPackageFactory returns null.
    
    Fixes https://github.com/flutter/flutter/issues/134073

--- packages/flutter_tools/lib/src/commands/drive.dart
+++ packages/flutter_tools/lib/src/commands/drive.dart
@@ -247,7 +247,7 @@ class DriveCommand extends RunCommandBase {
   String? get applicationBinaryPath => stringArg(FlutterOptions.kUseApplicationBinary);
 
   Future<Device?> get targetedDevice async {
-    return findTargetDevice(includeDevicesUnsupportedByProject: applicationBinaryPath == null);
+    return findTargetDevice(includeDevicesUnsupportedByProject: applicationBinaryPath != null);
   }
 
   // Wireless iOS devices need `publish-port` to be enabled because it requires mDNS.
