cc02421809b9534c53fa7de770260a53f26c288d
Sam Rawlins <srawlins@google.com>
2026-08-25 17:34:20 -0700

    [material] Remove unused parameters from constructors of generic test class
    We already don't allow unused parameters from non-generic classes. There was a bug preventing us from reporting such parameters on generic classes.
    
    This is work towards dart-lang/sdk#47839

--- packages/flutter/test/material/scaffold_test.dart
+++ packages/flutter/test/material/scaffold_test.dart
@@ -3882,7 +3882,7 @@ class _GeometryCachePainter extends CustomPainter {
 }
 
 class _CustomPageRoute<T> extends PageRoute<T> {
-  _CustomPageRoute({required this.builder, this.maintainState = true, super.fullscreenDialog});
+  _CustomPageRoute({required this.builder, super.fullscreenDialog});
 
   final WidgetBuilder builder;
 
@@ -3896,7 +3896,7 @@ class _CustomPageRoute<T> extends PageRoute<T> {
   String? get barrierLabel => null;
 
   @override
-  final bool maintainState;
+  bool get maintainState = true;
 
   @override
   Widget buildPage(
