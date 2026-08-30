dece19f1c1979086d8f2754884b2a4652ca5e003
Dev Ttangkong <ttankkeo112@gmail.com>
2026-08-31 01:48:55 +0900

    Enable CAMetalLayer drawable timeout

--- engine/src/flutter/shell/platform/darwin/ios/framework/Source/FlutterMetalLayer.mm
+++ engine/src/flutter/shell/platform/darwin/ios/framework/Source/FlutterMetalLayer.mm
@@ -176,6 +176,7 @@ FLUTTER_ASSERT_ARC
   if (self = [super init]) {
     self.device = MTLCreateSystemDefaultDevice();
     self.pixelFormat = MTLPixelFormatBGRA8Unorm;
+    self.allowsNextDrawableTimeout = YES;
     _availableTextures = [[NSMutableSet alloc] init];
 
     [[NSNotificationCenter defaultCenter] addObserver:self
