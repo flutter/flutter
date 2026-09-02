54e4c4dade594080a0af6b0ef52f2d4b7d23477e
Ben Konyi <bkonyi@google.com>
2026-09-02 19:11:31 +0000

    [tool] Cleanup temporary scripts

--- fix_compiler.py
+++
@@ -1,9 +0,0 @@
-import re
-
-with open('packages/flutter_tools/test/general.shard/test/test_compiler_test.dart', 'r') as f:
-    text = f.read()
-
-text = "import 'package:flutter_tools/src/base/context.dart';\nimport '../../src/fake_process_manager.dart';\n" + text
-
-with open('packages/flutter_tools/test/general.shard/test/test_compiler_test.dart', 'w') as f:
-    f.write(text)
