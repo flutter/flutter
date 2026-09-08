fc0e0386f3cf9872e7dbca66537e76e0a0be0dda
LongCatIsLooong <31859944+LongCatIsLooong@users.noreply.github.com>
2026-09-08 12:59:55 -0700

    ????

--- dev/flutter_analyzer_plugin/lib/src/rules/null_initialized_debug_expensive_fields.dart
+++ dev/flutter_analyzer_plugin/lib/src/rules/null_initialized_debug_expensive_fields.dart
@@ -58,7 +58,7 @@ class _Visitor extends SimpleAstVisitor<void> {
     }
   }
 
-  bool _isCorrectInitializer(Expression? initializer) {
+  static bool _isCorrectInitializer(Expression? initializer) {
     if (initializer case ConditionalExpression(
       :final Expression condition,
       elseExpression: NullLiteral(),
--- packages/flutter_test/lib/src/binding.dart
+++ packages/flutter_test/lib/src/binding.dart
@@ -228,7 +228,10 @@ class _TestFlutterView implements FlutterView {
 
   @override
   void render(ui.Scene scene, {ui.Size? size}) {
+    final stopwatch = Stopwatch();
+    stopwatch.start();
     onRender?.call(size);
+    stopwatch.stop();
   }
 
   @override
