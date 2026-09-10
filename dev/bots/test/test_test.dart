flutter/flutter

commit 43754916b41f33102f18d94846073280edede8b6

Increase number of 'Mac tool_integration_tests' subshards from 5 to 10 (#189040)
Rename builders from `tool_integration_tests_1_5` to
`tool_integration_tests_1` so the total number isn't encoded in the
builder name. Because it currently _is_ encoded in the builder name
(which isn't parsed, it's just informational) the original builders need
to become `bringup:true` unnecessarily, since adding shards creates new
builders.

Queue times are ~4 seconds, which means we seem to have enough capacity,
so throwing more shards at this problem shouldn't be an issue. Will keep
an eye on queue times after this change, though.

Fixes https://github.com/flutter/flutter/issues/189039

### 7 shards
Shard | Time
-- | -- |
1 | [23
minutes](https://ci.chromium.org/ui/p/flutter/builders/try/Mac%20tool_integration_tests_1_5/26349)
2 | [14
minutes](https://ci.chromium.org/ui/p/flutter/builders/try/Mac%20tool_integration_tests_2_5/29560)
3 | [14
minutes](https://ci.chromium.org/ui/p/flutter/builders/try/Mac%20tool_integration_tests_3_5/29422)
4 | [16
minutes](https://ci.chromium.org/ui/p/flutter/builders/try/Mac%20tool_integration_tests_4_5/29435)
5 | [25
minutes](https://ci.chromium.org/ui/p/flutter/builders/try/Mac%20tool_integration_tests_5_5/29533
)

### 9 shards 
Shard | Time
-- | -- |
1|[19+
minutes](https://ci.chromium.org/ui/p/flutter/builders/try/Mac%20tool_integration_tests_1_5/26351/overview)
2|[13
minutes](https://ci.chromium.org/ui/p/flutter/builders/try/Mac%20tool_integration_tests_2_5/29562/overview)
3|[11
minutes](https://ci.chromium.org/ui/p/flutter/builders/try/Mac%20tool_integration_tests_3_5/29424/overview)
4|[15
minutes](https://ci.chromium.org/ui/p/flutter/builders/try/Mac%20tool_integration_tests_4_5/29437/overview)
5|[13
minutes](https://ci.chromium.org/ui/p/flutter/builders/try/Mac%20tool_integration_tests_5_5/29535/overview)

### 10 shards
Shard | Time
-- | -- |
1|[13

.ci.yaml
@@ -4732,8 +4732,9 @@ targets:
       - engine/**
       - DEPS
 
-  - name: Mac tool_integration_tests_1_5
+  - name: Mac tool_integration_tests_1
     recipe: flutter/flutter_drone
+    bringup: true
     timeout: 60
     properties:
       cpu: arm64
@@ -4747,10 +4748,9 @@ targets:
           {"dependency": "goldctl", "version": "git_revision:c845c41b9b81bfcb11f2f0ab17b5b2386d634c31"}
         ]
       shard: tool_integration_tests
-      subshard: "1_5"
+      subshard: "1_10"
       tags: >
         ["framework", "hostonly", "shard", "mac"]
-      test_timeout_secs: "2700"
     runIf:
       - dev/**
       - packages/flutter_tools/**
@@ -4759,8 +4759,9 @@ targets:
       - engine/**
       - DEPS
 
-  - name: Mac tool_integration_tests_2_5
+  - name: Mac tool_integration_tests_2
     recipe: flutter/flutter_drone
+    bringup: true
     timeout: 60
     properties:
       cpu: arm64
@@ -4774,10 +4775,9 @@ targets:
           {"dependency": "goldctl", "version": "git_revision:c845c41b9b81bfcb11f2f0ab17b5b2386d634c31"}
         ]
       shard: tool_integration_tests
-      subshard: "2_5"
+      subshard: "2_10"
       tags: >
         ["framework", "hostonly", "shard", "mac"]
-      test_timeout_secs: "2700"
     runIf:
       - dev/**
       - packages/flutter_tools/**
@@ -4786,8 +4786,9 @@ targets:
       - engine/**
       - DEPS
 
-  - name: Mac tool_integration_tests_3_5
+  - name: Mac tool_integration_tests_3
     recipe: flutter/flutter_drone
+    bringup: true
     timeout: 60
     properties:
       cpu: arm64
@@ -4801,10 +4802,9 @@ targets:
           {"dependency": "goldctl", "version": "git_revision:c845c41b9b81bfcb11f2f0ab17b5b2386d634c31"}
         ]
       shard: tool_integration_tests
-      subshard: "3_5"
+      subshard: "3_10"
       tags: >
         ["framework", "hostonly", "shard", "mac"]
-      test_timeout_secs: "2700"
     runIf:
       - dev/**
       - packages/flutter_tools/**
@@ -4813,8 +4813,9 @@ targets:
       - engine/**
       - DEPS
 
-  - name: Mac tool_integration_tests_4_5
+  - name: Mac tool_integration_tests_4
     recipe: flutter/flutter_drone
+    bringup: true
     timeout: 60
     properties:
       cpu: arm64
@@ -4828,10 +4829,9 @@ targets:
           {"dependency": "goldctl", "version": "git_revision:c845c41b9b81bfcb11f2f0ab17b5b2386d634c31"}
         ]
       shard: tool_integration_tests
-      subshard: "4_5"
+      subshard: "4_10"
       tags: >
         ["framework", "hostonly", "shard", "mac"]
-      test_timeout_secs: "2700"
     runIf:
       - dev/**
       - packages/flutter_tools/**
@@ -4840,8 +4840,9 @@ targets:
       - engine/**
       - DEPS
 
-  - name: Mac tool_integration_tests_5_5
+  - name: Mac tool_integration_tests_5
     recipe: flutter/flutter_drone
+    bringup: true
     timeout: 60
     properties:
       cpu: arm64
@@ -4855,10 +4856,144 @@ targets:
           {"dependency": "goldctl", "version": "git_revision:c845c41b9b81bfcb11f2f0ab17b5b2386d634c31"}
         ]
       shard: tool_integration_tests
-      subshard: "5_5"
+      subshard: "5_10"
       tags: >
         ["framework", "hostonly", "shard", "mac"]
-      test_timeout_secs: "2700"
+    runIf:
+      - dev/**
+      - packages/flutter_tools/**
+      - bin/**
+      - .ci.yaml
+      - engine/**
+      - DEPS
+
+  - name: Mac tool_integration_tests_6
+    recipe: flutter/flutter_drone
+    bringup: true
+    timeout: 60
+    properties:
+      cpu: arm64
+      add_recipes_cq: "true"
+      dependencies: >-
+        [
+          {"dependency": "android_sdk", "version": "version:36v9unmodified"},
+          {"dependency": "chrome_and_driver", "version": "version:145.0.7632.117"},
+          {"dependency": "open_jdk", "version": "version:21"},
+          {"dependency": "ruby", "version": "ruby_3.1-pod_1.13"},
+          {"dependency": "goldctl", "version": "git_revision:c845c41b9b81bfcb11f2f0ab17b5b2386d634c31"}
+        ]
+      shard: tool_integration_tests
+      subshard: "6_10"
+      tags: >
+        ["framework", "hostonly", "shard", "mac"]
+    runIf:
+      - dev/**
+      - packages/flutter_tools/**
+      - bin/**
+      - .ci.yaml
+      - engine/**
+      - DEPS
+
+  - name: Mac tool_integration_tests_7
+    recipe: flutter/flutter_drone
+    bringup: true
+    timeout: 60
+    properties:
+      cpu: arm64
+      add_recipes_cq: "true"
+      dependencies: >-
+        [
+          {"dependency": "android_sdk", "version": "version:36v9unmodified"},
+          {"dependency": "chrome_and_driver", "version": "version:145.0.7632.117"},
+          {"dependency": "open_jdk", "version": "version:21"},
+          {"dependency": "ruby", "version": "ruby_3.1-pod_1.13"},
+          {"dependency": "goldctl", "version": "git_revision:c845c41b9b81bfcb11f2f0ab17b5b2386d634c31"}
+        ]
+      shard: tool_integration_tests
+      subshard: "7_10"
+      tags: >
+        ["framework", "hostonly", "shard", "mac"]
+    runIf:
+      - dev/**
+      - packages/flutter_tools/**
+      - bin/**
+      - .ci.yaml
+      - engine/**
+      - DEPS
+
+  - name: Mac tool_integration_tests_8
+    recipe: flutter/flutter_drone
+    bringup: true
+    timeout: 60
+    properties:
+      cpu: arm64
+      add_recipes_cq: "true"
+      dependencies: >-
+        [
+          {"dependency": "android_sdk", "version": "version:36v9unmodified"},
+          {"dependency": "chrome_and_driver", "version": "version:145.0.7632.117"},
+          {"dependency": "open_jdk", "version": "version:21"},
+          {"dependency": "ruby", "version": "ruby_3.1-pod_1.13"},
+          {"dependency": "goldctl", "version": "git_revision:c845c41b9b81bfcb11f2f0ab17b5b2386d634c31"}
+        ]
+      shard: tool_integration_tests
+      subshard: "8_10"
+      tags: >
+        ["framework", "hostonly", "shard", "mac"]
+    runIf:
+      - dev/**
+      - packages/flutter_tools/**
+      - bin/**
+      - .ci.yaml
+      - engine/**
+      - DEPS
+
+  - name: Mac tool_integration_tests_9
+    recipe: flutter/flutter_drone
+    bringup: true
+    timeout: 60
+    properties:
+      cpu: arm64
+      add_recipes_cq: "true"
+      dependencies: >-
+        [
+          {"dependency": "android_sdk", "version": "version:36v9unmodified"},
+          {"dependency": "chrome_and_driver", "version": "version:145.0.7632.117"},
+          {"dependency": "open_jdk", "version": "version:21"},
+          {"dependency": "ruby", "version": "ruby_3.1-pod_1.13"},
+          {"dependency": "goldctl", "version": "git_revision:c845c41b9b81bfcb11f2f0ab17b5b2386d634c31"}
+        ]
+      shard: tool_integration_tests
+      subshard: "9_10"
+      tags: >
+        ["framework", "hostonly", "shard", "mac"]
+    runIf:
+      - dev/**
+      - packages/flutter_tools/**
+      - bin/**
+      - .ci.yaml
+      - engine/**
+      - DEPS
+
+  - name: Mac tool_integration_tests_10
+    recipe: flutter/flutter_drone
+    bringup: true
+    timeout: 60
+    properties:
+      cpu: arm64
+      add_recipes_cq: "true"
+      dependencies: >-
+        [
+          {"dependency": "android_sdk", "version": "version:36v9unmodified"},
+          {"dependency": "chrome_and_driver", "version": "version:145.0.7632.117"},
+          {"dependency": "open_jdk", "version": "version:21"},
+          {"dependency": "ruby", "version": "ruby_3.1-pod_1.13"},
+          {"dependency": "goldctl", "version": "git_revision:c845c41b9b81bfcb11f2f0ab17b5b2386d634c31"}
+        ]
+      shard: tool_integration_tests
+      subshard: "10_10"
+      tags: >
+        ["framework", "hostonly", "shard", "mac"]
     runIf:
       - dev/**
       - packages/flutter_tools/**

commit af20b8547a0b84c742a85113a655428cc1f3532d

[ci] Sort integration tests and restore Mac test_timeout_secs
Because integration test discovery relied on Directory.listSync(),
the order of tests was non-deterministic and depended on filesystem
directory iteration. On macOS, this caused multiple long-running tests
(break_on_framework_exceptions_test.dart and deferred_components_test.dart)
to cluster together in subshard 7_10, consuming almost 30 minutes.

Additionally, commit 43754916b41 inadvertently omitted test_timeout_secs: "2700"
from the Mac tool_integration_tests targets in .ci.yaml, causing the LUCI
step execution timeout to default to 30 minutes (1800s). Under bot congestion,
subshard 7_10 exceeded 30 minutes and was killed with SIGTERM.

This change:
- Sorts integration tests lexicographically before sharding so tests are
  deterministically distributed across shards and platforms, separating
  the heaviest tests into different subshards.
- Restores test_timeout_secs: "2700" for all 10 Mac tool_integration_tests targets.
- Adds regression tests in ci_yaml_validation_test.dart and test_test.dart.

Fixes https://github.com/flutter/flutter/issues/192470

.ci.yaml
@@ -4789,6 +4789,7 @@ targets:
       subshard: "1_10"
       tags: >
         ["framework", "hostonly", "shard", "mac"]
+      test_timeout_secs: "2700"
     runIf:
       - dev/**
       - packages/flutter_tools/**
@@ -4816,6 +4817,7 @@ targets:
       subshard: "2_10"
       tags: >
         ["framework", "hostonly", "shard", "mac"]
+      test_timeout_secs: "2700"
     runIf:
       - dev/**
       - packages/flutter_tools/**
@@ -4843,6 +4845,7 @@ targets:
       subshard: "3_10"
       tags: >
         ["framework", "hostonly", "shard", "mac"]
+      test_timeout_secs: "2700"
     runIf:
       - dev/**
       - packages/flutter_tools/**
@@ -4870,6 +4873,7 @@ targets:
       subshard: "4_10"
       tags: >
         ["framework", "hostonly", "shard", "mac"]
+      test_timeout_secs: "2700"
     runIf:
       - dev/**
       - packages/flutter_tools/**
@@ -4897,6 +4901,7 @@ targets:
       subshard: "5_10"
       tags: >
         ["framework", "hostonly", "shard", "mac"]
+      test_timeout_secs: "2700"
     runIf:
       - dev/**
       - packages/flutter_tools/**
@@ -4924,6 +4929,7 @@ targets:
       subshard: "6_10"
       tags: >
         ["framework", "hostonly", "shard", "mac"]
+      test_timeout_secs: "2700"
     runIf:
       - dev/**
       - packages/flutter_tools/**
@@ -4951,6 +4957,7 @@ targets:
       subshard: "7_10"
       tags: >
         ["framework", "hostonly", "shard", "mac"]
+      test_timeout_secs: "2700"
     runIf:
       - dev/**
       - packages/flutter_tools/**
@@ -4978,6 +4985,7 @@ targets:
       subshard: "8_10"
       tags: >
         ["framework", "hostonly", "shard", "mac"]
+      test_timeout_secs: "2700"
     runIf:
       - dev/**
       - packages/flutter_tools/**
@@ -5005,6 +5013,7 @@ targets:
       subshard: "9_10"
       tags: >
         ["framework", "hostonly", "shard", "mac"]
+      test_timeout_secs: "2700"
     runIf:
       - dev/**
       - packages/flutter_tools/**
@@ -5032,6 +5041,7 @@ targets:
       subshard: "10_10"
       tags: >
         ["framework", "hostonly", "shard", "mac"]
+      test_timeout_secs: "2700"
     runIf:
       - dev/**
       - packages/flutter_tools/**

dev/bots/test.dart
@@ -204,6 +204,7 @@ Future<void> _runCommandsToolTests() async {
       allTests.add(file.path);
     }
   }
+  allTests.sort();
 
   await runDartTest(
     _toolsPath,
@@ -222,6 +223,8 @@ Future<void> _runWebToolTests() async {
       allTests.add(file.path);
     }
   }
+  allTests.sort();
+
   await runDartTest(
     _toolsPath,
     forceSingleCore: true,
@@ -234,12 +237,14 @@ Future<void> _runWebToolTests() async {
 Future<void> _runToolHostCrossArchTests() async {}
 
 Future<void> _runIntegrationToolTests() async {
-  final List<String> allTests = Directory(path.join(_toolsPath, 'test', 'integration.shard'))
-      .listSync(recursive: true)
-      .whereType<File>()
-      .map<String>((FileSystemEntity entry) => path.relative(entry.path, from: _toolsPath))
-      .where((String testPath) => path.basename(testPath).endsWith('_test.dart'))
-      .toList();
+  final List<String> allTests =
+      Directory(path.join(_toolsPath, 'test', 'integration.shard'))
+          .listSync(recursive: true)
+          .whereType<File>()
+          .map<String>((FileSystemEntity entry) => path.relative(entry.path, from: _toolsPath))
+          .where((String testPath) => path.basename(testPath).endsWith('_test.dart'))
+          .toList()
+        ..sort();
 
   await runDartTest(
     _toolsPath,
@@ -268,12 +273,14 @@ Future<void> _runToolTests() async {
 
 Future<void> _runSnippetsTests() async {
   final String snippetsPath = path.join(flutterRoot, 'dev', 'snippets');
-  final List<String> allTests = Directory(path.join(snippetsPath, 'test'))
-      .listSync(recursive: true)
-      .whereType<File>()
-      .map<String>((FileSystemEntity entry) => path.relative(entry.path, from: _toolsPath))
-      .where((String testPath) => path.basename(testPath).endsWith('_test.dart'))
-      .toList();
+  final List<String> allTests =
+      Directory(path.join(snippetsPath, 'test'))
+          .listSync(recursive: true)
+          .whereType<File>()
+          .map<String>((FileSystemEntity entry) => path.relative(entry.path, from: _toolsPath))
+          .where((String testPath) => path.basename(testPath).endsWith('_test.dart'))
+          .toList()
+        ..sort();
 
   await runDartTest(
     snippetsPath,

dev/bots/suite_runners/run_add_to_app_life_cycle_tests.dart
@@ -10,7 +10,7 @@ import '../run_command.dart';
 import '../utils.dart';
 
 Future<void> addToAppLifeCycleRunner() async {
-  if (Platform.isMacOS) {
+  if (Platform.isMacOS || dryRun) {
     printProgress('${green}Running add-to-app life cycle iOS integration tests$reset...');
     final String addToAppDir = path.join(
       flutterRoot,

dev/bots/suite_runners/run_android_java17_integration_tool_tests.dart
@@ -31,7 +31,8 @@ Future<void> androidJava17IntegrationToolTestsRunner() async {
           .whereType<File>()
           .map<String>((FileSystemEntity entry) => path.relative(entry.path, from: toolsPath))
           .where((String testPath) => path.basename(testPath).endsWith('_test.dart'))
-          .toList();
+          .toList()
+        ..sort();
 
   await runDartTest(
     toolsPath,

dev/bots/suite_runners/run_android_preview_integration_tool_tests.dart
@@ -17,7 +17,8 @@ Future<void> androidPreviewIntegrationToolTestsRunner() async {
           .whereType<File>()
           .map<String>((FileSystemEntity entry) => path.relative(entry.path, from: toolsPath))
           .where((String testPath) => path.basename(testPath).endsWith('_test.dart'))
-          .toList();
+          .toList()
+        ..sort();
 
   await runDartTest(
     toolsPath,

dev/bots/test/ci_yaml_validation_test.dart
@@ -39,6 +39,22 @@ void main() {
 
     expect(properties['leak_tracking'], anyOf('true', 'false', isNull));
     expect(properties['test_randomization_off'], anyOf('true', 'false', isNull));
+
+    if (properties['shard'] == 'tool_integration_tests') {
+      final timeoutSecs = properties['test_timeout_secs'] as String?;
+      expect(
+        timeoutSecs,
+        isNotNull,
+        reason: '${target.name} must specify test_timeout_secs (e.g. "2700") to avoid hitting the 30-minute step execution timeout.',
+      );
+      if (timeoutSecs != null) {
+        expect(
+          int.parse(timeoutSecs),
+          greaterThanOrEqualTo(2700),
+          reason: '${target.name} test_timeout_secs should be at least 2700s (45 minutes).',
+        );
+      }
+    }
   }
 
   group('framework', () {

dev/bots/test/test_test.dart
@@ -178,6 +178,28 @@ void main() {
       expectExitCode(result, 0);
       expect(result.stdout, contains('|> bin/flutter'));
     }, testOn: 'posix');
+
+    test('tool_integration_tests runs tests in sorted order with --dry-run', () async {
+      final ProcessResult result = await runScript(<String, String>{
+        'SHARD': 'tool_integration_tests',
+        'SUBSHARD': '7_10',
+      }, <String>['--dry-run']);
+      expectExitCode(result, 0);
+
+      final List<String> lines = (result.stdout as String).split('\n');
+      final List<String> testLines = lines
+          .map((String line) => line.trim())
+          .where((String line) => line.startsWith('test/integration.shard/') && line.endsWith('_test.dart'))
+          .toList();
+
+      expect(testLines, isNotEmpty);
+      final sortedTestLines = List<String>.of(testLines)..sort();
+      expect(
+        testLines,
+        equals(sortedTestLines),
+        reason: 'Tests in tool_integration_tests must be sorted deterministically to prevent uneven clustering across platforms.',
+      );
+    });
   });
 
   test('selectTestsForSubShard distributes tests amongst subshards correctly', () async {