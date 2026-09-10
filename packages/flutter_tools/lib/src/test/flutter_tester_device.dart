bed33440c2e1d35b2969330138d4067b119ea613
Ben Konyi <bkonyi@google.com>
2026-09-10 16:58:02 +0000

    [flutter_tools] Properly detect unexpected process exit on Windows and fix leak-tracking flakes
    On Windows, FlutterTesterTestDevice.finished was returning cleanly on any
    process exit due to an outdated workaround intended only for intentional
    kill() invocations. When the test subprocess exited prematurely, finished
    resolved without error, causing flutter_platform.dart to close the test harness
    socket while test execution was still active.
    
    Track intentional process termination with _killed in FlutterTesterTestDevice
    so that unexpected exits on Windows throw TestDeviceException as they do on POSIX.
    In addition, cleanly unmount the widget tree in navigator_restoration_test.dart
    following an assertion test, and remove the unnecessary goldctl dependency from
    Windows framework_tests_widgets_leak_tracking in .ci.yaml to prevent post-submit
    Skia Gold triage flakes on secondary builders.
    
    Fixes https://github.com/flutter/flutter/issues/192469

--- packages/flutter_tools/lib/src/test/flutter_tester_device.dart
+++ packages/flutter_tools/lib/src/test/flutter_tester_device.dart
@@ -73,6 +73,7 @@ class FlutterTesterTestDevice extends TestDevice {
   Process? _process;
   HttpServer? _server;
   DevtoolsLauncher? _devToolsLauncher;
+  bool _killed = false;
 
   /// Starts the device.
   ///
@@ -227,6 +228,7 @@ class FlutterTesterTestDevice extends TestDevice {
 
   @override
   Future<void> kill() async {
+    _killed = true;
     logger.printTrace('test $id: Terminating flutter_tester process');
     _process?.kill(io.ProcessSignal.sigkill);
 
@@ -242,8 +244,7 @@ class FlutterTesterTestDevice extends TestDevice {
   Future<void> get finished async {
     final int exitCode = await _exitCode.future;
 
-    // On Windows, the [exitCode] and the terminating signal have no correlation.
-    if (platform.isWindows) {
+    if (_killed) {
       return;
     }
 
