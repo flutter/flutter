b2829c9ff4a9917224243a8f132971364cca2f29
Kate Lovett <katelovett@google.com>
2026-09-11 17:02:06 -0500

    Fix

--- engine/src/flutter/lib/web_ui/lib/src/engine/view_embedder/embedding_strategy/custom_element_embedding_strategy.dart
+++ engine/src/flutter/lib/web_ui/lib/src/engine/view_embedder/embedding_strategy/custom_element_embedding_strategy.dart
@@ -41,11 +41,9 @@ class CustomElementEmbeddingStrategy implements EmbeddingStrategy {
       ..style.display = 'block'
       ..style.overflow = 'hidden'
       ..style.position = 'relative'
-              // This is needed so the browser lets flutter handle all pointer events
-              // it receives, without canceling them.
-              ..style
-              .touchAction =
-          'none';
+      // This is needed so the browser lets flutter handle all pointer events
+      // it receives, without canceling them.
+      ..style.touchAction = 'none';
 
     hostElement.appendChild(rootElement);
 
