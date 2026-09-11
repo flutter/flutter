b8111d2062cacbb5e1cc7e3b1f725a834482f0e0
Matt Boetger <boetger@google.com>
2026-09-10 18:33:31 -0700

    refactor(embedder): Phase 6.2 Legacy Class Purge - Deletion of obsolete Android shell holder and platform view hierarchies

--- engine/src/flutter/shell/platform/android/library_loader.cc
+++ engine/src/flutter/shell/platform/android/library_loader.cc
@@ -6,8 +6,6 @@
 #include "flutter/shell/platform/android/android_image_generator.h"
 #include "flutter/shell/platform/android/flutter_embedder_native.h"
 #include "flutter/shell/platform/android/flutter_main.h"
-#include "flutter/shell/platform/android/platform_view_android.h"
-#include "flutter/shell/platform/android/vsync_waiter_android.h"
 
 // This is called by the VM when the shared library is first loaded.
 JNIEXPORT jint JNI_OnLoad(JavaVM* vm, void* reserved) {
@@ -21,19 +19,9 @@ JNIEXPORT jint JNI_OnLoad(JavaVM* vm, void* reserved) {
   result = flutter::FlutterMain::Register(env);
   FML_CHECK(result);
 
-  // Register PlatformView helper classes and callbacks without registering
-  // legacy FlutterJNI native methods.
-  result =
-      flutter::PlatformViewAndroid::Register(env, /*register_natives=*/false);
-  FML_CHECK(result);
-
   // Register FlutterEmbedderNative as the exclusive native entrypoint registrar
   // for io.flutter.embedding.engine.FlutterJNI.
   result = flutter::android::FlutterEmbedderNative::RegisterJni(env);
-  FML_CHECK(result);
-
-  // Register VSyncWaiter.
-  result = flutter::VsyncWaiterAndroid::Register(env);
   FML_CHECK(result);
 
   // Register AndroidImageDecoder.
