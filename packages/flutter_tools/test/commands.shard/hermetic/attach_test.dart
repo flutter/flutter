4944712d399ecc381f051008ac4839705fa378a6
Ben Konyi <bkonyi@google.com>
2026-09-13 02:52:51 +0000

    [flutter_tools] Format code and fix analyzer issues

--- packages/flutter_tools/test/commands.shard/hermetic/attach_test.dart
+++ packages/flutter_tools/test/commands.shard/hermetic/attach_test.dart
@@ -131,12 +131,11 @@ void main() {
             }
           });
           final hotRunner = FakeHotRunner();
-          hotRunner.onAttach =
-              (
-                Completer<DebugConnectionInfo>? connectionInfoCompleter,
-                Completer<void>? appStartedCompleter,
-                bool enableDevTools,
-              ) async => 0;
+          hotRunner.onAttach = (
+            Completer<DebugConnectionInfo>? connectionInfoCompleter,
+            Completer<void>? appStartedCompleter,
+            bool enableDevTools,
+          ) async => 0;
           hotRunner.exited = false;
           hotRunner.isWaitingForVmService = false;
           final hotRunnerFactory = FakeHotRunnerFactory()..hotRunner = hotRunner;
@@ -288,12 +287,11 @@ void main() {
             }
           });
           final hotRunner = FakeHotRunner();
-          hotRunner.onAttach =
-              (
-                Completer<DebugConnectionInfo>? connectionInfoCompleter,
-                Completer<void>? appStartedCompleter,
-                bool enableDevTools,
-              ) async => 0;
+          hotRunner.onAttach = (
+            Completer<DebugConnectionInfo>? connectionInfoCompleter,
+            Completer<void>? appStartedCompleter,
+            bool enableDevTools,
+          ) async => 0;
           hotRunner.exited = false;
           hotRunner.isWaitingForVmService = false;
           var passedArtifactTest = false;
@@ -363,12 +361,11 @@ void main() {
           );
           testDeviceManager.devices = <Device>[device];
           final hotRunner = FakeHotRunner();
-          hotRunner.onAttach =
-              (
-                Completer<DebugConnectionInfo>? connectionInfoCompleter,
-                Completer<void>? appStartedCompleter,
-                bool enableDevTools,
-              ) async => 0;
+          hotRunner.onAttach = (
+            Completer<DebugConnectionInfo>? connectionInfoCompleter,
+            Completer<void>? appStartedCompleter,
+            bool enableDevTools,
+          ) async => 0;
           hotRunner.exited = false;
           hotRunner.isWaitingForVmService = false;
           final hotRunnerFactory = FakeHotRunnerFactory()..hotRunner = hotRunner;
@@ -443,12 +440,11 @@ void main() {
           );
           testDeviceManager.devices = <Device>[device];
           final hotRunner = FakeHotRunner();
-          hotRunner.onAttach =
-              (
-                Completer<DebugConnectionInfo>? connectionInfoCompleter,
-                Completer<void>? appStartedCompleter,
-                bool enableDevTools,
-              ) async => 0;
+          hotRunner.onAttach = (
+            Completer<DebugConnectionInfo>? connectionInfoCompleter,
+            Completer<void>? appStartedCompleter,
+            bool enableDevTools,
+          ) async => 0;
           hotRunner.exited = false;
           hotRunner.isWaitingForVmService = false;
           final hotRunnerFactory = FakeHotRunnerFactory()..hotRunner = hotRunner;
@@ -528,12 +524,11 @@ void main() {
           );
           testDeviceManager.devices = <Device>[device];
           final hotRunner = FakeHotRunner();
-          hotRunner.onAttach =
-              (
-                Completer<DebugConnectionInfo>? connectionInfoCompleter,
-                Completer<void>? appStartedCompleter,
-                bool enableDevTools,
-              ) async => 0;
+          hotRunner.onAttach = (
+            Completer<DebugConnectionInfo>? connectionInfoCompleter,
+            Completer<void>? appStartedCompleter,
+            bool enableDevTools,
+          ) async => 0;
           hotRunner.exited = false;
           hotRunner.isWaitingForVmService = false;
           final hotRunnerFactory = FakeHotRunnerFactory()..hotRunner = hotRunner;
@@ -626,12 +621,11 @@ void main() {
           );
           testDeviceManager.devices = <Device>[device];
           final hotRunner = FakeHotRunner();
-          hotRunner.onAttach =
-              (
-                Completer<DebugConnectionInfo>? connectionInfoCompleter,
-                Completer<void>? appStartedCompleter,
-                bool enableDevTools,
-              ) async => 0;
+          hotRunner.onAttach = (
+            Completer<DebugConnectionInfo>? connectionInfoCompleter,
+            Completer<void>? appStartedCompleter,
+            bool enableDevTools,
+          ) async => 0;
           hotRunner.exited = false;
           hotRunner.isWaitingForVmService = false;
           final hotRunnerFactory = FakeHotRunnerFactory()..hotRunner = hotRunner;
@@ -818,12 +812,11 @@ void main() {
           const outputDill = '/tmp/output.dill';
 
           final hotRunner = FakeHotRunner();
-          hotRunner.onAttach =
-              (
-                Completer<DebugConnectionInfo>? connectionInfoCompleter,
-                Completer<void>? appStartedCompleter,
-                bool enableDevTools,
-              ) async => 0;
+          hotRunner.onAttach = (
+            Completer<DebugConnectionInfo>? connectionInfoCompleter,
+            Completer<void>? appStartedCompleter,
+            bool enableDevTools,
+          ) async => 0;
           hotRunner.exited = false;
           hotRunner.isWaitingForVmService = false;
 
@@ -880,12 +873,11 @@ void main() {
           device.onGetLogReader = () => fakeLogReader;
           testDeviceManager.devices = <Device>[device];
           final hotRunner = FakeHotRunner();
-          hotRunner.onAttach =
-              (
-                Completer<DebugConnectionInfo>? connectionInfoCompleter,
-                Completer<void>? appStartedCompleter,
-                bool enableDevTools,
-              ) async => 0;
+          hotRunner.onAttach = (
+            Completer<DebugConnectionInfo>? connectionInfoCompleter,
+            Completer<void>? appStartedCompleter,
+            bool enableDevTools,
+          ) async => 0;
           hotRunner.exited = false;
           hotRunner.isWaitingForVmService = false;
           final hotRunnerFactory = FakeHotRunnerFactory()..hotRunner = hotRunner;
@@ -929,12 +921,11 @@ flutter:
           device.onGetLogReader = () => fakeLogReader;
           testDeviceManager.devices = <Device>[device];
           final hotRunner = FakeHotRunner();
-          hotRunner.onAttach =
-              (
-                Completer<DebugConnectionInfo>? connectionInfoCompleter,
-                Completer<void>? appStartedCompleter,
-                bool enableDevTools,
-              ) async => 0;
+          hotRunner.onAttach = (
+            Completer<DebugConnectionInfo>? connectionInfoCompleter,
+            Completer<void>? appStartedCompleter,
+            bool enableDevTools,
+          ) async => 0;
           hotRunner.exited = false;
           hotRunner.isWaitingForVmService = false;
           final hotRunnerFactory = FakeHotRunnerFactory()..hotRunner = hotRunner;
@@ -1016,12 +1007,11 @@ flutter:
             }
           });
           final hotRunner = FakeHotRunner();
-          hotRunner.onAttach =
-              (
-                Completer<DebugConnectionInfo>? connectionInfoCompleter,
-                Completer<void>? appStartedCompleter,
-                bool enableDevTools,
-              ) async => 0;
+          hotRunner.onAttach = (
+            Completer<DebugConnectionInfo>? connectionInfoCompleter,
+            Completer<void>? appStartedCompleter,
+            bool enableDevTools,
+          ) async => 0;
           hotRunner.exited = false;
           hotRunner.isWaitingForVmService = false;
           final hotRunnerFactory = FakeHotRunnerFactory()..hotRunner = hotRunner;
@@ -2250,9 +2240,8 @@ class FakeMDnsClient extends Fake implements MDnsClient {
     if (T == IPAddressResourceRecord) {
       final String key = query.fullyQualifiedName;
       return Stream<IPAddressResourceRecord>.fromIterable(
-            ipResponse[key] ?? <IPAddressResourceRecord>[],
-          )
-          as Stream<T>;
+        ipResponse[key] ?? <IPAddressResourceRecord>[],
+      ) as Stream<T>;
     }
     throw UnsupportedError('Unsupported query type $T');
   }
--- packages/flutter_tools/test/general.shard/vmservice_test.dart
+++ packages/flutter_tools/test/general.shard/vmservice_test.dart
@@ -162,8 +162,7 @@ void main() {
 
     await flutterVmService.setAssetDirectory(
       assetsDirectory: Uri(
-        path:
-            'C:/Users/Tester/AppData/Local/Temp/hello_worldb42a6da5/hello_world/build/flutter_assets',
+        path: 'C:/Users/Tester/AppData/Local/Temp/hello_worldb42a6da5/hello_world/build/flutter_assets',
         scheme: 'file',
       ),
       viewId: 'abc',
@@ -178,8 +177,7 @@ void main() {
     expect(call.isolateId, 'def');
     expect(call.args, <String, String>{
       'viewId': 'abc',
-      'assetDirectory':
-          r'C:\Users\Tester\AppData\Local\Temp\hello_worldb42a6da5\hello_world\build\flutter_assets',
+      'assetDirectory': r'C:\Users\Tester\AppData\Local\Temp\hello_worldb42a6da5\hello_world\build\flutter_assets',
     });
   });
 
@@ -572,31 +570,28 @@ void main() {
       },
     );
 
-    testWithoutContext(
-      'when the isolate stream is already subscribed, returns an isolate with the registered extensionRPC',
-      () async {
-        final fakeVmServiceHost = FakeVmServiceHost(
-          requests: <VmServiceExpectation>[
-            const FakeVmServiceRequest(
-              method: 'streamListen',
-              args: <String, Object>{'streamId': 'Isolate'},
-              // Stream already subscribed - https://github.com/dart-lang/sdk/blob/main/runtime/vm/service/service.md#streamlisten
-              error: FakeRPCError(code: 103),
-            ),
-            listViewsRequest,
-            FakeVmServiceRequest(
-              method: 'getIsolate',
-              jsonResponse: isolate.toJson()..['extensionRPCs'] = <String>[kExtensionName],
-              args: <String, Object>{'isolateId': '1'},
-            ),
-          ],
-        );
+    testWithoutContext('when the isolate stream is already subscribed, returns an isolate with the registered extensionRPC', () async {
+      final fakeVmServiceHost = FakeVmServiceHost(
+        requests: <VmServiceExpectation>[
+          const FakeVmServiceRequest(
+            method: 'streamListen',
+            args: <String, Object>{'streamId': 'Isolate'},
+            // Stream already subscribed - https://github.com/dart-lang/sdk/blob/main/runtime/vm/service/service.md#streamlisten
+            error: FakeRPCError(code: 103),
+          ),
+          listViewsRequest,
+          FakeVmServiceRequest(
+            method: 'getIsolate',
+            jsonResponse: isolate.toJson()..['extensionRPCs'] = <String>[kExtensionName],
+            args: <String, Object>{'isolateId': '1'},
+          ),
+        ],
+      );
 
-        final vm_service.IsolateRef isolateRef = await fakeVmServiceHost.vmService
-            .findExtensionIsolate(kExtensionName);
-        expect(isolateRef.id, '1');
-      },
-    );
+      final vm_service.IsolateRef isolateRef = await fakeVmServiceHost.vmService
+          .findExtensionIsolate(kExtensionName);
+      expect(isolateRef.id, '1');
+    });
 
     testWithoutContext('returns an isolate with a extensionRPC that is registered later', () async {
       final fakeVmServiceHost = FakeVmServiceHost(
