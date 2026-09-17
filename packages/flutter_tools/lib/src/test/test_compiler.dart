c1bf84aee823a08148f44b3c5622f0e7459f375b
Ben Konyi <bkonyi@google.com>
2026-09-17 12:36:49 -0400

    Restore dot shorthand for targetPlatform

--- packages/flutter_tools/lib/src/test/test_compiler.dart
+++ packages/flutter_tools/lib/src/test/test_compiler.dart
@@ -233,7 +233,7 @@ class TestCompiler {
       fileSystem: fs,
       shutdownHooks: shutdownHooks,
       config: config,
-      targetPlatform: TargetPlatform.tester,
+      targetPlatform: .tester,
     );
     return residentCompiler;
   }
