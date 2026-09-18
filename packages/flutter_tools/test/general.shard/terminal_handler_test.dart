03ce54349560ffd7843f889e3dc6a20568878e78
Ben Konyi <bkonyi@google.com>
2026-09-18 04:47:17 +0000

    [tool] Remove duplicate import in terminal_handler_test.dart

--- packages/flutter_tools/test/general.shard/terminal_handler_test.dart
+++ packages/flutter_tools/test/general.shard/terminal_handler_test.dart
@@ -27,7 +27,6 @@ import 'package:vm_service/vm_service.dart' as vm_service;
 import '../src/common.dart';
 import '../src/fake_vm_services.dart';
 import '../src/fakes.dart';
-import '../src/fakes.dart';
 
 final fakeUnpausedIsolate = vm_service.Isolate(
   id: '1',
@@ -1286,7 +1285,12 @@ void main() {
     final residentRunner = FakeResidentRunner(
       FlutterDevice(
         FakeDevice(),
-        toolContext: TestToolContext(fileSystem: fs, logger: testLogger, processInfo: processInfo, artifacts: Artifacts.test()),
+        toolContext: TestToolContext(
+          fileSystem: fs,
+          logger: testLogger,
+          processInfo: processInfo,
+          artifacts: Artifacts.test(),
+        ),
         targetPlatform: .unsupported,
         buildInfo: BuildInfo.debug,
         generator: FakeResidentCompiler(),
@@ -1660,7 +1664,12 @@ TerminalHandler setUpTerminalHandler(
   final processInfo = ProcessInfo.test(MemoryFileSystem.test());
   final device = FlutterDevice(
     FakeDevice()..supportsScreenshot = supportsScreenshot,
-    toolContext: TestToolContext(fileSystem: localFileSystem, logger: testLogger, processInfo: processInfo, artifacts: Artifacts.test()),
+    toolContext: TestToolContext(
+      fileSystem: localFileSystem,
+      logger: testLogger,
+      processInfo: processInfo,
+      artifacts: Artifacts.test(),
+    ),
     buildInfo: BuildInfo(
       buildMode,
       '',
