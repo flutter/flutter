220a262cc6f9b08e5581d9acec1bd6954d4063c7
Kevin Moore <kevmoo@google.com>
2026-09-18 05:19:26 +0000

    fix(web_ui): omit group role on menu scrollables even when non-overflowing

--- engine/src/flutter/lib/web_ui/lib/src/engine/semantics/scrollable.dart
+++ engine/src/flutter/lib/web_ui/lib/src/engine/semantics/scrollable.dart
@@ -114,6 +114,13 @@ class SemanticScrollable extends SemanticRole {
   @override
   void update() {
     super.update();
+    semanticsObject.owner.addOneTimePostUpdateCallback(() {
+      if (!semanticsObject.hasLabel && _hasMenuAncestor()) {
+        setAriaRole('none');
+      } else {
+        setAriaRole('group');
+      }
+    });
     if (!_canScroll) {
       _cleanUp();
       return;
@@ -129,11 +136,6 @@ class SemanticScrollable extends SemanticRole {
     }
 
     semanticsObject.owner.addOneTimePostUpdateCallback(() {
-      if (!semanticsObject.hasLabel && _hasMenuAncestor()) {
-        setAriaRole('none');
-      } else {
-        setAriaRole('group');
-      }
       if (_canScroll) {
         final double? scrollPosition = semanticsObject.scrollPosition;
         assert(scrollPosition != null);
