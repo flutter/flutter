c54542033e48506fdf845a83cb0687e4a1d2eb68
Ben Konyi <bkonyi@google.com>
2026-09-18 02:20:42 +0000

    Merge branch updates and preserve ToolContext DevFS refactor

--- packages/flutter_tools/lib/src/devfs.dart
+++ packages/flutter_tools/lib/src/devfs.dart
@@ -5,7 +5,6 @@
 import 'dart:async';
 
 import 'package:package_config/package_config.dart';
-import 'package:process/process.dart';
 import 'package:vm_service/vm_service.dart' as vm_service;
 
 import 'artifacts.dart';
@@ -430,35 +429,33 @@ class DevFS {
     FlutterVmService serviceProtocol,
     this.fsName,
     this.rootDirectory, {
-    required Artifacts artifacts,
     required BuildMode buildMode,
-    required FileSystem fileSystem,
-    required Logger logger,
-    required OperatingSystemUtils osUtils,
-    required ProcessManager processManager,
+    required ToolContext toolContext,
     HttpClient? httpClient,
     this._stopwatchFactory = const StopwatchFactory(),
     Duration? uploadRetryThrottle,
-  }) : _vmService = serviceProtocol,
+  }) : _toolContext = toolContext,
+       _vmService = serviceProtocol,
        _httpWriter = _DevFSHttpWriter(
          fsName,
          serviceProtocol,
-         osUtils: osUtils,
-         logger: logger,
+         osUtils: toolContext.os,
+         logger: toolContext.logger,
          uploadRetryThrottle: uploadRetryThrottle,
          httpClient: httpClient ?? HttpClient(),
        ),
        _assetTransformer = DevelopmentAssetTransformer(
          transformer: AssetTransformer(
-           processManager: processManager,
-           fileSystem: fileSystem,
-           dartBinaryPath: artifacts.getArtifactPath(Artifact.engineDartBinary),
+           processManager: toolContext.processManager,
+           fileSystem: toolContext.fs,
+           dartBinaryPath: toolContext.artifacts.getArtifactPath(Artifact.engineDartBinary),
            buildMode: buildMode,
          ),
-         fileSystem: fileSystem,
-         logger: logger,
+         fileSystem: toolContext.fs,
+         logger: toolContext.logger,
        );
 
+  final ToolContext _toolContext;
   final FlutterVmService _vmService;
   final _DevFSHttpWriter _httpWriter;
   final StopwatchFactory _stopwatchFactory;
