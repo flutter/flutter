ba7ee48ee7a12767f0c16eb676f3f164daf23919
sorensp-goog <sorensp@google.com>
2026-09-21 15:04:03 +0200

    Update browser_environment.js

--- engine/src/flutter/lib/web_ui/flutter_js/src/browser_environment.js
+++ engine/src/flutter/lib/web_ui/flutter_js/src/browser_environment.js
@@ -62,7 +62,7 @@ const supportsDart2Wasm = () => {
   //          hello.dart
   //   % cat hello.support.js
   // ```
-  // It checks suport for Wasm GC, SIMD  and `js-string` builtins.
+  // It checks support for Wasm GC, SIMD, `js-string` builtins, and `try_table`.
   return (WebAssembly.validate(new Uint8Array([0,97,115,109,1,0,0,0,1,5,1,95,1,120,0]))&&WebAssembly.validate(new Uint8Array([0,97,115,109,1,0,0,0,1,5,1,96,0,1,123,3,2,1,0,10,10,1,8,0,65,0,253,15,253,98,11]))&&!WebAssembly.validate(new Uint8Array([0,97,115,109,1,0,0,0,1,4,1,96,0,0,2,23,1,14,119,97,115,109,58,106,115,45,115,116,114,105,110,103,4,99,97,115,116,0,0]),{"builtins":["js-string"]})&&WebAssembly.validate(new Uint8Array([0,97,115,109,1,0,0,0,1,4,1,96,0,0,3,2,1,0,10,13,1,11,0,2,64,31,64,1,2,0,11,11,11])));
 }
 
