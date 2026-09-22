af0f14728e7bd4969008696fefc4ed9babf6acf4
Matej Knopp <matej.knopp@gmail.com>
2026-09-22 22:09:47 +0200

    [win32] Multi-window should never have implicit view

--- engine/src/flutter/shell/platform/windows/host_window.cc
+++ engine/src/flutter/shell/platform/windows/host_window.cc
@@ -236,9 +236,10 @@ void HostWindow::InitializeFlutterView(
       params.initial_window_rect.width(), params.initial_window_rect.height(),
       engine_->display_manager(), engine_->windows_proc_table());
 
-  std::unique_ptr<FlutterWindowsView> view =
-      engine_->CreateView(std::move(view_window), params.is_sized_to_content,
-                          params.box_constraints, params.sizing_delegate);
+  std::unique_ptr<FlutterWindowsView> view = engine_->CreateView(
+      std::move(view_window), params.is_sized_to_content,
+      params.box_constraints, /* allow_implicit_view=*/false,
+      params.sizing_delegate);
   FML_CHECK(view != nullptr);
 
   view_controller_ =
