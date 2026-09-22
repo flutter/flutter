43b2a0c0b2dc44f2dc574dec4a45abfedb89188b
Matej Knopp <matej.knopp@gmail.com>
2026-09-22 22:29:34 +0200

    [win32] Remove the predicate from TimerThread condition variable

--- engine/src/flutter/shell/platform/windows/task_runner_window.cc
+++ engine/src/flutter/shell/platform/windows/task_runner_window.cc
@@ -59,10 +59,7 @@ void TimerThread::ScheduleAt(
 void TimerThread::TimerThreadMain() {
   std::unique_lock<std::mutex> lock(mutex_);
   while (callback_ != nullptr) {
-    cv_.wait_until(lock, next_fire_time_, [this]() {
-      return std::chrono::high_resolution_clock::now() >= next_fire_time_ ||
-             callback_ == nullptr;
-    });
+    cv_.wait_until(lock, next_fire_time_);
     auto scheduled_count = schedule_counter_;
     if (callback_) {
       lock.unlock();
