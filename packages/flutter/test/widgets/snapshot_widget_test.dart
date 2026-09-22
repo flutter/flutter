a4e6736959e4294438515fb75260eb84d4baf786
Harry Terkelsen <harry.terkelsen@gmail.com>
2026-09-22 13:52:45 -0700

    Remove unnecessary foundation.dart import
    kIsWeb is no longer referenced now that the skips are gone, and the
    analyzer flags the import as unnecessary (unnecessary_import).

--- packages/flutter/test/widgets/snapshot_widget_test.dart
+++ packages/flutter/test/widgets/snapshot_widget_test.dart
@@ -9,7 +9,6 @@ library;
 
 import 'dart:ui' as ui;
 
-import 'package:flutter/foundation.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
