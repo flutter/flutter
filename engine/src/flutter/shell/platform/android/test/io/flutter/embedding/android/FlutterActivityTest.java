919bb9cf65fa502e4d9ccdc7bd59badaf1d805c7
Camille Simon <camillesimon@google.com>
2026-09-22 15:48:58 -0700

    format

--- engine/src/flutter/shell/platform/android/test/io/flutter/embedding/android/FlutterActivityTest.java
+++ engine/src/flutter/shell/platform/android/test/io/flutter/embedding/android/FlutterActivityTest.java
@@ -4,7 +4,6 @@
 
 package io.flutter.embedding.android;
 
-import io.flutter.embedding.android.FlutterActivityLaunchConfigs;
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
--- engine/src/flutter/shell/platform/android/test/io/flutter/embedding/android/FlutterFragmentActivityTest.java
+++ engine/src/flutter/shell/platform/android/test/io/flutter/embedding/android/FlutterFragmentActivityTest.java
@@ -4,7 +4,6 @@
 
 package io.flutter.embedding.android;
 
-import io.flutter.embedding.android.FlutterActivityLaunchConfigs;
 import static io.flutter.embedding.android.FlutterFragment.ARG_CACHED_ENGINE_ID;
 import static io.flutter.embedding.android.FlutterFragment.ARG_DESTROY_ENGINE_WITH_FRAGMENT;
 import static org.junit.Assert.assertEquals;
