67666e6deca21f46374b1e3fec4fbc0b62177f9d
Harry Terkelsen <harry.terkelsen@gmail.com>
2026-09-23 12:58:05 -0700

    Restore accidentally removed pixel assertions in `proxy_box_test.dart`
    Restores `expect(getPixel(image.width - 1, 20), equals(0xffffffff));` in both `RenderRepaintBoundary` image capture tests (`RenderRepaintBoundary can capture images of itself` and `RenderRepaintBoundary can capture images of itself synchronously`).

--- packages/flutter/test/rendering/proxy_box_test.dart
+++ packages/flutter/test/rendering/proxy_box_test.dart
@@ -252,6 +252,7 @@ void main() {
     expect(getPixel(0, 0), equals(0x00000000));
     expect(getPixel(20, 20), equals(0x00000080));
     expect(getPixel(image.width - 1, 0), equals(0x00000000));
+    expect(getPixel(image.width - 1, 20), equals(0xffffffff));
   });
 
   test('RenderRepaintBoundary can capture images of itself synchronously', () async {
@@ -339,6 +340,7 @@ void main() {
     expect(getPixel(0, 0), equals(0x00000000));
     expect(getPixel(20, 20), equals(0x00000080));
     expect(getPixel(image.width - 1, 0), equals(0x00000000));
+    expect(getPixel(image.width - 1, 20), equals(0xffffffff));
   });
 
   test('RenderOpacity does not composite if it is transparent', () {
