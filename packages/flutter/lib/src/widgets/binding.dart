60032db1400ada630e933097f532747adc9178ad
Sarah Zakarias <zarah@google.com>
2026-09-25 08:44:49 +0000

    Fix doc references to Material and Cupertino in the widgets library and its tests

--- packages/flutter/lib/src/widgets/binding.dart
+++ packages/flutter/lib/src/widgets/binding.dart
@@ -4,6 +4,7 @@
 
 /// @docImport 'dart:ui';
 /// @docImport 'package:flutter/animation.dart';
+/// @docImport 'package:flutter/cupertino.dart';
 /// @docImport 'package:flutter/material.dart';
 /// @docImport 'package:flutter_test/flutter_test.dart';
 ///
